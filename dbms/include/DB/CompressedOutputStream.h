8af888b6a93d6fcf5a5a08ddf94ac8e9ba520cd2
Alexey Milovidov <milovidov@yandex-team.ru>
2009-08-06 19:22:37 +0000

    dbms: added CompressedOutputStream.

---
+++ dbms/include/DB/CompressedOutputStream.h
@@ -0,0 +1,71 @@
+#ifndef DBMS_COMPRESSED_OUTPUT_STREAM_H
+#define DBMS_COMPRESSED_OUTPUT_STREAM_H
+
+#include <istream>
+#include <ostream>
+#include <vector>
+
+#include <Poco/BufferedStreamBuf.h>
+
+#include <quicklz/quicklz_level1.h>
+
+#include <DB/CompressedStream.h>
+
+
+namespace DB
+{
+
+
+/** Аналогично Poco::DeflatingStreamBuf, но используется библиотека QuickLZ,
+  * а также поддерживается только ostream.
+  */
+class CompressingStreamBuf : public Poco::BufferedStreamBuf
+{
+public:
+	CompressingStreamBuf(std::ostream & ostr);
+	~CompressingStreamBuf();
+	int close();
+
+protected:
+	int writeToDevice(const char * buffer, std::streamsize length);
+
+private:
+	size_t pos_in_buffer;
+	std::ostream * p_ostr;
+	std::vector<char> uncompressed_buffer;
+	std::vector<char> compressed_buffer;
+	std::vector<char> scratch;
+
+	/** Сжимает данные, находящиеся в буфере и записывает их. */
+	void writeCompressedChunk();
+};
+
+
+/** Базовый класс для CompressingOutputStream; содержит CompressingStreamBuf
+  */
+class CompressingIOS : public virtual std::ios
+{
+public:
+	CompressingIOS(std::ostream & ostr);
+	CompressingStreamBuf * rdbuf();
+
+protected:
+	CompressingStreamBuf buf;
+};
+
+
+/** Сжимает всё с помощью алгоритма QuickLZ блоками не более DBMS_COMPRESSING_STREAM_BUFFER_SIZE.
+  * Для записи последнего блока, следует вызвать метод close().
+  */
+class CompressedOutputStream : public CompressingIOS, public std::ostream
+{
+public:
+	CompressedOutputStream(std::ostream & ostr);
+	int close();
+};
+
+
+}
+
+
+#endif
---
+++ dbms/src/CompressedOutputStream.cpp
@@ -0,0 +1,99 @@
+#include <algorithm>
+
+#include <DB/CompressedOutputStream.h>
+
+
+namespace DB
+{
+
+
+CompressingStreamBuf::CompressingStreamBuf(std::ostream & ostr)
+	: Poco::BufferedStreamBuf(DBMS_STREAM_BUFFER_SIZE, std::ios::out),
+	pos_in_buffer(0),
+	p_ostr(&ostr),
+	uncompressed_buffer(DBMS_COMPRESSING_STREAM_BUFFER_SIZE),
+	compressed_buffer(DBMS_COMPRESSING_STREAM_BUFFER_SIZE + QUICKLZ_ADDITIONAL_SPACE),
+	scratch(QLZ_SCRATCH_COMPRESS)
+{
+}
+
+
+CompressingStreamBuf::~CompressingStreamBuf()
+{
+	close();
+}
+
+
+void CompressingStreamBuf::writeCompressedChunk()
+{
+	size_t compressed_size = qlz_compress(
+		&uncompressed_buffer[0],
+		&compressed_buffer[0],
+		pos_in_buffer,
+		&scratch[0]);
+
+	p_ostr->write(&compressed_buffer[0], compressed_size);
+	pos_in_buffer = 0;
+}
+
+
+int CompressingStreamBuf::close()
+{
+	sync();
+
+	if (pos_in_buffer != 0)
+		writeCompressedChunk();
+	
+	return 0;
+}
+
+
+int CompressingStreamBuf::writeToDevice(const char * buffer, std::streamsize length)
+{
+	if (length == 0 || !p_ostr)
+		return 0;
+
+	size_t bytes_processed = 0;
+
+	while (bytes_processed < static_cast<size_t>(length))
+	{
+		size_t bytes_to_copy = std::min(uncompressed_buffer.size() - pos_in_buffer, static_cast<size_t>(length));
+		memcpy(&uncompressed_buffer[0], buffer, bytes_to_copy);
+		pos_in_buffer += bytes_to_copy;
+		bytes_processed += bytes_to_copy;
+		
+		if (pos_in_buffer == uncompressed_buffer.size())
+			writeCompressedChunk();
+	}
+
+	return static_cast<int>(length);
+}
+
+
+CompressingIOS::CompressingIOS(std::ostream & ostr)
+	: buf(ostr)
+{
+	poco_ios_init(&buf);
+}
+
+
+CompressingStreamBuf * CompressingIOS::rdbuf()
+{
+	return &buf;
+}
+
+
+CompressedOutputStream::CompressedOutputStream(std::ostream & ostr)
+	: CompressingIOS(ostr),
+	std::ostream(&buf)
+{
+}
+
+
+int CompressedOutputStream::close()
+{
+	return buf.close();
+}
+
+
+}
