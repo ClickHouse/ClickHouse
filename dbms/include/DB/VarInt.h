37d340b4044dc4151cd47d3318a46bfeceb9693b
Alexey Milovidov <milovidov@yandex-team.ru>
2010-01-21 18:23:34 +0000

    VarInt: added method getLengthOfVarUInt().

--- dbms/include/DB/VarInt.h
+++ dbms/include/DB/VarInt.h
@@ -16,6 +16,10 @@ void writeVarUInt(UInt x, std::ostream & ostr);
 void readVarUInt(UInt & x, std::istream & istr);
 
 
+/** Получить длину UInt64 в формате VarUInt */
+size_t getLengthOfVarUInt(UInt x);
+
+
 /** Записать Int64 в формате переменной длины (base128) */
 inline void writeVarInt(Int x, std::ostream & ostr)
 {
@@ -23,11 +27,12 @@ inline void writeVarInt(Int x, std::ostream & ostr)
 }
 
 
+// TODO: здесь баг
 /** Прочитать Int64, записанный в формате переменной длины (base128) */
 inline void readVarInt(Int & x, std::istream & istr)
 {
-	readVarUInt(reinterpret_cast<UInt&>(x), istr);
-	x = ((x >> 1) ^ (x << 63));
+	readVarUInt(*reinterpret_cast<UInt*>(&x), istr);
+	x = (x >> 1) ^ (x << 63);
 }
 
 
--- dbms/src/VarInt.cpp
+++ dbms/src/VarInt.cpp
@@ -137,4 +137,18 @@ void readVarUInt(UInt & x, std::istream & istr)
 }
 
 
+size_t getLengthOfVarUInt(UInt x)
+{
+	return x < (1ULL << 7) ? 1
+		: (x < (1ULL << 14) ? 2
+		: (x < (1ULL << 21) ? 3
+		: (x < (1ULL << 28) ? 4
+		: (x < (1ULL << 35) ? 5
+		: (x < (1ULL << 42) ? 6
+		: (x < (1ULL << 49) ? 7
+		: (x < (1ULL << 56) ? 8
+		: 9)))))));
+}
+
+
 }
