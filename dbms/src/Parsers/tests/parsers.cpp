9a9bdc8123bf932583c8c9861fe594735d9d6dc4
Alexey Milovidov <milovidov@yandex-team.ru>
2010-06-28 13:42:36 +0000

    dbms: development.

--- dbms/src/Parsers/tests/parsers.cpp
+++ dbms/src/Parsers/tests/parsers.cpp
@@ -11,7 +11,7 @@ int main(int argc, char ** argv)
 {
 	DB::ParserSelectQuery parser;
 	DB::ASTPtr ast;
-	std::string input = "SELECT f(1), '\\\\', [a, b, c], (a, b, c), 1 + 2 * -3, a = b OR c > d.1 + 2 * -g[0] AND NOT e < f * (x + y)";
+	std::string input = "SELECT 18446744073709551615, f(1), '\\\\', [a, b, c], (a, b, c), 1 + 2 * -3, a = b OR c > d.1 + 2 * -g[0] AND NOT e < f * (x + y)";
 	std::string expected;
 
 	const char * begin = input.data();
