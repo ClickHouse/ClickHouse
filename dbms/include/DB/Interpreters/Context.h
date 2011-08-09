568a4de64c27bc64b776a18ed682c2ff320563ba
Alexey Milovidov <milovidov@yandex-team.ru>
2011-08-09 19:19:25 +0000

    dbms: development [#CONV-2944].

---
+++ dbms/include/DB/Interpreters/Context.h
@@ -0,0 +1,27 @@
+#pragma once
+
+#include <map>
+
+#include <Poco/SharedPtr.h>
+
+#include <DB/Core/NamesAndTypes.h>
+#include <DB/Functions/IFunction.h>
+
+
+namespace DB
+{
+
+using Poco::SharedPtr;
+
+typedef std::map<String, SharedPtr<IFunction> > Functions;
+
+/** Набор известных объектов, которые могут быть использованы в запросе.
+  */
+struct Context
+{
+	Functions functions;
+	NamesAndTypes columns;
+};
+
+
+}
