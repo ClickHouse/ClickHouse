9166338e0b36acc16be77dd0de3470caaa14cae0
Alexey Milovidov <milovidov@yandex-team.ru>
2011-11-21 10:50:01 +0000

    WriteHelpers: improvement [#CONV-3632].

--- dbms/include/DB/IO/WriteHelpers.h
+++ dbms/include/DB/IO/WriteHelpers.h
@@ -175,7 +175,11 @@ void writeAnyEscapedString(const String & s, WriteBuffer & buf)
 
 inline void writeEscapedString(const String & s, WriteBuffer & buf)
 {
-	writeAnyEscapedString<'\''>(s, buf);
+	/// strpbrk хорошо оптимизирована (этот if ускоряет код в 1.5 раза)
+	if (NULL == strpbrk(s.data(), "\b\f\n\r\t\'\\") && strlen(s.data()) == s.size())
+		writeString(s, buf);
+	else
+		writeAnyEscapedString<'\''>(s, buf);
 }
 
 
