13d2f82f1b4dae849c8b912cbac5acb4262b07ca
Alexey Milovidov <milovidov@yandex-team.ru>
2011-11-21 10:52:05 +0000

    WriteHelpers: added comment [#CONV-3632].

--- dbms/include/DB/IO/WriteHelpers.h
+++ dbms/include/DB/IO/WriteHelpers.h
@@ -175,7 +175,7 @@ void writeAnyEscapedString(const String & s, WriteBuffer & buf)
 
 inline void writeEscapedString(const String & s, WriteBuffer & buf)
 {
-	/// strpbrk хорошо оптимизирована (этот if ускоряет код в 1.5 раза)
+	/// strpbrk в libc под Linux на процессорах с SSE 4.2 хорошо оптимизирована (этот if ускоряет код в 1.5 раза)
 	if (NULL == strpbrk(s.data(), "\b\f\n\r\t\'\\") && strlen(s.data()) == s.size())
 		writeString(s, buf);
 	else
