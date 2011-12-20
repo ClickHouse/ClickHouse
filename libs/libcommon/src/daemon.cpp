11e5fb35ecb52e7645543fe6174b0a9744e0f353
Alexey Milovidov <milovidov@yandex-team.ru>
2011-12-20 19:30:06 +0000

    Daemon: added stack trace in case of DB::Exception [#CONV-2807].

--- libs/libcommon/src/daemon.cpp
+++ libs/libcommon/src/daemon.cpp
@@ -27,6 +27,8 @@
 #include <Poco/ErrorHandler.h>
 #include <Poco/NumberFormatter.h>
 
+#include <DB/Core/Exception.h>
+
 #include "revision.h"
 
 
@@ -273,6 +275,11 @@ void Daemon::handleNotification(TaskFailedNotification *_tfn)
 	AutoPtr<TaskFailedNotification> fn(_tfn);
 	Logger *lg = &(logger());
 	LOG_ERROR(lg, "Task '" << fn->task()->name() << "' failed. Daemon is shutting down. Reason - " << fn->reason().displayText());
+
+	if (const DB::Exception * e = dynamic_cast<const DB::Exception *>(&fn->reason()))
+	{
+		LOG_ERROR(lg, "Stack trace:\n" << e->getStackTrace().toString());
+	}
 
 	ServerApplication::terminate();
 }
