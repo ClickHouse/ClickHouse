6852d6446bd9069deda017b386ab5879be21179a
Alexey Milovidov <milovidov@yandex-team.ru>
2012-02-02 23:44:59 +0000

    DB/IO: fixed copyData() function [#CONV-4023].

--- dbms/src/IO/copyData.cpp
+++ dbms/src/IO/copyData.cpp
@@ -6,9 +6,11 @@ namespace DB
 
 void copyData(ReadBuffer & from, WriteBuffer & to)
 {
+	/// Если дочитали до конца буфера, eof() либо заполнит буфер новыми данными и переместит курсор в начало, либо вернёт false.
 	while (!from.eof())
 	{
-		to.write(from.buffer().begin(), from.buffer().end() - from.buffer().begin());
+		/// buffer() - кусок данных, доступных для чтения; position() - курсор места, до которого уже дочитали.
+		to.write(from.position(), from.buffer().end() - from.position());
 		from.position() = from.buffer().end();
 	}
 }
