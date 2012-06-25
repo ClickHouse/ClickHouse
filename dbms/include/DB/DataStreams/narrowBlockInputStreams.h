384b6b26cea5c4d31a4f3ec7e7af8b909d6b28e1
Alexey Milovidov <milovidov@yandex-team.ru>
2012-06-25 03:04:34 +0000

    dbms: development [#CONV-2944].

--- dbms/include/DB/DataStreams/narrowBlockInputStreams.h
+++ dbms/include/DB/DataStreams/narrowBlockInputStreams.h
@@ -1,6 +1,6 @@
 #pragma once
 
-#include <DB/DataStreams/ConcatBlockInputStream.h>
+#include <DB/DataStreams/IBlockInputStream.h>
 
 
 namespace DB
@@ -12,30 +12,6 @@ namespace DB
   *
   * Старается клеить источники друг с другом равномерно.
   */
-BlockInputStreams narrowBlockInputStreams(BlockInputStreams & inputs, size_t width)
-{
-	if (inputs.size() <= width)
-		return inputs;
-
-	std::vector<BlockInputStreams> partitions(width);
-
-	/** Для распределения, используем генератор случайных чисел,
-	  * но не инициализируем его, чтобы результат был детерминированным.
-	  */
-	drand48_data rand_buf = {{0}};
-	long rand_res = 0;
-	
-	for (size_t i = 0, size = inputs.size(); i < size; ++i)
-	{
-		lrand48_r(&rand_buf, &rand_res);
-		partitions[rand_res % width].push_back(inputs[i]);	/// Теоретически, не слишком хорошо.
-	}
-
-	BlockInputStreams res(width);
-	for (size_t i = 0; i < width; ++i)
-		res[i] = new ConcatBlockInputStream(partitions[i]);
-
-	return res;
-}
+BlockInputStreams narrowBlockInputStreams(BlockInputStreams & inputs, size_t width);
 
 }
