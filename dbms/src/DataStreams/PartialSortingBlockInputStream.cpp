85b5ec6bd6fb9a20f3de1242dfbfb449effdef48
Alexey Milovidov <milovidov@yandex-team.ru>
2012-07-17 14:37:43 +0000

    dbms: development [#CONV-2944].

--- dbms/src/DataStreams/PartialSortingBlockInputStream.cpp
+++ dbms/src/DataStreams/PartialSortingBlockInputStream.cpp
@@ -1,3 +1,5 @@
+#include <DB/Interpreters/sortBlock.h>
+
 #include <DB/DataStreams/PartialSortingBlockInputStream.h>
 
 
@@ -5,72 +7,10 @@ namespace DB
 {
 
 
-struct PartialSortingLess
-{
-	typedef std::vector<std::pair<const IColumn *, int> > Columns;
-	Columns columns;
-
-	PartialSortingLess(const Block & block, const SortDescription & description)
-	{
-		for (size_t i = 0, size = description.size(); i < size; ++i)
-			columns.push_back(std::make_pair(
-				!description[i].column_name.empty()
-					? &*block.getByName(description[i].column_name).column
-					: &*block.getByPosition(description[i].column_number).column,
-				description[i].direction));
-	}
-
-	bool operator() (size_t a, size_t b) const
-	{
-		for (Columns::const_iterator it = columns.begin(); it != columns.end(); ++it)
-		{
-			int res = it->second * it->first->compareAt(a, b, *it->first);
-			if (res < 0)
-				return true;
-			else if (res > 0)
-				return false;
-		}
-		return false;
-	}
-};
-
-
 Block PartialSortingBlockInputStream::readImpl()
 {
 	Block res = input->read();
-	if (!res)
-		return res;
-
-	/// Если столбец сортировки один
-	if (description.size() == 1)
-	{
-		IColumn::Permutation perm = (!description[0].column_name.empty()
-			? res.getByName(description[0].column_name).column
-			: res.getByPosition(description[0].column_number).column)->getPermutation();
-
-		if (description[0].direction == -1)
-			for (size_t i = 0, size = perm.size(); i < size / 2; ++i)
-				std::swap(perm[i], perm[size - 1 - i]);
-		
-		size_t columns = res.columns();
-		for (size_t i = 0; i < columns; ++i)
-			res.getByPosition(i).column->permute(perm);
-	}
-	else
-	{
-		size_t size = res.rows();
-		IColumn::Permutation perm(size);
-		for (size_t i = 0; i < size; ++i)
-			perm[i] = i;
-
-		PartialSortingLess less(res, description);
-		std::sort(perm.begin(), perm.end(), less);
-
-		size_t columns = res.columns();
-		for (size_t i = 0; i < columns; ++i)
-			res.getByPosition(i).column->permute(perm);
-	}
-
+	sortBlock(res, description);
 	return res;
 }
 
