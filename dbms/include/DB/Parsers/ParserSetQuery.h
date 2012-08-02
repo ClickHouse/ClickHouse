f43a0696271f943c5049db09ad0734885f9df8bf
Alexey Milovidov <milovidov@yandex-team.ru>
2012-08-02 19:05:18 +0000

    dbms: development [#CONV-2944].

---
+++ dbms/include/DB/Parsers/ParserSetQuery.h
@@ -0,0 +1,20 @@
+#pragma once
+
+#include <DB/Parsers/IParserBase.h>
+#include <DB/Parsers/ExpressionElementParsers.h>
+
+
+namespace DB
+{
+
+/** Запрос типа такого:
+  * SET [GLOBAL] name1 = value1, name2 = value2, ...
+  */
+class ParserSetQuery : public IParserBase
+{
+protected:
+	String getName() { return "SET query"; }
+	bool parseImpl(Pos & pos, Pos end, ASTPtr & node, String & expected);
+};
+
+}
