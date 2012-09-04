a17bf6f10e56f07e5b0753eb50ed65eb04f756f3
Alexey Milovidov <milovidov@yandex-team.ru>
2012-09-04 19:57:17 +0000

    dbms: probably fixed error [#CONV-2944].

--- dbms/include/DB/DataStreams/UnionBlockInputStream.h
+++ dbms/include/DB/DataStreams/UnionBlockInputStream.h
@@ -14,22 +14,6 @@ namespace DB
 using Poco::SharedPtr;
 
 
-/** Будем использовать Poco::ThreadPool вместо boost::threadpool.
-  * Последний неудобен тем, что в нём не совсем так как надо узнаётся количество свободных потоков.
-  */
-
-class RunnableFromFunction : public Poco::Runnable
-{
-public:
-	typedef boost::function<void()> Func;
-	
-	RunnableFromFunction(const Func & func_) : func(func_) {}
-	void run() { func(); }
-
-private:
-	Func func;
-};
-
 
 /** Объединяет несколько источников в один.
   * Блоки из разных источников перемежаются друг с другом произвольным образом.
@@ -38,6 +22,7 @@ private:
   */
 class UnionBlockInputStream : public IProfilingBlockInputStream
 {
+	class Thread;
 public:
 	UnionBlockInputStream(BlockInputStreams inputs_, unsigned max_threads_ = 1)
 		: max_threads(std::min(inputs_.size(), static_cast<size_t>(max_threads_))),
@@ -48,9 +33,9 @@ public:
 
 		for (size_t i = 0; i < inputs_.size(); ++i)
 		{
-			threads_data.push_back(ThreadData());
-			threads_data.back().in = inputs_[i];
-			threads_data.back().i = i;
+			inputs_data.push_back(InputData());
+			inputs_data.back().in = inputs_[i];
+			inputs_data.back().i = i;
 		}
 	}
 
@@ -63,7 +48,7 @@ public:
 			{
 				Poco::ScopedLock<Poco::FastMutex> lock(mutex);
 
-				if (threads_data.empty())
+				if (inputs_data.empty())
 					return res;
 
 				ssize_t max_threads_to_start = pool.available();
@@ -74,22 +59,21 @@ public:
 	//				std::cerr << "Starting initial threads" << std::endl;
 
 					ssize_t started_threads = 0;
-					ThreadsData::iterator it = threads_data.begin();
-					while (it != threads_data.end() && 0 == it->count)
+					InputsData::iterator it = inputs_data.begin();
+					while (it != inputs_data.end() && 0 == it->count)
 					{
 	//					std::cerr << "Scheduling initial " << it->i << std::endl;
 						++it->count;
 						++started_threads;
 
-						it->runnable = new RunnableFromFunction(RunnableFromFunction::Func(
-							boost::bind(&UnionBlockInputStream::calculate, this, boost::ref(threads_data.back()))));
-						RunnableFromFunction & runnable = *it->runnable;
+						it->thread = new Thread(*this, inputs_data.back());
+						Thread & thread = *it->thread;
 
 						/// Переносим этот источник в конец списка
-						threads_data.push_back(*it);
-						threads_data.erase(it++);
+						inputs_data.push_back(*it);
+						inputs_data.erase(it++);
 												
-						pool.start(runnable);
+						pool.start(thread);
 
 						if (started_threads == max_threads_to_start)
 							break;
@@ -105,7 +89,7 @@ public:
 
 	//			std::cerr << std::endl << "pool.pending: " << pool.pending() << ", pool.active: " << pool.active() << ", pool.size: " << pool.size() << std::endl;
 	
-				if (threads_data.empty())
+				if (inputs_data.empty())
 					return res;
 
 	//			std::cerr << "Searching for first ready block" << std::endl;
@@ -113,8 +97,8 @@ public:
 				/** Найдём и вернём готовый непустой блок, если такой есть.
 				  * При чём, выберем блок из источника, из которого было получено меньше всего блоков.
 				  */
-				ThreadsData::iterator it = threads_data.begin();
-				while (it != threads_data.end())
+				InputsData::iterator it = inputs_data.begin();
+				while (it != inputs_data.end())
 				{
 					if (it->exception)
 						it->exception->rethrow();
@@ -122,7 +106,7 @@ public:
 					if (it->ready)
 					{
 						if (!it->block)
-							threads_data.erase(it++);
+							inputs_data.erase(it++);
 						else
 							break;
 					}
@@ -130,7 +114,7 @@ public:
 						++it;
 				}
 
-				if (it == threads_data.end())
+				if (it == inputs_data.end())
 				{
 	//				std::cerr << "Continue" << std::endl;
 					continue;
@@ -144,9 +128,8 @@ public:
 				it->reset();
 	//			std::cerr << "Scheduling again " << it->i << std::endl;
 				++it->count;
-				it->runnable = new RunnableFromFunction(RunnableFromFunction::Func(
-					boost::bind(&UnionBlockInputStream::calculate, this, boost::ref(*it))));
-				pool.start(*it->runnable);
+				it->thread = new Thread(*this, *it);
+				pool.start(*it->thread);
 
 				return res;
 			}
@@ -165,12 +148,15 @@ public:
 private:
 	unsigned max_threads;
 
+	/** Будем использовать Poco::ThreadPool вместо boost::threadpool.
+	  * Последний неудобен тем, что в нём не совсем так как надо узнаётся количество свободных потоков.
+	  */
 	Poco::ThreadPool pool;
 
 	/// Данные отдельного источника
-	struct ThreadData
+	struct InputData
 	{
-		SharedPtr<RunnableFromFunction> runnable;
+		SharedPtr<Thread> thread;
 		BlockInputStreamPtr in;
 		unsigned count;	/// Сколько блоков было вычислено
 		bool ready;		/// Блок уже вычислен
@@ -183,51 +169,59 @@ private:
 			ready = false;
 			block = Block();
 			exception = NULL;
-			runnable = NULL;
+			thread = NULL;
 		}
 
-		ThreadData() : count(0), ready(false), i(0) {}
+		InputData() : count(0), ready(false), i(0) {}
+	};
+
+	class Thread : public Poco::Runnable
+	{
+	public:
+		Thread(UnionBlockInputStream & parent_, InputData & data_) : parent(parent_), data(data_) {}
+
+		void run()
+		{
+			try
+			{
+				Block block = data.in->read();
+
+				{
+					Poco::ScopedLock<Poco::FastMutex> lock(parent.mutex);
+					data.ready = true;
+					data.block = block;
+				}
+			}
+			catch (const Exception & e)
+			{
+				data.exception = e.clone();
+			}
+			catch (const Poco::Exception & e)
+			{
+				data.exception = e.clone();
+			}
+			catch (const std::exception & e)
+			{
+				data.exception = new Exception(e.what(), ErrorCodes::STD_EXCEPTION);
+			}
+			catch (...)
+			{
+				data.exception = new Exception("Unknown exception", ErrorCodes::UNKNOWN_EXCEPTION);
+			}
+
+			parent.ready_any.set();
+		}
+
+	private:
+		UnionBlockInputStream & parent;
+		InputData & data;
 	};
 
 	/// Список упорядочен по количеству полученных из источника блоков.
-	typedef std::list<ThreadData> ThreadsData;
-	ThreadsData threads_data;
+	typedef std::list<InputData> InputsData;
+	InputsData inputs_data;
 	Poco::FastMutex mutex;
 	Poco::Semaphore ready_any;
-
-	
-	/// Вычисления, которые выполняться в отдельном потоке
-	void calculate(ThreadData & data)
-	{
-		try
-		{
-			Block block = data.in->read();
-
-			{
-				Poco::ScopedLock<Poco::FastMutex> lock(mutex);
-				data.ready = true;
-				data.block = block;
-			}
-		}
-		catch (const Exception & e)
-		{
-			data.exception = e.clone();
-		}
-		catch (const Poco::Exception & e)
-		{
-			data.exception = e.clone();
-		}
-		catch (const std::exception & e)
-		{
-			data.exception = new Exception(e.what(), ErrorCodes::STD_EXCEPTION);
-		}
-		catch (...)
-		{
-			data.exception = new Exception("Unknown exception", ErrorCodes::UNKNOWN_EXCEPTION);
-		}
-
-		ready_any.set();
-	}
 };
 
 }
