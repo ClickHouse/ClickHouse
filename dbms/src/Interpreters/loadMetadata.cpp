3238c9a5ae5bb7d455f107aa21acdd51669e4677
Alexey Milovidov <milovidov@yandex-team.ru>
2013-01-17 19:39:02 +0000

    dbms: probably faster loading [#CONV-2944].

--- dbms/src/Interpreters/loadMetadata.cpp
+++ dbms/src/Interpreters/loadMetadata.cpp
@@ -65,9 +65,6 @@ void loadMetadata(Context & context)
 		/// Цикл по таблицам
 		for (Poco::DirectoryIterator jt(it->path()); jt != dir_end; ++jt)
 		{
-			if (jt->isDirectory())
-				continue;
-			
 			/// Файлы имеют имена вида table_name.sql
 			if (jt.name().compare(jt.name().size() - 4, 4, ".sql"))
 				throw Exception("Incorrect file extension: " + jt.name() + " in metadata directory " + it->path(), ErrorCodes::INCORRECT_FILE_NAME);
