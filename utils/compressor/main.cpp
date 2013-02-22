0f2103247d9d9ee7659065e17504a437658d3d5f
Alexey Milovidov <milovidov@yandex-team.ru>
2013-02-22 19:24:56 +0000

    compressor: added 'stat' mode [#CONV-2944].

--- utils/compressor/main.cpp
+++ utils/compressor/main.cpp
@@ -1,12 +1,34 @@
 #include <iostream>
 
-#include <Poco/SharedPtr.h>
-
 #include <DB/IO/WriteBufferFromFileDescriptor.h>
 #include <DB/IO/ReadBufferFromFileDescriptor.h>
 #include <DB/IO/CompressedWriteBuffer.h>
 #include <DB/IO/CompressedReadBuffer.h>
+#include <DB/IO/WriteHelpers.h>
 #include <DB/IO/copyData.h>
+
+
+/// Выводит размеры разжатых и сжатых блоков для сжатого файла.
+void stat(DB::ReadBuffer & in, DB::WriteBuffer & out)
+{
+	char header[QUICKLZ_HEADER_SIZE];
+	
+	in.ignore(16);	/// checksum
+	in.readStrict(header, QUICKLZ_HEADER_SIZE);
+
+	size_t size_compressed = qlz_size_compressed(header);
+	if (size_compressed > DBMS_MAX_COMPRESSED_SIZE)
+		throw DB::Exception("Too large size_compressed. Most likely corrupted data.", DB::ErrorCodes::TOO_LARGE_SIZE_COMPRESSED);
+
+	size_t size_decompressed = qlz_size_decompressed(header);
+
+	DB::writeText(size_decompressed, out);
+	DB::writeChar('\t', out);
+	DB::writeText(size_compressed, out);
+ 	DB::writeChar('\n', out);
+
+	in.ignore(size_compressed - QUICKLZ_HEADER_SIZE);
+}
 
 
 int main(int argc, char ** argv)
@@ -15,40 +37,43 @@ int main(int argc, char ** argv)
 	{
 		bool decompress = false;
 		bool use_qlz = false;
+		bool stat_mode = false;
 
 		if (argc == 2)
 		{
 			decompress = 0 == strcmp(argv[1], "-d");
 			use_qlz = 0 == strcmp(argv[1], "--qlz");
+			stat_mode = 0 == strcmp(argv[1], "--stat");
 		}
 		
-		if (argc > 2 || (argc == 2 && !decompress && !use_qlz))
+		if (argc > 2 || (argc == 2 && !decompress && !use_qlz && !stat_mode))
 		{
-			std::cerr << "Usage: " << argv[0] << " [-d|--qlz] < in > out" << std::endl;
+			std::cerr << "Usage: " << argv[0] << " [-d|--qlz|--stat] < in > out" << std::endl;
 			return 1;
 		}
 
 		DB::CompressionMethod::Enum method = use_qlz ? DB::CompressionMethod::QuickLZ : DB::CompressionMethod::LZ4;
 
-		Poco::SharedPtr<DB::ReadBuffer> rb = new DB::ReadBufferFromFileDescriptor(STDIN_FILENO);
-		Poco::SharedPtr<DB::WriteBuffer> wb = new DB::WriteBufferFromFileDescriptor(STDOUT_FILENO);
-		Poco::SharedPtr<DB::ReadBuffer> from;
-		Poco::SharedPtr<DB::WriteBuffer> to;
+		DB::ReadBufferFromFileDescriptor rb(STDIN_FILENO);
+		DB::WriteBufferFromFileDescriptor wb(STDOUT_FILENO);
 
-		if (!decompress)
+		if (stat_mode)
 		{
-			/// Сжатие
-			from = rb;
-			to = new DB::CompressedWriteBuffer(*wb, method);
+			/// Вывести статистику для сжатого файла.
+			stat(rb, wb);
+		}
+		else if (decompress)
+		{
+			/// Разжатие
+			DB::CompressedReadBuffer from(rb);
+			DB::copyData(from, wb);
 		}
 		else
 		{
-			/// Разжатие
-			from = new DB::CompressedReadBuffer(*rb);
-			to = wb;
+			/// Сжатие
+			DB::CompressedWriteBuffer to(wb, method);
+			DB::copyData(rb, to);
 		}
-
-		DB::copyData(*from, *to);
 	}
 	catch (const DB::Exception & e)
 	{
