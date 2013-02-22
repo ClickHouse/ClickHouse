9f60383fee804a3e55e65b984ae6d320362bb6bb
Michael Kolupaev <mkolupaev@yandex-team.ru>
2013-02-22 09:41:13 +0000

    clickhouse: fixed comments [#CONV-6766].

--- dbms/include/DB/Interpreters/InterpreterDescribeQuery.h
+++ dbms/include/DB/Interpreters/InterpreterDescribeQuery.h
@@ -16,89 +16,89 @@ namespace DB
 {
 	
 	
-	/** Проверить, существует ли таблица. Вернуть одну строку с одним столбцом result типа UInt8 со значением 0 или 1.
-	 */
-	class InterpreterDescribeQuery
+/** Вернуть названия и типы столбцов указанной таблицы.
+	*/
+class InterpreterDescribeQuery
+{
+public:
+	InterpreterDescribeQuery(ASTPtr query_ptr_, Context & context_)
+		: query_ptr(query_ptr_), context(context_) {}
+	
+	BlockIO execute()
 	{
-	public:
-		InterpreterDescribeQuery(ASTPtr query_ptr_, Context & context_)
-			: query_ptr(query_ptr_), context(context_) {}
+		BlockIO res;
+		res.in = executeImpl();
+		res.in_sample = getSampleBlock();
 		
-		BlockIO execute()
+		return res;
+	}
+	
+	BlockInputStreamPtr executeAndFormat(WriteBuffer & buf)
+	{
+		Block sample = getSampleBlock();
+		ASTPtr format_ast = dynamic_cast<ASTDescribeQuery &>(*query_ptr).format;
+		String format_name = format_ast ? dynamic_cast<ASTIdentifier &>(*format_ast).name : "TabSeparated";
+		
+		BlockInputStreamPtr in = executeImpl();
+		BlockOutputStreamPtr out = context.getFormatFactory().getOutput(format_name, buf, sample);
+		
+		copyData(*in, *out);
+		
+		return in;
+	}
+	
+private:
+	ASTPtr query_ptr;
+	Context context;
+	
+	Block getSampleBlock()
+	{
+		Block block;
+		
+		ColumnWithNameAndType col;
+		col.name = "name";
+		col.type = new DataTypeString;
+		
+		block.insert(col);
+		
+		col.name = "type";
+		
+		block.insert(col);
+		
+		return block;
+	}
+	
+	BlockInputStreamPtr executeImpl()
+	{
+		const ASTDescribeQuery & ast = dynamic_cast<const ASTDescribeQuery &>(*query_ptr);
+		
+		NamesAndTypesList columns;
+		
 		{
-			BlockIO res;
-			res.in = executeImpl();
-			res.in_sample = getSampleBlock();
+			Poco::ScopedLock<Poco::Mutex> lock(context.getMutex());
 			
-			return res;
+			if (!context.isTableExist(ast.database, ast.table))
+				throw Exception("Table " + (ast.database.empty() ? "" : ast.database + ".") + ast.table + " doesn't exist", ErrorCodes::UNKNOWN_TABLE);
+			
+			columns = context.getTable(ast.database, ast.table)->getColumnsList();
 		}
 		
-		BlockInputStreamPtr executeAndFormat(WriteBuffer & buf)
+		ColumnString * name_column = new ColumnString;
+		ColumnString * type_column = new ColumnString;
+		
+		Block block;
+		block.insert(ColumnWithNameAndType(name_column, new DataTypeString, "name"));
+		block.insert(ColumnWithNameAndType(type_column, new DataTypeString, "type"));
+		
+		for (NamesAndTypesList::iterator it = columns.begin(); it != columns.end(); ++it)
 		{
-			Block sample = getSampleBlock();
-			ASTPtr format_ast = dynamic_cast<ASTDescribeQuery &>(*query_ptr).format;
-			String format_name = format_ast ? dynamic_cast<ASTIdentifier &>(*format_ast).name : "TabSeparated";
-			
-			BlockInputStreamPtr in = executeImpl();
-			BlockOutputStreamPtr out = context.getFormatFactory().getOutput(format_name, buf, sample);
-			
-			copyData(*in, *out);
-			
-			return in;
+			name_column->insert(it->first);
+			type_column->insert(it->second->getName());
 		}
 		
-	private:
-		ASTPtr query_ptr;
-		Context context;
-		
-		Block getSampleBlock()
-		{
-			Block block;
-			
-			ColumnWithNameAndType col;
-			col.name = "name";
-			col.type = new DataTypeString;
-			
-			block.insert(col);
-			
-			col.name = "type";
-			
-			block.insert(col);
-			
-			return block;
-		}
-		
-		BlockInputStreamPtr executeImpl()
-		{
-			const ASTDescribeQuery & ast = dynamic_cast<const ASTDescribeQuery &>(*query_ptr);
-			
-			NamesAndTypesList columns;
-			
-			{
-				Poco::ScopedLock<Poco::Mutex> lock(context.getMutex());
-				
-				if (!context.isTableExist(ast.database, ast.table))
-					throw Exception("Table " + (ast.database.empty() ? "" : ast.database + ".") + ast.table + " doesn't exist", ErrorCodes::UNKNOWN_TABLE);
-				
-				columns = context.getTable(ast.database, ast.table)->getColumnsList();
-			}
-			
-			ColumnString * name_column = new ColumnString;
-			ColumnString * type_column = new ColumnString;
-			
-			Block block;
-			block.insert(ColumnWithNameAndType(name_column, new DataTypeString, "name"));
-			block.insert(ColumnWithNameAndType(type_column, new DataTypeString, "type"));
-			
-			for (NamesAndTypesList::iterator it = columns.begin(); it != columns.end(); ++it)
-			{
-				name_column->insert(it->first);
-				type_column->insert(it->second->getName());
-			}
-			
-			return new OneBlockInputStream(block);
-		}
-	};
+		return new OneBlockInputStream(block);
+	}
+};
 	
 	
 }
--- dbms/include/DB/Interpreters/InterpreterShowCreateQuery.h
+++ dbms/include/DB/Interpreters/InterpreterShowCreateQuery.h
@@ -16,81 +16,81 @@ namespace DB
 {
 	
 	
-	/** Проверить, существует ли таблица. Вернуть одну строку с одним столбцом result типа UInt8 со значением 0 или 1.
-	 */
-	class InterpreterShowCreateQuery
+/** Вернуть одну строку с одним столбцом statement типа String с текстом запроса, создающего указанную таблицу.
+	*/
+class InterpreterShowCreateQuery
+{
+public:
+	InterpreterShowCreateQuery(ASTPtr query_ptr_, Context & context_)
+		: query_ptr(query_ptr_), context(context_) {}
+	
+	BlockIO execute()
 	{
-	public:
-		InterpreterShowCreateQuery(ASTPtr query_ptr_, Context & context_)
-			: query_ptr(query_ptr_), context(context_) {}
+		BlockIO res;
+		res.in = executeImpl();
+		res.in_sample = getSampleBlock();
 		
-		BlockIO execute()
+		return res;
+	}
+	
+	BlockInputStreamPtr executeAndFormat(WriteBuffer & buf)
+	{
+		Block sample = getSampleBlock();
+		ASTPtr format_ast = dynamic_cast<ASTShowCreateQuery &>(*query_ptr).format;
+		String format_name = format_ast ? dynamic_cast<ASTIdentifier &>(*format_ast).name : "TabSeparated";
+		
+		BlockInputStreamPtr in = executeImpl();
+		BlockOutputStreamPtr out = context.getFormatFactory().getOutput(format_name, buf, sample);
+		
+		copyData(*in, *out);
+		
+		return in;
+	}
+	
+private:
+	ASTPtr query_ptr;
+	Context context;
+	
+	Block getSampleBlock()
+	{
+		ColumnWithNameAndType col;
+		col.name = "statement";
+		col.type = new DataTypeString;
+		
+		Block block;
+		block.insert(col);
+		
+		return block;
+	}
+	
+	BlockInputStreamPtr executeImpl()
+	{
+		const ASTShowCreateQuery & ast = dynamic_cast<const ASTShowCreateQuery &>(*query_ptr);
+		
+		String res;
+		
 		{
-			BlockIO res;
-			res.in = executeImpl();
-			res.in_sample = getSampleBlock();
+			Poco::ScopedLock<Poco::Mutex> lock(context.getMutex());
+		
+			if (!context.isTableExist(ast.database, ast.table))
+				throw Exception("Table " + (ast.database.empty() ? "" : ast.database + ".") + ast.table + " doesn't exist", ErrorCodes::UNKNOWN_TABLE);
 			
-			return res;
+			std::stringstream stream;
+			formatAST(*context.getCreateQuery(ast.database, ast.table), stream, 0, false, true);
+			res = stream.str();
 		}
 		
-		BlockInputStreamPtr executeAndFormat(WriteBuffer & buf)
-		{
-			Block sample = getSampleBlock();
-			ASTPtr format_ast = dynamic_cast<ASTShowCreateQuery &>(*query_ptr).format;
-			String format_name = format_ast ? dynamic_cast<ASTIdentifier &>(*format_ast).name : "TabSeparated";
-			
-			BlockInputStreamPtr in = executeImpl();
-			BlockOutputStreamPtr out = context.getFormatFactory().getOutput(format_name, buf, sample);
-			
-			copyData(*in, *out);
-			
-			return in;
-		}
+		ColumnWithNameAndType col;
+		col.name = "statement";
+		col.type = new DataTypeString;
+		col.column = new ColumnConstString(1, res);
 		
-	private:
-		ASTPtr query_ptr;
-		Context context;
+		Block block;
+		block.insert(col);
 		
-		Block getSampleBlock()
-		{
-			ColumnWithNameAndType col;
-			col.name = "statement";
-			col.type = new DataTypeString;
-			
-			Block block;
-			block.insert(col);
-			
-			return block;
-		}
-		
-		BlockInputStreamPtr executeImpl()
-		{
-			const ASTShowCreateQuery & ast = dynamic_cast<const ASTShowCreateQuery &>(*query_ptr);
-			
-			String res;
-			
-			{
-				Poco::ScopedLock<Poco::Mutex> lock(context.getMutex());
-			
-				if (!context.isTableExist(ast.database, ast.table))
-					throw Exception("Table " + (ast.database.empty() ? "" : ast.database + ".") + ast.table + " doesn't exist", ErrorCodes::UNKNOWN_TABLE);
-				
-				std::stringstream stream;
-				formatAST(*context.getCreateQuery(ast.database, ast.table), stream, 0, false, true);
-				res = stream.str();
-			}
-			
-			ColumnWithNameAndType col;
-			col.name = "statement";
-			col.type = new DataTypeString;
-			col.column = new ColumnConstString(1, res);
-			
-			Block block;
-			block.insert(col);
-			
-			return new OneBlockInputStream(block);
-		}
-	};
+		return new OneBlockInputStream(block);
+	}
+};
 	
 	
 }
