848e3e269c4982c87638726462dc76a46feaccb4
Evgeniy Gatov <egatov@yandex-team.ru>
2013-04-15 00:42:29 +0000

    DBMS, ███████████: fixes/additions [#CONV-4143].

--- dbms/include/DB/Common/escapeForFileName.h
+++ dbms/include/DB/Common/escapeForFileName.h
@@ -29,3 +29,53 @@ inline std::string escapeForFileName(const std::string & s)
 
 	return res;
 }
+
+std::string unescapeForFileName(const std::string & s)
+{
+	std::string res;
+	const char * pos = s.data();
+	const char * end = pos + s.size();
+	
+	while (pos != end)
+	{
+		char c = *pos;
+		
+		if (c != '%')
+			res += c;
+		else
+		{
+			/// пропустим '%'
+			if (++pos == end) break;
+			c = *pos;
+			
+			char val = 0;
+			
+			switch (c)
+			{
+				case '0' ... '9':
+					val = (c - '0') * 16;
+					break;
+				case 'A' ... 'F':
+					val = (c - 'A' + 10) * 16;
+					break;
+			};
+			
+			if (++pos == end) break;
+			c = *pos;
+			
+			switch (c)
+			{
+				case '0' ... '9':
+					val += (c - '0');
+					break;
+				case 'A' ... 'F':
+					val += c - 'A' + 10;
+					break;
+			};
+			res += val;
+		}
+		
+		++pos;
+	}
+	return res;
+}
