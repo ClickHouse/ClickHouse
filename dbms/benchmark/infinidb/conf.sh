21d29773e6fd87b9114abc081cb505da7ae4731e
Pavel Kartavyy <kartavyy@yandex-team.ru>
2013-09-23 12:22:05 +0000

    benchmark: added logs for mysql[#CONV-8419]

--- dbms/benchmark/infinidb/conf.sh
+++ dbms/benchmark/infinidb/conf.sh
@@ -1,4 +1,4 @@
-CONF_DIR=/home/kartavyy/benchmark/infinidb
+CONF_DIR=/home/kartavyy/benchmark/mysql
 expect_file=$CONF_DIR/expect.tcl
 test_file=$CONF_DIR/queries.sql
-etc_init_d_service=/etc/init.d/infinidb
+etc_init_d_service=/etc/init.d/mysql
--- dbms/benchmark/infinidb/expect.tcl
+++ dbms/benchmark/infinidb/expect.tcl
@@ -2,20 +2,22 @@
 #!/bin/expect
 
 # Set timeout
-set timeout 400
+set timeout 600
 
 # Get arguments
 set query [lindex $argv 0]
 
-spawn /usr/local/Calpont/mysql/bin/mysql --defaults-file=/usr/local/Calpont/mysql/my.cnf -u root
+spawn mysql -u root
 
 expect "mysql>"
 send "use hits\r"
 
 expect "mysql>"
+
 send "$query\r"
 
 expect "mysql>"
+
 send "quit\r"
 
 expect eof
