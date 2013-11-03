5954b96c810eca105d8cff058b85e2583d27a703
Alexey Milovidov <milovidov@yandex-team.ru>
2013-11-03 05:34:28 +0000

    dbms: fixed error in prev. revision [#METR-8459].

--- dbms/src/Server/TCPHandler.h
+++ dbms/src/Server/TCPHandler.h
@@ -131,7 +131,7 @@ private:
 	void updateProgress(size_t rows, size_t bytes);
 
 	/// Вывести информацию о скорости выполнения SELECT запроса.
-	void logProfileInfo(Stopwatch & watch);
+	void logProfileInfo(Stopwatch & watch, IBlockInputStream & in);
 };
 
 
