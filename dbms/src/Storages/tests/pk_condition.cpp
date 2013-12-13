bda42aec9978f06bc334ed39fb8e16d1d61283c6
Alexey Milovidov <milovidov@yandex-team.ru>
2013-12-13 00:05:20 +0000

    Fixed build [#METR-2807].

--- dbms/src/Storages/tests/pk_condition.cpp
+++ dbms/src/Storages/tests/pk_condition.cpp
@@ -9,14 +9,14 @@ int main(int argc, const char ** argv)
 	Range range1;
 	Range range2;
 
-	range1.left = UInt64(101);
-	range1.right = UInt64(101);
+	range1.left = DB::UInt64(101);
+	range1.right = DB::UInt64(101);
 	range1.left_bounded = true;
 	range1.right_bounded = true;
 	range1.left_included = true;
 	range1.right_included = true;
 
-	range2.left = Int64(100);
+	range2.left = DB::Int64(100);
 	range2.left_bounded = true;
 	range2.left_included = true;
 
