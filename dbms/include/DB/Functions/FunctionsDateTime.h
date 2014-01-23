2c98354df3f5077c05f2a063b0baff5f988f099d
Sergey Fedorov <fets@yandex-team.ru>
2014-01-23 12:12:55 +0000

    functions: removed couple of unused date time funstions [METR-9582]

--- dbms/include/DB/Functions/FunctionsDateTime.h
+++ dbms/include/DB/Functions/FunctionsDateTime.h
@@ -170,8 +170,8 @@ struct ToRelativeWeekNumImpl
 
 struct ToRelativeDayNumImpl
 {
-	static inline UInt16 execute(UInt32 t, DateLUTSingleton & date_lut) { return date_lut.toRelativeDayNum(t); }
-	static inline UInt16 execute(UInt16 d, DateLUTSingleton & date_lut) { return date_lut.toRelativeDayNum(DayNum_t(d)); }
+	static inline UInt16 execute(UInt32 t, DateLUTSingleton & date_lut) { return date_lut.toDayNum(t); }
+	static inline UInt16 execute(UInt16 d, DateLUTSingleton & date_lut) { return DayNum_t(d); }
 };
 
 
@@ -180,7 +180,7 @@ struct ToRelativeHourNumImpl
 	static inline UInt32 execute(UInt32 t, DateLUTSingleton & date_lut) { return date_lut.toRelativeHourNum(t); }
 	static inline UInt32 execute(UInt16 d, DateLUTSingleton & date_lut)
 	{
-		throw Exception("Illegal type Date of argument for function toStartOfHour", ErrorCodes::ILLEGAL_TYPE_OF_ARGUMENT);
+		throw Exception("Illegal type Date of argument for function ToRelativeHourNum", ErrorCodes::ILLEGAL_TYPE_OF_ARGUMENT);
 	}
 };
 
@@ -189,16 +189,16 @@ struct ToRelativeMinuteNumImpl
 	static inline UInt32 execute(UInt32 t, DateLUTSingleton & date_lut) { return date_lut.toRelativeMinuteNum(t); }
 	static inline UInt32 execute(UInt16 d, DateLUTSingleton & date_lut)
 	{
-		throw Exception("Illegal type Date of argument for function toStartOfHour", ErrorCodes::ILLEGAL_TYPE_OF_ARGUMENT);
+		throw Exception("Illegal type Date of argument for function ToRelativeMinuteNum", ErrorCodes::ILLEGAL_TYPE_OF_ARGUMENT);
 	}
 };
 
 struct ToRelativeSecondNumImpl
 {
-	static inline UInt32 execute(UInt32 t, DateLUTSingleton & date_lut) { return date_lut.toRelativeSecondNum(t); }
+	static inline UInt32 execute(UInt32 t, DateLUTSingleton & date_lut) { return t; }
 	static inline UInt32 execute(UInt16 d, DateLUTSingleton & date_lut)
 	{
-		throw Exception("Illegal type Date of argument for function toStartOfHour", ErrorCodes::ILLEGAL_TYPE_OF_ARGUMENT);
+		throw Exception("Illegal type Date of argument for function ToRelativeSecondNum", ErrorCodes::ILLEGAL_TYPE_OF_ARGUMENT);
 	}
 };
 
