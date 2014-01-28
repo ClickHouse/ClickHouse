e839059abf8bec4ab5a8b9762acb608a7abafef2
Pavel Kartavyy <kartavyy@yandex-team.ru>
2014-01-28 20:25:28 +0000

    added SVN_PATH [#METR-9766]

--- libs/libcommon/src/create_revision.sh.cmake
+++ libs/libcommon/src/create_revision.sh.cmake
@@ -8,6 +8,7 @@ cd ${CMAKE_CURRENT_SOURCE_DIR};
 echo && (LC_ALL=C svn info ${PROJECT_SOURCE_DIR}/ 2>/dev/null || echo Revision 1) | grep Revision | cut -d " " -f 2 >> ${CMAKE_CURRENT_BINARY_DIR}/src/revision.h;
 
 echo -n "#define SVN_PATH " >> ${CMAKE_CURRENT_BINARY_DIR}/src/revision.h
-echo && (LC_ALL=C svn info ./ 2>/dev/null || echo URL: unknown) | grep URL | cut -d " " -f 2 | sed "s/https:\/\/███████████.yandex-team.ru\/conv\///g" >> ${CMAKE_CURRENT_BINARY_DIR}/src/revision.h;
+SVN_PATH="$((LC_ALL=C svn info ./ 2>/dev/null || echo URL: unknown) | grep URL | cut -d " " -f 2 | sed "s/https:\/\/███████████.yandex-team.ru\/conv\///g")"
+echo '"SVN_PATH"' >> ${CMAKE_CURRENT_BINARY_DIR}/src/revision.h;
 
 echo "#endif" >> ${CMAKE_CURRENT_BINARY_DIR}/src/revision.h
