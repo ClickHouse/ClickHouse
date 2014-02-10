217b5cf0138107d3d72114ca8c56dd1b920e8814
Michael Kolupaev <mkolupaev@yandex-team.ru>
2014-02-10 12:10:18 +0000

    clickhouse: better test for PKCondition. [#METR-9835]

--- dbms/src/Storages/tests/pk_condition.cpp
+++ dbms/src/Storages/tests/pk_condition.cpp
@@ -1,26 +1,52 @@
 #include <iostream>
 #include <DB/Storages/MergeTree/PKCondition.h>
+#include <DB/Parsers/ParserSelectQuery.h>
+#include <DB/DataTypes/DataTypesNumberFixed.h>
 
+using namespace DB;
+
+std::unique_ptr<PKCondition> cond;
+
+void check(UInt64 left, UInt64 right, bool can_be_true)
+{
+	Field fleft = left;
+	Field fright = right;
+	if (cond->mayBeTrueInRange(&fleft, &fright) != can_be_true)
+	{
+		std::cout << "failed range [" << left << ", " << right << "]" << std::endl;
+		exit(2);
+	}
+}
 
 int main(int argc, const char ** argv)
 {
-	using namespace DB;
+	ParserSelectQuery parser;
+	std::string query = "SELECT count() FROM pre.t WHERE (key > 9000 AND key < 100000 OR key > 200000 AND key < 1000000 OR key > 3000000 AND key < 8000000 OR key > 12000000)";
+	ASTPtr ast;
+	IParser::Pos pos = &query[0];
+	std::string error;
+	if (!parser.parse(pos, &query[0] + query.size(), ast, error))
+	{
+		std::cout << "couldn't parse query" << std::endl;
+		return 1;
+	}
+	Context context;
+	NamesAndTypesList columns;
+	columns.push_back(NameAndTypePair("key", new DataTypeUInt64));
+	SortDescription sort_descr;
+	sort_descr.push_back(SortColumnDescription("key", 1));
 
-	Range range1;
-	Range range2;
+	cond.reset(new PKCondition(ast, context, columns, sort_descr));
+	std::cout << "condition: " << cond->toString() << std::endl;
 
-	range1.left = DB::UInt64(101);
-	range1.right = DB::UInt64(101);
-	range1.left_bounded = true;
-	range1.right_bounded = true;
-	range1.left_included = true;
-	range1.right_included = true;
+	check(100, 1000, false);
+	check(1000, 9000, false);
+	check(1000, 9001, true);
+	check(9000, 9001, true);
+	check(9001, 9001, true);
+	check(120000, 130000, false);
 
-	range2.left = DB::Int64(100);
-	range2.left_bounded = true;
-	range2.left_included = true;
-
-	std::cerr << range1.toString() << (range1.intersectsRange(range2) ? " intersects " : " not intersects ") << range2.toString() << std::endl;
+	std::cout << "passed" << std::endl;
 
 	return 0;
 }
