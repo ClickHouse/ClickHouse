4d4aeb2ee9bdaea98fe628489f8136f154bcf89e
Michael Kolupaev <mkolupaev@yandex-team.ru>
2014-04-02 14:38:27 +0400

    zkutil: Inherited KeeperException from DB::Exception. [#METR-10202]

--- libs/libzkutil/include/zkutil/KeeperException.h
+++ libs/libzkutil/include/zkutil/KeeperException.h
@@ -1,15 +1,15 @@
 #pragma once
-#include <Poco/Exception.h>
+#include <statdaemons/Exception.h>
 #include <zkutil/Types.h>
 
 
 namespace zkutil
 {
 
-class KeeperException : public Poco::Exception
+class KeeperException : public DB::Exception
 {
 public:
-	KeeperException(const std::string & msg) : Poco::Exception(msg), code(ReturnCode::Ok) {}
+	KeeperException(const std::string & msg) : DB::Exception(msg), code(ReturnCode::Ok) {}
 	KeeperException(const std::string & msg, ReturnCode::type code_)
 		: Poco::Exception(msg + " (" + ReturnCode::toString(code_) + ")"), code(code_) {}
 	KeeperException(ReturnCode::type code_)
