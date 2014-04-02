5542aa8f2d2a75d7368c6eb1c38d77c82e9bda0f
Michael Kolupaev <mkolupaev@yandex-team.ru>
2014-04-02 14:29:54 +0400

    Moved DB::Exception and StackTrace to statdaemons (to allow zkutil to have stack trace in exceptions without depending on dbms). [#METR-10202]

--- dbms/src/Core/Exception.cpp
+++ dbms/src/Core/Exception.cpp
@@ -11,47 +11,6 @@
 namespace DB
 {
 
-Exception::Exception(int code): Poco::Exception(code) {}
-Exception::Exception(const std::string & msg, int code) : Poco::Exception(msg, code) {}
-Exception::Exception(const std::string & msg, const std::string & arg, int code): Poco::Exception(msg, arg, code) {}
-Exception::Exception(const std::string & msg, const Exception & exc, int code): Poco::Exception(msg, exc, code), trace(exc.trace) {}
-Exception::Exception(const Exception & exc) : Poco::Exception(exc), trace(exc.trace) {}
-Exception::Exception(const Poco::Exception & exc) : Poco::Exception(exc.displayText()) {}
-Exception::~Exception() throw() {}
-
-Exception & Exception::operator=(const Exception& exc)
-{
-	Poco::Exception::operator=(exc);
-	trace = exc.trace;	
-	return *this;
-}
-
-const char* Exception::name() const throw()
-{
-	return "DB::Exception";
-}
-
-const char* Exception::className() const throw()
-{
-	return "DB::Exception";
-}
-
-Exception * Exception::clone() const
-{
-	return new Exception(*this);
-}
-
-void Exception::rethrow() const
-{
-	throw *this;
-}
-
-void Exception::addMessage(const std::string & arg)
-{
-	extendedMessage(arg);
-}
-
-
 void throwFromErrno(const std::string & s, int code, int e)
 {
 	char buf[128];
