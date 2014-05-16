226ea78f5b179de3d5e72d0b8dd8ffc08d060332
Pavel Kartavyy <kartavyy@yandex-team.ru>
2014-05-16 16:29:52 +0400

    Revert "improved revision detection"
    This reverts commit 6bfc93eac0b05885e1cbc7954d394f4d64a607e5.

--- libs/libcommon/src/create_revision.sh.cmake
+++ libs/libcommon/src/create_revision.sh.cmake
@@ -8,8 +8,10 @@ cd ${CMAKE_CURRENT_SOURCE_DIR};
 if (git rev-parse --is-inside-work-tree &> /dev/null)
 then
 	# GIT
+	# git describe --tags находит ближайщий tag к текущему комиту с учетом веток.
+	# Если несколько тэгов относятся к одному коммиту, то вернется первый. Поэтому ревизия пакета иногда может отличаться от той версии, что пишет демон
 	git fetch --tags;
-	( git tag || echo 1 ) | tail -1 >> ${CMAKE_CURRENT_BINARY_DIR}/src/revision.h;
+	( git describe --tags || echo 1 ) | cut -d "-" -f 1 >> ${CMAKE_CURRENT_BINARY_DIR}/src/revision.h;
 else
 	#SVN
 	echo && (LC_ALL=C svn info ${PROJECT_SOURCE_DIR}/ 2>/dev/null || echo Revision 1) | grep Revision | cut -d " " -f 2 >> ${CMAKE_CURRENT_BINARY_DIR}/src/revision.h;
