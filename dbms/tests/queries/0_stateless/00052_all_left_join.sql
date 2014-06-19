7c0f53c34fb449e54528f31264c6bba8ec27defe
Alexey Milovidov <milovidov@yandex-team.ru>
2014-06-19 23:00:19 +0400

    dbms: JOINs: added tests [#METR-11370].

---
+++ dbms/tests/queries/0_stateless/00052_all_left_join.sql
@@ -0,0 +1,9 @@
+SELECT *, joined FROM
+(
+    SELECT number AS k FROM system.numbers LIMIT 10
+)
+ALL LEFT JOIN
+(
+    SELECT intDiv(number, 2) AS k, number AS joined FROM system.numbers LIMIT 10
+)
+USING k;
