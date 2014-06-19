a83915b62cc0783455c4cc46fec9b73b1b793a9e
Alexey Milovidov <milovidov@yandex-team.ru>
2014-06-19 23:11:42 +0400

    dbms: JOINs: more tests [#METR-11370].

---
+++ dbms/tests/queries/0_stateless/00054_join_string.sql
@@ -0,0 +1,9 @@
+SELECT *, joined FROM
+(
+    SELECT reinterpretAsString(number + reinterpretAsUInt8('A')) AS k FROM system.numbers LIMIT 10
+)
+ALL LEFT JOIN
+(
+    SELECT reinterpretAsString(intDiv(number, 2) + reinterpretAsUInt8('A')) AS k, number AS joined FROM system.numbers LIMIT 10
+)
+USING k;
