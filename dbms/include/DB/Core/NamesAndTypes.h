d848cfec91cca45ae10dda19d12ffc6c1e9bce4f
Michael Kolupaev <mkolupaev@yandex-team.ru>
2014-07-09 14:46:26 +0400

    dbms: made NameAndTypePair a struct (build is broken). [#METR-10202]

--- dbms/include/DB/Core/NamesAndTypes.h
+++ dbms/include/DB/Core/NamesAndTypes.h
@@ -14,7 +14,22 @@ namespace DB
 
 using Poco::SharedPtr;
 
-typedef std::pair<std::string, DataTypePtr> NameAndTypePair;
+struct NameAndTypePair
+{
+	String name;
+	DataTypePtr type;
+
+	bool operator<(const NameAndTypePair & rhs) const
+	{
+		return std::make_pair(name, type->getName()) < std::make_pair(rhs.name, rhs.type->getName());
+	}
+
+	bool operator==(const NameAndTypePair & rhs) const
+	{
+		return name == rhs.name && type->getName() == rhs.type->getName();
+	}
+};
+
 typedef std::list<NameAndTypePair> NamesAndTypesList;
 typedef SharedPtr<NamesAndTypesList> NamesAndTypesListPtr;
 typedef std::vector<NameAndTypePair> NamesAndTypes;
