4a364a09e34bc05126f74e8d55fc1cac04f3ec56
Andrey Mironov <hertz@yandex-team.ru>
2014-08-13 13:45:39 +0400

    do not create target block unless required. [#METR-12221]

--- dbms/include/DB/Storages/Distributed/DistributedBlockOutputStream.h
+++ dbms/include/DB/Storages/Distributed/DistributedBlockOutputStream.h
@@ -29,13 +29,23 @@ private:
 
 		const auto & key_column = block_with_key.getByName(storage.getShardingKeyColumnName()).column;
 		const auto total_weight = cluster.slot_to_shard.size();
-		/// seems like cloned blocks have the same underlying container
-		Blocks target_blocks(cluster.shard_info_vec.size(), block.cloneEmpty());
+
+		/// shard => block mapping
+		std::unordered_map<size_t, Block> target_blocks;
+		/// return iterator to target block, creating one if necessary
+		const auto get_target_block = [&target_blocks, &block] (const size_t idx) {
+			const auto it = target_blocks.find(idx);
+
+			if (it == std::end(target_blocks))
+				return target_blocks.emplace(idx, block.cloneEmpty()).first;
+
+			return it;
+		};
 
 		for (size_t row = 0; row < block.rows(); ++row)
 		{
 			const auto target_block_idx = key_column->get64(row) % total_weight;
-			auto & target_block = target_blocks[target_block_idx];
+			auto & target_block = get_target_block(target_block_idx)->second;;
 
 			for (size_t col = 0; col < block.columns(); ++col)
 			{
@@ -45,8 +55,8 @@ private:
 			}
 		}
 
-		for (size_t i = 0; i < target_blocks.size(); ++i)
-			writeImpl(target_blocks[i], i);
+		for (const auto & shard_block_pair : target_blocks)
+			writeImpl(shard_block_pair.second, shard_block_pair.first);
 	}
 
 	void writeImpl(const Block & block, const size_t shard_num = 0)
