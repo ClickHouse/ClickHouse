c5635ce6d9633bdb90ed56ea249fc298effa6313
Alexey Milovidov <milovidov@yandex-team.ru>
2014-11-20 03:05:14 +0300

    dbms: added function regionToPopulation; clickhouse-server, ███████████, ███████████, ███████████ needs to be released before regions_hierarchy.txt change [#METR-13326].

--- dbms/scripts/geobase_to_regions_hierarchy.pl
+++ dbms/scripts/geobase_to_regions_hierarchy.pl
@@ -5,7 +5,8 @@ use warnings;
 use geobase;
 
 foreach my $key (keys %Region) {
-	print $key . "\t" 
-		. ($Region{$key}->{parents}[-1] || 0) . "\t" 
-		. ($Region{$key}->{type} || 0) . "\n";
+	print $key . "\t"
+		. ($Region{$key}->{parents}[-1] || 0) . "\t"
+		. ($Region{$key}->{type} || 0) . "\t"
+		. (int($Region{$key}->{zip_old} || 0)) . "\n";	# zip_old, не смотря на название, содержит население региона.
 }
