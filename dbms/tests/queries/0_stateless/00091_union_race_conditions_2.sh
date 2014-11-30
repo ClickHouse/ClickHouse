0abde09572c931d7c76a606c7a872d6dddc38e8e
Alexey Milovidov <milovidov@yandex-team.ru>
2014-11-30 15:01:46 +0300

    dbms: added tests for UnionBlockInputStream [#METR-2944].

---
+++ dbms/tests/queries/0_stateless/00091_union_race_conditions_2.sh
@@ -0,0 +1,3 @@
+#!/bin/bash -e
+
+for i in {1..10}; do seq 1 100 | sed 's/.*/SELECT count() FROM (SELECT * FROM (SELECT * FROM system.numbers_mt LIMIT 111) LIMIT 55);/' | clickhouse-client -n --receive_timeout=1 --max_block_size=1 | grep -vE '^55$' && echo 'Fail!' && break; echo -n '.'; done; echo
