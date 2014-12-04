8533ebe8bd0273753055855ff4f80244018a4049
Andrey Mironov <hertz@yandex-team.ru>
2014-12-04 18:50:48 +0300

    Merge

--- dbms/include/DB/Storages/MergeTree/MergeTreeReader.h
+++ dbms/include/DB/Storages/MergeTree/MergeTreeReader.h
@@ -58,23 +58,6 @@ public:
 		}
 	}
 
-	const NameAndTypePair * getAddedColumnPtr() const { return added_column; }
-	void removeAddedColumnPtr() { added_column = nullptr; }
-
-	void removeColumn(const String & column_name)
-	{
-		for (auto it = columns.begin(); it != columns.end(); ++it)
-		{
-			if (it->name == column_name)
-			{
-				columns.erase(it);
-				break;
-			}
-		}
-
-		streams.erase(column_name);
-	}
-
 	/** Если столбцов нет в блоке, добавляет их, если есть - добавляет прочитанные значения к ним в конец.
 	  * Не добавляет столбцы, для которых нет файлов. Чтобы их добавить, нужно вызвать fillMissingColumns.
 	  * В блоке должно быть либо ни одного столбца из columns, либо все, для которых есть файлы. */
@@ -220,12 +203,20 @@ public:
 				}
 			}
 
-			size_t pos = 0;	/// Позиция, куда надо вставить недостающий столбец.
-			for (NamesAndTypesList::const_iterator it = columns.begin(); it != columns.end(); ++it, ++pos)
+			auto should_evaluate_defaults = false;
+			auto should_sort = false;
+			for (NamesAndTypesList::const_iterator it = columns.begin(); it != columns.end(); ++it)
 			{
 				/// insert default values only for columns without default expressions
-				if (!res.has(it->name) && storage.column_defaults.count(it->name) == 0)
+				if (!res.has(it->name))
 				{
+					should_sort = true;
+					if (storage.column_defaults.count(it->name) != 0)
+					{
+						should_evaluate_defaults = true;
+						continue;
+					}
+
 					ColumnWithNameAndType column;
 					column.name = it->name;
 					column.type = it->type;
@@ -252,12 +243,37 @@ public:
 							res.rows(), column.type->getDefault())).convertToFullColumn();
 					}
 
-					res.insert(pos, column);
+					res.insert(column);
 				}
 			}
 
-			/// evaluate defaulted columns
-			evaluateMissingDefaults(res, columns, storage.column_defaults, storage.context);
+			/// evaluate defaulted columns if necessary
+			if (should_evaluate_defaults)
+				evaluateMissingDefaults(res, columns, storage.column_defaults, storage.context);
+
+			/// remove added column to ensure same content among all blocks
+			if (added_column)
+			{
+				streams.erase(added_column->name);
+				columns.erase(std::begin(columns));
+			}
+
+			/// sort columns to ensure consistent order among all block
+			if (should_sort)
+			{
+				Block sorted_block;
+
+				for (const auto & name_and_type : columns)
+					sorted_block.insert(res.getByName(name_and_type.name));
+
+				std::swap(res, sorted_block);
+			}
+			else if (added_column)
+			{
+				res.erase(0);
+			}
+
+			added_column = nullptr;
 		}
 		catch (const Exception & e)
 		{
