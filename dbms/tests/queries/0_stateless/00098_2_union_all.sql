f906f6b1e2e8baf18cbdf1d66452bad82f28a7db
Alexey Arno <af-arno@yandex-team.ru>
2014-12-17 00:44:03 +0300

    Update functional tests. Add materialization in UNION ALL queries. [#METR-14099]

--- dbms/tests/queries/0_stateless/00098_2_union_all.sql
+++ dbms/tests/queries/0_stateless/00098_2_union_all.sql
@@ -1,1 +1,6 @@
-SELECT 1 UNION   ALL SELECT 2;
+SELECT val FROM
+(SELECT value AS val FROM data2013 WHERE name = 'Alice'
+UNION   ALL
+SELECT value AS val FROM data2014 WHERE name = 'Alice')
+ORDER BY val ASC;
+
