1221f2ba5381ac44ac4c6ecf32c437a3f3710d2b
Alexey Milovidov <milovidov@yandex-team.ru>
2014-12-24 23:54:02 +0300

    Fixed build [#METR-2944].

--- dbms/src/Common/tests/parallel_aggregation.cpp
+++ dbms/src/Common/tests/parallel_aggregation.cpp
@@ -25,46 +25,6 @@ typedef UInt64 Value;
 typedef std::vector<Key> Source;
 
 typedef HashMap<Key, Value> Map;
-
-
-template
-<
-	typename Key,
-	typename Cell,
-	typename Hash = DefaultHash<Key>,
-	typename Grower = HashTableGrower<8>,
-	typename Allocator = HashTableAllocator
->
-class TwoLevelHashMapTable : public TwoLevelHashTable<Key, Cell, Hash, Grower, Allocator, HashMapTable<Key, Cell, Hash, Grower, Allocator> >
-{
-public:
-	typedef Key key_type;
-	typedef typename Cell::Mapped mapped_type;
-	typedef typename Cell::value_type value_type;
-
-	mapped_type & operator[](Key x)
-	{
-		typename TwoLevelHashMapTable::iterator it;
-		bool inserted;
-		this->emplace(x, it, inserted);
-
-		if (!__has_trivial_constructor(mapped_type) && inserted)
-			new(&it->second) mapped_type();
-
-		return it->second;
-	}
-};
-
-template
-<
-	typename Key,
-	typename Mapped,
-	typename Hash = DefaultHash<Key>,
-	typename Grower = HashTableGrower<8>,
-	typename Allocator = HashTableAllocator
->
-using TwoLevelHashMap = TwoLevelHashMapTable<Key, HashMapCell<Key, Mapped, Hash>, Hash, Grower, Allocator>;
-
 typedef TwoLevelHashMap<Key, Value> MapTwoLevel;
 
 
