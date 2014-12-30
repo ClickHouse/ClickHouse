06a63495f3d97606b917d1c5d97b2073d2cb6cfe
Alexey Arno <af-arno@yandex-team.ru>
2014-12-30 17:11:02 +0300

    dbms: Server: add support for multiple connections in PoolWithFailoverBase. [#METR-14410]

--- dbms/include/DB/Client/ConnectionPool.h
+++ dbms/include/DB/Client/ConnectionPool.h
@@ -28,11 +28,7 @@ public:
 	typedef PoolBase<Connection>::Entry Entry;
 	virtual Entry get(Settings * settings = nullptr) = 0;
 
-	/** Выделяет до указанного количества соединений для работы. 
-	  * Соединения предоставляют доступ к разным репликам одного шарда.
-	  * Выкидывает исключение, если не удалось выделить ни одного соединения.
-	  */
-	virtual std::vector<Entry> getMany(Settings * settings = nullptr) 
+	virtual std::vector<Entry> getMany(unsigned max_connections, Settings * settings = nullptr) 
 	{
 		return std::vector<Entry>{ get(settings) };
 	}
