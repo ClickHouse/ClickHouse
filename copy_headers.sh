0d5a794e9385a637a365daa54f16aba322497ed1
Alexey Milovidov <milovidov@yandex-team.ru>
2015-01-09 01:10:30 +0300

    dbms: dynamic compiler: development [#METR-2944].

--- copy_headers.sh
+++ copy_headers.sh
@@ -5,17 +5,16 @@
 # Это затем может быть использовано, чтобы скомпилировать translation unit на другом сервере,
 #  используя ровно такой же набор заголовочных файлов.
 
-PROJECT_PATH=${1:-..}
-DST=${2:-$PROJECT_PATH/headers};
+SOURCE_PATH=${1:-.}
+DST=${2:-$SOURCE_PATH/../headers};
 
 for i in $(/usr/bin/c++ -M -xc++ -std=gnu++1y -Wall -Werror -march=native -O3 -g -shared -fPIC -rdynamic \
-	$(cat $PROJECT_PATH/src/CMakeLists.txt | grep include_directories | grep -v METRICA_BINARY_DIR | sed -e "s!\${METRICA_SOURCE_DIR}!$PROJECT_PATH/src!; s!include_directories (!-I !; s!)!!;" | tr '\n' ' ') \
-	$PROJECT_PATH/src/dbms/include/DB/Interpreters/SpecializedAggregator.h |
+	$(cat $SOURCE_PATH/CMakeLists.txt | grep include_directories | grep -v METRICA_BINARY_DIR | sed -e "s!\${METRICA_SOURCE_DIR}!$SOURCE_PATH!; s!include_directories (!-I !; s!)!!;" | tr '\n' ' ') \
+	$SOURCE_PATH/dbms/include/DB/Interpreters/SpecializedAggregator.h |
 	tr -d '\\' |
 	grep -v '.o:' |
 	ssed -R -e 's/^.+\.cpp / /');
 do
 	mkdir -p $DST/$(echo $i | ssed -R -e 's/\/[^/]*$/\//');
 	cp $i $DST/$i;
-#	echo $(readlink -f $i);
 done
