65e66d221676cf3a5f8eb8900416f83b8fe96f8f
Alexey Milovidov <milovidov@yandex-team.ru>
2015-01-10 17:31:23 +0300

    Fixed build [#METR-2944].

--- dbms/src/DataStreams/tests/aggregating_stream.cpp
+++ dbms/src/DataStreams/tests/aggregating_stream.cpp
@@ -92,7 +92,7 @@ int main(int argc, char ** argv)
 
 		DB::BlockInputStreamPtr stream = new DB::OneBlockInputStream(block);
 		stream = new DB::AggregatingBlockInputStream(stream, key_column_numbers, aggregate_descriptions, false, true,
-													 0, DB::OverflowMode::THROW, nullptr);
+													 0, DB::OverflowMode::THROW, nullptr, 0);
 
 		DB::WriteBufferFromOStream ob(std::cout);
 		DB::RowOutputStreamPtr row_out = new DB::TabSeparatedRowOutputStream(ob, sample);
