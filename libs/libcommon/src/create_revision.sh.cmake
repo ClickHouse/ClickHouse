982c2c557320b3a290a1d7e8e1d78f8b0a93e4a3
Pavel Kartavyy <kartavyy@yandex-team.ru>
2015-01-13 11:08:12 +0300

    create_revision.sh: better

--- libs/libcommon/src/create_revision.sh.cmake
+++ libs/libcommon/src/create_revision.sh.cmake
@@ -1,7 +1,7 @@
 #!/bin/bash
-mkdir -p "${CMAKE_CURRENT_BINARY_DIR}"/src
-echo "#ifndef REVISION" > ${CMAKE_CURRENT_BINARY_DIR}/src/revision.h
-echo -n "#define REVISION " >> ${CMAKE_CURRENT_BINARY_DIR}/src/revision.h
+mkdir -p "${CMAKE_CURRENT_BINARY_DIR}/src"
+echo "#ifndef REVISION" > "${CMAKE_CURRENT_BINARY_DIR}/src/revision.h"
+echo -n "#define REVISION " >> "${CMAKE_CURRENT_BINARY_DIR}/src/revision.h"
 
 cd "${CMAKE_CURRENT_SOURCE_DIR}";
 
@@ -15,6 +15,6 @@ revision=$(git tag --points-at HEAD 2> /dev/null | tail -1)
 if [[ "$revision" = "" ]]; then
 	revision=$( ( git describe --tags || echo 1 ) | cut -d "-" -f 1 )
 fi
-echo $revision >> "${CMAKE_CURRENT_BINARY_DIR}"/src/revision.h;
+echo $revision >> "${CMAKE_CURRENT_BINARY_DIR}/src/revision.h";
 
-echo "#endif" >> "${CMAKE_CURRENT_BINARY_DIR}"/src/revision.h
+echo "#endif" >> "${CMAKE_CURRENT_BINARY_DIR}/src/revision.h"
