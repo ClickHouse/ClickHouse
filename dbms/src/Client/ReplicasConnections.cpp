fddc332e6f12ca2cdf7e7f246ff8a8c298b57650
Alexey Arno <af-arno@yandex-team.ru>
2015-01-14 16:16:03 +0300

    dbms: Server: queries with several replicas: development [#METR-14410]

--- dbms/src/Client/ReplicasConnections.cpp
+++ dbms/src/Client/ReplicasConnections.cpp
@@ -1,45 +1,47 @@
 #include <DB/Client/ReplicasConnections.h>
+#include <DB/Client/ConnectionPool.h>
 
 namespace DB
 {
-	ReplicasConnections::ReplicasConnections(IConnectionPool * pool_, Settings * settings_, size_t timeout_microseconds_) :
-		pool(pool_),
+	ReplicasConnections::ReplicasConnections(IConnectionPool * pool_, Settings * settings_) :
 		settings(settings_),
-		timeout_microseconds(timeout_microseconds_)
+		select_timeout(settings->poll_interval * 1000000)
 	{
-        auto entries = pool->getMany(settings);
+        auto entries = pool_->getMany(settings);
+		valid_connections_count = entries.size();
+		connection_hash.reserve(valid_connections_count);
+
 		for (auto & entry : entries)
 		{
 			Connection * connection = &*entry;
-			connections.insert(std::make_pair(connection->socket, ConnectionInfo(connection)));
+			connection_hash.insert(std::make_pair(connection->socket.impl()->sockfd(), ConnectionInfo(connection)));
 		}
 	}
 
 	int ReplicasConnections::waitForReadEvent()
 	{
-		Poco::Net::Socket::SocketList read_list(connections.size());
-
-		auto it = read_list.begin();
-        for (auto & e : connections) 
-		{
-            ConnectionInfo & info = e.second;
-			info.can_read = false;
-			if (info.is_valid)
-			{
-				*it = e.first;
-				++it;
-			}
-        }
-
-        if (read_list.empty())
+		if (valid_connections_count == 0)
 			return 0;
 
-        int n = Poco::Net::Socket::select(read_list, write_list, except_list, Poco::Timespan(timeout_microseconds));
+		Poco::Net::Socket::SocketList read_list;
+		read_list.reserve(valid_connections_count);
+
+		for (auto & e : connection_hash)
+		{
+			ConnectionInfo & info = e.second;
+			info.can_read = false;
+			if (info.is_valid)
+				read_list.push_back(info.connection->socket);
+		}
+
+        int n = Poco::Net::Socket::select(read_list, write_list, except_list, select_timeout);
 
         for (const auto & socket : read_list) 
 		{
-			auto place = connections.find(socket);
-			ConnectionInfo & info = place->second;
+			auto it = connection_hash.find(socket.impl()->sockfd());
+			if (it == connection_hash.end())
+				throw Exception("Unexpected replica", ErrorCodes::UNEXPECTED_REPLICA);
+			ConnectionInfo & info = it->second;
 			info.can_read = true;
         }
 
@@ -48,23 +50,25 @@ namespace DB
 
 	ReplicasConnections::ConnectionInfo & ReplicasConnections::pickConnection()
 	{
-        int n = waitForReadEvent();
-        if (n == 0)
-            throw Exception("", ErrorCodes::UNKNOWN_PACKET_FROM_SERVER);
-
-        int max_packet_number = -1;
 		ConnectionInfo * res = nullptr;
 
-        for (auto & e : connections) {
-			ConnectionInfo & info = e.second;
-            if (info.can_read && 
-				(info.packet_number > max_packet_number)) {
-                max_packet_number = info.packet_number;
-				res = &info;
-            }
-        }
-        if (res == nullptr)
-			throw Exception("", ErrorCodes::UNKNOWN_PACKET_FROM_SERVER);
+        int n = waitForReadEvent();
+        if (n > 0)
+		{
+			int max_packet_number = -1;
+			for (auto & e : connection_hash) 
+			{
+				ConnectionInfo & info = e.second;
+				if (info.can_read && (info.packet_number > max_packet_number))
+				{
+					max_packet_number = info.packet_number;
+					res = &info;
+				}
+			}
+		}
+
+		if (res == nullptr)
+			throw Exception("No available replica", ErrorCodes::NO_AVAILABLE_REPLICA);
 
         return *res;
 	}
@@ -92,11 +96,13 @@ namespace DB
 					case Protocol::Server::ProfileInfo:
 					case Protocol::Server::Totals:
 					case Protocol::Server::Extremes:
+					case Protocol::Server::Exception:
 						++info.packet_number;
 						break;
 
 					default:
 						info.is_valid = false;
+						--valid_connections_count;
 						break;
 				}
 			}
@@ -106,7 +112,7 @@ namespace DB
 	void ReplicasConnections::sendQuery(const String & query, const String & query_id, UInt64 stage, 
 				   const Settings * settings_, bool with_pending_data)
 	{
-		for (auto & e : connections)
+		for (auto & e : connection_hash)
 		{
 			Connection * connection = e.second.connection;
 			connection->sendQuery(query, query_id, stage, settings_, with_pending_data);
