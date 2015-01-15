d26db81250f599b56b31a7fe4bc445243abcc333
Alexey Milovidov <milovidov@yandex-team.ru>
2015-01-15 23:46:21 +0300

    dbms: fixed error [#METR-14570].

--- copy_headers.sh
+++ copy_headers.sh
@@ -18,6 +18,8 @@
 SOURCE_PATH=${1:-.}
 DST=${2:-$SOURCE_PATH/../headers};
 
+PATH="/usr/local/bin:/usr/local/sbin:/usr/bin:$PATH"
+
 for i in $(clang -M -xc++ -std=gnu++1y -Wall -Werror -march=native -O3 -g -fPIC \
 	$(cat $SOURCE_PATH/CMakeLists.txt | grep include_directories | grep -v METRICA_BINARY_DIR | sed -e "s!\${METRICA_SOURCE_DIR}!$SOURCE_PATH!; s!include_directories (!-I !; s!)!!;" | tr '\n' ' ') \
 	$SOURCE_PATH/dbms/include/DB/Interpreters/SpecializedAggregator.h |
