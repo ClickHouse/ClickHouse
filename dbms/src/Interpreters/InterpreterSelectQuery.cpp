e86abf03116e863b468dfee08f0739c6917b8e2c
Alexey Arno <af-arno@yandex-team.ru>
2015-01-16 18:22:12 +0300

    dbms: Server: queries with several replicas: development [#METR-14410]

--- dbms/src/Interpreters/InterpreterSelectQuery.cpp
+++ dbms/src/Interpreters/InterpreterSelectQuery.cpp
@@ -99,6 +99,11 @@ void InterpreterSelectQuery::basicInit(BlockInputStreamPtr input_, const NamesAn
 			storage = context.getTable(database_name, table_name);
 		}
 
+		if (!storage->supportsParallelReplicas() && (settings.parallel_replicas_count > 0))
+			throw Exception("Storage engine " + storage->getName() 
+							+ " does not support parallel execution on several replicas",
+							ErrorCodes::STORAGE_DOESNT_SUPPORT_PARALLEL_REPLICAS);
+
 		table_lock = storage->lockStructure(false);
 		if (table_column_names.empty())
 			context.setColumns(storage->getColumnsListNonMaterialized());
