c132a7c428dc6e009e41848ce5a63f5a55acbd67
Alexey Arno <af-arno@yandex-team.ru>
2015-03-11 18:28:43 +0300

    dbms: Server: feature development. [#METR-15090]

--- dbms/src/IO/tests/write_buffer_aio.cpp
+++ dbms/src/IO/tests/write_buffer_aio.cpp
@@ -4,6 +4,7 @@
 #include <iostream>
 #include <fstream>
 #include <streambuf>
+#include <functional>
 #include <cstdlib>
 
 namespace
@@ -60,13 +61,67 @@ bool test1()
 	return (received == buf);
 }
 
-void run()
+bool test2()
+{
+	namespace fs = boost::filesystem;
+
+	static const std::string symbols = "ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
+
+	char pattern[] = "/tmp/fileXXXXXX";
+	char * dir = ::mkdtemp(pattern);
+	if (dir == nullptr)
+		die("Could not create directory");
+
+	const std::string directory = std::string(dir);
+	const std::string filename = directory + "/foo";
+
+	size_t n = 10 * DB::WriteBufferAIO::BLOCK_SIZE;
+
+	std::string buf;
+	buf.reserve(n);
+
+	for (size_t i = 0; i < n; ++i)
+		buf += symbols[i % symbols.length()];
+
+	{
+		DB::WriteBufferAIO out(filename, 3 * DB::WriteBufferAIO::BLOCK_SIZE);
+
+		if (out.getFileName() != filename)
+			return false;
+		if (out.getFD() == -1)
+			return false;
+
+		out.write(&buf[0], buf.length() / 2);
+		out.seek(DB::WriteBufferAIO::BLOCK_SIZE, SEEK_CUR);
+		out.write(&buf[buf.length() / 2], buf.length() / 2);
+	}
+
+	std::ifstream in(filename.c_str());
+	if (!in.is_open())
+		die("Could not open file");
+
+	std::string received{ std::istreambuf_iterator<char>(in), std::istreambuf_iterator<char>() };
+
+	in.close();
+	fs::remove_all(directory);
+
+	if (received.substr(0, buf.length() / 2) != buf.substr(0, buf.length() / 2))
+		return false;
+	if (received.substr(buf.length() / 2, DB::WriteBufferAIO::BLOCK_SIZE) != std::string(DB::WriteBufferAIO::BLOCK_SIZE, '\0'))
+		return false;
+	if (received.substr(buf.length() / 2 + DB::WriteBufferAIO::BLOCK_SIZE) != buf.substr(buf.length() / 2))
+		return false;
+
+	return true;
+}
+
+void run_test(unsigned int num, const std::function<bool()> func)
 {
 	bool ok;
 
 	try
 	{
-		ok = test1();
+		ok = func();
 	}
 	catch (const DB::Exception & ex)
 	{
@@ -80,9 +135,25 @@ void run()
 	}
 
 	if (ok)
-		std::cout << "Test passed\n";
+		std::cout << "Test " << num << " passed\n";
 	else
-		std::cout << "Test failed\n";
+		std::cout << "Test " << num << " failed\n";
+}
+
+void run()
+{
+	const std::vector<std::function<bool()> > tests =
+	{
+		test1,
+		test2
+	};
+
+	unsigned int num = 0;
+	for (const auto & test : tests)
+	{
+		++num;
+		run_test(num, test);
+	}
 }
 
 }
