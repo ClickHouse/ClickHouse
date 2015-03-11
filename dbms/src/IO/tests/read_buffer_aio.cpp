039385df874d3c6269e241cca2a6b6c7f41824d0
Alexey Arno <af-arno@yandex-team.ru>
2015-03-11 17:47:52 +0300

    dbms: Server: feature development. [#METR-15090]

--- dbms/src/IO/tests/read_buffer_aio.cpp
+++ dbms/src/IO/tests/read_buffer_aio.cpp
@@ -19,10 +19,12 @@ void run_test(unsigned int num, const std::function<bool()> func);
 bool test1(const std::string & filename);
 bool test2(const std::string & filename, const std::string & buf);
 bool test3(const std::string & filename, const std::string & buf);
-bool test4(const std::string & filename);
-bool test5(const std::string & filename, const std::string & buf);
+bool test4(const std::string & filename, const std::string & buf);
+bool test5(const std::string & filename);
 bool test6(const std::string & filename, const std::string & buf);
-bool test7(const std::string & filename);
+bool test7(const std::string & filename, const std::string & buf);
+bool test8(const std::string & filename);
+bool test9(const std::string & filename, const std::string & buf);
 
 void run()
 {
@@ -38,10 +40,12 @@ void run()
 		std::bind(test1, std::ref(filename)),
 		std::bind(test2, std::ref(filename), std::ref(buf)),
 		std::bind(test3, std::ref(filename), std::ref(buf)),
-		std::bind(test4, std::ref(filename)),
-		std::bind(test5, std::ref(filename), std::ref(buf)),
+		std::bind(test4, std::ref(filename), std::ref(buf)),
+		std::bind(test5, std::ref(filename)),
 		std::bind(test6, std::ref(filename), std::ref(buf)),
-		std::bind(test7, std::ref(filename))
+		std::bind(test7, std::ref(filename), std::ref(buf)),
+		std::bind(test8, std::ref(filename)),
+		std::bind(test9, std::ref(filename), std::ref(buf))
 	};
 
 	unsigned int num = 0;
@@ -146,7 +150,19 @@ bool test3(const std::string & filename, const std::string & buf)
 	return (newbuf == buf.substr(0, requested));
 }
 
-bool test4(const std::string & filename)
+bool test4(const std::string & filename, const std::string & buf)
+{
+	std::string newbuf;
+	newbuf.resize(buf.length());
+
+	DB::ReadBufferAIO in(filename, 3 * DB::ReadBufferAIO::BLOCK_SIZE);
+	in.setMaxBytes(0);
+	size_t n_read = in.read(&newbuf[0], newbuf.length());
+
+	return n_read == 0;
+}
+
+bool test5(const std::string & filename)
 {
 	bool ok = false;
 
@@ -162,7 +178,7 @@ bool test4(const std::string & filename)
 	return ok;
 }
 
-bool test5(const std::string & filename, const std::string & buf)
+bool test6(const std::string & filename, const std::string & buf)
 {
 	std::string newbuf;
 	newbuf.resize(buf.length());
@@ -180,7 +196,7 @@ bool test5(const std::string & filename, const std::string & buf)
 	return true;
 }
 
-bool test6(const std::string & filename, const std::string & buf)
+bool test7(const std::string & filename, const std::string & buf)
 {
 	std::string newbuf;
 	newbuf.resize(buf.length() - DB::ReadBufferAIO::BLOCK_SIZE);
@@ -192,7 +208,7 @@ bool test6(const std::string & filename, const std::string & buf)
 	return (newbuf == buf.substr(DB::ReadBufferAIO::BLOCK_SIZE));
 }
 
-bool test7(const std::string & filename)
+bool test8(const std::string & filename)
 {
 	bool ok = false;
 
@@ -200,6 +216,27 @@ bool test7(const std::string & filename)
 	{
 		DB::ReadBufferAIO in(filename, 3 * DB::ReadBufferAIO::BLOCK_SIZE);
 		(void) in.seek(DB::ReadBufferAIO::BLOCK_SIZE + 1, SEEK_CUR);
+	}
+	catch (const DB::Exception &)
+	{
+		ok = true;
+	}
+
+	return ok;
+}
+
+bool test9(const std::string & filename, const std::string & buf)
+{
+	bool ok = false;
+
+	try
+	{
+		std::string newbuf;
+		newbuf.resize(buf.length());
+
+		DB::ReadBufferAIO in(filename, 3 * DB::ReadBufferAIO::BLOCK_SIZE);
+		(void) in.read(&newbuf[0], newbuf.length());
+		in.setMaxBytes(9 * DB::ReadBufferAIO::BLOCK_SIZE);
 	}
 	catch (const DB::Exception &)
 	{
