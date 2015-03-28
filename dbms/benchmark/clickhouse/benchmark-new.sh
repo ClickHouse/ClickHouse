17e285892df69e07dca0adebda4ad3d36976bb0b
Alexey Milovidov <milovidov@yandex-team.ru>
2015-03-29 01:23:57 +0300

    dbms: updated benchmark script [#METR-15716].

--- dbms/benchmark/clickhouse/benchmark-new.sh
+++ dbms/benchmark/clickhouse/benchmark-new.sh
@@ -11,7 +11,7 @@ cat "$QUERIES_FILE" | sed "s/{table}/${TABLE}/g" | while read query; do
 	echo -n "["
 	for i in $(seq 1 $TRIES); do
 		RES=$(clickhouse-client --time --format=Null --query="$query" 2>&1)
-		[[ "$?" == "0" ]] && echo -n "${RES}" || echo "null"
+		[[ "$?" == "0" ]] && echo -n "${RES}" || echo -n "null"
 		[[ "$i" != $TRIES ]] && echo -n ", "
 	done
 	echo "],"
