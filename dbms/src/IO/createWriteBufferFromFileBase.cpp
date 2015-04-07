cc3577bc5ec3fb39a87a4aa66d55ac5575dde90c
Alexey Arno <af-arno@yandex-team.ru>
2015-04-07 17:16:49 +0300

    dbms: Server: feature development. [#METR-15090]

---
+++ dbms/src/IO/createWriteBufferFromFileBase.cpp
@@ -0,0 +1,18 @@
+#include <DB/IO/createWriteBufferFromFileBase.h>
+#include <DB/IO/WriteBufferFromFile.h>
+#include <DB/IO/WriteBufferAIO.h>
+
+namespace DB
+{
+
+WriteBufferFromFileBase * createWriteBufferFromFileBase(const std::string & filename_, size_t estimated_size,
+		size_t aio_threshold, size_t buffer_size_, int flags_, mode_t mode, char * existing_memory_, 
+		size_t alignment)
+{
+	if (estimated_size < aio_threshold)
+		return new WriteBufferFromFile(filename_, buffer_size_, flags_, mode, existing_memory_, alignment);
+	else
+		return new WriteBufferAIO(filename_, buffer_size_, flags_, mode, existing_memory_);
+}
+
+}
