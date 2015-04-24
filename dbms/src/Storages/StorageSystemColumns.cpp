7d12e18e16661711015ded0cf09447b68865ea6e
Alexey Arno <af-arno@yandex-team.ru>
2015-04-24 19:49:32 +0300

    dbms: Server: Feature development. [#METR-15859]

--- dbms/src/Storages/StorageSystemColumns.cpp
+++ dbms/src/Storages/StorageSystemColumns.cpp
@@ -44,59 +44,68 @@ BlockInputStreams StorageSystemColumns::read(
 	ColumnWithNameAndType column_default_type{ new ColumnString, new DataTypeString, "default_type" };
 	ColumnWithNameAndType column_default_expression{ new ColumnString, new DataTypeString, "default_expression" };
 
+	std::map<std::pair<std::string, std::string>, StoragePtr> storages;
+
 	{
 		Poco::ScopedLock<Poco::Mutex> lock(context.getMutex());
 
 		const Databases & databases = context.getDatabases();
+
 		for (const auto & database : databases)
 		{
 			const Tables & tables = database.second;
 			for (const auto & table : tables)
 			{
-				NamesAndTypesList columns;
-				ColumnDefaults column_defaults;
-
-				{
-					StoragePtr storage = table.second;
-					storage->lockStructure(false);
-
-					columns = storage->getColumnsList();
-					columns.insert(std::end(columns), std::begin(storage->alias_columns), std::end(storage->alias_columns));
-					column_defaults = storage->column_defaults;
-				}
-
-				for (const auto & column : columns)
-				{
-					column_database.column->insert(database.first);
-					column_table.column->insert(table.first);
-					column_name.column->insert(column.name);
-					column_type.column->insert(column.type->getName());
-
-					const auto it = column_defaults.find(column.name);
-					if (it == std::end(column_defaults))
-					{
-						column_default_type.column->insertDefault();
-						column_default_expression.column->insertDefault();
-					}
-					else
-					{
-						column_default_type.column->insert(toString(it->second.type));
-						column_default_expression.column->insert(queryToString(it->second.expression));
-					}
-				}
+				column_database.column->insert(database.first);
+				column_table.column->insert(table.first);
+				storages.insert(std::make_pair(std::make_pair(database.first, table.first), table.second));
 			}
 		}
 	}
 
-	Block filtered_block{ column_database, column_table, column_name, column_type, column_default_type, column_default_expression };
-	VirtualColumnUtils::filterBlockWithQuery(query, filtered_block, context);
+	{
+		Block filtered_block{ column_database, column_table };
+		VirtualColumnUtils::filterBlockWithQuery(query, filtered_block, context);
 
-	column_database = filtered_block.getByName("database");
-	column_table = filtered_block.getByName("table");
-	column_name = filtered_block.getByName("name");
-	column_type = filtered_block.getByName("type");
-	column_default_type = filtered_block.getByName("default_type");
-	column_default_expression = filtered_block.getByName("default_expression");
+		column_database = filtered_block.getByName("database");
+		column_table = filtered_block.getByName("table");
+	}
+
+	for (size_t i = 0; i < column_database.column->size(); ++i)
+	{
+		const auto & database_name = (*column_database.column)[i].safeGet<const std::string &>();
+		const auto & table_name = (*column_table.column)[i].safeGet<const std::string &>();
+
+		NamesAndTypesList columns;
+		ColumnDefaults column_defaults;
+
+		{
+			StoragePtr storage = storages.at(std::make_pair(database_name, table_name));
+			auto table_lock = storage->lockStructure(false);
+
+			columns = storage->getColumnsList();
+			columns.insert(std::end(columns), std::begin(storage->alias_columns), std::end(storage->alias_columns));
+			column_defaults = storage->column_defaults;
+		}
+
+		for (const auto & column : columns)
+		{
+			column_name.column->insert(column.name);
+			column_type.column->insert(column.type->getName());
+
+			const auto it = column_defaults.find(column.name);
+			if (it == std::end(column_defaults))
+			{
+				column_default_type.column->insertDefault();
+				column_default_expression.column->insertDefault();
+			}
+			else
+			{
+				column_default_type.column->insert(toString(it->second.type));
+				column_default_expression.column->insert(queryToString(it->second.expression));
+			}
+		}
+	}
 
 	return BlockInputStreams{ 1, new OneBlockInputStream{
 			{ column_database, column_table, column_name, column_type, column_default_type, column_default_expression }
