3dfb212562b13ebc6e206d835b982af1a2eb493f
Alexey Milovidov <milovidov@yandex-team.ru>
2015-05-19 00:20:43 +0300

    Merge

---
+++ dbms/tests/queries/0_stateless/00160_merge_and_index_in_in.sql
@@ -0,0 +1,14 @@
+DROP TABLE IF EXISTS test.mt;
+DROP TABLE IF EXISTS test.merge;
+
+CREATE TABLE test.mt (d Date DEFAULT toDate('2015-05-01'), x UInt64) ENGINE = MergeTree(d, x, 1);
+CREATE TABLE test.merge (d Date, x UInt64) ENGINE = Merge(test, '^mt$');
+
+SET max_block_size = 1000000;
+INSERT INTO test.mt SELECT number AS x FROM system.numbers LIMIT 100000;
+
+SELECT *, b FROM test.mt WHERE x IN (12345, 67890) AND NOT ignore(blockSize() < 10 AS b) ORDER BY x;
+SELECT *, b FROM test.merge WHERE x IN (12345, 67890) AND NOT ignore(blockSize() < 10 AS b) ORDER BY x;
+
+DROP TABLE test.merge;
+DROP TABLE test.mt;
