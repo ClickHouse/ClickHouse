f02d490f94bdd975517178cd0a6810548cb72c6f
Alexey Arno <af-arno@yandex-team.ru>
2015-06-29 16:54:08 +0300

    dbms: Server: Support for multiple time zones: development [#METR-15618]

--- libs/libcommon/src/DateLUT.cpp
+++ libs/libcommon/src/DateLUT.cpp
@@ -3,7 +3,6 @@
 
 #include <unicode/timezone.h>
 #include <unicode/unistr.h>
-#include <memory>
 
 std::string DateLUT::default_time_zone;
 
@@ -35,45 +34,51 @@ DateLUT::DateLUT()
 		zone_id = time_zone_ids->snext(status);
 	}
 
+	size_t group_id = 0;
+
 	for (const auto & time_zone : time_zones)
 	{
-		const UnicodeString & u_group_id = TimeZone::getEquivalentID(time_zone, 0);
-		std::string group_id;
+		const UnicodeString & u_group_name = TimeZone::getEquivalentID(time_zone, 0);
+		std::string group_name;
 
-		if (u_group_id.isEmpty())
+		if (u_group_name.isEmpty())
 		{
-			time_zone.toUTF8String(group_id);
+			time_zone.toUTF8String(group_name);
 
-			auto res = time_zone_to_group.insert(std::make_pair(group_id, group_id));
+			auto res = time_zone_to_group.insert(std::make_pair(group_name, group_id));
 			if (!res.second)
 				throw Poco::Exception("Failed to initialize time zone information.");
-			auto res2 = date_lut_impl_list.emplace(std::piecewise_construct, std::forward_as_tuple(group_id), std::forward_as_tuple(nullptr));
-			if (!res2.second)
-				throw Poco::Exception("Failed to initialize time zone information.");
+			++group_id;
 		}
 		else
 		{
-			u_group_id.toUTF8String(group_id);
+			u_group_name.toUTF8String(group_name);
 
-			auto it = time_zone_to_group.find(group_id);
+			auto it = time_zone_to_group.find(group_name);
 			if (it == time_zone_to_group.end())
 			{
 				auto count = TimeZone::countEquivalentIDs(time_zone);
+				if (count == 0)
+					throw Poco::Exception("Inconsistent time zone information.");
+
 				for (auto i = 0; i < count; ++i)
 				{
-					const UnicodeString & u_equivalent_id = TimeZone::getEquivalentID(time_zone, i);
-					std::string equivalent_id;
-					u_equivalent_id.toUTF8String(equivalent_id);
-					auto res = time_zone_to_group.insert(std::make_pair(equivalent_id, group_id));
+					const UnicodeString & u_name = TimeZone::getEquivalentID(time_zone, i);
+					std::string name;
+					u_name.toUTF8String(name);
+					auto res = time_zone_to_group.insert(std::make_pair(name, group_id));
 					if (!res.second)
 						throw Poco::Exception("Failed to initialize time zone information.");
 				}
-				auto res = date_lut_impl_list.emplace(std::piecewise_construct, std::forward_as_tuple(group_id), std::forward_as_tuple(nullptr));
-				if (!res.second)
-					throw Poco::Exception("Failed to initialize time zone information.");
+				++group_id;
 			}
 		}
 	}
+
+	if (group_id == 0)
+		throw Poco::Exception("Could not find any time zone information.");
+
+	date_lut_impl_list = std::make_unique<DateLUTImplList>(group_id);
 }
 
 DateLUTImpl & DateLUT::instance(const std::string & time_zone)
@@ -87,13 +92,9 @@ DateLUTImpl & DateLUT::get(const std::string & time_zone)
 	auto it = time_zone_to_group.find(time_zone);
 	if (it == time_zone_to_group.end())
 		throw Poco::Exception("Invalid time zone " + time_zone);
+
 	const auto & group_id = it->second;
-
-	auto it2 = date_lut_impl_list.find(group_id);
-	if (it2 == date_lut_impl_list.end())
-		throw Poco::Exception("Invalid group of equivalent time zones.");
-
-	auto & wrapper = it2->second;
+	auto & wrapper = (*date_lut_impl_list)[group_id];
 
 	DateLUTImpl * tmp = wrapper.load(std::memory_order_acquire);
 	if (tmp == nullptr)
@@ -102,7 +103,7 @@ DateLUTImpl & DateLUT::get(const std::string & time_zone)
 		tmp = wrapper.load(std::memory_order_acquire);
 		if (tmp == nullptr)
 		{
-			tmp = new DateLUTImpl(group_id);
+			tmp = new DateLUTImpl(time_zone);
 			wrapper.store(tmp, std::memory_order_release);
 		}
 	}
