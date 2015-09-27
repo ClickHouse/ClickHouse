e1836a1e274366f3d3279880561ebafb1deb9645
Alexey Milovidov <milovidov@yandex-team.ru>
2015-09-27 05:40:39 +0300

    dbms: addition to prev. revision [#METR-18213].

--- dbms/include/DB/Common/SimpleCache.h
+++ dbms/include/DB/Common/SimpleCache.h
@@ -21,7 +21,10 @@ template <typename F, F* f>
 class SimpleCache
 {
 private:
-	using Key = typename function_traits<F>::arguments;
+	template <typename... T>
+	using tuple_with_removed_refs = std::tuple<typename std::remove_reference<T>::type...>;
+
+	using Key = typename function_traits<F>::arguments_remove_reference;
 	using Result = typename function_traits<F>::result;
 
 	std::map<Key, Result> cache;
