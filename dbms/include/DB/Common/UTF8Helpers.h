39189f48d33d17114e262b2c93f9c176a0751df5
Andrey Mironov <hertz@yandex-team.ru>
2015-10-15 15:54:33 +0300

    dbms: UTF8Helpers.h: make all functions inline

--- dbms/include/DB/Common/UTF8Helpers.h
+++ dbms/include/DB/Common/UTF8Helpers.h
@@ -16,27 +16,27 @@ static const UInt8 CONTINUATION_OCTET_MASK = 0b11000000u;
 static const UInt8 CONTINUATION_OCTET = 0b10000000u;
 
 /// return true if `octet` binary repr starts with 10 (octet is a UTF-8 sequence continuation)
-static bool isContinuationOctet(const UInt8 octet)
+inline bool isContinuationOctet(const UInt8 octet)
 {
 	return (octet & CONTINUATION_OCTET_MASK) == CONTINUATION_OCTET;
 }
 
 /// moves `s` backward until either first non-continuation octet
-static void syncBackward(const UInt8 * & s)
+inline void syncBackward(const UInt8 * & s)
 {
 	while (isContinuationOctet(*s))
 		--s;
 }
 
 /// moves `s` forward until either first non-continuation octet or string end is met
-static void syncForward(const UInt8 * & s, const UInt8 * const end = nullptr)
+inline void syncForward(const UInt8 * & s, const UInt8 * const end = nullptr)
 {
 	while (s < end && isContinuationOctet(*s))
 		++s;
 }
 
 /// returns UTF-8 code point sequence length judging by it's first octet
-static std::size_t seqLength(const UInt8 first_octet)
+inline std::size_t seqLength(const UInt8 first_octet)
 {
 	if (first_octet < 0x80u)
 		return 1;
