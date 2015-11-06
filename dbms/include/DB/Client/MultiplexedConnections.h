29dc5f91d1fd83f48a26a6882fe21884b774801d
Alexey Arno <af-arno@yandex-team.ru>
2015-11-06 20:44:01 +0300

    Merge

--- dbms/include/DB/Client/MultiplexedConnections.h
+++ dbms/include/DB/Client/MultiplexedConnections.h
@@ -6,30 +6,38 @@
 #include <Poco/ScopedLock.h>
 #include <Poco/Mutex.h>
 
-
 namespace DB
 {
 
 
-/** Для получения данных сразу из нескольких реплик (соединений) в рамках одного потока.
-  * В качестве вырожденного случая, может также работать с одним соединением.
+/** Для получения данных сразу из нескольких реплик (соединений) из одного или нексольких шардов
+  * в рамках одного потока. В качестве вырожденного случая, может также работать с одним соединением.
   * Предполагается, что все функции кроме sendCancel всегда выполняются в одном потоке.
   *
   * Интерфейс почти совпадает с Connection.
   */
-class ParallelReplicas final : private boost::noncopyable
+class MultiplexedConnections final : private boost::noncopyable
 {
 public:
 	/// Принимает готовое соединение.
-	ParallelReplicas(Connection * connection_, const Settings * settings_, ThrottlerPtr throttler_);
+	MultiplexedConnections(Connection * connection_, const Settings * settings_, ThrottlerPtr throttler_);
 
 	/** Принимает пул, из которого нужно будет достать одно или несколько соединений.
 	  * Если флаг append_extra_info установлен, к каждому полученному блоку прилагается
 	  * дополнительная информация.
 	  * Если флаг get_all_replicas установлен, достаются все соединения.
 	  */
-	ParallelReplicas(IConnectionPool * pool_, const Settings * settings_, ThrottlerPtr throttler_,
-		bool append_extra_info = false, bool get_all_replicas = false);
+	MultiplexedConnections(IConnectionPool * pool_, const Settings * settings_, ThrottlerPtr throttler_,
+		bool append_extra_info = false, bool do_broadcast = false);
+
+	/** Принимает пулы, один для каждого шарда, из которих нужно будет достать одно или несколько
+	  * соединений.
+	  * Если флаг append_extra_info установлен, к каждому полученному блоку прилагается
+	  * дополнительная информация.
+	  * Если флаг do_broadcast установлен, достаются все соединения.
+	  */
+	MultiplexedConnections(ConnectionPools & pools_, const Settings * settings_, ThrottlerPtr throttler_,
+		bool append_extra_info = false, bool do_broadcast = false);
 
 	/// Отправить на реплики всё содержимое внешних таблиц.
 	void sendExternalTablesData(std::vector<ExternalTablesData> & data);
@@ -65,15 +73,44 @@ public:
 
 	/// Проверить, есть ли действительные реплики.
 	/// Без блокировки, потому что sendCancel() не меняет состояние реплик.
-	bool hasActiveReplicas() const { return active_replica_count > 0; }
+	bool hasActiveConnections() const { return active_connection_total_count > 0; }
 
 private:
-	/// Реплики хэшированные по id сокета
-	using ReplicaMap = std::unordered_map<int, Connection *>;
+	/// Соединения 1-го шарда, затем соединения 2-го шарда, и т.д.
+	using Connections = std::vector<Connection *>;
+
+	/// Состояние соединений одного шарда.
+	struct ShardState
+	{
+		/// Количество выделенных соединений, т.е. реплик, для этого шарда.
+		size_t allocated_connection_count;
+		/// Текущее количество действительных соединений к репликам этого шарда.
+		size_t active_connection_count;
+	};
+
+	/// Описание одной реплики.
+	struct ReplicaState
+	{
+		/// Индекс соединения.
+		size_t connection_index;
+		/// Владелец этой реплики.
+		ShardState * shard_state;
+	};
+
+	/// Реплики хэшированные по id сокета.
+	using ReplicaMap = std::unordered_map<int, ReplicaState>;
+
+	/// Состояние каждого шарда.
+	using ShardStates = std::vector<ShardState>;
 
 private:
-	/// Зарегистрировать реплику.
-	void registerReplica(Connection * connection);
+	void initFromShard(IConnectionPool * pool);
+
+	/// Зарегистрировать шарды.
+	void registerShards();
+
+	/// Зарегистрировать реплики одного шарда.
+	void registerReplicas(size_t index_begin, size_t index_end, ShardState & shard_state);
 
 	/// Внутренняя версия функции receivePacket без блокировки.
 	Connection::Packet receivePacketUnlocked();
@@ -94,13 +131,15 @@ private:
 
 private:
 	const Settings * settings;
+
+	Connections connections;
 	ReplicaMap replica_map;
+	ShardStates shard_states;
 
 	/// Если не nullptr, то используется, чтобы ограничить сетевой трафик.
 	ThrottlerPtr throttler;
 
 	std::vector<ConnectionPool::Entry> pool_entries;
-	ConnectionPool::Entry pool_entry;
 
 	/// Соединение, c которого был получен последний блок.
 	Connection * current_connection;
@@ -108,13 +147,15 @@ private:
 	std::unique_ptr<BlockExtraInfo> block_extra_info;
 
 	/// Текущее количество действительных соединений к репликам.
-	size_t active_replica_count;
+	size_t active_connection_total_count = 0;
 	/// Запрос выполняется параллельно на нескольких репликах.
 	bool supports_parallel_execution;
 	/// Отправили запрос
 	bool sent_query = false;
 	/// Отменили запрос
 	bool cancelled = false;
+
+	bool do_broadcast = false;
 
 	/// Мьютекс для того, чтобы функция sendCancel могла выполняться безопасно
 	/// в отдельном потоке.
