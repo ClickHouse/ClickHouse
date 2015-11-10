94cf4489293a1cd04e790e2a9dc810157e5dc8c6
Alexey Milovidov <milovidov@yandex-team.ru>
2015-11-11 00:23:45 +0300

    dbms: addition to prev. revision [#METR-18748].

--- dbms/tests/queries/0_stateless/00270_views_query_processing_stage.sql
+++ dbms/tests/queries/0_stateless/00270_views_query_processing_stage.sql
@@ -6,7 +6,7 @@ CREATE VIEW test.view1 AS SELECT number FROM system.numbers LIMIT 10;
 CREATE VIEW test.view2 AS SELECT number FROM system.numbers LIMIT 10;
 CREATE TABLE test.merge_view (number UInt64) ENGINE = Merge(test, '^view');
 
-SELECT 'Hello, world!' FROM merge_view LIMIT 5;
+SELECT 'Hello, world!' FROM test.merge_view LIMIT 5;
 
 DROP TABLE test.view1;
 DROP TABLE test.view2;
