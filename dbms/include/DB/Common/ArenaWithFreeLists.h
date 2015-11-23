24f986c9f15a16c64cc015cb4b51ab023169961d
Andrey Mironov <hertz@yandex-team.ru>
2015-11-23 22:04:15 +0300

    dbms: fixed ArenaWithFreeLists "off-by-one" error which lead to allocating blocks of size less than required.

--- dbms/include/DB/Common/ArenaWithFreeLists.h
+++ dbms/include/DB/Common/ArenaWithFreeLists.h
@@ -27,7 +27,13 @@ private:
 		return sizes;
 	}
 
-	static auto getMinBucketNum() { return 3; }
+	static auto sizeToPreviousPowerOfTwo(const int size) { return _bit_scan_reverse(size - 1); }
+
+	static auto getMinBucketNum()
+	{
+		static const auto val = sizeToPreviousPowerOfTwo(getSizes().front());
+		return val;
+	}
 	static auto getMaxFixedBlockSize() { return getSizes().back(); }
 
 	Arena pool;
@@ -36,7 +42,7 @@ private:
 	static std::size_t findFreeListIndex(const std::size_t size)
 	{
 		/// shift powers of two into previous bucket by subtracting 1
-		const auto bucket_num = _bit_scan_reverse(size - 1);
+		const auto bucket_num = sizeToPreviousPowerOfTwo(size);
 
 		return std::max(bucket_num, getMinBucketNum()) - getMinBucketNum();
 	}
