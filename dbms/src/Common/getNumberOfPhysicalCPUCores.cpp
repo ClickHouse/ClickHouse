8a6483d21a6f00e5049b94336e6252fd13245991
Alexey Milovidov <milovidov@yandex-team.ru>
2016-01-13 05:25:46 +0300

    dbms: addition [#METR-19609].

--- dbms/src/Common/getNumberOfPhysicalCPUCores.cpp
+++ dbms/src/Common/getNumberOfPhysicalCPUCores.cpp
@@ -20,11 +20,11 @@ unsigned getNumberOfPhysicalCPUCores()
 
 	cpu_raw_data_t raw_data;
 	if (0 != cpuid_get_raw_data(&raw_data))
-		throw DB::Exception("Cannot cpuid_get_raw_data: " + std::string(cpuid_error()), ErrorCodes::CPUID_ERROR);
+		throw DB::Exception("Cannot cpuid_get_raw_data: " + std::string(cpuid_error()), DB::ErrorCodes::CPUID_ERROR);
 
 	cpu_id_t data;
 	if (0 != cpu_identify(&raw_data, &data))
-		throw DB::Exception("Cannot cpu_identify: " + std::string(cpuid_error()), ErrorCodes::CPUID_ERROR);
+		throw DB::Exception("Cannot cpu_identify: " + std::string(cpuid_error()), DB::ErrorCodes::CPUID_ERROR);
 
 	return data.num_cores * data.total_logical_cpus / data.num_logical_cpus;
 
