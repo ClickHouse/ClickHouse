d7072341f03957b62afa096029d01c29626744b2
Pavel Kartavyy <kartavyy@yandex-team.ru>
2016-02-09 20:07:39 +0300

    create_revision.sh: add argument with path of generated file

--- libs/libcommon/src/create_revision.sh
+++ libs/libcommon/src/create_revision.sh
@@ -1,9 +1,18 @@
 #!/bin/bash
-mkdir -p "${CMAKE_CURRENT_BINARY_DIR}/src"
-echo "#ifndef REVISION" > "${CMAKE_CURRENT_BINARY_DIR}/src/revision.h"
-echo -n "#define REVISION " >> "${CMAKE_CURRENT_BINARY_DIR}/src/revision.h"
 
-cd "${CMAKE_CURRENT_SOURCE_DIR}";
+set -e
+
+if [[ $# -ne 1 ]]; then
+	echo usage: create_revision.sh out_file_path
+	exit 1
+fi
+
+out_file=$1
+dir=$(dirname $out_file)
+
+mkdir -p $dir
+echo "#ifndef REVISION" > $out_file
+echo -n "#define REVISION " >> $out_file
 
 # GIT
 git fetch --tags;
@@ -13,14 +22,15 @@ revision=$(git tag --points-at HEAD 2> /dev/null | tail -1)
 
 # или ближайший тэг если в данном комите нет тэгов
 if [[ "$revision" = "" ]]; then
-	revision=$( ( git describe --tags || echo 1 ) | cut -d "-" -f 1 )
+	revision=$( ( git describe --tags) | cut -d "-" -f 1 )
 fi
 
+# для stash выставляем жестко ревизию
 is_it_github=$( git config --get remote.origin.url | grep 'github' )
 if [[ "$is_it_github" = "" ]]; then
 	revision=53694
 fi
 
-echo $revision >> "${CMAKE_CURRENT_BINARY_DIR}/src/revision.h";
+echo $revision >> $out_file
 
-echo "#endif" >> "${CMAKE_CURRENT_BINARY_DIR}/src/revision.h"
+echo "#endif" >> $out_file
