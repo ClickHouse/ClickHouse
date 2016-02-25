b5ac93cf0e3a7b7d1e80b305c41da8afa0c7a713
Alexey Milovidov <milovidov@yandex-team.ru>
2016-02-25 05:37:31 +0300

    Fixed segfault [#METR-20155].

--- dbms/include/DB/DataStreams/MaterializingBlockOutputStream.h
+++ dbms/include/DB/DataStreams/MaterializingBlockOutputStream.h
@@ -16,7 +16,23 @@ public:
 	MaterializingBlockOutputStream(const BlockOutputStreamPtr & output)
 		: output{output} {}
 
-	void write(const Block & original_block) override
+	void write(const Block & block) override
+	{
+		output->write(materialize(block));
+	}
+
+	void flush() 										override { output->flush(); }
+	void writePrefix() 									override { output->writePrefix(); }
+	void writeSuffix() 									override { output->writeSuffix(); }
+	void setRowsBeforeLimit(size_t rows_before_limit) 	override { output->setRowsBeforeLimit(rows_before_limit); }
+	void setTotals(const Block & totals) 				override { output->setTotals(materialize(totals)); }
+	void setExtremes(const Block & extremes) 			override { output->setExtremes(materialize(extremes)); }
+	String getContentType() const 						override { return output->getContentType(); }
+
+private:
+	BlockOutputStreamPtr output;
+
+	static Block materialize(const Block & original_block)
 	{
 		/// copy block to get rid of const
 		auto block = original_block;
@@ -29,19 +45,8 @@ public:
 				src = converted;
 		}
 
-		output->write(block);
+		return block;
 	}
-
-	void flush() 										override { output->flush(); }
-	void writePrefix() 									override { output->writePrefix(); }
-	void writeSuffix() 									override { output->writeSuffix(); }
-	void setRowsBeforeLimit(size_t rows_before_limit) 	override { output->setRowsBeforeLimit(rows_before_limit); }
-	void setTotals(const Block & totals) 				override { output->setTotals(totals); }
-	void setExtremes(const Block & extremes) 			override { output->setExtremes(extremes); }
-	String getContentType() const 						override { return output->getContentType(); }
-
-private:
-	BlockOutputStreamPtr output;
 };
 
 }
