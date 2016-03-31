961bdfdaa0180c127dc78bfe2c0d744fb115cbb4
Alexey Milovidov <milovidov@yandex-team.ru>
2016-03-31 23:13:56 +0300

    Allowed to use FQDN for server name in Graphite metrics [#CSADMIN-10464].

--- libs/libdaemon/src/GraphiteWriter.cpp
+++ libs/libdaemon/src/GraphiteWriter.cpp
@@ -19,13 +19,31 @@ GraphiteWriter::GraphiteWriter(const std::string & config_name, const std::strin
 	if (root_path.size())
 		root_path += ".";
 
-	/// То же, что uname -n
-	std::string hostname_in_path = Poco::Net::DNS::hostName();
+	/** Что использовать в качестве имени сервера в названии метрики?
+	  *
+	  * По-умолчанию (для совместимости с существовавшим ранее поведением),
+	  *  в качестве имени сервера берётся строка, аналогичная uname -n,
+	  *  а затем к нему приписывается то, что указано в hostname_suffix, если есть.
+	  * Часто серверы настроены так, что, например, для сервера example01-01-1.yandex.ru, uname -n будет выдавать example01-01-1
+	  * Впрочем, uname -n может быть настроен произвольным образом. Он также может совпадать с FQDN.
+	  *
+	  * Если указано use_fqdn со значением true,
+	  *  то в качестве имени сервера берётся FQDN (uname -f),
+	  *  а значение hostname_suffix игнорируется.
+	  */
+	bool use_fqdn = config.getBool(config_name + ".use_fqdn", false);
+
+	std::string hostname_in_path = use_fqdn
+		? Poco::Net::DNS::thisHost().name()	/// То же, что uname -f
+		: Poco::Net::DNS::hostName();		/// То же, что uname -n
 
 	/// Заменяем точки на подчёркивания, чтобы Graphite не интерпретировал их, как разделители пути.
 	std::replace(std::begin(hostname_in_path), std::end(hostname_in_path), '.', '_');
 
-	root_path += hostname_in_path + config.getString(config_name + ".hostname_suffix", "");
+	root_path += hostname_in_path;
+
+	if (!use_fqdn)
+		root_path += config.getString(config_name + ".hostname_suffix", "");
 
 	if (sub_path.size())
 		root_path += "." + sub_path;
