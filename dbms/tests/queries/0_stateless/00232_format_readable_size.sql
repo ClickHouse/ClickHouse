d507c1f8af3c851d26e8aa10b2141e4402498ef8
Alexey Milovidov <milovidov@yandex-team.ru>
2016-03-31 04:30:07 +0300

    Updated tests after removing vectorclass library [#METR-20000].

--- dbms/tests/queries/0_stateless/00232_format_readable_size.sql
+++ dbms/tests/queries/0_stateless/00232_format_readable_size.sql
@@ -1,1 +1,1 @@
-SELECT exp(number) AS x, formatReadableSize(x), toUInt64(x) AS y, formatReadableSize(y), toInt32(y) AS z, formatReadableSize(z) FROM system.numbers LIMIT 70;
+SELECT round(exp(number), 6) AS x, formatReadableSize(x), toUInt64(x) AS y, formatReadableSize(y), toInt32(y) AS z, formatReadableSize(z) FROM system.numbers LIMIT 70;
