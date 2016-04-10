54d1ce01a650fdcb08f46a2e50ac76a211c3feba
Alexey Milovidov <milovidov@yandex-team.ru>
2016-04-10 05:29:45 +0300

    Preparation [#METR-19470].

--- dbms/include/DB/Dictionaries/MongoDBBlockInputStream.h
+++ dbms/include/DB/Dictionaries/MongoDBBlockInputStream.h
@@ -8,6 +8,7 @@
 #include <DB/DataTypes/DataTypeDate.h>
 #include <DB/DataTypes/DataTypeDateTime.h>
 #include <DB/Columns/ColumnString.h>
+#include <DB/Dictionaries/ExternalDatabaseHelper.h>
 #include <ext/range.hpp>
 #include <mongo/client/dbclient.h>
 #include <vector>
@@ -21,78 +22,16 @@ namespace DB
 /// Converts mongo::DBClientCursor to a stream of DB::Block`s
 class MongoDBBlockInputStream final : public IProfilingBlockInputStream
 {
-	enum struct value_type_t
-	{
-		UInt8,
-		UInt16,
-		UInt32,
-		UInt64,
-		Int8,
-		Int16,
-		Int32,
-		Int64,
-		Float32,
-		Float64,
-		String,
-		Date,
-		DateTime
-	};
-
 public:
 	MongoDBBlockInputStream(
-		std::unique_ptr<mongo::DBClientCursor> cursor_, const Block & sample_block_, const std::size_t max_block_size)
-		: cursor{std::move(cursor_)}, sample_block{sample_block_}, max_block_size{max_block_size}
+		std::unique_ptr<mongo::DBClientCursor> cursor_, const Block & sample_block, const std::size_t max_block_size)
+		: cursor{std::move(cursor_)}, max_block_size{max_block_size}
 	{
 		/// do nothing if cursor has no data
 		if (!cursor->more())
 			return;
 
-		const auto num_columns = sample_block.columns();
-		types.reserve(num_columns);
-		names.reserve(num_columns);
-		sample_columns.reserve(num_columns);
-
-		/// save types of each column to eliminate subsequent typeid_cast<> invocations
-		for (const auto idx : ext::range(0, num_columns))
-		{
-			const auto & column = sample_block.getByPosition(idx);
-			const auto type = column.type.get();
-
-			if (typeid_cast<const DataTypeUInt8 *>(type))
-				types.push_back(value_type_t::UInt8);
-			else if (typeid_cast<const DataTypeUInt16 *>(type))
-				types.push_back(value_type_t::UInt16);
-			else if (typeid_cast<const DataTypeUInt32 *>(type))
-				types.push_back(value_type_t::UInt32);
-			else if (typeid_cast<const DataTypeUInt64 *>(type))
-				types.push_back(value_type_t::UInt64);
-			else if (typeid_cast<const DataTypeInt8 *>(type))
-				types.push_back(value_type_t::Int8);
-			else if (typeid_cast<const DataTypeInt16 *>(type))
-				types.push_back(value_type_t::Int16);
-			else if (typeid_cast<const DataTypeInt32 *>(type))
-				types.push_back(value_type_t::Int32);
-			else if (typeid_cast<const DataTypeInt64 *>(type))
-				types.push_back(value_type_t::Int64);
-			else if (typeid_cast<const DataTypeFloat32 *>(type))
-				types.push_back(value_type_t::Float32);
-			else if (typeid_cast<const DataTypeFloat64 *>(type))
-				types.push_back(value_type_t::Float64);
-			else if (typeid_cast<const DataTypeString *>(type))
-				types.push_back(value_type_t::String);
-			else if (typeid_cast<const DataTypeDate *>(type))
-				types.push_back(value_type_t::Date);
-			else if (typeid_cast<const DataTypeDateTime *>(type))
-				types.push_back(value_type_t::DateTime);
-			else
-				throw Exception{
-					"Unsupported type " + type->getName(),
-					ErrorCodes::UNKNOWN_TYPE
-				};
-
-			names.emplace_back(column.name);
-			sample_columns.emplace_back(column.column.get());
-		}
+		helper.init(sample_block);
 	}
 
 	String getName() const override { return "MongoDB"; }
@@ -100,18 +39,19 @@ public:
 	String getID() const override
 	{
 		using stream = std::ostringstream;
-
 		return "MongoDB(@" + static_cast<stream &>(stream{} << cursor.get()).str() + ")";
 	}
 
 private:
+	using value_type_t = ExternalDatabaseHelper::value_type_t;
+
 	Block readImpl() override
 	{
 		/// return an empty block if cursor has no data
 		if (!cursor->more())
 			return {};
 
-		auto block = sample_block.cloneEmpty();
+		auto block = helper.sample_block.cloneEmpty();
 
 		/// cache pointers returned by the calls to getByPosition
 		std::vector<IColumn *> columns(block.columns());
@@ -127,12 +67,12 @@ private:
 
 			for (const auto idx : ext::range(0, size))
 			{
-				const auto & name = names[idx];
+				const auto & name = helper.names[idx];
 				const auto value = row[name];
 				if (value.ok())
-					insertValue(columns[idx], types[idx], value, name);
+					insertValue(columns[idx], helper.types[idx], value, name);
 				else
-					insertDefaultValue(columns[idx], *sample_columns[idx]);
+					insertDefaultValue(columns[idx], *helper.sample_columns[idx]);
 			}
 
 			++num_rows;
@@ -144,7 +84,7 @@ private:
 	}
 
 	static void insertValue(
-		IColumn * const column, const value_type_t type, const mongo::BSONElement & value, const mongo::StringData & name)
+		IColumn * const column, const value_type_t type, const mongo::BSONElement & value, const std::string & name)
 	{
 		switch (type)
 		{
@@ -153,9 +93,7 @@ private:
 				if (!value.isNumber() && value.type() != mongo::Bool)
 					throw Exception{
 						"Type mismatch, expected a number or Bool, got " + std::string{mongo::typeName(value.type())} +
-							" for column " + name.toString(),
-						ErrorCodes::TYPE_MISMATCH
-					};
+							" for column " + name, ErrorCodes::TYPE_MISMATCH};
 
 				static_cast<ColumnUInt8 *>(column)->insert(value.isNumber() ? value.numberInt() : value.boolean());
 				break;
@@ -165,9 +103,7 @@ private:
 				if (!value.isNumber())
 					throw Exception{
 						"Type mismatch, expected a number, got " + std::string{mongo::typeName(value.type())} +
-							" for column " + name.toString(),
-						ErrorCodes::TYPE_MISMATCH
-					};
+							" for column " + name, ErrorCodes::TYPE_MISMATCH};
 
 				static_cast<ColumnUInt16 *>(column)->insert(value.numberInt());
 				break;
@@ -177,9 +113,7 @@ private:
 				if (!value.isNumber())
 					throw Exception{
 						"Type mismatch, expected a number, got " + std::string{mongo::typeName(value.type())} +
-							" for column " + name.toString(),
-						ErrorCodes::TYPE_MISMATCH
-					};
+							" for column " + name, ErrorCodes::TYPE_MISMATCH};
 
 				static_cast<ColumnUInt32 *>(column)->insert(value.numberLong());
 				break;
@@ -189,9 +123,7 @@ private:
 				if (!value.isNumber())
 					throw Exception{
 						"Type mismatch, expected a number, got " + std::string{mongo::typeName(value.type())} +
-							" for column " + name.toString(),
-						ErrorCodes::TYPE_MISMATCH
-					};
+							" for column " + name, ErrorCodes::TYPE_MISMATCH};
 
 				static_cast<ColumnUInt64 *>(column)->insert(value.numberLong());
 				break;
@@ -201,9 +133,7 @@ private:
 				if (!value.isNumber() && value.type() != mongo::Bool)
 					throw Exception{
 						"Type mismatch, expected a number or Bool, got " + std::string{mongo::typeName(value.type())} +
-							" for column " + name.toString(),
-						ErrorCodes::TYPE_MISMATCH
-					};
+							" for column " + name, ErrorCodes::TYPE_MISMATCH};
 
 				static_cast<ColumnInt8 *>(column)->insert(value.isNumber() ? value.numberInt() : value.numberInt());
 				break;
@@ -213,9 +143,7 @@ private:
 				if (!value.isNumber())
 					throw Exception{
 						"Type mismatch, expected a number, got " + std::string{mongo::typeName(value.type())} +
-							" for column " + name.toString(),
-						ErrorCodes::TYPE_MISMATCH
-					};
+							" for column " + name, ErrorCodes::TYPE_MISMATCH};
 
 				static_cast<ColumnInt16 *>(column)->insert(value.numberInt());
 				break;
@@ -225,9 +153,7 @@ private:
 				if (!value.isNumber())
 					throw Exception{
 						"Type mismatch, expected a number, got " + std::string{mongo::typeName(value.type())} +
-							" for column " + name.toString(),
-						ErrorCodes::TYPE_MISMATCH
-					};
+							" for column " + name, ErrorCodes::TYPE_MISMATCH};
 
 				static_cast<ColumnInt32 *>(column)->insert(value.numberInt());
 				break;
@@ -237,9 +163,7 @@ private:
 				if (!value.isNumber())
 					throw Exception{
 						"Type mismatch, expected a number, got " + std::string{mongo::typeName(value.type())} +
-							" for column " + name.toString(),
-						ErrorCodes::TYPE_MISMATCH
-					};
+							" for column " + name, ErrorCodes::TYPE_MISMATCH};
 
 				static_cast<ColumnInt64 *>(column)->insert(value.numberLong());
 				break;
@@ -249,9 +173,7 @@ private:
 				if (!value.isNumber())
 					throw Exception{
 						"Type mismatch, expected a number, got " + std::string{mongo::typeName(value.type())} +
-							" for column " + name.toString(),
-						ErrorCodes::TYPE_MISMATCH
-					};
+							" for column " + name, ErrorCodes::TYPE_MISMATCH};
 
 				static_cast<ColumnFloat32 *>(column)->insert(value.number());
 				break;
@@ -261,9 +183,7 @@ private:
 				if (!value.isNumber())
 					throw Exception{
 						"Type mismatch, expected a number, got " + std::string{mongo::typeName(value.type())} +
-							" for column " + name.toString(),
-						ErrorCodes::TYPE_MISMATCH
-					};
+							" for column " + name, ErrorCodes::TYPE_MISMATCH};
 
 				static_cast<ColumnFloat64 *>(column)->insert(value.number());
 				break;
@@ -273,9 +193,7 @@ private:
 				if (value.type() != mongo::String)
 					throw Exception{
 						"Type mismatch, expected String, got " + std::string{mongo::typeName(value.type())} +
-							" for column " + name.toString(),
-						ErrorCodes::TYPE_MISMATCH
-					};
+							" for column " + name, ErrorCodes::TYPE_MISMATCH};
 
 				const auto string = value.String();
 				static_cast<ColumnString *>(column)->insertDataWithTerminatingZero(string.data(), string.size() + 1);
@@ -286,9 +204,7 @@ private:
 				if (value.type() != mongo::Date)
 					throw Exception{
 						"Type mismatch, expected Date, got " + std::string{mongo::typeName(value.type())} +
-							" for column " + name.toString(),
-						ErrorCodes::TYPE_MISMATCH
-					};
+							" for column " + name, ErrorCodes::TYPE_MISMATCH};
 
 				static_cast<ColumnUInt16 *>(column)->insert(
 					UInt16{DateLUT::instance().toDayNum(value.date().toTimeT())});
@@ -299,9 +215,7 @@ private:
 				if (value.type() != mongo::Date)
 					throw Exception{
 						"Type mismatch, expected Date, got " + std::string{mongo::typeName(value.type())} +
-							" for column " + name.toString(),
-						ErrorCodes::TYPE_MISMATCH
-					};
+							" for column " + name, ErrorCodes::TYPE_MISMATCH};
 
 				static_cast<ColumnUInt32 *>(column)->insert(value.date().toTimeT());
 				break;
@@ -315,11 +229,8 @@ private:
 	}
 
 	std::unique_ptr<mongo::DBClientCursor> cursor;
-	Block sample_block;
 	const std::size_t max_block_size;
-	std::vector<value_type_t> types;
-	std::vector<mongo::StringData> names;
-	std::vector<const IColumn *> sample_columns;
+	ExternalDatabaseHelper helper;
 };
 
 }
--- dbms/include/DB/Dictionaries/MySQLBlockInputStream.h
+++ dbms/include/DB/Dictionaries/MySQLBlockInputStream.h
@@ -7,6 +7,7 @@
 #include <DB/DataTypes/DataTypeDate.h>
 #include <DB/DataTypes/DataTypeDateTime.h>
 #include <DB/Columns/ColumnString.h>
+#include <DB/Dictionaries/ExternalDatabaseHelper.h>
 #include <ext/range.hpp>
 #include <mysqlxx/Query.h>
 #include <mysqlxx/PoolWithFailover.h>
@@ -24,80 +25,20 @@ namespace ErrorCodes
 /// Allows processing results of a MySQL query as a sequence of Blocks, simplifies chaining
 class MySQLBlockInputStream final : public IProfilingBlockInputStream
 {
-	enum struct value_type_t
-	{
-		UInt8,
-		UInt16,
-		UInt32,
-		UInt64,
-		Int8,
-		Int16,
-		Int32,
-		Int64,
-		Float32,
-		Float64,
-		String,
-		Date,
-		DateTime
-	};
-
 public:
 	MySQLBlockInputStream(
-		const mysqlxx::PoolWithFailover::Entry & entry, const std::string & query_str, const Block & sample_block_,
+		const mysqlxx::PoolWithFailover::Entry & entry, const std::string & query_str, const Block & sample_block,
 		const std::size_t max_block_size)
 		: entry{entry}, query{this->entry->query(query_str)}, result{query.use()},
-		  sample_block{sample_block_}, max_block_size{max_block_size}
+		  max_block_size{max_block_size}
 	{
 		if (sample_block.columns() != result.getNumFields())
 			throw Exception{
 				"mysqlxx::UseQueryResult contains " + toString(result.getNumFields()) + " columns while " +
 					toString(sample_block.columns()) + " expected",
-				ErrorCodes::NUMBER_OF_COLUMNS_DOESNT_MATCH
-			};
+				ErrorCodes::NUMBER_OF_COLUMNS_DOESNT_MATCH};
 
-		const auto num_columns = sample_block.columns();
-		types.reserve(num_columns);
-		sample_columns.reserve(num_columns);
-
-		for (const auto idx : ext::range(0, num_columns))
-		{
-			const auto & column = sample_block.getByPosition(idx);
-			const auto type = column.type.get();
-
-			if (typeid_cast<const DataTypeUInt8 *>(type))
-				types.push_back(value_type_t::UInt8);
-			else if (typeid_cast<const DataTypeUInt16 *>(type))
-				types.push_back(value_type_t::UInt16);
-			else if (typeid_cast<const DataTypeUInt32 *>(type))
-				types.push_back(value_type_t::UInt32);
-			else if (typeid_cast<const DataTypeUInt64 *>(type))
-				types.push_back(value_type_t::UInt64);
-			else if (typeid_cast<const DataTypeInt8 *>(type))
-				types.push_back(value_type_t::Int8);
-			else if (typeid_cast<const DataTypeInt16 *>(type))
-				types.push_back(value_type_t::Int16);
-			else if (typeid_cast<const DataTypeInt32 *>(type))
-				types.push_back(value_type_t::Int32);
-			else if (typeid_cast<const DataTypeInt64 *>(type))
-				types.push_back(value_type_t::Int64);
-			else if (typeid_cast<const DataTypeFloat32 *>(type))
-				types.push_back(value_type_t::Float32);
-			else if (typeid_cast<const DataTypeFloat64 *>(type))
-				types.push_back(value_type_t::Float64);
-			else if (typeid_cast<const DataTypeString *>(type))
-				types.push_back(value_type_t::String);
-			else if (typeid_cast<const DataTypeDate *>(type))
-				types.push_back(value_type_t::Date);
-			else if (typeid_cast<const DataTypeDateTime *>(type))
-				types.push_back(value_type_t::DateTime);
-			else
-				throw Exception{
-					"Unsupported type " + type->getName(),
-					ErrorCodes::UNKNOWN_TYPE
-				};
-
-			sample_columns.emplace_back(column.column.get());
-		}
+		helper.init(sample_block);
 	}
 
 	String getName() const override { return "MySQL"; }
@@ -108,13 +49,15 @@ public:
 	}
 
 private:
+	using value_type_t = ExternalDatabaseHelper::value_type_t;
+
 	Block readImpl() override
 	{
 		auto row = result.fetch();
 		if (!row)
 			return {};
 
-		auto block = sample_block.cloneEmpty();
+		auto block = helper.sample_block.cloneEmpty();
 
 		/// cache pointers returned by the calls to getByPosition
 		std::vector<IColumn *> columns(block.columns());
@@ -128,9 +71,9 @@ private:
 			{
 				const auto value = row[idx];
 				if (!value.isNull())
-					insertValue(columns[idx], types[idx], value);
+					insertValue(columns[idx], helper.types[idx], value);
 				else
-					insertDefaultValue(columns[idx], *sample_columns[idx]);
+					insertDefaultValue(columns[idx], *helper.sample_columns[idx]);
 			}
 
 			++num_rows;
@@ -157,12 +100,7 @@ private:
 			case value_type_t::Int64: static_cast<ColumnInt64 *>(column)->insert(value.getInt()); break;
 			case value_type_t::Float32: static_cast<ColumnFloat32 *>(column)->insert(value.getDouble()); break;
 			case value_type_t::Float64: static_cast<ColumnFloat64 *>(column)->insert(value.getDouble()); break;
-			case value_type_t::String:
-			{
-				const auto string = value.getString();
-				static_cast<ColumnString *>(column)->insertDataWithTerminatingZero(string.data(), string.size() + 1);
-				break;
-			}
+			case value_type_t::String: static_cast<ColumnString *>(column)->insertData(value.data(), value.size()); break;
 			case value_type_t::Date: static_cast<ColumnUInt16 *>(column)->insert(UInt16{value.getDate().getDayNum()}); break;
 			case value_type_t::DateTime: static_cast<ColumnUInt32 *>(column)->insert(time_t{value.getDateTime()}); break;
 		}
@@ -176,10 +114,8 @@ private:
 	mysqlxx::PoolWithFailover::Entry entry;
 	mysqlxx::Query query;
 	mysqlxx::UseQueryResult result;
-	Block sample_block;
 	const std::size_t max_block_size;
-	std::vector<value_type_t> types;
-	std::vector<const IColumn *> sample_columns;
+	ExternalDatabaseHelper helper;
 };
 
 }
