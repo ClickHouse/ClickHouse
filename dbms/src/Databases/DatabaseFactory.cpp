4cf43a7e792ad8d45c4d39243ba80ee5812f0d43
Alexey Milovidov <milovidov@yandex-team.ru>
2016-06-10 23:46:58 +0300

    Fixed error when table couldn't alter itself in constructor [#METR-21479].

--- dbms/src/Databases/DatabaseFactory.cpp
+++ dbms/src/Databases/DatabaseFactory.cpp
@@ -15,11 +15,10 @@ DatabasePtr DatabaseFactory::get(
 	const String & engine_name,
 	const String & database_name,
 	const String & path,
-	Context & context,
-	boost::threadpool::pool * thread_pool)
+	Context & context)
 {
 	if (engine_name == "Ordinary")
-		return std::make_shared<DatabaseOrdinary>(database_name, path, context, thread_pool);
+		return std::make_shared<DatabaseOrdinary>(database_name, path);
 
 	throw Exception("Unknown database engine: " + engine_name, ErrorCodes::UNKNOWN_DATABASE_ENGINE);
 }
