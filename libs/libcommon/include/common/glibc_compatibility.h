67890d185afb7b5a67bd536c6d8cb021885a88b5
Alexey Milovidov <milovidov@yandex-team.ru>
2016-06-27 09:52:27 +0300

    Addition to prev. revision [#METR-2944].

--- libs/libcommon/include/common/glibc_compatibility.h
+++ libs/libcommon/include/common/glibc_compatibility.h
@@ -10,7 +10,7 @@ extern "C" {
 #include <stdlib.h>
 #include <sys/select.h>
 
-inline long int __fdelt_chk(long int d)
+__attribute__((__weak__)) long int __fdelt_chk(long int d)
 {
 	if (d < 0 || d >= FD_SETSIZE)
 		abort();
@@ -19,7 +19,7 @@ inline long int __fdelt_chk(long int d)
 
 #include <sys/poll.h>
 
-inline int __poll_chk(struct pollfd * fds, nfds_t nfds, int timeout, __SIZE_TYPE__ fdslen)
+__attribute__((__weak__)) int __poll_chk(struct pollfd * fds, nfds_t nfds, int timeout, __SIZE_TYPE__ fdslen)
 {
 	if (fdslen / sizeof(*fds) < nfds)
 		abort();
@@ -27,11 +27,11 @@ inline int __poll_chk(struct pollfd * fds, nfds_t nfds, int timeout, __SIZE_TYPE
 }
 
 
-void * __memcpy_glibc_2_2_5(void *, const void *, size_t);
+__attribute__((__weak__)) void * __memcpy_glibc_2_2_5(void *, const void *, size_t);
 
 __asm__(".symver __memcpy_glibc_2_2_5, memcpy@GLIBC_2.2.5");
 
-inline void * __wrap_memcpy(void * dest, const void * src, size_t n)
+__attribute__((__weak__)) void * __wrap_memcpy(void * dest, const void * src, size_t n)
 {
     return __memcpy_glibc_2_2_5(dest, src, n);
 }
