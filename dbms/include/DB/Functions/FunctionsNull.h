91bfb1aee27864ea40ba3d7659f47147152ee34d
Alexey Arno <af-arno@yandex-team.ru>
2016-08-05 02:32:18 +0300

    dbms: Cleanup [#METR-19266]

--- dbms/include/DB/Functions/FunctionsNull.h
+++ dbms/include/DB/Functions/FunctionsNull.h
@@ -1,182 +1,49 @@
 #pragma once
 
 #include <DB/Functions/IFunction.h>
-#include <DB/Functions/FunctionsLogical.h>
-#include <DB/Functions/FunctionsConditional.h>
-#include <DB/DataTypes/DataTypesNumberFixed.h>
-#include <DB/DataTypes/DataTypeNull.h>
-#include <DB/DataTypes/DataTypeNullable.h>
-#include <DB/Columns/ColumnConst.h>
-#include <DB/Columns/ColumnNullable.h>
+#include <DB/DataTypes/IDataType.h>
+#include <DB/Core/ColumnNumbers.h>
 
 namespace DB
 {
+
+class Block;
+class Context;
 
 class FunctionIsNull : public IFunction
 {
 public:
 	static constexpr auto name = "isNull";
-	static FunctionPtr create(const Context & context) { return std::make_shared<FunctionIsNull>(); }
+	static FunctionPtr create(const Context & context);
 
-	std::string getName() const override
-	{
-		return name;
-	}
-
-	bool hasSpecialSupportForNulls() const override
-	{
-		return true;
-	}
-
-	DataTypePtr getReturnTypeImpl(const DataTypes & arguments) const override
-	{
-		if (arguments.size() != 1)
-			throw Exception("Number of arguments for function " + getName() + " doesn't match: passed "
-				+ toString(arguments.size()) + ", should be 1.",
-				ErrorCodes::NUMBER_OF_ARGUMENTS_DOESNT_MATCH);
-
-		return std::make_shared<DataTypeUInt8>();
-	}
-
-	void executeImpl(Block & block, const ColumnNumbers & arguments, size_t result) override
-	{
-		ColumnWithTypeAndName & elem = block.getByPosition(arguments[0]);
-		if (elem.column->isNull())
-		{
-			/// Trivial case.
-			block.getByPosition(result).column = std::make_shared<ColumnConstUInt8>(elem.column.get()->size(), 1);
-		}
-		else if (elem.column->isNullable())
-		{
-			/// Merely return the embedded null map.
-			ColumnNullable & nullable_col = static_cast<ColumnNullable &>(*elem.column);
-			block.getByPosition(result).column = nullable_col.getNullValuesByteMap();
-		}
-		else
-		{
-			/// Since no element is nullable, create a zero-filled null map.
-			block.getByPosition(result).column = std::make_shared<ColumnConstUInt8>(elem.column.get()->size(), 0);
-		}
-	}
+	std::string getName() const override;
+	bool hasSpecialSupportForNulls() const override;
+	DataTypePtr getReturnTypeImpl(const DataTypes & arguments) const override;
+	void executeImpl(Block & block, const ColumnNumbers & arguments, size_t result) override;
 };
 
 class FunctionIsNotNull : public IFunction
 {
 public:
 	static constexpr auto name = "isNotNull";
-	static FunctionPtr create(const Context & context) { return std::make_shared<FunctionIsNotNull>(); }
+	static FunctionPtr create(const Context & context);
 
-	std::string getName() const override
-	{
-		return name;
-	}
-
-	bool hasSpecialSupportForNulls() const override
-	{
-		return true;
-	}
-
-	DataTypePtr getReturnTypeImpl(const DataTypes & arguments) const override
-	{
-		if (arguments.size() != 1)
-			throw Exception("Number of arguments for function " + getName() + " doesn't match: passed "
-				+ toString(arguments.size()) + ", should be 1.",
-				ErrorCodes::NUMBER_OF_ARGUMENTS_DOESNT_MATCH);
-
-		return std::make_shared<DataTypeUInt8>();
-	}
-
-	void executeImpl(Block & block, const ColumnNumbers & arguments, size_t result) override
-	{
-		FunctionIsNull is_null_func;
-
-		ColumnWithTypeAndName temp_res;
-		temp_res.type = std::make_shared<DataTypeUInt8>();
-
-		block.insert(temp_res);
-
-		size_t temp_res_num = block.columns() - 1;
-
-		is_null_func.executeImpl(block, arguments, temp_res_num);
-
-		FunctionNot not_func;
-
-		ColumnNumbers new_args;
-		new_args.push_back(temp_res_num);
-		not_func.executeImpl(block, new_args, result);
-
-		block.erase(temp_res_num);
-	}
+	std::string getName() const override;
+	bool hasSpecialSupportForNulls() const override;
+	DataTypePtr getReturnTypeImpl(const DataTypes & arguments) const override;
+	void executeImpl(Block & block, const ColumnNumbers & arguments, size_t result) override;
 };
 
-/// XXX The code below is unfinished.
 class FunctionCoalesce : public IFunction
 {
 public:
 	static constexpr auto name = "coalesce";
-	static FunctionPtr create(const Context & context) { return std::make_shared<FunctionCoalesce>(); }
+	static FunctionPtr create(const Context & context);
 
-	std::string getName() const override
-	{
-		return name;
-	}
-
-	bool hasSpecialSupportForNulls() const override
-	{
-		return true;
-	}
-
-	DataTypePtr getReturnTypeImpl(const DataTypes & arguments) const override
-	{
-		DataTypes new_args;
-		for (size_t i = 0; i < arguments.size(); ++i)
-		{
-			new_args.push_back(std::make_shared<DataTypeUInt8>());
-			new_args.push_back(arguments[i]);
-		}
-		new_args.push_back(std::make_shared<DataTypeNull>());
-
-		return FunctionMultiIf{}.getReturnType(new_args);
-	}
-
-	void executeImpl(Block & block, const ColumnNumbers & arguments, size_t result) override
-	{
-		/// coalesce(arg0, arg1, ..., argN) is essentially
-		/// multiIf(isNotNull(arg0), arg0, isNotNull(arg1), arg1, ..., isNotNull(argN), argN, NULL)
-
-		for (size_t i = 0; i < arguments.size(); ++i)
-		{
-			ColumnWithTypeAndName elem;
-			elem.type = std::make_shared<DataTypeUInt8>();
-			elem.name = "isNotNull(" + block.getByPosition(arguments[i]).name + ")";
-
-			size_t res_pos = block.columns();
-			block.insert(elem);
-
-			FunctionIsNotNull{}.executeImpl(block, { arguments[i] }, res_pos);
-		}
-
-		ColumnNumbers new_args;
-		for (size_t i = 0; i < arguments.size(); ++i)
-		{
-			new_args.push_back(result + i);
-			new_args.push_back(arguments[i]);
-		}
-
-		/// For the NULL value.
-		new_args.push_back(block.columns());
-
-		/// Append a NULL column to block.
-		ColumnWithTypeAndName elem;
-		elem.column = std::make_shared<ColumnNull>(block.rowsInFirstColumn(), Null());
-		elem.type = std::make_shared<DataTypeNull>();
-		elem.name = "NULL";
-
-		block.insert(elem);
-
-		FunctionMultiIf{}.execute(block, new_args, result);
-	}
+	std::string getName() const override;
+	bool hasSpecialSupportForNulls() const override;
+	DataTypePtr getReturnTypeImpl(const DataTypes & arguments) const override;
+	void executeImpl(Block & block, const ColumnNumbers & arguments, size_t result) override;
 };
-
 
 }
--- dbms/src/Functions/FunctionsNull.cpp
+++ dbms/src/Functions/FunctionsNull.cpp
@@ -1,5 +1,12 @@
 #include <DB/Functions/FunctionsNull.h>
+#include <DB/Functions/FunctionsLogical.h>
+#include <DB/Functions/FunctionsConditional.h>
 #include <DB/Functions/FunctionFactory.h>
+#include <DB/DataTypes/DataTypesNumberFixed.h>
+#include <DB/DataTypes/DataTypeNull.h>
+#include <DB/DataTypes/DataTypeNullable.h>
+#include <DB/Columns/ColumnConst.h>
+#include <DB/Columns/ColumnNullable.h>
 
 namespace DB
 {
@@ -9,6 +16,171 @@ void registerFunctionsNull(FunctionFactory & factory)
 	factory.registerFunction<FunctionIsNull>();
 	factory.registerFunction<FunctionIsNotNull>();
 	factory.registerFunction<FunctionCoalesce>();
+}
+
+/// Implementation of isNull.
+
+FunctionPtr FunctionIsNull::create(const Context & context)
+{
+	return std::make_shared<FunctionIsNull>();
+}
+
+std::string FunctionIsNull::getName() const
+{
+	return name;
+}
+
+bool FunctionIsNull::hasSpecialSupportForNulls() const
+{
+	return true;
+}
+
+DataTypePtr FunctionIsNull::getReturnTypeImpl(const DataTypes & arguments) const
+{
+	if (arguments.size() != 1)
+		throw Exception("Number of arguments for function " + getName() + " doesn't match: passed "
+			+ toString(arguments.size()) + ", should be 1.",
+			ErrorCodes::NUMBER_OF_ARGUMENTS_DOESNT_MATCH);
+
+	return std::make_shared<DataTypeUInt8>();
+}
+
+void FunctionIsNull::executeImpl(Block & block, const ColumnNumbers & arguments, size_t result)
+{
+	ColumnWithTypeAndName & elem = block.getByPosition(arguments[0]);
+	if (elem.column->isNull())
+	{
+		/// Trivial case.
+		block.getByPosition(result).column = std::make_shared<ColumnConstUInt8>(elem.column.get()->size(), 1);
+	}
+	else if (elem.column->isNullable())
+	{
+		/// Merely return the embedded null map.
+		ColumnNullable & nullable_col = static_cast<ColumnNullable &>(*elem.column);
+		block.getByPosition(result).column = nullable_col.getNullValuesByteMap();
+	}
+	else
+	{
+		/// Since no element is nullable, create a zero-filled null map.
+		block.getByPosition(result).column = std::make_shared<ColumnConstUInt8>(elem.column.get()->size(), 0);
+	}
+}
+
+/// Implementation of isNotNull.
+
+FunctionPtr FunctionIsNotNull::create(const Context & context)
+{
+	return std::make_shared<FunctionIsNotNull>();
+}
+
+std::string FunctionIsNotNull::getName() const
+{
+	return name;
+}
+
+bool FunctionIsNotNull::hasSpecialSupportForNulls() const
+{
+	return true;
+}
+
+DataTypePtr FunctionIsNotNull::getReturnTypeImpl(const DataTypes & arguments) const
+{
+	if (arguments.size() != 1)
+		throw Exception("Number of arguments for function " + getName() + " doesn't match: passed "
+			+ toString(arguments.size()) + ", should be 1.",
+			ErrorCodes::NUMBER_OF_ARGUMENTS_DOESNT_MATCH);
+
+	return std::make_shared<DataTypeUInt8>();
+}
+
+void FunctionIsNotNull::executeImpl(Block & block, const ColumnNumbers & arguments, size_t result)
+{
+	FunctionIsNull is_null_func;
+
+	ColumnWithTypeAndName temp_res;
+	temp_res.type = std::make_shared<DataTypeUInt8>();
+
+	block.insert(temp_res);
+
+	size_t temp_res_num = block.columns() - 1;
+
+	is_null_func.executeImpl(block, arguments, temp_res_num);
+
+	FunctionNot not_func;
+
+	ColumnNumbers new_args;
+	new_args.push_back(temp_res_num);
+	not_func.executeImpl(block, new_args, result);
+
+	block.erase(temp_res_num);
+}
+
+/// Implementation of coalesce.
+
+FunctionPtr FunctionCoalesce::create(const Context & context)
+{
+	return std::make_shared<FunctionCoalesce>();
+}
+
+std::string FunctionCoalesce::getName() const
+{
+	return name;
+}
+
+bool FunctionCoalesce::hasSpecialSupportForNulls() const
+{
+	return true;
+}
+
+DataTypePtr FunctionCoalesce::getReturnTypeImpl(const DataTypes & arguments) const
+{
+	DataTypes new_args;
+	for (size_t i = 0; i < arguments.size(); ++i)
+	{
+		new_args.push_back(std::make_shared<DataTypeUInt8>());
+		new_args.push_back(arguments[i]);
+	}
+	new_args.push_back(std::make_shared<DataTypeNull>());
+
+	return FunctionMultiIf{}.getReturnType(new_args);
+}
+
+void FunctionCoalesce::executeImpl(Block & block, const ColumnNumbers & arguments, size_t result)
+{
+	/// coalesce(arg0, arg1, ..., argN) is essentially
+	/// multiIf(isNotNull(arg0), arg0, isNotNull(arg1), arg1, ..., isNotNull(argN), argN, NULL)
+
+	for (size_t i = 0; i < arguments.size(); ++i)
+	{
+		ColumnWithTypeAndName elem;
+		elem.type = std::make_shared<DataTypeUInt8>();
+		elem.name = "isNotNull(" + block.getByPosition(arguments[i]).name + ")";
+
+		size_t res_pos = block.columns();
+		block.insert(elem);
+
+		FunctionIsNotNull{}.executeImpl(block, { arguments[i] }, res_pos);
+	}
+
+	ColumnNumbers new_args;
+	for (size_t i = 0; i < arguments.size(); ++i)
+	{
+		new_args.push_back(result + i);
+		new_args.push_back(arguments[i]);
+	}
+
+	/// For the NULL value.
+	new_args.push_back(block.columns());
+
+	/// Append a NULL column to block.
+	ColumnWithTypeAndName elem;
+	elem.column = std::make_shared<ColumnNull>(block.rowsInFirstColumn(), Null());
+	elem.type = std::make_shared<DataTypeNull>();
+	elem.name = "NULL";
+
+	block.insert(elem);
+
+	FunctionMultiIf{}.execute(block, new_args, result);
 }
 
 }
