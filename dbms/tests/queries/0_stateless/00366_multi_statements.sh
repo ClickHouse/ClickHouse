ffbd901c9c7a6fb37aa7bd8a9e976ea133254f19
Alexey Milovidov <milovidov@yandex-team.ru>
2016-08-17 09:11:25 +0300

    Added test [#METR-22252].

--- dbms/tests/queries/0_stateless/00366_multi_statements.sh
+++ dbms/tests/queries/0_stateless/00366_multi_statements.sh
@@ -7,7 +7,7 @@ clickhouse-client --query="SELECT 1 ; "
 
 clickhouse-client --query="SELECT 1; S" 2>&1 | grep -o 'Syntax error'
 clickhouse-client --query="SELECT 1; SELECT 2" 2>&1 | grep -o 'Syntax error'
-clickhouse-client --query="SELECT 1; SELECT 2; 2>&1 | grep -o 'Syntax error'"
+clickhouse-client --query="SELECT 1; SELECT 2;" 2>&1 | grep -o 'Syntax error'
 clickhouse-client --query="SELECT 1; SELECT 2; SELECT" 2>&1 | grep -o 'Syntax error'
 
 clickhouse-client -n --query="SELECT 1; S" 2>&1 | grep -o 'Syntax error'
@@ -21,14 +21,10 @@ clickhouse-client --query="INSERT INTO test.t VALUES (1),(2),(3);"
 clickhouse-client --query="SELECT * FROM test.t"
 clickhouse-client --query="INSERT INTO test.t VALUES" <<< "(4),(5),(6)"
 clickhouse-client --query="SELECT * FROM test.t"
-clickhouse-client --query="INSERT INTO test.t VALUES (7)" <<< ",(8),(9)"
-clickhouse-client --query="SELECT * FROM test.t"
 
 clickhouse-client -n --query="INSERT INTO test.t VALUES (1),(2),(3);"
 clickhouse-client -n --query="SELECT * FROM test.t"
 clickhouse-client -n --query="INSERT INTO test.t VALUES" <<< "(4),(5),(6)"
-clickhouse-client -n --query="SELECT * FROM test.t"
-clickhouse-client -n --query="INSERT INTO test.t VALUES (7)" <<< ",(8),(9)"
 clickhouse-client -n --query="SELECT * FROM test.t"
 
 curl -sS 'http://localhost:8123/' -d "SELECT 1"
