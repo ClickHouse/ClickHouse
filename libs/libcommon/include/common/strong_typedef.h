38102a356c3a39a3536c3af7b9f283fecbe6a067
Pavel Kartavyy <kartavyy@yandex-team.ru>
2016-08-23 13:41:08 +0300

    StrongTypedef: fixed bug with same type. Supported only moved types like unique_ptr

--- libs/libcommon/include/common/strong_typedef.h
+++ libs/libcommon/include/common/strong_typedef.h
@@ -1,41 +1,62 @@
 #pragma once
 
 #include <boost/operators.hpp>
+#include <type_traits>
 
 /** https://svn.boost.org/trac/boost/ticket/5182
   */
 
-template <class T>
+template <class T, class Tag>
 struct StrongTypedef
-    : boost::totally_ordered1< StrongTypedef<T>
-							   , boost::totally_ordered2< StrongTypedef<T>, T> >
+	: boost::totally_ordered1< StrongTypedef<T, Tag>
+							   , boost::totally_ordered2< StrongTypedef<T, Tag>, T> >
 {
-	using Self = StrongTypedef<T>;
-    T t;
+	using Self = StrongTypedef<T, Tag>;
+	T t;
 
-    explicit StrongTypedef(const T t_) : t(t_) {};
-    StrongTypedef(): t() {};
-    StrongTypedef(const Self & t_) : t(t_.t){}
-    Self & operator=(const Self & rhs) { t = rhs.t; return *this;}
-    Self & operator=(const T & rhs) { t = rhs; return *this;}
-    operator const T & () const {return t; }
-    operator T & () { return t; }
-    bool operator==(const Self & rhs) const { return t == rhs.t; }
-    bool operator<(const Self & rhs) const { return t < rhs.t; }
-    T & toUnderType() { return t; }
-    const T & toUnderType() const { return t; }
+	template <class Enable = typename std::is_copy_constructible<T>::type>
+	explicit StrongTypedef(const T & t_) : t(t_) {};
+	template <class Enable = typename std::is_move_constructible<T>::type>
+	explicit StrongTypedef(T && t_) : t(std::move(t_)) {};
+
+	template <class Enable = typename std::is_default_constructible<T>::type>
+	StrongTypedef(): t() {};
+
+	StrongTypedef(const Self &) = default;
+	StrongTypedef(Self &&) = default;
+
+	Self & operator=(const Self &) = default;
+	Self & operator=(Self &&) = default;
+
+	template <class Enable = typename std::is_copy_assignable<T>::type>
+	Self & operator=(const T & rhs) { t = rhs; return *this;}
+
+	template <class Enable = typename std::is_move_assignable<T>::type>
+	Self & operator=(T && rhs) { t = std::move(rhs); return *this;}
+
+	operator const T & () const {return t; }
+	operator T & () { return t; }
+
+	bool operator==(const Self & rhs) const { return t == rhs.t; }
+	bool operator<(const Self & rhs) const { return t < rhs.t; }
+
+	T & toUnderType() { return t; }
+	const T & toUnderType() const { return t; }
 };
 
 namespace std
 {
-	template <class T>
-	struct hash<StrongTypedef<T>>
+	template <class T, class Tag>
+	struct hash<StrongTypedef<T, Tag>>
 	{
-		size_t operator()(const StrongTypedef<T> & x) const
+		size_t operator()(const StrongTypedef<T, Tag> & x) const
 		{
 			return std::hash<T>()(x.toUnderType());
 		}
 	};
 }
 
-#define STRONG_TYPEDEF(T, D) using D = StrongTypedef<T>;
+#define STRONG_TYPEDEF(T, D) \
+	struct D ## Tag {}; \
+	using D = StrongTypedef<T, D ## Tag>; \
+
