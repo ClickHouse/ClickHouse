5dc3b28774459ff9de9ff5dcad3212ca0f15f232
Alexey Arno <af-arno@yandex-team.ru>
2016-08-31 19:42:04 +0300

    dbms: Fixed deadlock. [#METR-18510]

--- dbms/src/Interpreters/ClusterProxy/AlterQueryConstructor.cpp
+++ dbms/src/Interpreters/ClusterProxy/AlterQueryConstructor.cpp
@@ -1,6 +1,7 @@
 #include <DB/Interpreters/ClusterProxy/AlterQueryConstructor.h>
 #include <DB/Interpreters/InterpreterAlterQuery.h>
 #include <DB/DataStreams/RemoteBlockInputStream.h>
+#include <DB/DataStreams/DeferredExecutionBlockInputStream.h>
 
 namespace DB
 {
@@ -17,8 +18,13 @@ namespace ClusterProxy
 
 BlockInputStreamPtr AlterQueryConstructor::createLocal(ASTPtr query_ast, const Context & context, const Cluster::Address & address)
 {
-	InterpreterAlterQuery interpreter{query_ast, context};
-	return interpreter.execute().in;
+	/// The ALTER query may be a resharding query that is a part of a distributed
+	/// job. Since the latter heavily relies on synchronization among its participating
+	/// nodes, it is very important to defer the execution of a local query so as
+	/// to prevent any deadlock.
+	auto interpreter = std::make_shared<InterpreterAlterQuery>(query_ast, context);
+	auto stream = std::make_shared<DeferredExecutionBlockInputStream>(interpreter);
+	return stream;
 }
 
 BlockInputStreamPtr AlterQueryConstructor::createRemote(IConnectionPool * pool, const std::string & query,
