72f0dba30da1ce6c96a068e79895ef496a2dae1f
Alexey Milovidov <milovidov@yandex-team.ru>
2016-10-25 01:33:40 +0300

    Addition to prev. revision [#METR-23279].

--- dbms/src/Server/HTTPHandler.cpp
+++ dbms/src/Server/HTTPHandler.cpp
@@ -253,6 +253,11 @@ void HTTPHandler::processQuery(
 	client_info.query_kind = ClientInfo::QueryKind::INITIAL_QUERY;
 	client_info.interface = ClientInfo::Interface::HTTP;
 
+	/// Query sent through HTTP interface is initial.
+	client_info.initial_user = client_info.current_user;
+	client_info.initial_query_id = client_info.current_query_id;
+	client_info.initial_address = client_info.current_address;
+
 	ClientInfo::HTTPMethod http_method = ClientInfo::HTTPMethod::UNKNOWN;
 	if (request.getMethod() == Poco::Net::HTTPServerRequest::HTTP_GET)
 		http_method = ClientInfo::HTTPMethod::GET;
