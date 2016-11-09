d4ba3941006506738144264d37fb0b062219230b
Vladimir Chebotarev <chebotarev@yandex-team.ru>
2016-11-09 23:06:29 +0300

    libmysqlxx: Secure way to patch mysqlclient library. [#METR-21516]

--- libs/libmysqlxx/patch.sh
+++ libs/libmysqlxx/patch.sh
@@ -2,12 +2,13 @@
 
 # Удаляем из библиотеки объектный файл с лишними символами, которые конфликтуют с имеющимися при статической сборке.
 
-LIB=$(find /usr/lib/ -name 'libmysqlclient.a')
+LIB=$1
+OUT=$2
 
 mkdir -p tmp
 cd tmp
 
 ar x $LIB
-ar t $LIB | grep -v 'my_new.cc.o' | xargs ar rcs ../$(basename $LIB)
+ar t $LIB | grep -v 'my_new.cc.o' | xargs ar rcs $OUT
 
 cd ..
