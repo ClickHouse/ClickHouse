cd15651dfda8ad4f01033a3989d19d313d64b543
Pavel Kartavyy <kartavyy@yandex-team.ru>
2016-11-25 12:29:17 +0300

    optimize if. added test for aliases

--- dbms/tests/queries/0_stateless/00393_if_with_constant_condition.sql
+++ dbms/tests/queries/0_stateless/00393_if_with_constant_condition.sql
@@ -3,3 +3,8 @@ SELECT 0 ? not_existing_column : 1 FROM system.numbers LIMIT 1;
 SELECT if(1, if(0, not_existing_column, 2), 0) FROM system.numbers LIMIT 1;
 
 SELECT (SELECT hasColumnInTable('system', 'numbers', 'not_existing')) ? not_existing : 42 FROM system.numbers LIMIT 1;
+
+/* alias test */
+SELECT 1 ? 1 : (0 as n), n FROM system.numbers LIMIT 1;
+SELECT 0 ? (number + 5 as n) : 0, n FROM system.numbers LIMIT 1;
+SELECT (2 as n) ?  1 : (number + 3 as nn), n, nn FROM system.numbers LIMIT 1;
