6998b5ecd9281be5f7ea978068be0af2dc08efb8
Alexey Milovidov <milovidov@yandex-team.ru>
2016-12-08 05:55:58 +0300

    ODBC: refreshment [#METR-18538].

--- dbms/src/ODBC/ResultSet.h
+++ dbms/src/ODBC/ResultSet.h
@@ -2,6 +2,7 @@
 
 #include <vector>
 #include <Poco/NumberParser.h>
+#include <Poco/Types.h>
 #include <sqltypes.h>
 
 #include "ReadHelpers.h"
@@ -14,8 +15,8 @@ class Field
 public:
 	std::string data;
 
-	UInt64 getUInt() const 	{ return Poco::NumberParser::parseUnsigned64(data); }
-	Int64 getInt() const		{ return Poco::NumberParser::parse64(data); }
+	Poco::UInt64 getUInt() const{ return Poco::NumberParser::parseUnsigned64(data); }
+	Poco::Int64 getInt() const	{ return Poco::NumberParser::parse64(data); }
 	float getFloat() const		{ return Poco::NumberParser::parseFloat(data); }
 	double getDouble() const	{ return Poco::NumberParser::parseFloat(data); }
 
