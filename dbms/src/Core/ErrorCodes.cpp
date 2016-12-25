c195b0212ba73c6df7edfea296d7715d14082622
Alexey Milovidov <milovidov@yandex-team.ru>
2016-12-25 07:39:22 +0300

    Fixed long delay when querying Merge table based on very large amount of StripeLog tables; fixed error in Merge table [#METR-23945].

--- dbms/src/Core/ErrorCodes.cpp
+++ dbms/src/Core/ErrorCodes.cpp
@@ -352,6 +352,7 @@ namespace ErrorCodes
 	extern const int CANNOT_LOAD_CONFIG = 347;
 	extern const int RESHARDING_NULLABLE_SHARDING_KEY = 348;
 	extern const int CANNOT_INSERT_NULL_IN_ORDINARY_COLUMN = 349;
+	extern const int INCOMPATIBLE_SOURCE_TABLES = 350;
 
 	extern const int KEEPER_EXCEPTION = 999;
 	extern const int POCO_EXCEPTION = 1000;
