ddc8fcf62338b01ed7b3991624700b226eea81f5
Alexey Milovidov <milovidov@yandex-team.ru>
2016-12-29 23:22:27 +0300

    Addition to prev. revision [#METR-23999].

--- dbms/include/DB/DataStreams/PrettySpaceBlockOutputStream.h
+++ dbms/include/DB/DataStreams/PrettySpaceBlockOutputStream.h
@@ -11,7 +11,7 @@ namespace DB
 class PrettySpaceBlockOutputStream : public PrettyBlockOutputStream
 {
 public:
-	PrettySpaceBlockOutputStream(WriteBuffer & ostr_, bool no_escapes_ = false, size_t max_rows_ = PRETTY_FORMAT_DEFAULT_MAX_ROWS)
+	PrettySpaceBlockOutputStream(WriteBuffer & ostr_, bool no_escapes_, size_t max_rows_)
 		: PrettyBlockOutputStream(ostr_, no_escapes_, max_rows_) {}
 
 	void write(const Block & block) override;
