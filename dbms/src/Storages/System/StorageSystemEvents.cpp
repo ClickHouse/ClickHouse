694b0fad971b4c0518780c1a3847e7a0639c5a03
Vitaliy Lyudvichenko <vludv@yandex-team.ru>
2016-12-31 03:06:23 +0300

    Reverted system.events changes. Added docs for system.metrics. [#METR-239111]

--- dbms/src/Storages/System/StorageSystemEvents.cpp
+++ dbms/src/Storages/System/StorageSystemEvents.cpp
@@ -1,5 +1,4 @@
 #include <DB/Common/ProfileEvents.h>
-#include <DB/Common/CurrentMetrics.h>
 #include <DB/Columns/ColumnString.h>
 #include <DB/DataTypes/DataTypeString.h>
 #include <DB/DataTypes/DataTypesNumberFixed.h>
@@ -11,18 +10,10 @@ namespace DB
 {
 
 
-enum class EventType
-{
-	ProfileEvent = 0,
-	CurrentMetric = 1
-};
-
-
 StorageSystemEvents::StorageSystemEvents(const std::string & name_)
 	: name(name_),
 	columns
 	{
-		{"type",		std::make_shared<DataTypeInt8>()},
 		{"event", 		std::make_shared<DataTypeString>()},
 		{"value",		std::make_shared<DataTypeUInt64>()}
 	}
@@ -55,23 +46,8 @@ BlockInputStreams StorageSystemEvents::read(
 
 		if (0 != value)
 		{
-			size_t col = 0;
-			block.unsafeGetByPosition(col++).column->insert(static_cast<Int64>(EventType::ProfileEvent));
-			block.unsafeGetByPosition(col++).column->insert(String(ProfileEvents::getDescription(ProfileEvents::Event(i))));
-			block.unsafeGetByPosition(col++).column->insert(value);
-		}
-	}
-
-	for (size_t i = 0, end = CurrentMetrics::end(); i < end; ++i)
-	{
-		UInt64 value = CurrentMetrics::values[i];
-
-		if (0 != value)
-		{
-			size_t col = 0;
-			block.unsafeGetByPosition(col++).column->insert(static_cast<Int64>(EventType::CurrentMetric));
-			block.unsafeGetByPosition(col++).column->insert(String(CurrentMetrics::getDescription(CurrentMetrics::Metric(i))));
-			block.unsafeGetByPosition(col++).column->insert(value);
+			block.unsafeGetByPosition(0).column->insert(String(ProfileEvents::getDescription(ProfileEvents::Event(i))));
+			block.unsafeGetByPosition(1).column->insert(value);
 		}
 	}
 
