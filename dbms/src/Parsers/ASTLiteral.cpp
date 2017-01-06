ae7e7e90eb1f82bd0fe0f887708d08b9e7755612
Alexey Milovidov <milovidov@yandex-team.ru>
2017-01-06 19:34:15 +0300

    Miscellaneous [#METR-2944].

--- dbms/src/Parsers/ASTLiteral.cpp
+++ dbms/src/Parsers/ASTLiteral.cpp
@@ -7,63 +7,6 @@ namespace DB
 {
 
 
-/** Обновляет SipHash по данным Field */
-class FieldVisitorHash : public StaticVisitor<>
-{
-private:
-	SipHash & hash;
-public:
-	FieldVisitorHash(SipHash & hash) : hash(hash) {}
-
-	void operator() (const Null 	& x) const
-	{
-		UInt8 type = Field::Types::Null;
-		hash.update(reinterpret_cast<const char *>(&type), sizeof(type));
-	}
-
-	void operator() (const UInt64 	& x) const
-	{
-		UInt8 type = Field::Types::UInt64;
-		hash.update(reinterpret_cast<const char *>(&type), sizeof(type));
-		hash.update(reinterpret_cast<const char *>(&x), sizeof(x));
-	}
-
-	void operator() (const Int64 	& x) const
-	{
-		UInt8 type = Field::Types::Int64;
-		hash.update(reinterpret_cast<const char *>(&type), sizeof(type));
-		hash.update(reinterpret_cast<const char *>(&x), sizeof(x));
-	}
-
-	void operator() (const Float64 	& x) const
-	{
-		UInt8 type = Field::Types::Float64;
-		hash.update(reinterpret_cast<const char *>(&type), sizeof(type));
-		hash.update(reinterpret_cast<const char *>(&x), sizeof(x));
-	}
-
-	void operator() (const String 	& x) const
-	{
-		UInt8 type = Field::Types::String;
-		hash.update(reinterpret_cast<const char *>(&type), sizeof(type));
-		size_t size = x.size();
-		hash.update(reinterpret_cast<const char *>(&size), sizeof(size));
-		hash.update(x.data(), x.size());
-	}
-
-	void operator() (const Array 	& x) const
-	{
-		UInt8 type = Field::Types::Array;
-		hash.update(reinterpret_cast<const char *>(&type), sizeof(type));
-		size_t size = x.size();
-		hash.update(reinterpret_cast<const char *>(&size), sizeof(size));
-
-		for (const auto & elem : x)
-			apply_visitor(*this, elem);
-	}
-};
-
-
 String ASTLiteral::getColumnName() const
 {
 	/// Отдельный случай для очень больших массивов. Вместо указания всех элементов, будем использовать хэш от содержимого.
