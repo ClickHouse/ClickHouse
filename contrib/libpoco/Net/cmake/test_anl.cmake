0df043fcece682a8938804589401d378265996c1
proller <proller@github.com>
2017-01-09 19:32:41 +0300

    fix static linking with libanl.a on ubuntu 16.10

---
+++ contrib/libpoco/Net/cmake/test_anl.cmake
@@ -0,0 +1,26 @@
+#
+# On ubuntu 16.10 static link libanl.a fails with -fPIC error
+#
+
+include(CheckCXXSourceRuns)
+
+if (USE_STATIC_LIBRARIES)
+	set(ANL_LIB_NAME "libanl.a")
+else()
+	set(ANL_LIB_NAME "anl")
+endif()
+
+find_library(ANL_LIB NAMES ${ANL_LIB_NAME})
+
+set(CMAKE_REQUIRED_LIBRARIES "${CMAKE_REQUIRED_LIBRARIES} ${ANL_LIB}")
+
+check_cxx_source_runs("
+	#include <netdb.h>
+	int main() { getaddrinfo_a(GAI_NOWAIT, nullptr, 0, nullptr); return 0;}
+" HAVE_GETADDRINFO_A)
+
+#message(STATUS "test_anl: USE_STATIC_LIBRARIES=${USE_STATIC_LIBRARIES} ANL_LIB_NAME=${ANL_LIB_NAME} ANL_LIB=${ANL_LIB} HAVE_GETADDRINFO_A=${HAVE_GETADDRINFO_A}")
+
+if (HAVE_GETADDRINFO_A)
+	add_definitions(-DHAVE_GETADDRINFO_A=1)
+endif ()
