9e06f407c8ee781ed8ddf98bdfcc31846bf2a0fe
Alexey Milovidov <milovidov@yandex-team.ru>
2017-01-14 11:55:14 +0300

    Less dependencies [#CLICKHOUSE-2]

--- dbms/src/Storages/MergeTree/MergeTreeDataSelectExecutor.cpp
+++ dbms/src/Storages/MergeTree/MergeTreeDataSelectExecutor.cpp
@@ -55,6 +55,7 @@ namespace ErrorCodes
 	extern const int INDEX_NOT_USED;
 	extern const int SAMPLING_NOT_SUPPORTED;
 	extern const int ILLEGAL_TYPE_OF_COLUMN_FOR_FILTER;
+	extern const int ILLEGAL_COLUMN;
 }
 
 
