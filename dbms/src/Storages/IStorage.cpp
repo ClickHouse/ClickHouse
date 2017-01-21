4dd26d3b591478624dc5656da453a6e539834370
Alexey Milovidov <milovidov@yandex-team.ru>
2017-01-21 07:00:41 +0300

    Less dependencies [#CLICKHOUSE-2].

---
+++ dbms/src/Storages/IStorage.cpp
@@ -0,0 +1,16 @@
+#include <DB/Storages/IStorage.h>
+
+
+namespace DB
+{
+
+TableStructureReadLock::TableStructureReadLock(StoragePtr storage_, bool lock_structure, bool lock_data)
+	: storage(storage_)
+{
+	if (lock_data)
+		data_lock.emplace(storage->data_lock);
+	if (lock_structure)
+		structure_lock.emplace(storage->structure_lock);
+}
+
+}
