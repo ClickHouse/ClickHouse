57f308d1e8df31143709870413246608efe4b4dd
Alexey Zatelepin <ztlpn@yandex-team.ru>
2017-01-31 15:17:42 +0300

    Introduce LRUCache::getOrSet(), use it to avoid mark cache stampede [#CLICKHOUSE-2115]

--- dbms/include/DB/Common/LRUCache.h
+++ dbms/include/DB/Common/LRUCache.h
@@ -4,9 +4,8 @@
 #include <list>
 #include <memory>
 #include <chrono>
-#include <Poco/ScopedLock.h>
 #include <mutex>
-#include <DB/Common/Exception.h>
+
 #include <common/logger_useful.h>
 
 
@@ -48,50 +47,73 @@ public:
 	{
 		std::lock_guard<std::mutex> lock(mutex);
 
-		auto it = cells.find(key);
-		if (it == cells.end())
-		{
+		auto res = getImpl(key, lock);
+		if (res)
+			++hits;
+		else
 			++misses;
-			return MappedPtr();
-		}
 
-		++hits;
-		Cell & cell = it->second;
-		updateCellTimestamp(cell);
-
-		/// Переместим ключ в конец очереди. Итератор остается валидным.
-		queue.splice(queue.end(), queue, cell.queue_iterator);
-
-		return cell.value;
+		return res;
 	}
 
-	void set(const Key & key, MappedPtr mapped)
+	void set(const Key & key, const MappedPtr & mapped)
 	{
 		std::lock_guard<std::mutex> lock(mutex);
 
-		auto res = cells.emplace(std::piecewise_construct,
-			std::forward_as_tuple(key),
-			std::forward_as_tuple());
+		setImpl(key, mapped, lock);
+	}
 
-		Cell & cell = res.first->second;
-		bool inserted = res.second;
-
-		if (inserted)
+	/// If the value for the key is in the cache, returns it. If it is not, calls load_func() to
+	/// produce it, saves the result in the cache and returns it.
+	/// Only one of several concurrent threads calling getOrSet() will call load_func(),
+	/// others will wait for that call to complete and will use its result (this helps prevent cache stampede).
+	/// Exceptions occuring in load_func will be propagated to the caller. Another thread from the
+	/// set of concurrent threads will then try to call its load_func etc.
+	template<typename LoadFunc>
+	std::pair<MappedPtr, bool> getOrSet(const Key & key, LoadFunc&& load_func)
+	{
+		InsertTokenHolder token_holder;
 		{
-			cell.queue_iterator = queue.insert(queue.end(), key);
+			std::lock_guard<std::mutex> lock(mutex);
+
+			auto val = getImpl(key, lock);
+			if (val)
+			{
+				++hits;
+				return std::make_pair(val, false);
+			}
+
+			auto & token = insert_tokens[key];
+			if (!token)
+				token = std::make_shared<InsertToken>(*this);
+
+			token_holder.acquire(&key, token, lock);
 		}
-		else
+
+		InsertToken * token = token_holder.token.get();
+
+		std::lock_guard<std::mutex> token_lock(token->mutex);
+
+		token_holder.cleaned_up = token->cleaned_up;
+
+		if (token->value)
 		{
-			current_size -= cell.size;
-			queue.splice(queue.end(), queue, cell.queue_iterator);
+			/// Another thread already produced the value while we waited for token->mutex.
+			++hits;
+			return std::make_pair(token->value, false);
 		}
 
-		cell.value = mapped;
-		cell.size = cell.value ? weight_function(*cell.value) : 0;
-		current_size += cell.size;
-		updateCellTimestamp(cell);
+		++misses;
+		token->value = load_func();
 
-		removeOverflow(cell.timestamp);
+		std::lock_guard<std::mutex> cache_lock(mutex);
+
+		setImpl(key, token->value, cache_lock);
+
+		if (!token->cleaned_up)
+			token_holder.cleanup(token_lock, cache_lock);
+
+		return std::make_pair(token->value, true);
 	}
 
 	void getStats(size_t & out_hits, size_t & out_misses) const
@@ -118,6 +140,7 @@ public:
 		std::lock_guard<std::mutex> lock(mutex);
 		queue.clear();
 		cells.clear();
+		insert_tokens.clear();
 		current_size = 0;
 		hits = 0;
 		misses = 0;
@@ -129,19 +152,84 @@ protected:
 	/// Суммарный вес выброшенных из кеша элементов.
 	/// Обнуляется каждый раз, когда информация добавляется в Profile events
 private:
+
+	/// Represents pending insertion attempt.
+	struct InsertToken
+	{
+		InsertToken(LRUCache & cache_) : cache(cache_) {}
+
+		std::mutex mutex;
+		bool cleaned_up = false; /// Protected by the token mutex
+		MappedPtr value; /// Protected by the token mutex
+
+		LRUCache & cache;
+		size_t refcount = 0; /// Protected by the cache mutex
+	};
+
+	using InsertTokenById = std::unordered_map<Key, std::shared_ptr<InsertToken>, HashFunction>;
+
+	/// This class is responsible for removing used insert tokens from the insert_tokens map.
+	/// Among several concurrent threads the first successful one is responsible for removal. But if they all
+	/// fail, then the last one is responsible.
+	struct InsertTokenHolder
+	{
+		const Key * key = nullptr;
+		std::shared_ptr<InsertToken> token;
+		bool cleaned_up = false;
+
+		InsertTokenHolder() = default;
+
+		void acquire(const Key * key_, const std::shared_ptr<InsertToken> & token_, std::lock_guard<std::mutex> & cache_lock)
+		{
+			key = key_;
+			token = token_;
+			++token->refcount;
+		}
+
+		void cleanup(std::lock_guard<std::mutex> & token_lock, std::lock_guard<std::mutex> & cache_lock)
+		{
+			auto it = token->cache.insert_tokens.find(*key);
+			if (it != token->cache.insert_tokens.end())
+				token->cache.insert_tokens.erase(it);
+
+			token->cleaned_up = true;
+			cleaned_up = true;
+		}
+
+		~InsertTokenHolder()
+		{
+			if (!token)
+				return;
+
+			if (cleaned_up)
+				return;
+
+			std::lock_guard<std::mutex> token_lock(token->mutex);
+
+			if (token->cleaned_up)
+				return;
+
+			std::lock_guard<std::mutex> cache_lock(token->cache.mutex);
+
+			--token->refcount;
+			if (token->refcount == 0)
+				cleanup(token_lock, cache_lock);
+		}
+	};
+
+	friend struct InsertTokenHolder;
+
 	using LRUQueue = std::list<Key>;
 	using LRUQueueIterator = typename LRUQueue::iterator;
 
 	struct Cell
 	{
-	public:
 		bool expired(const Timestamp & last_timestamp, const Delay & expiration_delay) const
 		{
 			return (expiration_delay == Delay::zero()) ||
 				((last_timestamp > timestamp) && ((last_timestamp - timestamp) > expiration_delay));
 		}
 
-	public:
 		MappedPtr value;
 		size_t size;
 		LRUQueueIterator queue_iterator;
@@ -149,6 +237,8 @@ private:
 	};
 
 	using Cells = std::unordered_map<Key, Cell, HashFunction>;
+
+	InsertTokenById insert_tokens;
 
 	LRUQueue queue;
 	Cells cells;
@@ -163,6 +253,50 @@ private:
 	size_t misses = 0;
 
 	WeightFunction weight_function;
+
+	MappedPtr getImpl(const Key & key, std::lock_guard<std::mutex> & cache_lock)
+	{
+		auto it = cells.find(key);
+		if (it == cells.end())
+		{
+			return MappedPtr();
+		}
+
+		Cell & cell = it->second;
+		updateCellTimestamp(cell);
+
+		/// Переместим ключ в конец очереди. Итератор остается валидным.
+		queue.splice(queue.end(), queue, cell.queue_iterator);
+
+		return cell.value;
+	}
+
+	void setImpl(const Key & key, const MappedPtr & mapped, std::lock_guard<std::mutex> & cache_lock)
+	{
+		auto res = cells.emplace(std::piecewise_construct,
+			std::forward_as_tuple(key),
+			std::forward_as_tuple());
+
+		Cell & cell = res.first->second;
+		bool inserted = res.second;
+
+		if (inserted)
+		{
+			cell.queue_iterator = queue.insert(queue.end(), key);
+		}
+		else
+		{
+			current_size -= cell.size;
+			queue.splice(queue.end(), queue, cell.queue_iterator);
+		}
+
+		cell.value = mapped;
+		cell.size = cell.value ? weight_function(*cell.value) : 0;
+		current_size += cell.size;
+		updateCellTimestamp(cell);
+
+		removeOverflow(cell.timestamp);
+	}
 
 	void updateCellTimestamp(Cell & cell)
 	{
