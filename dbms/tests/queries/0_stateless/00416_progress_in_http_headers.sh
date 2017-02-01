8d4a08cf9fd453d9b59f1b38cbaed7513cb1b43d
Alexey Milovidov <milovidov@yandex-team.ru>
2017-02-01 07:15:20 +0300

    Updated test [#CLICKHOUSE-32].

--- dbms/tests/queries/0_stateless/00416_progress_in_http_headers.sh
+++ dbms/tests/queries/0_stateless/00416_progress_in_http_headers.sh
@@ -4,4 +4,4 @@ curl -vsS 'http://localhost:8123/?max_block_size=1&send_progress_in_http_headers
 curl -sS 'http://localhost:8123/?max_block_size=1&send_progress_in_http_headers=1&http_headers_progress_interval_ms=0&enable_http_compression=1' -H 'Accept-Encoding: gzip' -d 'SELECT number FROM system.numbers LIMIT 10' | gzip -d
 
 # 'send_progress_in_http_headers' is false by default
-curl -vsS 'http://localhost:8123/?max_block_size=1&http_headers_progress_interval_ms=0' -d 'SELECT number FROM system.numbers LIMIT 10' 2>&1 | grep -q 'X-ClickHouse-Progress' && echo 'Fail'
+curl -vsS 'http://localhost:8123/?max_block_size=1&http_headers_progress_interval_ms=0' -d 'SELECT number FROM system.numbers LIMIT 10' 2>&1 | grep -q 'X-ClickHouse-Progress' && echo 'Fail' || true
