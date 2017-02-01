a4dfd58b89c1aef469b1abe45b047ad196d2475c
proller <proller@github.com>
2017-02-01 23:48:22 +0300

    release --version : only generate, commit and push new version

--- libs/libcommon/cmake/version.cmake
+++ libs/libcommon/cmake/version.cmake
@@ -1,6 +1,6 @@
 #This strings autochanged from release_lib.sh :
-set(VERSION_DESCRIBE v1.1.54141-testing)
-set(VERSION_REVISION 54141)
+set(VERSION_DESCRIBE v1.1.54146-testing)
+set(VERSION_REVISION 54146)
 #===end of autochange
 
 set(VERSION_MAJOR 1)
