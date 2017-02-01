d59f34f052d28faee9c2b0f880816cdfb6c3ee33
proller <proller@github.com>
2017-02-01 17:05:03 +0300

    Fix generating release version.

--- libs/libcommon/src/get_revision_lib.sh
+++
@@ -1,5 +0,0 @@
-# Get last revision number
-function get_revision {
-	BASEDIR=`dirname "$0"`
-	grep "set(VERSION_REVISION" ${BASEDIR}/libs/libcommon/cmake/version.cmake | sed 's/^.*VERSION_REVISION \(.*\))$/\1/'
-}
