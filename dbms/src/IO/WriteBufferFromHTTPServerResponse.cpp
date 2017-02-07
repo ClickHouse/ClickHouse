33f9917f530a8395863d793c70ff73c7d2c5c633
Alexey Milovidov <milovidov@yandex-team.ru>
2017-02-07 09:24:23 +0300

    Removed wrong code [#CLICKHOUSE-2].

--- dbms/src/IO/WriteBufferFromHTTPServerResponse.cpp
+++ dbms/src/IO/WriteBufferFromHTTPServerResponse.cpp
@@ -1,4 +1,3 @@
-#include <Poco/Version.h>
 #include <Poco/Net/HTTPServerResponse.h>
 
 #include <DB/Common/Exception.h>
@@ -31,12 +30,7 @@ void WriteBufferFromHTTPServerResponse::startSendHeaders()
 
 		setResponseDefaultHeaders(response);
 
-#if POCO_CLICKHOUSE_PATCH
 		std::tie(response_header_ostr, response_body_ostr) = response.beginSend();
-#else
-		response_body_ostr = &(response.send());
-		response_header_ostr = response_body_ostr;
-#endif
 	}
 }
 
@@ -47,12 +41,8 @@ void WriteBufferFromHTTPServerResponse::finishSendHeaders()
 	{
 		headers_finished_sending = true;
 
-#if POCO_CLICKHOUSE_PATCH
 		/// Send end of headers delimiter.
 		*response_header_ostr << "\r\n" << std::flush;
-#else
-		/// Newline autosent by response.send()
-#endif
 	}
 }
 
