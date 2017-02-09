256d6c05010189a3d4e0d4bdf3622742836591e6
Ivan Blinkov <ivan@blinkov.ru>
2017-02-09 19:08:59 +0300

    Add github pages script

---
+++ website/ghp.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+set -ex
+BASEDIR=$(mktemp)
+REMOTE_URL=$(git remote -v | grep origin | grep push | awk '{print $2;}')
+rm -rf "${BASEDIR}"
+git clone "${REMOTE_URL}" "${BASEDIR}"
+git checkout gh-pages
+(cd "${BASEDIR}"; git rm -r *)
+cat publish.txt | while read FILENAME
+do
+    cp "${FILENAME}" "${BASEDIR}/"
+    (cd "${BASEDIR}"; git add "${FILENAME}")
+done
+(cd "${BASEDIR}"; git commit -a -m "Website update"; git push origin gh-pages)
+rm -rf "${BASEDIR}"
