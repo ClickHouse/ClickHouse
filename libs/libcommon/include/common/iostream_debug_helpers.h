0e47dd42e3051e93ce28c7e9a91a0cbe0515847d
proller <proller@github.com>
2017-02-14 21:41:04 +0300

    fix

--- libs/libcommon/include/common/iostream_debug_helpers.h
+++ libs/libcommon/include/common/iostream_debug_helpers.h
@@ -3,7 +3,7 @@
 #include <vector>
 
 template <typename T>
-std::ostream & operator<<(std::ostream & stream, std::vector<T> & what)
+std::ostream & operator<<(std::ostream & stream, const std::vector<T> & what)
 {
 	stream << "vector(size = " << what.size() << ", capacity = " << what.capacity() << ")[";
 	bool first = true;
@@ -22,7 +22,7 @@ std::ostream & operator<<(std::ostream & stream, std::vector<T> & what)
 #include <array>
 
 template <typename T, size_t N>
-std::ostream & operator<<(std::ostream & stream, std::array<T, N> & what)
+std::ostream & operator<<(std::ostream & stream, const std::array<T, N> & what)
 {
 	stream << "array(size = " << what.size() << ")[";
 	bool first = true;
@@ -41,7 +41,7 @@ std::ostream & operator<<(std::ostream & stream, std::array<T, N> & what)
 #include <map>
 
 template <typename K, typename V>
-std::ostream & operator<<(std::ostream & stream, std::map<K, V> & what)
+std::ostream & operator<<(std::ostream & stream, const std::map<K, V> & what)
 {
 	stream << "map(size = " << what.size() << ")[";
 	bool first = true;
@@ -60,7 +60,7 @@ std::ostream & operator<<(std::ostream & stream, std::map<K, V> & what)
 #include <unordered_map>
 
 template <typename K, typename V>
-std::ostream & operator<<(std::ostream & stream, std::unordered_map<K, V> & what)
+std::ostream & operator<<(std::ostream & stream, const std::unordered_map<K, V> & what)
 {
 	stream << "unordered_map(size = " << what.size() << ")[";
 	bool first = true;
@@ -79,7 +79,7 @@ std::ostream & operator<<(std::ostream & stream, std::unordered_map<K, V> & what
 #include <utility>
 
 template <typename K, typename V>
-std::ostream & operator<<(std::ostream & stream, std::pair<K, V> & what)
+std::ostream & operator<<(std::ostream & stream, const std::pair<K, V> & what)
 {
 	stream << "pair[" << what.first << ", " << what.second << "]";
 	return stream;
