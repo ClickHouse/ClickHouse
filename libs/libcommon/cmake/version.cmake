bc9f8721db5b443c403690a6a1b187f24f73c252
Alexey Milovidov <milovidov@yandex-team.ru>
2017-02-14 16:41:08 +0300

    Auto version update to [54164]

--- libs/libcommon/cmake/version.cmake
+++ libs/libcommon/cmake/version.cmake
@@ -1,6 +1,6 @@
 #This strings autochanged from release_lib.sh :
-set(VERSION_DESCRIBE v1.1.54160-testing)
-set(VERSION_REVISION 54160)
+set(VERSION_DESCRIBE v1.1.54164-testing)
+set(VERSION_REVISION 54164)
 #===end of autochange
 
 set (VERSION_MAJOR 1)
