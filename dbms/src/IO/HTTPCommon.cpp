57c336f267bbf049f9ffdf87e50fe5f2cded4882
proller <proller@users.noreply.github.com>
2017-02-28 00:07:57 +0300

    HTTPDictionarySource support HTTPS (#510)
    * https support in dictionaries
    
    * Style

--- dbms/src/IO/HTTPCommon.cpp
+++ dbms/src/IO/HTTPCommon.cpp
@@ -1,7 +1,15 @@
 #include <DB/IO/HTTPCommon.h>
 
-#include <Poco/Util/Application.h>
+#include <Poco/Net/AcceptCertificateHandler.h>
+#include <Poco/Net/Context.h>
 #include <Poco/Net/HTTPServerResponse.h>
+#include <Poco/Net/InvalidCertificateHandler.h>
+#include <Poco/Net/PrivateKeyPassphraseHandler.h>
+#include <Poco/Net/RejectCertificateHandler.h>
+#include <Poco/Net/SSLManager.h>
+#include <Poco/Util/Application.h>
+#include <Poco/Util/Application.h>
+#include <Poco/Version.h>
 
 
 namespace DB
@@ -15,6 +23,31 @@ void setResponseDefaultHeaders(Poco::Net::HTTPServerResponse & response)
 	Poco::Timespan keep_alive_timeout(Poco::Util::Application::instance().config().getInt("keep_alive_timeout", 10), 0);
 	if (keep_alive_timeout.totalSeconds())
 		response.set("Keep-Alive", "timeout=" + std::to_string(keep_alive_timeout.totalSeconds()));
+}
+
+std::once_flag client_ssl_init_once;
+
+void clientSSLInit()
+{
+	// http://stackoverflow.com/questions/18315472/https-request-in-c-using-poco
+	Poco::Net::initializeSSL();
+	bool insecure = Poco::Util::Application::instance().config().getInt("https_client_insecure", false);
+	Poco::SharedPtr<Poco::Net::InvalidCertificateHandler> ptr_handler(insecure
+			? dynamic_cast<Poco::Net::InvalidCertificateHandler *>(new Poco::Net::AcceptCertificateHandler(true))
+			: dynamic_cast<Poco::Net::InvalidCertificateHandler *>(new Poco::Net::RejectCertificateHandler(true)));
+	Poco::Net::Context::Ptr ptr_context(new Poco::Net::Context(Poco::Net::Context::CLIENT_USE,
+		"",
+		"",
+		"",
+		insecure ? Poco::Net::Context::VERIFY_NONE : Poco::Net::Context::VERIFY_RELAXED,
+		9,
+		true));
+	ptr_context->enableSessionCache(true);
+#if POCO_VERSION >= 0x01070000
+	ptr_context->disableProtocols(Poco::Net::Context::PROTO_SSLV2 | Poco::Net::Context::PROTO_SSLV3);
+	ptr_context->preferServerCiphers();
+#endif
+	Poco::Net::SSLManager::instance().initializeClient(nullptr, ptr_handler, ptr_context);
 }
 
 }
