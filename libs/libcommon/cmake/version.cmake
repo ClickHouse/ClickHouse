6f106c748eed98ceb492768f0ca73cc51dc4da60
robot-metrika-test <robot-metrika-test@yandex-team.ru>
2017-02-27 10:01:52 +0300

    Auto version update to [54173]

--- libs/libcommon/cmake/version.cmake
+++ libs/libcommon/cmake/version.cmake
@@ -1,6 +1,6 @@
 #This strings autochanged from release_lib.sh :
-set(VERSION_DESCRIBE v1.1.54170-testing)
-set(VERSION_REVISION 54170)
+set(VERSION_DESCRIBE v1.1.54173-testing)
+set(VERSION_REVISION 54173)
 #===end of autochange
 
 set (VERSION_MAJOR 1)
