b09d9ff579b1c29d3ce46328fd57bc652d16ed66
Ivan Blinkov <ivan@blinkov.ru>
2017-03-28 20:52:14 +0300

    Basic support for website inside Docker

---
+++ website/docker.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+set -ex
+BASE_DIR=$(dirname $(readlink -f $0))
+IMAGE="clickhouse/website"
+TAG="0.1"
+FULL_NAME="${IMAGE}:${TAG}"
+REMOTE_NAME="registry.yandex.net/${FULL_NAME}"
+docker build -t "${FULL_NAME}" "${BASE_DIR}"
+docker tag "${FULL_NAME}" "${REMOTE_NAME}"
+docker push "${REMOTE_NAME}"
