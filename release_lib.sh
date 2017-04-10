72ccc6921225d71b158f2336daf379bab90412c2
proller <proller@users.noreply.github.com>
2017-04-10 20:43:30 +0300

    Debian: simpler package (#662)
    * Allow use external re2 with re2_st=re2
    
    * fix
    
    * remove dupe
    
    * use re2_st in FunctionsStringSearch.h
    
    * fix
    
    * move files from tools/etc to debian
    
    * dont generate control
    
    * version
    
    * ok.
    
    * wip
    
    * Cmake: dont touch CMAKE_INSTALL_PREFIX
    
    * wip
    
    * works!
    
    * clean
    
    * okay
    
    * like old
    
    * wip
    
    * wip
    
    * okay
    
    * clickhouse-server-base-dbg
    
    * clean
    
    * clickhouse-server-dbg
    
    * Debian: Remove daemons
    
    * Update rules
    
    * add source/format
    
    * control clean
    
    * add watch
    
    * clean
    
    * clean
    
    * temp fix build
    
    * clean
    
    * docs
    
    * fake make_control
    
    * fix
    
    * testme
    
    * testme
    
    * add metrika package
    
    * fake metrika files

--- release_lib.sh
+++ release_lib.sh
@@ -3,58 +3,9 @@ function get_revision {
     grep "set(VERSION_REVISION" ${BASEDIR}/dbms/cmake/version.cmake | sed 's/^.*VERSION_REVISION \(.*\))$/\1/'
 }
 
-function add_daemon_impl {
-    local daemon=$1
-    local control=$CONTROL
-    local dependencies=$2
-    local description_short="${daemon%/ daemon}"
-    local description_full=$3
-
-    echo -e "\n\n" >> $control;
-    echo "Package: $daemon" >> $control;
-    echo "Section: libdevel" >> $control;
-    echo "Architecture: any" >> $control;
-
-    echo -n "Depends: \${shlibs:Depends}, \${misc:Depends}" >> $control;
-    for dependency in $dependencies
-    do
-        echo -n ", $dependency" >> $control
-    done
-    echo >> $control
-
-    echo "Description: $description_short" >> $control;
-    echo " $description_full" >> $control;
-}
-
-# Создаём файл control из control.in.
-# добавляет в файл CONTROL секции для демонов из DAEMONS
+# remove me after fixing all testing-building scripts
 function make_control {
-    local CONTROL="$1"
-    local DAEMONS="$2"
-    rm -f $CONTROL
-    cp -f $CONTROL.in $CONTROL
-    for DAEMON_PKG in $DAEMONS
-    do
-        case "$DAEMON_PKG" in
-        'clickhouse-server' )
-            add_daemon_impl clickhouse-server-base 'adduser' 'clickhouse-server binary'
-            [ -n "$BUILD_PACKAGE_FOR_METRIKA" ] && add_daemon_impl clickhouse-server-metrika "clickhouse-server-base(=1.1.$REVISION)" 'Configuration files specific for Metrika project for clickhouse-server-base package'
-            add_daemon_impl clickhouse-server-common "clickhouse-server-base(=1.1.$REVISION)" 'Common configuration files for clickhouse-server-base package'
-        ;;
-        'clickhouse-client' )
-            add_daemon_impl clickhouse-client "clickhouse-server-base(=1.1.$REVISION)" "ClickHouse client and additional tools such as clickhouse-local and clickhouse-benchmark."
-        ;;
-        'clickhouse-benchmark' )
-            #skip it explicitly
-        ;;
-        'clickhouse-local' )
-            #skip it explicitly
-        ;;
-        * )
-            add_daemon_impl "${DAEMON_PKG}"
-        ;;
-        esac
-    done
+    true
 }
 
 # Генерируем номер ревизии.
@@ -133,7 +84,6 @@ function gen_changelog {
     CHDATE="$2"
     AUTHOR="$3"
     CHLOG="$4"
-    DAEMONS="$5"
 
     sed \
         -e "s/[@]REVISION[@]/$REVISION/g" \
@@ -147,7 +97,6 @@ function gen_changelog {
 # рабочая директория - где лежит сам скрипт
 function upload_debs {
     REVISION="$1"
-    DAEMONS="$2"
     # Определим репозиторий, в который надо загружать пакеты. Он соответствует версии Ubuntu.
     source /etc/lsb-release
 
