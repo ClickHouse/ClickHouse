3cbab4628028f000015cf2040e5177871dcb53e4
proller <proller@github.com>
2017-04-14 21:04:01 +0300

    fix formatting

--- libs/libmysqlxx/cmake/find_mysqlclient.cmake
+++ libs/libmysqlxx/cmake/find_mysqlclient.cmake
@@ -1,39 +1,39 @@
 option (ENABLE_MYSQL "Enamble Mysql" ON)
 
-if (ENABLE_MYSQL)
+    if (ENABLE_MYSQL)
 
-set (MYSQL_LIB_PATHS
-    "/usr/local/opt/mysql/lib"
-    "/usr/local/lib/mysql/"
-    "/usr/local/lib/mysql"
-    "/usr/local/lib64/mysql"
-    "/usr/mysql/lib/mysql"
-    "/usr/mysql/lib64/mysql"
-    "/usr/lib/mysql"
-    "/usr/lib64/mysql"
-    "/lib/mysql"
-    "/lib64/mysql"
-)
+    set (MYSQL_LIB_PATHS
+        "/usr/local/opt/mysql/lib"
+        "/usr/local/lib/mysql/"
+        "/usr/local/lib/mysql"
+        "/usr/local/lib64/mysql"
+        "/usr/mysql/lib/mysql"
+        "/usr/mysql/lib64/mysql"
+        "/usr/lib/mysql"
+        "/usr/lib64/mysql"
+        "/lib/mysql"
+        "/lib64/mysql"
+    )
 
-set (MYSQL_INCLUDE_PATHS
-    "/usr/local/opt/mysql/include"
-    "/usr/mysql/include/mysql"
-    "/usr/local/include/mysql"
-    "/usr/include/mysql"
-)
+    set (MYSQL_INCLUDE_PATHS
+        "/usr/local/opt/mysql/include"
+        "/usr/mysql/include/mysql"
+        "/usr/local/include/mysql"
+        "/usr/include/mysql"
+    )
 
-find_path (MYSQL_INCLUDE_DIR NAMES mysql/mysql.h PATHS ${MYSQL_INCLUDE_PATHS})
+    find_path (MYSQL_INCLUDE_DIR NAMES mysql/mysql.h PATHS ${MYSQL_INCLUDE_PATHS})
 
-if (USE_STATIC_LIBRARIES)
-    find_library (STATIC_MYSQLCLIENT_LIB libmysqlclient.a PATHS ${MYSQL_LIB_PATHS})
-else ()
-    find_library (MYSQLCLIENT_LIB mysqlclient PATHS ${MYSQL_LIB_PATHS})
-endif ()
+    if (USE_STATIC_LIBRARIES)
+        find_library (STATIC_MYSQLCLIENT_LIB libmysqlclient.a PATHS ${MYSQL_LIB_PATHS})
+    else ()
+        find_library (MYSQLCLIENT_LIB mysqlclient PATHS ${MYSQL_LIB_PATHS})
+    endif ()
 
-if (MYSQL_INCLUDE_DIR AND (STATIC_MYSQLCLIENT_LIB OR MYSQLCLIENT_LIB))
-    set (MYSQL_FOUND 1)
-    include_directories (${MYSQL_INCLUDE_DIR})
-endif ()
+    if (MYSQL_INCLUDE_DIR AND (STATIC_MYSQLCLIENT_LIB OR MYSQLCLIENT_LIB))
+        set (MYSQL_FOUND 1)
+        include_directories (${MYSQL_INCLUDE_DIR})
+    endif ()
 
 endif ()
 
