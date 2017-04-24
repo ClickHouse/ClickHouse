fd9d613dc936101254d9aecae1fce43f54968741
artpaul <stanly@yandex-team.ru>
2017-04-24 15:04:51 +0500

    #215 add tests

--- dbms/src/DataStreams/CastEnumBlockInputStream.h
+++ dbms/src/DataStreams/CastEnumBlockInputStream.h
@@ -1,7 +1,6 @@
 #pragma once
 
 #include <DataStreams/IProfilingBlockInputStream.h>
-#include <DataTypes/DataTypeEnum.h>
 
 #include <experimental/optional>
 #include <vector>
@@ -9,85 +8,23 @@
 namespace DB
 {
 
+/// Implicitly converts string and numeric values to Enum.
 class CastEnumBlockInputStream : public IProfilingBlockInputStream
 {
 public:
     CastEnumBlockInputStream(BlockInputStreamPtr input_,
                              const Block & in_sample_,
-                             const Block & out_sample_)
-    {
-        collectEnums(in_sample_, out_sample_);
-        children.push_back(input_);
-    }
+                             const Block & out_sample_);
 
-    String getName() const override { return "CastEnumBlockInputStream"; }
+    String getName() const override;
 
-    String getID() const override
-    {
-        std::stringstream res;
-        res << "CastEnumBlockInputStream(" << children.back()->getID() << ")";
-        return res.str();
-    }
+    String getID() const override;
 
 protected:
-    Block readImpl() override
-    {
-        Block block = children.back()->read();
-
-        if (!block || enum_types.empty())
-            return block;
-
-        Block res;
-        size_t s = block.columns();
-
-        for (size_t i = 0; i < s; ++i)
-        {
-            const auto & elem = block.getByPosition(i);
-
-            if (bool(enum_types[i]))
-            {
-                const auto & type = static_cast<const IDataTypeEnum*>(enum_types[i]->type.get());
-                ColumnPtr new_column = type->createColumn();
-
-                for (size_t j = 0; j < elem.column->size(); ++j)
-                    new_column->insert(type->castToValue((*elem.column)[j]));
-
-                res.insert({
-                    new_column,
-                    enum_types[i]->type,
-                    enum_types[i]->name}
-                );
-            }
-            else
-            {
-                res.insert(elem);
-            }
-        }
-
-        return res;
-    }
+    Block readImpl() override;
 
 private:
-    void collectEnums(const Block & in_sample, const Block & out_sample)
-    {
-        size_t in_size = in_sample.columns();
-        for (size_t i = 0; i < in_size; ++i)
-        {
-            const auto & in_elem  = in_sample.getByPosition(i);
-            const auto & out_elem = out_sample.getByPosition(i);
-
-            /// Force conversion only if source type is not Enum.
-            if ( dynamic_cast<IDataTypeEnum*>(out_elem.type.get()) &&
-                !dynamic_cast<IDataTypeEnum*>(in_elem.type.get()))
-            {
-                enum_types.push_back(NameAndTypePair(out_elem.name, out_elem.type));
-            }
-            else
-            {
-                enum_types.push_back(std::experimental::nullopt);
-            }
-        }
-    }
+    void collectEnums(const Block & in_sample, const Block & out_sample);
 
 private:
     std::vector<std::experimental::optional<NameAndTypePair>> enum_types;
