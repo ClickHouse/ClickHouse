948d9c39cd3cfcbf1f967e02108f2fdf03f71607
proller <proller@github.com>
2017-04-28 21:57:26 +0300

    Try fix macos build. Add more debug io helpers

--- dbms/src/Common/iostream_debug_helpers.cpp
+++ dbms/src/Common/iostream_debug_helpers.cpp
@@ -1,9 +1,14 @@
 #include "iostream_debug_helpers.h"
 
 #include <iostream>
-
-
+#include <Core/Block.h>
+#include <Core/Field.h>
+#include <Core/NamesAndTypes.h>
 #include <DataStreams/IBlockInputStream.h>
+#include <DataTypes/IDataType.h>
+#include <Functions/IFunction.h>
+#include <Storages/IStorage.h>
+
 std::ostream & operator<<(std::ostream & stream, const DB::IBlockInputStream & what)
 {
     stream << "IBlockInputStream(id = " << what.getID() << ", name = " << what.getName() << ")";
@@ -11,24 +16,18 @@ std::ostream & operator<<(std::ostream & stream, const DB::IBlockInputStream & w
     return stream;
 }
 
-
-#include <Core/NamesAndTypes.h>
-std::ostream & operator<<(std::ostream & stream, const DB::NameAndTypePair & what)
-{
-    stream << "NameAndTypePair(name = " << what.name << ", type = " << what.type << ")";
-    return stream;
-}
-
-
-#include <Core/Field.h>
 std::ostream & operator<<(std::ostream & stream, const DB::Field & what)
 {
     stream << "Field(type = " << what.getTypeName() << ")";
     return stream;
 }
 
+std::ostream & operator<<(std::ostream & stream, const DB::NameAndTypePair & what)
+{
+    stream << "NameAndTypePair(name = " << what.name << ", type = " << what.type << ")";
+    return stream;
+}
 
-#include <DataTypes/IDataType.h>
 std::ostream & operator<<(std::ostream & stream, const DB::IDataType & what)
 {
     stream << "IDataType(name = " << what.getName() << ", default = " << what.getDefault() << ", isNullable = " << what.isNullable()
@@ -36,11 +35,9 @@ std::ostream & operator<<(std::ostream & stream, const DB::IDataType & what)
     return stream;
 }
 
-
-#include <Storages/IStorage.h>
 std::ostream & operator<<(std::ostream & stream, const DB::IStorage & what)
 {
-    stream << "IStorage(name = " << what.getName() << "tableName" << what.getTableName() << ")"
+    stream << "IStorage(name = " << what.getName() << ", tableName = " << what.getTableName() << ")"
            << " {" << what.getColumnsList() << "}";
     // isRemote supportsSampling supportsFinal supportsPrewhere supportsParallelReplicas
     return stream;
@@ -49,5 +46,32 @@ std::ostream & operator<<(std::ostream & stream, const DB::IStorage & what)
 std::ostream & operator<<(std::ostream & stream, const DB::TableStructureReadLock & what)
 {
     stream << "TableStructureReadLock()";
+    return stream;
+}
+
+std::ostream & operator<<(std::ostream & stream, const DB::IFunction & what)
+{
+    stream << "IFunction(name = " << what.getName() << ", variadic = " << what.isVariadic() << ", args = " << what.getNumberOfArguments()
+           << ")";
+    return stream;
+}
+
+std::ostream & operator<<(std::ostream & stream, const DB::Block & what)
+{
+    stream << "Block(data = " << what.getColumns() << ")";
+    return stream;
+}
+
+std::ostream & operator<<(std::ostream & stream, const DB::ColumnWithTypeAndName & what)
+{
+    stream << "ColumnWithTypeAndName(name = " << what.name << ", type = " << what.type << ", column = " << what.column << ")";
+    return stream;
+}
+
+std::ostream & operator<<(std::ostream & stream, const DB::IColumn & what)
+{
+    stream << "IColumn(name = " << what.getName()
+           // TODO: maybe many flags here
+           << ")";
     return stream;
 }
