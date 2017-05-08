a21a6caf4ed93dfdc4b25a7287a3ae9221f52839
Dmitry Luhtionov <dmitryluhtionov@gmail.com>
2017-05-08 13:02:10 +0300

    Доюавленф функции FunctionMACNumToString и FunctionMACStringToNum

--- dbms/src/Functions/FunctionsCoding.cpp
+++ dbms/src/Functions/FunctionsCoding.cpp
@@ -14,6 +14,8 @@ void registerFunctionsCoding(FunctionFactory & factory)
     factory.registerFunction<FunctionIPv4StringToNum>();
     factory.registerFunction<FunctionIPv4NumToStringClassC>();
     factory.registerFunction<FunctionIPv4ToIPv6>();
+    factory.registerFunction<FunctionMACNumToString>();
+    factory.registerFunction<FunctionMACStringToNum>();
     factory.registerFunction<FunctionUUIDNumToString>();
     factory.registerFunction<FunctionUUIDStringToNum>();
     factory.registerFunction<FunctionHex>();
