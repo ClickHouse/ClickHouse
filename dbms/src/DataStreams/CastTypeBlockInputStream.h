4fd357a3118bb7be49910b1c7535b7cca22abdbe
Vitaliy Lyudvichenko <vludv@yandex-team.ru>
2017-05-22 13:20:22 +0300

    Add more clear comments, refined code. [#CLICKHOUSE-2957]

--- dbms/src/DataStreams/CastTypeBlockInputStream.h
+++ dbms/src/DataStreams/CastTypeBlockInputStream.h
@@ -27,20 +27,26 @@ private:
     const Context & context;
     Block ref_defenition;
 
+    /// Initializes cast_description and prepares tmp_conversion_block
     void initialize(const Block & src_block);
     bool initialized = false;
 
     struct CastElement
     {
+        /// Prepared function to do conversion
         std::shared_ptr<IFunction> function;
+        /// Position of first function argument in tmp_conversion_block
         size_t tmp_col_offset;
 
         CastElement(std::shared_ptr<IFunction> && function_, size_t tmp_col_offset_);
     };
 
     /// Describes required conversions on source block
+    /// Contains column numbers in source block that should be converted
     std::map<size_t, CastElement> cast_description;
-    /// Auxiliary block, stores arguments and results of required CAST calls
+
+    /// Auxiliary block, stores prefilled arguments and result for each CAST function in cast_description
+    /// 3 columns are allocated for each conversion: [blank of source column, column with res type name, blank of res column]
     Block tmp_conversion_block;
 };
 
