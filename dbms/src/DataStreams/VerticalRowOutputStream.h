7269f3a23b2e4fb4836ccce81be334cd3b2c3231
Alexey Milovidov <milovidov@yandex-team.ru>
2017-05-22 22:00:45 +0300

    Format Vertical: added support for totals, extremes and output_format_pretty_max_rows [#CLICKHOUSE-3014].

--- dbms/src/DataStreams/VerticalRowOutputStream.h
+++ dbms/src/DataStreams/VerticalRowOutputStream.h
@@ -18,24 +18,37 @@ class Context;
 class VerticalRowOutputStream : public IRowOutputStream
 {
 public:
-    VerticalRowOutputStream(WriteBuffer & ostr_, const Block & sample_, const Context & context);
+    VerticalRowOutputStream(WriteBuffer & ostr_, const Block & sample_, size_t max_rows_, const Context & context);
 
     void writeField(const IColumn & column, const IDataType & type, size_t row_num) override;
     void writeRowStartDelimiter() override;
     void writeRowBetweenDelimiter() override;
+    void writeSuffix() override;
 
     void flush() override;
+
+    void setTotals(const Block & totals_) override { totals = totals_; }
+    void setExtremes(const Block & extremes_) override { extremes = extremes_; }
 
 protected:
     virtual void writeValue(const IColumn & column, const IDataType & type, size_t row_num) const;
 
+    void writeTotals();
+    void writeExtremes();
+    /// For totals and extremes.
+    void writeSpecialRow(const Block & block, size_t row_num, const char * title);
+
     WriteBuffer & ostr;
     const Block sample;
+    size_t max_rows;
     size_t field_number = 0;
     size_t row_number = 0;
 
     using NamesAndPaddings = std::vector<String>;
     NamesAndPaddings names_and_paddings;
+
+    Block totals;
+    Block extremes;
 };
 
 
@@ -44,8 +57,7 @@ protected:
 class VerticalRawRowOutputStream final : public VerticalRowOutputStream
 {
 public:
-    VerticalRawRowOutputStream(WriteBuffer & ostr_, const Block & sample_, const Context & context)
-        : VerticalRowOutputStream(ostr_, sample_, context) {}
+    using VerticalRowOutputStream::VerticalRowOutputStream;
 
 protected:
     void writeValue(const IColumn & column, const IDataType & type, size_t row_num) const override;
