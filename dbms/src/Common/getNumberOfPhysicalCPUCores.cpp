0a553100760207d24a0b90f52d05a263520442ec
Alexey Milovidov <milovidov@yandex-team.ru>
2017-05-24 21:49:30 +0300

    Added comment [#CLICKHOUSE-3027].

--- dbms/src/Common/getNumberOfPhysicalCPUCores.cpp
+++ dbms/src/Common/getNumberOfPhysicalCPUCores.cpp
@@ -30,6 +30,12 @@ unsigned getNumberOfPhysicalCPUCores()
     unsigned res = data.num_cores * data.total_logical_cpus / data.num_logical_cpus;
 
     /// Also, libcpuid gives strange result on Google Compute Engine VMs.
+    /// Example:
+    ///  num_cores = 12,            /// number of physical cores on current CPU socket
+    ///  total_logical_cpus = 1,    /// total number of logical cores on all sockets
+    ///  num_logical_cpus = 24.     /// number of logical cores on current CPU socket
+    /// It means two-way hyper-threading (24 / 12), but contradictory, 'total_logical_cpus' == 1.
+
     if (res != 0)
         return res;
 #endif
