3c4cb9605ade7ebc58bccc899166b805645309bd
Alexey Milovidov <milovidov@yandex-team.ru>
2017-06-13 05:30:24 +0300

    Removed useless code [#CLICKHOUSE-31].

--- libs/libcommon/include/ext/range.h
+++ libs/libcommon/include/ext/range.h
@@ -3,35 +3,24 @@
 #include <type_traits>
 #include <utility>
 #include <iterator>
+#include <boost/iterator/counting_iterator.hpp>
+
 
 /** \brief Numeric range iterator, used to represent a half-closed interval [begin, end).
  *    In conjunction with std::reverse_iterator allows for forward and backward iteration
  *    over corresponding interval. */
 namespace ext
 {
-    /// @todo check whether difference_type should be signed (make_signed_t<T>)
-    template<typename T> struct range_iterator : std::iterator<
-        std::bidirectional_iterator_tag, T, T, void, T>
-    {
-        T current{};
+    template <typename T>
+    using range_iterator = boost::counting_iterator<T>;
 
-        range_iterator() = default;
-        range_iterator(const T t) : current(t) {}
-
-        T operator*() const { return current; }
-
-        range_iterator & operator++() { return ++current, *this; }
-        range_iterator & operator--() { return --current, *this; }
-
-        bool operator==(const range_iterator & other) const { return current == other.current; }
-        bool operator!=(const range_iterator & other) const { return current != other.current; }
-    };
-
-    template<typename T> using reverse_range_iterator = std::reverse_iterator<range_iterator<T>>;
+    template <typename T>
+    using reverse_range_iterator = std::reverse_iterator<range_iterator<T>>;
 
     /** \brief Range-based for loop adapter for (reverse_)range_iterator.
      *  By and large should be in conjunction with ext::range and ext::reverse_range. */
-    template<typename T, bool forward> struct range_wrapper
+    template <typename T, bool forward>
+    struct range_wrapper
     {
         using value_type = typename std::remove_reference<T>::type;
         using range_iterator_t = ext::range_iterator<value_type>;
