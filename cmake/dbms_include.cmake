609de9ffa949d230e69a7f6b4941648d3a2afa21
proller <proller@github.com>
2017-06-23 21:18:48 +0300

    Reorganize includes. part 3

--- cmake/dbms_include.cmake
+++ cmake/dbms_include.cmake
@@ -3,8 +3,3 @@ include_directories (${ClickHouse_SOURCE_DIR}/dbms/src)
 # for generated config_version.h and config.h:
 include_directories (${ClickHouse_BINARY_DIR}/dbms/src)
 
-# TODO:
-# move code with incldes from .h to .cpp and clean this list:
-include_directories (${ClickHouse_SOURCE_DIR}/libs/libcommon/include)
-# for generated config_common.h:
-include_directories (${ClickHouse_BINARY_DIR}/libs/libcommon/include)
