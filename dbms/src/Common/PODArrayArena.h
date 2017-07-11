c7a232772bd434a13da3c41358f9ff3a34b7b411
Vitaliy Lyudvichenko <vludv@yandex-team.ru>
2017-07-11 20:43:51 +0300

    Add adaptive allocator. [#CLICKHOUSE-3084]

--- dbms/src/Common/PODArrayArena.h
+++ dbms/src/Common/PODArrayArena.h
@@ -1,5 +1,6 @@
 #include <Common/PODArray.h>
 #include <Common/Arena.h>
+#include <Common/Allocator.h>
 
 
 namespace DB
@@ -9,20 +10,19 @@ namespace DB
 /// Used in aggregate functions
 struct ArenaAllocator
 {
-    void * alloc(size_t size, Arena * arena)
+    static void * alloc(size_t size, Arena * arena)
     {
         return arena->alloc(size);
     }
 
-    void * realloc(void * buf, size_t old_size, size_t new_size, Arena * arena)
+    static void * realloc(void * buf, size_t old_size, size_t new_size, Arena * arena)
     {
         char const * data = reinterpret_cast<char *>(buf);
 
+        // Invariant should be maintained: new_size > old_size
         if (data + old_size == arena->head->pos)
         {
             // Consecutive optimization
-            // Invariant should be maintained: new_size > old_size
-
             arena->allocContinue(new_size - old_size, data);
             return reinterpret_cast<void *>(const_cast<char *>(data));
         }
@@ -32,7 +32,40 @@ struct ArenaAllocator
         }
     }
 
-    void free(void * buf, size_t size) {}
+    static void free(void * buf, size_t size) {}
+};
+
+
+template <size_t REAL_ALLOCATION_TRESHOLD = 4096, typename TRealAllocator = Allocator<false>, typename TArenaAllocator = ArenaAllocator>
+class MixedArenaAllocator : private TRealAllocator
+{
+public:
+
+    void * alloc(size_t size, Arena * arena)
+    {
+        return (size < REAL_ALLOCATION_TRESHOLD) ? TArenaAllocator::alloc(size, arena) : TRealAllocator::alloc(size);
+    }
+
+    void * realloc(void * buf, size_t old_size, size_t new_size, Arena * arena)
+    {
+        // Invariant should be maintained: new_size > old_size
+
+        if (new_size < REAL_ALLOCATION_TRESHOLD)
+            return TArenaAllocator::realloc(buf, old_size, new_size, arena);
+
+        if (old_size >= REAL_ALLOCATION_TRESHOLD)
+            return TRealAllocator::realloc(buf, old_size, new_size);
+
+        void * new_buf = TRealAllocator::alloc(new_size);
+        memcpy(new_buf, buf, old_size);
+        return new_buf;
+    }
+
+    void free(void * buf, size_t size)
+    {
+        if (size >= REAL_ALLOCATION_TRESHOLD)
+            TRealAllocator::free(buf, size);
+    }
 };
 
 
