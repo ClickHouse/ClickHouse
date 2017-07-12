9a02c611a2a339d67b30cf8277e07dce02941ad9
Alexey Milovidov <milovidov@yandex-team.ru>
2017-07-12 07:17:05 +0300

    Using lexer (incomplete) [#CLICKHOUSE-2].

---
+++ dbms/src/Parsers/TokenIterator.h
@@ -0,0 +1,67 @@
+#pragma once
+
+#include <vector>
+#include <Parsers/Lexer.h>
+
+
+namespace DB
+{
+
+/** Parser operates on lazy stream of tokens.
+  * It could do lookaheads of any depth.
+  */
+
+/// Used as an input for parsers.
+class Tokens
+{
+private:
+    std::vector<Token> data;
+    Lexer lexer;
+
+public:
+    Tokens(const char * begin, const char * end) : lexer(begin, end) {}
+
+    const Token & operator[] (size_t index)
+    {
+        while (true)
+        {
+            if (index < data.size())
+                return data[index];
+
+            if (!data.empty() && data.back().type == TokenType::EndOfStream)
+                return data.back();
+
+            Token token = lexer.nextToken();
+
+            if (token.type != TokenType::Whitespace
+                && token.type != TokenType::Comment)
+                data.emplace_back(token);
+        }
+    }
+};
+
+
+/// To represent position in a token stream.
+class TokenIterator
+{
+private:
+    Tokens * tokens;
+    size_t index = 0;
+
+public:
+    TokenIterator(Tokens & tokens) : tokens(&tokens) {}
+
+    const Token & get() { return (*tokens)[index]; }
+    const Token & operator*() { return get(); }
+    const Token * operator->() { return &get(); }
+
+    TokenIterator & operator++()
+    {
+        ++index;
+        return *this;
+    }
+
+    bool isValid() { return get().type < TokenType::EndOfStream; }
+};
+
+}
