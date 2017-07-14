9ce4ff4c5b9199da63580663f317345b65938b9c
Vitaliy Lyudvichenko <vludv@yandex-team.ru>
2017-07-14 21:09:28 +0300

    Add check for unexisting column in CLEAR COLUMN. [#CLICKHOUSE-2896]

--- dbms/tests/queries/0_stateless/00446_clear_column_in_partition_concurrent.sh
+++ dbms/tests/queries/0_stateless/00446_clear_column_in_partition_concurrent.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
-set -e
 
 ch="clickhouse-client --stacktrace -q"
 
@@ -8,6 +7,14 @@ $ch "DROP TABLE IF EXISTS test.clear_column2"
 $ch "CREATE TABLE test.clear_column1 (d Date, i Int64, s String) ENGINE = ReplicatedMergeTree('/clickhouse/tables/test/clear_column', '1', d, d, 8192)"
 $ch "CREATE TABLE test.clear_column2 (d Date, i Int64, s String) ENGINE = ReplicatedMergeTree('/clickhouse/tables/test/clear_column', '2', d, d, 8192)"
 
+$ch "ALTER TABLE test.clear_column1 CLEAR COLUMN VasyaUnexistingColumn IN PARTITION '200001'" 1>/dev/null 2>/dev/null
+rc=$?
+if [ $rc -eq 0 ]; then
+    echo "An unexisisting column was ALTERed. Code: $rc"
+    exit -1
+fi
+
+set -e
 $ch "INSERT INTO test.clear_column1 VALUES ('2000-01-01', 1, 'a'), ('2000-02-01', 2, 'b')"
 $ch "INSERT INTO test.clear_column1 VALUES ('2000-01-01', 3, 'c'), ('2000-02-01', 4, 'd')"
 
@@ -24,5 +31,5 @@ wait
 $ch "SELECT DISTINCT * FROM test.clear_column1 WHERE d != toDate('2000-03-01') ORDER BY d, i, s"
 $ch "SELECT DISTINCT * FROM test.clear_column2 WHERE d != toDate('2000-03-01') ORDER BY d, i, s"
 
-$ch "DROP TABLE IF EXISTS test.clear_column1"
-$ch "DROP TABLE IF EXISTS test.clear_column2"
+#$ch "DROP TABLE IF EXISTS test.clear_column1"
+#$ch "DROP TABLE IF EXISTS test.clear_column2"
