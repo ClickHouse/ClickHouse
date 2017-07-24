7311d8bb1013465cbf26e8fa84f4e28ab15b4c7e
Alexey Milovidov <milovidov@yandex-team.ru>
2017-07-24 10:48:26 +0300

    Added support for non-constant and negative offset and size for substring function (continued) [#CLICKHOUSE-2].

--- dbms/src/Columns/ColumnVector.h
+++ dbms/src/Columns/ColumnVector.h
@@ -221,6 +221,11 @@ public:
 
     UInt64 get64(size_t n) const override;
 
+    UInt64 getUInt(size_t n) const override
+    {
+        return UInt64(data[n]);
+    }
+
     Int64 getInt(size_t n) const override
     {
         return Int64(data[n]);
