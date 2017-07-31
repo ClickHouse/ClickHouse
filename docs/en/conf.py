50cbb31406cf26a87a9c33299a4c9e9d857ebaf4
Ivan Blinkov <ivan@blinkov.ru>
2017-07-31 21:12:53 +0300

    CLICKHOUSE-3067: some documentation style tuning

--- docs/en/conf.py
+++ docs/en/conf.py
@@ -174,9 +174,9 @@ html_static_path = ['../_static']
 html_sidebars = {
     '**': [
         'about.html',
+        'searchbox.html',
         'navigation.html',
         'relations.html',
-        'searchbox.html',
     ]
 }
 
