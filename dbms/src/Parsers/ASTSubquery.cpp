48d2bbf2aa921f303710280ac4f9fedc0aabf682
Nikolai Kochetov <nik-kochetov@yandex-team.ru>
2017-08-10 17:46:46 +0300

    added former WITH clause to SELECT query

--- dbms/src/Parsers/ASTSubquery.cpp
+++ dbms/src/Parsers/ASTSubquery.cpp
@@ -3,7 +3,7 @@
 namespace DB
 {
 
-String ASTSubquery::getColumnName() const
+String ASTSubquery::getColumnNameImpl() const
 {
     /// This is a hack. We use alias, if available, because otherwise tree could change during analysis.
     return alias.empty() ? getTreeID() : alias;
