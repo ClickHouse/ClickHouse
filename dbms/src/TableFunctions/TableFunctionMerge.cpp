5ae1880c6f91d65ba6896819fadd71f2c31b3185
Vadim Skipin <vskipin@yandex-team.ru>
2017-08-18 22:30:55 +0300

    Fix typo

--- dbms/src/TableFunctions/TableFunctionMerge.cpp
+++ dbms/src/TableFunctions/TableFunctionMerge.cpp
@@ -71,7 +71,7 @@ StoragePtr TableFunctionMerge::execute(const ASTPtr & ast_function, const Contex
             " - name of source database and regexp for table names.",
             ErrorCodes::NUMBER_OF_ARGUMENTS_DOESNT_MATCH);
 
-    args[0] = evaluateConstantExpressionOrIdentidierAsLiteral(args[0], context);
+    args[0] = evaluateConstantExpressionOrIdentifierAsLiteral(args[0], context);
     args[1] = evaluateConstantExpressionAsLiteral(args[1], context);
 
     String source_database = static_cast<const ASTLiteral &>(*args[0]).value.safeGet<String>();
