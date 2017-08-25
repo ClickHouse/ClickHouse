f8c9059857979217b0d32d380db8d0bca5380027
Vitaliy Lyudvichenko <vludv@yandex-team.ru>
2017-08-25 19:19:57 +0300

    Fixed SIGABRT in DDLWorker. [#CLICKHOUSE-3251]

--- dbms/src/Interpreters/DDLWorker.cpp
+++ dbms/src/Interpreters/DDLWorker.cpp
@@ -886,7 +886,22 @@ void DDLWorker::run()
                 if (!e.isTemporaryError())
                 {
                     LOG_DEBUG(log, "Recovering ZooKeeper session after: " << getCurrentExceptionMessage(false));
-                    zookeeper = context.getZooKeeper();
+
+                    while (!stop_flag)
+                    {
+                        try
+                        {
+                            zookeeper = context.getZooKeeper();
+                            break;
+                        }
+                        catch (...)
+                        {
+                            tryLogCurrentException(__PRETTY_FUNCTION__);
+
+                            using namespace std::chrono_literals;
+                            std::this_thread::sleep_for(5s);
+                        }
+                    }
                 }
                 else
                 {
