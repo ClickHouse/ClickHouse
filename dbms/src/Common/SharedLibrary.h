3e4d1e39a2267666027e978df5d8f889796168c3
proller <proller@github.com>
2017-08-31 19:34:38 +0300

    Requested changes

--- dbms/src/Common/SharedLibrary.h
+++ dbms/src/Common/SharedLibrary.h
@@ -1,57 +1,30 @@
 #pragma once
 
+#include <memory>
 #include <string>
-#include <dlfcn.h>
-#include <Common/Exception.h>
 #include <boost/core/noncopyable.hpp>
-
 
 namespace DB
 {
-namespace ErrorCodes
-{
-    extern const int CANNOT_DLOPEN;
-    extern const int CANNOT_DLSYM;
-}
-
-
 /** Allows you to open a dynamic library and get a pointer to a function from it.
   */
 class SharedLibrary : private boost::noncopyable
 {
 public:
-    SharedLibrary(const std::string & path)
-    {
-        handle = dlopen(path.c_str(), RTLD_LAZY);
-        if (!handle)
-            throw Exception(std::string("Cannot dlopen: ") + dlerror(), ErrorCodes::CANNOT_DLOPEN);
-    }
+    SharedLibrary(const std::string & path);
 
-    ~SharedLibrary()
-    {
-        if (handle && dlclose(handle))
-            std::terminate();
-    }
+    ~SharedLibrary();
+
+    void * getImpl(const std::string & name, bool no_throw);
 
     template <typename Func>
     Func get(const std::string & name, bool no_throw = false)
     {
-        dlerror();
-
-        Func res = reinterpret_cast<Func>(dlsym(handle, name.c_str()));
-
-        if (char * error = dlerror())
-        {
-            if (no_throw)
-                return nullptr;
-            throw Exception(std::string("Cannot dlsym: ") + error, ErrorCodes::CANNOT_DLSYM);
-        }
-
-        return res;
+        return reinterpret_cast<Func>(getImpl(name, no_throw));
     }
 
 private:
-    void * handle;
+    void * handle = nullptr;
 };
 
 using SharedLibraryPtr = std::shared_ptr<SharedLibrary>;
