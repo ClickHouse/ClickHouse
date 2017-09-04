f68a0ea63bb5d66ae8bcc0bab8457b2961e899ca
proller <proller@users.noreply.github.com>
2017-09-04 20:04:33 +0300

    Fix mysql includes and remove libmariadb submodule (#1185)
    * Use bundled libmariadbclient (CLICKHOUSE-3261)
    
    * wip
    
    * wip
    
    * wip
    
    * Update docs about git clone. move docs/en/development/build_*.sh -> utils/build/build_debian.sh
    
    * Fix mysql includes
    
    * Cmake: don't install mariadb targets
    
    * Fix generated includes
    
    * Disable mysql tests
    
    * cmake: mariadb fixes
    
    * Fix
    
    * Update find_mysqlclient.cmake
    
    * Fix zlib
    
    * Remove bundled contrib/mariadb-connector-c
    
    * fix
    
    * Fix build
    
    * Fix includes

--- libs/libmysqlxx/cmake/find_mysqlclient.cmake
+++ libs/libmysqlxx/cmake/find_mysqlclient.cmake
@@ -1,15 +1,6 @@
 option (ENABLE_MYSQL "Enable MySQL" ON)
 
-option (USE_INTERNAL_MYSQL_LIBRARY "Set to FALSE to use system mysql library instead of bundled" ${NOT_UNBUNDLED})
-
 if (ENABLE_MYSQL)
-
-   if (NOT EXISTS "${ClickHouse_SOURCE_DIR}/contrib/mariadb-connector-c/CMakeLists.txt")
-      message (WARNING "submodule contrib/mariadb-connector-c is missing. to fix try run: git submodule update --init --recursive")
-      set (USE_INTERNAL_MYSQL_LIBRARY 0)
-   endif ()
-
-   if (NOT USE_INTERNAL_MYSQL_LIBRARY)
     set (MYSQL_LIB_PATHS
         "/usr/local/opt/mysql/lib"
         "/usr/local/lib/mysql/"
@@ -35,22 +26,16 @@ if (ENABLE_MYSQL)
     else ()
         find_library (MYSQLCLIENT_LIBRARIES mariadbclient mysqlclient PATHS ${MYSQL_LIB_PATHS})
     endif ()
-   endif ()
 
     if (MYSQL_INCLUDE_DIR AND (STATIC_MYSQLCLIENT_LIB OR MYSQLCLIENT_LIBRARIES))
-        include_directories (MYSQL_INCLUDE_DIR)
-    else ()                                   
-        set (USE_INTERNAL_MYSQL_LIBRARY 1)
-        set (MYSQLCLIENT_LIBRARIES mariadbclient)
-        set (MYSQL_INCLUDE_DIR "${ClickHouse_SOURCE_DIR}/contrib/mariadb-connector-c/include")
+        set (USE_MYSQL 1)
     endif ()
 
-    set(USE_MYSQL 1)
-    set(MYSQLXX_LIBRARY mysqlxx)
+    set (MYSQLXX_LIBRARY mysqlxx)
 endif ()
 
 if (USE_MYSQL)
-    message (STATUS "Using mysqlclient=${USE_MYSQL}: ${MYSQL_INCLUDE_DIR} : ${MYSQLCLIENT_LIBRARIES}; staticlib=${STATIC_MYSQLCLIENT_LIB} internal=${USE_INTERNAL_MYSQL_LIBRARY}")
+    message (STATUS "Using mysqlclient=${USE_MYSQL}: ${MYSQL_INCLUDE_DIR} : ${MYSQLCLIENT_LIBRARIES}; staticlib=${STATIC_MYSQLCLIENT_LIB}")
 else ()
     message (STATUS "Build without mysqlclient (support for MYSQL dictionary source will be disabled)")
 endif ()
