b37790d27e131c8d6bcb5749154dcbeebc050c76
Alexey Milovidov <milovidov@yandex-team.ru>
2017-09-04 23:21:55 +0300

    Minor modifications [#CLICKHOUSE-3201].

--- dbms/src/Functions/FunctionsGeo.cpp
+++ dbms/src/Functions/FunctionsGeo.cpp
@@ -8,6 +8,7 @@
 #include <Interpreters/ExpressionActions.h>
 #include <DataTypes/DataTypeTuple.h>
 #include <Columns/ColumnTuple.h>
+#include <IO/WriteHelpers.h>
 
 
 namespace DB
@@ -29,6 +30,9 @@ using PointInPolygonFranklin = boost::geometry::strategy::within::franklin<Args.
 template <template <typename...> typename Strategy>
 class FunctionPointInPolygon : public IFunction
 {
+private:
+    using CoordinateType = Float64;
+
 public:
     static const char * name;
 
@@ -52,6 +56,7 @@ public:
         return 0;
     }
 
+
     void getReturnTypeAndPrerequisitesImpl(
         const ColumnsWithTypeAndName & arguments, DataTypePtr & out_return_type, ExpressionActions::Actions & out_prerequisites) override
     {
@@ -65,21 +70,22 @@ public:
             const DataTypeTuple * tuple = checkAndGetDataType<DataTypeTuple>(&*arguments[i].type);
             if (tuple == nullptr)
             {
-                throw Exception("Argument " + std::to_string(i+1) + " for function " + getName() + " must be tuple.", ErrorCodes::ILLEGAL_TYPE_OF_ARGUMENT);
+                throw Exception("Argument " + toString(i + 1) + " for function " + getName() + " must be tuple.", ErrorCodes::ILLEGAL_TYPE_OF_ARGUMENT);
             }
 
             const DataTypes & elems = tuple->getElements();
 
             if (elems.size() != 2)
             {
-                throw Exception("Tuple in argument " + std::to_string(i+1) + " must have exactly two elements.", ErrorCodes::BAD_ARGUMENTS);
+                throw Exception("Tuple in argument " + toString(i + 1) + " must have exactly two elements.", ErrorCodes::BAD_ARGUMENTS);
             }
 
             for (size_t elems_index = 0; elems_index < elems.size(); ++elems_index)
             {
-                if (!checkDataType<DataTypeFloat64>(&*elems[elems_index]))
+                if (!checkDataType<DataTypeNumber<CoordinateType>>(&*elems[elems_index]))
                 {
-                    throw Exception("Tuple element " + std::to_string(elems_index + 1) + " in argument " + std::to_string(i+1) + " must be Float64.", ErrorCodes::BAD_ARGUMENTS);
+                    throw Exception("Tuple element " + toString(elems_index + 1) + " in argument " + toString(i + 1)
+                        + " must be " + TypeName<CoordinateType>::get() + ".", ErrorCodes::BAD_ARGUMENTS);
                 }
             }
         }
@@ -87,31 +93,28 @@ public:
         out_return_type = std::make_shared<DataTypeUInt8>();
     }
 
+
     void executeImpl(Block & block, const ColumnNumbers & arguments, size_t result) override
     {
-        if (block.rows() == 0)
-        {
-            return;
-        }
-
-        using PointType = boost::geometry::model::d2::point_xy<double>;
+        using PointType = boost::geometry::model::d2::point_xy<CoordinateType>;
         using PolygonType = boost::geometry::model::polygon<PointType>;
-        std::pair<double, double> min, max;
+        std::pair<CoordinateType, CoordinateType> min, max;
 
-        std::vector<PointType> polygon_points(arguments.size()-1);
+        std::vector<PointType> polygon_points(arguments.size() - 1);
 
         for (size_t i = 1; i < arguments.size(); ++i)
         {
             auto const_tuple_col = checkAndGetColumnConst<ColumnTuple>(block.getByPosition(arguments[i]).column.get());
             if (!const_tuple_col)
             {
-                throw Exception("Argument " + std::to_string(i+1) + " for function " + getName() + " must be constant tuple.", ErrorCodes::ILLEGAL_COLUMN);
+                throw Exception("Argument " + toString(i + 1) + " for function " + getName() + " must be constant tuple.", ErrorCodes::ILLEGAL_COLUMN);
             }
 
             TupleBackend data = const_tuple_col->getValue<Tuple>();
-            const double x = data[0].get<Float64>();
-            const double y = data[1].get<Float64>();
-            polygon_points[i-1] = PointType(x, y);
+            const CoordinateType x = data[0].get<Float64>();
+            const CoordinateType y = data[1].get<Float64>();
+            polygon_points[i - 1] = PointType(x, y);
+
             if (i == 1)
             {
                 min.first = x;
@@ -132,41 +135,43 @@ public:
         boost::geometry::assign_points(polygon, polygon_points);
 
         Strategy<PointType> strategy;
-        auto point_checker = [&](double x, double y) -> bool
+
+        auto point_checker = [&](CoordinateType x, CoordinateType y) -> bool
         {
             if (x < min.first || x > max.first || y < min.second || y > max.second)
-            {
                 return false;
-            }
+
             PointType point(x, y);
             return boost::geometry::covered_by(point, polygon, strategy);
         };
+
+        size_t rows = block.rows();
 
         auto point_column_const = checkAndGetColumnConst<ColumnTuple>(block.getByPosition(arguments[0]).column.get());
         if (point_column_const)
         {
             TupleBackend data = point_column_const->getValue<Tuple>();
-            const double point_x = data[0].get<Float64>();
-            const double point_y = data[1].get<Float64>();
-            UInt8 value = point_checker(point_x, point_y) ? 1 : 0;
-            block.getByPosition(result).column = DataTypeUInt8().createConstColumn(block.rows(), UInt64(value));
+            const CoordinateType point_x = data[0].get<Float64>();
+            const CoordinateType point_y = data[1].get<Float64>();
+            UInt8 value = point_checker(point_x, point_y);
+            block.getByPosition(result).column = DataTypeUInt8().createConstColumn(rows, UInt64(value));
             return;
         }
 
         auto & res = block.getByPosition(result);
-        res.column = std::make_shared<ColumnUInt8>(block.rows());
+        res.column = std::make_shared<ColumnUInt8>(rows);
         IColumn & result_column = *res.column;
         auto & result_data = static_cast<ColumnUInt8 &>(result_column).getData();
 
         auto point_column = checkAndGetColumn<ColumnTuple>(block.getByPosition(arguments[0]).column.get());
-        auto column_x = checkAndGetColumn<ColumnFloat64>(point_column->getData().getByPosition(0).column.get());
-        auto column_y = checkAndGetColumn<ColumnFloat64>(point_column->getData().getByPosition(1).column.get());
+        auto column_x = checkAndGetColumn<ColumnVector<CoordinateType>>(point_column->getData().getByPosition(0).column.get());
+        auto column_y = checkAndGetColumn<ColumnVector<CoordinateType>>(point_column->getData().getByPosition(1).column.get());
 
-        for (size_t i = 0; i < block.rows(); ++i)
+        for (size_t i = 0; i < rows; ++i)
         {
-            const double point_x = column_x->getElement(i);
-            const double point_y = column_y->getElement(i);
-            result_data[i] = point_checker(point_x, point_y) ? 1 : 0;
+            const CoordinateType point_x = column_x->getElement(i);
+            const CoordinateType point_y = column_y->getElement(i);
+            result_data[i] = point_checker(point_x, point_y);
         }
     }
 };
