1f27da95fd11803b526e0310baf1280e5281f1e5
proller <proller@github.com>
2017-09-06 22:06:26 +0300

    Again fix test 00501_http_head

--- dbms/tests/queries/0_stateless/00501_http_head.sh
+++ dbms/tests/queries/0_stateless/00501_http_head.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 
-( curl -v --max-time 0.3 -X "HEAD" 'http://localhost:18123/?query=SELECT%201';
-  curl -v --max-time 0.3 -X "HEAD" 'http://localhost:18123/?query=select+*+from+system.numbers+limit+1000000' ) 2>&1 | grep -e "^<" | grep -v "Date:"
+( curl -s --head "${CLICKHOUSE_URL:=http://localhost:8123/}?query=SELECT%201";
+  curl -s --head "${CLICKHOUSE_URL:=http://localhost:8123/}?query=select+*+from+system.numbers+limit+1000000" ) | grep -v "Date:"
