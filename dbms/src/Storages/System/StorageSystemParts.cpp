19c0e2fa9e6f0bed64cc90fdfec9320d1ddf6244
Vadim Skipin <vskipin@yandex-team.ru>
2017-09-11 15:39:01 +0300

    Context parameter added to the IDatabase methods

--- dbms/src/Storages/System/StorageSystemParts.cpp
+++ dbms/src/Storages/System/StorageSystemParts.cpp
@@ -93,7 +93,7 @@ BlockInputStreams StorageSystemParts::read(
             const DatabasePtr database = databases.at(database_name);
 
             offsets[i] = i ? offsets[i - 1] : 0;
-            for (auto iterator = database->getIterator(); iterator->isValid(); iterator->next())
+            for (auto iterator = database->getIterator(context); iterator->isValid(); iterator->next())
             {
                 String table_name = iterator->name();
                 StoragePtr storage = iterator->table();
