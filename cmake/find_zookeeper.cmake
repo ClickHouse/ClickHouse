08f8880820be1e86326a58adce121fd462bf035f
proller <proller@github.com>
2017-09-28 18:04:44 +0300

    zookeeper 3.4.10 as submodule

--- cmake/find_zookeeper.cmake
+++ cmake/find_zookeeper.cmake
@@ -9,7 +9,7 @@ if (ZOOKEEPER_LIBRARY AND ZOOKEEPER_INCLUDE_DIR)
     include_directories (${ZOOKEEPER_INCLUDE_DIR})
 else ()
     set (USE_INTERNAL_ZOOKEEPER_LIBRARY 1)
-    set (ZOOKEEPER_LIBRARY zookeeper_mt)
+    set (ZOOKEEPER_LIBRARY zookeeper)
 endif ()
 
 message (STATUS "Using zookeeper: ${ZOOKEEPER_INCLUDE_DIR} : ${ZOOKEEPER_LIBRARY}")
