e817de7e21c6806564ee36bb0c227bacfeb304b0
Nikolai Kochetov <nik-kochetov@yandex-team.ru>
2017-10-06 21:05:30 +0300

    added CatBoostModel [#CLICKHOUSE-3305]

--- dbms/src/Dictionaries/CatBoostModel.h
+++ dbms/src/Dictionaries/CatBoostModel.h
@@ -7,10 +7,20 @@
 namespace DB
 {
 
+struct CatBoostWrapperApi;
+class CatBoostWrapperApiProvider
+{
+public:
+    virtual ~CatBoostWrapperApiProvider() = default;
+    virtual const CatBoostWrapperApi & getApi() const = 0;
+};
+
+
 class CatBoostModel : public IExternalLoadable
 {
 public:
-    CatBoostModel(const Poco::Util::AbstractConfiguration & config, const std::string & config_prefix);
+    CatBoostModel(const Poco::Util::AbstractConfiguration & config, const std::string & config_prefix,
+                  const std::string & lib_path);
 
     const ExternalLoadableLifetime & getLifetime() const override;
 
@@ -30,10 +40,15 @@ public:
     void apply(const Columns & floatColumns, const Columns & catColumns, ColumnFloat64 & result);
 
 private:
-    ExternalLoadableLifetime lifetime;
     std::string name;
+    std::string model_path;
+    ExternalLoadableLifetime lifetime;
     std::exception_ptr creation_exception;
+    std::shared_ptr<CatBoostWrapperApiProvider> api_provider;
+    const CatBoostWrapperApi * api;
 
+    CatBoostModel(const std::string & name, const std::string & model_path,
+                  const std::string & lib_path, const ExternalLoadableLifetime & lifetime);
 };
 
 }
