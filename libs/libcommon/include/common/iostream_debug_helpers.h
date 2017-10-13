49f8080e52a5e87b036c201ecfc0b5ce33471eb4
proller <proller@github.com>
2017-10-13 20:47:59 +0300

    Small debud improvement

--- libs/libcommon/include/common/iostream_debug_helpers.h
+++ libs/libcommon/include/common/iostream_debug_helpers.h
@@ -144,7 +144,7 @@ std::ostream & operator<<(std::ostream & stream, const std::chrono::time_point<c
 template <typename T>
 std::ostream & operator<<(std::ostream & stream, const std::shared_ptr<T> & what)
 {
-    stream << "shared_ptr(use_count = " << what.use_count() << ") {";
+    stream << "shared_ptr("<< what.get() <<", use_count = " << what.use_count() << ") {";
     if (what)
         stream << *what;
     else
@@ -156,7 +156,7 @@ std::ostream & operator<<(std::ostream & stream, const std::shared_ptr<T> & what
 template <typename T>
 std::ostream & operator<<(std::ostream & stream, const std::unique_ptr<T> & what)
 {
-    stream << "unique_ptr {";
+    stream << "unique_ptr("<< what.get() <<") {";
     if (what)
         stream << *what;
     else
