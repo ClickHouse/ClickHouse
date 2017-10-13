39ff5115dd9d410c73b513ba33b231c5c14ad559
proller <proller@github.com>
2017-10-13 20:47:59 +0300

    Small debud improvement

--- dbms/src/Storages/MergeTree/MergeTreeData.cpp
+++ dbms/src/Storages/MergeTree/MergeTreeData.cpp
@@ -1554,7 +1554,7 @@ void MergeTreeData::delayInsertIfNeeded(Poco::Event * until)
     if (parts_count >= settings.parts_to_throw_insert)
     {
         ProfileEvents::increment(ProfileEvents::RejectedInserts);
-        throw Exception("Too much parts. Merges are processing significantly slower than inserts.", ErrorCodes::TOO_MUCH_PARTS);
+        throw Exception("Too much parts (" + toString(parts_count) + "). Merges are processing significantly slower than inserts.", ErrorCodes::TOO_MUCH_PARTS);
     }
 
     const size_t max_k = settings.parts_to_throw_insert - settings.parts_to_delay_insert; /// always > 0
