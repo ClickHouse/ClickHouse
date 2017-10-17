4e747d2812632f8649f87320ccd5dc87f10a69e7
Nikolai Kochetov <nik-kochetov@yandex-team.ru>
2017-10-17 13:44:46 +0300

    added ExternalModels to Context [#CLICKHOUSE-3305]
    fix build [#CLICKHOUSE-3305]
    
    fix build [#CLICKHOUSE-3305]
    
    fix build [#CLICKHOUSE-3305]
    
    fix build [#CLICKHOUSE-3305]
    
    fix build [#CLICKHOUSE-3305]

--- dbms/src/Interpreters/ExternalModels.cpp
+++ dbms/src/Interpreters/ExternalModels.cpp
@@ -1,4 +1,79 @@
-//
-// Created by nik-kochetov on 16.10.17.
-//
+#include <Interpreters/ExternalModels.h>
+#include <Interpreters/Context.h>
 
+namespace DB
+{
+
+namespace ErrorCodes
+{
+    extern const int INVALID_CONFIG_PARAMETER;
+}
+
+namespace
+{
+const ExternalLoaderUpdateSettings & getExternalModelsUpdateSettings()
+{
+    static ExternalLoaderUpdateSettings settings;
+    static std::once_flag flag;
+
+    std::call_once(flag, [] {
+        settings.check_period_sec = 5;
+        settings.backoff_initial_sec = 5;
+        /// 10 minutes
+        settings.backoff_max_sec = 10 * 60;
+    });
+
+    return settings;
+}
+
+const ExternalLoaderConfigSettings & getExternalModelsConfigSettings()
+{
+    static ExternalLoaderConfigSettings settings;
+    static std::once_flag flag;
+
+    std::call_once(flag, [] {
+        settings.external_config = "model";
+        settings.external_name = "name";
+
+        settings.path_setting_name = "models_config";
+    });
+
+    return settings;
+}
+}
+
+
+ExternalModels::ExternalModels(Context & context, bool throw_on_error)
+        : ExternalLoader(context.getConfigRef(),
+                         getExternalModelsUpdateSettings(),
+                         getExternalModelsConfigSettings(),
+                         &Logger::get("ExternalModels"),
+                         "external model"),
+          context(context)
+{
+    init(throw_on_error);
+}
+
+std::unique_ptr<IExternalLoadable> ExternalModels::create(
+        const std::string & name, const Configuration & config, const std::string & config_prefix)
+{
+    String type = config.getString(config_prefix + ".type");
+    ExternalLoadableLifetime lifetime(config, config_prefix);
+
+    /// TODO: add models factory.
+    if (type == "catboost")
+    {
+        return std::make_unique<CatBoostModel>(
+                name, config.getString(config_prefix + ".path"),
+                context.getConfigRef().getString("catboost_dynamic_library_path"),
+                lifetime, config.getUInt(config_prefix + ".float_features_count"),
+                config.getUInt(config_prefix + ".cat_features_count")
+        );
+    }
+    else
+    {
+        throw Exception("Unknown model type: " + type, ErrorCodes::INVALID_CONFIG_PARAMETER);
+    }
+}
+
+}
--- dbms/src/Interpreters/ExternalModels.h
+++ dbms/src/Interpreters/ExternalModels.h
@@ -1,8 +1,41 @@
-//
-// Created by nik-kochetov on 16.10.17.
-//
+#pragma once
 
-#ifndef CLICKHOUSE_EXTERNALMODELS_H
-#define CLICKHOUSE_EXTERNALMODELS_H
+#include <Dictionaries/CatBoostModel.h>
+#include <Interpreters/ExternalLoader.h>
+#include <common/logger_useful.h>
+#include <memory>
 
-#endif //CLICKHOUSE_EXTERNALMODELS_H
+
+namespace DB
+{
+
+class Context;
+
+/// Manages user-defined dictionaries.
+class ExternalModels : public ExternalLoader
+{
+public:
+    using ModelPtr = std::shared_ptr<IModel>;
+
+    /// Models will be loaded immediately and then will be updated in separate thread, each 'reload_period' seconds.
+    ExternalModels(Context & context, bool throw_on_error);
+
+    /// Forcibly reloads specified model.
+    void reloadModel(const std::string & name) { reload(name); }
+
+    ModelPtr getModel(const std::string & name) const
+    {
+        return std::static_pointer_cast<IModel>(getLoadable(name));
+    }
+
+protected:
+
+    std::unique_ptr<IExternalLoadable> create(const std::string & name, const Configuration & config,
+                                              const std::string & config_prefix) override;
+
+private:
+
+    Context & context;
+};
+
+}
