a01779233abf1df9bc0e723b519ac3ad5ec21b8f
proller <proller@users.noreply.github.com>
2017-10-20 20:59:44 +0300

    Cmake: fix build without downloaded submodules (#1379)

--- cmake/find_zookeeper.cmake
+++ cmake/find_zookeeper.cmake
@@ -1,5 +1,10 @@
 option (USE_INTERNAL_ZOOKEEPER_LIBRARY "Set to FALSE to use system zookeeper library instead of bundled" ${NOT_UNBUNDLED})
 
+if (NOT EXISTS "${ClickHouse_SOURCE_DIR}/contrib/zookeeper/src/c/CMakeLists.txt")
+   message (WARNING "submodule contrib/zookeeper is missing. to fix try run: \n git submodule update --init --recursive")
+   set (USE_INTERNAL_ZOOKEEPER_LIBRARY 0)
+endif ()
+
 if (NOT USE_INTERNAL_ZOOKEEPER_LIBRARY)
     find_library (ZOOKEEPER_LIBRARY zookeeper_mt)
     find_path (ZOOKEEPER_INCLUDE_DIR NAMES zookeeper/zookeeper.h PATHS ${ZOOKEEPER_INCLUDE_PATHS})
