7a70053ee00fa48949e836b62bb32558f1598ac2
proller <proller@github.com>
2017-10-21 01:19:08 +0300

    fix message

--- dbms/src/Interpreters/executeQuery.cpp
+++ dbms/src/Interpreters/executeQuery.cpp
@@ -65,12 +65,11 @@ static void logQuery(const String & query, const Context & context)
     const auto & initial_query_id = context.getClientInfo().initial_query_id;
     const auto & current_user = context.getClientInfo().current_user;
 
-    LOG_DEBUG(&Logger::get("executeQuery"), "(from " << context.getClientInfo().current_address.toString() << ") "
-    << "["
-    << (current_user != "default" ? " user=" + context.getClientInfo().current_user + "," : "")
-    << " query_id=" << current_query_id
-    << ( !initial_query_id.empty() && current_query_id != initial_query_id ? ", initial_query_id=" + initial_query_id : std::string())
-    << " ] "
+    LOG_DEBUG(&Logger::get("executeQuery"), "(from " << context.getClientInfo().current_address.toString()
+    << (current_user != "default" ? ", user: " + context.getClientInfo().current_user : "")
+    << ", query_id: " << current_query_id
+    << ( !initial_query_id.empty() && current_query_id != initial_query_id ? ", initial_query_id: " + initial_query_id : std::string())
+    << ") "
     << joinLines(query)
     );
 }
