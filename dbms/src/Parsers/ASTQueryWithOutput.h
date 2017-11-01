1d01d228a47ae9f1c4c96f2301d2f496b836ab5a
Nikolai Kochetov <nik-kochetov@yandex-team.ru>
2017-11-01 17:34:05 +0300

    removed macro DEFINE_AST_QUERY_WITH_TABLE_AND_OUTPUT
    supported table_functions in desctibe table query [#CLICKHOUSE-3397]

--- dbms/src/Parsers/ASTQueryWithOutput.h
+++ dbms/src/Parsers/ASTQueryWithOutput.h
@@ -17,7 +17,7 @@ public:
     ASTQueryWithOutput() = default;
     explicit ASTQueryWithOutput(const StringRange range_) : IAST(range_) {}
 
-    void formatImpl(const FormatSettings & s, FormatState & state, FormatStateStacked frame) const override final;
+    void formatImpl(const FormatSettings & s, FormatState & state, FormatStateStacked frame) const final;
 
 protected:
     /// NOTE: call this helper at the end of the clone() method of descendant class.
@@ -28,28 +28,28 @@ protected:
 };
 
 
-/// Declares the class-successor of ASTQueryWithOutput with implemented methods getID and clone.
-#define DEFINE_AST_QUERY_WITH_OUTPUT(Name, ID, Query) \
-class Name : public ASTQueryWithOutput \
-{ \
-public: \
-    Name() {} \
-    Name(StringRange range_) : ASTQueryWithOutput(range_) {} \
-    String getID() const override { return ID; }; \
-    \
-    ASTPtr clone() const override \
-    { \
-        std::shared_ptr<Name> res = std::make_shared<Name>(*this); \
-        res->children.clear(); \
-        cloneOutputOptions(*res); \
-        return res; \
-    } \
-\
-protected: \
-    void formatQueryImpl(const FormatSettings & settings, FormatState & state, FormatStateStacked frame) const override \
-    { \
-        settings.ostr << (settings.hilite ? hilite_keyword : "") << Query << (settings.hilite ? hilite_none : ""); \
-    } \
+template <typename AstIDAndQueryNames>
+class ASTQueryWithOutputImpl : public ASTQueryWithOutput
+{
+public:
+    explicit ASTQueryWithOutputImpl() = default;
+    explicit ASTQueryWithOutputImpl(StringRange range_) : ASTQueryWithOutput(range_) {}
+    String getID() const override { return AstIDAndQueryNames::ID; };
+
+    ASTPtr clone() const override
+    {
+        auto res = std::make_shared<ASTQueryWithOutputImpl<AstIDAndQueryNames>>(*this);
+        res->children.clear();
+        cloneOutputOptions(*res);
+        return res;
+    }
+
+protected:
+    void formatQueryImpl(const FormatSettings & settings, FormatState & state, FormatStateStacked frame) const override
+    {
+        settings.ostr << (settings.hilite ? hilite_keyword : "")
+                      << AstIDAndQueryNames::Query << (settings.hilite ? hilite_none : "");
+    }
 };
 
 }
