2d1702f8e67bf3de847c86708287290b7e35fa0e
Alexey Milovidov <milovidov@yandex-team.ru>
2017-11-06 03:21:35 +0300

    Added test for enhanced time zones support [#CLICKHOUSE-3402].

--- dbms/src/Functions/FunctionsDateTime.cpp
+++ dbms/src/Functions/FunctionsDateTime.cpp
@@ -20,6 +20,7 @@ static std::string extractTimeZoneNameFromColumn(const IColumn & column)
 
 std::string extractTimeZoneNameFromFunctionArguments(const ColumnsWithTypeAndName & arguments, size_t time_zone_arg_num)
 {
+    /// Explicit time zone may be passed in last argument.
     if (arguments.size() == time_zone_arg_num + 1)
     {
         return extractTimeZoneNameFromColumn(*arguments[time_zone_arg_num].column);
