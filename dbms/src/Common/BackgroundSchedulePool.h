263e26241370892158daf60998f96010cac6ef26
Alexey Milovidov <milovidov@yandex-team.ru>
2017-11-17 22:43:25 +0300

    Style [#CLICKHOUSE-2].

--- dbms/src/Common/BackgroundSchedulePool.h
+++ dbms/src/Common/BackgroundSchedulePool.h
@@ -1,7 +1,7 @@
 #pragma once
 
-#include "Poco/Notification.h"
-#include "Poco/NotificationQueue.h"
+#include <Poco/Notification.h>
+#include <Poco/NotificationQueue.h>
 #include <Poco/Timestamp.h>
 
 #include <thread>
@@ -10,10 +10,8 @@
 #include <vector>
 #include <map>
 #include <functional>
+#include <boost/noncopyable.hpp>
 
-#define DISABLE_COPY_AND_ASSIGN(C) \
-    C(C const&) = delete; \
-    C& operator=(C const&) = delete;
 
 namespace DB
 {
@@ -23,25 +21,22 @@ class TaskNotification;
 class BackgroundSchedulePool
 {
 public:
-
     class TaskInfo;
     using TaskHandle = std::shared_ptr<TaskInfo>;
+    using Tasks = std::multimap<Poco::Timestamp, TaskHandle>;
     using Task = std::function<void()>;
 
-    class TaskInfo : public std::enable_shared_from_this<TaskInfo>
+    class TaskInfo : public std::enable_shared_from_this<TaskInfo>, private boost::noncopyable
     {
     public:
-
-        TaskInfo(BackgroundSchedulePool & pool, const std::string& name, const Task & function);
-        DISABLE_COPY_AND_ASSIGN(TaskInfo);
+        TaskInfo(BackgroundSchedulePool & pool, const std::string & name, const Task & function);
 
         bool schedule();
         bool scheduleAfter(size_t ms);
-        bool removed() const {return removed_;}
+        bool removed() const { return removed_; }
         bool pause(bool value);
 
     private:
-
         using Guard = std::lock_guard<std::recursive_mutex>;
 
         friend class TaskNotification;
@@ -51,38 +46,35 @@ public:
         void execute();
 
         std::recursive_mutex lock_;
-        std::atomic<bool> removed_ = {false};
+        std::atomic<bool> removed_ {false};
         std::string name_;
         bool scheduled_ = false;
         BackgroundSchedulePool & pool_;
         Task function_;
-        std::multimap<Poco::Timestamp, std::shared_ptr<TaskInfo>>::iterator iterator_;
+        Tasks::iterator iterator_;
     };
 
     BackgroundSchedulePool(size_t size);
     ~BackgroundSchedulePool();
-    DISABLE_COPY_AND_ASSIGN(BackgroundSchedulePool);
 
     TaskHandle addTask(const std::string & name, const Task & task);
     void removeTask(const TaskHandle & task);
     size_t getNumberOfThreads() const { return size_;}
 
 private:
-
-    using Tasks = std::multimap<Poco::Timestamp, TaskHandle>;
     using Threads = std::vector<std::thread>;
 
     void threadFunction();
     void delayExecutionThreadFunction();
-    void scheduleDelayedTask(const TaskHandle& task, size_t ms);
-    void cancelDelayedTask(const TaskHandle& task);
+    void scheduleDelayedTask(const TaskHandle & task, size_t ms);
+    void cancelDelayedTask(const TaskHandle & task);
 
     const size_t size_;
     std::atomic<bool> shutdown_ {false};
     Threads threads_;
     Poco::NotificationQueue queue_;
 
-    //delayed notifications
+    // delayed notifications
 
     std::condition_variable wake_event_;
     std::mutex delayed_tasks_lock_;
