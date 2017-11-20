500933b5c1c8e0eab723a51a64fb658fff7aba65
Alexey Milovidov <milovidov@yandex-team.ru>
2017-11-20 07:22:21 +0300

    Removed useless code [#CLICKHOUSE-2].

--- dbms/src/Common/StringView.h
+++ dbms/src/Common/StringView.h
@@ -3,10 +3,3 @@
 #include <string_view>
 
 using StringView = std::string_view;
-
-/// It creates StringView from literal constant at compile time.
-template <typename TChar, size_t size>
-constexpr inline std::basic_string_view<TChar> makeStringView(const TChar (&str)[size])
-{
-    return std::basic_string_view<TChar>(str, size - 1);
-}
