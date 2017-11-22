571f92c7f6d965278f3ab38a5c8ee3ddeaa6f514
proller <proller@github.com>
2017-11-22 17:03:09 +0300

    Freebsd: zlib: dont use optimizations (broken asm)

--- cmake/find_zlib.cmake
+++ cmake/find_zlib.cmake
@@ -8,6 +8,9 @@ if (NOT ZLIB_FOUND)
     set (USE_INTERNAL_ZLIB_LIBRARY 1)
     set (ZLIB_COMPAT 1) # for zlib-ng, also enables WITH_GZFILEOP
     set (WITH_NATIVE_INSTRUCTIONS ${ARCHNATIVE})
+    if (CMAKE_SYSTEM MATCHES "FreeBSD")
+        set (WITH_OPTIM 0 CACHE INTERNAL "") # Bug in assembler
+    endif ()
     set (ZLIB_INCLUDE_DIR "${ClickHouse_SOURCE_DIR}/contrib/libzlib-ng" "${ClickHouse_BINARY_DIR}/contrib/libzlib-ng") # generated zconf.h
     set (ZLIB_INCLUDE_DIRS ${ZLIB_INCLUDE_DIR}) # for poco
     set (ZLIB_FOUND 1) # for poco
