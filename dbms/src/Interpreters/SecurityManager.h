cf6dd4e73bfce2711f847cbb9f123233a0d336f3
Roman Lipovsky <roman.lipovsky@gmail.com>
2017-11-28 00:31:13 +0300

    abstractions: runtime components factory + security manager

---
+++ dbms/src/Interpreters/SecurityManager.h
@@ -0,0 +1,29 @@
+#pragma once
+
+#include <Interpreters/ISecurityManager.h>
+
+#include <map>
+
+namespace DB
+{
+
+class SecurityManager : public ISecurityManager
+{
+private:
+    using Container = std::map<String, User>;
+    Container users;
+
+public:
+    void loadFromConfig(Poco::Util::AbstractConfiguration & config) override;
+
+    const User & authorizeAndGetUser(
+        const String & user_name,
+        const String & password,
+        const Poco::Net::IPAddress & address) const override;
+
+    const User & getUser(const String & user_name) override;
+
+    bool hasAccessToDatabase(const String & user_name, const String & database_name) const override;
+};
+
+}
