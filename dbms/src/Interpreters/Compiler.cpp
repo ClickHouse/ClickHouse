9b7f75940a852273e46d1e4559a87bb9e9756668
Vitaliy Lyudvichenko <vludv@yandex-team.ru>
2017-11-30 16:34:07 +0300

    Added missing include dirs for dynamic compilation. [#CLICKHOUSE-2]

--- dbms/src/Interpreters/Compiler.cpp
+++ dbms/src/Interpreters/Compiler.cpp
@@ -215,7 +215,7 @@ void Compiler::compile(
 
     std::stringstream command;
 
-    /// Slightly unconvenient.
+    /// Slightly inconvenient.
     command <<
         "LD_LIBRARY_PATH=" PATH_SHARE "/clickhouse/bin/"
         " " INTERNAL_COMPILER_EXECUTABLE
@@ -224,6 +224,7 @@ void Compiler::compile(
 #if INTERNAL_COMPILER_CUSTOM_ROOT
         " -isystem " INTERNAL_COMPILER_HEADERS_ROOT "/usr/local/include/"
         " -isystem " INTERNAL_COMPILER_HEADERS_ROOT "/usr/include/"
+        " -isystem " INTERNAL_COMPILER_HEADERS_ROOT "/usr/local/include/c++/*/"
         " -isystem " INTERNAL_COMPILER_HEADERS_ROOT "/usr/include/c++/*/"
         " -isystem " INTERNAL_COMPILER_HEADERS_ROOT "/usr/include/x86_64-linux-gnu/"
         " -isystem " INTERNAL_COMPILER_HEADERS_ROOT "/usr/include/x86_64-linux-gnu/c++/*/"
