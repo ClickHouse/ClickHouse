6fb18a7dc29cdcc10d1a464c4f368edd7727c9ac
Alexey Milovidov <milovidov@yandex-team.ru>
2017-12-01 17:15:38 +0300

    Added comments [#CLICKHOUSE-2838].

--- dbms/src/Storages/MergeTree/MergeTreePartChecker.cpp
+++ dbms/src/Storages/MergeTree/MergeTreePartChecker.cpp
@@ -74,7 +74,6 @@ public:
     size_t read(size_t rows)
     {
         ColumnPtr column = type->createColumn();
-// TODO        type->deserializeBinaryBulk(*column, uncompressed_hashing_buf, rows, 0);
         return column->size();
     }
 
