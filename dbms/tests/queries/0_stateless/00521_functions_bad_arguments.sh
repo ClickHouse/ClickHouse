e6347ac8d3127deff9ecf0093f3168af20271d55
proller <proller@github.com>
2017-12-04 19:17:49 +0300

    client ignore-error option

--- dbms/tests/queries/0_stateless/00521_functions_bad_arguments.sh
+++ dbms/tests/queries/0_stateless/00521_functions_bad_arguments.sh
@@ -7,6 +7,6 @@ CURDIR=$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)
 
 # todo: make --ignore-error function in clickhouse-client and load all queries as one .sql (or pipe)
 # todo: maybe add more strange usages
-perl -E "chomp, say qx{$CLICKHOUSE_CLIENT -q '\$_'} for map {chomp; (qq{SELECT \$_;}, qq{SELECT \$_();}, qq{SELECT \$_(NULL);}, qq{SELECT \$_([]);}, qq{SELECT \$_([NULL]);})} qx{$CLICKHOUSE_CLIENT -q 'SELECT name FROM system.functions ORDER BY name;'}" 2>&1 >/dev/null
+perl -E "say \$_ for map {chomp; (qq{SELECT \$_;}, qq{SELECT \$_();}, qq{SELECT \$_(NULL);}, qq{SELECT \$_([]);}, qq{SELECT \$_([NULL]);}, qq{SELECT \$_(-1);},)} qx{$CLICKHOUSE_CLIENT -q 'SELECT name FROM system.functions ORDER BY name;'}" | $CLICKHOUSE_CLIENT -n --ignore-error 2>&1 >/dev/null
 
-$CLICKHOUSE_CLIENT -q "Select 'Still alive'"
+$CLICKHOUSE_CLIENT -q "SELECT 'Still alive'"
