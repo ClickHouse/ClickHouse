1ba891f3fc40257775d1cb03d7cb0a393b19df69
Nikolai Kochetov <nik-kochetov@yandex-team.ru>
2017-12-11 20:29:24 +0300

    removed owned_cluster_for_insertion from StorageDistributed [#CLICKHOUSE-3397]

--- dbms/tests/queries/0_stateless/00516_insert_into_remote_table_function.sql
+++
@@ -1,6 +0,0 @@
-drop table if exists test.tab;
-create table test.tab (date Date, val UInt64) engine = MergeTree(date, (date, val), 8192);
-insert into table function remote('localhost', test.tab)  values ('2017-11-03', 42);
-insert into function remote('localhost', test.tab)  values ('2017-11-03', 43);
-select * from test.tab order by val;
-
