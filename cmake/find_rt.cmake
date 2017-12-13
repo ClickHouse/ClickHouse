741117a3908b41938f4ad9f214751504b4c06d0c
proller <proller@users.noreply.github.com>
2017-12-13 22:07:12 +0300

    Fix freebsd build. Little cmake fixes. (#1634)

--- cmake/find_rt.cmake
+++ cmake/find_rt.cmake
@@ -1,11 +1,11 @@
 if (APPLE)
     # lib from libs/libcommon
-    set (RT_LIBRARIES "apple_rt")
+    set (RT_LIBRARY "apple_rt")
 else ()
-    find_library (RT_LIBRARIES rt)
+    find_library (RT_LIBRARY rt)
 endif ()
 
-message(STATUS "Using rt: ${RT_LIBRARIES}")
+message(STATUS "Using rt: ${RT_LIBRARY}")
 
 function (target_link_rt_by_force TARGET)
     if (NOT APPLE)
