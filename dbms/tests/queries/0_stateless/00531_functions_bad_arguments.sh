65b5b177bbf7bafe931146d2f3046211a1357a21
proller <proller@github.com>
2017-12-15 23:22:41 +0300

    add bad queries from @filimonov

--- dbms/tests/queries/0_stateless/00531_functions_bad_arguments.sh
+++ dbms/tests/queries/0_stateless/00531_functions_bad_arguments.sh
@@ -5,6 +5,8 @@ CURDIR=$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)
 
 # Server should not crash on any function trash calls
 
+cat $CURDIR/00531_filimonov.data | $CLICKHOUSE_CLIENT -n --ignore-error >/dev/null 2>&1
+
 # todo: maybe add more strange usages
 perl -E "say \$_ for map {chomp; (qq{SELECT \$_;}, qq{SELECT \$_();}, qq{SELECT \$_(NULL);}, qq{SELECT \$_([]);}, qq{SELECT \$_([NULL]);}, qq{SELECT \$_(-1);}, qq{SELECT \$_('');},)} qx{$CLICKHOUSE_CLIENT -q 'SELECT name FROM system.functions ORDER BY name;'}" | $CLICKHOUSE_CLIENT -n --ignore-error >/dev/null 2>&1
 
