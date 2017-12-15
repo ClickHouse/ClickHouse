5f85f1d429a165f433f4bef936bba147397195b8
proller <proller@github.com>
2017-12-15 23:18:30 +0300

    client --ignore-error option (useful with --multiquery)

--- dbms/src/Server/Client.cpp
+++ dbms/src/Server/Client.cpp
@@ -545,6 +545,7 @@ private:
 
     bool process(const String & line)
     {
+        const auto ignore_error = config().getBool("ignore-error", false);
         if (config().has("multiquery"))
         {
             /// Several queries separated by ';'.
@@ -560,7 +561,22 @@ private:
                 const char * pos = begin;
                 ASTPtr ast = parseQuery(pos, end, true);
                 if (!ast)
+                {
+                    if (ignore_error)
+                    {
+                        // skip rest of bad query
+                        while (begin < end && *begin != ';')
+                        {
+                            ++begin;
+                        }
+                        // skip ';'
+                        if (begin < end)
+                          ++begin;
+
+                        continue;
+                    }
                     return true;
+                }
 
                 ASTInsertQuery * insert = typeid_cast<ASTInsertQuery *>(&*ast);
 
@@ -578,10 +594,17 @@ private:
                 while (isWhitespace(*begin) || *begin == ';')
                     ++begin;
 
-                if (!processSingleQuery(query, ast))
-                    return false;
+                try {
+                    if (!processSingleQuery(query, ast) && !ignore_error)
+                        return false;
+                }
+                catch (...)
+                {
+                    std::cerr << "Error on processing query: " << query << std::endl << getCurrentExceptionMessage(true);
+                    got_exception = true;
+                }
 
-                if (got_exception)
+                if (got_exception && !ignore_error)
                 {
                     if (is_interactive)
                         break;
@@ -749,7 +772,9 @@ private:
         ParserQuery parser(end);
         ASTPtr res;
 
-        if (is_interactive)
+        const auto ignore_error = config().getBool("ignore-error", false);
+
+        if (is_interactive || ignore_error)
         {
             String message;
             res = tryParseQuery(parser, pos, end, message, true, "", allow_multi_statements);
@@ -1267,6 +1292,7 @@ public:
             ("pager", boost::program_options::value<std::string>(), "pager")
             ("multiline,m", "multiline")
             ("multiquery,n", "multiquery")
+            ("ignore-error", "Do not stop processing in multiquery mode")
             ("format,f", boost::program_options::value<std::string>(), "default output format")
             ("vertical,E", "vertical output format, same as --format=Vertical or FORMAT Vertical or \\G at end of command")
             ("time,t", "print query execution time to stderr in non-interactive mode (for benchmarks)")
@@ -1372,6 +1398,8 @@ public:
             config().setBool("multiline", true);
         if (options.count("multiquery"))
             config().setBool("multiquery", true);
+        if (options.count("ignore-error"))
+            config().setBool("ignore-error", true);
         if (options.count("format"))
             config().setString("format", options["format"].as<std::string>());
         if (options.count("vertical"))
