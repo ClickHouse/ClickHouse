c6e959d1e9966c4aaa24600cdc536d3502874527
Marek Vavruša <mvavrusa@cloudflare.com>
2017-12-15 19:45:11 -0600

    StorageKafka: extended configuration, parallel consumers, offset tracking
    This contains many fixes and corrections for the Kafka engine.
    Most notably it now supports extended configuration similarly to GraphiteMergeTree.
    Now it also allows specification of consumer count to parallelize consumption of
    multiple partitions both in materialized views and in SELECT queries.
    
    The offsets are now committed in the insertSuffix() method after all rows
    are successfully read. If an exception is thrown during reading, affected consumer
    unsubscribes from all assignments and rejoins the consumer group to rewind offsets.
    This means that the consumer won't lose messages in case of write failures.

--- dbms/src/DataStreams/CapnProtoRowInputStream.h
+++ dbms/src/DataStreams/CapnProtoRowInputStream.h
@@ -29,7 +29,7 @@ public:
     using NestedFieldList = std::vector<NestedField>;
 
     /** schema_dir  - base path for schema files
-      * schema_file - location of the capnproto schema, e.g. "schema.canpn"
+      * schema_file - location of the capnproto schema, e.g. "schema.capnp"
       * root_object - name to the root object, e.g. "Message"
       */
     CapnProtoRowInputStream(ReadBuffer & istr_, const Block & header_, const String & schema_dir, const String & schema_file, const String & root_object);
