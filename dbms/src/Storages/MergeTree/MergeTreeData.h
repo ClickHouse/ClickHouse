5000c6496b29fa9c00a83f9f0b98cba7761b3ebc
Alexey Zatelepin <ztlpn@yandex-team.ru>
2017-12-19 17:55:50 +0300

    check PreCommitted parts when executing log entries and in PartChecker [#CLICKHOUSE-3508]

--- dbms/src/Storages/MergeTree/MergeTreeData.h
+++ dbms/src/Storages/MergeTree/MergeTreeData.h
@@ -349,7 +349,7 @@ public:
     DataParts getDataParts() const;
     DataPartsVector getDataPartsVector() const;
 
-    /// Returns an comitted part with the given name or a part containing it. If there is no such part, returns nullptr.
+    /// Returns a committed part with the given name or a part containing it. If there is no such part, returns nullptr.
     DataPartPtr getActiveContainingPart(const String & part_name);
 
     /// Returns the part with the given name and state or nullptr if no such part.
