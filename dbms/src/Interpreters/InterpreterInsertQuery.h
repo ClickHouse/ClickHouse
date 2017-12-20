8fc62b040c0e6f520dd425899edd8d87594c97e0
Kirill Shvakov <shvakov@gmail.com>
2017-12-20 09:39:52 +0200

    Access to TEMPORARY TABLES for read-only users

--- dbms/src/Interpreters/InterpreterInsertQuery.h
+++ dbms/src/Interpreters/InterpreterInsertQuery.h
@@ -4,7 +4,7 @@
 #include <DataStreams/BlockIO.h>
 #include <Interpreters/Context.h>
 #include <Interpreters/IInterpreter.h>
-
+#include <Parsers/ASTInsertQuery.h>
 
 namespace DB
 {
@@ -28,6 +28,8 @@ private:
     StoragePtr getTable();
 
     Block getSampleBlock();
+
+    void checkAccess(const ASTInsertQuery & query);
 
     ASTPtr query_ptr;
     const Context & context;
