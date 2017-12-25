12e29debc7ab32aec89bb86e1e728bfab048d534
proller <proller@github.com>
2017-12-25 20:16:29 +0300

    Fix 32bit build

--- dbms/src/Storages/MergeTree/MergeTreeDataSelectExecutor.cpp
+++ dbms/src/Storages/MergeTree/MergeTreeDataSelectExecutor.cpp
@@ -13,7 +13,7 @@
 
 /// Allow to use __uint128_t as a template parameter for boost::rational.
 // https://stackoverflow.com/questions/41198673/uint128-t-not-working-with-clang-and-libstdc
-#if !defined(__GLIBCXX_BITSIZE_INT_N_0)
+#if !defined(__GLIBCXX_BITSIZE_INT_N_0) && defined(__SIZEOF_INT128__)
 namespace std
 {
     template <>
