568d56ded0d939197504f2c0125f4be8571a8284
Kirill Shvakov <shvakov@gmail.com>
2017-12-25 11:31:58 +0200

    add test toStartOfFifteenMinutes

---
+++ dbms/tests/queries/0_stateless/00541_to_start_of_fifteen_minutes.sql
@@ -0,0 +1,8 @@
+SELECT 
+    DISTINCT result 
+FROM (
+    SELECT 
+        toStartOfFifteenMinutes(toDateTime('2017-12-25 00:00:00') + number * 60) AS result
+    FROM system.numbers
+    LIMIT 120
+) ORDER BY result
