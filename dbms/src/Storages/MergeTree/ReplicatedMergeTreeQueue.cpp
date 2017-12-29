b24a4b2111a42d0d661f9438e265534d440b007f
Silviu Caragea <silviu.caragea@woowteam.com>
2017-12-30 00:32:04 +0200

    Add back the buggy BackgroundSchedulePool

--- dbms/src/Storages/MergeTree/ReplicatedMergeTreeQueue.cpp
+++ dbms/src/Storages/MergeTree/ReplicatedMergeTreeQueue.cpp
@@ -252,7 +252,7 @@ bool ReplicatedMergeTreeQueue::remove(zkutil::ZooKeeperPtr zookeeper, const Stri
 }
 
 
-bool ReplicatedMergeTreeQueue::pullLogsToQueue(zkutil::ZooKeeperPtr zookeeper, zkutil::EventPtr next_update_event)
+bool ReplicatedMergeTreeQueue::pullLogsToQueue(zkutil::ZooKeeperPtr zookeeper, BackgroundSchedulePool::TaskHandle next_update_event)
 {
     std::lock_guard<std::mutex> lock(pull_logs_to_queue_mutex);
 
@@ -403,7 +403,7 @@ bool ReplicatedMergeTreeQueue::pullLogsToQueue(zkutil::ZooKeeperPtr zookeeper, z
     if (next_update_event)
     {
         if (zookeeper->exists(zookeeper_path + "/log/log-" + padIndex(index), nullptr, next_update_event))
-            next_update_event->set();
+            next_update_event->schedule();
     }
 
     return dirty_entries_loaded || !log_entries.empty();
