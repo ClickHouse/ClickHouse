c4687b5fb99009ca6d07ff93aa0fc1c39ca354af
Alexey Milovidov <milovidov@yandex-team.ru>
2018-01-02 08:54:34 +0300

    Unification of Nested: development [#CLICKHOUSE-2].

--- dbms/src/DataTypes/NestedUtils.h
+++ dbms/src/DataTypes/NestedUtils.h
@@ -10,10 +10,10 @@ namespace Nested
 {
     std::string concatenateName(const std::string & nested_table_name, const std::string & nested_field_name);
 
+    std::pair<std::string, std::string> splitName(const std::string & name);
+
     /// Returns the prefix of the name to the first '.'. Or the name is unchanged if there is no dot.
     std::string extractTableName(const std::string & nested_name);
-    /// Returns the name suffix after the first dot on the right '.'. Or the name is unchanged if there is no dot.
-    std::string extractElementName(const std::string & nested_name);
 
     /// Replace Array(Tuple(...)) columns to a multiple of Array columns in a form of `column_name.element_name`.
     NamesAndTypesList flatten(const NamesAndTypesList & names_and_types);
