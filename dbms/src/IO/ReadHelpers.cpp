043a9dd66b9713a96c8076b11551411fa94f4c79
Alexey Milovidov <milovidov@yandex-team.ru>
2018-01-13 07:43:10 +0300

    Added all methods #1665

--- dbms/src/IO/ReadHelpers.cpp
+++ dbms/src/IO/ReadHelpers.cpp
@@ -819,45 +819,6 @@ void readAndThrowException(ReadBuffer & buf, const String & additional_message)
 }
 
 
-/** Must successfully parse inf, INF and Infinity.
-  * All other variants in different cases are also parsed for simplicity.
-  */
-bool parseInfinity(ReadBuffer & buf)
-{
-    if (!checkStringCaseInsensitive("inf", buf))
-        return false;
-
-    /// Just inf.
-    if (buf.eof() || !isWordCharASCII(*buf.position()))
-        return true;
-
-    /// If word characters after inf, it should be infinity.
-    return checkStringCaseInsensitive("inity", buf);
-}
-
-
-/** Must successfully parse nan, NAN and NaN.
-  * All other variants in different cases are also parsed for simplicity.
-  */
-bool parseNaN(ReadBuffer & buf)
-{
-    return checkStringCaseInsensitive("nan", buf);
-}
-
-
-void assertInfinity(ReadBuffer & buf)
-{
-    if (!parseInfinity(buf))
-        throw Exception("Cannot parse infinity.", ErrorCodes::CANNOT_PARSE_INPUT_ASSERTION_FAILED);
-}
-
-void assertNaN(ReadBuffer & buf)
-{
-    if (!parseNaN(buf))
-        throw Exception("Cannot parse NaN.", ErrorCodes::CANNOT_PARSE_INPUT_ASSERTION_FAILED);
-}
-
-
 void skipToNextLineOrEOF(ReadBuffer & buf)
 {
     while (!buf.eof())
