ed5d1ef255d01edab6115c2a78ab22c0df28856d
Alexey Milovidov <milovidov@yandex-team.ru>
2018-01-14 05:01:48 +0300

    Added another method #1665

--- dbms/src/IO/tests/read_float_perf.cpp
+++ dbms/src/IO/tests/read_float_perf.cpp
@@ -36,8 +36,112 @@ $ for i in {1..10}; do echo $i; time ./read_float_perf < numbers$i.tsv; done
 
   */
 
-
 using namespace DB;
+
+
+template <typename T, typename ReturnType>
+ReturnType readFloatTextFastImpl2(T & x, ReadBuffer & in)
+{
+    static_assert(std::is_same_v<T, double> || std::is_same_v<T, float>, "Argument for readFloatTextImpl must be float or double");
+
+    static constexpr bool throw_exception = std::is_same_v<ReturnType, void>;
+
+    bool negative = false;
+    UInt64 digits = 0;
+
+    constexpr int max_significant_digits = std::numeric_limits<UInt64>::digits10;
+
+    size_t num_significant_digits = 0;
+    ssize_t position_of_point = -1;
+    bool in_leading_zeros = true;
+    bool in_exponent = false;
+    bool exponent_is_negative = false;
+    int exponent = 0;
+    size_t position_after_digits = 0;
+
+    while (!in.eof())
+    {
+        if ((*in.position() & 0xF0) == 0x30)
+        {
+            if (unlikely(in_exponent))
+            {
+                auto digit = *in.position() & 0x0F;
+
+                exponent *= 10;
+                exponent += digit;
+            }
+            else if (num_significant_digits < max_significant_digits)
+            {
+                auto digit = *in.position() & 0x0F;
+
+                digits *= 10;
+                digits += digit;
+
+                if (digit)
+                    in_leading_zeros = false;
+                if (!in_leading_zeros)
+                    ++num_significant_digits;
+            }
+        }
+        else if (*in.position() == '-')
+        {
+            if (unlikely(in_exponent))
+                exponent_is_negative = true;
+            else
+                negative = true;
+        }
+        else if (*in.position() == '.')
+        {
+            position_of_point = in.count();
+        }
+        else if (*in.position() == 'e' || *in.position() == 'E')
+        {
+            in_exponent = true;
+            if (!position_after_digits)
+                position_after_digits = in.count();
+        }
+        else if (*in.position() == 'i' || *in.position() == 'I')
+        {
+            if (assertOrParseInfinity<throw_exception>(in))
+            {
+                x = std::numeric_limits<T>::infinity();
+                if (negative)
+                    x = -x;
+                return ReturnType(true);
+            }
+            return ReturnType(false);
+        }
+        else if (*in.position() == 'n' || *in.position() == 'N')
+        {
+            if (assertOrParseNaN<throw_exception>(in))
+            {
+                x = std::numeric_limits<T>::quiet_NaN();
+                if (negative)
+                    x = -x;
+                return ReturnType(true);
+            }
+            return ReturnType(false);
+        }
+        else
+        {
+            if (!position_after_digits)
+                position_after_digits = in.count();
+            break;
+        }
+
+        ++in.position();
+    }
+
+    if (exponent_is_negative)
+        exponent = -exponent;
+
+    if (position_of_point != -1)
+        exponent -= position_after_digits - position_of_point - 1;
+
+    x = shift10(digits, exponent);
+
+    return ReturnType(true);
+}
 
 
 template <typename T, void F(T&, ReadBuffer&)>
@@ -75,6 +179,7 @@ try
     if (method == 1) loop<T, readFloatTextPrecise>(in, out);
     if (method == 2) loop<T, readFloatTextFast>(in, out);
     if (method == 3) loop<T, readFloatTextSimple>(in, out);
+    if (method == 4) loop<T, readFloatTextFastImpl2<Float64, void>>(in, out);
 
     return 0;
 }
