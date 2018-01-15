bd9bad5fa2fba51ae7d6d4bf95d8a82df69136e3
proller <proller@github.com>
2018-01-15 14:35:22 +0300

    Fxi test

--- dbms/tests/server_wrapper.sh
+++ dbms/tests/server_wrapper.sh
@@ -5,23 +5,30 @@ set -o pipefail
 
 CURDIR=$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)
 ROOTDIR=$(cd "$(dirname "${BASH_SOURCE[0]}")" && cd ../.. && pwd)
+LOGDIR=${DATADIR:=/tmp/clickhouse}
+LOGDIR=${LOGDIR:=$DATADIR/log}
+
+rm -rf $DATADIR
+
+mkdir -p $LOGDIR
 
 # Start a local clickhouse server which will be used to run tests
-PATH=$PATH:$ROOTDIR/build/dbms/src/Server \
-  $ROOTDIR/build/dbms/src/Server/clickhouse-server --config-file=$ROOTDIR/dbms/tests/server-test.xml 2>/dev/null &
+PATH=$PATH:$ROOTDIR/build${BUILD_TYPE}/dbms/src/Server \
+  $ROOTDIR/build${BUILD_TYPE}/dbms/src/Server/clickhouse-server --config-file=$ROOTDIR/dbms/tests/server-test.xml > $LOGDIR/stdout 2>&1 &
 CH_PID=$!
-#sleep 3
+sleep 3
 
 # Define needed stuff to kill test clickhouse server after tests completion
 function finish {
-  kill $CH_PID
+  kill $CH_PID || true
   wait
-  rm -rf /tmp/clickhouse
+  tail -n 50 $LOGDIR/stdout
+  rm -rf $DATADIR
 }
 trap finish EXIT
 
 # Do tests
 export CLICKHOUSE_CONFIG=${CLICKHOUSE_CONFIG:=$ROOTDIR/dbms/tests/server-test.xml}
 cd $ROOTDIR/dbms/tests
-PATH=$PATH:$ROOTDIR/build/dbms/src/Server \
-  ./clickhouse-test -c "$ROOTDIR/build/dbms/src/Server/clickhouse-client --config $ROOTDIR/dbms/tests/clickhouse-client.xml"
+PATH=$PATH:$ROOTDIR/build${BUILD_TYPE}/dbms/src/Server \
+  ./clickhouse-test -c "$ROOTDIR/build${BUILD_TYPE}/dbms/src/Server/clickhouse-client --config $ROOTDIR/dbms/tests/clickhouse-client.xml"
