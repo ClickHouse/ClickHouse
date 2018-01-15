abdbfbe856e463b6496d0c73138442e6b2f45472
Alexey Milovidov <milovidov@yandex-team.ru>
2018-01-15 08:56:50 +0300

    Experiments with LZ4 [#CLICKHOUSE-2]

--- utils/compressor/decompress_perf.cpp
+++ utils/compressor/decompress_perf.cpp
@@ -15,188 +15,12 @@
 #include <Common/Stopwatch.h>
 #include <Common/formatReadable.h>
 #include <Common/memcpySmall.h>
+#include <Common/LZ4_decompress_faster.h>
 #include <common/unaligned.h>
 
 
 /** for i in *.bin; do ./decompress_perf < $i > /dev/null; done
   */
-
-namespace LZ4
-{
-
-static void copy16(UInt8 * dst, const UInt8 * src)
-{
-    _mm_storeu_si128(reinterpret_cast<__m128i *>(dst),
-        _mm_loadu_si128(reinterpret_cast<const __m128i *>(src)));
-}
-
-static void wildCopy16(UInt8 * dst, const UInt8 * src, UInt8 * dst_end)
-{
-    do
-    {
-        copy16(dst, src);
-        dst += 16;
-        src += 16;
-    } while (dst < dst_end);
-}
-
-static void copy8(UInt8 * dst, const UInt8 * src)
-{
-    memcpy(dst, src, 8);
-}
-
-static void wildCopy8(UInt8 * dst, const UInt8 * src, UInt8 * dst_end)
-{
-    do
-    {
-        copy8(dst, src);
-        dst += 8;
-        src += 8;
-    } while (dst < dst_end);
-}
-
-
-static void copyOverlap16(UInt8 * op, const UInt8 *& match, const size_t offset)
-{
-    /// 4 % n.
-    static constexpr int shift1[]
-        = { 0,  1,  2,  1,  4,  4,  4,  4,  4,  4,  4,  4,  4,  4,  4,  4  };
-
-    /// 8 % n - 4 % n
-    static constexpr int shift2[]
-        = { 0,  0,  0,  1,  0, -1, -2, -3, -4,  4,  4,  4,  4,  4,  4,  4  };
-
-    /// 16 % n - 8 % n
-    static constexpr int shift3[]
-        = { 0,  0,  0, -1,  0, -2,  2,  1,  0, -1, -2, -3, -4, -5,  -6, -7 };
-
-    op[0] = match[0];
-    op[1] = match[1];
-    op[2] = match[2];
-    op[3] = match[3];
-
-    match += shift1[offset];
-    memcpy(op + 4, match, 4);
-    match += shift2[offset];
-    memcpy(op + 8, match, 8);
-    match += shift3[offset];
-}
-
-
-static void copyOverlap8(UInt8 * op, const UInt8 *& match, const size_t offset)
-{
-    /// 4 % n.
-    static constexpr int shift1[] = {0, 1, 2, 1, 4, 4, 4, 4};
-
-    /// 8 % n - 4 % n
-    static constexpr int shift2[] = {0, 0, 0, 1, 0, -1, -2, -3};
-
-    op[0] = match[0];
-    op[1] = match[1];
-    op[2] = match[2];
-    op[3] = match[3];
-
-    match += shift1[offset];
-    memcpy(op + 4, match, 4);
-    match += shift2[offset];
-}
-
-
-template <size_t N> void copy(UInt8 * dst, const UInt8 * src);
-template <> void copy<8>(UInt8 * dst, const UInt8 * src) { copy8(dst, src); };
-template <> void copy<16>(UInt8 * dst, const UInt8 * src) { copy16(dst, src); };
-
-template <size_t N> void wildCopy(UInt8 * dst, const UInt8 * src, UInt8 * dst_end);
-template <> void wildCopy<8>(UInt8 * dst, const UInt8 * src, UInt8 * dst_end) { wildCopy8(dst, src, dst_end); };
-template <> void wildCopy<16>(UInt8 * dst, const UInt8 * src, UInt8 * dst_end) { wildCopy16(dst, src, dst_end); };
-
-template <size_t N> void copyOverlap(UInt8 * op, const UInt8 *& match, const size_t offset);
-template <> void copyOverlap<8>(UInt8 * op, const UInt8 *& match, const size_t offset) { copyOverlap8(op, match, offset); };
-template <> void copyOverlap<16>(UInt8 * op, const UInt8 *& match, const size_t offset) { copyOverlap16(op, match, offset); };
-
-
-template <size_t copy_amount>
-void decompress(
-     const char * const __restrict source,
-     char * const __restrict dest,
-     size_t dest_size)
-{
-    const UInt8 * ip = (UInt8 *)source;
-    UInt8 * op = (UInt8 *)dest;
-    UInt8 * const output_end = op + dest_size;
-
-    while (1)
-    {
-        size_t length;
-
-        auto continue_read_length = [&]
-        {
-            unsigned s;
-            do
-            {
-                s = *ip++;
-                length += s;
-            } while (unlikely(s == 255));
-        };
-
-        /// Get literal length.
-
-        const unsigned token = *ip++;
-        length = token >> 4;
-        if (length == 0x0F)
-            continue_read_length();
-
-        /// Copy literals.
-
-        UInt8 * copy_end = op + length;
-
-        wildCopy<copy_amount>(op, ip, copy_end);
-
-        ip += length;
-        op = copy_end;
-
-        if (copy_end > output_end)
-            return;
-
-        /// Get match offset.
-
-        size_t offset = unalignedLoad<UInt16>(ip);
-        ip += 2;
-        const UInt8 * match = op - offset;
-
-        /// Get match length.
-
-        length = token & 0x0F;
-        if (length == 0x0F)
-            continue_read_length();
-        length += 4;
-
-        /// Copy match within block, that produce overlapping pattern.
-
-        copy_end = op + length;
-
-        if (unlikely(offset < copy_amount))
-        {
-            copyOverlap<copy_amount>(op, match, offset);
-        }
-        else
-        {
-            copy<copy_amount>(op, match);
-            match += copy_amount;
-        }
-
-        op += copy_amount;
-
-        copy<copy_amount>(op, match);
-        if (length > copy_amount * 2)
-            wildCopy<copy_amount>(op + copy_amount, match + copy_amount, copy_end);
-
-        op = copy_end;
-    }
-}
-
-}
-
 
 namespace DB
 {
@@ -218,7 +42,7 @@ protected:
     /// Points to memory, holding compressed block.
     char * compressed_buffer = nullptr;
 
-    LZ4Stat stat;
+    LZ4::Stat stat;
 
     size_t readCompressedData(size_t & size_decompressed, size_t & size_compressed_without_checksum)
     {
@@ -273,7 +97,7 @@ protected:
         if (method == static_cast<UInt8>(CompressionMethodByte::LZ4))
         {
             //LZ4::statistics(compressed_buffer + COMPRESSED_BLOCK_HEADER_SIZE, to, size_decompressed, stat);
-            LZ4::decompress<8>(compressed_buffer + COMPRESSED_BLOCK_HEADER_SIZE, to, size_decompressed);
+            LZ4::decompress(compressed_buffer + COMPRESSED_BLOCK_HEADER_SIZE, to, size_compressed_without_checksum, size_decompressed);
         }
         else
             throw Exception("Unknown compression method: " + toString(method), ErrorCodes::UNKNOWN_COMPRESSION_METHOD);
@@ -286,7 +110,7 @@ public:
     {
     }
 
-    LZ4Stat getStatistics() const { return stat; }
+    LZ4::Stat getStatistics() const { return stat; }
 };
 
 
