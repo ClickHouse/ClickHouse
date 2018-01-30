888cb2d3f67b4bc540aea31e19526279ea3520ae
Павел Литвиненко <digitaldistortion@ya.ru>
2018-01-30 17:05:50 +0300

    ci debug

--- docker/builder/build.sh
+++ docker/builder/build.sh
@@ -4,7 +4,7 @@ export THREADS=$(grep -c ^processor /proc/cpuinfo)
 export CC=gcc-7
 export CXX=g++-7
 
-cmake /server
-cd /server
 mkdir -p /server/build
+cmake /server
+cd /server/build
 make -j $THREADS
