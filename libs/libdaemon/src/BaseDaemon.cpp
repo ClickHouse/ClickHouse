40bf7fea5eb8bd313c6e987d9e9ef77f8b11ec7f
alexey-milovidov <milovidov@yandex-team.ru>
2018-02-08 21:59:27 +0300

    Update BaseDaemon.cpp

--- libs/libdaemon/src/BaseDaemon.cpp
+++ libs/libdaemon/src/BaseDaemon.cpp
@@ -703,7 +703,7 @@ void BaseDaemon::initialize(Application & self)
         /// Test: -- --1=1 --1=2 --3 5 7 8 -9 10 -11=12 14= 15== --16==17 --=18 --19= --20 21 22 --23 --24 25 --26 -27 28 ---29=30 -- ----31 32 --33 3-4
         Poco::AutoPtr<Poco::Util::MapConfiguration> map_config = new Poco::Util::MapConfiguration;
         std::string key;
-        for(auto &arg : argv())
+        for(auto & arg : argv())
         {
             auto key_start = arg.find_first_not_of('-');
             auto pos_minus = arg.find('-');
