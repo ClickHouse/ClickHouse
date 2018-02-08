5bfc9bc4670f0b7227d82e16c8f16ab2e054dacd
proller <proller@github.com>
2018-02-08 18:19:38 +0300

    Pbuilder ccache fixes

--- utils/travis/normal.sh
+++ utils/travis/normal.sh
@@ -20,7 +20,7 @@ date
 
 mkdir -p build
 cd build
-cmake .. -DCMAKE_CXX_COMPILER=`which $CXX` -DCMAKE_C_COMPILER=`which $CC` \
+cmake .. -DCMAKE_CXX_COMPILER=`which $DEB_CXX $CXX` -DCMAKE_C_COMPILER=`which $DEB_CC $CC` \
     `# Does not optimize to speedup build, skip debug info to use less disk` \
     -DCMAKE_C_FLAGS_ADD="-O0 -g0" -DCMAKE_CXX_FLAGS_ADD="-O0 -g0" \
     `# ignore ccache disabler on trusty` \
