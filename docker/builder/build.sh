6628a757dc44511984592b12959ca330376e9a90
proller <proller@users.noreply.github.com>
2018-02-12 18:42:27 +0300

    Docker: use ubuntu artful 17.10 for building (#1887)
    * Docker wip
    
    * no tests
    
    * fixes
    
    * No tests
    
    * Fixes
    
    * Clean

--- docker/builder/build.sh
+++ docker/builder/build.sh
@@ -1,9 +1,7 @@
 #!/bin/bash
 
-export THREADS=$(grep -c ^processor /proc/cpuinfo)
-export CC=gcc-7
-export CXX=g++-7
-
-mkdir -p /server/build
-cmake /server
-make -j $THREADS
+mkdir -p /server/build_docker
+cd /server/build_docker
+cmake /server -DUSE_EMBEDDED_COMPILER=1 -DENABLE_TESTS=0
+make -j $(nproc || grep -c ^processor /proc/cpuinfo)
+#ctest -V -j $(nproc || grep -c ^processor /proc/cpuinfo)
