7f305d5bc6ac8d28702ef1e7fcb46bd44c797b02
proller <proller@github.com>
2018-02-12 16:16:25 +0300

    Docker wip

--- docker/builder/build.sh
+++ docker/builder/build.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
 export THREADS=$(grep -c ^processor /proc/cpuinfo)
-export CC=gcc-7
-export CXX=g++-7
+#export CC=gcc-7
+#export CXX=g++-7
 
 mkdir -p /server/build
-cmake /server
+cmake /server -DUSE_EMBEDDED_COMPILER=1
 make -j $THREADS
