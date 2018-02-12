701c3848df30ec01754913148f6be3542d66b9f2
proller <proller@github.com>
2018-02-12 18:01:46 +0300

    Fixes

--- docker/builder/build.sh
+++ docker/builder/build.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
-mkdir -p /server/build
+mkdir -p /server/build_docker
+cd /server/build_docker
 cmake /server -DUSE_EMBEDDED_COMPILER=1 -DENABLE_TESTS=0
 make -j $(nproc || grep -c ^processor /proc/cpuinfo)
 #ctest -V -j $(nproc || grep -c ^processor /proc/cpuinfo)
