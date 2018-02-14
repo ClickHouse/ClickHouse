064c76a0257339f8641c83ffd9ebcf3a2af34c3f
robot-metrika-test <robot-metrika-test@yandex-team.ru>
2018-02-14 21:24:50 +0300

    Auto version update to [54352]

--- dbms/cmake/version.cmake
+++ dbms/cmake/version.cmake
@@ -1,6 +1,6 @@
 # This strings autochanged from release_lib.sh:
-set(VERSION_DESCRIBE v1.1.54351-testing)
-set(VERSION_REVISION 54351)
+set(VERSION_DESCRIBE v1.1.54352-testing)
+set(VERSION_REVISION 54352)
 # end of autochange
 
 set (VERSION_MAJOR 1)
