b82ad703caf60e994a45f6b03258942379a3e099
Ilya <is.homutov@gmail.com>
2018-02-16 18:43:21 +0300

    Prevent cron stdout
    This is bad idea to print something to stdout in cron jobs, because this messages can be sended via email

--- debian/clickhouse-server.cron.d
+++ debian/clickhouse-server.cron.d
@@ -1,1 +1,2 @@
-#*/10 * * * * root (which service && (service clickhouse-server condstart || true)) || /etc/init.d/clickhouse-server condstart 1>/dev/null 2>&1
+#*/10 * * * * root (which service > /dev/null && (service clickhouse-server condstart || true)) || /etc/init.d/clickhouse-server condstart 1>/dev/null 2>&1
+ to
