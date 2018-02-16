3d9e58550838bf7cec06bc45843f256c0974cf87
Alexey Zatelepin <ztlpn@yandex-team.ru>
2018-02-16 17:51:14 +0300

    rename musl_exp10 -> preciseExp10 for coding style compatibility

--- libs/libcommon/include/common/preciseExp10.h
+++ libs/libcommon/include/common/preciseExp10.h
@@ -4,7 +4,8 @@
   * For example, exp10(3) gives 1000.0000000000001
   *  despite the fact that 1000 is exactly representable in double and float.
   * Better to always use implementation from MUSL.
-  * Note: the musl_ prefix added to function names to avoid confusion with symbols from the system libm.
+  *
+  * Note: the function names are different to avoid confusion with symbols from the system libm.
   */
 
 #include <stdlib.h> /// for __THROW
@@ -17,9 +18,9 @@
 extern "C"
 {
 
-double musl_exp10(double x) __THROW;
-double musl_pow10(double x) __THROW;
-float musl_exp10f(float x) __THROW;
-float musl_pow10f(float x) __THROW;
+double preciseExp10(double x) __THROW;
+double precisePow10(double x) __THROW;
+float preciseExp10f(float x) __THROW;
+float precisePow10f(float x) __THROW;
 
 }
