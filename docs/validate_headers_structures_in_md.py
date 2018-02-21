eeebf86c8c17a7f04b2598b7bd11e657d2e1ff71
Ivan Blinkov <ivan@blinkov.ru>
2018-02-21 19:09:32 +0300

    This way it hurts eyes less

--- docs/validate_headers_structures_in_md.py
+++ docs/validate_headers_structures_in_md.py
@@ -1,21 +1,20 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
-
-
 # Gets all the files in SOURCES_TREE directory, shows all level headers for each file and skip or process files by user's selection.
 
-from os import walk
+import os
 
 SOURCES_TREE = 'ru'
 STOP_AT_THE_FIRST_FILE = False
 
-for (dirpath, dirnames, filenames) in walk(SOURCES_TREE):
-    for filename in filenames :
-        if filename == 'conf.py': continue
+for (dirpath, dirnames, filenames) in os.walk(SOURCES_TREE):
+    for filename in filenames:
+        if filename == 'conf.py':
+            continue
 
-        print "=== "+ dirpath+'/'+filename
+        print '=== ' + dirpath + '/' + filename
 
-        f = open(dirpath+'/'+filename)
+        f = open(dirpath + '/' + filename)
         content = f.readlines()
         f.close()
 
@@ -23,36 +22,43 @@ for (dirpath, dirnames, filenames) in walk(SOURCES_TREE):
         # Showing headers structure in md-file
         count_lines = 0
         for l in content:
-            if l.startswith('#'): print l
-            if l.startswith("==="): print content[count_lines - 1] + l
-            if l.startswith("---"): print content[count_lines - 1] + l
+            if l.startswith('#'):
+                print l
+            if l.startswith('==='):
+                print content[count_lines - 1] + l
+            if l.startswith('---'):
+                print content[count_lines - 1] + l
             count_lines += 1
 
         # At this stage user should check the headers structucture and choose what to to
         # Replace headers markup or not
-        choise = raw_input("What to do with a file (pass(s) or process(p)): ")
+        choise = raw_input('What to do with a file (pass(s) or process(p)): ')
 
-        if choise == 's': continue
+        if choise == 's':
+            continue
         else:
-            print "processing..."
+            print 'processing...'
             count_lines = 0
             for l in content:
-                if l.startswith("==="):
+                if l.startswith('==='):
                     print count_lines, content[count_lines -1], content[count_lines]
-                    content[count_lines - 1] = "# "+content[count_lines - 1]
+                    content[count_lines - 1] = '# ' + content[count_lines - 1]
                     content.pop(count_lines)
 
-                if l.startswith("---"):
+                if l.startswith('---'):
                     print count_lines, content[count_lines -1], content[count_lines]
-                    content[count_lines - 1] = "## "+content[count_lines - 1]
+                    content[count_lines - 1] = '## ' + content[count_lines - 1]
                     content.pop(count_lines)
 
                 count_lines += 1
 
-        f=open(dirpath+'/'+filename,"w")
-        for l in content: f.write(l)
+        f = open(dirpath + '/' + filename, 'w')
+        for l in content:
+            f.write(l)
         f.close()
 
-        if STOP_AT_THE_FIRST_FILE : break
+        if STOP_AT_THE_FIRST_FILE:
+            break
 
-    if STOP_AT_THE_FIRST_FILE : break
+    if STOP_AT_THE_FIRST_FILE:
+        break
