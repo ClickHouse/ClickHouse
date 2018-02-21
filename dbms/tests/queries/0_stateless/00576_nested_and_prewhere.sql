f6eb5b4b018b2b9b78244bbca5e0552f1ce572b3
Alexey Milovidov <milovidov@yandex-team.ru>
2018-02-21 04:39:46 +0300

    Enhanced test [#CLICKHOUSE-3608]

--- dbms/tests/queries/0_stateless/00576_nested_and_prewhere.sql
+++ dbms/tests/queries/0_stateless/00576_nested_and_prewhere.sql
@@ -6,4 +6,8 @@ INSERT INTO test.nested SELECT number, number % 2, range(number % 10) FROM syste
 ALTER TABLE test.nested ADD COLUMN n.b Array(UInt64);
 SELECT DISTINCT n.b FROM test.nested PREWHERE filter;
 
+ALTER TABLE test.nested ADD COLUMN n.c Array(UInt64) DEFAULT arrayMap(x -> x * 2, n.a);
+SELECT DISTINCT n.c FROM test.nested PREWHERE filter;
+SELECT DISTINCT n.a, n.c FROM test.nested PREWHERE filter;
+
 DROP TABLE test.nested;
