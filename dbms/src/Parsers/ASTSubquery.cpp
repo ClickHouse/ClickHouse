509a45cf9fa99fa02fe15e7dbe7f85613c26bfce
Alexey Milovidov <milovidov@yandex-team.ru>
2018-02-23 14:35:05 +0300

    Fixed test [#CLICKHOUSE-2]

--- dbms/src/Parsers/ASTSubquery.cpp
+++ dbms/src/Parsers/ASTSubquery.cpp
@@ -6,6 +6,10 @@ namespace DB
 
 String ASTSubquery::getColumnNameImpl() const
 {
+    /// This is a hack. We use alias, if available, because otherwise tree could change during analysis.
+    if (!alias.empty())
+        return alias;
+
     Hash hash = getTreeHash();
     return "__subquery_" + toString(hash.first) + "_" + toString(hash.second);
 }
