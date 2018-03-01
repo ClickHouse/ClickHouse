c68684804708145e3975e4cc98e39825f41a192d
proller <proller@github.com>
2018-03-01 17:17:57 +0300

    Allow use bundled *ssl library

---
+++ cmake/find_ssl.cmake
@@ -0,0 +1,42 @@
+option (USE_INTERNAL_SSL_LIBRARY "Set to FALSE to use system *ssl library instead of bundled" 0)
+
+set (OPENSSL_USE_STATIC_LIBS ${USE_STATIC_LIBRARIES})
+
+if (NOT USE_INTERNAL_SSL_LIBRARY)
+    if (APPLE)
+        set (OPENSSL_ROOT_DIR "/usr/local/opt/openssl")
+        # https://rt.openssl.org/Ticket/Display.html?user=guest&pass=guest&id=2232
+        if (USE_STATIC_LIBRARIES)
+            message(WARNING "Disable USE_STATIC_LIBRARIES if you have linking problems with OpenSSL on MacOS")
+        endif ()
+    endif ()
+    find_package (OpenSSL)
+
+    if (NOT OPENSSL_FOUND)
+        # Try to find manually.
+        set (OPENSSL_INCLUDE_PATHS "/usr/local/opt/openssl/include")
+        set (OPENSSL_PATHS "/usr/local/opt/openssl/lib")
+        find_path (OPENSSL_INCLUDE_DIR NAMES openssl/ssl.h PATHS ${OPENSSL_INCLUDE_PATHS})
+        find_library (OPENSSL_SSL_LIBRARY ssl PATHS ${OPENSSL_PATHS})
+        find_library (OPENSSL_CRYPTO_LIBRARY crypto PATHS ${OPENSSL_PATHS})
+        if (OPENSSL_SSL_LIBRARY AND OPENSSL_CRYPTO_LIBRARY AND OPENSSL_INCLUDE_DIR)
+            set (OPENSSL_LIBRARIES ${OPENSSL_SSL_LIBRARY} ${OPENSSL_CRYPTO_LIBRARY})
+            set (OPENSSL_FOUND 1)
+        endif ()
+    endif ()
+endif ()
+
+if (NOT OPENSSL_FOUND)
+    set (USE_INTERNAL_SSL_LIBRARY 1)
+    set (OPENSSL_INCLUDE_DIR "${ClickHouse_SOURCE_DIR}/contrib/ssl/include")
+    if (USE_STATIC_LIBRARIES)
+        set (OPENSSL_CRYPTO_LIBRARY crypto)
+        set (OPENSSL_SSL_LIBRARY ssl)
+    else ()
+        set (OPENSSL_CRYPTO_LIBRARY crypto-shared)
+        set (OPENSSL_SSL_LIBRARY ssl-shared)
+    endif ()
+    set (OPENSSL_LIBRARIES ${OPENSSL_SSL_LIBRARY} ${OPENSSL_CRYPTO_LIBRARY})
+endif ()
+
+message (STATUS "Using ssl=${OPENSSL_FOUND}: ${OPENSSL_INCLUDE_DIR} : ${OPENSSL_LIBRARIES}")
