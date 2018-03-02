ac13a7e372fd95a53e09db915595327b57e8abbe
Nikolai Kochetov <nik-kochetov@yandex-team.ru>
2018-03-02 15:59:41 +0300

    added odbc_default_field_size setting

--- dbms/src/Dictionaries/ODBCDictionarySource.h
+++ dbms/src/Dictionaries/ODBCDictionarySource.h
@@ -31,7 +31,7 @@ class ODBCDictionarySource final : public IDictionarySource
 public:
     ODBCDictionarySource(const DictionaryStructure & dict_struct_,
         const Poco::Util::AbstractConfiguration & config, const std::string & config_prefix,
-        const Block & sample_block);
+        const Block & sample_block, const Context & context);
 
     /// copy-constructor is provided in order to support cloneability
     ODBCDictionarySource(const ODBCDictionarySource & other);
