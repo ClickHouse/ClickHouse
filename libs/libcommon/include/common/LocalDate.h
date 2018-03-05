d3addad906ab4ec47b6c213ea20fe81aa51bece7
proller <proller@github.com>
2018-03-05 22:10:15 +0300

    fix

--- libs/libcommon/include/common/LocalDate.h
+++ libs/libcommon/include/common/LocalDate.h
@@ -18,7 +18,11 @@
   *
   * packed - for memcmp to work naturally (but because m_year is 2 bytes, on little endian, comparison is correct only before year 2047)
   */
-class __attribute__ ((__packed__)) LocalDate
+class
+#if !_MSC_VER
+    __attribute__ ((__packed__))
+#endif
+    LocalDate
 {
 private:
     unsigned short m_year;
