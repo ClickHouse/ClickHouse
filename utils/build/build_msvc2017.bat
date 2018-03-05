aaafdac9c5bd4340f1a61c2b5f07babd444b9980
proller <proller@github.com>
2018-03-05 04:41:17 -0800

    fix

--- utils/build/build_msvc2017.bat
+++ utils/build/build_msvc2017.bat
@@ -9,6 +9,6 @@ cd build
 
 :: Stage 1: try build client
 cmake .. -G "Visual Studio 15 2017 Win64" -DENABLE_CLICKHOUSE_ALL=0 -DENABLE_CLICKHOUSE_CLIENT=1 > cmake.log
-cmake --build . --target clickhouse > build.log
+cmake --build . --target clickhouse -- /m > build.log
 :: Stage 2: try build minimal server
 :: Stage 3: enable all possible features (ssl, ...)
