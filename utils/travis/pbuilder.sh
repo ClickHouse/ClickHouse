372be288100d318afea884d8f6b0f5f8dcf4e60f
proller <proller@github.com>
2018-03-15 00:15:30 +0300

    Test fixes, ./release now uses pbuilder by default

--- utils/travis/pbuilder.sh
+++ utils/travis/pbuilder.sh
@@ -1,5 +1,7 @@
 #!/bin/sh
 
+CUR_DIR=$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)
+
 # env CXX=clang++-5.0 CC=clang-5.0 DH_VERBOSE=1 utils/travis/pbuilder.sh
 
 set -e
@@ -14,6 +16,7 @@ env TEST_RUN=${TEST_RUN=1} \
     `# 00281 requires internal compiler` \
     `# 00416 requires patched poco from contrib/` \
     TEST_OPT="--skip long pocopatch $TEST_OPT" \
+    TEST_SSL="" `# <Error> Application: SSL context exception: Error loading certificate from file /etc/clickhouse-server/server.crt: No error -- when using system poco on artful` \
     TEST_TRUE=${TEST_TRUE=false} \
     `# travisci will not upload ccache cache after timeout (48min), use our less timeout` \
     PBUILDER_OPT="--timeout ${PBUILDER_TIMEOUT:=35m} $PBUILDER_OPT" \
@@ -25,6 +28,6 @@ env TEST_RUN=${TEST_RUN=1} \
     `# Use all possible contrib libs from system` \
     `# psmisc - killall` \
     EXTRAPACKAGES="psmisc clang-5.0 lld-5.0 liblld-5.0-dev libclang-5.0-dev liblld-5.0 libc++abi-dev libc++-dev libboost-program-options-dev libboost-system-dev libboost-filesystem-dev libboost-thread-dev zlib1g-dev liblz4-dev libdouble-conversion-dev libzookeeper-mt-dev libsparsehash-dev librdkafka-dev libpoco-dev libsparsehash-dev libgoogle-perftools-dev libzstd-dev libre2-dev $EXTRAPACKAGES" \
-    ./release --pbuilder $RELEASE_OPT
+    $CUR_DIR/../../release $RELEASE_OPT
 
 date
