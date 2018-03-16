9784921e07181244c94853c69b2d131ec39cc568
Kirill Shvakov <shvakov@gmail.com>
2018-03-16 11:39:00 +0200

    Test improvement

--- dbms/tests/queries/0_stateless/00607_materialized_view_partition.sql
+++ dbms/tests/queries/0_stateless/00607_materialized_view_partition.sql
@@ -3,17 +3,22 @@ DROP TABLE IF EXISTS v_dummy;
 CREATE TABLE dummy (date Date, value String) Engine = MergeTree(date, (date), 8192);
 CREATE MATERIALIZED VIEW v_dummy Engine = MergeTree(date, (date), 8192) AS SELECT date, value FROM dummy;
 
+SELECT '## Add 2 partitions';
 INSERT INTO dummy VALUES ('2018-02-14', 'A'), ('2018-03-14', 'B');
-
-SELECT DISTINCT partition FROM system.parts WHERE table = '.inner.v_dummy' AND active;
+SELECT DISTINCT partition FROM system.parts WHERE table = '.inner.v_dummy' AND active ORDER BY partition;
+SELECT '## Drop partition 201802';
 ALTER TABLE v_dummy DROP PARTITION 201802;
-SELECT DISTINCT partition FROM system.parts WHERE table = '.inner.v_dummy' AND active;
+SELECT DISTINCT partition FROM system.parts WHERE table = '.inner.v_dummy' AND active ORDER BY partition;
+SELECT '## Drop partition 201803';
 ALTER TABLE v_dummy DROP PARTITION 201803;
-SELECT DISTINCT partition FROM system.parts WHERE table = '.inner.v_dummy' AND active;
+SELECT DISTINCT partition FROM system.parts WHERE table = '.inner.v_dummy' AND active ORDER BY partition;
 
+SELECT '## Add 3 partitions';
 INSERT INTO dummy VALUES ('2018-01-14', 'A'), ('2018-02-14', 'B'), ('2018-03-14', 'C');
-SELECT DISTINCT partition FROM system.parts WHERE table = '.inner.v_dummy' AND active;
+SELECT DISTINCT partition FROM system.parts WHERE table = '.inner.v_dummy' AND active ORDER BY partition;
+SELECT '## Detach partition 201803';
 ALTER TABLE v_dummy DETACH PARTITION 201803;
-SELECT DISTINCT partition FROM system.parts WHERE table = '.inner.v_dummy' AND active;
+SELECT DISTINCT partition FROM system.parts WHERE table = '.inner.v_dummy' AND active ORDER BY partition;
+SELECT '## Attach partition 201803';
 ALTER TABLE v_dummy ATTACH PARTITION 201803;
-SELECT DISTINCT partition FROM system.parts WHERE table = '.inner.v_dummy' AND active;
+SELECT DISTINCT partition FROM system.parts WHERE table = '.inner.v_dummy' AND active ORDER BY partition;
--- dbms/tests/queries/0_stateless/00607_materialized_view_partition_exception.sh
+++ dbms/tests/queries/0_stateless/00607_materialized_view_partition_exception.sh
@@ -4,12 +4,30 @@ CURDIR=$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)
 . $CURDIR/../shell_config.sh
 
 $CLICKHOUSE_CLIENT -n --query="
+    DROP TABLE IF EXISTS test.mv_mv_test;
     DROP TABLE IF EXISTS test.mv_test;
     DROP TABLE IF EXISTS test.mv_test_src;
     CREATE TABLE test.mv_test     (date Date, value String) Engine = MergeTree(date, (date), 8192);
     CREATE TABLE test.mv_test_src (date Date, value String) Engine = MergeTree(date, (date), 8192);
     CREATE MATERIALIZED VIEW test.mv_mv_test TO test.mv_test AS SELECT * FROM test.mv_test_src;
+" 2> /dev/null;
+CODE=$?;
+[ "$CODE" -ne "0" ] && echo "Fail" && exit $CODE;
+
+$CLICKHOUSE_CLIENT -n --query="
     ALTER TABLE test.mv_mv_test DROP PARTITION 201801;
+" 2> /dev/null;
+CODE=$?;
+[ "$CODE" -ne "236" ] && [ "$CODE" -ne "0" ] && echo "Fail" && exit $CODE;
+
+$CLICKHOUSE_CLIENT -n --query="
+    ALTER TABLE test.mv_mv_test DETACH PARTITION 201801;
+" 2> /dev/null;
+CODE=$?;
+[ "$CODE" -ne "236" ] && [ "$CODE" -ne "0" ] && echo "Fail" && exit $CODE;
+
+$CLICKHOUSE_CLIENT -n --query="
+    ALTER TABLE test.mv_mv_test ATTACH PARTITION 201801;
 " 2> /dev/null;
 CODE=$?;
 [ "$CODE" -ne "236" ] && [ "$CODE" -ne "0" ] && echo "Fail" && exit $CODE;
