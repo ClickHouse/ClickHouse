b073f7abc57e984c15035325fbb831642336a81e
Alexey Milovidov <milovidov@yandex-team.ru>
2018-03-18 21:31:18 +0300

    Rewriting ZooKeeper library [#CLICKHOUSE-2]

--- dbms/src/Common/ZooKeeper/ZooKeeperImpl.h
+++ dbms/src/Common/ZooKeeper/ZooKeeperImpl.h
@@ -42,6 +42,13 @@ public:
 
     struct ACL
     {
+        static constexpr int32_t Read = 1;
+        static constexpr int32_t Write = 2;
+        static constexpr int32_t Create = 4;
+        static constexpr int32_t Delete = 8;
+        static constexpr int32_t Admin = 16;
+        static constexpr int32_t All = 0x1F;
+
         int32_t permissions;
         String scheme;
         String id;
@@ -238,8 +245,53 @@ public:
 
     void close();
 
+
+    enum ZOO_ERRORS
+    {
+        ZOK = 0, /*!< Everything is OK */
+
+        /** System and server-side errors.
+        * This is never thrown by the server, it shouldn't be used other than
+        * to indicate a range. Specifically error codes greater than this
+        * value, but lesser than {@link #ZAPIERROR}, are system errors. */
+        ZSYSTEMERROR = -1,
+        ZRUNTIMEINCONSISTENCY = -2, /*!< A runtime inconsistency was found */
+        ZDATAINCONSISTENCY = -3, /*!< A data inconsistency was found */
+        ZCONNECTIONLOSS = -4, /*!< Connection to the server has been lost */
+        ZMARSHALLINGERROR = -5, /*!< Error while marshalling or unmarshalling data */
+        ZUNIMPLEMENTED = -6, /*!< Operation is unimplemented */
+        ZOPERATIONTIMEOUT = -7, /*!< Operation timeout */
+        ZBADARGUMENTS = -8, /*!< Invalid arguments */
+        ZINVALIDSTATE = -9, /*!< Invliad zhandle state */
+
+        /** API errors.
+        * This is never thrown by the server, it shouldn't be used other than
+        * to indicate a range. Specifically error codes greater than this
+        * value are API errors (while values less than this indicate a
+        * {@link #ZSYSTEMERROR}).
+        */
+        ZAPIERROR = -100,
+        ZNONODE = -101, /*!< Node does not exist */
+        ZNOAUTH = -102, /*!< Not authenticated */
+        ZBADVERSION = -103, /*!< Version conflict */
+        ZNOCHILDRENFOREPHEMERALS = -108, /*!< Ephemeral nodes may not have children */
+        ZNODEEXISTS = -110, /*!< The node already exists */
+        ZNOTEMPTY = -111, /*!< The node has children */
+        ZSESSIONEXPIRED = -112, /*!< The session has been expired by the server */
+        ZINVALIDCALLBACK = -113, /*!< Invalid callback specified */
+        ZINVALIDACL = -114, /*!< Invalid ACL specified */
+        ZAUTHFAILED = -115, /*!< Client authentication failed */
+        ZCLOSING = -116, /*!< ZooKeeper is closing */
+        ZNOTHING = -117, /*!< (not error) no server responses to process */
+        ZSESSIONMOVED = -118 /*!<session moved to another server, so operation is ignored */
+    };
+
+    static const char * errorMessage(int32_t code);
+
 private:
     String root_path;
+    ACLs default_acls;
+
     Poco::Timespan session_timeout;
 
     Poco::Net::StreamSocket socket;
