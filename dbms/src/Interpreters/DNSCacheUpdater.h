8fd72a6777baf2af70b224a1397c208374b615a0
Vitaliy Lyudvichenko <vludv@yandex-team.ru>
2018-03-26 17:12:07 +0300

    Add automatic DROP DNS CACHE, update of SYSTEM queries. [#CLICKHOUSE-3645]

---
+++ dbms/src/Interpreters/DNSCacheUpdater.h
@@ -0,0 +1,42 @@
+#pragma once
+#include <memory>
+
+
+namespace DB
+{
+
+class Context;
+class BackgroundProcessingPool;
+class BackgroundProcessingPoolTaskInfo;
+
+
+/// Add a task to BackgroundProcessingPool that watch for ProfileEvents::NetworkErrors and updates DNS cache if it has increased
+class DNSCacheUpdater
+{
+public:
+
+    DNSCacheUpdater(Context & context);
+    ~DNSCacheUpdater();
+
+    /// Call it inside catch section
+    /// Returns true if it is a network error
+    static bool isNetworkError();
+
+    /// Checks if it is a network error and increments ProfileEvents::NetworkErrors
+    static bool incrementNetworkErrors();
+
+private:
+    bool run();
+
+    Context & context;
+    BackgroundProcessingPool & pool;
+    std::shared_ptr<BackgroundProcessingPoolTaskInfo> task_handle;
+    size_t last_num_network_erros = 0;
+    time_t last_update_time = 0;
+
+    static constexpr size_t min_errors_to_update_cache = 3;
+    static constexpr time_t min_update_period_seconds = 10;
+};
+
+
+}
