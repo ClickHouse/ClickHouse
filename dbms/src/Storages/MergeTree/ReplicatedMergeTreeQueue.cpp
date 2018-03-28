8a24554bc8f4386451f6ecaed2c2e4a966795960
Vitaliy Lyudvichenko <vludv@yandex-team.ru>
2018-03-28 15:48:40 +0300

    Faster DROP PARTITION. [#CLICKHOUSE-2]

--- dbms/src/Storages/MergeTree/ReplicatedMergeTreeQueue.cpp
+++ dbms/src/Storages/MergeTree/ReplicatedMergeTreeQueue.cpp
@@ -94,7 +94,12 @@ void ReplicatedMergeTreeQueue::initialize(
 void ReplicatedMergeTreeQueue::insertUnlocked(LogEntryPtr & entry, std::optional<time_t> & min_unprocessed_insert_time_changed, std::lock_guard<std::mutex> &)
 {
     virtual_parts.add(entry->new_part_name);
-    queue.push_back(entry);
+
+    /// Put 'DROP PARTITION' entries at the beginning of the queue not to make superfluous fetches of parts that will be eventually deleted
+    if (entry->type != LogEntry::DROP_RANGE)
+        queue.push_back(entry);
+    else
+        queue.push_front(entry);
 
     if (entry->type == LogEntry::GET_PART)
     {
