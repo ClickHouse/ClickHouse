5574993c7007fa22fa82ba268c9c279afdd05dac
Vitaliy Lyudvichenko <vludv@yandex-team.ru>
2018-03-30 19:25:26 +0300

    Speedup partition check, add more preformance output. [#CLICKHOUSE-2]
    Faster partition check.
    
    Added more debug info.

--- dbms/src/DataStreams/copyData.cpp
+++ dbms/src/DataStreams/copyData.cpp
@@ -1,6 +1,9 @@
 #include <DataStreams/IProfilingBlockInputStream.h>
 #include <DataStreams/IBlockOutputStream.h>
 #include <DataStreams/copyData.h>
+#include <Common/Stopwatch.h>
+#include <common/logger_useful.h>
+#include <iomanip>
 
 
 namespace DB
@@ -22,8 +25,29 @@ void copyDataImpl(IBlockInputStream & from, IBlockOutputStream & to, TCancelCall
     from.readPrefix();
     to.writePrefix();
 
-    while (Block block = from.read())
+    size_t num_blocks = 0;
+    double total_blocks_time = 0;
+    size_t slowest_block_number = 0;
+    double slowest_block_time = 0;
+
+    while (true)
     {
+        Stopwatch watch;
+        Block block = from.read();
+        double elapsed = watch.elapsedSeconds();
+
+        if (num_blocks == 0 || elapsed > slowest_block_time)
+        {
+            slowest_block_number = num_blocks;
+            slowest_block_time = elapsed;
+        }
+
+        total_blocks_time += elapsed;
+        ++num_blocks;
+
+        if (!block)
+            break;
+
         if (is_cancelled())
             break;
 
@@ -47,8 +71,28 @@ void copyDataImpl(IBlockInputStream & from, IBlockOutputStream & to, TCancelCall
     if (is_cancelled())
         return;
 
-    from.readSuffix();
-    to.writeSuffix();
+    auto log = &Poco::Logger::get("copyData");
+    bool print_dbg = num_blocks > 2;
+
+    if (print_dbg)
+    {
+        LOG_DEBUG(log, "Read " << num_blocks << " blocks. It took " << std::fixed << total_blocks_time << " seconds, average "
+                               << std::fixed << total_blocks_time / num_blocks * 1000 << " ms, the slowest block #" << slowest_block_number
+                               << " was read for " << std::fixed << slowest_block_time * 1000 << " ms ");
+    }
+
+    {
+        Stopwatch watch;
+        to.writeSuffix();
+        if (num_blocks > 1)
+            LOG_DEBUG(log, "It took " << std::fixed << watch.elapsedSeconds() << " for writeSuffix()");
+    }
+    {
+        Stopwatch watch;
+        from.readSuffix();
+        if (num_blocks > 1)
+            LOG_DEBUG(log, "It took " << std::fixed << watch.elapsedSeconds() << " seconds for readSuffix()");
+    }
 }
 
 
