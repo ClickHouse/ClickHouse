2c994b1a15bd41a183b1b6d4b52fd790eeedfdcf
proller <proller@github.com>
2018-04-03 12:47:47 +0300

    fix

--- dbms/src/Dictionaries/ODBCDictionarySource.h
+++ dbms/src/Dictionaries/ODBCDictionarySource.h
@@ -1,6 +1,10 @@
 #pragma once
 
+#pragma GCC diagnostic push
+#pragma GCC diagnostic ignored "-Wunused-parameter"
 #include <Poco/Data/SessionPool.h>
+#pragma GCC diagnostic pop
+
 #include <Dictionaries/IDictionarySource.h>
 #include <Dictionaries/ExternalQueryBuilder.h>
 #include <Dictionaries/DictionaryStructure.h>
