23296b5d0ce93e52aaa3418c086d641921b4ba3c
Vitaliy Lyudvichenko <vludv@yandex-team.ru>
2018-04-04 15:11:38 +0300

    Better timeouts handling. [#CLICKHOUSE-2]

--- dbms/src/Client/TimeoutSetter.h
+++ dbms/src/Client/TimeoutSetter.h
@@ -11,21 +11,22 @@ namespace DB
 /// Timeouts could be only decreased
 struct TimeoutSetter
 {
-    TimeoutSetter(Poco::Net::StreamSocket & socket_, const Poco::Timespan & send_timeout_, const Poco::Timespan & recieve_timeout_)
+    TimeoutSetter(Poco::Net::StreamSocket & socket_, const Poco::Timespan & send_timeout_, const Poco::Timespan & recieve_timeout_,
+                  bool limit_max_timeout = false)
         : socket(socket_), send_timeout(send_timeout_), recieve_timeout(recieve_timeout_)
     {
         old_send_timeout = socket.getSendTimeout();
         old_receive_timeout = socket.getReceiveTimeout();
 
-        if (old_send_timeout > send_timeout)
+        if (!limit_max_timeout || old_send_timeout > send_timeout)
             socket.setSendTimeout(send_timeout);
 
-        if (old_receive_timeout > recieve_timeout)
+        if (!limit_max_timeout || old_receive_timeout > recieve_timeout)
             socket.setReceiveTimeout(recieve_timeout);
     }
 
-    TimeoutSetter(Poco::Net::StreamSocket & socket_, const Poco::Timespan & timeout_)
-        : TimeoutSetter(socket_, timeout_, timeout_) {}
+    TimeoutSetter(Poco::Net::StreamSocket & socket_, const Poco::Timespan & timeout_, bool limit_max_timeout = false)
+        : TimeoutSetter(socket_, timeout_, timeout_, limit_max_timeout) {}
 
     ~TimeoutSetter()
     {
