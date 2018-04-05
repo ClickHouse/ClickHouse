da7975dc29108a7f565cbddf63334d452e7ca260
Alexey Milovidov <milovidov@yandex-team.ru>
2018-04-05 23:43:35 +0300

    Added test #797

--- dbms/tests/queries/0_stateless/00615_nullable_alter_optimize.sql
+++ dbms/tests/queries/0_stateless/00615_nullable_alter_optimize.sql
@@ -16,5 +16,8 @@ alter table test add column data Nullable(Float64);
 
 INSERT INTO test (dt,id, key,data) VALUES (now(), 100, 'key', 100500);
 
+SELECT * FROM test ORDER BY data NULLS FIRST;
 OPTIMIZE TABLE test;
+SELECT * FROM test ORDER BY data NULLS FIRST;
+
 DROP TABLE test;
