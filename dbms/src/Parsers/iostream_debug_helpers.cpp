8dd05bb171c4eaae7ace562bc26c79a090cc68e8
proller <proller@github.com>
2018-04-07 00:09:06 +0300

    wip

--- dbms/src/Parsers/iostream_debug_helpers.cpp
+++ dbms/src/Parsers/iostream_debug_helpers.cpp
@@ -14,17 +14,19 @@ std::ostream & operator<<(std::ostream & stream, const Token & what) {
 }
 
 std::ostream & operator<<(std::ostream & stream, const Expected & what) {
-    stream << "Expected {variants=" 
-    //<< what.variants 
-    << "; max_parsed_pos=" << what.max_parsed_pos << "}";
+    stream << "Expected {variants=";
+    dumpValue(stream, what.variants)
+       << "; max_parsed_pos=" << what.max_parsed_pos << "}";
     return stream;
 }
 
+/*
 std::ostream & operator<<(std::ostream & stream, const TokenIterator & what) {
     //stream << "TokenIterator ( index=" << what.index << ") {" << get() << "}";
     auto copy = what; // avoid const x[] call
     stream << "TokenIterator { "<< copy.get() << "}";
     return stream;
 }
+*/
 
 }
--- dbms/src/Parsers/iostream_debug_helpers.h
+++ dbms/src/Parsers/iostream_debug_helpers.h
@@ -6,12 +6,13 @@ namespace DB
 class Token;
 std::ostream & operator<<(std::ostream & stream, const Token & what);
 
-class Expected;
+struct Expected;
 std::ostream & operator<<(std::ostream & stream, const Expected & what);
 
+/*
 class TokenIterator;
 std::ostream & operator<<(std::ostream & stream, const TokenIterator & what);
-
+*/
 }
 
 /// some operator<< should be declared before operator<<(... std::shared_ptr<>)
