59a68d938fc5b610cff4a745ebc908d1876b3ab1
Alexey Milovidov <milovidov@yandex-team.ru>
2018-04-22 20:30:28 -0700

    Fixed error with Arrays inside Nested data type; added a test #2066

---
+++ dbms/tests/queries/0_stateless/00625_arrays_in_nested.sql
@@ -0,0 +1,83 @@
+USE test;
+
+DROP TABLE IF EXISTS nested;
+CREATE TABLE nested
+(
+    column Nested
+    (
+        name String,
+        names Array(String),
+        types Array(Enum8('PU' = 1, 'US' = 2, 'OTHER' = 3))
+    )
+) ENGINE = MergeTree ORDER BY tuple();
+
+INSERT INTO nested VALUES (['Hello', 'World'], [['a'], ['b', 'c']], [['PU', 'US'], ['OTHER']]);
+
+SELECT * FROM nested;
+
+
+DROP TABLE IF EXISTS nested;
+CREATE TABLE nested
+(
+    column Nested
+    (
+        name String,
+        names Array(String),
+        types Array(Enum8('PU' = 1, 'US' = 2, 'OTHER' = 3))
+    )
+) ENGINE = Log;
+
+INSERT INTO nested VALUES (['Hello', 'World'], [['a'], ['b', 'c']], [['PU', 'US'], ['OTHER']]);
+
+SELECT * FROM nested;
+
+
+DROP TABLE IF EXISTS nested;
+CREATE TABLE nested
+(
+    column Nested
+    (
+        name String,
+        names Array(String),
+        types Array(Enum8('PU' = 1, 'US' = 2, 'OTHER' = 3))
+    )
+) ENGINE = TinyLog;
+
+INSERT INTO nested VALUES (['Hello', 'World'], [['a'], ['b', 'c']], [['PU', 'US'], ['OTHER']]);
+
+SELECT * FROM nested;
+
+
+DROP TABLE IF EXISTS nested;
+CREATE TABLE nested
+(
+    column Nested
+    (
+        name String,
+        names Array(String),
+        types Array(Enum8('PU' = 1, 'US' = 2, 'OTHER' = 3))
+    )
+) ENGINE = StripeLog;
+
+INSERT INTO nested VALUES (['Hello', 'World'], [['a'], ['b', 'c']], [['PU', 'US'], ['OTHER']]);
+
+SELECT * FROM nested;
+
+
+DROP TABLE IF EXISTS nested;
+CREATE TABLE nested
+(
+    column Nested
+    (
+        name String,
+        names Array(String),
+        types Array(Enum8('PU' = 1, 'US' = 2, 'OTHER' = 3))
+    )
+) ENGINE = Memory;
+
+INSERT INTO nested VALUES (['Hello', 'World'], [['a'], ['b', 'c']], [['PU', 'US'], ['OTHER']]);
+
+SELECT * FROM nested;
+
+
+DROP TABLE nested;
