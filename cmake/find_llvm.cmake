9f3bff20f4d5fffd4bc1a4f5e2a4d65fb3b414ff
proller <proller@github.com>
2018-04-24 17:42:46 +0300

    Build fixes

--- cmake/find_llvm.cmake
+++ cmake/find_llvm.cmake
@@ -1,4 +1,4 @@
-option (ENABLE_EMBEDDED_COMPILER "Set to TRUE to enable support for 'compile' option for query execution" FALSE)
+option (ENABLE_EMBEDDED_COMPILER "Set to TRUE to enable support for 'compile' option for query execution" 1)
 
 if (ENABLE_EMBEDDED_COMPILER)
     # Based on source code of YT.
@@ -102,6 +102,6 @@ if (ENABLE_EMBEDDED_COMPILER)
     endif()
 
     if (LLVM_FOUND AND LLVM_INCLUDE_DIRS AND LLVM_LIBRARY_DIRS)
-        set(USE_EMBEDDED_COMPILER TRUE)
+        set (USE_EMBEDDED_COMPILER 1)
     endif()
 endif()
