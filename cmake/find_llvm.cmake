01bbf650f88582befd4aef7908fb7624516be101
Alexey Milovidov <milovidov@yandex-team.ru>
2018-05-07 11:59:24 +0300

    Build fixes #2277

--- cmake/find_llvm.cmake
+++ cmake/find_llvm.cmake
@@ -1,10 +1,12 @@
 option (ENABLE_EMBEDDED_COMPILER "Set to TRUE to enable support for 'compile' option for query execution" 1)
 
 if (ENABLE_EMBEDDED_COMPILER)
+    set (LLVM_PATHS "/usr/local/lib/llvm")
+
     if (CMAKE_CXX_COMPILER_ID STREQUAL "Clang")
-        find_package(LLVM CONFIG)
+        find_package(LLVM CONFIG PATHS ${LLVM_PATHS})
     else ()
-        find_package(LLVM 5 CONFIG)
+        find_package(LLVM 5 CONFIG PATHS ${LLVM_PATHS})
     endif ()
 
     if (LLVM_FOUND)
@@ -16,6 +18,9 @@ if (ENABLE_EMBEDDED_COMPILER)
         message(STATUS "LLVM Include Directory: ${LLVM_INCLUDE_DIRS}")
         message(STATUS "LLVM Library Directory: ${LLVM_LIBRARY_DIRS}")
         message(STATUS "LLVM C++ Compiler: ${LLVM_CXXFLAGS}")
+
+        option(LLVM_HAS_RTTI "Enable if LLVM was build with RTTI enabled" ON)
+
         set (USE_EMBEDDED_COMPILER 1)
     endif()
 endif()
