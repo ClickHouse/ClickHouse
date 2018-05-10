53fcbc11625cd26c07fece3c1a998cfe3b8f6eb2
sundy-li <543950155@qq.com>
2018-05-10 17:23:38 +0800

    Support data write to StorageMySQL table

--- dbms/src/Storages/StorageMySQL.cpp
+++ dbms/src/Storages/StorageMySQL.cpp
@@ -1,12 +1,20 @@
 #include <Storages/StorageMySQL.h>
 
 #if USE_MYSQL
+
 #include <Storages/StorageFactory.h>
 #include <Storages/transformQueryForExternalDatabase.h>
 #include <Dictionaries/MySQLBlockInputStream.h>
 #include <Interpreters/evaluateConstantExpression.h>
+#include <Interpreters/Settings.h>
+#include <DataStreams/IBlockOutputStream.h>
+#include <DataStreams/BlockOutputStreamFromRowOutputStream.h>
+#include <DataStreams/ValuesRowOutputStream.h>
 #include <Common/parseAddress.h>
+#include <IO/Operators.h>
+#include <IO/WriteHelpers.h>
 #include <Parsers/ASTLiteral.h>
+#include <mysqlxx/Transaction.h>
 
 
 namespace DB
@@ -55,6 +63,99 @@ BlockInputStreams StorageMySQL::read(
     return { std::make_shared<MySQLBlockInputStream>(pool.Get(), query, sample_block, max_block_size) };
 }
 
+
+class StorageMySQLBlockOutputStream : public IBlockOutputStream
+{
+public:
+    explicit StorageMySQLBlockOutputStream(
+            const StorageMySQL & storage,
+            const std::string & remote_database_name,
+            const std::string & remote_table_name ,
+            const mysqlxx::PoolWithFailover::Entry & entry,
+            const size_t & mysql_max_rows_to_insert)
+        : sample_block{storage.getSampleBlock()}
+        , remote_database_name{remote_database_name}
+        , remote_table_name{remote_table_name}
+        , entry{entry}
+        , max_batch_rows{mysql_max_rows_to_insert}
+    {
+    }
+
+   Block getHeader() const override { return sample_block; }
+
+   void write(const Block & block) override
+   {
+       auto blocks = splitBlocks(block, max_batch_rows);
+       mysqlxx::Transaction trans(entry);
+       try
+       {
+           for(const Block & batch_data : blocks)
+           {
+               writeBlockData(batch_data);
+           }
+           trans.commit();
+       }
+       catch(...)
+       {
+           trans.rollback();
+           throw;
+       }
+   }
+
+   void writeBlockData(const Block & block)
+   {
+       WriteBufferFromOwnString sqlbuf;
+       sqlbuf << "INSERT INTO  " << remote_database_name << "." << remote_table_name << " ( " << block.dumpNames() << " ) VALUES ";
+
+       auto writer = std::make_shared<BlockOutputStreamFromRowOutputStream>(std::make_shared<ValuesRowOutputStream>(sqlbuf), sample_block);
+       writer->write(block);
+       sqlbuf << ";";
+
+       auto query = this->entry->query(sqlbuf.str());
+       query.execute();
+   }
+
+   Blocks splitBlocks(const Block & block, const size_t & max_rows) const
+   {
+       const size_t splited_block_size = ceil(block.rows() * 1.0 / max_rows);
+       Blocks splitted_blocks(splited_block_size);
+
+       for (size_t idx = 0; idx < splited_block_size; ++idx)
+           splitted_blocks[idx] = block.cloneEmpty();
+
+       const size_t columns = block.columns();
+       const size_t rows = block.rows();
+       size_t offsets = 0;
+       size_t limits = max_batch_rows;
+       for (size_t idx = 0; idx < splited_block_size; ++idx)
+       {
+          // For last batch, limits should be the remain size
+          if(idx == splited_block_size - 1) limits = rows - offsets;
+          for(size_t col_idx = 0; col_idx < columns; ++col_idx)
+          {
+              splitted_blocks[idx].getByPosition(col_idx).column = block.getByPosition(col_idx).column->cut(offsets, limits);
+          }
+          offsets += max_batch_rows;
+       }
+
+       return splitted_blocks;
+   }
+
+
+private:
+   Block sample_block;
+   std::string remote_database_name;
+   std::string remote_table_name;
+   mysqlxx::PoolWithFailover::Entry entry;
+   size_t max_batch_rows;
+};
+
+
+BlockOutputStreamPtr StorageMySQL::write(
+    const ASTPtr & /*query*/, const Settings & settings)
+{
+    return std::make_shared<StorageMySQLBlockOutputStream>(*this, remote_database_name, remote_table_name, pool.Get(), settings.mysql_max_rows_to_insert);
+}
 
 void registerStorageMySQL(StorageFactory & factory)
 {
