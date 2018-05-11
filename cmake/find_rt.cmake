ed70e468f9305038c9a40affb9a758f939781c60
Alexey Milovidov <milovidov@yandex-team.ru>
2018-05-11 03:56:31 +0300

    Removed useless library [#CLICKHOUSE-2]

--- cmake/find_rt.cmake
+++ cmake/find_rt.cmake
@@ -1,8 +1,10 @@
 if (APPLE)
     # lib from libs/libcommon
     set (RT_LIBRARY "apple_rt")
-else ()
+elseif (ARCH_FREEBSD)
     find_library (RT_LIBRARY rt)
+else ()
+    set (RT_LIBRARY "")
 endif ()
 
 message(STATUS "Using rt: ${RT_LIBRARY}")
