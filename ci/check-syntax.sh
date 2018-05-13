fa59cbc8718343f339ad8510754daac27f97cf02
Alexey Milovidov <milovidov@yandex-team.ru>
2018-05-14 02:20:57 +0300

    CI: development [#CLICKHOUSE-2]

--- ci/check-syntax.sh
+++ ci/check-syntax.sh
@@ -3,10 +3,6 @@ set -e -x
 
 source default-config
 
-# NOTE: It will argue about
-#  fatal error: re2_st/re2.h: No such file or directory
-# due to generated headers.
-
 $SUDO apt-get install -y jq
 
 [[ -d "${WORKSPACE}/sources" ]] || die "Run get-sources.sh first"
@@ -15,6 +11,9 @@ mkdir -p "${WORKSPACE}/build"
 pushd "${WORKSPACE}/build"
 
 cmake -D CMAKE_BUILD_TYPE=Debug $CMAKE_FLAGS ../sources
+
+make re2_st # Generated headers
+
 jq --raw-output '.[] | .command' compile_commands.json | grep -v -P -- '-c .+/contrib/' | sed -r -e 's/-o\s+\S+/-fsyntax-only/' > syntax-commands
 xargs --arg-file=syntax-commands --max-procs=$THREADS --replace /bin/sh -c "{}"
 
