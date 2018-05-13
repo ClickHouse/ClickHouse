1fa8a9a6dfbbba6c8cbf4603df4b90c333019968
Alexey Milovidov <milovidov@yandex-team.ru>
2018-05-13 13:05:34 +0300

    Fixed unclean code #2352

--- dbms/src/AggregateFunctions/AggregateFunctionWindowFunnel.cpp
+++ dbms/src/AggregateFunctions/AggregateFunctionWindowFunnel.cpp
@@ -12,9 +12,15 @@ namespace
 
 AggregateFunctionPtr createAggregateFunctionWindowFunnel(const std::string & name, const DataTypes & arguments, const Array & params)
 {
+    if (params.size() != 1)
+        throw Exception{"Aggregate function " + name + " requires exactly one parameter.", ErrorCodes::NUMBER_OF_ARGUMENTS_DOESNT_MATCH};
 
-    if (params.size() <= 0 || params.size() > 32)
-        throw Exception("Aggregate function " + name + " requires (1, 32] event ids.", ErrorCodes::NUMBER_OF_ARGUMENTS_DOESNT_MATCH);
+    if (arguments.size() < 2)
+        throw Exception("Aggregate function " + name + " requires one timestamp argument and at least one event condition.", ErrorCodes::NUMBER_OF_ARGUMENTS_DOESNT_MATCH);
+
+    if (arguments.size() > AggregateFunctionWindowFunnelData::max_events + 1)
+        throw Exception("Too many event arguments for aggregate function " + name, ErrorCodes::NUMBER_OF_ARGUMENTS_DOESNT_MATCH);
+
     return std::make_shared<AggregateFunctionWindowFunnel>(arguments, params);
 }
 
