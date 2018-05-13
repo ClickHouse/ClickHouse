325c9b9213ebe4c113c200f7e37bb316ad11f53b
Alexey Milovidov <milovidov@yandex-team.ru>
2018-05-13 23:24:37 +0300

    CI: development [#CLICKHOUSE-2]

--- ci/run-with-docker.sh
+++ ci/run-with-docker.sh
@@ -2,4 +2,4 @@
 set -e
 
 PROJECT_ROOT="$(cd "$(dirname "$0")/.."; pwd -P)"
-docker run --network=host --mount=type=bind,source=PROJECT_ROOT,destination=/ClickHouse --workdir=/ClickHouse/ci --env=CONFIG "$1" "$2"
+docker run --network=host --mount=type=bind,source=${PROJECT_ROOT},destination=/ClickHouse --workdir=/ClickHouse/ci --env=CONFIG "$1" "$2"
