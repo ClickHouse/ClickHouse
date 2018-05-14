c0c126179a62ccaf925c369ad85bf3fb87d02d8d
sind <sindbag@gmail.com>
2018-05-14 14:22:17 +0300

    LZ4 headers

--- dbms/src/Compression/CompressionCodecLZ4.cpp
+++ dbms/src/Compression/CompressionCodecLZ4.cpp
@@ -1,1 +1,53 @@
-#include "CompressionCodecLZ4.h"
+#include <Columns/ColumnFixedString.h>
+#include <Columns/ColumnsNumber.h>
+#include <Compression/ICompressionCodec.h>
+
+#include <Compression/CompressionCodecLZ4.h>
+#include <Compression/CompressionCodecFactory.h>
+
+#include <Parsers/IAST.h>
+#include <Parsers/ASTLiteral.h>
+
+#include <Common/typeid_cast.h>
+
+
+namespace DB
+{
+
+namespace ErrorCodes
+{
+    extern const int CANNOT_READ_ALL_DATA;
+    extern const int TOO_LARGE_STRING_SIZE;
+    extern const int NUMBER_OF_ARGUMENTS_DOESNT_MATCH;
+    extern const int UNEXPECTED_AST_STRUCTURE;
+}
+
+
+template<typename T>
+static CodecPtr create(const ASTPtr &arguments)
+{
+    if (!arguments)
+        return std::make_shared<T>();
+
+    if (arguments->children.size() != 1)
+        throw Exception("LZ4 codec can optionally have only one argument - ???",
+                        ErrorCodes::NUMBER_OF_ARGUMENTS_DOESNT_MATCH);
+
+    const ASTLiteral *arg = typeid_cast<const ASTLiteral *>(arguments->children[0].get());
+    if (!arg || arg->value.getType() != Field::Types::String)
+        throw Exception("Parameter for LZ4 codec must be string literal", ErrorCodes::ILLEGAL_TYPE_OF_ARGUMENT);
+
+    return std::make_shared<T>(arg->value.get<String>());
+}
+
+void registerCodecLZ4(CompressionCodecFactory &factory)
+{
+    factory.registerDataType("LZ4", create<CompressionCodecLZ4>);
+}
+
+void registerCodecLZ4HC(CompressionCodecFactory &factory)
+{
+    factory.registerDataType("LZ4HC", create<CompressionCodecLZ4HC>);
+}
+
+}
--- dbms/src/Compression/CompressionCodecLZ4.h
+++ dbms/src/Compression/CompressionCodecLZ4.h
@@ -7,39 +7,34 @@ class CompressionCodecLZ4 : ICompressionCodec
 private:
 public:
     static constexpr bool is_hc = false;
+    std::string getName() const override
+    {
+        return "LZ4()"
+    }
 
+    const char * getFamilyName() const override
+    {
+        return "LZ4"
+    }
+
+    std::vector<uint8_t> getHeader() const override;
+    size_t getCompressedSize(size_t uncompressed_size) const override;
+
+    size_t compress(const char* source, char* dest, int inputSize, int maxOutputSize) const override;
+    size_t decompress(void* dest, size_t maxOutputSize, const void* source, size_t inputSize) const override;
+
+    bool equals(const ICompressionCodec & rhs) const override;
 };
 
 class CompressionCodecLZ4HC : CompressionCodecLZ4
 {
 public:
     static constexpr bool is_hc = true;
+
+    std::string getName() const override
+    {
+        return "LZ4HC()"
+    }
 };
-
-template < typename T >
-static CodecPtr create(const ASTPtr & arguments)
-{
-    if (!arguments)
-        return std::make_shared<T>();
-
-    if (arguments->children.size() != 1)
-        throw Exception("LZ4 codec can optionally have only one argument - ???", ErrorCodes::NUMBER_OF_ARGUMENTS_DOESNT_MATCH);
-
-    const ASTLiteral * arg = typeid_cast<const ASTLiteral *>(arguments->children[0].get());
-    if (!arg || arg->value.getType() != Field::Types::String)
-        throw Exception("Parameter for LZ4 codec must be string literal", ErrorCodes::ILLEGAL_TYPE_OF_ARGUMENT);
-
-    return std::make_shared<T>(arg->value.get<String>());
-}
-
-void registerCodecLZ4(CompressionCodecFactory & factory)
-{
-    factory.registerDataType("LZ4", create<CompressionCodecLZ4>);
-}
-
-void registerCodecLZ4HC(CompressionCodecFactory & factory)
-{
-    factory.registerDataType("LZ4HC", create<CompressionCodecLZ4HC>);
-}
 
 }
