a98d33f77fca314e7459742d1740b56e1b302e65
Alexey Milovidov <milovidov@yandex-team.ru>
2018-05-14 21:49:51 +0300

    CI: added support to build on FreeBSD under Vagrant [#CLICKHOUSE-2]

--- ci/install-compiler-from-packages.sh
+++ ci/install-compiler-from-packages.sh
@@ -3,27 +3,20 @@ set -e -x
 
 source default-config
 
-# TODO Non debian systems
 # TODO Install from PPA on older Ubuntu
 
-if [ -f '/etc/lsb-release' ]; then
-    source /etc/lsb-release
-    if [[ "$DISTRIB_ID" == "Ubuntu" ]]; then
-        if [[ "$COMPILER" == "gcc" ]]; then
-            $SUDO apt-get -y install gcc-${COMPILER_PACKAGE_VERSION} g++-${COMPILER_PACKAGE_VERSION}
-            export CC=gcc-${COMPILER_PACKAGE_VERSION}
-            export CXX=g++-${COMPILER_PACKAGE_VERSION}
-        elif [[ "$COMPILER" == "clang" ]]; then
-            [[ $(uname -m) == "x86_64" ]] && LLD="lld-${COMPILER_PACKAGE_VERSION}"
-            $SUDO apt-get -y install clang-${COMPILER_PACKAGE_VERSION} "$LLD" libc++-dev libc++abi-dev
-            export CC=clang-${COMPILER_PACKAGE_VERSION}
-            export CXX=clang++-${COMPILER_PACKAGE_VERSION}
-        else
-            die "Unknown compiler specified"
-        fi
-    else
-        die "Unknown Linux variant"
+./install-os-packages.sh ${COMPILER}-${COMPILER_PACKAGE_VERSION}
+
+if [[ "$COMPILER" == "gcc" ]]; then
+    if command -v gcc-${COMPILER_PACKAGE_VERSION}; then export CC=gcc-${COMPILER_PACKAGE_VERSION} CXX=g++-${COMPILER_PACKAGE_VERSION};
+    elif command -v gcc${COMPILER_PACKAGE_VERSION}; then export CC=gcc${COMPILER_PACKAGE_VERSION} CXX=g++${COMPILER_PACKAGE_VERSION};
+    elif command -v gcc; then export CC=gcc CXX=g++;
+    fi
+elif [[ "$COMPILER" == "clang" ]]; then
+    if command -v clang-${COMPILER_PACKAGE_VERSION}; then export CC=clang-${COMPILER_PACKAGE_VERSION} CXX=clang++-${COMPILER_PACKAGE_VERSION};
+    elif command -v clang${COMPILER_PACKAGE_VERSION}; then export CC=clang${COMPILER_PACKAGE_VERSION} CXX=clang++${COMPILER_PACKAGE_VERSION};
+    elif command -v clang; then export CC=clang CXX=clang++;
     fi
 else
-    die "Unknown OS"
+    die "Unknown compiler specified"
 fi
