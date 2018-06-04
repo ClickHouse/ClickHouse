b34c2443f43953f9a4fb50fb102f51a87535f4ae
Ivan Zhukov <electrocartridge@gmail.com>
2018-06-05 02:44:36 +0300

    Parquet input: support NULL values

--- dbms/src/DataStreams/ParquetBlockInputStream.cpp
+++ dbms/src/DataStreams/ParquetBlockInputStream.cpp
@@ -6,11 +6,13 @@
 #include <Core/ColumnWithTypeAndName.h>
 #include <Columns/IColumn.h>
 #include <Columns/ColumnsNumber.h>
+#include <Columns/ColumnNullable.h>
 #include <Columns/ColumnString.h>
 #include <common/DateLUTImpl.h>
 #include <DataStreams/ParquetBlockInputStream.h>
 #include <DataTypes/DataTypeFactory.h>
 #include <DataTypes/DataTypeDate.h>
+#include <DataTypes/DataTypeNullable.h>
 #include <IO/BufferBase.h>
 #include <IO/ReadBufferFromMemory.h>
 #include <IO/WriteHelpers.h>
@@ -140,21 +142,58 @@ void ParquetBlockInputStream::fillColumnWithDate32Data(std::shared_ptr<arrow::Co
     }
 }
 
+/// Creates a null bytemap from arrow's null bitmap
+void ParquetBlockInputStream::fillByteMapFromArrowColumn(std::shared_ptr<arrow::Column> & arrow_column, MutableColumnPtr & bytemap)
+{
+    PaddedPODArray<UInt8> & bytemap_data = static_cast<ColumnVector<UInt8> &>(*bytemap).getData();
+    bytemap_data.reserve(arrow_column->length());
 
-#define FOR_ARROW_NUMERIC_TYPES(M) \
-        M(arrow::Type::UINT8,  UInt8) \
-        M(arrow::Type::INT8,   Int8) \
-        M(arrow::Type::UINT16, UInt16) \
-        M(arrow::Type::INT16,  Int16) \
-        M(arrow::Type::UINT32, UInt32) \
-        M(arrow::Type::INT32,  Int32) \
-        M(arrow::Type::UINT64, UInt64) \
-        M(arrow::Type::INT64,  Int64) \
+    for (size_t chunk_i = 0; chunk_i != static_cast<size_t>(arrow_column->data()->num_chunks()); ++chunk_i)
+    {
+        std::shared_ptr<arrow::Array> chunk = arrow_column->data()->chunk(chunk_i);
+
+        for (size_t value_i = 0; value_i != static_cast<size_t>(chunk->length()); ++value_i)
+            bytemap_data.emplace_back(chunk->IsNull(value_i));
+    }
+}
+
+#define FOR_ARROW_NUMERIC_TYPES(M)      \
+        M(arrow::Type::UINT8,  UInt8)   \
+        M(arrow::Type::INT8,   Int8)    \
+        M(arrow::Type::UINT16, UInt16)  \
+        M(arrow::Type::INT16,  Int16)   \
+        M(arrow::Type::UINT32, UInt32)  \
+        M(arrow::Type::INT32,  Int32)   \
+        M(arrow::Type::UINT64, UInt64)  \
+        M(arrow::Type::INT64,  Int64)   \
         M(arrow::Type::FLOAT,  Float32) \
         M(arrow::Type::DOUBLE, Float64)
 
 
 using NameToColumnPtr = std::unordered_map<std::string, std::shared_ptr<arrow::Column>>;
+
+std::unordered_map<arrow::Type::type, std::shared_ptr<IDataType>> arrow_type_to_internal_type = {
+    {arrow::Type::UINT8,  std::make_shared<DataTypeUInt8>()},
+    {arrow::Type::INT8,   std::make_shared<DataTypeInt8>()},
+    {arrow::Type::UINT16, std::make_shared<DataTypeUInt16>()},
+    {arrow::Type::INT16,  std::make_shared<DataTypeInt16>()},
+    {arrow::Type::UINT32, std::make_shared<DataTypeUInt32>()},
+    {arrow::Type::INT32,  std::make_shared<DataTypeInt32>()},
+    {arrow::Type::UINT64, std::make_shared<DataTypeUInt64>()},
+    {arrow::Type::INT64,  std::make_shared<DataTypeInt64>()},
+    {arrow::Type::FLOAT,  std::make_shared<DataTypeFloat32>()},
+    {arrow::Type::DOUBLE, std::make_shared<DataTypeFloat64>()},
+
+    {arrow::Type::BOOL,   std::make_shared<DataTypeUInt8>()},
+    {arrow::Type::DATE32, std::make_shared<DataTypeDate>()},
+
+    {arrow::Type::STRING, std::make_shared<DataTypeString>()}//,
+    // TODO: add other types that are convertable to internal ones:
+    // 0. ENUM?
+    // 1. UUID -> String
+    // 2. JSON -> String
+};
+
 
 Block ParquetBlockInputStream::readImpl()
 {
@@ -170,8 +209,6 @@ Block ParquetBlockInputStream::readImpl()
         copyData(istr, file_buffer);
     }
 
-    // TODO: is it possible to read metadata only and then read columns one by one?
-    // TODO: seems like row groups are especially for that (kinda)
     arrow::Buffer buffer(file_data);
     // TODO: maybe use parquet::RandomAccessSource?
     auto reader = parquet::ParquetFileReader::Open(std::make_shared<::arrow::io::BufferReader>(buffer));
@@ -214,10 +251,31 @@ Block ParquetBlockInputStream::readImpl()
             throw Exception("Unsupported type " + arrow_column->type()->name() + " of a column " + arrow_column->name()/*, ErrorCodes::TODO*/);
         }
 
-        DataTypePtr internal_type = arrow_type_to_internal_type[arrow_type];
-        if (header_column.type->getName() != internal_type->getName())
+        // TODO: check if a column is const?
+        if (!header_column.type->isNullable() && arrow_column->null_count())
         {
-            throw Exception("Input data type " + internal_type->getName() + " for column \"" + header_column.name + "\" is not compatible with an actual type " + header_column.type->getName());
+            throw Exception("Can not insert NULL data into non-nullable column \"" + header_column.name + "\""/*, ErrorCodes::TODO*/);
+        }
+
+        const bool target_column_is_nullable = header_column.type->isNullable() || arrow_column->null_count();
+
+        const DataTypePtr internal_nested_type = arrow_type_to_internal_type[arrow_type];
+        const DataTypePtr internal_type = target_column_is_nullable ? makeNullable(internal_nested_type) : internal_nested_type;
+        const std::string internal_nested_type_name = internal_nested_type->getName();
+
+        const DataTypePtr column_nested_type = header_column.type->isNullable()
+            ? static_cast<const DataTypeNullable *>(header_column.type.get())->getNestedType()
+            : header_column.type;
+        const DataTypePtr column_type = header_column.type;
+        const std::string column_nested_type_name = column_nested_type->getName();
+
+        // TODO: can it be done with typeid_cast?
+        if (internal_nested_type_name != column_nested_type_name)
+        {
+            throw Exception(
+                "Input data type " + internal_nested_type_name + " for column \"" + header_column.name + "\" "
+                "is not compatible with a column type " + column_nested_type_name/*, ErrorCodes::TODO*/
+            );
         }
 
         ColumnWithTypeAndName column;
@@ -225,9 +283,7 @@ Block ParquetBlockInputStream::readImpl()
         column.type = internal_type;
 
         /// Data
-        MutableColumnPtr read_column = column.type->createColumn();
-
-        // TODO: support NULL values
+        MutableColumnPtr read_column = internal_nested_type->createColumn();
 
         switch (arrow_type)
         {
@@ -252,10 +308,18 @@ Block ParquetBlockInputStream::readImpl()
             // TODO: read UUID as a string?
             default:
                 throw Exception("Unsupported parquet type " + arrow_column->type()->name()/*, ErrorCodes::TODO*/);
-
         }
 
-        column.column = std::move(read_column);
+        if (column.type->isNullable())
+        {
+            MutableColumnPtr null_bytemap = DataTypeUInt8().createColumn();
+            fillByteMapFromArrowColumn(arrow_column, null_bytemap);
+            column.column = ColumnNullable::create(std::move(read_column), std::move(null_bytemap));
+        }
+        else
+        {
+            column.column = std::move(read_column);
+        }
         res.insert(std::move(column));
     }
 
