b7ca7f0619be1a0ce1cafa87c9edccde2cbb713b
Ivan Zhukov <electrocartridge@gmail.com>
2018-06-05 02:44:36 +0300

    Parquet input: support NULL values

--- dbms/src/DataStreams/ParquetBlockInputStream.h
+++ dbms/src/DataStreams/ParquetBlockInputStream.h
@@ -27,34 +27,13 @@ private:
     ReadBuffer & istr;
     Block header;
 
-    template <typename NumericType>
-    void fillColumnWithNumericData(std::shared_ptr<arrow::Column> & arrow_column, MutableColumnPtr & internal_column);
-
     void fillColumnWithStringData(std::shared_ptr<arrow::Column> & arrow_column, MutableColumnPtr & internal_column);
     void fillColumnWithBooleanData(std::shared_ptr<arrow::Column> & arrow_column, MutableColumnPtr & internal_column);
     void fillColumnWithDate32Data(std::shared_ptr<arrow::Column> & arrow_column, MutableColumnPtr & internal_column);
+    template <typename NumericType>
+    void fillColumnWithNumericData(std::shared_ptr<arrow::Column> & arrow_column, MutableColumnPtr & internal_column);
 
-    std::unordered_map<arrow::Type::type, std::shared_ptr<IDataType>> arrow_type_to_internal_type = {
-        {arrow::Type::UINT8,  std::make_shared<DataTypeUInt8>()},
-        {arrow::Type::INT8,   std::make_shared<DataTypeInt8>()},
-        {arrow::Type::UINT16, std::make_shared<DataTypeUInt16>()},
-        {arrow::Type::INT16,  std::make_shared<DataTypeInt16>()},
-        {arrow::Type::UINT32, std::make_shared<DataTypeUInt32>()},
-        {arrow::Type::INT32,  std::make_shared<DataTypeInt32>()},
-        {arrow::Type::UINT64, std::make_shared<DataTypeUInt64>()},
-        {arrow::Type::INT64,  std::make_shared<DataTypeInt64>()},
-        {arrow::Type::FLOAT,  std::make_shared<DataTypeFloat32>()},
-        {arrow::Type::DOUBLE, std::make_shared<DataTypeFloat64>()},
-
-        {arrow::Type::BOOL,   std::make_shared<DataTypeUInt8>()},
-        {arrow::Type::DATE32, std::make_shared<DataTypeDate>()},
-
-        {arrow::Type::STRING, std::make_shared<DataTypeString>()}//,
-        // TODO: add other types that are convertable to internal ones:
-        // 0. ENUM?
-        // 1. UUID -> String
-        // 2. JSON -> String
-    };
+    void fillByteMapFromArrowColumn(std::shared_ptr<arrow::Column> & arrow_column, MutableColumnPtr & bytemap);
 
     // TODO: check that this class implements every part of its parent
 };
