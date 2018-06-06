2ca21dfde7a870cebb42d6b8ce239fef3b877522
Vadim <plahtinskiy.ve@phystech.edu>
2018-06-06 22:36:14 +0300

    fix

--- dbms/tests/queries/0_stateless/00640_endsWith.sql
+++ dbms/tests/queries/0_stateless/00640_endsWith.sql
@@ -9,8 +9,8 @@ DROP TABLE IF EXISTS endsWith_test;
 CREATE TABLE endsWith_test(S1 String, S2 String, S3 FixedString(2)) ENGINE=Memory;
 INSERT INTO endsWith_test values ('11', '22', '33'), ('a', 'a', 'bb'), ('abc', 'bc', '23');
 
-SELECT COUNT() FROM endsWith_test WHERE endsWith(s1, s1);
-SELECT COUNT() FROM endsWith_test WHERE endsWith(s1, s2);
-SELECT COUNT() FROM endsWith_test WHERE endsWith(s2, s3);
+SELECT COUNT() FROM endsWith_test WHERE endsWith(S1, S1);
+SELECT COUNT() FROM endsWith_test WHERE endsWith(S1, S2);
+SELECT COUNT() FROM endsWith_test WHERE endsWith(S2, S3);
 DROP TABLE endsWith_test;
 
