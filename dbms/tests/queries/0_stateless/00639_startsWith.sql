6e7631accb5e94799e0d415d47c7b1f91b5df80b
Vadim <plahtinskiy.ve@phystech.edu>
2018-06-06 22:36:14 +0300

    fix

--- dbms/tests/queries/0_stateless/00639_startsWith.sql
+++ dbms/tests/queries/0_stateless/00639_startsWith.sql
@@ -9,7 +9,7 @@ DROP TABLE IF EXISTS startsWith_test;
 CREATE TABLE startsWith_test(S1 String, S2 String, S3 FixedString(2)) ENGINE=Memory;
 INSERT INTO startsWith_test values ('11', '22', '33'), ('a', 'a', 'bb'), ('abc', 'ab', '23');
 
-SELECT COUNT() FROM startsWith_test WHERE startsWith(s1, s1);
-SELECT COUNT() FROM startsWith_test WHERE startsWith(s1, s2);
-SELECT COUNT() FROM startsWith_test WHERE startsWith(s2, s3);
+SELECT COUNT() FROM startsWith_test WHERE startsWith(S1, S1);
+SELECT COUNT() FROM startsWith_test WHERE startsWith(S1, S2);
+SELECT COUNT() FROM startsWith_test WHERE startsWith(S2, S3);
 DROP TABLE startsWith_test;
