d1d20bee835888daf0639d8547d3c734ed543cfd
Alexey Milovidov <milovidov@yandex-team.ru>
2018-06-07 00:15:47 +0300

    Modified tests #2429

--- dbms/tests/queries/0_stateless/00639_startsWith.sql
+++ dbms/tests/queries/0_stateless/00639_startsWith.sql
@@ -1,3 +1,5 @@
+USE test;
+
 SELECT startsWith(s, 'He') FROM (SELECT arrayJoin(['', 'H', 'He', 'Hellow', '3434', 'fffffffffdHe']) AS s);
 SELECT startsWith(s, '') FROM (SELECT arrayJoin(['', 'h', 'hi']) AS s);
 SELECT startsWith('123', '123');
