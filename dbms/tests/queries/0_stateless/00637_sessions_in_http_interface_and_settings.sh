583b9af56f6a4e7c1ec91cbbd443a2a00dc06b6f
Alexey Milovidov <milovidov@yandex-team.ru>
2018-06-06 20:35:35 +0300

    Fixed error: user settings don't apply when using session_id parameter [#CLICKHOUSE-3763]

---
+++ dbms/tests/queries/0_stateless/00637_sessions_in_http_interface_and_settings.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+
+CURDIR=$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)
+. $CURDIR/../shell_config.sh
+
+user=readonly
+address=${CLICKHOUSE_HOST}
+port=${CLICKHOUSE_PORT_HTTP}
+url="${CLICKHOUSE_PORT_HTTP_PROTO}://readonly@$address:$port/?session_id=test"
+select="SELECT name, value, changed FROM system.settings WHERE name = 'readonly'"
+
+${CLICKHOUSE_CURL} -sS $url --data-binary "$select"
