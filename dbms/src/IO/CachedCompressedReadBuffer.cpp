a14089ac949dc871b2a401c8686e4b9439ea2f1e
Alexey Milovidov <milovidov@yandex-team.ru>
2018-06-12 06:25:54 +0300

    Fixed potential issue #1890

--- dbms/src/IO/CachedCompressedReadBuffer.cpp
+++ dbms/src/IO/CachedCompressedReadBuffer.cpp
@@ -48,20 +48,20 @@ bool CachedCompressedReadBuffer::nextImpl()
         if (owned_cell->compressed_size)
         {
             owned_cell->data.resize(size_decompressed + LZ4::ADDITIONAL_BYTES_AT_END_OF_BUFFER);
-            decompress(owned_cell->data.m_data, size_decompressed, size_compressed_without_checksum);
+            decompress(owned_cell->data.data(), size_decompressed, size_compressed_without_checksum);
 
             /// Put data into cache.
             cache->set(key, owned_cell);
         }
     }
 
-    if (owned_cell->data.m_size == 0)
+    if (owned_cell->data.size() == 0)
     {
         owned_cell = nullptr;
         return false;
     }
 
-    working_buffer = Buffer(owned_cell->data.m_data, owned_cell->data.m_data + owned_cell->data.m_size);
+    working_buffer = Buffer(owned_cell->data.data(), owned_cell->data.data() + owned_cell->data.size() - LZ4::ADDITIONAL_BYTES_AT_END_OF_BUFFER);
 
     file_pos += owned_cell->compressed_size;
 
