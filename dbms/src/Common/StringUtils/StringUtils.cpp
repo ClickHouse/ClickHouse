e4776e8b5d00fe6cd927ac09b056253291725fe9
Alexey Milovidov <milovidov@yandex-team.ru>
2018-06-16 05:31:41 +0300

    Fixed build (no include path was specified) #2513

--- dbms/src/Common/StringUtils/StringUtils.cpp
+++ dbms/src/Common/StringUtils/StringUtils.cpp
@@ -1,5 +1,5 @@
 #include "StringUtils.h"
-#include <boost/algorithm/string.hpp>
+
 
 namespace detail
 {
@@ -14,11 +14,4 @@ bool endsWith(const std::string & s, const char * suffix, size_t suffix_size)
     return s.size() >= suffix_size && 0 == memcmp(s.data() + s.size() - suffix_size, suffix, suffix_size);
 }
 
-}
-
-std::string trim(const std::string & str, const std::function<bool(char)> & predicate)
-{
-    std::string trimmed = str;
-    boost::trim_if(trimmed, predicate);
-    return trimmed;
 }
