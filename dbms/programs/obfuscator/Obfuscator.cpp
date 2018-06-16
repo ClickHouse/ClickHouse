3335a57b2233217040bdc18c9d2ab43ba756baed
Alexey Milovidov <milovidov@yandex-team.ru>
2018-06-16 04:44:25 +0300

    Data obfuscator: added documentation [#CLICKHOUSE-2]

--- dbms/programs/obfuscator/Obfuscator.cpp
+++ dbms/programs/obfuscator/Obfuscator.cpp
@@ -35,6 +35,51 @@
 #include <boost/algorithm/string.hpp>
 #include <boost/container/flat_map.hpp>
 
+#include <common/iostream_debug_helpers.h>
+
+
+static const char * documantation = R"(
+Simple tool for table data obfuscation.
+
+It reads input table and produces output table, that retain some properties of input, but contains different data.
+It allows to publish almost real production data for usage in benchmarks.
+
+It is designed to retain the following properties of data:
+- cardinalities of values (number of distinct values) for every column and for every tuple of columns;
+- conditional cardinalities: number of distinct values of one column under condition on value of another column;
+- probability distributions of absolute value of integers; sign of signed integers; exponent and sign for floats;
+- probability distributions of length of strings;
+- probability of zero values of numbers; empty strings and arrays, NULLs;
+- data compression ratio when compressed with LZ77 and entropy family of codecs;
+- continuouty (magnitude of difference) of time values across table; continuouty of floating point values.
+- date component of DateTime values;
+- UTF-8 validity of string values;
+- string values continue to look somewhat natural.
+
+Most of the properties above are viable for performance testing:
+- reading data, filtering, aggregation and sorting will work at almost the same speed
+  as on original data due to saved cardinalities, magnitudes, compression ratios, etc.
+
+It works in deterministic fashion: you define a seed value and transform is totally determined by input data and by seed.
+Some transforms are one to one and could be reversed, so you need to have large enough seed and keep it in secret.
+
+It use some cryptographic primitives to transform data, but from the cryptographic point of view,
+  it doesn't do anything properly and you should never consider the result as secure, unless you have other reasons for it.
+
+It may retain some data you don't want to publish.
+
+It always leave numbers 0, 1, -1 as is. Also it leaves dates, lengths of arrays and null flags exactly as in source data.
+For example, you have a column IsMobile in your table with values 0 and 1. In transformed data, it will have the same value.
+So, the user will be able to count exact ratio of mobile traffic.
+
+Another example, suppose you have some private data in your table, like user email and you don't want to publish any single email address.
+If your table is large enough and contain multiple different emails and there is no email that have very high frequency than all others,
+  it will perfectly anonymize all data. But if you have small amount of different values in a column, it can possibly reproduce some of them.
+And you should take care and look at exact algorithm, how this tool works, and probably fine tune some of it command line parameters.
+
+This tool works fine only with reasonable amount of data (at least 1000s of rows).
+)";
+
 
 namespace DB
 {
@@ -379,6 +424,7 @@ struct MarkovModelParameters
     size_t num_buckets_cutoff;
     size_t frequency_add;
     double frequency_desaturate;
+    size_t determinator_sliding_window_size;
 };
 
 
@@ -541,6 +587,8 @@ public:
             for (auto & elem : table)
             {
                 Histogram & histogram = elem.second;
+                if (!histogram.total)
+                    continue;
 
                 if (histogram.total + histogram.count_end < params.frequency_cutoff)
                 {
@@ -571,6 +619,8 @@ public:
             for (auto & elem : table)
             {
                 Histogram & histogram = elem.second;
+                if (!histogram.total)
+                    continue;
 
                 for (auto & bucket : histogram.buckets)
                     bucket.second += params.frequency_add;
@@ -585,13 +635,15 @@ public:
             for (auto & elem : table)
             {
                 Histogram & histogram = elem.second;
+                if (!histogram.total)
+                    continue;
 
                 double average = histogram.total / histogram.buckets.size();
 
                 UInt64 new_total = 0;
                 for (auto & bucket : histogram.buckets)
                 {
-                    bucket.second = bucket.second * (1 - params.frequency_desaturate) + average * params.frequency_desaturate;
+                    bucket.second = bucket.second * (1.0 - params.frequency_desaturate) + average * params.frequency_desaturate;
                     new_total += bucket.second;
                 }
 
@@ -629,7 +681,9 @@ public:
                 throw Exception("Logical error in markov model");
 
             size_t offset_from_begin_of_string = pos - data;
-            constexpr size_t determinator_sliding_window_size = 8;
+            size_t determinator_sliding_window_size = params.determinator_sliding_window_size;
+            if (determinator_sliding_window_size > determinator_size)
+                determinator_sliding_window_size = determinator_size;
 
             size_t determinator_sliding_window_overflow = offset_from_begin_of_string + determinator_sliding_window_size > determinator_size
                 ? offset_from_begin_of_string + determinator_sliding_window_size - determinator_size : 0;
@@ -894,6 +948,7 @@ try
         ("num-buckets-cutoff", po::value<UInt64>()->default_value(2), "cutoff for number of different possible continuations for a context: remove all histograms with less than specified number of buckets")
         ("frequency-add", po::value<UInt64>()->default_value(0), "add a constant to every count to lower probability distribution skew")
         ("frequency-desaturate", po::value<double>()->default_value(0), "0..1 - move every frequency towards average to lower probability distribution skew")
+        ("determinator-sliding-window-size", po::value<UInt64>()->default_value(8), "size of a sliding window in a source string - its hash is used as a seed for RNG in markov model")
         ;
 
     po::parsed_options parsed = po::command_line_parser(argc, argv).options(description).run();
@@ -906,7 +961,8 @@ try
         || !options.count("input-format")
         || !options.count("output-format"))
     {
-        std::cout << "Usage: " << argv[0] << " [options] < in > out\n"
+        std::cout << documantation << "\n"
+            << "\nUsage: " << argv[0] << " [options] < in > out\n"
             << "\nInput must be seekable file (it will be read twice).\n"
             << "\n" << description << "\n"
             << "\nExample:\n    " << argv[0] << " --seed \"$(head -c16 /dev/urandom)\" --input-format TSV --output-format TSV --structure 'CounterID UInt32, URLDomain String, URL String, SearchPhrase String, Title String' < stats.tsv\n";
@@ -932,6 +988,7 @@ try
     markov_model_params.num_buckets_cutoff = options["num-buckets-cutoff"].as<UInt64>();
     markov_model_params.frequency_add = options["frequency-add"].as<UInt64>();
     markov_model_params.frequency_desaturate = options["frequency-desaturate"].as<double>();
+    markov_model_params.determinator_sliding_window_size = options["determinator-sliding-window-size"].as<UInt64>();
 
     // Create header block
     std::vector<std::string> structure_vals;
