987644e1e7dea470b0b292e30aaa8115845385b9
Sergei Tsetlin (rekub) <rekub@yandex-team.ru>
2018-06-19 16:36:53 +0300

    shift arguments, some problem seems found!

--- dbms/tests/queries/0_stateless/00647_multiply_aggregation_state.sql
+++ dbms/tests/queries/0_stateless/00647_multiply_aggregation_state.sql
@@ -1,3 +1,6 @@
 SELECT countMerge(x) AS y FROM ( SELECT countState() * 2 AS x FROM ( SELECT 1 ));
 SELECT countMerge(x) AS y FROM ( SELECT countState() * 0 AS x FROM ( SELECT 1 UNION ALL SELECT 2));
 SELECT sumMerge(y) AS z FROM ( SELECT sumState(x) * 11 AS y FROM ( SELECT 1 AS x UNION ALL SELECT 2 AS x));
+SELECT countMerge(x) AS y FROM ( SELECT 2 * countState() AS x FROM ( SELECT 1 ));
+SELECT countMerge(x) AS y FROM ( SELECT 0 * countState() AS x FROM ( SELECT 1 UNION ALL SELECT 2));
+SELECT sumMerge(y) AS z FROM ( SELECT 3 * sumState(x) * 2 AS y FROM ( SELECT 1 AS x UNION ALL SELECT 2 AS x));
