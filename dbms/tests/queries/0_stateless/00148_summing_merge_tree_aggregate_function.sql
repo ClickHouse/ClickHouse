fff2291990f03f6385830abc6a2457bd62bdc9c3
Constantin S. Pan <kvapen@gmail.com>
2018-06-22 16:29:04 +0100

    Fix the test for AggregateFunction in SummingMergeTree

--- dbms/tests/queries/0_stateless/00148_summing_merge_tree_aggregate_function.sql
+++ dbms/tests/queries/0_stateless/00148_summing_merge_tree_aggregate_function.sql
@@ -2,9 +2,9 @@ drop table if exists test.summing_merge_tree_aggregate_function;
 
 create table test.summing_merge_tree_aggregate_function (d materialized today(), k UInt64, c UInt64, u AggregateFunction(uniq, UInt64)) engine=SummingMergeTree(d, k, 8192);
 
-insert into test.summing_merge_tree_aggregate_function select 1, 1, uniqState(UInt64(123));
-insert into test.summing_merge_tree_aggregate_function select 1, 1, uniqState(UInt64(456));
+insert into test.summing_merge_tree_aggregate_function select 1, 1, uniqState(toUInt64(123));
+insert into test.summing_merge_tree_aggregate_function select 1, 1, uniqState(toUInt64(456));
 optimize table test.summing_merge_tree_aggregate_function;
-select * from test.summing_merge_tree_aggregate_function;
+select k, sum(c), uniqMerge(u) from test.summing_merge_tree_aggregate_function group by k;
 
 drop table test.summing_merge_tree_aggregate_function;
