22909beed96a370d283a9caed9d47a23f30b794f
Alexey Zatelepin <ztlpn@yandex-team.ru>
2018-06-22 13:43:35 +0300

    add comments [#CLICKHOUSE-3747]

--- dbms/src/Storages/MergeTree/ActiveDataPartSet.h
+++ dbms/src/Storages/MergeTree/ActiveDataPartSet.h
@@ -43,6 +43,8 @@ public:
         return *this;
     }
 
+    /// Returns true if the part was actually added. If out_replaced_parts != nullptr, it will contain
+    /// parts that were replaced from the set by the newly added part.
     bool add(const String & name, Strings * out_replaced_parts = nullptr);
 
     bool remove(const MergeTreePartInfo & part_info)
