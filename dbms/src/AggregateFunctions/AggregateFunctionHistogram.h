b18a0e3ec0e539a5b2a57b845351db11afbde035
Alexey Milovidov <milovidov@yandex-team.ru>
2018-07-05 00:49:32 +0300

    Reviewing #2521

--- dbms/src/AggregateFunctions/AggregateFunctionHistogram.h
+++ dbms/src/AggregateFunctions/AggregateFunctionHistogram.h
@@ -38,51 +38,52 @@ public:
     using Weight = Float64;
 
 private:
-    struct WeightedValue {
+    struct WeightedValue
+    {
         Mean mean;
         Weight weight;
 
-        WeightedValue operator + (const WeightedValue& other)
+        WeightedValue operator+ (const WeightedValue & other)
         {
             return {mean + other.weight * (other.mean - mean) / (other.weight + weight), other.weight + weight};
         }
     };
 
-private:
-    UInt32 max_bins;
-
     // quantity of stored weighted-values
-    UInt32 size;
-
-    // calculated lower and upper bounds of seen points
-    Mean lower_bound;
-    Mean upper_bound;
-
-    static constexpr Mean epsilon = 1e-8;
+    UInt32 size = 0;
 
     // Weighted values representation of histogram.
     // We allow up to max_bins * 2 values stored in intermediate states
     // Is allocated in arena, so there is no explicit management.
-    WeightedValue* points;
+    WeightedValue * points = nullptr;
 
-private:
+    // calculated lower and upper bounds of seen points
+    Mean lower_bound = std::numeric_limits<Mean>::max();
+    Mean upper_bound = std::numeric_limits<Mean>::min();
+
+
+    static constexpr Mean epsilon = 1e-8;   /// TODO How this value was chosen?
+
+
     void sort()
     {
         std::sort(points, points + size,
-                [](const WeightedValue& first, const WeightedValue& second)
-                {
-                    return first.mean < second.mean;
-                });
+            [](const WeightedValue & first, const WeightedValue & second)
+            {
+                return first.mean < second.mean;
+            });
     }
 
     /**
      * Repeatedly fuse most close values until max_bins bins left
      */
-    void compress()
+    void compress(UInt32 max_bins)
     {
         sort();
-        auto newsz = size;
-        if (size <= max_bins) return;
+
+        auto new_size = size;
+        if (size <= max_bins)
+            return;
 
         // Maintain doubly-linked list of "active" points
         // and store neighbour pairs in priority queue by distance
@@ -90,17 +91,20 @@ private:
         AutoArray<UInt32> next(size + 1);
         AutoArray<bool> active(size + 1, true);
         active[size] = false;
+
         auto delete_node = [&](UInt32 i)
         {
             previous[next[i]] = previous[i];
             next[previous[i]] = next[i];
             active[i] = false;
         };
-        for (size_t i = 0; i <= size; i++)
+
+        for (size_t i = 0; i <= size; ++i)
         {
             previous[i] = i - 1;
             next[i] = i + 1;
         }
+
         next[size] = 0;
         previous[0] = size;
 
@@ -111,34 +115,41 @@ private:
         std::priority_queue<QueueItem, std::vector<QueueItem>, std::greater<QueueItem>> queue;
 
         auto quality = [&](UInt32 i) { return points[next[i]].mean - points[i].mean; };
-        for (size_t i = 0; i + 1 < size; i++)
+
+        for (size_t i = 0; i + 1 < size; ++i)
             queue.push({quality(i), i});
 
-        while (newsz > max_bins && !queue.empty())
+        while (new_size > max_bins && !queue.empty())
         {
             auto min_item = queue.top();
             queue.pop();
-            auto l = min_item.second;
-            auto r = next[l];
-            if (!active[l] || !active[r] || quality(l) > min_item.first)
+            auto left = min_item.second;
+            auto right = next[left];
+
+            if (!active[left] || !active[right] || quality(left) > min_item.first)
                 continue;
 
-            points[l] = points[l] + points[r];
+            points[left] = points[left] + points[right];
 
-            delete_node(r);
-            if (active[next[l]])
-                queue.push({quality(l), l});
-            if (active[previous[l]])
-                queue.push({quality(previous[l]), previous[l]});
+            delete_node(right);
+            if (active[next[left]])
+                queue.push({quality(left), left});
+            if (active[previous[left]])
+                queue.push({quality(previous[left]), previous[left]});
 
-            newsz--;
+            --new_size;
         }
 
-        size_t l = 0;
-        for (size_t r = 0; r < size; r++)
-            if (active[r])
-                points[l++] = points[r];
-        size = newsz;
+        size_t left = 0;
+        for (size_t right = 0; right < size; ++right)
+        {
+            if (active[right])
+            {
+                points[left] = points[right];
+                ++left;
+            }
+        }
+        size = new_size;
     }
 
     /***
@@ -147,44 +158,37 @@ private:
      */
     void unique()
     {
-        size_t l = 0;
-        for (auto r = l + 1; r < size; r++)
+        size_t left = 0;
+
+        for (auto r = left + 1; r < size; ++r)
         {
-            if (points[l].mean + epsilon >= points[r].mean)
+            if (points[left].mean + epsilon >= points[r].mean)
             {
-                points[l] = points[l] + points[r];
+                points[left] = points[left] + points[r];
             }
             else
             {
-                l++;
-                points[l] = points[r];
+                ++left;
+                points[left] = points[r];
             }
         }
-        size = l + 1;
+        size = left + 1;
     }
 
-    void init(Arena* arena)
+    void init(Arena * arena, UInt32 max_bins)
     {
-        points = reinterpret_cast<WeightedValue*>(arena->alloc(max_bins * 2 * sizeof(WeightedValue)));
+        points = reinterpret_cast<WeightedValue *>(arena->alloc(max_bins * 2 * sizeof(WeightedValue)));
     }
 
 public:
-    AggregateFunctionHistogramData(UInt32 max_bins)
-        : max_bins(max_bins)
-        , size(0)
-        , lower_bound(std::numeric_limits<Mean>::max())
-        , upper_bound(std::numeric_limits<Mean>::min())
-        , points(nullptr)
+    void insertResultInto(ColumnVector<Mean> & to_lower, ColumnVector<Mean> & to_upper, ColumnVector<Weight> & to_weights, UInt32 max_bins)
     {
-    }
-
-    void insertResultInto(ColumnVector<Mean>& to_lower, ColumnVector<Mean>& to_upper, ColumnVector<Weight>& to_weights) {
         if (!points) return;
 
-        compress();
+        compress(max_bins);
         unique();
 
-        for (size_t i = 0; i < size; i++)
+        for (size_t i = 0; i < size; ++i)
         {
             to_lower.insert((i == 0) ? lower_bound : (points[i].mean + points[i - 1].mean) / 2);
             to_upper.insert((i + 1 == size) ? upper_bound : (points[i].mean + points[i + 1].mean) / 2);
@@ -196,28 +200,29 @@ public:
         }
     }
 
-    void add(Mean value, Weight weight, Arena* arena)
+    void add(Mean value, Weight weight, Arena * arena, UInt32 max_bins)
     {
         if (!points)
-            init(arena);
-        points[size++] = {value, weight};
+            init(arena, max_bins);
+
+        points[size] = {value, weight};
+        ++size;
+
         lower_bound = std::min(lower_bound, value);
         upper_bound = std::max(upper_bound, value);
 
         if (size >= max_bins * 2)
-        {
-            compress();
-        }
+            compress(max_bins);
     }
 
-    void merge(const AggregateFunctionHistogramData& other, Arena* arena)
+    void merge(const AggregateFunctionHistogramData & other, Arena * arena, UInt32 max_bins)
     {
         lower_bound = std::min(lower_bound, other.lower_bound);
         upper_bound = std::max(lower_bound, other.upper_bound);
-        for (size_t i = 0; i < other.size; i++)
-        {
-            add(other.points[i].mean, other.points[i].weight, arena);
-        }
+
+        /// TODO possibly inefficient
+        for (size_t i = 0; i < other.size; ++i)
+            add(other.points[i].mean, other.points[i].weight, arena, max_bins);
     }
 
     void write(WriteBuffer & buf) const
@@ -229,7 +234,7 @@ public:
         buf.write(reinterpret_cast<const char *>(points), size * sizeof(WeightedValue));
     }
 
-    void read(ReadBuffer & buf, Arena* arena)
+    void read(ReadBuffer & buf, Arena * arena, UInt32 max_bins)
     {
         buf.read(reinterpret_cast<char *>(&lower_bound), sizeof(lower_bound));
         buf.read(reinterpret_cast<char *>(&upper_bound), sizeof(upper_bound));
@@ -240,60 +245,25 @@ public:
             throw Exception("Too many bins", ErrorCodes::TOO_LARGE_ARRAY_SIZE);
 
         if (!points)
-            init(arena);
+            init(arena, max_bins);
 
         buf.read(reinterpret_cast<char *>(points), size * sizeof(points[0]));
     }
-
 };
 
+
 template <typename T>
-class AggregateFunctionHistogram final: public IAggregateFunctionHelper<AggregateFunctionHistogram<T>>
+class AggregateFunctionHistogram final: public IAggregateFunctionDataHelper<AggregateFunctionHistogramData, AggregateFunctionHistogram<T>>
 {
 private:
     using Data = AggregateFunctionHistogramData;
 
     UInt32 max_bins;
 
-    Data& data(AggregateDataPtr place) const
-    {
-        return *reinterpret_cast<Data*>(place);
-    }
-
-    const Data& data(ConstAggregateDataPtr place) const
-    {
-        return *reinterpret_cast<const Data*>(place);
-    }
-
 public:
     AggregateFunctionHistogram(UInt32 max_bins)
         : max_bins(max_bins)
     {
-    }
-
-    void destroy(AggregateDataPtr place) const noexcept override
-    {
-        data(place).~Data();
-    }
-
-    bool hasTrivialDestructor() const override
-    {
-        return std::is_trivially_destructible_v<Data>;
-    }
-
-    size_t alignOfData() const override
-    {
-        return alignof(Data);
-    }
-
-    size_t sizeOfData() const override
-    {
-        return sizeof(Data);
-    }
-
-    void create(AggregateDataPtr place) const override
-    {
-        new (place) Data(max_bins);
     }
 
     DataTypePtr getReturnType() const override
@@ -321,12 +291,12 @@ public:
     void add(AggregateDataPtr place, const IColumn ** columns, size_t row_num, Arena * arena) const override
     {
         auto val = static_cast<const ColumnVector<T> &>(*columns[0]).getData()[row_num];
-        this->data(place).add(static_cast<Data::Mean>(val), 1, arena);
+        this->data(place).add(static_cast<Data::Mean>(val), 1, arena, max_bins);
     }
 
     void merge(AggregateDataPtr place, ConstAggregateDataPtr rhs, Arena * arena) const override
     {
-        this->data(place).merge(this->data(rhs), arena);
+        this->data(place).merge(this->data(rhs), arena, max_bins);
     }
 
     void serialize(ConstAggregateDataPtr place, WriteBuffer & buf) const override
@@ -336,12 +306,12 @@ public:
 
     void deserialize(AggregateDataPtr place, ReadBuffer & buf, Arena * arena) const override
     {
-        this->data(place).read(buf, arena);
+        this->data(place).read(buf, arena, max_bins);
     }
 
     void insertResultInto(ConstAggregateDataPtr place, IColumn & to) const override
     {
-        auto& data = this->data(const_cast<AggregateDataPtr>(place));
+        auto & data = this->data(const_cast<AggregateDataPtr>(place));
 
         auto & to_array = static_cast<ColumnArray &>(to);
         ColumnArray::Offsets & offsets_to = to_array.getOffsets();
@@ -350,7 +320,7 @@ public:
         auto & to_lower = static_cast<ColumnVector<Data::Mean> &>(to_tuple.getColumn(0));
         auto & to_upper = static_cast<ColumnVector<Data::Mean> &>(to_tuple.getColumn(1));
         auto & to_weights = static_cast<ColumnVector<Data::Weight> &>(to_tuple.getColumn(2));
-        data.insertResultInto(to_lower, to_upper, to_weights);
+        data.insertResultInto(to_lower, to_upper, to_weights, max_bins);
 
         offsets_to.push_back(to_tuple.size());
     }
