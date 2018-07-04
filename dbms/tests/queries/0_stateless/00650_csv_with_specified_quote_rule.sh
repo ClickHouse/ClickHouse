2a4bb430007f33e285d7874116eff0b5879b3909
Alexey Milovidov <milovidov@yandex-team.ru>
2018-07-05 00:15:29 +0300

    Adaptations #2574

--- dbms/tests/queries/0_stateless/00650_csv_with_specified_quote_rule.sh
+++ dbms/tests/queries/0_stateless/00650_csv_with_specified_quote_rule.sh
@@ -8,7 +8,7 @@ $CLICKHOUSE_CLIENT --query="DROP TABLE IF EXISTS test.csv";
 $CLICKHOUSE_CLIENT --query="CREATE TABLE test.csv (s String, n UInt64, d Date) ENGINE = Memory";
 
 echo "'single quote' not end, 123, 2016-01-01
-'em good, 456, 2016-01-02" | $CLICKHOUSE_CLIENT --format_csv_single_quote=0 --query="INSERT INTO test.csv FORMAT CSV";
+'em good, 456, 2016-01-02" | $CLICKHOUSE_CLIENT --format_csv_allow_single_quotes=0 --query="INSERT INTO test.csv FORMAT CSV";
 
 $CLICKHOUSE_CLIENT --query="SELECT * FROM test.csv ORDER BY d";
 
@@ -17,7 +17,7 @@ $CLICKHOUSE_CLIENT --query="DROP TABLE test.csv";
 $CLICKHOUSE_CLIENT --query="CREATE TABLE test.csv (s String, n UInt64, d Date) ENGINE = Memory";
 
 echo "'single quote' not end, 123, 2016-01-01
-'em good, 456, 2016-01-02" | $CLICKHOUSE_CLIENT --multiquery --query="SET format_csv_single_quote=0; INSERT INTO test.csv FORMAT CSV";
+'em good, 456, 2016-01-02" | $CLICKHOUSE_CLIENT --multiquery --query="SET format_csv_allow_single_quotes=0; INSERT INTO test.csv FORMAT CSV";
 
 $CLICKHOUSE_CLIENT --query="SELECT * FROM test.csv ORDER BY d";
 
@@ -28,7 +28,7 @@ $CLICKHOUSE_CLIENT --query="DROP TABLE IF EXISTS test.csv";
 $CLICKHOUSE_CLIENT --query="CREATE TABLE test.csv (s String, n UInt64, d Date) ENGINE = Memory";
 
 echo '"double quote" not end, 123, 2016-01-01
-"em good, 456, 2016-01-02' | $CLICKHOUSE_CLIENT --format_csv_double_quote=0 --query="INSERT INTO test.csv FORMAT CSV";
+"em good, 456, 2016-01-02' | $CLICKHOUSE_CLIENT --format_csv_allow_double_quotes=0 --query="INSERT INTO test.csv FORMAT CSV";
 
 $CLICKHOUSE_CLIENT --query="SELECT * FROM test.csv ORDER BY d";
 
@@ -37,7 +37,7 @@ $CLICKHOUSE_CLIENT --query="DROP TABLE test.csv";
 $CLICKHOUSE_CLIENT --query="CREATE TABLE test.csv (s String, n UInt64, d Date) ENGINE = Memory";
 
 echo '"double quote" not end, 123, 2016-01-01
-"em good, 456, 2016-01-02' | $CLICKHOUSE_CLIENT --multiquery --query="SET format_csv_double_quote=0; INSERT INTO test.csv FORMAT CSV";
+"em good, 456, 2016-01-02' | $CLICKHOUSE_CLIENT --multiquery --query="SET format_csv_allow_double_quotes=0; INSERT INTO test.csv FORMAT CSV";
 
 $CLICKHOUSE_CLIENT --query="SELECT * FROM test.csv ORDER BY d";
 
