3385bacb201e98b9b827c3098dd73bb03dd38245
Alexey Milovidov <milovidov@yandex-team.ru>
2018-07-05 01:37:48 +0300

    Make the test results more stable with respect to floating point calculations method #2521

--- dbms/tests/queries/0_stateless/00647_histogram.sql
+++ dbms/tests/queries/0_stateless/00647_histogram.sql
@@ -1,6 +1,7 @@
 select histogram(5)(number-10) from (select * from system.numbers limit 20);
 select histogram(5)(number) from (select * from system.numbers limit 20);
-select histogram(3)(sin(number)) from (select * from system.numbers limit 10);
-select histogram(1)(sin(number-40)) from (select * from system.numbers limit 80);
+
+WITH arrayJoin(histogram(3)(sin(number))) AS res select round(res.1, 2), round(res.2, 2), round(res.3, 2) from (select * from system.numbers limit 10);
+WITH arrayJoin(histogram(1)(sin(number-40))) AS res SELECT round(res.1, 2), round(res.2, 2), round(res.3, 2) from (select * from system.numbers limit 80);
 
 SELECT histogram(10)(-2);
