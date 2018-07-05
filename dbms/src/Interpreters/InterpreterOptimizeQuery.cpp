c28f201e2d8e08bdf79e9038dc6066309aa583aa
Amos Bird <amosbird@gmail.com>
2018-07-06 01:32:14 +0800

    Make OPTIMIZE be able to merge all parts with FINAL.

--- dbms/src/Interpreters/InterpreterOptimizeQuery.cpp
+++ dbms/src/Interpreters/InterpreterOptimizeQuery.cpp
@@ -18,9 +18,6 @@ BlockIO InterpreterOptimizeQuery::execute()
 {
     const ASTOptimizeQuery & ast = typeid_cast<const ASTOptimizeQuery &>(*query_ptr);
 
-    if (ast.final && !ast.partition)
-        throw Exception("FINAL flag for OPTIMIZE query is meaningful only with specified PARTITION", ErrorCodes::BAD_ARGUMENTS);
-
     StoragePtr table = context.getTable(ast.database, ast.table);
     auto table_lock = table->lockStructure(true, __PRETTY_FUNCTION__);
     table->optimize(query_ptr, ast.partition, ast.final, ast.deduplicate, context);
