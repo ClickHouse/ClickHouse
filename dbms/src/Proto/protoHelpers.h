bac1f77620492b5096600479972cba42257db743
chertus <chertus@yandex-team.ru>
2018-07-10 14:40:33 +0300

    insert defaults (first working version)

--- dbms/src/Proto/protoHelpers.h
+++ dbms/src/Proto/protoHelpers.h
@@ -1,11 +1,23 @@
 #pragma once
 
+#include <Storages/ColumnDefault.h>
 
 namespace DB
 {
     class Context;
     class Block;
 
+    struct TableMetaInfo
+    {
+        TableMetaInfo(const String & database_, const String & table_)
+            : database(database_), table(table_)
+        {}
+
+        const String & database;
+        const String & table;
+        ColumnDefaults column_defaults;
+    };
+
     Block storeContextBlock(Context & context);
-    void loadContextBlock(const Block & block, Context & context);
+    void loadTableMetaInfo(const Block & block, TableMetaInfo & table_meta);
 }
