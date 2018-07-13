2275ef046203c9d81435eee912d478668e86362a
chertus <chertus@yandex-team.ru>
2018-07-13 21:06:47 +0300

    fix inserts into unknown table, ex. 'table function url(...)'

--- dbms/src/Storages/TableMetadata.cpp
+++ dbms/src/Storages/TableMetadata.cpp
@@ -7,6 +7,9 @@ namespace DB
 {
     void TableMetadata::loadFromContext(const Context & context)
     {
+        if (!context.isTableExist(database, table))
+            return;
+
         StoragePtr storage = context.getTable(database, table);
         const ColumnsDescription & table_columns = storage->getColumns();
         column_defaults = table_columns.defaults;
