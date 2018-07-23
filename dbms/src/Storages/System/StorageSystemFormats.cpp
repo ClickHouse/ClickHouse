220f4d6c43fd0f3d23083541c60778c7668de3c4
alesapin <alesapin@gmail.com>
2018-07-23 20:12:29 +0300

    CLICKHOUSE-3772: Fix strange initialization

--- dbms/src/Storages/System/StorageSystemFormats.cpp
+++ dbms/src/Storages/System/StorageSystemFormats.cpp
@@ -9,8 +9,8 @@ void StorageSystemFormats::fillData(MutableColumns & res_columns) const
     for (const auto & pair : formats)
     {
         const auto & [name, creator_pair] = pair;
-        UInt64 has_input_format = UInt64(creator_pair.first != nullptr);
-        UInt64 has_output_format = UInt64(creator_pair.second != nullptr);
+        UInt64 has_input_format(creator_pair.first != nullptr);
+        UInt64 has_output_format(creator_pair.second != nullptr);
         res_columns[0]->insert(name);
         res_columns[1]->insert(has_input_format);
         res_columns[2]->insert(has_output_format);
