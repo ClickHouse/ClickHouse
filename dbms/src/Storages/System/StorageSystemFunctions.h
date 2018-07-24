ee6eddb1594fdc548924de38648453af2ad1ee49
alexey-milovidov <milovidov@yandex-team.ru>
2018-07-24 21:39:21 +0300

    Update StorageSystemFunctions.h

--- dbms/src/Storages/System/StorageSystemFunctions.h
+++ dbms/src/Storages/System/StorageSystemFunctions.h
@@ -13,7 +13,7 @@ class Context;
 /** Implements `functions`system table, which allows you to get a list
   * all normal and aggregate functions.
   */
- class StorageSystemFunctions : public ext::shared_ptr_helper<StorageSystemFunctions>, public IStorageSystemOneBlock<StorageSystemFunctions>
+class StorageSystemFunctions : public ext::shared_ptr_helper<StorageSystemFunctions>, public IStorageSystemOneBlock<StorageSystemFunctions>
 {
 public:
     std::string getName() const override { return "SystemFunctions"; }
