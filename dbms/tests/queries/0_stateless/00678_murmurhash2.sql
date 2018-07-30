c4acfb882161b0a35fbc1bb6f6fca27c6410ef9e
Alexey Milovidov <milovidov@yandex-team.ru>
2018-07-30 20:43:13 +0300

    Fixed test #2756

--- dbms/tests/queries/0_stateless/00678_murmurhash2.sql
+++ dbms/tests/queries/0_stateless/00678_murmurhash2.sql
@@ -1,9 +1,9 @@
-SELECT murmurHash2(123456);
-SELECT murmurHash2(CAST(3 AS UInt8));
-SELECT murmurHash2(CAST(1.2684 AS Float32));
-SELECT murmurHash2(CAST(-154477 AS Int64));
-SELECT murmurHash2('foo');
-SELECT murmurHash2(CAST('bar' AS FixedString(3)));
-SELECT murmurHash2(x) FROM (SELECT CAST(1 AS Enum8('a' = 1, 'b' = 2)) as x);
+SELECT murmurHash2_32(123456);
+SELECT murmurHash2_32(CAST(3 AS UInt8));
+SELECT murmurHash2_32(CAST(1.2684 AS Float32));
+SELECT murmurHash2_32(CAST(-154477 AS Int64));
+SELECT murmurHash2_32('foo');
+SELECT murmurHash2_32(CAST('bar' AS FixedString(3)));
+SELECT murmurHash2_32(x) FROM (SELECT CAST(1 AS Enum8('a' = 1, 'b' = 2)) as x);
 
 
