d856ad2d6ef10e6a54cf0352eba3ddc7c65940bb
Alexey Milovidov <milovidov@yandex-team.ru>
2018-08-01 00:24:49 +0300

    More tests [#CLICKHOUSE-3861]

--- dbms/src/Dictionaries/tests/validate-odbc-connection-string.sh
+++ dbms/src/Dictionaries/tests/validate-odbc-connection-string.sh
@@ -36,3 +36,6 @@
 ./validate-odbc-connection-string 'abc={  } ; dsn = {hello world}  ;_= {...}; Driver=x' 2>&1
 ./validate-odbc-connection-string 'abc={}; abc=def' 2>&1
 ./validate-odbc-connection-string 'abc={};;' 2>&1
+./validate-odbc-connection-string 'DSN=myconnection' 2>&1
+./validate-odbc-connection-string 'DSN=myconnection;UID=username;PWD=password;HOST=127.0.0.1;PORT=5432;DATABASE=my_db' 2>&1
+./validate-odbc-connection-string 'DSN=MSSQL;UID=test;PWD=test' 2>&1
