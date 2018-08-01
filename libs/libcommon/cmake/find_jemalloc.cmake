5815819447585b4a38d718e69caa001223686779
Alexey Milovidov <milovidov@yandex-team.ru>
2018-08-01 09:03:54 +0300

    Add support for jemalloc: development [#CLICKHOUSE-2]

--- libs/libcommon/cmake/find_jemalloc.cmake
+++ libs/libcommon/cmake/find_jemalloc.cmake
@@ -3,17 +3,16 @@ option (USE_INTERNAL_JEMALLOC_LIBRARY "Set to FALSE to use system jemalloc libra
 
 if (ENABLE_JEMALLOC)
     if (USE_INTERNAL_JEMALLOC_LIBRARY)
-        set (JEMALLOC_INCLUDE_DIR "${ClickHouse_SOURCE_DIR}/contrib/jemalloc/include" "${ClickHouse_BINARY_DIR}/contrib/jemalloc/include")
         set (JEMALLOC_LIBRARIES "jemalloc")
     else ()
         find_package (JeMalloc)
     endif ()
 
-    if (JEMALLOC_INCLUDE_DIR AND JEMALLOC_LIBRARIES)
+    if (JEMALLOC_LIBRARIES)
         set (USE_JEMALLOC 1)
     else ()
         message (FATAL_ERROR "ENABLE_JEMALLOC is set to true, but library was not found")
     endif ()
 
-    message (STATUS "Using jemalloc=${USE_JEMALLOC}: ${JEMALLOC_INCLUDE_DIR} : ${JEMALLOC_LIBRARIES}")
+    message (STATUS "Using jemalloc=${USE_JEMALLOC}: ${JEMALLOC_LIBRARIES}")
 endif ()
