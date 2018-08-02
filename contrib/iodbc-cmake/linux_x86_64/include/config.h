ff91b606ecf5402a8ee8e7a29fb849c4c7ff4fea
Alexey Milovidov <milovidov@yandex-team.ru>
2018-08-02 05:13:00 +0300

    Using internal iodbc library [#CLICKHOUSE-2]

---
+++ contrib/iodbc-cmake/linux_x86_64/include/config.h
@@ -0,0 +1,158 @@
+/* include/config.h.  Generated from config.h.in by configure.  */
+/* include/config.h.in.  Generated from configure.ac by autoheader.  */
+
+/* Default location for FILEDSN data sources */
+#define DEFAULT_FILEDSNPATH "/etc/ODBCDataSources"
+
+/* Define if we detected a GUI library we can use */
+/* #undef GUI */
+
+/* Define to 1 if you have the `asprintf' function. */
+#define HAVE_ASPRINTF 1
+
+/* Define to 1 if you have the `dladdr' function */
+#define HAVE_DLADDR 1
+
+/* Define if you have the -ldld library */
+/* #undef HAVE_DLD */
+
+/* Define to 1 if you have the <dld.h> header file. */
+/* #undef HAVE_DLD_H */
+
+/* Define to 1 if you have the <dlfcn.h> header file. */
+#define HAVE_DLFCN_H 1
+
+/* Define to 1 if you have the `dlopen' function. */
+/* #undef HAVE_DLOPEN */
+
+/* Define to 1 if you have the <dl.h> header file. */
+/* #undef HAVE_DL_H */
+
+/* Define to 1 if you have the Dl_info structure */
+/* #undef HAVE_DL_INFO */
+
+/* Define if you have the DYLD library */
+/* #undef HAVE_DYLD */
+
+/* Define to 1 if you have the `gettimeofday' function. */
+#define HAVE_GETTIMEOFDAY 1
+
+/* Define to 1 if you have the <inttypes.h> header file. */
+#define HAVE_INTTYPES_H 1
+
+/* Define if you have the -ldl library */
+#define HAVE_LIBDL 1
+
+/* Define to 1 if you have the `localtime_r' function. */
+#define HAVE_LOCALTIME_R 1
+
+/* Define to 1 if you have the <memory.h> header file. */
+#define HAVE_MEMORY_H 1
+
+/* Define to 1 if you have the `setenv' function. */
+#define HAVE_SETENV 1
+
+/* Define if you have the shl_load function */
+/* #undef HAVE_SHL_LOAD */
+
+/* Define to 1 if you have the `snprintf' function. */
+#define HAVE_SNPRINTF 1
+
+/* Define to 1 if you have the <stdint.h> header file. */
+#define HAVE_STDINT_H 1
+
+/* Define to 1 if you have the <stdlib.h> header file. */
+#define HAVE_STDLIB_H 1
+
+/* Define to 1 if you have the `strerror' function. */
+#define HAVE_STRERROR 1
+
+/* Define to 1 if you have the <strings.h> header file. */
+#define HAVE_STRINGS_H 1
+
+/* Define to 1 if you have the <string.h> header file. */
+#define HAVE_STRING_H 1
+
+/* Define to 1 if you have the <sys/stat.h> header file. */
+#define HAVE_SYS_STAT_H 1
+
+/* Define to 1 if you have the <sys/types.h> header file. */
+#define HAVE_SYS_TYPES_H 1
+
+/* Define to 1 if you have the `towlower' function. */
+#define HAVE_TOWLOWER 1
+
+/* Define to 1 if you have the <unistd.h> header file. */
+#define HAVE_UNISTD_H 1
+
+/* Define to 1 if you have the <wchar.h> header file. */
+#define HAVE_WCHAR_H 1
+
+/* Define to 1 if you have the `wcscat' function. */
+#define HAVE_WCSCAT 1
+
+/* Define to 1 if you have the `wcschr' function. */
+#define HAVE_WCSCHR 1
+
+/* Define to 1 if you have the `wcscmp' function. */
+#define HAVE_WCSCMP 1
+
+/* Define to 1 if you have the `wcscpy' function. */
+#define HAVE_WCSCPY 1
+
+/* Define to 1 if you have the `wcslen' function. */
+#define HAVE_WCSLEN 1
+
+/* Define to 1 if you have the `wcsncasecmp' function. */
+#define HAVE_WCSNCASECMP 1
+
+/* Define to 1 if you have the `wcsncpy' function. */
+#define HAVE_WCSNCPY 1
+
+/* Define to 1 if you have the `_dlopen' function. */
+/* #undef HAVE__DLOPEN */
+
+/* Define to the sub-directory where libtool stores uninstalled libraries. */
+#define LT_OBJDIR ".libs/"
+
+/* Define if dlsym() requires a leading underscore in symbol names. */
+/* #undef NEED_USCORE */
+
+/* Name of package */
+#define PACKAGE "libiodbc"
+
+/* Define to the address where bug reports for this package should be sent. */
+#define PACKAGE_BUGREPORT "iodbc@openlinksw.com"
+
+/* Define to the full name of this package. */
+#define PACKAGE_NAME "iODBC Driver Manager"
+
+/* Define to the full name and version of this package. */
+#define PACKAGE_STRING "iODBC Driver Manager 3.52.12"
+
+/* Define to the one symbol short name of this package. */
+#define PACKAGE_TARNAME "libiodbc"
+
+/* Define to the home page for this package. */
+#define PACKAGE_URL ""
+
+/* Define to the version of this package. */
+#define PACKAGE_VERSION "3.52.12"
+
+/* Define to 1 if you have the ANSI C header files. */
+#define STDC_HEADERS 1
+
+/* Define path to systemwide odbcinst.ini file */
+#define SYS_ODBCINST_INI "/etc/odbcinst.ini"
+
+/* Define path to systemwide odbc.ini file */
+#define SYS_ODBC_INI "/etc/odbc.ini"
+
+/* Define to 1 if you can safely include both <sys/time.h> and <time.h>. */
+#define TIME_WITH_SYS_TIME 1
+
+/* Version number of package */
+#define VERSION "3.52.12"
+
+/* Define to empty if `const' does not conform to ANSI C. */
+/* #undef const */
