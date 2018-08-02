cf23c4a9930d4270eb310d47c07ad5ab0802125b
Alexey Milovidov <milovidov@yandex-team.ru>
2018-08-02 11:08:17 +0300

    Using unixodbc as submodule [#CLICKHOUSE-3]

--- ci/install-libraries.sh
+++ ci/install-libraries.sh
@@ -6,7 +6,6 @@ source default-config
 ./install-os-packages.sh libssl-dev
 ./install-os-packages.sh libicu-dev
 ./install-os-packages.sh libreadline-dev
-./install-os-packages.sh libunixodbc-dev
 
 if [[ "$ENABLE_EMBEDDED_COMPILER" == 1 && "$USE_LLVM_LIBRARIES_FROM_SYSTEM" == 1 ]]; then
     ./install-os-packages.sh llvm-libs-5.0
