47554229c0802b883087b00d352445ffce30168a
Alexey Milovidov <milovidov@yandex-team.ru>
2018-08-05 08:25:37 +0300

    Updated tests #2094

--- dbms/tests/queries/0_stateless/00225_join_duplicate_columns.sql
+++
@@ -1,9 +0,0 @@
-select b from (select 1 as a, 42 as c) any left join (select 2 as b, 2 as b, 41 as c) using c;
-select b from (select 1 as a, 42 as c) any left join (select 2 as b, 2 as b, 42 as c) using c;
-
-select c,a,a,b,b from
-  (select 1 as a, 1 as a, 42 as c group by c order by a,c)
- any left join
-  (select 2 as b, 2 as b, 41 as c group by c order by b,c)
- using c
- order by b;
