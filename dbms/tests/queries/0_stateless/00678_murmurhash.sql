2cca555993597955d0b649e39489281a98e0f004
Anton Popov <pad11rus@gmail.com>
2018-08-06 23:53:28 +0300

    Update 00678_murmurhash.sql

--- dbms/tests/queries/0_stateless/00678_murmurhash.sql
+++ dbms/tests/queries/0_stateless/00678_murmurhash.sql
@@ -27,3 +27,6 @@ SELECT murmurHash3_32(1);
 SELECT murmurHash3_64('foo');
 SELECT murmurHash3_64('\x01');
 SELECT murmurHash3_64(1);
+
+SELECT hex(murmurHash3_128('foo'));
+SELECT hex(murmurHash3_128('\x01'));
