a6cff9c5a55e94525348b007c849c13df94d9d50
alesapin <alesapin@gmail.com>
2018-08-08 19:15:29 +0300

    CLICKHOUSE-3878: Better odbc-bridge with ability to handle custom logs and exceptions

--- dbms/programs/odbc-bridge/Handlers.h
+++ dbms/programs/odbc-bridge/Handlers.h
@@ -3,7 +3,6 @@
 #include <Interpreters/Context.h>
 #include <Poco/Logger.h>
 #include <Poco/Net/HTTPRequestHandler.h>
-#include <Poco/Net/HTTPRequestHandlerFactory.h>
 
 #pragma GCC diagnostic push
 #pragma GCC diagnostic ignored "-Wunused-parameter"
@@ -12,15 +11,15 @@
 
 namespace DB
 {
-class ODBCHTTPHandler : public Poco::Net::HTTPRequestHandler
+class ODBCHandler : public Poco::Net::HTTPRequestHandler
 {
 public:
-    ODBCHTTPHandler(std::shared_ptr<Poco::Data::SessionPool> pool_,
+    ODBCHandler(std::shared_ptr<Poco::Data::SessionPool> pool_,
         const std::string & format_,
         size_t max_block_size_,
         size_t keep_alive_timeout_,
         std::shared_ptr<Context> context_)
-        : log(&Poco::Logger::get("ODBCHTTPHandler"))
+        : log(&Poco::Logger::get("ODBCHandler"))
         , pool(pool_)
         , format(format_)
         , max_block_size(max_block_size_)
@@ -48,23 +47,5 @@ public:
 
 private:
     size_t keep_alive_timeout;
-};
-
-class ODBCRequestHandlerFactory : public Poco::Net::HTTPRequestHandlerFactory
-{
-public:
-    ODBCRequestHandlerFactory(const std::string & name_, size_t keep_alive_timeout_, std::shared_ptr<Context> context_)
-        : log(&Poco::Logger::get(name_)), name(name_), keep_alive_timeout(keep_alive_timeout_), context(context_)
-    {
-    }
-
-    Poco::Net::HTTPRequestHandler * createRequestHandler(const Poco::Net::HTTPServerRequest & request) override;
-
-private:
-    Poco::Logger * log;
-    std::string name;
-    size_t keep_alive_timeout;
-    std::shared_ptr<Context> context;
-    std::unordered_map<std::string, std::shared_ptr<Poco::Data::SessionPool>> pool_map;
 };
 }
