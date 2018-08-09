3e285ce73b446c3fc6735567156d586778bc9188
Nikolai Kochetov <nik-kochetov@yandex-team.ru>
2018-08-09 13:48:19 +0300

    Fixed DataTypeWithDictionary serialization.

--- dbms/tests/queries/0_stateless/00688_low_cardinality_serialization.sql
+++ dbms/tests/queries/0_stateless/00688_low_cardinality_serialization.sql
@@ -12,9 +12,15 @@ create table test.lc_small_dict (str StringWithDictionary) engine = MergeTree or
 create table test.lc_big_dict (str StringWithDictionary) engine = MergeTree order by str;
 
 insert into test.lc_small_dict select toString(number % 1000) from system.numbers limit 1000000;
-select sum(toUInt64OrZero(str)) from test.lc_small_dict;
-
 insert into test.lc_big_dict select toString(number) from system.numbers limit 1000000;
+
+detach table test.lc_small_dict;
+detach table test.lc_big_dict;
+
+attach table test.lc_small_dict;
+attach table test.lc_big_dict;
+
+select sum(toUInt64OrZero(str)) from test.lc_small_dict;
 select sum(toUInt64OrZero(str)) from test.lc_big_dict;
 
 drop table if exists test.lc_small_dict;
