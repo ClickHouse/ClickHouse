2da8140977bafff316d7063c1deec56677897c2a
proller <proller@github.com>
2018-08-09 18:29:43 +0300

    Fix comment

--- dbms/src/Formats/ODBCDriver2BlockOutputStream.h
+++ dbms/src/Formats/ODBCDriver2BlockOutputStream.h
@@ -15,7 +15,7 @@ class WriteBuffer;
   * ODBC driver is designed to be build for different platforms without dependencies from the main code,
   *  so the format is made that way so that it can be as easy as possible to parse it.
   * A header is displayed with the required information.
-  * The data is then output in the order of the rows. Each value is displayed as follows: length in VarUInt format, then data in text form.
+  * The data is then output in the order of the rows. Each value is displayed as follows: length in Int32 format (-1 for NULL), then data in text form.
   */
 class ODBCDriver2BlockOutputStream : public IBlockOutputStream
 {
