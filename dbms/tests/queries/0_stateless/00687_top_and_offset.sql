4e5aaaa9f1f858410bc1c23a1ca95ce95de9d218
Vadim <plahtinskiy.ve@phystech.edu>
2018-08-13 13:47:38 +0300

    Update 00687_top_and_offset.sql

--- dbms/tests/queries/0_stateless/00687_top_and_offset.sql
+++ dbms/tests/queries/0_stateless/00687_top_and_offset.sql
@@ -15,5 +15,6 @@ INSERT INTO test.test VALUES (9);
 SELECT TOP 2 * FROM test.test;
 SELECT TOP (2) * FROM test.test;
 SELECT * FROM test.test LIMIT 2 OFFSET 2;
+SELECT TOP 2 * FROM test.test LIMIT 2; -- { clientError 404 } 
 
 DROP TABLE test.test;
