cf363ab96783a2eadf523d81d5734c25f3007c84
Alexey Milovidov <milovidov@yandex-team.ru>
2018-08-21 02:39:41 +0300

    Addition to prev. revision #2877

--- dbms/src/Common/ErrorCodes.cpp
+++ dbms/src/Common/ErrorCodes.cpp
@@ -388,6 +388,7 @@ namespace ErrorCodes
     extern const int PTHREAD_ERROR = 411;
     extern const int NETLINK_ERROR = 412;
     extern const int CANNOT_SET_SIGNAL_HANDLER = 413;
+    extern const int CANNOT_READLINE = 414;
 
     extern const int KEEPER_EXCEPTION = 999;
     extern const int POCO_EXCEPTION = 1000;
