5f7117e6a27f4b094a23c363f8cb203d75bbffbb
Vadim <plahtinskiy.ve@phystech.edu>
2018-08-21 17:32:28 +0300

    Update MergeTreeDataSelectExecutor.cpp

--- dbms/src/Storages/MergeTree/MergeTreeDataSelectExecutor.cpp
+++ dbms/src/Storages/MergeTree/MergeTreeDataSelectExecutor.cpp
@@ -135,7 +135,6 @@ BlockInputStreams MergeTreeDataSelectExecutor::read(
     const Names & column_names_to_return,
     const SelectQueryInfo & query_info,
     const Context & context,
-    QueryProcessingStage::Enum & processed_stage,
     const size_t max_block_size,
     const unsigned num_streams,
     const std::unordered_map<String, Int64> & max_blocks_number_to_read) const
