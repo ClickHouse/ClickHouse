2aa7eb463aa46c9eb3e93faeb70153d462856791
Alexey Milovidov <milovidov@yandex-team.ru>
2018-08-21 08:03:35 +0300

    Fixed test #2888

--- dbms/tests/queries/0_stateless/00690_insert_select_converting_exception_message.sh
+++ dbms/tests/queries/0_stateless/00690_insert_select_converting_exception_message.sh
@@ -1,8 +1,9 @@
 #!/usr/bin/env bash
 
+CLICKHOUSE_CLIENT_SERVER_LOGS_LEVEL=none
+
 CURDIR=$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)
 . $CURDIR/../shell_config.sh
-
 
 ${CLICKHOUSE_CLIENT} --query "DROP TABLE IF EXISTS test.test;"
 ${CLICKHOUSE_CLIENT} --query "CREATE TABLE test.test (val Int64) engine = Memory;"
