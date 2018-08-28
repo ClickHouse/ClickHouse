cc310847bcd36fd6a2d981c7e26140ec7d3d32f9
Amos Bird <amosbird@gmail.com>
2018-08-28 19:03:52 +0800

    Enable sse2 for CSV parsing.
    Testing data
    
    ```
    select 'aaaaaaaa,bbbbbbbb,cccccccc,dddddddd,eeeeeeee,ffffffff,gggg,hhh' from numbers(3000000) into outfile '/tmp/test.csv'
    ```
    
    Testing command
    ```
    echo "select count() from file('/tmp/test.csv', CSV, 'a String, b String, c String, d String, e String, f String, g String, h String') where not ignore(e)" | clickhouse-benchmark
    
    ```
    
    Before
    ```
    QPS: 1.317, RPS: 3949749.687, MiB/s: 478.380, result RPS: 1.317, result MiB/s: 0.000.
    0.000%  0.704 sec.
    10.000% 0.712 sec.
    20.000% 0.718 sec.
    30.000% 0.726 sec.
    40.000% 0.739 sec.
    50.000% 0.754 sec.
    60.000% 0.770 sec.
    70.000% 0.788 sec.
    80.000% 0.798 sec.
    90.000% 0.815 sec.
    95.000% 0.826 sec.
    99.000% 0.850 sec.
    99.900% 0.857 sec.
    99.990% 0.858 sec.
    ```
    
    After
    ```
    QPS: 1.533, RPS: 4598308.336, MiB/s: 556.932, result RPS: 1.533, result MiB/s: 0.000.
    0.000%  0.626 sec.
    10.000% 0.635 sec.
    20.000% 0.639 sec.
    30.000% 0.642 sec.
    40.000% 0.643 sec.
    50.000% 0.645 sec.
    60.000% 0.649 sec.
    70.000% 0.652 sec.
    80.000% 0.658 sec.
    90.000% 0.682 sec.
    95.000% 0.710 sec.
    99.000% 0.727 sec.
    99.900% 0.733 sec.
    99.990% 0.734 sec.
    ```

--- dbms/src/IO/ReadHelpers.cpp
+++ dbms/src/IO/ReadHelpers.cpp
@@ -10,6 +10,10 @@
 #include <common/find_first_symbols.h>
 #include <stdlib.h>
 
+#if __SSE2__
+    #include <emmintrin.h>
+#endif
+
 namespace DB
 {
 
@@ -552,9 +556,29 @@ void readCSVStringInto(Vector & s, ReadBuffer & buf, const FormatSettings::CSV &
         while (!buf.eof())
         {
             char * next_pos = buf.position();
-            while (next_pos < buf.buffer().end()
-                && *next_pos != delimiter && *next_pos != '\r' && *next_pos != '\n')    /// NOTE You can make a SIMD version.
-                ++next_pos;
+
+            [&]() {
+#if __SSE2__
+                auto rc = _mm_set1_epi8('\r');
+                auto nc = _mm_set1_epi8('\n');
+                auto dc = _mm_set1_epi8(delimiter);
+                for (; next_pos + 15 < buf.buffer().end(); next_pos += 16)
+                {
+                    __m128i bytes = _mm_loadu_si128(reinterpret_cast<const __m128i *>(next_pos));
+                    auto eq = _mm_or_si128(_mm_or_si128(_mm_cmpeq_epi8(bytes, rc), _mm_cmpeq_epi8(bytes, nc)), _mm_cmpeq_epi8(bytes, dc));
+                    uint16_t bit_mask = _mm_movemask_epi8(eq);
+                    if (bit_mask)
+                    {
+                        next_pos += __builtin_ctz(bit_mask);
+                        return;
+                    }
+                }
+#endif
+                while (next_pos < buf.buffer().end()
+                    && *next_pos != delimiter && *next_pos != '\r' && *next_pos != '\n')
+                    ++next_pos;
+            }();
+
 
             appendToStringOrVector(s, buf.position(), next_pos);
             buf.position() = next_pos;
