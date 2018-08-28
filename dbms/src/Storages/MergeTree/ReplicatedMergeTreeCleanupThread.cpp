794fbf86940d051bbbb8a9e307abcf4d08cc350e
Alexey Milovidov <milovidov@yandex-team.ru>
2018-08-28 03:21:02 +0300

    Miscellaneous #2758

--- dbms/src/Storages/MergeTree/ReplicatedMergeTreeCleanupThread.cpp
+++ dbms/src/Storages/MergeTree/ReplicatedMergeTreeCleanupThread.cpp
@@ -246,7 +246,7 @@ void ReplicatedMergeTreeCleanupThread::markLostReplicas(const std::unordered_map
     }
 
     if (candidate_lost_replicas.size() == replicas_count)
-        throw Exception("All replicas will be lost", ErrorCodes::ALL_REPLICAS_LOST);
+        throw Exception("All replicas are stale: we won't mark any replica as lost", ErrorCodes::ALL_REPLICAS_LOST);
 
     std::vector<zkutil::ZooKeeper::FutureMulti> futures;
     for (size_t i = 0; i < candidate_lost_replicas.size(); ++i)
