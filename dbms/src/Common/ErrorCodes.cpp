33363218497898dc7262c0a2d0d9f3fe30c05ce5
VadimPE <men19977@mail.ru>
2018-08-28 16:57:31 +0300

    CLICKHOUSE-3934 add join_default_strictness

--- dbms/src/Common/ErrorCodes.cpp
+++ dbms/src/Common/ErrorCodes.cpp
@@ -389,6 +389,7 @@ namespace ErrorCodes
     extern const int NETLINK_ERROR = 412;
     extern const int CANNOT_SET_SIGNAL_HANDLER = 413;
     extern const int CANNOT_READLINE = 414;
+    extern const int EXPECT_ALL_OR_ANY = 415;
 
     extern const int KEEPER_EXCEPTION = 999;
     extern const int POCO_EXCEPTION = 1000;
