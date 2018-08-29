4c49a0cb061798185185baac4547c414335dd5f3
VadimPE <men19977@mail.ru>
2018-08-29 17:13:20 +0300

    CLICKHOUSE-3847 fix time for cleanupthread and sleep()

--- dbms/tests/integration/test_recovery_replica/test.py
+++ dbms/tests/integration/test_recovery_replica/test.py
@@ -10,7 +10,7 @@ def fill_nodes(nodes, shard):
             CREATE DATABASE test;
 
             CREATE TABLE test_table(date Date, id UInt32)
-            ENGINE = ReplicatedMergeTree('/clickhouse/tables/test{shard}/replicated', '{replica}') ORDER BY id PARTITION BY toYYYYMM(date) SETTINGS min_replicated_logs_to_keep=3, max_replicated_logs_to_keep=5, cleanup_delay_period=10, cleanup_delay_period_random_add=10;
+            ENGINE = ReplicatedMergeTree('/clickhouse/tables/test{shard}/replicated', '{replica}') ORDER BY id PARTITION BY toYYYYMM(date) SETTINGS min_replicated_logs_to_keep=3, max_replicated_logs_to_keep=5, cleanup_delay_period=0, cleanup_delay_period_random_add=0;
         '''.format(shard=shard, replica=node.name))
 
 cluster = ClickHouseCluster(__file__)
@@ -40,10 +40,10 @@ def test_recovery(start_cluster):
     for i in range(100):
         node1.query("INSERT INTO test_table VALUES (1, {})".format(i))
 
-    time.sleep(40)
+    time.sleep(2)
 
     node2.query("ATTACH TABLE test_table")
 
-    time.sleep(40)
+    time.sleep(2)
 
     assert node1.query("SELECT count(*) FROM test_table") == node2.query("SELECT count(*) FROM test_table")
