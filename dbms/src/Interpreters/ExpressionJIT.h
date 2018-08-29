06adf19053a826676391e4521500720a55ccadb6
alesapin <alesapin@gmail.com>
2018-08-29 20:51:23 +0300

    CLICKHOUSE-3800: Remove tab

--- dbms/src/Interpreters/ExpressionJIT.h
+++ dbms/src/Interpreters/ExpressionJIT.h
@@ -11,7 +11,7 @@ namespace DB
 
 /// For each APPLY_FUNCTION action, try to compile the function to native code; if the only uses of a compilable
 /// function's result are as arguments to other compilable functions, inline it and leave the now-redundant action as-is.
-    void compileFunctions(ExpressionActions::Actions & actions, const Names & output_columns, const Block & sample_block, const Settings & settings);
+void compileFunctions(ExpressionActions::Actions & actions, const Names & output_columns, const Block & sample_block, const Settings & settings);
 
 }
 
