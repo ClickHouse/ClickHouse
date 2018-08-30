ef93950e969e74b35ea338dbcae597683b74cf18
proller <proller@github.com>
2018-08-30 22:07:24 +0300

    USE_INTERNAL_CONSISTENT_HASHING_LIBRARY

--- dbms/src/Functions/FunctionsConsistentHashing.h
+++ dbms/src/Functions/FunctionsConsistentHashing.h
@@ -8,8 +8,8 @@
 #include <Common/typeid_cast.h>
 #include <common/likely.h>
 
-#include <mailru/sumbur.h>
-#include <yandex/consistent_hashing.h>
+#include <sumbur.h>
+#include <consistent_hashing.h>
 
 
 namespace DB
