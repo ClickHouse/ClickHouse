f59289841ae5b07fe056a95bd00e40414c5529b6
alesapin <alesapin@gmail.com>
2018-09-03 18:02:24 +0300

    CLICKHOUSE-3894: Remove retry and add sleep

--- dbms/tests/integration/test_distributed_ddl/test.py
+++ dbms/tests/integration/test_distributed_ddl/test.py
@@ -30,7 +30,7 @@ def ddl_check_query(instance, query, num_hosts=None):
     if num_hosts is None:
         num_hosts = len(cluster.instances)
 
-    contents = instance.query_with_retry(query, check_callback=lambda x: len(x.strip().split('\n')) == num_hosts)
+    contents = instance.query(query)
     check_all_hosts_sucesfully_executed(contents, num_hosts)
     return contents
 
@@ -282,7 +282,9 @@ ENGINE = Distributed(cluster_without_replication, default, merge, i)
     assert TSV(instance.query("SELECT i FROM all_merge_32 ORDER BY i")) == TSV(''.join(['{}\n'.format(x) for x in xrange(4)]))
 
 
+    time.sleep(5)
     ddl_check_query(instance, "ALTER TABLE merge ON CLUSTER cluster_without_replication MODIFY COLUMN i Int64")
+    time.sleep(5)
     ddl_check_query(instance, "ALTER TABLE merge ON CLUSTER cluster_without_replication ADD COLUMN s DEFAULT toString(i) FORMAT TSV")
 
     assert TSV(instance.query("SELECT i, s FROM all_merge_64 ORDER BY i")) == TSV(''.join(['{}\t{}\n'.format(x,x) for x in xrange(4)]))
