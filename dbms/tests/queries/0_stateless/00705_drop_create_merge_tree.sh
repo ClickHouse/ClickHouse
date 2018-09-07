199d8734f98fa7d04ebf2119431c5f56a7ed4e5a
Alexey Milovidov <milovidov@yandex-team.ru>
2018-09-07 04:39:48 +0300

    Fixed race condition in DROP/CREATE MergeTree tables [#CLICKHOUSE-3939]

---
+++ dbms/tests/queries/0_stateless/00705_drop_create_merge_tree.sh
@@ -0,0 +1,25 @@
+#!/usr/bin/env bash
+set -e
+
+CURDIR=$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)
+. $CURDIR/../shell_config.sh
+
+function stress()
+{
+    while true; do
+        ${CLICKHOUSE_CLIENT} --query "CREATE TABLE IF NOT EXISTS test.table (x UInt8) ENGINE = MergeTree ORDER BY tuple()" 2>/dev/null
+        ${CLICKHOUSE_CLIENT} --query "DROP TABLE test.table" 2>/dev/null
+    done
+}
+
+# https://stackoverflow.com/questions/9954794/execute-a-shell-function-with-timeout
+export -f stress
+
+for thread in {1..5}; do
+    timeout 10 bash -c stress &
+done
+
+wait
+echo
+
+${CLICKHOUSE_CLIENT} --query "DROP TABLE IF EXISTS test.table";
