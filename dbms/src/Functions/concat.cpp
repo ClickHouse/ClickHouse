6cb5c6ee800cda1c6dc58aef89dd3b1e2af50680
Alexey Milovidov <milovidov@yandex-team.ru>
2018-09-10 04:39:06 +0300

    Every function in its own file, part 3

--- dbms/src/Functions/concat.cpp
+++ dbms/src/Functions/concat.cpp
@@ -154,6 +154,7 @@ public:
 
     String getName() const override { return name; }
     size_t getNumberOfArguments() const override { return 0; }
+    bool isVariadic() const override { return true; }
 
 protected:
     FunctionBasePtr buildImpl(const ColumnsWithTypeAndName & arguments, const DataTypePtr & /*return_type*/) const override
