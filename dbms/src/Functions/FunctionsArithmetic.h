1ecb9fb271a2ee6ba1eafb9a440ff0f0bd5a0a05
chertus <chertus@gmail.com>
2018-09-10 17:26:47 +0300

    fix build

--- dbms/src/Functions/FunctionsArithmetic.h
+++ dbms/src/Functions/FunctionsArithmetic.h
@@ -1659,7 +1659,7 @@ public:
             using DataType = std::decay_t<decltype(type)>;
             using T0 = typename DataType::FieldType;
             using T1 = typename Op<T0>::ResultType;
-            if constexpr (!std::is_same_v<T1, InvalidType> && !IsDecimalDataType<DataType> && Op<T0>::compilable)
+            if constexpr (!std::is_same_v<T1, InvalidType> && !IsDataTypeDecimal<DataType> && Op<T0>::compilable)
             {
                 auto & b = static_cast<llvm::IRBuilder<> &>(builder);
                 auto * v = nativeCast(b, types[0], values[0](), std::make_shared<DataTypeNumber<T1>>());
