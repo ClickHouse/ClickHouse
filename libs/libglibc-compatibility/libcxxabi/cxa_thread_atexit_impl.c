3ac2b94ded63d288dfb7b553af5f08bd3bdcf04f
Alexey Milovidov <milovidov@yandex-team.ru>
2018-09-14 03:00:24 +0300

    Added dummy implementation of __cxa_thread_atexit_impl [#CLICKHOUSE-3989]

--- libs/libglibc-compatibility/libcxxabi/cxa_thread_atexit_impl.c
+++ libs/libglibc-compatibility/libcxxabi/cxa_thread_atexit_impl.c
@@ -1,1 +1,5 @@
-int __cxa_thread_atexit_impl(void(*)(void*) dtor, void * obj, void * dso_symbol) { return 0; }
+/// We provide dummy implementation as a replacement of this function from glibc,
+///  because we ensure that this function is not used.
+/// We provide our own implementation of __cxa_thread_atexit that doesn't use __cxa_thread_atexit_impl.
+/// But it is still needed for linking.
+int __cxa_thread_atexit_impl(void (*dtor)(void*), void * obj, void * dso_symbol) { __builtin_trap(); }
