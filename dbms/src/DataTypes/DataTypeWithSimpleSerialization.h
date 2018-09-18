0300256a07ca2fcdef4b9829bee335d1567ab04a
chertus <chertus@gmail.com>
2018-09-18 19:29:44 +0300

    add comment

--- dbms/src/DataTypes/DataTypeWithSimpleSerialization.h
+++ dbms/src/DataTypes/DataTypeWithSimpleSerialization.h
@@ -4,6 +4,8 @@
 namespace DB
 {
 
+/// Helper class to define same IDataType text (de)serialization for all the variants (escaped, quoted, JSON, CSV).
+/// You need to define serializeText() and deserializeText() in derived class.
 class DataTypeWithSimpleSerialization : public IDataType
 {
 protected:
