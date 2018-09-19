8e370b7605165020b14595c73304c37c464c4dcd
alesapin <alesapin@gmail.com>
2018-09-19 20:52:09 +0300

    Fix test

--- dbms/tests/queries/0_stateless/00719_insert_block_without_column.sh
+++ dbms/tests/queries/0_stateless/00719_insert_block_without_column.sh
@@ -8,7 +8,7 @@ CURDIR=$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)
 ${CLICKHOUSE_CLIENT} --query "select number as SomeID, number+1 as OtherID from system.numbers limit 1000 into outfile '${CLICKHOUSE_TMP}/test_squashing_block_without_column.out' format Native"
 
 ${CLICKHOUSE_CLIENT} --query "drop table if exists test.squashed_numbers"
-${CLICKHOUSE_CLIENT} --query "create table test.squashed_numbers (SomeID UInt8, DifferentID UInt8, OtherID UInt8) engine Memory"
+${CLICKHOUSE_CLIENT} --query "create table test.squashed_numbers (SomeID UInt64, DifferentID UInt64, OtherID UInt64) engine Memory"
 
 cat ${CLICKHOUSE_TMP}/test_squashing_block_without_column.out | ${CLICKHOUSE_CLIENT} --query "insert into test.squashed_numbers format Native"
 
