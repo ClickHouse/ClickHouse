b93ea8e7ccbae6a0bd752d3a870c6c6bce1edd76
Nikolai Kochetov <nik-kochetov@yandex-team.ru>
2018-09-19 14:59:40 +0300

    Fixed default value index for LowCardinality(Nullable).

--- dbms/tests/queries/0_stateless/00688_low_cardinality_alter_add_column.sql
+++ dbms/tests/queries/0_stateless/00688_low_cardinality_alter_add_column.sql
@@ -1,5 +1,6 @@
-create table test_cardinality (x String) engine = MergeTree order by tuple();
-insert into test_cardinality (x) select concat('v', toString(number)) from numbers(10);
-alter table test_cardinality add column y LowCardinality(String);
-select * from test_cardinality;
-
+drop table if exists test.cardinality;
+create table test.cardinality (x String) engine = MergeTree order by tuple();
+insert into test.cardinality (x) select concat('v', toString(number)) from numbers(10);
+alter table test.cardinality add column y LowCardinality(String);
+select * from test.cardinality;
+drop table if exists test.cardinality;
