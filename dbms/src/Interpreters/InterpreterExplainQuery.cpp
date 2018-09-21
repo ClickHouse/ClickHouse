679422a0b3bdb1164d31ffb28fe7137857af3d01
chertus <chertus@gmail.com>
2018-09-21 18:20:23 +0300

    explain ast [CLICKHOUSE-4001]

---
+++ dbms/src/Interpreters/InterpreterExplainQuery.cpp
@@ -0,0 +1,51 @@
+#include <sstream>
+
+#include <DataStreams/OneBlockInputStream.h>
+#include <DataStreams/BlockIO.h>
+#include <DataTypes/DataTypeString.h>
+#include <Parsers/queryToString.h>
+#include <Common/typeid_cast.h>
+#include <Interpreters/InterpreterExplainQuery.h>
+#include <Parsers/ASTExplainQuery.h>
+
+
+namespace DB
+{
+
+BlockIO InterpreterExplainQuery::execute()
+{
+    BlockIO res;
+    res.in = executeImpl();
+    return res;
+}
+
+
+Block InterpreterExplainQuery::getSampleBlock()
+{
+    Block block;
+
+    ColumnWithTypeAndName col;
+    col.name = "ast";
+    col.type = std::make_shared<DataTypeString>();
+    col.column = col.type->createColumn();
+    block.insert(col);
+
+    return block;
+}
+
+
+BlockInputStreamPtr InterpreterExplainQuery::executeImpl()
+{
+    const ASTExplainQuery & ast = typeid_cast<const ASTExplainQuery &>(*query);
+
+    std::stringstream ss;
+    ast.dumpSExpressions(ss);
+
+    Block sample_block = getSampleBlock();
+    MutableColumns res_columns = sample_block.cloneEmptyColumns();
+    res_columns[0]->insert(ss.str());
+
+    return std::make_shared<OneBlockInputStream>(sample_block.cloneWithColumns(std::move(res_columns)));
+}
+
+}
