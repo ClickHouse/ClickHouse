3b2fb9a4d25ef9391efe7f68995ae8f0b7f1a41c
CurtizJ <pad11rus@gmail.com>
2018-09-25 14:27:28 +0300

    fix test

--- dbms/tests/integration/test_distributed_ddl/test.py
+++ dbms/tests/integration/test_distributed_ddl/test.py
@@ -322,7 +322,7 @@ ENGINE = ReplicatedMergeTree('/clickhouse/tables/{layer}-{shard}/hits', '{replic
 ENGINE = Distributed('{cluster}', '{database}', '{table}', value % 4)
 """)
 
-    assert TSV(instance.query("SELECT value FROM db.distr ORDER BY value")) == TSV('1\n4\n')
+    assert TSV(instance.query("SELECT value FROM db.distr ORDER BY value")) == TSV('1\n2\n')
     assert TSV(instance.query("SELECT value FROM `{database}`.`{distributed}` ORDER BY value")) == TSV('1\n2\n')
     assert TSV(cluster.instances['ch3'].query("SELECT value FROM db.distr ORDER BY value")) == TSV('1\n2\n')
     assert TSV(cluster.instances['ch3'].query("SELECT value FROM `{database}`.`{distributed}` ORDER BY value")) == TSV('1\n2\n')
