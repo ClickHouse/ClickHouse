27b7654abc9d9ade0c91cbc352cd6205dc97d588
Alexey Milovidov <milovidov@yandex-team.ru>
2018-09-26 03:31:40 +0300

    Every function in its own file, part 5 [#CLICKHOUSE-2]

---
+++ dbms/src/Functions/subtractHours.cpp
@@ -0,0 +1,18 @@
+#include <Functions/IFunction.h>
+#include <Functions/FunctionFactory.h>
+#include <Functions/FunctionDateOrDateTimeAddInterval.h>
+
+
+namespace DB
+{
+
+using FunctionSubtractHours = FunctionDateOrDateTimeAddInterval<SubtractHoursImpl>;
+
+void registerFunctionSubtractHours(FunctionFactory & factory)
+{
+    factory.registerFunction<FunctionSubtractHours>();
+}
+
+}
+
+
