ea1a56a2552d86706a373db47c76d97d0605da68
Alexey Milovidov <milovidov@yandex-team.ru>
2018-10-07 23:47:03 +0300

    Improved test #3230

--- dbms/tests/queries/0_stateless/00725_memory_tracking.sql
+++ dbms/tests/queries/0_stateless/00725_memory_tracking.sql
@@ -1,3 +1,3 @@
-SELECT value > 0 FROM system.metrics WHERE metric = 'MemoryTracking';
+SELECT least(value, 0) FROM system.metrics WHERE metric = 'MemoryTracking';
 SELECT length(range(100000000));
-SELECT value > 0 FROM system.metrics WHERE metric = 'MemoryTracking';
+SELECT least(value, 0) FROM system.metrics WHERE metric = 'MemoryTracking';
