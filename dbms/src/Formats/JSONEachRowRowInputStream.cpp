aad28680240f48312e568bbb01cb663a1d97db3f
Alexey Milovidov <milovidov@yandex-team.ru>
2018-10-10 00:21:52 +0300

    Attempt to fix performance regression while parsing JSONEachRow #3223

--- dbms/src/Formats/JSONEachRowRowInputStream.cpp
+++ dbms/src/Formats/JSONEachRowRowInputStream.cpp
@@ -94,15 +94,14 @@ StringRef JSONEachRowRowInputStream::readColumnName(ReadBuffer & buf)
 
     if (nested_prefix_length == 0 && buf.position() + 1 < buf.buffer().end())
     {
-        const char * next_pos = find_first_symbols<'\\', '"'>(buf.position() + 1, buf.buffer().end());
+        char * next_pos = find_first_symbols<'\\', '"'>(buf.position() + 1, buf.buffer().end());
 
         if (next_pos != buf.buffer().end() && *next_pos != '\\')
         {
             /// The most likely option is that there is no escape sequence in the key name, and the entire name is placed in the buffer.
             assertChar('"', buf);
             StringRef res(buf.position(), next_pos - buf.position());
-            buf.position() += next_pos - buf.position();
-            assertChar('"', buf);
+            buf.position() = next_pos + 1;
             return res;
         }
     }
