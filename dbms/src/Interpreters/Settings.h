c7bf0e77eac26b5ffc322b750bcb67dbecb83139
chertus <chertus@gmail.com>
2018-10-09 19:50:42 +0300

    add get64() for Decimal32 and Decimal64; remove allow_experimental_decimal_type setting [issue-3282]

--- dbms/src/Interpreters/Settings.h
+++ dbms/src/Interpreters/Settings.h
@@ -282,7 +282,6 @@ struct Settings
     M(SettingUInt64, low_cardinality_max_dictionary_size, 8192, "Maximum size (in rows) of shared global dictionary for LowCardinality type.") \
     M(SettingBool, low_cardinality_use_single_dictionary_for_part, false, "LowCardinality type serialization setting. If is true, than will use additional keys when global dictionary overflows. Otherwise, will create several shared dictionaries.") \
     M(SettingBool, allow_experimental_low_cardinality_type, false, "Allows to create table with LowCardinality types.") \
-    M(SettingBool, allow_experimental_decimal_type, false, "Enables Decimal data type.") \
     M(SettingBool, decimal_check_overflow, true, "Check overflow of decimal arithmetic/comparison operations") \
     \
     M(SettingBool, prefer_localhost_replica, 1, "1 - always send query to local replica, if it exists. 0 - choose replica to send query between local and remote ones according to load_balancing") \
