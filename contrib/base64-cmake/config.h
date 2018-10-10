b381ca277567a0f2f33456b73464c27824e487fd
Alexander Krasheninnikov <kras3@gmail.com>
2018-10-10 04:04:07 +0300

    Initial implementation

---
+++ contrib/base64-cmake/config.h
@@ -0,0 +1,8 @@
+#define HAVE_AVX2                  0
+#define HAVE_NEON32                0
+#define HAVE_NEON64                0
+#define HAVE_SSSE3                 0
+#define HAVE_SSE41                 1
+#define HAVE_SSE42                 1
+#define HAVE_AVX                   0
+#define HAVE_FAST_UNALIGNED_ACCESS 1
