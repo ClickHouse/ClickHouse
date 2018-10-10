cc2686cdf1f781a6e267b512a801f9c280f42a55
VadimPE <men19977@mail.ru>
2018-10-10 19:13:14 +0300

    ISSUES-2581 add tests

---
+++ dbms/tests/queries/0_stateless/00732_quorum_insert_zookeeper.sql
@@ -0,0 +1,34 @@
+DROP TABLE IF EXISTS test.quorum1;
+DROP TABLE IF EXISTS test.quorum2;
+
+CREATE TABLE test.quorum1(x UInt32, y Date) ENGINE ReplicatedMergeTree('/clickhouse/tables/test/fetches', '1') ORDER BY x PARTITION BY y;
+CREATE TABLE test.quorum2(x UInt32, y Date) ENGINE ReplicatedMergeTree('/clickhouse/tables/test/fetches', '2') ORDER BY x PARTITION BY y;
+
+SET insert_quorum=2;
+SET select_sequential_consistency=1;
+SET insert_quorum_timeout=0;
+
+INSERT INTO test.quorum1 VALUES (1, 1);
+INSERT INTO test.quorum1 VALUES (2, 1);
+INSERT INTO test.quorum1 VALUES (1, 100);
+
+SELECT count(*) FROM test.quorum1;
+SELECT count(*) FROM test.quorum2;
+
+SYSTEM STOP FETCHES test.quorum1;
+
+
+INSERT INTO test.quorum2 VALUES (1, 200); -- { serverError 319 } --
+
+SELECT count(*) FROM test.quorum1;
+SELECT count(*) FROM test.quorum2;
+
+
+SYSTEM START FETCHES test.quorum1;
+
+SYSTEM SYNC REPLICA test.quorum1;
+ 
+
+SELECT count(*) FROM test.quorum1;
+SELECT count(*) FROM test.quorum2;
+
