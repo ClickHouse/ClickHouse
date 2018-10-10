edaf084325f61d7c50c3b2d64289e85003328185
Vadim <plahtinskiy.ve@phystech.edu>
2018-10-10 20:49:42 +0300

    Update 00732_quorum_insert_zookeeper.sql

--- dbms/tests/queries/0_stateless/00732_quorum_insert_zookeeper.sql
+++ dbms/tests/queries/0_stateless/00732_quorum_insert_zookeeper.sql
@@ -6,7 +6,6 @@ CREATE TABLE test.quorum2(x UInt32, y Date) ENGINE ReplicatedMergeTree('/clickho
 
 SET insert_quorum=2;
 SET select_sequential_consistency=1;
-SET insert_quorum_timeout=0;
 
 INSERT INTO test.quorum1 VALUES (1, 1);
 INSERT INTO test.quorum1 VALUES (2, 1);
@@ -15,20 +14,20 @@ INSERT INTO test.quorum1 VALUES (1, 100);
 SELECT count(*) FROM test.quorum1;
 SELECT count(*) FROM test.quorum2;
 
-SYSTEM STOP FETCHES test.quorum1;
+SET insert_quorum_timeout=0;
 
+SYSTEM STOP FETCHES test.quorum1;
 
 INSERT INTO test.quorum2 VALUES (1, 200); -- { serverError 319 } --
 
 SELECT count(*) FROM test.quorum1;
 SELECT count(*) FROM test.quorum2;
 
+SET insert_quorum_timeout=100;
 
 SYSTEM START FETCHES test.quorum1;
-
 SYSTEM SYNC REPLICA test.quorum1;
  
-
 SELECT count(*) FROM test.quorum1;
 SELECT count(*) FROM test.quorum2;
 
