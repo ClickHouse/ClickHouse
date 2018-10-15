c05b1126cbf51c9d0f8fdc6b3e95db84884bfff6
VadimPE <men19977@mail.ru>
2018-10-15 17:55:02 +0300

    fix test

--- dbms/tests/queries/0_stateless/00732_quorum_insert_lost_part_zookeeper.sql
+++ dbms/tests/queries/0_stateless/00732_quorum_insert_lost_part_zookeeper.sql
@@ -13,15 +13,14 @@ SET insert_quorum_timeout=0;
 
 SYSTEM STOP FETCHES test.quorum1;
 
-INSERT INTO test.quorum2 VALUES (1, toDate('2018-11-15')); -- { serverError 319 } --
+INSERT INTO test.quorum2 VALUES (1, '2018-11-15'); -- { serverError 319 }
 
 SELECT count(*) FROM test.quorum1;
 SELECT count(*) FROM test.quorum2;
 
 SET select_sequential_consistency=0;
 
-SELECT count(*) FROM test.quorum2;
-
+SELECT x FROM test.quorum2 ORDER BY x;
 SET select_sequential_consistency=1;
 
 SET insert_quorum_timeout=100;
@@ -29,8 +28,8 @@ SET insert_quorum_timeout=100;
 SYSTEM START FETCHES test.quorum1;
 SYSTEM SYNC REPLICA test.quorum1;
  
-SELECT count(*) FROM test.quorum1;
-SELECT count(*) FROM test.quorum2;
+SELECT x FROM test.quorum1 ORDER BY x;
+SELECT x FROM test.quorum2 ORDER BY x;
 
 DROP TABLE IF EXISTS test.quorum1;
 DROP TABLE IF EXISTS test.quorum2;
--- dbms/tests/queries/0_stateless/00732_quorum_insert_simple_test_2_parts_zookeeper.sql
+++ dbms/tests/queries/0_stateless/00732_quorum_insert_simple_test_2_parts_zookeeper.sql
@@ -9,12 +9,12 @@ CREATE TABLE test.quorum2(x UInt32, y Date) ENGINE ReplicatedMergeTree('/clickho
 SET insert_quorum=2;
 SET select_sequential_consistency=1;
 
-INSERT INTO test.quorum1 VALUES (1, toDate('2018-11-15'));
-INSERT INTO test.quorum1 VALUES (2, toDate('2018-11-15'));
-INSERT INTO test.quorum1 VALUES (1, toDate('2018-12-16'));
+INSERT INTO test.quorum1 VALUES (1, '2018-11-15');
+INSERT INTO test.quorum1 VALUES (2, '2018-11-15');
+INSERT INTO test.quorum1 VALUES (3, '2018-12-16');
 
-SELECT count(*) FROM test.quorum1;
-SELECT count(*) FROM test.quorum2;
+SELECT x FROM test.quorum1 ORDER BY x;
+SELECT x FROM test.quorum2 ORDER BY x;
 
 DROP TABLE IF EXISTS test.quorum1;
 DROP TABLE IF EXISTS test.quorum2;
