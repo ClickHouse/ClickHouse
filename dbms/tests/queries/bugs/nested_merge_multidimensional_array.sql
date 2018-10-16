05f18bae896223ae8a89261779ffa9ce00750056
Alexey Milovidov <milovidov@yandex-team.ru>
2018-10-16 23:51:12 +0300

    More simple test [#CLICKHOUSE-3901]

--- dbms/tests/queries/bugs/nested_merge_multidimensional_array.sql
+++ dbms/tests/queries/bugs/nested_merge_multidimensional_array.sql
@@ -1,10 +1,10 @@
 DROP TABLE IF EXISTS test.sites;
-CREATE TABLE test.sites (Domain String, `Users.UserID` Array(UInt64), `Users.Dates` Array(Array(Date))) ENGINE = MergeTree ORDER BY Domain SETTINGS vertical_merge_algorithm_min_rows_to_activate = 0, vertical_merge_algorithm_min_columns_to_activate = 0;
+CREATE TABLE test.sites (Domain UInt8, `Users.UserID` Array(UInt64), `Users.Dates` Array(Array(Date))) ENGINE = MergeTree ORDER BY Domain SETTINGS vertical_merge_algorithm_min_rows_to_activate = 0, vertical_merge_algorithm_min_columns_to_activate = 0;
 
 SYSTEM STOP MERGES;
 
-INSERT INTO test.sites VALUES ('yandex.ru',[4773498661529650001],[[]]),('yandex.ru',[1936412071529050001],[[]]);
-INSERT INTO test.sites VALUES ('yandex.ru/video',[4773498661529650001],[['2018-06-22']]);
+INSERT INTO test.sites VALUES (1,[1],[[]]);
+INSERT INTO test.sites VALUES (2,[1],[['2018-06-22']]);
 
 SELECT count(), countArray(Users.Dates), countArrayArray(Users.Dates) FROM test.sites;
 SYSTEM START MERGES;
