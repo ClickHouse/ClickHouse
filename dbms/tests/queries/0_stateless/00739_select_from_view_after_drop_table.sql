d3d8f80482c298cf3bd2e8b8fe32423458ed8dfa
CurtizJ <pad11rus@gmail.com>
2018-10-17 18:13:10 +0300

    add test

---
+++ dbms/tests/queries/0_stateless/00739_select_from_view_after_drop_table.sql
@@ -0,0 +1,8 @@
+DROP TABLE IF EXISTS test.t;
+DROP TABLE IF EXISTS test.v;
+
+CREATE TABLE test.t(a Int) ENGINE = Memory;
+CREATE VIEW test.v AS SELECT * FROM test.t;
+
+DROP TABLE test.t;
+SELECT * FROM test.v; -- {serverError 60}
