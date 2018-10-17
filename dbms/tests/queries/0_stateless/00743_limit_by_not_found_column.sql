2dc6a1b936a9251e864f7d44c9d52d5a6e2e2b1f
Alexey Milovidov <milovidov@yandex-team.ru>
2018-10-17 08:24:32 +0300

    Added more test cases #1164

--- dbms/tests/queries/0_stateless/00743_limit_by_not_found_column.sql
+++ dbms/tests/queries/0_stateless/00743_limit_by_not_found_column.sql
@@ -16,7 +16,7 @@ FROM
         info
     ORDER BY cnt DESC
     LIMIT 5 BY message
-)
+);
 
 DROP TABLE installation_stats;
 
@@ -30,4 +30,36 @@ FROM
         Currency
     FROM Accounts 
     LIMIT 2 BY Currency
+);
+
+CREATE TEMPORARY TABLE commententry1 (created_date Date, link_id String, subreddit String);
+INSERT INTO commententry1 VALUES ('2016-01-01', 'xyz', 'cpp');
+
+SELECT concat('http://reddit.com/r/', subreddit, '/comments/', replaceRegexpOne(link_id, 't[0-9]_', ''))
+FROM
+(
+    SELECT
+        y,
+        subreddit,
+        link_id,
+        cnt
+    FROM
+    (
+        SELECT
+            created_date AS y,
+            link_id,
+            subreddit,
+            count(*) AS cnt
+        FROM commententry1
+        WHERE toYear(created_date) = 2016
+        GROUP BY
+            y,
+            link_id,
+            subreddit
+        ORDER BY y ASC
+    )
+    ORDER BY
+        y ASC,
+        cnt DESC
+    LIMIT 1 BY y
 );
