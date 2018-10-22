bcd40e77a4c517f8b260490b8bab0074b4eed88f
alexey-milovidov <milovidov@yandex-team.ru>
2018-10-22 23:38:16 +0300

    Update 00726_modulo_for_date.sql

--- dbms/tests/queries/0_stateless/00726_modulo_for_date.sql
+++ dbms/tests/queries/0_stateless/00726_modulo_for_date.sql
@@ -1,12 +1,13 @@
-SELECT toDate('21-06-2018') % 234 = toInt16(toDate('21-06-2018')) % 234;
-SELECT toDate('21-06-2018') % 23456 = toInt16(toDate('21-06-2018')) % 23456;
-SELECT toDate('21-06-2018') % 12376 = toInt16(toDate('21-06-2018')) % 12376;
-SELECT toDate('21-06-2018 12:12:12') % 234 = toInt32(toDate('21-06-2018 12:12:12')) % 234;
-SELECT toDate('21-06-2018 12:12:12') % 23456 = toInt32(toDate('21-06-2018 12:12:12')) % 23456;
-SELECT toDate('21-06-2018 12:12:12') % 12376 = toInt32(toDate('21-06-2018 12:12:12')) % 12376;
-SELECT toDate('21-06-2018') % 234.8 = toInt16(toDate('21-06-2018')) % 234.8;
-SELECT toDate('21-06-2018') % 23456.8 = toInt16(toDate('21-06-2018')) % 23456.8;
-SELECT toDate('21-06-2018') % 12376.8 = toInt16(toDate('21-06-2018')) % 12376.8;
-SELECT toDate('21-06-2018 12:12:12') % 234.8 = toInt32(toDate('21-06-2018 12:12:12')) % 234.8;
-SELECT toDate('21-06-2018 12:12:12') % 23456.8 = toInt32(toDate('21-06-2018 12:12:12')) % 23456.8;
-SELECT toDate('21-06-2018 12:12:12') % 12376.8 = toInt32(toDate('21-06-2018 12:12:12')) % 12376.8;
+SELECT toDate('2018-06-21') % 234 = toInt16(toDate('2018-06-21')) % 234;
+SELECT toDate('2018-06-21') % 23456 = toInt16(toDate('2018-06-21')) % 23456;
+SELECT toDate('2018-06-21') % 12376 = toInt16(toDate('2018-06-21')) % 12376;
+SELECT toDateTime('2018-06-21 12:12:12') % 234 = toInt32(toDateTime('2018-06-21 12:12:12')) % 234;
+SELECT toDateTime('2018-06-21 12:12:12') % 23456 = toInt32(toDateTime('2018-06-21 12:12:12')) % 23456;
+SELECT toDateTime('2018-06-21 12:12:12') % 12376 = toInt32(toDateTime('2018-06-21 12:12:12')) % 12376;
+
+SELECT toDate('2018-06-21') % 234.8 = toInt16(toDate('2018-06-21')) % 234.8;
+SELECT toDate('2018-06-21') % 23456.8 = toInt16(toDate('2018-06-21')) % 23456.8;
+SELECT toDate('2018-06-21') % 12376.8 = toInt16(toDate('2018-06-21')) % 12376.8;
+SELECT toDateTime('2018-06-21 12:12:12') % 234.8 = toInt32(toDateTime('2018-06-21 12:12:12')) % 234.8;
+SELECT toDateTime('2018-06-21 12:12:12') % 23456.8 = toInt32(toDateTime('2018-06-21 12:12:12')) % 23456.8;
+SELECT toDateTime('2018-06-21 12:12:12') % 12376.8 = toInt32(toDateTime('2018-06-21 12:12:12')) % 12376.8;
