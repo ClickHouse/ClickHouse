8d91b9389df1c39ebfa65e0265a7b19edfaff9e5
VadimPE <men19977@mail.ru>
2018-10-22 14:49:06 +0300

    CLICKHOUSE-4021 add test

--- dbms/tests/queries/0_stateless/00726_read_in_primary_key_order.sql
+++ dbms/tests/queries/0_stateless/00726_read_in_primary_key_order.sql
@@ -4,7 +4,15 @@ DROP TABLE IF EXISTS test.merge1;
 
 CREATE TABLE test.merge1(a Date, b UInt64) ENGINE=MergeTree() PARTITION BY a ORDER BY b;
 
-INSERT INTO test.merge1 VALUES (1, 1), (1, 2), (1, 3), (100, 1), (100, 2), (200, 1), (200, 4);
+INSERT INTO test.merge1 VALUES (1, 1), (1, 2), (1, 3);
+INSERT INTO test.merge1 VALUES (100, 4), (100, 5);
+INSERT INTO test.merge1 VALUES (200, 6), (200, 7);
+INSERT INTO test.merge1 VALUES (300, 8), (300, 9), (300, 0);
+
+SELECT b FROM test.merge1 ORDER BY b;
+SELECT b FROM test.merge1 ORDER BY b DESC;
+
+SET max_threads=1;
 
 SELECT b FROM test.merge1 ORDER BY b;
 SELECT b FROM test.merge1 ORDER BY b DESC;
