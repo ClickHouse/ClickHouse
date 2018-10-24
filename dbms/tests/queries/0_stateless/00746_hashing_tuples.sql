89808b89b6ee276f312a9eb0dd9df184fde68e9c
CurtizJ <pad11rus@gmail.com>
2018-10-24 16:27:49 +0300

    update test

--- dbms/tests/queries/0_stateless/00746_hashing_tuples.sql
+++ dbms/tests/queries/0_stateless/00746_hashing_tuples.sql
@@ -1,15 +1,19 @@
 SELECT sipHash64(1, 2, 3);
 SELECT sipHash64(1, 3, 2);
-SELECT sipHash64('a', [1, 2, 3], 4);
+SELECT sipHash64(('a', [1, 2, 3], 4, (4, ['foo', 'bar'], 1, (1, 2))));
+
+SELECT halfMD5(1, 2, 3);
+SELECT halfMD5(1, 3, 2);
+SELECT halfMD5(('a', [1, 2, 3], 4, (4, ['foo', 'bar'], 1, (1, 2))));
 
 SELECT murmurHash2_32(1, 2, 3);
 SELECT murmurHash2_32(1, 3, 2);
-SELECT murmurHash2_32('a', [1, 2, 3], 4);
+SELECT murmurHash2_32(('a', [1, 2, 3], 4, (4, ['foo', 'bar'], (1, 2))));
 
 SELECT murmurHash2_64(1, 2, 3);
 SELECT murmurHash2_64(1, 3, 2);
-SELECT murmurHash2_64('a', [1, 2, 3], 4);
+SELECT murmurHash2_64(('a', [1, 2, 3], 4, (4, ['foo', 'bar'], 1, (1, 2))));
 
 SELECT murmurHash3_64(1, 2, 3);
 SELECT murmurHash3_64(1, 3, 2);
-SELECT murmurHash3_64('a', [1, 2, 3], 4);
+SELECT murmurHash3_64(('a', [1, 2, 3], 4, (4, ['foo', 'bar'], 1, (1, 2))));
