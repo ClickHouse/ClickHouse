a557cacf09241305a827f97ccd0361c9066beb3f
abyss7 <5627721+abyss7@users.noreply.github.com>
2018-10-30 17:05:44 +0300

    Fix build and tests on Fedora (#3496)
    * Fix some tests and build on Fedora 28
    
    * Update contrib/ssl
    
    * Try `sudo` first, then without `sudo`.

--- dbms/tests/queries/0_stateless/00417_system_build_options.sh
+++ dbms/tests/queries/0_stateless/00417_system_build_options.sh
@@ -3,4 +3,4 @@
 CURDIR=$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)
 . $CURDIR/../shell_config.sh
 
-$CLICKHOUSE_CLIENT --query="SELECT * FROM system.build_options" | perl -lnE 'print $1 if /(BUILD_DATE|BUILD_TYPE|CXX_COMPILER|CXX_FLAGS|LINK_FLAGS)\s+\S+/';
+$CLICKHOUSE_CLIENT --query="SELECT * FROM system.build_options" | perl -lnE 'print $1 if /(BUILD_DATE|BUILD_TYPE|CXX_COMPILER)\s+\S+/ || /(CXX_FLAGS|LINK_FLAGS)/';
