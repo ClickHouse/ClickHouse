77af9f7abbb7d7b7ad851cc96c004671842f1f8e
Alexey Milovidov <milovidov@yandex-team.ru>
2018-11-01 18:55:28 +0300

    Fixed error code in test #3503

--- dbms/tests/queries/0_stateless/00748_insert_array_with_null.sql
+++ dbms/tests/queries/0_stateless/00748_insert_array_with_null.sql
@@ -3,7 +3,7 @@ DROP TABLE IF EXISTS test.arraytest;
 CREATE TABLE test.arraytest ( created_date Date DEFAULT toDate(created_at), created_at DateTime DEFAULT now(), strings Array(String) DEFAULT emptyArrayString()) ENGINE = MergeTree(created_date, cityHash64(created_at), (created_date, cityHash64(created_at)), 8192);
 
 INSERT INTO test.arraytest (created_at, strings) VALUES (now(), ['aaaaa', 'bbbbb', 'ccccc']);
-INSERT INTO test.arraytest (created_at, strings) VALUES (now(), ['aaaaa', 'bbbbb', null]); -- { clientError 53 }
+INSERT INTO test.arraytest (created_at, strings) VALUES (now(), ['aaaaa', 'bbbbb', null]); -- { clientError 321 }
 
 SELECT strings from test.arraytest;
 
