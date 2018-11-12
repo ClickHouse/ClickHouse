fd9de5bbc1bce8a0e27f99434d800d1c5156c9fb
proller <proller@github.com>
2018-11-12 14:55:10 +0300

    tidy

--- dbms/src/Common/hasLinuxCapability.cpp
+++ dbms/src/Common/hasLinuxCapability.cpp
@@ -2,16 +2,15 @@
 
 #if defined(__linux__)
 
-#include <Common/Exception.h>
-#include <unistd.h>
 #include <syscall.h>
-#include <linux/netlink.h>
+#include <unistd.h>
 #include <linux/capability.h>
+#include <linux/netlink.h>
+#include <Common/Exception.h>
 
 
 namespace DB
 {
-
 namespace ErrorCodes
 {
     extern const int NETLINK_ERROR;
@@ -20,26 +19,24 @@ namespace ErrorCodes
 
 namespace
 {
+    bool hasLinuxCapabilityImpl(decltype(CAP_NET_ADMIN) cap)
+    {
+        /// See man getcap.
+        __user_cap_header_struct request{};
+        request.version = _LINUX_CAPABILITY_VERSION_1; /// It's enough to check just single CAP_NET_ADMIN capability we are interested.
+        request.pid = getpid();
 
-bool hasLinuxCapabilityImpl(decltype(CAP_NET_ADMIN) cap)
-{
-    /// See man getcap.
-    __user_cap_header_struct request{};
-    request.version = _LINUX_CAPABILITY_VERSION_1;  /// It's enough to check just single CAP_NET_ADMIN capability we are interested.
-    request.pid = getpid();
+        __user_cap_data_struct response{};
 
-    __user_cap_data_struct response{};
+        /// Avoid dependency on 'libcap'.
+        if (0 != syscall(SYS_capget, &request, &response))
+            throwFromErrno("Cannot do 'capget' syscall", ErrorCodes::NETLINK_ERROR);
 
-    /// Avoid dependency on 'libcap'.
-    if (0 != syscall(SYS_capget, &request, &response))
-        throwFromErrno("Cannot do 'capget' syscall", ErrorCodes::NETLINK_ERROR);
+        if (!((1 << cap) & response.effective))
+            return false;
 
-    if (!((1 << cap) & response.effective))
-        return false;
-
-    return true;
-}
-
+        return true;
+    }
 }
 
 bool hasLinuxCapability(decltype(CAP_NET_ADMIN) cap)
@@ -47,7 +44,6 @@ bool hasLinuxCapability(decltype(CAP_NET_ADMIN) cap)
     static bool res = hasLinuxCapabilityImpl(cap);
     return res;
 }
-
 }
 
 #endif
--- dbms/src/Common/hasLinuxCapability.h
+++ dbms/src/Common/hasLinuxCapability.h
@@ -4,9 +4,7 @@
 
 namespace DB
 {
-
 bool hasLinuxCapability(decltype(CAP_NET_ADMIN) cap);
-
 }
 
 #endif
