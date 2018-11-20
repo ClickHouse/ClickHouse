949087f879d7d35ac42390fcd175a0b1bdfdc1e6
proller <proller@github.com>
2018-11-20 20:45:12 +0300

    Try fix apple build part 3

--- dbms/src/Core/Field.h
+++ dbms/src/Core/Field.h
@@ -597,7 +597,7 @@ template <> struct NearestFieldType<UInt16>  { using Type = UInt64; };
 template <> struct NearestFieldType<UInt32>  { using Type = UInt64; };
 template <> struct NearestFieldType<UInt64>  { using Type = UInt64; };
 #ifdef __APPLE__
-template <> struct NearestFieldType<time_t>  { using Type = UInt64; };
+template <> struct NearestFieldType<time_t>  { using Type = Int64; };
 template <> struct NearestFieldType<size_t>  { using Type = UInt64; };
 #endif
 template <> struct NearestFieldType<UInt128> { using Type = UInt128; };
