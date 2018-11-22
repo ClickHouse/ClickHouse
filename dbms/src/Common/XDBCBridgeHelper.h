2b2195d4dee5562bf55b456707caa9a11300776f
alesapin <alesapin@gmail.com>
2018-11-22 19:01:17 +0300

    Fix stupid if

--- dbms/src/Common/XDBCBridgeHelper.h
+++ dbms/src/Common/XDBCBridgeHelper.h
@@ -272,8 +272,7 @@ struct ODBCBridgeMixin
 
         std::stringstream command;
 
-#if CLICKHOUSE_SPLIT_BINARY
-#else
+#if !CLICKHOUSE_SPLIT_BINARY
         cmd_args.push_back("odbc-bridge");
 #endif
 
