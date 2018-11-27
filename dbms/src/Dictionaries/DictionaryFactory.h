5fd81f1849909c7ba6734d5585c82d3ea28bd75c
proller <proller@github.com>
2018-11-27 20:27:50 +0300

    format

--- dbms/src/Dictionaries/DictionaryFactory.h
+++ dbms/src/Dictionaries/DictionaryFactory.h
@@ -1,36 +1,35 @@
 #pragma once
 
-#include "IDictionary.h"
 #include <ext/singleton.h>
+#include "IDictionary.h"
 
 namespace Poco
 {
-    namespace Util
-    {
-        class AbstractConfiguration;
-    }
+namespace Util
+{
+    class AbstractConfiguration;
+}
 
-    class Logger;
+class Logger;
 }
 
 namespace DB
 {
-
 class Context;
 
 class DictionaryFactory : public ext::singleton<DictionaryFactory>
 {
 public:
-    DictionaryPtr create(const std::string & name, const Poco::Util::AbstractConfiguration & config,
-        const std::string & config_prefix, Context & context) const;
+    DictionaryPtr
+    create(const std::string & name, const Poco::Util::AbstractConfiguration & config, const std::string & config_prefix, Context & context)
+        const;
 
     using Creator = std::function<DictionaryPtr(
         const std::string & name,
         const DictionaryStructure & dict_struct,
         const Poco::Util::AbstractConfiguration & config,
         const std::string & config_prefix,
-        DictionarySourcePtr source_ptr
-        )>;
+        DictionarySourcePtr source_ptr)>;
 
     void registerLayout(const std::string & layout_type, Creator create_layout);
 
