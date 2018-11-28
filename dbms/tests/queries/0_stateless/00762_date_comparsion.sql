3fabaa28ebd091b25e8bd0e148736ab8fe8bd423
Nikolai Kochetov <nik-kochetov@yandex-team.ru>
2018-11-28 15:20:16 +0300

    Updated test with Date and numbers comparsion.

--- dbms/tests/queries/0_stateless/00762_date_comparsion.sql
+++ dbms/tests/queries/0_stateless/00762_date_comparsion.sql
@@ -1,5 +1,8 @@
 SET send_logs_level = 'none';
 
+select today() < 2018-11-14; -- { serverError 43 }
+select toDate('2018-01-01') < '2018-11-14';
+
 select toDate('2018-01-01') < '2018-01-01';
 select toDate('2018-01-01') == '2018-01-01';
 select toDate('2018-01-01') != '2018-01-01';
@@ -10,4 +13,5 @@ select toDate('2018-01-01') != toDate('2018-01-01');
 select toDate('2018-01-01') < 1;  -- { serverError 43 }
 select toDate('2018-01-01') == 1; -- { serverError 43 }
 select toDate('2018-01-01') != 1; -- { serverError 43 }
+
 
