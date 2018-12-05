caa7a11af9e51a3fa972474783568a46f6717089
Alexey Milovidov <milovidov@yandex-team.ru>
2018-12-05 06:41:44 +0300

    Declarative function signatures (experimental) [#CLICKHOUSE-2]

--- dbms/src/Interpreters/tests/function_signatures.cpp
+++ dbms/src/Interpreters/tests/function_signatures.cpp
@@ -1,8 +1,24 @@
 #include <iostream>
 
 #include <DataTypes/DataTypeFactory.h>
+#include <Parsers/ParserCreateQuery.h>
+#include <Parsers/ASTLiteral.h>
 
 #include <Interpreters/FunctionSignature.h>
+
+
+namespace DB
+{
+    namespace ErrorCodes
+    {
+        extern const int SYNTAX_ERROR;
+    }
+
+    namespace FunctionSignature
+    {
+
+    }
+}
 
 
 using namespace DB;
@@ -10,35 +26,61 @@ using namespace DB;
 int main(int argc, char ** argv)
 try
 {
-    std::string signature(argc >= 2 ? argv[1] : "f(T, T) -> T");
-
-    std::cerr << signature << "\n";
-
-    Tokens tokens(signature.data(), signature.data() + signature.size());
-    TokenIterator it(tokens);
-
-    auto & type_factory = DataTypeFactory::instance();
-
-    FunctionSignature::FunctionSignaturePtr res;
-    if (FunctionSignature::parseFunctionSignature(it, res))
+    if (argc < 3)
     {
-        std::cerr << "Parsed successfully.\n";
-
-        ColumnsWithTypeAndName args
-        {
-            { nullptr, type_factory.get("UInt8"), "" },
-            { nullptr, type_factory.get("String"), "" },
-        };
-
-        auto return_type = res->check(args);
-
-        if (!return_type)
-            std::cerr << "Check fail.\n";
-        else
-            std::cerr << "Return type: " << return_type->getName() << "\n";
+        std::cerr << R"(Usage: ./function_signatures "signature" "Type [value], Type [value]..."\n)";
+        return 1;
     }
-    else
-        std::cerr << "Parse failed.\n";
+
+    std::string signature = argv[1];
+    std::string types_and_constants = argv[2];
+
+    ColumnsWithTypeAndName args;
+    {
+        Tokens tokens(types_and_constants.data(), types_and_constants.data() + types_and_constants.size());
+        TokenIterator pos(tokens);
+
+        auto & type_factory = DataTypeFactory::instance();
+        ParserIdentifierWithOptionalParameters type_parser;
+        ParserLiteral value_parser;
+
+        while (!pos->isEnd())
+        {
+            ASTPtr type_ast;
+            Expected expected;
+            if (!type_parser.parse(pos, type_ast, expected))
+                throw Exception("Cannot parse type", ErrorCodes::SYNTAX_ERROR);
+            DataTypePtr type = type_factory.get(type_ast);
+
+            ASTPtr value_ast;
+            if (value_parser.parse(pos, value_ast, expected))
+                args.emplace_back(type->createColumnConst(1, typeid_cast<const ASTLiteral &>(*value_ast).value), type, "");
+            else
+                args.emplace_back(nullptr, type, "");
+
+            ParserToken(TokenType::Comma).ignore(pos);
+        }
+    }
+
+    {
+        Tokens tokens(signature.data(), signature.data() + signature.size());
+        TokenIterator it(tokens);
+
+        FunctionSignature::FunctionSignaturePtr res;
+        if (FunctionSignature::parseFunctionSignature(it, res))
+        {
+            std::cerr << "Parsed successfully.\n";
+
+            auto return_type = res->check(args);
+
+            if (!return_type)
+                std::cerr << "Check fail.\n";
+            else
+                std::cerr << "Return type: " << return_type->getName() << "\n";
+        }
+        else
+            std::cerr << "Parse failed.\n";
+    }
 
     return 0;
 }
