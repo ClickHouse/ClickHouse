182adf1e40cbcf18f6016c13b82cc44a6669fb9d
alesapin <alesapin@gmail.com>
2018-12-10 21:17:35 +0300

    Spaces instead of tab

--- libs/libglibc-compatibility/musl/getrandom.c
+++ libs/libglibc-compatibility/musl/getrandom.c
@@ -6,5 +6,5 @@
 ssize_t getrandom(void *buf, size_t buflen, unsigned flags)
 {
     /// There was cancellable syscall (syscall_cp), but I don't care too.
-	return syscall(SYS_getrandom, buf, buflen, flags);
+    return syscall(SYS_getrandom, buf, buflen, flags);
 }
