3f3003a1c89b4e417c76c28fdb58fe0180a45346
proller <proller@github.com>
2018-12-12 19:28:16 +0300

    test

--- dbms/tests/queries/0_stateless/00766_parquet.sh
+++ dbms/tests/queries/0_stateless/00766_parquet.sh
@@ -14,18 +14,28 @@ ${CLICKHOUSE_CLIENT} --query="DROP TABLE test.contributors"
 
 ${CLICKHOUSE_CLIENT} --query="DROP TABLE IF EXISTS test.numbers"
 ${CLICKHOUSE_CLIENT} --query="CREATE TABLE test.numbers (number UInt64) ENGINE = Memory"
-${CLICKHOUSE_CLIENT} --query="SELECT * FROM system.numbers LIMIT 65000 FORMAT Parquet" | ${CLICKHOUSE_CLIENT} --query="INSERT INTO test.numbers FORMAT Parquet"
-#TODO fix it with 65k+ limit: Code: 33. DB::Exception: Error while reading parquet data: IOError: Unexpected end of stream: Page was smaller (4277) than expected (524288) 
+# less than default block size (65k)
+${CLICKHOUSE_CLIENT} --query="SELECT * FROM system.numbers LIMIT 10000 FORMAT Parquet" | ${CLICKHOUSE_CLIENT} --query="INSERT INTO test.numbers FORMAT Parquet"
+${CLICKHOUSE_CLIENT} --query="SELECT * FROM test.numbers ORDER BY number DESC LIMIT 10"
+${CLICKHOUSE_CLIENT} --query="TRUNCATE TABLE test.numbers"
 
-#${CLICKHOUSE_CLIENT} --query="SELECT * FROM test.numbers ORDER BY number DESC LIMIT 10 " > ${CLICKHOUSE_TMP}/00766_parquet.numbers
-#diff $CUR_DIR/00766_parquet.reference ${CLICKHOUSE_TMP}/00766_parquet.numbers
-${CLICKHOUSE_CLIENT} --query="SELECT * FROM test.numbers ORDER BY number DESC LIMIT 10 " 
+# More than default block size
+#${CLICKHOUSE_CLIENT} --query="SELECT * FROM system.numbers LIMIT 70000 FORMAT Parquet" | ${CLICKHOUSE_CLIENT} --query="INSERT INTO test.numbers FORMAT Parquet"
+#${CLICKHOUSE_CLIENT} --query="SELECT * FROM test.numbers ORDER BY number DESC LIMIT 10"
+
+${CLICKHOUSE_CLIENT} --query="TRUNCATE TABLE test.numbers"
+${CLICKHOUSE_CLIENT} --max_block_size=2 --query="SELECT * FROM system.numbers LIMIT 3 FORMAT Parquet" | ${CLICKHOUSE_CLIENT} --query="INSERT INTO test.numbers FORMAT Parquet"
+${CLICKHOUSE_CLIENT} --query="SELECT * FROM test.numbers ORDER BY number DESC LIMIT 10"
 
 ${CLICKHOUSE_CLIENT} --query="DROP TABLE test.numbers"
-
 
 ${CLICKHOUSE_CLIENT} --query="DROP TABLE IF EXISTS test.events"
 ${CLICKHOUSE_CLIENT} --query="CREATE TABLE test.events (event String, value UInt64, description String) ENGINE = Memory"
 ${CLICKHOUSE_CLIENT} --query="SELECT * FROM system.events FORMAT Parquet" | ${CLICKHOUSE_CLIENT} --query="INSERT INTO test.events FORMAT Parquet"
 ${CLICKHOUSE_CLIENT} --query="SELECT event, description FROM test.events WHERE event IN ('ContextLock', 'Query') ORDER BY event"
 ${CLICKHOUSE_CLIENT} --query="DROP TABLE test.events"
+
+#${CLICKHOUSE_CLIENT} --max_block_size=1 --query="SELECT * FROM system.numbers LIMIT 10 FORMAT Parquet" > ${CLICKHOUSE_TMP}/t1.pq
+#${CLICKHOUSE_CLIENT} --max_block_size=5 --query="SELECT * FROM system.numbers LIMIT 10 FORMAT Parquet" > ${CLICKHOUSE_TMP}/t5.pq
+#${CLICKHOUSE_CLIENT} --max_block_size=15 --query="SELECT * FROM system.numbers LIMIT 10 FORMAT Parquet" > ${CLICKHOUSE_TMP}/t15.pq
+#${CLICKHOUSE_CLIENT} --query="SELECT * FROM system.numbers LIMIT 100000 FORMAT Parquet" > ${CLICKHOUSE_TMP}/t100000.pq
