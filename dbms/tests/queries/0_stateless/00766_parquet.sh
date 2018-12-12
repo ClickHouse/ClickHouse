1258bc009642186aa59ecf88eeaad521fe013e95
proller <proller@github.com>
2018-12-12 19:32:55 +0300

    fix

--- dbms/tests/queries/0_stateless/00766_parquet.sh
+++ dbms/tests/queries/0_stateless/00766_parquet.sh
@@ -24,7 +24,7 @@ ${CLICKHOUSE_CLIENT} --query="TRUNCATE TABLE test.numbers"
 #${CLICKHOUSE_CLIENT} --query="SELECT * FROM test.numbers ORDER BY number DESC LIMIT 10"
 
 ${CLICKHOUSE_CLIENT} --query="TRUNCATE TABLE test.numbers"
-${CLICKHOUSE_CLIENT} --max_block_size=2 --query="SELECT * FROM system.numbers LIMIT 3 FORMAT Parquet" | ${CLICKHOUSE_CLIENT} --query="INSERT INTO test.numbers FORMAT Parquet"
+#${CLICKHOUSE_CLIENT} --max_block_size=2 --query="SELECT * FROM system.numbers LIMIT 3 FORMAT Parquet" | ${CLICKHOUSE_CLIENT} --query="INSERT INTO test.numbers FORMAT Parquet"
 ${CLICKHOUSE_CLIENT} --query="SELECT * FROM test.numbers ORDER BY number DESC LIMIT 10"
 
 ${CLICKHOUSE_CLIENT} --query="DROP TABLE test.numbers"
