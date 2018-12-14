cc229ac9ccb5372c3b64026613c709fe6a5734b6
alesapin <alesapin@gmail.com>
2018-12-14 14:32:23 +0300

    Remove strange test

--- dbms/tests/queries/0_stateless/00732_compression_codec.sql
+++
@@ -1,9 +0,0 @@
-DROP TABLE IF EXISTS test.compression_codec;
-
-CREATE TABLE test.compression_codec(day Date CODEC(ZSTD), its UInt32 CODEC(Delta(UInt32), LZ4HC(2)))
-
-INSERT INTO test.compression_codec('2018-01-01', '')
-
-SELECT * FROM test.compression_codec;
-
-DROP TABLE IF EXISTS test.compression_codec;
