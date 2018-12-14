972b1c1b8df8d82345a7a8983b30d8052548d7c5
Alexey Milovidov <milovidov@yandex-team.ru>
2018-12-14 22:28:37 +0300

    Always build ODBC bridge as a separate binary #3360

--- dbms/src/Dictionaries/LibraryDictionarySource.cpp
+++ dbms/src/Dictionaries/LibraryDictionarySource.cpp
@@ -135,7 +135,7 @@ LibraryDictionarySource::LibraryDictionarySource(
             "LibraryDictionarySource: Can't load lib " + toString() + ": " + Poco::File(path).path() + " - File doesn't exist",
             ErrorCodes::FILE_DOESNT_EXIST);
     description.init(sample_block);
-    library = std::make_shared<SharedLibrary>(path);
+    library = std::make_shared<SharedLibrary>(path, RTLD_LAZY | RTLD_DEEPBIND);
     settings = std::make_shared<CStringsHolder>(getLibSettings(config, config_prefix + lib_config_settings));
     if (auto libNew = library->tryGet<decltype(lib_data) (*)(decltype(&settings->strings), decltype(&ClickHouseLibrary::log))>(
             "ClickHouseDictionary_v3_libNew"))
