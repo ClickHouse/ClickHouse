7ad3f7c731223244234b671b76e70f1f1fe699a8
alesapin <alesapin@gmail.com>
2018-12-17 11:31:59 +0300

    Debuging codec multiple

--- dbms/src/Compression/CompressionCodecMultiple.cpp
+++ dbms/src/Compression/CompressionCodecMultiple.cpp
@@ -35,9 +35,10 @@ void CompressionCodecMultiple::getCodecDesc(String & codec_desc_)
 size_t CompressionCodecMultiple::getCompressedReserveSize(size_t uncompressed_size)
 {
     for (auto & codec : codecs)
-        uncompressed_size = codec->getCompressedReserveSize(uncompressed_size);
+        uncompressed_size += codec->getCompressedReserveSize(uncompressed_size);
 
-    return sizeof(UInt8) + codecs.size() + uncompressed_size;
+    ///  MultipleCodecByte  TotalCodecs  ByteForEachCodec       data
+    return sizeof(UInt8) + sizeof(UInt8) + codecs.size() + uncompressed_size;
 }
 
 size_t CompressionCodecMultiple::compress(char * source, size_t source_size, char * dest)
@@ -48,11 +49,15 @@ size_t CompressionCodecMultiple::compress(char * source, size_t source_size, cha
     PODArray<char> uncompressed_buf(source_size);
     uncompressed_buf.insert(source, source + source_size);
 
-    dest[0] = static_cast<char>(codecs.size());
-    for (size_t idx = 0; idx < codecs.size(); ++idx)
+    dest[0] = static_cast<char>(getMethodByte());
+    dest[1] = static_cast<char>(codecs.size());
+    std::cerr << "(compress) codecs size:" << codecs.size() << std::endl;
+    std::cerr << "(compress) desc:" << codec_desc << std::endl;
+    size_t codecs_byte_pos = 2;
+    for (size_t idx = 0; idx < codecs.size(); ++idx, ++codecs_byte_pos)
     {
         const auto codec = codecs[idx];
-        dest[idx + 1] = codec->getMethodByte();
+        dest[codecs_byte_pos] = codec->getMethodByte();
         compressed_buf.resize(without_method_header_size + codec->getCompressedReserveSize(source_size));
 
         size_t size_compressed = without_method_header_size;
@@ -66,21 +71,24 @@ size_t CompressionCodecMultiple::compress(char * source, size_t source_size, cha
         source_size = size_compressed;
     }
 
-    memcpy(&dest[codecs.size() + 1], &compressed_buf[0], source_size);
+    memcpy(&dest[2 + codecs.size()], &compressed_buf[0], source_size);
     return source_size;
 }
 
 size_t CompressionCodecMultiple::decompress(char * source, size_t source_size, char * dest, size_t decompressed_size)
 {
-    UInt8 compression_methods_size = source[0];
+    UInt8 compression_methods_size = source[1];
+    std::cerr << "(decompress) Methods size:" << +compression_methods_size << std::endl;
 
     PODArray<char> compressed_buf;
     PODArray<char> uncompressed_buf;
-    compressed_buf.insert(&source[compression_methods_size + 1], &source[source_size - (compression_methods_size + 1)]);
+    compressed_buf.insert(&source[compression_methods_size + 2], &source[source_size - (compression_methods_size + 2)]);
 
     for (size_t idx = 0; idx < compression_methods_size; ++idx)
     {
-        UInt8 compression_method = source[idx + 1];
+        UInt8 compression_method = source[idx + 2];
+        std::cerr << std::hex;
+        std::cerr << "(decompress) Compression method byte:" << +compression_method << std::endl;
         const auto codec = CompressionCodecFactory::instance().get(compression_method);
         codec->decompress(&compressed_buf[8], 0, uncompressed_buf.data(), 0);
         uncompressed_buf.swap(compressed_buf);
@@ -88,6 +96,13 @@ size_t CompressionCodecMultiple::decompress(char * source, size_t source_size, c
 
     memcpy(dest, uncompressed_buf.data(), decompressed_size);
     return decompressed_size;
+}
+
+void registerCodecMultiple(CompressionCodecFactory & factory)
+{
+    factory.registerSimpleCompressionCodec("Multiple", static_cast<char>(CompressionMethodByte::Multiple), [&](){
+        return std::make_shared<CompressionCodecMultiple>();
+    });
 }
 
 }
--- dbms/tests/queries/0_stateless/00804_test_custom_compression_codecs.sql
+++ dbms/tests/queries/0_stateless/00804_test_custom_compression_codecs.sql
@@ -2,6 +2,7 @@ SET send_logs_level = 'none';
 DROP TABLE IF EXISTS test.compression_codec;
 
 CREATE TABLE test.compression_codec(id UInt64 CODEC(LZ4), data String CODEC(ZSTD), ddd Date CODEC(NONE), somenum Float64 CODEC(ZSTD(2))) ENGINE = MergeTree() ORDER BY tuple();
+CREATE TABLE test.compression_codec(id UInt64 CODEC(LZ4, ZSTD, NONE), data String CODEC(ZSTD), ddd Date CODEC(NONE), somenum Float64 CODEC(ZSTD(2))) ENGINE = MergeTree() ORDER BY tuple();
 
 INSERT INTO test.compression_codec VALUES(1, 'hello', toDate('2018-12-14'), 1.1);
 INSERT INTO test.compression_codec VALUES(2, 'world', toDate('2018-12-15'), 2.2);
