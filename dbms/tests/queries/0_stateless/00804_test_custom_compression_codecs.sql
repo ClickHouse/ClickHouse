1406ef5ab8935093554b7c67f56b582db0b53a9d
alesapin <alesapin@gmail.com>
2018-12-18 17:34:18 +0300

    More tests on compression codecs

--- dbms/tests/queries/0_stateless/00804_test_custom_compression_codecs.sql
+++ dbms/tests/queries/0_stateless/00804_test_custom_compression_codecs.sql
@@ -1,8 +1,8 @@
 SET send_logs_level = 'none';
+
 DROP TABLE IF EXISTS test.compression_codec;
 
 CREATE TABLE test.compression_codec(id UInt64 CODEC(LZ4), data String CODEC(ZSTD), ddd Date CODEC(NONE), somenum Float64 CODEC(ZSTD(2))) ENGINE = MergeTree() ORDER BY tuple();
-CREATE TABLE test.compression_codec(id UInt64 CODEC(LZ4, ZSTD, NONE), data String CODEC(ZSTD), ddd Date CODEC(NONE), somenum Float64 CODEC(ZSTD(2))) ENGINE = MergeTree() ORDER BY tuple();
 
 INSERT INTO test.compression_codec VALUES(1, 'hello', toDate('2018-12-14'), 1.1);
 INSERT INTO test.compression_codec VALUES(2, 'world', toDate('2018-12-15'), 2.2);
@@ -29,3 +29,44 @@ CREATE TABLE test.params_when_no_params(id UInt64 CODEC(LZ4(1))) ENGINE = MergeT
 DROP TABLE IF EXISTS test.bad_codec;
 DROP TABLE IF EXISTS test.params_when_no_params;
 DROP TABLE IF EXISTS test.too_many_params;
+
+
+DROP TABLE IF EXISTS test.compression_codec_multiple;
+
+CREATE TABLE test.compression_codec_multiple (
+    id UInt64 CODEC(LZ4, ZSTD, NONE),
+    data String CODEC(ZSTD, NONE, LZ4, LZ4),
+    ddd Date CODEC(NONE, NONE, NONE, LZ4, ZSTD),
+    somenum Float64 CODEC(LZ4, LZ4, ZSTD, ZSTD, ZSTD)
+) ENGINE = MergeTree() ORDER BY tuple();
+
+INSERT INTO test.compression_codec_multiple VALUES (1, 'world', toDate('2018-10-05'), 1.1), (2, 'hello', toDate('2018-10-01'), 2.2), (3, 'buy', toDate('2018-10-11'), 3.3);
+
+SELECT * FROM test.compression_codec_multiple ORDER BY id;
+
+INSERT INTO test.compression_codec_multiple select modulo(number, 100), toString(number), toDate('2018-12-01'), 5.5 * number from system.numbers limit 10000;
+
+SELECT count(*) from test.compression_codec_multiple;
+
+SELECT count(distinct data) from test.compression_codec_multiple;
+
+SELECT floor(sum(somenum), 1) from test.compression_codec_multiple;
+
+TRUNCATE TABLE test.compression_codec_multiple;
+
+INSERT INTO test.compression_codec_multiple select modulo(number, 100), toString(number), toDate('2018-12-01'), 5.5 * number from system.numbers limit 10000;
+
+SELECT sum(cityHash64(*)) from test.compression_codec_multiple;
+
+DROP TABLE IF EXISTS test.compression_codec_multiple_more_types;
+
+CREATE TABLE test.compression_codec_multiple_more_types (
+    id Decimal128(13) CODEC(ZSTD, LZ4, ZSTD, ZSTD),
+    data FixedString(12) CODEC(ZSTD, ZSTD, NONE, NONE, NONE),
+    ddd Nested (age UInt8, Name String) CODEC(LZ4, NONE, NONE, NONE, ZSTD)
+) ENGINE = MergeTree() ORDER BY tuple();
+
+INSERT INTO test.compression_codec_multiple_more_types VALUES(1.5555555555555, 'hello world!', [77], ['John']);
+INSERT INTO test.compression_codec_multiple_more_types VALUES(7.1, 'xxxxxxxxxxxx', [127], ['Henry']);
+
+SELECT * FROM test.compression_codec_multiple_more_types order by id;
