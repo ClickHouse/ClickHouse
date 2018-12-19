fb5dbb7959697970342f6dddce2d64e82980aac3
Sergey V. Galtsev <sergey-v-galtsev@github.com>
2018-12-20 01:47:42 +0300

    Do not require the server to fail to start in the test.

--- dbms/tests/integration/test_match_process_uid_against_data_owner/test.py
+++ dbms/tests/integration/test_match_process_uid_against_data_owner/test.py
@@ -13,16 +13,16 @@ def test_different_user():
     if current_user_id != 0:
         return
 
+    other_user_id = pwd.getpwnam('nobody').pw_uid
+
     cluster = ClickHouseCluster(__file__)
     node = cluster.add_instance('node')
 
-    with pytest.raises(Exception):
-        cluster.start()
-
-    other_user_id = pwd.getpwnam('nobody').pw_uid
+    cluster.start()
 
     docker_api = docker.from_env().api
     container = node.get_docker_handle()
+    container.stop()
     container.start()
     container.exec_run('chown {} /var/lib/clickhouse'.format(other_user_id), privileged=True)
     container.exec_run(CLICKHOUSE_START_COMMAND)
@@ -30,8 +30,8 @@ def test_different_user():
     cluster.shutdown() # cleanup
 
     with open(os.path.join(node.path, 'logs/clickhouse-server.err.log')) as log:
+        expected_message = "Effective user of the process \(.*\) does not match the owner of the data \(.*\)\. Run under 'sudo -u .*'\."
         last_message = log.readlines()[-1].strip()
-        expected_message = 'Effective user of the process \(.*\) does not match the owner of the data \(.*\)'
 
         if re.search(expected_message, last_message) is None:
             pytest.fail('Expected the server to fail with a message "{}", but the last message is "{}"'.format(expected_message, last_message))
