b5b6abcd969d1b97d59ec14bac430cb80ab5098a
Alexey Milovidov <milovidov@yandex-team.ru>
2018-12-22 19:19:16 +0300

    Added test #3909

---
+++ dbms/tests/queries/0_stateless/00808_array_enumerate_segfault.sql
@@ -0,0 +1,3 @@
+SELECT arrayEnumerateUniq(anyHeavy([]), []);
+SELECT arrayEnumerateDense([], [sequenceCount(NULL)]); -- { serverError 190 }
+SELECT arrayEnumerateDense([STDDEV_SAMP(NULL, 910947.571364)], [NULL]);
