7678db6e9da09d40fc9a4f0d8ed3d49a70e24ee1
Alexey Milovidov <milovidov@yandex-team.ru>
2018-12-24 01:20:44 +0300

    Added a test for already fixed bug [#CLICKHOUSE-2]

--- dbms/tests/queries/bugs/fuzzy.sql
+++ dbms/tests/queries/bugs/fuzzy.sql
@@ -1,9 +1,6 @@
 SELECT sequenceCount((CAST((( SELECT NULL ) AS rg, ( SELECT ( SELECT [], '<e', caseWithExpr([NULL], -588755.149, []), retention(addWeeks((CAST((-7644612.39732) AS DateTime)), -23578040.02833), (CAST(([]) AS DateTime)), (CAST(([010977.08]) AS String))), emptyArrayToSingle('') ) , '\0', toUInt64([], 't3hw@'), '\0', toStartOfQuarter(-4230.1872, []) ) ) AS Date)));
-SELECT globalNotIn(['"wh'], [NULL]);
-SELECT globalIn([''], [NULL])
 SELECT ( SELECT toDecimal128([], rowNumberInBlock()) ) , lcm('', [[(CAST(('>A') AS String))]]);
 SELECT truncate(895, -16);
-SELECT notIn([['']], [[NULL]]);
 SELECT subtractDays((CAST((-5263074.47) AS DateTime)), -737895);
 SELECT quantileDeterministic([], findClusterIndex(( SELECT subtractDays((CAST((566450.398706) AS DateTime)), 54) ) )), '\0', [];
 SELECT addDays((CAST((96.338) AS DateTime)), -3);
