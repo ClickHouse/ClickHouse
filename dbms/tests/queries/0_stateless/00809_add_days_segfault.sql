fd3679529463806b8be8c591362171f5bbcc493d
Alexey Milovidov <milovidov@yandex-team.ru>
2018-12-24 01:10:48 +0300

    Added test #3913

---
+++ dbms/tests/queries/0_stateless/00809_add_days_segfault.sql
@@ -0,0 +1,6 @@
+SELECT ignore(addDays(toDateTime(0), -1));
+SELECT ignore(subtractDays(toDateTime(0), 1));
+SELECT ignore(addDays((CAST((96.338) AS DateTime)), -3));
+
+SELECT ignore(addDays(toDate(0), -1));
+SELECT ignore(subtractDays(toDate(0), 1));
