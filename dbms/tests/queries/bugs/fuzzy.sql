563063a0525fe9f936de3a9bc201fbf65abd7d54
Alexey Milovidov <milovidov@yandex-team.ru>
2018-12-24 01:20:44 +0300

    Added a test for already fixed bug [#CLICKHOUSE-2]

--- dbms/tests/queries/bugs/fuzzy.sql
+++ dbms/tests/queries/bugs/fuzzy.sql
@@ -1,5 +1,6 @@
 SELECT globalNotIn(['"wh'], [NULL]);
 SELECT globalIn([''], [NULL])
+SELECT notIn([['']], [[NULL]]);
+
 SELECT ( SELECT toDecimal128([], rowNumberInBlock()) ) , lcm('', [[(CAST(('>A') AS String))]]);
 SELECT truncate(895, -16);
-SELECT notIn([['']], [[NULL]]);
