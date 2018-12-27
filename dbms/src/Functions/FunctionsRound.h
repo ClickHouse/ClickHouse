e308d2e069f8a7eef2cafba5c24c8a3763c4775b
Alexey Milovidov <milovidov@yandex-team.ru>
2018-12-27 03:10:38 +0300

    Addition to prev. revision #3926

--- dbms/src/Functions/FunctionsRound.h
+++ dbms/src/Functions/FunctionsRound.h
@@ -4,6 +4,7 @@
 #include <IO/WriteHelpers.h>
 #include <DataTypes/getLeastSupertype.h>
 #include <DataTypes/DataTypeArray.h>
+#include <DataTypes/DataTypesNumber.h>
 #include <Columns/ColumnVector.h>
 #include <Interpreters/castColumn.h>
 
