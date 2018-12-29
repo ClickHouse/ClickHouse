68c3879d956e5996e9ec39e91a5cd960475ebb88
Nikita Vasilev <nekitvas@gmail.com>
2018-12-29 14:12:41 +0300

    index condition

--- dbms/src/Storages/MergeTree/MergeTreeIndexes.h
+++ dbms/src/Storages/MergeTree/MergeTreeIndexes.h
@@ -6,42 +6,74 @@
 #include <memory>
 #include <Core/Block.h>
 #include <ext/singleton.h>
+#include <Storages/SelectQueryInfo.h>
+#include <Storages/MergeTree/MarkRange.h>
 #include <Interpreters/ExpressionActions.h>
 #include <Parsers/ASTIndexDeclaration.h>
 
 namespace DB
 {
 
-enum class INDEX_TYPES {
+enum class INDEX_TYPE {
     NONE = 0
 };
 
 
 class MergeTreeIndex;
 
+using MergeTreeIndexPtr = std::shared_ptr<MergeTreeIndex>;
+using MergeTreeIndexes = std::vector<MergeTreeIndexPtr>;
 
+
+/// Data structure storing some data for each MergeTreeDataPart
 struct MergeTreeIndexPart
 {
     friend MergeTreeIndex;
 
 public:
-    virtual ~MergeTreeIndexPart() {};
+    virtual ~MergeTreeIndexPart() = default;
 
-    virtual void update(const Block & block, const Names & column_names) = 0;
-    virtual void merge(const MergeTreeIndexPart & other) = 0;
+    virtual INDEX_TYPE indexType() const;
 
-    virtual INDEX_TYPES indexType() const {
-        return INDEX_TYPES::NONE;
-    }
+    void update(const Block & block, const Names & column_names);
+    void merge(const MergeTreeIndexPart & other);
 
 protected:
-    MergeTreeIndexPart() {};
+    MergeTreeIndexPart() = default;
+
+    virtual void updateImpl(const Block & block, const Names & column_names) = 0;
+    virtual void mergeImpl(const MergeTreeIndexPart & other) = 0;
+
+    MergeTreeIndexPtr owner;
 };
 
-using MergeTreeIndexPartPtr = std::unique_ptr<MergeTreeIndexPart>;
+using MergeTreeIndexPartPtr = std::shared_ptr<MergeTreeIndexPart>;
 using MergeTreeIndexParts = std::vector<MergeTreeIndexPartPtr>;
 
 
+/// Condition on the index.
+class IndexCondition {
+    friend MergeTreeIndex;
+
+public:
+    virtual ~IndexCondition() = default;
+
+    virtual INDEX_TYPE indexType() const;
+
+    // methods like KeyCondition
+    virtual bool alwaysUnknownOrTrue() const = 0;
+    virtual bool maybeTrueInRange(const MarkRange & range) const = 0;
+
+protected:
+    IndexCondition() = default;
+
+    MergeTreeIndexPtr owner;
+};
+
+using IndexConditionPtr = std::shared_ptr<IndexCondition>;
+
+
+/// Structure for storing index info like columns, expression, arguments, ...
 class MergeTreeIndex
 {
 public:
@@ -50,22 +82,19 @@ public:
 
     virtual ~MergeTreeIndex() {};
 
-    virtual INDEX_TYPES indexType() const {
-        return INDEX_TYPES::NONE;
-    }
-
-    virtual bool alwaysUnknownOrTrue() const = 0;
-    virtual bool maybeTrueOn(MergeTreeIndexPartPtr part) const = 0;
+    virtual INDEX_TYPE indexType() const = 0;
 
     virtual MergeTreeIndexPartPtr createEmptyIndexPart() const = 0;
+    virtual IndexConditionPtr createIndexCondition(const SelectQueryInfo & query_info,
+                                                const Context & context,
+                                                const Names & key_column_names,
+                                                const ExpressionActionsPtr & key_expr) const = 0;
 
     String name;
     ExpressionActionsPtr expr;
     Block sample;
 };
 
-using MergeTreeIndexPtr = std::unique_ptr<MergeTreeIndex>;
-using MergeTreeIndexes = std::vector<MergeTreeIndexPtr>;
 
 class MergeTreeIndexFactory : public ext::singleton<MergeTreeIndexFactory>
 {
@@ -83,7 +112,7 @@ public:
     }
 
 protected:
-    MergeTreeIndexFactory() {};
+    MergeTreeIndexFactory() = default;
 
 private:
     using Indexes = std::unordered_map<std::string, Creator>;
