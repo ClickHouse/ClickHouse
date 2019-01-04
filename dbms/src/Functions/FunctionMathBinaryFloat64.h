4622f5dc7c4f6deca019ea4affc47fd9874649a3
Alexey Milovidov <milovidov@yandex-team.ru>
2019-01-04 16:32:08 +0300

    Added some warnings from clang's -Weverything

--- dbms/src/Functions/FunctionMathBinaryFloat64.h
+++ dbms/src/Functions/FunctionMathBinaryFloat64.h
@@ -146,7 +146,7 @@ private:
             block.getByPosition(result).column = std::move(dst);
             return true;
         }
-        else if (const auto right_arg_typed = checkAndGetColumnConst<ColumnVector<RightType>>(right_arg))
+        if (const auto right_arg_typed = checkAndGetColumnConst<ColumnVector<RightType>>(right_arg))
         {
             auto dst = ColumnVector<Float64>::create();
 
@@ -205,7 +205,7 @@ private:
                 throw Exception{"Illegal column " + right_arg->getName() + " of second argument of function " + getName(),
                     ErrorCodes::ILLEGAL_COLUMN};
             }
-            else if (const auto left_arg_typed = checkAndGetColumnConst<ColVecLeft>(left_arg))
+            if (const auto left_arg_typed = checkAndGetColumnConst<ColVecLeft>(left_arg))
             {
                 if (executeTyped<LeftType, RightType>(block, result, left_arg_typed, right_arg))
                     return true;
