c93b54a88d80cd0d1b42ba0253cfbfa52cec4b47
Alexey Milovidov <milovidov@yandex-team.ru>
2019-01-09 19:32:34 +0300

    Removed obsolete tweak, because KDevelop has migrated to clang parser long time ago

--- libs/libcommon/include/common/Types.h
+++ libs/libcommon/include/common/Types.h
@@ -1,6 +1,5 @@
 #pragma once
 #include <cstdint>
-#include <cstddef>
 
 using Int8 = int8_t;
 using Int16 = int16_t;
@@ -11,9 +10,3 @@ using UInt8 = uint8_t;
 using UInt16 = uint16_t;
 using UInt32 = uint32_t;
 using UInt64 = uint64_t;
-
-/// Workaround for the issue, that KDevelop doesn't see time_t and size_t types (for syntax highlight).
-#ifdef IN_KDEVELOP_PARSER
-    using time_t = Int64;
-    using size_t = UInt64;
-#endif
