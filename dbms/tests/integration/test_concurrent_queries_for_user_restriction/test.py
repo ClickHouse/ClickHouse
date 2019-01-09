b8efafd400d37c0be10706c6485997c51a78386e
alesapin <alesapin@gmail.com>
2019-01-09 15:21:04 +0300

    Fix bug with wrong user restrictions in remote table func

--- dbms/tests/integration/test_concurrent_queries_for_user_restriction/test.py
+++ dbms/tests/integration/test_concurrent_queries_for_user_restriction/test.py
@@ -22,33 +22,26 @@ def started_cluster():
         node1.query("create table nums (number UInt64) ENGINE = MergeTree() order by tuple()")
         node1.query("insert into nums values(0),(1)")
 
+        node2.query("create table nums (number UInt64) ENGINE = MergeTree() order by tuple()")
+        node2.query("insert into nums values(0),(1)")
+
         yield cluster
     finally:
         cluster.shutdown()
 
-def num_getter(num):
-    if num % 2 == 0:
-        return node1
-    else:
-        return node2
-
-@pytest.mark.parametrize("node_getter", [
-    (lambda _: node1),
-    (lambda _: node2),
-    (num_getter),
-])
-def test_exception_message(started_cluster, node_getter):
+def test_exception_message(started_cluster):
     assert node1.query("select ID from distr_tbl order by ID") == "0\n1\n"
     assert node1.query("select number from nums order by number") == "0\n1\n"
-    try:
-        p = Pool(10)
-        def query(num):
-            node = node_getter(num)
-            node.query(
-                    "select sleep(2) from distr_tbl where ID GLOBAL IN (select number from remote('node1', 'default', 'nums'))",
-                    user='good')
 
-        p.map(query, xrange(3))
+    def node_busy(_):
+        for i in xrange(10):
+            node1.query("select sleep(2)", user='default')
+
+    busy_pool = Pool(3)
+    busy_pool.map_async(node_busy, xrange(3))
+    time.sleep(1) # wait a little until polling start
+    try:
+        assert node2.query("select number from remote('node1', 'default', 'nums')", user='good') == "0\n1\n"
     except Exception as ex:
-        assert 'Too many simultaneous queries for user good.' in ex.message
         print ex.message
+        assert False, "Exception thrown while max_concurrent_queries_for_user is not exceeded"
