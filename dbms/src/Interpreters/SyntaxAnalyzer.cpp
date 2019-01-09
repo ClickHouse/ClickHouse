7ba268049aca6eb21e327a5bb7de05ede90ffc2f
chertus <chertus@gmail.com>
2019-01-09 20:06:40 +0300

    extract OptimizeIfWithConstantConditionVisitor from SyntaxAnalyzer

--- dbms/src/Interpreters/SyntaxAnalyzer.cpp
+++ dbms/src/Interpreters/SyntaxAnalyzer.cpp
@@ -11,6 +11,7 @@
 #include <Interpreters/ExecuteScalarSubqueriesVisitor.h>
 #include <Interpreters/PredicateExpressionsOptimizer.h>
 #include <Interpreters/ExternalDictionaries.h>
+#include <Interpreters/OptimizeIfWithConstantConditionVisitor.h>
 
 #include <Parsers/ASTSelectQuery.h>
 #include <Parsers/ASTLiteral.h>
@@ -34,7 +35,6 @@ namespace DB
 
 namespace ErrorCodes
 {
-    extern const int NUMBER_OF_ARGUMENTS_DOESNT_MATCH;
     extern const int ALIAS_REQUIRED;
     extern const int MULTIPLE_EXPRESSIONS_FOR_ALIAS;
     extern const int EMPTY_NESTED_TABLE;
@@ -207,99 +207,6 @@ void executeScalarSubqueries(ASTPtr & query, const Context & context, size_t sub
     LogAST log;
     ExecuteScalarSubqueriesVisitor::Data visitor_data{context, subquery_depth};
     ExecuteScalarSubqueriesVisitor(visitor_data, log.stream()).visit(query);
-}
-
-bool tryExtractConstValueFromCondition(const ASTPtr & condition, bool & value)
-{
-    /// numeric constant in condition
-    if (const ASTLiteral * literal = typeid_cast<ASTLiteral *>(condition.get()))
-    {
-        if (literal->value.getType() == Field::Types::Int64 ||
-            literal->value.getType() == Field::Types::UInt64)
-        {
-            value = literal->value.get<Int64>();
-            return true;
-        }
-    }
-
-    /// cast of numeric constant in condition to UInt8
-    if (const ASTFunction * function = typeid_cast<ASTFunction * >(condition.get()))
-    {
-        if (function->name == "CAST")
-        {
-            if (ASTExpressionList * expr_list = typeid_cast<ASTExpressionList *>(function->arguments.get()))
-            {
-                const ASTPtr & type_ast = expr_list->children.at(1);
-                if (const ASTLiteral * type_literal = typeid_cast<ASTLiteral *>(type_ast.get()))
-                {
-                    if (type_literal->value.getType() == Field::Types::String &&
-                        type_literal->value.get<std::string>() == "UInt8")
-                        return tryExtractConstValueFromCondition(expr_list->children.at(0), value);
-                }
-            }
-        }
-    }
-
-    return false;
-}
-
-/// Remove Function_if AST if condition is constant.
-void optimizeIfWithConstantCondition(ASTPtr & current_ast, SyntaxAnalyzerResult::Aliases & aliases)
-{
-    if (!current_ast)
-        return;
-
-    for (ASTPtr & child : current_ast->children)
-    {
-        auto * function_node = typeid_cast<ASTFunction *>(child.get());
-        if (!function_node || function_node->name != "if")
-        {
-            optimizeIfWithConstantCondition(child, aliases);
-            continue;
-        }
-
-        optimizeIfWithConstantCondition(function_node->arguments, aliases);
-        auto * args = typeid_cast<ASTExpressionList *>(function_node->arguments.get());
-
-        if (args->children.size() != 3)
-            throw Exception("Wrong number of arguments for function 'if' (" + toString(args->children.size()) + " instead of 3)",
-                            ErrorCodes::NUMBER_OF_ARGUMENTS_DOESNT_MATCH);
-
-        ASTPtr condition_expr = args->children[0];
-        ASTPtr then_expr = args->children[1];
-        ASTPtr else_expr = args->children[2];
-
-        bool condition;
-        if (tryExtractConstValueFromCondition(condition_expr, condition))
-        {
-            ASTPtr replace_ast = condition ? then_expr : else_expr;
-            ASTPtr child_copy = child;
-            String replace_alias = replace_ast->tryGetAlias();
-            String if_alias = child->tryGetAlias();
-
-            if (replace_alias.empty())
-            {
-                replace_ast->setAlias(if_alias);
-                child = replace_ast;
-            }
-            else
-            {
-                /// Only copy of one node is required here.
-                /// But IAST has only method for deep copy of subtree.
-                /// This can be a reason of performance degradation in case of deep queries.
-                ASTPtr replace_ast_deep_copy = replace_ast->clone();
-                replace_ast_deep_copy->setAlias(if_alias);
-                child = replace_ast_deep_copy;
-            }
-
-            if (!if_alias.empty())
-            {
-                auto alias_it = aliases.find(if_alias);
-                if (alias_it != aliases.end() && alias_it->second.get() == child_copy.get())
-                    alias_it->second = child;
-            }
-        }
-    }
 }
 
 /** Calls to these functions in the GROUP BY statement would be
@@ -883,7 +790,7 @@ SyntaxAnalyzerResultPtr SyntaxAnalyzer::analyze(
     executeScalarSubqueries(query, context, subquery_depth);
 
     /// Optimize if with constant condition after constants was substituted instead of sclalar subqueries.
-    optimizeIfWithConstantCondition(query, result.aliases);
+    OptimizeIfWithConstantConditionVisitor(result.aliases).visit(query);
 
     /// GROUP BY injective function elimination.
     optimizeGroupBy(select_query, source_columns_set, context);
