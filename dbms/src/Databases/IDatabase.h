9031be381ad9965dbe7ab1a528621b77d50e28e9
Alexey Milovidov <milovidov@yandex-team.ru>
2019-01-11 19:58:43 +0300

    Attempt to implemnt global thread pool #4018

--- dbms/src/Databases/IDatabase.h
+++ dbms/src/Databases/IDatabase.h
@@ -8,10 +8,8 @@
 #include <functional>
 #include <Poco/File.h>
 #include <Common/escapeForFileName.h>
+#include <common/ThreadPool.h>
 #include <Interpreters/Context.h>
-
-
-class ThreadPool;
 
 
 namespace DB
