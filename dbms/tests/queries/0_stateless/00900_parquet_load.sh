abc6e6c7ad37acc03374ea09bd10b11ce345b137
proller <proller@github.com>
2019-01-15 12:47:22 +0300

    wip

--- dbms/tests/queries/0_stateless/00900_parquet_load.sh
+++ dbms/tests/queries/0_stateless/00900_parquet_load.sh
@@ -21,16 +21,13 @@ DATA_DIR=$CUR_DIR/data_parquet
 # BUG! nulls.snappy.parquet
 # why? repeated_no_annotation.parquet
 
-#for DATA_SOURCE_DIR in $ROOT_DIR/contrib/arrow/cpp/submodules/parquet-testing/data $ROOT_DIR/contrib/arrow/python/pyarrow/tests/data/parquet; do
-#  for NAME in `ls -1 $DATA_SOURCE_DIR/*.parquet | xargs -n 1 basename | sort --stable`; do
-    #[ ! -f "$DATA_DIR/$NAME" ] && DATA_DIR=$DATA_SOURCE_DIR
 for NAME in `ls -1 $DATA_DIR/*.parquet | xargs -n 1 basename | sort`; do
     echo === Try load data from $NAME
 
     JSON=$DATA_DIR/$NAME.json
 
-    # To update:
-    # [ -n "$BUILD_DIR" ] && $BUILD_DIR/contrib/arrow-cmake/parquet-reader --json $DATA_DIR/$NAME > $JSON
+    # If you want change or add .parquet file - rm data_parquet/*.json
+    [ -n "$BUILD_DIR" ] && [ ! -e $JSON ] && $BUILD_DIR/contrib/arrow-cmake/parquet-reader --json $DATA_DIR/$NAME > $JSON
 
     # Debug only:
     # [ -n "$BUILD_DIR" ] && $BUILD_DIR/contrib/arrow-cmake/parquet-reader $DATA_DIR/$NAME > $DATA_DIR/$NAME.dump
@@ -45,5 +42,4 @@ for NAME in `ls -1 $DATA_DIR/*.parquet | xargs -n 1 basename | sort`; do
 
     ${CLICKHOUSE_CLIENT} --query="SELECT * FROM test.parquet_load"
     ${CLICKHOUSE_CLIENT} --query="DROP TABLE test.parquet_load"
-# done
 done
