9220cfdfd94476fcd728fbbf3440e316eac96205
comunodi <alex_kote@bk.ru>
2019-01-16 01:08:56 +0300

    Add basic functionality for dict

---
+++ dbms/src/Dictionaries/RedisDictionarySource.h
@@ -0,0 +1,84 @@
+#pragma once
+
+#include <Common/config.h>
+#if USE_POCO_REDIS
+
+#    include "DictionaryStructure.h"
+#    include "IDictionarySource.h"
+
+namespace Poco
+{
+    namespace Util
+    {
+        class AbstractConfiguration;
+    }
+
+    namespace Redis
+    {
+        class Client;
+    }
+}
+
+
+namespace DB
+{
+/// Allows loading dictionaries from a Redis collection
+    class RedisDictionarySource final : public IDictionarySource
+    {
+        RedisDictionarySource(
+                const DictionaryStructure & dict_struct,
+                const std::string & host,
+                UInt16 port,
+                const Block & sample_block);
+
+    public:
+        RedisDictionarySource(
+                const DictionaryStructure & dict_struct,
+                const Poco::Util::AbstractConfiguration & config,
+                const std::string & config_prefix,
+                Block & sample_block);
+
+        RedisDictionarySource(const RedisDictionarySource & other);
+
+        ~RedisDictionarySource() override;
+
+        BlockInputStreamPtr loadAll() override;
+
+        BlockInputStreamPtr loadUpdatedAll() override
+        {
+            throw Exception{"Method loadUpdatedAll is unsupported for RedisDictionarySource", ErrorCodes::NOT_IMPLEMENTED};
+        }
+
+        bool supportsSelectiveLoad() const override { return true; }
+
+        BlockInputStreamPtr loadIds(const std::vector<UInt64> & /* ids */) override {throw 1;};
+
+        BlockInputStreamPtr loadKeys(const Columns & /* key_columns */, const std::vector<size_t> & /* requested_rows */) override {throw 1;};
+
+        /// @todo: for Redis, modification date can somehow be determined from the `_id` object field
+        bool isModified() const override { return true; }
+
+        ///Not yet supported
+        bool hasUpdateField() const override { return false; }
+
+        DictionarySourcePtr clone() const override { return std::make_unique<RedisDictionarySource>(*this); }
+
+        std::string toString() const override;
+
+    private:
+        const DictionaryStructure dict_struct;
+        const std::string host;
+        const UInt16 port;
+        Block sample_block;
+
+        std::shared_ptr<Poco::Redis::Client> client;
+    };
+
+}
+#endif
+
+/*namespace DB
+{
+class DictionarySourceFactory;
+void registerDictionarySourceRedis(DictionarySourceFactory & factory);
+}*/
