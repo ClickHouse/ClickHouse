b5dc9a45b8cd1c5c3a3513313f55af85bd30ca4a
Alexey Milovidov <milovidov@yandex-team.ru>
2019-01-16 03:19:47 +0300

    Removed test case that reproduces a bug

--- dbms/tests/queries/0_stateless/00804_rollup_with_having.sql
+++ dbms/tests/queries/0_stateless/00804_rollup_with_having.sql
@@ -10,6 +10,3 @@ INSERT INTO test.rollup_having VALUES ('a', 'b');
 
 SELECT a, b, count(*) FROM test.rollup_having GROUP BY a, b WITH ROLLUP HAVING a IS NOT NULL;
 SELECT a, b, count(*) FROM test.rollup_having GROUP BY a, b WITH ROLLUP HAVING a IS NOT NULL and b IS NOT NULL;
-
-SELECT a, b, count(*) FROM test.rollup_having GROUP BY a, b WITH ROLLUP WITH TOTALS HAVING a IS NOT NULL;
-SELECT a, b, count(*) FROM test.rollup_having GROUP BY a, b WITH ROLLUP WITH TOTALS HAVING a IS NOT NULL and b IS NOT NULL;
