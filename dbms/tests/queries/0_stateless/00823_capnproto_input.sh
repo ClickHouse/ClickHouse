8e1b2201a9c056efdd7913505be1f8fb444bf6a0
alexey-milovidov <milovidov@yandex-team.ru>
2019-01-17 18:02:49 +0300

    Update 00823_capnproto_input.sh

--- dbms/tests/queries/0_stateless/00823_capnproto_input.sh
+++ dbms/tests/queries/0_stateless/00823_capnproto_input.sh
@@ -5,7 +5,7 @@ set -e
 CURDIR=$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)
 . $CURDIR/../shell_config.sh
 
-#create the scjema file
+#create the schema file
 echo "@0x803231eaa402b968;struct NestedNestedOne {nestednestednumber @0 :UInt64;}struct NestedNestedTwo {nestednestedtext @0 :Text;}struct NestedOne {nestednestedone @0 :NestedNestedOne;nestednestedtwo @1 :NestedNestedTwo;nestednumber  @2:UInt64;}struct NestedTwo {nestednestedone @0 :NestedNestedOne;nestednestedtwo @1 :    NestedNestedTwo;nestedtext @2 :Text;}struct CapnProto {    number   @0 :UInt64;string       @1 :Text;nestedone @2 : NestedOne;nestedtwo @3 : NestedTwo;nestedthree @4 : NestedNestedTwo;}" > test.capnp
 
 $CLICKHOUSE_CLIENT -q "DROP TABLE IF EXISTS test.capnproto_input"
