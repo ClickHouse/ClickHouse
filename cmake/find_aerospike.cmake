eaca18f5013d630f329cc896082a8e53f2ead430
Tretyakov Gleb <glebx777@gmail.com>
2019-01-19 18:33:16 +0300

    Add submodule

---
+++ cmake/find_aerospike.cmake
@@ -0,0 +1,13 @@
+message("AAAAA")
+
+if (NOT EXISTS "${ClickHouse_SOURCE_DIR}/contrib/aerospike/src/main/_bin.c")
+    message (WARNING "submodule contrib/aerospike is missing. to fix try run: \n git submodule update --init --recursive")
+else()
+    if (NOT EXISTS "${ClickHouse_SOURCE_DIR}/contrib/aerospike")
+        message (WARNING "submodule contrib/aerospike is missing. to fix try run: \n git submodule update --init --recursive")
+    else()
+        set (AEROSPIKE_INCLUDE_DIR ${ClickHouse_SOURCE_DIR}/contrib/aerospike/src/include)
+        set (AEROSPIKE_LIBRARY aerospike)
+        set (USE_AEROSPIKE 1)
+    endif()
+endif ()
