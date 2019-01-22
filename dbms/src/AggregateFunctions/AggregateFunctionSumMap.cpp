3c2d66742ae7653fad0ec4cd29d48e53277f6f8a
Léo Ercolanelli <leo.ercolanelli@contentsquare.com>
2019-01-22 17:47:43 +0100

    implement sumMapFiltered

--- dbms/src/AggregateFunctions/AggregateFunctionSumMap.cpp
+++ dbms/src/AggregateFunctions/AggregateFunctionSumMap.cpp
@@ -12,10 +12,10 @@ namespace DB
 namespace
 {
 
-AggregateFunctionPtr createAggregateFunctionSumMap(const std::string & name, const DataTypes & arguments, const Array & params)
-{
-    assertNoParameters(name, params);
+using SumMapArgs = std::pair<const DataTypePtr &, DataTypes>;
 
+SumMapArgs parseArguments(const std::string & name, const DataTypes & arguments)
+{
     if (arguments.size() < 2)
         throw Exception("Aggregate function " + name + " requires at least two arguments of Array type.",
             ErrorCodes::NUMBER_OF_ARGUMENTS_DOESNT_MATCH);
@@ -24,6 +24,7 @@ AggregateFunctionPtr createAggregateFunctionSumMap(const std::string & name, con
     if (!array_type)
         throw Exception("First argument for function " + name + " must be an array.",
             ErrorCodes::ILLEGAL_TYPE_OF_ARGUMENT);
+
 
     const DataTypePtr & keys_type = array_type->getNestedType();
 
@@ -37,6 +38,15 @@ AggregateFunctionPtr createAggregateFunctionSumMap(const std::string & name, con
         values_types.push_back(array_type->getNestedType());
     }
 
+    return  {keys_type, std::move(values_types)};
+}
+
+AggregateFunctionPtr createAggregateFunctionSumMap(const std::string & name, const DataTypes & arguments, const Array & params)
+{
+    assertNoParameters(name, params);
+
+    auto [keys_type, values_types] = parseArguments(name, arguments);
+
     AggregateFunctionPtr res(createWithNumericBasedType<AggregateFunctionSumMap>(*keys_type, keys_type, values_types));
     if (!res)
         res.reset(createWithDecimalType<AggregateFunctionSumMap>(*keys_type, keys_type, values_types));
@@ -46,11 +56,30 @@ AggregateFunctionPtr createAggregateFunctionSumMap(const std::string & name, con
     return res;
 }
 
+AggregateFunctionPtr createAggregateFunctionSumMapFiltered(const std::string & name, const DataTypes & arguments, const Array & params)
+{
+    if (params.size() != 1)
+        throw Exception("Aggregate function " + name + "requires exactly one parameter of Array type.",
+            ErrorCodes::NUMBER_OF_ARGUMENTS_DOESNT_MATCH);
+
+    Array keys_to_keep = params.front().safeGet<Array>();
+
+    auto [keys_type, values_types] = parseArguments(name, arguments);
+
+    AggregateFunctionPtr res(createWithNumericBasedType<AggregateFunctionSumMapFiltered>(*keys_type, keys_type, values_types, keys_to_keep));
+    if (!res)
+        res.reset(createWithDecimalType<AggregateFunctionSumMapFiltered>(*keys_type, keys_type, values_types, keys_to_keep));
+    if (!res)
+        throw Exception("Illegal type of argument for aggregate function " + name, ErrorCodes::ILLEGAL_TYPE_OF_ARGUMENT);
+
+    return res;
+}
 }
 
 void registerAggregateFunctionSumMap(AggregateFunctionFactory & factory)
 {
     factory.registerFunction("sumMap", createAggregateFunctionSumMap);
+    factory.registerFunction("sumMapFiltered", createAggregateFunctionSumMapFiltered);
 }
 
 }
