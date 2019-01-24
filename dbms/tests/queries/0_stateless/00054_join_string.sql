8a11f753bef9841ff1418e759c96100def3c727b
chertus <chertus@gmail.com>
2019-01-24 21:34:31 +0300

    fix test results

--- dbms/tests/queries/0_stateless/00054_join_string.sql
+++ dbms/tests/queries/0_stateless/00054_join_string.sql
@@ -1,9 +1,9 @@
-SELECT * FROM
+SELECT a.*, b.* FROM
 (
     SELECT reinterpretAsString(number + reinterpretAsUInt8('A')) AS k FROM system.numbers LIMIT 10
-)
+) AS a
 ALL LEFT JOIN
 (
     SELECT reinterpretAsString(intDiv(number, 2) + reinterpretAsUInt8('A')) AS k, number AS joined FROM system.numbers LIMIT 10
-)
+) AS b
 USING k;
