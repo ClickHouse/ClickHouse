02f236e45709fca2e8d82e5caf644b1c27487ff0
chertus <chertus@gmail.com>
2019-01-25 16:15:29 +0300

    one more test

--- dbms/tests/queries/0_stateless/00819_ast_refactoring_bugs.sql
+++ dbms/tests/queries/0_stateless/00819_ast_refactoring_bugs.sql
@@ -20,3 +20,11 @@ ARRAY JOIN
 WHERE CounterID = 100500;
 
 DROP TABLE test.visits;
+
+select u, cumSum from (
+   select u, min(d) mn, max(d) mx, groupArray(d) dg, groupArray(v) vg,
+       arrayMap(x -> x + mn, range(toUInt32(mx - mn + 1))) days,
+       toString(arrayCumSum(arrayMap( x -> vg[indexOf(dg, x)] , days))) cumSum
+   from (select 1 u, today()-1 d, 1 v)
+   group by u
+);
