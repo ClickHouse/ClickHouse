e9721c9548d01f016a53a625f3836af4fe79f930
Vitaly Baranov <vitbar@yandex-team.ru>
2019-01-27 12:15:32 +0300

    Add support for absolute format schema paths.

--- dbms/src/Formats/FormatSchemaInfo.cpp
+++ dbms/src/Formats/FormatSchemaInfo.cpp
@@ -27,7 +27,9 @@ FormatSchemaInfo::FormatSchemaInfo(const Context & context, const String & schem
     }
 
     size_t colon_pos = format_schema.find(':');
-    if ((colon_pos == String::npos) || (colon_pos == 0) || (colon_pos == format_schema.length() - 1))
+    Poco::Path path;
+    if ((colon_pos == String::npos) || (colon_pos == 0) || (colon_pos == format_schema.length() - 1)
+        || path.assign(format_schema.substr(0, colon_pos)).getFileName().empty())
     {
         throw Exception(
             "Format schema requires the 'format_schema' setting to have the 'schema_file:message_name' format"
@@ -36,25 +38,44 @@ FormatSchemaInfo::FormatSchemaInfo(const Context & context, const String & schem
             ErrorCodes::BAD_ARGUMENTS);
     }
 
-    Poco::Path path(format_schema.substr(0, colon_pos));
-    if (context.hasGlobalContext() && (context.getGlobalContext().getApplicationType() == Context::ApplicationType::SERVER))
-    {
-        if (path.isAbsolute())
-            throw Exception("Absolute path in the 'format_schema' setting is prohibited: " + path.toString(), ErrorCodes::BAD_ARGUMENTS);
+    is_null = false;
+    message_name = format_schema.substr(colon_pos + 1);
 
-        if (path.depth() >= 1 && path.directory(0) == "..")
-            throw Exception(
-                "Path in the 'format_schema' setting shouldn't go outside the 'format_schema_path' directory: " + path.toString(),
-                ErrorCodes::BAD_ARGUMENTS);
-    }
+    auto default_schema_directory = [&context]()
+    {
+        static const String str = Poco::Path(context.getFormatSchemaPath()).makeAbsolute().makeDirectory().toString();
+        return str;
+    };
+    auto is_server = [&context]()
+    {
+        return context.hasGlobalContext() && (context.getGlobalContext().getApplicationType() == Context::ApplicationType::SERVER);
+    };
 
     if (path.getExtension().empty() && !schema_file_extension.empty())
         path.setExtension(schema_file_extension);
 
-    schema_path = path.toString();
-    schema_directory = context.getFormatSchemaPath();
-    message_name = format_schema.substr(colon_pos + 1);
-    is_null = false;
+    if (path.isAbsolute())
+    {
+        if (is_server())
+            throw Exception("Absolute path in the 'format_schema' setting is prohibited: " + path.toString(), ErrorCodes::BAD_ARGUMENTS);
+        schema_path = path.getFileName();
+        schema_directory = path.makeParent().toString();
+    }
+    else if (path.depth() >= 1 && path.directory(0) == "..")
+    {
+        if (is_server())
+            throw Exception(
+                "Path in the 'format_schema' setting shouldn't go outside the 'format_schema_path' directory: " + path.toString(),
+                ErrorCodes::BAD_ARGUMENTS);
+        path = Poco::Path(default_schema_directory()).resolve(path).toString();
+        schema_path = path.getFileName();
+        schema_directory = path.makeParent().toString();
+    }
+    else
+    {
+        schema_path = path.toString();
+        schema_directory = default_schema_directory();
+    }
 }
 
 }
