8e12eedbb33750b47f5a7023c223854b5ab067f0
Oleg Favstov <favstovol1@gmail.com>
2019-01-27 19:51:05 +0300

    Initial commit

---
+++ cmake/find_cassandra.cmake
@@ -0,0 +1,12 @@
+if (NOT DEFINED ENABLE_CASSANDRA OR ENABLE_CASSANDRA)
+    if (NOT EXISTS "${ClickHouse_SOURCE_DIR}/contrib/cassandra")
+        message (WARNING "submodule contrib/cassandra is missing. to fix try run: \n git submodule update --init --recursive")
+    else()
+        set (CASSANDRA_INCLUDE_DIR
+                "${ClickHouse_SOURCE_DIR}/contrib/cassandra/include/")
+        set (CASSANDRA_LIBRARY cassandra)
+        set (USE_CASSANDRA 1)
+
+        message(STATUS "Using cassandra: ${CASSANDRA_LIBRARY}")
+    endif()
+endif()
