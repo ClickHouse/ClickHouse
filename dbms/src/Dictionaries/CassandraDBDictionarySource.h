36688baa6c2b78b8ba06cd1ea71f8dece2321edd
Oleg Favstov <favstovol1@gmail.com>
2019-01-27 19:51:05 +0300

    Initial commit

---
+++ dbms/src/Dictionaries/CassandraDBDictionarySource.h
@@ -0,0 +1,16 @@
+#pragma once
+
+#include <Common/config.h>
+
+#if USE_CASSANDRA
+
+#    include "DictionaryStructure.h"
+#    include "IDictionarySource.h"
+#    include <cassandra.h>
+
+namespace DB
+{
+
+}
+
+#endif
