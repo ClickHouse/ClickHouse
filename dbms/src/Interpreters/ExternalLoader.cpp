b4a3ee119261bd5d857def08567a1ac68f792211
proller <proller@github.com>
2019-01-30 17:06:42 +0300

    Fix rethrowing exception #4188

--- dbms/src/Interpreters/ExternalLoader.cpp
+++ dbms/src/Interpreters/ExternalLoader.cpp
@@ -223,6 +223,8 @@ void ExternalLoader::reloadAndUpdate(bool throw_on_error)
                 else
                 {
                     tryLogException(exception, log, "Cannot update " + object_name + " '" + name + "', leaving old version");
+                    if (throw_on_error)
+                        std::rethrow_exception(exception);
                 }
             }
         }
