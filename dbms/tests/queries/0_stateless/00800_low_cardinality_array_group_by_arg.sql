cfc9aa1c2a85802d405307b0ec09f819ee1d7a66
Nikolai Kochetov <nik-kochetov@yandex-team.ru>
2019-01-30 12:20:50 +0300

    Fix unstable test.

--- dbms/tests/queries/0_stateless/00800_low_cardinality_array_group_by_arg.sql
+++ dbms/tests/queries/0_stateless/00800_low_cardinality_array_group_by_arg.sql
@@ -22,7 +22,7 @@ ORDER BY (dt, id) SETTINGS index_granularity = 8192;
 insert into test.table1 (dt, id, arr) values ('2019-01-14', 1, ['aaa']);
 insert into test.table2 (dt, id, arr) values ('2019-01-14', 1, ['aaa','bbb','ccc']);
 
-select dt, id, groupArrayArray(arr)
+select dt, id, arraySort(groupArrayArray(arr))
 from (
 	select dt, id, arr from test.table1
 	where dt = '2019-01-14' and id = 1
