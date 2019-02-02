c8c2f64b2dd57a454c4a02f738b08c7e07873a9e
Alexey <aveselovskiy@oxirate.com>
2019-02-02 07:19:06 -0800

    tests

---
+++ dbms/tests/queries/0_stateless/00834_date_datetime_cmp.sql
@@ -0,0 +1,4 @@
+select toDateTime('2017-06-28 12:01:01') < toDate('2017-07-01')
+select toDateTime('2017-06-28 12:01:01') > toDate('2017-07-01')
+SELECT toDate('2017-06-28') < toDate('2017-07-01')
+SELECT toDate('2017-06-28') > toDate('2017-07-01')
