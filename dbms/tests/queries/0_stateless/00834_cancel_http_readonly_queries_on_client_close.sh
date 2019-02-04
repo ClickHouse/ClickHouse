80db617348f169d4ef09d50b7f7b965f08cf9a58
Alexey Milovidov <milovidov@yandex-team.ru>
2019-02-04 21:55:06 +0300

    Fixed test #4213

--- dbms/tests/queries/0_stateless/00834_cancel_http_readonly_queries_on_client_close.sh
+++ dbms/tests/queries/0_stateless/00834_cancel_http_readonly_queries_on_client_close.sh
@@ -3,17 +3,5 @@
 CURDIR=$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)
 . $CURDIR/../shell_config.sh
 
-
-${CLICKHOUSE_CURL} -sS "${CLICKHOUSE_URL}?query_id=cancel_http_readonly_queries_on_client_close&cancel_http_readonly_queries_on_client_close=1&query=SELECT+count()+FROM+system.numbers" &
-REQUEST_CURL_PID=$!
-sleep 0.1
-
-# Check query is registered
-$CLICKHOUSE_CLIENT -q "SELECT count() FROM system.processes where query_id='cancel_http_readonly_queries_on_client_close'"
-
-# Kill client (curl process)
-kill -SIGTERM $REQUEST_CURL_PID
-sleep 0.1
-
-# Check query is killed after client is gone
-$CLICKHOUSE_CLIENT -q "SELECT count() FROM system.processes where query_id='cancel_http_readonly_queries_on_client_close'"
+${CLICKHOUSE_CURL} --max-time 0.1 -sS "${CLICKHOUSE_URL}?query_id=cancel_http_readonly_queries_on_client_close&cancel_http_readonly_queries_on_client_close=1&query=SELECT+count()+FROM+system.numbers" 2>&1 | grep -cF 'curl: (28)'
+${CLICKHOUSE_CURL} -sS --data "SELECT count() FROM system.processes WHERE query_id = 'cancel_http_readonly_queries_on_client_close'" "${CLICKHOUSE_URL}"
