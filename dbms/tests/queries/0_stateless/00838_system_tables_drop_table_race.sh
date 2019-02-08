9750377787a30d3243267872eede5d4390e74cfe
Alexey Milovidov <milovidov@yandex-team.ru>
2019-02-08 17:30:53 +0300

    Lowered number of iterations in test

--- dbms/tests/queries/0_stateless/00838_system_tables_drop_table_race.sh
+++ dbms/tests/queries/0_stateless/00838_system_tables_drop_table_race.sh
@@ -8,6 +8,6 @@ CURDIR=$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)
 $CLICKHOUSE_CLIENT -q "DROP TABLE IF EXISTS test.table"
 
 seq 1 100 | sed -r -e "s/.+/CREATE TABLE test.table (x UInt8) ENGINE = MergeTree ORDER BY x; DROP TABLE test.table;/" | $CLICKHOUSE_CLIENT -n &
-seq 1 1000 | sed -r -e "s/.+/SELECT * FROM system.tables WHERE database = 'test' LIMIT 1000000, 1;/" | $CLICKHOUSE_CLIENT -n &
+seq 1 100 | sed -r -e "s/.+/SELECT * FROM system.tables WHERE database = 'test' LIMIT 1000000, 1;/" | $CLICKHOUSE_CLIENT -n &
 
 wait
