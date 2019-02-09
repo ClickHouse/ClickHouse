6ebbffc6856cae6ae041d1a790c584ce634b4409
Alexey Milovidov <milovidov@yandex-team.ru>
2019-02-10 02:42:22 +0300

    Removed "allow_experimental_low_cardinality_type" setting from tests and docs

--- dbms/tests/queries/0_stateless/00688_low_cardinality_prewhere.sql
+++ dbms/tests/queries/0_stateless/00688_low_cardinality_prewhere.sql
@@ -1,4 +1,3 @@
-set allow_experimental_low_cardinality_type = 1;
 drop table if exists test.lc_prewhere;
 create table test.lc_prewhere (key UInt64, val UInt64, str StringWithDictionary, s String) engine = MergeTree order by key settings index_granularity = 8192;
 insert into test.lc_prewhere select number, if(number < 10 or number > 8192 * 9, 1, 0), toString(number) as s, s from system.numbers limit 100000;
