afa734f0b3e544fd7c198394432a5c0a4502df6f
Alexey Milovidov <milovidov@yandex-team.ru>
2019-02-10 00:13:58 +0300

    Fixed implementation of "arrayWithConstant" #4309

--- dbms/src/Functions/arrayWithConstant.cpp
+++ dbms/src/Functions/arrayWithConstant.cpp
@@ -3,22 +3,22 @@
 #include <Functions/FunctionFactory.h>
 #include <DataTypes/DataTypeArray.h>
 #include <DataTypes/DataTypesNumber.h>
-#include <DataTypes/getLeastSupertype.h>
 #include <Columns/ColumnArray.h>
 
-#include <iostream>
 
-namespace DB {
-namespace ErrorCodes {
+namespace DB
+{
+
+namespace ErrorCodes
+{
     extern const int ILLEGAL_TYPE_OF_ARGUMENT;
 }
 
-/*
-* arrayWithConstant(num, const) - make array of constants with length num.
-* arrayWithConstant(3, 'hello') = ['hello', 'hello', 'hello']
-* arrayWithConstant(1, 'hello') = ['hello']
-* arrayWithConstant(0, 'hello') = []
-*/
+/* arrayWithConstant(num, const) - make array of constants with length num.
+ * arrayWithConstant(3, 'hello') = ['hello', 'hello', 'hello']
+ * arrayWithConstant(1, 'hello') = ['hello']
+ * arrayWithConstant(0, 'hello') = []
+ */
 
 class FunctionArrayWithConstant : public IFunction
 {
@@ -28,35 +28,37 @@ public:
     static FunctionPtr create(const Context &) { return std::make_shared<FunctionArrayWithConstant>(); }
 
     String getName() const override { return name; }
-
     size_t getNumberOfArguments() const override { return 2; }
 
     DataTypePtr getReturnTypeImpl(const DataTypes & arguments) const override
     {
-        if (!isUnsignedInteger(arguments[0])) {
+        if (!isNumber(arguments[0]))
             throw Exception("Illegal type " + arguments[0]->getName() +
-                            " of argument of function " + getName() +
-                            ", expected Integer", ErrorCodes::ILLEGAL_TYPE_OF_ARGUMENT);
-        }
+                " of argument of function " + getName() +
+                ", expected Integer", ErrorCodes::ILLEGAL_TYPE_OF_ARGUMENT);
         return std::make_shared<DataTypeArray>(arguments[1]);
     }
 
-
     bool useDefaultImplementationForConstants() const override { return true; }
-
     bool useDefaultImplementationForNulls() const override { return false; }
-    void executeImpl(Block & block, const ColumnNumbers & arguments, size_t result, size_t) override
+
+    void executeImpl(Block & block, const ColumnNumbers & arguments, size_t result, size_t num_rows) override
     {
-        const auto * num = block.getByPosition(arguments[0]).column.get();
-        const auto * constant = block.getByPosition(arguments[1]).column.get();
-        uint64_t a = num->getUInt(0);
+        const auto * col_num = block.getByPosition(arguments[0]).column.get();
+        const auto * col_value = block.getByPosition(arguments[1]).column.get();
 
         auto offsets_col = ColumnArray::ColumnOffsets::create();
         ColumnArray::Offsets & offsets = offsets_col->getData();
+        offsets.reserve(num_rows);
 
-        offsets.push_back(a);
-        auto data_col = constant->replicate(offsets);
-        block.getByPosition(result).column = ColumnArray::create(std::move(data_col), std::move(offsets_col));
+        ColumnArray::Offset offset = 0;
+        for (size_t i = 0; i < num_rows; ++i)
+        {
+            offset += col_num->getUInt(i);
+            offsets.push_back(offset);
+        }
+
+        block.getByPosition(result).column = ColumnArray::create(col_value->replicate(offsets), std::move(offsets_col));
     }
 };
 
@@ -65,8 +67,4 @@ void registerFunctionArrayWithConstant(FunctionFactory & factory)
     factory.registerFunction<FunctionArrayWithConstant>();
 }
 
-} // namespace DB
-
-
-
-
+}
