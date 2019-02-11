670a1a4f045443c10ad8357e5611d043f0a3dfc3
chertus <chertus@gmail.com>
2019-02-11 22:33:04 +0300

    test for issue-3998

--- dbms/tests/queries/0_stateless/00818_alias_bug_4110.sql
+++ dbms/tests/queries/0_stateless/00818_alias_bug_4110.sql
@@ -3,3 +3,6 @@ select s.a + 1 as a, s.a as b from (select 10 as a) s;
 select s.a + 1 as a, s.a + 1 as b from (select 10 as a) s;
 select s.a + 1 as b, s.a + 2 as a from (select 10 as a) s;
 select s.a + 2 as b, s.a + 1 as a from (select 10 as a) s;
+
+SELECT 0 as t FROM (SELECT 1 as t) as inn WHERE inn.t = 1;
+SELECT sum(value) as value FROM (SELECT 1 as value) as data WHERE data.value > 0;
