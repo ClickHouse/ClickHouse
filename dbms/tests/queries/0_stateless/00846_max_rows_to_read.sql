bc8460a1f98f7268634ee07832c8a88ea2204d08
Alexey Milovidov <milovidov@yandex-team.ru>
2019-02-13 22:06:30 +0300

    Improved test

--- dbms/tests/queries/0_stateless/00846_max_rows_to_read.sql
+++ dbms/tests/queries/0_stateless/00846_max_rows_to_read.sql
@@ -9,7 +9,9 @@ SELECT count() FROM numbers(21); -- { serverError 158 }
 
 SET read_overflow_mode = 'break';
 
-SELECT count() FROM numbers(30);
 SELECT count() FROM numbers(19);
 SELECT count() FROM numbers(20);
 SELECT count() FROM numbers(21);
+SELECT count() FROM numbers(29); -- one extra block is read and it is Ok.
+SELECT count() FROM numbers(30);
+SELECT count() FROM numbers(31);
