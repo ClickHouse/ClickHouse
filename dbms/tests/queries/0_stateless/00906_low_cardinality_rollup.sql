117d0e456ccc265e5d4f7de76c95689d7dd4c6c7
Nikolai Kochetov <nik-kochetov@yandex-team.ru>
2019-02-13 19:56:06 +0300

    Added test for LowCardinality with rollup and cube.

---
+++ dbms/tests/queries/0_stateless/00906_low_cardinality_rollup.sql
@@ -0,0 +1,14 @@
+SET allow_experimental_low_cardinality_type = 1;
+
+DROP TABLE if exists test.lc;
+CREATE TABLE test.lc (a LowCardinality(Nullable(String)), b LowCardinality(Nullable(String))) ENGINE = MergeTree order by tuple();
+INSERT INTO test.lc VALUES ('a', 'b');
+INSERT INTO test.lc VALUES ('c', 'd');
+
+SELECT a, b, count(a) FROM test.lc GROUP BY a, b WITH ROLLUP;
+SELECT a, count(a) FROM test.lc GROUP BY a WITH ROLLUP;
+
+SELECT a, b, count(a) FROM test.lc GROUP BY a, b WITH CUBE;
+SELECT a, count(a) FROM test.lc GROUP BY a WITH CUBE;
+
+DROP TABLE if exists test.lc;
