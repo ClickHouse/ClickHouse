72ee24e5a3d42968801907d48ae22e0f47b0922e
proller <proller@github.com>
2019-02-13 19:09:34 +0300

    clickhouse-odbc #157 : Remove LowCardinality from type

--- dbms/src/Formats/ODBCDriver2BlockOutputStream.cpp
+++ dbms/src/Formats/ODBCDriver2BlockOutputStream.cpp
@@ -6,6 +6,7 @@
 
 
 #include <Core/iostream_debug_helpers.h>
+#include <DataTypes/DataTypeLowCardinality.h>
 
 
 namespace DB
@@ -86,8 +87,10 @@ void ODBCDriver2BlockOutputStream::writePrefix()
     writeODBCString(out, "type");
     for (size_t i = 0; i < columns; ++i)
     {
-        const ColumnWithTypeAndName & col = header.getByPosition(i);
-        writeODBCString(out, col.type->getName());
+        auto type = header.getByPosition(i).type;
+        if (type->lowCardinality())
+            type = removeLowCardinality(type);
+        writeODBCString(out, type->getName());
     }
 }
 
