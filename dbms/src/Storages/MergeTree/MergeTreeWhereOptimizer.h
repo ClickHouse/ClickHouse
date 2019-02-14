75b67c475b07b63ba144dac4132594cecd6599be
Alexey Milovidov <milovidov@yandex-team.ru>
2019-02-15 02:51:17 +0300

    Better "optimize_move_to_prewhere" heuristic; simpler code

--- dbms/src/Storages/MergeTree/MergeTreeWhereOptimizer.h
+++ dbms/src/Storages/MergeTree/MergeTreeWhereOptimizer.h
@@ -17,9 +17,6 @@ class ASTSelectQuery;
 class ASTFunction;
 class MergeTreeData;
 
-using IdentifierNameSet = std::set<std::string>;
-
-
 /** Identifies WHERE expressions that can be placed in PREWHERE by calculating respective
  *  sizes of columns used in particular expression and identifying "good" conditions of
  *  form "column_name = constant", where "constant" is outside some `threshold` specified in advance.
@@ -40,25 +37,53 @@ public:
 private:
     void optimize(ASTSelectQuery & select) const;
 
+    struct Condition
+    {
+        ASTPtr node;
+        size_t columns_size = 0;
+        NameSet identifiers;
+        bool viable = false;
+        bool good = false;
+
+        auto tuple() const
+        {
+            return std::make_tuple(!viable, !good, columns_size);
+        }
+
+        /// Is condition a better candidate for moving to PREWHERE?
+        bool operator< (const Condition & rhs) const
+        {
+            return tuple() < rhs.tuple();
+        }
+    };
+
+    using Conditions = std::list<Condition>;
+
+    void analyzeImpl(Conditions & res, const ASTPtr & node) const;
+
+    /// Transform conjunctions chain in WHERE expression to Conditions list.
+    Conditions analyze(const ASTPtr & expression) const;
+
+    /// Transform Conditions list to WHERE or PREWHERE expression.
+    ASTPtr reconstruct(const Conditions & conditions) const;
+
     void calculateColumnSizes(const MergeTreeData & data, const Names & column_names);
 
     void optimizeConjunction(ASTSelectQuery & select, ASTFunction * const fun) const;
 
     void optimizeArbitrary(ASTSelectQuery & select) const;
 
-    size_t getIdentifiersColumnSize(const IdentifierNameSet & identifiers) const;
+    size_t getIdentifiersColumnSize(const NameSet & identifiers) const;
 
-    bool isConditionGood(const IAST * condition) const;
+    bool isConditionGood(const ASTPtr & condition) const;
 
-    static void collectIdentifiersNoSubqueries(const IAST * const ast, IdentifierNameSet & set);
+    bool hasPrimaryKeyAtoms(const ASTPtr & ast) const;
 
-    bool hasPrimaryKeyAtoms(const IAST * ast) const;
-
-    bool isPrimaryKeyAtom(const IAST * const ast) const;
+    bool isPrimaryKeyAtom(const ASTPtr & ast) const;
 
     bool isConstant(const ASTPtr & expr) const;
 
-    bool isSubsetOfTableColumns(const IdentifierNameSet & identifiers) const;
+    bool isSubsetOfTableColumns(const NameSet & identifiers) const;
 
     /** ARRAY JOIN'ed columns as well as arrayJoin() result cannot be used in PREWHERE, therefore expressions
       *    containing said columns should not be moved to PREWHERE at all.
