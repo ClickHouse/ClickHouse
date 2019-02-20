cdbbb5068ba3a0924689b2fdcdc8522ebd385c1e
Mikhail f. Shiryaev <mr.felixoid@gmail.com>
2019-02-20 17:45:51 +0100

    Apply pep8 to test_graphite_merge_tree

--- dbms/tests/integration/test_graphite_merge_tree/test.py
+++ dbms/tests/integration/test_graphite_merge_tree/test.py
@@ -8,31 +8,38 @@ from helpers.test_tools import TSV
 
 
 cluster = ClickHouseCluster(__file__)
-instance = cluster.add_instance('instance', main_configs=['configs/graphite_rollup.xml'])
+instance = cluster.add_instance('instance',
+                                main_configs=['configs/graphite_rollup.xml'])
+q = instance.query
+
 
 @pytest.fixture(scope="module")
 def started_cluster():
     try:
         cluster.start()
-        instance.query('CREATE DATABASE test')
+        q('CREATE DATABASE test')
 
         yield cluster
 
     finally:
         cluster.shutdown()
 
+
 @pytest.fixture
 def graphite_table(started_cluster):
-    instance.query('''
+    q('''
 DROP TABLE IF EXISTS test.graphite;
 CREATE TABLE test.graphite
     (metric String, value Float64, timestamp UInt32, date Date, updated UInt32)
-    ENGINE = GraphiteMergeTree(date, (metric, timestamp), 8192, 'graphite_rollup');
+    ENGINE = GraphiteMergeTree('graphite_rollup')
+    PARTITION BY toYYYYMM(date)
+    ORDER BY (metric, timestamp)
+    SETTINGS index_granularity=8192;
 ''')
 
     yield
 
-    instance.query('DROP TABLE test.graphite')
+    q('DROP TABLE test.graphite')
 
 
 def test_rollup_versions(graphite_table):
@@ -40,13 +47,14 @@ def test_rollup_versions(graphite_table):
     rounded_timestamp = timestamp - timestamp % 60
     date = datetime.date.today().isoformat()
 
-    q = instance.query
-
-    # Insert rows with timestamps relative to the current time so that the first retention clause is active.
+    # Insert rows with timestamps relative to the current time so that the
+    # first retention clause is active.
     # Two parts are created.
     q('''
-INSERT INTO test.graphite (metric, value, timestamp, date, updated) VALUES ('one_min.x1', 100, {timestamp}, '{date}', 1);
-INSERT INTO test.graphite (metric, value, timestamp, date, updated) VALUES ('one_min.x1', 200, {timestamp}, '{date}', 2);
+INSERT INTO test.graphite (metric, value, timestamp, date, updated)
+      VALUES ('one_min.x1', 100, {timestamp}, '{date}', 1);
+INSERT INTO test.graphite (metric, value, timestamp, date, updated)
+      VALUES ('one_min.x1', 200, {timestamp}, '{date}', 2);
 '''.format(timestamp=timestamp, date=date))
 
     expected1 = '''\
@@ -54,7 +62,9 @@ one_min.x1	100	{timestamp}	{date}	1
 one_min.x1	200	{timestamp}	{date}	2
 '''.format(timestamp=timestamp, date=date)
 
-    assert TSV(q('SELECT * FROM test.graphite ORDER BY updated')) == TSV(expected1)
+    assert TSV(
+        q('SELECT * FROM test.graphite ORDER BY updated')
+    ) == TSV(expected1)
 
     q('OPTIMIZE TABLE test.graphite')
 
@@ -67,8 +77,6 @@ one_min.x1	200	{timestamp}	{date}	2
 
 
 def test_rollup_aggregation(graphite_table):
-    q = instance.query
-
     # This query essentially emulates what rollup does.
     result1 = q('''
 SELECT avg(v), max(upd)
@@ -91,7 +99,8 @@ FROM (SELECT timestamp,
 '''
     assert TSV(result1) == TSV(expected1)
 
-    # Timestamp 1111111111 is in sufficiently distant past so that the last retention clause is active.
+    # Timestamp 1111111111 is in sufficiently distant past
+    # so that the last retention clause is active.
     result2 = q('''
 INSERT INTO test.graphite
     SELECT 'one_min.x' AS metric,
@@ -114,7 +123,7 @@ one_min.x	999634.9918367347	1111444200	2017-02-02	499999
 
 
 def test_rollup_aggregation_2(graphite_table):
-    result = instance.query('''
+    result = q('''
 INSERT INTO test.graphite
     SELECT 'one_min.x' AS metric,
            toFloat64(number) AS value,
@@ -136,7 +145,7 @@ one_min.x	24	1111110600	2017-02-02	100
 
 
 def test_multiple_paths_and_versions(graphite_table):
-    result = instance.query('''
+    result = q('''
 INSERT INTO test.graphite
     SELECT 'one_min.x' AS metric,
            toFloat64(number) AS value,
@@ -163,7 +172,9 @@ OPTIMIZE TABLE test.graphite PARTITION 201702 FINAL;
 SELECT * FROM test.graphite;
 ''')
 
-    with open(p.join(p.dirname(__file__), 'test_multiple_paths_and_versions.reference')) as reference:
+    with open(p.join(p.dirname(__file__),
+                     'test_multiple_paths_and_versions.reference')
+              ) as reference:
         assert TSV(result) == TSV(reference)
 
 
@@ -177,14 +188,18 @@ def test_multiple_output_blocks(graphite_table):
 
         for j in range(3):
             cur_time = rolled_up_time + 100 * j
-            to_insert += 'one_min.x1	{}	{}	2001-09-09	1\n'.format(10 * j, cur_time)
-            to_insert += 'one_min.x1	{}	{}	2001-09-09	2\n'.format(10 * (j + 1), cur_time)
+            to_insert += 'one_min.x1	{}	{}	2001-09-09	1\n'.format(
+                10 * j, cur_time
+            )
+            to_insert += 'one_min.x1	{}	{}	2001-09-09	2\n'.format(
+                10 * (j + 1), cur_time
+            )
 
         expected += 'one_min.x1	20	{}	2001-09-09	2\n'.format(rolled_up_time)
 
-    instance.query('INSERT INTO test.graphite FORMAT TSV', to_insert)
+    q('INSERT INTO test.graphite FORMAT TSV', to_insert)
 
-    result = instance.query('''
+    result = q('''
 OPTIMIZE TABLE test.graphite PARTITION 200109 FINAL;
 
 SELECT * FROM test.graphite;
@@ -200,14 +215,14 @@ zzzzzzzz	100	1000000001	2001-09-09	1
 zzzzzzzz	200	1000000001	2001-09-09	2
 '''
 
-    instance.query('INSERT INTO test.graphite FORMAT TSV', to_insert)
+    q('INSERT INTO test.graphite FORMAT TSV', to_insert)
 
     expected = '''\
 one_min.x1	100	999999600	2001-09-09	1
 zzzzzzzz	200	1000000001	2001-09-09	2
 '''
 
-    result = instance.query('''
+    result = q('''
 OPTIMIZE TABLE test.graphite PARTITION 200109 FINAL;
 
 SELECT * FROM test.graphite;
@@ -215,27 +230,38 @@ SELECT * FROM test.graphite;
 
     assert TSV(result) == TSV(expected)
 
+
 def test_path_dangling_pointer(graphite_table):
-    instance.query('''
+    q('''
 DROP TABLE IF EXISTS test.graphite2;
 CREATE TABLE test.graphite2
-  (metric String, value Float64, timestamp UInt32, date Date, updated UInt32)
-  ENGINE = GraphiteMergeTree(date, (metric, timestamp), 1, 'graphite_rollup');
-  ''')
+    (metric String, value Float64, timestamp UInt32, date Date, updated UInt32)
+    ENGINE = GraphiteMergeTree('graphite_rollup')
+    PARTITION BY toYYYYMM(date)
+    ORDER BY (metric, timestamp)
+    SETTINGS index_granularity=1;
+    ''')
 
-    path = 'abcd' * 4000000 # 16MB
-    instance.query('INSERT INTO test.graphite2 FORMAT TSV', "{}\t0.0\t0\t2018-01-01\t100\n".format(path))
-    instance.query('INSERT INTO test.graphite2 FORMAT TSV', "{}\t0.0\t0\t2018-01-01\t101\n".format(path))
+    path = 'abcd' * 4000000  # 16MB
+    q('INSERT INTO test.graphite2 FORMAT TSV',
+      "{}\t0.0\t0\t2018-01-01\t100\n".format(path))
+    q('INSERT INTO test.graphite2 FORMAT TSV',
+      "{}\t0.0\t0\t2018-01-01\t101\n".format(path))
     for version in range(10):
-        instance.query('INSERT INTO test.graphite2 FORMAT TSV', "{}\t0.0\t0\t2018-01-01\t{}\n".format(path, version))
+        q('INSERT INTO test.graphite2 FORMAT TSV',
+          "{}\t0.0\t0\t2018-01-01\t{}\n".format(path, version))
 
     while True:
-      instance.query('OPTIMIZE TABLE test.graphite2 PARTITION 201801 FINAL')
-      parts = int(instance.query("SELECT count() FROM system.parts WHERE active AND database='test' AND table='graphite2'"))
-      if parts == 1:
-        break
-      print "Parts", parts
+        q('OPTIMIZE TABLE test.graphite2 PARTITION 201801 FINAL')
+        parts = int(q("SELECT count() FROM system.parts "
+                      "WHERE active AND database='test' "
+                      "AND table='graphite2'"))
+        if parts == 1:
+            break
+        print('Parts', parts)
 
-    assert TSV(instance.query("SELECT value, timestamp, date, updated FROM test.graphite2")) == TSV("0\t0\t2018-01-01\t101\n")
+    assert TSV(
+        q("SELECT value, timestamp, date, updated FROM test.graphite2")
+    ) == TSV("0\t0\t2018-01-01\t101\n")
 
-    instance.query('DROP TABLE test.graphite2')
+    q('DROP TABLE test.graphite2')
