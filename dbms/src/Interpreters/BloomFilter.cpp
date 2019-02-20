10bc08b8307422922fe04a176949f08029a9f54c
Nikita Vasilev <nekitvas@gmail.com>
2019-02-20 12:02:19 +0300

    added StringBloomFilter

---
+++ dbms/src/Interpreters/BloomFilter.cpp
@@ -0,0 +1,53 @@
+#include "BloomFilter.h"
+
+#include <city.h>
+
+
+namespace DB
+{
+
+LinearCongruentialGenerator::LinearCongruentialGenerator(size_t seed, UInt64 a_, UInt64 c_, UInt64 m_)
+    : current(seed), a(a_), c(c_), m(m_) {}
+
+UInt64 LinearCongruentialGenerator::next()
+{
+    return current = (current * a + c) % m;
+}
+
+
+StringBloomFilter::StringBloomFilter(size_t size_, size_t hashes_, size_t seed_)
+    : size(size_), hashes(hashes_), seed(seed_), filter(size, 0) {}
+
+bool StringBloomFilter::find(const char * data, size_t len)
+{
+    LinearCongruentialGenerator lcg(seed);
+    for (size_t i = 0; i < hashes; ++i)
+    {
+        size_t pos = CityHash_v1_0_2::CityHash64WithSeed(data, len, lcg.next()) % (8 * size);
+        if (!(filter[pos / 8] & (1 << (pos % 8))))
+            return false;
+    }
+    return true;
+}
+
+void StringBloomFilter::add(const char * data, size_t len)
+{
+    LinearCongruentialGenerator lcg(seed);
+    for (size_t i = 0; i < hashes; ++i)
+    {
+        size_t pos = CityHash_v1_0_2::CityHash64WithSeed(data, len, lcg.next()) % (8 * size);
+        filter[pos / 8] |= (1 << (pos % 8));
+    }
+}
+
+bool StringBloomFilter::contains(const StringBloomFilter & bf)
+{
+    for (size_t i = 0; i < size; ++i)
+    {
+        if ((filter[i] & bf.filter[i]) != bf.filter[i])
+            return false;
+    }
+    return true;
+}
+
+}
---
+++ dbms/src/Interpreters/BloomFilter.h
@@ -0,0 +1,56 @@
+#pragma once
+
+#include <Core/Types.h>
+#include <vector>
+
+namespace DB
+{
+
+/// Good constants for LCG from wikipedia.
+constexpr UInt64 LCG_A = 84589;
+constexpr UInt64 LCG_C = 45989;
+constexpr UInt64 LCG_M = 217728;
+
+/// LinearCongruentialGenerator for generating random seeds for hash functions.
+/// It is used there because it is very fast and lightweight.
+/// https://en.wikipedia.org/wiki/Linear_congruential_generator
+class LinearCongruentialGenerator
+{
+public:
+    LinearCongruentialGenerator(
+            size_t seed, UInt64 a_ = LCG_A, UInt64 c_ = LCG_C, UInt64 m_ = LCG_M);
+
+    UInt64 next();
+
+private:
+    UInt64 current;
+    UInt64 a;
+    UInt64 c;
+    UInt64 m;
+};
+
+
+/// Bloom filter for strings.
+class StringBloomFilter
+{
+public:
+    /// size -- size of filter in bytes.
+    /// hashes -- number of used hash functions.
+    /// seed -- random seed for hash functions generation.
+    StringBloomFilter(size_t size_, size_t hashes_, size_t seed_);
+
+    bool find(const char * data, size_t len);
+    void add(const char * data, size_t len);
+
+    /// Checks if this contains everything from another bloom filter.
+    /// Bloom filters must have equal size and seed.
+    bool contains(const StringBloomFilter & bf);
+
+private:
+    size_t size;
+    size_t hashes;
+    size_t seed;
+    std::vector<UInt8> filter;
+};
+
+}
