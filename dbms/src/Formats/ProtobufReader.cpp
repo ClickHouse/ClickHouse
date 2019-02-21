4793ccb3fcca6e9fc7d64c6abdbba1e0a05e5256
Vitaly Baranov <vitbar@yandex-team.ru>
2019-02-21 18:49:42 +0300

    Improve performance of ProtobufReader.

--- dbms/src/Formats/ProtobufReader.cpp
+++ dbms/src/Formats/ProtobufReader.cpp
@@ -5,7 +5,6 @@
 #include <boost/numeric/conversion/cast.hpp>
 #include <DataTypes/DataTypesDecimal.h>
 #include <Formats/ProtobufReader.h>
-#include <IO/ReadBuffer.h>
 #include <IO/ReadBufferFromString.h>
 #include <IO/ReadHelpers.h>
 #include <IO/WriteBufferFromVector.h>
@@ -34,8 +33,9 @@ namespace
         BITS32 = 5,
     };
 
-    // The following inequation should be always true to simplify conditions:
+    // The following should be always true:
     // REACHED_END < any cursor position < min(END_OF_VARINT, END_OF_GROUP)
+    // This inequation helps to check conditions in SimpleReader.
     constexpr UInt64 END_OF_VARINT = static_cast<UInt64>(-1);
     constexpr UInt64 END_OF_GROUP = static_cast<UInt64>(-2);
 
@@ -51,7 +51,7 @@ namespace
 
 ProtobufReader::SimpleReader::SimpleReader(ReadBuffer & in_)
     : in(in_)
-    , cursor(1 /* Should be greater than REACHED_END to simplify conditions */)
+    , cursor(1 /* We starts at cursor == 1 to keep any cursor value > REACHED_END, this allows to simplify conditions */)
     , current_message_end(REACHED_END)
     , field_end(REACHED_END)
 {
@@ -62,7 +62,7 @@ bool ProtobufReader::SimpleReader::startMessage()
     if ((current_message_end == REACHED_END) && parent_message_ends.empty())
     {
         // Start reading a root message.
-        if (in.eof())
+        if (unlikely(in.eof()))
             return false;
         size_t size_of_message = readVarint();
         current_message_end = cursor + size_of_message;
@@ -86,7 +86,7 @@ void ProtobufReader::SimpleReader::endMessage()
             ignoreGroup();
         else if (cursor < current_message_end)
             ignore(current_message_end - cursor);
-        else if (cursor > current_message_end)
+        else if (unlikely(cursor > current_message_end))
         {
             if (!parent_message_ends.empty())
                 unknownFormat();
@@ -110,7 +110,7 @@ void ProtobufReader::SimpleReader::endRootMessage()
     {
         if (cursor < message_end)
             ignore(message_end - cursor);
-        else if (cursor > message_end)
+        else if (unlikely(cursor > message_end))
             moveCursorBackward(cursor - message_end);
     }
     parent_message_ends.clear();
@@ -138,7 +138,7 @@ bool ProtobufReader::SimpleReader::readFieldNumber(UInt32 & field_number)
     }
 
     UInt64 varint = readVarint();
-    if (varint & (static_cast<UInt64>(0xFFFFFFFF) << 32))
+    if (unlikely(varint & (static_cast<UInt64>(0xFFFFFFFF) << 32)))
         unknownFormat();
     UInt32 key = static_cast<UInt32>(varint);
     field_number = (key >> 3);
@@ -185,7 +185,7 @@ bool ProtobufReader::SimpleReader::readFieldNumber(UInt32 & field_number)
 
 bool ProtobufReader::SimpleReader::readUInt(UInt64 & value)
 {
-    if (cursor >= field_end)
+    if (unlikely(cursor >= field_end))
     {
         field_end = REACHED_END;
         return false;
@@ -217,7 +217,7 @@ bool ProtobufReader::SimpleReader::readSInt(Int64 & value)
 template<typename T>
 bool ProtobufReader::SimpleReader::readFixed(T & value)
 {
-    if (cursor >= field_end)
+    if (unlikely(cursor >= field_end))
     {
         field_end = REACHED_END;
         return false;
@@ -230,7 +230,7 @@ bool ProtobufReader::SimpleReader::readFixed(T & value)
 
 bool ProtobufReader::SimpleReader::readStringInto(PaddedPODArray<UInt8> & str)
 {
-    if (cursor > field_end)
+    if (unlikely(cursor > field_end))
         return false;
     size_t length = field_end - cursor;
     size_t old_size = str.size();
@@ -260,81 +260,70 @@ void ProtobufReader::SimpleReader::moveCursorBackward(UInt64 num_bytes)
     cursor -= num_bytes;
 }
 
-UInt64 ProtobufReader::SimpleReader::readVarint()
+UInt64 ProtobufReader::SimpleReader::continueReadingVarint(UInt64 first_byte)
 {
+    UInt64 result = (first_byte & ~static_cast<UInt64>(0x80));
     char c;
-    UInt64 result = 0;
 
-#define PROTOBUF_READER_VARINT_READ_HELPER(i) \
-    in.readStrict(&c, 1); \
-    result |= static_cast<UInt64>(c) << (7 * i); \
-    if constexpr (i < 9) \
+#define PROTOBUF_READER_READ_VARINT_BYTE(byteNo) \
+    in.readStrict(c); \
+    ++cursor; \
+    if constexpr (byteNo < 10) \
     { \
-        if (!(c & 0x80)) \
-        { \
-            cursor += i + 1; \
+        result |= static_cast<UInt64>(static_cast<UInt8>(c)) << (7 * (byteNo - 1)); \
+        if (likely(!(c & 0x80))) \
             return result; \
-        } \
-        if constexpr (i < 8) \
-            result &= ((static_cast<UInt64>(0x80) << (7 * i)) - 1); \
     } \
     else \
     { \
-        if (c == 1) \
-        { \
-            cursor += i + 1; \
+        if (likely(c == 1)) \
             return result; \
-        } \
-    }
-    PROTOBUF_READER_VARINT_READ_HELPER(0);
-    PROTOBUF_READER_VARINT_READ_HELPER(1);
-    PROTOBUF_READER_VARINT_READ_HELPER(2);
-    PROTOBUF_READER_VARINT_READ_HELPER(3);
-    PROTOBUF_READER_VARINT_READ_HELPER(4);
-    PROTOBUF_READER_VARINT_READ_HELPER(5);
-    PROTOBUF_READER_VARINT_READ_HELPER(6);
-    PROTOBUF_READER_VARINT_READ_HELPER(7);
-    PROTOBUF_READER_VARINT_READ_HELPER(8);
-    PROTOBUF_READER_VARINT_READ_HELPER(9);
-#undef PROTOBUF_READER_VARINT_READ_HELPER
+    } \
+    if constexpr (byteNo < 9) \
+        result &= ~(static_cast<UInt64>(0x80) << (7 * (byteNo - 1)));
+    PROTOBUF_READER_READ_VARINT_BYTE(2);
+    PROTOBUF_READER_READ_VARINT_BYTE(3);
+    PROTOBUF_READER_READ_VARINT_BYTE(4);
+    PROTOBUF_READER_READ_VARINT_BYTE(5);
+    PROTOBUF_READER_READ_VARINT_BYTE(6);
+    PROTOBUF_READER_READ_VARINT_BYTE(7);
+    PROTOBUF_READER_READ_VARINT_BYTE(8);
+    PROTOBUF_READER_READ_VARINT_BYTE(9);
+    PROTOBUF_READER_READ_VARINT_BYTE(10);
+#undef PROTOBUF_READER_READ_VARINT_BYTE
 
     unknownFormat();
-    return 0;
+    __builtin_unreachable();
 }
 
 void ProtobufReader::SimpleReader::ignoreVarint()
 {
     char c;
 
-#define PROTOBUF_READER_VARINT_IGNORE_HELPER(i) \
-    in.readStrict(&c, 1); \
-    if constexpr (i < 9) \
+#define PROTOBUF_READER_IGNORE_VARINT_BYTE(byteNo) \
+    in.readStrict(c); \
+    ++cursor; \
+    if constexpr (byteNo < 10) \
     { \
-        if (!(c & 0x80)) \
-        { \
-            cursor += i + 1; \
+        if (likely(!(c & 0x80))) \
             return; \
-        } \
     } \
     else \
     { \
-        if (c == 1) \
-        { \
-            cursor += i + 1; \
+        if (likely(c == 1)) \
             return; \
-        } \
     }
-    PROTOBUF_READER_VARINT_IGNORE_HELPER(0);
-    PROTOBUF_READER_VARINT_IGNORE_HELPER(1);
-    PROTOBUF_READER_VARINT_IGNORE_HELPER(2);
-    PROTOBUF_READER_VARINT_IGNORE_HELPER(3);
-    PROTOBUF_READER_VARINT_IGNORE_HELPER(4);
-    PROTOBUF_READER_VARINT_IGNORE_HELPER(5);
-    PROTOBUF_READER_VARINT_IGNORE_HELPER(6);
-    PROTOBUF_READER_VARINT_IGNORE_HELPER(7);
-    PROTOBUF_READER_VARINT_IGNORE_HELPER(8);
-    PROTOBUF_READER_VARINT_IGNORE_HELPER(9);
-#undef PROTOBUF_READER_VARINT_IGNORE_HELPER
+    PROTOBUF_READER_IGNORE_VARINT_BYTE(1);
+    PROTOBUF_READER_IGNORE_VARINT_BYTE(2);
+    PROTOBUF_READER_IGNORE_VARINT_BYTE(3);
+    PROTOBUF_READER_IGNORE_VARINT_BYTE(4);
+    PROTOBUF_READER_IGNORE_VARINT_BYTE(5);
+    PROTOBUF_READER_IGNORE_VARINT_BYTE(6);
+    PROTOBUF_READER_IGNORE_VARINT_BYTE(7);
+    PROTOBUF_READER_IGNORE_VARINT_BYTE(8);
+    PROTOBUF_READER_IGNORE_VARINT_BYTE(9);
+    PROTOBUF_READER_IGNORE_VARINT_BYTE(10);
+#undef PROTOBUF_READER_IGNORE_VARINT_BYTE
 
     unknownFormat();
 }
@@ -667,10 +656,10 @@ private:
     {
         if (!readTempString())
             return false;
-        StringRef strref = StringRef(temp_string.data(), temp_string.size());
-        auto it = enum_name_to_value_map->find(strref);
+        StringRef ref(temp_string.data(), temp_string.size());
+        auto it = enum_name_to_value_map->find(ref);
         if (it == enum_name_to_value_map->end())
-            cannotConvertValue(strref.toString(), "Enum");
+            cannotConvertValue(ref.toString(), "Enum");
         value = static_cast<T>(it->second);
         return true;
     }
@@ -688,7 +677,7 @@ private:
     template <typename T>
     void prepareEnumNameToValueMap(const std::vector<std::pair<String, T>> & name_value_pairs)
     {
-        if (enum_name_to_value_map.has_value())
+        if (likely(enum_name_to_value_map.has_value()))
             return;
         enum_name_to_value_map.emplace();
         for (const auto & name_value_pair : name_value_pairs)
@@ -792,7 +781,7 @@ private:
         if (!readField(number))
             return false;
         value = numericCast<EnumType>(number);
-         if (set_of_enum_values->find(value) == set_of_enum_values->end())
+        if (set_of_enum_values->find(value) == set_of_enum_values->end())
             cannotConvertValue(toString(value), "Enum");
         return true;
     }
@@ -800,7 +789,7 @@ private:
     template<typename EnumType>
     void prepareSetOfEnumValues(const std::vector<std::pair<String, EnumType>> & name_value_pairs)
     {
-        if (set_of_enum_values.has_value())
+        if (likely(set_of_enum_values.has_value()))
             return;
         set_of_enum_values.emplace();
         for (const auto & name_value_pair : name_value_pairs)
@@ -884,8 +873,8 @@ public:
         bool b;
         if (!readField(b))
             return false;
-        WriteBufferFromVector<PaddedPODArray<UInt8>> buf(str);
-        writeString(StringRef(b ? "true" : "false"), buf);
+        StringRef ref(b ? "true" : "false");
+        str.insert(ref.data, ref.data + ref.size);
         return true;
     }
 
@@ -941,9 +930,8 @@ public:
         auto it = enum_pbnumber_to_name_map->find(pbnumber);
         if (it == enum_pbnumber_to_name_map->end())
             cannotConvertValue(toString(pbnumber), "Enum");
-
-        WriteBufferFromVector<PaddedPODArray<UInt8>> buf(str);
-        writeString(it->second, buf);
+        const auto & ref = it->second;
+        str.insert(ref.data, ref.data + ref.size);
         return true;
     }
 
@@ -994,7 +982,7 @@ private:
 
     void prepareEnumPbNumberToNameMap()
     {
-        if (enum_pbnumber_to_name_map.has_value())
+        if (likely(enum_pbnumber_to_name_map.has_value()))
             return;
         enum_pbnumber_to_name_map.emplace();
         const auto * enum_type = field->enum_type();
@@ -1008,7 +996,7 @@ private:
     template <typename T>
     void prepareEnumPbNumberToValueMap(const std::vector<std::pair<String, T>> & name_value_pairs)
     {
-        if (enum_pbnumber_to_value_map.has_value())
+        if (likely(enum_pbnumber_to_value_map.has_value()))
             return;
         enum_pbnumber_to_value_map.emplace();
         for (const auto & name_value_pair : name_value_pairs)
