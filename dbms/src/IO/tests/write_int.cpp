9b66d107811aae1d7118b071fcbe3189a3620c6c
Alexey Milovidov <milovidov@yandex-team.ru>
2019-02-25 23:21:05 +0300

    Simplified test

--- dbms/src/IO/tests/write_int.cpp
+++ dbms/src/IO/tests/write_int.cpp
@@ -6,7 +6,6 @@
 #include <IO/ReadHelpers.h>
 #include <IO/ReadBufferFromString.h>
 #include <IO/WriteBufferFromFileDescriptor.h>
-#include <IO/Operators.h>
 
 
 using namespace DB;
@@ -17,11 +16,10 @@ void NO_INLINE write(ReadBuffer & in, WriteBuffer & out, size_t size)
     for (size_t i = 0; i < size; ++i)
     {
         writeIntText(i, out);
+        writeChar(' ', out);
 
         int tmp;
         readIntText(tmp, in);
-
-        out << "parsed text: \n";
 
         if (in.eof() || *in.position() != '\t')
             return;
