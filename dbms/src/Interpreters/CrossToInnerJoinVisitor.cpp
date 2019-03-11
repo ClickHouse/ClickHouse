150fbc43c25d547cabbab7dd64562c0dea0ee85f
chertus <chertus@gmail.com>
2019-03-11 22:45:04 +0300

    improve CrossToInnerJoinVisitor for multiple JOINS

--- dbms/src/Interpreters/CrossToInnerJoinVisitor.cpp
+++ dbms/src/Interpreters/CrossToInnerJoinVisitor.cpp
@@ -18,7 +18,50 @@ namespace DB
 namespace ErrorCodes
 {
     extern const int LOGICAL_ERROR;
+    extern const int NOT_IMPLEMENTED;
 }
+
+namespace
+{
+
+struct JoinedTable
+{
+    DatabaseAndTableWithAlias table;
+    ASTTablesInSelectQueryElement * element = nullptr;
+    ASTTableJoin * join = nullptr;
+
+    JoinedTable(ASTPtr table_element)
+    {
+        element = typeid_cast<ASTTablesInSelectQueryElement *>(table_element.get());
+        if (!element)
+            throw Exception("Logical error: TablesInSelectQueryElement expected", ErrorCodes::LOGICAL_ERROR);
+
+        if (element->table_join)
+        {
+            join = typeid_cast<ASTTableJoin *>(element->table_join.get());
+            if (join->kind == ASTTableJoin::Kind::Cross ||
+                join->kind == ASTTableJoin::Kind::Comma)
+            {
+                if (!join->children.empty())
+                    throw Exception("Logical error: CROSS JOIN has expressions", ErrorCodes::LOGICAL_ERROR);
+            }
+
+            if (join->using_expression_list)
+                throw Exception("Multiple CROSS/COMMA JOIN do not support USING", ErrorCodes::NOT_IMPLEMENTED);
+        }
+
+        auto & expr = typeid_cast<const ASTTableExpression &>(*element->table_expression);
+        table = DatabaseAndTableWithAlias(expr);
+    }
+
+    void rewriteCommaToCross()
+    {
+        if (join)
+            join->kind = ASTTableJoin::Kind::Cross;
+    }
+
+    bool canAttachOnExpression() const { return join && !join->on_expression; }
+};
 
 /// It checks if where expression could be moved to JOIN ON expression partially or entirely.
 class CheckExpressionVisitorData
@@ -26,14 +69,16 @@ class CheckExpressionVisitorData
 public:
     using TypeToVisit = const ASTFunction;
 
-    CheckExpressionVisitorData(const std::vector<DatabaseAndTableWithAlias> & tables_)
+    CheckExpressionVisitorData(const std::vector<JoinedTable> & tables_)
         : tables(tables_)
-        , save_where(false)
-        , flat_ands(true)
+        , ands_only(true)
     {}
 
     void visit(const ASTFunction & node, ASTPtr & ast)
     {
+        if (!ands_only)
+            return;
+
         if (node.name == "and")
         {
             if (!node.arguments || node.arguments->children.empty())
@@ -42,50 +87,50 @@ public:
             for (auto & child : node.arguments->children)
             {
                 if (auto func = typeid_cast<const ASTFunction *>(child.get()))
-                {
-                    if (func->name == "and")
-                        flat_ands = false;
                     visit(*func, child);
-                }
                 else
-                    save_where = true;
+                    ands_only = false;
             }
         }
         else if (node.name == "equals")
         {
-            if (checkEquals(node))
-                asts_to_join_on.push_back(ast);
-            else
-                save_where = true;
+            if (size_t min_table = canMoveEqualsToJoinOn(node))
+                asts_to_join_on[min_table].push_back(ast);
         }
         else
-            save_where = true;
+        {
+            ands_only = false;
+            asts_to_join_on.clear();
+        }
     }
 
-    bool matchAny() const { return !asts_to_join_on.empty(); }
-    bool matchAll() const { return matchAny() && !save_where; }
-    bool canReuseWhere() const { return matchAll() && flat_ands; }
+    bool complex() const { return !ands_only; }
+    bool matchAny(size_t t) const { return asts_to_join_on.count(t); }
 
-    ASTPtr makeOnExpression()
+    ASTPtr makeOnExpression(size_t table_pos)
     {
-        if (asts_to_join_on.size() == 1)
-            return asts_to_join_on[0]->clone();
+        if (!asts_to_join_on.count(table_pos))
+            return {};
+
+        std::vector<ASTPtr> & expressions = asts_to_join_on[table_pos];
+
+        if (expressions.size() == 1)
+            return expressions[0]->clone();
 
         std::vector<ASTPtr> arguments;
-        arguments.reserve(asts_to_join_on.size());
-        for (auto & ast : asts_to_join_on)
+        arguments.reserve(expressions.size());
+        for (auto & ast : expressions)
             arguments.emplace_back(ast->clone());
 
         return makeASTFunction("and", std::move(arguments));
     }
 
 private:
-    const std::vector<DatabaseAndTableWithAlias> & tables;
-    std::vector<ASTPtr> asts_to_join_on;
-    bool save_where;
-    bool flat_ands;
+    const std::vector<JoinedTable> & tables;
+    std::map<size_t, std::vector<ASTPtr>> asts_to_join_on;
+    bool ands_only;
 
-    bool checkEquals(const ASTFunction & node)
+    size_t canMoveEqualsToJoinOn(const ASTFunction & node)
     {
         if (!node.arguments)
             throw Exception("Logical error: function requires argiment", ErrorCodes::LOGICAL_ERROR);
@@ -102,7 +147,8 @@ private:
 
     /// Check if the identifiers are from different joined tables. If it's a self joint, tables should have aliases.
     /// select * from t1 a cross join t2 b where a.x = b.x
-    bool checkIdentifiers(const ASTIdentifier & left, const ASTIdentifier & right)
+    /// @return table position to attach expression to or 0.
+    size_t checkIdentifiers(const ASTIdentifier & left, const ASTIdentifier & right)
     {
         /// {best_match, berst_table_pos}
         std::pair<size_t, size_t> left_best{0, 0};
@@ -110,14 +156,14 @@ private:
 
         for (size_t i = 0; i < tables.size(); ++i)
         {
-            size_t match = IdentifierSemantic::canReferColumnToTable(left, tables[i]);
+            size_t match = IdentifierSemantic::canReferColumnToTable(left, tables[i].table);
             if (match > left_best.first)
             {
                 left_best.first = match;
                 left_best.second = i;
             }
 
-            match = IdentifierSemantic::canReferColumnToTable(right, tables[i]);
+            match = IdentifierSemantic::canReferColumnToTable(right, tables[i].table);
             if (match > right_best.first)
             {
                 right_best.first = match;
@@ -125,59 +171,45 @@ private:
             }
         }
 
-        return left_best.first && right_best.first && (left_best.second != right_best.second);
+        if (left_best.first && right_best.first && (left_best.second != right_best.second))
+        {
+            size_t table_pos = std::max(left_best.second, right_best.second);
+            if (tables[table_pos].canAttachOnExpression())
+                return table_pos;
+        }
+        return 0;
     }
 };
 
-
-static bool extractTableName(const ASTTableExpression & expr, std::vector<DatabaseAndTableWithAlias> & names)
-{
-    /// Subselects are not supported.
-    if (!expr.database_and_table_name)
-        return false;
-
-    names.emplace_back(DatabaseAndTableWithAlias(expr));
-    return true;
-}
+using CheckExpressionMatcher = OneTypeMatcher<CheckExpressionVisitorData, false>;
+using CheckExpressionVisitor = InDepthNodeVisitor<CheckExpressionMatcher, true>;
 
 
-static ASTPtr getCrossJoin(ASTSelectQuery & select, std::vector<DatabaseAndTableWithAlias> & table_names)
+bool getTables(ASTSelectQuery & select, std::vector<JoinedTable> & joined_tables, size_t & num_comma)
 {
     if (!select.tables)
-        return {};
+        return false;
 
     auto tables = typeid_cast<const ASTTablesInSelectQuery *>(select.tables.get());
     if (!tables)
-        return {};
+        return false;
 
     size_t num_tables = tables->children.size();
-    if (num_tables != 2)
-        return {};
+    if (num_tables < 2)
+        return false;
 
-    auto left = typeid_cast<const ASTTablesInSelectQueryElement *>(tables->children[0].get());
-    auto right = typeid_cast<const ASTTablesInSelectQueryElement *>(tables->children[1].get());
-    if (!left || !right || !right->table_join)
-        return {};
-
-    if (auto join = typeid_cast<const ASTTableJoin *>(right->table_join.get()))
+    joined_tables.reserve(num_tables);
+    for (auto & child : tables->children)
     {
-        if (join->kind == ASTTableJoin::Kind::Cross ||
-            join->kind == ASTTableJoin::Kind::Comma)
-        {
-            if (!join->children.empty())
-                throw Exception("Logical error: CROSS JOIN has expressions", ErrorCodes::LOGICAL_ERROR);
+        joined_tables.emplace_back(JoinedTable(child));
 
-            auto & left_expr = typeid_cast<const ASTTableExpression &>(*left->table_expression);
-            auto & right_expr = typeid_cast<const ASTTableExpression &>(*right->table_expression);
-
-            table_names.reserve(2);
-            if (extractTableName(left_expr, table_names) &&
-                extractTableName(right_expr, table_names))
-                return right->table_join;
-        }
+        if (ASTTableJoin * join = joined_tables.back().join)
+            if (join->kind == ASTTableJoin::Kind::Comma)
+                ++num_comma;
     }
+    return true;
+}
 
-    return {};
 }
 
 
@@ -187,40 +219,47 @@ void CrossToInnerJoinMatcher::visit(ASTPtr & ast, Data & data)
         visit(*t, ast, data);
 }
 
-void CrossToInnerJoinMatcher::visit(ASTSelectQuery & select, ASTPtr & ast, Data & data)
+void CrossToInnerJoinMatcher::visit(ASTSelectQuery & select, ASTPtr &, Data & data)
 {
-    using CheckExpressionMatcher = OneTypeMatcher<CheckExpressionVisitorData, false>;
-    using CheckExpressionVisitor = InDepthNodeVisitor<CheckExpressionMatcher, true>;
+    size_t num_comma = 0;
+    std::vector<JoinedTable> joined_tables;
+    if (!getTables(select, joined_tables, num_comma))
+        return;
+
+    /// COMMA to CROSS
+
+    if (num_comma)
+    {
+        if (num_comma != (joined_tables.size() - 1))
+            throw Exception("Mix of COMMA and other JOINS is not supported", ErrorCodes::NOT_IMPLEMENTED);
+
+        for (auto & table : joined_tables)
+            table.rewriteCommaToCross();
+    }
+
+    /// CROSS to INNER
 
     if (!select.where_expression)
         return;
 
-    std::vector<DatabaseAndTableWithAlias> table_names;
-    ASTPtr ast_join = getCrossJoin(select, table_names);
-    if (!ast_join)
-        return;
-
-    CheckExpressionVisitor::Data visitor_data{table_names};
+    CheckExpressionVisitor::Data visitor_data{joined_tables};
     CheckExpressionVisitor(visitor_data).visit(select.where_expression);
 
-    if (visitor_data.matchAny())
+    if (visitor_data.complex())
+        return;
+
+    for (size_t i = 1; i < joined_tables.size(); ++i)
     {
-        auto & join = typeid_cast<ASTTableJoin &>(*ast_join);
-        join.kind = ASTTableJoin::Kind::Inner;
-        join.strictness = ASTTableJoin::Strictness::All;
+        if (visitor_data.matchAny(i))
+        {
+            ASTTableJoin & join = *joined_tables[i].join;
+            join.kind = ASTTableJoin::Kind::Inner;
+            join.strictness = ASTTableJoin::Strictness::All;
 
-        if (visitor_data.canReuseWhere())
-            join.on_expression.swap(select.where_expression);
-        else
-            join.on_expression = visitor_data.makeOnExpression();
-
-        if (visitor_data.matchAll())
-            select.where_expression.reset();
-
-        join.children.push_back(join.on_expression);
-
-        ast = ast->clone(); /// rewrite AST in right manner
-        data.done = true;
+            join.on_expression = visitor_data.makeOnExpression(i);
+            join.children.push_back(join.on_expression);
+            data.done = true;
+        }
     }
 }
 
