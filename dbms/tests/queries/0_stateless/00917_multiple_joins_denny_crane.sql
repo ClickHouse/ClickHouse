de61df8b5ed1f19315ee76de26d018042659a867
Ivan Lezhankin <ilezhankin@yandex-team.ru>
2019-03-14 17:59:45 +0300

    Disable part with predicate optimization for now

--- dbms/tests/queries/0_stateless/00917_multiple_joins_denny_crane.sql
+++ dbms/tests/queries/0_stateless/00917_multiple_joins_denny_crane.sql
@@ -13,23 +13,7 @@ from (
 select a.ANIMAL a, 'CAT' b, c.ANIMAL c, d.ANIMAL d
 from ANIMAL a join ANIMAL b on a.ANIMAL = b.ANIMAL
         left outer  join ANIMAL c on (b.ANIMAL = c.ANIMAL)
-        right outer join (select * from ANIMAL union all select * from ANIMAL 
-                          union all select * from ANIMAL)  d on (a.ANIMAL = d.ANIMAL)
-where d.ANIMAL <> 'CAT' and c.ANIMAL <>'DOG' and b.ANIMAL <> 'FISH') as x
-where x.b >= 'CAT'
-group by x.b
-having ANIMAL >= 0) ANIMAL
-where ANIMAL.ANIMAL >= 0;
-
-set enable_optimize_predicate_expression = 1;
-
-select * from (
-select x.b x, count(distinct x.c) ANIMAL
-from (
-select a.ANIMAL a, 'CAT' b, c.ANIMAL c, d.ANIMAL d
-from ANIMAL a join ANIMAL b on a.ANIMAL = b.ANIMAL
-        left outer  join ANIMAL c on (b.ANIMAL = c.ANIMAL)
-        right outer join (select * from ANIMAL union all select * from ANIMAL 
+        right outer join (select * from ANIMAL union all select * from ANIMAL
                           union all select * from ANIMAL)  d on (a.ANIMAL = d.ANIMAL)
 where d.ANIMAL <> 'CAT' and c.ANIMAL <>'DOG' and b.ANIMAL <> 'FISH') as x
 where x.b >= 'CAT'
