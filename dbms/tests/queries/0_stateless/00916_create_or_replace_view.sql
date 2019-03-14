84420eefef7728f09c44e1e8c72202145058f638
Alexey Milovidov <milovidov@yandex-team.ru>
2019-03-14 04:11:07 +0300

    Fixed test #4654.

--- dbms/tests/queries/0_stateless/00916_create_or_replace_view.sql
+++ dbms/tests/queries/0_stateless/00916_create_or_replace_view.sql
@@ -2,5 +2,8 @@ DROP TABLE IF EXISTS test.t;
 
 CREATE OR REPLACE VIEW test.t (number UInt64) AS SELECT number FROM system.numbers;
 SHOW CREATE TABLE test.t;
-CREATE OR REPLACE VIEW test.t (next_number UInt64) AS SELECT number+1 FROM system.numbers;
+
+CREATE OR REPLACE VIEW test.t AS SELECT number+1 AS next_number FROM system.numbers;
 SHOW CREATE TABLE test.t;
+
+DROP TABLE test.t;
