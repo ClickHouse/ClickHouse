14fdc2c66cea5eae0d4148207f1461c67532e0a6
alesapin <alesapin@gmail.com>
2019-03-18 19:21:52 +0300

    Fix granule size less than row size

--- dbms/tests/queries/0_stateless/00926_adaptive_index_granularity.sql
+++ dbms/tests/queries/0_stateless/00926_adaptive_index_granularity.sql
@@ -1,3 +1,27 @@
+DROP TABLE IF EXISTS test.zero_rows_per_granule;
+
+CREATE TABLE test.zero_rows_per_granule (
+  p Date,
+  k UInt64,
+  v1 UInt64,
+  v2 Int64
+) ENGINE MergeTree() PARTITION BY toYYYYMM(p) ORDER BY k SETTINGS index_granularity_bytes = 20;
+
+INSERT INTO test.zero_rows_per_granule (p, k, v1, v2) VALUES ('2018-05-15', 1, 1000, 2000), ('2018-05-16', 2, 3000, 4000), ('2018-05-17', 3, 5000, 6000), ('2018-05-18', 4, 7000, 8000);
+
+SELECT COUNT(*) FROM test.zero_rows_per_granule;
+
+OPTIMIZE TABLE test.zero_rows_per_granule FINAL;
+
+
+INSERT INTO test.zero_rows_per_granule (p, k, v1, v2) VALUES ('2018-05-15', 1, 1000, 2000), ('2018-05-16', 5, 3000, 4000), ('2018-05-17', 6, 5000, 6000), ('2018-05-19', 7, 7000, 8000);
+
+SELECT COUNT(*) FROM test.zero_rows_per_granule;
+
+DROP TABLE IF EXISTS test.zero_rows_per_granule;
+
+-----
+
 DROP TABLE IF EXISTS test.two_rows_per_granule;
 
 CREATE TABLE test.two_rows_per_granule (
@@ -20,7 +44,9 @@ SELECT COUNT(*) FROM test.two_rows_per_granule;
 
 DROP TABLE IF EXISTS test.two_rows_per_granule;
 
-DROP TABLE IF EXISTS test.two_rows_per_granule;
+------
+
+DROP TABLE IF EXISTS test.four_rows_per_granule;
 
 CREATE TABLE test.four_rows_per_granule (
   p Date,
