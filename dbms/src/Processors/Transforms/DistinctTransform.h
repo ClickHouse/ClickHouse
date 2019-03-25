b2e79039922329953c8e8572e065910d815e762c
Nikolai Kochetov <nik-kochetov@yandex-team.ru>
2019-03-25 19:37:27 +0300

    Added DistinctTransform

---
+++ dbms/src/Processors/Transforms/DistinctTransform.h
@@ -0,0 +1,15 @@
+//
+// Created by nik-kochetov on 3/25/19.
+//
+
+#ifndef CLICKHOUSE_DISTINCTTRANSFORM_H
+#define CLICKHOUSE_DISTINCTTRANSFORM_H
+
+
+class DistinctTransform
+{
+
+};
+
+
+#endif //CLICKHOUSE_DISTINCTTRANSFORM_H
