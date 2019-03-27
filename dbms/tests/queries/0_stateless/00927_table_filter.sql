14e4a6e4cb2f5c048ea878b2100c2b6cc4e79228
Ivan Lezhankin <ilezhankin@yandex-team.ru>
2019-03-27 17:55:23 +0300

    [WIP] Add test reference

--- dbms/tests/queries/0_stateless/00927_table_filter.sql
+++ dbms/tests/queries/0_stateless/00927_table_filter.sql
@@ -6,7 +6,7 @@ DROP TABLE IF EXISTS filtered_table3;
 CREATE TABLE test.filtered_table1 (a UInt8, b UInt8) ENGINE MergeTree ORDER BY a;
 INSERT INTO test.filtered_table1 values (0, 0), (0, 1), (1, 0), (1, 1);
 
--- Filter: max(a + b, c - d) > 5, values: (4, 3, 2, 1), (0, 0, 6, 0)
+-- Filter: a + b < 1 or c - d > 5, values: (0, 0, 0, 0), (0, 0, 6, 0)
 CREATE TABLE test.filtered_table2 (a UInt8, b UInt8, c UInt8, d UInt8) ENGINE MergeTree ORDER BY a;
 INSERT INTO test.filtered_table2 values (0, 0, 0, 0), (1, 2, 3, 4), (4, 3, 2, 1), (0, 0, 6, 0);
 
@@ -14,10 +14,10 @@ INSERT INTO test.filtered_table2 values (0, 0, 0, 0), (1, 2, 3, 4), (4, 3, 2, 1)
 CREATE TABLE test.filtered_table3 (a UInt8, b UInt8, c UInt16 ALIAS a + b) ENGINE MergeTree ORDER BY a;
 INSERT INTO test.filtered_table3 values (0, 0), (0, 1), (1, 0), (1, 1);
 
-SELECT '-- PREWHERE should fail'
-SELECT * FROM test.filtered_table1 PREWHERE 1;
-SELECT * FROM test.filtered_table2 PREWHERE 1;
-SELECT * FROM test.filtered_table3 PREWHERE 1;
+SELECT '-- PREWHERE should fail';
+SELECT * FROM test.filtered_table1 PREWHERE 1; -- { serverError 182 }
+SELECT * FROM test.filtered_table2 PREWHERE 1; -- { serverError 182 }
+SELECT * FROM test.filtered_table3 PREWHERE 1; -- { serverError 182 }
 
 SELECT * FROM test.filtered_table1;
 SELECT * FROM test.filtered_table2;
