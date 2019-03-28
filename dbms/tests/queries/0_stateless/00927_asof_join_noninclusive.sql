9fc7f67c52cdbf8570e32d0dc110932d416539c6
Martijn Bakker <gladdyu@gmail.com>
2019-03-28 18:59:10 +0000

    add no asof joined values to tests

--- dbms/tests/queries/0_stateless/00927_asof_join_noninclusive.sql
+++ dbms/tests/queries/0_stateless/00927_asof_join_noninclusive.sql
@@ -4,8 +4,9 @@ DROP TABLE IF EXISTS A;
 DROP TABLE IF EXISTS B;
 
 CREATE TABLE A(k UInt32, t DateTime, a Float64) ENGINE = MergeTree() ORDER BY (k, t);
-INSERT INTO A(k,t,a) VALUES (1,1,1),(1,2,2),(1,3,3),(1,4,4),(1,5,5);
-INSERT INTO A(k,t,a) VALUES (2,1,1),(2,2,2),(2,3,3),(2,4,4),(2,5,5);
+INSERT INTO A(k,t,a) VALUES (1,1,1),(1,2,2),(1,3,3),(1,4,4),(1,5,5);  -- multiple joined values
+INSERT INTO A(k,t,a) VALUES (2,1,1),(2,2,2),(2,3,3),(2,4,4),(2,5,5);  -- one joined value
+INSERT INTO A(k,t,a) VALUES (3,1,1),(3,2,2),(3,3,3),(3,4,4),(3,5,5);  -- no joined values
 
 CREATE TABLE B(k UInt32, t DateTime, b Float64) ENGINE = MergeTree() ORDER BY (k, t);
 INSERT INTO B(k,t,b) VALUES (1,2,2),(1,4,4);
