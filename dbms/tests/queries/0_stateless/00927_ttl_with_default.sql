bd3b4467a88d50e67047de221651d578c2121e06
CurtizJ <pad11rus@gmail.com>
2019-03-30 05:32:33 +0300

    attempt to fix test

--- dbms/tests/queries/0_stateless/00927_ttl_with_default.sql
+++ dbms/tests/queries/0_stateless/00927_ttl_with_default.sql
@@ -4,6 +4,7 @@ create table test.ttl (d DateTime, a Int default 111 ttl d + interval 1 DAY) eng
 insert into test.ttl values (toDateTime('2000-10-10 00:00:00'), 1);
 insert into test.ttl values (toDateTime('2000-10-10 00:00:00'), 2);
 optimize table test.ttl final;
+select sleep(0.1);
 select a from test.ttl order by a;
 
 drop table if exists test.ttl;
@@ -12,4 +13,15 @@ create table test.ttl (d DateTime, a Int, b default a * 2 ttl d + interval 1 DAY
 insert into test.ttl values (toDateTime('2000-10-10 00:00:00'), 1, 100);
 insert into test.ttl values (toDateTime('2000-10-10 00:00:00'), 2, 200);
 optimize table test.ttl final;
+select sleep(0.1);
 select a, b from test.ttl order by a;
+
+drop table if exists test.ttl;
+
+create table test.ttl (d DateTime, a Int, b default 222 ttl d + interval 1 DAY) engine = MergeTree order by tuple() partition by toDayOfMonth(d);
+insert into test.ttl values (toDateTime('2000-10-10 00:00:00'), 1, 5);
+insert into test.ttl values (toDateTime('2100-10-10 00:00:00'), 2, 10);
+optimize table test.ttl final;
+select sleep(0.1);
+select a, b from test.ttl order by a;
+
