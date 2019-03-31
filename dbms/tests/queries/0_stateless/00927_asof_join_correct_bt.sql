27776ca929c3b529dcb505aff0d00a1a712a4771
Martijn Bakker <gladdyu@gmail.com>
2019-03-31 22:56:37 +0100

    fix up wrong assumption that the sample_block_with_keys has same ordering as key_names_right

--- dbms/tests/queries/0_stateless/00927_asof_join_correct_bt.sql
+++ dbms/tests/queries/0_stateless/00927_asof_join_correct_bt.sql
@@ -8,8 +8,18 @@ INSERT INTO A(k,t,a) VALUES (1,101,1),(1,102,2),(1,103,3),(1,104,4),(1,105,5);
 
 CREATE TABLE B(k UInt32, t UInt32, b UInt64) ENGINE = MergeTree() ORDER BY (k, t);
 INSERT INTO B(k,t,b) VALUES (1,102,2), (1,104,4);
-
 SELECT A.k, A.t, A.a, B.b, B.t, B.k FROM A ASOF LEFT JOIN B USING(k,t) ORDER BY (A.k, A.t);
+DROP TABLE B;
+
+
+CREATE TABLE B(t UInt32, k UInt32, b UInt64) ENGINE = MergeTree() ORDER BY (k, t);
+INSERT INTO B(k,t,b) VALUES (1,102,2), (1,104,4);
+SELECT A.k, A.t, A.a, B.b, B.t, B.k FROM A ASOF LEFT JOIN B USING(k,t) ORDER BY (A.k, A.t);
+DROP TABLE B;
+
+CREATE TABLE B(k UInt32, b UInt64, t UInt32) ENGINE = MergeTree() ORDER BY (k, t);
+INSERT INTO B(k,t,b) VALUES (1,102,2), (1,104,4);
+SELECT A.k, A.t, A.a, B.b, B.t, B.k FROM A ASOF LEFT JOIN B USING(k,t) ORDER BY (A.k, A.t);
+DROP TABLE B;
 
 DROP TABLE A;
-DROP TABLE B;
