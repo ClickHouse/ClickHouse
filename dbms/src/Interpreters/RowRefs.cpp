e910deb4e3d6ba628c18cc16a52b9ed0ae1fbd53
chertus <chertus@gmail.com>
2019-04-01 19:44:15 +0300

    get rid of macro in AsofRowRefs

--- dbms/src/Interpreters/RowRefs.cpp
+++ dbms/src/Interpreters/RowRefs.cpp
@@ -1,24 +1,46 @@
 #include <Interpreters/RowRefs.h>
 
+#include <Common/typeid_cast.h>
 #include <Common/ColumnsHashing.h>
 #include <Core/Block.h>
 #include <Columns/IColumn.h>
 
-#include <optional>
 
 namespace DB
 {
 
-void AsofRowRefs::Lookups::create(AsofRowRefs::Type which)
+namespace
+{
+
+/// maps enum values to types
+template <typename F>
+void callWithType(AsofRowRefs::Type which, F && f)
 {
     switch (which)
     {
-        case Type::EMPTY: break;
-    #define M(NAME, TYPE) \
-        case Type::NAME: NAME = std::make_unique<typename decltype(NAME)::element_type>(); break;
-            APPLY_FOR_ASOF_JOIN_VARIANTS(M)
-    #undef M
+        case AsofRowRefs::Type::key32:  return f(AsofRowRefs::LookupTypes<UInt32>());
+        case AsofRowRefs::Type::key64:  return f(AsofRowRefs::LookupTypes<UInt64>());
+        case AsofRowRefs::Type::keyf32: return f(AsofRowRefs::LookupTypes<Float32>());
+        case AsofRowRefs::Type::keyf64: return f(AsofRowRefs::LookupTypes<Float64>());
     }
+
+    __builtin_unreachable();
+}
+
+} // namespace
+
+
+void AsofRowRefs::createLookup(AsofRowRefs::Type which)
+{
+    auto call = [&](const auto & types)
+    {
+        using Types = std::decay_t<decltype(types)>;
+        using SearcherType = typename Types::SearcherType;
+
+        lookups = std::make_unique<SearcherType>();
+    };
+
+    callWithType(which, call);
 }
 
 template<typename T>
@@ -26,49 +48,68 @@ using AsofGetterType = ColumnsHashing::HashMethodOneNumber<T, T, T, false>;
 
 void AsofRowRefs::insert(const IColumn * asof_column, const Block * block, size_t row_num, Arena & pool)
 {
-    switch (type)
+    auto call = [&](const auto & types)
     {
-        case Type::EMPTY: break;
-    #define M(NAME, TYPE) \
-        case Type::NAME: {  \
-            auto asof_getter = AsofGetterType<TYPE>(asof_column); \
-            auto entry = Entry<TYPE>(asof_getter.getKey(row_num, pool), RowRef(block, row_num));  \
-            lookups.NAME->insert(entry); \
-            break;    \
-        }
-            APPLY_FOR_ASOF_JOIN_VARIANTS(M)
-    #undef M
-    }
+        using Types = std::decay_t<decltype(types)>;
+        using ElementType = typename Types::ElementType;
+        using SearcherPtr = typename Types::Ptr;
+
+        auto asof_getter = AsofGetterType<ElementType>(asof_column);
+        auto entry = Entry<ElementType>(asof_getter.getKey(row_num, pool), RowRef(block, row_num));
+
+        std::get<SearcherPtr>(lookups)->insert(entry);
+    };
+
+    callWithType(*type, call);
 }
 
 const RowRef * AsofRowRefs::findAsof(const IColumn * asof_column, size_t row_num, Arena & pool) const
 {
-    switch (type)
-    {
-        case Type::EMPTY: return nullptr;
-    #define M(NAME, TYPE) \
-        case Type::NAME: {  \
-            auto asof_getter = AsofGetterType<TYPE>(asof_column); \
-            TYPE key = asof_getter.getKey(row_num, pool);   \
-            auto it = lookups.NAME->upper_bound(Entry<TYPE>(key));   \
-            if (it == lookups.NAME->cbegin())  \
-                return nullptr;  \
-            return &((--it)->row_ref); \
-        }
-            APPLY_FOR_ASOF_JOIN_VARIANTS(M)
-    #undef M
-    }
+    const RowRef * out = nullptr;
 
-    __builtin_unreachable();
+    auto call = [&](const auto & types)
+    {
+        using Types = std::decay_t<decltype(types)>;
+        using ElementType = typename Types::ElementType;
+        using SearcherPtr = typename Types::Ptr;
+
+        auto asof_getter = AsofGetterType<ElementType>(asof_column);
+        ElementType key = asof_getter.getKey(row_num, pool);
+        auto & typed_lookup = std::get<SearcherPtr>(lookups);
+
+        auto it = typed_lookup->upper_bound(Entry<ElementType>(key));
+        if (it != typed_lookup->cbegin())
+            out = &((--it)->row_ref);
+    };
+
+    callWithType(*type, call);
+    return out;
 }
 
-std::optional<std::pair<AsofRowRefs::Type, size_t>> AsofRowRefs::getTypeSize(const IColumn * asof_column)
+std::optional<AsofRowRefs::Type> AsofRowRefs::getTypeSize(const IColumn * asof_column, size_t & size)
 {
-    #define M(NAME, TYPE) \
-    if (strcmp(#TYPE, asof_column->getFamilyName()) == 0) \
-        return std::make_pair(Type::NAME,sizeof(TYPE));
-    APPLY_FOR_ASOF_JOIN_VARIANTS(M)
-    #undef M
+    if (typeid_cast<const ColumnVector<UInt32> *>(asof_column))
+    {
+        size = sizeof(UInt32);
+        return Type::key32;
+    }
+    else if (typeid_cast<const ColumnVector<UInt64> *>(asof_column))
+    {
+        size = sizeof(UInt64);
+        return Type::key64;
+    }
+    else if (typeid_cast<const ColumnVector<Float32> *>(asof_column))
+    {
+        size = sizeof(Float32);
+        return Type::keyf32;
+    }
+    else if (typeid_cast<const ColumnVector<Float64> *>(asof_column))
+    {
+        size = sizeof(Float64);
+        return Type::keyf64;
+    }
+
+    size = 0;
     return {};
 }
 
