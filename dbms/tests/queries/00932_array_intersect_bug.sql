cd7368dc6de0e633328d2afad513bf69b3eb5b0a
Nikolai Kochetov <nik-kochetov@yandex-team.ru>
2019-04-01 16:29:20 +0300

    Added tests for arrayIntersect.

---
+++ dbms/tests/queries/00932_array_intersect_bug.sql
@@ -0,0 +1,5 @@
+SELECT arrayIntersect(['a', 'b', 'c'], ['a', 'a']);
+SELECT arrayIntersect([1, 1], [2, 2]);
+SELECT arrayIntersect([1, 1], [1, 2]);
+SELECT arrayIntersect([1, 1, 1], [3], [2, 2, 2]);
+
