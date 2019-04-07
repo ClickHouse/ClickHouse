5eb47f661af9f38a351543596900fe45cac2d19f
Yuriy <yurrio@ya.ru>
2019-04-07 13:29:30 +0300

    added golang mysql client test

--- dbms/tests/integration/test_mysql_protocol/test.py
+++ dbms/tests/integration/test_mysql_protocol/test.py
@@ -1,7 +1,9 @@
 # coding: utf-8
 
 import os
+import docker
 import pytest
+import subprocess
 import pymysql.connections
 
 from docker.models.containers import Container
@@ -12,35 +14,37 @@ from helpers.cluster import ClickHouseCluster
 SCRIPT_DIR = os.path.dirname(os.path.realpath(__file__))
 
 cluster = ClickHouseCluster(__file__, base_configs_dir=os.path.join(SCRIPT_DIR, './configs'))
-
-node = cluster.add_instance('node', with_mysql=True)
+node = cluster.add_instance('node')
 
 server_port = 9001
 
 
 @pytest.fixture(scope="module")
-def started_cluster():
+def server_address():
     cluster.start()
     try:
-        yield cluster
+        yield cluster.get_instance_ip('node')
     finally:
         cluster.shutdown()
 
 
 @pytest.fixture(scope='module')
-def mysql_container(started_cluster):
-    # type: (ClickHouseCluster) -> Container
-    yield started_cluster.docker_client.containers.get(started_cluster.get_instance_docker_id('mysql1'))
+def mysql_client():
+    docker_compose = os.path.join(SCRIPT_DIR, 'clients', 'mysql', 'docker_compose.yml')
+    subprocess.check_call(['docker-compose', '-p', cluster.project_name, '-f', docker_compose, 'up', '--no-recreate', '-d', '--build'])
+    yield docker.from_env().containers.get(cluster.project_name + '_mysql1_1')
 
 
 @pytest.fixture(scope='module')
-def server_address(started_cluster):
-    yield started_cluster.get_instance_ip('node')
+def golang_container():
+    docker_compose = os.path.join(SCRIPT_DIR, 'clients', 'golang', 'docker_compose.yml')
+    subprocess.check_call(['docker-compose', '-p', cluster.project_name, '-f', docker_compose, 'up', '--no-recreate', '-d', '--build'])
+    yield docker.from_env().containers.get(cluster.project_name + '_golang1_1')
 
 
-def test_select(mysql_container, server_address):
+def test_mysql_client(mysql_client, server_address):
     # type: (Container, str) -> None
-    code, (stdout, stderr) = mysql_container.exec_run('''
+    code, (stdout, stderr) = mysql_client.exec_run('''
         mysql --protocol tcp -h {host} -P {port} default -u default --enable-cleartext-plugin --password=123
         -e "select 1 as a;"
         -e "select 'тест' as b;"
@@ -48,22 +52,14 @@ def test_select(mysql_container, server_address):
 
     assert stdout == 'a\n1\nb\nтест\n'
 
-
-def test_authentication(mysql_container, server_address):
-    # type: (Container, str) -> None
-
-    code, (stdout, stderr) = mysql_container.exec_run('''
+    code, (stdout, stderr) = mysql_client.exec_run('''
         mysql --protocol tcp -h {host} -P {port} default -u default --enable-cleartext-plugin --password=abc -e "select 1 as a;"
     '''.format(host=server_address, port=server_port), demux=True)
 
     assert stderr == 'mysql: [Warning] Using a password on the command line interface can be insecure.\n' \
                      'ERROR 193 (00000): Wrong password for user default\n'
 
-
-def test_change_database(mysql_container, server_address):
-    # type: (Container, str) -> None
-
-    code, (stdout, stderr) = mysql_container.exec_run('''
+    code, (stdout, stderr) = mysql_client.exec_run('''
         mysql --protocol tcp -h {host} -P {port} default -u default --enable-cleartext-plugin --password=123
         -e "use system;"
         -e "select count(*) from (select name from tables limit 1);"
@@ -75,7 +71,7 @@ def test_change_database(mysql_container, server_address):
                      "ERROR 81 (00000) at line 1: Database system2 doesn't exist\n"
 
 
-def test_py_client(server_address):
+def test_python_client(server_address):
     with pytest.raises(pymysql.InternalError) as exc_info:
         pymysql.connections.Connection(host=server_address, user='default', password='abacab', database='default', port=server_port)
 
@@ -98,3 +94,21 @@ def test_py_client(server_address):
         client.select_db('system2')
 
     assert exc_info.value.args == (81, "Database system2 doesn't exist")
+
+
+def test_golang_client(server_address, golang_container):
+    # type: (str, Container) -> None
+    code, (stdout, stderr) = golang_container.exec_run('./main --host {host} --port {port} --user default --password 123 --database '
+                                                       'abc'.format(host=server_address, port=server_port), demux=True)
+
+    assert code == 1
+    assert stderr == "Error 81: Database abc doesn't exist\n"
+
+    code, (stdout, stderr) = golang_container.exec_run('./main --host {host} --port {port} --user default --password 123 --database '
+                                                       'default'.format(host=server_address, port=server_port), demux=True)
+
+    assert code == 0
+
+    with open(os.path.join(SCRIPT_DIR, 'clients', 'golang', '0.reference')) as fp:
+        reference = fp.read()
+        assert stdout == reference
