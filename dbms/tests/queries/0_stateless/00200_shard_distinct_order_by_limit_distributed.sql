54431302b49321fcf6c97439b73413e04b68dc45
proller <proller@github.com>
2019-04-10 20:40:48 +0300

    fix tests

--- dbms/tests/queries/0_stateless/00200_shard_distinct_order_by_limit_distributed.sql
+++ dbms/tests/queries/0_stateless/00200_shard_distinct_order_by_limit_distributed.sql
@@ -1,5 +1,5 @@
-DROP TABLE IF EXISTS numbers_memory;
-CREATE TABLE numbers_memory AS system.numbers ENGINE = Memory;
-INSERT INTO numbers_memory SELECT number FROM system.numbers LIMIT 100;
-SELECT DISTINCT number FROM remote('127.0.0.{2,3}', default.numbers_memory) ORDER BY number LIMIT 10;
-DROP TABLE numbers_memory;
+DROP TABLE IF EXISTS test.numbers_memory;
+CREATE TABLE test.numbers_memory AS system.numbers ENGINE = Memory;
+INSERT INTO test.numbers_memory SELECT number FROM system.numbers LIMIT 100;
+SELECT DISTINCT number FROM remote('127.0.0.{2,3}', test.numbers_memory) ORDER BY number LIMIT 10;
+DROP TABLE test.numbers_memory;
