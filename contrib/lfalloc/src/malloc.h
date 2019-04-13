1cc684b665ddfbedaf7b8751e29ba634d4c2e240
Danila Kutenin <kutdanila@yandex.ru>
2019-04-13 15:07:43 +0300

    Enable LFAlloc for MarkCache and UncompressedCache

--- contrib/lfalloc/src/malloc.h
+++ contrib/lfalloc/src/malloc.h
@@ -1,25 +1,16 @@
 #pragma once
 
 #include <string.h>
+#include <stdlib.h>
 #include "util/system/compiler.h"
 
 namespace NMalloc {
-    struct TMallocInfo {
-        TMallocInfo();
+    volatile inline bool IsAllocatorCorrupted = false;
 
-        const char* Name;
-
-        bool (*SetParam)(const char* param, const char* value);
-        const char* (*GetParam)(const char* param);
-
-        bool (*CheckParam)(const char* param, bool defaultValue);
-    };
-
-    extern volatile bool IsAllocatorCorrupted;
-    void AbortFromCorruptedAllocator();
-
-    // this function should be implemented by malloc implementations
-    TMallocInfo MallocInfo();
+    static inline void AbortFromCorruptedAllocator() {
+        IsAllocatorCorrupted = true;
+        abort();
+    }
 
     struct TAllocHeader {
         void* Block;
---
+++ dbms/src/Common/LFAllocator.h
@@ -0,0 +1,21 @@
+#pragma once
+
+#include <Common/config.h>
+#include <cstddef>
+
+#if !USE_LFALLOC
+#error "do not include this file until USE_LFALLOC is set to 1"
+#endif
+
+namespace DB
+{
+struct LFAllocator
+{
+    static void * alloc(size_t size, size_t alignment = 0);
+
+    static void free(void * buf, size_t);
+
+    static void * realloc(void * buf, size_t, size_t new_size, size_t alignment = 0);
+};
+
+}
