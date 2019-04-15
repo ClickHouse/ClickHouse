3a23360e02a50fd22b1a006f11fb264f7d1a80ca
Guillaume Tassery <tassery.guillaume@gmail.com>
2019-04-15 17:09:01 +0700

    Add functional test for size parameter on groupUniqArray

--- dbms/tests/queries/0_stateless/00399_group_uniq_array_date_datetime.sql
+++ dbms/tests/queries/0_stateless/00399_group_uniq_array_date_datetime.sql
@@ -1,5 +1,12 @@
 DROP TABLE IF EXISTS test.grop_uniq_array_date;
-CREATE TABLE test.grop_uniq_array_date (d Date, dt DateTime) ENGINE = Memory;
-INSERT INTO test.grop_uniq_array_date VALUES (toDate('2016-12-16'), toDateTime('2016-12-16 12:00:00')) (toDate('2016-12-16'), toDateTime('2016-12-16 12:00:00'));
+CREATE TABLE test.grop_uniq_array_date (d Date, dt DateTime, id Integer) ENGINE = Memory;
+
+INSERT INTO test.grop_uniq_array_date VALUES (toDate('2016-12-16'), toDateTime('2016-12-16 12:00:00'), 1) (toDate('2016-12-16'), toDateTime('2016-12-16 12:00:00'), 1);
 SELECT groupUniqArray(d), groupUniqArray(dt) FROM test.grop_uniq_array_date;
+
+INSERT INTO test.grop_uniq_array_date VALUES (toDate('2016-12-17'), toDateTime('2016-12-17 12:00:00'), 1), (toDate('2016-12-18'), toDateTime('2016-12-18 12:00:00'), 1), (toDate('2016-12-16'), toDateTime('2016-12-16 12:00:00'), 2);
+
+SELECT length(groupUniqArray(2)(d)), length(groupUniqArray(2)(dt)), length(groupUniqArray(d)), length(groupUniqArray(dt)) FROM test.grop_uniq_array_date GROUP BY id ORDER BY id;
+SELECT length(groupUniqArray(10000)(d)), length(groupUniqArray(10000)(dt)) FROM test.grop_uniq_array_date GROUP BY id ORDER BY id;
+
 DROP TABLE IF EXISTS test.grop_uniq_array_date;
