26cc13bd28464320197cc4356e1889f8d68a97fa
proller <proller@users.noreply.github.com>
2019-04-16 17:13:13 +0300

    Tests: Allow use any test database (remove test. hardcode where it possible) (#5008)

--- dbms/tests/queries/0_stateless/00712_prewhere_with_missing_columns.sql
+++ dbms/tests/queries/0_stateless/00712_prewhere_with_missing_columns.sql
@@ -1,14 +1,14 @@
-DROP TABLE IF EXISTS test.mergetree;
-CREATE TABLE test.mergetree (x UInt8, s String) ENGINE = MergeTree ORDER BY tuple();
+DROP TABLE IF EXISTS mergetree;
+CREATE TABLE mergetree (x UInt8, s String) ENGINE = MergeTree ORDER BY tuple();
 
-INSERT INTO test.mergetree VALUES (1, 'Hello, world!');
-SELECT * FROM test.mergetree;
+INSERT INTO mergetree VALUES (1, 'Hello, world!');
+SELECT * FROM mergetree;
 
-ALTER TABLE test.mergetree ADD COLUMN y UInt8 DEFAULT 0;
-INSERT INTO test.mergetree VALUES (2, 'Goodbye.', 3);
-SELECT * FROM test.mergetree ORDER BY x;
+ALTER TABLE mergetree ADD COLUMN y UInt8 DEFAULT 0;
+INSERT INTO mergetree VALUES (2, 'Goodbye.', 3);
+SELECT * FROM mergetree ORDER BY x;
 
-SELECT s FROM test.mergetree PREWHERE x AND y ORDER BY s;
-SELECT s, y FROM test.mergetree PREWHERE x AND y ORDER BY s;
+SELECT s FROM mergetree PREWHERE x AND y ORDER BY s;
+SELECT s, y FROM mergetree PREWHERE x AND y ORDER BY s;
 
-DROP TABLE test.mergetree;
+DROP TABLE mergetree;
