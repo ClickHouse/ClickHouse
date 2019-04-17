c92a0f3ac5eacd93873f84da259d59d0a7a69c59
Alexander Tokmakov <tavplubix@gmail.com>
2019-04-17 21:10:24 +0300

    Fixes

--- dbms/src/Formats/TemplateRowInputStream.h
+++ dbms/src/Formats/TemplateRowInputStream.h
@@ -21,9 +21,8 @@ public:
 
     void readPrefix() override;
 
-    // TODO
-    //bool allowSyncAfterError() const override;
-    //void syncAfterError() override;
+    bool allowSyncAfterError() const override;
+    void syncAfterError() override;
 
 private:
     void deserializeField(const IDataType & type, IColumn & column, ColumnFormat col_format);
@@ -31,16 +30,15 @@ private:
     bool checkForSuffix();
     bool compareSuffixPart(StringRef & suffix, BufferBase::Position pos, size_t available);
 
-    bool parseRowAndPrintDiagnosticInfo(MutableColumns & columns, WriteBuffer & out,
-                                                size_t max_length_of_column_name, size_t max_length_of_data_type_name) override;
-    void tryDeserializeFiled(MutableColumns & columns, size_t col_idx,
-                                     ReadBuffer::Position & prev_pos, ReadBuffer::Position & curr_pos) override;
+    bool parseRowAndPrintDiagnosticInfo(MutableColumns & columns, WriteBuffer & out) override;
+    void tryDeserializeFiled(const DataTypePtr & type, IColumn & column, size_t input_position, ReadBuffer::Position & prev_pos,
+                             ReadBuffer::Position & curr_pos) override;
     bool isGarbageAfterField(size_t after_col_idx, ReadBuffer::Position pos) override;
     void writeErrorStringForWrongDelimiter(WriteBuffer & out, const String & description, const String & delim);
 
-
 private:
     PeekableReadBuffer buf;
+    DataTypes data_types;
 
     FormatSettings settings;
     ParsedTemplateFormat format;
