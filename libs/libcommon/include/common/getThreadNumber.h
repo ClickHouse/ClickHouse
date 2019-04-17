cb1cc9dbecc85350e413559ce73daf688642a76e
proller <proller@users.noreply.github.com>
2019-04-17 17:53:54 +0300

    Move Poco/Ext/ThreadNumber.h to common/getThreadNumber.h (#5016)

--- libs/libcommon/include/common/getThreadNumber.h
+++ libs/libcommon/include/common/getThreadNumber.h
@@ -3,12 +3,5 @@
 /** Последовательный номер потока, начиная с 1, среди тех потоков, для которых был получен этот номер.
   * Используется при логгировании.
   */
-namespace Poco
-{
 
-namespace ThreadNumber
-{
-    unsigned get();
-}
-
-}
+unsigned getThreadNumber();
