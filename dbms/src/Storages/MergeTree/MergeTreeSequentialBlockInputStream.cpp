98392b0ffe7ba71f5d9eeb1d21283291cff7d146
Nikolai Kochetov <nik-kochetov@yandex-team.ru>
2019-04-17 20:12:59 +0300

    Undo fix header for MergeTreeSequentialBlockInputStream.

--- dbms/src/Storages/MergeTree/MergeTreeSequentialBlockInputStream.cpp
+++ dbms/src/Storages/MergeTree/MergeTreeSequentialBlockInputStream.cpp
@@ -36,6 +36,9 @@ MergeTreeSequentialBlockInputStream::MergeTreeSequentialBlockInputStream(
 
     addTotalRowsApprox(data_part->rows_count);
 
+    header = storage.getSampleBlockForColumns(columns_to_read);
+    fixHeader(header);
+
     /// Add columns because we don't want to read empty blocks
     injectRequiredColumns(storage, data_part, columns_to_read);
     NamesAndTypesList columns_for_reader;
@@ -57,18 +60,6 @@ MergeTreeSequentialBlockInputStream::MergeTreeSequentialBlockInputStream(
         /* bytes to use AIO (this is hack) */
         read_with_direct_io ? 1UL : std::numeric_limits<size_t>::max(),
         DBMS_DEFAULT_BUFFER_SIZE);
-
-    header = storage.getSampleBlockForColumns(columns_to_read);
-    fixHeader(header);
-
-    bool should_reorder = false, should_evaluate_missing_defaults = false;
-    reader->fillMissingColumns(header, should_reorder, should_evaluate_missing_defaults, header.rows());
-
-    if (should_evaluate_missing_defaults)
-        reader->evaluateMissingDefaults(header);
-
-    if (should_reorder)
-        reader->reorderColumns(header, header.getNames(), nullptr);
 }
 
 
