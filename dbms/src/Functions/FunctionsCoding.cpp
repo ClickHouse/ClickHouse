153c9d6455ad9a88e3433ebea9138980258a1e2c
Guillaume Tassery <tassery.guillaume@gmail.com>
2019-04-22 18:21:59 +0700

    Add function for get an IP range from a CIDR

--- dbms/src/Functions/FunctionsCoding.cpp
+++ dbms/src/Functions/FunctionsCoding.cpp
@@ -28,6 +28,7 @@ void registerFunctionsCoding(FunctionFactory & factory)
     factory.registerFunction<FunctionBitmaskToArray>();
     factory.registerFunction<FunctionToIPv4>();
     factory.registerFunction<FunctionToIPv6>();
+    factory.registerFunction<FunctionIPv6CIDRtoIPv6Range>();
 }
 
 }
