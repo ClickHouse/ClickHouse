1935d890cd3652644ca8934b4133c34df933cfca
hcz <h.cz@qq.com>
2019-04-23 11:04:03 +0800

    Add test set 00937_bool_domain

---
+++ dbms/tests/queries/0_stateless/00937_bool_domain.sql
@@ -0,0 +1,15 @@
+DROP TABLE IF EXISTS test.bool_test;
+
+CREATE TABLE test.bool_test (value Bool) ENGINE = Memory;
+
+-- value column shoud have type 'Bool'
+SHOW CREATE TABLE test.bool_test;
+
+INSERT INTO test.bool_test (value) VALUES ('false'), ('true'), (0), (1);
+INSERT INTO test.bool_test (value) FORMAT JSONEachRow {"value":false}{"value":true}
+
+SELECT value FROM test.bool_test;
+SELECT value FROM test.bool_test FORMAT JSONEachRow;
+SELECT toUInt64(value) FROM test.bool_test;
+
+DROP TABLE IF EXISTS test.bool_test;
