d1b60215c4a09a22850bababdad1da9b94254389
proller <proller@github.com>
2019-04-24 20:51:00 +0300

    Packager: fixes

---
+++ docker/packager/binary/build.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+set -x -e
+
+mkdir -p build/build_docker
+cd build/build_docker
+ccache -s ||:
+cmake .. -DCMAKE_BUILD_TYPE=$BUILD_TYPE -DSANITIZE=$SANITIZER $CMAKE_FLAGS
+ninja
+ccache -s ||:
+mv ./dbms/programs/clickhouse* /output
+mv ./dbms/unit_tests_dbms /output
+mv ./dbms/programs/odbc-bridge/clickhouse-odbc-bridge /output
