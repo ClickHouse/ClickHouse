41fd77d1eda3c9af727995129f10e4b50588fed6
Vitaly Baranov <vitbar@yandex-team.ru>
2019-04-24 18:03:15 +0300

    Implement new constraints: "min" and "readonly".

--- dbms/tests/integration/test_settings_constraints/test.py
+++ dbms/tests/integration/test_settings_constraints/test.py
@@ -18,6 +18,48 @@ def started_cluster():
         cluster.shutdown()
 
 
+def test_read_only_constraint(started_cluster):
+    # Change a setting for session with SET.
+    assert instance.query("SELECT value FROM system.settings WHERE name='force_index_by_date'") ==\
+           "0\n"
+
+    expected_error = "Setting force_index_by_date should not be changed"
+    assert expected_error in instance.query_and_get_error("SET force_index_by_date=1")
+
+    # Change a setting for query with SETTINGS.
+    assert instance.query("SELECT value FROM system.settings WHERE name='force_index_by_date'") ==\
+           "0\n"
+
+    assert expected_error in instance.query_and_get_error(
+           "SELECT value FROM system.settings WHERE name='force_index_by_date' "
+           "SETTINGS force_index_by_date=1")
+
+
+def test_min_constraint(started_cluster):
+    # Change a setting for session with SET.
+    assert instance.query("SELECT value FROM system.settings WHERE name='max_memory_usage'") ==\
+           "10000000000\n"
+
+    assert instance.query("SET max_memory_usage=5000000000;\n"
+                          "SELECT value FROM system.settings WHERE name='max_memory_usage'") ==\
+           "5000000000\n"
+
+    expected_error = "Setting max_memory_usage shouldn't be less than 5000000000"
+    assert expected_error in instance.query_and_get_error("SET max_memory_usage=4999999999")
+
+    # Change a setting for query with SETTINGS.
+    assert instance.query("SELECT value FROM system.settings WHERE name='max_memory_usage'") ==\
+           "10000000000\n"
+
+    assert instance.query("SET max_memory_usage=5000000001;\n"
+                          "SELECT value FROM system.settings WHERE name='max_memory_usage'") ==\
+           "5000000001\n"
+
+    assert expected_error in instance.query_and_get_error(
+           "SELECT value FROM system.settings WHERE name='max_memory_usage' "
+           "SETTINGS max_memory_usage=4999999999")
+
+
 def test_max_constraint(started_cluster):
     # Change a setting for session with SET.
     assert instance.query("SELECT value FROM system.settings WHERE name='max_memory_usage'") ==\
@@ -30,20 +72,14 @@ def test_max_constraint(started_cluster):
     expected_error = "Setting max_memory_usage shouldn't be greater than 20000000000"
     assert expected_error in instance.query_and_get_error("SET max_memory_usage=20000000001")
 
-    assert instance.query("SET max_memory_usage=11000000000;\n"
-                          "SELECT value FROM system.settings WHERE name='max_memory_usage'") ==\
-           "11000000000\n"
-
-    # Change a setting for query with SETTINGS.
+     # Change a setting for query with SETTINGS.
     assert instance.query("SELECT value FROM system.settings WHERE name='max_memory_usage'") ==\
            "10000000000\n"
 
     assert instance.query("SELECT value FROM system.settings WHERE name='max_memory_usage' "
-                          "SETTINGS max_memory_usage=20000000000") == "20000000000\n"
+                          "SETTINGS max_memory_usage=19999999999") == "19999999999\n"
 
     assert expected_error in instance.query_and_get_error(
            "SELECT value FROM system.settings WHERE name='max_memory_usage' "
            "SETTINGS max_memory_usage=20000000001")
-
-    assert instance.query("SELECT value FROM system.settings WHERE name='max_memory_usage' "
-                          "SETTINGS max_memory_usage=12000000000") == "12000000000\n"
+ 
