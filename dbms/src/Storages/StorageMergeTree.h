33201fa9a80dbd6f90a207bb1aa66d25fe110037
ivan-kush <ivan.v.kush@yandex.ru>
2019-04-25 21:57:43 +0300

    rev

--- dbms/src/Storages/StorageMergeTree.h
+++ dbms/src/Storages/StorageMergeTree.h
@@ -133,7 +133,7 @@ private:
     BackgroundProcessingPool::TaskHandle background_task_handle;
 
     std::vector<MergeTreeData::AlterDataPartTransactionPtr> prepare_alter_transactions(
-        const ColumnsDescription& new_columns, const IndicesDescription& new_indices, const size_t thread_pool_size);
+        const ColumnsDescription& new_columns, const IndicesDescription& new_indices, const Context & context);
 
     void loadMutations();
 
