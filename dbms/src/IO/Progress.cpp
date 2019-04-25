ced71ac9fd420b5f3a51f5441d5c01d39fe2c5e1
Guillaume Tassery <tassery.guillaume@gmail.com>
2019-04-25 18:25:49 +0700

    Send how many rows and bytes were written during the query

--- dbms/src/IO/Progress.cpp
+++ dbms/src/IO/Progress.cpp
@@ -14,15 +14,21 @@ void ProgressValues::read(ReadBuffer & in, UInt64 /*server_revision*/)
     size_t new_rows = 0;
     size_t new_bytes = 0;
     size_t new_total_rows = 0;
-
+    size_t new_write_rows = 0;
+    size_t new_write_bytes = 0;
+ 
     readVarUInt(new_rows, in);
     readVarUInt(new_bytes, in);
     readVarUInt(new_total_rows, in);
-
+    readVarUInt(new_write_rows, in);
+    readVarUInt(new_write_bytes, in);
+ 
     rows = new_rows;
     bytes = new_bytes;
     total_rows = new_total_rows;
-}
+    write_rows = new_write_rows;
+    write_bytes = new_write_bytes;
+} 
 
 
 void ProgressValues::write(WriteBuffer & out, UInt64 /*client_revision*/) const
@@ -30,6 +36,8 @@ void ProgressValues::write(WriteBuffer & out, UInt64 /*client_revision*/) const
     writeVarUInt(rows, out);
     writeVarUInt(bytes, out);
     writeVarUInt(total_rows, out);
+    writeVarUInt(write_rows, out);
+    writeVarUInt(write_bytes, out);
 }
 
 
@@ -42,6 +50,10 @@ void ProgressValues::writeJSON(WriteBuffer & out) const
     writeText(rows, out);
     writeCString("\",\"read_bytes\":\"", out);
     writeText(bytes, out);
+    writeCString("\",\"write_rows\":\"", out);
+    writeText(write_rows, out);
+    writeCString("\",\"write_bytes\":\"", out);
+    writeText(write_bytes, out);
     writeCString("\",\"total_rows\":\"", out);
     writeText(total_rows, out);
     writeCString("\"}", out);
@@ -56,6 +68,8 @@ void Progress::read(ReadBuffer & in, UInt64 server_revision)
     rows.store(values.rows, std::memory_order_relaxed);
     bytes.store(values.bytes, std::memory_order_relaxed);
     total_rows.store(values.total_rows, std::memory_order_relaxed);
+    write_rows.store(values.write_rows, std::memory_order_relaxed);
+    write_bytes.store(values.write_bytes, std::memory_order_relaxed);
 }
 
 
--- dbms/src/IO/Progress.h
+++ dbms/src/IO/Progress.h
@@ -20,12 +20,32 @@ struct ProgressValues
     size_t rows;
     size_t bytes;
     size_t total_rows;
+    size_t write_rows;
+    size_t write_bytes;   
 
     void read(ReadBuffer & in, UInt64 server_revision);
     void write(WriteBuffer & out, UInt64 client_revision) const;
     void writeJSON(WriteBuffer & out) const;
 };
 
+struct ReadProgress
+{
+    size_t rows;
+    size_t bytes;
+    size_t total_rows;
+
+    ReadProgress(size_t rows_, size_t bytes_, size_t total_rows_ = 0)
+        : rows(rows_), bytes(bytes_), total_rows(total_rows_) {}
+};
+
+struct WriteProgress
+{
+    size_t write_rows;
+    size_t write_bytes;
+
+    WriteProgress(size_t write_rows_, size_t write_bytes_)
+        : write_rows(write_rows_), write_bytes(write_bytes_) {}
+};
 
 /** Progress of query execution.
   * Values, transferred over network are deltas - how much was done after previously sent value.
@@ -42,9 +62,17 @@ struct Progress
       */
     std::atomic<size_t> total_rows {0};
 
+
+    std::atomic<size_t> write_rows {0};
+    std::atomic<size_t> write_bytes {0};
+
     Progress() {}
     Progress(size_t rows_, size_t bytes_, size_t total_rows_ = 0)
         : rows(rows_), bytes(bytes_), total_rows(total_rows_) {}
+    Progress(ReadProgress read_progress)
+        : rows(read_progress.rows), bytes(read_progress.bytes), total_rows(read_progress.total_rows) {}
+    Progress(WriteProgress write_progress)
+        : write_rows(write_progress.write_rows), write_bytes(write_progress.write_bytes)  {}
 
     void read(ReadBuffer & in, UInt64 server_revision);
     void write(WriteBuffer & out, UInt64 client_revision) const;
@@ -58,8 +86,10 @@ struct Progress
         rows += rhs.rows;
         bytes += rhs.bytes;
         total_rows += rhs.total_rows;
+        write_rows += rhs.write_rows;
+        write_bytes += rhs.write_bytes;
 
-        return rhs.rows ? true : false;
+        return rhs.rows || rhs.write_rows ? true : false;
     }
 
     void reset()
@@ -67,6 +97,8 @@ struct Progress
         rows = 0;
         bytes = 0;
         total_rows = 0;
+        write_rows = 0;
+        write_bytes = 0;
     }
 
     ProgressValues getValues() const
@@ -76,6 +108,8 @@ struct Progress
         res.rows = rows.load(std::memory_order_relaxed);
         res.bytes = bytes.load(std::memory_order_relaxed);
         res.total_rows = total_rows.load(std::memory_order_relaxed);
+        res.write_rows = write_rows.load(std::memory_order_relaxed);
+        res.write_bytes = write_bytes.load(std::memory_order_relaxed);
 
         return res;
     }
@@ -87,7 +121,9 @@ struct Progress
         res.rows = rows.fetch_and(0);
         res.bytes = bytes.fetch_and(0);
         res.total_rows = total_rows.fetch_and(0);
-
+        res.write_rows = write_rows.fetch_and(0);
+        res.write_bytes = write_bytes.fetch_and(0);
+ 
         return res;
     }
 
@@ -96,7 +132,9 @@ struct Progress
         rows = other.rows.load(std::memory_order_relaxed);
         bytes = other.bytes.load(std::memory_order_relaxed);
         total_rows = other.total_rows.load(std::memory_order_relaxed);
-
+        write_rows = other.write_rows.load(std::memory_order_relaxed);
+        write_bytes = other.write_bytes.load(std::memory_order_relaxed);
+ 
         return *this;
     }
 
