661c840fbe5333f3622dc9095c9b7aef448e41fd
Alexey Milovidov <milovidov@yandex-team.ru>
2019-04-25 04:16:26 +0300

    Code cleanups after #4439

--- dbms/src/Columns/ColumnVector.cpp
+++ dbms/src/Columns/ColumnVector.cpp
@@ -68,11 +68,32 @@ struct ColumnVector<T>::greater
     bool operator()(size_t lhs, size_t rhs) const { return CompareHelper<T>::greater(parent.data[lhs], parent.data[rhs], nan_direction_hint); }
 };
 
+
+namespace
+{
+    template <typename T>
+    struct ValueWithIndex
+    {
+        T value;
+        UInt32 index;
+    };
+
+    template <typename T>
+    struct RadixSortTraits : RadixSortNumTraits<T>
+    {
+        using Element = ValueWithIndex<T>;
+        static T & extractKey(Element & elem) { return elem.value; }
+    };
+}
+
 template <typename T>
 void ColumnVector<T>::getPermutation(bool reverse, size_t limit, int nan_direction_hint, IColumn::Permutation & res) const
 {
     size_t s = data.size();
     res.resize(s);
+
+    if (s == 0)
+        return;
 
     if (limit >= s)
         limit = 0;
@@ -89,33 +110,78 @@ void ColumnVector<T>::getPermutation(bool reverse, size_t limit, int nan_directi
     }
     else
     {
-        if constexpr ((std::is_signed_v<T> || std::is_unsigned_v<T>) && !std::is_same_v<T, UInt128>)
+        /// A case for radix sort
+        if constexpr (std::is_arithmetic_v<T> && !std::is_same_v<T, UInt128>)
         {
-            PaddedPODArray<std::pair<T, size_t>> pairs(s);
-            for (size_t i = 0; i < s; ++i)
-                pairs[i] = {data[i], i};
+            /// Thresholds on size. Lower threshold is arbitrary. Upper threshold is chosen by the type for histogram counters.
+            if (s >= 256 && s <= std::numeric_limits<UInt32>::max())
+            {
+                PaddedPODArray<ValueWithIndex<T>> pairs(s);
+                for (UInt32 i = 0; i < s; ++i)
+                    pairs[i] = {data[i], i};
 
-            radixSort(pairs.data(), s, nan_direction_hint);
+                RadixSort<RadixSortTraits<T>>::execute(pairs.data(), s);
 
-            if (reverse)
-                for (size_t i = 0; i < s; ++i)
-                    res[s - 1 - i] = pairs[i].second;
-            else
-                for (size_t i = 0; i < s; ++i)
-                    res[i] = pairs[i].second;
+                /// Radix sort treats all NaNs to be greater than all numbers.
+                /// If the user needs the opposite, we must move them accordingly.
+                size_t nans_to_move = 0;
+                if (std::is_floating_point_v<T> && nan_direction_hint < 0)
+                {
+                    for (ssize_t i = s - 1; i >= 0; --i)
+                    {
+                        if (isNaN(pairs[i].value))
+                            ++nans_to_move;
+                        else
+                            break;
+                    }
+                }
+
+                if (reverse)
+                {
+                    if (nans_to_move)
+                    {
+                        for (size_t i = 0; i < s - nans_to_move; ++i)
+                            res[i] = pairs[s - nans_to_move - 1 - i].index;
+                        for (size_t i = s - nans_to_move; i < s; ++i)
+                            res[i] = pairs[s - 1 - (i - (s - nans_to_move))].index;
+                    }
+                    else
+                    {
+                        for (size_t i = 0; i < s; ++i)
+                            res[s - 1 - i] = pairs[i].index;
+                    }
+                }
+                else
+                {
+                    if (nans_to_move)
+                    {
+                        for (size_t i = 0; i < nans_to_move; ++i)
+                            res[i] = pairs[i + s - nans_to_move].index;
+                        for (size_t i = nans_to_move; i < s; ++i)
+                            res[i] = pairs[i - nans_to_move].index;
+                    }
+                    else
+                    {
+                        for (size_t i = 0; i < s; ++i)
+                            res[i] = pairs[i].index;
+                    }
+                }
+
+                return;
+            }
         }
+
+        /// Default sorting algorithm.
+        for (size_t i = 0; i < s; ++i)
+            res[i] = i;
+
+        if (reverse)
+            pdqsort(res.begin(), res.end(), greater(*this, nan_direction_hint));
         else
-        {
-            for (size_t i = 0; i < s; ++i)
-                res[i] = i;
-
-            if (reverse)
-                pdqsort(res.begin(), res.end(), greater(*this, nan_direction_hint));
-            else
-                pdqsort(res.begin(), res.end(), less(*this, nan_direction_hint));
-        }
+            pdqsort(res.begin(), res.end(), less(*this, nan_direction_hint));
     }
 }
+
 
 template <typename T>
 const char * ColumnVector<T>::getFamilyName() const
