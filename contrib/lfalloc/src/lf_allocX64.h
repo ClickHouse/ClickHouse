852ea727fb23b80e708b3cbf61c0fba6e757e733
Danila Kutenin <kutdanila@yandex.ru>
2019-04-28 13:09:01 +0300

    Exponentially small

--- contrib/lfalloc/src/lf_allocX64.h
+++ contrib/lfalloc/src/lf_allocX64.h
@@ -360,13 +360,13 @@ static char* AllocWithMMap(uintptr_t sz, EMMapMode mode) {
     static thread_local std::mt19937_64 generator(std::random_device{}());
     std::uniform_int_distribution<intptr_t> distr(areaStart, areaFinish - sz);
     char* largeBlock;
-    static constexpr size_t maxAttempts = 10;
+    static constexpr size_t MaxAttempts = 100;
     size_t attempt = 0;
     do
     {
         largeBlock = (char*)mmap(reinterpret_cast<void*>(distr(generator)), sz, PROT_READ | PROT_WRITE, MAP_PRIVATE | MAP_ANON, -1, 0);
         ++attempt;
-    } while (uintptr_t(((char*)largeBlock - ALLOC_START) + sz) >= areaFinish && attempt < maxAttempts && munmap(largeBlock, sz) == 0);
+    } while (uintptr_t(((char*)largeBlock - ALLOC_START) + sz) >= areaFinish && attempt < MaxAttempts && munmap(largeBlock, sz) == 0);
 #else
     char* largeBlock = (char*)mmap(0, sz, PROT_READ | PROT_WRITE, MAP_PRIVATE | MAP_ANON, -1, 0);
 #endif
