6a3966cd08018a8da6d538fa2388dbd06f0c507a
Nikolai Kochetov <KochetovNicolai@users.noreply.github.com>
2019-04-30 17:46:17 +0300

    Update PreparedSets.h

--- dbms/src/Interpreters/PreparedSets.h
+++ dbms/src/Interpreters/PreparedSets.h
@@ -16,6 +16,8 @@ struct PreparedSetKey
     /// if left hand sides of the IN operators have different types).
     static PreparedSetKey forLiteral(const IAST & ast, DataTypes types_)
     {
+        /// Remove LowCardinality types from type list because Set doesn't support LowCardinality keys now, 
+        ///   just converts LowCardinality to ordinary types.
         for (auto & type : types_)
             type = recursiveRemoveLowCardinality(type);
 
