e0bd16c1f4e60d7c62d6e7e6044da08adc4fd2fb
Guillaume Tassery <tassery.guillaume@gmail.com>
2019-05-06 13:14:37 +0700

    Backward compatibility for write progress with old version of server and client

--- dbms/src/IO/Progress.h
+++ dbms/src/IO/Progress.h
@@ -23,34 +23,28 @@ struct ProgressValues
     size_t write_rows;
     size_t write_bytes;   
 
+
     template <typename ReadImpl = AllProgressValueImpl>
-    void read(ReadBuffer & in, UInt64 server_revision)
-    {
-        ReadImpl::read(*this, in, server_revision);
-    }
+    inline void read(ReadBuffer & in, UInt64 server_revision);
 
     template <typename WriteImpl = AllProgressValueImpl>
-    void write(WriteBuffer & out, UInt64 client_revision) const
-    {
-        WriteImpl::write(*this, out, client_revision);
-    }
+    inline void write(WriteBuffer & out, UInt64 client_revision) const;
 
     template <typename WriteJSONImpl = AllProgressValueImpl>
-    void writeJSON(WriteBuffer & out) const
-    {
-        WriteJSONImpl::writeJSON(*this, out);
-    }
+    inline void writeJSON(WriteBuffer & out) const;
 };
 
 struct AllProgressValueImpl
 {
     static void read(ProgressValues & value, ReadBuffer & in, UInt64 server_revision);
-    static void write(const ProgressValues & value, WriteBuffer & out, UInt64 client_revision) ;
+    static void write(const ProgressValues & value, WriteBuffer & out, UInt64 client_revision);
     static void writeJSON(const ProgressValues & value, WriteBuffer & out);
 };
 
 struct ReadProgressValueImpl : public AllProgressValueImpl
 {
+    static void read(ProgressValues & value, ReadBuffer & in, UInt64 server_revision);
+    static void write(const ProgressValues & value, WriteBuffer & out, UInt64 client_revision);
     static void writeJSON(const ProgressValues & value, WriteBuffer & out);
 };
 
@@ -198,12 +192,46 @@ void Progress::write(WriteBuffer & out, UInt64 client_revision) const
     getValues().write<T>(out, client_revision);
 }
 
-
 template <typename T>
 void Progress::writeJSON(WriteBuffer & out) const
 {
     getValues().writeJSON<T>(out);
 }
 
+template <typename ReadImpl>
+inline void ProgressValues::read(ReadBuffer & in, UInt64 server_revision)
+{
+    if (server_revision >= DBMS_MIN_REVISION_WITH_CLIENT_WRITE_INFO)
+        ReadImpl::read(*this, in, server_revision);
+    else
+        read<ReadProgressValueImpl>(in, server_revision);
+}
+
+template <>
+inline void ProgressValues::read<ReadProgressValueImpl>(ReadBuffer & in, UInt64 server_revision)
+{
+    ReadProgressValueImpl::read(*this, in, server_revision);
+}
+
+template <typename WriteImpl>
+inline void ProgressValues::write(WriteBuffer & out, UInt64 client_revision) const
+{
+    if (client_revision >= DBMS_MIN_REVISION_WITH_CLIENT_WRITE_INFO)
+        WriteImpl::write(*this, out, client_revision);
+    else
+        write<ReadProgressValueImpl>(out, client_revision);
+}
+
+template <>
+inline void ProgressValues::write<ReadProgressValueImpl>(WriteBuffer & out, UInt64 client_revision) const
+{
+    ReadProgressValueImpl::write(*this, out, client_revision);
+}
+
+template <typename WriteJSONImpl>
+void ProgressValues::writeJSON(WriteBuffer & out) const
+{
+    WriteJSONImpl::writeJSON(*this, out);
+}
 
 }
