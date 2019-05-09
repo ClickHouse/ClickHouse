40e9bfe8895d2245ba85f7ec3317f869d80283cf
alexey-milovidov <milovidov@yandex-team.ru>
2019-05-09 23:06:57 +0300

    Merge pull request #5228 from andyyzh/fix_bitmap_issue_5220
    Fix bitmap functions crash the server bug segfault
    
    (cherry picked from commit 9f5e1a5b806e6b13d13b425477f3320325f0ab8e)

--- dbms/tests/queries/0_stateless/00829_bitmap_function.sql
+++ dbms/tests/queries/0_stateless/00829_bitmap_function.sql
@@ -9,15 +9,15 @@ SELECT bitmapOrCardinality(bitmapBuild([1,2,3]),bitmapBuild([3,4,5]));
 SELECT bitmapXorCardinality(bitmapBuild([1,2,3]),bitmapBuild([3,4,5]));
 SELECT bitmapAndnotCardinality(bitmapBuild([1,2,3]),bitmapBuild([3,4,5]));
 
-DROP TABLE IF EXISTS test.bitmap_test;
-CREATE TABLE test.bitmap_test(pickup_date Date, city_id UInt32, uid UInt32)ENGINE = Memory;
-INSERT INTO test.bitmap_test SELECT '2019-01-01', 1, number FROM numbers(1,50);
-INSERT INTO test.bitmap_test SELECT '2019-01-02', 1, number FROM numbers(11,60);
+DROP TABLE IF EXISTS bitmap_test;
+CREATE TABLE bitmap_test(pickup_date Date, city_id UInt32, uid UInt32)ENGINE = Memory;
+INSERT INTO bitmap_test SELECT '2019-01-01', 1, number FROM numbers(1,50);
+INSERT INTO bitmap_test SELECT '2019-01-02', 1, number FROM numbers(11,60);
 
 
-SELECT groupBitmap( uid ) AS user_num FROM test.bitmap_test;
+SELECT groupBitmap( uid ) AS user_num FROM bitmap_test;
 
-SELECT pickup_date, groupBitmap( uid ) AS user_num, bitmapToArray(groupBitmapState( uid )) AS users FROM test.bitmap_test GROUP BY pickup_date;
+SELECT pickup_date, groupBitmap( uid ) AS user_num, bitmapToArray(groupBitmapState( uid )) AS users FROM bitmap_test GROUP BY pickup_date;
 
 SELECT
     bitmapCardinality(day_today) AS today_users,
@@ -28,11 +28,11 @@ SELECT
     bitmapXorCardinality(day_today, day_before) AS diff_users
 FROM
 (
- SELECT city_id, groupBitmapState( uid ) AS day_today FROM test.bitmap_test WHERE pickup_date = '2019-01-02' GROUP BY city_id
+ SELECT city_id, groupBitmapState( uid ) AS day_today FROM bitmap_test WHERE pickup_date = '2019-01-02' GROUP BY city_id
  )
 ALL LEFT JOIN
 (
- SELECT city_id, groupBitmapState( uid ) AS day_before FROM test.bitmap_test WHERE pickup_date = '2019-01-01' GROUP BY city_id
+ SELECT city_id, groupBitmapState( uid ) AS day_before FROM bitmap_test WHERE pickup_date = '2019-01-01' GROUP BY city_id
 )
 USING city_id;
 
@@ -45,17 +45,17 @@ SELECT
     bitmapCardinality(bitmapXor(day_today, day_before)) AS diff_users
 FROM
 (
- SELECT city_id, groupBitmapState( uid ) AS day_today FROM test.bitmap_test WHERE pickup_date = '2019-01-02' GROUP BY city_id
+ SELECT city_id, groupBitmapState( uid ) AS day_today FROM bitmap_test WHERE pickup_date = '2019-01-02' GROUP BY city_id
  )
 ALL LEFT JOIN
 (
- SELECT city_id, groupBitmapState( uid ) AS day_before FROM test.bitmap_test WHERE pickup_date = '2019-01-01' GROUP BY city_id
+ SELECT city_id, groupBitmapState( uid ) AS day_before FROM bitmap_test WHERE pickup_date = '2019-01-01' GROUP BY city_id
 )
 USING city_id;
 
-
-DROP TABLE IF EXISTS test.bitmap_state_test;
-CREATE TABLE test.bitmap_state_test
+-- bitmap state test
+DROP TABLE IF EXISTS bitmap_state_test;
+CREATE TABLE bitmap_state_test
 (
 	pickup_date Date,
 	city_id UInt32,
@@ -63,15 +63,34 @@ CREATE TABLE test.bitmap_state_test
 )
 ENGINE = AggregatingMergeTree( pickup_date, ( pickup_date, city_id ), 8192);
 
-INSERT INTO test.bitmap_state_test SELECT 
+INSERT INTO bitmap_state_test SELECT 
     pickup_date, 
     city_id,
     groupBitmapState(uid) AS uv
-FROM test.bitmap_test
+FROM bitmap_test
 GROUP BY pickup_date, city_id;
 	
-SELECT pickup_date, groupBitmapMerge(uv) AS users from test.bitmap_state_test group by pickup_date;
+SELECT pickup_date, groupBitmapMerge(uv) AS users from bitmap_state_test group by pickup_date;
 
-DROP TABLE IF EXISTS test.bitmap_test;
-DROP TABLE IF EXISTS test.bitmap_state_test;
+-- between column and expression test
+DROP TABLE IF EXISTS bitmap_column_expr_test;
+CREATE TABLE bitmap_column_expr_test
+(
+    t DateTime,
+    z AggregateFunction(groupBitmap, UInt32)
+)
+ENGINE = MergeTree
+PARTITION BY toYYYYMMDD(t)
+ORDER BY t;
 
+INSERT INTO bitmap_column_expr_test VALUES (now(), bitmapBuild(cast([3,19,47] as Array(UInt32))));
+
+SELECT bitmapAndCardinality( bitmapBuild(cast([19,7] as Array(UInt32))), z) from bitmap_column_expr_test;
+SELECT bitmapAndCardinality( z, bitmapBuild(cast([19,7] as Array(UInt32))) ) from bitmap_column_expr_test;
+
+select bitmapCardinality(bitmapAnd(bitmapBuild(cast([19,7] as Array(UInt32))), z )) from bitmap_column_expr_test;
+select bitmapCardinality(bitmapAnd(z, bitmapBuild(cast([19,7] as Array(UInt32))))) from bitmap_column_expr_test;
+
+DROP TABLE IF EXISTS bitmap_test;
+DROP TABLE IF EXISTS bitmap_state_test;
+DROP TABLE IF EXISTS bitmap_column_expr_test;
