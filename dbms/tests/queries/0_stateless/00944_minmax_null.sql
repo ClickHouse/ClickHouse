d4e154a221d932f52d5bf0832a441355ffa9de3a
Nikita Vasilev <nekitvas@gmail.com>
2019-05-12 20:08:28 +0300

    mayBeTrueOnGranule

--- dbms/tests/queries/0_stateless/00944_minmax_null.sql
+++ dbms/tests/queries/0_stateless/00944_minmax_null.sql
@@ -9,16 +9,16 @@ CREATE TABLE min_max_with_nullable_string (
 
 INSERT INTO min_max_with_nullable_string(t) VALUES (now()) (now());
 
-SELECT count() FROM min_max_with_nullable_string WHERE nullable_str == '.';
+SELECT count() FROM min_max_with_nullable_string WHERE nullable_str = '.';
 
 INSERT INTO min_max_with_nullable_string(t, nullable_str) VALUES (now(), '.') (now(), '.');
 
-SELECT count() FROM min_max_with_nullable_string WHERE nullable_str == '.';
+SELECT count() FROM min_max_with_nullable_string WHERE nullable_str = '.';
 
 INSERT INTO min_max_with_nullable_string(t, nullable_str) VALUES (now(), NULL) (now(), '.') (now(), NULL) (now(), '.') (now(), NULL);
 
-SELECT count() FROM min_max_with_nullable_string WHERE nullable_str == '.';
+SELECT count() FROM min_max_with_nullable_string WHERE nullable_str = '.';
 
-SELECT count() FROM min_max_with_nullable_string WHERE nullable_str == '';
+SELECT count() FROM min_max_with_nullable_string WHERE nullable_str = '';
 
 DROP TABLE min_max_with_nullable_string;
