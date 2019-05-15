b419827965cd73fe97f8df535428159c4bb6c8e7
proller <proller@github.com>
2019-05-15 19:27:44 +0300

    clang-format of AggregateFunctions/AggregateFunctionTSGroupSum.*

--- dbms/src/AggregateFunctions/AggregateFunctionTSGroupSum.cpp
+++ dbms/src/AggregateFunctions/AggregateFunctionTSGroupSum.cpp
@@ -1,31 +1,30 @@
-#include <AggregateFunctions/AggregateFunctionFactory.h>
-#include <AggregateFunctions/AggregateFunctionTSGroupSum.h>
-#include <AggregateFunctions/Helpers.h>
-#include <AggregateFunctions/FactoryHelpers.h>
+#include "AggregateFunctionTSGroupSum.h"
+#include "AggregateFunctionFactory.h"
+#include "FactoryHelpers.h"
+#include "Helpers.h"
 
 
 namespace DB
 {
-
-    namespace
+namespace
+{
+    template <bool rate>
+    AggregateFunctionPtr createAggregateFunctionTSgroupSum(const std::string & name, const DataTypes & arguments, const Array & params)
     {
-        template<bool rate>
-        AggregateFunctionPtr createAggregateFunctionTSgroupSum(const std::string & name, const DataTypes & arguments, const Array & params)
-        {
-            assertNoParameters(name, params);
+        assertNoParameters(name, params);
 
-            if (arguments.size() < 3)
-                throw Exception("Not enough event arguments for aggregate function " + name, ErrorCodes::NUMBER_OF_ARGUMENTS_DOESNT_MATCH);
+        if (arguments.size() < 3)
+            throw Exception("Not enough event arguments for aggregate function " + name, ErrorCodes::NUMBER_OF_ARGUMENTS_DOESNT_MATCH);
 
-            return std::make_shared<AggregateFunctionTSgroupSum<rate>>(arguments);
-        }
-
+        return std::make_shared<AggregateFunctionTSgroupSum<rate>>(arguments);
     }
 
-    void registerAggregateFunctionTSgroupSum(AggregateFunctionFactory & factory)
-    {
-        factory.registerFunction("TSgroupSum", createAggregateFunctionTSgroupSum<false>, AggregateFunctionFactory::CaseInsensitive);
-        factory.registerFunction("TSgroupRateSum", createAggregateFunctionTSgroupSum<true>, AggregateFunctionFactory::CaseInsensitive);
-    }
+}
+
+void registerAggregateFunctionTSgroupSum(AggregateFunctionFactory & factory)
+{
+    factory.registerFunction("TSgroupSum", createAggregateFunctionTSgroupSum<false>, AggregateFunctionFactory::CaseInsensitive);
+    factory.registerFunction("TSgroupRateSum", createAggregateFunctionTSgroupSum<true>, AggregateFunctionFactory::CaseInsensitive);
+}
 
 }
