0760a3436f52e0069e2d7b8cef19e2ce0dbbeb9e
Yangkuan Liu <liuyangkuan@gmail.com>
2019-05-16 00:16:25 +0800

    add AggregateFunction TSgroup{Rate}Sum (#4542)

---
+++ dbms/src/AggregateFunctions/AggregateFunctionTSGroupSum.cpp
@@ -0,0 +1,31 @@
+#include <AggregateFunctions/AggregateFunctionFactory.h>
+#include <AggregateFunctions/AggregateFunctionTSGroupSum.h>
+#include <AggregateFunctions/Helpers.h>
+#include <AggregateFunctions/FactoryHelpers.h>
+
+
+namespace DB
+{
+
+    namespace
+    {
+        template<bool rate>
+        AggregateFunctionPtr createAggregateFunctionTSgroupSum(const std::string & name, const DataTypes & arguments, const Array & params)
+        {
+            assertNoParameters(name, params);
+
+            if (arguments.size() < 3)
+                throw Exception("Not enough event arguments for aggregate function " + name, ErrorCodes::NUMBER_OF_ARGUMENTS_DOESNT_MATCH);
+
+            return std::make_shared<AggregateFunctionTSgroupSum<rate>>(arguments);
+        }
+
+    }
+
+    void registerAggregateFunctionTSgroupSum(AggregateFunctionFactory & factory)
+    {
+        factory.registerFunction("TSgroupSum", createAggregateFunctionTSgroupSum<false>, AggregateFunctionFactory::CaseInsensitive);
+        factory.registerFunction("TSgroupRateSum", createAggregateFunctionTSgroupSum<true>, AggregateFunctionFactory::CaseInsensitive);
+    }
+
+}
