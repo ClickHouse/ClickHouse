18229c60eb411af5ecc2ed9eafa6c41d7d4a3728
Yuriy <yurrio@ya.ru>
2019-05-16 06:34:04 +0300

    added MySQL wire protocol presentational format

--- dbms/src/Interpreters/Context.h
+++ dbms/src/Interpreters/Context.h
@@ -473,6 +473,9 @@ public:
     IHostContextPtr & getHostContext();
     const IHostContextPtr & getHostContext() const;
 
+    /// MySQL wire protocol state.
+    size_t sequence_id = 0;
+    uint32_t client_capabilities = 0;
 private:
     /** Check if the current client has access to the specified database.
       * If access is denied, throw an exception.
