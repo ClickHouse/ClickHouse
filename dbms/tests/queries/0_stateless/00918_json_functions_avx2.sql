d59bd8022a65630428dc7b910d767a632c74d053
Vitaly Baranov <vitbar@yandex-team.ru>
2019-05-16 16:35:31 +0300

    Add tests cases and fix documentation for JSON functions.

--- dbms/tests/queries/0_stateless/00918_json_functions_avx2.sql
+++ dbms/tests/queries/0_stateless/00918_json_functions_avx2.sql
@@ -18,13 +18,19 @@ SELECT '--JSONType--';
 SELECT JSONType('{"a": "hello", "b": [-100, 200.0, 300]}');
 SELECT JSONType('{"a": "hello", "b": [-100, 200.0, 300]}', 'b');
 
-SELECT '--JSONExtract<type>--';
-SELECT JSONExtractString('{"a": "hello", "b": [-100, 200.0, 300]}', 'a');
-SELECT JSONExtractString('{"a": "hello", "b": [-100, 200.0, 300]}', 1);
+SELECT '--JSONExtract<numeric>--';
 SELECT JSONExtractInt('{"a": "hello", "b": [-100, 200.0, 300]}', 'b', 1);
 SELECT JSONExtractFloat('{"a": "hello", "b": [-100, 200.0, 300]}', 'b', 2);
 SELECT JSONExtractUInt('{"a": "hello", "b": [-100, 200.0, 300]}', 'b', -1);
 SELECT JSONExtractBool('{"passed": true}', 'passed');
+
+SELECT '--JSONExtractString--';
+SELECT JSONExtractString('{"a": "hello", "b": [-100, 200.0, 300]}', 'a');
+SELECT JSONExtractString('{"a": "hello", "b": [-100, 200.0, 300]}', 1);
+select JSONExtractString('{"abc":"\\n\\u0000"}', 'abc');
+select JSONExtractString('{"abc":"\\u263a"}', 'abc');
+select JSONExtractString('{"abc":"\\u263"}', 'abc');
+select JSONExtractString('{"abc":"hello}', 'abc');
 
 SELECT '--JSONExtract (generic)--';
 SELECT JSONExtract('{"a": "hello", "b": [-100, 200.0, 300]}', 'Tuple(String, Array(Float64))');
@@ -41,8 +47,15 @@ SELECT JSONExtract('{"a": "hello", "b": [-100, 200.0, 300]}', 'b', 'Array(Nullab
 SELECT JSONExtract('{"a": "hello", "b": [-100, 200.0, 300]}', 'b', 1, 'Int8');
 SELECT JSONExtract('{"a": "hello", "b": [-100, 200.0, 300]}', 'b', 2, 'Int32');
 SELECT JSONExtract('{"a": "hello", "b": [-100, 200.0, 300]}', 'b', 4, 'Nullable(Int64)');
+SELECT JSONExtract('{"passed": true}', 'passed', 'UInt8');
 SELECT JSONExtract('{"day": "Thursday"}', 'day', 'Enum8(\'Sunday\' = 0, \'Monday\' = 1, \'Tuesday\' = 2, \'Wednesday\' = 3, \'Thursday\' = 4, \'Friday\' = 5, \'Saturday\' = 6)');
 SELECT JSONExtract('{"day": 5}', 'day', 'Enum8(\'Sunday\' = 0, \'Monday\' = 1, \'Tuesday\' = 2, \'Wednesday\' = 3, \'Thursday\' = 4, \'Friday\' = 5, \'Saturday\' = 6)');
+
+SELECT '--JSONExtractKeysAndValues--';
+SELECT JSONExtractKeysAndValues('{"a": "hello", "b": [-100, 200.0, 300]}', 'String');
+SELECT JSONExtractKeysAndValues('{"a": "hello", "b": [-100, 200.0, 300]}', 'Array(Float64)');
+SELECT JSONExtractKeysAndValues('{"a": "hello", "b": "world"}', 'String');
+SELECT JSONExtractKeysAndValues('{"x": {"a": 5, "b": 7, "c": 11}}', 'x', 'Int8');
 
 SELECT '--JSONExtractRaw--';
 SELECT JSONExtractRaw('{"a": "hello", "b": [-100, 200.0, 300]}');
@@ -56,9 +69,5 @@ SELECT JSONExtractRaw('{"a": "hello", "b": [-100, 200.0, 300], "c":{"d":[121,144
 SELECT JSONExtractRaw('{"a": "hello", "b": [-100, 200.0, 300], "c":{"d":[121,144]}}', 'c', 'd', 3);
 SELECT JSONExtractRaw('{"passed": true}');
 SELECT JSONExtractRaw('{}');
-
-SELECT '--JSONExtractKeysAndValues--';
-SELECT JSONExtractKeysAndValues('{"a": "hello", "b": [-100, 200.0, 300]}', 'String');
-SELECT JSONExtractKeysAndValues('{"a": "hello", "b": [-100, 200.0, 300]}', 'Array(Float64)');
-SELECT JSONExtractKeysAndValues('{"a": "hello", "b": "world"}', 'String');
-SELECT JSONExtractKeysAndValues('{"x": {"a": 5, "b": 7, "c": 11}}', 'x', 'Int8');
+select JSONExtractRaw('{"abc":"\\n\\u0000"}', 'abc');
+select JSONExtractRaw('{"abc":"\\u263a"}', 'abc');
