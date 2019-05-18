37d6568cbfe162819efa7ac770dc3070c3a67f09
Danila Kutenin <kutdanila@yandex.ru>
2019-05-18 15:52:33 +0300

    some optimizations

--- dbms/src/Functions/formatString.cpp
+++ dbms/src/Functions/formatString.cpp
@@ -79,6 +79,8 @@ public:
         std::vector<const ColumnString::Chars *> data(arguments.size() - 1);
         std::vector<const ColumnString::Offsets *> offsets(arguments.size() - 1);
         std::vector<size_t> fixed_string_N(arguments.size() - 1);
+        bool has_column_string = false;
+        bool has_column_fixed_string = false;
         for (size_t i = 1; i < arguments.size(); ++i)
         {
             const ColumnPtr column = block.getByPosition(arguments[i]).column;
@@ -97,8 +99,14 @@ public:
                     "Illegal column " + column->getName() + " of argument of function " + getName(),
                     ErrorCodes::ILLEGAL_COLUMN);
         }
-        Impl::vector(std::move(pattern), data, offsets, fixed_string_N, col_res->getChars(), col_res->getOffsets(), input_rows_count);
-
+        if (has_column_string && has_column_fixed_string)
+            Impl::template vector<true, true>(std::move(pattern), data, offsets, fixed_string_N, col_res->getChars(), col_res->getOffsets(), input_rows_count);
+        else if (!has_column_string && has_column_fixed_string)
+            Impl::template vector<false, true>(std::move(pattern), data, offsets, fixed_string_N, col_res->getChars(), col_res->getOffsets(), input_rows_count);
+        else if (has_column_string && !has_column_fixed_string)
+            Impl::template vector<true, false>(std::move(pattern), data, offsets, fixed_string_N, col_res->getChars(), col_res->getOffsets(), input_rows_count);
+        else
+            Impl::template vector<true, true>(std::move(pattern), data, offsets, fixed_string_N, col_res->getChars(), col_res->getOffsets(), input_rows_count);
         block.getByPosition(result).column = std::move(col_res);
     }
 };
@@ -123,7 +131,8 @@ struct FormatImpl
         }
     }
 
-    static void vector(
+    template <bool HasColumnString, bool HasColumnFixedString>
+    static inline void vector(
         String pattern,
         const std::vector<const ColumnString::Chars *> & data,
         const std::vector<const ColumnString::Offsets *> & offsets,
@@ -273,18 +282,25 @@ struct FormatImpl
             {
                 UInt64 arg = index_positions[j - 1];
                 auto offset_ptr = offsets[arg];
-                UInt64 arg_offset;
-                UInt64 size;
-                if (offset_ptr)
+                UInt64 arg_offset = 0;
+                UInt64 size = 0;
+
+                if constexpr (HasColumnString)
                 {
-                    arg_offset = (*offset_ptr)[i - 1];
-                    size = (*offset_ptr)[i] - arg_offset - 1;
+                    if (!HasColumnFixedString || offset_ptr)
+                    {
+                        arg_offset = (*offset_ptr)[i - 1];
+                        size = (*offset_ptr)[i] - arg_offset - 1;
+                    }
                 }
-                else
+
+                if constexpr (HasColumnFixedString)
                 {
-                    arg_offset = fixed_string_N[arg] * i;
-                    size = fixed_string_N[arg];
-                    std::cout << arg_offset << ' ' << arg << ' ' << std::string(reinterpret_cast<const char *>(data[arg]->data()), data[arg]->size()) << ' ' << data[arg]->size() << ' ' << size << std::endl;
+                    if (!HasColumnString || !offset_ptr)
+                    {
+                        arg_offset = fixed_string_N[arg] * i;
+                        size = fixed_string_N[arg];
+                    }
                 }
 
 
