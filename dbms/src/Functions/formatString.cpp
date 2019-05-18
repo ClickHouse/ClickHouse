4d187185b0fb08ce8b86ad33daf8febc125dba13
Danila Kutenin <kutdanila@yandex.ru>
2019-05-18 16:21:48 +0300

    style

--- dbms/src/Functions/formatString.cpp
+++ dbms/src/Functions/formatString.cpp
@@ -1,6 +1,6 @@
-#include <DataTypes/DataTypeString.h>
-#include <Columns/ColumnString.h>
 #include <Columns/ColumnFixedString.h>
+#include <Columns/ColumnString.h>
+#include <DataTypes/DataTypeString.h>
 #include <Functions/FunctionFactory.h>
 #include <Functions/FunctionHelpers.h>
 #include <Functions/IFunction.h>
@@ -15,7 +15,6 @@
 
 namespace DB
 {
-
 namespace ErrorCodes
 {
     extern const int ILLEGAL_COLUMN;
@@ -45,20 +44,23 @@ public:
     DataTypePtr getReturnTypeImpl(const DataTypes & arguments) const override
     {
         if (arguments.empty())
-            throw Exception("Number of arguments for function " + getName() + " doesn't match: passed " + toString(arguments.size())
-                + ", should be at least 1",
+            throw Exception(
+                "Number of arguments for function " + getName() + " doesn't match: passed " + toString(arguments.size())
+                    + ", should be at least 1",
                 ErrorCodes::NUMBER_OF_ARGUMENTS_DOESNT_MATCH);
 
         if (arguments.size() > Impl::ArgumentThreshold)
-            throw Exception("Number of arguments for function " + getName() + " doesn't match: passed " + toString(arguments.size())
-                + ", should be at most " + std::to_string(Impl::ArgumentThreshold),
+            throw Exception(
+                "Number of arguments for function " + getName() + " doesn't match: passed " + toString(arguments.size())
+                    + ", should be at most " + std::to_string(Impl::ArgumentThreshold),
                 ErrorCodes::NUMBER_OF_ARGUMENTS_DOESNT_MATCH);
 
         for (const auto arg_idx : ext::range(0, arguments.size()))
         {
             const auto arg = arguments[arg_idx].get();
             if (!isStringOrFixedString(arg))
-                throw Exception("Illegal type " + arg->getName() + " of argument " + std::to_string(arg_idx + 1) + " of function " + getName(),
+                throw Exception(
+                    "Illegal type " + arg->getName() + " of argument " + std::to_string(arg_idx + 1) + " of function " + getName(),
                     ErrorCodes::ILLEGAL_TYPE_OF_ARGUMENT);
         }
 
@@ -98,17 +100,21 @@ public:
             }
             else
                 throw Exception(
-                    "Illegal column " + column->getName() + " of argument of function " + getName(),
-                    ErrorCodes::ILLEGAL_COLUMN);
+                    "Illegal column " + column->getName() + " of argument of function " + getName(), ErrorCodes::ILLEGAL_COLUMN);
         }
+
         if (has_column_string && has_column_fixed_string)
-            Impl::template vector<true, true>(std::move(pattern), data, offsets, fixed_string_N, col_res->getChars(), col_res->getOffsets(), input_rows_count);
+            Impl::template vector<true, true>(
+                std::move(pattern), data, offsets, fixed_string_N, col_res->getChars(), col_res->getOffsets(), input_rows_count);
         else if (!has_column_string && has_column_fixed_string)
-            Impl::template vector<false, true>(std::move(pattern), data, offsets, fixed_string_N, col_res->getChars(), col_res->getOffsets(), input_rows_count);
+            Impl::template vector<false, true>(
+                std::move(pattern), data, offsets, fixed_string_N, col_res->getChars(), col_res->getOffsets(), input_rows_count);
         else if (has_column_string && !has_column_fixed_string)
-            Impl::template vector<true, false>(std::move(pattern), data, offsets, fixed_string_N, col_res->getChars(), col_res->getOffsets(), input_rows_count);
+            Impl::template vector<true, false>(
+                std::move(pattern), data, offsets, fixed_string_N, col_res->getChars(), col_res->getOffsets(), input_rows_count);
         else
-            Impl::template vector<true, true>(std::move(pattern), data, offsets, fixed_string_N, col_res->getChars(), col_res->getOffsets(), input_rows_count);
+            Impl::template vector<true, true>(
+                std::move(pattern), data, offsets, fixed_string_N, col_res->getChars(), col_res->getOffsets(), input_rows_count);
         block.getByPosition(result).column = std::move(col_res);
     }
 };
@@ -116,20 +122,20 @@ public:
 
 struct FormatImpl
 {
-
     static constexpr size_t ArgumentThreshold = 1024;
     static constexpr size_t RightPadding = 15;
 
     static void parseNumber(const String & description, UInt64 l, UInt64 r, UInt64 & res)
     {
         res = 0;
-        for (UInt64 pos = l; pos < r; pos ++)
+        for (UInt64 pos = l; pos < r; pos++)
         {
             if (!isNumericASCII(description[pos]))
                 throw Exception("Not a number in curly braces at position " + std::to_string(pos), ErrorCodes::LOGICAL_ERROR);
             res = res * 10 + description[pos] - '0';
             if (res >= ArgumentThreshold)
-                throw Exception("Too big number for arguments, must be at most " + std::to_string(ArgumentThreshold), ErrorCodes::LOGICAL_ERROR);
+                throw Exception(
+                    "Too big number for arguments, must be at most " + std::to_string(ArgumentThreshold), ErrorCodes::LOGICAL_ERROR);
         }
     }
 
@@ -143,7 +149,6 @@ struct FormatImpl
         ColumnString::Offsets & res_offsets,
         size_t input_rows_count)
     {
-
         /// The subsequent indexes of strings we should use. e.g `Hello world {1} {3} {1} {0}` this array will be filled with [1, 3, 1, 0, ... (garbage)].
         UInt64 index_positions[ArgumentThreshold];
         UInt64 * index_positions_ptr = index_positions;
@@ -201,7 +206,8 @@ struct FormatImpl
                 if (last_open == i)
                 {
                     if (is_plain_numbering && !*is_plain_numbering)
-                        throw Exception("Cannot switch from automatic field numbering to manual field specification", ErrorCodes::LOGICAL_ERROR);
+                        throw Exception(
+                            "Cannot switch from automatic field numbering to manual field specification", ErrorCodes::LOGICAL_ERROR);
                     is_plain_numbering = true;
                     if (index_if_plain >= offsets.size())
                         throw Exception("Argument is too big for formatting", ErrorCodes::LOGICAL_ERROR);
@@ -211,14 +217,16 @@ struct FormatImpl
                 else
                 {
                     if (is_plain_numbering && *is_plain_numbering)
-                        throw Exception("Cannot switch from automatic field numbering to manual field specification", ErrorCodes::LOGICAL_ERROR);
+                        throw Exception(
+                            "Cannot switch from automatic field numbering to manual field specification", ErrorCodes::LOGICAL_ERROR);
                     is_plain_numbering = false;
 
                     UInt64 arg;
                     parseNumber(pattern, last_open, i, arg);
 
                     if (arg >= offsets.size())
-                        throw Exception("Argument is too big for formatting. Note that indexing starts from zero", ErrorCodes::LOGICAL_ERROR);
+                        throw Exception(
+                            "Argument is too big for formatting. Note that indexing starts from zero", ErrorCodes::LOGICAL_ERROR);
 
                     *index_positions_ptr = arg;
                     ++index_positions_ptr;
@@ -236,24 +244,27 @@ struct FormatImpl
 
         UInt64 final_size = 0;
 
-        for (std::string& str : substrings)
+        for (std::string & str : substrings)
         {
             /// Handling double braces (escaping).
             size_t i = 0;
             bool should_delete = true;
-            str.erase(std::remove_if(str.begin(), str.end(),
-                [&i, &should_delete, &str](char)
-                {
-                    bool is_double_brace = (str[i] == '{' && str[i + 1] == '{') || (str[i] == '}' && str[i + 1] == '}');
-                    ++i;
-                    if (is_double_brace && should_delete)
-                    {
-                        should_delete = false;
-                        return true;
-                    }
-                    should_delete = true;
-                    return false;
-                }), str.end());
+            str.erase(
+                std::remove_if(
+                    str.begin(),
+                    str.end(),
+                    [&i, &should_delete, &str](char) {
+                        bool is_double_brace = (str[i] == '{' && str[i + 1] == '{') || (str[i] == '}' && str[i + 1] == '}');
+                        ++i;
+                        if (is_double_brace && should_delete)
+                        {
+                            should_delete = false;
+                            return true;
+                        }
+                        should_delete = true;
+                        return false;
+                    }),
+                str.end());
 
             /// To use memcpySmallAllowReadWriteOverflow15 for substrings we should allocate a bit more to each string.
             /// That was chosen due to perfomance issues.
