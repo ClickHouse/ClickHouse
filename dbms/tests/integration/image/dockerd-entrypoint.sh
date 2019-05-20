828cf831b01ef8869a947ffe572d8ede38a04b02
Mikhail Filimonov <1549571+filimonov@users.noreply.github.com>
2019-05-20 10:11:53 +0200

    Sereval improvements in integration tests runner

--- dbms/tests/integration/image/dockerd-entrypoint.sh
+++ dbms/tests/integration/image/dockerd-entrypoint.sh
@@ -3,7 +3,18 @@ set -e
 
 dockerd --host=unix:///var/run/docker.sock --host=tcp://0.0.0.0:2375 &>/var/log/somefile &
 
-sleep 3 # to actual start
+set +e
+reties=0
+while true; do
+    docker info &>/dev/null && break
+    reties=$[$reties+1]
+    if [[ $reties -ge 100 ]]; then # 10 sec max
+        echo "Can't start docker daemon, timeout exceeded." >&2
+        exit 1;
+    fi
+    sleep 0.1
+done
+set -e
 
 echo "Start tests"
 export CLICKHOUSE_TESTS_SERVER_BIN_PATH=/clickhouse
@@ -11,4 +22,5 @@ export CLICKHOUSE_TESTS_CLIENT_BIN_PATH=/clickhouse
 export CLICKHOUSE_TESTS_BASE_CONFIG_DIR=/clickhouse-config
 export CLICKHOUSE_ODBC_BRIDGE_BINARY_PATH=/clickhouse-odbc-bridge
 
-cd /ClickHouse/dbms/tests/integration && pytest $PYTEST_OPTS
+cd /ClickHouse/dbms/tests/integration
+exec "$@"
