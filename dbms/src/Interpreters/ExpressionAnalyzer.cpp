9553fc8b3ed0718ded7c2ce516b479ea613b9a32
Constantin S. Pan <kvapen@gmail.com>
2019-05-23 17:25:53 +0100

    Mark all input columns in LIMIT BY as required output
    The query analyzer only marks the actual arguments of LIMIT BY as required
    output for the LimitBy step in the pipeline. This is fine, unless the query is
    distributed, in which case the first stage might remove a column that is used
    at the second stage (e.g. for ORDER BY) but is not part of the final select.
    
    Prevent removal of any columns in LimitBy by marking all input columns as
    required output.

--- dbms/src/Interpreters/ExpressionAnalyzer.cpp
+++ dbms/src/Interpreters/ExpressionAnalyzer.cpp
@@ -849,8 +849,16 @@ bool ExpressionAnalyzer::appendLimitBy(ExpressionActionsChain & chain, bool only
 
     getRootActions(select_query->limitBy(), only_types, step.actions);
 
+    NameSet aggregated_names;
+    for (const auto & column : aggregated_columns)
+    {
+            step.required_output.push_back(column.name);
+            aggregated_names.insert(column.name);
+    }
+
     for (const auto & child : select_query->limitBy()->children)
-        step.required_output.push_back(child->getColumnName());
+        if (!aggregated_names.count(child->getColumnName()))
+            step.required_output.push_back(child->getColumnName());
 
     return true;
 }
