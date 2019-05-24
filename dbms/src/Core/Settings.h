9534f1c9c98bd51849a985a4a82ddf6bafaa6991
overshov <ershov.ov@yandex.ru>
2019-05-24 20:06:18 +0200

    Enable parallel reading by default, check what happens

--- dbms/src/Core/Settings.h
+++ dbms/src/Core/Settings.h
@@ -102,7 +102,7 @@ struct Settings : public SettingsCollection<Settings>
     M(SettingBool, distributed_group_by_no_merge, false, "Do not merge aggregation states from different servers for distributed query processing - in case it is for certain that there are different keys on different shards.") \
     M(SettingBool, optimize_skip_unused_shards, false, "Assumes that data is distributed by sharding_key. Optimization to skip unused shards if SELECT query filters by sharding_key.") \
     \
-    M(SettingBool, enable_parallel_reading, false, "Enable parallel_reading for several data formats (JSON, TSV, TKSV, Values, CSV).") \
+    M(SettingBool, enable_parallel_reading, true, "Enable parallel_reading for several data formats (JSON, TSV, TKSV, Values, CSV).") \
     M(SettingUInt64, max_threads_for_parallel_reading, 0, "The maximum number of threads to parallel reading. By default, it is set to max_threads.") \
     M(SettingUInt64, min_chunk_size_for_parallel_reading, 0, "The minimum chunk size, which each thread tries to parse under mutex in parallel reading.") \
     \
