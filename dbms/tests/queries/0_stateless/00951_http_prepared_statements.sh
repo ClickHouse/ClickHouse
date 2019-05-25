ec3cae04f8c98b0b696901497904ed4f3c688c96
Alexander Tretiakov <tretialex7@google.com>
2019-05-26 01:51:21 +0300

    fix test

--- dbms/tests/queries/0_stateless/00951_http_prepared_statements.sh
+++ dbms/tests/queries/0_stateless/00951_http_prepared_statements.sh
@@ -9,11 +9,11 @@ ${CLICKHOUSE_CURL} -sS $CLICKHOUSE_URL -d "CREATE TABLE ps (i UInt8, s String) E
 ${CLICKHOUSE_CURL} -sS $CLICKHOUSE_URL -d "INSERT INTO ps VALUES (1, 'Hello, world')";
 ${CLICKHOUSE_CURL} -sS $CLICKHOUSE_URL -d "INSERT INTO ps VALUES (2, 'test')";
 
-${CLICKHOUSE_CURL} "${CLICKHOUSE_URL}?param_id=1"\
-	-d "SELECT * FROM ps WHERE i = {id:UInt8}";
-${CLICKHOUSE_CURL} "${CLICKHOUSE_URL}?param_phrase=Hello,+world"\
-	-d "SELECT * FROM ps WHERE s = {phrase:String}";
-${CLICKHOUSE_CURL} "${CLICKHOUSE_URL}?param_id=2&param_phrase=test"\
-	-d "SELECT * FROM ps WHERE i = {id:UInt8} and s = {phrase:String}";
+${CLICKHOUSE_CURL} -sS "${CLICKHOUSE_URL}?param_id=1"\
+	-d "SELECT * FROM ps WHERE i = {id:UInt8} ORDER BY i, s";
+${CLICKHOUSE_CURL} -sS "${CLICKHOUSE_URL}?param_phrase=Hello,+world"\
+	-d "SELECT * FROM ps WHERE s = {phrase:String} ORDER BY i, s";
+${CLICKHOUSE_CURL} -sS "${CLICKHOUSE_URL}?param_id=2&param_phrase=test"\
+	-d "SELECT * FROM ps WHERE i = {id:UInt8} and s = {phrase:String} ORDER BY i, s";
 
 ${CLICKHOUSE_CURL} -sS $CLICKHOUSE_URL -d "DROP TABLE ps";
