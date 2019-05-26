47c2b751ccc25f04533a2d377506497a5191d23a
Anastasiya Rodigina <anrodigina@gmail.com>
2019-05-26 12:08:48 +0000

    Edit little silly misprints

--- dbms/tests/queries/0_stateless/00940_order_by_pk_order.sql
+++ dbms/tests/queries/0_stateless/00940_order_by_pk_order.sql
@@ -4,8 +4,8 @@ DROP TABLE IF EXISTS test.pk_order;
 SET optimize_pk_order = 1;
 
 CREATE TABLE test.pk_order(a UInt64, b UInt64, c UInt64, d UInt64) ENGINE=MergeTree() ORDER BY (a, b);
-INSERT INTO test.pk_order(a, b, c, d) VALUES (1, 1, 101, 1), (1, 2, 102, 1), (1, 3, 103, 1), (1, 4, 104, 1);;
-INSERT INTO test.pk_order(a, b, c, d)  VALUES (1, 5, 104, 1), (1, 6, 105, 1), (2, 1, 106, 2), (2, 1, 107, 2);
+INSERT INTO test.pk_order(a, b, c, d) VALUES (1, 1, 101, 1), (1, 2, 102, 1), (1, 3, 103, 1), (1, 4, 104, 1);
+INSERT INTO test.pk_order(a, b, c, d) VALUES (1, 5, 104, 1), (1, 6, 105, 1), (2, 1, 106, 2), (2, 1, 107, 2);
 
 INSERT INTO test.pk_order(a, b, c, d) VALUES (2, 2, 107, 2), (2, 3, 108, 2), (2, 4, 109, 2);
 
