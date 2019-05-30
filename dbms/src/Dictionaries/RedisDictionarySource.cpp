c58effc2af382192d389852ecf1bb0373bd85f3b
comunodi <alex_kote@bk.ru>
2019-05-31 00:06:39 +0300

    Fix inserting keys with hash map

--- dbms/src/Dictionaries/RedisDictionarySource.cpp
+++ dbms/src/Dictionaries/RedisDictionarySource.cpp
@@ -36,16 +36,10 @@ namespace DB
 #if USE_POCO_REDIS
 
 #    include <Poco/Redis/Array.h>
-#    include <Poco/Redis/AsyncReader.h>
 #    include <Poco/Redis/Client.h>
 #    include <Poco/Redis/Command.h>
-#    include <Poco/Redis/Error.h>
-#    include <Poco/Redis/Exception.h>
-#    include <Poco/Redis/RedisEventArgs.h>
-#    include <Poco/Redis/RedisStream.h>
 #    include <Poco/Redis/Type.h>
 #    include <Poco/Util/AbstractConfiguration.h>
-#    include <Poco/Version.h>
 
 #    include <IO/WriteHelpers.h>
 #    include <Common/FieldVisitors.h>
