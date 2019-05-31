0eb9461665f77de57138d9889ba085347c46fdb2
proller <proller@github.com>
2019-05-31 19:02:53 +0300

    wip

--- dbms/tests/queries/0_stateless/00950_dict_get.sql
+++ dbms/tests/queries/0_stateless/00950_dict_get.sql
@@ -1,14 +1,14 @@
 use default;
 drop table if exists ints;
-drop table if exists strings;
+drop table if exists strings_00950;
 drop table if exists decimals;
 
 create table ints (key UInt64, i8 Int8, i16 Int16, i32 Int32, i64 Int64, u8 UInt8, u16 UInt16, u32 UInt32, u64 UInt64) Engine = Memory;
-create table strings (key UInt64, str String) Engine = Memory;
+create table strings_00950 (key UInt64, str String) Engine = Memory;
 create table decimals (key UInt64, d32 Decimal32(4), d64 Decimal64(6), d128 Decimal128(1)) Engine = Memory;
 
 insert into ints values (1, 1, 1, 1, 1, 1, 1, 1, 1);
-insert into strings values (1, '1');
+insert into strings_00950 values (1, '1');
 insert into decimals values (1, 1, 1, 1);
 
 select 'dictGet', 'flat_ints' as dict_name, toUInt64(1) as k,
@@ -241,5 +241,5 @@ select 'dictGetOrDefault', 'complex_cache_decimals' as dict_name, tuple(toUInt64
     dictGetOrDefault(dict_name, 'd128', k, toDecimal128(42, 1));
 
 drop table ints;
-drop table strings;
+drop table strings_00950;
 drop table decimals;
