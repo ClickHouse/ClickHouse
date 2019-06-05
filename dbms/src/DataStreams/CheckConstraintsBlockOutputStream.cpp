7de978fd441093a98d27763e7acda82c06b7f425
Gleb Novikov <gnovikov@iponweb.net>
2019-06-05 11:05:46 +0300

    Implemented memoryIsByte, replaced memoryIsZero with it, implemented memory check in CheckConstraintsBlockOutputStream

--- dbms/src/DataStreams/CheckConstraintsBlockOutputStream.cpp
+++ dbms/src/DataStreams/CheckConstraintsBlockOutputStream.cpp
@@ -1,5 +1,8 @@
 #include <DataStreams/CheckConstraintsBlockOutputStream.h>
+#include <Functions/FunctionHelpers.h>
+#include <common/find_symbols.h>
 #include <Parsers/formatAST.h>
+#include <Columns/ColumnsCommon.h>
 
 namespace DB
 {
@@ -31,17 +34,33 @@ void CheckConstraintsBlockOutputStream::writeSuffix()
     output->writeSuffix();
 }
 
-bool CheckConstraintsBlockOutputStream::checkConstraintOnBlock(const Block & block, const ExpressionActionsPtr & constraint)
+bool CheckConstraintsBlockOutputStream::checkImplMemory(const Block & block, const ExpressionActionsPtr & constraint)
+{
+    Block res = block;
+    constraint->execute(res);
+    assert(block.columns() == res.columns() - 1);
+    ColumnWithTypeAndName res_column = res.safeGetByPosition(res.columns() - 1);
+    auto res_column_uint8 = checkAndGetColumn<ColumnUInt8>(res_column.column.get());
+    return memoryIsByte(res_column_uint8->getRawDataBegin<1>(), res_column_uint8->byteSize(), 0x1);
+}
+
+bool CheckConstraintsBlockOutputStream::checkImplBool(const Block & block, const ExpressionActionsPtr & constraint)
 {
     Block res = block;
     constraint->execute(res);
     assert(block.columns() == res.columns() - 1);
     ColumnWithTypeAndName res_column = res.safeGetByPosition(res.columns() - 1);
     size_t column_size = res_column.column->size();
+    // std::cerr << "Sizes of constraints: " << res_column.column->size() << ' ' << res_column.column->get << '\n';
     for (size_t i = 0; i < column_size; ++i)
         if (!res_column.column->getBool(i))
             return false;
     return true;
+}
+
+bool CheckConstraintsBlockOutputStream::checkConstraintOnBlock(const Block & block, const ExpressionActionsPtr & constraint)
+{
+    return checkImplMemory(block, constraint);
 }
 
 }
