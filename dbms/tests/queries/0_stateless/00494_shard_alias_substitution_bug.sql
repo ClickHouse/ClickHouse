6773d83c9a4b981b03fc24b6fc06e32486c51f2d
Ivan Lezhankin <ilezhankin@yandex-team.ru>
2019-06-07 18:41:24 +0300

    Fix some tests

--- dbms/tests/queries/0_stateless/00494_shard_alias_substitution_bug.sql
+++ dbms/tests/queries/0_stateless/00494_shard_alias_substitution_bug.sql
@@ -1,8 +1,8 @@
-DROP TABLE IF EXISTS test.nested;
-CREATE TABLE test.nested (n Nested(x UInt8)) ENGINE = Memory;
-INSERT INTO test.nested VALUES ([1, 2]);
-SELECT 1 AS x FROM remote('127.0.0.2', test.nested) ARRAY JOIN n.x;
-DROP TABLE test.nested;
+DROP TABLE IF EXISTS nested;
+CREATE TABLE nested (n Nested(x UInt8)) ENGINE = Memory;
+INSERT INTO nested VALUES ([1, 2]);
+SELECT 1 AS x FROM remote('127.0.0.2', currentDatabase(), nested) ARRAY JOIN n.x;
+DROP TABLE nested;
 
 SELECT dummy AS dummy, dummy AS b FROM system.one;
 SELECT dummy AS dummy, dummy AS b, b AS c FROM system.one;
