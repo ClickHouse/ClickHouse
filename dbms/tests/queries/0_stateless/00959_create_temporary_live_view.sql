2d4f362c5bf011d06e3a562706a3ffcddb45c8ed
Vitaliy Zakaznikov <vzakaznikov@protonmail.com>
2019-06-07 09:05:14 -0400

    * Adding JSONEachRowWithProgress format   that can be used with WATCH queries when HTTP connection is used   in this case progress events are used as a heartbeat * Adding new tests that use JSONEachRowWithProgress format * Adding tests for WATCH query heartbeats when HTTP connection is used * Small fixes to uexpect.py

--- dbms/tests/queries/0_stateless/00959_create_temporary_live_view.sql
+++ dbms/tests/queries/0_stateless/00959_create_temporary_live_view.sql
@@ -9,7 +9,7 @@ CREATE TABLE test.mt (a Int32) Engine=MergeTree order by tuple();
 CREATE TEMPORARY LIVE VIEW test.lv AS SELECT sum(a) FROM test.mt;
 
 SHOW TABLES LIKE 'lv';
-SELECT sleep(1);
+SELECT sleep(2);
 SHOW TABLES LIKE 'lv';
 
 DROP TABLE test.mt;
