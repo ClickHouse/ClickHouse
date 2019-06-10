1ebe3047ffc6719b4f817a25ea66a8d2f9ee47ac
Vitaliy Zakaznikov <vzakaznikov@protonmail.com>
2019-06-10 18:12:06 -0400

    * Moving uexpect.py to dbms/tests/integration/helpers folder * Updating uexpect.py to support close() method and   context manager for proper resource cleanup * Updating tests to use uexpect context manager

--- dbms/tests/queries/0_stateless/00960_live_view_watch_events_live.py
+++ dbms/tests/queries/0_stateless/00960_live_view_watch_events_live.py
@@ -6,7 +6,7 @@ import signal
 
 CURDIR = os.path.dirname(os.path.realpath(__file__))
 
-uexpect = imp.load_source('uexpect', os.path.join(CURDIR, 'uexpect.py'))
+uexpect = imp.load_source('uexpect', os.path.join(CURDIR, '..', '..', 'integration', 'helpers', 'uexpect.py'))
 
 def client(name=''):
     client = uexpect.spawn(os.environ.get('CLICKHOUSE_CLIENT'))
@@ -18,29 +18,28 @@ def client(name=''):
 
 prompt = ':\) '
 end_of_block = r'.*\xe2\x94\x82\r\n.*\xe2\x94\x98\r\n'
-client1 = client('client1>')
-client2 = client('client2>')
 
-client1.expect(prompt)
-client2.expect(prompt)
+with client('client1>') as client1, client('client2>') as client2:
+    client1.expect(prompt)
+    client2.expect(prompt)
 
-client1.send('DROP TABLE IF EXISTS test.lv')
-client1.expect(prompt)
-client1.send(' DROP TABLE IF EXISTS test.mt')
-client1.expect(prompt)
-client1.send('CREATE TABLE test.mt (a Int32) Engine=MergeTree order by tuple()')
-client1.expect(prompt)
-client1.send('CREATE LIVE VIEW test.lv AS SELECT sum(a) FROM test.mt')
-client1.expect(prompt)
-client1.send('WATCH test.lv EVENTS')
-client2.send('INSERT INTO test.mt VALUES (1),(2),(3)')
-client1.expect('2.*4cd0592103888d4682de9a32a23602e3' + end_of_block)
-client2.send('INSERT INTO test.mt VALUES (4),(5),(6)')
-client1.expect('3.*2186dbea325ee4c56b67e9b792e993a3' + end_of_block)
-# send Ctrl-C
-os.kill(client1.process.pid,signal.SIGINT)
-client1.expect(prompt)
-client1.send('DROP TABLE test.lv')
-client1.expect(prompt)
-client1.send('DROP TABLE test.mt')
-client1.expect(prompt)
+    client1.send('DROP TABLE IF EXISTS test.lv')
+    client1.expect(prompt)
+    client1.send(' DROP TABLE IF EXISTS test.mt')
+    client1.expect(prompt)
+    client1.send('CREATE TABLE test.mt (a Int32) Engine=MergeTree order by tuple()')
+    client1.expect(prompt)
+    client1.send('CREATE LIVE VIEW test.lv AS SELECT sum(a) FROM test.mt')
+    client1.expect(prompt)
+    client1.send('WATCH test.lv EVENTS')
+    client2.send('INSERT INTO test.mt VALUES (1),(2),(3)')
+    client1.expect('2.*4cd0592103888d4682de9a32a23602e3' + end_of_block)
+    client2.send('INSERT INTO test.mt VALUES (4),(5),(6)')
+    client1.expect('3.*2186dbea325ee4c56b67e9b792e993a3' + end_of_block)
+    # send Ctrl-C
+    os.kill(client1.process.pid,signal.SIGINT)
+    client1.expect(prompt)
+    client1.send('DROP TABLE test.lv')
+    client1.expect(prompt)
+    client1.send('DROP TABLE test.mt')
+    client1.expect(prompt)
--- dbms/tests/queries/0_stateless/00971_live_view_watch_http_heartbeat.py
+++ dbms/tests/queries/0_stateless/00971_live_view_watch_http_heartbeat.py
@@ -6,7 +6,7 @@ import signal
 
 CURDIR = os.path.dirname(os.path.realpath(__file__))
 
-uexpect = imp.load_source('uexpect', os.path.join(CURDIR, 'uexpect.py'))
+uexpect = imp.load_source('uexpect', os.path.join(CURDIR, '..', '..', 'integration', 'helpers', 'uexpect.py'))
 
 def client(name='', command=None):
     if command is None:
@@ -21,37 +21,37 @@ def client(name='', command=None):
 
 prompt = ':\) '
 end_of_block = r'.*\xe2\x94\x82\r\n.*\xe2\x94\x98\r\n'
-client1 = client('client1>')
-client1.expect(prompt)
 
-client1.send('DROP TABLE IF EXISTS test.lv')
-client1.expect(prompt)
-client1.send(' DROP TABLE IF EXISTS test.mt')
-client1.expect(prompt)
-client1.send('CREATE TABLE test.mt (a Int32) Engine=MergeTree order by tuple()')
-client1.expect(prompt)
-client1.send('CREATE LIVE VIEW test.lv AS SELECT sum(a) FROM test.mt')
-client1.expect(prompt)
+with client('client1>') as client1, client('client2>', ['bash', '--noediting']) as client2:
+    client1.expect(prompt)
 
-client2 = client('client2>', ['bash', '--noediting'])
-client2.expect('[\$#] ')
-client2.send('wget -O- -q "http://localhost:8123/?live_view_heartbeat_interval=1&query=WATCH test.lv FORMAT JSONEachRowWithProgress"')
-client2.expect('"progress".*',)
-client2.expect('{"row":{"sum(a)":"0","_version":"1"}}\r\n', escape=True)
-client2.expect('"progress".*\r\n')
-# heartbeat is provided by progress message
-client2.expect('"progress".*\r\n')
+    client1.send('DROP TABLE IF EXISTS test.lv')
+    client1.expect(prompt)
+    client1.send(' DROP TABLE IF EXISTS test.mt')
+    client1.expect(prompt)
+    client1.send('CREATE TABLE test.mt (a Int32) Engine=MergeTree order by tuple()')
+    client1.expect(prompt)
+    client1.send('CREATE LIVE VIEW test.lv AS SELECT sum(a) FROM test.mt')
+    client1.expect(prompt)
 
-client1.send('INSERT INTO test.mt VALUES (1),(2),(3)')
-client1.expect(prompt)
+    client2.expect('[\$#] ')
+    client2.send('wget -O- -q "http://localhost:8123/?live_view_heartbeat_interval=1&query=WATCH test.lv FORMAT JSONEachRowWithProgress"')
+    client2.expect('"progress".*',)
+    client2.expect('{"row":{"sum(a)":"0","_version":"1"}}\r\n', escape=True)
+    client2.expect('"progress".*\r\n')
+    # heartbeat is provided by progress message
+    client2.expect('"progress".*\r\n')
 
-client2.expect('"progress".*"read_rows":"2".*\r\n')
-client2.expect('{"row":{"sum(a)":"6","_version":"2"}}\r\n', escape=True)
+    client1.send('INSERT INTO test.mt VALUES (1),(2),(3)')
+    client1.expect(prompt)
 
-## send Ctrl-C
-os.kill(client2.process.pid,signal.SIGINT)
+    client2.expect('"progress".*"read_rows":"2".*\r\n')
+    client2.expect('{"row":{"sum(a)":"6","_version":"2"}}\r\n', escape=True)
 
-client1.send('DROP TABLE test.lv')
-client1.expect(prompt)
-client1.send('DROP TABLE test.mt')
-client1.expect(prompt)
+    ## send Ctrl-C
+    os.kill(client2.process.pid,signal.SIGINT)
+
+    client1.send('DROP TABLE test.lv')
+    client1.expect(prompt)
+    client1.send('DROP TABLE test.mt')
+    client1.expect(prompt)
