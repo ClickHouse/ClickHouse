ba6c85b1910d79a98025814babe15d7450dedc95
unknown <inv2004@gmail.com>
2019-06-12 09:13:42 -0400

    movingSum : test added

---
+++ dbms/tests/queries/0_stateless/00953_moving_sum.sql
@@ -0,0 +1,38 @@
+DROP TABLE IF EXISTS moving_sum_num;
+DROP TABLE IF EXISTS moving_sum_dec;
+
+CREATE TABLE moving_sum_num (
+  k String,
+  dt DateTime,
+  v UInt64
+)
+ENGINE = MergeTree ORDER BY (k, dt);
+
+INSERT INTO moving_sum_num
+  SELECT 'b' k, toDateTime('2001-02-03 00:00:00')+number as dt, number as v
+  FROM system.numbers
+  LIMIT 5
+  UNION ALL
+  SELECT 'a' k, toDateTime('2001-02-03 00:00:00')+number as dt, number as v
+  FROM system.numbers
+  LIMIT 5;
+
+INSERT INTO moving_sum_num
+  SELECT 'b' k, toDateTime('2001-02-03 01:00:00')+number as dt, 5+number as v
+  FROM system.numbers
+  LIMIT 5;
+
+SELECT k, movingSum(v) FROM moving_sum_num GROUP BY k ORDER BY k FORMAT TabSeparatedWithNamesAndTypes;
+SELECT k, movingSum(3)(v) FROM moving_sum_num GROUP BY k ORDER BY k FORMAT TabSeparatedWithNamesAndTypes;
+
+CREATE TABLE moving_sum_dec ENGINE = Memory AS
+  SELECT k, dt, toDecimal64(v, 2) as v
+  FROM moving_sum_num;
+
+SELECT k, movingSum(v) FROM moving_sum_dec GROUP BY k ORDER BY k FORMAT TabSeparatedWithNamesAndTypes;
+SELECT k, movingSum(v) FROM moving_sum_dec GROUP BY k ORDER BY k FORMAT TabSeparatedWithNamesAndTypes;
+
+DROP TABLE moving_sum_dec;
+DROP TABLE moving_sum_num;
+
+
