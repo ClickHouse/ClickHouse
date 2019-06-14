51e03a26ac42095060a04b39c9c2b1c211092331
Alexey Milovidov <milovidov@yandex-team.ru>
2019-06-14 20:15:30 +0300

    Fixes for #5331

--- dbms/src/Interpreters/ReplaceQueryParameterVisitor.cpp
+++ dbms/src/Interpreters/ReplaceQueryParameterVisitor.cpp
@@ -9,6 +9,8 @@
 #include <Parsers/ASTLiteral.h>
 #include <Parsers/ASTQueryParameter.h>
 #include <Interpreters/ReplaceQueryParameterVisitor.h>
+#include <Interpreters/addTypeConversionToAST.h>
+
 
 namespace DB
 {
@@ -18,7 +20,7 @@ void ReplaceQueryParameterVisitor::visit(ASTPtr & ast)
     for (auto & child : ast->children)
     {
         if (child->as<ASTQueryParameter>())
-            visitvisitQueryParameter(child);
+            visitQueryParameter(child);
         else
             visit(child);
     }
@@ -37,17 +39,9 @@ void ReplaceQueryParameterVisitor::visitQueryParameter(ASTPtr & ast)
 {
     const auto & ast_param = ast->as<ASTQueryParameter &>();
     const String & value = getParamValue(ast_param.name);
-    const String & type = ast_param.type;
+    const String & type_name = ast_param.type;
 
-    /// Replacing all occurrences of types Date and DateTime with String.
-    /// String comparison is used in "WHERE" conditions with this types.
-    
-    /// TODO: WTF, totally incorrect
-
-    boost::replace_all(type, "DateTime", "String");
-    boost::replace_all(type, "Date", "String");
-
-    const auto data_type = DataTypeFactory::instance().get(type);
+    const auto data_type = DataTypeFactory::instance().get(type_name);
     auto temp_column_ptr = data_type->createColumn();
     IColumn & temp_column = *temp_column_ptr;
     ReadBufferFromString read_buffer{value};
@@ -55,10 +49,9 @@ void ReplaceQueryParameterVisitor::visitQueryParameter(ASTPtr & ast)
     data_type->deserializeAsWholeText(temp_column, read_buffer, format_settings);
 
     if (!read_buffer.eof())
-        throw Exception("Expected correct value in parameter with name '" + ast_param->name + "'", ErrorCodes::BAD_ARGUMENTS);
+        throw Exception("Value " + value + " cannot be parsed as " + type_name + " for query parameter '"  + ast_param.name + "'", ErrorCodes::BAD_ARGUMENTS);
 
-    Field field = temp_column[0];
-    ast = std::make_shared<ASTLiteral>(std::move(field));
+    ast = addTypeConversionToAST(std::make_shared<ASTLiteral>(temp_column[0]), type_name);
 }
 
 }
