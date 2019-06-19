60c85c4f241d81ea764a4ba1f0e76255c02ef71d
Ivan Remen <i.remen@corp.mail.ru>
2019-06-19 11:09:01 +0300

    Fix CRC32 function name in tests

--- dbms/tests/queries/0_stateless/00936_crc32_function.sql
+++ dbms/tests/queries/0_stateless/00936_crc32_function.sql
@@ -10,11 +10,11 @@ INSERT INTO table1 VALUES('aasq', 'xxz');
 INSERT INTO table1 VALUES('zxcqwer', '');
 INSERT INTO table1 VALUES('', '');
 
-select crc32('string');
-select crc32('string'), crc32('test');
-select crc32(str1) from table1 order by crc32(str1);
-select crc32(str2) from table1 order by crc32(str2);
-select crc32(str1), crc32(str2) from table1 order by crc32(str1), crc32(str2);
-select str1, str2, crc32(str1), crc32(str2) from table1 order by crc32(str1), crc32(str2);
+select CRC32('string');
+select CrC32('string'), crc32('test'); -- We want to test, that function name is case-insensitive
+select CRC32(str1) from table1 order by CRC32(str1);
+select CRC32(str2) from table1 order by CRC32(str2);
+select CRC32(str1), CRC32(str2) from table1 order by CRC32(str1), CRC32(str2);
+select str1, str2, CRC32(str1), CRC32(str2) from table1 order by CRC32(str1), CRC32(str2);
 
 DROP TABLE table1;
