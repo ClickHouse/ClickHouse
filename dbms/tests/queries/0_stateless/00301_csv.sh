a350c7f752be7ace664be9d3218e8c4e41ebf491
Alexander Kuzmenkov <akuzm@yandex-team.ru>
2019-06-20 12:10:34 +0300

    Fix the test for eof

--- dbms/tests/queries/0_stateless/00301_csv.sh
+++ dbms/tests/queries/0_stateless/00301_csv.sh
@@ -6,7 +6,7 @@ CURDIR=$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)
 $CLICKHOUSE_CLIENT --query="DROP TABLE IF EXISTS csv";
 $CLICKHOUSE_CLIENT --query="CREATE TABLE csv (s String, n UInt64 DEFAULT 1, d Date DEFAULT '2019-06-19') ENGINE = Memory";
 
-echo '"Hello, world", 123, "2016-01-01"
+printf '"Hello, world", 123, "2016-01-01"
 "Hello, ""world""", "456", 2016-01-02,
 Hello "world", 789 ,2016-01-03
 "Hello
