883a9f03bceb6522a0cf73a90fb610a2cf2ea370
hcz <h.cz@qq.com>
2019-06-25 12:28:07 +0800

    Fix parameter handling

--- dbms/src/AggregateFunctions/AggregateFunctionResample.cpp
+++ dbms/src/AggregateFunctions/AggregateFunctionResample.cpp
@@ -8,6 +8,7 @@ namespace DB
 
 namespace ErrorCodes
 {
+    extern const int ILLEGAL_TYPE_OF_ARGUMENT;
     extern const int NUMBER_OF_ARGUMENTS_DOESNT_MATCH;
 }
 
@@ -50,48 +51,53 @@ public:
         const Array & params
     ) const override
     {
-        for (const Field & param : params)
-        {
-            if (
-                param.getType() != Field::Types::UInt64
-                && param.getType() != Field::Types::Int64
-            )
-                return nullptr;
-        }
-
         WhichDataType which {
             arguments.back()
         };
 
-        if (
-            which.isNativeUInt()
-            || which.isDateOrDateTime()
-        )
+        if (which.isNativeUInt() || which.isDateOrDateTime())
+        {
+            UInt64 begin = params[params.size() - 3].safeGet<UInt64>();
+            UInt64 end = params[params.size() - 2].safeGet<UInt64>();
+            UInt64 step = params[params.size() - 1].safeGet<UInt64>();
+
             return std::make_shared<AggregateFunctionResample<UInt64>>(
                 nested_function,
-                params.front().get<UInt64>(),
-                params[1].get<UInt64>(),
-                params.back().get<UInt64>(),
+                begin,
+                end,
+                step,
                 arguments,
                 params
             );
+        }
 
-        if (
-            which.isNativeInt()
-            || which.isEnum()
-            || which.isInterval()
-        )
+        if (which.isNativeInt() || which.isEnum() || which.isInterval())
+        {
+            Int64 begin, end, step;
+
+            // notice: UInt64 -> Int64 may lead to overflow
+            if (!params[params.size() - 3].tryGet<Int64>(begin))
+                begin = params[params.size() - 3].safeGet<UInt64>();
+            if (!params[params.size() - 2].tryGet<Int64>(end))
+                end = params[params.size() - 2].safeGet<UInt64>();
+            if (!params[params.size() - 1].tryGet<Int64>(step))
+                step = params[params.size() - 1].safeGet<UInt64>();
+
             return std::make_shared<AggregateFunctionResample<Int64>>(
                 nested_function,
-                params.front().get<Int64>(),
-                params[1].get<Int64>(),
-                params.back().get<Int64>(),
+                begin,
+                end,
+                step,
                 arguments,
                 params
             );
+        }
 
-        // TODO
-        return nullptr;
+        throw Exception(
+            "Illegal types of argument for aggregate function " + getName()
+                + ", the last three parameters and the last argument should be native Ints or UInts",
+            ErrorCodes::ILLEGAL_TYPE_OF_ARGUMENT
+        );
     }
 };
 
