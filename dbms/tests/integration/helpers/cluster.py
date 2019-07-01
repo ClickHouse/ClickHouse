729223857b2c086e724c8690656ac24989fecfb2
Ivan Lezhankin <ilezhankin@yandex-team.ru>
2019-07-01 16:49:00 +0300

    Don't re-new docker volumes in all tests

--- dbms/tests/integration/helpers/cluster.py
+++ dbms/tests/integration/helpers/cluster.py
@@ -338,7 +338,7 @@ class ClickHouseCluster:
 
         self.docker_client = docker.from_env(version=self.docker_api_version)
 
-        common_opts = ['up', '-d', '--force-recreate', '--renew-anon-volumes']
+        common_opts = ['up', '-d', '--force-recreate']
 
         if self.with_zookeeper and self.base_zookeeper_cmd:
             subprocess_check_call(self.base_zookeeper_cmd + common_opts)
@@ -347,23 +347,23 @@ class ClickHouseCluster:
             self.wait_zookeeper_to_start(120)
 
         if self.with_mysql and self.base_mysql_cmd:
-            subprocess_check_call(self.base_mysql_cmd+ common_opts)
+            subprocess_check_call(self.base_mysql_cmd + common_opts)
             self.wait_mysql_to_start(120)
 
         if self.with_postgres and self.base_postgres_cmd:
-            subprocess_check_call(self.base_postgres_cmd+ common_opts)
+            subprocess_check_call(self.base_postgres_cmd + common_opts)
             self.wait_postgres_to_start(120)
 
         if self.with_kafka and self.base_kafka_cmd:
-            subprocess_check_call(self.base_kafka_cmd+ common_opts)
+            subprocess_check_call(self.base_kafka_cmd + common_opts + ['--renew-anon-volumes'])
             self.kafka_docker_id = self.get_instance_docker_id('kafka1')
 
         if self.with_hdfs and self.base_hdfs_cmd:
-            subprocess_check_call(self.base_hdfs_cmd+ common_opts)
+            subprocess_check_call(self.base_hdfs_cmd + common_opts)
             self.wait_hdfs_to_start(120)
 
         if self.with_mongo and self.base_mongo_cmd:
-            subprocess_check_call(self.base_mongo_cmd+ common_opts)
+            subprocess_check_call(self.base_mongo_cmd + common_opts)
             self.wait_mongo_to_start(30)
 
         subprocess_check_call(self.base_cmd + ['up', '-d', '--no-recreate'])
