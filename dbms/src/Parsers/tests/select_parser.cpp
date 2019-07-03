4557412feda17ca6b5564a15a67ed92e27e33a62
alesapin <alesapin@gmail.com>
2019-07-03 23:57:05 +0300

    Remove unexpected changes

--- dbms/src/Parsers/tests/select_parser.cpp
+++ dbms/src/Parsers/tests/select_parser.cpp
@@ -10,17 +10,16 @@ try
 {
     using namespace DB;
 
-    //std::string input =
-    //    " SELECT 18446744073709551615, f(1), '\\\\', [a, b, c], (a, b, c), 1 + 2 * -3, a = b OR c > d.1 + 2 * -g[0] AND NOT e < f * (x + y)"
-    //    " FROM default.hits"
-    //    " WHERE CounterID = 101500 AND UniqID % 3 = 0"
-    //    " GROUP BY UniqID"
-    //    " HAVING SUM(Refresh) > 100"
-    //    " ORDER BY Visits, PageViews"
-    //    " LIMIT LENGTH('STRING OF 20 SYMBOLS') - 20 + 1000, 10.05 / 5.025 * 5"
-    //    " INTO OUTFILE 'test.out'"
-    //    " FORMAT TabSeparated";
-    std::string input = "CHECK TABLE txxx PARTITION 201910";
+    std::string input =
+        " SELECT 18446744073709551615, f(1), '\\\\', [a, b, c], (a, b, c), 1 + 2 * -3, a = b OR c > d.1 + 2 * -g[0] AND NOT e < f * (x + y)"
+        " FROM default.hits"
+        " WHERE CounterID = 101500 AND UniqID % 3 = 0"
+        " GROUP BY UniqID"
+        " HAVING SUM(Refresh) > 100"
+        " ORDER BY Visits, PageViews"
+        " LIMIT LENGTH('STRING OF 20 SYMBOLS') - 20 + 1000, 10.05 / 5.025 * 5"
+        " INTO OUTFILE 'test.out'"
+        " FORMAT TabSeparated";
 
     ParserQueryWithOutput parser;
     ASTPtr ast = parseQuery(parser, input.data(), input.data() + input.size(), "", 0);
