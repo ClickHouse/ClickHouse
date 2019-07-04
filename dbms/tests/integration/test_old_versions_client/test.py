b0d20f91142746b9ed4660a149cd7dfb370dc6a1
alesapin <alesapin@gmail.com>
2019-07-04 13:59:42 +0300

    Add test for client-server interaction with different versions

--- dbms/tests/integration/test_old_versions_client/test.py
+++ dbms/tests/integration/test_old_versions_client/test.py
@@ -9,7 +9,7 @@ from helpers.test_tools import assert_eq_with_retry
 cluster = ClickHouseCluster(__file__)
 node18_14 = cluster.add_instance('node18_14', image='yandex/clickhouse-server:18.14.19', with_installed_binary=True)
 node19_1 = cluster.add_instance('node19_1', image='yandex/clickhouse-server:19.1.16', with_installed_binary=True)
-node19_4 = cluster.add_instance('node19_4', image='yandex/clickhouse-server:v19.4.5.35', with_installed_binary=True)
+node19_4 = cluster.add_instance('node19_4', image='yandex/clickhouse-server:19.4.5.35', with_installed_binary=True)
 node19_6 = cluster.add_instance('node19_6', image='yandex/clickhouse-server:19.6.3.18', with_installed_binary=True)
 node_new = cluster.add_instance('node_new')
 
@@ -18,19 +18,21 @@ def setup_nodes():
     try:
         cluster.start()
         for n in (node18_14, node19_1, node19_4, node19_6, node_new):
-            n.query('''CREATE TABLE test_table (date Date, id UInt32, value UInt64) ENGINE = MergeTree() ORDER BY tuple()''')
+            n.query('''CREATE TABLE test_table (id UInt32, value UInt64) ENGINE = MergeTree() ORDER BY tuple()''')
+
+        yield cluster
     finally:
         cluster.shutdown()
 
 
 def query_from_one_node_to_another(client_node, server_node, query):
-        client_node.exec_in_container(["bash", "-c", "/usr/bin/clickhouse client --host {} --query '{}'".format(server_node.name, query)])
+    client_node.exec_in_container(["bash", "-c", "/usr/bin/clickhouse client --host {} --query '{}'".format(server_node.name, query)])
 
 def test_client_from_different_versions(setup_nodes):
     old_nodes = (node18_14, node19_1, node19_4, node19_6,)
     # from new to old
     for n in old_nodes:
-        query_from_one_node_to_another(node_new, n, "INSERT INTO test_table VALUES (toDate('2018-10-01'), 1, 1)")
+        query_from_one_node_to_another(node_new, n, "INSERT INTO test_table VALUES (1, 1)")
 
     for n in old_nodes:
         query_from_one_node_to_another(node_new, n, "SELECT COUNT() FROM test_table")
@@ -40,9 +42,9 @@ def test_client_from_different_versions(setup_nodes):
 
     # from old to new
     for i, n in enumerate(old_nodes):
-        query_from_one_node_to_another(n, node_new, "INSERT INTO test_table VALUES (toDate('2018-10-01'), {i}, {i})".format(i))
+        query_from_one_node_to_another(n, node_new, "INSERT INTO test_table VALUES ({i}, {i})".format(i=i))
 
     for n in old_nodes:
         query_from_one_node_to_another(n, node_new, "SELECT COUNT() FROM test_table")
 
-    assert n.query("SELECT COUNT() FROM test_table") == str(len(old_nodes)) + "\n"
+    assert node_new.query("SELECT COUNT() FROM test_table") == str(len(old_nodes)) + "\n"
