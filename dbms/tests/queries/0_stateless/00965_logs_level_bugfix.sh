a92dac0452807cbbd6e9083c65c9fe0102664601
Nikita Mikhaylov <mikhaylovnikitka@gmail.com>
2019-07-08 17:15:48 +0300

    multithreading

--- dbms/tests/queries/0_stateless/00965_logs_level_bugfix.sh
+++ dbms/tests/queries/0_stateless/00965_logs_level_bugfix.sh
@@ -5,7 +5,13 @@ CURDIR=$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)
 
 cp /dev/null 00965_logs_level_bugfix.tmp
 
+clickhouse-client --send_logs_level="trace" --query="SELECT 1;" 2>> 00965_logs_level_bugfix.tmp
 clickhouse-client --send_logs_level="debug" --query="SELECT 1;" 2>> 00965_logs_level_bugfix.tmp
+clickhouse-client --send_logs_level="information" --query="SELECT 1;" 2>> 00965_logs_level_bugfix.tmp
+clickhouse-client --send_logs_level="warning" --query="SELECT 1;" 2>> 00965_logs_level_bugfix.tmp
+clickhouse-client --send_logs_level="error" --query="SELECT 1;" 2>> 00965_logs_level_bugfix.tmp
+clickhouse-client --send_logs_level="none" --query="SELECT 1;" 2>> 00965_logs_level_bugfix.tmp
+
 awk '{ print $8 }' 00965_logs_level_bugfix.tmp
 
- 
+
