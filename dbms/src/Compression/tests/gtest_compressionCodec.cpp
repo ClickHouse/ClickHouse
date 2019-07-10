0f4075478e940f38390c3ef44c55d323aeefc61e
Vasily Nemkov <V.Nemkov@gmail.com>
2019-07-10 10:28:47 +0300

    Fixed DoubleDelta encoding cases for random Int32 and Int64.

--- dbms/src/Compression/tests/gtest_compressionCodec.cpp
+++ dbms/src/Compression/tests/gtest_compressionCodec.cpp
@@ -8,16 +8,16 @@
 
 #include <boost/format.hpp>
 
+#include <bitset>
 #include <cmath>
 #include <initializer_list>
 #include <iomanip>
-#include <memory>
-#include <vector>
-#include <typeinfo>
-#include <iterator>
-#include <optional>
 #include <iostream>
-#include <bitset>
+#include <iterator>
+#include <memory>
+#include <typeinfo>
+#include <vector>
+
 #include <string.h>
 
 #pragma GCC diagnostic ignored "-Wsign-compare"
@@ -119,17 +119,22 @@ template <typename T, typename ContainerLeft, typename ContainerRight>
                 result = ::testing::AssertionFailure();
             }
 
-            result << "mismatching " << sizeof(T) << "-byte item #" << i
-                   << "\nexpected: " << bin(left_value)
-                   << "\ngot     : " << bin(right_value)
-                   << std::endl;
-
-            if (++mismatching_items >= MAX_MISMATCHING_ITEMS)
+            if (++mismatching_items <= MAX_MISMATCHING_ITEMS)
             {
-                result << "..." << std::endl;
-                break;
+                result << "mismatching " << sizeof(T) << "-byte item #" << i
+                   << "\nexpected: " << bin(left_value) << " (0x" << std::hex << left_value << ")"
+                   << "\ngot     : " << bin(right_value) << " (0x" << std::hex << right_value << ")"
+                   << std::endl;
+                if (mismatching_items == MAX_MISMATCHING_ITEMS)
+                {
+                    result << "..." << std::endl;
+                }
             }
         }
+    }
+    if (mismatching_items > 0)
+    {
+        result << "\ntotal mismatching items:" << mismatching_items << " of " << size;
     }
 
     return result;
@@ -137,17 +142,47 @@ template <typename T, typename ContainerLeft, typename ContainerRight>
 
 struct CodecTestParam
 {
+    std::string type_name;
     std::vector<char> source_data;
     UInt8 data_byte_size;
+    double min_compression_ratio;
     std::string case_name;
+
+    // to allow setting ratio after building with complex builder functions.
+    CodecTestParam && setRatio(const double & ratio) &&
+    {
+        this->min_compression_ratio = ratio;
+        return std::move(*this);
+    }
 };
+
+CodecTestParam operator+(CodecTestParam && left, CodecTestParam && right)
+{
+    assert(left.type_name == right.type_name);
+    assert(left.data_byte_size == right.data_byte_size);
+
+    std::vector data(std::move(left.source_data));
+    data.insert(data.end(), right.source_data.begin(), right.source_data.end());
+
+    return CodecTestParam{
+                        left.type_name,
+                        std::move(data),
+                        left.data_byte_size,
+                        std::min(left.min_compression_ratio, right.min_compression_ratio),
+                        left.case_name + " + " + right.case_name
+    };
+}
 
 std::ostream & operator<<(std::ostream & ostr, const CodecTestParam & param)
 {
     return ostr << "name: " << param.case_name
+                << "\ntype name:" << param.type_name
                 << "\nbyte size: " << static_cast<UInt32>(param.data_byte_size)
                 << "\ndata size: " << param.source_data.size();
 }
+
+// compression ratio < 1.0 means that codec output is smaller than input.
+const double DEFAULT_MIN_COMPRESSION_RATIO = 1.0;
 
 template <typename T, typename... Args>
 CodecTestParam makeParam(Args && ... args)
@@ -162,11 +197,11 @@ CodecTestParam makeParam(Args && ... args)
         write_pos += sizeof(v);
     }
 
-    return CodecTestParam{std::move(data), sizeof(T),
-                (boost::format("%1% %2%") % (sizeof(T) * std::size(vals)) % " predefined values").str()};
+    return CodecTestParam{type_name<T>(), std::move(data), sizeof(T), DEFAULT_MIN_COMPRESSION_RATIO,
+                (boost::format("%1% values of %2%") % std::size(vals) % type_name<T>()).str()};
 }
 
-template <typename T, size_t Begin = 1, size_t End = 10000, typename Generator>
+template <typename T, size_t Begin = 1, size_t End = 10001, typename Generator>
 CodecTestParam generateParam(Generator gen, const char* gen_name)
 {
     static_assert (End >= Begin, "End must be not less than Begin");
@@ -181,8 +216,8 @@ CodecTestParam generateParam(Generator gen, const char* gen_name)
         write_pos += sizeof(v);
     }
 
-    return CodecTestParam{std::move(data), sizeof(T),
-                (boost::format("%1% from %2% (%3% => %4%)") % type_name<T>() % gen_name % Begin % End).str()};
+    return CodecTestParam{type_name<T>(), std::move(data), sizeof(T), DEFAULT_MIN_COMPRESSION_RATIO,
+                (boost::format("%1% values of %2% from %3%") % (End - Begin) % type_name<T>() % gen_name).str()};
 }
 
 void TestTranscoding(ICompressionCodec * codec, const CodecTestParam & param)
@@ -216,6 +251,13 @@ void TestTranscoding(ICompressionCodec * codec, const CodecTestParam & param)
         default:
             FAIL() << "Invalid data_byte_size: " << param.data_byte_size;
     }
+    const auto header_size = codec->getHeaderSize();
+    const auto compression_ratio = (encoded_size - header_size) / (source_data.size() * 1.0);
+
+    ASSERT_LE(compression_ratio, param.min_compression_ratio)
+            << "\n\tdecoded size: " << source_data.size()
+            << "\n\tencoded size: " << encoded_size
+            << "(no header: " << encoded_size - header_size << ")";
 }
 
 class CodecTest : public ::testing::TestWithParam<CodecTestParam>
@@ -230,19 +272,33 @@ public:
 
 TEST_P(CodecTest, DoubleDelta)
 {
-    const auto & param = GetParam();
+    auto param = GetParam();
     auto codec = std::make_unique<CompressionCodecDoubleDelta>(param.data_byte_size);
+    if (param.type_name == type_name<Float32>() || param.type_name == type_name<Float64>())
+    {
+        // dd doesn't work great with many cases of integers and may result in very poor compression rate.
+        param.min_compression_ratio *= 1.5;
+    }
 
     TestTranscoding(codec.get(), param);
 }
 
 TEST_P(CodecTest, Gorilla)
 {
-    const auto & param = GetParam();
+    auto param = GetParam();
     auto codec = std::make_unique<CompressionCodecGorilla>(param.data_byte_size);
+    if (param.type_name == type_name<UInt32>() || param.type_name == type_name<Int32>()
+            || param.type_name == type_name<UInt64>() || param.type_name == type_name<Int64>())
+    {
+        // gorilla doesn't work great with many cases of integers and may result in very poor compression rate.
+        param.min_compression_ratio *= 1.5;
+    }
 
     TestTranscoding(codec.get(), param);
 }
+
+// Here we use generators to produce test payload for codecs.
+// Generator is a callable that should produce output value of the same type as input value.
 
 auto SameValueGenerator = [](auto value)
 {
@@ -261,30 +317,44 @@ auto SequentialGenerator = [](auto stride = 1)
     };
 };
 
+// Generator that helps debugging output of other generators
+// by logging every output value alongside iteration index and input.
+//auto LoggingProxyGenerator = [](auto other_generator, const char * name, std::ostream & ostr, const int limit = std::numeric_limits<int>::max())
+//{
+//    ostr << "\n\nValues from " << name << ":\n";
+//    auto count = std::make_shared<int>(0);
+//    return [&, count](auto i)
+//    {
+//        using ValueType = decltype(i);
+//        const auto ret = static_cast<ValueType>(other_generator(i));
+//        if (++(*count) < limit)
+//        {
+//            ostr << "\t" << *count << " : " << i << " => " << ret << "\n";
+//        }
+
+//        return ret;
+//    };
+//};
+
 template <typename T>
 struct MonotonicGenerator
 {
     MonotonicGenerator(T stride = 1, size_t max_step = 10)
-        : prev_value{},
+        : prev_value(0),
           stride(stride),
           max_step(max_step)
     {}
 
     template <typename U>
-    U operator()(U i)
+    U operator()(U)
     {
-        if (!prev_value.has_value())
-        {
-            prev_value = i * stride;
-        }
-
-        const U result = *prev_value + static_cast<T>(stride * (rand() % max_step));
+        const U result = prev_value + static_cast<T>(stride * (rand() % max_step));
 
         prev_value = result;
         return result;
     }
 
-    std::optional<T> prev_value;
+    T prev_value;
     const T stride;
     const size_t max_step;
 };
@@ -301,24 +371,44 @@ auto MinMaxGenerator = [](auto i)
     }
 };
 
-auto RandomGenerator = [](auto i) {return static_cast<decltype(i)>(rand());};
+template <typename T>
+struct RandomGenerator
+{
+    RandomGenerator(T seed = 0, T value_cap = std::numeric_limits<T>::max())
+        : e(seed),
+          value_cap(value_cap)
+    {
+    }
+
+    template <typename U>
+    U operator()(U i)
+    {
+        return static_cast<decltype(i)>(distribution(e) % value_cap);
+    }
+
+private:
+    std::default_random_engine e;
+    std::uniform_int_distribution<T> distribution;
+    const T value_cap;
+};
 
 auto RandomishGenerator = [](auto i)
 {
     return static_cast<decltype(i)>(sin(static_cast<double>(i) * i) * i);
 };
 
-INSTANTIATE_TEST_CASE_P(Basic,
+// helper macro to produce human-friendly test case name
+#define G(generator) generator, #generator
+
+INSTANTIATE_TEST_CASE_P(Mixed,
     CodecTest,
     ::testing::Values(
-        makeParam<UInt32>(1, 2, 3, 4),
-        makeParam<UInt64>(1, 2, 3, 4),
-        makeParam<Float32>(1.1, 2.2, 3.3, 4.4),
-        makeParam<Float64>(1.1, 2.2, 3.3, 4.4)
+        generateParam<Int32,  1, 3>(G(MinMaxGenerator)) + generateParam<Int32,  1, 11>(G(SequentialGenerator(1))).setRatio(1),
+        generateParam<UInt32, 1, 3>(G(MinMaxGenerator)) + generateParam<UInt32, 1, 11>(G(SequentialGenerator(1))).setRatio(1),
+        generateParam<Int64,  1, 3>(G(MinMaxGenerator)) + generateParam<Int64,  1, 11>(G(SequentialGenerator(1))).setRatio(1),
+        generateParam<UInt64, 1, 3>(G(MinMaxGenerator)) + generateParam<UInt64, 1, 11>(G(SequentialGenerator(1))).setRatio(1)
     ),
 );
-
-#define G(generator) generator, #generator
 
 INSTANTIATE_TEST_CASE_P(Same,
     CodecTest,
@@ -359,18 +449,20 @@ INSTANTIATE_TEST_CASE_P(Monotonic,
 INSTANTIATE_TEST_CASE_P(Random,
     CodecTest,
     ::testing::Values(
-        generateParam<UInt32>(G(RandomGenerator)),
-        generateParam<UInt64>(G(RandomGenerator))
+        generateParam<UInt32>(G(RandomGenerator<UInt32>(0, 1000'000'000))).setRatio(1.2),
+        generateParam<UInt64>(G(RandomGenerator<UInt64>(0, 1000'000'000))).setRatio(1.1)
     ),
 );
 
-INSTANTIATE_TEST_CASE_P(RandomLike,
+INSTANTIATE_TEST_CASE_P(Randomish,
     CodecTest,
     ::testing::Values(
-        generateParam<Int32>(G(RandomishGenerator)),
-        generateParam<Int64>(G(RandomishGenerator)),
-        generateParam<Float32>(G(RandomishGenerator)),
-        generateParam<Float64>(G(RandomishGenerator))
+        generateParam<Int32>(G(RandomishGenerator)).setRatio(1.1),
+        generateParam<Int64>(G(RandomishGenerator)).setRatio(1.1),
+        generateParam<UInt32>(G(RandomishGenerator)).setRatio(1.1),
+        generateParam<UInt64>(G(RandomishGenerator)).setRatio(1.1),
+        generateParam<Float32>(G(RandomishGenerator)).setRatio(1.1),
+        generateParam<Float64>(G(RandomishGenerator)).setRatio(1.1)
     ),
 );
 
