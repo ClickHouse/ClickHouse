280025d5b4931205d3186d055ed69c2ccfe9685e
Vitaliy Zakaznikov <vzakaznikov@protonmail.com>
2019-07-10 08:22:31 -0400

    * Updating live view tests to workaround an issue with clickhouse-client exiting when   Ctrl-C is pressed during WATCH query execution instead of going to the   client prompt.

--- dbms/tests/queries/0_stateless/00964_live_view_watch_events_heartbeat.py
+++ dbms/tests/queries/0_stateless/00964_live_view_watch_events_heartbeat.py
@@ -34,7 +34,10 @@ with client(name='client1>', log=log) as client1, client(name='client2>', log=lo
     client1.expect('Progress: 2.00 rows.*\)')
     # send Ctrl-C
     client1.send('\x03', eol='')
-    client1.expect(prompt)
+    match = client1.expect('(%s)|([#\$] )' % prompt)
+    if match.groups()[1]:
+        client1.send(client1.command)
+        client1.expect(prompt)    
     client1.send('DROP TABLE test.lv')
     client1.expect(prompt)
     client1.send('DROP TABLE test.mt')
--- dbms/tests/queries/0_stateless/helpers/client.py
+++ dbms/tests/queries/0_stateless/helpers/client.py
@@ -14,6 +14,7 @@ def client(command=None, name='', log=None):
     client = uexpect.spawn(['/bin/bash','--noediting'])
     if command is None:
         command = os.environ.get('CLICKHOUSE_BINARY', 'clickhouse') + '-client'
+    client.command = command
     client.eol('\r')
     client.logger(log, prefix=name)
     client.timeout(20)
