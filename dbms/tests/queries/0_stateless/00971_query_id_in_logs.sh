066337e197f7cf2809c9e833ca6fa10bfd660532
Alexey Milovidov <milovidov@yandex-team.ru>
2019-07-15 22:29:54 +0300

    Fixed test

--- dbms/tests/queries/0_stateless/00971_query_id_in_logs.sh
+++ dbms/tests/queries/0_stateless/00971_query_id_in_logs.sh
@@ -6,4 +6,4 @@ CURDIR=$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)
 set -e
 
 # No log lines without query id
-$CLICKHOUSE_CLIENT --send_logs_level=trace --query_id=hello --query="SELECT count() FROM numbers(10)" 2>&1 | grep -vF ' {hello} ' | grep -P '<\w+>'
+$CLICKHOUSE_CLIENT --send_logs_level=trace --query_id=hello --query="SELECT count() FROM numbers(10)" 2>&1 | grep -vF ' {hello} ' | grep -P '<\w+>' ||:
