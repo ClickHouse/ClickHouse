bd15f874921856c750fa693ca3ab44c7d3e866de
Nikita Mikhaylov <mikhaylovnikitka@gmail.com>
2019-07-16 19:27:42 +0300

    changes after review

--- dbms/tests/queries/0_stateless/00965_pocopatch_logs_level_bugfix.sh
+++ dbms/tests/queries/0_stateless/00965_pocopatch_logs_level_bugfix.sh
@@ -3,7 +3,7 @@
 CURDIR=$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)
 . $CURDIR/../shell_config.sh
 
-cp /dev/null 00965_logs_level_bugfix.tmp
+> 00965_logs_level_bugfix.tmp
 
 clickhouse-client --send_logs_level="trace" --query="SELECT 1;" 2>> 00965_logs_level_bugfix.tmp
 clickhouse-client --send_logs_level="debug" --query="SELECT 1;" 2>> 00965_logs_level_bugfix.tmp
