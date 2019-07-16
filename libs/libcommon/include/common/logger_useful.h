5771a236fbb827bc681e9b1a4e7de19bfc2da6eb
Nikita Mikhaylov <mikhaylovnikitka@gmail.com>
2019-07-16 19:45:24 +0300

    better

--- libs/libcommon/include/common/logger_useful.h
+++ libs/libcommon/include/common/logger_useful.h
@@ -20,20 +20,23 @@ using DB::CurrentThread;
 
 /// Logs a message to a specified logger with that level.
 
-#define LOG_SIMPLE(logger, message, priority, PRIORITY)                                         \
+#define LOG_SIMPLE(logger, message, priority, PRIORITY) do                                      \
+{                                                                                               \
     const bool is_clients_log = (CurrentThread::getGroup() != nullptr) &&                       \
             (CurrentThread::getGroup()->client_logs_level >= (priority));                       \
     if ((logger)->is((PRIORITY)) || is_clients_log) {                                           \
         std::stringstream oss_internal_rare;                                                    \
         oss_internal_rare << message;                                                           \
-        auto channel = (logger)->getChannel();                                                  \
-        channel->log(Message("", oss_internal_rare.str(), (PRIORITY)));                         \
+        if ( auto channel = (logger)->getChannel() ) {                                          \
+            channel->log(Message("", oss_internal_rare.str(), (PRIORITY)));                     \
+        }                                                                                       \
     }                                                                                           \
+} while (false)
 
-#define LOG_WARNING(logger, message) do { LOG_SIMPLE(logger, message, LogsLevel::warning, Message::PRIO_WARNING ) } while(false)
-#define LOG_TRACE(logger, message)   do { LOG_SIMPLE(logger, message, LogsLevel::trace, Message::PRIO_TRACE) } while(false)
-#define LOG_DEBUG(logger, message)   do { LOG_SIMPLE(logger, message, LogsLevel::debug, Message::PRIO_DEBUG) } while(false)
-#define LOG_INFO(logger, message)    do { LOG_SIMPLE(logger, message, LogsLevel::information, Message::PRIO_INFORMATION) } while(false)
-#define LOG_ERROR(logger, message)   do { LOG_SIMPLE(logger, message, LogsLevel::error, Message::PRIO_ERROR) } while(false)
 
+#define LOG_TRACE(logger, message)   LOG_SIMPLE(logger, message, LogsLevel::trace, Message::PRIO_TRACE)
+#define LOG_DEBUG(logger, message)   LOG_SIMPLE(logger, message, LogsLevel::debug, Message::PRIO_DEBUG)
+#define LOG_INFO(logger, message)    LOG_SIMPLE(logger, message, LogsLevel::information, Message::PRIO_INFORMATION)
+#define LOG_WARNING(logger, message) LOG_SIMPLE(logger, message, LogsLevel::warning, Message::PRIO_WARNING)
+#define LOG_ERROR(logger, message)   LOG_SIMPLE(logger, message, LogsLevel::error, Message::PRIO_ERROR)
 
