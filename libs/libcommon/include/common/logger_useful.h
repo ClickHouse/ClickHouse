54e66f4e8ae47e78db64be5bc0a498edfe62a224
Nikita Mikhailov <jakalletti@sandbox-virtual>
2019-07-16 11:36:50 +0300

    small commit to fix build check

--- libs/libcommon/include/common/logger_useful.h
+++ libs/libcommon/include/common/logger_useful.h
@@ -6,6 +6,7 @@
 #include <atomic>
 #include <Poco/Logger.h>
 #include <Poco/Message.h>
+#include <Poco/Version.h>
 #include <Core/SettingsCommon.h>
 #include <Common/CurrentThread.h>
 
@@ -21,38 +22,6 @@ using DB::CurrentThread;
 /// Logs a message to a specified logger with that level.
 
 #if defined(POCO_CLICKHOUSE_PATCH)
-
-#define LOG_TRACE(logger, message) do { \
-    if ((logger)->trace()) {\
-    std::stringstream oss_internal_rare;    \
-    oss_internal_rare << message; \
-    (logger)->trace(oss_internal_rare.str());}} while(false)
-
-#define LOG_DEBUG(logger, message) do { \
-    if ((logger)->debug()) {\
-    std::stringstream oss_internal_rare;    \
-    oss_internal_rare << message; \
-    (logger)->debug(oss_internal_rare.str());}} while(false)
-
-#define LOG_INFO(logger, message) do { \
-    if ((logger)->information()) {\
-    std::stringstream oss_internal_rare;    \
-    oss_internal_rare << message; \
-    (logger)->information(oss_internal_rare.str());}} while(false)
-
-#define LOG_WARNING(logger, message) do { \
-    if ((logger)->warning()) {\
-    std::stringstream oss_internal_rare;    \
-    oss_internal_rare << message; \
-    (logger)->warning(oss_internal_rare.str());}} while(false)
-
-#define LOG_ERROR(logger, message) do { \
-    if ((logger)->error()) {\
-    std::stringstream oss_internal_rare;    \
-    oss_internal_rare << message; \
-    (logger)->error(oss_internal_rare.str());}} while(false)
-
-#else
 
 #define LOG_TRACE(logger, message) do { \
     const bool is_clients_log = (CurrentThread::getGroup() != nullptr) && (CurrentThread::getGroup()->client_logs_level >= LogsLevel::trace); \
@@ -108,6 +77,38 @@ using DB::CurrentThread;
     } else { \
         (logger)->error(oss_internal_rare.str()); \
     }}} while(false)
+
+#else
+
+#define LOG_TRACE(logger, message) do { \
+    if ((logger)->trace()) {\
+    std::stringstream oss_internal_rare;    \
+    oss_internal_rare << message; \
+    (logger)->trace(oss_internal_rare.str());}} while(false)
+
+#define LOG_DEBUG(logger, message) do { \
+    if ((logger)->debug()) {\
+    std::stringstream oss_internal_rare;    \
+    oss_internal_rare << message; \
+    (logger)->debug(oss_internal_rare.str());}} while(false)
+
+#define LOG_INFO(logger, message) do { \
+    if ((logger)->information()) {\
+    std::stringstream oss_internal_rare;    \
+    oss_internal_rare << message; \
+    (logger)->information(oss_internal_rare.str());}} while(false)
+
+#define LOG_WARNING(logger, message) do { \
+    if ((logger)->warning()) {\
+    std::stringstream oss_internal_rare;    \
+    oss_internal_rare << message; \
+    (logger)->warning(oss_internal_rare.str());}} while(false)
+
+#define LOG_ERROR(logger, message) do { \
+    if ((logger)->error()) {\
+    std::stringstream oss_internal_rare;    \
+    oss_internal_rare << message; \
+    (logger)->error(oss_internal_rare.str());}} while(false)
 
 #endif
 
