8289472b9821cc3e813036febb72bb8af8b67765
Nikita Mikhaylov <mikhaylovnikitka@gmail.com>
2019-07-22 18:09:33 +0300

    style improvements

--- dbms/src/Interpreters/TextLog.cpp
+++ dbms/src/Interpreters/TextLog.cpp
@@ -15,9 +15,27 @@
 #include <Poco/Net/IPAddress.h>
 #include <array>
 
-namespace DB {
+namespace DB
+{
 
-Block TextLogElement::createBlock() {
+template <> struct NearestFieldTypeImpl<Message::Priority> { using Type = UInt64; };
+
+Block TextLogElement::createBlock()
+{
+
+    auto priority_datatype = std::make_shared<DataTypeEnum8>(
+        DataTypeEnum8::Values
+        {
+                {"FATAL",          static_cast<Int8>(Message::PRIO_FATAL)},
+                {"CRITICAL",       static_cast<Int8>(Message::PRIO_CRITICAL)},
+                {"ERROR",          static_cast<Int8>(Message::PRIO_ERROR)},
+                {"WARNING",        static_cast<Int8>(Message::PRIO_WARNING)},
+                {"NOTICE",         static_cast<Int8>(Message::PRIO_NOTICE)},
+                {"INFORMATION",    static_cast<Int8>(Message::PRIO_INFORMATION)},
+                {"DEBUG",          static_cast<Int8>(Message::PRIO_DEBUG)},
+                {"TRACE",          static_cast<Int8>(Message::PRIO_TRACE)}
+        });
+
     return
     {
         {std::make_shared<DataTypeDate>(),                                                    "event_date"},
@@ -28,11 +46,13 @@ Block TextLogElement::createBlock() {
         {std::make_shared<DataTypeUInt32>(),                                                  "thread_number"},
         {std::make_shared<DataTypeUInt32>(),                                                  "os_thread_id"},
 
-        {std::make_shared<DataTypeUInt8>(),                                                   "level"},
+        {std::move(priority_datatype),                                                        "level"},
 
         {std::make_shared<DataTypeString>(),                                                  "query_id"},
         {std::make_shared<DataTypeString>(),                                                  "logger_name"},
         {std::make_shared<DataTypeString>(),                                                  "message"},
+
+        {std::make_shared<DataTypeUInt32>(),                                                  "revision"},
 
         {std::make_shared<DataTypeLowCardinality>(std::make_shared<DataTypeString>()),        "source_file"},
         {std::make_shared<DataTypeUInt64>(),                                                  "source_line"}
@@ -46,7 +66,6 @@ void TextLogElement::appendToBlock(Block & block) const
     size_t i = 0;
 
     columns[i++]->insert(DateLUT::instance().toDayNum(event_time));
-
     columns[i++]->insert(event_time);
     columns[i++]->insert(microseconds);
 
@@ -55,15 +74,16 @@ void TextLogElement::appendToBlock(Block & block) const
     columns[i++]->insert(thread_number);
     columns[i++]->insert(os_thread_id);
 
-    columns[i++]->insert(UInt8(level));
+    columns[i++]->insert(level);
 
     columns[i++]->insert(query_id);
     columns[i++]->insert(logger_name);
     columns[i++]->insert(message);
+
+    columns[i++]->insert(ClickHouseRevision::get());
 
     columns[i++]->insert(source_file);
     columns[i++]->insert(source_line);
 }
 
 }
-
