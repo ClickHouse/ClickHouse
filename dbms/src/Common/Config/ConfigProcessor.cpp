4588f68104489a256bc57d2e98ab3d47396163ad
dimarub2000 <dimarub2000@gmail.com>
2019-07-23 17:41:41 +0300

    changes after review

--- dbms/src/Common/Config/ConfigProcessor.cpp
+++ dbms/src/Common/Config/ConfigProcessor.cpp
@@ -210,8 +210,8 @@ void ConfigProcessor::mergeRecursive(XMLDocumentPtr config, Node * config_root, 
 
 void ConfigProcessor::merge(XMLDocumentPtr config, XMLDocumentPtr with)
 {
-    Node * config_root = getRootNode(&*config);
-    Node * with_root = getRootNode(&*with);
+    Node * config_root = getRootNode(config.get());
+    Node * with_root = getRootNode(with.get());
 
     if (config_root->nodeName() != with_root->nodeName())
         throw Poco::Exception("Root element doesn't have the corresponding root element as the config file. It must be <" + config_root->nodeName() + ">");
