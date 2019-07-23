9c66ccdbd657b74081fcc8ff0e3d1ec52aed9f68
Vitaly Baranov <vitbar@yandex-team.ru>
2019-07-23 16:34:05 +0300

    Add more protobuf IO tests.

--- dbms/tests/queries/0_stateless/00825_protobuf_format_output.sh
+++ dbms/tests/queries/0_stateless/00825_protobuf_format_output.sh
@@ -12,6 +12,7 @@ set -e -o pipefail
 # Run the client.
 $CLICKHOUSE_CLIENT --multiquery <<EOF
 DROP TABLE IF EXISTS out_persons_00825;
+DROP TABLE IF EXISTS out_squares_00825;
 
 CREATE TABLE out_persons_00825 (uuid UUID,
                                 name String,
@@ -40,9 +41,12 @@ CREATE TABLE out_persons_00825 (uuid UUID,
                                 \`nestiness_a_B.c_E\` Array(UInt32)
                                ) ENGINE = MergeTree ORDER BY tuple();
 
+CREATE TABLE out_squares_00825 (number UInt32, square UInt64) ENGINE = MergeTree ORDER BY tuple();
+
 INSERT INTO out_persons_00825 VALUES (toUUID('a7522158-3d41-4b77-ad69-6c598ee55c49'), 'Ivan', 'Petrov', 'male', toDate('1980-12-29'), 'png', '+74951234567', 1, toDateTime('2019-01-05 18:45:00'), 38, 'capricorn', ['Yesterday', 'Flowers'], [255, 0, 0], 'Moscow', [55.753215, 37.622504], 3.14, 214.10, 0.1, 5.8, 17060000000, ['meter', 'centimeter', 'kilometer'], [1, 0.01, 1000], 500, [501, 502]);
 INSERT INTO out_persons_00825 VALUES (toUUID('c694ad8a-f714-4ea3-907d-fd54fb25d9b5'), 'Natalia', 'Sokolova', 'female', toDate('1992-03-08'), 'jpg', NULL, 0, NULL, 26, 'pisces', [], [100, 200, 50], 'Plymouth', [50.403724, -4.142123], 3.14159, NULL, 0.007, 5.4, -20000000000000, [], [], NULL, []);
 INSERT INTO out_persons_00825 VALUES (toUUID('a7da1aa6-f425-4789-8947-b034786ed374'), 'Vasily', 'Sidorov', 'male', toDate('1995-07-28'), 'bmp', '+442012345678', 1, toDateTime('2018-12-30 00:00:00'), 23, 'leo', ['Sunny'], [250, 244, 10], 'Murmansk', [68.970682, 33.074981], 3.14159265358979, 100000000000, 800, -3.2, 154400000, ['pound'], [16], 503, []);
+INSERT INTO out_squares_00825 VALUES (2, 4), (0, 0), (3, 9);
 
 SELECT * FROM out_persons_00825 ORDER BY name FORMAT Protobuf SETTINGS format_schema = '$CURDIR/00825_protobuf_format:Person';
 SELECT 'ALTERNATIVE->';
@@ -51,6 +55,9 @@ SELECT 'STRINGS->';
 SELECT * FROM out_persons_00825 ORDER BY name FORMAT Protobuf SETTINGS format_schema = '$CURDIR/00825_protobuf_format:StrPerson';
 SELECT 'SYNTAX2->';
 SELECT * FROM out_persons_00825 ORDER BY name FORMAT Protobuf SETTINGS format_schema = '$CURDIR/00825_protobuf_format_syntax2:Syntax2Person';
+SELECT 'SQUARES->';
+SELECT * FROM out_squares_00825 ORDER BY number FORMAT Protobuf SETTINGS format_schema = '$CURDIR/00825_protobuf_format:NumberAndSquare';
 
 DROP TABLE IF EXISTS out_persons_00825;
+DROP TABLE IF EXISTS out_squares_00825;
 EOF
