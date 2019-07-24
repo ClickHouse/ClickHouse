87dd5ba3d2e3695d74966c68d801bae889cf7b0e
Nikita Mikhaylov <mikhaylovnikitka@gmail.com>
2019-07-24 11:41:31 +0300

    better test

--- dbms/tests/queries/0_stateless/00974_text_log_table_not_empty.sql
+++ dbms/tests/queries/0_stateless/00974_text_log_table_not_empty.sql
@@ -1,6 +1,9 @@
-SELECT 1;
+SELECT 6103;
 
 SYSTEM FLUSH LOGS;
 
-SELECT count() > 0
-FROM system.text_log;
+SELECT count(query) > 0
+FROM system.text_log AS natural
+INNER JOIN system.query_log USING (query_id)
+WHERE query = 'SELECT 6103'
+
