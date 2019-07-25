26580844f51076dda148c2b37d097202d0a39674
alexey-milovidov <milovidov@yandex-team.ru>
2019-07-25 04:07:06 +0300

    Update 00974_final_predicate_push_down.sql

--- dbms/tests/queries/0_stateless/00974_final_predicate_push_down.sql
+++ dbms/tests/queries/0_stateless/00974_final_predicate_push_down.sql
@@ -6,12 +6,12 @@ CREATE TABLE test_00974
     x Int32,
     ver UInt64
 )
-ENGINE = ReplacingMergeTree(date, x, 4096);
+ENGINE = ReplacingMergeTree(date, x, 1);
 
 INSERT INTO test_00974 VALUES ('2019-07-23', 1, 1), ('2019-07-23', 1, 2);
 INSERT INTO test_00974 VALUES ('2019-07-23', 2, 1), ('2019-07-23', 2, 2);
 
-SELECT COUNT() FROM (SELECT * FROM test_00974 FINAL) where x = 1 SETTINGS allow_push_predicate_to_final_subquery = 0  FORMAT JSON ;
-SELECT COUNT() FROM (SELECT * FROM test_00974 FINAL) where x = 1 SETTINGS allow_push_predicate_to_final_subquery = 1  FORMAT JSON ;
+SELECT COUNT() FROM (SELECT * FROM test_00974 FINAL) where x = 1 SETTINGS allow_push_predicate_to_final_subquery = 0;
+SELECT COUNT() FROM (SELECT * FROM test_00974 FINAL) where x = 1 SETTINGS allow_push_predicate_to_final_subquery = 1, max_rows_to_read = 2;
 
 DROP TABLE test_00974;
