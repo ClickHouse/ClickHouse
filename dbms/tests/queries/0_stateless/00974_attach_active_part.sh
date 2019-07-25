77d9b0a1011d38c98e37902afe5d48c643597678
Alexander Tokmakov <avtokmakov@yandex-team.ru>
2019-07-25 14:09:01 +0300

    test attach active part fails

--- dbms/tests/queries/0_stateless/00974_attach_active_part.sh
+++ dbms/tests/queries/0_stateless/00974_attach_active_part.sh
@@ -6,9 +6,7 @@ CURDIR=$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)
 $CLICKHOUSE_CLIENT --query="DROP TABLE IF EXISTS attach_bug";
 $CLICKHOUSE_CLIENT --query="CREATE TABLE attach_bug (n UInt64) ENGINE = MergeTree() PARTITION BY intDiv(n, 4) ORDER BY n";
 $CLICKHOUSE_CLIENT --query="INSERT INTO attach_bug SELECT number FROM system.numbers LIMIT 16";
-$CLICKHOUSE_CLIENT --query="ALTER TABLE attach_bug ATTACH PART '../1_2_2_0'" 2> /dev/null; # | grep "<some exception message>"
+$CLICKHOUSE_CLIENT --query="ALTER TABLE attach_bug ATTACH PART '../1_2_2_0'" 2>&1 | grep "Invalid part name" > /dev/null && echo 'OK'
 $CLICKHOUSE_CLIENT --query="SElECT name FROM system.parts WHERE table='attach_bug' ORDER BY name FORMAT TSV";
-$CLICKHOUSE_CLIENT --query="SElECT count() FROM attach_bug FORMAT TSV";             # will fail
+$CLICKHOUSE_CLIENT --query="SElECT count() FROM attach_bug FORMAT TSV";
 $CLICKHOUSE_CLIENT --query="DROP TABLE attach_bug";
-
-
