69300e5f98add48d0851eb66163d09535a8a14e9
Guillaume Tassery <gtassery@partners.accedian.com>
2019-07-26 13:54:50 +0200

    Drop db on move partition test

--- dbms/tests/queries/0_stateless/00975_move_partition_merge_tree.sql
+++ dbms/tests/queries/0_stateless/00975_move_partition_merge_tree.sql
@@ -1,3 +1,6 @@
+DROP TABLE IF EXISTS test_move_partition_src;
+DROP TABLE IF EXISTS test_move_partition_dest;
+
 CREATE TABLE IF NOT EXISTS test_move_partition_src (
     pk UInt8,
     val UInt32
@@ -12,9 +15,6 @@ CREATE TABLE IF NOT EXISTS test_move_partition_dest (
   PARTITION BY pk
   ORDER BY (pk, val);
 
-TRUNCATE TABLE test_move_partition_src;
-TRUNCATE TABLE test_move_partition_dest;
-
 INSERT INTO test_move_partition_src SELECT number % 2, number FROM system.numbers LIMIT 10000000;
 
 SELECT count() FROM test_move_partition_src;
@@ -24,3 +24,6 @@ ALTER TABLE test_move_partition_src MOVE PARTITION 1 TO test_move_partition_dest
 
 SELECT count() FROM test_move_partition_src;
 SELECT count() FROM test_move_partition_dest;
+
+DROP TABLE test_move_partition_src;
+DROP TABLE test_move_partition_dest;
