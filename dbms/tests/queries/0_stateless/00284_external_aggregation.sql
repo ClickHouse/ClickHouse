578abb19c6d674adcb443f072dda0dfb408900f6
Nikolai Kochetov <nik-kochetov@yandex-team.ru>
2019-07-29 19:50:30 +0300

    Update 00284_external_aggregation.

--- dbms/tests/queries/0_stateless/00284_external_aggregation.sql
+++ dbms/tests/queries/0_stateless/00284_external_aggregation.sql
@@ -1,5 +1,8 @@
 SET max_bytes_before_external_group_by = 100000000;
-SET max_memory_usage = 201000000;
+-- In worst case, 256 two-level hash tables could be resized at the same time.
+-- Also, small hash tables have factor 4.
+SET max_memory_usage = 301000000;
 
-SELECT sum(k), sum(c) FROM (SELECT number AS k, count() AS c FROM (SELECT * FROM system.numbers LIMIT 10000000) GROUP BY k);
-SELECT sum(k), sum(c), max(u) FROM (SELECT number AS k, count() AS c, uniqArray(range(number % 16)) AS u FROM (SELECT * FROM system.numbers LIMIT 1000000) GROUP BY k);
+set max_block_size=8192;
+SELECT sum(k), sum(c) FROM (SELECT number AS k, count() AS c FROM (SELECT * FROM system.numbers LIMIT 10000000) GROUP BY k) settings max_block_size=8192;
+SELECT sum(k), sum(c), max(u) FROM (SELECT number AS k, count() AS c, uniqArray(range(number % 16)) AS u FROM (SELECT * FROM system.numbers LIMIT 1000000) GROUP BY k) settings max_block_size=8192;
