52a9341082e6c2c318aa95a8eb48583f06c7944d
Nikolai Kochetov <nik-kochetov@yandex-team.ru>
2019-07-29 13:51:56 +0300

    Update send logs tests.

--- dbms/tests/queries/0_stateless/00965_send_logs_level_concurrent_queries.sh
+++ dbms/tests/queries/0_stateless/00965_send_logs_level_concurrent_queries.sh
@@ -3,12 +3,9 @@
 CURDIR=$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)
 . $CURDIR/../shell_config.sh
 
-> 00965_send_logs_level_concurrent_queries_first.tmp
-> 00965_send_logs_level_concurrent_queries_second.tmp
-
-clickhouse-client --send_logs_level="trace" --query="SELECT * from numbers(100000);" 2>&1 | awk '{ print $8 }' > 00965_send_logs_level_concurrent_queries_first.tmp &
-clickhouse-client --send_logs_level="trace" --query="SELECT * from numbers(100000);" 2>&1 | awk '{ print $8 }' > 00965_send_logs_level_concurrent_queries_second.tmp &
+for i in {1..100}; do
+    $CLICKHOUSE_BINARY client --send_logs_level="trace" --query="SELECT * from numbers(100000);" > /dev/null 2> /dev/null &
+    $CLICKHOUSE_BINARY client --send_logs_level="information" --query="SELECT * from numbers(100000);" 2>&1 | awk '{ print $8 }' | grep "Debug\|Trace" &
+done
 
 wait
-
-diff 00965_send_logs_level_concurrent_queries_first.tmp 00965_send_logs_level_concurrent_queries_second.tmp
