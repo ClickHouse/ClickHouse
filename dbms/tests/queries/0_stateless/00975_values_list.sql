eb53b57a4b89ceeff7536bd90f0f8bb5b5591df1
Dmitry Rubashkin <dimarub2000@sandbox-407922969>
2019-07-29 19:23:22 +0300

    drop table

--- dbms/tests/queries/0_stateless/00975_values_list.sql
+++ dbms/tests/queries/0_stateless/00975_values_list.sql
@@ -8,4 +8,5 @@ SELECT subtractYears(date, 1), subtractYears(date_time, 1) FROM VALUES('date Dat
 
 SELECT * FROM VALUES('s String', ('abra'), ('cadabra'), ('abracadabra'));
 
-SELECT * FROM VALUES('n UInt64, s String, ss String', (1 + 22, '23', toString(23)), (toUInt64('24'), '24', concat('2', '4'))); 
+SELECT * FROM VALUES('n UInt64, s String, ss String', (1 + 22, '23', toString(23)), (toUInt64('24'), '24', concat('2', '4')));
+DROP TABLE values_list;
