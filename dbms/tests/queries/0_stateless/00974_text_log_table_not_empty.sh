6102ca75283b1f21e6e8df21ee6566d29650fbe7
Nikita Mikhaylov <mikhaylovnikitka@gmail.com>
2019-07-31 18:43:55 +0300

    update flappy test

--- dbms/tests/queries/0_stateless/00974_text_log_table_not_empty.sh
+++ dbms/tests/queries/0_stateless/00974_text_log_table_not_empty.sh
@@ -4,6 +4,16 @@ CURDIR=$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)
 . $CURDIR/../shell_config.sh
 
 ${CLICKHOUSE_CLIENT} --query="SELECT 6103"
-sleep 0.1
+
+>00974_text_log_table_not_empty.tmp
+
+for (( i=1; i <= 50; i++ )) 
+do
+
 ${CLICKHOUSE_CLIENT} --query="SYSTEM FLUSH LOGS"
-${CLICKHOUSE_CLIENT} --query="SELECT count() > 0 FROM system.text_log WHERE position(system.text_log.message, 'SELECT 6103') > 0"
+sleep 0.1;
+if [[ $($CLICKHOUSE_CURL -sS "$CLICKHOUSE_URL" -d "SELECT count() > 0 FROM system.text_log WHERE position(system.text_log.message, 'SELECT 6103') > 0") == 1 ]]; then echo 1; exit; fi;
+
+done;
+
+
