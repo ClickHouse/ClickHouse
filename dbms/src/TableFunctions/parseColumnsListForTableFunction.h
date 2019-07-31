a87d1ea57755f3ed5d239efa7c9187f09c90f765
Dmitry Rubashkin <dimarub2000@sandbox-407922969>
2019-07-31 18:58:28 +0300

    Minor style fixes

--- dbms/src/TableFunctions/parseColumnsListForTableFunction.h
+++ dbms/src/TableFunctions/parseColumnsListForTableFunction.h
@@ -5,8 +5,7 @@
 
 namespace DB
 {
-/*Parses a common argument for table functions such as table structure given in string*/
+/// Parses a common argument for table functions such as table structure given in string
 void parseColumnsListFromString(const std::string & structure, Block & sample_block, const Context & context);
 
 }
-
