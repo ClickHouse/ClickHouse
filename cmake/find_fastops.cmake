36906a78b5b0279cf776493068e2526981c7e336
Alexey Milovidov <milovidov@yandex-team.ru>
2019-08-01 03:45:36 +0300

    Fixed typo

--- cmake/find_fastops.cmake
+++ cmake/find_fastops.cmake
@@ -2,7 +2,7 @@ option (ENABLE_FASTOPS "Enable fast vectorized mathematical functions library by
 
 if (ENABLE_FASTOPS)
     if(NOT EXISTS "${ClickHouse_SOURCE_DIR}/contrib/fastops/fastops/fastops.h")
-        message(FATAL_ERROR "submodule contrib/${INTERNAL_ZLIB_NAME} is missing. to fix try run: \n git submodule update --init --recursive")
+        message(FATAL_ERROR "submodule contrib/fastops is missing. to fix try run: \n git submodule update --init --recursive")
         set(USE_FASTOPS 0)
     endif()
     set (USE_FASTOPS 1)
