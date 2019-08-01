7c37450c3d6eda4da4119064e0242168ae7cf16d
alesapin <alesapin@gmail.com>
2019-08-01 20:43:10 +0300

    Better code and tests

--- dbms/src/Core/SettingsCommon.cpp
+++ dbms/src/Core/SettingsCommon.cpp
@@ -4,6 +4,7 @@
 #include <Common/getNumberOfPhysicalCPUCores.h>
 #include <Common/FieldVisitors.h>
 #include <IO/ReadHelpers.h>
+#include <IO/ReadBufferFromString.h>
 #include <IO/WriteHelpers.h>
 
 
@@ -26,6 +27,7 @@ namespace ErrorCodes
     extern const int SIZE_OF_FIXED_STRING_DOESNT_MATCH;
     extern const int BAD_ARGUMENTS;
     extern const int UNKNOWN_SETTING;
+    extern const int CANNOT_PARSE_BOOL;
 }
 
 
@@ -66,12 +68,25 @@ void SettingNumber<Type>::set(const String & x)
 template <>
 void SettingNumber<bool>::set(const String & x)
 {
-    if (x == "false")
-        set(false);
-    else if (x == "true")
-        set(true);
+    if (x.size() == 1)
+    {
+        if (x[0] == '0')
+            set(false);
+        else if (x[0] == '1')
+            set(true);
+        else
+            throw Exception("Cannot parse bool from string '" + x + "'", ErrorCodes::CANNOT_PARSE_BOOL);
+    }
     else
-        set(parse<bool>(x));
+    {
+        ReadBufferFromString buf(x);
+        if (checkStringCaseInsensitive("true", buf))
+            set(true);
+        else if (checkStringCaseInsensitive("false", buf))
+            set(false);
+        else
+            throw Exception("Cannot parse bool from string '" + x + "'", ErrorCodes::CANNOT_PARSE_BOOL);
+    }
 }
 
 template <typename Type>
