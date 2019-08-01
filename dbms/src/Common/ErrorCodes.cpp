49dc271d647051965a0e69611a4814a33809c957
alesapin <alesapin@gmail.com>
2019-08-01 20:43:10 +0300

    Cherry picking

--- dbms/src/Common/ErrorCodes.cpp
+++ dbms/src/Common/ErrorCodes.cpp
@@ -434,6 +434,14 @@ namespace ErrorCodes
     extern const int BAD_QUERY_PARAMETER = 457;
     extern const int CANNOT_UNLINK = 458;
     extern const int CANNOT_SET_THREAD_PRIORITY = 459;
+    extern const int CANNOT_CREATE_TIMER = 460;
+    extern const int CANNOT_SET_TIMER_PERIOD = 461;
+    extern const int CANNOT_DELETE_TIMER = 462;
+    extern const int CANNOT_FCNTL = 463;
+    extern const int CANNOT_PARSE_ELF = 464;
+    extern const int CANNOT_PARSE_DWARF = 465;
+    extern const int INSECURE_PATH = 466;
+    extern const int CANNOT_PARSE_BOOL = 467;
 
     extern const int KEEPER_EXCEPTION = 999;
     extern const int POCO_EXCEPTION = 1000;
