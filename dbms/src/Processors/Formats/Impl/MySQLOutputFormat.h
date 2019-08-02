5ab028f0bc69e0f9b83d4dcc203c6fb57e3a5392
Nikolai Kochetov <nik-kochetov@yandex-team.ru>
2019-08-02 15:41:21 +0300

    Update MySQLOutputFormat.

--- dbms/src/Processors/Formats/Impl/MySQLOutputFormat.h
+++ dbms/src/Processors/Formats/Impl/MySQLOutputFormat.h
@@ -25,13 +25,13 @@ public:
 
     void consume(Chunk) override;
     void finalize() override;
-
+    void flush() override;
 private:
 
     bool initialized = false;
 
     const Context & context;
-    std::shared_ptr<MySQLProtocol::PacketSender> packet_sender;
+    MySQLProtocol::PacketSender packet_sender;
     FormatSettings format_settings;
 };
 
