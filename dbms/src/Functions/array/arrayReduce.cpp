6aa65d926aa866ed702b142084bc74132dd9133e
Nikita Mikhaylov <mikhaylovnikitka@gmail.com>
2019-08-02 15:43:00 +0300

    done

--- dbms/src/Functions/array/arrayReduce.cpp
+++ dbms/src/Functions/array/arrayReduce.cpp
@@ -43,7 +43,7 @@ public:
     bool isVariadic() const override { return true; }
     size_t getNumberOfArguments() const override { return 0; }
 
-    bool useDefaultImplementationForConstants() const override { return true; }
+    bool useDefaultImplementationForConstants() const override { return false; }
     ColumnNumbers getArgumentsThatAreAlwaysConstant() const override { return {0}; }
 
     DataTypePtr getReturnTypeImpl(const ColumnsWithTypeAndName & arguments) const override;
