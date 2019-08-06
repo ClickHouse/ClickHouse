9f05cb553a0adfa920f286c0c04480f5ddcc8939
Alexander Tokmakov <avtokmakov@yandex-team.ru>
2019-08-06 21:54:06 +0300

    refactor throwFromErrno

--- dbms/src/IO/WriteBufferFromFileDescriptor.cpp
+++ dbms/src/IO/WriteBufferFromFileDescriptor.cpp
@@ -56,7 +56,7 @@ void WriteBufferFromFileDescriptor::nextImpl()
         if ((-1 == res || 0 == res) && errno != EINTR)
         {
             ProfileEvents::increment(ProfileEvents::WriteBufferFromFileDescriptorWriteFailed);
-            throwFromErrno("Cannot write to file " + getFileName(), ErrorCodes::CANNOT_WRITE_TO_FILE_DESCRIPTOR);
+            throwFromErrno("Cannot write to file " + getFileName(), getFileName(), ErrorCodes::CANNOT_WRITE_TO_FILE_DESCRIPTOR);
         }
 
         if (res > 0)
@@ -111,7 +111,7 @@ void WriteBufferFromFileDescriptor::sync()
     /// Request OS to sync data with storage medium.
     int res = fsync(fd);
     if (-1 == res)
-        throwFromErrno("Cannot fsync " + getFileName(), ErrorCodes::CANNOT_FSYNC);
+        throwFromErrno("Cannot fsync " + getFileName(), getFileName(), ErrorCodes::CANNOT_FSYNC);
 }
 
 
@@ -119,7 +119,7 @@ off_t WriteBufferFromFileDescriptor::doSeek(off_t offset, int whence)
 {
     off_t res = lseek(fd, offset, whence);
     if (-1 == res)
-        throwFromErrno("Cannot seek through file " + getFileName(), ErrorCodes::CANNOT_SEEK_THROUGH_FILE);
+        throwFromErrno("Cannot seek through file " + getFileName(), getFileName(), ErrorCodes::CANNOT_SEEK_THROUGH_FILE);
     return res;
 }
 
@@ -128,7 +128,7 @@ void WriteBufferFromFileDescriptor::doTruncate(off_t length)
 {
     int res = ftruncate(fd, length);
     if (-1 == res)
-        throwFromErrno("Cannot truncate file " + getFileName(), ErrorCodes::CANNOT_TRUNCATE_FILE);
+        throwFromErrno("Cannot truncate file " + getFileName(), getFileName(), ErrorCodes::CANNOT_TRUNCATE_FILE);
 }
 
 }
