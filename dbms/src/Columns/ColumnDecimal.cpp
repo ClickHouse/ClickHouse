57f8658f03bb44e7e48f12272aef0a72157cbd15
chertus <chertus@gmail.com>
2019-08-07 17:12:57 +0300

    minor perf improvement

--- dbms/src/Columns/ColumnDecimal.cpp
+++ dbms/src/Columns/ColumnDecimal.cpp
@@ -30,6 +30,8 @@ int ColumnDecimal<T>::compareAt(size_t n, size_t m, const IColumn & rhs_, int) c
     const T & a = data[n];
     const T & b = other.data[m];
 
+    if (scale == other.scale)
+        return a > b ? 1 : (a < b ? -1 : 0);
     return decimalLess<T>(b, a, other.scale, scale) ? 1 : (decimalLess<T>(a, b, scale, other.scale) ? -1 : 0);
 }
 
