0d6504ff3361f739b032ab500596ed93bce7ec0f
Alexey Milovidov <milovidov@yandex-team.ru>
2019-08-10 19:47:59 +0300

    One more test

--- dbms/tests/queries/0_stateless/00984_parser_stack_overflow.sh
+++ dbms/tests/queries/0_stateless/00984_parser_stack_overflow.sh
@@ -3,4 +3,8 @@
 CURDIR=$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)
 . $CURDIR/../shell_config.sh
 
+# Too deep recursion
 perl -e 'print "(" x 10000 ' | $CLICKHOUSE_CURL -sS "$CLICKHOUSE_URL" --data-binary @- | grep -oF 'exceeded'
+
+# But this is Ok
+perl -e 'print "SELECT 1" . (",1" x 10000) ' | $CLICKHOUSE_CURL -sS "$CLICKHOUSE_URL" --data-binary @- | wc -c
