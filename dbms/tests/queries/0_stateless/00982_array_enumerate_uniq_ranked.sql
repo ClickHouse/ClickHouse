53ebe745e756aeb725ea671f567f7966c896d9cb
Alexander Tokmakov <avtokmakov@yandex-team.ru>
2019-08-12 23:11:25 +0300

    remove table function Values from test

--- dbms/tests/queries/0_stateless/00982_array_enumerate_uniq_ranked.sql
+++ dbms/tests/queries/0_stateless/00982_array_enumerate_uniq_ranked.sql
@@ -1,1 +1,5 @@
-SELECT arrayEnumerateUniqRanked(x, 2) FROM VALUES('x Array(Array(String))', ([[]]), ([['a'], ['a'], ['b']]), ([['a'], ['a'], ['b']]));
+DROP TABLE IF EXISTS array_enumerate_uniq_ranked;
+CREATE TABLE array_enumerate_uniq_ranked (x Array(Array(String))) Engine=Memory;
+INSERT INTO array_enumerate_uniq_ranked VALUES ([[]]), ([['a'], ['a'], ['b']]), ([['a'], ['a'], ['b']]);
+SELECT arrayEnumerateUniqRanked(x, 2) FROM array_enumerate_uniq_ranked;
+DROP TABLE array_enumerate_uniq_ranked;
