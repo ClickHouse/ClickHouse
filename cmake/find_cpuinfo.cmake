8d3446e51505e1a65db22612edffdb56781afdaf
proller <proller@github.com>
2019-08-13 19:25:54 +0300

    cmake: fix cpuinfo

--- cmake/find_cpuinfo.cmake
+++ cmake/find_cpuinfo.cmake
@@ -1,8 +1,8 @@
 option(USE_INTERNAL_CPUINFO_LIBRARY "Set to FALSE to use system cpuinfo library instead of bundled" ${NOT_UNBUNDLED})
 
 # Now we have no contrib/libcpuinfo, use from system.
-if (USE_INTERNAL_CPUINFO_LIBRARY AND NOT EXISTS "${ClickHouse_SOURCE_DIR}/contrib/libcpuinfo/include")
-   #message (WARNING "submodule contrib/libcpuid is missing. to fix try run: \n git submodule update --init --recursive")
+if (NOT EXISTS "${ClickHouse_SOURCE_DIR}/contrib/libcpuinfo/include")
+   #message (WARNING "submodule contrib/libcpuinfo is missing. to fix try run: \n git submodule update --init --recursive")
    set (USE_INTERNAL_CPUINFO_LIBRARY 0)
    set (MISSING_INTERNAL_CPUINFO_LIBRARY 1)
 endif ()
@@ -12,7 +12,7 @@ if(NOT USE_INTERNAL_CPUINFO_LIBRARY)
     find_path(CPUINFO_INCLUDE_DIR NAMES cpuinfo.h PATHS ${CPUINFO_INCLUDE_PATHS})
 endif()
 
-if(CPUID_LIBRARY AND CPUID_INCLUDE_DIR)
+if(CPUINFO_LIBRARY AND CPUINFO_INCLUDE_DIR)
     set(USE_CPUINFO 1)
 elseif(NOT MISSING_INTERNAL_CPUINFO_LIBRARY)
     set(CPUINFO_INCLUDE_DIR ${ClickHouse_SOURCE_DIR}/contrib/libcpuinfo/include)
