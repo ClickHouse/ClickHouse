457967d1eac71c923e708d6bc76c19e95545a734
Nikita Mikhaylov <mikhaylovnikitka@gmail.com>
2019-08-13 17:40:19 +0300

    better

--- dbms/src/Interpreters/MetricLog.cpp
+++ dbms/src/Interpreters/MetricLog.cpp
@@ -1,8 +1,11 @@
 #include <Interpreters/MetricLog.h>
 #include <DataTypes/DataTypesNumber.h>
 #include <DataTypes/DataTypeString.h>
+<<<<<<< HEAD
 #include <DataTypes/DataTypeDate.h>
 #include <DataTypes/DataTypeDateTime.h>
+=======
+>>>>>>> 46a5ec5c16ed274956cc2051d0d2da213d975051
 
 namespace DB
 {
@@ -26,12 +29,10 @@ Block MetricLogElement::createBlock()
     //CurrentMetrics
     for (size_t i = 0, end = CurrentMetrics::end(); i < end; ++i)
     {
-
         std::string name;
         name += "CurrentMetric_";
         name += CurrentMetrics::getName(ProfileEvents::Event(i));
         columns_with_type_and_name.emplace_back(std::make_shared<DataTypeInt64>(), name);
-
     }
 
     return Block(columns_with_type_and_name);
@@ -50,18 +51,14 @@ void MetricLogElement::appendToBlock(Block & block) const
     for (size_t i = 0, end = ProfileEvents::end(); i < end; ++i)
     {
         UInt64 value = ProfileEvents::global_counters[i];
-
         columns[iter++]->insert(value);
-
     }
 
     //CurrentMetrics
     for (size_t i = 0, end = CurrentMetrics::end(); i < end; ++i)
     {
         UInt64 value = CurrentMetrics::values[i];
-
         columns[iter++]->insert(value);
-
     }
 }
 
