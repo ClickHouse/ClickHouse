1ba635918c2e9a287c9e98a16876b389be5dc348
Nikita Mikhaylov <mikhaylovnikitka@gmail.com>
2019-08-13 19:47:12 +0300

    refactor

--- dbms/src/Interpreters/MetricLog.cpp
+++ dbms/src/Interpreters/MetricLog.cpp
@@ -59,4 +59,46 @@ void MetricLogElement::appendToBlock(Block & block) const
     }
 }
 
+void MetricLog::startCollectMetric(size_t collect_interval_milliseconds_)
+{
+    collect_interval_milliseconds = collect_interval_milliseconds_;
+    is_shutdown_metric_thread = false;
+    metric_flush_thread = ThreadFromGlobalPool([this] { metricThreadFunction(); });
+}
+
+void MetricLog::stopCollectMetric()
+{
+    bool old_val = false;
+    if (!is_shutdown_metric_thread.compare_exchange_strong(old_val, true))
+        return;
+    metric_flush_thread.join();
+}
+
+void MetricLog::metricThreadFunction()
+{
+    const size_t flush_interval_milliseconds = 1000;
+    while (true)
+    {
+        try
+        {
+            const auto prev_timepoint = std::chrono::system_clock::now();
+
+            if (is_shutdown_metric_thread)
+                break;
+
+            MetricLogElement elem;
+            elem.event_time = std::time(nullptr);
+            this->add(elem);
+
+            const auto next_timepoint = prev_timepoint + std::chrono::milliseconds(flush_interval_milliseconds);
+            std::this_thread::sleep_until(next_timepoint);
+        }
+        catch (...)
+        {
+            tryLogCurrentException(__PRETTY_FUNCTION__);
+        }
+
+    }
+}
+
 }
--- dbms/src/Interpreters/MetricLog.h
+++ dbms/src/Interpreters/MetricLog.h
@@ -19,6 +19,20 @@ struct MetricLogElement
 class MetricLog : public SystemLog<MetricLogElement>
 {
     using SystemLog<MetricLogElement>::SystemLog;
+
+public:
+    /// Launches a background thread to collect metrics with interval
+    void startCollectMetric(size_t collect_interval_milliseconds_);
+
+    /// Stop background thread. Call before shutdown.
+    void stopCollectMetric();
+
+private:
+    void metricThreadFunction();
+
+    ThreadFromGlobalPool metric_flush_thread;
+    size_t collect_interval_milliseconds;
+    std::atomic<bool> is_shutdown_metric_thread{false};
 };
 
 }
