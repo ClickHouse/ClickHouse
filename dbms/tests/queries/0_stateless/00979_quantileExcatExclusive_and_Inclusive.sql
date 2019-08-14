76432d83be9151062e20fed9618aff64bff88f1a
Dmitry Rubashkin <dimarub2000@sandbox-407922969>
2019-08-14 16:47:48 +0300

    Tests fixed

--- dbms/tests/queries/0_stateless/00979_quantileExcatExclusive_and_Inclusive.sql
+++ dbms/tests/queries/0_stateless/00979_quantileExcatExclusive_and_Inclusive.sql
@@ -6,7 +6,7 @@ SELECT quantilesExactInclusive(0.25, 0.5, 0.75, 0.9, 0.95, 0.99, 0.999)(x) FROM 
 SELECT quantilesExact(0.25, 0.5, 0.75, 0.9, 0.95, 0.99, 0.999)(x) FROM (SELECT number AS x FROM num);
 
 SELECT quantileExactExclusive(0.6)(x) FROM (SELECT number AS x FROM num);
-SELECT quantileExactExclusive(0.6)(x) FROM (SELECT number AS x FROM num);
+SELECT quantileExactInclusive(0.6)(x) FROM (SELECT number AS x FROM num);
 SELECT quantileExact(0.6)(x) FROM (SELECT number AS x FROM num);
 
 DROP TABLE num;
