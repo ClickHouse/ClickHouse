36e675fd3d398ea172506b29dbacc9e965249ec8
alesapin <alesapin@gmail.com>
2019-08-19 14:18:58 +0300

    Simplify lock

--- dbms/src/Storages/MergeTree/MergeTreeData.cpp
+++ dbms/src/Storages/MergeTree/MergeTreeData.cpp
@@ -1645,7 +1645,7 @@ void MergeTreeData::alterSettings(
         const Context & context,
         TableStructureWriteLockHolder & table_lock_holder)
 {
-    std::unique_lock lock(settings_mutex);
+    std::lock_guard lock(settings_mutex);
     MutableMergeTreeSettingsPtr settings = std::move(*guarded_settings.getPtr()).mutate();
     settings->updateFromChanges(new_changes);
     IStorage::alterSettings(new_changes, current_database_name, current_table_name, context, table_lock_holder);
