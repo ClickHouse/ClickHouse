17e93b8f2d9672616a2e5b15148258467886a423
Alexey Milovidov <milovidov@yandex-team.ru>
2019-08-21 03:48:34 +0300

    Disable ELF on Mac OS #6429

--- dbms/src/Common/Elf.h
+++ dbms/src/Common/Elf.h
@@ -1,44 +1,15 @@
 #pragma once
 
+#ifdef __ELF__
+
 #include <IO/MMapReadBufferFromFile.h>
 
 #include <string>
 #include <optional>
 #include <functional>
 
-#ifdef __APPLE__
-
-#include <libelf/gelf.h>
-
-typedef Elf64_Addr GElf_Addr;
-typedef Elf64_Half GElf_Half;
-typedef Elf64_Off GElf_Off;
-typedef Elf64_Sword GElf_Sword;
-typedef Elf64_Word GElf_Word;
-typedef Elf64_Sxword GElf_Sxword;
-typedef Elf64_Xword GElf_Xword;
-
-typedef Elf64_Ehdr GElf_Ehdr;
-typedef Elf64_Phdr GElf_Phdr;
-typedef Elf64_Shdr GElf_Shdr;
-typedef Elf64_Dyn GElf_Dyn;
-typedef Elf64_Rel GElf_Rel;
-typedef Elf64_Rela GElf_Rela;
-typedef Elf64_Sym GElf_Sym;
-
-#define DT_GNU_HASH	0x6ffffef5 /* GNU-style hash table. */
-
-#define __ELF_NATIVE_CLASS __WORDSIZE
-#define ElfW(type)         _ElfW(Elf, __ELF_NATIVE_CLASS, type)
-#define _ElfW(e, w, t)     _ElfW_1(e, w, _##t)
-#define _ElfW_1(e, w, t)   e##w##t
-
-#else
-
 #include <elf.h>
 #include <link.h>
-
-#endif
 
 
 using ElfAddr = ElfW(Addr);
@@ -92,3 +63,5 @@ private:
 };
 
 }
+
+#endif
