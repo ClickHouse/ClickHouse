c63eeb8933e5a6d39cfc9d2bef3b6c8ddb2a57f3
CurtizJ <pad11rus@gmail.com>
2019-08-26 17:54:29 +0300

    add FinishSortingTransform, refactor MergeSortingTransform

---
+++ dbms/src/Processors/Transforms/FinishSortingTransform.h
@@ -0,0 +1,37 @@
+#pragma once
+
+#include <Core/SortDescription.h>
+#include <Processors/IProcessor.h>
+#include <Processors/Transforms/MergeSortingTransform.h>
+
+namespace DB
+{
+
+/** Takes stream already sorted by `x` and finishes sorting it by (`x`, `y`).
+ *  During sorting only chunks with rows that equal by `x` saved in RAM.
+ * */
+class FinishSortingTransform : public SortingTransform
+{
+public:
+    /// limit - if not 0, allowed to return just first 'limit' rows in sorted order.
+    FinishSortingTransform(const Block & header, const SortDescription & description_sorted_,
+        const SortDescription & description_to_sort_,
+        size_t max_merged_block_size_, UInt64 limit_);
+
+    String getName() const override { return "FinishSortingTransform"; }
+
+protected:
+    void consume(Chunk chunk) override;
+    void generate() override;
+
+private:
+    SortDescription description_sorted;
+
+    Chunk tail_chunk;
+    // Chunks chunks;
+
+    bool end_of_stream = false;
+    size_t total_rows_processed = 0;
+};
+
+}
