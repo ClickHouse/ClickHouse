4d5a0631e257db542151da4564d1b306e9a64b3e
Dmitry Rubashkin <dimarub2000@sandbox-407922969>
2019-08-26 15:47:19 +0300

    Try to fix

--- dbms/src/Common/TerminalSize.cpp
+++ dbms/src/Common/TerminalSize.cpp
@@ -3,15 +3,13 @@
 #include <Common/Exception.h>
 #include <Common/TerminalSize.h>
 
-namespace po = boost::program_options;
-
 
 namespace DB::ErrorCodes
 {
     extern const int SYSTEM_ERROR;
 }
 
-unsigned short int getTerminalWidth()
+uint16_t getTerminalWidth()
 {
     if (isatty(STDIN_FILENO))
     {
@@ -25,13 +23,13 @@ unsigned short int getTerminalWidth()
     return 0;
 }
 
-po::options_description createOptionsDescription(const std::string &caption, unsigned short terminal_width)
+po::options_description createOptionsDescription(const std::string &caption, uint16_t terminal_width)
 {
     unsigned line_length = po::options_description::m_default_line_length;
     unsigned min_description_length = line_length / 2;
     std::string longest_option_desc = "--http_native_compression_disable_checksumming_on_decompress";
 
-    line_length = std::max(static_cast<unsigned short>(longest_option_desc.size()), terminal_width);
+    line_length = std::max(static_cast<uint16_t>(longest_option_desc.size()), terminal_width);
     min_description_length = std::min(min_description_length, line_length - 2);
 
     return po::options_description(caption, line_length, min_description_length);
