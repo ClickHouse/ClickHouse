a169213691521e4f3debc288f2fa1e6ef6394303
CurtizJ <pad11rus@gmail.com>
2019-08-27 21:11:02 +0300

    test for limit with ties

--- dbms/tests/queries/0_stateless/00996_limit_with_ties.sql
+++ dbms/tests/queries/0_stateless/00996_limit_with_ties.sql
@@ -1,7 +1,9 @@
 DROP TABLE IF EXISTS ties;
 CREATE TABLE ties (a Int) ENGINE = Memory;
 
-INSERT INTO ties VALUES (1), (1), (2), (2), (2), (3), (3);
+-- SET experimental_use_processors=1;
+
+INSERT INTO ties VALUES (1), (1), (2), (2), (2), (2) (3), (3);
 
 SELECT a FROM ties order by a limit 1 with ties;
 SELECT '*';
@@ -11,36 +13,23 @@ SELECT a FROM ties order by a limit 5 with ties;
 SELECT '*';
 
 SET max_block_size = 2;
-SELECT a FROM ties order by a limit 1 with ties;
+SELECT a FROM ties order by a limit 1, 1 with ties;
+SELECT '*';
+SELECT a FROM ties order by a limit 1, 2 with ties;
 SELECT '*';
 SELECT a FROM ties order by a limit 2 with ties;
 SELECT '*';
-SELECT a FROM ties order by a limit 3 with ties;
+SELECT a FROM ties order by a limit 2, 3 with ties;
 SELECT '*';
 SELECT a FROM ties order by a limit 4 with ties;
-SELECT '*';
-SELECT a FROM ties order by a limit 6 with ties;
 SELECT '*';
 
 SET max_block_size = 3;
 SELECT a FROM ties order by a limit 1 with ties;
 SELECT '*';
-SELECT a FROM ties order by a limit 2 with ties;
+SELECT a FROM ties order by a limit 2, 3 with ties;
 SELECT '*';
-SELECT a FROM ties order by a limit 3 with ties;
-SELECT '*';
-SELECT a FROM ties order by a limit 4 with ties;
-SELECT '*';
-SELECT a FROM ties order by a limit 6 with ties;
-SELECT '*';
-
-
-SET max_block_size = 1;
-SELECT a FROM ties order by a limit 3 with ties;
-SELECT '*';
-SELECT a FROM ties order by a limit 5 with ties;
-SELECT '*';
-SELECT a FROM ties order by a limit 6 with ties;
+SELECT a FROM ties order by a limit 3, 2 with ties;
 SELECT '*';
 
 DROP TABLE ties;
