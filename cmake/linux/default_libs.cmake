3aad5596708c1d938c6ada4667a7837748af40f2
Ivan Lezhankin <ilezhankin@yandex-team.ru>
2019-08-30 19:50:43 +0300

    Refactor some cmake files

--- cmake/linux/default_libs.cmake
+++ cmake/linux/default_libs.cmake
@@ -3,19 +3,17 @@
 
 set (DEFAULT_LIBS "-nodefaultlibs")
 
-if (OS_LINUX)
-    # We need builtins from Clang's RT even without libcxx - for ubsan+int128.
-    # See https://bugs.llvm.org/show_bug.cgi?id=16404
-    if (COMPILER_CLANG)
-        execute_process (COMMAND ${CMAKE_CXX_COMPILER} --print-file-name=libclang_rt.builtins-${CMAKE_SYSTEM_PROCESSOR}.a OUTPUT_VARIABLE BUILTINS_LIBRARY OUTPUT_STRIP_TRAILING_WHITESPACE)
-    else ()
-        set (BUILTINS_LIBRARY "-lgcc")
-    endif ()
-
-    set (DEFAULT_LIBS "${DEFAULT_LIBS} ${BUILTINS_LIBRARY} ${COVERAGE_OPTION} -lc -lm -lrt -lpthread -ldl")
-
-    message(STATUS "Default libraries: ${DEFAULT_LIBS}")
+# We need builtins from Clang's RT even without libcxx - for ubsan+int128.
+# See https://bugs.llvm.org/show_bug.cgi?id=16404
+if (COMPILER_CLANG)
+    execute_process (COMMAND ${CMAKE_CXX_COMPILER} --print-file-name=libclang_rt.builtins-${CMAKE_SYSTEM_PROCESSOR}.a OUTPUT_VARIABLE BUILTINS_LIBRARY OUTPUT_STRIP_TRAILING_WHITESPACE)
+else ()
+    set (BUILTINS_LIBRARY "-lgcc")
 endif ()
+
+set (DEFAULT_LIBS "${DEFAULT_LIBS} ${BUILTINS_LIBRARY} ${COVERAGE_OPTION} -lc -lm -lrt -lpthread -ldl")
+
+message(STATUS "Default libraries: ${DEFAULT_LIBS}")
 
 set(CMAKE_CXX_STANDARD_LIBRARIES ${DEFAULT_LIBS})
 set(CMAKE_C_STANDARD_LIBRARIES ${DEFAULT_LIBS})
@@ -42,6 +40,7 @@ target_link_libraries(global-group INTERFACE
 
 link_libraries(global-group)
 
+# FIXME: remove when all contribs will get custom cmake lists
 install(
     TARGETS global-group global-libs
     EXPORT global
