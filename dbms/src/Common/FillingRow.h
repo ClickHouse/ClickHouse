756b5c2f012fffc0d3a7f7c783046a7452d2345a
CurtizJ <pad11rus@gmail.com>
2019-09-02 20:18:44 +0300

    style fixes

--- dbms/src/Common/FillingRow.h
+++ dbms/src/Common/FillingRow.h
@@ -10,7 +10,7 @@ namespace DB
 bool less(const Field & lhs, const Field & rhs, int direction);
 bool equals(const Field & lhs, const Field & rhs);
 
-/** Helps to implement modifier WITH FILL for ORDER BY clause. 
+/** Helps to implement modifier WITH FILL for ORDER BY clause.
  *  Stores row as array of fields and provides functions to generate next row for filling gaps and for comparing rows.
  *  Used in FillingBlockInputStream and in FillingTransform.
  */
