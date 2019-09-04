4de1740e0f0a18fbc459f9a8915c4babb721a12c
Alexander Tokmakov <avtokmakov@yandex-team.ru>
2019-09-04 22:42:01 +0300

    improvements

--- dbms/src/Core/Settings.h
+++ dbms/src/Core/Settings.h
@@ -177,6 +177,7 @@ struct Settings : public SettingsCollection<Settings>
     \
     M(SettingBool, input_format_values_interpret_expressions, true, "For Values format: if field could not be parsed by streaming parser, run SQL parser and try to interpret it as SQL expression.") \
     M(SettingBool, input_format_values_deduce_templates_of_expressions, true, "For Values format: if field could not be parsed by streaming parser, run SQL parser, deduce template of the SQL expression, try to parse all rows using template and then interpret expression for all rows.") \
+    M(SettingBool, input_format_values_accurate_types_of_literals, true, "For Values format: when parsing and interpreting expressions using template, check actual type of literal to avoid possible overflow and precision issues.") \
     \
     M(SettingBool, output_format_json_quote_64bit_integers, true, "Controls quoting of 64-bit integers in JSON output format.") \
     \
--- dbms/src/Processors/Formats/Impl/ValuesBlockInputFormat.h
+++ dbms/src/Processors/Formats/Impl/ValuesBlockInputFormat.h
@@ -24,6 +24,9 @@ public:
     /** Data is parsed using fast, streaming parser.
       * If interpret_expressions is true, it will, in addition, try to use SQL parser and interpreter
       *  in case when streaming parser could not parse field (this is very slow).
+      * If deduce_templates_of_expressions is true, try to deduce template of expression in some row and use it
+      * to parse and interpret expressions in other rows (in most cases it's faster
+      * than interpreting expressions in each row separately, but it's still slower than streaming parsing)
       */
     ValuesBlockInputFormat(ReadBuffer & in_, const Block & header_, const RowInputFormatParams & params_,
                            const Context & context_, const FormatSettings & format_settings_);
@@ -34,13 +37,15 @@ private:
     typedef std::vector<std::optional<ConstantExpressionTemplate>> ConstantExpressionTemplates;
 
     Chunk generate() override;
+
     bool parseExpressionUsingTemplate(MutableColumnPtr & column, size_t column_idx);
-    void readValue(IColumn & column, size_t column_idx, bool generate_template);
+    void readValueOrParseSeparateExpression(IColumn & column, size_t column_idx);
+    void parseExpression(IColumn & column, size_t column_idx, bool deduce_template);
 
-    void parseExpression(IColumn & column, size_t column_idx, bool generate_template);
     inline void assertDelimiterAfterValue(size_t column_idx);
+    inline bool checkDelimiterAfterValue(size_t column_idx);
 
-    bool shouldGenerateNewTemplate(size_t column_idx);
+    bool shouldDeduceNewTemplate(size_t column_idx);
 
     void readSuffix() { buf.assertCanBeDestructed(); }
 
@@ -56,7 +61,7 @@ private:
     size_t num_columns;
     size_t total_rows = 0;
 
-    std::vector<size_t> attempts_to_generate_template;
+    std::vector<size_t> attempts_to_deduce_template;
     std::vector<size_t> rows_parsed_using_template;
 
     ParserExpression parser;
