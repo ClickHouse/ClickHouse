be5c099e02dd3ec7b87744f5a029aa12ad074ab2
proller <proller@github.com>
2019-09-05 19:13:25 +0300

    wip

--- cmake/find_orc.cmake
+++ cmake/find_orc.cmake
@@ -6,3 +6,10 @@ set(USE_INTERNAL_ORC_LIBRARY ON)
 if (ARROW_LIBRARY)
     set(USE_ORC 1)
 endif()
+
+
+find_path(CYRUS_SASL_INCLUDE_DIR sasl/sasl.h)
+find_library(CYRUS_SASL_SHARED_LIB sasl2)
+if (NOT CYRUS_SASL_INCLUDE_DIR OR NOT CYRUS_SASL_SHARED_LIB)
+    set(USE_ORC 0)
+endif()
