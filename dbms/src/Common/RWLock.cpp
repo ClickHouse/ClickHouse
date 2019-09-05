8df58a3b7b0b7f387069f733c8c1bd718f6b553c
akazz <akazz@github.com>
2019-09-05 18:14:17 +0300

    Refactored Clients -> Referers

--- dbms/src/Common/RWLock.cpp
+++ dbms/src/Common/RWLock.cpp
@@ -39,11 +39,10 @@ class RWLockImpl::LockHolderImpl
 {
     RWLock parent;
     GroupsContainer::iterator it_group;
-    ClientsContainer::iterator it_client;
     QueryIdToHolder::key_type query_id;
     CurrentMetrics::Increment active_client_increment;
 
-    LockHolderImpl(RWLock && parent, GroupsContainer::iterator it_group, ClientsContainer::iterator it_client);
+    LockHolderImpl(RWLock && parent, GroupsContainer::iterator it_group);
 
 public:
 
@@ -114,6 +113,7 @@ RWLockImpl::LockHolder RWLockImpl::getLock(RWLockImpl::Type type, const String &
     std::unique_lock<std::mutex> lock(mutex);
 
     /// Check if the same query is acquiring previously acquired lock
+    /// This FastPath tries to create a "light copy" of a lock without creating a separate LockHolderImpl
     if (request_has_query_id)
     {
         const auto it_query = query_id_to_holder.find(query_id);
@@ -163,7 +163,7 @@ RWLockImpl::LockHolder RWLockImpl::getLock(RWLockImpl::Type type, const String &
         if (type == Type::Read && request_has_query_id && !queue.empty())
             all_read_locks.check(query_id);
 
-        /// Create new group of clients
+        /// Create a new group of locking requests
         it_group = queue.emplace(queue.end(), type);
     }
     else
@@ -174,23 +174,8 @@ RWLockImpl::LockHolder RWLockImpl::getLock(RWLockImpl::Type type, const String &
         /// Will append myself to last group
         it_group = std::prev(queue.end());
     }
-
-    ClientsContainer::iterator it_client;
-    /// Append myself to the end of chosen group
-    auto & clients = it_group->clients;
-    try
-    {
-        it_client = clients.emplace(clients.end(), type);
-    }
-    catch (...)
-    {
-        /// Remove group if it was the first client in the group and an error occurred
-        if (clients.empty())
-            queue.erase(it_group);
-        throw;
-    }
-
-    res.reset(new LockHolderImpl(shared_from_this(), it_group, it_client));
+    ++it_group->referers;
+    res.reset(new LockHolderImpl(shared_from_this(), it_group));
 
     /// Wait a notification until we will be the only in the group.
     it_group->cv.wait(lock, [&] () { return it_group == queue.begin(); });
@@ -202,8 +187,8 @@ RWLockImpl::LockHolder RWLockImpl::getLock(RWLockImpl::Type type, const String &
 
         if (type == Type::Read)
             all_read_locks.add(query_id);
+        res->query_id = query_id;
     }
-    res->query_id = query_id;
 
     finalize_metrics();
     return res;
@@ -217,14 +202,11 @@ RWLockImpl::LockHolderImpl::~LockHolderImpl()
     /// Remove weak_ptrs to the holder, since there are no owners of the current lock
     parent->query_id_to_holder.erase(query_id);
 
-    if (*it_client == RWLockImpl::Read && query_id != RWLockImpl::NO_QUERY)
+    if (it_group->type == RWLockImpl::Read && query_id != RWLockImpl::NO_QUERY)
         all_read_locks.remove(query_id);
 
-    /// Removes myself from client list of our group
-    it_group->clients.erase(it_client);
-
-    /// Remove the group if we were the last client and notify the next group
-    if (it_group->clients.empty())
+    /// Remove the group if we were the last referer and notify the next group
+    if (--it_group->referers == 0)
     {
         auto & parent_queue = parent->queue;
         parent_queue.erase(it_group);
@@ -235,10 +217,9 @@ RWLockImpl::LockHolderImpl::~LockHolderImpl()
 }
 
 
-RWLockImpl::LockHolderImpl::LockHolderImpl(RWLock && parent_, RWLockImpl::GroupsContainer::iterator it_group_,
-                                             RWLockImpl::ClientsContainer::iterator it_client_)
-    : parent{std::move(parent_)}, it_group{it_group_}, it_client{it_client_},
-      active_client_increment{(*it_client == RWLockImpl::Read) ? CurrentMetrics::RWLockActiveReaders
+RWLockImpl::LockHolderImpl::LockHolderImpl(RWLock && parent_, RWLockImpl::GroupsContainer::iterator it_group_)
+    : parent{std::move(parent_)}, it_group{it_group_},
+      active_client_increment{(it_group_->type == RWLockImpl::Read) ? CurrentMetrics::RWLockActiveReaders
                                                                : CurrentMetrics::RWLockActiveWriters}
 {
 }
