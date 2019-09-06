9b722522e4f5a2e0312d6989ef163f878979e74b
proller <proller@github.com>
2019-09-06 23:15:00 +0300

    Try fix

--- cmake/find_hdfs3.cmake
+++ cmake/find_hdfs3.cmake
@@ -2,17 +2,17 @@ if (NOT ARCH_ARM AND NOT OS_FREEBSD AND NOT APPLE AND USE_PROTOBUF)
     option (ENABLE_HDFS "Enable HDFS" 1)
 endif ()
 
-
 if (ENABLE_HDFS)
 option(USE_INTERNAL_HDFS3_LIBRARY "Set to FALSE to use system HDFS3 instead of bundled" ${NOT_UNBUNDLED})
 
-if (ENABLE_HDFS AND NOT EXISTS "${ClickHouse_SOURCE_DIR}/contrib/libhdfs3/include/hdfs/hdfs.h")
+if (NOT EXISTS "${ClickHouse_SOURCE_DIR}/contrib/libhdfs3/include/hdfs/hdfs.h")
    message (WARNING "submodule contrib/libhdfs3 is missing. to fix try run: \n git submodule update --init --recursive")
    set (USE_INTERNAL_HDFS3_LIBRARY 0)
 endif ()
 
 if (NOT USE_INTERNAL_HDFS3_LIBRARY)
-    find_package(hdfs3)
+    find_library(HDFS3_LIBRARY hdfs3)
+    find_path(HDFS3_INCLUDE_DIR NAMES hdfs/hdfs.h PATHS ${HDFS3_INCLUDE_PATHS})
 endif ()
 
 if (HDFS3_LIBRARY AND HDFS3_INCLUDE_DIR)
