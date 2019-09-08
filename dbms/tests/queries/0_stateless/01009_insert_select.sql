4227cb8134e02178a6f6da97f9b470935743da93
root <root@server50-ke.mars.ljnode.com>
2019-09-08 18:48:26 +0800

    add test.

---
+++ dbms/tests/queries/0_stateless/01009_insert_select.sql
@@ -0,0 +1,19 @@
+CREATE TABLE test.test_insert_t1 (`dt` Date, `uid` String, `name` String, `city` String) ENGINE = MergeTree PARTITION BY toYYYYMMDD(dt) ORDER BY name SETTINGS index_granularity = 8192;
+CREATE TABLE test.test_insert_t2 (`dt` Date, `uid` String) ENGINE = MergeTree PARTITION BY toYYYYMMDD(dt) ORDER BY uid SETTINGS index_granularity = 8192;
+CREATE TABLE test.test_insert_t3 (`dt` Date, `uid` String, `name` String, `city` String) ENGINE = MergeTree PARTITION BY toYYYYMMDD(dt) ORDER BY name SETTINGS index_granularity = 8192;
+
+INSERT INTO test.test_insert_t1 SELECT '2019-09-01',toString(number),toString(rand()),toString(rand()) FROM system.numbers WHERE number > 10 limit 1000000;
+INSERT INTO test.test_insert_t2 SELECT '2019-09-01',toString(number) FROM system.numbers WHERE number >=0 limit 200;
+INSERT INTO test.test_insert_t2 SELECT '2019-09-01',toString(number) FROM system.numbers WHERE number >=100000 limit 200;
+INSERT INTO test.test_insert_t2 SELECT '2019-09-01',toString(number) FROM system.numbers WHERE number >=300000 limit 200;
+INSERT INTO test.test_insert_t2 SELECT '2019-09-01',toString(number) FROM system.numbers WHERE number >=500000 limit 200;
+INSERT INTO test.test_insert_t2 SELECT '2019-09-01',toString(number) FROM system.numbers WHERE number >=700000 limit 200;
+INSERT INTO test.test_insert_t2 SELECT '2019-09-01',toString(number) FROM system.numbers WHERE number >=900000 limit 200;
+
+INSERT INTO test.test_insert_t3 SELECT '2019-09-01', uid, name, city FROM ( SELECT dt, uid, name, city FROM test.test_insert_t1 WHERE dt = '2019-09-01') t1 GLOBAL ANY INNER JOIN (SELECT uid FROM test.test_insert_t2 WHERE dt = '2019-09-01') t2 on t1.uid=t2.uid;
+
+SELECT count(*) from test.test_insert_t3;
+
+DROP TABLE test.test_insert_t1;
+DROP TABLE test.test_insert_t2;
+DROP TABLE test.test_insert_t3;
