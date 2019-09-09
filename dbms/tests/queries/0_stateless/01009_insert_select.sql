0c8605033918bebf6433122a10012d29bf5e0d71
root <root@server50-ke.mars.ljnode.com>
2019-09-09 11:21:19 +0800

    better stateless test 01009_insert_select and fmt

--- dbms/tests/queries/0_stateless/01009_insert_select.sql
+++ dbms/tests/queries/0_stateless/01009_insert_select.sql
@@ -1,4 +1,7 @@
 Set any_join_distinct_right_table_keys=1;
+DROP TABLE IF EXISTS test.test_insert_t1;
+DROP TABLE IF EXISTS test.test_insert_t2;
+DROP TABLE IF EXISTS test.test_insert_t3;
 CREATE TABLE test.test_insert_t1 (`dt` Date, `uid` String, `name` String, `city` String) ENGINE = MergeTree PARTITION BY toYYYYMMDD(dt) ORDER BY name SETTINGS index_granularity = 8192;
 CREATE TABLE test.test_insert_t2 (`dt` Date, `uid` String) ENGINE = MergeTree PARTITION BY toYYYYMMDD(dt) ORDER BY uid SETTINGS index_granularity = 8192;
 CREATE TABLE test.test_insert_t3 (`dt` Date, `uid` String, `name` String, `city` String) ENGINE = MergeTree PARTITION BY toYYYYMMDD(dt) ORDER BY name SETTINGS index_granularity = 8192;
@@ -13,7 +16,7 @@ INSERT INTO test.test_insert_t2 SELECT '2019-09-01',toString(number) FROM system
 
 INSERT INTO test.test_insert_t3 SELECT '2019-09-01', uid, name, city FROM ( SELECT dt, uid, name, city FROM test.test_insert_t1 WHERE dt = '2019-09-01') t1 GLOBAL ANY INNER JOIN (SELECT uid FROM test.test_insert_t2 WHERE dt = '2019-09-01') t2 on t1.uid=t2.uid;
 
-SELECT count(*) from test.test_insert_t3;
+SELECT count(*) FROM test.test_insert_t3;
 
 DROP TABLE test.test_insert_t1;
 DROP TABLE test.test_insert_t2;
