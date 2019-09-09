c9c0c17de5ea24d762ddea3aa3510de5b06c3bfc
root <root@server50-ke.mars.ljnode.com>
2019-09-09 10:57:25 +0800

    fix stateless test 01009_insert_select

--- dbms/tests/queries/0_stateless/01009_insert_select.sql
+++ dbms/tests/queries/0_stateless/01009_insert_select.sql
@@ -1,3 +1,4 @@
+Set any_join_distinct_right_table_keys=1;
 CREATE TABLE test.test_insert_t1 (`dt` Date, `uid` String, `name` String, `city` String) ENGINE = MergeTree PARTITION BY toYYYYMMDD(dt) ORDER BY name SETTINGS index_granularity = 8192;
 CREATE TABLE test.test_insert_t2 (`dt` Date, `uid` String) ENGINE = MergeTree PARTITION BY toYYYYMMDD(dt) ORDER BY uid SETTINGS index_granularity = 8192;
 CREATE TABLE test.test_insert_t3 (`dt` Date, `uid` String, `name` String, `city` String) ENGINE = MergeTree PARTITION BY toYYYYMMDD(dt) ORDER BY name SETTINGS index_granularity = 8192;
