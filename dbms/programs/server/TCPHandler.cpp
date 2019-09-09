ed308fe25ff26d1405d248ec3cf96d3b12794c26
Nikolai Kochetov <nik-kochetov@yandex-team.ru>
2019-09-09 17:57:12 +0300

    Update TCPHandler

--- dbms/programs/server/TCPHandler.cpp
+++ dbms/programs/server/TCPHandler.cpp
@@ -597,7 +597,15 @@ void TCPHandler::processOrdinaryQueryWithProcessors(size_t num_threads)
                 lazy_format->finish();
                 lazy_format->clearQueue();
 
-                pool.wait();
+                try
+                {
+                    pool.wait();
+                }
+                catch (...)
+                {
+                    /// If exception was thrown during pipeline execution, skip it while processing other exception.
+                }
+
                 pipeline = QueryPipeline()
         );
 
