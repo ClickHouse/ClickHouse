d1a96616ad1b511b2fd37d92a305313f96673460
Ivan Lezhankin <ilezhankin@yandex-team.ru>
2019-09-19 20:59:56 +0300

    Implement auto-labelling for the backporting script.

--- utils/github/__main__.py
+++ utils/github/__main__.py
@@ -20,12 +20,17 @@
 '''
 
 from . import local, query
-
-from termcolor import colored  # `pip install termcolor`
+from . import parser as parse_description
 
 import argparse
 import re
 import sys
+
+try:
+    from termcolor import colored  # `pip install termcolor`
+except ImportError:
+    sys.exit("Package 'termcolor' not found. Try run: `pip3 install termcolor`")
+
 
 CHECK_MARK = colored('🗸', 'green')
 CROSS_MARK = colored('🗙', 'red')
@@ -44,6 +49,8 @@ parser.add_argument('--token', type=str, required=True,
     help='token for Github access')
 parser.add_argument('--login', type=str,
     help='filter authorship by login')
+parser.add_argument('--auto-label', action='store_true', dest='autolabel',
+    help='try to automatically parse PR description and put labels')
 
 args = parser.parse_args()
 
@@ -83,14 +90,23 @@ def print_responsible(pull_request):
 bad_pull_requests = []  # collect and print if not empty
 need_backporting = []
 for pull_request in pull_requests:
-    label_found = False
 
-    for label in github.get_labels(pull_request):
-        if label['name'].startswith('pr-'):
-            label_found = True
-            if label['color'] == 'ff0000':
-                need_backporting.append(pull_request)
-            break
+    def find_label():
+        for label in github.get_labels(pull_request):
+            if label['name'].startswith('pr-'):
+                if label['color'] == 'ff0000':
+                    need_backporting.append(pull_request)
+                return True
+        return False
+
+    label_found = find_label()
+
+    if not label_found and args.autolabel:
+        print(f"Trying to auto-label pull-request: {pull_request['number']}")
+        description = parse_description.Description(pull_request)
+        if description.label_name:
+            github.set_label(pull_request, description.label_name)
+            label_found = find_label()
 
     if not label_found:
         bad_pull_requests.append(pull_request)
