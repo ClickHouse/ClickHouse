e1176e7b20a01cd23c0582f4a037a96005e5441a
alesapin <alesapin@gmail.com>
2019-09-20 22:57:09 +0300

    Better shared binary build

--- docker/packager/binary/build.sh
+++ docker/packager/binary/build.sh
@@ -14,3 +14,6 @@ mv ./dbms/programs/clickhouse* /output
 mv ./dbms/unit_tests_dbms /output
 find . -name '*.so' -print -exec mv '{}' /output \;
 find . -name '*.so.*' -print -exec mv '{}' /output \;
+tar -czvf shared_build.tgz /output
+rm -r /output/*
+mv shared_build.tgz /output
