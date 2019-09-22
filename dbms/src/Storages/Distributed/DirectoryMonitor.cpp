9533ff12ddd5d2fab161381d27ed3c964f4f1c9d
alexey-milovidov <milovidov@yandex-team.ru>
2019-09-22 05:12:55 +0300

    Merge pull request #7032 from azat-archive/distributed-directory-monitor-SIGSEGV
    Avoid SIGSEGV on batch send failure (file with index XX is absent)
    
    (cherry picked from commit 5970aafd9aa5f1c885365297dfdb3d10fec8fdb8)

--- dbms/src/Storages/Distributed/DirectoryMonitor.cpp
+++ dbms/src/Storages/Distributed/DirectoryMonitor.cpp
@@ -390,7 +390,8 @@ struct StorageDistributedDirectoryMonitor::Batch
                 remote->writePrepared(in);
             }
 
-            remote->writeSuffix();
+            if (remote)
+                remote->writeSuffix();
         }
         catch (const Exception & e)
         {
