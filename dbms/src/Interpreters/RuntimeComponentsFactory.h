903f826640e3bc1a2ba834aaba571e22b2320086
alesapin <alesapin@gmail.com>
2019-09-26 13:08:38 +0300

    Remove runtime factory and remove one redundant object

--- dbms/src/Interpreters/RuntimeComponentsFactory.h
+++ dbms/src/Interpreters/RuntimeComponentsFactory.h
@@ -2,7 +2,6 @@
 
 #include <Dictionaries/Embedded/GeoDictionariesLoader.h>
 #include <Interpreters/ExternalLoaderConfigRepository.h>
-#include <Interpreters/IRuntimeComponentsFactory.h>
 #include <Interpreters/UsersManager.h>
 
 namespace DB
@@ -11,25 +10,25 @@ namespace DB
 /** Default implementation of runtime components factory
   * used by native server application.
   */
-class RuntimeComponentsFactory : public IRuntimeComponentsFactory
+class RuntimeComponentsFactory
 {
 public:
-    std::unique_ptr<IUsersManager> createUsersManager() override
+    std::unique_ptr<IUsersManager> createUsersManager()
     {
         return std::make_unique<UsersManager>();
     }
 
-    std::unique_ptr<IGeoDictionariesLoader> createGeoDictionariesLoader() override
+    std::unique_ptr<IGeoDictionariesLoader> createGeoDictionariesLoader()
     {
         return std::make_unique<GeoDictionariesLoader>();
     }
 
-    std::unique_ptr<IExternalLoaderConfigRepository> createExternalDictionariesConfigRepository() override
+    std::unique_ptr<IExternalLoaderConfigRepository> createExternalDictionariesConfigRepository()
     {
         return std::make_unique<ExternalLoaderConfigRepository>();
     }
 
-    std::unique_ptr<IExternalLoaderConfigRepository> createExternalModelsConfigRepository() override
+    std::unique_ptr<IExternalLoaderConfigRepository> createExternalModelsConfigRepository()
     {
         return std::make_unique<ExternalLoaderConfigRepository>();
     }
