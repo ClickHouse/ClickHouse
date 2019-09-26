9c919007aa525d25c58382310e845a0975e420f8
alesapin <alesapin@gmail.com>
2019-09-26 14:19:10 +0300

    Add missed files

---
+++ dbms/src/Interpreters/ExternalDictionariesLoader.cpp
@@ -0,0 +1,30 @@
+#include <Interpreters/ExternalDictionariesLoader.h>
+#include <Interpreters/Context.h>
+#include <Dictionaries/DictionaryFactory.h>
+
+namespace DB
+{
+
+/// Must not acquire Context lock in constructor to avoid possibility of deadlocks.
+ExternalDictionariesLoader::ExternalDictionariesLoader(
+    std::unique_ptr<IExternalLoaderConfigRepository> config_repository,
+    const Poco::Util::AbstractConfiguration & config,
+    Context & context_)
+        : ExternalLoader(config,
+                         "external dictionary",
+                         &Logger::get("ExternalDictionariesLoader")),
+        context(context_)
+{
+    addConfigRepository(std::move(config_repository), {"dictionary", "name", "dictionaries_config"});
+    enableAsyncLoading(true);
+    enablePeriodicUpdates(true);
+}
+
+
+ExternalLoader::LoadablePtr ExternalDictionariesLoader::create(
+        const std::string & name, const Poco::Util::AbstractConfiguration & config, const std::string & key_in_config) const
+{
+    return DictionaryFactory::instance().create(name, config, key_in_config, context);
+}
+
+}
