48f3344f9a07dd073df824d6ef902d5ccf8ccaa9
root <root@server50-ke.mars.ljnode.com>
2019-09-28 00:38:13 +0800

    Add const.

--- dbms/src/Interpreters/InterpreterSelectQuery.h
+++ dbms/src/Interpreters/InterpreterSelectQuery.h
@@ -254,7 +254,7 @@ private:
     void initSettings();
 
     /// Whether you need to check if the set is empty before ExpressionActions is executed. Create a CheckNonEmptySetBlockInputStream if needed.
-    BlockInputStreamPtr createCheckNonEmptySetIfNeed(BlockInputStreamPtr stream, ExpressionActionsPtr expression);
+    BlockInputStreamPtr createCheckNonEmptySetIfNeed(BlockInputStreamPtr stream, const ExpressionActionsPtr & expression) const;
 
     const SelectQueryOptions options;
     ASTPtr query_ptr;
