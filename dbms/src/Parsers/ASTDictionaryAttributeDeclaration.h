3fe7d84233955fcba60c613cf94bb6ab6ed71d6c
alesapin <alesapin@gmail.com>
2019-10-01 17:54:28 +0300

    Add ASTs for dictionaries

---
+++ dbms/src/Parsers/ASTDictionaryAttributeDeclaration.h
@@ -0,0 +1,47 @@
+#pragma once
+
+#include <Parsers/IAST.h>
+#include <Parsers/ASTExpressionList.h>
+
+namespace DB
+{
+
+/// AST for single dictionary attribute in dictionary DDL query
+class ASTDictionaryAttributeDeclaration : public IAST
+{
+public:
+    /// Attribute name
+    String name;
+    /// Attribute type
+    ASTPtr type;
+    /// Attribute default value
+    ASTPtr default_value;
+    /// Attribute expression
+    ASTPtr expression;
+    /// Is atribute mirrored to the parent identifier
+    bool hierarchical;
+    /// Flag that shows whether the id->attribute image is injective
+    bool injective;
+    /// MongoDB object ID
+    bool is_object_id;
+
+    String getID(char delim) const override { return "DictionaryAttributeDeclaration" + (delim + name); }
+
+    ASTPtr clone() const override;
+    void formatImpl(const FormatSettings & settings, FormatState & state, FormatStateStacked frame) const override;
+};
+
+
+class ASTDictionaryAttributesDeclarations : public IAST
+{
+public:
+    ASTExpressionList * attributes = nullptr;
+
+    String getID(char) const override { return "Dictionary attributes definition"; }
+
+    ASTPtr clone() const override;
+
+    void formatImpl(const FormatSettings & s, FormatState & state, FormatStateStacked frame) const override;
+};
+
+}
---
+++ dbms/src/Parsers/ASTFunctionWithKeyValueArguments.h
@@ -0,0 +1,42 @@
+#pragma once
+
+#include <Parsers/IAST.h>
+#include <Core/Types.h>
+
+namespace DB
+{
+
+/// Pair of values where second can also be pair
+class ASTPair : public IAST
+{
+public:
+    String first;
+    ASTPtr second;
+
+public:
+    String getID(char delim) const override;
+
+    ASTPtr clone() const override;
+
+    void formatImpl(const FormatSettings & settings, FormatState & state, FormatStateStacked frame) const override;
+};
+
+
+/// key-value just a pair "key value" separated by space, where key just a word (USER, PASSWORD, HOST etc) and value is just a literal.
+/// KeyValueFunction is a function which arguments consist of either key-value pairs or another KeyValueFunction
+/// For example: SOURCE(USER 'clickhouse' PASSWORD 'qwerty123' PORT 9000 REPLICA(HOST '127.0.0.1' PRIORITY 1) TABLE 'some_table')
+class ASTFunctionWithKeyValueArguments : public IAST
+{
+public:
+    String name;
+    ASTPtr elements;
+
+public:
+    String getID(char delim) const override;
+
+    ASTPtr clone() const override;
+
+    void formatImpl(const FormatSettings & settings, FormatState & state, FormatStateStacked frame) const override;
+};
+
+}
