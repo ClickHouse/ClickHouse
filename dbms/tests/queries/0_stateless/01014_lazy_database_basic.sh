3ace7cb69cd93ce6528014441f267d7684723d36
Vasilyev Nikita <ns-vasilev@yandex-team.ru>
2019-10-02 16:06:57 +0300

    test rename

--- dbms/tests/queries/0_stateless/01014_lazy_database_basic.sh
+++ dbms/tests/queries/0_stateless/01014_lazy_database_basic.sh
@@ -11,44 +11,58 @@ ${CLICKHOUSE_CLIENT} -n -q "
     CREATE TABLE testlazy.tlog (a UInt64, b UInt64) ENGINE = TinyLog;
 "
 
-sleep 2
+sleep 1.5
 
 ${CLICKHOUSE_CLIENT} -q "
     SELECT * FROM system.tables WHERE database = 'testlazy';
 "
 
-sleep 2
+sleep 1.5
 
 ${CLICKHOUSE_CLIENT} -q "
     SELECT database, name, metadata_modification_time FROM system.tables WHERE database = 'testlazy';
 "
 
-sleep 2
+sleep 1.5
 
 ${CLICKHOUSE_CLIENT} -n -q "
-    INSERT INTO testlazy.log VALUES (1, 1);
+    SELECT * FROM testlazy.log LIMIT 0; -- drop testlazy.log from cache
+    RENAME TABLE testlazy.log TO testlazy.log2;
+    SELECT database, name, metadata_modification_time FROM system.tables WHERE database = 'testlazy';
+"
+
+sleep 1.5
+
+${CLICKHOUSE_CLIENT} -q "
+    SELECT database, name, metadata_modification_time FROM system.tables WHERE database = 'testlazy';
+"
+
+sleep 1.5
+
+${CLICKHOUSE_CLIENT} -n -q "
+    INSERT INTO testlazy.log2 VALUES (1, 1);
     INSERT INTO testlazy.slog VALUES (2, 2);
     INSERT INTO testlazy.tlog VALUES (3, 3);
-    SELECT * FROM testlazy.log;
+    SELECT * FROM testlazy.log2;
     SELECT * FROM testlazy.slog;
     SELECT * FROM testlazy.tlog;
 "
 
-sleep 2
+sleep 1.5
 
 ${CLICKHOUSE_CLIENT} -n -q "
-    SELECT * FROM testlazy.tlog LIMIT 0; -- drop testlazy.log from cache
-    DROP TABLE testlazy.log;
+    SELECT * FROM testlazy.log2 LIMIT 0; -- drop testlazy.log2 from cache
+    DROP TABLE testlazy.log2;
 "
 
-sleep 2
+sleep 1.5
 
 ${CLICKHOUSE_CLIENT} -n -q "
     SELECT * FROM testlazy.slog;
     SELECT * FROM testlazy.tlog;
 "
 
-sleep 2
+sleep 1.5
 
 ${CLICKHOUSE_CLIENT} -q "
     DROP DATABASE testlazy;
