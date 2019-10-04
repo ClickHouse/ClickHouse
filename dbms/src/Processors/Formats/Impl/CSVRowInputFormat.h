6ae6f42ad9358cdf2399cc21f2fe0c69a20e4ea3
Alexander Tokmakov <avtokmakov@yandex-team.ru>
2019-10-04 20:19:49 +0300

    refactor null_as_default

--- dbms/src/Processors/Formats/Impl/CSVRowInputFormat.h
+++ dbms/src/Processors/Formats/Impl/CSVRowInputFormat.h
@@ -61,12 +61,7 @@ private:
         return *pos != '\n' && *pos != '\r' && *pos != format_settings.csv.delimiter;
     }
 
-    /// For setting input_format_null_as_default
-    DataTypes nullable_types;
-    MutableColumns nullable_columns;
-    OptionalIndexes column_idx_to_nullable_column_idx;
-
-    bool readField(IColumn & column, const DataTypePtr & type, bool is_last_file_column, size_t column_idx);
+    bool readField(IColumn & column, const DataTypePtr & type, bool is_last_file_column);
 };
 
 }
