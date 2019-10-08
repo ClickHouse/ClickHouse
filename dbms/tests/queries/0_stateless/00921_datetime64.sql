9cac8bb8bd46b3a6273f270928f8b7832de5372e
Vasily Nemkov <V.Nemkov@gmail.com>
2019-10-08 07:57:31 +0300

    Updated the tests for better debugging.

--- dbms/tests/queries/0_stateless/00921_datetime64.sql
+++ dbms/tests/queries/0_stateless/00921_datetime64.sql
@@ -7,10 +7,14 @@ SELECT CAST(1 as DateTime64(100)); -- { serverError 69 } # too big scale
 SELECT CAST(1 as DateTime64(-1)); -- { serverError 43 } # signed scale parameter type
 SELECT CAST(1 as DateTime64(3, 'qqq')); -- { serverError 1000 } # invalid timezone
 
+SELECT ignore(now64());
+
 CREATE TABLE A(t DateTime64(3, 'UTC')) ENGINE = MergeTree() ORDER BY t;
-INSERT INTO A(t) VALUES (1556879125123456789), ('2019-05-03 11:25:25.123456789'), (now64(3)), (now64(6)), (now64(0));
+INSERT INTO A(t) VALUES (1556879125123456789), ('2019-05-03 11:25:25.123456789');
 
 SELECT toString(t, 'UTC'), toDate(t), toStartOfDay(t), toStartOfQuarter(t), toTime(t), toStartOfMinute(t) FROM A ORDER BY t;
+
+INSERT INTO A(t) VALUES (now64(3)), (now64(6)), (now64(0));
 
 DROP TABLE A;
  -- issue toDate does a reinterpret_cast of the datetime64 which is incorrect
--- dbms/tests/queries/0_stateless/00921_datetime64_compatibility.sh
+++ dbms/tests/queries/0_stateless/00921_datetime64_compatibility.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/env bash
 
+# We should have correct env vars from shell_config.sh to run this test
 CURDIR=$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)
 . $CURDIR/../shell_config.sh
 
-# We should have correct env vars from shell_config.sh to run this test
-
-python $CURDIR/00921_datetime64_compatibility.python | ${CLICKHOUSE_CLIENT} -nm
+# in order to check queries individually (does not stop on the first one that fails):
+IFS=$'\n'; for I in $($CURDIR/00921_datetime64_compatibility.python) ; do "${CLICKHOUSE_CLIENT}" -nm -q "$I"; echo ; done # 2>&1 | tee datetime64_compat.log
