0479a7a82bbec962df8984bdb8f3c258c769f8a8
alexey-milovidov <milovidov@yandex-team.ru>
2019-10-08 21:52:48 +0300

    Update 01016_null_part_minmax.sql

--- dbms/tests/queries/0_stateless/01016_null_part_minmax.sql
+++ dbms/tests/queries/0_stateless/01016_null_part_minmax.sql
@@ -1,3 +1,5 @@
 -- this test checks that null values are correctly serialized inside minmax index (issue #7113)
-create table null_01016 (x Nullable(String)) engine MergeTree order by ifNull(x, 'order-null') partition by ifNull(x, 'partition-null');
+drop table if exists null_01016;
+create table if not exists null_01016 (x Nullable(String)) engine MergeTree order by ifNull(x, 'order-null') partition by ifNull(x, 'partition-null');
 insert into null_01016 values (null);
+drop table null_01016;
