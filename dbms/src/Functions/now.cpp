7afff3d661db840c93921208ff09b623cba8b21f
Vasily Nemkov <V.Nemkov@gmail.com>
2019-10-09 23:01:52 +0300

    Style fixes

--- dbms/src/Functions/now.cpp
+++ dbms/src/Functions/now.cpp
@@ -16,7 +16,8 @@ namespace ErrorCodes
     extern const int NUMBER_OF_ARGUMENTS_DOESNT_MATCH;
 }
 
-DateTime64::NativeType nowSubsecond(UInt32 scale) {
+DateTime64::NativeType nowSubsecond(UInt32 scale)
+{
     timespec spec;
     clock_gettime(CLOCK_REALTIME, &spec);
 
