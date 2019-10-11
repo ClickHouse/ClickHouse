f8b869b92507bc5cc4842b3827b5290655f17fd8
Vasily Nemkov <V.Nemkov@gmail.com>
2019-10-11 11:12:45 +0300

    TMP: stashing tmp changes on test environment

--- dbms/tests/queries/0_stateless/00921_datetime64_compatibility.sh
+++ dbms/tests/queries/0_stateless/00921_datetime64_compatibility.sh
@@ -4,5 +4,8 @@
 CURDIR=$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)
 . $CURDIR/../shell_config.sh
 
+export CH_C_SETTINGS='--format_custom_field_delimiter=\  --format_custom_row_before_delimiter= --format_custom_row_between_delimiter= --format_custom_result_before_delimiter= --format_custom_result_after_delimiter='
 # in order to check queries individually (does not stop on the first one that fails):
-IFS=$'\n'; for I in $($CURDIR/00921_datetime64_compatibility.python) ; do "${CLICKHOUSE_CLIENT}" -nm -q "$I"; echo ; done # 2>&1 | tee datetime64_compat.log
+IFS=$'\n'; for I in $($CURDIR/00921_datetime64_compatibility.python) ; do "${CLICKHOUSE_CLIENT}" -nm -q "$I" $CH_C_SETTINGS; echo ; done 2>&1 \
+    | tr "\n" "\t" \
+    | sed -Eu 's/-------+\t+/\n/g; s/Received exception from server \(version 19.16.1\):\s+//g; s/DB::Exception: Received from localhost:9000. //g; s/\s+$//g; '
