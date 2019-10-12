1174fc346bba33285a8be96e50d8c947edac46c5
VDimir <Vdimir@users.noreply.github.com>
2019-10-12 11:42:43 +0000

    Add isValidJSON function

--- dbms/src/Functions/FunctionsJSON.h
+++ dbms/src/Functions/FunctionsJSON.h
@@ -279,6 +279,7 @@ private:
 
 
 struct NameJSONHas { static constexpr auto name{"JSONHas"}; };
+struct NameIsValidJSON { static constexpr auto name{"isValidJSON"}; };
 struct NameJSONLength { static constexpr auto name{"JSONLength"}; };
 struct NameJSONKey { static constexpr auto name{"JSONKey"}; };
 struct NameJSONType { static constexpr auto name{"JSONType"}; };
@@ -292,11 +293,23 @@ struct NameJSONExtractKeysAndValues { static constexpr auto name{"JSONExtractKey
 struct NameJSONExtractRaw { static constexpr auto name{"JSONExtractRaw"}; };
 
 
-template <typename JSONParser>
-class JSONHasImpl
+template <typename JSONParser, bool support_key_lookup>
+class JSONCheckImpl
 {
 public:
-    static DataTypePtr getType(const char *, const ColumnsWithTypeAndName &) { return std::make_shared<DataTypeUInt8>(); }
+    static DataTypePtr getType(const char * function_name, const ColumnsWithTypeAndName & arguments) {
+        if constexpr (!support_key_lookup) {
+            if (arguments.size() != 1)
+                throw Exception{"Function " + String(function_name) + " needs exactly one argument",
+                                ErrorCodes::NUMBER_OF_ARGUMENTS_DOESNT_MATCH};
+        }
+        else
+        {
+            UNUSED(function_name);
+            UNUSED(arguments);
+        }
+        return std::make_shared<DataTypeUInt8>();
+    }
 
     using Iterator = typename JSONParser::Iterator;
     static bool addValueToColumn(IColumn & dest, const Iterator &)
@@ -310,6 +323,10 @@ public:
     static void prepare(const char *, const Block &, const ColumnNumbers &, size_t) {}
 };
 
+template <typename JSONParser>
+using JSONHasImpl = JSONCheckImpl<JSONParser, true>;
+template <typename JSONParser>
+using isValidJSONImpl = JSONCheckImpl<JSONParser, false>;
 
 template <typename JSONParser>
 class JSONLengthImpl
