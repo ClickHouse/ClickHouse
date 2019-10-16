8a38c89dc116767f6a725d414f3ef023edee00f5
Vladimir Chebotarev <vladimir.chebotarev@gmail.com>
2019-10-16 10:32:37 +0300

    Fixed ClickHouse after changing syntax.

--- dbms/src/Parsers/ASTTTLElement.cpp
+++ dbms/src/Parsers/ASTTTLElement.cpp
@@ -1,4 +1,5 @@
 #include <Columns/Collator.h>
+#include <Common/quoteString.h>
 #include <Parsers/ASTTTLElement.h>
 
 
@@ -8,18 +9,17 @@ namespace DB
 void ASTTTLElement::formatImpl(const FormatSettings & settings, FormatState & state, FormatStateStacked frame) const
 {
     children.front()->formatImpl(settings, state, frame);
-    if (destination_type == DestinationType::DISK) {
-        settings.ostr << " TO DISK ";
-    } else if (destination_type == DestinationType::VOLUME) {
-        settings.ostr << " TO VOLUME ";
-    } else if (destination_type == DestinationType::DELETE) {
-        settings.ostr << " DELETE";
+    if (destination_type == DestinationType::DISK)
+    {
+        settings.ostr << " TO DISK " << quoteString(destination_name);
     }
-
-    if (destination_type == DestinationType::DISK || destination_type == DestinationType::VOLUME) {
-        WriteBufferFromOwnString destination_name_buf;
-        writeQuoted(destination_name, destination_name_buf);
-        settings.ostr << destination_name_buf.str();
+    else if (destination_type == DestinationType::VOLUME)
+    {
+        settings.ostr << " TO VOLUME " << quoteString(destination_name);
+    }
+    else if (destination_type == DestinationType::DELETE)
+    {
+        settings.ostr << " DELETE";
     }
 }
 
