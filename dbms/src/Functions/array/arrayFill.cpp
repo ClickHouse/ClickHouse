2be06255b12db66ec020313e194961a2293e0dbf
hcz <hczhcz@users.noreply.github.com>
2019-10-18 21:31:18 +0800

    Update arrayFill.cpp

--- dbms/src/Functions/array/arrayFill.cpp
+++ dbms/src/Functions/array/arrayFill.cpp
@@ -45,7 +45,8 @@ struct ArrayFillImpl
 
                 for (; end <= array_end; ++end)
                 {
-                    if (end == array_end || fill[end + 1] != fill[begin]) {
+                    if (end == array_end || fill[end + 1] != fill[begin])
+                    {
                         if (fill[begin])
                         {
                             if constexpr (Reverse)
