cd961e4273888cdbaf003dda824f884ceb112948
chertus <chertus@gmail.com>
2019-10-21 19:22:54 +0300

    minor visitors improvement

--- dbms/src/Interpreters/InDepthNodeVisitor.h
+++ dbms/src/Interpreters/InDepthNodeVisitor.h
@@ -10,13 +10,13 @@ namespace DB
 
 /// Visits AST tree in depth, call functions for nodes according to Matcher type data.
 /// You need to define Data, visit() and needChildVisit() in Matcher class.
-template <typename Matcher, bool _top_to_bottom, typename T>
-class InDepthNodeVisitorTemplate
+template <typename Matcher, bool _top_to_bottom, typename T = ASTPtr>
+class InDepthNodeVisitor
 {
 public:
     using Data = typename Matcher::Data;
 
-    InDepthNodeVisitorTemplate(Data & data_, std::ostream * ostr_ = nullptr)
+    InDepthNodeVisitor(Data & data_, std::ostream * ostr_ = nullptr)
     :   data(data_),
         visit_depth(0),
         ostr(ostr_)
@@ -49,42 +49,26 @@ private:
 };
 
 template <typename Matcher, bool top_to_bottom>
-using InDepthNodeVisitor = InDepthNodeVisitorTemplate<Matcher, top_to_bottom, ASTPtr>;
-
-template <typename Matcher, bool top_to_bottom>
-using ConstInDepthNodeVisitor = InDepthNodeVisitorTemplate<Matcher, top_to_bottom, const ASTPtr>;
+using ConstInDepthNodeVisitor = InDepthNodeVisitor<Matcher, top_to_bottom, const ASTPtr>;
 
 /// Simple matcher for one node type without complex traversal logic.
-template <typename _Data, bool _visit_children = true>
+template <typename Data_, bool visit_children = true, typename T = ASTPtr>
 class OneTypeMatcher
 {
 public:
-    using Data = _Data;
+    using Data = Data_;
     using TypeToVisit = typename Data::TypeToVisit;
 
-    static bool needChildVisit(ASTPtr &, const ASTPtr &) { return _visit_children; }
+    static bool needChildVisit(const ASTPtr &, const ASTPtr &) { return visit_children; }
 
-    static void visit(ASTPtr & ast, Data & data)
+    static void visit(T & ast, Data & data)
     {
         if (auto * t = typeid_cast<TypeToVisit *>(ast.get()))
             data.visit(*t, ast);
     }
 };
 
-/// Links two simple matches into resulting one. There's no complex traversal logic: all the children would be visited.
-template <typename First, typename Second>
-class LinkedMatcher
-{
-public:
-    using Data = std::pair<typename First::Data, typename Second::Data>;
-
-    static bool needChildVisit(ASTPtr &, const ASTPtr &) { return true; }
-
-    static void visit(ASTPtr & ast, Data & data)
-    {
-        First::visit(ast, data.first);
-        Second::visit(ast, data.second);
-    }
-};
+template <typename Data, bool visit_children = true>
+using ConstOneTypeMatcher = OneTypeMatcher<Data, visit_children, const ASTPtr>;
 
 }
