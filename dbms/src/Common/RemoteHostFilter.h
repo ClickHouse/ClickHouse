30892f4690c185eb5d33ee4450d3398d9d81600b
millb <mikhail.korotov.io@yandex.ru>
2019-10-21 17:36:24 +0300

    Support redirect improvement

--- dbms/src/Common/RemoteHostFilter.h
+++ dbms/src/Common/RemoteHostFilter.h
@@ -11,16 +11,18 @@ namespace DB
 class RemoteHostFilter
 {
 public:
-    void checkURL(const Poco::URI &uri); /// If URL not allowed in config.xml throw UNACCEPTABLE_URL Exception
+    void checkURL(const Poco::URI &uri) const; /// If URL not allowed in config.xml throw UNACCEPTABLE_URL Exception
 
     void setValuesFromConfig(const Poco::Util::AbstractConfiguration &config);
 
-    void checkHostAndPort(const std::string & host, const std::string & port);
+    void checkHostAndPort(const std::string & host, const std::string & port) const;
+
+    RemoteHostFilter() {}
 
 private:
     std::unordered_set<std::string> primary_hosts;      /// Allowed primary (<host>) URL from config.xml
     std::vector<std::string> regexp_hosts;              /// Allowed regexp (<hots_regexp>) URL from config.xml
 
-    bool checkString(const std::string &host);
+    bool checkString(const std::string &host) const;
 };
 }
---
+++ dbms/src/IO/ReadWriteBufferFromHTTPWithHostFilter.cpp
@@ -0,0 +1,1 @@
+#include <IO/ReadWriteBufferFromHTTPWithHostFilter.h>
