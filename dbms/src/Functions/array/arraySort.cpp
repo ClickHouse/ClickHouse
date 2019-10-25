3767cb76de3aaf57d56502e152f53d167fc7c242
hcz <h.cz@qq.com>
2019-10-25 11:25:02 +0800

    Improve style

--- dbms/src/Functions/array/arraySort.cpp
+++ dbms/src/Functions/array/arraySort.cpp
@@ -63,7 +63,7 @@ struct NameArrayReverseSort { static constexpr auto name = "arrayReverseSort"; }
 using FunctionArraySort = FunctionArrayMapped<ArraySortImpl<true>, NameArraySort>;
 using FunctionArrayReverseSort = FunctionArrayMapped<ArraySortImpl<false>, NameArrayReverseSort>;
 
-void registerFunctionArraySort(FunctionFactory & factory)
+void registerFunctionsArraySort(FunctionFactory & factory)
 {
     factory.registerFunction<FunctionArraySort>();
     factory.registerFunction<FunctionArrayReverseSort>();
