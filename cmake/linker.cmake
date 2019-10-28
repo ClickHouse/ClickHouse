f95c352f204db3fc2cde5c85747d5dfb1362583a
Ivan Lezhankin <ilezhankin@yandex-team.ru>
2019-10-28 22:15:25 +0300

    Another fix attempt

--- cmake/linker.cmake
+++ cmake/linker.cmake
@@ -1,29 +1,19 @@
 option (LINKER_NAME "Linker name or full path")
 
-set (LLD_NAMES "ld.lld" "lld")
-
-if (CMAKE_CXX_COMPILER)
-    string(REGEX MATCH "-?[0-9]+(.[0-9]+)?$" COMPILER_POSTFIX ${CMAKE_CXX_COMPILER})
-    list(APPEND LLD_NAMES "lld${COMPILER_POSTFIX}")
-endif ()
-
-find_program (LLD_PATH NAMES ${LLD_NAMES})
+find_program (LLD_PATH NAMES "ld.lld" "lld")
 find_program (GOLD_PATH NAMES "ld.gold" "gold")
 
 if (NOT LINKER_NAME)
     if (LLD_PATH)
         set (LINKER_NAME "lld")
-        set (CMAKE_LINKER ${LLD_PATH})
     elseif (GOLD_PATH)
         set (LINKER_NAME "gold")
-        set (CMAKE_LINKER ${GOLD_PATH})
     endif ()
 endif ()
 
 if (LINKER_NAME)
     set (CMAKE_EXE_LINKER_FLAGS "${CMAKE_EXE_LINKER_FLAGS} -fuse-ld=${LINKER_NAME}")
     set (CMAKE_SHARED_LINKER_FLAGS "${CMAKE_SHARED_LINKER_FLAGS} -fuse-ld=${LINKER_NAME}")
-    list (APPEND CMAKE_REQUIRED_LINK_OPTIONS "-fuse-ld=${LINKER_NAME}")
 
-    message(STATUS "Using linker: ${LINKER_NAME} (selected from: LLD_PATH=${LLD_PATH} GOLD_PATH=${GOLD_PATH})")
+    message(STATUS "Using custom linker by name: ${LINKER_NAME}")
 endif ()
