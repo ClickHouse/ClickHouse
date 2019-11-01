5333258e7e8f3569b3d3b31a51462652d9c902d2
Amos Bird <amosbird@gmail.com>
2019-11-01 10:48:15 +0800

    Complement to tuple parser

--- dbms/tests/queries/0_stateless/01021_tuple_parser.sql
+++ dbms/tests/queries/0_stateless/01021_tuple_parser.sql
@@ -2,4 +2,12 @@ SELECT toTypeName((1,)), (1,);
 
 SET enable_debug_queries = 1;
 
-ANALYZE SELECT (1,)
+ANALYZE SELECT (1,);
+
+DROP TABLE IF EXISTS tuple_values;
+
+CREATE TABLE tuple_values (t Tuple(int)) ENGINE = Memory;
+
+INSERT INTO tuple_values VALUES ((1)), ((2,));
+
+DROP TABLE tuple_values;
