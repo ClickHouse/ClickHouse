ad12099642bfc396bb53a21f8277a66d760bf900
Amos Bird <amosbird@gmail.com>
2019-11-02 13:55:06 +0800

    ubsan fix

--- libs/libcommon/include/common/Types.h
+++ libs/libcommon/include/common/Types.h
@@ -1,5 +1,8 @@
 #pragma once
 #include <cstdint>
+#include <cstdlib>
+#include <type_traits>
+#include <algorithm>
 
 using Int8 = int8_t;
 using Int16 = int16_t;
@@ -10,3 +13,43 @@ using UInt8 = uint8_t;
 using UInt16 = uint16_t;
 using UInt32 = uint32_t;
 using UInt64 = uint64_t;
+
+/// The standard library type traits, such as std::is_arithmetic, with one exception
+/// (std::common_type), are "set in stone". Attempting to specialize them causes undefined behavior.
+/// So instead of using the std type_traits, we use our own version which allows extension.
+template <typename T>
+struct is_signed
+{
+    static constexpr bool value = std::is_signed_v<T>;
+};
+
+template <typename T>
+inline constexpr bool is_signed_v = is_signed<T>::value;
+
+template <typename T>
+struct is_unsigned
+{
+    static constexpr bool value = std::is_unsigned_v<T>;
+};
+
+template <typename T>
+inline constexpr bool is_unsigned_v = is_unsigned<T>::value;
+
+template <typename T>
+struct is_integral
+{
+    static constexpr bool value = std::is_integral_v<T>;
+};
+
+template <typename T>
+inline constexpr bool is_integral_v = is_integral<T>::value;
+
+template <typename T>
+struct is_arithmetic
+{
+    static constexpr bool value = std::is_arithmetic_v<T>;
+};
+
+template <typename T>
+inline constexpr bool is_arithmetic_v = is_arithmetic<T>::value;
+
