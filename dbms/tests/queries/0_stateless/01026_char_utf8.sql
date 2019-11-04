0eb819cc28e301237a47491ec9d4095ba9153d3e
Alexey Milovidov <milovidov@yandex-team.ru>
2019-11-05 01:47:46 +0300

    Added another test case

--- dbms/tests/queries/0_stateless/01026_char_utf8.sql
+++ dbms/tests/queries/0_stateless/01026_char_utf8.sql
@@ -1,2 +1,3 @@
 SELECT char(0xD0, 0xBF, 0xD1, 0x80, 0xD0, 0xB8, 0xD0, 0xB2, 0xD0, 0xB5, 0xD1, 0x82) AS hello;
 SELECT char(-48,-65,-47,-128,-48,-72,-48,-78,-48,-75,-47,-126) AS hello;
+SELECT char(-48, 0xB0 + number,-47,-128,-48,-72,-48,-78,-48,-75,-47,-126) AS hello FROM numbers(16);
