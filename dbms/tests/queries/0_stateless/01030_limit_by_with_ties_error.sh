8d50b0afc7ef2af7c12bf554e7c4b639f330694e
Nikita Mikhaylov <mikhaylovnikitka@gmail.com>
2019-11-06 17:18:28 +0300

    better

--- dbms/tests/queries/0_stateless/01030_limit_by_with_ties_error.sh
+++ dbms/tests/queries/0_stateless/01030_limit_by_with_ties_error.sh
@@ -7,3 +7,7 @@ CLICKHOUSE_CLIENT=`echo ${CLICKHOUSE_CLIENT} | sed 's/'"--send_logs_level=${CLIC
 
 $CLICKHOUSE_CLIENT --query="SELECT * FROM (SELECT number % 5 AS a, count() AS b, c FROM numbers(10) ARRAY JOIN [1,2] AS c GROUP BY a,c) AS table ORDER BY a LIMIT 3 WITH TIES BY a" 2>&1 | grep -q "Code: 490." && echo 'OK' || echo 'FAIL' ||:
 
+$CLICKHOUSE_CLIENT --query="SELECT * FROM VALUES('Phrase String, Payload String', ('hello', 'x'), ('world', 'x'), ('hello', 'z'), ('upyachka', 'a'), ('test', 'b'), ('foo', 'c'), ('bar', 'd')) ORDER BY Payload LIMIT 1 WITH TIES BY Phrase LIMIT 5;" 2>&1 | grep -q "Code: 490." && echo 'OK' || echo 'FAIL' ||:
+
+$CLICKHOUSE_CLIENT --query="SELECT * FROM VALUES('Phrase String, Payload String', ('hello', 'x'), ('world', 'x'), ('hello', 'z'), ('upyachka', 'a'), ('test', 'b'), ('foo', 'c'), ('bar', 'd')) ORDER BY Payload LIMIT 1 BY Phrase LIMIT 5 WITH TIES"
+
