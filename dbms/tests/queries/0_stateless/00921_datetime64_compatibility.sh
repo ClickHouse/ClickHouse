abbf63196b17227d39b3c8e3ad0f0072a1f91920
Vasily Nemkov <V.Nemkov@gmail.com>
2019-11-07 13:58:16 +0300

    Fixed 00921_datetime64_compatibility.python test case
    Running it correctly so test runner does not report failure on intended exceptions.

--- dbms/tests/queries/0_stateless/00921_datetime64_compatibility.sh
+++ dbms/tests/queries/0_stateless/00921_datetime64_compatibility.sh
@@ -1,11 +1,15 @@
 #!/usr/bin/env bash
 
+CLICKHOUSE_CLIENT_SERVER_LOGS_LEVEL="none"
 # We should have correct env vars from shell_config.sh to run this test
 CURDIR=$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)
 . $CURDIR/../shell_config.sh
 
-export CH_C_SETTINGS='--format_custom_field_delimiter=\  --format_custom_row_before_delimiter= --format_custom_row_between_delimiter= --format_custom_result_before_delimiter= --format_custom_result_after_delimiter='
-# in order to check queries individually (does not stop on the first one that fails):
-IFS=$'\n'; for I in $($CURDIR/00921_datetime64_compatibility.python) ; do "${CLICKHOUSE_CLIENT}" -nm -q "$I" $CH_C_SETTINGS; echo ; done 2>&1 \
-    | tr "\n" "\t" \
-    | sed -Eu 's/-------+\t+/\n/g; s/Received exception from server \(version 19.16.1\):\s+//g; s/DB::Exception: Received from localhost:9000. //g; s/\s+$//g; '
+# In order to check queries individually (don't stop on the first one that fails):
+#IFS=$'\n'; for I in $(${CURDIR}/00921_datetime64_compatibility.python) ; do unset IFS; ${CLICKHOUSE_CLIENT} --query "${I}"; echo ; done 2>&1;
+
+# ${CURDIR}/00921_datetime64_compatibility.python
+
+${CURDIR}/00921_datetime64_compatibility.python \
+    | ${CLICKHOUSE_CLIENT} --ignore-error -T -nm --calculate_text_stack_trace 0 --log-level 'error' 2>&1 \
+    | sed -Ee 's/Received exception from server .*//g; s/(Code: [0-9]+). DB::Exception: Received from .* DB::Exception/\1/g'
