f8d90af4cea10528016b676fcef834a83d73d901
Vasily Nemkov <V.Nemkov@gmail.com>
2019-11-13 12:46:53 +0300

    Fixed regression of converting Date to DateTime (and vice versa) Field values on INSERT.

--- dbms/src/Interpreters/convertFieldToType.cpp
+++ dbms/src/Interpreters/convertFieldToType.cpp
@@ -165,6 +165,8 @@ Field convertFieldToTypeImpl(const Field & src, const IDataType & type, const ID
     WhichDataType which_from_type;
     if (from_type_hint)
     {
+        which_from_type = WhichDataType(*from_type_hint);
+
         // This was added to mitigate converting DateTime64-Field (a typedef to a Decimal64) to DataTypeDate64-compatitable type.
         if (from_type_hint && from_type_hint->equals(type))
         {
