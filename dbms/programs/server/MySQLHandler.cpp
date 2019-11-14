292a7ae256871f3180c2a7a27cf0fddd744fe469
alexey-milovidov <milovidov@yandex-team.ru>
2019-11-14 20:32:56 +0300

    Update MySQLHandler.cpp

--- dbms/programs/server/MySQLHandler.cpp
+++ dbms/programs/server/MySQLHandler.cpp
@@ -223,7 +223,8 @@ void MySQLHandler::authenticate(const String & user_name, const String & auth_pl
         authPluginSSL();
     }
 
-    try {
+    try
+    {
         std::optional<String> auth_response = auth_plugin_name == auth_plugin->getName() ? std::make_optional<String>(initial_auth_response) : std::nullopt;
         auth_plugin->authenticate(user_name, auth_response, connection_context, packet_sender, secure_connection, socket().peerAddress());
     }
