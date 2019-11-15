c5a54b713f12b7e932995d45b6646bc845922398
Nikita Mikhaylov <mikhaylovnikitka@gmail.com>
2019-11-15 14:30:19 +0300

    bug test added

---
+++ dbms/tests/queries/0_stateless/01030_incorrect_count_merge_tree.sql
@@ -0,0 +1,25 @@
+drop table if exists tst;
+
+create table tst (timestamp DateTime, val Nullable(Int8)) engine SummingMergeTree partition by toYYYYMM(timestamp) ORDER by (timestamp);
+
+insert into tst values ('2018-02-01 00:00:00', 1), ('2018-02-02 00:00:00', 2);
+
+select count() from tst;
+select count() from tst final;
+
+select count() from tst where timestamp is not null;
+select count() from tst final where timestamp is not null;
+
+select count() from tst where val is not null;
+select count() from tst final where val is not null;
+
+select count() from tst final where timestamp>0;
+select count() from tst final prewhere timestamp > 0;
+
+select count() from tst final where val>0;
+select count() from tst final prewhere val>0;
+
+drop table tst;
+
+
+
