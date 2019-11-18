4bebf34215236c6be33057f7489ba5ec83b326f9
Pervakov Grigory <pervakovg@yandex-team.ru>
2019-11-18 18:33:43 +0300

    Complete test with sleep

--- dbms/tests/integration/test_reload_max_table_size_to_drop/test.py
+++ dbms/tests/integration/test_reload_max_table_size_to_drop/test.py
@@ -1,6 +1,5 @@
 import time
 import pytest
-import sys
 import os
 
 from helpers.cluster import ClickHouseCluster
@@ -10,46 +9,41 @@ cluster = ClickHouseCluster(__file__)
 node = cluster.add_instance('node', config_dir="configs")
 
 SCRIPT_DIR = os.path.dirname(os.path.realpath(__file__))
-config_dir = os.path.join(SCRIPT_DIR, './_instances/node/configs')
+CONFIG_PATH = os.path.join(SCRIPT_DIR, './_instances/node/configs/config.xml')
 
 
 @pytest.fixture(scope="module")
 def start_cluster():
     try:
         cluster.start()
-        node.query("CREATE DATABASE chtest;")
-        node.query(
-            '''
-            CREATE TABLE chtest.reload_max_table_size (date Date, id UInt32) 
-            ENGINE = MergeTree(date, id, 8192)
-            '''
-        )
-        node.query("INSERT INTO chtest.reload_max_table_size VALUES (now(), 0)")
+        node.query("CREATE TABLE test(date Date, id UInt32) ENGINE = MergeTree(date, id, 8192)")
         yield cluster
     finally:
-        #cluster.shutdown()
-        pass
+        cluster.shutdown()
 
 
 def test_reload_max_table_size_to_drop(start_cluster):
-    config = open(config_dir + '/config.xml', 'r')
+    node.query("INSERT INTO test VALUES (now(), 0)")
+
+    time.sleep(5)  # wait for data part commit
+
+    drop = node.get_query_request("DROP TABLE test")
+    out, err = drop.get_answer_and_error()
+    assert out == ""
+    assert err != ""
+
+    config = open(CONFIG_PATH, 'r')
     config_lines = config.readlines()
     config.close()
-
-    error = node.get_query_request("DROP TABLE chtest.reload_max_table_size") # change to query_and_get_error after fix
-    print >> sys.stderr, 'error: ' + error.get_answer()
-    print >> sys.stderr, 'path: ' + config_dir
-    print >> sys.stderr, 'config: '
-    for line in config_lines:
-        print >> sys.stderr, line
-
-    assert error != "" # Crashes due to illigal config
-
     config_lines = map(lambda line: line.replace("<max_table_size_to_drop>1", "<max_table_size_to_drop>1000000"),
                        config_lines)
-    config = open(config_dir + '/config.xml', 'w')
+    config = open(CONFIG_PATH, 'w')
     config.writelines(config_lines)
+    config.close()
 
-    time.sleep(50000)
-    error = node.query_and_get_error("DROP TABLE chtest.reload_max_table_size")
-    assert error == ""
+    time.sleep(5)  # wait for config reload
+
+    drop = node.get_query_request("DROP TABLE test")
+    out, err = drop.get_answer_and_error()
+    assert out == ""
+    assert err == ""
