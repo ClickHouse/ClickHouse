9ab19e7efaa38796af9ef5b2086e029a153feb47
Nikita Mikhaylov <mikhaylovnikitka@gmail.com>
2019-11-19 17:28:57 +0300

    add more tests

--- dbms/tests/queries/0_stateless/01030_incorrect_count_merge_tree.sql
+++ dbms/tests/queries/0_stateless/01030_incorrect_count_merge_tree.sql
@@ -1,25 +1,16 @@
-drop table if exists tst;
 
-create table tst (timestamp DateTime, val Nullable(Int8)) engine SummingMergeTree partition by toYYYYMM(timestamp) ORDER by (timestamp);
+drop table if exists tst2;
+create table tst2
+(
+	timestamp DateTime,
+	val Nullable(Int8)
+) engine MergeTree partition by toYYYYMM(timestamp) ORDER by (timestamp);
 
-insert into tst values ('2018-02-01 00:00:00', 1), ('2018-02-02 00:00:00', 2);
+insert into tst2 values ('2018-02-01 00:00:00', 1), ('2018-02-02 00:00:00', 2);
 
-select count() from tst;
-select count() from tst final;
+select * from tst2;
+select count() from tst2 where val is not null;
+select count() from tst2 where timestamp is not null;
+select count(*) from tst2 where timestamp > '2017-01-01 00:00:00'
 
-select count() from tst where timestamp is not null;
-select count() from tst final where timestamp is not null;
-
-select count() from tst where val is not null;
-select count() from tst final where val is not null;
-
-select count() from tst final where timestamp>0;
-select count() from tst final prewhere timestamp > 0;
-
-select count() from tst final where val>0;
-select count() from tst final prewhere val>0;
-
-drop table tst;
-
-
-
+drop table tst2;
