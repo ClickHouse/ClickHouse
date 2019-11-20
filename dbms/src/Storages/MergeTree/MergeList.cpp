a9047ff6976165e7a41fde308948e48ede03f81e
Vladimir Chebotarev <vladimir.chebotarev@gmail.com>
2019-11-20 11:51:52 +0300

    Attempt to add `is_move`, `source_part_paths`, `result_part_path` columns to `system.merges`.

--- dbms/src/Storages/MergeTree/MergeList.cpp
+++ dbms/src/Storages/MergeTree/MergeList.cpp
@@ -17,13 +17,16 @@ namespace DB
 MergeListElement::MergeListElement(const std::string & database_, const std::string & table_, const FutureMergedMutatedPart & future_part)
     : database{database_}, table{table_}, partition_id{future_part.part_info.partition_id}
     , result_part_name{future_part.name}
+    // FIXME result_part_path
     , result_data_version{future_part.part_info.getDataVersion()}
     , num_parts{future_part.parts.size()}
     , thread_number{getThreadNumber()}
 {
+    // FIXME is_move
     for (const auto & source_part : future_part.parts)
     {
         source_part_names.emplace_back(source_part->name);
+        source_part_paths.emplace_back(source_part->getFullPath());
 
         std::shared_lock<std::shared_mutex> part_lock(source_part->columns_lock);
 
@@ -54,8 +57,10 @@ MergeInfo MergeListElement::getInfo() const
     res.database = database;
     res.table = table;
     res.result_part_name = result_part_name;
+    res.result_part_path = result_part_path;
     res.partition_id = partition_id;
     res.is_mutation = is_mutation;
+    res.is_move = is_move;
     res.elapsed = watch.elapsedSeconds();
     res.progress = progress.load(std::memory_order_relaxed);
     res.num_parts = num_parts;
@@ -72,6 +77,9 @@ MergeInfo MergeListElement::getInfo() const
 
     for (const auto & source_part_name : source_part_names)
         res.source_part_names.emplace_back(source_part_name);
+
+    for (const auto & source_part_path : source_part_paths)
+        res.source_part_paths.emplace_back(source_part_path);
 
     return res;
 }
--- dbms/src/Storages/System/StorageSystemMerges.cpp
+++ dbms/src/Storages/System/StorageSystemMerges.cpp
@@ -16,8 +16,11 @@ NamesAndTypesList StorageSystemMerges::getNamesAndTypes()
         {"num_parts", std::make_shared<DataTypeUInt64>()},
         {"source_part_names", std::make_shared<DataTypeArray>(std::make_shared<DataTypeString>())},
         {"result_part_name", std::make_shared<DataTypeString>()},
+        {"source_part_paths", std::make_shared<DataTypeArray>(std::make_shared<DataTypeString>())},
+        {"result_part_path", std::make_shared<DataTypeString>()},
         {"partition_id", std::make_shared<DataTypeString>()},
         {"is_mutation", std::make_shared<DataTypeUInt8>()},
+        {"is_move", std::make_shared<DataTypeUInt8>()},
         {"total_size_bytes_compressed", std::make_shared<DataTypeUInt64>()},
         {"total_size_marks", std::make_shared<DataTypeUInt64>()},
         {"bytes_read_uncompressed", std::make_shared<DataTypeUInt64>()},
@@ -45,8 +48,11 @@ void StorageSystemMerges::fillData(MutableColumns & res_columns, const Context &
             res_columns[i++]->insert(merge.num_parts);
             res_columns[i++]->insert(merge.source_part_names);
             res_columns[i++]->insert(merge.result_part_name);
+            res_columns[i++]->insert(merge.source_part_paths);
+            res_columns[i++]->insert(merge.result_part_path);
             res_columns[i++]->insert(merge.partition_id);
             res_columns[i++]->insert(merge.is_mutation);
+            res_columns[i++]->insert(merge.is_move);
             res_columns[i++]->insert(merge.total_size_bytes_compressed);
             res_columns[i++]->insert(merge.total_size_marks);
             res_columns[i++]->insert(merge.bytes_read_uncompressed);
