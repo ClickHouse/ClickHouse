59d9504cbf2f7262f8c66620644128f462f0dbf3
Alexander Tokmakov <avtokmakov@yandex-team.ru>
2019-11-21 18:37:59 +0300

    move table locks to main pipeline

--- dbms/src/Processors/QueryPipeline.cpp
+++ dbms/src/Processors/QueryPipeline.cpp
@@ -487,9 +487,9 @@ void QueryPipeline::unitePipelines(
         processors.insert(processors.end(), pipeline.processors.begin(), pipeline.processors.end());
         streams.insert(streams.end(), pipeline.streams.begin(), pipeline.streams.end());
 
+        table_locks.insert(table_locks.end(), std::make_move_iterator(pipeline.table_locks.begin()), std::make_move_iterator(pipeline.table_locks.end()));
         interpreter_context.insert(interpreter_context.end(), pipeline.interpreter_context.begin(), pipeline.interpreter_context.end());
         storage_holder.insert(storage_holder.end(), pipeline.storage_holder.begin(), pipeline.storage_holder.end());
-        /// Do we need also copy table_locks?
     }
 
     if (!extremes.empty())
