ee4237d244e15ab189cb1424d97f0f5120fc6084
millb <mikhail.korotov.io@yandex.ru>
2019-11-23 23:25:32 +0300

    Working commit

--- dbms/src/Processors/Formats/Impl/JSONCompactEachRowWithNamesAndTypesRowOutputFormat.cpp
+++ dbms/src/Processors/Formats/Impl/JSONCompactEachRowWithNamesAndTypesRowOutputFormat.cpp
@@ -52,7 +52,7 @@ void JSONCompactEachRowWithNamesAndTypesRowOutputFormat::writeBeforeTotals()
 void JSONCompactEachRowWithNamesAndTypesRowOutputFormat::writeTotals(const Columns & columns, size_t row_num)
 {
     size_t num_columns = columns.size();
-
+    writeChar('[', out);
     for (size_t i = 0; i < num_columns; ++i)
     {
         if (i != 0)
@@ -60,7 +60,7 @@ void JSONCompactEachRowWithNamesAndTypesRowOutputFormat::writeTotals(const Colum
 
         JSONCompactEachRowRowOutputFormat::writeField(*columns[i], *types[i], row_num);
     }
-    writeChar('\n', out);
+    writeCString("]\n", out);
 }
 
 void JSONCompactEachRowWithNamesAndTypesRowOutputFormat::writeAfterTotals()
