f2ee921d5cc62b958c1a1bebc7094dd156a63f78
Mikhail Korotov <millb@sandbox-410109003>
2019-11-24 18:55:10 +0300

    Tests added

---
+++ dbms/tests/queries/0_stateless/01034_JSONCompactEachRowWithNamesAndTypes.sql
@@ -0,0 +1,7 @@
+DROP TABLE IF EXISTS test_table;
+CREATE TABLE test_table (value UInt8, name String) ENGINE = MergeTree() ORDER BY value;
+INSERT INTO test_table VALUES (1, 'a'), (2, 'b'), (3, 'c');
+SELECT * FROM test_table FORMAT JSONCompactEachRowWithNamesAndTypes;
+SELECT name, count() AS c FROM test_table GROUP BY name WITH TOTALS ORDER BY name FORMAT JSONCompactEachRowWithNamesAndTypes;
+DROP TABLE IF EXISTS test_table;
+
