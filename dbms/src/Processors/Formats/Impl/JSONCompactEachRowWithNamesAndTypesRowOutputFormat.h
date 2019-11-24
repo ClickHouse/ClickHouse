fcaad3987a3a45a89b922b4ed845d824b4d169f8
millb <mikhail.korotov.io@yandex.ru>
2019-11-24 18:45:37 +0300

    Working commit

--- dbms/src/Processors/Formats/Impl/JSONCompactEachRowWithNamesAndTypesRowOutputFormat.h
+++ dbms/src/Processors/Formats/Impl/JSONCompactEachRowWithNamesAndTypesRowOutputFormat.h
@@ -19,8 +19,10 @@ public:
     String getName() const override { return "JSONCompactEachRowWithNamesAndTypesRowOutputFormat"; }
 
     void writePrefix() override;
-    
+
+    void writeBeforeTotals() override {}
     void writeTotals(const Columns & columns, size_t row_num) override;
+    void writeAfterTotals() override {}
 
 protected:
     /// Totals allowed
