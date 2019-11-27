4f2f474fdef4a86ddf4c916dc0d7bc0d1cf2c06e
Nikolai Kochetov <nik-kochetov@yandex-team.ru>
2019-11-27 19:24:44 +0300

    Add info about affected ports to IProcessor::prepare

--- dbms/src/Processors/Executors/PipelineExecutor.h
+++ dbms/src/Processors/Executors/PipelineExecutor.h
@@ -43,12 +43,18 @@ private:
 
     struct Edge
     {
+        Edge(UInt64 to_, bool backward_, UInt64 input_port_number_, UInt64 output_port_number_)
+            : to(to_), backward(backward_), input_port_number(input_port_number_), output_port_number(output_port_number_) {}
+
         UInt64 to = std::numeric_limits<UInt64>::max();
+        bool backward;
+        UInt64 input_port_number;
+        UInt64 output_port_number;
 
         /// Edge version is increased when port's state is changed (e.g. when data is pushed). See Port.h for details.
         /// To compare version with prev_version we can decide if neighbour processor need to be prepared.
-        UInt64 version = 0;
-        UInt64 prev_version = 0;
+        UInt64 version = 1;
+        UInt64 prev_version = 0; /// prev version is zero so ve traverse all edges after the first prepare.
     };
 
     /// Use std::list because new ports can be added to processor during execution.
@@ -58,7 +64,6 @@ private:
     /// Can be owning or not. Owning means that executor who set this status can change node's data and nobody else can.
     enum class ExecStatus
     {
-        New,  /// prepare wasn't called yet. Initial state. Non-owning.
         Idle,  /// prepare returned NeedData or PortFull. Non-owning.
         Preparing,  /// some executor is preparing processor, or processor is in task_queue. Owning.
         Executing,  /// prepare returned Ready and task is executing. Owning.
@@ -87,17 +92,19 @@ private:
         Edges directEdges;
         Edges backEdges;
 
-        std::atomic<ExecStatus> status;
-        /// This flag can be set by any executor.
-        /// When enabled, any executor can try to atomically set Preparing state to status.
-        std::atomic_bool need_to_be_prepared;
+        ExecStatus status;
+        std::mutex status_mutex;
+
         /// Last state for profiling.
         IProcessor::Status last_processor_status = IProcessor::Status::NeedData;
 
         std::unique_ptr<ExecutionState> execution_state;
 
+        IProcessor::PortNumbers updated_input_ports;
+        IProcessor::PortNumbers updated_output_ports;
+
         Node(IProcessor * processor_, UInt64 processor_id)
-            : processor(processor_), status(ExecStatus::New), need_to_be_prepared(false)
+            : processor(processor_), status(ExecStatus::Idle)
         {
             execution_state = std::make_unique<ExecutionState>();
             execution_state->processor = processor;
@@ -105,8 +112,8 @@ private:
         }
 
         Node(Node && other) noexcept
-            : processor(other.processor), status(other.status.load())
-            , need_to_be_prepared(other.need_to_be_prepared.load()), execution_state(std::move(other.execution_state))
+            : processor(other.processor), status(other.status)
+            , execution_state(std::move(other.execution_state))
         {
         }
     };
