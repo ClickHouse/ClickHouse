5f57e0ebcb4c9344773e453badc77d0125f5f6b1
hcz <h.cz@qq.com>
2019-11-29 15:05:48 +0800

    Fix relative path test

--- dbms/tests/integration/test_relative_filepath/test.py
+++ dbms/tests/integration/test_relative_filepath/test.py
@@ -16,18 +16,17 @@ def start_cluster():
 
 def test_filepath(start_cluster):
     # 2 rows data
-    some_data = "\t111.222\nData\t333.444"
+    some_data = "Test\t111.222\nData\t333.444"
 
-    node.exec_in_container(['bash', '-c', 'echo "{}{}" > {}'.format(
-        filename,
+    node.exec_in_container(['bash', '-c', 'echo "{}" > {}'.format(
         some_data,
         path_to_userfiles_from_defaut_config + "/relative_user_file_test"
     )], privileged=True, user='root')
 
     test_requests = [("relative_user_file_test", "2"),
-                     ("../" + path_to_userfiles_from_defaut_config + "relative_user_file_test", "2")]
+                     ("../" + path_to_userfiles_from_defaut_config + "/relative_user_file_test", "2")]
 
     for pattern, value in test_requests:
         assert node.query('''
-            select count(*) from file('strange_names/{}', 'TSV', 'text String, number Float64')
+            select count() from file('strange_names/{}', 'TSV', 'text String, number Float64')
         '''.format(pattern)) == '{}\n'.format(value)
