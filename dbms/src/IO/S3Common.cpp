a42f38b75ee79fe2f2c8f3cb8e461cc66e39029d
Vladimir Chebotarev <vladimir.chebotarev@gmail.com>
2019-12-01 14:24:55 +0300

    Fixed tests and logic of authorization in S3.

--- dbms/src/IO/S3Common.cpp
+++ dbms/src/IO/S3Common.cpp
@@ -1,5 +1,6 @@
 #include <IO/S3Common.h>
 
+#include <ctime>
 #include <iterator>
 #include <sstream>
 
@@ -22,14 +23,23 @@ void S3Helper::authenticateRequest(Poco::Net::HTTPRequest & request,
         return;
 
     /// Limitations:
-    /// 1. Virtual hosted-style requests are not supported.
+    /// 1. Virtual hosted-style requests are not supported (e.g. `http://johnsmith.net.s3.amazonaws.com/homepage.html`).
     /// 2. AMZ headers are not supported (TODO).
+
+    if (!request.has("Date"))
+    {
+        char buffer[1024];
+        time_t now = std::time(nullptr);
+        struct tm *my_tm = std::gmtime(&now);
+        std::strftime(buffer, sizeof(buffer), "%a, %d %b %Y %H:%M:%S GMT", my_tm); /// See RFC 1123.
+        request.set("Date", buffer);
+    }
 
     String string_to_sign = request.getMethod() + "\n"
         + request.get("Content-MD5", "") + "\n"
         + request.get("Content-Type", "") + "\n"
-        + request.get("Date", "") + "\n"
-        + Poco::URI(request.getURI()).getPath();
+        + request.get("Date") + "\n"
+        + Poco::URI(request.getURI()).getPathAndQuery();
 
     Poco::HMACEngine<Poco::SHA1Engine> engine(secret_access_key);
     engine.update(string_to_sign);
@@ -37,6 +47,7 @@ void S3Helper::authenticateRequest(Poco::Net::HTTPRequest & request,
     std::ostringstream signature;
     Poco::Base64Encoder encoder(signature);
     std::copy(digest.begin(), digest.end(), std::ostream_iterator<char>(encoder));
+    encoder.close();
 
     request.set("Authorization", "AWS " + access_key_id + ":" + signature.str());
 }
