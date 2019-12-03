004e49d0769c46ee1c61eb8ffdac2dc933a2d1ca
alexey-milovidov <milovidov@yandex-team.ru>
2019-12-03 03:49:05 +0300

    Update IInputFormat.cpp

--- dbms/src/Processors/Formats/IInputFormat.cpp
+++ dbms/src/Processors/Formats/IInputFormat.cpp
@@ -20,7 +20,7 @@ void IInputFormat::resetParser()
     if (in.hasPendingData())
         throw Exception("Unread data in IInputFormat::resetParser. Most likely it's a bug.", ErrorCodes::LOGICAL_ERROR);
 
-    // those are protected attributes from ISource (i didn't want to propagate resetParser up there)
+    // those are protected attributes from ISource (I didn't want to propagate resetParser up there)
     finished = false;
     got_exception = false;
 
