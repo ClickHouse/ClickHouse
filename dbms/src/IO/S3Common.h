61d52ce1401bd0588273f44754e3a50608b680b7
Alexey Milovidov <milovidov@yandex-team.ru>
2019-12-03 04:22:25 +0300

    Fixed bug in #7623

--- dbms/src/IO/S3Common.h
+++ dbms/src/IO/S3Common.h
@@ -8,9 +8,10 @@
 namespace DB
 {
 
-struct S3Helper
+namespace S3Helper
 {
-    static void authenticateRequest(Poco::Net::HTTPRequest & request,
+    void authenticateRequest(
+        Poco::Net::HTTPRequest & request,
         const String & access_key_id,
         const String & secret_access_key);
 };
