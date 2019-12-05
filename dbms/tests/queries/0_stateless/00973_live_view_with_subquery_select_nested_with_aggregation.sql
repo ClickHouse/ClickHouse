90a069af19438236d7403558c69b1aa2addc18ac
Vitaliy Zakaznikov <vzakaznikov@altinity.com>
2019-12-05 22:42:00 +0100

    Fixing issue with support for nested subqueries. Adding additional live view with subquery tests.

---
+++ dbms/tests/queries/0_stateless/00973_live_view_with_subquery_select_nested_with_aggregation.sql
@@ -0,0 +1,21 @@
+SET allow_experimental_live_view = 1;
+
+DROP TABLE IF EXISTS test.lv;
+DROP TABLE IF EXISTS test.mt;
+
+CREATE TABLE test.mt (a Int32) Engine=MergeTree order by tuple();
+CREATE LIVE VIEW test.lv AS SELECT * FROM ( SELECT sum(a) FROM ( SELECT a FROM (SELECT a FROM test.mt) ) );
+
+INSERT INTO test.mt VALUES (1),(2),(3);
+
+SELECT *,_version FROM test.lv;
+SELECT *,_version FROM test.lv;
+
+INSERT INTO test.mt VALUES (1),(2),(3);
+
+SELECT *,_version FROM test.lv;
+SELECT *,_version FROM test.lv;
+
+DROP TABLE test.lv;
+DROP TABLE test.mt;
+
