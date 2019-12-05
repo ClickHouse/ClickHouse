9e827fbcf2d5d477e2da2469320de8261b160117
Konstantin Malanchev <hombit@gmail.com>
2019-12-05 16:42:37 +0300

    Add h3EdgeLengthM function

--- dbms/src/Functions/registerFunctionsGeo.cpp
+++ dbms/src/Functions/registerFunctionsGeo.cpp
@@ -14,6 +14,7 @@ void registerFunctionGeohashesInBox(FunctionFactory & factory);
 
 #if USE_H3
 void registerFunctionGeoToH3(FunctionFactory &);
+void registerFunctionH3EdgeLengthM(FunctionFactory &);
 void registerFunctionH3GetResolution(FunctionFactory &);
 void registerFunctionH3IsValid(FunctionFactory &);
 void registerFunctionH3KRing(FunctionFactory &);
@@ -30,6 +31,7 @@ void registerFunctionsGeo(FunctionFactory & factory)
 
 #if USE_H3
     registerFunctionGeoToH3(factory);
+	registerFunctionH3EdgeLengthM(factory);
 	registerFunctionH3GetResolution(factory);
 	registerFunctionH3IsValid(factory);
 	registerFunctionH3KRing(factory);
