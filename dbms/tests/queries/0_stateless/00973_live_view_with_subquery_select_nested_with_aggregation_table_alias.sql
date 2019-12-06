c86423022a35be9dcf5a8810cb6541966f3c6b2d
Vitaliy Zakaznikov <vzakaznikov@altinity.com>
2019-12-06 13:34:52 +0100

    Updating tests to use a column alias in addition to the table alias.

--- dbms/tests/queries/0_stateless/00973_live_view_with_subquery_select_nested_with_aggregation_table_alias.sql
+++ dbms/tests/queries/0_stateless/00973_live_view_with_subquery_select_nested_with_aggregation_table_alias.sql
@@ -4,7 +4,7 @@ DROP TABLE IF EXISTS test.lv;
 DROP TABLE IF EXISTS test.mt;
 
 CREATE TABLE test.mt (a Int32) Engine=MergeTree order by tuple();
-CREATE LIVE VIEW test.lv AS SELECT * FROM ( SELECT sum(boo.a) FROM ( SELECT foo.a FROM (SELECT a FROM test.mt) AS foo) AS boo );
+CREATE LIVE VIEW test.lv AS SELECT * FROM ( SELECT sum(boo.x) FROM ( SELECT foo.x FROM (SELECT a AS x FROM test.mt) AS foo) AS boo );
 
 INSERT INTO test.mt VALUES (1),(2),(3);
 
