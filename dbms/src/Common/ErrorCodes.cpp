be52de7b2ddbd2e47612aaa8b4f5c7347cccc776
Nikita Mikhaylov <mikhaylovnikitka@gmail.com>
2019-12-06 13:44:04 +0300

    Merge pull request #7637 from nikitamikhaylov/limit_with_ties
    Unexpected behaviour of LIMIT WITH TIES and LIMIT BY
    
    (cherry picked from commit 5a45c7561dacf8521eb36c030af9aeba878361ec)

--- dbms/src/Common/ErrorCodes.cpp
+++ dbms/src/Common/ErrorCodes.cpp
@@ -464,6 +464,15 @@ namespace ErrorCodes
     extern const int CANNOT_GET_CREATE_DICTIONARY_QUERY = 487;
     extern const int UNKNOWN_DICTIONARY = 488;
     extern const int INCORRECT_DICTIONARY_DEFINITION = 489;
+    extern const int CANNOT_FORMAT_DATETIME = 490;
+    extern const int UNACCEPTABLE_URL = 491;
+    extern const int ACCESS_ENTITY_NOT_FOUND = 492;
+    extern const int ACCESS_ENTITY_ALREADY_EXISTS = 493;
+    extern const int ACCESS_ENTITY_FOUND_DUPLICATES = 494;
+    extern const int ACCESS_ENTITY_STORAGE_READONLY = 495;
+    extern const int QUOTA_REQUIRES_CLIENT_KEY = 496;
+    extern const int NOT_ENOUGH_PRIVILEGES = 497;
+    extern const int LIMIT_BY_WITH_TIES_IS_NOT_SUPPORTED = 498;
 
     extern const int KEEPER_EXCEPTION = 999;
     extern const int POCO_EXCEPTION = 1000;
