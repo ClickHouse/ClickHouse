42c59af1272421d40dceb68b5c988d5176958bea
Alexander Kuzmenkov <akuzm@yandex-team.ru>
2019-12-09 17:49:21 +0300

    Remove trailing whitespace.

--- dbms/src/Common/ShellCommand.h
+++ dbms/src/Common/ShellCommand.h
@@ -33,7 +33,7 @@ private:
     ShellCommand(pid_t pid_, int in_fd_, int out_fd_, int err_fd_, bool terminate_in_destructor_);
 
     static Poco::Logger * getLogger();
-    
+
     /// Print command name and the list of arguments to log. NOTE: No escaping of arguments is performed.
     static void logCommand(const char * filename, char * const argv[]);
 
