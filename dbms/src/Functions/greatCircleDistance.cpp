41a6eedadc12bf3f6dbd470001e218cf1f04bfcc
Alexey Milovidov <milovidov@yandex-team.ru>
2019-12-09 04:02:09 +0300

    Added comment

--- dbms/src/Functions/greatCircleDistance.cpp
+++ dbms/src/Functions/greatCircleDistance.cpp
@@ -152,6 +152,11 @@ float distance(float lon1deg, float lat1deg, float lon2deg, float lat2deg)
         // points are close enough; use flat ellipsoid model
         // interpolate metric coefficients using latitudes midpoint
 
+        /// Why comparing only difference in longitude?
+        /// If longitudes are different enough, there is a big difference between great circle line and a line with constant latitude.
+        ///  (Remember how a plane flies from Moscow to New York)
+        /// But if longitude is close but latitude is different enough, there is no difference between meridian and great circle line.
+
         float latitude_midpoint = (lat1deg + lat2deg + 180) * METRIC_LUT_SIZE / 360; // [-90, 90] degrees -> [0, KTABLE] indexes
         size_t latitude_midpoint_index = static_cast<size_t>(latitude_midpoint) & (METRIC_LUT_SIZE - 1);
 
