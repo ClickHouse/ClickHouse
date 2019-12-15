e2f347a5ad68c9a33fa39eaf43671f4eeca1f7dc
Andrei Bodrov <ap.bodrov@gmail.com>
2019-12-15 16:41:50 +0300

    fix test

--- dbms/tests/queries/0_stateless/01035_avg_weighted.sql
+++
@@ -1,4 +0,0 @@
-SELECT avgWeighted(x, weight) FROM (SELECT t.1 AS x, t.2 AS weight FROM (SELECT arrayJoin([(1, 5), (2, 4), (3, 3), (4, 2), (5, 1)]) AS t));
-SELECT avgWeighted(x, weight) FROM (SELECT t.1 AS x, t.2 AS weight FROM (SELECT arrayJoin([(1, 0), (2, 0), (3, 0), (4, 0), (5, 0)]) AS t));
-
-SELECT avgWeighted(toDecimal64(0, 0), toFloat64(0))
