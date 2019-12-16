0042c8271e49f37256e2c577610685faefca9d38
Alexey Milovidov <milovidov@yandex-team.ru>
2019-12-16 21:41:51 +0300

    Changes after review of #8034

--- dbms/src/Functions/h3GetResolution.cpp
+++ dbms/src/Functions/h3GetResolution.cpp
@@ -7,19 +7,8 @@
 #    include <Common/typeid_cast.h>
 #    include <ext/range.h>
 
-
-extern "C" {
-#    ifdef __clang__
-#        pragma clang diagnostic push
-#        pragma clang diagnostic ignored "-Wdocumentation"
-#    endif
-
 #    include <h3api.h>
 
-#    ifdef __clang__
-#        pragma clang diagnostic pop
-#    endif
-}
 
 namespace DB
 {
