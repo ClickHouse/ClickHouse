326e418434f3285ecb2feadcae973824eb3f9213
Mikhail Korotov <millb@sandbox-410109003>
2019-12-18 15:01:28 +0300

    Test added

--- dbms/tests/queries/0_stateless/01039_test_setting_parse.sql
+++ dbms/tests/queries/0_stateless/01039_test_setting_parse.sql
@@ -1,7 +1,16 @@
-SET max_memory_usage = 10000000001;
-
+SET max_memory_usage = '1G';
 SELECT value FROM system.settings WHERE name = 'max_memory_usage';
-
-SET max_memory_usage = '1G'; -- { serverError 27 }
-
+SET max_memory_usage = '3Gi';
+SELECT value FROM system.settings WHERE name = 'max_memory_usage';
+SET max_memory_usage = '1567k';
+SELECT value FROM system.settings WHERE name = 'max_memory_usage';
+SET max_memory_usage = '123ki';
+SELECT value FROM system.settings WHERE name = 'max_memory_usage';
+SET max_memory_usage = '12M';
+SELECT value FROM system.settings WHERE name = 'max_memory_usage';
+SET max_memory_usage = '31Mi';
+SELECT value FROM system.settings WHERE name = 'max_memory_usage';
+SET max_memory_usage = '1T';
+SELECT value FROM system.settings WHERE name = 'max_memory_usage';
+SET max_memory_usage = '1Ti';
 SELECT value FROM system.settings WHERE name = 'max_memory_usage';
