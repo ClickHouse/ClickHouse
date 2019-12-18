a51fbf8f85eecee4e502fb143c6b2eb849ad2af3
Andrei Bodrov <ap.bodrov@gmail.com>
2019-12-18 15:48:40 +0300

    typo :(

--- dbms/tests/queries/0_stateless/01035_avg_weighted.sh
+++ dbms/tests/queries/0_stateless/01035_avg_weighted.sh
@@ -8,4 +8,4 @@ ${CLICKHOUSE_CLIENT} --query="SELECT avgWeighted(x, weight) FROM (SELECT t.1 AS 
 ${CLICKHOUSE_CLIENT} --query="SELECT avgWeighted(x, weight) FROM (SELECT t.1 AS x, t.2 AS weight FROM (SELECT arrayJoin([(1, 0), (2, 0), (3, 0), (4, 0), (5, 0)]) AS t));"
 
 echo `${CLICKHOUSE_CLIENT} --server_logs_file=/dev/null --query="SELECT avgWeighted(toDecimal64(0, 0), toFloat64(0))" 2>&1` \
-  | grep -c 'Code: 43. DB::Exception: .* DB::Exception:.* Different types .* of argument for aggregate function avgWeighted'
+  | grep -c 'Code: 43. DB::Exception: .* DB::Exception:.* Different types .* of arguments for aggregate function avgWeighted'
