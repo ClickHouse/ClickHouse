dc17b436d1a80001d95b8bc7a07b37197ca7e2b8
alexey-milovidov <milovidov@yandex-team.ru>
2019-12-19 21:09:29 +0300

    Merge pull request #8241 from vitlibar/fix-allowed-client-hosts-checks
    Fix checking if a client host is allowed.
    
    (cherry picked from commit 0b70706f04195d60ce205dec6826d7e6b815a80e)

--- dbms/src/Access/AllowedClientHosts.cpp
+++ dbms/src/Access/AllowedClientHosts.cpp
@@ -9,6 +9,7 @@
 #include <ext/scope_guard.h>
 #include <boost/range/algorithm/find.hpp>
 #include <boost/range/algorithm/find_first_of.hpp>
+#include <ifaddrs.h>
 
 
 namespace DB
@@ -30,9 +31,11 @@ namespace
         if (addr.family() == IPAddress::IPv6)
             return addr;
 
+        if (addr.isLoopback())
+            return IPAddress("::1");
+
         return IPAddress("::FFFF:" + addr.toString());
     }
-
 
     IPAddress maskToIPv6(const IPAddress & mask)
     {
@@ -48,11 +51,11 @@ namespace
         IPAddress addr_v6 = toIPv6(address);
 
         /// Resolve by hand, because Poco don't use AI_ALL flag but we need it.
-        addrinfo * ai = nullptr;
+        addrinfo * ai_begin = nullptr;
         SCOPE_EXIT(
         {
-            if (ai)
-                freeaddrinfo(ai);
+            if (ai_begin)
+                freeaddrinfo(ai_begin);
         });
 
         addrinfo hints;
@@ -60,26 +63,26 @@ namespace
         hints.ai_family = AF_UNSPEC;
         hints.ai_flags |= AI_V4MAPPED | AI_ALL;
 
-        int ret = getaddrinfo(host.c_str(), nullptr, &hints, &ai);
-        if (0 != ret)
-            throw Exception("Cannot getaddrinfo: " + std::string(gai_strerror(ret)), ErrorCodes::DNS_ERROR);
+        int err = getaddrinfo(host.c_str(), nullptr, &hints, &ai_begin);
+        if (err)
+            throw Exception("Cannot getaddrinfo(" + host + "): " + gai_strerror(err), ErrorCodes::DNS_ERROR);
 
-        for (; ai != nullptr; ai = ai->ai_next)
+        for (const addrinfo * ai = ai_begin; ai; ai = ai->ai_next)
         {
             if (ai->ai_addrlen && ai->ai_addr)
             {
-                if (ai->ai_family == AF_INET6)
+                if (ai->ai_family == AF_INET)
                 {
-                    if (addr_v6 == IPAddress(
-                        &reinterpret_cast<sockaddr_in6*>(ai->ai_addr)->sin6_addr, sizeof(in6_addr),
-                        reinterpret_cast<sockaddr_in6*>(ai->ai_addr)->sin6_scope_id))
+                    const auto & sin = *reinterpret_cast<const sockaddr_in *>(ai->ai_addr);
+                    if (addr_v6 == toIPv6(IPAddress(&sin.sin_addr, sizeof(sin.sin_addr))))
                     {
                         return true;
                     }
                 }
-                else if (ai->ai_family == AF_INET)
+                else if (ai->ai_family == AF_INET6)
                 {
-                    if (addr_v6 == toIPv6(IPAddress(&reinterpret_cast<sockaddr_in *>(ai->ai_addr)->sin_addr, sizeof(in_addr))))
+                    const auto & sin = *reinterpret_cast<const sockaddr_in6*>(ai->ai_addr);
+                    if (addr_v6 == IPAddress(&sin.sin6_addr, sizeof(sin.sin6_addr), sin.sin6_scope_id))
                     {
                         return true;
                     }
@@ -99,19 +102,61 @@ namespace
     }
 
 
+    std::vector<IPAddress> getAddressesOfLocalhostImpl()
+    {
+        std::vector<IPAddress> addresses;
+
+        ifaddrs * ifa_begin = nullptr;
+        SCOPE_EXIT({
+            if (ifa_begin)
+                freeifaddrs(ifa_begin);
+        });
+
+        int err = getifaddrs(&ifa_begin);
+        if (err)
+            return {IPAddress{"127.0.0.1"}, IPAddress{"::1"}};
+
+        for (const ifaddrs * ifa = ifa_begin; ifa; ifa = ifa->ifa_next)
+        {
+            if (!ifa->ifa_addr)
+                continue;
+            if (ifa->ifa_addr->sa_family == AF_INET)
+            {
+                const auto & sin = *reinterpret_cast<const sockaddr_in *>(ifa->ifa_addr);
+                addresses.push_back(toIPv6(IPAddress(&sin.sin_addr, sizeof(sin.sin_addr))));
+            }
+            else if (ifa->ifa_addr->sa_family == AF_INET6)
+            {
+                const auto & sin = *reinterpret_cast<const sockaddr_in6 *>(ifa->ifa_addr);
+                addresses.push_back(IPAddress(&sin.sin6_addr, sizeof(sin.sin6_addr), sin.sin6_scope_id));
+            }
+        }
+        return addresses;
+    }
+
+
+    /// Checks if a specified address pointers to the localhost.
+    bool isLocalAddress(const IPAddress & address)
+    {
+        static const std::vector<IPAddress> local_addresses = getAddressesOfLocalhostImpl();
+        return boost::range::find(local_addresses, address) != local_addresses.end();
+    }
+
+
     String getHostByAddressImpl(const IPAddress & address)
     {
         Poco::Net::SocketAddress sock_addr(address, 0);
 
         /// Resolve by hand, because Poco library doesn't have such functionality.
         char host[1024];
-        int gai_errno = getnameinfo(sock_addr.addr(), sock_addr.length(), host, sizeof(host), nullptr, 0, NI_NAMEREQD);
-        if (0 != gai_errno)
-            throw Exception("Cannot getnameinfo: " + std::string(gai_strerror(gai_errno)), ErrorCodes::DNS_ERROR);
+        int err = getnameinfo(sock_addr.addr(), sock_addr.length(), host, sizeof(host), nullptr, 0, NI_NAMEREQD);
+        if (err)
+            throw Exception("Cannot getnameinfo(" + address.toString() + "): " + gai_strerror(err), ErrorCodes::DNS_ERROR);
 
         /// Check that PTR record is resolved back to client address
         if (!isAddressOfHost(address, host))
             throw Exception("Host " + String(host) + " isn't resolved back to " + address.toString(), ErrorCodes::DNS_ERROR);
+
         return host;
     }
 
@@ -158,6 +203,7 @@ AllowedClientHosts::AllowedClientHosts(const AllowedClientHosts & src)
 AllowedClientHosts & AllowedClientHosts::operator =(const AllowedClientHosts & src)
 {
     addresses = src.addresses;
+    loopback = src.loopback;
     subnets = src.subnets;
     host_names = src.host_names;
     host_regexps = src.host_regexps;
@@ -175,6 +221,7 @@ AllowedClientHosts::AllowedClientHosts(AllowedClientHosts && src)
 AllowedClientHosts & AllowedClientHosts::operator =(AllowedClientHosts && src)
 {
     addresses = std::move(src.addresses);
+    loopback = src.loopback;
     subnets = std::move(src.subnets);
     host_names = std::move(src.host_names);
     host_regexps = std::move(src.host_regexps);
@@ -186,6 +233,7 @@ AllowedClientHosts & AllowedClientHosts::operator =(AllowedClientHosts && src)
 void AllowedClientHosts::clear()
 {
     addresses.clear();
+    loopback = false;
     subnets.clear();
     host_names.clear();
     host_regexps.clear();
@@ -204,6 +252,8 @@ void AllowedClientHosts::addAddress(const IPAddress & address)
     IPAddress addr_v6 = toIPv6(address);
     if (boost::range::find(addresses, addr_v6) == addresses.end())
         addresses.push_back(addr_v6);
+    if (addr_v6.isLoopback())
+        loopback = true;
 }
 
 
@@ -291,28 +341,26 @@ bool AllowedClientHosts::containsAllAddresses() const
 }
 
 
-bool AllowedClientHosts::contains(const IPAddress & address) const
-{
-    return containsImpl(address, String(), nullptr);
-}
-
-
 void AllowedClientHosts::checkContains(const IPAddress & address, const String & user_name) const
 {
-    String error;
-    if (!containsImpl(address, user_name, &error))
-        throw Exception(error, ErrorCodes::IP_ADDRESS_NOT_ALLOWED);
+    if (!contains(address))
+    {
+        if (user_name.empty())
+            throw Exception("It's not allowed to connect from address " + address.toString(), ErrorCodes::IP_ADDRESS_NOT_ALLOWED);
+        else
+            throw Exception("User " + user_name + " is not allowed to connect from address " + address.toString(), ErrorCodes::IP_ADDRESS_NOT_ALLOWED);
+    }
 }
 
 
-bool AllowedClientHosts::containsImpl(const IPAddress & address, const String & user_name, String * error) const
+bool AllowedClientHosts::contains(const IPAddress & address) const
 {
-    if (error)
-        error->clear();
-
     /// Check `ip_addresses`.
     IPAddress addr_v6 = toIPv6(address);
     if (boost::range::find(addresses, addr_v6) != addresses.end())
+        return true;
+
+    if (loopback && isLocalAddress(addr_v6))
         return true;
 
     /// Check `ip_subnets`.
@@ -325,14 +373,13 @@ bool AllowedClientHosts::containsImpl(const IPAddress & address, const String & 
     {
         try
         {
-            if (isAddressOfHost(address, host_name))
+            if (isAddressOfHost(addr_v6, host_name))
                 return true;
         }
-        catch (Exception & e)
+        catch (const Exception & e)
         {
             if (e.code() != ErrorCodes::DNS_ERROR)
-                e.rethrow();
-
+                throw;
             /// Try to ignore DNS errors: if host cannot be resolved, skip it and try next.
             LOG_WARNING(
                 &Logger::get("AddressPatterns"),
@@ -342,38 +389,31 @@ bool AllowedClientHosts::containsImpl(const IPAddress & address, const String & 
     }
 
     /// Check `host_regexps`.
-    if (!host_regexps.empty())
+    try
     {
-        compileRegexps();
-        try
+        String resolved_host = getHostByAddress(addr_v6);
+        if (!resolved_host.empty())
         {
-            String resolved_host = getHostByAddress(address);
+            compileRegexps();
             for (const auto & compiled_regexp : compiled_host_regexps)
             {
-                if (compiled_regexp && compiled_regexp->match(resolved_host))
+                Poco::RegularExpression::Match match;
+                if (compiled_regexp && compiled_regexp->match(resolved_host, match))
                     return true;
             }
         }
-        catch (Exception & e)
-        {
-            if (e.code() != ErrorCodes::DNS_ERROR)
-                e.rethrow();
-
-            /// Try to ignore DNS errors: if host cannot be resolved, skip it and try next.
-            LOG_WARNING(
-                &Logger::get("AddressPatterns"),
-                "Failed to check if the allowed client hosts contain address " << address.toString() << ". " << e.displayText()
-                                                                               << ", code = " << e.code());
-        }
     }
-
-    if (error)
+    catch (const Exception & e)
     {
-        if (user_name.empty())
-            *error = "It's not allowed to connect from address " + address.toString();
-        else
-            *error = "User " + user_name + " is not allowed to connect from address " + address.toString();
+        if (e.code() != ErrorCodes::DNS_ERROR)
+            throw;
+        /// Try to ignore DNS errors: if host cannot be resolved, skip it and try next.
+        LOG_WARNING(
+            &Logger::get("AddressPatterns"),
+            "Failed to check if the allowed client hosts contain address " << address.toString() << ". " << e.displayText()
+                                                                         << ", code = " << e.code());
     }
+
     return false;
 }
 
