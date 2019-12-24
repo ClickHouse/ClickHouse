2da0f07353981e26f0f39e60be89a2abe36beb2f
Alexey Milovidov <milovidov@yandex-team.ru>
2019-12-24 20:16:38 +0300

    Fixed build

--- dbms/src/IO/WriteBufferFromFileBase.h
+++ dbms/src/IO/WriteBufferFromFileBase.h
@@ -18,7 +18,7 @@ public:
     off_t seek(off_t off, int whence = SEEK_SET);
     void truncate(off_t length = 0);
     virtual off_t getPositionInFile() = 0;
-    virtual void sync() = 0;
+    void sync() override = 0;
     virtual std::string getFileName() const = 0;
     virtual int getFD() const = 0;
 
