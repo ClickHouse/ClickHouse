8a55049ede7d395768a40bb4a9eaffd0bb7dc705
achulkov2 <achulkov2@gmail.com>
2019-12-24 21:22:20 +0300

    First test

---
+++ dbms/tests/queries/0_stateless/01037_polygon_dict.sql
@@ -0,0 +1,70 @@
+-- Must use `test_00950` database and these tables - they're configured in dbms/tests/*_dictionary.xml
+create database if not exists test_01037;
+use test_01037;
+drop table if exists polygons;
+
+create table polygons (key Array(Array(Array(Array(Float64)))), name String, u64 UInt64) Engine = Memory;
+insert into polygons values (
+                             [
+                                 [
+                                     [
+                                         [1, 3],
+                                         [1, 1],
+                                         [3, 1],
+                                         [3, -1],
+                                         [1, -1],
+                                         [1, -3],
+                                         [-1. -3],
+                                         [-1, -1],
+                                         [-3, -1],
+                                         [-3, 1],
+                                         [-1, 1],
+                                         [-1, 3]
+                                     ]
+                                 ],
+                                 [
+                                     [
+                                         [5, 5],
+                                         [5, 1],
+                                         [7, 1],
+                                         [7, 7],
+                                         [1, 7],
+                                         [1, 5]
+                                     ]
+                                 ]
+                             ],
+                             'Click',
+                             42);
+insert into polygons values (
+                             [
+                                 [
+                                     [
+                                         [5, 5],
+                                         [5, -5],
+                                         [-5, -5],
+                                         [-5, 5]
+                                     ],
+                                     [
+                                         [1, 3],
+                                         [1, 1],
+                                         [3, 1],
+                                         [3, -1],
+                                         [1, -1],
+                                         [1, -3],
+                                         [-1. -3],
+                                         [-1, -1],
+                                         [-3, -1],
+                                         [-3, 1],
+                                         [-1, 1],
+                                         [-1, 3]
+                                     ]
+                                 ]
+                             ],
+                             'House',
+                             314159);
+
+select 'dictGet', 'polygons' as dict_name, (0.0, 0.0) as key,
+       dictGet(dict_name, 'name', key),
+       dictGet(dict_name, 'u64', key);
+drop table polygons;
+drop database test_01037;
