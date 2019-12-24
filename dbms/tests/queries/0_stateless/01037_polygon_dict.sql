3da84a31fb462dd93392fd301ee0574d61107a4b
achulkov2 <achulkov2@gmail.com>
2019-12-24 21:48:48 +0300

    Trying to fix test

--- dbms/tests/queries/0_stateless/01037_polygon_dict.sql
+++ dbms/tests/queries/0_stateless/01037_polygon_dict.sql
@@ -4,9 +4,7 @@ use test_01037;
 drop table if exists polygons;
 
 create table polygons (key Array(Array(Array(Array(Float64)))), name String, u64 UInt64) Engine = Memory;
-insert into polygons values ([[[[1, 3], [1, 1], [3, 1], [3, -1], [1, -1], [1, -3], [-1. -3], [-1, -1], [-3, -1], [-3, 1], [-1, 1], [-1, 3]]], [[[5, 5], [5, 1], [7, 1], [7, 7], [1, 7], [1, 5]]]],
-                             'Click',
-                             42);
+insert into polygons values ([[[[1, 3], [1, 1], [3, 1], [3, -1], [1, -1], [1, -3], [-1. -3], [-1, -1], [-3, -1], [-3, 1], [-1, 1], [-1, 3]]], [[[5, 5], [5, 1], [7, 1], [7, 7], [1, 7], [1, 5]]]], 'Click', 42);
 /*
 insert into polygons values (
                              [
