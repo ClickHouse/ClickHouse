20217a041846ede6de2482dc5244501f0befa7a3
achulkov2 <achulkov2@gmail.com>
2019-12-24 22:28:41 +0300

    Trying to fix test

--- dbms/tests/queries/0_stateless/01037_polygon_dict.sql
+++ dbms/tests/queries/0_stateless/01037_polygon_dict.sql
@@ -5,36 +5,8 @@ drop table if exists polygons;
 
 create table polygons (key Array(Array(Array(Array(Float64)))), name String, u64 UInt64) Engine = Memory;
 insert into polygons values ([[[[1, 3], [1, 1], [3, 1], [3, -1], [1, -1], [1, -3], [-1. -3], [-1, -1], [-3, -1], [-3, 1], [-1, 1], [-1, 3]]], [[[5, 5], [5, 1], [7, 1], [7, 7], [1, 7], [1, 5]]]], 'Click', 42);
-/*
-insert into polygons values (
-                             [
-                                 [
-                                     [
-                                         [5, 5],
-                                         [5, -5],
-                                         [-5, -5],
-                                         [-5, 5]
-                                     ],
-                                     [
-                                         [1, 3],
-                                         [1, 1],
-                                         [3, 1],
-                                         [3, -1],
-                                         [1, -1],
-                                         [1, -3],
-                                         [-1. -3],
-                                         [-1, -1],
-                                         [-3, -1],
-                                         [-3, 1],
-                                         [-1, 1],
-                                         [-1, 3]
-                                     ]
-                                 ]
-                             ],
-                             'House',
-                             314159);
-*/
-select 'dictGet', 'polygons' as dict_name, (0.0, 0.0) as key,
+insert into polygons values ([[[[5, 5], [5, -5], [-5, -5], [-5, 5]], [[1, 3], [1, 1], [3, 1], [3, -1], [1, -1], [1, -3], [-1. -3], [-1, -1], [-3, -1], [-3, 1], [-1, 1], [-1, 3]]]], 'House', 314159);
+select 'dictGet', 'polygons' as dict_name, tuple(0.0, 0.0) as key,
        dictGet(dict_name, 'name', key),
        dictGet(dict_name, 'u64', key);
 drop table polygons;
