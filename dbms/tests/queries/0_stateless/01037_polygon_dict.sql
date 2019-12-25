7c1fa017bac10fcf5af27c79a7b9b318e4c6e8ab
achulkov2 <achulkov2@gmail.com>
2019-12-25 15:05:37 +0300

    Trying to fix test

--- dbms/tests/queries/0_stateless/01037_polygon_dict.sql
+++ dbms/tests/queries/0_stateless/01037_polygon_dict.sql
@@ -1,13 +1,30 @@
--- Must use `test_00950` database and these tables - they're configured in dbms/tests/*_dictionary.xml
-create database if not exists test_01037;
-use test_01037;
-drop table if exists polygons;
+SET send_logs_level = 'none';
 
-create table polygons (key Array(Array(Array(Array(Float64)))), name String, u64 UInt64) Engine = Memory;
-insert into polygons values ([[[[1, 3], [1, 1], [3, 1], [3, -1], [1, -1], [1, -3], [-1. -3], [-1, -1], [-3, -1], [-3, 1], [-1, 1], [-1, 3]]], [[[5, 5], [5, 1], [7, 1], [7, 7], [1, 7], [1, 5]]]], 'Click', 42);
-insert into polygons values ([[[[5, 5], [5, -5], [-5, -5], [-5, 5]], [[1, 3], [1, 1], [3, 1], [3, -1], [1, -1], [1, -3], [-1. -3], [-1, -1], [-3, -1], [-3, 1], [-1, 1], [-1, 3]]]], 'House', 314159);
-select 'dictGet', 'polygons' as dict_name, tuple(0.0, 0.0) as key,
+DROP DATABASE IF EXISTS test_01037;
+
+CREATE DATABASE test_01037 Engine = Ordinary;
+
+DROP TABLE IF EXISTS test_01037.polygons;
+
+CREATE TABLE polygons (key Array(Array(Array(Array(Float64)))), name String, u64 UInt64) Engine = Memory;
+INSERT INTO polygons VALUES ([[[[1, 3], [1, 1], [3, 1], [3, -1], [1, -1], [1, -3], [-1. -3], [-1, -1], [-3, -1], [-3, 1], [-1, 1], [-1, 3]]], [[[5, 5], [5, 1], [7, 1], [7, 7], [1, 7], [1, 5]]]], 'Click', 42);
+INSERT INTO polygons VALUES ([[[[5, 5], [5, -5], [-5, -5], [-5, 5]], [[1, 3], [1, 1], [3, 1], [3, -1], [1, -1], [1, -3], [-1. -3], [-1, -1], [-3, -1], [-3, 1], [-1, 1], [-1, 3]]]], 'House', 314159);
+
+CREATE DICTIONARY test_01037.dict
+(
+  polygon Array(Array(Array(Array(Float64)))),
+  name String DEFAULT '',
+  value UInt64 DEFAULT 42
+)
+PRIMARY KEY polygon
+SOURCE(CLICKHOUSE(HOST 'localhost' PORT 9000 USER 'default' TABLE 'polygons' PASSWORD '' DB 'test_01037'))
+LIFETIME(MIN 1 MAX 10)
+LAYOUT(POLYGON())
+
+select 'dictGet', 'test_01037.dict' as dict_name, tuple(0.0, 0.0) as key,
        dictGet(dict_name, 'name', key),
        dictGet(dict_name, 'u64', key);
-drop table polygons;
-drop database test_01037;
+
+DROP DICTIONARY IF EXISTS test_01037.dict;
+DROP TABLE IF EXISTS test_01037.polygons;
+DROP DATABASE IF EXISTS test_01037;
