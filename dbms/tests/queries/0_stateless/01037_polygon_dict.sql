b6d0f23ac86c62ff578a9626ce1fbeec9d8c8950
achulkov2 <achulkov2@gmail.com>
2019-12-25 15:39:49 +0300

    Trying to fix test

--- dbms/tests/queries/0_stateless/01037_polygon_dict.sql
+++ dbms/tests/queries/0_stateless/01037_polygon_dict.sql
@@ -4,27 +4,34 @@ DROP DATABASE IF EXISTS test_01037;
 
 CREATE DATABASE test_01037 Engine = Ordinary;
 
+DROP DICTIONARY IF EXISTS test_01037.dict;
 DROP TABLE IF EXISTS test_01037.polygons;
 
-CREATE TABLE polygons (key Array(Array(Array(Array(Float64)))), name String, u64 UInt64) Engine = Memory;
-INSERT INTO polygons VALUES ([[[[1, 3], [1, 1], [3, 1], [3, -1], [1, -1], [1, -3], [-1. -3], [-1, -1], [-3, -1], [-3, 1], [-1, 1], [-1, 3]]], [[[5, 5], [5, 1], [7, 1], [7, 7], [1, 7], [1, 5]]]], 'Click', 42);
-INSERT INTO polygons VALUES ([[[[5, 5], [5, -5], [-5, -5], [-5, 5]], [[1, 3], [1, 1], [3, 1], [3, -1], [1, -1], [1, -3], [-1. -3], [-1, -1], [-3, -1], [-3, 1], [-1, 1], [-1, 3]]]], 'House', 314159);
+CREATE TABLE test_01037.polygons (key Array(Array(Array(Array(Float64)))), name String, value UInt64) Engine = Memory;
+INSERT INTO test_01037.polygons VALUES ([[[[1, 3], [1, 1], [3, 1], [3, -1], [1, -1], [1, -3], [-1, -3], [-1, -1], [-3, -1], [-3, 1], [-1, 1], [-1, 3]]], [[[5, 5], [5, 1], [7, 1], [7, 7], [1, 7], [1, 5]]]], 'Click', 42);
+INSERT INTO test_01037.polygons VALUES ([[[[5, 5], [5, -5], [-5, -5], [-5, 5]], [[1, 3], [1, 1], [3, 1], [3, -1], [1, -1], [1, -3], [-1, -3], [-1, -1], [-3, -1], [-3, 1], [-1, 1], [-1, 3]]]], 'House', 314159);
 
 CREATE DICTIONARY test_01037.dict
 (
-  polygon Array(Array(Array(Array(Float64)))),
+  key Array(Array(Array(Array(Float64)))),
   name String DEFAULT '',
   value UInt64 DEFAULT 42
 )
-PRIMARY KEY polygon
+PRIMARY KEY key
 SOURCE(CLICKHOUSE(HOST 'localhost' PORT 9000 USER 'default' TABLE 'polygons' PASSWORD '' DB 'test_01037'))
 LIFETIME(MIN 1 MAX 10)
-LAYOUT(POLYGON())
+LAYOUT(POLYGON());
 
-select 'dictGet', 'test_01037.dict' as dict_name, tuple(0.0, 0.0) as key,
+DROP TABLE IF EXISTS test_01037.points;
+
+CREATE TABLE test_01037.points (x Float64, y Float64) ENGINE = Memory;
+INSERT INTO test_01037.points VALUES (0.0, 0.0), (3.0, 3.0), (5.0, 6.0), (-100.0, -42.0), (5.0, 5.0);
+
+select 'dictGet', 'test_01037.dict' as dict_name, tuple(x, y) as key,
        dictGet(dict_name, 'name', key),
-       dictGet(dict_name, 'u64', key);
+       dictGet(dict_name, 'value', key) from test_01037.points;
 
-DROP DICTIONARY IF EXISTS test_01037.dict;
-DROP TABLE IF EXISTS test_01037.polygons;
-DROP DATABASE IF EXISTS test_01037;
+DROP DICTIONARY test_01037.dict;
+DROP TABLE test_01037.polygons;
+DROP TABLE test_01037.points;
+DROP DATABASE test_01037;
