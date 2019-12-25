c5d9a7f850d32771f32b0ab352c454f8e0da1fff
Vasily Nemkov <V.Nemkov@gmail.com>
2019-12-26 00:07:53 +0300

    Argument validation fixes
    * Fixed crash when converting nullptr char pointer to std::string
    * Updatedexepected error code for toDateTime64

--- dbms/tests/queries/0_stateless/00921_datetime64_basic.sql
+++ dbms/tests/queries/0_stateless/00921_datetime64_basic.sql
@@ -9,7 +9,7 @@ SELECT CAST(1 as DateTime64(3, 'qqq')); -- { serverError 1000 } # invalid timezo
 
 SELECT toDateTime64('2019-09-16 19:20:11.234', 'abc'); -- { serverError 43 } # invalid scale
 SELECT toDateTime64('2019-09-16 19:20:11.234', 100); -- { serverError 69 } # too big scale
-SELECT toDateTime64(CAST([['CLb5Ph ']], 'String'), uniqHLL12('2Gs1V', 752)); -- { serverError 43 } # non-const string and non-const scale
+SELECT toDateTime64(CAST([['CLb5Ph ']], 'String'), uniqHLL12('2Gs1V', 752)); -- { serverError 44 } # non-const string and non-const scale
 SELECT toDateTime64('2019-09-16 19:20:11.234', 3, 'qqq'); -- { serverError 1000 } # invalid timezone
 
 SELECT ignore(now64(gccMurmurHash())); -- { serverError 43 } # Illegal argument type
