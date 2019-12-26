35b0a928455d952cd82db5a32636b0d6630d18d7
Ivan Lezhankin <ilezhankin@yandex-team.ru>
2019-12-26 18:30:25 +0300

    Initial replacement readline → replxx

---
+++ libs/libcommon/include/common/LineReader.h
@@ -0,0 +1,34 @@
+#pragma once
+
+#include <common/Types.h>
+
+#ifdef USE_REPLXX
+#   include <replxx.hxx>
+#endif
+
+class LineReader
+{
+public:
+    LineReader(const String & history_file_path, char extender, char delimiter = 0);  /// if delimiter != 0, then it's multiline mode
+    ~LineReader();
+
+    /// Reads the whole line until delimiter (in multiline mode) or until the last line without extender.
+    /// If resulting line is empty, it means the user interrupted the input.
+    /// Non-empty line is appended to history - without duplication.
+    /// Typical delimiter is ';' (semicolon) and typical extender is '\' (backslash).
+    String readLine(const String & first_prompt, const String & second_prompt);
+
+private:
+    String input;
+    String prev_line;
+    const String history_file_path;
+    const char extender;
+    const char delimiter;
+
+    bool readOneLine(const String & prompt);
+    void addToHistory(const String & line);
+
+#ifdef USE_REPLXX
+    replxx::Replxx rx;
+#endif
+};
