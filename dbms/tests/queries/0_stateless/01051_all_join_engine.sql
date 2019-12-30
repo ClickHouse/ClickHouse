6b80ba52beb99af0d4c3a5a072da7aa0ce6d86f3
chertus <chertus@gmail.com>
2019-12-30 22:43:40 +0300

    fix crash in StorageJoin (add restriction)

--- dbms/tests/queries/0_stateless/01051_all_join_engine.sql
+++ dbms/tests/queries/0_stateless/01051_all_join_engine.sql
@@ -33,20 +33,43 @@ SELECT * FROM t1 RIGHT JOIN right_join j USING(x) ORDER BY x, str, s;
 SELECT 'full';
 SELECT * FROM t1 FULL JOIN full_join j USING(x) ORDER BY x, str, s;
 
--- TODO
--- SET join_use_nulls = 1;
--- 
--- SELECT 'left (join_use_nulls)';
--- SELECT * FROM t1 LEFT JOIN left_join j USING(x) ORDER BY x, str, s;
--- 
--- SELECT 'inner (join_use_nulls)';
--- SELECT * FROM t1 INNER JOIN inner_join j USING(x) ORDER BY x, str, s;
--- 
--- SELECT 'right (join_use_nulls)';
--- SELECT * FROM t1 RIGHT JOIN right_join j USING(x) ORDER BY x, str, s;
--- 
--- SELECT 'full (join_use_nulls)';
--- SELECT * FROM t1 FULL JOIN full_join j USING(x) ORDER BY x, str, s;
+SET join_use_nulls = 1;
+
+SELECT * FROM t1 LEFT JOIN left_join j USING(x) ORDER BY x, str, s; -- { serverError 264 }
+SELECT * FROM t1 FULL JOIN full_join j USING(x) ORDER BY x, str, s; -- { serverError 264 }
+
+SELECT 'inner (join_use_nulls mix)';
+SELECT * FROM t1 INNER JOIN inner_join j USING(x) ORDER BY x, str, s;
+
+SELECT 'right (join_use_nulls mix)';
+SELECT * FROM t1 RIGHT JOIN right_join j USING(x) ORDER BY x, str, s;
+
+DROP TABLE left_join;
+DROP TABLE inner_join;
+DROP TABLE right_join;
+DROP TABLE full_join;
+
+CREATE TABLE left_join (x UInt32, s String) engine = Join(ALL, LEFT, x) SETTINGS join_use_nulls = 1;
+CREATE TABLE inner_join (x UInt32, s String) engine = Join(ALL, INNER, x) SETTINGS join_use_nulls = 1;
+CREATE TABLE right_join (x UInt32, s String) engine = Join(ALL, RIGHT, x) SETTINGS join_use_nulls = 1;
+CREATE TABLE full_join (x UInt32, s String) engine = Join(ALL, FULL, x) SETTINGS join_use_nulls = 1;
+
+INSERT INTO left_join (x, s) VALUES (2, 'b1'), (2, 'b2'), (4, 'b3'), (4, 'b4'), (4, 'b5'), (5, 'b6');
+INSERT INTO inner_join (x, s) VALUES (2, 'b1'), (2, 'b2'), (4, 'b3'), (4, 'b4'), (4, 'b5'), (5, 'b6');
+INSERT INTO right_join (x, s) VALUES (2, 'b1'), (2, 'b2'), (4, 'b3'), (4, 'b4'), (4, 'b5'), (5, 'b6');
+INSERT INTO full_join (x, s) VALUES (2, 'b1'), (2, 'b2'), (4, 'b3'), (4, 'b4'), (4, 'b5'), (5, 'b6');
+
+SELECT 'left (join_use_nulls)';
+SELECT * FROM t1 LEFT JOIN left_join j USING(x) ORDER BY x, str, s;
+
+SELECT 'inner (join_use_nulls)';
+SELECT * FROM t1 INNER JOIN inner_join j USING(x) ORDER BY x, str, s;
+
+SELECT 'right (join_use_nulls)';
+SELECT * FROM t1 RIGHT JOIN right_join j USING(x) ORDER BY x, str, s;
+
+SELECT 'full (join_use_nulls)';
+SELECT * FROM t1 FULL JOIN full_join j USING(x) ORDER BY x, str, s;
 
 DROP TABLE t1;
 
