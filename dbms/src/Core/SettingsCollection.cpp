bb0454bb2e2b7c431b593370b06a7ec753d8648a
Nikolai Kochetov <nik-kochetov@yandex-team.ru>
2020-01-02 21:11:43 +0300

    Revert prev commit.

--- dbms/src/Core/SettingsCollection.cpp
+++ dbms/src/Core/SettingsCollection.cpp
@@ -216,10 +216,6 @@ void SettingMaxThreads::setAuto()
 UInt64 SettingMaxThreads::getAutoValue() const
 {
     static auto res = getNumberOfPhysicalCPUCores();
-
-    if (res > 32)
-        res /= 2;
-
     return res;
 }
 
