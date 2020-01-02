066af460579fef8823b142987fc6a2b6d4763625
Alexey Milovidov <milovidov@yandex-team.ru>
2020-01-02 11:40:00 +0300

    Addition to prev. revision

--- cmake/linux/default_libs.cmake
+++ cmake/linux/default_libs.cmake
@@ -27,6 +27,15 @@ elseif (COMPILER_CLANG)
     set (COMPILER_INCLUDE_DIRS ${COMPILER_CLANG_INCLUDE_DIR})
 endif ()
 
+# Global libraries
+
+add_library(global-libs INTERFACE)
+
+# Unfortunately '-pthread' doesn't work with '-nodefaultlibs'.
+# Just make sure we have pthreads at all.
+set(THREADS_PREFER_PTHREAD_FLAG ON)
+find_package(Threads REQUIRED)
+
 # glibc-compatibility library relies to fixed version of libc headers
 # (because minor changes in function attributes between different glibc versions will introduce incompatibilities)
 # This is for x86_64. For other architectures we have separate toolchains.
@@ -42,18 +51,13 @@ if (ARCH_AMD64)
         ${COMPILER_INCLUDE_DIRS})
 
     # Disable unwanted includes to get more isolated build. The build should not depend on the percularities of the user environment.
+    # NOTE It is very similar to using custom "toolchain". And using custom toolchain is more simple but is less convenient for users.
+    # This is also very helpful to avoid mess with system libraries (e.g. using wrong version of zlib.h)
+    # This will also help for further migration to musl-libc.
+
     set (CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -nostdinc")
     set (CMAKE_C_FLAGS "${CMAKE_C_FLAGS} -nostdinc")
 endif ()
-
-# Global libraries
-
-add_library(global-libs INTERFACE)
-
-# Unfortunately '-pthread' doesn't work with '-nodefaultlibs'.
-# Just make sure we have pthreads at all.
-set(THREADS_PREFER_PTHREAD_FLAG ON)
-find_package(Threads REQUIRED)
 
 add_subdirectory(libs/libglibc-compatibility)
 include (cmake/find/unwind.cmake)
