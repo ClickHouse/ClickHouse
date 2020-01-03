77e55134b57a06c91c4da749485775295906f3de
Azat Khuzhin <a3at.mail@gmail.com>
2020-01-04 00:43:38 +0300

    Add a test for OPTIMIZE of SimpleAggregateFunction(LowCardinality(Nullable()))

--- dbms/tests/queries/0_stateless/00915_simple_aggregate_function.sql
+++ dbms/tests/queries/0_stateless/00915_simple_aggregate_function.sql
@@ -35,4 +35,6 @@ insert into simple values(10,'22222222222222222222222222222222222222222222222222
 select * from simple final;
 select toTypeName(nullable_str),toTypeName(low_str),toTypeName(ip),toTypeName(status) from simple limit 1;
 
+optimize table simple final;
+
 drop table simple;
