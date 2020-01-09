808cd4ed455c4d084e87d472c11b0b17770cd9a7
Vladimir Chebotarev <vladimir.chebotarev@gmail.com>
2020-01-09 20:58:06 +0300

    Added clearing of old DELETE expression during `ALTER MODIFY TTL`.

--- dbms/src/Storages/MergeTree/MergeTreeData.cpp
+++ dbms/src/Storages/MergeTree/MergeTreeData.cpp
@@ -634,7 +634,10 @@ void MergeTreeData::setTTLExpressions(const ColumnsDescription::ColumnTTLs & new
     if (new_ttl_table_ast)
     {
         if (!only_check)
+        {
             move_ttl_entries.clear();
+            ttl_table_ast = nullptr;
+        }
 
         bool seen_delete_ttl = false;
         for (auto ttl_element_ptr : new_ttl_table_ast->children)
