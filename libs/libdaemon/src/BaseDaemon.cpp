e1dd7107dcd3d047fc07996a9bdc1c626a09f724
Alexey Milovidov <milovidov@yandex-team.ru>
2020-01-10 19:47:56 +0300

    Removed some trash

--- libs/libdaemon/src/BaseDaemon.cpp
+++ libs/libdaemon/src/BaseDaemon.cpp
@@ -498,7 +498,7 @@ void BaseDaemon::terminate()
 void BaseDaemon::kill()
 {
     dumpCoverageReportIfPossible();
-    pid.clear();
+    pid.reset();
     if (::raise(SIGKILL) != 0)
         throw Poco::SystemException("cannot kill process");
 }
@@ -668,7 +668,7 @@ void BaseDaemon::initialize(Application & self)
 
     /// Create pid file.
     if (config().has("pid"))
-        pid.seed(config().getString("pid"));
+        pid.emplace(config().getString("pid"));
 
     /// Change path for logging.
     if (!log_path.empty())
@@ -835,7 +835,7 @@ bool isPidRunning(pid_t pid)
     return 0;
 }
 
-void BaseDaemon::PID::seed(const std::string & file_)
+void BaseDaemon::PID::PID(const std::string & file_)
 {
     file = Poco::Path(file_).absolute().toString();
     Poco::File poco_file(file);
@@ -862,35 +862,28 @@ void BaseDaemon::PID::seed(const std::string & file_)
 
     if (-1 == fd)
     {
-        file.clear();
         if (EEXIST == errno)
             throw Poco::Exception("Pid file exists, should not start daemon.");
         throw Poco::CreateFileException("Cannot create pid file.");
     }
 
+    SCOPE_EXIT({ close(fd); });
+
+    std::stringstream s;
+    s << getpid();
+    if (static_cast<ssize_t>(s.str().size()) != write(fd, s.str().c_str(), s.str().size()))
+        throw Poco::Exception("Cannot write to pid file.");
+}
+
+void BaseDaemon::PID::~PID()
+{
     try
     {
-        std::stringstream s;
-        s << getpid();
-        if (static_cast<ssize_t>(s.str().size()) != write(fd, s.str().c_str(), s.str().size()))
-            throw Poco::Exception("Cannot write to pid file.");
-        pid_created = true;
+        Poco::File(file).remove();
     }
     catch (...)
     {
-        close(fd);
-        throw;
-    }
-
-    close(fd);
-}
-
-void BaseDaemon::PID::clear()
-{
-    if (!file.empty() && pid_created)
-    {
-        Poco::File(file).remove();
-        file.clear();
+        DB::tryLogCurrentException(__PRETTY_FUNCTION__);
     }
 }
 
