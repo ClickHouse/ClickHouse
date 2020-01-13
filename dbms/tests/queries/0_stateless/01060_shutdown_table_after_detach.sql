896318fb58f6c9248f6c0e50c81cebd60deb77ff
alexey-milovidov <milovidov@yandex-team.ru>
2020-01-13 17:46:45 +0300

    Merge pull request #8602 from PerformanceVision/remove_partition
    Remove outdated partition when a MergeTree database has been shutdown

---
+++ dbms/tests/queries/0_stateless/01060_shutdown_table_after_detach.sql
@@ -0,0 +1,16 @@
+CREATE TABLE IF NOT EXISTS test Engine = MergeTree ORDER BY number AS SELECT number, toString(rand()) x from numbers(10000000);
+
+SELECT count() FROM test;
+
+ALTER TABLE test detach partition tuple();
+
+SELECT count() FROM test;
+
+DETACH TABLE test;
+ATTACH TABLE test;
+
+ALTER TABLE test ATTACH PARTITION tuple();
+
+SELECT count() FROM test;
+
+DROP TABLE test;
