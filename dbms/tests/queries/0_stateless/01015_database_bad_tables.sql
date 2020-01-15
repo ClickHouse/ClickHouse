a8e62788cd8a5057fccfa57419b240051ec25acf
Nikita Vasilev <>
2020-01-15 15:49:38 +0300

    test

--- dbms/tests/queries/0_stateless/01015_database_bad_tables.sql
+++ dbms/tests/queries/0_stateless/01015_database_bad_tables.sql
@@ -1,3 +1,5 @@
+DROP DATABASE IF EXISTS testlazy;
+
 CREATE TABLE `таблица_со_странным_названием` (a UInt64, b UInt64) ENGINE = Log;
 INSERT INTO `таблица_со_странным_названием` VALUES (1, 1);
 SELECT * FROM `таблица_со_странным_названием`;
@@ -8,4 +10,12 @@ CREATE TABLE testlazy.`таблица_со_странным_названием` 
 INSERT INTO testlazy.`таблица_со_странным_названием` VALUES (1, 1);
 SELECT * FROM testlazy.`таблица_со_странным_названием`;
 DROP TABLE testlazy.`таблица_со_странным_названием`;
-DROP DATABASE testlazy
+DROP DATABASE testlazy;
+
+CREATE DATABASE testlazy ENGINE = Lazy(10);
+DROP TABLE IF EXISTS testlazy.test;
+CREATE TABLE IF NOT EXISTS testlazy.test (a UInt64, b UInt64) ENGINE = Log;
+INSERT INTO testlazy.test VALUES (1, 1);
+SELECT * FROM testlazy.test;
+DROP TABLE IF EXISTS testlazy.test;
+DROP DATABASE testlazy;
