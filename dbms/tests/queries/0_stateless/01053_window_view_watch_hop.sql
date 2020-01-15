64668fb819c8adbcf2e9874a593e4b84afa6a397
LB <lb@vxider.com>
2020-01-15 17:20:19 +0800

    update tests

--- dbms/tests/queries/0_stateless/01053_window_view_watch_hop.sql
+++ dbms/tests/queries/0_stateless/01053_window_view_watch_hop.sql
@@ -7,15 +7,12 @@ CREATE TABLE test.mt(a Int32, timestamp DateTime) Engine=MergeTree order by tupl
 CREATE WINDOW VIEW test.wv AS SELECT count(a), HOP_START(wid) as w_start, HOP_END(wid) as w_end FROM test.mt group by HOP(timestamp, INTERVAL '1' SECOND, INTERVAL '5' SECOND) as wid;
 
 INSERT INTO test.mt VALUES (1, toDateTime('2020-01-09 12:00:01'));
-INSERT INTO test.mt VALUES (2, toDateTime('2020-01-09 12:00:05'));
 WATCH test.wv LIMIT 1;
 
 INSERT INTO test.mt VALUES (3, toDateTime('2020-01-09 12:00:10'));
-INSERT INTO test.mt VALUES (4, toDateTime('2020-01-09 12:00:15'));
 WATCH test.wv LIMIT 1;
 
 INSERT INTO test.mt VALUES (5, toDateTime('2020-01-09 12:00:20'));
-INSERT INTO test.mt VALUES (6, toDateTime('2020-01-09 12:00:35'));
 WATCH test.wv LIMIT 1;
 
 DROP TABLE test.wv;
