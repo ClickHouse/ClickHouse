8af3dda0bb5926452d2d3282d4b5a802295f800a
Alexey Milovidov <milovidov@yandex-team.ru>
2020-01-17 23:00:46 +0300

    Added comments

--- dbms/src/Functions/bitCount.cpp
+++ dbms/src/Functions/bitCount.cpp
@@ -13,6 +13,9 @@ struct BitCountImpl
 
     static inline ResultType apply(A a)
     {
+        /// We count bits in the value representation in memory. For example, we support floats.
+        /// We need to avoid sign-extension when converting signed numbers to larger type. So, uint8_t(-1) has 8 bits.
+
         return __builtin_popcountll(ext::bit_cast<unsigned long long>(a));
     }
 
