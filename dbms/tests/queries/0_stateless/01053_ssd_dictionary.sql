7859ef03dc60de45bcd6bdf8489a34294b9511bf
Nikita Vasilev <nekitvas@gmail.com>
2020-01-18 14:47:58 +0300

    select * from ssd_dictionary

--- dbms/tests/queries/0_stateless/01053_ssd_dictionary.sql
+++ dbms/tests/queries/0_stateless/01053_ssd_dictionary.sql
@@ -17,6 +17,28 @@ ORDER BY id;
 
 INSERT INTO database_for_dict.table_for_dict VALUES (1, 100, -100), (2, 3, 4), (5, 6, 7), (10, 9, 8);
 
+DROP DICTIONARY IF EXISTS database_for_dict.ssd_dict;
+
+CREATE DICTIONARY database_for_dict.ssd_dict
+(
+    id UInt64,
+    a UInt64 DEFAULT 0,
+    b Int32 DEFAULT -1
+)
+PRIMARY KEY id
+SOURCE(CLICKHOUSE(HOST 'localhost' PORT 9000 USER 'default' TABLE 'table_for_dict' PASSWORD '' DB 'database_for_dict'))
+LIFETIME(MIN 1000 MAX 2000)
+LAYOUT(SSD(PARTITION_SIZE 8192 PATH '/mnt/disk4/clickhouse_dicts/1d'));
+
+SELECT 'TEST_SMALL';
+SELECT dictGetInt32('database_for_dict.ssd_dict', 'b', toUInt64(1));
+SELECT dictGetInt32('database_for_dict.ssd_dict', 'b', toUInt64(4));
+SELECT dictGetUInt64('database_for_dict.ssd_dict', 'a', toUInt64(5));
+SELECT dictGetUInt64('database_for_dict.ssd_dict', 'a', toUInt64(6));
+
+SELECT * FROM database_for_dict.ssd_dict;
+DROP DICTIONARY database_for_dict.ssd_dict;
+
 DROP TABLE IF EXISTS database_for_dict.keys_table;
 
 CREATE TABLE database_for_dict.keys_table
@@ -44,7 +66,7 @@ CREATE DICTIONARY database_for_dict.ssd_dict
 PRIMARY KEY id
 SOURCE(CLICKHOUSE(HOST 'localhost' PORT 9000 USER 'default' TABLE 'table_for_dict' PASSWORD '' DB 'database_for_dict'))
 LIFETIME(MIN 1000 MAX 2000)
-LAYOUT(SSD(MAX_PARTITION_SIZE 1000 PATH '/mnt/disk4/clickhouse_dicts/1'));
+LAYOUT(SSD(PARTITION_SIZE 8192 PATH '/mnt/disk4/clickhouse_dicts/1d'));
 
 SELECT 'UPDATE DICTIONARY';
 -- 118
@@ -103,7 +125,7 @@ CREATE DICTIONARY database_for_dict.ssd_dict
 PRIMARY KEY id
 SOURCE(CLICKHOUSE(HOST 'localhost' PORT 9000 USER 'default' TABLE 'table_for_dict' PASSWORD '' DB 'database_for_dict'))
 LIFETIME(MIN 1000 MAX 2000)
-LAYOUT(SSD(MAX_PARTITION_SIZE 1000 PATH '/mnt/disk4/clickhouse_dicts/2'));
+LAYOUT(SSD(PARTITION_SIZE 8192 PATH '/mnt/disk4/clickhouse_dicts/2d'));
 
 SELECT 'UPDATE DICTIONARY (MT)';
 -- 118
