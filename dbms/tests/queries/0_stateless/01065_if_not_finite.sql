08b0d0cda17d7514df28fbdfadf21418d2cd2d5c
Alexey Milovidov <milovidov@yandex-team.ru>
2020-01-18 16:06:19 +0300

    Added test

--- dbms/tests/queries/0_stateless/01065_if_not_finite.sql
+++ dbms/tests/queries/0_stateless/01065_if_not_finite.sql
@@ -7,3 +7,5 @@ SELECT ifNotFinite(-1 / 0, 2);
 SELECT ifNotFinite(log(0), NULL);
 SELECT ifNotFinite(sqrt(-1), -42);
 SELECT ifNotFinite(1234567890123456789, -1234567890123456789); -- { serverError 386 }
+
+SELECT ifNotFinite(NULL, 1);
