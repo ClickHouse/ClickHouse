85a60673a6b67706a1ca7fc91c7e464a09185df3
Vitaliy Zakaznikov <vzakaznikov@altinity.com>
2020-01-21 17:02:40 +0100

    First attempt to fix issue https://github.com/ClickHouse/ClickHouse/issues/7878

---
+++ dbms/tests/queries/0_stateless/01069_materialized_view_alter_target_table.sql
@@ -0,0 +1,15 @@
+DROP TABLE IF EXISTS mv_source;
+DROP TABLE IF EXISTS mv_target;
+
+CREATE TABLE mv_source (`a` UInt64) ENGINE = MergeTree ORDER BY tuple();
+CREATE TABLE mv_target (`a` UInt64) ENGINE = MergeTree ORDER BY tuple();
+
+CREATE MATERIALIZED VIEW mv TO mv_target AS SELECT * FROM mv_source;
+
+INSERT INTO mv_source VALUES (1);
+
+ALTER TABLE mv_target ADD COLUMN b UInt8;
+INSERT INTO mv_source VALUES (1),(2),(3);
+
+SELECT * FROM mv ORDER BY a;
+SELECT * FROM mv_target ORDER BY a;
