d68f8d1f8e7d72db40684a2aca5f2fc7fb5e55c5
Mikhail Korotov <55493615+millb@users.noreply.github.com>
2020-01-22 16:58:36 +0300

    Update QueryLog.h

--- dbms/src/Interpreters/QueryLog.h
+++ dbms/src/Interpreters/QueryLog.h
@@ -38,7 +38,7 @@ struct QueryLogElement
     time_t query_start_time{};
     UInt64 query_duration_ms{};
 
-    /// The data fetched from 1457488640DB to execute the query
+    /// The data fetched from DB to execute the query
     UInt64 read_rows{};
     UInt64 read_bytes{};
 
