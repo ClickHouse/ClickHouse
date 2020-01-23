276d1c233e15a7aa06a08311f80067e931d766d2
Mikhail Korotov <55493615+millb@users.noreply.github.com>
2020-01-23 16:38:55 +0300

    Update 01071_http_header_exception_code.sh

--- dbms/tests/queries/0_stateless/01071_http_header_exception_code.sh
+++ dbms/tests/queries/0_stateless/01071_http_header_exception_code.sh
@@ -1,7 +1,6 @@
 #!/usr/bin/env bash
-set -e
 
 CURDIR=$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)
 . $CURDIR/../shell_config.sh
 
-${CLICKHOUSE_CURL_COMMAND} -I -sSg "${CLICKHOUSE_URL}&?query=BADREQUEST" | grep -o 'X-ClickHouse-Exception-Code: 62'
+${CLICKHOUSE_CURL_COMMAND} -I -sSg "${CLICKHOUSE_URL}?query=BADREQUEST" | grep -o 'X-ClickHouse-Exception-Code: 62'
