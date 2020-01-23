c7616ff11aed9701622802636a49c96669fb61db
Alexander Kuzmenkov <akuzm@yandex-team.ru>
2020-01-23 20:48:26 +0300

    [wip] performance comparison

--- docker/test/performance-comparison/eqmed.sql
+++ docker/test/performance-comparison/eqmed.sql
@@ -1,10 +1,10 @@
 -- input is table(query text, run UInt32, version int, time float)
 select
 --   abs(diff_percent) > rd_quantiles_percent[3] fail,
-   floor(original_medians_array.time_by_version[1], 4) m1,
-   floor(original_medians_array.time_by_version[2], 4) m2,
-   floor((m1 - m2) / m1, 3) diff_percent,
-   arrayMap(x -> floor(x / m1, 3), rd.rd_quantiles) rd_quantiles_percent,
+   floor(original_medians_array.time_by_version[1], 4) left,
+   floor(original_medians_array.time_by_version[2], 4) right,
+   floor((right - left) / left, 3) diff_percent,
+   arrayMap(x -> floor(x / left, 3), rd.rd_quantiles) rd_quantiles_percent,
    query 
 from
    (
