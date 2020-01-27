70eeedee93731513659a8a459446a2aeebeb9237
Andrei Chulkov <achulkov2@sandbox-410109003>
2020-01-28 00:09:38 +0300

    Rename columns to avoid ambiguity with sql

--- dbms/tests/queries/0_stateless/01038_array_of_unnamed_tuples.sql
+++ dbms/tests/queries/0_stateless/01038_array_of_unnamed_tuples.sql
@@ -4,13 +4,13 @@ DROP TABLE IF EXISTS array_of_tuples;
 
 CREATE TABLE array_of_tuples 
 (
-    first Array(Tuple(Float64, Float64)), 
-    second Array(Tuple(UInt8, UInt16, UInt32))
+    f Array(Tuple(Float64, Float64)), 
+    s Array(Tuple(UInt8, UInt16, UInt32))
 ) ENGINE = Memory;
 
 INSERT INTO array_of_tuples values ([(1, 2), (2, 3), (3, 4)], array(tuple(1, 2, 3), tuple(2, 3, 4))), (array((1.0, 2.0)), [tuple(4, 3, 1)]);
 
-SELECT first from array_of_tuples;
-SELECT second from array_of_tuples;
+SELECT f from array_of_tuples;
+SELECT s from array_of_tuples;
 
 DROP TABLE array_of_tuples;
