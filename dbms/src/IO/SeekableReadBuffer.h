8f91c8b93435e7e76d3f126b00571eb6dd0df0b8
Pavel Kovalenko <jokserfn@gmail.com>
2020-01-27 22:51:48 +0300

    Formatting issues.

--- dbms/src/IO/SeekableReadBuffer.h
+++ dbms/src/IO/SeekableReadBuffer.h
@@ -19,6 +19,12 @@ public:
     SeekableReadBuffer(Position ptr, size_t size, size_t offset)
         : ReadBuffer(ptr, size, offset) {}
 
+    /**
+     * Shifts buffer current position to given offset.
+     * @param off Offset.
+     * @param whence Seek mode (@see SEEK_SET, @see SEEK_CUR).
+     * @return New position from the begging of underlying buffer / file.
+     */
     virtual off_t seek(off_t off, int whence) = 0;
 };
 
