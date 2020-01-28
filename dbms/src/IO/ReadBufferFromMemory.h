0572f966561d902200401a9b82a3e0897293a006
Pavel Kovalenko <jokserfn@gmail.com>
2020-01-28 16:15:47 +0300

    Formatting issues.

--- dbms/src/IO/ReadBufferFromMemory.h
+++ dbms/src/IO/ReadBufferFromMemory.h
@@ -5,7 +5,6 @@
 
 namespace DB
 {
-
 /** Allows to read from memory range.
   * In comparison with just ReadBuffer, it only adds convenient constructors, that do const_cast.
   * In fact, ReadBuffer will not modify data in buffer, but it requires non-const pointer.
@@ -13,16 +12,19 @@ namespace DB
 class ReadBufferFromMemory : public SeekableReadBuffer
 {
 public:
-    ReadBufferFromMemory(const char * buf, size_t size)
-        : SeekableReadBuffer(const_cast<char *>(buf), size, 0) {}
+    ReadBufferFromMemory(const char * buf, size_t size) : SeekableReadBuffer(const_cast<char *>(buf), size, 0) {}
 
     ReadBufferFromMemory(const unsigned char * buf, size_t size)
-        : SeekableReadBuffer(const_cast<char *>(reinterpret_cast<const char *>(buf)), size, 0) {}
+        : SeekableReadBuffer(const_cast<char *>(reinterpret_cast<const char *>(buf)), size, 0)
+    {
+    }
 
     ReadBufferFromMemory(const signed char * buf, size_t size)
-        : SeekableReadBuffer(const_cast<char *>(reinterpret_cast<const char *>(buf)), size, 0) {}
+        : SeekableReadBuffer(const_cast<char *>(reinterpret_cast<const char *>(buf)), size, 0)
+    {
+    }
 
-	off_t seek(off_t off, int whence) override;
+    off_t seek(off_t off, int whence) override;
 };
 
 }
