3a5de8b8746ac7e60c22d35a3f4b4d1cb60e7f6c
Alexey Milovidov <milovidov@yandex-team.ru>
2020-01-28 19:59:32 +0300

    Update script for CHYT

--- dbms/benchmark/clickhouse/benchmark-chyt.sh
+++ dbms/benchmark/clickhouse/benchmark-chyt.sh
@@ -9,7 +9,10 @@ cat "$QUERIES_FILE" | sed "s|{table}|\"${TABLE}\"|g" | while read query; do
     echo -n "["
     for i in $(seq 1 $TRIES); do
         while true; do
-            RES=$(command time -f %e -o /dev/stdout curl -sS --location-trusted -H "Authorization: OAuth $YT_TOKEN" "$YT_PROXY.yt.yandex.net/query?default_format=Null&database=*$YT_CLIQUE_ID" --data-binary @- <<< "$query" 2>/dev/null) && break;
+            RES=$(command time -f %e -o /dev/stdout curl -sS --location-trusted -H "Authorization: OAuth $YT_TOKEN" "$YT_PROXY.yt.yandex.net/query?default_format=Null&max_memory_usage=100000000000&max_memory_usage_for_all_queries=100000000000&max_concurrent_queries_for_user=100&database=*$YT_CLIQUE_ID" --data-binary @- <<< "$query" 2>/dev/null);
+            if [[ $? == 0 ]]; then
+                [[ $RES =~ 'fail|exception' ]] || break;
+            fi
         done
 
         [[ "$?" == "0" ]] && echo -n "${RES}" || echo -n "null"
