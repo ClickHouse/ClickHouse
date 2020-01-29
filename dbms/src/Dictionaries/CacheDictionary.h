364f10d3528020b73549398cf6dabf673967a03a
Nikita Mikhaylov <mikhaylovnikitka@gmail.com>
2020-01-29 13:58:44 +0300

    atomic added

--- dbms/src/Dictionaries/CacheDictionary.h
+++ dbms/src/Dictionaries/CacheDictionary.h
@@ -319,7 +319,7 @@ private:
     std::unique_ptr<ArenaWithFreeLists> string_arena;
 
     mutable std::exception_ptr last_exception;
-    mutable size_t error_count = 0;
+    mutable std::atomic<size_t> error_count = 0;
     mutable std::chrono::system_clock::time_point backoff_end_time;
 
     mutable pcg64 rnd_engine;
