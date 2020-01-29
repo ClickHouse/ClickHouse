58df79f2ad449a070098ae144eefffd2cb62ae18
Nikolai Kochetov <KochetovNicolai@users.noreply.github.com>
2020-01-29 13:11:13 +0300

    Merge pull request #7796 from ClickHouse/processors-4.3
    Processors and system.numbers
    
    (cherry picked from commit 8ce6040306025ee438c2abeb6bcd90da3da3c894)

--- dbms/src/Processors/IProcessor.h
+++ dbms/src/Processors/IProcessor.h
@@ -222,7 +222,11 @@ public:
     /// In case if query was cancelled executor will wait till all processors finish their jobs.
     /// Generally, there is no reason to check this flag. However, it may be reasonable for long operations (e.g. i/o).
     bool isCancelled() const { return is_cancelled; }
-    void cancel() { is_cancelled = true; }
+    void cancel()
+    {
+        is_cancelled = true;
+        onCancel();
+    }
 
     virtual ~IProcessor() = default;
 
@@ -271,6 +275,9 @@ public:
     void setStream(size_t value) { stream_number = value; }
     size_t getStream() const { return stream_number; }
     constexpr static size_t NO_STREAM = std::numeric_limits<size_t>::max();
+
+protected:
+    virtual void onCancel() {}
 
 private:
     std::atomic<bool> is_cancelled{false};
