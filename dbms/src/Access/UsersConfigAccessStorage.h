6a8a69fd0ca1a24216e4d477314420e0ee00fbf9
Vitaly Baranov <vitbar@yandex-team.ru>
2020-01-29 18:51:12 +0300

    Use the generic class ext::scope_guard for subscriptions in IAccessStorage instead of a special class.

--- dbms/src/Access/UsersConfigAccessStorage.h
+++ dbms/src/Access/UsersConfigAccessStorage.h
@@ -32,8 +32,8 @@ private:
     UUID insertImpl(const AccessEntityPtr & entity, bool replace_if_exists) override;
     void removeImpl(const UUID & id) override;
     void updateImpl(const UUID & id, const UpdateFunc & update_func) override;
-    SubscriptionPtr subscribeForChangesImpl(const UUID & id, const OnChangedHandler & handler) const override;
-    SubscriptionPtr subscribeForChangesImpl(std::type_index type, const OnChangedHandler & handler) const override;
+    ext::scope_guard subscribeForChangesImpl(const UUID & id, const OnChangedHandler & handler) const override;
+    ext::scope_guard subscribeForChangesImpl(std::type_index type, const OnChangedHandler & handler) const override;
     bool hasSubscriptionImpl(const UUID & id) const override;
     bool hasSubscriptionImpl(std::type_index type) const override;
 
