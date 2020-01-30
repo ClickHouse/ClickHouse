dbbfd68c1ae8b8f9c3288d778fd604a39f5d53fc
Nikolai Kochetov <nik-kochetov@yandex-team.ru>
2020-01-30 13:30:48 +0300

    Fix StorageBuffer/

--- dbms/src/Storages/StorageBuffer.cpp
+++ dbms/src/Storages/StorageBuffer.cpp
@@ -207,7 +207,7 @@ Pipes StorageBuffer::readWithProcessors(
                 query_info.input_sorting_info = query_info.order_by_optimizer->getInputOrder(destination);
 
             /// The destination table has the same structure of the requested columns and we can simply read blocks from there.
-            pipes_from_dst = readAsPipes(destination, column_names, query_info, context, processed_stage, max_block_size, num_streams));
+            pipes_from_dst = readAsPipes(destination, column_names, query_info, context, processed_stage, max_block_size, num_streams);
         }
         else
         {
