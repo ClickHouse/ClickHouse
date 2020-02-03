cbcb7895e4fcf51706ed2b85d3a02263fa6784a6
Alexander Kuzmenkov <akuzm@yandex-team.ru>
2020-02-03 22:50:08 +0300

    Avoid extra copying when demangling names.

--- libs/libcommon/include/common/demangle.h
+++ libs/libcommon/include/common/demangle.h
@@ -14,3 +14,28 @@ inline std::string demangle(const char * name)
     int status = 0;
     return demangle(name, status);
 }
+
+// abi::__cxa_demangle returns a C string of known size that should be deleted
+// with free().
+struct DemangleResult
+{
+    char * data = nullptr;
+    size_t size = 0;
+
+    DemangleResult() = default;
+    DemangleResult(DemangleResult &) = delete;
+    DemangleResult(DemangleResult && other)
+    {
+        std::swap(data, other.data);
+        std::swap(size, other.size);
+    }
+    ~DemangleResult()
+    {
+        if (data)
+        {
+            free(data);
+        }
+    }
+};
+
+DemangleResult try_demangle(const char * name);
