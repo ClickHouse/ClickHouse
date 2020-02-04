b0652d62853fed847d6fe72e406a9e24def93ba9
Nikita Mikhaylov <mikhaylovnikitka@gmail.com>
2020-02-04 20:28:32 +0300

    better

--- dbms/tests/queries/0_stateless/01054_cache_dictionary_overflow_cell.sql
+++ dbms/tests/queries/0_stateless/01054_cache_dictionary_overflow_cell.sql
@@ -25,27 +25,27 @@ insert into test_01054.ints values (18, 18, 18, 18, 18, 18, 18, 18, 18);
 insert into test_01054.ints values (19, 19, 19, 19, 19, 19, 19, 19, 19);
 insert into test_01054.ints values (20, 20, 20, 20, 20, 20, 20, 20, 20);
 
---select 
---dictGet('one_cell_cache_ints', 'i8', toUInt64(1)), 
---dictGet('one_cell_cache_ints', 'i8', toUInt64(2)), 
---dictGet('one_cell_cache_ints', 'i8', toUInt64(3)), 
---dictGet('one_cell_cache_ints', 'i8', toUInt64(4)), 
---dictGet('one_cell_cache_ints', 'i8', toUInt64(5)), 
---dictGet('one_cell_cache_ints', 'i8', toUInt64(6)), 
---dictGet('one_cell_cache_ints', 'i8', toUInt64(7)), 
---dictGet('one_cell_cache_ints', 'i8', toUInt64(8)), 
---dictGet('one_cell_cache_ints', 'i8', toUInt64(9)), 
---dictGet('one_cell_cache_ints', 'i8', toUInt64(10)), 
---dictGet('one_cell_cache_ints', 'i8', toUInt64(11)), 
---dictGet('one_cell_cache_ints', 'i8', toUInt64(12)), 
---dictGet('one_cell_cache_ints', 'i8', toUInt64(13)), 
---dictGet('one_cell_cache_ints', 'i8', toUInt64(14)), 
---dictGet('one_cell_cache_ints', 'i8', toUInt64(15)), 
---dictGet('one_cell_cache_ints', 'i8', toUInt64(16)), 
---dictGet('one_cell_cache_ints', 'i8', toUInt64(17)), 
---dictGet('one_cell_cache_ints', 'i8', toUInt64(18)), 
---dictGet('one_cell_cache_ints', 'i8', toUInt64(19)), 
---dictGet('one_cell_cache_ints', 'i8', toUInt64(20));
+select 
+dictGet('one_cell_cache_ints', 'i8', toUInt64(1)), 
+dictGet('one_cell_cache_ints', 'i8', toUInt64(2)), 
+dictGet('one_cell_cache_ints', 'i8', toUInt64(3)), 
+dictGet('one_cell_cache_ints', 'i8', toUInt64(4)), 
+dictGet('one_cell_cache_ints', 'i8', toUInt64(5)), 
+dictGet('one_cell_cache_ints', 'i8', toUInt64(6)), 
+dictGet('one_cell_cache_ints', 'i8', toUInt64(7)), 
+dictGet('one_cell_cache_ints', 'i8', toUInt64(8)), 
+dictGet('one_cell_cache_ints', 'i8', toUInt64(9)), 
+dictGet('one_cell_cache_ints', 'i8', toUInt64(10)), 
+dictGet('one_cell_cache_ints', 'i8', toUInt64(11)), 
+dictGet('one_cell_cache_ints', 'i8', toUInt64(12)), 
+dictGet('one_cell_cache_ints', 'i8', toUInt64(13)), 
+dictGet('one_cell_cache_ints', 'i8', toUInt64(14)), 
+dictGet('one_cell_cache_ints', 'i8', toUInt64(15)), 
+dictGet('one_cell_cache_ints', 'i8', toUInt64(16)), 
+dictGet('one_cell_cache_ints', 'i8', toUInt64(17)), 
+dictGet('one_cell_cache_ints', 'i8', toUInt64(18)), 
+dictGet('one_cell_cache_ints', 'i8', toUInt64(19)), 
+dictGet('one_cell_cache_ints', 'i8', toUInt64(20));
 
 SELECT arrayMap(x -> dictGet('one_cell_cache_ints', 'i8', toUInt64(x)), array)
 FROM 
