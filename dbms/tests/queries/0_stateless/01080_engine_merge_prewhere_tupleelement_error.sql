dada5cf6cad001ac85a08296dbee6e2f0e3e6f35
Denis Zhuravlev <deniszhuravlov@gmail.com>
2020-02-14 16:17:29 -0400

    Update 01080_engine_merge_prewhere_tupleelement_error.sql
    optimize_move_to_prewhere=0

--- dbms/tests/queries/0_stateless/01080_engine_merge_prewhere_tupleelement_error.sql
+++ dbms/tests/queries/0_stateless/01080_engine_merge_prewhere_tupleelement_error.sql
@@ -4,6 +4,8 @@ CREATE TABLE A1( a DateTime ) ENGINE = MergeTree ORDER BY tuple();
 CREATE TABLE A_M as A1 ENGINE = Merge(currentDatabase(), '^A1$');
 insert into A1(a) select now();
 
+set optimize_move_to_prewhere=0;
+
 SELECT tupleElement(arrayJoin([(1, 1)]), 1) FROM A_M PREWHERE tupleElement((1, 1), 1) =1;
 
 SELECT tupleElement(arrayJoin([(1, 1)]), 1) FROM A_M WHERE tupleElement((1, 1), 1) =1;
