accbf56fb6ba702648469992a7eedeee619145f5
Azat Khuzhin <a3at.mail@gmail.com>
2020-02-17 00:00:05 +0300

    Fix PartialSortingTransform by expand column to full
    Refs: 2e03ac09ecdc103e
    Refs: #6606

--- dbms/src/Processors/Transforms/PartialSortingTransform.cpp
+++ dbms/src/Processors/Transforms/PartialSortingTransform.cpp
@@ -1,12 +1,13 @@
 #include <Processors/Transforms/PartialSortingTransform.h>
 #include <Interpreters/sortBlock.h>
+#include <DataStreams/materializeBlock.h>
 
 namespace DB
 {
 
 PartialSortingTransform::PartialSortingTransform(
     const Block & header_, SortDescription & description_, UInt64 limit_, bool do_count_rows_)
-    : ISimpleTransform(header_, header_, false)
+    : ISimpleTransform(header_, {materializeBlock(header_)}, false)
     , description(description_), limit(limit_), do_count_rows(do_count_rows_)
 {
 }
@@ -20,6 +21,10 @@ void PartialSortingTransform::transform(Chunk & chunk)
     chunk.clear();
 
     sortBlock(block, description, limit);
+
+    for (auto & column : block.getColumns())
+        column = column->convertToFullColumnIfConst();
+
     chunk.setColumns(block.getColumns(), block.rows());
 }
 
