a116e12fef1eca18289761aba7de3abe438aa1af
achulkov2 <achulkov2@gmail.com>
2020-02-18 16:32:18 +0300

    Add new files

---
+++ dbms/src/Dictionaries/PolygonDictionaryImplementations.h
@@ -0,0 +1,52 @@
+#pragma once
+
+#include "PolygonDictionary.h"
+#include "PolygonDictionaryUtils.h"
+
+namespace DB
+{
+
+/** Simple implementation of the polygon dictionary. Doesn't generate anything during its construction.
+*  Iterates over all stored polygons for each query, checking each of them in linear time.
+*  Retrieves the polygon with the smallest area containing the given point. If there is more than one any such polygon
+*  may be returned.
+*/
+class SimplePolygonDictionary : public IPolygonDictionary
+{
+public:
+    SimplePolygonDictionary(
+            const std::string & database_,
+            const std::string & name_,
+            const DictionaryStructure & dict_struct_,
+            DictionarySourcePtr source_ptr_,
+            DictionaryLifetime dict_lifetime_,
+            InputType input_type_,
+            PointType point_type_);
+
+    std::shared_ptr<const IExternalLoadable> clone() const override;
+
+private:
+    bool find(const Point & point, size_t & id) const override;
+};
+
+class GridPolygonDictionary : public IPolygonDictionary
+{
+public:
+    GridPolygonDictionary(
+            const std::string & database_,
+            const std::string & name_,
+            const DictionaryStructure & dict_struct_,
+            DictionarySourcePtr source_ptr_,
+            DictionaryLifetime dict_lifetime_,
+            InputType input_type_,
+            PointType point_type_);
+
+    std::shared_ptr<const IExternalLoadable> clone() const override;
+
+private:
+    bool find(const Point & point, size_t & id) const override;
+
+    GridRoot grid;
+};
+
+}
