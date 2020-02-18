8db2f90f785e61112143ad69578e95231e1f1496
Ivan Blinkov <github@blinkov.ru>
2020-02-18 17:19:44 +0300

    Show documentation revision in footer

--- docs/tools/website.py
+++ docs/tools/website.py
@@ -4,10 +4,16 @@ import shutil
 
 import cssmin
 import htmlmin
+import jinja2
 import jsmin
+
 
 def build_website(args):
     logging.info('Building website')
+    env = jinja2.Environment(
+        loader=args.output_dir
+    )
+
     shutil.copytree(
         args.website_dir,
         args.output_dir,
@@ -21,9 +27,25 @@ def build_website(args):
         )
     )
 
+    for root, _, filenames in os.walk(args.output_dir):
+        for filename in filenames:
+            path = os.path.join(root, filename)
+            if not filename.endswith('.html'):
+                continue
+            logging.info('Processing %s', path)
+            with open(path, 'rb') as f:
+                content = f.read().decode('utf-8')
+
+            template = env.from_string(content)
+            content = template.render(args.__dict__)
+
+            with open(path, 'wb') as f:
+                f.write(content.encode('utf-8'))
+
 
 def minify_website(args):
     if args.minify:
+        logging.info('Minifying website')
         for root, _, filenames in os.walk(args.output_dir):
             for filename in filenames:
                 path = os.path.join(root, filename)
@@ -33,15 +55,15 @@ def minify_website(args):
                     filename.endswith('.js')
                 ):
                     continue
-    
+
                 logging.info('Minifying %s', path)
                 with open(path, 'rb') as f:
                     content = f.read().decode('utf-8')
-                if filename.endswith('.html'):
-                    content = htmlmin.minify(content, remove_empty_space=False)
-                elif filename.endswith('.css'):
-                    content = cssmin.cssmin(content)
-                elif filename.endswith('.js'):
-                    content = jsmin.jsmin(content)
+                    if filename.endswith('.html'):
+                        content = htmlmin.minify(content, remove_empty_space=False)
+                    elif filename.endswith('.css'):
+                        content = cssmin.cssmin(content)
+                    elif filename.endswith('.js'):
+                        content = jsmin.jsmin(content)
                 with open(path, 'wb') as f:
                     f.write(content.encode('utf-8'))
