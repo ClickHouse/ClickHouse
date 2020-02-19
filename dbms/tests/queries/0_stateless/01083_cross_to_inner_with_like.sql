732c50408a63ee4c3d07672e34b074f8f9ed7c19
chertus <chertus@gmail.com>
2020-02-19 20:29:57 +0300

    disable crazy push down for test

--- dbms/tests/queries/0_stateless/01083_cross_to_inner_with_like.sql
+++ dbms/tests/queries/0_stateless/01083_cross_to_inner_with_like.sql
@@ -5,6 +5,7 @@ CREATE TABLE n (k UInt32) ENGINE = Memory;
 CREATE TABLE r (k UInt32, name String) ENGINE = Memory;
 
 SET enable_debug_queries = 1;
+SET enable_optimize_predicate_expression = 0;
 
 ANALYZE SELECT * FROM n, r WHERE n.k = r.k AND r.name = 'A';
 ANALYZE SELECT * FROM n, r WHERE n.k = r.k AND r.name LIKE 'A%';
