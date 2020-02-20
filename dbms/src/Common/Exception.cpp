71737327c3f9c3f3b98cb264c0f1d8ac6850accb
Alexander Kuzmenkov <akuzm@yandex-team.ru>
2020-02-20 15:29:53 +0300

    Log error message before aborting on LOGICAL_ERROR.

--- dbms/src/Common/Exception.cpp
+++ dbms/src/Common/Exception.cpp
@@ -35,7 +35,14 @@ Exception::Exception(const std::string & msg, int code)
     : Poco::Exception(msg, code)
 {
     // In debug builds, treat LOGICAL_ERROR as an assertion failure.
-    assert(code != ErrorCodes::LOGICAL_ERROR);
+    // Log the message before we fail.
+#ifndef NDEBUG
+    if (code == ErrorCodes::LOGICAL_ERROR)
+    {
+        LOG_ERROR(&Poco::Logger::root(), "Logical error: '" + msg + "'.");
+        assert(false);
+    }
+#endif
 }
 
 Exception::Exception(CreateFromPocoTag, const Poco::Exception & exc)
