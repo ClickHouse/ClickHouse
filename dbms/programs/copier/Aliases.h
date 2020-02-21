0b4d8bdb08bb7de45f893dc5a6d8212b51afc67b
Nikita Mikhaylov <mikhaylovnikitka@gmail.com>
2020-02-21 19:00:50 +0300

    move partition added

--- dbms/programs/copier/Aliases.h
+++ dbms/programs/copier/Aliases.h
@@ -9,6 +9,7 @@ namespace DB
     using ConfigurationPtr = Poco::AutoPtr<Poco::Util::AbstractConfiguration>;
 
     using DatabaseAndTableName = std::pair<String, String>;
+    using ListOfDatabasesAndTableNames = std::vector<DatabaseAndTableName>;
 
     /// Hierarchical description of the tasks
     struct ShardPartitionPiece;
