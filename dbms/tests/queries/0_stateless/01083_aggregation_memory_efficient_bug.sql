23c5c6f8746c926db99dcb7de5a1dd17303f07a2
Nikolai Kochetov <nik-kochetov@yandex-team.ru>
2020-02-21 22:03:45 +0300

    Make more deterministic test.

--- dbms/tests/queries/0_stateless/01083_aggregation_memory_efficient_bug.sql
+++ dbms/tests/queries/0_stateless/01083_aggregation_memory_efficient_bug.sql
@@ -1,6 +1,6 @@
 drop table if exists da_memory_efficient_shard;
 create table da_memory_efficient_shard(A Int64, B Int64) Engine=MergeTree order by A partition by B % 2;
-insert into da_memory_efficient_shard select toString(rand()), number from numbers(100000);
+insert into da_memory_efficient_shard select number, number from numbers(100000);
 
 set distributed_aggregation_memory_efficient = 1, group_by_two_level_threshold = 1, group_by_two_level_threshold_bytes=1;
 
