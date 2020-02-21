c29e03e550dfd3ca17b6356a6a7d4c6ffd3e718c
Ivan Blinkov <github@blinkov.ru>
2020-02-22 00:26:36 +0300

    actually same as master

--- docs/tools/release.sh
+++ docs/tools/release.sh
@@ -4,8 +4,8 @@ set -ex
 BASE_DIR=$(dirname $(readlink -f $0))
 BUILD_DIR="${BASE_DIR}/../build"
 PUBLISH_DIR="${BASE_DIR}/../publish"
-TEST_DOMAIN="${TEST_DOMAIN:-test.clickhouse.tech}"
-GIT_TEST_URI="${GIT_TEST_URI:-git@github.com:ClickHouse/clickhouse-test.github.io.git}"
+TEST_DOMAIN="${TEST_DOMAIN:-clickhouse.tech}"
+GIT_TEST_URI="${GIT_TEST_URI:-git@github.com:ClickHouse/clickhouse.github.io.git}"
 GIT_PROD_URI="git@github.com:ClickHouse/clickhouse.github.io.git"
 EXTRA_BUILD_ARGS="${EXTRA_BUILD_ARGS:---enable-stable-releases}"
 
@@ -37,22 +37,6 @@ then
     git push origin master
     cd "${BUILD_DIR}"
     DOCKER_HASH=$(head -c 16 < /dev/urandom | xxd -p)
-else
-    rm -rf "${BUILD_DIR}" || true
-    rm -rf "${PUBLISH_DIR}" || true
-    git clone "${GIT_TEST_URI}" "${BUILD_DIR}"
-    git clone "${GIT_PROD_URI}" "${PUBLISH_DIR}"
-    cd "${PUBLISH_DIR}"
-    git config user.email "robot-clickhouse@yandex-team.ru"
-    git config user.name "robot-clickhouse"
-    git rm -rf *
-    git commit -a -m "wipe old release"
-    rm -rf "${BUILD_DIR}/.git"
-    cp -R "${BUILD_DIR}"/* .
-    echo -n "clickhouse.tech" > CNAME
-    git add *
-    git commit -a -m "add new release at $(date)"
-    git push origin master
 fi
 
 QLOUD_ENDPOINT="https://platform.yandex-team.ru/api/v1"
