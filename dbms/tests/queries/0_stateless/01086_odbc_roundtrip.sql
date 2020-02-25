5a717ce8d989f7c39aa1d2d2de247e5909df31fa
filimonov <1549571+filimonov@users.noreply.github.com>
2020-02-25 01:55:00 +0100

    missed edit

--- dbms/tests/queries/0_stateless/01086_odbc_roundtrip.sql
+++ dbms/tests/queries/0_stateless/01086_odbc_roundtrip.sql
@@ -6,8 +6,9 @@ CREATE DATABASE test_01086;
 USE test_01086;
 
 CREATE TABLE t (x UInt8, y Float32, z String) ENGINE = Memory;
-INSERT INTO t VALUES (1,0.1,'a'),(2,0.2,'b'),(3,0.3,'c');
+INSERT INTO t VALUES (1,0.1,'a я'),(2,0.2,'b ą'),(3,0.3,'c d');
 
 select * from odbc('DSN={ClickHouse DSN (ANSI)}','test_01086','t') ORDER BY x;
+select * from odbc('DSN={ClickHouse DSN (Unicode)}','test_01086','t') ORDER BY x;
 
 DROP DATABASE test_01086;
