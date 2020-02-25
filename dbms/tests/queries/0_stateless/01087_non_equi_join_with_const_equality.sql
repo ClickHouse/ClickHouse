61f6e8eae27c67f591490eee95bae65ccc1f9878
Artem Zuikov <chertus@gmail.com>
2020-02-25 21:57:14 +0300

    better test

--- dbms/tests/queries/0_stateless/01087_non_equi_join_with_const_equality.sql
+++ dbms/tests/queries/0_stateless/01087_non_equi_join_with_const_equality.sql
@@ -27,3 +27,37 @@ select * from (select number as n from numbers(4)) t1
 join (select number as n from numbers(4)) as t2
 on t2.n = toUInt64(0)
 order by t1.n, t2.n;
+
+select '-';
+
+set enable_optimize_predicate_expression = 0;
+
+set join_algorithm = 'hash';
+
+select * from (select number as n from numbers(4)) t1
+join (select number as n from numbers(4)) as t2
+on t1.n = toUInt64(0)
+order by t1.n, t2.n;
+
+select '-';
+
+select * from (select number as n from numbers(4)) t1
+join (select number as n from numbers(4)) as t2
+on t2.n = toUInt64(0)
+order by t1.n, t2.n;
+
+select '-';
+
+set join_algorithm = 'partial_merge';
+
+select * from (select number as n from numbers(4)) t1
+join (select number as n from numbers(4)) as t2
+on t1.n = toUInt64(0)
+order by t1.n, t2.n;
+
+select '-';
+
+select * from (select number as n from numbers(4)) t1
+join (select number as n from numbers(4)) as t2
+on t2.n = toUInt64(0)
+order by t1.n, t2.n;
