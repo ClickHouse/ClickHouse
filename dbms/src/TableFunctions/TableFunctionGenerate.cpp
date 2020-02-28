5b11c70433f88cbe9d960a8ae52c1dfb681c4862
Yatsishin Ilya <2159081+qoega@users.noreply.github.com>
2020-02-28 12:19:11 +0300

    more docs

--- dbms/src/TableFunctions/TableFunctionGenerate.cpp
+++ dbms/src/TableFunctions/TableFunctionGenerate.cpp
@@ -22,8 +22,6 @@ namespace DB
 namespace ErrorCodes
 {
     extern const int NUMBER_OF_ARGUMENTS_DOESNT_MATCH;
-    extern const int BAD_ARGUMENTS;
-    extern const int BAD_TYPE_OF_FIELD;
     extern const int LOGICAL_ERROR;
 }
 
