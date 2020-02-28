8f5ad14cc5d071bff83925a97d35defb106d55ef
Alexander Kuzmenkov <akuzm@yandex-team.ru>
2020-02-28 20:08:51 +0300

    performance comparison - 7 runs

--- docker/test/performance-comparison/entrypoint.sh
+++ docker/test/performance-comparison/entrypoint.sh
@@ -48,7 +48,7 @@ done
 export PYTHONIOENCODING=utf-8
 
 # Use a default number of runs if not told otherwise
-export CHPC_RUNS=${CHPC_RUNS:-17}
+export CHPC_RUNS=${CHPC_RUNS:-7}
 
 # Even if we have some errors, try our best to save the logs.
 set +e
