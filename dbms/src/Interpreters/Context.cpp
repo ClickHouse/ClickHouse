d4d48a016594ee7e2ccc8c62b3ad72f24bddcabc
Alexey Milovidov <milovidov@yandex-team.ru>
2020-03-05 23:02:55 +0300

    Fix style

--- dbms/src/Interpreters/Context.cpp
+++ dbms/src/Interpreters/Context.cpp
@@ -93,6 +93,7 @@ namespace ErrorCodes
     extern const int LOGICAL_ERROR;
     extern const int UNKNOWN_SCALAR;
     extern const int AUTHENTICATION_FAILED;
+    extern const int NOT_IMPLEMENTED;
 }
 
 
