4eef09911762d62a303056dc7c2d3413a849c255
Alexey Milovidov <milovidov@yandex-team.ru>
2020-03-06 00:50:58 +0300

    Fixed error

--- dbms/src/Interpreters/Context.cpp
+++ dbms/src/Interpreters/Context.cpp
@@ -220,7 +220,7 @@ private:
     }
 
     /// Close sessions, that has been expired. Returns how long to wait for next session to be expired, if no new sessions will be added.
-    std::chrono::steady_clock::duration closeSessions(std::unique_lock<std::mutex> &)
+    std::chrono::steady_clock::duration closeSessions(std::unique_lock<std::mutex> & lock)
     {
         const auto now = std::chrono::steady_clock::now();
 
