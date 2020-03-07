db2e8c36c775969049abf94f994a5b5908e2251d
Alexey Milovidov <milovidov@yandex-team.ru>
2020-03-08 01:17:45 +0300

    Updated test

--- dbms/tests/queries/0_stateless/01087_table_function_generate.sql
+++ dbms/tests/queries/0_stateless/01087_table_function_generate.sql
@@ -154,7 +154,7 @@ SELECT
 FROM generateRandom('i FixedString(4)')
 LIMIT 1;
 SELECT
-    i
+    hex(i)
 FROM generateRandom('i FixedString(4)', 1, 10, 10)
 LIMIT 10;
 SELECT '-';
@@ -183,7 +183,7 @@ CREATE TABLE test_table_2(a Array(Int8), b UInt32, c Nullable(String), d Decimal
 INSERT INTO test_table_2 SELECT * FROM generateRandom('a Array(Int8), b UInt32, c Nullable(String), d Decimal32(4), e Nullable(Enum16(\'h\' = 1, \'w\' = 5 , \'o\' = -200)), f Float64, g Tuple(Date, DateTime, DateTime64, UUID), h FixedString(2)', 10, 5, 3)
 LIMIT 10;
 
-SELECT * FROM test_table_2;
+SELECT a, b, c, d, e, f, g, hex(h) FROM test_table_2;
 SELECT '-';
 
 DROP TABLE IF EXISTS test_table_2;
