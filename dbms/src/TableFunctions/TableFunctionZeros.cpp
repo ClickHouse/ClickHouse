7e7f836e69308fe1098f2c7ca78f15ee855496e9
Nikolai Kochetov <nik-kochetov@yandex-team.ru>
2020-03-10 16:14:49 +0300

    Added table functions zeros and zeros_mt

---
+++ dbms/src/TableFunctions/TableFunctionZeros.cpp
@@ -0,0 +1,5 @@
+//
+// Created by nik-kochetov on 3/10/20.
+//
+
+#include "TableFunctionZeros.h"
---
+++ dbms/src/TableFunctions/TableFunctionZeros.h
@@ -0,0 +1,15 @@
+//
+// Created by nik-kochetov on 3/10/20.
+//
+
+#ifndef CLICKHOUSE_TABLEFUNCTIONZEROS_H
+#define CLICKHOUSE_TABLEFUNCTIONZEROS_H
+
+
+class TableFunctionZeros
+{
+
+};
+
+
+#endif //CLICKHOUSE_TABLEFUNCTIONZEROS_H
