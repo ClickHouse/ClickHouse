d438c2d63bbab3250a824a46af42244138de1486
Ivan Blinkov <github@blinkov.ru>
2020-03-12 13:53:47 +0300

    Get rid of inline js for Metrika + split base.html to parts

--- website/js/base.js
+++ website/js/base.js
@@ -32,4 +32,28 @@ $(document).ready(function () {
             window.history.replaceState('', document.title, dst);
         }
     });
+    (function (d, w, c) {
+        (w[c] = w[c] || []).push(function() {
+            try {
+                w.yaCounter18343495 = new Ya.Metrika2({
+                    id:18343495,
+                    clickmap:true,
+                    trackLinks:true,
+                    accurateTrackBounce:true,
+                    webvisor:true
+                });
+            } catch(e) { }
+        });
+
+        var n = d.getElementsByTagName("script")[0],
+            s = d.createElement("script"),
+            f = function () { n.parentNode.insertBefore(s, n); };
+        s.type = "text/javascript";
+        s.async = true;
+        s.src = "https://mc.yandex.ru/metrika/tag.js";
+
+        if (w.opera == "[object Opera]") {
+            d.addEventListener("DOMContentLoaded", f, false);
+        } else { f(); }
+    })(document, window, "yandex_metrika_callbacks2");
 });
