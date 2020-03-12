cb7f4fbdd6e831d327c042389dcabb7c19b32e4d
Ivan Blinkov <github@blinkov.ru>
2020-03-12 13:08:36 +0300

    Avoid mentioning token in logs

--- docs/tools/release.sh
+++ docs/tools/release.sh
@@ -38,7 +38,9 @@ then
     if [[ ! -z "${CLOUDFLARE_TOKEN}" ]]
     then
         sleep 1m
+        set +x
         curl -X POST "https://api.cloudflare.com/client/v4/zones/4fc6fb1d46e87851605aa7fa69ca6fe0/purge_cache" -H "Authorization: Bearer ${CLOUDFLARE_TOKEN}" -H "Content-Type:application/json" --data '{"purge_everything":true}'
+        set -x
     fi
     cd "${BUILD_DIR}"
     DOCKER_HASH=$(head -c 16 < /dev/urandom | xxd -p)
