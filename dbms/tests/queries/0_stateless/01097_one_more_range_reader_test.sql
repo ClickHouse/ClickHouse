5fd26ae83e23dfc2bb6450ac4bc3697e6d8d1d4e
Anton Popov <pad11rus@gmail.com>
2020-03-12 03:50:45 +0300

    Update 01097_one_more_range_reader_test.sql

--- dbms/tests/queries/0_stateless/01097_one_more_range_reader_test.sql
+++ dbms/tests/queries/0_stateless/01097_one_more_range_reader_test.sql
@@ -5,6 +5,6 @@ create table t (id UInt32, a Int) engine = MergeTree order by id;
 insert into t values (1, 0) (2, 1) (3, 0) (4, 0) (5, 0);
 alter table t add column s String default 'foo';
 
-select s from t prewhere a = 1 where rand() % 2 = 0 limit 1;
+select s from t prewhere a = 1;
 
 drop table t;
