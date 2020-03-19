0401fedb1b20a0ef304ab6e914f21a0ec874ad34
Ivan Blinkov <github@blinkov.ru>
2020-03-19 21:03:34 +0300

    add normalize-en-markdown.sh

---
+++ docs/tools/translate/normalize-en-markdown.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+# Usage: normalize-en-markdown.sh <path>
+set -e
+BASE_DIR=$(dirname $(readlink -f $0))
+TEMP_FILE=$(mktemp)
+trap 'rm -f -- "${TEMP_FILE}"' INT TERM HUP EXIT
+INPUT="$1"
+cat "${INPUT}" > "${TEMP_FILE}"
+"${BASE_DIR}/translate.sh" "en" "${TEMP_FILE}" "${INPUT}"
