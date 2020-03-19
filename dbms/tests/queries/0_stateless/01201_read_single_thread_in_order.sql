93daa16271220b4385522a6a46b06abe262dc584
Alexey Milovidov <milovidov@yandex-team.ru>
2020-03-19 22:55:16 +0300

    Better test

--- dbms/tests/queries/0_stateless/01201_read_single_thread_in_order.sql
+++ dbms/tests/queries/0_stateless/01201_read_single_thread_in_order.sql
@@ -8,6 +8,7 @@ ENGINE = MergeTree
 ORDER BY number
 SETTINGS index_granularity = 128;
 
+SET min_insert_block_size_rows = 0, min_insert_block_size_bytes = 0;
 INSERT INTO t SELECT number FROM numbers(10000000);
 
 SET max_threads = 1, max_block_size = 12345;
