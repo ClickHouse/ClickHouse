2c1dd9386276e59b78e661443d4d986e6a23ba0a
alesapin <alesapin@gmail.com>
2020-03-20 14:22:25 +0300

    Fix 01045 test

--- dbms/tests/queries/0_stateless/01045_zookeeper_system_mutations_with_parts_names.sh
+++ dbms/tests/queries/0_stateless/01045_zookeeper_system_mutations_with_parts_names.sh
@@ -5,6 +5,17 @@ CURDIR=$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)
 
 . $CURDIR/mergetree_mutations.lib
 
+function wait_mutation_to_start()
+{
+    query_wait=`$CLICKHOUSE_CLIENT --query="SELECT length(parts_to_do_names) FROM system.mutations where table = 'replicated_table_for_mutations'" 2>&1`
+
+    while [ "$query_wait" == "0" ] || [ -z "$query_wait" ]
+    do
+        query_wait=`$CLICKHOUSE_CLIENT --query="SELECT length(parts_to_do_names) FROM system.mutations where table = '$1'" 2>&1`
+        sleep 0.5
+    done
+}
+
 ${CLICKHOUSE_CLIENT} --query="DROP TABLE IF EXISTS table_for_mutations"
 
 ${CLICKHOUSE_CLIENT} --query="CREATE TABLE table_for_mutations(k UInt32, v1 UInt64) ENGINE MergeTree ORDER BY k PARTITION BY modulo(k, 2)"
@@ -18,6 +29,8 @@ ${CLICKHOUSE_CLIENT} --query="SELECT sum(v1) FROM table_for_mutations"
 ${CLICKHOUSE_CLIENT} --query="ALTER TABLE table_for_mutations UPDATE v1 = v1 + 1 WHERE 1"
 
 ${CLICKHOUSE_CLIENT} --query="SELECT is_done, parts_to_do_names, parts_to_do FROM system.mutations where table = 'table_for_mutations'"
+
+wait_mutation_to_start "table_for_mutations"
 
 ${CLICKHOUSE_CLIENT} --query="SYSTEM START MERGES"
 
@@ -41,6 +54,8 @@ ${CLICKHOUSE_CLIENT} --query="INSERT INTO replicated_table_for_mutations select 
 ${CLICKHOUSE_CLIENT} --query="SELECT sum(v1) FROM replicated_table_for_mutations"
 
 ${CLICKHOUSE_CLIENT} --query="ALTER TABLE replicated_table_for_mutations UPDATE v1 = v1 + 1 WHERE 1"
+
+wait_mutation_to_start "replicated_table_for_mutations"
 
 ${CLICKHOUSE_CLIENT} --query="SELECT is_done, parts_to_do_names, parts_to_do FROM system.mutations where table = 'replicated_table_for_mutations'"
 
