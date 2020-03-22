d885b8f4e3d4770a6a52eba31188a4880f6652e2
Andrey Chulkov <achulkov2@gmail.com>
2020-03-22 16:46:59 +0300

    Update grid parameter

--- dbms/src/Dictionaries/PolygonDictionaryUtils.h
+++ dbms/src/Dictionaries/PolygonDictionaryUtils.h
@@ -70,7 +70,7 @@ public:
     [[nodiscard]] const FinalCell * find(Float64 x, Float64 y) const override;
 
     /** When a cell is split every side is split into kSplit pieces producing kSplit * kSplit equal smaller cells. */
-    static constexpr size_t kSplit = 4;
+    static constexpr size_t kSplit = 2;
     static constexpr size_t kMultiProcessingDepth = 3;
 
 private:
