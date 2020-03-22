1aaa8a1bac9e35152660a44f37dc2da161d4939a
Andrey Chulkov <achulkov2@gmail.com>
2020-03-22 15:56:25 +0300

    Fix CE

--- dbms/src/Dictionaries/PolygonDictionaryImplementations.cpp
+++ dbms/src/Dictionaries/PolygonDictionaryImplementations.cpp
@@ -116,7 +116,7 @@ SmartPolygonDictionary::SmartPolygonDictionary(
         : IPolygonDictionary(database_, name_, dict_struct_, std::move(source_ptr_), dict_lifetime_, input_type_, point_type_)
 {
     buckets.reserve(polygons.size());
-    for (size_t i = 0; i < bucekts.size(); ++i)
+    for (size_t i = 0; i < buckets.size(); ++i)
     {
         buckets.emplace_back({polygons[i]});
     }
