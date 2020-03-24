288d2402e011359868cef84a43f532a7c622b29e
Ivan Blinkov <github@blinkov.ru>
2020-03-24 19:55:36 +0300

    WIP on mkdocs theme

---
+++ website/js/dark-mode.js
@@ -0,0 +1,3 @@
+if (localStorage.getItem('dark-mode')) {
+    document.getElementsByTagName('body')[0].className = 'dark-mode';
+}
--- website/js/docs.js
+++ website/js/docs.js
@@ -1,7 +1,33 @@
+function resizeSidebars() {
+    $('#sidebar, #toc').css({
+        'height': ($(window).height() - $('#top-nav').height()) + 'px'});
+    $('body').attr('data-offset', $(window).height().toString());
+
+}
 $(document).ready(function () {
-    $('#content h1, #content h2, #content h3, #content h4, #content h5, #content h6').mouseenter(function() {
+    resizeSidebars();
+    $(window).resize(resizeSidebars);
+    // $(window).scroll(function () {
+    $(window).on('activate.bs.scrollspy', function () {
+        var maxActiveOffset = 0;
+        $('#toc .nav-link.active').each(function() {
+            if (maxActiveOffset < $(this).offset().top) {
+                maxActiveOffset = $(this).offset().top;
+            }
+        });
+        $('#toc .nav-link').each(function() {
+            if (maxActiveOffset >= $(this).offset().top) {
+                $(this).addClass('active');
+            } else {
+                $(this).removeClass('active');
+            }
+        });
+    });
+    var headers = $('#content h1, #content h2, #content h3, #content h4, #content h5, #content h6');
+    headers.mouseenter(function() {
         $(this).find('.headerlink').show();
-    }).mouseleave(function() {
+    });
+    headers.mouseleave(function() {
         $(this).find('.headerlink').hide();
     });
 });
