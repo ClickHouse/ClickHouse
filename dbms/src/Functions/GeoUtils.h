06520941ee6031344651c475f1b510ade8ee2b6a
Alexey Milovidov <milovidov@yandex-team.ru>
2020-03-25 21:51:48 +0300

    Removed useless header

--- dbms/src/Functions/GeoUtils.h
+++ dbms/src/Functions/GeoUtils.h
@@ -25,8 +25,6 @@
 #include <boost/geometry/geometries/polygon.hpp>
 #include <boost/geometry/geometries/multi_polygon.hpp>
 #include <boost/geometry/geometries/segment.hpp>
-#include <boost/geometry/algorithms/comparable_distance.hpp>
-#include <boost/geometry/strategies/cartesian/distance_pythagoras.hpp>
 
 #include <array>
 #include <vector>
@@ -660,34 +658,6 @@ std::string serialize(Polygon && polygon)
     return result;
 }
 
-size_t geohashEncode(Float64 longitude, Float64 latitude, uint8_t precision, char * out);
+}
 
-void geohashDecode(const char * encoded_string, size_t encoded_len, Float64 * longitude, Float64 * latitude);
-
-std::vector<std::pair<Float64, Float64>> geohashCoverBox(Float64 longitude_min, Float64 latitude_min, Float64 longitude_max, Float64 latitude_max, uint8_t precision, UInt32 max_items = 0);
-
-struct GeohashesInBoxPreparedArgs
-{
-    UInt64 items_count = 0;
-    uint8_t precision = 0;
-
-    Float64 longitude_min = 0.0;
-    Float64 latitude_min = 0.0;
-    Float64 longitude_max = 0.0;
-    Float64 latitude_max = 0.0;
-
-    Float64 longitude_step = 0.0;
-    Float64 latitude_step = 0.0;
-};
-
-GeohashesInBoxPreparedArgs geohashesInBoxPrepare(const Float64 longitude_min,
-                                              const Float64 latitude_min,
-                                              Float64 longitude_max,
-                                              Float64 latitude_max,
-                                              uint8_t precision);
-
-UInt64 geohashesInBox(const GeohashesInBoxPreparedArgs & args, char * out);
-
-} /// GeoUtils
-
-} /// DB
+}
