a5b041c9c18cdbaa678a1b4caf7bd03bb4cbcbc9
alexey-milovidov <milovidov@yandex-team.ru>
2020-03-25 13:18:04 +0300

    Update pointInPolygon.cpp

--- dbms/src/Functions/pointInPolygon.cpp
+++ dbms/src/Functions/pointInPolygon.cpp
@@ -5,7 +5,6 @@
 #include <boost/geometry.hpp>
 #include <boost/geometry/geometries/point_xy.hpp>
 #include <boost/geometry/geometries/polygon.hpp>
-#include <boost/geometry/geometries/polygon.hpp>
 
 #include <Columns/ColumnArray.h>
 #include <Columns/ColumnFixedString.h>
