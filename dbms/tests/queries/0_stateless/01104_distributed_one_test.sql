a1225d9450060db3b14a1a4f73950a1bf6bbfa1e
Artem Zuikov <chertus@gmail.com>
2020-03-27 13:46:04 +0300

    better tests

--- dbms/tests/queries/0_stateless/01104_distributed_one_test.sql
+++ dbms/tests/queries/0_stateless/01104_distributed_one_test.sql
@@ -7,4 +7,12 @@ SELECT 'local_1', toUInt8(1) AS dummy FROM system.one AS o WHERE o.dummy = 1;
 SELECT 'distributed_0', _shard_num, toUInt8(1) AS dummy FROM d_one AS o WHERE o.dummy = 0;
 SELECT 'distributed_1', _shard_num, toUInt8(1) AS dummy FROM d_one AS o WHERE o.dummy = 1;
 
+SET distributed_product_mode = 'local';
+
+SELECT 'local_0', toUInt8(1) AS dummy FROM system.one AS o WHERE o.dummy = 0;
+SELECT 'local_1', toUInt8(1) AS dummy FROM system.one AS o WHERE o.dummy = 1;
+
+SELECT 'distributed_0', _shard_num, toUInt8(1) AS dummy FROM d_one AS o WHERE o.dummy = 0;
+SELECT 'distributed_1', _shard_num, toUInt8(1) AS dummy FROM d_one AS o WHERE o.dummy = 1;
+
 DROP TABLE d_one;
