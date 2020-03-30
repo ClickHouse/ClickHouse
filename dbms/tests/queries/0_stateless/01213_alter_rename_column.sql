feb3ac3dda26159738efac80deea5827cb785b9b
alesapin <alesapin@gmail.com>
2020-03-30 16:34:19 +0300

    Add simple validation for renames

--- dbms/tests/queries/0_stateless/01213_alter_rename_column.sql
+++ dbms/tests/queries/0_stateless/01213_alter_rename_column.sql
@@ -20,4 +20,10 @@ ALTER TABLE table_for_rename RENAME COLUMN value1 to renamed_value1;
 
 SELECT renamed_value1 FROM table_for_rename WHERE key = 1;
 
+SELECT * FROM table_for_rename WHERE key = 1 FORMAT TSVWithNames;
+
+ALTER TABLE table_for_rename RENAME COLUMN value3 to value2; --{serverError 15}
+ALTER TABLE table_for_rename RENAME COLUMN value3 TO r1, RENAME COLUMN value3 TO r2; --{serverError 36}
+ALTER TABLE table_for_rename RENAME COLUMN value3 TO r1, RENAME COLUMN r1 TO value1; --{serverError 36}
+
 DROP TABLE IF EXISTS table_for_rename;
