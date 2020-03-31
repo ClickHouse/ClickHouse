2691496eb5b2acde529a1d7221869f037aab3f98
achulkov2 <achulkov2@gmail.com>
2020-03-31 13:52:26 +0300

    Fix mysql + join bug (#9949)

--- dbms/src/Storages/StorageMySQL.cpp
+++ dbms/src/Storages/StorageMySQL.cpp
@@ -71,7 +71,7 @@ Pipes StorageMySQL::read(
 {
     check(column_names_);
     String query = transformQueryForExternalDatabase(
-        *query_info_.query, getColumns().getOrdinary(), IdentifierQuotingStyle::BackticksMySQL, remote_database_name, remote_table_name, context_);
+        query_info_, getColumns().getOrdinary(), IdentifierQuotingStyle::BackticksMySQL, remote_database_name, remote_table_name, context_);
 
     Block sample_block;
     for (const String & column_name : column_names_)
