968e843f4b538bf6d8c6a7d117f7db9882f0a7a4
Artem Zuikov <chertus@gmail.com>
2020-04-01 17:21:37 +0300

    Fix columns override in distributed queries (#9972)

--- dbms/tests/queries/0_stateless/00818_inner_join_bug_3567.sql
+++ dbms/tests/queries/0_stateless/00818_inner_join_bug_3567.sql
@@ -1,14 +1,16 @@
-DROP TABLE IF EXISTS using1;
-DROP TABLE IF EXISTS using2;
+DROP TABLE IF EXISTS table1;
+DROP TABLE IF EXISTS table2;
 
-CREATE TABLE using1(a String, b DateTime) ENGINE=MergeTree order by a;
-CREATE TABLE using2(c String, a String, d DateTime) ENGINE=MergeTree order by c;
+CREATE TABLE table1(a String, b Date) ENGINE MergeTree order by a;
+CREATE TABLE table2(c String, a String, d Date) ENGINE MergeTree order by c;
 
-INSERT INTO using1 VALUES ('a', '2018-01-01 00:00:00') ('b', '2018-01-01 00:00:00') ('c', '2018-01-01 00:00:00');
-INSERT INTO using2 VALUES ('d', 'd', '2018-01-01 00:00:00') ('b', 'b', '2018-01-01 00:00:00') ('c', 'c', '2018-01-01 00:00:00');
+INSERT INTO table1 VALUES ('a', '2018-01-01') ('b', '2018-01-01') ('c', '2018-01-01');
+INSERT INTO table2 VALUES ('D', 'd', '2018-01-01') ('B', 'b', '2018-01-01') ('C', 'c', '2018-01-01');
 
-SELECT * FROM using1 t1 ALL LEFT JOIN (SELECT *, c as a, d as b FROM using2) t2 USING (a, b) ORDER BY d;
-SELECT * FROM using1 t1 ALL INNER JOIN (SELECT *, c as a, d as b FROM using2) t2 USING (a, b) ORDER BY d;
+SELECT * FROM table1 t1 FORMAT PrettyCompact;
+SELECT *, c as a, d as b FROM table2 FORMAT PrettyCompact;
+SELECT * FROM table1 t1 ALL LEFT JOIN (SELECT *, c, d as b FROM table2) t2 USING (a, b) ORDER BY d FORMAT PrettyCompact;
+SELECT * FROM table1 t1 ALL INNER JOIN (SELECT *, c, d as b FROM table2) t2 USING (a, b) ORDER BY d FORMAT PrettyCompact;
 
-DROP TABLE using1;
-DROP TABLE using2;
+DROP TABLE table1;
+DROP TABLE table2;
--- dbms/tests/queries/0_stateless/01104_distributed_one_test.sql
+++ dbms/tests/queries/0_stateless/01104_distributed_one_test.sql
@@ -1,5 +1,5 @@
 DROP TABLE IF EXISTS d_one;
-CREATE TABLE d_one (dummy UInt8) ENGINE = Distributed(test_cluster_two_shards_localhost, system, one, rand());
+CREATE TABLE d_one (dummy UInt8) ENGINE = Distributed(test_cluster_two_shards, system, one, rand());
 
 SELECT 'local_0', toUInt8(1) AS dummy FROM system.one AS o WHERE o.dummy = 0;
 SELECT 'local_1', toUInt8(1) AS dummy FROM system.one AS o WHERE o.dummy = 1;
@@ -16,3 +16,6 @@ SELECT 'distributed_0', _shard_num, toUInt8(1) AS dummy FROM d_one AS o WHERE o.
 SELECT 'distributed_1', _shard_num, toUInt8(1) AS dummy FROM d_one AS o WHERE o.dummy = 1 ORDER BY _shard_num;
 
 DROP TABLE d_one;
+
+SELECT 'remote_0', toUInt8(1) AS dummy FROM remote('127.0.0.2', system, one) AS o WHERE o.dummy = 0;
+SELECT 'remote_1', toUInt8(1) AS dummy FROM remote('127.0.0.2', system, one) AS o WHERE o.dummy = 1;
