d5b9ba78e34a1b7abfc93c322207447a76cc2a7d
Vasily Nemkov <V.Nemkov@gmail.com>
2020-04-02 13:53:13 +0300

    Fixed final garbage token
    Also, added getTrailingZeroBitsUnsafe(), updated comments and cleaned up
    test cases

--- dbms/src/Storages/tests/gtest_SplitTokenExtractor.cpp
+++ dbms/src/Storages/tests/gtest_SplitTokenExtractor.cpp
@@ -55,13 +55,16 @@ TEST_P(SplitTokenExtractorTest, next)
     for (const auto & expected_token : param.tokens)
     {
         SCOPED_TRACE(++i);
-        EXPECT_TRUE(token_extractor.next(data->data(), data->size(), &pos, &token_start, &token_len));
+        ASSERT_TRUE(token_extractor.next(data->data(), data->size(), &pos, &token_start, &token_len));
         EXPECT_EQ(expected_token, param.source.substr(token_start, token_len))
                 << " token_start:" << token_start << " token_len: " << token_len;
     }
+
+    ASSERT_FALSE(token_extractor.next(data->data(), data->size(), &pos, &token_start, &token_len));
 }
 
-#define BINARY_STRING(str) std::string{str, sizeof(str)-1}
+// Helper to allow strings with embedded '\0' chars.
+#define BINARY_STRING(str) std::string{str, sizeof(str) - 1}
 
 INSTANTIATE_TEST_SUITE_P(ShortSingleToken,
     SplitTokenExtractorTest,
@@ -89,13 +92,13 @@ INSTANTIATE_TEST_SUITE_P(UTF8,
     ::testing::ValuesIn(std::initializer_list<SplitTokenExtractorTestCase>{
         {
             "Single token with mixed ASCII and UTF-8 chars",
-            BINARY_STRING("abc\u0442" "123\u0447XYZ\u043A"),
+            "abc\u0442" "123\u0447XYZ\u043A",
             {"abc\u0442" "123\u0447XYZ\u043A"}
         },
         {
             "Multiple UTF-8 tokens",
-            BINARY_STRING("\u043F\u0440\u0438\u0432\u0435\u0442, u043C\u0438\u0440!"),
-            {"\u043F\u0440\u0438\u0432\u0435\u0442", "u043C\u0438\u0440"}
+            "\u043F\u0440\u0438\u0432\u0435\u0442, \u043C\u0438\u0440!",
+            {"\u043F\u0440\u0438\u0432\u0435\u0442", "\u043C\u0438\u0440"}
         },
     })
 );
@@ -105,7 +108,7 @@ INSTANTIATE_TEST_SUITE_P(MultipleTokens,
     ::testing::ValuesIn(std::initializer_list<SplitTokenExtractorTestCase>{
         {
             "Multiple tokens separated by whitespace",
-            BINARY_STRING("\nabc 123\tXYZ\r"),
+            "\nabc 123\tXYZ\r",
             {
                 "abc", "123", "XYZ"
             }
@@ -130,7 +133,14 @@ INSTANTIATE_TEST_SUITE_P(MultipleTokens,
             "\xdc\xdd\xde\xdf\xe0\xe1\xe2\xe3\xe4\xe5\xe6\xe7\xe8\xe9\xea\xeb\xec\xed\xee\xef\xf0"
             "\xf1\xf2\xf3\xf4\xf5\xf6\xf7\xf8\xf9\xfa\xfb\xfc\xfd\xfe\xff"),
             {
-                "0123456789", "ABCDEFGHIJKLMNOPQRSTUVWXYZ", "abcdefghijklmnopqrstuvwxyz"
+                "0123456789", "ABCDEFGHIJKLMNOPQRSTUVWXYZ", "abcdefghijklmnopqrstuvwxyz",
+                "\x80\x81\x82\x83\x84\x85\x86\x87"
+                "\x88\x89\x8a\x8b\x8c\x8d\x8e\x8f\x90\x91\x92\x93\x94\x95\x96\x97\x98\x99\x9a\x9b\x9c"
+                "\x9d\x9e\x9f\xa0\xa1\xa2\xa3\xa4\xa5\xa6\xa7\xa8\xa9\xaa\xab\xac\xad\xae\xaf\xb0\xb1"
+                "\xb2\xb3\xb4\xb5\xb6\xb7\xb8\xb9\xba\xbb\xbc\xbd\xbe\xbf\xc0\xc1\xc2\xc3\xc4\xc5\xc6"
+                "\xc7\xc8\xc9\xca\xcb\xcc\xcd\xce\xcf\xd0\xd1\xd2\xd3\xd4\xd5\xd6\xd7\xd8\xd9\xda\xdb"
+                "\xdc\xdd\xde\xdf\xe0\xe1\xe2\xe3\xe4\xe5\xe6\xe7\xe8\xe9\xea\xeb\xec\xed\xee\xef\xf0"
+                "\xf1\xf2\xf3\xf4\xf5\xf6\xf7\xf8\xf9\xfa\xfb\xfc\xfd\xfe\xff"
             }
         }
     })
