a027b3319595cf7b7959a3d81847ef0a97afe64f
alesapin <alesapin@gmail.com>
2020-04-03 14:19:17 +0300

    Better test

--- tests/queries/0_stateless/01213_alter_rename_nested.sql
+++ tests/queries/0_stateless/01213_alter_rename_nested.sql
@@ -4,13 +4,13 @@ CREATE TABLE table_for_rename_nested
     date Date,
     key UInt64,
     n Nested(x UInt32, y String),
-    value1 LowCardinality(String) -- column with several files
+    value1 Array(Array(LowCardinality(String))) -- column with several files
 )
 ENGINE = MergeTree()
 PARTITION BY date
 ORDER BY key;
 
-INSERT INTO table_for_rename_nested (date, key, n.x, n.y, value1) SELECT toDate('2019-10-01'), number, [number + 1, number + 2, number + 3], ['a', 'b', 'c'], toString(number) FROM numbers(10);
+INSERT INTO table_for_rename_nested (date, key, n.x, n.y, value1) SELECT toDate('2019-10-01'), number, [number + 1, number + 2, number + 3], ['a', 'b', 'c'], [[toString(number)]] FROM numbers(10);
 
 SELECT n.x FROM table_for_rename_nested WHERE key = 7;
 SELECT n.y FROM table_for_rename_nested WHERE key = 7;
