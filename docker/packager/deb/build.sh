040d64c0a96e86393c0acae30f12c4bd266aff55
Yatsishin Ilya <2159081+qoega@users.noreply.github.com>
2020-04-07 14:33:50 +0300

    wip

--- docker/packager/deb/build.sh
+++ docker/packager/deb/build.sh
@@ -14,8 +14,8 @@ mv /*.tgz /output ||: # if exists
 if [ "binary" == "$BINARY_OUTPUT" ]
 then
   mkdir /output/binary
-  mv ./programs/clickhouse* /output/binary
-  mv ./dbms/unit_tests_dbms /output/binary
+  mv /build/obj-x86_64-linux-gnu/programs/clickhouse* /output/binary
+  mv /build/obj-x86_64-linux-gnu/dbms/unit_tests_dbms /output/binary
 fi
 ccache --show-stats ||:
 ln -s /usr/lib/x86_64-linux-gnu/libOpenCL.so.1.0.0 /usr/lib/libOpenCL.so ||:
