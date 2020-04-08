872123e7da5f9c4feb1502d2cc4d27402f58a40c
Ivan Lezhankin <ilezhankin@yandex-team.ru>
2020-04-08 18:08:19 +0300

    Backport the renames

--- docker/packager/binary/build.sh
+++ docker/packager/binary/build.sh
@@ -16,15 +16,15 @@ rm -f CMakeCache.txt
 cmake .. -LA -DCMAKE_BUILD_TYPE=$BUILD_TYPE -DSANITIZE=$SANITIZER $CMAKE_FLAGS
 ninja
 ccache --show-stats ||:
-mv ./dbms/programs/clickhouse* /output
-mv ./dbms/unit_tests_dbms /output
+mv ./programs/clickhouse* /output
+mv ./unit_tests_dbms /output
 find . -name '*.so' -print -exec mv '{}' /output \;
 find . -name '*.so.*' -print -exec mv '{}' /output \;
 
 # Different files for performance test.
 if [ "performance" == "$COMBINED_OUTPUT" ]
 then
-    cp -r ../dbms/tests/performance /output
+    cp -r ../tests/performance /output
     rm /output/unit_tests_dbms ||:
     rm /output/clickhouse-odbc-bridge ||:
 fi
@@ -33,9 +33,9 @@ fi
 if [ "" != "$COMBINED_OUTPUT" ]
 then
     mkdir -p /output/config
-    cp ../dbms/programs/server/config.xml /output/config
-    cp ../dbms/programs/server/users.xml /output/config
-    cp -r ../dbms/programs/server/config.d /output/config
+    cp ../programs/server/config.xml /output/config
+    cp ../programs/server/users.xml /output/config
+    cp -r ../programs/server/config.d /output/config
     tar -czvf "$COMBINED_OUTPUT.tgz" /output
     rm -r /output/*
     mv "$COMBINED_OUTPUT.tgz" /output
---
+++ tests/queries/conftest.py
@@ -0,0 +1,54 @@
+import pytest
+
+import os
+import sys
+import tempfile
+
+from server import ServerThread
+
+
+def pytest_addoption(parser):
+    parser.addoption(
+        "--builddir", action="store", default=None, help="Path to build directory to use binaries from",
+    )
+
+
+@pytest.fixture(scope='module')
+def cmdopts(request):
+    return {
+        'builddir': request.config.getoption("--builddir"),
+    }
+
+
+@pytest.fixture(scope='module')
+def bin_prefix(cmdopts):
+    prefix = 'clickhouse'
+    if cmdopts['builddir'] is not None:
+        prefix = os.path.join(cmdopts['builddir'], 'programs', prefix)
+    return prefix
+
+
+# TODO: also support stateful queries.
+QUERIES_PATH = os.path.join(os.path.dirname(os.path.abspath(__file__)), '0_stateless')
+
+@pytest.fixture(scope='module', params=[f for f in os.listdir(QUERIES_PATH) if f.endswith('.sql')])
+def sql_query(request):
+    return os.path.join(QUERIES_PATH, os.path.splitext(request.param)[0])
+
+
+@pytest.fixture
+def standalone_server(bin_prefix, tmp_path):
+    server = ServerThread(bin_prefix, str(tmp_path))
+    server.start()
+    wait_result = server.wait()
+
+    if wait_result is not None:
+        with open(os.path.join(server.log_dir, 'server', 'stdout.txt'), 'r') as f:
+            print >> sys.stderr, f.read()
+        with open(os.path.join(server.log_dir, 'server', 'stderr.txt'), 'r') as f:
+            print >> sys.stderr, f.read()
+        pytest.fail('Server died unexpectedly with code {code}'.format(code=server._proc.returncode), pytrace=False)
+
+    yield server
+
+    server.stop()
