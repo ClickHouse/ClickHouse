5b2b8d38fa50fd8b1f195c5890be8c103ad65b61
Pavel Kovalenko <jokserfn@gmail.com>
2020-04-08 11:41:13 +0300

    Download part trough disk interface.

--- src/Storages/MergeTree/MergeTreeData.h
+++ src/Storages/MergeTree/MergeTreeData.h
@@ -625,6 +625,9 @@ public:
         return storage_settings.get();
     }
 
+    /// Get relative table path
+    String getRelativeDataPath() const { return relative_data_path; }
+
     /// Get table path on disk
     String getFullPathOnDisk(const DiskPtr & disk) const;
 
