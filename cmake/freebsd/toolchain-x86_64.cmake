a650fd03ab35c3fe8a545dc6c55582f482662a87
Ivan Lezhankin <ilezhankin@yandex-team.ru>
2020-04-09 15:02:54 +0300

    Fix build

--- cmake/freebsd/toolchain-x86_64.cmake
+++ cmake/freebsd/toolchain-x86_64.cmake
@@ -7,6 +7,8 @@ set (CMAKE_SYSROOT "${CMAKE_CURRENT_LIST_DIR}/../toolchain/freebsd-x86_64")
 
 set (CMAKE_TRY_COMPILE_TARGET_TYPE STATIC_LIBRARY)  # disable linkage check - it doesn't work in CMake
 
+set (CMAKE_AR "/usr/bin/ar" CACHE FILEPATH "" FORCE)
+
 set (LINKER_NAME "lld" CACHE STRING "" FORCE)
 
 set (CMAKE_EXE_LINKER_FLAGS_INIT "-fuse-ld=lld")
