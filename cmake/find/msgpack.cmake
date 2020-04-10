24503101c53f164fc9dfc2eac6af16d4955f8b96
Azat Khuzhin <a3at.mail@gmail.com>
2020-04-10 10:46:23 +0300

    Add ability to use unbundled msgpack
    Actually it works before but by accidentally and with warnings.

--- cmake/find/msgpack.cmake
+++ cmake/find/msgpack.cmake
@@ -1,2 +1,17 @@
-set(MSGPACK_INCLUDE_DIR ${ClickHouse_SOURCE_DIR}/contrib/msgpack-c/include)
+option (USE_INTERNAL_MSGPACK_LIBRARY "Set to FALSE to use system msgpack library instead of bundled" ${NOT_UNBUNDLED})
+
+if (USE_INTERNAL_MSGPACK_LIBRARY)
+    if (NOT EXISTS "${ClickHouse_SOURCE_DIR}/contrib/msgpack-c/include/msgpack.hpp")
+        message(WARNING "submodule contrib/msgpack-c is missing. to fix try run: \n git submodule update --init --recursive")
+        set(USE_INTERNAL_MSGPACK_LIBRARY     0)
+        set(MISSING_INTERNAL_MSGPACK_LIBRARY 1)
+    endif()
+endif()
+
+if (USE_INTERNAL_MSGPACK_LIBRARY)
+    set(MSGPACK_INCLUDE_DIR ${ClickHouse_SOURCE_DIR}/contrib/msgpack-c/include)
+else()
+    find_path(MSGPACK_INCLUDE_DIR NAMES msgpack.hpp PATHS ${MSGPACK_INCLUDE_PATHS})
+endif()
+
 message(STATUS "Using msgpack: ${MSGPACK_INCLUDE_DIR}")
