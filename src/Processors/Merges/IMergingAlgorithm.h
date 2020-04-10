a57cc590714078596d10f61c6cc38256af44d3cc
Nikolai Kochetov <nik-kochetov@yandex-team.ru>
2020-04-10 12:32:34 +0300

    Fix build.

--- src/Processors/Merges/IMergingAlgorithm.h
+++ src/Processors/Merges/IMergingAlgorithm.h
@@ -24,7 +24,7 @@ public:
     virtual void consume(Chunk chunk, size_t source_num) = 0;
     virtual Status merge() = 0;
 
-    IMergingAlgorithm(const IMergingAlgorithm &) = default;
+    IMergingAlgorithm() = default;
     IMergingAlgorithm(IMergingAlgorithm &&) = default;
     virtual ~IMergingAlgorithm() = default;
 };
