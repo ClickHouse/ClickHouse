ba840b5a59e296414bf1483e4330016b6133a2a9
Nikolai Kochetov <nik-kochetov@yandex-team.ru>
2020-04-10 19:44:47 +0300

    Added VersionedCollapsingAlgorithm.

--- src/Processors/Merges/VersionedCollapsingTransform.h
+++ src/Processors/Merges/VersionedCollapsingTransform.h
@@ -1,15 +1,7 @@
 #pragma once
 
 #include <Processors/Merges/IMergingTransform.h>
-#include <Processors/Merges/RowRef.h>
-#include <Processors/Merges/FixedSizeDequeWithGaps.h>
-#include <Processors/Merges/MergedData.h>
-#include <Core/SortDescription.h>
-#include <Core/SortCursor.h>
-#include <DataStreams/ColumnGathererStream.h>
-
-#include <common/logger_useful.h>
-#include <queue>
+#include <Processors/Merges/VersionedCollapsingAlgorithm.h>
 
 
 namespace DB
@@ -20,7 +12,7 @@ namespace DB
   *   (the columns by which the data is sorted, including specially specified version column),
   *   merges any pair of consecutive rows with opposite sign.
   */
-class VersionedCollapsingTransform final : public IMergingTransform
+class VersionedCollapsingTransform final : public IMergingTransform2<VersionedCollapsingAlgorithm>
 {
 public:
     /// Don't need version column. It's in primary key.
@@ -29,52 +21,20 @@ public:
         SortDescription description_, const String & sign_column_,
         size_t max_block_size,
         WriteBuffer * out_row_sources_buf_ = nullptr,
-        bool use_average_block_sizes = false);
+        bool use_average_block_sizes = false)
+        : IMergingTransform2(
+            num_inputs, header, header, true,
+            header,
+            num_inputs,
+            std::move(description_),
+            sign_column_,
+            max_block_size,
+            out_row_sources_buf_,
+            use_average_block_sizes)
+    {
+    }
 
     String getName() const override { return "VersionedCollapsingTransform"; }
-    void work() override;
-
-protected:
-    void initializeInputs() override;
-    void consume(Chunk chunk, size_t input_number) override;
-
-private:
-    Logger * log = &Logger::get("VersionedCollapsingTransform");
-
-    MergedData merged_data;
-
-    SortDescription description;
-    size_t sign_column_number = 0;
-
-    /// Used in Vertical merge algorithm to gather non-PK/non-index columns (on next step)
-    /// If it is not nullptr then it should be populated during execution
-    WriteBuffer * out_row_sources_buf = nullptr;
-
-    using RowRef = detail::RowRefWithOwnedChunk;
-    const size_t max_rows_in_queue;
-
-    /// Allocator must be destroyed after all RowRefs.
-    detail::SharedChunkAllocator chunk_allocator;
-
-    /// Chunks currently being merged.
-    using SourceChunks = std::vector<detail::SharedChunkPtr>;
-    SourceChunks source_chunks;
-    SortCursorImpls cursors;
-
-    SortingHeap<SortCursor> queue;
-    bool is_queue_initialized = false;
-
-    /// Rows with the same primary key and sign.
-    FixedSizeDequeWithGaps<RowRef> current_keys;
-    Int8 sign_in_queue = 0;
-
-    std::queue<RowSourcePart> current_row_sources;   /// Sources of rows with the current primary key
-
-    void insertGap(size_t gap_size);
-    void insertRow(size_t skip_rows, const RowRef & row);
-    void merge();
-    void updateCursor(Chunk chunk, size_t source_num);
-    void setRowRef(RowRef & row, SortCursor & cursor) { row.set(cursor, source_chunks[cursor.impl->order]); }
 };
 
 }
