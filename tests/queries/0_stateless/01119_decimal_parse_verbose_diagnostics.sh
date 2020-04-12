f1fe20e7ef54120c82387033e0101489c79f6533
Alexey Milovidov <milovidov@yandex-team.ru>
2020-04-12 17:59:43 +0300

    Added a test #10202

---
+++ tests/queries/0_stateless/01119_decimal_parse_verbose_diagnostics.sh
@@ -0,0 +1,6 @@
+#!/usr/bin/env bash
+
+CURDIR=$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)
+. $CURDIR/../shell_config.sh
+
+$CLICKHOUSE_LOCAL --query "SELECT * FROM table" --input-format CSV --structure 'd Decimal64(10)' <<<'12345678'$'\n''123456789' 2>&1 | grep -v -F 'version'
