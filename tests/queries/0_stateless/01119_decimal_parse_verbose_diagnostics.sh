2a4e88191a7aaf2ecf04f7fb94be96357eda43be
Alexey Milovidov <milovidov@yandex-team.ru>
2020-04-12 22:10:43 +0300

    Update test

--- tests/queries/0_stateless/01119_decimal_parse_verbose_diagnostics.sh
+++ tests/queries/0_stateless/01119_decimal_parse_verbose_diagnostics.sh
@@ -3,4 +3,4 @@
 CURDIR=$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)
 . $CURDIR/../shell_config.sh
 
-$CLICKHOUSE_LOCAL --query "SELECT * FROM table" --input-format CSV --structure 'd Decimal64(10)' <<<'12345678'$'\n''123456789' 2>&1 | grep -v -F 'version'
+$CLICKHOUSE_LOCAL --query "SELECT * FROM table" --input-format CSV --structure 'd Decimal64(10)' <<<'12345678'$'\n''123456789' 2>&1 | grep -v -F 'version' | sed 's/Exception//';
