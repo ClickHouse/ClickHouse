060a151f4642fc80ec692dec8fdc78c66d2eee0a
Nikolai Kochetov <nik-kochetov@yandex-team.ru>
2020-04-14 21:58:22 +0300

    Fix test with vesrioned collapsing.

--- src/Processors/Merges/Algorithms/VersionedCollapsingAlgorithm.cpp
+++ src/Processors/Merges/Algorithms/VersionedCollapsingAlgorithm.cpp
@@ -16,7 +16,8 @@ VersionedCollapsingAlgorithm::VersionedCollapsingAlgorithm(
     : IMergingAlgorithmWithSharedChunks(
             num_inputs, std::move(description_), out_row_sources_buf_, MAX_ROWS_IN_MULTIVERSION_QUEUE)
     , merged_data(header.cloneEmptyColumns(), use_average_block_sizes, max_block_size)
-    , max_rows_in_queue(MAX_ROWS_IN_MULTIVERSION_QUEUE - 1)  /// -1 for +1 in FixedSizeDequeWithGaps's internal buffer
+    /// -1 for +1 in FixedSizeDequeWithGaps's internal buffer. 3 is a reasonable minimum size to collapse anything.
+    , max_rows_in_queue(std::min(std::max<size_t>(3, max_block_size), MAX_ROWS_IN_MULTIVERSION_QUEUE) - 1)
     , current_keys(max_rows_in_queue)
 {
     sign_column_number = header.getPositionByName(sign_column_);
