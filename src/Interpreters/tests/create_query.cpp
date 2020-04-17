283898bf674b74e50a6e2fb4681485f9ca3b1b2d
Nikolai Kochetov <nik-kochetov@yandex-team.ru>
2020-04-17 19:34:45 +0300

    Fix build.

--- src/Interpreters/tests/create_query.cpp
+++ src/Interpreters/tests/create_query.cpp
@@ -78,8 +78,8 @@ try
     ParserCreateQuery parser;
     ASTPtr ast = parseQuery(parser, input.data(), input.data() + input.size(), "", 0, 0);
 
-    SharedContextHolder shared_context = Context::createShared(shared_context.get());
-    Context context = Context::createGlobal();
+    SharedContextHolder shared_context = Context::createShared();
+    Context context = Context::createGlobal(shared_context.get());
     context.makeGlobalContext();
 
     context.setPath("./");
