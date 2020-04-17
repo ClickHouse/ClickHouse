33b622d428b0923bd9e7f3db778c2e3631ed30d4
Yatsishin Ilya <2159081+qoega@users.noreply.github.com>
2020-04-17 19:01:16 +0300

    Move integration tests docker files to docker/ directory. Rename image -> runner.

--- docker/test/integration/runner/modprobe.sh
+++ docker/test/integration/runner/modprobe.sh
@@ -9,10 +9,10 @@ set -eu
 # Docker often uses "modprobe -va foo bar baz"
 # so we ignore modules that start with "-"
 for module; do
-	if [ "${module#-}" = "$module" ]; then
-		ip link show "$module" || true
-		lsmod | grep "$module" || true
-	fi
+  if [ "${module#-}" = "$module" ]; then
+    ip link show "$module" || true
+    lsmod | grep "$module" || true
+  fi
 done
 
 # remove /usr/local/... from PATH so we can exec the real modprobe as a last resort
