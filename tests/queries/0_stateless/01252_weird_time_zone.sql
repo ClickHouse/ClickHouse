7d8f1e8f41a8449fe9f427fedc2883457db29ea9
Alexey Milovidov <milovidov@yandex-team.ru>
2020-04-20 10:23:17 +0300

    More tests

--- tests/queries/0_stateless/01252_weird_time_zone.sql
+++ tests/queries/0_stateless/01252_weird_time_zone.sql
@@ -1,7 +1,8 @@
-SELECT toDateTime('2020-01-02 03:04:05', 'Pacific/Kiritimati');
-SELECT toDateTime('2020-01-02 03:04:05', 'Africa/El_Aaiun');
-SELECT toDateTime('2020-01-02 03:04:05', 'Asia/Pyongyang');
-SELECT toDateTime('2020-01-02 03:04:05', 'Pacific/Kwajalein');
-SELECT toDateTime('2020-01-02 03:04:05', 'Pacific/Apia');
-SELECT toDateTime('2020-01-02 03:04:05', 'Pacific/Enderbury');
-SELECT toDateTime('2020-01-02 03:04:05', 'Pacific/Fakaofo');
+SELECT toDateTime('2020-01-02 03:04:05', 'Pacific/Kiritimati') AS x, toStartOfDay(x), toHour(x);
+SELECT toDateTime('2020-01-02 03:04:05', 'Africa/El_Aaiun') AS x, toStartOfDay(x), toHour(x);
+SELECT toDateTime('2020-01-02 03:04:05', 'Asia/Pyongyang') AS x, toStartOfDay(x), toHour(x);
+SELECT toDateTime('2020-01-02 03:04:05', 'Pacific/Kwajalein') AS x, toStartOfDay(x), toHour(x);
+SELECT toDateTime('2020-01-02 03:04:05', 'Pacific/Apia') AS x, toStartOfDay(x), toHour(x);
+SELECT toDateTime('2020-01-02 03:04:05', 'Pacific/Enderbury') AS x, toStartOfDay(x), toHour(x);
+SELECT toDateTime('2020-01-02 03:04:05', 'Pacific/Fakaofo') AS x, toStartOfDay(x), toHour(x);
+
