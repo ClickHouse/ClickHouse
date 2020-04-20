bb3c69b3267aec6a27c8572e3471c93a76f2086b
Alexey Milovidov <milovidov@yandex-team.ru>
2020-04-20 05:35:19 +0300

    More tests

--- tests/queries/0_stateless/01251_dict_is_in_infinite_loop.sql
+++ tests/queries/0_stateless/01251_dict_is_in_infinite_loop.sql
@@ -28,6 +28,70 @@ SELECT dictIsIn('database_for_dict.dictionary_with_hierarchy', materialize(toUIn
 SELECT dictIsIn('database_for_dict.dictionary_with_hierarchy', toUInt64(11), materialize(toUInt64(222)));
 SELECT dictIsIn('database_for_dict.dictionary_with_hierarchy', materialize(toUInt64(22)), materialize(toUInt64(111)));
 
+SELECT dictGetHierarchy('database_for_dict.dictionary_with_hierarchy', toUInt64(11));
+SELECT dictGetHierarchy('database_for_dict.dictionary_with_hierarchy', toUInt64(22));
+SELECT dictGetHierarchy('database_for_dict.dictionary_with_hierarchy', materialize(toUInt64(11)));
+SELECT dictGetHierarchy('database_for_dict.dictionary_with_hierarchy', materialize(toUInt64(22)));
+
+
+DROP DICTIONARY IF EXISTS database_for_dict.dictionary_with_hierarchy;
+
+CREATE DICTIONARY database_for_dict.dictionary_with_hierarchy
+(
+    id UInt64, parent_id UInt64 HIERARCHICAL, value String
+)
+PRIMARY KEY id
+SOURCE(CLICKHOUSE(host 'localhost' port 9000 user 'default' table 'dict_source'))
+LAYOUT(FLAT())
+LIFETIME(MIN 1 MAX 1);
+
+SELECT dictIsIn('database_for_dict.dictionary_with_hierarchy', toUInt64(2), toUInt64(1));
+
+SELECT dictIsIn('database_for_dict.dictionary_with_hierarchy', toUInt64(22), toUInt64(11));
+SELECT dictIsIn('database_for_dict.dictionary_with_hierarchy', materialize(toUInt64(22)), toUInt64(11));
+SELECT dictIsIn('database_for_dict.dictionary_with_hierarchy', toUInt64(11), materialize(toUInt64(22)));
+SELECT dictIsIn('database_for_dict.dictionary_with_hierarchy', materialize(toUInt64(22)), materialize(toUInt64(11)));
+
+SELECT dictIsIn('database_for_dict.dictionary_with_hierarchy', toUInt64(22), toUInt64(111));
+SELECT dictIsIn('database_for_dict.dictionary_with_hierarchy', materialize(toUInt64(22)), toUInt64(111));
+SELECT dictIsIn('database_for_dict.dictionary_with_hierarchy', toUInt64(11), materialize(toUInt64(222)));
+SELECT dictIsIn('database_for_dict.dictionary_with_hierarchy', materialize(toUInt64(22)), materialize(toUInt64(111)));
+
+SELECT dictGetHierarchy('database_for_dict.dictionary_with_hierarchy', toUInt64(11));
+SELECT dictGetHierarchy('database_for_dict.dictionary_with_hierarchy', toUInt64(22));
+SELECT dictGetHierarchy('database_for_dict.dictionary_with_hierarchy', materialize(toUInt64(11)));
+SELECT dictGetHierarchy('database_for_dict.dictionary_with_hierarchy', materialize(toUInt64(22)));
+
+
+DROP DICTIONARY IF EXISTS database_for_dict.dictionary_with_hierarchy;
+
+CREATE DICTIONARY database_for_dict.dictionary_with_hierarchy
+(
+    id UInt64, parent_id UInt64 HIERARCHICAL, value String
+)
+PRIMARY KEY id
+SOURCE(CLICKHOUSE(host 'localhost' port 9000 user 'default' table 'dict_source'))
+LAYOUT(CACHE(SIZE_IN_CELLS 10))
+LIFETIME(MIN 1 MAX 1);
+
+SELECT dictIsIn('database_for_dict.dictionary_with_hierarchy', toUInt64(2), toUInt64(1));
+
+SELECT dictIsIn('database_for_dict.dictionary_with_hierarchy', toUInt64(22), toUInt64(11));
+SELECT dictIsIn('database_for_dict.dictionary_with_hierarchy', materialize(toUInt64(22)), toUInt64(11));
+SELECT dictIsIn('database_for_dict.dictionary_with_hierarchy', toUInt64(11), materialize(toUInt64(22)));
+SELECT dictIsIn('database_for_dict.dictionary_with_hierarchy', materialize(toUInt64(22)), materialize(toUInt64(11)));
+
+SELECT dictIsIn('database_for_dict.dictionary_with_hierarchy', toUInt64(22), toUInt64(111));
+SELECT dictIsIn('database_for_dict.dictionary_with_hierarchy', materialize(toUInt64(22)), toUInt64(111));
+SELECT dictIsIn('database_for_dict.dictionary_with_hierarchy', toUInt64(11), materialize(toUInt64(222)));
+SELECT dictIsIn('database_for_dict.dictionary_with_hierarchy', materialize(toUInt64(22)), materialize(toUInt64(111)));
+
+SELECT dictGetHierarchy('database_for_dict.dictionary_with_hierarchy', toUInt64(11));
+SELECT dictGetHierarchy('database_for_dict.dictionary_with_hierarchy', toUInt64(22));
+SELECT dictGetHierarchy('database_for_dict.dictionary_with_hierarchy', materialize(toUInt64(11)));
+SELECT dictGetHierarchy('database_for_dict.dictionary_with_hierarchy', materialize(toUInt64(22)));
+
+
 DROP DICTIONARY database_for_dict.dictionary_with_hierarchy;
 DROP TABLE dict_source;
 DROP DATABASE database_for_dict;
