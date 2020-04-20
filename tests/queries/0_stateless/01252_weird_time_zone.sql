effd6c2de5f4e67893c9b8e6ed15f2bb5d5839a5
Alexey Milovidov <milovidov@yandex-team.ru>
2020-04-20 10:21:20 +0300

    Added a test

---
+++ tests/queries/0_stateless/01252_weird_time_zone.sql
@@ -0,0 +1,7 @@
+SELECT toDateTime('2020-01-02 03:04:05', 'Pacific/Kiritimati');
+SELECT toDateTime('2020-01-02 03:04:05', 'Africa/El_Aaiun');
+SELECT toDateTime('2020-01-02 03:04:05', 'Asia/Pyongyang');
+SELECT toDateTime('2020-01-02 03:04:05', 'Pacific/Kwajalein');
+SELECT toDateTime('2020-01-02 03:04:05', 'Pacific/Apia');
+SELECT toDateTime('2020-01-02 03:04:05', 'Pacific/Enderbury');
+SELECT toDateTime('2020-01-02 03:04:05', 'Pacific/Fakaofo');
