5b9a0146262a35d57607d895a9245748782d226c
BohuTANG <overred.shuttler@gmail.com>
2020-04-20 19:27:58 +0800

    add ERR packet parse

--- src/Core/tests/mysql_client.cpp
+++ src/Core/tests/mysql_client.cpp
@@ -6,7 +6,7 @@ int main(int, char **)
     using namespace DB;
 
     UInt16 port = 4407;
-    String host = "127.0.0.1", user = "root", password = "mock";
+    String host = "127.0.0.1", user = "mock", password = "mock";
     MySQLClient client(host, port, user, password, "");
     client.connect();
     return 0;
