cc0b1f53b3466ed57d7f05ac98cfdaed5c0584c6
Nikolai Kochetov <nik-kochetov@yandex-team.ru>
2020-04-22 13:04:34 +0300

    Fix build.

--- src/AggregateFunctions/tests/quantile-t-digest.cpp
+++ src/AggregateFunctions/tests/quantile-t-digest.cpp
@@ -6,14 +6,14 @@ int main(int, char **)
 {
     using namespace DB;
 
-    QuantileTDigest<float> tdigest;
+    QuantileTDigest<float, true, false> tdigest;
     tdigest.add(1);
     tdigest.add(2);
     tdigest.add(3);
     std::cout << tdigest.get(0.5) << "\n";
     WriteBufferFromOwnString wb;
     tdigest.serialize(wb);
-    QuantileTDigest<float> other;
+    QuantileTDigest<float, true, false> other;
     ReadBufferFromString rb{wb.str()};
     other.deserialize(rb);
     std::cout << other.get(0.5) << "\n";
