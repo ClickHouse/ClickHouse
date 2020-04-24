66e627070203c7e81accead30f5ffc7f5e18d4be
Azat Khuzhin <a3at.mail@gmail.com>
2020-04-25 02:03:27 +0300

    Avoid processing all batches before Distributed shutdown

--- src/Storages/StorageDistributed.cpp
+++ src/Storages/StorageDistributed.cpp
@@ -570,6 +570,8 @@ void StorageDistributed::startup()
 
 void StorageDistributed::shutdown()
 {
+    monitors_blocker.cancelForever();
+
     std::lock_guard lock(cluster_nodes_mutex);
     cluster_nodes_data.clear();
 }
