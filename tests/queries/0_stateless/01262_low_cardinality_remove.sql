dc8787a7b8c70908eb660659c7192b4f51cd49ed
Alexey Milovidov <milovidov@yandex-team.ru>
2020-04-26 19:39:48 +0300

    Move old bug to test

--- tests/queries/0_stateless/01262_low_cardinality_remove.sql
+++ tests/queries/0_stateless/01262_low_cardinality_remove.sql
@@ -7,18 +7,18 @@ CREATE TABLE IF NOT EXISTS testTable (
 ) ENGINE MergeTree()
 ORDER BY (A);
 
-INSERT INTO testTable VALUES ('A', 1),('B',2),('C',3)
+INSERT INTO testTable VALUES ('A', 1),('B',2),('C',3);
 
 CREATE VIEW testView AS 
-SELECT 
+SELECT
  A as ALow, -- like account
  B
-FROM  
+FROM
    testTable;
 
 SELECT CAST(ALow, 'String') AS AStr
 FROM testView
-GROUP BY AStr;
+GROUP BY AStr ORDER BY AStr;
 
 DROP TABLE testTable;
 
@@ -30,7 +30,13 @@ ORDER BY (A);
 
 SELECT CAST(ALow, 'String') AS AStr
 FROM testView
-GROUP BY AStr;
+GROUP BY AStr ORDER BY AStr;
+
+INSERT INTO testTable VALUES ('A', 1),('B',2),('C',3);
+
+SELECT CAST(ALow, 'String') AS AStr
+FROM testView
+GROUP BY AStr ORDER BY AStr;
 
 DROP TABLE IF EXISTS testView;
 DROP TABLE IF EXISTS testTable;
