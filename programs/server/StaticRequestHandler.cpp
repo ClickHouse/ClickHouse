25fef0578b43e401150ede02a7bbee59a41433b7
zhang2014 <coswde@gmail.com>
2020-04-26 19:06:14 +0800

    ISSUES-5436 add integration test

--- programs/server/StaticRequestHandler.cpp
+++ programs/server/StaticRequestHandler.cpp
@@ -7,6 +7,8 @@
 #include <IO/ReadBufferFromFile.h>
 #include <IO/WriteBufferFromString.h>
 #include <IO/copyData.h>
+#include <IO/WriteHelpers.h>
+#include <IO/WriteBufferFromHTTPServerResponse.h>
 
 #include <Common/Exception.h>
 
@@ -22,55 +24,135 @@ namespace DB
 namespace ErrorCodes
 {
     extern const int INCORRECT_FILE_NAME;
+    extern const int HTTP_LENGTH_REQUIRED;
     extern const int INVALID_CONFIG_PARAMETER;
 }
 
-void StaticRequestHandler::handleRequest(Poco::Net::HTTPServerRequest &, Poco::Net::HTTPServerResponse & response)
+static inline WriteBufferPtr responseWriteBuffer(Poco::Net::HTTPServerRequest & request, Poco::Net::HTTPServerResponse & response, unsigned int keep_alive_timeout)
+{
+    /// The client can pass a HTTP header indicating supported compression method (gzip or deflate).
+    String http_response_compression_methods = request.get("Accept-Encoding", "");
+    CompressionMethod http_response_compression_method = CompressionMethod::None;
+
+    if (!http_response_compression_methods.empty())
+    {
+        /// If client supports brotli - it's preferred.
+        /// Both gzip and deflate are supported. If the client supports both, gzip is preferred.
+        /// NOTE parsing of the list of methods is slightly incorrect.
+
+        if (std::string::npos != http_response_compression_methods.find("br"))
+            http_response_compression_method = CompressionMethod::Brotli;
+        else if (std::string::npos != http_response_compression_methods.find("gzip"))
+            http_response_compression_method = CompressionMethod::Gzip;
+        else if (std::string::npos != http_response_compression_methods.find("deflate"))
+            http_response_compression_method = CompressionMethod::Zlib;
+    }
+
+    bool client_supports_http_compression = http_response_compression_method != CompressionMethod::None;
+
+    return std::make_shared<WriteBufferFromHTTPServerResponse>(
+        request, response, keep_alive_timeout, client_supports_http_compression, http_response_compression_method);
+}
+
+static inline void trySendExceptionToClient(
+    const std::string & s, int exception_code,
+    Poco::Net::HTTPServerRequest & request, Poco::Net::HTTPServerResponse & response , WriteBuffer & out)
 {
     try
     {
-        setResponseDefaultHeaders(response, server.config().getUInt("keep_alive_timeout", 10));
+        response.set("X-ClickHouse-Exception-Code", toString<int>(exception_code));
 
+        /// If HTTP method is POST and Keep-Alive is turned on, we should read the whole request body
+        /// to avoid reading part of the current request body in the next request.
+        if (request.getMethod() == Poco::Net::HTTPRequest::HTTP_POST
+            && response.getKeepAlive() && !request.stream().eof() && exception_code != ErrorCodes::HTTP_LENGTH_REQUIRED)
+            request.stream().ignore(std::numeric_limits<std::streamsize>::max());
+
+        response.setStatusAndReason(Poco::Net::HTTPResponse::HTTP_INTERNAL_SERVER_ERROR);
+
+        if (!response.sent())
+            response.send() << s << std::endl;
+        else
+        {
+            if (out.count() != out.offset())
+                out.position() = out.buffer().begin();
+
+            writeString(s, out);
+            writeChar('\n', out);
+
+            out.next();
+            out.finalize();
+        }
+    }
+    catch (...)
+    {
+        tryLogCurrentException("StaticRequestHandler", "Cannot send exception to client");
+    }
+}
+
+void StaticRequestHandler::handleRequest(Poco::Net::HTTPServerRequest & request, Poco::Net::HTTPServerResponse & response)
+{
+    auto keep_alive_timeout = server.config().getUInt("keep_alive_timeout", 10);
+    const auto & out = responseWriteBuffer(request, response, keep_alive_timeout);
+
+    try
+    {
         response.setContentType(content_type);
-        response.setStatusAndReason(Poco::Net::HTTPResponse::HTTPStatus(status));
 
-        response.sendBuffer(response_content.data(), response_content.size());
+        if (request.getVersion() == Poco::Net::HTTPServerRequest::HTTP_1_1)
+            response.setChunkedTransferEncoding(true);
+
+        /// Workaround. Poco does not detect 411 Length Required case.
+        if (request.getMethod() == Poco::Net::HTTPRequest::HTTP_POST && !request.getChunkedTransferEncoding() && !request.hasContentLength())
+            throw Exception("The Transfer-Encoding is not chunked and there is no Content-Length header for POST request", ErrorCodes::HTTP_LENGTH_REQUIRED);
+
+        setResponseDefaultHeaders(response, keep_alive_timeout);
+        response.setStatusAndReason(Poco::Net::HTTPResponse::HTTPStatus(status));
+        writeResponse(*out);
     }
     catch (...)
     {
         tryLogCurrentException("StaticRequestHandler");
+
+        int exception_code = getCurrentExceptionCode();
+        std::string exception_message = getCurrentExceptionMessage(false, true);
+        trySendExceptionToClient(exception_message, exception_code, request, response, *out);
     }
 }
 
-StaticRequestHandler::StaticRequestHandler(IServer & server_, const String & expression, int status_, const String & content_type_)
-    : server(server_), status(status_), content_type(content_type_)
+void StaticRequestHandler::writeResponse(WriteBuffer & out)
 {
     static const String file_prefix = "file://";
     static const String config_prefix = "config://";
 
-    if (startsWith(expression, file_prefix))
+    if (startsWith(response_expression, file_prefix))
     {
-        std::string config_dir = Poco::Path(server.context().getPath()).parent().toString();
-        const std::string & file_path = config_dir + expression.substr(file_prefix.size(), expression.size() - file_prefix.size());
+        const auto & user_files_absolute_path = Poco::Path(server.context().getUserFilesPath()).makeAbsolute().makeDirectory().toString();
+        const auto & file_name = response_expression.substr(file_prefix.size(), response_expression.size() - file_prefix.size());
 
+        const auto & file_path = Poco::Path(user_files_absolute_path, file_name).makeAbsolute().toString();
         if (!Poco::File(file_path).exists())
-            throw Exception("Invalid file name for static HTTPHandler." + file_path, ErrorCodes::INCORRECT_FILE_NAME);
+            throw Exception("Invalid file name " + file_path + " for static HTTPHandler. ", ErrorCodes::INCORRECT_FILE_NAME);
 
-        WriteBufferFromOwnString out;
         ReadBufferFromFile in(file_path);
         copyData(in, out);
-        response_content = out.str();
     }
-    else if (startsWith(expression, config_prefix))
+    else if (startsWith(response_expression, config_prefix))
     {
-        if (expression.size() <= config_prefix.size())
-            throw Exception("Static routing rule handler must contain a complete configuration path, for example: config://config_key",
+        if (response_expression.size() <= config_prefix.size())
+            throw Exception( "Static routing rule handler must contain a complete configuration path, for example: config://config_key",
                 ErrorCodes::INVALID_CONFIG_PARAMETER);
 
-        response_content = server.config().getString(expression.substr(config_prefix.size(), expression.size() - config_prefix.size()), "Ok.\n");
+        const auto & config_path = response_expression.substr(config_prefix.size(), response_expression.size() - config_prefix.size());
+        writeString(server.config().getRawString(config_path, "Ok.\n"), out);
     }
     else
-        response_content = expression;
+        writeString(response_expression, out);
+}
+
+StaticRequestHandler::StaticRequestHandler(IServer & server_, const String & expression, int status_, const String & content_type_)
+    : server(server_), status(status_), content_type(content_type_), response_expression(expression)
+{
 }
 
 Poco::Net::HTTPRequestHandlerFactory * createStaticHandlerFactory(IServer & server, const std::string & config_prefix)
@@ -80,7 +162,7 @@ Poco::Net::HTTPRequestHandlerFactory * createStaticHandlerFactory(IServer & serv
     std::string response_content_type = server.config().getString(config_prefix + ".handler.content_type", "text/plain; charset=UTF-8");
 
     return addFiltersFromConfig(new RoutingRuleHTTPHandlerFactory<StaticRequestHandler>(
-        server, std::move(response_content), status, std::move(response_content_type)), server.config(), config_prefix);
+        server, std::move(response_content), std::move(status), std::move(response_content_type)), server.config(), config_prefix);
 }
 
 }
--- tests/integration/test_http_handlers_config/test.py
+++ tests/integration/test_http_handlers_config/test.py
@@ -45,6 +45,73 @@ def test_predefine_query_handler():
 
         assert 404 == cluster.instance.http_request('test_predefine_handler_get?max_threads=1', method='POST', headers={'XXX': 'xxx'}).status_code
 
-        assert 400 == cluster.instance.http_request('test_predefine_handler_get?max_threads=1', method='GET', headers={'XXX': 'xxx'}).status_code
+        assert 500 == cluster.instance.http_request('test_predefine_handler_get?max_threads=1', method='GET', headers={'XXX': 'xxx'}).status_code
 
-        assert '1\n' == cluster.instance.http_request('test_predefine_handler_get?max_threads=1&setting_name=max_threads', method='GET', headers={'XXX': 'xxx'}).content
+        assert 'max_threads\t1\n' == cluster.instance.http_request('test_predefine_handler_get?max_threads=1&setting_name=max_threads', method='GET', headers={'XXX': 'xxx'}).content
+
+        assert 'max_threads\t1\nmax_alter_threads\t1\n' == cluster.instance.http_request(
+            'query_param_with_url/max_threads?max_threads=1&max_alter_threads=1', headers={'XXX': 'max_alter_threads'}).content
+
+
+def test_fixed_static_handler():
+    with contextlib.closing(SimpleCluster(ClickHouseCluster(__file__), "static_handler", "test_static_handler")) as cluster:
+        assert 404 == cluster.instance.http_request('', method='GET', headers={'XXX': 'xxx'}).status_code
+
+        assert 404 == cluster.instance.http_request('test_get_fixed_static_handler', method='GET', headers={'XXX': 'bad'}).status_code
+
+        assert 404 == cluster.instance.http_request('test_get_fixed_static_handler', method='POST', headers={'XXX': 'xxx'}).status_code
+
+        assert 402 == cluster.instance.http_request('test_get_fixed_static_handler', method='GET', headers={'XXX': 'xxx'}).status_code
+        assert 'text/html; charset=UTF-8' == cluster.instance.http_request('test_get_fixed_static_handler', method='GET', headers={'XXX': 'xxx'}).headers['Content-Type']
+        assert 'Test get static handler and fix content' == cluster.instance.http_request('test_get_fixed_static_handler', method='GET', headers={'XXX': 'xxx'}).content
+
+
+def test_config_static_handler():
+    with contextlib.closing(SimpleCluster(ClickHouseCluster(__file__), "static_handler", "test_static_handler")) as cluster:
+        assert 404 == cluster.instance.http_request('', method='GET', headers={'XXX': 'xxx'}).status_code
+
+        assert 404 == cluster.instance.http_request('test_get_config_static_handler', method='GET', headers={'XXX': 'bad'}).status_code
+
+        assert 404 == cluster.instance.http_request('test_get_config_static_handler', method='POST', headers={'XXX': 'xxx'}).status_code
+
+        # check default status code
+        assert 200 == cluster.instance.http_request('test_get_config_static_handler', method='GET', headers={'XXX': 'xxx'}).status_code
+        assert 'text/plain; charset=UTF-8' == cluster.instance.http_request('test_get_config_static_handler', method='GET', headers={'XXX': 'xxx'}).headers['Content-Type']
+        assert 'Test get static handler and config content' == cluster.instance.http_request('test_get_config_static_handler', method='GET', headers={'XXX': 'xxx'}).content
+
+
+def test_absolute_path_static_handler():
+    with contextlib.closing(SimpleCluster(ClickHouseCluster(__file__), "static_handler", "test_static_handler")) as cluster:
+        cluster.instance.exec_in_container(
+            ['bash', '-c', 'echo "<html><body>Absolute Path File</body></html>" > /var/lib/clickhouse/user_files/absolute_path_file.html'],
+            privileged=True, user='root')
+
+        assert 404 == cluster.instance.http_request('', method='GET', headers={'XXX': 'xxx'}).status_code
+
+        assert 404 == cluster.instance.http_request('test_get_absolute_path_static_handler', method='GET', headers={'XXX': 'bad'}).status_code
+
+        assert 404 == cluster.instance.http_request('test_get_absolute_path_static_handler', method='POST', headers={'XXX': 'xxx'}).status_code
+
+        # check default status code
+        assert 200 == cluster.instance.http_request('test_get_absolute_path_static_handler', method='GET', headers={'XXX': 'xxx'}).status_code
+        assert 'text/html; charset=UTF-8' == cluster.instance.http_request('test_get_absolute_path_static_handler', method='GET', headers={'XXX': 'xxx'}).headers['Content-Type']
+        assert '<html><body>Absolute Path File</body></html>\n' == cluster.instance.http_request('test_get_absolute_path_static_handler', method='GET', headers={'XXX': 'xxx'}).content
+
+
+def test_relative_path_static_handler():
+    with contextlib.closing(SimpleCluster(ClickHouseCluster(__file__), "static_handler", "test_static_handler")) as cluster:
+        cluster.instance.exec_in_container(
+            ['bash', '-c', 'echo "<html><body>Relative Path File</body></html>" > /var/lib/clickhouse/user_files/relative_path_file.html'],
+            privileged=True, user='root')
+
+        assert 404 == cluster.instance.http_request('', method='GET', headers={'XXX': 'xxx'}).status_code
+
+        assert 404 == cluster.instance.http_request('test_get_relative_path_static_handler', method='GET', headers={'XXX': 'bad'}).status_code
+
+        assert 404 == cluster.instance.http_request('test_get_relative_path_static_handler', method='POST', headers={'XXX': 'xxx'}).status_code
+
+        # check default status code
+        assert 200 == cluster.instance.http_request('test_get_relative_path_static_handler', method='GET', headers={'XXX': 'xxx'}).status_code
+        assert 'text/html; charset=UTF-8' == cluster.instance.http_request('test_get_relative_path_static_handler', method='GET', headers={'XXX': 'xxx'}).headers['Content-Type']
+        assert '<html><body>Relative Path File</body></html>\n' == cluster.instance.http_request('test_get_relative_path_static_handler', method='GET', headers={'XXX': 'xxx'}).content
+
