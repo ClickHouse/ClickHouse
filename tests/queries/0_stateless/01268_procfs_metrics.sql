c708882e3b3c857a4c6dcf251257c2f8238f4c9c
alexey-milovidov <milovidov@yandex-team.ru>
2020-04-28 23:43:26 +0300

    Update 01268_procfs_metrics.sql

--- tests/queries/0_stateless/01268_procfs_metrics.sql
+++ tests/queries/0_stateless/01268_procfs_metrics.sql
@@ -1,5 +1,5 @@
-# Sandbox does not provide CAP_NET_ADMIN capability but does have ProcFS mounted at /proc
-# This ensures that OS metrics can be collected
+-- Sandbox does not provide CAP_NET_ADMIN capability but does have ProcFS mounted at /proc
+-- This ensures that OS metrics can be collected
 
 select count() > 0 from system.query_log where type = 2 settings log_queries = 1 Format Null;
 select count() > 0 from system.query_log where type = 2 and has(ProfileEvents.Names, 'OSReadChars');
