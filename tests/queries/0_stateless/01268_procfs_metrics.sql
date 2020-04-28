2e11fbcd290678755c5057c5faf1ddbd3db97a1a
alexey-milovidov <milovidov@yandex-team.ru>
2020-04-28 23:44:27 +0300

    Update 01268_procfs_metrics.sql

--- tests/queries/0_stateless/01268_procfs_metrics.sql
+++ tests/queries/0_stateless/01268_procfs_metrics.sql
@@ -2,4 +2,5 @@
 -- This ensures that OS metrics can be collected
 
 select count() > 0 from system.query_log where type = 2 settings log_queries = 1 Format Null;
+SYSTEM FLUSH LOGS;
 select count() > 0 from system.query_log where type = 2 and has(ProfileEvents.Names, 'OSReadChars');
