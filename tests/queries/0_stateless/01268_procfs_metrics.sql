0ce783200ac888049b62e5a1c794d2290df4a327
Alexander Kazakov <Akazz@users.noreply.github.com>
2020-04-29 14:01:03 +0300

    Dropped bad ProcFS test

--- tests/queries/0_stateless/01268_procfs_metrics.sql
+++
@@ -1,6 +0,0 @@
--- Sandbox does not provide CAP_NET_ADMIN capability but does have ProcFS mounted at /proc
--- This ensures that OS metrics can be collected
-
-select count() > 0 from system.query_log where type = 2 settings log_queries = 1 Format Null;
-SYSTEM FLUSH LOGS;
-select count() > 0 from system.query_log where type = 2 and has(ProfileEvents.Names, 'OSReadChars');
