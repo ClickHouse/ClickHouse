c15fe728fdf9a87a82e7ebd09d9b069f8f265e32
Alexey Milovidov <milovidov@yandex-team.ru>
2020-05-01 21:26:52 +0300

    Lower memory usage in tests #10277

--- tests/queries/1_stateful/00154_avro.sql
+++ tests/queries/1_stateful/00154_avro.sql
@@ -1,5 +1,7 @@
 DROP TABLE IF EXISTS test.avro;
 
+SET max_threads = 1, max_block_size = 8192, min_insert_block_size_rows = 8192, min_insert_block_size_bytes = 1048576; -- lower memory usage
+
 CREATE TABLE test.avro AS test.hits ENGINE = File(Avro);
 INSERT INTO test.avro SELECT * FROM test.hits WHERE intHash64(WatchID) % 100 = 0;
 
