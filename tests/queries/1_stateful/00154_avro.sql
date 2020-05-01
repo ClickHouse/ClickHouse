251166e03eb903223fc9e494e720068e016d847d
alexey-milovidov <milovidov@yandex-team.ru>
2020-05-01 23:13:18 +0300

    Update 00154_avro.sql

--- tests/queries/1_stateful/00154_avro.sql
+++ tests/queries/1_stateful/00154_avro.sql
@@ -3,9 +3,9 @@ DROP TABLE IF EXISTS test.avro;
 SET max_threads = 1, max_block_size = 8192, min_insert_block_size_rows = 8192, min_insert_block_size_bytes = 1048576; -- lower memory usage
 
 CREATE TABLE test.avro AS test.hits ENGINE = File(Avro);
-INSERT INTO test.avro SELECT * FROM test.hits WHERE intHash64(WatchID) % 100 = 0;
+INSERT INTO test.avro SELECT * FROM test.hits LIMIT 10000;
 
-SELECT sum(cityHash64(*)) FROM test.hits WHERE intHash64(WatchID) % 100 = 0;
+SELECT sum(cityHash64(*)) FROM (SELECT * FROM test.hits LIMIT 10000);
 SELECT sum(cityHash64(*)) FROM test.avro;
 
 DROP TABLE test.avro;
