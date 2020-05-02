ea7038b1b096cb9679c5135a648087482f04031a
alexey-milovidov <milovidov@yandex-team.ru>
2020-05-02 22:14:20 +0300

    Update 01270_optimize_skip_unused_shards_low_cardinality.sql

--- tests/queries/0_stateless/01270_optimize_skip_unused_shards_low_cardinality.sql
+++ tests/queries/0_stateless/01270_optimize_skip_unused_shards_low_cardinality.sql
@@ -8,3 +8,6 @@ drop table if exists dist_01270;
 create table data_01270 (key LowCardinality(Int)) Engine=Null();
 create table dist_01270 as data_01270 Engine=Distributed(test_cluster_two_shards, currentDatabase(), data_01270, key);
 select * from dist_01270 where key = 1;
+
+drop table data_01270;
+drop table dist_01270;
