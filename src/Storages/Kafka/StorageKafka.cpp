f999c6da0818773b1dbc86516d279f2093f03f63
Azat Khuzhin <a3at.mail@gmail.com>
2020-05-03 01:24:21 +0300

    Force destrying of kafka buffers from the shutdown in StorageKafka

--- src/Storages/Kafka/StorageKafka.cpp
+++ src/Storages/Kafka/StorageKafka.cpp
@@ -170,17 +170,14 @@ void StorageKafka::shutdown()
     // Interrupt streaming thread
     stream_cancelled = true;
 
+    LOG_TRACE(log, "Waiting for cleanup");
+    task->deactivate();
+
     // Close all consumers
     for (size_t i = 0; i < num_created_consumers; ++i)
-    {
         auto buffer = popReadBuffer();
-        // FIXME: not sure if we really close consumers here, and if we really need to close them here.
-    }
 
-    LOG_TRACE(log, "Waiting for cleanup");
     rd_kafka_wait_destroyed(CLEANUP_TIMEOUT_MS);
-
-    task->deactivate();
 }
 
 
