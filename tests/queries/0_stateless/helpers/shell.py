c692d46f983b75c53083ef46462ac79cc5970a9d
alexey-milovidov <milovidov@yandex-team.ru>
2020-05-03 04:46:06 +0300

    Trigger CI

--- tests/queries/0_stateless/helpers/shell.py
+++ tests/queries/0_stateless/helpers/shell.py
@@ -11,7 +11,7 @@ import uexpect
 class shell(object):
     def __init__(self, command=None, name='', log=None, prompt='[#\$] '):
         if command is None:
-            command = ['/bin/bash','--noediting']
+            command = ['/bin/bash', '--noediting']
         self.prompt = prompt
         self.client = uexpect.spawn(command)
         self.client.eol('\r')
