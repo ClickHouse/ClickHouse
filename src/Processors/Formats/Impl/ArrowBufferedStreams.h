2f8435813c4548daa10ca020ca02a05c91614530
FawnD2 <zzosimova@ya.ru>
2020-05-04 17:49:13 +0300

    Remove unused stream

--- src/Processors/Formats/Impl/ArrowBufferedStreams.h
+++ src/Processors/Formats/Impl/ArrowBufferedStreams.h
@@ -10,32 +10,6 @@
 namespace DB
 {
 
-class ArrowBufferedInputStream : public arrow::io::InputStream
-{
-public:
-    explicit ArrowBufferedInputStream(ReadBuffer & in_);
-
-    // FileInterface
-    ::arrow::Status Close() override;
-
-    ::arrow::Status Tell(int64_t * position) const override;
-
-    bool closed() const override { return !is_open; }
-
-    // Readable
-    ::arrow::Status Read(int64_t nbytes, int64_t* bytes_read, void * out) override;
-
-    // Does not copy if not necessary
-    ::arrow::Status Read(int64_t nbytes, std::shared_ptr<::arrow::Buffer> * out) override;
-
-private:
-    ReadBuffer & in;
-    int64_t total_length = 0;
-    bool is_open = false;
-
-    ARROW_DISALLOW_COPY_AND_ASSIGN(ArrowBufferedInputStream);
-};
-
 class ArrowBufferedOutputStream : public arrow::io::OutputStream
 {
 public:
