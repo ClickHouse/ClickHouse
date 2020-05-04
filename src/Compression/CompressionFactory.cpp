6b0bffcdab4c870fbf733f01f4deef7b5ec5b5d1
alexey-milovidov <milovidov@yandex-team.ru>
2020-05-04 03:47:41 +0300

    Update CompressionFactory.cpp

--- src/Compression/CompressionFactory.cpp
+++ src/Compression/CompressionFactory.cpp
@@ -19,6 +19,7 @@ namespace ErrorCodes
 {
     extern const int LOGICAL_ERROR;
     extern const int UNKNOWN_CODEC;
+    extern const int BAD_ARGUMENTS;
     extern const int UNEXPECTED_AST_STRUCTURE;
     extern const int DATA_TYPE_CANNOT_HAVE_ARGUMENTS;
 }
