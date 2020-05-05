d35150ecf340d9b8a8597eb1fce9b924fded2fd6
Alexander Tokmakov <avtokmakov@yandex-team.ru>
2020-05-05 23:32:34 +0300

    fix http readonly request stucks on client close

--- tests/queries/0_stateless/00834_cancel_http_readonly_queries_on_client_close.sh
+++ tests/queries/0_stateless/00834_cancel_http_readonly_queries_on_client_close.sh
@@ -10,3 +10,10 @@ do
     ${CLICKHOUSE_CURL} -sS --data "SELECT count() FROM system.processes WHERE query_id = 'cancel_http_readonly_queries_on_client_close'" "${CLICKHOUSE_URL}" | grep '0' && break
     sleep 0.2
 done
+
+${CLICKHOUSE_CURL} -sS -X POST "${CLICKHOUSE_URL}&session_id=test_00834_session&readonly=2&cancel_http_readonly_queries_on_client_close=1" -d "CREATE TEMPORARY TABLE table_tmp AS SELECT 1 FORMAT JSON"
+${CLICKHOUSE_CURL} -sS "${CLICKHOUSE_URL}&session_id=test_00834_session&query=DROP+TEMPORARY+TABLE+table_tmp"
+
+url_https="https://${CLICKHOUSE_HOST}:${CLICKHOUSE_PORT_HTTPS}/?session_id=test_00834_session"
+${CLICKHOUSE_CURL} -sSk "$url_https&readonly=2&cancel_http_readonly_queries_on_client_close=1" -d "CREATE TEMPORARY TABLE table_tmp AS SELECT 1 FORMAT JSON"
+${CLICKHOUSE_CURL} -sSk "$url_https&session_id=test_00834_session&query=DROP+TEMPORARY+TABLE+table_tmp"
