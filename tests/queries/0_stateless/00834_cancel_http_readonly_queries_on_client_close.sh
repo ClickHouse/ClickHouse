00458c9a7deb997db9fd17cf99d0b371fbfc24ce
Alexander Tokmakov <avtokmakov@yandex-team.ru>
2020-05-06 15:31:50 +0300

    trigger CI

--- tests/queries/0_stateless/00834_cancel_http_readonly_queries_on_client_close.sh
+++ tests/queries/0_stateless/00834_cancel_http_readonly_queries_on_client_close.sh
@@ -15,5 +15,5 @@ ${CLICKHOUSE_CURL} -sS -X POST "${CLICKHOUSE_URL}&session_id=test_00834_session&
 ${CLICKHOUSE_CURL} -sS "${CLICKHOUSE_URL}&session_id=test_00834_session&query=DROP+TEMPORARY+TABLE+table_tmp"
 
 url_https="https://${CLICKHOUSE_HOST}:${CLICKHOUSE_PORT_HTTPS}/?session_id=test_00834_session"
-${CLICKHOUSE_CURL} -sSk "$url_https&readonly=2&cancel_http_readonly_queries_on_client_close=1" -d "CREATE TEMPORARY TABLE table_tmp AS SELECT 1 FORMAT JSON"
+${CLICKHOUSE_CURL} -sSk -X POST "$url_https&readonly=2&cancel_http_readonly_queries_on_client_close=1" -d "CREATE TEMPORARY TABLE table_tmp AS SELECT 1 FORMAT JSON"
 ${CLICKHOUSE_CURL} -sSk "$url_https&session_id=test_00834_session&query=DROP+TEMPORARY+TABLE+table_tmp"
