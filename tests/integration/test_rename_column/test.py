65f139f374fe60676f4a3c62e11d234efd6d0787
Vitaliy Zakaznikov <vzakaznikov@altinity.com>
2020-05-07 21:00:17 +0200

    Adding more integration tests for alter rename column.

--- tests/integration/test_rename_column/test.py
+++ tests/integration/test_rename_column/test.py
@@ -1,6 +1,7 @@
 from __future__ import print_function
 
 import time
+import random
 import pytest
 
 from multiprocessing.dummy import Pool
@@ -9,13 +10,17 @@ from helpers.client import QueryRuntimeException
 from helpers.network import PartitionManager
 from helpers.test_tools import TSV
 
-main_configs=['configs/remote_servers.xml']
+node_options = dict(
+    with_zookeeper=True,
+    main_configs=['configs/remote_servers.xml'],
+    config_dir='configs',
+    tmpfs=['/external:size=200M'])
 
 cluster = ClickHouseCluster(__file__)
-node1 = cluster.add_instance('node1', with_zookeeper=True, macros={"shard": 0, "replica": 1}, main_configs=main_configs )
-node2 = cluster.add_instance('node2', with_zookeeper=True, macros={"shard": 0, "replica": 2}, main_configs=main_configs )
-node3 = cluster.add_instance('node3', with_zookeeper=True, macros={"shard": 1, "replica": 1}, main_configs=main_configs )
-node4 = cluster.add_instance('node4', with_zookeeper=True, macros={"shard": 1, "replica": 2}, main_configs=main_configs )
+node1 = cluster.add_instance('node1', macros={"shard": 0, "replica": 1}, **node_options)
+node2 = cluster.add_instance('node2', macros={"shard": 0, "replica": 2}, **node_options)
+node3 = cluster.add_instance('node3', macros={"shard": 1, "replica": 1}, **node_options)
+node4 = cluster.add_instance('node4', macros={"shard": 1, "replica": 2}, **node_options)
 nodes = [node1, node2, node3, node4]
 
 @pytest.fixture(scope="module")
@@ -33,18 +38,44 @@ def drop_table(nodes, table_name):
         node.query("DROP TABLE IF EXISTS {} NO DELAY".format(table_name))
     time.sleep(1)
 
-def create_table(nodes, table_name):
+def create_table(nodes, table_name, with_storage_policy=False, with_time_column=False,
+        with_ttl_move=False, with_ttl_delete=False):
+    extra_columns = ""
+    settings = []
+
     for node in nodes:
         sql = """
             CREATE TABLE {table_name}
-            (
-                num UInt32,
-                num2 UInt32 DEFAULT num + 1
-            )
-            ENGINE = ReplicatedMergeTree('/clickhouse/tables/test/{table_name}', '{replica}')
-            ORDER BY num PARTITION BY num % 100;
-        """.format(table_name=table_name, replica=node.name)
-        node.query(sql)
+                (
+                    num UInt32,
+                    num2 UInt32 DEFAULT num + 1{extra_columns}
+                )
+                ENGINE = ReplicatedMergeTree('/clickhouse/tables/test/{table_name}', '{replica}')
+                ORDER BY num PARTITION BY num % 100
+            """
+        if with_ttl_move:
+            sql += """
+                TTL time TO DISK 'external'
+            """
+        if with_ttl_delete:
+            sql += """
+                TTL time DELETE
+            """
+            settings.append("merge_with_ttl_timeout = 1")
+
+        if with_storage_policy:
+            settings.append("storage_policy='default_with_external'")
+
+        if settings:
+            sql += """
+                SETTINGS {}
+            """.format(", ".join(settings))
+
+        if with_time_column:
+            extra_columns = """,
+                    time DateTime
+                """
+        node.query(sql.format(table_name=table_name, replica=node.name, extra_columns=extra_columns))
 
 def create_distributed_table(node, table_name):
         sql = """
@@ -68,7 +99,8 @@ def drop_distributed_table(node, table_name):
     node.query("DROP TABLE IF EXISTS {}_replicated ON CLUSTER test_cluster".format(table_name))
     time.sleep(1)
 
-def insert(node, table_name, chunk=1000, col_names=None, iterations=1, ignore_exception=False, slow=False, with_many_parts=False, offset=0):
+def insert(node, table_name, chunk=1000, col_names=None, iterations=1, ignore_exception=False,
+        slow=False, with_many_parts=False, offset=0, with_time_column=False):
     if col_names is None:
         col_names = ['num', 'num2']
     for i in range(iterations):
@@ -76,7 +108,11 @@ def insert(node, table_name, chunk=1000, col_names=None, iterations=1, ignore_ex
             query = ["SET max_partitions_per_insert_block = 10000000"]
             if with_many_parts:
                 query.append("SET max_insert_block_size = 64")
-            if slow:
+            if with_time_column:
+                query.append(
+                    "INSERT INTO {table_name} ({col0}, {col1}, time) SELECT number AS {col0}, number + 1 AS {col1}, now() + 10 AS time FROM numbers_mt({chunk})"
+                .format(table_name=table_name, chunk=chunk, col0=col_names[0], col1=col_names[1]))
+            elif slow:
                 query.append(
                     "INSERT INTO {table_name} ({col0}, {col1}) SELECT number + sleepEachRow(0.001) AS {col0}, number + 1 AS {col1} FROM numbers_mt({chunk})"
                 .format(table_name=table_name, chunk=chunk, col0=col_names[0], col1=col_names[1]))
@@ -123,6 +159,17 @@ def rename_column_on_cluster(node, table_name, name, new_name, iterations=1, ign
             node.query("ALTER TABLE {table_name} ON CLUSTER test_cluster RENAME COLUMN {name} to {new_name}".format(
                 table_name=table_name, name=name, new_name=new_name
             ))
+        except QueryRuntimeException as ex:
+            if not ignore_exception:
+                raise
+
+def alter_move(node, table_name, iterations=1, ignore_exception=False):
+    for i in range(iterations):
+        move_part = random.randint(0, 99)
+        move_volume = 'external'# if random.randint(0, 1) > 0 else 'default'
+        try:
+            node.query("ALTER TABLE {table_name} MOVE PARTITION '{move_part}' TO VOLUME '{move_volume}'"
+                .format(table_name=table_name, move_part=move_part, move_volume=move_volume))
         except QueryRuntimeException as ex:
             if not ignore_exception:
                 raise
@@ -324,6 +371,99 @@ def test_rename_with_parallel_slow_select(started_cluster):
         select(node1, table_name, "num2", "1099\n")
         select(node2, table_name, "num2", "1099\n", poll=30)
         select(node3, table_name, "num2", "1099\n", poll=30)
+    finally:
+        drop_table(nodes, table_name)
+
+def test_rename_with_parallel_moves(started_cluster):
+    table_name = "test_rename_with_parallel_moves"
+    drop_table(nodes, table_name)
+    try:
+        create_table(nodes, table_name, with_storage_policy=True)
+        insert(node1, table_name, 1000)
+
+        p = Pool(15)
+        tasks = []
+
+        tasks.append(p.apply_async(alter_move, (node1, table_name, 100, True)))
+        tasks.append(p.apply_async(alter_move, (node2, table_name, 100, True)))
+        tasks.append(p.apply_async(alter_move, (node3, table_name, 100, True)))
+        tasks.append(p.apply_async(rename_column, (node1, table_name, "num2", "foo2", 100, True)))
+        tasks.append(p.apply_async(rename_column, (node2, table_name, "foo2", "foo3", 100, True)))
+        tasks.append(p.apply_async(rename_column, (node3, table_name, "num3", "num2", 100, True)))
+
+        for task in tasks:
+            task.get(timeout=240)
+
+        # rename column back to original
+        rename_column(node1, table_name, "foo2", "num2", 1, True)
+        rename_column(node1, table_name, "foo3", "num2", 1, True)
+
+        # check that select still works
+        select(node1, table_name, "num2", "999\n")
+        select(node2, table_name, "num2", "999\n", poll=30)
+        select(node3, table_name, "num2", "999\n", poll=30)
+    finally:
+        drop_table(nodes, table_name)
+
+def test_rename_with_parallel_ttl_move(started_cluster):
+    table_name = 'test_rename_with_parallel_ttl_move'
+    try:
+        create_table(nodes, table_name, with_storage_policy=True, with_time_column=True, with_ttl_move=True)
+        rename_column(node1, table_name, "time", "time2", 1, False)
+        rename_column(node1, table_name, "time2", "time", 1, False)
+
+        p = Pool(15)
+        tasks = []
+
+        tasks.append(p.apply_async(insert, (node1, table_name, 10000, ["num", "num2"], 1, False, False, True, 0, True)))
+        time.sleep(5)
+        tasks.append(p.apply_async(rename_column, (node1, table_name, "num2", "foo2", 500, True)))
+        tasks.append(p.apply_async(rename_column, (node2, table_name, "foo2", "foo3", 500, True)))
+        tasks.append(p.apply_async(rename_column, (node3, table_name, "num3", "num2", 500, True)))
+
+        for task in tasks:
+            task.get(timeout=240)
+
+        # rename column back to original
+        rename_column(node1, table_name, "foo2", "num2", 1, True)
+        rename_column(node1, table_name, "foo3", "num2", 1, True)
+
+        # check that select still works
+        select(node1, table_name, "num2", "9990\n")
+    finally:
+        drop_table(nodes, table_name)
+
+def test_rename_with_parallel_ttl_delete(started_cluster):
+    table_name = 'test_rename_with_parallel_ttl_delete'
+    try:
+        create_table(nodes, table_name, with_time_column=True, with_ttl_delete=True)
+        rename_column(node1, table_name, "time", "time2", 1, False)
+        rename_column(node1, table_name, "time2", "time", 1, False)
+
+        def merge_parts(node, table_name, iterations=1):
+            for i in range(iterations):
+                node.query("OPTIMIZE TABLE {}".format(table_name))
+
+        p = Pool(15)
+        tasks = []
+
+        tasks.append(p.apply_async(insert, (node1, table_name, 10000, ["num", "num2"], 1, False, False, True, 0, True)))
+        time.sleep(15)
+        tasks.append(p.apply_async(rename_column, (node1, table_name, "num2", "foo2", 500, True)))
+        tasks.append(p.apply_async(rename_column, (node2, table_name, "foo2", "foo3", 500, True)))
+        tasks.append(p.apply_async(rename_column, (node3, table_name, "num3", "num2", 500, True)))
+        tasks.append(p.apply_async(merge_parts, (node1, table_name, 500)))
+        tasks.append(p.apply_async(merge_parts, (node2, table_name, 500)))
+        tasks.append(p.apply_async(merge_parts, (node3, table_name, 500)))
+
+        for task in tasks:
+            task.get(timeout=240)
+
+        # rename column back to original
+        rename_column(node1, table_name, "foo2", "num2", 1, True)
+        rename_column(node1, table_name, "foo3", "num2", 1, True)
+
+        assert int(node1.query("SELECT count() FROM {}".format(table_name)).strip()) < 9000
     finally:
         drop_table(nodes, table_name)
 
