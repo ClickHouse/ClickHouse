c19eb2c26784f9655dc9b375ab1d70b9df4088ea
Andrey Chulkov <achulkov2@gmail.com>
2020-05-07 18:43:29 +0300

    fix typo

--- dbms/src/Dictionaries/PolygonDictionaryUtils.cpp
+++ dbms/src/Dictionaries/PolygonDictionaryUtils.cpp
@@ -525,7 +525,7 @@ void BucketsSinglePolygonIndex::indexBuild(const Polygon & polygon)
         {
             if (l & 1)
             {
-                this->edges_index_tree[l++].eplace_back(all_edges[i]);
+                this->edges_index_tree[l++].emplace_back(all_edges[i]);
                 ++total_index_edges;
             }
             if (r & 1)
