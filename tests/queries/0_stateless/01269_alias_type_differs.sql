d2c813fa7a7e5a8e3254e0c6bd17a2995e410775
alexey-milovidov <milovidov@yandex-team.ru>
2020-05-11 06:23:58 +0300

    Update 01269_alias_type_differs.sql

--- tests/queries/0_stateless/01269_alias_type_differs.sql
+++ tests/queries/0_stateless/01269_alias_type_differs.sql
@@ -18,3 +18,5 @@ ALTER TABLE data_01269 DROP COLUMN alias;
 ALTER TABLE data_01269 ADD COLUMN alias UInt8 ALIAS value>0;
 SELECT toTypeName(alias) FROM data_01269;
 SELECT any(alias) FROM data_01269;
+
+DROP TABLE data_01269;
