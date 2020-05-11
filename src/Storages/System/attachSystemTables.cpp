6f33cdb1e7ec690d845e3a1ee7065ac1372d541d
Alexey Milovidov <milovidov@yandex-team.ru>
2020-05-11 05:39:01 +0300

    Added "system.licenses" table

--- src/Storages/System/attachSystemTables.cpp
+++ src/Storages/System/attachSystemTables.cpp
@@ -37,6 +37,7 @@
 #include <Storages/System/StorageSystemTables.h>
 #include <Storages/System/StorageSystemZooKeeper.h>
 #include <Storages/System/StorageSystemContributors.h>
+#include <Storages/System/StorageSystemLicenses.h>
 #include <Storages/System/StorageSystemDisks.h>
 #include <Storages/System/StorageSystemStoragePolicies.h>
 #include <Storages/System/StorageSystemZeros.h>
@@ -75,6 +76,7 @@ void attachSystemTablesLocal(IDatabase & system_database)
     system_database.attachTable("collations", StorageSystemCollations::create("collations"));
     system_database.attachTable("table_engines", StorageSystemTableEngines::create("table_engines"));
     system_database.attachTable("contributors", StorageSystemContributors::create("contributors"));
+    system_database.attachTable("licenses", StorageSystemLicenses::create("licenses"));
 #ifdef OS_LINUX
     system_database.attachTable("stack_trace", StorageSystemStackTrace::create("stack_trace"));
 #endif
