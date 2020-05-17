befe1d879ebd98ac2f21c8250f6c803b850948c6
alexey-milovidov <milovidov@yandex-team.ru>
2020-05-17 08:12:33 +0300

    Update DataTypesDecimal.cpp

--- src/DataTypes/DataTypesDecimal.cpp
+++ src/DataTypes/DataTypesDecimal.cpp
@@ -22,9 +22,9 @@ namespace ErrorCodes
 {
     extern const int NUMBER_OF_ARGUMENTS_DOESNT_MATCH;
     extern const int ILLEGAL_TYPE_OF_ARGUMENT;
+    extern const int DECIMAL_OVERFLOW;
 }
 
-//
 
 template <typename T>
 std::string DataTypeDecimal<T>::doGetName() const
