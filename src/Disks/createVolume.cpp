c2ba73cb17975661f69e9aef3dc4530d7fe2459b
alesapin <alesapin@gmail.com>
2020-05-18 11:42:39 +0300

    boop CI

--- src/Disks/createVolume.cpp
+++ src/Disks/createVolume.cpp
@@ -7,8 +7,8 @@ VolumePtr createVolumeFromReservation(const ReservationPtr & reservation, Volume
 {
     if (other_volume->getType() == VolumeType::JBOD || other_volume->getType() == VolumeType::SINGLE_DISK)
     {
-        // Since reservation on JBOD chosies one of disks and makes reservation there, volume
-        // for such type of reservation will be with one disk.
+        /// Since reservation on JBOD chosies one of disks and makes reservation there, volume
+        /// for such type of reservation will be with one disk.
         return std::make_shared<SingleDiskVolume>(other_volume->getName(), reservation->getDisk());
     }
     return nullptr;
