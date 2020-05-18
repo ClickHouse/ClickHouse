b8d447297b198a440ed3edcb3a5327b803ea70d3
Andrey Skobtsov <andrewskobcov@yandex.ru>
2020-05-19 01:26:35 +0300

    Reverted switch case indentation

--- src/Common/ThreadProfileEvents.cpp
+++ src/Common/ThreadProfileEvents.cpp
@@ -55,24 +55,24 @@ TasksStatsCounters::TasksStatsCounters(const UInt64 tid, const MetricsProvider p
 {
     switch (provider)
     {
-        case MetricsProvider::Netlink:
-            stats_getter = [metrics_provider = std::make_shared<TaskStatsInfoGetter>(), tid]()
-            {
-                ::taskstats result;
-                metrics_provider->getStat(result, tid);
-                return result;
-            };
-            break;
-        case MetricsProvider::Procfs:
-            stats_getter = [metrics_provider = std::make_shared<ProcfsMetricsProvider>(tid)]()
-            {
-                ::taskstats result;
-                metrics_provider->getTaskStats(result);
-                return result;
-            };
-            break;
-        case MetricsProvider::None:
-            ;
+    case MetricsProvider::Netlink:
+        stats_getter = [metrics_provider = std::make_shared<TaskStatsInfoGetter>(), tid]()
+        {
+            ::taskstats result;
+            metrics_provider->getStat(result, tid);
+            return result;
+        };
+        break;
+    case MetricsProvider::Procfs:
+        stats_getter = [metrics_provider = std::make_shared<ProcfsMetricsProvider>(tid)]()
+        {
+            ::taskstats result;
+            metrics_provider->getTaskStats(result);
+            return result;
+        };
+        break;
+    case MetricsProvider::None:
+        ;
     }
 }
 
