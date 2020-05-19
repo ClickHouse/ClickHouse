87b30cc9bb4c29a1f09e8d388a0d0a8b00e6cabb
myrrc <me@myrrec.space>
2020-05-19 21:32:19 +0300

    preparing other test

--- src/Common/tests/igrabber_allocator.cpp
+++ src/Common/tests/igrabber_allocator.cpp
@@ -15,17 +15,33 @@ int main() noexcept
 
          printf("All regions: %lu, expected 1\n Used regions: %lu, expected 1\n Unused regions: %lu, expected 0\n",
                  stats.regions, stats.used_regions, stats.unused_regions);
+
+         stats.print(std::cout);
      }
 
-    auto stats = cache.getStats();
+     {
+         auto pair = cache.getOrSet(1, [] {return 100; }, [](void *) { return 1; });
+         auto stats = cache.getStats();
+
+         printf("All regions: %lu, expected 1\n Used regions: %lu, expected 1\n Unused regions: %lu, expected 0\n",
+                 stats.regions, stats.used_regions, stats.unused_regions);
+
+         stats.print(std::cout);
+     }
+
+
+     auto stats = cache.getStats();
 
      printf("All regions: %lu, expected 1\n Used regions: %lu, expected 0\n Unused regions: %lu, expected 1\n",
              stats.regions, stats.used_regions, stats.unused_regions);
+
+     stats.print(std::cout);
 
     cache.shrinkToFit();
 
     cache.getStats().print(std::cout);
 
     std::cout << "Is nullptr:" << (cache.get(0).get() == nullptr) << "\n";
+    std::cout << "Is nullptr:" << (cache.get(1).get() == nullptr) << "\n";
 }
 
