35b916433a12706398b28a710c2c0066895ecf1a
Anton Popov <pad11rus@gmail.com>
2020-05-19 16:00:32 +0300

    fix TTL with defaults after modify

--- src/DataStreams/TTLBlockInputStream.cpp
+++ src/DataStreams/TTLBlockInputStream.cpp
@@ -34,22 +34,25 @@ TTLBlockInputStream::TTLBlockInputStream(
 
     const auto & storage_columns = storage.getColumns();
     const auto & column_defaults = storage_columns.getDefaults();
+
     ASTPtr default_expr_list = std::make_shared<ASTExpressionList>();
+    for (const auto & [name, _] : storage.column_ttl_entries_by_name)
+    {
+        auto it = column_defaults.find(name);
+        if (it != column_defaults.end())
+        {
+            auto column = storage_columns.get(name);
+            auto expression = it->second.expression->clone();
+            default_expr_list->children.emplace_back(setAlias(addTypeConversionToAST(std::move(expression), column.type->getName()), it->first));
+        }
+    }
+
     for (const auto & [name, ttl_info] : old_ttl_infos.columns_ttl)
     {
         if (force || isTTLExpired(ttl_info.min))
         {
             new_ttl_infos.columns_ttl.emplace(name, IMergeTreeDataPart::TTLInfo{});
             empty_columns.emplace(name);
-
-            auto it = column_defaults.find(name);
-
-            if (it != column_defaults.end())
-            {
-                auto column = storage_columns.get(name);
-                auto expression = it->second.expression->clone();
-                default_expr_list->children.emplace_back(setAlias(addTypeConversionToAST(std::move(expression), column.type->getName()), it->first));
-            }
         }
         else
             new_ttl_infos.columns_ttl.emplace(name, ttl_info);
