f13862872770cad328434824d2050ccec0b3ddc4
potya <papotemkin@edu.hse.ru>
2020-05-20 01:18:49 +0300

    Add cast_keep_nullable setting

--- src/Core/Settings.h
+++ src/Core/Settings.h
@@ -428,6 +428,7 @@ struct Settings : public SettingsCollection<Settings>
     M(SettingUInt64, mark_cache_min_lifetime, 0, "Obsolete setting, does nothing. Will be removed after 2020-05-31", 0) \
     M(SettingBool, partial_merge_join, false, "Obsolete. Use join_algorithm='prefer_partial_merge' instead.", 0) \
     M(SettingUInt64, max_memory_usage_for_all_queries, 0, "Obsolete. Will be removed after 2020-10-20", 0) \
+    M(SettingBool, cast_keep_nullable, true, "Cast operator keep Nullable for new data type", 0) \
 
 
     DECLARE_SETTINGS_COLLECTION(LIST_OF_SETTINGS)
