262970f9a79237f756ec965ab40df444c6be5b02
myrrc <me@myrrec.space>
2020-05-19 19:18:10 +0300

    trying to figure out the bug

--- src/Common/Allocators/IGrabberAllocator_fwd.h
+++ src/Common/Allocators/IGrabberAllocator_fwd.h
@@ -46,6 +46,29 @@ struct Stats
     size_t evictions {0};
     size_t evicted_bytes {0};
     size_t secondary_evictions {0};
+
+    template <class Ostream>
+    void print(Ostream& out_stream) const noexcept
+    {
+        out_stream
+            << "total_chunks_size: "                << total_chunks_size << "\n"
+            << "total_allocated_size: "             << total_allocated_size << "\n"
+            << "total_size_currently_initialized: " << total_currently_initialized_size << "\n"
+            << "total_size_in_use: "                << total_currently_used_size << "\n"
+            << "num_chunks: "                       << chunks_count << "\n"
+            << "num_regions: "                      << all_regions_count << "\n"
+            << "num_free_regions: "                 << free_regions_count << "\n"
+            << "num_regions_in_use: "               << used_regions_count << "\n"
+            << "num_keyed_regions: "                << keyed_regions_count << "\n"
+            << "hits: "                             << hits << "\n"
+            << "concurrent_hits: "                  << concurrent_hits << "\n"
+            << "misses: "                           << misses << "\n"
+            << "allocations: "                      << allocations << "\n"
+            << "allocated_bytes: "                  << allocated_bytes << "\n"
+            << "evictions: "                        << evictions << "\n"
+            << "evicted_bytes: "                    << evicted_bytes << "\n"
+            << "secondary_evictions: "              << secondary_evictions << "\n";
+    }
 };
 }
 
