26af5ddf706ac48f39294771a39b0577b495d266
alesapin <alesapin@gmail.com>
2020-05-21 21:53:08 +0300

    Merge pull request #11109 from ClickHouse/less_verbose_logging
    Less verbose logging in mutation finalization task
    
    (cherry picked from commit 19067621190677a0589ae3af84514f9a8cd94356)

--- src/Storages/StorageReplicatedMergeTree.cpp
+++ src/Storages/StorageReplicatedMergeTree.cpp
@@ -2341,7 +2341,6 @@ void StorageReplicatedMergeTree::mergeSelectingTask()
 
 void StorageReplicatedMergeTree::mutationsFinalizingTask()
 {
-    LOG_DEBUG(log, "Trying to finalize mutations");
     bool needs_reschedule = false;
 
     try
