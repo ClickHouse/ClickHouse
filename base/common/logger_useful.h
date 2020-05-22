418b676b3c4bfa64bc40db6b3b880359abef5eae
Alexey Milovidov <milovidov@yandex-team.ru>
2020-05-22 13:58:29 +0300

    fmt-style logging, part 1

--- base/common/logger_useful.h
+++ base/common/logger_useful.h
@@ -3,15 +3,14 @@
 /// Macros for convenient usage of Poco logger.
 
 #include <sstream>
+#include <fmt/format.h>
 #include <Poco/Logger.h>
 #include <Poco/Message.h>
 #include <Poco/Version.h>
 #include <Common/CurrentThread.h>
 
-#ifndef QUERY_PREVIEW_LENGTH
-#define QUERY_PREVIEW_LENGTH 160
-#endif
 
+/// TODO Remove this.
 using Poco::Logger;
 using Poco::Message;
 using DB::LogsLevel;
@@ -19,7 +18,7 @@ using DB::CurrentThread;
 
 /// Logs a message to a specified logger with that level.
 
-#define LOG_SIMPLE(logger, message, priority, PRIORITY) do                        \
+#define LOG_IMPL(logger, priority, PRIORITY, message) do                          \
 {                                                                                 \
     const bool is_clients_log = (CurrentThread::getGroup() != nullptr) &&         \
             (CurrentThread::getGroup()->client_logs_level >= (priority));         \
@@ -41,10 +40,38 @@ using DB::CurrentThread;
 } while (false)
 
 
-#define LOG_TRACE(logger, message)   LOG_SIMPLE(logger, message, LogsLevel::trace, Message::PRIO_TRACE)
-#define LOG_DEBUG(logger, message)   LOG_SIMPLE(logger, message, LogsLevel::debug, Message::PRIO_DEBUG)
-#define LOG_INFO(logger, message)    LOG_SIMPLE(logger, message, LogsLevel::information, Message::PRIO_INFORMATION)
-#define LOG_WARNING(logger, message) LOG_SIMPLE(logger, message, LogsLevel::warning, Message::PRIO_WARNING)
-#define LOG_ERROR(logger, message)   LOG_SIMPLE(logger, message, LogsLevel::error, Message::PRIO_ERROR)
-#define LOG_FATAL(logger, message)   LOG_SIMPLE(logger, message, LogsLevel::error, Message::PRIO_FATAL)
+#define LOG_TRACE(logger, message)   LOG_IMPL(logger, LogsLevel::trace, Message::PRIO_TRACE, message)
+#define LOG_DEBUG(logger, message)   LOG_IMPL(logger, LogsLevel::debug, Message::PRIO_DEBUG, message)
+#define LOG_INFO(logger, message)    LOG_IMPL(logger, LogsLevel::information, Message::PRIO_INFORMATION, message)
+#define LOG_WARNING(logger, message) LOG_IMPL(logger, LogsLevel::warning, Message::PRIO_WARNING, message)
+#define LOG_ERROR(logger, message)   LOG_IMPL(logger, LogsLevel::error, Message::PRIO_ERROR, message)
+#define LOG_FATAL(logger, message)   LOG_IMPL(logger, LogsLevel::error, Message::PRIO_FATAL, message)
 
+
+#define LOG_IMPL_FORMATTED(logger, priority, PRIORITY, message, ...) do         \
+{                                                                                 \
+    const bool is_clients_log = (CurrentThread::getGroup() != nullptr) &&         \
+            (CurrentThread::getGroup()->client_logs_level >= (priority));         \
+    if ((logger)->is((PRIORITY)) || is_clients_log)                               \
+    {                                                                             \
+        std::string formatted_message = fmt::format(message, __VA_ARGS__);        \
+        if (auto channel = (logger)->getChannel())                                \
+        {                                                                         \
+            std::string file_function;                                            \
+            file_function += __FILE__;                                            \
+            file_function += "; ";                                                \
+            file_function += __PRETTY_FUNCTION__;                                 \
+            Message poco_message((logger)->name(), formatted_message,             \
+                                 (PRIORITY), file_function.c_str(), __LINE__);    \
+            channel->log(poco_message);                                           \
+        }                                                                         \
+    }                                                                             \
+} while (false)
+
+
+#define LOG_TRACE_FORMATTED(logger, message, ...)   LOG_IMPL_FORMATTED(logger, LogsLevel::trace, Message::PRIO_TRACE, message, __VA_ARGS__)
+#define LOG_DEBUG_FORMATTED(logger, message, ...)   LOG_IMPL_FORMATTED(logger, LogsLevel::debug, Message::PRIO_DEBUG, message, __VA_ARGS__)
+#define LOG_INFO_FORMATTED(logger, message, ...)    LOG_IMPL_FORMATTED(logger, LogsLevel::information, Message::PRIO_INFORMATION, message, __VA_ARGS__)
+#define LOG_WARNING_FORMATTED(logger, message, ...) LOG_IMPL_FORMATTED(logger, LogsLevel::warning, Message::PRIO_WARNING, message, __VA_ARGS__)
+#define LOG_ERROR_FORMATTED(logger, message, ...)   LOG_IMPL_FORMATTED(logger, LogsLevel::error, Message::PRIO_ERROR, message, __VA_ARGS__)
+#define LOG_FATAL_FORMATTED(logger, message, ...)   LOG_IMPL_FORMATTED(logger, LogsLevel::error, Message::PRIO_FATAL, message, __VA_ARGS__)
