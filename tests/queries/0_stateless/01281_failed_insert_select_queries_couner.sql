d06e7bab7e579f3d11426be7ffbbabd1371dfe8a
Nikita Orlov <nikitao466@gmail.com>
2020-05-23 13:45:27 +0300

    issue-7224 Fix failing tests

--- tests/queries/0_stateless/01281_failed_insert_select_queries_couner.sql
+++ tests/queries/0_stateless/01281_failed_insert_select_queries_couner.sql
@@ -18,7 +18,7 @@ SELECT current_value - previous_value
 FROM (
     SELECT event, value as current_value FROM system.events WHERE event like 'FailedInsertQuery'
 ) AS previous
-ALL JOIN (
+ALL LEFT JOIN (
     SELECT event, value as previous_value FROM current_failed_query_metrics
 ) AS current
 on previous.event = current.event;
@@ -31,7 +31,7 @@ SELECT current_value - previous_value
 FROM (
     SELECT event, value as current_value FROM system.events WHERE event like 'FailedInsertQuery'
 ) AS previous
-ALL JOIN (
+ALL LEFT JOIN (
     SELECT event, value as previous_value FROM current_failed_query_metrics
 ) AS current
 on previous.event = current.event;
@@ -44,7 +44,7 @@ SELECT current_value - previous_value
 FROM (
     SELECT event, value as current_value FROM system.events WHERE event like 'FailedSelectQuery'
 ) AS previous
-ALL JOIN (
+ALL LEFT  JOIN (
     SELECT event, value as previous_value FROM current_failed_query_metrics
 ) AS current
 on previous.event = current.event;
@@ -56,7 +56,7 @@ SELECT current_value - previous_value
 FROM (
     SELECT event, value as current_value FROM system.events WHERE event like 'FailedSelectQuery'
 ) AS previous
-ALL JOIN (
+ALL LEFT JOIN (
     SELECT event, value as previous_value FROM current_failed_query_metrics
 ) AS current
 on previous.event = current.event;
