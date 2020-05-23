149ba72210cdbc5d83b39478e88fe34794331824
Nikita Vasilev <vasnikserg@yandex.ru>
2020-05-23 17:16:52 +0300

    fix

--- src/Dictionaries/SSDComplexKeyCacheDictionary.cpp
+++ src/Dictionaries/SSDComplexKeyCacheDictionary.cpp
@@ -1733,7 +1733,6 @@ void registerDictionarySSDComplexKeyCache(DictionaryFactory & factory)
         const auto max_partitions_count = config.getInt(layout_prefix + ".complex_key_ssd_cache.max_partitions_count", DEFAULT_PARTITIONS_COUNT);
         if (max_partitions_count <= 0)
             throw Exception{name + ": dictionary of layout 'complex_key_ssd_cache' cannot have 0 (or less) max_partitions_count", ErrorCodes::BAD_ARGUMENTS};
-        max_partitions_count = 1;
 
         const auto block_size = config.getInt(layout_prefix + ".complex_key_ssd_cache.block_size", DEFAULT_SSD_BLOCK_SIZE);
         if (block_size <= 0)
