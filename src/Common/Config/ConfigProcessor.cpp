c11dad1ee4ecf05852bfc8785c28847c3f8641b2
Alexey Milovidov <milovidov@yandex-team.ru>
2020-05-23 22:05:41 +0300

    Make all LOG in single line (preparation)

--- src/Common/Config/ConfigProcessor.cpp
+++ src/Common/Config/ConfigProcessor.cpp
@@ -568,10 +568,7 @@ ConfigProcessor::LoadedConfig ConfigProcessor::loadConfigWithZooKeeperIncludes(
         if (!zk_exception)
             throw;
 
-        LOG_WARNING(
-                log,
-                "Error while processing from_zk config includes: " + zk_exception->message() +
-                ". Config will be loaded from preprocessed file: " + preprocessed_path);
+        LOG_WARNING(                 log,                 "Error while processing from_zk config includes: " + zk_exception->message() +                 ". Config will be loaded from preprocessed file: " + preprocessed_path);
 
         config_xml = dom_parser.parse(preprocessed_path);
     }
