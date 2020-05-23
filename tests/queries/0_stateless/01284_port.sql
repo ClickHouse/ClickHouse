60a7550647df60e7339f56425bb5a7ad31e40d20
Azat Khuzhin <a3at.mail@gmail.com>
2020-05-23 21:14:52 +0300

    Add optional default_port argument for the port() function

--- tests/queries/0_stateless/01284_port.sql
+++ tests/queries/0_stateless/01284_port.sql
@@ -11,6 +11,15 @@ select port('http://foobar.com:80/');
 select port('//foobar.com:80/');
 select port('foobar.com:80');
 
+select 'default-port';
+select port('http://127.0.0.1/', toUInt16(80));
+select port('http://foobar.com/', toUInt16(80));
+
+-- unsupported
+/* ILLEGAL_TYPE_OF_ARGUMENT */ select port(toFixedString('', 1)); -- { serverError 43; }
+/* ILLEGAL_TYPE_OF_ARGUMENT */ select port('', 1); -- { serverError 43; }
+/* NUMBER_OF_ARGUMENTS_DOESNT_MATCH */ select port('', 1, 1); -- { serverError 42; }
+
 --
 -- Known limitations of domain() (getURLHost())
 --
