108e3f2a9e2dd190de6689eaf5e25bc5c3314856
Alexey Milovidov <milovidov@yandex-team.ru>
2020-05-24 13:42:13 +0300

    Fix test (looks like it was incorrect)

--- src/IO/tests/gtest_s3_uri.cpp
+++ src/IO/tests/gtest_s3_uri.cpp
@@ -17,7 +17,7 @@ TEST(S3UriTest, validPatterns)
 {
     {
         S3::URI uri(Poco::URI("https://jokserfn.s3.yandexcloud.net/data"));
-        ASSERT_EQ("https://jokserfn.s3.yandexcloud.net", uri.endpoint);
+        ASSERT_EQ("https://s3.yandexcloud.net", uri.endpoint);
         ASSERT_EQ("jokserfn", uri.bucket);
         ASSERT_EQ("data", uri.key);
     }
