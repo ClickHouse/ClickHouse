0d1577c5da6f54e76d447c457803800ac01b4baf
Dmitrii Kovalkov <dimas.kovas@gmail.com>
2020-05-24 14:25:07 +0200

    Better avx2 implementation for rand(). Expected to be ~10% faster

--- src/Functions/FunctionsRandom.cpp
+++ src/Functions/FunctionsRandom.cpp
@@ -9,8 +9,6 @@
 namespace DB
 {
 
-DECLARE_MULTITARGET_CODE(
-
 namespace
 {
     /// NOTE Probably
@@ -50,7 +48,22 @@ namespace
         generator.seed(calcSeed(rand_seed, additional_seed));
     }
 
+    /// The array of random numbers from 'head -c8 /dev/urandom | xxd -p'.
+    /// Can be used for creating seeds for random generators.
+    constexpr std::array<UInt64, 32> random_numbers = {
+        0x0c8ff307dabc0c4cULL, 0xf4bce78bf3821c1bULL, 0x4eb628a1e189c21aULL, 0x85ae000d253e0dbcULL,
+        0xc98073e6480f8a10ULL, 0xb17e9b70a084d570ULL, 0x1361c752b768da8cULL, 0x3d915f60c06d144dULL,
+        0xd5bc9b7aced79587ULL, 0x66c28000ba8a66cfULL, 0x0fb58da7a48820f5ULL, 0x540ee1b57aa861a1ULL,
+        0x212f11936ef2db04ULL, 0xa3939cd900edcc58ULL, 0xc676c84420170102ULL, 0xcbdc824e8b4bf3edULL,
+
+        0x8296f9d93cc94e3bULL, 0x78a7e826d62085b2ULL, 0xaa30620211fc6c69ULL, 0xbd38de52f0a93677ULL,
+        0x19983de8d79dcc4eULL, 0x8afe883ef2199e6fULL, 0xb7160f7ed022b60aULL, 0x2ce173d373ddafd4ULL,
+        0x15762761bb55b9acULL, 0x3e448fc94fdd28e7ULL, 0xa5121232adfbe70aULL, 0xb1e0f6d286112804ULL,
+        0x6062e96de9554806ULL, 0xcc679b329c28882aULL, 0x5c6d29f45cbc060eULL, 0x1af1325a86ffb162ULL,
+    };
 }
+
+DECLARE_DEFAULT_CODE(
 
 void RandImpl::execute(char * output, size_t size)
 {
@@ -61,10 +74,10 @@ void RandImpl::execute(char * output, size_t size)
 
     UInt64 rand_seed = randomSeed();
 
-    seed(generator0, rand_seed, 0xfb4121280b2ab902ULL + reinterpret_cast<intptr_t>(output));
-    seed(generator1, rand_seed, 0x0121cf76df39c673ULL + reinterpret_cast<intptr_t>(output));
-    seed(generator2, rand_seed, 0x17ae86e3a19a602fULL + reinterpret_cast<intptr_t>(output));
-    seed(generator3, rand_seed, 0x8b6e16da7e06d622ULL + reinterpret_cast<intptr_t>(output));
+    seed(generator0, rand_seed, random_numbers[0] + reinterpret_cast<intptr_t>(output));
+    seed(generator1, rand_seed, random_numbers[1] + reinterpret_cast<intptr_t>(output));
+    seed(generator2, rand_seed, random_numbers[2] + reinterpret_cast<intptr_t>(output));
+    seed(generator3, rand_seed, random_numbers[3] + reinterpret_cast<intptr_t>(output));
 
     for (const char * end = output + size; output < end; output += 16)
     {
@@ -76,43 +89,40 @@ void RandImpl::execute(char * output, size_t size)
     /// It is guaranteed (by PaddedPODArray) that we can overwrite up to 15 bytes after end.
 }
 
-namespace
-{
+) // DECLARE_DEFAULT_CODE
 
-// The array of random numbers from 'head -c8 /dev/urandom | xxd -p'.
-// Can be used for creating seeds for random generators.
-constexpr std::array<UInt64, 32> random_numbers = {
-    0x0c8ff307dabc0c4cULL, 0xf4bce78bf3821c1bULL, 0x4eb628a1e189c21aULL, 0x85ae000d253e0dbcULL,
-    0xc98073e6480f8a10ULL, 0xb17e9b70a084d570ULL, 0x1361c752b768da8cULL, 0x3d915f60c06d144dULL,
-    0xd5bc9b7aced79587ULL, 0x66c28000ba8a66cfULL, 0x0fb58da7a48820f5ULL, 0x540ee1b57aa861a1ULL,
-    0x212f11936ef2db04ULL, 0xa3939cd900edcc58ULL, 0xc676c84420170102ULL, 0xcbdc824e8b4bf3edULL,
-
-    0x8296f9d93cc94e3bULL, 0x78a7e826d62085b2ULL, 0xaa30620211fc6c69ULL, 0xbd38de52f0a93677ULL,
-    0x19983de8d79dcc4eULL, 0x8afe883ef2199e6fULL, 0xb7160f7ed022b60aULL, 0x2ce173d373ddafd4ULL,
-    0x15762761bb55b9acULL, 0x3e448fc94fdd28e7ULL, 0xa5121232adfbe70aULL, 0xb1e0f6d286112804ULL,
-    0x6062e96de9554806ULL, 0xcc679b329c28882aULL, 0x5c6d29f45cbc060eULL, 0x1af1325a86ffb162ULL,
-};
-
-};
+DECLARE_AVX2_SPECIFIC_CODE(
 
 using namespace VectorExtension;
 
-template <int VecSize>
-void RandVecImpl<VecSize>::execute(char * output, size_t size)
+/* Takes 2 vectors with LinearCongruentialGenerator states and combines them into vector with random values.
+ * From every rand-state we use only bits 15...47 to generate random vector.
+ */
+inline UInt64x4 CombineValues(UInt64x4 a, UInt64x4 b)
 {
-    static_assert(VecSize >= 4);
-    static_assert(4 * VecSize <= random_numbers.size());
+    auto xa = reinterpret_cast<__m256i>(a);
+    auto xb = reinterpret_cast<__m256i>(b);
+    /// Every state is 8-byte value and we need to use only 4 from the middle.
+    /// Swap the low half and the high half of every state to move these bytes from the middle to sides.
+    /// xa = xa[1, 0, 3, 2, 5, 4, 7, 6]
+    xa = _mm256_shuffle_epi32(xa, 0xb1);
+    /// Now every 8-byte value in xa is xx....xx and every value in xb is ..xxxx.. where x is random byte we want to use.
+    /// Just blend them to get the result vector.
+    /// res = xa[0],xb[1,2],xa[3,4],xb[5,6],xa[7,8],xb[9,10],xa[11,12],xb[13,14],xa[15]
+    auto res = _mm256_blend_epi16(xa, xb, 0x66); 
+    return reinterpret_cast<UInt64x4>(res);
+}
 
-    using VecUInt64 = UInt64x<VecSize>;
-    using VecUInt32 = UInt32x<VecSize>;
-
+void RandImpl::execute(char * output, size_t size)
+{
     if (size == 0)
         return;
 
     char * end = output + size;
 
+    constexpr int vec_size = 4;
     constexpr int safe_overwrite = 15;
-    constexpr int bytes_per_write = 4 * sizeof(VecUInt32);
+    constexpr int bytes_per_write = 4 * sizeof(UInt64x4);
 
     UInt64 rand_seed = randomSeed();
 
@@ -126,56 +136,49 @@ void RandVecImpl<VecSize>::execute(char * output, size_t size)
 
     constexpr UInt64 c = LinearCongruentialGenerator::c;
 
-    VecUInt64 gens1{};
-    VecUInt64 gens2{};
-    VecUInt64 gens3{};
-    VecUInt64 gens4{};
-    for (int i = 0; i < VecSize; ++i)
+    UInt64x4 gens1{};
+    UInt64x4 gens2{};
+    UInt64x4 gens3{};
+    UInt64x4 gens4{};
+    for (int i = 0; i < vec_size; ++i)
     {
         gens1[i] = calcSeed(rand_seed, random_numbers[i] + reinterpret_cast<intptr_t>(output));
-        gens2[i] = calcSeed(rand_seed, random_numbers[i + VecSize] + reinterpret_cast<intptr_t>(output));
-        gens3[i] = calcSeed(rand_seed, random_numbers[i + 2 * VecSize] + reinterpret_cast<intptr_t>(output));
-        gens4[i] = calcSeed(rand_seed, random_numbers[i + 3 * VecSize] + reinterpret_cast<intptr_t>(output));
+        gens2[i] = calcSeed(rand_seed, random_numbers[i + vec_size] + reinterpret_cast<intptr_t>(output));
+        gens3[i] = calcSeed(rand_seed, random_numbers[i + 2 * vec_size] + reinterpret_cast<intptr_t>(output));
+        gens4[i] = calcSeed(rand_seed, random_numbers[i + 3 * vec_size] + reinterpret_cast<intptr_t>(output));
     }
 
     while ((end - output) + safe_overwrite >= bytes_per_write)
     {
         gens1 = gens1 * a + c;
-        VecUInt32 values1 = __builtin_convertvector(gens1 >> 16, VecUInt32);
-        unalignedStore<VecUInt32>(output, values1);
         gens2 = gens2 * a + c;
-        VecUInt32 values2 = __builtin_convertvector(gens2 >> 16, VecUInt32);
-        unalignedStore<VecUInt32>(output + sizeof(VecUInt32), values2);
+        unalignedStore<UInt64x4>(output, CombineValues(gens1, gens2));
         gens3 = gens3 * a + c;
-        VecUInt32 values3 = __builtin_convertvector(gens3 >> 16, VecUInt32);
-        unalignedStore<VecUInt32>(output + 2 * sizeof(VecUInt32), values3);
         gens4 = gens4 * a + c;
-        VecUInt32 values4 = __builtin_convertvector(gens4 >> 16, VecUInt32);
-        unalignedStore<VecUInt32>(output + 3 * sizeof(VecUInt32), values4);
+        unalignedStore<UInt64x4>(output + sizeof(UInt64x4), CombineValues(gens3, gens4));
+        gens1 = gens1 * a + c;
+        gens2 = gens2 * a + c;
+        unalignedStore<UInt64x4>(output + 2 * sizeof(UInt64x4), CombineValues(gens1, gens2));
+        gens3 = gens3 * a + c;
+        gens4 = gens4 * a + c;
+        unalignedStore<UInt64x4>(output + 3 * sizeof(UInt64x4), CombineValues(gens3, gens4));
         output += bytes_per_write;
     }
 
     // Process tail
     while ((end - output) > 0)
     {
-        gens1 = gens1 * a + c;;
-        VecUInt32 values = __builtin_convertvector(gens1 >> 16, VecUInt32);
-        for (int i = 0; i < VecSize && (end - output) > 0; ++i)
+        gens1 = gens1 * a + c;
+        gens2 = gens2 * a + c;
+        UInt64x4 values = CombineValues(gens1, gens2);
+        for (int i = 0; i < vec_size && (end - output) > 0; ++i)
         {
-            unalignedStore<UInt32>(output, values[i]);
-            output += sizeof(UInt32);
+            unalignedStore<UInt64>(output, values[i]);
+            output += sizeof(UInt64);
         }
     }
 }
 
-) // DECLARE_MULTITARGET_CODE
-
-DECLARE_AVX2_SPECIFIC_CODE(
-    template struct RandVecImpl<4>;
 ) // DECLARE_AVX2_SPECIFIC_CODE
-
-DECLARE_AVX512F_SPECIFIC_CODE(
-    template struct RandVecImpl<8>;
-) // DECLARE_AVX512F_SPECIFIC_CODE
 
 }
