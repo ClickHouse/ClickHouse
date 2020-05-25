512687f1a3fd9708c44a8d5d2d11c7bf7e33a8d1
Nikita Mikhaylov <mikhaylovnikitka@gmail.com>
2020-05-25 19:55:55 +0300

    return back virtual columns

--- src/Storages/Kafka/StorageKafka.cpp
+++ src/Storages/Kafka/StorageKafka.cpp
@@ -123,7 +123,12 @@ StorageKafka::StorageKafka(
     UInt64 max_block_size_,
     size_t skip_broken_,
     bool intermediate_commit_)
-    : IStorage(table_id_)
+    : IStorage(table_id_,
+    ColumnsDescription({{"_topic", std::make_shared<DataTypeString>()},
+                        {"_key", std::make_shared<DataTypeString>()},
+                        {"_offset", std::make_shared<DataTypeUInt64>()},
+                        {"_partition", std::make_shared<DataTypeUInt64>()},
+                        {"_timestamp", std::make_shared<DataTypeNullable>(std::make_shared<DataTypeDateTime>())}}, true))
     , global_context(context_.getGlobalContext())
     , kafka_context(Context(global_context))
     , topics(global_context.getMacros()->expand(topics_))
