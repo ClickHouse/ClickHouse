2974d81b2ecb947d81973c65da9f3c9b46216087
potya <papotemkin@edu.hse.ru>
2020-05-25 21:58:30 +0300

    Fix erros and add test

--- tests/queries/0_stateless/01269_create_with_null.sql
+++ tests/queries/0_stateless/01269_create_with_null.sql
@@ -1,11 +1,42 @@
 DROP TABLE IF EXISTS data_null;
+DROP TABLE IF EXISTS set_null;
 
 CREATE TABLE data_null (
     a INT NULL,
     b INT NOT NULL,
-    C Nullable(INT)
-);
+    c Nullable(INT),
+    d INT
+) engine=Memory();
 
-INSERT INTO data_null VALUES (1, 2, 3);
 
-SELECT toTypeName(*) FROM data_null;
+INSERT INTO data_null VALUES (1, 2, 3, 4);
+
+SELECT toTypeName(a), toTypeName(b), toTypeName(c), toTypeName(d) FROM data_null;
+
+
+CREATE TABLE data_null (
+    a Nullable(INT) NULL,
+    b INT NOT NULL,
+    c Nullable(INT)
+) engine=Memory();  --{serverError 377}
+
+
+CREATE TABLE data_null (
+    a INT NULL,
+    b Nullable(INT) NOT NULL,
+    c Nullable(INT)
+) engine=Memory();  --{serverError 377}
+
+SET data_type_default_nullable='true';
+
+CREATE TABLE set_null (
+    a INT NULL,
+    b INT NOT NULL,
+    c Nullable(INT),
+    d INT
+) engine=Memory();
+
+
+INSERT INTO set_null VALUES (1, 2, 3, 4);
+
+SELECT toTypeName(a), toTypeName(b), toTypeName(c), toTypeName(d) FROM set_null;
