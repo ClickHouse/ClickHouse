18656841a3204bf5d717aca4db54add5c30459ee
alesapin <alesapin@gmail.com>
2020-05-27 16:34:23 +0300

    Implement methods for index struct

--- src/Storages/StorageInMemoryMetadata.h
+++ src/Storages/StorageInMemoryMetadata.h
@@ -142,4 +142,15 @@ struct StorageMetadataSkipIndexField
     static StorageMetadataSkipIndexField getSkipIndexFromAST(const ASTPtr & definition_ast, const ColumnsDescription & columns, const Context & context);
 };
 
+struct StorageMetadataSkipIndices
+{
+    std::vector<StorageMetadataSkipIndexField> indices;
+
+    bool empty() const;
+    bool has(const String & name) const;
+    String toString() const;
+
+    static StorageMetadataSkipIndices parse(const String & str, const ColumnsDescription & columns, const Context & context);
+};
+
 }
