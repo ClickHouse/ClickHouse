4abf42c44eccaa1a6e51d2d0e638e6eb52e7d4e1
Gleb Novikov <glebnovikov98@gmail.com>
2020-05-28 08:38:55 +0300

    Generic volume is coming...
    1. SingleDiskVolume for temporary volumes
    2. Generic VolumePtr in StoragePolicies
    3. Removed max_data_part_size in system.storage_policies, added volume_type

--- src/Server/HTTPHandler.cpp
+++ src/Server/HTTPHandler.cpp
@@ -366,7 +366,7 @@ void HTTPHandler::processQuery(
 
         if (buffer_until_eof)
         {
-            const std::string tmp_path(context.getTemporaryVolume()->getNextDisk()->getPath());
+            const std::string tmp_path(context.getTemporaryVolume()->getDisk()->getPath());
             const std::string tmp_path_template(tmp_path + "http_buffers/");
 
             auto create_tmp_disk_buffer = [tmp_path_template] (const WriteBufferPtr &)
