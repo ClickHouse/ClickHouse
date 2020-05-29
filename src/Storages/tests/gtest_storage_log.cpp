a1296041c6a8128f1494a717114e9883deaa5a43
alesapin <alesapin@gmail.com>
2020-05-29 11:18:46 +0300

    Merge pull request #11262 from ClickHouse/fix_unit_broken_tests
    Fix unit broken tests
    
    (cherry picked from commit 9a34310ead9df490212d2b3d9253c2d79f133e6e)

--- src/Storages/tests/gtest_storage_log.cpp
+++ src/Storages/tests/gtest_storage_log.cpp
@@ -68,7 +68,7 @@ using DiskImplementations = testing::Types<DB::DiskMemory, DB::DiskLocal>;
 TYPED_TEST_SUITE(StorageLogTest, DiskImplementations);
 
 // Returns data written to table in Values format.
-std::string writeData(int rows, DB::StoragePtr & table, DB::Context & context)
+std::string writeData(int rows, DB::StoragePtr & table, const DB::Context & context)
 {
     using namespace DB;
 
@@ -103,7 +103,7 @@ std::string writeData(int rows, DB::StoragePtr & table, DB::Context & context)
 }
 
 // Returns all table data in Values format.
-std::string readData(DB::StoragePtr & table, DB::Context & context)
+std::string readData(DB::StoragePtr & table, const DB::Context & context)
 {
     using namespace DB;
 
@@ -135,7 +135,7 @@ std::string readData(DB::StoragePtr & table, DB::Context & context)
 TYPED_TEST(StorageLogTest, testReadWrite)
 {
     using namespace DB;
-    auto context_holder = getContext();
+    const auto & context_holder = getContext();
 
     std::string data;
 
