50e371e2b4b82ff8fb69dd608cfc436211e238e0
Andrei Nekrashevich <axolm@sandbox-407923068>
2020-05-29 05:06:21 +0300

    fix issues

--- tests/queries/0_stateless/01284_fuzz_bits.sql
+++ tests/queries/0_stateless/01284_fuzz_bits.sql
@@ -1,4 +1,36 @@
-SELECT fuzzBits(toString('string'), 1); -- { serverError 43 }
-SELECT fuzzBits('', 0.3); -- { serverError 44 }
-SELECT length(fuzzBits(randomString(100), 0.5));
-SELECT toTypeName(fuzzBits(randomString(100), 0.5));
+SELECT
+  fuzzBits(toString('string'), 1);
+-- { serverError 43 }
+SELECT
+  fuzzBits('', 0.3);
+SELECT
+  length(fuzzBits(randomString(100), 0.5));
+SELECT
+  toTypeName(fuzzBits(randomString(100), 0.5));
+SELECT
+  toTypeName(fuzzBits(toFixedString('abacaba', 10), 0.9));
+
+SELECT
+  (
+    (0.3 * 0.99) * 8 * 10000 < sum
+    AND sum < (0.3 * 1.01) * 8 * 10000
+  ) AS res
+FROM
+  (
+    SELECT
+      arraySum(
+        id -> bitCount(
+          reinterpretAsUInt8(
+            substring(
+              fuzzBits(
+                arrayStringConcat(arrayMap(x -> toString('\0'), range(10000))),
+                0.3
+              ),
+              id + 1,
+              1
+            )
+          )
+        ),
+        range(10000)
+      ) as sum
+  )
