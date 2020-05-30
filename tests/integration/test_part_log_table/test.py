9b4d6ac81886d45cc51e94655617a228d17230dc
Alexey Milovidov <milovidov@yandex-team.ru>
2020-05-30 21:42:19 +0300

    Revert test

--- tests/integration/test_part_log_table/test.py
+++ tests/integration/test_part_log_table/test.py
@@ -21,21 +21,22 @@ def test_config_without_part_log(start_cluster):
     node1.query("CREATE TABLE test_table(word String, value UInt64) ENGINE=MergeTree() ORDER BY value")
     assert "Table system.part_log doesn't exist" in node1.query_and_get_error("SELECT * FROM system.part_log")
     node1.query("INSERT INTO test_table VALUES ('name', 1)")
-    node1.query("SYSTEM FLUSH LOGS")
+    time.sleep(10)
     assert "Table system.part_log doesn't exist" in node1.query_and_get_error("SELECT * FROM system.part_log")
 
 def test_config_with_standard_part_log(start_cluster):
-    assert node2.query("SELECT * FROM system.part_log") == ''
+    assert "Table system.part_log doesn't exist" in node2.query_and_get_error("SELECT * FROM system.part_log")
     node2.query("CREATE TABLE test_table(word String, value UInt64) ENGINE=MergeTree() Order by value")
-    assert node2.query("SELECT * FROM system.part_log") == ''
+    assert "Table system.part_log doesn't exist" in node2.query_and_get_error("SELECT * FROM system.part_log")
     node2.query("INSERT INTO test_table VALUES ('name', 1)")
-    node2.query("SYSTEM FLUSH LOGS")
-    assert int(node2.query("SELECT count() FROM system.part_log")) == 1
+    time.sleep(10)
+    assert node2.query("SELECT * FROM system.part_log") != ""
 
 def test_config_with_non_standard_part_log(start_cluster):
-    assert node3.query("SELECT * FROM system.own_part_log") == ''
+    assert "Table system.table_name doesn't exist" in node3.query_and_get_error("SELECT * FROM system.table_name")
     node3.query("CREATE TABLE test_table(word String, value UInt64) ENGINE=MergeTree() Order by value")
-    assert node3.query("SELECT * FROM system.own_part_log") == ''
+    assert "Table system.table_name doesn't exist" in node3.query_and_get_error("SELECT * FROM system.table_name")
     node3.query("INSERT INTO test_table VALUES ('name', 1)")
-    node3.query("SYSTEM FLUSH LOGS")
-    assert int(node3.query("SELECT count() FROM system.own_part_log")) == 1
+    time.sleep(10)
+    assert node3.query("SELECT * FROM system.table_name") != ""
+
