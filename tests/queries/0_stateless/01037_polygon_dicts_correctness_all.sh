54a746e24d1350497a42dee24abe146dcb274e1e
Arthur Petukhovsky <petuhovskiy@yandex.ru>
2020-05-31 21:47:54 +0300

    Make less calls to clickhouse-client in tests
    By @achulkov2

--- tests/queries/0_stateless/01037_polygon_dicts_correctness_all.sh
+++ tests/queries/0_stateless/01037_polygon_dicts_correctness_all.sh
@@ -9,21 +9,20 @@ declare -a SearchTypes=("POLYGON" "POLYGON_INDEX_EACH" "POLYGON_INDEX_CELL")
 
 tar -xf ${CURDIR}/01037_test_data_search.tar.gz -C ${CURDIR}
 
-$CLICKHOUSE_CLIENT --query="DROP DATABASE IF EXISTS test_01037;"
-
-$CLICKHOUSE_CLIENT --query="CREATE DATABASE test_01037 Engine = Ordinary;"
-
-$CLICKHOUSE_CLIENT --query="DROP TABLE IF EXISTS test_01037.points;"
-
-$CLICKHOUSE_CLIENT --query="CREATE TABLE test_01037.points (x Float64, y Float64) ENGINE = Memory;"
+$CLICKHOUSE_CLIENT -n --query="
+DROP DATABASE IF EXISTS test_01037;
+CREATE DATABASE test_01037 Engine = Ordinary;
+DROP TABLE IF EXISTS test_01037.points;
+CREATE TABLE test_01037.points (x Float64, y Float64) ENGINE = Memory;
+"
 
 $CLICKHOUSE_CLIENT --query="INSERT INTO test_01037.points FORMAT TSV" --max_insert_block_size=100000 < "${CURDIR}/01037_point_data"
 
 rm ${CURDIR}/01037_point_data
 
-$CLICKHOUSE_CLIENT --query="DROP TABLE IF EXISTS test_01037.polygons_array;"
+$CLICKHOUSE_CLIENT -n --query="
+DROP TABLE IF EXISTS test_01037.polygons_array;
 
-$CLICKHOUSE_CLIENT --query="
 CREATE TABLE test_01037.polygons_array
 (
    key Array(Array(Array(Array(Float64)))),
@@ -37,11 +36,13 @@ $CLICKHOUSE_CLIENT --query="INSERT INTO test_01037.polygons_array FORMAT JSONEac
 
 rm ${CURDIR}/01037_polygon_data
 
-$CLICKHOUSE_CLIENT --query="DROP DICTIONARY IF EXISTS test_01037.dict_array;"
-
 for type in ${SearchTypes[@]};
-do 
+do
+   outputFile="${TMP_DIR}/results${type}.out"
+
    $CLICKHOUSE_CLIENT -n --query="
+   DROP DICTIONARY IF EXISTS test_01037.dict_array;
+
    CREATE DICTIONARY test_01037.dict_array
    (
    key Array(Array(Array(Array(Float64)))),
@@ -51,19 +52,16 @@ do
    PRIMARY KEY key
    SOURCE(CLICKHOUSE(HOST 'localhost' PORT 9000 USER 'default' TABLE 'polygons_array' PASSWORD '' DB 'test_01037'))
    LIFETIME(0)
-   LAYOUT($type());"
+   LAYOUT($type());
 
-   outputFile="${TMP_DIR}/results${type}.out"
-
-   $CLICKHOUSE_CLIENT -n --query="
    select 'dictGet', 'test_01037.dict_array' as dict_name, tuple(x, y) as key,
       dictGet(dict_name, 'value', key) from test_01037.points order by x, y;
    " > $outputFile
 
    diff -q "${CURDIR}/01037_polygon_dicts_correctness_all.ans" "$outputFile"
-
-   $CLICKHOUSE_CLIENT --query="DROP DICTIONARY test_01037.dict_array;"
 done
 
-$CLICKHOUSE_CLIENT --query="DROP TABLE test_01037.points;"
-$CLICKHOUSE_CLIENT --query="DROP DATABASE test_01037;"
+$CLICKHOUSE_CLIENT -n --query="
+DROP TABLE test_01037.points;
+DROP DATABASE test_01037;
+"
