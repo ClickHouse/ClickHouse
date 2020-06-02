263f627721743f2f13add5363f3b0e503677d8ca
Nikolai Kochetov <nik-kochetov@yandex-team.ru>
2020-06-02 18:59:57 +0300

    Refactor RemoteBlockInputStream.

---
+++ src/DataStreams/RemoteQueryExecutor.h
@@ -0,0 +1,16 @@
+//
+// Created by nik-kochetov on 6/2/20.
+//
+
+#ifndef CLICKHOUSE_REMOTEQUERYEXECUTOR_H
+#define CLICKHOUSE_REMOTEQUERYEXECUTOR_H
+
+
+
+class RemoteQueryExecutor {
+
+};
+
+
+
+#endif //CLICKHOUSE_REMOTEQUERYEXECUTOR_H
