35734aadde1b7cd0f68cc6a513bab1e8497229f8
Ivan Blinkov <github@blinkov.ru>
2020-06-02 08:15:11 +0300

    apply comment

--- src/Common/StackTrace.cpp
+++ src/Common/StackTrace.cpp
@@ -313,7 +313,7 @@ const StackTrace::Frames & StackTrace::getFrames() const
 {
     if (!frames.has_value())
     {
-        frames = std::array<Frame, 32>();
+        frames.emplace({});
         symbolize(frame_pointers.data(), offset, size, frames);
     }
     return frames;
