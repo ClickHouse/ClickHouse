0cb86494266aece9b92e0521841a77aeaf82c230
Guillaume Tassery <gtassery@partners.accedian.com>
2020-06-03 13:57:18 +0700

    take into account special character for netloc

--- src/Functions/URL/netloc.h
+++ src/Functions/URL/netloc.h
@@ -2,41 +2,128 @@
 
 #include "FunctionsURL.h"
 #include <common/find_symbols.h>
+#include "protocol.h"
+#include <cstring>
+#include <Common/StringUtils/StringUtils.h>
 
 
 namespace DB
 {
 
-struct ExtractNetloc
-{
-    /// We use the same as domain function
-    static size_t getReserveLengthForElement() { return 15; }
-
-    static void execute(Pos data, size_t size, Pos & res_data, size_t & res_size)
+    struct ExtractNetloc
     {
-        res_data = data;
-        res_size = size;
+        /// We use the same as domain function
+        static size_t getReserveLengthForElement() { return 15; }
 
-        Pos pos = data;
-        Pos end = pos + size;
+        static inline StringRef getNetworkLocation(const char * data, size_t size)
+        {
+            Pos pos = data;
+            Pos end = data + size;
 
-        pos = find_first_symbols<'/'>(pos, end);
-        if (end == pos)
-            return;
+            if (*pos == '/' && *(pos + 1) == '/')
+            {
+                pos += 2;
+            }
+            else
+            {
+                Pos scheme_end = data + std::min(size, 16UL);
+                for (++pos; pos < scheme_end; ++pos)
+                {
+                    if (!isAlphaNumericASCII(*pos))
+                    {
+                        switch (*pos)
+                        {
+                            case '.':
+                            case '-':
+                            case '+':
+                                break;
+                            case ' ': /// restricted symbols
+                            case '\t':
+                            case '<':
+                            case '>':
+                            case '%':
+                            case '{':
+                            case '}':
+                            case '|':
+                            case '\\':
+                            case '^':
+                            case '~':
+                            case '[':
+                            case ']':
+                            case ';':
+                            case '=':
+                            case '&':
+                                return StringRef{};
+                            default:
+                                goto exloop;
+                        }
+                    }
+                }
+exloop: if ((scheme_end - pos) > 2 && *pos == ':' && *(pos + 1) == '/' && *(pos + 2) == '/')
+            pos += 3;
+        else
+            pos = data;
+            }
 
-        /// Strings are zero-terminated.
-        bool has_subsequent_slash = pos[1] == '/';
-        if (!has_subsequent_slash)
-            return;
-        /// Search for next slash or question mark
-        /// Note than currently the netloc function doesn't support
-        /// if we have a question mark as an username or password.
-        /// This choice has been made for not hurting performance with still taking into
-        /// acount URL without a slash in the end of a query, with a query string.
-        res_data = pos + 2;
-        res_size = find_first_symbols<'/', '?'>(pos + 2, end) - res_data;
-    }
-};
+            bool has_identification = false;
+            Pos question_mark_pos = end;
+            Pos slash_pos = end;
+            auto start_of_host = pos;
+            for (; pos < end; ++pos)
+            {
+                switch (*pos)
+                {
+                    case '/':
+                        if (has_identification)
+                            return StringRef(start_of_host, pos - start_of_host);
+                        else
+                            slash_pos = pos;
+                        break;
+                    case '?':
+                        if (has_identification)
+                            return StringRef(start_of_host, pos - start_of_host);
+                        else
+                            question_mark_pos = pos;
+                        break;
+                    case '#':
+                        return StringRef(start_of_host, pos - start_of_host);
+                    case '@': /// foo:bar@example.ru
+                        has_identification = true;
+                        break;
+                    case ' ': /// restricted symbols in whole URL
+                    case '\t':
+                    case '<':
+                    case '>':
+                    case '%':
+                    case '{':
+                    case '}':
+                    case '|':
+                    case '\\':
+                    case '^':
+                    case '~':
+                    case '[':
+                    case ']':
+                    case ';':
+                    case '=':
+                    case '&':
+                        return StringRef(start_of_host, std::min(std::min(pos - 1, question_mark_pos), slash_pos) - start_of_host);
+                }
+            }
+
+            if (has_identification)
+                return StringRef(start_of_host, pos - start_of_host);
+            else
+                return StringRef(start_of_host, std::min(std::min(pos, question_mark_pos), slash_pos) - start_of_host);
+        }
+
+        static void execute(Pos data, size_t size, Pos & res_data, size_t & res_size)
+        {
+            StringRef host = getNetworkLocation(data, size);
+
+            res_data = host.data;
+            res_size = host.size;
+        }
+    };
 
 }
 
