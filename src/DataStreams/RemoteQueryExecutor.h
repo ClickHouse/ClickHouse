ce69fa9e3e2015b03b9a3e1e8964d23eaff07bd5
Nikolai Kochetov <nik-kochetov@yandex-team.ru>
2020-06-04 16:16:58 +0300

    Fix build.

--- src/DataStreams/RemoteQueryExecutor.h
+++ src/DataStreams/RemoteQueryExecutor.h
@@ -61,8 +61,8 @@ public:
     void cancel();
 
     /// Get totals and extremes if any.
-    Block getTotals() const { return std::move(totals); }
-    Block getExtremes() const { return std::move(extremes); }
+    Block getTotals() { return std::move(totals); }
+    Block getExtremes() { return std::move(extremes); }
 
     /// Set callback for progress. It will be called on Progress packet.
     void setProgressCallback(ProgressCallback callback) { progress_callback = std::move(callback); }
