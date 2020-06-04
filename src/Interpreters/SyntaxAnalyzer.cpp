0a5c05b06bec55e093d3055f347b288bd8a77157
Artem Zuikov <chertus@gmail.com>
2020-06-05 01:10:52 +0300

    rename v2

--- src/Interpreters/SyntaxAnalyzer.cpp
+++ src/Interpreters/SyntaxAnalyzer.cpp
@@ -823,7 +823,7 @@ SyntaxAnalyzerResultPtr SyntaxAnalyzer::analyzeSelect(
         optimizeIf(query, result.aliases, settings.optimize_if_chain_to_miltiif);
 
         /// Move arithmetic operations out of aggregation functions
-        optimizeArithmeticOperationsInAgr(query, settings.optimize_ast_arithmetic);
+        optimizeArithmeticOperationsInAgr(query, settings.optimize_arithmetic_operations_in_aggregate_functions);
 
         /// Push the predicate expression down to the subqueries.
         result.rewrite_subqueries = PredicateExpressionsOptimizer(context, tables_with_column_names, settings).optimize(*select_query);
--- tests/queries/0_stateless/01271_optimize_arithmetic_operations_in_aggr_func.sql
+++ tests/queries/0_stateless/01271_optimize_arithmetic_operations_in_aggr_func.sql
@@ -1,10 +1,10 @@
-set optimize_ast_arithmetic = 1;
+set optimize_arithmetic_operations_in_aggregate_functions = 1;
 
 SELECT sum(number * -3) + min(2 * number * -3) - max(-1 * -2 * number * -3) FROM numbers(10000000);
 SELECT max(log(2) * number) FROM numbers(10000000);
 SELECT round(max(log(2) * 3 * sin(0.3) * number * 4)) FROM numbers(10000000);
 
-set optimize_ast_arithmetic = 0;
+set optimize_arithmetic_operations_in_aggregate_functions = 0;
 
 SELECT sum(number * -3) + min(2 * number * -3) - max(-1 * -2 * number * -3) FROM numbers(10000000);
 SELECT max(log(2) * number) FROM numbers(10000000);
