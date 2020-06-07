14a891057d292a164c4179bfddaef45a74eaf83a
Alexey Ilyukhov <livace@yandex-team.ru>
2020-06-07 17:58:34 +0300

    Add other parsers

--- src/Functions/geometryFromColumn.cpp
+++ src/Functions/geometryFromColumn.cpp
@@ -5,16 +5,13 @@ namespace DB {
 
 namespace {
 
-size_t getArrayDepth(const ColumnWithTypeAndName & col, size_t max_depth)
+size_t getArrayDepth(DataTypePtr data_type, size_t max_depth)
 {
     size_t depth = 0;
-    DataTypePtr data_type = col.type;
-
     while (isArray(data_type) && depth != max_depth + 1)
     {
         data_type = static_cast<const DataTypeArray &>(*data_type).getNestedType();
     }
-
     return max_depth;
 }
 
@@ -22,11 +19,11 @@ size_t getArrayDepth(const ColumnWithTypeAndName & col, size_t max_depth)
 
 GeometryFromColumnParser makeGeometryFromColumnParser(const ColumnWithTypeAndName & col)
 {
-    switch (getArrayDepth(col, 3)) {
+    switch (getArrayDepth(col.type, 3)) {
         case 0: return Float64PointFromColumnParser(*col.column);
         case 1: return Float64RingFromColumnParser(*col.column);
-        // case 2: return parsePolygon(col, i);
-        // case 3: return parseMultyPoligon(col, i);
+        case 2: return Float64PolygonFromColumnParser(*col.column);
+        case 3: return Float64MultiPolygonFromColumnParser(*col.column);
         default: throw Exception("Cannot parse geometry from column with type " + col.type->getName()
                 + ", array depth is too big", ErrorCodes::ILLEGAL_COLUMN);
     }
