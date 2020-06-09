4770de302a4604516aaed1f5e00943928fdacca4
alesapin <alesapin@gmail.com>
2020-06-09 18:04:56 +0300

    Better test

--- tests/queries/0_stateless/01304_direct_io.sh
+++ tests/queries/0_stateless/01304_direct_io.sh
@@ -9,8 +9,9 @@ $CLICKHOUSE_CLIENT --multiquery --query "
     INSERT INTO bug SELECT rand64(), '2020-06-07' FROM numbers(50000000);
     OPTIMIZE TABLE bug FINAL;"
 
-$CLICKHOUSE_BENCHMARK --database $CLICKHOUSE_DATABASE --iterations 10 --max_threads 100 --min_bytes_to_use_direct_io 1 <<< "SELECT sum(UserID) FROM bug PREWHERE NOT ignore(Date)" >/dev/null 2>$CLICKHOUSE_TMP/err
-cat $CLICKHOUSE_TMP/err
+$CLICKHOUSE_BENCHMARK --database $CLICKHOUSE_DATABASE --iterations 10 --max_threads 100 --min_bytes_to_use_direct_io 1 <<< "SELECT sum(UserID) FROM bug PREWHERE NOT ignore(Date)" 1>/dev/null 2>$CLICKHOUSE_TMP/err
+cat $CLICKHOUSE_TMP/err | grep Exception
+cat $CLICKHOUSE_TMP/err | grep Loaded
 
 $CLICKHOUSE_CLIENT --multiquery --query "
     DROP TABLE bug;"
