6aa02f0066b190b023ae8781d44e7091b638da94
Vasily Nemkov <V.Nemkov@gmail.com>
2020-06-09 12:47:59 +0300

    extractAllGroupsHorizontal and extractAllGroupsVertical
    Split tests, fixed some error messages
    
    Fixed test and error reporting of extractGroups

---
+++ src/Functions/registerFunctionsStringRegexp.cpp
@@ -0,0 +1,46 @@
+namespace DB
+{
+
+class FunctionFactory;
+
+void registerFunctionLike(FunctionFactory &);
+void registerFunctionNotLike(FunctionFactory &);
+void registerFunctionMatch(FunctionFactory &);
+void registerFunctionExtract(FunctionFactory &);
+void registerFunctionReplaceOne(FunctionFactory &);
+void registerFunctionReplaceAll(FunctionFactory &);
+void registerFunctionReplaceRegexpOne(FunctionFactory &);
+void registerFunctionReplaceRegexpAll(FunctionFactory &);
+void registerFunctionMultiMatchAny(FunctionFactory &);
+void registerFunctionMultiMatchAnyIndex(FunctionFactory &);
+void registerFunctionMultiMatchAllIndices(FunctionFactory &);
+void registerFunctionMultiFuzzyMatchAny(FunctionFactory &);
+void registerFunctionMultiFuzzyMatchAnyIndex(FunctionFactory &);
+void registerFunctionMultiFuzzyMatchAllIndices(FunctionFactory &);
+void registerFunctionExtractGroups(FunctionFactory &);
+void registerFunctionExtractAllGroupsVertical(FunctionFactory &);
+void registerFunctionExtractAllGroupsHorizontal(FunctionFactory &);
+
+void registerFunctionsStringRegexp(FunctionFactory & factory)
+{
+    registerFunctionLike(factory);
+    registerFunctionNotLike(factory);
+    registerFunctionMatch(factory);
+    registerFunctionExtract(factory);
+    registerFunctionReplaceOne(factory);
+    registerFunctionReplaceAll(factory);
+    registerFunctionReplaceRegexpOne(factory);
+    registerFunctionReplaceRegexpAll(factory);
+    registerFunctionMultiMatchAny(factory);
+    registerFunctionMultiMatchAnyIndex(factory);
+    registerFunctionMultiMatchAllIndices(factory);
+    registerFunctionMultiFuzzyMatchAny(factory);
+    registerFunctionMultiFuzzyMatchAnyIndex(factory);
+    registerFunctionMultiFuzzyMatchAllIndices(factory);
+    registerFunctionExtractGroups(factory);
+    registerFunctionExtractAllGroupsVertical(factory);
+    registerFunctionExtractAllGroupsHorizontal(factory);
+}
+
+}
+
