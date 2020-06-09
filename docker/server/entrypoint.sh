da0b5e53bda1caf5dd2043c2a99afa163289fe23
alexey-milovidov <milovidov@yandex-team.ru>
2020-06-10 01:48:58 +0300

    Merge pull request #11550 from realadvisor/master
    Fix ClickHouse init process failed in docker if any script is added into docker-entrypoint-initdb.d 
    
    (cherry picked from commit 13b225799ec4a136dcfd08b1d5209be3cc7dba55)

--- docker/server/entrypoint.sh
+++ docker/server/entrypoint.sh
@@ -69,7 +69,7 @@ if [ -n "$(ls /docker-entrypoint-initdb.d/)" ]; then
 
     # check if clickhouse is ready to accept connections
     # will try to send ping clickhouse via http_port (max 12 retries, with 1 sec delay)
-    if ! wget --spider --quiet --tries=12 --waitretry=1 --retry-connrefused "http://localhost:$HTTP_PORT/ping" ; then
+    if ! wget --spider --quiet --prefer-family=IPv6 --tries=12 --waitretry=1 --retry-connrefused "http://localhost:$HTTP_PORT/ping" ; then
         echo >&2 'ClickHouse init process failed.'
         exit 1
     fi
