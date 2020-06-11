c9c03089b8f17de23eec986e87006137798fbeaa
Ruslan <kam_rus_200101@mail.ru>
2020-06-12 01:42:10 +0300

    code style

--- src/Interpreters/AnyInputOptimize.cpp
+++ src/Interpreters/AnyInputOptimize.cpp
@@ -13,8 +13,6 @@ namespace DB
 namespace ErrorCodes
 {
     extern const int LOGICAL_ERROR;
-    extern const int NUMBER_OF_ARGUMENTS_DOESNT_MATCH;
-    extern const int UNEXPECTED_AST_STRUCTURE;
     extern const int ILLEGAL_AGGREGATION;
 }
 
