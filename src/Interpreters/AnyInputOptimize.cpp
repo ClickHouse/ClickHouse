ae6fe74dddad96fac102f64d7562889a9a185206
Ruslan <kam_rus_200101@mail.ru>
2020-06-12 01:32:27 +0300

    build + requested changes

--- src/Interpreters/AnyInputOptimize.cpp
+++ src/Interpreters/AnyInputOptimize.cpp
@@ -15,6 +15,7 @@ namespace ErrorCodes
     extern const int LOGICAL_ERROR;
     extern const int NUMBER_OF_ARGUMENTS_DOESNT_MATCH;
     extern const int UNEXPECTED_AST_STRUCTURE;
+    extern const int ILLEGAL_AGGREGATION;
 }
 
 namespace
@@ -30,20 +31,23 @@ ASTPtr * getExactChild(const ASTPtr & ast, const size_t ind)
 ///recursive searching of identifiers
 void changeAllIdentifiers(ASTPtr & ast, size_t ind)
 {
-    if ((*getExactChild(ast, ind))->as<ASTIdentifier>())
+    ASTPtr * exact_child = getExactChild(ast, ind);
+    ast->dumpTree(std::cerr);
+    if ((*exact_child)->as<ASTIdentifier>())
     {
         ///put new any
-        ASTPtr oldAST = (*getExactChild(ast, ind));
-        (*getExactChild(ast, ind)) = makeASTFunction("any");
-        (*getExactChild(ast, ind))->as<ASTFunction>()->arguments->children.push_back(oldAST);
+        ASTPtr oldAST = *exact_child;
+        *exact_child = makeASTFunction(any);
+        (*exact_child)->as<ASTFunction>()->arguments->children.push_back(oldAST);
     }
-    else if ((*getExactChild(ast, ind))->as<ASTFunction>() &&
-        !AggregateFunctionFactory::instance().isAggregateFunctionName((*getExactChild(ast, ind))->as<ASTFunction>()->name))
-        for (size_t i = 0; i < (*getExactChild(ast, ind))->as<ASTFunction>()->arguments->children.size(); i++)
-            changeAllIdentifiers((*getExactChild(ast, ind)), i);
+    else if ((*exact_child)->as<ASTFunction>() &&
+        !AggregateFunctionFactory::instance().isAggregateFunctionName((*exact_child)->as<ASTFunction>()->name))
+        for (size_t i = 0; i < (*exact_child)->as<ASTFunction>()->arguments->children.size(); i++)
+            changeAllIdentifiers(*exact_child, i);
     else if (ast->as<ASTFunction>()->arguments->children[ind]->as<ASTFunction>() &&
-             AggregateFunctionFactory::instance().isAggregateFunctionName((*getExactChild(ast, ind))->as<ASTFunction>()->name))
-        throw Exception("Aggregate function is found inside another aggregate function in query", 184);
+             AggregateFunctionFactory::instance().isAggregateFunctionName((*exact_child)->as<ASTFunction>()->name))
+        throw Exception("Aggregate function " + (*exact_child)->as<ASTFunction>()->name +
+                            " is found inside aggregate function any in query", ErrorCodes::ILLEGAL_AGGREGATION);
 }
 
 
@@ -55,15 +59,12 @@ void AnyInputMatcher::visit(ASTPtr & current_ast, Data data)
         return;
 
     auto * function_node = current_ast->as<ASTFunction>();
-    if (function_node && function_node->name == "any" && function_node->arguments->children[0]->as<ASTFunction>())
+    if (function_node && function_node->name == any && function_node->arguments->children[0]->as<ASTFunction>())
     {
         ///cut any
-        current_ast = function_node->arguments->children[0];
-        if (current_ast->as<ASTFunction>())
-            for (size_t i = 0; i < current_ast->as<ASTFunction>()->arguments->children.size(); ++i)
-                changeAllIdentifiers(current_ast, i);
-        else
-            changeAllIdentifiers(current_ast, 0);
+        current_ast = (function_node->arguments->children[0])->clone();
+        for (size_t i = 0; i < current_ast->as<ASTFunction>()->arguments->children.size(); ++i)
+            changeAllIdentifiers(current_ast, i);
     }
 }
 
