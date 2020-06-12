41dd5e1003dd182c3b2612110e777c097586006a
Azat Khuzhin <a3at.mail@gmail.com>
2020-06-12 18:32:08 +0300

    tests/allocator: do not create threads on fly, pre-create them instead
    Since:
    - this clickhouse uses thread pool anyway
    - allocator can have some hooks/extra stuff that is done for each new
      thread

--- base/common/tests/allocator.cpp
+++ base/common/tests/allocator.cpp
@@ -4,7 +4,7 @@
 #include <thread>
 
 
-void thread_func()
+void alloc_loop()
 {
     for (size_t i = 0; i < 100; ++i)
     {
@@ -33,16 +33,21 @@ void thread_func()
     }
 }
 
+void thread_func()
+{
+    for (size_t i = 0; i < 1000; ++i)
+    {
+        alloc_loop();
+    }
+}
+
 
 int main(int, char **)
 {
     std::vector<std::thread> threads(16);
-    for (size_t i = 0; i < 1000; ++i)
-    {
-        for (auto & thread : threads)
-            thread = std::thread(thread_func);
-        for (auto & thread : threads)
-            thread.join();
-    }
+    for (auto & thread : threads)
+        thread = std::thread(thread_func);
+    for (auto & thread : threads)
+        thread.join();
     return 0;
 }
