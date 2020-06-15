6a73e7d136ef60fa939da83aea8186a7a9cf1d28
alexey-milovidov <milovidov@yandex-team.ru>
2020-06-15 19:21:52 +0300

    Trigger CI

--- tests/queries/0_stateless/01315_count_distinct_return_not_nullable.sql
+++ tests/queries/0_stateless/01315_count_distinct_return_not_nullable.sql
@@ -7,13 +7,11 @@ SELECT uniqExact(number >= 5 ? number : NULL) FROM numbers(10);
 SELECT count(DISTINCT number >= 5 ? number : NULL) FROM numbers(10);
 
 SELECT '---';
-
 SELECT count(NULL);
 SELECT uniq(NULL);
 SELECT count(DISTINCT NULL);
 
 SELECT '---';
-
 SELECT avg(NULL);
 SELECT sum(NULL);
 SELECT corr(NULL, NULL);
