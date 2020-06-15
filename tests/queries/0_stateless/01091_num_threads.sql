a5c7b32a3788bbaeb4ad38f02a048d4645b59f8d
Alexey Milovidov <milovidov@yandex-team.ru>
2020-06-16 02:42:10 +0300

    Fix test

--- tests/queries/0_stateless/01091_num_threads.sql
+++ tests/queries/0_stateless/01091_num_threads.sql
@@ -4,7 +4,7 @@ set log_query_threads=1;
 SELECT 1;
 SYSTEM FLUSH LOGS;
 
-WITH 
+WITH
     (
         SELECT query_id
         FROM system.query_log
@@ -19,11 +19,11 @@ WHERE (event_date >= (today() - 1)) AND (query_id = id) AND (thread_id != master
 select sum(number) from numbers(1000000);
 SYSTEM FLUSH LOGS;
 
-WITH 
+WITH
     (
         SELECT query_id
         FROM system.query_log
-        WHERE (query = 'SELECT sum(number) FROM numbers(1000000)') AND (event_date >= (today() - 1))
+        WHERE (query LIKE 'select sum(number) from numbers(1000000);%') AND (event_date >= (today() - 1))
         ORDER BY event_time DESC
         LIMIT 1
     ) AS id
@@ -34,11 +34,11 @@ WHERE (event_date >= (today() - 1)) AND (query_id = id) AND (thread_id != master
 select sum(number) from numbers_mt(1000000);
 SYSTEM FLUSH LOGS;
 
-WITH 
+WITH
     (
         SELECT query_id
         FROM system.query_log
-        WHERE (query = 'SELECT sum(number) FROM numbers_mt(1000000)') AND (event_date >= (today() - 1))
+        WHERE (query LIKE 'select sum(number) from numbers_mt(1000000);%') AND (event_date >= (today() - 1))
         ORDER BY event_time DESC
         LIMIT 1
     ) AS id
