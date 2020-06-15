eabbabed04aba337e94d32b4a869bc84e883fbec
Anton Popov <pad11rus@gmail.com>
2020-06-16 01:24:00 +0300

    fix 'LIMIT WITH TIES' with aliases

--- src/Interpreters/InterpreterSelectQuery.cpp
+++ src/Interpreters/InterpreterSelectQuery.cpp
@@ -973,6 +973,14 @@ void InterpreterSelectQuery::executeImpl(QueryPipeline & pipeline, const BlockIn
 
             executeWithFill(pipeline);
 
+            /// If we have 'WITH TIES', we need execute limit before projection,
+            /// because in that case columns from 'ORDER BY' are used.
+            if (query.limit_with_ties)
+            {
+                executeLimit(pipeline);
+                has_prelimit = true;
+            }
+
             /** We must do projection after DISTINCT because projection may remove some columns.
               */
             executeProjection(pipeline, expressions.final_projection);
