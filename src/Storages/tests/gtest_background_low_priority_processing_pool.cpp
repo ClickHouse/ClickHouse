680fff3a65414b61cc20afad46cdb01dfc05a030
UnluckyAF <kirill.barukhov@yandex.ru>
2020-06-15 19:32:11 +0300

    fix review

--- src/Storages/tests/gtest_background_low_priority_processing_pool.cpp
+++ src/Storages/tests/gtest_background_low_priority_processing_pool.cpp
@@ -7,17 +7,25 @@
 #include <Poco/Util/Application.h>
 #include <daemon/BaseDaemon.h>
 
-DB::BackgroundProcessingPoolTaskResult checkLow()
+DB::BackgroundProcessingPoolTaskResult checkLow(std::vector<bool>& checks, int i, std::mutex& mut)
 {
     int policy;
     sched_param param;
     if (pthread_getschedparam(pthread_self(), &policy, &param))
     {
+        mut.lock();
+        checks[i] = false;
+        mut.unlock();
+
         return DB::BackgroundProcessingPoolTaskResult::ERROR;
     }
 
     if (policy != SCHED_IDLE)
     {
+        mut.lock();
+        checks[i] = false;
+        mut.unlock();
+
         return DB::BackgroundProcessingPoolTaskResult::ERROR;
     }
     return DB::BackgroundProcessingPoolTaskResult::SUCCESS;
@@ -74,7 +82,7 @@ static DB::ConfigurationPtr getConfigurationFromXMLString(const char * xml_data)
     return {new Poco::Util::XMLConfiguration{&input_source}};
 }
 
-TEST(LowPriorityBackgroundProcessingPool, SimpleCase)
+TEST(BackgroundLowPriorityProcessingPool, SimpleCase)
 {
     using namespace DB;
     const auto & context_holder = getContext();
@@ -87,26 +95,32 @@ TEST(LowPriorityBackgroundProcessingPool, SimpleCase)
     BackgroundProcessingPool & pool = ctx.getBackgroundLowPriorityPool();
 
     auto task_handle = pool.createTask([&data] { return odd(data); });
-    pool.startTask(task_handle);
+    pool.startTask(task_handle, false);
     auto even_handle = pool.createTask([&data] { return even(data); });
-    pool.startTask(even_handle);
-    auto check_low_handle = pool.createTask([] { return checkLow(); });
-    pool.startTask(check_low_handle);
-
+    pool.startTask(even_handle, false);
     std::this_thread::sleep_for(std::chrono::seconds(1));
     pool.removeTask(task_handle);
     pool.removeTask(even_handle);
-    pool.removeTask(check_low_handle);
-    ctx.shutdown();
-
     for (int i = 0; i < 10; ++i)
     {
         ASSERT_EQ(i, data[i]) << "failed on " << i << " step\n";
     }
+
+    std::mutex mut;
+    std::vector<bool> checks(16, true);
+    std::vector<BackgroundProcessingPool::TaskHandle> handles(16);
+    for (int i = 0; i < 16; ++i) {
+        handles[i] = pool.createTask([&] { return checkLow(checks, i, mut); });
+        pool.startTask(handles[i]);
+    }
+    std::this_thread::sleep_for(std::chrono::seconds(1));
+    for (int i = 0; i < 16; ++i) {
+        pool.removeTask(handles[i]);
+    }
+    for (bool c : checks) {
+        ASSERT_TRUE(c) << "failed on low priority checks\n";
+    }
+
+    ctx.shutdown();
 }
 
-int main(int argc, char **argv)
-{
-    ::testing::InitGoogleTest(&argc, argv);
-    return RUN_ALL_TESTS();
-}
