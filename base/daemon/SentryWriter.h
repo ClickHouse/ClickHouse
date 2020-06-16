0e77692a278b818d03bfd0987c7115802f89a648
Ivan Blinkov <github@blinkov.ru>
2020-06-16 15:56:28 +0300

    improvements after review comments

--- base/daemon/SentryWriter.h
+++ base/daemon/SentryWriter.h
@@ -7,7 +7,13 @@
 
 #include <string>
 
-/// Sends crash reports to ClickHouse core developer team via https://sentry.io
+/// \brief Sends crash reports to ClickHouse core developer team via https://sentry.io
+///
+/// This feature can enabled with "send_crash_reports.enabled" server setting,
+/// in this case reports are sent only for official ClickHouse builds.
+///
+/// It is possible to send those reports to your own sentry account or account of consulting company you hired
+/// by overriding "send_crash_reports.endpoint" setting. "send_crash_reports.debug" setting will allow to do that for
 class SentryWriter
 {
 public:
@@ -15,6 +21,8 @@ public:
 
     static void initialize(Poco::Util::LayeredConfiguration & config);
     static void shutdown();
+
+    /// Not signal safe and can't be called from a signal handler
     static void onFault(
         int sig,
         const siginfo_t & info,
