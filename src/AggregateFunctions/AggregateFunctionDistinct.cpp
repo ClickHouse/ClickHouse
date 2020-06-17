092f38cc929aeaf534a30fdc296e728450a41920
Anton Popov <pad11rus@gmail.com>
2020-06-17 13:02:04 +0300

    distinct combinator for single numeric arguments

--- src/AggregateFunctions/AggregateFunctionDistinct.cpp
+++ src/AggregateFunctions/AggregateFunctionDistinct.cpp
@@ -1,5 +1,6 @@
 #include <AggregateFunctions/AggregateFunctionDistinct.h>
 #include <AggregateFunctions/AggregateFunctionCombinatorFactory.h>
+#include <AggregateFunctions/Helpers.h>
 #include <Common/typeid_cast.h>
 #include "registerAggregateFunctions.h"
 
@@ -9,6 +10,7 @@ namespace DB
 namespace ErrorCodes
 {
     extern const int NUMBER_OF_ARGUMENTS_DOESNT_MATCH;
+    extern const int ILLEGAL_TYPE_OF_ARGUMENT;
 }
 
 class AggregateFunctionCombinatorDistinct final : public IAggregateFunctionCombinator
@@ -22,19 +24,30 @@ public:
             throw Exception("Incorrect number of arguments for aggregate function with " + getName() + " suffix",
                             ErrorCodes::NUMBER_OF_ARGUMENTS_DOESNT_MATCH);
 
-        DataTypes nested_arguments;
-        for (const auto & type : arguments)
-        {
-            nested_arguments.push_back(type);
-        }
-
-        return nested_arguments;
+        return arguments;
     }
 
     AggregateFunctionPtr transformAggregateFunction(
             const AggregateFunctionPtr & nested_function, const DataTypes & arguments, const Array &) const override
     {
-        return std::make_shared<AggregateFunctionDistinct>(nested_function, arguments);
+        AggregateFunctionPtr res;
+        if (arguments.size() == 1)
+        {
+            res = AggregateFunctionPtr(createWithNumericType<AggregateFunctionDistinctSingleNumericImpl>(*arguments[0], nested_function, arguments));
+            if (res)
+                return res;
+
+            if (arguments[0]->isValueUnambiguouslyRepresentedInContiguousMemoryRegion())
+                return std::make_shared<AggregateFunctionDistinctSingleGenericImpl<true>>(nested_function, arguments);
+            else
+                return std::make_shared<AggregateFunctionDistinctSingleGenericImpl<false>>(nested_function, arguments);
+        }
+
+        if (!res)
+            throw Exception("Illegal type " /* + argument_type->getName() + */
+                            " of argument for aggregate function " + nested_function->getName() + getName(), ErrorCodes::ILLEGAL_TYPE_OF_ARGUMENT);
+
+        return res;
     }
 };
 
