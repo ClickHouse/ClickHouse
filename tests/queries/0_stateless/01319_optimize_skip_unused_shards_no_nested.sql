f4460b799591947423a315f006b6e83195e4a9de
Azat Khuzhin <a3at.mail@gmail.com>
2020-06-18 21:45:40 +0300

    Improve 01319_optimize_skip_unused_shards_no_nested
    Before there is no check that optimize_skip_unused_shards was working
    for the first level, use cluster with unavalable shard to guarantee
    this.

--- tests/queries/0_stateless/01319_optimize_skip_unused_shards_no_nested.sql
+++ tests/queries/0_stateless/01319_optimize_skip_unused_shards_no_nested.sql
@@ -7,8 +7,10 @@ create table data_01319 (key Int, sub_key Int) Engine=Null();
 set force_optimize_skip_unused_shards=2;
 set optimize_skip_unused_shards=1;
 
-create table dist_layer_01319 as data_01319 Engine=Distributed(test_cluster_two_shards, currentDatabase(), data_01319, sub_key%2);
-create table dist_01319 as data_01319 Engine=Distributed(test_cluster_two_shards, currentDatabase(), dist_layer_01319, key%2);
+create table dist_layer_01319 as data_01319 Engine=Distributed(test_cluster_two_shards, currentDatabase(), data_01319, sub_key);
+-- test_unavailable_shard here to check that optimize_skip_unused_shards always
+-- remove some nodes from the cluster for the first nesting level
+create table dist_01319 as data_01319 Engine=Distributed(test_unavailable_shard, currentDatabase(), dist_layer_01319, key+1);
 select * from dist_01319 where key = 1; -- { serverError 507 }
 set optimize_skip_unused_shards=2; -- no nested
 select * from dist_01319 where key = 1;
