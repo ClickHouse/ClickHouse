6e38d9ec10fcb3cbc69bbe2da098567da794ea9f
Nikolai Kochetov <nik-kochetov@yandex-team.ru>
2020-06-19 15:26:03 +0300

    Update test.

--- tests/queries/0_stateless/01323_too_many_threads_bug.sql
+++ tests/queries/0_stateless/01323_too_many_threads_bug.sql
@@ -6,9 +6,14 @@ insert into table_01323_many_parts select number from numbers(100000);
 
 set max_threads = 16;
 set log_queries = 1;
-select x from table_01323_many_parts order by x limit 10;
+select x from table_01323_many_parts limit 10 format Null;
 
 system flush logs;
-select length(thread_ids) <= 20 from system.query_log where event_date >= today() - 1 and lower(query) like '%select x from table_01323_many_parts%' and type = 'QueryFinish' order by query_start_time desc limit 1;
+select length(thread_ids) <= 4 from system.query_log where event_date >= today() - 1 and lower(query) like '%select x from table_01323_many_parts%' and type = 'QueryFinish' order by query_start_time desc limit 1;
+
+select x from table_01323_many_parts order by x limit 10 format Null;
+
+system flush logs;
+select length(thread_ids) <= 20 from system.query_log where event_date >= today() - 1 and lower(query) like '%select x from table_01323_many_parts order by x%' and type = 'QueryFinish' order by query_start_time desc limit 1;
 
 drop table if exists table_01323_many_parts;
