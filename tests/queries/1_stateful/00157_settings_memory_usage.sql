6cc51c661958d8c8c4b0f8d0bc9e9fc2b464681b
Alexander Kuzmenkov <akuzm@yandex-team.ru>
2020-06-19 22:18:39 +0300

    fixup

--- tests/queries/1_stateful/00157_settings_memory_usage.sql
+++ tests/queries/1_stateful/00157_settings_memory_usage.sql
@@ -1,1 +1,1 @@
-select max(CounterID + UserID) from test.hits settings max_memory_usage = 1; -- { serverError 241 }
+select max(CounterID - UserID) from test.hits settings max_memory_usage = 1; -- { serverError 241 }
