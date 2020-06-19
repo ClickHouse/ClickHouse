8591f651f1be192a8747f7b5d9f06429892e7a91
Nikolai Kochetov <nik-kochetov@yandex-team.ru>
2020-06-19 16:34:40 +0300

    Fix Context leak in InterpreterSelectQuery

--- src/Processors/QueryPlan/ReadFromPreparedSource.cpp
+++ src/Processors/QueryPlan/ReadFromPreparedSource.cpp
@@ -4,15 +4,17 @@
 namespace DB
 {
 
-ReadFromPreparedSource::ReadFromPreparedSource(Pipe pipe_)
+ReadFromPreparedSource::ReadFromPreparedSource(Pipe pipe_, std::shared_ptr<Context> context_)
     : ISourceStep(DataStream{.header = pipe_.getHeader()})
     , pipe(std::move(pipe_))
+    , context(std::move(context_))
 {
 }
 
 void ReadFromPreparedSource::initializePipeline(QueryPipeline & pipeline)
 {
     pipeline.init(std::move(pipe));
+    pipeline.addInterpreterContext(std::move(context));
 }
 
 }
