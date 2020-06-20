5c2e9da5836f810d6961533157784cf85e45adf1
Alexey Milovidov <milovidov@yandex-team.ru>
2020-06-20 12:17:43 +0300

    Added a function "buildId"

---
+++ src/Functions/buildId.cpp
@@ -0,0 +1,53 @@
+#if defined(__ELF__) && !defined(__FreeBSD__)
+
+#include <Functions/IFunctionImpl.h>
+#include <Functions/FunctionFactory.h>
+#include <DataTypes/DataTypeString.h>
+#include <Common/SymbolIndex.h>
+#include <Core/Field.h>
+
+
+namespace DB
+{
+
+/** buildId() - returns the compiler build id of the running binary.
+  */
+class FunctionVersion : public IFunction
+{
+public:
+    static constexpr auto name = "buildId";
+    static FunctionPtr create(const Context &)
+    {
+        return std::make_shared<FunctionVersion>();
+    }
+
+    String getName() const override
+    {
+        return name;
+    }
+
+    size_t getNumberOfArguments() const override
+    {
+        return 0;
+    }
+
+    DataTypePtr getReturnTypeImpl(const DataTypes & /*arguments*/) const override
+    {
+        return std::make_shared<DataTypeString>();
+    }
+
+    void executeImpl(Block & block, const ColumnNumbers &, size_t result, size_t input_rows_count) override
+    {
+        block.getByPosition(result).column = DataTypeString().createColumnConst(input_rows_count, SymbolIndex::instance().getBuildIDHex());
+    }
+};
+
+
+void registerFunctionVersion(FunctionFactory & factory)
+{
+    factory.registerFunction<FunctionVersion>();
+}
+
+}
+
+#endif
