7fcf942d24cc76aedb2c0c1a67aa2ffaf53175f1
Alexey Milovidov <milovidov@yandex-team.ru>
2020-06-20 14:29:23 +0300

    Turn off traps

--- src/Functions/trap.cpp
+++ src/Functions/trap.cpp
@@ -1,4 +1,4 @@
-#if 1
+#if 0
 
 #include <Functions/IFunction.h>
 #include <Functions/FunctionFactory.h>
@@ -63,8 +63,6 @@ public:
 
     void executeImpl(Block & block, const ColumnNumbers & arguments, size_t result, size_t input_rows_count) override
     {
-        std::cerr << "executeImpl: " << !!CurrentThread::getInternalTextLogsQueue() << "\n";
-
         if (const ColumnConst * column = checkAndGetColumnConst<ColumnString>(block.getByPosition(arguments[0]).column.get()))
         {
             String mode = column->getValue<String>();
