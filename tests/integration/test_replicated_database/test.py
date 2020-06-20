e8e4e4d21c559fc3548d791dea65aa7871e8d19f
Val <valeryxbaturin@gmail.com>
2020-06-20 18:38:20 +0300

    add tests for db replicated

--- tests/integration/test_replicated_database/test.py
+++ tests/integration/test_replicated_database/test.py
@@ -10,18 +10,16 @@ logging.getLogger().addHandler(logging.StreamHandler())
 
 cluster = ClickHouseCluster(__file__)
 
-node1 = cluster.add_instance('node1', macros={'replica': 'test1'}, with_zookeeper=True)
-node2 = cluster.add_instance('node2', macros={'replica': 'test2'}, with_zookeeper=True)
-node3 = cluster.add_instance('node3', macros={'replica': 'test3'}, with_zookeeper=True)
+node1 = cluster.add_instance('node1', main_configs=['configs/disable_snapshots.xml'], with_zookeeper=True, stay_alive=True)
+node2 = cluster.add_instance('node2', main_configs=['configs/disable_snapshots.xml'], with_zookeeper=True)
+node3 = cluster.add_instance('node3', main_configs=['configs/disable_snapshots.xml'], with_zookeeper=True)
 
 @pytest.fixture(scope="module")
 def started_cluster():
     try:
         cluster.start()
-
-        for node in [node1, node2]:
-            node.query("DROP DATABASE IF EXISTS testdb")
-            node.query("CREATE DATABASE testdb ENGINE = Replicated('/clickhouse/databases/test1', '{replica}');")
+        node1.query("CREATE DATABASE testdb ENGINE = Replicated('/clickhouse/databases/test1', 'replica1');")
+        node2.query("CREATE DATABASE testdb ENGINE = Replicated('/clickhouse/databases/test1', 'replica2');")
         yield cluster
 
     finally:
@@ -49,15 +47,13 @@ def test_simple_alter_table(started_cluster):
     assert node1.query("desc table testdb.alter_test") == node2.query("desc table testdb.alter_test")
 
 def test_create_replica_after_delay(started_cluster):
-    DURATION_SECONDS = 2
-
-    node3.query("CREATE DATABASE testdb ENGINE = Replicated('/clickhouse/databases/test1', '{replica}');")
+    node3.query("CREATE DATABASE testdb ENGINE = Replicated('/clickhouse/databases/test1', 'replica3');")
 
     node1.query("ALTER TABLE testdb.alter_test ADD COLUMN Added3 UInt32 ;")
     node1.query("ALTER TABLE testdb.alter_test ADD COLUMN Added4 UInt32 ;")
     node1.query("ALTER TABLE testdb.alter_test ADD COLUMN Added5 UInt32 ;")
 
-    time.sleep(DURATION_SECONDS)
+    time.sleep(6)
 
     assert node3.query("desc table testdb.alter_test") == node1.query("desc table testdb.alter_test")
 
@@ -77,8 +73,22 @@ def test_alters_from_different_replicas(started_cluster):
 
     time.sleep(DURATION_SECONDS)
 
-    logging.info("NODE3")
-    logging.info(node3.query("desc table testdb.concurrent_test"))
-    logging.info("NODE1")
-    logging.info(node1.query("desc table testdb.concurrent_test"))
     assert node3.query("desc table testdb.concurrent_test") == node1.query("desc table testdb.concurrent_test")
+
+def test_drop_and_create_table(started_cluster):
+    node1.query("DROP TABLE testdb.concurrent_test")
+    node1.query("CREATE TABLE testdb.concurrent_test (CounterID UInt32, StartDate Date, UserID UInt32, VisitID UInt32, NestedColumn Nested(A UInt8, S String), ToDrop UInt32) ENGINE = MergeTree(StartDate, intHash32(UserID), (CounterID, StartDate, intHash32(UserID), VisitID), 8192);")
+    time.sleep(5)
+    assert node3.query("desc table testdb.concurrent_test") == node1.query("desc table testdb.concurrent_test")
+
+def test_replica_restart(started_cluster):
+    node1.restart_clickhouse()
+    time.sleep(5)
+    assert node3.query("desc table testdb.concurrent_test") == node1.query("desc table testdb.concurrent_test")
+
+#def test_drop_and_create_replica(started_cluster):
+#    node1.query("DROP DATABASE testdb")
+#    node1.query("CREATE DATABASE testdb ENGINE = Replicated('/clickhouse/databases/test1', 'replica1');")
+#    time.sleep(6)
+#    assert node3.query("desc table testdb.concurrent_test") == node1.query("desc table testdb.concurrent_test")
+
