603e690c1836f97f4949f952c2557f2eb371bf82
alexey-milovidov <milovidov@yandex-team.ru>
2020-06-20 21:52:36 +0300

    Update Allocator.cpp

--- src/Common/Allocator.cpp
+++ src/Common/Allocator.cpp
@@ -5,12 +5,12 @@
   * in third-party code.
   */
 #ifdef NDEBUG
-    __attribute__((__weak__)) extern const size_t MMAP_THRESHOLD = 64 * (1ULL << 20);
+    __attribute__((__weak__)) const size_t MMAP_THRESHOLD = 64 * (1ULL << 20);
 #else
     /**
       * In debug build, use small mmap threshold to reproduce more memory
       * stomping bugs. Along with ASLR it will hopefully detect more issues than
       * ASan. The program may fail due to the limit on number of memory mappings.
       */
-    __attribute__((__weak__)) extern const size_t MMAP_THRESHOLD = 4096;
+    __attribute__((__weak__)) const size_t MMAP_THRESHOLD = 4096;
 #endif
