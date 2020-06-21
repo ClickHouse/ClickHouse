b3bfeca6bab6c632046aaa371985766d3c95c320
alexey-milovidov <milovidov@yandex-team.ru>
2020-06-21 17:25:41 +0300

    Update FunctionsComparison.h

--- src/Functions/FunctionsComparison.h
+++ src/Functions/FunctionsComparison.h
@@ -56,6 +56,7 @@ namespace ErrorCodes
     extern const int ILLEGAL_TYPE_OF_ARGUMENT;
     extern const int LOGICAL_ERROR;
     extern const int NOT_IMPLEMENTED;
+    extern const int BAD_ARGUMENTS;
 }
 
 
