b8ae056535c1ee815f4dcd8fefae9e13c5a6258f
Anton Popov <pad11rus@gmail.com>
2020-06-22 17:55:49 +0300

    more optimal aggregate functions with both 'if' and 'distinct' combinators

--- tests/queries/0_stateless/01259_combinator_distinct.sql
+++ tests/queries/0_stateless/01259_combinator_distinct.sql
@@ -5,5 +5,11 @@ SELECT finalizeAggregation(countState(DISTINCT toString(number % 20))) FROM numb
 SELECT round(corrStable(DISTINCT x, y), 5) FROM (SELECT number % 10 AS x, number % 5 AS y FROM numbers(1000));
 SELECT round(corrStable(x, y), 5) FROM (SELECT DISTINCT number % 10 AS x, number % 5 AS y FROM numbers(1000));
 
-
 SELECT sum(DISTINCT y) FROM (SELECT number % 5 AS x, number % 15 AS y FROM numbers(1000)) GROUP BY x;
+
+SET enable_debug_queries = 1;
+SELECT countIf(DISTINCT number % 10, number % 5 = 2) FROM numbers(10000);
+ANALYZE SELECT countIf(DISTINCT number % 10, number % 5 = 2) FROM numbers(10000);
+
+SELECT sumIf(DISTINCT number % 10, number % 5 = 2) FROM numbers(10000);
+ANALYZE SELECT sumIf(DISTINCT number % 10, number % 5 = 2) FROM numbers(10000);
