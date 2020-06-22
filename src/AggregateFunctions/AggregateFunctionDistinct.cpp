00daa0fb12b75890562017bf4b85a8fcf72996b6
Anton Popov <pad11rus@gmail.com>
2020-06-22 16:29:11 +0300

    add perf test for distinct combinator

--- src/AggregateFunctions/AggregateFunctionDistinct.cpp
+++ src/AggregateFunctions/AggregateFunctionDistinct.cpp
@@ -10,7 +10,6 @@ namespace DB
 namespace ErrorCodes
 {
     extern const int NUMBER_OF_ARGUMENTS_DOESNT_MATCH;
-    extern const int ILLEGAL_TYPE_OF_ARGUMENT;
 }
 
 class AggregateFunctionCombinatorDistinct final : public IAggregateFunctionCombinator
