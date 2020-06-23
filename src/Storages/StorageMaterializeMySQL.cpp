9324c0ee497e311b886277054d43b4c184d6cb4c
zhang2014 <coswde@gmail.com>
2020-06-24 01:45:55 +0800

    ISSUES-4006 fix crash when dump data

--- src/Storages/StorageMaterializeMySQL.cpp
+++ src/Storages/StorageMaterializeMySQL.cpp
@@ -1,9 +1,14 @@
 #include <Storages/StorageMaterializeMySQL.h>
 
+#include <Parsers/ASTFunction.h>
 #include <Parsers/ASTSelectQuery.h>
 #include <Parsers/ASTTablesInSelectQuery.h>
 
+#include <Parsers/ASTLiteral.h>
+#include <Parsers/ASTIdentifier.h>
+
 #include <Processors/Pipe.h>
+#include <Processors/Transforms/FilterTransform.h>
 
 namespace DB
 {
@@ -51,6 +56,20 @@ Pipes StorageMaterializeMySQL::read(
         require_columns_name.emplace_back(sign_column.name);
 
     return nested_storage->read(require_columns_name, query_info, context, processed_stage, max_block_size, num_streams);
+
+    /*for (auto & pipe : pipes)
+    {
+        std::cout << "Pipe Header Structure:" << pipe.getHeader().dumpStructure() << "\n";
+        ASTPtr expr = makeASTFunction(
+            "equals", std::make_shared<ASTIdentifier>(sign_column.name), std::make_shared<ASTLiteral>(Field(Int8(1))));
+        auto syntax = SyntaxAnalyzer(context).analyze(expr, pipe.getHeader().getNamesAndTypesList());
+        ExpressionActionsPtr expression_actions = ExpressionAnalyzer(expr, syntax, context).getActions(true);
+
+        pipe.addSimpleTransform(std::make_shared<FilterTransform>(pipe.getHeader(), expression_actions, expr->getColumnName(), false));
+        /// TODO: maybe need remove sign columns
+    }*/
+
+//    return pipes;
 }
 
 }
