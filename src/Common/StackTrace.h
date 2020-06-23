d9f5133a62271f2d97f50624bde874b4970410a0
Ivan Blinkov <github@blinkov.ru>
2020-06-24 00:27:04 +0300

    send build_id to sentry as well + fix some minor issues

--- src/Common/StackTrace.h
+++ src/Common/StackTrace.h
@@ -54,7 +54,7 @@ public:
     std::string toString() const;
 
     static std::string toString(void ** frame_pointers, size_t offset, size_t size);
-    static void symbolize(const void * const * frame_pointers, size_t offset, size_t size, StackTrace::Frames & frames);
+    static void symbolize(const FramePointers & frame_pointers, size_t offset, size_t size, StackTrace::Frames & frames);
 
     void toStringEveryLine(std::function<void(const std::string &)> callback) const;
 protected:
