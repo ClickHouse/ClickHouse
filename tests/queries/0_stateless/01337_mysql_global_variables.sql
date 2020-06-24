db4e23795d40321c1d0eb6b2b7b3613589099ca1
BohuTANG <overred.shuttler@gmail.com>
2020-06-24 15:05:50 +0800

    add column header to test

--- tests/queries/0_stateless/01337_mysql_global_variables.sql
+++ tests/queries/0_stateless/01337_mysql_global_variables.sql
@@ -1,5 +1,4 @@
 SELECT @@test; -- empty string
-SELECT @@max_allowed_packet;
-SELECT @@MAX_ALLOWED_PACKET;
-SELECT @@max_allowed_packet, number FROM system.numbers LIMIT 3;
-SHOW VARIABLES LIKE 'lower_case_table_names';
+SELECT @@max_allowed_packet FORMAT CSVWithNames;
+SELECT @@MAX_ALLOWED_PACKET FORMAT CSVWithNames;
+SELECT @@max_allowed_packet, number FROM system.numbers LIMIT 3 FORMAT CSVWithNames;
