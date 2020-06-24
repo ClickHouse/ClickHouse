4ee08e9d112e99cfa7f02a70459961080dbdff31
Alexander Tokmakov <avtokmakov@yandex-team.ru>
2020-06-25 00:36:18 +0300

    fix query parameter visitor

--- tests/queries/0_stateless/01015_insert_values_parametrized.sh
+++ tests/queries/0_stateless/01015_insert_values_parametrized.sh
@@ -15,6 +15,9 @@ $CLICKHOUSE_CLIENT --input_format_values_deduce_templates_of_expressions=1 --inp
 $CLICKHOUSE_CLIENT --input_format_values_deduce_templates_of_expressions=0 --input_format_values_interpret_expressions=1 --param_p_n="-1" --param_p_s="param" --param_p_a="[0.2,0.3]" --query="INSERT INTO insert_values_parametrized  VALUES \
 (5 + {p_n:Int8}, lower(concat('Evaluate', {p_s:String})), arrayIntersect([0, 0.2, 0.6], {p_a:Array(Nullable(Float32))}))"
 
+$CLICKHOUSE_CLIENT --param_p_n="5" --param_p_s="param" --param_p_a="[0.2,0.3]" --query="INSERT INTO insert_values_parametrized  VALUES \
+({p_n:Int8}, {p_s:String}, {p_a:Array(Nullable(Float32))})"
+
 $CLICKHOUSE_CLIENT --query="SELECT * FROM insert_values_parametrized ORDER BY n";
 
 $CLICKHOUSE_CLIENT --query="DROP TABLE insert_values_parametrized";
