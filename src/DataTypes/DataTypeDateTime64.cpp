abee8f0ade2f0258bfbb47bf3980b5b1d9d5af81
alexey-milovidov <milovidov@yandex-team.ru>
2020-06-24 12:54:04 +0300

    Merge pull request #11875 from ClickHouse/fix-fpe-datetime64
    Fix strange and wrong code around DateTime64
    
    (cherry picked from commit c2fba5179bca2fa7a83f36dd85e7f748d1579637)

--- src/DataTypes/DataTypeDateTime64.cpp
+++ src/DataTypes/DataTypeDateTime64.cpp
@@ -20,19 +20,34 @@
 #include <optional>
 #include <string>
 
+
 namespace DB
 {
+
+namespace ErrorCodes
+{
+    extern const int ARGUMENT_OUT_OF_BOUND;
+}
+
+static constexpr UInt32 max_scale = 9;
 
 DataTypeDateTime64::DataTypeDateTime64(UInt32 scale_, const std::string & time_zone_name)
     : DataTypeDecimalBase<DateTime64>(DecimalUtils::maxPrecision<DateTime64>(), scale_),
       TimezoneMixin(time_zone_name)
 {
+    if (scale > max_scale)
+        throw Exception("Scale " + std::to_string(scale) + " is too large for DateTime64. Maximum is up to nanoseconds (9).",
+            ErrorCodes::ARGUMENT_OUT_OF_BOUND);
 }
 
 DataTypeDateTime64::DataTypeDateTime64(UInt32 scale_, const TimezoneMixin & time_zone_info)
-    : DataTypeDecimalBase<DateTime64>(DecimalUtils::maxPrecision<DateTime64>() - scale_, scale_),
+    : DataTypeDecimalBase<DateTime64>(DecimalUtils::maxPrecision<DateTime64>(), scale_),
       TimezoneMixin(time_zone_info)
-{}
+{
+    if (scale > max_scale)
+        throw Exception("Scale " + std::to_string(scale) + " is too large for DateTime64. Maximum is up to nanoseconds (9).",
+            ErrorCodes::ARGUMENT_OUT_OF_BOUND);
+}
 
 std::string DataTypeDateTime64::doGetName() const
 {
