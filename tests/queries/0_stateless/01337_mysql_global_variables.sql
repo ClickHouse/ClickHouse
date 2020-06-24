9f85521b23cbe48e0ab91a7b3dff1b9f2ae1c7a9
BohuTANG <overred.shuttler@gmail.com>
2020-06-24 20:09:32 +0800

    Fix @@session.variables AS

--- tests/queries/0_stateless/01337_mysql_global_variables.sql
+++ tests/queries/0_stateless/01337_mysql_global_variables.sql
@@ -1,5 +1,6 @@
-SELECT @@test; -- empty string
+SELECT @@test;
 SELECT @@max_allowed_packet FORMAT CSVWithNames;
 SELECT @@MAX_ALLOWED_PACKET FORMAT CSVWithNames;
 SELECT @@max_allowed_packet, number FROM system.numbers LIMIT 3 FORMAT CSVWithNames;
 SELECT @@session.auto_increment_increment FORMAT CSVWithNames;
+SELECT @@session.auto_increment_increment AS auto_increment_increment FORMAT CSVWithNames;
