b8116a7059dfa5e6afbbdf01abc59ce731e771a3
Alexey Milovidov <milovidov@yandex-team.ru>
2020-06-25 22:46:02 +0300

    Minor modification

--- tests/queries/0_stateless/01343_min_bytes_to_use_mmap_io.sql
+++ tests/queries/0_stateless/01343_min_bytes_to_use_mmap_io.sql
@@ -1,11 +1,11 @@
-DROP TABLE IF EXISTS test;
-CREATE TABLE test (x String) ENGINE = MergeTree ORDER BY tuple();
-INSERT INTO test VALUES ('Hello, world');
+DROP TABLE IF EXISTS test_01343;
+CREATE TABLE test_01343 (x String) ENGINE = MergeTree ORDER BY tuple();
+INSERT INTO test_01343 VALUES ('Hello, world');
 
 SET min_bytes_to_use_mmap_io = 1;
-SELECT * FROM test;
+SELECT * FROM test_01343;
 
 SYSTEM FLUSH LOGS;
-SELECT PE.Values FROM system.query_log ARRAY JOIN ProfileEvents AS PE WHERE event_date >= yesterday() AND event_time >= now() - 300 AND query LIKE 'SELECT * FROM test%' AND PE.Names = 'CreatedReadBufferMMap' AND type = 2 ORDER BY event_time DESC LIMIT 1;
+SELECT PE.Values FROM system.query_log ARRAY JOIN ProfileEvents AS PE WHERE event_date >= yesterday() AND event_time >= now() - 300 AND query LIKE 'SELECT * FROM test_01343%' AND PE.Names = 'CreatedReadBufferMMap' AND type = 2 ORDER BY event_time DESC LIMIT 1;
 
-DROP TABLE test;
+DROP TABLE test_01343;
