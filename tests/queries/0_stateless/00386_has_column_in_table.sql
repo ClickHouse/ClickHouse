77964a08bdfecb0a0fa3ccb3e546a117b6e43b23
Alexander Tokmakov <avtokmakov@yandex-team.ru>
2020-06-26 12:51:23 +0300

    Cherry pick #11934 to 20.5: Fix some logical errors

--- tests/queries/0_stateless/00386_has_column_in_table.sql
+++ tests/queries/0_stateless/00386_has_column_in_table.sql
@@ -18,5 +18,14 @@ SELECT hasColumnInTable(currentDatabase(), 'has_column_in_table', 'nest.not_exis
 SELECT hasColumnInTable('localhost', currentDatabase(), 'has_column_in_table', 'nest.not_existing');
 SELECT hasColumnInTable(currentDatabase(), 'has_column_in_table', 'not_existing');
 SELECT hasColumnInTable('localhost', currentDatabase(), 'has_column_in_table', 'not_existing');
+SELECT hasColumnInTable('system', 'one', '');
+
+/* bad queries */
+SELECT hasColumnInTable('', '', '');  -- { serverError 60; }
+SELECT hasColumnInTable('', 't', 'c');  -- { serverError 81; }
+SELECT hasColumnInTable(currentDatabase(), '', 'c'); -- { serverError 60; }
+SELECT hasColumnInTable('d', 't', 's');  -- { serverError 81; }
+SELECT hasColumnInTable(currentDatabase(), 't', 's');  -- { serverError 60; }
+
 
 DROP TABLE has_column_in_table;
