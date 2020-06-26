4ebd15d9562037b5876094450fab09b414825149
Alexey Milovidov <milovidov@yandex-team.ru>
2020-06-26 23:40:29 +0300

    Update test

--- tests/queries/0_stateless/01344_alter_enum_partition_key.sql
+++ tests/queries/0_stateless/01344_alter_enum_partition_key.sql
@@ -11,6 +11,10 @@ OPTIMIZE TABLE test FINAL;
 SELECT * FROM test ORDER BY x;
 SELECT name, partition, partition_id FROM system.parts WHERE database = currentDatabase() AND table = 'test' AND active ORDER BY partition;
 
+ALTER TABLE test MODIFY COLUMN x Enum('hello' = 1, 'world' = 2); -- { serverError 524 }
+ALTER TABLE test MODIFY COLUMN x Enum('hello' = 1, 'world' = 2, 'test' = 3); -- { serverError 524 }
+ALTER TABLE test MODIFY COLUMN x Enum('hello' = 1, 'world' = 2, 'goodbye' = 4); -- { serverError 524 }
+
 ALTER TABLE test MODIFY COLUMN x Int8;
 INSERT INTO test VALUES (111, 'abc');
 OPTIMIZE TABLE test FINAL;
@@ -19,28 +23,6 @@ SELECT name, partition, partition_id FROM system.parts WHERE database = currentD
 
 ALTER TABLE test MODIFY COLUMN x Enum8('' = 1); -- { serverError 524 }
 ALTER TABLE test MODIFY COLUMN x Enum16('' = 1); -- { serverError 524 }
-
-ALTER TABLE test MODIFY COLUMN x Int16;
-INSERT INTO test VALUES (-1111, 'def');
-OPTIMIZE TABLE test FINAL;
-SELECT * FROM test ORDER BY x;
-SELECT name, partition, partition_id FROM system.parts WHERE database = currentDatabase() AND table = 'test' AND active ORDER BY partition;
-
-ALTER TABLE test MODIFY COLUMN x Date; -- { serverError 524 }
-
-ALTER TABLE test MODIFY COLUMN x Int32;
-INSERT INTO test VALUES (-11112222, 'ghi');
-OPTIMIZE TABLE test FINAL;
-SELECT * FROM test ORDER BY x;
-SELECT name, partition, partition_id FROM system.parts WHERE database = currentDatabase() AND table = 'test' AND active ORDER BY partition;
-
-ALTER TABLE test MODIFY COLUMN x DateTime; -- { serverError 524 }
-
-ALTER TABLE test MODIFY COLUMN x Int64;
-INSERT INTO test VALUES (-1111222233334444, 'jkl');
-OPTIMIZE TABLE test FINAL;
-SELECT * FROM test ORDER BY x;
-SELECT name, partition, partition_id FROM system.parts WHERE database = currentDatabase() AND table = 'test' AND active ORDER BY partition;
 
 ALTER TABLE test MODIFY COLUMN x UInt64; -- { serverError 524 }
 ALTER TABLE test MODIFY COLUMN x String; -- { serverError 524 }
