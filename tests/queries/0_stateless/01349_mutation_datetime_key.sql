fbadd159879069d133a0640add6c7e56e207b631
Alexey Milovidov <milovidov@yandex-team.ru>
2020-06-28 00:19:55 +0300

    Update test

--- tests/queries/0_stateless/01349_mutation_datetime_key.sql
+++ tests/queries/0_stateless/01349_mutation_datetime_key.sql
@@ -15,7 +15,7 @@ INSERT INTO cdp_orders VALUES ('hello', 'world', '2020-01-02 03:04:05');
 
 SELECT * FROM cdp_orders;
 SET mutations_sync = 1;
-ALTER TABLE default.cdp_orders DELETE WHERE order_time >= '2019-12-03 00:00:00';
+ALTER TABLE cdp_orders DELETE WHERE order_time >= '2019-12-03 00:00:00';
 SELECT * FROM cdp_orders;
 
 DROP TABLE cdp_orders;
