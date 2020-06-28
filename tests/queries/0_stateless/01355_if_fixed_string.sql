bf12006a956cf2c15724667ad837a237626ca7b7
Alexey Milovidov <milovidov@yandex-team.ru>
2020-06-29 00:10:34 +0300

    Fix function if with FixedString arguments of different sizes

---
+++ tests/queries/0_stateless/01355_if_fixed_string.sql
@@ -0,0 +1,2 @@
+SELECT if(number % 2, toFixedString(toString(number), 2), toFixedString(toString(-number), 5)) AS x, toTypeName(x) FROM system.numbers LIMIT 10;
+SELECT if(number % 2, toFixedString(toString(number), 2), toFixedString(toString(-number), 2)) AS x, toTypeName(x) FROM system.numbers LIMIT 10;
