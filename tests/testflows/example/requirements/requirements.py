321bad14eeb40f542644825d2ba65771d16a38ff
Vitaliy Zakaznikov <vzakaznikov@altinity.com>
2020-07-02 20:41:28 +0200

    Adding a template of the SRS with the notes how to use it.

--- tests/testflows/example/requirements/requirements.py
+++ tests/testflows/example/requirements/requirements.py
@@ -1,9 +1,66 @@
+# These requirements were auto generated
+# from software requirements specification (SRS)
+# document by TestFlows v1.6.200627.1211752.
+# Do not edit by hand but re-generate instead
+# using 'tfs requirements generate' command.
 from testflows.core import Requirement
 
-RQ_ClickHouse_Select_1 = Requirement(
-        name='RQ.ClickHouse.Select.1',
+RQ_CH_SRS001_Example_short_description_of_the_requirement_ = Requirement(
+        name='RQ.CH-SRS001.Example short description of the requirement ',
         version='1.0',
+        priority=None,
+        group=None,
+        type=None,
+        uid=None,
         description=(
-        '[ClickHouse] SHALL return `1` when user sends a simple `SELECT 1` query.\n'
-        )
+        'This is a long description of the requirement that can include any\n'
+        'relevant information. \n'
+        '\n'
+        'The one-line block that follows the requirement defines the `version` \n'
+        'of the requirement. The version is controlled manually and is used\n'
+        'to indicate material changes to the requirement that would \n'
+        'require tests that cover this requirement to be updated.\n'
+        '\n'
+        'It is a good practice to use requirement names that are broken\n'
+        'up into groups. It is not recommended to use only numbers\n'
+        'because if the requirement must be moved the numbering will not match.\n'
+        'Therefore, the requirement name should start with the group\n'
+        'name which is then followed by a number if any. For example,\n'
+        '\n'
+        '    RQ.SRS001.Group.Subgroup.1\n'
+        '\n'
+        "To keep names short, try to use abbreviations for the requirement's group name.\n"
+        ),
+        link=None
+    )
+
+RQ_CH_SRS001_Example_Subgroup_short_description_of_the_sub_requirement = Requirement(
+        name='RQ.CH-SRS001.Example.Subgroup short description of the sub-requirement',
+        version='1.0',
+        priority=None,
+        group=None,
+        type=None,
+        uid=None,
+        description=(
+        'This an example of a sub-requirement of the [RQ.CH-SRS001.Example](#rqch-srs001example).\n'
+        'CH_SRS001_Software_Requirements_Template.md\n'
+        ),
+        link=None
+    )
+
+RQ_CH_SRS001_Example_Select_1 = Requirement(
+        name='RQ.CH-SRS001.Example.Select.1',
+        version='1.0',
+        priority=None,
+        group=None,
+        type=None,
+        uid=None,
+        description=(
+        '[ClickHouse] SHALL return `1` when user executes query\n'
+        '\n'
+        '```sql\n'
+        'SELECT 1\n'
+        '```\n'
+        ),
+        link=None
     )
