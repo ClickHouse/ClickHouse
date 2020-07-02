ff1b6b0b398eaf98835d74848a42303eb19e0671
Vitaliy Zakaznikov <vzakaznikov@altinity.com>
2020-07-02 17:58:13 +0200

    Adding a simple example of using TestFlows.

---
+++ tests/testflows/example/requirements/requirements.py
@@ -0,0 +1,9 @@
+from testflows.core import Requirement
+
+RQ_ClickHouse_Select_1 = Requirement(
+        name='RQ.ClickHouse.Select.1',
+        version='1.0',
+        description=(
+        '[ClickHouse] SHALL return `1` when user sends a simple `SELECT 1` query.\n'
+        )
+    )
