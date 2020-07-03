9f596c8bbad86ea932285af6cb76d84762889c0e
Nikolai Kochetov <KochetovNicolai@users.noreply.github.com>
2020-07-03 10:35:04 +0300

    Merge pull request #12085 from ClickHouse/fix-11937
    Fix limiting the number of threads for VIEW.
    
    (cherry picked from commit 17c89f3fe0f85e293fb5a5ec907491068c1ab52d)

--- src/Interpreters/InterpreterSelectQuery.cpp
+++ src/Interpreters/InterpreterSelectQuery.cpp
@@ -1538,7 +1538,7 @@ void InterpreterSelectQuery::executeFetchColumns(
 
         if constexpr (pipeline_with_processors)
         {
-            if (streams.size() == 1 || pipes.size() == 1)
+            if (!storage->isView() && (streams.size() == 1 || pipes.size() == 1))
                 pipeline.setMaxThreads(1);
 
             /// Unify streams. They must have same headers.
