919c5754e404b0e7714362102405a81d030f8409
Alexey Milovidov <milovidov@yandex-team.ru>
2020-07-04 16:54:24 +0300

    Normalize "pid" file handling #3501

--- src/Common/StatusFile.h
+++ src/Common/StatusFile.h
@@ -1,11 +1,14 @@
 #pragma once
 
 #include <string>
+#include <functional>
 #include <boost/noncopyable.hpp>
 
 
 namespace DB
 {
+
+class WriteBuffer;
 
 
 /** Provides that no more than one server works with one data directory.
@@ -13,11 +16,18 @@ namespace DB
 class StatusFile : private boost::noncopyable
 {
 public:
-    explicit StatusFile(const std::string & path_);
+    using FillFunction = std::function<void(WriteBuffer&)>;
+
+    StatusFile(std::string path_, FillFunction fill_);
     ~StatusFile();
+
+    /// You can use one of these functions to fill the file or provide your own.
+    static FillFunction write_pid;
+    static FillFunction write_full_info;
 
 private:
     const std::string path;
+    FillFunction fill;
     int fd = -1;
 };
 
