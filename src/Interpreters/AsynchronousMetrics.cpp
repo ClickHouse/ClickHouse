d68ecdc84c121a628a18945dac4e3b35f2b643dd
Azat Khuzhin <a3at.mail@gmail.com>
2020-07-07 01:43:39 +0300

    Cap max_memory_usage* limits to the process resident memory
    There are still some issues with memory tracking, but now with per-user
    tracking:
    
        executeQuery: Code: 241, e.displayText() = DB::Exception: Memory limit (for user) exceeded: would use 437.72 GiB (attempt to allocate chunk of 4200926 bytes), maximum: 437.72 GiB (version 20.6.1.1) (from 10.7.140.7:31318)
    
    Although the server is mostly idle:
    
        SELECT formatReadableSize(memory_usage)
        FROM system.processes
    
        ┌─formatReadableSize(memory_usage)─┐
        │ 289.28 MiB                       │
        │ 155.75 MiB                       │
        │ 0.00 B                           │
        └──────────────────────────────────┘
    
    Refs: https://github.com/ClickHouse/ClickHouse/pull/10496/files#r450206865
    Cc: @alexey-milovidov

--- src/Interpreters/AsynchronousMetrics.cpp
+++ src/Interpreters/AsynchronousMetrics.cpp
@@ -208,7 +208,6 @@ void AsynchronousMetrics::update()
         /// Otherwise it might be calculated incorrectly - it can include a "drift" of memory amount.
         /// See https://github.com/ClickHouse/ClickHouse/issues/10293
         total_memory_tracker.set(data.resident);
-        CurrentMetrics::set(CurrentMetrics::MemoryTracking, data.resident);
     }
 #endif
 
