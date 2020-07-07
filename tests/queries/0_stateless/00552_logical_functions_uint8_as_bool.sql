66128acd113bbcaf8c43bac9de13b39093f6775b
Alexander Kazakov <Akazz@users.noreply.github.com>
2020-07-07 12:17:35 +0300

    Included const uint8 values in test

--- tests/queries/0_stateless/00552_logical_functions_uint8_as_bool.sql
+++ tests/queries/0_stateless/00552_logical_functions_uint8_as_bool.sql
@@ -1,6 +1,15 @@
 
 -- Test that UInt8 type is processed correctly as bool
 
+SELECT
+    1 AND 2,
+    2 AND 4,
+    1 AND 2 AND 4,
+    1 OR 2,
+    2 OR 4,
+    1 OR 2 OR 4
+;
+
 SELECT 
     toUInt8(bitAnd(number, 4)) AS a,
     toUInt8(bitAnd(number, 2)) AS b,
