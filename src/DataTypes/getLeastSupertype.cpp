faea28195ec7936261850395a5152cf526212198
alexey-milovidov <milovidov@yandex-team.ru>
2020-07-08 14:27:48 +0300

    Merge pull request #12275 from ClickHouse/supertype-low-cardinality
    Supertype for LowCardinality
    
    (cherry picked from commit 3f0c2573899c3181104f911a9e8308c24242eb93)

--- src/DataTypes/getLeastSupertype.cpp
+++ src/DataTypes/getLeastSupertype.cpp
@@ -9,6 +9,7 @@
 #include <DataTypes/DataTypeArray.h>
 #include <DataTypes/DataTypeTuple.h>
 #include <DataTypes/DataTypeNullable.h>
+#include <DataTypes/DataTypeLowCardinality.h>
 #include <DataTypes/DataTypeNothing.h>
 #include <DataTypes/DataTypeString.h>
 #include <DataTypes/DataTypeDateTime.h>
@@ -157,6 +158,39 @@ DataTypePtr getLeastSupertype(const DataTypes & types)
                 common_tuple_types[elem_idx] = getLeastSupertype(nested_types[elem_idx]);
 
             return std::make_shared<DataTypeTuple>(common_tuple_types);
+        }
+    }
+
+    /// For LowCardinality. This is above Nullable, because LowCardinality can contain Nullable but cannot be inside Nullable.
+    {
+        bool have_low_cardinality = false;
+        bool have_not_low_cardinality = false;
+
+        DataTypes nested_types;
+        nested_types.reserve(types.size());
+
+        for (const auto & type : types)
+        {
+            if (const DataTypeLowCardinality * type_low_cardinality = typeid_cast<const DataTypeLowCardinality *>(type.get()))
+            {
+                have_low_cardinality = true;
+                nested_types.emplace_back(type_low_cardinality->getDictionaryType());
+            }
+            else
+            {
+                have_not_low_cardinality = true;
+                nested_types.emplace_back(type);
+            }
+        }
+
+        /// All LowCardinality gives LowCardinality.
+        /// LowCardinality with high cardinality gives high cardinality.
+        if (have_low_cardinality)
+        {
+            if (have_not_low_cardinality)
+                return getLeastSupertype(nested_types);
+            else
+                return std::make_shared<DataTypeLowCardinality>(getLeastSupertype(nested_types));
         }
     }
 
