456fc88e1a34c1c867746ca3c95925237ae22b12
alexey-milovidov <milovidov@yandex-team.ru>
2020-07-11 00:21:58 +0300

    Update 01383_remote_ambiguous_column_shard.sql

--- tests/queries/0_stateless/01383_remote_ambiguous_column_shard.sql
+++ tests/queries/0_stateless/01383_remote_ambiguous_column_shard.sql
@@ -1,12 +1,11 @@
-DROP TABLE IF EXISTS fact;
-DROP TABLE IF EXISTS dimension;
+DROP DATABASE IF EXISTS test_01383;
+CREATE DATABASE test_01383;
 
-create table fact (id1 Int64, id2 Int64, value Int64) ENGINE = MergeTree() ORDER BY id1;
-create table dimension (id1 Int64, name String) ENGINE = MergeTree() ORDER BY id1;
-insert into fact values (1,2,10),(2,2,10),(3,3,10),(4,3,10);
-insert into dimension values (1,'name_1'),(2,'name_1'),(3,'name_3'),(4, 'name_4');
+create table test_01383.fact (id1 Int64, id2 Int64, value Int64) ENGINE = MergeTree() ORDER BY id1;
+create table test_01383.dimension (id1 Int64, name String) ENGINE = MergeTree() ORDER BY id1;
+insert into test_01383.fact values (1,2,10),(2,2,10),(3,3,10),(4,3,10);
+insert into test_01383.dimension values (1,'name_1'),(2,'name_1'),(3,'name_3'),(4, 'name_4');
 
-SELECT f.id1 AS ID, d.name AS Name, sum(f.value) FROM remote('127.0.0.{1,2,3}', currentDatabase(), fact) AS f LEFT JOIN dimension AS d ON f.id1 = d.id1 WHERE f.id1 = f.id2 GROUP BY ID, Name;
+SELECT f.id1 AS ID, d.name AS Name, sum(f.value) FROM remote('127.0.0.{1,2,3}', test_01383.fact) AS f LEFT JOIN test_01383.dimension AS d ON f.id1 = d.id1 WHERE f.id1 = f.id2 GROUP BY ID, Name;
 
-DROP TABLE fact;
-DROP TABLE dimension;
+DROP DATABASE test_01383;
