836c5a44cece3dd5364c9c90cbd8fdeefc8983a8
Yatsishin Ilya <2159081+qoega@users.noreply.github.com>
2020-07-10 14:12:00 +0300

    test_settings_constraints fix

--- tests/integration/test_settings_constraints/test.py
+++ tests/integration/test_settings_constraints/test.py
@@ -2,8 +2,7 @@ import pytest
 from helpers.cluster import ClickHouseCluster
 
 cluster = ClickHouseCluster(__file__)
-instance = cluster.add_instance('instance',
-                                config_dir="configs")
+instance = cluster.add_instance('instance', user_configs=["configs/users.xml"])
 
 
 
