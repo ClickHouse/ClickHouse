b55e6746d1c5906a9f7c76cf9005463a843f5352
Artem Zuikov <chertus@gmail.com>
2020-07-15 14:58:03 +0300

    standard join type syntax position

---
+++ tests/queries/0_stateless/01332_join_type_syntax_position.sql
@@ -0,0 +1,31 @@
+select * from numbers(1) t1 left outer join numbers(1) t2 using number;
+select * from numbers(1) t1 right outer join numbers(1) t2 using number;
+
+select * from numbers(1) t1 left any join numbers(1) t2 using number;
+select * from numbers(1) t1 right any join numbers(1) t2 using number;
+
+select * from numbers(1) t1 left semi join numbers(1) t2 using number;
+select * from numbers(1) t1 right semi join numbers(1) t2 using number;
+
+select * from numbers(1) t1 left anti join numbers(1) t2 using number;
+select * from numbers(1) t1 right anti join numbers(1) t2 using number;
+
+select * from numbers(1) t1 asof join numbers(1) t2 using number; -- { serverError 62 }
+select * from numbers(1) t1 left asof join numbers(1) t2 using number; -- { serverError 62 }
+
+-- legacy
+
+select * from numbers(1) t1 all left join numbers(1) t2 using number;
+select * from numbers(1) t1 all right join numbers(1) t2 using number;
+
+select * from numbers(1) t1 any left join numbers(1) t2 using number;
+select * from numbers(1) t1 any right join numbers(1) t2 using number;
+
+select * from numbers(1) t1 semi left join numbers(1) t2 using number;
+select * from numbers(1) t1 semi right join numbers(1) t2 using number;
+
+select * from numbers(1) t1 anti left join numbers(1) t2 using number;
+select * from numbers(1) t1 anti right join numbers(1) t2 using number;
+
+select * from numbers(1) t1 asof join numbers(1) t2 using number; -- { serverError 62 }
+select * from numbers(1) t1 asof left join numbers(1) t2 using number; -- { serverError 62 }
