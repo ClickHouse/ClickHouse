bbc957802b0c50d38e3f97b5c62dbcac552efdfa
alesapin <alesapin@gmail.com>
2020-07-16 12:10:15 +0300

    Fix incorrect adaptive granularity by default

--- tests/integration/test_adaptive_granularity_different_settings/test.py
+++ tests/integration/test_adaptive_granularity_different_settings/test.py
@@ -6,6 +6,9 @@ cluster = ClickHouseCluster(__file__)
 node1 = cluster.add_instance('node1', with_zookeeper=True)
 node2 = cluster.add_instance('node2', with_zookeeper=True)
 
+# no adaptive granularity by default
+node3 = cluster.add_instance('node3', image='yandex/clickhouse-server:19.9.5.36', with_installed_binary=True, stay_alive=True)
+
 @pytest.fixture(scope="module")
 def start_cluster():
     try:
@@ -47,3 +50,29 @@ def test_attach_detach(start_cluster):
 
     assert node1.query("SELECT COUNT() FROM  test") == "4\n"
     assert node2.query("SELECT COUNT() FROM  test") == "4\n"
+
+
+def test_mutate_with_mixed_granularity(start_cluster):
+    node3.query("""
+        CREATE TABLE test (date Date, key UInt64, value1 String, value2 String)
+        ENGINE = MergeTree
+        ORDER BY key PARTITION BY date""")
+
+    node3.query("INSERT INTO test SELECT toDate('2019-10-01') + number % 5, number, toString(number), toString(number * number) FROM numbers(500)")
+
+    assert node3.query("SELECT COUNT() FROM test") == "500\n"
+
+    node3.restart_with_latest_version()
+
+    assert node3.query("SELECT COUNT() FROM test") == "500\n"
+
+    node3.query("ALTER TABLE test MODIFY SETTING enable_mixed_granularity_parts = 1")
+
+    node3.query("INSERT INTO test SELECT toDate('2019-10-01') + number % 5, number, toString(number), toString(number * number) FROM numbers(500, 500)")
+
+    assert node3.query("SELECT COUNT() FROM test") == "1000\n"
+    assert node3.query("SELECT COUNT() FROM test WHERE key % 100 == 0") == "10\n"
+
+    node3.query("ALTER TABLE test DELETE WHERE key % 100 == 0", settings={"mutations_sync": "2"})
+
+    assert node3.query("SELECT COUNT() FROM test WHERE key % 100 == 0") == "0\n"
