d68153a1d377b006e48cfb97e7bbe8392d663684
alexey-milovidov <milovidov@yandex-team.ru>
2020-07-16 11:01:55 +0300

    Merge pull request #12529 from CurtizJ/fix-12521
    Better exception for function 'in' with invalid number of arguments
    
    (cherry picked from commit 6e9af5f17919b17eca7c6ad10b689233e4204432)

--- src/Interpreters/misc.h
+++ src/Interpreters/misc.h
@@ -1,7 +1,14 @@
 #pragma once
 
+#include <Common/StringUtils/StringUtils.h>
+#include <Parsers/ASTFunction.h>
 namespace DB
 {
+
+namespace ErrorCodes
+{
+    extern const int NUMBER_OF_ARGUMENTS_DOESNT_MATCH;
+}
 
 inline bool functionIsInOperator(const std::string & name)
 {
@@ -16,6 +23,21 @@ inline bool functionIsInOrGlobalInOperator(const std::string & name)
 inline bool functionIsLikeOperator(const std::string & name)
 {
     return name == "like" || name == "notLike";
+}
+
+inline bool checkFunctionIsInOrGlobalInOperator(const ASTFunction & func)
+{
+    if (functionIsInOrGlobalInOperator(func.name))
+    {
+        size_t num_arguments = func.arguments->children.size();
+        if (num_arguments != 2)
+            throw Exception("Wrong number of arguments passed to function in. Expected: 2, passed: " + std::to_string(num_arguments),
+                ErrorCodes::NUMBER_OF_ARGUMENTS_DOESNT_MATCH);
+
+        return true;
+    }
+
+    return false;
 }
 
 }
