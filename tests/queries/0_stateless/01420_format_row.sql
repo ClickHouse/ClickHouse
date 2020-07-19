b30240efc0dadf2451d6cf61d17245abefa66f45
Amos Bird <amosbird@gmail.com>
2020-07-19 10:55:08 +0800

    formatRow function

---
+++ tests/queries/0_stateless/01420_format_row.sql
@@ -0,0 +1,10 @@
+select formatRow('CSV', number, 'good') from numbers(3);
+select formatRowNoNewline('TSV', number, DATE '2001-12-12', 1.4) from numbers(3);
+select formatRow('JSONEachRow', number, toNullable(3), Null) from numbers(3);
+select formatRowNoNewline('JSONEachRow', *) from numbers(3);
+
+-- unknown format
+select formatRow('aaa', *) from numbers(3); -- { serverError 73 }
+
+-- unsupported format
+select formatRowNoNewline('Values', *) from numbers(3); -- { serverError 48 }
