8cd51af951d0fea1c6ebce3eac06e0d61d01b84f
Ilya Golshtein <gie@arenadata.io>
2020-07-22 22:04:33 +0000

    Built against submodules

---
+++ contrib/krb5-cmake/krb5/krb5.h
@@ -0,0 +1,12 @@
+#ifndef KRB5_KRB5_H_INCLUDED
+#define KRB5_KRB5_H_INCLUDED
+
+#include "krb5/krb5.hin"
+#include "lib/krb5/error_tables/krb5_err.h"
+#include "lib/krb5/error_tables/k5e1_err.h"
+#include "lib/krb5/error_tables/kdb5_err.h"
+#include "lib/krb5/error_tables/kv5m_err.h"
+#include "lib/krb5/error_tables/krb524_err.h"
+#include "lib/krb5/error_tables/asn1_err.h"
+
+#endif /* KRB5_KRB5_H_INCLUDED */
