4736636bf42e1497b4f8dfd2ed92b598681bf299
Ivan Lezhankin <ilezhankin@yandex-team.ru>
2020-07-22 12:53:55 +0300

    Refactor cmake lists for simdjson

--- cmake/find/simdjson.cmake
+++
@@ -1,8 +0,0 @@
-if (NOT EXISTS "${ClickHouse_SOURCE_DIR}/contrib/simdjson/include/simdjson.h")
-    message (WARNING "submodule contrib/simdjson is missing. to fix try run: \n git submodule update --init --recursive")
-    return()
-endif ()
-
-option (USE_SIMDJSON "Use simdjson" ON)
-
-message(STATUS "Using simdjson=${USE_SIMDJSON}")
