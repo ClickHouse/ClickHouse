104e1b57fabff6382d556e5fb5634ce3581a6b34
philip.han <philip.han@kakaocorp.com>
2020-07-24 00:49:32 +0900

    Remove extra ';' after member function definition

--- src/Functions/abtesting.cpp
+++ src/Functions/abtesting.cpp
@@ -213,7 +213,7 @@ public:
         }
 
         return true;
-    };
+    }
 
     void executeImpl(Block & block, const ColumnNumbers & arguments, size_t result, size_t input_rows_count) const override
     {
