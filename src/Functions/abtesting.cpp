272e909cf402d5dfbe3f4960321fa46cf7a7906a
philip.han <philip.han@kakaocorp.com>
2020-07-23 22:48:37 +0900

    Make IFunction::executeImpl const.

--- src/Functions/abtesting.cpp
+++ src/Functions/abtesting.cpp
@@ -215,7 +215,7 @@ public:
         return true;
     };
 
-    void executeImpl(Block & block, const ColumnNumbers & arguments, size_t result, size_t input_rows_count) override
+    void executeImpl(Block & block, const ColumnNumbers & arguments, size_t result, size_t input_rows_count) const override
     {
         if (input_rows_count == 0)
         {
