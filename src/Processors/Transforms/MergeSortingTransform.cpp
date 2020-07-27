de1bea4d491e39d4748b78160f95fd50a6327dbd
robot-clickhouse <robot-clickhouse@yandex-team.ru>
2020-07-27 18:16:59 +0300

    Backport #12618 to 20.3: Fix external sort pipeline stuck

--- src/Processors/Transforms/MergeSortingTransform.cpp
+++ src/Processors/Transforms/MergeSortingTransform.cpp
@@ -228,9 +228,8 @@ void MergeSortingTransform::generate()
             ProfileEvents::increment(ProfileEvents::ExternalSortMerge);
             LOG_INFO(log, "There are " << temporary_files.size() << " temporary sorted parts to merge.");
 
-            if (!chunks.empty())
-                processors.emplace_back(std::make_shared<MergeSorterSource>(
-                        header_without_constants, std::move(chunks), description, max_merged_block_size, limit));
+            processors.emplace_back(std::make_shared<MergeSorterSource>(
+                    header_without_constants, std::move(chunks), description, max_merged_block_size, limit));
         }
 
         generated_prefix = true;
