cc3ac00869d963cfe79d8af1a90a30ed2eb36753
Yatsishin Ilya <2159081+qoega@users.noreply.github.com>
2020-07-27 19:52:08 +0300

    typo

--- docker/test/stateless_with_coverage/run.sh
+++ docker/test/stateless_with_coverage/run.sh
@@ -46,7 +46,6 @@ ln -s /usr/share/clickhouse-test/config/ints_dictionary.xml /etc/clickhouse-serv
     ln -s /usr/share/clickhouse-test/config/strings_dictionary.xml /etc/clickhouse-server/dict_examples/; \
     ln -s /usr/share/clickhouse-test/config/decimals_dictionary.xml /etc/clickhouse-server/dict_examples/;
 
-<<<<<<< HEAD
 ln -s /usr/share/clickhouse-test/config/zookeeper.xml /etc/clickhouse-server/config.d/
 ln -s /usr/share/clickhouse-test/config/listen.xml /etc/clickhouse-server/config.d/
 ln -s /usr/share/clickhouse-test/config/part_log.xml /etc/clickhouse-server/config.d/
