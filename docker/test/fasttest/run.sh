a6c951557252b69999d9f07aff63f55c64cf46ca
alesapin <alesapin@gmail.com>
2020-07-28 14:18:08 +0300

    Better

--- docker/test/fasttest/run.sh
+++ docker/test/fasttest/run.sh
@@ -94,7 +94,7 @@ TESTS_TO_SKIP="parquet avro h3 odbc mysql sha256 _orc_ arrow 01098_temporary_and
 
 clickhouse-test -j 4 --no-long --testname --shard --zookeeper --skip $TESTS_TO_SKIP 2>&1 | ts '%Y-%m-%d %H:%M:%S' | tee /test_output/test_log.txt
 
-pkill clickhouse-server
+killall clickhouse-server
 
 clickhouse-server --config /etc/clickhouse-server/config.xml --daemon
 
