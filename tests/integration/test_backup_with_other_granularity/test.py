1bece3de0aa7e431fbf2ba86ce161b0d46430f00
alesapin <alesapin@gmail.com>
2020-07-28 12:36:08 +0300

    Remove strange code and test

--- tests/integration/test_backup_with_other_granularity/test.py
+++ tests/integration/test_backup_with_other_granularity/test.py
@@ -8,6 +8,7 @@ cluster = ClickHouseCluster(__file__)
 node1 = cluster.add_instance('node1', with_zookeeper=True, image='yandex/clickhouse-server:19.4.5.35', stay_alive=True, with_installed_binary=True)
 node2 = cluster.add_instance('node2', with_zookeeper=True, image='yandex/clickhouse-server:19.4.5.35', stay_alive=True, with_installed_binary=True)
 node3 = cluster.add_instance('node3', with_zookeeper=True, image='yandex/clickhouse-server:19.4.5.35', stay_alive=True, with_installed_binary=True)
+node4 = cluster.add_instance('node4')
 
 
 @pytest.fixture(scope="module")
@@ -128,3 +129,24 @@ def test_backup_from_old_version_config(started_cluster):
     assert node3.query("SELECT sum(A) FROM dest_table") == "3\n"
 
     assert node1.query("CHECK TABLE dest_table") == "1\n"
+
+
+def test_backup_and_alter(started_cluster):
+    node4.query("CREATE TABLE backup_table(A Int64, B String, C Date) Engine = MergeTree order by tuple()")
+
+    node4.query("INSERT INTO backup_table VALUES(2, '2', toDate('2019-10-01'))")
+
+    node4.query("ALTER TABLE backup_table FREEZE PARTITION tuple();")
+
+    node4.query("ALTER TABLE backup_table DROP COLUMN C")
+
+    node4.query("ALTER TABLE backup_table MODIFY COLUMN B UInt64")
+
+    node4.query("ALTER TABLE backup_table DROP PARTITION tuple()")
+
+    node4.exec_in_container(['bash', '-c', 'cp -r /var/lib/clickhouse/shadow/1/data/default/backup_table/all_1_1_0/ /var/lib/clickhouse/data/default/backup_table/detached'])
+
+    node4.query("ALTER TABLE backup_table ATTACH PARTITION tuple()")
+
+    assert node4.query("SELECT sum(A) FROM backup_table") == "2\n"
+    assert node4.query("SELECT B + 2 FROM backup_table") == "4\n"
