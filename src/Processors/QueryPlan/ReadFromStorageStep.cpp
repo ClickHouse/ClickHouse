5667c2ad7cdbfe6c86ed15a7669d0091b8e71a76
Nikolai Kochetov <KochetovNicolai@users.noreply.github.com>
2020-07-28 12:03:07 +0300

    Merge pull request #12840 from ClickHouse/remove-wrong-threads-limiting
    Remove thread limiting.
    
    (cherry picked from commit 1f2390b65e921167dab6249ad9fa1ec29ab9bc91)

--- src/Processors/QueryPlan/ReadFromStorageStep.cpp
+++ src/Processors/QueryPlan/ReadFromStorageStep.cpp
@@ -111,9 +111,6 @@ ReadFromStorageStep::ReadFromStorageStep(
         }
     }
 
-    if (pipes.size() == 1)
-        pipeline->setMaxThreads(1);
-
     for (auto & pipe : pipes)
         pipe.enableQuota();
 
