383248a28efd05d485b4e37f145e77e20b902913
Vladimir Chebotarev <vladimir.chebotarev@gmail.com>
2020-07-30 04:17:33 +0300

    Returned `max_data_part_size` to `system.storage_policies`.

--- src/Storages/System/StorageSystemStoragePolicies.cpp
+++ src/Storages/System/StorageSystemStoragePolicies.cpp
@@ -1,7 +1,10 @@
-#include <Columns/ColumnArray.h>
-#include <DataStreams/OneBlockInputStream.h>
 #include <Storages/System/StorageSystemStoragePolicies.h>
+
+#include <Columns/ColumnArray.h>
+#include <Columns/ColumnNullable.h>
+#include <DataStreams/OneBlockInputStream.h>
 #include <DataTypes/DataTypeArray.h>
+#include <DataTypes/DataTypeNullable.h>
 #include <Processors/Sources/SourceFromSingleChunk.h>
 #include <Interpreters/Context.h>
 
@@ -25,7 +28,8 @@ StorageSystemStoragePolicies::StorageSystemStoragePolicies(const std::string & n
              {"volume_priority", std::make_shared<DataTypeUInt64>()},
              {"disks", std::make_shared<DataTypeArray>(std::make_shared<DataTypeString>())},
              {"volume_type", std::make_shared<DataTypeString>()},
-             {"move_factor", std::make_shared<DataTypeFloat32>()}
+             {"max_data_part_size", std::make_shared<DataTypeNullable>(std::make_shared<DataTypeUInt64>())},
+             {"move_factor", std::make_shared<DataTypeFloat32>()},
     }));
     // TODO: Add string column with custom volume-type-specific options
     setInMemoryMetadata(storage_metadata);
@@ -47,6 +51,7 @@ Pipes StorageSystemStoragePolicies::read(
     MutableColumnPtr col_priority = ColumnUInt64::create();
     MutableColumnPtr col_disks = ColumnArray::create(ColumnString::create());
     MutableColumnPtr col_volume_type = ColumnString::create();
+    MutableColumnPtr col_max_part_size = ColumnNullable::create(ColumnUInt64::create(), ColumnUInt8::create());
     MutableColumnPtr col_move_factor = ColumnFloat32::create();
 
     for (const auto & [policy_name, policy_ptr] : context.getPoliciesMap())
@@ -63,6 +68,11 @@ Pipes StorageSystemStoragePolicies::read(
                 disks.push_back(disk_ptr->getName());
             col_disks->insert(disks);
             col_volume_type->insert(volumeTypeToString(volumes[i]->getType()));
+            auto volume_jbod = std::dynamic_pointer_cast<VolumeJBOD>(volumes[i]);
+            if (volume_jbod)
+                col_max_part_size->insert(volume_jbod->max_data_part_size);
+            else
+                col_max_part_size->insert(Null{});
             col_move_factor->insert(policy_ptr->getMoveFactor());
         }
     }
@@ -73,6 +83,7 @@ Pipes StorageSystemStoragePolicies::read(
     res_columns.emplace_back(std::move(col_priority));
     res_columns.emplace_back(std::move(col_disks));
     res_columns.emplace_back(std::move(col_volume_type));
+    res_columns.emplace_back(std::move(col_max_part_size));
     res_columns.emplace_back(std::move(col_move_factor));
 
     UInt64 num_rows = res_columns.at(0)->size();
