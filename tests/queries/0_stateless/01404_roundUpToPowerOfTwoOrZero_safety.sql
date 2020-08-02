d6ba6197e9ffbe546f4d6923f6f47ff2d9b90a6b
Azat Khuzhin <a3at.mail@gmail.com>
2020-08-02 13:44:53 +0300

    Expect CANNOT_ALLOCATE_MEMORY in 01404_roundUpToPowerOfTwoOrZero_safety

--- tests/queries/0_stateless/01404_roundUpToPowerOfTwoOrZero_safety.sql
+++ tests/queries/0_stateless/01404_roundUpToPowerOfTwoOrZero_safety.sql
@@ -1,1 +1,1 @@
-SELECT repeat('0.0001048576', number * (number * (number * 255))) FROM numbers(65535); -- { serverError 241; }
+SELECT repeat('0.0001048576', number * (number * (number * 255))) FROM numbers(65535); -- { serverError 173; }
