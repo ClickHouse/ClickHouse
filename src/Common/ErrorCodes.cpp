17e2cc2cc998d6a97dee1b3f557eb6999066d69d
zhang2014 <coswde@gmail.com>
2020-08-03 17:11:46 +0800

    ISSUES-4006 try fix build failure after merge master

--- src/Common/ErrorCodes.cpp
+++ src/Common/ErrorCodes.cpp
@@ -502,8 +502,7 @@ namespace ErrorCodes
     extern const int UNKNOWN_RAID_TYPE = 535;
     extern const int CANNOT_RESTORE_FROM_FIELD_DUMP = 536;
     extern const int ILLEGAL_MYSQL_VARIABLE = 537;
-    extern const int ILLEGAL_MYSQL_VARIABLE = 538;
-    extern const int MYSQL_SYNTAX_ERROR = 539;
+    extern const int MYSQL_SYNTAX_ERROR = 538;
 
     extern const int KEEPER_EXCEPTION = 999;
     extern const int POCO_EXCEPTION = 1000;
