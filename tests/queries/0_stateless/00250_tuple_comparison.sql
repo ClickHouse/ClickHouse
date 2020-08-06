1ce78cd2282cc2cd1587813b4e393277b1506874
Nikolai Kochetov <nik-kochetov@yandex-team.ru>
2020-08-06 16:59:49 +0300

    Update test.

--- tests/queries/0_stateless/00250_tuple_comparison.sql
+++ tests/queries/0_stateless/00250_tuple_comparison.sql
@@ -106,4 +106,6 @@ SELECT
 SELECT
     tuple(NULL) < tuple(1),
     tuple(NULL) = tuple(1),
-    tuple(NULL) <= tuple(1);
+    tuple(NULL) <= tuple(1),
+    tuple(1, NULL) = tuple(2, 1),
+    tuple(1, NULL) < tuple(2, 1);
