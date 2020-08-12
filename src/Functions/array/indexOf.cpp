1ed406dfd8d37fc226e7b178468032a14b93b0bf
myrrc <me@myrrec.space>
2020-08-12 19:11:35 +0300

    empty commit to restart CI

--- src/Functions/array/indexOf.cpp
+++ src/Functions/array/indexOf.cpp
@@ -5,7 +5,8 @@ namespace DB
 {
 struct NameIndexOf { static constexpr auto name = "indexOf"; };
 
-/// indexOf(arr, x) - returns the index of the element x (starting with 1), if it exists in the array, or 0 if it is not.
+/// indexOf(arr, x) - returns the index of the element x (starting with 1), if it exists in the array, or 0 if it
+/// doesn't.
 using FunctionIndexOf = FunctionArrayIndex<IndexOfAction, NameIndexOf>;
 
 void registerFunctionIndexOf(FunctionFactory & factory)
