9b3a85ec34e12270b11b9621820af17cab0276fc
Artem Zuikov <chertus@gmail.com>
2020-08-14 14:43:10 +0300

    update test

--- tests/queries/0_stateless/01246_least_greatest_generic.sql
+++ tests/queries/0_stateless/01246_least_greatest_generic.sql
@@ -15,8 +15,8 @@ SELECT greatest(1, inf);
 SELECT least(0., -0.);
 SELECT least(toNullable(123), 456);
 
+SELECT LEAST(-1, 18446744073709551615) x, toTypeName(x);
 -- This can be improved
-SELECT LEAST(-1, 18446744073709551615); -- { serverError 43 }
 SELECT LEAST(-1., 18446744073709551615); -- { serverError 43 }
 
 SELECT LEAST(-1., 18446744073709551615.);
