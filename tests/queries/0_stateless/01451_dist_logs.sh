23b9c5fe8439fdad81d3678433cbb0636327c053
Azat Khuzhin <a3at.mail@gmail.com>
2020-08-17 22:49:04 +0300

    Simplify 01451_dist_logs test

--- tests/queries/0_stateless/01451_dist_logs.sh
+++ tests/queries/0_stateless/01451_dist_logs.sh
@@ -1,12 +1,12 @@
 #!/usr/bin/env bash
 
+# level should be 'trace', otherwise it will not trigger the issue
 CLICKHOUSE_CLIENT_SERVER_LOGS_LEVEL=trace
+
 CURDIR=$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)
 . "$CURDIR"/../shell_config.sh
 
 # triggered not for the first query
-yes "select * from remote('127.{2,3}', system.numbers) where number = 10 limit 1;" 2>/dev/null | {
-    head -n20 | ${CLICKHOUSE_CLIENT} -n 2>&1 >/dev/null | grep 'DB::Exception: '
-}
-# grep above will fail during diff with .reference
-exit 0
+for _ in {1..20}; do
+    echo "select * from remote('127.{2,3}', system.numbers) where number = 10 limit 1;"
+done | ${CLICKHOUSE_CLIENT} -n 2>/dev/null
