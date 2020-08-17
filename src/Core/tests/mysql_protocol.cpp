4872dac44b395a8fcf0b1ffd2e8abb3605f115ba
BohuTANG <overred.shuttler@gmail.com>
2020-08-17 14:45:43 +0800

    ISSUES-4006 add GTID unit tests #4006

--- src/Core/tests/mysql_protocol.cpp
+++ src/Core/tests/mysql_protocol.cpp
@@ -21,7 +21,8 @@ int main(int argc, char ** argv)
     using namespace MySQLProtocol::ProtocolText;
 
 
-    uint8_t sequence_id = 1;
+    uint8_t server_sequence_id = 1;
+    uint8_t client_sequence_id = 1;
     String user = "default";
     String password = "123";
     String database;
@@ -37,18 +38,19 @@ int main(int argc, char ** argv)
 
     /// Handshake packet
     {
+
         /// 1. Greeting:
         /// 1.1 Server writes greeting to client
         std::string s0;
         WriteBufferFromString out0(s0);
 
         Handshake server_handshake(server_capability_flags, -1, "ClickHouse", "mysql_native_password", "aaaaaaaaaaaaaaaaaaaaa", CharacterSet::utf8_general_ci);
-        server_handshake.writePayload(out0, sequence_id);
+        server_handshake.writePayload(out0, server_sequence_id);
 
         /// 1.2 Client reads the greeting
         ReadBufferFromString in0(s0);
         Handshake client_handshake;
-        client_handshake.readPayload(in0, sequence_id);
+        client_handshake.readPayload(in0, client_sequence_id);
 
         /// Check packet
         ASSERT(server_handshake.capability_flags == client_handshake.capability_flags)
@@ -67,12 +69,12 @@ int main(int argc, char ** argv)
         String auth_plugin_data = native41.getAuthPluginData();
         HandshakeResponse client_handshake_response(
             client_capability_flags, max_packet_size, charset_utf8, user, database, auth_plugin_data, mysql_native_password);
-        client_handshake_response.writePayload(out1, sequence_id);
+        client_handshake_response.writePayload(out1, client_sequence_id);
 
         /// 2.2 Server reads the response
         ReadBufferFromString in1(s1);
         HandshakeResponse server_handshake_response;
-        server_handshake_response.readPayload(in1, sequence_id);
+        server_handshake_response.readPayload(in1, server_sequence_id);
 
         /// Check
         ASSERT(server_handshake_response.capability_flags == client_handshake_response.capability_flags)
@@ -89,12 +91,12 @@ int main(int argc, char ** argv)
         std::string s0;
         WriteBufferFromString out0(s0);
         OKPacket server(0x00, server_capability_flags, 0, 0, 0, "", "");
-        server.writePayload(out0, sequence_id);
+        server.writePayload(out0, server_sequence_id);
 
         // 2. Client reads packet
         ReadBufferFromString in0(s0);
         ResponsePacket client(server_capability_flags);
-        client.readPayload(in0, sequence_id);
+        client.readPayload(in0, client_sequence_id);
 
         // Check
         ASSERT(client.getType() == PACKET_OK)
@@ -109,12 +111,12 @@ int main(int argc, char ** argv)
         std::string s0;
         WriteBufferFromString out0(s0);
         ERRPacket server(123, "12345", "This is the error message");
-        server.writePayload(out0, sequence_id);
+        server.writePayload(out0, server_sequence_id);
 
         // 2. Client reads packet
         ReadBufferFromString in0(s0);
         ResponsePacket client(server_capability_flags);
-        client.readPayload(in0, sequence_id);
+        client.readPayload(in0, client_sequence_id);
 
         // Check
         ASSERT(client.getType() == PACKET_ERR)
@@ -130,12 +132,12 @@ int main(int argc, char ** argv)
         std::string s0;
         WriteBufferFromString out0(s0);
         EOFPacket server(1, 1);
-        server.writePayload(out0, sequence_id);
+        server.writePayload(out0, server_sequence_id);
 
         // 2. Client reads packet
         ReadBufferFromString in0(s0);
         ResponsePacket client(server_capability_flags);
-        client.readPayload(in0, sequence_id);
+        client.readPayload(in0, client_sequence_id);
 
         // Check
         ASSERT(client.getType() == PACKET_EOF)
@@ -150,12 +152,12 @@ int main(int argc, char ** argv)
         std::string s0;
         WriteBufferFromString out0(s0);
         ColumnDefinition server("schema", "tbl", "org_tbl", "name", "org_name", 33, 0x00, MYSQL_TYPE_STRING, 0x00, 0x00);
-        server.writePayload(out0, sequence_id);
+        server.writePayload(out0, server_sequence_id);
 
         // 2. Client reads packet
         ReadBufferFromString in0(s0);
         ColumnDefinition client;
-        client.readPayload(in0, sequence_id);
+        client.readPayload(in0, client_sequence_id);
 
         // Check
         ASSERT(client.column_type == server.column_type)
@@ -170,13 +172,95 @@ int main(int argc, char ** argv)
         ASSERT(client.schema == server.schema)
     }
 
+    /// GTID tests.
+    {
+        String str
+            = "2c5adab4-d64a-11e5-82df-ac162d72dac0:1-247743812,9f58c169-d121-11e7-835b-ac162db9c048:1-56060985:56060987-56061175:56061177-"
+              "56061224:56061226-75201528:75201530-75201755:75201757-75201983:75201985-75407550:75407552-75407604:75407606-75407661:"
+              "75407663-87889848:87889850-87889935:87889937-87890042:87890044-88391955:88391957-88392125:88392127-88392245:88392247-"
+              "88755771:88755773-88755826:88755828-88755921:88755923-100279047:100279049-100279126:100279128-100279247:100279249-121672430:"
+              "121672432-121672503:121672505-121672524:121672526-122946019:122946021-122946291:122946293-122946469:122946471-134313284:"
+              "134313286-134313415:134313417-134313648:134313650-136492728:136492730-136492784:136492786-136492904:136492906-145582402:"
+              "145582404-145582439:145582441-145582463:145582465-147455222:147455224-147455262:147455264-147455277:147455279-149319049:"
+              "149319051-149319261:149319263-150635915,a6d83ff6-bfcf-11e7-8c93-246e96158550:1-126618302";
+        GTIDSets gtid_sets;
+        gtid_sets.parse(str);
+        ASSERT(str == gtid_sets.toString())
+    }
+
+    {
+        struct Testcase
+        {
+            String name;
+            String gtid_sets;
+            String gtid_str;
+            String want;
+        };
+
+        Testcase cases[] = {
+            {"merge",
+                "10662d71-9d91-11ea-bbc2-0242ac110003:1-2:4-7",
+                "10662d71-9d91-11ea-bbc2-0242ac110003:3",
+                "10662d71-9d91-11ea-bbc2-0242ac110003:1-7"},
+
+            {"merge-front",
+                "10662d71-9d91-11ea-bbc2-0242ac110003:1-2:5-7",
+                "10662d71-9d91-11ea-bbc2-0242ac110003:3",
+                "10662d71-9d91-11ea-bbc2-0242ac110003:1-3:5-7"},
+
+            {"extend-interval",
+                "10662d71-9d91-11ea-bbc2-0242ac110003:1-2:6-7",
+                "10662d71-9d91-11ea-bbc2-0242ac110003:4",
+                "10662d71-9d91-11ea-bbc2-0242ac110003:1-2:4:6-7"},
+
+            {"extend-interval",
+                "10662d71-9d91-11ea-bbc2-0242ac110003:1-2:4:7-9",
+                "10662d71-9d91-11ea-bbc2-0242ac110003:5",
+                "10662d71-9d91-11ea-bbc2-0242ac110003:1-2:4-5:7-9"},
+
+            {"extend-interval",
+                "10662d71-9d91-11ea-bbc2-0242ac110003:6-7",
+                "10662d71-9d91-11ea-bbc2-0242ac110003:4",
+                "10662d71-9d91-11ea-bbc2-0242ac110003:4:6-7"},
+
+            {"extend-interval",
+                "10662d71-9d91-11ea-bbc2-0242ac110003:6-7",
+                "10662d71-9d91-11ea-bbc2-0242ac110003:9",
+                "10662d71-9d91-11ea-bbc2-0242ac110003:6-7:9"},
+
+            {"extend-interval",
+                "10662d71-9d91-11ea-bbc2-0242ac110003:6-7",
+                "20662d71-9d91-11ea-bbc2-0242ac110003:9",
+                "10662d71-9d91-11ea-bbc2-0242ac110003:6-7,20662d71-9d91-11ea-bbc2-0242ac110003:9"},
+        };
+
+        for (auto & tc : cases)
+        {
+            GTIDSets gtid_sets;
+            gtid_sets.parse(tc.gtid_sets);
+            ASSERT(tc.gtid_sets == gtid_sets.toString())
+
+            GTIDSets gtid_sets1;
+            gtid_sets1.parse(tc.gtid_str);
+
+            GTID gtid;
+            memcpy(gtid.uuid, gtid_sets1.sets[0].uuid, 16);
+            gtid.seq_no = gtid_sets1.sets[0].intervals[0].start;
+            gtid_sets.update(gtid);
+
+            String want = tc.want;
+            String got = gtid_sets.toString();
+            ASSERT(want == got)
+        }
+    }
+
     {
         try
         {
             boost::program_options::options_description desc("Allowed options");
             desc.add_options()("host", boost::program_options::value<std::string>()->required(), "master host")(
-                "port", boost::program_options::value<std::int32_t>()->required(), "master port")(
-                "user", boost::program_options::value<std::string>()->required(), "master user")(
+                "port", boost::program_options::value<std::int32_t>()->default_value(3306), "master port")(
+                "user", boost::program_options::value<std::string>()->default_value("root"), "master user")(
                 "password", boost::program_options::value<std::string>()->required(), "master password")(
                 "db", boost::program_options::value<std::string>()->required(), "replicate do db");
 
@@ -216,7 +300,7 @@ int main(int argc, char ** argv)
                         binlog_event->dump(std::cerr);
 
                         Position pos = slave.getPosition();
-                        std::cerr << "Binlog Name: " << pos.binlog_name << ", Pos: " << pos.binlog_pos << std::endl;
+                        pos.dump(std::cerr);
                         break;
                     }
                     case MYSQL_WRITE_ROWS_EVENT: {
@@ -224,7 +308,7 @@ int main(int argc, char ** argv)
                         binlog_event->dump(std::cerr);
 
                         Position pos = slave.getPosition();
-                        std::cerr << "Binlog Name: " << pos.binlog_name << ", Pos: " << pos.binlog_pos << std::endl;
+                        pos.dump(std::cerr);
                         break;
                     }
                     case MYSQL_UPDATE_ROWS_EVENT: {
@@ -232,7 +316,7 @@ int main(int argc, char ** argv)
                         binlog_event->dump(std::cerr);
 
                         Position pos = slave.getPosition();
-                        std::cerr << "Binlog Name: " << pos.binlog_name << ", Pos: " << pos.binlog_pos << std::endl;
+                        pos.dump(std::cerr);
                         break;
                     }
                     case MYSQL_DELETE_ROWS_EVENT: {
@@ -240,7 +324,7 @@ int main(int argc, char ** argv)
                         binlog_event->dump(std::cerr);
 
                         Position pos = slave.getPosition();
-                        std::cerr << "Binlog Name: " << pos.binlog_name << ", Pos: " << pos.binlog_pos << std::endl;
+                        pos.dump(std::cerr);
                         break;
                     }
                     default:
