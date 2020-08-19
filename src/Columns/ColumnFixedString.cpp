09a624eebf132b17065363f8778b244d4971b4c4
robot-clickhouse <robot-clickhouse@yandex-team.ru>
2020-08-19 23:12:40 +0300

    Backport #13887 to 20.6: Fix fixed string partial sort

--- src/Columns/ColumnFixedString.cpp
+++ src/Columns/ColumnFixedString.cpp
@@ -189,7 +189,7 @@ void ColumnFixedString::updatePermutation(bool reverse, size_t limit, int, Permu
         auto new_first = first;
         for (auto j = first + 1; j < last; ++j)
         {
-            if (memcmpSmallAllowOverflow15(chars.data() + j * n, chars.data() + new_first * n, n) != 0)
+            if (memcmpSmallAllowOverflow15(chars.data() + res[j] * n, chars.data() + res[new_first] * n, n) != 0)
             {
                 if (j - new_first > 1)
                     new_ranges.emplace_back(new_first, j);
@@ -210,7 +210,7 @@ void ColumnFixedString::updatePermutation(bool reverse, size_t limit, int, Permu
         auto new_first = first;
         for (auto j = first + 1; j < limit; ++j)
         {
-            if (memcmpSmallAllowOverflow15(chars.data() + j * n, chars.data() + new_first * n, n)  != 0)
+            if (memcmpSmallAllowOverflow15(chars.data() + res[j] * n, chars.data() + res[new_first] * n, n)  != 0)
             {
                 if (j - new_first > 1)
                     new_ranges.emplace_back(new_first, j);
@@ -221,7 +221,7 @@ void ColumnFixedString::updatePermutation(bool reverse, size_t limit, int, Permu
         auto new_last = limit;
         for (auto j = limit; j < last; ++j)
         {
-            if (memcmpSmallAllowOverflow15(chars.data() + j * n, chars.data() + new_first * n, n)  == 0)
+            if (memcmpSmallAllowOverflow15(chars.data() + res[j] * n, chars.data() + res[new_first] * n, n)  == 0)
             {
                 std::swap(res[new_last], res[j]);
                 ++new_last;
