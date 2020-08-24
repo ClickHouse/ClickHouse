2cdf973a8340f801371479319d6cd1e367724249
Alexey Milovidov <milovidov@yandex-team.ru>
2020-08-24 23:35:38 +0300

    Minor modifications

--- tests/queries/0_stateless/01455_time_zones.sql
+++ tests/queries/0_stateless/01455_time_zones.sql
@@ -1,9 +1,9 @@
--- There are currently 562 timezones which are used from the binary embedded inside the ClickHouse binary
--- Refer: contrib/cctz-cmake/CMakeLists.txt for the complete list. The count should match when queried.
+-- There are currently 594 timezones which are used from the binary embedded inside the ClickHouse binary
+-- Refer: contrib/cctz-cmake/CMakeLists.txt for the complete list. The count may change but we expect there will be at least 500 timezones.
 -- SELECT count(*)
 -- FROM system.time_zones
 --
 -- ┌─count()─┐
--- │     562 │
+-- │     594 │
 -- └─────────┘
-SELECT if ((SELECT count(*) FROM system.time_zones) = 562, 'ok', 'fail');
+SELECT if ((SELECT count(*) FROM system.time_zones) > 500, 'ok', 'fail');
