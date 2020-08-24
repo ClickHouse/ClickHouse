7c020470603a2e413a1526f69dcf61bfc607b952
hcz <h.cz@qq.com>
2020-08-24 10:51:04 +0800

    Fix tests

--- tests/queries/0_stateless/01445_create_table_as_table_function.sh
+++ tests/queries/0_stateless/01445_create_table_as_table_function.sh
@@ -5,6 +5,6 @@ set -e
 CUR_DIR=$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)
 . "$CUR_DIR"/../shell_config.sh
 
-${CLICKHOUSE_CLIENT} --query "CREATE TABLE system.columns AS numbers(10);" 2>&1 | grep -o "Code: 57"
-${CLICKHOUSE_CLIENT} --query "CREATE TABLE system.columns engine=Memory AS numbers(10);" 2>&1 | grep -o "Code: 62"
-${CLICKHOUSE_CLIENT} --query "CREATE TABLE system.columns AS numbers(10) Engine=Memory;" 2>&1 | grep -o "Code: 62"
+${CLICKHOUSE_CLIENT} --query "CREATE TABLE system.columns AS numbers(10);" 2>&1 | grep -F "Code: 57" > /dev/null && echo 'OK' || echo 'FAIL'
+${CLICKHOUSE_CLIENT} --query "CREATE TABLE system.columns engine=Memory AS numbers(10);" 2>&1 | grep -F "Code: 62" > /dev/null && echo 'OK' || echo 'FAIL'
+${CLICKHOUSE_CLIENT} --query "CREATE TABLE system.columns AS numbers(10) engine=Memory;" 2>&1 | grep -F "Code: 62" > /dev/null && echo 'OK' || echo 'FAIL'
