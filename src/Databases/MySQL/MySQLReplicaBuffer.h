15eb8b4ecf20a3dcc1b028b0991b6fbcc6a24167
adilyas <adygamovilyas@gmail.com>
2020-08-30 08:32:40 +0300

    rewrite MySQLReplicaBuffer

--- src/Databases/MySQL/MySQLReplicaBuffer.h
+++ src/Databases/MySQL/MySQLReplicaBuffer.h
@@ -1,34 +1,40 @@
 #include <mutex>
+#include <unordered_map>
+#include <utility>
+#include <vector>
 #include <Interpretes/StorageID.h>
 
 namespace DB {
 
 using BufferAndSortingColumns = std::pair<Block, std::vector<size_t>>;
 using BufferAndSortingColumnsPtr = std::shared_ptr<BufferAndSortingColumns>;
+using MySQLDatabaseBuffer = std::unordered_map<String, BufferAndSortingColumnsPtr>;
+using MySQLDatabaseBufferPtr = std::shared_ptr<MySQLDatabaseBuffer>;
 
 class MySQLReplicaBuffer {
 public:
-    BufferAndSortingColumnsPtr getTableDataBuffer(const String & mysql_table_name, const Context & context);
-    void finishProcessing();
+    std::vector<BufferAndSortingColumnsPtr> getTableDataBuffers(
+        const String & mysql_table_name,
+        const Context & context);
 
-    BlockPtr readNextBlock(const StorageID & table_id);
+    BlockPtr readBlock(const StorageID & table_id);
+    MySQLDatabaseBufferPtr readDatabaseBuffer();
 
     void registerTable(const StorageID & table_id);
     void registerDatabase(const String & database_name);
 
 private:
-    void flushData();
-
-    bool checkThresholds(const String & mysql_table_name);
+    bool checkThresholds(const StorageID & table_id);
+    bool checkDatabaseThresholds();
 
 private:
     std::mutex mutex;
 
     std::unordered_map<String, BufferAndSortingColumnsPtr> data;
-    std::unordered_map<String, std::queue<BlockPtr>> flushedData;
+    MySQLDatabaseBufferPtr database_data;
 
-    std::unordered_map<String, std::vector<StorageID>> consumerTables;
-    std::optional<String> chDatabase;
+    std::unordered_map<String, std::vector<StorageID>> consumer_tables;
+    std::optional<String> ch_database;
 };
 
 }
