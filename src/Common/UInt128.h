cbc37dafe90f3832f5c060ee842b814ac47677b5
Artem Zuikov <chertus@gmail.com>
2020-08-31 12:59:04 +0300

    Rerun tests

--- src/Common/UInt128.h
+++ src/Common/UInt128.h
@@ -173,7 +173,7 @@ struct DummyUInt256
 #pragma GCC diagnostic pop
 #endif
 
-    DummyUInt256 & operator= (const UInt64 rhs) { a = rhs; b = 0; c = 0; d = 0; return *this; }
+    DummyUInt256 & operator = (const UInt64 rhs) { a = rhs; b = 0; c = 0; d = 0; return *this; }
 };
 
 struct UInt256Hash
