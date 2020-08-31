33c5815eb307fee81f59f405a87b0276dc05521d
Alexander Kuzmenkov <akuzm@yandex-team.ru>
2020-09-01 02:33:42 +0300

    Add a HTML report for AST Fuzzer

--- programs/client/Client.cpp
+++ programs/client/Client.cpp
@@ -847,30 +847,24 @@ private:
             }
 
             // Parse and execute what we've read.
-            fprintf(stderr, "will now parse '%s'\n", text.c_str());
-
             const auto * new_end = processWithFuzzing(text);
 
             if (new_end > &text[0])
             {
                 const auto rest_size = text.size() - (new_end - &text[0]);
 
-                fprintf(stderr, "total %zd, rest %zd\n", text.size(), rest_size);
-
                 memcpy(&text[0], new_end, rest_size);
                 text.resize(rest_size);
             }
             else
             {
-                fprintf(stderr, "total %zd, can't parse\n", text.size());
+                // We didn't read enough text to parse a query. Will read more.
             }
 
-            if (!connection->isConnected())
+            if (!connection->ping())
             {
                 // Uh-oh...
-                std::cerr << "Lost connection to the server." << std::endl;
-                last_exception_received_from_server
-                    = std::make_unique<Exception>(210, "~");
+                last_exception_received_from_server = std::make_unique<Exception>(210, "Lost connection to the server.");
                 return;
             }
 
@@ -880,9 +874,6 @@ private:
                 // and we still cannot parse a single query in it. Abort.
                 std::cerr << "Read too much text and still can't parse a query."
                      " Aborting." << std::endl;
-                last_exception_received_from_server
-                    = std::make_unique<Exception>(1, "~");
-                // return;
                 exit(1);
             }
         }
