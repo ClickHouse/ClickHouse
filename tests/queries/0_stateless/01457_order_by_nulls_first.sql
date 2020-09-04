ef45c08e8cf4ad5b86732ddd4b4e4461a38f3824
Nikita Mikhaylov <mikhaylovnikitka@gmail.com>
2020-09-04 17:36:08 +0300

    fixed

--- tests/queries/0_stateless/01457_order_by_nulls_first.sql
+++ tests/queries/0_stateless/01457_order_by_nulls_first.sql
@@ -13,7 +13,7 @@ FROM order_by_nulls_first
 order by diff desc NULLS FIRST, traf
 limit 1, 4;
 
-select '---';
+select '--- DESC NULLS FIRST, ASC';
 
 SELECT
     diff,
@@ -22,5 +22,75 @@ FROM order_by_nulls_first
 ORDER BY
     diff DESC NULLS FIRST,
     traf ASC;
+
+select '--- DESC NULLS LAST, ASC';
+
+SELECT
+    diff,
+    traf
+FROM order_by_nulls_first
+ORDER BY
+    diff DESC NULLS LAST,
+    traf ASC;
+
+select '--- ASC NULLS FIRST, ASC';
+
+SELECT
+    diff,
+    traf
+FROM order_by_nulls_first
+ORDER BY
+    diff ASC NULLS FIRST,
+    traf ASC;
+
+select '--- ASC NULLS LAST, ASC';
+
+SELECT
+    diff,
+    traf
+FROM order_by_nulls_first
+ORDER BY
+    diff ASC NULLS LAST,
+    traf ASC;
+
+select '--- DESC NULLS FIRST, DESC';
+
+SELECT
+    diff,
+    traf
+FROM order_by_nulls_first
+ORDER BY
+    diff DESC NULLS FIRST,
+    traf DESC;
+
+select '--- DESC NULLS LAST, DESC';
+
+SELECT
+    diff,
+    traf
+FROM order_by_nulls_first
+ORDER BY
+    diff DESC NULLS LAST,
+    traf DESC;
+
+select '--- ASC NULLS FIRST, DESC';
+
+SELECT
+    diff,
+    traf
+FROM order_by_nulls_first
+ORDER BY
+    diff ASC NULLS FIRST,
+    traf DESC;
+
+select '--- ASC NULLS LAST, DESC';
+
+SELECT
+    diff,
+    traf
+FROM order_by_nulls_first
+ORDER BY
+    diff ASC NULLS LAST,
+    traf DESC;
 
 drop table if exists order_by_nulls_first;
