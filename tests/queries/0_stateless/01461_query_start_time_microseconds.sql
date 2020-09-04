2b3b8fed160f44f6f23d45c4c2e30b4749dd6b08
bharatnc <bharatnc@gmail.com>
2020-09-04 09:41:12 -0700

    tests query_start_time_microseconds for system.query_thread_log

--- tests/queries/0_stateless/01461_query_start_time_microseconds.sql
+++ tests/queries/0_stateless/01461_query_start_time_microseconds.sql
@@ -3,3 +3,7 @@ set log_queries = 1;
 select '01461_query_log_query_start_time_milliseconds_test';
 system flush logs;
 SELECT If((select count(query_start_time_microseconds)  from system.query_log WHERE query LIKE  '%01461_query_log_query_start_time_milliseconds_test%' AND query NOT LIKE '%system.query_log%') > 0, 'ok', 'fail');
+
+select '01461_query_thread_log_query_start_time_milliseconds_test';
+system flush logs;
+SELECT If((select count(query_start_time_microseconds)  from system.query_log WHERE query LIKE  '%01461_query_thread_log_query_start_time_milliseconds_test%' AND query NOT LIKE '%system.query_log%') > 0, 'ok', 'fail');
