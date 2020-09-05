6a5b885ac1167dd8bba4a0c8b091289dd4c1e79e
bharatnc <bharatnc@gmail.com>
2020-09-05 16:17:28 -0700

    AsynchronousMetricLog - add tests for event_time_microseconds column

---
+++ tests/queries/0_stateless/01473_event_time_microseconds.sql
@@ -0,0 +1,5 @@
+set log_queries = 1;
+
+select '01473_asynchronous_metric_log_event_start_time_milliseconds_test';
+system flush logs;
+SELECT If((select count(event_time_microseconds)  from system.asynchronous_metric_log) > 0, 'ok', 'fail'); -- success
