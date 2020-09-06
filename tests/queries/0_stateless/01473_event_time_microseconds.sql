ae34a16f1c59eb04a9801defe8518566dd7b0224
bharatnc <bharatnc@gmail.com>
2020-09-05 19:49:47 -0700

    AsynchronousMetricsLog - add improved tests
    This adds integration tests to test the asynchronous_metric_log table
    for event_time_microseconds field.
    
    Asynchronous metrics are updated once every 60s, so this setting needs
    to be overridden for having those metrics available in time so that the
    tests can run. So the `asynchronous_metric_update_period_s` setting is
    being overriden to be 2s.

--- tests/queries/0_stateless/01473_event_time_microseconds.sql
+++ tests/queries/0_stateless/01473_event_time_microseconds.sql
@@ -1,9 +1,11 @@
+-- This file contains tests for the event_time_microseconds field for various tables.
+-- Note: Only event_time_microseconds for asynchronous_metric_log table is tested via
+-- an integration test as those metrics take 60s by default to be updated.
+-- Refer: tests/integration/test_asynchronous_metric_log_table.
+
 set log_queries = 1;
 
-select '01473_asynchronous_metric_log_event_start_time_milliseconds_test';
+select '01473_metric_log_table_event_start_time_microseconds_test';
 system flush logs;
-SELECT If((select count(event_time_microseconds)  from system.asynchronous_metric_log) > 0, 'ok', 'fail'); -- success
-
-select '01473_metric_log_event_start_time_milliseconds_test';
-system flush logs;
+SELECT sleep(3) Format Null;
 SELECT If((select count(event_time_microseconds)  from system.metric_log) > 0, 'ok', 'fail'); -- success
