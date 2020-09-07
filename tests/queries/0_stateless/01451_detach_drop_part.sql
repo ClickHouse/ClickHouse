d937fb64393f75b3f8e1ba4a5859746c7f948eb6
Nicolae Vartolomei <nv@cloudflare.com>
2020-09-07 17:23:59 +0100

    Simplify ALTER TABLE ... DETACH|DROP PART tests

--- tests/queries/0_stateless/01451_detach_drop_part.sql
+++ tests/queries/0_stateless/01451_detach_drop_part.sql
@@ -1,31 +1,40 @@
-DROP TABLE IF EXISTS attach_01451_mt;
+DROP TABLE IF EXISTS mt;
 
-CREATE TABLE attach_01451_mt (v UInt8) ENGINE = MergeTree() order by tuple();
+CREATE TABLE mt (v UInt8) ENGINE = MergeTree() order by tuple();
+SYSTEM STOP MERGES;
 
-INSERT INTO attach_01451_mt VALUES (0);
-INSERT INTO attach_01451_mt VALUES (1);
-INSERT INTO attach_01451_mt VALUES (2);
+INSERT INTO mt VALUES (0);
+INSERT INTO mt VALUES (1);
+INSERT INTO mt VALUES (2);
 
-SELECT v FROM attach_01451_mt ORDER BY v;
+SELECT v FROM mt ORDER BY v;
 
-ALTER TABLE attach_01451_mt DETACH PART 'all_2_2_0';
+ALTER TABLE mt DETACH PART 'all_2_2_0';
 
-SELECT v FROM attach_01451_mt ORDER BY v;
+SELECT v FROM mt ORDER BY v;
 
-SELECT name FROM system.detached_parts WHERE table = 'attach_01451_mt';
+SELECT name FROM system.detached_parts WHERE table = 'mt';
 
-ALTER TABLE attach_01451_mt ATTACH PART 'all_2_2_0';
+ALTER TABLE mt ATTACH PART 'all_2_2_0';
 
-SELECT v FROM attach_01451_mt ORDER BY v;
+SELECT v FROM mt ORDER BY v;
 
-SELECT name FROM system.detached_parts WHERE table = 'attach_01451_mt';
+SELECT name FROM system.detached_parts WHERE table = 'mt';
 
 SELECT '-- drop part --';
 
-ALTER TABLE attach_01451_mt DROP PART 'all_4_4_0';
+ALTER TABLE mt DROP PART 'all_4_4_0';
 
-ALTER TABLE attach_01451_mt ATTACH PART 'all_4_4_0'; -- { serverError 233 }
+ALTER TABLE mt ATTACH PART 'all_4_4_0'; -- { serverError 233 }
 
-SELECT v FROM attach_01451_mt ORDER BY v;
+SELECT v FROM mt ORDER BY v;
 
-DROP TABLE attach_01451_mt;
+SELECT '-- resume merges --';
+SYSTEM START MERGES;
+OPTIMIZE TABLE mt FINAL;
+
+SELECT v FROM mt ORDER BY v;
+
+SELECT name FROM system.parts WHERE table = 'mt' AND active;
+
+DROP TABLE mt;
