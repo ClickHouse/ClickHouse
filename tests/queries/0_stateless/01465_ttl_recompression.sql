acbe21304a56cf840b0a54d2f1918d5a459bbcbb
alesapin <alesapin@gmail.com>
2020-09-07 14:46:32 +0300

    Fix test for compact parts

--- tests/queries/0_stateless/01465_ttl_recompression.sql
+++ tests/queries/0_stateless/01465_ttl_recompression.sql
@@ -10,7 +10,7 @@ CREATE TABLE recompression_table
 ORDER BY tuple()
 PARTITION BY key
 TTL dt + INTERVAL 1 MONTH RECOMPRESS CODEC(ZSTD(17)), dt + INTERVAL 1 YEAR RECOMPRESS CODEC(LZ4HC(10))
-SETTINGS min_rows_for_wide_part = 0;
+SETTINGS min_rows_for_wide_part = 0, min_bytes_for_wide_part = 0;
 
 SHOW CREATE TABLE recompression_table;
 
