24822d29bb58da83b8eeb04347aee0782dc6ed83
alesapin <alesapin@gmail.com>
2020-09-14 15:24:38 +0300

    Revert changes

--- docker/packager/deb/build.sh
+++ docker/packager/deb/build.sh
@@ -2,25 +2,6 @@
 
 set -x -e
 
-# Special dpkg-deb (https://github.com/ClickHouse-Extras/dpkg) version which is able
-# to compress files using pigz (https://zlib.net/pigz/) instead of gzip.
-# Significantly increase deb packaging speed and compatible with old systems
-
-counter=0
-until curl -O https://clickhouse-builds.s3.yandex.net/utils/1/dpkg-deb
-do
-    sleep 0.5
-    counter=$(($counter + 1))
-    echo "Cannot fetch better dpgk, retry $counter"
-    if [ "$counter" -gt 120 ]
-    then
-        echo "Cannot fetch busybox image all retries exceeded"
-        exit 1
-    fi
-done
-
-chmod +x dpkg-deb && cp dpkg-deb /usr/bin
-
 ccache --show-stats ||:
 ccache --zero-stats ||:
 build/release --no-pbuilder $ALIEN_PKGS | ts '%Y-%m-%d %H:%M:%S'
