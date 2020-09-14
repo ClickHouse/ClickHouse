921ef4f87e4a08793fca909a23583d78a2eca9e9
Alexander Kazakov <Akazz@users.noreply.github.com>
2020-09-15 00:19:34 +0300

    Renamed alter in partition test + minor things

--- tests/integration/test_mutations_in_partitions_of_merge_tree/test.py
+++ tests/integration/test_mutations_in_partitions_of_merge_tree/test.py
@@ -9,19 +9,17 @@ node1 = cluster.add_instance('node1',
             config_dir='configs',
             main_configs=['configs/logs_config.xml'],
             with_zookeeper=True,
-            stay_alive=True,
-            macros={"shard": 0, "replica": 1} )
+            stay_alive=True)
 
 node2 = cluster.add_instance('node2',
             config_dir='configs',
             main_configs=['configs/logs_config.xml'],
             with_zookeeper=True,
-            stay_alive=True,
-            macros={"shard": 0, "replica": 2} )
+            stay_alive=True)
 
 
 @pytest.fixture(scope="module")
-def start_cluster():
+def started_cluster():
     try:
         cluster.start()
         yield cluster
@@ -30,9 +28,32 @@ def start_cluster():
         cluster.shutdown()
 
 
-def test_trivial_alter_in_partition_merge_tree_with_where(start_cluster):
+def test_trivial_alter_in_partition_merge_tree_without_where(started_cluster):
+    try:
+        name = "test_trivial_alter_in_partition_merge_tree_without_where"
+        node1.query("DROP TABLE {} IF EXISTS".format(name))
+        node1.query("CREATE TABLE {} (p Int64, x Int64) ENGINE=MergeTree() ORDER BY tuple() PARTITION BY p".format(name))
+        node1.query("INSERT INTO {} VALUES (1, 2), (2, 3)".format(name))
+        with pytest.raises(helpers.client.QueryRuntimeException):
+            node1.query("ALTER TABLE {} UPDATE x = x + 1 IN PARTITION 1 SETTINGS mutations_sync = 2".format(name))
+        assert node1.query("SELECT sum(x) FROM {}".format(name)).splitlines() == ["5"]
+        with pytest.raises(helpers.client.QueryRuntimeException):
+            node1.query("ALTER TABLE {} UPDATE x = x + 1 IN PARTITION 2 SETTINGS mutations_sync = 2".format(name))
+        assert node1.query("SELECT sum(x) FROM {}".format(name)).splitlines() == ["5"]
+        with pytest.raises(helpers.client.QueryRuntimeException):
+            node1.query("ALTER TABLE {} DELETE IN PARTITION 1 SETTINGS mutations_sync = 2".format(name))
+        assert node1.query("SELECT sum(x) FROM {}".format(name)).splitlines() == ["5"]
+        with pytest.raises(helpers.client.QueryRuntimeException):
+            node1.query("ALTER TABLE {} DELETE IN PARTITION 2 SETTINGS mutations_sync = 2".format(name))
+        assert node1.query("SELECT sum(x) FROM {}".format(name)).splitlines() == ["5"]
+    finally:
+        node1.query("DROP TABLE {} IF EXISTS".format(name))
+
+
+def test_trivial_alter_in_partition_merge_tree_with_where(started_cluster):
     try:
         name = "test_trivial_alter_in_partition_merge_tree_with_where"
+        node1.query("DROP TABLE {} IF EXISTS".format(name))
         node1.query("CREATE TABLE {} (p Int64, x Int64) ENGINE=MergeTree() ORDER BY tuple() PARTITION BY p".format(name))
         node1.query("INSERT INTO {} VALUES (1, 2), (2, 3)".format(name))
         node1.query("ALTER TABLE {} UPDATE x = x + 1 IN PARTITION 2 WHERE p = 2 SETTINGS mutations_sync = 2".format(name))
@@ -44,37 +65,24 @@ def test_trivial_alter_in_partition_merge_tree_with_where(start_cluster):
         node1.query("ALTER TABLE {} DELETE IN PARTITION 1 WHERE p = 2 SETTINGS mutations_sync = 2".format(name))
         assert node1.query("SELECT sum(x) FROM {}".format(name)).splitlines() == ["2"]
     finally:
-        node1.query("DROP TABLE {}".format(name))
+        node1.query("DROP TABLE {} IF EXISTS".format(name))
 
 
-def test_trivial_alter_in_partition_merge_tree_without_where(start_cluster):
-    try:
-        name = "test_trivial_alter_in_partition_merge_tree_without_where"
-        node1.query("CREATE TABLE {} (p Int64, x Int64) ENGINE=MergeTree() ORDER BY tuple() PARTITION BY p".format(name))
-        node1.query("INSERT INTO {} VALUES (1, 2), (2, 3)".format(name))
-        with pytest.raises(helpers.client.QueryRuntimeException):
-            node1.query("ALTER TABLE {} UPDATE x = x + 1 IN PARTITION 2 SETTINGS mutations_sync = 2".format(name))
-        assert node1.query("SELECT sum(x) FROM {}".format(name)).splitlines() == ["5"]
-        with pytest.raises(helpers.client.QueryRuntimeException):
-            node1.query("ALTER TABLE {} UPDATE x = x + 1 IN PARTITION 3 SETTINGS mutations_sync = 2".format(name))
-        assert node1.query("SELECT sum(x) FROM {}".format(name)).splitlines() == ["5"]
-        with pytest.raises(helpers.client.QueryRuntimeException):
-            node1.query("ALTER TABLE {} DELETE IN PARTITION 2 SETTINGS mutations_sync = 2".format(name))
-        assert node1.query("SELECT sum(x) FROM {}".format(name)).splitlines() == ["5"]
-        with pytest.raises(helpers.client.QueryRuntimeException):
-            node1.query("ALTER TABLE {} DELETE IN PARTITION 3 SETTINGS mutations_sync = 2".format(name))
-        assert node1.query("SELECT sum(x) FROM {}".format(name)).splitlines() == ["5"]
-    finally:
-        node1.query("DROP TABLE {}".format(name))
-
-
-def test_trivial_alter_in_partition_replicated_merge_tree(start_cluster):
+def test_trivial_alter_in_partition_replicated_merge_tree(started_cluster):
     try:
         name = "test_trivial_alter_in_partition_replicated_merge_tree"
-        node1.query("CREATE TABLE {name} (p Int64, x Int64) ENGINE=ReplicatedMergeTree('/clickhouse/{name}', '1') ORDER BY tuple() PARTITION BY p".format(name=name))
-        node2.query("CREATE TABLE {name} (p Int64, x Int64) ENGINE=ReplicatedMergeTree('/clickhouse/{name}', '2') ORDER BY tuple() PARTITION BY p".format(name=name))
+
+        node1.query("DROP TABLE {} IF EXISTS".format(name))
+        node2.query("DROP TABLE {} IF EXISTS".format(name))
+
+        for node in (node1, node2):
+            node.query(
+                "CREATE TABLE {name} (p Int64, x Int64) ENGINE=ReplicatedMergeTree('/clickhouse/{name}', '{instance}') ORDER BY tuple() PARTITION BY p"
+                .format(name=name))
+
         node1.query("INSERT INTO {} VALUES (1, 2)".format(name))
         node2.query("INSERT INTO {} VALUES (2, 3)".format(name))
+
         node1.query("ALTER TABLE {} UPDATE x = x + 1 IN PARTITION 2 WHERE 1 SETTINGS mutations_sync = 2".format(name))
         for node in (node1, node2):
             assert node.query("SELECT sum(x) FROM {}".format(name)).splitlines() == ["6"]
@@ -92,5 +100,5 @@ def test_trivial_alter_in_partition_replicated_merge_tree(start_cluster):
         for node in (node1, node2):
             assert node.query("SELECT sum(x) FROM {}".format(name)).splitlines() == ["2"]
     finally:
-        node1.query("DROP TABLE {}".format(name))
-        node2.query("DROP TABLE {}".format(name))
+        node1.query("DROP TABLE {} IF EXISTS".format(name))
+        node2.query("DROP TABLE {} IF EXISTS".format(name))
