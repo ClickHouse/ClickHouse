2af234b2bbbc6965ff3a6bd0a51b900cd063cfc2
Nikolai Kochetov <nik-kochetov@yandex-team.ru>
2020-09-14 23:35:45 +0300

    Try simplify Visitor.

--- src/Common/Visitor.h
+++ src/Common/Visitor.h
@@ -65,8 +65,6 @@ class Visitor<>
 {
 public:
     using List = TypeList<>;
-
-    virtual ~Visitor() = default;
 };
 
 template <typename Type>
@@ -75,7 +73,11 @@ class Visitor<Type> : public Visitor<>
 public:
     using List = TypeList<Type>;
 
-    virtual void visit(Type &) = 0;
+    void visit(Type &)
+    {
+        throw Exception("visit(" + demangle(typeid(Type).name()) + " &)" + " is not implemented for class"
+                        + demangle(typeid(Visitor<Type>).name()), ErrorCodes::LOGICAL_ERROR);
+    }
 };
 
 template <typename Type, typename ... Types>
@@ -85,7 +87,11 @@ public:
     using List = TypeList<Type, Types ...>;
     using Visitor<Types ...>::visit;
 
-    virtual void visit(Type &) = 0;
+    void visit(Type &)
+    {
+        throw Exception("visit(" + demangle(typeid(Type).name()) + " &)" + " is not implemented for class"
+                        + demangle(typeid(Visitor<Type>).name()), ErrorCodes::LOGICAL_ERROR);
+    }
 };
 
 
@@ -102,7 +108,7 @@ class VisitorImplHelper<Derived, VisitorBase, Type> : public VisitorBase
 {
 public:
     using VisitorBase::visit;
-    void visit(Type & value) override { static_cast<Derived *>(this)->visitImpl(value); }
+    void visit(Type & value) { static_cast<Derived *>(this)->visitImpl(value); }
 
 protected:
     template <typename T>
@@ -119,7 +125,7 @@ class VisitorImplHelper<Derived, VisitorBase, Type, Types ...>
 {
 public:
     using VisitorImplHelper<Derived, VisitorBase, Types ...>::visit;
-    void visit(Type & value) override { static_cast<Derived *>(this)->visitImpl(value); }
+    void visit(Type & value) { static_cast<Derived *>(this)->visitImpl(value); }
 
 protected:
     template <typename T>
