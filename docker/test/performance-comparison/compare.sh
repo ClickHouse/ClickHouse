24dd33d5cbd6814c15d1ed7fc487988c46d66b16
Alexander Kuzmenkov <36882414+akuzm@users.noreply.github.com>
2020-09-15 13:44:21 +0300

    Update compare.sh

--- docker/test/performance-comparison/compare.sh
+++ docker/test/performance-comparison/compare.sh
@@ -160,13 +160,13 @@ function run_tests
     # those values.
     if [ "$PR_TO_TEST" == "0" ]
     then
-        CHPC_TEST_RUNS=${CHPC_RUNS:-7}
-        CHPC_MAX_QUERIES=${CHPC_MAX_QUERIES:-15}
+        CHPC_RUNS=${CHPC_RUNS:-7}
+        CHPC_MAX_QUERIES=${CHPC_MAX_QUERIES:-100}
     else
-        CHPC_TEST_RUNS=${CHPC_RUNS:-13}
+        CHPC_RUNS=${CHPC_RUNS:-13}
         CHPC_MAX_QUERIES=${CHPC_MAX_QUERIES:-0}
     fi
-    export CHPC_TEST_RUNS
+    export CHPC_RUNS
     export CHPC_MAX_QUERIES
 
     # Determine which concurrent benchmarks to run. For now, the only test
