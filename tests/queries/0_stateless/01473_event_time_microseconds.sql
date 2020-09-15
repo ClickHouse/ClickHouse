0df3fa00dbab7d7c8718f5ebad16a14a263f9e0c
bharatnc <bharatnc@gmail.com>
2020-09-14 18:57:31 -0700

    TraceLog - revert back to functional test

--- tests/queries/0_stateless/01473_event_time_microseconds.sql
+++ tests/queries/0_stateless/01473_event_time_microseconds.sql
@@ -23,6 +23,28 @@ WITH (
     ) AS time)
 SELECT if(dateDiff('second', toDateTime(time_with_microseconds), toDateTime(time)) = 0, 'ok', 'fail');
 
+SELECT '01473_trace_log_table_event_start_time_microseconds_test';
+SET log_queries = 1;
+SET query_profiler_real_time_period_ns = 0;
+SET query_profiler_cpu_time_period_ns = 1000000;
+-- a long enough query to trigger the query profiler and to record trace log
+SELECT sleep(2) FORMAT Null;
+SYSTEM FLUSH LOGS;
+WITH (
+      (
+          SELECT event_time_microseconds
+          FROM system.trace_log
+          ORDER BY event_time DESC
+          LIMIT 1
+      ) AS time_with_microseconds,
+      (
+          SELECT event_time
+          FROM system.trace_log
+          ORDER BY event_time DESC
+          LIMIT 1
+      ) AS t)
+SELECT if(dateDiff('second', toDateTime(time_with_microseconds), toDateTime(t)) = 0, 'ok', 'fail'); -- success
+
 SELECT '01473_query_log_table_event_start_time_microseconds_test';
 SYSTEM FLUSH LOGS;
 WITH (
