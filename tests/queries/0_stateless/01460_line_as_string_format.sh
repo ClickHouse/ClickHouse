976323cd616fef9a412b818578d7e7097877e57f
hexiaoting <hewenting_ict@163.com>
2020-09-15 17:03:10 +0800

    Fix unreleased bug for LineAsString Format

--- tests/queries/0_stateless/01460_line_as_string_format.sh
+++ tests/queries/0_stateless/01460_line_as_string_format.sh
@@ -14,6 +14,15 @@ echo '"id" : 1,
 
 Finally implement this new feature.' | $CLICKHOUSE_CLIENT --query="INSERT INTO line_as_string FORMAT LineAsString";
 
+$CLICKHOUSE_CLIENT --query="select repeat('aaa',50) from numbers(1000000)" > "${CLICKHOUSE_TMP}"/data1
+$CLICKHOUSE_CLIENT --query="select randomString(50000) FROM numbers(1000)" > "${CLICKHOUSE_TMP}"/data2
+
 $CLICKHOUSE_CLIENT --query="SELECT * FROM line_as_string";
+$CLICKHOUSE_CLIENT --query="truncate line_as_string";
+$CLICKHOUSE_CLIENT --query="INSERT INTO line_as_string FORMAT LineAsString" < "${CLICKHOUSE_TMP}"/data1
+$CLICKHOUSE_CLIENT --query="SELECT count(*) FROM line_as_string";
+$CLICKHOUSE_CLIENT --query="truncate line_as_string";
+$CLICKHOUSE_CLIENT --query="INSERT INTO line_as_string FORMAT LineAsString" < "${CLICKHOUSE_TMP}"/data2
+$CLICKHOUSE_CLIENT --query="SELECT count(*) FROM line_as_string";
 $CLICKHOUSE_CLIENT --query="DROP TABLE line_as_string"
 
