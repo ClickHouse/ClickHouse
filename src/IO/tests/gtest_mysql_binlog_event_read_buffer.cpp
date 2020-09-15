84c68947b947a382b5424a4f6713b34b12072aec
zhang2014 <coswde@gmail.com>
2020-09-15 22:07:03 +0800

    Add test for MySQLBinlogReadBuffer

--- src/IO/tests/gtest_mysql_binlog_event_read_buffer.cpp
+++ src/IO/tests/gtest_mysql_binlog_event_read_buffer.cpp
@@ -1,5 +1,6 @@
 #include <gtest/gtest.h>
 #include <Common/Exception.h>
+#include <IO/ConcatReadBuffer.h>
 #include <IO/ReadBufferFromMemory.h>
 #include <IO/MySQLBinlogEventReadBuffer.h>
 
@@ -17,4 +18,41 @@ TEST(MySQLBinlogEventReadBuffer, CheckBoundary)
     }
 }
 
+TEST(MySQLBinlogEventReadBuffer, NiceBufferSize)
+{
+    char res[2];
+    std::vector<char> memory_data(6, 0x01);
+    ReadBufferFromMemory nested_in(memory_data.data(), 6);
+
+    MySQLBinlogEventReadBuffer binlog_in(nested_in);
+    binlog_in.readStrict(res, 2);
+    ASSERT_EQ(res[0], 0x01);
+    ASSERT_EQ(res[1], 0x01);
+    ASSERT_TRUE(binlog_in.eof());
+}
+
+TEST(MySQLBinlogEventReadBuffer, BadBufferSizes)
+{
+    char res[4];
+    std::vector<ReadBufferPtr> buffers;
+    std::vector<ReadBuffer *> nested_buffers;
+    std::vector<std::shared_ptr<std::vector<char>>> memory_buffers_data;
+    std::vector<size_t> bad_buffers_size = {2, 1, 2, 3};
+
+    for (const auto & bad_buffer_size : bad_buffers_size)
+    {
+        memory_buffers_data.emplace_back(std::make_shared<std::vector<char>>(bad_buffer_size, 0x01));
+        buffers.emplace_back(std::make_shared<ReadBufferFromMemory>(memory_buffers_data.back()->data(), bad_buffer_size));
+        nested_buffers.emplace_back(buffers.back().get());
+    }
+
+    ConcatReadBuffer concat_buffer(nested_buffers);
+    MySQLBinlogEventReadBuffer binlog_in(concat_buffer);
+    binlog_in.readStrict(res, 4);
+    ASSERT_EQ(res[0], 0x01);
+    ASSERT_EQ(res[1], 0x01);
+    ASSERT_EQ(res[2], 0x01);
+    ASSERT_EQ(res[3], 0x01);
+    ASSERT_TRUE(binlog_in.eof());
+}
 
