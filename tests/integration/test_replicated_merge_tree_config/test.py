e1736584225ee8c36c7eb4ac6eba024c470850ce
alesapin <alesapin@gmail.com>
2020-09-16 19:53:58 +0300

    Fix integration test

--- tests/integration/test_replicated_merge_tree_config/test.py
+++ tests/integration/test_replicated_merge_tree_config/test.py
@@ -1,14 +1,14 @@
 import pytest
 from helpers.cluster import ClickHouseCluster
+import logging
+
+cluster = ClickHouseCluster(__file__)
+node = cluster.add_instance("node", main_configs=["configs/config.xml"], with_zookeeper=True)
 
 
 @pytest.fixture(scope="module")
-def cluster():
+def start_cluster():
     try:
-        cluster = ClickHouseCluster(__file__)
-        cluster.add_instance(
-            "node", config_dir="configs", with_zookeeper=True,
-        )
         logging.info("Starting cluster...")
         cluster.start()
         logging.info("Cluster started")
@@ -19,19 +19,18 @@ def cluster():
 
 
 @pytest.fixture(autouse=True)
-def drop_table(cluster):
+def drop_table(start_cluster):
     yield
     for node in cluster.instances.values():
         node.query("DROP TABLE IF EXISTS test1")
         node.query("DROP TABLE IF EXISTS test2")
 
 
-def test_replicated_merge_tree_settings(cluster):
-    node = cluster.instances["node"]
+def test_replicated_merge_tree_settings(start_cluster):
     node.query("CREATE TABLE test1 (id Int64) ENGINE MergeTree ORDER BY id")
     node.query(
         "CREATE TABLE test2 (id Int64) ENGINE ReplicatedMergeTree('/clickhouse/test', 'test') ORDER BY id"
     )
 
-    assert node.query("SHOW CREATE test1").endswith("100")
-    assert node.query("SHOW CREATE test2").endswith("200")
+    assert node.query("SHOW CREATE test1").strip().endswith("100")
+    assert node.query("SHOW CREATE test2").strip().endswith("200")
