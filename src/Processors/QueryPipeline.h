e013acb6b66e0986e2a33d5d6060ab0bd1c27dac
Nikolai Kochetov <nik-kochetov@yandex-team.ru>
2020-09-17 16:26:34 +0300

    Fix build.

--- src/Processors/QueryPipeline.h
+++ src/Processors/QueryPipeline.h
@@ -100,7 +100,7 @@ public:
     void addStorageHolder(StoragePtr storage) { pipe.addStorageHolder(std::move(storage)); }
     void addQueryPlan(std::unique_ptr<QueryPlan> plan) { pipe.addQueryPlan(std::move(plan)); }
     void setLimits(const StreamLocalLimits & limits) { pipe.setLimits(limits); }
-    void setQuota(const std::shared_ptr<const EnabledQuota> & quota) { pipe.setQuota(quota); };
+    void setQuota(const std::shared_ptr<const EnabledQuota> & quota) { pipe.setQuota(quota); }
 
     /// For compatibility with IBlockInputStream.
     void setProgressCallback(const ProgressCallback & callback);
