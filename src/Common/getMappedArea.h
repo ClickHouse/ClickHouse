068e8576b7a930c4d7fdd9420a17e69c8b93eaaf
Alexey Milovidov <milovidov@yandex-team.ru>
2020-09-17 15:53:52 +0300

    Corrections

---
+++ src/Common/getMappedArea.h
@@ -0,0 +1,10 @@
+#include <utility>
+
+namespace DB
+{
+
+/// Find the address and size of the mapped memory region pointed by ptr.
+/// Throw exception if not found.
+std::pair<void *, size_t> getMappedArea(void * ptr);
+
+}
