7def5285f89a396ee5a38c1f504faf75850de75b
Azat Khuzhin <a3at.mail@gmail.com>
2020-09-19 00:35:34 +0300

    Update shebang
    $ gg 'env python$' | cut -d: -f1 | sort -u | xargs sed -i 's/env python$/env python3/g'

--- tests/queries/0_stateless/00990_hasToken.python
+++ tests/queries/0_stateless/00990_hasToken.python
@@ -1,4 +1,4 @@
-#!/usr/bin/env python
+#!/usr/bin/env python3
 # encoding: utf-8
 
 import re
--- utils/make_changelog.py
+++ utils/make_changelog.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env python
+#!/usr/bin/env python3
 # Note: should work with python 2 and 3
 
 
