bc28a62c7252ba7ea482fc63f48a5b27ef1ba3a1
robot-clickhouse <robot-clickhouse@yandex-team.ru>
2020-09-19 16:01:30 +0300

    Backport #14495 to 20.8: updatePermumation with Nullable

--- src/Columns/ColumnTuple.cpp
+++ src/Columns/ColumnTuple.cpp
@@ -344,15 +344,19 @@ void ColumnTuple::getPermutation(bool reverse, size_t limit, int nan_direction_h
     }
 }
 
-void ColumnTuple::updatePermutation(bool reverse, size_t limit, int nan_direction_hint, IColumn::Permutation & res, EqualRanges & equal_range) const
+void ColumnTuple::updatePermutation(bool reverse, size_t limit, int nan_direction_hint, IColumn::Permutation & res, EqualRanges & equal_ranges) const
 {
-    for (const auto& column : columns)
-    {
-        column->updatePermutation(reverse, limit, nan_direction_hint, res, equal_range);
-        while (limit && !equal_range.empty() && limit <= equal_range.back().first)
-            equal_range.pop_back();
+    if (equal_ranges.empty())
+        return;
 
-        if (equal_range.empty())
+    for (const auto & column : columns)
+    {
+        column->updatePermutation(reverse, limit, nan_direction_hint, res, equal_ranges);
+
+        while (limit && !equal_ranges.empty() && limit <= equal_ranges.back().first)
+            equal_ranges.pop_back();
+
+        if (equal_ranges.empty())
             break;
     }
 }
