eb260f6a89d95646487f6421a8c2ca5829a86933
Alexey Milovidov <milovidov@yandex-team.ru>
2020-09-19 13:43:26 +0300

    Fix bad code

--- src/IO/S3/PocoHTTPClient.cpp
+++ src/IO/S3/PocoHTTPClient.cpp
@@ -1,3 +1,7 @@
+#include <Common/config.h>
+
+#if USE_AWS_S3
+
 #include "PocoHTTPClient.h"
 
 #include <utility>
@@ -13,6 +17,7 @@
 #include <Poco/Net/HTTPRequest.h>
 #include <Poco/Net/HTTPResponse.h>
 #include <common/logger_useful.h>
+
 
 namespace ProfileEvents
 {
@@ -264,3 +269,5 @@ void PocoHTTPClient::makeRequestInternal(
     }
 }
 }
+
+#endif
