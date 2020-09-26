11ab250a712c686b6fee7ea14df60d2c57634bf3
Alexey Milovidov <milovidov@yandex-team.ru>
2020-09-26 05:18:18 +0300

    More test cases

--- tests/queries/0_stateless/01508_query_obfuscator.sh
+++ tests/queries/0_stateless/01508_query_obfuscator.sh
@@ -4,3 +4,5 @@ CURDIR=$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)
 . "$CURDIR"/../shell_config.sh
 
 $CLICKHOUSE_FORMAT --seed Hello --obfuscate <<< "SELECT 123, 'Test://2020-01-01hello1234 at 2000-01-01T01:02:03', 12e100, Gibberish_id_testCool, hello(World), avgIf(remote('127.0.0.1'))"
+$CLICKHOUSE_FORMAT --seed Hello --obfuscate <<< "SELECT cost_first_screen between a and b, case when x >= 123 then y else null end"
+
