bd790f399fab0a63c74b85130f780cb1e0c1efc9
Alexander Tokmakov <avtokmakov@yandex-team.ru>
2020-10-02 22:34:37 +0300

    simplify test

--- tests/queries/0_stateless/01149_zookeeper_mutation_stuck_after_replace_partition.sql
+++ tests/queries/0_stateless/01149_zookeeper_mutation_stuck_after_replace_partition.sql
@@ -1,39 +1,28 @@
 drop table if exists mt;
 drop table if exists rmt;
 
+create table mt (n UInt64, s String) engine = MergeTree partition by intDiv(n, 10) order by n;
+insert into mt values (3, '3'), (4, '4');
 
-create table mt (`n` UInt64) engine = MergeTree partition by intDiv(n, 10) order by n;
+create table rmt (n UInt64, s String) engine = ReplicatedMergeTree('/clickhouse/test_01149/rmt', 'r1') partition by intDiv(n, 10) order by n;
+insert into rmt values (1,'1'), (2, '2');
 
-insert into mt values (3), (4), (33), (44);
-
-create table rmt (`n` UInt64) engine = ReplicatedMergeTree('/clickhouse/test_01149/rmt1', 'r1') partition by intDiv(n, 10) order by n;
-
-insert into rmt values (1), (2);
 select * from rmt;
 select * from mt;
+select table, partition_id, name, rows from system.parts where database=currentDatabase() and table in ('mt', 'rmt') order by table, name;
 
-select database, table, partition_id, name, rows from system.parts where database=currentDatabase() and table in ('mt', 'rmt') order by table, name;
-
-alter table rmt replace partition '0' from mt;
-select * from rmt;
-select database, table, partition_id, name, rows from system.parts where database=currentDatabase() and table in ('mt', 'rmt') order by table, name;
-
-alter table rmt add column s String;
-alter table rmt drop column s;
-select * from rmt;
-alter table rmt add column s String;
-insert into rmt values (1, '1'), (2, '2');
-alter table mt add column s String;
+alter table rmt update s = 's'||toString(n) where 1;    -- increment mutation version
 select * from rmt;
 alter table rmt replace partition '0' from mt;      -- (probably) removes parts incorrectly
 
-select database, table, partition_id, name, rows from system.parts where database=currentDatabase() and table in ('mt', 'rmt') order by table, name;
+--system restart replica rmt;  -- workaround
+
+select table, partition_id, name, rows from system.parts where database=currentDatabase() and table in ('mt', 'rmt') order by table, name;
 
 alter table rmt drop column s;  -- hangs waiting for mutation of removed parts
 
 -- see parts_to_do
-select * from system.mutations where database=currentDatabase() and table='rmt';
-
+select mutation_id, command, parts_to_do_names, parts_to_do, is_done from system.mutations where database=currentDatabase() and table='rmt';
 select * from rmt;
 
 drop table mt;
