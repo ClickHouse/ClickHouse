4d36c869857a9076c0e6b810dac1cd2b09a62475
hchen9 <hchen9@ebay.com>
2020-10-02 11:17:28 -0700

    Add newline in result reference

--- tests/queries/0_stateless/01513_count_without_select_sequence_consistency.sql
+++ tests/queries/0_stateless/01513_count_without_select_sequence_consistency.sql
@@ -25,7 +25,7 @@ INSERT INTO quorum2 VALUES (4, toDate('2020-12-16'));
 
 SYSTEM SYNC REPLICA quorum3;
 
--- Should read local committed parts without throwing error code: 289. DB::Exception: Replica doesn't have part 20201216_1_1_0 which was successfully written to quorum of other replicas.
+-- Should read local committed parts instead of throwing error code: 289. DB::Exception: Replica doesn't have part 20201216_1_1_0 which was successfully written to quorum of other replicas.
 SELECT count() FROM quorum1;
 
 SELECT count() FROM quorum2;
