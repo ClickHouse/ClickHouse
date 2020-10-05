e5080740a8d56b1241d57e3b4793de87f461f589
robot-clickhouse <robot-clickhouse@yandex-team.ru>
2020-10-05 14:25:36 +0300

    Backport #15516 to 20.7: Throw an error when a single parameter is passed to ReplicatedMergeTree instead of ignoring it

--- src/Storages/MergeTree/registerStorageMergeTree.cpp
+++ src/Storages/MergeTree/registerStorageMergeTree.cpp
@@ -433,7 +433,7 @@ static StoragePtr create(const StorageFactory::Arguments & args)
                         ErrorCodes::NO_REPLICA_NAME_GIVEN);
             ++arg_num;
         }
-        else if (is_extended_storage_def)
+        else if (is_extended_storage_def && arg_cnt == 0)
         {
             /// Try use default values if arguments are not specified.
             /// It works for ON CLUSTER queries when database engine is Atomic and there are {shard} and {replica} in config.
