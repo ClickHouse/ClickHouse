861889c0ee596f705f23a5e74324daffc710671d
Alexander Tokmakov <avtokmakov@yandex-team.ru>
2020-10-05 22:16:28 +0300

    fix another bug in mutations

--- tests/queries/0_stateless/01149_zookeeper_mutation_stuck_after_replace_partition.sql
+++ tests/queries/0_stateless/01149_zookeeper_mutation_stuck_after_replace_partition.sql
@@ -22,5 +22,17 @@ alter table rmt drop column s;
 select mutation_id, command, parts_to_do_names, parts_to_do, is_done from system.mutations where database=currentDatabase() and table='rmt';
 select * from rmt;
 
+drop table rmt sync;
+
+set replication_alter_partitions_sync=0;
+create table rmt (n UInt64, s String) engine = ReplicatedMergeTree('/clickhouse/test_01149/rmt', 'r1') partition by intDiv(n, 10) order by n;
+insert into rmt values (1,'1'), (2, '2');
+
+alter table rmt update s = 's'||toString(n) where 1;
+alter table rmt drop partition '0';
+
+set replication_alter_partitions_sync=1;
+alter table rmt drop column s;
+
 drop table mt;
 drop table rmt sync;
