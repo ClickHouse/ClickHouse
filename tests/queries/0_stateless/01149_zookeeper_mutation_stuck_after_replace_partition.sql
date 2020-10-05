30a18f7567d3209e79b39be76f40d751f0260f34
Alexander Tokmakov <avtokmakov@yandex-team.ru>
2020-10-05 16:52:03 +0300

    make it better

--- tests/queries/0_stateless/01149_zookeeper_mutation_stuck_after_replace_partition.sql
+++ tests/queries/0_stateless/01149_zookeeper_mutation_stuck_after_replace_partition.sql
@@ -1,5 +1,5 @@
 drop table if exists mt;
-drop table if exists rmt;
+drop table if exists rmt sync;
 
 create table mt (n UInt64, s String) engine = MergeTree partition by intDiv(n, 10) order by n;
 insert into mt values (3, '3'), (4, '4');
@@ -9,21 +9,18 @@ insert into rmt values (1,'1'), (2, '2');
 
 select * from rmt;
 select * from mt;
-select table, partition_id, name, rows from system.parts where database=currentDatabase() and table in ('mt', 'rmt') order by table, name;
+select table, partition_id, name, rows from system.parts where database=currentDatabase() and table in ('mt', 'rmt') and active=1 order by table, name;
 
-alter table rmt update s = 's'||toString(n) where 1;    -- increment mutation version
+alter table rmt update s = 's'||toString(n) where 1;
 select * from rmt;
-alter table rmt replace partition '0' from mt;      -- (probably) removes parts incorrectly
+alter table rmt replace partition '0' from mt;
 
---system restart replica rmt;  -- workaround
+select table, partition_id, name, rows from system.parts where database=currentDatabase() and table in ('mt', 'rmt') and active=1 order by table, name;
 
-select table, partition_id, name, rows from system.parts where database=currentDatabase() and table in ('mt', 'rmt') order by table, name;
+alter table rmt drop column s;
 
-alter table rmt drop column s;  -- hangs waiting for mutation of removed parts
-
--- see parts_to_do
 select mutation_id, command, parts_to_do_names, parts_to_do, is_done from system.mutations where database=currentDatabase() and table='rmt';
 select * from rmt;
 
 drop table mt;
-drop table rmt;
+drop table rmt sync;
