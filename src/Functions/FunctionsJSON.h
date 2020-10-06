72eeaea3c6eabbd544121dc6999df41442b9d541
robot-clickhouse <robot-clickhouse@yandex-team.ru>
2020-10-06 14:32:06 +0300

    Backport #15589 to 20.7: Use default implemetation for constants in JSON* functions

--- src/Functions/FunctionsJSON.h
+++ src/Functions/FunctionsJSON.h
@@ -279,7 +279,7 @@ public:
     String getName() const override { return Name::name; }
     bool isVariadic() const override { return true; }
     size_t getNumberOfArguments() const override { return 0; }
-    bool useDefaultImplementationForConstants() const override { return false; }
+    bool useDefaultImplementationForConstants() const override { return true; }
 
     DataTypePtr getReturnTypeImpl(const ColumnsWithTypeAndName & arguments) const override
     {
