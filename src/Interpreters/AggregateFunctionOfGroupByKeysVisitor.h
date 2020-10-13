778ccbc52fd666420aaf9a809c1409120f29fad2
zhang2014 <coswde@gmail.com>
2020-10-13 13:30:56 +0800

    ISSUES-12513 try fix difference expressions with same alias

--- src/Interpreters/AggregateFunctionOfGroupByKeysVisitor.h
+++ src/Interpreters/AggregateFunctionOfGroupByKeysVisitor.h
@@ -10,6 +10,7 @@
 #include <Parsers/ASTTablesInSelectQuery.h>
 #include <Parsers/IAST.h>
 #include <Common/typeid_cast.h>
+#include <Parsers/ASTSubquery.h>
 
 namespace DB
 {
@@ -86,7 +87,7 @@ public:
 
     static bool needChildVisit(const ASTPtr & node, const ASTPtr &)
     {
-        return !(node->as<ASTFunction>());
+        return !node->as<ASTSubquery>() && !(node->as<ASTFunction>());
     }
 
     static void visit(ASTPtr & ast, Data & data)
