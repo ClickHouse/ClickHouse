6d30622f1beae178f089cbc1e0b1ac0057b14f0d
nikitamikhaylov <mikhaylovnikitka@gmail.com>
2020-10-13 21:46:15 +0300

    test in comparison with scipy

--- src/AggregateFunctions/AggregateFunctionWelchTTest.h
+++ src/AggregateFunctions/AggregateFunctionWelchTTest.h
@@ -122,6 +122,16 @@ struct AggregateFunctionWelchTTestData final
         return std::pow(mean_x - mean_y, 2) / (getSxSquared() / size_x + getSySquared() / size_y);
     }
 
+    Float64 getTStatistic() const
+    {
+        if (size_x == 0 || size_y == 0)
+        {
+            throw Exception("Division by zero encountered in Aggregate function WelchTTest", ErrorCodes::BAD_ARGUMENTS);
+        }
+
+        return (mean_x - mean_y) / std::sqrt(getSxSquared() / size_x + getSySquared() / size_y);
+    }
+
     Float64 getDegreesOfFreedom() const
     {
         auto sx = getSxSquared();
@@ -154,9 +164,9 @@ struct AggregateFunctionWelchTTestData final
         return numenator / denominator;
     }
 
-    Float64 getResult() const
+    std::pair<Float64, Float64> getResult() const
     {
-        return getPValue();
+        return std::make_pair(getTStatistic(), getPValue());
     }
 };
 
@@ -178,7 +188,22 @@ public:
 
     DataTypePtr getReturnType() const override
     {
-        return std::make_shared<DataTypeNumber<Float64>>();
+        DataTypes types
+        {
+            std::make_shared<DataTypeNumber<Float64>>(),
+            std::make_shared<DataTypeNumber<Float64>>(),
+        };
+
+        Strings names
+        {
+            "t-statistic",
+            "p-value"
+        };
+
+        return std::make_shared<DataTypeTuple>(
+            std::move(types),
+            std::move(names)
+        );
     }
 
     void add(AggregateDataPtr place, const IColumn ** columns, size_t row_num, Arena *) const override
@@ -217,8 +242,16 @@ public:
             throw Exception("Aggregate function " + getName() + " requires samples to be of size > 1", ErrorCodes::BAD_ARGUMENTS);
         }
 
-        auto & column = static_cast<ColumnVector<Float64> &>(to);
-        column.getData().push_back(this->data(place).getResult());
+        Float64 t_statistic = 0.0;
+        Float64 p_value = 0.0;
+        std::tie(t_statistic, p_value) = this->data(place).getResult();
+
+        auto & column_tuple = assert_cast<ColumnTuple &>(to);
+        auto & column_stat = assert_cast<ColumnVector<Float64> &>(column_tuple.getColumn(0));
+        auto & column_value = assert_cast<ColumnVector<Float64> &>(column_tuple.getColumn(1));
+
+        column_stat.getData().push_back(t_statistic);
+        column_value.getData().push_back(p_value);
     }
 
 };
