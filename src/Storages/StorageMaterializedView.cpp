1003de75ed324c082bfde43d219acf07f0066f4b
tavplubix <avtokmakov@yandex-team.ru>
2020-10-15 15:06:44 +0300

    Update StorageMaterializedView.cpp

--- src/Storages/StorageMaterializedView.cpp
+++ src/Storages/StorageMaterializedView.cpp
@@ -125,7 +125,6 @@ Pipe StorageMaterializedView::read(
     Pipe pipe = storage->read(column_names, metadata_snapshot, query_info, context, processed_stage, max_block_size, num_streams);
     pipe.addTableLock(lock);
     pipe.addStorageHolder(storage);
-    
 
     return pipe;
 }
