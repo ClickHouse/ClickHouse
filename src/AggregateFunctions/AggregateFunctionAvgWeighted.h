8e7e232387fe19bc44ffaf39cb6057d3bb5e2283
myrrc <me@myrrec.space>
2020-10-15 13:36:00 +0300

    wip dealing with template magic

--- src/AggregateFunctions/AggregateFunctionAvgWeighted.h
+++ src/AggregateFunctions/AggregateFunctionAvgWeighted.h
@@ -4,21 +4,28 @@
 
 namespace DB
 {
-template <typename T, typename Data>
-class AggregateFunctionAvgWeighted final : public AggregateFunctionAvgBase<T, Data, AggregateFunctionAvgWeighted<T, Data>>
+
+template <typename Value, typename Weight, typename Largest, typename Data>
+class AggregateFunctionAvgWeighted final :
+    public AggregateFunctionAvgBase<Largest, Data, AggregateFunctionAvgWeighted<Value, Weight, Largest, Data>>
 {
 public:
-    using AggregateFunctionAvgBase<T, Data, AggregateFunctionAvgWeighted<T, Data>>::AggregateFunctionAvgBase;
+    using AggregateFunctionAvgBase<Largest, Data,
+        AggregateFunctionAvgWeighted<Value, Weight, Largest, Data>>::AggregateFunctionAvgBase;
 
+    template <class T>
     using ColVecType = std::conditional_t<IsDecimalNumber<T>, ColumnDecimal<T>, ColumnVector<T>>;
 
     void add(AggregateDataPtr place, const IColumn ** columns, size_t row_num, Arena *) const override
     {
-        const auto & values = static_cast<const ColVecType &>(*columns[0]);
-        const auto & weights = static_cast<const ColVecType &>(*columns[1]);
+        const auto & values = static_cast<const ColVecType<Value> &>(*columns[0]);
+        const auto & weights = static_cast<const ColVecType<Weight> &>(*columns[1]);
 
-        this->data(place).numerator += static_cast<typename Data::NumeratorType>(values.getData()[row_num]) * weights.getData()[row_num];
-        this->data(place).denominator += weights.getData()[row_num];
+        const auto value = values.getData()[row_num];
+        const auto weight = weights.getData()[row_num];
+
+        this->data(place).numerator += static_cast<typename Data::NumeratorType>(value) * weight;
+        this->data(place).denominator += weight;
     }
 
     String getName() const override { return "avgWeighted"; }
