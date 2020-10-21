69ae6ace4789cb5a906ecb0a500384326e3965ac
Nikolai Kochetov <nik-kochetov@yandex-team.ru>
2020-10-21 21:17:27 +0300

    Add untuple.

--- src/Interpreters/ActionsVisitor.h
+++ src/Interpreters/ActionsVisitor.h
@@ -118,6 +118,7 @@ public:
         bool no_makeset;
         bool only_consts;
         bool no_storage_or_local;
+        bool has_untuple = false;
         size_t visit_depth;
         ScopeStack actions_stack;
 
@@ -191,8 +192,11 @@ private:
     static void visit(const ASTIdentifier & identifier, const ASTPtr & ast, Data & data);
     static void visit(const ASTFunction & node, const ASTPtr & ast, Data & data);
     static void visit(const ASTLiteral & literal, const ASTPtr & ast, Data & data);
+    static void visit(ASTExpressionList & expression_list, const ASTPtr & ast, Data & data);
 
     static SetPtr makeSet(const ASTFunction & node, Data & data, bool no_subqueries);
+    static ASTs doUntuple(const ASTFunction * function, ActionsMatcher::Data & data);
+    static std::optional<NameAndTypePair> getNameAndTypeFromAST(const ASTPtr & ast, Data & data);
 };
 
 using ActionsVisitor = ActionsMatcher::Visitor;
