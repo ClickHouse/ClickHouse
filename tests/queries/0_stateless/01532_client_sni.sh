50bccadd8577c3d66354526e85bf7c48be41875b
alexey-milovidov <milovidov@yandex-team.ru>
2020-10-22 05:27:23 +0300

    Update 01532_client_sni.sh

--- tests/queries/0_stateless/01532_client_sni.sh
+++ tests/queries/0_stateless/01532_client_sni.sh
@@ -3,7 +3,7 @@
 CURDIR=$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)
 . "$CURDIR"/../shell_config.sh
 
-# Check that ClickHouse properly use SNI extension in Client Hello packet in HTTPS connection.
+# Check that ClickHouse properly uses SNI extension in Client Hello packet in HTTPS connection.
 
 strace -f -x -s10000 -e trace=write,sendto ${CLICKHOUSE_LOCAL} --query "SELECT * FROM url('https://yandex.ru/', RawBLOB, 'data String')" 2>&1 |
     grep -oF '\x00\x00\x00\x0e\x00\x0c\x00\x00\x09\x79\x61\x6e\x64\x65\x78\x2e\x72\x75'
