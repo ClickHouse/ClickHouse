fe9440689a8bf9d2ef173e8e6aac1e787867ca7d
nikitamikhaylov <mikhaylovnikitka@gmail.com>
2020-10-24 00:05:24 +0300

    better

--- src/Interpreters/ActionsVisitor.h
+++ src/Interpreters/ActionsVisitor.h
@@ -132,7 +132,7 @@ public:
         Data(const Context & context_, SizeLimits set_size_limit_, size_t subquery_depth_,
                 const NamesAndTypesList & source_columns_, ActionsDAGPtr actions,
                 PreparedSets & prepared_sets_, SubqueriesForSets & subqueries_for_sets_,
-                bool no_subqueries_, bool no_makeset_, bool only_consts_, bool no_storage_or_local_, 
+                bool no_subqueries_, bool no_makeset_, bool only_consts_, bool no_storage_or_local_,
                 bool has_having);
 
         /// Does result of the calculation already exists in the block.
