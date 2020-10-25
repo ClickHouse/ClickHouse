fb5d281c69d3e546545e2fcaaca431f98f0cb2a8
Alexey Milovidov <milovidov@yandex-team.ru>
2020-10-25 05:00:30 +0300

    Better test

--- tests/queries/0_stateless/01532_client_sni.sh
+++ tests/queries/0_stateless/01532_client_sni.sh
@@ -5,8 +5,11 @@ CURDIR=$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)
 
 # Check that ClickHouse properly uses SNI extension in Client Hello packet in HTTPS connection.
 
-echo -ne 'y\r\n' | strace -f -x -s10000 -e trace=write,sendto ${CLICKHOUSE_LOCAL} --query "SELECT * FROM url('https://localhost:${CLICKHOUSE_PORT_HTTPS}/', RawBLOB, 'data String')" 2>&1 |
-    grep -oF '\x00\x00\x00\x0e\x00\x0c\x00\x00\x09\x6c\x6f\x63\x61\x6c\x68\x6f\x73\x74'
+nc -q0 -l 5678 | xxd -p | grep -oF $'0000000e000c0000096c6f63616c686f7374' &
+
+${CLICKHOUSE_LOCAL} --query "SELECT * FROM url('https://localhost:5678/', RawBLOB, 'data String')" 2>&1 | grep -v -F 'Timeout'
+
+#   grep -oF '\x00\x00\x00\x0e\x00\x0c\x00\x00\x09\x6c\x6f\x63\x61\x6c\x68\x6f\x73\x74'
 #              ^^^^^^^^ ^^^^^^^ ^^^^^^^ ^^ ^^^^^^^ ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
 #                  |       |      |      |      |
 #           server name  data     |   hostname  |   l   o   c   a   l   h   o   s  t
