f65dfdb25d4c29d06abdd3be8228065986f148a2
Nikolai Kochetov <nik-kochetov@yandex-team.ru>
2020-10-28 18:54:19 +0300

    Update ActionsDAG::Index.

--- src/Processors/Transforms/ExpressionTransform.cpp
+++ src/Processors/Transforms/ExpressionTransform.cpp
@@ -1,6 +1,7 @@
 #include <Processors/Transforms/ExpressionTransform.h>
 #include <Interpreters/ExpressionAnalyzer.h>
 #include <Interpreters/ExpressionActions.h>
+#include <DataTypes/DataTypesNumber.h>
 
 namespace DB
 {
@@ -20,11 +21,14 @@ ExpressionTransform::ExpressionTransform(const Block & header_, ExpressionAction
 
 void ExpressionTransform::transform(Chunk & chunk)
 {
+    size_t num_rows = chunk.getNumRows();
     auto block = getInputPort().getHeader().cloneWithColumns(chunk.detachColumns());
+    if (!block)
+        block.insert({DataTypeUInt8().createColumnConst(num_rows, 0), std::make_shared<DataTypeUInt8>(), "_dummy"});
 
     expression->execute(block);
 
-    auto num_rows = block.rows();
+    num_rows = block.rows();
     chunk.setColumns(block.getColumns(), num_rows);
 }
 
