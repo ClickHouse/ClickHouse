f1a629b1ae6d492f9cd5613213fbfc1524cc5773
alesapin <alesapin@gmail.com>
2020-10-29 17:25:44 +0300

    Split requests and responses part from zookeeper

--- src/Common/ZooKeeper/ZooKeeperImpl.h
+++ src/Common/ZooKeeper/ZooKeeperImpl.h
@@ -5,6 +5,7 @@
 #include <Common/CurrentMetrics.h>
 #include <Common/ThreadPool.h>
 #include <Common/ZooKeeper/IKeeper.h>
+#include <Common/ZooKeeper/ZooKeeperCommon.h>
 
 #include <IO/ReadBuffer.h>
 #include <IO/WriteBuffer.h>
@@ -84,9 +85,6 @@ namespace Coordination
 {
 
 using namespace DB;
-
-struct ZooKeeperRequest;
-
 
 /** Usage scenario: look at the documentation for IKeeper class.
   */
@@ -193,7 +191,7 @@ private:
 
     struct RequestInfo
     {
-        std::shared_ptr<ZooKeeperRequest> request;
+        ZooKeeperRequestPtr request;
         ResponseCallback callback;
         WatchCallback watch;
         clock::time_point time;
@@ -245,32 +243,5 @@ private:
 
     CurrentMetrics::Increment active_session_metric_increment{CurrentMetrics::ZooKeeperSession};
 };
-
-struct ZooKeeperResponse;
-using ZooKeeperResponsePtr = std::shared_ptr<ZooKeeperResponse>;
-
-/// Exposed in header file for Yandex.Metrica code.
-struct ZooKeeperRequest : virtual Request
-{
-    ZooKeeper::XID xid = 0;
-    bool has_watch = false;
-    /// If the request was not send and the error happens, we definitely sure, that is has not been processed by the server.
-    /// If the request was sent and we didn't get the response and the error happens, then we cannot be sure was it processed or not.
-    bool probably_sent = false;
-
-    ZooKeeperRequest() = default;
-    ZooKeeperRequest(const ZooKeeperRequest &) = default;
-    virtual ~ZooKeeperRequest() override = default;
-
-    virtual ZooKeeper::OpNum getOpNum() const = 0;
-
-    /// Writes length, xid, op_num, then the rest.
-    void write(WriteBuffer & out) const;
-
-    virtual void writeImpl(WriteBuffer &) const = 0;
-
-    virtual ZooKeeperResponsePtr makeResponse() const = 0;
-};
-
 
 }
