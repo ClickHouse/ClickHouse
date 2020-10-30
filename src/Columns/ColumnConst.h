bcd660bb57862b2aae0572518c1ecde2be59c21b
Alexey Milovidov <milovidov@yandex-team.ru>
2020-10-30 08:35:18 +0300

    Minor fixes

--- src/Columns/ColumnConst.h
+++ src/Columns/ColumnConst.h
@@ -248,6 +248,8 @@ public:
     /// The constant value. It is valid even if the size of the column is 0.
     template <typename T>
     T getValue() const { return getField().safeGet<NearestFieldType<T>>(); }
+
+    bool isCollationSupported() const override { return true; }
 };
 
 }
