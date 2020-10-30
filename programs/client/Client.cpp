d299a5df67fe39df3eb733c355153a973c2b08f0
Ivan Lezhankin <ilezhankin@yandex-team.ru>
2020-10-30 05:04:10 +0300

    Remove ANALYZE and AST queries

--- programs/client/Client.cpp
+++ programs/client/Client.cpp
@@ -1499,7 +1499,7 @@ private:
 
     ASTPtr parseQuery(const char * & pos, const char * end, bool allow_multi_statements)
     {
-        ParserQuery parser(end, true);
+        ParserQuery parser(end);
         ASTPtr res;
 
         const auto & settings = context.getSettingsRef();
