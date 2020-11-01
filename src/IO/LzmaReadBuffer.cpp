78182efc28bbe781316cf74c27e12cacb657419c
a.palagashvili <a.palagashvili@corp.mail.ru>
2020-11-01 03:14:40 +0300

    changed moment of stream object initialization

--- src/IO/LzmaReadBuffer.cpp
+++ src/IO/LzmaReadBuffer.cpp
@@ -10,6 +10,7 @@ LzmaReadBuffer::LzmaReadBuffer(
     std::unique_ptr<ReadBuffer> in_, size_t buf_size = DBMS_DEFAULT_BUFFER_SIZE, char * existing_memory = nullptr, size_t alignment = 0)
     : BufferWithOwnMemory<ReadBuffer>(buf_size, existing_memory, alignment), in(std::move(in_))
 {
+    lstr = LZMA_STREAM_INIT;
     lstr.allocator = nullptr;
     lstr.next_in = nullptr;
     lstr.avail_in = 0;
@@ -19,7 +20,6 @@ LzmaReadBuffer::LzmaReadBuffer(
     // 500 mb
     uint64_t memlimit = 500 << 30;
 
-    lstr = LZMA_STREAM_INIT;
     lzma_ret ret = lzma_stream_decoder(&lstr, memlimit, LZMA_CONCATENATED | LZMA_IGNORE_CHECK);
     // lzma does not provide api for converting error code to string unlike zlib
     if (ret != LZMA_OK)
