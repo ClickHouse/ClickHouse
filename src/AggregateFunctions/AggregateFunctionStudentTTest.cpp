ac7267ce48a1f2d95c7174db698ff40249b89d47
Alexey Milovidov <milovidov@yandex-team.ru>
2020-11-02 12:50:08 +0300

    Make the code less bad
    (cherry picked from commit 911cd77c1a2981b33feae5576637f9a9f420c48d)

--- src/AggregateFunctions/AggregateFunctionStudentTTest.cpp
+++ src/AggregateFunctions/AggregateFunctionStudentTTest.cpp
@@ -1,18 +1,16 @@
 #include <AggregateFunctions/AggregateFunctionFactory.h>
-#include <AggregateFunctions/AggregateFunctionStudentTTest.h>
+#include <AggregateFunctions/AggregateFunctionTTest.h>
 #include <AggregateFunctions/FactoryHelpers.h>
+#include <AggregateFunctions/Moments.h>
+
 #include "registerAggregateFunctions.h"
 
-#include <AggregateFunctions/Helpers.h>
-#include <DataTypes/DataTypeAggregateFunction.h>
-
-
-// the return type is boolean (we use UInt8 as we do not have boolean in clickhouse)
 
 namespace ErrorCodes
 {
-extern const int NOT_IMPLEMENTED;
+    extern const int BAD_ARGUMENTS;
 }
+
 
 namespace DB
 {
@@ -20,33 +18,53 @@ namespace DB
 namespace
 {
 
+struct StudentTTestData : public TTestMoments<Float64>
+{
+    static constexpr auto name = "studentTTest";
+
+    std::pair<Float64, Float64> getResult() const
+    {
+        Float64 degrees_of_freedom = 2.0 * (m0 - 1);
+
+        Float64 mean_x = x1 / m0;
+        Float64 mean_y = y1 / m0;
+
+        /// Calculate s^2
+
+        /// The original formulae looks like
+        /// \frac{\sum_{i = 1}^{n_x}{(x_i - \bar{x}) ^ 2} + \sum_{i = 1}^{n_y}{(y_i - \bar{y}) ^ 2}}{n_x + n_y - 2}
+        /// But we made some mathematical transformations not to store original sequences.
+        /// Also we dropped sqrt, because later it will be squared later.
+
+        Float64 all_x = x2 + m0 * mean_x * mean_x - 2 * mean_x * m0;
+        Float64 all_y = y2 + m0 * mean_y * mean_y - 2 * mean_y * m0;
+
+        Float64 s2 = (all_x + all_y) / degrees_of_freedom;
+        Float64 std_err2 = 2.0 * s2 / m0;
+
+        /// t-statistic, squared
+        Float64 t_stat = (mean_x - mean_y) / sqrt(std_err2);
+
+        return {t_stat, getPValue(degrees_of_freedom, t_stat * t_stat)};
+    }
+};
+
 AggregateFunctionPtr createAggregateFunctionStudentTTest(const std::string & name, const DataTypes & argument_types, const Array & parameters)
 {
     assertBinary(name, argument_types);
     assertNoParameters(name, parameters);
 
-    AggregateFunctionPtr res;
+    if (!isNumber(argument_types[0]) || !isNumber(argument_types[1]))
+        throw Exception("Aggregate function " + name + " only supports numerical types", ErrorCodes::BAD_ARGUMENTS);
 
-    if (isDecimal(argument_types[0]) || isDecimal(argument_types[1]))
-    {
-        throw Exception("Aggregate function " + name + " only supports numerical types", ErrorCodes::NOT_IMPLEMENTED);
-    }
-    else
-    {
-        res.reset(createWithTwoNumericTypes<AggregateFunctionStudentTTest>(*argument_types[0], *argument_types[1], argument_types));
-    }
-
-    if (!res)
-    {
-        throw Exception("Aggregate function " + name + " only supports numerical types", ErrorCodes::NOT_IMPLEMENTED);
-    }
-
-    return res;
+    return std::make_shared<AggregateFunctionTTest<StudentTTestData>>(argument_types);
 }
+
 }
 
 void registerAggregateFunctionStudentTTest(AggregateFunctionFactory & factory)
 {
     factory.registerFunction("studentTTest", createAggregateFunctionStudentTTest, AggregateFunctionFactory::CaseInsensitive);
 }
+
 }
