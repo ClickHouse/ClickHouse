5daf042cd323bf6c1f8bca4ad917698bd3f6f566
nikitamikhaylov <mikhaylovnikitka@gmail.com>
2020-11-05 19:11:32 +0300

    style

--- src/Interpreters/ExpressionActions.cpp
+++ src/Interpreters/ExpressionActions.cpp
@@ -375,7 +375,7 @@ void ExpressionAction::execute(Block & block, bool dry_run) const
             auto source = block.getByName(source_name);
             block.erase(source_name);
             source.column = source.column->convertToFullColumnIfConst();
-            
+
             const ColumnArray * array = typeid_cast<const ColumnArray *>(source.column.get());
             if (!array)
                 throw Exception("ARRAY JOIN of not array: " + source_name, ErrorCodes::TYPE_MISMATCH);
