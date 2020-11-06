9fbc436e62d228af2493f0e32a0f59388976d238
Amos Bird <amosbird@gmail.com>
2020-11-06 11:50:58 +0800

    Transform single point

--- src/Storages/MergeTree/PartitionPruner.h
+++ src/Storages/MergeTree/PartitionPruner.h
@@ -9,6 +9,7 @@
 namespace DB
 {
 
+/// Pruning partitions in verbatim way using KeyCondition
 class PartitionPruner
 {
 private:
@@ -28,24 +29,7 @@ public:
     {
     }
 
-    bool canBePruned(DataPartPtr part)
-    {
-        if (part->isEmpty())
-            return true;
-        const auto & partition_id = part->info.partition_id;
-        bool is_valid;
-        if (auto it = partition_filter_map.find(partition_id); it != partition_filter_map.end())
-            is_valid = it->second;
-        else
-        {
-            const auto & partition_value = part->partition.value;
-            std::vector<FieldRef> index_value(partition_value.begin(), partition_value.end());
-            is_valid = partition_condition.mayBeTrueInRange(
-                partition_value.size(), index_value.data(), index_value.data(), partition_key.data_types);
-            partition_filter_map.emplace(partition_id, is_valid);
-        }
-        return !is_valid;
-    }
+    bool canBePruned(const DataPartPtr & part);
 
     bool isUseless() const { return useless; }
 };
