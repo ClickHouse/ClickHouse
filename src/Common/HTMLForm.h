c3973123bdd930692e4d40c81292edc028d191e9
Alexey Milovidov <milovidov@yandex-team.ru>
2020-11-07 19:03:23 +0300

    Fix error

--- src/Common/HTMLForm.h
+++ src/Common/HTMLForm.h
@@ -17,14 +17,12 @@ struct HTMLForm : public Poco::Net::HTMLForm
     {
         Poco::URI uri(request.getURI());
         std::istringstream istr(uri.getRawQuery());
-        istr.exceptions(std::ios::failbit);
         readUrl(istr);
     }
 
     HTMLForm(const Poco::URI & uri)
     {
         std::istringstream istr(uri.getRawQuery());
-        istr.exceptions(std::ios::failbit);
         readUrl(istr);
     }
 
