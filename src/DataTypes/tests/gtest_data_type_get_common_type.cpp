5e5779adfd2724b09060737e4126a80775eeb71a
Alexey Milovidov <milovidov@yandex-team.ru>
2020-11-07 19:11:20 +0300

    Fix error

--- src/DataTypes/tests/gtest_data_type_get_common_type.cpp
+++ src/DataTypes/tests/gtest_data_type_get_common_type.cpp
@@ -27,7 +27,6 @@ static auto typeFromString(const std::string & str)
 static auto typesFromString(const std::string & str)
 {
     std::istringstream data_types_stream(str);
-    data_types_stream.exceptions(std::ios::failbit);
     DataTypes data_types;
     std::string data_type;
     while (data_types_stream >> data_type)
