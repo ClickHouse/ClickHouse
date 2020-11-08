6c126402717406de2b5f0b7e270370b68fd7086b
Amos Bird <amosbird@gmail.com>
2020-11-08 14:21:58 +0800

    Update tests/queries/0_stateless/01540_verbatim_partition_pruning.sql
    Co-authored-by: Azat Khuzhin <a3at.mail@gmail.com>

--- tests/queries/0_stateless/01540_verbatim_partition_pruning.sql
+++ tests/queries/0_stateless/01540_verbatim_partition_pruning.sql
@@ -11,4 +11,15 @@ select * from xy where intHash64(x) % 100 = intHash64(1) % 100;
 -- This works too
 select * from xy where x = 1;
 
+--
+-- Test for equality.
+-- It is special operator that treated as an always monotonic 
+--
+set max_rows_to_read=100;
+drop table if exists xy;
+create table xy(x int, y int) engine MergeTree partition by x % 100 order by y settings index_granularity = 4096;
+-- insert enough data to make minmax index not enough for partition prunning
+insert into xy select number, number from numbers(10000);
+select * from xy where x = 1;
+
 drop table if exists xy;
