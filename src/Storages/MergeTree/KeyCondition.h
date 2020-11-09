e392bc418b7bd2c0374b96c388be7827c3fa658c
Nikolai Kochetov <nik-kochetov@yandex-team.ru>
2020-11-09 16:20:56 +0300

    Fix build

--- src/Storages/MergeTree/KeyCondition.h
+++ src/Storages/MergeTree/KeyCondition.h
@@ -403,7 +403,7 @@ private:
         DataTypePtr & out_type);
 
     bool canConstantBeWrappedByFunctions(
-        const ASTPtr & node, size_t & out_key_column_num, DataTypePtr & out_key_column_type, Field & out_value, DataTypePtr & out_type);
+        const ASTPtr & ast, size_t & out_key_column_num, DataTypePtr & out_key_column_type, Field & out_value, DataTypePtr & out_type);
 
     /// If it's possible to make an RPNElement
     /// that will filter values (possibly tuples) by the content of 'prepared_set',
