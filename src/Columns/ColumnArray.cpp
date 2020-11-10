7287915b16fe86919b98eb6369270e7fb1ad990f
Nikolai Kochetov <nik-kochetov@yandex-team.ru>
2020-11-10 23:25:29 +0300

    Try fix ci.

--- src/Columns/ColumnArray.cpp
+++ src/Columns/ColumnArray.cpp
@@ -20,7 +20,10 @@
 #include <Common/WeakHash.h>
 #include <Common/HashTable/Hash.h>
 
-#include <miniselect/floyd_rivest_select.h>
+#if !defined(ARCADIA_BUILD)
+    #include <miniselect/floyd_rivest_select.h>
+#endif
+
 
 namespace DB
 {
@@ -783,7 +786,11 @@ void ColumnArray::getPermutationImpl(size_t limit, Permutation & res, Comparator
     auto less = [&cmp](size_t lhs, size_t rhs){ return cmp(lhs, rhs) < 0; };
 
     if (limit)
+#if !defined(ARCADIA_BUILD)
         miniselect::floyd_rivest_partial_sort(res.begin(), res.begin() + limit, res.end(), less);
+#else
+        std::partial_sort(res.begin(), res.begin() + limit, res.end(), less);
+#endif
     else
         std::sort(res.begin(), res.end(), less);
 }
@@ -835,8 +842,11 @@ void ColumnArray::updatePermutationImpl(size_t limit, Permutation & res, EqualRa
             return;
 
         /// Since then we are working inside the interval.
-
+#if !defined(ARCADIA_BUILD)
         miniselect::floyd_rivest_partial_sort(res.begin() + first, res.begin() + limit, res.begin() + last, less);
+#else
+        std::partial_sort(res.begin() + first, res.begin() + limit, res.begin() + last, less);
+#endif
         auto new_first = first;
         for (auto j = first + 1; j < limit; ++j)
         {
