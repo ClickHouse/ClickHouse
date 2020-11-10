9867387a5f7c1c9afd733913c81bb5b31e631c95
Amos Bird <amosbird@gmail.com>
2020-11-10 10:16:46 +0800

    Fix crash in agg empty arglist

---
+++ tests/queries/0_stateless/01560_crash_in_agg_empty_arglist.sql
@@ -0,0 +1,1 @@
+SELECT any() as t, substring(query, 1, 70) AS query, avg(memory_usage) usage, count() count FROM system.query_log WHERE event_date >= toDate(1604295323) AND event_time >= toDateTime(1604295323) AND type in (1,2,3,4) and initial_user in ('') and('all' = 'all' or(positionCaseInsensitive(query, 'all') = 1)) GROUP BY query ORDER BY usage desc LIMIT 5; -- { serverError 42 }
