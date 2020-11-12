6f4e5bcaac2916b94c04f3333accb1ad5b060a02
nikitamikhaylov <mikhaylovnikitka@gmail.com>
2020-11-12 23:11:04 +0300

    style

--- src/Interpreters/Context.h
+++ src/Interpreters/Context.h
@@ -523,7 +523,7 @@ public:
     BackgroundSchedulePool & getSchedulePool();
     BackgroundSchedulePool & getDistributedSchedulePool();
 
-    /// Has distributed_ddl configuration or not. 
+    /// Has distributed_ddl configuration or not.
     bool hasDistributedDDL() const;
     void setDDLWorker(std::unique_ptr<DDLWorker> ddl_worker);
     DDLWorker & getDDLWorker() const;
