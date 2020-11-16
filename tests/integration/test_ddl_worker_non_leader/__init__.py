f8397422880b2ed4f9fca020ba628df72717d871
alesapin <alesapin@gmail.com>
2020-11-16 15:47:02 +0300

    Add a test

---
+++ tests/integration/test_ddl_worker_non_leader/__init__.py
@@ -0,0 +1,6 @@
+import pytest
+from helpers.cluster import ClickHouseCluster
+
+cluster = ClickHouseCluster(__file__)
+node1 = cluster.add_instance('node1', with_zookeeper=True)
+node2 = cluster.add_instance('node2', with_zookeeper=True)
