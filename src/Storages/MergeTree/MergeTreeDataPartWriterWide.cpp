7080f424e27f0e7e2c75804d2edd65bcbaa8c0fa
alesapin <alesapin@gmail.com>
2020-11-18 15:04:13 +0300

    Fix bug for skip indices and make code more complex

--- src/Storages/MergeTree/MergeTreeDataPartWriterWide.cpp
+++ src/Storages/MergeTree/MergeTreeDataPartWriterWide.cpp
@@ -96,7 +96,7 @@ void MergeTreeDataPartWriterWide::write(const Block & block,
     {
         size_t index_granularity_for_block = computeIndexGranularity(block);
         /// Finish last unfinished mark rows it it's required
-        adjustLastUnfinishedMark(index_granularity_for_block);
+        last_granule_was_adjusted = adjustLastUnfinishedMark(index_granularity_for_block);
         /// Fill index granularity with granules of new size
         fillIndexGranularity(index_granularity_for_block, block.rows());
     }
