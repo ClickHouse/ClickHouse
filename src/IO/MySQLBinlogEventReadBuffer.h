bed93ef71343c7b7069fa52fdfbecc2517747fc1
zhang2014 <coswde@gmail.com>
2020-11-19 12:02:43 +0800

    Try fix review comment

--- src/IO/MySQLBinlogEventReadBuffer.h
+++ src/IO/MySQLBinlogEventReadBuffer.h
@@ -8,12 +8,14 @@ namespace DB
 class MySQLBinlogEventReadBuffer : public ReadBuffer
 {
 protected:
+    static const size_t MAX_CHECKSUM_SIGNATURE_LENGTH = 4;
+
     ReadBuffer & in;
     size_t checksum_signature_length;
 
     size_t checksum_buff_size = 0;
     size_t checksum_buff_limit = 0;
-    char * checksum_buf = nullptr;
+    char checksum_buf[MAX_CHECKSUM_SIGNATURE_LENGTH];
 
     bool nextImpl() override;
 
