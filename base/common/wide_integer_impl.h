15b61298390fe379c611ada9712841b7e4532f13
fenglv <fenglv15@mails.ucas.ac.cn>
2020-11-22 03:42:16 +0000

    Avoid bigint_cast stack overflow

--- base/common/wide_integer_impl.h
+++ base/common/wide_integer_impl.h
@@ -239,6 +239,13 @@ struct integer<Bits, Signed>::_impl
     template <class T>
     constexpr static void set_multiplier(integer<Bits, Signed> & self, T t) noexcept {
         constexpr uint64_t max_int = std::numeric_limits<uint64_t>::max();
+
+        /// Replacing nan to zero, and inf to max(T) to avoid stack overflow in bigint_cast
+        if (isnan(t))
+            t = 0;
+        if (isinf(t))
+            t = std::numeric_limits<T>::max();
+
         const T alpha = t / max_int;
 
         if (alpha <= max_int)
