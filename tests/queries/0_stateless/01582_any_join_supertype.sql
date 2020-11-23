a885d851ec0347963efc3d238bcbc2f03c5e2d84
Alexey Milovidov <milovidov@yandex-team.ru>
2020-11-23 12:10:38 +0300

    More tests
    (cherry picked from commit 235541a532baf374f1745c3ae732ff865b4c8850)

--- tests/queries/0_stateless/01582_any_join_supertype.sql
+++ tests/queries/0_stateless/01582_any_join_supertype.sql
@@ -8,6 +8,7 @@ INSERT INTO foo VALUES ('2020-01-01', '2020-01-01 12:00:00', 'test1'), ('2020-01
 INSERT INTO bar VALUES ('2020-01-01', 'test2'), ('2020-01-01', 'test3');
 
 SET optimize_move_to_prewhere = 1;
+SET any_join_distinct_right_table_keys = 0;
 
 SELECT count()
 FROM foo ANY INNER JOIN bar USING (dimension_1)
@@ -20,6 +21,17 @@ WHERE toDate(foo.server_time, 'UTC') <= toDate('2020-04-30');
 
 SELECT toDateTime(foo.server_time, 'UTC') FROM foo
 SEMI JOIN bar USING (dimension_1) WHERE toDate(foo.server_time, 'UTC') <= toDate('2020-04-30');
+
+SET any_join_distinct_right_table_keys = 1;
+
+SELECT count()
+FROM foo ANY INNER JOIN bar USING (dimension_1)
+WHERE (foo.server_date <= '2020-11-07') AND (toDate(foo.server_time, 'Asia/Yekaterinburg') <= '2020-11-07');
+
+SELECT toDateTime(foo.server_time, 'UTC')
+FROM foo
+ANY INNER JOIN bar USING (dimension_1)
+WHERE toDate(foo.server_time, 'UTC') <= toDate('2020-04-30');
 
 DROP TABLE foo;
 DROP TABLE bar;
