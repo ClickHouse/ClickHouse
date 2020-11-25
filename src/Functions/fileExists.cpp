fe15236bf68f3893ce3de2c067c69492e4f43117
anton <anton@topvisor.com>
2020-11-25 11:48:01 +0300

    fix implementations

--- src/Functions/fileExists.cpp
+++ src/Functions/fileExists.cpp
@@ -32,7 +32,7 @@ public:
     static constexpr auto name = "fileExists";
     String getName() const override { return name; }
 
-    ColumnPtr execute(ColumnsWithTypeAndName & arguments, const DataTypePtr &, size_t input_rows_count) override
+    ColumnPtr execute(const ColumnsWithTypeAndName & arguments, const DataTypePtr &, size_t input_rows_count) override
     {
         const IColumn * arg_column = arguments[0].column.get();
         const ColumnString * arg_string = checkAndGetColumnConstData<ColumnString>(arg_column);
