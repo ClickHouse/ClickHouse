b198f2669e10cfea9ecc8d3a762bd1b9b68e0720
Azat Khuzhin <a3at.mail@gmail.com>
2020-11-26 23:29:37 +0300

    Fix and optimize countMatches()/countMatchesCaseInsensitive()
    - Update after IFunction interfaces changes
    - move type checks into FunctionCountMatches::getReturnTypeImpl()
    - Use StringRef over String
    - Separate out logic for counting sub matches into separate helper
    - Do not copy other regular expression matches, only the first
    - Add some comments
    - Set is_no_capture, to avoid check for number of subpatterns
    - Add countMatchesCaseInsensitive()
    - Reguster functions in case-sensitive manner, since this is not SQL
      standard

--- src/Functions/countMatches.cpp
+++ src/Functions/countMatches.cpp
@@ -1,13 +1,29 @@
 #include "FunctionFactory.h"
-#include "countMatchesImpl.h"
+#include "countMatches.h"
 
+namespace
+{
+
+struct FunctionCountMatchesCaseSensitive
+{
+    static constexpr auto name = "countMatches";
+    static constexpr bool case_insensitive = false;
+};
+struct FunctionCountMatchesCaseInsensitive
+{
+    static constexpr auto name = "countMatchesCaseInsensitive";
+    static constexpr bool case_insensitive = true;
+};
+
+}
 
 namespace DB
 {
 
 void registerFunctionCountMatches(FunctionFactory & factory)
 {
-    factory.registerFunction<FunctionCountMatches>(FunctionFactory::CaseInsensitive);
+    factory.registerFunction<FunctionCountMatches<FunctionCountMatchesCaseSensitive>>(FunctionFactory::CaseSensitive);
+    factory.registerFunction<FunctionCountMatches<FunctionCountMatchesCaseInsensitive>>(FunctionFactory::CaseSensitive);
 }
 
 }
