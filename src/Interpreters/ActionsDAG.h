32d64372c3a597bd05d75406d1b3fe3127698b93
Nikolai Kochetov <KochetovNicolai@users.noreply.github.com>
2020-11-26 15:09:56 +0300

    Update ActionsDAG.h

--- src/Interpreters/ActionsDAG.h
+++ src/Interpreters/ActionsDAG.h
@@ -136,7 +136,7 @@ public:
         void remove(Node * node)
         {
             auto it = map.find(node->result_name);
-            if (it == map.end())
+            if (it == map.end() || *it->second != node)
                 return;
 
             list.erase(it->second);
