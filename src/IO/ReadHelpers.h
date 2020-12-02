3e2447391bb4b5d686ec273904ba6a3ed751d5da
Alexey Milovidov <milovidov@yandex-team.ru>
2020-12-03 00:05:51 +0300

    Support for PROXY protocol

--- src/IO/ReadHelpers.h
+++ src/IO/ReadHelpers.h
@@ -476,6 +476,10 @@ void readStringUntilEOF(String & s, ReadBuffer & buf);
 // Buffer pointer is left at EOL, don't forget to advance it.
 void readEscapedStringUntilEOL(String & s, ReadBuffer & buf);
 
+/// Only 0x20 as whitespace character
+void readStringUntilWhitespace(String & s, ReadBuffer & buf);
+
+
 
 /** Read string in CSV format.
   * Parsing rules:
@@ -526,6 +530,9 @@ bool tryReadJSONStringInto(Vector & s, ReadBuffer & buf)
 {
     return readJSONStringInto<Vector, bool>(s, buf);
 }
+
+template <typename Vector>
+void readStringUntilWhitespaceInto(Vector & s, ReadBuffer & buf);
 
 /// This could be used as template parameter for functions above, if you want to just skip data.
 struct NullOutput
