e4a0ad33ae090c5039bfd68da5fe341196973ce8
Alexey Milovidov <milovidov@yandex-team.ru>
2020-12-03 00:09:46 +0300

    Added note

--- src/Server/TCPHandler.h
+++ src/Server/TCPHandler.h
@@ -104,6 +104,10 @@ public:
     /** parse_proxy_protocol_ - if true, expect and parse the header of PROXY protocol in every connection
       * and set the information about forwarded address accordingly.
       * See https://github.com/wolfeidau/proxyv2/blob/master/docs/proxy-protocol.txt
+      *
+      * Note: immediate IP address is always used for access control (accept-list of IP networks),
+      *  because it allows to check the IP ranges of the trusted proxy.
+      * Proxy-forwarded (original client) IP address is used for quota accounting if quota is keyed by forwarded IP.
       */
     TCPHandler(IServer & server_, const Poco::Net::StreamSocket & socket_, bool parse_proxy_protocol_)
         : Poco::Net::TCPServerConnection(socket_)
