a84354efb1a2cdd1f218d86fd0fe2f9565b05f78
Maksim Kita <kitaetoya@gmail.com>
2020-12-05 20:09:12 +0300

    Updated fast_float integration

--- cmake/find/fast_float.cmake
+++ cmake/find/fast_float.cmake
@@ -1,15 +1,6 @@
-option (USE_FAST_FLOAT "Use fast_float" ${ENABLE_LIBRARIES})
-
 if (NOT EXISTS "${ClickHouse_SOURCE_DIR}/contrib/fast_float/include/fast_float/fast_float.h")
-    message (WARNING "submodule contrib/fast_float is missing. to fix try run: \n git submodule update --init --recursive")
-    if (USE_FAST_FLOAT)
-        message (${RECONFIGURE_MESSAGE_LEVEL} "Can't find internal fast_float library")
-    endif()
-    return()
+    message (FATAL_ERROR "submodule contrib/fast_float is missing. to fix try run: \n git submodule update --init --recursive")
 endif ()
 
-if (USE_FAST_FLOAT)
-    set(FAST_FLOAT_INCLUDE_DIR "${ClickHouse_SOURCE_DIR}/contrib/fast_float/include/")
-endif ()
-
-message(STATUS "Using fast_float=${USE_FAST_FLOAT}")
+set(FAST_FLOAT_LIBRARY fast_float)
+set(FAST_FLOAT_INCLUDE_DIR "${ClickHouse_SOURCE_DIR}/contrib/fast_float/include/")
