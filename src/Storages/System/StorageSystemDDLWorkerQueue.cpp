48b107d9d28d0f3320d7cba58be26827190ceb17
bharatnc <bharatnc@gmail.com>
2020-12-05 12:34:13 -0800

    DDLWorkerQueueTable - populate active and finished columns

--- src/Storages/System/StorageSystemDDLWorkerQueue.cpp
+++ src/Storages/System/StorageSystemDDLWorkerQueue.cpp
@@ -1,7 +1,10 @@
-#include "StorageSystemDDLWorkerQueue.h"
-
 #include <algorithm>
 #include <string>
+
+#include "StorageSystemDDLWorkerQueue.h"
+
+#include <Columns/ColumnArray.h>
+#include <DataTypes/DataTypeArray.h>
 #include <DataTypes/DataTypeString.h>
 #include <Interpreters/Context.h>
 #include <Poco/Util/Application.h>
@@ -14,10 +17,11 @@ NamesAndTypesList StorageSystemDDLWorkerQueue::getNamesAndTypes()
 {
     return {
         {"name", std::make_shared<DataTypeString>()}, // query_<id>
-        {"active", std::make_shared<DataTypeString>()}, // string list of host_fqdn
-        {"finished", std::make_shared<DataTypeString>()}, // string list of host_fqdn
+        {"active", std::make_shared<DataTypeArray>(std::make_shared<DataTypeString>())}, // list of host_fqdn
+        {"finished", std::make_shared<DataTypeArray>(std::make_shared<DataTypeString>())}, // list of host_fqdn
     };
 }
+
 
 void StorageSystemDDLWorkerQueue::fillData(MutableColumns & res_columns, const Context & context, const SelectQueryInfo &) const
 {
@@ -38,7 +42,7 @@ void StorageSystemDDLWorkerQueue::fillData(MutableColumns & res_columns, const C
     /*  Dir contents of every query will be similar to
      [zk: localhost:2181(CONNECTED) 53] ls /clickhouse/task_queue/ddl/query-0000000004
     [active, finished]
-     */
+    */
     for (String q : queries)
     {
         /* Fetch all nodes under active & finished.
@@ -46,7 +50,7 @@ void StorageSystemDDLWorkerQueue::fillData(MutableColumns & res_columns, const C
         []
        [zk: localhost:2181(CONNECTED) 55] ls /clickhouse/task_queue/ddl/query-0000000004/finished
        [clickhouse01:9000, clickhouse02:9000, clickhouse03:9000, clickhouse04:9000]
-     */
+       */
         size_t col_num = 0;
         ddl_query_path = ddl_zookeeper_path + "/" + q;
         zkutil::Strings active_nodes = zookeeper->getChildren(ddl_query_path + "/active");
@@ -54,7 +58,21 @@ void StorageSystemDDLWorkerQueue::fillData(MutableColumns & res_columns, const C
 
         res_columns[col_num++]->insert(q); // name - query_<id>
 
-        // TODO insert comma seperated list of active and finished nodes
+        {
+            Array active_nodes_array;
+            active_nodes_array.reserve(active_nodes.size());
+            for (String active_node : active_nodes)
+                active_nodes_array.emplace_back(active_node);
+            res_columns[col_num++]->insert(active_nodes_array);
+        }
+
+        {
+            Array finished_nodes_array;
+            finished_nodes_array.reserve(active_nodes.size());
+            for (String finished_node : finished_nodes)
+                finished_nodes_array.emplace_back(finished_node);
+            res_columns[col_num++]->insert(finished_nodes_array);
+        }
     }
 }
 }
