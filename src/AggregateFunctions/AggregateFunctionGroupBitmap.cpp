c19ce7dad30c0223e214a0c97589a2adcc83e0c8
Amos Bird <amosbird@gmail.com>
2020-12-09 12:44:25 +0800

    rewrite with better code

--- src/AggregateFunctions/AggregateFunctionGroupBitmap.cpp
+++ src/AggregateFunctions/AggregateFunctionGroupBitmap.cpp
@@ -1,11 +1,10 @@
 #include <AggregateFunctions/AggregateFunctionFactory.h>
-#include <AggregateFunctions/Helpers.h>
 #include <AggregateFunctions/FactoryHelpers.h>
+#include <AggregateFunctions/Helpers.h>
 #include <DataTypes/DataTypeAggregateFunction.h>
 #include "registerAggregateFunctions.h"
 
-// TODO include this last because of a broken roaring header. See the comment
-// inside.
+// TODO include this last because of a broken roaring header. See the comment inside.
 #include <AggregateFunctions/AggregateFunctionGroupBitmap.h>
 
 namespace DB
@@ -17,46 +16,54 @@ namespace ErrorCodes
 
 namespace
 {
+    template <template <typename> class Data>
+    AggregateFunctionPtr createAggregateFunctionBitmap(const std::string & name, const DataTypes & argument_types, const Array & parameters)
+    {
+        assertNoParameters(name, parameters);
+        assertUnary(name, argument_types);
 
-template <template <typename> class Data>
-AggregateFunctionPtr createAggregateFunctionBitmap(const std::string & name, const DataTypes & argument_types, const Array & parameters)
-{
-    assertNoParameters(name, parameters);
-    assertUnary(name, argument_types);
+        if (!argument_types[0]->canBeUsedInBitOperations())
+            throw Exception(
+                "The type " + argument_types[0]->getName() + " of argument for aggregate function " + name
+                    + " is illegal, because it cannot be used in Bitmap operations",
+                ErrorCodes::ILLEGAL_TYPE_OF_ARGUMENT);
 
-    if (!argument_types[0]->canBeUsedInBitOperations())
-        throw Exception("The type " + argument_types[0]->getName() + " of argument for aggregate function " + name
-            + " is illegal, because it cannot be used in Bitmap operations",
-            ErrorCodes::ILLEGAL_TYPE_OF_ARGUMENT);
+        AggregateFunctionPtr res(createWithUnsignedIntegerType<AggregateFunctionBitmap, Data>(*argument_types[0], argument_types[0]));
 
-    AggregateFunctionPtr res(createWithUnsignedIntegerType<AggregateFunctionBitmap, Data>(*argument_types[0], argument_types[0]));
+        if (!res)
+            throw Exception(
+                "Illegal type " + argument_types[0]->getName() + " of argument for aggregate function " + name,
+                ErrorCodes::ILLEGAL_TYPE_OF_ARGUMENT);
 
-    if (!res)
-        throw Exception("Illegal type " + argument_types[0]->getName() + " of argument for aggregate function " + name, ErrorCodes::ILLEGAL_TYPE_OF_ARGUMENT);
+        return res;
+    }
 
-    return res;
-}
+    // Additional aggregate functions to manipulate bitmaps.
+    template <template <typename, typename> class AggregateFunctionTemplate>
+    AggregateFunctionPtr
+    createAggregateFunctionBitmapL2(const std::string & name, const DataTypes & argument_types, const Array & parameters)
+    {
+        assertNoParameters(name, parameters);
+        assertUnary(name, argument_types);
+        DataTypePtr argument_type_ptr = argument_types[0];
+        WhichDataType which(*argument_type_ptr);
+        if (which.idx != TypeIndex::AggregateFunction)
+            throw Exception(
+                "Illegal type " + argument_types[0]->getName() + " of argument for aggregate function " + name,
+                ErrorCodes::ILLEGAL_TYPE_OF_ARGUMENT);
 
-template <template <typename, typename> class AggregateFunctionTemplate>
-AggregateFunctionPtr createAggregateFunctionBitmapL2(const std::string & name, const DataTypes & argument_types, const Array & parameters)
-{
-    assertNoParameters(name, parameters);
-    assertUnary(name, argument_types);
-    DataTypePtr argument_type_ptr = argument_types[0];
-    WhichDataType which(*argument_type_ptr);
-    if (which.idx != TypeIndex::AggregateFunction)
-        throw Exception("Illegal type " + argument_types[0]->getName() + " of argument for aggregate function " + name, ErrorCodes::ILLEGAL_TYPE_OF_ARGUMENT);
+        const DataTypeAggregateFunction & datatype_aggfunc = dynamic_cast<const DataTypeAggregateFunction &>(*argument_type_ptr);
+        AggregateFunctionPtr aggfunc = datatype_aggfunc.getFunction();
+        argument_type_ptr = aggfunc->getArgumentTypes()[0];
+        AggregateFunctionPtr res(createWithUnsignedIntegerType<AggregateFunctionTemplate, AggregateFunctionGroupBitmapData>(
+            *argument_type_ptr, argument_type_ptr));
+        if (!res)
+            throw Exception(
+                "Illegal type " + argument_types[0]->getName() + " of argument for aggregate function " + name,
+                ErrorCodes::ILLEGAL_TYPE_OF_ARGUMENT);
 
-    const DataTypeAggregateFunction& datatype_aggfunc = dynamic_cast<const DataTypeAggregateFunction&>(*argument_type_ptr);
-    AggregateFunctionPtr aggfunc = datatype_aggfunc.getFunction();
-    argument_type_ptr = aggfunc->getArgumentTypes()[0];
-    AggregateFunctionPtr res(createWithUnsignedIntegerType<AggregateFunctionTemplate, AggregateFunctionGroupBitmapData>(*argument_type_ptr, argument_type_ptr));
-    if (!res)
-        throw Exception("Illegal type " + argument_types[0]->getName() + " of argument for aggregate function " + name, ErrorCodes::ILLEGAL_TYPE_OF_ARGUMENT);
-
-    return res;
-}
-
+        return res;
+    }
 }
 
 void registerAggregateFunctionsBitmap(AggregateFunctionFactory & factory)
