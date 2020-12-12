94070285408f3af50287517f3b5e1f0d52d70c63
sundy-li <543950155@qq.com>
2020-12-13 00:42:15 +0800

    update

--- src/Interpreters/addTypeConversionToAST.h
+++ src/Interpreters/addTypeConversionToAST.h
@@ -2,6 +2,7 @@
 
 #include <common/types.h>
 #include <Parsers/IAST_fwd.h>
+#include <Interpreters/Context.h>
 
 
 namespace DB
@@ -9,5 +10,8 @@ namespace DB
 
 /// It will produce an expression with CAST to get an AST with the required type.
 ASTPtr addTypeConversionToAST(ASTPtr && ast, const String & type_name);
+
+// If same type, then ignore the wrapper of CAST function
+ASTPtr addTypeConversionToAST(ASTPtr && ast, const String & type_name, const NamesAndTypesList & all_columns, const Context & context);
 
 }
