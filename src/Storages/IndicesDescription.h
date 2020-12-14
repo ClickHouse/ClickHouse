9e98cdad5a2356150cd336ad74ba191e7bce71b6
alesapin <alesapin@gmail.com>
2020-12-14 16:05:07 +0300

    Accident changes

--- src/Storages/IndicesDescription.h
+++ src/Storages/IndicesDescription.h
@@ -67,6 +67,7 @@ struct IndicesDescription : public std::vector<IndexDescription>
     bool has(const String & name) const;
     /// Convert description to string
     String toString() const;
+
     /// Parse description from string
     static IndicesDescription parse(const String & str, const ColumnsDescription & columns, const Context & context);
 
