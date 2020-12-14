b8ea51be4485261027f365ae581ded5f0311f2f9
myrrc <me@myrrec.space>
2020-12-14 21:34:43 +0300

    working on test stub

--- tests/integration/test_restore_replica/test.py
+++ tests/integration/test_restore_replica/test.py
@@ -13,9 +13,8 @@ def fill_nodes(nodes, shard):
             CREATE DATABASE test;
 
             CREATE TABLE test.test_table(num UInt32)
-            ENGINE = ReplicatedMergeTree('/clickhouse/tables/test/{shard}/replicated/test_table', '{replica}')
-            ORDER BY num
-            PARTITION BY intDiv(num % 1000);
+            ENGINE = ReplicatedMergeTree('/clickhouse/tables/test/{shard}/test_table', '{replica}')
+            ORDER BY num PARTITION BY intDiv(num, 1000);
         '''.format(shard=shard, replica=node.name))
 
 cluster = ClickHouseCluster(__file__)
@@ -41,39 +40,18 @@ def start_cluster():
 def test_restore_replica(start_cluster):
     zk = cluster.get_kazoo_client('zoo1')
 
-    node_1_1.query()
     node_1_1.query("INSERT INTO test.test_table SELECT * FROM numbers(1000, 2000)")
 
-    with PartitionManager() as pm:
-        ## make node_1_1 dead
-        pm.drop_instance_zk_connections(node_1_1)
-        time.sleep(10)
+    # 1. delete metadata
+    zk.delete("/clickhouse/tables/test/01/test_table", recursive=True)
+    assert zk.exists("/clickhouse/tables/test/01/test_table") is None
+    time.sleep(10)
 
-        assert "doesn't exist" in node_1_3.query_and_get_error("SYSTEM DROP REPLICA 'node_1_1' FROM TABLE test.test_table")
+    # 2. Assert there is an exception
+    node_1_1.query("SYSTEM RESTART REPLICA test.test_table")
+    node_1_1.query_and_get_error("INSERT INTO test.test_table SELECT * FROM numbers(1000,2000)")
 
-        assert "doesn't exist" in node_1_3.query_and_get_error("SYSTEM DROP REPLICA 'node_1_1' FROM DATABASE test1")
+    # 3. restore replica
+    node_1_1.query("SYSTEM RESTORE REPLICA test.test_table")
 
-        node_1_3.query("SYSTEM DROP REPLICA 'node_1_1'")
-        exists_replica_1_1 = zk.exists("/clickhouse/tables/test3/{shard}/replicated/test_table/replicas/{replica}".format(shard=1, replica='node_1_1'))
-        assert (exists_replica_1_1 != None)
-
-        ## If you want to drop a inactive/stale replicate table that does not have a local replica, you can following syntax(ZKPATH):
-        node_1_3.query("SYSTEM DROP REPLICA 'node_1_1' FROM ZKPATH '/clickhouse/tables/test2/{shard}/replicated/test_table'".format(shard=1))
-        exists_replica_1_1 = zk.exists("/clickhouse/tables/test2/{shard}/replicated/test_table/replicas/{replica}".format(shard=1, replica='node_1_1'))
-        assert (exists_replica_1_1 == None)
-
-        node_1_2.query("SYSTEM DROP REPLICA 'node_1_1' FROM TABLE test.test_table")
-        exists_replica_1_1 = zk.exists("/clickhouse/tables/test/{shard}/replicated/test_table/replicas/{replica}".format(shard=1, replica='node_1_1'))
-        assert (exists_replica_1_1 == None)
-
-        node_1_2.query("SYSTEM DROP REPLICA 'node_1_1' FROM DATABASE test1")
-        exists_replica_1_1 = zk.exists("/clickhouse/tables/test1/{shard}/replicated/test_table/replicas/{replica}".format(shard=1, replica='node_1_1'))
-        assert (exists_replica_1_1 == None)
-
-        node_1_3.query("SYSTEM DROP REPLICA 'node_1_1' FROM ZKPATH '/clickhouse/tables/test3/{shard}/replicated/test_table'".format(shard=1))
-        exists_replica_1_1 = zk.exists("/clickhouse/tables/test3/{shard}/replicated/test_table/replicas/{replica}".format(shard=1, replica='node_1_1'))
-        assert (exists_replica_1_1 == None)
-
-        node_1_2.query("SYSTEM DROP REPLICA 'node_1_1'")
-        exists_replica_1_1 = zk.exists("/clickhouse/tables/test4/{shard}/replicated/test_table/replicas/{replica}".format(shard=1, replica='node_1_1'))
-        assert (exists_replica_1_1 == None)
+    # 4. check if the data is present
