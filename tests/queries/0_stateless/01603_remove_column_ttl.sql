2a4a6b22b19638d18bf51e7b83c8f0a47fd4c2fb
alesapin <alesapin@gmail.com>
2020-12-16 10:26:18 +0300

    Fix remove TTL for column

---
+++ tests/queries/0_stateless/01603_remove_column_ttl.sql
@@ -0,0 +1,29 @@
+DROP TABLE IF EXISTS table_with_column_ttl;
+CREATE TABLE table_with_column_ttl
+(
+    EventTime DateTime,
+    UserID UInt64,
+    Age UInt8 TTL EventTime + INTERVAL 3 MONTH
+)
+ENGINE MergeTree()
+ORDER BY tuple();
+
+INSERT INTO table_with_column_ttl VALUES (now(), 1, 32);
+
+INSERT INTO table_with_column_ttl VALUES (now() - INTERVAL 4 MONTH, 2, 45);
+
+OPTIMIZE TABLE table_with_column_ttl FINAL;
+
+SELECT UserID, Age FROM table_with_column_ttl ORDER BY UserID;
+
+ALTER TABLE table_with_column_ttl MODIFY COLUMN Age REMOVE TTL;
+
+SHOW CREATE TABLE table_with_column_ttl;
+
+INSERT INTO table_with_column_ttl VALUES (now() - INTERVAL 10 MONTH, 3, 27);
+
+OPTIMIZE TABLE table_with_column_ttl FINAL;
+
+SELECT UserID, Age FROM table_with_column_ttl ORDER BY UserID;
+
+DROP TABLE table_with_column_ttl;
