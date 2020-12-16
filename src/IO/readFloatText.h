11c08acc268173a69c3577e513e693918b3e7018
nikitamikhaylov <mikhaylovnikitka@gmail.com>
2020-12-16 18:34:29 +0300

    better

--- src/IO/readFloatText.h
+++ src/IO/readFloatText.h
@@ -6,10 +6,14 @@
 #include <Common/StringUtils/StringUtils.h>
 #include <double-conversion/double-conversion.h>
 
+#ifdef __clang__
 #pragma clang diagnostic push
 #pragma clang diagnostic ignored "-Wunneeded-internal-declaration"
+#endif
 #include <fast_float/fast_float.h>
+#ifdef __clang__
 #pragma clang diagnostic pop
+#endif
 
 /** Methods for reading floating point numbers from text with decimal representation.
   * There are "precise", "fast" and "simple" implementations.
