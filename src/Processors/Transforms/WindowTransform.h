d92bb7fee55e16c71c4d69404fa4bdcd81bb0a2e
Alexander Kuzmenkov <akuzm@yandex-team.ru>
2020-12-16 15:57:47 +0300

    works across the blocks

--- src/Processors/Transforms/WindowTransform.h
+++ src/Processors/Transforms/WindowTransform.h
@@ -51,12 +51,21 @@ public:
     Block input_header;
 
     WindowDescription window_description;
+    // Indices of the PARTITION BY columns in block.
     std::vector<size_t> partition_by_indices;
+    // The columns for PARTITION BY and the row in these columns where the
+    // current partition started. They might be in some of the previous blocks,
+    // so we have to keep the shared ownership of the columns. We don't keep the
+    // entire block to save memory, only the needed columns, in the same order
+    // as the partition_by_indices array.
+    // Can be empty if there is no PARTITION BY.
+    // Columns are nullptr when it is the first partition.
+    std::vector<ColumnPtr> partition_start_columns;
+    size_t partition_start_row = 0;
 
     std::vector<WindowFunctionWorkspace> workspaces;
 
     std::unique_ptr<Arena> arena;
-    std::vector<AlignedBuffer> aggregate_function_data;
 };
 
 }
