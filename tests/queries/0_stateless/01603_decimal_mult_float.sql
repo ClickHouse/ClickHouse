bf5e836414f68f49d363708c8d32b0a9f0d576cc
myrrc <me@myrrec.space>
2020-12-18 00:03:45 +0300

    updated the test

--- tests/queries/0_stateless/01603_decimal_mult_float.sql
+++ tests/queries/0_stateless/01603_decimal_mult_float.sql
@@ -12,6 +12,8 @@ CREATE TABLE test01603 IF NOT EXISTS(
 INSERT INTO test01603(f64) SELECT 1 / num FROM system.numbers LIMIT 1000;
 
 SELECT sum(d * 1.1) FROM test01603;
+SELECT sum(8.01 * d) FROM test01603;
 SELECT sum(f64 * toDecimal64(80, 2)) FROM test01603;
+SELECT sum(toDecimal64(0.4, 2) * f32) FROM test01603;
 
 DROP TABLE test01603 IF EXISTS;
