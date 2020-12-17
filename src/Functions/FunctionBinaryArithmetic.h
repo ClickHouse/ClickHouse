ea2da764f6bd4e1500adaebfc9794879bf414526
myrrc <me@myrrec.space>
2020-12-17 21:33:51 +0300

    fixed result data type init

--- src/Functions/FunctionBinaryArithmetic.h
+++ src/Functions/FunctionBinaryArithmetic.h
@@ -990,7 +990,14 @@ public:
                 static constexpr const bool dec_a = IsDecimalNumber<T0>;
                 static constexpr const bool dec_b = IsDecimalNumber<T1>;
 
-                ResultDataType type = decimalResultType<is_multiply, is_division>(left, right);
+                ResultDataType type;
+
+                if constexpr(dec_a && IsFloatingPoint<RightDataType>)
+                    type = RightDataType();
+                else if constexpr(dec_b && IsFloatingPoint<LeftDataType>)
+                    type = LeftDataType();
+                else
+                    type = decimalResultType<is_multiply, is_division>(left, right);
 
                 typename ResultDataType::FieldType scale_a = type.scaleFactorFor(left, is_multiply);
                 typename ResultDataType::FieldType scale_b = type.scaleFactorFor(right, is_multiply || is_division);
