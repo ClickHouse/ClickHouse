737d4fc22fd2565e5551b15f6cd96aa419a271cd
myrrc <me@myrrec.space>
2020-12-17 19:43:29 +0300

    debug fix

--- src/Functions/FunctionBinaryArithmetic.h
+++ src/Functions/FunctionBinaryArithmetic.h
@@ -550,8 +550,10 @@ class FunctionBinaryArithmetic : public IFunction
     template <typename F>
     static bool castBothTypes(const IDataType * left, const IDataType * right, F && f)
     {
-        const bool l = castType(left, [](const auto& c){ return std::is_same_v<decltype(c), DataTypeDecimal<Decimal32>>; });
-        const bool r = castType(right, [](const auto& c){ return std::is_same_v<decltype(c), DataTypeFloat32>; });
+        const bool l = castType(left, [](const auto& c){ 
+            return std::is_same_v<decltype(c), DataTypeDecimal<Decimal32>>; });
+        const bool r = castType(right, [](const auto& c){ 
+            return std::is_same_v<decltype(c), DataTypeFloat32>; });
 
         assert(l);
         assert(r);
