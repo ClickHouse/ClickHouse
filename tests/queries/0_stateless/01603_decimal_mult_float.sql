5d861b4c76330a9207030e45e6a37d875208020a
myrrc <me@myrrec.space>
2020-12-18 16:04:19 +0300

    fixed the test

--- tests/queries/0_stateless/01603_decimal_mult_float.sql
+++ tests/queries/0_stateless/01603_decimal_mult_float.sql
@@ -7,7 +7,7 @@ CREATE TABLE IF NOT EXISTS test01603 (
     f64 Float64,
     d Decimal64(3) DEFAULT toDecimal32(f64, 3),
     f32 Float32 DEFAULT f64
-) ENGINE=Memory() ORDER BY f32;
+) ENGINE=MergeTree() ORDER BY f32;
 
 INSERT INTO test01603(f64) SELECT 1 / (number + 1) FROM system.numbers LIMIT 1000;
 
