c769781cc216d954b6ac92a71c37936146f1eddc
myrrc <me@myrrec.space>
2020-12-18 18:37:00 +0300

    another fix

--- tests/queries/0_stateless/01603_decimal_mult_float.sql
+++ tests/queries/0_stateless/01603_decimal_mult_float.sql
@@ -14,7 +14,9 @@ INSERT INTO test01603(f64) SELECT 1 / (number + 1) FROM system.numbers LIMIT 100
 SELECT sum(d * 1.1) FROM test01603;
 SELECT sum(8.01 * d) FROM test01603;
 SELECT sum(f64 * toDecimal64(80, 2)) FROM test01603;
-SELECT sum(toDecimal64(0.4, 2) * f32) FROM test01603;
+SELECT sum(toDecimal64(40, 2) * f32) FROM test01603;
+SELECT sum(f64 * toDecimal64(0.1, 2)) FROM test01603;
+SELECT sum(toDecimal64(0.3, 2) * f32) FROM test01603;
 
 SELECT sum(f64 * d) FROM test01603;
 SELECT sum(d * f64) FROM test01603;
