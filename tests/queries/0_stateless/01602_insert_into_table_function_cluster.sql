edf412cf0ed4b71d444f5ac7fe546537b74d877f
feng lv <fenglv15@mails.ucas.ac.cn>
2020-12-20 09:12:29 +0000

    fix test

--- tests/queries/0_stateless/01602_insert_into_table_function_cluster.sql
+++ tests/queries/0_stateless/01602_insert_into_table_function_cluster.sql
@@ -1,7 +1,11 @@
 DROP TABLE IF EXISTS default.x;
 
-CREATE TABLE default.x ON CLUSTER test_cluster_two_shards_localhost AS system.numbers ENGINE = Log;
+CREATE TABLE default.x ON CLUSTER test_shard_localhost AS system.numbers ENGINE = Log;
 
-INSERT INTO FUNCTION cluster('test_cluster_two_shards_localhost', default, x, rand()) SELECT * FROM numbers(10);
+INSERT INTO FUNCTION cluster('test_shard_localhost', default, x) SELECT * FROM numbers(10);
+-- In fact, in this case(just one shard), sharding key is not required
+INSERT INTO FUNCTION cluster('test_shard_localhost', default, x, rand()) SELECT * FROM numbers(10);
 
-DROP TABLE default.x ON CLUSTER test_cluster_two_shards_localhost;
+SELECT * FROM default.x ORDER BY number;
+
+DROP TABLE default.x ON CLUSTER test_shard_localhost;
