31b955e14ad067c03396592e808afd17fa536baf
Alexey Milovidov <milovidov@yandex-team.ru>
2020-12-21 03:38:53 +0300

    Slightly better for gcc-9

--- src/Access/ContextAccess.cpp
+++ src/Access/ContextAccess.cpp
@@ -376,7 +376,7 @@ bool ContextAccess::checkAccessImpl2(const AccessFlags & flags, const Args &... 
         return true;
     };
 
-    auto access_denied = [&](const String & error_msg, int error_code)
+    auto access_denied = [&](const String & error_msg, int error_code [[maybe_unused]])
     {
         if (trace_log)
             LOG_TRACE(trace_log, "Access denied: {}{}", (AccessRightsElement{flags, args...}.toString()),
