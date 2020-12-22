12a122d72599bcebc04e27a45acd590dc0ddaa3c
feng lv <fenglv15@mails.ucas.ac.cn>
2020-12-22 03:17:05 +0000

    update test
    update test
    
    fix
    
    fix
    
    fix

--- tests/queries/0_stateless/01602_insert_into_table_function_cluster.sql
+++ tests/queries/0_stateless/01602_insert_into_table_function_cluster.sql
@@ -1,15 +1,22 @@
 DROP TABLE IF EXISTS default.x;
 
-CREATE TABLE default.x AS system.numbers ENGINE = Log;
+CREATE TABLE default.x AS system.numbers ENGINE = MergeTree ORDER BY number;
+CREATE TABLE default.y AS system.numbers ENGINE = MergeTree ORDER BY number;
 
+-- Just one shard, sharding key isn't necessary
 INSERT INTO FUNCTION cluster('test_shard_localhost', default, x) SELECT * FROM numbers(10);
--- In fact, in this case(just one shard), sharding key is not required
 INSERT INTO FUNCTION cluster('test_shard_localhost', default, x, rand()) SELECT * FROM numbers(10);
 
-INSERT INTO FUNCTION remote('127.0.0.1', default, x, rand()) SELECT * FROM numbers(10);
+-- More than one shard, sharding key is necessary
+INSERT INTO FUNCTION cluster('test_cluster_two_shards_localhost', default, x) SELECT * FROM numbers(10); --{ serverError 55 }
+INSERT INTO FUNCTION cluster('test_cluster_two_shards_localhost', default, x, rand()) SELECT * FROM numbers(10);
 
-INSERT INTO FUNCTION remote('127.0.0.1', default, x, 'default', rand()) SELECT * FROM numbers(10);
+INSERT INTO FUNCTION remote('127.0.0.{1,2}', default, y, 'default') SELECT * FROM numbers(10); -- { serverError 55 }
+INSERT INTO FUNCTION remote('127.0.0.{1,2}', default, y, 'default', rand()) SELECT * FROM numbers(10);
 
 SELECT * FROM default.x ORDER BY number;
 
+SELECT * FROM remote('127.0.0.{1,2}', default, y) ORDER BY number;
+
 DROP TABLE default.x;
+DROP TABLE default.y;
