545aa5cd7a0fa8acf6ae117ce760fefb7c835ec9
Amos Bird <amosbird@gmail.com>
2020-12-23 01:51:09 +0800

    Fix 2-arg functions with constant in PK analysis

---
+++ tests/queries/0_stateless/01615_two_args_function_index_fix.sql
@@ -0,0 +1,9 @@
+drop table if exists bad_date_time;
+
+create table bad_date_time (time Datetime, count UInt16) Engine = MergeTree() ORDER BY (time);
+
+insert into bad_date_time values('2020-12-20 20:59:52', 1),  ('2020-12-20 21:59:52', 1),  ('2020-12-20 01:59:52', 1);
+
+select toDate(time, 'UTC') dt, min(toDateTime(time, 'UTC')), max(toDateTime(time, 'UTC')), sum(count) from bad_date_time where toDate(time, 'UTC') = '2020-12-19' group by dt;
+
+drop table if exists bad_date_time;
