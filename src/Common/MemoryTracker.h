a3ab42912901cdf80bb756c46a60d0edcff95c86
Azat Khuzhin <a3at.mail@gmail.com>
2020-12-25 01:18:37 +0300

    Split MemoryTracker header
    Allocator.h is included into almost everything (via PODArray.h), so keep
    only small part of MemoryTracker.h for it.

--- src/Common/MemoryTracker.h
+++ src/Common/MemoryTracker.h
@@ -190,12 +190,3 @@ public:
 };
 
 extern MemoryTracker total_memory_tracker;
-
-
-/// Convenience methods, that use current thread's memory_tracker if it is available.
-namespace CurrentMemoryTracker
-{
-    void alloc(Int64 size);
-    void realloc(Int64 old_size, Int64 new_size);
-    void free(Int64 size);
-}
