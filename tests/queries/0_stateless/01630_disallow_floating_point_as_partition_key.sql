731402c2f4f17335f5d0505f67cb3c2e66ab1e0f
hexiaoting <hewenting_ict@163.com>
2020-12-24 18:30:20 +0800

    Disallow floating point as partition key

---
+++ tests/queries/0_stateless/01630_disallow_floating_point_as_partition_key.sql
@@ -0,0 +1,2 @@
+CREATE TABLE test (a Float32, b int) Engine = MergeTree() ORDER BY tuple() PARTITION BY a; -- { serverError 36 }
+CREATE TABLE test (a Float32, b int, c String, d Float64) Engine = MergeTree() ORDER BY tuple() PARTITION BY (b, c, d); -- { serverError 36 }
