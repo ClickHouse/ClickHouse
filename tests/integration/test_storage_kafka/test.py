e81a99156fa5f65447e49a33484d654fe074faba
filimonov <1549571+filimonov@users.noreply.github.com>
2020-12-29 18:51:00 +0100

    Attempt to prevent test flap
    That extra SELECT is executed synchronously, after that all further actions will have a more stable timing.

--- tests/integration/test_storage_kafka/test.py
+++ tests/integration/test_storage_kafka/test.py
@@ -1617,6 +1617,8 @@ def test_kafka_virtual_columns2(kafka_cluster):
                      kafka_num_consumers = 2,
                      kafka_format = 'JSONEachRow';
 
+        SELECT * FROM test.kafka;
+
         CREATE MATERIALIZED VIEW test.view Engine=Log AS
         SELECT value, _key, _topic, _partition, _offset, toUnixTimestamp(_timestamp), toUnixTimestamp64Milli(_timestamp_ms), _headers.name, _headers.value FROM test.kafka;
         ''')
