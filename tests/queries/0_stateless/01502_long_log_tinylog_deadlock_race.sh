974c02e5fdc4ba0b1df49b77ae9983e50c082381
Alexey Milovidov <milovidov@yandex-team.ru>
2021-01-03 18:45:08 +0300

    Fix shellcheck

--- tests/queries/0_stateless/01502_long_log_tinylog_deadlock_race.sh
+++ tests/queries/0_stateless/01502_long_log_tinylog_deadlock_race.sh
@@ -4,8 +4,8 @@ set -e
 
 CLICKHOUSE_CLIENT_SERVER_LOGS_LEVEL=fatal
 
-# shellcheck source=../shell_config.sh
 CURDIR=$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)
+# shellcheck source=../shell_config.sh
 . "$CURDIR"/../shell_config.sh
 
 
