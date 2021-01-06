32d9ff6548bbe544791242f6b374cfab64f36563
Amos Bird <amosbird@gmail.com>
2021-01-06 17:18:48 +0800

    better

--- tests/queries/0_stateless/01632_max_partitions_to_read.sql
+++ tests/queries/0_stateless/01632_max_partitions_to_read.sql
@@ -2,16 +2,16 @@ drop table if exists p;
 
 create table p(d Date, i int, j int) engine MergeTree partition by d order by i settings max_partitions_to_read = 1;
 
-insert into p values (yesterday(), 1, 2), (today(), 3, 4);
+insert into p values ('2021-01-01', 1, 2), ('2021-01-02', 4, 5);
 
-select * from p order by i; -- default no limit
+select * from p order by i; -- { serverError 565 }
 
-select * from p order by i settings force_max_partition_limit = 0;
+select * from p order by i settings max_partitions_to_read = 2;
 
-select * from p order by i settings force_max_partition_limit = 1; -- { serverError 565 }
+select * from p order by i settings max_partitions_to_read = 0; -- unlimited
 
 alter table p modify setting max_partitions_to_read = 2;
 
-select * from p order by i settings force_max_partition_limit = 1;
+select * from p order by i;
 
 drop table if exists p;
