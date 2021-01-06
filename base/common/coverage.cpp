29822374aef5056fc8bcccbcfe8b7bd25795389f
myrrc <me@myrrec.space>
2021-01-06 23:53:28 +0300

    Initial: added the sanitizer callbacks
    moved coverage to base/common
    
    temp add
    
    Small bugfixes regarding old-style casts and variable renames
    
    Removed old include
    
    Removed old declaration
    
    Added the initial dump callback
    
    Added the definition to hide the sanitizer callbacks

--- base/common/coverage.cpp
+++ base/common/coverage.cpp
@@ -1,31 +1,48 @@
+#include <stdint.h>
+#include <stdio.h>
 #include "coverage.h"
 
-#if WITH_COVERAGE
+void dumpCoverageReportIfPossible()  {}
 
-#    include <mutex>
-#    include <unistd.h>
+// Use roaring bitmaps
+// Use some way to pass test id without the foss
 
+// This callback is inserted by the compiler as a module constructor
+// into every DSO. 'start' and 'stop' correspond to the
+// beginning and end of the section with the guards for the entire
+// binary (executable or DSO). The callback will be called at least
+// once per DSO and may be called multiple times with the same parameters.
+extern "C" void __sanitizer_cov_trace_pc_guard_init(uint32_t *start, uint32_t *stop) {
+  static uint64_t n;  // Counter for the guards.
 
-#    if defined(__clang__)
-extern "C" void __llvm_profile_dump(); // NOLINT
-#    elif defined(__GNUC__) || defined(__GNUG__)
-extern "C" void __gcov_exit();
-#    endif
+  if (start == stop || *start) return;  // Initialize only once.
 
-#endif
+  printf("INIT: %p %p\n",
+          static_cast<void *>(start),
+          static_cast<void *>(stop));
 
+  // initialize the roaring data
 
-void dumpCoverageReportIfPossible()
-{
-#if WITH_COVERAGE
-    static std::mutex mutex;
-    std::lock_guard lock(mutex);
+  for (uint32_t *x = start; x < stop; x++)
+    *x = ++n;  // Guards should start from 1.
+}
 
-#    if defined(__clang__)
-    __llvm_profile_dump(); // NOLINT
-#    elif defined(__GNUC__) || defined(__GNUG__)
-    __gcov_exit();
-#    endif
-
-#endif
+// This callback is inserted by the compiler on every edge in the
+// control flow (some optimizations apply).
+// Typically, the compiler will emit the code like this:
+//    if(*guard)
+//      __sanitizer_cov_trace_pc_guard(guard);
+// But for large functions it will emit a simple call:
+//    __sanitizer_cov_trace_pc_guard(guard);
+extern "C" void __sanitizer_cov_trace_pc_guard(uint32_t *edge_index) {
+  if (!*edge_index) return;  // Duplicate the guard check.
+  // If you set *guard to 0 this code will not be called again for this edge.
+  // Now you can get the PC and do whatever you want:
+  //   store it somewhere or symbolize it and print right away.
+  // The values of `*guard` are as you set them in
+  // __sanitizer_cov_trace_pc_guard_init and so you can make them consecutive
+  // and use them to dereference an array or a bit vector.
+  printf("guard: %p %x PC\n",
+          static_cast<void*>(edge_index),
+          *edge_index);
 }
