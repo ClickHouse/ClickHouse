45fb446e89295bea70b801a7ca840d55f8b5a287
Denis Glazachev <d.glazachev@gmail.com>
2021-01-06 07:53:03 +0400

    Fix test

--- tests/queries/0_stateless/01622_summary_elapsed_time_lower_bound.sh
+++ tests/queries/0_stateless/01622_summary_elapsed_time_lower_bound.sh
@@ -8,12 +8,12 @@ ${CLICKHOUSE_CURL} -vsS "${CLICKHOUSE_URL}" -d 'CREATE TABLE insert_number_query
 
 rows=100
 sleep_each_row=0.0123
-expected_elapsed_time=$(python -c "print($rows * $sleep_each_row * 1000000000)") # nanoseconds
+expected_elapsed_time=$(python3 -c "print($rows * $sleep_each_row * 1000000000)") # nanoseconds
 elapsed_time=$(
     ${CLICKHOUSE_CURL} -vsS "${CLICKHOUSE_URL}&send_progress_in_http_headers=1" -d "INSERT INTO insert_number_query (record) SELECT sleepEachRow($sleep_each_row) FROM numbers($rows)" 2>&1 | grep -E 'X-ClickHouse-Summary' | sed -r -e 's/^.*"elapsed_time":"([0-9]+)".*$/\1/'
 )
 
-if python -c "import sys; sys.exit(0 if $elapsed_time >= $expected_elapsed_time else 1)" ; then
+if python3 -c "import sys; sys.exit(0 if $elapsed_time >= $expected_elapsed_time else 1)" ; then
     echo "OK : actual elapsed time >= expected elapsed time ($expected_elapsed_time)"
 else
     echo "FAIL : actual elapsed time ($elapsed_time) < expected elapsed time ($expected_elapsed_time)"
