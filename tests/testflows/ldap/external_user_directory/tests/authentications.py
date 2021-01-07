742b470a5e66f311f0da39fde0c0ee9354e2fd5e
Vitaliy Zakaznikov <vzakaznikov@protonmail.com>
2021-01-07 18:42:54 -0500

    Fixing inproper usage of And step.

--- tests/testflows/ldap/external_user_directory/tests/authentications.py
+++ tests/testflows/ldap/external_user_directory/tests/authentications.py
@@ -968,7 +968,7 @@ def verification_cooldown_performance(self, server, rbac=False, iterations=5000)
         no_vcd_time = repeat_requests(server=server, iterations=iterations, vcd_value="0", rbac=rbac)
         metric("login_with_vcd_value_0", units="seconds", value=no_vcd_time)
 
-    with And("Log the performance improvement as a percentage."):
+    with Then("Log the performance improvement as a percentage"):
         metric("percentage_improvement", units="%", value=100*(no_vcd_time - vcd_time)/vcd_time)
 
 @TestOutline
