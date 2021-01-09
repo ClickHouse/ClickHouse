bbb32731f2dc07615be62f86b165fe473a18f41e
Azat Khuzhin <a3at.mail@gmail.com>
2021-01-09 17:51:30 +0300

    Rename fsync_tmp_directory to fsync_directories for Distributed engine

--- tests/queries/0_stateless/01644_distributed_async_insert_fsync_smoke.sql
+++ tests/queries/0_stateless/01644_distributed_async_insert_fsync_smoke.sql
@@ -13,7 +13,7 @@ select sum(*) from dist_01643;
 drop table dist_01643;
 
 select 'fsync';
-create table dist_01643 as data_01643 engine=Distributed(test_cluster_two_shards, currentDatabase(), data_01643, key) settings fsync_after_insert=1, fsync_tmp_directory=1;
+create table dist_01643 as data_01643 engine=Distributed(test_cluster_two_shards, currentDatabase(), data_01643, key) settings fsync_after_insert=1, fsync_directories=1;
 system stop distributed sends dist_01643;
 insert into dist_01643 select * from numbers(10) settings prefer_localhost_replica=0;
 select sum(*) from dist_01643;
