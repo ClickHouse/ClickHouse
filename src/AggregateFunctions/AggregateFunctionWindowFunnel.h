dd52f88e548cd4fa55ed3a2a51673a9099bd1bd3
HuFuwang <hufuwang@HuFuwangs-MacBook-Pro.local>
2021-01-09 22:37:49 +0800

    fix the inconsistent result of windowFunnel in case of same timestamp for different events.

--- src/AggregateFunctions/AggregateFunctionWindowFunnel.h
+++ src/AggregateFunctions/AggregateFunctionWindowFunnel.h
@@ -24,7 +24,7 @@ struct ComparePairFirst final
     template <typename T1, typename T2>
     bool operator()(const std::pair<T1, T2> & lhs, const std::pair<T1, T2> & rhs) const
     {
-        return lhs.first < rhs.first;
+        return lhs.first == rhs.first ? lhs.second < rhs.second : lhs.first < rhs.first;
     }
 };
 
