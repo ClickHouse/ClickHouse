c28ee1d163ec559f272f797b78852cca7fc18f91
sundy-li <543950155@qq.com>
2021-01-11 10:05:37 +0800

    Fix AggregateFunctionGroupBitmapData bitmapOrCardinality using wrong variable

--- src/AggregateFunctions/AggregateFunctionGroupBitmapData.h
+++ src/AggregateFunctions/AggregateFunctionGroupBitmapData.h
@@ -262,9 +262,10 @@ public:
         }
         else if (isSmall() && r1.isLarge())
         {
+            const auto & new_rb = r1.rb;
             for (const auto & x : small)
             {
-                if (rb->contains(static_cast<Value>(x.getValue())))
+                if (new_rb->contains(static_cast<Value>(x.getValue())))
                     ++ret;
             }
         }
