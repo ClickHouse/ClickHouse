9f22bcefb0177dd663456c4be4cb8350b82ec057
Mikhail Filimonov <mfilimonov@altinity.com>
2021-01-11 18:23:21 +0100

    Docker: better server entrypoint

--- docker/server/alpine-build.sh
+++ docker/server/alpine-build.sh
@@ -4,6 +4,7 @@ set -x
 REPO_CHANNEL="${REPO_CHANNEL:-stable}" # lts / testing / prestable / etc
 REPO_URL="${REPO_URL:-"https://repo.yandex.ru/clickhouse/tgz/${REPO_CHANNEL}"}"
 VERSION="${VERSION:-20.9.3.45}"
+DOCKER_IMAGE="${DOCKER_IMAGE:-yandex/clickhouse-server}"
 
 # where original files live
 DOCKER_BUILD_FOLDER="${BASH_SOURCE%/*}"
@@ -11,12 +12,12 @@ DOCKER_BUILD_FOLDER="${BASH_SOURCE%/*}"
 # we will create root for our image here
 CONTAINER_ROOT_FOLDER="${DOCKER_BUILD_FOLDER}/alpine-root"
 
-# where to put downloaded tgz
-TGZ_PACKAGES_FOLDER="${CONTAINER_ROOT_FOLDER}/tgz-packages"
-
-# clean up the root from old runs
+# clean up the root from old runs, it's reconstructed each time
 rm -rf "$CONTAINER_ROOT_FOLDER"
+mkdir -p "$CONTAINER_ROOT_FOLDER"
 
+# where to put downloaded tgz
+TGZ_PACKAGES_FOLDER="${DOCKER_BUILD_FOLDER}/tgz-packages"
 mkdir -p "$TGZ_PACKAGES_FOLDER"
 
 PACKAGES=( "clickhouse-client" "clickhouse-server" "clickhouse-common-static" )
@@ -24,7 +25,7 @@ PACKAGES=( "clickhouse-client" "clickhouse-server" "clickhouse-common-static" )
 # download tars from the repo
 for package in "${PACKAGES[@]}"
 do
-    wget -q --show-progress "${REPO_URL}/${package}-${VERSION}.tgz" -O "${TGZ_PACKAGES_FOLDER}/${package}-${VERSION}.tgz"
+    wget -c -q --show-progress "${REPO_URL}/${package}-${VERSION}.tgz" -O "${TGZ_PACKAGES_FOLDER}/${package}-${VERSION}.tgz"
 done
 
 # unpack tars
@@ -42,7 +43,7 @@ mkdir -p "${CONTAINER_ROOT_FOLDER}/etc/clickhouse-server/users.d" \
          "${CONTAINER_ROOT_FOLDER}/lib64"
 
 cp "${DOCKER_BUILD_FOLDER}/docker_related_config.xml" "${CONTAINER_ROOT_FOLDER}/etc/clickhouse-server/config.d/"
-cp "${DOCKER_BUILD_FOLDER}/entrypoint.alpine.sh"      "${CONTAINER_ROOT_FOLDER}/entrypoint.sh"
+cp "${DOCKER_BUILD_FOLDER}/entrypoint.sh"             "${CONTAINER_ROOT_FOLDER}/entrypoint.sh"
 
 ## get glibc components from ubuntu 20.04 and put them to expected place
 docker pull ubuntu:20.04
@@ -56,4 +57,5 @@ docker cp -L "${ubuntu20image}":/lib/x86_64-linux-gnu/libnss_dns.so.2 "${CONTAIN
 docker cp -L "${ubuntu20image}":/lib/x86_64-linux-gnu/libresolv.so.2  "${CONTAINER_ROOT_FOLDER}/lib"
 docker cp -L "${ubuntu20image}":/lib64/ld-linux-x86-64.so.2           "${CONTAINER_ROOT_FOLDER}/lib64"
 
-docker build "$DOCKER_BUILD_FOLDER" -f Dockerfile.alpine -t "yandex/clickhouse-server:${VERSION}-alpine" --pull
+docker build "$DOCKER_BUILD_FOLDER" -f Dockerfile.alpine -t "${DOCKER_IMAGE}:${VERSION}-alpine" --pull
+rm -rf "$CONTAINER_ROOT_FOLDER"
