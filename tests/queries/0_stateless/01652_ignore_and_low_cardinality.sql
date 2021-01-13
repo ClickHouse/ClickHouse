a0874d93c3778624e024701068d2c33d3606a1bc
Nikolai Kochetov <nik-kochetov@yandex-team.ru>
2021-01-13 17:47:08 +0300

    Add test.

---
+++ tests/queries/0_stateless/01652_ignore_and_low_cardinality.sql
@@ -0,0 +1,3 @@
+set allow_suspicious_low_cardinality_types = 1;
+CREATE TABLE lc_null_int8_defnull (val LowCardinality(Nullable(Int8)) DEFAULT NULL) ENGINE = MergeTree order by tuple();
+SELECT ignore(10, ignore(*), ignore(ignore(-2, 1025, *)), NULL, *), * FROM lc_null_int8_defnull AS values;
