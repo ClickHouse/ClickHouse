2db407cdc0bc8fdb0e2617d34bc568752ca53a2c
Yatsishin Ilya <2159081+qoega@users.noreply.github.com>
2021-01-14 18:37:15 +0300

    More tests

--- docker/test/sqlancer/run.sh
+++ docker/test/sqlancer/run.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+QUERIES
+
 set -e -x
 
 dpkg -i package_folder/clickhouse-common-static_*.deb
@@ -10,6 +12,15 @@ dpkg -i package_folder/clickhouse-client_*.deb
 service clickhouse-server start && sleep 5
 
 cd /sqlancer/sqlancer-master
-CLICKHOUSE_AVAILABLE=true mvn -Dtest=TestClickHouse test
+
+export TIMEOUT=60
+export NUM_QUERIES=1000
+
+java -jar target/SQLancer-*.jar --num-threads 10 --timeout-seconds $TIMEOUT --num-queries $NUM_QUERIES  --username default --password "" clickhouse --oracle TLPWhere /test_output/TLPWhere.log
+java -jar target/SQLancer-*.jar --num-threads 10 --timeout-seconds $TIMEOUT --num-queries $NUM_QUERIES  --username default --password "" clickhouse --oracle TLPGroupBy /test_output/TLPGroupBy.log
+java -jar target/SQLancer-*.jar --num-threads 10 --timeout-seconds $TIMEOUT --num-queries $NUM_QUERIES  --username default --password "" clickhouse --oracle TLPHaving /test_output/TLPHaving.log
+java -jar target/SQLancer-*.jar --num-threads 10 --timeout-seconds $TIMEOUT --num-queries $NUM_QUERIES  --username default --password "" clickhouse --oracle TLPWhere --oracle TLPGroupBy /test_output/TLPWhereGroupBy.log
+java -jar target/SQLancer-*.jar --num-threads 10 --timeout-seconds $TIMEOUT --num-queries $NUM_QUERIES  --username default --password "" clickhouse --oracle TLPDistinct /test_output/TLPDistinct.log
+java -jar target/SQLancer-*.jar --num-threads 10 --timeout-seconds $TIMEOUT --num-queries $NUM_QUERIES  --username default --password "" clickhouse --oracle TLPAggregate /test_output/TLPAggregate.log
 
 cp /sqlancer/sqlancer-master/target/surefire-reports/TEST-sqlancer.dbms.TestClickHouse.xml /test_output/result.xml
