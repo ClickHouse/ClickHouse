b923ed121b0bde10af7cb699b6543a3be8d6893e
Denis Glazachev <d.glazachev@gmail.com>
2021-01-14 21:04:42 +0400

    Mask varying elapsed time from the output

--- tests/queries/0_stateless/00969_live_view_watch_format_jsoneachrowwithprogress.sh
+++ tests/queries/0_stateless/00969_live_view_watch_format_jsoneachrowwithprogress.sh
@@ -1,3 +1,10 @@
+#!/usr/bin/env bash
+
+CURDIR=$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)
+# shellcheck source=../shell_config.sh
+. "$CURDIR"/../shell_config.sh
+
+${CLICKHOUSE_CURL} -sS "${CLICKHOUSE_URL}" -d @- <<EOF 2>&1 | sed -r -e 's/"elapsed_time":"[0-9]+"/"elapsed_time":"<ELAPSED-TIME>"/'
 SET allow_experimental_live_view = 1;
 
 DROP TABLE IF EXISTS lv;
@@ -18,3 +25,4 @@ WATCH lv LIMIT 0 FORMAT JSONEachRowWithProgress;
 
 DROP TABLE lv;
 DROP TABLE mt;
+EOF
