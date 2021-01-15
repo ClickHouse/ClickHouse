a3d482ab7d26d40ce71252ef8e662872e4a9e58a
Denis Glazachev <d.glazachev@gmail.com>
2021-01-15 20:57:47 +0400

    Fix failing tests

--- tests/queries/0_stateless/00969_live_view_watch_format_jsoneachrowwithprogress.sh
+++ tests/queries/0_stateless/00969_live_view_watch_format_jsoneachrowwithprogress.sh
@@ -4,25 +4,14 @@ CURDIR=$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)
 # shellcheck source=../shell_config.sh
 . "$CURDIR"/../shell_config.sh
 
-${CLICKHOUSE_CURL} -sS "${CLICKHOUSE_URL}" -d @- <<EOF 2>&1 | sed -r -e 's/"elapsed_time":"[0-9]+"/"elapsed_time":"<ELAPSED-TIME>"/'
-SET allow_experimental_live_view = 1;
-
-DROP TABLE IF EXISTS lv;
-DROP TABLE IF EXISTS mt;
-
-CREATE TABLE mt (a Int32) Engine=MergeTree order by tuple();
-CREATE LIVE VIEW lv AS SELECT sum(a) FROM mt;
-
-WATCH lv LIMIT 0 FORMAT JSONEachRowWithProgress;
-
-INSERT INTO mt VALUES (1),(2),(3);
-
-WATCH lv LIMIT 0 FORMAT JSONEachRowWithProgress;
-
-INSERT INTO mt VALUES (4),(5),(6);
-
-WATCH lv LIMIT 0 FORMAT JSONEachRowWithProgress;
-
-DROP TABLE lv;
-DROP TABLE mt;
-EOF
+$CLICKHOUSE_CLIENT --query="DROP TABLE IF EXISTS lv"
+$CLICKHOUSE_CLIENT --query="DROP TABLE IF EXISTS mt"
+$CLICKHOUSE_CLIENT --query="CREATE TABLE mt (a Int32) Engine=MergeTree order by tuple()"
+$CLICKHOUSE_CLIENT --allow_experimental_live_view 1 --query="CREATE LIVE VIEW lv AS SELECT sum(a) FROM mt"
+$CLICKHOUSE_CLIENT --allow_experimental_live_view 1 --query="WATCH lv LIMIT 0 FORMAT JSONEachRowWithProgress" | sed -r -e 's/"elapsed_time":"[0-9]+"/"elapsed_time":"<ELAPSED-TIME>"/'
+$CLICKHOUSE_CLIENT --query="INSERT INTO mt VALUES (1),(2),(3)"
+$CLICKHOUSE_CLIENT --allow_experimental_live_view 1  --query="WATCH lv LIMIT 0 FORMAT JSONEachRowWithProgress" | sed -r -e 's/"elapsed_time":"[0-9]+"/"elapsed_time":"<ELAPSED-TIME>"/'
+$CLICKHOUSE_CLIENT --query="INSERT INTO mt VALUES (4),(5),(6)"
+$CLICKHOUSE_CLIENT --allow_experimental_live_view 1  --query="WATCH lv LIMIT 0 FORMAT JSONEachRowWithProgress" | sed -r -e 's/"elapsed_time":"[0-9]+"/"elapsed_time":"<ELAPSED-TIME>"/'
+$CLICKHOUSE_CLIENT --query="DROP TABLE lv"
+$CLICKHOUSE_CLIENT --query="DROP TABLE mt"
