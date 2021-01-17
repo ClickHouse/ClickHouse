a89962137813eb8ea582effab4b57cfcc1080303
Azat Khuzhin <a3at.mail@gmail.com>
2021-01-17 12:48:51 +0300

    DistributedBlockOutputStream: Remove superfluous brackets for string construction

--- src/Storages/Distributed/DistributedBlockOutputStream.cpp
+++ src/Storages/Distributed/DistributedBlockOutputStream.cpp
@@ -614,7 +614,7 @@ void DistributedBlockOutputStream::writeToShard(const Block & block, const std::
 
     /// tmp directory is used to ensure atomicity of transactions
     /// and keep monitor thread out from reading incomplete data
-    std::string first_file_tmp_path{};
+    std::string first_file_tmp_path;
 
     auto reservation = storage.getStoragePolicy()->reserveAndCheck(block.bytes());
     const auto disk = reservation->getDisk();
