b07d6c51325bb445bf4ee37761a700fd43e8f55c
Alexander Kuzmenkov <akuzm@yandex-team.ru>
2021-01-18 20:15:58 +0300

    old flaky test

--- tests/queries/0_stateless/01531_query_log_query_comment.sql
+++ tests/queries/0_stateless/01531_query_log_query_comment.sql
@@ -4,9 +4,17 @@ set log_queries_min_type='QUERY_FINISH';
 set enable_global_with_statement=1;
 select /* test=01531, enable_global_with_statement=0 */ 2;
 system flush logs;
-select count() from system.query_log where event_time >= now() - interval 5 minute and query like '%select /* test=01531, enable_global_with_statement=0 */ 2%';
+select count() from system.query_log
+where event_time >= now() - interval 5 minute
+    and query like '%select /* test=01531, enable_global_with_statement=0 */ 2%'
+    and current_database = currentDatabase()
+    ;
 
 set enable_global_with_statement=1;
 select /* test=01531 enable_global_with_statement=1 */ 2;
 system flush logs;
-select count() from system.query_log where event_time >= now() - interval 5 minute and query like '%select /* test=01531 enable_global_with_statement=1 */ 2%';
+select count() from system.query_log
+where event_time >= now() - interval 5 minute
+    and query like '%select /* test=01531 enable_global_with_statement=1 */ 2%'
+    and current_database = currentDatabase()
+    ;
