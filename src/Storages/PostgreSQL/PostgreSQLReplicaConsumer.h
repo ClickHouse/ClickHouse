a1bcc5fb3987e8473a6d115aa33138fa34ea2873
kssenii <sumarokovakseniia@mail.ru>
2021-01-19 15:29:22 +0000

    Decode replication messages

--- src/Storages/PostgreSQL/PostgreSQLReplicaConsumer.h
+++ src/Storages/PostgreSQL/PostgreSQLReplicaConsumer.h
@@ -1,6 +1,6 @@
 #pragma once
 
-#include <Storages/StoragePostgreSQL.h>
+#include "PostgreSQLConnection.h"
 #include "PostgreSQLReplicationHandler.h"
 #include "pqxx/pqxx"
 
@@ -18,10 +18,19 @@ public:
             const LSNPosition & start_lsn);
 
     void run();
+    void createSubscription();
 
 private:
+    void readString(const char * message, size_t & pos, size_t size, String & result);
+    Int64 readInt64(const char * message, size_t & pos);
+    Int32 readInt32(const char * message, size_t & pos);
+    Int16 readInt16(const char * message, size_t & pos);
+    Int8 readInt8(const char * message, size_t & pos);
+    void readTupleData(const char * message, size_t & pos, size_t size);
+
     void startReplication(
             const std::string & slot_name, const std::string start_lsn, const int64_t timeline, const std::string & plugin_args);
+    void decodeReplicationMessage(const char * replication_message, size_t size);
 
     Poco::Logger * log;
     const std::string replication_slot_name;
