1063b22b4c62b498d232f8acc10017663debdf21
alesapin <alesapin@gmail.com>
2021-01-19 12:40:25 +0300

    Add write buffer from nuraft

---
+++ src/Coordination/ReadBufferFromNuraftBuffer.h
@@ -0,0 +1,17 @@
+#pragma once
+#include <IO/ReadBufferFromMemory.h>
+
+#include <libnuraft/nuraft.hxx>
+
+namespace DB
+{
+
+class ReadBufferFromNuraftBuffer : public ReadBufferFromMemory
+{
+public:
+    explicit ReadBufferFromNuraftBuffer(nuraft::ptr<nuraft::buffer> buffer)
+        : ReadBufferFromMemory(buffer->data_begin(), buffer->size())
+    {}
+};
+
+}
