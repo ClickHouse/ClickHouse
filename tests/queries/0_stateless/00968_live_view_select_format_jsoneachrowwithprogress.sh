ebe3d28ec1ab3cc88a7dc3735a04117326050c63
vdimir <vdimir@yandex-team.ru>
2021-01-20 15:51:42 +0300

    Pin progress to bottom of output in 00968_live_view_select_format_jsoneachrowwithprogress

--- tests/queries/0_stateless/00968_live_view_select_format_jsoneachrowwithprogress.sh
+++ tests/queries/0_stateless/00968_live_view_select_format_jsoneachrowwithprogress.sh
@@ -9,6 +9,8 @@ $CLICKHOUSE_CLIENT --query="DROP TABLE IF EXISTS mt"
 $CLICKHOUSE_CLIENT --query="CREATE TABLE mt (a Int32) Engine=MergeTree order by tuple()"
 $CLICKHOUSE_CLIENT --allow_experimental_live_view 1 --query="CREATE LIVE VIEW lv AS SELECT * FROM mt"
 $CLICKHOUSE_CLIENT --query="INSERT INTO mt VALUES (1),(2),(3)"
-$CLICKHOUSE_CLIENT --allow_experimental_live_view 1 --query="SELECT * FROM lv FORMAT JSONEachRowWithProgress" | sed -r -e 's/"elapsed_time":"[0-9]+"/"elapsed_time":"<ELAPSED-TIME>"/'
+$CLICKHOUSE_CLIENT --allow_experimental_live_view 1 --query="SELECT * FROM lv FORMAT JSONEachRowWithProgress" \
+    | sed -r -e 's/"elapsed_time":"[0-9]+"/"elapsed_time":"<ELAPSED-TIME>"/' \
+    | awk '/"progress"/{ progresstail += $0 } /"row"/{ print $0 } END{ print $progresstail }'
 $CLICKHOUSE_CLIENT --query="DROP TABLE lv"
 $CLICKHOUSE_CLIENT --query="DROP TABLE mt"
