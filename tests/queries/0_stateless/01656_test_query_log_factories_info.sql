191842f69896b66e691c6d2292b0098de4f1d5eb
kssenii <sumarokovakseniia@mail.ru>
2021-01-21 20:01:51 +0000

    Fast test fix

--- tests/queries/0_stateless/01656_test_query_log_factories_info.sql
+++ tests/queries/0_stateless/01656_test_query_log_factories_info.sql
@@ -1,18 +1,17 @@
 SELECT uniq(1, 1, 2, 2), count(toDate('2000-12-05') + number as d) FROM numbers(100);
-INSERT INTO function null('x Int128') SELECT * FROM numbers_mt(100);
 SYSTEM FLUSH LOGS;
 SELECT factory_aggregate_functions AS aggreagte_functions,
        factory_table_functions AS table_functions,
        factory_functions AS functions
 FROM system.query_log WHERE type = 'QueryFinish' AND
-(query LIKE '%toDate(\'2000-12-05\')%' OR query LIKE 'null(\'x Int128\')')
+(query LIKE '%toDate(\'2000-12-05\')%')
 ORDER BY query_start_time DESC LIMIT 1 FORMAT TabSeparatedWithNames;
 SELECT '';
 
 SELECT CAST(arrayJoin([NULL, NULL]) AS Nullable(UInt8)) AS x;
 SYSTEM FLUSH LOGS;
 SELECT factory_functions AS functions, factory_data_types as data_types
-FROM system.query_log WHERE type = 'QueryFinish' AND query LIKE 'SELECT k, groupArray%'
+FROM system.query_log WHERE type = 'QueryFinish' AND query LIKE 'SELECT CAST(arrayJ%'
 ORDER BY query_start_time DESC LIMIT 1 FORMAT TabSeparatedWithNames;
 SELECT '';
 
@@ -22,7 +21,10 @@ CREATE OR REPLACE TABLE test_query_log_factories_info1.memory_table (event Strin
 SYSTEM FLUSH LOGS;
 SELECT factory_data_types AS data_types, factory_databases AS databases, factory_storages AS storages
 FROM system.query_log
-WHERE type == 'QueryFinish' AND (query LIKE '%database test_query_log_factories_info%' OR query LIKE '%TABLE test%')
-ORDER BY query_start_time DESC LIMIT 2 FORMAT TabSeparatedWithNames;
-SELECT '';
+WHERE type == 'QueryFinish' AND (query LIKE '%database test_query_log_factories_info%')
+ORDER BY query_start_time DESC LIMIT 1 FORMAT TabSeparatedWithNames;
+SELECT factory_data_types AS data_types, factory_databases AS databases, factory_storages AS storages
+FROM system.query_log
+WHERE type == 'QueryFinish' AND (query LIKE '%TABLE test%')
+ORDER BY query_start_time DESC LIMIT 1 FORMAT TabSeparatedWithNames;
 
