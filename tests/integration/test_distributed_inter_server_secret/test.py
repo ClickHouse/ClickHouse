f517f3fe4c1f125dce2d5bb046d5e72a17c9f22c
sundy-li <543950155@qq.com>
2021-01-21 14:55:13 +0800

    Update more integration tests

--- tests/integration/test_distributed_inter_server_secret/test.py
+++ tests/integration/test_distributed_inter_server_secret/test.py
@@ -83,7 +83,7 @@ def get_query_user_info(node, query_pattern):
 def get_query_setting_on_shard(node, query_pattern, setting):
     node.query("SYSTEM FLUSH LOGS")
     return node.query("""
-    SELECT Settings.Values['{}']
+    SELECT Settings['{}']
     FROM system.query_log
     WHERE
         query LIKE '%{}%' AND
