154382925902d4d1d764b508bcedbeb477c026c7
keenwolf <keenwolf@gmail.com>
2021-01-23 16:53:43 +0800

    Clean some comments

--- src/Functions/FunctionFile.cpp
+++ src/Functions/FunctionFile.cpp
@@ -58,7 +58,6 @@ namespace DB
                 auto & res_chars = res->getChars();
                 auto & res_offsets = res->getOffsets();
 
-                //File access permission check
                 const String user_files_path = context.getUserFilesPath();
                 String user_files_absolute_path = Poco::Path(user_files_path).makeAbsolute().makeDirectory().toString();
                 Poco::Path poco_filepath = Poco::Path(filename);
@@ -67,27 +66,11 @@ namespace DB
                 const String file_absolute_path = poco_filepath.absolute().toString();
                 checkReadIsAllowed(user_files_absolute_path, file_absolute_path);
 
-                //Method-1: Read file with ReadBuffer
                 ReadBufferFromFile in(file_absolute_path);
                 ssize_t file_len = Poco::File(file_absolute_path).getSize();
                 res_chars.resize_exact(file_len + 1);
                 char *res_buf = reinterpret_cast<char *>(&res_chars[0]);
                 in.readStrict(res_buf, file_len);
-
-                /*
-                //Method-2(Just for reference): Read directly into the String buf, which avoiding one copy from PageCache to ReadBuffer
-                int fd;
-                if (-1 == (fd = open(file_absolute_path.c_str(), O_RDONLY)))
-                     throwFromErrnoWithPath("Cannot open file " + std::string(file_absolute_path), std::string(file_absolute_path),
-                                           errno == ENOENT ? ErrorCodes::FILE_DOESNT_EXIST : ErrorCodes::CANNOT_OPEN_FILE);
-                if (file_len != pread(fd, res_buf, file_len, 0))
-                    throwFromErrnoWithPath("Read failed with " + std::string(file_absolute_path), std::string(file_absolute_path),
-                                           ErrorCodes::CANNOT_READ_FROM_FILE_DESCRIPTOR);
-                if (0 != close(fd))
-                    throw Exception("Cannot close file " + std::string(file_absolute_path), ErrorCodes::CANNOT_CLOSE_FILE);
-                fd = -1;
-                */
-
                 res_offsets.push_back(file_len + 1);
                 res_buf[file_len] = '\0';
 
