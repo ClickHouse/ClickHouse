86ead0f0a9df8bb0e372b860b5fe8c287282fcbd
Azat Khuzhin <a3at.mail@gmail.com>
2021-01-23 08:25:07 +0300

    Suppress UBsan for addMinutes/addHours/addWeeks with inf
    https://clickhouse-test-reports.s3.yandex.net/19466/bb6f775b89da564cff9892a2443332a758e884c0/fuzzer_ubsan/report.html#fail1

--- tests/queries/0_stateless/01662_date_ubsan.sql
+++ tests/queries/0_stateless/01662_date_ubsan.sql
@@ -3,3 +3,7 @@
 SELECT addMinutes(toDateTime('2021-01-01 00:00:00', 'GMT'), 9223372036854775808);
 SELECT addHours(toDateTime('2021-01-01 00:00:00', 'GMT'), 9223372036854775808);
 SELECT addWeeks(toDateTime('2021-01-01 00:00:00', 'GMT'), 9223372036854775808);
+-- tests with inf
+SELECT addMinutes(toDateTime('2021-01-01 00:00:00', 'GMT'), inf);
+SELECT addHours(toDateTime('2021-01-01 00:00:00', 'GMT'), inf);
+SELECT addWeeks(toDateTime('2021-01-01 00:00:00', 'GMT'), inf);
