d119fe036b3f73b8bff3da4d26a27207fc5b850c
Alexey Milovidov <milovidov@yandex-team.ru>
2021-01-25 00:22:38 +0300

    Preparation for performing relocations

--- main.cpp
+++ main.cpp
@@ -56,10 +56,154 @@
 #define AT_MINSIGSTKSZ          51
 
 
+using int32_t = int;
+using uint32_t = unsigned int;
 using int64_t = long;
+using uint64_t = unsigned long;
 using ssize_t = long;
 using intptr_t = long;
 using size_t = unsigned long;
+
+
+struct ProgramHeader
+{
+    enum Type : uint32_t
+    {
+        NULL = 0,    /// Program header table entry unused
+        LOAD = 1,    /// Loadable program segment
+        DYNAMIC = 2, /// Dynamic linking information
+        INTERP = 3,  /// Program interpreter
+        NOTE = 4,    /// Auxiliary information
+        SHLIB = 5,   /// Reserved
+        PHDR = 6,    /// Entry for header table itself
+        TLS = 7,     /// Thread-local storage segment
+        NUM = 8,     /// Number of defined types
+        GNU_EH_FRAME = 0x6474e550, /// .eh_frame_hdr segment
+        GNU_STACK = 0x6474e551,    /// Indicates stack executability
+        GNU_RELRO = 0x6474e552,    /// Read-only after relocation
+    };
+
+    struct Flags
+    {
+        uint32_t data;
+
+        bool readable() const { return data & 4; }
+        bool writeable() const { return data & 2; }
+        bool executable() const { return data & 1; }
+    };
+
+    Type type;
+    Flags flags;
+    uint64_t offset;
+    char * virtual_address;
+    char * physical_address;
+    size_t size_in_file;
+    size_t size_in_memory;
+    size_t alignment;
+};
+
+
+struct DynamicTableEntry
+{
+    enum Tag : uint64_t
+    {
+        NULL = 0, /// Marks end of dynamic section
+        NEEDED = 1, /// Name of needed library
+        PLTRELSZ = 2, /// Size in bytes of PLT relocs
+        PLTGOT = 3, /// Processor defined value
+        HASH = 4, /// Address of symbol hash table
+        STRTAB = 5, /// Address of string table
+        SYMTAB = 6, /// Address of symbol table
+        RELA = 7, /// Address of Rela relocs
+        RELASZ = 8, /// Total size of Rela relocs
+        RELAENT = 9, /// Size of one Rela reloc
+        STRSZ = 10, /// Size of string table
+        SYMENT = 11, /// Size of one symbol table entry
+        INIT = 12, /// Address of init function
+        FINI = 13, /// Address of termination function
+        SONAME = 14, /// Name of shared object
+        RPATH = 15, /// Library search path (deprecated)
+        SYMBOLIC = 16, /// Start symbol search here
+        REL = 17, /// Address of Rel relocs
+        RELSZ = 18, /// Total size of Rel relocs
+        RELENT = 19, /// Size of one Rel reloc
+        PLTREL = 20, /// Type of reloc in PLT
+        DEBUG = 21, /// For debugging; unspecified
+        TEXTREL = 22, /// Reloc might modify .text
+        JMPREL = 23, /// Address of PLT relocs
+        BIND_NOW = 24, /// Process relocations of object
+        INIT_ARRAY = 25, /// Array with addresses of init fct
+        FINI_ARRAY = 26, /// Array with addresses of fini fct
+        INIT_ARRAYSZ = 27, /// Size in bytes of DT_INIT_ARRAY
+        FINI_ARRAYSZ = 28, /// Size in bytes of DT_FINI_ARRAY
+        RUNPATH = 29, /// Library search path
+        FLAGS = 30, /// Flags for the object being loaded
+        ENCODING = 32, /// Start of encoded range
+        PREINIT_ARRAY = 32, /// Array with addresses of preinit fct
+        PREINIT_ARRAYSZ = 33, /// size in bytes of DT_PREINIT_ARRAY
+        NUM = 34, /// Number used
+        LOOS = 0x6000000d, /// Start of OS-specific
+        HIOS = 0x6ffff000, /// End of OS-specific
+        LOPROC = 0x70000000, /// Start of processor-specific
+        HIPROC = 0x7fffffff, /// End of processor-specific
+
+        /* DT_* entries which fall between DT_VALRNGHI & DT_VALRNGLO use the
+           Dyn.d_un.d_val field of the Elf*_Dyn structure.  This follows Sun's
+           approach.
+        */
+        VALRNGLO = 0x6ffffd00,
+
+        GNU_PRELINKED = 0x6ffffdf5, /// Prelinking timestamp
+        GNU_CONFLICTSZ = 0x6ffffdf6, /// Size of conflict section
+        GNU_LIBLISTSZ = 0x6ffffdf7, /// Size of library list
+        CHECKSUM = 0x6ffffdf8,
+        PLTPADSZ = 0x6ffffdf9,
+        MOVEENT = 0x6ffffdfa,
+        MOVESZ = 0x6ffffdfb,
+        FEATURE_1 = 0x6ffffdfc, /// Feature selection (DTF_*).
+        POSFLAG_1 = 0x6ffffdfd,	/* Flags for DT_* entries, effecting the following DT_* entry.  */
+        SYMINSZ = 0x6ffffdfe, /// Size of syminfo table (in bytes)
+        SYMINENT = 0x6ffffdff, /// Entry size of syminfo
+
+        VALRNGHI = 0x6ffffdff,
+
+        /* DT_* entries which fall between DT_ADDRRNGHI & DT_ADDRRNGLO use the
+            Dyn.d_un.d_ptr field of the Elf*_Dyn structure.
+            If any adjustment is made to the ELF object after it has been
+            built these entries will need to be adjusted.
+        */
+        ADDRRNGLO = 0x6ffffe00,
+
+        GNU_HASH = 0x6ffffef5, /// GNU-style hash table.
+        TLSDESC_PLT = 0x6ffffef6,
+        TLSDESC_GOT = 0x6ffffef7,
+        GNU_CONFLICT = 0x6ffffef8, /// Start of conflict section
+        GNU_LIBLIST = 0x6ffffef9, /// Library list
+        CONFIG = 0x6ffffefa, /// Configuration information.
+        DEPAUDIT = 0x6ffffefb, /// Dependency auditing.
+        AUDIT = 0x6ffffefc, /// Object auditing.
+        PLTPAD = 0x6ffffefd, /// PLT padding.
+        MOVETAB = 0x6ffffefe, /// Move table.
+        SYMINFO = 0x6ffffeff, /// Syminfo table.
+
+        ADDRRNGHI = 0x6ffffeff,
+
+        /* The versioning entry types.  The next are defined as part of the
+            GNU extension.
+        */
+        VERSYM = 0x6ffffff0,
+
+        RELACOUNT = 0x6ffffff9,
+        RELCOUNT = 0x6ffffffa,
+    };
+
+    Tag tag;
+    union
+    {
+        uint64_t value;
+        char * ptr;
+    };
+};
 
 
 /// These symbols have hidden linkage to avoid them being called through PLT.
@@ -139,6 +283,15 @@ void ALWAYS_INLINE assert(bool x)
         __builtin_trap();
 }
 
+template <typename T>
+T * getauxval(uint64_t type, uint64_t * auxv)
+{
+    for (; *auxv; auxv += 2)
+        if (auxv[0] == type)
+            return reinterpret_cast<T*>(&auxv[1]);
+    return nullptr;
+}
+
 }
 
 
@@ -167,9 +320,9 @@ int main(int argc, char ** argv, char ** envp)
 
     assert(envp[envc] == nullptr);
 
-    char ** auxv = &envp[envc + 1];
+    uint64_t * auxv = reinterpret_cast<uint64_t *>(&envp[envc + 1]);
     size_t auxc = 0;
-    for (size_t i = 0; auxv[i]; ++i)
+    for (size_t i = 0; auxv[i]; i += 2)
         ++auxc;
 
     print_number(argc);
@@ -193,6 +346,46 @@ int main(int argc, char ** argv, char ** envp)
     print_string("\n");
     print_number(auxc);
 
+    /// Check some auxv
+    size_t * page_size = getauxval<size_t>(AT_PAGESZ, auxv);
+    assert(page_size);
+
+    print_string("\n");
+    print_number(*page_size);
+
+    print_string("\n");
+
+    size_t * program_header_entry_size = getauxval<size_t>(AT_PHENT, auxv);
+    assert(*program_header_entry_size == sizeof(ProgramHeader));
+
+    size_t * num_program_headers = getauxval<size_t>(AT_PHNUM, auxv);
+    if (!num_program_headers)
+        print_string("No program headers\n");
+    else
+        print_number(*num_program_headers);
+
+    print_string("\n");
+
+    ProgramHeader * program_headers = *getauxval<ProgramHeader *>(AT_PHDR, auxv);
+    for (size_t i = 0; i < *num_program_headers; ++i)
+    {
+        print_number(program_headers[i].type);
+        print_string(": ");
+        print_number(program_headers[i].flags.data);
+        print_string("\n");
+
+        if (program_headers[i].type == ProgramHeader::DYNAMIC)
+        {
+            DynamicTableEntry * dynamic_table_entries = reinterpret_cast<DynamicTableEntry *>(program_headers[i].virtual_address);
+            for (const auto * it = dynamic_table_entries; it->tag != DynamicTableEntry::NULL; ++it)
+            {
+                print_number(it->tag);
+                print_string("\n");
+            }
+        }
+    }
+
+    print_string("\nRunning main...\n");
     _exit(main(argc, argv, envp));
 }
 
