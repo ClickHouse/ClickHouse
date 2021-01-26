3102835d56eda12547330006fc991a13d7b84855
Azat Khuzhin <a3at.mail@gmail.com>
2021-01-27 00:44:08 +0300

    tests: Use total_bytes>0 for Distributed engine in 00753_system_columns_and_system_tables

--- tests/queries/0_stateless/00753_system_columns_and_system_tables.sql
+++ tests/queries/0_stateless/00753_system_columns_and_system_tables.sql
@@ -133,7 +133,7 @@ CREATE TABLE check_system_tables AS check_system_tables_null Engine=Distributed(
 SYSTEM STOP DISTRIBUTED SENDS check_system_tables;
 SELECT total_bytes, total_rows FROM system.tables WHERE name = 'check_system_tables';
 INSERT INTO check_system_tables SELECT * FROM numbers(1) SETTINGS prefer_localhost_replica=0;
-SELECT total_bytes, total_rows FROM system.tables WHERE name = 'check_system_tables';
+SELECT total_bytes>0, total_rows FROM system.tables WHERE name = 'check_system_tables';
 SYSTEM FLUSH DISTRIBUTED check_system_tables;
 SELECT total_bytes, total_rows FROM system.tables WHERE name = 'check_system_tables';
 DROP TABLE check_system_tables_null;
