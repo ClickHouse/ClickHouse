edd4ca8c727ac69c59e721decaca5621db2cd2e5
Vitaliy Zakaznikov <vzakaznikov@protonmail.com>
2021-01-26 14:54:26 -0500

    Starting to update AES encryption tests to support changes to the supported data types by the encrypt plaintext parameter.

--- tests/testflows/aes_encryption/tests/decrypt.py
+++ tests/testflows/aes_encryption/tests/decrypt.py
@@ -54,7 +54,7 @@ def invalid_ciphertext(self):
             d_aad = None if not aad_len else f"'{aad}'"
 
             for datatype, ciphertext in invalid_ciphertexts:
-                if datatype in ["NULL"]:
+                if datatype == "NULL" or datatype.endswith("Null"):
                     continue
                 with When(f"invalid ciphertext={ciphertext}"):
                     if "cfb" in mode or "ofb" in mode or "ctr" in mode:
@@ -431,17 +431,10 @@ def decryption(self):
                 ciphertext = f"unhex({ciphertext})"
                 compare = plaintext
 
-                if datatype == "IPv4":
-                    cast = "toIPv4(IPv4NumToString(reinterpretAsUInt32"
-                    endcast = "))"
-                elif datatype in ["DateTime64", "UUID", "IPv6", "LowCardinality", "Enum8", "Enum16", "Decimal32", "Decimal64", "Decimal128", "Array"]:
-                    xfail(reason="no conversion")
-                elif datatype == "NULL":
+                if datatype == "NULL" or datatype.endswith("Null"):
                     ciphertext = "NULL"
                     cast = "isNull"
                     compare = None
-                elif datatype in ["Float32", "Float64", "Date", "DateTime"] or "Int" in datatype:
-                    cast = f"reinterpretAs{datatype}"
 
                 decrypt(ciphertext=ciphertext, key=f"'{key[:key_len]}'", mode=mode,
                     iv=(None if not iv_len else f"'{iv[:iv_len]}'"),
