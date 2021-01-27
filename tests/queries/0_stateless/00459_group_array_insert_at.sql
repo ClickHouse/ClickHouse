0db606084a3924cbd0de2251a327cb8b2780ae75
Alexander Kuzmenkov <akuzm@yandex-team.ru>
2021-01-27 18:30:09 +0300

    change some tests to see the effect

--- tests/queries/0_stateless/00459_group_array_insert_at.sql
+++ tests/queries/0_stateless/00459_group_array_insert_at.sql
@@ -1,3 +1,4 @@
+-- remove this comment before merge
 SELECT groupArrayInsertAt(toString(number), number * 2) FROM (SELECT * FROM system.numbers LIMIT 10);
 SELECT groupArrayInsertAt('-')(toString(number), number * 2) FROM (SELECT * FROM system.numbers LIMIT 10);
 SELECT groupArrayInsertAt([123])(range(number), number * 2) FROM (SELECT * FROM system.numbers LIMIT 10);
