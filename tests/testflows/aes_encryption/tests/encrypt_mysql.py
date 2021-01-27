79cc2e4c1c747197732e6177a2cd52965d4f5534
Vitaliy Zakaznikov <vzakaznikov@protonmail.com>
2021-01-27 08:20:44 -0500

    Updating AES encryption SRS. Adding test for LowCardinality(FixedString(N)) data type for plaintext.

--- tests/testflows/aes_encryption/tests/encrypt_mysql.py
+++ tests/testflows/aes_encryption/tests/encrypt_mysql.py
@@ -265,7 +265,7 @@ def syntax(self):
 
 @TestScenario
 @Requirements(
-    RQ_SRS008_AES_MySQL_Encrypt_Function_Parameters_PlainText("1.0"),
+    RQ_SRS008_AES_MySQL_Encrypt_Function_Parameters_PlainText("2.0"),
     RQ_SRS008_AES_MySQL_Encrypt_Function_Parameters_Mode("1.0"),
     RQ_SRS008_AES_MySQL_Encrypt_Function_Parameters_Mode_ValuesFormat("1.0"),
     RQ_SRS008_AES_MySQL_Encrypt_Function_Parameters_Mode_Values("1.0")
