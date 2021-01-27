f6d7123d17407a8383ee4ea19d7499c8a1bd5aa3
Azat Khuzhin <a3at.mail@gmail.com>
2021-01-27 07:58:02 +0300

    tests: split 00753_system_columns_and_system_tables (to disable Distributed part for arcadia)

--- tests/queries/0_stateless/00753_system_columns_and_system_tables.sql
+++ tests/queries/0_stateless/00753_system_columns_and_system_tables.sql
@@ -126,15 +126,3 @@ SELECT total_bytes, total_rows FROM system.tables WHERE name = 'check_system_tab
 INSERT INTO check_system_tables SELECT number+50 FROM numbers(50);
 SELECT total_bytes, total_rows FROM system.tables WHERE name = 'check_system_tables';
 DROP TABLE check_system_tables;
-
-SELECT 'Check total_bytes/total_rows for Distributed';
-CREATE TABLE check_system_tables_null (key Int) Engine=Null();
-CREATE TABLE check_system_tables AS check_system_tables_null Engine=Distributed(test_shard_localhost, currentDatabase(), check_system_tables_null);
-SYSTEM STOP DISTRIBUTED SENDS check_system_tables;
-SELECT total_bytes, total_rows FROM system.tables WHERE name = 'check_system_tables';
-INSERT INTO check_system_tables SELECT * FROM numbers(1) SETTINGS prefer_localhost_replica=0;
-SELECT total_bytes>0, total_rows FROM system.tables WHERE name = 'check_system_tables';
-SYSTEM FLUSH DISTRIBUTED check_system_tables;
-SELECT total_bytes, total_rows FROM system.tables WHERE name = 'check_system_tables';
-DROP TABLE check_system_tables_null;
-DROP TABLE check_system_tables;
