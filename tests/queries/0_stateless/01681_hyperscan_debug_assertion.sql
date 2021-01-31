45d0d427a988bff6780a27520b109c393f0f9a1b
Alexey Milovidov <milovidov@yandex-team.ru>
2021-01-31 11:23:55 +0300

    Update test

--- tests/queries/0_stateless/01681_hyperscan_debug_assertion.sql
+++
@@ -1,6 +0,0 @@
--- We throw our own exception from operator new.
--- In previous versions of Hyperscan it triggered debug assertion as it only expected std::bad_alloc.
-
-SET allow_hyperscan = 1;
-SET max_memory_usage = 4000000;
-SELECT [1, 2, 3, 11] = arraySort(multiMatchAllIndices('фабрикант', ['', 'рикан', 'а', 'f[ae]b[ei]rl', 'ф[иаэе]б[еэи][рпл]', 'афиукд', 'a[ft],th', '^ф[аиеэ]?б?[еэи]?$', 'берлик', 'fab', 'фа[беьв]+е?[рлко]'])); -- { serverError 241 }
