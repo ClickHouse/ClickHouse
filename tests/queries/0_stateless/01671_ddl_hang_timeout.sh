bef5af3f57986a103bc6510ee98ef0f981cb821d
yiguolei <676222867@qq.com>
2021-02-01 11:35:02 +0800

    fix functional test

--- tests/queries/0_stateless/01671_ddl_hang_timeout.sh
+++ tests/queries/0_stateless/01671_ddl_hang_timeout.sh
@@ -7,14 +7,14 @@ $CLICKHOUSE_CLIENT --query "CREATE DATABASE test_01671"
 function thread_create_drop_table {
     while true; do
         REPLICA=$(($RANDOM % 10))
-        $CLICKHOUSE_CLIENT --query "CREATE TABLE test_01671.t1 (x UInt64, s Array(Nullable(String))) ENGINE = ReplicatedMergeTree('/clickhouse/tables/test_01671/test_01671', 'r_$REPLICA') order by x"
+        $CLICKHOUSE_CLIENT --query "CREATE TABLE IF NOT EXISTS test_01671.t1 (x UInt64, s Array(Nullable(String))) ENGINE = ReplicatedMergeTree('/clickhouse/tables/test_01671/test_01671', 'r_$REPLICA') order by x" 2>/dev/null
         sleep 0.0$RANDOM
-        $CLICKHOUSE_CLIENT --query "DROP TABLE test_01671.t1"
+        $CLICKHOUSE_CLIENT --query "DROP TABLE IF EXISTS test_01671.t1"
     done
 }
 function thread_alter_table {
     while true; do
-        $CLICKHOUSE_CLIENT --query "ALTER TABLE test_01671.t1 on cluster test_shard_localhost ADD COLUMN newcol UInt32"
+        $CLICKHOUSE_CLIENT --query "ALTER TABLE test_01671.t1 on cluster test_shard_localhost ADD COLUMN newcol UInt32" >/dev/null 2>&1
         sleep 0.0$RANDOM
     done
 }
