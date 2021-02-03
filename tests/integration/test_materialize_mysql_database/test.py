3fc1641d9167a1b285826e21d95305568dc57a34
Stig Bakken <stig@stigbakken.com>
2021-02-04 04:52:06 +0800

    Show details of MaterializeMySQL tables in `system.tables`

--- tests/integration/test_materialize_mysql_database/test.py
+++ tests/integration/test_materialize_mysql_database/test.py
@@ -242,3 +242,7 @@ def test_system_parts_table(started_cluster, started_mysql_8_0, clickhouse_node)
 def test_multi_table_update(started_cluster, started_mysql_8_0, started_mysql_5_7, clickhouse_node):
     materialize_with_ddl.multi_table_update_test(clickhouse_node, started_mysql_5_7, "mysql1")
     materialize_with_ddl.multi_table_update_test(clickhouse_node, started_mysql_8_0, "mysql8_0")
+
+@pytest.mark.parametrize(('clickhouse_node'), [node_db_ordinary, node_db_ordinary])
+def test_system_tables_table(started_cluster, started_mysql_8_0, clickhouse_node):
+    materialize_with_ddl.system_tables_test(clickhouse_node, started_mysql_8_0, "mysql8_0")
