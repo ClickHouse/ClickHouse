2bb7f7dc178c28520a5f2cdffb6b74db7bb9c995
Russ Frank <rf@oden.io>
2021-02-04 00:26:23 -0500

    clang-format

--- src/AggregateFunctions/AggregateFunctionDeltaSum.h
+++ src/AggregateFunctions/AggregateFunctionDeltaSum.h
@@ -1,21 +1,20 @@
 #pragma once
 
-#include <experimental/type_traits>
 #include <type_traits>
+#include <experimental/type_traits>
 
-#include <IO/WriteHelpers.h>
 #include <IO/ReadHelpers.h>
+#include <IO/WriteHelpers.h>
 
-#include <DataTypes/DataTypesNumber.h>
-#include <DataTypes/DataTypesDecimal.h>
 #include <Columns/ColumnVector.h>
+#include <DataTypes/DataTypesDecimal.h>
+#include <DataTypes/DataTypesNumber.h>
 
 #include <AggregateFunctions/IAggregateFunction.h>
 
 
 namespace DB
 {
-
 template <typename T>
 struct AggregationFunctionDeltaSumData
 {
@@ -27,39 +26,34 @@ struct AggregationFunctionDeltaSumData
 };
 
 template <typename T>
-class AggregationFunctionDeltaSum final : public IAggregateFunctionDataHelper<
-    AggregationFunctionDeltaSumData<T>, AggregationFunctionDeltaSum<T>>
+class AggregationFunctionDeltaSum final
+    : public IAggregateFunctionDataHelper<AggregationFunctionDeltaSumData<T>, AggregationFunctionDeltaSum<T>>
 {
 public:
     AggregationFunctionDeltaSum(const DataTypes & arguments, const Array & params)
-        : IAggregateFunctionDataHelper<
-          AggregationFunctionDeltaSumData<T>, AggregationFunctionDeltaSum<T>> {arguments, params}
+        : IAggregateFunctionDataHelper<AggregationFunctionDeltaSumData<T>, AggregationFunctionDeltaSum<T>>{arguments, params}
     {
         // empty constructor
     }
 
-    String getName() const override 
-    {
-        return "deltaSum";
-    }
+    String getName() const override { return "deltaSum"; }
 
-    DataTypePtr getReturnType() const override
-    {
-        return std::make_shared<DataTypeNumber<T>>();
-    }
+    DataTypePtr getReturnType() const override { return std::make_shared<DataTypeNumber<T>>(); }
 
     void ALWAYS_INLINE add(AggregateDataPtr place, const IColumn ** columns, size_t row_num, Arena *) const override
     {
         const T & value = (*columns[0])[row_num].get<T>();
 
-        if (this->data(place).last < value && this->data(place).seen_last) {
+        if (this->data(place).last < value && this->data(place).seen_last)
+        {
             this->data(place).sum += (value - this->data(place).last);
         }
 
         this->data(place).last = value;
         this->data(place).seen_last = true;
 
-        if (this->data(place).seen_first == false) {
+        if (this->data(place).seen_first == false)
+        {
             this->data(place).first = value;
             this->data(place).seen_first = true;
         }
@@ -67,9 +61,12 @@ public:
 
     void ALWAYS_INLINE merge(AggregateDataPtr place, ConstAggregateDataPtr rhs, Arena *) const override
     {
-        if ((this->data(place).last < this->data(rhs).first) && this->data(place).seen_last && this->data(rhs).seen_first) {
+        if ((this->data(place).last < this->data(rhs).first) && this->data(place).seen_last && this->data(rhs).seen_first)
+        {
             this->data(place).sum += this->data(rhs).sum + (this->data(rhs).first - this->data(place).last);
-        } else {
+        }
+        else
+        {
             this->data(place).sum += this->data(rhs).sum;
         }
 
@@ -77,7 +74,7 @@ public:
         this->data(place).first = this->data(rhs).first;
     }
 
-        void serialize(ConstAggregateDataPtr place, WriteBuffer & buf) const override
+    void serialize(ConstAggregateDataPtr place, WriteBuffer & buf) const override
     {
         writeIntBinary(this->data(place).sum, buf);
         writeIntBinary(this->data(place).first, buf);
