ef020b230582ec9c2fb79eb5db020e21bebec5b0
Russ Frank <rf@oden.io>
2021-02-04 11:55:37 -0500

    merge tests

--- tests/queries/0_stateless/01700_deltasum.sql
+++ tests/queries/0_stateless/01700_deltasum.sql
@@ -3,3 +3,5 @@ select deltaSum(arrayJoin([1, 2, 3, 0, 3, 4]));
 select deltaSum(arrayJoin([1, 2, 3, 0, 3, 4, 2, 3]));
 select deltaSum(arrayJoin([1, 2, 3, 0, 3, 3, 3, 3, 3, 4, 2, 3]));
 select deltaSum(arrayJoin([1, 2, 3, 0, 0, 0, 0, 3, 3, 3, 3, 3, 4, 2, 3]));
+select deltaSumMerge(rows) from (select deltaSumState(arrayJoin([0, 1])) as rows union all select deltaSumState(arrayJoin([4, 5])) as rows);
+select deltaSumMerge(rows) from (select deltaSumState(arrayJoin([4, 5])) as rows union all select deltaSumState(arrayJoin([0, 1])) as rows);
