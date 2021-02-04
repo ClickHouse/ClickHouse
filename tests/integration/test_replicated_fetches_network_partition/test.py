15b0560bcc210b7c2c7becdf72f8258577ed6b19
Nicolae Vartolomei <nv@cloudflare.com>
2021-02-04 20:21:20 +0000

    Reformat test and drop tables at the end to reduce the size of test results archive

--- tests/integration/test_replicated_fetches_network_partition/test.py
+++ tests/integration/test_replicated_fetches_network_partition/test.py
@@ -12,6 +12,7 @@ node1 = cluster.add_instance('node1', with_zookeeper=True)
 node2 = cluster.add_instance('node2', with_zookeeper=True)
 node3 = cluster.add_instance('node3', with_zookeeper=True, main_configs=['configs/merge_tree.xml'])
 
+
 @pytest.fixture(scope="module")
 def started_cluster():
     try:
@@ -36,23 +37,24 @@ def test_no_stall(started_cluster):
     node2.query("SYSTEM STOP MERGES")
     node3.query("SYSTEM STOP MERGES")
 
+    # Pause node3 until the test setup is prepared
+    node3.query("SYSTEM STOP FETCHES t")
+
+    node1.query("INSERT INTO t SELECT 1, '{}' FROM numbers(5000)".format(get_random_string(104857)))
+    node1.query("INSERT INTO t SELECT 2, '{}' FROM numbers(5000)".format(get_random_string(104857)))
+    node1.query("INSERT INTO t SELECT 3, '{}' FROM numbers(5000)".format(get_random_string(104857)))
+    node1.query("INSERT INTO t SELECT 4, '{}' FROM numbers(5000)".format(get_random_string(104857)))
+    node1.query("INSERT INTO t SELECT 5, '{}' FROM numbers(5000)".format(get_random_string(104857)))
+
+    # Make sure node2 has all the parts.
+    node2.query("SYSTEM SYNC REPLICA t")
+
+    # Do not allow sending from replica 2 yet, force node3 to initiate replication from node1.
+    node2.query("SYSTEM STOP REPLICATED SENDS")
+
+    print("replica 2 fully synced")
+
     with PartitionManager() as pm:
-        node3.query("SYSTEM STOP FETCHES t")
-
-        node1.query("INSERT INTO t SELECT 1, '{}' FROM numbers(5000)".format(get_random_string(104857)))
-        node1.query("INSERT INTO t SELECT 2, '{}' FROM numbers(5000)".format(get_random_string(104857)))
-        node1.query("INSERT INTO t SELECT 3, '{}' FROM numbers(5000)".format(get_random_string(104857)))
-        node1.query("INSERT INTO t SELECT 4, '{}' FROM numbers(5000)".format(get_random_string(104857)))
-        node1.query("INSERT INTO t SELECT 5, '{}' FROM numbers(5000)".format(get_random_string(104857)))
-
-        # Make sure node2 has all the parts.
-        node2.query("SYSTEM SYNC REPLICA t")
-
-        # Do not allow sending from replica 2 yet, force node3 to initiate replication from node1.
-        node2.query("SYSTEM STOP REPLICATED SENDS")
-
-        print("replica 2 fully synced")
-
         # Make node1 very slow, node3 should replicate from node2 instead.
         pm.add_network_delay(node1, 2000)
 
@@ -66,12 +68,20 @@ def test_no_stall(started_cluster):
         node2.query("SYSTEM START REPLICATED SENDS")
 
         for _ in range(1000):
-            print('Currently running fetches', node3.query("SELECT result_part_name, source_replica_hostname, progress FROM system.replicated_fetches").strip().split())
+            print('Currently running fetches:')
+            print(node3.query("SELECT result_part_name, source_replica_hostname, progress FROM system.replicated_fetches").strip())
+            print()
+
             parts_fetched = node3.query("SELECT count() FROM system.parts WHERE table = 't'").strip()
-            print('parts_fetched', parts_fetched)
+            print('parts_fetched:', parts_fetched)
+            print()
 
             # Replication done.
             if parts_fetched == "5":
                 break
 
             time.sleep(3)
+
+    node1.query("DROP TABLE t SYNC")
+    node2.query("DROP TABLE t SYNC")
+    node3.query("DROP TABLE t SYNC")
