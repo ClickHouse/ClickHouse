55443b1f5b813630ddba3c5a11fb3045e66dea74
Ivan Lezhankin <ilezhankin@yandex-team.ru>
2021-02-05 14:19:55 +0300

    Fix style check

--- tests/queries/0_stateless/01606_git_import.sh
+++ tests/queries/0_stateless/01606_git_import.sh
@@ -6,7 +6,7 @@ CURDIR=$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)
 
 # Clone some not too large repository and create a database from it.
 
-cd $CLICKHOUSE_TMP
+cd $CLICKHOUSE_TMP || exit
 
 # Protection for network errors
 for _ in {1..10}; do
