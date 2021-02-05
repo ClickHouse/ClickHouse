a1bcd4b128622ec2db79ea7564c60664784b52cc
Alexander Kuzmenkov <36882414+akuzm@users.noreply.github.com>
2021-02-05 10:06:39 +0300

    Update 01232_preparing_sets_race_condition_long.sh

--- tests/queries/0_stateless/01232_preparing_sets_race_condition_long.sh
+++ tests/queries/0_stateless/01232_preparing_sets_race_condition_long.sh
@@ -1,7 +1,5 @@
 #!/usr/bin/env bash
 
-# remove this comment before merge
-
 CURDIR=$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)
 # shellcheck source=../shell_config.sh
 . "$CURDIR"/../shell_config.sh
