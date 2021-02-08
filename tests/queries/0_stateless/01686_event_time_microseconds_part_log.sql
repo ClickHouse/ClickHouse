14fe627d325f0aa44841f0acd2d853050579cefe
bharatnc <bharatnc@gmail.com>
2021-02-08 07:37:47 -0800

    Tests - accept review suggestion

--- tests/queries/0_stateless/01686_event_time_microseconds_part_log.sql
+++ tests/queries/0_stateless/01686_event_time_microseconds_part_log.sql
@@ -13,19 +13,11 @@ INSERT INTO table_with_single_pk SELECT number, toString(number % 10) FROM numbe
 SYSTEM FLUSH LOGS;
 
 WITH (
-        (
-            SELECT event_time_microseconds
-            FROM system.part_log
-            ORDER BY event_time DESC
-            LIMIT 1
-        ) AS time_with_microseconds,
-        (
-            SELECT event_time
-            FROM system.part_log
-            ORDER BY event_time DESC
-            LIMIT 1
-        ) AS time
-    )
-SELECT if(dateDiff('second', toDateTime(time_with_microseconds), toDateTime(time)) = 0, 'ok', 'fail');
+         SELECT (event_time, event_time_microseconds)
+         FROM system.part_log
+         ORDER BY event_time DESC
+         LIMIT 1
+    ) AS time
+SELECT if(dateDiff('second', toDateTime(time.2), toDateTime(time.1)) = 0, 'ok', 'fail');
 
 DROP TABLE IF EXISTS table_with_single_pk;
