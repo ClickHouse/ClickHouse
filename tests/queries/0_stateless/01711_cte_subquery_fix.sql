34cc9d54308fc24d93c33c876d2db7833589bdea
alesapin <alesapin@gmail.com>
2021-02-09 14:54:13 +0300

    Update 01711_cte_subquery_fix.sql

--- tests/queries/0_stateless/01711_cte_subquery_fix.sql
+++ tests/queries/0_stateless/01711_cte_subquery_fix.sql
@@ -1,2 +1,3 @@
-create or replace table t engine = Memory as with cte as (select * from numbers(10)) select * from cte;
+drop table if exists t;
+create table t engine = Memory as with cte as (select * from numbers(10)) select * from cte;
 drop table t;
