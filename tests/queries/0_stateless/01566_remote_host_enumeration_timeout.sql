04ecb5241bdb86e12f3126a914b3c72ab2e015a8
Alexander Kuzmenkov <akuzm@yandex-team.ru>
2021-02-09 11:59:27 +0300

    try to fix the test

--- tests/queries/0_stateless/01566_remote_host_enumeration_timeout.sql
+++ tests/queries/0_stateless/01566_remote_host_enumeration_timeout.sql
@@ -1,2 +1,5 @@
-select * from remote('badhost{0..200}', system, one) settings max_execution_time = 1; -- { serverError 159 }
+-- somehow this one is too fast in release mode
+-- select * from remote('badhost{0..200}.test', system, one) settings max_execution_time = 1; -- { serverError 159 }
+
+-- addresses from TEST-NET-1
 select * from remote('192.0.2.{0..200}', system, one) settings max_execution_time = 1; -- { serverError 159 }
