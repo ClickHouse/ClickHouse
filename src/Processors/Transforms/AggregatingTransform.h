3f79dfb0edb4c5af679bc1827d268d4eb955208c
MaxTheHuman <maxsev1999@yandex.ru>
2021-02-09 23:52:12 +0300

    fix typos

--- src/Processors/Transforms/AggregatingTransform.h
+++ src/Processors/Transforms/AggregatingTransform.h
@@ -14,7 +14,7 @@ public:
     Arenas arenas;
     AggregatedArenasChunkInfo(Arenas arenas_)
         : arenas(std::move(arenas_))
-    {}  
+    {}
 };
 
 class AggregatedChunkInfo : public ChunkInfo
