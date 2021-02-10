1227ca3d7a1d75d83144bb6729b4b7c82e7ceade
alesapin <alesapin@gmail.com>
2021-02-10 14:49:41 +0300

    Fix min bytes for wide parts

--- tests/queries/0_stateless/01712_no_adaptive_granularity_vertical_merge.sql
+++ tests/queries/0_stateless/01712_no_adaptive_granularity_vertical_merge.sql
@@ -7,7 +7,7 @@ CREATE TABLE old_school_table
 )
 ENGINE = MergeTree()
 ORDER BY key
-SETTINGS index_granularity_bytes = 0, enable_mixed_granularity_parts = 0,
+SETTINGS index_granularity_bytes = 0, enable_mixed_granularity_parts = 0, min_bytes_for_wide_part = 0,
 vertical_merge_algorithm_min_rows_to_activate = 1, vertical_merge_algorithm_min_columns_to_activate = 1;
 
 INSERT INTO old_school_table VALUES (1, '1');
