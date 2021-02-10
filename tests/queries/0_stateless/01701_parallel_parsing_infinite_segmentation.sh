30d648dc3cfa12aef2ddf01a7424226edfbd91f4
Nikita Mikhailov <jakalletti@jakalletti-build.sas.yp-c.yandex.net>
2021-02-10 17:22:46 +0300

    better

--- tests/queries/0_stateless/01701_parallel_parsing_infinite_segmentation.sh
+++ tests/queries/0_stateless/01701_parallel_parsing_infinite_segmentation.sh
@@ -6,4 +6,4 @@ CURDIR=$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)
 
 python3 -c "print('{{\"a\":\"{}\", \"b\":\"{}\"}}'.format('clickhouse'* 10000000, 'dbms' * 100000000))" > big_json.json
 
-clickhouse-local --input_format_parallel_parsing=1 --max_memory_usage=0 -q "select count() from file('big_json.json', 'JSONEachRow', 'a String, b String')" 2>&1 | grep -q "min_chunk_bytes_for_parallel_parsing" && echo "Ok." || echo "FAIL" ||:
+${CLICKHOUSE_LOCAL} --input_format_parallel_parsing=1 --max_memory_usage=0 -q "select count() from file('big_json.json', 'JSONEachRow', 'a String, b String')" 2>&1 | grep -q "min_chunk_bytes_for_parallel_parsing" && echo "Ok." || echo "FAIL" ||:
