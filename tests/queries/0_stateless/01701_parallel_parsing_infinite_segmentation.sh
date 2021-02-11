c062e931638a5616bafec04d14d28d01dd655128
Nikita Mikhailov <jakalletti@jakalletti-build.sas.yp-c.yandex.net>
2021-02-11 15:00:14 +0300

    fix test

--- tests/queries/0_stateless/01701_parallel_parsing_infinite_segmentation.sh
+++ tests/queries/0_stateless/01701_parallel_parsing_infinite_segmentation.sh
@@ -5,7 +5,9 @@ CURDIR=$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)
 . "$CURDIR"/../shell_config.sh   
 
 python3 -c "for i in range(10):print('{{\"a\":\"{}\", \"b\":\"{}\"'.format('clickhouse'* 1000000, 'dbms' * 1000000))" > big_json.json
-python3 -c "for i in range(100):print('{{\"a\":\"{}\", \"b\":\"{}\"}}'.format('clickhouse'* 100000, 'dbms' * 100000))" > big_json.json
+python3 -c "for i in range(100):print('{{\"a\":\"{}\", \"b\":\"{}\"}}'.format('clickhouse'* 100000, 'dbms' * 100000))" >> big_json.json
 
 
 ${CLICKHOUSE_LOCAL} --input_format_parallel_parsing=1 --max_memory_usage=0 -q "select count() from file('big_json.json', 'JSONEachRow', 'a String, b String')" 2>&1 | grep -q "min_chunk_bytes_for_parallel_parsing" && echo "Ok." || echo "FAIL" ||:
+
+rm big_json.json
