ac149592c687646992df1cb0ddc19f0ab169e55a
Yatsishin Ilya <2159081+qoega@users.noreply.github.com>
2021-02-11 19:05:04 +0300

    wip

--- tests/integration/test_dictionaries_all_layouts_separate_sources/test_cassandra.py
+++ tests/integration/test_dictionaries_all_layouts_separate_sources/test_cassandra.py
@@ -15,7 +15,7 @@ node = None
 simple_tester = None
 complex_tester = None
 ranged_tester = None
-
+test_name = "cassandra"
 
 def setup_module(module):
     global cluster
@@ -24,10 +24,9 @@ def setup_module(module):
     global complex_tester
     global ranged_tester
 
-    for f in os.listdir(DICT_CONFIG_PATH):
-        os.remove(os.path.join(DICT_CONFIG_PATH, f))
 
     simple_tester = SimpleLayoutTester()
+    simple_tester.cleanup()
     simple_tester.create_dictionaries(SOURCE)
 
     complex_tester = ComplexLayoutTester()
@@ -37,24 +36,19 @@ def setup_module(module):
     ranged_tester.create_dictionaries(SOURCE)
     # Since that all .xml configs were created
 
-    cluster = ClickHouseCluster(__file__)
+    cluster = ClickHouseCluster(__file__, name=test_name)
 
-    dictionaries = []
     main_configs = []
     main_configs.append(os.path.join('configs', 'disable_ssl_verification.xml'))
     main_configs.append(os.path.join('configs', 'log_conf.xml'))
 
-    for fname in os.listdir(DICT_CONFIG_PATH):
-        dictionaries.append(os.path.join(DICT_CONFIG_PATH, fname))
+    dictionaries = simple_tester.list_dictionaries()
 
     node = cluster.add_instance('node', main_configs=main_configs, dictionaries=dictionaries, with_cassandra=True)
 
 
 def teardown_module(module):
-    global DICT_CONFIG_PATH
-    for fname in os.listdir(DICT_CONFIG_PATH):
-        os.remove(os.path.join(DICT_CONFIG_PATH, fname))
-
+    simple_tester.cleanup()
 
 @pytest.fixture(scope="module")
 def started_cluster():
--- tests/integration/test_dictionaries_all_layouts_separate_sources/test_clickhouse_remote.py
+++ tests/integration/test_dictionaries_all_layouts_separate_sources/test_clickhouse_remote.py
@@ -8,14 +8,14 @@ from helpers.cluster import ClickHouseCluster
 from helpers.dictionary import Field, Row, Dictionary, DictionaryStructure, Layout
 from helpers.external_sources import SourceClickHouse
 
-SOURCE = SourceClickHouse("RemoteClickHouse", "localhost", "9000", "clickhouse1", "9000", "default", "")
+SOURCE = SourceClickHouse("RemoteClickHouse", "localhost", "9000", "clickhouse_remote", "9000", "default", "")
 
 cluster = None
 node = None
 simple_tester = None
 complex_tester = None
 ranged_tester = None
-
+test_name = "remote"
 
 def setup_module(module):
     global cluster
@@ -24,37 +24,31 @@ def setup_module(module):
     global complex_tester
     global ranged_tester
 
-    for f in os.listdir(DICT_CONFIG_PATH):
-        os.remove(os.path.join(DICT_CONFIG_PATH, f))
-
-    simple_tester = SimpleLayoutTester()
+    simple_tester = SimpleLayoutTester(test_name)
+    simple_tester.cleanup()
     simple_tester.create_dictionaries(SOURCE)
 
-    complex_tester = ComplexLayoutTester()
+    complex_tester = ComplexLayoutTester(test_name)
     complex_tester.create_dictionaries(SOURCE)
 
-    ranged_tester = RangedLayoutTester()
+    ranged_tester = RangedLayoutTester(test_name)
     ranged_tester.create_dictionaries(SOURCE)
     # Since that all .xml configs were created
 
-    cluster = ClickHouseCluster(__file__)
+    cluster = ClickHouseCluster(__file__, name=test_name)
 
-    dictionaries = []
     main_configs = []
     main_configs.append(os.path.join('configs', 'disable_ssl_verification.xml'))
 
-    for fname in os.listdir(DICT_CONFIG_PATH):
-        dictionaries.append(os.path.join(DICT_CONFIG_PATH, fname))
+    dictionaries = simple_tester.list_dictionaries()
 
-    cluster.add_instance('clickhouse1', main_configs=main_configs)
+    cluster.add_instance('clickhouse_remote', main_configs=main_configs)
 
-    node = cluster.add_instance('node', main_configs=main_configs, dictionaries=dictionaries)
+    node = cluster.add_instance('remote_node', main_configs=main_configs, dictionaries=dictionaries)
 
 
 def teardown_module(module):
-    global DICT_CONFIG_PATH
-    for fname in os.listdir(DICT_CONFIG_PATH):
-        os.remove(os.path.join(DICT_CONFIG_PATH, fname))
+    simple_tester.cleanup()
 
 
 @pytest.fixture(scope="module")
