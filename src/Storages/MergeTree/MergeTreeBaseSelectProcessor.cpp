85277d6a417b633431ac18f2bdbdb7ca7da57568
Nikolai Kochetov <nik-kochetov@yandex-team.ru>
2021-02-15 22:54:47 +0300

    Comment debug output.

--- src/Storages/MergeTree/MergeTreeBaseSelectProcessor.cpp
+++ src/Storages/MergeTree/MergeTreeBaseSelectProcessor.cpp
@@ -334,12 +334,12 @@ void MergeTreeBaseSelectProcessor::executePrewhereActions(Block & block, const P
 {
     if (prewhere_info)
     {
-        std::cerr << "0: " << block.dumpStructure() << std::endl;
+        // std::cerr << "0: " << block.dumpStructure() << std::endl;
 
         if (prewhere_info->alias_actions)
             prewhere_info->alias_actions->execute(block);
 
-        std::cerr << "1: " << block.dumpStructure() << std::endl;
+        // std::cerr << "1: " << block.dumpStructure() << std::endl;
 
         if (prewhere_info->row_level_filter)
         {
@@ -351,12 +351,12 @@ void MergeTreeBaseSelectProcessor::executePrewhereActions(Block & block, const P
                     ErrorCodes::LOGICAL_ERROR);
             }
         }
-        std::cerr << "2: " << block.dumpStructure() << std::endl;
+        // std::cerr << "2: " << block.dumpStructure() << std::endl;
 
         if (prewhere_info->prewhere_actions)
             prewhere_info->prewhere_actions->execute(block);
 
-        std::cerr << "3: " << block.dumpStructure() << std::endl;
+        // std::cerr << "3: " << block.dumpStructure() << std::endl;
 
         auto & prewhere_column = block.getByName(prewhere_info->prewhere_column_name);
         if (!prewhere_column.type->canBeUsedInBooleanContext())
@@ -373,7 +373,7 @@ void MergeTreeBaseSelectProcessor::executePrewhereActions(Block & block, const P
             ctn.column = ctn.type->createColumnConst(block.rows(), 1u)->convertToFullColumnIfConst();
         }
 
-        std::cerr << "4: " << block.dumpStructure() << std::endl;
+        // std::cerr << "4: " << block.dumpStructure() << std::endl;
     }
 }
 
