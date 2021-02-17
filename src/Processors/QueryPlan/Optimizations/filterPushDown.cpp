56a5d1dafaa7cb08719277886000349490c47eda
Nikolai Kochetov <nik-kochetov@yandex-team.ru>
2021-02-17 21:48:26 +0300

    Skip stateful functions

--- src/Processors/QueryPlan/Optimizations/filterPushDown.cpp
+++ src/Processors/QueryPlan/Optimizations/filterPushDown.cpp
@@ -123,6 +123,9 @@ size_t tryPushDownFilter(QueryPlan::Node * parent_node, QueryPlan::Nodes & nodes
     if (!filter)
         return 0;
 
+    if (filter->getExpression()->hasStatefulFunctions())
+        return 0;
+
     if (auto * aggregating = typeid_cast<AggregatingStep *>(child.get()))
     {
         const auto & params = aggregating->getParams();
