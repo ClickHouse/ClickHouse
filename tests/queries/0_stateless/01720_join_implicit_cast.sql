6edb3503a4747e6659ad254d3959c5ee8982629e
vdimir <vdimir@yandex-team.ru>
2021-02-17 17:19:30 +0300

    Add tests to 01720_join_implicit_cast - switch, use_nulls

--- tests/queries/0_stateless/01720_join_implicit_cast.sql
+++ tests/queries/0_stateless/01720_join_implicit_cast.sql
@@ -94,8 +94,69 @@ SELECT * FROM t1 LEFT JOIN t2 ON(t1.a == t2.a) ORDER BY (a); -- { serverError 53
 SELECT * FROM t1 RIGHT JOIN t2 ON (t1.a == t2.a) ORDER BY (a); -- { serverError 53 }
 SELECT * FROM t1 INNER JOIN t2 ON (t1.a == t2.a) ORDER BY (a); -- { serverError 53 }
 
+
+SELECT '--- switch ---';
+
+SET join_algorithm = 'auto';
+SET max_bytes_in_join = 100;
+
+SELECT '- full -';
+SELECT a, b, t2.b FROM t1 FULL JOIN t2 USING (a) ORDER BY (a);
+SELECT '- left -';
+SELECT a, b, t2.b FROM t1 LEFT JOIN t2 USING (a) ORDER BY (a);
+SELECT '- right -';
+SELECT a, b, t2.b FROM t1 RIGHT JOIN t2 USING (a) ORDER BY (a);
+SELECT '- inner -';
+SELECT a, b, t2.b FROM t1 INNER JOIN t2 USING (a) ORDER BY (a);
+
+
+SELECT '- full -';
+SELECT a, t1.a, t2.a FROM t1 FULL JOIN t2 USING (a) ORDER BY (t1.a, t2.a);
+SELECT '- left -';
+SELECT a, t1.a, t2.a FROM t1 LEFT JOIN t2 USING (a) ORDER BY (t1.a, t2.a);
+SELECT '- right -';
+SELECT a, t1.a, t2.a FROM t1 RIGHT JOIN t2 USING (a) ORDER BY (t1.a, t2.a);
+SELECT '- inner -';
+SELECT a, t1.a, t2.a FROM t1 INNER JOIN t2 USING (a) ORDER BY (t1.a, t2.a);
+
+select '- agg -';
+SELECT sum(a) == 7 FROM t1 FULL JOIN t2 USING (a) WHERE b > 102 AND t2.b <= 204;
+SELECT sum(a) == 7 FROM t1 INNER JOIN t2 USING (a) WHERE b > 102 AND t2.b <= 204;
+
+SELECT sum(b) = 103 FROM t1 LEFT JOIN t2 USING (a) WHERE b > 102 AND t2.b < 204;
+SELECT sum(t2.b) = 203 FROM t1 RIGHT JOIN t2 USING (a) WHERE b > 102 AND t2.b < 204;
+
+SELECT '- types -';
+SELECT any(toTypeName(a)) == 'Int32' AND any(toTypeName(t2.a)) == 'Int32' FROM t1 FULL JOIN t2 USING (a);
+SELECT any(toTypeName(a)) == 'Int32' AND any(toTypeName(t2.a)) == 'Int32' FROM t1 LEFT JOIN t2 USING (a);
+SELECT any(toTypeName(a)) == 'Int32' AND any(toTypeName(t2.a)) == 'Int32' FROM t1 RIGHT JOIN t2 USING (a);
+SELECT any(toTypeName(a)) == 'Int32' AND any(toTypeName(t2.a)) == 'Int32' FROM t1 INNER JOIN t2 USING (a);
+
+SELECT toTypeName(any(a)) == 'Int32' AND toTypeName(any(t2.a)) == 'Int32' FROM t1 FULL JOIN t2 USING (a);
+SELECT min(toTypeName(a) == 'Int32' AND toTypeName(t2.a) == 'Int32') FROM t1 FULL JOIN t2 USING (a);
+
+SELECT * FROM t1 FULL JOIN t2 ON (t1.a == t2.a) ORDER BY (a); -- { serverError 53 }
+SELECT * FROM t1 LEFT JOIN t2 ON(t1.a == t2.a) ORDER BY (a); -- { serverError 53 }
+SELECT * FROM t1 RIGHT JOIN t2 ON (t1.a == t2.a) ORDER BY (a); -- { serverError 53 }
+SELECT * FROM t1 INNER JOIN t2 ON (t1.a == t2.a) ORDER BY (a); -- { serverError 53 }
+
+SET max_bytes_in_join = 0;
+
+SELECT '--- join use nulls ---';
+
+SET join_use_nulls = 1;
+
+SELECT '- full -';
+SELECT a, b, t2.b FROM t1 FULL JOIN t2 USING (a) ORDER BY (a);
+SELECT '- right -';
+SELECT a, b, t2.b FROM t1 RIGHT JOIN t2 USING (a) ORDER BY (a);
+
+SET join_use_nulls = 0;
+
 DROP TABLE IF EXISTS t1;
 DROP TABLE IF EXISTS t2;
+
+select '---';
 
 DROP TABLE IF EXISTS t_ab1;
 DROP TABLE IF EXISTS t_ab2;
@@ -106,6 +167,8 @@ INSERT INTO t_ab1 VALUES (0, 1, 1), (1, 2, 2);
 INSERT INTO t_ab2 VALUES (2, -1, 1), (3, 1, NULL), (4, 1, 257), (5, 1, -1), (6, 1, 1);
 
 SELECT '--- hash ---';
+
+SET join_algorithm = 'hash';
 
 SELECT '- full -';
 SELECT a, b FROM t_ab1 FULL JOIN t_ab2 USING (a, b) ORDER BY ifNull(t_ab1.id, t_ab2.id);
