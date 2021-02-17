be31873722cb62920e3ed7e801e778670f39fe9c
Maksim Kita <maksim-kita@yandex-team.ru>
2021-02-17 19:13:38 +0300

    Fixed style check

--- src/Processors/Formats/Impl/ValuesBlockInputFormat.cpp
+++ src/Processors/Formats/Impl/ValuesBlockInputFormat.cpp
@@ -28,7 +28,6 @@ namespace ErrorCodes
     extern const int SUPPORT_IS_DISABLED;
     extern const int ARGUMENT_OUT_OF_BOUND;
     extern const int CANNOT_READ_ALL_DATA;
-    extern const int UNSUPPORTED_METHOD;
 }
 
 
