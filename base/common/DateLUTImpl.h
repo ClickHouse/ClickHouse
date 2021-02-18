b8cc0fd449cdc3db831fb229204bfa1927d59f15
robot-clickhouse <robot-clickhouse@yandex-team.ru>
2021-02-19 00:34:07 +0300

    Backport #18565 to 20.8: Make Date larger then 2106-02-07 overflow as 0(1970-01-01)

--- base/common/DateLUTImpl.h
+++ base/common/DateLUTImpl.h
@@ -673,6 +673,10 @@ public:
         if (unlikely(year < DATE_LUT_MIN_YEAR || year > DATE_LUT_MAX_YEAR || month < 1 || month > 12 || day_of_month < 1 || day_of_month > 31))
             return DayNum(0); // TODO (nemkov, DateTime64 phase 2): implement creating real date for year outside of LUT range.
 
+        // The day after 2106-02-07 will not stored fully as struct Values, so just overflow it as 0
+        if (unlikely(year == DATE_LUT_MAX_YEAR && (month > 2 || (month == 2 && day_of_month > 7))))
+            return DayNum(0);
+
         return DayNum(years_months_lut[(year - DATE_LUT_MIN_YEAR) * 12 + month - 1] + day_of_month - 1);
     }
 
