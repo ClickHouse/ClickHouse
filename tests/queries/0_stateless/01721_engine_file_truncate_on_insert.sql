6db55fa0fc37f460a3e02151520a282a968b5d7c
M0r64n <M0r64n1635@gmail.com>
2021-02-18 11:36:17 +0400

    Correct file engine settings tests

--- tests/queries/0_stateless/01721_engine_file_truncate_on_insert.sql
+++ tests/queries/0_stateless/01721_engine_file_truncate_on_insert.sql
@@ -1,20 +1,21 @@
-INSERT INTO TABLE FUNCTION file('01718_file/test/data.TSV', 'TSV', 'id UInt32') VALUES ('file', 42);
+DROP TABLE IF EXISTS test;
+
+INSERT INTO TABLE FUNCTION file('01718_file/test/data.TSV', 'TSV', 'id UInt32') VALUES (1);
 ATTACH TABLE test FROM '01718_file/test' (id UInt8) ENGINE=File(TSV);
 
-CREATE TABLE file_engine_table (id UInt32) ENGINE=File(TabSeparated);
-
-INSERT INTO file_engine_table VALUES (1), (2), (3);
-INSERT INTO file_engine_table VALUES (4);
-SELECT * FROM file_engine_table;
+INSERT INTO test VALUES (2), (3);
+INSERT INTO test VALUES (4);
+SELECT * FROM test;
 
 SET engine_file_truncate_on_insert=0;
 
-INSERT INTO file_engine_table VALUES (5), (6);
-SELECT * FROM file_engine_table;
+INSERT INTO test VALUES (5), (6);
+SELECT * FROM test;
 
 SET engine_file_truncate_on_insert=1;
 
-INSERT INTO file_engine_table VALUES (0), (1), (2);
-SELECT * FROM file_engine_table;
+INSERT INTO test VALUES (0), (1), (2);
+SELECT * FROM test;
 
 SET engine_file_truncate_on_insert=0;
+DROP TABLE test;
