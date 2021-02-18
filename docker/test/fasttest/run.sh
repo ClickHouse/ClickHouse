97f4c457ec979fc489892472dfb50a93062b4ce5
Alexander Tokmakov <avtokmakov@yandex-team.ru>
2021-02-18 16:27:51 +0300

    fix MySQL COMM_FIELD_LIST response

--- docker/test/fasttest/run.sh
+++ docker/test/fasttest/run.sh
@@ -259,6 +259,7 @@ function run_tests
         00929_multi_match_edit_distance
         01681_hyperscan_debug_assertion
 
+        01176_mysql_client_interactive          # requires mysql client
         01031_mutations_interpreter_and_context
         01053_ssd_dictionary # this test mistakenly requires acces to /var/lib/clickhouse -- can't run this locally, disabled
         01083_expressions_in_engine_arguments
