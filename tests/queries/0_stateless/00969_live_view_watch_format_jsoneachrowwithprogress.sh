6607b74460ce0f9f503998c2791d3e11a3891afc
Denis Glazachev <d.glazachev@gmail.com>
2021-02-19 23:30:59 +0400

    Pin progress to bottom of output in 00969_live_view_watch_format_jsoneachrowwithprogress

--- tests/queries/0_stateless/00969_live_view_watch_format_jsoneachrowwithprogress.sh
+++ tests/queries/0_stateless/00969_live_view_watch_format_jsoneachrowwithprogress.sh
@@ -8,10 +8,16 @@ $CLICKHOUSE_CLIENT --query="DROP TABLE IF EXISTS lv"
 $CLICKHOUSE_CLIENT --query="DROP TABLE IF EXISTS mt"
 $CLICKHOUSE_CLIENT --query="CREATE TABLE mt (a Int32) Engine=MergeTree order by tuple()"
 $CLICKHOUSE_CLIENT --allow_experimental_live_view 1 --query="CREATE LIVE VIEW lv AS SELECT sum(a) FROM mt"
-$CLICKHOUSE_CLIENT --allow_experimental_live_view 1 --query="WATCH lv LIMIT 0 FORMAT JSONEachRowWithProgress" | sed -r -e 's/"cumulative_processing_time":"[0-9]+"/"cumulative_processing_time":"<CUMULATIVE-PROCESSING-TIME>"/'
+$CLICKHOUSE_CLIENT --allow_experimental_live_view 1 --query="WATCH lv LIMIT 0 FORMAT JSONEachRowWithProgress" \
+    | sed -r -e 's/"cumulative_processing_time":"[0-9]+"/"cumulative_processing_time":"<CUMULATIVE-PROCESSING-TIME>"/' \
+    | awk '/"progress"/{ progresstail += $0 } /"row"/{ print $0 } END{ print $progresstail }'
 $CLICKHOUSE_CLIENT --query="INSERT INTO mt VALUES (1),(2),(3)"
-$CLICKHOUSE_CLIENT --allow_experimental_live_view 1  --query="WATCH lv LIMIT 0 FORMAT JSONEachRowWithProgress" | sed -r -e 's/"cumulative_processing_time":"[0-9]+"/"cumulative_processing_time":"<CUMULATIVE-PROCESSING-TIME>"/'
+$CLICKHOUSE_CLIENT --allow_experimental_live_view 1  --query="WATCH lv LIMIT 0 FORMAT JSONEachRowWithProgress" \
+    | sed -r -e 's/"cumulative_processing_time":"[0-9]+"/"cumulative_processing_time":"<CUMULATIVE-PROCESSING-TIME>"/' \
+    | awk '/"progress"/{ progresstail += $0 } /"row"/{ print $0 } END{ print $progresstail }'
 $CLICKHOUSE_CLIENT --query="INSERT INTO mt VALUES (4),(5),(6)"
-$CLICKHOUSE_CLIENT --allow_experimental_live_view 1  --query="WATCH lv LIMIT 0 FORMAT JSONEachRowWithProgress" | sed -r -e 's/"cumulative_processing_time":"[0-9]+"/"cumulative_processing_time":"<CUMULATIVE-PROCESSING-TIME>"/'
+$CLICKHOUSE_CLIENT --allow_experimental_live_view 1  --query="WATCH lv LIMIT 0 FORMAT JSONEachRowWithProgress" \
+    | sed -r -e 's/"cumulative_processing_time":"[0-9]+"/"cumulative_processing_time":"<CUMULATIVE-PROCESSING-TIME>"/' \
+    | awk '/"progress"/{ progresstail += $0 } /"row"/{ print $0 } END{ print $progresstail }'
 $CLICKHOUSE_CLIENT --query="DROP TABLE lv"
 $CLICKHOUSE_CLIENT --query="DROP TABLE mt"
