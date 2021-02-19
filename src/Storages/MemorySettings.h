0a511ac5f088801d0274c0906d05f520848dcfbf
Alexey Milovidov <milovidov@yandex-team.ru>
2021-02-19 12:02:18 +0300

    Disable in-memory compression by default

--- src/Storages/MemorySettings.h
+++ src/Storages/MemorySettings.h
@@ -9,7 +9,7 @@ class ASTStorage;
 
 
 #define MEMORY_SETTINGS(M) \
-    M(Bool, compress, true, "Compress data in memory", 0) \
+    M(Bool, compress, false, "Compress data in memory", 0) \
 
 DECLARE_SETTINGS_TRAITS(memorySettingsTraits, MEMORY_SETTINGS)
 
