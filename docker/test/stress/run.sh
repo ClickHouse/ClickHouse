a1dcd77141991abf52d1a134f5a5f2577b614309
tavplubix <avtokmakov@yandex-team.ru>
2021-02-19 22:39:42 +0300

    Update run.sh

--- docker/test/stress/run.sh
+++ docker/test/stress/run.sh
@@ -114,4 +114,4 @@ mv /var/log/clickhouse-server/stderr.log /test_output/
 
 # Write check result into check_status.tsv
 clickhouse-local --structure "test String, res String" -q "SELECT 'failure', test FROM table WHERE res != 'OK'  LIMIT 1" < /test_output/test_results.tsv > /test_output/check_status.tsv
-[ -s /test_output/check_status.tsv ] || echo -e "success\tNo errors found"
+[ -s /test_output/check_status.tsv ] || echo -e "success\tNo errors found" > /test_output/check_status.tsv
