4320782032386627c2a27b7df55a060480d1cf5c
Pavel Kruglov <avogar@sandbox-633380738>
2021-02-21 20:43:36 +0300

    Style

--- src/Client/ConnectionEstablisher.cpp
+++ src/Client/ConnectionEstablisher.cpp
@@ -16,7 +16,6 @@ namespace ErrorCodes
     extern const int ATTEMPT_TO_READ_AFTER_EOF;
     extern const int NETWORK_ERROR;
     extern const int SOCKET_TIMEOUT;
-    extern const int LOGICAL_ERROR;
 }
 
 ConnectionEstablisher::ConnectionEstablisher(
