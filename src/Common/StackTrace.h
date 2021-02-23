2ced21c837006053542b9f04c338faebf504f413
Azat Khuzhin <a3at.mail@gmail.com>
2021-02-23 07:52:01 +0300

    Decrease number of stored stack frame pointers to fit into 512 PIPE_BUF

--- src/Common/StackTrace.h
+++ src/Common/StackTrace.h
@@ -37,8 +37,12 @@ public:
 
     static constexpr size_t capacity =
 #ifndef NDEBUG
-        /* The stacks are normally larger in debug version due to less inlining. */
-        64
+        /* The stacks are normally larger in debug version due to less inlining.
+         *
+         * NOTE: it cannot be larger then 56 right now, since otherwise it will
+         * not fit into minimal PIPE_BUF (512) in TraceCollector.
+         */
+        56
 #else
         32
 #endif
