d35171b864130a9124dd96cef030b315fa4d3b33
Nikita Mikhailov <jakalletti@jakalletti-build.sas.yp-c.yandex.net>
2021-02-24 16:14:17 +0300

    perimeter test

--- tests/queries/0_stateless/01720_country_perimeter_and_area.sh
+++ tests/queries/0_stateless/01720_country_perimeter_and_area.sh
@@ -7,10 +7,19 @@ CURDIR=$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)
 ${CLICKHOUSE_CLIENT} -q "drop table if exists country_polygons;"
 ${CLICKHOUSE_CLIENT} -q "create table country_polygons(name String, p Array(Array(Tuple(Float64, Float64)))) engine=MergeTree() order by tuple();"
 cat ${CURDIR}/country_polygons.tsv | ${CLICKHOUSE_CLIENT} -q "insert into country_polygons format TSV"
-${CLICKHOUSE_CLIENT} -q "SELECT name, polygonAreaGeographic(p) from country_polygons"
 
+${CLICKHOUSE_CLIENT} -q "SELECT name, polygonPerimeterGeographic(p) from country_polygons"
+${CLICKHOUSE_CLIENT} -q "SELECT '-------------------------------------'"
+${CLICKHOUSE_CLIENT} -q "SELECT name, polygonAreaGeographic(p) from country_polygons"
+${CLICKHOUSE_CLIENT} -q "SELECT '-------------------------------------'"
 ${CLICKHOUSE_CLIENT} -q "drop table if exists country_rings;"
+
+
 ${CLICKHOUSE_CLIENT} -q "create table country_rings(name String, p Array(Tuple(Float64, Float64))) engine=MergeTree() order by tuple();"
 cat ${CURDIR}/country_rings.tsv | ${CLICKHOUSE_CLIENT} -q "insert into country_rings format TSV"
+
+${CLICKHOUSE_CLIENT} -q "SELECT name, polygonPerimeterGeographic(p) from country_rings"
+${CLICKHOUSE_CLIENT} -q "SELECT '-------------------------------------'"
 ${CLICKHOUSE_CLIENT} -q "SELECT name, polygonAreaGeographic(p) from country_rings"
+${CLICKHOUSE_CLIENT} -q "SELECT '-------------------------------------'"
 ${CLICKHOUSE_CLIENT} -q "drop table if exists country_rings;"
