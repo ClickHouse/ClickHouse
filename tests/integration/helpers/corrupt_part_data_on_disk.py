f088dd445dd37729a36f2a567e788560c4cf44e1
Mike Kot <to@myrrc.dev>
2021-03-01 18:12:02 +0300

    Extended the test to check both the ALTER PARTITION and PART
    Added some notes about the SYSTEM SYNC REPLICA and ALTER ... DROP /
    ATTACH.

--- tests/integration/helpers/corrupt_part_data_on_disk.py
+++ tests/integration/helpers/corrupt_part_data_on_disk.py
@@ -1,7 +1,10 @@
-def corrupt_part_data_on_disk(node, table, part_name, is_detached=False):
-    parts_table = "system.detached_parts" is is_detached else "system.parts"
-    part_path = node.query(
-        "SELECT path FROM " + parts_table + " WHERE table = '{}' and name = '{}'".format(table, part_name)).strip()
+def corrupt_part_data_on_disk(node, table, part_name):
+    part_path = node.query("SELECT path FROM system.parts WHERE table = '{}' and name = '{}'"
+        .format(table, part_name)).strip()
+
+    corrupt_part_data_by_path(node, part_path)
+
+def corrupt_part_data_by_path(node, part_path):
     node.exec_in_container(['bash', '-c',
                             'cd {p} && ls *.bin | head -n 1 | xargs -I{{}} sh -c \'echo "1" >> $1\' -- {{}}'.format(
                                 p=part_path)], privileged=True)
--- tests/integration/test_attach_without_fetching/test.py
+++ tests/integration/test_attach_without_fetching/test.py
@@ -2,10 +2,9 @@ import time
 import pytest
 
 from helpers.cluster import ClickHouseCluster
-from helpers.cluster import ClickHouseKiller
 from helpers.test_tools import assert_eq_with_retry
 from helpers.network import PartitionManager
-from helpers.corrupt_part_data_on_disk import corrupt_part_data_on_disk
+from helpers.corrupt_part_data_on_disk import corrupt_part_data_by_path
 
 def fill_node(node):
     node.query(
@@ -39,60 +38,69 @@ def start_cluster():
 
 def check_data(nodes, detached_parts):
     for node in nodes:
+        node.query("SYSTEM SYNC REPLICA test")
+
+        print("Checking data integrity for", node.name, "; parts missing:", detached_parts)
+
         for i in range(10):
-            assert node.query("SELECT count() FROM test WHERE key % 10 == " + str(i)) ==
+            assert node.query("SELECT count() FROM test WHERE n % 10 == " + str(i)) == \
                 "0\n" if i in detached_parts else "10\n"
 
-        assert node.query("SELECT count() FROM system.parts WHERE table='test'") ==
+        print(node.query("SELECT partition_id, name FROM system.parts WHERE table='test'"))
+
+        assert node.query("SELECT count() FROM system.parts WHERE table='test'") == \
             str(10 - len(detached_parts)) + "\n"
 
-        assert node.query("SELECT count() FROM system.detached_parts WHERE table='test'") ==
-            str(len(detached_parts)) + "\n"
+        # We don't check for system.detached_parts count = len(detached parts) as the newly downloaded data
+        # is not removed from detached_parts (so this is wrong)
 
-# 1. Check that ALTER TABLE ATTACH PARTITION does not fetch data from other replicas if it's present in the
+        res: str = node.query("SELECT * FROM test ORDER BY n")
+
+        for other in nodes:
+            if other != node:
+                assert_eq_with_retry(other, "SELECT * FROM test ORDER BY n", res)
+
+
+# 1. Check that ALTER TABLE ATTACH PART|PARTITION does not fetch data from other replicas if it's present in the
 # detached/ folder.
-# 2. Check that ALTER TABLE ATTACH PARTITION downloads the data from other replicas if the detached/ folder
+# 2. Check that ALTER TABLE ATTACH PART|PARTITION downloads the data from other replicas if the detached/ folder
 # does not contain the part with the correct checksums.
 def test_attach_without_fetching(start_cluster):
+    # Note here requests are used for both PARTITION and PART. This is done for better test diversity.
+    # The partition and part are used interchangeably which is not true in most cases.
     # 0. Insert data on two replicas
     node_1.query("INSERT INTO test SELECT * FROM numbers(100)")
 
     check_data([node_1, node_2], detached_parts=[])
 
-    # 1. Detach the first three partition on the replicas
+    path_to_detached_part_1 = node_1.query("SELECT path from system.parts WHERE table='test' AND name='1_0_0_0'") \
+        .strip()
+    path_to_detached_part_1 =  path_to_detached_part_1.split('/')[:-1]
+    path_to_detached_part_1 = path_to_detached_part_1[:-1] + ["detached", path_to_detached_part_1[-1]]
+    path_to_detached_part_1 = "/".join(path_to_detached_part_1)
 
-    # This part will be fetched from other replicas as it would be missing in the detached/ folder
-    node_1.query("ALTER TABLE test DETACH PARTITION '0_0_0_0'")
-    # This part will be fetched from other replicas as the checksums won't match (we'll manually break the data).
-    node_1.query("ALTER TABLE test DETACH PARTITION '1_0_0_0'")
-    # This part will be copied locally and attached without fetch
-    node_1.query("ALTER TABLE test DETACH PARTITION '2_0_0_0'")
+    # 1. Detach the first two partitions/parts on the replicas
+    # This part will be fetched from other replicas as it would be missing in the detached/ folder and
+    # also attached locally.
+    node_1.query("ALTER TABLE test DETACH PART '0_0_0_0'")
+    # This partition will be just fetched from other replicas as the checksums won't match
+    # (we'll manually break the data).
+    node_1.query("ALTER TABLE test DETACH PARTITION 1")
 
-    check_data([node_1, node_2], detached_parts=[0, 1, 2])
+    check_data([node_1, node_2], detached_parts=[0, 1])
 
     # 2. Create the third replica
     fill_node(node_3)
 
-    # 3. Attach the first partition and check if it has been fetched correctly
-    node_3.query("ALTER TABLE test ATTACH PARTITION '0_0_0_0'")
-    check_data([node_1, node_2, node_3], detached_parts=[1, 2])
+    # 3. Attach the first part and check if it has been fetched correctly.
+    # Replica 3 should download the data from replica 2 as there is no local data.
+    # Replica 1 should attach the local data from detached/.
+    node_2.query("ALTER TABLE test ATTACH PART '0_0_0_0'")
+    check_data([node_1, node_2, node_3], detached_parts=[1])
 
-    # 4. Fetch the second partition to the third replica, break the data to corrupt the checksums,
-    # attach it and check if it also was fetched correctly.
-    node_3.query("ALTER TABLE test FETCH PARTITION '1_0_0_0' FROM '/clickhouse/tables/test'")
-    corrupt_part_data_on_disk(node_3, 'test', '1_0_0_0', is_detached=True)
-    node_3.query("ALTER TABLE test ATTACH PARTITION '1_0_0_0'")
-
-    check_data([node_1, node_2, node_3], detached_parts=[2])
-
-    # 5. Fetch the third partition to the third replica, break the network as so the replica won't be able to
-    # download the data, attach the partition (and check it has been attached from the local data)
-    node_3.query("ALTER TABLE test FETCH PARTITION '2_0_0_0' FROM '/clickhouse/tables/test'")
-
-    with PartitionManager() as pm:
-        pm.partition_instances(node_1, node_3)
-        pm.partition_instances(node_2, node_3)
-
-        node_3.query("ALTER TABLE test ATTACH PARTITION '2_0_0_0'")
-
+    # 4. Break the part data on the second node to corrupt the checksums.
+    # Replica 3 should download the data from replica 1 as there is no local data.
+    # Replica 2 should also download the data from 1 as the checksums won't match.
+    corrupt_part_data_by_path(node_2, path_to_detached_part_1)
+    node_1.query("ALTER TABLE test ATTACH PARTITION 1")
     check_data([node_1, node_2, node_3], detached_parts=[])
