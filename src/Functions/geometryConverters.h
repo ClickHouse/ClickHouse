9f3e0874e5d5e82189462d42f062770b14c26c50
Nikita Mikhailov <jakalletti@jakalletti-build.sas.yp-c.yandex.net>
2021-03-02 00:55:20 +0300

    mercator

--- src/Functions/geometryConverters.h
+++ src/Functions/geometryConverters.h
@@ -43,6 +43,7 @@ using CartesianRing = Ring<CartesianPoint>;
 using CartesianPolygon = Polygon<CartesianPoint>;
 using CartesianMultiPolygon = MultiPolygon<CartesianPoint>;
 
+/// Latitude, longitude
 using SphericalPoint = boost::geometry::model::point<Float64, 2, boost::geometry::cs::spherical_equatorial<boost::geometry::degree>>;
 using SphericalRing = Ring<SphericalPoint>;
 using SphericalPolygon = Polygon<SphericalPoint>;
