9411d496d9957419b0a6cd2928ba1447d32eb5d3
Amos Bird <amosbird@gmail.com>
2021-03-02 23:17:02 +0800

    Fix build

--- src/Functions/connectionID.cpp
+++ src/Functions/connectionID.cpp
@@ -6,34 +6,31 @@
 
 namespace DB
 {
-namespace
+
+/// Get the connection ID. It's used for MySQL handler only.
+class FunctionConnectionID : public IFunction
 {
-    /// Get the connection ID. It's used for MySQL handler only.
-    class FunctionConnectionID : public IFunction
+public:
+    static constexpr auto name = "connectionID";
+
+    explicit FunctionConnectionID(const Context & context_) : context(context_) {}
+
+    static FunctionPtr create(const Context & context) { return std::make_shared<FunctionConnectionID>(context); }
+
+    String getName() const override { return name; }
+
+    size_t getNumberOfArguments() const override { return 0; }
+
+    DataTypePtr getReturnTypeImpl(const DataTypes & /*arguments*/) const override { return std::make_shared<DataTypeUInt64>(); }
+
+    ColumnPtr executeImpl(const ColumnsWithTypeAndName &, const DataTypePtr & result_type, size_t input_rows_count) const override
     {
-    public:
-        static constexpr auto name = "connectionID";
+        return result_type->createColumnConst(input_rows_count, context.getClientInfo().connection_id);
+    }
 
-        FunctionConnectionID(const Context & context_) : context(context_) {}
-
-        static FunctionPtr create(const Context & context) { return std::make_shared<FunctionConnectionID>(context); }
-
-        String getName() const override { return name; }
-
-        size_t getNumberOfArguments() const override { return 0; }
-
-        DataTypePtr getReturnTypeImpl(const DataTypes & /*arguments*/) const override { return std::make_shared<DataTypeUInt64>(); }
-
-        ColumnPtr executeImpl(const ColumnsWithTypeAndName &, const DataTypePtr & result_type, size_t input_rows_count) const override
-        {
-            return result_type->createColumnConst(input_rows_count, context.getClientInfo().connection_id);
-        }
-
-    private:
-        const Context & context;
-    };
-
-}
+private:
+    const Context & context;
+};
 
 void registerFunctionConnectionID(FunctionFactory & factory)
 {
