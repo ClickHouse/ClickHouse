5296dcbafda4bba7d1b5bec0d350ec4a1200b80d
Anton Popov <pad11rus@gmail.com>
2021-03-02 17:13:27 +0300

    remove commented code

--- src/DataTypes/IDataType.cpp
+++ src/DataTypes/IDataType.cpp
@@ -27,44 +27,6 @@ namespace ErrorCodes
     extern const int ILLEGAL_COLUMN;
 }
 
-// String ISerialization::Substream::toString() const
-// {
-//     switch (type)
-//     {
-//         case ArrayElements:
-//             return "ArrayElements";
-//         case ArraySizes:
-//             return "ArraySizes";
-//         case NullableElements:
-//             return "NullableElements";
-//         case NullMap:
-//             return "NullMap";
-//         case TupleElement:
-//             return "TupleElement(" + tuple_element_name + ", "
-//                 + std::to_string(escape_tuple_delimiter) + ")";
-//         case DictionaryKeys:
-//             return "DictionaryKeys";
-//         case DictionaryIndexes:
-//             return "DictionaryIndexes";
-//     }
-
-//     __builtin_unreachable();
-// }
-
-// String ISerialization::SubstreamPath::toString() const
-// {
-//     WriteBufferFromOwnString wb;
-//     wb << "{";
-//     for (size_t i = 0; i < size(); ++i)
-//     {
-//         if (i != 0)
-//             wb << ", ";
-//         wb << at(i).toString();
-//     }
-//     wb << "}";
-//     return wb.str();
-// }
-
 IDataType::IDataType() : custom_name(nullptr), custom_text_serialization(nullptr), custom_streams(nullptr)
 {
 }
@@ -121,16 +83,6 @@ DataTypePtr IDataType::promoteNumericType() const
 {
     throw Exception("Data type " + getName() + " can't be promoted.", ErrorCodes::DATA_TYPE_CANNOT_BE_PROMOTED);
 }
-
-// void IDataType::serializeBinaryBulk(const IColumn &, WriteBuffer &, size_t, size_t) const
-// {
-//     throw Exception("Data type " + getName() + " must be serialized with multiple streams", ErrorCodes::MULTIPLE_STREAMS_REQUIRED);
-// }
-
-// void IDataType::deserializeBinaryBulk(IColumn &, ReadBuffer &, size_t, double) const
-// {
-//     throw Exception("Data type " + getName() + " must be deserialized with multiple streams", ErrorCodes::MULTIPLE_STREAMS_REQUIRED);
-// }
 
 size_t IDataType::getSizeOfValueInMemory() const
 {
@@ -238,190 +190,6 @@ void IDataType::insertDefaultInto(IColumn & column) const
     column.insertDefault();
 }
 
-// void IDataType::enumerateStreams(const StreamCallback & callback, SubstreamPath & path) const
-// {
-//     if (custom_streams)
-//         custom_streams->enumerateStreams(callback, path);
-//     else
-//         enumerateStreamsImpl(callback, path);
-// }
-
-// void IDataType::serializeBinaryBulkStatePrefix(
-//     SerializeBinaryBulkSettings & settings,
-//     SerializeBinaryBulkStatePtr & state) const
-// {
-//     if (custom_streams)
-//         custom_streams->serializeBinaryBulkStatePrefix(settings, state);
-//     else
-//         serializeBinaryBulkStatePrefixImpl(settings, state);
-// }
-
-// void IDataType::serializeBinaryBulkStateSuffix(
-//     SerializeBinaryBulkSettings & settings,
-//     SerializeBinaryBulkStatePtr & state) const
-// {
-//     if (custom_streams)
-//         custom_streams->serializeBinaryBulkStateSuffix(settings, state);
-//     else
-//         serializeBinaryBulkStateSuffixImpl(settings, state);
-// }
-
-// void IDataType::deserializeBinaryBulkStatePrefix(
-//     DeserializeBinaryBulkSettings & settings,
-//     DeserializeBinaryBulkStatePtr & state) const
-// {
-//     if (custom_streams)
-//         custom_streams->deserializeBinaryBulkStatePrefix(settings, state);
-//     else
-//         deserializeBinaryBulkStatePrefixImpl(settings, state);
-// }
-
-// void IDataType::serializeBinaryBulkWithMultipleStreams(
-//     const IColumn & column,
-//     size_t offset,
-//     size_t limit,
-//     SerializeBinaryBulkSettings & settings,
-//     SerializeBinaryBulkStatePtr & state) const
-// {
-//     if (custom_streams)
-//         custom_streams->serializeBinaryBulkWithMultipleStreams(column, offset, limit, settings, state);
-//     else
-//         serializeBinaryBulkWithMultipleStreamsImpl(column, offset, limit, settings, state);
-// }
-
-// void IDataType::deserializeBinaryBulkWithMultipleStreamsImpl(
-//     IColumn & column,
-//     size_t limit,
-//     DeserializeBinaryBulkSettings & settings,
-//     DeserializeBinaryBulkStatePtr & /* state */,
-//     SubstreamsCache * /* cache */) const
-// {
-//     if (ReadBuffer * stream = settings.getter(settings.path))
-//         deserializeBinaryBulk(column, *stream, limit, settings.avg_value_size_hint);
-// }
-
-
-// void IDataType::deserializeBinaryBulkWithMultipleStreams(
-//     ColumnPtr & column,
-//     size_t limit,
-//     DeserializeBinaryBulkSettings & settings,
-//     DeserializeBinaryBulkStatePtr & state,
-//     SubstreamsCache * cache) const
-// {
-//     if (custom_streams)
-//     {
-//         custom_streams->deserializeBinaryBulkWithMultipleStreams(column, limit, settings, state, cache);
-//         return;
-//     }
-
-//     /// Do not cache complex type, because they can be constructed
-//     /// from their subcolumns, which are in cache.
-//     if (!haveSubtypes())
-//     {
-//         auto cached_column = getFromSubstreamsCache(cache, settings.path);
-//         if (cached_column)
-//         {
-//             column = cached_column;
-//             return;
-//         }
-//     }
-
-//     auto mutable_column = column->assumeMutable();
-//     deserializeBinaryBulkWithMultipleStreamsImpl(*mutable_column, limit, settings, state, cache);
-//     column = std::move(mutable_column);
-
-//     if (!haveSubtypes())
-//         addToSubstreamsCache(cache, settings.path, column);
-// }
-
-// void IDataType::serializeAsTextEscaped(const IColumn & column, size_t row_num, WriteBuffer & ostr, const FormatSettings & settings) const
-// {
-//     if (custom_text_serialization)
-//         custom_text_serialization->serializeTextEscaped(column, row_num, ostr, settings);
-//     else
-//         serializeTextEscaped(column, row_num, ostr, settings);
-// }
-
-// void IDataType::deserializeAsTextEscaped(IColumn & column, ReadBuffer & istr, const FormatSettings & settings) const
-// {
-//     if (custom_text_serialization)
-//         custom_text_serialization->deserializeTextEscaped(column, istr, settings);
-//     else
-//         deserializeTextEscaped(column, istr, settings);
-// }
-
-// void IDataType::serializeAsTextQuoted(const IColumn & column, size_t row_num, WriteBuffer & ostr, const FormatSettings & settings) const
-// {
-//     if (custom_text_serialization)
-//         custom_text_serialization->serializeTextQuoted(column, row_num, ostr, settings);
-//     else
-//         serializeTextQuoted(column, row_num, ostr, settings);
-// }
-
-// void IDataType::deserializeAsTextQuoted(IColumn & column, ReadBuffer & istr, const FormatSettings & settings) const
-// {
-//     if (custom_text_serialization)
-//         custom_text_serialization->deserializeTextQuoted(column, istr, settings);
-//     else
-//         deserializeTextQuoted(column, istr, settings);
-// }
-
-// void IDataType::serializeAsTextCSV(const IColumn & column, size_t row_num, WriteBuffer & ostr, const FormatSettings & settings) const
-// {
-//     if (custom_text_serialization)
-//         custom_text_serialization->serializeTextCSV(column, row_num, ostr, settings);
-//     else
-//         serializeTextCSV(column, row_num, ostr, settings);
-// }
-
-// void IDataType::deserializeAsTextCSV(IColumn & column, ReadBuffer & istr, const FormatSettings & settings) const
-// {
-//     if (custom_text_serialization)
-//         custom_text_serialization->deserializeTextCSV(column, istr, settings);
-//     else
-//         deserializeTextCSV(column, istr, settings);
-// }
-
-// void IDataType::serializeAsText(const IColumn & column, size_t row_num, WriteBuffer & ostr, const FormatSettings & settings) const
-// {
-//     if (custom_text_serialization)
-//         custom_text_serialization->serializeText(column, row_num, ostr, settings);
-//     else
-//         serializeText(column, row_num, ostr, settings);
-// }
-
-// void IDataType::deserializeAsWholeText(IColumn & column, ReadBuffer & istr, const FormatSettings & settings) const
-// {
-//     if (custom_text_serialization)
-//         custom_text_serialization->deserializeWholeText(column, istr, settings);
-//     else
-//         deserializeWholeText(column, istr, settings);
-// }
-
-// void IDataType::serializeAsTextJSON(const IColumn & column, size_t row_num, WriteBuffer & ostr, const FormatSettings & settings) const
-// {
-//     if (custom_text_serialization)
-//         custom_text_serialization->serializeTextJSON(column, row_num, ostr, settings);
-//     else
-//         serializeTextJSON(column, row_num, ostr, settings);
-// }
-
-// void IDataType::deserializeAsTextJSON(IColumn & column, ReadBuffer & istr, const FormatSettings & settings) const
-// {
-//     if (custom_text_serialization)
-//         custom_text_serialization->deserializeTextJSON(column, istr, settings);
-//     else
-//         deserializeTextJSON(column, istr, settings);
-// }
-
-// void IDataType::serializeAsTextXML(const IColumn & column, size_t row_num, WriteBuffer & ostr, const FormatSettings & settings) const
-// {
-//     if (custom_text_serialization)
-//         custom_text_serialization->serializeTextXML(column, row_num, ostr, settings);
-//     else
-//         serializeTextXML(column, row_num, ostr, settings);
-// }
-
 void IDataType::setCustomization(DataTypeCustomDescPtr custom_desc_) const
 {
     /// replace only if not null
@@ -437,24 +205,6 @@ void IDataType::setCustomization(DataTypeCustomDescPtr custom_desc_) const
     if (custom_desc_->serialization)
         custom_serialization = std::move(custom_desc_->serialization);
 }
-
-// void IDataType::addToSubstreamsCache(SubstreamsCache * cache, const SubstreamPath & path, ColumnPtr column)
-// {
-//     if (cache && !path.empty())
-//         cache->emplace(getSubcolumnNameForStream(path), column);
-// }
-
-// ColumnPtr IDataType::getFromSubstreamsCache(SubstreamsCache * cache, const SubstreamPath & path)
-// {
-//     if (!cache || path.empty())
-//         return nullptr;
-
-//     auto it = cache->find(getSubcolumnNameForStream(path));
-//     if (it == cache->end())
-//         return nullptr;
-
-//     return it->second;
-// }
 
 SerializationPtr IDataType::getDefaultSerialization() const
 {
@@ -505,9 +255,6 @@ SerializationPtr IDataType::getSerialization(const IColumn & column) const
 
 SerializationPtr IDataType::getSerialization(const ISerialization::Settings & settings) const
 {
-    // std::cerr << "type: " << getName() << ", number_of_rows: " << settings.num_rows
-    //     << "non-default: " << settings.num_non_default_rows << ", ratio: " << settings.min_ratio_for_dense_serialization; 
-
     // if (settings.num_non_default_rows * settings.min_ratio_for_dense_serialization < settings.num_rows)
     //     return getSparseSerialization();
 
