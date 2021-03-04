e7df5fd3154bcae75eb02698e11703dcd2e78a38
alesapin <alesapin@gmail.com>
2021-03-04 11:00:26 +0300

    Enable snapshots

--- src/Coordination/tests/gtest_for_build.cpp
+++ src/Coordination/tests/gtest_for_build.cpp
@@ -898,7 +898,10 @@ TEST(CoordinationTest, SnapshotableHashMapTrySnapshot)
 void addNode(DB::NuKeeperStorage & storage, const std::string & path, const std::string & data, int64_t ephemeral_owner=0)
 {
     using Node = DB::NuKeeperStorage::Node;
-    storage.container.insertOrReplace(path, Node{.data=data, .stat.ephemeralOwner = ephemeral_owner});
+    Node node{};
+    node.data = data;
+    node.stat.ephemeralOwner = ephemeral_owner;
+    storage.container.insertOrReplace(path, node);
 }
 
 TEST(CoordinationTest, TestStorageSnapshotSimple)
