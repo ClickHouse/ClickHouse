5c5c76e9d50af7deed0a2e37d37584bf8a485ecf
Alexey Milovidov <milovidov@yandex-team.ru>
2021-03-08 13:22:17 +0300

    Remove useless files

--- contrib/FastMemcpy/memcpy_wrapper.cpp
+++
@@ -1,6 +0,0 @@
-#include "FastMemcpy.h"
-
-extern "C" void * memcpy(void * __restrict destination, const void * __restrict source, size_t size)
-{
-    return memcpy_fast(destination, source, size);
-}
