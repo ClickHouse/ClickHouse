b83564bdadf0b092e1aa517d4e1fb3f1633ced8a
Pavel Kruglov <avogar@sandbox-633380738>
2021-03-12 15:08:00 +0300

    Add test for path as a query parameter in system.zookeeper

---
+++ tests/queries/0_stateless/01753_system_zookeeper_query_param_path.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+
+CUR_DIR=$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)
+# shellcheck source=../shell_config.sh
+. "$CUR_DIR"/../shell_config.sh
+
+
+${CLICKHOUSE_CLIENT} --query="SELECT name FROM system.zookeeper WHERE path = {path:String}" --param_path "/"
+
