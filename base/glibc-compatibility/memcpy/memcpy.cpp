a446612e8aea837f0aa49dc20a2d8c1af5d575c9
Alexey Milovidov <milovidov@yandex-team.ru>
2021-03-13 10:39:43 +0300

    Remove currently unused code

--- base/glibc-compatibility/memcpy/memcpy.cpp
+++ base/glibc-compatibility/memcpy/memcpy.cpp
@@ -1,22 +1,6 @@
 #include "memcpy.h"
 
-#include <cpuid.h>
-#include <cstdint>
-
-
-bool have_avx = false;
-
-void init_memcpy()
-{
-    uint32_t eax;
-    uint32_t ebx;
-    uint32_t ecx;
-    uint32_t edx;
-    __cpuid(1, eax, ebx, ecx, edx);
-    have_avx = (ecx >> 28) & 1;
-}
-
 extern "C" void * memcpy(void * __restrict dst, const void * __restrict src, size_t size)
 {
-    return inline_memcpy(reinterpret_cast<char *>(dst), reinterpret_cast<const char *>(src), size);
+    return inline_memcpy(dst, src, size);
 }
