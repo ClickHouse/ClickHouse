68b3cbbfe96e5deb068f3901aa526eb048814478
Anton Popov <pad11rus@gmail.com>
2021-03-13 17:31:50 +0300

    fix conversions for fields in function 'arrayElement' for type Map

--- tests/queries/0_stateless/01720_type_map_and_casts.sql
+++ tests/queries/0_stateless/01720_type_map_and_casts.sql
@@ -51,11 +51,13 @@ INSERT INTO table_map_with_key_integer VALUES ('2020-01-01', map('00001192-0000-
 SELECT 'Map(UUID, UInt16)';
 
 SELECT m FROM table_map_with_key_integer;
-SELECT 
-    m[toUUID('00001192-0000-4000-6000-000000000001')], 
-    m[toUUID('00001192-0000-4000-7000-000000000001')], 
+SELECT
+    m[toUUID('00001192-0000-4000-6000-000000000001')],
+    m[toUUID('00001192-0000-4000-7000-000000000001')],
     m[toUUID('00001192-0000-4000-8000-000000000001')]
 FROM table_map_with_key_integer;
+
+SELECT m[257], m[1] FROM table_map_with_key_integer; -- { serverError 43 }
 
 DROP TABLE IF EXISTS table_map_with_key_integer;
 
@@ -70,6 +72,9 @@ SELECT 'Map(Int128, Int32)';
 SELECT m FROM table_map_with_key_integer;
 SELECT m[toInt128(-1)], m[toInt128(0)], m[toInt128(1234567898765432123456789)], m[toInt128(-1234567898765432123456789)] FROM table_map_with_key_integer;
 SELECT m[toInt128(number - 2)] FROM table_map_with_key_integer ARRAY JOIN range(4) AS number;
+
+SELECT m[-1], m[0], m[toInt128(1234567898765432123456789)], m[toInt128(-1234567898765432123456789)] FROM table_map_with_key_integer;
+SELECT m[toUInt64(0)], m[toInt64(0)], m[toUInt8(0)], m[toUInt16(0)] FROM table_map_with_key_integer;
 
 DROP TABLE IF EXISTS table_map_with_key_integer;
 
