138c945dbf227fdda57664f09509f035faf92329
Pavel Kruglov <avogar@sandbox-633380738>
2021-03-14 02:36:40 +0300

    Fix style

--- tests/queries/0_stateless/01753_system_zookeeper_query_param_path.sh
+++ tests/queries/0_stateless/01753_system_zookeeper_query_param_path.sh
@@ -6,9 +6,9 @@ CUR_DIR=$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)
 
 
 ${CLICKHOUSE_CLIENT} --query="DROP TABLE IF EXISTS test_01753";
-${CLICKHOUSE_CLIENT} --query="CREATE TABLE test_01753 (n Int8) ENGINE=ReplicatedMergeTree('/"$CLICKHOUSE_DATABASE"/test_01753/test', '1') ORDER BY n"
+${CLICKHOUSE_CLIENT} --query="CREATE TABLE test_01753 (n Int8) ENGINE=ReplicatedMergeTree('/$CLICKHOUSE_DATABASE/test_01753/test', '1') ORDER BY n"
 
-${CLICKHOUSE_CLIENT} --query="SELECT name FROM system.zookeeper WHERE path = {path:String}" --param_path $CLICKHOUSE_DATABASE"/test_01753"
+${CLICKHOUSE_CLIENT} --query="SELECT name FROM system.zookeeper WHERE path = {path:String}" --param_path "$CLICKHOUSE_DATABASE/test_01753"
 
 
 ${CLICKHOUSE_CLIENT} --query="DROP TABLE test_01753 SYNC";
