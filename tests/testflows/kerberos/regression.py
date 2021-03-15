b972dbd08e731049218ad4d25342ff48ff1aeaa1
zvonand <azvonov@altinity.com>
2021-03-15 12:06:17 +0300

    Reduced number of tests to essential-only

--- tests/testflows/kerberos/regression.py
+++ tests/testflows/kerberos/regression.py
@@ -30,8 +30,8 @@ def regression(self, local, clickhouse_binary_path, stress=None, parallel=None):
         self.context.cluster = cluster
 
         Feature(run=load("kerberos.tests.generic", "generic"), flags=TE)
-        Feature(run=load("kerberos.tests.config", "config"), flags=TE)
-        Feature(run=load("kerberos.tests.parallel", "parallel"), flags=TE)
+        # Feature(run=load("kerberos.tests.config", "config"), flags=TE)
+        # Feature(run=load("kerberos.tests.parallel", "parallel"), flags=TE)
 
 
 if main():
