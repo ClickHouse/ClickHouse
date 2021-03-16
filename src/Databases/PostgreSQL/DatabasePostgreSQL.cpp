89a8624738c385b9df645f823c2113336b4d2acb
kssenii <sumarokovakseniia@mail.ru>
2021-03-16 10:20:31 +0000

    Add double quotes

--- src/Databases/PostgreSQL/DatabasePostgreSQL.cpp
+++ src/Databases/PostgreSQL/DatabasePostgreSQL.cpp
@@ -16,6 +16,7 @@
 #include <Poco/DirectoryIterator.h>
 #include <Poco/File.h>
 #include <Databases/PostgreSQL/fetchPostgreSQLTableStructure.h>
+#include <Common/quoteString.h>
 
 
 namespace DB
@@ -162,7 +163,7 @@ StoragePtr DatabasePostgreSQL::fetchTable(const String & table_name, const Conte
             return StoragePtr{};
 
         auto use_nulls = context.getSettingsRef().external_table_functions_use_nulls;
-        auto columns = fetchPostgreSQLTableStructure(connection->conn(), table_name, use_nulls);
+        auto columns = fetchPostgreSQLTableStructure(connection->conn(), doubleQuoteString(table_name), use_nulls);
 
         if (!columns)
             return StoragePtr{};
