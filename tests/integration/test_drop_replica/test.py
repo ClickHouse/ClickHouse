d7482373d36510cb26214542b14dcacf238b8737
Alexander Tokmakov <avtokmakov@yandex-team.ru>
2021-03-18 19:59:51 +0300

    fix

--- tests/integration/test_drop_replica/test.py
+++ tests/integration/test_drop_replica/test.py
@@ -101,52 +101,52 @@ def test_drop_replica(start_cluster):
     assert "does not look like a table path" in \
            node_1_3.query_and_get_error("SYSTEM DROP REPLICA 'node_1_1' FROM ZKPATH '/clickhouse/tables/test'")
 
-    with PartitionManager() as pm:
-        ## make node_1_1 dead
-        pm.drop_instance_zk_connections(node_1_1)
+    node_1_1.query("DETACH DATABASE test")
+    for i in range(1, 5):
+        node_1_1.query("DETACH DATABASE test{}".format(i))
 
-        assert "doesn't exist" in node_1_3.query_and_get_error(
-            "SYSTEM DROP REPLICA 'node_1_1' FROM TABLE test.test_table")
+    assert "doesn't exist" in node_1_3.query_and_get_error(
+        "SYSTEM DROP REPLICA 'node_1_1' FROM TABLE test.test_table")
 
-        assert "doesn't exist" in node_1_3.query_and_get_error("SYSTEM DROP REPLICA 'node_1_1' FROM DATABASE test1")
+    assert "doesn't exist" in node_1_3.query_and_get_error("SYSTEM DROP REPLICA 'node_1_1' FROM DATABASE test1")
 
-        node_1_3.query("SYSTEM DROP REPLICA 'node_1_1'")
-        exists_replica_1_1 = zk.exists(
-            "/clickhouse/tables/test3/{shard}/replicated/test_table/replicas/{replica}".format(shard=1,
-                                                                                               replica='node_1_1'))
-        assert (exists_replica_1_1 != None)
+    node_1_3.query("SYSTEM DROP REPLICA 'node_1_1'")
+    exists_replica_1_1 = zk.exists(
+        "/clickhouse/tables/test3/{shard}/replicated/test_table/replicas/{replica}".format(shard=1,
+                                                                                           replica='node_1_1'))
+    assert (exists_replica_1_1 != None)
 
-        ## If you want to drop a inactive/stale replicate table that does not have a local replica, you can following syntax(ZKPATH):
-        node_1_3.query(
-            "SYSTEM DROP REPLICA 'node_1_1' FROM ZKPATH '/clickhouse/tables/test2/{shard}/replicated/test_table'".format(
-                shard=1))
-        exists_replica_1_1 = zk.exists(
-            "/clickhouse/tables/test2/{shard}/replicated/test_table/replicas/{replica}".format(shard=1,
-                                                                                               replica='node_1_1'))
-        assert (exists_replica_1_1 == None)
+    ## If you want to drop a inactive/stale replicate table that does not have a local replica, you can following syntax(ZKPATH):
+    node_1_3.query(
+        "SYSTEM DROP REPLICA 'node_1_1' FROM ZKPATH '/clickhouse/tables/test2/{shard}/replicated/test_table'".format(
+            shard=1))
+    exists_replica_1_1 = zk.exists(
+        "/clickhouse/tables/test2/{shard}/replicated/test_table/replicas/{replica}".format(shard=1,
+                                                                                           replica='node_1_1'))
+    assert (exists_replica_1_1 == None)
 
-        node_1_2.query("SYSTEM DROP REPLICA 'node_1_1' FROM TABLE test.test_table")
-        exists_replica_1_1 = zk.exists(
-            "/clickhouse/tables/test/{shard}/replicated/test_table/replicas/{replica}".format(shard=1,
-                                                                                              replica='node_1_1'))
-        assert (exists_replica_1_1 == None)
+    node_1_2.query("SYSTEM DROP REPLICA 'node_1_1' FROM TABLE test.test_table")
+    exists_replica_1_1 = zk.exists(
+        "/clickhouse/tables/test/{shard}/replicated/test_table/replicas/{replica}".format(shard=1,
+                                                                                          replica='node_1_1'))
+    assert (exists_replica_1_1 == None)
 
-        node_1_2.query("SYSTEM DROP REPLICA 'node_1_1' FROM DATABASE test1")
-        exists_replica_1_1 = zk.exists(
-            "/clickhouse/tables/test1/{shard}/replicated/test_table/replicas/{replica}".format(shard=1,
-                                                                                               replica='node_1_1'))
-        assert (exists_replica_1_1 == None)
+    node_1_2.query("SYSTEM DROP REPLICA 'node_1_1' FROM DATABASE test1")
+    exists_replica_1_1 = zk.exists(
+        "/clickhouse/tables/test1/{shard}/replicated/test_table/replicas/{replica}".format(shard=1,
+                                                                                           replica='node_1_1'))
+    assert (exists_replica_1_1 == None)
 
-        node_1_3.query(
-            "SYSTEM DROP REPLICA 'node_1_1' FROM ZKPATH '/clickhouse/tables/test3/{shard}/replicated/test_table'".format(
-                shard=1))
-        exists_replica_1_1 = zk.exists(
-            "/clickhouse/tables/test3/{shard}/replicated/test_table/replicas/{replica}".format(shard=1,
-                                                                                               replica='node_1_1'))
-        assert (exists_replica_1_1 == None)
+    node_1_3.query(
+        "SYSTEM DROP REPLICA 'node_1_1' FROM ZKPATH '/clickhouse/tables/test3/{shard}/replicated/test_table'".format(
+            shard=1))
+    exists_replica_1_1 = zk.exists(
+        "/clickhouse/tables/test3/{shard}/replicated/test_table/replicas/{replica}".format(shard=1,
+                                                                                           replica='node_1_1'))
+    assert (exists_replica_1_1 == None)
 
-        node_1_2.query("SYSTEM DROP REPLICA 'node_1_1'")
-        exists_replica_1_1 = zk.exists(
-            "/clickhouse/tables/test4/{shard}/replicated/test_table/replicas/{replica}".format(shard=1,
-                                                                                               replica='node_1_1'))
-        assert (exists_replica_1_1 == None)
+    node_1_2.query("SYSTEM DROP REPLICA 'node_1_1'")
+    exists_replica_1_1 = zk.exists(
+        "/clickhouse/tables/test4/{shard}/replicated/test_table/replicas/{replica}".format(shard=1,
+                                                                                           replica='node_1_1'))
+    assert (exists_replica_1_1 == None)
