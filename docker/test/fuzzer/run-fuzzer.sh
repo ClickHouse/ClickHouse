38130790c5bb579198a6e51402824e9b0c33908b
Alexander Kuzmenkov <36882414+akuzm@users.noreply.github.com>
2021-03-18 05:09:38 +0300

    Update run-fuzzer.sh

--- docker/test/fuzzer/run-fuzzer.sh
+++ docker/test/fuzzer/run-fuzzer.sh
@@ -20,6 +20,7 @@ function clone
     # the repo from the CI as well. For local runs, start directly from the "fuzz"
     # stage.
     rm -rf ch ||:
+    mkdir ch ||:
     wget -nv -nd -c "https://clickhouse-test-reports.s3.yandex.net/$PR_TO_TEST/$SHA_TO_TEST/repo/clickhouse_no_subs.tar.gz"
     tar -C ch --strip-components=1 -xf clickhouse_no_subs.tar.gz
     ls -lath ||:
