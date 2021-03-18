f5c3e3254431bccede3481b94d69c33b1d33ee19
s-kat <sakatkovskiy@edu.hse.ru>
2021-03-19 00:57:42 +0300

    Add charset_freq

--- src/Functions/FrequencyHolder.h
+++ src/Functions/FrequencyHolder.h
@@ -1,9 +1,10 @@
 #pragma once
 
-#include <Functions/FunctionsTextClassification.h>
-
+#include <Common/TLDListsHolder.h>
+#include <Common/StringUtils/StringUtils.h>
+#include <IO/ReadBufferFromFile.h>
+#include <string_view>
 #include <string>
-#include <Functions/FunctionFactory.h>
 #include <fstream>
 #include <algorithm>
 #include <cstring>
@@ -31,29 +32,44 @@ public:
     void parseDictionaries(const std::string& pt)
     {
         is_true = pt;
-        loadEmotionalDict("/home/sergey/datadump/myemo2.txt");
-        loadEncodingsFrequency("/home/sergey/data/dumps/encodings/russian/freq_enc/");
+        loadEmotionalDict("/home/sergey/ClickHouse/src/Functions/ClassificationDictionaries/emotional_dictionary_rus.txt");
+        loadEncodingsFrequency("/home/sergey/ClickHouse/src/Functions/ClassificationDictionaries/charset_freq.txt");
     }
 
 
-    void loadEncodingsFrequency(const std::string path_to_encodings_freq)
+    void loadEncodingsFrequency(const std::string path_to_charset_freq)
     {
-        std::vector<std::string> languages = {"freq_CP866", "freq_ISO", "freq_WINDOWS-1251", "freq_UTF-8"};
-        for (std::string & lang : languages) {
-            std::ifstream file(path_to_encodings_freq + lang + ".txt");
-            Map new_lang;
-            UInt16 bigram;
-            double count;
-            double total = 0;
-            while (file >> bigram >> count) {
-                new_lang[bigram] = count;
-                total += count;
+        char charset_name_buf [40];
+        std::string charset_name;
+        ReadBufferFromFile in(path_to_charset_freq);
+        while (!in.eof())
+        {
+            char * newline = find_first_symbols<'\n'>(in.position(), in.buffer().end());
+        
+            if (newline >= in.buffer().end())
+                break;
+
+            std::string_view line(in.position(), newline - in.position());
+            in.position() = newline + 1;
+
+            if (line.empty())
+                continue;
+            // Start load new charset
+            if (line.size() > 2 && line[0] == '/' && line[1] == '/')
+            {
+                const char * st = line.data();
+                sscanf(st + 2, "%39s", charset_name_buf);
+                std::string s(charset_name_buf);
+                charset_name = s;
+            } else
+            {
+                const char * st = line.data();
+                UInt16 bigram;
+                double frequency;
+                sscanf(st, "%hd %lg", &bigram, &frequency);
+                encodings_freq[charset_name][bigram] = frequency;
+
             }
-            for (auto & el : new_lang) {
-                el.second /= total;
-            }
-            encodings_freq[lang] = new_lang;
-            file.close();
         }
     }
 
