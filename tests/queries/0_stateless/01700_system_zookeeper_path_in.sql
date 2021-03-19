9b23a7565dc4630622ea2349935027ce5801ed7f
tavplubix <avtokmakov@yandex-team.ru>
2021-03-19 07:24:43 +0300

    trigger CI

--- tests/queries/0_stateless/01700_system_zookeeper_path_in.sql
+++ tests/queries/0_stateless/01700_system_zookeeper_path_in.sql
@@ -8,6 +8,7 @@ ORDER BY tuple();
 
 SELECT name FROM system.zookeeper WHERE path = '/clickhouse/01700_system_zookeeper_path_in/s1' AND name like 'block%' ORDER BY name;
 SELECT name FROM system.zookeeper WHERE path = '/clickhouse/01700_system_zookeeper_path_in/s1/replicas' AND name LIKE '%r1%' ORDER BY name;
+
 SELECT '========';
 SELECT name FROM system.zookeeper WHERE path IN ('/clickhouse/01700_system_zookeeper_path_in/s1') AND name LIKE 'block%' ORDER BY name;
 SELECT name FROM system.zookeeper WHERE path IN ('/clickhouse/01700_system_zookeeper_path_in/s1/replicas') AND name LIKE '%r1%' ORDER BY name;
