58236af8eeb5ffd055b3cd8da33bd33a0e73ad03
kssenii <sumarokovakseniia@mail.ru>
2021-03-20 14:13:57 +0000

    Separate common for materialize* storages

--- src/Storages/PostgreSQL/StorageMaterializePostgreSQL.cpp
+++ src/Storages/PostgreSQL/StorageMaterializePostgreSQL.cpp
@@ -21,6 +21,7 @@
 #include <Interpreters/InterpreterDropQuery.h>
 #include <Storages/StorageFactory.h>
 #include <common/logger_useful.h>
+#include <Storages/ReadFinalForExternalReplicaStorage.h>
 
 
 namespace DB
@@ -368,7 +369,7 @@ NamesAndTypesList StorageMaterializePostgreSQL::getVirtuals() const
 
 Pipe StorageMaterializePostgreSQL::read(
         const Names & column_names,
-        const StorageMetadataPtr & /* metadata_snapshot */,
+        const StorageMetadataPtr & metadata_snapshot,
         SelectQueryInfo & query_info,
         const Context & context,
         QueryProcessingStage::Enum processed_stage,
@@ -382,66 +383,15 @@ Pipe StorageMaterializePostgreSQL::read(
         if (!nested_storage)
             getNested();
 
-        auto storage_lock = nested_storage->lockForShare(context.getCurrentQueryId(), context.getSettingsRef().lock_acquire_timeout);
-
-        const StorageMetadataPtr & nested_metadata = nested_storage->getInMemoryMetadataPtr();
-        Block nested_header = nested_metadata->getSampleBlock();
-
-        ColumnWithTypeAndName & sign_column = nested_header.getByPosition(nested_header.columns() - 2);
-        ColumnWithTypeAndName & version_column = nested_header.getByPosition(nested_header.columns() - 1);
-
-        NameSet column_names_set = NameSet(column_names.begin(), column_names.end());
-
-        if (ASTSelectQuery * select_query = query_info.query->as<ASTSelectQuery>(); select_query && !column_names_set.count(version_column.name))
-        {
-            auto & tables_in_select_query = select_query->tables()->as<ASTTablesInSelectQuery &>();
-
-            if (!tables_in_select_query.children.empty())
-            {
-                auto & tables_element = tables_in_select_query.children[0]->as<ASTTablesInSelectQueryElement &>();
-
-                if (tables_element.table_expression)
-                    tables_element.table_expression->as<ASTTableExpression &>().final = true;
-            }
-        }
-
-        String filter_column_name;
-        Names require_columns_name = column_names;
-        ASTPtr expressions = std::make_shared<ASTExpressionList>();
-        if (column_names_set.empty() || !column_names_set.count(sign_column.name))
-        {
-            require_columns_name.emplace_back(sign_column.name);
-
-            const auto & sign_column_name = std::make_shared<ASTIdentifier>(sign_column.name);
-            const auto & fetch_sign_value = std::make_shared<ASTLiteral>(Field(Int8(1)));
-
-            expressions->children.emplace_back(makeASTFunction("equals", sign_column_name, fetch_sign_value));
-            filter_column_name = expressions->children.back()->getColumnName();
-
-            for (const auto & column_name : column_names)
-                expressions->children.emplace_back(std::make_shared<ASTIdentifier>(column_name));
-        }
-
-        Pipe pipe = nested_storage->read(
-                require_columns_name,
-                nested_metadata, query_info, context,
-                processed_stage, max_block_size, num_streams);
-
-        pipe.addTableLock(storage_lock);
-
-        if (!expressions->children.empty() && !pipe.empty())
-        {
-            Block pipe_header = pipe.getHeader();
-            auto syntax = TreeRewriter(context).analyze(expressions, pipe_header.getNamesAndTypesList());
-            ExpressionActionsPtr expression_actions = ExpressionAnalyzer(expressions, syntax, context).getActions(true);
-
-            pipe.addSimpleTransform([&](const Block & header)
-            {
-                return std::make_shared<FilterTransform>(header, expression_actions, filter_column_name, false);
-            });
-        }
-
-        return pipe;
+        return readFinalFromNestedStorage(
+                nested_storage,
+                column_names,
+                metadata_snapshot,
+                query_info,
+                context,
+                processed_stage,
+                max_block_size,
+                num_streams);
     }
 
     LOG_WARNING(&Poco::Logger::get("StorageMaterializePostgreSQL"), "Nested table {} is unavailable or is not loaded yet", getNestedTableName());
