56073db22d65cef453cfff0758235db012ee626a
feng lv <fenglv15@mails.ucas.ac.cn>
2021-03-20 12:10:22 +0000

    max distributed depth
    Add settings max_distributed_depth
    
    fix style
    
    fix
    
    fix
    
    fix
    
    fix test
    
    fix
    
    fix

--- src/Core/Settings.h
+++ src/Core/Settings.h
@@ -435,6 +435,7 @@ class IColumn;
     M(Bool, allow_experimental_database_replicated, false, "Allow to create databases with Replicated engine", 0) \
     M(UInt64, database_replicated_initial_query_timeout_sec, 300, "How long initial DDL query should wait for Replicated database to precess previous DDL queue entries", 0) \
     M(Bool, database_replicated_ddl_output, true, "Return table with query execution status as a result of DDL query", 0) \
+    M(UInt64, max_distributed_depth, 5, "Maximum distributed query depth", 0) \
     \
     /** Obsolete settings that do nothing but left for compatibility reasons. Remove each one after half a year of obsolescence. */ \
     \
