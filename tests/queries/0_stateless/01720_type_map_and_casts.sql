90f91508295f7f63c580a870affe8b7527a102fd
Anton Popov <pad11rus@gmail.com>
2021-03-23 00:10:46 +0300

    fix test

--- tests/queries/0_stateless/01720_type_map_and_casts.sql
+++ tests/queries/0_stateless/01720_type_map_and_casts.sql
@@ -11,7 +11,7 @@ SELECT 'Map(Int8, Int8)';
 
 SELECT m FROM table_map_with_key_integer;
 SELECT m[127], m[1], m[0], m[-1] FROM table_map_with_key_integer;
-SELECT m[toInt8(number - 2)] FROM table_map_with_key_integer ARRAY JOIN range(5) AS number;
+SELECT m[toInt8(number - 2)] FROM table_map_with_key_integer ARRAY JOIN [0, 1, 2, 3, 4] AS number;
 
 SELECT count() FROM table_map_with_key_integer WHERE m = map();
 
@@ -26,7 +26,7 @@ SELECT 'Map(Int32, UInt16)';
 
 SELECT m FROM table_map_with_key_integer;
 SELECT m[-1], m[2147483647], m[-2147483648] FROM table_map_with_key_integer;
-SELECT m[toInt32(number - 2)] FROM table_map_with_key_integer ARRAY JOIN range(5) AS number;
+SELECT m[toInt32(number - 2)] FROM table_map_with_key_integer ARRAY JOIN [0, 1, 2, 3, 4] AS number;
 
 DROP TABLE IF EXISTS table_map_with_key_integer;
 
@@ -39,7 +39,7 @@ SELECT 'Map(Date, Int32)';
 
 SELECT m FROM table_map_with_key_integer;
 SELECT m[toDate('2020-01-01')], m[toDate('2020-01-02')], m[toDate('2020-01-03')] FROM table_map_with_key_integer;
-SELECT m[toDate(number)] FROM table_map_with_key_integer ARRAY JOIN range(3) AS number;
+SELECT m[toDate(number)] FROM table_map_with_key_integer ARRAY JOIN [0, 1, 2] AS number;
 
 DROP TABLE IF EXISTS table_map_with_key_integer;
 
@@ -71,7 +71,7 @@ SELECT 'Map(Int128, String)';
 
 SELECT m FROM table_map_with_key_integer;
 SELECT m[toInt128(-1)], m[toInt128(0)], m[toInt128(1234567898765432123456789)], m[toInt128(-1234567898765432123456789)] FROM table_map_with_key_integer;
-SELECT m[toInt128(number - 2)] FROM table_map_with_key_integer ARRAY JOIN range(4) AS number;
+SELECT m[toInt128(number - 2)] FROM table_map_with_key_integer ARRAY JOIN [0, 1, 2, 3] AS number;
 
 SELECT m[-1], m[0], m[toInt128(1234567898765432123456789)], m[toInt128(-1234567898765432123456789)] FROM table_map_with_key_integer;
 SELECT m[toUInt64(0)], m[toInt64(0)], m[toUInt8(0)], m[toUInt16(0)] FROM table_map_with_key_integer;
