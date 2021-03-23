7c07b43597c9bebd4853dea50ff484be3a13ee01
Azat Khuzhin <a3at.mail@gmail.com>
2021-03-23 23:03:08 +0300

    Convert system.errors.stack_trace from String into Array(UInt64)
    This should decrease overhead for the errors collecting.

--- src/Common/Exception.cpp
+++ src/Common/Exception.cpp
@@ -36,7 +36,7 @@ namespace ErrorCodes
 
 /// - Aborts the process if error code is LOGICAL_ERROR.
 /// - Increments error codes statistics.
-void handle_error_code([[maybe_unused]] const std::string & msg, const std::string & stacktrace, int code, bool remote)
+void handle_error_code([[maybe_unused]] const std::string & msg, int code, bool remote, const Exception::FramePointers & trace)
 {
     // In debug builds and builds with sanitizers, treat LOGICAL_ERROR as an assertion failure.
     // Log the message before we fail.
@@ -47,20 +47,21 @@ void handle_error_code([[maybe_unused]] const std::string & msg, const std::stri
         abort();
     }
 #endif
-    ErrorCodes::increment(code, remote, msg, stacktrace);
+
+    ErrorCodes::increment(code, remote, msg, trace);
 }
 
 Exception::Exception(const std::string & msg, int code, bool remote_)
     : Poco::Exception(msg, code)
     , remote(remote_)
 {
-    handle_error_code(msg, getStackTraceString(), code, remote);
+    handle_error_code(msg, code, remote, getStackFramePointers());
 }
 
 Exception::Exception(const std::string & msg, const Exception & nested, int code)
     : Poco::Exception(msg, nested, code)
 {
-    handle_error_code(msg, getStackTraceString(), code, remote);
+    handle_error_code(msg, code, remote, getStackFramePointers());
 }
 
 Exception::Exception(CreateFromPocoTag, const Poco::Exception & exc)
@@ -99,6 +100,31 @@ std::string Exception::getStackTraceString() const
 #else
     return trace.toString();
 #endif
+}
+
+Exception::FramePointers Exception::getStackFramePointers() const
+{
+    FramePointers trace;
+#ifdef STD_EXCEPTION_HAS_STACK_TRACE
+    {
+        trace.resize(get_stack_trace_size());
+        for (size_t i = 0; i < trace.size(); ++i)
+        {
+            trace[i] = get_stack_trace_frames()[i];
+        }
+    }
+#else
+    {
+        size_t stack_trace_size = trace.getSize();
+        size_t stack_trace_offset = trace.getOffset();
+        trace.resize(stack_trace_size - stack_trace_offset);
+        for (size_t i = stack_trace_offset; i < stack_trace_size; ++i)
+        {
+            trace[i] = trace.getFramePointers()[i];
+        }
+    }
+#endif
+    return trace;
 }
 
 
