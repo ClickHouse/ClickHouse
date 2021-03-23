84b8fe9b9a97a8757e8aeed8e83751077aee52c2
s-kat <sakatkovskiy@edu.hse.ru>
2021-03-23 21:55:14 +0300

    Fix FrequencyHolder.h

--- src/Common/FrequencyHolder.h
+++ src/Common/FrequencyHolder.h
@@ -2,13 +2,19 @@
 #include <Common/TLDListsHolder.h>
 #include <Common/StringUtils/StringUtils.h>
 #include <IO/ReadBufferFromFile.h>
+#include <IO/ReadBufferFromString.h>
+#include <IO/ReadHelpers.h>
+#include <IO/readFloatText.h>
+#include <IO/Operators.h>
 #include <string_view>
 #include <string>
+#include <common/find_symbols.h>
 #include <fstream>
 #include <algorithm>
 #include <cstring>
 #include <limits>
 #include <unordered_map>
+
 
 
 namespace DB
@@ -17,8 +23,8 @@ namespace DB
 class FrequencyHolder
 {
 public:
-    using Map = std::unordered_map<UInt16, double>;
-    using Container = std::unordered_map<std::string, Map>;
+    using Map = std::unordered_map<UInt16, Float64>;
+    using Container = std::unordered_map<String, Map>;
 
 
     static FrequencyHolder & getInstance()
@@ -28,7 +34,7 @@ public:
     }
 
 
-    void parseDictionaries(const std::string& pt)
+    void parseDictionaries(const String & pt)
     {
         is_true = pt;
         loadEmotionalDict("/home/sergey/ClickHouse/src/Functions/ClassificationDictionaries/emotional_dictionary_rus.txt");
@@ -36,65 +42,61 @@ public:
     }
 
 
-    void loadEncodingsFrequency(const std::string path_to_charset_freq)
+    void loadEncodingsFrequency(const String & path_to_charset_freq)
     {
-        char charset_name_buf [40];
         UInt16 bigram;
-        double frequency;
-        std::string charset_name;
+        Float64 frequency;
+        String charset_name;
 
         ReadBufferFromFile in(path_to_charset_freq);
         while (!in.eof())
         {
             char * newline = find_first_symbols<'\n'>(in.position(), in.buffer().end());
-        
+
             if (newline >= in.buffer().end())
                 break;
 
             std::string_view line(in.position(), newline - in.position());
-            in.position() = newline + 1;
 
             if (line.empty())
                 continue;
             // Start load new charset
             if (line.size() > 2 && line[0] == '/' && line[1] == '/')
             {
-                const char * st = line.data();
-                sscanf(st + 2, "%39s", charset_name_buf);
-                std::string s(charset_name_buf);
-                charset_name = s;
+                ReadBufferFromMemory bufline(in.position() + 3, newline - in.position());
+                readString(charset_name, bufline);
             } else
             {
-                const char * st = line.data();
-                sscanf(st, "%hd %lg", &bigram, &frequency);
+                ReadBufferFromMemory buf_line(in.position(), newline - in.position());
+                readIntText(bigram, buf_line);
+                buf_line.ignore();
+                readFloatText(frequency, buf_line);
                 encodings_freq[charset_name][bigram] = frequency;
-
             }
+            in.position() = newline + 1;
         }
     }
 
 
-    void loadEmotionalDict(const std::string path_to_emotional_dict)
+    void loadEmotionalDict(const String & path_to_emotional_dict)
     {
 
-        char word_buf [40];
-        double tonality;
+        String word;
+        Float64 tonality;
+
         ReadBufferFromFile in(path_to_emotional_dict);
         while (!in.eof())
         {
             char * newline = find_first_symbols<'\n'>(in.position(), in.buffer().end());
-        
-            if (newline >= in.buffer().end())
-                break;
 
-            std::string_view line(in.position(), newline - in.position());
+            if (newline >= in.buffer().end()) { break; }
+
+            ReadBufferFromMemory buf_line(in.position(), newline - in.position());
             in.position() = newline + 1;
 
-            if (line.empty())
-                continue;
-            const char * st = line.data();
-            sscanf(st, "%39s %lg", word_buf, &tonality);
-            std::string word(word_buf);
+            readStringUntilWhitespace(word, buf_line);
+            buf_line.ignore();
+            readFloatText(tonality, buf_line);
 
             emotional_dict[word] = tonality;
 
@@ -102,13 +104,13 @@ public:
     }
 
 
-    const std::string & get_path()
+    const String & get_path()
     {
         return is_true;
     }
 
 
-    const std::unordered_map<std::string, double> getEmotionalDict()
+    const std::unordered_map<String, Float64> getEmotionalDict()
     {
         return emotional_dict;
     }
@@ -122,8 +124,8 @@ public:
 
 protected:
 
-    std::string is_true;
-    std::unordered_map<std::string, double> emotional_dict;
+    String is_true;
+    std::unordered_map<String, Float64> emotional_dict;
     Container encodings_freq;
 };
 }
