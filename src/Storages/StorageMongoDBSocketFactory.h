05de8084ccd8d1ebc5ed78e0cfd9e9e80655c272
Ubuntu <clarity-admin@clickhouse-build.aav45yqk120unnqzsvuytc3qfd.cx.internal.cloudapp.net>
2021-03-23 15:01:13 +0000

    MongoDB SSL Connection

---
+++ src/Storages/StorageMongoDBSocketFactory.h
@@ -0,0 +1,19 @@
+#pragma once
+
+#include <Poco/MongoDB/Connection.h>
+
+
+namespace DB
+{
+
+class StorageMongoDBSocketFactory : public Poco::MongoDB::Connection::SocketFactory
+{
+public:
+    virtual Poco::Net::StreamSocket createSocket(const std::string & host, int port, Poco::Timespan connectTimeout, bool secure) override;
+
+private:
+    Poco::Net::StreamSocket createPlainSocket(const std::string & host, int port);
+    Poco::Net::StreamSocket createSecureSocket(const std::string & host, int port);
+};
+
+}
