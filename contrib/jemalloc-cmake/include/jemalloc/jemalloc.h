f9f0d9d29e455dabb31f1b212750181ffa96f070
Anna <annaconda@bk.ru>
2021-03-24 23:38:00 +0300

    Revert "ignoring submodules"
    This reverts commit aba4645e08076fb8a5f0947f3a6e6b245d66c392.

---
+++ contrib/jemalloc-cmake/include/jemalloc/jemalloc.h
@@ -0,0 +1,16 @@
+#pragma once
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+#include <jemalloc/jemalloc_defs.h>
+#include <jemalloc/jemalloc_rename.h>
+#include <jemalloc/jemalloc_macros.h>
+#include <jemalloc/jemalloc_protos.h>
+#include <jemalloc/jemalloc_typedefs.h>
+
+#ifdef __cplusplus
+}
+#endif
+
