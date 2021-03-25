f8bcb257e0e09e9a1f0c67aaac777d000a1bda86
hexiaoting <hewenting_ict@163.com>
2021-03-25 11:15:03 +0800

    ReImplement AggregateFunctionSumCount

--- src/AggregateFunctions/AggregateFunctionSumCount.cpp
+++ src/AggregateFunctions/AggregateFunctionSumCount.cpp
@@ -4,7 +4,6 @@
 #include <AggregateFunctions/FactoryHelpers.h>
 #include "registerAggregateFunctions.h"
 
-
 namespace DB
 {
 namespace ErrorCodes
@@ -14,22 +13,12 @@ namespace ErrorCodes
 
 namespace
 {
-
-template <typename T>
-struct SumCountType
+bool allowType(const DataTypePtr& type) noexcept
 {
-    /// @note It uses slow Decimal128 (cause we need such a variant). sumWithOverflow is faster for Decimal32/64
-    using ResultType = std::conditional_t<IsDecimalNumber<T>,
-                                        std::conditional_t<std::is_same_v<T, Decimal256>, Decimal256, Decimal128>,
-                                        NearestFieldType<T>>;
-    using AggregateDataType = AggregateFunctionSumCountData<ResultType>;
-    using Function = AggregateFunctionSumCount<T, ResultType, AggregateDataType>;
-};
+    const WhichDataType t(type);
+    return t.isInt() || t.isUInt() || t.isFloat() || t.isDecimal();
+}
 
-
-template <typename T> using AggregateFunctionSumCountFunc = typename SumCountType<T>::Function;
-
-template <template <typename> class Function>
 AggregateFunctionPtr createAggregateFunctionSumCount(const std::string & name, const DataTypes & argument_types, const Array & parameters)
 {
     assertNoParameters(name, parameters);
@@ -37,14 +26,16 @@ AggregateFunctionPtr createAggregateFunctionSumCount(const std::string & name, c
 
     AggregateFunctionPtr res;
     DataTypePtr data_type = argument_types[0];
-    if (isDecimal(data_type))
-        res.reset(createWithDecimalType<Function>(*data_type, *data_type, argument_types));
-    else
-        res.reset(createWithNumericType<Function>(*data_type, argument_types));
+    if (!allowType(data_type))
+        throw Exception("Illegal type " + data_type->getName() + " of argument for aggregate function " + name,
+            ErrorCodes::ILLEGAL_TYPE_OF_ARGUMENT);
 
-    if (!res)
-        throw Exception("Illegal type " + argument_types[0]->getName() + " of argument for aggregate function " + name,
-                        ErrorCodes::ILLEGAL_TYPE_OF_ARGUMENT);
+    if (isDecimal(data_type))
+        res.reset(createWithDecimalType<AggregateFunctionSumCount>(
+            *data_type, argument_types, getDecimalScale(*data_type)));
+    else
+        res.reset(createWithNumericType<AggregateFunctionSumCount>(*data_type, argument_types));
+
     return res;
 }
 
@@ -52,7 +43,7 @@ AggregateFunctionPtr createAggregateFunctionSumCount(const std::string & name, c
 
 void registerAggregateFunctionSumCount(AggregateFunctionFactory & factory)
 {
-    factory.registerFunction("sumCount", createAggregateFunctionSumCount<AggregateFunctionSumCountFunc>);
+    factory.registerFunction("sumCount", createAggregateFunctionSumCount);
 }
 
 }
