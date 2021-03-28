cd89dbd5295c1245ef87a84e2e727e04774a68aa
Alexey Milovidov <milovidov@yandex-team.ru>
2021-03-28 22:34:23 +0300

    Just in case

--- tests/queries/0_stateless/01778_mmap_cache_infra.sql
+++ tests/queries/0_stateless/01778_mmap_cache_infra.sql
@@ -3,5 +3,5 @@
 SYSTEM DROP MMAP CACHE;
 
 SET system_events_show_zero_values = 1;
-SELECT event FROM system.events WHERE event LIKE '%MMap%';
-SELECT metric FROM system.metrics WHERE metric LIKE '%MMap%';
+SELECT event FROM system.events WHERE event LIKE '%MMap%' ORDER BY event;
+SELECT metric FROM system.metrics WHERE metric LIKE '%MMap%' ORDER BY metric;
