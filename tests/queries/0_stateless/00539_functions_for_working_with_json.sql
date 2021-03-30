0e18a39d93005ae681890947588214f8917edb32
kssenii <sumarokovakseniia@mail.ru>
2021-03-30 12:22:54 +0000

    Add test

--- tests/queries/0_stateless/00539_functions_for_working_with_json.sql
+++ tests/queries/0_stateless/00539_functions_for_working_with_json.sql
@@ -15,3 +15,11 @@ SELECT visitParamExtractRaw('{"myparam": "{"}', 'myparam');
 SELECT visitParamExtractRaw('{"myparam": "["}', 'myparam');
 SELECT visitParamExtractRaw('{"myparam": ["]", "2", "3"], "other":123}', 'myparam');
 SELECT visitParamExtractRaw('{"myparam": {"nested" : [1,2,3]}, "other":123}', 'myparam');
+
+SELECT JSONExtractInt('{"myparam":-1}', 'myparam');
+SELECT JSONExtractUInt('{"myparam":-1}', 'myparam');
+SELECT JSONExtractFloat('{"myparam":null}', 'myparam');
+SELECT JSONExtractFloat('{"myparam":-1}', 'myparam');
+SELECT JSONExtractBool('{"myparam":true}', 'myparam');
+SELECT JSONExtractString('{"myparam":"test_string"}', 'myparam');
+SELECT JSONExtractString('{"myparam":"test\\"string"}', 'myparam');
