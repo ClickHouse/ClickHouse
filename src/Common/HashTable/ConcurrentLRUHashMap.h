e937cbd35a3940b86ebb81398411628a1e36f49d
Maksim Kita <kitaetoya@gmail.com>
2021-03-30 22:00:30 +0300

    Updated LRUConcurrentHashMap

--- src/Common/HashTable/ConcurrentLRUHashMap.h
+++ src/Common/HashTable/ConcurrentLRUHashMap.h
@@ -55,7 +55,6 @@ public:
 
     explicit ConcurrentLRUHashMapImpl(size_t max_size_)
         : max_size(max_size_)
-        , epoch(max_size)
     {}
 
     /// TODO: std insert return value
@@ -98,7 +97,6 @@ public:
 
             value_to_update.value = Value(std::forward<Args>(args)...);
 
-            holes_count.fetch_add(1, std::memory_order_relaxed);
             size_t previous_epoch = epoch.fetch_add(1, std::memory_order_relaxed);
             value_to_update.epoch.store(previous_epoch, std::memory_order_relaxed);
         }
@@ -115,19 +113,7 @@ public:
 
         auto & value_to_update = iterator_in_map->second;
 
-        /// TODO: This place is tricky for concurrency
-
-        size_t epoch_value = epoch.load();
-        size_t holes_count_value = holes_count.load();
-
-        size_t last_valid_epoch = epoch_value - holes_count_value - max_size;
-
-        if (value_to_update.epoch < last_valid_epoch)
-            return nullptr;
-
-        holes_count.fetch_add(1, std::memory_order_relaxed);
-
-        size_t epoch_previous_value = epoch.fetch_add(1);
+        size_t epoch_previous_value = epoch.fetch_add(1, std::memory_order_relaxed);
         value_to_update.epoch = epoch_previous_value;
 
         return &iterator_in_map->second.value;
@@ -143,18 +129,8 @@ public:
     {
         WriteLockType write_lock(shared_mutex_provider.getSharedMutex());
 
-        size_t epoch_value = epoch.load();
-        size_t holes_count_value = holes_count.load();
-
         for (auto & [key, value] : map)
-        {
-            size_t last_valid_epoch = epoch_value - holes_count_value - max_size;
-
-            if (value.epoch < last_valid_epoch)
-                continue;
-
             std::forward<ForEachFunc>(func)(key, value.value);
-        }
     }
 
     size_t getMaxSize() const
@@ -165,8 +141,6 @@ public:
 
     size_t size() const
     {
-        /// This size is broken it can be greater than cache size by factor of 2
-        /// but to calculate size with valid cells it will be O(n)
         ReadLockType lock(shared_mutex_provider.getSharedMutex());
         return map.size();
     }
@@ -195,20 +169,38 @@ private:
 
     void eraseOldCells(WriteLockType &)
     {
-        size_t epoch_value = epoch.load();
-        size_t holes_count_value = holes_count.load();
-
-        for (auto it = map.begin(); it != map.end();)
+        struct EpochAndIterator
         {
-            const auto & value = it->second;
-            size_t last_valid_epoch = epoch_value - holes_count_value - max_size;
+            size_t epoch;
+            iterator it;
+        };
 
-            if (value.epoch < last_valid_epoch)
-                it = map.erase(it);
-            else
-                ++it;
+        std::vector<EpochAndIterator> epoch_and_iterator;
+        epoch_and_iterator.reserve(max_size);
+
+        for (auto it = map.begin(); it != map.end(); ++it)
+        {
+            const auto & node = it->second;
+            size_t node_epoch = node.epoch.load(std::memory_order_relaxed);
+            epoch_and_iterator.push_back(EpochAndIterator{node_epoch, it});
         }
-    }
+
+        std::sort(epoch_and_iterator.begin(), epoch_and_iterator.end(), [](const auto & lhs, const auto & rhs) { return lhs.epoch > rhs.epoch; });
+
+        size_t max_size_copy = max_size - 1;
+        epoch.store(max_size_copy, std::memory_order_relaxed);
+
+        for (size_t i = 0; i < max_size; ++i)
+        {
+            auto iterator_to_update = epoch_and_iterator[i].it;
+            auto & node = iterator_to_update->second;
+            node.epoch = max_size_copy;
+            --max_size_copy;
+        }
+
+        for (size_t i = max_size; i < epoch_and_iterator.size(); ++i)
+            map.erase(epoch_and_iterator[i].it);
+   }
 
     struct Node
     {
@@ -220,11 +212,11 @@ private:
     };
 
     size_t max_size;
-    std::atomic<size_t> epoch;
+    std::atomic<size_t> epoch = 0;
 
-    /// TODO: Abseil or our hash table
-    std::unordered_map<Key, Node> map;
-    std::atomic<size_t> holes_count = 0;
+    using HashMap = std::unordered_map<Key, Node>;
+    using iterator = typename HashMap::iterator;
+    HashMap map;
 
     mutable SharedMutexProvider shared_mutex_provider {};
 };
--- src/Common/tests/concurrent_lru_hash_map_perf.cpp
+++ src/Common/tests/concurrent_lru_hash_map_perf.cpp
@@ -1,37 +1,99 @@
 #include <iostream>
 #include <vector>
 #include <thread>
+#include <random>
+#include <pcg_random.hpp>
 
 #include <Common/HashTable/ConcurrentLRUHashMap.h>
+#include <Common/HashTable/LRUHashMap.h>
+#include <Common/Stopwatch.h>
+
+std::vector<UInt64> generateNumbersToInsert(size_t numbers_to_insert_size)
+{
+    std::vector<UInt64> numbers;
+    numbers.reserve(numbers_to_insert_size);
+
+    std::random_device rd;
+    pcg64 gen(rd());
+
+    UInt64 min = std::numeric_limits<UInt64>::min();
+    UInt64 max = std::numeric_limits<UInt64>::max();
+
+    auto distribution = std::uniform_int_distribution<>(min, max);
+
+    for (size_t i = 0; i < numbers_to_insert_size; ++i)
+    {
+        UInt64 number = distribution(gen);
+        numbers.emplace_back(number);
+    }
+
+    return numbers;
+}
 
 int main(int argc, char ** argv)
 {
     (void)(argc);
     (void)(argv);
 
-    ConcurrentLRUHashMap<int, int> cache(200000);
+    std::shared_mutex mtx;
+    ConcurrentLRUHashMap<UInt64, UInt64> cache(1000000);
 
-    size_t thread_count = 32;
-    std::vector<std::thread> insert_threads;
+    Stopwatch watch;
+    watch.start();
+
+    size_t thread_count = 8;
+    std::vector<std::thread> threads;
 
     for (size_t i = 0; i < thread_count; ++i)
     {
-        insert_threads.emplace_back([&]
+        threads.emplace_back([&]
         {
-            for (size_t index = 0; index < 1000000; ++index)
-                cache.insert(index, index);
+            std::unique_lock<std::shared_mutex> lock(mtx);
+            auto numbers = generateNumbersToInsert(2000000);
+
+            for (auto number : numbers)
+                cache.insert(number, number);
         });
     }
 
-    for (auto & insert_thread : insert_threads)
-    {
-        insert_thread.join();
-    }
+    for (auto & thread : threads)
+        thread.join();
 
-    cache.forEach([](auto & key, auto & value)
+    threads.clear();
+
+    std::cerr << "Insert take " << watch.elapsedMilliseconds() << " milliseconds size " << cache.size() << std::endl;
+
+    for (size_t count = 0; count < 120; ++count)
     {
-        std::cerr << "Key " << key << " value " << value << std::endl;
-    });
+        watch.restart();
+
+        size_t summ = 0;
+
+        for (size_t i = 0; i < thread_count; ++i)
+        {
+            threads.emplace_back([&]
+            {
+                auto numbers = generateNumbersToInsert(1000000);
+
+                for (auto number : numbers)
+                {
+                    std::shared_lock<std::shared_mutex> lock(mtx);
+                    const auto * value = cache.find(number);
+
+                    if (value != nullptr)
+                        summ += *value;
+                }
+            });
+        }
+
+        for (auto & thread : threads)
+            thread.join();
+
+        threads.clear();
+
+        std::cerr << "Read take " << watch.elapsedMilliseconds() << " milliseconds " << std::endl;
+        std::cerr << "Summ " << summ << std::endl;
+    }
 
     return 0;
 }
