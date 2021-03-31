0562dc9df236be0cf657192ec37323eca1c2967a
Peng Jian <pengjian.uestc@gmail.com>
2021-03-31 22:25:51 +0800

    remove the flag in the parser

--- src/Processors/Formats/IInputFormat.cpp
+++ src/Processors/Formats/IInputFormat.cpp
@@ -18,9 +18,7 @@ IInputFormat::IInputFormat(Block header, ReadBuffer & in_)
 
 void IInputFormat::resetParser()
 {
-    if (in.hasPendingData())
-        throw Exception("Unread data in IInputFormat::resetParser. Most likely it's a bug.", ErrorCodes::LOGICAL_ERROR);
-
+    in.ignoreAll();
     // those are protected attributes from ISource (I didn't want to propagate resetParser up there)
     finished = false;
     got_exception = false;
