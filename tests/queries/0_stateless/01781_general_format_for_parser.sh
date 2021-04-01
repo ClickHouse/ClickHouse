ed8de8cb09e5cee5ee51455165cc59ecd80f0cd0
Peng Jian <pengjian.uestc@gmail.com>
2021-04-01 11:41:42 +0800

    fix test case

--- tests/queries/0_stateless/01781_general_format_for_parser.sh
+++ tests/queries/0_stateless/01781_general_format_for_parser.sh
@@ -4,13 +4,9 @@ CUR_DIR=$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)
 # shellcheck source=../shell_config.sh
 . "$CUR_DIR"/../shell_config.sh
 
-TEST_OUTPUT=`$CLICKHOUSE_LOCAL --query "select toUInt8(1) as a, toUInt16(2) as b, '\xDE\xAD\xBE\xEF' as c, toFixedString('\xCA\xFE\xBA\xBE',4) as d from numbers(5) format Native" | xxd -ps`
+TEST_OUTPUT=`$CLICKHOUSE_LOCAL --query "select toUInt8(1) as a, toUInt16(2) as b, '\xDE\xAD\xBE\xEF' as c, toFixedString('\xCA\xFE\xBA\xBE',4) as d from numbers(5) format Native" | md5sum | cut -d ' ' -f1`
 
-EXPECTED_OUTPUT=`
-echo '040501610555496e7438010101010101620655496e743136020002000200
-02000200016306537472696e6704deadbeef04deadbeef04deadbeef04de
-adbeef04deadbeef01640e4669786564537472696e67283429cafebabeca
-febabecafebabecafebabecafebabe'`
+EXPECTED_OUTPUT=`echo 'd7255e6863ef058aa60064fab921fb2e'`
 
 if [ "$TEST_OUTPUT" = "$EXPECTED_OUTPUT" ]; then
     echo 'OK'
