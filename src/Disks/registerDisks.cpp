14287cc92ddb408fd21b6e14111a46dc5cc4d143
kssenii <sumarokovakseniia@mail.ru>
2021-04-02 22:09:12 +0000

    Fix fast test

--- src/Disks/registerDisks.cpp
+++ src/Disks/registerDisks.cpp
@@ -8,11 +8,14 @@
 
 namespace DB
 {
+
 void registerDiskLocal(DiskFactory & factory);
 void registerDiskMemory(DiskFactory & factory);
+
 #if USE_AWS_S3
 void registerDiskS3(DiskFactory & factory);
 #endif
+
 #if USE_HDFS
 void registerDiskHDFS(DiskFactory & factory);
 #endif
@@ -24,10 +27,13 @@ void registerDisks()
 
     registerDiskLocal(factory);
     registerDiskMemory(factory);
-    registerDiskHDFS(factory);
 
 #if USE_AWS_S3
     registerDiskS3(factory);
+#endif
+
+#if USE_HDFS
+    registerDiskHDFS(factory);
 #endif
 }
 
