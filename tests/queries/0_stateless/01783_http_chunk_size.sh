c05636cf7587ee4d515aa4446012770f76c113a1
Ivan Lezhankin <ilezhankin@yandex-team.ru>
2021-04-02 13:57:56 +0300

    Fix test a little bit

--- tests/queries/0_stateless/01783_http_chunk_size.sh
+++ tests/queries/0_stateless/01783_http_chunk_size.sh
@@ -4,12 +4,14 @@ CURDIR=$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)
 # shellcheck source=../shell_config.sh
 . "$CURDIR"/../shell_config.sh
 
-echo "DROP TABLE IF EXISTS table" | ${CLICKHOUSE_CURL} -sSg "${CLICKHOUSE_URL}&session_id=01783" -d @-
-echo "CREATE TABLE table (a String) ENGINE Memory()" | ${CLICKHOUSE_CURL} -sSg "${CLICKHOUSE_URL}&session_id=01783" -d @-
+URL="${CLICKHOUSE_URL}&session_id=id_${CLICKHOUSE_DATABASE}"
+
+echo "DROP TABLE IF EXISTS table" | ${CLICKHOUSE_CURL} -sSg "${URL}" -d @-
+echo "CREATE TABLE table (a String) ENGINE Memory()" | ${CLICKHOUSE_CURL} -sSg "${URL}" -d @-
 
 # NOTE: suppose that curl sends everything in a single chunk - there are no options to force the chunk-size.
-echo "SET max_query_size=44" | ${CLICKHOUSE_CURL} -sSg "${CLICKHOUSE_URL}&session_id=01783" -d @-
-echo -ne "INSERT INTO TABLE table FORMAT TabSeparated 1234567890 1234567890 1234567890 1234567890\n" | ${CLICKHOUSE_CURL} -H "Transfer-Encoding: chunked" -sS "${CLICKHOUSE_URL}&session_id=01783" --data-binary @-
+echo "SET max_query_size=44" | ${CLICKHOUSE_CURL} -sSg "${URL}" -d @-
+echo -ne "INSERT INTO TABLE table FORMAT TabSeparated 1234567890 1234567890 1234567890 1234567890\n" | ${CLICKHOUSE_CURL} -H "Transfer-Encoding: chunked" -sS "${URL}" --data-binary @-
 
-echo "SELECT * from table" | ${CLICKHOUSE_CURL} -sSg "${CLICKHOUSE_URL}&session_id=01783" -d @-
-echo "DROP TABLE table" | ${CLICKHOUSE_CURL} -sSg "${CLICKHOUSE_URL}&session_id=01783" -d @-
+echo "SELECT * from table" | ${CLICKHOUSE_CURL} -sSg "${URL}" -d @-
+echo "DROP TABLE table" | ${CLICKHOUSE_CURL} -sSg "${URL}" -d @-
