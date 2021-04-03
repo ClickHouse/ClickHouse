8cbfbb5de78d4738449f9986f6c81e60ed774ae6
kssenii <sumarokovakseniia@mail.ru>
2021-04-03 12:02:29 +0000

    Small refactoring, add hdfs to test_disk_types

--- src/Disks/HDFS/DiskHDFS.cpp
+++ src/Disks/HDFS/DiskHDFS.cpp
@@ -31,25 +31,19 @@ namespace ErrorCodes
 
 
 DiskHDFS::DiskHDFS(
-    const Poco::Util::AbstractConfiguration & config_,
     const String & name_,
     const String & hdfs_name_,
-    const String & metadata_path_)
-    : config(config_)
+    const String & metadata_path_,
+    const Context & context_)
+    : log(&Poco::Logger::get("DiskHDFS"))
     , name(name_)
     , hdfs_name(hdfs_name_)
     , metadata_path(std::move(metadata_path_))
+    , context(context_)
+    , config(context.getGlobalContext().getConfigRef())
     , builder(createHDFSBuilder(hdfs_name, config))
     , fs(createHDFSFS(builder.get()))
 {
-}
-
-
-ReservationPtr DiskHDFS::reserve(UInt64 bytes)
-{
-    if (!tryReserve(bytes))
-        return {};
-    return std::make_unique<DiskHDFSReservation>(std::static_pointer_cast<DiskHDFS>(shared_from_this()), bytes);
 }
 
 
@@ -108,6 +102,7 @@ void DiskHDFS::moveFile(const String & from_path, const String & to_path)
 {
     if (exists(to_path))
         throw Exception("File already exists: " + to_path, ErrorCodes::FILE_ALREADY_EXISTS);
+
     Poco::File(metadata_path + from_path).renameTo(metadata_path + to_path);
 }
 
@@ -128,43 +123,15 @@ void DiskHDFS::replaceFile(const String & from_path, const String & to_path)
 }
 
 
-//void DiskHDFS::copyFile(const String & from_path, const String & to_path)
-//{
-//    if (exists(to_path))
-//        remove(to_path);
-//
-//    Metadata from(metadata_path, from_path);
-//    Metadata to(metadata_path, to_path, true);
-//
-//    for (const auto & [path, size] : from.hdfs_objects)
-//    {
-//        auto new_path = hdfs_name + getRandomName();
-//        /// TODO:: hdfs copy semantics
-//        /*
-//        Aws::HDFS::Model::CopyObjectRequest req;
-//        req.SetCopySource(bucket + "/" + path);
-//        req.SetBucket(bucket);
-//        req.SetKey(new_path);
-//        throwIfError(client->CopyObject(req));
-//        */
-//        throw Exception("is not implemented yet", 1);
-//        to.addObject(new_path, size);
-//    }
-//
-//    to.save();
-//}
-
-
 std::unique_ptr<ReadBufferFromFileBase> DiskHDFS::readFile(const String & path, size_t buf_size, size_t, size_t, size_t, MMappedFileCache *) const
 {
     Metadata metadata(metadata_path, path);
 
-    LOG_DEBUG(
-        &Poco::Logger::get("DiskHDFS"),
+    LOG_DEBUG(log,
         "Read from file by path: {}. Existing HDFS objects: {}",
         backQuote(metadata_path + path), metadata.hdfs_objects.size());
 
-    return std::make_unique<ReadIndirectBufferFromHDFS>(config, hdfs_name, "", metadata, buf_size);
+    return std::make_unique<ReadIndirectBufferFromHDFS>(context, hdfs_name, "", metadata, buf_size);
 }
 
 
@@ -183,22 +150,20 @@ std::unique_ptr<WriteBufferFromFileBase> DiskHDFS::writeFile(const String & path
         /// Save empty metadata to disk to have ability to get file size while buffer is not finalized.
         metadata.save();
 
-        LOG_DEBUG(
-            &Poco::Logger::get("DiskHDFS"),
+        LOG_DEBUG(log,
             "Write to file by path: {}. New hdfs path: {}", backQuote(metadata_path + path), hdfs_path);
 
-        return std::make_unique<WriteIndirectBufferFromHDFS>(config, hdfs_path, file_name, metadata, buf_size);
+        return std::make_unique<WriteIndirectBufferFromHDFS>(context, hdfs_path, file_name, metadata, buf_size);
     }
     else
     {
         Metadata metadata(metadata_path, path);
 
-        LOG_DEBUG(
-            &Poco::Logger::get("DiskHDFS"),
+        LOG_DEBUG(log,
             "Append to file by path: {}. New hdfs path: {}. Existing HDFS objects: {}",
             backQuote(metadata_path + path), hdfs_path, metadata.hdfs_objects.size());
 
-        return std::make_unique<WriteIndirectBufferFromHDFS>(config, hdfs_path, file_name, metadata, buf_size);
+        return std::make_unique<WriteIndirectBufferFromHDFS>(context, hdfs_path, file_name, metadata, buf_size);
     }
 }
 
@@ -255,33 +220,6 @@ void DiskHDFS::removeRecursive(const String & path)
 }
 
 
-bool DiskHDFS::tryReserve(UInt64 bytes)
-{
-    std::lock_guard lock(reservation_mutex);
-    if (bytes == 0)
-    {
-        LOG_DEBUG(&Poco::Logger::get("DiskHDFS"), "Reserving 0 bytes on HDFS disk {}", backQuote(name));
-        ++reservation_count;
-        return true;
-    }
-
-    auto available_space = getAvailableSpace();
-    UInt64 unreserved_space = available_space - std::min(available_space, reserved_bytes);
-    if (unreserved_space >= bytes)
-    {
-        LOG_DEBUG(
-            &Poco::Logger::get("DiskHDFS"),
-            "Reserving {} on disk {}, having unreserved ",
-            formatReadableSizeWithBinarySuffix(bytes), backQuote(name), formatReadableSizeWithBinarySuffix(unreserved_space));
-
-        ++reservation_count;
-        reserved_bytes += bytes;
-        return true;
-    }
-    return false;
-}
-
-
 void DiskHDFS::listFiles(const String & path, std::vector<String> & file_names)
 {
     for (auto it = iterateDirectory(path); it->isValid(); it->next())
@@ -327,6 +265,41 @@ void DiskHDFS::setReadOnly(const String & path)
 }
 
 
+ReservationPtr DiskHDFS::reserve(UInt64 bytes)
+{
+    if (!tryReserve(bytes))
+        return {};
+    return std::make_unique<DiskHDFSReservation>(std::static_pointer_cast<DiskHDFS>(shared_from_this()), bytes);
+}
+
+
+bool DiskHDFS::tryReserve(UInt64 bytes)
+{
+    std::lock_guard lock(reservation_mutex);
+
+    if (bytes == 0)
+    {
+        LOG_DEBUG(log, "Reserving 0 bytes on HDFS disk {}", backQuote(name));
+        ++reservation_count;
+        return true;
+    }
+
+    auto available_space = getAvailableSpace();
+    UInt64 unreserved_space = available_space - std::min(available_space, reserved_bytes);
+    if (unreserved_space >= bytes)
+    {
+        LOG_DEBUG(log,
+            "Reserving {} on disk {}, having unreserved {}",
+            formatReadableSizeWithBinarySuffix(bytes), backQuote(name), formatReadableSizeWithBinarySuffix(unreserved_space));
+        ++reservation_count;
+        reserved_bytes += bytes;
+        return true;
+    }
+
+    return false;
+}
+
+
 void registerDiskHDFS(DiskFactory & factory)
 {
     auto creator = [](const String & name,
@@ -334,21 +307,45 @@ void registerDiskHDFS(DiskFactory & factory)
                       const String & config_prefix,
                       const Context & context) -> DiskPtr
     {
-        const auto * disk_config = config.createView(config_prefix);
-
         Poco::File disk{context.getPath() + "disks/" + name};
         disk.createDirectories();
-        DB::String uri{disk_config->getString("endpoint")};
+
+        DB::String uri{config.getString(config_prefix + ".endpoint")};
 
         if (uri.back() != '/')
             throw Exception(ErrorCodes::BAD_ARGUMENTS, "HDFS path must ends with '/', but '{}' doesn't.", uri);
 
         String metadata_path = context.getPath() + "disks/" + name + "/";
-
-        return std::make_shared<DiskHDFS>(config, name, uri, metadata_path);
+        return std::make_shared<DiskHDFS>(name, uri, metadata_path, context);
     };
 
     factory.registerDiskType("hdfs", creator);
 }
+
+//void DiskHDFS::copyFile(const String & from_path, const String & to_path)
+//{
+//    if (exists(to_path))
+//        remove(to_path);
+//
+//    Metadata from(metadata_path, from_path);
+//    Metadata to(metadata_path, to_path, true);
+//
+//    for (const auto & [path, size] : from.hdfs_objects)
+//    {
+//        auto new_path = hdfs_name + getRandomName();
+//        /// TODO:: hdfs copy semantics
+//        /*
+//        Aws::HDFS::Model::CopyObjectRequest req;
+//        req.SetCopySource(bucket + "/" + path);
+//        req.SetBucket(bucket);
+//        req.SetKey(new_path);
+//        throwIfError(client->CopyObject(req));
+//        */
+//        throw Exception("is not implemented yet", 1);
+//        to.addObject(new_path, size);
+//    }
+//
+//    to.save();
+//}
 
 }
