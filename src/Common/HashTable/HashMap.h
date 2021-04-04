4cc5c0a18adb67865b355cb856faa045f3d443fd
Maksim Kita <kitaetoya@gmail.com>
2021-04-05 01:01:20 +0300

    Update HashMap.h

--- src/Common/HashTable/HashMap.h
+++ src/Common/HashTable/HashMap.h
@@ -22,7 +22,7 @@ struct PairNoInit
     First first;
     Second second;
 
-    PairNoInit() = default;
+    PairNoInit() {};
 
     template <typename FirstValue>
     PairNoInit(FirstValue && first_, NoInitTag)
