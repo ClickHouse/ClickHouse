908d872b76e1ad446ffe5acd62cf84443da39374
kssenii <sumarokovakseniia@mail.ru>
2021-04-04 09:08:09 +0000

    Add seek method

--- src/Storages/HDFS/ReadBufferFromHDFS.cpp
+++ src/Storages/HDFS/ReadBufferFromHDFS.cpp
@@ -12,6 +12,9 @@ namespace ErrorCodes
 {
     extern const int NETWORK_ERROR;
     extern const int CANNOT_OPEN_FILE;
+    extern const int CANNOT_SEEK_THROUGH_FILE;
+    extern const int SEEK_POSITION_OUT_OF_BOUND;
+    extern const int CANNOT_TELL_THROUGH_FILE;
 }
 
 ReadBufferFromHDFS::~ReadBufferFromHDFS() = default;
@@ -26,6 +29,9 @@ struct ReadBufferFromHDFS::ReadBufferFromHDFSImpl
     HDFSBuilderWrapper builder;
     HDFSFSPtr fs;
 
+    off_t offset = 0;
+    bool initialized = false;
+
     ReadBufferFromHDFSImpl(const std::string & hdfs_name_,
         const Poco::Util::AbstractConfiguration & config_)
         : hdfs_uri(hdfs_name_),
@@ -39,23 +45,61 @@ struct ReadBufferFromHDFS::ReadBufferFromHDFSImpl
         fin = hdfsOpenFile(fs.get(), path.c_str(), O_RDONLY, 0, 0, 0);
 
         if (fin == nullptr)
-            throw Exception("Unable to open HDFS file: " + path + " error: " + std::string(hdfsGetLastError()),
-                ErrorCodes::CANNOT_OPEN_FILE);
-    }
-
-    int read(char * start, size_t size) const
-    {
-        int bytes_read = hdfsRead(fs.get(), fin, start, size);
-        if (bytes_read < 0)
-            throw Exception("Fail to read HDFS file: " + hdfs_uri + " " + std::string(hdfsGetLastError()),
-                ErrorCodes::NETWORK_ERROR);
-        return bytes_read;
+            throw Exception(ErrorCodes::CANNOT_OPEN_FILE,
+                "Unable to open HDFS file: {}, error: {}", path, std::string(hdfsGetLastError()));
     }
 
     ~ReadBufferFromHDFSImpl()
     {
         std::lock_guard lock(hdfs_init_mutex);
         hdfsCloseFile(fs.get(), fin);
+    }
+
+    void initialize() const
+    {
+        if (!offset)
+            return;
+
+        int seek_status = hdfsSeek(fs.get(), fin, offset);
+        if (seek_status != 0)
+            throw Exception(ErrorCodes::CANNOT_SEEK_THROUGH_FILE, "Fail to seek HDFS file: {}, error: {}", hdfs_uri, std::string(hdfsGetLastError()));
+    }
+
+    int read(char * start, size_t size)
+    {
+        if (!initialized)
+        {
+            initialize();
+            initialized = true;
+        }
+
+        int bytes_read = hdfsRead(fs.get(), fin, start, size);
+        if (bytes_read < 0)
+            throw Exception("Fail to read HDFS file: " + hdfs_uri + " " + std::string(hdfsGetLastError()),
+                ErrorCodes::NETWORK_ERROR);
+
+        return bytes_read;
+    }
+
+    int seek(off_t offset_, int whence)
+    {
+        if (initialized)
+            throw Exception("Seek is allowed only before first read attempt from the buffer.", ErrorCodes::CANNOT_SEEK_THROUGH_FILE);
+
+        if (whence != SEEK_SET)
+            throw Exception("Only SEEK_SET mode is allowed.", ErrorCodes::CANNOT_SEEK_THROUGH_FILE);
+
+        if (offset_ < 0)
+            throw Exception(ErrorCodes::SEEK_POSITION_OUT_OF_BOUND, "Seek position is out of bounds. Offset: {}", std::to_string(offset_));
+
+        offset = offset_;
+
+        return offset;
+    }
+
+    int tell() const
+    {
+        return offset;
     }
 };
 
@@ -65,7 +109,7 @@ std::mutex ReadBufferFromHDFS::ReadBufferFromHDFSImpl::hdfs_init_mutex;
 ReadBufferFromHDFS::ReadBufferFromHDFS(const std::string & hdfs_name_,
     const Poco::Util::AbstractConfiguration & config_,
     size_t buf_size_)
-    : BufferWithOwnMemory<ReadBuffer>(buf_size_)
+    : BufferWithOwnMemory<SeekableReadBuffer>(buf_size_)
     , impl(std::make_unique<ReadBufferFromHDFSImpl>(hdfs_name_, config_))
 {
 }
@@ -80,6 +124,18 @@ bool ReadBufferFromHDFS::nextImpl()
     else
         return false;
     return true;
+}
+
+
+off_t ReadBufferFromHDFS::seek(off_t off, int whence)
+{
+    return impl->seek(off, whence);
+}
+
+
+off_t ReadBufferFromHDFS::getPosition()
+{
+    return impl->tell() + count();
 }
 
 }
