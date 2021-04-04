988f23c9005d32256bf0fc2c4c925db01cd4cabd
alesapin <alesapin@gmail.com>
2021-04-04 12:24:32 +0300

    Fix style

--- tests/jepsen.clickhouse-keeper/src/jepsen/clickhouse_keeper/utils.clj
+++ tests/jepsen.clickhouse-keeper/src/jepsen/clickhouse_keeper/utils.clj
@@ -194,10 +194,10 @@
         dest-symlink (str common-prefix "/" expected-file-name)
         wget-opts (concat cu/std-wget-opts [:-O dest-file])]
     (when-not (cu/exists? dest-file)
-       (info "Downloading" url)
-       (do (c/exec :mkdir :-p binaries-cache-dir)
-           (c/cd binaries-cache-dir
-                 (cu/wget-helper! wget-opts url))))
+      (info "Downloading" url)
+      (do (c/exec :mkdir :-p binaries-cache-dir)
+          (c/cd binaries-cache-dir
+                (cu/wget-helper! wget-opts url))))
     (c/exec :rm :-rf dest-symlink)
     (c/exec :ln :-s dest-file dest-symlink)
     dest-symlink))
