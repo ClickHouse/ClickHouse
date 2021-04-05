5b8da64c4eac52599e41e8ca4acc361d0de59a91
Nikolai Kochetov <nik-kochetov@yandex-team.ru>
2021-04-05 19:51:02 +0300

    Revert "Revert "Merge pull request #19771 from ClickHouse/thread_state_improvements""
    This reverts commit 7e5639ad69a08cfa86933b3968c2975d1a941e90.

--- src/Interpreters/DDLWorker.cpp
+++ src/Interpreters/DDLWorker.cpp
@@ -670,12 +670,14 @@ bool DDLWorker::tryExecuteQuery(const String & query, const DDLTask & task, Exec
     ReadBufferFromString istr(query_to_execute);
     String dummy_string;
     WriteBufferFromString ostr(dummy_string);
+    std::optional<CurrentThread::QueryScope> query_scope;
 
     try
     {
         current_context = std::make_unique<Context>(context);
         current_context->getClientInfo().query_kind = ClientInfo::QueryKind::SECONDARY_QUERY;
         current_context->setCurrentQueryId(""); // generate random query_id
+        query_scope.emplace(*current_context);
         executeQuery(istr, ostr, false, *current_context, {});
     }
     catch (...)
@@ -690,20 +692,6 @@ bool DDLWorker::tryExecuteQuery(const String & query, const DDLTask & task, Exec
     LOG_DEBUG(log, "Executed query: {}", query);
 
     return true;
-}
-
-void DDLWorker::attachToThreadGroup()
-{
-    if (thread_group)
-    {
-        /// Put all threads to one thread pool
-        CurrentThread::attachToIfDetached(thread_group);
-    }
-    else
-    {
-        CurrentThread::initializeQuery();
-        thread_group = CurrentThread::getGroup();
-    }
 }
 
 
@@ -1122,8 +1110,6 @@ void DDLWorker::runMainThread()
     {
         try
         {
-            attachToThreadGroup();
-
             cleanup_event->set();
             processTasks();
 
