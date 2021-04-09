21489fd289caa43525488fcdfa8fafc9a0050c39
robot-clickhouse <robot-clickhouse@yandex-team.ru>
2021-04-09 12:21:33 +0300

    Backport #22876 to 21.1: `mannWhitneyUTest` and `rankCorr` with window functions

--- src/AggregateFunctions/AggregateFunctionRankCorrelation.h
+++ src/AggregateFunctions/AggregateFunctionRankCorrelation.h
@@ -58,6 +58,8 @@ public:
         return "rankCorr";
     }
 
+    bool allocatesMemoryInArena() const override { return true; }
+
     DataTypePtr getReturnType() const override
     {
         return std::make_shared<DataTypeNumber<Float64>>();
