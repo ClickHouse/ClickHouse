76d1e8243a1c618d7eb71e2bc6bac5e5cb1903b5
kssenii <sumarokovakseniia@mail.ru>
2021-04-09 00:17:49 +0000

    Try fix test

--- tests/queries/0_stateless/01802_test_postgresql_protocol_with_row_policy.sh
+++ tests/queries/0_stateless/01802_test_postgresql_protocol_with_row_policy.sh
@@ -5,38 +5,39 @@ CUR_DIR=$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)
 . "$CUR_DIR"/../shell_config.sh
 
 echo "
-DROP TABLE IF EXISTS postgresql_protocol_with_row_policy;
-DROP ROW POLICY IF EXISTS test_policy ON postgresql_protocol_with_row_policy;
+CREATE DATABASE IF NOT EXISTS db01802;
+DROP TABLE IF EXISTS db01802.postgresql;
+DROP ROW POLICY IF EXISTS test_policy ON db01802.postgresql;
 
-CREATE TABLE postgresql_protocol_with_row_policy (val UInt32) ENGINE=MergeTree ORDER BY val;
-INSERT INTO postgresql_protocol_with_row_policy SELECT number FROM numbers(10);
+CREATE TABLE db01802.postgresql (val UInt32) ENGINE=MergeTree ORDER BY val;
+INSERT INTO db01802.postgresql SELECT number FROM numbers(10);
 
 SELECT 'before row policy';
+SELECT * FROM db01802.postgresql;
 " | $CLICKHOUSE_CLIENT -n
 
-psql --host localhost --port 9005 default -c "SELECT * FROM postgresql_protocol_with_row_policy;"
 
 echo "
-DROP USER IF EXISTS postgresql_protocol_user_1;
-CREATE USER postgresql_protocol_user_1 HOST IP '127.0.0.1' IDENTIFIED WITH no_password;
-GRANT SELECT(val) ON postgresql_protocol_with_row_policy TO postgresql_protocol_user_1;
-CREATE ROW POLICY IF NOT EXISTS test_policy ON postgresql_protocol_with_row_policy FOR SELECT USING val = 2 TO postgresql_protocol_user_1;
+DROP USER IF EXISTS postgresql_user;
+CREATE USER postgresql_user HOST IP '127.0.0.1' IDENTIFIED WITH no_password;
+GRANT SELECT(val) ON db01802.postgresql TO postgresql_user;
+CREATE ROW POLICY IF NOT EXISTS test_policy ON db01802.postgresql FOR SELECT USING val = 2 TO postgresql_user;
 
 SELECT '';
 SELECT 'after row policy with no password';
 " | $CLICKHOUSE_CLIENT -n
 
-psql --host localhost --port 9005 default --user postgresql_protocol_user_1 -c "SELECT * FROM postgresql_protocol_with_row_policy;"
+psql --host localhost --port 9005 db01802 --user postgresql_user -c "SELECT * FROM postgresql;"
 
 echo "
-DROP USER IF EXISTS postgresql_protocol_user_2;
-DROP ROW POLICY IF EXISTS test_policy ON postgresql_protocol_with_row_policy;
-CREATE USER postgresql_protocol_user_2 HOST IP '127.0.0.1' IDENTIFIED WITH plaintext_password BY 'qwerty';
-GRANT SELECT(val) ON postgresql_protocol_with_row_policy TO postgresql_protocol_user_2;
-CREATE ROW POLICY IF NOT EXISTS test_policy ON postgresql_protocol_with_row_policy FOR SELECT USING val = 2 TO postgresql_protocol_user_2;
+DROP USER IF EXISTS postgresql_user;
+DROP ROW POLICY IF EXISTS test_policy ON db01802.postgresql;
+CREATE USER postgresql_user HOST IP '127.0.0.1' IDENTIFIED WITH plaintext_password BY 'qwerty';
+GRANT SELECT(val) ON db01802.postgresql TO postgresql_user;
+CREATE ROW POLICY IF NOT EXISTS test_policy ON db01802.postgresql FOR SELECT USING val = 2 TO postgresql_user;
 
 SELECT 'after row policy with plaintext_password';
 " | $CLICKHOUSE_CLIENT -n
 
-psql "postgresql://postgresql_protocol_user_2:qwerty@localhost:9005/default" -c "SELECT * FROM postgresql_protocol_with_row_policy;"
+psql "postgresql://postgresql_user:qwerty@localhost:9005/db01802" -c "SELECT * FROM postgresql;"
 
