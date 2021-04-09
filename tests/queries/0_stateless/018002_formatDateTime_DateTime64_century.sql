c882d290bdd0afb97ac7b1369a044f331f20807a
Vasily Nemkov <V.Nemkov@gmail.com>
2021-04-09 19:22:53 +0300

    Fixed parsing string to DateTime64 for large values.
    Before fix:
    SELECT toDateTime64('2201-01-12 12:12:12', 3, 'UTC')
    1970-03-26 09:10:48.237
    
    After fix:
    SELECT toDateTime64('2201-01-12 12:12:12', 3, 'UTC')
    2201-01-12 12:12:12.000

--- tests/queries/0_stateless/018002_formatDateTime_DateTime64_century.sql
+++ tests/queries/0_stateless/018002_formatDateTime_DateTime64_century.sql
@@ -13,4 +13,4 @@ SELECT formatDateTime(toDateTime64('1969-12-12 12:12:12', 6, 'Europe/Moscow'), '
 SELECT formatDateTime(toDateTime64('1989-12-12 12:12:12', 6, 'Europe/Moscow'), '%C');
 SELECT formatDateTime(toDateTime64('2019-09-16 19:20:12', 0, 'Europe/Moscow'), '%C');
 SELECT formatDateTime(toDateTime64('2105-12-12 12:12:12', 6, 'Europe/Moscow'), '%C');
-SELECT formatDateTime(toDateTime64('2205-12-12 12:12:12', 6, 'Europe/Moscow'), '%C');
+SELECT formatDateTime(toDateTime64('2205-01-12 12:12:12', 6, 'Europe/Moscow'), '%C');
