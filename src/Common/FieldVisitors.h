5b286b68aa7d44b004e878361bed5bf99684dab8
Alexey Milovidov <milovidov@yandex-team.ru>
2021-04-11 00:52:42 +0300

    Update tests

--- src/Common/FieldVisitors.h
+++ src/Common/FieldVisitors.h
@@ -185,6 +185,10 @@ public:
                 /// Conversion of infinite values to integer is undefined.
                 throw Exception("Cannot convert infinite value to integer type", ErrorCodes::CANNOT_CONVERT_TYPE);
             }
+            else if (x > std::numeric_limits<T>::max() || x < std::numeric_limits<T>::lowest())
+            {
+                throw Exception("Cannot convert out of range floating point value to integer type", ErrorCodes::CANNOT_CONVERT_TYPE);
+            }
         }
 
         if constexpr (std::is_same_v<Decimal256, T>)
@@ -193,9 +197,6 @@ public:
         }
         else
         {
-            if (x > std::numeric_limits<T>::max() || x < std::numeric_limits<T>::lowest())
-                throw Exception("Cannot convert out of range floating point value to integer type", ErrorCodes::CANNOT_CONVERT_TYPE);
-
             return T(x);
         }
     }
