2ff8c85475653a5dfb15151a4aaf5a8cf27ca555
benbiti <wangshouben@hotmail.com>
2021-04-12 21:00:43 +0800

    updated: process libuv as cassandra

--- cmake/find/amqpcpp.cmake
+++ cmake/find/amqpcpp.cmake
@@ -9,6 +9,14 @@ if (NOT ENABLE_AMQPCPP)
     return()
 endif()
 
+
+if (NOT EXISTS "${ClickHouse_SOURCE_DIR}/contrib/libuv")
+    message (ERROR "submodule contrib/libuv is missing. to fix try run: \n git submodule update --init --recursive")
+    message (${RECONFIGURE_MESSAGE_LEVEL} "Can't find internal libuv needed for AMQP-CPP library")
+    set (USE_AMQPCPP 0)
+    return()
+endif()
+
 if (NOT EXISTS "${ClickHouse_SOURCE_DIR}/contrib/AMQP-CPP/CMakeLists.txt")
     message (WARNING "submodule contrib/AMQP-CPP is missing. to fix try run: \n git submodule update --init --recursive")
     message (${RECONFIGURE_MESSAGE_LEVEL} "Can't find internal AMQP-CPP library")
@@ -17,11 +25,26 @@ if (NOT EXISTS "${ClickHouse_SOURCE_DIR}/contrib/AMQP-CPP/CMakeLists.txt")
 endif ()
 
 set (USE_AMQPCPP 1)
-set (AMQPCPP_LIBRARY AMQP-CPP)
+set (AMQPCPP_LIBRARY amqp-cpp)
+
+if (MAKE_STATIC_LIBRARIES)
+    set (LIBUV_LIBRARY uv_a)
+else()
+    set (LIBUV_LIBRARY uv)
+endif()
+
+set (LIBUV_ROOT_DIR "${ClickHouse_SOURCE_DIR}/contrib/libuv")
+set (LIBUV_INCLUDE_DIR "${LIBUV_ROOT_DIR}/include")
 
 set (AMQPCPP_INCLUDE_DIR "${ClickHouse_SOURCE_DIR}/contrib/AMQP-CPP/include")
 list (APPEND AMQPCPP_INCLUDE_DIR
-        "${ClickHouse_SOURCE_DIR}/contrib/AMQP-CPP/include"
+        "${LIBUV_INCLUDE_DIR}"
         "${ClickHouse_SOURCE_DIR}/contrib/AMQP-CPP")
 
+list (APPEND AMQPCPP_LIBRARY  "${LIBUV_LIBRARY}")
+
+# Assign libuv include and libraries
+#set(CASS_LIBS ${CASS_LIBS} ${LIBUV_LIBRARIES})
+
 message (STATUS "Using AMQP-CPP=${USE_AMQPCPP}: ${AMQPCPP_INCLUDE_DIR} : ${AMQPCPP_LIBRARY}")
+message (STATUS "Using libuv: ${LIBUV_ROOT_DIR} : ${LIBUV_LIBRARY}")
