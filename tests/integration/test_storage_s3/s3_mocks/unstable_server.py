edd3931d4e17562cc06b76488125b212fae3ef65
Vladimir Chebotarev <vladimir.chebotarev@gmail.com>
2021-04-13 23:19:17 +0300

    Fixed tests.

--- tests/integration/test_storage_s3/s3_mocks/unstable_server.py
+++ tests/integration/test_storage_s3/s3_mocks/unstable_server.py
@@ -10,22 +10,14 @@ def gen_n_digit_number(n):
     assert 0 < n < 19
     return random.randint(10**(n-1), 10**n-1)
 
+
 def gen_line():
-    # Create fixed length CSV line.
     columns = 4
-    total_digits = digits_to_go = (columns-1)*19//2
-    columns_to_go = columns-1
 
     row = []
     def add_number():
-        nonlocal digits_to_go
-        nonlocal columns_to_go
-        min_digits = max(1, digits_to_go - (columns_to_go-1) * 18)
-        max_digits = min(18, digits_to_go - (columns_to_go-1))
-        digits = random.randint(min_digits, max_digits)
+        digits = random.randint(1, 18)
         row.append(gen_n_digit_number(digits))
-        columns_to_go -= 1
-        digits_to_go -= digits
 
     for i in range(columns // 2):
         add_number()
@@ -34,20 +26,21 @@ def gen_line():
         add_number()
 
     line = ",".join(map(str, row)) + "\n"
-    assert total_digits + 1 == len(line) - columns
     return line.encode()
 
 
-line = gen_line()
-random.seed('Unstable server/1.0')
+random.seed("Unstable server/1.0")
+lines = b"".join((gen_line() for _ in range(500000)))
+
 
 class RequestHandler(http.server.BaseHTTPRequestHandler):
     def do_HEAD(self):
         if self.path == "/root/test.csv":
             self.from_bytes = 0
-            self.end_bytes = len(line)*500000
+            self.end_bytes = len(lines)
             self.size = self.end_bytes
-            self.stop_at = random.randint(900000, 1200000) # Block size is 1024**2.
+            self.send_block_size = 256
+            self.stop_at = random.randint(900000, 1200000) // self.send_block_size # Block size is 1024**2.
 
             if "Range" in self.headers:
                 cr = self.headers["Range"]
@@ -78,16 +71,10 @@ class RequestHandler(http.server.BaseHTTPRequestHandler):
 
 
     def do_GET(self):
-        global line
-
         self.do_HEAD()
         if self.path == "/root/test.csv":
-            lines = 0
-            for c, i in enumerate(range(self.from_bytes, self.end_bytes)):
-                j = i % len(line)
-                self.wfile.write(line[j:j+1])
-                if line[j:j+1] == b'\n':
-                    line = gen_line()
+            for c, i in enumerate(range(self.from_bytes, self.end_bytes, self.send_block_size)):
+                self.wfile.write(lines[i:min(i+self.send_block_size, self.end_bytes)])
                 if (c + 1) % self.stop_at == 0:
                     #self.wfile._sock.setsockopt(socket.SOL_SOCKET, socket.SO_LINGER, struct.pack("ii", 0, 0))
                     #self.wfile._sock.shutdown(socket.SHUT_RDWR)
@@ -99,5 +86,5 @@ class RequestHandler(http.server.BaseHTTPRequestHandler):
             self.wfile.write(b"OK")
 
 
-httpd = http.server.HTTPServer(('0.0.0.0', int(sys.argv[1])), RequestHandler)
+httpd = http.server.HTTPServer(("0.0.0.0", int(sys.argv[1])), RequestHandler)
 httpd.serve_forever()
