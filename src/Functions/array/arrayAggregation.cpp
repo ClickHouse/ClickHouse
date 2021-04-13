e83a2e2a04fcdb2b410a0d079447dbc31d63d8d7
hexiaoting <hewenting_ict@163.com>
2021-04-13 09:53:39 +0800

    Fix error

--- src/Functions/array/arrayAggregation.cpp
+++ src/Functions/array/arrayAggregation.cpp
@@ -15,6 +15,7 @@ namespace ErrorCodes
 {
     extern const int ILLEGAL_TYPE_OF_ARGUMENT;
     extern const int ILLEGAL_COLUMN;
+    extern const int DECIMAL_OVERFLOW;
 }
 
 enum class AggregateOperation
