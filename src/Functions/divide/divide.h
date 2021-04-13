42412f9a085834f732772a9cc706bd7265c2b46a
Alexey Milovidov <milovidov@yandex-team.ru>
2021-04-13 21:59:55 +0300

    extern template

--- src/Functions/divide/divide.h
+++ src/Functions/divide/divide.h
@@ -1,4 +1,4 @@
 #include <cstddef>
 
 template <typename A, typename B, typename ResultType>
-void divideImpl(const A * __restrict a_pos, B b, ResultType * __restrict c_pos, size_t size);
+extern void divideImpl(const A * __restrict a_pos, B b, ResultType * __restrict c_pos, size_t size);
