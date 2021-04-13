add38c0e10ee75ae7db3cbe1d0eb932f89c4932d
Azat Khuzhin <a3at.mail@gmail.com>
2021-04-13 09:14:06 +0300

    Cover _use_count in 01660_system_parts_smoke

--- tests/queries/0_stateless/01660_system_parts_smoke.sql
+++ tests/queries/0_stateless/01660_system_parts_smoke.sql
@@ -1,18 +1,28 @@
--- There is different code path when:
--- - _state is not requested
--- - _state is requested
--- - only _state is requested
+-- There is different code path for the following cases:
 SELECT * FROM system.parts FORMAT Null;
 SELECT *, _state FROM system.parts FORMAT Null;
 SELECT _state FROM system.parts FORMAT Null;
+SELECT *, _use_count FROM system.parts FORMAT Null;
+SELECT *, _state, _use_count FROM system.parts FORMAT Null;
+SELECT _state, _use_count FROM system.parts FORMAT Null;
+SELECT _use_count FROM system.parts FORMAT Null;
+-- And same for system.parts_columns
+SELECT * FROM system.parts_columns FORMAT Null;
+SELECT *, _state FROM system.parts_columns FORMAT Null;
+SELECT _state FROM system.parts_columns FORMAT Null;
+SELECT *, _use_count FROM system.parts_columns FORMAT Null;
+SELECT *, _state, _use_count FROM system.parts_columns FORMAT Null;
+SELECT _state, _use_count FROM system.parts_columns FORMAT Null;
+SELECT _use_count FROM system.parts_columns FORMAT Null;
 
 -- Create one table and see some columns in system.parts
 DROP TABLE IF EXISTS data_01660;
 CREATE TABLE data_01660 (key Int) Engine=MergeTree() ORDER BY key;
 SYSTEM STOP MERGES data_01660;
+SELECT _state FROM system.parts WHERE database = currentDatabase() AND table = 'data_01660';
 
 -- Empty
-SELECT _state FROM system.parts WHERE database = currentDatabase() AND table = 'data_01660';
+SELECT _state, _use_count FROM system.parts WHERE database = currentDatabase() AND table = 'data_01660';
 SELECT name, _state FROM system.parts WHERE database = currentDatabase() AND table = 'data_01660';
 SELECT name, active FROM system.parts WHERE database = currentDatabase() AND table = 'data_01660';
 
@@ -20,7 +30,8 @@ SELECT name, active FROM system.parts WHERE database = currentDatabase() AND tab
 SELECT '# two parts';
 INSERT INTO data_01660 VALUES (0);
 INSERT INTO data_01660 VALUES (1);
-SELECT _state FROM system.parts WHERE database = currentDatabase() AND table = 'data_01660';
+-- NOTE that use_count is at least 2 (due to system.parts also holds one "reference")
+SELECT _state, _use_count FROM system.parts WHERE database = currentDatabase() AND table = 'data_01660';
 SELECT name, _state FROM system.parts WHERE database = currentDatabase() AND table = 'data_01660';
 SELECT name, active FROM system.parts WHERE database = currentDatabase() AND table = 'data_01660';
 
