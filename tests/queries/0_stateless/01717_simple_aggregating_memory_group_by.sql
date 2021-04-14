1ef02656b4d31ff978ff2ceccb6d7c4281359a70
Arthur Petukhovsky <petuhovskiy@yandex.ru>
2021-04-14 18:34:23 +0300

    Fix simple test

--- tests/queries/0_stateless/01717_simple_aggregating_memory_group_by.sql
+++ tests/queries/0_stateless/01717_simple_aggregating_memory_group_by.sql
@@ -1,22 +1,22 @@
-DROP TABLE IF EXISTS VisitsData;
-DROP TABLE IF EXISTS AggrMemory;
+DROP TABLE IF EXISTS aggr_memory;
+DROP TABLE IF EXISTS visits_data;
 
-CREATE TABLE VisitsData ( `UserID` UInt32, `Region` String, `Day` UInt8, `Age` UInt8, `Sex` UInt8 ) ENGINE = MergeTree() ORDER BY (Day, UserID);
+CREATE TABLE visits_data ( `UserID` UInt32, `Region` String, `Day` UInt8, `Age` UInt8, `Sex` UInt8 ) ENGINE = MergeTree() ORDER BY (Day, UserID);
 
-INSERT INTO VisitsData VALUES (1, 'Moscow', 1, 30, 2);
-INSERT INTO VisitsData VALUES (2, 'SPb', 1, 32, 2);
-INSERT INTO VisitsData VALUES (3, 'Moscow', 1, 22, 1);
-INSERT INTO VisitsData VALUES (4, 'Moscow', 1, 27, 2);
-INSERT INTO VisitsData VALUES (5, 'SPb', 1, 40, 0);
-INSERT INTO VisitsData VALUES (6, 'SPb', 1, 20, 2);
-INSERT INTO VisitsData VALUES (3, 'Moscow', 2, 22, 1);
-INSERT INTO VisitsData VALUES (5, 'SPb', 2, 40, 0);
-INSERT INTO VisitsData VALUES (2, 'SPb', 2, 32, 2);
-INSERT INTO VisitsData VALUES (1, 'Moscow', 2, 30, 2);
-INSERT INTO VisitsData VALUES (3, 'Moscow', 2, 22, 1);
+INSERT INTO visits_data VALUES (1, 'Moscow', 1, 30, 2);
+INSERT INTO visits_data VALUES (2, 'SPb', 1, 32, 2);
+INSERT INTO visits_data VALUES (3, 'Moscow', 1, 22, 1);
+INSERT INTO visits_data VALUES (4, 'Moscow', 1, 27, 2);
+INSERT INTO visits_data VALUES (5, 'SPb', 1, 40, 0);
+INSERT INTO visits_data VALUES (6, 'SPb', 1, 20, 2);
+INSERT INTO visits_data VALUES (3, 'Moscow', 2, 22, 1);
+INSERT INTO visits_data VALUES (5, 'SPb', 2, 40, 0);
+INSERT INTO visits_data VALUES (2, 'SPb', 2, 32, 2);
+INSERT INTO visits_data VALUES (1, 'Moscow', 2, 30, 2);
+INSERT INTO visits_data VALUES (3, 'Moscow', 2, 22, 1);
 
 
-CREATE TABLE AggrMemory
+CREATE TABLE aggr_memory
 ( `UserID` UInt32, `Region` String, `Day` UInt8, `Age` UInt8, `Sex` UInt8 ) 
 ENGINE = AggregatingMemory()
 AS SELECT
@@ -26,14 +26,14 @@ AS SELECT
     count() AS Visits,
     countIf(Sex = 1) AS Boys,
     uniq(UserID) AS Users
-FROM VisitsData
+FROM visits_data
 GROUP BY Region, Day
 ;
 
-INSERT INTO AggrMemory SELECT * FROM test.sample WHERE Sex = 2;
-INSERT INTO AggrMemory SELECT * FROM test.sample WHERE Sex != 2;
+INSERT INTO aggr_memory SELECT * FROM visits_data WHERE Sex = 2;
+INSERT INTO aggr_memory SELECT * FROM visits_data WHERE Sex != 2;
 
-SELECT * FROM AggrMemory;
+SELECT * FROM aggr_memory;
 
-DROP TABLE IF EXISTS Visits;
-DROP TABLE IF EXISTS AggrMemory;
+DROP TABLE IF EXISTS aggr_memory;
+DROP TABLE IF EXISTS visits_data;
