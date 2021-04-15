6d6a66210265defaa7e3b5c7a111fec9ec7db7d0
dankondr <dankondr@icloud.com>
2021-04-15 21:25:51 +0300

    Implemented for string column

--- src/Functions/leftPadString.cpp
+++ src/Functions/leftPadString.cpp
@@ -4,10 +4,22 @@
 #include <Functions/FunctionFactory.h>
 #include <Functions/IFunctionImpl.h>
 
+#include <Functions/FunctionHelpers.h>
+#include <Functions/FunctionsConversion.h>
+#include <Functions/castTypeToEither.h>
+#include <Functions/extractTimeZoneFromFunctionArguments.h>
+
+#include <IO/WriteHelpers.h>
+
+#include <Core/DecimalFunctions.h>
+#include <common/DateLUTImpl.h>
+#include <common/find_symbols.h>
+
+#include <type_traits>
+
 
 namespace DB
 {
-
 namespace ErrorCodes
 {
     extern const int ILLEGAL_COLUMN;
@@ -17,6 +29,62 @@ namespace ErrorCodes
 
 namespace
 {
+    struct LeftPadStringImpl
+    {
+        static void vector(
+            const ColumnString::Chars & data,
+            const ColumnString::Offsets & offsets,
+            const size_t length,
+            const String & padstr,
+            ColumnString::Chars & res_data,
+            ColumnString::Offsets & res_offsets)
+        {
+            size_t size = offsets.size();
+            res_data.resize((length + 1 /* zero terminator */) * size);
+            res_offsets.resize(size);
+
+            size_t padstr_size = padstr.size();
+
+            ColumnString::Offset prev_offset = 0;
+            ColumnString::Offset res_prev_offset = 0;
+            for (size_t i = 0; i < size; ++i)
+            {
+                size_t data_length = offsets[i] - prev_offset - 1 /* zero terminator */;
+                if (data_length < length)
+                {
+                    for (size_t j = 0; j < length - data_length; ++j)
+                        res_data[res_prev_offset + j] = padstr[j % padstr_size];
+                    for (size_t j = length - data_length; j < length; ++j)
+                        res_data[res_prev_offset + j] = data[prev_offset + j - length + data_length];
+                }
+                else
+                {
+                    for (size_t j = 0; j < length; ++j)
+                        res_data[res_prev_offset + j] = data[prev_offset + j];
+                }
+                res_data[res_prev_offset + length] = 0;
+                res_prev_offset += length + 1;
+                res_offsets[i] = res_prev_offset;
+            }
+        }
+
+        static void
+        vectorFixed(const ColumnString::Chars & data, const size_t n, const size_t length, const String & padstr, ColumnString::Chars & res_data)
+        {
+            res_data.resize(data.size());
+            size_t size = data.size() / n;
+            for (size_t i = 0; i < size; ++i)
+            {
+                if (length < size)
+                {
+                }
+                else
+                {
+                }
+            }
+        }
+    };
+
     class FunctionLeftPadString : public IFunction
     {
     public:
@@ -34,10 +102,10 @@ namespace
         {
             size_t number_of_arguments = arguments.size();
 
-            if (number_of_arguments != 3)
+            if (number_of_arguments != 2 && number_of_arguments != 3)
                 throw Exception(
                     "Number of arguments for function " + getName() + " doesn't match: passed " + toString(number_of_arguments)
-                        + ", should be 3",
+                        + ", should be 2 or 3",
                     ErrorCodes::NUMBER_OF_ARGUMENTS_DOESNT_MATCH);
 
             if (!isStringOrFixedString(arguments[0]))
@@ -50,20 +118,61 @@ namespace
                     "Illegal type " + arguments[1]->getName() + " of second argument of function " + getName(),
                     ErrorCodes::ILLEGAL_TYPE_OF_ARGUMENT);
 
-            if (!isStringOrFixedString(arguments[2]))
+            if (number_of_arguments == 3 && !isStringOrFixedString(arguments[2]))
                 throw Exception(
                     "Illegal type " + arguments[2]->getName() + " of third argument of function " + getName(),
                     ErrorCodes::ILLEGAL_TYPE_OF_ARGUMENT);
 
-            return std::make_shared<DataTypeString>();
+            return arguments[0];
         }
 
-        ColumnPtr executeImpl(const ColumnsWithTypeAndName & arguments, const DataTypePtr &, size_t input_rows_count) const override { }
-    };
+        ColumnPtr executeImpl(const ColumnsWithTypeAndName & arguments, const DataTypePtr &, size_t input_rows_count) const override
+        {
+            const ColumnConst * len_column = checkAndGetColumnConst<Column>(arguments[1].column.get());
+            if (!len_column)
+                throw Exception(
+                    "Illegal column " + arguments[1].column->getName() + " of first ('len') argument of function " + getName()
+                        + ". Must be a number.", // FIXME
+                    ErrorCodes::ILLEGAL_COLUMN);
+            size_t len = len_column->getValue<size_t>();
 
+            String padstr = " ";
+            if (arguments.size() == 3)
+            {
+                const ColumnConst * pad_column = checkAndGetColumnConst<ColumnString>(arguments[2].column.get());
+                if (!pad_column)
+                    throw Exception(
+                        "Illegal column " + arguments[2].column->getName() + " of second ('pad') argument of function " + getName()
+                            + ". Must be constant string.",
+                        ErrorCodes::ILLEGAL_COLUMN);
+
+                padstr = pad_column->getValue<String>();
+            }
+
+            const ColumnPtr str_column = arguments[0].column;
+            if (const ColumnString * strings = checkAndGetColumn<ColumnString>(str_column.get()))
+            {
+                auto col_res = ColumnString::create();
+                LeftPadStringImpl::vector(
+                    strings->getChars(), strings->getOffsets(), len, padstr, col_res->getChars(), col_res->getOffsets());
+                return col_res;
+            }
+//            else if (const ColumnFixedString * strings_fixed = checkAndGetColumn<ColumnFixedString>(str_column.get()))
+//            {
+//                auto col_res = ColumnFixedString::create(strings_fixed->getN());
+//                LeftPadStringImpl::vectorFixed(strings_fixed->getChars(), strings_fixed->getN(), len, padstr, col_res->getChars());
+//                return col_res;
+//            }
+            else
+                throw Exception(
+                    "Illegal column " + arguments[0].column->getName() + " of first ('str') argument of function " + getName()
+                        + ". Must be a string or fixed string.",
+                    ErrorCodes::ILLEGAL_COLUMN);
+        }
+    };
 }
 
-void registerFunctionPrependString(FunctionFactory & factory)
+void registerFunctionLeftPadString(FunctionFactory & factory)
 {
     factory.registerFunction<FunctionLeftPadString>(FunctionFactory::CaseInsensitive);
     factory.registerAlias("lpad", "leftPadString", FunctionFactory::CaseInsensitive);
