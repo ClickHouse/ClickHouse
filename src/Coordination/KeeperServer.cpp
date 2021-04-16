92809d858df75abce14dae4954581ba4a58b1732
alesapin <alesapin@gmail.com>
2021-04-16 21:35:03 +0300

    Fix typo

--- src/Coordination/KeeperServer.cpp
+++ src/Coordination/KeeperServer.cpp
@@ -24,6 +24,7 @@ namespace ErrorCodes
     extern const int RAFT_ERROR;
     extern const int NO_ELEMENTS_IN_CONFIG;
     extern const int SUPPORT_IS_DISABLED;
+    extern const int LOGICAL_ERROR;
 }
 
 namespace
