9679bdd86e57d457e8e81e03111ed5d8f4ac9e3e
Maksim Kita <maksim-kita@yandex-team.ru>
2021-04-16 16:16:11 +0300

    Remove explicit database creation in tests

--- tests/queries/0_stateless/01813_use_local_shard_if_no_remote_connections.sql
+++ tests/queries/0_stateless/01813_use_local_shard_if_no_remote_connections.sql
@@ -1,19 +1,16 @@
-DROP DATABASE IF EXISTS 01813_db;
-CREATE DATABASE 01813_db;
+DROP TABLE IF EXISTS data;
+CREATE TABLE data (a Int64, b Int64) ENGINE = TinyLog();
 
-DROP TABLE IF EXISTS 01813_db.data;
-CREATE TABLE 01813_db.data (a Int64, b Int64) ENGINE = TinyLog();
+DROP TABLE IF EXISTS data_distributed;
+CREATE TABLE data_distributed (a Int64, b Int64) ENGINE = Distributed(test_shard_localhost, '', data);
 
-DROP TABLE IF EXISTS 01813_db.data_distributed;
-CREATE TABLE 01813_db.data_distributed (a Int64, b Int64) ENGINE = Distributed(test_shard_localhost, '01813_db', data);
-
-INSERT INTO 01813_db.data VALUES (0, 0);
+INSERT INTO data VALUES (0, 0);
 
 SET prefer_localhost_replica = 1;
-SELECT a / (SELECT sum(number) FROM numbers(10)) FROM 01813_db.data_distributed;
+SELECT a / (SELECT sum(number) FROM numbers(10)) FROM data_distributed;
 
 SET prefer_localhost_replica = 0;
-SELECT a / (SELECT sum(number) FROM numbers(10)) FROM 01813_db.data_distributed;
+SELECT a / (SELECT sum(number) FROM numbers(10)) FROM data_distributed;
 
-DROP TABLE 01813_db.data_distributed;
-DROP TABLE 01813_db.data;
+DROP TABLE data_distributed;
+DROP TABLE data;
