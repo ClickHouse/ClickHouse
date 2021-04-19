0420b7bb339f1eafa750408aa4e12c7d50efe150
Nikolai Kochetov <nik-kochetov@yandex-team.ru>
2021-04-19 17:25:43 +0300

    Reset replica state in MultiplexedConnections in case of exception.

--- tests/queries/0_stateless/01822_async_read_from_socket_crash.sh
+++ tests/queries/0_stateless/01822_async_read_from_socket_crash.sh
@@ -6,4 +6,4 @@ CURDIR=$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)
 
 
 
-for _ in {1..10}; do $CLICKHOUSE_CLIENT -q "select number from remote('127.0.0.{2,3}', numbers(20)) limit 8 settings max_block_size = 2, unknown_packet_in_send_data=4, sleep_in_send_data_ms=100, async_socket_for_remote=1 format Null" 2>&1 > /dev/null; done
+for _ in {1..10}; do $CLICKHOUSE_CLIENT -q "select number from remote('127.0.0.{2,3}', numbers(20)) limit 8 settings max_block_size = 2, unknown_packet_in_send_data=4, sleep_in_send_data_ms=100, async_socket_for_remote=1 format Null" > /dev/null 2>&1 || true; done
