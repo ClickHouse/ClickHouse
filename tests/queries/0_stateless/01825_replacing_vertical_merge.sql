a75f9c3ca959a4ec1a77762e6d7ffb20ae141c4a
Anton Popov <pad11rus@gmail.com>
2021-04-23 00:54:45 +0300

    add 'stop merges' to test

--- tests/queries/0_stateless/01825_replacing_vertical_merge.sql
+++ tests/queries/0_stateless/01825_replacing_vertical_merge.sql
@@ -10,10 +10,14 @@ SETTINGS vertical_merge_algorithm_min_rows_to_activate = 1,
     min_bytes_for_wide_part = 0,
     merge_max_block_size = 16;
 
+SYSTEM STOP MERGES replacing_table;
+
 INSERT INTO replacing_table SELECT number, number, number from numbers(16);
 INSERT INTO replacing_table SELECT 100, number, number from numbers(16);
 
 SELECT sum(a), count() FROM replacing_table;
+
+SYSTEM START MERGES replacing_table;
 
 OPTIMIZE TABLE replacing_table FINAL;
 
