55d2627a2770ed45b0080b1c560872fc9dbfc953
kssenii <sumarokovakseniia@mail.ru>
2021-04-23 04:16:36 +0000

    Fix test

--- tests/queries/0_stateless/01846_null_as_default_for_insert_select.sql
+++ tests/queries/0_stateless/01846_null_as_default_for_insert_select.sql
@@ -2,14 +2,14 @@ DROP TABLE IF EXISTS test_null_as_default;
 CREATE TABLE test_null_as_default (s String DEFAULT 'WORLD') ENGINE = Memory;
 
 INSERT INTO test_null_as_default SELECT 'HELLO' UNION ALL SELECT NULL;
-SELECT * FROM test_null_as_default;
+SELECT * FROM test_null_as_default ORDER BY s;
 SELECT '';
 
 INSERT INTO test_null_as_default SELECT NULL;
-SELECT * FROM test_null_as_default;
+SELECT * FROM test_null_as_default ORDER BY s;
 SELECT '';
 
 REPLACE TABLE test_null_as_default (s String DEFAULT 'WORLD', ss String DEFAULT 'PEOPLE') ENGINE = Memory;
 
 INSERT INTO test_null_as_default(s) SELECT 'HELLO' UNION ALL SELECT NULL;
-SELECT * FROM test_null_as_default;
+SELECT * FROM test_null_as_default ORDER BY s;
