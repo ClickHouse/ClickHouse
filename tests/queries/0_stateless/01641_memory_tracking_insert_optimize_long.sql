e20a6e0f9618e20a711f82b1626be4f0b1c16926
alexey-milovidov <milovidov@yandex-team.ru>
2021-04-25 11:05:59 +0300

    Update 01641_memory_tracking_insert_optimize_long.sql

--- tests/queries/0_stateless/01641_memory_tracking_insert_optimize_long.sql
+++ tests/queries/0_stateless/01641_memory_tracking_insert_optimize_long.sql
@@ -2,7 +2,8 @@ drop table if exists data_01641;
 
 create table data_01641 (key Int, value String) engine=MergeTree order by (key, repeat(value, 10)) settings old_parts_lifetime=0, min_bytes_for_wide_part=0;
 
-insert into data_01641 select number, toString(number) from numbers(120e6);
+SET max_block_size = 1000, min_insert_block_size_rows = 0, min_insert_block_size_bytes = 0;
+insert into data_01641 select number, toString(number) from numbers(120000);
 
 -- Definitely should fail and it proves that memory is tracked in OPTIMIZE query.
 set max_memory_usage='10Mi';
