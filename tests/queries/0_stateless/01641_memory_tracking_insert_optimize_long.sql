9fee1e5a106ce0821c3f1ed36102340727feb188
Alexey Milovidov <milovidov@yandex-team.ru>
2021-04-25 06:24:39 +0300

    Fix bad test 01641_memory_tracking_insert_optimize_long

--- tests/queries/0_stateless/01641_memory_tracking_insert_optimize_long.sql
+++ tests/queries/0_stateless/01641_memory_tracking_insert_optimize_long.sql
@@ -2,20 +2,10 @@ drop table if exists data_01641;
 
 create table data_01641 (key Int, value String) engine=MergeTree order by (key, repeat(value, 10)) settings old_parts_lifetime=0, min_bytes_for_wide_part=0;
 
--- peak memory usage is 170MiB
-set max_memory_usage='200Mi';
-system stop merges data_01641;
 insert into data_01641 select number, toString(number) from numbers(120e6);
 
--- peak:
--- - is 21MiB if background merges already scheduled
--- - is ~60MiB otherwise
-set max_memory_usage='80Mi';
-system start merges data_01641;
-optimize table data_01641 final;
-
--- definitely should fail
-set max_memory_usage='1Mi';
+-- Definitely should fail and it proves that memory is tracked in OPTIMIZE query.
+set max_memory_usage='10Mi';
 optimize table data_01641 final; -- { serverError 241 }
 
 drop table data_01641;
