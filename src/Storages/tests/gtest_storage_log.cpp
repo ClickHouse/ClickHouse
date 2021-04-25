44fbfef532e580eff0af5f25e441f3c8269c7a65
feng lv <fenglv15@mails.ucas.ac.cn>
2021-04-25 06:51:21 +0000

    fix tests build

--- src/Storages/tests/gtest_storage_log.cpp
+++ src/Storages/tests/gtest_storage_log.cpp
@@ -33,7 +33,7 @@ DB::StoragePtr createStorage(DB::DiskPtr & disk)
     names_and_types.emplace_back("a", std::make_shared<DataTypeUInt64>());
 
     StoragePtr table = StorageLog::create(
-        disk, "table/", StorageID("test", "test"), ColumnsDescription{names_and_types}, ConstraintsDescription{}, false, 1048576);
+        disk, "table/", StorageID("test", "test"), ColumnsDescription{names_and_types}, ConstraintsDescription{}, String{}, false, 1048576);
 
     table->startup();
 
