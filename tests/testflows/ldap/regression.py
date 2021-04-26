21a11441553a34c39f210e774fdc72a7d1a604c5
Vitaliy Zakaznikov <vzakaznikov@protonmail.com>
2021-04-26 13:58:32 -0400

    Debugging fails in LDAP tests.

--- tests/testflows/ldap/regression.py
+++ tests/testflows/ldap/regression.py
@@ -13,10 +13,12 @@ def regression(self, local, clickhouse_binary_path, parallel=None, stress=None):
     """ClickHouse LDAP integration regression module.
     """
     args = {"local": local, "clickhouse_binary_path": clickhouse_binary_path}
-
-    Feature(test=load("ldap.authentication.regression", "regression"))(**args)
-    Feature(test=load("ldap.external_user_directory.regression", "regression"))(**args)
-    Feature(test=load("ldap.role_mapping.regression", "regression"))(**args)
+    
+    for i in range(10):
+        with Module(f"run {i}"):
+            Feature(test=load("ldap.authentication.regression", "regression"))(**args)
+            Feature(test=load("ldap.external_user_directory.regression", "regression"))(**args)
+            Feature(test=load("ldap.role_mapping.regression", "regression"))(**args)
 
 if main():
     regression()
