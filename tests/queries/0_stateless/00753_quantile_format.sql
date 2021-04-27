4553c52f808fc037981b79156681c5c14c427758
RedClusive <nia4859@ya.ru>
2021-04-27 08:44:48 +0000

    fix test

--- tests/queries/0_stateless/00753_quantile_format.sql
+++ tests/queries/0_stateless/00753_quantile_format.sql
@@ -1,6 +1,6 @@
 DROP TABLE IF EXISTS datetime;
 
-CREATE TABLE datetime (d DateTime) ENGINE = Memory;
+CREATE TABLE datetime (d DateTime('UTC')) ENGINE = Memory;
 INSERT INTO datetime(d) VALUES(toDateTime('2016-06-15 23:00:00'));
 
 SELECT quantile(0.2)(d) FROM datetime;
