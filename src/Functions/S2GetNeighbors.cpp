e0d10706ba09e12b322695ba55416c5f83a801da
Andr0901 <andr09012002@gmail.com>
2021-04-29 01:16:45 +0300

    Initial commit

---
+++ src/Functions/S2GetNeighbors.cpp
@@ -0,0 +1,109 @@
+#include "config_functions.h"
+
+#include <Columns/ColumnArray.h>
+#include <Columns/ColumnsNumber.h>
+#include <DataTypes/DataTypesNumber.h>
+#include <DataTypes/DataTypeArray.h>
+#include <Functions/FunctionFactory.h>
+#include <Functions/IFunctionImpl.h>
+#include <Common/typeid_cast.h>
+#include <ext/range.h>
+
+#include "../contrib/s2geometry/src/s2/s2latlng.h"
+#include "../contrib/s2geometry/src/s2/s2cell_id.h"
+#include "../contrib/s2geometry/src/s2/s2point.h"
+
+namespace DB 
+{
+
+namespace ErrorCodes
+{
+    extern const int ILLEGAL_TYPE_OF_ARGUMENT;
+    extern const int NUMBER_OF_ARGUMENTS_DOESNT_MATCH;
+}
+
+namespace
+{
+
+/// TODO: Comment this
+class FunctionS2GetNeighbors : public IFunction
+{
+public:
+    static constexpr auto name = "S2GetNeighbors";
+
+    static FunctionPtr create(ContextPtr)
+    {
+        return std::make_shared<FunctionS2GetNeighbors>();
+    }
+
+    std::string getName() const override
+    {
+        return name;
+    }
+
+    size_t getNumberOfArguments() const override { return 1; }
+
+    bool useDefaultImplementationForConstants() const override { return true; }
+
+    DataTypePtr getReturnTypeImpl(const DataTypes & arguments) const override
+    {
+        size_t number_of_arguments = arguments.size();
+
+        if (number_of_arguments != 1) {
+            throw Exception("Number of arguments for function " + getName() + " doesn't match: passed "
+                + toString(number_of_arguments) + ", should be 2",
+                ErrorCodes::NUMBER_OF_ARGUMENTS_DOESNT_MATCH);
+        }
+
+        const auto * arg = arguments[0].get();
+
+        if (!WhichDataType(arg).isUInt64()) {
+            throw Exception(
+                "Illegal type " + arg->getName() + " of argument " + std::to_string(1) + " of function " + getName() + ". Must be Float64",
+                ErrorCodes::ILLEGAL_TYPE_OF_ARGUMENT);
+        }
+
+        return std::make_shared<DataTypeArray>(std::make_shared<DataTypeUInt64>());
+    }
+
+    ColumnPtr executeImpl(const ColumnsWithTypeAndName & arguments, const DataTypePtr &, size_t input_rows_count) const override
+    {
+        const auto * col_id = arguments[0].column.get();
+
+        auto dst = ColumnArray::create(ColumnUInt64::create());
+        auto & dst_data = dst->getData();
+        auto & dst_offsets = dst->getOffsets();
+        dst_offsets.resize(input_rows_count);
+        auto current_offset = 0;
+
+        for (const auto row : ext::range(0, input_rows_count))
+        {
+            const UInt64 id = col_id->getUInt(row);
+        
+            S2CellId cell_id(id);
+            S2CellId neighbors[4];
+            cell_id.GetEdgeNeighbors(neighbors);
+
+            dst_data.reserve(dst_data.size() + 4);
+            for (int i = 0; i < 4; ++i)
+            {
+                ++current_offset;
+                dst_data.insert(neighbors[i].id());
+            }
+            dst_offsets[row] = current_offset;
+        }
+
+        return dst;
+    }
+
+};
+
+}
+
+void registerFunctionS2GetNeighbors(FunctionFactory & factory)
+{
+    factory.registerFunction<FunctionS2GetNeighbors>();
+}
+
+
+}
---
+++ src/Functions/radiansToS2.cpp
@@ -0,0 +1,110 @@
+#include "config_functions.h"
+
+#include <Columns/ColumnsNumber.h>
+#include <DataTypes/DataTypesNumber.h>
+#include <Functions/FunctionFactory.h>
+#include <Functions/IFunctionImpl.h>
+#include <Common/typeid_cast.h>
+#include <ext/range.h>
+
+#include "../contrib/s2geometry/src/s2/s2latlng.h"
+#include "../contrib/s2geometry/src/s2/s2cell_id.h"
+
+class S2CellId;
+
+namespace DB 
+{
+
+namespace ErrorCodes
+{
+    extern const int ILLEGAL_TYPE_OF_ARGUMENT;
+    extern const int NUMBER_OF_ARGUMENTS_DOESNT_MATCH;
+}
+
+namespace
+{
+
+/// TODO: Comment this
+class FunctionRadiansToS2 : public IFunction
+{
+public:
+    static constexpr auto name = "radiansToS2";
+
+    static FunctionPtr create(ContextPtr)
+    {
+        return std::make_shared<FunctionRadiansToS2>();
+    }
+
+    std::string getName() const override
+    {
+        return name;
+    }
+
+    size_t getNumberOfArguments() const override { return 2; }
+
+    bool useDefaultImplementationForConstants() const override { return true; }
+
+    DataTypePtr getReturnTypeImpl(const DataTypes & arguments) const override
+    {
+        size_t number_of_arguments = arguments.size();
+
+        if (number_of_arguments != 2) {
+            throw Exception("Number of arguments for function " + getName() + " doesn't match: passed "
+                + toString(number_of_arguments) + ", should be 2",
+                ErrorCodes::NUMBER_OF_ARGUMENTS_DOESNT_MATCH);
+        }
+
+        const auto * arg = arguments[0].get();
+
+        if (!WhichDataType(arg).isFloat64()) {
+            throw Exception(
+                "Illegal type " + arg->getName() + " of argument " + std::to_string(1) + " of function " + getName() + ". Must be Float64",
+                ErrorCodes::ILLEGAL_TYPE_OF_ARGUMENT);
+        }
+
+        arg = arguments[1].get();
+        if (!WhichDataType(arg).isFloat64()) {
+            throw Exception(
+                "Illegal type " + arg->getName() + " of argument " + std::to_string(2) + " of function " + getName() + ". Must be Float64",
+                ErrorCodes::ILLEGAL_TYPE_OF_ARGUMENT);
+        }
+
+        return std::make_shared<DataTypeUInt64>();
+    }
+
+    ColumnPtr executeImpl(const ColumnsWithTypeAndName & arguments, const DataTypePtr &, size_t input_rows_count) const override
+    {
+        const auto * col_lon = arguments[0].column.get();
+        const auto * col_lat = arguments[1].column.get();
+
+        auto dst = ColumnVector<UInt64>::create();
+        auto & dst_data = dst->getData();
+        dst_data.resize(input_rows_count);
+
+        for (const auto row : ext::range(0, input_rows_count))
+        {
+            const double lon = col_lon->getFloat64(row);
+            double lat = col_lat->getFloat64(row);
+
+            lat = lon;
+        
+            S2LatLng lat_lng = S2LatLng::FromRadians(lat, lon);
+            S2CellId id(lat_lng);
+
+            dst_data[row] = id.id();
+        }
+
+        return dst;
+    }
+
+};
+
+}
+
+void registerRadiansDegreesToS2(FunctionFactory & factory)
+{
+    factory.registerFunction<FunctionRadiansToS2>();
+}
+
+
+}
