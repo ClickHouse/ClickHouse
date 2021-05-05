80ffcbcf86e6496244944ff21a28ac470a4ba4a6
Alexey Milovidov <milovidov@yandex-team.ru>
2021-05-05 23:56:44 +0300

    Fixed error; add a test

--- base/common/wide_integer_impl.h
+++ base/common/wide_integer_impl.h
@@ -1127,7 +1127,7 @@ constexpr integer<Bits, Signed>::operator T() const noexcept
     using UnsignedT = std::make_unsigned_t<T>;
 
     UnsignedT res{};
-    for (unsigned i = 0; i < _impl::item_count && i <= sizeof(T) / sizeof(base_type); ++i)
+    for (unsigned i = 0; i < _impl::item_count && i < (sizeof(T) + sizeof(base_type) - 1) / sizeof(base_type); ++i)
         res += UnsignedT(items[i]) << (sizeof(base_type) * 8 * i);
 
     return res;
