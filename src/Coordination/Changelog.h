1e23a2b412c20d14c933e1e91078d8ad15a74e91
Alexey Milovidov <milovidov@yandex-team.ru>
2021-05-08 23:50:12 +0300

    Fix warnings by PVS-Studio

--- src/Coordination/Changelog.h
+++ src/Coordination/Changelog.h
@@ -32,7 +32,7 @@ struct ChangelogRecordHeader
     ChangelogVersion version = CURRENT_CHANGELOG_VERSION;
     uint64_t index = 0; /// entry log number
     uint64_t term = 0;
-    nuraft::log_val_type value_type;
+    nuraft::log_val_type value_type{};
     uint64_t blob_size = 0;
 };
 
