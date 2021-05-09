fde6c8874029e07bee7c3e9370aca5cff2388560
Alexey Milovidov <milovidov@yandex-team.ru>
2021-05-09 19:01:38 +0300

    Merge with master

--- src/Functions/array/arrayDifference.cpp
+++ src/Functions/array/arrayDifference.cpp
@@ -70,7 +70,10 @@ struct ArrayDifferenceImpl
                     using ResultNativeType = typename Result::NativeType;
 
                     ResultNativeType result_value;
-                    bool overflow = common::subOverflow(static_cast<ResultNativeType>(curr.value), static_cast<ResultNativeType>(prev), result_value);
+                    bool overflow = common::subOverflow(
+                        static_cast<ResultNativeType>(curr.value),
+                        static_cast<ResultNativeType>(prev.value),
+                        result_value);
                     if (overflow)
                         throw Exception(ErrorCodes::DECIMAL_OVERFLOW, "Decimal math overflow");
 
