5b9c45add557b74459020f0fb7a4fd80fb83dd2f
万康 <mingge@xiaohongshu.com>
2021-05-11 12:18:11 +0800

    add tests

---
+++ tests/queries/0_stateless/01866_bitposition.sql
@@ -0,0 +1,16 @@
+select bitpositionToArray(0);
+select bitpositionToArray(1);
+select bitpositionToArray(-1);
+select bitpositionToArray(127);
+select bitpositionToArray(128);
+
+select bitpositionToArray(toInt16(0));
+select bitpositionToArray(toInt16(1));
+select bitpositionToArray(toInt16(-1));
+select bitpositionToArray(toInt16(32765));
+select bitpositionToArray(toInt16(32768));
+
+select bitpositionToArray(toInt64(0));
+select bitpositionToArray(toInt64(1));
+select bitpositionToArray(toInt64(-1));
+
