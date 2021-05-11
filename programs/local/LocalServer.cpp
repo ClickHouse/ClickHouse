9d4408c027c5a87dc0b924cf68226bb5b53d77d3
Amesaru <1v9always@mail.ru>
2021-05-11 20:14:09 +0300

    LocalServer.cpp fix

--- programs/local/LocalServer.cpp
+++ programs/local/LocalServer.cpp
@@ -413,7 +413,7 @@ void LocalServer::processQueries()
         progress.reset();
         progress_bar.show_progress_bar = false;
         progress_bar.written_progress_chars = 0;
-        progress_bar.written_first_block = false;;
+        progress_bar.written_first_block = false;
 
 
         ReadBufferFromString read_buf(query);
