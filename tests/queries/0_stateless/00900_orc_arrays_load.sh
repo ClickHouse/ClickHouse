b84c3daab4fe54370015952f7813977d27f950d2
Pavel Kruglov <avogar@avogar-dev.sas.yp-c.yandex.net>
2021-05-13 00:48:06 +0300

    Try to fix tests

--- tests/queries/0_stateless/00900_orc_arrays_load.sh
+++ tests/queries/0_stateless/00900_orc_arrays_load.sh
@@ -7,9 +7,8 @@ CUR_DIR=$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)
 DATA_FILE=$CUR_DIR/data_orc/array_test.orc
 
 ${CLICKHOUSE_CLIENT} --query="DROP TABLE IF EXISTS orc_load"
-${CLICKHOUSE_CLIENT} --query="CREATE TABLE orc_arrays (a1 Array(Int8), a2 Array(UInt8), a3 Array(Int16), a4 Array(UInt16), a5 Array(Int32), a6 Array(UInt32), a7 Array(Int64), a8 Array(UInt64), a9 Array(String), a10 Array(FixedString(4)), a11 Array(Float32), a12 Array(Float64), a13 Array(Date), a14 Array(Datetime), a15 Array(Decimal(4, 2)), a16 Array(Decimal(10, 2)), a17 Array(Decimal(25, 2))) ENGINE=Memory()"
+${CLICKHOUSE_CLIENT} --query="CREATE TABLE orc_arrays (arr1 Array(Int8), arr2 Array(UInt8), arr3 Array(Int16), arr4 Array(UInt16), arr5 Array(Int32), arr6 Array(UInt32), arr7 Array(Int64), arr8 Array(UInt64), arr9 Array(String), arr10 Array(FixedString(4)), arr11 Array(Float32), arr12 Array(Float64), arr13 Array(Date), arr14 Array(Datetime), arr15 Array(Decimal(4, 2)), arr16 Array(Decimal(10, 2)), arr17 Array(Decimal(25, 2))) ENGINE=Memory()"
 cat "$DATA_FILE"  | ${CLICKHOUSE_CLIENT} -q "insert into orc_arrays format ORC"
-timeout 3 ${CLICKHOUSE_CLIENT} -q "insert into orc_arrays format ORC" < $DATA_FILE
 ${CLICKHOUSE_CLIENT} --query="select * from orc_arrays"
 
 ${CLICKHOUSE_CLIENT} --query="drop table orc_arrays"
