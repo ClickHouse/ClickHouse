e02b8dda8060a79f6e2bc94da9c8b3ecd91e13c1
ANDREI STAROVEROV <staroverovad@sandbox-407923068>
2021-05-14 00:45:00 +0300

    Change error code in test 01854_create_simple_type_when_type_has_already_exist.sql

--- tests/queries/0_stateless/01854_create_simple_type_when_type_has_already_exist.sql
+++ tests/queries/0_stateless/01854_create_simple_type_when_type_has_already_exist.sql
@@ -1,5 +1,5 @@
 create type MyType3 as int;
 
-create type MyType3 as String; -- { serverError 582 }
+create type MyType3 as String; -- { serverError 585 }
 
-create type String as int; -- { serverError 582 }
+create type String as int; -- { serverError 585 }
