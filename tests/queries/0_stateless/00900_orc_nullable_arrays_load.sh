7a0065cb818535087b6032f2929e90e8f32afed8
Pavel Kruglov <avogar@avogar-dev.sas.yp-c.yandex.net>
2021-05-13 10:20:24 +0300

    Change tests

--- tests/queries/0_stateless/00900_orc_nullable_arrays_load.sh
+++ tests/queries/0_stateless/00900_orc_nullable_arrays_load.sh
@@ -4,11 +4,12 @@ CUR_DIR=$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)
 # shellcheck source=../shell_config.sh
 . "$CUR_DIR"/../shell_config.sh
 
-DATA_FILE=$CUR_DIR/data_orc/nullable_array_test.orc
-
 ${CLICKHOUSE_CLIENT} --query="DROP TABLE IF EXISTS orc_nullable_arrays"
 ${CLICKHOUSE_CLIENT} --query="CREATE TABLE orc_nullable_arrays (arr1 Array(Nullable(UInt32)), arr2 Array(Nullable(String)), arr3 Array(Nullable(Decimal(4, 2)))) ENGINE=Memory()"
-cat "$DATA_FILE"  | ${CLICKHOUSE_CLIENT} -q "insert into orc_nullable_arrays format ORC"
-${CLICKHOUSE_CLIENT} --query="select * from orc_nullable_arrays"
+${CLICKHOUSE_CLIENT} --query="INSERT INTO orc_nullable_arrays VALUES ([1,NULL,2],[NULL,'Some string',NULL],[0.00,NULL,42.42]),([NULL],[NULL],[NULL]),([],[],[])"
+${CLICKHOUSE_CLIENT} --query="SELECT * FROM orc_nullable_arrays FORMAT ORC" > "${CLICKHOUSE_TMP}"/nullable_arrays.orc
 
-${CLICKHOUSE_CLIENT} --query="drop table orc_nullable_arrays"
+cat "${CLICKHOUSE_TMP}"/nullable_arrays.orc | ${CLICKHOUSE_CLIENT} -q "INSERT INTO orc_nullable_arrays FORMAT ORC"
+
+${CLICKHOUSE_CLIENT} --query="SELECT * FROM orc_nullable_arrays"
+${CLICKHOUSE_CLIENT} --query="DROP TABLE orc_nullable_arrays"
