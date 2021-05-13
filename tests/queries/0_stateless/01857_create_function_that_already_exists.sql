ee3d488337fae3c42435cb86ab24066d82f012eb
ANDREI STAROVEROV <staroverovad@sandbox-407923068>
2021-05-14 00:43:02 +0300

    Change error code in test 01857_create_function_that_already_exists.sql

--- tests/queries/0_stateless/01857_create_function_that_already_exists.sql
+++ tests/queries/0_stateless/01857_create_function_that_already_exists.sql
@@ -1,2 +1,3 @@
 create function MyFunc3 as (a, b) -> a + b;
-create function MyFunc3 as (a) -> a || '!!!'; -- { serverError 49 }
+create function MyFunc3 as (a) -> a || '!!!'; -- { serverError 585 }
+create function cast as x -> x + 1; -- { serverError 585 }
