542f7c46d3effc20e9b9ef0ee72dca2050d61856
Vitaliy Zakaznikov <vzakaznikov@protonmail.com>
2021-05-14 16:06:12 -0400

    Adding experimental tests/queries/0_stateless/01900_alter_race_condition_stress.sh test.

---
+++ tests/queries/0_stateless/01900_alter_race_condition_stress.sh
@@ -0,0 +1,33 @@
+#!/usr/bin/env bash
+
+CURDIR=$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)
+# shellcheck source=../shell_config.sh
+. "$CURDIR"/../shell_config.sh
+
+N=100
+exitcode=0
+
+for i in $(seq 1 $N)
+do
+   ${CURDIR}/01900_alter_race_condition.sh.disabled > ${CURDIR}/01900_alter_race_condition_${i}.stdout 2>&1 &
+done
+
+wait
+
+for i in $(seq 1 $N)
+do
+   diff ${CURDIR}/01900_alter_race_condition_${i}.stdout ${CURDIR}/01900_alter_race_condition.reference > /dev/null 2>&1
+   rm -rf ${CURDIR}/01900_alter_race_condition_${i}.stdout
+   if [ $? -ne 0 ]; then
+      exitcode=1
+   fi
+done
+
+if [ $exitcode -ne 0 ]; then
+   echo "FAIL"
+   exit 1
+fi
+
+echo "OK"
+exit 0
+
