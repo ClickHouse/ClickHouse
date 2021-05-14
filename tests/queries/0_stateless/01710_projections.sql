a113acc40c570aad2ea9200ef0cd279881a43094
Amos Bird <amosbird@gmail.com>
2021-05-14 22:26:09 +0800

    Fix empty key

--- tests/queries/0_stateless/01710_projections.sql
+++ tests/queries/0_stateless/01710_projections.sql
@@ -38,6 +38,9 @@ select toStartOfMinute(datetime) dt_m, domain, sum(retry_count) / sum(duration),
 
 select toStartOfHour(toStartOfMinute(datetime)) dt_h, uniqHLL12(x_id), uniqHLL12(y_id) from projection_test group by dt_h order by dt_h;
 
+-- found by fuzzer
+SELECT 2, -1 FROM projection_test PREWHERE domain_alias = 1. WHERE domain = NULL GROUP BY -9223372036854775808 ORDER BY countIf(first_time = 0) / count(-2147483649) DESC NULLS LAST, 1048576 DESC NULLS LAST;
+
 drop table if exists projection_test;
 
 drop table if exists projection_without_key;
