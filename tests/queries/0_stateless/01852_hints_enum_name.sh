f1de872e99c7761854c59bbe0365b216091149a4
Maksim Kita <kitaetoya@gmail.com>
2021-05-20 00:26:35 +0300

    Try to debug test

--- tests/queries/0_stateless/01852_hints_enum_name.sh
+++ tests/queries/0_stateless/01852_hints_enum_name.sh
@@ -4,5 +4,5 @@ CURDIR=$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)
 # shellcheck source=../shell_config.sh
 . "$CURDIR"/../shell_config.sh
 
-$CLICKHOUSE_CLIENT --query="SELECT CAST('Helo', 'Enum(\'Hello\' = 1, \'World\' = 2)')" 2>&1 | grep -q "may be you meant: \['Hello'\]" && echo 'OK' || echo 'FAIL'
-
+OUTPUT=$($CLICKHOUSE_CLIENT --query="SELECT CAST('Helo', 'Enum(\'Hello\' = 1, \'World\' = 2)')" 2>&1)
+echo $OUTPUT | grep -q "may be you meant: \['Hello'\]" && echo 'OK' || echo $OUTPUT
