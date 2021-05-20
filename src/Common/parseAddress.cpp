80a7f97e6f54dee70884f59cfc38c6606f88c60a
Azat Khuzhin <a3at.mail@gmail.com>
2021-05-20 21:02:45 +0300

    parseAddress: improve parsing of port (by using tryParse() over parse())

--- src/Common/parseAddress.cpp
+++ src/Common/parseAddress.cpp
@@ -35,7 +35,12 @@ std::pair<std::string, UInt16> parseAddress(const std::string & str, UInt16 defa
 
     if (port != end)
     {
-        UInt16 port_number = parse<UInt16>(port + 1);
+        UInt16 port_number;
+        if (!tryParse<UInt16>(port_number, port + 1))
+        {
+            throw Exception(ErrorCodes::BAD_ARGUMENTS,
+                "Illegal port passed to function parseAddress: {}", port + 1);
+        }
         return { std::string(begin, port), port_number };
     }
     else if (default_port)
