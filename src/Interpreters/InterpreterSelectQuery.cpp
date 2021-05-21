0b19247898d8d50d2325340eab015084b5a7cb4b
MaxTheHuman <maxsev1999@yandex.ru>
2021-05-21 17:56:54 +0300

    grouping sets: make simple aggregation with grouping sets to work

--- src/Interpreters/InterpreterSelectQuery.cpp
+++ src/Interpreters/InterpreterSelectQuery.cpp
@@ -965,8 +965,7 @@ void InterpreterSelectQuery::executeImpl(QueryPlan & query_plan, std::optional<P
     bool aggregate_final =
         expressions.need_aggregate &&
         options.to_stage > QueryProcessingStage::WithMergeableState &&
-        !query.group_by_with_totals && !query.group_by_with_rollup && !query.group_by_with_cube &&
-        !query.group_by_with_grouping_sets;
+        !query.group_by_with_totals && !query.group_by_with_rollup && !query.group_by_with_cube;
 
     if (query_info.projection && query_info.projection->desc->type == ProjectionDescription::Type::Aggregate)
     {
@@ -2070,14 +2069,14 @@ void InterpreterSelectQuery::executeAggregation(QueryPlan & query_plan, const Ac
                 all_keys.push_back(header_before_aggregation.getPositionByName(key.name));
                 LOG_DEBUG(
                     log,
-                    "GroupingSets add key with name {} and number {}",
+                    "execute aggregation with grouping sets add key with name {} and number {}",
                     key.name,
                     header_before_aggregation.getPositionByName(key.name));
             }
             keys_vector.push_back(keys);
             LOG_DEBUG(
                 log,
-                "GroupingSets add keys set of size {}",
+                "execute aggregation with grouping sets add keys set of size {}",
                 keys.size());
         }
     }
@@ -2086,7 +2085,7 @@ void InterpreterSelectQuery::executeAggregation(QueryPlan & query_plan, const Ac
         for (const auto & key : query_analyzer->aggregationKeys())
         {
             keys.push_back(header_before_aggregation.getPositionByName(key.name));
-            LOG_DEBUG(log, "executeAggregation pushed back key with name {} and number {}", key.name, header_before_aggregation.getPositionByName(key.name));
+            LOG_DEBUG(log, "execute aggregation without grouping sets pushed back key with name {} and number {}", key.name, header_before_aggregation.getPositionByName(key.name));
         }
     }
 
@@ -2143,10 +2142,13 @@ void InterpreterSelectQuery::executeAggregation(QueryPlan & query_plan, const Ac
     }
     SortDescription group_by_sort_description;
 
-    if (group_by_info && settings.optimize_aggregation_in_order && !query.group_by_with_grouping_sets)
+    if (group_by_info && settings.optimize_aggregation_in_order && !query.group_by_with_grouping_sets) {
         group_by_sort_description = getSortDescriptionFromGroupBy(query);
-    else
+        LOG_DEBUG(log, "execute aggregation without grouping sets got group_by_sort_description");
+    } else {
         group_by_info = nullptr;
+        LOG_DEBUG(log, "execute aggregation didn't get group_by_sort_description");
+    }
 
     auto merge_threads = max_streams;
     auto temporary_data_merge_threads = settings.aggregation_memory_efficient_merge_threads
@@ -2155,7 +2157,7 @@ void InterpreterSelectQuery::executeAggregation(QueryPlan & query_plan, const Ac
 
     bool storage_has_evenly_distributed_read = storage && storage->hasEvenlyDistributedRead();
 
-    LOG_DEBUG(log, "GroupingSets step header before step structure: {}", query_plan.getCurrentDataStream().header.dumpStructure());
+    LOG_DEBUG(log, "execute aggregation header structure before step: {}", query_plan.getCurrentDataStream().header.dumpStructure());
     auto aggregating_step = std::make_unique<AggregatingStep>(
         query_plan.getCurrentDataStream(),
         *params_ptr,
@@ -2167,7 +2169,7 @@ void InterpreterSelectQuery::executeAggregation(QueryPlan & query_plan, const Ac
         storage_has_evenly_distributed_read,
         std::move(group_by_info),
         std::move(group_by_sort_description));
-    LOG_DEBUG(log, "GroupingSets step header after step structure: {}", aggregating_step->getOutputStream().header.dumpStructure());
+    LOG_DEBUG(log, "execute aggregation header structure after step: {}", aggregating_step->getOutputStream().header.dumpStructure());
     query_plan.addStep(std::move(aggregating_step));
 }
 
