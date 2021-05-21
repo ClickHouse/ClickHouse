71d1200b4d62c253f685f4468cac893c51f17d88
Anton Popov <pad11rus@gmail.com>
2021-05-22 02:22:22 +0300

    optimize some functions to subcolumns

--- src/Interpreters/RewriteFunctionToSubcolumnVisitor.cpp
+++ src/Interpreters/RewriteFunctionToSubcolumnVisitor.cpp
@@ -1,6 +1,7 @@
 #include <Interpreters/RewriteFunctionToSubcolumnVisitor.h>
 #include <DataTypes/NestedUtils.h>
 #include <Parsers/ASTIdentifier.h>
+#include <Parsers/ASTLiteral.h>
 
 namespace DB
 {
@@ -8,25 +9,45 @@ namespace DB
 namespace
 {
 
-ASTPtr transformToSubcolumn(const ASTIdentifier & identifier, const String & subcolumn_name)
+ASTPtr transformToSubcolumn(const String & name_in_storage, const String & subcolumn_name)
 {
-    return std::make_shared<ASTIdentifier>(Nested::concatenateName(identifier.name(), subcolumn_name));
+    return std::make_shared<ASTIdentifier>(Nested::concatenateName(name_in_storage, subcolumn_name));
 }
 
-ASTPtr transformIsNotNullToSubcolumn(const ASTIdentifier & identifier, const String & subcolumn_name)
+ASTPtr transformEmptyToSubcolumn(const String & name_in_storage, const String & subcolumn_name)
 {
-    auto ast = transformToSubcolumn(identifier, subcolumn_name);
-    ast = makeASTFunction("NOT", ast);
-    return ast;
+    auto ast = transformToSubcolumn(name_in_storage, subcolumn_name);
+    return makeASTFunction("equals", ast, std::make_shared<ASTLiteral>(0u));
 }
 
-const std::unordered_map<String, std::pair<String, decltype(&transformToSubcolumn)>> function_to_subcolumn =
+ASTPtr transformNotEmptyToSubcolumn(const String & name_in_storage, const String & subcolumn_name)
 {
-    {"length",    {"size0", transformToSubcolumn}},
-    {"isNull",    {"null", transformToSubcolumn}},
-    {"isNotNull", {"null", transformIsNotNullToSubcolumn}},
-    {"mapKeys",   {"keys", transformToSubcolumn}},
-    {"mapValues", {"values", transformToSubcolumn}}
+    auto ast = transformToSubcolumn(name_in_storage, subcolumn_name);
+    return makeASTFunction("notEquals", ast, std::make_shared<ASTLiteral>(0u));
+}
+
+ASTPtr transformIsNotNullToSubcolumn(const String & name_in_storage, const String & subcolumn_name)
+{
+    auto ast = transformToSubcolumn(name_in_storage, subcolumn_name);
+    return makeASTFunction("not", ast);
+}
+
+ASTPtr transformCountNullableToSubcolumn(const String & name_in_storage, const String & subcolumn_name)
+{
+    auto ast = transformToSubcolumn(name_in_storage, subcolumn_name);
+    return makeASTFunction("sum", makeASTFunction("not", ast));
+}
+
+const std::unordered_map<String, std::tuple<TypeIndex, String, decltype(&transformToSubcolumn)>> function_to_subcolumn =
+{
+    {"length",    {TypeIndex::Array, "size0", transformToSubcolumn}},
+    {"empty",     {TypeIndex::Array, "size0", transformEmptyToSubcolumn}},
+    {"notEmpty",  {TypeIndex::Array, "size0", transformNotEmptyToSubcolumn}},
+    {"isNull",    {TypeIndex::Nullable, "null", transformToSubcolumn}},
+    {"isNotNull", {TypeIndex::Nullable, "null", transformIsNotNullToSubcolumn}},
+    {"count",     {TypeIndex::Nullable, "null", transformCountNullableToSubcolumn}},
+    {"mapKeys",   {TypeIndex::Map, "keys", transformToSubcolumn}},
+    {"mapValues", {TypeIndex::Map, "values", transformToSubcolumn}},
 };
 
 }
@@ -38,15 +59,22 @@ void RewriteFunctionToSubcolumnData::visit(ASTFunction & function, ASTPtr & ast)
         return;
 
     const auto * identifier = arguments[0]->as<ASTIdentifier>();
-    if (!identifier || !columns_to_rewrite.count(identifier->name()))
+    if (!identifier)
         return;
 
     auto it = function_to_subcolumn.find(function.name);
     if (it == function_to_subcolumn.end())
         return;
 
-    const auto & [subcolumn_name, transformer] = it->second;
-    ast = transformer(*identifier, subcolumn_name);
+    const auto & [type_id, subcolumn_name, transformer] = it->second;
+    const auto & columns = metadata_snapshot->getColumns();
+    const auto & name_in_storage = identifier->name();
+
+    if (columns.has(name_in_storage)
+        && columns.get(name_in_storage).type->getTypeId() == type_id)
+    {
+        ast = transformer(name_in_storage, subcolumn_name);
+    }
 }
 
 }
