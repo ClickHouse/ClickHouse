d867847a1bae1f0c744146949b909de185e05ee7
Alexandra <alexelexa@gmail.com>
2021-05-23 12:44:45 +0000

    add optimize on encrypted disk integration test

--- tests/integration/test_encrypted_disk/test.py
+++ tests/integration/test_encrypted_disk/test.py
@@ -1,8 +1,9 @@
 import pytest
 from helpers.cluster import ClickHouseCluster
+from helpers.client import QueryRuntimeException
 
 
-
+FIRST_PART_NAME = "all_1_1_0"
 
 @pytest.fixture(scope="module")
 def cluster():
@@ -42,7 +43,7 @@ def test_encrypted_disk(cluster, policy):
     node.query("DROP TABLE IF EXISTS encrypted_test NO DELAY")
 
 
-@pytest.mark.parametrize("policy,disk,encrypted_disk", [("local_policy", "disk_local", "disk_local_encrypted"), ('s3_policy', 'disk_s3', 'disk_s3_encrypted')])
+@pytest.mark.parametrize("policy,disk,encrypted_disk", [("local_policy", "disk_local", "disk_local_encrypted"), ("s3_policy", "disk_s3", "disk_s3_encrypted")])
 def test_part_move(cluster, policy, disk, encrypted_disk):
     node = cluster.instances["node"]
     node.query(
@@ -60,9 +61,49 @@ def test_part_move(cluster, policy, disk, encrypted_disk):
     select_query = "SELECT * FROM encrypted_test ORDER BY id FORMAT Values"
     assert node.query(select_query) == "(0,'data'),(1,'data')"
 
-    node.query("ALTER TABLE encrypted_test MOVE PART 'all_1_1_0' TO DISK '{}'".format(encrypted_disk))
+    node.query("ALTER TABLE encrypted_test MOVE PART '{}' TO DISK '{}'".format(FIRST_PART_NAME, encrypted_disk))
     assert node.query(select_query) == "(0,'data'),(1,'data')"
 
-    node.query("ALTER TABLE encrypted_test MOVE PART 'all_1_1_0' TO DISK '{}'".format(disk))
+    with pytest.raises(QueryRuntimeException) as exc:
+        node.query("ALTER TABLE encrypted_test MOVE PART '{}' TO DISK '{}'".format(FIRST_PART_NAME, encrypted_disk))
+
+    assert("Part '{}' is already on disk '{}'".format(FIRST_PART_NAME, encrypted_disk) in str(exc.value))
+
+    node.query("ALTER TABLE encrypted_test MOVE PART '{}' TO DISK '{}'".format(FIRST_PART_NAME, disk))
     assert node.query(select_query) == "(0,'data'),(1,'data')"
+
+    node.query("DROP TABLE IF EXISTS encrypted_test NO DELAY")
+
+
+@pytest.mark.parametrize("policy,encrypted_disk", [("local_policy", "disk_local_encrypted"), ("s3_policy", "disk_s3_encrypted")])
+def test_optimize_table(cluster, policy, encrypted_disk):
+    node = cluster.instances["node"]
+    node.query(
+        """
+        CREATE TABLE encrypted_test (
+            id Int64,
+            data String
+        ) ENGINE=MergeTree()
+        ORDER BY id
+        SETTINGS storage_policy='{}'
+        """.format(policy)
+    )
+
+    node.query("INSERT INTO encrypted_test VALUES (0,'data'),(1,'data')")
+    select_query = "SELECT * FROM encrypted_test ORDER BY id FORMAT Values"
+    assert node.query(select_query) == "(0,'data'),(1,'data')"
+
+    node.query("ALTER TABLE encrypted_test MOVE PART '{}' TO DISK '{}'".format(FIRST_PART_NAME, encrypted_disk))
+    assert node.query(select_query) == "(0,'data'),(1,'data')"
+
+    node.query("INSERT INTO encrypted_test VALUES (2,'data'),(3,'data')")
+    node.query("OPTIMIZE TABLE encrypted_test FINAL")
+
+    with pytest.raises(QueryRuntimeException) as exc:
+        node.query("ALTER TABLE encrypted_test MOVE PART '{}' TO DISK '{}'".format(FIRST_PART_NAME, encrypted_disk))
+
+    assert("Part {} is not exists or not active".format(FIRST_PART_NAME) in str(exc.value))
+
+    assert node.query(select_query) == "(0,'data'),(1,'data'),(2,'data'),(3,'data')"
+
     node.query("DROP TABLE IF EXISTS encrypted_test NO DELAY")
