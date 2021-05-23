3057bbe8312b4a9c72ec82f9b87287d553066fbe
Alexey Milovidov <milovidov@yandex-team.ru>
2021-05-23 04:12:30 +0300

    Remove header files

--- src/Compression/CompressionCodecDensity.cpp
+++ src/Compression/CompressionCodecDensity.cpp
@@ -1,11 +1,41 @@
+#include <Compression/ICompressionCodec.h>
 #include <Compression/CompressionFactory.h>
 #include <Compression/CompressionInfo.h>
-#include <Compression/CompressionCodecDensity.h>
 #include <Parsers/ASTLiteral.h>
+
+#include <src/density_api.h>
 
 
 namespace DB
 {
+
+class CompressionCodecDensity : public ICompressionCodec
+{
+public:
+    static constexpr auto DENSITY_DEFAULT_ALGO = DENSITY_ALGORITHM_CHAMELEON; // by default aim on speed
+
+    CompressionCodecDensity(DENSITY_ALGORITHM algo_);
+
+    uint8_t getMethodByte() const override;
+
+    UInt32 getMaxCompressedDataSize(UInt32 uncompressed_size) const override;
+
+    void updateHash(SipHash & hash) const override;
+
+protected:
+    UInt32 doCompressData(const char * source, UInt32 source_size, char * dest) const override;
+
+    void doDecompressData(const char * source, UInt32 source_size, char * dest, UInt32 uncompressed_size) const override;
+
+    bool isCompression() const override { return true; }
+    bool isGenericCompression() const override { return true; }
+    bool isExperimental() const override { return true; }
+
+private:
+    const DENSITY_ALGORITHM algo;
+};
+
+
 namespace ErrorCodes
 {
     extern const int CANNOT_COMPRESS;
--- src/Compression/CompressionCodecLizard.cpp
+++ src/Compression/CompressionCodecLizard.cpp
@@ -1,4 +1,4 @@
-#include <Compression/CompressionCodecLizard.h>
+#include <Compression/ICompressionCodec.h>
 #include <Compression/CompressionFactory.h>
 #include <Compression/CompressionInfo.h>
 #include <Parsers/ASTLiteral.h>
@@ -8,6 +8,32 @@
 
 namespace DB
 {
+class CompressionCodecLizard : public ICompressionCodec
+{
+public:
+    static constexpr auto LIZARD_DEFAULT_LEVEL = 1;
+
+    CompressionCodecLizard(int level_);
+
+    uint8_t getMethodByte() const override;
+
+    UInt32 getMaxCompressedDataSize(UInt32 uncompressed_size) const override;
+
+    void updateHash(SipHash & hash) const override;
+
+protected:
+    UInt32 doCompressData(const char * source, UInt32 source_size, char * dest) const override;
+    void doDecompressData(const char * source, UInt32 source_size, char * dest, UInt32 uncompressed_size) const override;
+
+    bool isCompression() const override { return true; }
+    bool isGenericCompression() const override { return true; }
+    bool isExperimental() const override { return true; }
+
+private:
+    const int level;
+};
+
+
 namespace ErrorCodes
 {
     extern const int CANNOT_COMPRESS;
