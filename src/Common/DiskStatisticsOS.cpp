067ec0855cfeb16392e5bef121328abb5b6e7957
elevankoff <yegor.levankov@yandex.com>
2021-05-24 17:16:15 +0000

    Decorative fixes

--- src/Common/DiskStatisticsOS.cpp
+++ src/Common/DiskStatisticsOS.cpp
@@ -9,15 +9,15 @@
 #include <IO/ReadBuffer.h>
 #include <IO/ReadHelpers.h>
 
-namespace DB 
+namespace DB
 {
 
-namespace ErrorCodes 
+namespace ErrorCodes
 {
     extern const int CANNOT_STATVFS;
 }
 
-namespace 
+namespace
 {
     void readStringUntilWhitespaceAndSkipWhitespaceIfAny(String & s, ReadBuffer & buf)
     {
@@ -34,35 +34,35 @@ DiskStatisticsOS::DiskStatisticsOS() {}
 
 DiskStatisticsOS::~DiskStatisticsOS() {}
 
-DiskStatisticsOS::Data DiskStatisticsOS::get() 
+DiskStatisticsOS::Data DiskStatisticsOS::get()
 {
     ReadBufferFromFile mounts_in(mounts_filename, READ_BUFFER_BUF_SIZE, O_RDONLY | O_CLOEXEC);
 
     DiskStatisticsOS::Data data = {0, 0};
 
-    while (!mounts_in.eof()) 
+    while (!mounts_in.eof())
     {
         String filesystem = readNextFilesystem(mounts_in);
-        
+
         struct statvfs stat;
 
-        if (statvfs(filesystem.c_str(), &stat)) 
+        if (statvfs(filesystem.c_str(), &stat))
             throwFromErrno("Cannot statvfs", ErrorCodes::CANNOT_STATVFS);
-        
+
         uint64_t total_blocks = static_cast<uint64_t>(stat.f_blocks);
-        uint64_t free_blocks  = static_cast<uint64_t>(stat.f_bfree);
-        uint64_t used_blocks  = total_blocks - free_blocks; 
-        uint64_t block_size   = static_cast<uint64_t>(stat.f_bsize);
+        uint64_t free_blocks = static_cast<uint64_t>(stat.f_bfree);
+        uint64_t used_blocks = total_blocks - free_blocks;
+        uint64_t block_size = static_cast<uint64_t>(stat.f_bsize);
 
         data.total += total_blocks * block_size;
-        data.used  += used_blocks  * block_size;
+        data.used += used_blocks  * block_size;
     }
 
     return data;
 }
 
-String DiskStatisticsOS::readNextFilesystem(ReadBuffer& mounts_in) 
-{   
+String DiskStatisticsOS::readNextFilesystem(ReadBuffer& mounts_in)
+{
     String filesystem, unused;
 
     readStringUntilWhitespaceAndSkipWhitespaceIfAny(unused, mounts_in);
--- src/Common/DiskStatisticsOS.h
+++ src/Common/DiskStatisticsOS.h
@@ -1,3 +1,4 @@
+#pragma once
 #if defined (OS_LINUX)
 
 #include <cstdint>
@@ -6,17 +7,18 @@
 
 #include <IO/ReadBufferFromFile.h>
 
-namespace DB 
+namespace DB
 {
 
-/** Opens file /proc/mounts, reads all mounted filesytems and 
+/** Opens file /proc/mounts, reads all mounted filesystems and
   * calculates disk usage.
-  */ 
-class DiskStatisticsOS 
+  */
+class DiskStatisticsOS
 {
 public:
     // In bytes
-    struct Data {
+    struct Data
+    {
         uint64_t total;
         uint64_t used;
     };
