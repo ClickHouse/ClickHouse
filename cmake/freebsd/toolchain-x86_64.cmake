cf3275ce52a013b38289a47d660232b5ea6be47b
Yatsishin Ilya <2159081+qoega@users.noreply.github.com>
2021-05-25 18:29:04 +0300

    try

--- cmake/freebsd/toolchain-x86_64.cmake
+++ cmake/freebsd/toolchain-x86_64.cmake
@@ -10,7 +10,7 @@ set (CMAKE_TRY_COMPILE_TARGET_TYPE STATIC_LIBRARY)  # disable linkage check - it
 set (CMAKE_AR "/usr/bin/ar" CACHE FILEPATH "" FORCE)
 set (CMAKE_RANLIB "/usr/bin/ranlib" CACHE FILEPATH "" FORCE)
 
-set (LINKER_NAME "lld" CACHE STRING "" FORCE)
+set (LINKER_NAME "ld.lld" CACHE STRING "" FORCE)
 
 set (CMAKE_EXE_LINKER_FLAGS_INIT "-fuse-ld=lld")
 set (CMAKE_SHARED_LINKER_FLAGS_INIT "-fuse-ld=lld")
