def7164b62c3a0c2508ab717fcbc6bc3e774ad93
Alexey Milovidov <milovidov@yandex-team.ru>
2021-05-25 20:22:32 +0300

    Fix terrible test

--- tests/queries/0_stateless/01085_max_distributed_connections.sh
+++ tests/queries/0_stateless/01085_max_distributed_connections.sh
@@ -12,6 +12,7 @@ do
         --max_distributed_connections 9
         --max_threads 1
         --query "SELECT sleepEachRow(1) FROM remote('127.{2..10}', system.one)"
+        --format Null
     )
     # 5 less then 9 seconds (9 streams), but long enough to cover possible load peaks
     # "$@" left to pass manual options (like --experimental_use_processors 0) during manual testing
