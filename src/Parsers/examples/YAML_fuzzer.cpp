07db51d1f335bd1bb7949618345320b4e6cf1b7c
BoloniniD <denis@bolonin.ru>
2021-05-26 00:58:49 +0300

    Added check for temp file existance

--- src/Parsers/examples/YAML_fuzzer.cpp
+++ src/Parsers/examples/YAML_fuzzer.cpp
@@ -1,26 +1,34 @@
 #include <iostream>
 #include <fstream>
 #include <string>
-
-#include <Parsers/ASTCreateQuery.h>
-#include <Parsers/ParserCreateQuery.h>
-#include <Parsers/formatAST.h>
-#include <Parsers/parseQuery.h>
-
+#include <time.h>
+#include <filesystem>
 
 extern "C" int LLVMFuzzerTestOneInput(const uint8_t * data, size_t size)
 {
+    /// How to test:
+    /// build ClickHouse with YAML_fuzzer.cpp
+    /// ./YAML_fuzzer YAML_CORPUS
+    /// where YAML_CORPUS is a directory with different YAML configs for libfuzzer
+
+    srand(time(NULL));
+    std::string cur_file = std::to_string(rand());
+
+    while (std::filesystem::exists(cur_file))
+    {
+        std::string cur_file = std::to_string(rand());
+    }
+
     std::string input = std::string(reinterpret_cast<const char*>(data), size);
     DB::YAMLParser parser;
 
-    std::ofstream temp_file("YAML_fuzzer_data.yaml");
+    std::ofstream temp_file(cur_file);
     temp_file << input;
     temp_file.close();
     
-    YAMLParser::parse("YAML_fuzzer_data.yaml");
+    DB::YAMLParser::parse(cur_file);
 
-    remove("YAML_fuzzer_data.yaml");
-
+    remove(cur_file.c_str());
     return 0;
 }
 
