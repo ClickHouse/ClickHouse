e19cff70b89e417cd9c3690e9c4c34367acd035d
alesapin <alesapin@gmail.com>
2021-05-26 12:47:22 +0300

    More robust test

--- tests/queries/0_stateless/01882_check_max_parts_to_merge_at_once.sql
+++ tests/queries/0_stateless/01882_check_max_parts_to_merge_at_once.sql
@@ -16,14 +16,17 @@ SYSTEM STOP MERGES limited_merge_table;
 
 INSERT INTO limited_merge_table SELECT number FROM numbers(250);
 
-
 SYSTEM START MERGES limited_merge_table;
 
 OPTIMIZE TABLE limited_merge_table FINAL;
 
 SYSTEM FLUSH LOGS;
 
--- final optimize FINAL will merge all parts, but all previous merges must merge <= 3 parts
-SELECT length(merged_from) <= 3 FROM system.part_log WHERE event_type = 'MergeParts' and table = 'limited_merge_table' and database = currentDatabase() ORDER BY length(merged_from) DESC LIMIT 1 OFFSET 2;
+SELECT COUNT() FROM limited_merge_table;
+
+-- final optimize FINAL will merge all parts, but all previous merges must merge <= 3 parts.
+-- During concurrent run only one final merge can happen, thats why we have this `if`.
+SELECT if(length(topK(2)(length(merged_from))) == 2, arrayMin(topK(2)(length(merged_from))) <= 3, 1)
+FROM system.part_log WHERE table = 'limited_merge_table' and database = currentDatabase() and event_type = 'MergeParts';
 
 DROP TABLE IF EXISTS limited_merge_table;
