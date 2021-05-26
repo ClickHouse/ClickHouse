810870ce17b27ded97c792c586f3950d5c4f5c5b
Raúl Marín <git@rmr.ninja>
2021-05-26 13:48:33 +0200

    Add missing error codes to fix style check

--- src/AggregateFunctions/AggregateFunctionUniqCombined.cpp
+++ src/AggregateFunctions/AggregateFunctionUniqCombined.cpp
@@ -17,6 +17,7 @@ namespace ErrorCodes
 {
     extern const int NUMBER_OF_ARGUMENTS_DOESNT_MATCH;
     extern const int ARGUMENT_OUT_OF_BOUND;
+    extern const int ILLEGAL_TYPE_OF_ARGUMENT;
 }
 
 namespace
