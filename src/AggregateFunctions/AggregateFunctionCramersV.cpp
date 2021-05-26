674fa20b72f09fb5c2640bfa366f13b9a4740ef9
Vanyok-All-is-OK <71926682+Vanyok-All-is-OK@users.noreply.github.com>
2021-05-26 16:25:46 +0300

    Create AggregateFunctionCramersV.cpp

---
+++ src/AggregateFunctions/AggregateFunctionCramersV.cpp
@@ -0,0 +1,32 @@
+#include <AggregateFunctions/AggregateFunctionFactory.h>
+#include <AggregateFunctions/AggregateFunctionCramersV.h>
+#include <AggregateFunctions/FactoryHelpers.h>
+#include <AggregateFunctions/Helpers.h>
+#include "registerAggregateFunctions.h"
+#include <memory>
+
+
+namespace ErrorCodes
+{
+    extern const int NOT_IMPLEMENTED;
+}
+
+namespace DB
+{
+    namespace
+    {
+
+        AggregateFunctionPtr createAggregateFunctionCramersV(const std::string &, const DataTypes & argument_types, const Array &)
+        {
+            return std::make_shared<AggregateFunctionCramersV>(argument_types);
+        }
+
+    }
+
+
+    void registerAggregateFunctionCramersV(AggregateFunctionFactory & factory)
+    {
+        factory.registerFunction("CramersV", createAggregateFunctionCramersV);
+    }
+
+}
