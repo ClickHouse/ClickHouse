8ca95bf3fb2c0b9ea622b23a07895e73e987de1a
Dmitrii Kovalkov <dimas.kovas@gmail.com>
2021-05-27 12:55:15 +0300

    Fix flaky test

--- tests/queries/0_stateless/01881_total_bytes_storage_buffer.sql
+++ tests/queries/0_stateless/01881_total_bytes_storage_buffer.sql
@@ -6,18 +6,18 @@ CREATE TABLE test_buffer_table
 )
 ENGINE = Buffer('', '', 1, 1000000, 1000000, 1000000, 1000000, 1000000, 1000000);
 
-SELECT total_bytes FROM system.tables WHERE name = 'test_buffer_table';
+SELECT total_bytes FROM system.tables WHERE name = 'test_buffer_table' and database = currentDatabase();
 
 INSERT INTO test_buffer_table SELECT number FROM numbers(1000);
-SELECT total_bytes FROM system.tables WHERE name = 'test_buffer_table';
+SELECT total_bytes FROM system.tables WHERE name = 'test_buffer_table' and database = currentDatabase();
 
 OPTIMIZE TABLE test_buffer_table;
-SELECT total_bytes FROM system.tables WHERE name = 'test_buffer_table';
+SELECT total_bytes FROM system.tables WHERE name = 'test_buffer_table' and database = currentDatabase();
 
 INSERT INTO test_buffer_table SELECT number FROM numbers(1000);
-SELECT total_bytes FROM system.tables WHERE name = 'test_buffer_table';
+SELECT total_bytes FROM system.tables WHERE name = 'test_buffer_table' and database = currentDatabase();
 
 OPTIMIZE TABLE test_buffer_table;
-SELECT total_bytes FROM system.tables WHERE name = 'test_buffer_table';
+SELECT total_bytes FROM system.tables WHERE name = 'test_buffer_table' and database = currentDatabase();
 
 DROP TABLE test_buffer_table;
