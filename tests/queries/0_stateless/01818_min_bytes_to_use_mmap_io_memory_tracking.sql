4cf87c3afbff752ad589417df175a6f88f0635f9
robot-clickhouse <robot-clickhouse@yandex-team.ru>
2021-05-29 12:42:28 +0300

    Backport #23276 to 21.5: Revert \"[RFC] Fix memory tracking with min_bytes_to_use_mmap_io\"

--- tests/queries/0_stateless/01818_min_bytes_to_use_mmap_io_memory_tracking.sql
+++
@@ -1,5 +0,0 @@
-drop table if exists data_01818;
-
-create table data_01818 (key Int, value String) engine=MergeTree() order by key settings min_bytes_for_wide_part=0 as select number, randomPrintableASCII(100) from numbers(1e6);
-select * from data_01818 format Null settings min_bytes_to_use_mmap_io=1, max_memory_usage='20Mi', max_threads=1; -- { serverError 241 }
-select * from data_01818 format Null settings min_bytes_to_use_mmap_io=1e9, max_memory_usage='20Mi', max_threads=1;
