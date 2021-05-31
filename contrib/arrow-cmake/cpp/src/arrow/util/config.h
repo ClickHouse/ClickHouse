c277e5769615dfde1f86dfe35f8780211319c4b2
s-kat <sakatkovskiy@edu.hse.ru>
2021-05-31 19:01:28 +0300

    Try to build

---
+++ contrib/arrow-cmake/cpp/src/arrow/util/config.h
@@ -0,0 +1,39 @@
+// Licensed to the Apache Software Foundation (ASF) under one
+// or more contributor license agreements.  See the NOTICE file
+// distributed with this work for additional information
+// regarding copyright ownership.  The ASF licenses this file
+// to you under the Apache License, Version 2.0 (the
+// "License"); you may not use this file except in compliance
+// with the License.  You may obtain a copy of the License at
+//
+//   http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing,
+// software distributed under the License is distributed on an
+// "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+// KIND, either express or implied.  See the License for the
+// specific language governing permissions and limitations
+// under the License.
+
+#define ARROW_VERSION_MAJOR 
+#define ARROW_VERSION_MINOR 
+#define ARROW_VERSION_PATCH 
+#define ARROW_VERSION ((ARROW_VERSION_MAJOR * 1000) + ARROW_VERSION_MINOR) * 1000 + ARROW_VERSION_PATCH
+
+#define ARROW_VERSION_STRING ""
+
+#define ARROW_SO_VERSION ""
+#define ARROW_FULL_SO_VERSION ""
+
+#define ARROW_CXX_COMPILER_ID "Clang"
+#define ARROW_CXX_COMPILER_VERSION "11.0.0"
+#define ARROW_CXX_COMPILER_FLAGS " -fdiagnostics-color=always -fsized-deallocation  -gdwarf-aranges -msse4.1 -msse4.2 -mpopcnt -fasynchronous-unwind-tables -falign-functions=32   -Wall -Wno-unused-command-line-argument  -fdiagnostics-absolute-paths -fexperimental-new-pass-manager -Werror -w -D__DATE__= -D__TIME__= -D__TIMESTAMP__=  "
+
+#define ARROW_GIT_ID ""
+#define ARROW_GIT_DESCRIPTION ""
+
+#define ARROW_PACKAGE_KIND ""
+
+/* #undef ARROW_S3 */
+
+/* #undef GRPCPP_PP_INCLUDE */
