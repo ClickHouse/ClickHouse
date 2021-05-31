a00212e67a31388a3075d7d9dbfa021c43b095e3
Anton Popov <pad11rus@gmail.com>
2021-06-01 02:22:05 +0300

    better lock in StorageJoin

--- tests/queries/0_stateless/01821_join_table_mutation.sql
+++ tests/queries/0_stateless/01821_join_table_mutation.sql
@@ -4,13 +4,13 @@ CREATE TABLE join_table_mutation(id Int32, name String) ENGINE = Join(ANY, LEFT,
 
 INSERT INTO join_table_mutation select number, toString(number) from numbers(100);
 
-SELECT count(1) FROM join_table_mutation;
+SELECT count() FROM join_table_mutation;
 
 SELECT name FROM join_table_mutation WHERE id = 10;
 
 ALTER TABLE join_table_mutation DELETE WHERE id = 10;
 
-SELECT count(1) FROM join_table_mutation;
+SELECT count() FROM join_table_mutation;
 
 SELECT name FROM join_table_mutation WHERE id = 10;
 
@@ -20,4 +20,16 @@ SELECT name FROM join_table_mutation WHERE id = 10;
 
 ALTER TABLE join_table_mutation DELETE WHERE id % 2 = 0;
 
-SELECT count(1) FROM join_table_mutation;
+ALTER TABLE join_table_mutation UPDATE name = 'some' WHERE 1; -- {serverError 48}
+
+SELECT count() FROM join_table_mutation;
+
+ALTER TABLE join_table_mutation DELETE WHERE name IN ('1', '2', '3', '4');
+
+SELECT count() FROM join_table_mutation;
+
+ALTER TABLE join_table_mutation DELETE WHERE 1;
+
+SELECT count() FROM join_table_mutation;
+
+DROP TABLE join_table_mutation;
