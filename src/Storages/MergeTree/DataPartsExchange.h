3496de6e5dc060651d5b9d4a9d5fd8b09951ba73
Kseniia Sumarokova <54203879+kssenii@users.noreply.github.com>
2021-05-31 11:24:14 +0300

    Update DataPartsExchange.h

--- src/Storages/MergeTree/DataPartsExchange.h
+++ src/Storages/MergeTree/DataPartsExchange.h
@@ -56,7 +56,7 @@ public:
     /// Downloads a part to tmp_directory. If to_detached - downloads to the `detached` directory.
     MergeTreeData::MutableDataPartPtr fetchPart(
         const StorageMetadataPtr & metadata_snapshot,
-        ContextPtr context,
+        const Context & context,
         const String & part_name,
         const String & replica_path,
         const String & host,
@@ -88,7 +88,7 @@ private:
             const String & part_name,
             const UUID & part_uuid,
             const StorageMetadataPtr & metadata_snapshot,
-            ContextPtr context,
+            const Context & context,
             ReservationPtr reservation,
             PooledReadWriteBufferFromHTTP & in);
 
