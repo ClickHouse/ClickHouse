655f8c4b1ccad42a8fe2249f86383dc0699b6740
BoloniniD <denis@bolonin.ru>
2021-05-31 18:19:38 +0300

    More fixes

--- src/Common/examples/YAML_fuzzer.cpp
+++ src/Common/examples/YAML_fuzzer.cpp
@@ -11,26 +11,23 @@ extern "C" int LLVMFuzzerTestOneInput(const uint8_t * data, size_t size)
     /// build ClickHouse with YAML_fuzzer.cpp
     /// ./YAML_fuzzer YAML_CORPUS
     /// where YAML_CORPUS is a directory with different YAML configs for libfuzzer
-    char buf[L_tmpnam];
-    char* file_name = std::tmpnam(buf);
-    if (file_name == nullptr)
+    char file_name[L_tmpnam];
+    if (!std::tmpnam(file_name))
     {
         std::cerr << "Cannot create temp file!\n";
         return 1;
     }
-    std::string cur_file(file_name);
-
     std::string input = std::string(reinterpret_cast<const char*>(data), size);
     DB::YAMLParser parser;
 
     {
-        std::ofstream temp_file(cur_file);
+        std::ofstream temp_file(file_name);
         temp_file << input;
     }
 
     try
     {
-        DB::YAMLParser::parse(cur_file);
+        DB::YAMLParser::parse(std::string(file_name));
     }
     catch (...)
     {
