7795c8c047cade90b5b33b29504676feacd010bb
Kseniia Sumarokova <54203879+kssenii@users.noreply.github.com>
2021-06-01 12:20:26 +0300

    Update ReplicatedMergeTreeBlockOutputStream.cpp

--- src/Storages/MergeTree/ReplicatedMergeTreeBlockOutputStream.cpp
+++ src/Storages/MergeTree/ReplicatedMergeTreeBlockOutputStream.cpp
@@ -136,7 +136,7 @@ void ReplicatedMergeTreeBlockOutputStream::write(const Block & block)
     if (quorum)
         checkQuorumPrecondition(zookeeper);
 
-    auto part_blocks = storage.writer.splitBlockIntoParts(block, max_parts_per_block, metadata_snapshot, context);
+    auto part_blocks = storage.writer.splitBlockIntoParts(block, max_parts_per_block, metadata_snapshot, storage.context);
 
     for (auto & current_block : part_blocks)
     {
