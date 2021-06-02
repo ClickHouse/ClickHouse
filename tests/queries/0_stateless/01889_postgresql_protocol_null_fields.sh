47f2764ab8a345aa140fc11cf1a2c573df7241cf
kssenii <sumarokovakseniia@mail.ru>
2021-06-02 08:38:43 +0000

    Fix test

--- tests/queries/0_stateless/01889_postgresql_protocol_null_fields.sh
+++ tests/queries/0_stateless/01889_postgresql_protocol_null_fields.sh
@@ -4,4 +4,9 @@ CUR_DIR=$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)
 # shellcheck source=../shell_config.sh
 . "$CUR_DIR"/../shell_config.sh
 
-psql --host localhost --port ${CLICKHOUSE_PORT_POSTGRESQL} default -c "SELECT NULL;"
+echo "
+DROP USER IF EXISTS postgresql_user;
+CREATE USER postgresql_user HOST IP '127.0.0.1' IDENTIFIED WITH no_password;
+" | $CLICKHOUSE_CLIENT -n
+
+psql --host localhost --port ${CLICKHOUSE_PORT_POSTGRESQL} ${CLICKHOUSE_DATABASE} --user postgresql_user -c "SELECT NULL;"
