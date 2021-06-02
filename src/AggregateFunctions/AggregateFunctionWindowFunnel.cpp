dcc24c88163d2c2617dc6ab1c5ace2e13b843203
vdimir <vdimir@yandex-team.ru>
2021-06-02 11:57:01 +0300

    fix style AggregateFunctionWindowFunnel.cpp

--- src/AggregateFunctions/AggregateFunctionWindowFunnel.cpp
+++ src/AggregateFunctions/AggregateFunctionWindowFunnel.cpp
@@ -17,7 +17,6 @@ namespace ErrorCodes
 {
     extern const int ILLEGAL_TYPE_OF_ARGUMENT;
     extern const int NUMBER_OF_ARGUMENTS_DOESNT_MATCH;
-    extern const int UNKNOWN_AGGREGATE_FUNCTION;
 }
 
 namespace
