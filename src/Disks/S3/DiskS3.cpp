6b5c152f2053196c3e34a8ea211fe3272a84799d
Vladimir Chebotarev <vladimir.chebotarev@gmail.com>
2021-06-03 10:30:31 +0300

    Rebase fix.

--- src/Disks/S3/DiskS3.cpp
+++ src/Disks/S3/DiskS3.cpp
@@ -133,7 +133,7 @@ public:
 
     std::unique_ptr<ReadBufferFromS3> createReadBuffer(const String & path) override
     {
-        return std::make_unique<ReadBufferFromS3>(client_ptr, bucket, metadata.remote_fs_root_path + path, s3_max_single_read_retries, buf_size);
+        return std::make_unique<ReadBufferFromS3>(client_ptr, bucket, metadata.remote_fs_root_path + path, single_read_retry_strategy, buf_size);
     }
 
 private:
