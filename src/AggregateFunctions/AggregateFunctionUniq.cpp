7be5010f5971d51f24c80c8d9ea2a1dd5a119dde
robot-clickhouse <robot-clickhouse@yandex-team.ru>
2021-06-04 21:32:06 +0300

    Backport #24523 to 21.5: Fix crash when chaining uniqStates

--- src/AggregateFunctions/AggregateFunctionUniq.cpp
+++ src/AggregateFunctions/AggregateFunctionUniq.cpp
@@ -5,11 +5,8 @@
 
 #include <DataTypes/DataTypeDate.h>
 #include <DataTypes/DataTypeDateTime.h>
-#include <DataTypes/DataTypeString.h>
-#include <DataTypes/DataTypeFixedString.h>
 #include <DataTypes/DataTypeTuple.h>
 #include <DataTypes/DataTypeUUID.h>
-#include "registerAggregateFunctions.h"
 
 
 namespace DB
@@ -18,6 +15,7 @@ namespace DB
 namespace ErrorCodes
 {
     extern const int NUMBER_OF_ARGUMENTS_DOESNT_MATCH;
+    extern const int ILLEGAL_TYPE_OF_ARGUMENT;
 }
 
 
@@ -38,6 +36,12 @@ AggregateFunctionPtr createAggregateFunctionUniq(const std::string & name, const
             ErrorCodes::NUMBER_OF_ARGUMENTS_DOESNT_MATCH);
 
     bool use_exact_hash_function = !isAllArgumentsContiguousInMemory(argument_types);
+
+    const WhichDataType t(argument_types[0]);
+    if (t.isAggregateFunction())
+        throw Exception(
+            "Illegal type " + argument_types[0]->getName() + " of argument for aggregate function " + name,
+            ErrorCodes::ILLEGAL_TYPE_OF_ARGUMENT);
 
     if (argument_types.size() == 1)
     {
@@ -80,6 +84,12 @@ AggregateFunctionPtr createAggregateFunctionUniq(const std::string & name, const
     if (argument_types.empty())
         throw Exception("Incorrect number of arguments for aggregate function " + name,
             ErrorCodes::NUMBER_OF_ARGUMENTS_DOESNT_MATCH);
+
+    const WhichDataType t(argument_types[0]);
+    if (t.isAggregateFunction())
+        throw Exception(
+            "Illegal type " + argument_types[0]->getName() + " of argument for aggregate function " + name,
+            ErrorCodes::ILLEGAL_TYPE_OF_ARGUMENT);
 
     /// We use exact hash function if the user wants it;
     /// or if the arguments are not contiguous in memory, because only exact hash function have support for this case.
