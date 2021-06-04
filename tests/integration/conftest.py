05232e7c40277bbb4653e4af9e82667f06f79398
Yatsishin Ilya <2159081+qoega@users.noreply.github.com>
2021-06-04 17:33:22 +0300

    Make tests great again

--- tests/integration/conftest.py
+++ tests/integration/conftest.py
@@ -1,6 +1,24 @@
+import subprocess
+import pytest
+import logging
 from helpers.test_tools import TSV
+from helpers.network import _NetworkManager
 
+@pytest.fixture(autouse=True, scope="session")
+def cleanup_environment():
+    _NetworkManager.clean_all_user_iptables_rules()
+    try:
+        result = subprocess.run(['docker', 'container', 'list', '-a'])
+        if result.returncode==0 and int(result.stdout) > 1:
+            logging.debug("Trying to kill unstopped containers...")
+            subprocess.run(['docker', 'kill', f'`docker container list -a`'])
+            subprocess.run(['docker', 'rm', f'`docker container list -a`'])
+            logging.debug("Unstopped containers killed")
+            r = subprocess.run(['docker-compose', 'ps', '--services', '--all'])
+            logging.debug(f"Docker ps before start:{r.stdout}")
+        else:
+            logging.debug(f"No running containers")
+    except:
+        pass
 
-def pytest_assertrepr_compare(op, left, right):
-    if isinstance(left, TSV) and isinstance(right, TSV) and op == '==':
-        return ['TabSeparated values differ: '] + left.diff(right)
+    yield
