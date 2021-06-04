efc4e8ab30ce32fa83c6e4a9240c779f35095f2f
Yatsishin Ilya <2159081+qoega@users.noreply.github.com>
2021-06-04 18:00:59 +0300

    opt-in for cleanup

--- tests/integration/conftest.py
+++ tests/integration/conftest.py
@@ -9,16 +9,24 @@ def cleanup_environment():
     _NetworkManager.clean_all_user_iptables_rules()
     try:
         result = subprocess.run(['docker', 'container', 'list', '-a'])
-        if result.returncode==0 and int(result.stdout) > 1:
-            logging.debug("Trying to kill unstopped containers...")
-            subprocess.run(['docker', 'kill', f'`docker container list -a`'])
-            subprocess.run(['docker', 'rm', f'`docker container list -a`'])
-            logging.debug("Unstopped containers killed")
-            r = subprocess.run(['docker-compose', 'ps', '--services', '--all'])
-            logging.debug(f"Docker ps before start:{r.stdout}")
+        if result.returncode != 0:
+            logging.error(f"docker ps returned error:{str(result.stderr)}")
         else:
-            logging.debug(f"No running containers")
-    except:
+            if int(result.stdout) > 1:
+                if env["PYTEST_CLEANUP_CONTAINERS"] != 1:
+                    logging.warning(f"Docker containters({result.stdout}) are running before tests run. They can be left from previous pytest run and cause test failures.\n"\
+                                    "You can set env PYTEST_CLEANUP_CONTAINERS=1 or use runner with --cleanup-containers argument to enable automatic containers cleanup.")
+                else:
+                    logging.debug("Trying to kill unstopped containers...")
+                    subprocess.run(['docker', 'kill', f'`docker container list -a`'])
+                    subprocess.run(['docker', 'rm', f'`docker container list -a`'])
+                    logging.debug("Unstopped containers killed")
+                    r = subprocess.run(['docker-compose', 'ps', '--services', '--all'])
+                    logging.debug(f"Docker ps before start:{r.stdout}")
+            else:
+                logging.debug(f"No running containers")
+    except Exception as e:
+        logging.error(f"cleanup_environment:{str(e)}")
         pass
 
     yield
