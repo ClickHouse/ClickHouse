7417f3526d7d636290871a59d6f2a188c5d9663a
Nikita Mikhaylov <mikhaylovnikitka@gmail.com>
2021-06-04 14:21:47 +0000

    delete partition from original table in case of errors while moving

--- programs/copier/ClusterCopier.h
+++ programs/copier/ClusterCopier.h
@@ -125,7 +125,7 @@ protected:
 
     static constexpr UInt64 max_table_tries = 3;
     static constexpr UInt64 max_shard_partition_tries = 3;
-    static constexpr UInt64 max_shard_partition_piece_tries_for_alter = 3;
+    static constexpr UInt64 max_shard_partition_piece_tries_for_alter = 10;
 
     bool tryProcessTable(const ConnectionTimeouts & timeouts, TaskTable & task_table);
 
