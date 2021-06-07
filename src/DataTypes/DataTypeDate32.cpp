120da1fd73929d1871d5e19b037ee34e3d707219
neng.liu <1398775315@qq.com>
2021-06-07 06:41:58 -0700

    date32 initial code

---
+++ src/DataTypes/DataTypeDate32.cpp
@@ -0,0 +1,22 @@
+#include <DataTypes/DataTypeDate32.h>
+#include <DataTypes/Serializations/SerializationDate32.h>
+#include <DataTypes/DataTypeFactory.h>
+
+namespace DB
+{
+bool DataTypeDate32::equals(const IDataType & rhs) const
+{
+    return typeid(rhs) == typeid(*this);
+}
+
+SerializationPtr DataTypeDate32::doGetDefaultSerialization() const
+{
+    return std::make_shared<SerializationDate32>();
+}
+
+void registerDataTypeDate32(DataTypeFactory & factory)
+{
+    factory.registerSimpleDataType("Date32", [] { return DataTypePtr(std::make_shared<DataTypeDate32>()); }, DataTypeFactory::CaseInsensitive);
+}
+
+}
