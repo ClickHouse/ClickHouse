3c2a5fcdf55832c7877d4811120b289634a5da07
ANDREI STAROVEROV <staroverovad@sandbox-407923068>
2021-06-09 19:37:42 +0300

    Fix tests

--- tests/queries/0_stateless/01857_create_function_that_already_exists.sql
+++ tests/queries/0_stateless/01857_create_function_that_already_exists.sql
@@ -1,4 +1,4 @@
 create function MyFunc3 as (a, b) -> a + b;
-create function MyFunc3 as (a) -> a || '!!!'; -- { serverError 585 }
-create function cast as x -> x + 1; -- { serverError 585 }
+create function MyFunc3 as (a) -> a || '!!!'; -- { serverError 587 }
+create function cast as x -> x + 1; -- { serverError 587 }
 drop function MyFunc3;
--- tests/queries/0_stateless/01858_drop_unknown_and_system_function.sql
+++ tests/queries/0_stateless/01858_drop_unknown_and_system_function.sql
@@ -1,2 +1,2 @@
 drop function unknownFunc; -- { serverError 46 }
-drop function CAST; -- { serverError 586 }
+drop function CAST; -- { serverError 588 }
