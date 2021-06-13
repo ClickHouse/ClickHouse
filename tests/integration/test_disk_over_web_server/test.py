603c06d02ef2f61d6c5aa4a4df5adffccae2ab08
kssenii <sumarokovakseniia@mail.ru>
2021-06-13 12:56:22 +0000

    Add test

---
+++ tests/integration/test_disk_over_web_server/test.py
@@ -0,0 +1,50 @@
+import pytest
+
+from helpers.cluster import ClickHouseCluster
+
+
+@pytest.fixture(scope="module")
+def cluster():
+    try:
+        cluster = ClickHouseCluster(__file__)
+        cluster.add_instance("node", main_configs=["configs/storage_conf.xml"], with_nginx=True)
+        cluster.start()
+        yield cluster
+
+    finally:
+        cluster.shutdown()
+
+
+def test_insert_select(cluster):
+    node = cluster.instances["node"]
+    node.query("""
+        CREATE TABLE test1 (id Int32)
+        ENGINE = MergeTree() ORDER BY id
+        SETTINGS storage_policy = 'static';
+    """)
+
+    node.query("INSERT INTO test1 SELECT number FROM numbers(100)")
+    result = node.query("SELECT count() FROM test1")
+    assert(int(result) == 100)
+
+    node.query("DETACH TABLE test1")
+    node.query("ATTACH TABLE test1")
+    result = node.query("SELECT count() FROM test1")
+    assert(int(result) == 100)
+
+    node = cluster.instances["node"]
+    node.query("""
+        CREATE TABLE test2 (id Int32)
+        ENGINE = MergeTree() ORDER BY id
+        SETTINGS storage_policy = 'static';
+    """)
+
+    node.query("INSERT INTO test2 SELECT number FROM numbers(500000)")
+    result = node.query("SELECT id FROM test2 ORDER BY id")
+    expected = node.query("SELECT number FROM numbers(500000)")
+    assert(result == expected)
+
+    node.query("DETACH TABLE test2")
+    node.query("ATTACH TABLE test2")
+    result = node.query("SELECT count() FROM test2")
+    assert(int(result) == 500000)
