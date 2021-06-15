d6e986fb5c482e777ff5a80355d81962c5c1ff57
Pavel Kruglov <avogar@avogar-dev.sas.yp-c.yandex.net>
2021-06-15 12:53:48 +0300

    Fix special build

--- src/Functions/multiIf.cpp
+++ src/Functions/multiIf.cpp
@@ -262,7 +262,7 @@ public:
     }
 
 private:
-    void executeShortCircuitArguments(ColumnsWithTypeAndName & arguments) const
+    static void executeShortCircuitArguments(ColumnsWithTypeAndName & arguments)
     {
         int last_short_circuit_argument_index = checkShirtCircuitArguments(arguments);
         if (last_short_circuit_argument_index < 0)
