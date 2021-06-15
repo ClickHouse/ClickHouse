d9e69575f6b8160d2979c8549df1e567ed5f79c4
Nikolai Kochetov <KochetovNicolai@users.noreply.github.com>
2021-06-15 13:38:11 +0300

    Update test_detach_part_wrong_partition_id.py

--- tests/integration/test_backward_compatibility/test_detach_part_wrong_partition_id.py
+++ tests/integration/test_backward_compatibility/test_detach_part_wrong_partition_id.py
@@ -4,7 +4,7 @@ from helpers.cluster import ClickHouseCluster
 
 cluster = ClickHouseCluster(__file__)
 # Version 21.6.3.14 has incompatible partition id for tables with UUID in partition key.
-node1 = cluster.add_instance('node1', image='yandex/clickhouse-server', tag='21.6.3.14', stay_alive=True, with_installed_binary=True)
+node_21_6 = cluster.add_instance('node_21_6', image='yandex/clickhouse-server', tag='21.6.3.14', stay_alive=True, with_installed_binary=True)
 
 
 @pytest.fixture(scope="module")
@@ -19,14 +19,14 @@ def start_cluster():
 def test_detach_part_wrong_partition_id(start_cluster):
 
     # Here we create table with partition by UUID.
-    node1.query("create table tab (id UUID, value UInt32) engine = MergeTree PARTITION BY (id) order by tuple()")
-    node1.query("insert into tab values ('61f0c404-5cb3-11e7-907b-a6006ad3dba0', 2)")
+    node_21_6.query("create table tab (id UUID, value UInt32) engine = MergeTree PARTITION BY (id) order by tuple()")
+    node_21_6.query("insert into tab values ('61f0c404-5cb3-11e7-907b-a6006ad3dba0', 2)")
 
     # After restart, partition id will be different.
     # There is a single 0-level part, which will become broken.
     # We expect that it will not be removed (as usual for 0-level broken parts),
     # but moved to /detached
-    node1.restart_with_latest_version()
+    node_21_6.restart_with_latest_version()
 
-    num_detached = node1.query("select count() from  system.detached_parts")
+    num_detached = node_21_6.query("select count() from  system.detached_parts")
     assert num_detached == '1\n'
