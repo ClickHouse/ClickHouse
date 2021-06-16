8ad59461f42a00ce16d6a5a21d10625f8022775e
Maksim Kita <maksim-kita@yandex-team.ru>
2021-06-16 21:19:05 +0300

    Updated function dateName before merge

--- tests/queries/0_stateless/01811_datename.sql
+++ tests/queries/0_stateless/01811_datename.sql
@@ -1,56 +1,72 @@
-SELECT dateName('year', toDateTime('2021-04-14 11:22:33'));
-SELECT dateName('quarter', toDateTime('2021-04-14 11:22:33'));
-SELECT dateName('month', toDateTime('2021-04-14 11:22:33'));
-SELECT dateName('dayofyear', toDateTime('2021-04-14 11:22:33'));
-SELECT dateName('day', toDateTime('2021-04-14 11:22:33'));
-SELECT dateName('week', toDateTime('2021-04-14 11:22:33'));
-SELECT dateName('weekday', toDateTime('2021-04-14 11:22:33'));
-SELECT dateName('hour', toDateTime('2021-04-14 11:22:33'));
-SELECT dateName('minute', toDateTime('2021-04-14 11:22:33'));
-SELECT dateName('second', toDateTime('2021-04-14 11:22:33'));
+WITH
+    toDate('2021-04-14') AS date_value,
+    toDateTime('2021-04-14 11:22:33') AS date_time_value,
+    toDateTime64('2021-04-14 11:22:33', 3) AS date_time_64_value
+SELECT dateName('year', date_value), dateName('year', date_time_value), dateName('year', date_time_64_value);
 
+WITH
+    toDate('2021-04-14') AS date_value,
+    toDateTime('2021-04-14 11:22:33') AS date_time_value,
+    toDateTime64('2021-04-14 11:22:33', 3) AS date_time_64_value
+SELECT dateName('quarter', date_value), dateName('quarter', date_time_value), dateName('quarter', date_time_64_value);
 
-SELECT dateName('year', toDateTime64('2021-04-14 11:22:33', 3));
-SELECT dateName('quarter', toDateTime64('2021-04-14 11:22:33', 3));
-SELECT dateName('month', toDateTime64('2021-04-14 11:22:33', 3));
-SELECT dateName('dayofyear', toDateTime64('2021-04-14 11:22:33', 3));
-SELECT dateName('day', toDateTime64('2021-04-14 11:22:33', 3));
-SELECT dateName('week', toDateTime64('2021-04-14 11:22:33', 3));
-SELECT dateName('weekday', toDateTime64('2021-04-14 11:22:33', 3));
-SELECT dateName('hour', toDateTime64('2021-04-14 11:22:33', 3));
-SELECT dateName('minute', toDateTime64('2021-04-14 11:22:33', 3));
-SELECT dateName('second', toDateTime64('2021-04-14 11:22:33', 3));
+WITH
+    toDate('2021-04-14') AS date_value,
+    toDateTime('2021-04-14 11:22:33') AS date_time_value,
+    toDateTime64('2021-04-14 11:22:33', 3) AS date_time_64_value
+SELECT dateName('month', date_value), dateName('month', date_time_value), dateName('month', date_time_64_value);
 
+WITH
+    toDate('2021-04-14') AS date_value,
+    toDateTime('2021-04-14 11:22:33') AS date_time_value,
+    toDateTime64('2021-04-14 11:22:33', 3) AS date_time_64_value
+SELECT dateName('dayofyear', date_value), dateName('dayofyear', date_time_value), dateName('dayofyear', date_time_64_value);
 
-SELECT dateName('year', toDate('2021-04-14'));
-SELECT dateName('quarter', toDate('2021-04-14'));
-SELECT dateName('month', toDate('2021-04-14'));
-SELECT dateName('dayofyear', toDate('2021-04-14'));
-SELECT dateName('day', toDate('2021-04-14'));
-SELECT dateName('week', toDate('2021-04-14'));
-SELECT dateName('weekday', toDate('2021-04-14'));
+WITH
+    toDate('2021-04-14') AS date_value,
+    toDateTime('2021-04-14 11:22:33') AS date_time_value,
+    toDateTime64('2021-04-14 11:22:33', 3) AS date_time_64_value
+SELECT dateName('day', date_value), dateName('day', date_time_value), dateName('day', date_time_64_value);
 
+WITH
+    toDate('2021-04-14') AS date_value,
+    toDateTime('2021-04-14 11:22:33') AS date_time_value,
+    toDateTime64('2021-04-14 11:22:33', 3) AS date_time_64_value
+SELECT dateName('week', date_value), dateName('week', date_time_value), dateName('week', date_time_64_value);
 
-SELECT dateName('hour', toDateTime('2021-04-14 11:22:33'), 'Europe/Moscow'),
-       dateName('hour', toDateTime('2021-04-14 11:22:33'), 'UTC');
+WITH
+    toDate('2021-04-14') AS date_value,
+    toDateTime('2021-04-14 11:22:33') AS date_time_value,
+    toDateTime64('2021-04-14 11:22:33', 3) AS date_time_64_value
+SELECT dateName('weekday', date_value), dateName('weekday', date_time_value), dateName('weekday', date_time_64_value);
 
-SELECT dateName('weekday', toDate('2021-04-12'));
-SELECT dateName('weekday', toDate('2021-04-13'));
-SELECT dateName('weekday', toDate('2021-04-14'));
-SELECT dateName('weekday', toDate('2021-04-15'));
-SELECT dateName('weekday', toDate('2021-04-16'));
-SELECT dateName('weekday', toDate('2021-04-17'));
-SELECT dateName('weekday', toDate('2021-04-18'));
+WITH
+    toDateTime('2021-04-14 11:22:33') AS date_time_value,
+    toDateTime64('2021-04-14 11:22:33', 3) AS date_time_64_value
+SELECT dateName('hour', date_time_value), dateName('hour', date_time_64_value);
 
-SELECT dateName('month', toDate('2021-01-14'));
-SELECT dateName('month', toDate('2021-02-14'));
-SELECT dateName('month', toDate('2021-03-14'));
-SELECT dateName('month', toDate('2021-04-14'));
-SELECT dateName('month', toDate('2021-05-14'));
-SELECT dateName('month', toDate('2021-06-14'));
-SELECT dateName('month', toDate('2021-07-14'));
-SELECT dateName('month', toDate('2021-08-14'));
-SELECT dateName('month', toDate('2021-09-14'));
-SELECT dateName('month', toDate('2021-10-14'));
-SELECT dateName('month', toDate('2021-11-14'));
-SELECT dateName('month', toDate('2021-12-14'));
+WITH
+    toDateTime('2021-04-14 11:22:33') AS date_time_value,
+    toDateTime64('2021-04-14 11:22:33', 3) AS date_time_64_value
+SELECT dateName('minute', date_time_value), dateName('minute', date_time_64_value);
+
+WITH
+    toDateTime('2021-04-14 11:22:33') AS date_time_value,
+    toDateTime64('2021-04-14 11:22:33', 3) AS date_time_64_value
+SELECT dateName('second', date_time_value), dateName('second', date_time_64_value);
+
+WITH
+    toDateTime('2021-04-14 23:22:33', 'UTC') as date
+SELECT
+    dateName('weekday', date, 'UTC'),
+    dateName('hour', date, 'UTC'),
+    dateName('minute', date, 'UTC'),
+    dateName('second', date, 'UTC');
+
+WITH
+    toDateTime('2021-04-14 23:22:33', 'UTC') as date
+SELECT
+    dateName('weekday', date, 'Europe/Moscow'),
+    dateName('hour', date, 'Europe/Moscow'),
+    dateName('minute', date, 'Europe/Moscow'),
+    dateName('second', date, 'Europe/Moscow');
