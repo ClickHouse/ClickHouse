f0d71e60b05ed65fd243d33132fd5ea2b0e04a85
robot-clickhouse <robot-clickhouse@yandex-team.ru>
2021-06-17 23:12:57 +0300

    Backport #25300 to 21.3: Support simple aggregate functions for SummingMergeTree.

--- src/Processors/Merges/Algorithms/SummingSortedAlgorithm.cpp
+++ src/Processors/Merges/Algorithms/SummingSortedAlgorithm.cpp
@@ -10,6 +10,7 @@
 #include <DataTypes/DataTypeArray.h>
 #include <DataTypes/DataTypeCustomSimpleAggregateFunction.h>
 #include <DataTypes/NestedUtils.h>
+#include <DataTypes/DataTypeLowCardinality.h>
 #include <IO/WriteHelpers.h>
 
 namespace DB
@@ -42,6 +43,11 @@ struct SummingSortedAlgorithm::AggregateDescription
     IColumn * merged_column = nullptr;
     AlignedBuffer state;
     bool created = false;
+
+    /// Those types are used only for simple aggregate functions.
+    /// For LowCardinality, convert to nested type. nested_type is nullptr if no conversion needed.
+    DataTypePtr nested_type; /// Nested type for LowCardinality, if it is.
+    DataTypePtr real_type; /// Type in header.
 
     /// In case when column has type AggregateFunction:
     /// use the aggregate function from itself instead of 'function' above.
@@ -262,6 +268,11 @@ static SummingSortedAlgorithm::ColumnsDefinition defineColumns(
                     desc.init(simple->getFunction(), true);
                     if (desc.function->allocatesMemoryInArena())
                         def.allocates_memory_in_arena = true;
+
+                    desc.real_type = column.type;
+                    desc.nested_type = recursiveRemoveLowCardinality(desc.real_type);
+                    if (desc.real_type.get() == desc.nested_type.get())
+                        desc.nested_type = nullptr;
                 }
                 else if (!is_agg_func)
                 {
@@ -380,6 +391,12 @@ static MutableColumns getMergedDataColumns(
 
             columns.emplace_back(ColumnTuple::create(std::move(tuple_columns)));
         }
+        else if (desc.is_simple_agg_func_type)
+        {
+            const auto & type = desc.nested_type ? desc.nested_type
+                                                 : desc.real_type;
+            columns.emplace_back(type->createColumn());
+        }
         else
             columns.emplace_back(header.safeGetByPosition(desc.column_numbers[0]).column->cloneEmpty());
     }
@@ -390,13 +407,22 @@ static MutableColumns getMergedDataColumns(
     return columns;
 }
 
-static void preprocessChunk(Chunk & chunk)
+static void preprocessChunk(Chunk & chunk, const SummingSortedAlgorithm::ColumnsDefinition & def)
 {
     auto num_rows = chunk.getNumRows();
     auto columns = chunk.detachColumns();
 
     for (auto & column : columns)
         column = column->convertToFullColumnIfConst();
+
+    for (const auto & desc : def.columns_to_aggregate)
+    {
+        if (desc.is_simple_agg_func_type && desc.nested_type)
+        {
+            auto & col = columns[desc.column_numbers[0]];
+            col = recursiveRemoveLowCardinality(col);
+        }
+    }
 
     chunk.setColumns(std::move(columns), num_rows);
 }
@@ -422,6 +448,12 @@ static void postprocessChunk(
             size_t tuple_size = desc.column_numbers.size();
             for (size_t i = 0; i < tuple_size; ++i)
                 res_columns[desc.column_numbers[i]] = assert_cast<const ColumnTuple &>(*column).getColumnPtr(i);
+        }
+        else if (desc.is_simple_agg_func_type && desc.nested_type)
+        {
+            const auto & from_type = desc.nested_type;
+            const auto & to_type = desc.real_type;
+            res_columns[desc.column_numbers[0]] = recursiveTypeConversion(std::move(column), from_type, to_type);
         }
         else
             res_columns[desc.column_numbers[0]] = std::move(column);
@@ -666,14 +698,14 @@ void SummingSortedAlgorithm::initialize(Inputs inputs)
 {
     for (auto & input : inputs)
         if (input.chunk)
-            preprocessChunk(input.chunk);
+            preprocessChunk(input.chunk, columns_definition);
 
     initializeQueue(std::move(inputs));
 }
 
 void SummingSortedAlgorithm::consume(Input & input, size_t source_num)
 {
-    preprocessChunk(input.chunk);
+    preprocessChunk(input.chunk, columns_definition);
     updateCursor(input, source_num);
 }
 
