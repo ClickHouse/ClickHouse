6ddc9121d155cbfcfebf209f592a7a27a53ebc20
Nikolay Degterinsky <evillique@gmail.com>
2021-06-19 23:24:35 +0000

    Changed function names in stateless tokenize test

--- tests/queries/0_stateless/01889_tokenize.sql
+++ tests/queries/0_stateless/01889_tokenize.sql
@@ -1,9 +1,9 @@
-SELECT tokenize('It is quite a wonderful day, isn\'t it?');
-SELECT tokenize('There is.... so much to learn!');
-SELECT tokenize('22:00 email@yandex.ru');
-SELECT tokenize('Токенизация каких-либо других языков?');
+SELECT splitByNonAlpha('It is quite a wonderful day, isn\'t it?');
+SELECT splitByNonAlpha('There is.... so much to learn!');
+SELECT splitByNonAlpha('22:00 email@yandex.ru');
+SELECT splitByNonAlpha('Токенизация каких-либо других языков?');
 
-SELECT tokenizeWhitespace('It is quite a wonderful day, isn\'t it?');
-SELECT tokenizeWhitespace('There is.... so much to learn!');
-SELECT tokenizeWhitespace('22:00 email@yandex.ru');
-SELECT tokenizeWhitespace('Токенизация каких-либо других языков?');
+SELECT splitByWhitespace('It is quite a wonderful day, isn\'t it?');
+SELECT splitByWhitespace('There is.... so much to learn!');
+SELECT splitByWhitespace('22:00 email@yandex.ru');
+SELECT splitByWhitespace('Токенизация каких-либо других языков?');
