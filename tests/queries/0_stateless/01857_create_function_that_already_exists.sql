fa4d2614cc4bc6d616762579f9f3495183c480f2
ANDREI STAROVEROV <staroverovad@sandbox-407923068>
2021-06-21 22:45:17 +0300

    Update tests after merge conflicts

--- tests/queries/0_stateless/01857_create_function_that_already_exists.sql
+++ tests/queries/0_stateless/01857_create_function_that_already_exists.sql
@@ -1,4 +1,4 @@
 create function MyFunc3 as (a, b) -> a + b;
-create function MyFunc3 as (a) -> a || '!!!'; -- { serverError 587 }
-create function cast as x -> x + 1; -- { serverError 587 }
+create function MyFunc3 as (a) -> a || '!!!'; -- { serverError 588 }
+create function cast as x -> x + 1; -- { serverError 588 }
 drop function MyFunc3;
