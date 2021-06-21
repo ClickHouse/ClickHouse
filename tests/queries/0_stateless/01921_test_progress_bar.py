bceaea250d631c9f2770a621efc67e55b56c3f9f
kssenii <sumarokovakseniia@mail.ru>
2021-06-21 14:20:29 +0000

    Add test for progress bar

---
+++ tests/queries/0_stateless/01921_test_progress_bar.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python3
+import os
+import sys
+import signal
+
+CURDIR = os.path.dirname(os.path.realpath(__file__))
+sys.path.insert(0, os.path.join(CURDIR, 'helpers'))
+
+from client import client, prompt, end_of_block
+
+log = None
+# uncomment the line below for debugging
+#log=sys.stdout
+
+with client(name='client1>', log=log) as client1, client(name='client2>', log=log) as client2:
+    client1.expect(prompt)
+    client1.send('SELECT number FROM numbers(100) FORMAT Null')
+    client1.expect('Progress: 100\.00 rows, 800\.00 B.*' + end_of_block)
+    # 0 rows becuase Format Null.
+    client1.expect('0 rows in set. Elapsed: 0\.[\\w]{3} sec.' + end_of_block)
