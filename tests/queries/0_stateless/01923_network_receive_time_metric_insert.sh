99e08b7406c5161ba21b78f4904bfdc32d99877a
Alexey Milovidov <milovidov@yandex-team.ru>
2021-06-22 23:22:13 +0300

    Make network receive time metric to correctly include the time spent waiting for data from the client to INSERT #9958

---
+++ tests/queries/0_stateless/01923_network_receive_time_metric_insert.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env bash
+
+CURDIR=$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)
+# shellcheck source=../shell_config.sh
+. "$CURDIR"/../shell_config.sh
+
+${CLICKHOUSE_CLIENT} --multiquery --query "DROP TABLE IF EXISTS t; CREATE TABLE t (x UInt64) ENGINE = Memory;"
+
+# Rate limit is chosen for operation to spent about one second.
+seq 1 1000 | pv --quiet --rate-limit 3893 | ${CLICKHOUSE_CLIENT} --query "INSERT INTO t FORMAT TSV"
+
+# We check that the value of NetworkReceiveElapsedMicroseconds is correctly include the time spent waiting data from the client.
+${CLICKHOUSE_CLIENT} --multiquery --query "SYSTEM FLUSH LOGS;
+    SELECT ProfileEvents.Values[indexOf(ProfileEvents.Names, 'NetworkReceiveElapsedMicroseconds')] >= 1000000 FROM system.query_log
+        WHERE current_database = currentDatabase() AND query_kind = 'Insert' AND event_date >= yesterday() AND type = 2 ORDER BY event_time DESC LIMIT 1;"
+
+${CLICKHOUSE_CLIENT} --query "DROP TABLE t"
