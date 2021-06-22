e0f330331bbc4423c682d6c6b5665dd5b7c513c6
kssenii <sumarokovakseniia@mail.ru>
2021-06-22 18:56:47 +0300

    Fix exception

--- tests/queries/0_stateless/01921_test_progress_bar.py
+++ tests/queries/0_stateless/01921_test_progress_bar.py
@@ -12,9 +12,8 @@ log = None
 # uncomment the line below for debugging
 #log=sys.stdout
 
-with client(name='client1>', log=log) as client1, client(name='client2>', log=log) as client2:
+with client(name='client1>', log=log) as client1:
     client1.expect(prompt)
     client1.send('SELECT number FROM numbers(100) FORMAT Null')
     client1.expect('Progress: 100\.00 rows, 800\.00 B.*' + end_of_block)
-    # 0 rows becuase Format Null.
     client1.expect('0 rows in set. Elapsed: [\\w]{1}\.[\\w]{3} sec.' + end_of_block)
