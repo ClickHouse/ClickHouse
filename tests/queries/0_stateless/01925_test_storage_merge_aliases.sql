b402e87352652a9a2e230ec852cc4a5cfd83b691
kssenii <sumarokovakseniia@mail.ru>
2021-06-25 07:00:15 +0000

    Fix

--- tests/queries/0_stateless/01925_test_storage_merge_aliases.sql
+++ tests/queries/0_stateless/01925_test_storage_merge_aliases.sql
@@ -53,5 +53,5 @@ insert into alias_2 (dt, col, col2, col3) values ('2020-02-01', 1, 2, 3);
 
 select 'alias2';
 select colAlias0, colAlias1, colAlias2, colAlias3 from alias_2;
-select colAlias3, colAlias2, colAlias1, colAlias0 from merge;
-select * from merge;
+select colAlias3, colAlias2, colAlias1, colAlias0 from merge order by dt;
+select * from merge order by dt;
