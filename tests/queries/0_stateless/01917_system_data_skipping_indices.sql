e766d2cb1f246495f5dde6eb56d4907f4a02226d
alexey-milovidov <milovidov@yandex-team.ru>
2021-06-26 03:33:59 +0300

    Update 01917_system_data_skipping_indices.sql

--- tests/queries/0_stateless/01917_system_data_skipping_indices.sql
+++ tests/queries/0_stateless/01917_system_data_skipping_indices.sql
@@ -23,12 +23,12 @@ CREATE TABLE data_01917_2
 Engine=MergeTree()
 ORDER BY name;
 
-SELECT * FROM system.data_skipping_indices;
+SELECT * FROM system.data_skipping_indices WHERE database = currentDatabase();
 
-SELECT count(*) FROM system.data_skipping_indices WHERE table = 'data_01917';
-SELECT count(*) FROM system.data_skipping_indices WHERE table = 'data_01917_2';
+SELECT count(*) FROM system.data_skipping_indices WHERE table = 'data_01917' AND database = currentDatabase();
+SELECT count(*) FROM system.data_skipping_indices WHERE table = 'data_01917_2' AND database = currentDatabase();
 
-SELECT name FROM system.data_skipping_indices WHERE type = 'minmax';
+SELECT name FROM system.data_skipping_indices WHERE type = 'minmax' AND database = currentDatabase();
 
 DROP TABLE data_01917;
 DROP TABLE data_01917_2;
