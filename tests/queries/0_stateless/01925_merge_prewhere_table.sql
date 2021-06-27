a2c23c91a1bcf30e6bf3c6451bed0040a67456e5
alexey-milovidov <milovidov@yandex-team.ru>
2021-06-28 00:31:18 +0300

    Update 01925_merge_prewhere_table.sql

--- tests/queries/0_stateless/01925_merge_prewhere_table.sql
+++ tests/queries/0_stateless/01925_merge_prewhere_table.sql
@@ -4,7 +4,7 @@ DROP TABLE IF EXISTS x;
 
 create table x_1 engine=Log as select * from numbers(10);
 create table x_2 engine=Log as select * from numbers(10);
-create table x engine=Merge(default, '^x_(1|2)$') as x_1;
+create table x engine=Merge(currentDatabase(), '^x_(1|2)$') as x_1;
 
 select _table, count() from x group by _table order by _table;
 
