86c58315b7660e3fe02be8044f78a93dfa8bd152
alexey-milovidov <milovidov@yandex-team.ru>
2021-06-27 19:31:55 +0300

    Update 01917_prewhere_column_type.sql

--- tests/queries/0_stateless/01917_prewhere_column_type.sql
+++ tests/queries/0_stateless/01917_prewhere_column_type.sql
@@ -1,6 +1,6 @@
 DROP TABLE IF EXISTS t1;
 
-CREATE TABLE t1 ( s String, f Float32, e UInt16 ) ENGINE = MergeTree ORDER BY tuple();
+CREATE TABLE t1 ( s String, f Float32, e UInt16 ) ENGINE = MergeTree ORDER BY tuple() SETTINGS min_bytes_for_wide_part = '100G';
 
 INSERT INTO t1 VALUES ('111', 1, 1);
 
