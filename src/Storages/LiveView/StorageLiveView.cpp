4dbd659d64218733234a0ce98424fab5c8a4b9f9
Alexey Milovidov <milovidov@yandex-team.ru>
2021-06-27 15:40:07 +0300

    Change error code in LIVE VIEW

--- src/Storages/LiveView/StorageLiveView.cpp
+++ src/Storages/LiveView/StorageLiveView.cpp
@@ -81,8 +81,7 @@ static StorageID extractDependentTable(ASTPtr & query, ContextPtr context, const
     {
         auto * ast_select = subquery->as<ASTSelectWithUnionQuery>();
         if (!ast_select)
-            throw Exception("Logical error while creating StorageLiveView."
-                            " Could not retrieve table name from select query.",
+            throw Exception("LIVE VIEWs are only supported for queries from tables, but there is no table name in select query.",
                             DB::ErrorCodes::QUERY_IS_NOT_SUPPORTED_IN_LIVE_VIEW);
         if (ast_select->list_of_selects->children.size() != 1)
             throw Exception("UNION is not supported for LIVE VIEW", ErrorCodes::QUERY_IS_NOT_SUPPORTED_IN_LIVE_VIEW);
