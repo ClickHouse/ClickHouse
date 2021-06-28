79a1a6c599ceaf6a4d1a39552c390f5d4c9ada02
Ivan Lezhankin <ilezhankin@yandex-team.ru>
2021-06-28 22:56:38 +0300

    Revert "Filter out some insert-settings"
    This reverts commit 14c5eff6dd0a55c8d8d70019600b9253120e493f.

--- src/Parsers/ASTInsertQuery.h
+++ src/Parsers/ASTInsertQuery.h
@@ -8,7 +8,8 @@ namespace DB
 
 class ReadBuffer;
 
-/// Insert Query
+/** INSERT query
+  */
 class ASTInsertQuery : public IAST
 {
 public:
@@ -23,14 +24,11 @@ public:
     ASTPtr watch;
 
     /// Data to insert
-    /// FIXME: maybe merge this with 'tail' buffer and make unified 'data' buffer for non-Native data?
     const char * data = nullptr;
     const char * end = nullptr;
 
     /// Query may have additional data if buffer is not nullptr
     ReadBuffer * tail;
-
-    bool expectsNativeData() const { return !data && !tail; }
 
     /// Try to find table function input() in SELECT part
     void tryFindInputFunction(ASTPtr & input_function) const;
