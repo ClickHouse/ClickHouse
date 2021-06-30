8aae56f24a3d60b3bb10a2890c01f1ee6f7345a2
Anton Ivashkin <iantonspb@yandex-team.ru>
2021-06-30 12:06:11 +0300

    Minor fixes based on code review result

--- src/Common/Config/configReadClient.cpp
+++ src/Common/Config/configReadClient.cpp
@@ -3,32 +3,35 @@
 #include <Poco/Util/LayeredConfiguration.h>
 #include "ConfigProcessor.h"
 #include <filesystem>
+#include <iostream>
 
 namespace fs = std::filesystem;
 
 namespace DB
 {
+
+/// Checks if file exists without thowing an execption but with message in console.
+bool safeFsExists(const auto & path)
+{
+    std::error_code ec;
+    bool res = fs::exists(path, ec);
+    if (ec)
+    {
+        std::cerr << "Can't check '" << path << "': [" << ec.value() << "] "  << ec.message() << std::endl;
+    }
+    return res;
+};
+
 bool configReadClient(Poco::Util::LayeredConfiguration & config, const std::string & home_path)
 {
-    auto safe_exists = [](const auto & path)
-    {
-        std::error_code ec;
-        bool res = fs::exists(path, ec);
-        if (ec.value() > 0)
-        {
-            LOG_ERROR(&Poco::Logger::get("ClickHouseClient"), "Can't check '{}': [{}]({})", path, ec.value(), ec.message());
-        }
-        return res;
-    };
-
     std::string config_path;
     if (config.has("config-file"))
         config_path = config.getString("config-file");
-    else if (safe_exists("./clickhouse-client.xml"))
+    else if (safeFsExists("./clickhouse-client.xml"))
         config_path = "./clickhouse-client.xml";
-    else if (!home_path.empty() && safe_exists(home_path + "/.clickhouse-client/config.xml"))
+    else if (!home_path.empty() && safeFsExists(home_path + "/.clickhouse-client/config.xml"))
         config_path = home_path + "/.clickhouse-client/config.xml";
-    else if (safe_exists("/etc/clickhouse-client/config.xml"))
+    else if (safeFsExists("/etc/clickhouse-client/config.xml"))
         config_path = "/etc/clickhouse-client/config.xml";
 
     if (!config_path.empty())
