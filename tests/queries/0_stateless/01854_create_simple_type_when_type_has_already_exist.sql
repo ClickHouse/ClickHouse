86d6099b1990a9f8e91b0a657b22630d84c9b62d
ANDREI STAROVEROV <staroverovad@sandbox-407923068>
2021-07-04 01:23:50 +0300

    Update tests

--- tests/queries/0_stateless/01854_create_simple_type_when_type_has_already_exist.sql
+++ tests/queries/0_stateless/01854_create_simple_type_when_type_has_already_exist.sql
@@ -1,7 +1,7 @@
 create type MyType3 as int;
 
-create type MyType3 as String; -- { serverError 588 }
+create type MyType3 as String; -- { serverError 590 }
 
-create type String as int; -- { serverError 588 }
+create type String as int; -- { serverError 590 }
 
 drop type MyType3;
