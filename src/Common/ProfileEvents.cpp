e38e59c6e5b34f818cc6667ea865f277560a1353
Alexey Milovidov <milovidov@yandex-team.ru>
2021-07-06 01:09:39 +0300

    Make it even worse

--- src/Common/ProfileEvents.cpp
+++ src/Common/ProfileEvents.cpp
@@ -43,8 +43,6 @@
     M(MarkCacheHits, "") \
     M(MarkCacheMisses, "") \
     M(CreatedReadBufferOrdinary, "") \
-    M(CreatedReadBufferDirectIO, "") \
-    M(CreatedReadBufferDirectIOFailed, "") \
     M(CreatedReadBufferMMap, "") \
     M(CreatedReadBufferMMapFailed, "") \
     M(DiskReadElapsedMicroseconds, "Total time spent waiting for read syscall. This include reads from page cache.") \
