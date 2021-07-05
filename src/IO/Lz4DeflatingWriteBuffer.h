c79f885e41cab7186c86486ba94e1fd5ada2d4fd
bharatnc <bharatnc@gmail.com>
2021-07-04 23:19:07 -0700

    LZ4 - compress write initial implementation

--- src/IO/Lz4DeflatingWriteBuffer.h
+++ src/IO/Lz4DeflatingWriteBuffer.h
@@ -5,10 +5,10 @@
 #include <IO/WriteBuffer.h>
 
 #include <lz4.h>
+#include <lz4frame.h>
 
 namespace DB
 {
-
 /// Performs compression using lz4 library and writes compressed data to out_ WriteBuffer.
 class Lz4DeflatingWriteBuffer : public BufferWithOwnMemory<WriteBuffer>
 {
@@ -32,6 +32,19 @@ private:
 
     std::unique_ptr<WriteBuffer> out;
 
-    /// TODO: Complete Implementation
+    bool finished = false;
+
+
+    void * in_buff;
+    void * out_buff;
+    size_t in_chunk_size;
+    size_t out_capacity;
+
+    LZ4F_compressionContext_t ctx;
+
+    size_t compression_ctx;
+
+    uint64_t count_in;
+    uint64_t count_out;
 };
 }
