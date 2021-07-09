0b504e975eb42012469a52b2062f12832b940bc6
Alexander Tokmakov <avtokmakov@yandex-team.ru>
2021-07-09 18:04:16 +0300

    add enums

--- src/Interpreters/ZooKeeperLog.cpp
+++ src/Interpreters/ZooKeeperLog.cpp
@@ -1,31 +1,27 @@
 #include <Interpreters/ZooKeeperLog.h>
 #include <Columns/ColumnArray.h>
-#include <Columns/ColumnFixedString.h>
 #include <Columns/ColumnString.h>
 #include <Columns/ColumnsNumber.h>
 #include <DataTypes/DataTypeArray.h>
 #include <DataTypes/DataTypeDateTime64.h>
 #include <DataTypes/DataTypeDate.h>
 #include <DataTypes/DataTypeNullable.h>
-#include <DataTypes/DataTypeDateTime.h>
 #include <DataTypes/DataTypeEnum.h>
 #include <DataTypes/DataTypeFactory.h>
-#include <DataTypes/DataTypeMap.h>
-#include <DataTypes/DataTypeLowCardinality.h>
 #include <DataTypes/DataTypeString.h>
 #include <DataTypes/DataTypesNumber.h>
 #include <Interpreters/ProfileEventsExt.h>
 #include <Interpreters/QueryLog.h>
 #include <Poco/Net/IPAddress.h>
-#include <Common/ClickHouseRevision.h>
 #include <Common/IPv6ToBinary.h>
+#include <Common/ZooKeeper/ZooKeeperConstants.h>
 
 namespace DB
 {
 
 NamesAndTypesList ZooKeeperLogElement::getNamesAndTypes()
 {
-    auto event_type = std::make_shared<DataTypeEnum8>(
+    auto type_enum = std::make_shared<DataTypeEnum8>(
         DataTypeEnum8::Values
             {
                 {"Send",            static_cast<Int8>(SEND)},
@@ -33,9 +29,87 @@ NamesAndTypesList ZooKeeperLogElement::getNamesAndTypes()
                 {"Finalize",        static_cast<Int8>(FINALIZE)},
             });
 
+    auto op_num_enum = std::make_shared<DataTypeEnum16>(
+        DataTypeEnum16::Values
+            {
+                {"Watch",               0},
+                {"Close",               static_cast<Int16>(Coordination::OpNum::Close)},
+                {"Error",               static_cast<Int16>(Coordination::OpNum::Error)},
+                {"Create",              static_cast<Int16>(Coordination::OpNum::Create)},
+                {"Remove",              static_cast<Int16>(Coordination::OpNum::Remove)},
+                {"Exists",              static_cast<Int16>(Coordination::OpNum::Exists)},
+                {"Get",                 static_cast<Int16>(Coordination::OpNum::Get)},
+                {"Set",                 static_cast<Int16>(Coordination::OpNum::Set)},
+                {"GetACL",              static_cast<Int16>(Coordination::OpNum::GetACL)},
+                {"SetACL",              static_cast<Int16>(Coordination::OpNum::SetACL)},
+                {"SimpleList",          static_cast<Int16>(Coordination::OpNum::SimpleList)},
+                {"Sync",                static_cast<Int16>(Coordination::OpNum::Sync)},
+                {"Heartbeat",           static_cast<Int16>(Coordination::OpNum::Heartbeat)},
+                {"List",                static_cast<Int16>(Coordination::OpNum::List)},
+                {"Check",               static_cast<Int16>(Coordination::OpNum::Check)},
+                {"Multi",               static_cast<Int16>(Coordination::OpNum::Multi)},
+                {"Auth",                static_cast<Int16>(Coordination::OpNum::Auth)},
+                {"SessionID",           static_cast<Int16>(Coordination::OpNum::SessionID)},
+            });
+
+    auto error_enum = std::make_shared<DataTypeEnum8>(
+        DataTypeEnum8::Values
+            {
+                {"ZOK",                         static_cast<Int8>(Coordination::Error::ZOK)},
+
+                {"ZSYSTEMERROR",                static_cast<Int8>(Coordination::Error::ZSYSTEMERROR)},
+                {"ZRUNTIMEINCONSISTENCY",       static_cast<Int8>(Coordination::Error::ZRUNTIMEINCONSISTENCY)},
+                {"ZDATAINCONSISTENCY",          static_cast<Int8>(Coordination::Error::ZDATAINCONSISTENCY)},
+                {"ZCONNECTIONLOSS",             static_cast<Int8>(Coordination::Error::ZCONNECTIONLOSS)},
+                {"ZMARSHALLINGERROR",           static_cast<Int8>(Coordination::Error::ZMARSHALLINGERROR)},
+                {"ZUNIMPLEMENTED",              static_cast<Int8>(Coordination::Error::ZUNIMPLEMENTED)},
+                {"ZOPERATIONTIMEOUT",           static_cast<Int8>(Coordination::Error::ZOPERATIONTIMEOUT)},
+                {"ZBADARGUMENTS",               static_cast<Int8>(Coordination::Error::ZBADARGUMENTS)},
+                {"ZINVALIDSTATE",               static_cast<Int8>(Coordination::Error::ZINVALIDSTATE)},
+
+                {"ZAPIERROR",                   static_cast<Int8>(Coordination::Error::ZAPIERROR)},
+                {"ZNONODE",                     static_cast<Int8>(Coordination::Error::ZNONODE)},
+                {"ZNOAUTH",                     static_cast<Int8>(Coordination::Error::ZNOAUTH)},
+                {"ZBADVERSION",                 static_cast<Int8>(Coordination::Error::ZBADVERSION)},
+                {"ZNOCHILDRENFOREPHEMERALS",    static_cast<Int8>(Coordination::Error::ZNOCHILDRENFOREPHEMERALS)},
+                {"ZNODEEXISTS",                 static_cast<Int8>(Coordination::Error::ZNODEEXISTS)},
+                {"ZNOTEMPTY",                   static_cast<Int8>(Coordination::Error::ZNOTEMPTY)},
+                {"ZSESSIONEXPIRED",             static_cast<Int8>(Coordination::Error::ZSESSIONEXPIRED)},
+                {"ZINVALIDCALLBACK",            static_cast<Int8>(Coordination::Error::ZINVALIDCALLBACK)},
+                {"ZINVALIDACL",                 static_cast<Int8>(Coordination::Error::ZINVALIDACL)},
+                {"ZAUTHFAILED",                 static_cast<Int8>(Coordination::Error::ZAUTHFAILED)},
+                {"ZCLOSING",                    static_cast<Int8>(Coordination::Error::ZCLOSING)},
+                {"ZNOTHING",                    static_cast<Int8>(Coordination::Error::ZNOTHING)},
+                {"ZSESSIONMOVED",               static_cast<Int8>(Coordination::Error::ZSESSIONMOVED)},
+            });
+
+    auto watch_type_enum = std::make_shared<DataTypeEnum8>(
+        DataTypeEnum8::Values
+            {
+                {"NONE",                    0},
+                {"CREATED",                 static_cast<Int8>(Coordination::Event::CREATED)},
+                {"DELETED",                 static_cast<Int8>(Coordination::Event::DELETED)},
+                {"CHANGED",                 static_cast<Int8>(Coordination::Event::CHANGED)},
+                {"CHILD",                   static_cast<Int8>(Coordination::Event::CHILD)},
+                {"SESSION",                 static_cast<Int8>(Coordination::Event::SESSION)},
+                {"NOTWATCHING",             static_cast<Int8>(Coordination::Event::NOTWATCHING)},
+            });
+
+    auto watch_state_enum = std::make_shared<DataTypeEnum16>(
+        DataTypeEnum16::Values
+            {
+                {"NONE",                0},
+                {"EXPIRED_SESSION",         static_cast<Int16>(Coordination::State::EXPIRED_SESSION)},
+                {"AUTH_FAILED",             static_cast<Int16>(Coordination::State::AUTH_FAILED)},
+                {"CONNECTING",              static_cast<Int16>(Coordination::State::CONNECTING)},
+                {"ASSOCIATING",             static_cast<Int16>(Coordination::State::ASSOCIATING)},
+                {"CONNECTED",               static_cast<Int16>(Coordination::State::CONNECTED)},
+                {"NOTCONNECTED",            static_cast<Int16>(Coordination::State::NOTCONNECTED)},
+            });
+
     return
     {
-        {"type", std::move(event_type)},
+        {"type", std::move(type_enum)},
         {"event_date", std::make_shared<DataTypeDate>()},
         {"event_time", std::make_shared<DataTypeDateTime64>(6)},
         {"address", DataTypeFactory::instance().get("IPv6")},
@@ -44,7 +118,7 @@ NamesAndTypesList ZooKeeperLogElement::getNamesAndTypes()
 
         {"xid", std::make_shared<DataTypeInt32>()},
         {"has_watch", std::make_shared<DataTypeUInt8>()},
-        {"op_num", std::make_shared<DataTypeInt32>()},
+        {"op_num", op_num_enum},
         {"path", std::make_shared<DataTypeString>()},
 
         {"data", std::make_shared<DataTypeString>()},
@@ -58,10 +132,10 @@ NamesAndTypesList ZooKeeperLogElement::getNamesAndTypes()
         {"request_idx", std::make_shared<DataTypeUInt32>()},
 
         {"zxid", std::make_shared<DataTypeInt64>()},
-        {"error", std::make_shared<DataTypeNullable>(std::make_shared<DataTypeInt32>())},
+        {"error", std::make_shared<DataTypeNullable>(error_enum)},
 
-        {"watch_type", std::make_shared<DataTypeInt32>()},
-        {"watch_state", std::make_shared<DataTypeInt32>()},
+        {"watch_type", watch_type_enum},
+        {"watch_state", watch_state_enum},
 
         {"path_created", std::make_shared<DataTypeString>()},
 
