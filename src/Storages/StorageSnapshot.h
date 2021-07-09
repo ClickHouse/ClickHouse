948b841f4be91c7823de64383bf707b8722b9d77
Anton Popov <pad11rus@gmail.com>
2021-07-09 17:54:53 +0300

    dynamic subcolumns: add mode for DESCRIBE query to show deduced object types

--- src/Storages/StorageSnapshot.h
+++ src/Storages/StorageSnapshot.h
@@ -51,6 +51,8 @@ struct StorageSnapshot
     /// Verify that all the requested names are in the table and are set correctly:
     /// list of names is not empty and the names do not repeat.
     void check(const Names & column_names) const;
+
+    DataTypePtr getConcreteType(const String & column_name) const;
 };
 
 using StorageSnapshotPtr = std::shared_ptr<const StorageSnapshot>;
---
+++ tests/queries/0_stateless/01825_type_json_describe.sql
@@ -0,0 +1,16 @@
+DROP TABLE IF EXISTS t_json_desc;
+
+CREATE TABLE t_json_desc (data JSON) ENGINE = MergeTree ORDER BY tuple();
+
+INSERT INTO t_json_desc FORMAT JSONAsObject {"k1": 10}
+;
+
+DESC TABLE t_json_desc;
+DESC TABLE t_json_desc SETTINGS describe_extend_object_type = 1;
+
+INSERT INTO t_json_desc FORMAT JSONAsObject {"k1": "q", "k2": [1, 2, 3]}
+;
+
+DESC TABLE t_json_desc SETTINGS describe_extend_object_type = 1;
+
+DROP TABLE IF EXISTS t_json_desc;
