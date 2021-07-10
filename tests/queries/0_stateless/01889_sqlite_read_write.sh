c98fd1cdb5f568d1c7f1065d8e3a2f89c0384cf3
kssenii <sumarokovakseniia@mail.ru>
2021-07-10 10:31:21 +0000

    Fix database engine attach, more tests

--- tests/queries/0_stateless/01889_sqlite_read_write.sh
+++ tests/queries/0_stateless/01889_sqlite_read_write.sh
@@ -4,20 +4,51 @@ CUR_DIR=$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)
 # shellcheck source=../shell_config.sh
 . "$CUR_DIR"/../shell_config.sh
 
-DATA_FILE=$CUR_DIR/data_sqlite/test.sqlite3
+DATA_FILE=$CUR_DIR/data_sqlite/db1
 
 
 ${CLICKHOUSE_CLIENT} --query='DROP DATABASE IF EXISTS sqlite_database'
+
 ${CLICKHOUSE_CLIENT} --query="CREATE DATABASE sqlite_database ENGINE = SQLite('${DATA_FILE}')"
 
-${CLICKHOUSE_CLIENT} --query='SELECT * FROM sqlite_database.`Some table`'
+${CLICKHOUSE_CLIENT} --query="select 'show database tables:'";
+${CLICKHOUSE_CLIENT} --query='SHOW TABLES FROM sqlite_database;'
 
-${CLICKHOUSE_CLIENT} --query='SELECT `some field` FROM sqlite_database.`Some table`;'
+${CLICKHOUSE_CLIENT} --query="select 'describe table:'";
+${CLICKHOUSE_CLIENT} --query='DESCRIBE TABLE sqlite_database.table1;'
+${CLICKHOUSE_CLIENT} --query='DESCRIBE TABLE sqlite_database.table2;'
 
-${CLICKHOUSE_CLIENT} --query='SELECT `string field` FROM sqlite_database.`Some table`;'
+${CLICKHOUSE_CLIENT} --query="select 'describe table:'";
+${CLICKHOUSE_CLIENT} --query='SHOW CREATE TABLE sqlite_database.table1;' | sed -r 's/(.*SQLite)(.*)/\1/'
+${CLICKHOUSE_CLIENT} --query='SHOW CREATE TABLE sqlite_database.table2;' | sed -r 's/(.*SQLite)(.*)/\1/'
 
-${CLICKHOUSE_CLIENT} --query='SELECT * FROM sqlite_database.`Empty table`;'
+${CLICKHOUSE_CLIENT} --query="select 'select *:'";
+${CLICKHOUSE_CLIENT} --query='SELECT * FROM sqlite_database.table1 ORDER BY col2'
+${CLICKHOUSE_CLIENT} --query='SELECT * FROM sqlite_database.table2 ORDER BY col1;'
 
-${CLICKHOUSE_CLIENT} --query='SELECT field2 FROM sqlite_database.`Empty table`;'
+${CLICKHOUSE_CLIENT} --query="select 'test insert:'";
+${CLICKHOUSE_CLIENT} --query="INSERT INTO  sqlite_database.table1 VALUES ('line4', 4);"
+${CLICKHOUSE_CLIENT} --query="INSERT INTO  sqlite_database.table2 VALUES (4, 'text4');"
+
+${CLICKHOUSE_CLIENT} --query='SELECT * FROM sqlite_database.table1 WHERE col2 > 3;'
+${CLICKHOUSE_CLIENT} --query='SELECT * FROM sqlite_database.table2 WHERE col1 > 3;'
+
+sqlite3 data_sqlite/db1 'DELETE FROM table1 WHERE col2 > 3'
+sqlite3 data_sqlite/db1 'DELETE FROM table2 WHERE col1 > 3'
+
+${CLICKHOUSE_CLIENT} --query="select 'after delete:'";
+${CLICKHOUSE_CLIENT} --query='SELECT * FROM sqlite_database.table1 WHERE col2 > 3;'
+${CLICKHOUSE_CLIENT} --query='SELECT * FROM sqlite_database.table2 WHERE col1 > 3;'
+
+${CLICKHOUSE_CLIENT} --query="select 'after detach:'";
+${CLICKHOUSE_CLIENT} --query='DETACH DATABASE sqlite_database;'
+${CLICKHOUSE_CLIENT} --query='ATTACH DATABASE sqlite_database;'
+#${CLICKHOUSE_CLIENT} --query='DETACH TABLE sqlite_database.table1;'
+#${CLICKHOUSE_CLIENT} --query='DETACH TABLE sqlite_database.table2;'
+#${CLICKHOUSE_CLIENT} --query='ATTACH TABLE sqlite_database.table1;'
+#${CLICKHOUSE_CLIENT} --query='ATTACH TABLE sqlite_database.table2;'
+
+${CLICKHOUSE_CLIENT} --query='SELECT * FROM sqlite_database.table1 ORDER BY col2'
+${CLICKHOUSE_CLIENT} --query='SELECT * FROM sqlite_database.table2 ORDER BY col1;'
 
 ${CLICKHOUSE_CLIENT} --query='DROP DATABASE IF EXISTS sqlite_database;'
