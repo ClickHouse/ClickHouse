3b6ea2227be3d6e86b142e5d43d0f9d83992b1c5
jasine <jasinechen@gmail.com>
2021-07-13 15:28:15 +0800

    fix postgres test and array of DateTime64 (#1)
    fix postgres test and array of DateTime64

--- tests/integration/test_postgresql_replica_database_engine/test.py
+++ tests/integration/test_postgresql_replica_database_engine/test.py
@@ -236,7 +236,7 @@ def test_different_data_types(started_cluster):
            (
                 key Integer NOT NULL PRIMARY KEY,
                 a Date[] NOT NULL,                          -- Date
-                b Timestamp[] NOT NULL,                     -- DateTime
+                b Timestamp[] NOT NULL,                     -- DateTime64
                 c real[][] NOT NULL,                        -- Float32
                 d double precision[][] NOT NULL,            -- Float64
                 e decimal(5, 5)[][][] NOT NULL,             -- Decimal32
@@ -253,11 +253,11 @@ def test_different_data_types(started_cluster):
     for i in range(10):
         instance.query('''
             INSERT INTO postgres_database.test_data_types VALUES
-            ({}, -32768, -2147483648, -9223372036854775808, 1.12345, 1.1234567890, 2147483647, 9223372036854775807, '2000-05-12 12:12:12', '2000-05-12', 0.2, 0.2)'''.format(i))
+            ({}, -32768, -2147483648, -9223372036854775808, 1.12345, 1.1234567890, 2147483647, 9223372036854775807, '2000-05-12 12:12:12.012345', '2000-05-12', 0.2, 0.2)'''.format(i))
 
     check_tables_are_synchronized('test_data_types', 'id');
     result = instance.query('SELECT * FROM test_database.test_data_types ORDER BY id LIMIT 1;')
-    assert(result == '0\t-32768\t-2147483648\t-9223372036854775808\t1.12345\t1.123456789\t2147483647\t9223372036854775807\t2000-05-12 12:12:12\t2000-05-12\t0.20000\t0.20000\n')
+    assert(result == '0\t-32768\t-2147483648\t-9223372036854775808\t1.12345\t1.123456789\t2147483647\t9223372036854775807\t2000-05-12 12:12:12.012345\t2000-05-12\t0.20000\t0.20000\n')
 
     for i in range(10):
         col = random.choice(['a', 'b', 'c'])
@@ -270,7 +270,7 @@ def test_different_data_types(started_cluster):
         "VALUES ("
         "0, "
         "['2000-05-12', '2000-05-12'], "
-        "['2000-05-12 12:12:12', '2000-05-12 12:12:12'], "
+        "['2000-05-12 12:12:12.012345', '2000-05-12 12:12:12.012345'], "
         "[[1.12345], [1.12345], [1.12345]], "
         "[[1.1234567891], [1.1234567891], [1.1234567891]], "
         "[[[0.11111, 0.11111]], [[0.22222, 0.22222]], [[0.33333, 0.33333]]], "
@@ -284,7 +284,7 @@ def test_different_data_types(started_cluster):
     expected = (
         "0\t" +
         "['2000-05-12','2000-05-12']\t" +
-        "['2000-05-12 12:12:12','2000-05-12 12:12:12']\t" +
+        "['2000-05-12 12:12:12.012345','2000-05-12 12:12:12.012345']\t" +
         "[[1.12345],[1.12345],[1.12345]]\t" +
         "[[1.1234567891],[1.1234567891],[1.1234567891]]\t" +
         "[[[0.11111,0.11111]],[[0.22222,0.22222]],[[0.33333,0.33333]]]\t"
@@ -622,7 +622,7 @@ def test_virtual_columns(started_cluster):
     instance.query("INSERT INTO postgres_database.postgresql_replica_0 SELECT number, number from numbers(10)")
     check_tables_are_synchronized('postgresql_replica_0');
 
-    # just check that it works, no check with `expected` becuase _version is taken as LSN, which will be different each time.
+    # just check that it works, no check with `expected` because _version is taken as LSN, which will be different each time.
     result = instance.query('SELECT key, value, _sign, _version FROM test_database.postgresql_replica_0;')
     print(result)
 
