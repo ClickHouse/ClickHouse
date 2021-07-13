7e0ea40de29070dcf2a29b78ca18c04612a7a561
zxc111 <zxc9007@gmail.com>
2021-07-14 00:36:21 +0800

    fix test case

--- tests/queries/0_stateless/01942_create_table_with_sample.sql
+++ tests/queries/0_stateless/01942_create_table_with_sample.sql
@@ -6,10 +6,13 @@ ENGINE = MergeTree
 ORDER BY tuple(x)
 SAMPLE BY x;  -- { serverError 59 }
 
+DROP TABLE IF EXISTS test_sample.sample_correct;
 CREATE TABLE IF NOT EXISTS test_sample.sample_correct
 (`x` String)
 ENGINE = MergeTree
 ORDER BY tuple(sipHash64(x))
 SAMPLE BY sipHash64(x);
+
+DROP TABLE test_sample.sample_correct;
 
 DROP DATABASE test_sample;
