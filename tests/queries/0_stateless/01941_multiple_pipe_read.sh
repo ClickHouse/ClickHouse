2f27ff26009a4403a256f8b8a5eac1c28ffb9d07
BoloniniD <denis@bolonin.ru>
2021-07-13 16:49:29 +0300

    Some changes after test review

--- tests/queries/0_stateless/01941_multiple_pipe_read.sh
+++ tests/queries/0_stateless/01941_multiple_pipe_read.sh
@@ -6,46 +6,27 @@ CURDIR=$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)
 SAMPLE_FILE="$CURDIR/01941_sample_data.csv"
 STD_ERROR_CAPTURED="$CURDIR/01941_std_error_captured.log"
 
+set -e
+
 echo 'File generated:'
 ${CLICKHOUSE_LOCAL} -q "SELECT number, if(number in (4,6), 'AAA', 'BBB') from numbers(7) FORMAT CSV" --format_csv_delimiter=, >"$SAMPLE_FILE"
 cat "$SAMPLE_FILE"
 
 echo '******************'
 echo 'Attempt to read twice from a pipeline'
-cat ${CLICKHOUSE_LOCAL} --structure 'key String' -q 'select * from table; select * from table;' <<<foo 2>"$STD_ERROR_CAPTURED"
-ret = $?
-echo "Return code: $ret"
-if [ "$ret" -eq "0" ]; then
-    echo "OK"
-else
-    echo "FAILED: return code is not 0"
-fi
+${CLICKHOUSE_LOCAL} --structure 'key String' -q 'select * from table; select * from table;' <<<foo
 
 echo '******************'
-./clickhouse local --structure 'key String' -q 'select * from table; select * from table;' --file /tmp/foo
 echo 'Attempt to read twice from a regular file'
-${CLICKHOUSE_LOCAL} --structure 'key String' -q 'select * from table; select * from table;' --file "$SAMPLE_FILE" 2>"$STD_ERROR_CAPTURED"
-ret = $?
-echo "Return code: $ret"
-if [ "$ret" -eq "0" ]; then
-    echo "OK"
-else
-    echo "FAILED: return code is not 0"
-fi
+${CLICKHOUSE_LOCAL} --structure 'key String' -q 'select * from table; select * from table;' --file "$SAMPLE_FILE"
 
 echo '******************'
-./clickhouse local --structure 'key String' -q 'select * from table; select * from table;' --file /tmp/foo
 echo 'Attempt to read twice from a pipe'
-mkfifo /tmp/pipe
+tpipe=$(mktemp -u)
+mkfifo "$tpipe"
 echo "$SAMPLE_FILE" > /tmp/pipe &
-${CLICKHOUSE_LOCAL} --structure 'key String' -q 'select * from table; select * from table;' --file /tmp/pipe 2>"$STD_ERROR_CAPTURED"
-ret = $?
-echo "Return code: $ret"
-if [ "$ret" -eq "0" ]; then
-    echo "FAILED"
-else
-    echo "OK: cannot read from pipe twice"
-fi
+${CLICKHOUSE_LOCAL} --structure 'key String' -q 'select * from table; select * from table;' --file /tmp/pipe
+
 
 rm "$STD_ERROR_CAPTURED" "$SAMPLE_FILE"
 
