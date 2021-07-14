5efd011340bf86f915b1443221e2ce88f29f4880
zxc111 <zxc9007@gmail.com>
2021-07-14 11:53:09 +0800

    fix test case

--- tests/queries/0_stateless/01942_create_table_with_sample.sql
+++ tests/queries/0_stateless/01942_create_table_with_sample.sql
@@ -1,18 +1,14 @@
-CREATE DATABASE IF NOT EXISTS test_sample;
-
-CREATE TABLE IF NOT EXISTS test_sample.sample_incorrect
+CREATE TABLE IF NOT EXISTS sample_incorrect
 (`x` UUID)
 ENGINE = MergeTree
 ORDER BY tuple(x)
 SAMPLE BY x;  -- { serverError 59 }
 
-DROP TABLE IF EXISTS test_sample.sample_correct;
-CREATE TABLE IF NOT EXISTS test_sample.sample_correct
+DROP TABLE IF EXISTS sample_correct;
+CREATE TABLE IF NOT EXISTS sample_correct
 (`x` String)
 ENGINE = MergeTree
 ORDER BY tuple(sipHash64(x))
 SAMPLE BY sipHash64(x);
 
-DROP TABLE test_sample.sample_correct;
-
-DROP DATABASE test_sample;
+DROP TABLE sample_correct;
