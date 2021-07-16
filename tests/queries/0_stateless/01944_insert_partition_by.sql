5ef5abc75411c24fe79ff72d5e05c30d312d711f
vdimir <vdimir@yandex-team.ru>
2021-07-16 14:08:42 +0300

    Add validation to insert partition by key to s3

--- tests/queries/0_stateless/01944_insert_partition_by.sql
+++ tests/queries/0_stateless/01944_insert_partition_by.sql
@@ -1,1 +1,3 @@
 INSERT INTO TABLE FUNCTION file('foo.csv', 'CSV', 'id Int32, val Int32') PARTITION BY val VALUES (1, 1), (2, 2); -- { serverError 48 }
+INSERT INTO TABLE FUNCTION s3('http://localhost:9001/foo/test_{_partition_id}.csv', 'admin', 'admin', 'CSV', 'id Int32, val String') PARTITION BY val VALUES (1, ''); -- { serverError 6 }
+INSERT INTO TABLE FUNCTION s3('http://localhost:9001/foo/test_{_partition_id}.csv', 'admin', 'admin', 'CSV', 'id Int32, val String') PARTITION BY val VALUES (1, 'abc\x00abc'); -- { serverError 6 }
