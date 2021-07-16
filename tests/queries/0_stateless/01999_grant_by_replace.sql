f4cd3807584e74a70eae3100028e184214e4f0c0
caspian <caspian46@gmail.como>
2021-07-16 11:21:50 +0800

    add tests

---
+++ tests/queries/0_stateless/01999_grant_by_replace.sql
@@ -0,0 +1,35 @@
+DROP USER IF EXISTS test_user_01999;
+
+CREATE USER test_user_01999;
+SHOW CREATE USER test_user_01999;
+
+SELECT 'A';
+SHOW GRANTS FOR test_user_01999;
+
+GRANT SELECT ON db1.* TO test_user_01999;
+GRANT SHOW ON db2.table TO test_user_01999;
+
+SELECT 'B';
+SHOW GRANTS FOR test_user_01999;
+
+GRANT BY REPLACE SELECT(col1) ON db3.table TO test_user_01999;
+
+SELECT 'C';
+SHOW GRANTS FOR test_user_01999;
+
+GRANT BY REPLACE SELECT(col3) ON db3.table3, SELECT(col1, col2) ON db4.table4 TO test_user_01999;
+
+SELECT 'D';
+SHOW GRANTS FOR test_user_01999;
+
+GRANT BY REPLACE SELECT(cola) ON db5.table, INSERT(colb) ON db6.tb61, SHOW ON db7.* TO test_user_01999;
+
+SELECT 'E';
+SHOW GRANTS FOR test_user_01999;
+
+SELECT 'F';
+GRANT BY REPLACE SELECT ON all.* TO test_user_01999;
+SHOW GRANTS FOR test_user_01999;
+
+DROP USER test_user_01999;
+
