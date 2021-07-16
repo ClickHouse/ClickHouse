25d1d9c2b4f3bd74a5247768329743d4e6879500
caspian <caspian46@gmail.como>
2021-07-16 17:05:57 +0800

    update tests

--- tests/queries/0_stateless/01999_grant_by_replace.sql
+++ tests/queries/0_stateless/01999_grant_by_replace.sql
@@ -12,23 +12,27 @@ GRANT SHOW ON db2.table TO test_user_01999;
 SELECT 'B';
 SHOW GRANTS FOR test_user_01999;
 
-GRANT BY REPLACE SELECT(col1) ON db3.table TO test_user_01999;
+REPLACE GRANT SELECT(col1) ON db3.table TO test_user_01999;
 
 SELECT 'C';
 SHOW GRANTS FOR test_user_01999;
 
-GRANT BY REPLACE SELECT(col3) ON db3.table3, SELECT(col1, col2) ON db4.table4 TO test_user_01999;
+REPLACE GRANT SELECT(col3) ON db3.table3, SELECT(col1, col2) ON db4.table4 TO test_user_01999;
 
 SELECT 'D';
 SHOW GRANTS FOR test_user_01999;
 
-GRANT BY REPLACE SELECT(cola) ON db5.table, INSERT(colb) ON db6.tb61, SHOW ON db7.* TO test_user_01999;
+REPLACE GRANT SELECT(cola) ON db5.table, INSERT(colb) ON db6.tb61, SHOW ON db7.* TO test_user_01999;
 
 SELECT 'E';
 SHOW GRANTS FOR test_user_01999;
 
 SELECT 'F';
-GRANT BY REPLACE SELECT ON all.* TO test_user_01999;
+REPLACE GRANT SELECT ON all.* TO test_user_01999;
+SHOW GRANTS FOR test_user_01999;
+
+SELECT 'G';
+REPLACE GRANT USAGE ON *.* TO test_user_01999;
 SHOW GRANTS FOR test_user_01999;
 
 DROP USER test_user_01999;
