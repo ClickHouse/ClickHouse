83e581244c5b575d94a7d125099d45f0d320b927
Alexey Boykov <mathalex@mail.ru>
2021-07-16 16:18:18 +0300

    add current_database in tests

--- tests/queries/0_stateless/01943_query_id_check.sql
+++ tests/queries/0_stateless/01943_query_id_check.sql
@@ -1,10 +1,10 @@
 CREATE TABLE tmp ENGINE = TinyLog AS SELECT queryID();
 SYSTEM FLUSH LOGS;
-SELECT query FROM system.query_log WHERE query_id = (SELECT * FROM tmp) LIMIT 1;
+SELECT query FROM system.query_log WHERE query_id = (SELECT * FROM tmp) AND current_database = currentDatabase() LIMIT 1;
 DROP TABLE tmp;
 
 CREATE TABLE tmp ENGINE = TinyLog AS SELECT initialQueryID();
 SYSTEM FLUSH LOGS;
-SELECT query FROM system.query_log WHERE initial_query_id = (SELECT * FROM tmp) LIMIT 1;
+SELECT query FROM system.query_log WHERE initial_query_id = (SELECT * FROM tmp) AND current_database = currentDatabase() LIMIT 1;
 DROP TABLE tmp;
 
