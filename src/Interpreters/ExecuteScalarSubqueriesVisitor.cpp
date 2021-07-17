36dc4fc532c701cac4ec46cac44ec504e2f83770
robot-clickhouse <robot-clickhouse@yandex-team.ru>
2021-07-17 04:15:27 +0300

    Backport #26428 to 20.8: Fix msan crash from 22517

--- src/Interpreters/ExecuteScalarSubqueriesVisitor.cpp
+++ src/Interpreters/ExecuteScalarSubqueriesVisitor.cpp
@@ -158,9 +158,14 @@ void ExecuteScalarSubqueriesMatcher::visit(const ASTSubquery & subquery, ASTPtr 
     // Always convert to literals when there is no query context.
     if (data.only_analyze || !settings.enable_scalar_subquery_optimization || worthConvertingToLiteral(scalar) || !data.context.hasQueryContext())
     {
+        /// subquery and ast can be the same object and ast will be moved.
+        /// Save these fields to avoid use after move.
+        auto alias = subquery.alias;
+        auto prefer_alias_to_column_name = subquery.prefer_alias_to_column_name;
+
         auto lit = std::make_unique<ASTLiteral>((*scalar.safeGetByPosition(0).column)[0]);
-        lit->alias = subquery.alias;
-        lit->prefer_alias_to_column_name = subquery.prefer_alias_to_column_name;
+        lit->alias = alias;
+        lit->prefer_alias_to_column_name = prefer_alias_to_column_name;
         ast = addTypeConversionToAST(std::move(lit), scalar.safeGetByPosition(0).type->getName());
 
         /// If only analyze was requested the expression is not suitable for constant folding, disable it.
@@ -168,8 +173,8 @@ void ExecuteScalarSubqueriesMatcher::visit(const ASTSubquery & subquery, ASTPtr 
         {
             ast->as<ASTFunction>()->alias.clear();
             auto func = makeASTFunction("identity", std::move(ast));
-            func->alias = subquery.alias;
-            func->prefer_alias_to_column_name = subquery.prefer_alias_to_column_name;
+            func->alias = alias;
+            func->prefer_alias_to_column_name = prefer_alias_to_column_name;
             ast = std::move(func);
         }
     }
