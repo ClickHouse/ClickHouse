e2a2ab5e4dfd71335f71a4cd7054944495f86747
ANDREI STAROVEROV <staroverovad@sandbox-407923068>
2021-07-21 01:02:18 +0300

    Fix tests

--- src/Interpreters/InterpreterDropFunctionQuery.cpp
+++ src/Interpreters/InterpreterDropFunctionQuery.cpp
@@ -2,6 +2,7 @@
 #include <Interpreters/Context.h>
 #include <Interpreters/FunctionNameNormalizer.h>
 #include <Interpreters/InterpreterDropFunctionQuery.h>
+#include <Interpreters/UserDefinedObjectsOnDisk.h>
 #include <Functions/FunctionFactory.h>
 #include <Parsers/ASTDropFunctionQuery.h>
 
@@ -15,6 +16,7 @@ BlockIO InterpreterDropFunctionQuery::execute()
     FunctionNameNormalizer().visit(query_ptr.get());
     auto & drop_function_query = query_ptr->as<ASTDropFunctionQuery &>();
     FunctionFactory::instance().unregisterUserDefinedFunction(drop_function_query.function_name);
+    UserDefinedObjectsOnDisk::instance().removeUserDefinedFunction(getContext(), drop_function_query.function_name);
     return {};
 }
 
