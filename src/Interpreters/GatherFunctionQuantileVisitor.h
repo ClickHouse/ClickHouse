cb06e5fc4afb128cb402f45b0bbc196274685fc6
hexiaoting <hewenting_ict@163.com>
2021-07-21 14:43:40 +0800

    Fuse multi quantile funcs with same arguments to one function.

---
+++ src/Interpreters/GatherFunctionQuantileVisitor.h
@@ -0,0 +1,82 @@
+#pragma once
+
+#include <Parsers/ASTFunction.h>
+#include <Parsers/IAST.h>
+#include <Interpreters/InDepthNodeVisitor.h>
+#include <AggregateFunctions/AggregateFunctionQuantile.h>
+
+namespace DB
+{
+
+ static const std::unordered_map<String, String> quantile_fuse_name_mapping = {
+        {NameQuantile::name, NameQuantiles::name},
+        {NameQuantileDeterministic::name, NameQuantilesDeterministic::name},
+        {NameQuantileExact::name, NameQuantilesExact::name},
+        {NameQuantileExactLow::name, NameQuantilesExactLow::name},
+        {NameQuantileExactHigh::name, NameQuantilesExactHigh::name},
+        {NameQuantileExactExclusive::name, NameQuantilesExactExclusive::name},
+        {NameQuantileExactInclusive::name, NameQuantilesExactInclusive::name},
+        {NameQuantileExactWeighted::name, NameQuantilesExactWeighted::name},
+        {NameQuantileTiming::name, NameQuantilesTiming::name},
+        {NameQuantileTimingWeighted::name, NameQuantilesTimingWeighted::name},
+        {NameQuantileTDigest::name, NameQuantilesTDigest::name},
+        {NameQuantileTDigestWeighted::name, NameQuantilesTDigestWeighted::name},
+        {NameQuantileBFloat16::name, NameQuantilesBFloat16::name}
+    };
+
+/// Gather all the quantilexxx functions
+class GatherFunctionQuantileData
+{
+public:
+    struct FuseQuantileAggregatesData
+    {
+        std::unordered_map<String, std::vector<ASTFunction *>> arg_map_function;
+        void addFuncNode(ASTFunction * func)
+        {
+            auto argument = func->arguments->children.at(0)->getColumnName();
+
+            /// This functions needs two arguments.
+            if (func->name == NameQuantileDeterministic::name
+                || func->name == NameQuantileExactWeighted::name
+                || func->name == NameQuantileTimingWeighted::name
+                || func->name == NameQuantileTDigestWeighted::name)
+                argument = argument + "," + func->arguments->children.at(1)->getColumnName();
+
+            auto it = arg_map_function.find(argument);
+            if (it != arg_map_function.end())
+            {
+                it->second.push_back(func);
+            }
+            else
+            {
+                std::vector<ASTFunction *> new_func_list;
+                new_func_list.push_back(func);
+                arg_map_function[argument] = new_func_list;
+            }
+        }
+    };
+
+    using TypeToVisit = ASTFunction;
+    std::unordered_map<String, FuseQuantileAggregatesData> fuse_quantile;
+    void visit(ASTFunction & function, ASTPtr &)
+    {
+        if (quantile_fuse_name_mapping.find(function.name) == quantile_fuse_name_mapping.end())
+            return;
+
+        auto it = fuse_quantile.find(function.name);
+        if (it != fuse_quantile.end())
+        {
+            it->second.addFuncNode(&function);
+        }
+        else
+        {
+            FuseQuantileAggregatesData funcs{};
+            funcs.addFuncNode(&function);
+            fuse_quantile[function.name] = funcs;
+        }
+    }
+};
+
+using GatherFunctionQuantileVisitor = InDepthNodeVisitor<OneTypeMatcher<GatherFunctionQuantileData>, true>;
+
+}
---
+++ tests/queries/0_stateless/01956_fuse_quantile_optimization.sql
@@ -0,0 +1,54 @@
+DROP TABLE IF EXISTS datetime;
+CREATE TABLE datetime (d DateTime('UTC')) ENGINE = Memory;
+INSERT INTO datetime(d) VALUES(toDateTime('2016-06-15 23:00:00', 'UTC'))
+
+SELECT quantile(0.2)(d), quantile(0.3)(d) FROM datetime;
+SELECT quantileDeterministic(0.2)(d, 1), quantileDeterministic(0.5)(d, 1) FROM datetime;
+SELECT quantileExact(0.2)(d), quantileExact(0.5)(d) FROM datetime;
+SELECT quantileExactWeighted(0.2)(d, 1), quantileExactWeighted(0.4)(d, 1), quantileExactWeighted(0.3)(d, 2) FROM datetime;
+SELECT quantileTiming(0.2)(d), quantileTiming(0.3)(d), quantileTiming(0.2)(d+1) FROM datetime;
+SELECT quantileTimingWeighted(0.2)(d, 1), quantileTimingWeighted(0.3)(d, 1), quantileTimingWeighted(0.2)(d, 2) FROM datetime;
+SELECT quantileTDigest(0.2)(d), quantileTDigest(0.3)(d), quantileTDigest(0.2)(d + 1) FROM datetime;
+SELECT quantileTDigestWeighted(0.2)(d, 1), quantileTDigestWeighted(0.3)(d, 1), quantileTDigestWeighted(0.4)(d, 2) FROM datetime;
+SELECT quantileBFloat16(0.2)(d), quantileBFloat16(0.3)(d), quantileBFloat16(0.4)(d + 1) FROM datetime;
+
+
+SELECT '---------After fuse result-----------';
+set optimize_fuse_quantile=true;
+SELECT 'quantile:';
+EXPLAIN SYNTAX SELECT quantile(0.2)(d), quantile(0.3)(d) FROM datetime;
+SELECT quantile(0.2)(d), quantile(0.3)(d) FROM datetime;
+
+SELECT 'quantileDeterministic:';
+EXPLAIN SYNTAX SELECT quantileDeterministic(0.2)(d, 1), quantileDeterministic(0.5)(d, 1) FROM datetime;
+SELECT quantileDeterministic(0.2)(d, 1), quantileDeterministic(0.5)(d, 1) FROM datetime;
+
+SELECT 'quantileExact:';
+EXPLAIN SYNTAX SELECT quantileExact(0.2)(d), quantileExact(0.5)(d) FROM datetime;
+SELECT quantileExact(0.2)(d), quantileExact(0.5)(d) FROM datetime;
+
+SELECT 'quantileExactWeighted:';
+EXPLAIN SYNTAX SELECT quantileExactWeighted(0.2)(d, 1), quantileExactWeighted(0.4)(d, 1), quantileExactWeighted(0.3)(d, 2) FROM datetime;
+SELECT quantileExactWeighted(0.2)(d, 1), quantileExactWeighted(0.4)(d, 1), quantileExactWeighted(0.3)(d, 2) FROM datetime;
+
+SELECT 'quantileTiming:';
+EXPLAIN SYNTAX SELECT quantileTiming(0.2)(d), quantileTiming(0.3)(d), quantileTiming(0.2)(d+1) FROM datetime;
+SELECT quantileTiming(0.2)(d), quantileTiming(0.3)(d), quantileTiming(0.2)(d+1) FROM datetime;
+
+SELECT 'quantileTimingWeighted:';
+EXPLAIN SYNTAX SELECT quantileTimingWeighted(0.2)(d, 1), quantileTimingWeighted(0.3)(d, 1), quantileTimingWeighted(0.2)(d, 2) FROM datetime;
+SELECT quantileTimingWeighted(0.2)(d, 1), quantileTimingWeighted(0.3)(d, 1), quantileTimingWeighted(0.2)(d, 2) FROM datetime;
+
+SELECT 'quantileTDigest:';
+EXPLAIN SYNTAX SELECT quantileTDigest(0.2)(d), quantileTDigest(0.3)(d), quantileTDigest(0.2)(d + 1) FROM datetime;
+SELECT quantileTDigest(0.2)(d), quantileTDigest(0.3)(d), quantileTDigest(0.2)(d + 1) FROM datetime;
+
+SELECT 'quantileTDigestWeighted:';
+EXPLAIN SYNTAX SELECT quantileTDigestWeighted(0.2)(d, 1), quantileTDigestWeighted(0.3)(d, 1), quantileTDigestWeighted(0.4)(d, 2) FROM datetime;
+SELECT quantileTDigestWeighted(0.2)(d, 1), quantileTDigestWeighted(0.3)(d, 1), quantileTDigestWeighted(0.4)(d, 2) FROM datetime;
+
+SELECT 'quantileBFloat16:';
+EXPLAIN SYNTAX SELECT quantileBFloat16(0.2)(d), quantileBFloat16(0.3)(d), quantileBFloat16(0.4)(d + 1) FROM datetime;
+SELECT quantileBFloat16(0.2)(d), quantileBFloat16(0.3)(d), quantileBFloat16(0.4)(d + 1) FROM datetime;
+
+DROP TABLE datetime;
