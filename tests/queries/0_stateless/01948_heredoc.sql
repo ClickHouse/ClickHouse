3ad5733e8bb2ac3a99946cd4c7908e724beca2aa
Maksim Kita <kitaetoya@gmail.com>
2021-07-23 00:07:22 +0300

    Fixed code review issues

--- tests/queries/0_stateless/01948_heredoc.sql
+++ tests/queries/0_stateless/01948_heredoc.sql
@@ -1,4 +1,13 @@
+SELECT $$$$;
 SELECT $$VALUE$$;
 SELECT $doc$$doc$;
 SELECT $doc$VALUE$doc$;
 SELECT $doc$'VALUE'$doc$;
+SELECT $doc$$do$ $ doc$ $doc $ $doco$$doc$;
+SELECT $doc$$do$ $ doc$ $doc $ $doco$$doc$, $doc$$do$ $ doc$ $doc $ $doco$$doc$;
+
+SELECT $doc$ТЕСТ$doc$;
+SELECT $doc$该类型的引擎$doc$;
+
+SELECT $РАЗДЕЛИТЕЛЬ$VALUE$РАЗДЕЛИТЕЛЬ$;
+SELECT $该类型的引擎$VALUE$该类型的引擎$;
