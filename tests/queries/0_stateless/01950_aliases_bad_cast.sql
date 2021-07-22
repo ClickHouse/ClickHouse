de903143730a97204de7ced1f0e680909d983ad9
Alexey Milovidov <milovidov@yandex-team.ru>
2021-07-23 02:37:38 +0300

    Fix some tests

--- tests/queries/0_stateless/01950_aliases_bad_cast.sql
+++ tests/queries/0_stateless/01950_aliases_bad_cast.sql
@@ -1,2 +1,2 @@
-SELECT 1, * FROM (SELECT NULL AS `1`); -- { serverError 36 }
-SELECT '7', 'xyz', * FROM (SELECT NULL AS `'xyz'`); -- { serverError 36 }
+SELECT 1, * FROM (SELECT NULL AS `1`); -- { serverError 352 }
+SELECT '7', 'xyz', * FROM (SELECT NULL AS `'xyz'`); -- { serverError 352 }
