894d4892feeb3803d2838b5f786eebabaa148e3d
Alexey Milovidov <milovidov@yandex-team.ru>
2021-07-26 06:27:39 +0300

    Fix test

--- tests/queries/0_stateless/01440_to_date_monotonicity.sql
+++ tests/queries/0_stateless/01440_to_date_monotonicity.sql
@@ -2,7 +2,7 @@ DROP TABLE IF EXISTS tdm;
 DROP TABLE IF EXISTS tdm2;
 CREATE TABLE tdm (x DateTime('Europe/Moscow')) ENGINE = MergeTree ORDER BY x SETTINGS write_final_mark = 0;
 INSERT INTO tdm VALUES (now());
-SELECT count(x) FROM tdm WHERE toDate(x) < today() SETTINGS max_rows_to_read = 1;
+SELECT count(x) FROM tdm WHERE toDate(x) < toDate(now(), 'Europe/Moscow') SETTINGS max_rows_to_read = 1;
 
 SELECT toDate(-1), toDate(10000000000000, 'Europe/Moscow'), toDate(100), toDate(65536, 'UTC'), toDate(65535, 'Europe/Moscow');
 SELECT toDateTime(-1, 'Europe/Moscow'), toDateTime(10000000000000, 'Europe/Moscow'), toDateTime(1000, 'Europe/Moscow');
