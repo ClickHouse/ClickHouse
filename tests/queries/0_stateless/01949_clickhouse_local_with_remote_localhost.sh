2b410c744a38f5f50f8361e6ed81a44812e884fd
Raúl Marín <git@rmr.ninja>
2021-07-26 17:28:32 +0200

    Ignore ASAN warning

--- tests/queries/0_stateless/01949_clickhouse_local_with_remote_localhost.sh
+++ tests/queries/0_stateless/01949_clickhouse_local_with_remote_localhost.sh
@@ -12,12 +12,14 @@ if [ "$CLICKHOUSE_HOST" == "localhost" ]; then
     ${CLICKHOUSE_LOCAL} -q "SELECT 'test1', * FROM remote('127.0.0.1', '${CLICKHOUSE_DATABASE}.remote_table') LIMIT 3;" 2>&1 | awk '{print $1 $2}'
 
     # Now connecting to 127.0.0.1:9000 will connect to the database we are running tests against
-    ${CLICKHOUSE_LOCAL} -q "SELECT 'test2', * FROM remote('127.0.0.1:${CLICKHOUSE_PORT_TCP}', '${CLICKHOUSE_DATABASE}.remote_table') LIMIT 3 FORMAT CSV;"
+    ${CLICKHOUSE_LOCAL} -q "SELECT 'test2', * FROM remote('127.0.0.1:${CLICKHOUSE_PORT_TCP}', '${CLICKHOUSE_DATABASE}.remote_table') LIMIT 3 FORMAT CSV;" 2>&1 \
+        | grep -av "ASan doesn't fully support makecontext/swapcontext functions"
 
     # Same test now against localhost
     ${CLICKHOUSE_LOCAL} -q "SELECT 'test3', * FROM remote('localhost', '${CLICKHOUSE_DATABASE}.remote_table') LIMIT 3;" 2>&1 | awk '{print $1 $2}'
 
-    ${CLICKHOUSE_LOCAL} -q "SELECT 'test4', * FROM remote('localhost:${CLICKHOUSE_PORT_TCP}', '${CLICKHOUSE_DATABASE}.remote_table') LIMIT 3 FORMAT CSV;"
+    ${CLICKHOUSE_LOCAL} -q "SELECT 'test4', * FROM remote('localhost:${CLICKHOUSE_PORT_TCP}', '${CLICKHOUSE_DATABASE}.remote_table') LIMIT 3 FORMAT CSV;" 2>&1 \
+        | grep -av "ASan doesn't fully support makecontext/swapcontext functions"
 else
     # Can't test without localhost
     echo Code:81.
