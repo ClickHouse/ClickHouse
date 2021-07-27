0771f8bdc933558c9e638aa32101d31a66f4440f
Alexey Milovidov <milovidov@yandex-team.ru>
2021-07-27 06:55:59 +0300

    Fix test

--- tests/queries/0_stateless/01440_to_date_monotonicity.sql
+++ tests/queries/0_stateless/01440_to_date_monotonicity.sql
@@ -8,9 +8,11 @@ SELECT toDate(-1), toDate(10000000000000, 'Europe/Moscow'), toDate(100), toDate(
 SELECT toDateTime(-1, 'Europe/Moscow'), toDateTime(10000000000000, 'Europe/Moscow'), toDateTime(1000, 'Europe/Moscow');
 
 CREATE TABLE tdm2 (timestamp UInt32) ENGINE = MergeTree ORDER BY timestamp SETTINGS index_granularity = 1;
-INSERT INTO tdm2 VALUES (toUnixTimestamp('2000-01-01 13:12:12', 'Europe/Moscow')), (toUnixTimestamp('2000-01-01 14:12:12', 'Europe/Moscow')), (toUnixTimestamp('2000-01-01 15:12:12', 'Europe/Moscow'));
+
+INSERT INTO tdm2 VALUES (toUnixTimestamp('2000-01-01 13:12:12')), (toUnixTimestamp('2000-01-01 14:12:12')), (toUnixTimestamp('2000-01-01 15:12:12'));
+
 SET max_rows_to_read = 1;
-SELECT toDateTime(timestamp, 'Europe/Moscow') FROM tdm2 WHERE toHour(toDateTime(timestamp, 'Europe/Moscow')) = 13;
+SELECT toDateTime(timestamp) FROM tdm2 WHERE toHour(toDateTime(timestamp)) = 13;
 
 DROP TABLE tdm;
 DROP TABLE tdm2;
