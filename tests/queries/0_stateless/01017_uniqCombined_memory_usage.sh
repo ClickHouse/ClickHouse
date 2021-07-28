5ea59233932f9869688a84c8dc336f2c3fc2c334
Artur Filatenkov <613623@mail.ru>
2021-07-28 20:13:20 +0300

    fix test

--- tests/queries/0_stateless/01017_uniqCombined_memory_usage.sh
+++ tests/queries/0_stateless/01017_uniqCombined_memory_usage.sh
@@ -1,7 +1,6 @@
 #!/usr/bin/env bash
 
 CURDIR=$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)
-CLICKHOUSE_CLIENT_SERVER_LOGS_LEVEL=none
 # shellcheck source=../shell_config.sh
 . "$CURDIR"/../shell_config.sh
 
@@ -10,47 +9,76 @@ CLICKHOUSE_CLIENT_SERVER_LOGS_LEVEL=none
 # hence max_memory_usage for 100 rows = (96<<10)*100 = 9830400
 
 # HashTable for UInt32 (used until (1<<13) elements), hence 8192 elements
-$CLICKHOUSE_CLIENT --no-max-memory-usage-for-client --query "SELECT 'UInt32';"
-$CLICKHOUSE_CLIENT --no-max-memory-usage-for-client --query "SET max_memory_usage = 4000000;"
-$CLICKHOUSE_CLIENT --no-max-memory-usage-for-client --query "SELECT sum(u) FROM (SELECT intDiv(number, 8192) AS k, uniqCombined(number % 8192) u FROM numbers(8192 * 100) GROUP BY k);" # { serverError 241 }
-$CLICKHOUSE_CLIENT --no-max-memory-usage-for-client --query "SET max_memory_usage = 9830400;"
-$CLICKHOUSE_CLIENT --no-max-memory-usage-for-client --query "SELECT sum(u) FROM (SELECT intDiv(number, 8192) AS k, uniqCombined(number % 8192) u FROM numbers(8192 * 100) GROUP BY k);"
+
+# {first SELECT sum will return serverError 241 }
+$CLICKHOUSE_CLIENT --no-max-memory-usage-for-client --multiquery <<EOF
+SELECT 'UInt32';
+SET max_memory_usage = 4000000;
+SELECT sum(u) FROM (SELECT intDiv(number, 8192) AS k, uniqCombined(number % 8192) u FROM numbers(8192 * 100) GROUP BY k);
+EOF
+$CLICKHOUSE_CLIENT --no-max-memory-usage-for-client --multiquery <<EOF
+SET max_memory_usage = 9830400;
+SELECT sum(u) FROM (SELECT intDiv(number, 8192) AS k, uniqCombined(number % 8192) u FROM numbers(8192 * 100) GROUP BY k);
+EOF
 
 # HashTable for UInt64 (used until (1<<12) elements), hence 4096 elements
-$CLICKHOUSE_CLIENT --no-max-memory-usage-for-client --query "SELECT 'UInt64';"
-$CLICKHOUSE_CLIENT --no-max-memory-usage-for-client --query "SET max_memory_usage = 4000000;"
-$CLICKHOUSE_CLIENT --no-max-memory-usage-for-client --query "SELECT sum(u) FROM (SELECT intDiv(number, 4096) AS k, uniqCombined(reinterpretAsString(number % 4096)) u FROM numbers(4096 * 100) GROUP BY k);" # { serverError 241 }
-$CLICKHOUSE_CLIENT --no-max-memory-usage-for-client --query "SET max_memory_usage = 9830400;"
-$CLICKHOUSE_CLIENT --no-max-memory-usage-for-client --query "SELECT sum(u) FROM (SELECT intDiv(number, 4096) AS k, uniqCombined(reinterpretAsString(number % 4096)) u FROM numbers(4096 * 100) GROUP BY k);"
-
-$CLICKHOUSE_CLIENT --no-max-memory-usage-for-client --query "SELECT 'K=16';"
+# {first SELECT sum will return serverError 241 }
+$CLICKHOUSE_CLIENT --no-max-memory-usage-for-client --multiquery <<EOF
+SELECT 'UInt64';
+SET max_memory_usage = 4000000;
+SELECT sum(u) FROM (SELECT intDiv(number, 4096) AS k, uniqCombined(reinterpretAsString(number % 4096)) u FROM numbers(4096 * 100) GROUP BY k);
+EOF
+$CLICKHOUSE_CLIENT --no-max-memory-usage-for-client --multiquery <<EOF
+SET max_memory_usage = 9830400;
+SELECT sum(u) FROM (SELECT intDiv(number, 4096) AS k, uniqCombined(reinterpretAsString(number % 4096)) u FROM numbers(4096 * 100) GROUP BY k);
+SELECT 'K=16';
+EOF
 
 # HashTable for UInt32 (used until (1<<12) elements), hence 4096 elements
-$CLICKHOUSE_CLIENT --no-max-memory-usage-for-client --query "SELECT 'UInt32';"
-$CLICKHOUSE_CLIENT --no-max-memory-usage-for-client --query "SET max_memory_usage = 2000000;"
-$CLICKHOUSE_CLIENT --no-max-memory-usage-for-client --query "SELECT sum(u) FROM (SELECT intDiv(number, 4096) AS k, uniqCombined(16)(number % 4096) u FROM numbers(4096 * 100) GROUP BY k);" # { serverError 241 }
-$CLICKHOUSE_CLIENT --no-max-memory-usage-for-client --query "SET max_memory_usage = 4915200;"
-$CLICKHOUSE_CLIENT --no-max-memory-usage-for-client --query "SELECT sum(u) FROM (SELECT intDiv(number, 4096) AS k, uniqCombined(16)(number % 4096) u FROM numbers(4096 * 100) GROUP BY k);"
+# {first SELECT sum will return serverError 241 }
+$CLICKHOUSE_CLIENT --no-max-memory-usage-for-client --multiquery <<EOF
+SELECT 'UInt32';
+SET max_memory_usage = 2000000;
+SELECT sum(u) FROM (SELECT intDiv(number, 4096) AS k, uniqCombined(16)(number % 4096) u FROM numbers(4096 * 100) GROUP BY k);
+EOF
+$CLICKHOUSE_CLIENT --no-max-memory-usage-for-client --multiquery <<EOF
+SET max_memory_usage = 4915200;
+SELECT sum(u) FROM (SELECT intDiv(number, 4096) AS k, uniqCombined(16)(number % 4096) u FROM numbers(4096 * 100) GROUP BY k);
+EOF
 
 # HashTable for UInt64 (used until (1<<11) elements), hence 2048 elements
-$CLICKHOUSE_CLIENT --no-max-memory-usage-for-client --query "SELECT 'UInt64';"
-$CLICKHOUSE_CLIENT --no-max-memory-usage-for-client --query "SET max_memory_usage = 2000000;"
-$CLICKHOUSE_CLIENT --no-max-memory-usage-for-client --query "SELECT sum(u) FROM (SELECT intDiv(number, 2048) AS k, uniqCombined(16)(reinterpretAsString(number % 2048)) u FROM numbers(2048 * 100) GROUP BY k);" # { serverError 241 }
-$CLICKHOUSE_CLIENT --no-max-memory-usage-for-client --query "SET max_memory_usage = 4915200;"
-$CLICKHOUSE_CLIENT --no-max-memory-usage-for-client --query "SELECT sum(u) FROM (SELECT intDiv(number, 2048) AS k, uniqCombined(16)(reinterpretAsString(number % 2048)) u FROM numbers(2048 * 100) GROUP BY k);"
-
-$CLICKHOUSE_CLIENT --no-max-memory-usage-for-client --query "SELECT 'K=18';"
+# {first SELECT sum will return serverError 241 }
+$CLICKHOUSE_CLIENT --no-max-memory-usage-for-client --multiquery <<EOF
+SELECT 'UInt64';
+SET max_memory_usage = 2000000;
+SELECT sum(u) FROM (SELECT intDiv(number, 2048) AS k, uniqCombined(16)(reinterpretAsString(number % 2048)) u FROM numbers(2048 * 100) GROUP BY k);
+EOF
+$CLICKHOUSE_CLIENT --no-max-memory-usage-for-client --multiquery <<EOF
+SET max_memory_usage = 4915200;
+SELECT sum(u) FROM (SELECT intDiv(number, 2048) AS k, uniqCombined(16)(reinterpretAsString(number % 2048)) u FROM numbers(2048 * 100) GROUP BY k);
+SELECT 'K=18';
+EOF
 
 # HashTable for UInt32 (used until (1<<14) elements), hence 16384 elements
-$CLICKHOUSE_CLIENT --no-max-memory-usage-for-client --query "SELECT 'UInt32';"
-$CLICKHOUSE_CLIENT --no-max-memory-usage-for-client --query "SET max_memory_usage = 8000000;"
-$CLICKHOUSE_CLIENT --no-max-memory-usage-for-client --query "SELECT sum(u) FROM (SELECT intDiv(number, 16384) AS k, uniqCombined(18)(number % 16384) u FROM numbers(16384 * 100) GROUP BY k);" # { serverError 241 }
-$CLICKHOUSE_CLIENT --no-max-memory-usage-for-client --query "SET max_memory_usage = 19660800;"
-$CLICKHOUSE_CLIENT --no-max-memory-usage-for-client --query "SELECT sum(u) FROM (SELECT intDiv(number, 16384) AS k, uniqCombined(18)(number % 16384) u FROM numbers(16384 * 100) GROUP BY k);"
+# {first SELECT sum will return serverError 241 }
+$CLICKHOUSE_CLIENT --no-max-memory-usage-for-client --multiquery <<EOF
+SELECT 'UInt32';
+SET max_memory_usage = 8000000;
+SELECT sum(u) FROM (SELECT intDiv(number, 16384) AS k, uniqCombined(18)(number % 16384) u FROM numbers(16384 * 100) GROUP BY k);
+EOF
+$CLICKHOUSE_CLIENT --no-max-memory-usage-for-client --multiquery <<EOF
+SET max_memory_usage = 19660800;
+SELECT sum(u) FROM (SELECT intDiv(number, 16384) AS k, uniqCombined(18)(number % 16384) u FROM numbers(16384 * 100) GROUP BY k);
+EOF
 
 # HashTable for UInt64 (used until (1<<13) elements), hence 8192 elements
-$CLICKHOUSE_CLIENT --no-max-memory-usage-for-client --query "SELECT 'UInt64';"
-$CLICKHOUSE_CLIENT --no-max-memory-usage-for-client --query "SET max_memory_usage = 8000000;"
-$CLICKHOUSE_CLIENT --no-max-memory-usage-for-client --query "SELECT sum(u) FROM (SELECT intDiv(number, 8192) AS k, uniqCombined(18)(reinterpretAsString(number % 8192)) u FROM numbers(8192 * 100) GROUP BY k);" # { serverError 241 }
-$CLICKHOUSE_CLIENT --no-max-memory-usage-for-client --query "SET max_memory_usage = 19660800;"
-$CLICKHOUSE_CLIENT --no-max-memory-usage-for-client --query "SELECT sum(u) FROM (SELECT intDiv(number, 8192) AS k, uniqCombined(18)(reinterpretAsString(number % 8192)) u FROM numbers(8192 * 100) GROUP BY k);"
+# {first SELECT sum will return serverError 241 }
+$CLICKHOUSE_CLIENT --no-max-memory-usage-for-client --multiquery <<EOF
+SELECT 'UInt64';
+SET max_memory_usage = 8000000;
+SELECT sum(u) FROM (SELECT intDiv(number, 8192) AS k, uniqCombined(18)(reinterpretAsString(number % 8192)) u FROM numbers(8192 * 100) GROUP BY k);
+EOF
+$CLICKHOUSE_CLIENT --no-max-memory-usage-for-client --multiquery <<EOF
+SET max_memory_usage = 19660800;
+SELECT sum(u) FROM (SELECT intDiv(number, 8192) AS k, uniqCombined(18)(reinterpretAsString(number % 8192)) u FROM numbers(8192 * 100) GROUP BY k);
+EOF
--- tests/queries/0_stateless/01039_test_setting_parse.sh
+++ tests/queries/0_stateless/01039_test_setting_parse.sh
@@ -5,23 +5,43 @@ CLICKHOUSE_CLIENT_SERVER_LOGS_LEVEL=none
 # shellcheck source=../shell_config.sh
 . "$CURDIR"/../shell_config.sh
 
-$CLICKHOUSE_CLIENT --no-max-memory-usage-for-client --query "SET max_memory_usage = '1G';"
-$CLICKHOUSE_CLIENT --no-max-memory-usage-for-client --query "SELECT value FROM system.settings WHERE name = 'max_memory_usage';"
-$CLICKHOUSE_CLIENT --no-max-memory-usage-for-client --query "SET max_memory_usage = '3Gi';"
-$CLICKHOUSE_CLIENT --no-max-memory-usage-for-client --query "SELECT value FROM system.settings WHERE name = 'max_memory_usage';"
-$CLICKHOUSE_CLIENT --no-max-memory-usage-for-client --query "SET max_memory_usage = '1567k';"
-$CLICKHOUSE_CLIENT --no-max-memory-usage-for-client --query "SELECT value FROM system.settings WHERE name = 'max_memory_usage';"
-$CLICKHOUSE_CLIENT --no-max-memory-usage-for-client --query "SET max_memory_usage = '123ki';"
-$CLICKHOUSE_CLIENT --no-max-memory-usage-for-client --query "SELECT value FROM system.settings WHERE name = 'max_memory_usage';"
-$CLICKHOUSE_CLIENT --no-max-memory-usage-for-client --query "SET max_memory_usage = '1567K';"
-$CLICKHOUSE_CLIENT --no-max-memory-usage-for-client --query "SELECT value FROM system.settings WHERE name = 'max_memory_usage';"
-$CLICKHOUSE_CLIENT --no-max-memory-usage-for-client --query "SET max_memory_usage = '123Ki';"
-$CLICKHOUSE_CLIENT --no-max-memory-usage-for-client --query "SELECT value FROM system.settings WHERE name = 'max_memory_usage';"
-$CLICKHOUSE_CLIENT --no-max-memory-usage-for-client --query "SET max_memory_usage = '12M';"
-$CLICKHOUSE_CLIENT --no-max-memory-usage-for-client --query "SELECT value FROM system.settings WHERE name = 'max_memory_usage';"
-$CLICKHOUSE_CLIENT --no-max-memory-usage-for-client --query "SET max_memory_usage = '31Mi';"
-$CLICKHOUSE_CLIENT --no-max-memory-usage-for-client --query "SELECT value FROM system.settings WHERE name = 'max_memory_usage';"
-$CLICKHOUSE_CLIENT --no-max-memory-usage-for-client --query "SET max_memory_usage = '1T';"
-$CLICKHOUSE_CLIENT --no-max-memory-usage-for-client --query "SELECT value FROM system.settings WHERE name = 'max_memory_usage';"
-$CLICKHOUSE_CLIENT --no-max-memory-usage-for-client --query "SET max_memory_usage = '1Ti';"
-$CLICKHOUSE_CLIENT --no-max-memory-usage-for-client --query "SELECT value FROM system.settings WHERE name = 'max_memory_usage';"
+$CLICKHOUSE_CLIENT --no-max-memory-usage-for-client --multiquery <<EOF
+SET max_memory_usage = '1G';
+SELECT value FROM system.settings WHERE name = 'max_memory_usage';
+EOF
+$CLICKHOUSE_CLIENT --no-max-memory-usage-for-client --multiquery <<EOF
+SET max_memory_usage = '3Gi';
+SELECT value FROM system.settings WHERE name = 'max_memory_usage';
+EOF
+$CLICKHOUSE_CLIENT --no-max-memory-usage-for-client --multiquery <<EOF
+SET max_memory_usage = '1567k';
+SELECT value FROM system.settings WHERE name = 'max_memory_usage';
+EOF
+$CLICKHOUSE_CLIENT --no-max-memory-usage-for-client --multiquery <<EOF
+SET max_memory_usage = '123ki';
+SELECT value FROM system.settings WHERE name = 'max_memory_usage';
+EOF
+$CLICKHOUSE_CLIENT --no-max-memory-usage-for-client --multiquery <<EOF
+SET max_memory_usage = '1567K';
+SELECT value FROM system.settings WHERE name = 'max_memory_usage';
+EOF
+$CLICKHOUSE_CLIENT --no-max-memory-usage-for-client --multiquery <<EOF
+SET max_memory_usage = '123Ki';
+SELECT value FROM system.settings WHERE name = 'max_memory_usage';
+EOF
+$CLICKHOUSE_CLIENT --no-max-memory-usage-for-client --multiquery <<EOF
+SET max_memory_usage = '12M';
+SELECT value FROM system.settings WHERE name = 'max_memory_usage';
+EOF
+$CLICKHOUSE_CLIENT --no-max-memory-usage-for-client --multiquery <<EOF
+SET max_memory_usage = '31Mi';
+SELECT value FROM system.settings WHERE name = 'max_memory_usage';
+EOF
+$CLICKHOUSE_CLIENT --no-max-memory-usage-for-client --multiquery <<EOF
+SET max_memory_usage = '1T';
+SELECT value FROM system.settings WHERE name = 'max_memory_usage';
+EOF
+$CLICKHOUSE_CLIENT --no-max-memory-usage-for-client --multiquery <<EOF
+SET max_memory_usage = '1Ti';
+SELECT value FROM system.settings WHERE name = 'max_memory_usage';
+EOF
