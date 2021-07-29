0f7d4b5dc44144bbb427b54e7befa1f5a6ed4e77
Artur Filatenkov <613623@mail.ru>
2021-07-29 14:20:02 +0300

    add ignoring errors

--- tests/queries/0_stateless/01017_uniqCombined_memory_usage.sh
+++ tests/queries/0_stateless/01017_uniqCombined_memory_usage.sh
@@ -11,10 +11,10 @@ CURDIR=$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)
 # HashTable for UInt32 (used until (1<<13) elements), hence 8192 elements
 
 # {first SELECT sum will return serverError 241 }
-$CLICKHOUSE_CLIENT --no-max-memory-usage-for-client --multiquery <<EOF
+$CLICKHOUSE_CLIENT --no-max-memory-usage-for-client --multiquery --testmode <<EOF
 SELECT 'UInt32';
 SET max_memory_usage = 4000000;
-SELECT sum(u) FROM (SELECT intDiv(number, 8192) AS k, uniqCombined(number % 8192) u FROM numbers(8192 * 100) GROUP BY k);  -- { serverError 241 }
+SELECT sum(u) FROM (SELECT intDiv(number, 8192) AS k, uniqCombined(number % 8192) u FROM numbers(8192 * 100) GROUP BY k); -- { serverError 241 }
 EOF
 $CLICKHOUSE_CLIENT --no-max-memory-usage-for-client --multiquery <<EOF
 SET max_memory_usage = 9830400;
@@ -23,7 +23,7 @@ EOF
 
 # HashTable for UInt64 (used until (1<<12) elements), hence 4096 elements
 # {first SELECT sum will return serverError 241 }
-$CLICKHOUSE_CLIENT --no-max-memory-usage-for-client --multiquery <<EOF
+$CLICKHOUSE_CLIENT --no-max-memory-usage-for-client --multiquery --testmode <<EOF
 SELECT 'UInt64';
 SET max_memory_usage = 4000000;
 SELECT sum(u) FROM (SELECT intDiv(number, 4096) AS k, uniqCombined(reinterpretAsString(number % 4096)) u FROM numbers(4096 * 100) GROUP BY k); -- { serverError 241 }
@@ -36,7 +36,7 @@ EOF
 
 # HashTable for UInt32 (used until (1<<12) elements), hence 4096 elements
 # {first SELECT sum will return serverError 241 }
-$CLICKHOUSE_CLIENT --no-max-memory-usage-for-client --multiquery <<EOF
+$CLICKHOUSE_CLIENT --no-max-memory-usage-for-client --multiquery --testmode <<EOF
 SELECT 'UInt32';
 SET max_memory_usage = 2000000;
 SELECT sum(u) FROM (SELECT intDiv(number, 4096) AS k, uniqCombined(16)(number % 4096) u FROM numbers(4096 * 100) GROUP BY k); -- { serverError 241 }
@@ -48,7 +48,7 @@ EOF
 
 # HashTable for UInt64 (used until (1<<11) elements), hence 2048 elements
 # {first SELECT sum will return serverError 241 }
-$CLICKHOUSE_CLIENT --no-max-memory-usage-for-client --multiquery <<EOF
+$CLICKHOUSE_CLIENT --no-max-memory-usage-for-client --multiquery --testmode <<EOF
 SELECT 'UInt64';
 SET max_memory_usage = 2000000;
 SELECT sum(u) FROM (SELECT intDiv(number, 2048) AS k, uniqCombined(16)(reinterpretAsString(number % 2048)) u FROM numbers(2048 * 100) GROUP BY k); -- { serverError 241 }
@@ -61,7 +61,7 @@ EOF
 
 # HashTable for UInt32 (used until (1<<14) elements), hence 16384 elements
 # {first SELECT sum will return serverError 241 }
-$CLICKHOUSE_CLIENT --no-max-memory-usage-for-client --multiquery <<EOF
+$CLICKHOUSE_CLIENT --no-max-memory-usage-for-client --multiquery --testmode <<EOF
 SELECT 'UInt32';
 SET max_memory_usage = 8000000;
 SELECT sum(u) FROM (SELECT intDiv(number, 16384) AS k, uniqCombined(18)(number % 16384) u FROM numbers(16384 * 100) GROUP BY k); -- { serverError 241 }
@@ -73,7 +73,7 @@ EOF
 
 # HashTable for UInt64 (used until (1<<13) elements), hence 8192 elements
 # {first SELECT sum will return serverError 241 }
-$CLICKHOUSE_CLIENT --no-max-memory-usage-for-client --multiquery <<EOF
+$CLICKHOUSE_CLIENT --no-max-memory-usage-for-client --multiquery --testmode <<EOF
 SELECT 'UInt64';
 SET max_memory_usage = 8000000;
 SELECT sum(u) FROM (SELECT intDiv(number, 8192) AS k, uniqCombined(18)(reinterpretAsString(number % 8192)) u FROM numbers(8192 * 100) GROUP BY k); -- { serverError 241 }
