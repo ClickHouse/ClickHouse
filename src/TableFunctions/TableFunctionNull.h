557cce9faa81dab9073e92533bbe8b29665bd90a
Artur <613623@mail.ru>
2021-07-29 09:15:37 +0000

    correct typos

--- src/TableFunctions/TableFunctionNull.h
+++ src/TableFunctions/TableFunctionNull.h
@@ -28,7 +28,7 @@ private:
 namespace NullDoc
 {
 const char * doc = R"(
-Creates a temporary table of the specified structure with the [Null](../../engines/table-engines/special/null.md) table engine. According to the `Null`-engine properties, the table data is ignored and the table itself is immediately droped right after the query execution. The function is used for the convenience of test writing and demonstrations.
+Creates a temporary table of the specified structure with the [Null](../../engines/table-engines/special/null.md) table engine. According to the `Null`-engine properties, the table data is ignored and the table itself is immediately dropped right after the query execution. The function is used for the convenience of test writing and demonstrations.
 
 **Syntax** 
 
