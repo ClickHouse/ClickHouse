21599ffefc9e193ddf2c3ffc817b9d11f69928b5
alesapin <alesapin@gmail.com>
2021-07-29 16:11:05 +0300

    Simple validation for partition id before drop partition

--- tests/queries/0_stateless/01925_broken_partition_id_zookeeper.sql
+++ tests/queries/0_stateless/01925_broken_partition_id_zookeeper.sql
@@ -9,8 +9,8 @@ ENGINE = ReplicatedMergeTree('/clickhouse/test_01925_{database}/rmt', 'r1')
 ORDER BY tuple()
 PARTITION BY date;
 
-ALTER TABLE broken_partition DROP PARTITION ID '20210325_0_13241_6_12747';
+ALTER TABLE broken_partition DROP PARTITION ID '20210325_0_13241_6_12747'; --{serverError 248}
 
-ALTER TABLE broken_partition DROP PARTITION ID '20210325_0_13241_6_12747';
+ALTER TABLE broken_partition DROP PARTITION ID '20210325_0_13241_6_12747'; --{serverError 248}
 
 DROP TABLE IF EXISTS broken_partition;
