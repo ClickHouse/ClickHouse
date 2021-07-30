0dd648982950bf46af247c2fa72e656ae9cd2db5
alesapin <alesapin@gmail.com>
2021-07-30 16:51:15 +0300

    Relax fast test

--- docker/test/fasttest/run.sh
+++ docker/test/fasttest/run.sh
@@ -185,9 +185,6 @@ function clone_submodules
             contrib/dragonbox
             contrib/fast_float
             contrib/NuRaft
-            contrib/lemmagen-c
-            contrib/libstemmer_c
-            contrib/wordnet-blast
         )
 
         git submodule sync
