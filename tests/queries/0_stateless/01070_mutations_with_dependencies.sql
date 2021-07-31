b0df75626bb92b6f31a678f28af432f78879055b
terrylin <terrylin@tencent.com>
2021-07-31 19:37:13 +0800

    modify test cases about ttl

--- tests/queries/0_stateless/01070_mutations_with_dependencies.sql
+++ tests/queries/0_stateless/01070_mutations_with_dependencies.sql
@@ -2,14 +2,23 @@ drop table if exists ttl;
 set mutations_sync = 2;
 
 -- check that ttl info was updated after mutation.
-create table ttl (i Int, a Int, s String) engine = MergeTree order by i;
+create table ttl (i Int, a Int, s String) engine = MergeTree order by i
+SETTINGS merge_with_ttl_timeout=0,max_number_of_merges_with_ttl_in_pool=0;
+
 insert into ttl values (1, 1, 'a') (2, 1, 'b') (3, 1, 'c') (4, 1, 'd');
 
 alter table ttl modify ttl a % 2 = 0 ? today() - 10 : toDate('2100-01-01');
 alter table ttl materialize ttl;
-
 select * from ttl order by i;
+alter table ttl modify setting max_number_of_merges_with_ttl_in_pool = 1;
+optimize table ttl;
+select * from ttl order by i;
+alter table ttl modify setting max_number_of_merges_with_ttl_in_pool = 0;
+
 alter table ttl update a = 0 where i % 2 = 0;
+select * from ttl order by i;
+alter table ttl modify setting max_number_of_merges_with_ttl_in_pool = 1;
+optimize table ttl;
 select * from ttl order by i;
 
 drop table ttl;
@@ -18,27 +27,38 @@ select '===================';
 
 -- check that skip index is updated after column was modified by ttl.
 create table ttl (i Int, a Int, s String default 'b' ttl a % 2 = 0 ? today() - 10 : toDate('2100-01-01'),
-    index ind_s (s) type set(1) granularity 1) engine = MergeTree order by i;
+    index ind_s (s) type set(1) granularity 1) engine = MergeTree order by i
+    SETTINGS merge_with_ttl_timeout=0,max_number_of_merges_with_ttl_in_pool=0;
+
 insert into ttl values (1, 1, 'a') (2, 1, 'a') (3, 1, 'a') (4, 1, 'a');
 
 select count() from ttl where s = 'a';
 
 alter table ttl update a = 0 where i % 2 = 0;
-
+select count() from ttl where s = 'a';
+select count() from ttl where s = 'b';
+alter table ttl modify setting max_number_of_merges_with_ttl_in_pool = 1;
+optimize table ttl;
 select count() from ttl where s = 'a';
 select count() from ttl where s = 'b';
 
 drop table ttl;
 
 -- check only that it doesn't throw exceptions.
-create table ttl (i Int, s String) engine = MergeTree order by i ttl toDate('2000-01-01') TO DISK 'default';
+create table ttl (i Int, s String) engine = MergeTree order by i ttl toDate('2000-01-01') TO DISK 'default'
+SETTINGS merge_with_ttl_timeout=0,max_number_of_merges_with_ttl_in_pool=0;
+
 alter table ttl materialize ttl;
 drop table ttl;
 
 create table ttl (a Int, b Int, c Int default 42 ttl d, d Date, index ind (b * c) type minmax granularity 1)
-engine = MergeTree order by a;
+engine = MergeTree order by a SETTINGS merge_with_ttl_timeout=0,max_number_of_merges_with_ttl_in_pool=0;
+
 insert into ttl values (1, 2, 3, '2100-01-01');
 alter table ttl update d = '2000-01-01' where 1;
 alter table ttl materialize ttl;
+select * from ttl;
+alter table ttl modify setting max_number_of_merges_with_ttl_in_pool = 1;
+optimize table ttl;
 select * from ttl;
 drop table ttl;
--- tests/queries/0_stateless/01378_alter_rename_with_ttl_zookeeper.sql
+++ tests/queries/0_stateless/01378_alter_rename_with_ttl_zookeeper.sql
@@ -6,6 +6,8 @@ CREATE TABLE table_rename_with_ttl
   value1 String
 )
 ENGINE = ReplicatedMergeTree('/clickhouse/test/table_rename_with_ttl_01378', '1')
+SETTINGS merge_with_ttl_timeout=0,max_number_of_merges_with_ttl_in_pool=0;
+
 ORDER BY tuple();
 
 INSERT INTO table_rename_with_ttl SELECT toDate('2018-10-01') + number % 3, toString(number) from numbers(9);
@@ -20,6 +22,11 @@ SELECT count() FROM table_rename_with_ttl;
 ALTER TABLE table_rename_with_ttl RENAME COLUMN date1 TO renamed_date1;
 
 ALTER TABLE table_rename_with_ttl materialize TTL settings mutations_sync=2;
+
+SELECT count() FROM table_rename_with_ttl;
+
+ALTER TABLE table_rename_with_ttl MODIFY setting max_number_of_merges_with_ttl_in_pool = 1;
+optimize table table_rename_with_ttl;
 
 SELECT count() FROM table_rename_with_ttl;
 
