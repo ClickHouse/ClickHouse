79c55866b52d12ff24a4c7cdc5ef4bbe75d95dca
terrylin <terrylin@tencent.com>
2021-07-31 21:10:06 +0800

    modify test cases

--- tests/queries/0_stateless/01378_alter_rename_with_ttl_zookeeper.sql
+++ tests/queries/0_stateless/01378_alter_rename_with_ttl_zookeeper.sql
@@ -6,10 +6,8 @@ CREATE TABLE table_rename_with_ttl
   value1 String
 )
 ENGINE = ReplicatedMergeTree('/clickhouse/test/table_rename_with_ttl_01378', '1')
-order by value1
+ORDER BY tuple()
 SETTINGS merge_with_ttl_timeout=0,max_number_of_merges_with_ttl_in_pool=0;
-
-ORDER BY tuple();
 
 INSERT INTO table_rename_with_ttl SELECT toDate('2018-10-01') + number % 3, toString(number) from numbers(9);
 
