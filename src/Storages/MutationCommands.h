0d6e0181d6a110363dc4af6cc1ce15fa328f0596
Vladimir Chebotarev <vladimir.chebotarev@gmail.com>
2021-08-03 12:07:54 +0300

    MATERIALIZE COLUMN xxx FINAL

--- src/Storages/MutationCommands.h
+++ src/Storages/MutationCommands.h
@@ -43,6 +43,8 @@ struct MutationCommand
     /// WHERE part of mutation
     ASTPtr predicate;
 
+    bool materialize_column_final;
+
     /// Columns with corresponding actions
     std::unordered_map<String, ASTPtr> column_to_update_expression;
 
