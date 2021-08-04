2ad9432e7aa6a95cb23cd757009527835603b3e7
Nikolai Kochetov <nik-kochetov@yandex-team.ru>
2021-08-04 16:53:26 +0300

    Fix style.

--- src/Storages/StorageAggregatingMemory.cpp
+++ src/Storages/StorageAggregatingMemory.cpp
@@ -39,6 +39,7 @@ namespace ErrorCodes
 {
     extern const int NUMBER_OF_ARGUMENTS_DOESNT_MATCH;
     extern const int INCORRECT_QUERY;
+    extern const int LOGICAL_ERROR;
 }
 
 class AggregationKeyMatcher
