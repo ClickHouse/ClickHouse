7320411a7ff59b35cab2c3bb728580effcd9895d
Nikolai Kochetov <nik-kochetov@yandex-team.ru>
2021-08-06 16:39:11 +0300

    Fix bug from #23515.

---
+++ tests/queries/0_stateless/02003_bug_from_23515.sql
@@ -0,0 +1,6 @@
+drop table if exists tab;
+create table tab (a LowCardinality(String), b LowCardinality(String)) engine = MergeTree partition by a order by tuple() settings min_bytes_for_wide_part = 0, min_rows_for_wide_part = 0;
+
+insert into tab values ('1', 'a'), ('2', 'b');
+SELECT a = '1' FROM tab WHERE a = '1' and b='a';
+drop table if exists tab;
