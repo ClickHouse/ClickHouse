985a906c575b2e3eb04ad432303d6abd4e6ab6fa
Artur <613623@mail.ru>
2021-08-06 09:46:39 +0000

    correcting tests

--- tests/queries/0_stateless/02003_memory_limit_in_client.sh
+++ tests/queries/0_stateless/02003_memory_limit_in_client.sh
@@ -5,10 +5,10 @@ CURDIR=$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)
 . "$CURDIR"/../shell_config.sh
 
 # Big quiery. Memory limit
-${CLICKHOUSE_CLIENT} --max_memory_usage_in_client=1 --query "SELECT arrayMap(x -> range(x), range(number)) FROM numbers(1000) -- { serverError 241; }"
+${CLICKHOUSE_CLIENT} --max_memory_usage_in_client=1 --testmode --query "SELECT arrayMap(x -> range(x), range(number)) FROM numbers(1000) -- { serverError 241; }"
 
 # small quiery. In max untracked memory range
-${CLICKHOUSE_CLIENT} --max_memory_usage_in_client=1 --query "SELECT * FROM system.numbers LIMIT 60000"
+${CLICKHOUSE_CLIENT} --max_memory_usage_in_client=1 --testmode --query "SELECT * FROM system.numbers LIMIT 60000"
 
-# Have anough memory
-${CLICKHOUSE_CLIENT} --max_memory_usage_in_client=20000000 --query "SELECT arrayMap(x -> range(x), range(number)) FROM numbers(1000)"
+# Have anough memory (unlimited). Better not to set limit, because of adress sanitizer
+${CLICKHOUSE_CLIENT} --testmode --query "SELECT arrayMap(x -> range(x), range(number)) FROM numbers(125)"
