bf0297ccab4c3298213807a6d2ff596b8f07bef5
Nikolay Degterinsky <evillique@gmail.com>
2021-08-07 09:51:35 +0000

    Fix stateless test

--- tests/queries/0_stateless/02003_compress_bz2.sh
+++ tests/queries/0_stateless/02003_compress_bz2.sh
@@ -5,9 +5,13 @@ CURDIR=$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)
 . "$CURDIR"/../shell_config.sh
 
 USER_FILES_PATH=$(clickhouse-client --query "select _path,_file from file('nonexist.txt', 'CSV', 'val1 char')" 2>&1 | grep Exception | awk '{gsub("/nonexist.txt","",$9); print $9}')
+WORKING_FOLDER_02003="${USER_FILES_PATH}/${CLICKHOUSE_DATABASE}"
 
-${CLICKHOUSE_CLIENT} --query "SELECT * FROM (SELECT 'Hello, World!' as String) INTO OUTFILE '${USER_FILES_PATH}/bz2_compression.bz2'"
-bzip2 -t ${USER_FILES_PATH}/bz2_compression.bz2
-${CLICKHOUSE_CLIENT} --query "SELECT * FROM file('${USER_FILES_PATH}/bz2_compression.bz2', 'TabSeparated', 'col String')"
+rm -rf "${WORKING_FOLDER_02003}"
+mkdir ${WORKING_FOLDER_02003}
 
-rm -f "${USER_FILES_PATH}/bz2_compression.bz2"
+${CLICKHOUSE_CLIENT} --query "SELECT * FROM (SELECT 'Hello, World!' as String) INTO OUTFILE '${WORKING_FOLDER_02003}/bz2_compression.bz2'"
+bzip2 -t ${WORKING_FOLDER_02003}/bz2_compression.bz2
+${CLICKHOUSE_CLIENT} --query "SELECT * FROM file('${WORKING_FOLDER_02003}/bz2_compression.bz2', 'TabSeparated', 'col String')"
+
+rm -rf "${WORKING_FOLDER_02003}"
