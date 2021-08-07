c628e41b247586a34d04bbef313dffde3dc502d0
kssenii <sumarokovakseniia@mail.ru>
2021-08-07 10:12:59 +0000

    Fix some tests

--- src/Functions/FunctionsConversion.h
+++ src/Functions/FunctionsConversion.h
@@ -3488,15 +3488,15 @@ private:
 struct CastOverloadName
 {
     static constexpr auto cast_name = "CAST";
-    static constexpr auto accurate_cast_name = "CastAccurate";
-    static constexpr auto accurate_cast_or_null_name = "CastAccurateOrNull";
+    static constexpr auto accurate_cast_name = "accurateCast";
+    static constexpr auto accurate_cast_or_null_name = "accurateCastOrNull";
 };
 
 struct CastInternalOverloadName
 {
     static constexpr auto cast_name = "_CAST";
-    static constexpr auto accurate_cast_name = "_CastAccurate";
-    static constexpr auto accurate_cast_or_null_name = "_CastAccurateOrNull";
+    static constexpr auto accurate_cast_name = "accurate_Cast";
+    static constexpr auto accurate_cast_or_null_name = "accurate_CastOrNull";
 };
 
 template <CastType cast_type> using CastOverloadResolver = CastOverloadResolverImpl<cast_type, false, CastOverloadName, CastName>;
