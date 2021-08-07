d9599322637f83e202bd44b8c2243187392c83c2
Nikolay Degterinsky <evillique@gmail.com>
2021-08-07 15:15:49 +0000

    Small fixes

--- tests/queries/0_stateless/02003_compress_bz2.sh
+++ tests/queries/0_stateless/02003_compress_bz2.sh
@@ -8,7 +8,7 @@ USER_FILES_PATH=$(clickhouse-client --query "select _path,_file from file('nonex
 WORKING_FOLDER_02003="${USER_FILES_PATH}/${CLICKHOUSE_DATABASE}"
 
 rm -rf "${WORKING_FOLDER_02003}"
-mkdir ${WORKING_FOLDER_02003}
+mkdir "${WORKING_FOLDER_02003}"
 
 ${CLICKHOUSE_CLIENT} --query "SELECT * FROM (SELECT 'Hello, World!' as String) INTO OUTFILE '${WORKING_FOLDER_02003}/bz2_compression.bz2'"
 bzip2 -t ${WORKING_FOLDER_02003}/bz2_compression.bz2
