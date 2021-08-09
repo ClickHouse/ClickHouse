c4cf1eae0da5e6b462226c60d19e74e137ae38f5
Nikolai Kochetov <nik-kochetov@yandex-team.ru>
2021-08-09 18:46:39 +0300

    Try fix build.

--- src/Common/DenseHashSet.h
+++ src/Common/DenseHashSet.h
@@ -4,16 +4,16 @@
 #define HASH_FUN_H <unordered_map>
 #endif
 
-#include <sparsehash/dense_hash_map>
+#include <sparsehash/dense_hash_set>
 
 #if !defined(ARCADIA_BUILD)
-    template <class Key, class T, class HashFcn = std::hash<Key>,
-        class EqualKey = std::equal_to<Key>,
-            class Alloc = google::libc_allocator_with_realloc<std::pair<const Key, T>>>
-                using DenseHashSet = google::dense_hash_set<Key, T, HashFcn, EqualKey, Alloc>;
+    template <class Value, class HashFcn = std::hash<Value>,
+        class EqualKey = std::equal_to<Value>,
+        class Alloc = google::libc_allocator_with_realloc<Value>>
+    using DenseHashSet = google::dense_hash_set<Value, HashFcn, EqualKey, Alloc>;
 #else
-template <class Key, class T, class HashFcn = std::hash<Key>,
-    class EqualKey = std::equal_to<Key>,
-        class Alloc = google::sparsehash::libc_allocator_with_realloc<std::pair<const Key, T>>>
-            using DenseHashSet = google::sparsehash::dense_hash_set<Key, T, HashFcn, EqualKey, Alloc>;
+    template <class Value, class HashFcn = std::hash<Value>,
+        class EqualKey = std::equal_to<Value>,
+        class Alloc = google::sparsehash::libc_allocator_with_realloc<Value>>
+    using DenseHashSet = google::sparsehash::dense_hash_set<Value, HashFcn, EqualKey, Alloc>;
 #endif
