44cf26cf4cd5fa6314dd1679561e41d299fb6616
zxc111 <zxc9007@gmail.com>
2021-08-10 00:30:28 +0800

    update 02003_toDateTimeOrNull_support_integer.sql

--- tests/queries/0_stateless/02003_toDateTimeOrNull_support_integer.sql
+++ tests/queries/0_stateless/02003_toDateTimeOrNull_support_integer.sql
@@ -1,20 +1,32 @@
-SELECT toDateTimeOrNull(1583851242, 'Asia/Shanghai') = toDateTime(1583851242, 'Asia/Shanghai');
-SELECT toDateTime(1583851242, 'Asia/Shanghai') = toDateTimeOrZero(1583851242, 'Asia/Shanghai');
-
 SELECT toDate(123) = toDateOrNull(123);
-
 SELECT toDate(123) = toDateOrNull(toInt8(123));
 SELECT toDate(123) = toDateOrZero(toInt16(123));
 SELECT toDate(123) = toDateOrZero(toInt32(123));
 SELECT toDate(123) = toDateOrNull(toInt64(123));
+
+SELECT toDateOrNull(toInt8(123));
+SELECT toDateOrZero(toInt16(123));
+SELECT toDateOrZero(toInt32(1230));
+SELECT toDateOrNull(toInt64(12300));
 
 SELECT toDate(123) = toDateOrZero(toUInt8(123));
 SELECT toDate(123) = toDateOrNull(toUInt16(123));
 SELECT toDate(123) = toDateOrNull(toUInt32(123));
 SELECT toDate(123) = toDateOrZero(toUInt64(123));
 
+SELECT toDateOrZero(toUInt8(123));
+SELECT toDateOrNull(toUInt16(123));
+SELECT toDateOrNull(toUInt32(1230));
+SELECT toDateOrZero(toUInt64(12300));
+
 SELECT toDateOrNull('');
 SELECT toDate32OrNull('');
 
 SELECT toDateTimeOrNull('');
 SELECT toDateTimeOrZero('', 'Asia/Shanghai');
+
+SELECT toDateTimeOrZero(1583851242, 'Asia/Shanghai');
+SELECT toDateTimeOrNull(1583851242, 'Asia/Shanghai');
+
+SELECT toDateTimeOrNull(1583851242, 'Asia/Shanghai') = toDateTime(1583851242, 'Asia/Shanghai');
+SELECT toDateTime(1583851242, 'Asia/Shanghai') = toDateTimeOrZero(1583851242, 'Asia/Shanghai');
