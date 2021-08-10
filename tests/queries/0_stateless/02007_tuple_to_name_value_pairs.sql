a786ff1d6e00469b265ebc86eb9560c1326a5132
Braulio Valdivielso <brlvldvlsmrtnz@gmail.com>
2021-08-10 21:47:56 +0100

    fixup! clean up and add more tests

--- tests/queries/0_stateless/02007_tuple_to_name_value_pairs.sql
+++ tests/queries/0_stateless/02007_tuple_to_name_value_pairs.sql
@@ -1,5 +1,4 @@
 SELECT tupleToNameValuePairs(tuple(1, 2, 3));
--- [('1',1),('2',2),('3',3)]
 
 DROP TABLE IF EXISTS test02007;
 CREATE TABLE test02007 (
@@ -11,8 +10,6 @@ CREATE TABLE test02007 (
 INSERT INTO test02007 VALUES (tuple(tuple(1, 2), tuple(3, 4)));
 INSERT INTO test02007 VALUES (tuple(tuple(5, 6), tuple(7, 8)));
 SELECT tupleToNameValuePairs(col) FROM test02007 ORDER BY col;
--- [('a',(1,2)),('b',(3,4))]
--- [('a',(5,6)),('b',(7,8))]
 
 DROP TABLE IF EXISTS test02007;
 CREATE TABLE test02007 (
@@ -20,14 +17,9 @@ CREATE TABLE test02007 (
 ) ENGINE=Memory();
 INSERT INTO test02007 VALUES (tuple(3.3, 5.5, 6.6));
 SELECT untuple(arrayJoin(tupleToNameValuePairs(col))) from test02007;
--- CPU	3.3
--- Memory	5.5
--- Disk	6.6
 
 DROP TABLE IF EXISTS test02007;
 SELECT tupleToNameValuePairs(tuple(1, 1.3)); -- { serverError 43; } should it?
 SELECT tupleToNameValuePairs(tuple(1, [1,2])); -- { serverError 43; }
 SELECT tupleToNameValuePairs(tuple(1, 'a')); -- { serverError 43; }
 SELECT tupleToNameValuePairs(33); -- { serverError 43; }
-
-
