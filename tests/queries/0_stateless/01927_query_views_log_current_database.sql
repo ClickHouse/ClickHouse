90635053cea691e2b7779ad94f4715c5c1a396af
Raúl Marín <git@rmr.ninja>
2021-08-10 11:31:39 +0200

    MV: Test profile events and view type

--- tests/queries/0_stateless/01927_query_views_log_current_database.sql
+++ tests/queries/0_stateless/01927_query_views_log_current_database.sql
@@ -12,9 +12,9 @@ CREATE TABLE table_e (a Float64, count Int64) ENGINE MergeTree ORDER BY a;
 CREATE TABLE table_f (a Float64, count Int64) ENGINE MergeTree ORDER BY a;
 
 -- SETUP MATERIALIZED VIEWS
-CREATE MATERIALIZED VIEW matview_a_to_b TO table_b AS SELECT toFloat64(a) AS a, b AS count FROM table_a;
-CREATE MATERIALIZED VIEW matview_b_to_c TO table_c AS SELECT SUM(a) as a FROM table_b;
-CREATE MATERIALIZED VIEW matview_join_d_e TO table_f AS SELECT table_d.a as a, table_e.count as count FROM table_d LEFT JOIN table_e ON table_d.a = table_e.a;
+CREATE MATERIALIZED VIEW matview_a_to_b TO table_b AS SELECT toFloat64(a) AS a, b + sleepEachRow(0.000001) AS count FROM table_a;
+CREATE MATERIALIZED VIEW matview_b_to_c TO table_c AS SELECT SUM(a + sleepEachRow(0.000002)) as a FROM table_b;
+CREATE MATERIALIZED VIEW matview_join_d_e TO table_f AS SELECT table_d.a as a, table_e.count + sleepEachRow(0.000003) as count FROM table_d LEFT JOIN table_e ON table_d.a = table_e.a;
 
 -- SETUP LIVE VIEW
 ---- table_b_live_view (Int64)
@@ -43,21 +43,26 @@ SELECT
     written_rows,
     arraySort(databases) as databases,
     arraySort(tables) as tables,
-    arraySort(views) as views
+    arraySort(views) as views,
+    ProfileEvents['SleepFunctionCalls'] as sleep_calls,
+    ProfileEvents['SleepFunctionMicroseconds'] as sleep_us
 FROM system.query_log
 WHERE query like '-- INSERT 1%INSERT INTO table_a%'
   AND current_database = currentDatabase()
   AND event_date >= yesterday()
-FORMAT JSONEachRow;
+FORMAT Vertical;
 
 SELECT
     'Depending views' as stage,
     view_name,
+    view_type,
     status,
     view_target,
     view_query,
     read_rows,
-    written_rows
+    written_rows,
+    ProfileEvents['SleepFunctionCalls'] as sleep_calls,
+    ProfileEvents['SleepFunctionMicroseconds'] as sleep_us
 FROM system.query_views_log
 WHERE initial_query_id =
       (
@@ -69,7 +74,7 @@ WHERE initial_query_id =
           LIMIT 1
       )
 ORDER BY view_name
-FORMAT JSONEachRow;
+FORMAT Vertical;
 
 -- CHECK LOGS OF INSERT 2
 SELECT
@@ -78,21 +83,26 @@ SELECT
     written_rows,
     arraySort(databases) as databases,
     arraySort(tables) as tables,
-    arraySort(views) as views
+    arraySort(views) as views,
+    ProfileEvents['SleepFunctionCalls'] as sleep_calls,
+    ProfileEvents['SleepFunctionMicroseconds'] as sleep_us
 FROM system.query_log
 WHERE query like '-- INSERT 2%INSERT INTO table_d%'
   AND current_database = currentDatabase()
   AND event_date >= yesterday()
-FORMAT JSONEachRow;
+FORMAT Vertical;
 
 SELECT
     'Depending views 2' as stage,
     view_name,
+    view_type,
     status,
     view_target,
     view_query,
     read_rows,
-    written_rows
+    written_rows,
+    ProfileEvents['SleepFunctionCalls'] as sleep_calls,
+    ProfileEvents['SleepFunctionMicroseconds'] as sleep_us
 FROM system.query_views_log
 WHERE initial_query_id =
       (
@@ -104,7 +114,7 @@ WHERE initial_query_id =
           LIMIT 1
       )
 ORDER BY view_name
-    FORMAT JSONEachRow;
+FORMAT Vertical;
 
 -- TEARDOWN
 DROP TABLE table_b_live_view;
