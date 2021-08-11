49d15eb8505ab62e2bb59f7d8bebb775da176343
Braulio Valdivielso <brlvldvlsmrtnz@gmail.com>
2021-08-11 18:38:27 +0100

    rename tests

--- tests/queries/0_stateless/02008_tuple_to_name_value_pairs.sql
+++ tests/queries/0_stateless/02008_tuple_to_name_value_pairs.sql
@@ -1,24 +1,24 @@
 SELECT tupleToNameValuePairs(tuple(1, 2, 3));
 
-DROP TABLE IF EXISTS test02007;
-CREATE TABLE test02007 (
+DROP TABLE IF EXISTS test02008;
+CREATE TABLE test02008 (
        col Tuple(
            a Tuple(key1 int, key2 int),
            b Tuple(key1 int, key3 int)
        )
 ) ENGINE=Memory();
-INSERT INTO test02007 VALUES (tuple(tuple(1, 2), tuple(3, 4)));
-INSERT INTO test02007 VALUES (tuple(tuple(5, 6), tuple(7, 8)));
-SELECT tupleToNameValuePairs(col) FROM test02007 ORDER BY col;
+INSERT INTO test02008 VALUES (tuple(tuple(1, 2), tuple(3, 4)));
+INSERT INTO test02008 VALUES (tuple(tuple(5, 6), tuple(7, 8)));
+SELECT tupleToNameValuePairs(col) FROM test02008 ORDER BY col;
 
-DROP TABLE IF EXISTS test02007;
-CREATE TABLE test02007 (
+DROP TABLE IF EXISTS test02008;
+CREATE TABLE test02008 (
        col Tuple(CPU double, Memory double, Disk double)
 ) ENGINE=Memory();
-INSERT INTO test02007 VALUES (tuple(3.3, 5.5, 6.6));
-SELECT untuple(arrayJoin(tupleToNameValuePairs(col))) from test02007;
+INSERT INTO test02008 VALUES (tuple(3.3, 5.5, 6.6));
+SELECT untuple(arrayJoin(tupleToNameValuePairs(col))) from test02008;
 
-DROP TABLE IF EXISTS test02007;
+DROP TABLE IF EXISTS test02008;
 SELECT tupleToNameValuePairs(tuple(1, 1.3)); -- { serverError 43; }
 SELECT tupleToNameValuePairs(tuple(1, [1,2])); -- { serverError 43; }
 SELECT tupleToNameValuePairs(tuple(1, 'a')); -- { serverError 43; }
