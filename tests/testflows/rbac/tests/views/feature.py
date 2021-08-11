2d397e754bf5418f5099b422579e3a6a26abad44
Vitaliy Zakaznikov <vzakaznikov@protonmail.com>
2021-08-11 18:55:17 -0400

    Forcing views feature to be xfailed.

--- tests/testflows/rbac/tests/views/feature.py
+++ tests/testflows/rbac/tests/views/feature.py
@@ -5,6 +5,8 @@ from rbac.helper.common import *
 @TestFeature
 @Name("views")
 def feature(self):
+    xfail("broken", reason="https://github.com/ClickHouse/ClickHouse/issues/27570")
+
     with Pool(3) as pool:
         try:
             Feature(run=load("rbac.tests.views.view", "feature"), parallel=True, executor=pool)
