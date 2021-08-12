72cf9e9c75c9644e28bbf1b0773bceb3b2a7d491
Nikolai Kochetov <nik-kochetov@yandex-team.ru>
2021-08-12 12:18:20 +0300

    Add comment.

--- src/Common/DenseHashMap.h
+++ src/Common/DenseHashMap.h
@@ -1,6 +1,11 @@
 #pragma once
 #include <unordered_map>
 
+/// DenseHashMap is a wrapper for google::dense_hash_map.
+/// Some hacks are needed to make it work in "Arcadia".
+/// "Arcadia" is a proprietary monorepository in Yandex.
+/// It uses slightly changed version of sparsehash with a different set of hash functions (which we don't need).
+/// Those defines are needed to make it compile.
 #if defined(ARCADIA_BUILD)
 #define HASH_FUN_H <unordered_map>
 template <typename T>
