b25646bb02e286c8a600f1b73fa879f1b8cd6467
Kseniia Sumarokova <54203879+kssenii@users.noreply.github.com>
2021-08-14 17:29:24 +0300

    Update DatabasePostgreSQL.cpp

--- src/Databases/PostgreSQL/DatabasePostgreSQL.cpp
+++ src/Databases/PostgreSQL/DatabasePostgreSQL.cpp
@@ -156,7 +156,7 @@ StoragePtr DatabasePostgreSQL::tryGetTable(const String & table_name, ContextPtr
 }
 
 
-StoragePtr DatabasePostgreSQL::fetchTable(const String & table_name, ContextPtr, const bool table_checked) const
+StoragePtr DatabasePostgreSQL::fetchTable(const String & table_name, ContextPtr local_context, const bool table_checked) const
 {
     if (!cache_tables || !cached_tables.count(table_name))
     {
