6e5ce26bb3e06ef6af8c31bbd9d6e6d30abcb295
kssenii <sumarokovakseniia@mail.ru>
2021-08-15 13:28:44 +0300

    Update test

--- tests/queries/0_stateless/02008_test_union_distinct_in_subquery.sql
+++ tests/queries/0_stateless/02008_test_union_distinct_in_subquery.sql
@@ -5,5 +5,6 @@ CREATE DATABASE db1 ENGINE=Ordinary;
 CREATE DATABASE db2 ENGINE=Ordinary;
 CREATE DATABASE db3 ENGINE=Ordinary;
 
+select uuid from system.databases where name like 'db%' union distinct select uuid from system.databases where name like 'db%';
 SELECT count() FROM　(SELECT * FROM system.databases WHERE name LIKE 'db%' UNION DISTINCT SELECT * FROM system.databases WHERE name LIKE 'db%');
-SELECT uuid FROM　(SELECT * FROM system.databases WHERE name LIKE 'db%' UNION DISTINCT SELECT * FROM system.databases WHERE name LIKE 'db%');
+select uuid from　(select * from system.databases where name like 'db%' union distinct select * from system.databases where name like 'db%');
