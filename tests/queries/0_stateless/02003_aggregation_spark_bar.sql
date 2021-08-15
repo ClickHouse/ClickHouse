7f45c4a2b537649bcbfd16c2d7ad076963a6f88d
zhangxiao871 <zhangxiao871@ZBMAC-C02DN6312.local>
2021-08-15 19:50:32 +0800

    Add tests.

--- tests/queries/0_stateless/02003_aggregation_spark_bar.sql
+++ tests/queries/0_stateless/02003_aggregation_spark_bar.sql
@@ -1,17 +1,26 @@
-drop table if exists spark_bar_test;
+DROP TABLE IF EXISTS spark_bar_test;
 
 CREATE TABLE spark_bar_test (`cnt` UInt64,`event_date` Date) ENGINE = MergeTree ORDER BY event_date SETTINGS index_granularity = 8192;
 
-insert into spark_bar_test values(1,'2020-01-01'),(4,'2020-01-02'),(5,'2020-01-03'),(2,'2020-01-04'),(3,'2020-01-05'),(7,'2020-01-06'),(6,'2020-01-07'),(8,'2020-01-08'),(2,'2020-01-11');
+INSERT INTO spark_bar_test VALUES(1,'2020-01-01'),(4,'2020-01-02'),(5,'2020-01-03'),(2,'2020-01-04'),(3,'2020-01-05'),(7,'2020-01-06'),(6,'2020-01-07'),(8,'2020-01-08'),(2,'2020-01-11');
 
-select sparkbar(1)(event_date,cnt) from spark_bar_test;
-select sparkbar(2)(event_date,cnt) from spark_bar_test;
-select sparkbar(3)(event_date,cnt) from spark_bar_test;
-select sparkbar(4)(event_date,cnt) from spark_bar_test;
-select sparkbar(5)(event_date,cnt) from spark_bar_test;
-select sparkbar(6)(event_date,cnt) from spark_bar_test;
-select sparkbar(7)(event_date,cnt) from spark_bar_test;
-select sparkbar(8)(event_date,cnt) from spark_bar_test;
-select sparkbar(9)(event_date,cnt) from spark_bar_test;
-select sparkbar(10)(event_date,cnt) from spark_bar_test;
-select sparkbar(11)(event_date,cnt) from spark_bar_test;
+SELECT sparkbar(1)(event_date,cnt) from spark_bar_test;
+SELECT sparkbar(2)(event_date,cnt) from spark_bar_test;
+SELECT sparkbar(3)(event_date,cnt) from spark_bar_test;
+SELECT sparkbar(4)(event_date,cnt) from spark_bar_test;
+SELECT sparkbar(5)(event_date,cnt) from spark_bar_test;
+SELECT sparkbar(6)(event_date,cnt) from spark_bar_test;
+SELECT sparkbar(7)(event_date,cnt) from spark_bar_test;
+SELECT sparkbar(8)(event_date,cnt) from spark_bar_test;
+SELECT sparkbar(9)(event_date,cnt) from spark_bar_test;
+SELECT sparkbar(10)(event_date,cnt) from spark_bar_test;
+SELECT sparkbar(11)(event_date,cnt) from spark_bar_test;
+
+SELECT sparkbar(11,2,5)(event_date,cnt) from spark_bar_test;
+SELECT sparkbar(11,3,7)(event_date,cnt) from spark_bar_test;
+SELECT sparkbar(11,4,11)(event_date,cnt) from spark_bar_test;
+
+SELECT sparkbar(11,toDate('2020-01-02'),toDate('2020-01-02'))(event_date,cnt) from spark_bar_test;
+SELECT sparkbar(11,toDate('2020-01-02'),toDate('2020-01-05'))(event_date,cnt) from spark_bar_test;
+SELECT sparkbar(11,toDate('2020-01-03'),toDate('2020-01-07'))(event_date,cnt) from spark_bar_test;
+SELECT sparkbar(11,toDate('2020-01-04'),toDate('2020-01-11'))(event_date,cnt) from spark_bar_test;
