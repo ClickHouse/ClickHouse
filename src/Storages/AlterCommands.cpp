57f78b766c41259398c9c535a4d51a637e6bac22
vicgao <vicgao@tencent.com>
2021-08-17 20:52:16 +0800

    modify column datatype to Nullable(datatype) or Nullable(datatype) to Nullable(datatype) donot allways need mutations.

--- src/Storages/AlterCommands.cpp
+++ src/Storages/AlterCommands.cpp
@@ -697,9 +697,9 @@ bool isMetadataOnlyConversion(const IDataType * from, const IDataType * to)
 
         const auto * nullable_from = typeid_cast<const DataTypeNullable *>(from);
         const auto * nullable_to = typeid_cast<const DataTypeNullable *>(to);
-        if (nullable_from && nullable_to)
+        if (nullable_to)
         {
-            from = nullable_from->getNestedType().get();
+            from = nullable_from ? nullable_from->getNestedType().get() : from;
             to = nullable_to->getNestedType().get();
             continue;
         }
