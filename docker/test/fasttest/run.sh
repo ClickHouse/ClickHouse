6ee6fb2ef21d5f3368ca68e3b8e82eed49229942
Nikolay Degterinsky <evillique@gmail.com>
2021-08-18 17:34:51 +0300

    Add test

--- docker/test/fasttest/run.sh
+++ docker/test/fasttest/run.sh
@@ -314,6 +314,7 @@ function run_tests
         01799_long_uniq_theta_sketch
         01890_stem                               # depends on libstemmer_c
         02003_compress_bz2                       # depends on bzip2
+        02012_compress_lz4                       # depends on lz4
         collate
         collation
         _orc_
