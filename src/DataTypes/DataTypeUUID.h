7fe60b07879df34374011b2bfbbe7f1a8883e74e
Vasily Nemkov <V.Nemkov@gmail.com>
2021-08-18 23:34:07 +0300

    Special case of conversion code for types with custom serialization/deserialization
    Fixes CASTing from String or FixedString to IPv4 or IPv6 and back.

--- src/DataTypes/DataTypeUUID.h
+++ src/DataTypes/DataTypeUUID.h
@@ -15,9 +15,10 @@ public:
 
     using FieldType = UUID;
     using ColumnType = ColumnVector<UUID>;
+    static constexpr auto type_id = TypeIndex::UUID;
 
     const char * getFamilyName() const override { return "UUID"; }
-    TypeIndex getTypeId() const override { return TypeIndex::UUID; }
+    TypeIndex getTypeId() const override { return type_id; }
 
     Field getDefault() const override;
 
