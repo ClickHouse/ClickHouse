e4b6628d2a2b966a19f961de0fcf6e4439214012
Nikolay Degterinsky <evillique@gmail.com>
2021-08-20 19:40:38 +0300

    Fix test

--- tests/queries/0_stateless/01059_storage_file_compression.sql
+++ tests/queries/0_stateless/01059_storage_file_compression.sql
@@ -39,11 +39,20 @@ SELECT count(), max(x) FROM file;
 
 DROP TABLE file;
 
-SELECT count(), max(x) FROM file('data{1,2,3,4,5}.tsv.{gz,br,xz,zst,lz4}', TSV, 'x UInt64');
+CREATE TABLE file (x UInt64) ENGINE = File(TSV, 'data6.tsv.bz2');
+TRUNCATE TABLE file;
+
+INSERT INTO file SELECT * FROM numbers(1000000);
+SELECT count(), max(x) FROM file;
+
+DROP TABLE file;
+
+SELECT count(), max(x) FROM file('data{1,2,3,4,5,6}.tsv.{gz,br,xz,zst,lz4,bz2}', TSV, 'x UInt64');
 
 -- check that they are compressed
 SELECT count() < 1000000, max(x) FROM file('data1.tsv.br', RowBinary, 'x UInt8', 'none');
 SELECT count() < 3000000, max(x) FROM file('data2.tsv.gz', RowBinary, 'x UInt8', 'none');
 SELECT count() < 1000000, max(x) FROM file('data3.tsv.xz', RowBinary, 'x UInt8', 'none');
 SELECT count() < 1000000, max(x) FROM file('data4.tsv.zst', RowBinary, 'x UInt8', 'none');
-SELECT count() < 1000000, max(x) FROM file('data5.tsv.lz4', RowBinary, 'x UInt8', 'none');
+SELECT count() < 4000000, max(x) FROM file('data5.tsv.lz4', RowBinary, 'x UInt8', 'none');
+SELECT count() < 2000000, max(x) FROM file('data6.tsv.bz2', RowBinary, 'x UInt8', 'none');
