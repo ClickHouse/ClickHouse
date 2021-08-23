04222f4e0df8492fd2f61de105f61c07eaefa2c6
Alexey Boykov <mathalex@mail.ru>
2021-08-23 16:41:20 +0300

    Divide, negate, test

--- tests/queries/0_stateless/02011_tuple_vector_functions.sql
+++ tests/queries/0_stateless/02011_tuple_vector_functions.sql
@@ -4,3 +4,6 @@ SELECT tupleHammingDistance(tuple(1, 3), tuple(1, 2));
 SELECT tuple(1, 2) + tuple(3, 4) * tuple(5, 1) - tuple(6, 3);
 SELECT vectorDifference(tuplePlus(tuple(1, 2), tuple(3, 4)), tuple(5, 6));
 SELECT tupleMinus(vectorSum(tupleMultiply(tuple(1, 2), tuple(3, 4)), tuple(5, 6)), tuple(31, 41));
+
+SELECT tupleDivide(tuple(5, 8, 11),tuple(-2, 2, 4));
+SELECT tupleNegate(tuple(1, 0, 3.5));
