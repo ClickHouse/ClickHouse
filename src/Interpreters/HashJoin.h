38374ba07b52e0051e9b34c4f426990185c4ae66
vdimir <vdimir@yandex-team.ru>
2021-08-23 14:09:33 +0300

    fix join->getNonJoinedBlocks

--- src/Interpreters/HashJoin.h
+++ src/Interpreters/HashJoin.h
@@ -178,7 +178,8 @@ public:
       * Use only after all calls to joinBlock was done.
       * left_sample_block is passed without account of 'use_nulls' setting (columns will be converted to Nullable inside).
       */
-    std::shared_ptr<NotJoinedBlocks> getNonJoinedBlocks(const Block & result_sample_block, UInt64 max_block_size) const override;
+    std::shared_ptr<NotJoinedBlocks> getNonJoinedBlocks(
+        const Block & left_sample_block, const Block & result_sample_block, UInt64 max_block_size) const override;
 
     /// Number of keys in all built JOIN maps.
     size_t getTotalRowCount() const final;
