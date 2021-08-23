de5daa22abaa05506d9596ca8c22f11c2fb9bc3c
Vladimir Chebotarev <vladimir.chebotarev@gmail.com>
2021-08-23 08:33:10 +0300

    Revert "MATERIALIZE COLUMN xxx FINAL"
    This reverts commit bf1430c1ec9ece796c918724464b459d83050e81.

--- src/Storages/MergeTree/StorageFromMergeTreeDataPart.h
+++ src/Storages/MergeTree/StorageFromMergeTreeDataPart.h
@@ -78,16 +78,6 @@ public:
         return parts.front()->storage.getSettings()->materialize_ttl_recalculate_only;
     }
 
-    bool isColumnEmpty(const String & column_name) const
-    {
-        for (const auto & part : parts)
-        {
-            if (part->getColumnSize(column_name, DataTypeString()/* not used */).data_uncompressed)
-                return false;
-        }
-        return true;
-    }
-
 protected:
     /// Used in part mutation.
     StorageFromMergeTreeDataPart(const MergeTreeData::DataPartPtr & part_)
--- src/Storages/MutationCommands.cpp
+++ src/Storages/MutationCommands.cpp
@@ -82,7 +82,6 @@ std::optional<MutationCommand> MutationCommand::parse(ASTAlterCommand * command,
         res.type = MATERIALIZE_COLUMN;
         res.partition = command->partition;
         res.column_name = getIdentifierName(command->column);
-        res.materialize_column_final = command->materialize_column_final;
         return res;
     }
     else if (parse_alter_commands && command->type == ASTAlterCommand::MODIFY_COLUMN)
