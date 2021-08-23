d5b116b2da8226f7f1a87263ce443480a516b53c
Dmitry Novik <n0vik@yandex-team.ru>
2021-08-24 01:00:41 +0300

    update tests

--- tests/queries/0_stateless/02012_changed_enum_type_non_replicated.sql
+++ tests/queries/0_stateless/02012_changed_enum_type_non_replicated.sql
@@ -4,5 +4,5 @@ alter table enum_alter_issue modify column a Enum8('one' = 1, 'two' = 2, 'three'
 insert into enum_alter_issue values ('one'), ('two');
 alter table enum_alter_issue detach partition id 'all';
 alter table enum_alter_issue attach partition id 'all';
-select * from enum_alter_issue;
+select * from enum_alter_issue order by a;
 drop table enum_alter_issue;
