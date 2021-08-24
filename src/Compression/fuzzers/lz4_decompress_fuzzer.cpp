d5b7cb6da05ca2e0f092884ac54472e2e03b2124
Nikita Mikhaylov <mikhaylovnikitka@gmail.com>
2021-08-24 13:36:07 +0000

    Fix tests

--- src/Compression/fuzzers/lz4_decompress_fuzzer.cpp
+++ src/Compression/fuzzers/lz4_decompress_fuzzer.cpp
@@ -3,6 +3,7 @@
 
 #include <Compression/ICompressionCodec.h>
 #include <IO/BufferWithOwnMemory.h>
+#include <Compression/LZ4_decompress_faster.h>
 
 namespace DB
 {
@@ -19,7 +20,7 @@ extern "C" int LLVMFuzzerTestOneInput(const uint8_t * data, size_t size)
 try
 {
 
-    if (size < sizeof(AuxiliaryRandomData))
+    if (size < sizeof(AuxiliaryRandomData) + LZ4::ADDITIONAL_BYTES_AT_END_OF_BUFFER)
         return 0;
 
     const auto * p = reinterpret_cast<const AuxiliaryRandomData *>(data);
@@ -27,13 +28,14 @@ try
 
     size_t output_buffer_size = p->decompressed_size % 65536;
     size -= sizeof(AuxiliaryRandomData);
+    size -= LZ4::ADDITIONAL_BYTES_AT_END_OF_BUFFER;
     data += sizeof(AuxiliaryRandomData) / sizeof(uint8_t);
 
     // std::string input = std::string(reinterpret_cast<const char*>(data), size);
     // fmt::print(stderr, "Using input {} of size {}, output size is {}. \n", input, size, output_buffer_size);
 
     DB::Memory<> memory;
-    memory.resize(output_buffer_size + codec->getAdditionalSizeAtTheEndOfBuffer());
+    memory.resize(output_buffer_size + LZ4::ADDITIONAL_BYTES_AT_END_OF_BUFFER);
 
     codec->doDecompressData(reinterpret_cast<const char *>(data), size, memory.data(), output_buffer_size);
 
