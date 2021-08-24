628b152cd1bd66c1d0b06befe24a191a5231fefc
Maksim Kita <maksim-kita@yandex-team.ru>
2021-08-24 19:13:38 +0300

    Fixed build

--- src/Interpreters/InterpreterDropFunctionQuery.cpp
+++ src/Interpreters/InterpreterDropFunctionQuery.cpp
@@ -12,14 +12,14 @@ namespace DB
 
 BlockIO InterpreterDropFunctionQuery::execute()
 {
-    auto context = getContext();
-    context->checkAccess(AccessType::DROP_FUNCTION);
+    auto current_context = getContext();
+    current_context->checkAccess(AccessType::DROP_FUNCTION);
 
     FunctionNameNormalizer().visit(query_ptr.get());
     auto & drop_function_query = query_ptr->as<ASTDropFunctionQuery &>();
 
     UserDefinedFunctionFactory::instance().unregisterFunction(drop_function_query.function_name);
-    UserDefinedObjectsLoader::instance().removeObject(context, UserDefinedObjectType::Function, drop_function_query.function_name);
+    UserDefinedObjectsLoader::instance().removeObject(current_context, UserDefinedObjectType::Function, drop_function_query.function_name);
 
     return {};
 }
