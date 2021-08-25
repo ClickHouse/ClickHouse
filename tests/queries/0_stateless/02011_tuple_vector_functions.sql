5fea1b0a33ce26b58e8baff65050740609b3f77c
Alexey Boykov <mathalex@mail.ru>
2021-08-25 17:02:04 +0300

    Reduced copy-paste, dotProduct

--- tests/queries/0_stateless/02011_tuple_vector_functions.sql
+++ tests/queries/0_stateless/02011_tuple_vector_functions.sql
@@ -7,3 +7,6 @@ SELECT tupleMinus(vectorSum(tupleMultiply(tuple(1, 2), tuple(3, 4)), tuple(5, 6)
 
 SELECT tupleDivide(tuple(5, 8, 11),tuple(-2, 2, 4));
 SELECT tupleNegate(tuple(1, 0, 3.5));
+
+SELECT dotProduct(tuple(1, 2, 3), tuple(2, 3, 4));
+SELECT scalarProduct(tuple(-1, 2, 3.002), tuple(2, 3.4, 4));
