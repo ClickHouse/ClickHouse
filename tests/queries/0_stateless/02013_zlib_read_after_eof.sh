8e7e6232ae85aaef4f81ceda9b4edd33f69ddc20
Nikita Mikhaylov <mikhaylovnikitka@gmail.com>
2021-08-25 17:42:57 +0000

    Added a test

---
+++ tests/queries/0_stateless/02013_zlib_read_after_eof.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+
+
+CUR_DIR=$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)
+# shellcheck source=../shell_config.sh
+. "$CUR_DIR"/../shell_config.sh
+
+$CLICKHOUSE_CLIENT -q "CREATE TABLE graphite(\`Path\` String, \`Value\` Float64, \`Time\` UInt32, \`Date\` Date, \`Timestamp\` UInt32) \
+    ENGINE = MergeTree PARTITION BY toYYYYMM(Date) ORDER BY (Path, Time) SETTINGS index_granularity = 8192;"
+
+cat 02013_zlib_read_after_eof_data | go run 02013_zlib_read_after_eof.go
+
+
+$CLICKHOUSE_CLIENT -q "drop table graphite"
