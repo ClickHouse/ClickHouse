239b198a125ba1dc5c3da548d00a91d17afe76c5
Artur <613623@mail.ru>
2021-08-25 16:56:19 +0000

    fix eof

--- src/IO/ZlibInflatingReadBuffer.cpp
+++ src/IO/ZlibInflatingReadBuffer.cpp
@@ -48,40 +48,44 @@ ZlibInflatingReadBuffer::~ZlibInflatingReadBuffer()
 
 bool ZlibInflatingReadBuffer::nextImpl()
 {
-    if (eof)
-        return false;
-
-    if (!zstr.avail_in)
+    do
     {
-        in->nextIfAtEnd();
-        zstr.next_in = reinterpret_cast<unsigned char *>(in->position());
-        zstr.avail_in = in->buffer().end() - in->position();
-    }
-    zstr.next_out = reinterpret_cast<unsigned char *>(internal_buffer.begin());
-    zstr.avail_out = internal_buffer.size();
+        if (eof)
+            return false;
 
-    int rc = inflate(&zstr, Z_NO_FLUSH);
-
-    in->position() = in->buffer().end() - zstr.avail_in;
-    working_buffer.resize(internal_buffer.size() - zstr.avail_out);
-
-    if (rc == Z_STREAM_END)
-    {
-        if (in->eof())
+        if (!zstr.avail_in)
         {
-            eof = true;
-            return !working_buffer.empty();
+            in->nextIfAtEnd();
+            zstr.next_in = reinterpret_cast<unsigned char *>(in->position());
+            zstr.avail_in = in->buffer().end() - in->position();
         }
-        else
+        zstr.next_out = reinterpret_cast<unsigned char *>(internal_buffer.begin());
+        zstr.avail_out = internal_buffer.size();
+
+        int rc = inflate(&zstr, Z_NO_FLUSH);
+
+        in->position() = in->buffer().end() - zstr.avail_in;
+        working_buffer.resize(internal_buffer.size() - zstr.avail_out);
+
+        if (rc == Z_STREAM_END)
         {
-            rc = inflateReset(&zstr);
-            if (rc != Z_OK)
-                throw Exception(std::string("inflateReset failed: ") + zError(rc), ErrorCodes::ZLIB_INFLATE_FAILED);
-            return true;
+            if (in->eof())
+            {
+                eof = true;
+                return !working_buffer.empty();
+            }
+            else
+            {
+                rc = inflateReset(&zstr);
+                if (rc != Z_OK)
+                    throw Exception(std::string("inflateReset failed: ") + zError(rc), ErrorCodes::ZLIB_INFLATE_FAILED);
+                return true;
+            }
         }
+        if (rc != Z_OK)
+            throw Exception(std::string("inflate failed: ") + zError(rc), ErrorCodes::ZLIB_INFLATE_FAILED);
     }
-    if (rc != Z_OK)
-        throw Exception(std::string("inflate failed: ") + zError(rc), ErrorCodes::ZLIB_INFLATE_FAILED);
+    while (working_buffer.empty());
 
     return true;
 }
