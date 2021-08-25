6b10682903f949494cfa950d403bda9aaf74a2e1
Artur <613623@mail.ru>
2021-08-25 18:01:10 +0000

    improve test

--- tests/queries/0_stateless/02013_zlib_read_after_eof.sh
+++ tests/queries/0_stateless/02013_zlib_read_after_eof.sh
@@ -1,14 +1,17 @@
 #!/usr/bin/env bash
 
-
 CUR_DIR=$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)
 # shellcheck source=../shell_config.sh
 . "$CUR_DIR"/../shell_config.sh
 
+
+$CLICKHOUSE_CLIENT -q "DROP TABLE IF EXISTS graphite;"
+
 $CLICKHOUSE_CLIENT -q "CREATE TABLE graphite(\`Path\` String, \`Value\` Float64, \`Time\` UInt32, \`Date\` Date, \`Timestamp\` UInt32) \
     ENGINE = MergeTree PARTITION BY toYYYYMM(Date) ORDER BY (Path, Time) SETTINGS index_granularity = 8192;"
 
-cat 02013_zlib_read_after_eof_data | go run 02013_zlib_read_after_eof.go
+cat data_zlib/02013_zlib_read_after_eof_data | go run 02013_zlib_read_after_eof.go
 
+$CLICKHOUSE_CLIENT -q "SELECT count() FROM graphite;"
 
-$CLICKHOUSE_CLIENT -q "drop table graphite"
+$CLICKHOUSE_CLIENT -q "drop table graphite;"
