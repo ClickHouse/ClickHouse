49a04c71de91b80392e81a7e07f12aaff3c88d27
Nikita Mikhaylov <mikhaylovnikitka@gmail.com>
2021-08-26 21:53:32 +0000

    Added minio

--- tests/queries/0_stateless/02012_settings_clause_for_s3.sql
+++ tests/queries/0_stateless/02012_settings_clause_for_s3.sql
@@ -1,11 +1,13 @@
-DROP TABLE IF EXISTS table_with_range;
+-- DROP TABLE IF EXISTS table_with_range;
 
-CREATE TABLE table_with_range
-(
-    `name` String,
-    `value` UInt32
-)
-ENGINE = S3('https://storage.yandexcloud.net/my-test-bucket-768/{some,another}_prefix/some_file_{1..3}', 'CSV')
-SETTINGS input_format_with_names_use_header = 0;
+-- CREATE TABLE table_with_range
+-- (
+--     `name` String,
+--     `value` UInt32
+-- )
+-- ENGINE = S3('https://storage.yandexcloud.net/my-test-bucket-768/{some,another}_prefix/some_file_{1..3}', 'CSV')
+-- SETTINGS input_format_with_names_use_header = 0;
 
-DROP TABLE IF EXISTS table_with_range;
+select * from s3('http://localhost:11111/test/a.txt', 'test', 'testtest', 'LineAsString', 'line String');
+
+-- DROP TABLE IF EXISTS table_with_range;
