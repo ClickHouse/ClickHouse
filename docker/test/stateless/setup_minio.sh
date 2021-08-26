1adbb2db1801e1bc9bf4e9e968ae42b381ae5896
Nikita Mikhaylov <mikhaylovnikitka@gmail.com>
2021-08-27 02:51:51 +0300

    Update setup_minio.sh

--- docker/test/stateless/setup_minio.sh
+++ docker/test/stateless/setup_minio.sh
@@ -8,12 +8,12 @@ chmod +x minio
 ./minio server --address ":11111" ./data > /dev/null &
 
 wget 'https://dl.min.io/client/mc/release/linux-amd64/mc'
-chmod +x mc
+chmod +x ./mc
 
-mc alias set clickminio http://localhost:11111 clickhouse clickhouse
-mc admin user add clickminio test testtest
-mc admin policy set clickminio readwrite user=test
-mc mb clickminio/test
+./mc alias set clickminio http://localhost:11111 clickhouse clickhouse
+./mc admin user add clickminio test testtest
+./mc admin policy set clickminio readwrite user=test
+./mc mb clickminio/test
 
 echo -e "a b c\na b c\na b c\n" > a.txt
-mc cp a.txt clickminio/test
+./mc cp a.txt clickminio/test
