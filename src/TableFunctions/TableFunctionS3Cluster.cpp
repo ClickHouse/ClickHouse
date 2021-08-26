e41d1cff1d9ca08037ee6ae433c9592c28736c68
Nikita Mikhaylov <mikhaylovnikitka@gmail.com>
2021-08-26 18:39:06 +0000

    Fix test

--- src/TableFunctions/TableFunctionS3Cluster.cpp
+++ src/TableFunctions/TableFunctionS3Cluster.cpp
@@ -128,7 +128,7 @@ StoragePtr TableFunctionS3Cluster::executeImpl(
             ConstraintsDescription{},
             String{},
             context,
-            // No format_settings for S3Cluster, but maybe we could get them from current context ?
+            // No format_settings for S3Cluster
             std::nullopt,
             compression_method,
             /*distributed_processing=*/true);
