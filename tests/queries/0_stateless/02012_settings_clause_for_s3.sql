035d657ac31d56ca9906040768cce623a7c44e8f
Nikita Mikhaylov <mikhaylovnikitka@gmail.com>
2021-08-26 13:12:42 +0000

    Fix tests and update ya.make

--- tests/queries/0_stateless/02012_settings_clause_for_s3.sql
+++ tests/queries/0_stateless/02012_settings_clause_for_s3.sql
@@ -1,3 +1,5 @@
+DROP TABLE IF EXISTS table_with_range;
+
 CREATE TABLE table_with_range
 (
     `name` String,
@@ -6,3 +8,4 @@ CREATE TABLE table_with_range
 ENGINE = S3('https://storage.yandexcloud.net/my-test-bucket-768/{some,another}_prefix/some_file_{1..3}', 'CSV')
 SETTINGS input_format_with_names_use_header = 0;
 
+DROP TABLE IF EXISTS table_with_range;
