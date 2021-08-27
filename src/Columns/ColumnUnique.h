83dc149b5d8be420e405d220a7dab778fe433473
robot-clickhouse <robot-clickhouse@yandex-team.ru>
2021-08-27 15:45:35 +0300

    Backport #28205 to 21.3: Fix insertion of fields with Infinity values in nullable low cardinality columns

--- src/Columns/ColumnUnique.h
+++ src/Columns/ColumnUnique.h
@@ -282,7 +282,7 @@ size_t ColumnUnique<ColumnType>::getNullValueIndex() const
 template <typename ColumnType>
 size_t ColumnUnique<ColumnType>::uniqueInsert(const Field & x)
 {
-    if (x.getType() == Field::Types::Null)
+    if (x.isNull())
         return getNullValueIndex();
 
     if (isNumeric())
