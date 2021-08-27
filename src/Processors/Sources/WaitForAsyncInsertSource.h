9ad03fc6d401ebfbac2850abb389d1730c39f59e
Anton Popov <pad11rus@gmail.com>
2021-08-28 00:29:10 +0300

    add waiting for async inserts

---
+++ src/Processors/Sources/WaitForAsyncInsertSource.h
@@ -0,0 +1,37 @@
+#pragma once
+
+#include <Processors/ISource.h>
+#include <Interpreters/Context.h>
+
+namespace DB
+{
+
+class WaitForAsyncInsertSource : public ISource, WithContext
+{
+public:
+    WaitForAsyncInsertSource(
+        const Block & header, const String & query_id_,
+        size_t timeout_ms_, ContextPtr context_)
+        : ISource(std::move(header))
+        , WithContext(context_)
+        , query_id(query_id_)
+        , timeout_ms(timeout_ms_)
+    {
+    }
+
+    String getName() const override { return "WaitForAsyncInsert"; }
+
+protected:
+    Chunk generate() override
+    {
+        auto context = getContext();
+        context->waitForProcessingAsyncInsert(query_id, std::chrono::milliseconds(timeout_ms));
+        return Chunk();
+    }
+
+private:
+    String query_id;
+    size_t timeout_ms;
+};
+
+}
