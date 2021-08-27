e304f3d5b7a1564dfca0810f6b02bd2df0a0f1bd
Alexey Boykov <mathalex@mail.ru>
2021-08-27 18:44:04 +0300

    Names, tupleOperatorByNumber

--- tests/queries/0_stateless/02011_tuple_vector_functions.sql
+++ tests/queries/0_stateless/02011_tuple_vector_functions.sql
@@ -8,6 +8,11 @@ SELECT tupleMinus(vectorSum(tupleMultiply(tuple(1, 2), tuple(3, 4)), tuple(5, 6)
 SELECT tupleDivide(tuple(5, 8, 11),tuple(-2, 2, 4));
 SELECT tupleNegate(tuple(1, 0, 3.5));
 
+SELECT tupleMultiplyByNumber(tuple(1, 2, 3), 0.5);
+SELECT tupleDivideByNumber(tuple(1, 2.5, 3), 0.5);
+SELECT tupleMultiplyByNumber(tuple(1), 1);
+SELECT tupleDivideByNumber(tuple(1), 1);
+
 SELECT dotProduct(tuple(1, 2, 3), tuple(2, 3, 4));
 SELECT scalarProduct(tuple(-1, 2, 3.002), tuple(2, 3.4, 4));
 
