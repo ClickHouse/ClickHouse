4a88e5f281746ed642c7afbc2de72a79dd837e13
Nikita Mikhaylov <mikhaylovnikitka@gmail.com>
2021-08-27 13:12:19 +0000

    Better

--- tests/queries/0_stateless/02012_settings_clause_for_s3.sql
+++ tests/queries/0_stateless/02012_settings_clause_for_s3.sql
@@ -1,13 +1,7 @@
--- DROP TABLE IF EXISTS table_with_range;
+DROP TABLE IF EXISTS table_with_range;
 
--- CREATE TABLE table_with_range
--- (
---     `name` String,
---     `value` UInt32
--- )
--- ENGINE = S3('https://storage.yandexcloud.net/my-test-bucket-768/{some,another}_prefix/some_file_{1..3}', 'CSV')
--- SETTINGS input_format_with_names_use_header = 0;
+CREATE TABLE table_with_range(`name` String,`number` UInt32)　ENGINE = S3('http://localhost:11111/test/tsv_with_header.tsv', 'lapa', 'lapalapa', 'TSVWithNames')　SETTINGS input_format_with_names_use_header = 1;
 
-select * from s3('http://localhost:11111/test/a.txt', 'test', 'testtest', 'LineAsString', 'line String');
+select * from table_with_range;
 
--- DROP TABLE IF EXISTS table_with_range;
+DROP TABLE IF EXISTS table_with_range;
