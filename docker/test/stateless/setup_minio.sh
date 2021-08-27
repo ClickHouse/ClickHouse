2200cecf9a5516262e23a630209014514d51fbd9
Nikita Mikhaylov <mikhaylovnikitka@gmail.com>
2021-08-27 14:22:04 +0000

    Move dowloading to dockerfile

--- docker/test/stateless/setup_minio.sh
+++ docker/test/stateless/setup_minio.sh
@@ -2,13 +2,13 @@
 
 set -e -x -a
 
-wget 'https://dl.min.io/server/minio/release/linux-amd64/minio'
-chmod +x minio
+
+
 
 ./minio server --address ":11111" ./data > /dev/null &
 
-wget 'https://dl.min.io/client/mc/release/linux-amd64/mc'
-chmod +x ./mc
+
+
 
 ./mc alias set clickminio http://localhost:11111 clickhouse clickhouse
 ./mc admin user add clickminio test testtest
