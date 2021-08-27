083ca0f2831b0030dac53607eaed6a8e998003cd
Nikita Mikhaylov <mikhaylovnikitka@gmail.com>
2021-08-27 16:40:09 +0000

    Better

--- docker/test/stateless/setup_minio.sh
+++ docker/test/stateless/setup_minio.sh
@@ -2,18 +2,12 @@
 
 set -e -x -a
 
+/minio server --address ":11111" ./data > /dev/null &
 
-
-
-./minio server --address ":11111" ./data > /dev/null &
-
-
-
-
-./mc alias set clickminio http://localhost:11111 clickhouse clickhouse
-./mc admin user add clickminio test testtest
-./mc admin policy set clickminio readwrite user=test
-./mc mb clickminio/test
+/mc alias set clickminio http://localhost:11111 clickhouse clickhouse
+/mc admin user add clickminio test testtest
+/mc admin policy set clickminio readwrite user=test
+/mc mb clickminio/test
 
 
 # Upload data to Minio. By default after unpacking all tests will in
@@ -24,5 +18,5 @@ cd /usr/share/clickhouse-test/queries/0_stateless/test_minio
 FILES=$(ls .)
 for FILE in $FILES; do
     echo $FILE;
-    ./mc cp $FILE clickminio/test/$FILE;
+    /mc cp $FILE clickminio/test/$FILE;
 done
