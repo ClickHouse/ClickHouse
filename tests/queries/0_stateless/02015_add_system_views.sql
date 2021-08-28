addce5c1327b154839e1fd55faed67742c7d4fe2
zhongyuankai <zhongyuankai@didiglobal.com>
2021-08-28 12:33:35 +0800

    fix system views test

--- tests/queries/0_stateless/02015_add_system_views.sql
+++
@@ -1,8 +0,0 @@
-DROP TABLE IF EXISTS views_test;
-CREATE TABLE views_test (a UInt8, s String) ENGINE = MergeTree() ORDER BY a;
-DROP TABLE IF EXISTS views_test_view;
-CREATE MATERIALIZED VIEW views_test_view ENGINE = ReplacingMergeTree() ORDER BY a AS SELECT * FROM views_test;
-SELECT * FROM system.views WHERE name = 'views_test_view';
-DROP TABLE IF EXISTS views_test_view;
-SELECT * FROM system.views WHERE name = 'views_test_view';
-DROP TABLE IF EXISTS views_test;
