e189a2c3166d62ac1f0a118ad035d45090f6949a
Dmitry Novik <n0vik@yandex-team.ru>
2021-08-30 18:35:25 +0300

    Send profile events from all threads of current group

--- src/Server/TCPHandler.cpp
+++ src/Server/TCPHandler.cpp
@@ -666,6 +666,7 @@ void TCPHandler::processOrdinaryQueryWithProcessors()
                 /// Some time passed and there is a progress.
                 after_send_progress.restart();
                 sendProgress();
+                sendProfileEvents();
             }
 
             sendLogs();
@@ -691,6 +692,7 @@ void TCPHandler::processOrdinaryQueryWithProcessors()
             sendProfileInfo(executor.getProfileInfo());
             sendProgress();
             sendLogs();
+            sendProfileEvents();
         }
 
         if (state.is_connection_closed)
@@ -867,12 +869,12 @@ namespace
         MemoryTracker * memoryTracker,
         MutableColumns & columns,
         String const & host_name,
-        time_t current_time,
         UInt64 thread_id)
     {
         auto metric = memoryTracker->getMetric();
         if (metric == CurrentMetrics::end())
             return;
+        time_t current_time = time(nullptr);
 
         size_t i = 0;
         columns[i++]->insertData(host_name.data(), host_name.size());
@@ -890,13 +892,6 @@ namespace
 
 void TCPHandler::sendProfileEvents()
 {
-    auto thread_group = CurrentThread::getGroup();
-    auto const counters_snapshot = CurrentThread::getProfileEvents().getPartiallyAtomicSnapshot();
-    auto current_time = time(nullptr);
-    auto * memory_tracker = CurrentThread::getMemoryTracker();
-
-    auto const thread_id = CurrentThread::get().thread_id;
-
     auto profile_event_type = std::make_shared<DataTypeEnum8>(
         DataTypeEnum8::Values
         {
@@ -920,9 +915,17 @@ void TCPHandler::sendProfileEvents()
     Block block(std::move(temp_columns));
 
     MutableColumns columns = block.mutateColumns();
-    dumpProfileEvents(counters_snapshot, columns, server_display_name, current_time, thread_id);
-    dumpMemoryTracker(memory_tracker, columns, server_display_name, current_time, thread_id);
+    auto thread_group = CurrentThread::getGroup();
+    for (auto * thread : thread_group->threads)
+    {
+        auto const counters_snapshot = thread->performance_counters.getPartiallyAtomicSnapshot();
+        auto current_time = time(nullptr);
+        auto * memory_tracker = &thread->memory_tracker;
+        auto const thread_id = CurrentThread::get().thread_id;
 
+        dumpProfileEvents(counters_snapshot, columns, server_display_name, current_time, thread_id);
+        dumpMemoryTracker(memory_tracker, columns, server_display_name, thread_id);
+    }
     block.setColumns(std::move(columns));
 
     initProfileEventsBlockOutput(block);
