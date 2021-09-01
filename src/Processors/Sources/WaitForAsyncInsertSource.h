38911d73636ea8529c0e61cc8dc9b043db1b57f0
Anton Popov <pad11rus@gmail.com>
2021-09-02 02:18:09 +0300

    improvements of async inserts

--- src/Processors/Sources/WaitForAsyncInsertSource.h
+++ src/Processors/Sources/WaitForAsyncInsertSource.h
@@ -1,7 +1,7 @@
 #pragma once
 
 #include <Processors/ISource.h>
-#include <Interpreters/Context.h>
+#include <Interpreters/AsynchronousInsertQueue.h>
 
 namespace DB
 {
@@ -10,11 +10,11 @@ class WaitForAsyncInsertSource : public ISource, WithContext
 {
 public:
     WaitForAsyncInsertSource(
-        const String & query_id_, size_t timeout_ms_, ContextPtr context_)
+        const String & query_id_, size_t timeout_ms_, AsynchronousInsertQueue & queue_)
         : ISource(Block())
-        , WithContext(context_)
         , query_id(query_id_)
         , timeout_ms(timeout_ms_)
+        , queue(queue_)
     {
     }
 
@@ -23,16 +23,14 @@ public:
 protected:
     Chunk generate() override
     {
-        auto context = getContext();
-        auto * queue = context->getAsynchronousInsertQueue();
-        assert(queue);
-        queue->waitForProcessingQuery(query_id, std::chrono::milliseconds(timeout_ms));
+        queue.waitForProcessingQuery(query_id, std::chrono::milliseconds(timeout_ms));
         return Chunk();
     }
 
 private:
     String query_id;
     size_t timeout_ms;
+    AsynchronousInsertQueue & queue;
 };
 
 }
