e6748ee198e1c6c2aa1e3e515986ef730881545e
Yatsishin Ilya <2159081+qoega@users.noreply.github.com>
2021-09-02 15:43:15 +0300

    Improve work with iptables and services listing.

--- tests/integration/helpers/network.py
+++ tests/integration/helpers/network.py
@@ -212,6 +212,7 @@ class _NetworkManager:
             self._container = self._docker_client.containers.run('yandex/clickhouse-integration-helper',
                                                                  auto_remove=True,
                                                                  command=('sleep %s' % self.container_exit_timeout),
+                                                                 volumes={'/run/xtables.lock': {'bind': '/run/xtables.lock', 'mode': 'rw' }},
                                                                  detach=True, network_mode='host')
             container_id = self._container.id
             self._container_expire_time = time.time() + self.container_expire_timeout
