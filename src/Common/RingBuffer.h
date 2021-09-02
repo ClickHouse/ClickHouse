a6fe91ca47be99ab91dafc76de966619592dc0cf
Nikita Mikhaylov <mikhaylovnikitka@gmail.com>
2021-09-02 17:40:29 +0000

    get rid of half of allocations

---
+++ src/Common/RingBuffer.h
@@ -0,0 +1,122 @@
+#pragma once
+
+#include <vector>
+#include <iostream>
+
+namespace DB
+{
+
+/**
+ * A ring buffer of fixed size.
+ * With an ability to expand / narrow.
+ * When narrowing only first N elements remain.
+ */
+
+template <class T>
+class RingBuffer {
+public:
+    explicit RingBuffer(size_t capacity_) : capacity(capacity_)
+    {
+        buffer.assign(capacity, {});
+    }
+
+    size_t size() const
+    {
+        return count;
+    }
+
+    bool empty() const
+    {
+        return count == 0;
+    }
+
+    bool tryPush(T element)
+    {
+        if (count == capacity) {
+            return false;
+        }
+        buffer[(position + count) % capacity] = element;
+        ++count;
+        return true;
+    }
+
+    bool tryPop(T * element)
+    {
+        if (empty()) {
+            return false;
+        }
+        *element = std::move(buffer[position]);
+        --count;
+        position = (position + 1) % capacity;
+        return true;
+    }
+
+    template <typename Predicate>
+    void removeElements(Predicate && predicate)
+    {
+        /// Shift all elements to the beginning of the buffer
+        std::rotate(buffer.begin(), buffer.begin() + position, buffer.end());
+
+        /// Remove elements
+        auto end_removed = std::remove_if(buffer.begin(), buffer.begin() + count, predicate);
+
+        size_t new_count = std::distance(buffer.begin(), end_removed);
+
+        for (size_t i = new_count; i < count; ++i)
+            buffer[i] = T{};
+
+        count = new_count;
+        position = 0;
+    }
+
+
+    void resize(size_t new_capacity)
+    {
+        if (new_capacity > capacity)
+            expand(new_capacity);
+        else if (new_capacity < capacity)
+            narrow(new_capacity);
+    }
+
+private:
+
+    void expand(size_t new_capacity)
+    {
+        bool overflow = (position + count) > capacity;
+        buffer.resize(new_capacity);
+
+        if (overflow)
+        {
+            size_t count_before_end = capacity - position;
+            for (size_t i = 0; i < count_before_end; ++i)
+                buffer[new_capacity - i] = buffer[capacity - i];
+            position = new_capacity - count_before_end;
+        }
+
+        capacity = new_capacity;
+    }
+
+    void narrow(size_t new_capacity)
+    {
+        std::vector<T> new_buffer(new_capacity);
+
+        count = std::min(new_capacity, count);
+        for (size_t i = 0; i < count; ++i)
+            new_buffer[i] = buffer[(position + i) % capacity];
+
+        std::swap(buffer, new_buffer);
+
+        position = 0;
+        capacity = new_capacity;
+    }
+
+
+    std::vector<T> buffer;
+    size_t position{0};
+    size_t count{0};
+    size_t capacity{0};
+};
+
+
+
+}
