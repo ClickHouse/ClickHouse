3a497940a6fa5b395830aa8fa4716d171703632a
Maksim Kita <kitaetoya@gmail.com>
2021-09-02 14:53:20 +0300

    Added ExecutablePool storage

--- src/DataStreams/ShellCommandSource.h
+++ src/DataStreams/ShellCommandSource.h
@@ -3,8 +3,11 @@
 #include <memory>
 
 #include <common/logger_useful.h>
+#include <common/BorrowedObjectPool.h>
+
 #include <Common/ShellCommand.h>
 #include <Common/ThreadPool.h>
+
 #include <IO/ReadHelpers.h>
 #include <Formats/FormatFactory.h>
 #include <Processors/ISimpleTransform.h>
@@ -17,8 +20,9 @@
 namespace DB
 {
 
-/** A stream, that runs child process and sends data to its stdin in background thread,
-  * and receives data from its stdout.
+/** A stream, that get child process and sends data tasks.
+  * For each send data task background thread is created, send data tasks must send data to process input pipes.
+  * ShellCommandSource receives data from process stdout.
   */
 class ShellCommandSource final : public SourceWithProgress
 {
@@ -29,7 +33,7 @@ public:
         ContextPtr context,
         const std::string & format,
         const Block & sample_block,
-        std::unique_ptr<ShellCommand> command_,
+        std::unique_ptr<ShellCommand> && command_,
         Poco::Logger * log_,
         std::vector<SendDataTask> && send_data_tasks,
         size_t max_block_size = DEFAULT_BLOCK_SIZE)
@@ -48,7 +52,7 @@ public:
         ContextPtr context,
         const std::string & format,
         const Block & sample_block,
-        std::unique_ptr<ShellCommand> command_,
+        std::unique_ptr<ShellCommand> && command_,
         Poco::Logger * log_,
         size_t max_block_size = DEFAULT_BLOCK_SIZE)
         : SourceWithProgress(sample_block)
@@ -107,4 +111,173 @@ private:
     Poco::Logger * log;
 };
 
+/** A stream, that get child process and sends data tasks.
+  * For each send data task background thread is created, send data tasks must send data to process input pipes.
+  * ShellCommandPoolSource receives data from process stdout.
+  *
+  * Main difference with ShellCommandSource is that ShellCommandPoolSource initialized with process_pool and rows_to_read.
+  * Rows to read are necessary because processes in pool are not destroyed and work in read write loop.
+  * Source need to finish generating new chunks after rows_to_read rows are generated from process.
+  *
+  * If rows_to_read are not specified it is expected that script will output rows_to_read before other data.
+  *
+  * After source is destroyed process is returned to pool.
+  */
+
+using ProcessPool = BorrowedObjectPool<std::unique_ptr<ShellCommand>>;
+
+class ShellCommandPoolSource final : public SourceWithProgress
+{
+public:
+    using SendDataTask = std::function<void(void)>;
+
+    ShellCommandPoolSource(
+        ContextPtr context,
+        const std::string & format,
+        const Block & sample_block,
+        std::shared_ptr<ProcessPool> process_pool_,
+        std::unique_ptr<ShellCommand> && command_,
+        size_t rows_to_read_,
+        Poco::Logger * log_,
+        std::vector<SendDataTask> && send_data_tasks)
+        : SourceWithProgress(sample_block)
+        , process_pool(process_pool_)
+        , command(std::move(command_))
+        , rows_to_read(rows_to_read_)
+        , log(log_)
+    {
+        for (auto && send_data_task : send_data_tasks)
+        {
+            send_data_threads.emplace_back([task = std::move(send_data_task), this]()
+            {
+                try
+                {
+                    task();
+                }
+                catch (...)
+                {
+                    std::lock_guard<std::mutex> lock(send_data_lock);
+                    exception_during_send_data = std::current_exception();
+                }
+            });
+        }
+
+        pipeline.init(Pipe(FormatFactory::instance().getInput(format, command->out, sample_block, context, rows_to_read)));
+        executor = std::make_unique<PullingPipelineExecutor>(pipeline);
+    }
+
+    ShellCommandPoolSource(
+        ContextPtr context,
+        const std::string & format,
+        const Block & sample_block,
+        std::shared_ptr<ProcessPool> process_pool_,
+        std::unique_ptr<ShellCommand> && command_,
+        Poco::Logger * log_,
+        std::vector<SendDataTask> && send_data_tasks)
+        : SourceWithProgress(sample_block)
+        , process_pool(process_pool_)
+        , command(std::move(command_))
+        , log(log_)
+    {
+        for (auto && send_data_task : send_data_tasks)
+        {
+            send_data_threads.emplace_back([task = std::move(send_data_task), this]()
+            {
+                try
+                {
+                    task();
+                }
+                catch (...)
+                {
+                    std::lock_guard<std::mutex> lock(send_data_lock);
+                    exception_during_send_data = std::current_exception();
+                }
+            });
+        }
+
+        readText(rows_to_read, command->out);
+        pipeline.init(Pipe(FormatFactory::instance().getInput(format, command->out, sample_block, context, rows_to_read)));
+        executor = std::make_unique<PullingPipelineExecutor>(pipeline);
+    }
+
+
+    ~ShellCommandPoolSource() override
+    {
+        for (auto & thread : send_data_threads)
+            if (thread.joinable())
+                thread.join();
+
+        if (command)
+            process_pool->returnObject(std::move(command));
+    }
+
+protected:
+    Chunk generate() override
+    {
+        rethrowExceptionDuringReadIfNeeded();
+
+        if (current_read_rows == rows_to_read)
+            return {};
+
+        Chunk chunk;
+
+        try
+        {
+            if (!executor->pull(chunk))
+                return {};
+
+            current_read_rows += chunk.getNumRows();
+        }
+        catch (...)
+        {
+            tryLogCurrentException(log);
+            command = nullptr;
+            throw;
+        }
+
+        return chunk;
+    }
+
+public:
+    Status prepare() override
+    {
+        auto status = SourceWithProgress::prepare();
+
+        if (status == Status::Finished)
+        {
+            for (auto & thread : send_data_threads)
+                if (thread.joinable())
+                    thread.join();
+
+            rethrowExceptionDuringReadIfNeeded();
+        }
+
+        return status;
+    }
+
+    void rethrowExceptionDuringReadIfNeeded()
+    {
+        std::lock_guard<std::mutex> lock(send_data_lock);
+        if (exception_during_send_data)
+        {
+            command = nullptr;
+            std::rethrow_exception(exception_during_send_data);
+        }
+    }
+
+    String getName() const override { return "ShellCommandPoolSource"; }
+
+    std::shared_ptr<ProcessPool> process_pool;
+    std::unique_ptr<ShellCommand> command;
+    QueryPipeline pipeline;
+    std::unique_ptr<PullingPipelineExecutor> executor;
+    size_t rows_to_read = 0;
+    Poco::Logger * log;
+    std::vector<ThreadFromGlobalPool> send_data_threads;
+
+    size_t current_read_rows = 0;
+
+    std::mutex send_data_lock;
+    std::exception_ptr exception_during_send_data;
+};
 }
