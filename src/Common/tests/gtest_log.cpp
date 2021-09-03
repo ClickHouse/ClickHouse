497c2252037f01af170078d513276dbf96d8c459
alesapin <alesapin@gmail.com>
2021-09-03 13:07:40 +0300

    Test log level for CI

--- src/Common/tests/gtest_log.cpp
+++ src/Common/tests/gtest_log.cpp
@@ -6,6 +6,8 @@
 #include <Poco/Logger.h>
 #include <Poco/AutoPtr.h>
 #include <Poco/NullChannel.h>
+#include <Poco/StreamChannel.h>
+#include <sstream>
 
 
 TEST(Logger, Log)
@@ -16,4 +18,32 @@ TEST(Logger, Log)
 
     /// This test checks that we don't pass this string to fmtlib, because it is the only argument.
     EXPECT_NO_THROW(LOG_INFO(log, "Hello {} World"));
+}
+
+TEST(Logger, TestLog)
+{
+    {   /// Test logs visible for test level
+        Poco::Logger::root().setLevel("test");
+        std::ostringstream oss;
+        Poco::Logger::root().setChannel(Poco::AutoPtr<Poco::StreamChannel>(new Poco::StreamChannel(oss)));
+        Poco::Logger * log = &Poco::Logger::get("Log");
+        LOG_TEST(log, "Hello World");
+
+        EXPECT_EQ(oss.str(), "Hello World\n");
+    }
+
+    {   /// Test logs invisible for other levels
+        for (const auto & level : {"trace", "debug", "information", "warning", "fatal"})
+        {
+            Poco::Logger::root().setLevel(level);
+            std::ostringstream oss;
+            Poco::Logger::root().setChannel(Poco::AutoPtr<Poco::StreamChannel>(new Poco::StreamChannel(oss)));
+            Poco::Logger * log = &Poco::Logger::get("Log");
+
+            LOG_TEST(log, "Hello World");
+
+            EXPECT_EQ(oss.str(), "");
+        }
+    }
+
 }
