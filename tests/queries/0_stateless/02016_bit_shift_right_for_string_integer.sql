e73719ab806510d1f6e1573f0804b7b35bdb57a8
zhangxiao871 <zhangxiao871@jd.com>
2021-09-05 13:21:06 +0800

    Fix FixedString type ConstConst.

--- tests/queries/0_stateless/02016_bit_shift_right_for_string_integer.sql
+++ tests/queries/0_stateless/02016_bit_shift_right_for_string_integer.sql
@@ -130,3 +130,5 @@ SELECT bin(bitShiftRight('Hello', 42));  --A blank line
 SELECT 'FixedString ConstVector';
 SELECT id as shift_right_bit,toFixedString('Hello', 10) as arg,bin(bitShiftRight(toFixedString('Hello', 10), id)) as fixed_string_res FROM test_bit_shift_right_string_integer;
 SELECT id as shift_right_bit,toFixedString('Hel', 10) as arg,bin(bitShiftRight(toFixedString('Hel', 10), id)) as fixed_string_res FROM test_bit_shift_right_string_integer;
+
+DROP TABLE IF EXISTS test_bit_shift_right_string_integer;
