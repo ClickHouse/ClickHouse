8cb02a4b274abefc6f5ce43931031bbff805f036
Anton Popov <pad11rus@gmail.com>
2021-09-09 19:10:53 +0300

    add some comments

--- src/Processors/Executors/StreamingFormatExecutor.h
+++ src/Processors/Executors/StreamingFormatExecutor.h
@@ -8,9 +8,16 @@ namespace DB
 
 using SimpleTransformPtr = std::shared_ptr<ISimpleTransform>;
 
+/// Recieves format and allows to execute
+/// it multiple times for streaming processing of data.
 class StreamingFormatExecutor
 {
 public:
+    /// Callback is called, when got exception, while executing format.
+    /// It provides currently accumulated columns to make a roolback, for example,
+    /// and exception to rethrow or add context to it.
+    /// Should return number of rows, which are added to callback
+    /// to result columns in comparison to previous call of `execute`.
     using ErrorCallback = std::function<size_t(const MutableColumns &, Exception &)>;
 
     StreamingFormatExecutor(
@@ -19,7 +26,10 @@ public:
         ErrorCallback on_error_ = [](const MutableColumns &, Exception &) -> size_t { throw; },
         SimpleTransformPtr adding_defaults_transform_ = nullptr);
 
+    /// Returns numbers of newly read rows.
     size_t execute();
+
+    /// Releases currently accumulated columns.
     MutableColumns getResultColumns();
 
 private:
