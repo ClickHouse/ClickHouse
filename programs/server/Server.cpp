cd4010b1c90077a190979f7044362ddeda97b50d
tavplubix <avtokmakov@yandex-team.ru>
2021-09-09 12:37:51 +0300

    Merging #24866 (#28691)
    * Add StorageSystemISTables.cpp/.h
    
    * Another attempt
    
    * Columns and Views
    
    * Add information schema db and fix information schema 'tables' table
    
    * fix build
    
    * remove copy-paste, add views to system tables
    
    * add test
    
    * fix
    
    * fix_tests
    
    Co-authored-by: Damir Petrov <petrovdamir2235@gmail.com>
    Co-authored-by: Damir Petrov <0442a403@verstehen.sas.yp-c.yandex.net>
    Co-authored-by: mergify[bot] <37929162+mergify[bot]@users.noreply.github.com>

--- programs/server/Server.cpp
+++ programs/server/Server.cpp
@@ -56,6 +56,7 @@
 #include <Access/AccessControlManager.h>
 #include <Storages/StorageReplicatedMergeTree.h>
 #include <Storages/System/attachSystemTables.h>
+#include <Storages/System/attachInformationSchemaTables.h>
 #include <AggregateFunctions/registerAggregateFunctions.h>
 #include <Functions/registerFunctions.h>
 #include <TableFunctions/registerTableFunctions.h>
@@ -1131,6 +1132,8 @@ if (ThreadFuzzer::instance().isEffective())
         global_context->setSystemZooKeeperLogAfterInitializationIfNeeded();
         /// After the system database is created, attach virtual system tables (in addition to query_log and part_log)
         attachSystemTablesServer(*database_catalog.getSystemDatabase(), has_zookeeper);
+        attachInformationSchema(global_context, *database_catalog.getDatabase(DatabaseCatalog::INFORMATION_SCHEMA));
+        attachInformationSchema(global_context, *database_catalog.getDatabase(DatabaseCatalog::INFORMATION_SCHEMA_UPPERCASE));
         /// Firstly remove partially dropped databases, to avoid race with MaterializedMySQLSyncThread,
         /// that may execute DROP before loadMarkedAsDroppedTables() in background,
         /// and so loadMarkedAsDroppedTables() will find it and try to add, and UUID will overlap.
