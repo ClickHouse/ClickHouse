b515f125bbc18509e7b6957e1109668164e3b1c3
Nikolai Kochetov <nik-kochetov@yandex-team.ru>
2021-09-10 14:42:52 +0300

    Fix build and style.

---
+++ src/Processors/Executors/PushingAsyncPipelineExecutor.h
@@ -0,0 +1,62 @@
+#pragma once
+#include <memory>
+#include <atomic>
+
+namespace DB
+{
+
+class Block;
+class Chunk;
+class Chain;
+class PushingAsyncSource;
+
+class PipelineExecutor;
+using PipelineExecutorPtr = std::shared_ptr<PipelineExecutor>;
+
+class IProcessor;
+using ProcessorPtr = std::shared_ptr<IProcessor>;
+using Processors = std::vector<ProcessorPtr>;
+
+/// Pushing executor for Chain of processors using several background threads.
+/// Typical usage is:
+///
+/// PushingAsyncPipelineExecutor executor(chain);
+/// executor.start();
+/// while (auto chunk = ...)
+///     executor.push(std::move(chunk));
+/// executor.finish();
+class PushingAsyncPipelineExecutor
+{
+public:
+    explicit PushingAsyncPipelineExecutor(Chain & chain, size_t num_threads_);
+    ~PushingAsyncPipelineExecutor();
+
+    /// Get structure of returned block or chunk.
+    const Block & getHeader() const;
+
+    void start();
+
+    void push(Chunk chunk);
+    void push(Block block);
+
+    void finish();
+
+    /// Stop execution. It is not necessary, but helps to stop execution before executor is destroyed.
+    void cancel();
+
+    struct Data;
+
+private:
+    size_t num_threads;
+
+    Chain & chain;
+    std::shared_ptr<PushingAsyncSource> pushing_source;
+
+    std::unique_ptr<Processors> processors;
+    bool started = false;
+    bool finished = false;
+
+    std::unique_ptr<Data> data;
+};
+
+}
