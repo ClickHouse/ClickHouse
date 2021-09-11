e4e584a3de7ace0dd67cc55b84a25de92eecc204
Azat Khuzhin <a3at.mail@gmail.com>
2021-09-11 12:07:41 +0300

    Do not transform (k,v) IN ((K, V)) -> (k, v) IN (K, V) during parsing
    Before this patch the query `(k,v) IN ((1, 2))` was transformed `(k,v)
    IN (1,2)` by ClickHouse, and this produce the following error while
    sending such query to MySQL:
    
        DB::Exception: mysqlxx::BadQuery: Operand should contain 2 column(s)
    
    (There is a workaround though, add another value for IN section, i.e.
    `(k,v) IN ((1,2),(0,0))`, but this is icky)
    
    v0: (k,v) IN ((1,2)) -> (k,v) IN tuple((1,2))
        But these is not supported by transformQueryForExternalDatabase()
    
    v1: (k,v) IN ((1,2)) -> (k,v) IN ((1,2))
        But now you will get an error for the following query:
    
            SELECT ('foo', 'bar') IN (('foo', 'bar'))
    
        Now you will get:
    
            Logical error: 'Trying to get name of not a column: ExpressionList'.
    
        (from two places, first is DB::ActionsMatcher::visit, easy to fix by
         inherit ASTExpressionList from ASTWithAlias, but the second is
         evaluteConstantExpression(), which  requires additional support for
         these case while creating a set in DB::createBlockForSet())
    
    v2: (k,v) IN ((1,2)) -> (k,v) IN tuple((1,2))
        So instead I prefer these, and add support for this case in
        transformQueryForExternalDatabase() in the next patch.

--- src/Parsers/ExpressionElementParsers.cpp
+++ src/Parsers/ExpressionElementParsers.cpp
@@ -110,18 +110,26 @@ bool ParserParenthesisExpression::parseImpl(Pos & pos, ASTPtr & node, Expected &
         return false;
     }
 
+    /// special case for one-element tuple
     if (expr_list.children.size() == 1 && is_elem)
     {
-        node = expr_list.children.front();
+        auto * ast_literal = expr_list.children.front()->as<ASTLiteral>();
+        /// but only if it's argument is not tuple,
+        /// since otherwise it will do incorrect transformation:
+        ///
+        ///     (foo,bar) IN (('foo','bar')) -> (foo,bar) IN ('foo','bar')
+        if (!(ast_literal && ast_literal->value.getType() == Field::Types::Tuple))
+        {
+            node = expr_list.children.front();
+            return true;
+        }
     }
-    else
-    {
-        auto function_node = std::make_shared<ASTFunction>();
-        function_node->name = "tuple";
-        function_node->arguments = contents_node;
-        function_node->children.push_back(contents_node);
-        node = function_node;
-    }
+
+    auto function_node = std::make_shared<ASTFunction>();
+    function_node->name = "tuple";
+    function_node->arguments = contents_node;
+    function_node->children.push_back(contents_node);
+    node = function_node;
 
     return true;
 }
