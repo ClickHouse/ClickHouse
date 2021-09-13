f33b05d0d307432a5808dde1094d5a19eba33481
vdimir <vdimir@yandex-team.ru>
2021-09-13 12:34:34 +0300

    Not implemented for asof and auto join with multiple ORs

--- tests/queries/0_stateless/01429_join_on_error_messages.sql
+++ tests/queries/0_stateless/01429_join_on_error_messages.sql
@@ -8,3 +8,11 @@ SELECT 1 FROM (select 1 a) A JOIN (select 1 b) B ON a = b AND a > b; -- { server
 SELECT 1 FROM (select 1 a) A JOIN (select 1 b) B ON a = b AND a < b; -- { serverError 403 }
 SELECT 1 FROM (select 1 a) A JOIN (select 1 b) B ON a = b AND a >= b; -- { serverError 403 }
 SELECT 1 FROM (select 1 a) A JOIN (select 1 b) B ON a = b AND a <= b; -- { serverError 403 }
+
+SET join_algorithm = 'partial_merge';
+SELECT 1 FROM (select 1 a) A JOIN (select 1 b) B ON a = b OR a = b; -- { serverError 48 }
+
+SET join_algorithm = 'auto';
+SELECT 1 FROM (select 1 a) A JOIN (select 1 b) B ON a = b OR a = b; -- { serverError 48 }
+
+SET join_algorithm = 'hash';
