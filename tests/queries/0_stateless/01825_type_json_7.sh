d09f973b83a0dd35d4cb9e3f17be8dbc041179be
Anton Popov <pad11rus@gmail.com>
2021-09-13 17:02:38 +0300

    dynamic columns: fix several cases of parsing json

---
+++ tests/queries/0_stateless/01825_type_json_7.sh
@@ -0,0 +1,29 @@
+#!/usr/bin/env bash
+
+CURDIR=$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)
+# shellcheck source=../shell_config.sh
+. "$CURDIR"/../shell_config.sh
+
+$CLICKHOUSE_CLIENT -q "DROP TABLE IF EXISTS t_json_7;"
+
+$CLICKHOUSE_CLIENT -q "CREATE TABLE t_json_7 (data JSON) ENGINE = MergeTree ORDER BY tuple();"
+
+cat <<EOF | $CLICKHOUSE_CLIENT -q "INSERT INTO t_json_7 FORMAT JSONAsObject"
+{
+    "key": "v1",
+    "categories": null
+}
+{
+    "key": "v2",
+    "categories": ["foo", "bar"]
+}
+{
+    "key": "v3",
+    "categories": null
+}
+EOF
+
+$CLICKHOUSE_CLIENT -q "SELECT DISTINCT toTypeName(data) FROM t_json_7;"
+$CLICKHOUSE_CLIENT -q "SELECT data.key, data.categories FROM t_json_7 ORDER BY data.key"
+
+$CLICKHOUSE_CLIENT -q "DROP TABLE t_json_7;"
