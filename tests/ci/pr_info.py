deb6f53c684e2225e43037c5ecda177363bb91e7
alesapin <alesapin@gmail.com>
2021-09-15 16:45:44 +0300

    Trying better

--- tests/ci/pr_info.py
+++ tests/ci/pr_info.py
@@ -5,8 +5,13 @@ import json
 class PRInfo:
     def __init__(self, github_event):
         print(json.dumps(github_event, indent=4, sort_keys=True))
+
         self.number = github_event['number']
-        self.sha = github_event['after']
+        if 'after' in github_event:
+            self.sha = github_event['after']
+        else:
+            self.sha = os.getenv('GITHUB_SHA')
+
         self.labels = set([l['name'] for l in github_event['pull_request']['labels']])
         self.user_login = github_event['pull_request']['user']['login']
         user_orgs_response = requests.get(github_event['pull_request']['user']['organizations_url'])
