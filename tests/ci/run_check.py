b96cfbc6d23b9bb365d8e8e4bb2202374926817d
alesapin <alesapin@gmail.com>
2021-09-15 16:09:17 +0300

    FGix

--- tests/ci/run_check.py
+++ tests/ci/run_check.py
@@ -113,7 +113,7 @@ if __name__ == "__main__":
     can_run, description = should_run_checks_for_pr(pr_info)
     gh = Github(os.getenv("GITHUB_TOKEN"))
     if not can_run:
-        task_url = f"https://github.com/ClickHouse/ClickHouse/actions/runs/{run_id}"
+        task_url = f"https://github.com/ClickHouse/ClickHouse/actions/runs/{os.getenv('GITHUB_RUN_ID')}"
         commit = get_commit(gh, pr_info.sha)
         url = f"https://github.com/ClickHouse/ClickHouse/actions/runs/{os.getenv('GITHUB_RUN_ID')}"
         commit.create_status(context=NAME, description=description, state="failed", target_url=url)
