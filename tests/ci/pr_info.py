a90addf45c051a9a788f62dda84178267f5bc00d
alesapin <alesapin@gmail.com>
2021-09-15 17:15:02 +0300

    Check for orgs request

--- tests/ci/pr_info.py
+++ tests/ci/pr_info.py
@@ -4,9 +4,7 @@ import json
 import os
 
 class PRInfo:
-    def __init__(self, github_event):
-        print(json.dumps(github_event, indent=4, sort_keys=True))
-
+    def __init__(self, github_event, need_orgs=False):
         self.number = github_event['number']
         if 'after' in github_event:
             self.sha = github_event['after']
@@ -15,14 +13,12 @@ class PRInfo:
 
         self.labels = set([l['name'] for l in github_event['pull_request']['labels']])
         self.user_login = github_event['pull_request']['user']['login']
-        user_orgs_response = requests.get(github_event['pull_request']['user']['organizations_url'])
-        if user_orgs_response.ok:
-            response_json = user_orgs_response.json()
-            self.user_orgs = set(org['id'] for org in response_json)
-        else:
-            self.user_orgs = set([])
-
-        print(self.get_dict())
+        self.user_orgs = set([])
+        if need_orgs:
+            user_orgs_response = requests.get(github_event['pull_request']['user']['organizations_url'])
+            if user_orgs_response.ok:
+                response_json = user_orgs_response.json()
+                self.user_orgs = set(org['id'] for org in response_json)
 
     def get_dict(self):
         return {
