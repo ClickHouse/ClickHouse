2b67d54d5862aaa1ed60e16cba99a2cd230bdf8c
Pavel Kruglov <avogar@avogar-dev.sas.yp-c.yandex.net>
2021-09-16 14:25:45 +0300

    tmp

--- src/Processors/Formats/Impl/MsgPackRowInputFormat.cpp
+++ src/Processors/Formats/Impl/MsgPackRowInputFormat.cpp
@@ -7,15 +7,18 @@
 #include <IO/ReadHelpers.h>
 
 #include <DataTypes/DataTypeArray.h>
-#include <DataTypes/DataTypeDateTime.h>
 #include <DataTypes/DataTypeDateTime64.h>
 #include <DataTypes/DataTypeNullable.h>
+#include <DataTypes/DataTypeMap.h>
+#include <DataTypes/DataTypeLowCardinality.h>
 
 #include <Columns/ColumnArray.h>
 #include <Columns/ColumnFixedString.h>
 #include <Columns/ColumnNullable.h>
 #include <Columns/ColumnString.h>
 #include <Columns/ColumnsNumber.h>
+#include <Columns/ColumnMap.h>
+#include <Columns/ColumnLowCardinality.h>
 
 namespace DB
 {
@@ -49,6 +52,8 @@ void MsgPackVisitor::reset()
 {
     info_stack = {};
 }
+
+
 
 void MsgPackVisitor::insert_integer(UInt64 value) // NOLINT
 {
@@ -102,8 +107,15 @@ void MsgPackVisitor::insert_integer(UInt64 value) // NOLINT
             assert_cast<DataTypeDateTime64::ColumnType &>(info.column).insertValue(value);
             break;
         }
+        case TypeIndex::LowCardinality:
+        {
+            WhichDataType which(info.type);
+            if (!which.isUInt() && !which.isInt() && !which.is)
+            assert_cast<ColumnLowCardinality &>(info.column).insert(Field(value));
+            break;
+        }
         default:
-            throw Exception("Type " + info.type->getName() + " is not supported for MsgPack input format", ErrorCodes::ILLEGAL_COLUMN);
+            throw Exception(ErrorCodes::ILLEGAL_COLUMN, "Cannot insert MessagePack integer into column with type {}.", info_stack.top().type->getName());
     }
 }
 
@@ -121,24 +133,34 @@ bool MsgPackVisitor::visit_negative_integer(Int64 value) // NOLINT
 
 bool MsgPackVisitor::visit_str(const char* value, size_t size) // NOLINT
 {
+    if (!isStinfo_stack.top().type)
     info_stack.top().column.insertData(value, size);
     return true;
 }
 
 bool MsgPackVisitor::visit_float32(Float32 value) // NOLINT
 {
+    if (!WhichDataType(info_stack.top().type).isFloat32())
+        throw Exception(ErrorCodes::ILLEGAL_COLUMN, "Cannot insert MessagePack float32 into column with type {}.", info_stack.top().type->getName());
+
     assert_cast<ColumnFloat32 &>(info_stack.top().column).insertValue(value);
     return true;
 }
 
 bool MsgPackVisitor::visit_float64(Float64 value) // NOLINT
 {
+    if (!WhichDataType(info_stack.top().type).isFloat64())
+        throw Exception(ErrorCodes::ILLEGAL_COLUMN, "Cannot insert MessagePack float32 into column with type {}.", info_stack.top().type->getName());
+
     assert_cast<ColumnFloat64 &>(info_stack.top().column).insertValue(value);
     return true;
 }
 
 bool MsgPackVisitor::start_array(size_t size) // NOLINT
 {
+    if (!isArray(info_stack.top().type))
+        throw Exception(ErrorCodes::ILLEGAL_COLUMN, "Cannot insert MessagePack array into column with type {}.", info_stack.top().type->getName());
+
     auto nested_type = assert_cast<const DataTypeArray &>(*info_stack.top().type).getNestedType();
     ColumnArray & column_array = assert_cast<ColumnArray &>(info_stack.top().column);
     ColumnArray::Offsets & offsets = column_array.getOffsets();
@@ -152,6 +174,49 @@ bool MsgPackVisitor::end_array() // NOLINT
 {
     info_stack.pop();
     return true;
+}
+
+bool MsgPackVisitor::start_map(uint32_t size) // NOLINT
+{
+    if (!isMap(info_stack.top().type))
+        throw Exception(ErrorCodes::ILLEGAL_COLUMN, "Cannot insert MessagePack map into column with type {}.", info_stack.top().type->getName());
+    ColumnArray & column_array = assert_cast<ColumnMap &>(info_stack.top().column).getNestedColumn();
+    ColumnArray::Offsets & offsets = column_array.getOffsets();
+    offsets.push_back(offsets.back() + size);
+    return true;
+}
+
+bool MsgPackVisitor::start_map_key() // NOLINT
+{
+    auto key_column = assert_cast<ColumnMap &>(info_stack.top().column).getNestedData().getColumns()[0];
+    auto key_type = assert_cast<DataTypeMap &>(*info_stack.top().type).getKeyType();
+    info_stack.push(Info{*key_column, key_type});
+    return true;
+}
+
+bool MsgPackVisitor::end_map_key() // NOLINT
+{
+    info_stack.pop();
+    return true;
+}
+
+bool MsgPackVisitor::start_map_value() // NOLINT
+{
+    auto value_column = assert_cast<ColumnMap &>(info_stack.top().column).getNestedData().getColumns()[1];
+    auto value_type = assert_cast<DataTypeMap &>(*info_stack.top().type).getValueType();
+    info_stack.push(Info{*value_column, value_type});
+    return true;
+}
+
+bool MsgPackVisitor::end_map_value() // NOLINT
+{
+    info_stack.pop();
+    return true;
+}
+
+bool MsgPackVisitor::visit_nil()
+{
+
 }
 
 void MsgPackVisitor::parse_error(size_t, size_t) // NOLINT
