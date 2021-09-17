453201561d2c9b9fe2aa068bb5d3d60b02920695
robot-clickhouse <robot-clickhouse@yandex-team.ru>
2021-09-17 11:48:04 +0300

    Backport #28797 to 21.8: Rewrite StorageRabbitMQ a little

--- src/Storages/RabbitMQ/RabbitMQHandler.h
+++ src/Storages/RabbitMQ/RabbitMQHandler.h
@@ -17,6 +17,7 @@ namespace Loop
     static const UInt8 STOP = 2;
 }
 
+using ChannelPtr = std::unique_ptr<AMQP::TcpChannel>;
 
 class RabbitMQHandler : public AMQP::LibUvHandler
 {
@@ -40,8 +41,8 @@ public:
 
     void stopLoop();
 
-    bool connectionRunning() { return connection_running.load(); }
-    bool loopRunning() { return loop_running.load(); }
+    bool connectionRunning() const { return connection_running.load(); }
+    bool loopRunning() const { return loop_running.load(); }
 
     void updateLoopState(UInt8 state) { loop_state.store(state); }
     UInt8 getLoopState() { return loop_state.load(); }
@@ -54,5 +55,7 @@ private:
     std::atomic<UInt8> loop_state;
     std::mutex startup_mutex;
 };
+
+using RabbitMQHandlerPtr = std::shared_ptr<RabbitMQHandler>;
 
 }
