6440663f4f09fadc4a1fae5565a130c31c6b44c5
anneji <anneji@tencent.com>
2021-09-17 17:29:38 +0800

    add test cases

---
+++ tests/queries/0_stateless/02028_add_default_database_for_alterquery_on_cluster.sql
@@ -0,0 +1,17 @@
+-- Tags: distributed, no-parallely
+DROP DATABASE IF EXISTS 02028_db;
+CREATE DATABASE 02028_db ON CLUSTER test_shard_localhost;
+USE 02028_db;
+
+CREATE TABLE t1_local ON CLUSTER test_shard_localhost(partition_col_1 String, tc1 int,tc2 int)ENGINE=MergeTree() PARTITION BY partition_col_1 ORDER BY tc1;
+CREATE TABLE t2_local ON CLUSTER test_shard_localhost(partition_col_1 String, tc1 int,tc2 int)ENGINE=MergeTree() PARTITION BY partition_col_1 ORDER BY tc1;
+
+INSERT INTO t1_local VALUES('partition1', 1,1);
+INSERT INTO t1_local VALUES('partition2', 1,1);
+INSERT INTO t2_local VALUES('partition1', 3,3);
+INSERT INTO t2_local VALUES('partition2', 6,6);
+
+ALTER TABLE t1_local ON CLUSTER test_shard_localhost REPLACE PARTITION 'partition1' FROM  t2_local;
+ALTER TABLE t1_local ON CLUSTER test_shard_localhost MOVE PARTITION 'partition2' TO TABLE t2_local;
+
+DROP DATABASE 02028_db ON CLUSTER test_shard_localhost;
