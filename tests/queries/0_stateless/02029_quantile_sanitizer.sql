152942aedf702f8f9ea68765b23beebb1feb0fb8
vdimir <vdimir@yandex-team.ru>
2021-09-17 13:42:46 +0300

    Update 02029_quantile_sanitizer.sql

--- tests/queries/0_stateless/02029_quantile_sanitizer.sql
+++ tests/queries/0_stateless/02029_quantile_sanitizer.sql
@@ -1,2 +1,1 @@
-SELECT quantileTDigest(0.8)(toDateTime('2106-02-07 09:28:15')); -- { serverError DECIMAL_OVERFLOW }
 SELECT quantileTDigestWeighted(-0.)(toDateTime(10000000000.), 1); -- { serverError DECIMAL_OVERFLOW }
