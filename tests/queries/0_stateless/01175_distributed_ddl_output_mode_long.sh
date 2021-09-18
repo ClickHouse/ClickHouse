6bc6ef8f2e1a3c4c1ad8a4e8adfe9e647e47dc6c
Alexander Tokmakov <avtokmakov@yandex-team.ru>
2021-09-19 01:12:11 +0300

    fix test

--- tests/queries/0_stateless/01175_distributed_ddl_output_mode_long.sh
+++ tests/queries/0_stateless/01175_distributed_ddl_output_mode_long.sh
@@ -12,6 +12,9 @@ CURDIR=$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)
 TMP_OUT=$(mktemp "$CURDIR/01175_distributed_ddl_output_mode_long.XXXXXX")
 trap 'rm -f ${TMP_OUT:?}' EXIT
 
+TIMEOUT=300
+MIN_TIMEOUT=1
+
 # We execute a distributed DDL query with timeout 1 to check that one host is unavailable and will time out and other complete successfully.
 # But sometimes one second is not enough even for healthy host to succeed. Repeat the test in this case.
 function run_until_out_contains()
@@ -19,12 +22,12 @@ function run_until_out_contains()
     PATTERN=$1
     shift
 
-    for _ in {1..20}
+    for ((i=MIN_TIMEOUT; i<10; i++))
     do
-        "$@" > "$TMP_OUT" 2>&1
+        "$@" --distributed_ddl_task_timeout="$i" > "$TMP_OUT" 2>&1
         if grep -q "$PATTERN" "$TMP_OUT"
         then
-            cat "$TMP_OUT"
+            cat "$TMP_OUT" | sed "s/distributed_ddl_task_timeout (=$i)/distributed_ddl_task_timeout (=$MIN_TIMEOUT)/g"
             break
         fi
     done
@@ -33,17 +36,12 @@ function run_until_out_contains()
 RAND_COMMENT="01175_DDL_$RANDOM"
 LOG_COMMENT="${CLICKHOUSE_LOG_COMMENT}_$RAND_COMMENT"
 
-
 CLICKHOUSE_CLIENT_WITH_SETTINGS=${CLICKHOUSE_CLIENT/--log_comment=\'${CLICKHOUSE_LOG_COMMENT}\'/--log_comment=\'${LOG_COMMENT}\'}
 CLICKHOUSE_CLIENT_WITH_SETTINGS+=" --output_format_parallel_formatting=0 "
 CLICKHOUSE_CLIENT_WITH_SETTINGS+=" --distributed_ddl_entry_format_version=2 "
 
 CLIENT=${CLICKHOUSE_CLIENT_WITH_SETTINGS}
-CLIENT+=" --distributed_ddl_task_timeout=300 "
-
-CLIENT_TIMEOUT=${CLICKHOUSE_CLIENT_WITH_SETTINGS}
-CLIENT_TIMEOUT+=" --distributed_ddl_task_timeout=3 "
-
+CLIENT+=" --distributed_ddl_task_timeout=$TIMEOUT "
 
 $CLICKHOUSE_CLIENT -q "drop table if exists none;"
 $CLICKHOUSE_CLIENT -q "drop table if exists throw;"
@@ -57,28 +55,28 @@ $CLIENT --distributed_ddl_output_mode=none -q "create table none on cluster test
 $CLIENT --distributed_ddl_output_mode=none -q "create table none on cluster test_shard_localhost (n int) engine=Memory;" 2>&1 | sed "s/DB::Exception/Error/g" | sed "s/ (version.*)//"
 # Timeout
 
-run_until_out_contains 'There are 1 unfinished hosts' $CLIENT_TIMEOUT --distributed_ddl_output_mode=none -q "drop table if exists none on cluster test_unavailable_shard;" 2>&1 | sed "s/DB::Exception/Error/g" | sed "s/ (version.*)//" | sed "s/Watching task .* is executing longer/Watching task <task> is executing longer/"
+run_until_out_contains 'There are 1 unfinished hosts' $CLICKHOUSE_CLIENT_WITH_SETTINGS --distributed_ddl_output_mode=none -q "drop table if exists none on cluster test_unavailable_shard;" 2>&1 | sed "s/DB::Exception/Error/g" | sed "s/ (version.*)//" | sed "s/Watching task .* is executing longer/Watching task <task> is executing longer/"
 
 
 $CLIENT --distributed_ddl_output_mode=throw -q "select value from system.settings where name='distributed_ddl_output_mode';"
 $CLIENT --distributed_ddl_output_mode=throw -q "create table throw on cluster test_shard_localhost (n int) engine=Memory;"
 $CLIENT --distributed_ddl_output_mode=throw -q "create table throw on cluster test_shard_localhost (n int) engine=Memory format Null;" 2>&1 | sed "s/DB::Exception/Error/g" | sed "s/ (version.*)//"
 
-run_until_out_contains 'There are 1 unfinished hosts' $CLIENT_TIMEOUT --distributed_ddl_output_mode=throw -q "drop table if exists throw on cluster test_unavailable_shard;" 2>&1 | sed "s/DB::Exception/Error/g" | sed "s/ (version.*)//" | sed "s/Watching task .* is executing longer/Watching task <task> is executing longer/"
+run_until_out_contains 'There are 1 unfinished hosts' $CLICKHOUSE_CLIENT_WITH_SETTINGS --distributed_ddl_output_mode=throw -q "drop table if exists throw on cluster test_unavailable_shard;" 2>&1 | sed "s/DB::Exception/Error/g" | sed "s/ (version.*)//" | sed "s/Watching task .* is executing longer/Watching task <task> is executing longer/"
 
 
 $CLIENT --distributed_ddl_output_mode=null_status_on_timeout -q "select value from system.settings where name='distributed_ddl_output_mode';"
 $CLIENT --distributed_ddl_output_mode=null_status_on_timeout -q "create table null_status on cluster test_shard_localhost (n int) engine=Memory;"
 $CLIENT --distributed_ddl_output_mode=null_status_on_timeout -q "create table null_status on cluster test_shard_localhost (n int) engine=Memory format Null;" 2>&1 | sed "s/DB::Exception/Error/g" | sed "s/ (version.*)//"
 
-run_until_out_contains '9000	0		' $CLIENT_TIMEOUT --distributed_ddl_output_mode=null_status_on_timeout -q "drop table if exists null_status on cluster test_unavailable_shard;"
+run_until_out_contains '9000	0		' $CLICKHOUSE_CLIENT_WITH_SETTINGS --distributed_ddl_output_mode=null_status_on_timeout -q "drop table if exists null_status on cluster test_unavailable_shard;"
 
 
 $CLIENT --distributed_ddl_output_mode=never_throw -q "select value from system.settings where name='distributed_ddl_output_mode';"
 $CLIENT --distributed_ddl_output_mode=never_throw -q "create table never_throw on cluster test_shard_localhost (n int) engine=Memory;"
 $CLIENT --distributed_ddl_output_mode=never_throw -q "create table never_throw on cluster test_shard_localhost (n int) engine=Memory;" 2>&1 | sed "s/DB::Exception/Error/g" | sed "s/ (version.*)//"
 
-run_until_out_contains '9000	0		' $CLIENT_TIMEOUT --distributed_ddl_output_mode=never_throw -q "drop table if exists never_throw on cluster test_unavailable_shard;"
+run_until_out_contains '9000	0		' $CLICKHOUSE_CLIENT_WITH_SETTINGS --distributed_ddl_output_mode=never_throw -q "drop table if exists never_throw on cluster test_unavailable_shard;"
 
 
 $CLICKHOUSE_CLIENT -q "drop table if exists none;"
@@ -91,4 +89,5 @@ $CLICKHOUSE_CLIENT -q "select entry_version, initiator_host, initiator_port, clu
     host, port, status, exception_code, replace(replaceRegexpOne(exception_text, ' \(version.*', ''), 'Exception', 'Error'), abs(query_finish_time - query_create_time - query_duration_ms/1000) <= 1 , query_duration_ms < 600000
     from system.distributed_ddl_queue
     where arrayExists((key, val) -> key='log_comment' and val like '%$RAND_COMMENT%', mapKeys(settings), mapValues(settings))
+    and arrayExists((key, val) -> key='distributed_ddl_task_timeout' and val in ('$TIMEOUT', '$MIN_TIMEOUT'), mapKeys(settings), mapValues(settings))
     order by entry, host, port, exception_code"
