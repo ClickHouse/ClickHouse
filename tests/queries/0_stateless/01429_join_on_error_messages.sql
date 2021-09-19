cb87a244889968ad4a8ed7e088a7d2ed7a0c6861
Ilya Golshtein <gie@arenadata.io>
2021-09-20 00:41:11 +0300

    MAX_DISJUNCTS instead of MAX_ORS in ORs in JOIN

--- tests/queries/0_stateless/01429_join_on_error_messages.sql
+++ tests/queries/0_stateless/01429_join_on_error_messages.sql
@@ -17,7 +17,7 @@ SET join_algorithm = 'auto';
 SELECT 1 FROM (select 1 a) A JOIN (select 1 b, 1 c) B ON a = b OR a = c; -- { serverError 48 }
 -- works for a = b OR a = b because of equivalent disjunct optimization
 
-SELECT 1 FROM (select 1 a, 1 aa, 1 aaa, 1 aaaa) A JOIN (select 1 b, 1 bb, 1 bbb, 1 bbbb, 1 bbbbb) B ON a = b OR a = bb OR a = bbb OR a = bbbb OR aa = b OR aa = bb OR aa = bbb OR aa = bbbb OR aaa = b OR aaa = bb OR aaa = bbb OR aaa = bbbb OR aaaa = b OR aaaa = bb OR aaaa = bbb OR aaaa = bbbb OR a = bbbbb; -- { serverError 48 }
+SELECT 1 FROM (select 1 a, 1 aa, 1 aaa, 1 aaaa) A JOIN (select 1 b, 1 bb, 1 bbb, 1 bbbb, 1 bbbbb) B ON a = b OR a = bb OR a = bbb OR a = bbbb OR aa = b OR aa = bb OR aa = bbb OR aa = bbbb OR aaa = b OR aaa = bb OR aaa = bbb OR aaa = bbbb OR aaaa = b OR aaaa = bb OR aaaa = bbb OR aaaa = bbbb; -- { serverError 48 }
 
 
 SET join_algorithm = 'hash';
