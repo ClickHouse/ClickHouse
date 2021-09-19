7d24e110e45490893412a4adca8762e67c1eee28
Alexey Milovidov <milovidov@yandex-team.ru>
2021-09-19 20:23:06 +0300

    Make something with more sense

--- src/Common/ExponentiallySmoothedCounter.h
+++ src/Common/ExponentiallySmoothedCounter.h
@@ -14,89 +14,74 @@ namespace DB
   * It can be understood as an average over sliding window, but with different kernel.
   *
   * As an advantage, it is easy to update. Instead of collecting values and calculating a series of x1 / 2 + x2 / 4 + x3 / 8...
-  * just calculate x_old * weight + x_new * (1 - weight), where weight is an exponent of (negative) time passed.
+  * just calculate x_old / 2 + x_new / 2.
   *
   * It is often used for resource usage metrics. For example, "load average" in Linux is exponentially smoothed moving average.
   * We can use exponentially smoothed counters in query scheduler.
-  *
-  * It is possible to update the value with values in monotonic order of time.
-  * If it is updated with non-monotonic order, the calculation becomes non-deterministic.
   */
-
-template <typename Derived>
-struct ExponentiallySmoothedBase
+struct ExponentiallySmoothedAverage
 {
     double value = 0;
-    /// So the first update will have weight near 1. Cannot use -inf because subtraction will lead to nan.
-    double update_time = std::numeric_limits<double>::lowest();
+    double update_time = 0;
 
-    ExponentiallySmoothedBase()
+    ExponentiallySmoothedAverage()
     {
     }
 
-    ExponentiallySmoothedBase(double current_value, double current_time)
+    ExponentiallySmoothedAverage(double current_value, double current_time)
         : value(current_value), update_time(current_time)
     {
     }
 
-    double decay(double current_time, double prev_time, double half_decay_time) const
+    static double scale(double time_passed, double half_decay_time)
     {
-        return exp2((prev_time - current_time) / half_decay_time);
+        return exp2(-time_passed / half_decay_time);
+    }
+
+    static double sumWeights(double half_decay_time)
+    {
+        double k = scale(1.0, half_decay_time);
+        return 1 / (1 - k);
+    }
+
+    ExponentiallySmoothedAverage remap(double current_time, double half_decay_time) const
+    {
+        return ExponentiallySmoothedAverage(value * scale(current_time - update_time, half_decay_time), current_time);
+    }
+
+    static ExponentiallySmoothedAverage merge(const ExponentiallySmoothedAverage & a, const ExponentiallySmoothedAverage & b, double half_decay_time)
+    {
+        if (a.update_time > b.update_time)
+            return ExponentiallySmoothedAverage(a.value + b.remap(a.update_time, half_decay_time).value, a.update_time);
+        if (a.update_time < b.update_time)
+            return ExponentiallySmoothedAverage(b.value + a.remap(b.update_time, half_decay_time).value, b.update_time);
+
+        return ExponentiallySmoothedAverage(a.value + b.value, a.update_time);
+    }
+
+    void merge(const ExponentiallySmoothedAverage & other, double half_decay_time)
+    {
+        *this = merge(*this, other, half_decay_time);
+    }
+
+    void add(double new_value, double current_time, double half_decay_time)
+    {
+        merge(ExponentiallySmoothedAverage(new_value, current_time), half_decay_time);
+    }
+
+    double get(double half_decay_time) const
+    {
+        return value / sumWeights(half_decay_time);
     }
 
     double get(double current_time, double half_decay_time) const
     {
-        return value * decay(current_time, update_time, half_decay_time);
+        return remap(current_time, half_decay_time).get(half_decay_time);
     }
 
-    void merge(const ExponentiallySmoothedBase & other, double half_decay_time)
+    bool less(const ExponentiallySmoothedAverage & other, double half_decay_time) const
     {
-        static_cast<Derived *>(this)->add(other.value, other.update_time, half_decay_time);
-    }
-
-    bool less(const ExponentiallySmoothedBase & other, double half_decay_time) const
-    {
-        return get(other.update_time, half_decay_time) < other.value;
-    }
-};
-
-struct ExponentiallySmoothedAverage : ExponentiallySmoothedBase<ExponentiallySmoothedAverage>
-{
-    void add(double new_value, double current_time, double half_decay_time)
-    {
-        if (current_time > update_time)
-        {
-            /// Add newer value.
-            double old_value_weight = decay(current_time, update_time, half_decay_time);
-            value = value * old_value_weight + new_value * (1 - old_value_weight);
-            update_time = current_time;
-        }
-        else
-        {
-            /// Add older value.
-            double new_value_weight = decay(update_time, current_time, half_decay_time);
-            value = value * (1 - new_value_weight) + new_value * new_value_weight;
-        }
-    }
-};
-
-struct ExponentiallySmoothedSum : ExponentiallySmoothedBase<ExponentiallySmoothedSum>
-{
-    void add(double new_value, double current_time, double half_decay_time)
-    {
-        if (current_time > update_time)
-        {
-            /// Add newer value.
-            double old_value_weight = decay(current_time, update_time, half_decay_time);
-            value = value * old_value_weight + new_value;
-            update_time = current_time;
-        }
-        else
-        {
-            /// Add older value.
-            double new_value_weight = decay(update_time, current_time, half_decay_time);
-            value = value + new_value * new_value_weight;
-        }
+        return remap(other.update_time, half_decay_time).value < other.value;
     }
 };
 
