0eac948a119e6adf1dd007616488ba945751988f
Nikolai Kochetov <nik-kochetov@yandex-team.ru>
2021-09-21 19:37:32 +0300

    Add cancell callback to completed executor.

--- src/Processors/Executors/CompletedPipelineExecutor.cpp
+++ src/Processors/Executors/CompletedPipelineExecutor.cpp
@@ -1,6 +1,7 @@
 #include <Processors/Executors/CompletedPipelineExecutor.h>
 #include <Processors/Executors/PipelineExecutor.h>
 #include <Processors/QueryPipeline.h>
+#include <iostream>
 
 namespace DB
 {
@@ -16,10 +17,55 @@ CompletedPipelineExecutor::CompletedPipelineExecutor(QueryPipeline & pipeline_) 
         throw Exception(ErrorCodes::LOGICAL_ERROR, "Pipeline for CompletedPipelineExecutor must be completed");
 }
 
+void CompletedPipelineExecutor::setCancelCallback(std::function<bool()> is_cancelled, size_t interactive_timeout_ms_)
+{
+    is_cancelled_callback = is_cancelled;
+    interactive_timeout_ms = interactive_timeout_ms_;
+}
+
 void CompletedPipelineExecutor::execute()
 {
     PipelineExecutor executor(pipeline.processors, pipeline.process_list_element);
-    executor.execute(pipeline.getNumThreads());
+
+    if (interactive_timeout_ms)
+    {
+        bool is_done = false;
+        std::mutex mutex;
+        std::exception_ptr exception;
+        ThreadFromGlobalPool thread([&]()
+        {
+            try
+            {
+                executor.execute(pipeline.getNumThreads());
+                std::lock_guard lock(mutex);
+            }
+            catch (...)
+            {
+                exception = std::current_exception();
+            }
+            is_done = true;
+        });
+
+        {
+            std::condition_variable condvar;
+            std::unique_lock lock(mutex);
+            while (!is_done)
+            {
+                condvar.wait_for(lock, std::chrono::milliseconds(interactive_timeout_ms), [&]() { return is_done; });
+
+                if (is_cancelled_callback())
+                {
+                    executor.cancel();
+                    is_done = true;
+                }
+            }
+        }
+        thread.join();
+        if (exception)
+            std::rethrow_exception(exception);
+    }
+    else
+        executor.execute(pipeline.getNumThreads());
 }
 
 }
