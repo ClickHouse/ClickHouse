208dfc998afd68bb2d56978c2bb4f7d37fbebfa3
bharatnc <bharatnc@gmail.com>
2021-09-21 17:39:59 -0700

    H3 - fix logic to return icosahedron faces

--- tests/queries/0_stateless/2021_h3_get_faces.sql
+++ tests/queries/0_stateless/2021_h3_get_faces.sql
@@ -9,6 +9,6 @@ INSERT INTO h3_indexes VALUES (stringToH3('85283473fffffff'));
 INSERT INTO h3_indexes VALUES (stringToH3('0x8167bffffffffffL'));
 INSERT INTO h3_indexes VALUES (stringToH3('0x804dfffffffffffL'));
 
-SELECT h3GetFaces(h3_index) FROM h3_indexes ORDER BY h3_index;
+SELECT arraySort(h3GetFaces(h3_index)) FROM h3_indexes ORDER BY h3_index;
 
 DROP TABLE h3_indexes;
