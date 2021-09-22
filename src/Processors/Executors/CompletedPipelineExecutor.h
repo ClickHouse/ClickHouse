96d75a172398e2c1f25652bbc3a5609becf3af2c
Nikolai Kochetov <nik-kochetov@yandex-team.ru>
2021-09-22 13:57:00 +0300

    Refactor pushing to views.

--- src/Processors/Executors/CompletedPipelineExecutor.h
+++ src/Processors/Executors/CompletedPipelineExecutor.h
@@ -6,12 +6,19 @@ namespace DB
 
 class QueryPipeline;
 
+/// Executor for completed QueryPipeline.
+/// Allows to specify a callback which checks if execution should be cancelled.
+/// If callback is specified, runs execution in a separate thread.
 class CompletedPipelineExecutor
 {
 public:
     explicit CompletedPipelineExecutor(QueryPipeline & pipeline_);
     ~CompletedPipelineExecutor();
+
+    /// This callback will be called each interactive_timeout_ms (if it is not 0).
+    /// If returns true, query would be cancelled.
     void setCancelCallback(std::function<bool()> is_cancelled, size_t interactive_timeout_ms_);
+
     void execute();
     struct Data;
 
