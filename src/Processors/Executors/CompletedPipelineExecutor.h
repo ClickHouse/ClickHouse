d018cb7adfd516ea290b6b3436b027e5780032de
Nikolai Kochetov <nik-kochetov@yandex-team.ru>
2021-09-22 11:30:02 +0300

    Fry fix executor again.

--- src/Processors/Executors/CompletedPipelineExecutor.h
+++ src/Processors/Executors/CompletedPipelineExecutor.h
@@ -10,13 +10,16 @@ class CompletedPipelineExecutor
 {
 public:
     explicit CompletedPipelineExecutor(QueryPipeline & pipeline_);
+    ~CompletedPipelineExecutor();
     void setCancelCallback(std::function<bool()> is_cancelled, size_t interactive_timeout_ms_);
     void execute();
+    struct Data;
 
 private:
     QueryPipeline & pipeline;
     std::function<bool()> is_cancelled_callback;
     size_t interactive_timeout_ms = 0;
+    std::unique_ptr<Data> data;
 };
 
 }
