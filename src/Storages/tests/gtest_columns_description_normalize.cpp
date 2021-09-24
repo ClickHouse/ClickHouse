4f1a81b962cf9bc01a42e18e3a4e872ca8f14e43
Anton Popov <pad11rus@gmail.com>
2021-09-24 14:41:49 +0300

    better formatting

--- src/Storages/tests/gtest_columns_description_normalize.cpp
+++ src/Storages/tests/gtest_columns_description_normalize.cpp
@@ -7,19 +7,17 @@ using namespace DB;
 
 TEST(ColumnsDescription, Normalize)
 {
-    constexpr auto columns = R"(columns format version: 1
-3 columns:
-`a` UInt32
-`b` String	DEFAULT	If(a = 0, \'true\', \'false\')
-`c` String	DEFAULT	cAsT(a, 'String')
-)";
+    constexpr auto columns = "columns format version: 1\n"
+                             "3 columns:\n"
+                             "`a` UInt32\n"
+                             "`b` String\tDEFAULT\tIf(a = 0, 'true', 'false')\n"
+                             "`c` String\tDEFAULT\tcAsT(a, 'String')\n";
 
-    constexpr auto columns_normalized = R"(columns format version: 1
-3 columns:
-`a` UInt32
-`b` String	DEFAULT	if(a = 0, \'true\', \'false\')
-`c` String	DEFAULT	cast(a, 'String')
-)";
+    constexpr auto columns_normalized = "columns format version: 1\n"
+                                        "3 columns:\n"
+                                        "`a` UInt32\n"
+                                        "`b` String\tDEFAULT\tif(a = 0, 'true', 'false')\n"
+                                        "`c` String\tDEFAULT\tcast(a, 'String')\n";
 
     tryRegisterFunctions();
 
