03890eb8cf27a14ed5b41e5c4ed9a7793897014f
Alexey Milovidov <milovidov@yandex-team.ru>
2021-09-27 02:16:58 +0300

    Improve test

--- tests/queries/0_stateless/2024_merge_regexp_assert.sql
+++ tests/queries/0_stateless/2024_merge_regexp_assert.sql
@@ -1,1 +1,7 @@
-SELECT a FROM merge(REGEXP('.'), 'query_log'); -- { serverError 47 }
+DROP TABLE IF EXISTS t;
+CREATE TABLE t (b UInt8) ENGINE = Memory;
+SELECT a FROM merge(REGEXP('.'), '^t$'); -- { serverError 47 }
+SELECT a FROM merge(REGEXP('\0'), '^t$'); -- { serverError 47 }
+SELECT a FROM merge(REGEXP('\0a'), '^t$'); -- { serverError 47 }
+SELECT a FROM merge(REGEXP('\0a'), '^$'); -- { serverError 36 }
+DROP TABLE t;
