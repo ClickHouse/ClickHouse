13b8b56fcb7f29c10e13e2fa654e7719f6cb4c33
Amos Bird <amosbird@gmail.com>
2021-09-28 17:46:46 +0800

    disable fasttest

--- tests/queries/0_stateless/02013_json_function_null_column.sql
+++ tests/queries/0_stateless/02013_json_function_null_column.sql
@@ -1,3 +1,5 @@
+-- Tags: no-fasttest
+
 SELECT JSONExtract('{"string_value":null}', 'string_value', 'Nullable(String)') as x, toTypeName(x);
 SELECT JSONExtract('{"string_value":null}', 'string_value', 'String') as x, toTypeName(x);
 SELECT JSONExtract(toNullable('{"string_value":null}'), 'string_value', 'Nullable(String)') as x, toTypeName(x);
