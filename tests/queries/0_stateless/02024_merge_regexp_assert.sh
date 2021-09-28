21375fc67b6b040e19da6eed778012685f5083cd
Anton Popov <pad11rus@gmail.com>
2021-09-28 17:06:27 +0300

    fix test 02024_merge_regexp_assert

---
+++ tests/queries/0_stateless/02024_merge_regexp_assert.sh
@@ -0,0 +1,25 @@
+#!/usr/bin/env bash
+
+CURDIR=$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)
+# shellcheck source=../shell_config.sh
+. "$CURDIR"/../shell_config.sh
+
+function run_with_retry
+{
+    query=$1
+    error=$2
+
+    for _ in {1..200}; do
+        ${CLICKHOUSE_CLIENT} -q "$query" 2>&1 | grep -a -o -m1 "$error" && break
+    done
+}
+
+${CLICKHOUSE_CLIENT} -q "DROP TABLE IF EXISTS t;"
+${CLICKHOUSE_CLIENT} -q "CREATE TABLE t (b UInt8) ENGINE = Memory;"
+
+run_with_retry "SELECT a FROM merge(REGEXP('.'), '^t$');" "UNKNOWN_IDENTIFIER"
+run_with_retry "SELECT a FROM merge(REGEXP('\0'), '^t$');" "UNKNOWN_IDENTIFIER"
+run_with_retry "SELECT a FROM merge(REGEXP('\0a'), '^t$');" "UNKNOWN_IDENTIFIER"
+run_with_retry "SELECT a FROM merge(REGEXP('\0a'), '^$');" "BAD_ARGUMENTS"
+
+${CLICKHOUSE_CLIENT} -q "DROP TABLE t;"
