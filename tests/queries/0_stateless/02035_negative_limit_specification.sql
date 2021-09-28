c9ea05bf58eb3798595dcd41ef50fbeb0bddd9ea
WangZengrui <joeywang98@outlook.com>
2021-09-29 03:34:04 +0800

    fix test

--- tests/queries/0_stateless/02035_negative_limit_specification.sql
+++ tests/queries/0_stateless/02035_negative_limit_specification.sql
@@ -7,3 +7,4 @@ select * from node order by name limit -5;
 select * from node order by name limit -5 offset -2;
 select * from node order by name limit -5 with ties;
 select * from (select * from node order by name) order by name limit -5;
+
