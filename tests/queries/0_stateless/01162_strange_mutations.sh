6e386d2eeca8529d8b97a08d6b279a6ffcc0c09e
Alexander Tokmakov <avtokmakov@yandex-team.ru>
2021-09-29 13:37:44 +0300

    preserve table alias when adding default database

--- tests/queries/0_stateless/01162_strange_mutations.sh
+++ tests/queries/0_stateless/01162_strange_mutations.sh
@@ -4,7 +4,7 @@ CURDIR=$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)
 # shellcheck source=../shell_config.sh
 . "$CURDIR"/../shell_config.sh
 
-declare -a engines=("MergeTree" "ReplicatedMergeTree('/test/01162/$CLICKHOUSE_DATABASE', '1')")
+declare -a engines=("MergeTree" "ReplicatedMergeTree('/test/01162/$CLICKHOUSE_TEST_ZOOKEEPER_PREFIX', '1')")
 
 for engine in "${engines[@]}"
 do
@@ -13,7 +13,9 @@ do
     $CLICKHOUSE_CLIENT -q "insert into t values (1)"
     $CLICKHOUSE_CLIENT -q "insert into t values (2)"
     $CLICKHOUSE_CLIENT -q "select * from t order by n"
-    $CLICKHOUSE_CLIENT -q "alter table t delete where n global in (select * from (select * from t))"
+    $CLICKHOUSE_CLIENT --mutations_sync=1 -q "alter table t delete where n global in (select * from (select * from t where n global in (1::Int32)))"
+    $CLICKHOUSE_CLIENT -q "select * from t order by n"
+    $CLICKHOUSE_CLIENT --mutations_sync=1 -q "alter table t delete where n global in (select t1.n from t as t1 join t as t2 on t1.n=t2.n where t1.n global in (select 2::Int32))"
     $CLICKHOUSE_CLIENT -q "select count() from t"
     $CLICKHOUSE_CLIENT -q "drop table t"
 done
