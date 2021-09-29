847f14cb6a511c55eca37109adc9b6e056bff0f6
Kseniia Sumarokova <54203879+kssenii@users.noreply.github.com>
2021-09-29 16:18:24 +0300

    Update ODBCConnectionFactory.h

--- programs/odbc-bridge/ODBCConnectionFactory.h
+++ programs/odbc-bridge/ODBCConnectionFactory.h
@@ -84,9 +84,9 @@ T execute(nanodbc::ConnectionHolderPtr connection_holder, std::function<T(nanodb
         tryLogCurrentException(__PRETTY_FUNCTION__);
         /// SQLState, connection related errors start with 08 (main: 08S01), cursor invalid state is 24000.
         /// Invalid cursor state is a retriable error.
-        /// Invalid transaction state 25000. Truncate to 4 letters, because nanodbc does it.
+        /// Invalid transaction state 25000. Truncate to 2 letters on purpose.
         /// https://docs.microsoft.com/ru-ru/sql/odbc/reference/appendixes/appendix-a-odbc-error-codes?view=sql-server-ver15
-        if (e.state().starts_with("08") || e.state().starts_with("24") || e.state().starts_with("2500"))
+        if (e.state().starts_with("08") || e.state().starts_with("24") || e.state().starts_with("25"))
         {
             connection_holder->updateConnection();
             return query_func(connection_holder->get());
