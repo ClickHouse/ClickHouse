ee03ea75948fe09e4f4f7ffe9aa3731fd007540d
WangZengrui <joeywang98@outlook.com>
2021-09-29 09:27:36 +0800

    fix test

--- tests/queries/0_stateless/02035_negative_limit_specification.sql
+++ tests/queries/0_stateless/02035_negative_limit_specification.sql
@@ -1,5 +1,5 @@
-create table node (id UInt32, name String) engine = MergeTree() order by id
-insert into node values(0,'wang'),(1,'zhang'),(2,'liu'),(3, 'ding'),(4, 'sun'),(5,'wang'),(6,'zhang'),(7,'liu'),(8, 'ding'),(9, 'sun'),(10, 'sun');
+create table node (id UInt32, name String) engine = MergeTree() order by id;
+insert into node values (0,'wang'),(1,'zhang'),(2,'liu'),(3, 'ding'),(4, 'sun'),(5,'wang'),(6,'zhang'),(7,'liu'),(8, 'ding'),(9, 'sun'),(10, 'sun');
 
 select * from node limit -5;
 select * from node limit -5 offset -2;
@@ -7,4 +7,3 @@ select * from node order by name limit -5;
 select * from node order by name limit -5 offset -2;
 select * from node order by name limit -5 with ties;
 select * from (select * from node order by name) order by name limit -5;
-
