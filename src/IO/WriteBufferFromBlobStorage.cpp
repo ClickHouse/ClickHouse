9329573c273a0c9becdcb9f813d1442052b58441
Jakub Kuklis <jakub.kuklis@contentsquare.com>
2021-10-01 08:32:28 +0000

    Progressing with WriteBufferFromBlobStorage

--- src/IO/WriteBufferFromBlobStorage.cpp
+++ src/IO/WriteBufferFromBlobStorage.cpp
@@ -6,11 +6,22 @@
 
 #include <IO/WriteBufferFromBlobStorage.h>
 
+
 namespace DB
 {
 
+WriteBufferFromBlobStorage::WriteBufferFromBlobStorage(
+    Azure::Storage::Blobs::BlobContainerClient blob_container_client_,
+    const String & blob_path_,
+    size_t buf_size_) :
+    blob_container_client(blob_container_client_),
+    blob_path(blob_path_),
+    buf_size(buf_size_) {}
+
+
 void WriteBufferFromBlobStorage::nextImpl() {
-    std::cout << "WriteBufferFromBlobStorage:nextImpl\n\n\n";
+    std::cout << "buf_size: " << buf_size << "\n";
+    // std::cout << "WriteBufferFromBlobStorage:nextImpl\n\n\n";
 }
 
 }
--- src/IO/WriteBufferFromBlobStorage.h
+++ src/IO/WriteBufferFromBlobStorage.h
@@ -8,6 +8,8 @@
 
 #include <IO/BufferWithOwnMemory.h>
 #include <IO/WriteBuffer.h>
+#include <azure/storage/blobs.hpp>
+
 
 namespace DB
 {
@@ -15,9 +17,17 @@ namespace DB
 class WriteBufferFromBlobStorage : public BufferWithOwnMemory<WriteBuffer>
 {
 public:
-    explicit WriteBufferFromBlobStorage() {}
+    explicit WriteBufferFromBlobStorage(
+        Azure::Storage::Blobs::BlobContainerClient blob_container_client_,
+        const String & blob_path_,
+        size_t buf_size_);
 
     void nextImpl() override;
+
+private:
+    Azure::Storage::Blobs::BlobContainerClient blob_container_client;
+    const String blob_path;
+    size_t buf_size;
 };
 
 }
