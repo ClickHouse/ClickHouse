4b4985a545099b2dbe34944d06e5d40d04ce8232
Jakub Kuklis <jakub.kuklis@contentsquare.com>
2021-10-01 13:41:10 +0000

    Uploading to Blob Storage on INSERT seems to do something reasonable

--- src/IO/WriteBufferFromBlobStorage.cpp
+++ src/IO/WriteBufferFromBlobStorage.cpp
@@ -14,14 +14,29 @@ WriteBufferFromBlobStorage::WriteBufferFromBlobStorage(
     Azure::Storage::Blobs::BlobContainerClient blob_container_client_,
     const String & blob_path_,
     size_t buf_size_) :
+    BufferWithOwnMemory<WriteBuffer>(buf_size_, nullptr, 0),
     blob_container_client(blob_container_client_),
     blob_path(blob_path_),
-    buf_size(buf_size_) {}
+    buf_size(buf_size_)
+{
+    // allocateBuffer();
+}
 
+void WriteBufferFromBlobStorage::allocateBuffer()
+{
+
+}
 
 void WriteBufferFromBlobStorage::nextImpl() {
     std::cout << "buf_size: " << buf_size << "\n";
     // std::cout << "WriteBufferFromBlobStorage:nextImpl\n\n\n";
+
+    if (!offset())
+        return;
+
+    Azure::Core::IO::MemoryBodyStream tmp_buffer(reinterpret_cast<uint8_t *>(position()), offset());
+
+    blob_container_client.UploadBlob(blob_path, tmp_buffer);
 }
 
 }
