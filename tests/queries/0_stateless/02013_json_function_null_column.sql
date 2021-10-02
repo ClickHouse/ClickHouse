1265896b265afed9e16c15b9cad502360073aae2
Amos Bird <amosbird@gmail.com>
2021-10-02 12:20:56 +0800

    Fix nullable processing in JSONFunctions

--- tests/queries/0_stateless/02013_json_function_null_column.sql
+++ tests/queries/0_stateless/02013_json_function_null_column.sql
@@ -6,3 +6,4 @@ SELECT JSONExtract(toNullable('{"string_value":null}'), 'string_value', 'Nullabl
 SELECT JSONExtract(toNullable('{"string_value":null}'), 'string_value', 'String') as x, toTypeName(x);
 SELECT JSONExtract(NULL, 'string_value', 'Nullable(String)') as x, toTypeName(x);
 SELECT JSONExtract(NULL, 'string_value', 'String') as x, toTypeName(x);
+SELECT JSONExtractString('["a", "b", "c", "d", "e"]', idx) FROM (SELECT arrayJoin([2, NULL, 2147483646, 65535, 65535, 3]) AS idx);
