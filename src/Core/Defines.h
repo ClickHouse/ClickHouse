70e1f4d4027732aabb9a0462fca715831a3c0d95
Azat Khuzhin <a3at.mail@gmail.com>
2021-10-03 12:43:51 +0300

    Move ASAN_{UN,}POISON_MEMORY_REGION to base/defines

--- src/Core/Defines.h
+++ src/Core/Defines.h
@@ -114,11 +114,6 @@
 #define DBMS_DISTRIBUTED_SIGNATURE_HEADER 0xCAFEDACEull
 #define DBMS_DISTRIBUTED_SIGNATURE_HEADER_OLD_FORMAT 0xCAFECABEull
 
-#if !__has_include(<sanitizer/asan_interface.h>) || !defined(ADDRESS_SANITIZER)
-#   define ASAN_UNPOISON_MEMORY_REGION(a, b)
-#   define ASAN_POISON_MEMORY_REGION(a, b)
-#endif
-
 /// Actually, there may be multiple acquisitions of different locks for a given table within one query.
 /// Check with IStorage class for the list of possible locks
 #define DBMS_DEFAULT_LOCK_ACQUIRE_TIMEOUT_SEC 120
