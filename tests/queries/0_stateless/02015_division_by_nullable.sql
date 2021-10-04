bec6e66f97b0d5d5ac708b43f2a874b3dfee6ccf
avogar <pav.cruglov@yandex.ru>
2021-10-04 18:22:06 +0300

    Remove code duplication, fix bug, add more tests

--- tests/queries/0_stateless/02015_division_by_nullable.sql
+++ tests/queries/0_stateless/02015_division_by_nullable.sql
@@ -4,10 +4,22 @@ SELECT 1 / CAST(materialize(NULL), 'Nullable(Decimal(7, 2))');
 SELECT materialize(1) / CAST(materialize(NULL), 'Nullable(Decimal(7, 2))');
 
 
+SELECT 1 / CAST(1, 'Nullable(Decimal(7, 2))');
+SELECT materialize(1) / CAST(1, 'Nullable(Decimal(7, 2))');
+SELECT 1 / CAST(materialize(1), 'Nullable(Decimal(7, 2))');
+SELECT materialize(1) / CAST(materialize(1), 'Nullable(Decimal(7, 2))');
+
+
 SELECT intDiv(1, CAST(NULL, 'Nullable(Decimal(7, 2))'));
 SELECT intDiv(materialize(1), CAST(NULL, 'Nullable(Decimal(7, 2))'));
 SELECT intDiv(1, CAST(materialize(NULL), 'Nullable(Decimal(7, 2))'));
 SELECT intDiv(materialize(1), CAST(materialize(NULL), 'Nullable(Decimal(7, 2))'));
+
+
+SELECT intDiv(1, CAST(1, 'Nullable(Decimal(7, 2))'));
+SELECT intDiv(materialize(1), CAST(1, 'Nullable(Decimal(7, 2))'));
+SELECT intDiv(1, CAST(materialize(1), 'Nullable(Decimal(7, 2))'));
+SELECT intDiv(materialize(1), CAST(materialize(1), 'Nullable(Decimal(7, 2))'));
 
 
 SELECT toDecimal32(1, 2) / CAST(NULL, 'Nullable(UInt32)');
@@ -16,10 +28,22 @@ SELECT toDecimal32(1, 2) / CAST(materialize(NULL), 'Nullable(UInt32)');
 SELECT materialize(toDecimal32(1, 2)) / CAST(materialize(NULL), 'Nullable(UInt32)');
 
 
+SELECT toDecimal32(1, 2) / CAST(1, 'Nullable(UInt32)');
+SELECT materialize(toDecimal32(1, 2)) / CAST(1, 'Nullable(UInt32)');
+SELECT toDecimal32(1, 2) / CAST(materialize(1), 'Nullable(UInt32)');
+SELECT materialize(toDecimal32(1, 2)) / CAST(materialize(1), 'Nullable(UInt32)');
+
+
 SELECT intDiv(1, CAST(NULL, 'Nullable(UInt32)'));
 SELECT intDiv(materialize(1), CAST(NULL, 'Nullable(UInt32)'));
 SELECT intDiv(1, CAST(materialize(NULL), 'Nullable(UInt32)'));
 SELECT intDiv(materialize(1), CAST(materialize(NULL), 'Nullable(UInt32)'));
+
+
+SELECT intDiv(1, CAST(1, 'Nullable(UInt32)'));
+SELECT intDiv(materialize(1), CAST(1, 'Nullable(UInt32)'));
+SELECT intDiv(1, CAST(materialize(1), 'Nullable(UInt32)'));
+SELECT intDiv(materialize(1), CAST(materialize(1), 'Nullable(UInt32)'));
 
 
 SELECT 1 % CAST(NULL, 'Nullable(UInt32)');
@@ -28,10 +52,22 @@ SELECT 1 % CAST(materialize(NULL), 'Nullable(UInt32)');
 SELECT materialize(1) % CAST(materialize(NULL), 'Nullable(UInt32)');
 
 
+SELECT 1 % CAST(1, 'Nullable(UInt32)');
+SELECT materialize(1) % CAST(1, 'Nullable(UInt32)');
+SELECT 1 % CAST(materialize(1), 'Nullable(UInt32)');
+SELECT materialize(1) % CAST(materialize(1), 'Nullable(UInt32)');
+
+
 SELECT intDiv(1, CAST(NULL, 'Nullable(Float32)'));
 SELECT intDiv(materialize(1), CAST(NULL, 'Nullable(Float32)'));
 SELECT intDiv(1, CAST(materialize(NULL), 'Nullable(Float32)'));
 SELECT intDiv(materialize(1), CAST(materialize(NULL), 'Nullable(Float32)'));
+
+
+SELECT intDiv(1, CAST(1, 'Nullable(Float32)'));
+SELECT intDiv(materialize(1), CAST(1, 'Nullable(Float32)'));
+SELECT intDiv(1, CAST(materialize(1), 'Nullable(Float32)'));
+SELECT intDiv(materialize(1), CAST(materialize(1), 'Nullable(Float32)'));
 
 
 SELECT 1 % CAST(NULL, 'Nullable(Float32)');
@@ -40,8 +76,14 @@ SELECT 1 % CAST(materialize(NULL), 'Nullable(Float32)');
 SELECT materialize(1) % CAST(materialize(NULL), 'Nullable(Float32)');
 
 
+SELECT 1 % CAST(1, 'Nullable(Float32)');
+SELECT materialize(1) % CAST(1, 'Nullable(Float32)');
+SELECT 1 % CAST(materialize(1), 'Nullable(Float32)');
+SELECT materialize(1) % CAST(materialize(1), 'Nullable(Float32)');
+
+
 DROP TABLE IF EXISTS nullable_division;
-CREATE TABLE nullable_division (x UInt32, y UInt32, a Decimal(7, 2), b Decimal(7, 2)) ENGINE=MergeTree() order by x;
+CREATE TABLE nullable_division (x UInt32, y Nullable(UInt32), a Decimal(7, 2), b Nullable(Decimal(7, 2))) ENGINE=MergeTree() order by x;
 INSERT INTO nullable_division VALUES (1, 1, 1, 1), (1, NULL, 1, NULL), (1, 0, 1, 0);
 
 SELECT if(y = 0, 0, intDiv(x, y)) from nullable_division;
