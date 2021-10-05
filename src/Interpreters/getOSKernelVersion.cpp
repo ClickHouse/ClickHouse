4c96a9ee0709acc80b1ba8eaf1ef3287a4bc8eb8
WangZengrui <joeywang98@outlook.com>
2021-10-05 09:08:16 +0800

    add getOSKernelVersion

--- src/Interpreters/getOSKernelVersion.cpp
+++ src/Interpreters/getOSKernelVersion.cpp
@@ -7,28 +7,22 @@ namespace DB
 
 namespace ErrorCodes
 {
-    extern const int BAD_ARGUMENTS;
+    extern const int SYSTEM_ERROR;
 }
     
 String getOSKernelVersion()
 {
-    struct utsname os_kernel_version;
-    int buf = uname(&os_kernel_version);
+    struct utsname os_kernel_info;
+    int buf = uname(&os_kernel_info);
     if (buf < 0)
     {
         throw Exception(
-            "EFAULT buf is not valid.",
-            ErrorCodes::BAD_ARGUMENTS);
+            "EFAULT buffer is not valid.",
+            ErrorCodes::SYSTEM_ERROR);
     }
     else
     {
-        // std::cout <<"sysname: " << os_kernel_version.sysname << " nodename: " << os_kernel_version.nodename 
-        //           << " release: " << os_kernel_version.release << " version: " << os_kernel_version.version 
-        //           << " machine: " << os_kernel_version.machine << std::endl;
-
-        return "sysname: " + String(os_kernel_version.sysname) + " nodename: " + String(os_kernel_version.nodename) 
-                  + " release: " + String(os_kernel_version.release) + " version: " + String(os_kernel_version.version) 
-                  + " machine: " + String(os_kernel_version.machine);
+        return String(os_kernel_info.sysname) + " " + String(os_kernel_info.release);
     }
 }
 
