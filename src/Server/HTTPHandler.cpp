39f4f4ac781cf73fe20aa4ee2bd9c180faa7937d
Artur Filatenkov <613623@mail.ru>
2021-10-05 17:43:33 +0300

    refactor after move common to base

--- src/Server/HTTPHandler.cpp
+++ src/Server/HTTPHandler.cpp
@@ -24,15 +24,15 @@
 #include <Server/HTTPHandlerFactory.h>
 #include <Server/HTTPHandlerRequestFilter.h>
 #include <Server/IServer.h>
-#include <common/logger_useful.h>
+#include <base/logger_useful.h>
 #include <Common/SettingsChanges.h>
 #include <Common/StringUtils/StringUtils.h>
 #include <Common/escapeForFileName.h>
 #include <Common/setThreadName.h>
 #include <Common/typeid_cast.h>
 
-#include <common/getFQDNOrHostName.h>
-#include <common/scope_guard.h>
+#include <base/getFQDNOrHostName.h>
+#include <base/scope_guard.h>
 #include <Server/HTTP/HTTPResponse.h>
 
 #if !defined(ARCADIA_BUILD)
