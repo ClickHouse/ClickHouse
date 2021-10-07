76b3cba98cbfa53d2116c3aa8ff5ae96fa1c3a34
Nikolai Kochetov <nik-kochetov@yandex-team.ru>
2021-10-07 13:06:59 +0300

    Add logger.

--- src/Core/examples/coro.cpp
+++ src/Core/examples/coro.cpp
@@ -7,6 +7,10 @@
 #include <optional>
 
 #include <Common/Exception.h>
+#include <common/logger_useful.h>
+#include <Poco/ConsoleChannel.h>
+#include <Poco/Logger.h>
+#include <Poco/AutoPtr.h>
 
 template <typename T>
 using coroutine_handle = std::experimental::coroutine_handle<T>;
@@ -32,90 +36,97 @@ struct suspend_never_val
 
 template <typename T>
 struct resumable {
-  struct promise_type {
+    struct promise_type
+    {
+        using coro_handle = coroutine_handle<promise_type>;
+        auto get_return_object() { return coro_handle::from_promise(*this); }
+        auto initial_suspend() { return suspend_never(); }
+        auto final_suspend() noexcept { return suspend_never_val<T>{*r->value}; }
+        //void return_void() {}
+        void return_value(T value_) { r->value = value_; }
+        void unhandled_exception()
+        {
+            DB::tryLogCurrentException("Logger");
+            r->exception = std::current_exception();
+        }
+
+        explicit promise_type(std::string tag_) : tag(tag_) {}
+        ~promise_type() { std::cout << "~promise_type " << tag << std::endl; }
+        std::string tag;
+        coro_handle next;
+        resumable * r = nullptr;
+    };
+
     using coro_handle = coroutine_handle<promise_type>;
-    auto get_return_object() { return coro_handle::from_promise(*this); }
-    auto initial_suspend() { return suspend_never(); }
-    auto final_suspend() noexcept { return suspend_never_val<T>{*r->value}; }
-    //void return_void() {}
-    void return_value(T value_) { r->value = value_; }
-    void unhandled_exception() { r->exception = std::current_exception(); }
 
-    explicit promise_type(std::string tag_) : tag(tag_) {}
-    ~promise_type() { std::cout << "~promise_type " << tag << std::endl; }
-    std::string tag;
-    coro_handle next;
-    resumable * r = nullptr;
-  };
+    bool await_ready() const noexcept { return false; }
+    void await_suspend(coro_handle g) noexcept
+    {
+        std::cout << "  await_suspend " << my.promise().tag << std::endl;
+        std::cout << "  g tag " << g.promise().tag << std::endl;
+        g.promise().next = my;
+    }
+    T await_resume() noexcept
+    {
+        std::cout << "  await_res " << my.promise().tag << std::endl;
+        return *value;
+    }
 
-  using coro_handle = coroutine_handle<promise_type>;
-
-  bool await_ready() const noexcept { return false; }
-  void await_suspend(coro_handle g) noexcept
-  {
-      std::cout << "  await_suspend " << my.promise().tag << std::endl;
-      std::cout << "  g tag " << g.promise().tag << std::endl;
-      g.promise().next = my;
-  }
-  T await_resume() noexcept
-  {
-      std::cout << "  await_res " << my.promise().tag << std::endl;
-    return *value;
-  }
-
-  resumable(coro_handle handle) : my(handle), tag(handle.promise().tag)
-  {
-      assert(handle);
-      my.promise().r = this;
-      std::cout << "    resumable " << tag << std::endl;
-  }
-  resumable(resumable &) = delete;
-  resumable(resumable &&rhs) : my(rhs.my), tag(rhs.tag)
-  {
-      rhs.my = {};
-      std::cout << "    resumable&& " << tag << std::endl;
-  }
-  static bool resume_impl(resumable *r)
-  {
-      if (r->value)
+    resumable(coro_handle handle) : my(handle), tag(handle.promise().tag)
+    {
+        assert(handle);
+        my.promise().r = this;
+        std::cout << "    resumable " << tag << std::endl;
+    }
+    resumable(resumable &) = delete;
+    resumable(resumable &&rhs) : my(rhs.my), tag(rhs.tag)
+    {
+        rhs.my = {};
+        std::cout << "    resumable&& " << tag << std::endl;
+    }
+    static bool resume_impl(resumable *r)
+    {
+        if (r->value)
         return false;
 
-      auto & next = r->my.promise().next;
+        auto & next = r->my.promise().next;
 
-      if (next)
-      {
-          if (resume_impl(next.promise().r))
+        if (next)
+        {
+            if (resume_impl(next.promise().r))
             return true;
-          next = {};
-      }
+            next = {};
+        }
 
-      if (!r->value)
-      {
-        r->my.resume();
-        if (r->exception)
-            std::rethrow_exception(r->exception);
-      }
-      return !r->value;
-  }
+        if (!r->value)
+        {
+            r->my.resume();
+            if (r->exception)
+                std::rethrow_exception(r->exception);
+        }
+        return !r->value;
+    }
 
-  bool resume() {
-      return resume_impl(this);
-  }
+    bool resume()
+    {
+        return resume_impl(this);
+    }
 
-  T res()
-  {
-      return *value;
-  }
+    T res()
+    {
+        return *value;
+    }
 
-  ~resumable() {
-      std::cout << "    ~resumable " << tag << std::endl;
-  }
+    ~resumable()
+    {
+        std::cout << "    ~resumable " << tag << std::endl;
+    }
 
 private:
-  coro_handle my;
-  std::string tag;
-  std::optional<T> value;
-  std::exception_ptr exception;
+    coro_handle my;
+    std::string tag;
+    std::optional<T> value;
+    std::exception_ptr exception;
 };
 
 resumable<int> boo(std::string tag)
@@ -140,25 +151,33 @@ resumable<int> bar(std::string tag)
 }
 
 resumable<int> foo(std::string tag) {
-  std::cout << "Hello" << std::endl;
-  auto res1 = co_await bar("bar1");
-  std::cout << "Coro " << res1 << std::endl;
-  auto res2 = co_await bar("bar2");
-  std::cout << "World " << res2 << std::endl;
-  co_return res1 * res2; // 2 * 2 = 4
+    std::cout << "Hello" << std::endl;
+    auto res1 = co_await bar("bar1");
+    std::cout << "Coro " << res1 << std::endl;
+    auto res2 = co_await bar("bar2");
+    std::cout << "World " << res2 << std::endl;
+    co_return res1 * res2; // 2 * 2 = 4
 }
 
 int main()
-try
 {
-  auto t = foo("foo");
-  std::cout << ".. started" << std::endl;
-  while (t.resume())
-    std::cout << ".. yielded" << std::endl;
-  std::cout << ".. done: " << t.res() << std::endl;
-}
-catch (DB::Exception & e)
-{
-    std::cout << "Got exception " << e.what() << std::endl;
-    std::cout << e.getStackTraceString() << std::endl;
+    Poco::AutoPtr<Poco::ConsoleChannel> app_channel(new Poco::ConsoleChannel(std::cerr));
+    Poco::Logger::root().setChannel(app_channel);
+    Poco::Logger::root().setLevel("trace");
+
+    LOG_INFO(&Poco::Logger::get(""), "Starting");
+
+    try
+    {
+        auto t = foo("foo");
+        std::cout << ".. started" << std::endl;
+        while (t.resume())
+        std::cout << ".. yielded" << std::endl;
+        std::cout << ".. done: " << t.res() << std::endl;
+    }
+    catch (DB::Exception & e)
+    {
+        std::cout << "Got exception " << e.what() << std::endl;
+        std::cout << e.getStackTraceString() << std::endl;
+    }
 }
