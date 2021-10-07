a171dfd75ec3f27e630696b46ee30f77bca66eb6
Azat Khuzhin <a3at.mail@gmail.com>
2021-10-07 21:13:56 +0300

    Make 01092_memory_profiler more robust for dev env

--- tests/queries/0_stateless/01092_memory_profiler.sql
+++ tests/queries/0_stateless/01092_memory_profiler.sql
@@ -4,6 +4,7 @@ SET allow_introspection_functions = 1;
 
 SET memory_profiler_step = 1000000;
 SET memory_profiler_sample_probability = 1;
+SET log_queries = 1;
 
 SELECT ignore(groupArray(number), 'test memory profiler') FROM numbers(10000000);
 SYSTEM FLUSH LOGS;
