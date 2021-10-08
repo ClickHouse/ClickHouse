81b6a39068cdc55f8a59a37ce4ee80326d0ed424
Nikolai Kochetov <nik-kochetov@yandex-team.ru>
2021-10-08 21:33:51 +0300

    Fix some tests.

--- src/Formats/NativeFormat.cpp
+++ src/Formats/NativeFormat.cpp
@@ -3,6 +3,7 @@
 #include <Formats/FormatFactory.h>
 #include <Processors/Formats/IInputFormat.h>
 #include <Processors/Formats/IOutputFormat.h>
+#include <Processors/Transforms/AggregatingTransform.h>
 
 
 namespace DB
@@ -14,18 +15,21 @@ class NativeInputFormat final : public IInputFormat
 public:
     NativeInputFormat(ReadBuffer & buf, const Block & header)
         : IInputFormat(header, buf)
-        , reader(buf, header) {}
+        , reader(buf, header, 0) {}
 
     String getName() const override { return "Native"; }
 
     void resetParser() override
     {
+        IInputFormat::resetParser();
         reader.resetParser();
     }
 
     Chunk generate() override
     {
         auto block = reader.read();
+        if (!block)
+            return {};
 
         assertBlocksHaveEqualStructure(getPort().getHeader(), block, getName());
         block.checkNumberOfRows();
@@ -59,8 +63,16 @@ protected:
     {
         if (chunk)
         {
-
             auto block = getPort(PortKind::Main).getHeader();
+
+            // const auto & info = chunk.getChunkInfo();
+            // const auto * agg_info = typeid_cast<const AggregatedChunkInfo *>(info.get());
+            // if (agg_info)
+            // {
+            //     block.info.bucket_num = agg_info->bucket_num;
+            //     block.info.is_overflows = agg_info->is_overflows;
+            // }
+
             block.setColumns(chunk.detachColumns());
             writer.write(block);
         }
