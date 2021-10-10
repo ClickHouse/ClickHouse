49ef86e9a3037e288a1a2e0e3d61157853cd6d20
robot-clickhouse <robot-clickhouse@yandex-team.ru>
2021-10-11 02:39:13 +0300

    Backport #29951 to 21.10: Fix shutdown of AccessControlManager

--- programs/server/Server.cpp
+++ programs/server/Server.cpp
@@ -1051,6 +1051,10 @@ if (ThreadFuzzer::instance().isEffective())
         server.start();
 
     SCOPE_EXIT({
+        /// Stop reloading of the main config. This must be done before `global_context->shutdown()` because
+        /// otherwise the reloading may pass a changed config to some destroyed parts of ContextSharedPart.
+        main_config_reloader.reset();
+
         /** Ask to cancel background jobs all table engines,
           *  and also query_log.
           * It is important to do early, not in destructor of Context, because
@@ -1090,9 +1094,6 @@ if (ThreadFuzzer::instance().isEffective())
 
         /// Wait server pool to avoid use-after-free of destroyed context in the handlers
         server_pool.joinAll();
-
-        // Uses a raw pointer to global context for getting ZooKeeper.
-        main_config_reloader.reset();
 
         /** Explicitly destroy Context. It is more convenient than in destructor of Server, because logger is still available.
           * At this moment, no one could own shared part of Context.
