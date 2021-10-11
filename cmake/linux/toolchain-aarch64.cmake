87d535790a106e5bb2b4af4c19a73ab7a1662030
Alexey Milovidov <milovidov@yandex-team.ru>
2021-10-11 06:55:00 +0300

    Improve search for ar and ranlib

--- cmake/linux/toolchain-aarch64.cmake
+++ cmake/linux/toolchain-aarch64.cmake
@@ -10,8 +10,11 @@ set (TOOLCHAIN_PATH "${CMAKE_CURRENT_LIST_DIR}/../../contrib/sysroot/linux-aarch
 
 set (CMAKE_SYSROOT "${TOOLCHAIN_PATH}/aarch64-linux-gnu/libc")
 
-set (CMAKE_AR "llvm-ar" CACHE FILEPATH "" FORCE)
-set (CMAKE_RANLIB "llvm-ranlib" CACHE FILEPATH "" FORCE)
+find_program (LLVM_AR_PATH NAMES "llvm-ar" "llvm-ar-13" "llvm-ar-12" "llvm-ar-11" "llvm-ar-10" "llvm-ar-9" "llvm-ar-8")
+find_program (LLVM_RANLIB_PATH NAMES "llvm-ranlib" "llvm-ranlib-13" "llvm-ranlib-12" "llvm-ranlib-11" "llvm-ranlib-10" "llvm-ranlib-9"
+
+set (CMAKE_AR "${LLVM_AR_PATH}" CACHE FILEPATH "" FORCE)
+set (CMAKE_RANLIB "${LLVM_RANLIB_PATH}" CACHE FILEPATH "" FORCE)
 
 set (CMAKE_C_FLAGS_INIT "${CMAKE_C_FLAGS} --gcc-toolchain=${TOOLCHAIN_PATH}")
 set (CMAKE_CXX_FLAGS_INIT "${CMAKE_CXX_FLAGS} --gcc-toolchain=${TOOLCHAIN_PATH}")
