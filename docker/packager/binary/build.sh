3450218abe1782012dc338ec2ecdbabb7e635685
Alexey Milovidov <milovidov@yandex-team.ru>
2021-10-11 07:02:19 +0300

    Remove old toolchain

--- docker/packager/binary/build.sh
+++ docker/packager/binary/build.sh
@@ -6,9 +6,6 @@ mkdir -p build/cmake/toolchain/darwin-x86_64
 tar xJf MacOSX11.0.sdk.tar.xz -C build/cmake/toolchain/darwin-x86_64 --strip-components=1
 ln -sf darwin-x86_64 build/cmake/toolchain/darwin-aarch64
 
-mkdir -p build/cmake/toolchain/linux-aarch64
-tar xJf gcc-arm-8.3-2019.03-x86_64-aarch64-linux-gnu.tar.xz -C build/cmake/toolchain/linux-aarch64 --strip-components=1
-
 mkdir -p build/cmake/toolchain/freebsd-x86_64
 tar xJf freebsd-11.3-toolchain.tar.xz -C build/cmake/toolchain/freebsd-x86_64 --strip-components=1
 
