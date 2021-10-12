0c9e7c33dbad3deef505154c6b6b3489c5d1ec84
Nickita Taranov <nickita.taranov@gmail.com>
2021-10-13 00:23:40 +0300

    support nullable for ColumnConst

--- tests/queries/0_stateless/00255_array_concat_string.sql
+++ tests/queries/0_stateless/00255_array_concat_string.sql
@@ -8,3 +8,6 @@ SELECT arrayStringConcat(arrayMap(x -> toString(x), range(number)), '') FROM sys
 SELECT arrayStringConcat(arrayMap(x -> toString(x), range(number)), ',') FROM system.numbers LIMIT 10;
 SELECT arrayStringConcat(arrayMap(x -> transform(x, [0, 1, 2, 3, 4, 5, 6, 7, 8], ['yandex', 'google', 'test', '123', '', 'hello', 'world', 'goodbye', 'xyz'], ''), arrayMap(x -> x % 9, range(number))), ' ') FROM system.numbers LIMIT 20;
 SELECT arrayStringConcat(arrayMap(x -> toString(x), range(number % 4))) FROM system.numbers LIMIT 10;
+SELECT arrayStringConcat([Null, 'hello', Null, 'world', Null, 'xyz', 'def', Null], ';');
+SELECT arrayStringConcat([Null, Null], ';');
+SELECT arrayStringConcat([Null::Nullable(String), Null::Nullable(String)], ';');
