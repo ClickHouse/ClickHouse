8b26fea684c16024382e02f70e27bdda2a095a09
Vitaly Orlov <orloffv@yandex-team.ru>
2021-10-13 00:08:00 +0300

    Review fixes

--- tests/queries/0_stateless/00918_json_functions.sql
+++ tests/queries/0_stateless/00918_json_functions.sql
@@ -233,9 +233,9 @@ SELECT JSONExtractKeysAndValuesRaw('{"a": "hello", "b": [-100, 200.0, 300], "c":
 
 SELECT '--JSONExtractKeys--';
 SELECT JSONExtractKeys('{"a": "hello", "b": [-100, 200.0, 300]}');
-SELECT JSONExtractKeysAndValuesRaw('{"a": "hello", "b": [-100, 200.0, 300]}', 'b');
-SELECT JSONExtractKeysAndValuesRaw('{"a": "hello", "b": [-100, 200.0, 300]}', 'a');
-SELECT JSONExtractKeysAndValuesRaw('{"a": "hello", "b": [-100, 200.0, 300], "c":{"d":[121,144]}}', 'c');
+SELECT JSONExtractKeys('{"a": "hello", "b": [-100, 200.0, 300]}', 'b');
+SELECT JSONExtractKeys('{"a": "hello", "b": [-100, 200.0, 300]}', 'a');
+SELECT JSONExtractKeys('{"a": "hello", "b": [-100, 200.0, 300], "c":{"d":[121,144]}}', 'c');
 
 SELECT '--const/non-const mixed--';
 SELECT JSONExtractString('["a", "b", "c", "d", "e"]', idx) FROM (SELECT arrayJoin([1,2,3,4,5]) AS idx);
