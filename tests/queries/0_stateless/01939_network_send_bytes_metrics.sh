a8fa3914b530567ebb37cea9df636d38b8a18a99
Dmitry Novik <n0vik@yandex-team.ru>
2021-10-13 17:54:46 +0300

    Fix 01939_network_send_bytes_metrics

--- tests/queries/0_stateless/01939_network_send_bytes_metrics.sh
+++ tests/queries/0_stateless/01939_network_send_bytes_metrics.sh
@@ -10,7 +10,7 @@ ${CLICKHOUSE_CLIENT} --query "SELECT number FROM numbers(1000)" > /dev/null
 
 ${CLICKHOUSE_CLIENT} --multiquery --query "SYSTEM FLUSH LOGS;
     WITH ProfileEvents['NetworkSendBytes'] AS bytes
-    SELECT bytes >= 8000 AND bytes < 9000 ? 1 : bytes FROM system.query_log
+    SELECT bytes >= 8000 AND bytes < 9500 ? 1 : bytes FROM system.query_log
         WHERE current_database = currentDatabase() AND query_kind = 'Select' AND event_date >= yesterday() AND type = 2 ORDER BY event_time DESC LIMIT 1;"
 
 ${CLICKHOUSE_CLIENT} --query "DROP TABLE t"
