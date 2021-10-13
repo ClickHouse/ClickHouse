ae465b63064d12315b9df16053ca57a05b2f7ebe
alesapin <alesapin@gmail.com>
2021-10-13 18:12:04 +0300

    Fix test

--- tests/queries/1_stateful/00167_read_bytes_from_fs.sql
+++ tests/queries/1_stateful/00167_read_bytes_from_fs.sql
@@ -1,7 +1,7 @@
-SELECT sum(cityHash64(*)) FROM test.hits SETTINGS max_threads=40
+SELECT sum(cityHash64(*)) FROM test.hits SETTINGS max_threads=40;
 
 SYSTEM FLUSH LOGS;
 
--- We had a bug which lead to additional compressed data read. hits compressed size if about 1.2G, but we read more then 3GB.
+-- We had a bug which lead to additional compressed data read. test.hits compressed size is about 1.2Gb, but we read more then 3Gb.
 -- Small additional reads still possible, so we compare with about 1.5Gb.
-SELECT ProfileEvents['ReadBufferFromFileDescriptorReadBytes'] < 1500000000 from system.query_log where query = 'SELECT sum(cityHash64(*)) FROM datasets.hits_v1 SETTINGS max_threads=40' and type = 'QueryFinish'
+SELECT ProfileEvents['ReadBufferFromFileDescriptorReadBytes'] < 1500000000 from system.query_log where query = 'SELECT sum(cityHash64(*)) FROM datasets.hits_v1 SETTINGS max_threads=40' and databaser=currentDatabase() and type = 'QueryFinish';
