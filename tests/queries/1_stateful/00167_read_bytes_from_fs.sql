386d47cb227b580a63635e1b94a9d8a765d5cb97
alesapin <alesapin@gmail.com>
2021-10-14 10:49:12 +0300

    Fix typo

--- tests/queries/1_stateful/00167_read_bytes_from_fs.sql
+++ tests/queries/1_stateful/00167_read_bytes_from_fs.sql
@@ -4,4 +4,4 @@ SYSTEM FLUSH LOGS;
 
 -- We had a bug which lead to additional compressed data read. test.hits compressed size is about 1.2Gb, but we read more then 3Gb.
 -- Small additional reads still possible, so we compare with about 1.5Gb.
-SELECT ProfileEvents['ReadBufferFromFileDescriptorReadBytes'] < 1500000000 from system.query_log where query = 'SELECT sum(cityHash64(*)) FROM datasets.hits_v1 SETTINGS max_threads=40' and databaser=currentDatabase() and type = 'QueryFinish';
+SELECT ProfileEvents['ReadBufferFromFileDescriptorReadBytes'] < 1500000000 from system.query_log where query = 'SELECT sum(cityHash64(*)) FROM datasets.hits_v1 SETTINGS max_threads=40' and current_database = currentDatabase() and type = 'QueryFinish';
