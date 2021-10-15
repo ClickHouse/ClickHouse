ec188000b63aa050a9539b331719469e53749dce
Nikita Mikhaylov <mikhaylovnikitka@gmail.com>
2021-10-15 17:06:20 +0000

    Style + more uncomment

--- src/Parsers/fuzzers/codegen_fuzzer/codegen_select_fuzzer.cpp
+++ src/Parsers/fuzzers/codegen_fuzzer/codegen_select_fuzzer.cpp
@@ -14,13 +14,14 @@
 void GenerateSentence(const Sentence&, std::string &, int);
 
 
-DEFINE_BINARY_PROTO_FUZZER(const Sentence& main) {
+DEFINE_BINARY_PROTO_FUZZER(const Sentence& main)
+{
     static std::string input;
     input.reserve(4096);
 
     GenerateSentence(main, input, 0);
-    if (input.size()) {
-
+    if (input.size())
+    {
         std::cout << input << std::endl;
 
         DB::ParserQueryWithOutput parser(input.data() + input.size());
@@ -30,9 +31,8 @@ DEFINE_BINARY_PROTO_FUZZER(const Sentence& main) {
             DB::WriteBufferFromOStream out(std::cerr, 4096);
             DB::formatAST(*ast, out);
             std::cerr << std::endl;
-        } catch (...) {
-
         }
+        catch (...) {}
 
         input.clear();
     }
