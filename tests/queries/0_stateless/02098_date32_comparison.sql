06e75724e5ffa89c5f3a49c353e0ce4b952acbdb
lhuang09287750 <liang.huang@kyligence.io>
2021-10-15 08:09:37 +0000

    complete date32 comparison with date/datetime/string and give a test file

---
+++ tests/queries/0_stateless/02098_date32_comparison.sql
@@ -0,0 +1,6 @@
+select  toDate32('1990-01-01') = toDate('1990-01-01');
+select  toDate('1991-01-02') > toDate32('1990-01-01');
+select  toDate32('1990-01-01') = toDateTime('1990-01-01');
+select  toDateTime('1991-01-02') > toDate32('1990-01-01');
+select  toDate32('1990-01-01') = '1990-01-01';
+select  '1991-01-02' > toDate32('1990-01-01');
