9336f9c8a90106eb497fa98cd825719594ac4591
alexey-milovidov <milovidov@yandex-team.ru>
2021-10-16 03:57:26 +0300

    Update codegen_select_fuzzer.cpp

--- src/Parsers/fuzzers/codegen_fuzzer/codegen_select_fuzzer.cpp
+++ src/Parsers/fuzzers/codegen_fuzzer/codegen_select_fuzzer.cpp
@@ -25,7 +25,7 @@ DEFINE_BINARY_PROTO_FUZZER(const Sentence& main)
         std::cout << input << std::endl;
 
         DB::ParserQueryWithOutput parser(input.data() + input.size());
-        try 
+        try
         {
             DB::ASTPtr ast = parseQuery(parser, input.data(), input.data() + input.size(), "", 0, 0);
 
