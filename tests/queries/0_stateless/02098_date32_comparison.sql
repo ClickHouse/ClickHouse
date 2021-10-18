cb4a8c95808722f0531f02ee2ca7572de431af68
lhuang0928 <liang.huang@kyligence.io>
2021-10-18 08:53:25 +0000

    fix date32 comparison with datetime/datetime64

--- tests/queries/0_stateless/02098_date32_comparison.sql
+++ tests/queries/0_stateless/02098_date32_comparison.sql
@@ -2,5 +2,7 @@ select  toDate32('1990-01-01') = toDate('1990-01-01');
 select  toDate('1991-01-02') > toDate32('1990-01-01');
 select  toDate32('1990-01-01') = toDateTime('1990-01-01');
 select  toDateTime('1991-01-02') > toDate32('1990-01-01');
+select  toDate32('1990-01-01') = toDateTime64('1990-01-01',2);
+select  toDateTime64('1991-01-02',2) > toDate32('1990-01-01');
 select  toDate32('1990-01-01') = '1990-01-01';
 select  '1991-01-02' > toDate32('1990-01-01');
