38e97c33844e2c1742b0a08883cb48ed45eb4cc4
kssenii <sumarokovakseniia@mail.ru>
2021-10-20 10:49:05 +0300

    Better test

--- tests/queries/0_stateless/02006_test_positional_arguments.sql
+++ tests/queries/0_stateless/02006_test_positional_arguments.sql
@@ -22,25 +22,22 @@ select x1, x2, x3 from test order by 3 limit 1 by 3;
 select x1, x2, x3 from test order by x3 limit 1 by x1;
 select x1, x2, x3 from test order by 3 limit 1 by 1;
 
-select x3, x2, x1 from test order by x3 + x3;
-select x3, x2, x1 from test order by 1 + 1;
-
-select x3, x2, x1 from test order by (x3 + x3) * x1;
-select x3, x2, x1 from test order by (1 + 1) * 3;
+explain syntax select x3, x2, x1 from test order by 1 + 1;
+explain syntax select x3, x2, x1 from test order by (1 + 1) * 3;
 
 select x2, x1 from test group by x2 + x1; -- { serverError 215 }
 select x2, x1 from test group by 1 + 2; -- { serverError 215 }
 
-select x3, x2, x1 from test order by 1;
-select x3 + 1, x2, x1 from test order by 1;
-select x3, x3 - x2, x2, x1 from test order by 2;
-select x3, if(0, x3, plus(x1, x2)), x1 + x2 from test order by 2;
-select max(x1), x2 from test group by 2 order by 1, 2;
+explain syntax select x3, x2, x1 from test order by 1;
+explain syntax select x3 + 1, x2, x1 from test order by 1;
+explain syntax select x3, x3 - x2, x2, x1 from test order by 2;
+explain syntax select x3, if(x3 > 10, x3, plus(x1, x2)), x1 + x2 from test order by 2;
+explain syntax select max(x1), x2 from test group by 2 order by 1, 2;
+
 select max(x1), x2 from test group by 1, 2; -- { serverError 43 }
 select x1 + x2, x3 from test group by x1 + x2, x3;
 
 select x3, x2, x1 from test order by x3 * 2, x2, x1; -- check x3 * 2 does not become x3 * x2
-select x3, x2, x1 from test order by 1 + 1, 2, 1;
 
 explain syntax select x1, x3 from test group by 1 + 2, 1, 2;
 explain syntax select x1 + x3, x3 from test group by 1, 2;
