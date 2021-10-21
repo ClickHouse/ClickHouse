d1e7a0122e341e1616115dab33f77b02fbf58928
kssenii <sumarokovakseniia@mail.ru>
2021-10-21 14:23:10 +0000

    Fix test

--- tests/queries/0_stateless/02050_clickhouse_client_local_exception.sh
+++ tests/queries/0_stateless/02050_clickhouse_client_local_exception.sh
@@ -4,6 +4,5 @@ CURDIR=$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)
 # shellcheck source=../shell_config.sh
 . "$CURDIR"/../shell_config.sh
 
-$CLICKHOUSE_CLIENT --query="SELECT 1 INTO OUTFILE 'test.native.zst' FORMAT Native" 2>&1 | grep -q "Code: 76. DB::ErrnoException: Cannot open file test.native.zst, errno: 17, strerror: File exists." && echo 'OK' || echo 'FAIL' ||:
-$CLICKHOUSE_LOCAL --query="SELECT 1 INTO OUTFILE 'test.native.zst' FORMAT Native" 2>&1 | grep -q "Code: 76. DB::ErrnoException: Cannot open file test.native.zst, errno: 17, strerror: File exists." && echo 'OK' || echo 'FAIL' ||:
-
+touch test_exception
+$CLICKHOUSE_LOCAL --query="SELECT 1 INTO OUTFILE 'test_exception' FORMAT Native" 2>&1 | grep -q "Code: 76. DB::ErrnoException:" && echo 'OK' || echo 'FAIL' ||:
