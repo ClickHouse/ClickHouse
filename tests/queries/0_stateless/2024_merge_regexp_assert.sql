8bceb474b2ede919f7713ac1ab30a89a88d5f9c7
Kseniia Sumarokova <54203879+kssenii@users.noreply.github.com>
2021-10-21 10:18:01 +0300

    Remove flacky test

--- tests/queries/0_stateless/2024_merge_regexp_assert.sql
+++
@@ -1,7 +0,0 @@
-DROP TABLE IF EXISTS t;
-CREATE TABLE t (b UInt8) ENGINE = Memory;
-SELECT a FROM merge(REGEXP('.'), '^t$'); -- { serverError 47 }
-SELECT a FROM merge(REGEXP('\0'), '^t$'); -- { serverError 47 }
-SELECT a FROM merge(REGEXP('\0a'), '^t$'); -- { serverError 47 }
-SELECT a FROM merge(REGEXP('\0a'), '^$'); -- { serverError 36 }
-DROP TABLE t;
