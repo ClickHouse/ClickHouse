ae42b5792d16bc9272cf4beaf2a113deb8db8f25
feng lv <fenglv15@mails.ucas.ac.cn>
2021-10-22 02:36:24 +0000

    fix

--- cmake/find/filelog.cmake
+++ cmake/find/filelog.cmake
@@ -6,8 +6,10 @@ if (NOT ENABLE_FILELOG)
 endif()
 
 # StorageFileLog only support Linux platform
-if(${CMAKE_SYSTEM_NAME} MATCHES "Linux")
+if (OS_LINUX)
     set (USE_FILELOG 1)
     message (STATUS "Using StorageFileLog = 1")
-endif(${CMAKE_SYSTEM_NAME} MATCHES "Linux")
+else()
+    message(STATUS "StorageFileLog is only supported on Linux")
+endif ()
 
