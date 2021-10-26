8eb6c5fba927539997b953c0eda3ab4fbca5c30c
alesapin <alesapin@gmail.com>
2021-10-26 13:34:12 +0300

    Conditional run

--- tests/ci/run_check.py
+++ tests/ci/run_check.py
@@ -122,5 +122,9 @@ if __name__ == "__main__":
         commit.create_status(context=NAME, description=description, state="failure", target_url=url)
         sys.exit(1)
     else:
-        print("::notice ::Can run")
-        commit.create_status(context=NAME, description=description, state="pending", target_url=url)
+        if 'pr-documentation' in pr_info.labels or 'pr-doc-fix' in pr_info.labels:
+            commit.create_status(context=NAME, description="Skipping checks for documentation", state="success", target_url=url)
+            print("::notice ::Can run, but it's documentation PR, skipping")
+        else:
+            print("::notice ::Can run")
+            commit.create_status(context=NAME, description=description, state="pending", target_url=url)
