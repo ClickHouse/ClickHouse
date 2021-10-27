3c0bfaf8681f737ddefd1df038a0da882be6c99a
Maksim Kita <kitaetoya@gmail.com>
2021-10-27 18:49:18 +0300

    Updated UserDefinedSQLFunctionFactory

--- src/Interpreters/InterpreterDropFunctionQuery.cpp
+++ src/Interpreters/InterpreterDropFunctionQuery.cpp
@@ -12,35 +12,21 @@
 namespace DB
 {
 
-namespace ErrorCodes
-{
-    extern const int UNSUPPORTED_METHOD;
-}
-
 BlockIO InterpreterDropFunctionQuery::execute()
 {
     FunctionNameNormalizer().visit(query_ptr.get());
-    auto * drop_function_query = query_ptr->as<ASTDropFunctionQuery>();
-
-    if (!drop_function_query)
-        throw Exception(ErrorCodes::UNSUPPORTED_METHOD, "Expected DROP FUNCTION query");
+    ASTDropFunctionQuery & drop_function_query = query_ptr->as<ASTDropFunctionQuery &>();
 
     AccessRightsElements access_rights_elements;
     access_rights_elements.emplace_back(AccessType::DROP_FUNCTION);
 
-    if (!drop_function_query->cluster.empty())
+    if (!drop_function_query.cluster.empty())
         return executeDDLQueryOnCluster(query_ptr, getContext(), access_rights_elements);
 
     auto current_context = getContext();
     current_context->checkAccess(access_rights_elements);
 
-    auto & user_defined_functions_factory = UserDefinedSQLFunctionFactory::instance();
-
-    if (drop_function_query->if_exists && !user_defined_functions_factory.has(drop_function_query->function_name))
-        return {};
-
-    UserDefinedSQLFunctionFactory::instance().unregisterFunction(drop_function_query->function_name);
-    UserDefinedSQLObjectsLoader::instance().removeObject(current_context, UserDefinedSQLObjectType::Function, drop_function_query->function_name);
+    UserDefinedSQLFunctionFactory::instance().unregisterFunction(current_context, drop_function_query.function_name, drop_function_query.if_exists);
 
     return {};
 }
