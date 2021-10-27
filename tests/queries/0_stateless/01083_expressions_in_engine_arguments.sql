b96bd7ee4268e3eb15904da10f3bc329d77556d1
Kseniia Sumarokova <54203879+kssenii@users.noreply.github.com>
2021-10-27 12:29:43 +0300

    Update 01083_expressions_in_engine_arguments.sql

--- tests/queries/0_stateless/01083_expressions_in_engine_arguments.sql
+++ tests/queries/0_stateless/01083_expressions_in_engine_arguments.sql
@@ -1,5 +1,15 @@
 -- Tags: no-parallel, no-fasttest
 
+DROP TABLE IF EXISTS file;
+DROP TABLE IF EXISTS url;
+DROP TABLE IF EXISTS view;
+DROP TABLE IF EXISTS buffer;
+DROP TABLE IF EXISTS merge;
+DROP TABLE IF EXISTS merge_tf;
+DROP TABLE IF EXISTS distributed;
+DROP TABLE IF EXISTS distributed_tf;
+DROP DICTIONARY IF EXISTS dict;
+
 CREATE TABLE file (n Int8) ENGINE = File(upper('tsv') || 'WithNames' || 'AndTypes');
 CREATE TABLE buffer (n Int8) ENGINE = Buffer(currentDatabase(), file, 16, 10, 200, 10000, 1000000, 10000000, 1000000000);
 CREATE TABLE merge (n Int8) ENGINE = Merge('', lower('DISTRIBUTED'));
