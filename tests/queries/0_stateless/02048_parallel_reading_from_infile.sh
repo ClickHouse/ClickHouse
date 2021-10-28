72685d0c7848c568bd7a5d6fcd7b2da846f38b87
Artur <613623@mail.ru>
2021-10-28 18:46:51 +0000

    refactoring and test improvement

--- tests/queries/0_stateless/02048_parallel_reading_from_infile.sh
+++ tests/queries/0_stateless/02048_parallel_reading_from_infile.sh
@@ -11,20 +11,24 @@ set -e
 [ -e "${CLICKHOUSE_TMP}"/test_infile_parallel ] && rm "${CLICKHOUSE_TMP}"/test_infile_parallel_1
 [ -e "${CLICKHOUSE_TMP}"/test_infile_parallel ] && rm "${CLICKHOUSE_TMP}"/test_infile_parallel_2
 
-echo "Hello" > "${CLICKHOUSE_TMP}"/test_infile_parallel
-echo "Hello" > "${CLICKHOUSE_TMP}"/test_infile_parallel_1
-echo "Hello" > "${CLICKHOUSE_TMP}"/test_infile_parallel_2
+${CLICKHOUSE_CLIENT} --query "DROP TABLE IF EXISTS test_infile_parallel;"
+${CLICKHOUSE_CLIENT} --query "CREATE TABLE test_infile_parallel (Id Int32,Value Enum('first' = 1, 'second' = 2)) ENGINE=Memory();"
+${CLICKHOUSE_CLIENT} --query "SET input_format_tsv_enum_as_number = 1;"
+${CLICKHOUSE_CLIENT} --query "INSERT INTO test_infile_parallel VALUES (102, 2), (103, 1);"
+${CLICKHOUSE_CLIENT} --query "SELECT * FROM test_infile_parallel INTO OUTFILE 'test_infile_parallel' FORMAT TSV;"
+${CLICKHOUSE_CLIENT} --query "SELECT * FROM test_infile_parallel INTO OUTFILE 'test_infile_parallel_1' FORMAT TSV;"
+${CLICKHOUSE_CLIENT} --query "SELECT * FROM test_infile_parallel INTO OUTFILE 'test_infile_parallel_2' FORMAT TSV;"
 
 gzip "${CLICKHOUSE_TMP}"/test_infile_parallel
 
-${CLICKHOUSE_CLIENT} --query "DROP TABLE IF EXISTS test_infile_parallel;"
-${CLICKHOUSE_CLIENT} --query "CREATE TABLE test_infile_parallel (word String) ENGINE=Memory();"
-${CLICKHOUSE_CLIENT} --query "INSERT INTO test_infile_parallel FROM INFILE '${CLICKHOUSE_TMP}/test_infile_parallel*' FORMAT CSV;"
-${CLICKHOUSE_CLIENT} --query "SELECT count() FROM test_infile_parallel;"
+${CLICKHOUSE_CLIENT} --query "INSERT INTO test_infile_parallel FROM INFILE '${CLICKHOUSE_TMP}/test_infile_parallel*' FORMAT TSV;"
+${CLICKHOUSE_CLIENT} --query "SELECT count() FROM test_infile_parallel WHERE Value='first';"
+${CLICKHOUSE_CLIENT} --query "SELECT count() FROM test_infile_parallel WHERE Value='second';"
 
 ${CLICKHOUSE_LOCAL} --multiquery <<EOF
 DROP TABLE IF EXISTS test_infile_parallel; 
-CREATE TABLE test_infile_parallel (word String) ENGINE=Memory(); 
-INSERT INTO test_infile_parallel FROM INFILE '${CLICKHOUSE_TMP}/test_infile_parallel*' FORMAT CSV;
-SELECT count() FROM test_infile_parallel;
+CREATE TABLE test_infile_parallel (Id Int32,Value Enum('first' = 1, 'second' = 2)) ENGINE=Memory(); 
+INSERT INTO test_infile_parallel FROM INFILE '${CLICKHOUSE_TMP}/test_infile_parallel*' FORMAT TSV;
+SELECT count() FROM test_infile_parallel WHERE Value='first';
+SELECT count() FROM test_infile_parallel WHERE Value='second';
 EOF
