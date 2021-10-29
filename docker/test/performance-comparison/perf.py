dd0adc1ec2a4375a7a5be31523e98866ada9f06a
Nikolai Kochetov <KochetovNicolai@users.noreply.github.com>
2021-10-29 11:24:52 +0300

    Update perf.py

--- docker/test/performance-comparison/perf.py
+++ docker/test/performance-comparison/perf.py
@@ -284,7 +284,7 @@ for query_index in queries_to_run:
                 #   it makes the results unstable.
                 res = c.execute(q, query_id = prewarm_id,
                     settings = {'max_execution_time': args.max_query_seconds,
-                        'query_profiler_real_time_period_ns': 10000000})
+                        'query_profiler_real_time_period_ns': 10000001})
             except clickhouse_driver.errors.Error as e:
                 # Add query id to the exception to make debugging easier.
                 e.args = (prewarm_id, *e.args)
