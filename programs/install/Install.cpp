9b66d8999d90f638cacbd10c2c933d34d4f81479
Teja Srivastasa <tejasrivastav@gmail.com>
2021-10-29 02:00:57 +0200

    Update installation success message #30672

--- programs/install/Install.cpp
+++ programs/install/Install.cpp
@@ -809,13 +809,23 @@ int mainEntryClickHouseInstall(int argc, char ** argv)
         if (has_password_for_default_user)
             maybe_password = " --password";
 
-        fmt::print(
+        if (fs::exists(pid_file)) {
+            fmt::print(
+            "\nClickHouse has been successfully installed.\n"
+            "\nRestart clickhouse-server with:\n"
+            " sudo clickhouse restart\n"
+            "\nStart clickhouse-client with:\n"
+            " clickhouse-client{}\n\n",
+            maybe_password);
+        } else {
+            fmt::print(
             "\nClickHouse has been successfully installed.\n"
             "\nStart clickhouse-server with:\n"
             " sudo clickhouse start\n"
             "\nStart clickhouse-client with:\n"
             " clickhouse-client{}\n\n",
             maybe_password);
+        }
     }
     catch (const fs::filesystem_error &)
     {
