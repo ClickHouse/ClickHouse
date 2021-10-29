0e83437f20a3e2942fa67dcfe58be26dc146f4b3
Neng Liu <neng.liu@kyligence.io>
2021-10-29 10:35:10 +0800

    add a count aggregate

--- utils/local-engine/local_engine.cpp
+++ utils/local-engine/local_engine.cpp
@@ -1,4 +1,6 @@
+#include "Poco/Logger.h"
 #include <Processors/Executors/PipelineExecutor.h>
+#include <Processors/Executors/PullingPipelineExecutor.h>
 #include <Processors/Sinks/NullSink.h>
 #include <Processors/Sources/SourceFromSingleChunk.h>
 #include <Processors/Formats/Impl/CSVRowInputFormat.h>
@@ -12,8 +14,10 @@
 #include <AggregateFunctions/registerAggregateFunctions.h>
 #include <Functions/registerFunctions.h>
 #include <Functions/FunctionFactory.h>
+#include <AggregateFunctions/AggregateFunctionFactory.h>
 #include <Interpreters/ActionsDAG.h>
 #include <Processors/QueryPlan/FilterStep.h>
+#include <Processors/QueryPlan/AggregatingStep.h>
 #include <Processors/QueryPlan/ReadFromPreparedSource.h>
 #include <Processors/QueryPlan/QueryPlan.h>
 #include <Processors/QueryPipeline.h>
@@ -57,9 +61,7 @@ Block getTableHeader(std::map<std::string, std::string> & cols)
     return Block(*internalCols);
 }
 
-std::shared_ptr<CSVRowInputFormat> getSource(const std::string &file, Block &header) {
-//    std::unique_ptr<ReadBufferFromFile> buf = std::make_unique<ReadBufferFromFile>(file);
-    auto buf = ReadBufferFromFile(file);
+std::shared_ptr<CSVRowInputFormat> getSource(ReadBuffer & buf, Block &header) {
     FormatSettings settings;
     return std::make_shared<CSVRowInputFormat>(header, buf, RowInputFormatParams{.max_block_size=100}, false, settings);
 }
@@ -87,12 +89,17 @@ void registerAllFunctions()
     registerAggregateFunctions();
 }
 
-FunctionOverloadResolverPtr getFunction(const std::string & name)
+FunctionOverloadResolverPtr getFunction(const std::string & name, ContextPtr context)
 {
-    auto shared_context = Context::createShared();
-    auto global_context = Context::createGlobal(shared_context.get());
+
     auto & factory = FunctionFactory::instance();
-    return factory.get(name, global_context);
+    return factory.get(name, context);
+}
+
+AggregateFunctionPtr getAggregateFunction(const std::string & name, DataTypes arg_types) {
+    auto & factory = AggregateFunctionFactory::instance();
+    AggregateFunctionProperties properties;
+    return factory.get(name, arg_types, Array{}, properties);
 }
 
 ActionsDAG::NodeRawConstPtrs getArguments(ActionsDAG::NodeRawConstPtrs nodes, std::vector<std::string>& args) {
@@ -118,7 +125,7 @@ NamesAndTypesList blockToNameAndTypeList(Block & header)
     return types;
 }
 
-QueryPlanStepPtr buildFilter(Block & header)
+QueryPlanStepPtr buildFilter(Block & header, ContextPtr context)
 {
     auto actions_dag = std::make_shared<ActionsDAG>(std::move(blockToNameAndTypeList(header)));
 //    auto int_type = std::make_shared<DataTypeInt32>();
@@ -126,15 +133,67 @@ QueryPlanStepPtr buildFilter(Block & header)
 //    actions_dag->addOrReplaceInIndex(const_node);
     std::string empty_string;
     std::vector<std::string> args = {"x1", "x2"};
-    const auto & filter_node = actions_dag->addFunction(std::move(getFunction("less")), getArguments(actions_dag->getIndex(), args), std::move(empty_string));
+    const auto & filter_node = actions_dag->addFunction(std::move(getFunction("less", context)), getArguments(actions_dag->getIndex(), args), std::move(empty_string));
     actions_dag->getIndex().push_back(&filter_node);
     DataStream input_stream = DataStream{.header=header};
     auto filter = std::make_unique<FilterStep>(input_stream, actions_dag, std::move(filter_node.result_name), true);
     return std::move(filter);
 }
 
+void buildAgg(Block & header, QueryPlan& query_plan, ContextPtr context)
+{
+    auto aggregates = AggregateDescriptions();
+    auto count = AggregateDescription();
+    count.column_name = "count(x2)";
+    count.arguments = ColumnNumbers{1};
+    count.argument_names = Names{"x2"};
+    auto int_type = std::make_shared<DataTypeInt32>();
+    count.function = getAggregateFunction("count", {int_type});
+    aggregates.push_back(count);
+    Settings settings;
+    Aggregator::Params params(
+        header,
+        ColumnNumbers{0},
+        aggregates,
+        false,
+        settings.max_rows_to_group_by,
+        settings.group_by_overflow_mode,
+        settings.group_by_two_level_threshold,
+        settings.group_by_two_level_threshold_bytes,
+        settings.max_bytes_before_external_group_by,
+        settings.empty_result_for_aggregation_by_empty_set,
+        context->getTemporaryVolume(),
+        settings.max_threads,
+        settings.min_free_disk_space_for_temporary_data,
+        settings.compile_aggregate_expressions,
+        settings.min_count_to_compile_aggregate_expression);
+
+    SortDescription group_by_sort_description;
+
+    auto merge_threads = 1;
+    auto temporary_data_merge_threads = settings.aggregation_memory_efficient_merge_threads
+        ? static_cast<size_t>(settings.aggregation_memory_efficient_merge_threads)
+        : static_cast<size_t>(settings.max_threads);
+
+
+    auto aggregating_step = std::make_unique<AggregatingStep>(
+        query_plan.getCurrentDataStream(),
+        params,
+        true,
+        settings.max_block_size,
+        merge_threads,
+        temporary_data_merge_threads,
+        false,
+        nullptr,
+        std::move(group_by_sort_description));
+
+    query_plan.addStep(std::move(aggregating_step));
+}
+
 int main(int argc, char ** argv)
 {
+    auto shared_context = Context::createShared();
+    auto global_context = Context::createGlobal(shared_context.get());
     registerAllFunctions();
     auto & factory = FunctionFactory::instance();
     std::ifstream ifs("/Users/neng.liu/Documents/GitHub/ClickHouse/utils/local-engine/table.json");
@@ -144,36 +203,28 @@ int main(int argc, char ** argv)
     d.ParseStream(isw);
     auto cols = getColumns(d);
     auto header = getTableHeader(*cols);
-//    std::for_each(header.getNames().begin(), header.getNames().end(), [](const std::string & name) {
-//        std::cout << name << std::endl;
-//    });
 
     QueryPlan query_plan;
-//    auto source = getSource("/Users/neng.liu/Documents/GitHub/ClickHouse/utils/local-engine/table.csv", header);
-    auto x1 = ColumnInt32::create({1,2,3});
-    auto x2 = ColumnInt32::create({3,2,1});
-    Columns columns;
-    columns.emplace_back(std::move(x1));
-    columns.emplace_back(std::move(x2));
-    Chunk chunk(std::move(columns),3);
-    auto source = std::make_shared<SourceFromSingleChunk>(header, std::move(chunk));
-    std::shared_ptr<QueryPipelines> query_pipelines = std::make_shared<QueryPipelines>();
+    auto file = "/Users/neng.liu/Documents/GitHub/ClickHouse/utils/local-engine/table.csv";
+    auto buf = std::make_unique<ReadBufferFromFilePRead>(file);
 
+    auto source = getSource(*buf, header);
+
+    std::unique_ptr<QueryPipelines> query_pipelines = std::make_unique<QueryPipelines>();
     auto source_step = std::make_unique<ReadFromStorageStep>(Pipe(source), "CSV");
     query_plan.addStep(std::move(source_step));
 
-    auto filter = buildFilter(header);
+    auto filter = buildFilter(header, global_context);
     query_plan.addStep(std::move(filter));
+    buildAgg(header, query_plan, global_context);
     QueryPlanOptimizationSettings optimization_settings{.optimize_plan=false};
     auto query_pipline = query_plan.buildQueryPipeline(optimization_settings, BuildQueryPipelineSettings());
 
     auto buffer = WriteBufferFromFile("/Users/neng.liu/Documents/GitHub/ClickHouse/output.txt");
-    auto output = std::make_shared<CSVRowOutputFormat>(buffer, header, true, RowOutputFormatParams(), FormatSettings());
+    auto output = std::make_shared<CSVRowOutputFormat>(buffer, query_pipline->getHeader(), true, RowOutputFormatParams(), FormatSettings());
     query_pipline->setOutputFormat(output);
     auto executor = query_pipline->execute();
     executor->execute(1);
-//    auto chunk = source->generate();
-//    std::cout << chunk.getNumRows();
 }
 
 //    auto col = ColumnUInt8::create(1, 1);
