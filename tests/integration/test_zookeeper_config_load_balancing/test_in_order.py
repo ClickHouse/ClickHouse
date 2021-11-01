49e03025dde6513921787d0ab9b56df27fb557b7
zhangxiao871 <821008736@qq.com>
2021-11-01 18:26:50 +0800

    better test

--- tests/integration/test_zookeeper_config_load_balancing/test_in_order.py
+++ tests/integration/test_zookeeper_config_load_balancing/test_in_order.py
@@ -6,11 +6,11 @@ from helpers.cluster import ClickHouseCluster
 cluster = ClickHouseCluster(__file__, zookeeper_config_path='configs/zookeeper_config_in_order.xml')
 
 node1 = cluster.add_instance('node1', with_zookeeper=True,
-                                main_configs=["configs/remote_servers.xml", "configs/zookeeper_config_in_order.xml", "configs/zookeeper_log.xml"])
+                                main_configs=["configs/remote_servers.xml", "configs/zookeeper_config_in_order.xml"])
 node2 = cluster.add_instance('node2', with_zookeeper=True,
-                                main_configs=["configs/remote_servers.xml", "configs/zookeeper_config_in_order.xml", "configs/zookeeper_log.xml"])
+                                main_configs=["configs/remote_servers.xml", "configs/zookeeper_config_in_order.xml"])
 node3 = cluster.add_instance('node3', with_zookeeper=True,
-                                main_configs=["configs/remote_servers.xml", "configs/zookeeper_config_in_order.xml", "configs/zookeeper_log.xml"])
+                                main_configs=["configs/remote_servers.xml", "configs/zookeeper_config_in_order.xml"])
 
 
 @pytest.fixture(scope="module", autouse=True)
@@ -23,31 +23,8 @@ def started_cluster():
     finally:
         cluster.shutdown()
 
-def wait_zookeeper_node_to_start(started_cluster, zk_nodes, timeout=60):
-    start = time.time()
-    while time.time() - start < timeout:
-        try:
-            for instance in zk_nodes:
-                conn = started_cluster.get_kazoo_client(instance)
-                conn.get_children('/')
-            print("All instances of ZooKeeper started")
-            return
-        except Exception as ex:
-            print(("Can't connect to ZooKeeper " + str(ex)))
-            time.sleep(0.5)
-
 def test_in_order(started_cluster):
-    wait_zookeeper_node_to_start(started_cluster, ["zoo1", "zoo2", "zoo3"])
-    time.sleep(10)
-    zoo1_ip = started_cluster.get_instance_ip("zoo1")
-    for i, node in enumerate([node1, node3]):
-        node.query('DROP TABLE IF EXISTS simple SYNC')
-        node.query('''
-        CREATE TABLE simple (date Date, id UInt32)
-        ENGINE = ReplicatedMergeTree('/clickhouse/tables/0/simple', '{replica}', date, id, 8192);
-        '''.format(replica=node.name))
 
-    time.sleep(5)
-    assert '::ffff:' + str(zoo1_ip) + '\n' == node1.query('SELECT IPv6NumToString(address) FROM system.zookeeper_log ORDER BY event_time DESC LIMIT 1')
-    assert '::ffff:' + str(zoo1_ip) + '\n' == node2.query('SELECT IPv6NumToString(address) FROM system.zookeeper_log ORDER BY event_time DESC LIMIT 1')
-    assert '::ffff:' + str(zoo1_ip) + '\n' == node3.query('SELECT IPv6NumToString(address) FROM system.zookeeper_log ORDER BY event_time DESC LIMIT 1')
+    assert '1' == str(node1.exec_in_container(['bash', '-c', "lsof -a -i4 -i6 -itcp -w | grep 'roottestzookeeperconfigloadbalancing_zoo1_1.roottestzookeeperconfigloadbalancing_default:2181' | grep ESTABLISHED | wc -l"], privileged=True, user='root')).strip()
+    assert '1' == str(node2.exec_in_container(['bash', '-c', "lsof -a -i4 -i6 -itcp -w | grep 'roottestzookeeperconfigloadbalancing_zoo1_1.roottestzookeeperconfigloadbalancing_default:2181' | grep ESTABLISHED | wc -l"], privileged=True, user='root')).strip()
+    assert '1' == str(node3.exec_in_container(['bash', '-c', "lsof -a -i4 -i6 -itcp -w | grep 'roottestzookeeperconfigloadbalancing_zoo1_1.roottestzookeeperconfigloadbalancing_default:2181' | grep ESTABLISHED | wc -l"], privileged=True, user='root')).strip()
