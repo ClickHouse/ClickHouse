2f45ffe496db905ef0638339a183f4949f45f109
vdimir <vdimir@clickhouse.com>
2021-11-01 15:59:50 +0300

    Format 00725_join_on_bug_4.sql

--- tests/queries/0_stateless/00725_join_on_bug_4.sql
+++ tests/queries/0_stateless/00725_join_on_bug_4.sql
@@ -1,13 +1,13 @@
-drop table if exists t_00725_4;
-drop table if exists s_00725_4;
+DROP TABLE IF EXISTS t_00725_4;
+DROP TABLE IF EXISTS s_00725_4;
 
-create table t_00725_4(a Int64, b Int64, c String) engine = TinyLog;
-insert into t_00725_4 values(1,1,'a'),(2,2,'b');
-create table s_00725_4(a Int64, b Int64, c String) engine = TinyLog;
-insert into s_00725_4 values(1,1,'a');
+CREATE TABLE t_00725_4 (a Int64, b Int64, c String) ENGINE = TinyLog;
+INSERT INTO t_00725_4 VALUES (1, 1, 'a'), (2, 2, 'b');
 
+CREATE TABLE s_00725_4(a Int64, b Int64, c String) ENGINE = TinyLog;
+INSERT INTO s_00725_4 VALUES (1, 1, 'a');
 
-select t_00725_4.* from t_00725_4 all left join s_00725_4 on (s_00725_4.a = t_00725_4.a and s_00725_4.b = t_00725_4.b) where s_00725_4.a = 0 and s_00725_4.b = 0;
+SELECT t_00725_4.* FROM t_00725_4 ALL LEFT JOIN s_00725_4 ON (s_00725_4.a = t_00725_4.a AND s_00725_4.b = t_00725_4.b) WHERE s_00725_4.a = 0 AND s_00725_4.b = 0;
 
-drop table if exists t_00725_4;
-drop table if exists s_00725_4;
+DROP TABLE IF EXISTS t_00725_4;
+DROP TABLE IF EXISTS s_00725_4;
