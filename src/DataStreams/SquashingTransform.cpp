cad5bc7e6c1bd4969b4f50a326bea04ebbfd9fcf
filimonov <1549571+filimonov@users.noreply.github.com>
2021-11-02 11:22:00 +0100

    Update SquashingTransform.cpp

--- src/DataStreams/SquashingTransform.cpp
+++ src/DataStreams/SquashingTransform.cpp
@@ -115,10 +115,9 @@ bool SquashingTransform::isEnoughSize(const Block & block)
 
     for (const auto & [column, type, name] : block)
     {
-        if (!column)
-            throw Exception("Column " + name + " in block is nullptr, in method isEnoughSize."
-                            , ErrorCodes::SIZES_OF_COLUMNS_DOESNT_MATCH);
-        else if (!rows)
+        assert(column && "Invalid column in block");
+
+        if (!rows)
             rows = column->size();
         else if (rows != column->size())
             throw Exception("Sizes of columns doesn't match", ErrorCodes::SIZES_OF_COLUMNS_DOESNT_MATCH);
