21041090bc30dd84f4ba86162ad1e5298fd60139
Vasily Nemkov <V.Nemkov@gmail.com>
2021-11-03 22:52:32 +0200

    Added tests for CREATE [MATERIALIZED|LIVE] VIEW ... COMMENT ''

---
+++ tests/queries/0_stateless/02048_views_with_comment.sql
@@ -0,0 +1,10 @@
+# Make sure that any kind of `VIEW` can be created with a `COMMENT` clause
+# and value of that clause is visible as `comment` column of `system.tables` table.
+
+CREATE VIEW view_comment_test AS (SELECT 1) COMMENT 'simple view';
+CREATE MATERIALIZED VIEW materialized_view_comment_test TO test1 (a UInt64) AS (SELECT 1) COMMENT 'materialized view';
+
+SET allow_experimental_live_view=1;
+CREATE LIVE VIEW live_view_comment_test AS (SELECT 1) COMMENT 'live view';
+
+SELECT name, engine, comment FROM system.tables WHERE name LIKE '%view_comment_test' ORDER BY name;
