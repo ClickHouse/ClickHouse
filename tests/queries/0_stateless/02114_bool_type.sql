e2276f55d657f1af365f21b3a7958766ea2770ad
MaxWk <610379995@qq.com>
2021-11-04 16:46:09 +0800

    add tests

---
+++ tests/queries/0_stateless/02114_bool_type.sql
@@ -0,0 +1,16 @@
+DROP TABLE IF EXISTS test.bool_test;
+
+CREATE TABLE test.bool_test (value Bool) ENGINE = Memory;
+
+-- value column shoud have type 'Bool'
+SHOW CREATE TABLE test.bool_test;
+
+INSERT INTO test.bool_test (value) VALUES ('false'), ('true'), (0), (1);
+INSERT INTO test.bool_test (value) FORMAT JSONEachRow {"value":false}{"value":true}{"value":0}{"value":1}
+
+SELECT value FROM test.bool_test;
+SELECT value FROM test.bool_test FORMAT JSONEachRow;
+SELECT toUInt64(value) FROM test.bool_test;
+SELECT value FROM test.bool_test where value > 0;
+
+DROP TABLE IF EXISTS test.bool_test;
