a39b14ff0c71f9062e72f3022dcbceaebec766de
Artur <613623@mail.ru>
2021-11-05 11:07:31 +0000

    remove check for settings from test

--- tests/queries/0_stateless/02048_parallel_reading_from_infile.sh
+++ tests/queries/0_stateless/02048_parallel_reading_from_infile.sh
@@ -11,25 +11,15 @@ CURDIR=$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)
 
 
 echo -e "102\t2" > "${CLICKHOUSE_TMP}"/test_infile_parallel
-
 echo -e "102\tsecond" > "${CLICKHOUSE_TMP}"/test_infile_parallel_1
-
 echo -e "103\tfirst" > "${CLICKHOUSE_TMP}"/test_infile_parallel_2
 
 ${CLICKHOUSE_CLIENT} --query "DROP TABLE IF EXISTS test_infile_parallel;"
 ${CLICKHOUSE_CLIENT} --query "CREATE TABLE test_infile_parallel (Id Int32,Value Enum('first' = 1, 'second' = 2)) ENGINE=Memory();"
-${CLICKHOUSE_CLIENT} --query "SET input_format_tsv_enum_as_number = 0;"
 
 gzip "${CLICKHOUSE_TMP}"/test_infile_parallel
 
-# Check that settings are applied
 ${CLICKHOUSE_CLIENT} --query "INSERT INTO test_infile_parallel FROM INFILE '${CLICKHOUSE_TMP}/test_infile_parallel*' FORMAT TSV;"  
-${CLICKHOUSE_CLIENT} --query "SELECT count() FROM test_infile_parallel WHERE Value='first';"
-${CLICKHOUSE_CLIENT} --query "SELECT count() FROM test_infile_parallel WHERE Value='second';"
-
-${CLICKHOUSE_CLIENT} --query "SET input_format_tsv_enum_as_number = 1;"
-
-${CLICKHOUSE_CLIENT} --query "INSERT INTO test_infile_parallel FROM INFILE '${CLICKHOUSE_TMP}/test_infile_parallel*' FORMAT TSV;"
 ${CLICKHOUSE_CLIENT} --query "SELECT count() FROM test_infile_parallel WHERE Value='first';"
 ${CLICKHOUSE_CLIENT} --query "SELECT count() FROM test_infile_parallel WHERE Value='second';"
 
