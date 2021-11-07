610f3ed3e56e643e9f3a9da123b71c27674c5b0b
taiyang-li <654010905@qq.com>
2021-11-07 20:20:35 +0800

    add map function mapFromArrays

--- tests/queries/0_stateless/01651_map_functions.sql
+++ tests/queries/0_stateless/01651_map_functions.sql
@@ -2,14 +2,15 @@ set allow_experimental_map_type = 1;
 
 -- String type
 drop table if exists table_map;
-create table table_map (a Map(String, String), b String) engine = Memory;
-insert into table_map values ({'name':'zhangsan', 'age':'10'}, 'name'), ({'name':'lisi', 'gender':'female'},'age');
+create table table_map (a Map(String, String), b String, c Array(String), d Array(String)) engine = Memory;
+insert into table_map values ({'name':'zhangsan', 'age':'10'}, 'name', ['name', 'age'], ['zhangsan', '10']), ({'name':'lisi', 'gender':'female'},'age',['name', 'gender'], ['lisi', 'female']);
 select mapContains(a, 'name') from table_map;
 select mapContains(a, 'gender') from table_map;
 select mapContains(a, 'abc') from table_map;
 select mapContains(a, b) from table_map;
 select mapContains(a, 10) from table_map; -- { serverError 386 }
 select mapKeys(a) from table_map;
+select mapFromArrays(c, d) from table_map;
 drop table if exists table_map;
 
 CREATE TABLE table_map (a Map(UInt8, Int), b UInt8, c UInt32) engine = MergeTree order by tuple();
@@ -27,3 +28,7 @@ select map( 'aa', 4, 'bb' , 5) as m, mapKeys(m), mapValues(m);
 select map( 'aa', 4, 'bb' , 5) as m, mapContains(m, 'aa'), mapContains(m, 'k');
 
 select map(0, 0) as m, mapContains(m, number % 2) from numbers(2);
+
+select mapFromArrays(['aa', 'bb'], [4, 5]);
+select mapFromArrays(['aa', 'bb'], [4, 5], [6, 7]); -- { serverError 42 }
+select mapFromArrays(['aa', 'bb'], [4, 5, 6]); -- { serverError 190 }
