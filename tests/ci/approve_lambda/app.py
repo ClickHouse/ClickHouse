0ee8bb3c006460d83b76ffc61f17d25148b75720
alesapin <alesapin@gmail.com>
2021-11-08 13:05:59 +0300

    Fixes in approve lambda

--- tests/ci/approve_lambda/app.py
+++ tests/ci/approve_lambda/app.py
@@ -1,11 +1,9 @@
 #!/usr/bin/env python3
 
-import logging
 import json
 import time
 import fnmatch
 from collections import namedtuple
-import sys
 import jwt
 
 import requests
@@ -27,7 +25,12 @@ SUSPICIOUS_PATTERNS = [
 MAX_RETRY = 5
 
 WorkflowDescription = namedtuple('WorkflowDescription',
-                                 ['action', 'run_id', 'event', 'sender_login', 'fork_owner_login', 'fork_branch', 'sender_orgs'])
+                                 ['name', 'action', 'run_id', 'event', 'sender_login',
+                                  'workflow_id', 'fork_owner_login', 'fork_branch', 'sender_orgs'])
+
+TRUSTED_WORKFLOW_IDS = {
+    14586616, # Cancel workflows, always trusted
+}
 
 TRUSTED_ORG_IDS = {
     7409213,   # yandex
@@ -119,16 +122,16 @@ def get_key_and_app_from_aws():
 
 def is_trusted_sender(pr_user_login, pr_user_orgs):
     if pr_user_login in TRUSTED_CONTRIBUTORS:
-        logging.info("User '%s' is trusted", pr_user_login)
+        print(f"User '{pr_user_login}' is trusted")
         return True
 
-    logging.info("User '%s' is not trusted", pr_user_login)
+    print(f"User '{pr_user_login}' is not trusted")
 
     for org_id in pr_user_orgs:
         if org_id in TRUSTED_ORG_IDS:
-            logging.info("Org '%s' is trusted; will mark user %s as trusted", org_id, pr_user_login)
+            print(f"Org '{org_id}' is trusted; will mark user {pr_user_login} as trusted")
             return True
-        logging.info("Org '%s' is not trusted", org_id)
+        print(f"Org '{org_id}' is not trusted")
 
     return False
 
@@ -139,7 +142,7 @@ def _exec_get_with_retry(url):
             response.raise_for_status()
             return response.json()
         except Exception as ex:
-            logging.info("Got exception executing request %s", ex)
+            print("Got exception executing request", ex)
             time.sleep(i + 1)
 
     raise Exception("Cannot execute GET request with retries")
@@ -157,11 +160,12 @@ def _exec_post_with_retry(url, token, data=None):
             if response.status_code == 403:
                 data = response.json()
                 if 'message' in data and data['message'] == 'This workflow run is not waiting for approval':
+                    print("Workflow doesn't need approval")
                     return data
             response.raise_for_status()
             return response.json()
         except Exception as ex:
-            logging.info("Got exception executing request %s", ex)
+            print("Got exception executing request", ex)
             time.sleep(i + 1)
 
     raise Exception("Cannot execute POST request with retry")
@@ -179,7 +183,10 @@ def get_workflow_description_from_event(event):
     fork_branch = event['workflow_run']['head_branch']
     orgs_data = _exec_get_with_retry(event['sender']['organizations_url'])
     sender_orgs = [org['id'] for org in orgs_data]
+    name = event['workflow_run']['name']
+    workflow_id = event['workflow_run']['workflow_id']
     return WorkflowDescription(
+        name=name,
         action=action,
         sender_login=sender_login,
         run_id=run_id,
@@ -187,6 +194,7 @@ def get_workflow_description_from_event(event):
         fork_owner_login=fork_owner,
         fork_branch=fork_branch,
         sender_orgs=sender_orgs,
+        workflow_id=workflow_id,
     )
 
 
@@ -195,33 +203,36 @@ def get_changed_files_for_pull_request(pull_request):
 
     changed_files = set([])
     for i in range(1, 31):
-        logging.info("Requesting changed files page %s", i)
+        print("Requesting changed files page", i)
         url = f"{API_URL}/pulls/{number}/files?page={i}&per_page=100"
         data = _exec_get_with_retry(url)
-        logging.info("Got %s changed files", len(data))
+        print(f"Got {len(data)} changed files")
+        if len(data) == 0:
+            print("No more changed files")
+            break
+
         for change in data:
-            logging.info("Adding changed file %s", change['filename'])
+            #print("Adding changed file", change['filename'])
             changed_files.add(change['filename'])
 
         if len(changed_files) >= SUSPICIOUS_CHANGED_FILES_NUMBER:
-            logging.info("More than %s changed files. Will stop fetching new files.", len(changed_files))
+            print(f"More than {len(changed_files)} changed files. Will stop fetching new files.")
             break
 
     return changed_files
 
 def check_suspicious_changed_files(changed_files):
     if len(changed_files) >= SUSPICIOUS_CHANGED_FILES_NUMBER:
-        logging.info("Too many files changed %s, need manual approve", len(changed_files))
+        print(f"Too many files changed {len(changed_files)}, need manual approve")
         return True
 
     for path in changed_files:
         for pattern in SUSPICIOUS_PATTERNS:
             if fnmatch.fnmatch(path, pattern):
-                logging.info("File %s match suspicious pattern %s, will not approve automatically", path, pattern)
+                print(f"File {path} match suspicious pattern {pattern}, will not approve automatically")
                 return True
-            logging.info("File %s doesn't match suspicious pattern %s", path, pattern)
 
-    logging.info("No changed files match suspicious patterns, run will be approved")
+    print("No changed files match suspicious patterns, run will be approved")
     return False
 
 def approve_run(run_id, token):
@@ -248,22 +259,27 @@ def get_token_from_aws():
     return get_access_token(encoded_jwt, installation_id)
 
 def main(event):
-    print("Got event", event)
     token = get_token_from_aws()
     event_data = json.loads(event['body'])
-    print("Event body", event_data)
-    print("Type of event_data", type(event_data))
     workflow_description = get_workflow_description_from_event(event_data)
-    logging.info("Got workflow description %s", workflow_description)
-    if workflow_description.event != "requested":
-        logging.info("Exiting, event type is %s", workflow_description.event)
+
+    print("Got workflow description", workflow_description)
+    if workflow_description.action != "requested":
+        print("Exiting, event action is", workflow_description.action)
+        return
+
+    if workflow_description.workflow_id in TRUSTED_WORKFLOW_IDS:
+        print("Workflow in trusted list, approving run")
+        approve_run(workflow_description.run_id, token)
+        return
 
     if is_trusted_sender(workflow_description.sender_login, workflow_description.sender_orgs):
+        print("Sender is trusted, approving run")
         approve_run(workflow_description.run_id, token)
         return
 
     pull_requests = _get_pull_requests_from(workflow_description.fork_owner_login, workflow_description.fork_branch)
-    logging.info("Got pull requests for workflow %s", len(pull_requests))
+    print("Got pull requests for workflow", len(pull_requests))
     if len(pull_requests) > 1:
         raise Exception("Received more than one PR for workflow run")
 
@@ -271,17 +287,16 @@ def main(event):
         raise Exception("Cannot find any pull requests for workflow run")
 
     pull_request = pull_requests[0]
-    logging.info("Pull request for workflow number %s", pull_request['number'])
+    print("Pull request for workflow number", pull_request['number'])
 
     changed_files = get_changed_files_for_pull_request(pull_request)
-    logging.info("Totally have %s changed files in PR", len(changed_files))
+    print(f"Totally have {len(changed_files)} changed files in PR:", changed_files)
     if check_suspicious_changed_files(changed_files):
-        logging.info("Pull Request %s has suspicious changes, label it for manuall approve", pull_request['number'])
+        print(f"Pull Request {pull_request['number']} has suspicious changes, label it for manuall approve")
         label_manual_approve(pull_request, token)
     else:
-        logging.info("Pull Request %s has no suspicious changes", pull_request['number'])
+        print(f"Pull Request {pull_request['number']} has no suspicious changes")
         approve_run(workflow_description.run_id, token)
 
 def handler(event, _):
-    logging.basicConfig(level=logging.INFO)
     main(event)
