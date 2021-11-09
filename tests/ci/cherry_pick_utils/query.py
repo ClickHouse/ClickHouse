da196c7a7159a43efe3fc3fd3cca7de30c020968
alesapin <alesapin@gmail.com>
2021-11-09 14:34:08 +0300

    More retries

--- tests/ci/cherry_pick_utils/query.py
+++ tests/ci/cherry_pick_utils/query.py
@@ -366,9 +366,9 @@ class Query:
         from urllib3.util.retry import Retry
 
         def requests_retry_session(
-            retries=3,
-            backoff_factor=0.3,
-            status_forcelist=(500, 502, 504),
+            retries=5,
+            backoff_factor=0.5,
+            status_forcelist=(403, 500, 502, 504),
             session=None,
         ):
             session = session or requests.Session()
