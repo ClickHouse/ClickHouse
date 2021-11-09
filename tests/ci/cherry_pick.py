5af2b0d16af352312f957b9909c7f9d2d6f165f1
alesapin <alesapin@gmail.com>
2021-11-09 15:40:03 +0300

    Use robot token

--- tests/ci/cherry_pick.py
+++ tests/ci/cherry_pick.py
@@ -5,7 +5,7 @@ import logging
 import os
 import subprocess
 
-from get_robot_token import get_best_robot_token
+from get_robot_token import get_parameter_from_ssm
 from ssh import SSHKey
 from cherry_pick_utils.backport import Backport
 from cherry_pick_utils.cherrypick import CherryPick
@@ -19,13 +19,12 @@ if __name__ == "__main__":
     if not os.path.exists(temp_path):
         os.makedirs(temp_path)
 
-    token = get_best_robot_token()
 
     sys.path.append(os.path.join(repo_path, "utils/github"))
 
 
     with SSHKey("ROBOT_CLICKHOUSE_SSH_KEY"):
-        token = get_best_robot_token()
+        token = get_parameter_from_ssm("github_robot_token_1")
 
         bp = Backport(token, os.environ.get("REPO_OWNER"), os.environ.get("REPO_NAME"), os.environ.get("REPO_TEAM"))
         def cherrypick_run(token, pr, branch):
