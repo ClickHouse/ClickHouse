07708e99a5038432dcfe4b688d6dbe5cba010a7a
cmsxbc <i@cmsis.me>
2021-11-11 08:22:57 +0800

    fix ErrorCode in test because of confilict

--- tests/queries/0_stateless/02114_offset_fetch_without_order_by.sh
+++ tests/queries/0_stateless/02114_offset_fetch_without_order_by.sh
@@ -4,4 +4,4 @@ CUR_DIR=$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)
 # shellcheck source=../shell_config.sh
 . "$CUR_DIR"/../shell_config.sh
 
-$CLICKHOUSE_CURL -sS "$CLICKHOUSE_URL" -d "SELECT number from numbers(10) OFFSET 1 ROWS FETCH FIRST 1 ROWS ONLY;" | grep -oF 'Code: 627'
+$CLICKHOUSE_CURL -sS "$CLICKHOUSE_URL" -d "SELECT number from numbers(10) OFFSET 1 ROWS FETCH FIRST 1 ROWS ONLY;" | grep -oF 'Code: 628'
