885d98fd52a66f2ffd1c0956272afe9feb5342f7
robot-clickhouse <robot-clickhouse@clickhouse.com>
2021-11-11 21:53:44 +0000

    Backport #31226 to 21.10: Fix typo in USE_MYSQL check

--- src/Parsers/ParserExternalDDLQuery.cpp
+++ src/Parsers/ParserExternalDDLQuery.cpp
@@ -9,7 +9,7 @@
 #include <Parsers/ParserDropQuery.h>
 #include <Parsers/ParserRenameQuery.h>
 
-#ifdef USE_MYSQL
+#if USE_MYSQL
 #    include <Parsers/MySQL/ASTAlterQuery.h>
 #    include <Parsers/MySQL/ASTCreateQuery.h>
 #endif
@@ -17,7 +17,7 @@
 namespace DB
 {
 
-#ifdef USE_MYSQL
+#if USE_MYSQL
 namespace ErrorCodes
 {
     extern const int MYSQL_SYNTAX_ERROR;
@@ -43,7 +43,7 @@ bool ParserExternalDDLQuery::parseImpl(IParser::Pos & pos, ASTPtr & node, Expect
     bool res = false;
     if (external_ddl_query->from->name == "MySQL")
     {
-#ifdef USE_MYSQL
+#if USE_MYSQL
         ParserDropQuery p_drop_query;
         ParserRenameQuery p_rename_query;
         MySQLParser::ParserAlterQuery p_alter_query;
