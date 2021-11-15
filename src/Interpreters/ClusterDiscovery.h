84fb4354bc7abd2466efd8eaa6be6a7efdd7909d
vdimir <vdimir@clickhouse.com>
2021-11-15 17:52:52 +0300

    cluster discovery worker thread

--- src/Interpreters/ClusterDiscovery.h
+++ src/Interpreters/ClusterDiscovery.h
@@ -1,10 +1,14 @@
 #pragma once
 
-#include <unordered_map>
-#include <base/defines.h>
-#include <Poco/Logger.h>
+#include <Common/ConcurrentBoundedQueue.h>
+#include <Common/ThreadPool.h>
 #include <Common/ZooKeeper/Common.h>
 
+#include <Poco/Logger.h>
+
+#include <base/defines.h>
+
+#include <unordered_map>
 
 namespace DB
 {
@@ -20,10 +24,17 @@ public:
 
     void start();
 
+    ~ClusterDiscovery();
 
 private:
+    Strings getNodeNames(zkutil::ZooKeeperPtr & zk, const String & zk_root, const String & cluster_name);
     Strings getNodes(zkutil::ZooKeeperPtr & zk, const String & zk_root, const Strings & nodes);
+
+    void updateCluster(const String & cluster_name);
     void updateCluster(const String & cluster_name, const String & zk_root);
+
+    void runMainThread();
+    void shutdown();
 
     /// cluster name -> path in zk
     std::unordered_map<String, String> clusters;
@@ -32,6 +43,11 @@ private:
 
     String node_name;
     UInt16 server_port;
+
+    using UpdateQueue = ConcurrentBoundedQueue<std::string>;
+    std::shared_ptr<UpdateQueue> queue;
+    std::atomic<bool> stop_flag = false;
+    ThreadFromGlobalPool main_thread;
 
     Poco::Logger * log;
 };
