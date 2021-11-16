e7d3dbeebd22c131d7ef14067e35609bcd4e4dfd
vdimir <vdimir@clickhouse.com>
2021-11-16 16:01:57 +0300

    cluster discovery init only with zookeeper

--- programs/server/Server.cpp
+++ programs/server/Server.cpp
@@ -841,7 +841,7 @@ if (ThreadFuzzer::instance().isEffective())
             // in a lot of places. For now, disable updating log configuration without server restart.
             //setTextLog(global_context->getTextLog());
             updateLevels(*config, logger());
-            global_context->setClustersConfig(config);
+            global_context->setClustersConfig(config, has_zookeeper);
             global_context->setMacros(std::make_unique<Macros>(*config, "macros", log));
             global_context->setExternalAuthenticatorsConfig(*config);
 
@@ -1555,7 +1555,8 @@ if (ThreadFuzzer::instance().isEffective())
             server.start();
         LOG_INFO(log, "Ready for connections.");
 
-        global_context->registerNodeForClusterDiscovery();
+        if (has_zookeeper)
+            global_context->registerNodeForClusterDiscovery();
 
         SCOPE_EXIT_SAFE({
             LOG_DEBUG(log, "Received termination signal.");
