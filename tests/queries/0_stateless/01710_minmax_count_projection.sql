e31be4ac96c72d5fe59201c5d2eb88b6d3c32162
Amos Bird <amosbird@gmail.com>
2021-11-17 23:11:23 +0800

    fix

--- tests/queries/0_stateless/01710_minmax_count_projection.sql
+++ tests/queries/0_stateless/01710_minmax_count_projection.sql
@@ -46,7 +46,7 @@ select count() from t;
 drop table t;
 
 drop table if exists d;
-create table d (dt DateTime, j int) engine MergeTree partition by toDate(dt) order by tuple();
+create table d (dt DateTime, j int) engine MergeTree partition by (toDate(dt), ceiling(j), toDate(dt), CEILING(j)) order by tuple();
 insert into d values ('2021-10-24 10:00:00', 10), ('2021-10-25 10:00:00', 10), ('2021-10-26 10:00:00', 10), ('2021-10-27 10:00:00', 10);
 select min(dt), max(dt), count() from d where toDate(dt) >= '2021-10-25';
 drop table d;
