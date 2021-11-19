86ad0e26e761bf813497bd623fccf2f7672dfcca
vdimir <vdimir@clickhouse.com>
2021-11-19 12:59:23 +0300

    disable `partial_merge_join_left_table_buffer_bytes` due to bug

--- src/Interpreters/MergeJoin.cpp
+++ src/Interpreters/MergeJoin.cpp
@@ -554,7 +554,10 @@ MergeJoin::MergeJoin(std::shared_ptr<TableJoin> table_join_, const Block & right
 
     if (size_t max_bytes = table_join->maxBytesInLeftBuffer(); max_bytes > 0)
     {
-        left_blocks_buffer = std::make_shared<SortedBlocksBuffer>(left_sort_description, max_bytes);
+        /// Disabled due to https://github.com/ClickHouse/ClickHouse/issues/31009
+        // left_blocks_buffer = std::make_shared<SortedBlocksBuffer>(left_sort_description, max_bytes);
+        LOG_WARNING(log, "`partial_merge_join_left_table_buffer_bytes` is disabled in current version of ClickHouse");
+        UNUSED(left_blocks_buffer);
     }
 }
 
