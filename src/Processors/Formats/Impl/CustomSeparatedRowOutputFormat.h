fdd1f53d3a829331ce1fd94a914b934f76a25150
Kruglov Pavel <48961922+Avogar@users.noreply.github.com>
2021-11-19 16:52:48 +0300

    Update CustomSeparatedRowOutputFormat.h

--- src/Processors/Formats/Impl/CustomSeparatedRowOutputFormat.h
+++ src/Processors/Formats/Impl/CustomSeparatedRowOutputFormat.h
@@ -23,7 +23,7 @@ private:
     void writeRowStartDelimiter() override;
     void writeRowEndDelimiter() override;
     void writeRowBetweenDelimiter() override;
-    void doWritePrefix() override;
+    void writePrefix() override;
     void writeSuffix() override;
 
     void writeLine(const std::vector<String> & values);
