a79a0fa26545f0d2f8d6815f27368789ca13fada
alexey-milovidov <milovidov@clickhouse.com>
2021-11-24 00:34:57 +0300

    Update run_check.py

--- tests/ci/run_check.py
+++ tests/ci/run_check.py
@@ -37,7 +37,7 @@ TRUSTED_CONTRIBUTORS = {
     "codyrobert",   # Flickerbox engineer
     "damozhaeva",   # DOCSUP
     "den-crane",
-    "flickerbox-tom", # Flickerbox 
+    "flickerbox-tom", # Flickerbox
     "gyuton",       # technical writer, Yandex
     "hagen1778",    # Roman Khavronenko, seasoned contributor
     "hczhcz",
