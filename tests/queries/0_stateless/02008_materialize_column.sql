4ae92fe6031bb0961e57668bf145f44a5a1b8f22
Vladimir Chebotarev <vladimir.chebotarev@gmail.com>
2021-11-25 00:38:46 +0300

    Update 02008_materialize_column.sql

--- tests/queries/0_stateless/02008_materialize_column.sql
+++ tests/queries/0_stateless/02008_materialize_column.sql
@@ -5,35 +5,35 @@ SET mutations_sync = 2;
 CREATE TABLE tmp (x Int64) ENGINE = MergeTree() ORDER BY tuple() PARTITION BY tuple();
 INSERT INTO tmp SELECT * FROM system.numbers LIMIT 20;
 
-ALTER TABLE tmp ADD COLUMN s String DEFAULT toString(x);
-SELECT groupArray(x), groupArray(s) FROM tmp;
+ALTER TABLE tmp ADD COLUMN column_with_default String DEFAULT toString(x);
+SELECT groupArray(x), groupArray(column_with_default) FROM tmp;
 
-ALTER TABLE tmp MODIFY COLUMN s String DEFAULT toString(x+1);
-SELECT groupArray(x), groupArray(s) FROM tmp;
+ALTER TABLE tmp MODIFY COLUMN column_with_default String DEFAULT toString(x+1);
+SELECT groupArray(x), groupArray(column_with_default) FROM tmp;
 
-ALTER TABLE tmp MATERIALIZE COLUMN s;
-ALTER TABLE tmp MODIFY COLUMN s String DEFAULT toString(x+2);
-SELECT groupArray(x), groupArray(s) FROM tmp;
+ALTER TABLE tmp MATERIALIZE COLUMN column_with_default;
+ALTER TABLE tmp MODIFY COLUMN column_with_default String DEFAULT toString(x+2);
+SELECT groupArray(x), groupArray(column_with_default) FROM tmp;
 
-ALTER TABLE tmp MATERIALIZE COLUMN s;
-ALTER TABLE tmp MODIFY COLUMN s String DEFAULT toString(x+3);
-SELECT groupArray(x), groupArray(s) FROM tmp;
-ALTER TABLE tmp DROP COLUMN s;
+ALTER TABLE tmp MATERIALIZE COLUMN column_with_default;
+ALTER TABLE tmp MODIFY COLUMN column_with_default String DEFAULT toString(x+3);
+SELECT groupArray(x), groupArray(column_with_default) FROM tmp;
+ALTER TABLE tmp DROP COLUMN column_with_default;
 
-ALTER TABLE tmp ADD COLUMN s String MATERIALIZED toString(x);
-SELECT groupArray(x), groupArray(s) FROM tmp;
+ALTER TABLE tmp ADD COLUMN column_with_materialized String MATERIALIZED toString(x);
+SELECT groupArray(x), groupArray(column_with_materialized) FROM tmp;
 
-ALTER TABLE tmp MODIFY COLUMN s String MATERIALIZED toString(x+1);
-SELECT groupArray(x), groupArray(s) FROM tmp;
+ALTER TABLE tmp MODIFY COLUMN column_with_materialized String MATERIALIZED toString(x+1);
+SELECT groupArray(x), groupArray(column_with_materialized) FROM tmp;
 
-ALTER TABLE tmp MATERIALIZE COLUMN s;
-ALTER TABLE tmp MODIFY COLUMN s String MATERIALIZED toString(x+2);
-SELECT groupArray(x), groupArray(s) FROM tmp;
+ALTER TABLE tmp MATERIALIZE COLUMN column_with_materialized;
+ALTER TABLE tmp MODIFY COLUMN column_with_materialized String MATERIALIZED toString(x+2);
+SELECT groupArray(x), groupArray(column_with_materialized) FROM tmp;
 
-ALTER TABLE tmp MATERIALIZE COLUMN s;
-ALTER TABLE tmp MODIFY COLUMN s String MATERIALIZED toString(x+3);
-SELECT groupArray(x), groupArray(s) FROM tmp;
-ALTER TABLE tmp DROP COLUMN s;
+ALTER TABLE tmp MATERIALIZE COLUMN column_with_materialized;
+ALTER TABLE tmp MODIFY COLUMN column_with_materialized String MATERIALIZED toString(x+3);
+SELECT groupArray(x), groupArray(column_with_materialized) FROM tmp;
+ALTER TABLE tmp DROP COLUMN column_with_materialized;
 
 DROP TABLE tmp;
 
