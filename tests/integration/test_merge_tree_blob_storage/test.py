9c1094219441d1bf0901a084cce5a51ed991a8c6
Jakub Kuklis <jk.kuklis@gmail.com>
2021-11-26 15:04:53 +0100

    Running Azurite docker in the integration test for Blob Storage support

--- tests/integration/test_merge_tree_blob_storage/test.py
+++ tests/integration/test_merge_tree_blob_storage/test.py
@@ -18,7 +18,8 @@ def cluster():
     try:
         cluster = ClickHouseCluster(__file__)
         cluster.add_instance("node",
-            main_configs=["configs/config.d/storage_conf.xml", "configs/config.d/bg_processing_pool_conf.xml"])
+            main_configs=["configs/config.d/storage_conf.xml", "configs/config.d/bg_processing_pool_conf.xml"],
+            with_azurite=True)
         logging.info("Starting cluster...")
         cluster.start()
         logging.info("Cluster started")
@@ -49,6 +50,7 @@ def create_table(node, table_name, **additional_settings):
     node.query(f"DROP TABLE IF EXISTS {table_name}")
     node.query(create_table_statement)
     assert node.query(f"SELECT COUNT(*) FROM {table_name}") == "0\n"
+    node.query(f"INSERT INTO {table_name} VALUES ('2021-11-13', 3, 'hello')")
 
 
 @pytest.mark.parametrize(
