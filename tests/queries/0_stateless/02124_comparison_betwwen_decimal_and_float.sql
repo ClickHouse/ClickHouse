80eda40afd5c6faadf4fec463ff1e20d6aaa67b2
feng lv <fenglv15@mails.ucas.ac.cn>
2021-11-30 03:12:12 +0000

    Enable comparison betwwen Decimal and Float
    add tests
    
    update test

---
+++ tests/queries/0_stateless/02124_comparison_betwwen_decimal_and_float.sql
@@ -0,0 +1,14 @@
+select CAST(1.0, 'Decimal(15,2)') > CAST(1, 'Float64');
+select CAST(1.0, 'Decimal(15,2)') = CAST(1, 'Float64');
+select CAST(1.0, 'Decimal(15,2)') < CAST(1, 'Float64');
+select CAST(1.0, 'Decimal(15,2)') > CAST(-1, 'Float64');
+select CAST(1.0, 'Decimal(15,2)') = CAST(-1, 'Float64');
+select CAST(1.0, 'Decimal(15,2)') < CAST(-1, 'Float64');
+select CAST(1.0, 'Decimal(15,2)') > CAST(1, 'Float32');
+select CAST(1.0, 'Decimal(15,2)') = CAST(1, 'Float32');
+select CAST(1.0, 'Decimal(15,2)') < CAST(1, 'Float32');
+select CAST(1.0, 'Decimal(15,2)') > CAST(-1, 'Float32');
+select CAST(1.0, 'Decimal(15,2)') = CAST(-1, 'Float32');
+select CAST(1.0, 'Decimal(15,2)') < CAST(-1, 'Float32');
+
+SELECT toDecimal32('11.00', 2) > 1.;
