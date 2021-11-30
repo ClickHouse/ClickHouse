c6abe60bcca25344bcd031c250daa3fd033b5ec7
taiyang-li <654010905@qq.com>
2021-11-30 18:06:26 +0800

    add new input format HiveTextRowInputFormat

---
+++ src/Processors/Formats/Impl/HiveTextRowInputFormat.cpp
@@ -0,0 +1,50 @@
+#include <Processors/Formats/Impl/HiveTextRowInputFormat.h>
+
+#if USE_HIVE
+namespace DB
+{
+
+namespace ErrorCodes
+{
+    extern const int NOT_IMPLEMENTED;
+}
+
+HiveTextRowInputFormat::HiveTextRowInputFormat(
+    const Block & header_, ReadBuffer & in_, const Params & params_, const FormatSettings & format_settings_)
+    : CSVRowInputFormat(header_, buf, params_, true, false, format_settings_)
+    , buf(in_)
+{
+}
+
+void HiveTextRowInputFormat::readPrefix()
+{
+    std::vector<bool> read_columns(data_types.size(), false);
+    /// For Hive Text file, read the first row to get exact number of columns.
+    auto values = readNames();
+    input_field_names = format_settings.csv.input_field_names;
+    input_field_names.resize(values.size());
+    for (const auto & column_name : input_field_names)
+        addInputColumn(column_name, read_columns);
+
+    for (size_t i = 0; i != read_columns.size(); ++i)
+    {
+        if (!read_columns[i])
+            column_mapping->not_presented_columns.push_back(i);
+    }
+}
+
+std::vector<String> HiveTextRowInputFormat::readNames()
+{
+    PeekableReadBufferCheckpoint checkpoint{buf};
+    auto values = readHeaderRow();
+    buf.rollbackToCheckpoint();
+    return values;
+}
+
+std::vector<String> HiveTextRowInputFormat::readTypes()
+{
+    throw Exception(ErrorCodes::NOT_IMPLEMENTED, "HiveTextRowInputFormat::readTypes is not implemented");
+}
+
+}
+#endif
---
+++ src/Processors/Formats/Impl/HiveTextRowInputFormat.h
@@ -0,0 +1,33 @@
+#pragma once
+
+#include <Common/config.h>
+
+#if USE_HIVE
+#include <IO/PeekableReadBuffer.h>
+#include <Processors/Formats/Impl/CSVRowInputFormat.h>
+
+
+namespace DB
+{
+
+/// A stream for input data in Hive Text format.
+/// Parallel parsing is disabled currently.
+class HiveTextRowInputFormat : public CSVRowInputFormat
+{
+public:
+    HiveTextRowInputFormat(const Block & header_, ReadBuffer & in_, const Params & params_, const FormatSettings & format_settings_);
+
+    String getName() const override { return "HiveTextRowInputFormat"; }
+
+protected:
+    void readPrefix() override;
+    std::vector<String> readNames() override;
+    std::vector<String> readTypes() override;
+
+private:
+    PeekableReadBuffer buf;
+    std::vector<String> input_field_names;
+};
+}
+
+#endif
