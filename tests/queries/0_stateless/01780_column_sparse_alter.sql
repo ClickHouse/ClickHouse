bb12e064af4c5278364d1d12a8c59fa56f30191a
Anton Popov <pad11rus@gmail.com>
2021-12-01 21:21:38 +0300

    fix test

--- tests/queries/0_stateless/01780_column_sparse_alter.sql
+++ tests/queries/0_stateless/01780_column_sparse_alter.sql
@@ -8,9 +8,9 @@ SETTINGS ratio_of_defaults_for_sparse_serialization = 0.5;
 
 INSERT INTO t_sparse_alter SELECT
     number,
-    if (number % 10 = 0, number, 0),
-    if (number % 20 = 0, toString(number), '')
-FROM numbers(200);
+    if (number % 11 = 0, number, 0),
+    if (number % 13 = 0, toString(number), '')
+FROM numbers(2000);
 
 SELECT column, serialization_kind FROM system.parts_columns WHERE database = currentDatabase() AND table = 't_sparse_alter' AND active ORDER BY name;
 
