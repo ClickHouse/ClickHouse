024a3b46263e4304deac0288b921af533549dde6
Jakub Kuklis <jk.kuklis@gmail.com>
2021-12-01 08:50:22 +0000

    Fixing submodule update for clickhouse compilation in the fast test

--- docker/test/fasttest/run.sh
+++ docker/test/fasttest/run.sh
@@ -186,6 +186,7 @@ function clone_submodules
             contrib/dragonbox
             contrib/fast_float
             contrib/NuRaft
+            contrib/azure
         )
 
         git submodule sync
