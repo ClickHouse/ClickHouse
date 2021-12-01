71e6c7e92ed85ac51b7582e22365e0483ed70d63
alesapin <alesapin@gmail.com>
2021-12-01 17:23:51 +0300

    Add ability to rerun checks

--- tests/ci/fast_test_check.py
+++ tests/ci/fast_test_check.py
@@ -15,6 +15,7 @@ from docker_pull_helper import get_image_with_version
 from commit_status_helper import post_commit_status
 from clickhouse_helper import ClickHouseHelper, mark_flaky_tests, prepare_tests_results_for_clickhouse
 from stopwatch import Stopwatch
+from rerun_helper import RerunHelper
 
 NAME = 'Fast test (actions)'
 
@@ -66,6 +67,11 @@ if __name__ == "__main__":
     pr_info = PRInfo(get_event())
 
     gh = Github(get_best_robot_token())
+
+    rerun_helper = RerunHelper(gh, pr_info, NAME)
+    if rerun_helper.is_already_finished_by_status():
+        logging.info("Check is already finished according to github status, exiting")
+        sys.exit(0)
 
     docker_image = get_image_with_version(temp_path, 'clickhouse/fasttest')
 
