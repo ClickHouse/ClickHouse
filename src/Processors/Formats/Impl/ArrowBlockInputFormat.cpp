e2d1ed15682c88a8ae816d93a09d21fab4cf7aed
taiyang-li <654010905@qq.com>
2021-12-02 20:51:19 +0800

    fix error

--- src/Processors/Formats/Impl/ArrowBlockInputFormat.cpp
+++ src/Processors/Formats/Impl/ArrowBlockInputFormat.cpp
@@ -64,6 +64,8 @@ Chunk ArrowBlockInputFormat::generate()
 
     ++record_batch_current;
 
+    arrow_column_to_ch_column->arrowTableToCHChunk(res, *table_result);
+
     /// If defaults_for_omitted_fields is true, calculate the default values from default expression for omitted fields.
     /// Otherwise fill the missing columns with zero values of its type.
     if (format_settings.defaults_for_omitted_fields)
