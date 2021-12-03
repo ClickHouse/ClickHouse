ebff6485673018f786c680c47220b0b8791fac61
Jakub Kuklis <jk.kuklis@gmail.com>
2021-12-03 16:53:23 +0100

    Comming back to StorageSharedKeyCredential for the integration test, get 400 Bad requests from Azurite when trying to create a container now

--- tests/integration/helpers/cluster.py
+++ tests/integration/helpers/cluster.py
@@ -1400,6 +1400,7 @@ class ClickHouseCluster:
         # TODO: assumes package azure-storage-blob is installed either locally or in the docker
         from azure.storage.blob import BlobServiceClient
         connection_string = "DefaultEndpointsProtocol=http;AccountName=devstoreaccount1;AccountKey=Eby8vdM02xNOcqFlqUwJPLlmEtlCDXJ1OUzFT50uSRZ6IFsuFq2UVErCz4I6tq/K1SZFPTOtr/KBHBeksoGMGw==;BlobEndpoint=http://127.0.0.1:10000/devstoreaccount1;"
+        time.sleep(1)
         start = time.time()
         while time.time() - start < timeout:
             try:
