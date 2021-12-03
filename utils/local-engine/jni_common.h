55099c49a1bc8c1c75c79cb1ea0349fb59863a52
neng.liu <neng.liu@kyligence.io>
2021-12-03 14:15:59 +0000

    add jni common

---
+++ utils/local-engine/jni_common.h
@@ -0,0 +1,41 @@
+#pragma once
+
+static jclass io_exception_class;
+static jclass runtime_exception_class;
+static jclass unsupportedoperation_exception_class;
+static jclass illegal_access_exception_class;
+static jclass illegal_argument_exception_class;
+
+jclass CreateGlobalClassReference(JNIEnv* env, const char* class_name) {
+    jclass local_class = env->FindClass(class_name);
+    jclass global_class = (jclass)env->NewGlobalRef(local_class);
+    env->DeleteLocalRef(local_class);
+    if (global_class == nullptr) {
+        std::string error_message =
+            "Unable to createGlobalClassReference for" + std::string(class_name);
+        env->ThrowNew(illegal_access_exception_class, error_message.c_str());
+    }
+    return global_class;
+}
+
+jmethodID GetMethodID(JNIEnv* env, jclass this_class, const char* name, const char* sig) {
+    jmethodID ret = env->GetMethodID(this_class, name, sig);
+    if (ret == nullptr) {
+        std::string error_message = "Unable to find method " + std::string(name) +
+            " within signature" + std::string(sig);
+        env->ThrowNew(illegal_access_exception_class, error_message.c_str());
+    }
+
+    return ret;
+}
+
+jmethodID GetStaticMethodID(JNIEnv* env, jclass this_class, const char* name,
+                            const char* sig) {
+    jmethodID ret = env->GetStaticMethodID(this_class, name, sig);
+    if (ret == nullptr) {
+        std::string error_message = "Unable to find static method " + std::string(name) +
+            " within signature" + std::string(sig);
+        env->ThrowNew(illegal_access_exception_class, error_message.c_str());
+    }
+    return ret;
+}
