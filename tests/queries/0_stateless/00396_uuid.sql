b5b2e27ecd577023f677d8cca5eb4bbba6e299ea
frank chen <frank.chen021@outlook.com>
2021-12-06 17:12:34 +0800

    keep leading zeros for UUID
    Signed-off-by: frank chen <frank.chen021@outlook.com>

--- tests/queries/0_stateless/00396_uuid.sql
+++ tests/queries/0_stateless/00396_uuid.sql
@@ -17,7 +17,13 @@ with generateUUIDv4() as uuid,
     hex(uuid) as str2
 select str1 = str2;
 
+-- hex on UUID always generate 32 characters even there're leading zeros
+select lower(hex(toUUID('00000000-80e7-46f8-0000-9d773a2fd319')));
+
 with generateUUIDv4() as uuid,
     bin(reverse(reinterpretAsString(uuid))) as bin_str1,
     bin(uuid) as bin_str2
 select bin_str1 = bin_str2;
+
+-- bin on UUID always generate 128 characters even there're leading zeros
+select length(bin(toUUID('00000000-80e7-46f8-0000-9d773a2fd319')));
