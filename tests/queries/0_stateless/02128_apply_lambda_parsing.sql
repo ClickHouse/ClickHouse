fc382afacb4a2f0bfe63e544484725d503489b60
Kruglov Pavel <48961922+Avogar@users.noreply.github.com>
2021-12-06 14:04:19 +0300

    Update test

--- tests/queries/0_stateless/02128_apply_lambda_parsing.sql
+++ tests/queries/0_stateless/02128_apply_lambda_parsing.sql
@@ -1,13 +1,13 @@
-WITH * APPLY lambda(e); -- { clientError NUMBER_OF_ARGUMENTS_DOESNT_MATCH }
-SELECT * APPLY lambda(); -- { clientError NUMBER_OF_ARGUMENTS_DOESNT_MATCH }
-SELECT * APPLY lambda(1); -- { clientError NUMBER_OF_ARGUMENTS_DOESNT_MATCH }
-SELECT * APPLY lambda(x); -- { clientError NUMBER_OF_ARGUMENTS_DOESNT_MATCH }
-SELECT * APPLY lambda(range(1)); -- { clientError NUMBER_OF_ARGUMENTS_DOESNT_MATCH }
-SELECT * APPLY lambda(range(x)); -- { clientError NUMBER_OF_ARGUMENTS_DOESNT_MATCH }
-SELECT * APPLY lambda(1, 2); -- { clientError TYPE_MISMATCH }
-SELECT * APPLY lambda(x, y); -- { clientError TYPE_MISMATCH }
-SELECT * APPLY lambda((x, y), 2); -- { clientError BAD_ARGUMENTS }
-SELECT * APPLY lambda((x, y), x + y); -- { clientError BAD_ARGUMENTS }
-SELECT * APPLY lambda(tuple(1), 1); -- { clientError BAD_ARGUMENTS }
+WITH * APPLY lambda(e); -- { clientError SYNTAX_ERROR }
+SELECT * APPLY lambda(); -- { clientError SYNTAX_ERROR }
+SELECT * APPLY lambda(1); -- { clientError SYNTAX_ERROR }
+SELECT * APPLY lambda(x); -- { clientError SYNTAX_ERROR }
+SELECT * APPLY lambda(range(1)); -- { clientError SYNTAX_ERROR }
+SELECT * APPLY lambda(range(x)); -- { clientError SYNTAX_ERROR }
+SELECT * APPLY lambda(1, 2); -- { clientError SYNTAX_ERROR }
+SELECT * APPLY lambda(x, y); -- { clientError SYNTAX_ERROR }
+SELECT * APPLY lambda((x, y), 2); -- { clientError SYNTAX_ERROR }
+SELECT * APPLY lambda((x, y), x + y); -- { clientError SYNTAX_ERROR }
+SELECT * APPLY lambda(tuple(1), 1); -- { clientError SYNTAX_ERROR }
 SELECT * APPLY lambda(tuple(x), 1) FROM numbers(5);
 SELECT * APPLY lambda(tuple(x), x + 1) FROM numbers(5);
