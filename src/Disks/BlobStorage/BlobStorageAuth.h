74e26354ccc869d3b75346ba8cbf7ad5109c6256
Jakub Kuklis <jk.kuklis@gmail.com>
2021-12-07 15:40:26 +0100

    Committing missing files

---
+++ src/Disks/BlobStorage/BlobStorageAuth.h
@@ -0,0 +1,18 @@
+#if !defined(ARCADIA_BUILD)
+#include <Common/config.h>
+#endif
+
+#if USE_AZURE_BLOB_STORAGE
+
+#include <Disks/IDiskRemote.h>
+#include <azure/storage/blobs.hpp>
+
+namespace DB
+{
+
+std::shared_ptr<Azure::Storage::Blobs::BlobContainerClient> getBlobContainerClient(
+    const Poco::Util::AbstractConfiguration & config, const String & config_prefix);
+
+}
+
+#endif
