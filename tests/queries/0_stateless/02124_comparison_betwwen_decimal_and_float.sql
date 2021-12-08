d0c5a887a35a2b89ff8adff600a44ba797235792
feng lv <fenglv15@mails.ucas.ac.cn>
2021-12-08 13:16:55 +0000

    update tests

--- tests/queries/0_stateless/02124_comparison_betwwen_decimal_and_float.sql
+++ tests/queries/0_stateless/02124_comparison_betwwen_decimal_and_float.sql
@@ -1,14 +1,20 @@
 select CAST(1.0, 'Decimal(15,2)') > CAST(1, 'Float64');
 select CAST(1.0, 'Decimal(15,2)') = CAST(1, 'Float64');
 select CAST(1.0, 'Decimal(15,2)') < CAST(1, 'Float64');
+select CAST(1.0, 'Decimal(15,2)') != CAST(1, 'Float64');
 select CAST(1.0, 'Decimal(15,2)') > CAST(-1, 'Float64');
 select CAST(1.0, 'Decimal(15,2)') = CAST(-1, 'Float64');
 select CAST(1.0, 'Decimal(15,2)') < CAST(-1, 'Float64');
+select CAST(1.0, 'Decimal(15,2)') != CAST(-1, 'Float64');
 select CAST(1.0, 'Decimal(15,2)') > CAST(1, 'Float32');
 select CAST(1.0, 'Decimal(15,2)') = CAST(1, 'Float32');
 select CAST(1.0, 'Decimal(15,2)') < CAST(1, 'Float32');
+select CAST(1.0, 'Decimal(15,2)') != CAST(1, 'Float32');
 select CAST(1.0, 'Decimal(15,2)') > CAST(-1, 'Float32');
 select CAST(1.0, 'Decimal(15,2)') = CAST(-1, 'Float32');
 select CAST(1.0, 'Decimal(15,2)') < CAST(-1, 'Float32');
+select CAST(1.0, 'Decimal(15,2)') != CAST(-1, 'Float32');
 
 SELECT toDecimal32('11.00', 2) > 1.;
+
+SELECT 0.1 > 0.1::Decimal256(70);
