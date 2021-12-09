6f5e7c2502e9f2665a398f5a2895235a259676de
kssenii <sumarokovakseniia@mail.ru>
2021-12-10 01:10:28 +0300

    Extend test

--- tests/queries/0_stateless/02129_window_functions_disable_optimizations.sql
+++ tests/queries/0_stateless/02129_window_functions_disable_optimizations.sql
@@ -11,4 +11,17 @@ SELECT
     if((number % 2) = 0, 0.5, 1) AS a,
     30 AS b,
     sum(a * b) OVER (ORDER BY number ASC) AS s
-FROM numbers(10)
+FROM numbers(10);
+
+SET optimize_aggregators_of_group_by_keys=1;
+
+SELECT
+    *,
+    if(number = 1, 1, 0) as a,
+    max(a) OVER (ORDER BY number ASC) AS s
+FROM numbers(10);
+
+SET optimize_group_by_function_keys = 1;
+SELECT round(sum(log(2) * number), 6) AS k FROM numbers(10000)
+GROUP BY (number % 2) * (number % 3), number % 3, number % 2
+HAVING sum(log(2) * number) > 346.57353 ORDER BY k;
