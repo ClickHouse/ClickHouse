8e3529818ec71bce00b7645ac6f4c7b5186b3f35
vxider <lb@vxider.com>
2021-12-10 18:01:54 +0000

    update window view tests

--- tests/queries/0_stateless/01052_window_view_proc_tumble_to_now.sh
+++ tests/queries/0_stateless/01052_window_view_proc_tumble_to_now.sh
@@ -11,10 +11,10 @@ DROP TABLE IF EXISTS dst;
 DROP TABLE IF EXISTS wv;
 
 CREATE TABLE dst(count UInt64) Engine=MergeTree ORDER BY tuple();
-CREATE TABLE mt(a Int32, timestamp DateTime) ENGINE=MergeTree ORDER BY tuple();
-CREATE WINDOW VIEW wv TO dst AS SELECT count(a) AS count FROM mt GROUP BY tumble(timestamp, INTERVAL '1' SECOND, 'US/Samoa') AS wid;
+CREATE TABLE mt(a Int32) ENGINE=MergeTree ORDER BY tuple();
+CREATE WINDOW VIEW wv TO dst AS SELECT count(a) AS count FROM mt GROUP BY tumble(now('US/Samoa'), INTERVAL '1' SECOND, 'US/Samoa') AS wid;
 
-INSERT INTO mt VALUES (1, now('US/Samoa') + 1);
+INSERT INTO mt VALUES (1);
 EOF
 
 while true; do
