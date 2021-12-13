5eb5134ceef822a33173e806eb1fbc87c591638d
Jakub Kuklis <jk.kuklis@gmail.com>
2021-12-13 10:52:02 +0100

    Removing any extra work and correcting the cache path in the cache test

--- tests/integration/test_merge_tree_blob_storage/test.py
+++ tests/integration/test_merge_tree_blob_storage/test.py
@@ -62,6 +62,17 @@ def test_create_table(cluster):
     create_table(node, TABLE_NAME)
 
 
+def test_simple_insert_select(cluster):
+    node = cluster.instances[NODE_NAME]
+    create_table(node, TABLE_NAME)
+
+    values = "('2021-11-13',3,'hello')"
+    node.query(f"INSERT INTO {TABLE_NAME} VALUES {values}")
+    assert node.query(f"SELECT dt, id, data FROM {TABLE_NAME} FORMAT Values") == values
+    blob_container_client = cluster.blob_service_client.get_container_client(CONTAINER_NAME)
+    assert len(list(blob_container_client.list_blobs())) >= 12 # 1 format file + 2 skip index files + 9 regular MergeTree files + leftovers from other tests
+
+
 def test_inserts_selects(cluster):
     node = cluster.instances[NODE_NAME]
     create_table(node, TABLE_NAME)
@@ -322,26 +333,12 @@ def test_read_after_cache_is_wiped(cluster):
     node = cluster.instances[NODE_NAME]
     create_table(node, TABLE_NAME)
 
-    node.query("SYSTEM FLUSH LOGS")
-    node.query("TRUNCATE TABLE system.query_log")
-
     values = "('2021-11-13',3,'hello'),('2021-11-14',4,'heyo')"
 
     node.query(f"INSERT INTO {TABLE_NAME} VALUES {values}")
 
     # Wipe cache
-    cluster.exec_in_container(cluster.get_container_id(NODE_NAME), ["rm", "-rf", "/var/lib/clickhouse/disks/blob_storage/cache/"])
+    cluster.exec_in_container(cluster.get_container_id(NODE_NAME), ["rm", "-rf", "/var/lib/clickhouse/disks/blob_storage_disk/cache/"])
 
     # After cache is populated again, only .bin files should be accessed from Blob Storage.
-    assert node.query(f"SELECT * FROM {TABLE_NAME} order by id FORMAT Values") == values
-
-
-def test_simple_insert_select(cluster):
-    node = cluster.instances[NODE_NAME]
-    create_table(node, TABLE_NAME)
-
-    values = "('2021-11-13',3,'hello')"
-    node.query(f"INSERT INTO {TABLE_NAME} VALUES {values}")
-    assert node.query(f"SELECT dt, id, data FROM {TABLE_NAME} FORMAT Values") == values
-    blob_container_client = cluster.blob_service_client.get_container_client(CONTAINER_NAME)
-    assert len(list(blob_container_client.list_blobs())) >= 12 # 1 format file + 2 skip index files + 9 regular MergeTree files + leftovers from other tests
+    assert node.query(f"SELECT * FROM {TABLE_NAME} order by dt, id FORMAT Values") == values
