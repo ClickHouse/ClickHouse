6bf5a61b3b8ce3d2acbc3325c182e00135842f73
Nikolai Kochetov <KochetovNicolai@users.noreply.github.com>
2021-12-16 21:20:39 +0300

    Update WindowTransform.cpp

--- src/Processors/Transforms/WindowTransform.cpp
+++ src/Processors/Transforms/WindowTransform.cpp
@@ -952,7 +952,6 @@ void WindowTransform::updateAggregationState()
             auto * columns = ws.argument_columns.data();
             // Removing arena.get() from the loop makes it faster somehow...
             auto * arena_ptr = arena.get();
-
             for (auto row = first_row; row < past_the_end_row; ++row)
             {
                 a->add(buf, columns, row, arena_ptr);
