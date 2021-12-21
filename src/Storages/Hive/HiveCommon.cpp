b0a03b79ae2f6cd34be0d797291ca6f4dbb5bd8d
lgbo-ustc <lgbo.ustc@gmail.com>
2021-12-21 10:39:59 +0800

    1. add tryDel and getOrTrySet in LRUCache 2. fixed bugs in LRUCache for evicting elements

--- src/Storages/Hive/HiveCommon.cpp
+++ src/Storages/Hive/HiveCommon.cpp
@@ -11,6 +11,7 @@ namespace ErrorCodes
 {
     extern const int NO_HIVEMETASTORE;
     extern const int BAD_ARGUMENTS;
+    extern const int LOGICAL_ERROR;
 }
 
 bool HiveMetastoreClient::shouldUpdateTableMetadata(
@@ -108,7 +109,12 @@ void HiveMetastoreClient::clearTableMetadata(const String & db_name, const Strin
     std::lock_guard lock{mutex};
     HiveTableMetadataPtr metadata = table_metadata_cache.get(cache_key);
     if (metadata)
-        table_metadata_cache.set(cache_key, nullptr);
+    {
+        if (!table_metadata_cache.tryDel(cache_key))
+        {
+            throw Exception(ErrorCodes::LOGICAL_ERROR, "Try to clear table metadata failed.");
+        }
+    }
 }
 
 void HiveMetastoreClient::setClient(std::shared_ptr<Apache::Hadoop::Hive::ThriftHiveMetastoreClient> client_)
