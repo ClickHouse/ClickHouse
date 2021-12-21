c7fddae4f48bf4ecffec73eb7e49965d06a8f48e
N. Kolotov <n.kolotov@outlook.com>
2021-12-21 16:28:21 +0300

    Fixed test env, removed chaining.

--- tests/queries/0_stateless/02151_client_option_echo.sh
+++ tests/queries/0_stateless/02151_client_option_echo.sh
@@ -1,12 +1,16 @@
 #!/usr/bin/env bash
 
-# single query echo on
-${CLICKHOUSE_CLIENT} --echo --query="DROP TABLE IF EXISTS echo_test_0" && \
-# single query echo off
-${CLICKHOUSE_CLIENT} --query="DROP TABLE IF EXISTS echo_test_1" && \
-# multi query echo on
-${CLICKHOUSE_CLIENT} --echo --multiquery --query="DROP TABLE IF EXISTS echo_test_2;DROP TABLE IF EXISTS echo_test_3" && \
-# multi query echo off
-${CLICKHOUSE_CLIENT} --multiquery --query="DROP TABLE IF EXISTS echo_test_4;DROP TABLE IF EXISTS echo_test_5" && \
+CURDIR=$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)
 
-true
+# shellcheck source=../shell_config.sh
+
+. "$CURDIR"/../shell_config.sh
+
+# single query echo on
+${CLICKHOUSE_CLIENT} --echo --query="DROP TABLE IF EXISTS echo_test_0"
+# single query echo off
+${CLICKHOUSE_CLIENT} --query="DROP TABLE IF EXISTS echo_test_1"
+# multi query echo on
+${CLICKHOUSE_CLIENT} --echo --multiquery --query="DROP TABLE IF EXISTS echo_test_2;DROP TABLE IF EXISTS echo_test_3"
+# multi query echo off
+${CLICKHOUSE_CLIENT} --multiquery --query="DROP TABLE IF EXISTS echo_test_4;DROP TABLE IF EXISTS echo_test_5"
