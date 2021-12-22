c07012108e916758007232bce32f1ed33e9ef8ec
Azat Khuzhin <a3at.mail@gmail.com>
2021-12-22 21:06:40 +0300

    tests: fix 01502_jemalloc_percpu_arena with debug binary

--- tests/queries/0_stateless/01502_jemalloc_percpu_arena.sh
+++ tests/queries/0_stateless/01502_jemalloc_percpu_arena.sh
@@ -9,8 +9,11 @@ CURDIR=$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)
 
 ncpus="$(getconf _NPROCESSORS_ONLN)"
 
-# In debug build abort_conf:true is set by default, disable it explicitly
-export MALLOC_CONF=abort_conf:false
+# In debug build the following settings enabled by default:
+# - abort_conf
+# - abort
+# Disable them explicitly (will enable when required).
+export MALLOC_CONF=abort_conf:false,abort:false
 
 # Regression for:
 #
@@ -25,10 +28,10 @@ taskset --cpu-list $((ncpus-1)) ${CLICKHOUSE_LOCAL} -q 'select * from numbers_mt
 # and with abort_conf:true it is not allowed
 (
     # subshell is required to suppress "Aborted" message from the shell.
-    MALLOC_CONF=abort_conf:true
+    MALLOC_CONF=abort_conf:true,abort:true
     taskset --cpu-list $((ncpus-1)) ${CLICKHOUSE_LOCAL} -q 'select 1'
 ) |& grep -F 'Number of CPUs is not deterministic'
 
 # this command should not fail because we specify narenas explicitly
 # (even with abort_conf:true)
-MALLOC_CONF=abort_conf:true,narenas:$((ncpus)) taskset --cpu-list $((ncpus-1)) ${CLICKHOUSE_LOCAL} -q 'select 1' 2>&1
+MALLOC_CONF=abort_conf:true,abort:false,narenas:$((ncpus)) taskset --cpu-list $((ncpus-1)) ${CLICKHOUSE_LOCAL} -q 'select 1' 2>&1
