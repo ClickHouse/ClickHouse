11321add8f714ac63c582300b36599c3f6be096f
taiyang-li <654010905@qq.com>
2021-12-22 11:19:21 +0800

    inject git info for clickhouse-client clickhouse-server

--- programs/client/Client.cpp
+++ programs/client/Client.cpp
@@ -21,6 +21,7 @@
 #include <base/find_symbols.h>
 
 #include <Common/config_version.h>
+#include <Common/git_info.h>
 #include <Common/Exception.h>
 #include <Common/formatReadable.h>
 #include <Common/TerminalSize.h>
@@ -76,6 +77,13 @@ namespace ErrorCodes
     extern const int AUTHENTICATION_FAILED;
 }
 
+namespace GitInfo 
+{
+    extern const std::string GIT_SHA1;
+    extern const std::string GIT_BRANCH;
+    extern const std::string GIT_DATE;
+    extern const std::string GIT_COMMIT_SUBJECT;
+}
 
 void Client::processError(const String & query) const
 {
@@ -1143,6 +1151,14 @@ void Client::processOptions(const OptionsDescription & options_description,
         global_context->getClientInfo().client_trace_context.tracestate = options["opentelemetry-tracestate"].as<std::string>();
 }
 
+static void showClientVersion()
+{
+    std::cout << DBMS_NAME << " client version " << VERSION_STRING << VERSION_OFFICIAL << "." << std::endl;
+    std::cout << "GIT SHA1: " << GitInfo::GIT_SHA1 << std::endl;
+    std::cout << "GIT BRANCH: " << GitInfo::GIT_BRANCH << std::endl;
+    std::cout << "GIT DATE: " << GitInfo::GIT_DATE << std::endl;
+    std::cout << "GIT COMMIT SUBJECT: " << GitInfo::GIT_COMMIT_SUBJECT << std::endl;
+}
 
 void Client::processConfig()
 {
