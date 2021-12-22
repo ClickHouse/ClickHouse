c321ed5b9e5e2df1e7abee2a06813b29859f8ea2
Rajkumar <varada.rajkumar@gmail.com>
2021-12-22 13:39:45 -0800

    Review comments

--- src/Common/FiberStack.h
+++ src/Common/FiberStack.h
@@ -33,12 +33,10 @@ public:
     /// Current value is just enough for all tests in our CI. It's not selected in some special
     /// way. We will have 40 pages with 4KB page size.
     static constexpr size_t default_stack_size = 192 * 1024; /// 64KB was not enough for tests
-    static constexpr size_t default_page_size = 4096;
 
     explicit FiberStack(size_t stack_size_ = default_stack_size) : stack_size(stack_size_)
     {
-        ssize_t tmp_page_size = getPageSize();
-        page_size = (tmp_page_size == -1) ? default_page_size : tmp_page_size;
+        page_size = getPageSize();
     }
 
     boost::context::stack_context allocate()
