fab618ed8c7cadc76b6d8020b5717cc2d84e737f
robot-clickhouse <robot-clickhouse@clickhouse.com>
2021-12-23 10:13:51 +0000

    Backport #33065 to 21.12: Merge #33050

--- src/Formats/NativeReader.cpp
+++ src/Formats/NativeReader.cpp
@@ -175,8 +175,8 @@ Block NativeReader::read()
             auto & header_column = header.getByName(column.name);
             if (!header_column.type->equals(*column.type))
             {
-                column.column = recursiveTypeConversion(column.column, column.type, header.getByPosition(i).type);
-                column.type = header.getByPosition(i).type;
+                column.column = recursiveTypeConversion(column.column, column.type, header.safeGetByPosition(i).type);
+                column.type = header.safeGetByPosition(i).type;
             }
         }
 
