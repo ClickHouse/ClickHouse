8b9d2f571c2d5a5495fd9a04cc61b237945f0cf9
Alexey Milovidov <milovidov@yandex-team.ru>
2021-12-25 17:20:33 +0300

    Fix style

--- tests/queries/0_stateless/02154_parser_backtracking.sh
+++ tests/queries/0_stateless/02154_parser_backtracking.sh
@@ -7,4 +7,4 @@ CURDIR=$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)
 # Should finish in reasonable time (milliseconds).
 # In previous versions this query led to exponential backtracking.
 
-echo 'SELECT '$(perl -e 'print "CAST(" x 100')'a b c'$(perl -e 'print ")" x 100') | ${CLICKHOUSE_LOCAL} --max_parser_depth 10000 2>&1 | grep -cF 'Syntax error'
+echo 'SELECT '"$(perl -e 'print "CAST(" x 100')"'a b c'"$(perl -e 'print ")" x 100')" | ${CLICKHOUSE_LOCAL} --max_parser_depth 10000 2>&1 | grep -cF 'Syntax error'
