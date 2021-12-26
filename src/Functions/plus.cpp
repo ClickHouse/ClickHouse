6734b592bb59939a7a01136ab24b4033154be2fc
feng lv <fenglv15@mails.ucas.ac.cn>
2021-12-26 09:04:12 +0000

    decimal plus float

--- src/Functions/plus.cpp
+++ src/Functions/plus.cpp
@@ -32,7 +32,12 @@ struct PlusImpl
     template <typename Result = ResultType>
     static inline bool apply(A a, B b, Result & c)
     {
-        return common::addOverflow(static_cast<Result>(a), b, c);
+        if constexpr (std::is_same_v<Result, double>)
+        {
+            c = static_cast<Result>(a) + b;
+        }
+        else
+            return common::addOverflow(static_cast<Result>(a), b, c);
     }
 
 #if USE_EMBEDDED_COMPILER
