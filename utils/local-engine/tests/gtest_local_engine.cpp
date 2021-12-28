bdca24736209df352a0ca93b0a541ca294269d7f
neng.liu <neng.liu@kyligence.io>
2021-12-28 03:49:37 +0000

    fix ut errors

--- utils/local-engine/tests/gtest_local_engine.cpp
+++ utils/local-engine/tests/gtest_local_engine.cpp
@@ -35,9 +35,7 @@ TEST(TestSelect, ReadRel)
     ASSERT_EQ(plan->relations_size(), 1);
     std::cout << "start execute" <<std::endl;
     dbms::LocalExecutor local_executor;
-    dbms::SerializedPlanParser::initFunctionEnv();
-    auto context =  dbms::Context::createGlobal(dbms::Context::createShared().get());
-    dbms::SerializedPlanParser parser(context);
+    dbms::SerializedPlanParser parser(dbms::SerializedPlanParser::global_context);
     auto query_plan = parser.parse(std::move(plan));
     local_executor.execute(std::move(query_plan));
     ASSERT_TRUE(local_executor.hasNext());
@@ -79,7 +77,6 @@ TEST(TestSelect, TestFilter)
     ASSERT_EQ(plan->relations_size(), 1);
     std::cout << "start execute" <<std::endl;
     dbms::LocalExecutor local_executor;
-    dbms::SerializedPlanParser::initFunctionEnv();
     dbms::SerializedPlanParser parser(SerializedPlanParser::global_context);
     auto query_plan = parser.parse(std::move(plan));
     local_executor.execute(std::move(query_plan));
@@ -126,7 +123,6 @@ TEST(TestSelect, TestAgg)
     ASSERT_EQ(plan->relations_size(), 1);
     std::cout << "start execute" <<std::endl;
     dbms::LocalExecutor local_executor;
-    dbms::SerializedPlanParser::initFunctionEnv();
     dbms::SerializedPlanParser parser(SerializedPlanParser::global_context);
     auto query_plan = parser.parse(std::move(plan));
     local_executor.execute(std::move(query_plan));
@@ -143,57 +139,6 @@ TEST(TestSelect, TestAgg)
     }
 }
 
-TEST(TestSelect, PerformanceTest)
-{
-
-    Stopwatch stopwatch;
-    stopwatch.start();
-    for (int i=0; i < 10; i++)
-    {
-        dbms::SerializedSchemaBuilder schema_builder;
-        auto *schema = schema_builder
-                          .column("l_orderkey", "I64")
-                          .column("l_partkey", "I64")
-                          .column("l_suppkey", "I64")
-                          .column("l_linenumber", "I32")
-                          .column("l_quantity", "FP64")
-                          .column("l_extendedprice", "FP64")
-                          .column("l_discount", "FP64")
-                          .column("l_tax", "FP64")
-                          //                      .column("l_returnflag", "String")
-                          //                      .column("l_linestatus", "String")
-                          .column("l_shipdate_new", "FP64")
-                          .column("l_commitdate_new", "FP64")
-                          .column("l_receiptdate_new", "FP64")
-                          //                      .column("l_shipinstruct", "String")
-                          //                      .column("l_shipmode", "String")
-                          //                      .column("l_comment", "String")
-                          .build();
-        dbms::SerializedPlanBuilder plan_builder;
-        auto plan = plan_builder.read("/home/kyligence/Documents/intel-gazelle-test.snappy.parquet", std::move(schema)).build();
-
-        ASSERT_TRUE(plan->relations(0).has_read());
-        ASSERT_EQ(plan->relations_size(), 1);
-        dbms::SerializedPlanParser parser(SerializedPlanParser::global_context);
-        auto query_plan = parser.parse(std::move(plan));
-        std::cout << "start execute" << std::endl;
-        dbms::LocalExecutor local_executor;
-
-        local_executor.execute(std::move(query_plan));
-        ASSERT_TRUE(local_executor.hasNext());
-        while (local_executor.hasNext())
-        {
-            local_engine::SparkRowInfoPtr spark_row_info = local_executor.next();
-            ASSERT_GT(spark_row_info->getNumRows(), 0);
-            std::cout << "fetch batch" << spark_row_info->getNumRows() << " rows"
-                      << ""
-                      << "" << std::endl;
-        }
-    }
-    auto duration = stopwatch.elapsedMilliseconds();
-    std::cout <<"duration:" << duration << std::endl;
-}
-
 TEST(TestSelect, MergeTreeWriteTest)
 {
 //    DB::StorageID id("default", "test");
@@ -204,15 +149,12 @@ TEST(TestSelect, MergeTreeWriteTest)
 //    auto merging_params = DB::MergeTreeData::MergingParams();
 //    auto storage_setting = std::make_unique<DB::MergeTreeSettings>();
 
-    Poco::URI uri("hdfs://clusterB/test.txt");
-    std::cout << uri.toString() << std::endl;
-    ASSERT_GT("clusterB", uri.getHost());
-
 //    DB::MergeTreeData(id, relative_path, storage_in_memory_metadata, global, "", merging_params, std::move(storage_setting), false, false, nullptr);
 }
 
 int main(int argc, char **argv)
 {
+    dbms::SerializedPlanParser::initFunctionEnv();
     ::testing::InitGoogleTest(&argc,argv);
     return RUN_ALL_TESTS();
 }
