6c5116be9d9c5b54c1b30933d4e8cf4d22c6d952
Anton Popov <pad11rus@gmail.com>
2021-12-29 00:43:33 +0300

    fix style check

--- tests/queries/0_stateless/02156_async_insert_query_log.sh
+++ tests/queries/0_stateless/02156_async_insert_query_log.sh
@@ -4,9 +4,6 @@ CURDIR=$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)
 # shellcheck source=../shell_config.sh
 . "$CURDIR"/../shell_config.sh
 
-url_wait="${CLICKHOUSE_URL}&async_insert=1&wait_for_async_insert=1"
-url_no_wait="${CLICKHOUSE_URL}&async_insert=1&wait_for_async_insert=1"
-
 ${CLICKHOUSE_CLIENT} -q "DROP TABLE IF EXISTS async_inserts_2156"
 ${CLICKHOUSE_CLIENT} -q "CREATE TABLE async_inserts_2156 (id UInt32, s String) ENGINE = Memory"
 
