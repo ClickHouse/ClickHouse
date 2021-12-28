ab057297029b9f851df13a316f66e7453e09d686
Maksim Kita <kitaetoya@gmail.com>
2021-12-28 23:49:41 +0300

    Fixed tests

--- tests/queries/0_stateless/02155_dictionary_comment.sql
+++ tests/queries/0_stateless/02155_dictionary_comment.sql
@@ -17,34 +17,37 @@ PRIMARY KEY id
 SOURCE(CLICKHOUSE(TABLE '02155_test_table'))
 LAYOUT(DIRECT());
 
-SELECT name, comment FROM system.dictionaries WHERE name == '02155_test_dictionary';
+SELECT name, comment FROM system.dictionaries WHERE name == '02155_test_dictionary' AND database == currentDatabase();
 
 ALTER TABLE 02155_test_dictionary COMMENT COLUMN value 'value_column'; --{serverError 48}
 
 ALTER TABLE 02155_test_dictionary MODIFY COMMENT '02155_test_dictionary_comment_0';
-SELECT name, comment FROM system.dictionaries WHERE name == '02155_test_dictionary';
-SELECT name, comment FROM system.tables WHERE name == '02155_test_dictionary';
+SELECT name, comment FROM system.dictionaries WHERE name == '02155_test_dictionary' AND database == currentDatabase();
+SELECT name, comment FROM system.tables WHERE name == '02155_test_dictionary' AND database == currentDatabase();
 
 SELECT * FROM 02155_test_dictionary;
-SELECT name, comment FROM system.dictionaries WHERE name == '02155_test_dictionary';
-SELECT name, comment FROM system.tables WHERE name == '02155_test_dictionary';
+SELECT name, comment FROM system.dictionaries WHERE name == '02155_test_dictionary' AND database == currentDatabase();
+SELECT name, comment FROM system.tables WHERE name == '02155_test_dictionary' AND database == currentDatabase();
 
 ALTER TABLE 02155_test_dictionary MODIFY COMMENT '02155_test_dictionary_comment_1';
-SELECT name, comment FROM system.dictionaries WHERE name == '02155_test_dictionary';
-SELECT name, comment FROM system.tables WHERE name == '02155_test_dictionary';
+SELECT name, comment FROM system.dictionaries WHERE name == '02155_test_dictionary' AND database == currentDatabase();
+SELECT name, comment FROM system.tables WHERE name == '02155_test_dictionary' AND database == currentDatabase();
 
+DROP TABLE IF EXISTS 02155_test_dictionary_view;
 CREATE TABLE 02155_test_dictionary_view
 (
     id UInt64,
     value String
-) ENGINE=Dictionary('02155_test_dictionary');
+) ENGINE=Dictionary(concat(currentDatabase(), '.02155_test_dictionary'));
 
 SELECT * FROM 02155_test_dictionary_view;
 
 ALTER TABLE 02155_test_dictionary_view COMMENT COLUMN value 'value_column'; --{serverError 48}
+
 ALTER TABLE 02155_test_dictionary_view MODIFY COMMENT '02155_test_dictionary_view_comment_0';
-SELECT name, comment FROM system.tables WHERE name == '02155_test_dictionary_view';
-SELECT name, comment FROM system.tables WHERE name == '02155_test_dictionary_view';
+SELECT name, comment FROM system.tables WHERE name == '02155_test_dictionary_view' AND database == currentDatabase();
+SELECT name, comment FROM system.tables WHERE name == '02155_test_dictionary_view' AND database == currentDatabase();
 
 DROP TABLE 02155_test_dictionary_view;
+DROP TABLE 02155_test_table;
 DROP DICTIONARY 02155_test_dictionary;
