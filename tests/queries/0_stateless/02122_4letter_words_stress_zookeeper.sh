b4e9762cec403381224b719194004c8b8c97f8e8
alesapin <alesapin@clickhouse.com>
2021-12-28 16:40:34 +0300

    Update 02122_4letter_words_stress_zookeeper.sh

--- tests/queries/0_stateless/02122_4letter_words_stress_zookeeper.sh
+++ tests/queries/0_stateless/02122_4letter_words_stress_zookeeper.sh
@@ -18,7 +18,7 @@ function four_letter_thread()
 function create_drop_thread()
 {
     while true; do
-        num=$RANDOM
+        num=$(($RANDOM % 10 + 1))
         $CLICKHOUSE_CLIENT --query "CREATE TABLE test_table$num (key UInt64, value1 UInt8, value2 UInt8) ENGINE = ReplicatedMergeTree('/clickhouse/tables/$CLICKHOUSE_TEST_ZOOKEEPER_PREFIX/test_table$num', '0') ORDER BY key"
         sleep 0.$RANDOM
         $CLICKHOUSE_CLIENT --query "DROP TABLE IF EXISTS test_table$num"
@@ -43,7 +43,7 @@ timeout $TIMEOUT bash -c create_drop_thread 2> /dev/null &
 
 wait
 
-for i in $(seq 1 10); do
+for num in $(seq 1 10); do
     $CLICKHOUSE_CLIENT --query "DROP TABLE IF EXISTS test_table$num" 2>/dev/null
     while  [ $? -ne 0 ]; do
         $CLICKHOUSE_CLIENT --query "DROP TABLE IF EXISTS test_table$num" 2>/dev/null
