86a925c45f8801cdab33d27e326e41517ae525ff
vdimir <vdimir@clickhouse.com>
2021-12-29 15:06:27 +0300

    Simplify case in test parse_date_lowcard_default_throw

--- tests/queries/0_stateless/02155_parse_date_lowcard_default_throw.sql
+++ tests/queries/0_stateless/02155_parse_date_lowcard_default_throw.sql
@@ -1,4 +1,1 @@
-SELECT parseDateTimeBestEffort(q0.date_field) AS parsed_date
-FROM (SELECT 1 AS pk1) AS t1
-INNER JOIN ( SELECT 1 AS pk1, toLowCardinality('15-JUL-16') AS date_field ) AS q0
-ON q0.pk1 = t1.pk1;
+SELECT parseDateTimeBestEffort(toLowCardinality(materialize('15-JUL-16')));
