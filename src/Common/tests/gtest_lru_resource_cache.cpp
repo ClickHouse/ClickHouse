6b6a82f3b9f707fc856393b12fe8fa521ac0e24f
lgbo-ustc <lgbo.ustc@gmail.com>
2021-12-29 18:18:38 +0800

    add MappedHolder to get cache values

--- src/Common/tests/gtest_lru_resource_cache.cpp
+++ src/Common/tests/gtest_lru_resource_cache.cpp
@@ -3,21 +3,21 @@
 #include <gtest/gtest.h>
 #include <Common/LRUResourceCache.h>
 
-TEST(LRUResourceCache, acquire)
+TEST(LRUResourceCache, get)
 {
     using MyCache = DB::LRUResourceCache<int, int>;
     auto mcache = MyCache(10, 10);
     int x = 10;
     auto load_int = [&] { return std::make_shared<int>(x); };
-    auto val = mcache.acquire(1, load_int);
+    auto holder1 = mcache.getOrSet(1, load_int);
     x = 11;
-    val = mcache.acquire(2, load_int);
-    ASSERT_TRUE(val != nullptr);
-    ASSERT_TRUE(*val == 11);
+    auto holder2 = mcache.getOrSet(2, load_int);
+    ASSERT_TRUE(holder2 != nullptr);
+    ASSERT_TRUE(holder2->value() == 11);
 
-    val = mcache.acquire(1);
-    ASSERT_TRUE(val != nullptr);
-    ASSERT_TRUE(*val == 10);
+    auto holder3 = mcache.get(1);
+    ASSERT_TRUE(holder3 != nullptr);
+    ASSERT_TRUE(holder3->value() == 10);
 }
 
 TEST(LRUResourceCache, remove)
@@ -26,27 +26,20 @@ TEST(LRUResourceCache, remove)
     auto mcache = MyCache(10, 10);
     int x = 10;
     auto load_int = [&] { return std::make_shared<int>(x); };
-    auto val = mcache.acquire(1, load_int);
-    x = 11;
-    val = mcache.acquire(2, load_int);
+    auto holder0 = mcache.getOrSet(1, load_int);
+    auto holder1 = mcache.getOrSet(1, load_int);
 
-    auto succ = mcache.tryRemove(3);
-    ASSERT_TRUE(succ);
-
-    succ = mcache.tryRemove(1);
+    auto succ = MyCache::MappedHolder::tryRemove(&holder0);
     ASSERT_TRUE(!succ);
-    val = mcache.acquire(1);
-    ASSERT_TRUE(val != nullptr);
-    ASSERT_TRUE(*val == 10);
+    holder0 = mcache.get(1);
+    ASSERT_TRUE(holder0 != nullptr);
+    ASSERT_TRUE(holder0->value() == 10);
 
-    mcache.release(1);
-    succ = mcache.tryRemove(1);
-    ASSERT_TRUE(!succ);
-    mcache.release(1);
-    succ = mcache.tryRemove(1);
+    holder0 = nullptr;
+    succ = MyCache::MappedHolder::tryRemove(&holder1);
     ASSERT_TRUE(succ);
-    val = mcache.acquire(1);
-    ASSERT_TRUE(val == nullptr);
+    holder1 = mcache.get(1);
+    ASSERT_TRUE(holder1 == nullptr);
 }
 
 struct MyWeight
@@ -60,27 +53,27 @@ TEST(LRUResourceCache, evict_on_weight)
     auto mcache = MyCache(5, 10);
     int x = 2;
     auto load_int = [&] { return std::make_shared<int>(x); };
-    auto val = mcache.acquire(1, load_int);
-    mcache.release(1);
+    auto holder1 = mcache.getOrSet(1, load_int);
+    holder1 = nullptr;
 
-    val = mcache.acquire(2, load_int);
-    mcache.release(2);
+    auto holder2 = mcache.getOrSet(2, load_int);
+    holder2 = nullptr;
 
     x = 3;
-    val = mcache.acquire(3, load_int);
-    ASSERT_TRUE(val != nullptr);
+    auto holder3 = mcache.getOrSet(3, load_int);
+    ASSERT_TRUE(holder3 != nullptr);
 
     auto w = mcache.weight();
     ASSERT_EQ(w, 5);
     auto n = mcache.size();
     ASSERT_EQ(n, 2);
 
-    val = mcache.acquire(1);
-    ASSERT_TRUE(val == nullptr);
-    val = mcache.acquire(2);
-    ASSERT_TRUE(val != nullptr);
-    val = mcache.acquire(3);
-    ASSERT_TRUE(val != nullptr);
+    holder1 = mcache.get(1);
+    ASSERT_TRUE(holder1 == nullptr);
+    holder2 = mcache.get(2);
+    ASSERT_TRUE(holder2 != nullptr);
+    holder3 = mcache.get(3);
+    ASSERT_TRUE(holder3 != nullptr);
 }
 
 TEST(LRUResourceCache, evict_on_weight_v2)
@@ -89,30 +82,30 @@ TEST(LRUResourceCache, evict_on_weight_v2)
     auto mcache = MyCache(5, 10);
     int x = 2;
     auto load_int = [&] { return std::make_shared<int>(x); };
-    auto val = mcache.acquire(1, load_int);
-    mcache.release(1);
+    auto holder1 = mcache.getOrSet(1, load_int);
+    holder1 = nullptr;
 
-    val = mcache.acquire(2, load_int);
-    mcache.release(2);
+    auto holder2 = mcache.getOrSet(2, load_int);
+    holder2 = nullptr;
 
-    val = mcache.acquire(1);
-    mcache.release(1);
+    holder1 = mcache.get(1);
+    holder1 = nullptr;
 
     x = 3;
-    val = mcache.acquire(3, load_int);
-    ASSERT_TRUE(val != nullptr);
+    auto holder3 = mcache.getOrSet(3, load_int);
+    ASSERT_TRUE(holder3 != nullptr);
 
     auto w = mcache.weight();
     ASSERT_EQ(w, 5);
     auto n = mcache.size();
     ASSERT_EQ(n, 2);
 
-    val = mcache.acquire(1);
-    ASSERT_TRUE(val != nullptr);
-    val = mcache.acquire(2);
-    ASSERT_TRUE(val == nullptr);
-    val = mcache.acquire(3);
-    ASSERT_TRUE(val != nullptr);
+    holder1 = mcache.get(1);
+    ASSERT_TRUE(holder1 != nullptr);
+    holder2 = mcache.get(2);
+    ASSERT_TRUE(holder2 == nullptr);
+    holder3 = mcache.get(3);
+    ASSERT_TRUE(holder3 != nullptr);
 }
 
 TEST(LRUResourceCache, evict_on_weight_v3)
@@ -121,30 +114,30 @@ TEST(LRUResourceCache, evict_on_weight_v3)
     auto mcache = MyCache(5, 10);
     int x = 2;
     auto load_int = [&] { return std::make_shared<int>(x); };
-    auto val = mcache.acquire(1, load_int);
-    mcache.release(1);
+    auto holder1 = mcache.getOrSet(1, load_int);
+    holder1 = nullptr;
 
-    val = mcache.acquire(2, load_int);
-    mcache.release(2);
+    auto holder2 = mcache.getOrSet(2, load_int);
+    holder2 = nullptr;
 
-    val = mcache.acquire(1, load_int);
-    mcache.release(1);
+    holder1 = mcache.getOrSet(1, load_int);
+    holder1 = nullptr;
 
     x = 3;
-    val = mcache.acquire(3, load_int);
-    ASSERT_TRUE(val != nullptr);
+    auto holder3 = mcache.getOrSet(3, load_int);
+    ASSERT_TRUE(holder3 != nullptr);
 
     auto w = mcache.weight();
     ASSERT_EQ(w, 5);
     auto n = mcache.size();
     ASSERT_EQ(n, 2);
 
-    val = mcache.acquire(1);
-    ASSERT_TRUE(val != nullptr);
-    val = mcache.acquire(2);
-    ASSERT_TRUE(val == nullptr);
-    val = mcache.acquire(3);
-    ASSERT_TRUE(val != nullptr);
+    holder1 = mcache.get(1);
+    ASSERT_TRUE(holder1 != nullptr);
+    holder2 = mcache.get(2);
+    ASSERT_TRUE(holder2 == nullptr);
+    holder3 = mcache.get(3);
+    ASSERT_TRUE(holder3 != nullptr);
 }
 
 TEST(LRUResourceCache, evict_on_size)
@@ -153,27 +146,27 @@ TEST(LRUResourceCache, evict_on_size)
     auto mcache = MyCache(5, 2);
     int x = 2;
     auto load_int = [&] { return std::make_shared<int>(x); };
-    auto val = mcache.acquire(1, load_int);
-    mcache.release(1);
+    auto holder1 = mcache.getOrSet(1, load_int);
+    holder1 = nullptr;
 
-    val = mcache.acquire(2, load_int);
-    mcache.release(2);
+    auto holder2 = mcache.getOrSet(2, load_int);
+    holder2 = nullptr;
 
     x = 3;
-    val = mcache.acquire(3, load_int);
-    ASSERT_TRUE(val != nullptr);
+    auto holder3 = mcache.getOrSet(3, load_int);
+    ASSERT_TRUE(holder3 != nullptr);
 
     auto n = mcache.size();
     ASSERT_EQ(n, 2);
     auto w = mcache.weight();
     ASSERT_EQ(w, 2);
 
-    val = mcache.acquire(1);
-    ASSERT_TRUE(val == nullptr);
-    val = mcache.acquire(2);
-    ASSERT_TRUE(val != nullptr);
-    val = mcache.acquire(3);
-    ASSERT_TRUE(val != nullptr);
+    holder1 = mcache.get(1);
+    ASSERT_TRUE(holder1 == nullptr);
+    holder2 = mcache.get(2);
+    ASSERT_TRUE(holder2 != nullptr);
+    holder3 = mcache.get(3);
+    ASSERT_TRUE(holder3 != nullptr);
 }
 
 TEST(LRUResourceCache, not_evict_used_element)
@@ -182,95 +175,95 @@ TEST(LRUResourceCache, not_evict_used_element)
     auto mcache = MyCache(7, 10);
     int x = 2;
     auto load_int = [&] { return std::make_shared<int>(x); };
-    auto val = mcache.acquire(1, load_int);
+    auto holder1 = mcache.getOrSet(1, load_int);
 
-    val = mcache.acquire(2, load_int);
-    mcache.release(2);
+    auto holder2 = mcache.getOrSet(2, load_int);
+    holder2 = nullptr;
 
-    val = mcache.acquire(3, load_int);
-    mcache.release(3);
+    auto holder3 = mcache.getOrSet(3, load_int);
+    holder3 = nullptr;
 
     x = 3;
-    val = mcache.acquire(4, load_int);
-    ASSERT_TRUE(val != nullptr);
+    auto holder4 = mcache.getOrSet(4, load_int);
+    ASSERT_TRUE(holder4 != nullptr);
 
     auto n = mcache.size();
     ASSERT_EQ(n, 3);
     auto w = mcache.weight();
     ASSERT_EQ(w, 7);
 
-    val = mcache.acquire(1);
-    ASSERT_TRUE(val != nullptr);
-    val = mcache.acquire(2);
-    ASSERT_TRUE(val == nullptr);
-    val = mcache.acquire(3);
-    ASSERT_TRUE(val != nullptr);
-    val = mcache.acquire(4);
-    ASSERT_TRUE(val != nullptr);
+    holder1 = mcache.get(1);
+    ASSERT_TRUE(holder1 != nullptr);
+    holder2 = mcache.get(2);
+    ASSERT_TRUE(holder2 == nullptr);
+    holder3 = mcache.get(3);
+    ASSERT_TRUE(holder3 != nullptr);
+    holder4 = mcache.get(4);
+    ASSERT_TRUE(holder4 != nullptr);
 }
 
-TEST(LRUResourceCache, acquire_fail)
+TEST(LRUResourceCache, get_fail)
 {
     using MyCache = DB::LRUResourceCache<int, int, MyWeight>;
     auto mcache = MyCache(5, 10);
     int x = 2;
     auto load_int = [&] { return std::make_shared<int>(x); };
-    auto val = mcache.acquire(1, load_int);
-    val = mcache.acquire(2, load_int);
-    val = mcache.acquire(3, load_int);
-    ASSERT_TRUE(val == nullptr);
+    auto holder1 = mcache.getOrSet(1, load_int);
+    auto holder2 = mcache.getOrSet(2, load_int);
+    auto holder3 = mcache.getOrSet(3, load_int);
+    ASSERT_TRUE(holder3 == nullptr);
 
     auto n = mcache.size();
     ASSERT_EQ(n, 2);
     auto w = mcache.weight();
     ASSERT_EQ(w, 4);
-    val = mcache.acquire(1);
-    ASSERT_TRUE(val != nullptr);
-    val = mcache.acquire(2);
-    ASSERT_TRUE(val != nullptr);
-    val = mcache.acquire(3);
-    ASSERT_TRUE(val == nullptr);
+    holder1 = mcache.get(1);
+    ASSERT_TRUE(holder1 != nullptr);
+    holder2 = mcache.get(2);
+    ASSERT_TRUE(holder2 != nullptr);
+    holder3 = mcache.get(3);
+    ASSERT_TRUE(holder3 == nullptr);
 }
 
-TEST(LRUResourceCache, dup_acquire)
+TEST(LRUResourceCache, dup_get)
 {
     using MyCache = DB::LRUResourceCache<int, int, MyWeight>;
     auto mcache = MyCache(20, 10);
     int x = 2;
     auto load_int = [&] { return std::make_shared<int>(x); };
-    auto val = mcache.acquire(1, load_int);
-    mcache.release(1);
+    auto holder1 = mcache.getOrSet(1, load_int);
+    holder1 = nullptr;
     x = 11;
-    val = mcache.acquire(1, load_int);
-    ASSERT_TRUE(val != nullptr);
+    holder1 = mcache.getOrSet(1, load_int);
+    ASSERT_TRUE(holder1 != nullptr);
 
     auto n = mcache.size();
     ASSERT_EQ(n, 1);
     auto w = mcache.weight();
     ASSERT_EQ(w, 2);
-    val = mcache.acquire(1);
-    ASSERT_TRUE(val != nullptr);
-    ASSERT_TRUE(*val == 2);
+    holder1 = mcache.get(1);
+    ASSERT_TRUE(holder1 != nullptr);
+    ASSERT_TRUE(holder1->value() == 2);
 }
 
-TEST(LRUResourceCache, re_acquire)
+TEST(LRUResourceCache, re_get)
 {
     using MyCache = DB::LRUResourceCache<int, int, MyWeight>;
     auto mcache = MyCache(20, 10);
     int x = 2;
     auto load_int = [&] { return std::make_shared<int>(x); };
-    auto val = mcache.acquire(1, load_int);
-    mcache.release(1);
-    mcache.tryRemove(1);
+    auto holder1 = mcache.getOrSet(1, load_int);
+    MyCache::MappedHolder::tryRemove(&holder1);
+
     x = 11;
-    val = mcache.acquire(1, load_int);
-    ASSERT_TRUE(val != nullptr);
+    holder1 = mcache.getOrSet(1, load_int);
+    ASSERT_TRUE(holder1 != nullptr);
 
     auto n = mcache.size();
     ASSERT_EQ(n, 1);
     auto w = mcache.weight();
     ASSERT_EQ(w, 11);
-    val = mcache.acquire(1);
-    ASSERT_TRUE(val != nullptr);
-    ASSERT_TRUE(*val == 11);
+    holder1 = mcache.get(1);
+    ASSERT_TRUE(holder1 != nullptr);
+    ASSERT_TRUE(holder1->value() == 11);
 }
