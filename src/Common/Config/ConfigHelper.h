f0e82e2cb64cfdcceb8077249786da2c555066cb
cmsxbc <i@cmsis.me>
2021-12-31 00:47:01 +0800

    add comment for ConfigHelper::getBool

--- src/Common/Config/ConfigHelper.h
+++ src/Common/Config/ConfigHelper.h
@@ -11,6 +11,8 @@ namespace Poco
 namespace DB::ConfigHelper
 {
 
+/// The behavior is like `config.getBool(key, default_)`,
+/// except when the tag is empty (aka. self-closing), `empty_as` will be used instead of throwing Poco::Exception.
 bool getBool(const Poco::Util::AbstractConfiguration & config, const std::string & key, bool default_, bool empty_as);
 
 }
