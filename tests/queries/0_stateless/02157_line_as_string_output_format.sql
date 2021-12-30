8a312e50120dfeef93916d791c7212c46f4d03c4
alexey-milovidov <milovidov@clickhouse.com>
2021-12-30 23:49:36 +0300

    Update 02157_line_as_string_output_format.sql

--- tests/queries/0_stateless/02157_line_as_string_output_format.sql
+++ tests/queries/0_stateless/02157_line_as_string_output_format.sql
@@ -1,1 +1,1 @@
-SELECT number * 246 + 10 AS n, toDate('2000-01-01') + n AS d, range(n) AS arr, (n, d) AS tuple FROM system.numbers LIMIT 2 FORMAT LineAsString;
+SELECT 'Hello \\ World' FORMAT LineAsString;
