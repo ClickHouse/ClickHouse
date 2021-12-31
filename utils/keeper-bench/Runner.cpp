1584c1350b96a1697726831bdf6961d716251ef8
zhanglistar <zhanglinuxstar@gmail.com>
2021-12-31 15:36:54 +0800

    1. fix compress timeedout problem.

--- utils/keeper-bench/Runner.cpp
+++ utils/keeper-bench/Runner.cpp
@@ -180,8 +180,8 @@ std::vector<std::shared_ptr<Coordination::ZooKeeper>> Runner::getConnections()
             "", /*identity type*/
             "", /*identity*/
             Poco::Timespan(0, 30000 * 1000),
-            Poco::Timespan(0, 1000 * 1000),
-            Poco::Timespan(0, 10000 * 1000),
+            Poco::Timespan(0, 3000 * 1000),
+            Poco::Timespan(0, 30000 * 1000),
             nullptr));
     }
 
