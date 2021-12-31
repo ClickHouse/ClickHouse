f65aeea8a9429a65f179f16c3242c385d6ecf59c
cmsxbc <i@cmsis.me>
2021-12-31 08:30:21 +0800

    use .empty() instead of .size() == 0

--- src/Common/Config/ConfigHelper.cpp
+++ src/Common/Config/ConfigHelper.cpp
@@ -13,7 +13,7 @@ bool getBool(const Poco::Util::AbstractConfiguration & config, const std::string
         return default_;
     Poco::Util::AbstractConfiguration::Keys sub_keys;
     config.keys(key, sub_keys);
-    if (sub_keys.size() == 0 && config.getString(key).size() == 0)
+    if (sub_keys.empty() && config.getString(key).empty())
         return empty_as;
     return config.getBool(key, default_);
 }
