8971c3a930a5b4f8ace8b441ffa8fb83f9533b4a
taiyang-li <654010905@qq.com>
2022-01-01 09:16:30 +0800

    fix hivte text

--- src/Processors/Formats/Impl/HiveTextRowInputFormat.cpp
+++ src/Processors/Formats/Impl/HiveTextRowInputFormat.cpp
@@ -26,7 +26,7 @@ HiveTextRowInputFormat::HiveTextRowInputFormat(
 HiveTextRowInputFormat::HiveTextRowInputFormat(
     const Block & header_, std::unique_ptr<PeekableReadBuffer> buf_, const Params & params_, const FormatSettings & format_settings_)
     : CSVRowInputFormat(
-        header_, *buf_, params_, true, false, format_settings_, std::make_unique<HiveTextFormatReader>(std::move(buf_), format_settings))
+        header_, *buf_, params_, true, false, format_settings_, std::make_unique<HiveTextFormatReader>(std::move(buf_), format_settings_))
 {
 }
 
