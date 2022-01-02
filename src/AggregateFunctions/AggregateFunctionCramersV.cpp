4a094c2efd48d9377f037172a37a060eecf3a534
Alexey Milovidov <milovidov@yandex-team.ru>
2022-01-02 21:50:41 +0300

    Merging contingency coefficients

--- src/AggregateFunctions/AggregateFunctionCramersV.cpp
+++ src/AggregateFunctions/AggregateFunctionCramersV.cpp
@@ -1,27 +1,56 @@
 #include <AggregateFunctions/AggregateFunctionFactory.h>
-#include <AggregateFunctions/AggregateFunctionCramersV.h>
+#include <AggregateFunctions/CrossTab.h>
 #include <AggregateFunctions/FactoryHelpers.h>
 #include <AggregateFunctions/Helpers.h>
-#include "registerAggregateFunctions.h"
 #include <memory>
+#include <cmath>
+
 
 namespace DB
 {
+
 namespace
 {
 
-AggregateFunctionPtr createAggregateFunctionCramersV(const std::string & name, const DataTypes & argument_types,
-                                                         const Array & parameters, const Settings *)
+struct CramersVData : CrossTabData
 {
-    assertNoParameters(name, parameters);
-    return std::make_shared<AggregateFunctionCramersV<AggregateFunctionCramersVData>>(argument_types);
-}
+    static const char * getName()
+    {
+        return "cramersV";
+    }
+
+    Float64 getResult() const
+    {
+        if (count < 2)
+            return std::numeric_limits<Float64>::quiet_NaN();
+
+        Float64 phi = 0.0;
+        for (const auto & [key, value_ab] : count_ab)
+        {
+            Float64 value_a = count_a.at(key.items[0]);
+            Float64 value_b = count_b.at(key.items[1]);
+
+            phi += value_ab * value_ab / (value_a * value_b) * count - 2 * value_ab + (value_a * value_b) / count;
+        }
+
+        phi /= count;
+        UInt64 q = std::min(count_a.size(), count_b.size());
+        phi /= q - 1;
+
+        return sqrt(phi);
+    }
+};
 
 }
 
 void registerAggregateFunctionCramersV(AggregateFunctionFactory & factory)
 {
-    factory.registerFunction("CramersV", createAggregateFunctionCramersV);
+    factory.registerFunction(CramersVData::getName(),
+ [](const std::string & name, const DataTypes & argument_types, const Array & parameters, const Settings *)
+        {
+            assertNoParameters(name, parameters);
+            return std::make_shared<AggregateFunctionCrossTab<CramersVData>>(argument_types);
+        });
 }
 
 }
--- src/AggregateFunctions/AggregateFunctionTheilsU.cpp
+++ src/AggregateFunctions/AggregateFunctionTheilsU.cpp
@@ -1,48 +1,43 @@
 #include <AggregateFunctions/AggregateFunctionFactory.h>
-#include <AggregateFunctions/AggregateFunctionCramersV.h>
+#include <AggregateFunctions/CrossTab.h>
 #include <AggregateFunctions/FactoryHelpers.h>
 #include <AggregateFunctions/Helpers.h>
-#include "registerAggregateFunctions.h"
 #include <memory>
+#include <cmath>
 
-namespace ErrorCodes
-{
-extern const int BAD_ARGUMENTS;
-}
 
 namespace DB
 {
+
 namespace
 {
 
-
-    struct TheilsUData : public AggregateFunctionCramersVData
+struct TheilsUData : CrossTabData
 {
-    Float64 get_result() const
+    static const char * getName()
     {
-        if (cur_size < 2){
-            throw Exception("Aggregate function theil's u requires at least 2 values in columns", ErrorCodes::BAD_ARGUMENTS);
-        }
-        Float64 h_x = 0.0;
-        for (const auto & cell : n_i) {
-            UInt64 count_x_tmp = cell.getMapped();
-            Float64 count_x = Float64(count_x_tmp);
-            h_x += (count_x / cur_size) * (log(count_x / cur_size));
-        }
+        return "theilsU";
+    }
 
+    Float64 getResult() const
+    {
+        if (count < 2)
+            return std::numeric_limits<Float64>::quiet_NaN();
+
+        Float64 h_a = 0.0;
+        for (const auto & [key, value] : count_a)
+        {
+            Float64 value_float = value;
+            h_a += (value_float / count) * log(value_float / count);
+        }
 
         Float64 dep = 0.0;
-        for (const auto & cell : pairs) {
-            UInt128 hash_pair = cell.getKey();
-            UInt64 count_of_pair_tmp = cell.getMapped();
-            Float64 count_of_pair = Float64(count_of_pair_tmp);
+        for (const auto & [key, value] : count_ab)
+        {
+            Float64 value_ab = value;
+            Float64 value_b = count_b.at(key.items[1]);
 
-            UInt64 hash2 = (hash_pair >> 64);
-
-            UInt64 count2_tmp = n_j.find(hash2)->getMapped();
-            Float64 count2 = Float64 (count2_tmp);
-
-            dep += (count_of_pair / cur_size) * log(count_of_pair / count2);
+            dep += (value_ab / count) * log(value_ab / value_b);
         }
 
         dep -= h_x;
@@ -51,18 +46,16 @@ namespace
     }
 };
 
-
-AggregateFunctionPtr createAggregateFunctionTheilsU(const std::string & name, const DataTypes & argument_types, const Array & parameters, const Settings *)
-{
-    assertNoParameters(name, parameters);
-    return std::make_shared<AggregateFunctionCramersV<TheilsUData>>(argument_types);
-}
-
-}
-
 void registerAggregateFunctionTheilsU(AggregateFunctionFactory & factory)
 {
-    factory.registerFunction("TheilsU", createAggregateFunctionTheilsU);
+    factory.registerFunction(TheilsUData::getName(),
+    [](const std::string & name, const DataTypes & argument_types, const Array & parameters, const Settings *)
+        {
+            assertNoParameters(name, parameters);
+            return std::make_shared<AggregateFunctionCrossTab<TheilsUData>>(argument_types);
+        });
+}
+
 }
 
 }
