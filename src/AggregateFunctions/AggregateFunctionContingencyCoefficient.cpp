49d0d1b57c19c926845080cc3330f2cf6c21e58a
alexey-milovidov <milovidov@clickhouse.com>
2022-01-03 16:45:02 +0300

    Fix CLion

--- src/AggregateFunctions/AggregateFunctionContingencyCoefficient.cpp
+++ src/AggregateFunctions/AggregateFunctionContingencyCoefficient.cpp
@@ -11,28 +11,29 @@ namespace DB
 namespace
 {
 
-    struct ContingencyData : CrossTabData
+struct ContingencyData : CrossTabData
+{
+    static const char * getName() { return "contingency"; }
+
+    Float64 getResult() const
     {
-        static const char * getName() { return "contingency"; }
+        if (count < 2)
+            return std::numeric_limits<Float64>::quiet_NaN();
 
-        Float64 getResult() const
+        Float64 phi = 0.0;
+        for (const auto & [key, value_ab] : count_ab)
         {
-            if (count < 2)
-                return std::numeric_limits<Float64>::quiet_NaN();
+            Float64 value_a = count_a.at(key.items[0]);
+            Float64 value_b = count_b.at(key.items[1]);
 
-            Float64 phi = 0.0;
-            for (const auto & [key, value_ab] : count_ab)
-            {
-                Float64 value_a = count_a.at(key.items[0]);
-                Float64 value_b = count_b.at(key.items[1]);
-
-                phi += value_ab * value_ab / (value_a * value_b) * count - 2 * value_ab + (value_a * value_b) / count;
-            }
-            phi /= count;
-
-            return sqrt(phi / (phi + count));
+            phi += value_ab * value_ab / (value_a * value_b) * count - 2 * value_ab + (value_a * value_b) / count;
         }
-    };
+        phi /= count;
+
+        return sqrt(phi / (phi + count));
+    }
+};
+
 }
 
 void registerAggregateFunctionContingency(AggregateFunctionFactory & factory)
