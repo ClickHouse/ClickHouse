f71fd85e3c1265076b17d4762d87a4bf4f07d29f
michael1589 <binghan1589@163.com>
2022-01-06 17:18:04 +0800

    replace with python on url auth test

--- tests/queries/0_stateless/02157_url_auth.sh
+++ tests/queries/0_stateless/02157_url_auth.sh
@@ -7,41 +7,12 @@ CURDIR=$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)
 . "$CURDIR"/../shell_config.sh
 
 # We should have correct env vars from shell_config.sh to run this test
+python3 "$CURDIR"/02157_url_auth.python &
 
-DOCKER_NAME="url_basic_auth_go_webserver"
-DOCKER_IMAGE="go/webserver:0105"
-cat <<EOF >> Dockerfile
-FROM golang:alpine as builder
+sleep 1
 
-RUN apk --no-cache add git
+${CLICKHOUSE_CLIENT} --query "select * from url('http://admin1:password@127.0.0.1:33339/example', 'RawBLOB', 'a String')" 2>&1 | grep Exception
+${CLICKHOUSE_CLIENT} --query "select * from url('http://admin2:password%2F@127.0.0.1:33339/example', 'RawBLOB', 'a String')" 2>&1 | grep Exception
+${CLICKHOUSE_CLIENT} --query "select * from url('http://admin3%3F%2F%3APassWord%5E%23%3F%2F@127.0.0.1:33339/example', 'RawBLOB', 'a String')" 2>&1 | grep Exception
+${CLICKHOUSE_CLIENT} --query "select * from url('http://admin4*%25%3Aok@127.0.0.1:33339/example', 'RawBLOB', 'a String')" 2>&1 | grep Exception
 
-WORKDIR /go/src/webserver
-
-COPY 02157_url_basic_auth.go .
-
-RUN go mod init
-
-RUN go mod tidy
-
-RUN CGO_ENABLED=0 go build -a -installsuffix cgo -o app .
-
-FROM alpine:latest as prod
-
-RUN apk --no-cache add ca-certificates
-
-WORKDIR /root/
-
-COPY --from=0 /go/src/webserver/app .
-
-CMD ["./app"]
-EOF
-
-docker build -t ${DOCKER_IMAGE} . >/dev/null
-docker run --rm -d --name ${DOCKER_NAME} -p 33339:33339 ${DOCKER_IMAGE} >/dev/null
-clickhouse-client --query "select * from url('http://admin1:password@127.0.0.1:33339/example', 'RawBLOB', 'a String')" 2>&1 | grep Exception
-clickhouse-client --query "select * from url('http://admin2:password%2F@127.0.0.1:33339/example', 'RawBLOB', 'a String')" 2>&1 | grep Exception
-clickhouse-client --query "select * from url('http://admin3%3F%2F%3APassWord%5E%23%3F%2F@127.0.0.1:33339/example', 'RawBLOB', 'a String')" 2>&1 | grep Exception
-clickhouse-client --query "select * from url('http://admin4*%25%3Aok@127.0.0.1:33339/example', 'RawBLOB', 'a String')" 2>&1 | grep Exception
-docker stop ${DOCKER_NAME}
-docker rmi ${DOCKER_IMAGE} >/dev/null
-rm -f Dockerfile
