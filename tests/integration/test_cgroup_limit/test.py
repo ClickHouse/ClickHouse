c157f7171a018ce97d98049afde8af506706d520
JaySon-Huang <tshent@qq.com>
2022-01-08 02:44:06 +0800

    Fix typo
    Signed-off-by: JaySon-Huang <tshent@qq.com>

--- tests/integration/test_cgroup_limit/test.py
+++ tests/integration/test_cgroup_limit/test.py
@@ -7,7 +7,7 @@ from tempfile import NamedTemporaryFile
 import pytest
 
 def run_command_in_container(cmd, *args):
-    # /clickhouse is mounted by interation tests runner
+    # /clickhouse is mounted by integration tests runner
     alternative_binary = os.getenv('CLICKHOUSE_BINARY', '/clickhouse')
     if alternative_binary:
         args += (
