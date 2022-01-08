cd00a00d017a7f860553b61c3ca147eacd21bee2
michael1589 <binghan1589@163.com>
2022-01-08 11:21:33 +0800

    kill the http server if exists before test

--- tests/queries/0_stateless/02157_url_auth.sh
+++ tests/queries/0_stateless/02157_url_auth.sh
@@ -7,12 +7,18 @@ CURDIR=$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)
 . "$CURDIR"/../shell_config.sh
 
 # We should have correct env vars from shell_config.sh to run this test
-python3 "$CURDIR"/02157_url_auth.python &
+export PORT=33339
+export PID=`lsof -i:${PORT} |awk '{print $2}'| tail -1`
+if [ -n "${PID}" ]; then
+    kill -9 ${PID}
+fi
+
+python3 "$CURDIR"/02157_url_auth.python ${PORT} &
 
 sleep 1
 
-${CLICKHOUSE_CLIENT} --query "select * from url('http://admin1:password@127.0.0.1:33339/example', 'RawBLOB', 'a String')" 2>&1 | grep Exception
-${CLICKHOUSE_CLIENT} --query "select * from url('http://admin2:password%2F@127.0.0.1:33339/example', 'RawBLOB', 'a String')" 2>&1 | grep Exception
-${CLICKHOUSE_CLIENT} --query "select * from url('http://admin3%3F%2F%3APassWord%5E%23%3F%2F@127.0.0.1:33339/example', 'RawBLOB', 'a String')" 2>&1 | grep Exception
-${CLICKHOUSE_CLIENT} --query "select * from url('http://admin4*%25%3Aok@127.0.0.1:33339/example', 'RawBLOB', 'a String')" 2>&1 | grep Exception
+${CLICKHOUSE_CLIENT} --query "select * from url('http://admin1:password@127.0.0.1:33339/example', 'RawBLOB', 'a String')" | grep Exception
+${CLICKHOUSE_CLIENT} --query "select * from url('http://admin2:password%2F@127.0.0.1:33339/example', 'RawBLOB', 'a String')" | grep Exception
+${CLICKHOUSE_CLIENT} --query "select * from url('http://admin3%3F%2F%3APassWord%5E%23%3F%2F@127.0.0.1:33339/example', 'RawBLOB', 'a String')" | grep Exception
+${CLICKHOUSE_CLIENT} --query "select * from url('http://admin4*%25%3Aok@127.0.0.1:33339/example', 'RawBLOB', 'a String')" | grep Exception
 
