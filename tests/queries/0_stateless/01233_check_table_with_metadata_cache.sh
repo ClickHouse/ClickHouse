8aab7df7f5d0790f22f368f0cd8ea4d4cafdf6b8
李扬 <654010905@qq.com>
2022-01-11 20:30:40 -0600

    Update tests/queries/0_stateless/01233_check_table_with_metadata_cache.sh
    Co-authored-by: Azat Khuzhin <a3at.mail@gmail.com>

--- tests/queries/0_stateless/01233_check_table_with_metadata_cache.sh
+++ tests/queries/0_stateless/01233_check_table_with_metadata_cache.sh
@@ -1,7 +1,8 @@
 #!/usr/bin/env bash
 # Tags: no-fasttest
 # Tag no-fasttest: setting use_metadata_cache=true is not supported in fasttest, because clickhouse binary in fasttest is build without RocksDB.
-
+# To suppress Warning messages from CHECK TABLE
+CLICKHOUSE_CLIENT_SERVER_LOGS_LEVEL=error
 CURDIR=$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)
 # shellcheck source=../shell_config.sh
 . "$CURDIR"/../shell_config.sh
