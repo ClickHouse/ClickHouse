9ceaaeaf91c52b9ac61455620c0ec9c6e96fd274
Alexander Tokmakov <avtokmakov@yandex-team.ru>
2022-01-12 20:19:24 +0300

    Revert "fix hang up with command 'drop table system.query_log sync' -- add test case"
    This reverts commit c5aa0bd23d07b9e6110ca8deb3813f5c61d44c7b.

--- tests/queries/0_stateless/02167_hang_in_drop_query_log_sync.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-
-CURDIR=$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)
-# shellcheck source=../shell_config.sh
-. "$CURDIR"/../shell_config.sh
-
-timeout 3s $CLICKHOUSE_CLIENT --receive_timeout=2 -q "DROP TABLE system.query_log sync;" 2>&1 | grep -o "Timeout exceeded"
