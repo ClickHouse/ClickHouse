fddcc30d4b8879accb8745cfb0b40fbce88355db
Kruglov Pavel <48961922+Avogar@users.noreply.github.com>
2022-01-13 15:26:30 +0300

    Update 02168_avro_bug.sql

--- tests/queries/0_stateless/02168_avro_bug.sql
+++ tests/queries/0_stateless/02168_avro_bug.sql
@@ -1,3 +1,4 @@
+-- Tags: no-fasttest
 insert into table function file('data.avro', 'Avro', 'x UInt64') select * from numbers(10);
 insert into table function file('data.avro', 'Avro', 'x UInt64') select * from numbers(10);
 insert into table function file('data.avro', 'Avro', 'x UInt64') select * from numbers(10);
