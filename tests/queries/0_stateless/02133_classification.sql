8c1c3f0ce816b59ef74db8fd3a3ca0d33052b482
Nikolay Degterinsky <evillique@gmail.com>
2022-01-19 10:31:04 +0000

    Small fix

--- tests/queries/0_stateless/02133_classification.sql
+++ tests/queries/0_stateless/02133_classification.sql
@@ -8,7 +8,10 @@ SELECT detectLanguage('Sweet are the uses of adversity which, like the toad, ugl
 SELECT detectLanguage('A vaincre sans peril, on triomphe sans gloire.');
 SELECT detectLanguage('二兎を追う者は一兎をも得ず');
 SELECT detectLanguage('有情饮水饱，无情食饭饥。');
+SELECT detectLanguage('*****///// _____ ,,,,,,,, .....');
 SELECT detectLanguageMixed('二兎を追う者は一兎をも得ず二兎を追う者は一兎をも得ず A vaincre sans peril, on triomphe sans gloire.');
+SELECT detectLanguageMixed('어디든 가치가 있는 곳으로 가려면 지름길은 없다');
+SELECT detectLanguageMixed('*****///// _____ ,,,,,,,, .....');
 
 SELECT detectCharset('Plain English');
 SELECT detectLanguageUnknown('Plain English');
