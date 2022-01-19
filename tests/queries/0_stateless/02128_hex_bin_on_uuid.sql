c2fa9f3159d2f02d8dd61ffbf9e8435d31c870c5
Frank Chen <frank.chen021@outlook.com>
2022-01-19 17:15:36 +0800

    Update test case
    Signed-off-by: Frank Chen <frank.chen021@outlook.com>

--- tests/queries/0_stateless/02128_hex_bin_on_uuid.sql
+++ tests/queries/0_stateless/02128_hex_bin_on_uuid.sql
@@ -2,17 +2,15 @@
 select length(hex(generateUUIDv4()));
 
 with generateUUIDv4() as uuid,
-    hex(reverse(reinterpretAsString(uuid))) as str1,
-    hex(uuid) as str2
+    replace(toString(uuid), '-', '') as str1,
+    lower(hex(uuid)) as str2
 select str1 = str2;
 
 -- hex on UUID always generate 32 characters even there're leading zeros
 select lower(hex(toUUID('00000000-80e7-46f8-0000-9d773a2fd319')));
 
-with generateUUIDv4() as uuid,
-    bin(reverse(reinterpretAsString(uuid))) as bin_str1,
-    bin(uuid) as bin_str2
-select bin_str1 = bin_str2;
+-- length should be 128
+select length(bin(generateUUIDv4()));
 
 -- bin on UUID always generate 128 characters even there're leading zeros
-select length(bin(toUUID('00000000-80e7-46f8-0000-9d773a2fd319')));
+select bin(toUUID('00000000-80e7-46f8-0000-9d773a2fd319'));
