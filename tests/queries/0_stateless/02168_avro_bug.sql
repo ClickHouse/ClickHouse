a166963d919b83ea1a0a04b1b0c7e239a6b69271
Kruglov Pavel <48961922+Avogar@users.noreply.github.com>
2022-01-19 13:14:03 +0300

    Update 02168_avro_bug.sql

--- tests/queries/0_stateless/02168_avro_bug.sql
+++ tests/queries/0_stateless/02168_avro_bug.sql
@@ -1,4 +1,4 @@
--- Tags: no-fasttest
+-- Tags: no-fasttest, no-parallel
 insert into table function file('data.avro', 'Parquet', 'x UInt64') select * from numbers(10);
 insert into table function file('data.avro', 'Parquet', 'x UInt64') select * from numbers(10); -- { serverError CANNOT_APPEND_TO_FILE }
 insert into table function file('data.avro', 'Parquet', 'x UInt64') select * from numbers(10); -- { serverError CANNOT_APPEND_TO_FILE }
