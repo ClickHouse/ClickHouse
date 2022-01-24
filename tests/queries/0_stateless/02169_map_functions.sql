a435c3a1e81b709a30d314d4dfdc0a3aaa5bfaa8
hexiaoting <hewenting_ict@163.com>
2022-01-24 18:33:13 +0800

    Fix mapFilter bug

--- tests/queries/0_stateless/02169_map_functions.sql
+++ tests/queries/0_stateless/02169_map_functions.sql
@@ -6,4 +6,5 @@ INSERT INTO table_map SELECT number, map('key3', number, 'key2', number + 1, 'ke
 SELECT mapFilter((k,v)->k like '%3' and v > 102, col) FROM table_map ORDER BY id;
 SELECT col, mapFilter((k, v) -> ((v % 10) > 1), col) FROM table_map ORDER BY id ASC;
 SELECT mapMap((k,v)->(k,v+1), col)  FROM table_map ORDER BY id;
+SELECT mapFilter((k,v)->0, col) from table_map;
 DROP TABLE table_map;
