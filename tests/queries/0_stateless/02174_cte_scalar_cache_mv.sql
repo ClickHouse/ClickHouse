163ce44f9530218715509b59fe2ab2be82928800
Raúl Marín <git@rmr.ninja>
2022-01-24 17:28:32 +0100

    Verify that independent subqueries in MVs are cached and reused

--- tests/queries/0_stateless/02174_cte_scalar_cache_mv.sql
+++ tests/queries/0_stateless/02174_cte_scalar_cache_mv.sql
@@ -58,10 +58,76 @@ WHERE
   AND query LIKE '-- FIRST INSERT\nINSERT INTO t1\n%'
   AND event_date >= yesterday() AND event_time > now() - interval 10 minute;
 
+DROP TABLE mv1;
+
+CREATE TABLE t3 (z Int64) ENGINE = Memory;
+CREATE MATERIALIZED VIEW mv2 TO t3 AS
+SELECT
+   -- This includes an unnecessarily complex query to verify that the local cache is used (since it uses t1)
+    sum(i) + sum(j) + (SELECT * FROM (SELECT min(i) + min(j) FROM (SELECT * FROM system.one _a, t1 _b))) AS z
+FROM t1;
+
+-- SECOND INSERT
+INSERT INTO t1
+SELECT 0 as i, number as j from numbers(100)
+SETTINGS
+    min_insert_block_size_rows=5,
+    max_insert_block_size=5,
+    min_insert_block_size_rows_for_materialized_views=5,
+    max_block_size=5,
+    max_threads=1;
+
+SELECT * FROM t3 ORDER BY z ASC;
+SYSTEM FLUSH LOGS;
+SELECT
+    '02177_MV_2',
+    ProfileEvents['ScalarSubqueriesGlobalCacheHit'] as scalar_cache_global_hit,
+    ProfileEvents['ScalarSubqueriesLocalCacheHit'] as scalar_cache_local_hit,
+    ProfileEvents['ScalarSubqueriesCacheMiss'] as scalar_cache_miss
+FROM system.query_log
+WHERE
+        current_database = currentDatabase()
+  AND type = 'QueryFinish'
+  AND query LIKE '-- SECOND INSERT\nINSERT INTO t1%'
+  AND event_date >= yesterday() AND event_time > now() - interval 10 minute;
+
+DROP TABLE mv2;
 
 
+CREATE TABLE t4 (z Int64) ENGINE = Memory;
+CREATE MATERIALIZED VIEW mv3 TO t4 AS
+SELECT
+    -- This includes an unnecessarily complex query but now it uses t2 so it can be cached
+    min(i) + min(j) + (SELECT * FROM (SELECT min(k) + min(l) FROM (SELECT * FROM system.one _a, t2 _b))) AS z
+FROM t1;
 
+-- THIRD INSERT
+INSERT INTO t1
+SELECT number as i, number as j from numbers(100)
+    SETTINGS
+    min_insert_block_size_rows=5,
+    max_insert_block_size=5,
+    min_insert_block_size_rows_for_materialized_views=5,
+    max_block_size=5,
+    max_threads=1;
+SYSTEM FLUSH LOGS;
 
--- MV SOURCE SHOULD USE LOCAL CACHE
--- MV SOURCE DEEP IN THE CALL SHOULD USE LOCAL CACHE
--- CHECK PERF TEST (EXISTING FOR SCALAR AND MAYBE ADD ONE WITH MVS)
+SELECT * FROM t4 ORDER BY z ASC;
+
+SELECT
+    '02177_MV_3',
+    ProfileEvents['ScalarSubqueriesGlobalCacheHit'] as scalar_cache_global_hit,
+    ProfileEvents['ScalarSubqueriesLocalCacheHit'] as scalar_cache_local_hit,
+    ProfileEvents['ScalarSubqueriesCacheMiss'] as scalar_cache_miss
+FROM system.query_log
+WHERE
+        current_database = currentDatabase()
+  AND type = 'QueryFinish'
+  AND query LIKE '-- THIRD INSERT\nINSERT INTO t1%'
+  AND event_date >= yesterday() AND event_time > now() - interval 10 minute;
+
+DROP TABLE mv3;
+DROP TABLE t1;
+DROP TABLE t2;
+DROP TABLE t3;
+DROP TABLE t4;
