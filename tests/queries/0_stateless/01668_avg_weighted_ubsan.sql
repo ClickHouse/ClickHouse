709a92ff059d61a788b26f90d503edc40f523dd5
alesapin <alesapin@gmail.com>
2022-01-25 11:22:17 +0300

    Remove flaky stateless tests

--- tests/queries/0_stateless/01668_avg_weighted_ubsan.sql
+++
@@ -1,4 +0,0 @@
-SELECT round(avgWeighted(x, y)) FROM (SELECT 1023 AS x, 1000000000 AS y UNION ALL SELECT 10 AS x, -9223372036854775808 AS y);
-select avgWeighted(number, toDecimal128(number, 9)) from numbers(0);
-select avg(toDecimal128(number, 9)) from numbers(0);
-select avgWeighted(number, toDecimal128(0, 9)) from numbers(10);
