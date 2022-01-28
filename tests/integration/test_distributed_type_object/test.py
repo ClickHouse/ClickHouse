a6ab19ce5850f5fce6ca1700ceb70b001b062394
Anton Popov <pad11rus@gmail.com>
2022-01-28 19:24:17 +0300

    fix reading missed subcolumns from distributed

--- tests/integration/test_distributed_type_object/test.py
+++ tests/integration/test_distributed_type_object/test.py
@@ -42,3 +42,16 @@ def test_distributed_type_object(started_cluster):
 
     expected = TSV("120\n")
     assert TSV(node1.query("SELECT sum(data.k2 * id) FROM dist_table")) == expected
+
+    node1.query("TRUNCATE TABLE local_table")
+    node2.query("TRUNCATE TABLE local_table")
+
+    node1.query('INSERT INTO local_table FORMAT JSONEachRow {"id": 1, "data": {"k1": "aa", "k2": {"k3": "bb", "k4": "c"}}} {"id": 2, "data": {"k1": "ee", "k5": "ff"}};')
+    node2.query('INSERT INTO local_table FORMAT JSONEachRow {"id": 3, "data": {"k5":"foo"}};')
+
+    expected = TSV("""
+1\taa\tbb\tc\t
+2\tee\t\t\tff
+3\t\t\t\tfoo""")
+
+    assert TSV(node1.query("SELECT id, data.k1, data.k2.k3, data.k2.k4, data.k5 FROM dist_table ORDER BY id")) == expected
