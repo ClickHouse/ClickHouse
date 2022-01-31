4c9ba1dfd700141867feab1a98908c36b4160f62
Maksim Kita <maksim-kita@clickhouse.com>
2022-01-31 22:25:35 +0000

    Fixed tests

--- tests/queries/0_stateless/01780_column_sparse_alter.sql
+++ tests/queries/0_stateless/01780_column_sparse_alter.sql
@@ -12,14 +12,14 @@ INSERT INTO t_sparse_alter SELECT
     if (number % 13 = 0, toString(number), '')
 FROM numbers(2000);
 
-SELECT column, serialization_kind FROM system.parts_columns WHERE database = currentDatabase() AND table = 't_sparse_alter' AND active ORDER BY name;
+SELECT column, serialization_kind FROM system.parts_columns WHERE database = currentDatabase() AND table = 't_sparse_alter' AND active ORDER BY column;
 
 SELECT uniqExact(u), uniqExact(s) FROM t_sparse_alter;
 
 ALTER TABLE t_sparse_alter DROP COLUMN s, RENAME COLUMN u TO t;
 ALTER TABLE t_sparse_alter MODIFY COLUMN t UInt16;
 
-SELECT column, serialization_kind FROM system.parts_columns WHERE database = currentDatabase() AND table = 't_sparse_alter' AND active ORDER BY name;
+SELECT column, serialization_kind FROM system.parts_columns WHERE database = currentDatabase() AND table = 't_sparse_alter' AND active ORDER BY column;
 
 SELECT uniqExact(t) FROM t_sparse_alter;
 
