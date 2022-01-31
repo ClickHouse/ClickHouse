516d9ca6b16aeb54a26b44974f399e5de845c0db
Yatsishin Ilya <2159081+qoega@users.noreply.github.com>
2022-01-31 17:11:15 +0000

    add initial test

---
+++ tests/queries/0_stateless/02184_default_table_engine.sql
@@ -0,0 +1,17 @@
+CREATE TABLE table_02184 (x UInt8); --{serverError 119}
+SET default_table_engine = 'Log';
+CREATE TABLE table_02184 (x UInt8);
+DESCRIBE TABLE table_02184;
+DROP TABLE table_02184;
+SET default_table_engine = 'MergeTree';
+CREATE TABLE table_02184 (x UInt8); --{serverError 42}
+CREATE TABLE table_02184 (x UInt8, PRIMARY KEY (x));
+DESCRIBE TABLE table_02184;
+DROP TABLE table_02184;
+CREATE TABLE table_02184 (x UInt8) PARTITION BY x; --{serverError 36}
+CREATE TABLE table_02184 (x UInt8) ORDER BY x;
+DESCRIBE TABLE table_02184;
+DROP TABLE table_02184;
+CREATE TABLE table_02184 (x UInt8) PRIMARY KEY x;
+DESCRIBE TABLE table_02184;
+DROP TABLE table_02184;
