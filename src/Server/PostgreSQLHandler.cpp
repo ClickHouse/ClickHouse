98365f431a04f83650510bed4ffbd796e1e67381
Azat Khuzhin <a3at.mail@gmail.com>
2022-02-01 13:17:43 +0300

    Use fmt::format over Poco::format in PostgreSQLHandler
    Signed-off-by: Azat Khuzhin <a.khuzhin@semrush.com>

--- src/Server/PostgreSQLHandler.cpp
+++ src/Server/PostgreSQLHandler.cpp
@@ -222,7 +222,7 @@ void PostgreSQLHandler::cancelRequest()
     std::unique_ptr<PostgreSQLProtocol::Messaging::CancelRequest> msg =
         message_transport->receiveWithPayloadSize<PostgreSQLProtocol::Messaging::CancelRequest>(8);
 
-    String query = Poco::format("KILL QUERY WHERE query_id = 'postgres:%d:%d'", msg->process_id, msg->secret_key);
+    String query = fmt::format("KILL QUERY WHERE query_id = 'postgres:{:d}:{:d}'", msg->process_id, msg->secret_key);
     ReadBufferFromString replacement(query);
 
     auto query_context = session->makeQueryContext();
@@ -287,7 +287,7 @@ void PostgreSQLHandler::processQuery()
         {
             secret_key = dis(gen);
             auto query_context = session->makeQueryContext();
-            query_context->setCurrentQueryId(Poco::format("postgres:%d:%d", connection_id, secret_key));
+            query_context->setCurrentQueryId(fmt::format("postgres:{:d}:{:d}", connection_id, secret_key));
 
             CurrentThread::QueryScope query_scope{query_context};
             ReadBufferFromString read_buf(spl_query);
