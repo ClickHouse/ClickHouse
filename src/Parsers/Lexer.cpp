a6807b45faadf5b081d6264b323c9f9991fa2462
Aaron Katz <95644870+aaronstephenkatz@users.noreply.github.com>
2022-02-01 09:40:17 -0800

    adding # as a recognized start of single line comment

--- src/Parsers/Lexer.cpp
+++ src/Parsers/Lexer.cpp
@@ -280,6 +280,18 @@ Token Lexer::nextTokenImpl()
             }
             return Token(TokenType::Slash, token_begin, pos);
         }
+        case '#':   /// start of single line comment, MySQL style
+        {           /// PostgreSQL has some operators using '#' character.
+                    /// For less ambiguity, we will recognize a comment only if # is followed by whitespace.
+                    /// or #! as a special case for "shebang".
+                    /// #hello - not a comment
+                    /// # hello - a comment
+                    /// #!/usr/bin/clickhouse-local --queries-file - a comment
+            ++pos;
+            if (pos < end && (*pos == ' ' || *pos == '!'))
+                return comment_until_end_of_line();
+            return Token(TokenType::Error, token_begin, pos);
+        }
         case '%':
             return Token(TokenType::Percent, token_begin, ++pos);
         case '=':   /// =, ==
