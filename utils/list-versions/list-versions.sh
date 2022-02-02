5ceca04976894b971669cb3e9d691e241f55e874
Mikhail f. Shiryaev <felixoid@clickhouse.com>
2022-02-02 16:04:19 +0100

    Replace custom formats by `short` alias

--- utils/list-versions/list-versions.sh
+++ utils/list-versions/list-versions.sh
@@ -2,4 +2,4 @@
 
 # refname:strip=2: default tag name when format is not set
 # creatordate is always defined for all tags
-git tag --list 'v*-lts' 'v*-stable' --format='%(refname:strip=2)	%(creatordate:format:%F)' | sort -rV
+git tag --list 'v*-lts' 'v*-stable' --format='%(refname:short)	%(creatordate:short)' | sort -rV
