7e323f4b06f0b18482bd36a52a663d282c0f513f
Saad Ur Rahman <saadurrahman@apache.org>
2022-02-04 23:16:14 -0500

    [IO] HTTP User-Agent header
    Added HTTP User-Agent Header to HTTP requests.
    
    User-Agent: ClickHouse/VERSION_STRING

--- src/IO/ReadWriteBufferFromHTTP.h
+++ src/IO/ReadWriteBufferFromHTTP.h
@@ -19,6 +19,7 @@
 #include <Common/DNSResolver.h>
 #include <Common/RemoteHostFilter.h>
 #include <Common/config.h>
+#include <Common/config_version.h>
 #include <base/logger_useful.h>
 #include <Poco/URIStreamFactory.h>
 
@@ -275,6 +276,8 @@ namespace detail
                                 "must be http_max_tries >= 1 (current is {}) and "
                                 "0 < http_retry_initial_backoff_ms < settings.http_retry_max_backoff_ms (now 0 < {} < {})",
                                 settings.http_max_tries, settings.http_retry_initial_backoff_ms, settings.http_retry_max_backoff_ms);
+
+            http_header_entries.emplace_back(std::make_pair("User-Agent", fmt::format("ClickHouse/{}", VERSION_STRING)));
 
             if (!delay_initialization)
                 initialize();
