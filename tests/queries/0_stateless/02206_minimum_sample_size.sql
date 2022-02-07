73d712e6c3cf4310faa2be10cfd4b19a704ab225
achimbab <07c00h@gmail.com>
2022-02-07 11:25:37 +0900

    Minor fix for tests

--- tests/queries/0_stateless/02206_minimum_sample_size.sql
+++ tests/queries/0_stateless/02206_minimum_sample_size.sql
@@ -4,7 +4,7 @@ DROP TABLE IF EXISTS minimum_sample_size_continuos;
 CREATE TABLE minimum_sample_size_continuos (baseline Float64, sigma Float64, mde Float64, power Float64, alpha Float64) ENGINE = Memory();
 INSERT INTO minimum_sample_size_continuos VALUES (20.0, 10.0, 0.05, 0.8, 0.05);
 INSERT INTO minimum_sample_size_continuos VALUES (200.0, 10.0, 0.05, 0.85, 0.10);
-WITH minSampleSizeContinous(baseline, sigma, mde, power, alpha) AS res SELECT res.1, roundBankers(res.2, 2), roundBankers(res.3, 2) FROM minimum_sample_size_continuos;
+WITH minSampleSizeContinous(baseline, sigma, mde, power, alpha) AS res SELECT res.1, roundBankers(res.2, 2), roundBankers(res.3, 2) FROM minimum_sample_size_continuos ORDER BY res.1;
 DROP TABLE IF EXISTS minimum_sample_size_continuos;
 
 WITH minSampleSizeConversion(0.9, 0.01, 0.8, 0.05) AS res SELECT res.1, roundBankers(res.2, 2), roundBankers(res.3, 2);
@@ -13,5 +13,5 @@ DROP TABLE IF EXISTS minimum_sample_size_conversion;
 CREATE TABLE minimum_sample_size_conversion (p1 Float64, mdes Float64, power Float64, alpha Float64) ENGINE = Memory();
 INSERT INTO minimum_sample_size_conversion VALUES (0.9, 0.01, 0.8, 0.05);
 INSERT INTO minimum_sample_size_conversion VALUES (0.8, 0.02, 0.85, 0.10);
-WITH minSampleSizeConversion(p1, mdes, power, alpha) AS res SELECT res.1, roundBankers(res.2, 2), roundBankers(res.3, 2) FROM minimum_sample_size_conversion;
+WITH minSampleSizeConversion(p1, mdes, power, alpha) AS res SELECT res.1, roundBankers(res.2, 2), roundBankers(res.3, 2) FROM minimum_sample_size_conversion ORDER BY res.1;
 DROP TABLE IF EXISTS minimum_sample_size_conversion;
