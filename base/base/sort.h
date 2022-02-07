10293acafbd40099fe4bdd8c490e72c00ab67d92
Maksim Kita <maksim-kita@clickhouse.com>
2022-02-07 21:34:40 +0000

    Sort added equal items ranges randomization

--- base/base/sort.h
+++ base/base/sort.h
@@ -2,6 +2,89 @@
 
 #include <pdqsort.h>
 
+#ifdef NDEBUG
+
+#include <pcg_random.hpp>
+
+/** Same as libcxx std::__debug_less. Just without dependency on private part of standard library.
+  * Check that Comparator induce strict weak ordering.
+  */
+template <typename Comparator>
+class DebugLessComparator
+{
+public:
+    constexpr DebugLessComparator(Comparator & cmp_)
+        : cmp(cmp_)
+    {}
+
+    template <typename LhsType, typename RhsType>
+    constexpr bool operator()(const LhsType & lhs, const RhsType & rhs)
+    {
+        bool lhs_less_than_rhs = cmp(lhs, rhs);
+        if (lhs_less_than_rhs)
+            assert(!cmp(rhs, lhs));
+
+        return lhs_less_than_rhs;
+    }
+
+    template <typename LhsType, typename RhsType>
+    constexpr bool operator()(LhsType & lhs, RhsType & rhs)
+    {
+        bool lhs_less_than_rhs = cmp(lhs, rhs);
+        if (lhs_less_than_rhs)
+            assert(!cmp(rhs, lhs));
+
+        return lhs_less_than_rhs;
+    }
+
+private:
+    Comparator & cmp;
+};
+
+template <typename Comparator>
+using ComparatorWrapper = DebugLessComparator<Comparator>;
+
+template <typename RandomIt, typename Compare>
+void shuffleItemsInEqualRanges(RandomIt first, RandomIt last, Compare compare)
+{
+    static pcg64 rng(randomSeed());
+
+    bool equal_range_started = false;
+    RandomIt equal_range_start_it;
+
+    while (true)
+    {
+        auto next = first + 1;
+        if (next == last)
+            break;
+
+        bool first_equal_second = compare(*first, *next) == compare(*next, *first);
+
+        if (equal_range_started && !first_equal_second)
+        {
+            std::shuffle(equal_range_start_it, next, rng);
+            equal_range_started = false;
+        }
+        else if (!equal_range_started && first_equal_second)
+        {
+            equal_range_started = true;
+            equal_range_start_it = first;
+        }
+
+        ++first;
+    }
+
+    if (equal_range_started)
+        std::shuffle(equal_range_start_it, last, rng);
+}
+
+#else
+
+template <typename Comparator>
+using ComparatorWrapper = Comparator;
+
+#endif
+
 #pragma GCC diagnostic push
 #pragma GCC diagnostic ignored "-Wold-style-cast"
 
@@ -10,19 +93,33 @@
 template <typename RandomIt>
 void nth_element(RandomIt first, RandomIt nth, RandomIt last)
 {
-    ::miniselect::floyd_rivest_select(first, nth, last);
-}
+    using value_type = typename std::iterator_traits<RandomIt>::value_type;
+    using comparator = std::less<value_type>;
 
-template <typename RandomIt>
-void partial_sort(RandomIt first, RandomIt middle, RandomIt last)
-{
-    ::miniselect::floyd_rivest_partial_sort(first, middle, last);
+    comparator compare;
+    ComparatorWrapper<comparator> compare_wrapper = compare;
+
+    ::miniselect::floyd_rivest_select(first, nth, last, compare_wrapper);
 }
 
 template <typename RandomIt, typename Compare>
 void partial_sort(RandomIt first, RandomIt middle, RandomIt last, Compare compare)
 {
-    ::miniselect::floyd_rivest_partial_sort(first, middle, last, compare);
+    ComparatorWrapper<Compare> compare_wrapper = compare;
+    ::miniselect::floyd_rivest_partial_sort(first, middle, last, compare_wrapper);
+
+#ifdef NDEBUG
+    ::shuffleItemsInEqualRanges(first, middle, compare_wrapper);
+#endif
+}
+
+
+template <typename RandomIt>
+void partial_sort(RandomIt first, RandomIt middle, RandomIt last)
+{
+    using value_type = typename std::iterator_traits<RandomIt>::value_type;
+    using comparator = std::less<value_type>;
+    ::partial_sort(first, middle, last, comparator());
 }
 
 #pragma GCC diagnostic pop
@@ -30,7 +127,12 @@ void partial_sort(RandomIt first, RandomIt middle, RandomIt last, Compare compar
 template <typename RandomIt, typename Compare>
 void sort(RandomIt first, RandomIt last, Compare compare)
 {
-    ::pdqsort(first, last, compare);
+    ComparatorWrapper<Compare> compare_wrapper = compare;
+    ::pdqsort(first, last, compare_wrapper);
+
+#ifdef NDEBUG
+    ::shuffleItemsInEqualRanges(first, last, compare_wrapper);
+#endif
 }
 
 template <typename RandomIt>
@@ -38,5 +140,5 @@ void sort(RandomIt first, RandomIt last)
 {
     using value_type = typename std::iterator_traits<RandomIt>::value_type;
     using comparator = std::less<value_type>;
-    ::pdqsort(first, last, comparator());
+    ::sort(first, last, comparator());
 }
