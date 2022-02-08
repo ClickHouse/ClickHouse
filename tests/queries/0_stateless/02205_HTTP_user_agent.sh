e17ce0726318d19f8741e95a22f7d3a9e58ddc41
Saad Ur Rahman <saadurrahman@apache.org>
2022-02-08 14:22:39 -0500

    [Tests] HTTP User Agent
    Added noop logger in <HttpProcessor> to suppress <stderr> output logging on inbound connections. Pipe to </dev/null> in shell script no longer needed.

--- tests/queries/0_stateless/02205_HTTP_user_agent.sh
+++ tests/queries/0_stateless/02205_HTTP_user_agent.sh
@@ -4,5 +4,5 @@ CURDIR=$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)
 # shellcheck source=../shell_config.sh
 . "$CURDIR"/../shell_config.sh
 
-python3 "$CURDIR"/02205_HTTP_user_agent.python 2>/dev/null
+python3 "$CURDIR"/02205_HTTP_user_agent.python
 
