52af9dd14ab2b532158f63db569342758f3d53ff
Saad Ur Rahman <saadurrahman@apache.org>
2022-02-08 11:44:51 -0500

    [Tests] HTTP User Agent
    Un-commented line required for test runner.

--- tests/queries/0_stateless/02205_HTTP_user_agent.sh
+++ tests/queries/0_stateless/02205_HTTP_user_agent.sh
@@ -2,7 +2,7 @@
 
 CURDIR=$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)
 # shellcheck source=../shell_config.sh
-#. "$CURDIR"/../shell_config.sh
+. "$CURDIR"/../shell_config.sh
 
 python3 "$CURDIR"/02205_HTTP_user_agent.python 2>/dev/null
 
