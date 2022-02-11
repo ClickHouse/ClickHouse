77ce66a90ce213612b14e786812bc10d26860dbb
Azat Khuzhin <a3at.mail@gmail.com>
2022-02-11 08:14:42 +0300

    tests: update 01505_pipeline_executor_UAF after jemalloc update
    Signed-off-by: Azat Khuzhin <a.khuzhin@semrush.com>

--- tests/queries/0_stateless/01505_pipeline_executor_UAF.sh
+++ tests/queries/0_stateless/01505_pipeline_executor_UAF.sh
@@ -5,14 +5,13 @@ CURDIR=$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)
 # shellcheck source=../shell_config.sh
 . "$CURDIR"/../shell_config.sh
 
-# In debug build abort_conf:true is set by default, disable it explicitly
-# to avoid "Number of CPUs is not deterministic" error from jemalloc.
-export MALLOC_CONF=abort_conf:false
+# Avoid failures due to "Number of CPUs is not deterministic" error from jemalloc.
+export MALLOC_CONF=abort_conf:false,abort:false
 
 # Regression for UAF in ThreadPool.
 # (Triggered under TSAN)
 for _ in {1..10}; do
     ${CLICKHOUSE_LOCAL} -q 'select * from numbers_mt(100000000) settings max_threads=100 FORMAT Null'
     # Binding to specific CPU is not required, but this makes the test more reliable.
-    taskset --cpu-list 0 ${CLICKHOUSE_LOCAL} -q 'select * from numbers_mt(100000000) settings max_threads=100 FORMAT Null'
+    taskset --cpu-list 0 ${CLICKHOUSE_LOCAL} -q 'select * from numbers_mt(100000000) settings max_threads=100 FORMAT Null' 2>&1
 done
