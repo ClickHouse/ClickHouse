1d99e04551819abada26cf864b16493640ade652
Azat Khuzhin <a3at.mail@gmail.com>
2022-02-11 13:04:15 +0300

    tests: make 01505_pipeline_executor_UAF compatible with sanitizers builds
    Signed-off-by: Azat Khuzhin <a.khuzhin@semrush.com>

--- tests/queries/0_stateless/01505_pipeline_executor_UAF.sh
+++ tests/queries/0_stateless/01505_pipeline_executor_UAF.sh
@@ -13,5 +13,11 @@ export MALLOC_CONF=abort_conf:false,abort:false
 for _ in {1..10}; do
     ${CLICKHOUSE_LOCAL} -q 'select * from numbers_mt(100000000) settings max_threads=100 FORMAT Null'
     # Binding to specific CPU is not required, but this makes the test more reliable.
-    taskset --cpu-list 0 ${CLICKHOUSE_LOCAL} -q 'select * from numbers_mt(100000000) settings max_threads=100 FORMAT Null' 2>&1
+    taskset --cpu-list 0 ${CLICKHOUSE_LOCAL} -q 'select * from numbers_mt(100000000) settings max_threads=100 FORMAT Null' 2>&1 | {
+        # build with santiziers does not have jemalloc
+        # and for jemalloc we have separate test
+        # 01502_jemalloc_percpu_arena
+        grep -v '<jemalloc>: Number of CPUs detected is not deterministic. Per-CPU arena disabled.'
+    }
 done
+exit 0
