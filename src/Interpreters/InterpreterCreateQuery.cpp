4a9d0c84df0a1e65dea96ec1ebd893d505c32a8d
tavplubix <tavplubix@gmail.com>
2022-02-11 13:59:13 +0300

    Update InterpreterCreateQuery.cpp

--- src/Interpreters/InterpreterCreateQuery.cpp
+++ src/Interpreters/InterpreterCreateQuery.cpp
@@ -662,7 +662,7 @@ InterpreterCreateQuery::TableProperties InterpreterCreateQuery::getTableProperti
     }
     /// We can have queries like "CREATE TABLE <table> ENGINE=<engine>" if <engine>
     /// supports schema inference (will determine table structure in it's constructor).
-    else if (!StorageFactory::instance().checkIfStorageSupportsSchemaInterface(create.storage->engine->name))
+    else if (!StorageFactory::instance().checkIfStorageSupportsSchemaInterface(create.storage->engine->name)) // NOLINT
         throw Exception("Incorrect CREATE query: required list of column descriptions or AS section or SELECT.", ErrorCodes::INCORRECT_QUERY);
 
     /// Even if query has list of columns, canonicalize it (unfold Nested columns).
