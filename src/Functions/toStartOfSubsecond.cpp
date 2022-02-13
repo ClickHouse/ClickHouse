196ffb4827733be407a36dbd9537d3bddfe58820
zvonand <azvonov@altinity.com>
2022-02-13 18:41:45 +0300

    toStartOf[...]second works

--- src/Functions/toStartOfSubsecond.cpp
+++ src/Functions/toStartOfSubsecond.cpp
@@ -24,7 +24,7 @@ using FunctionToStartOfNanosecond = FunctionDateOrDateTimeToSomething<DataTypeDa
 
 void registerFunctionToStartOfNanosecond(FunctionFactory & factory)
 {
-    factory.registerFunction<FunctionToStartOfMillisecond>();
+    factory.registerFunction<FunctionToStartOfNanosecond>();
 }
 
 }
