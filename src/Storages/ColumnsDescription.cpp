fe2f04cf3ce400bfc5670a9be35c8289da0ed97f
Anton Popov <pad11rus@gmail.com>
2022-02-16 02:42:50 +0300

    fix quadratic complexity while adding subcolumns

--- src/Storages/ColumnsDescription.cpp
+++ src/Storages/ColumnsDescription.cpp
@@ -652,17 +652,16 @@ ColumnsDescription ColumnsDescription::parse(const String & str)
 
 void ColumnsDescription::addSubcolumns(const String & name_in_storage, const DataTypePtr & type_in_storage)
 {
-    for (const auto & subcolumn_name : type_in_storage->getSubcolumnNames())
+    IDataType::forEachSubcolumn([&](const auto &, const auto & subname, const auto & subdata)
     {
-        auto subcolumn = NameAndTypePair(name_in_storage, subcolumn_name,
-            type_in_storage, type_in_storage->getSubcolumnType(subcolumn_name));
+        auto subcolumn = NameAndTypePair(name_in_storage, subname, type_in_storage, subdata.type);
 
         if (has(subcolumn.name))
             throw Exception(ErrorCodes::ILLEGAL_COLUMN,
                 "Cannot add subcolumn {}: column with this name already exists", subcolumn.name);
 
         subcolumns.get<0>().insert(std::move(subcolumn));
-    }
+    }, {type_in_storage->getDefaultSerialization(), type_in_storage, nullptr, nullptr});
 }
 
 void ColumnsDescription::removeSubcolumns(const String & name_in_storage)
