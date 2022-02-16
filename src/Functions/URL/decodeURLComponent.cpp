bf693581801c4ba0222861a486ac3ca56c965aef
zzsmdfj <zzsmdfj@gmail.com>
2022-02-16 10:19:20 +0800

    to #31092_add_encodeURLComponent_function

--- src/Functions/URL/decodeURLComponent.cpp
+++ src/Functions/URL/decodeURLComponent.cpp
@@ -11,6 +11,31 @@ namespace ErrorCodes
     extern const int ILLEGAL_COLUMN;
 }
 
+static size_t encodeURL(const char * src, size_t src_size, char * dst, bool space_as_plus)
+{
+    char * dst_pos = dst;
+    for (size_t i = 0; i < src_size - 1; i++)
+    {
+        if ((src[i] >= '0' && src[i] <= '9') || (src[i] >= 'a' && src[i] <= 'z') || (src[i] >= 'A' && src[i] <= 'Z')
+            || src[i] == '-' || src[i] == '_' || src[i] == '.' || src[i] == '!' || src[i] == '~' || (src[i] >= '\'' && src[i] <= '*'))
+        {
+            *dst_pos++ = src[i];
+        }
+        else if (src[i] == ' ' && space_as_plus)
+        {
+            *dst_pos++ = '+';
+        }
+        else
+        {
+            *dst_pos++ = '%';
+            *dst_pos++ = hexDigitUppercase(src[i] >> 4);
+            *dst_pos++ = hexDigitUppercase(src[i] & 0xf);
+        }
+    }
+    *dst_pos++ = src[src_size - 1];
+    return dst_pos - dst;
+}
+
 /// We assume that size of the dst buf isn't less than src_size.
 static size_t decodeURL(const char * src, size_t src_size, char * dst, bool plus_as_space)
 {
@@ -83,8 +108,8 @@ static size_t decodeURL(const char * src, size_t src_size, char * dst, bool plus
 
 
 /// Percent decode of URL data.
-template <bool plus_as_space>
-struct DecodeURLComponentImpl
+template <bool plus_as_space, bool encode>
+struct CodeURLComponentImpl
 {
     static void vector(const ColumnString::Chars & data, const ColumnString::Offsets & offsets,
         ColumnString::Chars & res_data, ColumnString::Offsets & res_offsets)
@@ -100,8 +125,11 @@ struct DecodeURLComponentImpl
         {
             const char * src_data = reinterpret_cast<const char *>(&data[prev_offset]);
             size_t src_size = offsets[i] - prev_offset;
-            size_t dst_size = decodeURL(src_data, src_size, reinterpret_cast<char *>(res_data.data() + res_offset), plus_as_space);
-
+            size_t dst_size;
+            if (encode)
+                dst_size = encodeURL(src_data, src_size, reinterpret_cast<char *>(res_data.data() + res_offset), plus_as_space);
+            else
+                dst_size = decodeURL(src_data, src_size, reinterpret_cast<char *>(res_data.data() + res_offset), plus_as_space);
             res_offset += dst_size;
             res_offsets[i] = res_offset;
             prev_offset = offsets[i];
@@ -118,14 +146,20 @@ struct DecodeURLComponentImpl
 
 
 struct NameDecodeURLComponent { static constexpr auto name = "decodeURLComponent"; };
+struct NameEncodeURLComponent { static constexpr auto name = "encodeURLComponent"; };
 struct NameDecodeURLFormComponent { static constexpr auto name = "decodeURLFormComponent"; };
-using FunctionDecodeURLComponent = FunctionStringToString<DecodeURLComponentImpl<false>, NameDecodeURLComponent>;
-using FunctionDecodeURLFormComponent = FunctionStringToString<DecodeURLComponentImpl<true>, NameDecodeURLFormComponent>;
+struct NameEncodeURLFormComponent { static constexpr auto name = "encodeURLFormComponent"; };
+using FunctionDecodeURLComponent = FunctionStringToString<CodeURLComponentImpl<false, false>, NameDecodeURLComponent>;
+using FunctionEncodeURLComponent = FunctionStringToString<CodeURLComponentImpl<false, true>, NameEncodeURLComponent>;
+using FunctionDecodeURLFormComponent = FunctionStringToString<CodeURLComponentImpl<true, false>, NameDecodeURLFormComponent>;
+using FunctionEncodeURLFormComponent = FunctionStringToString<CodeURLComponentImpl<true, true>, NameEncodeURLFormComponent>;
 
-void registerFunctionDecodeURLComponent(FunctionFactory & factory)
+void registerFunctionEncodeAndDecodeURLComponent(FunctionFactory & factory)
 {
     factory.registerFunction<FunctionDecodeURLComponent>();
+    factory.registerFunction<FunctionEncodeURLComponent>();
     factory.registerFunction<FunctionDecodeURLFormComponent>();
+    factory.registerFunction<FunctionEncodeURLFormComponent>();
 }
 
 }
