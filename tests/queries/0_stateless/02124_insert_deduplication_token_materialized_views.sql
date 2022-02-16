12b6654dcb96ffa7c505ed6259960f7363dc6ca4
Denny Crane <denis.zhuravlov@gmail.com>
2022-02-16 17:25:13 -0400

    test that insert_deduplication_token allow to do idempotent insertion with materialized_views

--- tests/queries/0_stateless/02124_insert_deduplication_token_materialized_views.sql
+++ tests/queries/0_stateless/02124_insert_deduplication_token_materialized_views.sql
@@ -7,15 +7,15 @@ drop table if exists test_mv_c sync;
 
 set deduplicate_blocks_in_dependent_materialized_views=0;
 
-CREATE TABLE test (A Int64, B Int64) ENGINE = ReplicatedMergeTree ('/clickhouse/test/tables/test','1') ORDER BY tuple()  ;
+CREATE TABLE test (A Int64, B Int64) ENGINE = ReplicatedMergeTree() ORDER BY tuple()  ;
 
-CREATE MATERIALIZED VIEW test_mv_a Engine=ReplicatedMergeTree ('/clickhouse/test/tables/test_mv_a','1') 
+CREATE MATERIALIZED VIEW test_mv_a Engine=ReplicatedMergeTree()
 order by tuple()  AS SELECT A, count() c FROM test group by A;
 
-CREATE MATERIALIZED VIEW test_mv_b Engine=ReplicatedMergeTree ('/clickhouse/test/tables/test_mv_b','1') 
+CREATE MATERIALIZED VIEW test_mv_b Engine=ReplicatedMergeTree()
 partition by A order by tuple()  AS SELECT A, count() c FROM test group by A;
 
-CREATE MATERIALIZED VIEW test_mv_c Engine=ReplicatedMergeTree ('/clickhouse/test/tables/test_mv_c','1') 
+CREATE MATERIALIZED VIEW test_mv_c Engine=ReplicatedMergeTree()
 order by tuple()  AS SELECT A, count() c FROM test group by A;
 
 SET max_partitions_per_insert_block = 1;
@@ -41,15 +41,15 @@ drop table if exists test_mv_c sync;
 
 set deduplicate_blocks_in_dependent_materialized_views=1;
 
-CREATE TABLE test (A Int64, B Int64) ENGINE = ReplicatedMergeTree ('/clickhouse/test/tables/test','1') ORDER BY tuple()  ;
+CREATE TABLE test (A Int64, B Int64) ENGINE = ReplicatedMergeTree() ORDER BY tuple()  ;
 
-CREATE MATERIALIZED VIEW test_mv_a Engine=ReplicatedMergeTree ('/clickhouse/test/tables/test_mv_a','1') 
+CREATE MATERIALIZED VIEW test_mv_a Engine=ReplicatedMergeTree()
 order by tuple()  AS SELECT A, count() c FROM test group by A;
 
-CREATE MATERIALIZED VIEW test_mv_b Engine=ReplicatedMergeTree ('/clickhouse/test/tables/test_mv_b','1') 
+CREATE MATERIALIZED VIEW test_mv_b Engine=ReplicatedMergeTree()
 partition by A order by tuple()  AS SELECT A, count() c FROM test group by A;
 
-CREATE MATERIALIZED VIEW test_mv_c Engine=ReplicatedMergeTree ('/clickhouse/test/tables/test_mv_c','1') 
+CREATE MATERIALIZED VIEW test_mv_c Engine=ReplicatedMergeTree()
 order by tuple()  AS SELECT A, count() c FROM test group by A;
 
 SET max_partitions_per_insert_block = 1;
@@ -75,15 +75,15 @@ drop table if exists test_mv_c sync;
 
 set deduplicate_blocks_in_dependent_materialized_views=0;
 
-CREATE TABLE test (A Int64, B Int64) ENGINE = ReplicatedMergeTree ('/clickhouse/test/tables/test','1') ORDER BY tuple()  ;
+CREATE TABLE test (A Int64, B Int64) ENGINE = ReplicatedMergeTree() ORDER BY tuple()  ;
 
-CREATE MATERIALIZED VIEW test_mv_a Engine=ReplicatedMergeTree ('/clickhouse/test/tables/test_mv_a','1') 
+CREATE MATERIALIZED VIEW test_mv_a Engine=ReplicatedMergeTree()
 order by tuple()  AS SELECT A, count() c FROM test group by A;
 
-CREATE MATERIALIZED VIEW test_mv_b Engine=ReplicatedMergeTree ('/clickhouse/test/tables/test_mv_b','1') 
+CREATE MATERIALIZED VIEW test_mv_b Engine=ReplicatedMergeTree()
 partition by A order by tuple()  AS SELECT A, count() c FROM test group by A;
 
-CREATE MATERIALIZED VIEW test_mv_c Engine=ReplicatedMergeTree ('/clickhouse/test/tables/test_mv_c','1') 
+CREATE MATERIALIZED VIEW test_mv_c Engine=ReplicatedMergeTree()
 order by tuple()  AS SELECT A, count() c FROM test group by A;
 
 SET max_partitions_per_insert_block = 1;
@@ -93,7 +93,7 @@ INSERT INTO test SELECT number%3, 1 FROM numbers(9) SETTINGS insert_deduplicatio
 INSERT INTO test SELECT number%3, 2 FROM numbers(9) SETTINGS insert_deduplication_token = 'test2';
 INSERT INTO test SELECT number%3, 2 FROM numbers(9) SETTINGS insert_deduplication_token = 'test2';
 
-select 
+select
   (select count() from test),
   (select sum(c) from test_mv_a),
   (select sum(c) from test_mv_b),
@@ -108,15 +108,15 @@ drop table if exists test_mv_c sync;
 
 set deduplicate_blocks_in_dependent_materialized_views=1;
 
-CREATE TABLE test (A Int64, B Int64) ENGINE = ReplicatedMergeTree ('/clickhouse/test/tables/test','1') ORDER BY tuple()  ;
+CREATE TABLE test (A Int64, B Int64) ENGINE = ReplicatedMergeTree() ORDER BY tuple()  ;
 
-CREATE MATERIALIZED VIEW test_mv_a Engine=ReplicatedMergeTree ('/clickhouse/test/tables/test_mv_a','1') 
+CREATE MATERIALIZED VIEW test_mv_a Engine=ReplicatedMergeTree()
 order by tuple()  AS SELECT A, count() c FROM test group by A;
 
-CREATE MATERIALIZED VIEW test_mv_b Engine=ReplicatedMergeTree ('/clickhouse/test/tables/test_mv_b','1') 
+CREATE MATERIALIZED VIEW test_mv_b Engine=ReplicatedMergeTree()
 partition by A order by tuple()  AS SELECT A, count() c FROM test group by A;
 
-CREATE MATERIALIZED VIEW test_mv_c Engine=ReplicatedMergeTree ('/clickhouse/test/tables/test_mv_c','1') 
+CREATE MATERIALIZED VIEW test_mv_c Engine=ReplicatedMergeTree()
 order by tuple()  AS SELECT A, count() c FROM test group by A;
 
 
@@ -127,7 +127,7 @@ INSERT INTO test SELECT number%3, 1 FROM numbers(9) SETTINGS insert_deduplicatio
 INSERT INTO test SELECT number%3, 2 FROM numbers(9) SETTINGS insert_deduplication_token = 'test2';
 INSERT INTO test SELECT number%3, 2 FROM numbers(9) SETTINGS insert_deduplication_token = 'test2';
 
-select 
+select
   (select count() from test),
   (select sum(c) from test_mv_a),
   (select sum(c) from test_mv_b),
