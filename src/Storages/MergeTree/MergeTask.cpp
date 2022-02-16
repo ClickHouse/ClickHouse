ce92f6aab1f37f2b37dc481d6a650e23edd18f38
Maksim Kita <maksim-kita@clickhouse.com>
2022-02-16 14:01:42 +0000

    Fixed tests

--- src/Storages/MergeTree/MergeTask.cpp
+++ src/Storages/MergeTree/MergeTask.cpp
@@ -802,7 +802,7 @@ void MergeTask::ExecuteAndFinalizeHorizontalPart::createMergedStream()
     {
         case MergeTreeData::MergingParams::Ordinary:
             merged_transform = std::make_shared<MergingSortedTransform>(
-                header, pipes.size(), sort_description, merge_block_size, 0, compile_sort_description, ctx->rows_sources_write_buf.get(), true, ctx->blocks_are_granules_size);
+                header, pipes.size(), sort_description, compile_sort_description, merge_block_size, 0, ctx->rows_sources_write_buf.get(), true, ctx->blocks_are_granules_size);
             break;
 
         case MergeTreeData::MergingParams::Collapsing:
