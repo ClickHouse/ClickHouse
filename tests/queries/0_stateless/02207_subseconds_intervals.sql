d5d285141deae92188ac02d81662422586906748
zvonand <azvonov@altinity.com>
2022-02-17 01:39:46 +0300

    updated negative fracs

--- tests/queries/0_stateless/02207_subseconds_intervals.sql
+++ tests/queries/0_stateless/02207_subseconds_intervals.sql
@@ -1,44 +1,44 @@
 SELECT 'test intervals';
 
 SELECT '- test nanoseconds';
-select toStartOfInterval(toDateTime64('1980-12-12 12:12:12.123456789', 9), INTERVAL 7 NANOSECOND); -- In normal range, source scale matches result
-select toStartOfInterval(toDateTime64('1980-12-12 12:12:12.1234567', 7), INTERVAL 7 NANOSECOND); -- In normal range, source scale less than result
+select toStartOfInterval(toDateTime64('1980-12-12 12:12:12.123456789', 9), INTERVAL 1 NANOSECOND); -- In normal range, source scale matches result
+select toStartOfInterval(toDateTime64('1980-12-12 12:12:12.1234567', 7), INTERVAL 1 NANOSECOND); -- In normal range, source scale less than result
 
-select toStartOfInterval(toDateTime64('1930-12-12 12:12:12.123456789', 9), INTERVAL 7 NANOSECOND); -- Below normal range, source scale matches result
-select toStartOfInterval(toDateTime64('1930-12-12 12:12:12.1234567', 7), INTERVAL 7 NANOSECOND); -- Below normal range, source scale less than result
+select toStartOfInterval(toDateTime64('1930-12-12 12:12:12.123456789', 9), INTERVAL 1 NANOSECOND); -- Below normal range, source scale matches result
+select toStartOfInterval(toDateTime64('1930-12-12 12:12:12.1234567', 7), INTERVAL 1 NANOSECOND); -- Below normal range, source scale less than result
 
-select toStartOfInterval(toDateTime64('2220-12-12 12:12:12.123456789', 9), INTERVAL 7 NANOSECOND); -- Above normal range, source scale matches result
-select toStartOfInterval(toDateTime64('2220-12-12 12:12:12.1234567', 7), INTERVAL 7 NANOSECOND); -- Above normal range, source scale less than result
+select toStartOfInterval(toDateTime64('2220-12-12 12:12:12.123456789', 9), INTERVAL 1 NANOSECOND); -- Above normal range, source scale matches result
+select toStartOfInterval(toDateTime64('2220-12-12 12:12:12.1234567', 7), INTERVAL 1 NANOSECOND); -- Above normal range, source scale less than result
 
 
 SELECT '- test microseconds';
-select toStartOfInterval(toDateTime64('1980-12-12 12:12:12.123456', 6), INTERVAL 7 MICROSECOND); -- In normal range, source scale matches result
-select toStartOfInterval(toDateTime64('1980-12-12 12:12:12.1234', 4), INTERVAL 7 MICROSECOND); -- In normal range, source scale less than result
-select toStartOfInterval(toDateTime64('1980-12-12 12:12:12.12345678', 8), INTERVAL 7 MICROSECOND); -- In normal range, source scale greater than result
+select toStartOfInterval(toDateTime64('1980-12-12 12:12:12.123456', 6), INTERVAL 1 MICROSECOND); -- In normal range, source scale matches result
+select toStartOfInterval(toDateTime64('1980-12-12 12:12:12.1234', 4), INTERVAL 1 MICROSECOND); -- In normal range, source scale less than result
+select toStartOfInterval(toDateTime64('1980-12-12 12:12:12.12345678', 8), INTERVAL 1 MICROSECOND); -- In normal range, source scale greater than result
 
 
-select toStartOfInterval(toDateTime64('1930-12-12 12:12:12.123456', 6), INTERVAL 7 MICROSECOND); -- Below normal range, source scale matches result
-select toStartOfInterval(toDateTime64('1930-12-12 12:12:12.1234', 4), INTERVAL 7 MICROSECOND); -- Below normal range, source scale less than result
-select toStartOfInterval(toDateTime64('1930-12-12 12:12:12.12345678', 8), INTERVAL 7 MICROSECOND); -- Below normal range, source scale greater than result
+select toStartOfInterval(toDateTime64('1930-12-12 12:12:12.123456', 6), INTERVAL 1 MICROSECOND); -- Below normal range, source scale matches result
+select toStartOfInterval(toDateTime64('1930-12-12 12:12:12.1234', 4), INTERVAL 1 MICROSECOND); -- Below normal range, source scale less than result
+select toStartOfInterval(toDateTime64('1930-12-12 12:12:12.12345678', 8), INTERVAL 1 MICROSECOND); -- Below normal range, source scale greater than result
 
 
-select toStartOfInterval(toDateTime64('2220-12-12 12:12:12.123456', 6), INTERVAL 7 MICROSECOND); -- Above normal range, source scale matches result
-select toStartOfInterval(toDateTime64('2220-12-12 12:12:12.1234', 4), INTERVAL 7 MICROSECOND); -- Above normal range, source scale less than result
-select toStartOfInterval(toDateTime64('2220-12-12 12:12:12.12345678', 8), INTERVAL 7 MICROSECOND); -- Above normal range, source scale greater than result
+select toStartOfInterval(toDateTime64('2220-12-12 12:12:12.123456', 6), INTERVAL 1 MICROSECOND); -- Above normal range, source scale matches result
+select toStartOfInterval(toDateTime64('2220-12-12 12:12:12.1234', 4), INTERVAL 1 MICROSECOND); -- Above normal range, source scale less than result
+select toStartOfInterval(toDateTime64('2220-12-12 12:12:12.12345678', 8), INTERVAL 1 MICROSECOND); -- Above normal range, source scale greater than result
 
 
 SELECT '- test milliseconds';
-select toStartOfInterval(toDateTime64('1980-12-12 12:12:12.123', 3), INTERVAL 7 MILLISECOND); -- In normal range, source scale matches result
-select toStartOfInterval(toDateTime64('1980-12-12 12:12:12.12', 2), INTERVAL 7 MILLISECOND); -- In normal range, source scale less than result
-select toStartOfInterval(toDateTime64('1980-12-12 12:12:12.123456', 6), INTERVAL 7 MILLISECOND); -- In normal range, source scale greater than result
+select toStartOfInterval(toDateTime64('1980-12-12 12:12:12.123', 3), INTERVAL 1 MILLISECOND); -- In normal range, source scale matches result
+select toStartOfInterval(toDateTime64('1980-12-12 12:12:12.12', 2), INTERVAL 1 MILLISECOND); -- In normal range, source scale less than result
+select toStartOfInterval(toDateTime64('1980-12-12 12:12:12.123456', 6), INTERVAL 1 MILLISECOND); -- In normal range, source scale greater than result
 
-select toStartOfInterval(toDateTime64('1930-12-12 12:12:12.123', 3), INTERVAL 7 MILLISECOND); -- Below normal range, source scale matches result
-select toStartOfInterval(toDateTime64('1930-12-12 12:12:12.12', 2), INTERVAL 7 MILLISECOND); -- Below normal range, source scale less than result
-select toStartOfInterval(toDateTime64('1930-12-12 12:12:12.123456', 6), INTERVAL 7 MILLISECOND); -- Below normal range, source scale greater than result
+select toStartOfInterval(toDateTime64('1930-12-12 12:12:12.123', 3), INTERVAL 1 MILLISECOND); -- Below normal range, source scale matches result
+select toStartOfInterval(toDateTime64('1930-12-12 12:12:12.12', 2), INTERVAL 1 MILLISECOND); -- Below normal range, source scale less than result
+select toStartOfInterval(toDateTime64('1930-12-12 12:12:12.123456', 6), INTERVAL 1 MILLISECOND); -- Below normal range, source scale greater than result
 
-select toStartOfInterval(toDateTime64('2220-12-12 12:12:12.123', 3), INTERVAL 7 MILLISECOND); -- Above normal range, source scale matches result
-select toStartOfInterval(toDateTime64('2220-12-12 12:12:12.12', 2), INTERVAL 7 MILLISECOND); -- Above normal range, source scale less than result
-select toStartOfInterval(toDateTime64('2220-12-12 12:12:12.123456', 6), INTERVAL 7 MILLISECOND); -- Above normal range, source scale greater than result
+select toStartOfInterval(toDateTime64('2220-12-12 12:12:12.123', 3), INTERVAL 1 MILLISECOND); -- Above normal range, source scale matches result
+select toStartOfInterval(toDateTime64('2220-12-12 12:12:12.12', 2), INTERVAL 1 MILLISECOND); -- Above normal range, source scale less than result
+select toStartOfInterval(toDateTime64('2220-12-12 12:12:12.123456', 6), INTERVAL 1 MILLISECOND); -- Above normal range, source scale greater than result
 
 
 SELECT 'test add[...]seconds()';
