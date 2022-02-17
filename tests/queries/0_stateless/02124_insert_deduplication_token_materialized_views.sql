acb36592c94de8c67e30d8d612481eacf851f8ea
Denny Crane <denis.zhuravlov@gmail.com>
2022-02-17 00:03:34 -0400

    Update 02124_insert_deduplication_token_materialized_views.sql

--- tests/queries/0_stateless/02124_insert_deduplication_token_materialized_views.sql
+++ tests/queries/0_stateless/02124_insert_deduplication_token_materialized_views.sql
@@ -7,13 +7,13 @@ drop table if exists test_mv_c sync;
 
 set deduplicate_blocks_in_dependent_materialized_views=0;
 
-CREATE TABLE test (A Int64, B Int64) ENGINE = ReplicatedMergeTree ('/clickhouse/test/tables/test','1') ORDER BY tuple()  ;
+CREATE TABLE test (A Int64, B Int64) ENGINE = ReplicatedMergeTree () ORDER BY tuple()  ;
 
-CREATE MATERIALIZED VIEW test_mv_a Engine=ReplicatedMergeTree ('/clickhouse/test/tables/test_mv_a','1') order by tuple()  AS SELECT A, count() c FROM test group by A;
+CREATE MATERIALIZED VIEW test_mv_a Engine=ReplicatedMergeTree () order by tuple()  AS SELECT A, count() c FROM test group by A;
 
-CREATE MATERIALIZED VIEW test_mv_b Engine=ReplicatedMergeTree ('/clickhouse/test/tables/test_mv_b','1') partition by A order by tuple()  AS SELECT A, count() c FROM test group by A;
+CREATE MATERIALIZED VIEW test_mv_b Engine=ReplicatedMergeTree () partition by A order by tuple()  AS SELECT A, count() c FROM test group by A;
 
-CREATE MATERIALIZED VIEW test_mv_c Engine=ReplicatedMergeTree ('/clickhouse/test/tables/test_mv_c','1') order by tuple()  AS SELECT A, count() c FROM test group by A;
+CREATE MATERIALIZED VIEW test_mv_c Engine=ReplicatedMergeTree () order by tuple()  AS SELECT A, count() c FROM test group by A;
 
 SET max_partitions_per_insert_block = 1;
 INSERT INTO test SELECT number%3, 1 FROM numbers(9); -- { serverError 252 }
