c58b86f0e68847f5d1c85ccdfe206fd5f74ffedd
Denny Crane <denis.zhuravlov@gmail.com>
2022-02-17 09:31:32 -0400

    fix zk path

--- tests/queries/0_stateless/02124_insert_deduplication_token_materialized_views.sql
+++ tests/queries/0_stateless/02124_insert_deduplication_token_materialized_views.sql
@@ -7,13 +7,17 @@ drop table if exists test_mv_c sync;
 
 set deduplicate_blocks_in_dependent_materialized_views=0;
 
-CREATE TABLE test (A Int64, B Int64) ENGINE = ReplicatedMergeTree () ORDER BY tuple()  ;
+CREATE TABLE test (A Int64, B Int64) ENGINE = ReplicatedMergeTree ('/clickhouse/tables/{database}/test_02124/{table}', '1')
+ORDER BY tuple()  ;
 
-CREATE MATERIALIZED VIEW test_mv_a Engine=ReplicatedMergeTree () order by tuple()  AS SELECT A, count() c FROM test group by A;
+CREATE MATERIALIZED VIEW test_mv_a Engine=ReplicatedMergeTree ('/clickhouse/tables/{database}/test_02124/{table}', '1')
+order by tuple()  AS SELECT A, count() c FROM test group by A;
 
-CREATE MATERIALIZED VIEW test_mv_b Engine=ReplicatedMergeTree () partition by A order by tuple()  AS SELECT A, count() c FROM test group by A;
+CREATE MATERIALIZED VIEW test_mv_b Engine=ReplicatedMergeTree ('/clickhouse/tables/{database}/test_02124/{table}', '1')
+partition by A order by tuple()  AS SELECT A, count() c FROM test group by A;
 
-CREATE MATERIALIZED VIEW test_mv_c Engine=ReplicatedMergeTree () order by tuple()  AS SELECT A, count() c FROM test group by A;
+CREATE MATERIALIZED VIEW test_mv_c Engine=ReplicatedMergeTree ('/clickhouse/tables/{database}/test_02124/{table}', '1')
+order by tuple()  AS SELECT A, count() c FROM test group by A;
 
 SET max_partitions_per_insert_block = 1;
 INSERT INTO test SELECT number%3, 1 FROM numbers(9); -- { serverError 252 }
@@ -68,7 +72,7 @@ INSERT INTO test SELECT number%3, 1 FROM numbers(9) SETTINGS insert_deduplicatio
 INSERT INTO test SELECT number%3, 2 FROM numbers(9) SETTINGS insert_deduplication_token = 'test2';
 INSERT INTO test SELECT number%3, 2 FROM numbers(9) SETTINGS insert_deduplication_token = 'test2';
 
-select 
+select
   (select count() from test),
   (select sum(c) from test_mv_a),
   (select sum(c) from test_mv_b),
@@ -90,7 +94,7 @@ INSERT INTO test SELECT number%3, 1 FROM numbers(9) SETTINGS insert_deduplicatio
 INSERT INTO test SELECT number%3, 2 FROM numbers(9) SETTINGS insert_deduplication_token = 'test2';
 INSERT INTO test SELECT number%3, 2 FROM numbers(9) SETTINGS insert_deduplication_token = 'test2';
 
-select 
+select
   (select count() from test),
   (select sum(c) from test_mv_a),
   (select sum(c) from test_mv_b),
