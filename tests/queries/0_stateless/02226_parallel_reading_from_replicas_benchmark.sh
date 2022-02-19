98e30a824344ea967d52f6cfdf56e058037f3c34
Azat Khuzhin <a3at.mail@gmail.com>
2022-02-19 18:03:04 +0300

    Fix 02226_parallel_reading_from_replicas_benchmark for older shellcheck
    By shellcheck 0.8 does not complains, while on CI shellcheck 0.7.0 and
    it does complains [1]:
    
        In 02226_parallel_reading_from_replicas_benchmark.sh line 17:
            --allow_experimental_parallel_reading_from_replicas=1
            ^-- SC2191: The = here is literal. To assign by index, use ( [index]=value ) with no spaces. To keep as literal, quote it.
    
        Did you mean:
            "--allow_experimental_parallel_reading_from_replicas=1"
    
      [1]: https://s3.amazonaws.com/clickhouse-test-reports/34751/d883af711822faf294c876b017cbf745b1cda1b3/style_check__actions_/shellcheck_output.txt
    
    Signed-off-by: Azat Khuzhin <a.khuzhin@semrush.com>

--- tests/queries/0_stateless/02226_parallel_reading_from_replicas_benchmark.sh
+++ tests/queries/0_stateless/02226_parallel_reading_from_replicas_benchmark.sh
@@ -14,8 +14,8 @@ as select * from numbers(1);
 #
 #   Logical error: 'Coordinator for parallel reading from replicas is not initialized'.
 opts=(
-    --allow_experimental_parallel_reading_from_replicas=1
-    --max_parallel_replicas=3
+    --allow_experimental_parallel_reading_from_replicas 1
+    --max_parallel_replicas 3
 
     --iterations 1
 )
