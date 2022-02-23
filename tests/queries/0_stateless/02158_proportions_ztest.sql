62beb454ec87fd0bc66b8e9f8370733e6c9ff761
achimbab <07c00h@gmail.com>
2022-02-24 04:01:24 +0900

    Fix proportionsZTest

--- tests/queries/0_stateless/02158_proportions_ztest.sql
+++ tests/queries/0_stateless/02158_proportions_ztest.sql
@@ -1,7 +1,7 @@
 SELECT proportionsZTest(10, 11, 100, 101, 0.95, 'unpooled');
 
 DROP TABLE IF EXISTS proportions_ztest;
-CREATE TABLE proportions_ztest (sx UInt64, sy UInt64, tx UInt64, ty UInt64, ci Float64, var String) Engine = Memory();
-INSERT INTO proportions_ztest VALUES (10, 11, 100, 101, 0.95, 'unpooled');
-SELECT proportionsZTest(sx, sy, tx, ty, ci, var) FROM proportions_ztest;
+CREATE TABLE proportions_ztest (sx UInt64, sy UInt64, tx UInt64, ty UInt64) Engine = Memory();
+INSERT INTO proportions_ztest VALUES (10, 11, 100, 101);
+SELECT proportionsZTest(sx, sy, tx, ty, 0.95, 'unpooled') FROM proportions_ztest;
 DROP TABLE IF EXISTS proportions_ztest;
