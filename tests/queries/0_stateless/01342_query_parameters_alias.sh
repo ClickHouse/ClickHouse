4cc8d7570e4bb690628f7fc257927013e3ab5891
李扬 <654010905@qq.com>
2022-03-01 19:21:46 +0800

    Update 01342_query_parameters_alias.sh

--- tests/queries/0_stateless/01342_query_parameters_alias.sh
+++ tests/queries/0_stateless/01342_query_parameters_alias.sh
@@ -4,4 +4,4 @@ CURDIR=$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)
 # shellcheck source=../shell_config.sh
 . "$CURDIR"/../shell_config.sh
 
-$CLICKHOUSE_CLIENT --param_x '\N' --query 'SELECT {x:Nullable(Nothing)} as a' --format TSVWithNamesAndTypes
+$CLICKHOUSE_CLIENT --param_x '\N' --query 'SELECT {x:Nullable(Nothing)} as a' --external --format TSVWithNamesAndTypes
