77ed6fcb3c3d35390614c05216cb9dc022ecc7a9
vdimir <vdimir@clickhouse.com>
2022-03-01 14:40:01 +0000

    wip

--- python_module/mylib.cpp
+++ python_module/mylib.cpp
@@ -5,14 +5,66 @@
 #include <Core/ServerUUID.h>
 #include <IO/WriteHelpers.h>
 #include <Client/LocalConnection.h>
+#include <Common/tests/gtest_global_context.h>
+#include <Common/Config/ConfigProcessor.h>
+#include <Poco/Util/XMLConfiguration.h>
+
+using namespace DB;
+
+DB::ConfigurationPtr setupUsers();
+
+const ContextHolder & getContext()
+{
+    static ContextHolder holder;
+    holder.context->setUsersConfig(setupUsers());
+    return holder;
+}
+
+
+static ConfigurationPtr getConfigurationFromXMLString(const char * xml_data)
+{
+    std::stringstream ss{std::string{xml_data}};    // STYLE_CHECK_ALLOW_STD_STRING_STREAM
+    Poco::XML::InputSource input_source{ss};
+    return {new Poco::Util::XMLConfiguration{&input_source}};
+}
+
+
+DB::ConfigurationPtr setupUsers()
+{
+    static const char * minimal_default_user_xml =
+        "<clickhouse>"
+        "    <profiles>"
+        "        <default></default>"
+        "    </profiles>"
+        "    <users>"
+        "        <default>"
+        "            <password></password>"
+        "            <networks>"
+        "                <ip>::/0</ip>"
+        "            </networks>"
+        "            <profile>default</profile>"
+        "            <quota>default</quota>"
+        "        </default>"
+        "    </users>"
+        "    <quotas>"
+        "        <default></default>"
+        "    </quotas>"
+        "</clickhouse>";
+
+
+    return getConfigurationFromXMLString(minimal_default_user_xml);
+}
+
 
 namespace py = pybind11;
 
 std::string hello()
 {
-    auto sctx  = DB::Context::createShared();
-    auto gctx = DB::Context::createGlobal(sctx.get());
-    DB::LocalConnection lc(gctx);
+    std::cout << __FILE__ << ":" << __LINE__ << "\n";
+
+    const auto & ctx = getContext();
+
+    DB::LocalConnection lc(ctx.context);
     std::string res;
     if (lc.checkConnected())
     {
@@ -22,8 +74,10 @@ std::string hello()
     {
         res += "no";
     }
+
     DB::ConnectionTimeouts ts;
-    lc.sendQuery(ts, "SELECT 1", "", DB::QueryProcessingStage::Complete, nullptr, nullptr, false);
+    lc.sendQuery(ts, "SELECT 1 as x", "", DB::QueryProcessingStage::Complete, nullptr, nullptr, false);
+    //...
     return res;
     // return toString(DB::ServerUUID::get());
 }
