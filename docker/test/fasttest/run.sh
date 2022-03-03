b2ed5552cc5c6b6428bb04f7fe07dfeb26995853
Azat Khuzhin <a3at.mail@gmail.com>
2022-03-03 15:42:29 +0300

    fasttest: use bash arrays over wrap lines with backslash
    Signed-off-by: Azat Khuzhin <a.khuzhin@semrush.com>

--- docker/test/fasttest/run.sh
+++ docker/test/fasttest/run.sh
@@ -263,9 +263,19 @@ function run_tests
     if [[ $NPROC == 0 ]]; then
       NPROC=1
     fi
-    time clickhouse-test --hung-check -j "${NPROC}" --order=random \
-            --fast-tests-only --no-long --testname --shard --zookeeper --check-zookeeper-session \
-            -- "$FASTTEST_FOCUS" 2>&1 \
+
+    local test_opts=(
+        --hung-check
+        --fast-tests-only
+        --no-long
+        --testname
+        --shard
+        --zookeeper
+        --check-zookeeper-session
+        --order random
+        --jobs "${NPROC}"
+    )
+    time clickhouse-test "${test_opts[@]}" -- "$FASTTEST_FOCUS" 2>&1 \
         | ts '%Y-%m-%d %H:%M:%S' \
         | tee "$FASTTEST_OUTPUT/test_result.txt"
     set -e
