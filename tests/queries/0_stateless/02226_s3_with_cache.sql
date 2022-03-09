aab3de76ca70a4b74c27fdec27d25918b4b8bf06
kssenii <sumarokovakseniia@mail.ru>
2022-03-09 12:44:08 +0100

    Update test

--- tests/queries/0_stateless/02226_s3_with_cache.sql
+++ tests/queries/0_stateless/02226_s3_with_cache.sql
@@ -1,5 +1,7 @@
 -- Tags: no-parallel, no-fasttest, long
 
+SET max_memory_usage='20G';
+
 CREATE TABLE test (key UInt32, value String) Engine=MergeTree() ORDER BY key SETTINGS storage_policy='s3_cache';
 INSERT INTO test SELECT * FROM generateRandom('key UInt32, value String') LIMIT 100000000;
 
@@ -35,8 +37,10 @@ AND current_database = currentDatabase()
 ORDER BY query_start_time DESC
 LIMIT 1;
 
+SET remote_filesystem_read_method='threadpool';
+
 SELECT * FROM test WHERE value LIKE '%abc%' ORDER BY value LIMIT 10 FORMAT Null;
 SELECT * FROM test ORDER BY value LIMIT 10 FORMAT Null;
 SELECT * FROM test WHERE value LIKE '%dba%' ORDER BY value LIMIT 10 FORMAT Null;
 
-DROP TABLE test NO DELAY;
+DROP TABLE test;
