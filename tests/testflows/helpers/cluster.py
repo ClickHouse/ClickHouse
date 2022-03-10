fad713dbffddd871fa91c5d5847beb80cb12f2ed
Vitaliy Zakaznikov <vzakaznikov@protonmail.com>
2022-03-09 19:40:12 -0500

    Fixing stop_clickhouse method again.

--- tests/testflows/helpers/cluster.py
+++ tests/testflows/helpers/cluster.py
@@ -198,9 +198,9 @@ class ClickHouseNode(Node):
             self.command(f"kill -TERM {pid}", exitcode=0, steps=False)
 
         with And("checking pid does not exist"):
-            for attempt in retries(timeout=100, delay=3):
+            for i, attempt in enumerate(retries(timeout=100, delay=3)):
                 with attempt:
-                    if attempt.number > 0 and attempt.number % 20 == 0:
+                    if i > 0 and i % 20 == 0:
                         self.command(f"kill -KILL {pid}", steps=False)
                     if self.command(f"ps {pid}", steps=False, no_checks=True).exitcode != 1:
                         fail("pid still alive")
