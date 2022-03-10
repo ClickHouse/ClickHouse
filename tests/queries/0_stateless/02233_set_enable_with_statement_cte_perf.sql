289070a55c36ad0a2915c4aeb8394dc15983b883
Pablo Alegre <palegre@tinybird.co>
2022-03-10 11:11:46 +0100

    fixup! Add test for #26965 * Add a test for 'WITH'+CTE performance if enable_global_with_statement is enabled

--- tests/queries/0_stateless/02233_set_enable_with_statement_cte_perf.sql
+++ tests/queries/0_stateless/02233_set_enable_with_statement_cte_perf.sql
@@ -21,7 +21,7 @@ WITH 'test' AS u SELECT count() FROM ev WHERE a IN (SELECT a FROM idx);
 
 SYSTEM FLUSH LOGS;
 
-SELECT count(read_rows) FROM (SELECT read_rows FROM system.query_log WHERE current_database=currentDatabase() AND type='QueryFinish' AND query LIKE '%test_enable_global_with_statement_performance%' ORDER BY event_date, event_time DESC LIMIT 3) GROUP BY read_rows;
+SELECT count(read_rows) FROM (SELECT read_rows FROM system.query_log WHERE current_database=currentDatabase() AND type='QueryFinish' AND query LIKE '-- test_enable_global_with_statement_performance%' ORDER BY initial_query_start_time_microseconds DESC LIMIT 3) GROUP BY read_rows;
 
 DROP TABLE IF EXISTS ev;
 DROP TABLE IF EXISTS idx;
