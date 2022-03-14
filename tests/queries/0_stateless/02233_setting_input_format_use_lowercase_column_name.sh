6e42014efbeb57ef17e1fe558a710bc52ffbd82c
kssenii <sumarokovakseniia@mail.ru>
2022-03-14 20:19:53 +0100

    Fix

--- tests/queries/0_stateless/02233_setting_input_format_use_lowercase_column_name.sh
+++ tests/queries/0_stateless/02233_setting_input_format_use_lowercase_column_name.sh
@@ -6,7 +6,7 @@ CUR_DIR=$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)
 . "$CUR_DIR"/../shell_config.sh
 
 echo "Parquet"
-DATA_FILE=$CUR_DIR/data_parquet/test_setting_input_format_use_lowercase_column_name.parquet
+DATA_FILE=$CUR_DIR/02233_data/test_setting_input_format_use_lowercase_column_name.parquet
 ${CLICKHOUSE_CLIENT} --query="DROP TABLE IF EXISTS parquet_load"
 ${CLICKHOUSE_CLIENT} --query="CREATE TABLE parquet_load (id String, score Int32) ENGINE = Memory"
 cat "$DATA_FILE" | ${CLICKHOUSE_CLIENT} -q "INSERT INTO parquet_load FORMAT Parquet SETTINGS input_format_use_lowercase_column_name=true"
@@ -14,7 +14,7 @@ ${CLICKHOUSE_CLIENT} --query="SELECT * FROM parquet_load"
 ${CLICKHOUSE_CLIENT} --query="drop table parquet_load"
 
 echo "ORC"
-DATA_FILE=$CUR_DIR/data_orc/test_setting_input_format_use_lowercase_column_name.orc
+DATA_FILE=$CUR_DIR/02233_data/test_setting_input_format_use_lowercase_column_name.orc
 ${CLICKHOUSE_CLIENT} --query="DROP TABLE IF EXISTS orc_load"
 ${CLICKHOUSE_CLIENT} --query="CREATE TABLE orc_load (id String, score Int32) ENGINE = Memory"
 cat "$DATA_FILE" | ${CLICKHOUSE_CLIENT} -q "INSERT INTO orc_load FORMAT ORC SETTINGS input_format_use_lowercase_column_name=true"
