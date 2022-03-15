7df495d70b5a6d44d2446b8d12355ac0a4e9b618
Nikolai Kochetov <nik-kochetov@yandex-team.ru>
2022-03-15 15:33:43 +0000

    Reset THreadPool's thread memory tracker parent to global after S3 upload task is finished.

--- src/Disks/S3/DiskS3.cpp
+++ src/Disks/S3/DiskS3.cpp
@@ -272,6 +272,17 @@ std::unique_ptr<WriteBufferFromFileBase> DiskS3::writeFile(const String & path, 
             SCOPE_EXIT_SAFE(
                 if (thread_group)
                     CurrentThread::detachQueryIfNotDetached();
+
+                /// After we detached from the thread_group, parent for memory_tracker inside ThreadStatus will be reset to it's parent.
+                /// Typically, it may be changes from Process to User.
+                /// Usually it could be ok, because thread pool task is executed before user-level memory tracker is destroyed.
+                /// However, thread could stay alive inside the thread pool, and it's ThreadStatus as well.
+                /// When, finally, we destroy the thread (and the ThreadStatus),
+                /// it can use memory tracker in the ~ThreadStatus in order to alloc/free untracked_memory,\
+                /// and by this time user-level memory tracker may be already destroyed.
+                ///
+                /// As a work-around, reset memory tracker to total, which is always alive.
+                CurrentThread::get().memory_tracker.setParent(&total_memory_tracker);
             );
             callback();
         });
