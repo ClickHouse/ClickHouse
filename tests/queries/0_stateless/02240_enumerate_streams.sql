93550d547e387802b3720e6f725b47b7b461e971
taiyang-li <654010905@qq.com>
2022-03-15 18:25:36 +0800

    add doc and tests

---
+++ tests/queries/0_stateless/02240_enumerate_streams.sql
@@ -0,0 +1,5 @@
+select enumerateStreams('Array(Int8)');
+select enumerateStreams('Map(String, Int64)');
+select enumerateStreams('Tuple(String, Int64, Float64)');
+select enumerateStreams('LowCardinality(String)');
+select enumerateStreams('Nullable(String)');
