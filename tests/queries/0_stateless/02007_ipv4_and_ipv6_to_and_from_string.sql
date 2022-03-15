225368aa2c94d4a532397df0ce52d5442cbbcb0e
Maksim Kita <maksim@clickhouse.com>
2022-03-15 18:34:23 +0000

    Fixed tests

--- tests/queries/0_stateless/02007_ipv4_and_ipv6_to_and_from_string.sql
+++ tests/queries/0_stateless/02007_ipv4_and_ipv6_to_and_from_string.sql
@@ -1,13 +1,11 @@
-SET cast_ipv4_ipv6_default_on_conversion_error = 1;
-
 SELECT CAST('127.0.0.1' as IPv4) as v, toTypeName(v);
 SELECT CAST(toIPv4('127.0.0.1') as String) as v, toTypeName(v);
 
 SELECT CAST('2001:0db8:0000:85a3:0000:0000:ac1f:8001' as IPv6) as v, toTypeName(v);
 SELECT CAST(toIPv6('2001:0db8:0000:85a3:0000:0000:ac1f:8001') as String) as v, toTypeName(v);
 
-SELECT toIPv4('hello') as v, toTypeName(v);
-SELECT toIPv6('hello') as v, toTypeName(v);
+SELECT toIPv4OrDefault('hello') as v, toTypeName(v);
+SELECT toIPv6OrDefault('hello') as v, toTypeName(v);
 
 SELECT CAST('hello' as IPv4) as v, toTypeName(v); -- { serverError CANNOT_PARSE_DOMAIN_VALUE_FROM_STRING }
 SELECT CAST('hello' as IPv6) as v, toTypeName(v); -- { serverError CANNOT_PARSE_DOMAIN_VALUE_FROM_STRING }
