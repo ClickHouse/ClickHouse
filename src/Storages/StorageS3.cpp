29476e248fb2aae53974f7ed850f0f7e983e81fd
Saad Ur Rahman <saadurrahman@apache.org>
2022-03-14 22:03:58 -0400

    [Storages] Added Version ID parameter for S3.

--- src/Storages/StorageS3.cpp
+++ src/Storages/StorageS3.cpp
@@ -274,7 +274,7 @@ bool StorageS3Source::initialize()
     file_path = fs::path(bucket) / current_key;
 
     read_buf = wrapReadBufferWithCompressionMethod(
-        std::make_unique<ReadBufferFromS3>(client, bucket, current_key, max_single_read_retries, getContext()->getReadSettings()),
+        std::make_unique<ReadBufferFromS3>(client, bucket, version_id, current_key, max_single_read_retries, getContext()->getReadSettings()),
         chooseCompressionMethod(current_key, compression_hint));
     auto input_format = getContext()->getInputFormat(format, *read_buf, sample_block, max_block_size, format_settings);
     QueryPipelineBuilder builder;
@@ -927,7 +927,12 @@ ColumnsDescription StorageS3::getTableStructureFromDataImpl(
 
             return wrapReadBufferWithCompressionMethod(
                 std::make_unique<ReadBufferFromS3>(
-                    client_auth.client, client_auth.uri.bucket, current_key, max_single_read_retries, ctx->getReadSettings()),
+                    client_auth.client,
+                    client_auth.uri.bucket,
+                    client_auth.uri.version_id,
+                    current_key,
+                    max_single_read_retries,
+                    ctx->getReadSettings()),
                 chooseCompressionMethod(current_key, compression_method));
         };
 
