082ce9fb4d971d17cdc98098b4e6262fc2086680
avogar <pav.cruglov@yandex.ru>
2022-03-17 12:28:43 +0000

    Fix tests

--- tests/queries/0_stateless/01059_storage_file_compression.sh
+++ tests/queries/0_stateless/01059_storage_file_compression.sh
@@ -6,7 +6,7 @@ CURDIR=$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)
 # shellcheck source=../shell_config.sh
 . "$CURDIR"/../shell_config.sh
 
-for m in br
+for m in gz br xz zst lz4 bz2
 do
     ${CLICKHOUSE_CLIENT} --query "DROP TABLE IF EXISTS file"
     ${CLICKHOUSE_CLIENT} --query "CREATE TABLE file (x UInt64) ENGINE = File(TSV, '${CLICKHOUSE_DATABASE}/${m}.tsv.${m}')"
@@ -15,3 +15,11 @@ do
     ${CLICKHOUSE_CLIENT} --query "SELECT count(), max(x) FROM file"
     ${CLICKHOUSE_CLIENT} --query "DROP TABLE file"
 done
+
+${CLICKHOUSE_CLIENT} --query "SELECT count(), max(x) FROM file('${CLICKHOUSE_DATABASE}/{gz,br,xz,zst,lz4,bz2}.tsv.{gz,br,xz,zst,lz4,bz2}', TSV, 'x UInt64')"
+
+for m in gz br xz zst lz4 bz2
+do
+    ${CLICKHOUSE_CLIENT} --query "SELECT count() < 4000000, max(x) FROM file('${CLICKHOUSE_DATABASE}/${m}.tsv.${m}', RowBinary, 'x UInt8', 'none')"
+done
+
