d957f5a37e333a555ba72e65ebc9bb6b5989ae75
Sergei Trifonov <sergei@clickhouse.com>
2022-03-17 13:21:32 +0100

    add comments in threadPoolCallbackRunner.h

--- src/Common/threadPoolCallbackRunner.h
+++ src/Common/threadPoolCallbackRunner.h
@@ -6,8 +6,10 @@
 namespace DB
 {
 
+/// High-order function to run callbacks (functions with 'void()' signature) somewhere asynchronously
 using CallbackRunner = std::function<void(std::function<void()>)>;
 
+/// Creates CallbackRunner that runs every callback with 'pool->scheduleOrThrow()'
 CallbackRunner threadPoolCallbackRunner(ThreadPool & pool);
 
 }
