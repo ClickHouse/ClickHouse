d277614eea33f436bf52275ceb478cad8df7a4a5
Maksim Kita <kitaetoya@gmail.com>
2022-03-17 12:33:56 +0100

    Update 02233_set_enable_with_statement_cte_perf.sql

--- tests/queries/0_stateless/02233_set_enable_with_statement_cte_perf.sql
+++ tests/queries/0_stateless/02233_set_enable_with_statement_cte_perf.sql
@@ -4,10 +4,10 @@ DROP TABLE IF EXISTS idx;
 CREATE TABLE ev (a Int32, b Int32) Engine=MergeTree() ORDER BY a;
 CREATE TABLE idx (a Int32) Engine=MergeTree() ORDER BY a;
 INSERT INTO ev SELECT number, number FROM numbers(10000000);
-INSERT INTO idx SELECT number*5 FROM numbers(1000);
+INSERT INTO idx SELECT number * 5 FROM numbers(1000);
 
 -- test_enable_global_with_statement_performance_1
-WITH 'test' AS u SELECT count()FROM ev WHERE a IN (SELECT a FROM idx) SETTINGS enable_global_with_statement = 1;
+WITH 'test' AS u SELECT count() FROM ev WHERE a IN (SELECT a FROM idx) SETTINGS enable_global_with_statement = 1;
 
 -- test_enable_global_with_statement_performance_2
 SELECT count() FROM ev WHERE a IN (SELECT a FROM idx) SETTINGS enable_global_with_statement = 1;
