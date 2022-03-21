6e2684a922006496719b5b77f4af7eacd91c28ff
Igor Markelov <0987654321lion.zz@gmail.com>
2022-03-21 18:15:29 +0000

    Diameter calculation

--- src/AggregateFunctions/AggregateFunctionGraphDiameter.cpp
+++ src/AggregateFunctions/AggregateFunctionGraphDiameter.cpp
@@ -1,5 +1,7 @@
 #include "AggregateFunctionGraphOperation.h"
 
+#include <utility>
+
 namespace DB
 {
 
@@ -10,8 +12,30 @@ public:
 
     String getName() const override { return "GraphDiameter"; }
 
+    std::pair<UInt64, StringRef> calculateDiameter(const StringRef& vertex, const StringRef& parent, const HashMap<StringRef, std::vector<StringRef>>& graph) {
+        std::pair<UInt64, StringRef> answer = {0, vertex};
+        for (const auto& next : graph.at(vertex)) {
+            if (next == parent) {
+                continue;
+            }
+            const auto& cur_answer = calculateDiameter(next, vertex, graph);
+            cur_answer.first += 1;
+            if (cur_answer.first > answer.first) {
+                answer = cur_answer;
+            }
+        }
+        return answer;
+    }
+
     UInt64 calculateOperation(ConstAggregateDataPtr __restrict place, [[maybe_unused]] Arena* arena) const override {
-        return this->data(place).edges_count;
+        const auto& graph = this->data(place).graph;
+        if (graph.size() < 2) {
+            return 0;
+        }
+        auto cur = calculateDiameter(graph.begin()->first, graph.begin()->first, graph);
+        auto answer = calculateDiameter(cur.second, cur.second, graph);
+        return answer.first;
+        // return this->data(place).edges_count;
     }
 };
 
