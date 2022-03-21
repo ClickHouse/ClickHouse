04e6e6ab5e4818f8774ecf3878ca2205d8e1aae6
Antonio Andelic <antonio@clickhouse.com>
2022-03-21 14:52:26 +0000

    Add ParallelReadBuffer for S3

--- src/IO/ReadBufferFromS3.h
+++ src/IO/ReadBufferFromS3.h
@@ -1,5 +1,6 @@
 #pragma once
 
+#include <Common/RangeGenerator.h>
 #include <Common/config.h>
 
 #if USE_AWS_S3
@@ -10,6 +11,7 @@
 #include <IO/ReadBuffer.h>
 #include <IO/ReadSettings.h>
 #include <IO/SeekableReadBuffer.h>
+#include <IO/ParallelReadBuffer.h>
 
 #include <aws/s3/model/GetObjectResult.h>
 
@@ -62,6 +64,12 @@ public:
 
     size_t getFileOffsetOfBufferEnd() const override { return offset; }
 
+    void setRange(off_t begin, off_t end)
+    {
+        offset = begin;
+        read_until_position = end;
+    }
+
 private:
     std::unique_ptr<ReadBuffer> initialize();
 
@@ -74,6 +82,71 @@ private:
     /// There is different seek policy for disk seek and for non-disk seek
     /// (non-disk seek is applied for seekable input formats: orc, arrow, parquet).
     bool restricted_seek;
+};
+
+/// Creates separate ReadBufferFromS3 for sequence of ranges of particular object
+class ReadBufferS3Factory : public ParallelReadBuffer::ReadBufferFactory
+{
+public:
+    explicit ReadBufferS3Factory(
+        std::shared_ptr<Aws::S3::S3Client> client_ptr_,
+        const String & bucket_,
+        const String & key_,
+        size_t range_step_,
+        size_t object_size_,
+        UInt64 s3_max_single_read_retries_,
+        const ReadSettings &read_settings)
+        : client_ptr(client_ptr_)
+        , bucket(bucket_)
+        , key(key_)
+        , settings(read_settings)
+        , range_generator(object_size_, range_step_)
+        , range_step(range_step_)
+        , object_size(object_size_)
+        , s3_max_single_read_retries(s3_max_single_read_retries_)
+    {
+        assert(range_step > 0);
+        assert(range_step < object_size);
+    }
+
+    size_t totalRanges() const
+    {
+        return static_cast<size_t>(round(static_cast<float>(object_size) / range_step));
+    }
+
+    SeekableReadBufferPtr getReader() override
+    {
+        const auto next_range = range_generator.nextRange();
+        if (!next_range)
+        {
+            return nullptr;
+        }
+
+        auto reader = std::make_shared<ReadBufferFromS3>(
+                client_ptr, bucket, key, s3_max_single_read_retries, settings);
+        reader->setRange(next_range->first, next_range->second - 1);
+        return reader;
+    }
+
+    off_t seek(off_t off, [[maybe_unused]] int whence) override
+    {
+        range_generator = RangeGenerator{object_size, range_step, static_cast<size_t>(off)};
+        return off;
+    }
+
+    std::optional<size_t> getTotalSize() override { return object_size; }
+
+private:
+    std::shared_ptr<Aws::S3::S3Client> client_ptr;
+    const String bucket;
+    const String key;
+    ReadSettings settings;
+
+    RangeGenerator range_generator;
+    size_t range_step;
+    size_t object_size;
+
+    UInt64 s3_max_single_read_retries;
 };
 
 }
