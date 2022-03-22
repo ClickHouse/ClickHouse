38bc853adae85dc1f4c5f164fea3c8212e3d619a
Mikhail Stetsyuk <stetsyuk@yandex-team.com>
2022-03-22 11:54:28 +0300

    consider the case when cache.get(key) == nullptr in CachingTransform::transform

--- src/Processors/Transforms/CachingTransform.cpp
+++ src/Processors/Transforms/CachingTransform.cpp
@@ -6,12 +6,11 @@ namespace DB
 void CachingTransform::transform(Chunk & chunk)
 {
     CacheKey cache_key{query_ptr, header};
-    auto cache_value = cache.get(cache_key);
-
-    auto header_ = cache_value->first;
-    auto chunks = std::move(cache_value->second);
-    chunks.push_back(chunk.clone());
-    cache.set(cache_key, std::make_shared<Data>(header_, std::move(chunks)));
+    if (cache.get(cache_key) == nullptr)
+    {
+        cache.set(cache_key, std::make_shared<Data>(header, Chunks{}));
+    }
+    cache.get(cache_key)->second.push_back(chunk.clone());
 }
 
 };
