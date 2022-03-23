7225d20d2c2ea6f2c12060f89ea0cdea0e501ed0
Nikita Mikhaylov <nikitamikhaylov@clickhouse.com>
2022-03-23 20:00:09 +0000

    Update test

--- tests/queries/0_stateless/01801_s3_cluster.sql
+++ tests/queries/0_stateless/01801_s3_cluster.sql
@@ -19,3 +19,13 @@ select * from s3Cluster('test_cluster_two_shards', 'http://localhost:11111/test/
 select * from s3Cluster('test_cluster_two_shards', 'http://localhost:11111/test/{a,b,c}.tsv', 'test', 'testtest', 'TSV') ORDER BY a, b, c;
 select * from s3Cluster('test_cluster_two_shards', 'http://localhost:11111/test/{a,b,c}.tsv', 'test', 'testtest', 'TSV', 'a UInt64, b UInt64, c UInt64') ORDER BY a, b, c;
 select * from s3Cluster('test_cluster_two_shards', 'http://localhost:11111/test/{a,b,c}.tsv', 'test', 'testtest', 'TSV', 'a UInt64, b UInt64, c UInt64', 'auto') ORDER BY a, b, c;
+
+
+desc s3Cluster('test_cluster_two_shards', 'http://localhost:11111/test/{a,b,c}.tsv');
+desc s3Cluster('test_cluster_two_shards', 'http://localhost:11111/test/{a,b,c}.tsv', 'TSV');
+desc s3Cluster('test_cluster_two_shards', 'http://localhost:11111/test/{a,b,c}.tsv', 'TSV', 'a UInt64, b UInt64, c UInt64');
+desc s3Cluster('test_cluster_two_shards', 'http://localhost:11111/test/{a,b,c}.tsv', 'test', 'testtest');
+desc s3Cluster('test_cluster_two_shards', 'http://localhost:11111/test/{a,b,c}.tsv', 'TSV', 'a UInt64, b UInt64, c UInt64', 'auto');
+desc s3Cluster('test_cluster_two_shards', 'http://localhost:11111/test/{a,b,c}.tsv', 'test', 'testtest', 'TSV');
+desc s3Cluster('test_cluster_two_shards', 'http://localhost:11111/test/{a,b,c}.tsv', 'test', 'testtest', 'TSV', 'a UInt64, b UInt64, c UInt64');;
+desc s3Cluster('test_cluster_two_shards', 'http://localhost:11111/test/{a,b,c}.tsv', 'test', 'testtest', 'TSV', 'a UInt64, b UInt64, c UInt64', 'auto');;
