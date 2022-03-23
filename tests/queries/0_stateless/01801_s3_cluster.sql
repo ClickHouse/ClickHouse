2e7729423644ee4cc12bf2730655665d3746871a
Nikita Mikhaylov <nikitamikhaylov@clickhouse.com>
2022-03-23 17:34:19 +0000

    Added test

--- tests/queries/0_stateless/01801_s3_cluster.sql
+++ tests/queries/0_stateless/01801_s3_cluster.sql
@@ -1,5 +1,21 @@
 -- Tags: no-fasttest
 -- Tag no-fasttest: Depends on AWS
 
+select * from s3('http://localhost:11111/test/{a,b,c}.tsv') ORDER BY a, b, c;
+select * from s3('http://localhost:11111/test/{a,b,c}.tsv', 'TSV') ORDER BY a, b, c;
+select * from s3('http://localhost:11111/test/{a,b,c}.tsv', 'TSV', 'a UInt64, b UInt64, c UInt64') ORDER BY a, b, c;
+select * from s3('http://localhost:11111/test/{a,b,c}.tsv', 'test', 'testtest') ORDER BY a, b, c;
+select * from s3('http://localhost:11111/test/{a,b,c}.tsv', 'TSV', 'a UInt64, b UInt64, c UInt64', 'auto') ORDER BY a, b, c;
+select * from s3('http://localhost:11111/test/{a,b,c}.tsv', 'test', 'testtest', 'TSV') ORDER BY a, b, c;
 select * from s3('http://localhost:11111/test/{a,b,c}.tsv', 'test', 'testtest', 'TSV', 'a UInt64, b UInt64, c UInt64') ORDER BY a, b, c;
+select * from s3('http://localhost:11111/test/{a,b,c}.tsv', 'test', 'testtest', 'TSV', 'a UInt64, b UInt64, c UInt64', 'auto') ORDER BY a, b, c;
+
+
+select * from s3Cluster('test_cluster_two_shards', 'http://localhost:11111/test/{a,b,c}.tsv') ORDER BY a, b, c;
+select * from s3Cluster('test_cluster_two_shards', 'http://localhost:11111/test/{a,b,c}.tsv', 'TSV') ORDER BY a, b, c;
+select * from s3Cluster('test_cluster_two_shards', 'http://localhost:11111/test/{a,b,c}.tsv', 'TSV', 'a UInt64, b UInt64, c UInt64') ORDER BY a, b, c;
+select * from s3Cluster('test_cluster_two_shards', 'http://localhost:11111/test/{a,b,c}.tsv', 'test', 'testtest') ORDER BY a, b, c;
+select * from s3Cluster('test_cluster_two_shards', 'http://localhost:11111/test/{a,b,c}.tsv', 'TSV', 'a UInt64, b UInt64, c UInt64', 'auto') ORDER BY a, b, c;
+select * from s3Cluster('test_cluster_two_shards', 'http://localhost:11111/test/{a,b,c}.tsv', 'test', 'testtest', 'TSV') ORDER BY a, b, c;
 select * from s3Cluster('test_cluster_two_shards', 'http://localhost:11111/test/{a,b,c}.tsv', 'test', 'testtest', 'TSV', 'a UInt64, b UInt64, c UInt64') ORDER BY a, b, c;
+select * from s3Cluster('test_cluster_two_shards', 'http://localhost:11111/test/{a,b,c}.tsv', 'test', 'testtest', 'TSV', 'a UInt64, b UInt64, c UInt64', 'auto') ORDER BY a, b, c;
