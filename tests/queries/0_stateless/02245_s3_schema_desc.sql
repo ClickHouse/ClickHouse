be6a98311259a9cdb983c2532d51afb8ca7b8a8d
Nikita Mikhaylov <mikhaylovnikitka@gmail.com>
2022-03-25 19:56:34 +0100

    Update 02245_s3_schema_desc.sql

--- tests/queries/0_stateless/02245_s3_schema_desc.sql
+++ tests/queries/0_stateless/02245_s3_schema_desc.sql
@@ -1,3 +1,5 @@
+-- Tags: no-fasttest
+-- Tag no-fasttest: Depends on AWS
 
 desc s3Cluster('test_cluster_two_shards_localhost', 'http://localhost:11111/test/{a,b,c}.tsv');
 desc s3Cluster('test_cluster_two_shards_localhost', 'http://localhost:11111/test/{a,b,c}.tsv', 'TSV');
