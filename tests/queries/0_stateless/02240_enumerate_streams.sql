c9acc550087d60d2316c083bc57e92573c845b04
taiyang-li <654010905@qq.com>
2022-03-25 14:51:59 +0800

    rename function name

--- tests/queries/0_stateless/02240_enumerate_streams.sql
+++ tests/queries/0_stateless/02240_enumerate_streams.sql
@@ -1,8 +1,8 @@
-select enumerateStreams('Array(Int8)');
-select enumerateStreams('Map(String, Int64)');
-select enumerateStreams('Tuple(String, Int64, Float64)');
-select enumerateStreams('LowCardinality(String)');
-select enumerateStreams('Nullable(String)');
-select enumerateStreams([1,2,3]);
-select enumerateStreams(map('a', 1, 'b', 2));
-select enumerateStreams(tuple('a', 1, 'b', 2));
+select getTypeSerializationStreams('Array(Int8)');
+select getTypeSerializationStreams('Map(String, Int64)');
+select getTypeSerializationStreams('Tuple(String, Int64, Float64)');
+select getTypeSerializationStreams('LowCardinality(String)');
+select getTypeSerializationStreams('Nullable(String)');
+select getTypeSerializationStreams([1,2,3]);
+select getTypeSerializationStreams(map('a', 1, 'b', 2));
+select getTypeSerializationStreams(tuple('a', 1, 'b', 2));
