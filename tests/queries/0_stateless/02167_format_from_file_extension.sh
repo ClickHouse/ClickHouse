8143526cd276e6c9ba3d85b5dbe37352deb93a7b
Anton Kozlov <anton@cloudflare.com>
2022-03-28 16:11:20 +0100

    [test] Adds clean-up for functional test 02167
    Running this test twice now doesn't result in a failure due to the fact that the temporary file is being cleaned up

--- tests/queries/0_stateless/02167_format_from_file_extension.sh
+++ tests/queries/0_stateless/02167_format_from_file_extension.sh
@@ -5,6 +5,12 @@ CURDIR=$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)
 # shellcheck source=../shell_config.sh
 . "$CURDIR"/../shell_config.sh
 
+function cleanup()
+{
+    user_files_path=$(clickhouse-client --query "select _path,_file from file('nonexist.txt', 'CSV', 'val1 char')" 2>&1 | grep Exception | awk '{gsub("/nonexist.txt","",$9); print $9}')
+    rm $user_files_path/test_02167.*
+}
+trap cleanup EXIT
 
 for format in TSV TabSeparated TSVWithNames TSVWithNamesAndTypes CSV Parquet ORC Arrow JSONEachRow JSONCompactEachRow CustomSeparatedWithNamesAndTypes
 do
