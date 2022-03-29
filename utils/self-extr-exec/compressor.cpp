e7b5e744620b75d3bcf13527ebb4ed9eefb88258
FArthur-cmd <613623@mail.ru>
2022-03-29 19:30:09 +0000

    improve self-extr-executable

--- utils/self-extr-exec/compressor.cpp
+++ utils/self-extr-exec/compressor.cpp
@@ -18,12 +18,12 @@ int doCompress(char * input, char * output, off_t & in_offset, off_t & out_offse
         return 1;
     }
     in_offset += input_size;
-    out_offset += output_size;
+    out_offset += compressed_size;
     return 0;
 }
 
 /// compress data from opened file into output file
-int compress(int in_fd, int out_fd, int level=3)
+int compress(int in_fd, int out_fd, int level=5)
 {
     /// read data about input file
     struct stat info_in;
@@ -32,25 +32,33 @@ int compress(int in_fd, int out_fd, int level=3)
 	    std::cout << "Empty input file" << std::endl;
 	    return 1;
     }
+    std::cout << "In current size is " << info_in.st_size << std::endl;
 
     /// Read data about output file.
     /// Compressed data will be added to the end of file
     /// It will allow to create self extracting executable from file
     struct stat info_out;
-    fstat(out_fd, &info_out);
+    fstat(out_fd, &info_out); 
+    std::cout << "Out current size is " << info_out.st_size << std::endl;
+
+    /// NOTE: next parametrs depend on binary size
+    // 6402520 is size of stripped decompressor
+    size_t start = 6405000ull;
+
+    // 22550744 size of decompressor
+    // size_t start = 22550780ull;
 
     /// As experiments showed, size of compressed file is 4 times less than clickhouse executable
     /// Get a little bit more memory to prevent errors with size. 
     /// For compression this difference will not be huge
-    ftruncate(out_fd, info_out.st_size + info_in.st_size / 3);
-    off_t in_offset = 0, out_offset = info_out.st_size;
+    ftruncate(out_fd, start + info_in.st_size / 3);
+    off_t in_offset = 0, out_offset = start;
 
     /// mmap files
-    char * input = static_cast<char*>(mmap(nullptr, info_in.st_size, PROT_READ | PROT_EXEC, MAP_SHARED , in_fd, 0));
-    char * output = static_cast<char*>(mmap(nullptr, info_out.st_size + info_in.st_size / 3, PROT_READ | PROT_WRITE | PROT_EXEC, MAP_SHARED, out_fd, 0));
+    char * input = static_cast<char*>(mmap(nullptr, info_in.st_size, PROT_READ, MAP_PRIVATE , in_fd, 0));
+    char * output = static_cast<char*>(mmap(nullptr, start + info_in.st_size / 3, PROT_READ | PROT_WRITE | PROT_EXEC, MAP_SHARED, out_fd, 0));
     if (input == reinterpret_cast<char*>(-1) || output == reinterpret_cast<char*>(-1))
     {
-        std::cout << (input == reinterpret_cast<char*>(-1)) << " " << (output == reinterpret_cast<char*>(-1)) << std::endl;
         perror(nullptr);
         return 1;
     }
@@ -59,37 +67,36 @@ int compress(int in_fd, int out_fd, int level=3)
     ZSTD_CCtx * cctx = ZSTD_createCCtx();
     ZSTD_CCtx_setParameter(cctx, ZSTD_c_compressionLevel, level);
     ZSTD_CCtx_setParameter(cctx, ZSTD_c_checksumFlag, 1);
-    // ZSTD_CCtx_setParameter(cctx, ZSTD_c_strategy, 9);
 
     /// Remember size of file. It will help to avoid using additional memory 
     /// during decompression
-    // char * file_size = reinterpret_cast<char *>(&info_in.st_size);
-    // for (size_t i = 0; i < sizeof(info_in.st_size)/sizeof(char); ++i)
-    //     output[out_offset++] = *file_size;
+    char * file_size = reinterpret_cast<char *>(&info_in.st_size);
+    for (size_t i = 0; i < sizeof(info_in.st_size)/sizeof(char); ++i)
+        output[out_offset++] = *(file_size + i);
 
 
     /// limits for size of block to prevent high memory usage or bad compression
-    // off_t max_block_size = 100000000ull;
-    // off_t min_block_size = 10000000ull;
+    off_t max_block_size = 1ull<<27;
+    off_t min_block_size = 1ull<<23;
     off_t size = 0;
+    std::cout << in_offset <<  " " << out_offset << std::endl;
 
     /// Compress data
     while (in_offset < info_in.st_size)
     {
         /// take blocks of maximum size
         /// optimize last block (it can be bigger, if it is not too huge)
-        // if (info_in.st_size - in_offset < max_block_size || info_in.st_size - in_offset < max_block_size + min_block_size)
-        //     size = info_in.st_size - in_offset;
-        // else
-        //     size = max_block_size;
-        size = info_in.st_size - in_offset;
+        if (info_in.st_size - in_offset < max_block_size || info_in.st_size - in_offset < max_block_size + min_block_size)
+            size = info_in.st_size - in_offset;
+        else
+            size = max_block_size;
 
         /// Compress data or exit if error happens
         if (0 != doCompress(input, output, in_offset, out_offset, size, ZSTD_compressBound(size), cctx))
         {
             ftruncate(out_fd, info_out.st_size);
             munmap(input, info_in.st_size);
-            munmap(output, info_out.st_size + info_in.st_size / 3);
+            munmap(output, start + info_in.st_size / 3);
             return 1;
         }
         std::cout << in_offset <<  " " << out_offset << std::endl;
@@ -98,7 +105,7 @@ int compress(int in_fd, int out_fd, int level=3)
     /// Shrink file size and unmap
     ftruncate(out_fd, out_offset);
     munmap(input, info_in.st_size);
-    munmap(output, info_out.st_size + info_in.st_size / 3);
+    munmap(output, start + info_in.st_size / 3);
     return 0;
 }
 
@@ -110,15 +117,15 @@ int main(int argc, char* argv[])
         return 0;
     }
 
-    int input_fd = open(argv[1], O_RDWR);
+    int input_fd = open(argv[1], O_RDONLY);
     if (input_fd == -1)
     {
         perror(nullptr);
         return 0;
     }
     
-    int output_fd = open(argv[2], O_RDWR | O_CREAT, 0777);
-    if (input_fd == -1)
+    int output_fd = open(argv[2], O_RDWR | O_CREAT, 0775);
+    if (output_fd == -1)
     {
         perror(nullptr);
         return 0;
--- utils/self-extr-exec/decompressor.cpp
+++ utils/self-extr-exec/decompressor.cpp
@@ -1,7 +1,109 @@
-#include <iostream>
+#include <cstddef>
+#include <cstdio>
+#include <zstd.h>
+#include <sys/mman.h>
+#include <fcntl.h>
+#include <sys/stat.h>
+#include <sys/types.h>
+#include <unistd.h>
+#include <stdlib.h>
+
+/// decompress part
+int doDecompress(char * input, char * output, off_t & in_offset, off_t & out_offset,
+               off_t input_size, off_t output_size, ZSTD_DCtx* dctx)
+{
+    size_t decompressed_size = ZSTD_decompressDCtx(dctx, output + out_offset, output_size, input + in_offset, input_size);
+    if (ZSTD_isError(decompressed_size))
+    {
+        return 1;
+    }
+    in_offset += input_size;
+    out_offset += decompressed_size;
+    return 0;
+}
+
+/// decompress data from in_fd into out_fd
+int decompress(int in_fd, int out_fd)
+{
+    /// Read data about output file.
+    /// Compressed data will replace data in file
+    struct stat info_in;
+    fstat(in_fd, &info_in);
+
+    /// NOTE: next parametrs depend on binary size
+    // 22550780ull for full, 6405000ull for stripped;
+    off_t in_offset = 6405000ull /*size of decompressor*/, out_offset = 0;
+
+    /// mmap files
+    char * input = static_cast<char*>(mmap(nullptr, info_in.st_size, PROT_READ, MAP_SHARED , in_fd, 0));
+    if (input == reinterpret_cast<char*>(-1))
+    {
+        perror(nullptr);
+        return 1;
+    }
+
+    /// Create context
+    ZSTD_DCtx * dctx = ZSTD_createDCtx();
+
+    /// Read size of file. It will help to avoid using additional memory 
+    /// during decompression.
+    size_t * file_size = reinterpret_cast<size_t *>(input + in_offset);
+    in_offset += sizeof(size_t);
+    
+    /// Prepare output file
+    ftruncate(out_fd, *file_size);
+    char * output = static_cast<char*>(mmap(nullptr, *file_size, PROT_READ | PROT_WRITE | PROT_EXEC, MAP_SHARED, out_fd, 0));
+    if (output == reinterpret_cast<char*>(-1))
+    {
+        perror(nullptr);
+        return 1;
+    }    
+
+    off_t size = 0;
+    off_t max_block_size = 1ull<<27;
+
+    /// Compress data
+    while (in_offset < info_in.st_size)
+    {
+        size = ZSTD_findFrameCompressedSize(input + in_offset, max_block_size);
+
+        /// Compress data or exit if error happens
+        if (0 != doDecompress(input, output, in_offset, out_offset, size, max_block_size, dctx))
+        {
+            munmap(input, info_in.st_size);
+            munmap(output, *file_size);
+            return 1;
+        }
+    }
+
+    /// Shrink file size and unmap
+    munmap(output, *file_size);
+    munmap(input, info_in.st_size);
+    return 0;
+}
 
 int main()
 {
-    std::cout << "Test" << std::endl;
+    int input_fd = open("decompressor", O_RDONLY);
+    if (input_fd == -1)
+    {
+        perror(nullptr);
+        return 0;
+    }
+    
+    int output_fd = open("clickhouse_decompressed", O_RDWR | O_CREAT, 0775);
+    if (output_fd == -1)
+    {
+        perror(nullptr);
+        return 0;
+    }
+
+    if (0 != decompress(input_fd, output_fd))
+    {
+        return 1;
+    }
+
+    close(input_fd);
+    close(output_fd);
     return 0;
 }
