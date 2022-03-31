4db5043ed45fffe512d716bd056a3dc5ae76a1d2
Kruglov Pavel <48961922+Avogar@users.noreply.github.com>
2022-03-31 12:50:53 +0200

    Fix test

--- tests/queries/0_stateless/02245_s3_schema_desc.sql
+++ tests/queries/0_stateless/02245_s3_schema_desc.sql
@@ -1,14 +1,14 @@
 -- Tags: no-fasttest
 -- Tag no-fasttest: Depends on AWS
 
-desc file('data_minio/{a,b,c}.tsv');
-desc file('data_minio/{a,b,c}.tsv', 'TSV');
-desc file('data_minio/{a,b,c}.tsv', 'TSV', 'c1 UInt64, c2 UInt64, c3 UInt64');
-desc file('data_minio/{a,b,c}.tsv');
-desc file('data_minio/{a,b,c}.tsv', 'TSV', 'c1 UInt64, c2 UInt64, c3 UInt64', 'auto');
-desc file('data_minio/{a,b,c}.tsv', 'TSV');
-desc file('data_minio/{a,b,c}.tsv', 'TSV', 'c1 UInt64, c2 UInt64, c3 UInt64');
-desc file('data_minio/{a,b,c}.tsv', 'TSV', 'c1 UInt64, c2 UInt64, c3 UInt64', 'auto');
+desc s3Cluster('test_cluster_two_shards_localhost', 'http://localhost:11111/test/{a,b,c}.tsv');
+desc s3Cluster('test_cluster_two_shards_localhost', 'http://localhost:11111/test/{a,b,c}.tsv', 'TSV');
+desc s3Cluster('test_cluster_two_shards_localhost', 'http://localhost:11111/test/{a,b,c}.tsv', 'TSV', 'c1 UInt64, c2 UInt64, c3 UInt64');
+desc s3Cluster('test_cluster_two_shards_localhost', 'http://localhost:11111/test/{a,b,c}.tsv', 'test', 'testtest');
+desc s3Cluster('test_cluster_two_shards_localhost', 'http://localhost:11111/test/{a,b,c}.tsv', 'TSV', 'c1 UInt64, c2 UInt64, c3 UInt64', 'auto');
+desc s3Cluster('test_cluster_two_shards_localhost', 'http://localhost:11111/test/{a,b,c}.tsv', 'test', 'testtest', 'TSV');
+desc s3Cluster('test_cluster_two_shards_localhost', 'http://localhost:11111/test/{a,b,c}.tsv', 'test', 'testtest', 'TSV', 'c1 UInt64, c2 UInt64, c3 UInt64');
+desc s3Cluster('test_cluster_two_shards_localhost', 'http://localhost:11111/test/{a,b,c}.tsv', 'test', 'testtest', 'TSV', 'c1 UInt64, c2 UInt64, c3 UInt64', 'auto');
 
 
 SELECT * FROM s3(decodeURLComponent(NULL), [NULL]);  --{serverError 170}
