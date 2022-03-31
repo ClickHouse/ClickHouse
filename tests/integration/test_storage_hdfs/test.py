d0e5b3571cf6c0455e60312177694f13ef8bac57
lgbo-ustc <lgbo.ustc@gmail.com>
2022-03-31 09:28:07 +0800

    fixed code style

--- tests/integration/test_storage_hdfs/test.py
+++ tests/integration/test_storage_hdfs/test.py
@@ -555,11 +555,11 @@ def test_insert_select_schema_inference(started_cluster):
 
 def test_cluster_join(started_cluster):
     result = node1.query(
-        '''
+        """
         SELECT l.id,r.id FROM hdfsCluster('test_cluster_two_shards', 'hdfs://hdfs1:9000/test_hdfsCluster/file*', 'TSV', 'id UInt32') as l
         JOIN hdfsCluster('test_cluster_two_shards', 'hdfs://hdfs1:9000/test_hdfsCluster/file*', 'TSV', 'id UInt32') as r
         ON l.id = r.id
-        '''
+    """
     )
     assert "AMBIGUOUS_COLUMN_NAME" not in result
 
