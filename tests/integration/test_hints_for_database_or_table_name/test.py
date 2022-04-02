3606c060edcab9f4e6ce46b8e03e3e16a6f5c4cb
Peng Jian <pengjian.uestc@gmail.com>
2022-04-02 18:08:51 +0800

    format test codes

--- tests/integration/test_hints_for_database_or_table_name/test.py
+++ tests/integration/test_hints_for_database_or_table_name/test.py
@@ -17,34 +17,30 @@ def started_cluster():
 
 
 def test_hints_for_database_name(started_cluster):
-    node.query('''
+    node.query("""
         CREATE TABLE default.table_x (i Int64) ENGINE=Memory;
         INSERT INTO default.table_x SELECT 1;
-        ''')
+        """)
 
     with pytest.raises(QueryRuntimeException, match=r'DB::Exception: Database defaul doesn\'t exist. Maybe you meant: \[\'default\'\]'):
-        node.query('''
-            SELECT * FROM defaul.table_y LIMIT 1;
-            ''')
+        node.query("""SELECT * FROM defaul.table_y LIMIT 1;""")
     assert int(node.query('SELECT count() FROM default.table_x')) == 1
-    node.query('''
-            DROP TABLE default.table_x;
-            ''')
+    node.query("""DROP TABLE default.table_x;""")
 
 
 def test_hints_for_table_name(started_cluster):
-    node.query('''
+    node.query("""
         CREATE TABLE default.table_x (i Int64) ENGINE=Memory;
         CREATE TABLE default.table_z (i Int64) ENGINE=Memory;
         INSERT INTO default.table_x SELECT 1;
-        ''')
+        """)
     with pytest.raises(QueryRuntimeException, match=r'DB::Exception: Table default.table_y doesn\'t exist. Maybe you meant: \[\'default.table_x\'\]'):
-        node.query('''
+        node.query("""
             SELECT * FROM default.table_y LIMIT 1;
-            ''')
+            """)
     assert int(node.query('SELECT count() FROM default.table_x')) == 1
-    node.query('''
+    node.query("""
             DROP TABLE default.table_x;
             DROP TABLE default.table_z;
-            ''')
+            """)
 
