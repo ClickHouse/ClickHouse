0fef627ea35597ca051173d86343ce16debc3545
Mikhail Stetsyuk <stetsyuk@yandex-team.com>
2022-04-04 13:46:31 +0300

    add use_query_cache setting

--- src/Interpreters/InterpreterSelectQuery.cpp
+++ src/Interpreters/InterpreterSelectQuery.cpp
@@ -595,7 +595,7 @@ InterpreterSelectQuery::InterpreterSelectQuery(
 
 void InterpreterSelectQuery::buildQueryPlan(QueryPlan & query_plan)
 {
-    if (auto query_result = cache.get({query_ptr, source_header}))
+    if (auto query_result = cache.get({query_ptr, source_header}); context->getSettingsRef().use_query_cache && query_result)
     {
         const auto &header= query_result->first;
         const auto &chunks = query_result->second;
@@ -608,10 +608,12 @@ void InterpreterSelectQuery::buildQueryPlan(QueryPlan & query_plan)
     }
     executeImpl(query_plan, std::move(input_pipe));
 
-    auto caching_step = std::make_unique<CachingStep>(query_plan.getCurrentDataStream(), cache, query_ptr);
-    caching_step->setStepDescription("Cache query result");
-    query_plan.addStep(std::move(caching_step));
-
+    if (context->getSettingsRef().use_query_cache)
+    {
+        auto caching_step = std::make_unique<CachingStep>(query_plan.getCurrentDataStream(), cache, query_ptr);
+        caching_step->setStepDescription("Cache query result");
+        query_plan.addStep(std::move(caching_step));
+    }
     /// We must guarantee that result structure is the same as in getSampleBlock()
     ///
     /// But if it's a projection query, plan header does not match result_header.
