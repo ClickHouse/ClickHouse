c8a13b18f099fe92a021ab1a659bf15acba87215
liugaoji <757394026@qq.com>
2022-04-05 19:52:26 +0800

    fix #35889 Wrong order of NOT NULL data type modifier and DEFAULT

---
+++ tests/queries/0_stateless/02252_create_temporary_table_default_test.sql
@@ -0,0 +1,8 @@
+CREATE TEMPORARY TABLE create_temporary_table_test1 (x INT NOT NULL DEFAULT 1)
+CREATE TEMPORARY TABLE create_temporary_table_test2 (x INT DEFAULT 1 NOT NULL)
+DROP TABLE create_temporary_table_test1
+DROP TABLE create_temporary_table_test2
+
+
+
+
