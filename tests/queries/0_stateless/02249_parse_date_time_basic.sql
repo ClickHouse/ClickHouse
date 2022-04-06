c7d72b92dac1d3650c61bdfd3d6a4286dbc97819
Yakov Olkhovskiy <yakov@clickhouse.com>
2022-04-05 20:32:52 -0400

    explicit timezone added to test

--- tests/queries/0_stateless/02249_parse_date_time_basic.sql
+++ tests/queries/0_stateless/02249_parse_date_time_basic.sql
@@ -1,6 +1,6 @@
 SET date_time_output_format='iso';
 drop table if exists t;
-CREATE TABLE t (a DateTime, b String, c String, d String, e Int32) ENGINE = Memory;
+CREATE TABLE t (a DateTime('UTC'), b String, c String, d String, e Int32) ENGINE = Memory;
 INSERT INTO t(a, b, c, d ,e)  VALUES ('2022-03-31','','','',1);
 INSERT INTO t(a, b, c, d ,e)  VALUES (1648804224,'','','',2);
 INSERT INTO t(a, b, c, d ,e)  VALUES ('2022-03-31 10:18:56','','','',3);
