6c0a6884600c3dfaadfc970172c6b467f47ab924
Nicolae Vartolomei <nv@cloudflare.com>
2022-04-06 09:08:10 +0000

    Fix style

--- src/Storages/MergeTree/TaskObserverMetrics.h
+++ src/Storages/MergeTree/TaskObserverMetrics.h
@@ -5,33 +5,27 @@
 namespace DB
 {
 
-namespace ErrorCodes
-{
-    extern const int LOGICAL_ERROR;
-}
-
 class TaskObserverMetrics : public boost::noncopyable
 {
 public:
-  TaskObserverMetrics() : thread_group(std::make_shared<ThreadGroupStatus>()) {}
-  ~TaskObserverMetrics() {}
+    TaskObserverMetrics() : thread_group(std::make_shared<ThreadGroupStatus>()) { }
+    ~TaskObserverMetrics() { }
 
-  bool doResume()
-  {
-      CurrentThread::attachTo(thread_group);
-      return true;
-  }
+    bool doResume()
+    {
+        CurrentThread::attachTo(thread_group);
+        return true;
+    }
 
-  bool doSuspend()
-  {
-      CurrentThread::detachQueryIfNotDetached();
-      return true;
-  }
+    bool doSuspend()
+    {
+        CurrentThread::detachQueryIfNotDetached();
+        return true;
+    }
 
 
 private:
-  ThreadGroupStatusPtr thread_group;
+    ThreadGroupStatusPtr thread_group;
 };
-
 
 }
