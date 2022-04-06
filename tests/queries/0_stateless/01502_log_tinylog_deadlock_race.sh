c56148425e30e3a90c6a12cb4c64e459c760154e
Azat Khuzhin <a3at.mail@gmail.com>
2022-04-06 09:24:04 +0300

    tests: use proper wait in 01502_log_tinylog_deadlock_race
    CI: https://s3.amazonaws.com/clickhouse-test-reports/35865/e330d691a3a1981582ebbe20e3ecc15247644f19/stateless_tests__address__actions__[2/2].html
    Signed-off-by: Azat Khuzhin <a.khuzhin@semrush.com>

--- tests/queries/0_stateless/01502_log_tinylog_deadlock_race.sh
+++ tests/queries/0_stateless/01502_log_tinylog_deadlock_race.sh
@@ -10,46 +10,40 @@ CURDIR=$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)
 . "$CURDIR"/../shell_config.sh
 
 
-function thread_create {
-    while true; do
-        $CLICKHOUSE_CLIENT --query "CREATE TABLE IF NOT EXISTS $1 (x UInt64, s Array(Nullable(String))) ENGINE = $2"
-        sleep 0.0$RANDOM
-    done
+function thread_create()
+{
+    $CLICKHOUSE_CLIENT --query "CREATE TABLE IF NOT EXISTS $1 (x UInt64, s Array(Nullable(String))) ENGINE = $2"
+    sleep 0.0$RANDOM
 }
 
-function thread_drop {
-    while true; do
-        $CLICKHOUSE_CLIENT --query "DROP TABLE IF EXISTS $1"
-        sleep 0.0$RANDOM
-    done
+function thread_drop()
+{
+    $CLICKHOUSE_CLIENT --query "DROP TABLE IF EXISTS $1"
+    sleep 0.0$RANDOM
 }
 
-function thread_rename {
-    while true; do
-        $CLICKHOUSE_CLIENT --query "RENAME TABLE $1 TO $2" 2>&1 | grep -v -e 'Received exception from server' -e '^(query: ' | grep -v -P 'Code: (60|57)'
-        sleep 0.0$RANDOM
-    done
+function thread_rename()
+{
+    $CLICKHOUSE_CLIENT --query "RENAME TABLE $1 TO $2" 2>&1 | grep -v -e 'Received exception from server' -e '^(query: ' | grep -v -P 'Code: (60|57)'
+    sleep 0.0$RANDOM
 }
 
-function thread_select {
-    while true; do
-        $CLICKHOUSE_CLIENT --query "SELECT * FROM $1 FORMAT Null" 2>&1 | grep -v -e 'Received exception from server' -e '^(query: ' | grep -v -P 'Code: (60|218)'
-        sleep 0.0$RANDOM
-    done
+function thread_select()
+{
+    $CLICKHOUSE_CLIENT --query "SELECT * FROM $1 FORMAT Null" 2>&1 | grep -v -e 'Received exception from server' -e '^(query: ' | grep -v -P 'Code: (60|218)'
+    sleep 0.0$RANDOM
 }
 
-function thread_insert {
-    while true; do
-        $CLICKHOUSE_CLIENT --query "INSERT INTO $1 SELECT rand64(1), [toString(rand64(2))] FROM numbers($2)" 2>&1 | grep -v -e 'Received exception from server' -e '^(query: '| grep -v -P 'Code: (60|218)'
-        sleep 0.0$RANDOM
-    done
+function thread_insert()
+{
+    $CLICKHOUSE_CLIENT --query "INSERT INTO $1 SELECT rand64(1), [toString(rand64(2))] FROM numbers($2)" 2>&1 | grep -v -e 'Received exception from server' -e '^(query: '| grep -v -P 'Code: (60|218)'
+    sleep 0.0$RANDOM
 }
 
-function thread_insert_select {
-    while true; do
-        $CLICKHOUSE_CLIENT --query "INSERT INTO $1 SELECT * FROM $2" 2>&1 | grep -v -e 'Received exception from server' -e '^(query: ' | grep -v -P 'Code: (60|218)'
-        sleep 0.0$RANDOM
-    done
+function thread_insert_select()
+{
+    $CLICKHOUSE_CLIENT --query "INSERT INTO $1 SELECT * FROM $2" 2>&1 | grep -v -e 'Received exception from server' -e '^(query: ' | grep -v -P 'Code: (60|218)'
+    sleep 0.0$RANDOM
 }
 
 export -f thread_create
@@ -65,18 +59,18 @@ export -f thread_insert_select
 function test_with_engine {
     echo "Testing $1"
 
-    timeout 10 bash -c "thread_create t1 $1" &
-    timeout 10 bash -c "thread_create t2 $1" &
-    timeout 10 bash -c 'thread_drop t1' &
-    timeout 10 bash -c 'thread_drop t2' &
-    timeout 10 bash -c 'thread_rename t1 t2' &
-    timeout 10 bash -c 'thread_rename t2 t1' &
-    timeout 10 bash -c 'thread_select t1' &
-    timeout 10 bash -c 'thread_select t2' &
-    timeout 10 bash -c 'thread_insert t1 5' &
-    timeout 10 bash -c 'thread_insert t2 10' &
-    timeout 10 bash -c 'thread_insert_select t1 t2' &
-    timeout 10 bash -c 'thread_insert_select t2 t1' &
+    clickhouse_client_loop_timeout 10 thread_create t1 $1 &
+    clickhouse_client_loop_timeout 10 thread_create t2 $1 &
+    clickhouse_client_loop_timeout 10 thread_drop t1 &
+    clickhouse_client_loop_timeout 10 thread_drop t2 &
+    clickhouse_client_loop_timeout 10 thread_rename t1 t2 &
+    clickhouse_client_loop_timeout 10 thread_rename t2 t1 &
+    clickhouse_client_loop_timeout 10 thread_select t1 &
+    clickhouse_client_loop_timeout 10 thread_select t2 &
+    clickhouse_client_loop_timeout 10 thread_insert t1 5 &
+    clickhouse_client_loop_timeout 10 thread_insert t2 10 &
+    clickhouse_client_loop_timeout 10 thread_insert_select t1 t2 &
+    clickhouse_client_loop_timeout 10 thread_insert_select t2 t1 &
 
     wait
     echo "Done $1"
