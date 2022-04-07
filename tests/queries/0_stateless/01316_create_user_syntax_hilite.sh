edbc03c50ea94da315daa0bb235ee0782a308c6b
LAL2211 <lalitsrikant@ibm.com>
2022-04-07 14:50:10 -0400

    removed test script Hardcoded Database Account Password

--- tests/queries/0_stateless/01316_create_user_syntax_hilite.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-
-CURDIR=$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)
-# shellcheck source=../shell_config.sh
-. "$CURDIR"/../shell_config.sh
-
-set -e
-
-$CLICKHOUSE_FORMAT --hilite <<< "CREATE USER user IDENTIFIED WITH PLAINTEXT_PASSWORD BY 'hello'"
