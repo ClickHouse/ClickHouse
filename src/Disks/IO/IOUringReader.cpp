7b5e13a9ebe811ad05cbd72463c8456a4e91fb15
Saulius Valatka <saulius.vl@gmail.com>
2022-04-12 23:20:46 +0300

    implement pending request queue to prevent CQ overflows, more error checking, reduce size of io_uring

--- src/Disks/IO/IOUringReader.cpp
+++ src/Disks/IO/IOUringReader.cpp
@@ -1,13 +1,13 @@
 #if defined(OS_LINUX)
 
 #include "IOUringReader.h"
+#include <base/errnoToString.h>
 #include <Common/assert_cast.h>
 #include <Common/Exception.h>
 #include <Common/ProfileEvents.h>
 #include <Common/CurrentMetrics.h>
 #include <Common/Stopwatch.h>
 #include <Common/setThreadName.h>
-#include <base/errnoToString.h>
 #include <future>
 
 namespace ProfileEvents
@@ -16,7 +16,6 @@ namespace ProfileEvents
     extern const Event ReadBufferFromFileDescriptorReadFailed;
     extern const Event ReadBufferFromFileDescriptorReadBytes;
 
-    extern const Event IOUringSQEQueueFullRetries;
     extern const Event IOUringSQEsSubmitted;
     extern const Event IOUringSQEsResubmits;
     extern const Event IOUringShortReads;
@@ -24,7 +23,8 @@ namespace ProfileEvents
 
 namespace CurrentMetrics
 {
-    extern const Metric IOUringEnqueuedEvents;
+    extern const Metric IOUringPendingEvents;
+    extern const Metric IOUringInFlightEvents;
     extern const Metric Read;
 }
 
@@ -36,11 +36,11 @@ namespace ErrorCodes
     extern const int LOGICAL_ERROR;
     extern const int CANNOT_READ_FROM_FILE_DESCRIPTOR;
     extern const int IO_URING_INIT_FAILED;
-    extern const int IO_URING_QUEUE_FULL;
+    extern const int IO_URING_SUBMIT_ERROR;
     extern const int IO_URING_WAIT_ERROR;
 }
 
-IOUringReader::IOUringReader(size_t queue_size_)
+IOUringReader::IOUringReader(uint32_t entries_)
 {
     struct io_uring_probe * probe = io_uring_get_probe();
     if (!probe)
@@ -55,10 +55,17 @@ IOUringReader::IOUringReader(size_t queue_size_)
     if (!is_supported)
         return;
 
-    int ret = io_uring_queue_init(queue_size_, &ring, 0);
-    if (ret < 0)
-        throwFromErrno(fmt::format("Failed initializing io_uring of size {}", queue_size_), ErrorCodes::IO_URING_INIT_FAILED);
+    struct io_uring_params params =
+    {
+        .cq_entries = 0, // filled by the kernel, initializing to silence warning
+        .flags = 0,
+    };
 
+    int ret = io_uring_queue_init_params(entries_, &ring, &params);
+    if (ret < 0)
+        throwFromErrno("Failed initializing io_uring", ErrorCodes::IO_URING_INIT_FAILED, -ret);
+
+    cq_entries = params.cq_entries;
     ring_completion_monitor = ThreadFromGlobalPool([this] { monitorRing(); });
 }
 
@@ -66,72 +73,118 @@ std::future<IAsynchronousReader::Result> IOUringReader::submit(Request request)
 {
     assert(request.size);
 
-    // take lock here because we're modifying an std::map and submitting to the ring,
-    // the monitor thread can also modify the map and re-submit events
+    // take lock here because we're modifying containers and submitting to the ring,
+    // the monitor thread can also do the same
     std::unique_lock lock{mutex};
 
     // use the requested read destination address as the request id, the assumption
     // here is that we won't get asked to fill in the same address more than once in parallel
     auto request_id = reinterpret_cast<UInt64>(request.buf);
 
-    const auto [kv, is_newly_inserted] = enqueued_requests.emplace(request_id, EnqueuedRequest{
-        .promise = std::promise<IAsynchronousReader::Result>{},
+    std::promise<IAsynchronousReader::Result> promise;
+    auto enqueued_request = EnqueuedRequest
+    {
+        .promise = std::move(promise),
         .request = request,
+        .resubmitting = false,
         .bytes_read = 0
-    });
+    };
 
-    if (!is_newly_inserted)
-        return makeFailedResult(ErrorCodes::LOGICAL_ERROR, "Tried enqueuing read request for {} that is already submitted", request_id);
-
-    EnqueuedRequest & enqueued = kv->second;
-    trySubmitRequest(request_id, enqueued, false);
-
-    return enqueued.promise.get_future();
-}
-
-bool IOUringReader::trySubmitRequest(UInt64 request_id, EnqueuedRequest & enqueued, bool resubmitting)
-{
-    auto request = enqueued.request;
-    int fd = assert_cast<const LocalFileDescriptor &>(*request.descriptor).fd;
-
-    struct io_uring_sqe * sqe = nullptr;
-    for (int i = 0; i < 1000; ++i) // try a few times in case the queue is full.
+    // if there's room in the completion queue submit the request to the ring immediately,
+    // otherwise push it to the back of the pending queue
+    if (in_flight_requests.size() < cq_entries)
     {
-        sqe = io_uring_get_sqe(&ring);
-        if (sqe) break;
-
-        ProfileEvents::increment(ProfileEvents::IOUringSQEQueueFullRetries);
-    }
-
-    int submitted = 0;
-    if (sqe)
-    {
-        io_uring_sqe_set_data(sqe, reinterpret_cast<void*>(request_id));
-        io_uring_prep_read(sqe, fd, request.buf, request.size - enqueued.bytes_read, request.offset + enqueued.bytes_read);
-        submitted = io_uring_submit(&ring);
-    }
-
-    if (submitted == 0)
-    {
-        failPromise(enqueued.promise, ErrorCodes::IO_URING_QUEUE_FULL, "Submission queue is full, failed to get an SQE");
-        if (resubmitting) // an existing request failed, decrement counters
+        if (submitToRing(enqueued_request))
+        {
+            const auto [kv, success] = in_flight_requests.emplace(request_id, std::move(enqueued_request));
+            if (!success)
+            {
+                ProfileEvents::increment(ProfileEvents::ReadBufferFromFileDescriptorReadFailed);
+                return makeFailedResult(ErrorCodes::LOGICAL_ERROR, "Tried enqueuing read request for {} that is already submitted", request_id);
+            }
+            return (kv->second).promise.get_future();
+        }
+        else
         {
             ProfileEvents::increment(ProfileEvents::ReadBufferFromFileDescriptorReadFailed);
-            CurrentMetrics::sub(CurrentMetrics::IOUringEnqueuedEvents);
-            CurrentMetrics::sub(CurrentMetrics::Read);
+            return makeFailedResult(ErrorCodes::IO_URING_SUBMIT_ERROR, "Failed submitting SQE");
         }
-
-        return false;
     }
+    else
+    {
+        CurrentMetrics::add(CurrentMetrics::IOUringPendingEvents);
+        pending_requests.push_back(std::move(enqueued_request));
+        return pending_requests.back().promise.get_future();
+    }
+}
 
-    if (!resubmitting) // new request, increment counters
+bool IOUringReader::submitToRing(EnqueuedRequest & enqueued)
+{
+    struct io_uring_sqe * sqe = io_uring_get_sqe(&ring);
+    if (!sqe)
+        return false;
+
+    auto request = enqueued.request;
+    auto request_id = reinterpret_cast<UInt64>(request.buf);
+    int fd = assert_cast<const LocalFileDescriptor &>(*request.descriptor).fd;
+
+    io_uring_sqe_set_data64(sqe, request_id);
+    io_uring_prep_read(sqe, fd, request.buf, request.size - enqueued.bytes_read, request.offset + enqueued.bytes_read);
+    int submitted = io_uring_submit(&ring);
+
+    if (submitted <= 0)
+        return false;
+
+    if (!enqueued.resubmitting)
     {
         ProfileEvents::increment(ProfileEvents::IOUringSQEsSubmitted);
-        CurrentMetrics::add(CurrentMetrics::IOUringEnqueuedEvents);
+        CurrentMetrics::add(CurrentMetrics::IOUringInFlightEvents);
         CurrentMetrics::add(CurrentMetrics::Read);
     }
 
     return true;
+}
+
+void IOUringReader::failRequest(const EnqueuedIterator & requestIt, int code, const std::string & message)
+{
+    ProfileEvents::increment(ProfileEvents::ReadBufferFromFileDescriptorReadFailed);
+    (requestIt->second).promise.set_exception(std::make_exception_ptr(Exception(code, message)));
+
+    finalizeRequest(requestIt);
+}
+
+void IOUringReader::finalizeRequest(const EnqueuedIterator & requestIt)
+{
+    in_flight_requests.erase(requestIt);
+
+    CurrentMetrics::sub(CurrentMetrics::IOUringInFlightEvents);
+    CurrentMetrics::sub(CurrentMetrics::Read);
+
+    // since we just finalized a request there's now room in the completion queue,
+    // see if there are any pending requests and submit one from the front of the queue
+    if (!pending_requests.empty())
+    {
+        auto pending_request = std::move(pending_requests.front());
+        pending_requests.pop_front();
+
+        if (submitToRing(pending_request))
+        {
+            auto request_id = reinterpret_cast<UInt64>(pending_request.request.buf);
+            if (!in_flight_requests.contains(request_id))
+                in_flight_requests.emplace(request_id, std::move(pending_request));
+            else
+                failPromise(pending_request.promise,
+                    ErrorCodes::LOGICAL_ERROR,
+                    "Tried enqueuing pending read request for {} that is already submitted", request_id);
+        }
+        else
+        {
+            failPromise(pending_request.promise, ErrorCodes::IO_URING_SUBMIT_ERROR, "Failed submitting SQE for pending request");
+            ProfileEvents::increment(ProfileEvents::ReadBufferFromFileDescriptorReadFailed);
+        }
+
+        CurrentMetrics::sub(CurrentMetrics::IOUringPendingEvents);
+    }
 }
 
 void IOUringReader::monitorRing()
@@ -146,8 +199,11 @@ void IOUringReader::monitorRing()
         struct io_uring_cqe * cqe;
         int ret = io_uring_wait_cqe(&ring, &cqe);
 
+        if (ret == -EAGAIN)
+            continue;
+
         if (ret < 0)
-            throwFromErrno(fmt::format("Failed waiting for io_uring CQEs: {}", ret), ErrorCodes::IO_URING_WAIT_ERROR);
+            throwFromErrno("Failed waiting for io_uring CQEs", ErrorCodes::IO_URING_WAIT_ERROR, -ret);
 
         // user_data zero means a noop event sent from the destructor meant to interrupt the thread
         if (cancelled.load(std::memory_order_relaxed) || cqe->user_data == 0)
@@ -157,34 +213,28 @@ void IOUringReader::monitorRing()
         std::unique_lock lock{mutex};
 
         auto request_id = cqe->user_data;
-        const auto it = enqueued_requests.find(request_id);
-        if (it == enqueued_requests.end())
-            throwFromErrno(
-                fmt::format("Got a completion event for a request {} that was not submitted", request_id),
-                ErrorCodes::LOGICAL_ERROR);
+        const auto it = in_flight_requests.find(request_id);
+        if (it == in_flight_requests.end())
+            throw Exception(ErrorCodes::LOGICAL_ERROR, "Got a completion event for a request {} that was not submitted", request_id);
 
         auto & enqueued = it->second;
 
+        if (cqe->res == -EAGAIN)
+        {
+            enqueued.resubmitting = true;
+            ProfileEvents::increment(ProfileEvents::IOUringSQEsResubmits);
+
+            if (!submitToRing(enqueued))
+                failRequest(it, ErrorCodes::IO_URING_SUBMIT_ERROR, "Failed re-submitting SQE");
+
+            io_uring_cqe_seen(&ring, cqe);
+            continue;
+        }
+
         if (cqe->res < 0)
         {
-            if (cqe->res == -EAGAIN)
-            {
-                ProfileEvents::increment(ProfileEvents::IOUringSQEsResubmits);
-
-                trySubmitRequest(request_id, enqueued, true);
-                io_uring_cqe_seen(&ring, cqe);
-
-                continue;
-            }
-
             int fd = assert_cast<const LocalFileDescriptor &>(*enqueued.request.descriptor).fd;
-
-            failPromise(enqueued.promise, ErrorCodes::CANNOT_READ_FROM_FILE_DESCRIPTOR, "Cannot read from file {}", fd);
-            enqueued_requests.erase(it);
-
-            ProfileEvents::increment(ProfileEvents::ReadBufferFromFileDescriptorReadFailed);
-            CurrentMetrics::sub(CurrentMetrics::IOUringEnqueuedEvents);
-            CurrentMetrics::sub(CurrentMetrics::Read);
+            failRequest(it, ErrorCodes::CANNOT_READ_FROM_FILE_DESCRIPTOR, fmt::format("Cannot read from file {}", fd));
 
             io_uring_cqe_seen(&ring, cqe);
             continue;
@@ -203,16 +253,15 @@ void IOUringReader::monitorRing()
         if (bytes_read > 0 && total_bytes_read < enqueued.request.size)
         {
             // potential short read, re-submit
+            enqueued.resubmitting = true;
             enqueued.bytes_read += bytes_read;
-            trySubmitRequest(request_id, enqueued, true);
+            if (!submitToRing(enqueued))
+                failRequest(it, ErrorCodes::IO_URING_SUBMIT_ERROR, "Failed re-submitting SQE for short read");
         }
         else
         {
             enqueued.promise.set_value(Result{ .size = total_bytes_read, .offset = enqueued.request.ignore });
-            enqueued_requests.erase(it);
-
-            CurrentMetrics::sub(CurrentMetrics::IOUringEnqueuedEvents);
-            CurrentMetrics::sub(CurrentMetrics::Read);
+            finalizeRequest(it);
         }
 
         io_uring_cqe_seen(&ring, cqe);
