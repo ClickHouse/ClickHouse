99848df6ce179aea048cc0c61f6fad6edc3137bb
Azat Khuzhin <a3at.mail@gmail.com>
2022-04-12 20:38:16 +0300

    tests: do not use process substution in 02050_client_profile_events
    bash does not waits then and so if the next line will be executed fast
    enough, result of process substituted command can appers in stdout after
    it.
    
    Signed-off-by: Azat Khuzhin <a.khuzhin@semrush.com>

--- tests/queries/0_stateless/02050_client_profile_events.sh
+++ tests/queries/0_stateless/02050_client_profile_events.sh
@@ -8,7 +8,7 @@ echo 'do not print any ProfileEvents packets'
 $CLICKHOUSE_CLIENT -q 'select * from numbers(1e5) format Null' |& grep -c 'SelectedRows'
 
 echo 'print only last (and also number of rows to provide more info in case of failures)'
-$CLICKHOUSE_CLIENT --max_block_size=65505 --print-profile-events --profile-events-delay-ms=-1 -q 'select * from numbers(1e5)' 2> >(grep -o -e '\[ 0 \] SelectedRows: .*$' -e Exception) 1> >(wc -l)
+$CLICKHOUSE_CLIENT --max_block_size=65505 --print-profile-events --profile-events-delay-ms=-1 -q 'select * from numbers(1e5)' |& grep -o -e '\[ 0 \] SelectedRows: .*$' -e Exception
 
 echo 'print everything'
 profile_events="$($CLICKHOUSE_CLIENT --max_block_size 1 --print-profile-events -q 'select sleep(1) from numbers(2) format Null' |& grep -c 'SelectedRows')"
