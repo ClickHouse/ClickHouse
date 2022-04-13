70c64adaca1bfb0531b5e5c4073ed423e090f794
lgbo-ustc <lgbo.ustc@gmail.com>
2022-04-13 15:02:26 +0800

    fixed typos

--- src/Storages/Hive/HiveQueryTask.h
+++ src/Storages/Hive/HiveQueryTask.h
@@ -18,7 +18,7 @@
 namespace DB
 {
 /**
- * @brief An abstract strategy for collecting hive files. Make StorageHive more extensible and cant support hive cluster query later
+ * @brief An abstract strategy for collecting hive files. Make StorageHive more extensible and can't support hive cluster query later
  */
 class IHiveQueryTaskFilesCollector : public WithContext
 {
