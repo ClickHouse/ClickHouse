5e75043f5cb867000a0b82908aceafd1b3fd292d
Maksim Kita <kitaetoya@gmail.com>
2022-04-13 16:41:21 +0200

    Fixed tests

--- tests/queries/1_stateful/00172_early_constant_folding.sql
+++ tests/queries/1_stateful/00172_early_constant_folding.sql
@@ -1,1 +1,1 @@
-EXPLAIN PIPELINE SELECT count(JavaEnable) FROM test.hits WHERE WatchID = 1 OR Title = 'next' OR URL = 'prev' OR OriginalURL = '???' OR 1;
+EXPLAIN PIPELINE SELECT count(JavaEnable) FROM test.hits WHERE WatchID = 1 OR Title = 'next' OR URL = 'prev' OR URL = '???' OR 1;
