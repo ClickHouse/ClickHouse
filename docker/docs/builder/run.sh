7a7a5a72cb78c1d7e2e3606649b706e6d4622694
Mikhail f. Shiryaev <felixoid@clickhouse.com>
2022-04-15 00:38:16 +0200

    Intermediate step of migration to docusaurus

---
+++ docker/docs/builder/run.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+set -ex
+
+# Update docs repo
+git pull
+
+# The repo is usually mounted to /ClickHouse
+
+for lang in en ru zh
+do
+  if [ ! -e "/opt/clickhouse-docs/docs/${lang}" ] && [ -d "/ClickHouse/docs/${lang}" ]; then
+    cp -r "/ClickHouse/docs/${lang}" "/opt/clickhouse-docs/docs/${lang}"
+  fi
+done
+
+if [[ $# -lt 1 ]] || [[ "$1" == "--"* ]]; then
+  exec yarn build "$@"
+fi
+
+exec "$@"
