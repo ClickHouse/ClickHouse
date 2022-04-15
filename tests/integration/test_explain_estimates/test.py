81a50de26f3b9228c5ce3a3f48971beffd47cbe7
taiyang-li <654010905@qq.com>
2022-04-15 20:57:12 +0800

    fix integration tests

--- tests/integration/test_explain_estimates/test.py
+++ tests/integration/test_explain_estimates/test.py
@@ -24,7 +24,9 @@ def test_explain_estimates(start_cluster):
     node1.query("OPTIMIZE TABLE test")
     # sum(marks) - 1 because EXPLAIN ESIMATES does not include final mark.
     system_parts_result = node1.query(
-        "SELECT any(database), any(table), count() as parts, sum(rows) as rows, sum(marks)-1 as marks FROM system.parts WHERE database = 'default' AND table = 'test' and active = 1 GROUP BY (database, table)"
+        """
+        with count() as parts, sum(rows) as rows, sum(marks)-1 as marks SELECT any(database), any(table), concat('{"selected_marks":', marks::String, ',"selected_parts":', parts::String, ',"selected_rows":', rows::String, '}') FROM system.parts WHERE database = 'default' AND table = 'test' and active = 1 GROUP BY (database, table)"
+        """
     )
     explain_estimates_result = node1.query("EXPLAIN ESTIMATE SELECT * FROM test")
     assert system_parts_result == explain_estimates_result
