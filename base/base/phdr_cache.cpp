e2fa6d3c782c2bc78022fb0df77838e66ecd2124
Alexey Milovidov <milovidov@yandex-team.ru>
2022-04-16 00:28:56 +0200

    Remove Arcadia

--- base/base/phdr_cache.cpp
+++ base/base/phdr_cache.cpp
@@ -2,7 +2,7 @@
 #pragma clang diagnostic ignored "-Wreserved-identifier"
 #endif
 
-/// This code was based on the code by Fedor Korotkiy (prime@yandex-team.ru) for YT product in Yandex.
+/// This code was based on the code by Fedor Korotkiy https://www.linkedin.com/in/fedor-korotkiy-659a1838/
 
 #include <base/defines.h>
 
