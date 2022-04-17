c45d91cd969b0f278a60b7b5c14c4c3e720cae6f
Alexey Milovidov <milovidov@yandex-team.ru>
2022-04-17 22:02:55 +0200

    Remove useless code

--- src/Interpreters/AsynchronousMetricLog.cpp
+++ src/Interpreters/AsynchronousMetricLog.cpp
@@ -32,18 +32,11 @@ void AsynchronousMetricLogElement::appendToBlock(MutableColumns & columns) const
     columns[column_idx++]->insert(value);
 }
 
-
-static inline UInt64 time_in_seconds(std::chrono::time_point<std::chrono::system_clock> timepoint)
-{
-    return std::chrono::duration_cast<std::chrono::seconds>(timepoint.time_since_epoch()).count();
-}
-
 void AsynchronousMetricLog::addValues(const AsynchronousMetricValues & values)
 {
     AsynchronousMetricLogElement element;
 
-    const auto now = std::chrono::system_clock::now();
-    element.event_time = time_in_seconds(now);
+    element.event_time = time(nullptr);
     element.event_date = DateLUT::instance().toDayNum(element.event_time);
 
     for (const auto & [key, value] : values)
