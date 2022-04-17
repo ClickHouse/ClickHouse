6807f8428d55156d6db3dc2fd7bc89000a3d6b05
Alexey Milovidov <milovidov@yandex-team.ru>
2022-04-17 22:01:58 +0200

    Remove microseconds from system.asynchronous_metric_log

--- src/Interpreters/MetricLog.cpp
+++ src/Interpreters/MetricLog.cpp
@@ -78,17 +78,17 @@ void MetricLog::shutdown()
 }
 
 
-inline UInt64 time_in_milliseconds(std::chrono::time_point<std::chrono::system_clock> timepoint)
+static inline UInt64 time_in_milliseconds(std::chrono::time_point<std::chrono::system_clock> timepoint)
 {
     return std::chrono::duration_cast<std::chrono::milliseconds>(timepoint.time_since_epoch()).count();
 }
 
-inline UInt64 time_in_microseconds(std::chrono::time_point<std::chrono::system_clock> timepoint)
+static inline UInt64 time_in_microseconds(std::chrono::time_point<std::chrono::system_clock> timepoint)
 {
     return std::chrono::duration_cast<std::chrono::microseconds>(timepoint.time_since_epoch()).count();
 }
 
-inline UInt64 time_in_seconds(std::chrono::time_point<std::chrono::system_clock> timepoint)
+static inline UInt64 time_in_seconds(std::chrono::time_point<std::chrono::system_clock> timepoint)
 {
     return std::chrono::duration_cast<std::chrono::seconds>(timepoint.time_since_epoch()).count();
 }
