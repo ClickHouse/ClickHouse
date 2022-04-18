6c905f85cd27b2bf9183121f010d3b0cf521ecd3
Alexey Milovidov <milovidov@clickhouse.com>
2022-04-18 23:07:34 +0300

    Update SessionLog.cpp

--- src/Interpreters/SessionLog.cpp
+++ src/Interpreters/SessionLog.cpp
@@ -29,12 +29,12 @@ namespace
 {
 using namespace DB;
 
-static inline DateTime64 time_in_microseconds(std::chrono::time_point<std::chrono::system_clock> timepoint)
+inline DateTime64 time_in_microseconds(std::chrono::time_point<std::chrono::system_clock> timepoint)
 {
     return std::chrono::duration_cast<std::chrono::microseconds>(timepoint.time_since_epoch()).count();
 }
 
-static inline time_t time_in_seconds(std::chrono::time_point<std::chrono::system_clock> timepoint)
+inline time_t time_in_seconds(std::chrono::time_point<std::chrono::system_clock> timepoint)
 {
     return std::chrono::duration_cast<std::chrono::seconds>(timepoint.time_since_epoch()).count();
 }
