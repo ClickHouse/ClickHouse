a7985053eeb5a31d920857992de062902d51cc3f
Larry Luo <larry.luo@ca.ibm.com>
2022-04-18 17:20:58 -0400

    Fixed style check error

--- src/Parsers/tests/gtest_Parser.cpp
+++ src/Parsers/tests/gtest_Parser.cpp
@@ -53,7 +53,7 @@ TEST_P(ParserTest, parseQuery)
         {
             EXPECT_THROW(parseQuery(*parser, input_text.begin(), input_text.end(), 0, 0), DB::Exception);
         }
-        else 
+        else
         {
             ASTPtr ast;
             ASSERT_NO_THROW(ast = parseQuery(*parser, input_text.begin(), input_text.end(), 0, 0));
