8c6bef4207eb0904de63d85b9878b46eab088eff
KinderRiven <1339764596@qq.com>
2022-04-20 00:13:42 +0800

    fix add const for some function.

--- src/Common/FileCacheSettings.h
+++ src/Common/FileCacheSettings.h
@@ -17,12 +17,14 @@ namespace DB
 struct FileCacheSettings
 {
     std::string cache_method = "ARC";
-    double size_ratio = 0.2; /// just for ARC
-    int move_threshold = 4; /// just for ARC
     size_t max_size = 0;
     size_t max_elements = REMOTE_FS_OBJECTS_CACHE_DEFAULT_MAX_ELEMENTS;
     size_t max_file_segment_size = REMOTE_FS_OBJECTS_CACHE_DEFAULT_MAX_FILE_SEGMENT_SIZE;
     bool cache_on_write_operations = false;
+
+    /// Just for ARC
+    double size_ratio = REMOTE_FS_OBJECTS_ARC_CACHE_DEFAULT_SIZE_RATIO;
+    int move_threshold = REMOTE_FS_OBJECTS_ARC_CACHE_DEFAULT_MOVE_THRESHOLD;
 
     void loadFromConfig(const Poco::Util::AbstractConfiguration & config, const std::string & config_prefix);
 };
