382c717d09d5a0e2c85f80cafc823dcba92a9b4a
Mikhail f. Shiryaev <felixoid@clickhouse.com>
2022-04-20 18:21:37 +0200

    Speed-up fetching submodules

--- docker/test/fasttest/run.sh
+++ docker/test/fasttest/run.sh
@@ -179,7 +179,7 @@ function clone_submodules
         )
 
         git submodule sync
-        git submodule update --depth 1 --init "${SUBMODULES_TO_UPDATE[@]}"
+        git submodule update --jobs=16 --depth 1 --init "${SUBMODULES_TO_UPDATE[@]}"
         git submodule foreach git reset --hard
         git submodule foreach git checkout @ -f
         git submodule foreach git clean -xfd
