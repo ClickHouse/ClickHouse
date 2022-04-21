60271d2396b92b6f762fd53c8e02e0ab43d8d26c
FArthur-cmd <613623@mail.ru>
2022-04-21 12:26:30 +0000

    Add passphrase for certificates

--- src/Server/CertificateReloader.h
+++ src/Server/CertificateReloader.h
@@ -51,16 +51,14 @@ public:
     int setCertificate(SSL * ssl);
 
 private:
-    CertificateReloader()
-    {
-    }
+    CertificateReloader() = default;
 
     Poco::Logger * log = &Poco::Logger::get("CertificateReloader");
 
     struct File
     {
         const char * description;
-        File(const char * description_) : description(description_) {}
+        explicit File(const char * description_) : description(description_) {}
 
         std::string path;
         std::filesystem::file_time_type modification_time;
@@ -76,7 +74,7 @@ private:
         Poco::Crypto::X509Certificate cert;
         Poco::Crypto::EVPPKey key;
 
-        Data(std::string cert_path, std::string key_path);
+        Data(std::string cert_path, std::string key_path, std::string pass_phrase);
     };
 
     MultiVersion<Data> data;
