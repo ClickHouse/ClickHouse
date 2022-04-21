b6ef227586f425c65d09faa7ae4c609d3e417f21
KinderRiven <1339764596@qq.com>
2022-04-21 22:19:51 +0800

    add cache log table in system database

--- src/Interpreters/CacheLog.h
+++ src/Interpreters/CacheLog.h
@@ -12,16 +12,17 @@ namespace DB
 
 struct CacheLogElement
 {
-    String current_query_id;
-
-    Float64 hit_ratio;
-    Float64 miss_ratio;
+    String query_id;
 
     UInt64 hit_count;
     UInt64 miss_count;
 
-    std::unordered_map<String, size_t> hit_records;
-    std::unordered_map<String, size_t> miss_records;
+    /// Float64 hit_ratio;
+    /// Float64 miss_ratio;
+
+    /// TODO for next version
+    /// std::unordered_map<String, size_t> hit_records;
+    /// std::unordered_map<String, size_t> miss_records;
 
     static std::string name() { return "CacheLog"; }
 
@@ -29,10 +30,9 @@ struct CacheLogElement
     static NamesAndAliases getNamesAndAliases() { return {}; }
 
     void appendToBlock(MutableColumns & columns) const;
-    static void appendClientInfo(const ClientInfo & client_info, MutableColumns & columns, size_t & i);
 };
 
-class QueryLog : public SystemLog<CacheLogElement>
+class CacheLog : public SystemLog<CacheLogElement>
 {
     using SystemLog<CacheLogElement>::SystemLog;
 };
