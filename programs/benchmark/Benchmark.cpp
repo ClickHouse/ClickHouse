164647cc05cd0cd1568b03e4ec2c2cdd5b4c89ed
Tian Xinhui <xinhui_tian@126.com>
2022-04-22 18:31:07 +0800

    Update programs/benchmark/Benchmark.cpp
    Co-authored-by: Vladimir C <vdimir@clickhouse.com>

--- programs/benchmark/Benchmark.cpp
+++ programs/benchmark/Benchmark.cpp
@@ -552,9 +552,9 @@ private:
 
             print_key_value("QPS", info->queries.load() / seconds);
             print_key_value("RPS", info->read_rows / seconds);
-            print_key_value("MiBPS", info->read_bytes / seconds);
+            print_key_value("MiBPS", info->read_bytes / seconds / 1048576);
             print_key_value("RPS_result", info->result_rows / seconds);
-            print_key_value("MiBPS_result", info->result_bytes / seconds);
+            print_key_value("MiBPS_result", info->result_bytes / seconds / 1048576);
             print_key_value("num_queries", info->queries.load());
             print_key_value("num_errors", info->errors, false);
 
