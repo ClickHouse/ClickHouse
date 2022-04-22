3751c69aaacd052ff8985d447d64c174efe5d6be
taiyang-li <654010905@qq.com>
2022-04-22 15:38:19 +0800

    fix code style

--- src/Common/tests/gtest_range_generator.cpp
+++ src/Common/tests/gtest_range_generator.cpp
@@ -9,18 +9,14 @@ TEST(RangeGenerator, Common)
     RangeGenerator g{25, 10};
     EXPECT_EQ(g.totalRanges(), 3);
 
+    std::vector<RangeGenerator::Range> ranges{{0, 10}, {10, 20}, {20, 25}};
+    for (size_t i = 0; i < 3; ++i)
+    {
+        auto r = g.nextRange();
+        EXPECT_TRUE(r);
+        EXPECT_EQ(r, ranges[i]);
+    }
+
     auto r = g.nextRange();
-    EXPECT_TRUE(r);
-    EXPECT_EQ(*r, RangeGenerator::Range(0, 10));
-
-    r = g.nextRange();
-    EXPECT_TRUE(r);
-    EXPECT_EQ(*r, RangeGenerator::Range(10, 20));
-
-    r = g.nextRange();
-    EXPECT_TRUE(r);
-    EXPECT_EQ(*r, RangeGenerator::Range(20, 25));
-
-    r = g.nextRange();
     EXPECT_TRUE(!r);
 }
