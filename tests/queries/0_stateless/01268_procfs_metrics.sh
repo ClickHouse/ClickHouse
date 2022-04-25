990ba0143af0a702c820061f4973aba1d8a81af3
Azat Khuzhin <a3at.mail@gmail.com>
2022-04-25 08:19:55 +0300

    tests: adopt 01268_procfs_metrics for difference between proc and taskstruct
    Signed-off-by: Azat Khuzhin <a.khuzhin@semrush.com>

--- tests/queries/0_stateless/01268_procfs_metrics.sh
+++ tests/queries/0_stateless/01268_procfs_metrics.sh
@@ -8,6 +8,14 @@ CURDIR=$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)
 # shellcheck source=../shell_config.sh
 . "$CURDIR"/../shell_config.sh
 
-$CLICKHOUSE_LOCAL --profile-events-delay-ms=-1 --print-profile-events -q "SELECT * FROM file('${BASH_SOURCE[0]}', 'LineAsString') FORMAT Null" |& {
-    grep -F -o -e OSCPUVirtualTimeMicroseconds -e OSReadChars
-} | sort | uniq
+# NOTE: netlink taskstruct interface uses rounding to 1KB [1], so we cannot use ${BASH_SOURCE[0]}
+#
+#   [1]: https://elixir.bootlin.com/linux/v5.18-rc4/source/kernel/tsacct.c#L101
+tmp_path=$(mktemp "$CURDIR/01268_procfs_metrics.XXXXXX")
+trap 'rm -f $tmp_path' EXIT
+truncate -s1025 "$tmp_path"
+
+$CLICKHOUSE_LOCAL --profile-events-delay-ms=-1 --print-profile-events -q "SELECT * FROM file('$tmp_path', 'LineAsString') FORMAT Null" |& grep -m1 -F -o -e OSReadChars
+# NOTE: that OSCPUVirtualTimeMicroseconds is in microseconds, so 1e6 is not enough.
+$CLICKHOUSE_LOCAL --profile-events-delay-ms=-1 --print-profile-events -q "SELECT * FROM numbers(10e6) FORMAT Null" |& grep -m1 -F -o -e OSCPUVirtualTimeMicroseconds
+exit 0
