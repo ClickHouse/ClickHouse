fa1bd65e9b3e68196b96b321b7ee8cb5a84d6d23
root <mallik.hassan@ibm.com>
2022-04-26 11:55:45 -0700

    stylecheck fix in Python file

--- tests/integration/test_structured_logging_json/test.py
+++ tests/integration/test_structured_logging_json/test.py
@@ -7,6 +7,7 @@ from xml.etree import ElementTree
 cluster = ClickHouseCluster(__file__)
 node = cluster.add_instance("node", stay_alive=True)
 
+
 @pytest.fixture(scope="module")
 def start_cluster():
     try:
@@ -14,6 +15,7 @@ def start_cluster():
         yield cluster
     finally:
         cluster.shutdown()
+
 
 def get_log_array(logs):
     log_array = []
@@ -25,20 +27,20 @@ def get_log_array(logs):
             temp_log = ""
     return log_array
 
+
 def is_json(log_json):
-  try:
-    json.loads(log_json)
-  except ValueError as e:
-    return False
-  return True
+    try:
+        json.loads(log_json)
+    except ValueError as e:
+        return False
+    return True
+
 
 def test_structured_logging_json_format(start_cluster):
-    config = node.exec_in_container(
-        ["cat", "/etc/clickhouse-server/config.xml"]
-    )
+    config = node.exec_in_container(["cat", "/etc/clickhouse-server/config.xml"])
     root = ElementTree.fromstring(config)
-    for logger in root.findall('logger'):
-        if logger.find('json') is None:
+    for logger in root.findall("logger"):
+        if logger.find("json") is None:
             pytest.skip("JSON is not activated in config.xml")
 
     node.query("SELECT 1")
@@ -48,7 +50,7 @@ def test_structured_logging_json_format(start_cluster):
     result = True
     for i in range(0, len(log_array)):
         temporary_result = is_json(log_array[i])
-        result  &= temporary_result
+        result &= temporary_result
         # we will test maximum 5 logs
         if i >= min(5, len(log_array) - 1):
             break
