0b0fa8453bcfb1a1b9045813765242cd19aec851
lgbo-ustc <lgbo.ustc@gmail.com>
2022-04-26 18:06:16 +0800

    fixed bug: resize on left pipeline cause the order by result wrong

---
+++ src/Processors/QueryPlan/ResizeStreamsStep.h
@@ -0,0 +1,15 @@
+#include <Processors/QueryPlan/IQueryPlanStep.h>
+#include <Processors/QueryPlan/ITransformingStep.h>
+
+namespace DB
+{
+class ResizeStreamsStep : public ITransformingStep
+{
+public:
+    explicit ResizeStreamsStep(const DataStream & input_stream_, size_t pipeline_streams_);
+    String getName() const override { return "ResizeStreamsStep"; }
+    void transformPipeline(QueryPipelineBuilder & pipeline, const BuildQueryPipelineSettings &) override;
+private:
+    size_t pipeline_streams;
+};
+}
