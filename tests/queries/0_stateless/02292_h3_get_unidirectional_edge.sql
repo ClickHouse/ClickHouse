12f74bc124108f3d3141d384987963e78a2430ea
bharatnc <bharatnc@gmail.com>
2022-05-01 10:47:35 -0700

    add tests for h3GetunidirectionalEdge

---
+++ tests/queries/0_stateless/02292_h3_get_unidirectional_edge.sql
@@ -0,0 +1,4 @@
+-- Tags: no-fasttest
+
+select h3GetUnidirectionalEdge(stringToH3('85283473fffffff'), stringToH3('85283477fffffff'));
+select h3GetUnidirectionalEdge(stringToH3('85283473fffffff'), stringToH3('85283473fffffff'));
