e4c7717534f7b8d1f17452a5c912e73a655247aa
bharatnc <bharatnc@gmail.com>
2022-05-01 13:48:22 -0700

    add tests h3GetOriginIndexFromUnidirectionalEdge

---
+++ tests/queries/0_stateless/02292_h3_get_origin_index_from_unidirectional_edge.sql
@@ -0,0 +1,5 @@
+-- Tags: no-fasttest
+
+SELECT h3GetOriginIndexFromUnidirectionalEdge(1248204388774707199);
+SELECT h3GetOriginIndexFromUnidirectionalEdge(1248204388774707197);
+SELECT h3GetOriginIndexFromUnidirectionalEdge(599686042433355775);
