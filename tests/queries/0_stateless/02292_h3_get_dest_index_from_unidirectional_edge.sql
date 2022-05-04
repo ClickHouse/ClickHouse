b019dac4a9f1299add12d3303dd22e3234fbb974
bharatnc <bharatnc@gmail.com>
2022-05-04 15:55:05 -0700

    combine tests to a single file

--- tests/queries/0_stateless/02292_h3_get_dest_index_from_unidirectional_edge.sql
+++
@@ -1,4 +0,0 @@
--- Tags: no-fasttest
-
-SELECT h3GetDestinationIndexFromUnidirectionalEdge(1248204388774707197);
-SELECT h3GetDestinationIndexFromUnidirectionalEdge(599686042433355773);
--- tests/queries/0_stateless/02292_h3_get_unidirectional_edge.sql
+++
@@ -1,4 +0,0 @@
--- Tags: no-fasttest
-
-select h3GetUnidirectionalEdge(stringToH3('85283473fffffff'), stringToH3('85283477fffffff'));
-select h3GetUnidirectionalEdge(stringToH3('85283473fffffff'), stringToH3('85283473fffffff'));
