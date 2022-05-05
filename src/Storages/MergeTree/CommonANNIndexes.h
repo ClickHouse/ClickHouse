d381d3a50c6559461295cb212a5489ff4658dd54
Filatenkov Artur <58165623+FArthur-cmd@users.noreply.github.com>
2022-05-06 01:10:54 +0300

    Update CommonANNIndexes.h

--- src/Storages/MergeTree/CommonANNIndexes.h
+++ src/Storages/MergeTree/CommonANNIndexes.h
@@ -1,6 +1,7 @@
 #pragma once
 
 #include <Storages/MergeTree/KeyCondition.h>
+#include <Storages/MergeTree/MergeTreeIndices.h>
 
 #include <optional>
 #include <vector>
@@ -215,5 +216,12 @@ private:
 };
 
 }
+
+// condition interface for Ann indexes. Returns vector of indexes of ranges in granule which are useful for query.
+class IMergeTreeIndexConditionAnn: public IMergeTreeIndexCondition
+{
+    public:
+    virtual std::vector<size_t> getUsefulRanges(MergeTreeIndexGranulePtr idx_granule) const = 0;
+};
 
 }
