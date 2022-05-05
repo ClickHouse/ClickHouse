817a058ca2089cc9d20622cda70985e4b34d5c9f
Alexander Pechenkin <pechenkin.aa@phystech.edu>
2022-05-05 19:28:47 +0000

    add optimization.

---
+++ src/Storages/MergeTree/MergeTreeIndicesANNCondition.h
@@ -0,0 +1,11 @@
+#include <Storages/MergeTree/MergeTreeIndices.h>
+#include <vector>
+namespace DB
+{
+// condition interface for Ann indexes. Returns vector of indexes of ranges in granule which are useful for query.
+class IMergeTreeIndexConditionAnn: public IMergeTreeIndexCondition
+{
+    public:
+    virtual std::vector<size_t> getUsefulRanges(MergeTreeIndexGranulePtr idx_granule) const = 0;
+};
+}
