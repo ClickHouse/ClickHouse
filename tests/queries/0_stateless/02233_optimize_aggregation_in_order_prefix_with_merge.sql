661d715427734cb94eaa3cb8fa98ba29cf4b38f5
Alexey Milovidov <milovidov@yandex-team.ru>
2022-05-06 00:23:47 +0200

    Fix tests

--- tests/queries/0_stateless/02233_optimize_aggregation_in_order_prefix_with_merge.sql
+++ tests/queries/0_stateless/02233_optimize_aggregation_in_order_prefix_with_merge.sql
@@ -8,5 +8,4 @@ insert into data_02233 values (2, 10, 100, 1000)(2, 20, 200, 2000);
 SELECT child_key, parent_key, child_key FROM data_02233 GROUP BY parent_key, child_key, child_key ORDER BY child_key, parent_key ASC NULLS LAST SETTINGS max_threads = 1, optimize_aggregation_in_order = 1;
 SELECT child_key, parent_key, child_key FROM data_02233 GROUP BY parent_key, child_key, child_key WITH TOTALS ORDER BY child_key, parent_key ASC NULLS LAST SETTINGS max_threads = 1, optimize_aggregation_in_order = 1;
 
--- { echoOff }
 drop table data_02233;
