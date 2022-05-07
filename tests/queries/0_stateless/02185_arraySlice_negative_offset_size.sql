65d5531f719591590058293350b1efc9132c9b14
Alexey Milovidov <milovidov@clickhouse.com>
2022-05-07 13:51:15 +0300

    Update 02185_arraySlice_negative_offset_size.sql

--- tests/queries/0_stateless/02185_arraySlice_negative_offset_size.sql
+++ tests/queries/0_stateless/02185_arraySlice_negative_offset_size.sql
@@ -15,7 +15,7 @@ create table t
     r Int8
 ) engine = Memory;
 
-insert into t values ([1, 2, 3, 4, 5, 6, 7, 8], -2, -2),([1, 2, 3, 4, 5, 6, 7, 8], -3, -3);
+insert into t values ([1, 2, 3, 4, 5, 6, 7, 8], -2, -2), ([1, 2, 3, 4, 5, 6, 7, 8], -3, -3);
 
 select arraySlice(s, -2, -2) from t;
 select arraySlice(s, l, -2) from t;
