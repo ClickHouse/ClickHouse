bf37a4d3d62f314acc71bc9ef12484652046d62b
Antonio Andelic <antonio@clickhouse.com>
2022-05-09 08:32:25 +0000

    Add processLocal for read requests

--- src/Coordination/KeeperStorage.h
+++ src/Coordination/KeeperStorage.h
@@ -1,14 +1,14 @@
 #pragma once
 
-#include <Common/ZooKeeper/IKeeper.h>
-#include <Common/ConcurrentBoundedQueue.h>
-#include <Common/ZooKeeper/ZooKeeperCommon.h>
-#include <Coordination/SessionExpiryQueue.h>
-#include <Coordination/ACLMap.h>
-#include <Coordination/SnapshotableHashTable.h>
-#include <IO/WriteBufferFromString.h>
 #include <unordered_map>
 #include <vector>
+#include <Coordination/ACLMap.h>
+#include <Coordination/SessionExpiryQueue.h>
+#include <Coordination/SnapshotableHashTable.h>
+#include <IO/WriteBufferFromString.h>
+#include <Common/ConcurrentBoundedQueue.h>
+#include <Common/ZooKeeper/IKeeper.h>
+#include <Common/ZooKeeper/ZooKeeperCommon.h>
 
 #include <absl/container/flat_hash_set.h>
 
@@ -29,7 +29,6 @@ struct KeeperStorageSnapshot;
 class KeeperStorage
 {
 public:
-
     struct Node
     {
         uint64_t acl_id = 0; /// 0 -- no ACL by default
@@ -41,26 +40,18 @@ public:
         Node() : size_bytes(sizeof(Node)) { }
 
         /// Object memory size
-        uint64_t sizeInBytes() const
-        {
-            return size_bytes;
-        }
+        uint64_t sizeInBytes() const { return size_bytes; }
 
         void setData(String new_data);
 
-        const auto & getData() const noexcept
-        {
-            return data;
-        }
+        const auto & getData() const noexcept { return data; }
 
         void addChild(StringRef child_path);
 
         void removeChild(StringRef child_path);
 
-        const auto & getChildren() const noexcept
-        {
-            return children;
-        }
+        const auto & getChildren() const noexcept { return children; }
+
     private:
         String data;
         ChildrenSet children{};
@@ -85,10 +76,7 @@ public:
         std::string scheme;
         std::string id;
 
-        bool operator==(const AuthID & other) const
-        {
-            return scheme == other.scheme && id == other.id;
-        }
+        bool operator==(const AuthID & other) const { return scheme == other.scheme && id == other.id; }
     };
 
     using RequestsForSessions = std::vector<RequestForSession>;
@@ -112,7 +100,7 @@ public:
     /// container.
     Container container;
 
-    struct CreateNodeDelta 
+    struct CreateNodeDelta
     {
         Coordination::Stat stat;
         bool is_ephemeral;
@@ -152,27 +140,18 @@ public:
     {
     };
 
-    using Operation = std::variant<CreateNodeDelta, RemoveNodeDelta, UpdateNodeDelta, SetACLDelta, ErrorDelta, SubDeltaEnd, FailedMultiDelta>;
+    using Operation
+        = std::variant<CreateNodeDelta, RemoveNodeDelta, UpdateNodeDelta, SetACLDelta, ErrorDelta, SubDeltaEnd, FailedMultiDelta>;
 
     struct Delta
     {
-        Delta(String path_, int64_t zxid_, Operation operation_)
-            : path(std::move(path_))
-            , zxid(zxid_)
-            , operation(std::move(operation_))
-        {}
+        Delta(String path_, int64_t zxid_, Operation operation_) : path(std::move(path_)), zxid(zxid_), operation(std::move(operation_)) { }
 
-        Delta(int64_t zxid_, Coordination::Error error)
-            : Delta("", zxid_, ErrorDelta{error})
-        {}
+        Delta(int64_t zxid_, Coordination::Error error) : Delta("", zxid_, ErrorDelta{error}) { }
 
-        Delta(int64_t zxid_, SubDeltaEnd subdelta)
-            : Delta("", zxid_, subdelta)
-        {}
+        Delta(int64_t zxid_, SubDeltaEnd subdelta) : Delta("", zxid_, subdelta) { }
 
-        Delta(int64_t zxid_, FailedMultiDelta failed_multi)
-            : Delta("", zxid_, failed_multi)
-        {}
+        Delta(int64_t zxid_, FailedMultiDelta failed_multi) : Delta("", zxid_, failed_multi) { }
 
         String path;
         int64_t zxid;
@@ -181,7 +160,7 @@ public:
 
     struct CurrentNodes
     {
-        explicit CurrentNodes(KeeperStorage & storage_) : storage(storage_) {}
+        explicit CurrentNodes(KeeperStorage & storage_) : storage(storage_) { }
 
         template <typename Visitor>
         void applyDeltas(StringRef path, const Visitor & visitor) const
@@ -206,7 +185,14 @@ public:
 
     Coordination::Error commit(int64_t zxid, int64_t session_id);
 
-    bool createNode(const std::string & path, String data, const Coordination::Stat & stat, bool is_sequental, bool is_ephemeral, Coordination::ACLs node_acls, int64_t session_id);
+    bool createNode(
+        const std::string & path,
+        String data,
+        const Coordination::Stat & stat,
+        bool is_sequental,
+        bool is_ephemeral,
+        Coordination::ACLs node_acls,
+        int64_t session_id);
     bool removeNode(const std::string & path, int32_t version);
 
     /// Mapping session_id -> set of ephemeral nodes paths
@@ -228,15 +214,12 @@ public:
 
     /// Currently active watches (node_path -> subscribed sessions)
     Watches watches;
-    Watches list_watches;   /// Watches for 'list' request (watches on children).
+    Watches list_watches; /// Watches for 'list' request (watches on children).
 
     void clearDeadWatches(int64_t session_id);
 
     /// Get current zxid
-    int64_t getZXID() const
-    {
-        return zxid;
-    }
+    int64_t getZXID() const { return zxid; }
 
     const String superdigest;
 
@@ -260,7 +243,13 @@ public:
 
     /// Process user request and return response.
     /// check_acl = false only when converting data from ZooKeeper.
-    ResponsesForSessions processRequest(const Coordination::ZooKeeperRequestPtr & request, int64_t session_id, int64_t time, std::optional<int64_t> new_last_zxid, bool check_acl = true);
+    ResponsesForSessions processRequest(
+        const Coordination::ZooKeeperRequestPtr & request,
+        int64_t session_id,
+        int64_t time,
+        std::optional<int64_t> new_last_zxid,
+        bool check_acl = true,
+        bool is_local = false);
     void preprocessRequest(const Coordination::ZooKeeperRequestPtr & request, int64_t session_id, int64_t time, int64_t new_last_zxid);
 
     void finalize();
@@ -268,70 +257,37 @@ public:
     /// Set of methods for creating snapshots
 
     /// Turn on snapshot mode, so data inside Container is not deleted, but replaced with new version.
-    void enableSnapshotMode(size_t up_to_version)
-    {
-        container.enableSnapshotMode(up_to_version);
-    }
+    void enableSnapshotMode(size_t up_to_version) { container.enableSnapshotMode(up_to_version); }
 
     /// Turn off snapshot mode.
-    void disableSnapshotMode()
-    {
-        container.disableSnapshotMode();
-    }
+    void disableSnapshotMode() { container.disableSnapshotMode(); }
 
-    Container::const_iterator getSnapshotIteratorBegin() const
-    {
-        return container.begin();
-    }
+    Container::const_iterator getSnapshotIteratorBegin() const { return container.begin(); }
 
     /// Clear outdated data from internal container.
-    void clearGarbageAfterSnapshot()
-    {
-        container.clearOutdatedNodes();
-    }
+    void clearGarbageAfterSnapshot() { container.clearOutdatedNodes(); }
 
     /// Get all active sessions
-    const SessionAndTimeout & getActiveSessions() const
-    {
-        return session_and_timeout;
-    }
+    const SessionAndTimeout & getActiveSessions() const { return session_and_timeout; }
 
     /// Get all dead sessions
-    std::vector<int64_t> getDeadSessions() const
-    {
-        return session_expiry_queue.getExpiredSessions();
-    }
+    std::vector<int64_t> getDeadSessions() const { return session_expiry_queue.getExpiredSessions(); }
 
     /// Introspection functions mostly used in 4-letter commands
-    uint64_t getNodesCount() const
-    {
-        return container.size();
-    }
+    uint64_t getNodesCount() const { return container.size(); }
 
-    uint64_t getApproximateDataSize() const
-    {
-        return container.getApproximateDataSize();
-    }
+    uint64_t getApproximateDataSize() const { return container.getApproximateDataSize(); }
 
-    uint64_t getArenaDataSize() const
-    {
-        return container.keyArenaSize();
-    }
+    uint64_t getArenaDataSize() const { return container.keyArenaSize(); }
 
 
     uint64_t getTotalWatchesCount() const;
 
-    uint64_t getWatchedPathsCount() const
-    {
-        return watches.size() + list_watches.size();
-    }
+    uint64_t getWatchedPathsCount() const { return watches.size() + list_watches.size(); }
 
     uint64_t getSessionsWithWatchesCount() const;
 
-    uint64_t getSessionWithEphemeralNodesCount() const
-    {
-        return ephemerals.size();
-    }
+    uint64_t getSessionWithEphemeralNodesCount() const { return ephemerals.size(); }
     uint64_t getTotalEphemeralNodesCount() const;
 
     void dumpWatches(WriteBufferFromOwnString & buf) const;
