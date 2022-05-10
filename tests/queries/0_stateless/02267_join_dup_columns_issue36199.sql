837d4bd13f9015328064f093d28ed6e76f17a0d9
vdimir <vdimir@clickhouse.com>
2022-05-10 14:30:02 +0000

    Revert "Continue fixing #36199."
    This reverts commit 1feba64a4ec335bbe59218915bad987e31653575.

--- tests/queries/0_stateless/02267_join_dup_columns_issue36199.sql
+++ tests/queries/0_stateless/02267_join_dup_columns_issue36199.sql
@@ -5,13 +5,9 @@ FROM ( SELECT 2 AS x ) AS t1
 RIGHT JOIN ( SELECT count('x'), count('y'), 2 AS x ) AS t2
 ON t1.x = t2.x;
 
-select * from (select 2 as x) as t1 right join (select count('x'), count('y'), 0 as x) as t2 on t1.x = t2.x;
-
 set join_algorithm = 'partial_merge';
 
 SELECT *
 FROM ( SELECT 2 AS x ) AS t1
 RIGHT JOIN ( SELECT count('x'), count('y'), 2 AS x ) AS t2
 ON t1.x = t2.x;
-
-select * from (select 2 as x) as t1 right join (select count('x'), count('y'), 0 as x) as t2 on t1.x = t2.x;
