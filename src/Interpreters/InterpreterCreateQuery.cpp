3e0b65b83b5b833c06ff70580b45bff7286bd521
Ubuntu <ubuntu@ip-10-2-4-167.eu-west-1.compute.internal>
2022-05-11 20:12:05 +0000

    Ignore AUTO_INCREMENT in CREATE TABLE to simplify migration from MySQL
    Issue #35893

--- src/Interpreters/InterpreterCreateQuery.cpp
+++ src/Interpreters/InterpreterCreateQuery.cpp
@@ -552,6 +552,16 @@ ColumnsDescription InterpreterCreateQuery::getColumnsDescription(
 
         column.name = col_decl.name;
 
+        /// ignore or not other database extensions depending on compatibility settings
+        if (col_decl.default_specifier == "AUTO_INCREMENT"
+            && !context_->getSettingsRef().compatibility_ignore_auto_increment_in_create_table)
+        {
+            throw Exception{
+                "AUTO_INCREMENT is not supported. To ignore the keyword in column declaration set "
+                "`compatibility_ignore_auto_increment_in_create_table` to true",
+                ErrorCodes::SYNTAX_ERROR};
+        }
+
         if (col_decl.default_expression)
         {
             ASTPtr default_expr =
