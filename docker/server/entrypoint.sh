a675305df61dbfde35c01aa4043e0e15dbcc4be7
Alexey Milovidov <milovidov@yandex-team.ru>
2022-05-11 03:18:28 +0200

    Fix shellcheck

--- docker/server/entrypoint.sh
+++ docker/server/entrypoint.sh
@@ -95,8 +95,8 @@ if [ -n "$(ls /docker-entrypoint-initdb.d/)" ] || [ -n "$CLICKHOUSE_DB" ]; then
     HTTP_PORT="$(clickhouse extract-from-config --config-file "$CLICKHOUSE_CONFIG" --key=http_port)"
 
     # Drop privileges
-    CLICKHOUSE_SETUID="${USER}"
-    CLICKHOUSE_SETGID="${GROUP}"
+    export CLICKHOUSE_SETUID="${USER}"
+    export CLICKHOUSE_SETGID="${GROUP}"
 
     # Listen only on localhost until the initialization is done
     /usr/bin/clickhouse-server --config-file="$CLICKHOUSE_CONFIG" -- --listen_host=127.0.0.1 &
