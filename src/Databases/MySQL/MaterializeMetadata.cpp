fc8ea5fe03da3b46744dda0e9590d22e3a265786
Mostafa Dahab <mostafa@dahab.io>
2022-05-12 16:33:19 +0300

    Use selected database
    Signed-off-by: Mostafa Dahab <mostafa@dahab.io>

--- src/Databases/MySQL/MaterializeMetadata.cpp
+++ src/Databases/MySQL/MaterializeMetadata.cpp
@@ -294,6 +294,7 @@ void MaterializeMetadata::startReplication(
     try
     {
         connection->query("FLUSH TABLES;").execute();
+        connection->query("USE " + database + ";").execute();
 
         if (materialized_tables_list.empty())
         {
