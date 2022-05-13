c12bab0945a536d502135eac7285ed7e6eaffd90
Alexandra Pilipyuk <aspasparagus@gmail.com>
2022-05-13 19:09:40 +0300

    Add Neon optimizatoin to memcpySmall.h

--- src/Common/memcpySmall.h
+++ src/Common/memcpySmall.h
@@ -15,8 +15,8 @@
   * 2. Much time/instructions are spend to process "tails" of data.
   *
   * There are cases when function could be implemented in more optimal way, with help of some assumptions.
-  * One of that assumptions - ability to read and write some number of bytes after end of passed memory regions.
-  * Under that assumption, it is possible not to implement difficult code to process tails of data and do copy always by big chunks.
+  * One of that assumptions - ability to read and write some number of bytes after the end of passed memory regions.
+  * Under that assumption, it is possible not to implement difficult code to process tails of data and copy always by big chunks.
   *
   * This case is typical, for example, when many small pieces of data are gathered to single contiguous piece of memory in a loop.
   * - because each next copy will overwrite excessive data after previous copy.
@@ -54,6 +54,29 @@ inline void memcpySmallAllowReadWriteOverflow15(void * __restrict dst, const voi
 /** NOTE There was also a function, that assumes, that you could read any bytes inside same memory page of src.
   * This function was unused, and also it requires special handling for Valgrind and ASan.
   */
+
+#elif defined(__aarch64__)   /// Implementation for arm platform, similar to x86
+#include <arm_neon.h>
+
+namespace detail
+{
+    inline void memcpySmallAllowReadWriteOverflow15Impl(char * __restrict dst, const char * __restrict src, ssize_t n)
+    {
+        while (n > 0)
+        {
+            vst1q_s8(dst, vld1q_s8(src));
+
+            dst += 16;
+            src += 16;
+            n -= 16;
+        }
+    }
+}
+
+inline void memcpySmallAllowReadWriteOverflow15(void * __restrict dst, const void * __restrict src, size_t n)
+{
+    detail::memcpySmallAllowReadWriteOverflow15Impl(reinterpret_cast<char *>(dst), reinterpret_cast<const char *>(src), n);
+}
 
 #else    /// Implementation for other platforms.
 
