a2870ef65ec44ecf14ecb9ddc192d2705f7f01ac
Raúl Marín <git@rmr.ninja>
2022-05-13 17:28:05 +0200

    Allow negative values for seconds

--- tests/queries/0_stateless/02294_decimal_second_errors.sql
+++ tests/queries/0_stateless/02294_decimal_second_errors.sql
@@ -1,9 +1,11 @@
 SELECT 1 SETTINGS max_execution_time=NaN; -- { serverError 72 }
 SELECT 1 SETTINGS max_execution_time=Infinity; -- { serverError 72 };
 SELECT 1 SETTINGS max_execution_time=-Infinity; -- { serverError 72 };
-SELECT 1 SETTINGS max_execution_time=-0.5; -- { serverError 72 };
-SELECT 1 SETTINGS max_execution_time=-0.000000000001; -- { serverError 72 };
-SELECT 1 SETTINGS max_execution_time=-0.0; -- { serverError 72 };
+
+-- Ok values
+SELECT 1 SETTINGS max_execution_time=-0.5;
+SELECT 1 SETTINGS max_execution_time=0.5;
+SELECT 1 SETTINGS max_execution_time=-1;
 SELECT 1 SETTINGS max_execution_time=0.0;
-SELECT 1 SETTINGS max_execution_time=10.5;
+SELECT 1 SETTINGS max_execution_time=-0.0;
 SELECT 1 SETTINGS max_execution_time=10;
