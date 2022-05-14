d6a860ebb4c93f87ca7d6d9a412da60d277c86ea
alesapin <alesapin@gmail.com>
2022-05-14 14:49:01 +0200

    Format queries better

--- tests/queries/0_stateless/02241_remote_filesystem_cache_on_insert.sql
+++ tests/queries/0_stateless/02241_remote_filesystem_cache_on_insert.sql
@@ -9,13 +9,43 @@ CREATE TABLE test (key UInt32, value String) Engine=MergeTree() ORDER BY key SET
 
 SYSTEM DROP FILESYSTEM CACHE;
 
-SELECT file_segment_range_begin, file_segment_range_end, size, state FROM (SELECT file_segment_range_begin, file_segment_range_end, size, state, local_path FROM (SELECT arrayJoin(cache_paths) AS cache_path, local_path, remote_path FROM system.remote_data_paths ) AS data_paths INNER JOIN system.filesystem_cache AS caches ON data_paths.cache_path = caches.cache_path) WHERE endsWith(local_path, 'data.bin') FORMAT Vertical;
+SELECT file_segment_range_begin, file_segment_range_end, size, state
+FROM
+(
+    SELECT file_segment_range_begin, file_segment_range_end, size, state, local_path
+    FROM
+    (
+        SELECT arrayJoin(cache_paths) AS cache_path, local_path, remote_path
+        FROM system.remote_data_paths
+    ) AS data_paths
+    INNER JOIN
+        system.filesystem_cache AS caches
+    ON data_paths.cache_path = caches.cache_path
+)
+WHERE endsWith(local_path, 'data.bin')
+FORMAT Vertical;
+
 SELECT count() FROM (SELECT arrayJoin(cache_paths) AS cache_path, local_path, remote_path FROM system.remote_data_paths ) AS data_paths INNER JOIN system.filesystem_cache AS caches ON data_paths.cache_path = caches.cache_path;
 SELECT count() FROM system.filesystem_cache;
 
 INSERT INTO test SELECT number, toString(number) FROM numbers(100);
 
-SELECT file_segment_range_begin, file_segment_range_end, size, state FROM (SELECT file_segment_range_begin, file_segment_range_end, size, state, local_path FROM (SELECT arrayJoin(cache_paths) AS cache_path, local_path, remote_path FROM system.remote_data_paths ) AS data_paths INNER JOIN system.filesystem_cache AS caches ON data_paths.cache_path = caches.cache_path) WHERE endsWith(local_path, 'data.bin') FORMAT Vertical;
+SELECT file_segment_range_begin, file_segment_range_end, size, state
+FROM
+(
+    SELECT file_segment_range_begin, file_segment_range_end, size, state, local_path
+    FROM
+    (
+        SELECT arrayJoin(cache_paths) AS cache_path, local_path, remote_path
+        FROM system.remote_data_paths
+    ) AS data_paths
+    INNER JOIN
+        system.filesystem_cache AS caches
+    ON data_paths.cache_path = caches.cache_path
+)
+WHERE endsWith(local_path, 'data.bin')
+FORMAT Vertical;
+
 SELECT count() FROM (SELECT arrayJoin(cache_paths) AS cache_path, local_path, remote_path FROM system.remote_data_paths ) AS data_paths INNER JOIN system.filesystem_cache AS caches ON data_paths.cache_path = caches.cache_path;
 SELECT count() FROM system.filesystem_cache;
 
@@ -33,7 +63,22 @@ SYSTEM DROP FILESYSTEM CACHE;
 
 INSERT INTO test SELECT number, toString(number) FROM numbers(100, 200);
 
-SELECT file_segment_range_begin, file_segment_range_end, size, state FROM (SELECT file_segment_range_begin, file_segment_range_end, size, state, local_path FROM (SELECT arrayJoin(cache_paths) AS cache_path, local_path, remote_path FROM system.remote_data_paths ) AS data_paths INNER JOIN system.filesystem_cache AS caches ON data_paths.cache_path = caches.cache_path) WHERE endsWith(local_path, 'data.bin') FORMAT Vertical;
+SELECT file_segment_range_begin, file_segment_range_end, size, state
+FROM
+(
+    SELECT file_segment_range_begin, file_segment_range_end, size, state, local_path
+    FROM
+    (
+        SELECT arrayJoin(cache_paths) AS cache_path, local_path, remote_path
+        FROM system.remote_data_paths
+    ) AS data_paths
+    INNER JOIN
+        system.filesystem_cache AS caches
+    ON data_paths.cache_path = caches.cache_path
+)
+WHERE endsWith(local_path, 'data.bin')
+FORMAT Vertical;
+
 SELECT count() FROM (SELECT arrayJoin(cache_paths) AS cache_path, local_path, remote_path FROM system.remote_data_paths ) AS data_paths INNER JOIN system.filesystem_cache AS caches ON data_paths.cache_path = caches.cache_path;
 SELECT count() FROM system.filesystem_cache;
 
