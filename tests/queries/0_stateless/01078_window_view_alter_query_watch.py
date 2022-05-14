0db753c29e43dd7d36a3b70506e770ab73b5e0ba
Vxider <lb@vxider.com>
2022-05-14 09:39:55 +0000

    update test

--- tests/queries/0_stateless/01078_window_view_alter_query_watch.py
+++ tests/queries/0_stateless/01078_window_view_alter_query_watch.py
@@ -13,11 +13,10 @@ log = None
 # uncomment the line below for debugging
 # log=sys.stdout
 
-with client(name="client1>", log=log) as client1, client(
-    name="client2>", log=log
-) as client2:
+with client(name="client1>", log=log) as client1, client(name="client2>", log=log) as client2, client(name="client3>", log=log) as client3:
     client1.expect(prompt)
     client2.expect(prompt)
+    client3.expect(prompt)
 
     client1.send("SET allow_experimental_window_view = 1")
     client1.expect(prompt)
@@ -25,6 +24,8 @@ with client(name="client1>", log=log) as client1, client(
     client1.expect(prompt)
     client2.send("SET allow_experimental_window_view = 1")
     client2.expect(prompt)
+    client3.send("SET allow_experimental_window_view = 1")
+    client3.expect(prompt)
 
     client1.send("CREATE DATABASE IF NOT EXISTS 01078_window_view_alter_query_watch")
     client1.expect(prompt)
@@ -58,10 +59,8 @@ with client(name="client1>", log=log) as client1, client(
         "ALTER TABLE 01078_window_view_alter_query_watch.wv MODIFY QUERY SELECT count(a) * 2 AS count, tumbleEnd(wid) AS w_end FROM 01078_window_view_alter_query_watch.mt GROUP BY tumble(timestamp, INTERVAL '5' SECOND, 'US/Samoa') AS wid"
     )
     client2.expect("Ok.")
-    client2.expect(prompt)
-    client1.expect(prompt)
-    client1.send("WATCH 01078_window_view_alter_query_watch.wv")
-    client1.expect("Query id" + end_of_block)
+    client3.send("WATCH 01078_window_view_alter_query_watch.wv")
+    client3.expect("Query id" + end_of_block)
     client2.send(
         "INSERT INTO 01078_window_view_alter_query_watch.mt VALUES (1, '1990/01/01 12:00:11');"
     )
@@ -70,18 +69,18 @@ with client(name="client1>", log=log) as client1, client(
         "INSERT INTO 01078_window_view_alter_query_watch.mt VALUES (1, '1990/01/01 12:00:18');"
     )
     client2.expect("Ok.")
-    client1.expect("2" + end_of_block)
-    client1.expect("Progress: 1.00 rows.*\)")
+    client3.expect("2" + end_of_block)
+    client3.expect("Progress: 1.00 rows.*\)")
 
     # send Ctrl-C
-    client1.send("\x03", eol="")
-    match = client1.expect("(%s)|([#\$] )" % prompt)
+    client3.send("\x03", eol="")
+    match = client3.expect("(%s)|([#\$] )" % prompt)
     if match.groups()[1]:
-        client1.send(client1.command)
-        client1.expect(prompt)
-    client1.send("DROP TABLE 01078_window_view_alter_query_watch.wv NO DELAY;")
-    client1.expect(prompt)
-    client1.send("DROP TABLE 01078_window_view_alter_query_watch.mt;")
-    client1.expect(prompt)
-    client1.send("DROP DATABASE IF EXISTS 01078_window_view_alter_query_watch;")
-    client1.expect(prompt)
+        client3.send(client3.command)
+        client3.expect(prompt)
+    client3.send("DROP TABLE 01078_window_view_alter_query_watch.wv NO DELAY;")
+    client3.expect(prompt)
+    client3.send("DROP TABLE 01078_window_view_alter_query_watch.mt;")
+    client3.expect(prompt)
+    client3.send("DROP DATABASE IF EXISTS 01078_window_view_alter_query_watch;")
+    client3.expect(prompt)
