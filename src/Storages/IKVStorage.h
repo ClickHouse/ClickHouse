7b283fe10c037876aeb170307f2fde10deb42978
dankondr <dankondr@icloud.com>
2022-05-14 18:00:21 +0000

    Add IKVStorage interface

---
+++ src/Storages/IKVStorage.h
@@ -0,0 +1,25 @@
+#pragma once
+
+#include <Storages/IStorage.h>
+
+namespace DB
+{
+
+
+/// Storage that support key-value requests
+class IKeyValueStorage : public IStorage
+{
+public:
+    using IStorage::IStorage;
+
+    /// Get key name that supports key-value requests
+    virtual const String & getPrimaryKey() const = 0;
+
+    /// Get data directly by keys
+    virtual Chunk getByKeys(
+        const ColumnWithTypeAndName & col,
+        const Block & sample_block,
+        PaddedPODArray<UInt8> * null_map) const = 0;
+};
+
+}
