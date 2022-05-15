eeb26fe1e4ee089104b7a382b75066bfd9491ba2
Artyom Balabanov <artiom.balabanov@gmail.com>
2022-05-15 10:10:57 +0000

    Add methods for serialization and bug fix

--- src/AggregateFunctions/AggregateFunctionOrderBy.h
+++ src/AggregateFunctions/AggregateFunctionOrderBy.h
@@ -8,8 +8,6 @@
 #include <Common/assert_cast.h>
 #include <DataTypes/DataTypeArray.h>
 #include <Interpreters/AggregationCommon.h>
-#include <IO/WriteHelpers.h>
-#include <IO/ReadHelpers.h>
 
 #include <cctype>
 #include <memory>
@@ -32,26 +30,38 @@ struct OrderByData {
     size_t num_arguments;
     size_t num_arguments_for_sorting;
 
+    std::vector<String> join(const String& s) {
+        String curr = "";
+        std::vector<String> ans;
+        size_t i = 0;
+        while (i < s.size()) {
+            if (s[i] == ',') {
+                ans.push_back(curr);
+                curr = "";
+                if ((i + 1 < s.size()) && (s[i + 1] == ' ')) {
+                    i += 2;
+                } else {
+                    i++;
+                }
+            } else {
+                curr.push_back(s[i]);
+                i++;
+            }
+        }
+        ans.push_back(curr);
+        return ans;
+    }
+
     void parseString(const String& s)
     {
-        bool flag = false;
-        for (auto symb : s) {
-            if (flag) {
-                if (symb == 'A') {
-                    bitmap.push_back(true);
-                } else {
-                    bitmap.push_back(false);
-                }
-            }
-            if (symb == ',')
-                if (!flag) {
-                    bitmap.push_back(true);
-                    flag = !flag;
-                }
-            if (isalpha(symb))
-                continue;
-            if (isspace(symb)) {
-                flag = !flag;
+        auto strs = join(s);
+        for (String& str : strs) {
+            std::transform(str.begin(), str.end(), str.begin(), [](unsigned char c) -> unsigned char { return std::toupper(c); });
+            auto pos = str.find("DESC");
+            if ((pos != std::string::npos) && (pos != 0)) {
+                bitmap.push_back(false);
+            } else {
+                bitmap.push_back(true);
             }
         }
         num_arguments_for_sorting = bitmap.size();
@@ -61,19 +71,19 @@ struct OrderByData {
     {
         num_arguments = types.size();
         parseString(param_str);
-        data.reserve(bitmap.size());
+        data.reserve(num_arguments);
         for (size_t i = 0; i != num_arguments; ++i) {
             data.push_back(types[i]->createColumn());
         }
     }
 
-    std::unique_ptr<IColumn*[]> sort() {
+    std::vector<MutableColumnPtr> sort() {
         IColumn::Permutation p;
         for (size_t i = 0; i != data[0]->size(); ++i) {
             p.push_back(i);
         }
 
-        std::unique_ptr<IColumn*[]> ptr{new IColumn*[num_arguments - num_arguments_for_sorting]}; 
+        std::vector<MutableColumnPtr> vec; 
         EqualRanges er(1, std::pair<size_t, size_t>(0, data[0]->size()));
 
         for (size_t i = num_arguments - num_arguments_for_sorting; i != num_arguments; ++i) {
@@ -84,11 +94,10 @@ struct OrderByData {
         }
 
         for (size_t i = 0; i != num_arguments - num_arguments_for_sorting; ++i) {
-            MutableColumnPtr permuted_column = IColumn::mutate(data[i]->permute(p, 0));
-            ptr[i] = permuted_column.get();
+            vec.push_back(IColumn::mutate(data[i]->permute(p, 0)));
         }
 
-        return ptr;
+        return vec;
     }
 };
 
@@ -153,54 +162,70 @@ public:
 
     void merge(AggregateDataPtr __restrict place, ConstAggregateDataPtr rhs, Arena *) const override 
     {
-        for (size_t i = 0; i != reinterpret_cast<const OrderByData *>(rhs)->data[0]->size(); ++i) { 
-            for (size_t j = 0; j != reinterpret_cast<const OrderByData *>(rhs)->data.size(); ++j) {
-                reinterpret_cast<OrderByData *>(place)->data[j]->insertFrom(*(reinterpret_cast<const OrderByData *>(rhs)->data[j]), i);
-            }         
-        }
+        for (size_t i = 0; i != reinterpret_cast<const OrderByData *>(rhs)->data.size(); ++i) {
+            size_t sz = reinterpret_cast<const OrderByData *>(rhs)->data[0]->size();
+            reinterpret_cast<OrderByData *>(place)->data[i]->insertRangeFrom(*(reinterpret_cast<const OrderByData *>(rhs)->data[i]), 0, sz);
+        }         
     }
 
     void serialize(ConstAggregateDataPtr __restrict place, WriteBuffer & buf, std::optional<size_t>) const override 
     {
-        size_t column_size = reinterpret_cast<const OrderByData *>(place)->data[0]->size();
-        writeBinary(column_size, buf);
-        auto size = reinterpret_cast<const OrderByData *>(place)->data.size();
-        for (size_t i = 0; i != size; ++i) {
-            for (size_t j = 0; j != column_size; ++j) {
-                auto ob_data = reinterpret_cast<const OrderByData *>(place);
-                writeBinary((*ob_data->data[i])[j].get<TypeIndexToType<TypeIndex::UInt8>>(), buf);
-            }
+        for (size_t i = 0; i != reinterpret_cast<const OrderByData *>(place)->data.size(); ++i) {
+            const MutableColumnPtr& mutable_column = reinterpret_cast<const OrderByData *>(place)->data[i];
+            size_t sz = reinterpret_cast<const OrderByData *>(place)->data[i]->size();
+            ColumnPtr column(mutable_column->cloneResized(sz));
+            auto serialization = types_[i]->getDefaultSerialization();
+
+            ISerialization::SerializeBinaryBulkSettings settings;
+            settings.getter = [&buf](ISerialization::SubstreamPath) -> WriteBuffer * { return &buf; };
+            settings.position_independent_encoding = false;
+            settings.low_cardinality_max_dictionary_size = 0;
+
+            ISerialization::SerializeBinaryBulkStatePtr state;
+            serialization->serializeBinaryBulkStatePrefix(settings, state);
+            serialization->serializeBinaryBulkWithMultipleStreams(*column, 0, sz, settings, state);
+            serialization->serializeBinaryBulkStateSuffix(settings, state);
         }
     }
 
     void deserialize(AggregateDataPtr __restrict place, ReadBuffer & buf, std::optional<size_t>, Arena *) const override
     {
-        size_t column_size;
-        readBinary(column_size, buf);
-        auto size = reinterpret_cast<OrderByData *>(place)->data.size();
-        for (size_t i = 0; i != size; ++i) {
-            for (size_t j = 0; j != column_size; ++j) {
-                TypeIndexToType<TypeIndex::UInt8> val; 
-                readBinary(val, buf);
-                Field f = val;
-                reinterpret_cast<OrderByData *>(place)->data[i]->insert(f);
-            }
+        for (size_t i = 0; i != reinterpret_cast<const OrderByData *>(place)->data.size(); ++i) {
+            MutableColumnPtr& mutable_column = reinterpret_cast<OrderByData *>(place)->data[i];
+            size_t sz = reinterpret_cast<OrderByData *>(place)->data[i]->size();
+            ColumnPtr column(mutable_column->cloneResized(sz));
+            auto serialization = types_[i]->getDefaultSerialization();
+            
+            ISerialization::DeserializeBinaryBulkSettings settings;
+            settings.getter = [&](ISerialization::SubstreamPath) -> ReadBuffer * { return &buf; };
+            settings.avg_value_size_hint = 0;
+            settings.position_independent_encoding = false;
+            settings.native_format = true;
+
+            ISerialization::DeserializeBinaryBulkStatePtr state;
+
+            serialization->deserializeBinaryBulkStatePrefix(settings, state);
+            serialization->deserializeBinaryBulkWithMultipleStreams(column, sz, settings, state, nullptr);
         }
     }
 
     void add(AggregateDataPtr __restrict place, const IColumn ** columns, size_t row_num, Arena *) const override 
     {
         for (size_t i = 0; i != num_arguments; ++i) {
-            reinterpret_cast<OrderByData *>(place)->data[i]->insertFrom((*columns)[i], row_num);
+            reinterpret_cast<OrderByData *>(place)->data[i]->insertFrom(*columns[i], row_num);
         }               
     }
 
     void insertResultInto(AggregateDataPtr __restrict place, IColumn & to, Arena * arena) const override 
     {
-        std::unique_ptr<IColumn*[]> columns = reinterpret_cast<OrderByData *>(place)->sort();
+        std::vector<MutableColumnPtr> columns = reinterpret_cast<OrderByData *>(place)->sort();
 
-        for (size_t i = 0; i != columns[0]->size(); ++i) {
-            IColumn** columns_ptr = columns.get(); 
+        IColumn* columns_ptr[columns.size()];
+        for (size_t i = 0; i != columns.size(); ++i) {
+            columns_ptr[i] = columns[i].get();
+        }
+
+        for (size_t i = 0; i != columns[0]->size(); ++i) { 
             nested_func->add(place + sizeof(OrderByData), const_cast<const IColumn**>(columns_ptr), i, arena);
         }
 
