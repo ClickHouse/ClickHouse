ba472c97b2fc7fe460298b8a2283fdc62fb79d6a
Vxider <lb@vxider.com>
2022-05-15 17:48:01 +0000

    update tests

--- tests/queries/0_stateless/01059_window_view_event_hop_watch_strict_asc.py
+++ tests/queries/0_stateless/01059_window_view_event_hop_watch_strict_asc.py
@@ -23,7 +23,7 @@ with client(name="client1>", log=log) as client1, client(
     client1.send("SET window_view_heartbeat_interval = 1")
     client1.expect(prompt)
 
-    client1.send("CREATE DATABASE db_01059_event_hop_watch_strict_asc")
+    client1.send("CREATE DATABASE IF NOT EXISTS db_01059_event_hop_watch_strict_asc")
     client1.expect(prompt)
     client1.send("DROP TABLE IF EXISTS db_01059_event_hop_watch_strict_asc.mt")
     client1.expect(prompt)
