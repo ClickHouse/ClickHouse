a161a21992614b2da54c7271de58c779606edc50
lgbo-ustc <lgbo.ustc@gmail.com>
2022-05-17 15:37:32 +0800

    add max partitions check for each hive table

--- src/Core/Settings.h
+++ src/Core/Settings.h
@@ -589,6 +589,7 @@ static constexpr UInt64 operator""_GiB(unsigned long long value)
     M(Bool, count_distinct_optimization, false, "Rewrite count distinct to subquery of group by", 0) \
     M(Bool, throw_on_unsupported_query_inside_transaction, true, "Throw exception if unsupported query is used inside transaction", 0) \
     M(Bool, throw_if_no_data_to_insert, true, "Enables or disables empty INSERTs, enabled by default", 0) \
+    M(UInt64, hive_max_query_partitions, 120, "Limit the max partitions could be queried for each hive table for safty.", 0) \
     // End of COMMON_SETTINGS
     // Please add settings related to formats into the FORMAT_FACTORY_SETTINGS and move obsolete settings to OBSOLETE_SETTINGS.
 
