c883342557b00bef94b9d1912658aaa813c6a537
Robert Schulze <robert@clickhouse.com>
2022-05-17 19:58:38 +0200

    Made the ccache integration more straightforward
    The new logic is simpler and should do the same as before.

--- cmake/ccache.cmake
+++ cmake/ccache.cmake
@@ -8,53 +8,42 @@ if (CMAKE_CXX_COMPILER_LAUNCHER MATCHES "ccache" OR CMAKE_C_COMPILER_LAUNCHER MA
     return()
 endif()
 
-if ((ENABLE_CCACHE OR NOT DEFINED ENABLE_CCACHE))
-    find_program (CCACHE_FOUND ccache)
-
-    if (CCACHE_FOUND)
-        set(ENABLE_CCACHE_BY_DEFAULT 1)
-    else()
-        set(ENABLE_CCACHE_BY_DEFAULT 0)
-    endif()
-endif()
-
-if (NOT CCACHE_FOUND AND NOT DEFINED ENABLE_CCACHE)
-    message(WARNING "CCache is not found. We recommend setting it up if you build ClickHouse from source often. "
-            "Setting it up will significantly reduce compilation time for 2nd and consequent builds")
-endif()
-
-option(ENABLE_CCACHE "Speedup re-compilations using ccache (external tool)" ${ENABLE_CCACHE_BY_DEFAULT})
+option(ENABLE_CCACHE "Speedup re-compilations using ccache (external tool)" ON)
 
 if (NOT ENABLE_CCACHE)
+    message(STATUS "Using ccache: no (disabled via configuration)")
     return()
 endif()
 
-if (CCACHE_FOUND)
-   execute_process(COMMAND ${CCACHE_FOUND} "-V" OUTPUT_VARIABLE CCACHE_VERSION)
-   string(REGEX REPLACE "ccache version ([0-9\\.]+).*" "\\1" CCACHE_VERSION ${CCACHE_VERSION})
+find_program (CCACHE_EXECUTABLE ccache)
 
-   if (CCACHE_VERSION VERSION_GREATER "3.2.0" OR NOT COMPILER_CLANG)
-      message(STATUS "Using ccache: ${CCACHE_FOUND} (version ${CCACHE_VERSION})")
-      set(LAUNCHER ${CCACHE_FOUND})
+if (NOT CCACHE_EXECUTABLE)
+    message(${RECONFIGURE_MESSAGE_LEVEL} "Using ccache: no (Could not find find ccache. To significantly reduce compile times for the 2nd, 3rd, etc. build, it is highly recommended to install ccache. To suppress this message, run cmake with -DENABLE_CCACHE=0)")
+    return()
+endif()
 
-      # debian (debhelpers) set SOURCE_DATE_EPOCH environment variable, that is
-      # filled from the debian/changelog or current time.
-      #
-      # - 4.0+ ccache always includes this environment variable into the hash
-      #   of the manifest, which do not allow to use previous cache,
-      # - 4.2+ ccache ignores SOURCE_DATE_EPOCH for every file w/o __DATE__/__TIME__
-      #
-      # Exclude SOURCE_DATE_EPOCH env for ccache versions between [4.0, 4.2).
-      if (CCACHE_VERSION VERSION_GREATER_EQUAL "4.0" AND CCACHE_VERSION VERSION_LESS "4.2")
-         message(STATUS "Ignore SOURCE_DATE_EPOCH for ccache")
-         set(LAUNCHER env -u SOURCE_DATE_EPOCH ${CCACHE_FOUND})
-      endif()
+execute_process(COMMAND ${CCACHE_EXECUTABLE} "-V" OUTPUT_VARIABLE CCACHE_VERSION)
+string(REGEX REPLACE "ccache version ([0-9\\.]+).*" "\\1" CCACHE_VERSION ${CCACHE_VERSION})
 
-      set (CMAKE_CXX_COMPILER_LAUNCHER ${LAUNCHER} ${CMAKE_CXX_COMPILER_LAUNCHER})
-      set (CMAKE_C_COMPILER_LAUNCHER ${LAUNCHER} ${CMAKE_C_COMPILER_LAUNCHER})
-   else ()
-       message(${RECONFIGURE_MESSAGE_LEVEL} "Using ccache: No. Found ${CCACHE_FOUND} (version ${CCACHE_VERSION}) but disabled because of bug: https://bugzilla.samba.org/show_bug.cgi?id=8118")
-   endif ()
-elseif (NOT CCACHE_FOUND)
-    message (${RECONFIGURE_MESSAGE_LEVEL} "Using ccache: No")
+if (CCACHE_VERSION VERSION_GREATER "3.2.0" OR NOT COMPILER_CLANG)
+    message(STATUS "Using ccache: ${CCACHE_EXECUTABLE} (version ${CCACHE_VERSION})")
+    set(LAUNCHER ${CCACHE_EXECUTABLE})
+
+   # debian (debhelpers) set SOURCE_DATE_EPOCH environment variable, that is
+   # filled from the debian/changelog or current time.
+   #
+   # - 4.0+ ccache always includes this environment variable into the hash
+   #   of the manifest, which do not allow to use previous cache,
+   # - 4.2+ ccache ignores SOURCE_DATE_EPOCH for every file w/o __DATE__/__TIME__
+   #
+   # Exclude SOURCE_DATE_EPOCH env for ccache versions between [4.0, 4.2).
+   if (CCACHE_VERSION VERSION_GREATER_EQUAL "4.0" AND CCACHE_VERSION VERSION_LESS "4.2")
+      message(STATUS "Ignore SOURCE_DATE_EPOCH for ccache")
+      set(LAUNCHER env -u SOURCE_DATE_EPOCH ${CCACHE_EXECUTABLE})
+   endif()
+
+   set (CMAKE_CXX_COMPILER_LAUNCHER ${LAUNCHER} ${CMAKE_CXX_COMPILER_LAUNCHER})
+   set (CMAKE_C_COMPILER_LAUNCHER ${LAUNCHER} ${CMAKE_C_COMPILER_LAUNCHER})
+else ()
+   message(${RECONFIGURE_MESSAGE_LEVEL} "Using ccache: No. Found ${CCACHE_EXECUTABLE} (version ${CCACHE_VERSION}) but disabled because of bug: https://bugzilla.samba.org/show_bug.cgi?id=8118")
 endif ()
