ec198498b0c2bd5f6deac95ff30618337a8f5aba
ltybc <dvsemenov138@yandex.ru>
2022-05-18 13:25:30 +0000

    minor refactoring

--- src/AggregateFunctions/AggregateFunctionOrderBy.cpp
+++ src/AggregateFunctions/AggregateFunctionOrderBy.cpp
@@ -18,19 +18,17 @@ namespace
 class AggregateFunctionCombinatorOrderBy final : public IAggregateFunctionCombinator
 {
 public:
-    String getName() const override { return "OrderBy"; }
+    String getName() const override 
+    { 
+        return "OrderBy"; 
+    }
 
     DataTypes transformArguments(const DataTypes & arguments) const override
     {
         if (arguments.empty())
             throw Exception("Incorrect number of arguments for aggregate function with " + getName() + " suffix",
                             ErrorCodes::NUMBER_OF_ARGUMENTS_DOESNT_MATCH);
-
-        DataTypes newArguments(arguments.size());
-        for (size_t i = 0; i < arguments.size(); i++) {
-            newArguments[i] = arguments[i];
-        }
-        return newArguments;
+        return arguments;
     }
 
     Array transformParameters(const Array & params) const override 
