3c85e8b455713433d0ac386125c38e0f35488fec
avogar <pav.cruglov@yandex.ru>
2022-05-18 12:36:13 +0000

    Fix tests

--- src/Storages/ColumnsDescription.cpp
+++ src/Storages/ColumnsDescription.cpp
@@ -31,6 +31,7 @@
 #include <Interpreters/ExpressionActions.h>
 #include <Interpreters/FunctionNameNormalizer.h>
 
+#include <Common/logger_useful.h>
 
 namespace DB
 {
@@ -543,9 +544,16 @@ Names ColumnsDescription::getNamesOfPhysical() const
 
 std::optional<NameAndTypePair> ColumnsDescription::tryGetColumn(const GetColumnsOptions & options, const String & column_name) const
 {
-    auto column_description = tryGetColumnDescription(options, column_name);
-    if (column_description)
-        return NameAndTypePair{column_description->name, column_description->type};
+    auto it = columns.get<1>().find(column_name);
+    if (it != columns.get<1>().end() && (defaultKindToGetKind(it->default_desc.kind) & options.kind))
+        return NameAndTypePair(it->name, it->type);
+
+    if (options.with_subcolumns)
+    {
+        auto jt = subcolumns.get<0>().find(column_name);
+        if (jt != subcolumns.get<0>().end())
+            return *jt;
+    }
 
     return {};
 }
