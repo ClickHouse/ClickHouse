f0c491312ad0c796760eb2babb905300828a7fae
Kruglov Pavel <48961922+Avogar@users.noreply.github.com>
2022-05-18 17:44:39 +0200

    Remove unnecessary include

--- src/Processors/Formats/Impl/ArrowColumnToCHColumn.cpp
+++ src/Processors/Formats/Impl/ArrowColumnToCHColumn.cpp
@@ -32,7 +32,6 @@
 #include <arrow/builder.h>
 #include <arrow/array.h>
 
-#include <Common/logger_useful.h>
 
 /// UINT16 and UINT32 are processed separately, see comments in readColumnFromArrowColumn.
 #define FOR_ARROW_NUMERIC_TYPES(M) \
