3b0556b30dce9b2d82389c6a716a63d4e78c9356
Kruglov Pavel <48961922+Avogar@users.noreply.github.com>
2022-05-18 17:43:28 +0200

    Update test

--- tests/queries/0_stateless/02304_orc_arrow_parquet_string_as_string.sql
+++ tests/queries/0_stateless/02304_orc_arrow_parquet_string_as_string.sql
@@ -1,3 +1,5 @@
+-- Tags: no-fasttest
+
 insert into function file(data_02304.parquet) select 'hello' as s from numbers(3) settings engine_file_truncate_on_insert=1, output_format_parquet_string_as_string=1;
 desc file(data_02304.parquet);
 insert into function file(data_02304.orc) select 'hello' as s from numbers(3) settings engine_file_truncate_on_insert=1, output_format_orc_string_as_string=1;
