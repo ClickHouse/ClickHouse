e1ed7317abcef3a284897f04c26da1c8a8ba6fd1
Kruglov Pavel <48961922+Avogar@users.noreply.github.com>
2022-05-18 17:16:08 +0200

    Fix fast test

--- tests/queries/0_stateless/02302_defaults_in_columnar_formats.sql
+++ tests/queries/0_stateless/02302_defaults_in_columnar_formats.sql
@@ -1,3 +1,5 @@
+-- Tags: no-fasttest
+
 insert into function file(data_02302.parquet) select 1 as x settings engine_file_truncate_on_insert=1;
 select * from file(data_02302.parquet, auto, 'x UInt8, y default 42, z default x + y') settings input_format_parquet_allow_missing_columns=1;
 insert into function file(data_02302.orc) select 1 as x settings engine_file_truncate_on_insert=1; 
