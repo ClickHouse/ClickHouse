6e158a799642ef2b347d775b3bb3b79fb6c5d7ea
Kruglov Pavel <48961922+Avogar@users.noreply.github.com>
2022-05-19 12:53:40 +0200

    Mark test as no-parallel

--- tests/queries/0_stateless/02304_orc_arrow_parquet_string_as_string.sql
+++ tests/queries/0_stateless/02304_orc_arrow_parquet_string_as_string.sql
@@ -1,4 +1,4 @@
--- Tags: no-fasttest
+-- Tags: no-fasttest, no-parallel
 
 insert into function file(data_02304.parquet) select 'hello' as s from numbers(3) settings engine_file_truncate_on_insert=1, output_format_parquet_string_as_string=1;
 desc file(data_02304.parquet);
