0cee5dda6ace59b06bf90b7d89670a3667c8df6d
Vladimir Chebotarev <vladimir.chebotarev@gmail.com>
2022-05-20 11:35:18 +0300

    Added one more symbol and proper comment.

--- src/IO/S3/PocoHTTPClient.cpp
+++ src/IO/S3/PocoHTTPClient.cpp
@@ -191,33 +191,18 @@ void PocoHTTPClient::makeRequestInternal(
 
             Poco::Net::HTTPRequest poco_request(Poco::Net::HTTPRequest::HTTP_1_1);
 
-            /** Aws::Http::URI will encode URL in appropriate way for AWS S3 server.
-              * Poco::URI also does that correctly but it's not compatible with AWS.
-              * For example, `+` symbol will not be converted to `%2B` by Poco and would
-              * be received as space symbol.
-              *
-              * References:
-              * https://github.com/aws/aws-sdk-java/issues/1946
-              * https://forums.aws.amazon.com/thread.jspa?threadID=55746
-              *
-              * Example:
-              * Suppose we are requesting a file: abc+def.txt
-              * To correctly do it, we need to construct an URL containing either:
-              * - abc%2Bdef.txt
-              * this is also technically correct:
-              * - abc+def.txt
-              * but AWS servers don't support it properly, interpreting plus character as whitespace
-              * although it is in path part, not in query string.
-              * e.g. this is not correct:
-              * - abc%20def.txt
-              *
-              * Poco will keep plus character as is (which is correct) while AWS servers will treat it as whitespace, which is not what is intended.
-              * To overcome this limitation, we encode URL with "Aws::Http::URI" and then pass already prepared URL to Poco.
+            /** According to RFC-2616, Request-URI is allowed to be encoded.
+              * However, there is no clear agreement on which exact symbols must be encoded.
+              * Effectively, `Poco::URI` chooses smaller subset of characters to encode,
+              * whereas Amazon S3 and Google Cloud Storage expects another one.
+              * In order to successfully execute a request, a path must be exact representation
+              * of decoded path used by `S3AuthSigner`.
+              * Therefore we shall encode some symbols "manually" to fit the signatures.
               */
 
             std::string path_and_query;
             const std::string & query = target_uri.getRawQuery();
-            const std::string reserved = "?#:;+@&="; /// Poco::URI::RESERVED_QUERY_PARAM without '/'.
+            const std::string reserved = "?#:;+@&=%"; /// Poco::URI::RESERVED_QUERY_PARAM without '/' plus percent sign.
             Poco::URI::encode(target_uri.getPath(), reserved, path_and_query);
             if (!query.empty())
             {
