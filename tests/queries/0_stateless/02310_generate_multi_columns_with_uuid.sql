3a17180a4ffcb500111b92dc0a69574dcd6cf8d8
Memo <joeywang98@outlook.com>
2022-05-22 13:42:36 +0800

    fix tests

--- tests/queries/0_stateless/02310_generate_multi_columns_with_uuid.sql
+++ tests/queries/0_stateless/02310_generate_multi_columns_with_uuid.sql
@@ -1,19 +1,5 @@
-select generateUUIDv4() from numbers(10);
+SELECT generateUUIDv4(1) = generateUUIDv4(2);
 
-select generateUUIDv4(1) from numbers(10);
+SELECT generateUUIDv4() = generateUUIDv4(1);
 
-select generateUUIDv4(2) from numbers(10);
-
-select generateUUIDv4(1), generateUUIDv4(1) from numbers(10);
-
-select generateUUIDv4(), generateUUIDv4() from numbers(10);
-
-select generateUUIDv4(1), generateUUIDv4() from numbers(10);
-
-select generateUUIDv4(), generateUUIDv4(1) from numbers(10);
-
-select generateUUIDv4(1), generateUUIDv4(2) from numbers(10);
-
-select generateUUIDv4(1), generateUUIDv4(1), generateUUIDv4(1) from numbers(10);
-
-select generateUUIDv4(1), generateUUIDv4(2), generateUUIDv4(3) from numbers(10);
+SELECT generateUUIDv4(1) = generateUUIDv4(1);
