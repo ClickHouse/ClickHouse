a6347dcea626fa21f26cdfd38055efa25acaba14
zhao zhou <zhou.zhao@intel.com>
2022-05-23 01:11:01 +0800

    fix deference null optional value issue. Dereference NUll optioanl value in debug mode will trigger about signal.

--- src/Columns/ColumnVector.cpp
+++ src/Columns/ColumnVector.cpp
@@ -574,34 +574,37 @@ ColumnPtr ColumnVector<T>::replicateSSE2(const IColumn::Offsets & offsets) const
         prev_offset = offsets[i];
         if (span == 1)
         {
-            if (!copy_begin)
-                copy_begin = i;
-            continue;
+	    if (!copy_begin)
+	       copy_begin = i;
+	    continue;
         }
 
         /// data :   11 22 33 44 55
         /// offsets: 0  1  2  3  3
         /// res:     22 33  44
+        if(copy_begin)
+	{
+	    size_t copy_size = i - (*copy_begin);
+	    bool remain = (copy_size & 3);
+	    size_t sse_copy_counter = (copy_size >> 2);
+	    sse_copy_counter = remain * (sse_copy_counter + 1) + (!remain) * (sse_copy_counter);
+	    auto it_tmp = it; // NOLINT
+	    size_t data_start = *copy_begin;
+	    copy_begin.reset();
+	    constexpr const int copy_mask = _MM_SHUFFLE(3, 2, 1, 0);
+	    while (sse_copy_counter)
+	    {
+	        __m128i data_to_copy = _mm_loadu_si128(reinterpret_cast<const __m128i *>(&data[data_start]));
+	        auto copy_result = _mm_shuffle_epi32(data_to_copy, copy_mask);
+	        _mm_storeu_si128(reinterpret_cast<__m128i *>(it_tmp), copy_result);
+	        it_tmp += 4;
+	        data_start += 4;
+	        --sse_copy_counter;
+	    }
 
-        size_t copy_size = (static_cast<bool>(copy_begin)) * (i - (*copy_begin));
-        bool remain = (copy_size & 3);
-        size_t sse_copy_counter = (copy_size >> 2);
-        sse_copy_counter = remain * (sse_copy_counter + 1) + (!remain) * (sse_copy_counter);
-        auto it_tmp = it; // NOLINT
-        size_t data_start = (static_cast<bool>(copy_begin))*(*copy_begin);
-        copy_begin.reset();
-        constexpr const int copy_mask = _MM_SHUFFLE(3, 2, 1, 0);
-        while (sse_copy_counter)
-        {
-            __m128i data_to_copy = _mm_loadu_si128(reinterpret_cast<const __m128i *>(&data[data_start]));
-            auto copy_result = _mm_shuffle_epi32(data_to_copy, copy_mask);
-            _mm_storeu_si128(reinterpret_cast<__m128i *>(it_tmp), copy_result);
-            it_tmp += 4;
-            data_start += 4;
-            --sse_copy_counter;
-        }
+	    it += copy_size;
+	}
 
-        it += copy_size;
         if (span == 0)
             continue;
 
@@ -612,13 +615,13 @@ ColumnPtr ColumnVector<T>::replicateSSE2(const IColumn::Offsets & offsets) const
         bool shuffle_remain = (shuffle_size & 3);
         size_t sse_shuffle_counter = (shuffle_size >> 2);
         sse_shuffle_counter = shuffle_remain * (sse_shuffle_counter + 1) + (!shuffle_remain) * (sse_shuffle_counter);
-        it_tmp = it;
+        auto it_tmp = it; // NOLINT
         constexpr const int shuffle_mask = (_MM_SHUFFLE(0, 0, 0, 0));
-        __m128i data_to_copy = _mm_loadu_si128(reinterpret_cast<const __m128i *>(&data[i]));
+        __m128i data_to_shuffle = _mm_loadu_si128(reinterpret_cast<const __m128i *>(&data[i]));
+	auto shuffle_result = _mm_shuffle_epi32(data_to_shuffle, shuffle_mask);
         while (sse_shuffle_counter)
         {
-            auto copy_result = _mm_shuffle_epi32(data_to_copy, shuffle_mask);
-            _mm_storeu_si128(reinterpret_cast<__m128i *>(it_tmp), copy_result);
+            _mm_storeu_si128(reinterpret_cast<__m128i *>(it_tmp), shuffle_result);
             it_tmp += 4;
             --sse_shuffle_counter;
         }
