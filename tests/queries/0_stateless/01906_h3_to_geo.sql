6fe9231ee87195b53a3f42b7c5751f42ec35506f
Alexey Milovidov <milovidov@yandex-team.ru>
2022-05-22 22:58:31 +0200

    Update some tests

--- tests/queries/0_stateless/01906_h3_to_geo.sql
+++ tests/queries/0_stateless/01906_h3_to_geo.sql
@@ -56,7 +56,7 @@ SELECT result FROM (
     SELECT
         (lat, lon) AS input_geo,
         h3ToGeo(geoToH3(lat, lon, res)) AS output_geo,
-        if(input_geo = output_geo, 'ok', 'fail') AS result
+        if(abs(input_geo.1 - output_geo.1) < 0.001 AND abs(input_geo.2 - output_geo.2) < 0.001, 'ok', 'fail') AS result
     FROM h3_geo
 );
 
