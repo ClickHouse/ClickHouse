dfbb324973830c36ea7f60de06a87c707d1aa9f0
avogar <pav.cruglov@yandex.ru>
2022-05-23 14:25:43 +0000

    Update test

--- tests/queries/0_stateless/02312_parquet_orc_arrow_names_tuples.sql
+++ tests/queries/0_stateless/02312_parquet_orc_arrow_names_tuples.sql
@@ -1,13 +1,29 @@
-create table parquet_02312 (x Tuple(a UInt32, b UInt32)) engine=File(Parquet);
-insert into parquet_02312 values ((1,2)), ((2,3)), ((3,4));
-select * from parquet_02312;
-drop table parquet_02312;
-create table parquet_02312 (x Tuple(a UInt32, b UInt32)) engine=File(Arrow);
-insert into parquet_02312 values ((1,2)), ((2,3)), ((3,4));
-select * from parquet_02312;
-drop table parquet_02312;
-create table parquet_02312 (x Tuple(a UInt32, b UInt32)) engine=File(ORC);
-insert into parquet_02312 values ((1,2)), ((2,3)), ((3,4));
-select * from parquet_02312;
-drop table parquet_02312;
+-- Tags: no-fasttest
+
+drop table if exists test_02312;
+create table test_02312 (x Tuple(a UInt32, b UInt32)) engine=File(Parquet);
+insert into test_02312 values ((1,2)), ((2,3)), ((3,4));
+select * from test_02312;
+drop table test_02312;
+create table test_02312 (x Tuple(a UInt32, b UInt32)) engine=File(Arrow);
+insert into test_02312 values ((1,2)), ((2,3)), ((3,4));
+select * from test_02312;
+drop table test_02312;
+create table test_02312 (x Tuple(a UInt32, b UInt32)) engine=File(ORC);
+insert into test_02312 values ((1,2)), ((2,3)), ((3,4));
+select * from test_02312;
+drop table test_02312;
+
+create table test_02312 (a Nested(b Nested(c UInt32))) engine=File(Parquet);
+insert into test_02312 values ([[(1), (2), (3)]]);
+select * from test_02312;
+drop table test_02312;
+create table test_02312 (a Nested(b Nested(c UInt32))) engine=File(Arrow);
+insert into test_02312 values ([[(1), (2), (3)]]);
+select * from test_02312;
+drop table test_02312;
+create table test_02312 (a Nested(b Nested(c UInt32))) engine=File(ORC);
+insert into test_02312 values ([[(1), (2), (3)]]);
+select * from test_02312;
+drop table test_02312;
 
