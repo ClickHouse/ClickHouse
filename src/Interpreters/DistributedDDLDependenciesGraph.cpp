c40579a0c280a2f5b33736b08c8234399ccffa80
Shaprunov Kirill <shaprunovk@mail.ru>
2022-05-23 04:25:30 +0300

    Fixed bugs

--- src/Interpreters/DistributedDDLDependenciesGraph.cpp
+++ src/Interpreters/DistributedDDLDependenciesGraph.cpp
@@ -142,7 +142,7 @@ void DependenciesGraph::logDependencyGraph() const
              tasks_dependencies.independent_queries.size());
     for (const auto & independent_query : tasks_dependencies.independent_queries)
     {
-        const auto & query_dependencies = tasks_dependencies.dependencies_info.at(independent_query);
+        auto & query_dependencies = tasks_dependencies.dependencies_info.at(independent_query);
 
         LOG_TEST(log,
                  "Independent query: {} have {} dependencies and {} dependent queries.",
