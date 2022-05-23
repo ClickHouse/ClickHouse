b73d49158d73245e8ee190dfefad196b017f9bcb
Kruglov Pavel <48961922+Avogar@users.noreply.github.com>
2022-05-23 17:01:45 +0200

    Fix test

--- tests/queries/0_stateless/02311_create_table_with_unknown_format.sql
+++ tests/queries/0_stateless/02311_create_table_with_unknown_format.sql
@@ -1,3 +1,5 @@
+-- Tags: no-fasttest
+
 create table test_02311 (x UInt32) engine=File(UnknownFormat); -- {serverError UNKNOWN_FORMAT}
 create table test_02311 (x UInt32) engine=URL('http://some/url', UnknownFormat); -- {serverError UNKNOWN_FORMAT}
 create table test_02311 (x UInt32) engine=S3('http://host:2020/test/data', UnknownFormat); -- {serverError UNKNOWN_FORMAT}
