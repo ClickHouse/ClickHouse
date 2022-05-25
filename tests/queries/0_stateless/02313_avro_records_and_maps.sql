0498b8b2221fd724fe159c638016791118e59ee1
avogar <pav.cruglov@yandex.ru>
2022-05-25 11:22:04 +0000

    Add tags to test

--- tests/queries/0_stateless/02313_avro_records_and_maps.sql
+++ tests/queries/0_stateless/02313_avro_records_and_maps.sql
@@ -1,3 +1,5 @@
+-- Tags: no-parallel, no-fasttest
+
 insert into function file(data_02313.avro) select tuple(number, 'String')::Tuple(a UInt32, b String) as t from numbers(3) settings engine_file_truncate_on_insert=1;
 desc file(data_02313.avro);
 select * from file(data_02313.avro);
