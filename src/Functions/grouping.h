7cd7782e4f25b0a2aad9cfa69cd9ffcb4da1e6cc
Dmitry Novik <n0vik@clickhouse.com>
2022-05-25 21:55:41 +0000

    Process columns more efficiently in GROUPING()

--- src/Functions/grouping.h
+++ src/Functions/grouping.h
@@ -43,18 +43,20 @@ public:
     template <typename AggregationKeyChecker>
     ColumnPtr executeImpl(const ColumnsWithTypeAndName & arguments, size_t input_rows_count, AggregationKeyChecker checker) const
     {
-        auto grouping_set_column = checkAndGetColumn<ColumnUInt64>(arguments[0].column.get());
+        const auto * grouping_set_column = checkAndGetColumn<ColumnUInt64>(arguments[0].column.get());
 
-        auto result = std::make_shared<DataTypeUInt64>()->createColumn();
+        auto result = ColumnUInt64::create();
+        auto & result_data = result->getData();
+        result_data.reserve(input_rows_count);
         for (size_t i = 0; i < input_rows_count; ++i)
         {
-            UInt64 set_index = grouping_set_column->get64(i);
+            UInt64 set_index = grouping_set_column->getElement(i);
 
             UInt64 value = 0;
             for (auto index : arguments_indexes)
                 value = (value << 1) + (checker(set_index, index) ? 1 : 0);
 
-            result->insert(Field(value));
+            result_data.push_back(value);
         }
         return result;
     }
