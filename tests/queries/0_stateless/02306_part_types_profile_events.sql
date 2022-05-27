36d5ebca96278e9956bf01b15784500c53557b16
Anton Popov <ap@clickhouse.com>
2022-05-27 12:27:25 +0000

    fix tests

--- tests/queries/0_stateless/02306_part_types_profile_events.sql
+++ tests/queries/0_stateless/02306_part_types_profile_events.sql
@@ -29,7 +29,7 @@ SELECT count(), sum(ProfileEvents['InsertedWideParts']), sum(ProfileEvents['Inse
         AND query ILIKE 'INSERT INTO%' AND type = 'QueryFinish';
 
 SELECT count(), sum(ProfileEvents['MergedIntoWideParts']), sum(ProfileEvents['MergedIntoCompactParts'])
-    FROM system.query_log WHERE has(databases, currentDatabase())
+    FROM system.query_log WHERE current_database = currentDatabase()
         AND log_comment = '02306_part_types_profile_events'
         AND query ILIKE 'OPTIMIZE TABLE%' AND type = 'QueryFinish';
 
@@ -39,6 +39,6 @@ SELECT part_type FROM system.part_log WHERE database = currentDatabase()
 
 SELECT part_type, count() > 0 FROM system.part_log WHERE database = currentDatabase()
     AND table = 't_parts_profile_events' AND event_type = 'MergeParts'
-    GROUP BY part_type;
+    GROUP BY part_type ORDER BY part_type;
 
 DROP TABLE t_parts_profile_events;
