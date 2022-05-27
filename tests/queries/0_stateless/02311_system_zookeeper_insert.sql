0edb385b8371a5aa32296443bdec354115a94ab5
Han Fei <hanfei19910905@gmail.com>
2022-05-28 00:10:24 +0800

    update tests

--- tests/queries/0_stateless/02311_system_zookeeper_insert.sql
+++ tests/queries/0_stateless/02311_system_zookeeper_insert.sql
@@ -3,21 +3,21 @@
 set allow_unrestricted_reads_from_keeper = 'true';
 
 -- test recursive create and big transaction
-insert into system.zookeeper (name, path, value) values ('c', '/testc/c/c/c/c/c/c', 11), ('e', '/testc/c/c/d', 10), ('c', '/testc/c/c/c/c/c/c/c', 10), ('c', '/testc/c/c/c/c/c/c', 9), ('f', '/testc/c/c/d', 11), ('g', '/testc/c/c/d', 12), ('g', '/testc/c/c/e', 13), ('g', '/testc/c/c/f', 14), ('g', '/testc/c/c/kk', 14);
+insert into system.zookeeper (name, path, value) values ('c', '/insert-testc/c/c/c/c/c/c', 11), ('e', '/insert-testc/c/c/d', 10), ('c', '/insert-testc/c/c/c/c/c/c/c', 10), ('c', '/insert-testc/c/c/c/c/c/c', 9), ('f', '/insert-testc/c/c/d', 11), ('g', '/insert-testc/c/c/d', 12), ('g', '/insert-testc/c/c/e', 13), ('g', '/insert-testc/c/c/f', 14), ('g', '/insert-testc/c/c/kk', 14);
 -- insert same value, suppose to have no side effects
-insert into system.zookeeper (name, path, value) values ('c', '/testc/c/c/c/c/c/c', 11), ('e', '/testc/c/c/d', 10), ('c', '/testc/c/c/c/c/c/c/c', 10), ('c', '/testc/c/c/c/c/c/c', 9), ('f', '/testc/c/c/d', 11), ('g', '/testc/c/c/d', 12), ('g', '/testc/c/c/e', 13), ('g', '/testc/c/c/f', 14), ('g', '/testc/c/c/kk', 15);
+insert into system.zookeeper (name, path, value) values ('c', '/insert-testc/c/c/c/c/c/c', 11), ('e', '/insert-testc/c/c/d', 10), ('c', '/insert-testc/c/c/c/c/c/c/c', 10), ('c', '/insert-testc/c/c/c/c/c/c', 9), ('f', '/insert-testc/c/c/d', 11), ('g', '/insert-testc/c/c/d', 12), ('g', '/insert-testc/c/c/e', 13), ('g', '/insert-testc/c/c/f', 14), ('g', '/insert-testc/c/c/kk', 15);
 --allow_unrestricted_reads_from_keeper=1 
-SELECT * FROM (SELECT path, name, value FROM system.zookeeper ORDER BY path, name) WHERE path LIKE '/test%';
+SELECT * FROM (SELECT path, name, value FROM system.zookeeper ORDER BY path, name) WHERE path LIKE '/insert-test%';
 
 SELECT '-------------------------';
 
 -- test inserting into root path
-insert into system.zookeeper (name, path, value) values ('testc', '/', 'x');
-insert into system.zookeeper (name, path, value) values ('testz', '/', 'y');
-insert into system.zookeeper (name, path, value) values ('testc', 'testz//c/cd/dd//', 'y');
-insert into system.zookeeper (name, value, path) values ('testz', 'z', '/');
+insert into system.zookeeper (name, path, value) values ('testc', '/insert-testx', 'x');
+insert into system.zookeeper (name, path, value) values ('testz', '/insert-testx', 'y');
+insert into system.zookeeper (name, path, value) values ('testc', 'insert-testz//c/cd/dd//', 'y');
+insert into system.zookeeper (name, value, path) values ('testz', 'z', '/insert-testx');
 
-SELECT * FROM (SELECT path, name, value FROM system.zookeeper ORDER BY path, name);
+SELECT * FROM (SELECT path, name, value FROM system.zookeeper ORDER BY path, name) WHERE path LIKE '/insert-test%';
 
 -- test exceptions 
 insert into system.zookeeper (name, path, value) values ('a/b/c', '/', 'y'); -- { serverError 36 }
