3ff32fe81dcdb86d555917e9ed5f7437996e4579
Yakov Olkhovskiy <yakov@clickhouse.com>
2022-05-28 12:30:05 -0400

    test is added

---
+++ tests/integration/test_storage_dict/test.py
@@ -0,0 +1,40 @@
+import pytest
+
+from helpers.cluster import ClickHouseCluster
+
+uuids = []
+
+
+@pytest.fixture(scope="module")
+def cluster():
+    try:
+        cluster = ClickHouseCluster(__file__)
+        cluster.add_instance(
+            "node1", main_configs=["configs/conf.xml"], with_nginx=True
+        )
+        cluster.start()
+
+        yield cluster
+
+    finally:
+        cluster.shutdown()
+
+
+def test_storage_dict(cluster):
+    node1 = cluster.instances["node1"]
+
+    node1.query(
+        f"insert into table function url(urldb) values ('foo', 'bar')"
+    )
+    result = node1.query(
+        f"select * from url(urldb)"
+    )
+    assert result.strip() == "foo\tbar"
+
+    node1.query(
+        f"create dictionary dict (k String, v String) primary key k source(http(name urldict)) layout(complex_key_hashed()) lifetime(min 0 max 100)"
+    )
+    result = node1.query(
+        f"select * from dict"
+    )
+    assert result.strip() == "foo\tbar"
