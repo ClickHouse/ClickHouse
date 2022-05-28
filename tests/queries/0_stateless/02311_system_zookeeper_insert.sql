d883194eb21ea53e32f7465e7064cf0d85edf49d
Han Fei <hanfei19910905@gmail.com>
2022-05-28 22:20:09 +0800

    try to make test stable

--- tests/queries/0_stateless/02311_system_zookeeper_insert.sql
+++ tests/queries/0_stateless/02311_system_zookeeper_insert.sql
@@ -23,7 +23,7 @@ SELECT '-------------------------';
 insert into test_zkinsert (name, path, value) values ('testc', '/2-insert-testx', 'x');
 insert into test_zkinsert (name, path, value) values ('testz', '/2-insert-testx', 'y');
 insert into test_zkinsert (name, path, value) values ('testc', '/2-insert-testz//c/cd/dd//', 'y');
-insert into test_zkinsert (name, value, path) values ('testz', 'z', '/2-insert-testx');
+insert into test_zkinsert (name, value, path) values ('testb', 'z', '/2-insert-testx');
 
 insert into system.zookeeper (name, path, value) SELECT name, '/' || currentDatabase() || path, value from test_zkinsert;
 
