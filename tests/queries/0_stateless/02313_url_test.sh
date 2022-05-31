48ec2577fc8678c5c87f6995ffa816ec20b54dac
avogar <pav.cruglov@yandex.ru>
2022-05-31 07:49:00 +0000

    Add test that crashes server

---
+++ tests/queries/0_stateless/02313_url_test.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+
+CUR_DIR=$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)
+# shellcheck source=../shell_config.sh
+. "$CUR_DIR"/../shell_config.sh
+
+for i in $(seq 1 200);
+do
+    $CLICKHOUSE_CLIENT -q "SELECT * FROM url('https://datasets.clickhouse.com/github_events_v2.native.xz') LIMIT 1" &
+done
