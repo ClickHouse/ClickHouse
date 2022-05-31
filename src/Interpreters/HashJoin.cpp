f670d960f4b0be94a62606e47d5d366870d4df23
vdimir <vdimir@clickhouse.com>
2022-05-31 16:02:06 +0000

    Rollback some changes from appendFromBlock

--- src/Interpreters/HashJoin.cpp
+++ src/Interpreters/HashJoin.cpp
@@ -986,29 +986,29 @@ public:
     }
 
     template <bool has_defaults>
-    void appendFromBlock(Block block, size_t row_num)
+    void appendFromBlock(const Block & block, size_t row_num)
     {
         if constexpr (has_defaults)
             applyLazyDefaults();
 
-        for (size_t j = 0, size = right_indexes.size(); j < size; ++j)
-        {
-            ColumnWithTypeAndName & column_from_block = block.getByPosition(right_indexes[j]);
-            if (type_name[j].type->lowCardinality() != column_from_block.type->lowCardinality())
-            {
-                JoinCommon::changeLowCardinalityInplace(column_from_block);
-            }
-        }
-
+#ifndef NDEBUG
         /// Like assertBlocksHaveEqualStructure but doesn't check low cardinality
         assertBlockEqualsStructureUpToLowCard(sample_block, block);
+#else
+        UNUSED(assertBlockEqualsStructureUpToLowCard);
+#endif
 
         if (is_join_get)
         {
             /// If it's joinGetOrNull, we need to wrap not-nullable columns in StorageJoin.
             for (size_t j = 0, size = right_indexes.size(); j < size; ++j)
             {
-                const auto & column_from_block = block.getByPosition(right_indexes[j]);
+                auto column_from_block = block.getByPosition(right_indexes[j]);
+                if (type_name[j].type->lowCardinality() != column_from_block.type->lowCardinality())
+                {
+                    JoinCommon::changeLowCardinalityInplace(column_from_block);
+                }
+
                 if (auto * nullable_col = typeid_cast<ColumnNullable *>(columns[j].get());
                     nullable_col && !column_from_block.column->isNullable())
                     nullable_col->insertFromNotNullable(*column_from_block.column, row_num);
@@ -1020,7 +1020,11 @@ public:
         {
             for (size_t j = 0, size = right_indexes.size(); j < size; ++j)
             {
-                const auto & column_from_block = block.getByPosition(right_indexes[j]);
+                auto column_from_block = block.getByPosition(right_indexes[j]);
+                if (type_name[j].type->lowCardinality() != column_from_block.type->lowCardinality())
+                {
+                    JoinCommon::changeLowCardinalityInplace(column_from_block);
+                }
                 columns[j]->insertFrom(*column_from_block.column, row_num);
             }
         }
