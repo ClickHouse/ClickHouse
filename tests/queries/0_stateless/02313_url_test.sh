fc9f8934dd139585f8899a3fcb6aaac1a042cca5
Kruglov Pavel <48961922+Avogar@users.noreply.github.com>
2022-05-31 10:02:42 +0200

    Update 02313_url_test.sh

--- tests/queries/0_stateless/02313_url_test.sh
+++ tests/queries/0_stateless/02313_url_test.sh
@@ -4,7 +4,7 @@ CUR_DIR=$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)
 # shellcheck source=../shell_config.sh
 . "$CUR_DIR"/../shell_config.sh
 
-for i in $(seq 1 200);
+for i in $(seq 1 1000);
 do
     $CLICKHOUSE_CLIENT -q "SELECT * FROM url('https://datasets.clickhouse.com/github_events_v2.native.xz') LIMIT 1" &
 done
