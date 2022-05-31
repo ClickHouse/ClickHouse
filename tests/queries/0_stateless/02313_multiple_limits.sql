70b91510fcbaa8ff86f9fa42dae49b215b5e71f1
Nikolai Kochetov <nik-kochetov@yandex-team.ru>
2022-05-31 19:40:50 +0000

    Update test.

--- tests/queries/0_stateless/02313_multiple_limits.sql
+++ tests/queries/0_stateless/02313_multiple_limits.sql
@@ -23,3 +23,84 @@ FROM
     )
     SETTINGS max_rows_to_read = 10, read_overflow_mode = 'break', max_block_size = 2
 );
+
+
+SELECT count()
+FROM
+(
+    SELECT x
+    FROM
+    (
+        SELECT zero AS x
+        FROM system.zeros
+        SETTINGS max_block_size = 2, max_rows_to_read = 10, read_overflow_mode = 'break'
+    )
+    UNION ALL
+        SELECT x
+    FROM
+    (
+        SELECT zero + 1 AS x
+        FROM system.zeros
+        SETTINGS max_block_size = 2, max_rows_to_read = 20, read_overflow_mode = 'break'
+    )
+);
+
+SELECT sum(x) >= 10
+FROM
+(
+    SELECT x
+    FROM
+    (
+        SELECT zero AS x
+        FROM system.zeros
+        SETTINGS max_block_size = 2, max_rows_to_read = 10, read_overflow_mode = 'break'
+    )
+    UNION ALL
+        SELECT x
+    FROM
+    (
+        SELECT zero + 1 AS x
+        FROM system.zeros
+        SETTINGS max_block_size = 2, max_rows_to_read = 20, read_overflow_mode = 'break'
+    )
+);
+
+SELECT count()
+FROM
+(
+    SELECT x
+    FROM
+    (
+        SELECT zero AS x
+        FROM system.zeros
+        SETTINGS max_block_size = 2, max_rows_to_read = 20, read_overflow_mode = 'break'
+    )
+    UNION ALL
+        SELECT x
+    FROM
+    (
+        SELECT zero + 1 AS x
+        FROM system.zeros
+        SETTINGS max_block_size = 2, max_rows_to_read = 10, read_overflow_mode = 'break'
+    )
+);
+
+SELECT sum(x) <= 10
+FROM
+(
+    SELECT x
+    FROM
+    (
+        SELECT zero AS x
+        FROM system.zeros
+        SETTINGS max_block_size = 2, max_rows_to_read = 20, read_overflow_mode = 'break'
+    )
+    UNION ALL
+        SELECT x
+    FROM
+    (
+        SELECT zero + 1 AS x
+        FROM system.zeros
+        SETTINGS max_block_size = 2, max_rows_to_read = 10, read_overflow_mode = 'break'
+    )
+);
