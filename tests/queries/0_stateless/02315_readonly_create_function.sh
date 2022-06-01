4413f0c7c12cd7a80c75c28e28ed2dac964e6686
Maksim Kita <kitaetoya@gmail.com>
2022-06-01 14:52:01 +0200

    Fixed tests

--- tests/queries/0_stateless/02315_readonly_create_function.sh
+++ tests/queries/0_stateless/02315_readonly_create_function.sh
@@ -4,4 +4,5 @@ CURDIR=$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)
 # shellcheck source=../shell_config.sh
 . "$CURDIR"/../shell_config.sh
 
-$CLICKHOUSE_CLIENT --readonly 1 --query "CREATE FUNCTION test_function AS (x) -> x + 1;" 2>&1 | grep -c -F 'Code: 164'
+$CLICKHOUSE_CLIENT --readonly 1 --query "CREATE FUNCTION test_function AS (x) -> x + 1;" 2>&1 | grep -q "Code: 164"
+echo $?;
