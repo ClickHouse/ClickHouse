b4cc8ce22b6f89c1ed5ddba6addf77e7a9d46d1d
Maksim Kita <kitaetoya@gmail.com>
2022-06-01 14:29:22 +0200

    Fixed style check

--- tests/integration/test_executable_user_defined_function/user_scripts/input_parameter.py
+++ tests/integration/test_executable_user_defined_function/user_scripts/input_parameter.py
@@ -2,7 +2,7 @@
 
 import sys
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     for line in sys.stdin:
-        print("Parameter " + str(sys.argv[1]) + " key " + str(line), end='')
+        print("Parameter " + str(sys.argv[1]) + " key " + str(line), end="")
         sys.stdout.flush()
