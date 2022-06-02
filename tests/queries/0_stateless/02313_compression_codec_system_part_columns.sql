e633315bd3c39f414910acd15b1fcb8bf75cfc29
xlwh <1079039435@qq.com>
2022-06-02 15:50:58 +0800

    Fix style check

--- tests/queries/0_stateless/02313_compression_codec_system_part_columns.sql
+++ tests/queries/0_stateless/02313_compression_codec_system_part_columns.sql
@@ -13,5 +13,5 @@ ORDER BY tuple();
 INSERT INTO test_2313(id,data,ddd,somenum,somestr) VALUES (101,'Hello,ClickHouse!',now(),1.0,'a');
 
 OPTIMIZE TABLE test_2313;
-SELECT column,compression_codec FROM system.parts_columns WHERE table='test_2313';
+SELECT column,compression_codec FROM system.parts_columns WHERE database = currentDatabase() AND table='test_2313';
 DROP TABLE test_2313;
