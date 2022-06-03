ac866842ec6469ea632e49ed7ded84a6c94970c0
Vxider <lb@vxider.com>
2022-06-03 10:37:43 +0000

    add timezone in test

--- tests/queries/0_stateless/01086_window_view_cleanup.sh
+++ tests/queries/0_stateless/01086_window_view_cleanup.sh
@@ -16,18 +16,18 @@ CREATE TABLE test_01086.dst(count UInt64, market Int32, w_end DateTime) Engine=M
 CREATE TABLE test_01086.mt(a Int32, market Int32, timestamp DateTime) ENGINE=MergeTree ORDER BY tuple();
 CREATE WINDOW VIEW test_01086.wv TO test_01086.dst WATERMARK=ASCENDING AS SELECT count(a) AS count, market, tumbleEnd(wid) AS w_end FROM test_01086.mt GROUP BY tumble(timestamp, INTERVAL '5' SECOND, 'US/Samoa') AS wid, market;
 
-INSERT INTO test_01086.mt VALUES (1, 1, '1990/01/01 12:00:00');
-INSERT INTO test_01086.mt VALUES (1, 2, '1990/01/01 12:00:01');
-INSERT INTO test_01086.mt VALUES (1, 3, '1990/01/01 12:00:02');
-INSERT INTO test_01086.mt VALUES (1, 4, '1990/01/01 12:00:05');
-INSERT INTO test_01086.mt VALUES (1, 5, '1990/01/01 12:00:06');
+INSERT INTO test_01086.mt VALUES (1, 1, toDateTime('1990/01/01 12:00:00', 'US/Samoa'));
+INSERT INTO test_01086.mt VALUES (1, 2, toDateTime('1990/01/01 12:00:01', 'US/Samoa'));
+INSERT INTO test_01086.mt VALUES (1, 3, toDateTime('1990/01/01 12:00:02', 'US/Samoa'));
+INSERT INTO test_01086.mt VALUES (1, 4, toDateTime('1990/01/01 12:00:05', 'US/Samoa'));
+INSERT INTO test_01086.mt VALUES (1, 5, toDateTime('1990/01/01 12:00:06', 'US/Samoa'));
 EOF
 
 while true; do
 	$CLICKHOUSE_CLIENT --query="SELECT count(*) FROM test_01086.\`.inner.wv\`" | grep -q "5" && break || sleep .5 ||:
 done
 
-$CLICKHOUSE_CLIENT --query="INSERT INTO test_01086.mt VALUES (1, 6, '1990/01/01 12:00:11');"
+$CLICKHOUSE_CLIENT --query="INSERT INTO test_01086.mt VALUES (1, 6, toDateTime('1990/01/01 12:00:11', 'US/Samoa'));"
 
 while true; do
 	$CLICKHOUSE_CLIENT --query="SELECT count(*) FROM test_01086.\`.inner.wv\`" | grep -q "3" && break || sleep .5 ||:
