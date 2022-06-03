b06c86c51533f8b841d45714bd6b11bfaa5cd297
vvd170501 <36827317+vvd170501@users.noreply.github.com>
2022-06-03 18:53:06 +0300

    Refactor UDF code generation

--- src/Processors/Sources/InterpretedLangSource.cpp
+++ src/Processors/Sources/InterpretedLangSource.cpp
@@ -12,6 +12,176 @@ namespace ErrorCodes
     extern const int UNSUPPORTED_METHOD;
 }
 
+namespace
+{
+
+class CodeGenerator
+{
+public:
+    CodeGenerator(std::stringstream& output_, const String& function_name_, const Strings& args_, const Strings& func_lines_) :
+        output(output_),
+        function_name(function_name_),
+        args(args_),
+        func_lines(func_lines_)
+    {
+        std::stringstream arg_stream;
+        if (args.empty())
+        {
+            throw Exception(ErrorCodes::UNSUPPORTED_METHOD, "Functions without arguments are not supported");
+        }
+        arg_stream << args[0];
+        for (size_t i = 1; i < args.size(); ++i) {
+            arg_stream << ", " << args[i];
+        }
+        arg_str = arg_stream.str();
+    }
+
+    virtual void generateImports() = 0;
+    virtual void generateFunction() = 0;
+    virtual void generateMain() = 0;
+    virtual ~CodeGenerator() = default;
+
+protected:
+    std::stringstream& output;
+    const String& function_name;
+    const Strings& args;
+    const Strings& func_lines;
+    String arg_str;
+};
+
+class PythonCodeGenerator : public CodeGenerator
+{
+    using CodeGenerator::CodeGenerator;
+
+public:
+    void generateImports() override
+    {
+        output << "import sys\n"
+                  "import os\n"
+                  "import numpy as np\n"
+                  "import scipy\n";  /// available by default  !! config imports
+    }
+
+    void generateFunction() override
+    {
+        output << "def " << function_name << "(" << arg_str << "):\n";
+        for (const String& line : func_lines) {
+            output << "    " << line << '\n';
+        }
+    }
+
+    void generateMain() override
+    {
+        // !! use more efficient format
+        // !! make array size configurable (split input into chunks)
+        output << "rows = []\n"
+                  "for line in sys.stdin:\n"
+                  "    rows.append([int(x) for x in line.split('\\t')])\n"  // !! convert input to array based on arg types
+                  "input_data = np.array(rows)\n";
+
+        output << "result_column = " << function_name << "(input_data[:,0]";
+        for (size_t i = 1; i < args.size(); ++i) {
+            output << ", input_data[:," << i << "]";
+        }
+        output << ")\n";
+
+        output << "assert result_column.ndim == 1\n"
+                  "for value in result_column:\n"
+                  "    sys.stdout.write(str(value)+'\\n')\n"
+                  "os.unlink(sys.argv[0])\n";
+    }
+};
+
+class JuliaCodeGenerator : public CodeGenerator
+{
+    using CodeGenerator::CodeGenerator;
+
+public:
+    void generateImports() override
+    {
+    }
+
+    void generateFunction() override
+    {
+        output << "function " << function_name << "(" << arg_str << ")\n";
+        for (const String& line : func_lines) {
+            output << "    " << line << '\n';  /// not necessary, just easier debugging
+        }
+        output << "end\n";
+    }
+
+    void generateMain() override
+    {
+        for (const String& arg : args) {
+            output << arg << " = Vector{Int}()\n";  /// init columns
+        }
+
+        output << "for line in eachline(stdin)\n"
+                  "    row = parse.(Int, split(line, '\\t'))\n";  // !! use real types
+        for (size_t i = 0; i < args.size(); ++i) {
+            output << "    push!(" << args[i] << ", row[" << i + 1 << "])\n";
+        }
+        output << "end\n";
+
+        output << "for value in " << function_name << "(" << arg_str << ")\n"
+                  "    println(value)\n"
+                  "end\n"
+                  "rm(PROGRAM_FILE)\n";
+    }
+};
+
+class RCodeGenerator : public CodeGenerator
+{
+public:
+    RCodeGenerator(std::stringstream& output_, const String& function_name_, const Strings& args_, const Strings& func_lines_,
+                   const String& script_path_) : CodeGenerator(output_, function_name_, args_, func_lines_), script_path(script_path_)
+    {
+    }
+
+    void generateImports() override
+    {
+    }
+
+    void generateFunction() override
+    {
+        output << function_name << " <- function(" << arg_str << ") {\n";
+        for (const String& line : func_lines) {
+            output << "    " << line << '\n';  /// not necessary, just easier debugging
+        }
+        output << "}\n";
+    }
+
+    void generateMain() override
+    {
+        for (const String& arg : args) {
+            output << arg << " <- NULL\n";  /// init columns
+        }
+        output << "input <- file(\"stdin\")\n"
+                  "open(input, blocking=TRUE)\n"
+                  "i <- 1\n";
+
+        output << "while (length(line <- readLines(input, n=1)) > 0) {\n"
+                  "    values <- strtoi(unlist(strsplit(line, \"\\t\")))\n";  // !! use real types
+        for (size_t arg_num = 0; arg_num < args.size(); ++arg_num) {
+            /// Idk why assignment by index works (couldn't find this anywhere in docs),
+            /// but it's much faster than append
+            output << "    " << args[arg_num] << "[i] <- values[" << arg_num + 1 << "]\n";
+        }
+        output << "    i <- i + 1\n"
+                  "}\n";
+
+        output << "for (value in " << function_name << "(" << arg_str << ")) {\n"
+                  "    write(value, stdout())\n"
+                  "}\n"
+                  "unlink(\"" << script_path << "\")\n";
+    }
+
+private:
+    const String& script_path;
+};
+
+}
+
 InterpretedLangSourceCoordinator::InterpretedLangSourceCoordinator(const Configuration & configuration_)
     : configuration(configuration_), cmd_coordinator({ .format = "TabSeparated" })
 {
@@ -23,133 +193,51 @@ Pipe InterpretedLangSourceCoordinator::createPipe(const String& function_name,
                                                   Block sample_block,
                                                   ContextPtr context)
 {
-    String command, code, arg_str;
     Strings args;
     {
         const auto & columns = input_pipes.back().getHeader().getColumnsWithTypeAndName();
-        std::stringstream arg_stream;
-        // !! implement for functions with 0 arg_str
+        // !! implement for functions with 0 args
         if (columns.empty())
         {
             throw Exception(ErrorCodes::UNSUPPORTED_METHOD, "Functions without arguments are not supported");
         }
-        arg_stream << columns[0].name;
         args.push_back(columns[0].name);
         for (size_t i = 1; i < columns.size(); ++i) {
-            arg_stream << ", " << columns[i].name;
             args.push_back(columns[i].name);
         }
-        arg_str = arg_stream.str();
     }
+    std::vector<String> func_lines;
+    boost::split(func_lines, function_body, [](char c) { return c == '\n'; });
 
     TemporaryFile script;
     script.keep();
+    String command;
+    std::stringstream code_str;
 
     /// TODO Cache generated code?
+    std::optional<std::variant<PythonCodeGenerator, JuliaCodeGenerator, RCodeGenerator>> gen;
     if (configuration.interpreter_type == "python") {
         /// Test: CREATE FUNCTION foo_py(x, y) 'return x + y' USING python
         command = "/usr/bin/python3";  // !! configure
-        std::stringstream code_str;
-
-        code_str << "import sys\n"
-                    "import os\n"
-                    "import numpy as np\n"
-                    "import scipy\n";  /// available by default  !! config imports
-
-        code_str << "def " << function_name << "(" << arg_str << "):\n";
-        std::vector<String> func_lines;
-        boost::split(func_lines, function_body, [](char c) { return c == '\n'; });
-        for (const String& line : func_lines) {
-            code_str << "    " << line << '\n';
-        }
-
-        // !! use more efficient format
-        // !! make array size configurable (split input into chunks)
-        code_str << "rows = []\n"
-                    "for line in sys.stdin:\n"
-                    "    rows.append([int(x) for x in line.split('\\t')])\n"  // !! convert input to array based on arg types
-                    "input_data = np.array(rows)\n";
-
-        code_str << "result_column = " << function_name << "(input_data[:,0]";
-        for (size_t i = 1; i < args.size(); ++i) {
-            code_str << ", input_data[:," << i << "]";
-        }
-        code_str << ")\n";
-
-        code_str << "assert result_column.ndim == 1\n"
-                    "for value in result_column:\n"
-                    "    sys.stdout.write(str(value)+'\\n')\n"
-                    "os.unlink(sys.argv[0])\n";
-        code = code_str.str();
+        gen.emplace(std::in_place_type<PythonCodeGenerator>, code_str, function_name, args, func_lines);
     } else if (configuration.interpreter_type == "julia") {
         /// Test: CREATE FUNCTION foo_jl(x, y) 'return x .+ y' USING julia
         command = "/usr/bin/julia";
-        std::stringstream code_str;
-
-        code_str << "function " << function_name << "(" << arg_str << ")\n";
-        std::vector<String> func_lines;
-        boost::split(func_lines, function_body, [](char c) { return c == '\n'; });
-        for (const String& line : func_lines) {
-            code_str << "    " << line << '\n';  /// not necessary, just easier debugging
-        }
-        code_str << "end\n";
-
-        for (const String& arg : args) {
-            code_str << arg << " = Vector{Int}()\n";
-        }
-
-        code_str << "for line in eachline(stdin)\n"
-                    "    row = parse.(Int, split(line, '\\t'))\n";  // !! use real types
-        for (size_t i = 0; i < args.size(); ++i) {
-            code_str << "    push!(" << args[i] << ", row[" << i + 1 << "])\n";
-        }
-        code_str << "end\n";
-
-        code_str << "for value in " << function_name << "(" << arg_str << ")\n"
-                    "    println(value)\n"
-                    "end\n"
-                    "rm(PROGRAM_FILE)\n";
-        code = code_str.str();
+        gen.emplace(std::in_place_type<JuliaCodeGenerator>, code_str, function_name, args, func_lines);
     } else if (configuration.interpreter_type == "R") {
         /// Test: CREATE FUNCTION foo_r(x, y) 'return(x + y)' USING R
         /// NOTE integer overflows produce NAs, and output cannot be parsed
         command = "/usr/bin/Rscript";
-        std::stringstream code_str;
-
-        code_str << function_name << " <- function(" << arg_str << ") {\n";
-        std::vector<String> func_lines;
-        boost::split(func_lines, function_body, [](char c) { return c == '\n'; });
-        for (const String& line : func_lines) {
-            code_str << "    " << line << '\n';  /// not necessary, just easier debugging
-        }
-        code_str << "}\n";
-
-        for (const String& arg : args) {
-            code_str << arg << " <- NULL\n";  /// init columns
-        }
-        code_str << "input <- file(\"stdin\")\n"
-                    "open(input, blocking=TRUE)\n"
-                    "i <- 1\n";
-
-        code_str << "while (length(line <- readLines(input, n=1)) > 0) {\n"
-                    "    values <- strtoi(unlist(strsplit(line, \"\\t\")))\n";  // !! use real types
-        for (size_t arg_num = 0; arg_num < args.size(); ++arg_num) {
-            /// Idk why assignment by index works (couldn't find this anywhere in docs),
-            /// but it's much faster than append
-            code_str << "    " << args[arg_num] << "[i] <- values[" << arg_num + 1 << "]\n";
-        }
-        code_str << "    i <- i + 1\n"
-                    "}\n";
-
-        code_str << "for (value in " << function_name << "(" << arg_str << ")) {\n"
-                "    write(value, stdout())\n"
-                "}\n"
-                "unlink(\"" << script.path() << "\")\n";
-        code = code_str.str();
-
+        gen.emplace(std::in_place_type<RCodeGenerator>, code_str, function_name, args, func_lines, script.path());
     } else {
         throw Exception(ErrorCodes::UNSUPPORTED_METHOD, "Unknown interpreter type: '{}'", configuration.interpreter_type);
     }
+    std::visit([](CodeGenerator& codegen) {
+       codegen.generateImports();  /// TODO make configurable
+       codegen.generateFunction();  /// final
+       codegen.generateMain();  /// TODO optimize, use real types for args and return value, split input
+    }, *gen);
+    String code = code_str.str();
     WriteBufferFromFile out(script.path(), code.size());
     writeString(code, out);
     out.next();
