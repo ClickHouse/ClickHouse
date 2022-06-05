5475f62363018e4fd8d09cfe9864f935907347d1
zvonand <azvonov@altinity.com>
2022-06-05 13:06:48 +0300

    32 to 64

--- src/Functions/timeSlots.cpp
+++ src/Functions/timeSlots.cpp
@@ -23,9 +23,9 @@ namespace ErrorCodes
 namespace
 {
 
-/** timeSlots(StartTime, Duration)
+/** timeSlots(StartTime, Duration[, Size=1800])
   * - for the time interval beginning at `StartTime` and continuing `Duration` seconds,
-  *   returns an array of time points, consisting of rounding down to half an hour (default; or another value) of points from this interval.
+  *   returns an array of time points, consisting of rounding down to Size (1800 seconds by default) of points from this interval.
   *  For example, timeSlots(toDateTime('2012-01-01 12:20:00'), 600) = [toDateTime('2012-01-01 12:00:00'), toDateTime('2012-01-01 12:30:00')].
   *  This is necessary to search for hits that are part of the corresponding visit.
   *
@@ -37,8 +37,8 @@ template <typename DurationType>
 struct TimeSlotsImpl
 {
     static void vectorVector(
-        const PaddedPODArray<UInt32> & starts, const PaddedPODArray<DurationType> & durations, UInt32 time_slot_size,
-        PaddedPODArray<UInt32> & result_values, ColumnArray::Offsets & result_offsets)
+        const PaddedPODArray<Int64> & starts, const PaddedPODArray<DurationType> & durations, UInt64 time_slot_size,
+        PaddedPODArray<Int64> & result_values, ColumnArray::Offsets & result_offsets)
     {
         size_t size = starts.size();
 
@@ -48,7 +48,7 @@ struct TimeSlotsImpl
         ColumnArray::Offset current_offset = 0;
         for (size_t i = 0; i < size; ++i)
         {
-            for (UInt32 value = starts[i] / time_slot_size, end = (starts[i] + durations[i]) / time_slot_size; value <= end; ++value)
+            for (Int64 value = starts[i] / time_slot_size, end = (starts[i] + durations[i]) / time_slot_size; value <= end; ++value)
             {
                 result_values.push_back(value * time_slot_size);
                 ++current_offset;
@@ -59,8 +59,8 @@ struct TimeSlotsImpl
     }
 
     static void vectorConstant(
-        const PaddedPODArray<UInt32> & starts, DurationType duration, UInt32 time_slot_size,
-        PaddedPODArray<UInt32> & result_values, ColumnArray::Offsets & result_offsets)
+        const PaddedPODArray<Int64> & starts, DurationType duration, UInt64 time_slot_size,
+        PaddedPODArray<Int64> & result_values, ColumnArray::Offsets & result_offsets)
     {
         size_t size = starts.size();
 
@@ -70,7 +70,7 @@ struct TimeSlotsImpl
         ColumnArray::Offset current_offset = 0;
         for (size_t i = 0; i < size; ++i)
         {
-            for (UInt32 value = starts[i] / time_slot_size, end = (starts[i] + duration) / time_slot_size; value <= end; ++value)
+            for (Int64 value = starts[i] / time_slot_size, end = (starts[i] + duration) / time_slot_size; value <= end; ++value)
             {
                 result_values.push_back(value * time_slot_size);
                 ++current_offset;
@@ -81,8 +81,8 @@ struct TimeSlotsImpl
     }
 
     static void constantVector(
-        UInt32 start, const PaddedPODArray<DurationType> & durations, UInt32 time_slot_size,
-        PaddedPODArray<UInt32> & result_values, ColumnArray::Offsets & result_offsets)
+        Int64 start, const PaddedPODArray<DurationType> & durations, UInt64 time_slot_size,
+        PaddedPODArray<Int64> & result_values, ColumnArray::Offsets & result_offsets)
     {
         size_t size = durations.size();
 
@@ -92,7 +92,7 @@ struct TimeSlotsImpl
         ColumnArray::Offset current_offset = 0;
         for (size_t i = 0; i < size; ++i)
         {
-            for (UInt32 value = start / time_slot_size, end = (start + durations[i]) / time_slot_size; value <= end; ++value)
+            for (Int64 value = start / time_slot_size, end = (start + durations[i]) / time_slot_size; value <= end; ++value)
             {
                 result_values.push_back(value * time_slot_size);
                 ++current_offset;
@@ -108,7 +108,7 @@ class FunctionTimeSlots : public IFunction
 {
 public:
     static constexpr auto name = "timeSlots";
-    static constexpr UInt32 TIME_SLOT_SIZE = 1800;
+    static constexpr UInt64 TIME_SLOT_SIZE = 1800;
     static FunctionPtr create(ContextPtr) { return std::make_shared<FunctionTimeSlots>(); }
 
     String getName() const override
@@ -129,33 +129,44 @@ public:
                             + toString(arguments.size()) + ", should be 2 or 3",
                             ErrorCodes::NUMBER_OF_ARGUMENTS_DOESNT_MATCH);
 
-        if (!WhichDataType(arguments[0].type).isDateTime())
-            throw Exception("Illegal type " + arguments[0].type->getName() + " of first argument of function " + getName() + ". Must be DateTime.",
-                ErrorCodes::ILLEGAL_TYPE_OF_ARGUMENT);
+        if (!WhichDataType(arguments[0].type).isDateTime() && !WhichDataType(arguments[0].type).isDateTime64())
+            throw Exception("Illegal type " + arguments[0].type->getName() + " of first argument of function " + getName()
+                            + ". Must be DateTime or DateTime64.", ErrorCodes::ILLEGAL_TYPE_OF_ARGUMENT);
 
-        if (!WhichDataType(arguments[1].type).isUInt32())
-            throw Exception("Illegal type " + arguments[1].type->getName() + " of second argument of function " + getName() + ". Must be UInt32.",
+        if (!WhichDataType(arguments[1].type).isNativeUInt())
+            throw Exception("Illegal type " + arguments[1].type->getName() + " of second argument of function " + getName() + ". Must be UInt64.",
                 ErrorCodes::ILLEGAL_TYPE_OF_ARGUMENT);
 
         if (arguments.size() == 3 && !WhichDataType(arguments[2].type).isNativeUInt())
-            throw Exception("Illegal type " + arguments[2].type->getName() + " of third argument of function " + getName() + ". Must be UInt32.",
+            throw Exception("Illegal type " + arguments[2].type->getName() + " of third argument of function " + getName() + ". Must be UInt64.",
                 ErrorCodes::ILLEGAL_TYPE_OF_ARGUMENT);
 
         /// If time zone is specified for source data type, attach it to the resulting type.
         /// Note that there is no explicit time zone argument for this function (we specify 2 as an argument number with explicit time zone).
-        return std::make_shared<DataTypeArray>(std::make_shared<DataTypeDateTime>(extractTimeZoneNameFromFunctionArguments(arguments, 3, 0)));
+        if (WhichDataType(arguments[0].type).isDateTime())
+        {
+            return std::make_shared<DataTypeArray>(std::make_shared<DataTypeDateTime>(extractTimeZoneNameFromFunctionArguments(arguments, 3, 0)));
+
+        }
+        else
+        {
+            auto dt64_scale = assert_cast<const DataTypeDateTime64 &>(*arguments[0].type).getScale();
+            return std::make_shared<DataTypeArray>(
+                std::make_shared<DataTypeDateTime64>(dt64_scale, extractTimeZoneNameFromFunctionArguments(arguments, 3, 0)));
+        }
+
     }
 
     ColumnPtr executeImpl(const ColumnsWithTypeAndName & arguments, const DataTypePtr &, size_t) const override
     {
-        const auto * starts = checkAndGetColumn<ColumnUInt32>(arguments[0].column.get());
-        const auto * const_starts = checkAndGetColumnConst<ColumnUInt32>(arguments[0].column.get());
+        const auto * starts = checkAndGetColumn<ColumnInt64>(arguments[0].column.get());
+        const auto * const_starts = checkAndGetColumnConst<ColumnInt64>(arguments[0].column.get());
 
-        const auto * durations = checkAndGetColumn<ColumnUInt32>(arguments[1].column.get());
-        const auto * const_durations = checkAndGetColumnConst<ColumnUInt32>(arguments[1].column.get());
+        const auto * durations = checkAndGetColumn<ColumnInt64>(arguments[1].column.get());
+        const auto * const_durations = checkAndGetColumnConst<ColumnInt64>(arguments[1].column.get());
 
-        auto res = ColumnArray::create(ColumnUInt32::create());
-        ColumnUInt32::Container & res_values = typeid_cast<ColumnUInt32 &>(res->getData()).getData();
+        auto res = ColumnArray::create(ColumnInt64::create());
+        ColumnInt64::Container & res_values = typeid_cast<ColumnInt64 &>(res->getData()).getData();
 
         auto time_slot_size = TIME_SLOT_SIZE;
 
@@ -163,33 +174,44 @@ public:
         {
             const auto * time_slot_column = checkAndGetColumn<ColumnConst>(arguments[2].column.get());
             if (!time_slot_column)
-                throw Exception("Third argument for function " + getName() + " must be constant UInt32", ErrorCodes::ILLEGAL_TYPE_OF_ARGUMENT);
+                throw Exception("Third argument for function " + getName() + " must be constant UInt64", ErrorCodes::ILLEGAL_TYPE_OF_ARGUMENT);
 
-            if (time_slot_size = time_slot_column->getValue<UInt32>(); time_slot_size == 0)
+            if (time_slot_size = time_slot_column->getValue<UInt64>(); time_slot_size == 0)
                 throw Exception("Third argument for function " + getName() + " must be greater than zero", ErrorCodes::ILLEGAL_COLUMN);
         }
 
         if (starts && durations)
         {
-            TimeSlotsImpl<UInt32>::vectorVector(starts->getData(), durations->getData(), time_slot_size, res_values, res->getOffsets());
+            TimeSlotsImpl<Int64>::vectorVector(starts->getData(), durations->getData(), time_slot_size, res_values, res->getOffsets());
             return res;
         }
         else if (starts && const_durations)
         {
-            TimeSlotsImpl<UInt32>::vectorConstant(starts->getData(), const_durations->getValue<UInt32>(), time_slot_size, res_values, res->getOffsets());
+            TimeSlotsImpl<Int64>::vectorConstant(starts->getData(), const_durations->getValue<Int64>(), time_slot_size, res_values, res->getOffsets());
             return res;
         }
         else if (const_starts && durations)
         {
-            TimeSlotsImpl<UInt32>::constantVector(const_starts->getValue<UInt32>(), durations->getData(), time_slot_size, res_values, res->getOffsets());
+            TimeSlotsImpl<Int64>::constantVector(const_starts->getValue<Int64>(), durations->getData(), time_slot_size, res_values, res->getOffsets());
             return res;
         }
         else
+            if (arguments.size() == 3)
+        {
             throw Exception("Illegal columns " + arguments[0].column->getName()
                     + ", " + arguments[1].column->getName()
                     + ", " + arguments[2].column->getName()
                     + " of arguments of function " + getName(),
                 ErrorCodes::ILLEGAL_COLUMN);
+        }
+        else
+        {
+            throw Exception("Illegal columns " + arguments[0].column->getName()
+                                + ", " + arguments[1].column->getName()
+                                + " of arguments of function " + getName(),
+                            ErrorCodes::ILLEGAL_COLUMN);
+        }
+
     }
 };
 
