d76bde4143bd1a1ab4a7be09caa5d49b547103ef
avogar <pav.cruglov@yandex.ru>
2022-06-07 11:23:59 +0000

    Fix

--- tests/queries/0_stateless/02317_functions_with_nothing.sql
+++ tests/queries/0_stateless/02317_functions_with_nothing.sql
@@ -3,3 +3,4 @@ SELECT JSONHas(arrayJoin([]));  -- {serverError ILLEGAL_TYPE_OF_ARGUMENT}
 SELECT isValidJSON(arrayJoin([]));  -- {serverError ILLEGAL_TYPE_OF_ARGUMENT}
 SELECT concat(arrayJoin([]), arrayJoin([NULL, ''])); -- {serverError ILLEGAL_TYPE_OF_ARGUMENT}
 SELECT plus(arrayJoin([]), arrayJoin([NULL, 1])); -- {serverError ILLEGAL_TYPE_OF_ARGUMENT}
+SELECT [concat(NULL, arrayJoin([]))];
