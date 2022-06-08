0951627b24fbd01bb1fdbbccd17157a4fd16ec54
Yong Wang <yong.wang@ibm.com>
2022-06-08 10:14:03 -0700

    Kusto-phase1: Add Support to Kusto Query Language
    This is the initial implement of Kusto Query Language.
    
    in this commit, we support the following features as MVP :
    
    Tabular expression statements
    Limit returned results
    Select Column (basic project)
    sort, order
    Perform string equality operations
    Filter using a list of elements
    Filter using common string operations
    Some string  operators
    Aggregate by columns
    Base aggregate functions
    only support avg, count ,min, max, sum
    Aggregate by time intervals

---
+++ src/Parsers/Kusto/ParserKQLSort.cpp
@@ -0,0 +1,71 @@
+#include <Parsers/ASTLiteral.h>
+#include <Parsers/IParserBase.h>
+#include <Parsers/ExpressionListParsers.h>
+#include <Parsers/ASTOrderByElement.h>
+#include <Parsers/Kusto/ParserKQLQuery.h>
+#include <Parsers/Kusto/ParserKQLSort.h>
+
+namespace DB
+{
+
+bool ParserKQLSort :: parseImpl(Pos & pos, ASTPtr & node, Expected & expected)
+{
+    if (op_pos.empty())
+        return true;
+
+    auto begin = pos;
+    bool has_dir = false;
+    std::vector <bool> has_directions;
+    ParserOrderByExpressionList order_list;
+    ASTPtr order_expression_list;
+
+    ParserKeyword by("by");
+
+    pos = op_pos.back();  // sort only affected by last one
+
+    if (!by.ignore(pos, expected))
+        return false;
+
+    if (!order_list.parse(pos,order_expression_list,expected))
+        return false;
+    if (!pos->isEnd() && pos->type != TokenType::PipeMark && pos->type != TokenType::Semicolon)
+        return false;
+
+    pos = op_pos.back();
+    while (!pos->isEnd() && pos->type != TokenType::PipeMark)
+    {
+        String tmp(pos->begin,pos->end);
+        if (tmp == "desc" or tmp == "asc")
+            has_dir = true;
+
+        if (pos->type == TokenType::Comma)
+        {
+            has_directions.push_back(has_dir);
+            has_dir = false;
+        }
+
+        ++pos;
+    }
+    has_directions.push_back(has_dir);
+
+    for (unsigned long i = 0; i < order_expression_list->children.size(); ++i)
+    {
+        if (!has_directions[i])
+        {
+            auto order_expr =  order_expression_list->children[i]->as<ASTOrderByElement>();
+            order_expr->direction = -1; // default desc
+            if (!order_expr->nulls_direction_was_explicitly_specified)
+                order_expr->nulls_direction = -1;
+            else
+                order_expr->nulls_direction = order_expr->nulls_direction == 1 ? -1 : 1;
+
+        }
+    }
+
+    node = order_expression_list;
+
+    pos =begin;
+    return true;
+}
+
+}
---
+++ src/Parsers/Kusto/ParserKQLSummarize.h
@@ -0,0 +1,19 @@
+#pragma once
+
+#include <Parsers/IParserBase.h>
+#include <Parsers/Kusto/ParserKQLQuery.h>
+
+namespace DB
+{
+
+class ParserKQLSummarize : public ParserKQLBase
+{
+public:
+    ASTPtr group_expression_list;
+protected:
+    const char * getName() const override { return "KQL summarize"; }
+    bool parseImpl(Pos & pos, ASTPtr & node, Expected & expected) override;
+
+};
+
+}
