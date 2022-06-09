bb9f784f16502c93cd270a570e648ec4609f5225
root <mallik.hassan@ibm.com>
2022-06-09 11:18:49 -0700

    added single unit test case for summarize bin()

--- src/Parsers/tests/gtest_Parser.cpp
+++ src/Parsers/tests/gtest_Parser.cpp
@@ -431,6 +431,10 @@ INSTANTIATE_TEST_SUITE_P(ParserKQLQuery, ParserTest,
             "SELECT\n    Occupation,\n    max(Age)\nFROM Customers\nGROUP BY Occupation"
         },
         {
+            "Customers |summarize count() by bin(Age, 10)",
+            "SELECT\n    toInt32(Age / 10) * 10 AS bin_int,\n    count(Age)\nFROM Customers\nGROUP BY bin_int"
+        }
+        {
             "Customers | where FirstName contains 'pet'",
             "SELECT *\nFROM Customers\nWHERE FirstName ILIKE '%pet%'"
         },
