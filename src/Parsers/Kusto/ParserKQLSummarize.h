acd5b66688dbda13beb660bab08410b86a2ad126
root <mallik.hassan@ibm.com>
2022-06-09 11:04:20 -0700

    Kusto summarize init

--- src/Parsers/Kusto/ParserKQLSummarize.h
+++ src/Parsers/Kusto/ParserKQLSummarize.h
@@ -5,15 +5,16 @@
 
 namespace DB
 {
-
 class ParserKQLSummarize : public ParserKQLBase
 {
 public:
     ASTPtr group_expression_list;
+
 protected:
     const char * getName() const override { return "KQL summarize"; }
     bool parseImpl(Pos & pos, ASTPtr & node, Expected & expected) override;
-
+    std::pair<String, String> removeLastWord(String input);
+    String getBinGroupbyString(String exprBin);
 };
 
 }
