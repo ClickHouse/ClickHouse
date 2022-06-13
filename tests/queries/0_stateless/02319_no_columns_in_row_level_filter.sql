b629833ef9aa71d091892d0284f7c773d6ea88e1
Alexander Gololobov <440544+davenger@users.noreply.github.com>
2022-06-13 12:32:24 +0200

    Test cases to check that row level filter is applied before prewhere actions are executed

--- tests/queries/0_stateless/02319_no_columns_in_row_level_filter.sql
+++ tests/queries/0_stateless/02319_no_columns_in_row_level_filter.sql
@@ -26,6 +26,11 @@ SELECT count(1) FROM test_table PREWHERE (n % 8192) < 4000;
 SELECT count(1) FROM test_table WHERE (n % 8192) < 4000;
 SELECT count(1) FROM test_table PREWHERE (n % 8192) < 4000 WHERE (n % 33) == 0;
 
+-- Run queries that have division by zero if row level filter isn't applied before prewhere
+SELECT count(1) FROM test_table PREWHERE 7 / (n % 5) > 2;
+SELECT count(1) FROM test_table WHERE 7 / (n % 5) > 2;
+SELECT count(1) FROM test_table PREWHERE 7 / (n % 5) > 2 WHERE (n % 33) == 0;
+
 DROP TABLE test_table;
 DROP ROW POLICY test_filter_policy ON test_table;
 DROP ROW POLICY test_filter_policy_2 ON test_table;
