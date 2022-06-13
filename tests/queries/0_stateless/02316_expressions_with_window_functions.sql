68ab4736ebbae8dcdfc99b20e7ce0f7fdc42e1e2
Dmitry Novik <n0vik@clickhouse.com>
2022-06-13 23:11:22 +0000

    Update test

--- tests/queries/0_stateless/02316_expressions_with_window_functions.sql
+++ tests/queries/0_stateless/02316_expressions_with_window_functions.sql
@@ -1,3 +1,9 @@
-SELECT sum(number) + 1
-FROM numbers(10000)
-GROUP BY number % 10;
+-- { echoOn }
+SELECT number, sum(number) + 1 OVER (PARTITION BY number % 10)
+FROM numbers(100)
+ORDER BY number;
+
+SELECT sum(number) + 1 AS x
+FROM numbers(100)
+GROUP BY number % 10
+ORDER BY x;
