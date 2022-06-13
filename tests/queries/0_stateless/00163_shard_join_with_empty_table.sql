5034caa94940b8e06f5be74f71c6026e5d5ad3e3
vdimir <vdimir@clickhouse.com>
2022-06-13 14:18:54 +0000

    upd 00163_shard_join_with_empty_table.sql

--- tests/queries/0_stateless/00163_shard_join_with_empty_table.sql
+++ tests/queries/0_stateless/00163_shard_join_with_empty_table.sql
@@ -1,4 +1,4 @@
--- Tags: shard, ignore-order
+-- Tags: shard
 
 SET any_join_distinct_right_table_keys = 1;
 SET joined_subquery_requires_alias = 0;
@@ -17,17 +17,16 @@ SELECT * FROM (
     USING n LIMIT 10
 ) ORDER BY number;
 
-
 SELECT * FROM (
     SELECT number, number / 2 AS n, j1, j2
     FROM remote('127.0.0.{2,3}', system.numbers)
     GLOBAL ANY LEFT JOIN (SELECT number / 3 AS n, number AS j1, 'Hello' AS j2 FROM system.numbers LIMIT 0)
-    USING n LIMIT 10;
+    USING n LIMIT 10
 ) ORDER BY number;
 
 SELECT * FROM (
     SELECT dummy + 2 AS number, number / 2 AS n, j1, j2
     FROM remote('127.0.0.{2,3}', system.one)
     GLOBAL ANY INNER JOIN (SELECT number / 3 AS n, number AS j1, 'Hello' AS j2 FROM system.numbers LIMIT 0)
-    USING n LIMIT 10;
+    USING n LIMIT 10
 ) ORDER BY number;
