34e63b20fb0a532ca6e8f63aae6caa4960d187f7
kssenii <sumarokovakseniia@mail.ru>
2022-06-13 21:27:54 +0200

    Review fixes

--- src/Storages/MergeTree/MergeTreeData.cpp
+++ src/Storages/MergeTree/MergeTreeData.cpp
@@ -1878,9 +1878,12 @@ size_t MergeTreeData::clearOldBrokenPartsFromDetachedDirecory()
 
         time_t current_time = time(nullptr);
         ssize_t threshold = current_time - getSettings()->merge_tree_clear_old_broken_detached_parts_interval_seconds;
-        time_t last_change_time = part_info.disk->getLastChanged(fs::path(relative_data_path) / "detached" / part_info.dir_name);
+        auto path = fs::path(relative_data_path) / "detached" / part_info.dir_name;
+        time_t last_change_time = part_info.disk->getLastChanged(path);
+        time_t last_modification_time = part_info.disk->getLastModified(path).epochTime();
+        time_t last_touch_time = std::max(last_change_time, last_modification_time);
 
-        if (last_change_time >= threshold)
+        if (last_touch_time == 0 || last_touch_time >= threshold)
             continue;
 
         renamed_parts.addPart(part_info.dir_name, "deleting_" + part_info.dir_name, part_info.disk);
