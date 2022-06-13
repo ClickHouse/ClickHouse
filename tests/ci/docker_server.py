f4d757cd6d02f21587d5293378c2722c37c16259
Pavel Novitskiy <pavel.novitsky@clickhouse.com>
2022-06-13 17:56:35 +0200

    remove unused args

--- tests/ci/docker_server.py
+++ tests/ci/docker_server.py
@@ -186,11 +186,11 @@ def gen_tags(version: ClickHouseVersion, release_type: str) -> List[str]:
     return tags
 
 
-def buildx_args(bucket_prefix: str, arch: str, version: ClickHouseVersion) -> List[str]:
+def buildx_args(bucket_prefix: str, arch: str) -> List[str]:
     args = [
         f"--platform=linux/{arch}",
         f"--label=build-url={GITHUB_RUN_URL}",
-        f"--label=com.clickhouse.ClickHouseVersion.githash={git.sha}",
+        f"--label=com.clickhouse.build.githash={git.sha}",
     ]
     if bucket_prefix:
         url = p.join(bucket_prefix, BUCKETS[arch])  # to prevent a double //
@@ -226,7 +226,7 @@ def build_and_push_image(
         metadata_path = p.join(TEMP_PATH, arch_tag)
         dockerfile = p.join(image.full_path, f"Dockerfile.{os}")
         cmd_args = list(init_args)
-        cmd_args.extend(buildx_args(bucket_prefix, arch, version))
+        cmd_args.extend(buildx_args(bucket_prefix, arch))
         if not push:
             cmd_args.append(f"--tag={image.repo}:{arch_tag}")
         cmd_args.extend(
