a8b17fec848a698dc8d0f6886b041089c991a923
lgbo-ustc <lgbo.ustc@gmail.com>
2022-06-16 16:53:11 +0800

    fixed a bug

--- src/Storages/Hive/StorageHive.h
+++ src/Storages/Hive/StorageHive.h
@@ -44,6 +44,7 @@ public:
     bool supportsIndexForIn() const override { return true; }
 
     bool supportsSubcolumns() const override { return true; }
+
     bool mayBenefitFromIndexForIn(
         const ASTPtr & /* left_in_operand */,
         ContextPtr /* query_context */,
