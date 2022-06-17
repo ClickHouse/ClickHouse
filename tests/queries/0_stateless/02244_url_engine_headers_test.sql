141e2bf85fa15c1130aedacfa94773eb8c152140
kssenii <sumarokovakseniia@mail.ru>
2022-06-17 14:53:16 +0200

    Headers for url in ast

--- tests/queries/0_stateless/02244_url_engine_headers_test.sql
+++ tests/queries/0_stateless/02244_url_engine_headers_test.sql
@@ -1,1 +1,6 @@
 select * from url(url_with_headers, url='http://127.0.0.1:8123?query=select+12', format='RawBLOB');
+select * from url('http://127.0.0.1:8123?query=select+12', 'RawBLOB', headers('X-ClickHouse-Format'='JSONEachRow'));
+select * from url('http://127.0.0.1:8123?query=select+12', 'RawBLOB', headers('X-ClickHouse-Format'='JSONEachRow', 'X-ClickHouse-Database'='kek')); -- { serverError 86 }
+drop table if exists url;
+create table url (i String) engine=URL('http://127.0.0.1:8123?query=select+12', 'RawBLOB', headers('X-ClickHouse-Format'='JSONEachRow'));
+select * from url;
