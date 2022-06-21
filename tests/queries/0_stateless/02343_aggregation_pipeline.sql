3ea034ae45e04df949f55767ec2d9a4978ab5841
Nikita Taranov <nikita.taranov@clickhouse.com>
2022-06-21 22:11:16 +0000

    fix tests

--- tests/queries/0_stateless/02343_aggregation_pipeline.sql
+++ tests/queries/0_stateless/02343_aggregation_pipeline.sql
@@ -4,6 +4,6 @@ explain pipeline select * from (select * from numbers_mt(1e8) group by number) g
 
 explain pipeline select * from (select * from numbers_mt(1e8) group by number) order by number settings max_threads = 16;
 
-explain pipeline select WatchID from remote('127.0.0.{1,2,3}', 'test', hits) group by WatchID settings max_threads = 16, prefer_localhost_replica = 1, distributed_aggregation_memory_efficient = 1;
+explain pipeline select number from remote('127.0.0.{1,2,3}', system, numbers_mt) group by number settings max_threads = 16, prefer_localhost_replica = 1, distributed_aggregation_memory_efficient = 1;
 
-explain pipeline select WatchID from remote('127.0.0.{1,2,3}', 'test', hits) group by WatchID settings max_threads = 16, prefer_localhost_replica = 1, distributed_aggregation_memory_efficient = 0;
+explain pipeline select number from remote('127.0.0.{1,2,3}', system, numbers_mt) group by number settings max_threads = 16, prefer_localhost_replica = 1, distributed_aggregation_memory_efficient = 0;
