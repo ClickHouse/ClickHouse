15e74f642c0841bbb2091f471fae5eff56b3c193
Yong Wang <yong.wang@ibm.com>
2022-06-22 12:00:47 -0700

    Kusto-phase2: Add common function to get argument for function convertion

--- src/Parsers/Kusto/KustoFunctions/IParserKQLFunction.cpp
+++ src/Parsers/Kusto/KustoFunctions/IParserKQLFunction.cpp
@@ -19,6 +19,12 @@
 namespace DB
 {
 
+namespace ErrorCodes
+{
+    extern const int SYNTAX_ERROR;
+}
+
+
 bool IParserKQLFunction::convert(String &out,IParser::Pos &pos)
 {
     return wrapConvertImpl(pos, IncreaseDepthTag{}, [&]
@@ -71,6 +77,52 @@ bool IParserKQLFunction::directMapping(String &out,IParser::Pos &pos,const Strin
 
     pos = begin;
     return false;
+}
+
+String IParserKQLFunction::getConvertedArgument(const String &fn_name, IParser::Pos &pos)
+{
+    String converted_arg;
+    std::unique_ptr<IParserKQLFunction> fun;
+
+    if (pos->type == TokenType::ClosingRoundBracket)
+        return converted_arg;
+
+    if (pos->isEnd() || pos->type == TokenType::PipeMark || pos->type == TokenType::Semicolon)
+        throw Exception("Syntax error near " + fn_name, ErrorCodes::SYNTAX_ERROR);
+
+    while (!pos->isEnd() && pos->type != TokenType::PipeMark && pos->type != TokenType::Semicolon)
+    {
+        String token = String(pos->begin,pos->end);
+        if (pos->type == TokenType::BareWord )
+        {
+            String converted;
+            fun = KQLFunctionFactory::get(token);
+            if ( fun && fun->convert(converted,pos))
+                converted_arg += converted;
+            else
+                converted_arg += token;
+        }
+        else if (pos->type == TokenType::Comma ||pos->type == TokenType::ClosingRoundBracket) 
+        {
+            break;
+        }
+        else
+            converted_arg += token;
+        ++pos;
+    }
+    return converted_arg;
+}
+
+String IParserKQLFunction::getKQLFunctionName(IParser::Pos &pos)
+{
+    String fn_name = String(pos->begin, pos->end);
+    ++pos;
+    if (pos->type != TokenType::OpeningRoundBracket)
+    {
+        --pos;
+        return "";
+    }
+    return  fn_name;
 }
 
 }
