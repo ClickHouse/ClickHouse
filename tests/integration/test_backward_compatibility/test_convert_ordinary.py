09f09bd96dc3ae1d5507496aac75a983b5c70a01
Alexander Tokmakov <tavplubix@clickhouse.com>
2022-06-23 21:40:05 +0200

    apply trash-formatter

--- tests/integration/test_backward_compatibility/test_convert_ordinary.py
+++ tests/integration/test_backward_compatibility/test_convert_ordinary.py
@@ -10,6 +10,7 @@ node = cluster.add_instance(
     with_installed_binary=True,
 )
 
+
 @pytest.fixture(scope="module")
 def start_cluster():
     try:
@@ -19,17 +20,23 @@ def start_cluster():
     finally:
         cluster.shutdown()
 
+
 def q(query):
     return node.query(query, settings={"log_queries": 1})
 
+
 def test_convert_system_db_to_atomic(start_cluster):
-    q("CREATE TABLE t(date Date, id UInt32) ENGINE = MergeTree PARTITION BY toYYYYMM(date) ORDER BY id")
+    q(
+        "CREATE TABLE t(date Date, id UInt32) ENGINE = MergeTree PARTITION BY toYYYYMM(date) ORDER BY id"
+    )
     q("INSERT INTO t VALUES (today(), 1)")
     q("INSERT INTO t SELECT number % 1000, number FROM system.numbers LIMIT 1000000")
 
     assert "1000001\n" == q("SELECT count() FROM t")
     assert "499999500001\n" == q("SELECT sum(id) FROM t")
-    assert "1970-01-01\t1000\t499500000\n1970-01-02\t1000\t499501000\n" == q("SELECT date, count(), sum(id) FROM t GROUP BY date ORDER BY date LIMIT 2")
+    assert "1970-01-01\t1000\t499500000\n1970-01-02\t1000\t499501000\n" == q(
+        "SELECT date, count(), sum(id) FROM t GROUP BY date ORDER BY date LIMIT 2"
+    )
     q("SYSTEM FLUSH LOGS")
 
     assert "query_log" in q("SHOW TABLES FROM system")
@@ -50,10 +57,13 @@ def test_convert_system_db_to_atomic(start_cluster):
     assert "part_log_0" in node.query("SHOW TABLES FROM system")
     assert "1\n" == node.query("SELECT count() != 0 FROM system.query_log_0")
     assert "1\n" == node.query("SELECT count() != 0 FROM system.part_log_0")
-    assert "1970-01-01\t1000\t499500000\n1970-01-02\t1000\t499501000\n" == node.query("SELECT date, count(), sum(id) FROM t GROUP BY date ORDER BY date LIMIT 2")
-    assert "INFORMATION_SCHEMA\ndefault\ninformation_schema\nsystem\n" == node.query("SELECT name FROM system.databases ORDER BY name")
+    assert "1970-01-01\t1000\t499500000\n1970-01-02\t1000\t499501000\n" == node.query(
+        "SELECT date, count(), sum(id) FROM t GROUP BY date ORDER BY date LIMIT 2"
+    )
+    assert "INFORMATION_SCHEMA\ndefault\ninformation_schema\nsystem\n" == node.query(
+        "SELECT name FROM system.databases ORDER BY name"
+    )
 
     assert "0\n" == node.count_in_log("<Error>")
     assert "0\n" == node.count_in_log("<Warning> Database")
     assert "0\n" == node.count_in_log("always include the lines below")
-
