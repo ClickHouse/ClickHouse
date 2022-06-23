766398ac07abdd6046340a375e3360bebe0d2d08
Igor Nikonov <igor@clickhouse.com>
2022-06-23 11:52:28 +0000

    Add plan tests for final distinct

--- tests/queries/0_stateless/02317_distinct_in_order_optimization.sql
+++ tests/queries/0_stateless/02317_distinct_in_order_optimization.sql
@@ -1,57 +1,71 @@
 drop table if exists distinct_in_order sync;
 create table distinct_in_order (a int, b int, c int) engine=MergeTree() order by (a, b, c);
 
-select 'enable optimize_distinct_in_order';
+select '-- enable optimize_distinct_in_order';
 set optimize_distinct_in_order=1;
-select 'distinct pipeline on empty table -> no optimization, source is ReadFromPreparedSource instead of ReadFromMergeTree';
+select '-- distinct pipeline on empty table -> no optimization, table is empty';
 explain pipeline select distinct * from distinct_in_order settings max_threads=1;
 
-select 'insert into table to use ReadFromMergeTree source';
-insert into distinct_in_order select number % number, number % 10, number % 5 from numbers(1,10);
+select '-- insert into table';
+insert into distinct_in_order select number % number, number % 5, number % 10 from numbers(1,10);
 
-select 'disable optimize_distinct_in_order';
+select '-- disable optimize_distinct_in_order';
 set optimize_distinct_in_order=0;
-select 'pipeline does _not_ contain the optimization';
+select '-- distinct all primary key columns -> no optimization';
 explain pipeline select distinct * from distinct_in_order settings max_threads=1;
 
-select 'enable optimize_distinct_in_order';
+select '-- enable optimize_distinct_in_order';
 set optimize_distinct_in_order=1;
-select 'distinct with all primary key columns -> optimization applied';
-insert into distinct_in_order select number % number, number % 10, number % 5 from numbers(1,10);
+
+select '-- distinct with all primary key columns -> pre-distinct optimization only';
 explain pipeline select distinct * from distinct_in_order settings max_threads=1;
-select 'distinct with primary key prefix -> optimization applied';
+
+select '-- distinct with primary key prefix -> pre-distinct optimization only';
 explain pipeline select distinct a, c from distinct_in_order settings max_threads=1;
-select 'distinct with non-primary key prefix -> no optimization';
+
+select '-- distinct with primary key prefix and order by on column in distinct -> pre-distinct and final distinct optimization';
+explain pipeline select distinct a, c from distinct_in_order order by c settings max_threads=1;
+
+select '-- distinct with primary key prefix and order by on column _not_ in distinct -> pre-distinct optimization only';
+explain pipeline select distinct a, c from distinct_in_order order by b settings max_threads=1;
+
+select '-- distinct with non-primary key prefix -> no optimization';
 explain pipeline select distinct b, c from distinct_in_order settings max_threads=1;
 
-select 'the same values in every chunk, distinct in order should skip entire chunks with the same key as previous one';
+select '-- distinct with non-primary key prefix and order by on column in distinct -> final distinct optimization only';
+explain pipeline select distinct b, c from distinct_in_order order by b settings max_threads=1;
+
+select '-- distinct with non-primary key prefix and order by on column _not_ in distinct -> no optimization';
+explain pipeline select distinct b, c from distinct_in_order order by a settings max_threads=1;
+
+select '-- the same values in every chunk, distinct in order should skip entire chunks with the same key as previous one';
 drop table if exists distinct_in_order sync;
 create table distinct_in_order (a int) engine=MergeTree() order by a settings index_granularity=10;
-insert into distinct_in_order (a) select * from zeros(30);
-select 'single-threaded distinct';
+insert into distinct_in_order (a) select * from zeros(100);
+select '-- single-threaded distinct';
 select distinct * from distinct_in_order settings max_block_size=10, max_threads=1;
-select 'multi-threaded distinct';
+select '-- multi-threaded distinct';
 select distinct * from distinct_in_order settings max_block_size=10;
 
-select 'skip part of chunk since it contains values from previous one';
+select '-- skip part of chunk since it contains values from previous one';
 drop table if exists distinct_in_order sync;
 create table distinct_in_order (a int) engine=MergeTree() order by a settings index_granularity=10;
 insert into distinct_in_order (a) select * from zeros(10);
 insert into distinct_in_order select * from numbers(10);
-select 'single-threaded distinct';
+select '-- single-threaded distinct';
 select distinct a from distinct_in_order settings max_block_size=10, max_threads=1;
-select 'multi-threaded distinct';
+select '-- multi-threaded distinct';
 select distinct a from distinct_in_order settings max_block_size=10;
 
-select 'table with not only primary key columns';
+select '-- create table with not only primary key columns';
 drop table if exists distinct_in_order sync;
 create table distinct_in_order (a int, b int, c int) engine=MergeTree() order by (a, b);
-insert into distinct_in_order select number % number, number % 10, number % 5 from numbers(1,1000000);
-select 'distinct with key-prefix only';
+insert into distinct_in_order select number % number, number % 5, number % 10 from numbers(1,1000000);
+select '-- distinct with primary key prefix only';
 select distinct a from distinct_in_order;
-select 'distinct with full key';
+select '-- distinct with full key';
 select distinct a,b from distinct_in_order order by b;
-select 'distinct with key prefix and non-sorted column';
+select '-- distinct with key prefix and non-sorted column';
 select distinct a,c from distinct_in_order order by c;
 
 -- drop table if exists distinct_in_order sync;
