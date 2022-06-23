fc5696486c26f580b18925b49bbf93e9b465efa8
Kruglov Pavel <48961922+Avogar@users.noreply.github.com>
2022-06-23 14:21:38 +0200

    Fix

--- tests/queries/0_stateless/02305_schema_inference_with_globs.sh
+++ tests/queries/0_stateless/02305_schema_inference_with_globs.sh
@@ -8,7 +8,7 @@ CUR_DIR=$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)
 $CLICKHOUSE_CLIENT -q "insert into function file(data1.jsonl) select NULL as x from numbers(10) settings engine_file_truncate_on_insert=1"
 $CLICKHOUSE_CLIENT -q "insert into function file(data2.jsonl) select NULL as x from numbers(10) settings engine_file_truncate_on_insert=1"
 $CLICKHOUSE_CLIENT -q "insert into function file(data3.jsonl) select NULL as x from numbers(10) settings engine_file_truncate_on_insert=1"
-$CLICKHOUSE_CLIENT -q "insert into function file(data4.jsonl) select number % 2 ? number : NULL as x from numbers(10) settings engine_file_truncate_on_insert=1"
+$CLICKHOUSE_CLIENT -q "insert into function file(data4.jsonl) select number % 2 ? number::UInt32 : NULL as x from numbers(10) settings engine_file_truncate_on_insert=1"
 
 $CLICKHOUSE_CLIENT -q "desc file('data*.jsonl') settings input_format_max_rows_to_read_for_schema_inference=32"
 $CLICKHOUSE_CLIENT -q "desc file('data*.jsonl') settings input_format_max_rows_to_read_for_schema_inference=100"
