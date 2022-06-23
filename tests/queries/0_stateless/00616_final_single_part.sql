fb249aca621e009c0bc283ac4231933b7df8bfcd
Alexander Tokmakov <tavplubix@clickhouse.com>
2022-06-23 14:11:16 +0200

    fix tests

--- tests/queries/0_stateless/00616_final_single_part.sql
+++ tests/queries/0_stateless/00616_final_single_part.sql
@@ -3,6 +3,7 @@ SET optimize_on_insert = 0;
 DROP TABLE IF EXISTS test_00616;
 DROP TABLE IF EXISTS replacing_00616;
 
+set allow_deprecated_syntax_for_merge_tree=1;
 CREATE TABLE test_00616
 (
     date Date,
