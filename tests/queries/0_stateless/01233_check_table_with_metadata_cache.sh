9a9b2f427b1b5c83dcec6cea8acc99d6550e5568
alesapin <alesapin@gmail.com>
2022-06-23 18:36:10 +0200

    Followup

--- tests/queries/0_stateless/01233_check_table_with_metadata_cache.sh
+++ tests/queries/0_stateless/01233_check_table_with_metadata_cache.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
-# Tags: no-fasttest, long
+# Tags: no-fasttest, long, no-s3-storage
 # Tag no-fasttest: setting use_metadata_cache=true is not supported in fasttest, because clickhouse binary in fasttest is build without RocksDB.
 # To suppress Warning messages from CHECK TABLE
 CLICKHOUSE_CLIENT_SERVER_LOGS_LEVEL=error
