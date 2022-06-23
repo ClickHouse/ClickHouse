5661280ef6ed8ae53467d6273aecb4b6b2297211
alesapin <alesapin@gmail.com>
2022-06-23 14:01:26 +0200

    Remove some read methods

--- src/Storages/MergeTree/MergeTreeDataWriter.h
+++ src/Storages/MergeTree/MergeTreeDataWriter.h
@@ -34,7 +34,10 @@ using BlocksWithPartition = std::vector<BlockWithPartition>;
 class MergeTreeDataWriter
 {
 public:
-    explicit MergeTreeDataWriter(MergeTreeData & data_) : data(data_), log(&Poco::Logger::get(data.getLogName() + " (Writer)")) {}
+    explicit MergeTreeDataWriter(MergeTreeData & data_)
+        : data(data_)
+        , log(&Poco::Logger::get(data.getLogName() + " (Writer)"))
+    {}
 
     /** Split the block to blocks, each of them must be written as separate part.
       *  (split rows by partition)
