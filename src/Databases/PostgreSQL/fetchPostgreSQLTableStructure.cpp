d52dc3201070a9dd2a57858d6c3772e6652cc8fe
Kseniia Sumarokova <54203879+kssenii@users.noreply.github.com>
2022-06-24 19:31:41 +0200

    Update fetchPostgreSQLTableStructure.cpp

--- src/Databases/PostgreSQL/fetchPostgreSQLTableStructure.cpp
+++ src/Databases/PostgreSQL/fetchPostgreSQLTableStructure.cpp
@@ -300,10 +300,11 @@ PostgreSQLTableStructure fetchPostgreSQLTableStructure(
             "and a.attnum = ANY(ix.indkey) "
             "and t.relkind in ('r', 'p') " /// simple tables
             "and t.relname = {} " /// Connection is already done to a needed database, only table name is needed.
-            "and t.relnamespace = {} "
+            "{}"
             "and ix.indisreplident = 't' " /// index is is replica identity index
-            "ORDER BY a.attname", /// column names
-        quoteString(postgres_table), quoteString(postgres_schema.empty() ? "public" : postgres_schema));
+            "ORDER BY a.attname", /// column name
+            (postgres_schema.empty() ? "" : "and t.relnamespace = " + quoteString(postgres_schema)) + " ",
+            quoteString(postgres_table));
 
         table.replica_identity_columns = readNamesAndTypesList(tx, postgres_table_with_schema, query, use_nulls, true);
     }
