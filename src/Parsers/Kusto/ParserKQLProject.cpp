fd6471e40f354baf2dece90635e20a955d510536
Yong Wang <yong.wang@ibm.com>
2022-06-24 13:05:52 -0700

    Kusto-phase2: Add alias support

--- src/Parsers/Kusto/ParserKQLProject.cpp
+++ src/Parsers/Kusto/ParserKQLProject.cpp
@@ -11,25 +11,7 @@ bool ParserKQLProject :: parseImpl(Pos & pos, ASTPtr & node, Expected & expected
     if (op_pos.empty())
         expr = "*";
     else
-    {
-        for (auto it = op_pos.begin(); it != op_pos.end(); ++it)
-        {
-            pos = *it ;
-            while (!pos->isEnd() && pos->type != TokenType::PipeMark)
-            {
-                if (pos->type == TokenType::BareWord)
-                {
-                    String tmp(pos->begin,pos->end);
-
-                    if (it != op_pos.begin() && columns.find(tmp) == columns.end())
-                        return false;
-                    columns.insert(tmp);
-                }
-                ++pos;
-            }
-        }
         expr = getExprFromToken(op_pos.back());
-    }
 
     Tokens tokens(expr.c_str(), expr.c_str()+expr.size());
     IParser::Pos new_pos(tokens, pos.max_depth);
