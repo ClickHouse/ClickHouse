cec6a0ebf8575c92cb1069ed7fd6e6d5e42b3573
alesapin <alesapin@gmail.com>
2022-06-24 17:33:43 +0200

    Fix comment

--- src/Storages/MergeTree/MergeTreeData.h
+++ src/Storages/MergeTree/MergeTreeData.h
@@ -549,10 +549,7 @@ public:
 
     /// Renames temporary part to a permanent part and adds it to the parts set.
     /// It is assumed that the part does not intersect with existing parts.
-    /// If increment != nullptr, part index is determining using increment. Otherwise part index remains unchanged.
-    /// If out_transaction != nullptr, adds the part in the PreActive state (the part will be added to the
-    /// active set later with out_transaction->commit()).
-    /// Else, commits the part immediately.
+    /// Adds the part in the PreActive state (the part will be added to the active set later with out_transaction->commit()).
     /// Returns true if part was added. Returns false if part is covered by bigger part.
     bool renameTempPartAndAdd(
         MutableDataPartPtr & part,
@@ -561,15 +558,15 @@ public:
 
     /// The same as renameTempPartAndAdd but the block range of the part can contain existing parts.
     /// Returns all parts covered by the added part (in ascending order).
-    /// If out_transaction == nullptr, marks covered parts as Outdated.
+    DataPartsVector renameTempPartAndReplace(
+        MutableDataPartPtr & part,
+        Transaction & out_transaction);
+
+    /// Unlocked version of previous one. Useful when added multiple parts with a single lock.
     DataPartsVector renameTempPartAndReplaceUnlocked(
         MutableDataPartPtr & part,
         Transaction & out_transaction,
         DataPartsLock & lock);
-
-    DataPartsVector renameTempPartAndReplace(
-        MutableDataPartPtr & part,
-        Transaction & out_transaction);
 
     /// Remove parts from working set immediately (without wait for background
     /// process). Transfer part state to temporary. Have very limited usage only
@@ -1249,14 +1246,13 @@ private:
     /// in precommited state and to transasction
     void preparePartForCommit(MutableDataPartPtr & part, Transaction & out_transaction, bool need_rename);
 
-    /// Low-level method for preparing parts for commit.
+    /// Low-level method for preparing parts for commit (in-memory).
     /// FIXME Merge MergeTreeTransaction and Transaction
     bool renameTempPartAndReplaceImpl(
         MutableDataPartPtr & part,
         Transaction & out_transaction,
         DataPartsLock & lock,
         DataPartsVector * out_covered_parts);
-
 
     /// RAII Wrapper for atomic work with currently moving parts
     /// Acquire them in constructor and remove them in destructor
