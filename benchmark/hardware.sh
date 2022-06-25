0568ebdc09351d564fb7928fa32c88527899a7aa
Alexey Milovidov <milovidov@clickhouse.com>
2022-06-25 04:29:43 +0200

    Better hardware benchmark

--- benchmark/hardware.sh
+++ benchmark/hardware.sh
@@ -1,6 +1,5 @@
 #!/bin/bash -e
 
-TABLE="hits"
 QUERIES_FILE="queries.sql"
 TRIES=3
 
@@ -42,24 +41,24 @@ if [[ $(./clickhouse client --query "EXISTS hits") == '1' && $(./clickhouse clie
 else
     echo "Will download the dataset"
     ./clickhouse client --max_insert_threads $(nproc || 4) --progress --query "
-        CREATE OR REPLACE TABLE ${TABLE} ENGINE = MergeTree PARTITION BY toYYYYMM(EventDate) ORDER BY (CounterID, EventDate, intHash32(UserID), EventTime)
+        CREATE OR REPLACE TABLE hits ENGINE = MergeTree PARTITION BY toYYYYMM(EventDate) ORDER BY (CounterID, EventDate, intHash32(UserID), EventTime)
         AS SELECT * FROM url('https://datasets.clickhouse.com/hits/native/hits_100m_obfuscated_{0..255}.native.zst')"
 
-    ./clickhouse client --query "SELECT 'The dataset size is: ', count() FROM ${TABLE}"
+    ./clickhouse client --query "SELECT 'The dataset size is: ', count() FROM hits"
 fi
 
 if [[ $(./clickhouse client --query "SELECT count() FROM system.parts WHERE table = 'hits' AND database = 'default' AND active") == '1' ]]; then
     echo "Dataset already prepared"
 else
     echo "Will prepare the dataset"
-    ./clickhouse client --query "OPTIMIZE TABLE ${TABLE} FINAL"
+    ./clickhouse client --query "OPTIMIZE TABLE hits FINAL"
 fi
 
 echo
 echo "Will perform benchmark. Results:"
 echo
 
-cat "$QUERIES_FILE" | sed "s/{table}/${TABLE}/g" | while read query; do
+cat "$QUERIES_FILE" | sed "s/{table}/hits/g" | while read query; do
     sync
     if [ "${OS}" = "Darwin" ] 
     then 
