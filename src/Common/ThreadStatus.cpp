9bb6b3db122dbe7897ed2533f84e3c81c63a1791
Alexey Milovidov <milovidov@clickhouse.com>
2022-06-25 03:59:01 +0200

    Slight improvement

--- src/Common/ThreadStatus.cpp
+++ src/Common/ThreadStatus.cpp
@@ -148,19 +148,10 @@ ThreadStatus::ThreadStatus()
 
 ThreadStatus::~ThreadStatus()
 {
-    try
-    {
-        if (untracked_memory > 0)
-            memory_tracker.alloc(untracked_memory);
-        else
-            memory_tracker.free(-untracked_memory);
-    }
-    catch (const DB::Exception &)
-    {
-        /// It's a minor tracked memory leak here (not the memory itself but it's counter).
-        /// We've already allocated a little bit more than the limit and cannot track it in the thread memory tracker or its parent.
-        tryLogCurrentException(log);
-    }
+    if (untracked_memory > 0)
+        memory_tracker.allocImpl(untracked_memory, false);
+    else
+        memory_tracker.free(-untracked_memory);
 
     if (thread_group)
     {
