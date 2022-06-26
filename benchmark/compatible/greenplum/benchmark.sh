f62aa0a8f6454f2dd0c75802d3a2897464077837
Alexey Milovidov <milovidov@clickhouse.com>
2022-06-26 10:14:00 +0200

    Compatible benchmark

---
+++ benchmark/compatible/greenplum/benchmark.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+sudo apt-get update
+sudo apt-get install docker.io
+sudo docker run -it --rm --volume=$(pwd):/workspace ubuntu:18.04
+
+cd workspace
+apt update
+wget 'https://github.com/greenplum-db/gpdb/releases/download/6.21.0/greenplum-db-6.21.0-ubuntu18.04-amd64.deb'
+
+apt install ./greenplum-db-6.21.0-ubuntu18.04-amd64.deb
+useradd gpadmin
+chown -R gpadmin:gpadmin /usr/local/greenplum*
+chgrp -R gpadmin /usr/local/greenplum*
