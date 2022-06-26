c953edbe3813ff074029d671036dba6a28585fff
Alexey Milovidov <milovidov@clickhouse.com>
2022-06-27 00:39:23 +0200

    Compatible benchmark

--- benchmark/compatible/duckdb/run.sh
+++ benchmark/compatible/duckdb/run.sh
@@ -3,5 +3,8 @@
 cat queries.sql | while read query; do
     sync
     echo 3 | sudo tee /proc/sys/vm/drop_caches >/dev/null
+
+    # We need to restart and reinitialize DuckDB after every query,
+    # because it often fails with Segmentation fault (core dumped)
     ./query.py <<< "${query}"
 done
