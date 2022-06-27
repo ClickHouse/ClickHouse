a35f2b784dbe91d2ba2a0b481f250781c5fde9b6
Alexey Milovidov <milovidov@clickhouse.com>
2022-06-27 03:48:57 +0200

    Compatible benchmark

--- benchmark/compatible/cratedb/benchmark.sh
+++ benchmark/compatible/cratedb/benchmark.sh
@@ -10,16 +10,16 @@ psql -U crate -h localhost --no-password -t -c 'SELECT 1'
 wget --continue 'https://datasets.clickhouse.com/hits_compatible/hits.tsv.gz'
 gzip -d hits.tsv.gz
 
-psql -U crate -h localhost --no-password -t -c 'CREATE DATABASE test'
-psql -U crate -h localhost --no-password test -t < create.sql
-psql -U crate -h localhost --no-password test -t -c '\timing' -c "\\copy hits FROM 'hits.tsv'"
+psql -U crate -h localhost --no-password -t < create.sql
+psql -U crate -h localhost --no-password -t -c '\timing' -c "COPY hits FROM 'file:///$(pwd)/hits.tsv'"
 
-#
-#
+# It failed to load the data.
+# COPY 0
+# Time: 1004421.355 ms (16:44.421)
 
 ./run.sh 2>&1 | tee log.txt
 
 du -bcs crate-*
 
 cat log.txt | grep -oP 'Time: \d+\.\d+ ms' | sed -r -e 's/Time: ([0-9]+\.[0-9]+) ms/\1/' |
-    awk '{ if (i % 3 == 0) { printf "[" }; printf $1; if (i % 3 != 2) { printf "," } else { print "]," }; ++i; }'
+    awk '{ if (i % 3 == 0) { printf "[" }; printf $1 / 1000; if (i % 3 != 2) { printf "," } else { print "]," }; ++i; }'
--- benchmark/compatible/timescaledb/benchmark.sh
+++ benchmark/compatible/timescaledb/benchmark.sh
@@ -36,4 +36,4 @@ sudo -u postgres psql test -t -c '\timing' -c "\\copy hits FROM 'hits.tsv'"
 sudo du -bcs /var/lib/postgresql/14/main/
 
 cat log.txt | grep -oP 'Time: \d+\.\d+ ms' | sed -r -e 's/Time: ([0-9]+\.[0-9]+) ms/\1/' |
-    awk '{ if (i % 3 == 0) { printf "[" }; printf $1; if (i % 3 != 2) { printf "," } else { print "]," }; ++i; }'
+    awk '{ if (i % 3 == 0) { printf "[" }; printf $1 / 1000; if (i % 3 != 2) { printf "," } else { print "]," }; ++i; }'
