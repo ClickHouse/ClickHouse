b0c9d1a25d8d07a2a3e98de93f31a57155d5ed69
avogar <pav.cruglov@yandex.ru>
2022-06-27 14:04:28 +0000

    Fix style

--- tests/integration/test_storage_hdfs/test.py
+++ tests/integration/test_storage_hdfs/test.py
@@ -646,14 +646,14 @@ def test_schema_inference_cache(started_cluster):
     desc_query = f"desc hdfs('hdfs://hdfs1:9000/test_cache.jsonl')"
     node1.query(desc_query)
     cache_misses = get_profile_event_for_query(
-        node1, desc_query, 'SchemaInferenceCacheMisses'
+        node1, desc_query, "SchemaInferenceCacheMisses"
     )
     assert cache_misses == 1
 
     desc_query = f"desc hdfs('hdfs://hdfs1:9000/test_cache.jsonl')"
     node1.query(desc_query)
     cache_hits = get_profile_event_for_query(
-        node1, desc_query, 'SchemaInferenceCacheHits'
+        node1, desc_query, "SchemaInferenceCacheHits"
     )
     assert cache_hits == 1
 
@@ -663,7 +663,7 @@ def test_schema_inference_cache(started_cluster):
     desc_query = f"desc hdfs('hdfs://hdfs1:9000/test_cache.jsonl')"
     node1.query(desc_query)
     cache_invalidations = get_profile_event_for_query(
-        node1, desc_query, 'SchemaInferenceCacheInvalidations'
+        node1, desc_query, "SchemaInferenceCacheInvalidations"
     )
     assert cache_invalidations == 1
 
@@ -673,7 +673,7 @@ def test_schema_inference_cache(started_cluster):
     desc_query = f"desc hdfs('hdfs://hdfs1:9000/test_cache1.jsonl') settings cache_ttl_for_hdfs_schema_inference=1"
     node1.query(desc_query)
     cache_misses = get_profile_event_for_query(
-        node1, desc_query, 'SchemaInferenceCacheMisses'
+        node1, desc_query, "SchemaInferenceCacheMisses"
     )
     assert cache_misses == 1
 
@@ -682,22 +682,22 @@ def test_schema_inference_cache(started_cluster):
     desc_query = f"desc hdfs('hdfs://hdfs1:9000/test_cache1.jsonl') settings cache_ttl_for_hdfs_schema_inference=1000"
     node1.query(desc_query)
     cache_misses = get_profile_event_for_query(
-        node1, desc_query, 'SchemaInferenceCacheMisses'
+        node1, desc_query, "SchemaInferenceCacheMisses"
     )
     assert cache_misses == 1
     cache_ttl_expirations = get_profile_event_for_query(
-        node1, desc_query, 'SchemaInferenceCacheTTLExpirations'
+        node1, desc_query, "SchemaInferenceCacheTTLExpirations"
     )
     assert cache_ttl_expirations == 1
 
     desc_query = f"desc hdfs('hdfs://hdfs1:9000/test_cache1.jsonl')"
     node1.query(desc_query)
     cache_hits = get_profile_event_for_query(
-        node1, desc_query, 'SchemaInferenceCacheHits'
+        node1, desc_query, "SchemaInferenceCacheHits"
     )
     assert cache_hits == 1
     cache_ttl_updates = get_profile_event_for_query(
-        node1, desc_query, 'SchemaInferenceCacheTTLUpdates'
+        node1, desc_query, "SchemaInferenceCacheTTLUpdates"
     )
     assert cache_ttl_updates == 1
 
@@ -707,7 +707,7 @@ def test_schema_inference_cache(started_cluster):
     desc_query = f"desc hdfs('hdfs://hdfs1:9000/test_cache1.jsonl') settings cache_ttl_for_hdfs_schema_inference=1"
     node1.query(desc_query)
     cache_invalidations = get_profile_event_for_query(
-        node1, desc_query, 'SchemaInferenceCacheInvalidations'
+        node1, desc_query, "SchemaInferenceCacheInvalidations"
     )
     assert cache_invalidations == 1
 
@@ -715,16 +715,16 @@ def test_schema_inference_cache(started_cluster):
 
     desc_query = f"desc hdfs('hdfs://hdfs1:9000/test_cache.jsonl')"
     node1.query(desc_query)
-    cache_hits = get_profile_event_for_query(node1, desc_query, 'SchemaInferenceCacheHits')
+    cache_hits = get_profile_event_for_query(node1, desc_query, "SchemaInferenceCacheHits")
     assert cache_hits == 1
     cache_ttl_expirations = get_profile_event_for_query(
-        node1, desc_query, 'SchemaInferenceCacheTTLExpirations'
+        node1, desc_query, "SchemaInferenceCacheTTLExpirations"
     )
     assert cache_ttl_expirations == 1
 
     desc_query = f"desc hdfs('hdfs://hdfs1:9000/test_cache1.jsonl')"
     node1.query(desc_query)
-    cache_misses = get_profile_event_for_query(node1, desc_query, 'SchemaInferenceCacheMisses')
+    cache_misses = get_profile_event_for_query(node1, desc_query, "SchemaInferenceCacheMisses")
     assert cache_misses == 1
 
     node1.query(
@@ -737,21 +737,21 @@ def test_schema_inference_cache(started_cluster):
     desc_query = f"desc hdfs('hdfs://hdfs1:9000/test_cache*.jsonl')"
     node1.query(desc_query)
     cache_hits = get_profile_event_for_query(
-        node1, desc_query, 'SchemaInferenceCacheHits'
+        node1, desc_query, "SchemaInferenceCacheHits"
     )
     assert cache_hits == 1
 
     desc_query = f"desc hdfs('hdfs://hdfs1:9000/test_cache2.jsonl')"
     node1.query(desc_query)
     cache_hits = get_profile_event_for_query(
-        node1, desc_query, 'SchemaInferenceCacheHits'
+        node1, desc_query, "SchemaInferenceCacheHits"
     )
     assert cache_hits == 1
 
     desc_query = f"desc hdfs('hdfs://hdfs1:9000/test_cache3.jsonl')"
     node1.query(desc_query)
     cache_hits = get_profile_event_for_query(
-        node1, desc_query, 'SchemaInferenceCacheHits'
+        node1, desc_query, "SchemaInferenceCacheHits"
     )
     assert cache_hits == 1
 
