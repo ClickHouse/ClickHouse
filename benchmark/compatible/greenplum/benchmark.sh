8d073ff0d5e9c46d7e045b48ebb6b820e19ad8d4
Alexey Milovidov <milovidov@clickhouse.com>
2022-06-27 05:31:19 +0200

    Compatible benchmark

--- benchmark/compatible/greenplum/benchmark.sh
+++ benchmark/compatible/greenplum/benchmark.sh
@@ -1,4 +1,17 @@
 #!/bin/bash
 
-sudo apt-get install -y docker.io
-sudo docker run --name greenplum --volume=$(pwd):/workspace gptext/gpdb6
+# NOTE: it requires Ubuntu 18.04
+# Greenplum does not install on any newer system.
+
+sudo apt update
+sudo apt install -y software-properties-common
+sudo add-apt-repository ppa:greenplum/db
+sudo apt update
+sudo apt install greenplum-db-6
+source /opt/greenplum-db-*.0/greenplum_path.sh
+cp $GPHOME/docs/cli_help/gpconfigs/gpinitsystem_singlenode .
+echo localhost > ./hostlist_singlenode
+sed -i "s/MASTER_HOSTNAME=[a-z_]*/MASTER_HOSTNAME=$(hostname)/" gpinitsystem_singlenode
+sudo mkdir /gpmaster
+sudo chmod 777 /gpmaster
+gpinitsystem -c gpinitsystem_singlenode
