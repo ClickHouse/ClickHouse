3a1b7e65f0f9c4b8eca94dfe998074cd9d320889
alesapin <alesapin@gmail.com>
2022-06-29 17:56:59 +0200

    Test ipv6 as well

--- tests/integration/test_server_start_and_ip_conversions/test.py
+++ tests/integration/test_server_start_and_ip_conversions/test.py
@@ -15,7 +15,7 @@ def start_cluster():
         cluster.shutdown()
 
 
-def test_restart_success():
+def test_restart_success_ipv4():
     node.query("""
         CREATE TABLE ipv4_test
         (
@@ -25,6 +25,22 @@ def test_restart_success():
         settings={"cast_ipv4_ipv6_default_on_conversion_error": 1})
 
     node.query("ALTER TABLE ipv4_test MODIFY COLUMN value IPv4 DEFAULT ''", settings={"cast_ipv4_ipv6_default_on_conversion_error": 1})
+
+    node.restart_clickhouse()
+
+    assert node.query("SELECT 1") == "1\n"
+
+
+def test_restart_success_ipv6():
+    node.query("""
+        CREATE TABLE ipv6_test
+        (
+            id UInt64,
+            value String
+        ) ENGINE=MergeTree ORDER BY id""",
+        settings={"cast_ipv4_ipv6_default_on_conversion_error": 1})
+
+    node.query("ALTER TABLE ipv6_test MODIFY COLUMN value IPv6 DEFAULT ''", settings={"cast_ipv4_ipv6_default_on_conversion_error": 1})
 
     node.restart_clickhouse()
 
