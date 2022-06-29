5a010c7a623bc67da46886d665d392bbcb29fa31
Maksim Kita <kitaetoya@gmail.com>
2022-06-29 19:19:47 +0200

    CacheDictionary simplify update queue

--- src/Dictionaries/CacheDictionaryUpdateQueue.h
+++ src/Dictionaries/CacheDictionaryUpdateQueue.h
@@ -77,6 +77,9 @@ private:
     std::atomic<bool> is_done{false};
     std::exception_ptr current_exception{nullptr}; /// NOLINT
 
+    mutable std::mutex update_mutex;
+    mutable std::condition_variable is_update_finished;
+
     /// While UpdateUnit is alive, it is accounted in update_queue size.
     CurrentMetrics::Increment alive_batch{CurrentMetrics::CacheDictionaryUpdateQueueBatches};
     CurrentMetrics::Increment alive_keys;
@@ -159,9 +162,6 @@ private:
 
     UpdateQueue update_queue;
     ThreadPool update_pool;
-
-    mutable std::mutex update_mutex;
-    mutable std::condition_variable is_update_finished;
 };
 
 extern template class CacheDictionaryUpdateQueue<DictionaryKeyType::Simple>;
