cd2cb10f605bcd706d9be731e0488d6c2bd3c369
Alexander Tokmakov <tavplubix@clickhouse.com>
2022-06-29 20:55:46 +0300

    another try to fix performance tests

--- docker/test/performance-comparison/download.sh
+++ docker/test/performance-comparison/download.sh
@@ -73,7 +73,16 @@ function download
     ) &
 
     wait
+    pwd
+    ls
+    ls db0
+    ls db0/metadata
     cd db0 && echo "ATTACH DATABASE default ENGINE=Ordinary" > metadata/default.sql
+    cd db0 && echo "ATTACH DATABASE datasets ENGINE=Ordinary" > metadata/datasets.sql
+    pwd
+    ls
+    ls db0
+    ls db0/metadata
 }
 
 download
