8f5582f95efd18eac8c926a89574b20e617b939c
alesapin <alesapin@gmail.com>
2022-06-29 20:29:50 +0200

    Review and style fixes

--- tests/integration/test_server_start_and_ip_conversions/test.py
+++ tests/integration/test_server_start_and_ip_conversions/test.py
@@ -6,6 +6,7 @@ from helpers.cluster import ClickHouseCluster
 cluster = ClickHouseCluster(__file__)
 node = cluster.add_instance("node", stay_alive=True)
 
+
 @pytest.fixture(scope="module", autouse=True)
 def start_cluster():
     try:
@@ -16,15 +17,20 @@ def start_cluster():
 
 
 def test_restart_success_ipv4():
-    node.query("""
+    node.query(
+        """
         CREATE TABLE ipv4_test
         (
             id UInt64,
             value String
         ) ENGINE=MergeTree ORDER BY id""",
-        settings={"cast_ipv4_ipv6_default_on_conversion_error": 1})
+        settings={"cast_ipv4_ipv6_default_on_conversion_error": 1},
+    )
 
-    node.query("ALTER TABLE ipv4_test MODIFY COLUMN value IPv4 DEFAULT ''", settings={"cast_ipv4_ipv6_default_on_conversion_error": 1})
+    node.query(
+        "ALTER TABLE ipv4_test MODIFY COLUMN value IPv4 DEFAULT ''",
+        settings={"cast_ipv4_ipv6_default_on_conversion_error": 1},
+    )
 
     node.restart_clickhouse()
 
@@ -32,15 +38,20 @@ def test_restart_success_ipv4():
 
 
 def test_restart_success_ipv6():
-    node.query("""
+    node.query(
+        """
         CREATE TABLE ipv6_test
         (
             id UInt64,
             value String
         ) ENGINE=MergeTree ORDER BY id""",
-        settings={"cast_ipv4_ipv6_default_on_conversion_error": 1})
+        settings={"cast_ipv4_ipv6_default_on_conversion_error": 1},
+    )
 
-    node.query("ALTER TABLE ipv6_test MODIFY COLUMN value IPv6 DEFAULT ''", settings={"cast_ipv4_ipv6_default_on_conversion_error": 1})
+    node.query(
+        "ALTER TABLE ipv6_test MODIFY COLUMN value IPv6 DEFAULT ''",
+        settings={"cast_ipv4_ipv6_default_on_conversion_error": 1},
+    )
 
     node.restart_clickhouse()
 
