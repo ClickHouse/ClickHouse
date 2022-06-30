4423baf72ca0770ba2cc75dafa1adcf681ef2711
Nikita Mikhaylov <mikhaylovnikitka@gmail.com>
2022-06-30 12:44:30 +0200

    Update programs/su/clickhouse-su.cpp

--- programs/su/clickhouse-su.cpp
+++ programs/su/clickhouse-su.cpp
@@ -59,7 +59,7 @@ void setUserAndGroup(std::string arg_uid, std::string arg_gid)
                 throwFromErrno(fmt::format("Cannot do 'getgrnam_r' to obtain gid from group name ({})", arg_gid), ErrorCodes::SYSTEM_ERROR);
 
             if (!result)
-                throw Exception(fmt::format("Group {} is not found in the system", arg_gid), ErrorCodes::BAD_ARGUMENTS);
+                throw Exception(ErrorCodes::BAD_ARGUMENTS, "Group {} is not found in the system", arg_gid);
 
             gid = entry.gr_gid;
         }
