e4dacdc2a129f9bfc08058d9025c37f0475d2620
Larry Luo <larry.luo@ca.ibm.com>
2022-06-30 16:29:29 -0700

    Make full text search tests reliable

--- tests/queries/0_stateless/02346_full_text_search.sql
+++ tests/queries/0_stateless/02346_full_text_search.sql
@@ -1,5 +1,7 @@
 SET log_queries = 1;
 SET max_threads = 1;
+TRUNCATE system.query_log;
+
 -- create table for gin(2)
 DROP TABLE IF EXISTS simple1;
 CREATE TABLE simple1(k UInt64,s String,INDEX af (s) TYPE gin(2) GRANULARITY 1) 
@@ -9,14 +11,14 @@ CREATE TABLE simple1(k UInt64,s String,INDEX af (s) TYPE gin(2) GRANULARITY 1)
 INSERT INTO simple1 VALUES (101, 'Alick a01'), (102, 'Blick a02'), (103, 'Click a03'),(104, 'Dlick a04'),(105, 'Elick a05'),(106, 'Alick a06'),(107, 'Blick a07'),(108, 'Click a08'),(109, 'Dlick a09'),(110, 'Elick b10'),(111, 'Alick b01'),(112, 'Blick b02'),(113, 'Click b03'),(114, 'Dlick b04'),(115, 'Elick b05'),(116, 'Alick b06'),(117, 'Blick b07'),(118, 'Click b08'),(119, 'Dlick b09'),(120, 'Elick b10');
 -- check gin index was created
 SELECT name, type FROM system.data_skipping_indices where (table =='simple1') limit 1;
+
 -- search gin index
-
 SELECT * FROM simple1 WHERE s LIKE '%01%';
-
+SYSTEM FLUSH LOGS;
 -- check the query only read 2 granules (4 rows total; each granule has 2 rows)
 SELECT read_rows, result_rows from system.query_log 
         where   query_kind ='Select' and 
-                startsWith(query, 'SELECT * FROM simple1 WHERE s LIKE \'%01%\'') 
+                endsWith(trimRight(query), 'SELECT * FROM simple1 WHERE s LIKE \'%01%\';') 
                 and result_rows==2 limit 1;
 
 -- create table for gin()
@@ -32,11 +34,11 @@ INSERT INTO simple2 VALUES (101, 'Alick a01'), (102, 'Blick a02'), (103, 'Click 
 SELECT name, type FROM system.data_skipping_indices where (table =='simple2') limit 1;
 -- search gin index
 SELECT * FROM simple2 WHERE hasToken(s, 'Alick');
-
+SYSTEM FLUSH LOGS;
 -- check the query only read 4 granules (8 rows total; each granule has 2 rows)
 SELECT read_rows, result_rows from system.query_log 
     where query_kind ='Select' 
-        and startsWith(query, 'SELECT * FROM simple2 WHERE hasToken(s, \'Alick\')') 
+        and endsWith(trimRight(query), 'SELECT * FROM simple2 WHERE hasToken(s, \'Alick\');') 
         and result_rows==4 limit 1;
 
 -- create table for gin(2) with two parts
@@ -51,13 +53,12 @@ INSERT INTO simple3 VALUES (201, 'rick c01'), (202, 'mick c02'),(203, 'nick c03'
 -- check gin index was created
 SELECT name, type FROM system.data_skipping_indices where (table =='simple3') limit 1;
 -- search gin index
-
 SELECT * FROM simple3 WHERE s LIKE '%01%';
-
+SYSTEM FLUSH LOGS;
 -- check the query only read 3 granules (6 rows total; each granule has 2 rows)
 SELECT read_rows, result_rows from system.query_log 
     where query_kind ='Select' 
-        and startsWith(query, 'SELECT * FROM simple3 WHERE s LIKE \'%01%\'') 
+        and endsWith(trimRight(query), 'SELECT * FROM simple3 WHERE s LIKE \'%01%\';') 
         and result_rows==3 limit 1;
 
 -- create table for gin(2) for utf8 string test
@@ -70,8 +71,9 @@ INSERT INTO simple4 VALUES (101, 'Alick 好'),(102, 'clickhouse你好'), (103, '
 SELECT name, type FROM system.data_skipping_indices where (table =='simple4') limit 1;
 -- search gin index
 SELECT * FROM simple4 WHERE s LIKE '%你好%';
+SYSTEM FLUSH LOGS;
 -- check the query only read 1 granule (2 rows total; each granule has 2 rows)
-SELECT read_rows, result_rows from system.query_log where query_kind ='Select' and startsWith(query, 'SELECT * FROM simple4 WHERE s LIKE \'%你好%\'') and result_rows==1 limit 1;
+SELECT read_rows, result_rows from system.query_log where query_kind ='Select' and endsWith(trimRight(query), 'SELECT * FROM simple4 WHERE s LIKE \'%你好%\';') and result_rows==1 limit 1;
 
 -- create table with 1000000 rows
 DROP TABLE IF EXISTS hextable;
@@ -88,6 +90,7 @@ CREATE TABLE hextable(k UInt64,s String,INDEX af(s) TYPE gin(0) GRANULARITY 1)
 SELECT name, type FROM system.data_skipping_indices where (table =='hextable') limit 1;
 -- search gin index
 select * from hextable where hasToken(s, '2D2D2D2D2D2D1540');
+SYSTEM FLUSH LOGS;
 -- check the query only read 3 granules (6 rows total; each granule has 2 rows)
 SELECT read_rows, result_rows from system.query_log 
     where query_kind ='Select' 
