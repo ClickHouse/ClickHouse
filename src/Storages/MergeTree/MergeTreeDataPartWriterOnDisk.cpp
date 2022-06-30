d6b9bf2070a141a95e42b3500311da2b49e7de16
HarryLeeIBM <hleeatwork@outlook.com>
2022-06-30 10:48:07 -0700

    Fix coding styles

--- src/Storages/MergeTree/MergeTreeDataPartWriterOnDisk.cpp
+++ src/Storages/MergeTree/MergeTreeDataPartWriterOnDisk.cpp
@@ -287,7 +287,7 @@ void MergeTreeDataPartWriterOnDisk::calculateAndSerializeSkipIndices(const Block
             }
             store = it->second;
         }
-    
+
         for (const auto & granule : granules_to_write)
         {
             if (skip_index_accumulated_marks[i] == index_helper->index.granularity)
