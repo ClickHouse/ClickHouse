0a9e046247df52950d8eb96a6b685c5e997e8ab6
alesapin <alesapin@clickhouse.com>
2022-07-01 16:04:20 +0200

    Update src/Disks/ObjectStorages/HDFS/registerDiskHDFS.cpp
    Co-authored-by: Dmitry Novik <n0vik@clickhouse.com>

--- src/Disks/ObjectStorages/HDFS/registerDiskHDFS.cpp
+++ src/Disks/ObjectStorages/HDFS/registerDiskHDFS.cpp
@@ -43,7 +43,7 @@ void registerDiskHDFS(DiskFactory & factory)
         auto metadata_storage = std::make_shared<MetadataStorageFromDisk>(metadata_disk, uri);
         uint64_t copy_thread_pool_size = config.getUInt(config_prefix + ".thread_pool_size", 16);
 
-        std::shared_ptr<IDisk> disk_result = std::make_shared<DiskObjectStorage>(
+        auto disk_result = std::make_shared<DiskObjectStorage>(
             name,
             uri,
             "DiskHDFS",
