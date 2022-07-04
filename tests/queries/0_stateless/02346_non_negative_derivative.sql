7d6b7f26124b6336a501d37f456061ae1e934b07
zvonand <azvonov@altinity.com>
2022-07-04 11:23:37 +0300

    updated tests

--- tests/queries/0_stateless/02346_non_negative_derivative.sql
+++ tests/queries/0_stateless/02346_non_negative_derivative.sql
@@ -7,7 +7,7 @@ CREATE TABLE nnd
 ENGINE=MergeTree()
 ORDER BY id;
 
-INSERT INTO nnd VALUES (1, toDateTime64('1979-12-12 21:21:21.123', 3, 'UTC'), 1.1), (2, toDateTime64('1979-12-12 21:21:21.124', 3, 'UTC'), 2.34), (3, toDateTime64('1979-12-12 21:21:21.127', 3, 'UTC'), 3.7);
+INSERT INTO nnd VALUES (1, toDateTime64('1979-12-12 21:21:21.123456788', 9, 'UTC'), 1.1), (2, toDateTime64('1979-12-12 21:21:21.123456789', 9, 'UTC'), 2.34), (3, toDateTime64('1979-12-12 21:21:21.127', 3, 'UTC'), 3.7);
 INSERT INTO nnd VALUES (4, toDateTime64('1979-12-12 21:21:21.129', 3, 'UTC'), 2.1), (5, toDateTime('1979-12-12 21:21:22', 'UTC'), 1.3345), (6, toDateTime('1979-12-12 21:21:23', 'UTC'), 1.54), (7, toDateTime('1979-12-12 21:21:23', 'UTC'), 1.54);
 
 -- shall work for precise intervals
