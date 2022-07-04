3eb97313d8a051ed253bfc6ebc08577883f93377
avogar <pav.cruglov@yandex.ru>
2022-07-04 14:46:10 +0000

    Allow null modifier in columns declaration for table functions

---
+++ tests/queries/0_stateless/02323_null_modifier_in_table_function.sql
@@ -0,0 +1,5 @@
+select * from values('x UInt8 NOT NULL', 1);
+select * from values('x UInt8 NULL', NULL);
+insert into function file(data_02323.tsv) select number % 2 ? number : NULL from numbers(3) settings engine_file_truncate_on_insert=1;
+select * from file(data_02323.tsv, auto, 'x UInt32 NOT NULL');
+select * from file(data_02323.tsv, auto, 'x UInt32 NULL');
