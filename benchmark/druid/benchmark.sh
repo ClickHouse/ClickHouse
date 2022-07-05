5c54a259df474cda033bee165d74dc7b1d349187
Ilya Yatsishin <2159081+qoega@users.noreply.github.com>
2022-07-05 09:39:05 +0000

    Disable query caching in Druid

--- benchmark/druid/benchmark.sh
+++ benchmark/druid/benchmark.sh
@@ -15,6 +15,10 @@ tar xf apache-druid-${VERSION}-bin.tar.gz
 # Have to increase indexer memory limit
 sed -i 's MaxDirectMemorySize=1g MaxDirectMemorySize=5g g' apache-druid-$VERSION/conf/druid/single-server/medium/middleManager/runtime.properties
 
+# Disable cache to test query performance
+sed -i 's druid.historical.cache.useCache=true druid.historical.cache.useCache=false g' apache-druid-$VERSION/conf/druid/single-server/medium/historical/runtime.properties
+sed -i 's druid.historical.cache.populateCache=true druid.historical.cache.populateCache=false g' apache-druid-$VERSION/conf/druid/single-server/medium/historical/runtime.properties
+
 # Druid launcher does not start Druid as a daemon. Run it in background
 ./apache-druid-${VERSION}/bin/start-single-server-medium &
 
@@ -22,9 +26,7 @@ sed -i 's MaxDirectMemorySize=1g MaxDirectMemorySize=5g g' apache-druid-$VERSION
 
 wget --continue 'https://datasets.clickhouse.com/hits_compatible/hits.tsv.gz'
 gzip -d hits.tsv.gz
-split --additional-suffix .tsv --verbose -n l/10 hits.tsv input
 
-# Running 10 tasks one by one to make it work in parallel
 ./apache-druid-${VERSION}/bin/post-index-task --file ingest.json --url http://localhost:8081
 
 # Run the queries
