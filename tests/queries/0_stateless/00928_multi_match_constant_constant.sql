2059f68a7d72804f2dff945685be2b59ddf37b73
Robert Schulze <robert@clickhouse.com>
2022-07-05 13:47:48 +0200

    Fix 00928_multi_match_constant_constant.sql
    Difficult to test because x86 and ARM behave differently due to
    vectorscan not available everyehwere ... The same stuff
    (multiFuzzyMatch) is tested elsewhere.

--- tests/queries/0_stateless/00928_multi_match_constant_constant.sql
+++ tests/queries/0_stateless/00928_multi_match_constant_constant.sql
@@ -1,12 +1,3 @@
--- Tags: no-fasttest
-
 SELECT multiMatchAny('goodbye', ['^hello[, ]+world$', 'go+d *bye', 'w(or)+ld']);
-SELECT multiFuzzyMatchAny('goodbye', 1, ['^hello[, ]+world$', 'go+d *bye', 'w(or)+ld']);
-
 SELECT multiMatchAnyIndex('goodbye', ['^hello[, ]+world$', 'go+d *bye', 'w(or)+ld']);
-SELECT multiFuzzyMatchAnyIndex('goodbye', 1, ['^hello[, ]+world$', 'go+d *bye', 'w(or)+ld']);
-
-SELECT multiMatchAllIndices('goodbye', ['^hello[, ]+world$', 'go+d *bye', 'w(or)+ld']);
-SELECT multiFuzzyMatchAllIndices('goodbye', 1, ['^hello[, ]+world$', 'go+d *bye', 'w(or)+ld']);
-
 SELECT multiSearchAllPositions('hello, world', ['hello', 'world']);
