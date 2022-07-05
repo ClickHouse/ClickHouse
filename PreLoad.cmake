dc28dfe57e836d87a3fdaf354aaab4c42cb66da4
Azat Khuzhin <a3at.mail@gmail.com>
2022-07-05 17:02:34 +0300

    Do not override compiler if it had been already set
    Before this patch:
    
        cmake -DCMAKE_C_COMPILER=foo .. # will use foo
        cmake .. # will use clang
    
    After:
    
        cmake -DCMAKE_C_COMPILER=foo .. # will use foo
        cmake .. # will use foo
    
    Signed-off-by: Azat Khuzhin <a.khuzhin@semrush.com>

--- PreLoad.cmake
+++ PreLoad.cmake
@@ -62,9 +62,10 @@ execute_process(COMMAND uname -m OUTPUT_VARIABLE ARCH)
 # By default, prefer clang on Linux
 # But note, that you still may change the compiler with -DCMAKE_C_COMPILER/-DCMAKE_CXX_COMPILER.
 if (OS MATCHES "Linux"
-    # some build systems may use CC/CXX env variables
     AND "$ENV{CC}" STREQUAL ""
-    AND "$ENV{CXX}" STREQUAL "")
+    AND "$ENV{CXX}" STREQUAL ""
+    AND NOT DEFINED CMAKE_C_COMPILER
+    AND NOT DEFINED CMAKE_CXX_COMPILER)
     find_program(CLANG_PATH clang)
     if (CLANG_PATH)
         set(CMAKE_C_COMPILER "clang" CACHE INTERNAL "")
