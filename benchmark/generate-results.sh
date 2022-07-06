13889be1d446a1264c414678b19e371b5201d486
Alexey Milovidov <milovidov@clickhouse.com>
2022-07-07 00:28:56 +0200

    Change generator for a new page

--- benchmark/generate-results.sh
+++ benchmark/generate-results.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
 
-ls -1 */results/*.txt | while read file; do SYSTEM=$(echo "$file" | grep -oP '^[\w-]+'); SETUP=$(echo "$file" | sed -r -e 's/^.*\/([a-zA-Z0-9_.-]+)\.txt$/\1/'); echo "[{\"system\": \"${SYSTEM} (${SETUP})\", \"version\": \"\", \"data_size\": 100000000, \"time\": \"2022-07-01 00:00:00\", \"comments\": \"\", \"result\": [
+ls -1 */results/*.txt | while read file
+do
+  SYSTEM=$(echo "$file" | grep -oP '^[\w-]+');
+  SETUP=$(echo "$file" | sed -r -e 's/^.*\/([a-zA-Z0-9_.-]+)\.txt$/\1/');
+
+  echo "{\"system\": \"${SYSTEM}\", \"machine\": \"${SETUP}\", \"time\": \"$(git log -1 --pretty="format:%cs" $file)\", \"result\": [
 $(grep -P '^\[.+\]' $file)
-]}]" > ../website/benchmark/dbms/results/${SYSTEM}.${SETUP}.json; done
+]},"
+done
