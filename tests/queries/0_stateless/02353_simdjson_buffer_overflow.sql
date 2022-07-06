4b3020c9a539df08cf6571944a8f770833e4ab13
Alexey Milovidov <milovidov@clickhouse.com>
2022-07-07 00:33:13 +0200

    Add a test for simdjson

---
+++ tests/queries/0_stateless/02353_simdjson_buffer_overflow.sql
@@ -0,0 +1,4 @@
+SET max_execution_time = 3;
+SET timeout_overflow_mode = 'break';
+
+SELECT count() FROM system.numbers_mt WHERE NOT ignore(JSONExtract('{' || repeat('"a":"b",', rand() % 10) || '"c":"d"}', 'a', 'String')) FORMAT Null;
