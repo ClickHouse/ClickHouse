8f1fadd054ddd4b9c5a1e1924cd489db5c91e82c
Frank Chen <frank.chen021@outlook.com>
2022-07-07 17:42:53 +0800

    Remove useless span

--- src/Interpreters/InterpreterFactory.cpp
+++ src/Interpreters/InterpreterFactory.cpp
@@ -112,8 +112,6 @@ namespace ErrorCodes
 
 std::unique_ptr<IInterpreter> InterpreterFactory::get(ASTPtr & query, ContextMutablePtr context, const SelectQueryOptions & options)
 {
-    OpenTelemetrySpanHolder span("InterpreterFactory::get()");
-
     ProfileEvents::increment(ProfileEvents::Query);
 
     if (query->as<ASTSelectQuery>())
