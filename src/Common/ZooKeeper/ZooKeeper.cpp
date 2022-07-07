7630fc76aebb6f6261812def7e73ea2afbae4fcc
Alexander Tokmakov <tavplubix@clickhouse.com>
2022-07-07 17:16:42 +0200

    avoid weird exception

--- src/Common/ZooKeeper/ZooKeeper.cpp
+++ src/Common/ZooKeeper/ZooKeeper.cpp
@@ -885,7 +885,7 @@ void ZooKeeper::waitForEphemeralToDisappearIfAny(const std::string & path)
     if (!tryGet(path, content, nullptr, eph_node_disappeared))
         return;
 
-    int32_t timeout_ms = 2 * session_timeout_ms;
+    int32_t timeout_ms = 3 * session_timeout_ms;
     if (!eph_node_disappeared->tryWait(timeout_ms))
         throw DB::Exception(DB::ErrorCodes::LOGICAL_ERROR,
                             "Ephemeral node {} still exists after {}s, probably it's owned by someone else. "
