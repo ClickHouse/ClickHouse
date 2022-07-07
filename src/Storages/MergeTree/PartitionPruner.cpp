19bd3d8d9ffd415f198cd575f30eb0d0e2e9ffe9
Amos Bird <amosbird@gmail.com>
2022-07-07 16:02:38 +0800

    Nullable partition pruning fix

--- src/Storages/MergeTree/PartitionPruner.cpp
+++ src/Storages/MergeTree/PartitionPruner.cpp
@@ -15,9 +15,21 @@ bool PartitionPruner::canBePruned(const DataPart & part)
     {
         const auto & partition_value = part.partition.value;
         std::vector<FieldRef> index_value(partition_value.begin(), partition_value.end());
+        for (auto & field : index_value)
+        {
+            // NULL_LAST
+            if (field.isNull())
+                field = POSITIVE_INFINITY;
+        }
         is_valid = partition_condition.mayBeTrueInRange(
             partition_value.size(), index_value.data(), index_value.data(), partition_key.data_types);
         partition_filter_map.emplace(partition_id, is_valid);
+        if (!is_valid)
+        {
+            WriteBufferFromOwnString buf;
+            part.partition.serializeText(part.storage, buf, FormatSettings{});
+            LOG_TRACE(&Poco::Logger::get("PartitionPruner"), "Partition {} gets pruned", buf.str());
+        }
     }
     return !is_valid;
 }
