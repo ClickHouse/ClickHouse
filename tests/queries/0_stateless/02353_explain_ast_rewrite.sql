a54033b749aab4c794c7b05a2c7d3db8aac7d656
Igor Nikonov <igor@clickhouse.com>
2022-07-07 20:31:42 +0000

    EXPLAIN AST rewrite: additional query in test

--- tests/queries/0_stateless/02353_explain_ast_rewrite.sql
+++ tests/queries/0_stateless/02353_explain_ast_rewrite.sql
@@ -1,4 +1,6 @@
 -- { echoOn }
 EXPLAIN AST rewrite=0 SELECT * FROM numbers(0);
 EXPLAIN AST rewrite=1 SELECT * FROM numbers(0);
+EXPLAIN AST rewrite=0 SELECT countDistinct(number) FROM numbers(0);
+EXPLAIN AST rewrite=1 SELECT countDistinct(number) FROM numbers(0);
 -- { echoOff }
