ef4f8e73ba2db7181e7b3445c71f729bc2a7da1b
Alexey Milovidov <milovidov@clickhouse.com>
2022-07-07 09:19:07 +0200

    Convert results to JSON

--- benchmark/generate-results.sh
+++ benchmark/generate-results.sh
@@ -1,11 +1,7 @@
 #!/bin/bash
 
-ls -1 */results/*.txt | while read file
+ls -1 */results/*.json | while read file
 do
-  SYSTEM=$(echo "$file" | grep -oP '^[\w-]+');
-  SETUP=$(echo "$file" | sed -r -e 's/^.*\/([a-zA-Z0-9_.-]+)\.txt$/\1/');
-
-  echo "{\"system\": \"${SYSTEM}\", \"machine\": \"${SETUP}\", \"time\": \"$(git log -1 --pretty="format:%cs" $file)\", \"result\": [
-$(grep -P '^\[.+\]' $file)
-]},"
+    cat "${file}"
+    echo ','
 done
