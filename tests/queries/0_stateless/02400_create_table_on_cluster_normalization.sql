89bea0586c32a4964c51c93eee7765757db66eae
Alexander Tokmakov <tavplubix@clickhouse.com>
2022-07-07 23:59:15 +0200

    fix tests

--- tests/queries/0_stateless/02400_create_table_on_cluster_normalization.sql
+++ tests/queries/0_stateless/02400_create_table_on_cluster_normalization.sql
@@ -1,8 +1,6 @@
 -- Tags: no-replicated-database
 -- Tag no-replicated-database: ON CLUSTER is not allowed
 
-set distributed_ddl_entry_format_version=3;
-
 create table local_t_l5ydey on cluster test_shard_localhost (
     c_qv5rv INTEGER ,
     c_rutjs4 INTEGER ,
@@ -12,7 +10,7 @@ create table local_t_l5ydey on cluster test_shard_localhost (
 ) engine=ReplicatedMergeTree('/clickhouse/tables/test_{database}/{shard}/local_t_l5ydey', '{replica}');
 
 create table t_l5ydey on cluster test_shard_localhost as local_t_l5ydey
-    engine=Distributed('test_cluster', currentDatabase(),'local_t_l5ydey', rand());
+    engine=Distributed('test_shard_localhost', currentDatabase(),'local_t_l5ydey', rand());
 
 insert into local_t_l5ydey values (1, 2, 3, '4');
 insert into t_l5ydey values (5, 6, 7, '8');
