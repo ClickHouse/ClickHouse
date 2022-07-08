70fb05b601628142d0242754bfffff8fef0e224d
Azat Khuzhin <a3at.mail@gmail.com>
2022-07-08 11:34:48 +0300

    tests: use unique ZooKeeper path for Replicated tables
    v2: get back no-parallel to avoid leftover message
    Signed-off-by: Azat Khuzhin <a.khuzhin@semrush.com>

--- tests/queries/0_stateless/00620_optimize_on_nonleader_replica_zookeeper.sql
+++ tests/queries/0_stateless/00620_optimize_on_nonleader_replica_zookeeper.sql
@@ -1,4 +1,4 @@
--- Tags: replica, no-replicated-database, no-parallel
+-- Tags: replica, no-replicated-database
 -- Tag no-replicated-database: Fails due to additional replicas or shards
 
 -- The test is mostly outdated as now every replica is leader and can do OPTIMIZE locally.
@@ -6,8 +6,8 @@
 DROP TABLE IF EXISTS rename1;
 DROP TABLE IF EXISTS rename2;
 DROP TABLE IF EXISTS rename3;
-CREATE TABLE rename1 (p Int64, i Int64, v UInt64) ENGINE = ReplicatedReplacingMergeTree('/clickhouse/test/tables/rename', '1', v) PARTITION BY p ORDER BY i;
-CREATE TABLE rename2 (p Int64, i Int64, v UInt64) ENGINE = ReplicatedReplacingMergeTree('/clickhouse/test/tables/rename', '2', v) PARTITION BY p ORDER BY i;
+CREATE TABLE rename1 (p Int64, i Int64, v UInt64) ENGINE = ReplicatedReplacingMergeTree('/clickhouse/tables/{database}/rename', '1', v) PARTITION BY p ORDER BY i;
+CREATE TABLE rename2 (p Int64, i Int64, v UInt64) ENGINE = ReplicatedReplacingMergeTree('/clickhouse/tables/{database}/rename', '2', v) PARTITION BY p ORDER BY i;
 
 INSERT INTO rename1 VALUES (0, 1, 0);
 INSERT INTO rename1 VALUES (0, 1, 1);
