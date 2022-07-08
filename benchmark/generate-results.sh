a14a585f8e408362fd4e3e6083626a52f3da2cd7
Alexey Milovidov <milovidov@clickhouse.com>
2022-07-08 06:42:56 +0200

    Update generator

--- benchmark/generate-results.sh
+++ benchmark/generate-results.sh
@@ -1,7 +1,20 @@
-#!/bin/bash
+#!/bin/bash -e
 
-ls -1 */results/*.json | while read file
-do
-    cat "${file}"
-    echo ','
-done
+(
+    sed '/^const data = \[$/q' index.html
+
+    FIRST=1
+    ls -1 */results/*.json | while read file
+    do
+        [ "${FIRST}" = "0" ] && echo ','
+        cat "${file}"
+        FIRST=0
+    done
+
+    echo ']; // end of data'
+    sed '0,/^\]; \/\/ end of data$/d' index.html
+
+) > index.html.new
+
+mv index.html index.html.bak
+mv index.html.new index.html
