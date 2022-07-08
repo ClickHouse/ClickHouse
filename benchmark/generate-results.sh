372f2e08e24f2035b5e22830c3afc8ef29fdd052
Alexey Milovidov <milovidov@clickhouse.com>
2022-07-08 09:54:06 +0200

    Better generator

--- benchmark/generate-results.sh
+++ benchmark/generate-results.sh
@@ -9,8 +9,8 @@
     FIRST=1
     ls -1 */results/*.json | while read file
     do
-        [ "${FIRST}" = "0" ] && echo ','
-        cat "${file}"
+        [ "${FIRST}" = "0" ] && echo -n ','
+        jq --compact-output ". += {\"source\": \"${file}\"}" "${file}"
         FIRST=0
     done
 
