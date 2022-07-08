c2157ff536941e3b4f460a51336efe82cf4fa40f
Ilya Yatsishin <2159081+qoega@users.noreply.github.com>
2022-07-08 20:14:58 +0000

    Correclty process Druid errors. Add restart after each query as some queries leave degraded state of Druid

--- benchmark/druid/run.sh
+++ benchmark/druid/run.sh
@@ -3,14 +3,22 @@
 TRIES=3
 cat queries.sql | while read query; do
     sync
+    for i in $(seq 1 100); do
+        CHECK=$(curl -o /dev/null -w '%{http_code}' -s -XPOST -H'Content-Type: application/json' http://localhost:8888/druid/v2/sql/ -d @check.json })
+	if [[ "$CHECK" == "200" ]]; then
+	    break
+	fi
+	sleep 1
+    done
     echo 3 | sudo tee /proc/sys/vm/drop_caches >/dev/null
-
     echo -n "["
     for i in $(seq 1 $TRIES); do
-        echo "{\"query\":\"$query\"}"| sed -e 's EventTime __time g' | tr -d ';' > query.json
-        RES=$(curl -o /dev/null -s -w '%{time_total}\n' -XPOST -H'Content-Type: application/json' http://localhost:8888/druid/v2/sql/ -d @query.json)
-        [[ "$?" == "0" ]] && echo -n "${RES}" || echo -n "null"
+        echo "{\"query\":\"$query\", \"context\": {\"timeout\": 1000000} }"| sed -e 's EventTime __time g' | tr -d ';' > query.json
+	curl -o /dev/null -w '%{http_code} %{time_total}\n' -s -XPOST -H'Content-Type: application/json' http://localhost:8888/druid/v2/sql/ -d @query.json | awk '{ if($1=="200") printf $2; else printf "null"; }'
         [[ "$i" != $TRIES ]] && echo -n ", "
     done
     echo "],"
+    # Ugly hack to measure independently queries. Otherwise some queries make Druid degraded and results are incorrect. For example after Q13 even SELECT 1 works for 7 seconds
+    pkill -f historical
+    sleep 3
 done
