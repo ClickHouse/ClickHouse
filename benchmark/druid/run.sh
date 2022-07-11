cc8810e72ad17204723ede3973aa4facb00ad34c
Ilya Yatsishin <2159081+qoega@users.noreply.github.com>
2022-07-11 11:23:11 +0000

    Druid: check correct result in check query

--- benchmark/druid/run.sh
+++ benchmark/druid/run.sh
@@ -4,16 +4,16 @@ TRIES=3
 cat queries.sql | while read query; do
     sync
     for i in $(seq 1 100); do
-        CHECK=$(curl -o /dev/null -w '%{http_code}' -s -XPOST -H'Content-Type: application/json' http://localhost:8888/druid/v2/sql/ -d @check.json })
-	if [[ "$CHECK" == "200" ]]; then
+        CHECK=$(curl -o - -w '%{http_code}' -s -XPOST -H'Content-Type: application/json' http://localhost:8888/druid/v2/sql/ -d @check.json })
+	if [[ "$CHECK" == "[{\"EXPR\$0\":99997497}]"$'\n'"200" ]]; then
 	    break
 	fi
 	sleep 1
     done
     echo 3 | sudo tee /proc/sys/vm/drop_caches >/dev/null
+    echo "{\"query\":\"$query\", \"context\": {\"timeout\": 1000000} }"| sed -e 's EventTime __time g' | tr -d ';' > query.json
     echo -n "["
     for i in $(seq 1 $TRIES); do
-        echo "{\"query\":\"$query\", \"context\": {\"timeout\": 1000000} }"| sed -e 's EventTime __time g' | tr -d ';' > query.json
 	curl -o /dev/null -w '%{http_code} %{time_total}\n' -s -XPOST -H'Content-Type: application/json' http://localhost:8888/druid/v2/sql/ -d @query.json | awk '{ if($1=="200") printf $2; else printf "null"; }'
         [[ "$i" != $TRIES ]] && echo -n ", "
     done
