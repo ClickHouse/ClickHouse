ffa5ef6293fd4668054b80005654eb96273255c3
Dmitry Novik <n0vik@clickhouse.com>
2022-07-12 00:14:41 +0000

    Update tests

--- tests/queries/0_stateless/02343_group_by_use_nulls.sql
+++ tests/queries/0_stateless/02343_group_by_use_nulls.sql
@@ -48,14 +48,3 @@ GROUP BY
     )
 ORDER BY (number, number % 2, val)
 SETTINGS group_by_use_nulls = 0;
-
-SELECT
-    CounterID AS k,
-    quantileBFloat16(0.5)(ResolutionWidth)
-FROM remote('127.0.0.{1,2}', datasets, hits_v1)
-GROUP BY k
-ORDER BY
-    count() DESC,
-    CounterID ASC
-LIMIT 10
-SETTINGS group_by_use_nulls = 1;
