397d05eb9c1e3e5630221ae82938765864bc4c58
Arthur Passos <arthur.ti@outlook.com>
2022-07-12 14:31:33 -0300

    Add DNSPTRResolverProvider docs

--- src/Common/DNSPTRResolverProvider.h
+++ src/Common/DNSPTRResolverProvider.h
@@ -4,6 +4,11 @@
 #include "DNSPTRResolver.h"
 
 namespace DB {
+    /*
+     * Provides a ready-to-use DNSPTRResolver instance.
+     * It hides 3rd party lib dependencies, handles initialization and lifetime.
+     * Since `get` function is static, it can be called from any context. Including cached static functions.
+     * */
     class DNSPTRResolverProvider
     {
     public:
