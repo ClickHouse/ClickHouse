9b92dc48dd7e2f4579f05c8a606cbbf221e99b91
Robert Schulze <robert@clickhouse.com>
2022-07-12 09:24:12 +0200

    Implement non-const needle arguments in multiSearchAllPositions

--- src/Functions/FunctionsMultiStringPosition.h
+++ src/Functions/FunctionsMultiStringPosition.h
@@ -27,22 +27,19 @@ namespace DB
 namespace ErrorCodes
 {
     extern const int ILLEGAL_TYPE_OF_ARGUMENT;
-    extern const int ILLEGAL_COLUMN;
-    extern const int NUMBER_OF_ARGUMENTS_DOESNT_MATCH;
 }
 
 
-template <typename Impl, typename Name>
+template <typename Impl>
 class FunctionsMultiStringPosition : public IFunction
 {
 public:
-    static constexpr auto name = Name::name;
+    static constexpr auto name = Impl::name;
 
     static FunctionPtr create(ContextPtr) { return std::make_shared<FunctionsMultiStringPosition>(); }
     String getName() const override { return name; }
     size_t getNumberOfArguments() const override { return 2; }
     bool useDefaultImplementationForConstants() const override { return true; }
-    ColumnNumbers getArgumentsThatAreAlwaysConstant() const override { return {1}; }
     bool isSuitableForShortCircuitArgumentsExecution(const DataTypesWithConstInfo & /*arguments*/) const override { return true; }
 
     DataTypePtr getReturnTypeImpl(const DataTypes & arguments) const override
@@ -67,49 +64,32 @@ public:
         const ColumnPtr & needles_ptr = arguments[1].column;
 
         const ColumnString * col_haystack_vector = checkAndGetColumn<ColumnString>(&*haystack_ptr);
+        assert(col_haystack_vector);
 
+        const ColumnArray * col_needles_vector = checkAndGetColumn<ColumnArray>(needles_ptr.get());
         const ColumnConst * col_needles_const = checkAndGetColumnConst<ColumnArray>(needles_ptr.get());
-
-        if (!col_needles_const)
-            throw Exception(
-                ErrorCodes::ILLEGAL_COLUMN,
-                "Illegal column {}. The array is not const", arguments[1].column->getName());
-
-        Array needles_arr = col_needles_const->getValue<Array>();
-
-        if (needles_arr.size() > std::numeric_limits<UInt8>::max())
-            throw Exception(
-                ErrorCodes::NUMBER_OF_ARGUMENTS_DOESNT_MATCH,
-                "Number of arguments for function {} doesn't match: passed {}, should be at most 255", getName(), needles_arr.size());
-
-        std::vector<std::string_view> needles;
-        needles.reserve(needles_arr.size());
-        for (const auto & el : needles_arr)
-            needles.emplace_back(el.get<String>());
-
-        const size_t column_haystack_size = haystack_ptr->size();
+        assert(static_cast<bool>(col_needles_vector) ^ static_cast<bool>(col_needles_const));
 
         using ResultType = typename Impl::ResultType;
         auto col_res = ColumnVector<ResultType>::create();
-        auto col_offsets = ColumnArray::ColumnOffsets::create(column_haystack_size);
+        auto col_offsets = ColumnArray::ColumnOffsets::create();
 
         auto & vec_res = col_res->getData();
         auto & offsets_res = col_offsets->getData();
+        /// the implementations are responsible for resizing the output column
 
-        vec_res.resize(column_haystack_size * needles.size());
-
-        if (col_haystack_vector)
-            Impl::vectorConstant(col_haystack_vector->getChars(), col_haystack_vector->getOffsets(), needles, vec_res);
+        if (col_needles_const)
+            Impl::vectorConstant(
+                col_haystack_vector->getChars(), col_haystack_vector->getOffsets(),
+                col_needles_const->getValue<Array>(),
+                vec_res, offsets_res);
         else
-            throw Exception(
-                    ErrorCodes::ILLEGAL_COLUMN,
-                    "Illegal column {}", arguments[0].column->getName());
+            Impl::vectorVector(
+                col_haystack_vector->getChars(), col_haystack_vector->getOffsets(),
+                col_needles_vector->getData(), col_needles_vector->getOffsets(),
+                vec_res, offsets_res);
 
-        size_t needles_size = needles.size();
-        size_t accum = needles_size;
-
-        for (size_t i = 0; i < column_haystack_size; ++i, accum += needles_size)
-            offsets_res[i] = accum;
+        // TODO: add comment about const const
 
         return ColumnArray::create(std::move(col_res), std::move(col_offsets));
     }
