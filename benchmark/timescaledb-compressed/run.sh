41715237ae79ef904d20d1a3d3cd43d4702917ce
Ilya Yatsishin <2159081+qoega@users.noreply.github.com>
2022-07-12 15:23:36 +0000

    Separate compress timescaledb test. Fix chmod to home - previous broke ssh. Non interactive install.

--- benchmark/timescaledb-compressed/run.sh
+++ benchmark/timescaledb-compressed/run.sh
@@ -8,6 +8,6 @@ cat queries.sql | while read query; do
 
     echo "$query";
     for i in $(seq 1 $TRIES); do
-        sudo -u postgres psql uncompressed -t -c '\timing' -c "$query" | grep 'Time'
+        psql test -t -c '\timing' -c "$query" | grep 'Time'
     done;
 done;
