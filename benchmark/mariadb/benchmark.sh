d10c665b716d6cde0a0bb8d804f63e70fd3faf1a
Alexey Milovidov <milovidov@clickhouse.com>
2022-07-12 11:31:23 +0200

    Add results for MariaDB

--- benchmark/mariadb/benchmark.sh
+++ benchmark/mariadb/benchmark.sh
@@ -19,3 +19,11 @@ time sudo mariadb test -e "LOAD DATA LOCAL INFILE 'hits.tsv' INTO TABLE hits"
 # 2:23:45 elapsed
 
 ./run.sh 2>&1 | tee log.txt
+
+sudo du -bcs /var/lib/mysql
+
+cat log.txt |
+  grep -P 'rows? in set|Empty set|^ERROR' |
+  sed -r -e 's/^ERROR.*$/null/; s/^.*?\((([0-9.]+) days? )?(([0-9.]+) hours? )?(([0-9.]+) min )?([0-9.]+) sec\).*?$/\2 \4 \6 \7/' |
+  awk '{ if ($4) { print $1 * 86400 + $2 * 3600 + $3 * 60 + $4 } else if ($3) { print $1 * 3600 + $2 * 60 + $3 } else if ($2) { print $1 * 60 + $2 } else { print $1 } }' |
+  awk '{ if (i % 3 == 0) { printf "[" }; printf $1; if (i % 3 != 2) { printf "," } else { print "]," }; ++i; }'
