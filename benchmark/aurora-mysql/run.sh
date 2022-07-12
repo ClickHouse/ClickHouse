18c81ff68b51dac615acc90026ac4e446e35e7e8
Alexey Milovidov <milovidov@clickhouse.com>
2022-07-12 20:20:58 +0200

    Move benchmark into separate repository

--- benchmark/aurora-mysql/run.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-TRIES=3
-
-cat queries.sql | while read query; do
-    for i in $(seq 1 $TRIES); do
-        mysql -h "${HOST}" -u admin --password="${PASSWORD}" test -vvv -e "${query}"
-    done;
-done;
