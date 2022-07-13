83c3670f2aafaeaa0a3462a6ac7f35ecaede478f
Nikolai Kochetov <nik-kochetov@yandex-team.ru>
2022-07-13 12:23:01 +0000

    Fix style

--- tests/queries/0_stateless/02357_query_cancellation_race.sh
+++ tests/queries/0_stateless/02357_query_cancellation_race.sh
@@ -6,4 +6,4 @@ CURDIR=$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)
 . "$CURDIR"/../shell_config.sh
 
 $CLICKHOUSE_CLIENT -q "create table tab (x UInt64, y String) engine = MergeTree order by x"
-for i in $(seq 1 100); do timeout -s 2 0.05 $CLICKHOUSE_CLIENT --interactive_delay 1000 -q "insert into tab select number, toString(number) from system.numbers" || true; done
+for _ in $(seq 1 100); do timeout -s 2 0.05 $CLICKHOUSE_CLIENT --interactive_delay 1000 -q "insert into tab select number, toString(number) from system.numbers" || true; done
