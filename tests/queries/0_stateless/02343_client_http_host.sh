d2bf27df58a19b5e8518f9507f9c52f1857cc3e8
bharatnc <bharatnc@gmail.com>
2022-07-13 22:33:18 -0700

    add a test for http_host field

---
+++ tests/queries/0_stateless/02343_client_http_host.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+
+CURDIR=$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)
+# shellcheck source=../shell_config.sh
+. "$CURDIR"/../shell_config.sh
+
+${CLICKHOUSE_CURL} -sS "${CLICKHOUSE_URL}" -d 'SELECT 1'  -H "Host: clickhouse.com"
+${CLICKHOUSE_CLIENT} --query "SYSTEM FLUSH LOGS"
+${CLICKHOUSE_CLIENT} --query "SELECT http_host FROM system.query_log WHERE http_host LIKE '%clickhouse%' LIMIT 1"
