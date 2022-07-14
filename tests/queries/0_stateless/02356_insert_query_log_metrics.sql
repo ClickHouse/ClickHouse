efb418a7e78d10b6aa6c1cd85fd25545ff6e679f
Yatsishin Ilya <2159081+qoega@users.noreply.github.com>
2022-07-14 16:30:59 +0000

    fix

--- tests/queries/0_stateless/02356_insert_query_log_metrics.sql
+++ tests/queries/0_stateless/02356_insert_query_log_metrics.sql
@@ -1,4 +1,4 @@
 CREATE TABLE 02356_destination (a Int64, b String) ENGINE = Memory;
 
 INSERT INTO 02356_destination (a, b) SELECT * FROM generateRandom('a Int64, b String') LIMIT 100 SETTINGS max_threads=1, max_block_size=100;
-SELECT read_rows = written_rows, read_rows = result_rows, read_bytes = written_bytes, read_bytes = result_bytes FROM system.query_log where normalized_query_hash = 1214411238725380014 and type='QueryFinish' FORMAT CSV;
+SELECT read_rows = written_rows, read_rows = result_rows, read_bytes = written_bytes, read_bytes = result_bytes FROM system.query_log where normalized_query_hash = 1214411238725380014 and type='QueryFinish' order by event_time desc limit 1 FORMAT CSV;
