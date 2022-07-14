ae554fc9d554e62a37dab44375e376620ed45612
Vasily Nemkov <V.Nemkov@gmail.com>
2022-07-14 20:54:40 +0300

    Disabled upstream's release workflow
    Starting a release from release_branches.yml
    Fixed error in build_report_check.py

--- tests/ci/build_report_check.py
+++ tests/ci/build_report_check.py
@@ -286,8 +286,6 @@ def main():
     if some_builds_are_missing:
         addition = f"({len(build_reports)} of {required_builds} builds are OK)"
 
-    description = f"{ok_builds}/{total_builds} builds are OK {addition}"
-
     description = f"{ok_groups}/{total_groups} artifact groups are OK {addition}"
 
     commit = get_commit(gh, pr_info.sha)
