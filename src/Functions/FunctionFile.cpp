6f5275d9bbe3b1dc062801e45bcb8bc597d0aa9a
Nikolay Degterinsky <evillique@gmail.com>
2022-07-15 10:08:14 +0000

    Fix build

--- src/Functions/FunctionFile.cpp
+++ src/Functions/FunctionFile.cpp
@@ -41,7 +41,7 @@ public:
 
     DataTypePtr getReturnTypeImpl(const ColumnsWithTypeAndName & arguments) const override
     {
-        if (arguments.size() < 1 || arguments.size() > 2)
+        if (arguments.empty() || arguments.size() > 2)
             throw Exception(
                 ErrorCodes::NUMBER_OF_ARGUMENTS_DOESNT_MATCH,
                 "Number of arguments for function {} doesn't match: passed {}, should be 1 or 2",
