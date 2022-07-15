cfa7433a4c5b6f57ab1270eceb1c2bc4524e5064
jinjunzh <jasper.zhu@intel.com>
2022-07-15 15:32:10 -0400

    fixed cosmetic issues

--- src/Compression/CompressionCodecDeflateQpl.h
+++ src/Compression/CompressionCodecDeflateQpl.h
@@ -19,9 +19,9 @@ public:
     DeflateQplJobHWPool();
     ~DeflateQplJobHWPool();
 
-    qpl_job * acquireJob(uint32_t * job_id);
+    qpl_job * acquireJob(UInt32 * job_id);
 
-    static qpl_job * releaseJob(uint32_t job_id);
+    static qpl_job * releaseJob(UInt32 job_id);
 
     static const bool & isJobPoolReady() { return job_pool_ready; }
 
@@ -30,17 +30,17 @@ public:
     static constexpr auto MAX_HW_JOB_NUMBER = 1024;
 
 private:
-    static bool tryLockJob(size_t index);
+    static bool tryLockJob(UInt32 index);
 
-    static void unLockJob(uint32_t index) { hw_job_ptr_locks[index].store(false); }
+    static void unLockJob(UInt32 index);
 
     class ReleaseJobObjectGuard
     {
-        uint32_t index;
+        UInt32 index;
         ReleaseJobObjectGuard() = delete;
 
     public:
-        ReleaseJobObjectGuard(const uint32_t index_) : index(index_){}
+        ReleaseJobObjectGuard(const UInt32 index_) : index(index_){}
 
         ~ReleaseJobObjectGuard(){ hw_job_ptr_locks[index].store(false); }
     };
@@ -60,8 +60,8 @@ class SoftwareCodecDeflateQpl
 {
 public:
     ~SoftwareCodecDeflateQpl();
-    uint32_t doCompressData(const char * source, uint32_t source_size, char * dest, uint32_t dest_size);
-    void doDecompressData(const char * source, uint32_t source_size, char * dest, uint32_t uncompressed_size);
+    UInt32 doCompressData(const char * source, UInt32 source_size, char * dest, UInt32 dest_size);
+    void doDecompressData(const char * source, UInt32 source_size, char * dest, UInt32 uncompressed_size);
 
 private:
     qpl_job * sw_job = nullptr;
@@ -73,12 +73,12 @@ class HardwareCodecDeflateQpl
 {
 public:
     /// RET_ERROR stands for hardware codec fail,need fallback to software codec.
-    static constexpr int32_t RET_ERROR = -1;
+    static constexpr Int32 RET_ERROR = -1;
 
     HardwareCodecDeflateQpl();
     ~HardwareCodecDeflateQpl();
-    int32_t doCompressData(const char * source, uint32_t source_size, char * dest, uint32_t dest_size) const;
-    int32_t doDecompressDataAsynchronous(const char * source, uint32_t source_size, char * dest, uint32_t uncompressed_size);
+    Int32 doCompressData(const char * source, UInt32 source_size, char * dest, UInt32 dest_size) const;
+    Int32 doDecompressDataAsynchronous(const char * source, UInt32 source_size, char * dest, UInt32 uncompressed_size);
     /// Flush result for previous asynchronous decompression requests.Must be used following with several calls of doDecompressDataReq.
     void flushAsynchronousDecompressRequests();
 
@@ -86,7 +86,7 @@ private:
     /// Asynchronous job map for decompression: job ID - job object.
     /// For each submission, push job ID && job object into this map;
     /// For flush, pop out job ID && job object from this map. Use job ID to release job lock and use job object to check job status till complete.
-    std::map<uint32_t, qpl_job *> decomp_async_job_map;
+    std::map<UInt32, qpl_job *> decomp_async_job_map;
     Poco::Logger * log;
 };
 
@@ -108,13 +108,13 @@ protected:
         return true;
     }
 
-    uint32_t doCompressData(const char * source, uint32_t source_size, char * dest) const override;
-    void doDecompressData(const char * source, uint32_t source_size, char * dest, uint32_t uncompressed_size) const override;
+    UInt32 doCompressData(const char * source, UInt32 source_size, char * dest) const override;
+    void doDecompressData(const char * source, UInt32 source_size, char * dest, UInt32 uncompressed_size) const override;
     ///Flush result for previous asynchronous decompression requests on asynchronous mode.
     void flushAsynchronousDecompressRequests() override;
 
 private:
-    uint32_t getMaxCompressedDataSize(uint32_t uncompressed_size) const override;
+    UInt32 getMaxCompressedDataSize(UInt32 uncompressed_size) const override;
     std::unique_ptr<HardwareCodecDeflateQpl> hw_codec;
     std::unique_ptr<SoftwareCodecDeflateQpl> sw_codec;
 };
