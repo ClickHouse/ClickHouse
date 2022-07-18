ab29e18b26fcf44874fc5f014fcd89970b8193f8
Alexander Gololobov <440544+davenger@users.noreply.github.com>
2022-07-18 12:11:31 +0200

    Speedup 02352_lightweight_delete test

--- tests/queries/0_stateless/02352_lightweight_delete.sql
+++ tests/queries/0_stateless/02352_lightweight_delete.sql
@@ -2,7 +2,7 @@ DROP TABLE IF EXISTS lwd_test;
 
 CREATE TABLE lwd_test (id UInt64 , value String) ENGINE MergeTree() ORDER BY id;
 
-INSERT INTO lwd_test SELECT number, randomString(10) FROM system.numbers LIMIT 10000000;
+INSERT INTO lwd_test SELECT number, randomString(10) FROM system.numbers LIMIT 1000000;
 
 SET mutations_sync = 1;
 SET allow_experimental_lightweight_delete_with_row_exists = 1;
@@ -13,9 +13,9 @@ SELECT 'Count', count() FROM lwd_test WHERE id >= 0;
 SELECT 'First row', id, length(value) FROM lwd_test ORDER BY id LIMIT 1;
 
 
-SELECT 'Delete 3M rows using lightweight DELETE';
+SELECT 'Delete 300K rows using lightweight DELETE';
 --ALTER TABLE lwd_test UPDATE _row_exists = 0 WHERE id < 3000000;
-DELETE FROM lwd_test WHERE id < 3000000;
+DELETE FROM lwd_test WHERE id < 300000;
 
 SELECT 'Rows in parts', SUM(rows) FROM system.parts WHERE database = currentDatabase() AND table = 'lwd_test' AND active;
 SELECT 'Count', count() FROM lwd_test WHERE id >= 0;
@@ -30,8 +30,8 @@ SELECT 'Count', count() FROM lwd_test WHERE id >= 0;
 SELECT 'First row', id, length(value) FROM lwd_test ORDER BY id LIMIT 1;
 
 
-SELECT 'Delete 3M more rows using lightweight DELETE';
-DELETE FROM lwd_test WHERE id < 6000000;
+SELECT 'Delete 300K more rows using lightweight DELETE';
+DELETE FROM lwd_test WHERE id < 600000;
 
 SELECT 'Rows in parts', SUM(rows) FROM system.parts WHERE database = currentDatabase() AND table = 'lwd_test' AND active;
 SELECT 'Count', count() FROM lwd_test WHERE id >= 0;
