0661ee94c3389b6ee33217c13206b52b1560ed69
Vitaly Baranov <vitlibar@clickhouse.com>
2022-07-18 14:25:14 +0200

    Auto set test's name in integration tests.

--- tests/integration/test_select_access_rights/test_main.py
+++ tests/integration/test_select_access_rights/test_main.py
@@ -3,7 +3,7 @@ from helpers.cluster import ClickHouseCluster
 from helpers.test_tools import TSV
 
 cluster = ClickHouseCluster(__file__)
-instance = cluster.add_instance("instance", name="main")
+instance = cluster.add_instance("instance")
 
 
 @pytest.fixture(scope="module", autouse=True)
