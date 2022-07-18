ff73f74612aa3b97bddbdcf88ce8cb0e72a57d35
Larry Luo <larry.luo@ca.ibm.com>
2022-07-18 15:56:57 -0400

    Add functional tests for tabular table summarize

---
+++ tests/queries/0_stateless/02366_kql_summarize.sql
@@ -0,0 +1,39 @@
+DROP TABLE IF EXISTS Customers;
+CREATE TABLE Customers
+(    
+    FirstName Nullable(String),
+    LastName String, 
+    Occupation String,
+    Education String,
+    Age Nullable(UInt8)
+) ENGINE = Memory;
+
+INSERT INTO Customers VALUES  ('Theodore','Diaz','Skilled Manual','Bachelors',28);
+INSERT INTO Customers VALUES  ('Stephanie','Cox','Management abcd defg','Bachelors',33);
+INSERT INTO Customers VALUES  ('Peter','Nara','Skilled Manual','Graduate Degree',26);
+INSERT INTO Customers VALUES  ('Latoya','Shen','Professional','Graduate Degree',25);
+INSERT INTO Customers VALUES  ('Joshua','Lee','Professional','Partial College',26);
+INSERT INTO Customers VALUES  ('Edward','Hernandez','Skilled Manual','High School',36);
+INSERT INTO Customers VALUES  ('Dalton','Wood','Professional','Partial College',42);
+INSERT INTO Customers VALUES  ('Christine','Nara','Skilled Manual','Partial College',33);
+INSERT INTO Customers VALUES  ('Cameron','Rodriguez','Professional','Partial College',28);
+INSERT INTO Customers VALUES  ('Angel','Stewart','Professional','Partial College',46);
+INSERT INTO Customers VALUES  ('Apple','','Skilled Manual','Bachelors',28);
+INSERT INTO Customers VALUES  (NULL,'why','Professional','Partial College',38);
+
+Select '-- test summarize --' ;
+set dialect='kusto';
+Customers | summarize count(), min(Age), max(Age), avg(Age), sum(Age);
+Customers | summarize count(), min(Age), max(Age), avg(Age), sum(Age) by Occupation;
+Customers | summarize countif(Age>40) by Occupation;
+Customers | summarize MyMax = maxif(Age, Age<40) by Occupation;
+Customers | summarize MyMin = minif(Age, Age<40) by Occupation;
+Customers | summarize MyAvg = avgif(Age, Age<40) by Occupation;
+Customers | summarize MySum = sumif(Age, Age<40) by Occupation;
+Customers | summarize dcount(Education, Occupation=='Professional');
+Customers | summarize dcountif(Education, Occupation=='Professional');
+Customers | summarize count() by bin(Age, 10)
+
+-- The following does not work
+-- arg_max()
+-- arg_min()
---
+++ tests/queries/0_stateless/02366_kql_tabular.sql
@@ -0,0 +1,133 @@
+DROP TABLE IF EXISTS Customers;
+CREATE TABLE Customers
+(    
+    FirstName Nullable(String),
+    LastName String, 
+    Occupation String,
+    Education String,
+    Age Nullable(UInt8)
+) ENGINE = Memory;
+
+INSERT INTO Customers VALUES ('Theodore','Diaz','Skilled Manual','Bachelors',28), ('Stephanie','Cox','Management','Bachelors',33), ('Peter','Nara','Skilled Manual','Graduate Degree',26), ('Latoya','Shen','Professional','Graduate Degree',25), ('Joshua','Lee','Professional','Partial College',26), ('Edward','Hernandez','Skilled Manual','High School',36), ('Dalton','Wood','Professional','Partial College',42), ('Christine','Nara','Skilled Manual','Partial College',33), ('Cameron','Rodriguez','Professional','Partial College',28), ('Angel','Stewart','Professional','Partial College',46);
+
+set dialect='clickhouse';
+Select '-- test Query only has table name: --';
+set dialect='kusto';
+Customers;
+
+set dialect='clickhouse';
+Select '-- Query has Column Selection --';
+set dialect='kusto';
+Customers | project FirstName,LastName,Occupation;
+
+set dialect='clickhouse';
+Select '-- Query has limit --';
+set dialect='kusto';
+Customers | project FirstName,LastName,Occupation | take 5;
+Customers | project FirstName,LastName,Occupation | limit 5;
+
+set dialect='clickhouse';
+Select '-- Query has second limit with bigger value --';
+set dialect='kusto';
+Customers | project FirstName,LastName,Occupation | take 5 | take 7;
+
+set dialect='clickhouse';
+Select '-- Query has second limit with smaller value --';
+set dialect='kusto';
+Customers | project FirstName,LastName,Occupation | take 5 | take 3;
+
+set dialect='clickhouse';
+Select '-- Query has second Column selection --';
+set dialect='kusto';
+Customers | project FirstName,LastName,Occupation | take 3 | project FirstName,LastName;
+
+set dialect='clickhouse';
+Select '-- Query has second Column selection with extra column --';
+set dialect='kusto';
+Customers| project FirstName,LastName,Occupation | take 3 | project FirstName,LastName,Education;
+
+-- set dialect='clickhouse';
+-- Select '-- Query with desc sort --';
+-- set dialect='kusto';
+-- Customers | project FirstName,LastName,Occupation | take 5 | sort by FirstName desc;
+-- Customers | project FirstName,LastName,Occupation | take 5 | order by Occupation desc;
+
+-- set dialect='clickhouse';
+-- Select '-- Query with asc sort --';
+-- set dialect='kusto';
+-- Customers | project FirstName,LastName,Occupation | take 5 | sort by Occupation asc;
+
+-- set dialect='clickhouse';
+-- Select '-- Query with sort (without keyword asc desc) --';
+-- set dialect='kusto';
+-- Customers | project FirstName,LastName,Occupation | take 5 | sort by FirstName;
+-- Customers | project FirstName,LastName,Occupation | take 5 | order by Occupation;
+
+-- set dialect='clickhouse';
+-- Select '-- Query with sort 2 Columns with different direction --';
+-- set dialect='kusto';
+-- Customers | project FirstName,LastName,Occupation | take 5 |sort by Occupation asc, LastName desc;
+
+-- set dialect='clickhouse';
+-- Select '-- Query with second sort --';
+-- set dialect='kusto';
+-- Customers | project FirstName,LastName,Occupation | take 5 |sort by Occupation desc |sort by Occupation asc;
+
+
+set dialect='clickhouse';
+Select '-- Test String Equals (==) --';
+set dialect='kusto';
+Customers | project FirstName,LastName,Occupation | where Occupation == 'Skilled Manual';
+
+set dialect='clickhouse';
+Select '-- Test String Not equals (!=) --';
+set dialect='kusto';
+Customers | project FirstName,LastName,Occupation | where Occupation != 'Skilled Manual';
+
+set dialect='clickhouse';
+Select '-- Test Filter using a list (in) --';
+set dialect='kusto';
+Customers | project FirstName,LastName,Occupation,Education | where Education in  ('Bachelors','High School');
+
+set dialect='clickhouse';
+Select '-- Test Filter using a list (!in) --';
+set dialect='kusto';
+Customers | project FirstName,LastName,Occupation,Education | where Education !in  ('Bachelors','High School');
+
+set dialect='clickhouse';
+Select '-- Test Filter using common string operations (contains_cs) --';
+set dialect='kusto';
+Customers | project FirstName,LastName,Occupation,Education | where Education contains_cs 'Coll';
+
+set dialect='clickhouse';
+Select '-- Test Filter using common string operations (startswith_cs) --';
+set dialect='kusto';
+Customers | project FirstName,LastName,Occupation,Education | where Occupation startswith_cs 'Prof';
+
+set dialect='clickhouse';
+Select '-- Test Filter using common string operations (endswith_cs) --';
+set dialect='kusto';
+Customers | project FirstName,LastName,Occupation,Education | where FirstName endswith_cs 'a';
+
+set dialect='clickhouse';
+Select '-- Test Filter using numerical equal (==) --';
+set dialect='kusto';
+Customers | project FirstName,LastName,Occupation,Education,Age | where Age == 26;
+
+set dialect='clickhouse';
+Select '-- Test Filter using numerical great and less (> , <) --';
+set dialect='kusto';
+Customers | project FirstName,LastName,Occupation,Education,Age | where Age > 30 and Age < 40;
+
+
+set dialect='clickhouse';
+Select '-- Test Filter using multi where --';
+set dialect='kusto';
+Customers | project FirstName,LastName,Occupation,Education,Age | where Age > 30 | where Occupation == 'Professional';
+
+-- TODO: verify the issue that order by can not be followed by other statements
+set dialect='clickhouse';
+Select '-- test sort, order --';
+set dialect='kusto';
+Customers | order by Age desc, FirstName asc;
+
