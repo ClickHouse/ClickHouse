37af9685481d623a06df2fb33350ae19f82976a4
Igor Nikonov <igor@clickhouse.com>
2022-07-19 12:40:50 +0000

    Rename option rewrite -> optimize

--- tests/queries/0_stateless/02353_explain_ast_rewrite.sql
+++
@@ -1,6 +0,0 @@
--- { echoOn }
-EXPLAIN AST rewrite=0 SELECT * FROM numbers(0);
-EXPLAIN AST rewrite=1 SELECT * FROM numbers(0);
-EXPLAIN AST rewrite=0 SELECT countDistinct(number) FROM numbers(0);
-EXPLAIN AST rewrite=1 SELECT countDistinct(number) FROM numbers(0);
--- { echoOff }
