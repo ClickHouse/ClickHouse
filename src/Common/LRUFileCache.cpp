f7bb69ef7abafe91652e68cb3d5129c5916bb2a8
Kseniia Sumarokova <54203879+kssenii@users.noreply.github.com>
2022-07-19 14:43:11 +0200

    Update LRUFileCache.cpp

--- src/Common/LRUFileCache.cpp
+++ src/Common/LRUFileCache.cpp
@@ -843,7 +843,9 @@ void LRUFileCache::loadCacheInfoIntoMemory(std::lock_guard<std::mutex> & cache_l
     if (!files.empty())
         throw Exception(
             REMOTE_FS_OBJECT_CACHE_ERROR, 
-            "Cache already initialized: this can be result of a first attempt during cache initialization. Please, check log for error messages");
+            "Cache initialization is partially made. "
+            "This can be a result of a first attempt to initialize cache. "
+            "Please, check log for error messages");
 
     fs::directory_iterator key_prefix_it{cache_base_path};
     for (; key_prefix_it != fs::directory_iterator(); ++key_prefix_it)
