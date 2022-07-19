b58f9adce9b9a36c8f83b55231e177582584d395
vdimir <vdimir@clickhouse.com>
2022-07-19 12:32:27 +0200

    Fix tests for cross_to_inner_join_rewrite

--- tests/queries/0_stateless/02364_setting_cross_to_inner_rewrite.sql
+++ tests/queries/0_stateless/02364_setting_cross_to_inner_rewrite.sql
@@ -11,8 +11,8 @@ INSERT INTO t2 VALUES ( 2 ), ( 3 ), ( 4 );
 
 SET cross_to_inner_join_rewrite = 1;
 SELECT count() = 1 FROM t1, t2 WHERE t1.x > t2.x;
-SELECT count() = 1 2ROM t1, t2 WHERE t1.x = t2.x;
-SELECT count() = 1 2ROM t1 CROSS JOIN t2 WHERE t1.x = t2.x;
+SELECT count() = 2 FROM t1, t2 WHERE t1.x = t2.x;
+SELECT count() = 2 FROM t1 CROSS JOIN t2 WHERE t1.x = t2.x;
 SELECT count() = 1 FROM t1 CROSS JOIN t2 WHERE t1.x > t2.x;
 
 SET cross_to_inner_join_rewrite = 2;
