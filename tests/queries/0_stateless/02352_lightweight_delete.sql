2388939fe97718f8b10e217df5d6f8f4b7212f38
Alexander Gololobov <440544+davenger@users.noreply.github.com>
2022-07-20 22:23:52 +0200

    Mix lightweight deletes with old-style alter-delete's

--- tests/queries/0_stateless/02352_lightweight_delete.sql
+++ tests/queries/0_stateless/02352_lightweight_delete.sql
@@ -13,9 +13,9 @@ SELECT 'Count', count() FROM lwd_test;
 SELECT 'First row', id, length(value) FROM lwd_test ORDER BY id LIMIT 1;
 
 
-SELECT 'Delete 300K rows using lightweight DELETE';
+SELECT 'Delete 100K rows using lightweight DELETE';
 --ALTER TABLE lwd_test UPDATE _row_exists = 0 WHERE id < 3000000;
-DELETE FROM lwd_test WHERE id < 300000;
+DELETE FROM lwd_test WHERE id < 100000;
 
 SELECT 'Rows in parts', SUM(rows) FROM system.parts WHERE database = currentDatabase() AND table = 'lwd_test' AND active;
 SELECT 'Count', count() FROM lwd_test;
@@ -30,8 +30,8 @@ SELECT 'Count', count() FROM lwd_test;
 SELECT 'First row', id, length(value) FROM lwd_test ORDER BY id LIMIT 1;
 
 
-SELECT 'Delete 300K more rows using lightweight DELETE';
-DELETE FROM lwd_test WHERE id < 600000;
+SELECT 'Delete 100K more rows using lightweight DELETE';
+DELETE FROM lwd_test WHERE id < 200000;
 
 SELECT 'Rows in parts', SUM(rows) FROM system.parts WHERE database = currentDatabase() AND table = 'lwd_test' AND active;
 SELECT 'Count', count() FROM lwd_test;
@@ -53,6 +53,32 @@ SELECT 'Rows in parts', SUM(rows) FROM system.parts WHERE database = currentData
 SELECT 'Count', count() FROM lwd_test;
 SELECT 'First row', id, length(value) FROM lwd_test ORDER BY id LIMIT 1;
 
+
+SELECT 'Delete 100K more rows using lightweight DELETE';
+DELETE FROM lwd_test WHERE id < 300000;
+
+SELECT 'Rows in parts', SUM(rows) FROM system.parts WHERE database = currentDatabase() AND table = 'lwd_test' AND active;
+SELECT 'Count', count() FROM lwd_test;
+SELECT 'First row', id, length(value) FROM lwd_test ORDER BY id LIMIT 1;
+
+
+SELECT 'Do ALTER DELETE mutation that does a "heavyweight" delete';
+ALTER TABLE lwd_test DELETE WHERE id % 3 == 0;
+
+SELECT 'Rows in parts', SUM(rows) FROM system.parts WHERE database = currentDatabase() AND table = 'lwd_test' AND active;
+SELECT 'Count', count() FROM lwd_test;
+SELECT 'First row', id, length(value) FROM lwd_test ORDER BY id LIMIT 1;
+
+SELECT 'Delete 100K more rows using lightweight DELETE';
+DELETE FROM lwd_test WHERE id >= 300000 and id < 400000;
+
+
+SELECT 'Force merge to cleanup deleted rows';
+OPTIMIZE TABLE lwd_test FINAL;
+
+SELECT 'Rows in parts', SUM(rows) FROM system.parts WHERE database = currentDatabase() AND table = 'lwd_test' AND active;
+SELECT 'Count', count() FROM lwd_test;
+SELECT 'First row', id, length(value) FROM lwd_test ORDER BY id LIMIT 1;
 
 
 DROP TABLE lwd_test;
