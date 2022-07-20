81cbd23b1336e629af203f4e1af426d95744eb25
Yong Wang <yong.wang@ibm.com>
2022-07-19 21:25:52 -0700

    Kusto-phase2 Fixed bug of Syntax error when Order By is followed by another statement

--- src/Parsers/Kusto/ParserKQLSort.cpp
+++ src/Parsers/Kusto/ParserKQLSort.cpp
@@ -32,7 +32,7 @@ bool ParserKQLSort :: parseImpl(Pos & pos, ASTPtr & node, Expected & expected)
         return false;
 
     pos = op_pos.back();
-    while (!pos->isEnd() && pos->type != TokenType::PipeMark)
+    while (!pos->isEnd() && pos->type != TokenType::PipeMark && pos->type != TokenType::Semicolon)
     {
         String tmp(new_pos->begin, new_pos->end);
         if (tmp == "desc" || tmp == "asc")
