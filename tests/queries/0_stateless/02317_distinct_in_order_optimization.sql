a3eb75becab03810d6c6ec5d4cc1254d2347e34b
Igor Nikonov <igor@clickhouse.com>
2022-07-20 22:26:17 +0000

    Test: compare result of DISTINCT with and w/o optimization

--- tests/queries/0_stateless/02317_distinct_in_order_optimization.sql
+++ tests/queries/0_stateless/02317_distinct_in_order_optimization.sql
@@ -44,3 +44,14 @@ select '-- distinct with non-key prefix and non-sorted column, order by non-sort
 select distinct b,c from distinct_in_order order by c desc;
 
 drop table if exists distinct_in_order sync;
+
+select '-- check that distinct with and w/o optimization produce the same result';
+drop table if exists distinct_in_order sync;
+drop table if exists ordinary_distinct sync;
+create table distinct_in_order (CounterID UInt32, EventDate Date) engine=MergeTree() order by (CounterID, EventDate);
+insert into distinct_in_order select distinct CounterID, EventDate from hits_v1 order by CounterID, EventDate settings optimize_distinct_in_order=1;
+create table ordinary_distinct (CounterID UInt32, EventDate Date) engine=MergeTree() order by (CounterID, EventDate);
+insert into ordinary_distinct select distinct CounterID, EventDate from hits_v1 settings optimize_distinct_in_order=0;
+select distinct * from distinct_in_order except select * from ordinary_distinct;
+drop table if exists distinct_in_order sync;
+drop table if exists ordinary_distinct sync;
