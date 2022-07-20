784ee115945761ca7fc1e8262891d6cc18728070
avogar <pav.cruglov@yandex.ru>
2022-07-20 11:16:25 +0000

    Add settings to skip fields with unsupported types in Protobuf/CapnProto schema inference

--- src/Core/Settings.h
+++ src/Core/Settings.h
@@ -686,6 +686,8 @@ static constexpr UInt64 operator""_GiB(unsigned long long value)
     M(Bool, input_format_csv_use_best_effort_in_schema_inference, true, "Use some tweaks and heuristics to infer schema in CSV format", 0) \
     M(Bool, input_format_tsv_use_best_effort_in_schema_inference, true, "Use some tweaks and heuristics to infer schema in TSV format", 0) \
     M(Bool, input_format_parquet_skip_columns_with_unsupported_types_in_schema_inference, false, "Allow to skip columns with unsupported types while schema inference for format Parquet", 0) \
+    M(Bool, input_format_protobuf_skip_fields_with_unsupported_types_in_schema_inference, false, "Allow to skip fields with unsupported types while schema inference for format Protobuf", 0) \
+    M(Bool, input_format_capn_proto_skip_fields_with_unsupported_types_in_schema_inference, false, "Allow to skip columns with unsupported types while schema inference for format CapnProto", 0) \
     M(Bool, input_format_orc_skip_columns_with_unsupported_types_in_schema_inference, false, "Allow to skip columns with unsupported types while schema inference for format ORC", 0) \
     M(Bool, input_format_arrow_skip_columns_with_unsupported_types_in_schema_inference, false, "Allow to skip columns with unsupported types while schema inference for format Arrow", 0) \
     M(String, column_names_for_schema_inference, "", "The list of column names to use in schema inference for formats without column names. The format: 'column1,column2,column3,...'", 0) \
--- tests/queries/0_stateless/format_schemas/02327_schema.capnp
+++ tests/queries/0_stateless/format_schemas/02327_schema.capnp
@@ -1,10 +1,11 @@
 @0x9ef128e10a8010b8;
 
-struct Nested1
+struct Empty
 {
 }
 
 struct Message
 {
-    tuple1 @0 : Nested1;
+    tuple1 @0 : Empty;
+    text @1 : Text;
 }
