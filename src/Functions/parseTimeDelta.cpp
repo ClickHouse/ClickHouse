4905f86e7fb82f2b0ee49df6de772906f10fb749
avogar <pav.cruglov@yandex.ru>
2022-07-21 08:55:51 +0000

    Extend time units in parseTimeDelta function

--- src/Functions/parseTimeDelta.cpp
+++ src/Functions/parseTimeDelta.cpp
@@ -18,7 +18,8 @@ namespace ErrorCodes
 
 namespace
 {
-    const std::unordered_map<std::string_view, Float64> time_unit_to_float = {
+    const std::unordered_map<std::string_view, Float64> time_unit_to_float =
+    {
         {"years", 365 * 24 * 3600},
         {"year", 365 * 24 * 3600},
         {"yr", 365 * 24 * 3600},
@@ -50,7 +51,25 @@ namespace
         {"second", 1},
         {"sec", 1},
         {"s", 1},
+
+        {"milliseconds", 1e-3},
+        {"millisecond", 1e-3},
+        {"millisec", 1e-3},
+        {"ms", 1e-3},
+
+        {"microseconds", 1e-6},
+        {"microsecond", 1e-6},
+        {"microsec", 1e-6},
+        {"μs", 1e-6},
+        {"us", 1e-6},
+
+        {"nanoseconds", 1e-9},
+        {"nanosecond", 1e-9},
+        {"nanosec", 1e-9},
+        {"ns", 1e-9},
     };
+
+    const std::unordered_set<char> separators = {' ', ';', '-', '+', ',', ':'};
 
     /** Prints amount of seconds in form of:
      * "1 year 2 months 4 weeks 12 days 3 hours 1 minute 33 seconds".
@@ -248,7 +267,7 @@ namespace
         static bool scanUnit(std::string_view & str, Int64 & index, Int64 last_pos)
         {
             int64_t begin_index = index;
-            while (index <= last_pos && isalpha(str[index]))
+            while (index <= last_pos && !isdigit(str[index]) && !separators.contains(str[index]))
             {
                 index++;
             }
@@ -271,8 +290,7 @@ namespace
             scanSpaces(str, index, last_pos);
 
             /// ignore separator
-            if (index <= last_pos
-                && (str[index] == ';' || str[index] == '-' || str[index] == '+' || str[index] == ',' || str[index] == ':'))
+            if (index <= last_pos && (separators.contains(str[index])))
             {
                 index++;
             }
