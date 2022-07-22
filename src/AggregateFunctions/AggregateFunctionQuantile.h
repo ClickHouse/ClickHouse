0e746c1afa01b520528d52e97078c41938779fc3
Amos Bird <amosbird@gmail.com>
2022-07-22 12:52:54 +0800

    More format refactor

--- src/AggregateFunctions/AggregateFunctionQuantile.h
+++ src/AggregateFunctions/AggregateFunctionQuantile.h
@@ -64,10 +64,9 @@ template <
     typename FloatReturnType,
     /// If true, the function will accept multiple parameters with quantile levels
     ///  and return an Array filled with many values of that quantiles.
-    bool returns_many
->
-class AggregateFunctionQuantile final : public IAggregateFunctionDataHelper<Data,
-    AggregateFunctionQuantile<Value, Data, Name, has_second_arg, FloatReturnType, returns_many>>
+    bool returns_many>
+class AggregateFunctionQuantile final
+    : public IAggregateFunctionDataHelper<Data, AggregateFunctionQuantile<Value, Data, Name, has_second_arg, FloatReturnType, returns_many>>
 {
 private:
     using ColVecType = ColumnVectorOrDecimal<Value>;
@@ -84,11 +83,14 @@ private:
 
 public:
     AggregateFunctionQuantile(const DataTypes & argument_types_, const Array & params)
-        : IAggregateFunctionDataHelper<Data, AggregateFunctionQuantile<Value, Data, Name, has_second_arg, FloatReturnType, returns_many>>(argument_types_, params)
-        , levels(params, returns_many), level(levels.levels[0]), argument_type(this->argument_types[0])
+        : IAggregateFunctionDataHelper<Data, AggregateFunctionQuantile<Value, Data, Name, has_second_arg, FloatReturnType, returns_many>>(
+            argument_types_, params)
+        , levels(params, returns_many)
+        , level(levels.levels[0])
+        , argument_type(this->argument_types[0])
     {
         if (!returns_many && levels.size() > 1)
-            throw Exception("Aggregate function " + getName() + " require one parameter or less", ErrorCodes::NUMBER_OF_ARGUMENTS_DOESNT_MATCH);
+            throw Exception(ErrorCodes::NUMBER_OF_ARGUMENTS_DOESNT_MATCH, "Aggregate function {} require one parameter or less", getName());
     }
 
     String getName() const override { return Name::name; }
@@ -139,9 +141,7 @@ public:
         }
 
         if constexpr (has_second_arg)
-            this->data(place).add(
-                value,
-                columns[1]->getUInt(row_num));
+            this->data(place).add(value, columns[1]->getUInt(row_num));
         else
             this->data(place).add(value);
     }
@@ -164,7 +164,6 @@ public:
 
     void insertResultInto(AggregateDataPtr __restrict place, IColumn & to, Arena *) const override
     {
-        /// const_cast is required because some data structures apply finalizaton (like sorting) for obtain a result.
         auto & data = this->data(place);
 
         if constexpr (returns_many)
@@ -210,7 +209,11 @@ public:
         {
             assertBinary(Name::name, types);
             if (!isUnsignedInteger(types[1]))
-                throw Exception("Second argument (weight) for function " + std::string(Name::name) + " must be unsigned integer, but it has type " + types[1]->getName(), ErrorCodes::ILLEGAL_TYPE_OF_ARGUMENT);
+                throw Exception(
+                    ErrorCodes::ILLEGAL_TYPE_OF_ARGUMENT,
+                    "Second argument (weight) for function {} must be unsigned integer, but it has type {}",
+                    Name::name,
+                    types[1]->getName());
         }
         else
             assertUnary(Name::name, types);
