3dff04f372a8717062d8205904cc9c13a40e1128
Vadim Volodin <vad.e.volodin@gmail.com>
2022-07-22 13:10:08 +0300

    remove exception for old partitions, add server configuration option to enable system unfreeze

--- src/Storages/Freeze.h
+++ src/Storages/Freeze.h
@@ -6,6 +6,10 @@
 namespace DB
 {
 
+namespace ErrorCodes {
+    extern const int SUPPORT_IS_DISABLED;
+}
+
 /// Special metadata used during freeze table. Required for zero-copy
 /// replication.
 struct FreezeMetaData
@@ -32,12 +36,19 @@ public:
 class Unfreezer
 {
 public:
-    Unfreezer(ContextPtr local_context) : local_context_(local_context) { zookeeper_ = local_context->getZooKeeper(); }
-    PartitionCommandsResultInfo unfreezePartitionsFromTableDirectory(MergeTreeData::MatcherFn matcher, const String & backup_name, const Disks & disks, const fs::path & table_directory, std::optional<MergeTreeDataFormatVersion> format_version);
+    Unfreezer(ContextPtr context) : local_context(context), zookeeper() { 
+        const auto & config = local_context->getConfigRef();
+        static constexpr auto config_key = "enable_system_unfreeze";
+        if (!config.getBool(config_key, false)) {
+            throw Exception(ErrorCodes::SUPPORT_IS_DISABLED, "Support for SYSTEM UNFREEZE query is disabled. You can enable it via '{}' server setting", config_key);
+        }
+        zookeeper = this->local_context->getZooKeeper(); 
+    }
+    PartitionCommandsResultInfo unfreezePartitionsFromTableDirectory(MergeTreeData::MatcherFn matcher, const String & backup_name, const Disks & disks, const fs::path & table_directory);
     BlockIO unfreeze(const String & backup_name);
 private:
-    ContextPtr local_context_;
-    zkutil::ZooKeeperPtr zookeeper_;
+    ContextPtr local_context;
+    zkutil::ZooKeeperPtr zookeeper;
     Poco::Logger * log = &Poco::Logger::get("Unfreezer");
     static constexpr std::string_view backup_directory_prefix = "shadow";
     static bool removeFreezedPart(DiskPtr disk, const String & path, const String & part_name, ContextPtr local_context,  zkutil::ZooKeeperPtr zookeeper);
