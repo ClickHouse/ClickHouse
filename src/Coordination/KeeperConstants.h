45bf3e2c82e0e7abb2ca479e0b8263267ea87b8c
Antonio Andelic <antonio@clickhouse.com>
2022-07-22 08:07:38 +0000

    Store only root system node

--- src/Coordination/KeeperConstants.h
+++ src/Coordination/KeeperConstants.h
@@ -1,10 +1,9 @@
 #pragma once
 
+#include <IO/WriteHelpers.h>
+
 namespace DB
 {
-
-const std::string keeper_system_path = "/keeper";
-const std::string keeper_api_version_path = keeper_system_path + "/api_version";
 
 enum class KeeperApiVersion : uint8_t
 {
@@ -13,5 +12,14 @@ enum class KeeperApiVersion : uint8_t
 };
 
 inline constexpr auto current_keeper_api_version = KeeperApiVersion::V1;
+
+const std::string keeper_system_path = "/keeper";
+const std::string keeper_api_version_path = keeper_system_path + "/api_version";
+
+using PathWithData = std::pair<std::string_view, std::string>;
+const std::vector<PathWithData> data_for_system_paths
+{
+    {keeper_api_version_path, toString(static_cast<uint8_t>(current_keeper_api_version))}
+};
 
 }
