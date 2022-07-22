4e995bce8f99e568b2c31a75a9cd9115ddc22390
avogar <pav.cruglov@yandex.ru>
2022-07-22 11:23:30 +0000

    Update tests

--- tests/queries/0_stateless/02367_cancel_write_into_s3.sh
+++ tests/queries/0_stateless/02367_cancel_write_into_s3.sh
@@ -7,18 +7,18 @@ CURDIR=$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)
 
 for i in $(seq 1 10);
 do
-    $CLICKHOUSE_CLIENT --query_id="02366_$i" -q "insert into function s3('http://localhost:11111/test/02367_data_$i.jsonl') select range(number % 1000) from numbers(100000) settings s3_truncate_on_insert=1, output_format_parallel_formatting=1" 2> /dev/null &
+    $CLICKHOUSE_CLIENT --query_id="02367_$i" -q "insert into function s3('http://localhost:11111/test/02367_data_$i.jsonl') select range(number % 1000) from numbers(100000) settings s3_truncate_on_insert=1, output_format_parallel_formatting=1" 2> /dev/null &
 done
 
 sleep 2
 
-$CLICKHOUSE_CLIENT -q "kill query where startsWith(query_id, '02367_') sync" > /dev/null
+$CLICKHOUSE_CLIENT -q "kill query where startsWith(query_id, '02367_') sync" | grep "finished" -c
 
 for i in $(seq 1 10);
 do
-    $CLICKHOUSE_CLIENT --query_id="02367_$i" -q "insert into function s3('http://localhost:11111/test/02366_data_$i.jsonl') select range(number % 1000) from numbers(100000) settings s3_truncate_on_inser=1, output_format_parallel_formatting=0" 2> /dev/null &
+    $CLICKHOUSE_CLIENT --query_id="02367_$i" -q "insert into function s3('http://localhost:11111/test/02367_data_$i.jsonl') select range(number % 1000) from numbers(100000) settings s3_truncate_on_insert=1, output_format_parallel_formatting=0" 2> /dev/null &
 done
 
 sleep 2
 
-$CLICKHOUSE_CLIENT -q "kill query where startsWith(query_id, '02367_') sync" > /dev/null
+$CLICKHOUSE_CLIENT -q "kill query where startsWith(query_id, '02367_') sync" | grep "finished" -c
