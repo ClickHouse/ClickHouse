3d03b2714b7f97942d18c069bba4c03828ea3983
Anton Popov <ap@clickhouse.com>
2022-07-22 23:05:11 +0000

    try to fix Nested

--- src/Storages/MergeTree/IMergeTreeReader.cpp
+++ src/Storages/MergeTree/IMergeTreeReader.cpp
@@ -42,12 +42,6 @@ IMergeTreeReader::IMergeTreeReader(
     , all_mark_ranges(all_mark_ranges_)
     , alter_conversions(storage.getAlterConversionsForPart(data_part))
 {
-    if (isWidePart(data_part))
-    {
-        /// For wide parts convert plain arrays of Nested to subcolumns
-        /// to allow to use shared offset column from cache.
-        columns = Nested::convertToSubcolumns(columns);
-    }
 }
 
 IMergeTreeReader::~IMergeTreeReader() = default;
