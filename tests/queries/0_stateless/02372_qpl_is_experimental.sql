b1da2eb9e63ad521f84244e237c33376a93879b6
Robert Schulze <robert@clickhouse.com>
2022-07-24 20:42:38 +0000

    Remove tests again ...
    They broke because QPL is compiled only when AVX2/512 is on. This is
    not the case in CI.

--- tests/queries/0_stateless/02372_qpl_is_experimental.sql
+++
@@ -1,9 +0,0 @@
-DROP TABLE IF EXISTS qpl_codec;
-
-CREATE TABLE qpl_codec (id Int32 CODEC(DEFLATE_QPL)) ENGINE = MergeTree() ORDER BY id; -- { serverError 36 }
-
-SET allow_experimental_codecs = 1;
-CREATE TABLE qpl_codec (id Int32 CODEC(DEFLATE_QPL)) ENGINE = MergeTree() ORDER BY id;
-
-DROP TABLE IF EXISTS qpl_codec;
-
