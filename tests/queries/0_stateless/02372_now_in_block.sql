4e36eeb139beeba8db08e11401043804526ca96b
Alexey Milovidov <milovidov@clickhouse.com>
2022-07-24 20:07:14 +0200

    More tests

--- tests/queries/0_stateless/02372_now_in_block.sql
+++ tests/queries/0_stateless/02372_now_in_block.sql
@@ -1,1 +1,4 @@
 SELECT count() FROM (SELECT DISTINCT nowInBlock(), nowInBlock('Pacific/Pitcairn') FROM system.numbers LIMIT 2);
+SELECT nowInBlock(1); -- { serverError 43 }
+SELECT nowInBlock(NULL) IS NULL;
+SELECT nowInBlock('UTC', 'UTC'); -- { serverError 42 }
