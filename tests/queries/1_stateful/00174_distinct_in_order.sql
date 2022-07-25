f574a00e7b42510696093ee0a4050323e4541832
Igor Nikonov <igor@clickhouse.com>
2022-07-25 07:56:50 +0000

    Fix test

--- tests/queries/1_stateful/00174_distinct_in_order.sql
+++ tests/queries/1_stateful/00174_distinct_in_order.sql
@@ -5,9 +5,9 @@ drop table if exists ordinary_distinct sync;
 
 select '-- DISTINCT colums are the same as in ORDER BY';
 create table distinct_in_order (CounterID UInt32, EventDate Date) engine=MergeTree() order by (CounterID, EventDate);
-insert into distinct_in_order select distinct CounterID, EventDate from hits_v1 order by CounterID, EventDate settings optimize_distinct_in_order=1;
+insert into distinct_in_order select distinct CounterID, EventDate from test.hits order by CounterID, EventDate settings optimize_distinct_in_order=1;
 create table ordinary_distinct (CounterID UInt32, EventDate Date) engine=MergeTree() order by (CounterID, EventDate);
-insert into ordinary_distinct select distinct CounterID, EventDate from hits_v1 order by CounterID, EventDate settings optimize_distinct_in_order=0;
+insert into ordinary_distinct select distinct CounterID, EventDate from test.hits order by CounterID, EventDate settings optimize_distinct_in_order=0;
 select distinct * from distinct_in_order except select * from ordinary_distinct;
 
 drop table if exists distinct_in_order sync;
@@ -15,9 +15,9 @@ drop table if exists ordinary_distinct sync;
 
 select '-- DISTINCT colums has prefix in ORDER BY columns';
 create table distinct_in_order (CounterID UInt32, EventDate Date) engine=MergeTree() order by (CounterID, EventDate);
-insert into distinct_in_order select distinct CounterID, EventDate from hits_v1 order by CounterID settings optimize_distinct_in_order=1;
+insert into distinct_in_order select distinct CounterID, EventDate from test.hits order by CounterID settings optimize_distinct_in_order=1;
 create table ordinary_distinct (CounterID UInt32, EventDate Date) engine=MergeTree() order by (CounterID, EventDate);
-insert into ordinary_distinct select distinct CounterID, EventDate from hits_v1 order by CounterID settings optimize_distinct_in_order=0;
+insert into ordinary_distinct select distinct CounterID, EventDate from test.hits order by CounterID settings optimize_distinct_in_order=0;
 select distinct * from distinct_in_order except select * from ordinary_distinct;
 
 drop table if exists distinct_in_order sync;
