8210e8c58c71bfe4c7ecfcc03fdc8861078022dd
fastio <pengjian.uestc@gmail.com>
2022-07-25 17:46:55 +0800

    fix test cases

--- tests/integration/test_auto_optimize_partitions/test.py
+++ tests/integration/test_auto_optimize_partitions/test.py
@@ -5,7 +5,7 @@ from helpers.cluster import ClickHouseCluster
 from helpers.test_tools import TSV
 
 cluster = ClickHouseCluster(__file__)
-node = cluster.add_instance("node", main_configs=["configs/zookeeper_config.xml"], with_zookeeper=True)
+node = cluster.add_instance("node", main_configs=["configs/zookeeper_config.xml", "configs/merge_tree.xml"], with_zookeeper=True)
 
 
 @pytest.fixture(scope="module")
@@ -26,50 +26,41 @@ def test_without_auto_optimize_merge_tree(start_cluster):
     node.query("INSERT INTO test SELECT 3")
 
 
-    expected = TSV('''all_0_0_0\t1\tall_1_1_0\t2\all_2_2_0\t3''')
-    assert TSV(node.query('SELECT _part, * FROM test')) == expected
+    time.sleep(5)
 
-    time.sleep(30)
-
-    expected = TSV('''all_0_0_0\t1\tall_1_1_0\t2\all_2_2_0\t3''')
-    assert TSV(node.query('SELECT _part, * FROM test')) == expected
+    expected = TSV('''3\n''')
+    assert TSV(node.query("SELECT count(*) FROM system.parts where table='test' and active=1")) == expected
 
     node.query("DROP TABLE test;")
 
 def test_auto_optimize_merge_tree(start_cluster):
     node.query(
-        "CREATE TABLE test (i Int64) ENGINE = MergeTree ORDER BY i SETTINGS auto_optimize_partition_after_seconds=30;"
+        "CREATE TABLE test (i Int64) ENGINE = MergeTree ORDER BY i SETTINGS auto_optimize_partition_after_seconds=5;"
     )
     node.query("INSERT INTO test SELECT 1")
     node.query("INSERT INTO test SELECT 2")
     node.query("INSERT INTO test SELECT 3")
 
 
-    expected = TSV('''all_0_0_0\t1\tall_1_1_0\t2\all_2_2_0\t3''')
-    assert TSV(node.query('SELECT _part, * FROM test')) == expected
+    time.sleep(10)
 
-    time.sleep(30)
-
-    expected = TSV('''all_0_2_1\t1\tall_0_2_1\t2\all_0_2_1\t3''')
-    assert TSV(node.query('SELECT _part, * FROM test')) == expected
+    expected = TSV('''1\n''')
+    assert TSV(node.query("SELECT count(*) FROM system.parts where table='test' and active=1")) == expected
 
     node.query("DROP TABLE test;")
 
 def test_auto_optimize_replicated_merge_tree(start_cluster):
     node.query(
-        "CREATE TABLE test (i Int64) ENGINE = ReplicatedMergeTree('/clickhouse/testing/test', 'node') ORDER BY i SETTINGS auto_optimize_partition_after_seconds=30;"
+        "CREATE TABLE test (i Int64) ENGINE = ReplicatedMergeTree('/clickhouse/testing/test', 'node') ORDER BY i SETTINGS auto_optimize_partition_after_seconds=5;"
     )
     node.query("INSERT INTO test SELECT 1")
     node.query("INSERT INTO test SELECT 2")
     node.query("INSERT INTO test SELECT 3")
 
 
-    expected = TSV('''all_0_0_0\t1\tall_1_1_0\t2\all_2_2_0\t3''')
-    assert TSV(node.query('SELECT _part, * FROM test')) == expected
+    time.sleep(10)
 
-    time.sleep(30)
-
-    expected = TSV('''all_0_2_1\t1\tall_0_2_1\t2\all_0_2_1\t3''')
-    assert TSV(node.query('SELECT _part, * FROM test')) == expected
+    expected = TSV('''1\n''')
+    assert TSV(node.query("SELECT count(*) FROM system.parts where table='test' and active=1")) == expected
 
     node.query("DROP TABLE test;")
