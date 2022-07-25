00711c637151a4d2cfc0471a5261b34c98655884
avogar <pav.cruglov@yandex.ru>
2022-07-25 15:05:12 +0000

    Update tests

--- tests/queries/0_stateless/02366_cancel_write_into_file.sh
+++ tests/queries/0_stateless/02366_cancel_write_into_file.sh
@@ -14,7 +14,6 @@ done
 sleep 2
 
 $CLICKHOUSE_CLIENT -q "kill query where startsWith(query_id, '02366_') sync" 2>&1 /dev/null
-$CLICKHOUSE_CLIENT -q "select demangle(addressToSymbol(arrayJoin(trace))) from system.stack_trace where startsWith(query_id, '02366_')" 2>&1 /dev/null
 
 for i in $(seq 1 10);
 do
@@ -24,5 +23,4 @@ done
 sleep 2
 
 $CLICKHOUSE_CLIENT -q "kill query where startsWith(query_id, '02366_') sync" 2>&1 /dev/null
-$CLICKHOUSE_CLIENT -q "select demangle(addressToSymbol(arrayJoin(trace))) from system.stack_trace where startsWith(query_id, '02366_')" 2>&1 /dev/null
 
