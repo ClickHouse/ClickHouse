35c267b3b1ccfdec6d9a1223e9cf36cbc8d74970
Vitaly Baranov <vitlibar@clickhouse.com>
2022-07-26 21:35:25 +0200

    Replace column "status_changed_time" with columns "start_time" and "end_time".

--- src/Storages/System/StorageSystemBackups.cpp
+++ src/Storages/System/StorageSystemBackups.cpp
@@ -18,8 +18,9 @@ NamesAndTypesList StorageSystemBackups::getNamesAndTypes()
         {"id", std::make_shared<DataTypeString>()},
         {"name", std::make_shared<DataTypeString>()},
         {"status", std::make_shared<DataTypeEnum8>(getBackupStatusEnumValues())},
-        {"status_changed_time", std::make_shared<DataTypeDateTime>()},
         {"error", std::make_shared<DataTypeString>()},
+        {"start_time", std::make_shared<DataTypeDateTime>()},
+        {"end_time", std::make_shared<DataTypeDateTime>()},
     };
     return names_and_types;
 }
@@ -31,16 +32,18 @@ void StorageSystemBackups::fillData(MutableColumns & res_columns, ContextPtr con
     auto & column_id = assert_cast<ColumnString &>(*res_columns[column_index++]);
     auto & column_name = assert_cast<ColumnString &>(*res_columns[column_index++]);
     auto & column_status = assert_cast<ColumnInt8 &>(*res_columns[column_index++]);
-    auto & column_status_changed_time = assert_cast<ColumnUInt32 &>(*res_columns[column_index++]);
     auto & column_error = assert_cast<ColumnString &>(*res_columns[column_index++]);
+    auto & column_start_time = assert_cast<ColumnUInt32 &>(*res_columns[column_index++]);
+    auto & column_end_time = assert_cast<ColumnUInt32 &>(*res_columns[column_index++]);
 
     auto add_row = [&](const BackupsWorker::Info & info)
     {
         column_id.insertData(info.id.data(), info.id.size());
         column_name.insertData(info.name.data(), info.name.size());
         column_status.insertValue(static_cast<Int8>(info.status));
-        column_status_changed_time.insertValue(info.status_changed_time);
         column_error.insertData(info.error_message.data(), info.error_message.size());
+        column_start_time.insertValue(std::chrono::system_clock::to_time_t(info.start_time));
+        column_end_time.insertValue(std::chrono::system_clock::to_time_t(info.end_time));
     };
 
     for (const auto & entry : context->getBackupsWorker().getAllInfos())
