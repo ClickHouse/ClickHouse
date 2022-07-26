f0d0e8640abd3b65df8bcc260c52ad267a104268
Alexander Tokmakov <tavplubix@clickhouse.com>
2022-07-26 14:59:07 +0300

    Update 02319_sql_standard_create_drop_index.sql

--- tests/queries/0_stateless/02319_sql_standard_create_drop_index.sql
+++ tests/queries/0_stateless/02319_sql_standard_create_drop_index.sql
@@ -16,8 +16,8 @@ select table, name, type, expr, granularity from system.data_skipping_indices wh
 
 drop table t_index;
 
-create table t_index(a int, b String) engine=ReplicatedMergeTree('/test/2319/{database}/', '1') order by a;
-create table t_index_replica(a int, b String) engine=ReplicatedMergeTree('/test/2319/{database}/', '2') order by a;
+create table t_index(a int, b String) engine=ReplicatedMergeTree('/test/2319/{database}', '1') order by a;
+create table t_index_replica(a int, b String) engine=ReplicatedMergeTree('/test/2319/{database}', '2') order by a;
 
 create index i_a on t_index(a) TYPE minmax GRANULARITY 4;
 create index if not exists i_a on t_index(a) TYPE minmax GRANULARITY 2;
