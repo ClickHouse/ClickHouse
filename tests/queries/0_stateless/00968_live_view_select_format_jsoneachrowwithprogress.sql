150e24f5a72bed3beeb33c07fa30a391a7bb47a2
vdimir <vdimir@clickhouse.com>
2022-07-27 12:36:19 +0200

    Check flaky 00968_live_view_select_format_jsoneachrowwithprogress

--- tests/queries/0_stateless/00968_live_view_select_format_jsoneachrowwithprogress.sql
+++ tests/queries/0_stateless/00968_live_view_select_format_jsoneachrowwithprogress.sql
@@ -8,7 +8,7 @@ DROP TABLE IF EXISTS mt;
 CREATE TABLE mt (a Int32) Engine=MergeTree order by tuple();
 CREATE LIVE VIEW lv AS SELECT * FROM mt;
 
-INSERT INTO mt VALUES (1),(2),(3);
+INSERT INTO mt VALUES (1), (2), (3);
 
 SELECT * FROM lv FORMAT JSONEachRowWithProgress;
 
