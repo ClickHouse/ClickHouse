b2f6b8abf29cd326e8954484ea00500b2df9d8de
robot-clickhouse <robot-clickhouse@clickhouse.com>
2022-07-27 12:03:21 +0000

    Backport #39619 to 22.3: Replace MemoryTrackerBlockerInThread to LockMemoryExceptionInThread

--- src/Common/SystemLogBase.cpp
+++ src/Common/SystemLogBase.cpp
@@ -76,7 +76,7 @@ void SystemLogBase<LogElement>::add(const LogElement & element)
     /// The size of allocation can be in order of a few megabytes.
     /// But this should not be accounted for query memory usage.
     /// Otherwise the tests like 01017_uniqCombined_memory_usage.sql will be flacky.
-    MemoryTrackerBlockerInThread temporarily_disable_memory_tracker(VariableContext::Global);
+    MemoryTrackerBlockerInThread temporarily_disable_memory_tracker;
 
     /// Should not log messages under mutex.
     bool queue_is_half_full = false;
