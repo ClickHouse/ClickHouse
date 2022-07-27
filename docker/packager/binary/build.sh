f91a0935cd576f70d24b98c1f04f0b05d008256f
Yakov Olkhovskiy <99031427+yakov-olkhovskiy@users.noreply.github.com>
2022-07-27 09:00:05 -0400

    check existence of compressed clickhouse

--- docker/packager/binary/build.sh
+++ docker/packager/binary/build.sh
@@ -104,7 +104,7 @@ if [ -n "$MAKE_DEB" ]; then
 fi
 
 mv ./programs/clickhouse* /output
-mv ./programs/self-extracting/clickhouse /output ||:
+[ -x ./programs/self-extracting/clickhouse ] && mv ./programs/self-extracting/clickhouse /output
 mv ./src/unit_tests_dbms /output ||: # may not exist for some binary builds
 find . -name '*.so' -print -exec mv '{}' /output \;
 find . -name '*.so.*' -print -exec mv '{}' /output \;
