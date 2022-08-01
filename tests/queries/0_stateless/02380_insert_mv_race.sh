cf342326755973e1f0f54d1c9346fa3d8f2ff71d
Nikolai Kochetov <nik-kochetov@yandex-team.ru>
2022-08-01 11:48:36 +0000

    Output header is now empty for every MV chain.
    Instead of checking that number of processors different for different
    threads, simply always return empty header from buildChainImpl(), by
    adding explicit conversion.
    
    v2: ignore UNKNOWN_TABLE errors in test

--- tests/queries/0_stateless/02380_insert_mv_race.sh
+++ tests/queries/0_stateless/02380_insert_mv_race.sh
@@ -9,58 +9,18 @@ CUR_DIR=$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)
 # shellcheck source=../shell_config.sh
 . "$CUR_DIR"/../shell_config.sh
 
-function bootstrap()
-{
-    $CLICKHOUSE_CLIENT -nm -q "
-        DROP TABLE IF EXISTS null;
-        CREATE TABLE null (key Int) ENGINE = Null;
+$CLICKHOUSE_CLIENT -nm -q "
+    DROP TABLE IF EXISTS null;
+    CREATE TABLE null (key Int) ENGINE = Null;
+    DROP TABLE IF EXISTS mv;
+    CREATE MATERIALIZED VIEW mv ENGINE = Null() AS SELECT * FROM null;
+"
 
-        DROP TABLE IF EXISTS mv;
-        CREATE MATERIALIZED VIEW mv ENGINE = Null() AS SELECT * FROM null;
-    "
-}
-
-function insert_thread()
-{
-    local opts=(
-        --max_insert_threads 100
-        --max_threads 100
-    )
-    local patterns=(
-        -e UNKNOWN_TABLE
-        -e TABLE_IS_DROPPED
-    )
-
-    while :; do
-        $CLICKHOUSE_CLIENT "${opts[@]}" -q "INSERT INTO null SELECT * FROM numbers_mt(1e6)" |& {
-            grep -F "DB::Exception: " | grep -v -F "${patterns[@]}"
-        }
-    done
-}
-export -f insert_thread
-
-function drop_thread()
-{
-    local opts=(
-        --database_atomic_wait_for_drop_and_detach_synchronously 1
-    )
-
-    while :; do
-        $CLICKHOUSE_CLIENT -nm "${opts[@]}" -q "DETACH TABLE mv"
-        sleep 0.01
-        $CLICKHOUSE_CLIENT -nm "${opts[@]}" -q "ATTACH TABLE mv"
-    done
-}
-export -f drop_thread
-
-function main()
-{
-    local test_timeout=1m
-
-    bootstrap
-    timeout "$test_timeout" bash -c insert_thread &
-    timeout "$test_timeout" bash -c drop_thread &
-
-    wait
-}
-main "$@"
+$CLICKHOUSE_CLIENT -q "INSERT INTO null SELECT * FROM numbers_mt(1000) settings max_threads=1000, max_insert_threads=1000, max_block_size=1" |& {
+    # To avoid handling stacktrace here, get only first line (-m1)
+    # this should be OK, since you cannot have multiple exceptions from the client anyway.
+    grep -m1 -F 'DB::Exception:' | grep -F -v -e 'UNKNOWN_TABLE'
+} &
+sleep 0.05
+$CLICKHOUSE_CLIENT -q "DETACH TABLE mv"
+wait
