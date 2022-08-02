e77a6333a1a0740f31ef30185e7f31bd6a6dd6ce
ltrk2 <107155950+ltrk2@users.noreply.github.com>
2022-08-02 07:35:21 -0700

    Implement KQL functions handling IPv4

--- src/Parsers/Kusto/KustoFunctions/KQLCastingFunctions.cpp
+++ src/Parsers/Kusto/KustoFunctions/KQLCastingFunctions.cpp
@@ -1,48 +1,54 @@
 #include <Parsers/IParserBase.h>
 #include <Parsers/Kusto/KustoFunctions/IParserKQLFunction.h>
-#include <Parsers/Kusto/KustoFunctions/KQLFunctionFactory.h>
 #include <Parsers/Kusto/KustoFunctions/KQLCastingFunctions.h>
+#include <Parsers/Kusto/KustoFunctions/KQLFunctionFactory.h>
+
+#include <format>
 
 namespace DB
 {
-bool ToBool::convertImpl(String &out,IParser::Pos &pos)
+bool ToBool::convertImpl(String & out, IParser::Pos & pos)
 {
-    String res = String(pos->begin,pos->end);
+    String res = String(pos->begin, pos->end);
     out = res;
     return false;
 }
 
-bool ToDateTime::convertImpl(String &out,IParser::Pos &pos)
+bool ToDateTime::convertImpl(String & out, IParser::Pos & pos)
 {
-    String res = String(pos->begin,pos->end);
+    String res = String(pos->begin, pos->end);
     out = res;
     return false;
 }
 
-bool ToDouble::convertImpl(String &out,IParser::Pos &pos)
+bool ToDouble::convertImpl(String & out, IParser::Pos & pos)
 {
-    String res = String(pos->begin,pos->end);
+    String res = String(pos->begin, pos->end);
     out = res;
     return false;
 }
 
-bool ToInt::convertImpl(String &out,IParser::Pos &pos)
+bool ToInt::convertImpl(String & out, IParser::Pos & pos)
 {
-    String res = String(pos->begin,pos->end);
+    String res = String(pos->begin, pos->end);
     out = res;
     return false;
 }
 
-bool ToString::convertImpl(String &out,IParser::Pos &pos)
+bool ToString::convertImpl(String & out, IParser::Pos & pos)
 {
-    String res = String(pos->begin,pos->end);
-    out = res;
-    return false;
+    const auto function_name = getKQLFunctionName(pos);
+    if (function_name.empty())
+        return false;
+
+    const auto param = getArgument(function_name, pos);
+    out = std::format("ifNull(toString({0}), '')", param);
+    return true;
 }
 
-bool ToTimeSpan::convertImpl(String &out,IParser::Pos &pos)
+bool ToTimeSpan::convertImpl(String & out, IParser::Pos & pos)
 {
-    String res = String(pos->begin,pos->end);
+    String res = String(pos->begin, pos->end);
     out = res;
     return false;
 }
