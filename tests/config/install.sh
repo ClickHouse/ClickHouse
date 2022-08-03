05a74850b31c29bea08d71f9a1604ec0fa615e38
Antonio Andelic <antonio2368@users.noreply.github.com>
2022-08-03 13:57:20 +0200

    Update tests/config/install.sh
    Co-authored-by: Alexander Tokmakov <tavplubix@clickhouse.com>

--- tests/config/install.sh
+++ tests/config/install.sh
@@ -82,7 +82,7 @@ ln -sf --backup=simple --suffix=_original.xml \
 
 # We randomize creating the snapshot on exit for Keeper to test out using older snapshots
 create_snapshot_on_exit=$(($RANDOM % 2))
-cat $DEST_SERVER_PATH/config.d/keeper_port.xml | sed "s|<create_snapshot_on_exit>true</create_snapshot_on_exit>|<create_snapshot_on_exit>$create_snapshot_on_exit</create_snapshot_on_exit>|" > $DEST_SERVER_PATH/config.d/keeper_port.xml
+sed --follow-symlinks -i "s|<create_snapshot_on_exit>true</create_snapshot_on_exit>|<create_snapshot_on_exit>$create_snapshot_on_exit</create_snapshot_on_exit>|" $DEST_SERVER_PATH/config.d/keeper_port.xml
 
 if [[ -n "$USE_POLYMORPHIC_PARTS" ]] && [[ "$USE_POLYMORPHIC_PARTS" -eq 1 ]]; then
     ln -sf $SRC_PATH/config.d/polymorphic_parts.xml $DEST_SERVER_PATH/config.d/
