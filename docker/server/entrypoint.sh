67a6b32a8985854667a0850abfbdd9bd7feda4eb
Alexey Milovidov <milovidov@clickhouse.com>
2022-08-05 07:12:50 +0300

    Update entrypoint.sh

--- docker/server/entrypoint.sh
+++ docker/server/entrypoint.sh
@@ -58,12 +58,12 @@ do
     [ -z "$dir" ] && continue
     # ensure directories exist
     if [ "$DO_CHOWN" = "1" ]; then
-      mkdir="mkdir"
+        mkdir="mkdir"
     else
-      # if DO_CHOWN=0 it means that the system does not map root user to "admin" permissions
-      # it mainly happens on NFS mounts where root==nobody for security reasons
-      # thus mkdir MUST run with user id/gid and not from nobody that has zero permissions
-      mkdir="/usr/bin/clickhouse su "${USER}:${GROUP}" mkdir"
+        # if DO_CHOWN=0 it means that the system does not map root user to "admin" permissions
+        # it mainly happens on NFS mounts where root==nobody for security reasons
+        # thus mkdir MUST run with user id/gid and not from nobody that has zero permissions
+        mkdir="/usr/bin/clickhouse su "${USER}:${GROUP}" mkdir"
     fi
     if ! $mkdir -p "$dir"; then
         echo "Couldn't create necessary directory: $dir"
