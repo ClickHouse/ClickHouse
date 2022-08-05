60f9f6855de76f3d55fdccc6a20ebacf7f54d611
Robert Schulze <robert@clickhouse.com>
2022-08-05 07:53:06 +0000

    feat: implement catboost in library-bridge
    This commit moves the catboost model evaluation out of the server
    process into the library-bridge binary. This serves two goals: On the
    one hand, crashes / memory corruptions of the catboost library no longer
    affect the server. On the other hand, we can forbid loading dynamic
    libraries in the server (catboost was the last consumer of this
    functionality), thus improving security.
    
    SQL syntax:
    
      SELECT
        catboostEvaluate('/path/to/model.bin', FEAT_1, ..., FEAT_N) > 0 AS prediction,
        ACTION AS target
      FROM amazon_train
      LIMIT 10
    
    Required configuration:
    
      <catboost_lib_path>/path/to/libcatboostmodel.so</catboost_lib_path>
    
    *** Implementation Details ***
    
    The internal protocol between the server and the library-bridge is
    simple:
    
    - HTTP GET on path "/extdict_ping":
      A ping, used during the handshake to check if the library-bridge runs.
    
    - HTTP POST on path "extdict_request"
      (1) Send a "catboost_GetTreeCount" request from the server to the
          bridge, containing a library path (e.g /home/user/libcatboost.so) and
          a model path (e.g. /home/user/model.bin). Rirst, this unloads the
          catboost library handler associated to the model path (if it was
          loaded), then loads the catboost library handler associated to the
          model path, then executes GetTreeCount() on the library handler and
          finally sends the result back to the server. Step (1) is called once
          by the server from FunctionCatBoostEvaluate::getReturnTypeImpl(). The
          library path handler is unloaded in the beginning because it contains
          state which may no longer be valid if the user runs
          catboost("/path/to/model.bin", ...) more than once and if "model.bin"
          was updated in between.
      (2) Send "catboost_Evaluate" from the server to the bridge, containing
          the model path and the features to run the interference on. Step (2)
          is called multiple times (once per chunk) by the server from function
          FunctionCatBoostEvaluate::executeImpl(). The library handler for the
          given model path is expected to be already loaded by Step (1).
    
    Fixes #27870

---
+++ programs/library-bridge/CatBoostLibraryHandlerFactory.cpp
@@ -0,0 +1,49 @@
+#include "CatBoostLibraryHandlerFactory.h"
+
+#include <Common/logger_useful.h>
+
+namespace DB
+{
+
+CatBoostLibraryHandlerFactory & CatBoostLibraryHandlerFactory::instance()
+{
+    static CatBoostLibraryHandlerFactory instance;
+    return instance;
+}
+
+CatBoostLibraryHandlerPtr CatBoostLibraryHandlerFactory::get(const String & model_path)
+{
+    std::lock_guard lock(mutex);
+
+    if (auto handler = library_handlers.find(model_path); handler != library_handlers.end())
+        return handler->second;
+    return nullptr;
+}
+
+void CatBoostLibraryHandlerFactory::create(const String & library_path, const String & model_path)
+{
+    std::lock_guard lock(mutex);
+
+    if (library_handlers.contains(model_path))
+    {
+        LOG_DEBUG(&Poco::Logger::get("CatBoostLibraryHandlerFactory"), "Cannot load catboost library handler for model path {} because it exists already", model_path);
+        return;
+    }
+
+    library_handlers.emplace(std::make_pair(model_path, std::make_shared<CatBoostLibraryHandler>(library_path, model_path)));
+    LOG_DEBUG(&Poco::Logger::get("CatBoostLibraryHandlerFactory"), "Loaded catboost library handler for model path {}.", model_path);
+}
+
+void CatBoostLibraryHandlerFactory::remove(const String & model_path)
+{
+    std::lock_guard lock(mutex);
+    bool deleted = library_handlers.erase(model_path);
+    if (!deleted)
+    {
+        LOG_DEBUG(&Poco::Logger::get("CatBoostLibraryHandlerFactory"), "Cannot unload catboost library handler for model path: {}", model_path);
+        return;
+    }
+    LOG_DEBUG(&Poco::Logger::get("CatBoostLibraryHandlerFactory"), "Unloaded catboost library handler for model path: {}", model_path);
+}
+
+}
