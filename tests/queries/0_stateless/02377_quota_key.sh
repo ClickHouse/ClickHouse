e2feb4a62f85a83d184e3197f66dfc69862edab6
Yakov Olkhovskiy <yakov@clickhouse.com>
2022-08-05 09:39:20 -0400

    dedicated http test

--- tests/queries/0_stateless/02377_quota_key.sh
+++ tests/queries/0_stateless/02377_quota_key.sh
@@ -11,7 +11,6 @@ ${CLICKHOUSE_CLIENT} -q "CREATE USER u_02377 IDENTIFIED WITH plaintext_password 
 ${CLICKHOUSE_CLIENT} -q "CREATE QUOTA q_02377 KEYED BY client_key FOR INTERVAL 1 month MAX queries = 100 TO u_02377;"
 
 ${CLICKHOUSE_CLIENT} --user=u_02377 --password=password --quota_key=q_02377 --query="select 1"
-curl -sS "${CLICKHOUSE_URL}&user=editor_api&password=password&quota_key=editor_api&query=SELECT%201"
 
 ${CLICKHOUSE_CLIENT} -q "DROP USER IF EXISTS u_02377"
 ${CLICKHOUSE_CLIENT} -q "drop quota if exists q_02377"
---
+++ tests/queries/0_stateless/02377_quota_key_http.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env bash
+# Tags: no-parallel
+
+CURDIR=$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)
+# shellcheck source=../shell_config.sh
+. "$CURDIR"/../shell_config.sh
+
+curl -sS "${CLICKHOUSE_URL}" --data "DROP USER IF EXISTS u_02377"
+curl -sS "${CLICKHOUSE_URL}" --data "drop quota if exists q_02377"
+curl -sS "${CLICKHOUSE_URL}" --data "CREATE USER u_02377 IDENTIFIED WITH plaintext_password BY 'password'"
+curl -sS "${CLICKHOUSE_URL}" --data "CREATE QUOTA q_02377 KEYED BY client_key FOR INTERVAL 1 month MAX queries = 100 TO u_02377"
+
+curl -sS -G "${CLICKHOUSE_URL}&user=editor_api&password=password&quota_key=editor_api&query=SELECT%201"
+
+curl -sS "${CLICKHOUSE_URL}" --data "DROP USER IF EXISTS u_02377"
+curl -sS "${CLICKHOUSE_URL}" --data "drop quota if exists q_02377"
