c794507ddec1bba9e15c3dade36c318dfd257a92
Vitaly Baranov <vitlibar@clickhouse.com>
2022-08-06 15:32:04 +0200

    Move non-public functions to cpp.

--- src/Databases/DDLDependencyVisitor.cpp
+++ src/Databases/DDLDependencyVisitor.cpp
@@ -33,6 +33,7 @@ namespace
         QualifiedTableName to_table{create.to_table_id.database_name, create.to_table_id.table_name};
         if (!to_table.table.empty())
         {
+            /// TO target_table (for materialized views)
             if (to_table.database.empty())
                 to_table.database = data.default_database;
             data.dependencies.emplace(to_table);
@@ -67,17 +68,114 @@ namespace
 
         data.dependencies.emplace(qualified_name);
     }
+
+    void extractTableNameFromArgument(const ASTFunction & function, DDLDependencyVisitor::Data & data, size_t arg_idx)
+    {
+        /// Just ignore incorrect arguments, proper exception will be thrown later
+        if (!function.arguments || function.arguments->children.size() <= arg_idx)
+            return;
+
+        QualifiedTableName qualified_name;
+
+        const auto * expr_list = function.arguments->as<ASTExpressionList>();
+        if (!expr_list)
+            return;
+
+        const auto * arg = expr_list->children[arg_idx].get();
+        if (const auto * literal = arg->as<ASTLiteral>())
+        {
+            if (literal->value.getType() != Field::Types::String)
+                return;
+
+            auto maybe_qualified_name = QualifiedTableName::tryParseFromString(literal->value.get<String>());
+            /// Just return if name if invalid
+            if (!maybe_qualified_name)
+                return;
+
+            qualified_name = std::move(*maybe_qualified_name);
+        }
+        else if (const auto * identifier = dynamic_cast<const ASTIdentifier *>(arg))
+        {
+            /// ASTIdentifier or ASTTableIdentifier
+            auto table_identifier = identifier->createTable();
+            /// Just return if table identified is invalid
+            if (!table_identifier)
+                return;
+
+            qualified_name.database = table_identifier->getDatabaseName();
+            qualified_name.table = table_identifier->shortName();
+        }
+        else
+        {
+            /// Just return because we don't validate AST in this function.
+            return;
+        }
+
+        if (qualified_name.database.empty())
+        {
+            /// It can be table/dictionary from default database or XML dictionary, but we cannot distinguish it here.
+            qualified_name.database = data.default_database;
+        }
+        data.dependencies.emplace(std::move(qualified_name));
+    }
+
+    void visitFunction(const ASTFunction & function, DDLDependencyVisitor::Data & data)
+    {
+        if (function.name == "joinGet" || function.name == "dictHas" || function.name == "dictIsIn" || function.name.starts_with("dictGet"))
+        {
+            /// dictGet('dict_name', attr_names, id_expr)
+            /// dictHas('dict_name', id_expr)
+            /// joinGet(join_storage_table_name, `value_column`, join_keys)
+            extractTableNameFromArgument(function, data, 0);
+        }
+        else if (function.name == "in" || function.name == "notIn" || function.name == "globalIn" || function.name == "globalNotIn")
+        {
+            /// in(x, table_name) - function for evaluating (x IN table_name)
+            extractTableNameFromArgument(function, data, 1);
+        }
+        else if (function.name == "dictionary")
+        {
+            /// dictionary(dict_name)
+            extractTableNameFromArgument(function, data, 0);
+        }
+    }
+
+    void visitTableEngine(const ASTStorage & storage, DDLDependencyVisitor::Data & data)
+    {
+        if (!storage.engine)
+            return;
+        if (storage.engine->name != "Dictionary")
+            return;
+
+        extractTableNameFromArgument(*storage.engine, data, 0);
+    }
+
+    void visitDictionaryDef(const ASTDictionary & dictionary, DDLDependencyVisitor::Data & data)
+    {
+        if (!dictionary.source || dictionary.source->name != "clickhouse" || !dictionary.source->elements)
+            return;
+
+        auto config = getDictionaryConfigurationFromAST(data.create_query->as<ASTCreateQuery &>(), data.global_context);
+        auto info = getInfoIfClickHouseDictionarySource(config, data.global_context);
+
+        if (!info || !info->is_local)
+            return;
+
+        if (info->table_name.database.empty())
+            info->table_name.database = data.default_database;
+        data.dependencies.emplace(std::move(info->table_name));
+    }
 }
 
 
 TableNamesSet getDependenciesSetFromCreateQuery(const ASTPtr & ast, const ContextPtr & global_context)
 {
     assert(global_context == global_context->getGlobalContext());
-    TableLoadingDependenciesVisitor::Data data;
+    DDLDependencyVisitor::Data data;
     data.default_database = global_context->getCurrentDatabase();
     data.create_query = ast;
     data.global_context = global_context;
-    TableLoadingDependenciesVisitor visitor{data};
+    DDLDependencyVisitor::Visitor visitor{data};
     visitor.visit(ast);
     data.dependencies.erase(data.table_name);
     return data.dependencies;
@@ -85,15 +183,14 @@ TableNamesSet getDependenciesSetFromCreateQuery(const ASTPtr & ast, const Contex
 
 void DDLDependencyVisitor::visit(const ASTPtr & ast, Data & data)
 {
-    /// Looking for functions in column default expressions and dictionary source definition
     if (auto * create = ast->as<ASTCreateQuery>())
         visitCreateQuery(*create, data);
     else if (const auto * function = ast->as<ASTFunction>())
-        visit(*function, data);
-    else if (const auto * dict_source = ast->as<ASTFunctionWithKeyValueArguments>())
-        visit(*dict_source, data);
+        visitFunction(*function, data);
+    else if (auto * dictionary = ast->as<ASTDictionary>())
+        visitDictionaryDef(*dictionary, data);
     else if (const auto * storage = ast->as<ASTStorage>())
-        visit(*storage, data);
+        visitTableEngine(*storage, data);
     else if (auto * expr = ast->as<ASTTableExpression>())
         visitTableExpression(*expr, data);
 }
@@ -101,106 +198,6 @@ void DDLDependencyVisitor::visit(const ASTPtr & ast, Data & data)
 bool DDLDependencyVisitor::needChildVisit(const ASTPtr &, const ASTPtr &)
 {
     return true;
-}
-
-void DDLDependencyVisitor::visit(const ASTFunction & function, Data & data)
-{
-    if (function.name == "joinGet" || function.name == "dictHas" || function.name == "dictIsIn" || function.name.starts_with("dictGet"))
-    {
-        /// dictGet('dict_name', attr_names, id_expr)
-        /// dictHas('dict_name', id_expr)
-        /// joinGet(join_storage_table_name, `value_column`, join_keys)
-        extractTableNameFromArgument(function, data, 0);
-    }
-    else if (function.name == "in" || function.name == "notIn" || function.name == "globalIn" || function.name == "globalNotIn")
-    {
-        /// in(x, table_name) - function for evaluating (x IN table_name)
-        extractTableNameFromArgument(function, data, 1);
-    }
-    else if (function.name == "dictionary")
-    {
-        /// dictionary(dict_name)
-        extractTableNameFromArgument(function, data, 0);
-    }
-}
-
-void DDLDependencyVisitor::visit(const ASTFunctionWithKeyValueArguments & dict_source, Data & data)
-{
-    if (dict_source.name != "clickhouse")
-        return;
-    if (!dict_source.elements)
-        return;
-
-    auto config = getDictionaryConfigurationFromAST(data.create_query->as<ASTCreateQuery &>(), data.global_context);
-    auto info = getInfoIfClickHouseDictionarySource(config, data.global_context);
-
-    if (!info || !info->is_local)
-        return;
-
-    if (info->table_name.database.empty())
-        info->table_name.database = data.default_database;
-    data.dependencies.emplace(std::move(info->table_name));
-}
-
-void DDLDependencyVisitor::visit(const ASTStorage & storage, Data & data)
-{
-    if (!storage.engine)
-        return;
-    if (storage.engine->name != "Dictionary")
-        return;
-
-    extractTableNameFromArgument(*storage.engine, data, 0);
-}
-
-
-void DDLDependencyVisitor::extractTableNameFromArgument(const ASTFunction & function, Data & data, size_t arg_idx)
-{
-    /// Just ignore incorrect arguments, proper exception will be thrown later
-    if (!function.arguments || function.arguments->children.size() <= arg_idx)
-        return;
-
-    QualifiedTableName qualified_name;
-
-    const auto * expr_list = function.arguments->as<ASTExpressionList>();
-    if (!expr_list)
-        return;
-
-    const auto * arg = expr_list->children[arg_idx].get();
-    if (const auto * literal = arg->as<ASTLiteral>())
-    {
-        if (literal->value.getType() != Field::Types::String)
-            return;
-
-        auto maybe_qualified_name = QualifiedTableName::tryParseFromString(literal->value.get<String>());
-        /// Just return if name if invalid
-        if (!maybe_qualified_name)
-            return;
-
-        qualified_name = std::move(*maybe_qualified_name);
-    }
-    else if (const auto * identifier = dynamic_cast<const ASTIdentifier *>(arg))
-    {
-        /// ASTIdentifier or ASTTableIdentifier
-        auto table_identifier = identifier->createTable();
-        /// Just return if table identified is invalid
-        if (!table_identifier)
-            return;
-
-        qualified_name.database = table_identifier->getDatabaseName();
-        qualified_name.table = table_identifier->shortName();
-    }
-    else
-    {
-        /// Just return because we don't validate AST in this function.
-        return;
-    }
-
-    if (qualified_name.database.empty())
-    {
-        /// It can be table/dictionary from default database or XML dictionary, but we cannot distinguish it here.
-        qualified_name.database = data.default_database;
-    }
-    data.dependencies.emplace(std::move(qualified_name));
 }
 
 }
--- src/Databases/DDLDependencyVisitor.h
+++ src/Databases/DDLDependencyVisitor.h
@@ -6,18 +6,15 @@
 namespace DB
 {
 
-class ASTFunction;
-class ASTFunctionWithKeyValueArguments;
-class ASTStorage;
-
 using TableNamesSet = std::unordered_set<QualifiedTableName>;
 
+/// Returns a list of all tables which should be loaded before a specified table by its create query.
+/// For example, a column default expression can use dictGet() to refer to a dictionary;
+/// or dictionary source can refer to a local ClickHouse table.
+/// Does not validate AST, works a best-effort way.
 TableNamesSet getDependenciesSetFromCreateQuery(const ASTPtr & ast, const ContextPtr & global_context);
 
-/// Visits ASTCreateQuery and extracts names of table (or dictionary) dependencies
-/// from column default expressions (joinGet, dictGet, etc)
-/// or dictionary source (for dictionaries from local ClickHouse table).
-/// Does not validate AST, works a best-effort way.
+/// Visits ASTCreateQuery and extracts the names of all tables which should be loaded before a specified table by its create query.
 class DDLDependencyVisitor
 {
 public:
@@ -34,15 +31,6 @@ public:
 
     static void visit(const ASTPtr & ast, Data & data);
     static bool needChildVisit(const ASTPtr & node, const ASTPtr & child);
-
-private:
-    static void visit(const ASTFunction & function, Data & data);
-    static void visit(const ASTFunctionWithKeyValueArguments & dict_source, Data & data);
-    static void visit(const ASTStorage & storage, Data & data);
-
-    static void extractTableNameFromArgument(const ASTFunction & function, Data & data, size_t arg_idx);
 };
-
-using TableLoadingDependenciesVisitor = DDLDependencyVisitor::Visitor;
 
 }
