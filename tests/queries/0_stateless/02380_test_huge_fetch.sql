4d6f7e94a59d64fa615942e5fe410f6a43dc6515
Denny Crane <denis.zhuravlov@gmail.com>
2022-08-06 00:56:45 -0300

    Update 02380_test_huge_fetch.sql

--- tests/queries/0_stateless/02380_test_huge_fetch.sql
+++ tests/queries/0_stateless/02380_test_huge_fetch.sql
@@ -5,7 +5,7 @@ DROP TABLE IF EXISTS fetch_test_huge1;
 
 create table fetch_test_huge (A Int64, huge_column String CODEC(NONE)) Engine=ReplicatedMergeTree('/clickhouse/tables/{database}/fetch_test_huge', 'r1') order by A;
 insert into fetch_test_huge select number, repeat(' ', 2000) from numbers(1e6) settings max_block_size=8192;
-select sleep(3) format Null; -- without sleep the optimize does not kick in
+system sync replica fetch_test_huge;
 optimize table fetch_test_huge final; 
 create table fetch_test_huge1(A Int64, huge_column String CODEC(NONE)) Engine=ReplicatedMergeTree('/clickhouse/tables/{database}/fetch_test_huge', 'r2') order by A;
 system sync replica fetch_test_huge;
