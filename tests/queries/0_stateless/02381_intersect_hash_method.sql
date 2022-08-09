9e865d48ce6466287df427dac35a9dd262e88060
Duc Canh Le <canhld94@gmail.com>
2022-08-09 18:50:17 +0800

    increase test size

--- tests/queries/0_stateless/02381_intersect_hash_method.sql
+++ tests/queries/0_stateless/02381_intersect_hash_method.sql
@@ -1,3 +1,3 @@
-SELECT 1 FROM numbers(3) INTERSECT SELECT 1 FROM numbers(3);
-SELECT toString(1) FROM numbers(3) INTERSECT SELECT toString(1) FROM numbers(3);
-SELECT '1' FROM numbers(3) INTERSECT SELECT '1' FROM numbers(3);
+SELECT 1 FROM numbers(10) INTERSECT SELECT 1 FROM numbers(10);
+SELECT toString(1) FROM numbers(10) INTERSECT SELECT toString(1) FROM numbers(10);
+SELECT '1' FROM numbers(10) INTERSECT SELECT '1' FROM numbers(10);
