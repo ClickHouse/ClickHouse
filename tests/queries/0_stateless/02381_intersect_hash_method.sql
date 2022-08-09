7910a93b02bb524e88bd68353e0547b6c3a26da3
Duc Canh Le <canhld94@gmail.com>
2022-08-09 18:25:52 +0800

    fix other hash methods and add tests

---
+++ tests/queries/0_stateless/02381_intersect_hash_method.sql
@@ -0,0 +1,3 @@
+SELECT 1 FROM numbers(3) INTERSECT SELECT 1 FROM numbers(3);
+SELECT toString(1) FROM numbers(3) INTERSECT SELECT toString(1) FROM numbers(3);
+SELECT '1' FROM numbers(3) INTERSECT SELECT '1' FROM numbers(3);
