ccf0a4ece55ae5d607460f926d2ec6b3d652b9f9
Mikhail f. Shiryaev <felixoid@clickhouse.com>
2022-08-10 23:15:07 +0200

    Backport the most recent code from master

--- tests/ci/git_helper.py
+++ tests/ci/git_helper.py
@@ -16,6 +16,9 @@ TAG_REGEXP = (
 )
 SHA_REGEXP = r"\A([0-9]|[a-f]){40}\Z"
 
+CWD = p.dirname(p.realpath(__file__))
+TWEAK = 1
+
 
 # Py 3.8 removeprefix and removesuffix
 def removeprefix(string: str, prefix: str):
@@ -49,15 +52,15 @@ def release_branch(name: str):
 class Runner:
     """lightweight check_output wrapper with stripping last NEW_LINE"""
 
-    def __init__(self, cwd: str = p.dirname(p.realpath(__file__))):
-        self.cwd = cwd
+    def __init__(self, cwd: str = CWD):
+        self._cwd = cwd
 
-    def run(self, cmd: str, cwd: Optional[str] = None) -> str:
+    def run(self, cmd: str, cwd: Optional[str] = None, **kwargs) -> str:
         if cwd is None:
             cwd = self.cwd
         logger.debug("Running command: %s", cmd)
         return subprocess.check_output(
-            cmd, shell=True, cwd=cwd, encoding="utf-8"
+            cmd, shell=True, cwd=cwd, encoding="utf-8", **kwargs
         ).strip()
 
     @property
@@ -95,18 +98,19 @@ def get_tags() -> List[str]:
 class Git:
     """A small wrapper around subprocess to invoke git commands"""
 
-    def __init__(self):
-        runner = Runner()
-        rel_root = runner.run("git rev-parse --show-cdup")
-        self.root = p.realpath(p.join(runner.cwd, rel_root))
-        self._tag_pattern = re.compile(TAG_REGEXP)
-        runner.cwd = self.root
-        self.run = runner.run
+    _tag_pattern = re.compile(TAG_REGEXP)
+
+    def __init__(self, ignore_no_tags: bool = False):
+        self.root = git_runner.cwd
+        self._ignore_no_tags = ignore_no_tags
+        self.run = git_runner.run
+        self.latest_tag = ""
+        self.new_tag = ""
         self.new_branch = ""
         self.branch = ""
         self.sha = ""
         self.sha_short = ""
-        self.description = ""
+        self.description = "shallow-checkout"
         self.commits_since_tag = 0
         self.update()
 
@@ -117,6 +121,16 @@ class Git:
         self.sha_short = self.sha[:11]
         # The following command shows the most recent tag in a graph
         # Format should match TAG_REGEXP
+        if self._ignore_no_tags and is_shallow():
+            try:
+                self._update_tags()
+            except subprocess.CalledProcessError:
+                pass
+
+            return
+        self._update_tags()
+
+    def _update_tags(self):
         self.latest_tag = self.run("git describe --tags --abbrev=0")
         # Format should be: {latest_tag}-{commits_since_tag}-g{sha_short}
         self.description = self.run("git describe --tags --long")
@@ -124,10 +138,11 @@ class Git:
             self.run(f"git rev-list {self.latest_tag}..HEAD --count")
         )
 
-    def check_tag(self, value: str):
+    @staticmethod
+    def check_tag(value: str):
         if value == "":
             return
-        if not self._tag_pattern.match(value):
+        if not Git._tag_pattern.match(value):
             raise ValueError(f"last tag {value} doesn't match the pattern")
 
     @property
@@ -153,10 +168,7 @@ class Git:
         if not self.latest_tag.endswith("-testing"):
             # When we are on the tag, we still need to have tweak=1 to not
             # break cmake with versions like 12.13.14.0
-            return self.commits_since_tag or 1
+            return self.commits_since_tag or TWEAK
 
         version = self.latest_tag.split("-", maxsplit=1)[0]
         return int(version.split(".")[-1]) + self.commits_since_tag
-
-    def get_tags(self) -> List[str]:
-        return self.run("git tag").split()
