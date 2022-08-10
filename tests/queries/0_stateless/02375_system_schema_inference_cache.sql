c47bc70d8c5f5290b0aa31320f20f3f8d3160689
avogar <pav.cruglov@yandex.ru>
2022-08-10 14:29:50 +0000

    Fix fasttest

--- tests/queries/0_stateless/02375_system_schema_inference_cache.sql
+++ tests/queries/0_stateless/02375_system_schema_inference_cache.sql
@@ -1,3 +1,5 @@
+-- Tags: no-fasttest
+
 insert into function file('02374_data1.jsonl') select number as x, 'str' as s from numbers(10);
 insert into function file('02374_data2.jsonl') select number as x, 'str' as s from numbers(10);
 
