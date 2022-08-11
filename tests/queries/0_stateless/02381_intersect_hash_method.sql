84cd867aa8e5ddc47b61d2614921c6b9941b82d6
Duc Canh Le <canhld94@gmail.com>
2022-08-11 10:46:06 +0800

    materialize column instead of handling column in hash method

--- tests/queries/0_stateless/02381_intersect_hash_method.sql
+++
@@ -1,3 +0,0 @@
-SELECT 1 FROM numbers(10) INTERSECT SELECT 1 FROM numbers(10);
-SELECT toString(1) FROM numbers(10) INTERSECT SELECT toString(1) FROM numbers(10);
-SELECT '1' FROM numbers(10) INTERSECT SELECT '1' FROM numbers(10);
