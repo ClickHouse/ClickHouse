42e8469143871b05035b186045762f304a5aba96
Nikolai Kochetov <nik-kochetov@yandex-team.ru>
2022-08-11 08:08:20 +0000

    Mute test.

--- tests/queries/0_stateless/00725_memory_tracking.sql
+++ tests/queries/0_stateless/00725_memory_tracking.sql
@@ -1,4 +1,4 @@
--- Tags: no-replicated-database
+-- Tags: no-replicated-database, no-tsan, no-asan, no-msan
 
 SELECT least(value, 0) FROM system.metrics WHERE metric = 'MemoryTracking';
 SELECT length(range(100000000));
