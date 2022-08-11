456291f33519a0a545f7fbcfb1e6e2a1f7292fa9
Kruglov Pavel <48961922+Avogar@users.noreply.github.com>
2022-08-11 13:08:56 +0200

    Fix style

--- src/Functions/UniqTheta/FunctionsUniqTheta.h
+++ src/Functions/UniqTheta/FunctionsUniqTheta.h
@@ -30,7 +30,6 @@ namespace DB
     namespace ErrorCodes
     {
         extern const int ILLEGAL_TYPE_OF_ARGUMENT;
-        extern const int LOGICAL_ERROR;
     }
 
     struct UniqThetaIntersectImpl
