ebea9f9359dce393e6a0ac6e5b5a267d32a07356
teng.ma <teng.ma@shopee.com>
2022-08-11 14:57:31 +0800

    Feature: Support Event Tracing when calling Aws S3 API
    Description:
    ============
    In  Computing && Storage Architecture,  using S3 as remote / Shared storage, the Method to access S3 is using AWS S3 API
    There is a gap between ClickHouse DB with Ozone Operation
    In ClickHouse, operation is SQL and background task
    In S3 , the operation should be AWS S3 API
    And one sql maybe can mapped to multiple  API
    
    Solution:
    =========
    Added Calling API as event into system.events table

--- src/IO/ReadBufferFromS3.cpp
+++ src/IO/ReadBufferFromS3.cpp
@@ -24,6 +24,8 @@ namespace ProfileEvents
     extern const Event ReadBufferFromS3Bytes;
     extern const Event ReadBufferFromS3RequestsErrors;
     extern const Event ReadBufferSeekCancelConnection;
+    extern const Event HeadS3Object;
+    extern const Event GetS3ObjectRequest;
 }
 
 namespace DB
@@ -249,7 +251,6 @@ size_t ReadBufferFromS3::getFileSize()
         return *file_size;
 
     auto object_size = S3::getObjectSize(client_ptr, bucket, key, version_id);
-
     file_size = object_size;
     return *file_size;
 }
@@ -275,6 +276,7 @@ SeekableReadBuffer::Range ReadBufferFromS3::getRemainingReadRange() const
 
 std::unique_ptr<ReadBuffer> ReadBufferFromS3::initialize()
 {
+    ProfileEvents::increment(ProfileEvents::GetS3ObjectRequest);
     Aws::S3::Model::GetObjectRequest req;
     req.SetBucket(bucket);
     req.SetKey(key);
