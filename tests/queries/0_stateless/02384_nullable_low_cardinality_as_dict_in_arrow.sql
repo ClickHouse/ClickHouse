7d4581a6c8634e9c73efaa43cba75642503ec963
Kruglov Pavel <48961922+Avogar@users.noreply.github.com>
2022-08-14 12:40:07 +0200

    Fix fasttest

--- tests/queries/0_stateless/02384_nullable_low_cardinality_as_dict_in_arrow.sql
+++ tests/queries/0_stateless/02384_nullable_low_cardinality_as_dict_in_arrow.sql
@@ -1,3 +1,5 @@
+-- Tags: no-fasttest
+
 insert into function file(02384_data.arrow) select toLowCardinality(toNullable('abc')) as lc settings output_format_arrow_low_cardinality_as_dictionary=1, engine_file_truncate_on_insert=1;
 desc file(02384_data.arrow);
 select * from file(02384_data.arrow);
