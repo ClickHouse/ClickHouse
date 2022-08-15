fbd4e0a0df7e3cd95a240cfd57be7b086b52f8a9
Duc Canh Le <canhld94@gmail.com>
2022-08-15 19:04:44 +0800

    fix storage merge on view cannot use index

--- src/Storages/StorageMerge.cpp
+++ src/Storages/StorageMerge.cpp
@@ -2,6 +2,7 @@
 #include <QueryPipeline/QueryPipelineBuilder.h>
 #include <Storages/StorageMerge.h>
 #include <Storages/StorageFactory.h>
+#include <Storages/StorageView.h>
 #include <Storages/VirtualColumnUtils.h>
 #include <Storages/AlterCommands.h>
 #include <Storages/checkAndGetLiteralArgument.h>
@@ -34,6 +35,7 @@
 #include <Processors/Sources/SourceFromSingleChunk.h>
 #include <Databases/IDatabase.h>
 #include <base/range.h>
+#include <fmt/core.h>
 #include <algorithm>
 
 
@@ -528,21 +530,31 @@ QueryPipelineBuilderPtr ReadFromMerge::createSources(
             real_column_names.push_back(ExpressionActions::getSmallestColumn(storage_snapshot->metadata->getColumns().getAllPhysical()));
 
         QueryPlan plan;
-        storage->read(
-            plan,
-            real_column_names,
-            storage_snapshot,
-            modified_query_info,
-            modified_context,
-            processed_stage,
-            max_block_size,
-            UInt32(streams_num));
+        if (StorageView * view = dynamic_cast<StorageView *>(storage.get()))
+        {
+            /// For view storage, we need to rewrite the modified_query_info.view_query to optimize read. The
+            /// most intuitive way is to use InterpreterSelectQuery.
+            modified_select.replaceDatabaseAndTable(database_name, table_name);
+            InterpreterSelectQuery(modified_query_info.query, modified_context, storage).buildQueryPlan(plan);
+        }
+        else
+        {
+            storage->read(
+                plan,
+                real_column_names,
+                storage_snapshot,
+                modified_query_info,
+                modified_context,
+                processed_stage,
+                max_block_size,
+                UInt32(streams_num));
 
-        if (!plan.isInitialized())
-            return {};
+            if (!plan.isInitialized())
+                return {};
 
-        if (auto * read_from_merge_tree = typeid_cast<ReadFromMergeTree *>(plan.getRootNode()->step.get()))
-            read_from_merge_tree->addFilterNodes(added_filter_nodes);
+            if (auto * read_from_merge_tree = typeid_cast<ReadFromMergeTree *>(plan.getRootNode()->step.get()))
+                read_from_merge_tree->addFilterNodes(added_filter_nodes);
+        }
 
         builder = plan.buildQueryPipeline(
             QueryPlanOptimizationSettings::fromContext(modified_context),
---
+++ tests/queries/0_stateless/02402_merge_engine_with_view.sql
@@ -0,0 +1,11 @@
+
+CREATE TABLE m0 (id UInt64) ENGINE=MergeTree ORDER BY id SETTINGS index_granularity = 1;
+INSERT INTO m0 SELECT number FROM numbers(10);
+CREATE TABLE m1 (id UInt64, s String) ENGINE=MergeTree ORDER BY id SETTINGS index_granularity = 1;
+INSERT INTO m1 SELECT number, 'boo' FROM numbers(10);
+CREATE VIEW m1v AS SELECT id FROM m1;
+
+CREATE TABLE m2 (id UInt64) ENGINE=Merge(test,'m0|m1v');
+
+SET max_bytes_to_read = 64;
+SELECT * FROM m2 WHERE id > 1 AND id < 5;
