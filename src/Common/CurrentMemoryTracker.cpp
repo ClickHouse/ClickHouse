c12a8a3f80b44a9183bc5afde3c5e615e2b57c61
Azat Khuzhin <a3at.mail@gmail.com>
2022-08-16 00:34:02 +0300

    Fix memory accounting in case of MEMORY_LIMIT_EXCEEDED errors
    In case of memory allocation had been failed, it should not be take into
    account anywhere, otherwise it will report incorrect memory usage for
    query and in logs.
    
    I found this while was digging into OOM in stress tests, and saw memory
    allocations that had been significantly greater the memory limit (even
    with overcommit).
    
    Signed-off-by: Azat Khuzhin <a.khuzhin@semrush.com>

--- src/Common/CurrentMemoryTracker.cpp
+++ src/Common/CurrentMemoryTracker.cpp
@@ -51,15 +51,20 @@ void CurrentMemoryTracker::allocImpl(Int64 size, bool throw_if_memory_exceeded)
     {
         if (current_thread)
         {
-            current_thread->untracked_memory += size;
+            Int64 will_be = current_thread->untracked_memory + size;
 
-            if (current_thread->untracked_memory > current_thread->untracked_memory_limit)
+            if (will_be > current_thread->untracked_memory_limit)
             {
                 /// Zero untracked before track. If tracker throws out-of-limit we would be able to alloc up to untracked_memory_limit bytes
                 /// more. It could be useful to enlarge Exception message in rethrow logic.
-                Int64 tmp = current_thread->untracked_memory;
                 current_thread->untracked_memory = 0;
-                memory_tracker->allocImpl(tmp, throw_if_memory_exceeded);
+                memory_tracker->allocImpl(will_be, throw_if_memory_exceeded);
+            }
+            else
+            {
+                /// Update after successful allocations,
+                /// since failed allocations should not be take into account.
+                current_thread->untracked_memory = will_be;
             }
         }
         /// total_memory_tracker only, ignore untracked_memory
