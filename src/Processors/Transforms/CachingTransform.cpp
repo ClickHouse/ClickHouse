a0dc4c4479fdbb48aac943aae49ef03350596f10
Mikhail Stetsyuk <stetsyuk@yandex-team.com>
2022-08-15 20:38:39 +0100

    remove LOG_FATAL

--- src/Processors/Transforms/CachingTransform.cpp
+++ src/Processors/Transforms/CachingTransform.cpp
@@ -5,7 +5,6 @@ namespace DB
 
 void CachingTransform::transform(Chunk & chunk)
 {
-    LOG_FATAL(&Poco::Logger::get("CachingTransform::transform"), "chunk.num_cols ={}, .structure = {}", chunk.getNumColumns(), chunk.dumpStructure());
     holder.insertChunk(chunk.clone());
 }
 
