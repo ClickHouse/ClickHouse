0bad9425d26d08fd2087940374ac5cee5b5f14f4
alesapin <alesapin@gmail.com>
2022-08-16 19:27:40 +0200

    Some improvements in tests

--- src/Disks/ObjectStorages/S3/S3ObjectStorage.cpp
+++ src/Disks/ObjectStorages/S3/S3ObjectStorage.cpp
@@ -267,6 +267,8 @@ void S3ObjectStorage::removeObjectImpl(const StoredObject & object, bool if_exis
     auto outcome = client_ptr->DeleteObject(request);
 
     throwIfUnexpectedError(outcome, if_exists);
+
+    LOG_TRACE(log, "Object with path {} was removed from S3", object.absolute_path);
 }
 
 void S3ObjectStorage::removeObjectsImpl(const StoredObjects & objects, bool if_exists)
@@ -310,6 +312,8 @@ void S3ObjectStorage::removeObjectsImpl(const StoredObjects & objects, bool if_e
             auto outcome = client_ptr->DeleteObjects(request);
 
             throwIfUnexpectedError(outcome, if_exists);
+
+            LOG_TRACE(log, "Objects with paths [{}] were removed from S3", keys);
         }
     }
 }
