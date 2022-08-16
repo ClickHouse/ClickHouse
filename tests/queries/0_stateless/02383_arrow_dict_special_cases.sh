331d0ed4a368e1ba57dae46891c296f862879aca
avogar <pav.cruglov@yandex.ru>
2022-08-16 12:56:51 +0000

    Fix tests

--- tests/queries/0_stateless/02383_arrow_dict_special_cases.sh
+++ tests/queries/0_stateless/02383_arrow_dict_special_cases.sh
@@ -10,6 +10,7 @@ USER_FILES_PATH=$(clickhouse-client --query "select _path,_file from file('nonex
 mkdir -p $USER_FILES_PATH/test_02383
 cp $CURDIR/data_arrow/dictionary*.arrow $USER_FILES_PATH/test_02383/
 cp $CURDIR/data_arrow/corrupted.arrow $USER_FILES_PATH/test_02383/
+cp $CURDIR/data_arrow/dict_with_nulls.arrow $USER_FILES_PATH/test_02383/
 
 $CLICKHOUSE_CLIENT -q "desc file('test_02383/dictionary1.arrow')"
 $CLICKHOUSE_CLIENT -q "select * from file('test_02383/dictionary1.arrow')"
@@ -20,6 +21,9 @@ $CLICKHOUSE_CLIENT -q "select * from file('test_02383/dictionary3.arrow')"
 
 $CLICKHOUSE_CLIENT -q "desc file('test_02383/corrupted.arrow')"
 $CLICKHOUSE_CLIENT -q "select * from file('test_02383/corrupted.arrow')" 2>&1 | grep -F -q "INCORRECT_DATA" && echo OK || echo FAIL
+
+$CLICKHOUSE_CLIENT -q "desc file('test_02383/dict_with_nulls.arrow')"
+$CLICKHOUSE_CLIENT -q "select * from file('test_02383/dict_with_nulls.arrow')"
 
 
 rm -rf $USER_FILES_PATH/test_02383
--- tests/queries/0_stateless/02384_nullable_low_cardinality_as_dict_in_arrow.sql
+++ tests/queries/0_stateless/02384_nullable_low_cardinality_as_dict_in_arrow.sql
@@ -1,6 +1,6 @@
 -- Tags: no-fasttest
 
-insert into function file(02384_data.arrow) select toLowCardinality(toNullable('abc')) as lc settings output_format_arrow_low_cardinality_as_dictionary=1, output_format_arrow_string_as_string=0,ngine_file_truncate_on_insert=1;
+insert into function file(02384_data.arrow) select toLowCardinality(toNullable('abc')) as lc settings output_format_arrow_low_cardinality_as_dictionary=1, output_format_arrow_string_as_string=0, engine_file_truncate_on_insert=1;
 desc file(02384_data.arrow);
 select * from file(02384_data.arrow);
 insert into function file(02384_data.arrow) select toLowCardinality(toNullable('abc')) as lc settings output_format_arrow_low_cardinality_as_dictionary=1, output_format_arrow_string_as_string=1, engine_file_truncate_on_insert=1;
