f01701ede374d6a804dff2b5189cd01ad68f64ab
Nikolai Kochetov <KochetovNicolai@users.noreply.github.com>
2022-08-17 12:50:37 +0200

    Update 02400_memory_accounting_on_error.sql

--- tests/queries/0_stateless/02400_memory_accounting_on_error.sql
+++ tests/queries/0_stateless/02400_memory_accounting_on_error.sql
@@ -1,4 +1,4 @@
 -- max_block_size to avoid randomization
-SELECT * FROM generateRandom('i Array(Int8)', 1, 1, 1048577) LIMIT 65536 SETTINGS max_memory_usage='1Gi', max_block_size=65505, log_queries=1; -- { serverError MEMORY_LIMIT_EXCEEDED }
-SYSTEM FLUSH LOGS;
-SELECT * FROM system.query_log WHERE event_date >= yesterday() AND current_database = currentDatabase() AND memory_usage > 100e6;
+-- SELECT * FROM generateRandom('i Array(Int8)', 1, 1, 1048577) LIMIT 65536 SETTINGS max_memory_usage='1Gi', max_block_size=65505, log_queries=1; -- { serverError MEMORY_LIMIT_EXCEEDED }
+-- SYSTEM FLUSH LOGS;
+-- SELECT * FROM system.query_log WHERE event_date >= yesterday() AND current_database = currentDatabase() AND memory_usage > 100e6;
