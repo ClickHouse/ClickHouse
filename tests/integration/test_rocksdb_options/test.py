5a463d923cb01071d2cb2642a20221fb6835a479
Lloyd-Pottiger <yan1579196623@gamil.com>
2022-08-17 23:27:18 +0800

    address comments
    Signed-off-by: Lloyd-Pottiger <yan1579196623@gamil.com>

--- tests/integration/test_rocksdb_options/test.py
+++ tests/integration/test_rocksdb_options/test.py
@@ -32,13 +32,13 @@ def test_valid_options(start_cluster):
     )
     node.query(
         """
-    CREATE TABLE test (key UInt64, value String) Engine=EmbeddedRocksDB(-1) PRIMARY KEY(key);
+    CREATE TABLE test (key UInt64, value String) Engine=EmbeddedRocksDB(0) PRIMARY KEY(key);
     DROP TABLE test;
     """
     )
     node.query(
         """
-    CREATE TABLE test (key UInt64, value String) Engine=EmbeddedRocksDB(-1, true) PRIMARY KEY(key);
+    CREATE TABLE test (key UInt64, value String) Engine=EmbeddedRocksDB(0, true) PRIMARY KEY(key);
     DROP TABLE test;
     """
     )
--- tests/queries/0_stateless/02381_rocksdb_read_only.sql
+++ tests/queries/0_stateless/02381_rocksdb_read_only.sql
@@ -1,6 +1,6 @@
 DROP TABLE IF EXISTS 02381_test;
 
-CREATE TABLE 02381_test (key UInt64, value String) Engine=EmbeddedRocksDB(0, true) PRIMARY KEY(key);
+CREATE TABLE 02381_test (key UInt64, value String) Engine=EmbeddedRocksDB("0", "true") PRIMARY KEY(key);
 
 SELECT value FROM system.rocksdb WHERE database = currentDatabase() and table = '02381_test' and name = 'number.keys.written';
 INSERT INTO 02381_test SELECT number, format('Hello, world ({})', toString(number)) FROM numbers(10000);
