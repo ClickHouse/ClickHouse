2822720ca22bc18f71e6b660fcb4cfe0badb1d0f
Duc Canh Le <canhld94@gmail.com>
2022-08-18 10:54:41 +0800

    fix tests

--- tests/queries/0_stateless/02402_merge_engine_with_view.sql
+++ tests/queries/0_stateless/02402_merge_engine_with_view.sql
@@ -7,4 +7,4 @@ CREATE VIEW m1v AS SELECT id FROM m1;
 
 CREATE TABLE m2 (id UInt64) ENGINE=Merge(currentDatabase(),'m0|m1v');
 
-SELECT * FROM m2 WHERE id > 1 AND id < 5 SETTINGS force_primary_key=1, max_bytes_to_read=64;
+SELECT * FROM m2 WHERE id > 1 AND id < 5 ORDER BY id SETTINGS force_primary_key=1, max_bytes_to_read=64;
