27befb63bea5b226ebbd1098bc141cb9fe88cf25
alesapin <alesapin@gmail.com>
2022-08-18 14:21:20 +0200

    Fix bug with multiple TTL merges on expired column

---
+++ tests/queries/0_stateless/02403_ttl_column_multiple_times.sql
@@ -0,0 +1,24 @@
+DROP TABLE IF EXISTS ttl_table;
+
+CREATE TABLE ttl_table
+(
+    EventDate Date,
+    Longitude Float64 TTL EventDate + toIntervalWeek(2)
+)
+ENGINE = MergeTree()
+ORDER BY EventDate
+SETTINGS vertical_merge_algorithm_min_rows_to_activate=1, vertical_merge_algorithm_min_columns_to_activate=1;
+
+INSERT INTO ttl_table VALUES(toDate('2020-10-01'), 144);
+
+SELECT * FROM ttl_table;
+
+OPTIMIZE TABLE ttl_table FINAL;
+
+SELECT * FROM ttl_table;
+
+OPTIMIZE TABLE ttl_table FINAL;
+
+SELECT * FROM ttl_table;
+
+DROP TABLE IF EXISTS ttl_table;
