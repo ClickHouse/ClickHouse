a5a3777e5200927a88d0df5261599c255588277c
alesapin <alesapin@clickhouse.com>
2022-08-19 11:36:33 +0200

    Update 02403_ttl_column_multiple_times.sql

--- tests/queries/0_stateless/02403_ttl_column_multiple_times.sql
+++ tests/queries/0_stateless/02403_ttl_column_multiple_times.sql
@@ -9,9 +9,13 @@ ENGINE = MergeTree()
 ORDER BY EventDate
 SETTINGS vertical_merge_algorithm_min_rows_to_activate=1, vertical_merge_algorithm_min_columns_to_activate=1;
 
+SYSTEM STOP MERGES ttl_table;
+
 INSERT INTO ttl_table VALUES(toDate('2020-10-01'), 144);
 
 SELECT * FROM ttl_table;
+
+SYSTEM START MERGES ttl_table;
 
 OPTIMIZE TABLE ttl_table FINAL;
 
