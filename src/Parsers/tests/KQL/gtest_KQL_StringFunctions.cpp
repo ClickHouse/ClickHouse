b72af2f5105726edb5334960a0ce9f4836c4b9b4
HeenaBansal2009 <heena.bansal@ibm.com>
2022-08-23 13:07:53 -0700

    Updated tests

--- src/Parsers/tests/KQL/gtest_KQL_StringFunctions.cpp
+++ src/Parsers/tests/KQL/gtest_KQL_StringFunctions.cpp
@@ -124,7 +124,7 @@ INSTANTIATE_TEST_SUITE_P(ParserKQLQuery, ParserTest,
         },
         {
             "print bin(datetime(1970-05-11 13:45:07), 1d)",
-            "SELECT parseDateTime64BestEffortOrNull(toFloat64(parseDateTime64BestEffortOrNull('1970-01-01 12:00:00.0', 9, 'UTC')) + (toInt64(((toFloat64(parseDateTime64BestEffortOrNull('2017-05-15 10:20:00.0', 9, 'UTC')) - toFloat64(parseDateTime64BestEffortOrNull('1970-01-01 12:00:00.0', 9, 'UTC'))) / 86400) + 0) * 86400), 9, 'UTC') AS res"
+            "SELECT parseDateTime64BestEffortOrNull(toInt64(toFloat64(parseDateTime64BestEffortOrNull('1970-05-11 13:45:07', 9, 'UTC')) / 86400) * 86400, 9, 'UTC')"
         }
 
 })));   
