3b02ce09ff8052c57082221e368a6c4957a36b12
BoloniniD <denis@bolonin.ru>
2022-08-23 21:26:19 +0300

    Add glibc symbols to glibc_compatibility

---
+++ base/glibc-compatibility/musl/fstatat.c
@@ -0,0 +1,29 @@
+#define _BSD_SOURCE
+#include <sys/stat.h>
+#include <stdint.h>
+#include "syscall.h"
+
+struct statx {
+	uint32_t stx_mask;
+	uint32_t stx_blksize;
+	uint64_t stx_attributes;
+	uint32_t stx_nlink;
+	uint32_t stx_uid;
+	uint32_t stx_gid;
+	uint16_t stx_mode;
+	uint16_t pad1;
+	uint64_t stx_ino;
+	uint64_t stx_size;
+	uint64_t stx_blocks;
+	uint64_t stx_attributes_mask;
+	struct {
+		int64_t tv_sec;
+		uint32_t tv_nsec;
+		int32_t pad;
+	} stx_atime, stx_btime, stx_ctime, stx_mtime;
+	uint32_t stx_rdev_major;
+	uint32_t stx_rdev_minor;
+	uint32_t stx_dev_major;
+	uint32_t stx_dev_minor;
+	uint64_t spare[14];
+};
