67efb66494840dd130df6c5c4c2933dfddc8b7d0
Raúl Marín <git@rmr.ninja>
2022-08-24 17:26:29 +0200

    Move test to the proper file

--- tests/queries/0_stateless/02212_cte_and_table_alias.sql
+++ tests/queries/0_stateless/02212_cte_and_table_alias.sql
@@ -39,22 +39,3 @@ WITH t AS
 SELECT count(*)
 FROM t AS a
 WHERE t.n < 5000;
-
-
--- Check now that function aliases are included in the hash too
--- The 2 subqueries are different as the first column alias is changed and that modifies the second column value
-SELECT
-    (
-        SELECT
-                1 + 2 AS number,
-                1 + number AS b
-        FROM system.numbers
-        LIMIT 10, 1
-    ),
-    (
-        SELECT
-                1 + 2 AS number2,
-                1 + number AS b
-        FROM system.numbers
-        LIMIT 10, 1
-    )
--- tests/queries/0_stateless/02383_query_hash_includes_aliases.sql
+++ tests/queries/0_stateless/02383_query_hash_includes_aliases.sql
@@ -10,4 +10,22 @@ SELECT
             1,
             number
         FROM numbers(1)
-    ) AS s2
+    ) AS s2;
+
+-- Check now that function aliases are included in the hash too
+-- The 2 subqueries are different as the first column alias is changed and that modifies the second column value
+SELECT
+    (
+        SELECT
+                1 + 2 AS number,
+                1 + number AS b
+        FROM system.numbers
+        LIMIT 10, 1
+    ),
+    (
+        SELECT
+                1 + 2 AS number2,
+                1 + number AS b
+        FROM system.numbers
+        LIMIT 10, 1
+    )
