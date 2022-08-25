558b179cb11103d46a3fedc12d2f2c69c94378ae
Frank Chen <frank.chen021@outlook.com>
2022-08-25 10:11:08 +0800

    Fix typo
    Signed-off-by: Frank Chen <frank.chen021@outlook.com>

--- src/Common/OpenTelemetryTraceContext.h
+++ src/Common/OpenTelemetryTraceContext.h
@@ -88,8 +88,7 @@ struct TracingContextOnThread : TracingContext
 ///
 /// It's also the root of all spans on current thread execution.
 ///
-/// Although it's SAFE to construct this object multiple times on one same thread,
-/// but rememeber only use it at the beginning of one thread
+/// Although it's SAFE to construct this object multiple times on one same thread, it shoud be created at the beginning of one thread execution.
 struct TracingContextHolder
 {
     /// Forbidden copy ctor and assignment to make the destructor safe
