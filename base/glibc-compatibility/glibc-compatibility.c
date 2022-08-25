055e9f1298d375d5603f69bd453f05f06c4848dc
BoloniniD <denis@bolonin.ru>
2022-08-25 20:59:52 +0300

    Move copy_file_range to glibc_compat

--- base/glibc-compatibility/glibc-compatibility.c
+++ base/glibc-compatibility/glibc-compatibility.c
@@ -175,6 +175,15 @@ void __explicit_bzero_chk(void * buf, size_t len, size_t unused)
     return explicit_bzero(buf, len);
 }
 
+#define _GNU_SOURCE
+#include <unistd.h>
+#include "syscall.h"
+
+ssize_t copy_file_range(int fd_in, off_t *off_in, int fd_out, off_t *off_out, size_t len, unsigned flags)
+{
+	return syscall(SYS_copy_file_range, fd_in, off_in, fd_out, off_out, len, flags);
+}
+
 
 #if defined (__cplusplus)
 }
