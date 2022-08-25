4ee1b52116e1f9d2e5e3e24a9425d7d3f7fe3be2
BoloniniD <denis@bolonin.ru>
2022-08-25 20:59:52 +0300

    Move copy_file_range to glibc_compat

--- base/glibc-compatibility/musl/copy_file_range.c
+++
@@ -1,8 +0,0 @@
-#define _GNU_SOURCE
-#include <unistd.h>
-#include "syscall.h"
-
-ssize_t copy_file_range(int fd_in, off_t *off_in, int fd_out, off_t *off_out, size_t len, unsigned flags)
-{
-	return syscall(SYS_copy_file_range, fd_in, off_in, fd_out, off_out, len, flags);
-}
