8582df039679edb1242a6e852f246515af3d6b2f
Alexey Milovidov <milovidov@clickhouse.com>
2022-08-26 02:53:56 +0200

    Fix test

--- tests/queries/0_stateless/02391_recursive_buffer.sql
+++ tests/queries/0_stateless/02391_recursive_buffer.sql
@@ -1,7 +1,7 @@
 DROP TABLE IF EXISTS test;
 CREATE TABLE test (key UInt32) Engine = Buffer(currentDatabase(), test, 16, 10, 100, 10000, 1000000, 10000000, 100000000);
 SELECT * FROM test; -- { serverError 269 }
-SELECT * FROM system.tables WHERE table = 'test' AND database = currentDatabase();
+SELECT * FROM system.tables WHERE table = 'test' AND database = currentDatabase() FORMAT Null;
 DROP TABLE test;
 
 DROP TABLE IF EXISTS test1;
