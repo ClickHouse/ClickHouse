d45e7e220639d1ef45eaee4090e82f8eeec8e9cf
Alexey Milovidov <milovidov@clickhouse.com>
2022-08-27 21:06:09 +0200

    All new table functions must be documented

---
+++ tests/queries/0_stateless/02414_all_new_table_functions_must_be_documented.sql
@@ -0,0 +1,3 @@
+-- This outputs the list of undocumented table functions. No new items in the list should appear.
+-- Please help shorten this list down to zero elements.
+SELECT name FROM system.table_functions WHERE length(description) < 10 ORDER BY name;
