166fc8688daae232c6f2d7bfd4ae972e7ee65b7f
Amos Bird <amosbird@gmail.com>
2022-08-29 10:09:47 +0800

    Add more test cases

--- tests/queries/0_stateless/01710_aggregate_projection_with_grouping_set.sql
+++ tests/queries/0_stateless/01710_aggregate_projection_with_grouping_set.sql
@@ -6,4 +6,10 @@ insert into test values ('a', 'x') ('a', 'y') ('b', 'x') ('b', 'y');
 
 select dim1, dim2, count() from test group by grouping sets ((dim1, dim2), dim1) order by dim1, dim2, count();
 
+select dim1, dim2, count() from test group by dim1, dim2 with rollup order by dim1, dim2, count();
+
+select dim1, dim2, count() from test group by dim1, dim2 with cube order by dim1, dim2, count();
+
+select dim1, dim2, count() from test group by dim1, dim2 with totals order by dim1, dim2, count();
+
 drop table test;
