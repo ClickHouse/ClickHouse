fa4c7259f9b991f983dc90ff5c752a9c83bd4272
Antonio Andelic <antonio2368@users.noreply.github.com>
2022-08-29 13:56:51 +0200

    Fix typos

--- src/Coordination/CoordinationSettings.cpp
+++ src/Coordination/CoordinationSettings.cpp
@@ -190,7 +190,7 @@ KeeperConfigurationAndSettings::loadFromConfig(const Poco::Util::AbstractConfigu
     ret->coordination_settings->loadFromConfig("keeper_server.coordination_settings", config);
 
     if (ret->coordination_settings->quorum_reads)
-        LOG_WARNING(&Poco::Logger::get("KeeperConfigurationAndSettings"), "Setting 'quorum_reads' is depricated. Please use 'read_mode'");
+        LOG_WARNING(&Poco::Logger::get("KeeperConfigurationAndSettings"), "Setting 'quorum_reads' is deprecated. Please use 'read_mode'");
 
     return ret;
 }
