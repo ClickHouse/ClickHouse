506bee1e2f7bc8d55883f002abb0a41e44b39516
Anton Popov <ap@clickhouse.com>
2022-08-29 14:31:09 +0000

    avoid logical errors at some common operation with ColumnObject

---
+++ tests/queries/0_stateless/01825_type_json_order_by.sql
@@ -0,0 +1,3 @@
+SELECT dummy FROM system.one ORDER BY materialize('{"k":"v"}'::JSON);
+SELECT dummy FROM system.one ORDER BY materialize('{"k":"v"}'::JSON), dummy;
+SELECT materialize('{"k":"v"}'::JSON) SETTINGS extremes = 1;
