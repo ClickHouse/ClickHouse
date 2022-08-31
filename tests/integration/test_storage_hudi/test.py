f2d5dd7092af7c00bafa74f506f536f9bb052389
Daniil Rubin <danyarubin@gmail.com>
2022-08-31 09:26:53 +0000

    Add select test

--- tests/integration/test_storage_hudi/test.py
+++ tests/integration/test_storage_hudi/test.py
@@ -5,6 +5,7 @@ import json
 import helpers.client
 import pytest
 from helpers.cluster import ClickHouseCluster
+from helpers.test_tools import TSV
 
 SCRIPT_DIR = os.path.dirname(os.path.realpath(__file__))
 
@@ -57,21 +58,12 @@ def upload_test_table(started_cluster):
         for name in files:
             started_cluster.minio_client.fput_object(bucket, os.path.join(address_without_prefix, name), os.path.join(address, name))
 
-    for obj in list(
-        started_cluster.minio_client.list_objects(
-            bucket,
-            recursive=True,
-        )
-    ):
-        logging.info(obj.object_name)
-
 @pytest.fixture(scope="module")
 def started_cluster():
     try:
         cluster = ClickHouseCluster(__file__)
         cluster.add_instance(
-            "dummy",
-            main_configs=["configs/conf.xml"],
+            "main_server",
             with_minio=True
         )
 
@@ -100,12 +92,35 @@ def run_query(instance, query, stdin=None, settings=None):
 
 
 def test_create_query(started_cluster):
-    instance = started_cluster.instances["dummy"]
+    instance = started_cluster.instances["main_server"]
     bucket = started_cluster.minio_bucket
 
     create_query = f"""CREATE TABLE hudi ENGINE=Hudi('http://{started_cluster.minio_ip}:{started_cluster.minio_port}/{bucket}/test_table/', 'minio', 'minio123')"""
 
     run_query(instance, create_query)
 
-def test_select_query():
-    pass
+def test_select_query(started_cluster):
+    instance = started_cluster.instances["main_server"]
+    bucket = started_cluster.minio_bucket
+    columns = ['_hoodie_commit_time', '_hoodie_commit_seqno', '_hoodie_record_key',
+               '_hoodie_partition_path', '_hoodie_file_name', 'begin_lat', 
+               'begin_lon', 'driver', 'end_lat', 'end_lon',
+                'fare', 'partitionpath', 'rider', 'ts', 'uuid']
+    
+    # create query in case table doesn't exist
+    create_query = f"""CREATE TABLE IF NOT EXISTS hudi ENGINE=Hudi('http://{started_cluster.minio_ip}:{started_cluster.minio_port}/{bucket}/test_table/', 'minio', 'minio123')"""
+
+    run_query(instance, create_query)
+
+    select_query = "SELECT {} FROM hudi FORMAT TSV"
+
+    for column_name in columns:
+        result = run_query(instance, select_query.format(column_name)).splitlines()
+        assert(len(result) > 0)    
+
+    # test if all partition paths is presented in result 
+    distinct_select_query = "SELECT DISTINCT partitionpath FROM hudi FORMAT TSV"
+    result = run_query(instance, distinct_select_query).splitlines()
+    expected = ['americas/brazil/sao_paulo', 'asia/india/chennai', 'americas/united_states/san_francisco']
+    
+    assert TSV(result) == TSV(expected)
