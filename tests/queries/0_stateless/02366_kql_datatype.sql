8b03a1a2612168a3fb3ca32e519cb93b8e328f36
Larry Luo <larry.luo@ca.ibm.com>
2022-09-06 11:03:24 -0400

    Adding more KQL functional tests.

--- tests/queries/0_stateless/02366_kql_datatype.sql
+++ tests/queries/0_stateless/02366_kql_datatype.sql
@@ -32,6 +32,8 @@ print datetime('2014-11-08 15:55:55.123456Z');
 print '-- time'
 print time('14.02:03:04.12345');
 print time('12:30:55.123');
+print time(1d);
+print time(-1d);
 print '-- guid'
 print guid(74be27de-1e4e-49d9-b579-fe0b331d3642)
 -- print guid(null)
@@ -51,7 +53,7 @@ print bool(null), int(null), long(null), real(null), double(null);
 print '-- dynamic'; -- only support 1D array at the moment
 print dynamic([1,2,3]);
 print dynamic(['a', 'b', 'c']);
--- print dynamic(null);
+print dynamic(null);
 
 print '-- cast functions'
 print '--tobool("true")'; -- == true
@@ -86,9 +88,11 @@ print todatetime("2015-12-24") == datetime(2015-12-24);
 print todatetime('abc') == null;
 print '-- make_timespan()';
 print v1=make_timespan(1,12), v2=make_timespan(1,12,30), v3=make_timespan(1,12,30,55.123);
-
+print '-- totimespan()';
+print totimespan('0.00:01:00');
+print '-- tolong()';
+print tolong('123')
+print tolong('abc');
 -- TODO:
--- print '-- totimespan()';
--- print totimespan('0.00:01:00');
 -- print totimespan('abc') == null;
 
--- tests/queries/0_stateless/02366_kql_func_dynamic.sql
+++ tests/queries/0_stateless/02366_kql_func_dynamic.sql
@@ -12,15 +12,21 @@ print '-- array_iif()';
 print array_iif(dynamic([true,false,true]), dynamic([1,2,3]), dynamic([4,5,6]));
 print array_iif(dynamic([1,0,1]), dynamic([1,2,3]), dynamic([4,5,6]));
 print array_iif(dynamic([true,false,true]), dynamic([1,2]), dynamic([4,5,6]));
--- print array_iif(dynamic(['a','b','c']), dynamic([1,2,3]), dynamic([4,5,6]));
+print array_iif(dynamic(['a','b','c']), dynamic([1,2,3]), dynamic([4,5,6]));
 print '-- array_concat()';
 print array_concat(dynamic([1,2,3]),dynamic([4,5,6]));
 print '-- array_slice()';
 print array_slice(dynamic([1,2,3]), 1, 2);
-print array_split(dynamic([1,2,3,4,5]), dynamic([1,-2]));
+print array_slice(dynamic([1,2,3,4,5]), -3, -2)
 print '-- array_split()';
+print array_split(dynamic([1,2,3,4,5]), dynamic([1,-2]));
 print array_split(dynamic([1,2,3,4,5]), 2);
 print array_split(dynamic([1,2,3,4,5]), dynamic([1,3]));
 print array_split(dynamic([1,2,3,4,5]), dynamic([-1,-2]));
--- array_sort_asc()
+print '-- array_sort_asc()';
+print array_sort_asc(dynamic([1,3,4,5,2]),dynamic(["a","b","c","d","e"]));
+print array_sort_asc(split("John,Paul,George,Ringo", ","));
+print array_sort_asc(dynamic([null,"blue","yellow","green",null]));
+print v=array_sort_asc(dynamic([null,"blue","yellow","green",null]), false);
+
 -- array_sort_desc()
