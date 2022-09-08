9acc73d811474b61a164debc02a9b730ffab1eb9
Vitaly Baranov <vitlibar@yandex.ru>
2022-09-08 17:51:29 +0200

    Update src/Storages/StorageReplicatedMergeTree.cpp
    Co-authored-by: Alexander Tokmakov <tavplubix@gmail.com>

--- src/Storages/StorageReplicatedMergeTree.cpp
+++ src/Storages/StorageReplicatedMergeTree.cpp
@@ -8264,7 +8264,7 @@ void StorageReplicatedMergeTree::backupData(
     else
         data_parts = getVisibleDataPartsVector(local_context);
 
-    auto backup_entries = backupParts(data_parts, "", local_context);
+    auto backup_entries = backupParts(data_parts, /* data_path_in_backup */ "", local_context);
 
     auto coordination = backup_entries_collector.getBackupCoordination();
     String shared_id = getTableSharedID();
