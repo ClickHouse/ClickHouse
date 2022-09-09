ef1179c6cb4e10254ad1aab9b30535df820ba26f
Antonio Andelic <antonio@clickhouse.com>
2022-09-09 12:24:26 +0000

    Mark primary key test as long

--- tests/queries/0_stateless/02419_keeper_map_primary_key.sh
+++ tests/queries/0_stateless/02419_keeper_map_primary_key.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
-# Tags: no-ordinary-database, no-fasttest
+# Tags: no-ordinary-database, no-fasttest, long
 
 CUR_DIR=$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)
 # shellcheck source=../shell_config.sh
