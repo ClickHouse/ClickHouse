f90b54946fed23dd1b7d52b4c3d3df3fc88768fb
David Zeng <3355478+davidzengxhsh@users.noreply.github.com>
2022-09-09 14:37:35 +0800

    Update cmake/cpu_features.cmake
    Co-authored-by: Robert Schulze <robert@clickhouse.com>

--- cmake/cpu_features.cmake
+++ cmake/cpu_features.cmake
@@ -34,7 +34,7 @@ elseif (ARCH_AARCH64)
 
     # The LSE extension for atomic operations is mandatory in ARM >= v8.1. As we target v8.0,
     # use a runtime-dispatch that falls back to non-atomic operations hardware w/o LSE.
-    # -moutline-atomic is supported from gcc >=10.1 and clang >=12.0
+    # -moutline-atomic is supported from GCC >=10.1 and Clang >=12.0
     set (COMPILER_FLAGS "${COMPILER_FLAGS} -moutline-atomics")
 
 elseif (ARCH_PPC64LE)
