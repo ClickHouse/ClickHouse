a1a1481c862de41fe777f2b424251af90de304d4
Igor Nikonov <igor@clickhouse.com>
2022-09-10 21:33:41 +0000

    test with pipeline

--- tests/queries/0_stateless/01655_plan_optimizations_optimize_read_in_window_order.sh
+++ tests/queries/0_stateless/01655_plan_optimizations_optimize_read_in_window_order.sh
@@ -5,10 +5,12 @@ CURDIR=$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)
 . "$CURDIR"/../shell_config.sh
 
 name=test_01655_plan_optimizations_optimize_read_in_window_order
+TRIM_LEADING_SPACES="sed -e 's/^[ \t]*//'"
 
 $CLICKHOUSE_CLIENT -q "drop table if exists ${name}"
 $CLICKHOUSE_CLIENT -q "drop table if exists ${name}_n"
 $CLICKHOUSE_CLIENT -q "drop table if exists ${name}_n_x"
+$CLICKHOUSE_CLIENT -q "drop table if exists ${name}_complex"
 
 $CLICKHOUSE_CLIENT -q "create table ${name} engine=MergeTree order by tuple() as select toInt64((sin(number)+2)*65535)%10 as n, number as x from numbers_mt(100000)"
 $CLICKHOUSE_CLIENT -q "create table ${name}_n engine=MergeTree order by n as select * from ${name} order by n"
@@ -20,9 +22,11 @@ $CLICKHOUSE_CLIENT -q "optimize table ${name}_n_x final"
 echo 'Partial sorting plan'
 echo '  optimize_read_in_window_order=0'
 $CLICKHOUSE_CLIENT -q "explain plan actions=1, description=1 select n, sum(x) OVER (ORDER BY n, x ROWS BETWEEN 100 PRECEDING AND CURRENT ROW) from ${name}_n SETTINGS optimize_read_in_window_order=0" | grep -i "sort description"
+$CLICKHOUSE_CLIENT -q "explain pipeline select n, sum(x) OVER (ORDER BY n, x ROWS BETWEEN 100 PRECEDING AND CURRENT ROW) from ${name}_n SETTINGS optimize_read_in_window_order=0, max_threads=1" | grep -i "MergeTreeThread" | eval $TRIM_LEADING_SPACES
 
 echo '  optimize_read_in_window_order=1'
 $CLICKHOUSE_CLIENT -q "explain plan actions=1, description=1 select n, sum(x) OVER (ORDER BY n, x ROWS BETWEEN 100 PRECEDING AND CURRENT ROW) from ${name}_n SETTINGS optimize_read_in_window_order=1" | grep -i "sort description"
+$CLICKHOUSE_CLIENT -q "explain pipeline select n, sum(x) OVER (ORDER BY n, x ROWS BETWEEN 100 PRECEDING AND CURRENT ROW) from ${name}_n SETTINGS optimize_read_in_window_order=1, max_threads=1" | grep -i "MergeTreeInOrder" | eval $TRIM_LEADING_SPACES
 
 echo 'No sorting plan'
 echo '  optimize_read_in_window_order=0'
