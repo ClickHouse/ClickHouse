5c5ecdbba1613c6f091637467a1e9d7eef5a6d7f
Denny Crane <denis.zhuravlov@gmail.com>
2022-09-10 15:20:47 -0300

    fix incorrect 02149_read_in_order_fixed_prefix.sql test

--- tests/queries/0_stateless/02149_read_in_order_fixed_prefix.sql
+++ tests/queries/0_stateless/02149_read_in_order_fixed_prefix.sql
@@ -58,5 +58,13 @@ INSERT INTO t_read_in_order SELECT toDateTime('2020-10-10 00:00:00') + number, 1
 EXPLAIN PIPELINE SELECT toStartOfDay(dt) as date, d FROM t_read_in_order ORDER BY date, round(d) LIMIT 5;
 SELECT toStartOfDay(dt) as date, d FROM t_read_in_order ORDER BY date, round(d) LIMIT 5;
 
+SELECT * from (
+  SELECT toStartOfDay(dt) as date, d FROM t_read_in_order ORDER BY date, round(d) LIMIT 50000000000
+  -- subquery with limit 50000000 to stabilize a test result and prevent order by d pushdown
+) order by d limit 5;
+
 EXPLAIN PIPELINE SELECT toStartOfDay(dt) as date, d FROM t_read_in_order ORDER BY date, round(d) LIMIT 5;
-SELECT toStartOfDay(dt) as date, d FROM t_read_in_order WHERE date = '2020-10-10' ORDER BY round(d) LIMIT 5;
+SELECT * from (
+  SELECT toStartOfDay(dt) as date, d FROM t_read_in_order WHERE date = '2020-10-10' ORDER BY round(d) LIMIT 50000000000
+  -- subquery with limit 50000000 to stabilize a test result and prevent order by d pushdown
+) order by d limit 5;
