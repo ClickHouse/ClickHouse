7ed2e6fdca5aa327b1a9f6a5775c02e2fb23a3e5
BoloniniD <denis@bolonin.ru>
2022-09-11 15:42:24 +0300

    Add embedded docs for BLAKE3

--- src/Functions/FunctionsHashing.cpp
+++ src/Functions/FunctionsHashing.cpp
@@ -44,8 +44,19 @@ REGISTER_FUNCTION(Hashing)
 
 
 #if USE_BLAKE3
-    factory.registerFunction<FunctionBLAKE3>();
+    factory.registerFunction<FunctionBLAKE3>(
+    {
+        R"(
+Calculates BLAKE3 hash string and returns the resulting set of bytes as FixedString.
+This cryptographic hash-function is integrated into ClickHouse with BLAKE3 Rust library.
+The function is rather fast and shows approximately two times faster performance compared to SHA-2, while generating hashes of the same length as SHA-256.
+It returns a BLAKE3 hash as a byte array with type FixedString(32).
+)",
+        Documentation::Examples{
+            {"hash", "SELECT hex(BLAKE3('ABC'))"}},
+        Documentation::Categories{"Hash"}
+    },
+    FunctionFactory::CaseInsensitive);
 #endif
-
 }
 }
