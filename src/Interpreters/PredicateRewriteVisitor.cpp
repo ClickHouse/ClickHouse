a14aae8f1dc1350aefa60c7d4c6b9d4e7e08498e
Alexander Tokmakov <tavplubix@clickhouse.com>
2022-09-13 20:44:45 +0200

    fix WHERE pushdown when subquery has OFFSET

--- src/Interpreters/PredicateRewriteVisitor.cpp
+++ src/Interpreters/PredicateRewriteVisitor.cpp
@@ -138,8 +138,10 @@ bool PredicateRewriteVisitorData::rewriteSubquery(ASTSelectQuery & subquery, con
     if ((!optimize_final && subquery.final())
         || (!optimize_with && subquery.with())
         || subquery.withFill()
-        || subquery.limitBy() || subquery.limitLength()
-        || hasNonRewritableFunction(subquery.select(), getContext()))
+        || subquery.limitBy() || subquery.limitByLength() || subquery.limitByOffset()
+        || hasNonRewritableFunction(subquery.select(), getContext())
+        || (subquery.orderBy() && (subquery.limitLength()
+                                || subquery.limitOffset())))
         return false;
 
     Names outer_columns = table_columns.columns.getNames();
