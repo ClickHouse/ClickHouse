e0f9f208e04a43c971547f4a055649cca83f9960
Alexander Tokmakov <tavplubix@clickhouse.com>
2022-09-13 14:55:11 +0300

    Merge pull request #38262 from PolyProgrammist/fix-ordinary-system-unfreeze
    Fix SYSTEM UNFREEZE for ordinary database

--- src/Interpreters/InterpreterSystemQuery.cpp
+++ src/Interpreters/InterpreterSystemQuery.cpp
@@ -539,7 +539,7 @@ BlockIO InterpreterSystemQuery::execute()
         {
             getContext()->checkAccess(AccessType::SYSTEM_UNFREEZE);
             /// The result contains information about deleted parts as a table. It is for compatibility with ALTER TABLE UNFREEZE query.
-            result = Unfreezer().unfreeze(query.backup_name, getContext());
+            result = Unfreezer(getContext()).systemUnfreeze(query.backup_name);
             break;
         }
         default:
