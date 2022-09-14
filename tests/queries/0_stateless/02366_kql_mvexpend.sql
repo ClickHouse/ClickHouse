cfdec48bdf1d9aba61004d58b848d45797a1cbd4
ltrk2 <107155950+ltrk2@users.noreply.github.com>
2022-09-14 13:27:53 -0700

    Implement functional tests for KQL functions

--- tests/queries/0_stateless/02366_kql_mvexpend.sql
+++ tests/queries/0_stateless/02366_kql_mvexpend.sql
@@ -1,21 +1,35 @@
-DROP TABLE IF EXISTS mv_expend_test_table;
-CREATE TABLE mv_expend_test_table
+-- datatable(a: int, b: dynamic, c: dynamic, d: dynamic) [
+--    1, dynamic(['Salmon', 'Steak', 'Chicken']), dynamic([1, 2, 3, 4]), dynamic([5, 6, 7, 8])
+-- ]
+
+DROP TABLE IF EXISTS mv_expand_test_table;
+CREATE TABLE mv_expand_test_table
 (    
    a UInt8,
    b Array(String),
    c Array(Int8),
    d Array(Int8)
 ) ENGINE = Memory;
-INSERT INTO mv_expend_test_table VALUES (1, ['Salmon', 'Steak','Chicken'],[1,2,3,4],[5,6,7,8]);
+INSERT INTO mv_expand_test_table VALUES (1, ['Salmon', 'Steak','Chicken'],[1,2,3,4],[5,6,7,8]);
 set dialect='kusto';
-print '-- mv-expend';
-mv_expend_test_table | mv-expand c;
-mv_expend_test_table | mv-expand c, d;
-mv_expend_test_table | mv-expand b | mv-expand c;
-mv_expend_test_table | mv-expand with_itemindex=index b, c, d;
-mv_expend_test_table | mv-expand array_concat(c,d);
-mv_expend_test_table | mv-expand x = c, y = d;
-mv_expend_test_table | mv-expand xy = array_concat(c, d);
-mv_expend_test_table | mv-expand xy = array_concat(c, d) limit 2| summarize count() by xy;
-mv_expend_test_table | mv-expand with_itemindex=index c,d to typeof(bool);
--- mv_expend_test_table | mv-expand c to typeof(bool);
+print '-- mv-expand --';
+print '-- mv_expand_test_table | mv-expand c --';
+mv_expand_test_table | mv-expand c;
+print '-- mv_expand_test_table | mv-expand c, d --';
+mv_expand_test_table | mv-expand c, d;
+print '-- mv_expand_test_table | mv-expand b | mv-expand c --';
+mv_expand_test_table | mv-expand b | mv-expand c;
+print '-- mv_expand_test_table | mv-expand with_itemindex=index b, c, d --';
+mv_expand_test_table | mv-expand with_itemindex=index b, c, d;
+print '-- mv_expand_test_table | mv-expand array_concat(c,d) --';
+mv_expand_test_table | mv-expand array_concat(c,d);
+print '-- mv_expand_test_table | mv-expand x = c, y = d --';
+mv_expand_test_table | mv-expand x = c, y = d;
+print '-- mv_expand_test_table | mv-expand xy = array_concat(c, d) --';
+mv_expand_test_table | mv-expand xy = array_concat(c, d);
+print '-- mv_expand_test_table | mv-expand xy = array_concat(c, d) limit 2| summarize count() by xy --';
+mv_expand_test_table | mv-expand xy = array_concat(c, d) limit 2| summarize count() by xy;
+print '-- mv_expand_test_table | mv-expand with_itemindex=index c,d to typeof(bool) --';
+mv_expand_test_table | mv-expand with_itemindex=index c,d to typeof(bool);
+print '-- mv_expand_test_table | mv-expand c to typeof(bool) --';
+mv_expand_test_table | mv-expand c to typeof(bool);
--- tests/queries/0_stateless/02366_kql_summarize.sql
+++ tests/queries/0_stateless/02366_kql_summarize.sql
@@ -1,3 +1,18 @@
+-- datatable(FirstName:string, LastName:string, Occupation:string, Education:string, Age:int) [
+--     'Theodore', 'Diaz', 'Skilled Manual', 'Bachelors', 28, 
+--     'Stephanie', 'Cox', 'Management abcd defg', 'Bachelors', 33, 
+--     'Peter', 'Nara', 'Skilled Manual', 'Graduate Degree', 26, 
+--     'Latoya', 'Shen', 'Professional', 'Graduate Degree', 25, 
+--     'Joshua', 'Lee', 'Professional', 'Partial College', 26, 
+--     'Edward', 'Hernandez', 'Skilled Manual', 'High School', 36, 
+--     'Dalton', 'Wood', 'Professional', 'Partial College', 42, 
+--     'Christine', 'Nara', 'Skilled Manual', 'Partial College', 33, 
+--     'Cameron', 'Rodriguez', 'Professional', 'Partial College', 28, 
+--     'Angel', 'Stewart', 'Professional', 'Partial College', 46, 
+--     'Apple', '', 'Skilled Manual', 'Bachelors', 28, 
+--     dynamic(null), 'why', 'Professional', 'Partial College', 38
+-- ]
+
 DROP TABLE IF EXISTS Customers;
 CREATE TABLE Customers
 (    
@@ -8,21 +23,16 @@ CREATE TABLE Customers
     Age Nullable(UInt8)
 ) ENGINE = Memory;
 
--- INSERT INTO Customers VALUES  ('Theodore','Diaz','Skilled Manual','Bachelors',28);
--- INSERT INTO Customers VALUES  ('Stephanie','Cox','Management abcd defg','Bachelors',33);
--- INSERT INTO Customers VALUES  ('Peter','Nara','Skilled Manual','Graduate Degree',26);
--- INSERT INTO Customers VALUES  ('Latoya','Shen','Professional','Graduate Degree',25);
--- INSERT INTO Customers VALUES  ('Joshua','Lee','Professional','Partial College',26);
--- INSERT INTO Customers VALUES  ('Edward','Hernandez','Skilled Manual','High School',36);
--- INSERT INTO Customers VALUES  ('Dalton','Wood','Professional','Partial College',42);
--- INSERT INTO Customers VALUES  ('Christine','Nara','Skilled Manual','Partial College',33);
--- INSERT INTO Customers VALUES  ('Cameron','Rodriguez','Professional','Partial College',28);
--- INSERT INTO Customers VALUES  ('Angel','Stewart','Professional','Partial College',46);
--- INSERT INTO Customers VALUES  ('Apple','','Skilled Manual','Bachelors',28);
--- INSERT INTO Customers VALUES  (NULL,'why','Professional','Partial College',38);
-
 INSERT INTO Customers VALUES  ('Theodore','Diaz','Skilled Manual','Bachelors',28),('Stephanie','Cox','Management abcd defg','Bachelors',33),('Peter','Nara','Skilled Manual','Graduate Degree',26),('Latoya','Shen','Professional','Graduate Degree',25),('Joshua','Lee','Professional','Partial College',26),('Edward','Hernandez','Skilled Manual','High School',36),('Dalton','Wood','Professional','Partial College',42),('Christine','Nara','Skilled Manual','Partial College',33),('Cameron','Rodriguez','Professional','Partial College',28),('Angel','Stewart','Professional','Partial College',46),('Apple','','Skilled Manual','Bachelors',28),(NULL,'why','Professional','Partial College',38);
 
+drop table if exists EventLog;
+create table EventLog
+(
+    LogEntry String,
+    Created Int64
+) ENGINE = Memory;
+
+insert into EventLog values ('Darth Vader has entered the room.', 546), ('Rambo is suspciously looking at Darth Vader.', 245234), ('Darth Sidious electrocutes both using Force Lightning.', 245554);
 
 Select '-- test summarize --' ;
 set dialect='kusto';
@@ -36,38 +46,46 @@ Customers | summarize MySum = sumif(Age, Age<40) by Occupation;
 Customers | summarize dcount(Education);
 Customers | summarize dcountif(Education, Occupation=='Professional');
 Customers | summarize count_ = count() by bin(Age, 10) | order by count_ asc;
+Customers | summarize job_count = count() by Occupation | where job_count > 0;
 
--- make_list()
+print '-- make_list() --';
 Customers | summarize f_list = make_list(Education) by Occupation;
 Customers | summarize f_list = make_list(Education, 2) by Occupation;
--- make_list_if()
+print '-- make_list_if() --';
 Customers | summarize f_list = make_list_if(FirstName, Age>30) by Occupation;
 Customers | summarize f_list = make_list_if(FirstName, Age>30, 1) by Occupation;
--- make_set()
+print '-- make_set() --';
 Customers | summarize f_list = make_set(Education) by Occupation;
 Customers | summarize f_list = make_set(Education, 2) by Occupation;
--- make_set_if()
+print '-- make_set_if() --';
 Customers | summarize f_list = make_set_if(Education, Age>30) by Occupation;
 Customers | summarize f_list = make_set_if(Education, Age>30, 1) by Occupation;
--- stdev()
+print '-- stdev() --';
 Customers | project Age | summarize stdev(Age);
--- stdevif()
+print '-- stdevif() --';
 Customers | project Age | summarize stdevif(Age, Age%2==0);
--- binary_all_and
+print '-- binary_all_and --';
 Customers | project Age | where Age > 40 | summarize binary_all_and(Age);
--- binary_all_or
+print '-- binary_all_or --';
 Customers | project Age | where Age > 40 | summarize binary_all_or(Age);
--- binary_all_xor
+print '-- binary_all_xor --';
 Customers | project Age | where Age > 40 | summarize binary_all_xor(Age);
 
--- TODO:
 Customers | project Age | summarize percentile(Age, 95);
 Customers | project Age | summarize percentiles(Age, 5, 50, 95);
 Customers | project Age | summarize percentiles(Age, 5, 50, 95)[1];
--- Customers | summarize w=count() by AgeBucket=bin(Age, 5) | summarize percentilew(AgeBucket, w, 75); -- expect 35
--- Customers | summarize w=count() by AgeBucket=bin(Age, 5) | summarize percentilesw(AgeBucket, w, 50, 75, 99.9); -- expect 25,35,45
+Customers | summarize w=count() by AgeBucket=bin(Age, 5) | summarize percentilew(AgeBucket, w, 75);
+Customers | summarize w=count() by AgeBucket=bin(Age, 5) | summarize percentilesw(AgeBucket, w, 50, 75, 99.9);
 
+print '-- Summarize following sort --';
+Customers | sort by FirstName | summarize count() by Occupation;
+
+print '-- summarize with bin --';
+EventLog | summarize count=count() by bin(Created, 1000);
+EventLog | summarize count=count() by bin(unixtime_seconds_todatetime(Created/1000), 1s);
+EventLog | summarize count=count() by time_label=bin(Created/1000, 1s);
+
+-- TODO:
 -- arg_max()
 -- arg_min()
 -- make_list_with_nulls()
--- Customers | sort by FirstName | summarize count(Education) by Occupation;
