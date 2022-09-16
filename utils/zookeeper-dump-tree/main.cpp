10d664d612f0aceec559f44a2489358ab039bf24
Antonio Andelic <antonio@clickhouse.com>
2022-09-16 09:55:15 +0000

    Use multiread for dump

--- utils/zookeeper-dump-tree/main.cpp
+++ utils/zookeeper-dump-tree/main.cpp
@@ -1,6 +1,7 @@
 #include <list>
 #include <iostream>
 #include <boost/program_options.hpp>
+#include "Common/ZooKeeper/Types.h"
 #include <Common/Exception.h>
 #include <Common/ZooKeeper/ZooKeeper.h>
 #include <Common/ZooKeeper/KeeperException.h>
@@ -37,8 +38,10 @@ int main(int argc, char ** argv)
 
         std::string initial_path = options.at("path").as<std::string>();
 
-        std::list<std::pair<std::string, std::future<Coordination::ListResponse>>> list_futures;
-        list_futures.emplace_back(initial_path, zookeeper->asyncGetChildren(initial_path));
+        std::list<std::pair<std::vector<std::string>, std::future<Coordination::MultiResponse>>> multi_futures;
+        Coordination::Requests requests;
+        requests.emplace_back(zkutil::makeListRequest(initial_path));
+        multi_futures.emplace_back(std::vector<std::string>{initial_path}, zookeeper->asyncMulti(requests));
 
         size_t num_reconnects = 0;
         constexpr size_t max_reconnects = 100;
@@ -50,15 +53,14 @@ int main(int argc, char ** argv)
                 if (num_reconnects == max_reconnects)
                     return false;
                 ++num_reconnects;
-                std::cerr << "num_reconnects: " << num_reconnects << "\n";
                 zookeeper = zookeeper->startNewSession();
             }
             return true;
         };
 
-        for (auto it = list_futures.begin(); it != list_futures.end(); ++it)
+        for (auto it = multi_futures.begin(); it != multi_futures.end(); ++it)
         {
-            Coordination::ListResponse response;
+            Coordination::MultiResponse response;
 
             try
             {
@@ -75,24 +77,44 @@ int main(int argc, char ** argv)
                     /// Reinitialize the session and move the node to the end of the queue for later retry.
                     if (!ensure_session())
                         throw;
-                    list_futures.emplace_back(it->first, zookeeper->asyncGetChildren(it->first));
+
+                    auto paths = std::move(it->first);
+
+                    Coordination::Requests new_requests;
+                    for (const auto & path : paths)
+                    {
+                        new_requests.emplace_back(zkutil::makeListRequest(path));
+                    }
+                    multi_futures.emplace_back(std::move(paths), zookeeper->asyncMulti(new_requests));
                     continue;
                 }
                 else
                     throw;
             }
 
-            std::cout << it->first << '\t' << response.stat.numChildren << '\t' << response.stat.dataLength;
-            if (dump_ctime)
-                std::cout << '\t' << response.stat.ctime;
-            std::cout << '\n';
-
-            for (const auto & name : response.names)
+            for (size_t i = 0; i < it->first.size(); ++i)
             {
-                std::string child_path = it->first == "/" ? it->first + name : it->first + '/' + name;
+                const auto & path = it->first[i];
+                const auto & list_response = dynamic_cast<const Coordination::ListResponse &>(*response.responses[i]);
+                std::cout << path << '\t' << list_response.stat.numChildren << '\t' << list_response.stat.dataLength;
+                if (dump_ctime)
+                    std::cout << '\t' << list_response.stat.ctime;
+                std::cout << '\n';
 
-                ensure_session();
-                list_futures.emplace_back(child_path, zookeeper->asyncGetChildren(child_path));
+                Coordination::Requests new_requests;
+                std::vector<std::string> children;
+                children.reserve(list_response.names.size());
+                for (const auto & name : list_response.names)
+                {
+                    std::string child_path = path == "/" ? path + name : path + '/' + name;
+
+                    ensure_session();
+                    new_requests.emplace_back(zkutil::makeListRequest(child_path));
+                    children.push_back(std::move(child_path));
+                }
+
+                if (!children.empty())
+                    multi_futures.emplace_back(std::move(children), zookeeper->asyncMulti(new_requests));
             }
         }
 
