822fb71ae53be1d26c659256318f86401e27e930
robot-clickhouse <robot-clickhouse@clickhouse.com>
2022-09-16 00:03:11 +0000

    Backport #41328 to 22.3: Download ccache from release PRs for backports

--- tests/ci/build_check.py
+++ tests/ci/build_check.py
@@ -278,7 +278,14 @@ def main():
     ccache_path = os.path.join(CACHES_PATH, build_name + "_ccache")
 
     logging.info("Will try to fetch cache for our build")
-    get_ccache_if_not_exists(ccache_path, s3_helper, pr_info.number, TEMP_PATH)
+    try:
+        get_ccache_if_not_exists(
+            ccache_path, s3_helper, pr_info.number, TEMP_PATH, pr_info.release_pr
+        )
+    except Exception as e:
+        # In case there are issues with ccache, remove the path and do not fail a build
+        logging.info("Failed to get ccache, building without it. Error: %s", e)
+        rmtree(ccache_path, ignore_errors=True)
 
     if not os.path.exists(ccache_path):
         logging.info("cache was not fetched, will create empty dir")
