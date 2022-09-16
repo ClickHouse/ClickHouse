c9d5654baf7e57ff891c5253c33150334fe1411a
Amesaru <essavin@edu.hse.ru>
2022-09-16 18:07:14 +0000

    trying eval

--- src/Functions/eval.cpp
+++ src/Functions/eval.cpp
@@ -1,13 +1,25 @@
 #include "Common/Exception.h"
-#include "Functions/FunctionHelpers.h"
-#include "Interpreters/Context.h"
-#include "Interpreters/Context_fwd.h"
-#include "Interpreters/executeQuery.h"
-#include "Processors/Executors/PullingPipelineExecutor.h"
-#include <Functions/FunctionFactory.h>
-#include <Functions/IFunction.h>
+
 #include <Columns/ColumnString.h>
 #include <DataTypes/DataTypeString.h>
+
+#include "Functions/FunctionHelpers.h"
+#include <Functions/FunctionFactory.h>
+#include <Functions/IFunction.h>
+
+#include "Interpreters/Context.h"
+#include "Interpreters/Context_fwd.h"
+#include <Interpreters/InterpreterSelectQuery.h>
+#include "Interpreters/ReplaceQueryParameterVisitor.h"
+
+#include "Parsers/ParserSelectQuery.h"
+#include "Parsers/formatAST.h"
+#include "Parsers/parseQuery.h"
+
+#include "Processors/Executors/PullingPipelineExecutor.h"
+
+
+
 
 namespace DB 
 {
@@ -24,11 +36,9 @@ class FunctionEval final : public IFunction
 public:
     static constexpr auto name = "eval";
 
-    explicit FunctionEval(ContextPtr context_) : context(context_) {
-        
-    }
+    static FunctionPtr create(ContextPtr context_) { return std::make_shared<FunctionEval>(context_); }
 
-    static FunctionPtr create(ContextMutablePtr context) { return std::make_shared<FunctionEval>(context); }
+    explicit FunctionEval(ContextPtr context_) : context(context_) {}
 
     std::string getName() const override { return name; }
 
@@ -50,9 +60,37 @@ public:
 
         String query = query_column->getValue<String>();
 
-        // Get parameter
-        const ColumnConst * param = checkAndGetColumnConst<ColumnString>(arguments[1].column.get());
+        // parsing query
+        ParserSelectQuery parser;
+        ASTPtr parsed_query = parseQuery(parser, query, 1000, 1000);
+        
+        // transfering parameters from arguments to context
+        ContextMutablePtr new_context;
+        new_context = Context::createCopy(context);
 
+        // TODO new_context->setQueryParameters(const NameToNameMap &parameters) 
+
+        //replacing parameters in parsed query
+        ReplaceQueryParameterVisitor visitor(new_context->getQueryParameters());
+        visitor.visit(parsed_query);
+
+        // interpreting query
+        InterpreterSelectQuery interpreter(parsed_query, new_context, SelectQueryOptions());
+        auto io = interpreter.execute();
+        PullingPipelineExecutor executor(io.pipeline);
+
+        Block block;
+        while (executor.pull(block)) {}
+        
+        if (block.rows() != 1)
+            throw Exception("correlated subquery returned " + toString(block.rows()) + " rows, not 1",
+                ErrorCodes::LOGICAL_ERROR);
+
+        if (block.columns() != 1)
+            throw Exception("correlated subquery returned " + toString(block.columns()) + " columns, not 1",
+                ErrorCodes::LOGICAL_ERROR);                
+        
+        
     }
 
 private:
