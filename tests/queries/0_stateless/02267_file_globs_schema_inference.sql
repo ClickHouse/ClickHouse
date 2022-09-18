7e0f61791029fe5f7233d91d7e790f3dec4ff160
Alexey Milovidov <milovidov@clickhouse.com>
2022-09-18 08:54:48 +0200

    Fix tests

--- tests/queries/0_stateless/02267_file_globs_schema_inference.sql
+++ tests/queries/0_stateless/02267_file_globs_schema_inference.sql
@@ -3,7 +3,7 @@
 insert into function file('02267_data2.jsonl') select NULL as x;
 insert into function file('02267_data3.jsonl') select * from numbers(0);
 insert into function file('02267_data4.jsonl') select 1 as x;
-select * from file('02267_data{2..4}.jsonl') order by x;
+select * from file('02267_data*.jsonl') order by x;
 
 insert into function file('02267_data1.jsonl', 'TSV') select 1 as x;
 insert into function file('02267_data1.jsonl', 'TSV') select [1,2,3] as x;
