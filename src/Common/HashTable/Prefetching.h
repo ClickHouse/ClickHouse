712bc27de7265ed8a7b38f9a2528aa8c3c66ed7b
Nikita Taranov <nikita.taranov@clickhouse.com>
2022-09-19 17:14:43 +0200

    fix

--- src/Common/HashTable/Prefetching.h
+++ src/Common/HashTable/Prefetching.h
@@ -34,7 +34,7 @@ public:
         static constexpr auto just_coefficient = 4;
         const auto single_iteration_latency = std::max<double>(1.0L * watch.elapsedNanoseconds() / iterations_to_measure, 1);
         return std::clamp<size_t>(
-            just_coefficient * assumed_load_latency_ns / single_iteration_latency, min_look_ahead_value, max_look_ahead_value);
+            ceil(just_coefficient * assumed_load_latency_ns / single_iteration_latency), min_look_ahead_value, max_look_ahead_value);
     }
 
     static constexpr size_t getInitialLookAheadValue() { return min_look_ahead_value; }
@@ -43,7 +43,7 @@ public:
 
 private:
     static constexpr size_t iterations_to_measure = 100;
-    static constexpr size_t min_look_ahead_value = 8;
+    static constexpr size_t min_look_ahead_value = 4;
     static constexpr size_t max_look_ahead_value = 32;
 
     Stopwatch watch;
