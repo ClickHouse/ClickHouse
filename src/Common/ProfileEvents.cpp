6ed53585abdf9c76c012bfe6cdd51bdf88d572b6
serxa <sergei@clickhouse.com>
2022-09-19 16:21:09 +0000

    rearrange and fix profile event descriptions

--- src/Common/ProfileEvents.cpp
+++ src/Common/ProfileEvents.cpp
@@ -301,6 +301,18 @@ The server successfully detected this situation and will download merged part fr
     M(DiskS3WriteRequestsThrottling, "Number of 429 and 503 errors in POST, DELETE, PUT and PATCH requests to DiskS3 storage.") \
     M(DiskS3WriteRequestsRedirects, "Number of redirects in POST, DELETE, PUT and PATCH requests to DiskS3 storage.") \
     \
+    M(DeleteS3Objects, "Number of S3 API DeleteObjects calls.") \
+    M(CopyS3Object, "Number of S3 API CopyObject calls.") \
+    M(ListS3Objects, "Number of S3 API ListObjects calls.") \
+    M(HeadS3Object,  "Number of S3 API HeadObject calls.") \
+    M(CreateS3MultipartUpload, "Number of S3 API CreateMultipartUpload calls.") \
+    M(UploadS3PartCopy, "Number of S3 API UploadPartCopy calls.") \
+    M(UploadS3Part, "Number of S3 API UploadPart calls.") \
+    M(AbortS3MultipartUpload, "Number of S3 API AbortMultipartUpload calls.") \
+    M(CompleteS3MultipartUpload, "Number of S3 API CompleteMultipartUpload calls.") \
+    M(PutS3ObjectRequest, "Number of S3 API PutObject calls.") \
+    M(GetS3ObjectRequest, "Number of S3 API GetObject calls.") \
+    \
     M(ReadBufferFromS3Microseconds, "Time spend in reading from S3.") \
     M(ReadBufferFromS3Bytes, "Bytes read from S3.") \
     M(ReadBufferFromS3RequestsErrors, "Number of exceptions while reading from S3.") \
@@ -410,17 +422,7 @@ The server successfully detected this situation and will download merged part fr
     M(OverflowBreak, "Number of times, data processing was cancelled by query complexity limitation with setting '*_overflow_mode' = 'break' and the result is incomplete.") \
     M(OverflowThrow, "Number of times, data processing was cancelled by query complexity limitation with setting '*_overflow_mode' = 'throw' and exception was thrown.") \
     M(OverflowAny, "Number of times approximate GROUP BY was in effect: when aggregation was performed only on top of first 'max_rows_to_group_by' unique keys and other keys were ignored due to 'group_by_overflow_mode' = 'any'.") \
-    M(DeleteS3Objects, "Number of s3 API DeleteObjects be called") \
-    M(CopyS3Object, "Number of s3 API CopyObject be called") \
-    M(ListS3Objects, "Number of s3 API ListObjects be called") \
-    M(HeadS3Object,  "Number of s3 API HeadObject be called") \
-    M(CreateS3MultipartUpload, "Number of s3 API CreateMultipartUpload be called") \
-    M(UploadS3PartCopy, "Number of s3 API UploadPartCopy be called") \
-    M(UploadS3Part, "Number of s3 API UploadS3Part be called") \
-    M(AbortS3MultipartUpload, "Number of s3 API AbortMultipartUpload be called") \
-    M(CompleteS3MultipartUpload, "Number of s3 API CompleteS3MultipartUpload be called") \
-    M(PutS3ObjectRequest, "Number of s3 API PutS3ObjectRequest be called") \
-    M(GetS3ObjectRequest, "Number of s3 API GetS3ObjectRequest be called")
+
 
 namespace ProfileEvents
 {
