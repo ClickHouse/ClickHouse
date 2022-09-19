4166f9734bf5338e190c1bac8653254a2618e597
Robert Schulze <robert@clickhouse.com>
2022-09-19 08:51:21 +0000

    Fix non-deterministic test results uncovered by SSE2-only build

--- tests/queries/0_stateless/02006_test_positional_arguments.sql
+++ tests/queries/0_stateless/02006_test_positional_arguments.sql
@@ -15,8 +15,8 @@ select x3, x2, x1 from test order by 1 desc;
 select x3, x2, x1 from test order by x3 desc;
 
 insert into test values (1, 10, 100), (10, 1, 10), (100, 100, 1);
-select x3, x2 from test group by x3, x2;
-select x3, x2 from test group by 1, 2;
+select x3, x2 from test group by x3, x2 order by x3;
+select x3, x2 from test group by 1, 2 order by x3;
 
 select x1, x2, x3 from test order by x3 limit 1 by x3;
 select x1, x2, x3 from test order by 3 limit 1 by 3;
