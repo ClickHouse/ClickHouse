fb3ebbb2d20d5077c81cb5b2b71d8b8e63b68c29
Mikhail Filimonov <mfilimonov@altinity.com>
2022-09-20 12:12:01 +0200

    Apply style fix for tests/integration/test_disk_types/test.py

--- tests/integration/test_disk_types/test.py
+++ tests/integration/test_disk_types/test.py
@@ -29,21 +29,27 @@ def cluster():
 
 def test_different_types(cluster):
     node = cluster.instances["node"]
-    response = TSV.toMat( node.query("SELECT * FROM system.disks FORMAT TSVWithNames") )
+    response = TSV.toMat(node.query("SELECT * FROM system.disks FORMAT TSVWithNames"))
 
-    assert len(response) > len(disk_types) # at least one extra line for header
+    assert len(response) > len(disk_types)  # at least one extra line for header
 
-    name_col_ix      = response[0].index("name")
-    type_col_ix      = response[0].index("type")
+    name_col_ix = response[0].index("name")
+    type_col_ix = response[0].index("type")
     encrypted_col_ix = response[0].index("is_encrypted")
 
     for fields in response[1:]:  # skip header
         assert len(fields) >= 7
-        assert disk_types.get(fields[name_col_ix], "UNKNOWN") == fields[type_col_ix], f"Wrong type ({fields[type_col_ix]}) for disk {fields[name_col_ix]}!"
+        assert (
+            disk_types.get(fields[name_col_ix], "UNKNOWN") == fields[type_col_ix]
+        ), f"Wrong type ({fields[type_col_ix]}) for disk {fields[name_col_ix]}!"
         if "encrypted" in fields[name_col_ix]:
-            assert fields[encrypted_col_ix] == "1", f"{fields[name_col_ix]} expected to be encrypted!"
+            assert (
+                fields[encrypted_col_ix] == "1"
+            ), f"{fields[name_col_ix]} expected to be encrypted!"
         else:
-            assert fields[encrypted_col_ix] == "0", f"{fields[name_col_ix]} expected to be non-encrypted!"
+            assert (
+                fields[encrypted_col_ix] == "0"
+            ), f"{fields[name_col_ix]} expected to be non-encrypted!"
 
 
 def test_select_by_type(cluster):
