7c0d46bbdcf4767112af1555721c58daffa43d57
Nicolae Vartolomei <nv@cloudflare.com>
2022-09-20 19:03:20 +0000

    Get rid of scary setting for testing fetching byte identical parts

--- tests/integration/test_mutation_fetch_fallback/test.py
+++ tests/integration/test_mutation_fetch_fallback/test.py
@@ -12,14 +12,13 @@ node2 = cluster.add_instance("node2", with_zookeeper=True, stay_alive=True)
 def start_cluster():
     try:
         cluster.start()
-        for i, node in enumerate([node1, node2]):
-            # node 2 will corrupt checksums, it must be down while node 1 commits mutation
-            # with correct checksums to zookeeper.
+        for ix, node in enumerate([node1, node2]):
             node.query_with_retry(
-                """CREATE TABLE fetch_fallback (k int, v int, z int)
+                """CREATE TABLE fetch_fallback (k int, v int, z String)
                 ENGINE = ReplicatedMergeTree('/clickhouse/tables/test/t0', '{}')
-                ORDER BY tuple()
-                SETTINGS testing_mutate_corrupt_checksums = {}""".format(i, i == 1)
+                ORDER BY tuple()""".format(
+                    ix
+                )
             )
         yield cluster
 
@@ -31,10 +30,13 @@ def test_mutation_fetch_fallback(start_cluster):
     node1.query("INSERT INTO fetch_fallback(k, v) VALUES (1, 3), (2, 7), (3, 4)")
 
     node2.stop_clickhouse()
-    node1.query("ALTER TABLE fetch_fallback DROP COLUMN z", settings={"mutations_sync": 1})
+    node1.query(
+        "ALTER TABLE fetch_fallback UPDATE z = hostName() WHERE 1 = 1",
+        settings={"mutations_sync": 1, "allow_nondeterministic_mutations": 1},
+    )
 
     node2.start_clickhouse()
     node1.query("SYSTEM SYNC REPLICA fetch_fallback", timeout=10)
     node2.query("SYSTEM SYNC REPLICA fetch_fallback", timeout=10)
 
-    node2.contains_in_log("We will download merged part from replica to force byte-identical result.")
+    assert node2.contains_in_log("We will download merged part from replica to force byte-identical result.")
