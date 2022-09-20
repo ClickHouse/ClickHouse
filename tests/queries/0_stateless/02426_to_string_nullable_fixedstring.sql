c1721cc4fc504f130d045acd4c5805c066721c54
Duc Canh Le <canhld94@gmail.com>
2022-09-20 13:27:34 +0800

    fix tests

--- tests/queries/0_stateless/02426_to_string_nullable_fixedstring.sql
+++ tests/queries/0_stateless/02426_to_string_nullable_fixedstring.sql
@@ -1,2 +1,2 @@
 SELECT CAST('a', 'Nullable(FixedString(1))') as s,  toTypeName(s), toString(s);
-SELECT toTypeName(s), toString(s) FROM (SELECT if(number % 3 = 0, NULL, toFixedString(toString(number), 1)) AS s from numbers(10))
+SELECT number, toTypeName(s), toString(s) FROM (SELECT number, if(number % 3 = 0, NULL, toFixedString(toString(number), 1)) AS s from numbers(10)) ORDER BY number;
