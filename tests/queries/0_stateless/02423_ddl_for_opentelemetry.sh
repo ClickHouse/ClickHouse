7489a95f0be86a0ba26236a99744ee42ed3d5e91
Frank Chen <frank.chen021@outlook.com>
2022-09-23 11:33:00 +0800

    Update test case to satisfy the ddl_format_version

--- tests/queries/0_stateless/02423_ddl_for_opentelemetry.sh
+++ tests/queries/0_stateless/02423_ddl_for_opentelemetry.sh
@@ -31,8 +31,15 @@ function execute_query()
 # $1 - OpenTelemetry Trace Id
 # $2 - Fields
 # $3 - operation_name pattern
+# $4 - extra condition
 function check_span()
 {
+    if [ -n "$4" ]; then
+        extra_condition=" AND ${4}"
+    else
+        extra_condition=""
+    fi
+
 ${CLICKHOUSE_CLIENT} -nq "
     SYSTEM FLUSH LOGS;
 
@@ -41,6 +48,8 @@ ${CLICKHOUSE_CLIENT} -nq "
     WHERE finish_date >= yesterday()
     AND   lower(hex(trace_id)) =    '${1}'
     AND   operation_name       like '${3}'
+    ${extra_condition}
+    Format TSKV
     ;"
 }
 
@@ -51,53 +60,56 @@ ${CLICKHOUSE_CLIENT} -q "
 DROP TABLE IF EXISTS ddl_test_for_opentelemetry;
 "
 
-case_no=1;
+# Support Replicated database engine
+cluster_name=$($CLICKHOUSE_CLIENT -q "select if(engine = 'Replicated', name, 'test_shard_localhost')  from system.databases where name='$CLICKHOUSE_DATABASE'")
 
 #
-# normal cases for ALL distributed_ddl_entry_format_version
+# Normal cases for ALL distributed_ddl_entry_format_version.
+# Only format_version 4 enables the tracing
 #
-for ddl_version in 1 2 3; do
+for ddl_version in 1 2 3 4; do
     # Echo a separator so that the reference file is more clear for reading
-    echo "===case ${case_no}===="
+    echo "===ddl_format_version ${ddl_version}===="
 
     trace_id=$(${CLICKHOUSE_CLIENT} -q "select lower(hex(generateUUIDv4()))");
-    execute_query $trace_id "CREATE TABLE ddl_test_for_opentelemetry ON CLUSTER test_shard_localhost (id UInt64) Engine=MergeTree ORDER BY id" "distributed_ddl_output_mode=none&distributed_ddl_entry_format_version=${ddl_version}" "/dev/null"
+    execute_query $trace_id "CREATE TABLE ddl_test_for_opentelemetry ON CLUSTER ${cluster_name} (id UInt64) Engine=MergeTree ORDER BY id" "distributed_ddl_output_mode=none&distributed_ddl_entry_format_version=${ddl_version}" "/dev/null"
 
-    check_span $trace_id "count()" "HTTPHandler"
-    check_span $trace_id "count()" "%DDLWorker::processTask%"
-    check_span $trace_id "attribute['clickhouse.cluster']" "%executeDDLQueryOnCluster%"
+    check_span $trace_id "count() AS httpHandler" "HTTPHandler"
+    check_span $trace_id "count() AS executeDDLQueryOnCluster" "%executeDDLQueryOnCluster%" "attribute['clickhouse.cluster']='${cluster_name}'"
+    check_span $trace_id "count() AS processTask" "%DDLWorker::processTask%"
 
-    # There should be two 'query' spans,
-    # one is for the HTTPHandler, the other is for the DDL executing in DDLWorker
-    check_span $trace_id "count()" "query"
+    # For format_version 4, there should be two 'query' spans,
+    # one is for the HTTPHandler, the other is for the DDL executing in DDLWorker.
+    #
+    # For other format, there should be only one 'query' span
+    #
+    check_span $trace_id "count() AS query" "query"
 
     # Remove table
     ${CLICKHOUSE_CLIENT} -q "
         DROP TABLE IF EXISTS ddl_test_for_opentelemetry;
     "
-
-    case_no=$(($case_no + 1))
 done
 
 #
 # an exceptional case, DROP a non-exist table
 #
 # Echo a separator so that the reference file is more clear for reading
-echo "===case ${case_no}===="
+echo "===exception===="
 
 # Since this query is supposed to fail, we redirect the error message to /dev/null to discard the error message so that it won't pollute the reference file.
 # The exception will be checked in the span log
 trace_id=$(${CLICKHOUSE_CLIENT} -q "select lower(hex(generateUUIDv4()))");
-execute_query $trace_id "DROP TABLE ddl_test_for_opentelemetry_non_exist ON CLUSTER test_shard_localhost" "distributed_ddl_output_mode=none" "/dev/null"
+execute_query $trace_id "DROP TABLE ddl_test_for_opentelemetry_non_exist ON CLUSTER ${cluster_name}" "distributed_ddl_output_mode=none&distributed_ddl_entry_format_version=4" "/dev/null"
 
-check_span $trace_id "count()" "HTTPHandler"
-check_span $trace_id "count()" "%DDLWorker::processTask%"
-check_span $trace_id "attribute['clickhouse.cluster']" "%executeDDLQueryOnCluster%"
+check_span $trace_id "count() AS httpHandler" "HTTPHandler"
+check_span $trace_id "count() AS executeDDLQueryOnCluster" "%executeDDLQueryOnCluster%" "attribute['clickhouse.cluster']='${cluster_name}'"
+check_span $trace_id "count() AS processTask" "%DDLWorker::processTask%"
 
 # There should be two 'query' spans,
 # one is for the HTTPHandler, the other is for the DDL executing in DDLWorker.
 # Both of these two spans contain exception
-check_span $trace_id "concat('exception_code=', attribute['clickhouse.exception_code'])" "query"
+check_span $trace_id "attribute['clickhouse.exception_code'] AS exceptionCode" "query"
 
 #
 # Tear down
