e2bca52eeb04c910aa29a1b0a1b10aa2f03ff354
Alfonso Martinez <alfonso@tinybird.co>
2022-09-23 13:08:27 +0200

    Fixed case for FixedString

--- src/Functions/FunctionsJSON.cpp
+++ src/Functions/FunctionsJSON.cpp
@@ -23,6 +23,7 @@
 #include <DataTypes/Serializations/SerializationDecimal.h>
 #include <DataTypes/DataTypesNumber.h>
 #include <DataTypes/DataTypeLowCardinality.h>
+#include <DataTypes/DataTypeFixedString.h>
 #include <DataTypes/DataTypeString.h>
 #include <DataTypes/DataTypesDecimal.h>
 #include <DataTypes/DataTypeUUID.h>
@@ -816,25 +817,26 @@ struct JSONExtractTree
         }
     };
 
-    class LowCardinalityNode : public Node
+    class LowCardinalityFixedStringNode : public Node
     {
     public:
-        LowCardinalityNode(DataTypePtr dictionary_type_, std::unique_ptr<Node> impl_)
-            : dictionary_type(dictionary_type_), impl(std::move(impl_)) {}
+        LowCardinalityFixedStringNode(const size_t fixed_length_)
+            : fixed_length(fixed_length_) {}
         bool insertResultToColumn(IColumn & dest, const Element & element) override
         {
-            auto from_col = dictionary_type->createColumn();
-            if (impl->insertResultToColumn(*from_col, element))
-            {
-                std::string_view value = from_col->getDataAt(0).toView();
-                assert_cast<ColumnLowCardinality &>(dest).insertData(value.data(), value.size());
-                return true;
-            }
-            return false;
+            auto str = element.getString();
+            
+            if (str.size() > fixed_length)
+                return false;
+
+            auto padded_str = str.data() + std::string(fixed_length - std::min(fixed_length, str.length()), '\0');
+
+            assert_cast<ColumnLowCardinality &>(dest).insertData(padded_str.data(), padded_str.size());
+            return true;
+
         }
     private:
-        DataTypePtr dictionary_type;
-        std::unique_ptr<Node> impl;
+        const size_t fixed_length;
     };
 
     class UUIDNode : public Node
@@ -1122,8 +1124,10 @@ struct JSONExtractTree
                 auto dictionary_type = typeid_cast<const DataTypeLowCardinality *>(type.get())->getDictionaryType();
                 auto impl = build(function_name, dictionary_type);
 
-                if ((*dictionary_type).getTypeId() == TypeIndex::FixedString)
-                    return std::make_unique<LowCardinalityNode>(dictionary_type, std::move(impl)); 
+                if ((*dictionary_type).getTypeId() == TypeIndex::FixedString) {
+                    auto fixed_length = typeid_cast<const DataTypeFixedString *>(dictionary_type.get())->getN();
+                    return std::make_unique<LowCardinalityFixedStringNode>(fixed_length); 
+                }
                 else
                     return impl; 
             }
