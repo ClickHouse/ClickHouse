307314e1bd8cc93bcfa52abdef6d605e9381ce4e
Kseniia Sumarokova <54203879+kssenii@users.noreply.github.com>
2022-09-24 13:42:58 +0200

    Update CachedOnDiskReadBufferFromFile.cpp

--- src/Disks/IO/CachedOnDiskReadBufferFromFile.cpp
+++ src/Disks/IO/CachedOnDiskReadBufferFromFile.cpp
@@ -250,7 +250,7 @@ CachedOnDiskReadBufferFromFile::getReadBufferForFileSegment(FileSegmentPtr & fil
         }
         else
         {
-            LOG_TRACE(log, "Bypassing cache because `read_from_filesystem_cache_if_exists_otherwise_bypass_cache` option is used");
+            LOG_TEST(log, "Bypassing cache because `read_from_filesystem_cache_if_exists_otherwise_bypass_cache` option is used");
             read_type = ReadType::REMOTE_FS_READ_BYPASS_CACHE;
             return getRemoteFSReadBuffer(*file_segment, read_type);
         }
