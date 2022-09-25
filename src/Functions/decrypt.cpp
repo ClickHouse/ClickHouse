e882b4f69445c18bc6914f689ef4e293125c058f
Duc Canh Le <canhld94@gmail.com>
2022-09-25 13:50:22 +0800

    remove settings, add tryDecrypt function

--- src/Functions/decrypt.cpp
+++ src/Functions/decrypt.cpp
@@ -12,6 +12,14 @@ struct DecryptImpl
 {
     static constexpr auto name = "decrypt";
     static constexpr auto compatibility_mode = OpenSSLDetails::CompatibilityMode::OpenSSL;
+    static constexpr bool use_null_when_decrypt_fail = false;
+};
+
+struct TryDecryptImpl
+{
+    static constexpr auto name = "tryDecrypt";
+    static constexpr auto compatibility_mode = OpenSSLDetails::CompatibilityMode::OpenSSL;
+    static constexpr bool use_null_when_decrypt_fail = true;
 };
 
 }
@@ -22,6 +30,7 @@ namespace DB
 REGISTER_FUNCTION(Decrypt)
 {
     factory.registerFunction<FunctionDecrypt<DecryptImpl>>();
+    factory.registerFunction<FunctionDecrypt<TryDecryptImpl>>();
 }
 
 }
