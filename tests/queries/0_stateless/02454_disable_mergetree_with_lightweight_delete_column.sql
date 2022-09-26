61af9ec2b0c1c5b14329f5d0c4e561ea681305b3
Alexander Gololobov <440544+davenger@users.noreply.github.com>
2022-09-26 19:12:52 +0200

    Added test case for drop column _row_exists

--- tests/queries/0_stateless/02454_disable_mergetree_with_lightweight_delete_column.sql
+++ tests/queries/0_stateless/02454_disable_mergetree_with_lightweight_delete_column.sql
@@ -3,8 +3,10 @@ drop table if exists t_row_exists;
 create table t_row_exists(a int, _row_exists int) engine=MergeTree order by a; --{serverError 44}
 
 create table t_row_exists(a int, b int) engine=MergeTree order by a;
-alter table t_row_exists add column _row_exists int; --{serverError 44}
-alter table t_row_exists rename column b to _row_exists; --{serverError 44}
+alter table t_row_exists add column _row_exists int; --{serverError ILLEGAL_COLUMN}
+alter table t_row_exists rename column b to _row_exists; --{serverError ILLEGAL_COLUMN}
+alter table t_row_exists drop column _row_exists; --{serverError NOT_FOUND_COLUMN_IN_BLOCK}
+alter table t_row_exists drop column unknown_column; --{serverError NOT_FOUND_COLUMN_IN_BLOCK}
 drop table t_row_exists;
 
 create table t_row_exists(a int, _row_exists int) engine=Memory;
@@ -13,6 +15,6 @@ select * from t_row_exists;
 drop table t_row_exists;
 
 create table t_row_exists(a int, b int) engine=Memory;
-alter table t_row_exists add column _row_exists int; --{serverError 48}
-alter table t_row_exists rename column b to _row_exists; --{serverError 48}
+alter table t_row_exists add column _row_exists int; --{serverError NOT_IMPLEMENTED}
+alter table t_row_exists rename column b to _row_exists; --{serverError NOT_IMPLEMENTED}
 drop table t_row_exists;
