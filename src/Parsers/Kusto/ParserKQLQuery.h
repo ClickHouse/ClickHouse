3ed73a4eeb049c505103ac1e5b0299f904c178b1
ltrk2 <107155950+ltrk2@users.noreply.github.com>
2022-09-26 07:48:53 -0700

    Reinstate const qualifiers

--- src/Parsers/Kusto/ParserKQLQuery.h
+++ src/Parsers/Kusto/ParserKQLQuery.h
@@ -9,11 +9,11 @@ class ParserKQLBase : public IParserBase
 {
 public:
     static String getExprFromToken(Pos & pos);
-    static String getExprFromToken(const String & text, uint32_t max_depth);
+    static String getExprFromToken(const String & text, const uint32_t max_depth);
     static String getExprFromPipe(Pos & pos);
     static bool setSubQuerySource(ASTPtr & select_query, ASTPtr & source, bool dest_is_subquery, bool src_is_subquery);
     static bool parseSQLQueryByString(ParserPtr && parser, String & query, ASTPtr & select_node, int32_t max_depth);
-    bool parseByString(const String expr, ASTPtr & node, uint32_t max_depth);
+    bool parseByString(const String expr, ASTPtr & node, const uint32_t max_depth);
 };
 
 class ParserKQLQuery : public IParserBase
