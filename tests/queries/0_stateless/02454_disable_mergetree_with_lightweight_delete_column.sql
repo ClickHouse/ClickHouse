c2c2de8718316bcf334a9d163ceb879bbeb57efe
Alexander Gololobov <440544+davenger@users.noreply.github.com>
2022-09-26 19:21:55 +0200

    Rename _row_exists column test case

--- tests/queries/0_stateless/02454_disable_mergetree_with_lightweight_delete_column.sql
+++ tests/queries/0_stateless/02454_disable_mergetree_with_lightweight_delete_column.sql
@@ -5,6 +5,7 @@ create table t_row_exists(a int, _row_exists int) engine=MergeTree order by a; -
 create table t_row_exists(a int, b int) engine=MergeTree order by a;
 alter table t_row_exists add column _row_exists int; --{serverError ILLEGAL_COLUMN}
 alter table t_row_exists rename column b to _row_exists; --{serverError ILLEGAL_COLUMN}
+alter table t_row_exists rename column _row_exists to c; --{serverError NOT_FOUND_COLUMN_IN_BLOCK}
 alter table t_row_exists drop column _row_exists; --{serverError NOT_FOUND_COLUMN_IN_BLOCK}
 alter table t_row_exists drop column unknown_column; --{serverError NOT_FOUND_COLUMN_IN_BLOCK}
 drop table t_row_exists;
