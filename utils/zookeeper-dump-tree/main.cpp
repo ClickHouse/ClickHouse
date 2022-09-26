99665f17ba7504e9fb2dc9c659953d7ca9a43630
Antonio Andelic <antonio@clickhouse.com>
2022-09-26 08:05:50 +0000

    Revert dump tree

--- utils/zookeeper-dump-tree/main.cpp
+++ utils/zookeeper-dump-tree/main.cpp
@@ -1,7 +1,6 @@
 #include <list>
 #include <iostream>
 #include <boost/program_options.hpp>
-#include "Common/ZooKeeper/Types.h"
 #include <Common/Exception.h>
 #include <Common/ZooKeeper/ZooKeeper.h>
 #include <Common/ZooKeeper/KeeperException.h>
@@ -38,11 +37,8 @@ int main(int argc, char ** argv)
 
         std::string initial_path = options.at("path").as<std::string>();
 
-        std::list<std::pair<std::vector<std::string>, std::future<Coordination::MultiResponse>>> multi_futures;
-        Coordination::Requests requests;
-        requests.emplace_back(zkutil::makeListRequest(initial_path));
-        requests.emplace_back(zkutil::makeGetRequest(initial_path));
-        multi_futures.emplace_back(std::vector<std::string>{initial_path}, zookeeper->asyncMulti(requests));
+        std::list<std::pair<std::string, std::future<Coordination::ListResponse>>> list_futures;
+        list_futures.emplace_back(initial_path, zookeeper->asyncGetChildren(initial_path));
 
         size_t num_reconnects = 0;
         constexpr size_t max_reconnects = 100;
@@ -54,14 +50,15 @@ int main(int argc, char ** argv)
                 if (num_reconnects == max_reconnects)
                     return false;
                 ++num_reconnects;
+                std::cerr << "num_reconnects: " << num_reconnects << "\n";
                 zookeeper = zookeeper->startNewSession();
             }
             return true;
         };
 
-        for (auto it = multi_futures.begin(); it != multi_futures.end(); ++it)
+        for (auto it = list_futures.begin(); it != list_futures.end(); ++it)
         {
-            Coordination::MultiResponse response;
+            Coordination::ListResponse response;
 
             try
             {
@@ -78,48 +75,24 @@ int main(int argc, char ** argv)
                     /// Reinitialize the session and move the node to the end of the queue for later retry.
                     if (!ensure_session())
                         throw;
-
-                    auto paths = std::move(it->first);
-
-                    Coordination::Requests new_requests;
-                    for (const auto & path : paths)
-                    {
-                        new_requests.emplace_back(zkutil::makeListRequest(path));
-                        new_requests.emplace_back(zkutil::makeGetRequest(path));
-                    }
-                    multi_futures.emplace_back(std::move(paths), zookeeper->asyncMulti(new_requests));
+                    list_futures.emplace_back(it->first, zookeeper->asyncGetChildren(it->first));
                     continue;
                 }
                 else
                     throw;
             }
 
-            for (size_t i = 0; i < it->first.size(); ++i)
+            std::cout << it->first << '\t' << response.stat.numChildren << '\t' << response.stat.dataLength;
+            if (dump_ctime)
+                std::cout << '\t' << response.stat.ctime;
+            std::cout << '\n';
+
+            for (const auto & name : response.names)
             {
-                const auto & path = it->first[i];
-                const auto & list_response = dynamic_cast<const Coordination::ListResponse &>(*response.responses[2 * i]);
-                const auto & get_response = dynamic_cast<const Coordination::GetResponse &>(*response.responses[2 * i + 1]);
-                std::cout << path << '\t' << list_response.stat.numChildren << '\t' << list_response.stat.dataLength;
-                if (dump_ctime)
-                    std::cout << '\t' << list_response.stat.ctime;
-                std::cout << '\t' << get_response.data;
-                std::cout << '\n';
+                std::string child_path = it->first == "/" ? it->first + name : it->first + '/' + name;
 
-                Coordination::Requests new_requests;
-                std::vector<std::string> children;
-                children.reserve(list_response.names.size());
-                for (const auto & name : list_response.names)
-                {
-                    std::string child_path = path == "/" ? path + name : path + '/' + name;
-
-                    ensure_session();
-                    new_requests.emplace_back(zkutil::makeListRequest(child_path));
-                    new_requests.emplace_back(zkutil::makeGetRequest(child_path));
-                    children.push_back(std::move(child_path));
-                }
-
-                if (!children.empty())
-                    multi_futures.emplace_back(std::move(children), zookeeper->asyncMulti(new_requests));
+                ensure_session();
+                list_futures.emplace_back(child_path, zookeeper->asyncGetChildren(child_path));
             }
         }
 
