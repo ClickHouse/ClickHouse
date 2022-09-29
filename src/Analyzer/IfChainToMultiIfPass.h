49029b75cb5b1b376738a2cd37df040395274771
Maksim Kita <kitaetoya@gmail.com>
2022-09-29 13:30:32 +0200

    Added IfChainToMultiIfPass

---
+++ src/Analyzer/IfChainToMultiIfPass.h
@@ -0,0 +1,26 @@
+#pragma once
+
+#include <Analyzer/IQueryTreePass.h>
+
+namespace DB
+{
+
+/** Convert if chain into multiIf.
+  * Replace if(cond_1, then_1_value, if(cond_2, ...)) chains into multiIf(cond_1, then_1_value, cond_2, ...).
+  *
+  * Example: SELECT if(cond_1, then_1_value, if(cond_2, then_2_value, else_value));
+  * Result: SELECT multiIf(cond_1, then_1_value, cond_2, then_2_value, else_value);
+  */
+class IfChainToMultiIfPass : public IQueryTreePass
+{
+public:
+    String getName() override { return "IfChainToMultiIfPass"; }
+
+    String getDescription() override { return "Optimize if chain to multiIf"; }
+
+    void run(QueryTreeNodePtr query_tree_node, ContextPtr context) override;
+
+};
+
+}
+
