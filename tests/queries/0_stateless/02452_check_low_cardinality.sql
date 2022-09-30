875145a813161f1fa34c74de5c0536981a0c24b7
Alfonso Martinez <alfonso@tinybird.co>
2022-09-30 12:41:41 +0200

    FIxed corner case in lowCardinalityFixedString and FixedString

--- tests/queries/0_stateless/02452_check_low_cardinality.sql
+++ tests/queries/0_stateless/02452_check_low_cardinality.sql
@@ -1,11 +1,14 @@
 -- Tags: no-fasttest
 DROP TABLE IF EXISTS test_low_cardinality_string;
+DROP TABLE IF EXISTS test_low_cardinality_string_nested_json;
 DROP TABLE IF EXISTS test_low_cardinality_uuid;
 DROP TABLE IF EXISTS test_low_cardinality_int;
 CREATE TABLE test_low_cardinality_string (data String) ENGINE MergeTree ORDER BY data;
+CREATE TABLE test_low_cardinality_string_nested_json (data String) ENGINE MergeTree ORDER BY data;
 CREATE TABLE test_low_cardinality_uuid (data String) ENGINE MergeTree ORDER BY data;
 CREATE TABLE test_low_cardinality_int (data String) ENGINE MergeTree ORDER BY data;
 INSERT INTO test_low_cardinality_string (data) VALUES ('{"a": "hi", "b": "hello", "c": "hola", "d": "see you, bye, bye"}');
+INSERT INTO test_low_cardinality_string_nested_json (data) VALUES ('{"a" : {"b" : {"c" : 1, "d" : "str"}}}');
 INSERT INTO test_low_cardinality_int (data) VALUES ('{"a": 11, "b": 2222, "c": 33333333, "d": 4444444444444444}');
 INSERT INTO test_low_cardinality_uuid (data) VALUES ('{"a": "2d49dc6e-ddce-4cd0-afb8-790956df54c4", "b": "2d49dc6e-ddce-4cd0-afb8-790956df54c3", "c": "2d49dc6e-ddce-4cd0-afb8-790956df54c1", "d": "2d49dc6e-ddce-4cd0-afb8-790956df54c1"}');
 SELECT JSONExtract(data, 'Tuple(
@@ -57,6 +60,14 @@ SELECT JSONExtract(data, 'Tuple(
                             d LowCardinality(UUID)
                             
                             )') AS json FROM test_low_cardinality_uuid;
+SELECT JSONExtract('{"a" : {"b" : {"c" : 1, "d" : "str"}}}', 'Tuple(
+                            a LowCardinality(FixedString(30)),
+                            b LowCardinality(FixedString(20)),
+                            c LowCardinality(FixedString(20)),
+                            d LowCardinality(FixedString(20))
+
+                            )') AS json FROM test_low_cardinality_string;
+DROP TABLE test_low_cardinality_string_nested_json;
 DROP TABLE test_low_cardinality_string;
 DROP TABLE test_low_cardinality_uuid;
 DROP TABLE test_low_cardinality_int;
