20dd6aba6db2d94e3da8fcbbf256d9078a575b16
JackyWoo <wuchienchao@qq.com>
2022-10-02 11:17:57 +0800

    fix tests

--- tests/queries/0_stateless/00800_function_java_hash_with_unsigined_types.sh
+++ tests/queries/0_stateless/00800_function_java_hash_with_unsigined_types.sh
@@ -5,15 +5,19 @@ CURDIR=$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)
 # shellcheck source=../shell_config.sh
 . "$CURDIR"/../shell_config.sh
 
-${CLICKHOUSE_CLIENT} -q "SELECT javaHash(toUInt8(1))" |& {
-    grep -F  'DB::Exception:'  | wc -l
+exception_pattern='DB::Exception:'
+
+function check()
+{
+  ${CLICKHOUSE_CLIENT} -q "$1" |& {
+      if [[ `grep -F  $exception_pattern  | wc -l` -gt 0 ]]
+      then
+        echo 'Not supported'
+      fi
+  }
 }
-${CLICKHOUSE_CLIENT} -q "SELECT javaHash(toUInt16(1))" |& {
-    grep -F  'DB::Exception:'  | wc -l
-}
-${CLICKHOUSE_CLIENT} -q "SELECT javaHash(toUInt32(1))" |& {
-    grep -F  'DB::Exception:'  | wc -l
-}
-${CLICKHOUSE_CLIENT} -q "SELECT javaHash(toUInt64(1))" |& {
-    grep -F  'DB::Exception:'  | wc -l
-}
+
+check "SELECT javaHash(toUInt8(1))"
+check "SELECT javaHash(toUInt16(1))"
+check "SELECT javaHash(toUInt32(1))"
+check "SELECT javaHash(toUInt64(1))"
