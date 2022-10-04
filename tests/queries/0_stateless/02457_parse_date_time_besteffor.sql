e30297e599ee337f108bb7e8f5dcbb2c7f0e80b3
flynn <fenglv15@mails.ucas.ac.cn>
2022-10-04 13:36:28 +0000

    fix and update test

--- tests/queries/0_stateless/02457_parse_date_time_besteffor.sql
+++
@@ -1,5 +0,0 @@
-select parseDateTimeBestEffort('01/12/2017, 18:31:44');
-select parseDateTimeBestEffortUS('01/12/2017, 18:31:44');
-select parseDateTimeBestEffort('01/12/2017,18:31:44');
-select parseDateTimeBestEffortUS('01/12/2017,18:31:44');
-select parseDateTimeBestEffort('01/12/2017,'); -- { serverError CANNOT_PARSE_DATETIME}
