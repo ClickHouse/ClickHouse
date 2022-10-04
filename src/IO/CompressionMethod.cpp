e2d42590c82386aaf72a28ffde6a78e34a279bba
Nikolay Degterinsky <evillique@gmail.com>
2022-10-04 03:13:46 +0000

    Add new HTTP compression methods

--- src/IO/CompressionMethod.cpp
+++ src/IO/CompressionMethod.cpp
@@ -55,6 +55,32 @@ std::string toContentEncodingName(CompressionMethod method)
     __builtin_unreachable();
 }
 
+CompressionMethod chooseHTTPCompressionMethod(const std::string & list)
+{
+    /// If client supports brotli - it's preferred.
+    /// Both gzip and deflate are supported. If the client supports both, gzip is preferred.
+    /// NOTE parsing of the list of methods is slightly incorrect.
+
+    if (std::string::npos != list.find("br"))
+        return CompressionMethod::Brotli;
+    else if (std::string::npos != list.find("gzip"))
+        return CompressionMethod::Gzip;
+    else if (std::string::npos != list.find("deflate"))
+        return CompressionMethod::Zlib;
+    else if (std::string::npos != list.find("xz"))
+        return CompressionMethod::Xz;
+    else if (std::string::npos != list.find("zstd"))
+        return CompressionMethod::Zstd;
+    else if (std::string::npos != list.find("lz4"))
+        return CompressionMethod::Lz4;
+    else if (std::string::npos != list.find("snappy"))
+        return CompressionMethod::Snappy;
+    else if (std::string::npos != list.find("bz2"))
+        return CompressionMethod::Bzip2;
+    else
+        return CompressionMethod::None;
+}
+
 CompressionMethod chooseCompressionMethod(const std::string & path, const std::string & hint)
 {
     std::string file_extension;
