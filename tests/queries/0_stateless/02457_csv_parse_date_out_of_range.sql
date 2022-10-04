f28b5559858e6353f968a10d55b737725253bf9d
zvonand <azvonov@altinity.com>
2022-10-04 22:55:47 +0300

    fix test

--- tests/queries/0_stateless/02457_csv_parse_date_out_of_range.sql
+++
@@ -1,6 +0,0 @@
-DROP TABLE IF EXISTS test_date_out_of_range sync;
-CREATE TABLE test_date_out_of_range (f String, t Date) engine=Memory();
-INSERT INTO test_date_out_of_range format CSV 'above',2200-12-31
-INSERT INTO test_date_out_of_range format CSV 'below',1900-01-01
-SELECT * from test_date_out_of_range;
-DROP TABLE test_date_out_of_range SYNC;
