f0294cd6eda5c6f2a239cbe8cfa1c6f33f1e0996
Sema Checherinda <Sema.Checherinda@clickhouse.com>
2022-10-04 18:15:50 +0200

    fix test_merge_tree_s3_restore

--- tests/integration/helpers/wait_for_helpers.py
+++ tests/integration/helpers/wait_for_helpers.py
@@ -12,19 +12,19 @@ def _parse_table_database(table, database):
     return table, "default"
 
 
-def wait_for_delete_inactive_parts(node, table, database=None):
+def wait_for_delete_inactive_parts(node, table, database=None, **kwargs):
     table, database = _parse_table_database(table, database)
     inactive_parts_query = (
         f"SELECT count() FROM system.parts "
         f"WHERE not active AND table = '{table}' AND database = '{database}';"
     )
-    assert_eq_with_retry(node, inactive_parts_query, "0\n")
+    assert_eq_with_retry(node, inactive_parts_query, "0\n", **kwargs)
 
 
-def wait_for_delete_empty_parts(node, table, database=None, timeout=60):
+def wait_for_delete_empty_parts(node, table, database=None, **kwargs):
     table, database = _parse_table_database(table, database)
     empty_parts_query = (
         f"SELECT count() FROM system.parts "
         f"WHERE active AND rows = 0 AND table = '{table}' AND database = '{database}'"
     )
-    assert_eq_with_retry(node, empty_parts_query, "0\n")
+    assert_eq_with_retry(node, empty_parts_query, "0\n", **kwargs)
