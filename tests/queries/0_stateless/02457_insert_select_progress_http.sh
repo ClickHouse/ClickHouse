c5a4d618757dc94dc6dc563ecd90c51b1eaa006b
Azat Khuzhin <a3at.mail@gmail.com>
2022-10-05 18:06:39 +0200

    tests: improve INSERT SELECT over HTTP coverage
    Signed-off-by: Azat Khuzhin <a.khuzhin@semrush.com>

--- tests/queries/0_stateless/02457_insert_select_progress_http.sh
+++ tests/queries/0_stateless/02457_insert_select_progress_http.sh
@@ -4,9 +4,6 @@ CURDIR=$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)
 # shellcheck source=../shell_config.sh
 . "$CURDIR"/../shell_config.sh
 
-${CLICKHOUSE_CURL} -sS "${CLICKHOUSE_URL}" -d @- <<< "drop table if exists insert_select_progress_http"
-${CLICKHOUSE_CURL} -sS "${CLICKHOUSE_URL}" -d @- <<< "create table insert_select_progress_http(n UInt16) engine = MergeTree order by n"
-
-${CLICKHOUSE_CURL} -sS "${CLICKHOUSE_URL}&send_progress_in_http_headers=1" -d @- <<< "insert into insert_select_progress_http select * from numbers(1e3)" -v |& grep X-ClickHouse-Summary
-
-${CLICKHOUSE_CURL} -sS "${CLICKHOUSE_URL}" -d @- <<< "drop table insert_select_progress_http"
+${CLICKHOUSE_CURL} -sS "${CLICKHOUSE_URL}&send_progress_in_http_headers=1&http_headers_progress_interval_ms=0" -d @- <<< "insert into function null('_ Int') select * from numbers(5) settings max_block_size=1" -v |& {
+    grep -F -e X-ClickHouse-Progress: -e X-ClickHouse-Summary:
+}
