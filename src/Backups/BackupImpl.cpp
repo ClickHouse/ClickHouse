94566abda95706f7d4a33a0a6b75c3adddc63291
Azat Khuzhin <a3at.mail@gmail.com>
2022-10-07 12:59:11 +0200

    Fix reusing of files > 4GB from base backup
    Previosly u64 numbers was truncated to u32 numbers during writing to the
    mdatadata xml file, and further incremental backup cannot reuse them,
    since the file in base backup is smaller.
    
    P.S. There can be other places, I thought about enabling
    -Wshorten-64-to-32, but there are lots of warnings right now.
    
    Signed-off-by: Azat Khuzhin <a.khuzhin@semrush.com>

--- src/Backups/BackupImpl.cpp
+++ src/Backups/BackupImpl.cpp
@@ -275,7 +275,7 @@ void BackupImpl::writeBackupMetadata()
     assert(!is_internal_backup);
 
     Poco::AutoPtr<Poco::Util::XMLConfiguration> config{new Poco::Util::XMLConfiguration()};
-    config->setUInt("version", CURRENT_BACKUP_VERSION);
+    config->setUInt64("version", CURRENT_BACKUP_VERSION);
     config->setString("timestamp", toString(LocalDateTime{timestamp}));
     config->setString("uuid", toString(*uuid));
 
@@ -302,7 +302,7 @@ void BackupImpl::writeBackupMetadata()
     {
         String prefix = index ? "contents.file[" + std::to_string(index) + "]." : "contents.file.";
         config->setString(prefix + "name", info.file_name);
-        config->setUInt(prefix + "size", info.size);
+        config->setUInt64(prefix + "size", info.size);
         if (info.size)
         {
             config->setString(prefix + "checksum", hexChecksum(info.checksum));
@@ -311,7 +311,7 @@ void BackupImpl::writeBackupMetadata()
                 config->setBool(prefix + "use_base", true);
                 if (info.base_size != info.size)
                 {
-                    config->setUInt(prefix + "base_size", info.base_size);
+                    config->setUInt64(prefix + "base_size", info.base_size);
                     config->setString(prefix + "base_checksum", hexChecksum(info.base_checksum));
                 }
             }
@@ -367,7 +367,7 @@ void BackupImpl::readBackupMetadata()
     Poco::AutoPtr<Poco::Util::XMLConfiguration> config{new Poco::Util::XMLConfiguration()};
     config->load(stream);
 
-    version = config->getUInt("version");
+    version = config->getUInt64("version");
     if ((version < INITIAL_BACKUP_VERSION) || (version > CURRENT_BACKUP_VERSION))
         throw Exception(ErrorCodes::BACKUP_VERSION_NOT_SUPPORTED, "Backup {}: Version {} is not supported", backup_name, version);
 
@@ -389,13 +389,13 @@ void BackupImpl::readBackupMetadata()
             String prefix = "contents." + key + ".";
             FileInfo info;
             info.file_name = config->getString(prefix + "name");
-            info.size = config->getUInt(prefix + "size");
+            info.size = config->getUInt64(prefix + "size");
             if (info.size)
             {
                 info.checksum = unhexChecksum(config->getString(prefix + "checksum"));
 
                 bool use_base = config->getBool(prefix + "use_base", false);
-                info.base_size = config->getUInt(prefix + "base_size", use_base ? info.size : 0);
+                info.base_size = config->getUInt64(prefix + "base_size", use_base ? info.size : 0);
                 if (info.base_size)
                     use_base = true;
 
