e07a3e1193cb1939e5c4c591cd0f983b0f451516
FArthur-cmd <613623@mail.ru>
2022-10-08 10:33:37 +0000

    add default

---
+++ tests/queries/0_stateless/02458_default_setting.sql
@@ -0,0 +1,7 @@
+-- Tags: no-parallel
+
+SELECT value FROM system.settings where name='max_insert_block_size';
+SET max_insert_block_size=100000;
+SELECT value FROM system.settings where name='max_insert_block_size';
+SET max_insert_block_size=DEFAULT;
+SELECT value FROM system.settings where name='max_insert_block_size';
