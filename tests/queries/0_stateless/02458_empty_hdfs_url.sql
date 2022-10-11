03a099f8c5ceb9d1028fa68f14d992ef0043c6c7
Ilya Yatsishin <2159081+qoega@users.noreply.github.com>
2022-10-11 11:59:48 +0200

    address review comments

--- tests/queries/0_stateless/02458_empty_hdfs_url.sql
+++ tests/queries/0_stateless/02458_empty_hdfs_url.sql
@@ -1,2 +1,5 @@
 -- Tags: no-fasttest
 SELECT * FROM hdfsCluster('test_cluster', '', 'TSV'); -- { serverError BAD_ARGUMENTS }
+SELECT * FROM hdfsCluster('test_cluster', ' ', 'TSV'); -- { serverError BAD_ARGUMENTS }
+SELECT * FROM hdfsCluster('test_cluster', '/', 'TSV'); -- { serverError BAD_ARGUMENTS }
+SELECT * FROM hdfsCluster('test_cluster', 'http/', 'TSV'); -- { serverError BAD_ARGUMENTS }
