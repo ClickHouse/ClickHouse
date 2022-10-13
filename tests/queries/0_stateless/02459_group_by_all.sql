3bb43affabe77cab65f9b9e8e4563509e1309a26
taofengliu <taofengliu@outlook.com>
2022-10-13 15:15:28 +0800

    more tests for GROUP BY ALL

--- tests/queries/0_stateless/02459_group_by_all.sql
+++ tests/queries/0_stateless/02459_group_by_all.sql
@@ -3,12 +3,15 @@ DROP TABLE IF EXISTS group_by_all;
 CREATE TABLE group_by_all
 (
     a int,
-    b int
+    b int,
+    ALL int
 )
 engine = Memory;
 
-INSERT INTO group_by_all VALUES (1, 2), (1, 2), (2, 4), (2, 5), (3, 5);
+INSERT INTO group_by_all VALUES (1, 2, 3), (1, 2, 3), (2, 4, 4), (2, 5, 4), (3, 5, 5), (3, 5, 6);
 
 SELECT a, COUNT(b) FROM group_by_all GROUP BY ALL;
 SELECT 1, a, COUNT(b) FROM group_by_all GROUP BY ALL;
 SELECT (a * 2) + 1, COUNT(b) FROM group_by_all GROUP BY ALL;
+SELECT `ALL`, COUNT(b) FROM group_by_all GROUP BY `ALL`;
+SELECT a, `ALL`, COUNT(b) FROM group_by_all GROUP BY ALL;
