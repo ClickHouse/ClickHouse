96c911a1cf0671a39b998e16981a567b71007feb
Arthur Passos <arthur.ti@outlook.com>
2022-10-14 15:23:10 -0300

    style fix?

--- tests/integration/test_host_regexp_multiple_ptr_records_concurrent/scripts/stress_test.py
+++ tests/integration/test_host_regexp_multiple_ptr_records_concurrent/scripts/stress_test.py
@@ -10,45 +10,47 @@ success_counter = 0
 number_of_threads = 1
 number_of_iterations = 400
 
+
 def perform_request():
 
     buffer = BytesIO()
     crl = pycurl.Curl()
-    crl.setopt(pycurl.INTERFACE, '192.168.0.157')
+    crl.setopt(pycurl.INTERFACE, "192.168.0.157")
     crl.setopt(crl.WRITEDATA, buffer)
-    crl.setopt(crl.URL, f'http://{server_ip}:8123/?query=select+1&user=test_dns')
+    crl.setopt(crl.URL, f"http://{server_ip}:8123/?query=select+1&user=test_dns")
 
     crl.perform()
 
     # End curl session
     crl.close()
 
-    str_response = buffer.getvalue().decode('iso-8859-1')
+    str_response = buffer.getvalue().decode("iso-8859-1")
     expected_response = "1\n"
 
     mutex.acquire()
 
     global success_counter
 
-    if (str_response == expected_response):
+    if str_response == expected_response:
         success_counter += 1
 
     mutex.release()
-    
-   # print(buffer.getvalue().decode('iso-8859-1'))
+
 
 def perform_multiple_requests(n):
     for i in range(n):
         perform_request()
 
+
 threads = []
 
 
 for i in range(number_of_threads):
-    thread = threading.Thread(target=perform_multiple_requests, args=(number_of_iterations,))
+    thread = threading.Thread(
+        target=perform_multiple_requests, args=(number_of_iterations,)
+    )
     thread.start()
     threads.append(thread)
-    
 
 for thread in threads:
     thread.join()
