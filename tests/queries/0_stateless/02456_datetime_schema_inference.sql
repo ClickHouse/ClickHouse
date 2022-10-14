024d0706c58cdfb65a14289d38e11b0a10fdd800
Kruglov Pavel <48961922+Avogar@users.noreply.github.com>
2022-10-14 21:52:56 +0200

    Fix test

--- tests/queries/0_stateless/02456_datetime_schema_inference.sql
+++ tests/queries/0_stateless/02456_datetime_schema_inference.sql
@@ -6,10 +6,10 @@ select * from format('TSV', '2022-04-22T03:45:06.381Z');
 select * from format('TSV', '01/12/1925');
 set date_time_input_format = 'best_effort';
 select * from format('TSV', '2022-04-22T03:45:06.381');
-select * from format('TSV', '2022-04-22T03:45:06.381Z');
+select toTimeZone(c1, 'UTC') from format('TSV', '2022-04-22T03:45:06.381Z');
 select * from format('TSV', '01/12/1925');
 set date_time_input_format = 'best_effort_us';
 select * from format('TSV', '2022-04-22T03:45:06.381');
-select * from format('TSV', '2022-04-22T03:45:06.381Z');
+select toTimeZone(c1, 'UTC') from format('TSV', '2022-04-22T03:45:06.381Z');
 select * from format('TSV', '01/12/1925');
 
