1b65ea6c6b1fb4b6a724fc977d38150d4c27a9c9
Ilya Yatsishin <2159081+qoega@users.noreply.github.com>
2022-10-17 12:56:20 +0200

    other cluster name

--- tests/queries/0_stateless/02458_empty_hdfs_url.sql
+++ tests/queries/0_stateless/02458_empty_hdfs_url.sql
@@ -1,5 +1,5 @@
 -- Tags: no-fasttest, no-cpu-aarch64
-SELECT * FROM hdfsCluster('test_cluster', '', 'TSV'); -- { serverError BAD_ARGUMENTS }
-SELECT * FROM hdfsCluster('test_cluster', ' ', 'TSV'); -- { serverError BAD_ARGUMENTS }
-SELECT * FROM hdfsCluster('test_cluster', '/', 'TSV'); -- { serverError BAD_ARGUMENTS }
-SELECT * FROM hdfsCluster('test_cluster', 'http/', 'TSV'); -- { serverError BAD_ARGUMENTS }
+SELECT * FROM hdfsCluster('test_shard_localhost', '', 'TSV'); -- { serverError BAD_ARGUMENTS }
+SELECT * FROM hdfsCluster('test_shard_localhost', ' ', 'TSV'); -- { serverError BAD_ARGUMENTS }
+SELECT * FROM hdfsCluster('test_shard_localhost', '/', 'TSV'); -- { serverError BAD_ARGUMENTS }
+SELECT * FROM hdfsCluster('test_shard_localhost', 'http/', 'TSV'); -- { serverError BAD_ARGUMENTS }
