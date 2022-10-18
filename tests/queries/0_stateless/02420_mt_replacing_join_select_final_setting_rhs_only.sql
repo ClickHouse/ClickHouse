49d1d4edc0978f040f5984b4c4ed49ca83830e62
Arthur Passos <arthur.ti@outlook.com>
2022-10-18 17:13:54 -0300

    Add tests to validate select final setting works on joins

---
+++ tests/queries/0_stateless/02420_mt_replacing_join_select_final_setting_rhs_only.sql
@@ -0,0 +1,10 @@
+create table if not exists lhs (x String) engine=ReplacingMergeTree() ORDER BY x;
+create table if not exists rhs (x String) engine=ReplacingMergeTree() ORDER BY x SETTINGS force_select_final=1;
+
+insert into lhs values ('abc');
+insert into lhs values ('abc');
+
+insert into rhs values ('abc');
+insert into rhs values ('abc');
+
+select count() from lhs inner join rhs on lhs.x = rhs.x;
