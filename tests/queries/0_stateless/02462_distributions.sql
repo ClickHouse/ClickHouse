311cac8ecec1279f3954c069393b34f4f1acfe71
Nikita Mikhaylov <nikitamikhaylov@clickhouse.com>
2022-10-18 18:36:02 +0200

    Small test

--- tests/queries/0_stateless/02462_distributions.sql
+++ tests/queries/0_stateless/02462_distributions.sql
@@ -20,3 +20,5 @@ SELECT DISTINCT if (a >= toFloat64(0), 'Ok', 'Fail') FROM (SELECT randBinomial(3
 SELECT DISTINCT if (a >= toFloat64(0), 'Ok', 'Fail') FROM (SELECT randNegativeBinomial(3, 0.5) AS a FROM numbers(100000));
 # Values should be >= 0
 SELECT DISTINCT if (a >= toFloat64(0), 'Ok', 'Fail') FROM (SELECT randPoisson(44) AS a FROM numbers(100000));
+# No errors
+SELECT randUniform(1, 2, 1), randNormal(0, 1, 'abacaba'), randLogNormal(0, 10, 'b'), randChiSquared(1, 1), randStudentT(7, '8'), randFisherF(23, 42, 100), randBernoulli(0.5, 2), randBinomial(3, 0.5, 1), randNegativeBinomial(3, 0.5, 2), randPoisson(44, 44) FORMAT Null;
