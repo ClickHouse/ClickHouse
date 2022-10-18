c457f7dff3d100c29c24e1592f665a9d03ecffed
Nikolai Kochetov <nik-kochetov@yandex-team.ru>
2022-10-18 12:21:16 +0000

    Fix and more tests.

--- tests/queries/0_stateless/02461_mullable_pk_monotonicity_bug.sql
+++ tests/queries/0_stateless/02461_mullable_pk_monotonicity_bug.sql
@@ -1,6 +1,27 @@
 create table tab (x Nullable(UInt8)) engine = MergeTree order by x settings allow_nullable_key = 1, index_granularity = 2;
 insert into tab select number from numbers(4);
 
+set allow_suspicious_low_cardinality_types=1;
+set max_rows_to_read = 2;
+
+SELECT x + 1 FROM tab where plus(x, 1) <= 2 order by x;
+SELECT x + 1 FROM tab where plus(x, 1::Nullable(UInt8)) <= 2 order by x;
+SELECT x + 1 FROM tab where plus(x, 1::LowCardinality(UInt8)) <= 2 order by x;
+SELECT 1 + x FROM tab where plus(1, x) <= 2 order by x;
+SELECT 1 + x FROM tab where plus(1::Nullable(UInt8), x) <= 2 order by x;
+SELECT 1 + x FROM tab where plus(1::LowCardinality(UInt8), x) <= 2 order by x;
+
+drop table tab;
+
+set max_rows_to_read = 100;
+
+create table tab (x LowCardinality(UInt8)) engine = MergeTree order by x settings allow_nullable_key = 1, index_granularity = 2;
+insert into tab select number from numbers(4);
+
 set max_rows_to_read = 2;
 SELECT x + 1 FROM tab where plus(x, 1) <= 2 order by x;
+SELECT x + 1 FROM tab where plus(x, 1::Nullable(UInt8)) <= 2 order by x;
+SELECT x + 1 FROM tab where plus(x, 1::LowCardinality(UInt8)) <= 2 order by x;
 SELECT 1 + x FROM tab where plus(1, x) <= 2 order by x;
+SELECT 1 + x FROM tab where plus(1::Nullable(UInt8), x) <= 2 order by x;
+SELECT 1 + x FROM tab where plus(1::LowCardinality(UInt8), x) <= 2 order by x;
