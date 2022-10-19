16e55a44359bb1475d9672b5ec220ee4edbb74f6
Amos Bird <amosbird@gmail.com>
2022-10-19 12:15:27 +0800

    Fix misbehavior of key analysis.
    When key types cannot be inside Nullable, it can break key analysis. See
    https://github.com/ClickHouse/ClickHouse/issues/42456.

--- src/Interpreters/castColumn.h
+++ src/Interpreters/castColumn.h
@@ -8,5 +8,6 @@ namespace DB
 ColumnPtr castColumn(const ColumnWithTypeAndName & arg, const DataTypePtr & type);
 ColumnPtr castColumnAccurate(const ColumnWithTypeAndName & arg, const DataTypePtr & type);
 ColumnPtr castColumnAccurateOrNull(const ColumnWithTypeAndName & arg, const DataTypePtr & type);
+ColumnPtr tryCastColumnAccurate(const ColumnWithTypeAndName & arg, const DataTypePtr & type);
 
 }
