2dc2b6722b9da128bef98eb29603d868f4b04744
Yatsishin Ilya <2159081+qoega@users.noreply.github.com>
2022-10-19 16:30:01 +0000

    update docker scripts

--- docker/test/sqlancer/run.sh
+++ docker/test/sqlancer/run.sh
@@ -28,8 +28,8 @@ else
     exit 1
 fi
 
-
-/clickhouse server -P /clickhouse-server.pid -L /clickhouse-server.log -E /clickhouse-server.log.err --daemon
+cd /workspace
+/clickhouse server -P /workspace/clickhouse-server.pid -L /workspace/clickhouse-server.log -E /workspace/clickhouse-server.log.err --daemon
 
 for _ in $(seq 1 30); do if [[ $(wget -q 'localhost:8123' -O-) == 'Ok.' ]]; then break ; else sleep 1; fi ; done
 
@@ -37,23 +37,19 @@ cd /sqlancer/sqlancer-master
 
 export TIMEOUT=300
 export NUM_QUERIES=1000
+export NUM_THREADS=30
 
-( java -jar target/sqlancer-*.jar --num-threads 10 --timeout-seconds $TIMEOUT --num-queries $NUM_QUERIES  --username default --password "" clickhouse --oracle TLPWhere | tee /workspace/TLPWhere.out )  3>&1 1>&2 2>&3 | tee /workspace/TLPWhere.err
-( java -jar target/sqlancer-*.jar --num-threads 10 --timeout-seconds $TIMEOUT --num-queries $NUM_QUERIES  --username default --password "" clickhouse --oracle TLPGroupBy | tee /workspace/TLPGroupBy.out )  3>&1 1>&2 2>&3 | tee /workspace/TLPGroupBy.err
-( java -jar target/sqlancer-*.jar --num-threads 10 --timeout-seconds $TIMEOUT --num-queries $NUM_QUERIES  --username default --password "" clickhouse --oracle TLPHaving | tee /workspace/TLPHaving.out )  3>&1 1>&2 2>&3 | tee /workspace/TLPHaving.err
-( java -jar target/sqlancer-*.jar --num-threads 10 --timeout-seconds $TIMEOUT --num-queries $NUM_QUERIES  --username default --password "" clickhouse --oracle TLPWhere --oracle TLPGroupBy | tee /workspace/TLPWhereGroupBy.out )  3>&1 1>&2 2>&3 | tee /workspace/TLPWhereGroupBy.err
-( java -jar target/sqlancer-*.jar --num-threads 10 --timeout-seconds $TIMEOUT --num-queries $NUM_QUERIES  --username default --password "" clickhouse --oracle TLPDistinct | tee /workspace/TLPDistinct.out )  3>&1 1>&2 2>&3 | tee /workspace/TLPDistinct.err
-( java -jar target/sqlancer-*.jar --num-threads 10 --timeout-seconds $TIMEOUT --num-queries $NUM_QUERIES  --username default --password "" clickhouse --oracle TLPAggregate | tee /workspace/TLPAggregate.out )  3>&1 1>&2 2>&3 | tee /workspace/TLPAggregate.err
+( java -jar target/sqlancer-*.jar --num-threads $NUM_THREADS --timeout-seconds $TIMEOUT --num-queries $NUM_QUERIES  --username default --password "" clickhouse --oracle TLPWhere | tee /workspace/TLPWhere.out )  3>&1 1>&2 2>&3 | tee /workspace/TLPWhere.err
+( java -jar target/sqlancer-*.jar --num-threads $NUM_THREADS --timeout-seconds $TIMEOUT --num-queries $NUM_QUERIES  --username default --password "" clickhouse --oracle TLPGroupBy | tee /workspace/TLPGroupBy.out )  3>&1 1>&2 2>&3 | tee /workspace/TLPGroupBy.err
+( java -jar target/sqlancer-*.jar --num-threads $NUM_THREADS --timeout-seconds $TIMEOUT --num-queries $NUM_QUERIES  --username default --password "" clickhouse --oracle TLPHaving | tee /workspace/TLPHaving.out )  3>&1 1>&2 2>&3 | tee /workspace/TLPHaving.err
+( java -jar target/sqlancer-*.jar --num-threads $NUM_THREADS --timeout-seconds $TIMEOUT --num-queries $NUM_QUERIES  --username default --password "" clickhouse --oracle TLPWhere --oracle TLPGroupBy | tee /workspace/TLPWhereGroupBy.out )  3>&1 1>&2 2>&3 | tee /workspace/TLPWhereGroupBy.err
+( java -jar target/sqlancer-*.jar --num-threads $NUM_THREADS --timeout-seconds $TIMEOUT --num-queries $NUM_QUERIES  --username default --password "" clickhouse --oracle TLPDistinct | tee /workspace/TLPDistinct.out )  3>&1 1>&2 2>&3 | tee /workspace/TLPDistinct.err
+( java -jar target/sqlancer-*.jar --num-threads $NUM_THREADS --timeout-seconds $TIMEOUT --num-queries $NUM_QUERIES  --username default --password "" clickhouse --oracle TLPAggregate | tee /workspace/TLPAggregate.out )  3>&1 1>&2 2>&3 | tee /workspace/TLPAggregate.err
 
-pkill -F /clickhouse-server.pid
+ls /workspace
+pkill -F /workspace/clickhouse-server.pid
 
 for _ in $(seq 1 30); do if [[ $(wget -q 'localhost:8123' -O-) == 'Ok.' ]]; then sleep 1 ; else break; fi ; done
 
-ls /var/log/clickhouse-server/
-tar czf /workplace/logs.tar.gz -C /var/log/clickhouse-server/ .
-tail -n 1000 /var/log/clickhouse-server/stderr.log > /workplace/stderr.log
-tail -n 1000 /var/log/clickhouse-server/stdout.log > /workplace/stdout.log
-tail -n 1000 /var/log/clickhouse-server/clickhouse-server.log > /workplace/clickhouse-server.log
-
-/process_sqlancer_result.py || echo -e "failure\tCannot parse results" > /workplace/check_status.tsv
-ls /workplace
+/process_sqlancer_result.py || echo -e "failure\tCannot parse results" > /workspace/check_status.tsv
+ls /workspace
