1eebc08ed09e237fee27c7ed879bde4d4892b055
Amos Bird <amosbird@gmail.com>
2022-10-22 15:47:13 +0800

    Only fix LowCardinality for now

--- src/Interpreters/castColumn.h
+++ src/Interpreters/castColumn.h
@@ -8,6 +8,5 @@ namespace DB
 ColumnPtr castColumn(const ColumnWithTypeAndName & arg, const DataTypePtr & type);
 ColumnPtr castColumnAccurate(const ColumnWithTypeAndName & arg, const DataTypePtr & type);
 ColumnPtr castColumnAccurateOrNull(const ColumnWithTypeAndName & arg, const DataTypePtr & type);
-ColumnPtr tryCastColumnAccurate(const ColumnWithTypeAndName & arg, const DataTypePtr & type);
 
 }
