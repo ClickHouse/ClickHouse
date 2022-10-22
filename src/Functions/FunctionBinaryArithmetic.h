ca0cb00c371ea637c57160571eb8abb79de05683
Alexey Milovidov <milovidov@clickhouse.com>
2022-10-22 07:38:22 +0200

    Fix error

--- src/Functions/FunctionBinaryArithmetic.h
+++ src/Functions/FunctionBinaryArithmetic.h
@@ -898,6 +898,10 @@ class FunctionBinaryArithmetic : public IFunction
         if (isDateOrDate32(arguments[1].type) || isDateTime(arguments[1].type) || isDateTime64(arguments[1].type))
             std::swap(new_arguments[0], new_arguments[1]);
 
+        /// Change interval argument type to its representation
+        if (WhichDataType(new_arguments[1].type).isInterval())
+            new_arguments[1].type = std::make_shared<DataTypeNumber<DataTypeInterval::FieldType>>();
+
         auto function = function_builder->build(new_arguments);
         return function->execute(new_arguments, result_type, input_rows_count);
     }
