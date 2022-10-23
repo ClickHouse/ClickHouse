4a8326ff0c33758570aee25702ed99143e43d853
Han Fei <hanfei19910905@gmail.com>
2022-10-23 23:22:52 +0200

    fix typo

--- src/Storages/SelectQueryInfo.h
+++ src/Storages/SelectQueryInfo.h
@@ -220,7 +220,7 @@ struct SelectQueryInfo
     Block minmax_count_projection_block;
     MergeTreeDataSelectAnalysisResultPtr merge_tree_select_result_ptr;
 
-    // If limit is not 0, that means it's a trival limit query.
+    // If limit is not 0, that means it's a trivial limit query.
     UInt64 limit = 0;
 
     InputOrderInfoPtr getInputOrderInfo() const
