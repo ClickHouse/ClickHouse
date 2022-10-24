120a43facc754ed7d1ae5c6b5ae3f2d3f17619c2
Nikolai Kochetov <nk@clickhouse.com>
2022-10-24 13:43:24 +0200

    Fixing build.

--- src/Disks/ObjectStorages/S3/S3ObjectStorage.cpp
+++ src/Disks/ObjectStorages/S3/S3ObjectStorage.cpp
@@ -230,7 +230,7 @@ std::unique_ptr<WriteBufferFromFileBase> S3ObjectStorage::writeObject( /// NOLIN
         throw Exception(ErrorCodes::BAD_ARGUMENTS, "S3 doesn't support append to files");
 
     auto settings_ptr = s3_settings.get();
-    ScheduleFunc scheduler;
+    ThreadPoolCallbackRunner<void> scheduler;
     if (write_settings.s3_allow_parallel_part_upload)
         scheduler = threadPoolCallbackRunner<void>(getThreadPoolWriter(), "VFSWrite");
 
