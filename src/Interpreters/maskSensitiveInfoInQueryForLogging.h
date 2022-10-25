c07fe6921fc886568be993d0dd8e771980d4d682
Vitaly Baranov <vitlibar@clickhouse.com>
2022-10-25 14:59:12 +0200

    Add comments.

--- src/Interpreters/maskSensitiveInfoInQueryForLogging.h
+++ src/Interpreters/maskSensitiveInfoInQueryForLogging.h
@@ -9,9 +9,11 @@ namespace DB
 
 /// Makes a version of a query without sensitive information (e.g. passwords) for logging.
 /// The parameter `parsed query` is allowed to be nullptr if the query cannot be parsed.
+/// Does not validate AST, works a best-effort way.
 String maskSensitiveInfoInQueryForLogging(const String & query, const ASTPtr & parsed_query, const ContextPtr & context);
 
 /// Makes a version of backup name without sensitive information (e.g. passwords) for logging.
+/// Does not validate AST, works a best-effort way.
 String maskSensitiveInfoInBackupNameForLogging(const String & backup_name, const ASTPtr & ast, const ContextPtr & context);
 
 }
