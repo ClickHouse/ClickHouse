b279e975c46921f56d24196ec0cc75203d6d2715
Smita Kulkarni <Smita.Kulkarni@clickhouse.com>
2022-10-26 18:45:23 +0200

    Added new field allow_readonly in system.table_functions to allow using table functions in readonly mode. Implementation: * Added a new field allow_readonly to table system.table_functions. * Updated to use new field allow_readonly to allow using table functions in readonly mode. * Added TableFunctionProperties similar to AggregateFunctionProperties. * The functions allowed in readonly mode are in this set table_functions_allowed_in_readonly_mode. Testing: * Added a test for filesystem tests/queries/0_stateless/02473_functions_in_readonly_mode.sh Documentation: * Updated the english documentation for Table Functions.

--- src/TableFunctions/TableFunctionFactory.cpp
+++ src/TableFunctions/TableFunctionFactory.cpp
@@ -16,16 +16,22 @@ namespace ErrorCodes
     extern const int LOGICAL_ERROR;
 }
 
+static const NameSet table_functions_allowed_in_readonly_mode
+{
+    "null", "view", "viewIfPermitted", "numbers", "numbers_mt", "generateRandom", "values", "cluster", "clusterAllReplicas"
+};
 
 void TableFunctionFactory::registerFunction(
     const std::string & name, TableFunctionCreator creator, Documentation doc, CaseSensitiveness case_sensitiveness)
 {
-    if (!table_functions.emplace(name, TableFunctionFactoryData{creator, doc}).second)
+    bool allowed_in_readonly_mode = table_functions_allowed_in_readonly_mode.contains(name);
+
+    if (!table_functions.emplace(name, TableFunctionFactoryData{creator, {doc,allowed_in_readonly_mode}}).second)
         throw Exception("TableFunctionFactory: the table function name '" + name + "' is not unique",
             ErrorCodes::LOGICAL_ERROR);
 
     if (case_sensitiveness == CaseInsensitive
-        && !case_insensitive_table_functions.emplace(Poco::toLower(name), TableFunctionFactoryData{creator, doc}).second)
+        && !case_insensitive_table_functions.emplace(Poco::toLower(name), TableFunctionFactoryData{creator, {doc,allowed_in_readonly_mode}}).second)
         throw Exception("TableFunctionFactory: the case insensitive table function name '" + name + "' is not unique",
                         ErrorCodes::LOGICAL_ERROR);
 }
@@ -59,13 +65,13 @@ TableFunctionPtr TableFunctionFactory::tryGet(
     auto it = table_functions.find(name);
     if (table_functions.end() != it)
     {
-        res = it->second.first();
+        res = it->second.creator();
     }
     else
     {
         it = case_insensitive_table_functions.find(Poco::toLower(name));
         if (case_insensitive_table_functions.end() != it)
-            res = it->second.first();
+            res = it->second.creator();
     }
 
     if (!res)
@@ -86,13 +92,29 @@ bool TableFunctionFactory::isTableFunctionName(const std::string & name) const
     return table_functions.contains(name);
 }
 
-Documentation TableFunctionFactory::getDocumentation(const std::string & name) const
+std::optional<TableFunctionProperties> TableFunctionFactory::tryGetProperties(const String & name) const
 {
-    auto it = table_functions.find(name);
-    if (it == table_functions.end())
-        throw Exception(ErrorCodes::UNKNOWN_FUNCTION, "Unknown table function {}", name);
+    return tryGetPropertiesImpl(name);
+}
 
-    return it->second.second;
+std::optional<TableFunctionProperties> TableFunctionFactory::tryGetPropertiesImpl(const String & name_param) const
+{
+    String name = getAliasToOrName(name_param);
+    Value found;
+
+    /// Find by exact match.
+    if (auto it = table_functions.find(name); it != table_functions.end())
+    {
+        found = it->second;
+    }
+
+    if (auto jt = case_insensitive_table_functions.find(Poco::toLower(name)); jt != case_insensitive_table_functions.end())
+        found = jt->second;
+
+    if (found.creator)
+        return found.properties;
+
+    return {};
 }
 
 TableFunctionFactory & TableFunctionFactory::instance()
