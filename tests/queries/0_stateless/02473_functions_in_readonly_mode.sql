ebc335ea12b1cddd95aae8d25bd6c7ae028c081f
Smita Kulkarni <Smita.Kulkarni@clickhouse.com>
2022-10-26 18:49:14 +0200

    Removed extra line from test - 42414 Enable table functions in readonly mode

--- tests/queries/0_stateless/02473_functions_in_readonly_mode.sql
+++ tests/queries/0_stateless/02473_functions_in_readonly_mode.sql
@@ -2,4 +2,3 @@ SELECT * from numbers(1);
 select * from format(JSONEachRow, '{"x" : [123, "str"]}');
 SELECT * from numbers(1) SETTINGS readonly=1;
 select * from format(JSONEachRow, '{"x" : [123, "str"]}') SETTINGS readonly=1; -- { serverError READONLY }
-set readonly=0;
