11a933b0465bedea333ff48dfb90078869d5822f
vdimir <vdimir@clickhouse.com>
2022-10-27 11:34:45 +0000

    broke style in src/Interpreters/InterpreterSelectQuery.cpp

--- src/Interpreters/InterpreterSelectQuery.cpp
+++ src/Interpreters/InterpreterSelectQuery.cpp
@@ -138,8 +138,7 @@ FilterDAGInfoPtr generateFilterActions(
     expr_list->children.push_back(row_policy_filter->clone());
 
     /// Keep columns that are required after the filter actions.
-    for (const auto & column_str : prerequisite_columns)
-    {
+    for (const auto & column_str : prerequisite_columns) {
         ParserExpression expr_parser;
         expr_list->children.push_back(parseQuery(expr_parser, column_str, 0, context->getSettingsRef().max_parser_depth));
     }
