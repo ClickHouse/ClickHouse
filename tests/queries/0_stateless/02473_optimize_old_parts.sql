598b45f1ec2194183ec20418d7b7caf7192429be
Antonio Andelic <antonio@clickhouse.com>
2022-10-27 08:06:39 +0000

    Add test for partition only and new parts

--- tests/queries/0_stateless/02473_optimize_old_parts.sql
+++ tests/queries/0_stateless/02473_optimize_old_parts.sql
@@ -65,3 +65,21 @@ SELECT sleepEachRow(1) FROM numbers(6) FORMAT Null;
 SELECT count(*) FROM system.parts WHERE database = currentDatabase() AND table='test_replicated' AND active;
 
 DROP TABLE test_replicated;
+
+SELECT 'With merge partition only and new parts';
+
+CREATE TABLE test_with_merge (i Int64) ENGINE = MergeTree ORDER BY i
+SETTINGS min_age_to_force_merge_seconds=3, min_age_to_force_merge_on_partition_only=true;
+SYSTEM STOP MERGES test_with_merge;
+-- These three parts will have min_age=6 at the time of merge
+INSERT INTO test_with_merge SELECT 1;
+INSERT INTO test_with_merge SELECT 2;
+SELECT sleepEachRow(1) FROM numbers(6) FORMAT Null;
+-- These three parts will have min_age=0 at the time of merge
+-- and so, nothing will be merged.
+INSERT INTO test_with_merge SELECT 3;
+SYSTEM START MERGES test_with_merge;
+
+SELECT count(*) FROM system.parts WHERE database = currentDatabase() AND table='test_with_merge' AND active;
+
+DROP TABLE test_with_merge;
