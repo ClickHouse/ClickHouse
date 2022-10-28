9e7c971cd6138d5dc70ed456c061269b39e6c838
Anton Popov <anton@clickhouse.com>
2022-10-28 23:26:06 +0000

    fix some tests

--- tests/queries/0_stateless/01606_merge_from_wide_to_compact.sql
+++ tests/queries/0_stateless/01606_merge_from_wide_to_compact.sql
@@ -4,7 +4,8 @@ CREATE TABLE wide_to_comp (a Int, b Int, c Int)
     ENGINE = MergeTree ORDER BY a
     settings vertical_merge_algorithm_min_rows_to_activate = 1,
     vertical_merge_algorithm_min_columns_to_activate = 1,
-    min_bytes_for_wide_part = 0;
+    min_bytes_for_wide_part = 0,
+    min_rows_for_wide_part = 0;
 
 SYSTEM STOP merges wide_to_comp;
 
--- tests/queries/0_stateless/01623_constraints_column_swap.sql
+++ tests/queries/0_stateless/01623_constraints_column_swap.sql
@@ -1,3 +1,5 @@
+-- Tags: no-random-merge-tree-settings
+
 SET convert_query_to_cnf = 1;
 SET optimize_using_constraints = 1;
 SET optimize_move_to_prewhere = 1;
