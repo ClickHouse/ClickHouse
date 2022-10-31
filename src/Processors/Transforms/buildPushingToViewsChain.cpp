4dfc1401563642ed0e5b70f638996b81fabc7f26
Trevor North <trevor@bugsnag.com>
2022-10-31 16:49:21 +0000

    Ensure query context exists when building view push chain
    For the scenario in which a materialized view is attached to the
    destination table of a window view there is no available query context
    to which to add log information so skip this step.

--- src/Processors/Transforms/buildPushingToViewsChain.cpp
+++ src/Processors/Transforms/buildPushingToViewsChain.cpp
@@ -366,7 +366,7 @@ Chain buildPushingToViewsChain(
         chains.emplace_back(std::move(out));
 
         /// Add the view to the query access info so it can appear in system.query_log
-        if (!no_destination)
+        if (!no_destination && context->hasQueryContext())
         {
             context->getQueryContext()->addQueryAccessInfo(
                 backQuoteIfNeed(database_table.getDatabaseName()), views_data->views.back().runtime_stats->target_name, {}, "", database_table.getFullTableName());
