62d449338879eefc247a30b855b665eaa48280db
Alexander Tokmakov <tavplubix@clickhouse.com>
2022-10-31 14:00:50 +0100

    fix

--- tests/queries/0_stateless/02447_drop_database_replica.sh
+++ tests/queries/0_stateless/02447_drop_database_replica.sh
@@ -6,8 +6,8 @@ CURDIR=$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)
 
 db="rdb_$CLICKHOUSE_DATABASE"
 
-$CLICKHOUSE_CLIENT -q "create database $db engine=Replicated('/test/$CLICKHOUSE_DATABASE/rdb', 's1', 'r1')"
-$CLICKHOUSE_CLIENT -q "create table $db.t as system.query_log"
+$CLICKHOUSE_CLIENT --allow_experimental_database_replicated=1 -q "create database $db engine=Replicated('/test/$CLICKHOUSE_DATABASE/rdb', 's1', 'r1')"
+$CLICKHOUSE_CLIENT -q "create table $db.t as system.query_log"   # Suppress database = currentDatabase()
 $CLICKHOUSE_CLIENT -q "show tables from $db"
 
 $CLICKHOUSE_CLIENT -q "system drop database replica 's1|r1' from table t" 2>&1| grep -Fac "SYNTAX_ERROR"
@@ -20,23 +20,23 @@ $CLICKHOUSE_CLIENT -q "system drop database replica 's2|r1' from zkpath '/test/$
 $CLICKHOUSE_CLIENT -q "system drop database replica 's2/r1' from zkpath '/test/$CLICKHOUSE_DATABASE/rdb'" 2>&1| grep -Fac "Invalid replica name"
 
 db2="${db}_2"
-$CLICKHOUSE_CLIENT -q "create database $db2 engine=Replicated('/test/$CLICKHOUSE_DATABASE/rdb', 's1', 'r2')"
+$CLICKHOUSE_CLIENT --allow_experimental_database_replicated=1 -q "create database $db2 engine=Replicated('/test/$CLICKHOUSE_DATABASE/rdb', 's1', 'r2')"
 $CLICKHOUSE_CLIENT -q "select cluster, shard_num, replica_num from system.clusters where cluster='$db' order by shard_num, replica_num"
 $CLICKHOUSE_CLIENT -q "system drop database replica 's1|r1' from database $db2" 2>&1| grep -Fac "is active, cannot drop it"
 
 $CLICKHOUSE_CLIENT -q "detach database $db2"
 $CLICKHOUSE_CLIENT -q "system drop database replica 's1|r2' from database $db"
 $CLICKHOUSE_CLIENT -q "attach database $db2" 2>/dev/null
-$CLICKHOUSE_CLIENT -q "create table $db2.t2 as system.query_log" 2>&1| grep -Fac "Database is in readonly mode"
+$CLICKHOUSE_CLIENT -q "create table $db2.t2 as system.query_log" 2>&1| grep -Fac "Database is in readonly mode"   # Suppress database = currentDatabase()
 
 $CLICKHOUSE_CLIENT -q "detach database $db"
 $CLICKHOUSE_CLIENT -q "system drop database replica 's1|r1' from zkpath '/test/$CLICKHOUSE_DATABASE/rdb/'"
 $CLICKHOUSE_CLIENT -q "attach database $db" 2>/dev/null
-$CLICKHOUSE_CLIENT -q "create table $db.t2 as system.query_log" 2>&1| grep -Fac "Database is in readonly mode"
+$CLICKHOUSE_CLIENT -q "create table $db.t2 as system.query_log" 2>&1| grep -Fac "Database is in readonly mode"   # Suppress database = currentDatabase()
 $CLICKHOUSE_CLIENT -q "show tables from $db"
 
 db3="${db}_3"
-$CLICKHOUSE_CLIENT -q "create database $db3 engine=Replicated('/test/$CLICKHOUSE_DATABASE/rdb', 's1', 'r1')"
+$CLICKHOUSE_CLIENT --allow_experimental_database_replicated=1 -q "create database $db3 engine=Replicated('/test/$CLICKHOUSE_DATABASE/rdb', 's1', 'r1')"
 $CLICKHOUSE_CLIENT -q "select cluster, shard_num, replica_num from system.clusters where cluster='$db3'"
 
 $CLICKHOUSE_CLIENT -q "drop database $db"
