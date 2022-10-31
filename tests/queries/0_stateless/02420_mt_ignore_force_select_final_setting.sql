5708780c44dbc9dbb952a149435826caeb3cfa42
Arthur Passos <arthur.ti@outlook.com>
2022-10-31 11:09:25 -0300

    add order by to tests to guarantee ordering

--- tests/queries/0_stateless/02420_mt_ignore_force_select_final_setting.sql
+++ tests/queries/0_stateless/02420_mt_ignore_force_select_final_setting.sql
@@ -9,7 +9,7 @@ INSERT INTO tbl SELECT number as id, 'bar' AS val FROM numbers(100000);
 SELECT * FROM tbl WHERE id = 10000; -- single row expected (bar), because the force_select_final is in action
 SELECT * FROM tbl final WHERE id = 10000; -- single row expected (bar), because the force_select_final is in action and FINAL is there.
 
-SELECT * FROM tbl WHERE id = 10000 SETTINGS ignore_force_select_final=1; -- now we see 2 'real' rows
+SELECT * FROM tbl WHERE id = 10000 ORDER BY val DESC SETTINGS ignore_force_select_final=1; -- now we see 2 'real' rows
 SELECT * FROM tbl FINAL WHERE id = 10000 SETTINGS ignore_force_select_final=1; -- now we see single row again.
 
 SYSTEM START MERGES tbl;
