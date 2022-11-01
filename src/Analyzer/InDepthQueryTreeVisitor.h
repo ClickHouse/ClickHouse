5295cb0e940777f42291bb8b771187baf7ed027f
Dmitry Novik <n0vik@clickhouse.com>
2022-11-01 22:11:56 +0000

    Correctrly enable optimize_or_like_chain

--- src/Analyzer/InDepthQueryTreeVisitor.h
+++ src/Analyzer/InDepthQueryTreeVisitor.h
@@ -1,8 +1,13 @@
 #pragma once
 
+#include <optional>
+#include <utility>
+#include <Common/SettingsChanges.h>
 #include <Common/Exception.h>
+#include <Core/Settings.h>
 
 #include <Analyzer/IQueryTreeNode.h>
+#include <Analyzer/QueryNode.h>
 
 
 namespace DB
@@ -83,5 +88,87 @@ private:
 
 template <typename Derived>
 using ConstInDepthQueryTreeVisitor = InDepthQueryTreeVisitor<Derived, true /*const_visitor*/>;
+
+template <typename Impl>
+class OptionalInDepthQueryTreeVisitor : Impl
+{
+    friend struct StateSwitcher;
+
+    Settings settings;
+    bool is_enabled = false;
+
+    struct StateSwitcher
+    {
+        StateSwitcher(OptionalInDepthQueryTreeVisitor * visitor_, SettingsChanges rollback_changes_, bool value)
+            : visitor(visitor_)
+            , rollback_changes(std::move(rollback_changes_))
+            , previous_value(visitor_->is_enabled)
+        {
+            visitor->is_enabled = value;
+        }
+
+        StateSwitcher(StateSwitcher &&) noexcept = default;
+
+        ~StateSwitcher()
+        {
+            visitor->is_enabled = previous_value;
+            for (const auto & change : rollback_changes)
+                visitor->settings.set(change.name, change.value);
+        }
+
+        OptionalInDepthQueryTreeVisitor * visitor;
+        SettingsChanges rollback_changes;
+        bool previous_value;
+    };
+
+    std::optional<StateSwitcher> updateState(QueryTreeNodePtr & node)
+    {
+        auto * query = node->as<QueryNode>();
+        if (!query)
+            return {};
+
+        SettingsChanges rollback_changes;
+        for (const auto & change : query->getSettingsChanges())
+        {
+            rollback_changes.push_back(SettingChange{ change.name, settings.get(change.name) });
+            settings.set(change.name, change.value);
+        }
+        return std::make_optional<StateSwitcher>(this, std::move(rollback_changes), Impl::isEnabled(settings));
+    }
+
+    void visitChildren(QueryTreeNodePtr & node)
+    {
+        for (auto & child : node->getChildren())
+        {
+            if (!child)
+                continue;
+
+            visit(child);
+        }
+    }
+
+public:
+
+    template <typename ...Args>
+    explicit OptionalInDepthQueryTreeVisitor(const Settings & settings_, Args && ...args)
+        : Impl(std::forward<Args>(args)...)
+        , settings(settings_)
+    {}
+
+    bool needVisit(QueryTreeNodePtr & node)
+    {
+        return is_enabled && Impl::needVisit(node);
+    }
+
+    void visit(QueryTreeNodePtr & node)
+    {
+        auto switcher = updateState(node);
+
+        if (needVisit(node))
+            Impl::visitImpl(node);
+
+        visitChildren(node);
+    }
+};
 
 }
