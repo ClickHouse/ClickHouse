2f1e4bbad7840a8e47c4c361b2e4eaca038cde75
zvonand <azvonov@altinity.com>
2022-11-01 13:21:17 +0300

    added documentation

--- src/Functions/FunctionsDecimalArithmetics.cpp
+++ src/Functions/FunctionsDecimalArithmetics.cpp
@@ -5,11 +5,13 @@ namespace DB
 {
 REGISTER_FUNCTION(DivideDecimals)
 {
-    factory.registerFunction<FunctionsDecimalArithmetics<DivideDecimalsImpl>>();
+    factory.registerFunction<FunctionsDecimalArithmetics<DivideDecimalsImpl>>(Documentation(
+        "Decimal division with given precision. Slower than simple `divide`, but has controlled precision and no sound overflows"));
 }
 
 REGISTER_FUNCTION(MultiplyDecimals)
 {
-    factory.registerFunction<FunctionsDecimalArithmetics<MultiplyDecimalsImpl>>();
+    factory.registerFunction<FunctionsDecimalArithmetics<MultiplyDecimalsImpl>>(Documentation(
+        "Decimal multiplication with given precision. Slower than simple `divide`, but has controlled precision and no sound overflows"));
 }
 }
