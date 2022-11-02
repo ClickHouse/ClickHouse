80c4d27a895d19783c417e83419b0f6e4ef83b3c
Suzy Wang <suzy.wang@ibm.com>
2022-11-02 06:56:46 -0700

    reverse columnVector change and update new fix

--- base/base/bit_cast.h
+++ base/base/bit_cast.h
@@ -3,6 +3,7 @@
 #include <cstring>
 #include <algorithm>
 #include <type_traits>
+#include <base/unaligned.h>
 
 
 /** Returns value `from` converted to type `To` while retaining bit representation.
@@ -13,6 +14,12 @@ template <typename To, typename From>
 std::decay_t<To> bit_cast(const From & from)
 {
     To res {};
-    memcpy(static_cast<void*>(&res), &from, std::min(sizeof(res), sizeof(from)));
+    if constexpr (std::endian::native == std::endian::little || !std::is_arithmetic_v<To>)
+      memcpy(static_cast<void*>(&res), &from, std::min(sizeof(res), sizeof(from)));
+    else
+    {
+      uint32_t offset = (sizeof(res) > sizeof(from)) ? (sizeof(res) - sizeof(from)) : 0;
+      reverseMemcpy(reinterpret_cast<char *>(&res) + offset, &from, std::min(sizeof(res), sizeof(from)));
+    }
     return res;
 }
