3fb0e7c17fad00df9385e25e40c3edeb1eb45bf7
Suzy Wang <suzy.wang@ibm.com>
2022-11-02 06:56:46 -0700

    reverse columnVector change and update new fix

--- src/Columns/ColumnVector.cpp
+++ src/Columns/ColumnVector.cpp
@@ -438,10 +438,7 @@ UInt64 ColumnVector<T>::get64(size_t n [[maybe_unused]]) const
 {
     if constexpr (is_arithmetic_v<T>)
     {
-        if constexpr (std::endian::native == std::endian::little)
-            return bit_cast<UInt64>(data[n]);
-        else
-            return __builtin_bswap64(bit_cast<UInt64>(data[n]));
+        return bit_cast<UInt64>(data[n]);
     }
     else
         throw Exception(ErrorCodes::NOT_IMPLEMENTED, "Cannot get the value of {} as UInt64", TypeName<T>);
