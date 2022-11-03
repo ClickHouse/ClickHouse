88a1c9241b64ab308f536815f78d9ce4a403133d
Smita Kulkarni <Smita.Kulkarni@clickhouse.com>
2022-11-03 11:57:32 +0100

    Removed finished_asts updation when alias is replaced and updated alias of the replaced ast it its initial name so that we don’t replace it further - 42452 Bug fix for parent node corrupted

--- src/Interpreters/QueryNormalizer.cpp
+++ src/Interpreters/QueryNormalizer.cpp
@@ -119,9 +119,9 @@ void QueryNormalizer::visit(ASTIdentifier & node, ASTPtr & ast, Data & data)
         }
         else
         {
+            auto alias_name = ast->getAliasOrColumnName();
             ast = alias_node->clone();
-            if (data.finished_asts.contains(alias_node))
-                data.finished_asts[ast] = ast;
+            ast->setAlias(alias_name);
         }
     }
 }
