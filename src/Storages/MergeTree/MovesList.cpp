d289547d10acd333a8ae6d1b05d1e55181a542a2
serxa <sergei@clickhouse.com>
2022-11-07 20:33:25 +0000

    fill more fields

--- src/Storages/MergeTree/MovesList.cpp
+++ src/Storages/MergeTree/MovesList.cpp
@@ -6,9 +6,14 @@ namespace DB
 {
 
 MovesListElement::MovesListElement(
-    const StorageID & table_id_, const std::string & partition_id_)
+        const StorageID & table_id_,
+        const std::string & partition_id_,
+        const std::string & target_disk_path_,
+        UInt64 part_size_)
     : table_id(table_id_)
     , partition_id(partition_id_)
+    , target_disk_path(target_disk_path_)
+    , part_size(part_size_)
     , thread_id(getThreadId())
 {
 }
@@ -19,8 +24,7 @@ MoveInfo MovesListElement::getInfo() const
     res.database = table_id.database_name;
     res.table = table_id.table_name;
     res.partition_id = partition_id;
-    res.result_part_name = result_part_name;
-    res.result_part_path = result_part_path;
+    res.target_disk_path = target_disk_path;
     res.part_size = part_size;
     res.elapsed = watch.elapsedSeconds();
     res.thread_id = thread_id;
