0261ff509575433607c7bab06b5bb0301ede4899
Nikolai Kochetov <nik-kochetov@yandex-team.ru>
2022-11-09 20:02:32 +0000

    Fix test.

--- tests/queries/0_stateless/02155_read_in_order_max_rows_to_read.sql
+++ tests/queries/0_stateless/02155_read_in_order_max_rows_to_read.sql
@@ -16,7 +16,7 @@ SELECT a FROM t_max_rows_to_read ORDER BY a LIMIT 5 SETTINGS max_rows_to_read = 
 SELECT a FROM t_max_rows_to_read WHERE a = 10 OR a = 20 SETTINGS max_rows_to_read = 12;
 
 SELECT a FROM t_max_rows_to_read ORDER BY a LIMIT 20 FORMAT Null SETTINGS max_rows_to_read = 12; -- { serverError 158 }
-SELECT a FROM t_max_rows_to_read WHERE a > 10 ORDER BY a LIMIT 5 SETTINGS max_rows_to_read = 12; -- { serverError 158 }
+SELECT a FROM t_max_rows_to_read WHERE a > 10 ORDER BY a LIMIT 5 FORMAT Null SETTINGS max_rows_to_read = 12; -- { serverError 158 }
 SELECT a FROM t_max_rows_to_read WHERE a = 10 OR a = 20 FORMAT Null SETTINGS max_rows_to_read = 4; -- { serverError 158 }
 
 DROP TABLE t_max_rows_to_read;
