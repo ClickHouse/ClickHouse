c523ffd696d8f0ec417030f6b0c789efacad0ac2
vdimir <vdimir@clickhouse.com>
2022-11-09 14:33:11 +0000

    Update tests for FuseFunctionsPass

--- tests/queries/0_stateless/02476_fuse_sum_count.sql
+++ tests/queries/0_stateless/02476_fuse_sum_count.sql
@@ -12,10 +12,9 @@ SELECT avg(a), sum(a) FROM (SELECT a FROM fuse_tbl WHERE isNull(a));
 SELECT avg(a), sum(a) FROM (SELECT a FROM fuse_tbl WHERE isNotNull(a));
 
 SELECT avg(b), sum(b) FROM (SELECT b FROM fuse_tbl);
-SELECT avg(b) * 3, sum(b) + 1 + count(b), count(b) * count(b) FROM (SELECT b FROM fuse_tbl);
+SELECT avg(b) * 3, sum(b) + 1 + count(b), count(b) * count(b), count() FROM (SELECT b FROM fuse_tbl);
 
--- TODO(@vdimir): uncomment after https://github.com/ClickHouse/ClickHouse/pull/42865
--- SELECT sum(b), count(b) from (SELECT x as b FROM (SELECT sum(b) as x, count(b)  FROM fuse_tbl) );
+SELECT sum(b), count(b) from (SELECT x as b FROM (SELECT sum(b) as x, count(b)  FROM fuse_tbl) );
 
 SELECT sum(a + 1), sum(b), count(b), avg(b), count(a + 1), sum(a + 2), count(a) from fuse_tbl SETTINGS optimize_syntax_fuse_functions = 0;
 SELECT sum(a + 1), sum(b), count(b), avg(b), count(a + 1), sum(a + 2), count(a) from fuse_tbl;
@@ -25,8 +24,7 @@ EXPLAIN QUERY TREE run_passes = 1 SELECT sum(b), avg(b) from fuse_tbl;
 EXPLAIN QUERY TREE run_passes = 1 SELECT sum(a + 1), sum(b), count(b), avg(b), count(a + 1), sum(a + 2), count(a) from fuse_tbl;
 EXPLAIN QUERY TREE run_passes = 1 SELECT avg(b) * 3, sum(b) + 1 + count(b), count(b) * count(b) FROM (SELECT b FROM fuse_tbl);
 
--- TODO(@vdimir): uncomment after https://github.com/ClickHouse/ClickHouse/pull/42865
--- EXPLAIN QUERY TREE run_passes = 1 SELECT sum(b), count(b) from (SELECT x as b FROM (SELECT sum(b) as x, count(b)  FROM fuse_tbl) );
+EXPLAIN QUERY TREE run_passes = 1 SELECT sum(b), count(b) from (SELECT x as b FROM (SELECT sum(b) as x, count(b)  FROM fuse_tbl) );
 
 SELECT sum(x), count(x), avg(x) FROM (SELECT number :: Decimal32(0) AS x FROM numbers(0)) SETTINGS optimize_syntax_fuse_functions = 0;
 SELECT sum(x), count(x), avg(x) FROM (SELECT number :: Decimal32(0) AS x FROM numbers(0));
--- tests/queries/0_stateless/02477_fuse_quantiles.sql
+++ tests/queries/0_stateless/02477_fuse_quantiles.sql
@@ -13,7 +13,7 @@ SELECT quantile(a - 1), quantile(b - 1) + 1, quantile(0.8)(b - 1) + 1, quantile(
 
 EXPLAIN QUERY TREE run_passes = 1 SELECT quantile(a - 1), quantile(b - 1) + 1, quantile(0.8)(b - 1) + 1, quantile(0.8)(b - 1) + 2, quantile(0.9)(b - 1) + 1 FROM fuse_tbl;
 
--- -- TODO(@vdimir): uncomment after https://github.com/ClickHouse/ClickHouse/pull/42865
--- SELECT quantile(0.5)(b), quantile(0.9)(b) from (SELECT x as b, quantile(0.5)(b) as x, quantile(0.9)(b) FROM (SELECT quantile(0.5)(b) as x, quantile(0.9)(b)  FROM fuse_tbl) );
+SELECT quantile(0.5)(b), quantile(0.9)(b) from (SELECT x + 1 as b FROM (SELECT quantile(0.5)(b) as x, quantile(0.9)(b) FROM fuse_tbl) GROUP BY x);
+EXPLAIN QUERY TREE run_passes = 1 SELECT quantile(0.5)(b), quantile(0.9)(b) from (SELECT x + 1 as b FROM (SELECT quantile(0.5)(b) as x, quantile(0.9)(b) FROM fuse_tbl) GROUP BY x);
 
 DROP TABLE fuse_tbl;
