7a05a52e211125101668c746695f07f362ed1766
vdimir <vdimir@clickhouse.com>
2022-11-09 11:43:59 +0000

    Style fixes for FuseFunctionsPass

--- src/Analyzer/Passes/FuseFunctionsPass.h
+++ src/Analyzer/Passes/FuseFunctionsPass.h
@@ -6,17 +6,19 @@ namespace DB
 {
 
 /*
- * Try to fuse sum/avg/count with identical arguments to one sumCount call,
- * if we have at least two different functions.
- * E.g. we will replace sum(x) and count(x) with sumCount(x).1 and sumCount(x).2,
- * Result of sumCount() will be calculated only once because of CSE.
+ * This pass replaces several calls of aggregate functions of the same family into one call.
+ * Result will be calculated only once because of CSE.
+ *
+ * Replaces:
+ * `sum(x), count(x), avg(x)` with `sumCount(x).1, sumCount(x).2, sumCount(x).1 / toFloat64(sumCount(x).2)`
+ * `quantile(0.5)(x), quantile(0.9)(x)` with `quantiles(0.5, 0.9)(x)[1], quantiles(0.5, 0.9)(x)[2]`
  */
 class FuseFunctionsPass final : public IQueryTreePass
 {
 public:
     String getName() override { return "FuseFunctionsPass"; }
 
-    String getDescription() override { return "Replaces sum/avg/count with identical arguments to one sumCount call"; }
+    String getDescription() override { return "Replaces several calls of aggregate functions of the same family into one call"; }
 
     void run(QueryTreeNodePtr query_tree_node, ContextPtr context) override;
 };
