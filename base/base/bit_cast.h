f624143532eaa18ef69390011a9206cd9407ecb5
Suzy Wang <suzy.wang@ibm.com>
2022-11-10 12:42:38 -0800

    fix typo

--- base/base/bit_cast.h
+++ base/base/bit_cast.h
@@ -13,8 +13,8 @@ template <typename To, typename From>
 std::decay_t<To> bit_cast(const From & from)
 {
     /**
-     * Assume the source value is 0xAABBCCDD (i.e. sizeof(from) == 4). 
-     * Its BE representation is 0xAABBCCDD, the LE representation is 0xDDCCBBAA. 
+     * Assume the source value is 0xAABBCCDD (i.e. sizeof(from) == 4).
+     * Its BE representation is 0xAABBCCDD, the LE representation is 0xDDCCBBAA.
      * Further assume, sizeof(res) == 8 and that res is initially zeroed out.
      * With LE, the result after bit_cast will be 0xDDCCBBAA00000000 --> input value == output value.
      * With BE, the result after bit_cast will be 0x00000000AABBCCDD --> input value == output value.
@@ -25,7 +25,7 @@ std::decay_t<To> bit_cast(const From & from)
       memcpy(static_cast<void*>(&res), &from, std::min(sizeof(res), sizeof(from)));
     else
     {
-      uint32_t offset = sizeof(res) - sizeof(to);
+      uint32_t offset = sizeof(res) - sizeof(from);
       memcpy(reinterpret_cast<char *>(&res) + offset, &from, std::min(sizeof(res), sizeof(from)));
     }
     return res;
