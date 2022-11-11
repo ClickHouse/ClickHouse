6f93083fa3f92cfadd9566e69d2a0607fde2c024
zzsmdfj <zzsmdfj@gmail.com>
2022-11-11 11:42:49 +0800

    to 42624_revert_idiosyncrasies_select-fix code style

--- tests/queries/0_stateless/01062_window_view_event_hop_watch_asc.py
+++ tests/queries/0_stateless/01062_window_view_event_hop_watch_asc.py
@@ -29,7 +29,7 @@ with client(name="client1>", log=log) as client1, client(
 
     client1.send(
 +       "CREATE DATABASE IF NOT EXISTS `01062_window_view_event_hop_watch_asc`"
-+    )
+    )
     client1.expect(prompt)
     client1.send("DROP TABLE IF EXISTS `01062_window_view_event_hop_watch_asc`.mt")
     client1.expect(prompt)
--- tests/queries/0_stateless/01069_window_view_proc_tumble_watch.py
+++ tests/queries/0_stateless/01069_window_view_proc_tumble_watch.py
@@ -33,7 +33,7 @@ with client(name="client1>", log=log) as client1, client(
     client1.expect(prompt)
     client1.send(
 +       "DROP TABLE IF EXISTS `01069_window_view_proc_tumble_watch`.wv NO DELAY"
-+    )
+    )
     client1.expect(prompt)
 
     client1.send(
