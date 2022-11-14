cf7297323fdc36fa8ed8ce9fefdc9438c750524c
Alfonso Martinez <alfonso@tinybird.co>
2022-11-14 16:12:28 +0100

    Cleaned sh file

--- tests/queries/0_stateless/02479_race_condition_between_insert_and_droppin_mv.sh
+++ tests/queries/0_stateless/02479_race_condition_between_insert_and_droppin_mv.sh
@@ -2,8 +2,7 @@
 # Tags: no-fasttest
 
 CURDIR=$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)
+# shellcheck source=../shell_config.sh
 . "$CURDIR"/../shell_config.sh
-
-# We should have correct env vars from shell_config.sh to run this test
 
 python3 "$CURDIR"/02479_race_condition_between_insert_and_droppin_mv.python
