ce5af569096b1c34863b1c389b5d75e7d606325a
Youenn Lebras <youenn.lebras@contentsquare.com>
2022-11-15 11:47:20 +0100

    Fix a test

--- src/Processors/Merges/Algorithms/ReplacingSortedAlgorithm.cpp
+++ src/Processors/Merges/Algorithms/ReplacingSortedAlgorithm.cpp
@@ -82,10 +82,13 @@ IMergingAlgorithm::Status ReplacingSortedAlgorithm::merge()
         if (out_row_sources_buf)
             current_row_sources.emplace_back(current.impl->order, true);
 
-        UInt8 is_deleted = assert_cast<const ColumnUInt8 &>(*current->all_columns[is_deleted_column_number]).getData()[current->getRow()];
-        if ((is_deleted_column_number!=-1) && (is_deleted != 1) && (is_deleted != 0))
+    UInt8 is_deleted = 0;
+    if ((is_deleted_column_number!=-1)) {
+        is_deleted = assert_cast<const ColumnUInt8 &>(*current->all_columns[is_deleted_column_number]).getData()[current->getRow()];
+        if ((is_deleted != 1) && (is_deleted != 0))
             throw Exception("Incorrect data: is_deleted = " + toString(is_deleted) + " (must be 1 or 0).",
                             ErrorCodes::INCORRECT_DATA);
+    }
 
         /// A non-strict comparison, since we select the last row for the same version values.
         if (version_column_number == -1
