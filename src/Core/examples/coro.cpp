396f33585112ec1a19b72d223e640a40c728b0c1
Nikolai Kochetov <nik-kochetov@yandex-team.ru>
2022-11-16 19:04:32 +0000

    fix coro stack

--- src/Core/examples/coro.cpp
+++ src/Core/examples/coro.cpp
@@ -31,10 +31,17 @@ struct suspend_value // NOLINT(readability-identifier-naming)
     T val;
 };
 
+struct PromiseStack
+{
+    PromiseStack * prev_promise = nullptr;
+};
+
+thread_local PromiseStack * promise_stack = nullptr;
+
 template <typename T>
 struct Task
 {
-    struct promise_type // NOLINT(readability-identifier-naming)
+    struct promise_type : public PromiseStack // NOLINT(readability-identifier-naming)
     {
         using coro_handle = std::coroutine_handle<promise_type>;
         auto get_return_object() { return coro_handle::from_promise(*this); } // NOLINT(readability-identifier-naming)
@@ -52,9 +59,18 @@ struct Task
                 }
             }
 
+            prev_promise = promise_stack;
+            promise_stack = this;
+
+            addr_tag = reinterpret_cast<void *>(0xABABABABABA);
+
             return std::suspend_never();
         } // NOLINT(readability-identifier-naming)
-        auto final_suspend() noexcept { return suspend_value<T>{*r->value}; } // NOLINT(readability-identifier-naming)
+        auto final_suspend() noexcept // NOLINT(readability-identifier-naming)
+        {
+            promise_stack = prev_promise;
+            return suspend_value<T>{*r->value};
+        }
         //void return_void() {}
         void return_value(T value_) { r->value = value_; } // NOLINT(readability-identifier-naming)
         void unhandled_exception() // NOLINT(readability-identifier-naming)
@@ -67,8 +83,9 @@ struct Task
         ~promise_type() { std::cout << "~promise_type " << tag << std::endl; }
         //coro_handle self;
         void * addr;
-        std::string tag;
+        void * addr_tag;
         coro_handle next;
+        std::string tag;
         Task * r = nullptr;
     };
 
@@ -123,6 +140,8 @@ struct Task
         return !r->value;
     }
 
+    static void tmp() {}
+
     bool resume()
     {
         return resumeImpl(this);
@@ -168,11 +187,31 @@ struct co_gethandle_awaitable
     Task<int>::coro_handle m_hWaiter;
 };
 
-void print_ptrs(void * addr, size_t count)
+void print_ptrs(void * addr, int before, int after)
 {
     char * ptr = static_cast<char *>(addr);
-    for (size_t i = 0; i < count; ++i)
+    for (int i = before; i <= after; ++i)
         std::cout << *reinterpret_cast<void **>(static_cast<void *>(ptr + i * 8)) << std::endl;
+}
+
+void fixTrace(StackTrace::FramePointers & frames, size_t offset, size_t size)
+{
+    if (promise_stack == nullptr)
+        return;
+
+    constexpr auto start = Task<int>::resumeImpl;
+    constexpr auto end = Task<int>::tmp;
+    PromiseStack * last_promise = promise_stack->prev_promise;
+    for (size_t i = offset; i < size && last_promise; ++i)
+    {
+        uintptr_t frame = reinterpret_cast<uintptr_t>(frames[i]);
+        std::cerr << reinterpret_cast<uintptr_t>(start) << ' ' << frame << ' ' << reinterpret_cast<uintptr_t>(end) << std::endl;
+        if (reinterpret_cast<uintptr_t>(start) <= frame && frame <= reinterpret_cast<uintptr_t>(end))
+        {
+            frames[i] = *reinterpret_cast<void **>(reinterpret_cast<char *>(last_promise) - sizeof(void *) * 2);
+            last_promise = last_promise->prev_promise;
+        }
+    }
 }
 
 Task<int> boo([[maybe_unused]] std::string tag)
@@ -186,12 +225,20 @@ Task<int> boo([[maybe_unused]] std::string tag)
     auto frames = st.getFramePointers();
     std::cout << StackTrace::toStringStatic(frames, st.getOffset(), st.getSize());
     std::cout << "----------\n";
-    frames[2] = my_handle.promise().addr;
+    //frames[2] = my_handle.promise().addr;
     std::cout << StackTrace::toStringStatic(frames, st.getOffset(), st.getSize());
     std::cout << "-=--------\n";
-    frames[2] = *reinterpret_cast<void **>(static_cast<char *>(*reinterpret_cast<void **>(&my_handle)) + sizeof(void *) * 2);
+    //frames[2] = *reinterpret_cast<void **>(static_cast<char *>(*reinterpret_cast<void **>(&my_handle)) + sizeof(void *) * 2);
+    //std::cout << StackTrace::toStringStatic(frames, st.getOffset(), st.getSize());
+    std::cerr << "stack next : " << static_cast<void *>(promise_stack->prev_promise) << std::endl;
+    std::cerr << "resume_impl_addr : " << reinterpret_cast<void *>(Task<int>::resumeImpl) << std::endl;
+    std::cerr << "tmp_addr : " << reinterpret_cast<void *>(Task<int>::tmp) << std::endl;
+    print_ptrs(*reinterpret_cast<void **>(&my_handle), -5, 10);
+    std::cerr << "....\n";
+    print_ptrs(reinterpret_cast<void *>(promise_stack->prev_promise), -5, 10);
+    //frames[2] = *reinterpret_cast<void **>(reinterpret_cast<char *>(promise_stack->prev_promise) - sizeof(void *) * 2);
+    fixTrace(frames, st.getOffset(), st.getSize());
     std::cout << StackTrace::toStringStatic(frames, st.getOffset(), st.getSize());
-    //print_ptrs(*reinterpret_cast<void **>(&my_handle), 5);
 
     std::cout << "y" << std::endl;
     co_return 1;
@@ -221,6 +268,7 @@ Task<int> foo([[maybe_unused]] std::string tag)
 
 int main()
 {
+    //__builtin_coro_resume(reinterpret_cast<void *>(0xABCABCABC));
     Poco::AutoPtr<Poco::ConsoleChannel> app_channel(new Poco::ConsoleChannel(std::cerr));
     Poco::Logger::root().setChannel(app_channel);
     Poco::Logger::root().setLevel("trace");
