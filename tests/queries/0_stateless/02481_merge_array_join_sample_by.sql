646796316958ba802a522eac46d794926745f0a0
Antonio Andelic <antonio2368@users.noreply.github.com>
2022-11-17 14:25:32 +0100

    Fix test

--- tests/queries/0_stateless/02481_merge_array_join_sample_by.sql
+++ tests/queries/0_stateless/02481_merge_array_join_sample_by.sql
@@ -3,7 +3,7 @@ DROP TABLE IF EXISTS 02481_merge;
 
 CREATE TABLE 02481_mergetree(x UInt64, y UInt64, arr Array(String)) ENGINE = MergeTree ORDER BY x SAMPLE BY x;
 
-CREATE TABLE 02481_merge(x UInt64, y UInt64, arr Array(String)) ENGINE = Merge('default', '^(02481_mergetree)$');
+CREATE TABLE 02481_merge(x UInt64, y UInt64, arr Array(String)) ENGINE = Merge(currentDatabase(), '^(02481_mergetree)$');
 
 INSERT INTO 02481_mergetree SELECT number, number + 1, [1,2] FROM system.numbers LIMIT 100000;
 
