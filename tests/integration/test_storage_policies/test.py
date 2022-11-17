1b1285ea7372ede794f869bbb1f1d7b03c1b74a7
robot-clickhouse <robot-clickhouse@clickhouse.com>
2022-11-17 14:46:25 +0000

    Automatic style fix

--- tests/integration/test_storage_policies/test.py
+++ tests/integration/test_storage_policies/test.py
@@ -8,7 +8,7 @@ SCRIPT_DIR = os.path.dirname(os.path.realpath(__file__))
 CONFIG_DIR = os.path.join(SCRIPT_DIR, "configs")
 
 cluster = ClickHouseCluster(__file__)
-node = cluster.add_instance("node", main_configs=['configs/disks.xml'], stay_alive=True)
+node = cluster.add_instance("node", main_configs=["configs/disks.xml"], stay_alive=True)
 
 
 @pytest.fixture(scope="module")
@@ -21,12 +21,20 @@ def started_cluster():
 
 
 def test_storage_policy_configuration_change(started_cluster):
-    node.query("CREATE TABLE a (x UInt64) ENGINE = MergeTree ORDER BY x SETTINGS storage_policy = 'test_policy'")
+    node.query(
+        "CREATE TABLE a (x UInt64) ENGINE = MergeTree ORDER BY x SETTINGS storage_policy = 'test_policy'"
+    )
 
     node.stop_clickhouse()
-    node.copy_file_to_container(os.path.join(CONFIG_DIR, "disk2_only.xml"), "/etc/clickhouse-server/config.d/disks.xml")
+    node.copy_file_to_container(
+        os.path.join(CONFIG_DIR, "disk2_only.xml"),
+        "/etc/clickhouse-server/config.d/disks.xml",
+    )
     node.start_clickhouse()
 
     node.stop_clickhouse()
-    node.copy_file_to_container(os.path.join(CONFIG_DIR, "disks.xml"), "/etc/clickhouse-server/config.d/disks.xml")
+    node.copy_file_to_container(
+        os.path.join(CONFIG_DIR, "disks.xml"),
+        "/etc/clickhouse-server/config.d/disks.xml",
+    )
     node.start_clickhouse()
