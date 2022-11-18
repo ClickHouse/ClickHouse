538d1cc7c1675d0ff1469c2dec62397e3632ae32
zzsmdfj <zzsmdfj@gmail.com>
2022-11-18 10:37:17 +0800

    to 42624_revert_idiosyncrasies_select-fix failed tests

--- tests/queries/0_stateless/02354_annoy.sql
+++ tests/queries/0_stateless/02354_annoy.sql
@@ -2,9 +2,9 @@
 
 SET allow_experimental_annoy_index = 1;
 
-DROP TABLE IF EXISTS 02354_annoy;
+DROP TABLE IF EXISTS annoy_02354;
 
-CREATE TABLE 02354_annoy
+CREATE TABLE annoy_02354
 (
     id Int32,
     embedding Array(Float32),
@@ -14,43 +14,43 @@ ENGINE = MergeTree
 ORDER BY id
 SETTINGS index_granularity=5;
 
-INSERT INTO 02354_annoy VALUES (1, [0.0, 0.0, 10.0]), (2, [0.0, 0.0, 10.5]), (3, [0.0, 0.0, 9.5]), (4, [0.0, 0.0, 9.7]), (5, [0.0, 0.0, 10.2]), (6, [10.0, 0.0, 0.0]), (7, [9.5, 0.0, 0.0]), (8, [9.7, 0.0, 0.0]), (9, [10.2, 0.0, 0.0]), (10, [10.5, 0.0, 0.0]), (11, [0.0, 10.0, 0.0]), (12, [0.0, 9.5, 0.0]), (13, [0.0, 9.7, 0.0]), (14, [0.0, 10.2, 0.0]), (15, [0.0, 10.5, 0.0]);
+INSERT INTO annoy_02354 VALUES (1, [0.0, 0.0, 10.0]), (2, [0.0, 0.0, 10.5]), (3, [0.0, 0.0, 9.5]), (4, [0.0, 0.0, 9.7]), (5, [0.0, 0.0, 10.2]), (6, [10.0, 0.0, 0.0]), (7, [9.5, 0.0, 0.0]), (8, [9.7, 0.0, 0.0]), (9, [10.2, 0.0, 0.0]), (10, [10.5, 0.0, 0.0]), (11, [0.0, 10.0, 0.0]), (12, [0.0, 9.5, 0.0]), (13, [0.0, 9.7, 0.0]), (14, [0.0, 10.2, 0.0]), (15, [0.0, 10.5, 0.0]);
 
 SELECT *
-FROM 02354_annoy
+FROM annoy_02354
 WHERE L2Distance(embedding, [0.0, 0.0, 10.0]) < 1.0
 LIMIT 5;
 
 SELECT *
-FROM 02354_annoy
+FROM annoy_02354
 ORDER BY L2Distance(embedding, [0.0, 0.0, 10.0])
 LIMIT 3;
 
-SET param_02354_target_vector='[0.0, 0.0, 10.0]';
+SET param_target_vector_02354_='[0.0, 0.0, 10.0]';
 
 SELECT *
-FROM 02354_annoy
-WHERE L2Distance(embedding, {02354_target_vector: Array(Float32)}) < 1.0
+FROM annoy_02354
+WHERE L2Distance(embedding, {target_vector_02354_: Array(Float32)}) < 1.0
 LIMIT 5;
 
 SELECT *
-FROM 02354_annoy
-ORDER BY L2Distance(embedding, {02354_target_vector: Array(Float32)})
+FROM annoy_02354
+ORDER BY L2Distance(embedding, {target_vector_02354_: Array(Float32)})
 LIMIT 3;
 
 SELECT *
-FROM 02354_annoy
+FROM annoy_02354
 ORDER BY L2Distance(embedding, [0.0, 0.0])
 LIMIT 3; -- { serverError 80 }
 
-DROP TABLE IF EXISTS 02354_annoy;
+DROP TABLE IF EXISTS annoy_02354;
 
 -- ------------------------------------
 -- Check that weird base columns are rejected
 
 -- Index spans >1 column
 
-CREATE TABLE 02354_annoy
+CREATE TABLE annoy_02354
 (
     id Int32,
     embedding Array(Float32),
@@ -62,7 +62,7 @@ SETTINGS index_granularity=5; -- {serverError 7 }
 
 -- Index must be created on Array(Float32) or Tuple(Float32)
 
-CREATE TABLE 02354_annoy
+CREATE TABLE annoy_02354
 (
     id Int32,
     embedding Float32,
@@ -73,7 +73,7 @@ ORDER BY id
 SETTINGS index_granularity=5; -- {serverError 44 }
 
 
-CREATE TABLE 02354_annoy
+CREATE TABLE annoy_02354
 (
     id Int32,
     embedding Array(Float64),
@@ -83,7 +83,7 @@ ENGINE = MergeTree
 ORDER BY id
 SETTINGS index_granularity=5; -- {serverError 44 }
 
-CREATE TABLE 02354_annoy
+CREATE TABLE annoy_02354
 (
     id Int32,
     embedding Tuple(Float32, Float64),
@@ -93,7 +93,7 @@ ENGINE = MergeTree
 ORDER BY id
 SETTINGS index_granularity=5; -- {serverError 44 }
 
-CREATE TABLE 02354_annoy
+CREATE TABLE annoy_02354
 (
     id Int32,
     embedding Array(LowCardinality(Float32)),
@@ -103,7 +103,7 @@ ENGINE = MergeTree
 ORDER BY id
 SETTINGS index_granularity=5; -- {serverError 44 }
 
-CREATE TABLE 02354_annoy
+CREATE TABLE annoy_02354
 (
     id Int32,
     embedding Array(Nullable(Float32)),
