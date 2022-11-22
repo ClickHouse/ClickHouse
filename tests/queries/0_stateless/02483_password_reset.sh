351864394c06b7a56ddb07aa5e41968a285d8222
Alexey Milovidov <milovidov@clickhouse.com>
2022-11-22 19:46:03 +0100

    Add a test

--- tests/queries/0_stateless/02483_password_reset.sh
+++ tests/queries/0_stateless/02483_password_reset.sh
@@ -6,6 +6,8 @@ CURDIR=$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)
 # shellcheck source=../shell_config.sh
 . "$CURDIR"/../shell_config.sh
 
+# The exception message contains instruction on how to reset the password:
+
 $CLICKHOUSE_CLIENT --password incorrect-password --query "SELECT 1" 2>&1 | grep -o 'password is incorrect'
 $CLICKHOUSE_CLIENT --password incorrect-password --query "SELECT 1" 2>&1 | grep -o -P 'reset.+password'
 
