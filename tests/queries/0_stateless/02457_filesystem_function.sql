4502fc0e815b27b3bedc9909ef26743d55e1bb7d
Anton Popov <anton@clickhouse.com>
2022-11-22 01:02:36 +0000

    fix flaky tests

--- tests/queries/0_stateless/02457_filesystem_function.sql
+++ tests/queries/0_stateless/02457_filesystem_function.sql
@@ -1,2 +1,6 @@
-select filesystemCapacity('default') >= filesystemAvailable('default') and filesystemAvailable('default') >= filesystemUnreserved('default');
+-- Tags: no-fasttest
+
+select filesystemCapacity('s3_disk') >= filesystemAvailable('s3_disk') and filesystemAvailable('s3_disk') >= filesystemUnreserved('s3_disk');
+select filesystemCapacity('default') >= filesystemAvailable('default') and filesystemAvailable('default') >= 0 and filesystemUnreserved('default') >= 0;
+
 select filesystemCapacity('__un_exists_disk'); -- { serverError UNKNOWN_DISK }
