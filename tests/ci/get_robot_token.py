cdc0b32a5da1498f50b2f31c8790bfa53ef317ab
Mikhail f. Shiryaev <felixoid@clickhouse.com>
2022-11-22 12:43:17 +0100

    Use all parameters with prefixes from ssm

--- tests/ci/get_robot_token.py
+++ tests/ci/get_robot_token.py
@@ -1,4 +1,6 @@
 #!/usr/bin/env python3
+import logging
+
 import boto3  # type: ignore
 from github import Github  # type: ignore
 
@@ -9,14 +11,28 @@ def get_parameter_from_ssm(name, decrypt=True, client=None):
     return client.get_parameter(Name=name, WithDecryption=decrypt)["Parameter"]["Value"]
 
 
-def get_best_robot_token(token_prefix_env_name="github_robot_token_", total_tokens=4):
+def get_best_robot_token(token_prefix_env_name="github_robot_token_"):
     client = boto3.client("ssm", region_name="us-east-1")
-    tokens = {}
-    for i in range(1, total_tokens + 1):
-        token_name = token_prefix_env_name + str(i)
-        token = get_parameter_from_ssm(token_name, True, client)
-        gh = Github(token, per_page=100)
-        rest, _ = gh.rate_limiting
-        tokens[token] = rest
+    parameters = client.describe_parameters(
+        ParameterFilters=[
+            {"Key": "Name", "Option": "BeginsWith", "Values": [token_prefix_env_name]}
+        ]
+    )["Parameters"]
+    assert parameters
+    token = {"login": "", "value": "", "rest": 0}
 
-    return max(tokens.items(), key=lambda x: x[1])[0]
+    for token_name in [p["Name"] for p in parameters]:
+        value = get_parameter_from_ssm(token_name, True, client)
+        gh = Github(value, per_page=100)
+        login = gh.get_user().login
+        rest, _ = gh.rate_limiting
+        logging.info("Get token for user %s with %s remaining requests", login, rest)
+        if token["rest"] < rest:
+            token = {"login": login, "value": value, "rest": rest}
+
+    assert token["login"]
+    logging.info(
+        "User %s with %s remaining requests is used", token["login"], token["rest"]
+    )
+
+    return token["value"]
