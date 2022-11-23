19034dce0c8819c8999ff44618c8b95ce7218d37
xiedeyantu <czjourney@163.com>
2022-11-23 23:36:12 +0800

    s3 table function can support select nested column using {column_name}.{subcolumn_name}

--- src/Storages/StorageS3.h
+++ src/Storages/StorageS3.h
@@ -253,6 +253,8 @@ private:
         ContextPtr ctx,
         std::unordered_map<String, S3::ObjectInfo> * object_infos = nullptr);
 
+    bool supportsSubcolumns() const override;
+
     bool supportsSubsetOfColumns() const override;
 
     static std::optional<ColumnsDescription> tryGetColumnsFromCache(
