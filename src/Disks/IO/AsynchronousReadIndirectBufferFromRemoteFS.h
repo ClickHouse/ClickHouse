e03599ce3264fee01b762837f05f996080be0cc3
robot-clickhouse <robot-clickhouse@clickhouse.com>
2022-11-24 11:04:41 +0000

    Backport #42129 to 22.9: Fix buffer resize with `remote_filesystem_read_method=read` + fs cache

--- src/Disks/IO/AsynchronousReadIndirectBufferFromRemoteFS.h
+++ src/Disks/IO/AsynchronousReadIndirectBufferFromRemoteFS.h
@@ -3,6 +3,7 @@
 #include <Common/config.h>
 #include <IO/ReadBufferFromFile.h>
 #include <IO/AsynchronousReader.h>
+#include <IO/ReadSettings.h>
 #include <utility>
 
 namespace Poco { class Logger; }
@@ -11,7 +12,6 @@ namespace DB
 {
 
 class ReadBufferFromRemoteFSGather;
-struct ReadSettings;
 
 /**
  * Reads data from S3/HDFS/Web using stored paths in metadata.
@@ -64,7 +64,9 @@ private:
 
     std::future<IAsynchronousReader::Result> asyncReadInto(char * data, size_t size);
 
-    AsynchronousReaderPtr reader;
+    ReadSettings read_settings;
+
+    IAsynchronousReader & reader;
 
     Int32 priority;
 
