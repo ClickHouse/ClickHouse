69165243c5f95644ade0e81f0b33d90afe1b08ba
凌涛 <lingtaolf@gmail.com>
2022-11-25 10:11:09 +0800

    update test

--- tests/queries/0_stateless/02483_add_engine_full_column_to_system_databases.sql
+++ tests/queries/0_stateless/02483_add_engine_full_column_to_system_databases.sql
@@ -1,5 +1,5 @@
 DROP DATABASE IF EXISTS replicated_database_test; 
-CREATE DATABASE IF NOT EXISTS replicated_database_test ENGINE = Replicated('some/path/' || currentDatabase() || '/replicated_database_test', 'shard_1', 'replica_1');
+CREATE DATABASE IF NOT EXISTS replicated_database_test ENGINE = Replicated('some/path/' || currentDatabase() || '/replicated_database_test', 'shard_1', 'replica_1') SETTINGS max_broken_tables_ratio=1;
 SELECT engine_full FROM system.databases WHERE name = 'replicated_database_test';
 DROP DATABASE IF EXISTS replicated_database_test; 
 
