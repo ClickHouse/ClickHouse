d1d4d9a434733ffb38302caf8549a5533591ab23
Roman Vasin <r.vasin@arenadata.io>
2022-11-25 13:44:17 +0000

    Make array working on tables data correctly

--- src/Functions/URL/cutURLParameter.cpp
+++ src/Functions/URL/cutURLParameter.cpp
@@ -78,58 +78,48 @@ public:
                 ErrorCodes::ILLEGAL_COLUMN);
     }
 
-    static void cutURL(const ColumnString::Chars & data,
-        std::string pattern,
-        ColumnString::Chars & res_data, ColumnString::Offsets & res_offsets,
-        size_t prev_offset, size_t cur_offset, size_t & res_offset, size_t offset_index)
+    static void cutURL(ColumnString::Chars & data, std::string pattern)
     {
-
         pattern += '=';
         const char * param_str = pattern.c_str();
         size_t param_len = pattern.size();
 
-            const char * url_begin = reinterpret_cast<const char *>(&data[prev_offset]);
-            const char * url_end = reinterpret_cast<const char *>(&data[cur_offset]) - 1;
-            const char * begin_pos = url_begin;
-            const char * end_pos = begin_pos;
+        const char * url_begin = reinterpret_cast<const char *>(&data[0]);
+        const char * url_end = reinterpret_cast<const char *>(&data[data.size()-1]) - 1;
+        const char * begin_pos = url_begin;
+        const char * end_pos = begin_pos;
 
-            do
+        do
+        {
+            const char * query_string_begin = find_first_symbols<'?', '#'>(url_begin, url_end);
+            if (query_string_begin + 1 >= url_end)
+                break;
+
+            const char * pos = static_cast<const char *>(memmem(query_string_begin + 1, url_end - query_string_begin - 1, param_str, param_len));
+            if (pos == nullptr)
+                break;
+
+            if (pos[-1] != '?' && pos[-1] != '#' && pos[-1] != '&')
             {
-                const char * query_string_begin = find_first_symbols<'?', '#'>(url_begin, url_end);
-                if (query_string_begin + 1 >= url_end)
-                    break;
+                pos = nullptr;
+                break;
+            }
 
-                const char * pos = static_cast<const char *>(memmem(query_string_begin + 1, url_end - query_string_begin - 1, param_str, param_len));
-                if (pos == nullptr)
-                    break;
+            begin_pos = pos;
+            end_pos = begin_pos + param_len;
 
-                if (pos[-1] != '?' && pos[-1] != '#' && pos[-1] != '&')
-                {
-                    pos = nullptr;
-                    break;
-                }
+            /// Skip the value.
+            while (*end_pos && *end_pos != '&' && *end_pos != '#')
+                ++end_pos;
 
-                begin_pos = pos;
-                end_pos = begin_pos + param_len;
+            /// Capture '&' before or after the parameter.
+            if (*end_pos == '&')
+                ++end_pos;
+            else if (begin_pos[-1] == '&')
+                --begin_pos;
+        } while (false);
 
-                /// Skip the value.
-                while (*end_pos && *end_pos != '&' && *end_pos != '#')
-                    ++end_pos;
-
-                /// Capture '&' before or after the parameter.
-                if (*end_pos == '&')
-                    ++end_pos;
-                else if (begin_pos[-1] == '&')
-                    --begin_pos;
-            } while (false);
-
-            size_t cut_length = (url_end - url_begin) - (end_pos - begin_pos);
-            res_data.resize(res_offset + cut_length + 1);
-            memcpySmallAllowReadWriteOverflow15(&res_data[res_offset], url_begin, begin_pos - url_begin);
-            memcpySmallAllowReadWriteOverflow15(&res_data[res_offset] + (begin_pos - url_begin), end_pos, url_end - end_pos);
-            res_offset += cut_length + 1;
-            res_data[res_offset - 1] = 0;
-            res_offsets[offset_index] = res_offset;
+        data.erase(data.begin() + (begin_pos - url_begin), data.begin() + (end_pos - url_begin));
     }
 
     static void vector(const ColumnString::Chars & data,
@@ -143,35 +133,40 @@ public:
 
         size_t prev_offset = 0;
         size_t res_offset = 0;
-        size_t prev_res_offset = 0;
 
+        ColumnString::Chars res_chars;
 
         for (size_t i = 0; i < offsets.size(); ++i)
         {
             size_t cur_offset = offsets[i];
 
+            res_chars.clear();
+            res_chars.insert(res_chars.end(), data.begin()+prev_offset, data.begin()+cur_offset);
+
             if (!col_const_array)
             {
-                cutURL(data, col_needle->getValue<String>(), res_data, res_offsets, prev_offset, cur_offset, res_offset, i);
+                cutURL(res_chars, col_needle->getValue<String>());
             }
             else
             {
                 size_t data_size = col_const_array->getData().size();
                 // assert(data_size>0);
 
-                res_offset = 0;
                 for (size_t j = 0; j < data_size; ++j)
                 {
-                    res_offset = prev_res_offset;
                     auto field = col_const_array->getData()[j];
                     // auto typename_ = field.getTypeName();
                     // assert(typename_ != "");
-                    auto pattern = field.get<String>();
-                    assert(pattern != "");
-                    cutURL(data, pattern, res_data, res_offsets, prev_offset, cur_offset, res_offset, i);
+                    cutURL(res_chars, field.get<String>());
                 }
-                prev_res_offset = res_offset;
+
             }
+            res_data.resize(res_offset + res_chars.size() + 1);
+            memcpySmallAllowReadWriteOverflow15(&res_data[res_offset], &res_chars[0], res_chars.size());
+            res_offset += res_chars.size() + 1;
+            res_data[res_offset - 1] = 0;
+            res_offsets[i] = res_offset;
+
             prev_offset = cur_offset;
         }
     }
