9883c3c0273f6b30d2c3ae5ce666a1f515e4a245
chen <czjourney@163.com>
2022-11-25 15:17:10 +0800

    Update 01801_s3_cluster_count.sql

--- tests/queries/0_stateless/01801_s3_cluster_count.sql
+++ tests/queries/0_stateless/01801_s3_cluster_count.sql
@@ -1,5 +1,5 @@
 -- Tags: no-fasttest
 -- Tag no-fasttest: Depends on AWS
 
-select COUNT() from s3Cluster('test_cluster_two_shards_localhost', 'http://localhost:11111/test/{a,b,c}.tsv') ORDER BY c1, c2, c3;
-select COUNT(*) from s3Cluster('test_cluster_two_shards_localhost', 'http://localhost:11111/test/{a,b,c}.tsv') ORDER BY c1, c2, c3;
+select COUNT() from s3Cluster('test_cluster_two_shards_localhost', 'http://localhost:11111/test/{a,b,c}.tsv');
+select COUNT(*) from s3Cluster('test_cluster_two_shards_localhost', 'http://localhost:11111/test/{a,b,c}.tsv');
