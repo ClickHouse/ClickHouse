e6c283e7dfa14f746c4472c5f582b5c16fb61077
Dmitry Novik <n0vik@clickhouse.com>
2022-11-29 16:38:06 +0000

    Fix AggregateFunctionMinMaxAny::createResultType

--- src/AggregateFunctions/AggregateFunctionMinMaxAny.h
+++ src/AggregateFunctions/AggregateFunctionMinMaxAny.h
@@ -1219,7 +1219,7 @@ private:
 
 public:
     explicit AggregateFunctionsSingleValue(const DataTypePtr & type)
-        : IAggregateFunctionDataHelper<Data, AggregateFunctionsSingleValue<Data>>({type}, {}, createResultType())
+        : IAggregateFunctionDataHelper<Data, AggregateFunctionsSingleValue<Data>>({type}, {}, createResultType(type))
         , serialization(type->getDefaultSerialization())
     {
         if (StringRef(Data::name()) == StringRef("min")
@@ -1233,12 +1233,11 @@ public:
 
     String getName() const override { return Data::name(); }
 
-    DataTypePtr createResultType() const
+    static DataTypePtr createResultType(const DataTypePtr & type_)
     {
-        auto result_type = this->argument_types.at(0);
         if constexpr (Data::is_nullable)
-            return makeNullable(result_type);
-        return result_type;
+            return makeNullable(type_);
+        return type_;
     }
 
     void add(AggregateDataPtr __restrict place, const IColumn ** columns, size_t row_num, Arena * arena) const override
