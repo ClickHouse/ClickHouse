66742a8e2a27dbd62a2ee84663e40383dc5235f5
Youenn Lebras <youenn.lebras@contentsquare.com>
2022-11-30 12:00:40 +0100

    Fix style issues

--- src/Storages/StorageReplicatedMergeTree.cpp
+++ src/Storages/StorageReplicatedMergeTree.cpp
@@ -4561,7 +4561,6 @@ bool StorageReplicatedMergeTree::optimize(
         LOG_DEBUG(log, "Cleanup the ReplicatedMergeTree.");
 
     auto handle_noop = [&] (const char * fmt_string, auto ...args)
-
     {
         LOG_DEBUG(log, fmt::runtime(fmt_string), args...);
         if (query_context->getSettingsRef().optimize_throw_if_noop)
@@ -4576,6 +4575,7 @@ bool StorageReplicatedMergeTree::optimize(
 
     auto try_assign_merge = [&](const String & partition_id) -> bool
     {
+        LOG_INFO(log, "## -- try_assign_merge --\n");
         constexpr size_t max_retries = 10;
         size_t try_no = 0;
         for (; try_no < max_retries; ++try_no)
@@ -4619,14 +4619,15 @@ bool StorageReplicatedMergeTree::optimize(
             }
 
             /// If there is nothing to merge then we treat this merge as successful (needed for optimize final optimization)
-            if (select_decision == SelectPartsDecision::NOTHING_TO_MERGE) {
-                std::cout << "select_decision == SelectPartsDecision::NOTHING_TO_MERGE" << std::endl;
+            if (select_decision == SelectPartsDecision::NOTHING_TO_MERGE)
+            {
+                LOG_INFO(log, "## select_decision == SelectPartsDecision::NOTHING_TO_MERGE\n");
                 return false;
             }
 
             if (select_decision != SelectPartsDecision::SELECTED)
             {
-                std::cout << "sselect_decision != SelectPartsDecision::SELECTED" << std::endl;
+                LOG_INFO(log, "## select_decision != SelectPartsDecision::SELECTED\n");
                 constexpr const char * message_fmt = "Cannot select parts for optimization: {}";
                 assert(disable_reason != unknown_disable_reason);
                 if (!partition_id.empty())
@@ -4634,7 +4635,7 @@ bool StorageReplicatedMergeTree::optimize(
                 return handle_noop(message_fmt, disable_reason);
             }
 
-        std::cout << "----------------------------" << std::endl;
+            LOG_INFO(log, "## ----------------------------\n");
             ReplicatedMergeTreeLogEntryData merge_entry;
             CreateMergeEntryResult create_result = createLogEntryToMergeParts(
                 zookeeper, future_merged_part->parts,
@@ -4645,16 +4646,18 @@ bool StorageReplicatedMergeTree::optimize(
 
             if (create_result == CreateMergeEntryResult::MissingPart)
             {
-        std::cout << "create_result == CreateMergeEntryResult::MissingPart" << std::endl;
+                LOG_INFO(log, "## create_result == CreateMergeEntryResult::MissingPart\n");
 
                 static constexpr const char * message_fmt = "Can't create merge queue node in ZooKeeper, because some parts are missing";
                 return handle_noop(message_fmt);
             }
 
-            if (create_result == CreateMergeEntryResult::LogUpdated){
-        std::cout << "create_result == CreateMergeEntryResult::LogUpdated" << std::endl;
+            if (create_result == CreateMergeEntryResult::LogUpdated)
+            {
+                LOG_INFO(log, "## create_result == CreateMergeEntryResult::LogUpdated\n");
                 continue;
-                }
+            }
+            LOG_INFO(log, "## merge_entries.push_back\n");
 
             merge_entries.push_back(std::move(merge_entry));
             return true;
