f7ea81d4b10881ce86df8e97d572b8ad2d6a5fbd
avogar <pav.cruglov@yandex.ru>
2022-11-30 21:30:21 +0000

    Better test

--- tests/queries/0_stateless/02496_row_binary_large_string_size.sh
+++ tests/queries/0_stateless/02496_row_binary_large_string_size.sh
@@ -4,5 +4,4 @@ CUR_DIR=$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)
 # shellcheck source=../shell_config.sh
 . "$CUR_DIR"/../shell_config.sh
 
-$CLICKHOUSE_LOCAL -q "select * from file('data_binary/02496_large_string_size.bin', auto, 's String') settings input_format_max_binary_string_size=100000" 2>&1 | grep -q -F "TOO_LARGE_STRING_SIZE" && echo "OK" || echo "FAIL"
-
+printf '\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff' | $CLICKHOUSE_LOCAL --input_format_max_binary_string_size=100000 --input-format=RowBinary --structure='s String' -q "select * from table" 2>&1 | grep -q -F "TOO_LARGE_STRING_SIZE" && echo "OK" || echo FAIL"" 
