825035d483ed38fa4239951038f66f601c6aa0ae
Vitaly Baranov <vitlibar@clickhouse.com>
2022-12-01 13:43:58 +0100

    Remove StorageID::operator < (because of its inconsistency with ==).

--- src/Interpreters/StorageID.h
+++ src/Interpreters/StorageID.h
@@ -73,7 +73,6 @@ struct StorageID
 
     bool hasDatabase() const { return !database_name.empty(); }
 
-    bool operator<(const StorageID & rhs) const;
     bool operator==(const StorageID & rhs) const;
 
     void assertNotEmpty() const
