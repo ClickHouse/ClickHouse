c2a19fc354e17a31d22d3baecfd7511ced6be1cd
Alexander Tokmakov <tavplubix@gmail.com>
2022-12-01 19:41:29 +0300

    Update test.py

--- tests/integration/test_alter_settings_on_cluster/test.py
+++ tests/integration/test_alter_settings_on_cluster/test.py
@@ -53,6 +53,16 @@ def test_default_database_on_cluster(started_cluster):
             sql="SHOW CREATE test_local_table FORMAT TSV",
         ).endswith("old_parts_lifetime = 100\n")
 
+    ch1.query_and_get_error(
+        database="test_default_database",
+        sql="ALTER TABLE test_local_table MODIFY SETTING temporary_directories_lifetime = 1 RESET SETTING old_parts_lifetime;",
+    )
+
+    ch1.query_and_get_error(
+        database="test_default_database",
+        sql="ALTER TABLE test_local_table RESET SETTING old_parts_lifetime MODIFY SETTING temporary_directories_lifetime = 1;",
+    )
+
     ch1.query(
         database="test_default_database",
         sql="ALTER TABLE test_local_table ON CLUSTER 'cluster' RESET SETTING old_parts_lifetime;",
