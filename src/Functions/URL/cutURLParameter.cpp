69c46f5893d32b7344cb1cbf05457ffebeea7fc6
Roman Vasin <r.vasin@arenadata.io>
2022-12-02 11:05:54 +0000

    Remove unneeded header files

--- src/Functions/URL/cutURLParameter.cpp
+++ src/Functions/URL/cutURLParameter.cpp
@@ -1,16 +1,7 @@
-#include <Columns/ColumnArray.h>
-#include <Columns/ColumnConst.h>
 #include <Columns/ColumnString.h>
-#include <Columns/ColumnFixedString.h>
-#include <Columns/ColumnVector.h>
 #include <DataTypes/DataTypeArray.h>
 #include <DataTypes/DataTypeString.h>
-#include <DataTypes/DataTypesNumber.h>
 #include <Functions/FunctionHelpers.h>
-#include <Functions/IFunction.h>
-#include <IO/WriteHelpers.h>
-#include <Interpreters/Context.h>
-#include <base/StringRef.h>
 #include <Functions/FunctionFactory.h>
 #include <base/find_symbols.h>
 
