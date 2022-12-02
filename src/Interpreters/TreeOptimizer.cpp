1760f5b2b7c0237328e6fc2e9b6b7fe2300bd766
Duc Canh Le <canhld94@gmail.com>
2022-12-02 17:56:23 +0800

    fix some bad logics in ast level optimization

--- src/Interpreters/TreeOptimizer.cpp
+++ src/Interpreters/TreeOptimizer.cpp
@@ -788,9 +788,9 @@ void TreeOptimizer::optimizeIf(ASTPtr & query, Aliases & aliases, bool if_chain_
         OptimizeIfChainsVisitor().visit(query);
 }
 
-void TreeOptimizer::optimizeCountConstantAndSumOne(ASTPtr & query)
+void TreeOptimizer::optimizeCountConstantAndSumOne(ASTPtr & query, ContextPtr context)
 {
-    RewriteCountVariantsVisitor::visit(query);
+    RewriteCountVariantsVisitor(context).visit(query);
 }
 
 void TreeOptimizer::apply(ASTPtr & query, TreeRewriterResult & result,
@@ -835,7 +835,7 @@ void TreeOptimizer::apply(ASTPtr & query, TreeRewriterResult & result,
         optimizeAnyFunctions(query);
 
     if (settings.optimize_normalize_count_variants)
-        optimizeCountConstantAndSumOne(query);
+        optimizeCountConstantAndSumOne(query, context);
 
     if (settings.optimize_multiif_to_if)
         optimizeMultiIfToIf(query);
