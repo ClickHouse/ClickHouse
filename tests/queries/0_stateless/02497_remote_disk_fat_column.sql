104629cab647d6697feacb130e22e957a3b8c0a4
Nikolai Kochetov <nik-kochetov@yandex-team.ru>
2022-12-05 21:27:54 +0000

    Fix style.

--- tests/queries/0_stateless/02497_remote_disk_fat_column.sql
+++ tests/queries/0_stateless/02497_remote_disk_fat_column.sql
@@ -1,4 +1,4 @@
--- Tags: no-parallel, no-random-settings, no-fasttest, no-tsan, no-asan, no-msan
+-- Tags: no-random-settings, no-fasttest, no-tsan, no-asan, no-msan
 set allow_suspicious_fixed_string_types=1;
 create table fat_granularity (x UInt32, fat FixedString(160000)) engine = MergeTree order by x settings storage_policy = 's3_cache';
 
