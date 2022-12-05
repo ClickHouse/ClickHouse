dff7fc496fb3863db4c4f943847cc4b19bf66385
Arthur Passos <arthur.ti@outlook.com>
2022-12-05 14:00:30 -0300

    rename methods

--- src/Functions/keyvaluepair/src/impl/state/KeyStateHandler.h
+++ src/Functions/keyvaluepair/src/impl/state/KeyStateHandler.h
@@ -13,9 +13,9 @@ class KeyStateHandler : StateHandler {
 public:
     KeyStateHandler(char key_value_delimiter, char escape_character, std::optional<char> enclosing_character);
 
-    [[nodiscard]] NextState waitKey(const std::string & file, size_t pos) const;
-    [[nodiscard]] NextStateWithElement readKey(const std::string & file, size_t pos) const;
-    [[nodiscard]] NextStateWithElement readEnclosedKey(const std::string  &file, size_t pos) const;
+    [[nodiscard]] NextState wait(const std::string & file, size_t pos) const;
+    [[nodiscard]] NextStateWithElement read(const std::string & file, size_t pos) const;
+    [[nodiscard]] NextStateWithElement readEnclosed(const std::string  &file, size_t pos) const;
     [[nodiscard]] NextState readKeyValueDelimiter(const std::string & file, size_t pos) const;
 
 private:
