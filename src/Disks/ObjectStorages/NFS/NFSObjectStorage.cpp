28a896a7e1bb48680d031a3e9615d74c29bf6750
lihaibo42 <lihaibo42@jd.com>
2022-12-05 13:58:01 +0800

    Use read threadpool for NFS

--- src/Disks/ObjectStorages/NFS/NFSObjectStorage.cpp
+++ src/Disks/ObjectStorages/NFS/NFSObjectStorage.cpp
@@ -1,16 +1,6 @@
 #include "NFSObjectStorage.h"
 #include <Common/getRandomASCIIString.h>
 
-//#include <Common/randomSeed.h>
-//#include <Common/createHardLink.h>
-//#include <Common/filesystemHelpers.h>
-//#include <Common/quoteString.h>
-
-
-//#include <Disks/LocalDirectorySyncGuard.h>
-//#include <Disks/DiskLocal.h>
-//#include <Disks/DiskFactory.h>
-
 #include <Disks/IO/ReadIndirectBufferFromRemoteFS.h>
 #include <Disks/IO/WriteIndirectBufferFromRemoteFS.h>
 #include <Disks/IO/AsynchronousReadIndirectBufferFromRemoteFS.h>
@@ -24,12 +14,6 @@
 #include <IO/NFS/WriteBufferFromNFS.h>
 
 #include <Interpreters/Context.h>
-
-//#include <Disks/IO/createReadBufferFromFileBase.h>
-//#include <Disks/IO/AsynchronousReadIndirectBufferFromRemoteFS.h>
-//#include <Disks/IO/ReadBufferFromRemoteFSGather.h>
-//#include <base/FnTraits.h>
-//#include <boost/algorithm/string/predicate.hpp>
 
 
 namespace CurrentMetrics
@@ -118,8 +102,16 @@ std::unique_ptr<ReadBufferFromFileBase> NFSObjectStorage::readObjects( /// NOLIN
     };
 
     auto nfs_impl = std::make_unique<ReadBufferFromRemoteFSGather>(std::move(read_buffer_creator), objects, disk_read_settings);
-    auto buf = std::make_unique<ReadIndirectBufferFromRemoteFS>(std::move(nfs_impl), read_settings);
-    return std::make_unique<SeekAvoidingReadBuffer>(std::move(buf), settings->min_bytes_for_seek);
+    if (read_settings.remote_fs_method == RemoteFSReadMethod::threadpool)
+    {
+        auto reader = getThreadPoolReader();
+        return std::make_unique<AsynchronousReadIndirectBufferFromRemoteFS>(reader, disk_read_settings, std::move(nfs_impl));
+    }
+    else
+    {
+        auto buf = std::make_unique<ReadIndirectBufferFromRemoteFS>(std::move(nfs_impl), read_settings);
+        return std::make_unique<SeekAvoidingReadBuffer>(std::move(buf), settings->min_bytes_for_seek);
+    }
 }
 
 std::unique_ptr<WriteBufferFromFileBase> NFSObjectStorage::writeObject( /// NOLINT
@@ -201,83 +193,6 @@ std::unique_ptr<IObjectStorage> NFSObjectStorage::cloneObjectStorage(const std::
 {
     throw Exception(ErrorCodes::UNSUPPORTED_METHOD, "HDFS object storage doesn't support cloning");
 }
-
-/*
-RemoteFSPathKeeperPtr DiskNFS::createFSPathKeeper() const
-{
-    return std::make_shared<NFSPathKeeper>(settings->objects_chunk_size_to_delete);
-}
-
-void DiskNFS::removeFromRemoteFS(RemoteFSPathKeeperPtr fs_paths_keeper)
-{
-    auto * nfs_paths_keeper = dynamic_cast<NFSPathKeeper *>(fs_paths_keeper.get());
-    if (nfs_paths_keeper)
-        nfs_paths_keeper->removePaths([&](std::vector<std::string> && chunk)
-        {
-            for (const auto & nfs_object_path : chunk)
-            {
-                const String & nfs_path = nfs_object_path;
-                auto fs_path = fs::path(nfs_path);
-                if (0 != unlink(fs_path.c_str()) && errno != ENOENT)
-                    throwFromErrnoWithPath("Cannot unlink file " + fs_path.string(), fs_path, ErrorCodes::CANNOT_UNLINK);
-            }
-        });
-}
-*/
-
-/*
-std::unique_ptr<ReadBufferFromFileBase> DiskNFS::readFile(const String & path, const ReadSettings & read_settings, std::optional<size_t>, std::optional<size_t>) const
-{
-    auto metadata = readMetadata(path);
-
-    if (metadata.remote_fs_objects.size() > 0)
-        LOG_TEST(log, "Read from file by path {}. Existing NFS objects: {}, first path {}, size {}",
-            path, metadata.remote_fs_objects.size(),
-            metadata.remote_fs_objects.begin()->first, metadata.remote_fs_objects.begin()->second);
-    else
-        LOG_TEST(log, "Read from file by path {}. Existing NFS objects: {}", path, metadata.remote_fs_objects.size());
-
-    ReadSettings disk_read_settings{read_settings};
-    disk_read_settings.remote_fs_buffer_size = settings->remote_file_buffer_size;
-    if (cache)
-        disk_read_settings.remote_fs_cache = cache;
-
-    auto nfs_impl = std::make_unique<ReadBufferFromNFSGather>(path, settings->nfs_max_single_read_retries, remote_fs_root_path, 
-        metadata, disk_read_settings);
-    auto buf = std::make_unique<ReadIndirectBufferFromRemoteFS>(std::move(nfs_impl));
-    return std::make_unique<SeekAvoidingReadBuffer>(std::move(buf), settings->min_bytes_for_seek);
-}
-
-std::unique_ptr<WriteBufferFromFileBase> DiskNFS::writeFile(const String & path, size_t buf_size, WriteMode mode)
-{
-    /// Path to store new NFS object.
-    auto file_name = getRandomName();
-    auto nfs_path = remote_fs_root_path + file_name;
-
-    LOG_TEST(log, "{} to file by path: {}, metadata remote path {}.", mode == WriteMode::Rewrite ? "Write" : "Append", path, nfs_path);
-
-    int flags = (mode == WriteMode::Append) ? (O_APPEND | O_CREAT | O_WRONLY) : -1;
-    auto nfs_buffer = std::make_unique<WriteBufferFromNFS>(nfs_path, config, buf_size, flags);
-    auto create_metadata_callback = [this, path, mode, file_name] (size_t count)
-    {
-        readOrCreateUpdateAndStoreMetadata(path, mode, false, [file_name, count] (Metadata & metadata) { metadata.addObject(file_name, count); return true; });
-    };
-
-    return std::make_unique<WriteIndirectBufferFromRemoteFS>(std::move(nfs_buffer), std::move(create_metadata_callback), path);
-}
-
-
-bool DiskNFS::checkUniqueId(const String & nfs_file_path) const
-{
-    if (!boost::algorithm::starts_with(nfs_file_path, remote_fs_root_path))
-        return false;
-    const size_t begin_of_path = nfs_file_path.find('/', nfs_file_path.find("//") + 2);
-    const String remote_fs_object_path = nfs_file_path.substr(begin_of_path);
-    LOG_TEST(log, "Check unique ID, nfs file path {}, remote fs root path {}, remote_fs_object_path {}",
-                nfs_file_path, remote_fs_root_path, remote_fs_object_path);
-    return fs::exists(fs::path(remote_fs_root_path) / remote_fs_object_path);
-}
-*/
 
 }
 
