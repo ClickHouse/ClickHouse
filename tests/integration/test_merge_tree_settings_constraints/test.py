6428b8a1bb072322cdd0040a1310bcdd13ef016c
serxa <sergei@clickhouse.com>
2022-12-07 20:36:04 +0000

    check one more case in test

--- tests/integration/test_merge_tree_settings_constraints/test.py
+++ tests/integration/test_merge_tree_settings_constraints/test.py
@@ -20,6 +20,11 @@ def start_cluster():
 
 
 def test_merge_tree_settings_constraints():
+
+    assert "Setting storage_policy should not be changed" in instance.query_and_get_error(
+        f"CREATE TABLE wrong_table (number Int64) engine = MergeTree() ORDER BY number SETTINGS storage_policy = 'secret_policy'"
+    )
+
     expected_error = "Setting min_bytes_for_wide_part should"
 
     assert expected_error in instance.query_and_get_error(
