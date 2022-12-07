ec17e28a29ab47ab54ce805d3c01e933afc4c0d0
Antonio Andelic <antonio@clickhouse.com>
2022-12-07 07:47:16 +0000

    Add classes for new pass

---
+++ src/Analyzer/Passes/IfTransformStringsToEnumPass.cpp
@@ -0,0 +1,12 @@
+#include <Analyzer/Passes/IfTransformStringsToEnumPass.h>
+#include "Analyzer/IQueryTreeNode.h"
+#include "Interpreters/Context_fwd.h"
+
+namespace DB
+{
+
+void IfTransformStringsToEnumPass::run(QueryTreeNodePtr, ContextPtr)
+{
+}
+
+}
