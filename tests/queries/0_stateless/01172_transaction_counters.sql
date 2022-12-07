17e0b46ed901f2f3bd3288073180af5714eb7a01
Sema Checherinda <Sema.Checherinda@clickhouse.com>
2022-12-07 14:29:34 +0100

    make 01172_transaction_counters.sql stable

--- tests/queries/0_stateless/01172_transaction_counters.sql
+++ tests/queries/0_stateless/01172_transaction_counters.sql
@@ -2,7 +2,7 @@
 
 drop table if exists txn_counters;
 
-create table txn_counters (n Int64, creation_tid DEFAULT transactionID()) engine=MergeTree order by n;
+create table txn_counters (n Int64, creation_tid DEFAULT transactionID()) engine=MergeTree order by n SETTINGS old_parts_lifetime=3600;
 
 insert into txn_counters(n) values (1);
 select transactionID();
