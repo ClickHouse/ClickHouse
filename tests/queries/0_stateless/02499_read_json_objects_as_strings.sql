d0f9bb2ec2bac5f2f644847a90e03d4d13d56d9a
avogar <pav.cruglov@yandex.ru>
2022-12-08 18:58:18 +0000

    Allow to parse JSON objects into Strings

---
+++ tests/queries/0_stateless/02499_read_json_objects_as_strings.sql
@@ -0,0 +1,4 @@
+set input_format_json_read_objects_as_strings=1;
+desc format(JSONEachRow, '{"x" : "abc"}, {"x" : {"a" : 10, "b" : "abc"}}');
+select * from format(JSONEachRow, '{"x" : "abc"}, {"x" : {"a" : 10, "b" : "abc"}}');
+
