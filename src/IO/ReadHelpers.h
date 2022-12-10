dd640e0c14421ad08966c81da11c1e85c5f0d8a1
Yakov Olkhovskiy <yakov@clickhouse.com>
2022-12-10 04:39:43 +0000

    add variant parser and use it for toIPv6

--- src/IO/ReadHelpers.h
+++ src/IO/ReadHelpers.h
@@ -862,34 +862,8 @@ inline bool tryReadIPv4Text(IPv4 & ip, ReadBuffer & buf)
 template <typename ReturnType = void>
 inline ReturnType readIPv6TextImpl(IPv6 & ip, ReadBuffer & buf)
 {
-    unsigned char * p = reinterpret_cast<unsigned char *>(ip.toUnderType().items);
-
-    if (parseIPv4(buf.position(), [&buf](){ return buf.eof(); }, p))
-    {
-        if constexpr (std::endian::native == std::endian::little)
-        {
-            p[15] = p[0]; p[0] = 0;
-            p[14] = p[1]; p[1] = 0;
-            p[13] = p[2]; p[2] = 0;
-            p[12] = p[3]; p[3] = 0;
-        }
-        else
-        {
-            p[15] = p[3]; p[3] = 0;
-            p[14] = p[2]; p[2] = 0;
-            p[13] = p[1]; p[1] = 0;
-            p[12] = p[0]; p[0] = 0;
-        }
-
-        p[11] = 0xff;
-        p[10] = 0xff;
+    if (parseIPv6orIPv4(buf.position(), [&buf](){ return buf.eof(); }, reinterpret_cast<unsigned char *>(ip.toUnderType().items)))
         return ReturnType(true);
-    }
-
-    if (parseIPv6(buf.position(), [&buf](){ return buf.eof(); }, p))
-        return ReturnType(true);
-
-    memset(p, '\0', IPV6_BINARY_LENGTH);
 
     if constexpr (std::is_same_v<ReturnType, void>)
         throw ParsingException(std::string("Cannot parse IPv6 ").append(buf.position(), buf.available()), ErrorCodes::CANNOT_PARSE_IPV6);
