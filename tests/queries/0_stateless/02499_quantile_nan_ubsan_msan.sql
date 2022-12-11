a46699afcb07bc48d631091a9666703a53c72fa5
Alexey Milovidov <milovidov@clickhouse.com>
2022-12-11 03:19:06 +0100

    Add more tests

--- tests/queries/0_stateless/02499_quantile_nan_ubsan_msan.sql
+++ tests/queries/0_stateless/02499_quantile_nan_ubsan_msan.sql
@@ -10,3 +10,13 @@ SELECT DISTINCT arrayReduce('quantilesDeterministic(0.5)', materialize([]::Array
 SELECT DISTINCT arrayReduce('quantilesDeterministic(0, 0.5, 0.9, 1)', materialize([]::Array(DateTime('UTC'))), []::Array(UInt64)) FROM numbers(1000) LIMIT 10;
 SELECT DISTINCT arrayReduce('quantiles(0.5)', [CAST(-1, 'UInt256'), CAST(-2, 'UInt256')]) FROM numbers(1000) LIMIT 10;
 SELECT DISTINCT arrayReduce('quantiles(0.5)', []::Array(Float64)) FROM numbers(1000) LIMIT 10;
+
+SELECT quantile(0.5)(now()::DateTime('UTC')) WHERE 0;
+SELECT quantile(0.5)(now()::DateTime('UTC')) WHERE 0 WITH TOTALS;
+SELECT arrayReduce('quantile(0.5)', []::Array(DateTime('UTC')));
+
+SELECT DISTINCT arrayReduce('quantile(0.5)', materialize([]::Array(DateTime('UTC')))) FROM numbers(1000) LIMIT 10;
+SELECT DISTINCT arrayReduce('quantile(0.5)', [0xFFFFFFFFFFFFFFFF, 0xFFFFFFFFFFFFFFFE]) FROM numbers(1000) LIMIT 10;
+SELECT DISTINCT arrayReduce('quantileDeterministic(0.5)', materialize([]::Array(DateTime('UTC'))), []::Array(UInt64)) FROM numbers(1000) LIMIT 10;
+SELECT DISTINCT arrayReduce('quantile(0.5)', [CAST(-1, 'UInt256'), CAST(-2, 'UInt256')]) FROM numbers(1000) LIMIT 10;
+SELECT DISTINCT arrayReduce('quantile(0.5)', []::Array(Float64)) FROM numbers(1000) LIMIT 10;
