746cd8debde317d96d01336bf66b81301d53133d
Yakov Olkhovskiy <yakov@clickhouse.com>
2022-12-12 23:42:17 +0000

    IPv6CIDRToRange to always return IPv6 so to make it backward compatible, revert test relying on this

--- tests/queries/0_stateless/00938_ipv6_cidr_range.sql
+++ tests/queries/0_stateless/00938_ipv6_cidr_range.sql
@@ -25,11 +25,11 @@ SELECT IPv6NumToString(ip), cidr, IPv6CIDRToRange(ip, cidr) FROM ipv6_range;
 
 DROP TABLE ipv6_range;
 
-SELECT IPv6CIDRToRange(toIPv6('2001:0db8:0000:85a3:0000:0000:ac1f:8001'), 0);
-SELECT IPv6CIDRToRange(toIPv6('2001:0db8:0000:85a3:0000:0000:ac1f:8001'), 128);
-SELECT IPv6CIDRToRange(toIPv6('ffff:ffff:ffff:ffff:ffff:ffff:ffff:ffff'), 64);
-SELECT IPv6CIDRToRange(toIPv6('0000:0000:0000:0000:0000:0000:0000:0000'), 8);
-SELECT IPv6CIDRToRange(toIPv6('ffff:0000:0000:0000:0000:0000:0000:0000'), 4);
+SELECT IPv6CIDRToRange(IPv6StringToNum('2001:0db8:0000:85a3:0000:0000:ac1f:8001'), 0);
+SELECT IPv6CIDRToRange(IPv6StringToNum('2001:0db8:0000:85a3:0000:0000:ac1f:8001'), 128);
+SELECT IPv6CIDRToRange(IPv6StringToNum('ffff:ffff:ffff:ffff:ffff:ffff:ffff:ffff'), 64);
+SELECT IPv6CIDRToRange(IPv6StringToNum('0000:0000:0000:0000:0000:0000:0000:0000'), 8);
+SELECT IPv6CIDRToRange(IPv6StringToNum('ffff:0000:0000:0000:0000:0000:0000:0000'), 4);
 SELECT IPv6CIDRToRange(IPv6StringToNum('2001:0db8:0000:85a3:0000:0000:ac1f:8001'), 128) = IPv6CIDRToRange(IPv6StringToNum('2001:0db8:0000:85a3:0000:0000:ac1f:8001'), 200) ;
 
-SELECT IPv6CIDRToRange(toIPv6('2001:0db8:0000:85a3:0000:0000:ac1f:8001'), toUInt8(128 - number)) FROM numbers(2);
+SELECT IPv6CIDRToRange(IPv6StringToNum('2001:0db8:0000:85a3:0000:0000:ac1f:8001'), toUInt8(128 - number)) FROM numbers(2);
