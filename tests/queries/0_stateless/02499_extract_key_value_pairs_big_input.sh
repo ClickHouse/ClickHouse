a79c0786b70fe2e64679ebd07fc436e26c170528
Arthur Passos <arthur.ti@outlook.com>
2022-12-13 11:05:56 -0300

    fix tests

--- tests/queries/0_stateless/02499_extract_key_value_pairs_big_input.sh
+++ tests/queries/0_stateless/02499_extract_key_value_pairs_big_input.sh
@@ -5,10 +5,10 @@ CUR_DIR=$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)
 # shellcheck source=../shell_config.sh
 . "$CUR_DIR"/../shell_config.sh
 
-DATA_FILE=$CUR_DIR/data_parse_key_value/big_input.json.gz
+DATA_FILE=$CUR_DIR/data_extract_key_value_pairs/big_input_file.txt
 ${CLICKHOUSE_CLIENT} --query="DROP TABLE IF EXISTS json_map"
 ${CLICKHOUSE_CLIENT} --query="CREATE TABLE json_map (map Map(String, String)) ENGINE = Memory"
-zcat "$DATA_FILE" | ${CLICKHOUSE_CLIENT} -q "INSERT INTO json_map SELECT extractKeyValuePairs(file('big_input_file.txt'), '\\\', ':', ',', '\"')"
+cat "$DATA_FILE" | ${CLICKHOUSE_CLIENT} -q "INSERT INTO json_map SELECT extractKeyValuePairs(line, '\\\', ':', ',', '\"') FROM input('line String') FORMAT LineAsString"
 ${CLICKHOUSE_CLIENT} --query="SELECT * FROM json_map" | md5sum
 ${CLICKHOUSE_CLIENT} --query="SELECT count() FROM json_map"
 ${CLICKHOUSE_CLIENT} --query="drop table json_map"
