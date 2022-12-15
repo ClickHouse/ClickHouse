f696af6a2ce44626a2b16042be0b3d134b4f5f83
Arthur Passos <arthur.ti@outlook.com>
2022-12-15 09:47:36 -0300

    improve lib cmakefiles, add state docs and minor changes

--- src/Functions/keyvaluepair/src/impl/state/State.h
+++ src/Functions/keyvaluepair/src/impl/state/State.h
@@ -7,28 +7,31 @@ namespace DB
 
 enum State
 {
+    // Skip characters until it finds a valid first key character. Might jump to READING_KEY, READING_ENCLOSED_KEY or END.
     WAITING_KEY,
+    // Tries to read a key. Might jump to WAITING_KEY, WAITING_VALUE or END.
     READING_KEY,
+    // Tries to read an enclosed/ quoted key. Might jump to WAITING_KEY, READING_KV_DELIMITER or END.
     READING_ENCLOSED_KEY,
+    // Tries to read the key value pair delimiter. Might jump to WAITING_KEY, WAITING_VALUE or END.
     READING_KV_DELIMITER,
+    // Skip characters until it finds a valid first value character. Might jump to READING_ENCLOSED_VALUE, READING_EMPTY_VALUE or READING_VALUE.
     WAITING_VALUE,
+    // Tries to read a value. Jumps to FLUSH_PAIR.
     READING_VALUE,
+    // Tries to read an enclosed/ quoted value. Might jump to FLUSH_PAIR or END.
     READING_ENCLOSED_VALUE,
+    // "Reads" an empty value. Jumps to FLUSH_PAIR.
     READING_EMPTY_VALUE,
+    // In this state, both key and value have already been collected and should be flushed. Might jump to WAITING_KEY or END.
     FLUSH_PAIR,
     END
 };
 
 struct NextState
 {
-    std::size_t pos;
+    std::size_t position_in_string;
     State state;
-};
-
-struct NextStateWithElement
-{
-    NextState state;
-    std::string_view element;
 };
 
 }
