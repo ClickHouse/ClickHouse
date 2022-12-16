c44da1fa1462d36f331821095656defe51f46bf2
Robert Schulze <robert@clickhouse.com>
2022-12-16 10:54:01 +0000

    Cosmetics

--- tests/queries/0_stateless/02494_query_result_cache_eligible_queries.sql
+++ tests/queries/0_stateless/02494_query_result_cache_eligible_queries.sql
@@ -9,11 +9,11 @@ SET enable_experimental_query_result_cache = true;
 
 -- check that SELECT statements create entries in the query result cache ...
 SELECT 1 SETTINGS enable_experimental_query_result_cache = true;
-SELECT count(*) FROM system.queryresult_cache;
+SELECT COUNT(*) FROM system.queryresult_cache;
 
 -- ... yet EXPLAIN SELECT should not create such an entry ...
 EXPLAIN SELECT 2 SETTINGS enable_experimental_query_result_cache = true;
-SELECT count(*) FROM system.queryresult_cache;
+SELECT COUNT(*) FROM system.queryresult_cache;
 
 SYSTEM DROP QUERY RESULT CACHE;
 
@@ -21,49 +21,49 @@ SYSTEM DROP QUERY RESULT CACHE;
 
 -- CREATE
 CREATE TABLE eligible_test (a String) ENGINE=MergeTree ORDER BY a; --  SETTINGS enable_experimental_query_result_cache = true; -- SETTINGS rejected as unknown
-SELECT count(*) FROM system.queryresult_cache;
+SELECT COUNT(*) FROM system.queryresult_cache;
 
 -- ALTER
 ALTER TABLE eligible_test ADD COLUMN b String SETTINGS enable_experimental_query_result_cache = true;
-SELECT count(*) FROM system.queryresult_cache;
+SELECT COUNT(*) FROM system.queryresult_cache;
 
 -- INSERT
 INSERT INTO eligible_test VALUES('a', 'b'); -- SETTINGS enable_experimental_query_result_cache = true; -- SETTINGS rejected as unknown
-SELECT count(*) FROM system.queryresult_cache;
+SELECT COUNT(*) FROM system.queryresult_cache;
 INSERT INTO eligible_test SELECT * FROM eligible_test SETTINGS enable_experimental_query_result_cache = true;
-SELECT count(*) FROM system.queryresult_cache;
+SELECT COUNT(*) FROM system.queryresult_cache;
 
 -- SHOW
 SHOW TABLES SETTINGS enable_experimental_query_result_cache = true;
-SELECT count(*) FROM system.queryresult_cache;
+SELECT COUNT(*) FROM system.queryresult_cache;
 
 -- CHECK
 CHECK TABLE eligible_test SETTINGS enable_experimental_query_result_cache = true;
-SELECT count(*) FROM system.queryresult_cache;
+SELECT COUNT(*) FROM system.queryresult_cache;
 
 -- DESCRIBE
 DESCRIBE TABLE eligible_test SETTINGS enable_experimental_query_result_cache = true;
-SELECT count(*) FROM system.queryresult_cache;
+SELECT COUNT(*) FROM system.queryresult_cache;
 
 -- EXISTS
 EXISTS TABLE eligible_test SETTINGS enable_experimental_query_result_cache = true;
-SELECT count(*) FROM system.queryresult_cache;
+SELECT COUNT(*) FROM system.queryresult_cache;
 
 -- KILL
 KILL QUERY WHERE query_id='3-857d-4a57-9ee0-3c7da5d60a90' SETTINGS enable_experimental_query_result_cache = true;
-SELECT count(*) FROM system.queryresult_cache;
+SELECT COUNT(*) FROM system.queryresult_cache;
 
 -- OPTIMIZE
 OPTIMIZE TABLE eligible_test FINAL SETTINGS enable_experimental_query_result_cache = true;
-SELECT count(*) FROM system.queryresult_cache;
+SELECT COUNT(*) FROM system.queryresult_cache;
 
 -- TRUNCATE
 TRUNCATE TABLE eligible_test SETTINGS enable_experimental_query_result_cache = true;
-SELECT count(*) FROM system.queryresult_cache;
+SELECT COUNT(*) FROM system.queryresult_cache;
 
 -- RENAME
 RENAME TABLE eligible_test TO eligible_test2 SETTINGS enable_experimental_query_result_cache = true;
-SELECT count(*) FROM system.queryresult_cache;
+SELECT COUNT(*) FROM system.queryresult_cache;
 
 SYSTEM DROP QUERY RESULT CACHE;
 DROP TABLE eligible_test2;
--- tests/queries/0_stateless/02494_query_result_cache_ignore_nondeterministic_functions.sql
+++ tests/queries/0_stateless/02494_query_result_cache_ignore_nondeterministic_functions.sql
@@ -3,12 +3,12 @@
 SYSTEM DROP QUERY RESULT CACHE;
 
 -- rand() is non-deterministic, with default settings no entry in the query result cache should be created
-SELECT count(rand(1)) SETTINGS enable_experimental_query_result_cache = true;
-SELECT count(*) FROM system.queryresult_cache;
+SELECT COUNT(rand(1)) SETTINGS enable_experimental_query_result_cache = true;
+SELECT COUNT(*) FROM system.queryresult_cache;
 
 -- force an entry
-SELECT count(rand(1)) SETTINGS enable_experimental_query_result_cache = true, query_result_cache_ignore_nondeterministic_functions = false;
-SELECT count(*) FROM system.queryresult_cache;
+SELECT COUNT(RAND(1)) SETTINGS enable_experimental_query_result_cache = true, query_result_cache_ignore_nondeterministic_functions = false;
+SELECT COUNT(*) FROM system.queryresult_cache;
 
 SYSTEM DROP QUERY RESULT CACHE;
 
