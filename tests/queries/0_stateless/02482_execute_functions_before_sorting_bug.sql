59120f61d9b6106c8ba924913c665f87ef9e3493
avogar <pav.cruglov@yandex.ru>
2022-12-16 13:17:21 +0000

    Better test

--- tests/queries/0_stateless/02482_execute_functions_before_sorting_bug.sql
+++ tests/queries/0_stateless/02482_execute_functions_before_sorting_bug.sql
@@ -5,5 +5,5 @@ insert into test select 1;
 insert into test select 2;
 select x + 1e10 from test order by 1e10, x;
 select x + (1e10 + 1e20) from test order by (1e10 + 1e20), x;
-select x + (pi() + exp(2)) from test order by (pi() + exp(2)), x;
+select x + (pow(2, 2) + pow(3, 2)) from test order by (pow(2,2) + pow(3, 2)), x;
 drop table test;
