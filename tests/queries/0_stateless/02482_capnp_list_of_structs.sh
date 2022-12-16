a7a1ebbed5ca1521ef77c48197a8018691c104b7
avogar <pav.cruglov@yandex.ru>
2022-12-16 14:44:49 +0000

    Try fix tests

--- tests/queries/0_stateless/02482_capnp_list_of_structs.sh
+++ tests/queries/0_stateless/02482_capnp_list_of_structs.sh
@@ -8,7 +8,7 @@ CURDIR=$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)
 USER_FILES_PATH=$(clickhouse-client --query "select _path,_file from file('nonexist.txt', 'CSV', 'val1 char')" 2>&1 | grep Exception | awk '{gsub("/nonexist.txt","",$9); print $9}')
 touch $USER_FILES_PATH/data.capnp
 
-SCHEMADIR=$(clickhouse-client --query "select * from file('02482_data.capnp', 'CapnProto', 'val1 char') settings format_schema='nonexist:Message'" 2>&1 | grep Exception | grep -oP "file \K.*(?=/nonexist.capnp)")
+SCHEMADIR=$(clickhouse-client --query "select * from file('data.capnp', 'CapnProto', 'val1 char') settings format_schema='nonexist:Message'" 2>&1 | grep Exception | grep -oP "file \K.*(?=/nonexist.capnp)")
 CLIENT_SCHEMADIR=$CURDIR/format_schemas
 SERVER_SCHEMADIR=test_02482
 mkdir -p $SCHEMADIR/$SERVER_SCHEMADIR
