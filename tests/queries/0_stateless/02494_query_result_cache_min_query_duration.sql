83ea8deeee9265b9e36fdd8cea6d30eba64bf8be
Robert Schulze <robert@clickhouse.com>
2022-12-17 18:10:02 +0000

    Fix 02494_query_result_cache_min_query_duration

--- tests/queries/0_stateless/02494_query_result_cache_min_query_duration.sql
+++ tests/queries/0_stateless/02494_query_result_cache_min_query_duration.sql
@@ -2,17 +2,16 @@
 
 SYSTEM DROP QUERY RESULT CACHE;
 
--- this does not create a cache entry ...
-SET query_result_cache_max_entry_size = 0;
+-- this does create an entry in the query result cache
 SELECT 1 SETTINGS enable_experimental_query_result_cache = true;
-SELECT count(*) FROM system.queryresult_cache;
+SELECT COUNT(*) FROM system.queryresult_cache;
 
 SYSTEM DROP QUERY RESULT CACHE;
 
--- ... but this does
-SET query_result_cache_max_entry_size = 9999999;
+-- ... but this does not because the query executes much faster than the milliseconds threshold
+SET query_result_cache_max_entry_size = 10000;
 SELECT 1 SETTINGS enable_experimental_query_result_cache = true;
-SELECT count(*) FROM system.queryresult_cache;
+SELECT COUNT(*) FROM system.queryresult_cache;
 
 SYSTEM DROP QUERY RESULT CACHE;
 
