69c0932ca1b873f2c12b8f0b21dbbb15e57c22d9
Robert Schulze <robert@clickhouse.com>
2022-12-19 09:23:57 +0000

    Fix 02494_drop_query_result_cache (add "no-parallel" tag)

--- tests/queries/0_stateless/02494_query_result_cache_drop_cache.sql
+++ tests/queries/0_stateless/02494_query_result_cache_drop_cache.sql
@@ -1,11 +1,9 @@
--- { echoOn }
+-- Tags: no-parallel
 
 -- run query and cache query result
 SELECT 1 SETTINGS enable_experimental_query_result_cache = true;
-1
 SELECT count(*) FROM system.queryresult_cache;
-1
+
 -- query results are no longer in cache after drop
 SYSTEM DROP QUERY RESULT CACHE;
 SELECT count(*) FROM system.queryresult_cache;
-0
