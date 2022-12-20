d1756124eec04c665e4fee8ecd8c0f7037f5b5cf
Alexey Milovidov <milovidov@clickhouse.com>
2022-12-20 15:19:41 +0300

    Update 02509_h3_arguments.sql

--- tests/queries/0_stateless/02509_h3_arguments.sql
+++ tests/queries/0_stateless/02509_h3_arguments.sql
@@ -6,11 +6,6 @@ DROP TABLE IF EXISTS data_table;
 CREATE TABLE data_table (id UInt64, longitude Float64, latitude Float64) ENGINE=MergeTree ORDER BY id;
 INSERT INTO data_table SELECT number, number, number FROM numbers(10);
 SELECT geoToH3(longitude,  latitude, toUInt8(8)) AS h3Index FROM data_table ORDER BY 1;
-
-DROP TABLE data_table;
-
-CREATE TABLE data_table (id UInt64, longitude Float64, latitude Float64) ENGINE=MergeTree ORDER BY id;
-INSERT INTO data_table SELECT number, number, number FROM numbers(10);
 SELECT geoToH3(longitude,  latitude, toUInt8(longitude - longitude + 8)) AS h3Index FROM data_table ORDER BY 1;
 
 DROP TABLE data_table;
