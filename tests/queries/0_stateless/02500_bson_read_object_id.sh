0a49a3812e61cd756b15cb287651996e35230c5c
avogar <pav.cruglov@yandex.ru>
2022-12-20 16:25:13 +0000

    Try fix test

--- tests/queries/0_stateless/02500_bson_read_object_id.sh
+++ tests/queries/0_stateless/02500_bson_read_object_id.sh
@@ -5,6 +5,6 @@ CURDIR=$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)
 # shellcheck source=../shell_config.sh
 . "$CURDIR"/../shell_config.sh
 
-$CLICKHOUSE_LOCAL -q "desc file('data_bson/comments.bson')"
-$CLICKHOUSE_LOCAL -q "select _id from file('data_bson/comments.bson') format Null"
+$CLICKHOUSE_LOCAL -q "desc file('$CURDIR/data_bson/comments.bson')"
+$CLICKHOUSE_LOCAL -q "select _id from file('$CURDIR/data_bson/comments.bson') format Null"
 
