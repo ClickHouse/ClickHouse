dd3ea5e937f7f0fbd3889fcc388859c3d95d2132
Kruglov Pavel <48961922+Avogar@users.noreply.github.com>
2022-12-20 19:14:52 +0100

    Update tests/queries/0_stateless/02513_csv_bool_allow_crlf.sh
    Co-authored-by: Nick Sweeting <nsweeting@gmail.com>

--- tests/queries/0_stateless/02513_csv_bool_allow_crlf.sh
+++ tests/queries/0_stateless/02513_csv_bool_allow_crlf.sh
@@ -4,4 +4,4 @@ CURDIR=$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)
 # shellcheck source=../shell_config.sh
 . "$CURDIR"/../shell_config.sh
 
-echo -ne "True\r\nFalse\r\n" | $CLICKHOUSE_LOCAL --stracture='x Bool' --input-format=CSV -q "select * from table";
+echo -ne "True\r\nFalse\r\n" | $CLICKHOUSE_LOCAL --structure='x Bool' --input-format=CSV -q "select * from table";
