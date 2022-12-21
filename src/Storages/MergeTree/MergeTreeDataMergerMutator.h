edcd0d95ca31674780709ebce1dadd016443830b
alesapin <alesapin@gmail.com>
2022-12-21 23:25:50 +0100

    Revert "Remove explicit dependency on fast test to debug the issue"
    This reverts commit 2fe969a2e1789312833ba5741d475d6ba43adcec.

--- src/Storages/MergeTree/MergeTreeDataMergerMutator.h
+++ src/Storages/MergeTree/MergeTreeDataMergerMutator.h
@@ -45,7 +45,7 @@ public:
                                                         const MergeTreeTransaction *,
                                                         String *)>;
 
-    explicit MergeTreeDataMergerMutator(MergeTreeData & data_);
+    MergeTreeDataMergerMutator(MergeTreeData & data_);
 
     /** Get maximum total size of parts to do merge, at current moment of time.
       * It depends on number of free threads in background_pool and amount of free space in disk.
