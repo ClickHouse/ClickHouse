05b294fc3b3bb3f595441fba37fac521314df911
Nikolay Degterinsky <evillique@gmail.com>
2022-12-28 01:18:21 +0000

    Better test for allow_implicit_no_password

--- tests/integration/test_password_constraints/test.py
+++ tests/integration/test_password_constraints/test.py
@@ -5,7 +5,7 @@ from helpers.cluster import ClickHouseCluster
 cluster = ClickHouseCluster(__file__)
 
 node = cluster.add_instance("node", main_configs=["configs/complexity_rules.xml"])
-
+node1 = cluster.add_instance("node1", main_configs=["configs/allow_implicit_no_password.xml"])
 
 @pytest.fixture(scope="module")
 def start_cluster():
@@ -40,3 +40,15 @@ def test_complexity_rules(start_cluster):
 
     node.query("CREATE USER u_5 IDENTIFIED WITH plaintext_password BY 'aA!000000000'")
     node.query("DROP USER u_5")
+
+
+def test_allow_implicit_no_password():
+
+    error_message = "Authentication type NO_PASSWORD must be explicitly specified, check the setting allow_implicit_no_password in the server configuration"
+    assert error_message in node1.query_and_get_error(
+        "CREATE USER u1"
+    )
+
+    node1.query("CREATE USER u2 IDENTIFIED WITH no_password")
+    node1.query("CREATE USER u3 IDENTIFIED BY 'qwe123'")
+    node1.query("DROP USER u2, u3")
