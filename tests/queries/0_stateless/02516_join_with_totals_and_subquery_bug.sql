97700557e252cc3e99544628162d0587f2386c42
Nikolai Kochetov <nik-kochetov@yandex-team.ru>
2022-12-28 13:11:49 +0000

    Update test.

--- tests/queries/0_stateless/02516_join_with_totals_and_subquery_bug.sql
+++ tests/queries/0_stateless/02516_join_with_totals_and_subquery_bug.sql
@@ -12,4 +12,42 @@ INNER JOIN
     SELECT 1
     GROUP BY 1
         WITH TOTALS
-) AS t2 USING (a)
+) AS t2 USING (a);
+
+SELECT a
+FROM
+(
+    SELECT
+        NULL AS a,
+        NULL AS b,
+        NULL AS c
+    UNION ALL
+    SELECT
+        100000000000000000000.,
+        NULL,
+        NULL
+    WHERE 0
+    GROUP BY
+        GROUPING SETS ((NULL))
+        WITH TOTALS
+) AS js1
+ALL LEFT JOIN
+(
+    SELECT
+        NULL AS a,
+        2147483647 AS d
+    GROUP BY
+        NULL,
+        '214748364.8'
+        WITH CUBE
+        WITH TOTALS
+    UNION ALL
+    SELECT
+        2147483646,
+        NULL
+    GROUP BY
+        base64Encode(materialize(NULL)),
+        NULL
+        WITH TOTALS
+) AS js2 USING (a)
+ORDER BY b ASC NULLS FIRST;
