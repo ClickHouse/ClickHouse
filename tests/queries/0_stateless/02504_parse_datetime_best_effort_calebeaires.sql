8ee297de7409ff185e791b198cfbc24c1fe0720d
Alexey Milovidov <milovidov@clickhouse.com>
2022-12-28 15:22:25 +0100

    Fix test

--- tests/queries/0_stateless/02504_parse_datetime_best_effort_calebeaires.sql
+++ tests/queries/0_stateless/02504_parse_datetime_best_effort_calebeaires.sql
@@ -2,4 +2,4 @@ CREATE TEMPORARY TABLE my_table (col_date Date, col_date32 Date32, col_datetime 
 insert into `my_table` (`col_date`, `col_date32`, `col_datetime`, `col_datetime32`, `col_datetime64`) values (parseDateTime64BestEffort('1969-01-01'), '1969-01-01', parseDateTime64BestEffort('1969-01-01 10:42:00'), parseDateTime64BestEffort('1969-01-01 10:42:00'), parseDateTime64BestEffort('1969-01-01 10:42:00'));
 
 -- The values for Date32 and DateTime64 will be year 1969, while the values of Date, DateTime will contain a value affected by implementation-defined overflow and can be arbitrary.
-SELECT * APPLY(x -> (toTypeName(x), x)) FROM my_table;
+SELECT col_date, col_date32, toYear(col_datetime), toYear(col_datetime32), col_datetime64 FROM my_table;
