73af1698b023ec83aa85db0ec756b276279ac441
Yatsishin Ilya <2159081+qoega@users.noreply.github.com>
2022-12-29 19:07:14 +0000

    black

--- tests/integration/test_grpc_protocol/test.py
+++ tests/integration/test_grpc_protocol/test.py
@@ -42,7 +42,8 @@ node = cluster.add_instance(
     main_configs=["configs/grpc_config.xml"],
     # Bug in TSAN reproduces in this test https://github.com/grpc/grpc/issues/29550#issuecomment-1188085387
     # second_deadlock_stack -- just ordinary option we use everywhere, don't want to overwrite it
-    env_variables={"TSAN_OPTIONS": "report_atomic_races=0 second_deadlock_stack=1"},)
+    env_variables={"TSAN_OPTIONS": "report_atomic_races=0 second_deadlock_stack=1"},
+)
 main_channel = None
 
 
