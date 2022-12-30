a3441caf581f4ddc7e60d2da81b03486e4ee95d7
taiyang-li <654010905@qq.com>
2022-12-30 18:27:44 +0800

    add uts

---
+++ tests/queries/0_stateless/02518_rewrite_aggregate_function_with_if.sql
@@ -0,0 +1,27 @@
+set optimize_rewrite_aggregate_function_with_if = false;
+
+explain syntax select sum(if(number % 2, number, 0)) from numbers(100);
+explain syntax select sum(if(number % 2, 0, number)) from numbers(100);
+
+explain syntax select sum(if(number % 2, number, null)) from numbers(100);
+explain syntax select sum(if(number % 2, null, number)) from numbers(100);
+
+explain syntax select avg(if(number % 2, number, null)) from numbers(100);
+explain syntax select avg(if(number % 2, null, number)) from numbers(100);
+
+explain syntax select quantiles(0.5, 0.9, 0.99)(if(number % 2, number, null)) from numbers(100);
+explain syntax select quantiles(0.5, 0.9, 0.99)(if(number % 2, null, number)) from numbers(100);
+
+set optimize_rewrite_aggregate_function_with_if = true;
+
+explain syntax select sum(if(number % 2, number, 0)) from numbers(100);
+explain syntax select sum(if(number % 2, 0, number)) from numbers(100);
+
+explain syntax select sum(if(number % 2, number, null)) from numbers(100);
+explain syntax select sum(if(number % 2, null, number)) from numbers(100);
+
+explain syntax select avg(if(number % 2, number, null)) from numbers(100);
+explain syntax select avg(if(number % 2, null, number)) from numbers(100);
+
+explain syntax select quantiles(0.5, 0.9, 0.99)(if(number % 2, number, null)) from numbers(100);
+explain syntax select quantiles(0.5, 0.9, 0.99)(if(number % 2, null, number)) from numbers(100);
