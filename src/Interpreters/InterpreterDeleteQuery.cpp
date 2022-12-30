57dc0786b471f713f0885444d3a7432dcfb04998
robot-clickhouse <robot-clickhouse@users.noreply.github.com>
2022-12-30 14:02:56 +0000

    Backport #44705 to 22.10: Fix crash on delete from materialized view

--- src/Interpreters/InterpreterDeleteQuery.cpp
+++ src/Interpreters/InterpreterDeleteQuery.cpp
@@ -23,6 +23,7 @@ namespace ErrorCodes
 {
     extern const int TABLE_IS_READ_ONLY;
     extern const int SUPPORT_IS_DISABLED;
+    extern const int BAD_ARGUMENTS;
 }
 
 
@@ -58,8 +59,7 @@ BlockIO InterpreterDeleteQuery::execute()
     auto table_lock = table->lockForShare(getContext()->getCurrentQueryId(), getContext()->getSettingsRef().lock_acquire_timeout);
     auto metadata_snapshot = table->getInMemoryMetadataPtr();
 
-    auto merge_tree = std::dynamic_pointer_cast<MergeTreeData>(table);
-    if (!merge_tree)
+    if (table->supportsDelete())
     {
         /// Convert to MutationCommand
         MutationCommands mutation_commands;
@@ -75,39 +75,45 @@ BlockIO InterpreterDeleteQuery::execute()
         table->mutate(mutation_commands, getContext());
         return {};
     }
+    else if (table->supportsLightweightDelete())
+    {
+        if (!getContext()->getSettingsRef().allow_experimental_lightweight_delete)
+            throw Exception(ErrorCodes::SUPPORT_IS_DISABLED, "Lightweight delete mutate is experimental. Set `allow_experimental_lightweight_delete` setting to enable it");
 
-    if (!getContext()->getSettingsRef().allow_experimental_lightweight_delete)
-        throw Exception(ErrorCodes::SUPPORT_IS_DISABLED, "Lightweight delete mutate is experimental. Set `allow_experimental_lightweight_delete` setting to enable it");
+        /// Convert to MutationCommand
+        MutationCommands mutation_commands;
+        MutationCommand mut_command;
 
-    /// Convert to MutationCommand
-    MutationCommands mutation_commands;
-    MutationCommand mut_command;
+        /// Build "UPDATE _row_exists = 0 WHERE predicate" query
+        mut_command.type = MutationCommand::Type::UPDATE;
+        mut_command.predicate = delete_query.predicate;
 
-    /// Build "UPDATE _row_exists = 0 WHERE predicate" query
-    mut_command.type = MutationCommand::Type::UPDATE;
-    mut_command.predicate = delete_query.predicate;
+        auto command = std::make_shared<ASTAlterCommand>();
+        command->type = ASTAlterCommand::UPDATE;
+        command->predicate = delete_query.predicate;
+        command->update_assignments = std::make_shared<ASTExpressionList>();
+        auto set_row_does_not_exist = std::make_shared<ASTAssignment>();
+        set_row_does_not_exist->column_name = LightweightDeleteDescription::FILTER_COLUMN.name;
+        auto zero_value = std::make_shared<ASTLiteral>(DB::Field(UInt8(0)));
+        set_row_does_not_exist->children.push_back(zero_value);
+        command->update_assignments->children.push_back(set_row_does_not_exist);
+        command->children.push_back(command->predicate);
+        command->children.push_back(command->update_assignments);
+        mut_command.column_to_update_expression[set_row_does_not_exist->column_name] = zero_value;
+        mut_command.ast = command->ptr();
 
-    auto command = std::make_shared<ASTAlterCommand>();
-    command->type = ASTAlterCommand::UPDATE;
-    command->predicate = delete_query.predicate;
-    command->update_assignments = std::make_shared<ASTExpressionList>();
-    auto set_row_does_not_exist = std::make_shared<ASTAssignment>();
-    set_row_does_not_exist->column_name = LightweightDeleteDescription::FILTER_COLUMN.name;
-    auto zero_value = std::make_shared<ASTLiteral>(DB::Field(UInt8(0)));
-    set_row_does_not_exist->children.push_back(zero_value);
-    command->update_assignments->children.push_back(set_row_does_not_exist);
-    command->children.push_back(command->predicate);
-    command->children.push_back(command->update_assignments);
-    mut_command.column_to_update_expression[set_row_does_not_exist->column_name] = zero_value;
-    mut_command.ast = command->ptr();
+        mutation_commands.emplace_back(mut_command);
 
-    mutation_commands.emplace_back(mut_command);
+        table->checkMutationIsPossible(mutation_commands, getContext()->getSettingsRef());
+        MutationsInterpreter(table, metadata_snapshot, mutation_commands, getContext(), false).validate();
+        table->mutate(mutation_commands, getContext());
 
-    table->checkMutationIsPossible(mutation_commands, getContext()->getSettingsRef());
-    MutationsInterpreter(table, metadata_snapshot, mutation_commands, getContext(), false).validate();
-    table->mutate(mutation_commands, getContext());
-
-    return {};
+        return {};
+    }
+    else
+    {
+        throw Exception(ErrorCodes::BAD_ARGUMENTS, "DELETE query is not supported for table");
+    }
 }
 
 }
