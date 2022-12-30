f1c125ba896b0ac7dd920c8554d2af2e90a211e3
Nikolai Kochetov <nik-kochetov@yandex-team.ru>
2022-12-30 17:23:09 +0000

    Update test.

--- tests/queries/0_stateless/02517_wrong_total_structure_crash.sql
+++ tests/queries/0_stateless/02517_wrong_total_structure_crash.sql
@@ -1,7 +1,7 @@
-CREATE TABLE alias10__fuzz_13 (`Id` Array(Array(UInt256)), `EventDate` Array(String), `field1` Array(Array(Nullable(Int8))), `field2` Array(Date), `field3` Array(Array(Array(UInt128)))) ENGINE = Distributed(test_shard_localhost, currentDatabase(), alias_local10);
+CREATE OR REPLACE TABLE alias10__fuzz_13 (`Id` Array(Array(UInt256)), `EventDate` Array(String), `field1` Array(Array(Nullable(Int8))), `field2` Array(Date), `field3` Array(Array(Array(UInt128)))) ENGINE = Distributed(test_shard_localhost, currentDatabase(), alias_local10);
 
 set allow_deprecated_syntax_for_merge_tree=1;
-CREATE TABLE alias_local10 (
+CREATE OR REPLACE TABLE alias_local10 (
   Id Int8,
   EventDate Date DEFAULT '2000-01-01',
   field1 Int8,
@@ -18,3 +18,8 @@ CREATE OR REPLACE TABLE local (x Int8) ENGINE = Memory;
 CREATE OR REPLACE TABLE distributed (x Array(Int8)) ENGINE = Distributed(test_shard_localhost, currentDatabase(), local);
 SET prefer_localhost_replica = 0;
 SELECT x FROM distributed GROUP BY x WITH TOTALS; -- { serverError TYPE_MISMATCH }
+
+DROP TABLE distributed;
+DROP TABLE local;
+DROP TABLE alias_local10;
+DROP TABLE alias10__fuzz_13;
