cb85000edf565a212f93c27b6659c99c32612251
Han Fei <hanfei19910905@gmail.com>
2022-12-31 21:21:03 +0100

    output debug info when test fails

--- tests/queries/0_stateless/02515_cleanup_async_insert_block_ids.sh
+++ tests/queries/0_stateless/02515_cleanup_async_insert_block_ids.sh
@@ -20,7 +20,7 @@ for i in {1..100}; do
     $CLICKHOUSE_CLIENT --async_insert 1 --async_insert_deduplicate 1 --wait_for_async_insert 0 --query "insert into t_async_insert_cleanup values ($i), ($((i + 1))), ($((i + 2)))"
 done
 
-while true; do
+for i in {1..300}; do
     answer=$($CLICKHOUSE_CLIENT --query "SELECT count(*) FROM system.zookeeper WHERE path like '/clickhouse/tables/$CLICKHOUSE_TEST_ZOOKEEPER_PREFIX/t_async_insert_cleanup/async_blocks%' settings allow_unrestricted_reads_from_keeper = 'true'")
     if [ $answer == '10' ]; then
         $CLICKHOUSE_CLIENT -n --query "DROP TABLE t_async_insert_cleanup NO DELAY;"
@@ -28,3 +28,5 @@ while true; do
     fi
     sleep 1
 done
+
+$($CLICKHOUSE_CLIENT --query "SELECT count(*) FROM system.zookeeper WHERE path like '/clickhouse/tables/$CLICKHOUSE_TEST_ZOOKEEPER_PREFIX/t_async_insert_cleanup/async_blocks%' settings allow_unrestricted_reads_from_keeper = 'true'")
