7b487dd92361a0fef607ffa982784914f443df32
flynn <fenglv15@mails.ucas.ac.cn>
2023-01-02 12:37:43 +0000

    fix

--- tests/queries/0_stateless/01509_output_format_pretty_row_numbers.sql
+++ tests/queries/0_stateless/01509_output_format_pretty_row_numbers.sql
@@ -24,10 +24,10 @@ SELECT * FROM (SELECT 1 AS a UNION ALL SELECT 2 as a) ORDER BY a FORMAT PrettyCo
 SELECT * FROM (SELECT 1 AS a UNION ALL SELECT 2 as a) ORDER BY a FORMAT PrettySpace;
 SELECT * FROM (SELECT 1 AS a UNION ALL SELECT 2 as a) ORDER BY a FORMAT PrettySpaceNoEscapes;
 
--- SELECT * FROM numbers(10) ORDER BY number FORMAT Pretty;
--- SELECT * FROM numbers(10) ORDER BY number FORMAT PrettyCompact;
--- SELECT * FROM numbers(10) ORDER BY number FORMAT PrettyCompactMonoBlock;
--- SELECT * FROM numbers(10) ORDER BY number FORMAT PrettyNoEscapes;
--- SELECT * FROM numbers(10) ORDER BY number FORMAT PrettyCompactNoEscapes;
--- SELECT * FROM numbers(10) ORDER BY number FORMAT PrettySpace
--- SELECT * FROM numbers(10) ORDER BY number FORMAT PrettySpaceNoEscapes;
+SELECT * FROM numbers(10) ORDER BY number FORMAT Pretty;
+SELECT * FROM numbers(10) ORDER BY number FORMAT PrettyCompact;
+SELECT * FROM numbers(10) ORDER BY number FORMAT PrettyCompactMonoBlock;
+SELECT * FROM numbers(10) ORDER BY number FORMAT PrettyNoEscapes;
+SELECT * FROM numbers(10) ORDER BY number FORMAT PrettyCompactNoEscapes;
+SELECT * FROM numbers(10) ORDER BY number FORMAT PrettySpace;
+SELECT * FROM numbers(10) ORDER BY number FORMAT PrettySpaceNoEscapes;
