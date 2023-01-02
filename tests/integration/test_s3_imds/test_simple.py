92be50bd68d17454f8964ac6589bd35c6f24688c
Vitaly Baranov <vitlibar@clickhouse.com>
2023-01-02 15:34:31 +0100

    Add test for IMDS session token.

--- tests/integration/test_s3_imds/test_simple.py
+++ tests/integration/test_s3_imds/test_simple.py
@@ -3,8 +3,8 @@ from helpers.cluster import ClickHouseCluster
 from helpers.mock_servers import start_mock_servers
 import os
 
-EC2_METADATA_SERVER_HOSTNAME = "resolver"
-EC2_METADATA_SERVER_PORT = 8080
+METADATA_SERVER_HOSTNAME = "resolver"
+METADATA_SERVER_PORT = 8080
 
 cluster = ClickHouseCluster(__file__)
 
@@ -13,21 +13,21 @@ node = cluster.add_instance(
     with_minio=True,
     main_configs=["configs/use_environment_credentials.xml"],
     env_variables={
-        "AWS_EC2_METADATA_SERVICE_ENDPOINT": f"{EC2_METADATA_SERVER_HOSTNAME}:{EC2_METADATA_SERVER_PORT}",
+        "AWS_EC2_METADATA_SERVICE_ENDPOINT": f"{METADATA_SERVER_HOSTNAME}:{METADATA_SERVER_PORT}",
     },
 )
 
 
-def start_ec2_metadata_server():
-    script_dir = os.path.join(os.path.dirname(__file__), "ec2_metadata_server")
+def start_metadata_server():
+    script_dir = os.path.join(os.path.dirname(__file__), "metadata_servers")
     start_mock_servers(
         cluster,
         script_dir,
         [
             (
-                "request_response_server.py",
-                EC2_METADATA_SERVER_HOSTNAME,
-                EC2_METADATA_SERVER_PORT,
+                "simple_server.py",
+                METADATA_SERVER_HOSTNAME,
+                METADATA_SERVER_PORT,
             )
         ],
     )
@@ -37,13 +37,13 @@ def start_ec2_metadata_server():
 def start_cluster():
     try:
         cluster.start()
-        start_ec2_metadata_server()
+        start_metadata_server()
         yield
     finally:
         cluster.shutdown()
 
 
-def test_credentials_from_ec2_metadata():
+def test_credentials_from_metadata():
     node.query(
         f"INSERT INTO FUNCTION s3('http://{cluster.minio_host}:{cluster.minio_port}/{cluster.minio_bucket}/test1.jsonl') SELECT * FROM numbers(100)"
     )
@@ -56,11 +56,15 @@ def test_credentials_from_ec2_metadata():
     )
 
     expected_logs = [
+        "Calling EC2MetadataService to get token failed, falling back to less secure way",
         "Getting default credentials for ec2 instance from resolver:8080",
         "Calling EC2MetadataService resource, /latest/meta-data/iam/security-credentials returned credential string myrole",
         "Calling EC2MetadataService resource /latest/meta-data/iam/security-credentials/myrole",
         "Successfully pulled credentials from EC2MetadataService with access key",
     ]
 
+    node.query("SYSTEM FLUSH LOGS")
     for expected_msg in expected_logs:
-        node.contains_in_log("AWSEC2InstanceProfileConfigLoader: " + expected_msg)
+        assert node.contains_in_log(
+            "AWSEC2InstanceProfileConfigLoader: " + expected_msg
+        )
