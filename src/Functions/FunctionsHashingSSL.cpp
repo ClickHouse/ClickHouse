74a6ed86d9b46bc50d58686500e3a173e9528a86
Nikolay Degterinsky <evillique@gmail.com>
2023-01-03 02:21:26 +0000

    Fix MSan build

---
+++ src/Functions/FunctionsHashingSSL.cpp
@@ -0,0 +1,25 @@
+#include "config.h"
+
+#if USE_SSL
+
+#include "FunctionsHashing.h"
+#include <Functions/FunctionFactory.h>
+
+
+namespace DB
+{
+
+REGISTER_FUNCTION(HashingSSL)
+{
+    factory.registerFunction<FunctionMD4>();
+    factory.registerFunction<FunctionHalfMD5>();
+    factory.registerFunction<FunctionMD5>();
+    factory.registerFunction<FunctionSHA1>();
+    factory.registerFunction<FunctionSHA224>();
+    factory.registerFunction<FunctionSHA256>();
+    factory.registerFunction<FunctionSHA384>();
+    factory.registerFunction<FunctionSHA512>();
+}
+}
+
+#endif
