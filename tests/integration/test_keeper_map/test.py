43bc4d637453f8594d680f703c519ec93c787233
Antonio Andelic <antonio2368@users.noreply.github.com>
2023-01-10 14:16:28 +0100

    Change table name

--- tests/integration/test_keeper_map/test.py
+++ tests/integration/test_keeper_map/test.py
@@ -154,35 +154,35 @@ def test_keeper_map_without_zk(started_cluster):
             assert "Coordination::Exception" in error
 
     assert_keeper_exception_after_partition(
-        "CREATE TABLE test_keeper_map (key UInt64, value UInt64) ENGINE = KeeperMap('/test1') PRIMARY KEY(key);"
+        "CREATE TABLE test_keeper_map_without_zk (key UInt64, value UInt64) ENGINE = KeeperMap('/test_without_zk') PRIMARY KEY(key);"
     )
 
     node.query(
-        "CREATE TABLE test_keeper_map (key UInt64, value UInt64) ENGINE = KeeperMap('/test1') PRIMARY KEY(key);"
+        "CREATE TABLE test_keeper_map_without_zk (key UInt64, value UInt64) ENGINE = KeeperMap('/test_without_zk') PRIMARY KEY(key);"
     )
 
     assert_keeper_exception_after_partition(
-        "INSERT INTO test_keeper_map VALUES (1, 11)"
+        "INSERT INTO test_keeper_map_without_zk VALUES (1, 11)"
     )
-    node.query("INSERT INTO test_keeper_map VALUES (1, 11)")
+    node.query("INSERT INTO test_keeper_map_without_zk VALUES (1, 11)")
 
-    assert_keeper_exception_after_partition("SELECT * FROM test_keeper_map")
-    node.query("SELECT * FROM test_keeper_map")
+    assert_keeper_exception_after_partition("SELECT * FROM test_keeper_map_without_zk")
+    node.query("SELECT * FROM test_keeper_map_without_zk")
 
     with PartitionManager() as pm:
         pm.drop_instance_zk_connections(node)
         node.restart_clickhouse(60)
-        error = node.query_and_get_error("SELECT * FROM test_keeper_map")
+        error = node.query_and_get_error("SELECT * FROM test_keeper_map_without_zk")
         assert "Failed to activate table because of connection issues" in error
 
-    node.query("SELECT * FROM test_keeper_map")
+    node.query("SELECT * FROM test_keeper_map_without_zk")
 
     client = get_genuine_zk()
-    remove_children(client, "/test_keeper_map/test1")
+    remove_children(client, "/test_keeper_map/test_without_zk")
     node.restart_clickhouse(60)
-    error = node.query_and_get_error("SELECT * FROM test_keeper_map")
+    error = node.query_and_get_error("SELECT * FROM test_keeper_map_without_zk")
     assert "Failed to activate table because of invalid metadata in ZooKeeper" in error
 
-    node.query("DETACH TABLE test_keeper_map")
+    node.query("DETACH TABLE test_keeper_map_without_zk")
 
     client.stop()
