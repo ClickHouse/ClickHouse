3996a5d9561eb60d9e2ee0b6d80a3822caa6b06f
Robert Schulze <robert@clickhouse.com>
2023-01-11 15:18:14 +0000

    Cosmetics

--- tests/queries/0_stateless/02494_query_result_cache_exception_handling.sql
+++ tests/queries/0_stateless/02494_query_result_cache_exception_handling.sql
@@ -3,7 +3,7 @@
 SYSTEM DROP QUERY RESULT CACHE;
 
 -- If an exception is thrown during query execution, no entry must be created in the query result cache
-SELECT throwIf(1) SETTINGS enable_experimental_query_result_cache = true; -- { serverError 395 }
+SELECT throwIf(1) SETTINGS enable_experimental_query_result_cache = true; -- { serverError FUNCTION_THROW_IF_VALUE_IS_NON_ZERO }
 SELECT COUNT(*) FROM system.query_result_cache;
 
 SYSTEM DROP QUERY RESULT CACHE;
