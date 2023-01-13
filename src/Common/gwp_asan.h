c87ec4cc01cc4a6741b91ddf154619d4e72ab524
Han Fei <hanfei19910905@gmail.com>
2023-01-13 15:22:14 +0100

    enable gwp_asan by option

--- src/Common/gwp_asan.h
+++ src/Common/gwp_asan.h
@@ -1,20 +1,28 @@
 #pragma once
 
+#include "config.h"
+
 #include <base/defines.h>
 
+#if USE_GWP_ASAN
 #include <gwp_asan/guarded_pool_allocator.h>
 #include <gwp_asan/optional/options_parser.h>
+#endif
 
 namespace Memory
 {
 
+#if USE_GWP_ASAN
 static gwp_asan::GuardedPoolAllocator GuardedAlloc;
+#endif
 
 inline ALWAYS_INLINE void initGWPAsan()
 {
+#if USE_GWP_ASAN
     gwp_asan::options::initOptions();
     gwp_asan::options::Options &opts = gwp_asan::options::getOptions();
     GuardedAlloc.init(opts);
+#endif
 }
 
 }
