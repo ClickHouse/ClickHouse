164b3b5f48f3d39b4e6894c980f34ecf70f515ae
Antonio Andelic <antonio@clickhouse.com>
2023-01-13 08:41:44 +0000

    Fix test

--- tests/queries/0_stateless/02526_kv_engine_different_filter_type.sql
+++ tests/queries/0_stateless/02526_kv_engine_different_filter_type.sql
@@ -1,9 +1,9 @@
--- Tags: use-rocksdb
+-- Tags: no-ordinary-database, use-rocksdb
 
 DROP TABLE IF EXISTS 02526_keeper_map;
 DROP TABLE IF EXISTS 02526_rocksdb;
 
-CREATE TABLE 02526_keeper_map (`key` String, `value` UInt32) ENGINE = KeeperMap('/02526_kv_filter_types') PRIMARY KEY key;
+CREATE TABLE 02526_keeper_map (`key` String, `value` UInt32) ENGINE = KeeperMap('/' || currentDatabase() || '/02526_kv_filter_types') PRIMARY KEY key;
 INSERT INTO 02526_keeper_map SELECT * FROM generateRandom('`key` String, `value` UInt32') LIMIT 100;
 SELECT * FROM 02526_keeper_map WHERE key in (SELECT number * 5 FROM numbers(1000)) FORMAT Null;
 
