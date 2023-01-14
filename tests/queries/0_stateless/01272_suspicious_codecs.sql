a5e14a85ab1b07d017bb05e23f0840ad2701f174
Robert Schulze <robert@clickhouse.com>
2023-01-14 17:29:10 +0000

    Fix 01272_suspicious_codecs

--- tests/queries/0_stateless/01272_suspicious_codecs.sql
+++ tests/queries/0_stateless/01272_suspicious_codecs.sql
@@ -11,7 +11,7 @@ CREATE TABLE codecs
     c Float32 CODEC(Gorilla),
     d UInt8 CODEC(Delta, LZ4),
     e Float64 CODEC(Gorilla, ZSTD),
-    f UInt32 CODEC(Delta, Delta, Gorilla),
+    f Float64 CODEC(Delta, Delta, Gorilla),
     g DateTime CODEC(DoubleDelta),
     h DateTime64 CODEC(DoubleDelta, LZ4),
     i String CODEC(NONE)
