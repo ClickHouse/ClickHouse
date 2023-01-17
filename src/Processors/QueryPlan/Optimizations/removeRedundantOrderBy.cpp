401ba38d75fa8f536f9ac1a4549d0d5be10b48ad
Igor Nikonov <igor@clickhouse.com>
2023-01-17 13:39:18 +0000

    Fix: update input/output stream properties
    After removing sorting step we need to update sorting properties of
    input/ouput streams

--- src/Processors/QueryPlan/Optimizations/removeRedundantOrderBy.cpp
+++ src/Processors/QueryPlan/Optimizations/removeRedundantOrderBy.cpp
@@ -149,7 +149,7 @@ public:
             || typeid_cast<SortingStep *>(current_step) /// (3) ORDER BY will change order of previous sorting
             || typeid_cast<AggregatingStep *>(current_step)) /// (4) aggregation change order
         {
-            logStep("steps_affect_order/push", current_node);
+            logStep("nodes_affect_order/push", current_node);
             nodes_affect_order.push_back(current_node);
         }
 
@@ -159,10 +159,10 @@ public:
     void visitBottomUpImpl(QueryPlan::Node * current_node, QueryPlan::Node *)
     {
         /// we come here when all children of current_node are visited,
-        /// so it's a node which affect order, remove it from the corresponding stack
+        /// so, if it's a node which affect order, remove it from the corresponding stack
         if (!nodes_affect_order.empty() && nodes_affect_order.back() == current_node)
         {
-            logStep("node_affect_order/pop", current_node);
+            logStep("nodes_affect_order/pop", current_node);
             nodes_affect_order.pop_back();
         }
     }
@@ -173,8 +173,36 @@ private:
         if (!canRemoveCurrentSorting())
             return false;
 
-        /// need to remove sorting
+        /// remove sorting
         parent_node->children.front() = sorting_node->children.front();
+
+        /// sorting removed, so need to update sorting traits for upstream steps
+        const DataStream * input_stream = &parent_node->children.front()->step->getOutputStream();
+        chassert(parent_node == (stack.rbegin() + 1)->node); /// skip element on top of stack since it's sorting which was just removed
+        for (StackWithParent::const_reverse_iterator it = stack.rbegin() + 1; it != stack.rend(); ++it)
+        {
+            const QueryPlan::Node * node = it->node;
+            logStep("update sorting traits", node);
+
+            auto * step = node->step.get();
+            auto * trans = dynamic_cast<ITransformingStep *>(step);
+            if (!trans)
+            {
+                logStep("stop update sorting traits: node is not transforming step", node);
+                break;
+            }
+
+            trans->updateInputStream(*input_stream);
+            input_stream = &trans->getOutputStream();
+
+            /// update sorting properties though stack until reach node which affects order (inclusive)
+            if (node == nodes_affect_order.back())
+            {
+                logStep("stop update sorting traits: reached node which affect order", node);
+                break;
+            }
+        }
+
         return true;
     }
 
@@ -255,7 +283,7 @@ private:
             }
             else
             {
-                const auto * trans = typeid_cast<const ITransformingStep *>(step);
+                const auto * trans = dynamic_cast<const ITransformingStep *>(step);
                 if (!trans)
                     break;
 
