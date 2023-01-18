19ab388693f18eedde865e016d9847b417c37c48
Igor Nikonov <954088+devcrafter@users.noreply.github.com>
2023-01-18 20:12:34 +0100

    Revert "Revert "Remove redundant sorting""

--- src/Processors/QueryPlan/Optimizations/QueryPlanOptimizationSettings.cpp
+++ src/Processors/QueryPlan/Optimizations/QueryPlanOptimizationSettings.cpp
@@ -14,6 +14,7 @@ QueryPlanOptimizationSettings QueryPlanOptimizationSettings::fromSettings(const 
     settings.distinct_in_order = from.optimize_distinct_in_order;
     settings.read_in_order = from.optimize_read_in_order && from.query_plan_read_in_order;
     settings.aggregation_in_order = from.optimize_aggregation_in_order && from.query_plan_aggregation_in_order;
+    settings.remove_redundant_sorting = from.query_plan_remove_redundant_sorting;
     return settings;
 }
 
