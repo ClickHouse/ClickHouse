9d32e6da211f0f0d27b9f6006377a9603d1dd09c
Igor Nikonov <igor@clickhouse.com>
2023-01-19 01:07:31 +0000

    Shell test instead of sql one

--- tests/queries/0_stateless/02496_remove_redundant_sorting.sh
+++ tests/queries/0_stateless/02496_remove_redundant_sorting.sh
@@ -24,7 +24,7 @@ FROM
 ORDER BY number ASC"
 $CLICKHOUSE_CLIENT -nq "$DISABLE_OPTIMIZATION;EXPLAIN header=1 $query"
 
-function explain_and_execute {
+function run_query {
     echo "-- query"
     echo "$1"
     echo "-- explain"
@@ -35,7 +35,7 @@ function explain_and_execute {
 
 echo "-- Enabled query_plan_remove_redundant_sorting"
 echo "-- ORDER BY removes ORDER BY clauses in subqueries"
-explain_and_execute "$query"
+run_query "$query"
 
 echo "-- ORDER BY cannot remove ORDER BY in subquery WITH FILL"
 query="SELECT *
@@ -51,7 +51,7 @@ FROM
     ORDER BY number ASC WITH FILL STEP 1
 )
 ORDER BY number ASC"
-explain_and_execute "$query"
+run_query "$query"
 
 echo "-- ORDER BY cannot remove ORDER BY in subquery with LIMIT BY"
 query="SELECT *
@@ -68,7 +68,7 @@ FROM
     LIMIT 1 BY number
 )
 ORDER BY number ASC"
-explain_and_execute "$query"
+run_query "$query"
 
 echo "-- CROSS JOIN with subqueries, nor ORDER BY nor GROUP BY in main query -> only ORDER BY clauses in most inner subqueries will be removed"
 query="SELECT *
@@ -93,157 +93,129 @@ FROM
     )
     ORDER BY number DESC
 ) AS t2"
-explain_and_execute "$query"
+run_query "$query"
 
-# SELECT '-- CROSS JOIN with subqueries, ORDER BY in main query -> all ORDER BY clauses will be removed in subqueries';
-# EXPLAIN header=1
-# SELECT *
-# FROM
-# (
-#     SELECT number
-#     FROM
-#     (
-#         SELECT number
-#         FROM numbers(3)
-#         ORDER BY number DESC
-#     )
-#     ORDER BY number ASC
-# ) AS t1,
-# (
-#     SELECT number
-#     FROM
-#     (
-#         SELECT number
-#         FROM numbers(3)
-#         ORDER BY number ASC
-#     )
-#     ORDER BY number DESC
-# ) AS t2
-# ORDER BY t1.number ASC;
+echo "-- CROSS JOIN with subqueries, ORDER BY in main query -> all ORDER BY clauses will be removed in subqueries"
+query="SELECT *
+FROM
+(
+    SELECT number
+    FROM
+    (
+        SELECT number
+        FROM numbers(3)
+        ORDER BY number DESC
+    )
+    ORDER BY number ASC
+) AS t1,
+(
+    SELECT number
+    FROM
+    (
+        SELECT number
+        FROM numbers(3)
+        ORDER BY number ASC
+    )
+    ORDER BY number DESC
+) AS t2
+ORDER BY t1.number ASC"
+run_query "$query"
 
-# SELECT '-- GROUP BY with aggregation function which does NOT depend on order -> eliminate ORDER BY(s) in _all_ subqueries';
-# EXPLAIN header=1
-# SELECT sum(number)
-# FROM
-# (
-#     SELECT *
-#     FROM
-#     (
-#         SELECT *
-#         FROM numbers(3)
-#         ORDER BY number ASC
-#     )
-#     ORDER BY number DESC
-# )
-# GROUP BY number;
+echo "-- GROUP BY with aggregation function which does NOT depend on order -> eliminate ORDER BY(s) in _all_ subqueries"
+query="SELECT sum(number)
+FROM
+(
+    SELECT *
+    FROM
+    (
+        SELECT *
+        FROM numbers(3)
+        ORDER BY number ASC
+    )
+    ORDER BY number DESC
+)
+GROUP BY number"
+run_query "$query"
 
-# SELECT '-- GROUP BY with aggregation function which depends on order -> keep ORDER BY in first subquery, and eliminate in second subquery';
-# EXPLAIN header=1
-# SELECT any(number)
-# FROM
-# (
-#     SELECT *
-#     FROM
-#     (
-#         SELECT *
-#         FROM numbers(3)
-#         ORDER BY number ASC
-#     )
-#     ORDER BY number DESC
-# )
-# GROUP BY number;
+echo "-- GROUP BY with aggregation function which depends on order -> keep ORDER BY in first subquery, and eliminate in second subquery"
+query="SELECT any(number)
+FROM
+(
+    SELECT *
+    FROM
+    (
+        SELECT *
+        FROM numbers(3)
+        ORDER BY number ASC
+    )
+    ORDER BY number DESC
+)
+GROUP BY number"
+run_query "$query"
 
-# SELECT '-- query with aggregation function but w/o GROUP BY -> remove sorting';
-# EXPLAIN
-# SELECT sum(number)
-# FROM
-# (
-#     SELECT *
-#     FROM numbers(10)
-#     ORDER BY number DESC
-# );
+echo "-- query with aggregation function but w/o GROUP BY -> remove sorting"
+query="SELECT sum(number)
+FROM
+(
+    SELECT *
+    FROM numbers(10)
+    ORDER BY number DESC
+)"
+run_query "$query"
 
-# SELECT '-- check that optimization is applied recursively to subqueries as well';
-# SELECT '-- GROUP BY with aggregation function which does NOT depend on order -> eliminate ORDER BY in most inner subquery here';
-# EXPLAIN header=1
-# SELECT a
-# FROM
-# (
-#     SELECT sum(number) AS a
-#     FROM
-#     (
-#         SELECT *
-#         FROM numbers(3)
-#         ORDER BY number ASC
-#     )
-#     GROUP BY number
-# )
-# ORDER BY a ASC;
+echo "-- check that optimization is applied recursively to subqueries as well"
+echo "-- GROUP BY with aggregation function which does NOT depend on order -> eliminate ORDER BY in most inner subquery here"
+query="SELECT a
+FROM
+(
+    SELECT sum(number) AS a
+    FROM
+    (
+        SELECT *
+        FROM numbers(3)
+        ORDER BY number ASC
+    )
+    GROUP BY number
+)
+ORDER BY a ASC"
+run_query "$query"
 
-# SELECT '-- GROUP BY with aggregation function which depends on order -> ORDER BY in subquery is kept due to the aggregation function';
-# EXPLAIN header=1
-# SELECT a
-# FROM
-# (
-#     SELECT any(number) AS a
-#     FROM
-#     (
-#         SELECT *
-#         FROM numbers(3)
-#         ORDER BY number ASC
-#     )
-#     GROUP BY number
-# )
-# ORDER BY a ASC;
+echo "-- GROUP BY with aggregation function which depends on order -> ORDER BY in subquery is kept due to the aggregation function"
+query="SELECT a
+FROM
+(
+    SELECT any(number) AS a
+    FROM
+    (
+        SELECT *
+        FROM numbers(3)
+        ORDER BY number ASC
+    )
+    GROUP BY number
+)
+ORDER BY a ASC"
+run_query "$query"
 
-# SELECT '-- Check that optimization works for subqueries as well, - main query have neither ORDER BY nor GROUP BY';
-# EXPLAIN header=1
-# SELECT a
-# FROM
-# (
-#     SELECT any(number) AS a
-#     FROM
-#     (
-#         SELECT *
-#         FROM
-#         (
-#             SELECT *
-#             FROM numbers(3)
-#             ORDER BY number DESC
-#         )
-#         ORDER BY number ASC
-#     )
-#     GROUP BY number
-# )
-# WHERE a > 0;
-
-# SELECT '-- disable common optimization to avoid functions to be lifted up (liftUpFunctions optimization), needed for testing with stateful function';
-# SET query_plan_enable_optimizations = 0;
-
-# SELECT '-- neighbor() as stateful function prevents removing inner ORDER BY since its result depends on order';
-# EXPLAIN
-# SELECT
-#     number,
-#     neighbor(number, 2)
-# FROM
-# (
-#     SELECT *
-#     FROM numbers(10)
-#     ORDER BY number DESC
-# )
-# ORDER BY number ASC;
-
-# SELECT '-- non-stateful function does _not_ prevent removing inner ORDER BY';
-# EXPLAIN
-# SELECT
-#     number,
-#     plus(number, 2)
-# FROM
-# (
-#     SELECT *
-#     FROM numbers(10)
-#     ORDER BY number DESC
-# )
+echo "-- Check that optimization works for subqueries as well, - main query have neither ORDER BY nor GROUP BY"
+query="SELECT a
+FROM
+(
+    SELECT any(number) AS a
+    FROM
+    (
+        SELECT *
+        FROM
+        (
+            SELECT *
+            FROM numbers(3)
+            ORDER BY number DESC
+        )
+        ORDER BY number ASC
+    )
+    GROUP BY number
+)
+WHERE a > 0"
+run_query "$query"
 
 echo "-- GROUP BY in most inner query makes execution parallelized, and removing inner sorting steps will keep it that way. But need to correctly update data streams sorting properties after removing sorting steps"
 query="SELECT *
@@ -260,4 +232,31 @@ FROM
     ORDER BY number ASC
 )
 ORDER BY number ASC"
-explain_and_execute "$query"
+run_query "$query"
+
+echo "-- disable common optimization to avoid functions to be lifted up (liftUpFunctions optimization), needed for testing with stateful function"
+ENABLE_OPTIMIZATION="SET query_plan_enable_optimizations=0;$ENABLE_OPTIMIZATION"
+echo "-- neighbor() as stateful function prevents removing inner ORDER BY since its result depends on order"
+query="SELECT
+    number,
+    neighbor(number, 2)
+FROM
+(
+    SELECT *
+    FROM numbers(10)
+    ORDER BY number DESC
+)
+ORDER BY number ASC"
+run_query "$query"
+
+echo "-- non-stateful function does _not_ prevent removing inner ORDER BY"
+query="SELECT
+    number,
+    plus(number, 2)
+FROM
+(
+    SELECT *
+    FROM numbers(10)
+    ORDER BY number DESC
+)"
+run_query "$query"
