56efda1a368760ba8f82565b4ced4b58ff4312c3
Vladimir C <vdimir@clickhouse.com>
2023-01-19 13:33:23 +0100

    Update 02534_join_prewhere_bug_44062.sql

--- tests/queries/0_stateless/02534_join_prewhere_bug_44062.sql
+++ tests/queries/0_stateless/02534_join_prewhere_bug_44062.sql
@@ -1,6 +1,9 @@
 
-CREATE OR REPLACE TABLE test1 ( `col1` UInt64, `col2` Int8 ) ENGINE = MergeTree ORDER BY col1;
-CREATE OR REPLACE TABLE test2 ( `col1` UInt64, `col3` Int16 ) ENGINE = MergeTree ORDER BY col1;
+DROP TABLE IF EXISTS test1;
+DROP TABLE IF EXISTS test2;
+
+CREATE TABLE test1 ( `col1` UInt64, `col2` Int8 ) ENGINE = MergeTree ORDER BY col1;
+CREATE TABLE test2 ( `col1` UInt64, `col3` Int16 ) ENGINE = MergeTree ORDER BY col1;
 
 INSERT INTO test1 VALUES (123, 123), (12321, -30), (321, -32);
 INSERT INTO test2 VALUES (123, 5600), (321, 5601);
