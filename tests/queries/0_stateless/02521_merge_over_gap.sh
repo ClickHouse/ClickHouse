d4f914cf5358f83856faf9e1e9174ae781925334
Sema Checherinda <Sema.Checherinda@clickhouse.com>
2023-01-20 12:10:31 +0100

    add a comment, add a check, fix test

--- tests/queries/0_stateless/02521_merge_over_gap.sh
+++ tests/queries/0_stateless/02521_merge_over_gap.sh
@@ -33,7 +33,7 @@ $CLICKHOUSE_CLIENT --query "SELECT name, rows, active FROM system.parts WHERE ta
 $CLICKHOUSE_CLIENT --query "SYSTEM START MERGES table_with_gap;"
 
 $CLICKHOUSE_CLIENT --query "OPTIMIZE TABLE table_with_gap FINAL SETTINGS optimize_throw_if_noop=1;" 2>&1 |\
-  grep "There is a part in a gap between two active parts" |\
+  grep "There is an outdated part in a gap between two active parts" |\
   grep -o "CANNOT_ASSIGN_OPTIMIZE" | uniq || echo "NO ERROR"
 
 $CLICKHOUSE_CLIENT --query "OPTIMIZE TABLE table_with_gap;"
