f47e5e948aebd4e5cf760b02c4d67347aeb8387e
Azat Khuzhin <a3at.mail@gmail.com>
2023-01-21 22:52:03 +0100

    Fix possible in-use table after DETACH
    Right now in case of DETACH/ATTACH there can be a window when after the
    table had been DETACH'ed someone will still use it, the common example
    here is MVs handling.
    
    It happens because TableExclusiveLockHolder does not guards the
    shard_ptr of the IStorage, and so if someone holds it, then it can use
    it. So if ATTACH will be done for this table then, you can have multiple
    instances of it.
    
    It is not possible for DROP, because before using a table, you should
    lock it and after table had been DROP'ed you cannot lock it anymore.
    
    So let's do the same for DETACH.
    
    Signed-off-by: Azat Khuzhin <a.khuzhin@semrush.com>

--- src/Databases/DatabaseOnDisk.cpp
+++ src/Databases/DatabaseOnDisk.cpp
@@ -377,14 +377,14 @@ void DatabaseOnDisk::renameTable(
     if (dictionary && table && !table->isDictionary())
         throw Exception("Use RENAME/EXCHANGE TABLE (instead of RENAME/EXCHANGE DICTIONARY) for tables", ErrorCodes::INCORRECT_QUERY);
 
+    table_lock = table->lockExclusively(
+        local_context->getCurrentQueryId(), local_context->getSettingsRef().lock_acquire_timeout);
+
     detachTable(local_context, table_name);
 
     UUID prev_uuid = UUIDHelpers::Nil;
     try
     {
-        table_lock = table->lockExclusively(
-            local_context->getCurrentQueryId(), local_context->getSettingsRef().lock_acquire_timeout);
-
         table_metadata_path = getObjectMetadataPath(table_name);
         attach_query = parseQueryFromMetadata(log, local_context, table_metadata_path);
         auto & create = attach_query->as<ASTCreateQuery &>();
