d15f3ffdcaf43db30d0bc1bd859fefe2e7b1a09d
vdimir <vdimir@clickhouse.com>
2023-01-24 11:23:17 +0000

    wip

--- tests/queries/0_stateless/02378_part_log_profile_events.sql
+++ tests/queries/0_stateless/02378_part_log_profile_events.sql
@@ -7,13 +7,14 @@ SET max_block_size = 64, max_insert_block_size = 64, min_insert_block_size_rows 
 INSERT INTO test SELECT number AS key, sipHash64(number) AS val FROM numbers(512);
 
 SYSTEM FLUSH LOGS;
+
 SELECT
-    count(DISTINCT query_id) == 1
-    AND count() >= 512 / 64 -- 512 rows inserted, 64 rows per block
-    AND SUM(ProfileEvents['MergeTreeDataWriterRows']) == 512
-    AND SUM(ProfileEvents['MergeTreeDataWriterUncompressedBytes']) > 1024
-    AND SUM(ProfileEvents['MergeTreeDataWriterCompressedBytes']) > 1024
-    AND SUM(ProfileEvents['MergeTreeDataWriterBlocks']) >= 8
+    if(count(DISTINCT query_id) == 1, 'Ok', 'Error: ' || toString(count(DISTINCT query_id))),
+    if(count() == 512 / 64, 'Ok', 'Error: ' || toString(count())), -- 512 rows inserted, 64 rows per block
+    if(SUM(ProfileEvents['MergeTreeDataWriterRows']) == 512, 'Ok', 'Error: ' || toString(SUM(ProfileEvents['MergeTreeDataWriterRows']))),
+    if(SUM(ProfileEvents['MergeTreeDataWriterUncompressedBytes']) >= 1024, 'Ok', 'Error: ' || toString(SUM(ProfileEvents['MergeTreeDataWriterUncompressedBytes']))),
+    if(SUM(ProfileEvents['MergeTreeDataWriterCompressedBytes']) >= 1024, 'Ok', 'Error: ' || toString(SUM(ProfileEvents['MergeTreeDataWriterCompressedBytes']))),
+    if(SUM(ProfileEvents['MergeTreeDataWriterBlocks']) >= 8, 'Ok', 'Error: ' || toString(SUM(ProfileEvents['MergeTreeDataWriterBlocks'])))
 FROM system.part_log
 WHERE event_time > now() - INTERVAL 10 MINUTE
     AND database == currentDatabase() AND table == 'test'
@@ -24,7 +25,8 @@ OPTIMIZE TABLE test FINAL;
 
 SYSTEM FLUSH LOGS;
 SELECT
-    count() >= 2 AND SUM(ProfileEvents['MergedRows']) >= 512
+    if(count() > 2, 'Ok', 'Error: ' || toString(count())),
+    if(SUM(ProfileEvents['MergedRows']) >= 512, 'Ok', 'Error: ' || toString(SUM(ProfileEvents['MergedRows'])))
 FROM system.part_log
 WHERE event_time > now() - INTERVAL 10 MINUTE
     AND database == currentDatabase() AND table == 'test'
@@ -34,10 +36,11 @@ WHERE event_time > now() - INTERVAL 10 MINUTE
 ALTER TABLE test UPDATE val = 0 WHERE key % 2 == 0 SETTINGS mutations_sync = 2;
 
 SYSTEM FLUSH LOGS;
+
 SELECT
-    count() == 2
-    AND SUM(ProfileEvents['SelectedRows']) == 512
-    AND SUM(ProfileEvents['FileOpen']) > 2
+    if(count() == 2, 'Ok', 'Error: ' || toString(count())),
+    if(SUM(ProfileEvents['SelectedRows']) == 512, 'Ok', 'Error: ' || toString(SUM(ProfileEvents['SelectedRows']))),
+    if(SUM(ProfileEvents['FileOpen']) > 1, 'Ok', 'Error: ' || toString(SUM(ProfileEvents['FileOpen'])))
 FROM system.part_log
 WHERE event_time > now() - INTERVAL 10 MINUTE
     AND database == currentDatabase() AND table == 'test'
