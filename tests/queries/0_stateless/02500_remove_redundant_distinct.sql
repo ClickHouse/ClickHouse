b34c4dd0069601fd9707831ffc42a78f985c1f9d
Igor Nikonov <igor@clickhouse.com>
2023-01-25 17:09:26 +0000

    Update

--- tests/queries/0_stateless/02500_remove_redundant_distinct.sql
+++ tests/queries/0_stateless/02500_remove_redundant_distinct.sql
@@ -1,9 +1,9 @@
-set allow_experimental_analyzer=1;
+-- set allow_experimental_analyzer=1;
 set optimize_duplicate_order_by_and_distinct = 0;
 
 set query_plan_remove_redundant_distinct=0;
 
-EXPLAIN header=1
+EXPLAIN --header=1
 SELECT DISTINCT *
 FROM
 (
@@ -16,7 +16,7 @@ FROM
 );
 
 set query_plan_remove_redundant_distinct=1;
-EXPLAIN header=1
+EXPLAIN --header=1
 SELECT DISTINCT *
 FROM
 (
