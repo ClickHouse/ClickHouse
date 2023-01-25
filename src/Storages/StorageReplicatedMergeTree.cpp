218f3e247ebf157936f5e0ea943af4ffb6240904
Alexander Tokmakov <tavplubix@clickhouse.com>
2023-01-26 00:28:46 +0300

    Update src/Storages/StorageReplicatedMergeTree.cpp

--- src/Storages/StorageReplicatedMergeTree.cpp
+++ src/Storages/StorageReplicatedMergeTree.cpp
@@ -8114,7 +8114,7 @@ StorageReplicatedMergeTree::unlockSharedData(const IMergeTreeDataPart & part, co
     auto shared_id = getTableSharedID();
     if (shared_id == toString(UUIDHelpers::Nil))
     {
-        LOG_TRACE(log, "Part {} blobs can be rem)oved, because table {} comletely dropped", part.name, getStorageID().getNameForLogs());
+        LOG_TRACE(log, "Part {} blobs can be removed, because table {} comletely dropped", part.name, getStorageID().getNameForLogs());
         return std::make_pair(true, NameSet{});
     }
 
