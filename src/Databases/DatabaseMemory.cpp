e323419db98f25adf562bbae455aa7f27db9224e
Roman Vasin <r.vasin@arenadata.io>
2023-01-26 09:47:36 +0000

    Fix removing data on disk for table engines like Log for DatabaseMemory

--- src/Databases/DatabaseMemory.cpp
+++ src/Databases/DatabaseMemory.cpp
@@ -70,8 +70,7 @@ void DatabaseMemory::dropTable(
 
         if (table->storesDataOnDisk())
         {
-            // assert(getDatabaseName() != DatabaseCatalog::TEMPORARY_DATABASE);
-            fs::path table_data_dir{getTableDataPath(table_name)};
+            fs::path table_data_dir{fs::path{getContext()->getPath()} / getTableDataPath(table_name)};
             if (fs::exists(table_data_dir))
                 fs::remove_all(table_data_dir);
         }
@@ -79,7 +78,6 @@ void DatabaseMemory::dropTable(
     catch (...)
     {
         std::lock_guard lock{mutex};
-        // assert(database_name != DatabaseCatalog::TEMPORARY_DATABASE);
         attachTableUnlocked(table_name, table);
         throw;
     }
