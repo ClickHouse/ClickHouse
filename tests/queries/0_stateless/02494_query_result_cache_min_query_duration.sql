7d97fab2dcf1a5640f89065111e904c52f4e1fda
Robert Schulze <robert@clickhouse.com>
2023-01-26 19:22:24 +0000

    Untangle query result cache settings
    This PR modularizes the configuration options of the query result cache
    (QRC).
    
    1. Add a feature toggle "allow_experimental_query_result_cache = false"
       which controls whether the experimental QRC can be used at all. This
       configuration option will be removed once the QRC becomes
       non-experimental.
    
    2. Modularize the existing two configuration settings
       "enable_experimental_query_result_cache" and
       "enable_experimental_query_result_cache_passive_usage" into three
       orthogonal settings:
    
       - "use_query_result_cache = false" controls whether a query (or all
         queries of the session) utilize the query result cache
    
       - settings "use_query_result_cache_passive_usage = true" and
         "use_query_result_cache_active_usage = true" control the way a
         query (or all queries of the session) utilize the query result
         cache: "passive" use is reading, "active" use is writing.

--- tests/queries/0_stateless/02494_query_result_cache_min_query_duration.sql
+++ tests/queries/0_stateless/02494_query_result_cache_min_query_duration.sql
@@ -1,10 +1,12 @@
 -- Tags: no-parallel
 -- Tag no-parallel: Messes with internal cache
 
+SET allow_experimental_query_result_cache = true;
+
 SYSTEM DROP QUERY RESULT CACHE;
 
 -- This creates an entry in the query result cache ...
-SELECT 1 SETTINGS enable_experimental_query_result_cache = true;
+SELECT 1 SETTINGS use_query_result_cache = true;
 SELECT COUNT(*) FROM system.query_result_cache;
 
 SYSTEM DROP QUERY RESULT CACHE;
@@ -12,7 +14,7 @@ SYSTEM DROP QUERY RESULT CACHE;
 SELECT '---';
 
 -- ... but this does not because the query executes much faster than the specified minumum query duration for caching the result
-SELECT 1 SETTINGS enable_experimental_query_result_cache = true, query_result_cache_min_query_duration = 10000;
+SELECT 1 SETTINGS use_query_result_cache = true, query_result_cache_min_query_duration = 10000;
 SELECT COUNT(*) FROM system.query_result_cache;
 
 SYSTEM DROP QUERY RESULT CACHE;
