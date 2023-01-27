379850b6c457c6bb67e802b7877f34f82edadff4
freedomDR <1640145602@qq.com>
2023-01-27 01:10:40 +0000

    add system sync file cache

--- src/Interpreters/InterpreterSystemQuery.cpp
+++ src/Interpreters/InterpreterSystemQuery.cpp
@@ -56,6 +56,7 @@
 #include <Common/ThreadFuzzer.h>
 #include <csignal>
 #include <algorithm>
+#include <unistd.h>
 
 #include "config.h"
 
@@ -290,6 +291,13 @@ BlockIO InterpreterSystemQuery::execute()
             if (!out.str().empty())
                 LOG_DEBUG(log, "The command {} returned output: {}", command, out.str());
             res->wait();
+            break;
+        }
+        case Type::SYNC_FILE_CACHE:
+        {
+            // It's too heavy.
+            LOG_DEBUG(log, "will call syscall sync() function, It's too heavy.");
+            sync();
             break;
         }
         case Type::DROP_DNS_CACHE:
@@ -1131,6 +1139,11 @@ AccessRightsElements InterpreterSystemQuery::getRequiredAccessForDDLOnCluster() 
         case Type::UNFREEZE:
         {
             required_access.emplace_back(AccessType::SYSTEM_UNFREEZE);
+            break;
+        }
+        case Type::SYNC_FILE_CACHE:
+        {
+            required_access.emplace_back(AccessType::SYSTEM_SYNC_FILE_CACHE);
             break;
         }
         case Type::STOP_LISTEN_QUERIES:
