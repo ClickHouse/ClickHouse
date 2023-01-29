fa6dea8b6b08b5ec00e04a300923a84cb11560ae
taiyang-li <654010905@qq.com>
2023-01-29 19:50:30 +0800

    remove useless code

--- src/Analyzer/IQueryTreeNode.h
+++ src/Analyzer/IQueryTreeNode.h
@@ -72,9 +72,6 @@ class IQueryTreeNode : public TypePromotion<IQueryTreeNode>
 public:
     virtual ~IQueryTreeNode() = default;
 
-    IQueryTreeNode & operator=(const IQueryTreeNode &) = default;
-    IQueryTreeNode & operator=(IQueryTreeNode &&) = default;
-
     /// Get query tree node type
     virtual QueryTreeNodeType getNodeType() const = 0;
 
