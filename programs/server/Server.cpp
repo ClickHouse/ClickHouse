18ff23c5873c0cd7009039e204d27499b20dddd1
HarryLeeIBM <hleeatwork@outlook.com>
2023-01-30 20:38:50 +0000

    Fix build error in clang 14

--- programs/server/Server.cpp
+++ programs/server/Server.cpp
@@ -703,7 +703,7 @@ try
     {
         const String config_path = config().getString("config-file", "config.xml");
         const auto config_dir = std::filesystem::path{config_path}.replace_filename("openssl.conf");
-        setenv("OPENSSL_CONF", config_dir.string(), true);
+        setenv("OPENSSL_CONF", config_dir.string().c_str(), true);
     }
 #endif
 
