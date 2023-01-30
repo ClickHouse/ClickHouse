a728dd71ac02f3b4fdf3e39874dbe319c3a9bca1
taiyang-li <654010905@qq.com>
2023-01-30 15:10:43 +0800

    update comments

--- src/Interpreters/RewriteAggregateFunctionWithIfVisitor.h
+++ src/Interpreters/RewriteAggregateFunctionWithIfVisitor.h
@@ -10,10 +10,10 @@ namespace DB
 
 class ASTFunction;
 
-/// Rewrite 'sum(if())' to 'sumIf()'
-/// sum(if(cond, a, 0)) -> sumIf(a, cond)
-/// sum(if(cond, a, null)) -> sumIf(a, cond)
-/// avg(if(cond, a, null)) -> avgIf(a, cond)
+/// Rewrite '<aggregate-function>(if())' to '<aggregate-function>If[OrNull]()'
+/// sum(if(cond, a, 0)) -> sumIf[OrNull](a, cond)
+/// sum(if(cond, a, null)) -> sumIf[OrNull](a, cond)
+/// avg(if(cond, a, null)) -> avgIf[OrNull](a, cond)
 /// ...
 class RewriteAggregateFunctionWithIfMatcher
 {
