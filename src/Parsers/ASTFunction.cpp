6165625e278217f5497d4a06c74188b43e4ae1e1
Vitaly Baranov <vitlibar@yandex.ru>
2023-01-31 14:43:48 +0100

    Update src/Parsers/ASTFunction.cpp
    Co-authored-by: SmitaRKulkarni <64093672+SmitaRKulkarni@users.noreply.github.com>

--- src/Parsers/ASTFunction.cpp
+++ src/Parsers/ASTFunction.cpp
@@ -66,7 +66,7 @@ namespace
             /// Result constructed by default means no arguments will be hidden.
             size_t start = static_cast<size_t>(-1);
             size_t count = 0; /// Mostly it's either 0 or 1. There are only a few cases where `count` can be greater than 1 (e.g. see `encrypt`).
-                              /// In all known cases secret arguments are consequent.
+                              /// In all known cases secret arguments are consecutive
             bool are_named = false; /// Arguments like `password = 'password'` are considered as named arguments.
         };
 
