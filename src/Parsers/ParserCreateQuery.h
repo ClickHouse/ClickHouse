dd6626c32c7034209961b9c0e9e3dac511f4ace7
Yakov Olkhovskiy <99031427+yakov-olkhovskiy@users.noreply.github.com>
2023-02-01 19:59:12 -0500

    Update ParserCreateQuery.h

--- src/Parsers/ParserCreateQuery.h
+++ src/Parsers/ParserCreateQuery.h
@@ -243,7 +243,12 @@ bool IParserColumnDeclaration<NameParser>::parseImpl(Pos & pos, ASTPtr & node, E
             auto default_function = std::make_shared<ASTFunction>();
             default_function->name = "defaultValueOfTypeName";
             default_function->arguments = std::make_shared<ASTExpressionList>();
-            default_function->arguments->children.emplace_back(std::make_shared<ASTLiteral>(type->as<ASTFunction>()->formatWithSecretsHidden()));
+
+            WriteBufferFromOwnString buf;
+            IAST::FormatSettings settings{buf, true};
+            type->as<ASTFunction>()->format(settings);
+
+            default_function->arguments->children.emplace_back(std::make_shared<ASTLiteral>(buf.str()));
             default_expression = default_function;
         }
 
