bd57c01dbfa59285f613e06cc52b2720ba632e45
FFFFFFFHHHHHHH <916677625@qq.com>
2023-02-02 12:08:25 +0800

    add test

--- tests/queries/0_stateless/02515_aggregate_functions_statistics.sql
+++ tests/queries/0_stateless/02515_aggregate_functions_statistics.sql
@@ -8,49 +8,34 @@ SELECT corrMatrix(a_value) FROM (select a_value from fh limit 0);
 
 SELECT corrMatrix(a_value) FROM (select a_value from fh limit 1);
 
-SELECT corrMatrix(a_value, b_value) FROM (select a_value, b_value from fh limit 0);
-
-SELECT corrMatrix(a_value, b_value) FROM (select a_value, b_value from fh limit 1);
-
-SELECT corrMatrix(a_value, b_value, c_value) FROM (select a_value, b_value, c_value from fh limit 0);
-
-SELECT corrMatrix(a_value, b_value, c_value) FROM (select a_value, b_value, c_value from fh limit 1);
-
 SELECT corrMatrix(a_value, b_value, c_value, d_value) FROM (select a_value, b_value, c_value, d_value from fh limit 0);
 
 SELECT corrMatrix(a_value, b_value, c_value, d_value) FROM (select a_value, b_value, c_value, d_value from fh limit 1);
 
+SELECT arrayMap(x -> arrayMap(y -> round(y, 5), x), corrMatrix(a_value, b_value, c_value, d_value))  FROM fh;
+
+SELECT round(abs(corr(x1,x2) - corrMatrix(x1,x2)[1][2]), 5), round(abs(corr(x1,x1) - corrMatrix(x1,x2)[1][1]), 5), round(abs(corr(x2,x2) - corrMatrix(x1,x2)[2][2]), 5) from (select randNormal(100, 1) as x1, randNormal(100,5) as x2 from numbers(100000));
 
 SELECT covarSampMatrix(a_value) FROM (select a_value from fh limit 0);
 
 SELECT covarSampMatrix(a_value) FROM (select a_value from fh limit 1);
 
-SELECT covarSampMatrix(a_value, b_value) FROM (select a_value, b_value from fh limit 0);
-
-SELECT covarSampMatrix(a_value, b_value) FROM (select a_value, b_value from fh limit 1);
-
-SELECT covarSampMatrix(a_value, b_value, c_value) FROM (select a_value, b_value, c_value from fh limit 0);
-
-SELECT covarSampMatrix(a_value, b_value, c_value) FROM (select a_value, b_value, c_value from fh limit 1);
-
 SELECT covarSampMatrix(a_value, b_value, c_value, d_value) FROM (select a_value, b_value, c_value, d_value from fh limit 0);
 
 SELECT covarSampMatrix(a_value, b_value, c_value, d_value) FROM (select a_value, b_value, c_value, d_value from fh limit 1);
 
+SELECT arrayMap(x -> arrayMap(y -> round(y, 5), x), covarSampMatrix(a_value, b_value, c_value, d_value))  FROM fh;
+
+SELECT round(abs(covarSamp(x1,x2) - covarSampMatrix(x1,x2)[1][2]), 5), round(abs(covarSamp(x1,x1) - covarSampMatrix(x1,x2)[1][1]), 5), round(abs(covarSamp(x2,x2) - covarSampMatrix(x1,x2)[2][2]), 5) from (select randNormal(100, 1) as x1, randNormal(100,5) as x2 from numbers(100000));
 
 SELECT covarPopMatrix(a_value) FROM (select a_value from fh limit 0);
 
 SELECT covarPopMatrix(a_value) FROM (select a_value from fh limit 1);
 
-SELECT covarPopMatrix(a_value, b_value) FROM (select a_value, b_value from fh limit 0);
-
-SELECT covarPopMatrix(a_value, b_value) FROM (select a_value, b_value from fh limit 1);
-
-SELECT covarPopMatrix(a_value, b_value, c_value) FROM (select a_value, b_value, c_value from fh limit 0);
-
-SELECT covarPopMatrix(a_value, b_value, c_value) FROM (select a_value, b_value, c_value from fh limit 1);
-
 SELECT covarPopMatrix(a_value, b_value, c_value, d_value) FROM (select a_value, b_value, c_value, d_value from fh limit 0);
 
 SELECT covarPopMatrix(a_value, b_value, c_value, d_value) FROM (select a_value, b_value, c_value, d_value from fh limit 1);
 
+SELECT arrayMap(x -> arrayMap(y -> round(y, 5), x), covarPopMatrix(a_value, b_value, c_value, d_value))  FROM fh;
+
+SELECT round(abs(covarPop(x1,x2) - covarPopMatrix(x1,x2)[1][2]), 5), round(abs(covarPop(x1,x1) - covarPopMatrix(x1,x2)[1][1]), 5), round(abs(covarPop(x2,x2) - covarPopMatrix(x1,x2)[2][2]), 5) from (select randNormal(100, 1) as x1, randNormal(100,5) as x2 from numbers(100000));
