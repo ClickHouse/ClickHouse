3ea3b3545d8c37e20ce5d5674038807030656590
Dmitry Novik <n0vik@clickhouse.com>
2023-02-03 18:02:50 +0000

    Add statefull test

--- tests/queries/1_stateful/00173_group_by_use_nulls.sql
+++ tests/queries/1_stateful/00173_group_by_use_nulls.sql
@@ -8,3 +8,17 @@ ORDER BY
     CounterID ASC
 LIMIT 10
 SETTINGS group_by_use_nulls = 1;
+
+-- { echoOn }
+set allow_experimental_analyzer = 1;
+
+SELECT
+    CounterID AS k,
+    quantileBFloat16(0.5)(ResolutionWidth)
+FROM remote('127.0.0.{1,2}', test, hits)
+GROUP BY k
+ORDER BY
+    count() DESC,
+    CounterID ASC
+LIMIT 10
+SETTINGS group_by_use_nulls = 1;
