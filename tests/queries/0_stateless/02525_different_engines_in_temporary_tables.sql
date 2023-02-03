fa78c5b45982a96950cf30a59fd15441f8a0c0ad
Roman Vasin <r.vasin@arenadata.io>
2023-02-03 08:26:04 +0000

    Add TinyLog and StripeLog table engines to test

--- tests/queries/0_stateless/02525_different_engines_in_temporary_tables.sql
+++ tests/queries/0_stateless/02525_different_engines_in_temporary_tables.sql
@@ -21,3 +21,25 @@ ENGINE = Log;
 INSERT INTO table_log_02525 VALUES (1, 'a'), (2, 'b'), (3, 'c');
 SELECT * FROM table_log_02525;
 DROP TEMPORARY TABLE table_log_02525;
+
+DROP TEMPORARY TABLE IF EXISTS table_stripe_log_02525;
+CREATE TEMPORARY TABLE table_stripe_log_02525
+(
+    id UInt64,
+    info String
+)
+ENGINE = StripeLog;
+INSERT INTO table_stripe_log_02525 VALUES (1, 'a'), (2, 'b'), (3, 'c');
+SELECT * FROM table_stripe_log_02525;
+DROP TEMPORARY TABLE table_stripe_log_02525;
+
+DROP TEMPORARY TABLE IF EXISTS table_tiny_log_02525;
+CREATE TEMPORARY TABLE table_tiny_log_02525
+(
+    id UInt64,
+    info String
+)
+ENGINE = TinyLog;
+INSERT INTO table_tiny_log_02525 VALUES (1, 'a'), (2, 'b'), (3, 'c');
+SELECT * FROM table_tiny_log_02525;
+DROP TEMPORARY TABLE table_tiny_log_02525;
