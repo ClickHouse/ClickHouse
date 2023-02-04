a10f020b2d61a2e35b97fab3fabaaedebb1ff929
flynn <fenglv15@mails.ucas.ac.cn>
2023-02-04 14:28:31 +0000

    Storage Log faminy support settings storage policy

--- src/Storages/StorageLogSettings.cpp
+++ src/Storages/StorageLogSettings.cpp
@@ -1,17 +1,27 @@
 #include "StorageLogSettings.h"
+#include <Disks/StoragePolicy.h>
+#include <Interpreters/Context.h>
 #include <Parsers/ASTCreateQuery.h>
 #include <Parsers/ASTSetQuery.h>
 
 namespace DB
 {
-String getDiskName(ASTStorage & storage_def)
+String getDiskName(ASTStorage & storage_def, ContextPtr context)
 {
     if (storage_def.settings)
     {
         SettingsChanges changes = storage_def.settings->changes;
         for (const auto & change : changes)
+        {
+            /// How about both disk and storage_policy are specified?
             if (change.name == "disk")
                 return change.value.safeGet<String>();
+            if (change.name == "storage_policy")
+            {
+                auto policy = context->getStoragePolicy(change.value.safeGet<String>());
+                return policy->getAnyDisk()->getName();
+            }
+        }
     }
     return "default";
 }
