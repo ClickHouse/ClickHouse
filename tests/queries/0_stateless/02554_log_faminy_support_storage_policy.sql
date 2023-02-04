d602ceb9c1f0b00b5466a571f62467d7d810daa0
flynn <fenglv15@mails.ucas.ac.cn>
2023-02-04 14:28:31 +0000

    Storage Log faminy support settings storage policy

---
+++ tests/queries/0_stateless/02554_log_faminy_support_storage_policy.sql
@@ -0,0 +1,15 @@
+DROP TABLE IF EXISTS test_2554_log;
+CREATE TABLE test_2554_log (n UInt32) ENGINE = Log SETTINGS storage_policy = 'default';
+
+INSERT INTO test_2554_log SELECT 1;
+SELECT * FROM test_2554_log;
+
+DROP TABLE test_2554_log;
+
+DROP TABLE IF EXISTS test_2554_tinylog;
+CREATE TABLE test_2554_tinylog (n UInt32) ENGINE = Log SETTINGS storage_policy = 'default';
+
+INSERT INTO test_2554_tinylog SELECT 1;
+SELECT * FROM test_2554_tinylog;
+
+DROP TABLE test_2554_tinylog;
