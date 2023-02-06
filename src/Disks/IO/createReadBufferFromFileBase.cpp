3f6b1814449438826cbee6a0ddf2abf792bfea4b
Robert Schulze <robert@clickhouse.com>
2023-02-06 16:55:07 +0000

    Fix build with -DENABLE_LIBURING=0 (or -DENABLE_LIBRARIES=0)
    Follow-up to #38456
    
    The problem was that code which uses liburing was conditionally build
    depending on define OS_LINUX and not based on whether liburing was
    enabled/disabled at build time.

--- src/Disks/IO/createReadBufferFromFileBase.cpp
+++ src/Disks/IO/createReadBufferFromFileBase.cpp
@@ -7,7 +7,7 @@
 #include <Disks/IO/ThreadPoolReader.h>
 #include <IO/SynchronousReader.h>
 #include <Common/ProfileEvents.h>
-
+#include "config.h"
 
 namespace ProfileEvents
 {
@@ -84,7 +84,7 @@ std::unique_ptr<ReadBufferFromFileBase> createReadBufferFromFileBase(
         }
         else if (settings.local_fs_method == LocalFSReadMethod::io_uring)
         {
-#if defined(OS_LINUX)
+#if USE_LIBURING
             static std::shared_ptr<IOUringReader> reader = std::make_shared<IOUringReader>(512);
             if (!reader->isSupported())
                 throw Exception(ErrorCodes::UNSUPPORTED_METHOD, "io_uring is not supported by this system");
