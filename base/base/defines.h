a88903f27610157485dc285c4606a09e3d1be626
Azat Khuzhin <a3at.mail@gmail.com>
2023-02-07 17:25:38 +0100

    Do not execute expressions inside chassert() in release builds (with sizeof() trick)
    Signed-off-by: Azat Khuzhin <a.khuzhin@semrush.com>

--- base/base/defines.h
+++ base/base/defines.h
@@ -130,7 +130,11 @@
         #define chassert(x) static_cast<bool>(x) ? void(0) : ::DB::abortOnFailedAssertion(#x)
         #define UNREACHABLE() abort()
     #else
-        #define chassert(x) (void)(x)
+        /// Here sizeof() trick is used to suppress unused warning for result,
+        /// since simple "(void)x" will evaluate the expression, while
+        /// "sizeof(!(x))" will not.
+        #define NIL_EXPRESSION(x) (void)sizeof(!(x))
+        #define chassert(x) NIL_EXPRESSION(x)
         #define UNREACHABLE() __builtin_unreachable()
     #endif
 #endif
