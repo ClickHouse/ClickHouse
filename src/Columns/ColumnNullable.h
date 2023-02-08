151206f9eb909d574e2b9f230bf593ec64db5e8d
avogar <pav.cruglov@yandex.ru>
2023-02-08 19:14:28 +0000

    Fix low cardinality case

--- src/Columns/ColumnNullable.h
+++ src/Columns/ColumnNullable.h
@@ -220,5 +220,6 @@ private:
 
 ColumnPtr makeNullable(const ColumnPtr & column);
 ColumnPtr makeNullableSafe(const ColumnPtr & column);
+ColumnPtr makeNullableOrLowCardinalityNullable(const ColumnPtr & column);
 
 }
