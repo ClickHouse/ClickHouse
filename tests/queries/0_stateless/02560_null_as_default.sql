9afee5757cb2df18496013b9a4b9c4f28bc0863c
avogar <pav.cruglov@yandex.ru>
2023-02-08 15:48:18 +0000

    Use default of columnt type in insert_null_as_default if column DEFAULT values is not specified

---
+++ tests/queries/0_stateless/02560_null_as_default.sql
@@ -0,0 +1,7 @@
+drop table if exists test;
+create table test (x UInt64) engine=Memory();
+set insert_null_as_default=1;
+insert into test select number % 2 ? NULL : 42 as x from numbers(2);
+select * from test;
+drop table test;
+
