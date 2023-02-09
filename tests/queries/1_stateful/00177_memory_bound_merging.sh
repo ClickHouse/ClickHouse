a81bdbc42607376824e59a568e9e79133651998a
Nikita Taranov <nikita.taranov@clickhouse.com>
2023-02-09 12:45:26 +0000

    one more time

--- tests/queries/1_stateful/00177_memory_bound_merging.sh
+++ tests/queries/1_stateful/00177_memory_bound_merging.sh
@@ -54,12 +54,16 @@ test3() {
         SET cluster_for_parallel_replicas = 'test_cluster_one_shard_three_replicas_localhost';
         SET max_threads = 16, prefer_localhost_replica = 1, read_in_order_two_level_merge_threshold = 1000, query_plan_aggregation_in_order = 1, distributed_aggregation_memory_efficient = 1;
 
-        EXPLAIN PIPELINE
-        SELECT URL, EventDate, max(URL)
-        FROM test.hits
-        WHERE CounterID = 1704509 AND UserID = 4322253409885123546
-        GROUP BY URL, EventDate
-        SETTINGS optimize_aggregation_in_order = 1, enable_memory_bound_merging_of_aggregation_results = 1, allow_experimental_parallel_reading_from_replicas = 1, max_parallel_replicas = 3, use_hedged_requests = 0"
+        SELECT replaceRegexpOne(explain, '^ *(\w+).*', '\\1')
+        FROM (
+            EXPLAIN PIPELINE
+            SELECT URL, EventDate, max(URL)
+            FROM test.hits
+            WHERE CounterID = 1704509 AND UserID = 4322253409885123546
+            GROUP BY URL, EventDate
+            SETTINGS optimize_aggregation_in_order = 1, enable_memory_bound_merging_of_aggregation_results = 1, allow_experimental_parallel_reading_from_replicas = 1, max_parallel_replicas = 3, use_hedged_requests = 0
+        )
+        WHERE explain LIKE '%Aggr%Transform%' OR explain LIKE '%InOrder%'"
 }
 
 test1
