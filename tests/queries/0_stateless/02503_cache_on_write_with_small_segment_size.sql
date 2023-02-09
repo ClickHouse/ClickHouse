cf10c816d2e5b11102f9cf8bdf94a5e9c5d0b686
kssenii <sumarokovakseniia@mail.ru>
2023-02-09 12:08:00 +0100

    Fix tests

--- tests/queries/0_stateless/02503_cache_on_write_with_small_segment_size.sql
+++
@@ -1,6 +0,0 @@
-drop table ttt if exists;
-create table ttt (id Int32, value String) engine=MergeTree() order by tuple()  settings storage_policy='s3_cache_7';
-insert into ttt settings throw_on_error_from_cache_on_write_operations = 1 select number, toString(number) from numbers(100000);
-select * from ttt format Null;
-select count() from ttt;
-drop table ttt no delay;
