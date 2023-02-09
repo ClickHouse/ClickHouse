66e05103598c0ccfd82e177c6d0fb42b15045a22
vdimir <vdimir@clickhouse.com>
2023-02-09 11:11:29 +0000

    Check explain result in 25336_read_in_order_final_desc

--- tests/queries/0_stateless/25336_read_in_order_final_desc.sql
+++ tests/queries/0_stateless/25336_read_in_order_final_desc.sql
@@ -18,4 +18,23 @@ FROM mytable FINAL
 WHERE key = 5
 ORDER BY timestamp DESC;
 
+
+SELECT if(explain like '%ReadType: InOrder%', 'Ok', 'Error: ' || explain) FROM (
+    EXPLAIN PLAN actions = 1
+    SELECT timestamp, value
+    FROM mytable FINAL
+    WHERE key = 5
+    ORDER BY timestamp
+) WHERE explain like '%ReadType%';
+
+
+SELECT if(explain like '%ReadType: Default%', 'Ok', 'Error: ' || explain) FROM (
+    EXPLAIN PLAN actions = 1
+    SELECT timestamp, value
+    FROM mytable FINAL
+    WHERE key = 5
+    ORDER BY timestamp DESC
+) WHERE explain like '%ReadType%';
+
+
 DROP TABLE IF EXISTS mytable;
