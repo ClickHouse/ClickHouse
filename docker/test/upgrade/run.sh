2b72ea8fdcc09b3ce88340618b45a80cfcaa3992
Kruglov Pavel <48961922+Avogar@users.noreply.github.com>
2023-02-10 18:28:19 +0100

    Update run.sh

--- docker/test/upgrade/run.sh
+++ docker/test/upgrade/run.sh
@@ -416,6 +416,7 @@ else
                -e "Session expired" \
                -e "TOO_MANY_PARTS" \
                -e "Authentication failed" \
+               -e "Container already exists" \
         /var/log/clickhouse-server/clickhouse-server.upgrade.log | zgrep -Fa "<Error>" > /test_output/upgrade_error_messages.txt \
         && echo -e "Error message in clickhouse-server.log (see upgrade_error_messages.txt)$FAIL$(head_escaped /test_output/bc_check_error_messages.txt)" \
             >> /test_output/test_results.tsv \
