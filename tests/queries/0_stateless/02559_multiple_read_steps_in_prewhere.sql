943f2ea212db183306429ba99fa0e6508651f581
Alexander Gololobov <440544+davenger@users.noreply.github.com>
2023-02-12 21:41:13 +0100

    Use 2 columns in test to have 2 read steps

--- tests/queries/0_stateless/02559_multiple_read_steps_in_prewhere.sql
+++ tests/queries/0_stateless/02559_multiple_read_steps_in_prewhere.sql
@@ -1,26 +1,26 @@
 DROP TABLE IF EXISTS test_02559;
 
-CREATE TABLE test_02559 (id UInt64) ENGINE=MergeTree ORDER BY id;
+CREATE TABLE test_02559 (id1 UInt64, id2 UInt64) ENGINE=MergeTree ORDER BY id1;
 
-INSERT INTO test_02559 SELECT number FROM numbers(10);
+INSERT INTO test_02559 SELECT number, number FROM numbers(10);
 
 SET enable_multiple_prewhere_read_steps=true, move_all_conditions_to_prewhere=true;
 
 -- { echoOn }
 
-SELECT cast(id as UInt16) AS id16 FROM test_02559 PREWHERE id16 and (id % 40000) LIMIT 10;
+SELECT cast(id1 as UInt16) AS id16 FROM test_02559 PREWHERE id16 and (id2 % 40000) LIMIT 10;
 
-SELECT cast(id as UInt16) AS cond1, (id % 40000) AS cond2, (cond1 AND cond2) AS cond FROM test_02559 PREWHERE cond LIMIT 10;
+SELECT cast(id1 as UInt16) AS cond1, (id2 % 40000) AS cond2, (cond1 AND cond2) AS cond FROM test_02559 PREWHERE cond LIMIT 10;
 
-SELECT cast(id as UInt16) AS cond1, (id % 40000) AS cond2, (cond1 AND cond2) AS cond FROM test_02559 PREWHERE cond AND id > 4 LIMIT 10;
+SELECT cast(id1 as UInt16) AS cond1, (id2 % 40000) AS cond2, (cond1 AND cond2) AS cond FROM test_02559 PREWHERE cond AND id2 > 4 LIMIT 10;
 
-SELECT cast(id as UInt16) AS cond1, (id % 40000) AS cond2, (cond1 AND cond2) AS cond FROM test_02559 PREWHERE id > 5 AND cond LIMIT 10;
+SELECT cast(id1 as UInt16) AS cond1, (id2 % 40000) AS cond2, (cond1 AND cond2) AS cond FROM test_02559 PREWHERE id2 > 5 AND cond LIMIT 10;
 
-SELECT cast(id as UInt16) AS cond1, (id % 40000) AS cond2, (cond1 AND cond2) AS cond FROM test_02559 PREWHERE cond1 AND id > 6 AND cond2 LIMIT 10;
+SELECT cast(id1 as UInt16) AS cond1, (id2 % 40000) AS cond2, (cond1 AND cond2) AS cond FROM test_02559 PREWHERE cond1 AND id2 > 6 AND cond2 LIMIT 10;
 
-SELECT cast(id as UInt16) AS cond1 FROM test_02559 PREWHERE cond1 LIMIT 10; -- { serverError ILLEGAL_TYPE_OF_COLUMN_FOR_FILTER }
+SELECT cast(id1 as UInt16) AS cond1 FROM test_02559 PREWHERE cond1 LIMIT 10; -- { serverError ILLEGAL_TYPE_OF_COLUMN_FOR_FILTER }
 
-SELECT count() FROM test_02559 PREWHERE 1 OR ignore(id) WHERE ignore(id)=0;
+SELECT count() FROM test_02559 PREWHERE id2>=0 AND (1 OR ignore(id1)) WHERE ignore(id1)=0;
 
 -- { echoOff }
 
