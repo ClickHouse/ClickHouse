fec64b926258fac99b2d0eef28dd6c1982304675
Nikolai Kochetov <nik-kochetov@yandex-team.ru>
2023-02-13 19:56:31 +0000

    Normal Projections analysis using query plan [In progress]

--- src/Processors/QueryPlan/Optimizations/Optimizations.h
+++ src/Processors/QueryPlan/Optimizations/Optimizations.h
@@ -93,7 +93,8 @@ using Stack = std::vector<Frame>;
 void optimizePrimaryKeyCondition(const Stack & stack);
 void optimizeReadInOrder(QueryPlan::Node & node, QueryPlan::Nodes & nodes);
 void optimizeAggregationInOrder(QueryPlan::Node & node, QueryPlan::Nodes &);
-bool optimizeUseProjections(QueryPlan::Node & node, QueryPlan::Nodes &);
+bool optimizeUseAggProjections(QueryPlan::Node & node, QueryPlan::Nodes & nodes);
+bool optimizeUseNormalProjections(Stack & stack, QueryPlan::Nodes & nodes);
 
 /// Enable memory bound merging of aggregation states for remote queries
 /// in case it was enabled for local plan
