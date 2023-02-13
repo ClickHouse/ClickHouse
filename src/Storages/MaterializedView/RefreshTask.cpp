3c514734d41b93b883df2960f2cc3e1e45193d6b
koloshmet <mvguzov@edu.hse.ru>
2023-02-13 03:02:03 +0200

    merged with master

--- src/Storages/MaterializedView/RefreshTask.cpp
+++ src/Storages/MaterializedView/RefreshTask.cpp
@@ -36,7 +36,7 @@ std::variant<RefreshEveryTimer, RefreshAfterTimer> makeRefreshTimer(const ASTRef
         case AFTER:
             return RefreshAfterTimer{strategy.interval};
         default:
-            throw Exception("Unknown refresh strategy kind", ErrorCodes::BAD_ARGUMENTS);
+            throw Exception(ErrorCodes::BAD_ARGUMENTS, "Unknown refresh strategy kind");
     }
 }
 
@@ -63,7 +63,7 @@ RefreshTaskHolder RefreshTask::create(
     if (strategy.dependencies)
     {
         if (strategy.schedule_kind != ASTRefreshStrategy::ScheduleKind::AFTER)
-            throw Exception("Dependencies are allowed only for AFTER refresh kind", ErrorCodes::BAD_ARGUMENTS);
+            throw Exception(ErrorCodes::BAD_ARGUMENTS, "Dependencies are allowed only for AFTER refresh kind");
 
         task->deps_entries.reserve(strategy.dependencies->children.size());
         for (auto && dependency : strategy.dependencies->children)
