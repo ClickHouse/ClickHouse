4101f7cf308bdf695deb85da9725e699e6f0ab86
vdimir <vdimir@clickhouse.com>
2023-02-14 13:12:10 +0000

    Tests for CrossToInnerJoinPass

---
+++ tests/queries/0_stateless/02564_analyzer_cross_to_inner.sql
@@ -0,0 +1,62 @@
+SET allow_experimental_analyzer = 1;
+
+DROP TABLE IF EXISTS t1;
+DROP TABLE IF EXISTS t2;
+DROP TABLE IF EXISTS t3;
+
+CREATE TABLE t1 (a UInt64, b UInt64) ENGINE = Memory;
+INSERT INTO t1 VALUES (1, 2), (3, 4), (5, 6);
+
+CREATE TABLE t2 (a UInt64, b UInt64) ENGINE = Memory;
+INSERT INTO t2 VALUES (3, 4), (5, 6), (7, 8);
+
+CREATE TABLE t3 (a UInt64, b UInt64) ENGINE = Memory;
+INSERT INTO t3 VALUES (5, 6), (7, 8), (9, 10);
+
+SET cross_to_inner_join_rewrite = 1;
+
+SELECT * FROM t1, t2, (SELECT a as x from t3 where a + 1 = b ) as t3
+WHERE t1.a = if(t2.b > 0, t2.a, 0) AND t2.a = t3.x AND 1
+;
+
+SELECT * FROM t1, t2, (SELECT a as x from t3 where a + 1 = b ) as t3
+WHERE t1.a = if(t2.b > 0, t2.a, 0)
+ORDER BY t1.a, t2.a, t3.x
+;
+
+-- rewrite two joins
+SELECT countIf(explain like '%strictness: ALL, %kind: INNER%'), countIf(explain like '%kind: COMMA%') FROM (
+    EXPLAIN QUERY TREE
+    SELECT * FROM t1, t2, (SELECT a as x from t3 where a + 1 = b ) as t3
+    WHERE t1.a = if(t2.b > 0, t2.a, 0) AND t2.a = t3.x AND 1
+) WHERE explain like '% JOIN % kind: %'
+SETTINGS allow_experimental_analyzer = 0 -- workaround for viewExplain
+;
+
+-- setting is disabled
+SELECT countIf(explain like '%strictness: ALL, %kind: INNER%'), countIf(explain like '%kind: COMMA%') FROM (
+    EXPLAIN QUERY TREE
+    SELECT * FROM t1, t2, (SELECT a as x from t3 where a + 1 = b ) as t3
+    WHERE t1.a = if(t2.b > 0, t2.a, 0) AND t2.a = t3.x AND 1
+    SETTINGS cross_to_inner_join_rewrite = 0
+) WHERE explain like '% JOIN % kind: %'
+SETTINGS allow_experimental_analyzer = 0 -- workaround for viewExplain
+;
+
+-- only one join can be rewritten
+SELECT countIf(explain like '%strictness: ALL, %kind: INNER%'), countIf(explain like '%kind: COMMA%') FROM (
+    EXPLAIN QUERY TREE
+    SELECT * FROM t1, t2, (SELECT a as x from t3 where a + 1 = b ) as t3
+    WHERE t1.a = if(t2.b > 0, t2.a, 0)
+) WHERE explain like '% JOIN % kind: %'
+SETTINGS allow_experimental_analyzer = 0 -- workaround for viewExplain
+;
+
+-- throw in force mode
+SELECT * FROM t1, t2, (SELECT a as x from t3 where a + 1 = b ) as t3
+WHERE t1.a = if(t2.b > 0, t2.a, 0)
+SETTINGS cross_to_inner_join_rewrite = 2; -- { serverError INCORRECT_QUERY }
+
+DROP TABLE IF EXISTS t1;
+DROP TABLE IF EXISTS t2;
+DROP TABLE IF EXISTS t3;
