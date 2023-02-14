53298f5a17bc88faffef80b1a47f2920912f5bda
Roman Vasin <r.vasin@arenadata.io>
2023-02-14 11:16:00 +0000

    Add test for ReplicatedMergeTree

--- tests/queries/0_stateless/02525_different_engines_in_temporary_tables.sql
+++ tests/queries/0_stateless/02525_different_engines_in_temporary_tables.sql
@@ -44,6 +44,16 @@ INSERT INTO table_tiny_log_02525 VALUES (1, 'a'), (2, 'b'), (3, 'c');
 SELECT * FROM table_tiny_log_02525;
 DROP TEMPORARY TABLE table_tiny_log_02525;
 
+DROP TEMPORARY TABLE IF EXISTS table_replicated_merge_tree_02525;
+CREATE TEMPORARY TABLE table_replicated_merge_tree_02525
+(
+    id UInt64,
+    info String
+)
+ENGINE ReplicatedMergeTree('/clickhouse/tables/{database}/test_02525/table_replicated_merge_tree_02525', 'r1')
+ORDER BY id
+PRIMARY KEY id; -- { serverError 80 }
+
 DROP TEMPORARY TABLE IF EXISTS table_keeper_map_02525;
 CREATE TEMPORARY TABLE table_keeper_map_02525
 (
