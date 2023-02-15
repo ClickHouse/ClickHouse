c6b02d3c9d74f4861587ce6a2e9d994a40504f86
Robert Schulze <robert@clickhouse.com>
2023-02-15 09:29:37 +0000

    Remove POCO_DLL

--- base/poco/NetSSL_OpenSSL/include/Poco/Net/NetSSL.h
+++ base/poco/NetSSL_OpenSSL/include/Poco/Net/NetSSL.h
@@ -32,13 +32,6 @@
 // NetSSL_API functions as being imported from a DLL, whereas this DLL sees symbols
 // defined with this macro as being exported.
 //
-#if (defined(_WIN32) || defined(__CYGWIN__)) && defined(POCO_DLL)
-#    if defined(NetSSL_EXPORTS)
-#        define NetSSL_API __declspec(dllexport)
-#    else
-#        define NetSSL_API __declspec(dllimport)
-#    endif
-#endif
 
 
 #if !defined(NetSSL_API)
