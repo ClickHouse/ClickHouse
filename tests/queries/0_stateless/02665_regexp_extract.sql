e7ab7ee037ea5018ba887678093916464dcc7f2e
Alexander Gololobov <440544+davenger@users.noreply.github.com>
2023-02-16 19:24:56 +0100

    Some more corner cases

--- tests/queries/0_stateless/02665_regexp_extract.sql
+++ tests/queries/0_stateless/02665_regexp_extract.sql
@@ -1,6 +1,7 @@
 select regexpExtract('100-200', '(\\d+)-(\\d+)', 1);
 select regexpExtract('100-200', '(\\d+)-(\\d+)');
 select regexpExtract('100-200', '(\\d+)-(\\d+)', 2);
+select regexpExtract('100-200', '(\\d+)-(\\d+)', 0);
 select regexpExtract('100-200', '(\\d+).*', 1);
 select regexpExtract('100-200', '([a-z])', 1);
 select regexpExtract(null, '([a-z])', 1);
@@ -33,8 +34,12 @@ select regexpExtract(materialize('100-200'), null, materialize(1));
 select regexpExtract(materialize('100-200'), '([a-z])', materialize(null));
 
 
-select regexpExtract('100-200'); -- { serverError 42 }
-select regexpExtract('100-200', '(\\d+)-(\\d+)', 1, 2); -- { serverError 42 }
-select regexpExtract(cast('100-200' as FixedString(10)), '(\\d+)-(\\d+)', 1); -- { serverError 43 }
-select regexpExtract('100-200', cast('(\\d+)-(\\d+)' as FixedString(20)), 1); -- { serverError 43 }
-select regexpExtract('100-200', materialize('(\\d+)-(\\d+)'), 1); -- { serverError 44 }
+select regexpExtract('100-200'); -- { serverError NUMBER_OF_ARGUMENTS_DOESNT_MATCH }
+select regexpExtract('100-200', '(\\d+)-(\\d+)', 1, 2); -- { serverError NUMBER_OF_ARGUMENTS_DOESNT_MATCH }
+select regexpExtract(cast('100-200' as FixedString(10)), '(\\d+)-(\\d+)', 1); -- { serverError ILLEGAL_TYPE_OF_ARGUMENT }
+select regexpExtract('100-200', cast('(\\d+)-(\\d+)' as FixedString(20)), 1); -- { serverError ILLEGAL_TYPE_OF_ARGUMENT }
+select regexpExtract('100-200', materialize('(\\d+)-(\\d+)'), 1); -- { serverError ILLEGAL_COLUMN }
+select regexpExtract('100-200', '(\\d+)-(\\d+)', 3); -- { serverError INDEX_OF_POSITIONAL_ARGUMENT_IS_OUT_OF_RANGE }
+select regexpExtract('100-200', '(\\d+)-(\\d+)', -1); -- { serverError INDEX_OF_POSITIONAL_ARGUMENT_IS_OUT_OF_RANGE }
+select regexpExtract('100-200', '\\d+-\\d+', 0);
+select regexpExtract('100-200', '\\d+-\\d+', 1);-- { serverError INDEX_OF_POSITIONAL_ARGUMENT_IS_OUT_OF_RANGE }
