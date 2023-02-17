046e2bb8eac0127f56fdaaedf54bee3cbb8f4fea
Anton Popov <anton@clickhouse.com>
2023-02-17 16:50:20 +0000

    fix issues with sparse columns

--- tests/queries/0_stateless/02008_materialize_column.sql
+++ tests/queries/0_stateless/02008_materialize_column.sql
@@ -8,33 +8,33 @@ INSERT INTO tmp SELECT * FROM system.numbers LIMIT 20;
 ALTER TABLE tmp MATERIALIZE COLUMN x; -- { serverError 36 }
 
 ALTER TABLE tmp ADD COLUMN s String DEFAULT toString(x);
-SELECT groupArray(x), groupArray(s) FROM tmp;
+SELECT arraySort(arraySort(groupArray(x))), groupArray(s) FROM tmp;
 
 ALTER TABLE tmp MODIFY COLUMN s String DEFAULT toString(x+1);
-SELECT groupArray(x), groupArray(s) FROM tmp;
+SELECT arraySort(groupArray(x)), groupArray(s) FROM tmp;
 
 ALTER TABLE tmp MATERIALIZE COLUMN s;
 ALTER TABLE tmp MODIFY COLUMN s String DEFAULT toString(x+2);
-SELECT groupArray(x), groupArray(s) FROM tmp;
+SELECT arraySort(groupArray(x)), groupArray(s) FROM tmp;
 
 ALTER TABLE tmp MATERIALIZE COLUMN s;
 ALTER TABLE tmp MODIFY COLUMN s String DEFAULT toString(x+3);
-SELECT groupArray(x), groupArray(s) FROM tmp;
+SELECT arraySort(groupArray(x)), groupArray(s) FROM tmp;
 ALTER TABLE tmp DROP COLUMN s;
 
 ALTER TABLE tmp ADD COLUMN s String MATERIALIZED toString(x);
-SELECT groupArray(x), groupArray(s) FROM tmp;
+SELECT arraySort(groupArray(x)), groupArray(s) FROM tmp;
 
 ALTER TABLE tmp MODIFY COLUMN s String MATERIALIZED toString(x+1);
-SELECT groupArray(x), groupArray(s) FROM tmp;
+SELECT arraySort(groupArray(x)), groupArray(s) FROM tmp;
 
 ALTER TABLE tmp MATERIALIZE COLUMN s;
 ALTER TABLE tmp MODIFY COLUMN s String MATERIALIZED toString(x+2);
-SELECT groupArray(x), groupArray(s) FROM tmp;
+SELECT arraySort(groupArray(x)), groupArray(s) FROM tmp;
 
 ALTER TABLE tmp MATERIALIZE COLUMN s;
 ALTER TABLE tmp MODIFY COLUMN s String MATERIALIZED toString(x+3);
-SELECT groupArray(x), groupArray(s) FROM tmp;
+SELECT arraySort(groupArray(x)), groupArray(s) FROM tmp;
 ALTER TABLE tmp DROP COLUMN s;
 
 DROP TABLE tmp;
