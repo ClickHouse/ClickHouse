5427368fda5a5cfba9fbdadf56afefbd7ebdfbe3
Alexey Milovidov <milovidov@clickhouse.com>
2023-02-20 00:26:46 +0100

    Simpler

--- tests/queries/0_stateless/02667_wrong_structure.sql
+++ tests/queries/0_stateless/02667_wrong_structure.sql
@@ -1,11 +1,1 @@
-DROP TABLE IF EXISTS test_table__fuzz_2;
-
-SET allow_suspicious_low_cardinality_types = 1;
-
-CREATE TABLE test_table__fuzz_2 (`id` LowCardinality(UInt64), `value` LowCardinality(String)) ENGINE = TinyLog;
-
-INSERT INTO test_table__fuzz_2 VALUES (9806329011943062144,'wS6*');
-
-SELECT arrayMap(x -> (id + (SELECT 1 AS id WHERE [10][NULL])), [1048575]) FROM test_table__fuzz_2;
-
-DROP TABLE test_table__fuzz_2;
+SELECT arrayMap(x -> (toLowCardinality(1) + (SELECT 1 WHERE 0)), [1]);
