60a0aace46638de371b39def370c56d39cdf8d1c
Antonio Andelic <antonio@clickhouse.com>
2023-02-21 12:56:09 +0000

    Add tests

--- src/Analyzer/Passes/OrEqualityChainToInPass.h
+++ src/Analyzer/Passes/OrEqualityChainToInPass.h
@@ -5,6 +5,26 @@
 namespace DB
 {
 
+/**
+ * This pass replaces chains of equality functions inside an OR with a single IN operator.
+ * The replacement is done if:
+ *  - rhs of the equality function is a literal
+ *  - length of chain is at least 'optimize_min_equality_disjunction_chain_length' long OR lhs is LowCardinality
+ *
+ * E.g. (optimize_min_equality_disjunction_chain_length = 2)
+ * -------------------------------
+ * SELECT *
+ * FROM table
+ * WHERE a = 1 OR b = 'test' OR a = 2;
+ *
+ * will be transformed into
+ *
+ * SELECT *
+ * FROM TABLE
+ * WHERE b = 'test' OR a IN (1, 2);
+ * -------------------------------
+ */
+
 class OrEqualityChainToInPass final : public IQueryTreePass
 {
 public:
