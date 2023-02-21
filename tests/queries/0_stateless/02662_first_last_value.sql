e32b9d53abd1cbc7a0740d5002a0a4b69ec25255
lgbo-ustc <lgbo.ustc@gmail.com>
2023-02-21 16:06:05 +0800

    fixed tests

--- tests/queries/0_stateless/02662_first_last_value.sql
+++ tests/queries/0_stateless/02662_first_last_value.sql
@@ -13,4 +13,4 @@ select first_value(b) respect nulls from test;
 -- last value
 select last_value(b) from test;
 select last_value(b) ignore nulls from test;
-select last_value(true)(b) respect nulls from test;
+select last_value(b) respect nulls from test;
