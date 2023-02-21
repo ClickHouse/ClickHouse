7f9da7e19634b9b4b0cf29c47f22c6b074cf56e3
Dmitry Novik <n0vik@clickhouse.com>
2023-02-21 01:43:01 +0000

    Fix test

--- tests/integration/test_async_insert_memory/test.py
+++ tests/integration/test_async_insert_memory/test.py
@@ -21,15 +21,20 @@ def test_memory_usage():
         "CREATE TABLE async_table(data Array(UInt64)) ENGINE=MergeTree() ORDER BY data"
     )
 
-    response = node.get_query_request(
-        "SELECT groupArray(number + sleepEachRow(0.0001)) FROM numbers(1000000) SETTINGS max_memory_usage_for_user={}".format(30 * (2 ** 23))
+    node.get_query_request(
+        "SELECT count() FROM system.numbers"
     )
 
     INSERT_QUERY = "INSERT INTO async_table SETTINGS async_insert=1, wait_for_async_insert=1 VALUES ({})"
-    for i in range(10):
-        node.query(INSERT_QUERY.format([i in range(i * 5000000, (i + 1) * 5000000)]))
+    for iter in range(10):
+        values = list(range(iter * 5000000, (iter + 1) * 5000000))
+        node.query(INSERT_QUERY.format(values))
+
+    response = node.get_query_request(
+        "SELECT groupArray(number) FROM numbers(1000000) SETTINGS max_memory_usage_for_user={}".format(30 * (2 ** 23))
+    )
 
     _, err = response.get_answer_and_error()
-    assert err == "", "Query failed"
+    assert err == "", "Query failed with error {}".format(err)
 
     node.query("DROP TABLE async_table")
