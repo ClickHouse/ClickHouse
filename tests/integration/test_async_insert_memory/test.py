2a65e36d7d475d8c8206926f5ac79876eeb96ccc
robot-clickhouse <robot-clickhouse@users.noreply.github.com>
2023-02-21 02:06:16 +0000

    Automatic style fix

--- tests/integration/test_async_insert_memory/test.py
+++ tests/integration/test_async_insert_memory/test.py
@@ -21,9 +21,7 @@ def test_memory_usage():
         "CREATE TABLE async_table(data Array(UInt64)) ENGINE=MergeTree() ORDER BY data"
     )
 
-    node.get_query_request(
-        "SELECT count() FROM system.numbers"
-    )
+    node.get_query_request("SELECT count() FROM system.numbers")
 
     INSERT_QUERY = "INSERT INTO async_table SETTINGS async_insert=1, wait_for_async_insert=1 VALUES ({})"
     for iter in range(10):
@@ -31,7 +29,9 @@ def test_memory_usage():
         node.query(INSERT_QUERY.format(values))
 
     response = node.get_query_request(
-        "SELECT groupArray(number) FROM numbers(1000000) SETTINGS max_memory_usage_for_user={}".format(30 * (2 ** 23))
+        "SELECT groupArray(number) FROM numbers(1000000) SETTINGS max_memory_usage_for_user={}".format(
+            30 * (2**23)
+        )
     )
 
     _, err = response.get_answer_and_error()
