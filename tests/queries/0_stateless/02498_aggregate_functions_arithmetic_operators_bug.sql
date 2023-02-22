6d70e97d01ae04dc8cb24ade4514c33328be491c
ducle.canh <ducle.canh@bytedance.com>
2023-02-22 13:30:57 +0800

    fix logic + better test

--- tests/queries/0_stateless/02498_aggregate_functions_arithmetic_operators_bug.sql
+++
@@ -1,2 +0,0 @@
-SELECT max(100-number), min(100-number) FROM numbers(2) SETTINGS optimize_arithmetic_operations_in_aggregate_functions = 1;
-SELECT max(100-number), min(100-number) FROM numbers(2)  SETTINGS optimize_arithmetic_operations_in_aggregate_functions = 1, allow_experimental_analyzer = 1;
