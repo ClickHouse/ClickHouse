2fbe75957d12b083ac19d222d3d88e73cc526f10
pufit <pufit@clickhouse.com>
2023-02-23 18:24:57 -0500

    Add `default` column for `system.settings` and `system.server_settings`

--- src/Storages/System/StorageSystemServerSettings.cpp
+++ src/Storages/System/StorageSystemServerSettings.cpp
@@ -11,6 +11,7 @@ NamesAndTypesList StorageSystemServerSettings::getNamesAndTypes()
     return {
         {"name", std::make_shared<DataTypeString>()},
         {"value", std::make_shared<DataTypeString>()},
+        {"default", std::make_shared<DataTypeString>()},
         {"changed", std::make_shared<DataTypeUInt8>()},
         {"description", std::make_shared<DataTypeString>()},
         {"type", std::make_shared<DataTypeString>()},
@@ -28,9 +29,10 @@ void StorageSystemServerSettings::fillData(MutableColumns & res_columns, Context
         const auto & setting_name = setting.getName();
         res_columns[0]->insert(setting_name);
         res_columns[1]->insert(setting.getValueString());
-        res_columns[2]->insert(setting.isValueChanged());
-        res_columns[3]->insert(setting.getDescription());
-        res_columns[4]->insert(setting.getTypeName());
+        res_columns[2]->insert(setting.getDefaultValueString());
+        res_columns[3]->insert(setting.isValueChanged());
+        res_columns[4]->insert(setting.getDescription());
+        res_columns[5]->insert(setting.getTypeName());
     }
 }
 
