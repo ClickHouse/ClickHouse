93dd9293ff6853acfa0c69024fab6afa495e32a8
Roman Vasin <r.vasin@arenadata.io>
2023-02-28 10:04:33 +0000

    Use INCORRECT_QUERY in test and add query with remote()

--- tests/queries/0_stateless/02525_different_engines_in_temporary_tables.sql
+++ tests/queries/0_stateless/02525_different_engines_in_temporary_tables.sql
@@ -9,6 +9,9 @@ ORDER BY id
 PRIMARY KEY id;
 INSERT INTO table_merge_tree_02525 VALUES (1, 'a'), (2, 'b'), (3, 'c');
 SELECT * FROM table_merge_tree_02525;
+-- Check that temporary table with MergeTree is not sent to remote servers
+-- The query with remote() should not fail
+SELECT dummy FROM remote('127.0.0.{1,2}', system, one);
 DROP TEMPORARY TABLE table_merge_tree_02525;
 
 DROP TEMPORARY TABLE IF EXISTS table_log_02525;
@@ -52,7 +55,7 @@ CREATE TEMPORARY TABLE table_replicated_merge_tree_02525
 )
 ENGINE ReplicatedMergeTree('/clickhouse/tables/{database}/test_02525/table_replicated_merge_tree_02525', 'r1')
 ORDER BY id
-PRIMARY KEY id; -- { serverError 80 }
+PRIMARY KEY id; -- { serverError INCORRECT_QUERY }
 
 DROP TEMPORARY TABLE IF EXISTS table_keeper_map_02525;
 CREATE TEMPORARY TABLE table_keeper_map_02525
@@ -60,4 +63,4 @@ CREATE TEMPORARY TABLE table_keeper_map_02525
     key String,
     value UInt32
 ) Engine=KeeperMap('/' || currentDatabase() || '/test02525')
-PRIMARY KEY(key); -- { serverError 80 }
+PRIMARY KEY(key); -- { serverError INCORRECT_QUERY }
