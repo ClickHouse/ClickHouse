f177c99747b766e46ff9ca8f535869449f97c558
KevinyhZou <kevinyunhe@163.com>
2023-03-01 16:28:30 +0800

    fix ci error

--- tests/queries/0_stateless/02674_space_function.sql
+++ tests/queries/0_stateless/02674_space_function.sql
@@ -14,6 +14,6 @@ SELECT space(u8), length(space(u8)) FROM defaults;
 SELECT space(u16), length(space(u16)) FROM defaults;
 SELECT space(u32), length(space(u32)) from defaults;
 SELECT space(u64), length(space(u64)) FROM defaults;
-SELECT space(NULL) FROM default;
+SELECT space(NULL) FROM defaults;
 
 DROP TABLE defaults;
