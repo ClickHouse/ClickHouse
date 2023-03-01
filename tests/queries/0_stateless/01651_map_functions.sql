119ea045acf5edffb8e141b6abd285a8bfc5cdb8
taiyang-li <654010905@qq.com>
2023-03-01 21:19:39 +0800

    change as request

--- tests/queries/0_stateless/01651_map_functions.sql
+++ tests/queries/0_stateless/01651_map_functions.sql
@@ -8,7 +8,7 @@ select mapContains(a, 'name') from table_map;
 select mapContains(a, 'gender') from table_map;
 select mapContains(a, 'abc') from table_map;
 select mapContains(a, b) from table_map;
-select mapContains(a, 10) from table_map; -- { serverError 386 }
+select mapContains(a, 10) from table_map; -- { serverError NO_COMMON_TYPE }
 select mapKeys(a) from table_map;
 select mapFromArrays(c, d) from table_map;
 drop table if exists table_map;
@@ -16,8 +16,8 @@ drop table if exists table_map;
 CREATE TABLE table_map (a Map(UInt8, Int), b UInt8, c UInt32, d Array(String), e Array(String)) engine = MergeTree order by tuple();
 insert into table_map select map(number, number), number, number, [number, number, number], [number*2, number*3, number*4] from numbers(1000, 3);
 select mapContains(a, b), mapContains(a, c), mapContains(a, 233) from table_map;
-select mapContains(a, 'aaa') from table_map; -- { serverError 386 }
-select mapContains(b, 'aaa') from table_map; -- { serverError 43 }
+select mapContains(a, 'aaa') from table_map; -- { serverError NO_COMMON_TYPE }
+select mapContains(b, 'aaa') from table_map; -- { serverError ILLEGAL_TYPE_OF_ARGUMENT }
 select mapKeys(a) from table_map;
 select mapValues(a) from table_map;
 select mapFromArrays(d, e) from table_map;
@@ -34,8 +34,8 @@ select mapFromArrays(['aa', 'bb'], [4, 5]);
 select mapFromArrays(['aa', 'bb'], materialize([4, 5])) from numbers(2);
 select mapFromArrays(materialize(['aa', 'bb']), [4, 5]) from numbers(2);
 select mapFromArrays(materialize(['aa', 'bb']), materialize([4, 5])) from numbers(2);
-select mapFromArrays('aa', [4, 5]); -- { serverError 43 }
-select mapFromArrays(['aa', 'bb'], 5); -- { serverError 43 }
-select mapFromArrays(['aa', 'bb'], [4, 5], [6, 7]); -- { serverError 42 }
-select mapFromArrays(['aa', 'bb'], [4, 5, 6]); -- { serverError 190 }
-select mapFromArrays([[1,2], [3,4]], [4, 5, 6]); -- { serverError 36 }
+select mapFromArrays('aa', [4, 5]); -- { serverError ILLEGAL_TYPE_OF_ARGUMENT }
+select mapFromArrays(['aa', 'bb'], 5); -- { serverError ILLEGAL_TYPE_OF_ARGUMENT }
+select mapFromArrays(['aa', 'bb'], [4, 5], [6, 7]); -- { serverError NUMBER_OF_ARGUMENTS_DOESNT_MATCH }
+select mapFromArrays(['aa', 'bb'], [4, 5, 6]); -- { serverError SIZES_OF_ARRAYS_DONT_MATCH }
+select mapFromArrays([[1,2], [3,4]], [4, 5, 6]); -- { serverError BAD_ARGUMENTS }
