388157e3ee72bc0cb1f704f865b6db232b9481ad
Yakov Olkhovskiy <99031427+yakov-olkhovskiy@users.noreply.github.com>
2023-03-03 17:27:32 -0500

    Update HTTPServerConnection.h

--- src/Server/HTTP/HTTPServerConnection.h
+++ src/Server/HTTP/HTTPServerConnection.h
@@ -21,18 +21,6 @@ public:
         Poco::Net::HTTPServerParams::Ptr params,
         HTTPRequestHandlerFactoryPtr factory);
 
-    HTTPServerConnection(
-        HTTPContextPtr context_,
-        TCPServer & tcp_server_,
-        const Poco::Net::StreamSocket & socket_,
-        Poco::Net::HTTPServerParams::Ptr params_,
-        HTTPRequestHandlerFactoryPtr factory_,
-        const String & forwarded_for_)
-    : HTTPServerConnection(context_, tcp_server_, socket_, params_, factory_)
-    {
-        forwarded_for = forwarded_for_;
-    }
-
     void run() override;
 
 protected:
