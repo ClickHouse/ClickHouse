bdc5775d0bc301db746b467a0916a119f18baf68
Alexander Gololobov <440544+davenger@users.noreply.github.com>
2023-03-03 16:22:38 +0100

    Short and long tests

--- tests/queries/0_stateless/02581_share_big_sets_between_mutation_tasks_long.sql
+++ tests/queries/0_stateless/02581_share_big_sets_between_mutation_tasks_long.sql
@@ -1,3 +1,5 @@
+-- Tags: long, no-tsan, no-asan, no-ubsan, no-msan
+
 DROP TABLE IF EXISTS 02581_trips;
 
 CREATE TABLE 02581_trips(id UInt32, description String) ENGINE=MergeTree ORDER BY id;
@@ -13,6 +15,6 @@ SELECT count() from 02581_trips;
 SELECT name FROM system.parts WHERE database=currentDatabase() AND table = '02581_trips' AND active ORDER BY name;
 
 -- Run mutation with a 'IN big subquery'
-ALTER TABLE 02581_trips UPDATE description='' WHERE id IN (SELECT (number+5)::UInt32 FROM numbers(10000000)) SETTINGS mutations_sync=2;
+ALTER TABLE 02581_trips UPDATE description='' WHERE id IN (SELECT (number+5)::UInt32 FROM numbers(100000000)) SETTINGS mutations_sync=2;
 
 DROP TABLE 02581_trips;
