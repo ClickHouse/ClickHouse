bf365cec8c654bd592f32355c2ebf41c1743ee69
Arthur Passos <arthur.ti@outlook.com>
2023-03-07 10:08:04 -0300

    Modify find_first_symbols so it works as expected for find_first_not_symbols

--- src/Common/tests/gtest_find_symbols.cpp
+++ src/Common/tests/gtest_find_symbols.cpp
@@ -4,6 +4,12 @@
 #include <gtest/gtest.h>
 
 
+template <char ... symbols>
+void test_find_first_not(std::string_view haystack, std::string_view::iterator expected_output)
+{
+    ASSERT_EQ(expected_output, find_first_not_symbols<symbols...>(haystack.begin(), haystack.end()));
+}
+
 TEST(FindSymbols, SimpleTest)
 {
     std::string s = "Hello, world! Goodbye...";
@@ -35,4 +41,40 @@ TEST(FindSymbols, SimpleTest)
         splitInto<' ', ','>(vals, "s String", true);
         ASSERT_EQ(vals, (std::vector<std::string>{"s", "String"}));
     }
+}
+
+TEST(FindNotSymbols, AllSymbolsPresent)
+{
+    std::string_view s = "hello world hello";
+    test_find_first_not<'h', 'e', 'l', 'o', 'w', 'r', 'd', ' '>(s, s.end());
+}
+
+TEST(FindNotSymbols, NoSymbolsMatch)
+{
+    std::string_view s = "abcdefg";
+    test_find_first_not<'h', 'i', 'j'>(s, s.begin());
+}
+
+TEST(FindNotSymbols, ExtraSymbols)
+{
+    std::string_view s = "hello_world_hello";
+    test_find_first_not<'h', 'e', 'l', 'o', ' '>(s, s.begin() + 5u);
+}
+
+TEST(FindNotSymbols, EmptyString)
+{
+    std::string_view s;
+    test_find_first_not<'h', 'e', 'l', 'o', 'w', 'r', 'd', ' '>(s, s.end());
+}
+
+TEST(FindNotSymbols, SingleChar)
+{
+    std::string_view s = "a";
+    test_find_first_not<'a'>(s, s.end());
+}
+
+TEST(FindNotSymbols, NullCharacter)
+{
+    auto s = std::string_view("abcdefg\0x", 9u);
+    test_find_first_not<'a', 'b', 'c', 'd', 'e', 'f', 'g'>(s, s.begin() + 7u);
 }
