bc0e75e3d5079691ea2bde4a8314474cb3327d60
taiyang-li <654010905@qq.com>
2023-03-07 18:27:02 +0800

    fix failed uts

--- src/AggregateFunctions/AggregateFunctionQuantile.h
+++ src/AggregateFunctions/AggregateFunctionQuantile.h
@@ -26,7 +26,7 @@ namespace ErrorCodes
 {
     extern const int ILLEGAL_TYPE_OF_ARGUMENT;
     extern const int NUMBER_OF_ARGUMENTS_DOESNT_MATCH;
-    extern const int LOGICAL_ERROR;
+    extern const int BAD_ARGUMENTS;
 }
 
 template <typename> class QuantileTiming;
--- tests/queries/0_stateless/02661_quantile_approx.sql
+++ tests/queries/0_stateless/02661_quantile_approx.sql
@@ -1,3 +1,4 @@
+-- { echoOn }
 with arrayJoin([0, 1, 2, 10]) as x select quantilesApprox(100, 0.5, 0.4, 0.1)(x);
 with arrayJoin([0, 6, 7, 9, 10]) as x select quantileApprox(100, 0.5)(x);
 
@@ -13,17 +14,18 @@ select quantilesApprox(1000, 100/1000, 200/1000, 250/1000, 314/1000, 777/1000)(n
 select quantilesApprox(10000, 100/1000, 200/1000, 250/1000, 314/1000, 777/1000)(number + 1) from numbers(1000);
 
 
-select quantileApprox() from numbers(10); -- { serverError NUMBER_OF_ARGUMENTS_DOESNT_MATCH }
-select quantileApprox(100) from numbers(10);
-select quantileApprox(100, 0.5) from numbers(10);
-select quantileApprox(100, 0.5, 0.75) from numbers(10); -- { serverError NUMBER_OF_ARGUMENTS_DOESNT_MATCH }
-select quantileApprox('abc', 0.5) from numbers(10); -- { serverError ILLEGAL_TYPE_OF_ARGUMENT }
-select quantileApprox(1.23, 0.5) from numbers(10); -- { serverError ILLEGAL_TYPE_OF_ARGUMENT }
-select quantileApprox(-100, 0.5) from numbers(10); -- { serverError BAD_ARGUMENTS }
+select quantileApprox()(number) from numbers(10); -- { serverError BAD_ARGUMENTS }
+select quantileApprox(100)(number) from numbers(10);
+select quantileApprox(100, 0.5)(number) from numbers(10);
+select quantileApprox(100, 0.5, 0.75)(number) from numbers(10); -- { serverError NUMBER_OF_ARGUMENTS_DOESNT_MATCH }
+select quantileApprox('abc', 0.5)(number) from numbers(10); -- { serverError ILLEGAL_TYPE_OF_ARGUMENT }
+select quantileApprox(1.23, 0.5)(number) from numbers(10); -- { serverError ILLEGAL_TYPE_OF_ARGUMENT }
+select quantileApprox(-100, 0.5)(number) from numbers(10); -- { serverError BAD_ARGUMENTS }
 
-select quantilesApprox() from numbers(10); -- { serverError NUMBER_OF_ARGUMENTS_DOESNT_MATCH }
-select quantilesApprox(100) from numbers(10); -- { serverError NUMBER_OF_ARGUMENTS_DOESNT_MATCH }
-select quantilesApprox(100, 0.5) from numbers(10);
-select quantilesApprox('abc', 0.5, 0.75) from numbers(10); -- { serverError ILLEGAL_TYPE_OF_ARGUMENT }
-select quantilesApprox(1.23, 0.5, 0.75) from numbers(10); -- { serverError ILLEGAL_TYPE_OF_ARGUMENT }
-select quantilesApprox(-100, 0.5, 0.75) from numbers(10); -- { serverError BAD_ARGUMENTS }
+select quantilesApprox()(number) from numbers(10); -- { serverError BAD_ARGUMENTS }
+select quantilesApprox(100)(number) from numbers(10); -- { serverError NUMBER_OF_ARGUMENTS_DOESNT_MATCH }
+select quantilesApprox(100, 0.5)(number) from numbers(10);
+select quantilesApprox('abc', 0.5, 0.75)(number) from numbers(10); -- { serverError ILLEGAL_TYPE_OF_ARGUMENT }
+select quantilesApprox(1.23, 0.5, 0.75)(number) from numbers(10); -- { serverError ILLEGAL_TYPE_OF_ARGUMENT }
+select quantilesApprox(-100, 0.5, 0.75)(number) from numbers(10); -- { serverError BAD_ARGUMENTS }
+-- { echoOff }
