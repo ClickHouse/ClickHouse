79d4d029b87ebb9dd3cd61e3774ef16dbbc133a7
Igor Nikonov <igor@clickhouse.com>
2023-03-07 22:40:11 +0000

    Fix Distinct properties

--- src/Processors/QueryPlan/DistinctStep.cpp
+++ src/Processors/QueryPlan/DistinctStep.cpp
@@ -26,13 +26,14 @@ static bool checkColumnsAlreadyDistinct(const Names & columns, const NameSet & d
 
 static ITransformingStep::Traits getTraits(bool pre_distinct, bool already_distinct_columns)
 {
+    const bool preserves_number_of_streams = pre_distinct || already_distinct_columns;
     return ITransformingStep::Traits
     {
         {
             .preserves_distinct_columns = already_distinct_columns, /// Will be calculated separately otherwise
             .returns_single_stream = !pre_distinct && !already_distinct_columns,
-            .preserves_number_of_streams = pre_distinct || already_distinct_columns,
-            .preserves_sorting = true, /// Sorting is preserved indeed because of implementation.
+            .preserves_number_of_streams = preserves_number_of_streams,
+            .preserves_sorting = preserves_number_of_streams,
         },
         {
             .preserves_number_of_rows = false,
