e9e090f17ec75bfe3d0a35838a6281162c041e96
xiedeyantu <czjourney@163.com>
2023-03-08 21:57:33 +0800

    add test

--- tests/queries/0_stateless/25400_marked_dropped_tables.sql
+++ tests/queries/0_stateless/25400_marked_dropped_tables.sql
@@ -4,3 +4,4 @@ CREATE TABLE 25400_marked_dropped_tables (id Int32) Engine=MergeTree() ORDER BY 
 DROP TABLE 25400_marked_dropped_tables;
 
 SELECT table, engine FROM system.marked_dropped_tables LIMIT 1;
+DESCRIBE TABLE system.marked_dropped_tables;
