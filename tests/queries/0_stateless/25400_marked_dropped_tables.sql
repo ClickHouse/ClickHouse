995bff560f641e82ee4c9c168812ed5c3bc12fa6
xiedeyantu <czjourney@163.com>
2023-03-08 21:54:33 +0800

    add test

---
+++ tests/queries/0_stateless/25400_marked_dropped_tables.sql
@@ -0,0 +1,6 @@
+DROP TABLE IF EXISTS 25400_marked_dropped_tables;
+
+CREATE TABLE 25400_marked_dropped_tables (id Int32) Engine=MergeTree() ORDER BY id;
+DROP TABLE 25400_marked_dropped_tables;
+
+SELECT table, engine FROM system.marked_dropped_tables LIMIT 1;
