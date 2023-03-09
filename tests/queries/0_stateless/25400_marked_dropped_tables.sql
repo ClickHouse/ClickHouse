b64028862dda4fdbaa0905a28b7bfedd62df0566
chen <czjourney@163.com>
2023-03-10 06:17:30 +0800

    Update 25400_marked_dropped_tables.sql

--- tests/queries/0_stateless/25400_marked_dropped_tables.sql
+++ tests/queries/0_stateless/25400_marked_dropped_tables.sql
@@ -1,4 +1,4 @@
-SET database_atomic_wait_for_drop_and_detach_synchronously = 0
+SET database_atomic_wait_for_drop_and_detach_synchronously = 0;
 DROP TABLE IF EXISTS 25400_marked_dropped_tables;
 
 CREATE TABLE 25400_marked_dropped_tables (id Int32) Engine=MergeTree() ORDER BY id;
