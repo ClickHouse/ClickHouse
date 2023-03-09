acffc98bd1b8165bf2cf15ae031da055cdc1a9d5
Igor Nikonov <igor@clickhouse.com>
2023-03-09 12:22:32 +0000

    Fix build

--- src/Processors/QueryPlan/Optimizations/removeRedundantDistinct.cpp
+++ src/Processors/QueryPlan/Optimizations/removeRedundantDistinct.cpp
@@ -64,6 +64,37 @@ namespace
         return non_const_columns;
     }
 
+    const ActionsDAG::Node * getOriginalNodeForOutputAlias(const ActionsDAGPtr & actions, const String & output_name)
+    {
+        /// find alias in output
+        const ActionsDAG::Node * output_alias = nullptr;
+        for (const auto * node : actions->getOutputs())
+        {
+            if (node->result_name == output_name)
+            {
+                output_alias = node;
+                break;
+            }
+        }
+        if (!output_alias)
+        {
+            logDebug("getOriginalNodeForOutputAlias: no output alias found", output_name);
+            return nullptr;
+        }
+
+        /// find original(non alias) node it refers to
+        const ActionsDAG::Node * node = output_alias;
+        while (node && node->type == ActionsDAG::ActionType::ALIAS)
+        {
+            chassert(!node->children.empty());
+            node = node->children.front();
+        }
+        if (node && node->type != ActionsDAG::ActionType::INPUT)
+            return nullptr;
+
+        return node;
+    }
+
     bool compareAggregationKeysWithDistinctColumns(
         const Names & aggregation_keys, const DistinctColumns & distinct_columns, const ActionsDAGPtr & path_actions)
     {
