6e4fcbe3ef732f4c367f3d623cb9488861839c3f
Robert Schulze <robert@clickhouse.com>
2023-03-10 10:54:46 +0000

    Compile with C++23

--- src/Processors/Formats/Impl/PrettySpaceBlockOutputFormat.cpp
+++ src/Processors/Formats/Impl/PrettySpaceBlockOutputFormat.cpp
@@ -45,7 +45,7 @@ void PrettySpaceBlockOutputFormat::writeChunk(const Chunk & chunk, PortKind port
 
         if (col.type->shouldAlignRightInPrettyFormats())
         {
-            for (ssize_t k = 0; k < std::max(static_cast<ssize_t>(0), static_cast<ssize_t>(max_widths[i] - name_widths[i])); ++k)
+            for (ssize_t k = 0; k < std::max(0z, static_cast<ssize_t>(max_widths[i] - name_widths[i])); ++k)
                 writeChar(' ', out);
 
             if (format_settings.pretty.color)
@@ -62,7 +62,7 @@ void PrettySpaceBlockOutputFormat::writeChunk(const Chunk & chunk, PortKind port
             if (format_settings.pretty.color)
                 writeCString("\033[0m", out);
 
-            for (ssize_t k = 0; k < std::max(static_cast<ssize_t>(0), static_cast<ssize_t>(max_widths[i] - name_widths[i])); ++k)
+            for (ssize_t k = 0; k < std::max(0z, static_cast<ssize_t>(max_widths[i] - name_widths[i])); ++k)
                 writeChar(' ', out);
         }
     }
