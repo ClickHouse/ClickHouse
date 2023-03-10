73be76266a56d9e95a36037049c5493586b57765
Alexey Milovidov <milovidov@yandex-team.ru>
2023-03-10 05:01:06 +0300

    Update SchemaInferenceUtils.cpp

--- src/Formats/SchemaInferenceUtils.cpp
+++ src/Formats/SchemaInferenceUtils.cpp
@@ -984,7 +984,7 @@ DataTypePtr tryInferNumberFromString(std::string_view field, const FormatSetting
         if (tryReadIntText(tmp_int, buf) && buf.eof())
             return std::make_shared<DataTypeInt64>();
 
-        /// We cam safely get back to the start of buffer, because we read from a string and we didn't reach eof.
+        /// We can safely get back to the start of buffer, because we read from a string and we didn't reach eof.
         buf.position() = buf.buffer().begin();
 
         /// In case of Int64 overflow, try to infer UInt64
@@ -993,7 +993,7 @@ DataTypePtr tryInferNumberFromString(std::string_view field, const FormatSetting
             return std::make_shared<DataTypeUInt64>();
     }
 
-    /// We cam safely get back to the start of buffer, because we read from a string and we didn't reach eof.
+    /// We can safely get back to the start of buffer, because we read from a string and we didn't reach eof.
     buf.position() = buf.buffer().begin();
 
     Float64 tmp;
