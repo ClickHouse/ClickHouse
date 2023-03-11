8298fd333226ccfce3b6ee037b7fb662473e51f1
Alexander Tokmakov <tavplubix@clickhouse.com>
2023-03-11 18:54:20 +0300

    Update stress.py

--- tests/ci/stress.py
+++ tests/ci/stress.py
@@ -37,6 +37,7 @@ def get_options(i, upgrade_check):
         if join_alg_num % 5 == 2:
             client_options.append("join_algorithm='full_sorting_merge'")
         if join_alg_num % 5 == 3 and not upgrade_check:
+            # Some crashes are not fixed in 22.2 yet, so ignore the setting in Upgrade check
             client_options.append("join_algorithm='grace_hash'")
         if join_alg_num % 5 == 4:
             client_options.append("join_algorithm='auto'")
