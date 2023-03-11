a45014d30e07396fde7c554db8f05ff5c5e60956
flynn <fenglv15@mails.ucas.ac.cn>
2023-03-11 22:54:15 +0800

    Revert "Remove a feature"

--- tests/queries/0_stateless/02678_explain_pipeline_graph.sql
+++
@@ -1,7 +0,0 @@
--- The server does not crash after these queries:
-
-DROP TABLE IF EXISTS t1;
-CREATE TABLE t1(ID UInt64, name String) engine=MergeTree order by ID;
-insert into t1(ID, name) values (1, 'abc'), (2, 'bbb');
-explain pipeline graph=1 select count(ID) from t1 FORMAT Null;
-DROP TABLE t1;
