80d248aa06c36e56fb8b019fa90ea6a4542f62f7
Antonio Andelic <antonio@clickhouse.com>
2023-03-13 15:52:35 +0000

    Finish first part of CNF

--- src/Analyzer/Passes/CNF.cpp
+++ src/Analyzer/Passes/CNF.cpp
@@ -9,6 +9,7 @@
 #include <Functions/FunctionFactory.h>
 
 #include <Common/checkStackSize.h>
+#include "Interpreters/ActionsDAG.h"
 
 namespace DB::Analyzer
 {
@@ -22,55 +23,60 @@ bool isLogicalFunction(const FunctionNode & function_node)
     return name == "and" || name == "or" || name == "not";
 }
 
-class SplitMultiLogicVisitor : public InDepthQueryTreeVisitorWithContext<SplitMultiLogicVisitor>
+template <typename... Args>
+QueryTreeNodePtr createFunctionNode(const FunctionOverloadResolverPtr & function_resolver, Args &&... args)
+{
+    auto function_node = std::make_shared<FunctionNode>(function_resolver->getName());
+    auto & new_arguments = function_node->getArguments().getNodes();
+    new_arguments.reserve(sizeof...(args));
+    (new_arguments.push_back(std::forward<Args>(args)), ...);
+    function_node->resolveAsFunction(function_resolver);
+    return function_node;
+}
+
+size_t countAtoms(const QueryTreeNodePtr & node)
+{
+    checkStackSize();
+
+    const auto * function_node = node->as<FunctionNode>();
+    if (!function_node || !isLogicalFunction(*function_node))
+        return 1;
+
+    size_t atom_count = 0;
+    const auto & arguments = function_node->getArguments().getNodes();
+    for (const auto & argument : arguments)
+        atom_count += countAtoms(argument);
+
+    return atom_count;
+}
+
+class SplitMultiLogicVisitor
 {
 public:
-    using Base = InDepthQueryTreeVisitorWithContext<SplitMultiLogicVisitor>;
-
     explicit SplitMultiLogicVisitor(ContextPtr context)
-        : Base(std::move(context))
+        : current_context(std::move(context))
     {}
 
-    static bool needChildVisit(QueryTreeNodePtr & parent, QueryTreeNodePtr &)
+    void visit(QueryTreeNodePtr & node)
     {
-        auto * function_node = parent->as<FunctionNode>();
+        checkStackSize();
 
-        if (!function_node)
-            return false;
-
-        return isLogicalFunction(*function_node);
-    }
-
-    void visitImpl(QueryTreeNodePtr & node)
-    {
         auto * function_node = node->as<FunctionNode>();
         if (!function_node || !isLogicalFunction(*function_node))
-        {
-            ++atom_count;
             return;
-        }
 
         const auto & name = function_node->getFunctionName();
 
         if (name == "and" || name == "or")
         {
-            auto function_resolver = FunctionFactory::instance().get(name, getContext());
+            auto function_resolver = FunctionFactory::instance().get(name, current_context);
 
             const auto & arguments = function_node->getArguments().getNodes();
             if (arguments.size() > 2)
             {
                 QueryTreeNodePtr current = arguments[0];
                 for (size_t i = 1; i < arguments.size(); ++i)
-                {
-                    QueryTreeNodes new_arguments;
-                    new_arguments.reserve(2);
-                    new_arguments.push_back(std::move(current));
-                    new_arguments.push_back(arguments[i]);
-                    auto new_function_node = std::make_shared<FunctionNode>();
-                    new_function_node->getArguments().getNodes() = std::move(new_arguments);
-                    new_function_node->resolveAsFunction(function_resolver);
-                    current = std::move(new_function_node);
-                }
+                    current = createFunctionNode(function_resolver, std::move(current), arguments[i]);
 
                 auto & new_function_node = current->as<FunctionNode &>();
                 function_node->getArguments().getNodes() = std::move(new_function_node.getArguments().getNodes());
@@ -81,9 +87,185 @@ public:
         {
             assert(name == "not");
         }
+
+        auto & arguments = function_node->getArguments().getNodes();
+        for (auto & argument : arguments)
+            visit(argument);
     }
 
-    size_t atom_count = 0;
+private:
+    ContextPtr current_context;
+};
+
+class PushNotVisitor
+{
+public:
+    explicit PushNotVisitor(ContextPtr context)
+        : current_context(std::move(context))
+    {}
+
+    void visit(QueryTreeNodePtr & node, bool add_negation)
+    {
+        checkStackSize();
+
+        auto * function_node = node->as<FunctionNode>();
+
+        if (!function_node || !isLogicalFunction(*function_node))
+        {
+            if (add_negation)
+                node = createFunctionNode(FunctionFactory::instance().get("not", current_context), std::move(node));
+            return;
+        }
+
+        std::string_view function_name = function_node->getFunctionName();
+        if (function_name == "and" || function_name == "or")
+        {
+            if (add_negation)
+            {
+                auto function_resolver = FunctionFactory::instance().get(function_name == "and" ? "or" : "and", current_context);
+                function_node->resolveAsFunction(function_resolver);
+            }
+
+            auto & arguments = function_node->getArguments().getNodes();
+            for (auto & argument : arguments)
+                visit(argument, add_negation);
+            return;
+        }
+
+        assert(function_name == "not");
+        auto & arguments = function_node->getArguments().getNodes();
+        assert(arguments.size() == 1);
+        node = arguments[0];
+        visit(node, !add_negation);
+    }
+
+private:
+    ContextPtr current_context;
+};
+
+class PushOrVisitor
+{
+public:
+    PushOrVisitor(ContextPtr context, size_t max_atoms_, size_t num_atoms_)
+        : max_atoms(max_atoms_)
+        , num_atoms(num_atoms_)
+        , and_resolver(FunctionFactory::instance().get("and", context))
+        , or_resolver(FunctionFactory::instance().get("or", context))
+    {}
+
+    bool visit(QueryTreeNodePtr & node)
+    {
+        if (max_atoms && num_atoms > max_atoms)
+            return false;
+
+        checkStackSize();
+
+        auto * function_node = node->as<FunctionNode>();
+
+        if (!function_node)
+            return true;
+
+        std::string_view name = function_node->getFunctionName();
+
+        if (name == "or" || name == "and")
+        {
+            auto & arguments = function_node->getArguments().getNodes();
+            for (auto & argument : arguments)
+                visit(argument);
+        }
+
+        if (name == "or")
+        {
+            auto & arguments = function_node->getArguments().getNodes();
+            assert(arguments.size() == 2);
+
+            size_t and_node_id = arguments.size();
+
+            for (size_t i = 0; i < arguments.size(); ++i)
+            {
+                auto & argument = arguments[i];
+                if (auto * argument_function_node = argument->as<FunctionNode>();
+                    argument_function_node && argument_function_node->getFunctionName() == "and")
+                    and_node_id = i;
+            }
+
+            if (and_node_id == arguments.size())
+                return true;
+
+            auto & other_node = arguments[1 - and_node_id];
+            auto & and_function_arguments = arguments[and_node_id]->as<FunctionNode &>().getArguments().getNodes();
+
+            auto lhs = createFunctionNode(or_resolver, other_node->clone(), std::move(and_function_arguments[0]));
+            num_atoms += countAtoms(other_node);
+
+            auto rhs = createFunctionNode(or_resolver, std::move(other_node), std::move(and_function_arguments[1]));
+            node = createFunctionNode(and_resolver, std::move(lhs), std::move(rhs));
+
+            visit(node);
+        }
+
+        return true;
+    }
+
+private:
+    size_t max_atoms;
+    size_t num_atoms;
+
+    const FunctionOverloadResolverPtr and_resolver;
+    const FunctionOverloadResolverPtr or_resolver;
+};
+
+class CollectGroupsVisitor
+{
+public:
+    void visit(QueryTreeNodePtr & node)
+    {
+        CNF::OrGroup or_group;
+        visitImpl(node, or_group);
+        if (!or_group.empty())
+            and_group.insert(std::move(or_group));
+    }
+
+    CNF::AndGroup and_group;
+
+private:
+    void visitImpl(QueryTreeNodePtr & node, CNF::OrGroup & or_group)
+    {
+        checkStackSize();
+
+        auto * function_node = node->as<FunctionNode>();
+        if (!function_node || !isLogicalFunction(*function_node))
+        {
+            or_group.insert(CNF::AtomicFormula{false, std::move(node)});
+            return;
+        }
+
+        std::string_view name = function_node->getFunctionName();
+
+        if (name == "and")
+        {
+            auto & arguments = function_node->getArguments().getNodes();
+            for (auto & argument : arguments)
+            {
+                CNF::OrGroup argument_or_group;
+                visitImpl(argument, argument_or_group);
+                if (!argument_or_group.empty())
+                    and_group.insert(std::move(argument_or_group));
+            }
+        }
+        else if (name == "or")
+        {
+            auto & arguments = function_node->getArguments().getNodes();
+            for (auto & argument : arguments)
+                visitImpl(argument, or_group);
+        }
+        else
+        {
+            assert(name == "not");
+            auto & arguments = function_node->getArguments().getNodes();
+            or_group.insert(CNF::AtomicFormula{true, std::move(arguments[0])});
+        }
+    }
 };
 
 }
@@ -91,6 +273,13 @@ public:
 bool CNF::AtomicFormula::operator==(const AtomicFormula & rhs) const
 {
     return negative == rhs.negative && node_with_hash == rhs.node_with_hash;
+}
+
+bool CNF::AtomicFormula::operator<(const AtomicFormula & rhs) const
+{
+    return node_with_hash.hash == rhs.node_with_hash.hash
+        ? negative < rhs.negative
+        : node_with_hash.hash < rhs.node_with_hash.hash;
 }
 
 std::string CNF::dump() const
@@ -119,13 +308,171 @@ std::string CNF::dump() const
     return res.str();
 }
 
-std::optional<CNF> CNF::tryBuildCNF(const QueryTreeNodePtr & node, ContextPtr context, size_t)
+CNF & CNF::transformGroups(std::function<OrGroup(const OrGroup &)> fn)
 {
+    AndGroup result;
+
+    for (const auto & group : statements)
+    {
+        auto new_group = fn(group);
+        if (!new_group.empty())
+            result.insert(std::move(new_group));
+    }
+
+    statements = std::move(result);
+    return *this;
+}
+
+CNF & CNF::transformAtoms(std::function<AtomicFormula(const AtomicFormula &)> fn)
+{
+    transformGroups([fn](const OrGroup & group)
+    {
+        OrGroup result;
+        for (const auto & atom : group)
+        {
+            auto new_atom = fn(atom);
+            if (new_atom.node_with_hash.node)
+                result.insert(std::move(new_atom));
+        }
+
+        return result;
+    });
+
+    return *this;
+}
+
+CNF & CNF::pushNotIntoFunctions(const ContextPtr & context)
+{
+    transformAtoms([&](const AtomicFormula & atom)
+    {
+		if (!atom.negative)
+		    return atom;
+
+		static const std::unordered_map<std::string, std::string> inverse_relations = {
+		    {"equals", "notEquals"},
+		    {"less", "greaterOrEquals"},
+		    {"lessOrEquals", "greater"},
+		    {"in", "notIn"},
+		    {"like", "notLike"},
+		    {"empty", "notEmpty"},
+		    {"notEquals", "equals"},
+		    {"greaterOrEquals", "less"},
+		    {"greater", "lessOrEquals"},
+		    {"notIn", "in"},
+		    {"notLike", "like"},
+		    {"notEmpty", "empty"},
+		};
+
+		auto * function_node = atom.node_with_hash.node->as<FunctionNode>();
+		if (!function_node)
+			return atom;
+
+		if (auto it = inverse_relations.find(function_node->getFunctionName()); it != inverse_relations.end())
+        {
+			auto inverse_function_resolver = FunctionFactory::instance().get(it->second, context);
+			function_node->resolveAsFunction(inverse_function_resolver);
+			return AtomicFormula{!atom.negative, atom.node_with_hash.node};
+		}
+
+		return atom;
+    });
+
+    std::cout << "gorup: " << statements.size() << std::endl;
+
+    return *this;
+}
+
+CNF::CNF(AndGroup statements_)
+    : statements(std::move(statements_))
+{}
+
+std::optional<CNF> CNF::tryBuildCNF(const QueryTreeNodePtr & node, ContextPtr context, size_t max_growth_multiplier)
+{
+    auto * function_node = node->as<FunctionNode>();
+
+    if (!function_node || !isLogicalFunction(*function_node))
+        throw Exception(ErrorCodes::LOGICAL_ERROR, "Cannot convert nodes that are not logical functions to CNF");
+
     auto node_cloned = node->clone();
-    SplitMultiLogicVisitor split_visitor(std::move(context));
-    split_visitor.visit(node_cloned);
-//    size_t num_atoms = countAtoms(node);
-    return std::nullopt;
+
+    size_t atom_count = countAtoms(node_cloned);
+    size_t max_atoms = max_growth_multiplier ? std::max(MAX_ATOMS_WITHOUT_CHECK, atom_count * max_growth_multiplier) : 0;
+
+    {
+        SplitMultiLogicVisitor visitor(context);
+        visitor.visit(node_cloned);
+    }
+
+    {
+        PushNotVisitor visitor(context);
+        visitor.visit(node_cloned, false);
+    }
+
+    if (PushOrVisitor visitor(context, max_atoms, atom_count);
+        !visitor.visit(node_cloned))
+            return std::nullopt;
+
+    CollectGroupsVisitor collect_visitor;
+    collect_visitor.visit(node_cloned);
+
+    if (collect_visitor.and_group.empty())
+        return std::nullopt;
+
+    return CNF{std::move(collect_visitor.and_group)};
+}
+
+QueryTreeNodePtr CNF::toQueryTree(ContextPtr context) const
+{
+	if (statements.empty())
+		return nullptr;
+
+    QueryTreeNodes and_arguments;
+    and_arguments.reserve(statements.size());
+
+    auto not_resolver = FunctionFactory::instance().get("not", context);
+    auto or_resolver = FunctionFactory::instance().get("or", context);
+    auto and_resolver = FunctionFactory::instance().get("and", context);
+
+    const auto function_node_from_atom = [&](const auto & atom) -> QueryTreeNodePtr
+    {
+        auto cloned_node = atom.node_with_hash.node->clone();
+        if (atom.negative)
+            return createFunctionNode(not_resolver, std::move(cloned_node));
+
+        return std::move(cloned_node);
+    };
+
+    for (const auto & or_group : statements)
+    {
+        if (or_group.size() == 1)
+        {
+            const auto & atom = *or_group.begin();
+            and_arguments.push_back(function_node_from_atom(atom));
+        }
+        else
+        {
+            QueryTreeNodes or_arguments;
+            or_arguments.reserve(or_group.size());
+
+            for (const auto & atom : or_group)
+                or_arguments.push_back(function_node_from_atom(atom));
+
+            auto or_function = std::make_shared<FunctionNode>("or");
+            or_function->getArguments().getNodes() = std::move(or_arguments);
+            or_function->resolveAsFunction(or_resolver);
+
+            and_arguments.push_back(std::move(or_function));
+        }
+    }
+
+    if (and_arguments.size() == 1)
+        return std::move(and_arguments[0]);
+
+    auto and_function = std::make_shared<FunctionNode>("and");
+    and_function->getArguments().getNodes() = std::move(and_arguments);
+    and_function->resolveAsFunction(and_resolver);
+
+    return and_function;
 }
 
 }
