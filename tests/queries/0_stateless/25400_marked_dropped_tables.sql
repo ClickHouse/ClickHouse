84136ddc3c5e0bfda639dab330e7f0217c13a26e
xiedeyantu <czjourney@163.com>
2023-03-15 00:56:59 +0800

    fix test

--- tests/queries/0_stateless/25400_marked_dropped_tables.sql
+++ tests/queries/0_stateless/25400_marked_dropped_tables.sql
@@ -1,4 +1,4 @@
--- Tags: no-ordinary-database, no-parallel
+-- Tags: no-ordinary-database
 
 SET database_atomic_wait_for_drop_and_detach_synchronously = 0;
 DROP TABLE IF EXISTS 25400_marked_dropped_tables;
@@ -6,6 +6,6 @@ DROP TABLE IF EXISTS 25400_marked_dropped_tables;
 CREATE TABLE 25400_marked_dropped_tables (id Int32) Engine=MergeTree() ORDER BY id;
 DROP TABLE 25400_marked_dropped_tables;
 
-SELECT table, engine FROM system.marked_dropped_tables LIMIT 1;
+SELECT table, engine FROM system.marked_dropped_tables WHERE database = currentDatabase() LIMIT 1;
 DESCRIBE TABLE system.marked_dropped_tables;
  
