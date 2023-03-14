b0d345e2f319736b0f670429fa95f4ed0b96a39f
Yatsishin Ilya <2159081+qoega@users.noreply.github.com>
2023-03-14 11:23:12 +0000

    revert Poco HelpFormatter default width - try to split tests and run separately

--- tests/queries/0_stateless/02598_clickhouse_help_program_options.sh
+++ tests/queries/0_stateless/02598_clickhouse_help_program_options.sh
@@ -10,41 +10,42 @@ CURDIR=$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)
 # In CI there is no tty and we just ignore failed stty calls.
 # Set 80 to have same as default size as in notty.
 backup_stty_size=$(stty size 2>/dev/null | awk '{print $2}' ||:)
-stty columns 78 2>/dev/null ||:
+stty columns 80 2>/dev/null ||:
 
 echo "================BINARY=========================="
 
 echo -e "\nclickhouse --help\n"
 $CLICKHOUSE_BINARY --help
-
 echo -e "\nclickhouse help\n"
 $CLICKHOUSE_BINARY help
-echo -e "\nclickhouse local\n"
-$CLICKHOUSE_BINARY local --help | perl -0777 -pe 's/Main options:.*\n\n//igs'
-echo -e "\nclickhouse client\n"
-$CLICKHOUSE_BINARY client --help | perl -0777 -pe 's/Main options:.*\n\n//igs'
+
 echo -e "\nclickhouse benchmark\n"
 $CLICKHOUSE_BINARY benchmark --help | perl -0777 -pe 's/Allowed options:.*\n\n//igs' 
-echo -e "\nclickhouse server\n"
-$CLICKHOUSE_BINARY server --help | perl -0777 -pe 's/Main options:.*\n\n//igs'
-echo -e "\nclickhouse extract\n"
-$CLICKHOUSE_BINARY extract-from-config --help
+echo -e "\nclickhouse client\n"
+$CLICKHOUSE_BINARY client --help | perl -0777 -pe 's/Main options:.*\n\n//igs'
+echo -e "\nclickhouse local\n"
+$CLICKHOUSE_BINARY local --help | perl -0777 -pe 's/Main options:.*\n\n//igs'
 echo -e "\nclickhouse compressor\n"
 $CLICKHOUSE_BINARY compressor --help
+echo -e "\nclickhouse disks\n"
+$CLICKHOUSE_BINARY disks --help | perl -0777 -pe 's/Main options:.*\n\n//igs'
+echo -e "\nclickhouse extract\n"
+$CLICKHOUSE_BINARY extract-from-config --help
 echo -e "\nclickhouse format\n"
 $CLICKHOUSE_BINARY format --help
-echo -e "\nclickhouse copier\n"
-$CLICKHOUSE_BINARY copier --help
-echo -e "\nclickhouse obfuscator\n"
-$CLICKHOUSE_BINARY obfuscator --help
 echo -e "\nclickhouse git-import\n"
 $CLICKHOUSE_BINARY git-import --help
-echo -e "\nclickhouse keeper\n"
-$CLICKHOUSE_BINARY keeper --help
-echo -e "\nclickhouse keeper-converter\n"
-$CLICKHOUSE_BINARY keeper-converter --help
 echo -e "\nclickhouse install\n"
 $CLICKHOUSE_BINARY install --help
+echo -e "\nclickhouse keeper-converter\n"
+$CLICKHOUSE_BINARY keeper-converter --help
+echo -e "\nclickhouse obfuscator\n"
+$CLICKHOUSE_BINARY obfuscator --help
+echo -e "\nclickhouse static\n"
+$CLICKHOUSE_BINARY static-files-disk-uploader --help
+
+
+
 echo -e "\nclickhouse start\n"
 $CLICKHOUSE_BINARY start --help
 echo -e "\nclickhouse stop\n"
@@ -53,14 +54,10 @@ echo -e "\nclickhouse status\n"
 $CLICKHOUSE_BINARY status --help
 echo -e "\nclickhouse restart\n"
 $CLICKHOUSE_BINARY restart --help
-echo -e "\nclickhouse static\n"
-$CLICKHOUSE_BINARY static-files-disk-uploader --help
 echo -e "\nclickhouse su\n"
 $CLICKHOUSE_BINARY su --help
 echo -e "\nclickhouse hash\n"
 $CLICKHOUSE_BINARY hash-binary --help | grep -v 'Current binary hash'
-echo -e "\nclickhouse disks\n"
-$CLICKHOUSE_BINARY disks --help | perl -0777 -pe 's/Main options:.*\n\n//igs'
 
 echo "================SYMLINK=============================="
 
@@ -70,22 +67,16 @@ echo -e "\nclickhouse-client\n"
 ${CLICKHOUSE_BINARY}-client --help | perl -0777 -pe 's/Main options:.*\n\n//igs'
 echo -e "\nclickhouse-benchmark\n"
 ${CLICKHOUSE_BINARY}-benchmark --help | perl -0777 -pe 's/Allowed options:.*\n\n//igs' 
-echo -e "\nclickhouse-server\n"
-${CLICKHOUSE_BINARY}-server --help | perl -0777 -pe 's/Main options:.*\n\n//igs'
 echo -e "\nclickhouse-extract\n"
 ${CLICKHOUSE_BINARY}-extract-from-config --help
 echo -e "\nclickhouse-compressor\n"
 ${CLICKHOUSE_BINARY}-compressor --help
 echo -e "\nclickhouse-format\n"
 ${CLICKHOUSE_BINARY}-format --help
-echo -e "\nclickhouse-copier\n"
-${CLICKHOUSE_BINARY}-copier --help
 echo -e "\nclickhouse-obfuscator\n"
 ${CLICKHOUSE_BINARY}-obfuscator --help
 echo -e "\nclickhouse-git-import\n"
 ${CLICKHOUSE_BINARY}-git-import --help
-echo -e "\nclickhouse-keeper\n"
-${CLICKHOUSE_BINARY}-keeper --help
 echo -e "\nclickhouse-keeper-converter\n"
 ${CLICKHOUSE_BINARY}-keeper-converter --help
 echo -e "\nclickhouse-static-files-disk-uploader\n"
