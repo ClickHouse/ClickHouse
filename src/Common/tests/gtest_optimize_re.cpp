1149b3312bb9b65a4c31a0da8ebfa5ac5ca31d07
Han Fei <hanfei19910905@gmail.com>
2023-03-14 17:44:02 +0100

    fix test

--- src/Common/tests/gtest_optimize_re.cpp
+++ src/Common/tests/gtest_optimize_re.cpp
@@ -4,7 +4,7 @@
 
 TEST(OptimizeRE, analyze)
 {
-    auto test_f = [](const std::string & regexp, const std::string & answer, std::vector<std::string> expect_alternatives = {})
+    auto test_f = [](const std::string & regexp, const std::string & answer, std::vector<std::string> expect_alternatives = {}, bool trival_expected = false)
     {
         std::string required;
         bool is_trivial;
@@ -14,8 +14,10 @@ TEST(OptimizeRE, analyze)
         std::cerr << regexp << std::endl;
         EXPECT_EQ(required, answer);
         EXPECT_EQ(alternatives, expect_alternatives);
+        EXPECT_EQ(is_trivial, trival_expected);
     };
-    test_f("abc", "abc");
+    test_f("abc", "abc", {}, true);
+    test_f("c([^k]*)de", "");
     test_f("abc(de)fg", "abcdefg");
     test_f("abc(de|xyz)fg", "abc", {"abcdefg", "abcxyzfg"});
     test_f("abc(de?f|xyz)fg", "abc", {"abcd", "abcxyzfg"});
@@ -35,8 +37,8 @@ TEST(OptimizeRE, analyze)
     test_f(R"(abc(de|xyz|(\{xx\}))fg)", "abc", {"abcdefg", "abcxyzfg", "abc{xx}fg"});
     test_f("abc(abc|fg)?xyzz", "xyzz");
     test_f("abc(abc|fg){0,1}xyzz", "xyzz");
-    test_f("abc(abc|fg)xyzz|bcdd?k|bc(f|g|h?)z", "", {"abcabcxyzz", "abcfgxyzz", "bcd", "bcfz", "bcgz", "bcz"});
-    test_f("abc(abc|fg)xyzz|bc(dd?x|kk?y|(f))k|bc(f|g|h?)z", "", {"abcabcxyzz", "abcfgxyzz", "bcd", "bck", "bcfk", "bcfz", "bcgz", "bcz"});
+    test_f("abc(abc|fg)xyzz|bcdd?k|bc(f|g|h?)z", "", {"abcabcxyzz", "abcfgxyzz", "bcd", "bcfz", "bcgz", ""});
+    test_f("abc(abc|fg)xyzz|bc(dd?x|kk?y|(f))k|bc(f|g|h?)z", "", {"abcabcxyzz", "abcfgxyzz", "bcd", "bck", "bcfk", "bcfz", "bcgz", ""});
     test_f("((?:abc|efg|xyz)/[a-zA-Z0-9]{1-50})(/?[^ ]*|)", "", {"abc/", "efg/", "xyz/"});
     test_f(R"([Bb]ai[Dd]u[Ss]pider(?:-[A-Za-z]{1,30})(?:-[A-Za-z]{1,30}|)|bingbot|\bYeti(?:-[a-z]{1,30}|)|Catchpoint(?: bot|)|[Cc]harlotte|Daumoa(?:-feedfetcher|)|(?:[a-zA-Z]{1,30}-|)Googlebot(?:-[a-zA-Z]{1,30}|))", "", {"pider-", "bingbot", "Yeti-", "Yeti", "Catchpoint bot", "Catchpoint", "harlotte", "Daumoa-feedfetcher", "Daumoa", "Googlebot-", "Googlebot"});
 }
