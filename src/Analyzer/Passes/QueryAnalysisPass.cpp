6c2084052c28f1e63f12e59396b68fe17ad6cdb6
Maksim Kita <kitaetoya@gmail.com>
2023-03-14 21:50:40 +0100

    Fixed tests

--- src/Analyzer/Passes/QueryAnalysisPass.cpp
+++ src/Analyzer/Passes/QueryAnalysisPass.cpp
@@ -111,7 +111,6 @@ namespace ErrorCodes
     extern const int NOT_IMPLEMENTED;
     extern const int ALIAS_REQUIRED;
     extern const int NUMBER_OF_ARGUMENTS_DOESNT_MATCH;
-    extern const int ILLEGAL_PREWHERE;
     extern const int UNKNOWN_TABLE;
 }
 
