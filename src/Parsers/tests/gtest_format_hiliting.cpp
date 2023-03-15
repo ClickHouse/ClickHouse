bf4a3a23fa0c4f4dbd57d5df41507ac70e976ca6
natasha <murfel@users.noreply.github.com>
2023-03-15 23:56:20 +0000

    compare string without hilites first, for easier debug

--- src/Parsers/tests/gtest_format_hiliting.cpp
+++ src/Parsers/tests/gtest_format_hiliting.cpp
@@ -45,45 +45,76 @@ std::string substitution(const std::string & s)
     return hilite(s, DB::IAST::hilite_substitution);
 }
 
+std::vector<const char *> HILITES =
+    {
+        DB::IAST::hilite_keyword,
+        DB::IAST::hilite_identifier,
+        DB::IAST::hilite_function,
+        DB::IAST::hilite_operator,
+        DB::IAST::hilite_alias,
+        DB::IAST::hilite_substitution,
+        DB::IAST::hilite_none
+    };
+
+[[maybe_unused]] const char * consume_hilites(const char ** it)
+{
+    const char * last_hilite = nullptr;
+    while (true)
+    {
+        bool changed_hilite = false;
+        for (const char * hilite : HILITES)
+        {
+            if (std::string_view(*it).starts_with(hilite))
+            {
+                *it += strlen(hilite);
+                changed_hilite = true;
+                last_hilite = hilite;
+            }
+        }
+        if (!changed_hilite)
+            break;
+    }
+    return last_hilite;
+}
+
+std::string remove_hilites(const std::string_view & string)
+{
+    const char * it = string.begin();
+    std::stringstream ss;
+    while (true)
+    {
+        consume_hilites(&it);
+        if (it == string.end())
+            return ss.str();
+        ss << *(it++);
+    }
+}
+
+bool are_equal_with_hilites_removed(const std::string_view & left, const std::string_view & right)
+{
+    return remove_hilites(left) == remove_hilites(right);
+}
 
 bool are_equal_with_hilites(const std::string_view & left, const std::string_view & right)
 {
+    if (!are_equal_with_hilites_removed(left, right))
+        return false;
+
     const char * left_it = left.begin();
     const char * right_it = right.begin();
     const char * left_hilite = DB::IAST::hilite_none;
     const char * right_hilite = DB::IAST::hilite_none;
-    std::unordered_set<const char *> hilites
-        = {DB::IAST::hilite_keyword,
-           DB::IAST::hilite_identifier,
-           DB::IAST::hilite_alias,
-           DB::IAST::hilite_function,
-           DB::IAST::hilite_operator,
-           DB::IAST::hilite_substitution,
-           DB::IAST::hilite_none};
 
     while (true)
     {
-        // Consume hilites.
-        bool changed_hilite = false;
-        do
-        {
-            changed_hilite = false;
-            for (const char * hilite : hilites)
-            {
-                if (std::string_view(left_it, left.end()).starts_with(hilite))
-                {
-                    left_hilite = hilite;
-                    left_it += strlen(hilite);
-                    changed_hilite = true;
-                }
-                if (std::string_view(right_it, right.end()).starts_with(hilite))
-                {
-                    right_hilite = hilite;
-                    right_it += strlen(hilite);
-                    changed_hilite = true;
-                }
-            }
-        } while (changed_hilite);
+        // Consume all prefix hilites, update the current hilite to be the last one.
+        const char * last_hilite = consume_hilites(&left_it);
+        if (last_hilite != nullptr)
+            left_hilite = last_hilite;
+
+        last_hilite = consume_hilites(&right_it);
+        if (last_hilite != nullptr)
+            right_hilite = last_hilite;
 
         if (left_it == left.end() && right_it == right.end())
             return true;
@@ -93,6 +124,7 @@ bool are_equal_with_hilites(const std::string_view & left, const std::string_vie
 
         // Lookup one character.
         // Check characters match.
+        // Redundant check, given the hilite-ignorant comparison at the beginning, but let's keep it just in case.
         if (*left_it != *right_it)
             return false;
 
@@ -106,19 +138,44 @@ bool are_equal_with_hilites(const std::string_view & left, const std::string_vie
     }
 }
 
+TEST(FormatHiliting, MetaTestConsumeHilites)
+{
+    using namespace DB;
+    std::stringstream ss;
+    // The order is different from the order in HILITES on purpose.
+    ss << IAST::hilite_keyword
+       << IAST::hilite_alias
+       << IAST::hilite_identifier
+       << IAST::hilite_none
+       << IAST::hilite_operator
+       << IAST::hilite_substitution
+       << IAST::hilite_function
+       << "test" << IAST::hilite_keyword;
+    std::string string = ss.str();
+    const char * it = string.c_str();
+    const char * expected_it = strchr(it, 't');
+    const char * last_hilite = consume_hilites(&it);
+    ASSERT_EQ(expected_it, it);
+    ASSERT_TRUE(last_hilite != nullptr);
+    ASSERT_EQ(IAST::hilite_function, last_hilite);
+}
+
+TEST(FormatHiliting, MetaTestRemoveHilites)
+{
+    using namespace DB;
+    std::stringstream ss;
+    ss << IAST::hilite_keyword
+       << "te" << IAST::hilite_alias << IAST::hilite_identifier
+       << "s" << IAST::hilite_none
+       << "t" << IAST::hilite_operator << IAST::hilite_substitution << IAST::hilite_function;
+    ASSERT_EQ("test", remove_hilites(ss.str()));
+}
+
 TEST(FormatHiliting, MetaTestAreEqualWithHilites)
 {
     ASSERT_PRED2(are_equal_with_hilites, "", "");
 
-    std::unordered_set<const char *> hilites
-        = {DB::IAST::hilite_keyword,
-           DB::IAST::hilite_identifier,
-           DB::IAST::hilite_alias,
-           DB::IAST::hilite_function,
-           DB::IAST::hilite_operator,
-           DB::IAST::hilite_substitution,
-           DB::IAST::hilite_none};
-    for (const char * hilite : hilites)
+    for (const char * hilite : HILITES)
     {
         ASSERT_PRED2(are_equal_with_hilites, "", std::string_view(hilite));
         ASSERT_PRED2(are_equal_with_hilites, std::string_view(hilite), "");
