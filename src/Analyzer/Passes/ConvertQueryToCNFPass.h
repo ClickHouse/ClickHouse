7f8622752c3d34b64a1fb371d4fae6f8128469cf
Antonio Andelic <antonio@clickhouse.com>
2023-03-17 14:17:16 +0000

    Update description

--- src/Analyzer/Passes/ConvertQueryToCNFPass.h
+++ src/Analyzer/Passes/ConvertQueryToCNFPass.h
@@ -8,9 +8,9 @@ namespace DB
 class ConvertQueryToCNFPass final : public IQueryTreePass
 {
 public:
-    String getName() override { return "ConvertQueryToCnfPass"; }
+    String getName() override { return "ConvertQueryToCNFPass"; }
 
-    String getDescription() override { return "Convert query to CNF"; }
+    String getDescription() override { return "Convert query to CNF and apply optimizations using constraints"; }
 
     void run(QueryTreeNodePtr query_tree_node, ContextPtr context) override;
 };
