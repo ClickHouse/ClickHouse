f7218a497fc87c4373ea2c8d009f377d287e0185
Han Fei <hanfei19910905@gmail.com>
2023-03-18 00:58:32 +0100

    fix test

--- tests/queries/0_stateless/25341_deduplication_in_mv.sh
+++ tests/queries/0_stateless/25341_deduplication_in_mv.sh
@@ -6,8 +6,8 @@ CURDIR=$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)
 
 $CLICKHOUSE_CLIENT --query="DROP DATABASE IF EXISTS test SYNC";
 $CLICKHOUSE_CLIENT --query="CREATE DATABASE test";
-$CLICKHOUSE_CLIENT --query="CREATE TABLE test.landing (time DateTime, number Int64) Engine=ReplicatedReplacingMergeTree('/clickhouse/tables/{layer}-{shard}/test.landing','{replica}') PARTITION BY toYYYYMMDD(time) ORDER BY time";
-$CLICKHOUSE_CLIENT --query="CREATE MATERIALIZED VIEW IF NOT EXISTS test.mv ENGINE = ReplicatedSummingMergeTree('/clickhouse/tables/{layer}-{shard}/test.mv','{replica}') PARTITION BY toYYYYMMDD(hour) ORDER BY hour AS SELECT toStartOfHour(time) AS hour, sum(number) AS sum_amount FROM test.landing GROUP BY hour";
+$CLICKHOUSE_CLIENT --query="CREATE TABLE test.landing (time DateTime, number Int64) Engine=ReplicatedReplacingMergeTree('/clickhouse/tables/{shard}/{database}/test.landing','{replica}') PARTITION BY toYYYYMMDD(time) ORDER BY time";
+$CLICKHOUSE_CLIENT --query="CREATE MATERIALIZED VIEW IF NOT EXISTS test.mv ENGINE = ReplicatedSummingMergeTree('/clickhouse/tables/{shard}/{database}/test.mv','{replica}') PARTITION BY toYYYYMMDD(hour) ORDER BY hour AS SELECT toStartOfHour(time) AS hour, sum(number) AS sum_amount FROM test.landing GROUP BY hour";
 
 printf '2023-09-01 12:23:34, 42,
 '| $CLICKHOUSE_CLIENT --query="INSERT INTO test.landing (*) settings max_insert_delayed_streams_for_parallel_write=0 FORMAT CSV";
@@ -22,8 +22,8 @@ $CLICKHOUSE_CLIENT --query="""SELECT * FROM test.mv FINAL ORDER BY hour""";
 
 $CLICKHOUSE_CLIENT --query="DROP DATABASE IF EXISTS test SYNC";
 $CLICKHOUSE_CLIENT --query="CREATE DATABASE test";
-$CLICKHOUSE_CLIENT --query="CREATE TABLE test.landing (time DateTime, number Int64) Engine=ReplicatedReplacingMergeTree('/clickhouse/tables/{layer}-{shard}/test.landing','{replica}') PARTITION BY toYYYYMMDD(time) ORDER BY time";
-$CLICKHOUSE_CLIENT --query="CREATE MATERIALIZED VIEW IF NOT EXISTS test.mv ENGINE = ReplicatedSummingMergeTree('/clickhouse/tables/{layer}-{shard}/test.mv','{replica}') PARTITION BY toYYYYMMDD(hour) ORDER BY hour AS SELECT toStartOfHour(time) AS hour, sum(number) AS sum_amount FROM test.landing GROUP BY hour";
+$CLICKHOUSE_CLIENT --query="CREATE TABLE test.landing (time DateTime, number Int64) Engine=ReplicatedReplacingMergeTree('/clickhouse/tables/{shard}/{database}/test.landing','{replica}') PARTITION BY toYYYYMMDD(time) ORDER BY time";
+$CLICKHOUSE_CLIENT --query="CREATE MATERIALIZED VIEW IF NOT EXISTS test.mv ENGINE = ReplicatedSummingMergeTree('/clickhouse/tables/{shard}/{database}/test.mv','{replica}') PARTITION BY toYYYYMMDD(hour) ORDER BY hour AS SELECT toStartOfHour(time) AS hour, sum(number) AS sum_amount FROM test.landing GROUP BY hour";
 
 printf '2023-09-01 12:23:34, 42,
 '| $CLICKHOUSE_CLIENT --query="INSERT INTO test.landing (*) settings max_insert_delayed_streams_for_parallel_write=2 FORMAT CSV";
