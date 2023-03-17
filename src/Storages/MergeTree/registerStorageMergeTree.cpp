3748e647521e510219de81118b8bc01bf07997bb
flynn <fenglv15@mails.ucas.ac.cn>
2023-03-17 08:34:22 +0000

    fix style

--- src/Storages/MergeTree/registerStorageMergeTree.cpp
+++ src/Storages/MergeTree/registerStorageMergeTree.cpp
@@ -33,7 +33,6 @@ namespace ErrorCodes
     extern const int UNKNOWN_STORAGE;
     extern const int NO_REPLICA_NAME_GIVEN;
     extern const int CANNOT_EXTRACT_TABLE_STRUCTURE;
-    extern const int ILLEGAL_TYPE_OF_ARGUMENT;
 }
 
 
