359defbd26fc7457f582cd6902e90e9acc8a0f13
Arthur Passos <arthur.ti@outlook.com>
2023-03-20 15:26:00 -0300

    move runtime find_first_symbols to find_symbols.h

--- src/Functions/keyvaluepair/src/impl/state/strategies/util/CharacterFinder.cpp
+++ src/Functions/keyvaluepair/src/impl/state/strategies/util/CharacterFinder.cpp
@@ -1,74 +1,15 @@
 #include "CharacterFinder.h"
-#include <nmmintrin.h>
+#include <base/find_symbols.h>
 
 
 namespace DB
 {
 
-template <bool positive>
-constexpr bool maybe_negate(bool x)
-{
-    return positive == x;
-}
-
-static bool is_in(char c, const char * needles, size_t num_chars)
-{
-    for (auto i = 0u; i < num_chars; i++)
-    {
-        if (c == needles[i])
-        {
-            return true;
-        }
-    }
-
-    return false;
-}
-
-template <bool positive>
-inline const char * find_first_symbols_sse42(const char * const begin, const char * const end, const char * needle, size_t num_chars)
-{
-    const char * pos = begin;
-
-#if defined(__SSE4_2__)
-    constexpr int mode = _SIDD_UBYTE_OPS | _SIDD_CMP_EQUAL_ANY | _SIDD_LEAST_SIGNIFICANT;
-
-    const __m128i set = _mm_loadu_si128(reinterpret_cast<const __m128i *>(needle));
-
-    for (; pos + 15 < end; pos += 16)
-    {
-        __m128i bytes = _mm_loadu_si128(reinterpret_cast<const __m128i *>(pos));
-
-        if constexpr (positive)
-        {
-            if (_mm_cmpestrc(set, num_chars, bytes, 16, mode))
-                return pos + _mm_cmpestri(set, num_chars, bytes, 16, mode);
-        }
-        else
-        {
-            if (_mm_cmpestrc(set, num_chars, bytes, 16, mode | _SIDD_NEGATIVE_POLARITY))
-                return pos + _mm_cmpestri(set, num_chars, bytes, 16, mode | _SIDD_NEGATIVE_POLARITY);
-        }
-    }
-#endif
-
-    for (; pos < end; ++pos)
-        if (maybe_negate<positive>(is_in(*pos, needle, num_chars)))
-            return pos;
-
-    return nullptr;
-}
-
-template <bool positive = true>
-auto find_first_symbols_sse42(std::string_view haystack, std::string_view needle)
-{
-    return find_first_symbols_sse42<positive>(haystack.begin(), haystack.end(), needle.begin(), needle.size());
-}
-
 std::optional<CharacterFinder::Position> CharacterFinder::findFirst(std::string_view haystack, const std::vector<char> & needles)
 {
     if (!needles.empty())
     {
-        if (const auto * ptr = find_first_symbols_sse42(haystack, {needles.begin(), needles.end()}))
+        if (const auto * ptr = find_first_symbols_or_null(haystack, {needles.begin(), needles.end()}))
         {
             return ptr - haystack.begin();
         }
@@ -94,7 +35,7 @@ std::optional<CharacterFinder::Position> CharacterFinder::findFirstNot(std::stri
         return 0u;
     }
 
-    if (const auto * ptr = find_first_symbols_sse42<false>(haystack, {needles.begin(), needles.end()}))
+    if (const auto * ptr = find_first_not_symbols_or_null(haystack, {needles.begin(), needles.end()}))
     {
         return ptr - haystack.begin();
     }
