96a9e9fab09ffeb04bb57dd80c5b4c459bd5cd23
kssenii <sumarokovakseniia@mail.ru>
2023-03-21 17:42:50 +0100

    Fix

--- tests/integration/test_storage_hudi/test.py
+++ tests/integration/test_storage_hudi/test.py
@@ -12,8 +12,9 @@ import pyspark
 
 SCRIPT_DIR = os.path.dirname(os.path.realpath(__file__))
 TABLE_NAME = "test_hudi_table"
-USER_FILES_PATH = "/ClickHouse/tests/integration/test_storage_hudi/_instances/node1/database/user_files"
-
+USER_FILES_PATH = os.path.join(
+    SCRIPT_DIR, "./_instances/node1/database/user_files"
+)
 
 @pytest.fixture(scope="module")
 def started_cluster():
