7bfd8b0df6043e817dff7a145cc6e79d2c7e8e93
Alfonso Martinez <alfonso@tinybird.co>
2023-03-21 09:48:01 +0100

    Removed repeated move in partitions

--- src/Storages/MergeTree/ReplicatedMergeTreeSink.cpp
+++ src/Storages/MergeTree/ReplicatedMergeTreeSink.cpp
@@ -499,7 +499,7 @@ void ReplicatedMergeTreeSinkImpl<async_insert>::consume(Chunk chunk)
     if (streams > 0 && streams <= max_insert_delayed_streams_for_parallel_write)
     {
         delayed_chunk = std::make_unique<ReplicatedMergeTreeSinkImpl<async_insert>::DelayedChunk>(replicas_num);
-        delayed_chunk->partitions = std::move(partitions);
+        delayed_chunk->partitions = partitions;
     }
 
     finishDelayedChunk(zookeeper);
