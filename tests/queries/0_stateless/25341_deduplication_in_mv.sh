08696cb20fafb8162c06f46b6d57d10a4dfc2d65
Alfonso Martinez <alfonso@tinybird.co>
2023-03-21 17:41:46 +0100

    changed name of database in test to avoid conflicts

--- tests/queries/0_stateless/25341_deduplication_in_mv.sh
+++ tests/queries/0_stateless/25341_deduplication_in_mv.sh
@@ -1,36 +1,39 @@
 #!/usr/bin/env bash
+# Tagd: zookeeper
 
 CURDIR=$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)
 # shellcheck source=../shell_config.sh
 . "$CURDIR"/../shell_config.sh
 
-$CLICKHOUSE_CLIENT --query="DROP DATABASE IF EXISTS test SYNC";
-$CLICKHOUSE_CLIENT --query="CREATE DATABASE test";
-$CLICKHOUSE_CLIENT --query="CREATE TABLE test.landing (time DateTime, number Int64) Engine=ReplicatedReplacingMergeTree('/clickhouse/tables/{shard}/{database}/test.landing','{replica}') PARTITION BY toYYYYMMDD(time) ORDER BY time";
-$CLICKHOUSE_CLIENT --query="CREATE MATERIALIZED VIEW IF NOT EXISTS test.mv ENGINE = ReplicatedSummingMergeTree('/clickhouse/tables/{shard}/{database}/test.mv','{replica}') PARTITION BY toYYYYMMDD(hour) ORDER BY hour AS SELECT toStartOfHour(time) AS hour, sum(number) AS sum_amount FROM test.landing GROUP BY hour";
+$CLICKHOUSE_CLIENT --query="DROP DATABASE IF EXISTS test_deduplication_in_mv SYNC";
+$CLICKHOUSE_CLIENT --query="CREATE DATABASE test_deduplication_in_mv";
+$CLICKHOUSE_CLIENT --query="CREATE TABLE test_deduplication_in_mv.landing (time DateTime, number Int64) Engine=ReplicatedReplacingMergeTree('/clickhouse/tables/{shard}/{database}/test_deduplication_in_mv.landing','{replica}') PARTITION BY toYYYYMMDD(time) ORDER BY time";
+$CLICKHOUSE_CLIENT --query="CREATE MATERIALIZED VIEW IF NOT EXISTS test_deduplication_in_mv.mv ENGINE = ReplicatedSummingMergeTree('/clickhouse/tables/{shard}/{database}/test_deduplication_in_mv.mv','{replica}') PARTITION BY toYYYYMMDD(hour) ORDER BY hour AS SELECT toStartOfHour(time) AS hour, sum(number) AS sum_amount FROM test_deduplication_in_mv.landing GROUP BY hour";
 
 printf '2023-09-01 12:23:34, 42,
-'| $CLICKHOUSE_CLIENT --query="INSERT INTO test.landing (*) settings max_insert_delayed_streams_for_parallel_write=0 FORMAT CSV";
-
-printf '2023-09-01 12:23:34, 42,
-2022-09-01 12:23:34, 42,
-'| $CLICKHOUSE_CLIENT --query="INSERT INTO test.landing (*) settings max_insert_delayed_streams_for_parallel_write=0 FORMAT CSV";
-
-$CLICKHOUSE_CLIENT --query="""SELECT * FROM test.landing FINAL ORDER BY time""";
-$CLICKHOUSE_CLIENT --query="""SELECT * FROM test.mv FINAL ORDER BY hour""";
-
-
-$CLICKHOUSE_CLIENT --query="DROP DATABASE IF EXISTS test SYNC";
-$CLICKHOUSE_CLIENT --query="CREATE DATABASE test";
-$CLICKHOUSE_CLIENT --query="CREATE TABLE test.landing (time DateTime, number Int64) Engine=ReplicatedReplacingMergeTree('/clickhouse/tables/{shard}/{database}/test.landing','{replica}') PARTITION BY toYYYYMMDD(time) ORDER BY time";
-$CLICKHOUSE_CLIENT --query="CREATE MATERIALIZED VIEW IF NOT EXISTS test.mv ENGINE = ReplicatedSummingMergeTree('/clickhouse/tables/{shard}/{database}/test.mv','{replica}') PARTITION BY toYYYYMMDD(hour) ORDER BY hour AS SELECT toStartOfHour(time) AS hour, sum(number) AS sum_amount FROM test.landing GROUP BY hour";
-
-printf '2023-09-01 12:23:34, 42,
-'| $CLICKHOUSE_CLIENT --query="INSERT INTO test.landing (*) settings max_insert_delayed_streams_for_parallel_write=2 FORMAT CSV";
+'| $CLICKHOUSE_CLIENT --query="INSERT INTO test_deduplication_in_mv.landing (*) settings max_insert_delayed_streams_for_parallel_write=0 FORMAT CSV";
 
 printf '2023-09-01 12:23:34, 42,
 2022-09-01 12:23:34, 42,
-'| $CLICKHOUSE_CLIENT --query="INSERT INTO test.landing (*) settings max_insert_delayed_streams_for_parallel_write=2 FORMAT CSV";
+'| $CLICKHOUSE_CLIENT --query="INSERT INTO test_deduplication_in_mv.landing (*) settings max_insert_delayed_streams_for_parallel_write=0 FORMAT CSV";
 
-$CLICKHOUSE_CLIENT --query="""SELECT * FROM test.landing FINAL ORDER BY time""";
-$CLICKHOUSE_CLIENT --query="""SELECT * FROM test.mv FINAL ORDER BY hour""";
+$CLICKHOUSE_CLIENT --query="""SELECT * FROM test_deduplication_in_mv.landing FINAL ORDER BY time""";
+$CLICKHOUSE_CLIENT --query="""SELECT * FROM test_deduplication_in_mv.mv FINAL ORDER BY hour""";
+
+$CLICKHOUSE_CLIENT --query="DROP DATABASE test_deduplication_in_mv SYNC";
+
+$CLICKHOUSE_CLIENT --query="CREATE DATABASE test_deduplication_in_mv";
+$CLICKHOUSE_CLIENT --query="CREATE TABLE test_deduplication_in_mv.landing (time DateTime, number Int64) Engine=ReplicatedReplacingMergeTree('/clickhouse/tables/{shard}/{database}/test_deduplication_in_mv.landing','{replica}') PARTITION BY toYYYYMMDD(time) ORDER BY time";
+$CLICKHOUSE_CLIENT --query="CREATE MATERIALIZED VIEW IF NOT EXISTS test_deduplication_in_mv.mv ENGINE = ReplicatedSummingMergeTree('/clickhouse/tables/{shard}/{database}/test_deduplication_in_mv.mv','{replica}') PARTITION BY toYYYYMMDD(hour) ORDER BY hour AS SELECT toStartOfHour(time) AS hour, sum(number) AS sum_amount FROM test_deduplication_in_mv.landing GROUP BY hour";
+
+printf '2023-09-01 12:23:34, 42,
+'| $CLICKHOUSE_CLIENT --query="INSERT INTO test_deduplication_in_mv.landing (*) settings max_insert_delayed_streams_for_parallel_write=2 FORMAT CSV";
+
+printf '2023-09-01 12:23:34, 42,
+2022-09-01 12:23:34, 42,
+'| $CLICKHOUSE_CLIENT --query="INSERT INTO test_deduplication_in_mv.landing (*) settings max_insert_delayed_streams_for_parallel_write=2 FORMAT CSV";
+
+$CLICKHOUSE_CLIENT --query="""SELECT * FROM test_deduplication_in_mv.landing FINAL ORDER BY time""";
+$CLICKHOUSE_CLIENT --query="""SELECT * FROM test_deduplication_in_mv.mv FINAL ORDER BY hour""";
+
+$CLICKHOUSE_CLIENT --query="DROP DATABASE test_deduplication_in_mv SYNC";
