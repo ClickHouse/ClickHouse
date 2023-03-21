f55f33b64dc37257c93e1ced4c5e157f20daeec2
Azat Khuzhin <a3at.mail@gmail.com>
2023-03-21 08:54:05 +0200

    Avoid using s3_truncate_on_insert in 02700_s3_part_INT_MAX
    Seems that version of MinIO that is used on CI is too slow for this [1],
    I've tried the same locally with RELEASE.2023-02-10T18-48-39Z and
    everythings works OK, but I have NVME locally and more recent MinIO.
    
      [1]: https://s3.amazonaws.com/clickhouse-test-reports/47693/5c701b090c3ec10cb2a4a708b60d364eb02192fe/stateless_tests_flaky_check__asan_.html
    
    Signed-off-by: Azat Khuzhin <a.khuzhin@semrush.com>

--- tests/queries/0_stateless/02700_s3_part_INT_MAX.sql
+++
@@ -1,8 +0,0 @@
--- Tags: no-parallel, long
-
--- Regression test for crash in case of part exceeds INT_MAX
-INSERT INTO FUNCTION s3('http://localhost:11111/test/test_INT_MAX.tsv', '', '', 'TSV')
-SELECT repeat('a', 1024) FROM numbers((pow(2, 30) * 2) / 1024)
-SETTINGS s3_truncate_on_insert = 1, s3_max_single_part_upload_size = '10Gi';
-
-SELECT count() FROM s3('http://localhost:11111/test/test_INT_MAX.tsv');
