83ee185fce0f220c85cf92acea09f92ee423e25d
zvonand <azvonov@altinity.com>
2023-03-21 16:33:19 +0100

    upd

--- src/Functions/FunctionFormatDecimal.cpp
+++ src/Functions/FunctionFormatDecimal.cpp
@@ -7,7 +7,16 @@ namespace DB
 
 REGISTER_FUNCTION(FormatDecimal)
 {
-    factory.registerFunction<FunctionFormatDecimal>();
+    factory.registerFunction<FunctionFormatDecimal>(
+        {
+            R"(
+Returns string representation of a number. First argument is the number of any numeric type,
+second argument is the desired number of digits in fractional part. Returns String.
+
+        )",
+            Documentation::Examples{{"formatDecimal", "SELECT formatDecimal(2.1456,2)"}},
+            Documentation::Categories{"String"}
+        }, FunctionFactory::CaseInsensitive);
 }
 
 }
