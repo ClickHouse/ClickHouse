6be4cd071db09b2bacc357dd625c66206c9e563a
xiedeyantu <czjourney@163.com>
2023-03-21 20:14:26 +0800

    support undrop table

---
+++ src/Interpreters/InterpreterUndropQuery.cpp
@@ -0,0 +1,77 @@
+#include <Interpreters/Context.h>
+#include <Interpreters/executeDDLQueryOnCluster.h>
+#include <Interpreters/InterpreterUndropQuery.h>
+#include <Access/Common/AccessRightsElement.h>
+#include <Parsers/ASTUndropQuery.h>
+
+#include "config.h"
+
+namespace DB
+{
+
+namespace ErrorCodes
+{
+    extern const int LOGICAL_ERROR;
+    extern const int TABLE_ALREADY_EXISTS;
+    extern const int SUPPORT_IS_DISABLED;
+}
+
+InterpreterUndropQuery::InterpreterUndropQuery(const ASTPtr & query_ptr_, ContextMutablePtr context_) : WithMutableContext(context_), query_ptr(query_ptr_)
+{
+}
+
+
+BlockIO InterpreterUndropQuery::execute()
+{
+    if (!getContext()->getSettingsRef().allow_experimental_undrop_table_query)
+        throw Exception(ErrorCodes::SUPPORT_IS_DISABLED,
+                        "Undrop table is experimental. "
+                        "Set `allow_experimental_undrop_table_query` setting to enable it");
+
+    getContext()->checkAccess(AccessType::UNDROP_TABLE);
+    auto & undrop = query_ptr->as<ASTUndropQuery &>();
+    if (!undrop.cluster.empty() && !maybeRemoveOnCluster(query_ptr, getContext()))
+    {
+        DDLQueryOnClusterParams params;
+        params.access_to_check = getRequiredAccessForDDLOnCluster();
+        return executeDDLQueryOnCluster(query_ptr, getContext(), params);
+    }
+
+    if (undrop.table)
+        return executeToTable(undrop);
+    else
+        throw Exception(ErrorCodes::LOGICAL_ERROR, "Nothing to undrop, both names are empty");
+}
+
+BlockIO InterpreterUndropQuery::executeToTable(ASTUndropQuery & query)
+{
+    auto table_id = StorageID(query);
+    auto guard = DatabaseCatalog::instance().getDDLGuard(table_id.database_name, table_id.table_name);
+
+    auto context = getContext();
+    if (table_id.database_name.empty())
+    {
+        table_id.database_name = context->getCurrentDatabase();
+        query.setDatabase(table_id.database_name);
+    }
+
+    auto database = DatabaseCatalog::instance().getDatabase(table_id.database_name);
+    if (database->isTableExist(table_id.table_name, getContext()))
+        throw Exception(
+            ErrorCodes::TABLE_ALREADY_EXISTS, "Cannot Undrop table, {}.{} already exists", backQuote(table_id.database_name), backQuote(table_id.table_name));
+
+    database->checkMetadataFilenameAvailability(table_id.table_name);
+
+    DatabaseCatalog::instance().dequeueDroppedTableCleanup(table_id);
+    return {};
+}
+
+AccessRightsElements InterpreterUndropQuery::getRequiredAccessForDDLOnCluster() const
+{
+    AccessRightsElements required_access;
+    const auto & undrop = query_ptr->as<const ASTUndropQuery &>();
+
+    required_access.emplace_back(AccessType::UNDROP_TABLE, undrop.getDatabase(), undrop.getTable());
+    return required_access;
+}
+}
---
+++ tests/queries/0_stateless/02681_undrop_query_uuid.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env bash
+# Tags: no-ordinary-database, distributed, zookeeper
+
+CURDIR=$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)
+# shellcheck source=../shell_config.sh
+. "$CURDIR"/../shell_config.sh
+
+echo 'test MergeTree with uuid'
+${CLICKHOUSE_CLIENT} -q "drop table if exists 02681_undrop_uuid sync;"
+uuid=$(${CLICKHOUSE_CLIENT} --query "SELECT reinterpretAsUUID(currentDatabase())")
+${CLICKHOUSE_CLIENT} -q "create table 02681_undrop_uuid UUID '$uuid' (id Int32) Engine=MergeTree() order by id;"
+${CLICKHOUSE_CLIENT} -q "insert into 02681_undrop_uuid values (1),(2),(3);"
+${CLICKHOUSE_CLIENT} -q "drop table 02681_undrop_uuid settings database_atomic_wait_for_drop_and_detach_synchronously = 0;"
+${CLICKHOUSE_CLIENT} -q "select table from system.marked_dropped_tables where table = '02681_undrop_uuid' limit 1;"
+${CLICKHOUSE_CLIENT} -q "undrop table 02681_undrop_uuid UUID '$uuid' settings allow_experimental_undrop_table_query = 1;"
+${CLICKHOUSE_CLIENT} -q "select * from 02681_undrop_uuid order by id;"
+${CLICKHOUSE_CLIENT} -q "drop table 02681_undrop_uuid sync;"
