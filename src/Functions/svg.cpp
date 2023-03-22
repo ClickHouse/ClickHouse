8c1a02a4246ef4ed9fb2006a51dfe151fc39c33f
Alexander Tokmakov <tavplubix@clickhouse.com>
2023-03-22 18:22:26 +0100

    fix

--- src/Functions/svg.cpp
+++ src/Functions/svg.cpp
@@ -13,8 +13,7 @@ namespace DB
 namespace ErrorCodes
 {
     extern const int ILLEGAL_TYPE_OF_ARGUMENT;
-    extern const int TOO_MANY_ARGUMENTS_FOR_FUNCTION;
-    extern const int TOO_FEW_ARGUMENTS_FOR_FUNCTION;
+    extern const int NUMBER_OF_ARGUMENTS_DOESNT_MATCH;
 }
 
 class FunctionSvg : public IFunction
@@ -50,7 +49,7 @@ public:
     {
         if (arguments.empty() || arguments.size() > 2)
         {
-            throw Exception(ErrorCodes::TOO_MANY_ARGUMENTS_FOR_FUNCTION, "Incorrect number of arguments: expected 1 or 2 arguments");
+            throw Exception(ErrorCodes::NUMBER_OF_ARGUMENTS_DOESNT_MATCH, "Incorrect number of arguments: expected 1 or 2 arguments");
         }
         else if (arguments.size() == 2 && checkAndGetDataType<DataTypeString>(arguments[1].get()) == nullptr)
         {
