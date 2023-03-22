09776118e22d394bc6db055432a79f2cd0eb52ba
chen <czjourney@163.com>
2023-03-22 14:27:56 +0800

    Update 02681_undrop_query_uuid.sh

--- tests/queries/0_stateless/02681_undrop_query_uuid.sh
+++ tests/queries/0_stateless/02681_undrop_query_uuid.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
-# Tags: no-ordinary-database, distributed, zookeeper
+# Tags: no-ordinary-database, no-replicated-database
 
 CURDIR=$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)
 # shellcheck source=../shell_config.sh
