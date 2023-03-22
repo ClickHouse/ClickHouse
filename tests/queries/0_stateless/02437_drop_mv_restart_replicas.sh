ec02d7747a2313d9713314e18df77af7c4fcd697
Alexander Tokmakov <tavplubix@clickhouse.com>
2023-03-22 17:02:33 +0100

    fix test

--- tests/queries/0_stateless/02437_drop_mv_restart_replicas.sh
+++ tests/queries/0_stateless/02437_drop_mv_restart_replicas.sh
@@ -12,14 +12,14 @@ $CLICKHOUSE_CLIENT -q "grant all on db_$CLICKHOUSE_DATABASE.* to u_$CLICKHOUSE_D
 function thread_ddl()
 {
     while true; do
-        $CLICKHOUSE_CLIENT -q "create database db_$CLICKHOUSE_DATABASE"
-        $CLICKHOUSE_CLIENT -q "CREATE TABLE db_$CLICKHOUSE_DATABASE.test (test String, A Int64, B Int64) ENGINE = ReplicatedMergeTree ('/clickhouse/tables/{database}/test_02124/{table}', '1') ORDER BY tuple();"
-        $CLICKHOUSE_CLIENT -q "CREATE MATERIALIZED VIEW db_$CLICKHOUSE_DATABASE.test_mv_a Engine=ReplicatedMergeTree ('/clickhouse/tables/{database}/test_02124/{table}', '1') order by tuple() AS SELECT test, A, count() c FROM db_$CLICKHOUSE_DATABASE.test group by test, A;"
-        $CLICKHOUSE_CLIENT -q "CREATE MATERIALIZED VIEW db_$CLICKHOUSE_DATABASE.test_mv_b Engine=ReplicatedMergeTree ('/clickhouse/tables/{database}/test_02124/{table}', '1') partition by A order by tuple() AS SELECT test, A, count() c FROM db_$CLICKHOUSE_DATABASE.test group by test, A;"
-        $CLICKHOUSE_CLIENT -q "CREATE MATERIALIZED VIEW db_$CLICKHOUSE_DATABASE.test_mv_c Engine=ReplicatedMergeTree ('/clickhouse/tables/{database}/test_02124/{table}', '1') order by tuple() AS SELECT test, A, count() c FROM db_$CLICKHOUSE_DATABASE.test group by test, A;"
+        $CLICKHOUSE_CLIENT -q "create database if not exists db_$CLICKHOUSE_DATABASE"
+        $CLICKHOUSE_CLIENT -q "CREATE TABLE if not exists db_$CLICKHOUSE_DATABASE.test (test String, A Int64, B Int64) ENGINE = ReplicatedMergeTree ('/clickhouse/tables/{database}/test_02124/{table}', '1') ORDER BY tuple();"
+        $CLICKHOUSE_CLIENT -q "CREATE MATERIALIZED VIEW if not exists db_$CLICKHOUSE_DATABASE.test_mv_a Engine=ReplicatedMergeTree ('/clickhouse/tables/{database}/test_02124/{table}', '1') order by tuple() AS SELECT test, A, count() c FROM db_$CLICKHOUSE_DATABASE.test group by test, A;"
+        $CLICKHOUSE_CLIENT -q "CREATE MATERIALIZED VIEW if not exists db_$CLICKHOUSE_DATABASE.test_mv_b Engine=ReplicatedMergeTree ('/clickhouse/tables/{database}/test_02124/{table}', '1') partition by A order by tuple() AS SELECT test, A, count() c FROM db_$CLICKHOUSE_DATABASE.test group by test, A;"
+        $CLICKHOUSE_CLIENT -q "CREATE MATERIALIZED VIEW if not exists db_$CLICKHOUSE_DATABASE.test_mv_c Engine=ReplicatedMergeTree ('/clickhouse/tables/{database}/test_02124/{table}', '1') order by tuple() AS SELECT test, A, count() c FROM db_$CLICKHOUSE_DATABASE.test group by test, A;"
         sleep 0.$RANDOM;
         sleep 0.$RANDOM;
-        $CLICKHOUSE_CLIENT -q "drop database db_$CLICKHOUSE_DATABASE"
+        $CLICKHOUSE_CLIENT -q "drop database if exists db_$CLICKHOUSE_DATABASE"
     done
 }
 
@@ -46,10 +46,10 @@ export -f thread_restart;
 
 TIMEOUT=30
 
-timeout $TIMEOUT bash -c thread_ddl 2>&1| grep -Fa "Exception: " | grep -Fv "TABLE_IS_DROPPED" | grep -Fv "UNKNOWN_TABLE" & #2> /dev/null &
+timeout $TIMEOUT bash -c thread_ddl 2>&1| grep -Fa "Exception: " | grep -Fv -e "TABLE_IS_DROPPED" -e "UNKNOWN_TABLE" -e "DATABASE_NOT_EMPTY" &
 timeout $TIMEOUT bash -c thread_insert 2> /dev/null &
 timeout $TIMEOUT bash -c thread_restart &
 
 wait
 
-$CLICKHOUSE_CLIENT -q "drop database if exists db_$CLICKHOUSE_DATABASE" 2>&1| grep -Fa "Exception: " | grep -Fv "DATABASE_NOT_EMPTY" ||:
+$CLICKHOUSE_CLIENT -q "drop database if exists db_$CLICKHOUSE_DATABASE" 2>&1| grep -Fa "Exception: " | grep -Fv -e "TABLE_IS_DROPPED" -e "UNKNOWN_TABLE" -e "DATABASE_NOT_EMPTY" ||:
