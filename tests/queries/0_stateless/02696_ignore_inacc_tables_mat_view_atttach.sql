57ceb3436669ae60ac046eb562cd2627a5a98eb5
MikhailBurdukov <burdukvmikhail@gmail.com>
2023-03-25 13:36:58 +0000

    Remove database from test

--- tests/queries/0_stateless/02696_ignore_inacc_tables_mat_view_atttach.sql
+++ tests/queries/0_stateless/02696_ignore_inacc_tables_mat_view_atttach.sql
@@ -1,34 +1,29 @@
 SET send_logs_level = 'fatal';
 
-DROP DATABASE IF EXISTS test_db;
+CREATE TABLE test_table (n Int32, s String) ENGINE MergeTree PARTITION BY n ORDER BY n;
 
-CREATE DATABASE test_db;
+CREATE TABLE mview_backend (n Int32, n2 Int64) ENGINE MergeTree PARTITION BY n ORDER BY n;
 
-CREATE TABLE test_db.table (n Int32, s String) ENGINE MergeTree PARTITION BY n ORDER BY n;
+CREATE MATERIALIZED VIEW mview TO mview_backend AS SELECT n, n * n AS "n2" FROM test_table;
 
-CREATE TABLE test_db.mview_backend (n Int32, n2 Int64) ENGINE MergeTree PARTITION BY n ORDER BY n;
+DROP TABLE test_table;
 
-CREATE MATERIALIZED VIEW test_db.mview TO test_db.mview_backend AS SELECT n, n * n AS "n2" FROM test_db.table;
-
-DROP TABLE test_db.table;
-
-DETACH TABLE test_db.mview;
+DETACH TABLE mview;
 
 /* Check that we get an exception with the option. */
 
 SET skip_materialized_view_checking_if_source_table_not_exist = 0;
-ATTACH TABLE test_db.mview; --{serverError 60}
+ATTACH TABLE mview; --{serverError 60}
 
 /* Check that we don't get an exception with the option. */
 SET skip_materialized_view_checking_if_source_table_not_exist = 1;
-ATTACH TABLE test_db.mview;
+ATTACH TABLE mview;
 
 /* Check if the data in the materialized view is updated after the restore.*/
-CREATE TABLE test_db.table (n Int32, s String) ENGINE MergeTree PARTITION BY n ORDER BY n;
+CREATE TABLE test_table (n Int32, s String) ENGINE MergeTree PARTITION BY n ORDER BY n;
 
-INSERT INTO test_db.table VALUES (3,'some_val');
+INSERT INTO test_table VALUES (3,'some_val');
 
-SELECT n,s  FROM test_db.table ORDER BY n;
-SELECT n,n2 FROM test_db.mview ORDER by n;
+SELECT n,s  FROM test_table ORDER BY n;
+SELECT n,n2 FROM mview ORDER by n;
 
-DROP DATABASE test_db;
