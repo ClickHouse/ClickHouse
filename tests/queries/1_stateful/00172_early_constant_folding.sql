33fc58b1d2f6be68574c4c09dad07615e627ccef
Igor Nikonov <igor@clickhouse.com>
2023-03-26 21:06:55 +0000

    tests

--- tests/queries/1_stateful/00172_early_constant_folding.sql
+++ tests/queries/1_stateful/00172_early_constant_folding.sql
@@ -1,4 +1,6 @@
 -- Tags: no-parallel-replicas
 
+set allow_experimental_analyzer=0;
+
 set max_threads=10;
 EXPLAIN PIPELINE SELECT count(JavaEnable) FROM test.hits WHERE WatchID = 1 OR Title = 'next' OR URL = 'prev' OR URL = '???' OR 1;
