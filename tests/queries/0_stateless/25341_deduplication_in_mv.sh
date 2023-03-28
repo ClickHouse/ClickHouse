31857fde2209c3a110e25dccd236a5c8a4dde97c
Alfonso Martinez <alfonso@tinybird.co>
2023-03-28 13:01:34 +0200

    Fixed test 25341 to avoid flakiness

--- tests/queries/0_stateless/25341_deduplication_in_mv.sh
+++ tests/queries/0_stateless/25341_deduplication_in_mv.sh
@@ -18,18 +18,16 @@ printf '2023-09-01 12:23:34, 42,
 $CLICKHOUSE_CLIENT --query="""SELECT * FROM landing FINAL ORDER BY time""";
 $CLICKHOUSE_CLIENT --query="""SELECT * FROM mv FINAL ORDER BY hour""";
 
-$CLICKHOUSE_CLIENT --query="DROP TABLE landing SYNC";
-$CLICKHOUSE_CLIENT --query="DROP TABLE mv SYNC";
 
-$CLICKHOUSE_CLIENT --query="CREATE TABLE landing (time DateTime, number Int64) Engine=ReplicatedReplacingMergeTree('/clickhouse/tables/{shard}/{database}/landing','{replica}') PARTITION BY toYYYYMMDD(time) ORDER BY time";
-$CLICKHOUSE_CLIENT --query="CREATE MATERIALIZED VIEW IF NOT EXISTS mv ENGINE = ReplicatedSummingMergeTree('/clickhouse/tables/{shard}/{database}/mv','{replica}') PARTITION BY toYYYYMMDD(hour) ORDER BY hour AS SELECT toStartOfHour(time) AS hour, sum(number) AS sum_amount FROM landing GROUP BY hour";
+$CLICKHOUSE_CLIENT --query="CREATE TABLE landing2 (time DateTime, number Int64) Engine=ReplicatedReplacingMergeTree('/clickhouse/tables/{shard}/{database}/landing2','{replica}') PARTITION BY toYYYYMMDD(time) ORDER BY time";
+$CLICKHOUSE_CLIENT --query="CREATE MATERIALIZED VIEW IF NOT EXISTS mv2 ENGINE = ReplicatedSummingMergeTree('/clickhouse/tables/{shard}/{database}/mv2','{replica}') PARTITION BY toYYYYMMDD(hour) ORDER BY hour AS SELECT toStartOfHour(time) AS hour, sum(number) AS sum_amount FROM landing2 GROUP BY hour";
 
 printf '2023-09-01 12:23:34, 42,
-'| $CLICKHOUSE_CLIENT --query="INSERT INTO landing (*) settings max_insert_delayed_streams_for_parallel_write=2 FORMAT CSV";
+'| $CLICKHOUSE_CLIENT --query="INSERT INTO landing2 (*) settings max_insert_delayed_streams_for_parallel_write=2 FORMAT CSV";
 
 printf '2023-09-01 12:23:34, 42,
 2022-09-01 12:23:34, 42,
-'| $CLICKHOUSE_CLIENT --query="INSERT INTO landing (*) settings max_insert_delayed_streams_for_parallel_write=2 FORMAT CSV";
+'| $CLICKHOUSE_CLIENT --query="INSERT INTO landing2 (*) settings max_insert_delayed_streams_for_parallel_write=2 FORMAT CSV";
 
-$CLICKHOUSE_CLIENT --query="""SELECT * FROM landing FINAL ORDER BY time""";
-$CLICKHOUSE_CLIENT --query="""SELECT * FROM mv FINAL ORDER BY hour""";
+$CLICKHOUSE_CLIENT --query="""SELECT * FROM landing2 FINAL ORDER BY time""";
+$CLICKHOUSE_CLIENT --query="""SELECT * FROM mv2 FINAL ORDER BY hour""";
