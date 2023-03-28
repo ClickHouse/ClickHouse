4b0b5301beac23a22bb20e192a286700b0b93a8c
Alexander Tokmakov <tavplubix@clickhouse.com>
2023-03-29 00:55:03 +0200

    improve script for updating clickhouse-docs

--- docs/get-clickhouse-docs.sh
+++ docs/get-clickhouse-docs.sh
@@ -27,5 +27,27 @@ else
     exit 1
     ;;
   esac
+
+  if [ -n "$2" ]; then
+    set_git_hook="$2"
+  elif [ ! -n "$1" ]; then
+    read -rp "Would you like to setup git hook for automatic update? (y|n): " set_git_hook
+  fi
+
+  if [ "$set_git_hook" = "y" ]; then
+    hook_command="$(pwd)/pull-clickhouse-docs-hook.sh 24"
+    hook_file=$(realpath "$(pwd)/../.git/hooks/post-checkout")
+    already_have=$(grep -Faq "pull-clickhouse-docs-hook.sh" "$hook_file" 2>/dev/null && echo 1 || echo 0)
+    if [ $already_have -eq 0 ]; then
+      echo "Appending '$hook_command' to $hook_file"
+      echo "$hook_command" >> "$hook_file"
+      chmod u+x "$hook_file"  # Just in case it did not exist before append
+    else
+      echo "Looks like the update hook already exists, will not add another one"
+    fi
+  elif [ ! "$set_git_hook" = "n" ]; then
+    echo "Expected 'y' or 'n', got '$set_git_hook', will not setup git hook"
+  fi
+
   git clone "$git_url" "clickhouse-docs"
 fi
