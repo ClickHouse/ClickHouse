a862973f145d563fb58e69db12c3466ee715a404
natasha <murfel@users.noreply.github.com>
2023-03-28 11:28:31 +0100

    amend comment on hilite equality

--- src/Parsers/tests/gtest_format_hiliting.cpp
+++ src/Parsers/tests/gtest_format_hiliting.cpp
@@ -98,7 +98,7 @@ bool are_equal_with_hilites_removed(std::string_view left, std::string_view righ
 /*
  * Hilited queries cannot be compared symbol-by-symbol, as there's some frivolousness introduced with the hilites. Specifically:
  * 1. Whitespaces could be hilited with any hilite type.
- * 2. Hilite could or could be not reset with hilite_none before the next hilite, i.e. strings a and b are equal.
+ * 2. Hilite could or could be not reset with hilite_none before the next hilite, i.e. the following strings a and b are equal:
  *      a. hilite_keyword foo hilite_none hilite_operator +
  *      b. hilite_keyword foo hilite_operator +
  */
