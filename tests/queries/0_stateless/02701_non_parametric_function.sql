2bb61bec30ad845db2234f13b230c146acd4bb8a
save-my-heart <save-my-heart@outlook.com>
2023-03-29 12:58:35 +0800

    fix tests

--- tests/queries/0_stateless/02701_non_parametric_function.sql
+++ tests/queries/0_stateless/02701_non_parametric_function.sql
@@ -3,3 +3,5 @@ SELECT * FROM system.numbers WHERE number > toUInt64(10)(number) LIMIT 10; -- { 
 CREATE FUNCTION sum_udf as (x, y) -> (x + y);
 
 SELECT sum_udf(1)(1, 2); -- { serverError 309 }
+
+DROP FUNCTION sum_udf;
