7640e89d2f04a9aad56480d435dcd318a3483381
Alexander Tokmakov <tavplubix@clickhouse.com>
2023-03-31 20:58:57 +0200

    exclude more subdirs

--- contrib/sparse-checkout/update-grpc.sh
+++ contrib/sparse-checkout/update-grpc.sh
@@ -4,12 +4,14 @@ echo "Using sparse checkout for grpc"
 
 FILES_TO_CHECKOUT=$(git rev-parse --git-dir)/info/sparse-checkout
 echo '/*' > $FILES_TO_CHECKOUT
-# FIXME for some reason we need it for build
-# echo '!/test/*' >> $FILES_TO_CHECKOUT
-# echo '!/tools/*' >> $FILES_TO_CHECKOUT
+echo '!/test/*' >> $FILES_TO_CHECKOUT
+echo '/test/build/*' >> $FILES_TO_CHECKOUT
+echo '!/tools/*' >> $FILES_TO_CHECKOUT
+echo '/tools/codegen/*' >> $FILES_TO_CHECKOUT
 echo '!/examples/*' >> $FILES_TO_CHECKOUT
 echo '!/doc/*' >> $FILES_TO_CHECKOUT
-# echo '!/src/csharp/*' >> $FILES_TO_CHECKOUT
+# FIXME why do we need csharp?
+#echo '!/src/csharp/*' >> $FILES_TO_CHECKOUT
 echo '!/src/python/*' >> $FILES_TO_CHECKOUT
 echo '!/src/objective-c/*' >> $FILES_TO_CHECKOUT
 echo '!/src/php/*' >> $FILES_TO_CHECKOUT
--- contrib/sparse-checkout/update-llvm-project.sh
+++ contrib/sparse-checkout/update-llvm-project.sh
@@ -4,19 +4,23 @@ echo "Using sparse checkout for llvm-project"
 
 FILES_TO_CHECKOUT=$(git rev-parse --git-dir)/info/sparse-checkout
 echo '/*' > $FILES_TO_CHECKOUT
-echo '!/llvm/test/*' >> $FILES_TO_CHECKOUT
-echo '!/llvm/docs/*' >> $FILES_TO_CHECKOUT
-echo '!/llvm/unittests/*' >> $FILES_TO_CHECKOUT
-echo '!/llvm/tools/*' >> $FILES_TO_CHECKOUT
-echo '!/clang/*' >> $FILES_TO_CHECKOUT
-echo '!/clang-tools-extra/*' >> $FILES_TO_CHECKOUT
-echo '!/lldb/*' >> $FILES_TO_CHECKOUT
-echo '!/mlir/*' >> $FILES_TO_CHECKOUT
-echo '!/polly/*' >> $FILES_TO_CHECKOUT
-echo '!/lld/*' >> $FILES_TO_CHECKOUT
-echo '!/flang/*' >> $FILES_TO_CHECKOUT
+echo '!/*/*' >> $FILES_TO_CHECKOUT
+echo '/llvm/*' >> $FILES_TO_CHECKOUT
+echo '!/llvm/*/*' >> $FILES_TO_CHECKOUT
+echo '/llvm/cmake/*' >> $FILES_TO_CHECKOUT
+echo '/llvm/projects/*' >> $FILES_TO_CHECKOUT
+echo '/llvm/include/*' >> $FILES_TO_CHECKOUT
+echo '/llvm/lib/*' >> $FILES_TO_CHECKOUT
+echo '/llvm/utils/TableGen/*' >> $FILES_TO_CHECKOUT
+echo '/libcxxabi/*' >> $FILES_TO_CHECKOUT
+echo '!/libcxxabi/test/*' >> $FILES_TO_CHECKOUT
+echo '/libcxx/*' >> $FILES_TO_CHECKOUT
 echo '!/libcxx/test/*' >> $FILES_TO_CHECKOUT
+echo '/libunwind/*' >> $FILES_TO_CHECKOUT
+echo '!/libunwind/test/*' >> $FILES_TO_CHECKOUT
+echo '/compiler-rt/*' >> $FILES_TO_CHECKOUT
 echo '!/compiler-rt/test/*' >> $FILES_TO_CHECKOUT
+echo '/cmake/*' >> $FILES_TO_CHECKOUT
 
 git config core.sparsecheckout true
 git checkout $1
