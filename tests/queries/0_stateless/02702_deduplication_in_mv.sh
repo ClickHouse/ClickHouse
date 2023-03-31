f0ba0c7c83b800087f8f7244b8d1c8f5b06b4556
Alfonso Martinez <alfonso@tinybird.co>
2023-03-31 12:18:09 +0200

    Running test without FINAL

--- tests/queries/0_stateless/02702_deduplication_in_mv.sh
+++ tests/queries/0_stateless/02702_deduplication_in_mv.sh
@@ -6,7 +6,7 @@ CURDIR=$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)
 . "$CURDIR"/../shell_config.sh
 
 $CLICKHOUSE_CLIENT --query="CREATE TABLE landing (time DateTime, number Int64) Engine=ReplicatedReplacingMergeTree('/clickhouse/tables/{shard}/{database}/landing','{replica}') PARTITION BY toYYYYMMDD(time) ORDER BY time";
-$CLICKHOUSE_CLIENT --query="CREATE MATERIALIZED VIEW IF NOT EXISTS mv ENGINE = ReplicatedSummingMergeTree('/clickhouse/tables/{shard}/{database}/mv','{replica}') PARTITION BY toYYYYMMDD(hour) ORDER BY hour AS SELECT toStartOfHour(time) AS hour, sum(number) AS sum_amount FROM landing GROUP BY hour";
+$CLICKHOUSE_CLIENT --query="CREATE MATERIALIZED VIEW mv ENGINE = ReplicatedSummingMergeTree('/clickhouse/tables/{shard}/{database}/mv','{replica}') PARTITION BY toYYYYMMDD(hour) ORDER BY hour AS SELECT toStartOfHour(time) AS hour, sum(number) AS sum_amount FROM landing GROUP BY hour";
 
 printf '2023-09-01 12:23:34, 42,
 '| $CLICKHOUSE_CLIENT --query="INSERT INTO landing (*) settings max_insert_delayed_streams_for_parallel_write=2 FORMAT CSV";
@@ -15,5 +15,8 @@ printf '2023-09-01 12:23:34, 42,
 2022-09-01 12:23:34, 42,
 '| $CLICKHOUSE_CLIENT --query="INSERT INTO landing (*) settings max_insert_delayed_streams_for_parallel_write=2 FORMAT CSV";
 
-$CLICKHOUSE_CLIENT --query="""SELECT * FROM landing FINAL ORDER BY time""";
-$CLICKHOUSE_CLIENT --query="""SELECT * FROM mv FINAL ORDER BY hour""";
+
+$CLICKHOUSE_CLIENT --query="""OPTIMIZE TABLE mv""";
+
+$CLICKHOUSE_CLIENT --query="""SELECT * FROM landing ORDER BY time""";
+$CLICKHOUSE_CLIENT --query="""SELECT * FROM mv ORDER BY hour""";
