61cfad1f6d9c3f301c56df9abd2c722ad57e59bf
natasha <murfel@users.noreply.github.com>
2023-04-03 13:39:17 +0100

    extract hilite comparator into a utility

--- src/Parsers/tests/gtest_format_hiliting.cpp
+++ src/Parsers/tests/gtest_format_hiliting.cpp
@@ -6,6 +6,8 @@
 #include <gtest/gtest.h>
 #include <Common/StackTrace.h>
 
+#include <utils/hilite_comparator/HiliteComparator.h>
+
 String hilite(const String & s, const char * hilite_type)
 {
     return hilite_type + s + DB::IAST::hilite_none;
@@ -41,105 +43,6 @@ String substitution(const String & s)
     return hilite(s, DB::IAST::hilite_substitution);
 }
 
-std::vector<const char *> HILITES =
-    {
-        DB::IAST::hilite_keyword,
-        DB::IAST::hilite_identifier,
-        DB::IAST::hilite_function,
-        DB::IAST::hilite_operator,
-        DB::IAST::hilite_alias,
-        DB::IAST::hilite_substitution,
-        DB::IAST::hilite_none
-    };
-
-[[maybe_unused]] const char * consume_hilites(const char ** ptr_ptr)
-{
-    const char * last_hilite = nullptr;
-    while (true)
-    {
-        bool changed_hilite = false;
-        for (const char * hilite : HILITES)
-        {
-            if (std::string_view(*ptr_ptr).starts_with(hilite))
-            {
-                *ptr_ptr += strlen(hilite);
-                changed_hilite = true;
-                last_hilite = hilite;
-            }
-        }
-        if (!changed_hilite)
-            break;
-    }
-    return last_hilite;
-}
-
-String remove_hilites(std::string_view string)
-{
-    const char * ptr = string.begin();
-    String string_without_hilites;
-    while (true)
-    {
-        consume_hilites(&ptr);
-        if (ptr == string.end())
-            return string_without_hilites;
-        string_without_hilites += *(ptr++);
-    }
-}
-
-bool are_equal_with_hilites_removed(std::string_view left, std::string_view right)
-{
-    return remove_hilites(left) == remove_hilites(right);
-}
-
-/*
- * Hilited queries cannot be compared symbol-by-symbol, as there's some frivolousness introduced with the hilites. Specifically:
- * 1. Whitespaces could be hilited with any hilite type.
- * 2. Hilite could or could be not reset with hilite_none before the next hilite, i.e. the following strings a and b are equal:
- *      a. hilite_keyword foo hilite_none hilite_operator +
- *      b. hilite_keyword foo hilite_operator +
- */
-bool are_equal_with_hilites(std::string_view left, std::string_view right)
-{
-    if (!are_equal_with_hilites_removed(left, right))
-        return false;
-
-    const char * left_it = left.begin();
-    const char * right_it = right.begin();
-    const char * left_hilite = DB::IAST::hilite_none;
-    const char * right_hilite = DB::IAST::hilite_none;
-
-    while (true)
-    {
-        // Consume all prefix hilites, update the current hilite to be the last one.
-        const char * last_hilite = consume_hilites(&left_it);
-        if (last_hilite != nullptr)
-            left_hilite = last_hilite;
-
-        last_hilite = consume_hilites(&right_it);
-        if (last_hilite != nullptr)
-            right_hilite = last_hilite;
-
-        if (left_it == left.end() && right_it == right.end())
-            return true;
-
-        if (left_it == left.end() || right_it == right.end())
-            return false;
-
-        // Lookup one character.
-        // Check characters match.
-        // Redundant check, given the hilite-ignorant comparison at the beginning, but let's keep it just in case.
-        if (*left_it != *right_it)
-            return false;
-
-        // Check hilites match if it's not a whitespace.
-        if (!std::isspace(*left_it) && left_hilite != right_hilite)
-            return false;
-
-        // Consume one character.
-        left_it++;
-        right_it++;
-    }
-}
 
 TEST(FormatHiliting, MetaTestConsumeHilites)
 {
---
+++ utils/hilite_comparator/HiliteComparator.cpp
@@ -0,0 +1,22 @@
+#include "HiliteComparator.h"
+
+namespace HiliteComparator {
+
+std::vector<const char *> HILITES;
+
+[[maybe_unused]] const char * consume_hilites(const char ** ptr_ptr);
+
+String remove_hilites(std::string_view string);
+
+bool are_equal_with_hilites_removed(std::string_view left, std::string_view right);
+
+/*
+ * Hilited queries cannot be compared symbol-by-symbol, as there's some frivolousness introduced with the hilites. Specifically:
+ * 1. Whitespaces could be hilited with any hilite type.
+ * 2. Hilite could or could be not reset with hilite_none before the next hilite, i.e. the following strings a and b are equal:
+ *      a. hilite_keyword foo hilite_none hilite_operator +
+ *      b. hilite_keyword foo hilite_operator +
+ */
+bool are_equal_with_hilites(std::string_view left, std::string_view right);
+
+}
