a39c9b3db81f3f2000ffbb08b4879cc786136aa7
MikhailBurdukov <burdukvmikhail@gmail.com>
2023-04-05 17:09:16 +0000

    Fix build

--- src/Databases/DDLLoadingDependencyVisitor.cpp
+++ src/Databases/DDLLoadingDependencyVisitor.cpp
@@ -138,13 +138,15 @@ void DDLLoadingDependencyVisitor::extractTableNameFromArgument(const ASTFunction
     if (const auto * dict_function = arg->as<ASTFunction>())
     {
         if (!functionIsDictGet(dict_function->name))
-        return;
+            return;
+        
         /// Get the dictionary name from `dict*` function.
         const auto * literal_arg = dict_function->arguments->as<ASTExpressionList>()->children[0].get();
         const auto * dictionary_name = literal_arg->as<ASTLiteral>();
 
         if (!dictionary_name)
             return;
+        
         if (dictionary_name->value.getType() != Field::Types::String)
             return;
 
