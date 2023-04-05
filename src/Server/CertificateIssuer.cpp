77d0a4d84223f6135dafa37aad0291e51399add3
Sergey Lokhmatikov <lokhmatikov.htc@gmail.com>
2023-04-05 13:32:34 +0000

    Done main architecture, need to implement acme protocol

--- src/Server/CertificateIssuer.cpp
+++ src/Server/CertificateIssuer.cpp
@@ -7,8 +7,23 @@
 namespace DB
 {
 
-void CertificateIssuer::UpdateCertificates(){
-     LOG_ERROR(log, "AAAAAAAAAAAA\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\nAAAAAAAAAAAA");
+void CertificateIssuer::UpdateCertificates(const LetsEncryptConfigurationData & config_data, std::function<void()> callback)
+{
+    LOG_ERROR(log, "AAAAAAAAAAAA\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\nAAAAAAAAAAAA {}", config_data.reissue_hours_before);
+    callback();
+
+}
+
+void CertificateIssuer::UpdateCertificatesIfNeeded(const Poco::Util::AbstractConfiguration & config){
+    if (config.getBool("LetsEncrypt.enableAutomaticIssue", false))
+        //TODO: Проверить что если серты есть и не устарели, то не перезапрашивать
+        UpdateCertificates(LetsEncryptConfigurationData(config), [](){});
+}
+
+CertificateIssuer::LetsEncryptConfigurationData::LetsEncryptConfigurationData(const Poco::Util::AbstractConfiguration & config)
+{
+    is_issuing_enabled = config.getBool("LetsEncrypt.enableAutomaticIssue", false);
+    reissue_hours_before = config.getInt("LetsEncrypt.reissueHoursBefore", 48);
 }
 
 }
--- src/Server/CertificateReloader.h
+++ src/Server/CertificateReloader.h
@@ -21,6 +21,16 @@
 #include <unordered_map>
 #include <mutex>
 
+#include <Poco/Logger.h>
+#include <Poco/Util/AbstractConfiguration.h>
+#include <openssl/ssl.h>
+#include <openssl/x509v3.h>
+#include <Poco/Crypto/RSAKey.h>
+#include <Poco/Crypto/X509Certificate.h>
+#include <Common/MultiVersion.h>
+#include <Common/Logger.h>
+#include <Server/CertificateIssuer.h>
+
 
 namespace DB
 {
@@ -43,6 +53,17 @@ public:
 
         Data(std::string cert_path, std::string key_path, std::string pass_phrase);
     };
+    /// Initialize the callback and perform the initial cert loading
+    void init();
+
+    /// Handle configuration reload
+    void tryLoad(const Poco::Util::AbstractConfiguration & config);
+
+    /// Reload certificates
+    void reloadCertificates();
+
+    /// A callback for OpenSSL
+    int setCertificate(SSL * ssl);
 
     struct File
     {
