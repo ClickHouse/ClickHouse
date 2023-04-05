8bd6fa471de9333878fafb8f9229cae8204faf37
kssenii <sumarokovakseniia@mail.ru>
2023-04-05 17:36:20 +0200

    Remove too noisy logging

--- src/Interpreters/Cache/LRUFileCachePriority.h
+++ src/Interpreters/Cache/LRUFileCachePriority.h
@@ -2,7 +2,6 @@
 
 #include <list>
 #include <Interpreters/Cache/IFileCachePriority.h>
-#include <Common/logger_useful.h>
 
 namespace DB
 {
@@ -33,7 +32,6 @@ public:
 
 private:
     LRUQueue queue;
-    Poco::Logger * log = &Poco::Logger::get("LRUFileCachePriority");
 };
 
 class LRUFileCachePriority::LRUFileCacheIterator : public IFileCachePriority::IIterator
