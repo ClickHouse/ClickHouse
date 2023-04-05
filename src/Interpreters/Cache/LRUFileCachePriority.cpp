4fd983967b7a13f1354444a393a38ec9a289d44a
kssenii <sumarokovakseniia@mail.ru>
2023-04-05 17:36:20 +0200

    Remove too noisy logging

--- src/Interpreters/Cache/LRUFileCachePriority.cpp
+++ src/Interpreters/Cache/LRUFileCachePriority.cpp
@@ -34,8 +34,6 @@ IFileCachePriority::WriteIterator LRUFileCachePriority::add(const Key & key, siz
     CurrentMetrics::add(CurrentMetrics::FilesystemCacheSize, size);
     CurrentMetrics::add(CurrentMetrics::FilesystemCacheElements);
 
-    LOG_TEST(log, "Added entry into LRU queue, key: {}, offset: {}", key.toString(), offset);
-
     return std::make_shared<LRUFileCacheIterator>(this, iter);
 }
 
@@ -53,8 +51,6 @@ void LRUFileCachePriority::removeAll(std::lock_guard<std::mutex> &)
 {
     CurrentMetrics::sub(CurrentMetrics::FilesystemCacheSize, cache_size);
     CurrentMetrics::sub(CurrentMetrics::FilesystemCacheElements, queue.size());
-
-    LOG_TEST(log, "Removed all entries from LRU queue");
 
     queue.clear();
     cache_size = 0;
@@ -87,8 +83,6 @@ void LRUFileCachePriority::LRUFileCacheIterator::removeAndGetNext(std::lock_guar
 
     CurrentMetrics::sub(CurrentMetrics::FilesystemCacheSize, queue_iter->size);
     CurrentMetrics::sub(CurrentMetrics::FilesystemCacheElements);
-
-    LOG_TEST(cache_priority->log, "Removed entry from LRU queue, key: {}, offset: {}", queue_iter->key.toString(), queue_iter->offset);
 
     queue_iter = cache_priority->queue.erase(queue_iter);
 }
