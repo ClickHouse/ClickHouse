f46f098c78edd740fd86a7844e4f9fa43ff64482
avogar <avogar@clickhouse.com>
2023-04-05 09:55:49 +0000

    Better

--- tests/queries/0_stateless/02707_protobuf_unnamed_tuple_as_nested_message.sh
+++ tests/queries/0_stateless/02707_protobuf_unnamed_tuple_as_nested_message.sh
@@ -4,21 +4,7 @@
 CURDIR=$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)
 # shellcheck source=../shell_config.sh
 . "$CURDIR"/../shell_config.sh
+SCHEMADIR=$CURDIR/format_schemas
 
-echo 'syntax = "proto3";
-
-message Nested {
-    int32 a = 1;
-    string b = 2;
-    repeated int32 c = 3;
-};
-
-message Message {
-    Nested x = 1;
-};' > 02707_schema_$CLICKHOUSE_TEST_UNIQUE_NAME.proto
-
-
-$CLICKHOUSE_LOCAL -q "select tuple(42, 'Hello', [1,2,3]) as x format Protobuf settings format_schema='02707_schema_$CLICKHOUSE_TEST_UNIQUE_NAME:Message'" | $CLICKHOUSE_LOCAL --input-format Protobuf --structure='x Tuple(UInt32, String, Array(UInt32))' -q "select * from table" --format_schema="02707_schema_$CLICKHOUSE_TEST_UNIQUE_NAME:Message"
-
-rm 02707_schema_$CLICKHOUSE_TEST_UNIQUE_NAME.proto
+$CLICKHOUSE_LOCAL -q "select tuple(42, 'Hello', [1,2,3]) as x format Protobuf settings format_schema='$SCHEMADIR/02707_schema:Message'" | $CLICKHOUSE_LOCAL --input-format Protobuf --structure='x Tuple(UInt32, String, Array(UInt32))' -q "select * from table" --format_schema="$SCHEMADIR/02707_schema:Message"
 
