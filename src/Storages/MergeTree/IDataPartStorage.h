7b33744618994151348c37cc8bacde9cf92c7bb5
Smita Kulkarni <Smita.Kulkarni@clickhouse.com>
2023-04-06 20:11:40 +0200

    Added a check to clear and not load marks asynchronously from outdated parts

--- src/Storages/MergeTree/IDataPartStorage.h
+++ src/Storages/MergeTree/IDataPartStorage.h
@@ -284,6 +284,8 @@ public:
     /// It may be flush of buffered data or similar.
     virtual void precommitTransaction() = 0;
     virtual bool hasActiveTransaction() const = 0;
+
+    mutable std::atomic<bool> is_part_outdated = false;
 };
 
 using DataPartStoragePtr = std::shared_ptr<const IDataPartStorage>;
