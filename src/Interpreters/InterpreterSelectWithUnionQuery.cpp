90c8e466cfd50a7cd03ff725bcb58898b844cd4a
Saveliy Belyakov <belyakov.saveliy@gmail.com>
2023-04-06 12:39:31 +0300

    True flag in InterpreterSelectWithUnionQuery

--- src/Interpreters/InterpreterSelectWithUnionQuery.cpp
+++ src/Interpreters/InterpreterSelectWithUnionQuery.cpp
@@ -378,7 +378,7 @@ BlockIO InterpreterSelectWithUnionQuery::execute()
     buildQueryPlan(query_plan);
 
     auto builder = query_plan.buildQueryPipeline(
-        QueryPlanOptimizationSettings::fromContext(context),
+        QueryPlanOptimizationSettings::fromContext(context, true),
         BuildQueryPipelineSettings::fromContext(context));
 
     res.pipeline = QueryPipelineBuilder::getPipeline(std::move(*builder));
