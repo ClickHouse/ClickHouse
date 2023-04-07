03e9ebbeea584f22644b9510b061761932521d3d
Sergey Lokhmatikov <lokhmatikov.htc@gmail.com>
2023-04-07 13:33:01 +0000

    We now can send files

--- src/Server/FileRequestHandler.cpp
+++ src/Server/FileRequestHandler.cpp
@@ -5,8 +5,11 @@
 #include <Poco/Net/HTTPServerResponse.h>
 #include <Poco/Util/LayeredConfiguration.h>
 
-#include <IO/HTTPCommon.h>
 #include <Common/getResource.h>
+#include <IO/copyData.h>
+#include <IO/HTTPCommon.h>
+#include <IO/ReadBufferFromFile.h>
+#include <IO/WriteBufferFromString.h>
 
 #include <re2/re2.h>
 
@@ -24,16 +27,20 @@ void FileRequestHandler::handleRequest(HTTPServerRequest & request, HTTPServerRe
 {
     auto keep_alive_timeout = server.config().getUInt("keep_alive_timeout", 10);
 
-    response.setContentType("text/html; charset=UTF-8");
+    response.setContentType("application/octet-stream");
 
     if (request.getVersion() == HTTPServerRequest::HTTP_1_1)
         response.setChunkedTransferEncoding(true);
 
     setResponseDefaultHeaders(response, keep_alive_timeout);
 
+    std::string file_response;
+    DB::WriteBufferFromString out_buffer(file_response);
+    DB::ReadBufferFromFile in_buffer(base_directory_path + request.getURI());
+    DB::copyData(in_buffer, out_buffer);
 
     response.setStatusAndReason(Poco::Net::HTTPResponse::HTTP_OK);
-    *response.send() << getResource(base_directory_path + request.getURI());
+    *response.send() << file_response;
 
 }
 
