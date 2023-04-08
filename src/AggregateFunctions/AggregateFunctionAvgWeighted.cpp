96e50c2b41463219b5b803582f351236f1142997
Mike Kot <myrrc@double.cloud>
2023-04-08 18:27:24 +0000

    fix

--- src/AggregateFunctions/AggregateFunctionAvgWeighted.cpp
+++ src/AggregateFunctions/AggregateFunctionAvgWeighted.cpp
@@ -1,12 +1,9 @@
 #include <AggregateFunctions/AggregateFunctionFactory.h>
-#include "Interpreters/executeQuery.h"
 
 namespace DB
 {
 void registerAggregateFunctionAvgWeighted(AggregateFunctionFactory &)
 {
-    //const std::string_view udf = "create function avg as (x) -> sum(x) / count(x)";
-    const std::string_view udf = "create function avgWeighted as (x, w) -> toFloat64(sum(x * w) / sum(w))";
-    auto block = executeQuery(udf, getContext(), /*internal*/true);
+    //"create function avgWeighted as (x, w) -> sum(x * w) / sum(w)";
 }
 }
