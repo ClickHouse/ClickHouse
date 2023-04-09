0bc1617a4193ae8bb54356988b486ed0a769b033
FriendLey <friendley@zju.edu.cn>
2023-04-09 17:24:54 +0800

    update code style.

--- src/Functions/soundex.cpp
+++ src/Functions/soundex.cpp
@@ -6,43 +6,40 @@
 
 namespace DB
 {
-namespace ErrorCodes
-{
-    extern const int ILLEGAL_TYPE_OF_ARGUMENT;
-}
-
-namespace
-{
 
 struct SoundexImpl
 {
     static constexpr auto name = "soundex";
     static constexpr size_t length = 4;
     // enum { length = 4 };
-                                      /* ABCDEFGHIJKLMNOPQRSTUVWXYZ */
-                                      /* :::::::::::::::::::::::::: */
+    /* ABCDEFGHIJKLMNOPQRSTUVWXYZ */
+    /* :::::::::::::::::::::::::: */
     static constexpr auto soundex_map = "01230120022455012623010202";
 
-    static char get_scode(const char **ptr, const char *in_end) {
-        while (*ptr < in_end && !std::isalpha(**ptr)) {
+    static char get_scode(const char ** ptr, const char * in_end)
+    {
+        while (*ptr < in_end && !std::isalpha(**ptr))
+        {
             (*ptr)++;
         }
-        if (*ptr == in_end) return 0;
+        if (*ptr == in_end)
+            return 0;
         return soundex_map[std::toupper(**ptr) - 'A'];
     }
 
-    static void apply(const char* begin, const size_t size, unsigned char * out_char_data)
+    static void apply(const char * begin, const size_t size, unsigned char * out_char_data)
     {
-
-        const char* in_cur = begin;
-        const char* in_end = begin + size;
+        const char * in_cur = begin;
+        const char * in_end = begin + size;
         unsigned char * out_end = out_char_data + length;
 
-        while (in_cur < in_end && !std::isalpha(*in_cur)) {
+        while (in_cur < in_end && !std::isalpha(*in_cur))
+        {
             in_cur++;
         }
-        if (in_cur < in_end) {
-          *out_char_data++ = std::toupper(*in_cur);
+        if (in_cur < in_end)
+        {
+            *out_char_data++ = std::toupper(*in_cur);
         }
         char last_ch = get_scode(&in_cur, in_end);
 
@@ -50,23 +47,24 @@ struct SoundexImpl
         in_cur++;
         while (in_cur < in_end && out_char_data < out_end && (ch = get_scode(&in_cur, in_end)) != 0)
         {
-            if (in_cur == in_end) break;
+            if (in_cur == in_end)
+            {
+                break;
+            }
             in_cur++;
             if ((ch != '0') && (ch != last_ch))
             {
-              *out_char_data++ = ch;
+                *out_char_data++ = ch;
             }
             last_ch = ch;
         }
-        while (out_char_data < out_end) {
-          *out_char_data++ = '0';
+        while (out_char_data < out_end)
+        {
+            *out_char_data++ = '0';
         }
         return;
     }
-
 };
-
-}
 
 REGISTER_FUNCTION(Soundex)
 {
