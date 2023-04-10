9868dd750e94d1f3ffe89d636d598bd866f25c2d
flynn <fenglv15@mails.ucas.ac.cn>
2023-04-10 13:53:43 +0000

    fix

--- tests/queries/0_stateless/02710_show_table.sql
+++ tests/queries/0_stateless/02710_show_table.sql
@@ -1,7 +1,9 @@
 DROP TABLE IF EXISTS t_2710_show_table;
 
-CREATE TABLE t_2710_show_table(n1 UInt32, s String) engine=MergeTree order by n1 SETTINGS index_granularity = 8192;
+CREATE TABLE t_2710_show_table(n1 UInt32, s String) engine=Log;
 SHOW TABLE t_2710_show_table;
+SHOW CREATE TABLE t_2710_show_table;
+SHOW CREATE t_2710_show_table;
 
 DROP TABLE t_2710_show_table;
 
