3f1deb3da76382696c779b52ed59191345846603
Artem Pershin <artem.pershin@genestack.com>
2023-04-10 15:27:30 +0300

    Fix tests

--- tests/queries/0_stateless/02711_information_schema_is_nullable_lowcadinality.sql
+++ tests/queries/0_stateless/02711_information_schema_is_nullable_lowcadinality.sql
@@ -1,9 +1,9 @@
-CREATE TABLE users (low_card_nullable_name LowCardinality(Nullable(String)), name String, nullable_name Nullable(String)) ENGINE = Memory;
+CREATE TABLE users_low_cardinality_nullable (low_card_nullable_name LowCardinality(Nullable(String)), name String, nullable_name Nullable(String)) ENGINE = Memory;
 
 SELECT
     column_name,
     data_type,
     is_nullable
 FROM information_schema.columns
-WHERE (table_schema = 'default') AND (table_name = 'users')
+WHERE (table_name = 'users_low_cardinality_nullable')
 ORDER BY column_name;
