e4470d3c2b8f203d451bac43c8db15a290240ebb
Artem Pershin <artem.pershin@genestack.com>
2023-04-11 00:04:24 +0300

    Fix tests

--- tests/queries/0_stateless/02711_information_schema_is_nullable_lowcadinality.sql
+++ tests/queries/0_stateless/02711_information_schema_is_nullable_lowcadinality.sql
@@ -5,5 +5,5 @@ SELECT
     data_type,
     is_nullable
 FROM information_schema.columns
-WHERE (table_name = 'users_low_cardinality_nullable')
+WHERE (table_schema = currentDatabase()) AND (table_name = 'users_low_cardinality_nullable')
 ORDER BY column_name;
