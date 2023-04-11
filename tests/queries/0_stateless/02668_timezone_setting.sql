e36addb96a7eaaba8f9a90383d3e77020a1a61e8
Vasily Nemkov <V.Nemkov@gmail.com>
2023-04-11 13:03:03 +0200

    Hackish way of setting up timezone on the client
    Warning: lots of debug logging

--- tests/queries/0_stateless/02668_timezone_setting.sql
+++ tests/queries/0_stateless/02668_timezone_setting.sql
@@ -1,9 +1,11 @@
+SET timezone = 'Абырвалг'; -- { serverError BAD_ARGUMENTS}
+
 SET timezone = 'Asia/Novosibirsk';
 SELECT toDateTime64(toDateTime64('1999-12-12 23:23:23.123', 3), 3, 'Europe/Zurich');
 SELECT toDateTime64(toDateTime64('1999-12-12 23:23:23.123', 3), 3, 'Europe/Zurich') SETTINGS timezone = 'Europe/Zurich';
 
-SET timezone = 'Europe/Zurich';
+SET timezone = 'Asia/Manila';
 SELECT toDateTime64(toDateTime64('1999-12-12 23:23:23.123', 3), 3, 'Asia/Novosibirsk');
 
-SET timezone = 'Абырвалг';
-select now(); -- { serverError POCO_EXCEPTION }
+SELECT timezone(), serverTimeZone(), timezoneOf(now()) SETTINGS timezone = 'Europe/Zurich';
+SELECT timezone(), serverTimeZone(), timezoneOf(now()) SETTINGS timezone = 'Pacific/Pitcairn';
