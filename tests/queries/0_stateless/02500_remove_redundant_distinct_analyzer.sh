7dfbdce0cf3956d14b42e31465c598198b031b00
Igor Nikonov <igor@clickhouse.com>
2023-04-12 10:09:27 +0000

    Fix shellcheck

--- tests/queries/0_stateless/02500_remove_redundant_distinct_analyzer.sh
+++ tests/queries/0_stateless/02500_remove_redundant_distinct_analyzer.sh
@@ -4,4 +4,5 @@ CURDIR=$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)
 # shellcheck source=../shell_config.sh
 . "$CURDIR"/../shell_config.sh
 
+# shellcheck source=./02496_remove_redundant_distinct.sh
 ENABLE_ANALYZER=1 . "$CURDIR"/02500_remove_redundant_distinct.sh
