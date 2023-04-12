bded857c736ce3b86ea05d4d39e1e91bb4c2314f
lgbo-ustc <lgbo.ustc@gmail.com>
2023-04-12 16:10:30 +0800

    update test case

--- tests/queries/0_stateless/02713_group_array_nullable.sql
+++ tests/queries/0_stateless/02713_group_array_nullable.sql
@@ -1,2 +1,5 @@
-SELECT groupArray(number) FROM numbers(10) SETTINGS aggregate_functions_null_for_empty = 1;
-SELECT groupArrayLast(2)(number) FROM numbers(10) SETTINGS aggregate_functions_null_for_empty = 1;
+SET aggregate_functions_null_for_empty = 1;
+	
+SELECT groupArray(1);
+SELECT groupArray(number) FROM numbers(10);
+SELECT groupArrayLast(2)(number) FROM numbers(10);
