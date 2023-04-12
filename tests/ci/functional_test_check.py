f8e4d6bb41eae52d2dcc0da9284fffedc8d1c134
Dmitry Novik <n0vik@clickhouse.com>
2023-04-12 15:18:28 +0000

    Add CI run with new analyzer enabled

--- tests/ci/functional_test_check.py
+++ tests/ci/functional_test_check.py
@@ -53,6 +53,8 @@ def get_additional_envs(check_name, run_by_hash_num, run_by_hash_total):
         result.append("USE_PARALLEL_REPLICAS=1")
     if "s3 storage" in check_name:
         result.append("USE_S3_STORAGE_FOR_MERGE_TREE=1")
+    if "analyzer" in check_name:
+        result.append("USE_NEW_ANALYZER=1")
 
     if run_by_hash_total != 0:
         result.append(f"RUN_BY_HASH_NUM={run_by_hash_num}")
