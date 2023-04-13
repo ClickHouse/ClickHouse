64d840514748a31903a9825c93b9b09e21b3841d
serxa <sergei@clickhouse.com>
2023-04-13 18:43:58 +0000

    add dependency cycle detection

--- src/Common/tests/gtest_async_loader.cpp
+++ src/Common/tests/gtest_async_loader.cpp
@@ -64,3 +64,47 @@ TEST(AsyncLoader, Smoke)
 
     t.loader.stop();
 }
+
+TEST(AsyncLoader, CycleDetection)
+{
+    AsyncLoaderTest t;
+
+    auto job_func = [&] (const LoadJob &) {};
+
+    LoadJobPtr cycle_breaker; // To avoid memleak we introduce with a cycle
+
+    try
+    {
+        std::vector<LoadJobPtr> jobs;
+        jobs.push_back(makeLoadJob({}, "job0", job_func));
+        jobs.push_back(makeLoadJob({ jobs[0] }, "job1", job_func));
+        jobs.push_back(makeLoadJob({ jobs[0], jobs[1] }, "job2", job_func));
+        jobs.push_back(makeLoadJob({ jobs[0], jobs[2] }, "job3", job_func));
+
+        // Actually it is hard to construct a cycle, but suppose someone was able to succeed violating constness
+        const_cast<LoadJobSet &>(jobs[1]->dependencies).insert(jobs[3]);
+        cycle_breaker = jobs[1];
+
+        // Add couple unrelated jobs
+        jobs.push_back(makeLoadJob({ jobs[1] }, "job4", job_func));
+        jobs.push_back(makeLoadJob({ jobs[4] }, "job5", job_func));
+        jobs.push_back(makeLoadJob({ jobs[3] }, "job6", job_func));
+        jobs.push_back(makeLoadJob({ jobs[1], jobs[2], jobs[3], jobs[4], jobs[5], jobs[6] }, "job7", job_func));
+
+        // Also add another not connected jobs
+        jobs.push_back(makeLoadJob({}, "job8", job_func));
+        jobs.push_back(makeLoadJob({}, "job9", job_func));
+        jobs.push_back(makeLoadJob({ jobs[9] }, "job10", job_func));
+
+        auto task1 = t.loader.schedule({ jobs.begin(), jobs.end()});
+        FAIL();
+    }
+    catch (Exception & e)
+    {
+        int present[] = { 0, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0 };
+        for (int i = 0; i < std::size(present); i++)
+            ASSERT_EQ(e.message().find(fmt::format("job{}", i)) != String::npos, present[i]);
+    }
+
+    const_cast<LoadJobSet &>(cycle_breaker->dependencies).clear();
+}
