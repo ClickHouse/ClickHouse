3c9aa39e4f4542c6cdd9f6afecda4289812866a2
Arthur Passos <arthur.ti@outlook.com>
2023-04-13 09:55:51 -0300

    fix escap sequences unit tests, values are not allowed to start with escape sequences anymore

--- src/Functions/keyvaluepair/tests/gtest_escaping_key_value_pair_extractor.cpp
+++ src/Functions/keyvaluepair/tests/gtest_escaping_key_value_pair_extractor.cpp
@@ -24,7 +24,7 @@ TEST(extractKVPairEscapingKeyValuePairExtractor, EscapeSequences)
     auto keys = ColumnString::create();
     auto values = ColumnString::create();
 
-    auto pairs_count = extractor->extract(R"(key1:\xFF key2:\n\t\r)"sv, keys, values);
+    auto pairs_count = extractor->extract(R"(key1:a\xFF key2:a\n\t\r)"sv, keys, values);
 
     ASSERT_EQ(pairs_count, 2u);
     ASSERT_EQ(keys->size(), pairs_count);
@@ -33,8 +33,8 @@ TEST(extractKVPairEscapingKeyValuePairExtractor, EscapeSequences)
     ASSERT_EQ(keys->getDataAt(0).toView(), "key1");
     ASSERT_EQ(keys->getDataAt(1).toView(), "key2");
 
-    assert_byte_equality(values->getDataAt(0), {0xFF});
-    assert_byte_equality(values->getDataAt(1), {0xA, 0x9, 0xD});
+    assert_byte_equality(values->getDataAt(0), {'a', 0xFF});
+    assert_byte_equality(values->getDataAt(1), {'a', 0xA, 0x9, 0xD});
 }
 
 }
