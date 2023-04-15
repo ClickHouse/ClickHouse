f3584a3506229ce3bd8a7fb066377ab9dc6bbed7
Vitaly Baranov <vitlibar@clickhouse.com>
2023-04-15 11:28:27 +0200

    Allow starting integration tests without spark.

--- tests/integration/helpers/cluster.py
+++ tests/integration/helpers/cluster.py
@@ -378,7 +378,6 @@ class ClickHouseCluster:
         self.env_variables["ASAN_OPTIONS"] = "use_sigaltstack=0"
         self.env_variables["CLICKHOUSE_WATCHDOG_ENABLE"] = "0"
         self.env_variables["CLICKHOUSE_NATS_TLS_SECURE"] = "0"
-        self.env_variables["SPARK_PATH"] = os.environ.get("SPARK_HOME")
         self.up_called = False
 
         custom_dockerd_host = custom_dockerd_host or os.environ.get(
@@ -621,6 +620,7 @@ class ClickHouseCluster:
             logging.debug(f"Removed :{self.instances_dir}")
 
         if with_spark:
+            self.env_variables["SPARK_PATH"] = os.environ.get("SPARK_HOME")
             # if you change packages, don't forget to update them in docker/test/integration/runner/dockerd-entrypoint.sh
             (
                 pyspark.sql.SparkSession.builder.appName("spark_test")
