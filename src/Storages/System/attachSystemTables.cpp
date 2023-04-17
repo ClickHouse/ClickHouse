9f969c418ea34f2be93caa1f0c1030fc70af37b8
Nikita Mikhaylov <mikhaylovnikitka@gmail.com>
2023-04-17 23:12:04 +0200

    Update src/Storages/System/attachSystemTables.cpp
    Co-authored-by: Dmitry Novik <n0vik@clickhouse.com>

--- src/Storages/System/attachSystemTables.cpp
+++ src/Storages/System/attachSystemTables.cpp
@@ -135,7 +135,7 @@ void attachSystemTablesLocal(ContextPtr context, IDatabase & system_database)
     attach<StorageSystemUserDirectories>(context, system_database, "user_directories", "Contains the information about configured user directories - directories on the file system from which ClickHouse server is allowed to read user provided data.");
     attach<StorageSystemPrivileges>(context, system_database, "privileges", "Contains a list of all available privileges that could be granted to a user or role.");
     attach<StorageSystemErrors>(context, system_database, "errors", "Contains a list of all error which have ever happened including the error code, last time and message with unsymbolized stacktrace.");
-    attach<StorageSystemWarnings>(context, system_database, "warnings", "Contains warnings about server configuration to be displayed in clickhouse-client one any is connected.");
+    attach<StorageSystemWarnings>(context, system_database, "warnings", "Contains warnings about server configuration to be displayed by clickhouse-client right after it connects to the server.");
     attach<StorageSystemDataSkippingIndices>(context, system_database, "data_skipping_indices", "Contains all the information about all the data skipping indices in tables, similar to system.columns.");
     attach<StorageSystemLicenses>(context, system_database, "licenses", "Сontains licenses of third-party libraries that are located in the contrib directory of ClickHouse sources.");
     attach<StorageSystemTimeZones>(context, system_database, "time_zones", "Contains a list of time zones that are supported by the ClickHouse server. This list of timezones might vary depending on the version of ClickHouse.");
