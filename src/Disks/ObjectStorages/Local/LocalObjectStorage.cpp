a470deafbc02ec3fa7303f294114a663420ef14c
kssenii <sumarokovakseniia@mail.ru>
2023-04-17 19:11:42 +0200

    Fix style check

--- src/Disks/ObjectStorages/Local/LocalObjectStorage.cpp
+++ src/Disks/ObjectStorages/Local/LocalObjectStorage.cpp
@@ -22,10 +22,7 @@ namespace DB
 namespace ErrorCodes
 {
     extern const int NOT_IMPLEMENTED;
-}
-
-namespace ErrorCodes
-{
+    extern const int BAD_ARGUMENTS;
     extern const int CANNOT_UNLINK;
 }
 
