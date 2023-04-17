929336f61509efe048dcc7f10d3fce0b9839fe3b
Konstantin Bogdanov <var1able@var1able.ru>
2023-04-17 16:52:30 +0200

    Prototype

--- src/Common/ZooKeeper/ZooKeeperConstants.h
+++ src/Common/ZooKeeper/ZooKeeperConstants.h
@@ -37,6 +37,7 @@ enum class OpNum : int32_t
     // CH Keeper specific operations
     FilteredList = 500,
     CheckNotExists = 501,
+    CreateIfNotExists = 502,
 
     SessionID = 997, /// Special internal request
 };
