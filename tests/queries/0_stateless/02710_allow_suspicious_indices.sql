a975211185094978833e74930b0a0dab513ad148
凌涛 <lingtaolf@gmail.com>
2023-04-18 10:26:35 +0800

    add more tests

--- tests/queries/0_stateless/02710_allow_suspicious_indices.sql
+++ tests/queries/0_stateless/02710_allow_suspicious_indices.sql
@@ -3,11 +3,7 @@ CREATE TABLE allow_suspicious_indices (id UInt32) ENGINE = MergeTree() order by 
 DROP TABLE IF EXISTS allow_suspicious_indices;
 CREATE TABLE allow_suspicious_indices (id UInt32) ENGINE = MergeTree() order by (id, id) settings allow_suspicious_indices = 0;  -- { serverError BAD_ARGUMENTS }
 DROP TABLE IF EXISTS allow_suspicious_indices;
-CREATE TABLE allow_suspicious_indices (id UInt32, index idx (id, id) TYPE minmax GRANULARITY 1) ENGINE = MergeTree() order by id settings allow_suspicious_indices = 1;
-DROP TABLE IF EXISTS allow_suspicious_indices;
 CREATE TABLE allow_suspicious_indices (id UInt32, index idx (id, id) TYPE minmax) ENGINE = MergeTree() order by id settings allow_suspicious_indices = 1;
-DROP TABLE IF EXISTS allow_suspicious_indices;
-CREATE TABLE allow_suspicious_indices (id UInt32, index idx (id, id) TYPE minmax GRANULARITY 1) ENGINE = MergeTree() order by id settings allow_suspicious_indices = 0;  -- { serverError BAD_ARGUMENTS }
 DROP TABLE IF EXISTS allow_suspicious_indices;
 CREATE TABLE allow_suspicious_indices (id UInt32, index idx (id, id) TYPE minmax) ENGINE = MergeTree() order by id settings allow_suspicious_indices = 0;  -- { serverError BAD_ARGUMENTS }
 DROP TABLE IF EXISTS allow_suspicious_indices;
@@ -16,5 +12,11 @@ ALTER TABLE allow_suspicious_indices ADD COLUMN `id2` UInt32, MODIFY ORDER BY (i
 DROP TABLE IF EXISTS allow_suspicious_indices;
 CREATE TABLE allow_suspicious_indices (id UInt32) ENGINE = MergeTree() order by id + 1 settings allow_suspicious_indices = 0;
 ALTER TABLE allow_suspicious_indices ADD COLUMN `id2` UInt32, MODIFY ORDER BY (id + 1, id2 + 1, id + 1);  -- { serverError BAD_ARGUMENTS }
+DROP TABLE IF EXISTS allow_suspicious_indices;  
+CREATE TABLE allow_suspicious_indices (id UInt32) ENGINE = MergeTree() order by id + 1 settings allow_suspicious_indices = 0;
+ALTER TABLE allow_suspicious_indices ADD INDEX idx (id+1, id+1) type minmax;  -- { serverError BAD_ARGUMENTS }
+DROP TABLE IF EXISTS allow_suspicious_indices;  
+CREATE TABLE allow_suspicious_indices (id UInt32) ENGINE = MergeTree() order by id + 1 settings allow_suspicious_indices = 1;
+ALTER TABLE allow_suspicious_indices ADD INDEX idx (id+1, id+1) type minmax;
 DROP TABLE IF EXISTS allow_suspicious_indices;  
 
