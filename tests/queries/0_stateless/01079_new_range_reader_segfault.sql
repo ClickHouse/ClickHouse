50064f261b02d20273b39a87674bd5a376fb763a
Robert Schulze <robert@clickhouse.com>
2023-04-19 15:40:06 +0000

    Replace by rowNumberInBlock()

--- tests/queries/0_stateless/01079_new_range_reader_segfault.sql
+++ tests/queries/0_stateless/01079_new_range_reader_segfault.sql
@@ -3,9 +3,9 @@ drop table if exists t;
 create table t (a Int) engine = MergeTree order by a;
 
 -- some magic to satisfy conditions to run optimizations in MergeTreeRangeReader
-insert into t select number < 20000 ? 0 : 1 from numbers(50000);
+insert into t select number < 20 ? 0 : 1 from numbers(50);
 alter table t add column s String default 'foo';
 
-select s from t prewhere a != 1 where rand() % 2 = 0 limit 1;
+select s from t prewhere a != 1 where rowNumberInBlock() % 2 = 0 limit 1;
 
 drop table t;
