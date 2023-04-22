f07e1fb29edd7f6ead140703dea4bf0f50190888
Rory Crispin <Rory.crispin@clickhouse.com>
2023-04-22 17:02:05 +0100

    trailing whitespace

--- src/Dictionaries/getDictionaryConfigurationFromAST.cpp
+++ src/Dictionaries/getDictionaryConfigurationFromAST.cpp
@@ -573,7 +573,7 @@ void checkPrimaryKey(const AttributeNameToConfiguration & all_attrs, const Names
 
 void checkLifetime(const ASTCreateQuery & query)
 {
-    if (query.dictionary->layout && query.dictionary->layout->layout_type == "direct") 
+    if (query.dictionary->layout && query.dictionary->layout->layout_type == "direct")
     {
         if (query.dictionary->lifetime)
             throw Exception(
