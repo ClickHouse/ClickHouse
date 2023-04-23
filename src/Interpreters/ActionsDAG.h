49b0e8e6c35cc57fb55b0e050b71587f7a0b7eb0
liuneng <1398775315@qq.com>
2023-04-23 15:07:11 +0800

    change for complex type from liyang

--- src/Interpreters/ActionsDAG.h
+++ src/Interpreters/ActionsDAG.h
@@ -390,6 +390,7 @@ private:
     Node & addNode(Node node);
 
     const Node & addFunctionImpl(
+        const FunctionOverloadResolverPtr & function,
         const FunctionBasePtr & function_base,
         NodeRawConstPtrs children,
         ColumnsWithTypeAndName arguments,
