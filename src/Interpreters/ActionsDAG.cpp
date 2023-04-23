c4a677a85e61a4ff8bf05b117b1ed4699795914a
liuneng <1398775315@qq.com>
2023-04-23 15:07:11 +0800

    change for complex type from liyang
    (cherry picked from commit 70c7f84d620c049d52aab9e57c6b0304faeae8d6)
    (cherry picked from commit d85def8395ec00f048ce70f1607bade63f640f5f)
    (cherry picked from commit 48eaea5b5247303f6862945d11d83802c3ecde73)
    (cherry picked from commit 1d8b42d3096b0035f16621d0d857901d2ea87e3c)

--- src/Interpreters/ActionsDAG.cpp
+++ src/Interpreters/ActionsDAG.cpp
@@ -349,6 +349,7 @@ const ActionsDAG::Node & ActionsDAG::addFunction(
 
     auto function_base = function->build(arguments);
     return addFunctionImpl(
+        function,
         function_base,
         std::move(children),
         std::move(arguments),
@@ -365,6 +366,7 @@ const ActionsDAG::Node & ActionsDAG::addFunction(
     auto [arguments, all_const] = getFunctionArguments(children);
 
     return addFunctionImpl(
+        {},
         function.getFunction(),
         std::move(children),
         std::move(arguments),
@@ -381,6 +383,7 @@ const ActionsDAG::Node & ActionsDAG::addFunction(
     auto [arguments, all_const] = getFunctionArguments(children);
 
     return addFunctionImpl(
+        {},
         function_base,
         std::move(children),
         std::move(arguments),
@@ -406,6 +409,7 @@ const ActionsDAG::Node & ActionsDAG::addCast(const Node & node_to_cast, const Da
 }
 
 const ActionsDAG::Node & ActionsDAG::addFunctionImpl(
+    const FunctionOverloadResolverPtr & function,
     const FunctionBasePtr & function_base,
     NodeRawConstPtrs children,
     ColumnsWithTypeAndName arguments,
@@ -422,6 +426,7 @@ const ActionsDAG::Node & ActionsDAG::addFunctionImpl(
     node.function_base = function_base;
     node.result_type = result_type;
     node.function = node.function_base->prepare(arguments);
+    node.function->setResolver(function); /// It's needed for spark compatibility.
 
     /// If all arguments are constants, and function is suitable to be executed in 'prepare' stage - execute function.
     if (node.function_base->isSuitableForConstantFolding())
@@ -3350,6 +3355,7 @@ std::optional<ActionsDAG> ActionsDAG::buildFilterActionsDAG(
                 auto function_base = function_overload_resolver ? function_overload_resolver->build(arguments) : node->function_base;
 
                 result_node = &result_dag.addFunctionImpl(
+                    function_overload_resolver,
                     function_base,
                     std::move(function_children),
                     std::move(arguments),
--- src/Interpreters/ActionsDAG.h
+++ src/Interpreters/ActionsDAG.h
@@ -537,6 +537,7 @@ private:
     Node & addNode(Node node);
 
     const Node & addFunctionImpl(
+        const FunctionOverloadResolverPtr & function,
         const FunctionBasePtr & function_base,
         NodeRawConstPtrs children,
         ColumnsWithTypeAndName arguments,
