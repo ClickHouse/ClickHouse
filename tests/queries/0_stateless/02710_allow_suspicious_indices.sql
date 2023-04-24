f26a3875992abc6ac8250a40c62e763b7e4aaeb3
Robert Schulze <robert@clickhouse.com>
2023-04-24 08:54:14 +0000

    Some fixups

--- tests/queries/0_stateless/02710_allow_suspicious_indices.sql
+++ tests/queries/0_stateless/02710_allow_suspicious_indices.sql
@@ -1,28 +1,24 @@
-DROP TABLE IF EXISTS allow_suspicious_indices;
-CREATE TABLE allow_suspicious_indices (id UInt32) ENGINE = MergeTree() order by (id, id) settings allow_suspicious_indices = 1;
-DROP TABLE IF EXISTS allow_suspicious_indices;
-CREATE TABLE allow_suspicious_indices (id UInt32) ENGINE = MergeTree() order by (id, id) settings allow_suspicious_indices = 0;  -- { serverError BAD_ARGUMENTS }
-DROP TABLE IF EXISTS allow_suspicious_indices;
-CREATE TABLE allow_suspicious_indices (id UInt32, index idx (id, id) TYPE minmax) ENGINE = MergeTree() order by id settings allow_suspicious_indices = 1;
-DROP TABLE IF EXISTS allow_suspicious_indices;
-CREATE TABLE allow_suspicious_indices (id UInt32, index idx (id, id) TYPE minmax) ENGINE = MergeTree() order by id settings allow_suspicious_indices = 0;  -- { serverError BAD_ARGUMENTS }
-DROP TABLE IF EXISTS allow_suspicious_indices;
-CREATE TABLE allow_suspicious_indices (id UInt32) ENGINE = MergeTree() order by id + 1 settings allow_suspicious_indices = 0;
-ALTER TABLE allow_suspicious_indices ADD COLUMN `id2` UInt32, MODIFY ORDER BY (id + 1, id2 + 1, id2 + 1);  -- { serverError BAD_ARGUMENTS }
-DROP TABLE IF EXISTS allow_suspicious_indices;
-CREATE TABLE allow_suspicious_indices (id UInt32) ENGINE = MergeTree() order by id + 1 settings allow_suspicious_indices = 0;
-ALTER TABLE allow_suspicious_indices ADD COLUMN `id2` UInt32, MODIFY ORDER BY (id + 1, id2 + 1, id + 1);  -- { serverError BAD_ARGUMENTS }
-DROP TABLE IF EXISTS allow_suspicious_indices;
-CREATE TABLE allow_suspicious_indices (id UInt32) ENGINE = MergeTree() order by id + 1 settings allow_suspicious_indices = 0;
-ALTER TABLE allow_suspicious_indices ADD INDEX idx (id+1, id+1) type minmax;  -- { serverError BAD_ARGUMENTS }
-DROP TABLE IF EXISTS allow_suspicious_indices;
-CREATE TABLE allow_suspicious_indices (id UInt32) ENGINE = MergeTree() order by id + 1 settings allow_suspicious_indices = 1;
-ALTER TABLE allow_suspicious_indices ADD INDEX idx (id+1, id+1) type minmax;
-DROP TABLE IF EXISTS allow_suspicious_indices;
+-----------------------------------------------------------
+-- Check CREATE TABLE
 
-SET allow_suspicious_indices = 1;
-CREATE TABLE allow_suspicious_indices (id UInt32) ENGINE = MergeTree() order by (id);
-ALTER TABLE allow_suspicious_indices ADD COLUMN `id2` UInt32, MODIFY ORDER BY (id, id2 + 1, id2 + 1);
-DROP TABLE IF EXISTS allow_suspicious_indices;
-CREATE TABLE allow_suspicious_indices (id UInt32) ENGINE = MergeTree() order by (id);
-ALTER TABLE allow_suspicious_indices ADD INDEX idx (id+1, id+1) type minmax;
+DROP TABLE IF EXISTS tbl;
+CREATE TABLE tbl (id UInt32) ENGINE = MergeTree() ORDER BY (id + 1, id + 1);  -- { serverError BAD_ARGUMENTS }
+CREATE TABLE tbl (id UInt32) ENGINE = MergeTree() ORDER BY (id + 1, id + 1) SETTINGS allow_suspicious_indices = 1;
+
+DROP TABLE IF EXISTS tbl;
+CREATE TABLE tbl (id UInt32, INDEX idx (id + 1, id + 1) TYPE minmax) ENGINE = MergeTree() ORDER BY id;  -- { serverError BAD_ARGUMENTS }
+CREATE TABLE tbl (id UInt32, INDEX idx (id + 1, id + 1) TYPE minmax) ENGINE = MergeTree() ORDER BY id SETTINGS allow_suspicious_indices = 1;
+
+-----------------------------------------------------------
+-- Check ALTER TABLE
+
+DROP TABLE IF EXISTS tbl;
+CREATE TABLE tbl (id1 UInt32) ENGINE = MergeTree() ORDER BY id1;
+ALTER TABLE tbl ADD COLUMN `id2` UInt32, MODIFY ORDER BY (id1, id2, id2);  -- { serverError BAD_ARGUMENTS }
+ALTER TABLE tbl ADD COLUMN `id2` UInt32, MODIFY ORDER BY (id1, id2, id1);  -- { serverError BAD_ARGUMENTS }
+ALTER TABLE tbl ADD COLUMN `id2` UInt32, MODIFY ORDER BY (id1, id2, id2) SETTINGS allow_suspicious_indices = 1;
+
+DROP TABLE IF EXISTS tbl;
+CREATE TABLE tbl (id UInt32) ENGINE = MergeTree() ORDER BY id;
+ALTER TABLE tbl ADD INDEX idx (id+1, id, id+1) TYPE minmax;  -- { serverError BAD_ARGUMENTS }
+ALTER TABLE tbl ADD INDEX idx (id+1, id, id+1) TYPE minmax SETTINGS allow_suspicious_indices = 1;
