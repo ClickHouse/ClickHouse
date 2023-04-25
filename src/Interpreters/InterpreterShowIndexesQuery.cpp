eebc1c54c4c199d460ea46037a7f6be49970518b
Robert Schulze <robert@clickhouse.com>
2023-04-25 18:04:19 +0000

    Initial version of SHOW INDEXES

---
+++ src/Interpreters/InterpreterShowIndexesQuery.cpp
@@ -0,0 +1,124 @@
+#include <Interpreters/InterpreterShowIndexesQuery.h>
+
+#include <IO/WriteBufferFromString.h>
+#include <Parsers/ASTShowIndexesQuery.h>
+#include <Parsers/formatAST.h>
+#include <Interpreters/Context.h>
+#include <Interpreters/executeQuery.h>
+#include <IO/Operators.h>
+#include <boost/algorithm/string.hpp>
+
+
+namespace DB
+{
+
+
+InterpreterShowIndexesQuery::InterpreterShowIndexesQuery(const ASTPtr & query_ptr_, ContextMutablePtr context_)
+    : WithMutableContext(context_)
+    , query_ptr(query_ptr_)
+{
+}
+
+
+String InterpreterShowIndexesQuery::getRewrittenQuery()
+{
+    const auto & query = query_ptr->as<ASTShowIndexesQuery &>();
+
+    String database;
+    String table;
+    if (query.from_table.contains("."))
+    {
+        /// FROM <db>.<table> (abbreviated form)
+        chassert(query.from_database.empty());
+        std::vector<String> split;
+        boost::split(split, query.from_table, boost::is_any_of("."));
+        chassert(split.size() == 2);
+        database = split[0];
+        table = split[1];
+    }
+    else if (query.from_database.empty())
+    {
+        /// FROM <table>
+        chassert(!query.from_table.empty());
+        database = getContext()->getCurrentDatabase();
+        table = query.from_table;
+    }
+    else
+    {
+        /// FROM <database> FROM <table>
+        chassert(!query.from_database.empty());
+        chassert(!query.from_table.empty());
+        database = query.from_database;
+        table = query.from_table;
+    }
+
+    WriteBufferFromOwnString where_expression_buf;
+    if (query.where_expression)
+        where_expression_buf << "WHERE (" << query.where_expression << ")";
+    String where_expression = where_expression_buf.str();
+
+    WriteBufferFromOwnString rewritten_query;
+    rewritten_query     << "SELECT * FROM ("
+                        << "(SELECT "
+                            << "name AS table, "
+                            << "0 AS non_unique, "
+                            << "'PRIMARY' AS key_name, "
+                            << "NULL AS seq_in_index, "
+                            << "NULL AS column_name, "
+                            << "'A' AS collation, "
+                            << "NULL AS cardinality, "
+                            << "NULL AS sub_part, "
+                            << "NULL AS packed, "
+                            << "NULL AS null, "
+                            << "'primary' AS index_type, "
+                            << "NULL AS comment, "
+                            << "NULL AS index_comment, "
+                            << "'YES' AS visible, "
+                            << "primary_key AS expression "
+                        << "FROM system.tables "
+                        << "WHERE "
+                            << "database = '" << database << "' "
+                            << "AND name = '" << table << "'"
+                    << ") UNION ALL ("
+                        << "SELECT "
+                            << "table AS table, "
+                            << "0 AS non_unique, "
+                            << "name AS key_name, "
+                            << "NULL AS seq_in_index, "
+                            << "NULL AS column_name, "
+                            << "NULL AS collation, "
+                            << "NULL AS cardinality, "
+                            << "NULL AS sub_part, "
+                            << "NULL AS packed, "
+                            << "NULL AS null, "
+                            << "type AS index_type, "
+                            << "NULL AS comment, "
+                            << "NULL AS index_comment, "
+                            << "'YES' AS visible, "
+                            << "expr AS expression "
+                        << "FROM system.data_skipping_indices "
+                        << "WHERE "
+                            << "database = '" << database << "' "
+                            << "AND table = '" << table << "'"
+                            << ")) "
+                        << where_expression;
+
+    /// Sorting is strictly speaking not necessary but 1. it is convenient for users, 2. SQL currently does not allow to
+    /// sort the output of SHOW COLUMNS otherwise (SELECT * FROM (SHOW COLUMNS ...) ORDER BY ...) is rejected) and 3. some
+    /// SQL tests can take advantage of this.
+    rewritten_query << " ORDER BY index_type, expression";
+
+    std::cout << rewritten_query.str() << std::endl;
+
+    return rewritten_query.str();
+}
+
+
+BlockIO InterpreterShowIndexesQuery::execute()
+{
+    return executeQuery(getRewrittenQuery(), getContext(), true);
+}
+
+
+}
+
--- src/Parsers/ParserShowIndexesQuery.cpp
+++ src/Parsers/ParserShowIndexesQuery.cpp
@@ -1,8 +1,9 @@
-#include <Parsers/ParserShowColumnsQuery.h>
+#include <Parsers/ParserShowIndexesQuery.h>
 
+// TODO
 #include <Parsers/ASTIdentifier_fwd.h>
 #include <Parsers/ASTLiteral.h>
-#include <Parsers/ASTShowColumnsQuery.h>
+#include <Parsers/ASTShowIndexesQuery.h>
 #include <Parsers/CommonParsers.h>
 #include <Parsers/ExpressionElementParsers.h>
 #include <Parsers/ExpressionListParsers.h>
@@ -10,13 +11,12 @@
 namespace DB
 {
 
-bool ParserShowColumnsQuery::parseImpl(Pos & pos, ASTPtr & node, Expected & expected)
+bool ParserShowIndexesQuery::parseImpl(Pos & pos, ASTPtr & node, Expected & expected)
 {
-    ASTPtr like;
     ASTPtr from_database;
     ASTPtr from_table;
 
-    auto query = std::make_shared<ASTShowColumnsQuery>();
+    auto query = std::make_shared<ASTShowIndexesQuery>();
 
     if (!ParserKeyword("SHOW").ignore(pos, expected))
         return false;
@@ -24,10 +24,7 @@ bool ParserShowColumnsQuery::parseImpl(Pos & pos, ASTPtr & node, Expected & expe
     if (ParserKeyword("EXTENDED").ignore(pos, expected))
         query->extended = true;
 
-    if (ParserKeyword("FULL").ignore(pos, expected))
-        query->full = true;
-
-    if (!(ParserKeyword("COLUMNS").ignore(pos, expected) || ParserKeyword("FIELDS").ignore(pos, expected)))
+    if (!(ParserKeyword("INDEX").ignore(pos, expected) || ParserKeyword("INDEXES").ignore(pos, expected) || ParserKeyword("KEYS").ignore(pos, expected)))
         return false;
 
     if (ParserKeyword("FROM").ignore(pos, expected) || ParserKeyword("IN").ignore(pos, expected))
@@ -48,29 +45,9 @@ bool ParserShowColumnsQuery::parseImpl(Pos & pos, ASTPtr & node, Expected & expe
 
     tryGetIdentifierNameInto(from_database, query->from_database);
 
-    if (ParserKeyword("NOT").ignore(pos, expected))
-        query->not_like = true;
-
-    if (bool insensitive = ParserKeyword("ILIKE").ignore(pos, expected); insensitive || ParserKeyword("LIKE").ignore(pos, expected))
-    {
-        if (insensitive)
-            query->case_insensitive_like = true;
-
-        if (!ParserStringLiteral().parse(pos, like, expected))
-            return false;
-    }
-    else if (query->not_like)
-        return false;
-    else if (ParserKeyword("WHERE").ignore(pos, expected))
+    if (ParserKeyword("WHERE").ignore(pos, expected))
         if (!ParserExpressionWithOptionalAlias(false).parse(pos, query->where_expression, expected))
             return false;
-
-    if (ParserKeyword("LIMIT").ignore(pos, expected))
-        if (!ParserExpressionWithOptionalAlias(false).parse(pos, query->limit_length, expected))
-            return false;
-
-    if (like)
-        query->like = like->as<ASTLiteral &>().value.safeGet<const String &>();
 
     node = query;
 
@@ -78,3 +55,4 @@ bool ParserShowColumnsQuery::parseImpl(Pos & pos, ASTPtr & node, Expected & expe
 }
 
 }
+
