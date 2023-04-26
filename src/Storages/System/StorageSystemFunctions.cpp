3e1e86cff29b3df7baa1c10455dc65f4d48d5bd0
DanRoscigno <dan@roscigno.com>
2023-04-26 14:26:49 -0400

    examples added

--- src/Storages/System/StorageSystemFunctions.cpp
+++ src/Storages/System/StorageSystemFunctions.cpp
@@ -22,6 +22,19 @@ enum class FunctionOrigin : Int8
     EXECUTABLE_USER_DEFINED = 2
 };
 
+std::string exampleMapToString( const std::map<std::string, std::string> &example )
+  // in the source, examples for functions are stored in a map. To
+  // create the documentation, we need to convert the map to a string.
+{
+        std::string stringToReturn;
+
+        for (auto iter = example.begin(); iter != example.end(); ++iter){
+           stringToReturn.append(iter->first);
+           stringToReturn.append(iter->second);
+        }
+        return stringToReturn;
+}
+
 namespace
 {
     template <typename Factory>
@@ -29,7 +42,6 @@ namespace
         MutableColumns & res_columns,
         const String & name,
         UInt64 is_aggregate,
-        const String & example,
         const String & create_query,
         FunctionOrigin function_origin,
         const Factory & factory)
@@ -41,6 +53,7 @@ namespace
         {
             res_columns[2]->insert(false);
             res_columns[3]->insertDefault();
+            res_columns[7]->insert(false);
         }
         else
         {
@@ -57,15 +70,24 @@ namespace
         if constexpr (std::is_same_v<Factory, FunctionFactory>)
         {
             if (factory.isAlias(name))
+            {
                 res_columns[6]->insertDefault();
+                res_columns[7]->insert(false);
+            }
             else
+            {
                 res_columns[6]->insert(factory.getDocumentation(name).description);
+                res_columns[7]->insert(exampleMapToString((factory.getDocumentation(name).examples)));
+            }
         }
         else
+          {
             res_columns[6]->insertDefault();
-        res_columns[7]->insert(example);
+            res_columns[7]->insert(false);
+          }
     }
 }
+
 
 std::vector<std::pair<String, Int8>> getOriginEnumsAndValues()
 {
@@ -96,14 +118,14 @@ void StorageSystemFunctions::fillData(MutableColumns & res_columns, ContextPtr c
     const auto & function_names = functions_factory.getAllRegisteredNames();
     for (const auto & function_name : function_names)
     {
-        fillRow(res_columns, function_name, UInt64(0), "example here", "", FunctionOrigin::SYSTEM, functions_factory);
+        fillRow(res_columns, function_name, UInt64(0), "", FunctionOrigin::SYSTEM, functions_factory);
     }
 
     const auto & aggregate_functions_factory = AggregateFunctionFactory::instance();
     const auto & aggregate_function_names = aggregate_functions_factory.getAllRegisteredNames();
     for (const auto & function_name : aggregate_function_names)
     {
-        fillRow(res_columns, function_name, UInt64(1), "example here", "", FunctionOrigin::SYSTEM, aggregate_functions_factory);
+        fillRow(res_columns, function_name, UInt64(1), "", FunctionOrigin::SYSTEM, aggregate_functions_factory);
     }
 
     const auto & user_defined_sql_functions_factory = UserDefinedSQLFunctionFactory::instance();
@@ -111,14 +133,14 @@ void StorageSystemFunctions::fillData(MutableColumns & res_columns, ContextPtr c
     for (const auto & function_name : user_defined_sql_functions_names)
     {
         auto create_query = queryToString(user_defined_sql_functions_factory.get(function_name));
-        fillRow(res_columns, function_name, UInt64(0), "example here", create_query, FunctionOrigin::SQL_USER_DEFINED, user_defined_sql_functions_factory);
+        fillRow(res_columns, function_name, UInt64(0), create_query, FunctionOrigin::SQL_USER_DEFINED, user_defined_sql_functions_factory);
     }
 
     const auto & user_defined_executable_functions_factory = UserDefinedExecutableFunctionFactory::instance();
     const auto & user_defined_executable_functions_names = user_defined_executable_functions_factory.getRegisteredNames(context);
     for (const auto & function_name : user_defined_executable_functions_names)
     {
-        fillRow(res_columns, function_name, UInt64(0), "example here", "", FunctionOrigin::EXECUTABLE_USER_DEFINED, user_defined_executable_functions_factory);
+        fillRow(res_columns, function_name, UInt64(0), "", FunctionOrigin::EXECUTABLE_USER_DEFINED, user_defined_executable_functions_factory);
     }
 }
 
