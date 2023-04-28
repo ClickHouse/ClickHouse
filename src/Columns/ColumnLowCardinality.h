b8717bc671bc4fd89f0f144ec24176518c830fc0
Alexey Milovidov <milovidov@clickhouse.com>
2023-04-29 00:14:10 +0200

    Fix progress bar for LowCardinality fields with shared dictionaries

--- src/Columns/ColumnLowCardinality.h
+++ src/Columns/ColumnLowCardinality.h
@@ -160,7 +160,9 @@ public:
 
     void reserve(size_t n) override { idx.reserve(n); }
 
-    size_t byteSize() const override { return idx.getPositions()->byteSize() + getDictionary().byteSize(); }
+    /// Don't count the dictionary size as it can be shared between different blocks.
+    size_t byteSize() const override { return idx.getPositions()->byteSize(); }
+
     size_t byteSizeAt(size_t n) const override { return getDictionary().byteSizeAt(getIndexes().getUInt(n)); }
     size_t allocatedBytes() const override { return idx.getPositions()->allocatedBytes() + getDictionary().allocatedBytes(); }
 
