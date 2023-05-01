21fdfe47c33b2def80f672b9ad53f17241e9bf79
Dmitry Novik <n0vik@clickhouse.com>
2023-05-01 18:02:08 +0200

    Use non-default user in test

--- tests/integration/test_async_insert_memory/test.py
+++ tests/integration/test_async_insert_memory/test.py
@@ -6,7 +6,11 @@ from helpers.cluster import ClickHouseCluster
 
 cluster = ClickHouseCluster(__file__)
 
-node = cluster.add_instance("node")
+node = cluster.add_instance(
+    "node",
+        user_configs=[
+        "configs/users.xml",
+    ],)
 
 
 @pytest.fixture(scope="module", autouse=True)
@@ -19,16 +23,19 @@ def start_cluster():
 
 
 def test_memory_usage():
+    node.query("CREATE USER IF NOT EXISTS A")
+    node.query("GRANT ALL ON *.* TO A")
+
     node.query(
         "CREATE TABLE async_table(data Array(UInt64)) ENGINE=MergeTree() ORDER BY data"
     )
 
-    node.get_query_request("SELECT count() FROM system.numbers")
+    node.get_query_request("SELECT count() FROM system.numbers", user="A")
 
     INSERT_QUERY = "INSERT INTO async_table SETTINGS async_insert=1, wait_for_async_insert=1 VALUES ({})"
     for iter in range(10):
         values = list(range(iter * 5000000, (iter + 1) * 5000000))
-        node.query(INSERT_QUERY.format(values))
+        node.query(INSERT_QUERY.format(values), user="A")
 
     # Wait until buffers are freed
     time.sleep(5)
@@ -36,10 +43,11 @@ def test_memory_usage():
     response = node.get_query_request(
         "SELECT groupArray(number) FROM numbers(1000000) SETTINGS max_memory_usage_for_user={}".format(
             30 * (2**23)
-        )
+        ), user="A"
     )
 
     _, err = response.get_answer_and_error()
     assert err == "", "Query failed with error {}".format(err)
 
     node.query("DROP TABLE async_table")
+    node.query("DROP USER IF EXISTS A")
