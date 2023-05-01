1de88f2f99647580512c5e9d0d5577634ba594d0
Daniil Rubin <danyarubin@gmail.com>
2023-05-01 23:39:12 +0700

    Use variant, better style

--- src/IO/AbstractHashingWriteBuffer.h
+++ src/IO/AbstractHashingWriteBuffer.h
@@ -6,6 +6,7 @@
 #include <Core/Types.h>
 
 #include <optional>
+#include <variant>
 
 namespace DB
 {
@@ -17,110 +18,112 @@ public:
     AbstractHashingWriteBuffer(WriteBuffer & out_, bool cryptographic_mode_)
         : cryptographic_mode(cryptographic_mode_)
     {
-        if (cryptographic_mode) {
-            cryptoBuf.emplace(out_);
+        if (!cryptographic_mode) {
+            underlying_buf.emplace<HashingWriteBuffer>(out_);
         } else {
-            hashingBuf.emplace(out_);
+            underlying_buf.emplace<CryptoHashingWriteBuffer>(out_);
         }
     }
+private:
+    template<class... Ts> struct overloaded : Ts... { using Ts::operator()...; };
+    template<class... Ts> overloaded(Ts...) -> overloaded<Ts...>;
+
+    struct RefFunc {
+        RefFunc(): _unused(nullptr, 0) {}
+        WriteBuffer& operator()(HashingWriteBuffer& buf) {
+            return buf;
+        }
+        WriteBuffer& operator()(CryptoHashingWriteBuffer& buf) {
+            return buf;
+        }
+        WriteBuffer& operator()(std::monostate) {
+            std::unreachable();
+            return _unused;
+        }
+        WriteBuffer _unused;
+    };
+
+public:
 
     void sync()
     {
-        if (cryptographic_mode)
-        {
-            cryptoBuf->sync();
-        }
-        else
-        {
-            hashingBuf->sync();
-        }
+        auto functor = overloaded {
+            [](std::monostate) {},
+            [](auto& buf) {buf.sync();}
+        };
+        std::visit(functor, underlying_buf);
     }
 
     uint128 getHash()
     {
-        if (cryptographic_mode)
-        {
-            return cryptoBuf->getHash();
-        }
-        else
-        {
-            return hashingBuf->getHash();
-        }
+        auto functor = overloaded {
+            [](std::monostate) {return uint128{};},
+            [](auto& buf) {return buf.getHash();}
+        };
+        return std::visit(functor, underlying_buf);
     }
 
     void append(DB::BufferBase::Position data)
     {
-        if (cryptographic_mode)
-        {
-            cryptoBuf->append(data);
-        }
-        else
-        {
-            hashingBuf->append(data);
-        }
+        auto functor = overloaded {
+            [](std::monostate) {},
+            [data](auto& buf) {buf.append(data);}
+        };
+        std::visit(functor, underlying_buf);
     }
 
     void calculateHash(DB::BufferBase::Position data, size_t len)
     {
-        if (cryptographic_mode)
-        {
-            cryptoBuf->calculateHash(data, len);
-        }
-        else
-        {
-            hashingBuf->calculateHash(data, len);
-        }
+        auto functor = overloaded {
+            [](std::monostate) {},
+            [data, len](auto& buf) {buf.calculateHash(data,len);}
+        };
+        std::visit(functor, underlying_buf);
     }
 
-    size_t count() {
-        if (cryptographic_mode)
-        {
-            return cryptoBuf->count();
-        }
-        else
-        {
-            return hashingBuf->count();
-        }
+    size_t count()
+    {
+        auto functor = overloaded {
+            [](std::monostate) -> size_t {return 0;},
+            [](auto& buf) -> size_t {return buf.count();}
+        };
+        return std::visit(functor, underlying_buf);
     }
 
-    WriteBuffer& getBuf() {
-        if (cryptographic_mode)
-        {
-            return *cryptoBuf;
-        }
-        else
-        {
-            return *hashingBuf;
-        }
+    WriteBuffer & getBuf()
+    {
+        return std::visit(RefFunc{}, underlying_buf);
     }
 
-    inline void next() {
-        if (cryptographic_mode) {
-            cryptoBuf->next();
-        } else {
-            hashingBuf->next();
-        }
+    inline void next()
+    {
+        auto functor = overloaded {
+            [](std::monostate) {},
+            [](auto& buf) {buf.next();}
+        };
+        std::visit(functor, underlying_buf);
     }
 
-    inline void nextIfAtEnd() {
-        if (cryptographic_mode) {
-            cryptoBuf->nextIfAtEnd();
-        } else {
-            hashingBuf->nextIfAtEnd();
-        }
+    inline void nextIfAtEnd()
+    {
+        auto functor = overloaded {
+            [](std::monostate) {},
+            [](auto& buf) {buf.nextIfAtEnd();}
+        };
+        std::visit(functor, underlying_buf);
     }
 
-    size_t offset() const {
-        if (cryptographic_mode) {
-            return cryptoBuf->offset();
-        } else {
-            return hashingBuf->offset();
-        }
+    size_t offset() const
+    {
+        auto functor = overloaded {
+            [](std::monostate) -> size_t {return 0;},
+            [](auto& buf) -> size_t {return buf.offset();}
+        };
+        return std::visit(functor, underlying_buf);
     }
 
 private:
     bool cryptographic_mode;
-    std::optional<HashingWriteBuffer> hashingBuf;
-    std::optional<CryptoHashingWriteBuffer> cryptoBuf;
+    std::variant<std::monostate, HashingWriteBuffer, CryptoHashingWriteBuffer> underlying_buf;
 };
 }
