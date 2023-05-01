6566ed78635ed76db0925d3023bfe89b292021c7
Timur Solodovnikov <timur.solodovnikov@clickhouse.com>
2023-05-01 13:51:46 -0700

    removed loop & added tests

---
+++ tests/queries/0_stateless/02725_alias_columns_should_not_allow_compression_codec.sql
@@ -0,0 +1,9 @@
+
+create table if not exists alias_column_should_not_allow_compression ( user_id UUID, user_id_hashed ALIAS (cityHash64(user_id)))
+engine MergeTree
+partition by tuple()
+order by tuple();
+alter table alias_column_should_not_allow_compression modify column user_id codec(LZ4HC(1));
+alter table alias_column_should_not_allow_compression modify column user_id_hashed codec(LZ4HC(1)); -- { serverError BAD_ARGUMENTS }
+drop table alias_column_should_not_allow_compression;
+
