e58a63b4513d569c8b6923a3daa140190ff8aaa5
Timur Solodovnikov <timur.solodovnikov@clickhouse.com>
2023-05-01 16:13:45 -0700

    addressing PR comments

--- tests/queries/0_stateless/02725_alias_columns_should_not_allow_compression_codec.sql
+++ tests/queries/0_stateless/02725_alias_columns_should_not_allow_compression_codec.sql
@@ -4,4 +4,4 @@ create table if not exists alias_column_should_not_allow_compression_fail ( user
 alter table alias_column_should_not_allow_compression modify column user_id codec(LZ4HC(1));
 alter table alias_column_should_not_allow_compression modify column user_id_hashed codec(LZ4HC(1)); -- { serverError BAD_ARGUMENTS }
 alter table alias_column_should_not_allow_compression add column user_id_hashed_1 UInt64 ALIAS (cityHash64(user_id)) codec(LZ4HC(1)); -- { serverError BAD_ARGUMENTS }
-
+drop table if exists alias_column_should_not_allow_compression;
