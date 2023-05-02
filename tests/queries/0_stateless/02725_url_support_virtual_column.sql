10f9039e00320fc7b38ac6524a5940872859707e
Ziy1-Tan <ajb459684460@gmail.com>
2023-05-02 15:35:58 +0800

    Fix tests

--- tests/queries/0_stateless/02725_url_support_virtual_column.sql
+++ tests/queries/0_stateless/02725_url_support_virtual_column.sql
@@ -6,4 +6,3 @@ select _file, count() from url('http://127.0.0.1:8123/?query=select+1&user=defau
 select _path, _file, s from url('http://127.0.0.1:8123/?query=select+1&user=default', LineAsString, 's String');
 select _path, _file, s from url('http://127.0.0.1:8123/?query=select+1&user=default&password=wrong', LineAsString, 's String'); -- { serverError RECEIVED_ERROR_FROM_REMOTE_IO_SERVER }
 
-SELECT _path, _file, count() FROM url('https://dumps.wikimedia.org/other/pagecounts-raw/2007/2007-12/pagecounts-20071209-180000.gz', LineAsString) group by _path, _file;
