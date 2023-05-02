729eedae055aaaf0009a1c0145865ac8eb52bebd
Antonio Andelic <antonio@clickhouse.com>
2023-05-02 10:47:38 +0000

    Properly fix GCS with keys

--- src/IO/S3/ProviderType.h
+++ src/IO/S3/ProviderType.h
@@ -21,8 +21,6 @@ std::string_view toString(ProviderType provider_type);
 
 bool supportsMultiPartCopy(ProviderType provider_type);
 
-ProviderType getProviderTypeFromURL(const std::string & url);
-
 }
 
 #endif
