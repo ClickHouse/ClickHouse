f28591801f56adc791880fc8fe587e7546989d38
Alexey Milovidov <milovidov@clickhouse.com>
2023-05-02 20:29:30 +0300

    Update 02725_agg_projection_resprect_PK.sql

--- tests/queries/0_stateless/02725_agg_projection_resprect_PK.sql
+++ tests/queries/0_stateless/02725_agg_projection_resprect_PK.sql
@@ -29,3 +29,4 @@ FROM numbers_mt(1e5);
 select * from (EXPLAIN indexes = 1 SELECT c1, sum(c3) FROM t0 GROUP BY c1) where explain like '%ReadFromMergeTree%';
 select * from (EXPLAIN indexes = 1 SELECT c1, sum(c3) FROM t0 WHERE c1 = 100 GROUP BY c1) where explain like '%Granules%';
 
+DROP TABLE t0;
