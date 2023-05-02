2009efd4bc3c34168bcb7da44c6d00b99de485a4
Ziy1-Tan <ajb459684460@gmail.com>
2023-05-02 10:38:37 +0800

    Fix style
    Signed-off-by: Ziy1-Tan <ajb459684460@gmail.com>

--- src/Common/parseRemoteDescription.cpp
+++ src/Common/parseRemoteDescription.cpp
@@ -64,7 +64,8 @@ static bool parseNumber(const String & description, size_t l, size_t r, size_t &
  * abc{1..9}de{f,g,h}   - is a direct product, 27 shards.
  * abc{1..9}de{0|1}     - is a direct product, 9 shards, in each 2 replicas.
  */
-std::vector<String> parseRemoteDescription(const String & description, size_t l, size_t r, char separator, size_t max_addresses, const String& func_name)
+std::vector<String>
+parseRemoteDescription(const String & description, size_t l, size_t r, char separator, size_t max_addresses, const String & func_name)
 {
     std::vector<String> res;
     std::vector<String> cur;
