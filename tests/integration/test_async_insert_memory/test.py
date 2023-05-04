bc743f9efa261779dc159890bf78f8b679dc5c17
Dmitry Novik <n0vik@clickhouse.com>
2023-05-04 15:01:23 +0000

    Add test for fallback to synchronous inserts

--- tests/integration/test_async_insert_memory/test.py
+++ tests/integration/test_async_insert_memory/test.py
@@ -53,3 +53,44 @@ def test_memory_usage():
 
     node.query("DROP TABLE async_table")
     node.query("DROP USER IF EXISTS A")
+
+def test_synchronous_memory_usage():
+    node.query("CREATE USER IF NOT EXISTS A")
+    node.query("GRANT ALL ON *.* TO A")
+
+    node.query(
+        "CREATE TABLE async_table(data Array(UInt64)) ENGINE=MergeTree() ORDER BY data"
+    )
+
+    node.get_query_request("SELECT count() FROM system.numbers", user="A")
+
+    response = node.get_query_request(
+        "SELECT groupArray(number) FROM numbers(1000000) SETTINGS max_memory_usage_for_user={}".format(
+            30 * (2**23)
+        ),
+        user="A",
+    )
+
+    _, err = response.get_answer_and_error()
+    assert err == "", "Query failed with error {}".format(err)
+
+    INSERT_QUERY = "INSERT INTO async_table SETTINGS async_insert=1, wait_for_async_insert=1 VALUES ({})"
+    for iter in range(1):
+        values = list(range(0, 20000000))
+        node.query(INSERT_QUERY.format(values), user="A")
+
+    # Wait until buffers are freed
+    time.sleep(5)
+
+    response = node.get_query_request(
+        "SELECT groupArray(number) FROM numbers(1000000) SETTINGS max_memory_usage_for_user={}".format(
+            30 * (2**23)
+        ),
+        user="A",
+    )
+
+    _, err = response.get_answer_and_error()
+    assert err == "", "Query failed with error {}".format(err)
+
+    node.query("DROP TABLE async_table")
+    node.query("DROP USER IF EXISTS A")
