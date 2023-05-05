9032e23f87d2f8ed88ad62c79017c2c951d74464
kssenii <sumarokovakseniia@mail.ru>
2023-05-05 15:43:23 +0200

    Clean up storage_conf.xml, use dynamic disks for tests

--- tests/queries/0_stateless/02344_describe_cache.sql
+++ tests/queries/0_stateless/02344_describe_cache.sql
@@ -1,7 +1,4 @@
 -- Tags: no-fasttest, no-parallel
 
 SYSTEM DROP FILESYSTEM CACHE 's3_cache';
-SYSTEM DROP FILESYSTEM CACHE 's3_cache_2';
-
 DESCRIBE FILESYSTEM CACHE 's3_cache';
-DESCRIBE FILESYSTEM CACHE 's3_cache_2';
