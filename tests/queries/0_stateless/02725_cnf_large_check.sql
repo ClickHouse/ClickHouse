28403dbd034a82b601c2081d53d4e969948b43ce
Antonio Andelic <antonio@clickhouse.com>
2023-05-05 09:25:06 +0000

    Remove explain query

--- tests/queries/0_stateless/02725_cnf_large_check.sql
+++ tests/queries/0_stateless/02725_cnf_large_check.sql
@@ -9,15 +9,6 @@ FROM 02725_cnf
 WHERE (c5 AND (NOT c0)) OR ((NOT c3) AND (NOT c6) AND (NOT c1) AND (NOT c6)) OR (c7 AND (NOT c3) AND (NOT c5) AND (NOT c7)) OR ((NOT c8) AND c5) OR ((NOT c0)) OR ((NOT c8) AND (NOT c5) AND c1 AND c6 AND c3) OR (c7 AND (NOT c0) AND c6 AND c1 AND (NOT c2)) OR (c3 AND (NOT c9) AND c1)
 SETTINGS convert_query_to_cnf = 1, allow_experimental_analyzer = 1;
 
--- make sure we use CNF form
-SELECT count()
-FROM (
-    EXPLAIN QUERY TREE
-    SELECT count()
-    FROM 02725_cnf
-    WHERE (c5 AND (NOT c0)) OR ((NOT c3) AND (NOT c6) AND (NOT c1) AND (NOT c6)) OR (c7 AND (NOT c3) AND (NOT c5) AND (NOT c7)) OR ((NOT c8) AND c5) OR ((NOT c0)) OR ((NOT c8) AND (NOT c5) AND c1 AND c6 AND c3) OR (c7 AND (NOT c0) AND c6 AND c1 AND (NOT c2)) OR (c3 AND (NOT c9) AND c1)
-    SETTINGS convert_query_to_cnf = 1, allow_experimental_analyzer = 1);
-
 SELECT count()
 FROM 02725_cnf
 WHERE (c5 AND (NOT c0)) OR ((NOT c3) AND (NOT c6) AND (NOT c1) AND (NOT c6)) OR (c7 AND (NOT c3) AND (NOT c5) AND (NOT c7)) OR ((NOT c8) AND c5) OR ((NOT c0)) OR ((NOT c8) AND (NOT c5) AND c1 AND c6 AND c3) OR (c7 AND (NOT c0) AND c6 AND c1 AND (NOT c2)) OR (c3 AND (NOT c9) AND c1)
@@ -27,15 +18,6 @@ SELECT count()
 FROM 02725_cnf
 WHERE ((NOT c2) AND c2 AND (NOT c1)) OR ((NOT c2) AND c3 AND (NOT c5)) OR ((NOT c7) AND (NOT c8)) OR (c9 AND c6 AND c8 AND (NOT c8) AND (NOT c7))
 SETTINGS convert_query_to_cnf = 1, allow_experimental_analyzer = 1;
-
--- make sure we use CNF form
-SELECT count()
-FROM (
-    EXPLAIN QUERY TREE
-    SELECT count()
-    FROM 02725_cnf
-    WHERE ((NOT c2) AND c2 AND (NOT c1)) OR ((NOT c2) AND c3 AND (NOT c5)) OR ((NOT c7) AND (NOT c8)) OR (c9 AND c6 AND c8 AND (NOT c8) AND (NOT c7))
-    SETTINGS convert_query_to_cnf = 1, allow_experimental_analyzer = 1);
 
 SELECT count()
 FROM 02725_cnf
