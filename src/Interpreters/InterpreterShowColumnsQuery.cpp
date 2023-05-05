908e9153af42ec30684271e37e264a44d228b242
tpanetti <Thomas.Panetti@clickhouse.com>
2023-05-05 12:19:35 -0700

    Change SHOW COLUMNS query to display MySQL types in MySQL Compatibility mode
    This updates the SHOW COLUMN SQL query to display MySQL types when this
    query is issued by a client connected via MySQL Compatibility port

--- src/Interpreters/InterpreterShowColumnsQuery.cpp
+++ src/Interpreters/InterpreterShowColumnsQuery.cpp
@@ -26,11 +26,17 @@ String InterpreterShowColumnsQuery::getRewrittenQuery()
 
     WriteBufferFromOwnString rewritten_query;
 
-    rewritten_query << "SELECT name AS field, type AS type, startsWith(type, 'Nullable') AS null, trim(concatWithSeparator(' ', if(is_in_primary_key, 'PRI', ''), if (is_in_sorting_key, 'SOR', ''))) AS key, if(default_kind IN ('ALIAS', 'DEFAULT', 'MATERIALIZED'), default_expression, NULL) AS default, '' AS extra ";
-
     // TODO Interpret query.extended. It is supposed to show internal/virtual columns. Need to fetch virtual column names, see
     // IStorage::getVirtuals(). We can't easily do that via SQL.
 
+    // If connected via MySQL Compatibility mode, convert ClickHouse types to MySQL
+    if (getContext()->getClientInfo().interface == DB::ClientInfo::Interface::MYSQL)
+    {
+        rewritten_query << getMySQLQuery();
+    }
+    else {
+        rewritten_query << "SELECT name AS field, type AS type, startsWith(type, 'Nullable') AS null, trim(concatWithSeparator(' ', if(is_in_primary_key, 'PRI', ''), if (is_in_sorting_key, 'SOR', ''))) AS key, if(default_kind IN ('ALIAS', 'DEFAULT', 'MATERIALIZED'), default_expression, NULL) AS default, '' AS extra ";
+    }
     if (query.full)
     {
         /// "Full" mode is mostly for MySQL compat
@@ -93,6 +99,74 @@ String InterpreterShowColumnsQuery::getRewrittenQuery()
 
 }
 
+String InterpreterShowColumnsQuery::getMySQLQuery()
+{
+    WriteBufferFromOwnString mysql_specific_query;
+
+    mysql_specific_query << "SELECT name AS field, "
+        << "CASE "
+        << "  WHEN startsWith(type, 'Nullable') THEN "
+        << "    CASE "
+        << "      WHEN substring(type, 10, length(type) - 10) IN ('UInt8', 'Int8') THEN 'tinyint' "
+        << "      WHEN substring(type, 10, length(type) - 10) IN ('UInt16', 'Int16') THEN 'smallint' "
+        << "      WHEN substring(type, 10, length(type) - 10) IN ('UInt32', 'Int32') THEN 'int' "
+        << "      WHEN substring(type, 10, length(type) - 10) IN ('UInt64', 'Int64', 'UInt128', 'Int128', 'UInt256', 'Int256') THEN 'bigint' "
+        << "      WHEN substring(type, 10, length(type) - 10) = 'Float32' THEN 'float' "
+        << "      WHEN substring(type, 10, length(type) - 10) = 'Float64' THEN 'double' "
+        << "      WHEN substring(type, 10, length(type) - 10) LIKE 'Decimal%' THEN 'decimal' "
+        << "      WHEN substring(type, 10, length(type) - 10) = 'Boolean' THEN 'tinyint' "
+        << "      WHEN substring(type, 10, length(type) - 10) = 'String' THEN 'text' "
+        << "      WHEN substring(type, 10, length(type) - 10) LIKE 'FixedString%' THEN 'text' "
+        << "      WHEN substring(type, 10, length(type) - 10) LIKE 'Date%' THEN 'date' "
+        << "      WHEN substring(type, 10, length(type) - 10) LIKE 'DateTime%' THEN 'datetime' "
+        << "      WHEN substring(type, 10, length(type) - 10) = 'JSON' THEN 'json' "
+        << "      WHEN substring(type, 10, length(type) - 10) = 'UUID' THEN 'binary' "
+        << "      WHEN substring(type, 10, length(type) - 10) LIKE 'Enum%' THEN 'enum' "
+        << "      WHEN substring(type, 10, length(type) - 10) LIKE 'LowCardinality%' THEN 'text' "
+        << "      WHEN substring(type, 10, length(type) - 10) LIKE 'Array%' THEN 'json' "
+        << "      WHEN substring(type, 10, length(type) - 10) LIKE 'Map%' THEN 'json' "
+        << "      WHEN substring(type, 10, length(type) - 10) IN ('SimpleAggregateFunction', 'AggregateFunction') THEN 'text' "
+        << "      WHEN substring(type, 10, length(type) - 10) = 'Nested' THEN 'json' "
+        << "      WHEN substring(type, 10, length(type) - 10) LIKE 'Tuple%' THEN 'json' "
+        << "      WHEN substring(type, 10, length(type) - 10) LIKE 'IPv%' THEN 'text' "
+        << "      WHEN substring(type, 10, length(type) - 10) IN ('Expression', 'Set', 'Nothing', 'Interval') THEN 'text' "
+        << "      ELSE substring(type, 10, length(type) - 10) "
+        << "    END "
+        << "  ELSE "
+        << "    CASE "
+        << "      WHEN type IN ('UInt8', 'Int8') THEN 'tinyint' "
+        << "      WHEN type IN ('UInt16', 'Int16') THEN 'smallint' "
+        << "      WHEN type IN ('UInt32', 'Int32') THEN 'int' "
+        << "      WHEN type IN ('UInt64', 'Int64', 'UInt128', 'Int128', 'UInt256', 'Int256') THEN 'bigint' "
+        << "      WHEN type = 'Float32' THEN 'float' "
+        << "      WHEN type = 'Float64' THEN 'double' "
+        << "      WHEN type LIKE 'Decimal%' THEN 'decimal' "
+        << "      WHEN type = 'Boolean' THEN 'tinyint' "
+        << "      WHEN type = 'String' THEN 'text' "
+        << "      WHEN type LIKE 'FixedString%' THEN 'text' "
+        << "      WHEN type LIKE 'Date%' THEN 'date' "
+        << "      WHEN type LIKE 'DateTime%' THEN 'datetime' "
+        << "      WHEN type = 'JSON' THEN 'json' "
+        << "      WHEN type = 'UUID' THEN 'binary' "
+        << "      WHEN type LIKE 'Enum%' THEN 'enum' "
+        << "      WHEN type LIKE 'LowCardinality%' THEN 'text' "
+        << "      WHEN type LIKE 'Array%' THEN 'json' "
+        << "      WHEN type LIKE 'Map%' THEN 'json' "
+        << "      WHEN type IN ('SimpleAggregateFunction', 'AggregateFunction') THEN 'text' "
+        << "      WHEN type = 'Nested' THEN 'json' "
+        << "      WHEN type LIKE 'Tuple%' THEN 'json' "
+        << "      WHEN type LIKE 'IPv%' THEN 'text' "
+        << "      WHEN type IN ('Expression', 'Set', 'Nothing', 'Interval') THEN 'text' "
+        << "      ELSE type "
+        << "    END "
+        << "END AS type, "
+        << "startsWith(type, 'Nullable') AS null, "
+        << "trim(concatWithSeparator(' ', if(is_in_primary_key, 'PRI', ''), if (is_in_sorting_key, 'SOR', ''))) AS key, "
+        << "if(default_kind IN ('ALIAS', 'DEFAULT', 'MATERIALIZED'), default_expression, NULL) AS default, "
+        << "'' AS extra ";
+
+    return mysql_specific_query.str();
+}
 
 BlockIO InterpreterShowColumnsQuery::execute()
 {
