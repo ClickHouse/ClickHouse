858a7ed69df954a301353b39370846699622ff43
Alexey Milovidov <milovidov@clickhouse.com>
2023-05-07 21:20:12 +0300

    Update tests/queries/0_stateless/02733_distinct.sql
    Co-authored-by: Igor Nikonov <954088+devcrafter@users.noreply.github.com>

--- tests/queries/0_stateless/02733_distinct.sql
+++ tests/queries/0_stateless/02733_distinct.sql
@@ -11,6 +11,6 @@ ORDER BY (c1, c3);
 INSERT INTO test(c1, c2, c3) VALUES ('', '', '1'), ('', '', '2'),('v1', 'v2', '3'),('v1', 'v2', '4'),('v1', 'v2', '5');
 
 SELECT c1, c2, c3 FROM test GROUP BY c1, c2, c3 ORDER BY c1, c2, c3;
-SELECT DISTINCT c1, c2, c3 FROM test ORDER BY c1, c2, c3;
+SELECT DISTINCT c1, c2, c3 FROM test;
 
 DROP TABLE test;
