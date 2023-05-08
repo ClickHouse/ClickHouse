400fa724337ab278476f956323462b1d5b11e27b
Alexander Tokmakov <tavplubix@clickhouse.com>
2023-05-08 14:49:11 +0200

    fix

--- src/Databases/removeWhereConditionPlaceholder.cpp
+++ src/Databases/removeWhereConditionPlaceholder.cpp
@@ -3,12 +3,8 @@
 namespace DB
 {
 
-std::string removeWhereConditionPlaceholder(const std::string  & query)
+std::string removeWhereConditionPlaceholder(const std::string & query)
 {
-    /** In case UPDATE_FIELD is specified in {condition} for dictionary that must load all data.
-      * Replace {condition} with true_condition for initial dictionary load.
-      * For next dictionary loads {condition} will be updated with UPDATE_FIELD.
-      */
     static constexpr auto true_condition = "(1 = 1)";
     auto condition_position = query.find(CONDITION_PLACEHOLDER_TO_REPLACE_VALUE);
     if (condition_position != std::string::npos)
