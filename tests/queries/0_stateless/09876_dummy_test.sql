7fa56c0ebb42a38755c6a06476471ebcbc8d9010
dmitry.terenich@gmail.com <dmitry.terenich@gmail.com>
2023-05-08 12:47:35 +0300

    Improve the test

--- tests/queries/0_stateless/09876_dummy_test.sql
+++ tests/queries/0_stateless/09876_dummy_test.sql
@@ -1,13 +1,18 @@
-DROP TABLE IF EXISTS test09876_1;
-CREATE TABLE test09876_1 (x String) ENGINE=Memory;
-INSERT INTO test09876_1 VALUES ('a'), ('b'), ('c');
-SELECT trainEntropyLearnedHash(x, 'id1') FROM test09876_1;
-SELECT entropyLearnedHash(x, 'id1') FROM test09876_1;
-SELECT cityHash64(x) FROM test09876_1;
+DROP TABLE IF EXISTS test09876_ELH;
+DROP TABLE IF EXISTS test09876_CH;
+CREATE TABLE test09876_ELH (simple String, one_excess String, two_important String) ENGINE=Memory;
+CREATE TABLE test09876_CH (simple String, one_excess String, two_important String) ENGINE=Memory;
+INSERT INTO test09876_ELH VALUES ('a', 'aw', 'aaw'), ('b', 'bw', 'abw'), ('c', 'cw', 'baw');
+INSERT INTO test09876_CH VALUES ('a', 'a', 'aa'), ('b', 'b', 'ab'), ('c', 'c', 'ba');
 
-DROP TABLE IF EXISTS test09876_2;
-CREATE TABLE test09876_2 (x String) ENGINE=Memory;
-INSERT INTO test09876_2 VALUES ('aa'), ('ba'), ('ca');
-SELECT trainEntropyLearnedHash(x, 'id1') FROM test09876_2;
-SELECT entropyLearnedHash(x, 'id1') FROM test09876_2;
-SELECT cityHash64(x) FROM test09876_1;
+SELECT trainEntropyLearnedHash(simple, 'id1') FROM test09876_ELH;
+SELECT entropyLearnedHash(simple, 'id1') FROM test09876_ELH;
+SELECT cityHash64(simple) FROM test09876_CH;
+
+SELECT trainEntropyLearnedHash(one_excess, 'id2') FROM test09876_ELH;
+SELECT entropyLearnedHash(one_excess, 'id2') FROM test09876_ELH;
+SELECT cityHash64(one_excess) FROM test09876_CH;
+
+SELECT trainEntropyLearnedHash(two_important, 'id3') FROM test09876_ELH;
+SELECT entropyLearnedHash(two_important, 'id3') FROM test09876_ELH;
+SELECT cityHash64(two_important) FROM test09876_CH;
