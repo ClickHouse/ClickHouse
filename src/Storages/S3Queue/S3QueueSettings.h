446cf3c847eb77608fd01a1020fb16bda8f4ccf5
Sergey Katkovskiy <sergey.katkovskiy@mail.ru>
2023-05-10 18:17:26 +0300

    handle failed files

--- src/Storages/S3Queue/S3QueueSettings.h
+++ src/Storages/S3Queue/S3QueueSettings.h
@@ -14,16 +14,21 @@ class ASTStorage;
     M(S3QueueMode, \
       mode, \
       S3QueueMode::ORDERED, \
-      "With unordered mode, the set of all already processed files is tracked with persistent nodes in ZooKepeer", \
+      "With unordered mode, the set of all already processed files is tracked with persistent nodes in ZooKepeer." \
+      "With ordered mode, only the max name of the successfully consumed file stored.", \
       0) \
-    M(S3QueueAction, after_processing, S3QueueAction::KEEP, "Delete, keep or move file in S3 after processing", 0) \
+    M(S3QueueAction, after_processing, S3QueueAction::KEEP, "Delete or keep file in S3 after successful processing", 0) \
     M(String, keeper_path, "", "Zookeeper node path", 0) \
-    M(UInt64, s3queue_max_retries, 0, "Retry loading up to specified number of times", 0) \
+    M(UInt64, s3queue_loading_retries, 0, "Retry loading up to specified number of times", 0) \
     M(UInt64, s3queue_polling_min_timeout_ms, 1000, "Minimal timeout before next polling", 0) \
     M(UInt64, s3queue_polling_max_timeout_ms, 10000, "Maximum timeout before next polling", 0) \
-    M(UInt64, s3queue_polling_backoff_ms, 0, "Retry loading up to specified number of times", 0) \
+    M(UInt64, s3queue_polling_backoff_ms, 0, "Polling backoff", 0) \
     M(UInt64, s3queue_max_set_size, 1000, "Max set size for tracking processed files in unordered mode in ZooKeeper", 0) \
-    M(UInt64, s3queue_max_set_age_s, 0, "Maximum number of seconds to store processed files in ZooKeeper node (forever default)", 0) \
+    M(UInt64, \
+      s3queue_max_set_age_s, \
+      0, \
+      "Maximum number of seconds to store processed files in ZooKeeper node (store forever by default)", \
+      0) \
     M(UInt64, s3queue_polling_size, 50, "Maximum files to fetch from S3 with SELECT", 0)
 
 #define LIST_OF_S3QUEUE_SETTINGS(M, ALIAS) \
