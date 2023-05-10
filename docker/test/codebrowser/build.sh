eb122487ad2f6d6db9ba459db462f101092a72e2
Mikhail f. Shiryaev <felixoid@clickhouse.com>
2023-05-10 13:02:21 +0200

    Add logs to the report, do not chown unnecessary

--- docker/test/codebrowser/build.sh
+++ docker/test/codebrowser/build.sh
@@ -6,7 +6,8 @@ set -x -e
 STATIC_DATA=${STATIC_DATA:-/woboq_codebrowser/data}
 SOURCE_DIRECTORY=${SOURCE_DIRECTORY:-/build}
 BUILD_DIRECTORY=${BUILD_DIRECTORY:-/workdir/build}
-HTML_RESULT_DIRECTORY=${HTML_RESULT_DIRECTORY:-/workdir/output/html_report}
+OUTPUT_DIRECTORY=${OUTPUT_DIRECTORY:-/workdir/output}
+HTML_RESULT_DIRECTORY=${HTML_RESULT_DIRECTORY:-$OUTPUT_DIRECTORY/html_report}
 SHA=${SHA:-nosha}
 DATA=${DATA:-https://s3.amazonaws.com/clickhouse-test-reports/codebrowser/data}
 
@@ -15,9 +16,13 @@ read -ra CMAKE_FLAGS <<< "${CMAKE_FLAGS:-}"
 mkdir -p "$BUILD_DIRECTORY" && cd "$BUILD_DIRECTORY"
 cmake "$SOURCE_DIRECTORY" -DCMAKE_CXX_COMPILER="/usr/bin/clang++-${LLVM_VERSION}" -DCMAKE_C_COMPILER="/usr/bin/clang-${LLVM_VERSION}" -DCMAKE_EXPORT_COMPILE_COMMANDS=ON -DENABLE_EMBEDDED_COMPILER=0 "${CMAKE_FLAGS[@]}"
 mkdir -p "$HTML_RESULT_DIRECTORY"
+echo 'Filter out too noisy "Error: filename" lines and keep them in full codebrowser_generator.log'
 /woboq_codebrowser/generator/codebrowser_generator -b "$BUILD_DIRECTORY" -a \
   -o "$HTML_RESULT_DIRECTORY" --execute-concurrency=0 -p "ClickHouse:$SOURCE_DIRECTORY:$SHA" \
-  -d "$DATA" |& ts '%Y-%m-%d %H:%M:%S'
+  -d "$DATA" \
+    |& ts '%Y-%m-%d %H:%M:%S' \
+    | tee "$OUTPUT_DIRECTORY/codebrowser_generator.log" \
+    | grep --line-buffered -v ':[0-9]* Error: '
 cp -r "$STATIC_DATA" "$HTML_RESULT_DIRECTORY/"
 /woboq_codebrowser/indexgenerator/codebrowser_indexgenerator "$HTML_RESULT_DIRECTORY" \
   -d "$DATA" |& ts '%Y-%m-%d %H:%M:%S'
