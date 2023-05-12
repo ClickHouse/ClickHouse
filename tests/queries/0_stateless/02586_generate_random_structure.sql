f3c2ce921c73af6876d317486305057d54789123
avogar <avogar@clickhouse.com>
2023-05-12 19:39:33 +0000

    Make better, allow generateRandom without structure argument

--- tests/queries/0_stateless/02586_generate_random_structure.sql
+++ tests/queries/0_stateless/02586_generate_random_structure.sql
@@ -9,3 +9,9 @@ select generateRandomStructure(5, '42'); -- {serverError ILLEGAL_TYPE_OF_ARGUMEN
 select generateRandomStructure(materialize(5), 42); -- {serverError ILLEGAL_COLUMN}
 select generateRandomStructure(5, materialize(42)); -- {serverError ILLEGAL_COLUMN}
 
+desc generateRandom(11);
+select * from generateRandom(11) limit 1;
+select * from generateRandom(11, 2) limit 1;
+select * from generateRandom(11, 2, 2) limit 1;
+select * from generateRandom(11, 2, 2, 2) limit 1; -- {serverError NUMBER_OF_ARGUMENTS_DOESNT_MATCH}
+
