0334edf35b6a9122d73ade8ffb3a73b42ef24d4c
Lirikl <akd160401@gmail.com>
2023-05-12 13:04:07 +0300

    fix Documentation

--- src/Functions/array/arrayFold.cpp
+++ src/Functions/array/arrayFold.cpp
@@ -218,9 +218,24 @@ private:
 
 REGISTER_FUNCTION(ArrayFold)
 {
-    factory.registerFunction<ArrayFold>("arrayFold", R"(
+    {
+        R"(
+Calculates BLAKE3 hash string and returns the resulting set of bytes as FixedString.
+This cryptographic hash-function is integrated into ClickHouse with BLAKE3 Rust library.
+The function is rather fast and shows approximately two times faster performance compared to SHA-2, while generating hashes of the same length as SHA-256.
+It returns a BLAKE3 hash as a byte array with type FixedString(32).
+)",
+        Documentation::Examples{
+            {"hash", "SELECT hex(BLAKE3('ABC'))"}},
+        Documentation::Categories{"Hash"}
+    },
+    factory.registerFunction<ArrayFold>("arrayFold", {R"(
         Function arrayFold(x1,...,xn,accum -> expression, array1,...,arrayn, init_accum) applies lambda function to a number of same sized array columns
         and collects result in accumulator. Accumulator can be either constant or column.
-        )");
+        )",
+        Documentation::Examples{
+            {"sum", "SELECT arrayFold(x,acc -> acc + x, [1,2,3,4], toInt64(0));"}},
+        Documentation::Categories{}
+    });
 }
 }
