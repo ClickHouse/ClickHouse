35d8d5d429947f41ac67001af116b7baaf7c92a3
Saveliy Belyakov <belyakov.saveliy@gmail.com>
2023-05-12 21:50:14 +0300

    Fixup

--- tests/queries/0_stateless/02736_order_by_with_data_hints.sql
+++ tests/queries/0_stateless/02736_order_by_with_data_hints.sql
@@ -1,13 +1,7 @@
-SELECT 'With Where:';
-
 SELECT number FROM numbers(500) WHERE number >= 100 AND number <= 120 ORDER BY number;
 SELECT number, 8 - 4 * number FROM numbers(300) WHERE number >= 200 AND number <= 220 ORDER BY number;
 
-SELECT 'With Transform:';
-
 SELECT number % 25 + 30 AS k, number FROM numbers(1000) WHERE number >= 500 AND number <= 550 ORDER BY k;
 SELECT 3 * (7 - number % 10) + 5 AS k, number, 'Desc' FROM numbers(300) WHERE number >= 250 AND number <= 270 ORDER BY k DESC;
-
-SELECT 'With Double Key:';
 
 SELECT number % 3 + 5 AS a, 5 * (number % 20) - 4 AS b, number FROM numbers(1000) WHERE number >= 950 AND number <= 975 ORDER BY a, b;
