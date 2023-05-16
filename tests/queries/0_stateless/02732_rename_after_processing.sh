eb7b48aab20172b79b1573c05c8a4baa02fe0804
alekseygolub <aleksey.golub97@mail.ru>
2023-05-16 18:48:39 +0000

    Fix test issues

--- tests/queries/0_stateless/02732_rename_after_processing.sh
+++ tests/queries/0_stateless/02732_rename_after_processing.sh
@@ -1,5 +1,4 @@
 #!/usr/bin/env bash
-# Tags: no-parallel
 
 CURDIR=$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)
 # shellcheck source=../shell_config.sh
@@ -9,28 +8,30 @@ CURDIR=$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)
 CLICKHOUSE_USER_FILES_PATH=$(clickhouse-client --query "select _path, _file from file('nonexist.txt', 'CSV', 'val1 char')" 2>&1 | grep Exception | awk '{gsub("/nonexist.txt","",$9); print $9}')
 
 # Prepare data
-mkdir -p ${CLICKHOUSE_USER_FILES_PATH}
-echo '"id","str","int","text"' > ${CLICKHOUSE_USER_FILES_PATH}/tmp.csv
-echo '1,"abc",123,"abacaba"' >> ${CLICKHOUSE_USER_FILES_PATH}/tmp.csv
-echo '2,"def",456,"bacabaa"' >> ${CLICKHOUSE_USER_FILES_PATH}/tmp.csv
-echo '3,"story",78912,"acabaab"' >> ${CLICKHOUSE_USER_FILES_PATH}/tmp.csv
-echo '4,"history",21321321,"cabaaba"' >> ${CLICKHOUSE_USER_FILES_PATH}/tmp.csv
+unique_name=${CLICKHOUSE_TEST_UNIQUE_NAME}
+tmp_dir=${CLICKHOUSE_USER_FILES_PATH}/${unique_name}
+mkdir -p $tmp_dir
+echo '"id","str","int","text"' > ${tmp_dir}/tmp.csv
+echo '1,"abc",123,"abacaba"' >> ${tmp_dir}/tmp.csv
+echo '2,"def",456,"bacabaa"' >> ${tmp_dir}/tmp.csv
+echo '3,"story",78912,"acabaab"' >> ${tmp_dir}/tmp.csv
+echo '4,"history",21321321,"cabaaba"' >> ${tmp_dir}/tmp.csv
 
-cp ${CLICKHOUSE_USER_FILES_PATH}/tmp.csv ${CLICKHOUSE_USER_FILES_PATH}/tmp1.csv
-cp ${CLICKHOUSE_USER_FILES_PATH}/tmp.csv ${CLICKHOUSE_USER_FILES_PATH}/tmp2.csv
-cp ${CLICKHOUSE_USER_FILES_PATH}/tmp.csv ${CLICKHOUSE_USER_FILES_PATH}/tmp3_1.csv
-cp ${CLICKHOUSE_USER_FILES_PATH}/tmp.csv ${CLICKHOUSE_USER_FILES_PATH}/tmp3_2.csv
-cp ${CLICKHOUSE_USER_FILES_PATH}/tmp.csv ${CLICKHOUSE_USER_FILES_PATH}/tmp4.csv
-cp ${CLICKHOUSE_USER_FILES_PATH}/tmp.csv ${CLICKHOUSE_USER_FILES_PATH}/tmp5.csv
+cp ${tmp_dir}/tmp.csv ${tmp_dir}/tmp1.csv
+cp ${tmp_dir}/tmp.csv ${tmp_dir}/tmp2.csv
+cp ${tmp_dir}/tmp.csv ${tmp_dir}/tmp3_1.csv
+cp ${tmp_dir}/tmp.csv ${tmp_dir}/tmp3_2.csv
+cp ${tmp_dir}/tmp.csv ${tmp_dir}/tmp4.csv
+cp ${tmp_dir}/tmp.csv ${tmp_dir}/tmp5.csv
 
 ### Checking that renaming works
 
 # simple select
-${CLICKHOUSE_CLIENT} --rename-files-after-processing="processed_%f%e" -q "SELECT COUNT(*) FROM file('tmp1.csv')"
-if [ -e "${CLICKHOUSE_USER_FILES_PATH}/processed_tmp1.csv" ]; then
+${CLICKHOUSE_CLIENT} --rename-files-after-processing="processed_%f%e" -q "SELECT COUNT(*) FROM file('${unique_name}/tmp1.csv')"
+if [ -e "${tmp_dir}/processed_tmp1.csv" ]; then
   echo "processed_tmp1.csv"
 fi
-if [ ! -e "${CLICKHOUSE_USER_FILES_PATH}/tmp1.csv" ]; then
+if [ ! -e "${tmp_dir}/tmp1.csv" ]; then
   echo "!tmp1.csv"
 fi
 
@@ -39,37 +40,37 @@ ${CLICKHOUSE_CLIENT} --rename-files-after-processing="processed_%f%e" --multilin
 SELECT
     sum(a.id) as aid,
     sum(b.id) as bid
-FROM file('tmp2.csv') AS a
-INNER JOIN file('tmp2.csv') AS b
+FROM file('${unique_name}/tmp2.csv') AS a
+INNER JOIN file('${unique_name}/tmp2.csv') AS b
 ON a.text = b.text
 """
-if [ -e "${CLICKHOUSE_USER_FILES_PATH}/processed_tmp2.csv" ]; then
+if [ -e "${tmp_dir}/processed_tmp2.csv" ]; then
   echo "processed_tmp2.csv"
 fi
-if [ ! -e "${CLICKHOUSE_USER_FILES_PATH}/tmp2.csv" ]; then
+if [ ! -e "${tmp_dir}/tmp2.csv" ]; then
     echo "!tmp2.csv"
 fi
 
 # rename multiple files
-${CLICKHOUSE_CLIENT} --rename-files-after-processing="processed_%f%e" -q "SELECT COUNT(*) FROM file('tmp3*.csv')"
-if [ -e "${CLICKHOUSE_USER_FILES_PATH}/processed_tmp3_1.csv" ]; then
+${CLICKHOUSE_CLIENT} --rename-files-after-processing="processed_%f%e" -q "SELECT COUNT(*) FROM file('${unique_name}/tmp3*.csv')"
+if [ -e "${tmp_dir}/processed_tmp3_1.csv" ]; then
     echo "processed_tmp3_1.csv"
 fi
-if [ -e "${CLICKHOUSE_USER_FILES_PATH}/processed_tmp3_2.csv" ]; then
+if [ -e "${tmp_dir}/processed_tmp3_2.csv" ]; then
     echo "processed_tmp3_2.csv"
 fi
-if [ ! -e "${CLICKHOUSE_USER_FILES_PATH}/tmp3_1.csv" ]; then
+if [ ! -e "${tmp_dir}/tmp3_1.csv" ]; then
     echo "!tmp3_1.csv"
 fi
-if [ ! -e "${CLICKHOUSE_USER_FILES_PATH}/tmp3_2.csv" ]; then
+if [ ! -e "${tmp_dir}/tmp3_2.csv" ]; then
     echo "!tmp3_2.csv"
 fi
 
 # check timestamp placeholder
-${CLICKHOUSE_CLIENT} --rename-files-after-processing="processed_%f_%t.csv" -q "SELECT COUNT(*) FROM file('tmp4.csv')"
-# ls ${CLICKHOUSE_USER_FILES_PATH} | grep -E "^processed_tmp4_[0-9]+\.csv$" > /dev/null && echo "OK"
+${CLICKHOUSE_CLIENT} --rename-files-after-processing="processed_%f_%t.csv" -q "SELECT COUNT(*) FROM file('${unique_name}/tmp4.csv')"
+# ls ${tmp_dir} | grep -E "^processed_tmp4_[0-9]+\.csv$" > /dev/null && echo "OK"
 rg="processed_tmp4_[0-9]+\.csv"
-for x in "${CLICKHOUSE_USER_FILES_PATH}"/processed*; do
+for x in "${tmp_dir}"/processed*; do
     if [[ $x =~ $rg ]]; then
         echo "OK"
         break
@@ -79,18 +80,34 @@ done
 ### Checking errors
 
 # cannot overwrite an existing file
-${CLICKHOUSE_CLIENT} --rename-files-after-processing="tmp.csv" -q "SELECT COUNT(*) FROM file('tmp5.csv')" \
+${CLICKHOUSE_CLIENT} --rename-files-after-processing="tmp.csv" -q "SELECT COUNT(*) FROM file('${unique_name}/tmp5.csv')" \
     2>&1| grep "already exists" > /dev/null && echo "OK"
-if [ -e "${CLICKHOUSE_USER_FILES_PATH}/tmp5.csv" ]; then
+if [ -e "${tmp_dir}/tmp5.csv" ]; then
     echo "tmp5.csv"
 fi
 
-# сannot move file from user_files
-${CLICKHOUSE_CLIENT} --rename-files-after-processing="../%f%e" -q "SELECT COUNT(*) FROM file('tmp5.csv')" \
+# сannot move file outside user_files
+${CLICKHOUSE_CLIENT} --rename-files-after-processing="../../%f%e" -q "SELECT COUNT(*) FROM file('${unique_name}/tmp5.csv')" \
     2>&1| grep "is not inside" > /dev/null && echo "OK"
-if [ -e "${CLICKHOUSE_USER_FILES_PATH}/tmp5.csv" ]; then
+if [ -e "${tmp_dir}/tmp5.csv" ]; then
+    echo "tmp5.csv"
+fi
+
+# check invalid placeholders
+
+# unknown type of placeholder (%k)
+${CLICKHOUSE_CLIENT} --rename-files-after-processing="processed_%f_%k" -q "SELECT COUNT(*) FROM file('${unique_name}/tmp5.csv')" \
+    2>&1| grep "Allowed placeholders only" > /dev/null && echo "OK"
+if [ -e "${tmp_dir}/tmp5.csv" ]; then
+    echo "tmp5.csv"
+fi
+
+# dd number of consecutive percentage signs after replace valid placeholders
+${CLICKHOUSE_CLIENT} --rename-files-after-processing="processed_%f_%%%%e" -q "SELECT COUNT(*) FROM file('${unique_name}/tmp5.csv')" \
+    2>&1| grep "Odd number of consecutive percentage signs" > /dev/null && echo "OK"
+if [ -e "${tmp_dir}/tmp5.csv" ]; then
     echo "tmp5.csv"
 fi
 
 # Clean
-rm -rd $CLICKHOUSE_USER_FILES_PATH
+rm -rd $tmp_dir
