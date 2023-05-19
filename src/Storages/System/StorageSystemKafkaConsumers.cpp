b413583d70a84ef4cf8edbaaeb0b498013ad9d62
Ilya Golshtein <igolshtein@altinity.com>
2023-05-19 10:01:21 +0000

    system_kafka_consumers: table populated by fake data

--- src/Storages/System/StorageSystemKafkaConsumers.cpp
+++ src/Storages/System/StorageSystemKafkaConsumers.cpp
@@ -25,16 +25,16 @@ NamesAndTypesList StorageSystemKafkaConsumers::getNamesAndTypes()
         {"assignments.topic", std::make_shared<DataTypeArray>(std::make_shared<DataTypeString>())},
         {"assignments.partition_id", std::make_shared<DataTypeArray>(std::make_shared<DataTypeString>())},
         {"assignments.current_offset", std::make_shared<DataTypeArray>(std::make_shared<DataTypeString>())},
-        // {"last_exception_time", std::make_shared<DataTypeDateTime>()},
-        // {"last_exception", std::make_shared<DataTypeString>()},
-        // {"last_poll_time", std::make_shared<DataTypeDateTime>()},
-        // {"num_messages_read", std::make_shared<DataTypeUInt64>()},
-        // {"last_commit_time", std::make_shared<DataTypeDateTime>()},
-        // {"num_commits", std::make_shared<DataTypeUInt64>()},
-        // {"last_rebalance_time", std::make_shared<DataTypeDateTime>()},
-        // {"num_rebalance_revocations", std::make_shared<DataTypeUInt64>()},
-        // {"num_rebalance_assignments", std::make_shared<DataTypeUInt64>()},
-        // {"is_currently_used", std::make_shared<DataTypeUInt8>()},
+        {"last_exception_time", std::make_shared<DataTypeDateTime>()},
+        {"last_exception", std::make_shared<DataTypeString>()},
+        {"last_poll_time", std::make_shared<DataTypeDateTime>()},
+        {"num_messages_read", std::make_shared<DataTypeUInt64>()},
+        {"last_commit_time", std::make_shared<DataTypeDateTime>()},
+        {"num_commits", std::make_shared<DataTypeUInt64>()},
+        {"last_rebalance_time", std::make_shared<DataTypeDateTime>()},
+        {"num_rebalance_revocations", std::make_shared<DataTypeUInt64>()},
+        {"num_rebalance_assignments", std::make_shared<DataTypeUInt64>()},
+        {"is_currently_used", std::make_shared<DataTypeUInt8>()},
     };
     return names_and_types;
 }
@@ -43,69 +43,39 @@ NamesAndTypesList StorageSystemKafkaConsumers::getNamesAndTypes()
 
 void StorageSystemKafkaConsumers::fillData(MutableColumns & res_columns, ContextPtr, const SelectQueryInfo &) const
 {
-
-#pragma GCC diagnostic push
-#pragma GCC diagnostic ignored "-Wunused-variable"
-
     auto tables_mark_dropped = DatabaseCatalog::instance().getTablesMarkedDropped();
 
     size_t index = 0;
 
-    // auto & column_index = assert_cast<ColumnUInt32 &>(*res_columns[index++]);
-    // auto & column_database = assert_cast<ColumnString &>(*res_columns[index++]);
-    // auto & column_table = assert_cast<ColumnString &>(*res_columns[index++]);
-    // auto & column_uuid = assert_cast<ColumnUUID &>(*res_columns[index++]).getData();
-    // auto & column_engine = assert_cast<ColumnString &>(*res_columns[index++]);
-    // auto & column_metadata_dropped_path = assert_cast<ColumnString &>(*res_columns[index++]);
-    // auto & column_table_dropped_time = assert_cast<ColumnUInt32 &>(*res_columns[index++]);
+
+    auto & database = assert_cast<ColumnString &>(*res_columns[index++]);
+    auto & table = assert_cast<ColumnString &>(*res_columns[index++]);
+    auto & consumer_id = assert_cast<ColumnString &>(*res_columns[index++]); //(number? or string? - single clickhouse table can have many consumers)
+
+    auto & assigments_topics = assert_cast<ColumnString &>(assert_cast<ColumnArray &>(*res_columns[index]).getData());
+    auto & assigments_topics_offsets = assert_cast<ColumnArray &>(*res_columns[index++]).getOffsets();
+
+    auto & assigments_partition_id = assert_cast<ColumnString &>(assert_cast<ColumnArray &>(*res_columns[index]).getData());
+    auto & assigments_partition_id_offsets = assert_cast<ColumnArray &>(*res_columns[index++]).getOffsets();
+
+    auto & assigments_current_offset = assert_cast<ColumnString &>(assert_cast<ColumnArray &>(*res_columns[index]).getData());
+    auto & assigments_current_offset_offsets = assert_cast<ColumnArray &>(*res_columns[index++]).getOffsets();
 
 
-        auto & database = assert_cast<ColumnString &>(*res_columns[index++]);
-        auto & table = assert_cast<ColumnString &>(*res_columns[index++]);
-        auto & consumer_id = assert_cast<ColumnString &>(*res_columns[index++]); //(number? or string? - single clickhouse table can have many consumers)
-
-        auto & assigments_topics = assert_cast<ColumnString &>(assert_cast<ColumnArray &>(*res_columns[index]).getData());
-        auto & assigments_topics_offsets = assert_cast<ColumnArray &>(*res_columns[index++]).getOffsets();
-
-        auto & assigments_partition_id = assert_cast<ColumnString &>(assert_cast<ColumnArray &>(*res_columns[index]).getData());
-        auto & assigments_partition_id_offsets = assert_cast<ColumnArray &>(*res_columns[index++]).getOffsets();
-
-        auto & assigments_current_offset = assert_cast<ColumnString &>(assert_cast<ColumnArray &>(*res_columns[index]).getData());
-        auto & assigments_current_offset_offsets = assert_cast<ColumnArray &>(*res_columns[index++]).getOffsets();
-
-        // auto & assignments.topic = assert_cast<ColumnArray>(std::make_shared<DataTypeString>())},
-        // auto & assignments.partition_id = assert_cast<ColumnArray>(std::make_shared<DataTypeString>())},
-        // auto & assignments.current_offset = assert_cast<ColumnArray>(std::make_shared<DataTypeString>())},
-
-
-
-        // auto & last_exception_time = assert_cast<ColumnDateTime &>(*res_columns[index++]);
-        // auto & last_exception = assert_cast<ColumnString &>(*res_columns[index++]);
-        // auto & last_poll_time = assert_cast<ColumnDateTime &>(*res_columns[index++]);
-        // auto & num_messages_read = assert_cast<ColumnUInt64 &>(*res_columns[index++]);
-        // auto & last_commit_time = assert_cast<ColumnDateTime &>(*res_columns[index++]);
-        // auto & num_commits = assert_cast<ColumnUInt64 &>(*res_columns[index++]);
-        // auto & last_rebalance_time = assert_cast<ColumnDateTime &>(*res_columns[index++]);
-        // auto & num_rebalance_revocations = assert_cast<ColumnUInt64 &>(*res_columns[index++]);
-        // auto & num_rebalance_assignments = assert_cast<ColumnUInt64 &>(*res_columns[index++]);
-        // auto & is_currently_used = assert_cast<ColumnUInt8 &>(*res_columns[index++]);
+    auto & last_exception_time = assert_cast<ColumnDateTime &>(*res_columns[index++]);
+    auto & last_exception = assert_cast<ColumnString &>(*res_columns[index++]);
+    auto & last_poll_time = assert_cast<ColumnDateTime &>(*res_columns[index++]);
+    auto & num_messages_read = assert_cast<ColumnUInt64 &>(*res_columns[index++]);
+    auto & last_commit_time = assert_cast<ColumnDateTime &>(*res_columns[index++]);
+    auto & num_commits = assert_cast<ColumnUInt64 &>(*res_columns[index++]);
+    auto & last_rebalance_time = assert_cast<ColumnDateTime &>(*res_columns[index++]);
+    auto & num_rebalance_revocations = assert_cast<ColumnUInt64 &>(*res_columns[index++]);
+    auto & num_rebalance_assigments = assert_cast<ColumnUInt64 &>(*res_columns[index++]);
+    auto & is_currently_used = assert_cast<ColumnUInt8 &>(*res_columns[index++]);
 
 
     auto add_row = [&]()
     {
-        // column_index.insertValue(idx);
-        // column_database.insertData(table_mark_dropped.table_id.getDatabaseName().data(), table_mark_dropped.table_id.getDatabaseName().size());
-        // column_table.insertData(table_mark_dropped.table_id.getTableName().data(), table_mark_dropped.table_id.getTableName().size());
-        // column_uuid.push_back(table_mark_dropped.table_id.uuid.toUnderType());
-        // if (table_mark_dropped.table)
-        //     column_engine.insertData(table_mark_dropped.table->getName().data(), table_mark_dropped.table->getName().size());
-        // else
-        //     column_engine.insertData({}, 0);
-        // column_metadata_dropped_path.insertData(table_mark_dropped.metadata_path.data(), table_mark_dropped.metadata_path.size());
-        // column_table_dropped_time.insertValue(static_cast<UInt32>(table_mark_dropped.drop_time));
-
-
-
         std::string fake_database = "fake_kafka_database";
         database.insertData(fake_database.data(), fake_database.size());
 
@@ -133,34 +103,22 @@ void StorageSystemKafkaConsumers::fillData(MutableColumns & res_columns, Context
         assigments_current_offset.insertData(fake_current_offset_2.data(), fake_current_offset_2.size());
         assigments_current_offset_offsets.push_back(2);
 
+        last_exception_time.insert(0);
 
-#if 0
+        std::string fake_last_exception = "fake_last_exception";
+        last_exception.insertData(fake_last_exception.data(), fake_last_exception.size());
 
-
-        consumer_id.insertValue(assert_cast<ColumnString> &>(*res_columns[index++]); //(number? or string? - single clickhouse table can have many consumers)
-        assignments.topic.insertValue( = assert_cast<ColumnArray>(std::make_shared<DataTypeString>())},
-        assignments.partition_id.insertValue( = assert_cast<ColumnArray>(std::make_shared<DataTypeString>())},
-        assignments.current_offset.insertValue( = assert_cast<ColumnArray>(std::make_shared<DataTypeString>())},
-        last_exception_time.insertValue( = assert_cast<ColumnDateTime> &>(*res_columns[index++]);
-
-        last_exception.insertValue( = assert_cast<ColumnString> &>(*res_columns[index++]);
-        last_poll_time.insertValue( = assert_cast<ColumnDateTime> &>(*res_columns[index++]);
-        num_messages_read.insertValue( = assert_cast<ColumnUInt64> &>(*res_columns[index++]);
-        last_commit_time.insertValue( = assert_cast<ColumnDateTime> &>(*res_columns[index++]);
-        num_commits.insertValue( = assert_cast<ColumnUInt63> &>(*res_columns[index++]);
-last_rebalance_time = assert_cast<ColumnDateTime> &>(*res_columns[index++]);
-num_rebalance_revocations = assert_cast<ColumnUInt64> &>(*res_columns[index++]);
-num_rebalance_assignments = assert_cast<ColumnUInt64> &>(*res_columns[index++]);
-is_currently_used = assert_cast<ColumnUInt8> &>(*res_columns[index++]);
-#endif
-
+        last_poll_time.insert(0);
+        num_messages_read.insert(0);
+        last_commit_time.insert(0);
+        num_commits.insert(0);
+        last_rebalance_time.insert(0);
+        num_rebalance_revocations.insert(0);
+        num_rebalance_assigments.insert(0);
+        is_currently_used.insert(0);
     };
 
-    // UInt32 idx = 0;
-    // for (const auto & table_mark_dropped : tables_mark_dropped)
-    //     add_row(idx++, table_mark_dropped);
     add_row();
-#pragma GCC diagnostic pop
 }
 
 }
