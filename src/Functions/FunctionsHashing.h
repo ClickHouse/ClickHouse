5a1d8d79c0979edac6311442a58066bf3e1abb58
zhanglistar <zhanglinuxstar@gmail.com>
2023-05-19 17:07:13 +0800

    Fix tests

--- src/Functions/FunctionsHashing.h
+++ src/Functions/FunctionsHashing.h
@@ -1467,6 +1467,7 @@ public:
     size_t getNumberOfArguments() const override { return 0; }
     bool useDefaultImplementationForConstants() const override { return true; }
     bool isSuitableForShortCircuitArgumentsExecution(const DataTypesWithConstInfo & /*arguments*/) const override { return true; }
+    bool canBeExecutedOnLowCardinalityDictionary() const override { return false; }
 
     DataTypePtr getReturnTypeImpl(const DataTypes & /*arguments*/) const override
     {
