9be6522968e7002ec28b8fe53d52c4d9f91c4ba5
Daniil Rubin <danyarubin@gmail.com>
2023-05-20 10:27:59 +0300

    Add testing file

---
+++ test-cryptographic-merge-tree.py
@@ -0,0 +1,48 @@
+import os
+from subprocess import run, PIPE, DEVNULL
+
+def run_query(q):
+    res = run(['/home/ubuntu/ClickHouse/build/programs/clickhouse-client', '-t', '-q', f"{q}"], stdout=DEVNULL, stderr=PIPE, text=True)
+    return res.stderr
+
+create_db_query = "CREATE DATABASE IF NOT EXISTS test"
+
+create_query = """CREATE TABLE test.hits ( WatchID UInt64,  JavaEnable UInt8,  Title String,  GoodEvent Int16,  EventTime DateTime,  EventDate Date,  CounterID UInt32,  ClientIP UInt32,  ClientIP6 FixedString(16),  RegionID UInt32,  UserID UInt64,  CounterClass Int8,  OS UInt8,  UserAgent UInt8,  URL String,  Referer String,  URLDomain String,  RefererDomain String,  Refresh UInt8,  IsRobot UInt8,  RefererCategories Array(UInt16),  URLCategories Array(UInt16),  URLRegions Array(UInt32),  RefererRegions Array(UInt32),  ResolutionWidth UInt16,  ResolutionHeight UInt16,  ResolutionDepth UInt8,  FlashMajor UInt8,  FlashMinor UInt8,  FlashMinor2 String,  NetMajor UInt8,  NetMinor UInt8,  UserAgentMajor UInt16,  UserAgentMinor FixedString(2),  CookieEnable UInt8,  JavascriptEnable UInt8,  IsMobile UInt8,  MobilePhone UInt8,  MobilePhoneModel String,  Params String,  IPNetworkID UInt32,  TraficSourceID Int8,  SearchEngineID UInt16,  SearchPhrase String,  AdvEngineID UInt8,  IsArtifical UInt8,  WindowClientWidth UInt16,  WindowClientHeight UInt16,  ClientTimeZone Int16,  ClientEventTime DateTime,  SilverlightVersion1 UInt8,  SilverlightVersion2 UInt8,  SilverlightVersion3 UInt32,  SilverlightVersion4 UInt16,  PageCharset String,  CodeVersion UInt32,  IsLink UInt8,  IsDownload UInt8,  IsNotBounce UInt8,  FUniqID UInt64,  HID UInt32,  IsOldCounter UInt8,  IsEvent UInt8,  IsParameter UInt8,  DontCountHits UInt8,  WithHash UInt8,  HitColor FixedString(1),  UTCEventTime DateTime,  Age UInt8,  Sex UInt8,  Income UInt8,  Interests UInt16,  Robotness UInt8,  GeneralInterests Array(UInt16),  RemoteIP UInt32,  RemoteIP6 FixedString(16),  WindowName Int32,  OpenerName Int32,  HistoryLength Int16,  BrowserLanguage FixedString(2),  BrowserCountry FixedString(2),  SocialNetwork String,  SocialAction String,  HTTPError UInt16,  SendTiming Int32,  DNSTiming Int32,  ConnectTiming Int32,  ResponseStartTiming Int32,  ResponseEndTiming Int32,  FetchTiming Int32,  RedirectTiming Int32,  DOMInteractiveTiming Int32,  DOMContentLoadedTiming Int32,  DOMCompleteTiming Int32,  LoadEventStartTiming Int32,  LoadEventEndTiming Int32,  NSToDOMContentLoadedTiming Int32,  FirstPaintTiming Int32,  RedirectCount Int8,  SocialSourceNetworkID UInt8,  SocialSourcePage String,  ParamPrice Int64,  ParamOrderID String,  ParamCurrency FixedString(3),  ParamCurrencyID UInt16,  GoalsReached Array(UInt32),  OpenstatServiceName String,  OpenstatCampaignID String,  OpenstatAdID String,  OpenstatSourceID String,  UTMSource String,  UTMMedium String,  UTMCampaign String,  UTMContent String,  UTMTerm String,  FromTag String,  HasGCLID UInt8,  RefererHash UInt64,  URLHash UInt64,  CLID UInt32,  YCLID UInt64,  ShareService String,  ShareURL String,  ShareTitle String,  `ParsedParams.Key1` Array(String),  `ParsedParams.Key2` Array(String),  `ParsedParams.Key3` Array(String),  `ParsedParams.Key4` Array(String),  `ParsedParams.Key5` Array(String),  `ParsedParams.ValueDouble` Array(Float64),  IslandID FixedString(16),  RequestNum UInt32,  RequestTry UInt8) ENGINE = MergeTree ORDER BY(CounterID, EventDate, intHash32(UserID), EventTime) {};"""
+
+def run_insert():
+    q = "INSERT INTO test.hits FORMAT TSV"
+    with open('hits_v1.tsv', 'r') as f:
+        res = run(['/home/ubuntu/ClickHouse/build/programs/clickhouse-client', '-t', '-q', q], stdin=f, stdout=None, stderr=PIPE, text=True)
+    return res.stderr
+
+drop_query = "DROP TABLE test.hits"
+
+run_query(create_db_query)
+
+runs = 4
+hash_functions = ['MD4', 'MD5', 'SipHash', 'SHA256']
+
+measurements = dict()
+for f in hash_functions:
+    measurements[f] = {'select':[], 'insert':[], 'check':[]}
+
+settings_template = "SETTINGS cryptographic_mode=true, hash_function='{}'"
+
+for f in hash_functions:
+    if f == '':
+        run_query(create_query.format(''))
+    else:
+        a = run_query(create_query.format(settings_template.format(f)))
+        print(a)
+
+    for _ in range(runs):
+
+        measurements[f]['insert'].append(run_insert())
+        measurements[f]['select'].append(run_query('SELECT * FROM test.hits'))
+        measurements[f]['check'].append(run_query('CHECK TABLE test.hits'))
+
+        run_query('TRUNCATE TABLE test.hits')
+
+    run_query(drop_query)
+
+print(measurements)
