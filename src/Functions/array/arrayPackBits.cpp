95f26e2b7d38e7a0a7e1525897be6093df11b311
Eridanus <maodingxuan@foxmail.com>
2023-05-22 03:01:18 +0800

    interface for arrayPackBitsToUInt64

--- src/Functions/array/arrayPackBits.cpp
+++ src/Functions/array/arrayPackBits.cpp
@@ -1,5 +1,7 @@
+#include <Columns/ColumnDecimal.h>
 #include <Functions/FunctionFactory.h>
 #include <Functions/array/FunctionArrayMapped.h>
+#include <Common/logger_useful.h>
 
 namespace DB
 {
@@ -11,16 +13,33 @@ struct ArrayPackBitsToUint64Impl
     static bool needOneArray() { return true; }
 
 
-    static DataTypePtr getReturnType(const DataTypePtr & expression_return, const DataTypePtr & /*array_element*/)
+    static DataTypePtr getReturnType(const DataTypePtr & /*expression_return*/, const DataTypePtr & /*array_element*/)
     {
-        return std::make_shared<DataTypeArray>(expression_return);
-
-        //        return std::make_shared<DataTypeUInt8>();
+        return std::make_shared<DataTypeUInt64>();
     }
 
     static ColumnPtr execute(const ColumnArray & array, ColumnPtr mapped)
     {
-        return ColumnArray::create(mapped->convertToFullColumnIfConst(), array.getOffsetsPtr());
+        const auto & offsets = array.getOffsets();
+        auto out_column = ColumnUInt64::create(offsets.size());
+        ColumnUInt64::Container & out_counts = out_column->getData();
+
+        size_t pos = 0;
+        auto * log = &Poco::Logger::get("ArrayPackBitsToUint64Impl");
+        LOG_TRACE(log, "offset.size = {}", offsets.size());
+        for (size_t i = 0; i < offsets.size(); ++i)
+        {
+            Int64 bit = 0;
+            LOG_TRACE(log, "pos.size = {}", offsets[i]);
+            for (; pos < std::min(offsets[i], 64ULL); ++pos)
+            {
+                bit |= static_cast<UInt64>(mapped->getBool(pos)) << pos;
+                LOG_TRACE(log, "pos = {}, bit = {}", pos, bit);
+            }
+            LOG_TRACE(log, "bit = {}", bit);
+            out_counts[i] = static_cast<UInt64>(bit);
+        }
+        return out_column;
     }
 };
 
