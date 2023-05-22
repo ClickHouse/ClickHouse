1a448f48e80560d56c4aea5523fe67a4d197bb05
Robert Schulze <robert@clickhouse.com>
2023-05-22 20:03:29 +0000

    Extend tests

--- tests/queries/0_stateless/02752_space_function.sql
+++ tests/queries/0_stateless/02752_space_function.sql
@@ -1,24 +1,26 @@
-SELECT 'const times, uint';
-SELECT space(3::UInt8);
-SELECT space(3::UInt16);
-SELECT space(3::UInt32);
-SELECT space(3::UInt64);
-SELECT 'const times, int';
-SELECT space(3::Int8);
-SELECT space(3::Int16);
-SELECT space(3::Int32);
-SELECT space(3::Int64);
+SELECT 'const, uint';
+SELECT space(3::UInt8), length(space(3::UInt8));
+SELECT space(3::UInt16), length(space(3::UInt16));
+SELECT space(3::UInt32), length(space(3::UInt32));
+SELECT space(3::UInt64), length(space(3::UInt64));
+SELECT 'const, int';
+SELECT space(3::Int8), length(space(3::Int8));
+SELECT space(3::Int16), length(space(3::Int16));
+SELECT space(3::Int32), length(space(3::Int32));
+SELECT space(3::Int64), length(space(3::Int64));
 
-SELECT 'negative const times';
-SELECT space(-3::Int8);
-SELECT space(-3::Int16);
-SELECT space(-3::Int32);
-SELECT space(-3::Int64);
+SELECT 'const, int, negative';
+SELECT space(-3::Int8), length(space(-3::Int8));
+SELECT space(-3::Int16), length(space(-3::Int16));
+SELECT space(-3::Int32), length(space(-3::Int32));
+SELECT space(-3::Int64), length(space(-3::Int64));
 
 SELECT 'negative tests';
 SELECT space('abc'); -- { serverError ILLEGAL_TYPE_OF_ARGUMENT }
 SELECT space(['abc']); -- { serverError ILLEGAL_TYPE_OF_ARGUMENT }
 SELECT space(('abc')); -- { serverError ILLEGAL_TYPE_OF_ARGUMENT }
+
+SELECT 'null';
 SELECT space(NULL);
 
 DROP TABLE IF EXISTS defaults;
@@ -36,12 +38,23 @@ CREATE TABLE defaults
 
 INSERT INTO defaults values (3, 12, 4, 56, 3, 12, -4, 56) (2, 10, 21, 20, 2, 10, -21, 20) (1, 4, 9, 5, 1, 4, -9, 5) (0, 5, 7, 7, 0, 5, -7, 7);
 
-SELECT 'non-const times, uint';
+SELECT 'const, uint, multiple';
+SELECT space(30::UInt8) FROM defaults;
+SELECT space(30::UInt16) FROM defaults;
+SELECT space(30::UInt32) FROM defaults;
+SELECT space(30::UInt64) FROM defaults;
+SELECT 'const int, multiple';
+SELECT space(30::Int8) FROM defaults;
+SELECT space(30::Int16) FROM defaults;
+SELECT space(30::Int32) FROM defaults;
+SELECT space(30::Int64) FROM defaults;
+
+SELECT 'non-const, uint';
 SELECT space(u8), length(space(u8)) FROM defaults;
 SELECT space(u16), length(space(u16)) FROM defaults;
 SELECT space(u32), length(space(u32)) from defaults;
 SELECT space(u64), length(space(u64)) FROM defaults;
-SELECT 'non-const times, int';
+SELECT 'non-const, int';
 SELECT space(i8), length(space(i8)) FROM defaults;
 SELECT space(i16), length(space(i16)) FROM defaults;
 SELECT space(i32), length(space(i32)) from defaults;
