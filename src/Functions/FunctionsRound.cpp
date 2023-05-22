d982c911f1888e5774f5e8cdf7d57c139d4a489c
lhuang09287750 <liang.huang@kyligence.io>
2023-05-22 11:08:53 +0800

    fix expect result error and add doc info

--- src/Functions/FunctionsRound.cpp
+++ src/Functions/FunctionsRound.cpp
@@ -9,11 +9,20 @@ REGISTER_FUNCTION(Round)
 {
     factory.registerFunction<FunctionRound>("round", {}, FunctionFactory::CaseInsensitive);
     factory.registerFunction<FunctionRoundBankers>("roundBankers", {}, FunctionFactory::CaseSensitive);
-    factory.registerFunction<FunctionRoundHalfUp>("roundHalfUp", {}, FunctionFactory::CaseSensitive);
+    factory.registerFunction<FunctionRoundHalfUp>(
+        {
+            R"(Similar to function round,except that in case when given number has equal distance to surrounding numbers, the function rounds away from zero(towards +inf/-inf).)",
+            Documentation::Examples{
+                {"roundHalfUp", "SELECT roundHalfUp(3.165,2)"}},
+            Documentation::Categories{"Rounding"}
+        },
+        FunctionFactory::CaseSensitive);
+
     factory.registerFunction<FunctionFloor>("floor", {}, FunctionFactory::CaseInsensitive);
     factory.registerFunction<FunctionCeil>("ceil", {}, FunctionFactory::CaseInsensitive);
     factory.registerFunction<FunctionTrunc>("trunc", {}, FunctionFactory::CaseInsensitive);
     factory.registerFunction<FunctionRoundDown>();
+
 
     /// Compatibility aliases.
     factory.registerAlias("ceiling", "ceil", FunctionFactory::CaseInsensitive);
