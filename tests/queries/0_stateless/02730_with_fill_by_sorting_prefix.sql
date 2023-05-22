1d51488d3d98c505cd5c19918850bb75a0449345
Igor Nikonov <igor@clickhouse.com>
2023-05-22 22:14:16 +0000

    Tests: w/o TO, w/o FROM

--- tests/queries/0_stateless/02730_with_fill_by_sorting_prefix.sql
+++ tests/queries/0_stateless/02730_with_fill_by_sorting_prefix.sql
@@ -34,5 +34,13 @@ select * from ts order by sensor_id, timestamp with fill step 1 settings max_blo
 select * from ts order by sensor_id, timestamp with fill from 6 to 10 step 1 interpolate (value as 9999);
 select * from ts order by sensor_id, timestamp with fill from 6 to 10 step 1 interpolate (value as 9999) settings use_with_fill_by_sorting_prefix=0;
 
+-- without TO
+select * from ts order by sensor_id, timestamp with fill from 6 step 1 interpolate (value as 9999);
+select * from ts order by sensor_id, timestamp with fill from 6 step 1 interpolate (value as 9999) settings use_with_fill_by_sorting_prefix=0;
+
+-- without FROM
+select * from ts order by sensor_id, timestamp with fill to 10 step 1 interpolate (value as 9999);
+select * from ts order by sensor_id, timestamp with fill to 10 step 1 interpolate (value as 9999) settings use_with_fill_by_sorting_prefix=0;
+
 -- checking that sorting prefix columns can't be used in INTERPOLATE
 SELECT * FROM ts ORDER BY sensor_id, value, timestamp WITH FILL FROM 6 TO 10 INTERPOLATE ( value AS 1 ); -- { serverError INVALID_WITH_FILL_EXPRESSION }
