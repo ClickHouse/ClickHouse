484c91c47e3aa8365eec5ae29d03fb66a8372bb0
Mikhail f. Shiryaev <felixoid@clickhouse.com>
2023-05-22 20:39:22 +0200

    Add DRY_RUN and configurable PY_VERSION to lambda deployment

--- tests/ci/team_keys_lambda/build_and_deploy_archive.sh
+++ tests/ci/team_keys_lambda/build_and_deploy_archive.sh
@@ -5,11 +5,17 @@ WORKDIR=$(dirname "$0")
 WORKDIR=$(readlink -f "${WORKDIR}")
 cd "$WORKDIR"
 
-PY_VERSION=3.10
+# Do not deploy the lambda to AWS
+DRY_RUN=${DRY_RUN:-}
+# Python runtime to install dependencies
+PY_VERSION=${PY_VERSION:-3.10}
 PY_EXEC="python${PY_VERSION}"
+# Image to build the lambda zip package
 DOCKER_IMAGE="python:${PY_VERSION}-slim"
 LAMBDA_NAME=$(basename "$WORKDIR")
+# Rename the_lambda_name directory to the-lambda-name lambda in AWS
 LAMBDA_NAME=${LAMBDA_NAME//_/-}
+# The name of directory with lambda code
 PACKAGE=lambda-package
 rm -rf "$PACKAGE" "$PACKAGE".zip
 mkdir "$PACKAGE"
@@ -28,4 +34,6 @@ if [ -f requirements.txt ]; then
 fi
 ( cd "$PACKAGE" && zip -9 -r ../"$PACKAGE".zip . )
 
-aws lambda update-function-code --function-name "$LAMBDA_NAME" --zip-file fileb://"$PACKAGE".zip
+if [ -z "$DRY_RUN" ]; then
+  aws lambda update-function-code --function-name "$LAMBDA_NAME" --zip-file fileb://"$PACKAGE".zip
+fi
