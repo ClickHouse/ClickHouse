1925df30953d22af54819b3cdbe2663e4892fdae
Alexander Tokmakov <tavplubix@clickhouse.com>
2023-05-23 16:13:44 +0200

    add a test, fix other issues

--- tests/queries/0_stateless/02735_system_zookeeper_connection.sql
+++ tests/queries/0_stateless/02735_system_zookeeper_connection.sql
@@ -5,9 +5,9 @@ DROP TABLE IF EXISTS test_zk_connection_table;
 CREATE TABLE test_zk_connection_table (
     key UInt64
 )
-ENGINE ReplicatedMergeTree('/clickhouse/{database}/02731_zk_connection/{shard}', '{replica}')
+ENGINE ReplicatedMergeTree('zookeeper2:/clickhouse/{database}/02731_zk_connection/{shard}', '{replica}')
 ORDER BY tuple();
 
-select host, port, is_expired from system.zookeeper_connection where name='default_zookeeper';
+select name, host, port, index, is_expired, keeper_api_version from system.zookeeper_connection order by name;
 
 DROP TABLE IF EXISTS test_zk_connection_table;
