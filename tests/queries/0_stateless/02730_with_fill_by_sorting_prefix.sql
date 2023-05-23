d26f9e4022ae119d03b612a263f110f637cce93d
Igor Nikonov <igor@clickhouse.com>
2023-05-23 10:24:06 +0000

    Test with DESC order in sorting prefix

--- tests/queries/0_stateless/02730_with_fill_by_sorting_prefix.sql
+++ tests/queries/0_stateless/02730_with_fill_by_sorting_prefix.sql
@@ -31,16 +31,30 @@ insert into ts VALUES (1, 10, 1), (1, 12, 1), (3, 5, 1);
 insert into ts VALUES (3, 7, 1), (5, 1, 1), (5, 3, 1);
 select * from ts order by sensor_id, timestamp with fill step 1 settings max_block_size=3;
 
+-- FROM and TO
+-- ASC order in sorting prefix
 select * from ts order by sensor_id, timestamp with fill from 6 to 10 step 1 interpolate (value as 9999);
 select * from ts order by sensor_id, timestamp with fill from 6 to 10 step 1 interpolate (value as 9999) settings use_with_fill_by_sorting_prefix=0;
 
+-- DESC order in sorting prefix
+select * from ts order by sensor_id DESC, timestamp with fill from 6 to 10 step 1 interpolate (value as 9999);
+select * from ts order by sensor_id DESC, timestamp with fill from 6 to 10 step 1 interpolate (value as 9999) settings use_with_fill_by_sorting_prefix=0;
+
 -- without TO
+-- ASC order in sorting prefix
 select * from ts order by sensor_id, timestamp with fill from 6 step 1 interpolate (value as 9999);
 select * from ts order by sensor_id, timestamp with fill from 6 step 1 interpolate (value as 9999) settings use_with_fill_by_sorting_prefix=0;
+-- DESC order in sorting prefix
+select * from ts order by sensor_id DESC, timestamp with fill from 6 step 1 interpolate (value as 9999);
+select * from ts order by sensor_id DESC, timestamp with fill from 6 step 1 interpolate (value as 9999) settings use_with_fill_by_sorting_prefix=0;
 
 -- without FROM
+-- ASC order in sorting prefix
 select * from ts order by sensor_id, timestamp with fill to 10 step 1 interpolate (value as 9999);
 select * from ts order by sensor_id, timestamp with fill to 10 step 1 interpolate (value as 9999) settings use_with_fill_by_sorting_prefix=0;
+-- DESC order in sorting prefix
+select * from ts order by sensor_id DESC, timestamp with fill to 10 step 1 interpolate (value as 9999);
+select * from ts order by sensor_id DESC, timestamp with fill to 10 step 1 interpolate (value as 9999) settings use_with_fill_by_sorting_prefix=0;
 
 -- checking that sorting prefix columns can't be used in INTERPOLATE
 SELECT * FROM ts ORDER BY sensor_id, value, timestamp WITH FILL FROM 6 TO 10 INTERPOLATE ( value AS 1 ); -- { serverError INVALID_WITH_FILL_EXPRESSION }
