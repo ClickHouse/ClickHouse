98b437de4b79d3bb4a03ada5dc1c34fdf118ed33
Nikita Mikhaylov <nikitamikhaylov@clickhouse.com>
2023-05-24 17:02:31 +0000

    Better

--- src/Interpreters/InterpreterSelectQuery.cpp
+++ src/Interpreters/InterpreterSelectQuery.cpp
@@ -456,25 +456,23 @@ InterpreterSelectQuery::InterpreterSelectQuery(
         }
     }
 
+    /// Check support for JOINs for parallel replicas
     if (joined_tables.tablesCount() > 1 && (!settings.parallel_replicas_custom_key.value.empty() || settings.allow_experimental_parallel_reading_from_replicas > 0))
     {
         if (settings.allow_experimental_parallel_reading_from_replicas == 1)
         {
-            LOG_WARNING(log, "Joins are not supported with parallel replicas. Query will be executed without using them.");
+            LOG_WARNING(log, "JOINs are not supported with parallel replicas. Query will be executed without using them.");
             context->setSetting("allow_experimental_parallel_reading_from_replicas", Field(0));
             context->setSetting("parallel_replicas_custom_key", String{""});
         }
         else if (settings.allow_experimental_parallel_reading_from_replicas == 2)
         {
-            throw Exception(ErrorCodes::SUPPORT_IS_DISABLED, "Joins are not supported with parallel replicas");
+            throw Exception(ErrorCodes::SUPPORT_IS_DISABLED, "JOINs are not supported with parallel replicas");
         }
     }
 
-    /// Try to execute query without parallel replicas if we find that there is a FINAL modifier there.
+    /// Check support for FINAL for parallel replicas
     bool is_query_with_final = isQueryWithFinal(query_info, getSelectQuery());
-
-    std::cout << "Is query with FINAL modifier " << is_query_with_final << std::endl;
-
     if (is_query_with_final && (!settings.parallel_replicas_custom_key.value.empty() || settings.allow_experimental_parallel_reading_from_replicas > 0))
     {
         if (settings.allow_experimental_parallel_reading_from_replicas == 1)
