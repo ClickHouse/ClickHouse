9377a65060e9612dd714595e7deb29414be0a189
Umang Brahmbhatt <umangbrahmbhatt@Umangs-MacBook-Pro.local>
2023-05-24 15:38:33 -0400

    added amd64

--- tests/ci/worker/ubuntu_ami_for_ci.sh
+++ tests/ci/worker/ubuntu_ami_for_ci.sh
@@ -52,7 +52,7 @@ apt-get install --yes --no-install-recommends \
 
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
 
-echo "deb [arch=$(deb_arch) signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable" | tee /etc/apt/sources.list.d/docker.list > /dev/null
+echo "deb [arch=amd64 signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable" | tee /etc/apt/sources.list.d/docker.list > /dev/null
 
 apt-get update
 
