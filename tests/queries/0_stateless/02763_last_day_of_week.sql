d19d36aad1a2c887ec11ce478f3a8c00689a1cb5
Victor Krasnov <v.krasnov@arenadata.io>
2023-05-24 20:53:05 +0000

    Extend 02763_last_day_of_week test: add Date32 and DateTime64 types

--- tests/queries/0_stateless/02763_last_day_of_week.sql
+++ tests/queries/0_stateless/02763_last_day_of_week.sql
@@ -1,9 +1,17 @@
+WITH 
+    toDate('2018-12-25') + number AS d, 
+    toDate32(d) AS d32, 
+    toDateTime(d) AS dt, 
+    toDateTime64(d, 0) AS dt64
 SELECT
-    toDate('2018-12-25') + number AS x,
-    toDateTime(x) AS x_t,
-    toLastDayOfWeek(x) AS w0,
-    toLastDayOfWeek(x_t) AS wt0,
-    toLastDayOfWeek(x, 3) AS w3,
-    toLastDayOfWeek(x_t, 3) AS wt3
+    dt64,
+    toLastDayOfWeek(d) AS wd_0,
+    toLastDayOfWeek(d32) AS wd32_0,
+    toLastDayOfWeek(dt) AS wdt_0,
+    toLastDayOfWeek(dt64) AS wdt64_0,
+    toLastDayOfWeek(d, 3) AS wd_3,
+    toLastDayOfWeek(d32, 3) AS wd32_3,
+    toLastDayOfWeek(dt, 3) AS wdt_3,
+    toLastDayOfWeek(dt64, 3) AS wdt64_3
 FROM numbers(10);
 
