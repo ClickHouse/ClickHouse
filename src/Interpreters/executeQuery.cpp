00f5cc8608b55ecea0af065020d7d14c619056f5
Ubuntu <ubuntu@ip-10-10-10-195.eu-central-1.compute.internal>
2023-05-25 12:04:50 +0000

    Config

--- src/Interpreters/executeQuery.cpp
+++ src/Interpreters/executeQuery.cpp
@@ -76,7 +76,7 @@
 
 #include <Parsers/Kusto/ParserKQLStatement.h>
 
-#ifdef ENABLE_PRQL
+#ifdef USE_PRQL
 #include <prql.h>
 #endif
 
@@ -382,7 +382,7 @@ static std::tuple<ASTPtr, BlockIO> executeQueryImpl(
             /// TODO: parser should fail early when max_query_size limit is reached.
             ast = parseQuery(parser, begin, end, "", max_query_size, settings.max_parser_depth);
         }
-        #ifdef ENABLE_PRQL
+        #ifdef USE_PRQL
         else if (settings.dialect == Dialect::prql && !internal)
         {
             char * sql_query = new char[max_query_size];
