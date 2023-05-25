25ec389f7da021be50ac55b9922cb2106e5b3be7
serxa <sergei@clickhouse.com>
2023-05-25 12:29:12 +0000

    fix style

--- src/Common/Priority.h
+++ src/Common/Priority.h
@@ -4,7 +4,8 @@
 
 /// Common type for priority values.
 /// Separate type (rather than `Int64` is used just to avoid implicit conversion errors and to default-initialize
-struct Priority {
+struct Priority
+{
     Int64 value = 0; /// Note that lower value means higher priority.
     constexpr operator Int64() const { return value; } /// NOLINT
 };
