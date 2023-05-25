dd78008c9ec586a213e0e541b70dfe5055f7df0e
Roman Vasin <r.vasin@arenadata.io>
2023-05-25 09:36:41 +0000

    Rename encryptConfig() into decryptConfig()

--- src/Daemon/BaseDaemon.cpp
+++ src/Daemon/BaseDaemon.cpp
@@ -663,7 +663,7 @@ void BaseDaemon::initialize(Application & self)
     umask(umask_num);
 
     DB::ConfigProcessor(config_path).savePreprocessedConfig(loaded_config, "");
-    DB::ConfigProcessor(config_path).encryptConfig(loaded_config);
+    DB::ConfigProcessor(config_path).decryptConfig(loaded_config);
 
     /// Write core dump on crash.
     {
