cb9d0f50d4c52013888051b778708c162bcdbfcc
Robert Schulze <robert@clickhouse.com>
2023-05-26 13:46:59 +0000

    Build partial key positions from entire training data

--- tests/queries/0_stateless/02734_entropy_learned_hashing.sql
+++ tests/queries/0_stateless/02734_entropy_learned_hashing.sql
@@ -1,21 +1,30 @@
+-- Tags: no-parallel
+-- no-parallel because entropy-learned hash uses global state
+
+SET allow_experimental_hash_functions = 1;
+
+-- no commonalities between keys
 DROP TABLE IF EXISTS tbl1;
 CREATE TABLE tbl1 (x String) ENGINE=Memory;
--- no commonalities between keys
 INSERT INTO tbl1 VALUES ('a'), ('b'), ('c');
-SELECT trainEntropyLearnedHash(x, 'id1') FROM tbl1;
+SELECT prepareTrainEntropyLearnedHash(x, 'id1') FROM tbl1;
+SELECT trainEntropyLearnedHash('id1') FROM tbl1;
 SELECT entropyLearnedHash(x, 'id1') FROM tbl1;
 
-SELECT trainEntropyLearnedHash(x, 1) FROM tbl1; -- { serverError ILLEGAL_TYPE_OF_ARGUMENT }
--- SELECT trainEntropyLearnedHash(x, NULL) FROM tbl1; -- { serverError ILLEGAL_TYPE_OF_ARGUMENT }
-SELECT trainEntropyLearnedHash(1, 'id1') FROM tbl1; -- { serverError ILLEGAL_TYPE_OF_ARGUMENT }
-SELECT entropyLearnedHash(x, 'non-existing id') FROM tbl1; -- { serverError BAD_ARGUMENTS }
-
+-- with commonalities between keys
 DROP TABLE IF EXISTS tbl2;
 CREATE TABLE tbl2 (x String) ENGINE=Memory;
--- with commonalities between keys
 INSERT INTO tbl2 VALUES ('aa'), ('ba'), ('ca');
-SELECT trainEntropyLearnedHash(x, 'id2') FROM tbl2;
+SELECT prepareTrainEntropyLearnedHash(x, 'id2') FROM tbl2;
+SELECT trainEntropyLearnedHash('id2') FROM tbl2;
 SELECT entropyLearnedHash(x, 'id2') FROM tbl2;
+
+-- negative tests
+SELECT prepareTrainEntropyLearnedHash(x, 1) FROM tbl1; -- { serverError ILLEGAL_TYPE_OF_ARGUMENT }
+SELECT prepareTrainEntropyLearnedHash(1, 'id1') FROM tbl1; -- { serverError ILLEGAL_TYPE_OF_ARGUMENT }
+SELECT trainEntropyLearnedHash(1) FROM tbl1; -- { serverError ILLEGAL_TYPE_OF_ARGUMENT }
+SELECT entropyLearnedHash(1, 'id1') FROM tbl1; -- { serverError ILLEGAL_TYPE_OF_ARGUMENT }
+SELECT entropyLearnedHash(x, 'non-existing id') FROM tbl1; -- { serverError BAD_ARGUMENTS }
 
 DROP TABLE tbl1;
 DROP TABLE tbl2;
