5870e59d958538c6e961e5c950d6f0745060a55c
yariks5s <bryuh_yaroslaw@knu.ua>
2023-05-27 02:38:34 +0300

    fixed style check

--- tests/integration/test_wrong_db_or_table_name/test.py
+++ tests/integration/test_wrong_db_or_table_name/test.py
@@ -8,7 +8,7 @@ node = cluster.add_instance("node")
 
 
 @pytest.fixture(scope="module")
-def start():
+def started_cluster():
     try:
         cluster.start()
         yield cluster
@@ -16,40 +16,30 @@ def start():
         cluster.shutdown()
 
 
-def test_wrong_database_name(start):
-    node.query(
-        """
+def test_wrong_database_name(started_cluster):
+    node.query("""
         CREATE TABLE test.table_test (i Int64) ENGINE=Memory;
         INSERT INTO test.table_test SELECT 1;
-        """
-    )
+        """)
 
-    with pytest.raises(
-            QueryRuntimeException,
-            match="DB::Exception: Database tes doesn't exist. Maybe you wanted to type: ['test']",
-    ):
-        node.query("SELECT * FROM tes.table_test LIMIT 1;")
+    with pytest.raises(QueryRuntimeException, match="DB::Exception: Database tes doesn't exist. Maybe you wanted to type \\['test'\\]?"):
+        node.query("SELECT * FROM tes.table_test1 LIMIT 1;")
     assert int(node.query("SELECT count() FROM test.table_test;")) == 1
     node.query("DROP TABLE test.table_test;")
 
 
 def test_wrong_table_name(started_cluster):
-    node.query(
-        """
+    node.query("""
         CREATE TABLE test.table_test (i Int64) ENGINE=Memory;
-        CREATE TABLE test.table_test2 (i Int64) ENGINE=Memory;
+        CREATE TABLE test.table_test1 (i Int64) ENGINE=Memory;
         INSERT INTO test.table_test SELECT 1;
-        """
-    )
-    with pytest.raises(
-            QueryRuntimeException,
-            match="DB::Exception: Table table_test1 doesn't exist. Maybe you wanted to type: ['table_test2']",
-    ):
-        node.query("SELECT * FROM test.table_test1 LIMIT 1;")
+        """)
+    with pytest.raises(QueryRuntimeException, match="DB::Exception: Table test.table_test2 doesn't exist. Maybe you wanted to type \\['test.table_test1'\\]?"):
+        node.query("""
+            SELECT * FROM test.table_test2 LIMIT 1;
+            """)
     assert int(node.query("SELECT count() FROM test.table_test;")) == 1
-    node.query(
-        """
-        DROP TABLE test.table_test;
-        DROP TABLE test.table_test2;
-        """
-    )
+    node.query("""
+            DROP TABLE test.table_test;
+            DROP TABLE test.table_test1;
+            """)
