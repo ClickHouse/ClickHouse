72db6e9471e99eee3d5326512a4913585a103d91
Lucas Chang <weiweichang@tubi.tv>
2023-05-27 17:59:41 +0800

    fix date32

--- tests/queries/1_stateful/00178_query_datetime64_index.sql
+++ tests/queries/1_stateful/00178_query_datetime64_index.sql
@@ -4,5 +4,6 @@ CREATE TABLE datetime64_index_tbl(ts DateTime64(3, 'UTC')) ENGINE=MergeTree ORDE
 INSERT INTO datetime64_index_tbl(ts) VALUES(toDateTime64('2023-05-27 00:00:00', 3, 'UTC'));
 
 SELECT ts from datetime64_index_tbl where ts < toDate('2023-05-28');
+SELECT ts from datetime64_index_tbl where ts < toDate32('2023-05-28');
 
 DROP TABLE datetime64_index_tbl;
