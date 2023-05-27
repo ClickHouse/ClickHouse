b91b06b7714c26a06d8590a32979da7e0539cd48
Eridanus <maodingxuan@foxmail.com>
2023-05-27 21:47:58 +0800

    add tests

---
+++ tests/queries/0_stateless/02769_array_pack_bits.sql
@@ -0,0 +1,18 @@
+SELECT arrayPackBitsToUInt64(x -> x, [1, 0, 0, 0, 0, 0]);
+SELECT arrayPackBitsToUInt64(x -> x, [1, 0, 0, 0, 0, 1]);
+SELECT arrayPackBitsToUInt64(x -> x, [1, 0, 0, 0, 0, 0, 0, 0, 0]);
+SELECT arrayPackBitsToUInt64(x -> x, [1, 0, 0, 0, 0, 0, 0, 0, 0, 0]);
+SELECT arrayPackBitsToString(x -> x, [0, 0, 1, 1, 0, 0, 0, 0]);
+SELECT arrayPackBitsToString(x -> x, [0, 0, 1, 1, 0, 0, 0, 0,0, 0, 1, 1, 0, 0, 0, 0]);
+SELECT arrayPackBitsToFixedString(x -> x, 1, [0, 0, 1, 1, 0, 0, 0, 0]);
+SELECT arrayPackBitsToFixedString(x -> x, 2, [0, 0, 1, 1, 0, 0, 0, 0, 0, 0, 1, 1, 0, 0, 0, 0]);
+SELECT arrayPackBitsToFixedString(x -> x, 1, [0, 0, 1, 1, 0, 0, 0, 0, 0, 0, 1, 1, 0, 0, 0, 0]);
+SELECT arrayPackBitGroupsToUInt64(x -> x, 2, [1, 1]);
+SELECT arrayPackBitGroupsToUInt64(x -> x, 1, [1, 1]);
+SELECT arrayPackBitGroupsToString(x -> x, 6, [1, 1, 0, 0, 0, 1, 1, 1, 0, 0, 0, 0]);
+SELECT toString(arrayPackBitGroupsToFixedString(x -> x, 2, 6, [1, 1, 0, 0, 0, 1, 1, 1, 0, 0, 0, 0]));
+SELECT toString(arrayPackBitsToFixedString(x -> x, 1, [0, 0, 1, 1, 0, 0, 0, 0]));
+SELECT toString(arrayPackBitGroupsToFixedString(x -> x, 1, 8, [0, 0, 1, 1, 0, 0, 0, 0]));
+SELECT toString(arrayPackBitsToFixedString(x -> x, 1, [0, 0, 1, 1, 0, 0, 0, 0]));
+SELECT toString(arrayPackBitGroupsToFixedString(x -> x, 2 , 6,  [1,1,0,0,0,1,1,1,0,0,0,0]));
+SELECT toString(arrayPackBitGroupsToFixedString(x -> x, 1 , 6,  [1,1,0,0,0,0,1,1,0,0,0,0]));
