5a5ef7fc6a4f8896a54a03e9322e453a7baf235d
yariks5s <bryuh_yaroslaw@knu.ua>
2023-05-27 03:31:05 +0300

    fixed check black

--- tests/integration/test_wrong_db_or_table_name/test.py
+++ tests/integration/test_wrong_db_or_table_name/test.py
@@ -17,29 +17,45 @@ def started_cluster():
 
 
 def test_wrong_database_name(started_cluster):
-    node.query("""
+    node.query(
+        """
         CREATE TABLE test.table_test (i Int64) ENGINE=Memory;
         INSERT INTO test.table_test SELECT 1;
-        """)
+        """
+    )
 
-    with pytest.raises(QueryRuntimeException, match="DB::Exception: Database tes doesn't exist. Maybe you wanted to type \\['test'\\]?"):
+    with pytest.raises(
+            QueryRuntimeException,
+            match="DB::Exception: Database tes doesn't exist. Maybe you wanted to "
+                  "type \\['test'\\]?",
+    ):
         node.query("SELECT * FROM tes.table_test1 LIMIT 1;")
     assert int(node.query("SELECT count() FROM test.table_test;")) == 1
     node.query("DROP TABLE test.table_test;")
 
 
 def test_wrong_table_name(started_cluster):
-    node.query("""
+    node.query(
+        """
         CREATE TABLE test.table_test (i Int64) ENGINE=Memory;
         CREATE TABLE test.table_test1 (i Int64) ENGINE=Memory;
         INSERT INTO test.table_test SELECT 1;
-        """)
-    with pytest.raises(QueryRuntimeException, match="DB::Exception: Table test.table_test2 doesn't exist. Maybe you wanted to type \\['test.table_test1'\\]?"):
-        node.query("""
+        """
+    )
+    with pytest.raises(
+            QueryRuntimeException,
+            match="DB::Exception: Table test.table_test2 doesn't exist. Maybe you "
+                  "wanted to type \\['test.table_test1'\\]?",
+    ):
+        node.query(
+            """
             SELECT * FROM test.table_test2 LIMIT 1;
-            """)
+            """
+        )
     assert int(node.query("SELECT count() FROM test.table_test;")) == 1
-    node.query("""
+    node.query(
+        """
             DROP TABLE test.table_test;
             DROP TABLE test.table_test1;
-            """)
+            """
+    )
