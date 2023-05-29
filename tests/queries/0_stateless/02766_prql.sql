b065621cf015ec047a67e4a5bab51839ac0dfcc0
Ubuntu <ubuntu@ip-10-10-10-195.eu-central-1.compute.internal>
2023-05-29 04:33:58 +0000

    Edited test

--- tests/queries/0_stateless/02766_prql.sql
+++ tests/queries/0_stateless/02766_prql.sql
@@ -11,4 +11,4 @@ ORDER BY user_id;
 INSERT INTO aboba (user_id, message, timestamp, metric) VALUES (101, 'Hello, ClickHouse!', now(), -1.0), (102, 'Insert a lot of rows per batch', yesterday(), 1.41421 ), (102, 'Sort your data based on your commonly-used queries', today(), 2.718), (101, 'Granules are the smallest chunks of data read', now() + 5, 3.14159);
 SET dialect = 'prql';
 from aboba
-select user_id
+select [ user_id, message ]
