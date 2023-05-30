23ff1fc47fc279d9c6ab97eb55d1887fc68205bd
Umang Brahmbhatt <umangbrahmbhatt@Umangs-MacBook-Pro.local>
2023-05-30 15:24:13 -0400

    updated docker_manifest_merge

--- tests/ci/docker_manifests_merge.py
+++ tests/ci/docker_manifests_merge.py
@@ -11,7 +11,7 @@ from github import Github
 
 from clickhouse_helper import ClickHouseHelper, prepare_tests_results_for_clickhouse
 from commit_status_helper import format_description, get_commit, post_commit_status
-from env_helper import RUNNER_TEMP
+from env_helper import RUNNER_TEMP, DOCKER_USER, DOCKER_REPO
 from get_robot_token import get_best_robot_token, get_parameter_from_ssm
 from pr_info import PRInfo
 from report import TestResults, TestResult
@@ -174,11 +174,11 @@ def main():
     args = parse_args()
     if args.push:
         subprocess.check_output(  # pylint: disable=unexpected-keyword-arg
-            "docker login --username 'robotclickhouse' --password-stdin",
-            input=get_parameter_from_ssm("dockerhub_robot_password"),
-            encoding="utf-8",
-            shell=True,
-        )
+        "docker login {} --username '{}' --password-stdin".format(DOCKER_REPO, DOCKER_USER),
+        input=get_parameter_from_ssm("dockerhub_robot_password"),
+        encoding="utf-8",
+        shell=True,
+    )
 
     to_merge = {}
     for suf in args.suffixes:
