6e63276e53a13f50d1a46ca64650d20c151238a0
Nikita Mikhaylov <mikhaylovnikitka@gmail.com>
2023-05-30 14:15:08 +0200

    Update 02769_parallel_replicas_unavailable_shards.sh

--- tests/queries/0_stateless/02769_parallel_replicas_unavailable_shards.sh
+++ tests/queries/0_stateless/02769_parallel_replicas_unavailable_shards.sh
@@ -9,6 +9,6 @@ ${CLICKHOUSE_CLIENT} --query="DROP TABLE IF EXISTS test_parallel_replicas_unavai
 ${CLICKHOUSE_CLIENT} --query="CREATE TABLE test_parallel_replicas_unavailable_shards (n UInt64) ENGINE=MergeTree() ORDER BY tuple()"
 ${CLICKHOUSE_CLIENT} --query="INSERT INTO test_parallel_replicas_unavailable_shards SELECT * FROM numbers(10)"
 
-${CLICKHOUSE_CLIENT} --send_logs_level 'debug' --skip_unavailable_shards 1 --allow_experimental_parallel_reading_from_replicas 1 --max_parallel_replicas 11  --use_hedged_requests 0 --cluster_for_parallel_replicas 'parallel_replicas' --parallel_replicas_for_non_replicated_merge_tree 1 -q "SELECT count() FROM test_parallel_replicas_unavailable_shards WHERE NOT ignore(*)" 2>&1 | grep -Fac 'Replica number 10 is unavailable'
+${CLICKHOUSE_CLIENT} --send_logs_level 'debug' --skip_unavailable_shards 1 --allow_experimental_parallel_reading_from_replicas 1 --max_parallel_replicas 11  --use_hedged_requests 0 --cluster_for_parallel_replicas 'parallel_replicas' --parallel_replicas_for_non_replicated_merge_tree 1 -q "SELECT count() FROM test_parallel_replicas_unavailable_shards WHERE NOT ignore(*)" 2>&1 
 
 ${CLICKHOUSE_CLIENT} --query="DROP TABLE test_parallel_replicas_unavailable_shards"
