9e73d0b483a28e0de4e8d457251e5a3bae2992e3
Umang Brahmbhatt <umangbrahmbhatt@Umangs-MacBook-Pro.local>
2023-05-30 13:31:53 -0400

    testing for secret value

--- tests/ci/get_robot_token.py
+++ tests/ci/get_robot_token.py
@@ -39,55 +39,50 @@ def get_best_robot_token(token_prefix_env_name="github_robot_token_"):
         return ROBOT_TOKEN.value
     
     client = None
-    if VAULT_URL:
+    values = []
 
+    if VAULT_URL:
         client = hvac.Client(url=VAULT_URL,token=VAULT_TOKEN)
-        # path=f"{VAULT_MOUNT_POINT}/{VAULT_PATH}"
         parameters = client.secrets.kv.v2.read_secret_version(mount_point=VAULT_MOUNT_POINT,path=VAULT_PATH)["data"]["data"]
         parameters = {key: value for key, value in parameters.items() if key.startswith(token_prefix_env_name)}
-        # assert secrets
-        # print(secrets)
-
-        # parameters = [secret for secret in secrets if secret.startswith(token_prefix_env_name)]
         assert parameters
         print(parameters)
-    # else:
-    #     client = boto3.client("ssm", region_name="us-east-1")
-    #     parameters = client.describe_parameters(
-    #         ParameterFilters=[
-    #             {"Key": "Name", "Option": "BeginsWith", "Values": [token_prefix_env_name]}
-    #         ]
-    #     )["Parameters"]
-    #     assert parameters
+        values = list(parameters.values())
+        print(values)
+        # for value in parameters.values():
+        #     values.append(value)
+        #     print(value)
+    else:
+        client = boto3.client("ssm", region_name="us-east-1")
+        parameters = client.describe_parameters(
+            ParameterFilters=[
+                {"Key": "Name", "Option": "BeginsWith", "Values": [token_prefix_env_name]}
+            ]
+        )["Parameters"]
+        assert parameters
+        for token_name in [p["Name"] for p in parameters]:
+            value = get_parameter_from_ssm(token_name, True, client)
+            values.append(value)
 
-    # for token_name in [p["Name"] for p in parameters]:
-    for token_name in parameters:
-        value = get_parameter_from_ssm(parameters[token_name], True, client)
-        # value = parameters[token_name]["value"]
-        print(value)
+    for value in values:
+        gh = Github(value, per_page=100)
+        # Do not spend additional request to API by accessing user.login unless
+        # the token is chosen by the remaining requests number
+        user = gh.get_user()
+        rest, _ = gh.rate_limiting
+        logging.info("Get token with %s remaining requests", rest)
+        if ROBOT_TOKEN is None:
+            ROBOT_TOKEN = Token(user, value, rest)
+            continue
+        if ROBOT_TOKEN.rest < rest:
+            ROBOT_TOKEN.user, ROBOT_TOKEN.value, ROBOT_TOKEN.rest = user, value, rest
 
-    
-    # for value in parameters.values():
-    #     print(value)
+    assert ROBOT_TOKEN
+    logging.info(
+        "User %s with %s remaining requests is used",
+        ROBOT_TOKEN.user.login,
+        ROBOT_TOKEN.rest,
+    )
 
+    return ROBOT_TOKEN.value
 
-    #     gh = Github(value, per_page=100)
-    #     # Do not spend additional request to API by accessin user.login unless
-    #     # the token is chosen by the remaining requests number
-    #     user = gh.get_user()
-    #     rest, _ = gh.rate_limiting
-    #     logging.info("Get token with %s remaining requests", rest)
-    #     if ROBOT_TOKEN is None:
-    #         ROBOT_TOKEN = Token(user, value, rest)
-    #         continue
-    #     if ROBOT_TOKEN.rest < rest:
-    #         ROBOT_TOKEN.user, ROBOT_TOKEN.value, ROBOT_TOKEN.rest = user, value, rest
-
-    # assert ROBOT_TOKEN
-    # logging.info(
-    #     "User %s with %s remaining requests is used",
-    #     ROBOT_TOKEN.user.login,
-    #     ROBOT_TOKEN.rest,
-    # )
-
-    # return ROBOT_TOKEN.value
