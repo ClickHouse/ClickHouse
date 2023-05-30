6cac963193d54ccd42550a1b279017aae8f215d1
Umang Brahmbhatt <umangbrahmbhatt@Umangs-MacBook-Pro.local>
2023-05-30 11:35:34 -0400

    testing for secret value

--- tests/ci/get_robot_token.py
+++ tests/ci/get_robot_token.py
@@ -60,7 +60,8 @@ def get_best_robot_token(token_prefix_env_name="github_robot_token_"):
     #     )["Parameters"]
     #     assert parameters
 
-    for token_name in [p["Name"] for p in parameters]:
+    # for token_name in [p["Name"] for p in parameters]:
+    for token_name in parameters:
         # value = get_parameter_from_ssm(token_name, True, client)
         value = parameters[token_name]["value"]
         print(value)
