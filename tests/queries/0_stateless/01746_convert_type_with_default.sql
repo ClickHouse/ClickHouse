eb6bba297101f2a7e95fbf6de8939d8ada1818db
Dmitry Kardymon <d.kardymon@arenadata.io>
2023-05-30 14:46:44 +0000

    Added more tests for toDateOrDefault/toDateTimeOrDefault

--- tests/queries/0_stateless/01746_convert_type_with_default.sql
+++ tests/queries/0_stateless/01746_convert_type_with_default.sql
@@ -26,15 +26,19 @@ select toUInt256OrDefault('1xx', cast(2 as UInt256));
 select toInt256OrDefault('-1', cast(-2 as Int256));
 select toInt256OrDefault('-1xx', cast(-2 as Int256));
 
+select toDateOrDefault('2020-0x-02');
 select toDateOrDefault('2023-05-30');
+select toDateOrDefault('2023-05-30', '2000-01-01'::Date);
+select toDateOrDefault('2020-0x-02', '2023-05-30'::Date);
+select toDateOrDefault(-1);
 select toDateOrDefault(19507);
+select toDateOrDefault(19507, '2000-01-01'::Date);
+select toDateOrDefault(-1, '2000-01-01'::Date);
 
 select toDateTimeOrDefault('2023-05-30 14:38:20');
+select toDateTimeOrDefault('2023-05-30 14:38:20', 'UTC');
+select toDateTimeOrDefault('s2023', 'UTC', '2023-05-30 14:38:20'::DateTime('UTC'));
 select toDateTimeOrDefault(1685457500);
 
 SELECT toUUIDOrDefault('61f0c404-5cb3-11e7-907b-a6006ad3dba0', cast('59f0c404-5cb3-11e7-907b-a6006ad3dba0' as UUID));
 SELECT toUUIDOrDefault('-----61f0c404-5cb3-11e7-907b-a6006ad3dba0', cast('59f0c404-5cb3-11e7-907b-a6006ad3dba0' as UUID));
-
-
-
-
