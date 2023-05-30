e285ac406db357d03007381e8a155b60bb743c25
Victor Krasnov <v.krasnov@arenadata.io>
2023-05-30 13:16:25 +0000

    Integrate test 02763_last_day_of_week into existing test 00941_to_custom_week

--- tests/queries/0_stateless/00941_to_custom_week.sql
+++ tests/queries/0_stateless/00941_to_custom_week.sql
@@ -50,3 +50,21 @@ SELECT
     toStartOfWeek(x_t, 3) AS wt3
 FROM numbers(10);
 
+-- toLastDayOfWeek
+WITH
+    toDate('2018-12-25') + number AS d,
+    toDate32(d) AS d32,
+    toDateTime(d) AS dt,
+    toDateTime64(d, 0) AS dt64
+SELECT
+    dt64,
+    toLastDayOfWeek(d) AS wd_sun,
+    toLastDayOfWeek(d32) AS wd32_sun,
+    toLastDayOfWeek(dt) AS wdt_sun,
+    toLastDayOfWeek(dt64) AS wdt64_sun,
+    toLastDayOfWeek(d, 1) AS wd_mon,
+    toLastDayOfWeek(d32, 1) AS wd32_mon,
+    toLastDayOfWeek(dt, 1) AS wdt_mon,
+    toLastDayOfWeek(dt64, 1) AS wdt64_mon
+FROM numbers(10);
+
