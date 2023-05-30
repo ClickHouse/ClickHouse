fdb8a7b13fc2285aad4cf107289d98c774771c69
yariks5s <bryuh_yaroslaw@knu.ua>
2023-05-30 16:24:53 +0300

    added tests

--- tests/integration/test_wrong_db_or_table_name/test.py
+++ tests/integration/test_wrong_db_or_table_name/test.py
@@ -6,19 +6,18 @@ from helpers.test_tools import TSV
 cluster = ClickHouseCluster(__file__)
 node = cluster.add_instance("node")
 
-
 @pytest.fixture(scope="module")
-def started_cluster():
+def start():
     try:
         cluster.start()
         yield cluster
     finally:
         cluster.shutdown()
 
-
-def test_wrong_database_name(started_cluster):
+def test_wrong_database_name(start):
     node.query(
         """
+        CREATE DATABASE test;
         CREATE TABLE test.table_test (i Int64) ENGINE=Memory;
         INSERT INTO test.table_test SELECT 1;
         """
@@ -26,36 +25,76 @@ def test_wrong_database_name(started_cluster):
 
     with pytest.raises(
         QueryRuntimeException,
-        match="DB::Exception: Database tes doesn't exist. Maybe you wanted to "
-        "type \\['test'\\]?",
+        match="DB::Exception: Database tes doesn't exist. Maybe you wanted to type test?.",
     ):
-        node.query("SELECT * FROM tes.table_test1 LIMIT 1;")
+        node.query("SELECT * FROM tes.table_test LIMIT 1;")
     assert int(node.query("SELECT count() FROM test.table_test;")) == 1
-    node.query("DROP TABLE test.table_test;")
+    node.query(
+        """
+        DROP TABLE test.table_test;
+        DROP DATABASE test;
+        """
+    )
 
+def test_drop_wrong_database_name(start):
+    node.query(
+        """
+        CREATE DATABASE test;
+        """
+    )
+
+    with pytest.raises(
+        QueryRuntimeException,
+        match="DB::Exception: Database tes doesn't exist. Maybe you wanted to type test?.",
+    ):
+        node.query("DROP DATABASE tes;")
+    assert int(node.query("SELECT count() FROM test.table_test;")) == 1
+    node.query("DROP DATABASE test;")
 
 def test_wrong_table_name(started_cluster):
     node.query(
         """
+        CREATE DATABASE test;
         CREATE TABLE test.table_test (i Int64) ENGINE=Memory;
-        CREATE TABLE test.table_test1 (i Int64) ENGINE=Memory;
+        CREATE TABLE test.table_test2 (i Int64) ENGINE=Memory;
         INSERT INTO test.table_test SELECT 1;
         """
     )
     with pytest.raises(
         QueryRuntimeException,
-        match="DB::Exception: Table test.table_test2 doesn't exist. Maybe you "
-        "wanted to type \\['test.table_test1'\\]?",
+        match="DB::Exception: Table test.table_test1 doesn't exist. Maybe you wanted to type table_test?.",
     ):
         node.query(
             """
-            SELECT * FROM test.table_test2 LIMIT 1;
+            SELECT * FROM test.table_test1 LIMIT 1;
             """
         )
     assert int(node.query("SELECT count() FROM test.table_test;")) == 1
     node.query(
         """
             DROP TABLE test.table_test;
-            DROP TABLE test.table_test1;
+            DROP TABLE test.table_test2;
+            DROP DATABASE test;
             """
+    )
+
+def test_drop_wrong_table_name(start):
+    node.query(
+        """
+        CREATE DATABASE test;
+        CREATE TABLE test.table_test (i Int64) ENGINE=Memory;
+        """
+    )
+
+    with pytest.raises(
+        QueryRuntimeException,
+        match="DB::Exception: Table test.table_tes doesn't exist. Maybe you wanted to type table_test?.",
+    ):
+        node.query("DROP TABLE test.table_tes;")
+    assert int(node.query("SELECT count() FROM test.table_test;")) == 1
+    node.query(
+        """
+        DROP TABLE test.table_test;
+        DROP DATABASE test;
+        """
     )
