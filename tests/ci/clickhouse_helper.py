24984c1b3c398d42efa9d8a4c87c3c26ca1131eb
Umang Brahmbhatt <umangbrahmbhatt@Umangs-MacBook-Pro.local>
2023-05-30 23:06:11 -0400

    testing for generalization

--- tests/ci/clickhouse_helper.py
+++ tests/ci/clickhouse_helper.py
@@ -6,7 +6,7 @@ import time
 
 import requests  # type: ignore
 
-from get_robot_token import get_parameter_from_ssm
+from get_robot_token import get_parameter_from_ssm, VAULT_URL
 from pr_info import PRInfo
 from report import TestResults
 
@@ -141,11 +141,12 @@ def prepare_tests_results_for_clickhouse(
     report_url: str,
     check_name: str,
 ) -> List[dict]:
-    pull_request_url = "https://github.com/ClibMouse/ClickHouse/commits/master"
-    # if VAULT_URL:
-    #     pull_request_url = "https://github.com/ClibMouse/ClickHouse/commits/master"
-    # else:
-    #     pull_request_url = "https://github.com/ClickHouse/ClickHouse/commits/master"
+
+    # pull_request_url = "https://github.com/ClibMouse/ClickHouse/commits/master"
+    if VAULT_URL:
+        pull_request_url = "https://github.com/ClibMouse/ClickHouse/commits/master"
+    else:
+        pull_request_url = "https://github.com/ClickHouse/ClickHouse/commits/master"
 
     base_ref = "master"
     head_ref = "master"
