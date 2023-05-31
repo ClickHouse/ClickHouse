10cfcefec41c1e195b74d6b86f3fc2aeb290febc
Victor Krasnov <v.krasnov@arenadata.io>
2023-05-31 07:19:12 +0000

    Align toStartOfWeek() and toLastDayOfWeek() test cases in 01921_datatype_date32.sql test

--- tests/queries/0_stateless/00941_to_custom_week.sql
+++ tests/queries/0_stateless/00941_to_custom_week.sql
@@ -41,13 +41,21 @@ SELECT toDateTime(toDate('2016-12-22') + number, 'Asia/Istanbul' ) AS d,
 FROM numbers(21);
 
 -- toStartOfWeek
+WITH
+    toDate('2018-12-25') + number AS d,
+    toDate32(d) AS d32,
+    toDateTime(d) AS dt,
+    toDateTime64(d, 0) AS dt64
 SELECT
-    toDate('2018-12-25') + number AS x,
-    toDateTime(x) AS x_t,
-    toStartOfWeek(x) AS w0,
-    toStartOfWeek(x_t) AS wt0,
-    toStartOfWeek(x, 3) AS w3,
-    toStartOfWeek(x_t, 3) AS wt3
+    dt64,
+    toStartOfWeek(d) AS wd_sun,
+    toStartOfWeek(d32) AS wd32_sun,
+    toStartOfWeek(dt) AS wdt_sun,
+    toStartOfWeek(dt64) AS wdt64_sun,
+    toStartOfWeek(d, 1) AS wd_mon,
+    toStartOfWeek(d32, 1) AS wd32_mon,
+    toStartOfWeek(dt, 1) AS wdt_mon,
+    toStartOfWeek(dt64, 1) AS wdt64_mon
 FROM numbers(10);
 
 -- toLastDayOfWeek
