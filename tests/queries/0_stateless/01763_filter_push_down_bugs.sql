1a3c94293350426e5714116818a55ea39a0867b5
Han Fei <hanfei19910905@gmail.com>
2023-06-01 14:53:07 +0200

    make filter push down through cross join

--- tests/queries/0_stateless/01763_filter_push_down_bugs.sql
+++ tests/queries/0_stateless/01763_filter_push_down_bugs.sql
@@ -38,6 +38,22 @@ DROP TABLE IF EXISTS Test;
 
 select x, y from (select [0, 1, 2] as y, 1 as a, 2 as b) array join y as x where a = 1 and b = 2 and (x = 1 or x != 1) and x = 1;
 
+DROP TABLE IF EXISTS t;
 create table t(a UInt8) engine=MergeTree order by a;
 insert into t select * from numbers(2);
 select a from t t1 join t t2 on t1.a = t2.a where t1.a;
+DROP TABLE IF EXISTS t;
+
+DROP TABLE IF EXISTS t1;
+DROP TABLE IF EXISTS t2;
+CREATE TABLE t1 (id Int64, create_time DateTime) ENGINE = MergeTree ORDER BY id  AS SELECT number, toDateTime(number + 1600000000) from numbers(10000000) settings min_insert_block_size_rows=100000;
+CREATE TABLE t2 (delete_time DateTime) ENGINE = MergeTree ORDER BY delete_time AS SELECT toDateTime(number + 1610000000) from numbers(100);
+
+EXPLAIN indexes=1 SELECT id, delete_time FROM t1
+ CROSS JOIN (
+    SELECT delete_time
+    FROM t2 WHERE delete_time > '2023-05-30 00:00:00'
+) AS d WHERE create_time < delete_time AND id IN (10000001);
+
+DROP TABLE IF EXISTS t1;
+DROP TABLE IF EXISTS t2;
