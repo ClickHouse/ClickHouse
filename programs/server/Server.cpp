aceca2e2106d4e1f08e3d5164ec4b1eb5cc22d56
serxa <sergei@clickhouse.com>
2023-06-02 16:24:56 +0000

    fix server

--- programs/server/Server.cpp
+++ programs/server/Server.cpp
@@ -20,6 +20,7 @@
 #include <base/coverage.h>
 #include <base/getFQDNOrHostName.h>
 #include <base/safeExit.h>
+#include <Common/AsyncLoaderPoolId.h>
 #include <Common/MemoryTracker.h>
 #include <Common/ClickHouseRevision.h>
 #include <Common/DNSResolver.h>
@@ -1555,7 +1556,7 @@ try
         auto load_metadata = loadMetadata(global_context, default_database, server_settings.async_load_databases);
         /// If we need to convert database engines, disable async tables loading
         convertDatabasesEnginesIfNeed(load_metadata, global_context);
-        scheduleAndWaitLoad(system_startup_tasks);
+        scheduleAndWaitLoadAllIn(AsyncLoaderPoolId::Foreground, system_startup_tasks);
         database_catalog.startupBackgroundCleanup();
         /// After loading validate that default database exists
         database_catalog.assertDatabaseExists(default_database);
