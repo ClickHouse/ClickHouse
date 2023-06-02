96fe4b5107611a627b7981fdac2afe9304660e48
János Benjamin Antal <benjamin.antal@clickhouse.com>
2023-06-02 16:57:46 +0000

    Add tests

--- tests/queries/0_stateless/02771_system_user_processes.sh
+++ tests/queries/0_stateless/02771_system_user_processes.sh
@@ -4,12 +4,15 @@ CUR_DIR=$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)
 # shellcheck source=../shell_config.sh
 . "$CUR_DIR"/../shell_config.sh
 
-$CLICKHOUSE_CLIENT -q "DROP USER IF EXISTS test_user_02771"
-$CLICKHOUSE_CLIENT -q "CREATE USER test_user_02771"
-$CLICKHOUSE_CLIENT -u test_user_02771 -q "SELECT * FROM system.numbers LIMIT 1"
-$CLICKHOUSE_CLIENT -u test_user_02771 -q "SELECT * FROM system.numbers LIMIT 1"
-$CLICKHOUSE_CLIENT -q "SELECT user FROM system.user_processes"
+USER_POSTFIX=`random_str 10`
+USER="test_user_02771_$USER_POSTFIX"
+
+$CLICKHOUSE_CLIENT -q "SHOW USER PROCESSES" &>"${CLICKHOUSE_TMP}/test_output" && echo "SHOW USER PROCESSES query succeeded!" || cat "${CLICKHOUSE_TMP}/test_output" 
+$CLICKHOUSE_CLIENT -q "DROP USER IF EXISTS $USER"
+$CLICKHOUSE_CLIENT -q "CREATE USER $USER"
+$CLICKHOUSE_CLIENT -u "$USER" -q "SELECT * FROM system.numbers LIMIT 1"
+$CLICKHOUSE_CLIENT -u "$USER" -q "SELECT * FROM system.numbers LIMIT 1"
 $CLICKHOUSE_CLIENT -q "SELECT user, toBool(ProfileEvents['SelectQuery'] > 0), toBool(ProfileEvents['Query'] > 0) FROM system.user_processes WHERE user='default'"
-$CLICKHOUSE_CLIENT -q "SELECT user, ProfileEvents['SelectQuery'], ProfileEvents['Query'] FROM system.user_processes WHERE user='test_user_02771'"
-$CLICKHOUSE_CLIENT -q "DROP USER test_user_02771"
+$CLICKHOUSE_CLIENT -q "SELECT ProfileEvents['SelectQuery'], ProfileEvents['Query'] FROM system.user_processes WHERE user='$USER'"
+$CLICKHOUSE_CLIENT -q "DROP USER $USER"
 
