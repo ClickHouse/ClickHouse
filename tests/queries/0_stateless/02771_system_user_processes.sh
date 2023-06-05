4225cab2e8203b45e17188da28c3f6a1a330878c
János Benjamin Antal <benjamin.antal@clickhouse.com>
2023-06-05 08:34:25 +0000

    Rewrite bugprone shell script command

--- tests/queries/0_stateless/02771_system_user_processes.sh
+++ tests/queries/0_stateless/02771_system_user_processes.sh
@@ -7,7 +7,13 @@ CUR_DIR=$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)
 USER_POSTFIX=`random_str 10`
 USER="test_user_02771_$USER_POSTFIX"
 
-$CLICKHOUSE_CLIENT -q "SHOW USER PROCESSES" &>"${CLICKHOUSE_TMP}/test_output" && echo "SHOW USER PROCESSES query succeeded!" || cat "${CLICKHOUSE_TMP}/test_output" 
+if $CLICKHOUSE_CLIENT -q "SHOW USER PROCESSES" &>"${CLICKHOUSE_TMP}/test_output"
+then
+    echo "SHOW USER PROCESSES query succeeded!"
+else
+    cat "${CLICKHOUSE_TMP}/test_output"
+fi
+
 $CLICKHOUSE_CLIENT -q "DROP USER IF EXISTS $USER"
 $CLICKHOUSE_CLIENT -q "CREATE USER $USER"
 $CLICKHOUSE_CLIENT -u "$USER" -q "SELECT * FROM system.numbers LIMIT 1"
