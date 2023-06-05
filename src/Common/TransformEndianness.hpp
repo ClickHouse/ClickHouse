9b244e4148cb270a68efdb5d0313b55bd1b0b893
ltrk2 <107155950+ltrk2@users.noreply.github.com>
2023-06-05 06:55:34 -0700

    Implement review comments

--- src/Common/TransformEndianness.hpp
+++ src/Common/TransformEndianness.hpp
@@ -8,18 +8,35 @@
 namespace DB
 {
 template <std::endian endian, typename T>
-    requires is_big_int_v<T>
+requires std::is_integral_v<T>
+inline void transformEndianness(T & value)
+{
+    if constexpr (endian != std::endian::native)
+        value = std::byteswap(value);
+}
+
+template <std::endian endian, typename T>
+requires is_big_int_v<T>
 inline void transformEndianness(T & x)
 {
     if constexpr (std::endian::native != endian)
     {
-        std::ranges::transform(x.items, std::begin(x.items), [](auto& item) { return std::byteswap(item); });
-        std::ranges::reverse(x.items);
+        auto & items = x.items;
+        std::transform(std::begin(items), std::end(items), std::begin(items), [](auto & item) { return std::byteswap(item); });
+        std::reverse(std::begin(items), std::end(items));
     }
 }
 
 template <std::endian endian, typename T>
-    requires is_decimal<T> || std::is_floating_point_v<T>
+requires is_decimal<T>
+inline void transformEndianness(T & x)
+{
+    if constexpr (std::endian::native != endian)
+        transformEndianness<endian>(x.value);
+}
+
+template <std::endian endian, typename T>
+requires std::is_floating_point_v<T>
 inline void transformEndianness(T & value)
 {
     if constexpr (std::endian::native != endian)
@@ -30,15 +47,7 @@ inline void transformEndianness(T & value)
 }
 
 template <std::endian endian, typename T>
-    requires std::is_integral_v<T> && (sizeof(T) <= 8)
-inline void transformEndianness(T & value)
-{
-    if constexpr (endian != std::endian::native)
-        value = std::byteswap(value);
-}
-
-template <std::endian endian, typename T>
-    requires std::is_scoped_enum_v<T>
+requires std::is_scoped_enum_v<T>
 inline void transformEndianness(T & x)
 {
     using UnderlyingType = std::underlying_type_t<T>;
