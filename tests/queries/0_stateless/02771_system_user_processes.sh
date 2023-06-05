4a00daaadb172854b7b1664f1bc687e232c0a850
János Benjamin Antal <benjamin.antal@clickhouse.com>
2023-06-05 12:29:11 +0000

    Remove the usage of `SHOW USER PROCESSES` from tests

--- tests/queries/0_stateless/02771_system_user_processes.sh
+++ tests/queries/0_stateless/02771_system_user_processes.sh
@@ -7,13 +7,6 @@ CUR_DIR=$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)
 USER_POSTFIX=`random_str 10`
 USER="test_user_02771_$USER_POSTFIX"
 
-if $CLICKHOUSE_CLIENT -q "SHOW USER PROCESSES" &>"${CLICKHOUSE_TMP}/test_output"
-then
-    echo "SHOW USER PROCESSES query succeeded!"
-else
-    cat "${CLICKHOUSE_TMP}/test_output"
-fi
-
 $CLICKHOUSE_CLIENT -q "DROP USER IF EXISTS $USER"
 $CLICKHOUSE_CLIENT -q "CREATE USER $USER"
 $CLICKHOUSE_CLIENT -q "GRANT SELECT ON system.* TO $USER"
