41f843f783c98bdddce617914942e75dbf9cefc2
alesapin <alesapin@gmail.com>
2023-06-06 21:06:45 +0200

    Fix the most important check in the world

--- src/TableFunctions/TableFunctionAzureBlobStorage.cpp
+++ src/TableFunctions/TableFunctionAzureBlobStorage.cpp
@@ -33,7 +33,7 @@ namespace DB
 namespace ErrorCodes
 {
     extern const int NUMBER_OF_ARGUMENTS_DOESNT_MATCH;
-    extern const int LOGICAL_ERROR;
+    extern const int BAD_ARGUMENTS;
 }
 
 namespace
