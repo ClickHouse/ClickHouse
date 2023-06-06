d316add2f1f6ffa9cf6f2a1107a4d7d69960c72a
Roman Vasin <r.vasin@arenadata.io>
2023-06-06 15:24:29 +0000

    Add integration test test_config_decryption

---
+++ tests/integration/test_config_decryption/test.py
@@ -0,0 +1,31 @@
+import pytest
+import os
+from helpers.cluster import ClickHouseCluster
+
+cluster = ClickHouseCluster(__file__)
+node = cluster.add_instance("node", main_configs=["configs/config.xml"])
+
+
+@pytest.fixture(scope="module")
+def started_cluster():
+    try:
+        cluster.start()
+        yield cluster
+
+    finally:
+        cluster.shutdown()
+
+
+def test_successful_decryption(started_cluster):
+    assert (
+        node.query(
+            "select value from system.server_settings where name ='max_table_size_to_drop'"
+        )
+        == "60000000000\n"
+    )
+    assert (
+        node.query(
+            "select value from system.server_settings where name ='max_partition_size_to_drop'"
+        )
+        == "40000000000\n"
+    )
