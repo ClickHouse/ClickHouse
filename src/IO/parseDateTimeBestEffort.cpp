3f77b778e368c1c5e3cd3012d54107193094502b
Victor Krasnov <v.krasnov@arenadata.io>
2023-06-06 11:42:45 +0300

    Fix runtime bug

--- src/IO/parseDateTimeBestEffort.cpp
+++ src/IO/parseDateTimeBestEffort.cpp
@@ -586,7 +586,7 @@ ReturnType parseDateTimeBestEffortImpl(
     {
         time_t now = time(nullptr);
         UInt16 curr_year = local_time_zone.toYear(now);
-        year = now < local_time_zone.makeDateTime(year, month, day_of_month, hour, minute, second) ? curr_year - 1 : curr_year;
+        year = now < local_time_zone.makeDateTime(curr_year, month, day_of_month, hour, minute, second) ? curr_year - 1 : curr_year;
     }
 
     auto is_leap_year = (year % 400 == 0) || (year % 100 != 0 && year % 4 == 0);
