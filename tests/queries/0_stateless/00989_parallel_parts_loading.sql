9b375f44f53d6b08b5dc97395fe19fcf28aef57d
Nikita Mikhaylov <mikhaylovnikitka@gmail.com>
2023-06-06 14:42:56 +0200

    Rework loading and removing of data parts for MergeTree tables. (#49474)
    Co-authored-by: Sergei Trifonov <sergei@clickhouse.com>

--- tests/queries/0_stateless/00989_parallel_parts_loading.sql
+++ tests/queries/0_stateless/00989_parallel_parts_loading.sql
@@ -2,7 +2,7 @@
 
 DROP TABLE IF EXISTS mt;
 
-CREATE TABLE mt (x UInt64) ENGINE = MergeTree ORDER BY x SETTINGS max_part_loading_threads = 16, parts_to_delay_insert = 100000, parts_to_throw_insert = 100000;
+CREATE TABLE mt (x UInt64) ENGINE = MergeTree ORDER BY x SETTINGS parts_to_delay_insert = 100000, parts_to_throw_insert = 100000;
 
 SYSTEM STOP MERGES mt;
 
