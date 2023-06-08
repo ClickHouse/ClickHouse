ef1898dd7e7584f7d36229a9beb382d5a9c91ba0
flynn <fenglv15@mails.ucas.ac.cn>
2023-06-08 16:14:39 +0000

    update test

--- tests/queries/0_stateless/02771_log_faminy_truncate_count.sql
+++ tests/queries/0_stateless/02771_log_faminy_truncate_count.sql
@@ -1,27 +1,26 @@
--- Tags: no-parallel
-DROP TABLE IF EXISTS default.test_log;
-CREATE TABLE default.test_log
+DROP TABLE IF EXISTS test_log;
+CREATE TABLE test_log
 (
     `crypto_name` String,
     `trade_date` Date
 )
 ENGINE = Log;
 
-INSERT INTO default.test_log (crypto_name, trade_date) VALUES ('abc', '2021-01-01'), ('def', '2022-02-02');
+INSERT INTO test_log (crypto_name, trade_date) VALUES ('abc', '2021-01-01'), ('def', '2022-02-02');
 
-TRUNCATE TABLE default.test_log;
-SELECT count() FROM  default.test_log;
+TRUNCATE TABLE test_log;
+SELECT count() FROM  test_log;
 
-DROP TABLE IF EXISTS default.test_log;
-CREATE TABLE default.test_log
+DROP TABLE IF EXISTS test_log;
+CREATE TABLE test_log
 (
     `crypto_name` String,
     `trade_date` Date
 )
 ENGINE = StripeLog;
 
-INSERT INTO default.test_log (crypto_name, trade_date) VALUES ('abc', '2021-01-01'), ('def', '2022-02-02');
+INSERT INTO test_log (crypto_name, trade_date) VALUES ('abc', '2021-01-01'), ('def', '2022-02-02');
 
-TRUNCATE TABLE default.test_log;
-SELECT count() FROM  default.test_log;
-DROP TABLE default.test_log;
+TRUNCATE TABLE test_log;
+SELECT count() FROM  test_log;
+DROP TABLE test_log;
