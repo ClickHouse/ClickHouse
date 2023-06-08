621e30c453c1ec3119534536542d68bfc3260bc5
Ilya Golshtein <igolshtein@altinity.com>
2023-06-08 09:42:42 +0000

    system_kafka_consumers: start integrating with real kafka

--- src/Storages/System/StorageSystemKafkaConsumers.cpp
+++ src/Storages/System/StorageSystemKafkaConsumers.cpp
@@ -1,4 +1,3 @@
-#include <Storages/System/StorageSystemKafkaConsumers.h>
 #include <DataTypes/DataTypeArray.h>
 #include <DataTypes/DataTypeString.h>
 #include <DataTypes/DataTypesNumber.h>
@@ -7,6 +6,9 @@
 #include <Columns/ColumnString.h>
 #include <Columns/ColumnsNumber.h>
 #include <Columns/ColumnsDateTime.h>
+#include <Access/ContextAccess.h>
+#include <Storages/System/StorageSystemKafkaConsumers.h>
+#include <Storages/Kafka/StorageKafka.h>
 
 #include <Interpreters/Context.h>
 #include <Interpreters/DatabaseCatalog.h>
@@ -41,7 +43,7 @@ NamesAndTypesList StorageSystemKafkaConsumers::getNamesAndTypes()
 
 
 
-void StorageSystemKafkaConsumers::fillData(MutableColumns & res_columns, ContextPtr, const SelectQueryInfo &) const
+void StorageSystemKafkaConsumers::fillData(MutableColumns & res_columns, ContextPtr context, const SelectQueryInfo &) const
 {
     auto tables_mark_dropped = DatabaseCatalog::instance().getTablesMarkedDropped();
 
@@ -54,12 +56,15 @@ void StorageSystemKafkaConsumers::fillData(MutableColumns & res_columns, Context
 
     auto & assigments_topics = assert_cast<ColumnString &>(assert_cast<ColumnArray &>(*res_columns[index]).getData());
     auto & assigments_topics_offsets = assert_cast<ColumnArray &>(*res_columns[index++]).getOffsets();
+    size_t assigments_topics_last_offset = 0;
 
     auto & assigments_partition_id = assert_cast<ColumnString &>(assert_cast<ColumnArray &>(*res_columns[index]).getData());
     auto & assigments_partition_id_offsets = assert_cast<ColumnArray &>(*res_columns[index++]).getOffsets();
+    size_t assigments_partition_id_last_offset = 0;
 
     auto & assigments_current_offset = assert_cast<ColumnString &>(assert_cast<ColumnArray &>(*res_columns[index]).getData());
     auto & assigments_current_offset_offsets = assert_cast<ColumnArray &>(*res_columns[index++]).getOffsets();
+    size_t assigments_current_offset_last_offset = 0;
 
 
     auto & last_exception_time = assert_cast<ColumnDateTime &>(*res_columns[index++]);
@@ -73,14 +78,22 @@ void StorageSystemKafkaConsumers::fillData(MutableColumns & res_columns, Context
     auto & num_rebalance_assigments = assert_cast<ColumnUInt64 &>(*res_columns[index++]);
     auto & is_currently_used = assert_cast<ColumnUInt8 &>(*res_columns[index++]);
 
+    const auto access = context->getAccess();
 
-    auto add_row = [&]()
+    auto add_row = [&](const DatabaseTablesIteratorPtr & it)
     {
+        if (!access->isGranted(AccessType::SHOW_TABLES, it->databaseName(), it->name()))
+        {
+            return;
+        }
+
         std::string fake_database = "fake_kafka_database";
-        database.insertData(fake_database.data(), fake_database.size());
+        // database.insertData(fake_database.data(), fake_database.size());
+        database.insertData(it->databaseName().data(), it->databaseName().size());
 
         std::string fake_table = "fake_kafka_table";
-        table.insertData(fake_table.data(), fake_table.size());
+        // table.insertData(fake_table.data(), fake_table.size());
+        table.insertData(it->name().data(), it->name().size());
 
         std::string fake_consumer_id = "fake_consumer_id";
         consumer_id.insertData(fake_consumer_id.data(), fake_consumer_id.size());
@@ -89,19 +102,22 @@ void StorageSystemKafkaConsumers::fillData(MutableColumns & res_columns, Context
         std::string fake_assigments_topic_2 = "fake_assigments_topic_2";
         assigments_topics.insertData(fake_assigments_topic_1.data(), fake_assigments_topic_1.size());
         assigments_topics.insertData(fake_assigments_topic_2.data(), fake_assigments_topic_2.size());
-        assigments_topics_offsets.push_back(2);
+        assigments_topics_last_offset += 2;
+        assigments_topics_offsets.push_back(assigments_topics_last_offset);
 
         std::string fake_partition_id_1 = "fake_partition_id_1";
         std::string fake_partition_id_2 = "fake_partition_id_2";
         assigments_partition_id.insertData(fake_partition_id_1.data(), fake_partition_id_1.size());
         assigments_partition_id.insertData(fake_partition_id_2.data(), fake_partition_id_2.size());
-        assigments_partition_id_offsets.push_back(2);
+        assigments_partition_id_last_offset += 2;
+        assigments_partition_id_offsets.push_back(assigments_partition_id_last_offset);
 
         std::string fake_current_offset_1 = "fake_current_offset_1";
         std::string fake_current_offset_2 = "fake_current_offset_2";
         assigments_current_offset.insertData(fake_current_offset_1.data(), fake_current_offset_1.size());
         assigments_current_offset.insertData(fake_current_offset_2.data(), fake_current_offset_2.size());
-        assigments_current_offset_offsets.push_back(2);
+        assigments_current_offset_last_offset += 2;
+        assigments_current_offset_offsets.push_back(assigments_current_offset_last_offset);
 
         last_exception_time.insert(0);
 
@@ -118,7 +134,24 @@ void StorageSystemKafkaConsumers::fillData(MutableColumns & res_columns, Context
         is_currently_used.insert(0);
     };
 
-    add_row();
+    const bool show_tables_granted = access->isGranted(AccessType::SHOW_TABLES);
+
+    if (show_tables_granted)
+    {
+        auto databases = DatabaseCatalog::instance().getDatabases();
+        for (const auto & db : databases)
+        {
+            for (auto iterator = db.second->getTablesIterator(context); iterator->isValid(); iterator->next())
+            {
+                StoragePtr storage = iterator->table();
+                if (auto * kafka_table = dynamic_cast<StorageKafka *>(storage.get()))
+                {
+                    add_row(iterator);
+                }
+            }
+        }
+
+    }
 }
 
 }
