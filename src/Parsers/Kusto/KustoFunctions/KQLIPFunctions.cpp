96e07567c1dfd37b5ecd51eb8e0ebd091bd278f1
Yong Wang <yong.wang@ibm.com>
2023-06-09 11:50:00 -0700

    Kusto-phase2: fix another intExp2 integer overflow

--- src/Parsers/Kusto/KustoFunctions/KQLIPFunctions.cpp
+++ src/Parsers/Kusto/KustoFunctions/KQLIPFunctions.cpp
@@ -54,7 +54,7 @@ bool Ipv4IsInRange::convertImpl(String & out, IParser::Pos & pos)
     out = std::format(
         "if(isNull(IPv4StringToNumOrNull({0}) as ip_{3}) "
         "or isNull({1} as range_start_ip_{3}) or isNull({2} as range_mask_{3}), null, "
-        "bitXor(range_start_ip_{3}, bitAnd(ip_{3}, bitNot(toUInt32(intExp2(32 - range_mask_{3}) - 1)))) = 0)",
+        "bitXor(range_start_ip_{3}, bitAnd(ip_{3}, bitNot(toUInt32(intExp2(toInt32(32 - range_mask_{3})) - 1)))) = 0)",
         ip_address,
         kqlCallToExpression("parse_ipv4", {ip_range}, pos.max_depth),
         kqlCallToExpression("ipv4_netmask_suffix", {ip_range}, pos.max_depth),
