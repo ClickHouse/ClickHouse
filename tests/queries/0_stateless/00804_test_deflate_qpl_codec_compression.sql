6687bade787b8e0788ebd0af08ce9e77d333739f
Robert Schulze <robert@clickhouse.com>
2023-06-11 15:16:52 +0000

    Fix tests

--- tests/queries/0_stateless/00804_test_deflate_qpl_codec_compression.sql
+++ tests/queries/0_stateless/00804_test_deflate_qpl_codec_compression.sql
@@ -2,7 +2,8 @@
 -- no-fasttest because DEFLATE_QPL isn't available in fasttest
 -- no-cpu-aarch64 because DEFLATE_QPL is x86-only
 
-SET send_logs_level = 'fatal';
+-- A bunch of random DDLs to test the DEFLATE_QPL codec.
+
 SET enable_deflate_qpl_codec = 1;
 
 DROP TABLE IF EXISTS compression_codec;
@@ -20,6 +21,8 @@ CREATE TABLE compression_codec(
     sometuple Tuple(UInt16, UInt64) CODEC(DEFLATE_QPL),
 ) ENGINE = MergeTree() ORDER BY tuple();
 
+SHOW CREATE TABLE compression_codec;
+
 INSERT INTO compression_codec VALUES(1, 'hello', toDate('2018-12-14'), toDate32('2018-12-14'), 1.1, 'aaa', 5, [1,2,3], map('k1',1,'k2',2), tuple(1,2));
 INSERT INTO compression_codec VALUES(2, 'world', toDate('2018-12-15'), toDate32('2018-12-15'), 2.2, 'bbb', 6, [4,5,6], map('k3',3,'k4',4), tuple(3,4));
 INSERT INTO compression_codec VALUES(3, '!', toDate('2018-12-16'), toDate32('2018-12-16'), 3.3, 'ccc', 7, [7,8,9], map('k5',5,'k6',6), tuple(5,6));
@@ -34,5 +37,9 @@ DETACH TABLE compression_codec;
 ATTACH TABLE compression_codec;
 
 SELECT count(*) FROM compression_codec WHERE id = 2 GROUP BY id;
+
+INSERT INTO compression_codec SELECT 3, '!', toDate('2018-12-16'), toDate32('2018-12-16'), 3.3, 'ccc', 7, [7,8,9], map('k5',5,'k6',6), tuple(5,6) FROM system.numbers LIMIT 10000;
+
+SELECT count(*) FROM compression_codec WHERE id = 3 GROUP BY id;
 
 DROP TABLE IF EXISTS compression_codec;
