63ddc2719c293b2a051a2a27021c05829e41bf07
János Benjamin Antal <benjamin.antal@clickhouse.com>
2023-06-13 20:49:08 +0000

    Mark some fields maybe unused because of their conditional usage

--- src/Parsers/PRQL/ParserPRQLQuery.h
+++ src/Parsers/PRQL/ParserPRQLQuery.h
@@ -8,8 +8,8 @@ class ParserPRQLQuery final : public IParserBase
 {
 private:
     // These fields are not used when PRQL is disabled at build time
-    size_t max_query_size; // NOLINT(unused-private-field)
-    size_t max_parser_depth; // NOLINT(unused-private-field)
+    [[maybe_unused]] size_t max_query_size;
+    [[maybe_unused]] size_t max_parser_depth;
 
 public:
     ParserPRQLQuery(size_t max_query_size_, size_t max_parser_depth_) : max_query_size{max_query_size_}, max_parser_depth{max_parser_depth_}
