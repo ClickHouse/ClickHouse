b89f9f5f2dde645b13cb2fa4f19bc5260c428b98
János Benjamin Antal <benjamin.antal@clickhouse.com>
2023-06-13 14:47:50 +0000

    Suppress clang-tidy warning

--- src/Parsers/PRQL/ParserPRQLQuery.h
+++ src/Parsers/PRQL/ParserPRQLQuery.h
@@ -7,8 +7,9 @@ namespace DB
 class ParserPRQLQuery final : public IParserBase
 {
 private:
-    size_t max_query_size;
-    size_t max_parser_depth;
+    // These fields are not used when PRQL is disabled at build time
+    size_t max_query_size; // NOLINT(unused-private-field)
+    size_t max_parser_depth; // NOLINT(unused-private-field)
 
 public:
     ParserPRQLQuery(size_t max_query_size_, size_t max_parser_depth_) : max_query_size{max_query_size_}, max_parser_depth{max_parser_depth_}
