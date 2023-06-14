07f07607a373f2af968d83792e6e3dcc5e9541d1
Nikolai Kochetov <nik-kochetov@yandex-team.ru>
2023-06-14 08:48:16 +0000

    Hold context in pipeline resources.

--- src/QueryPipeline/QueryPipelineBuilder.h
+++ src/QueryPipeline/QueryPipelineBuilder.h
@@ -185,6 +185,7 @@ public:
 
     void addResources(QueryPlanResourceHolder resources_) { resources = std::move(resources_); }
     void setQueryIdHolder(std::shared_ptr<QueryIdHolder> query_id_holder) { resources.query_id_holders.emplace_back(std::move(query_id_holder)); }
+    void addContext(ContextPtr context) { resources.interpreter_context.emplace_back(std::move(context)); }
 
     /// Convert query pipeline to pipe.
     static Pipe getPipe(QueryPipelineBuilder pipeline, QueryPlanResourceHolder & resources);
