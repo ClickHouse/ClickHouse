c321f45a8f8798f94e73e94d494bd763b36a59f2
Roman Vasin <r.vasin@arenadata.io>
2023-06-15 14:50:52 +0000

    Add USE_SSL

--- src/Common/Config/ConfigReloader.cpp
+++ src/Common/Config/ConfigReloader.cpp
@@ -130,7 +130,10 @@ void ConfigReloader::reloadIfNewer(bool force, bool throw_on_error, bool fallbac
             return;
         }
         config_processor.savePreprocessedConfig(loaded_config, preprocessed_dir);
+
+#if USE_SSL
         config_processor.decryptConfig(loaded_config);
+#endif
 
         /** We should remember last modification time if and only if config was successfully loaded
          * Otherwise a race condition could occur during config files update:
