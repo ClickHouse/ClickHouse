2c63b17a74bacbeb04f48781159e3951bc4f9bd3
Vitaly Baranov <vitlibar@clickhouse.com>
2023-06-15 17:59:37 +0200

    Show halves of checksums in "system.parts", "system.projection_parts" and error messages in the correct order.

--- src/Storages/Distributed/DistributedAsyncInsertHeader.cpp
+++ src/Storages/Distributed/DistributedAsyncInsertHeader.cpp
@@ -40,8 +40,8 @@ DistributedAsyncInsertHeader DistributedAsyncInsertHeader::read(ReadBufferFromFi
         {
             throw Exception(ErrorCodes::CHECKSUM_DOESNT_MATCH,
                             "Checksum of extra info doesn't match: corrupted data. Reference: {}{}. Actual: {}{}.",
-                            getHexUIntLowercase(expected_checksum.low64), getHexUIntLowercase(expected_checksum.high64),
-                            getHexUIntLowercase(calculated_checksum.low64), getHexUIntLowercase(calculated_checksum.high64));
+                            getHexUIntLowercase(expected_checksum.high64), getHexUIntLowercase(expected_checksum.low64),
+                            getHexUIntLowercase(calculated_checksum.high64), getHexUIntLowercase(calculated_checksum.low64));
         }
 
         /// Read the parts of the header.
