3fa728bc27c18b737242c65b7aed295521182417
Nikolai Kochetov <KochetovNicolai@users.noreply.github.com>
2023-06-16 15:39:32 +0200

    Merge pull request #50489 from ClickHouse/vdimir/alter_moving_garbage
    Cleanup moving parts
    
    (cherry picked from commit df1ea0b572256759b4c61fc445a4fa9f0b81439a)

--- tests/integration/test_encrypted_disk/test.py
+++ tests/integration/test_encrypted_disk/test.py
@@ -83,7 +83,7 @@ def test_part_move(policy, destination_disks):
             data String
         ) ENGINE=MergeTree()
         ORDER BY id
-        SETTINGS storage_policy='{}'
+        SETTINGS storage_policy='{}', temporary_directories_lifetime=1
         """.format(
             policy
         )
