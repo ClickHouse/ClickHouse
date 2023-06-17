ca2e0fc0fea6d763e0b4bdf83f1c89768e45b99f
Alexey Milovidov <milovidov@clickhouse.com>
2023-06-17 15:38:30 +0200

    Update test

--- tests/queries/0_stateless/02790_jit_wrong_result.sql
+++ tests/queries/0_stateless/02790_jit_wrong_result.sql
@@ -4,6 +4,8 @@ INSERT INTO t2(c1, c0) VALUES (1697596429, 1259570390);
 INSERT INTO t2(c1, c2) VALUES (-871444251, 's,');
 INSERT INTO t2(c0, c2, c1) VALUES (-943999939, '', 1756486294);
 
+SELECT * FROM
+(
 SELECT MIN(t2.c0)
 FROM t2
 GROUP BY log(-(t2.c0 / (t2.c0 - t2.c0)))
@@ -18,6 +20,7 @@ SELECT MIN(t2.c0)
 FROM t2
 GROUP BY log(-(t2.c0 / (t2.c0 - t2.c0)))
 HAVING (NOT (NOT (-(NOT MIN(t2.c0))))) IS NULL
+)
 ORDER BY 1
 SETTINGS aggregate_functions_null_for_empty = 1, enable_optimize_predicate_expression = 0, min_count_to_compile_expression = 0;
 
