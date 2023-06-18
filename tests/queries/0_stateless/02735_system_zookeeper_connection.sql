cb9580d8d74fcdf7310a68e7677aa7963fac67a5
Alexander Tokmakov <tavplubix@gmail.com>
2023-06-18 13:12:51 +0300

    Update 02735_system_zookeeper_connection.sql

--- tests/queries/0_stateless/02735_system_zookeeper_connection.sql
+++ tests/queries/0_stateless/02735_system_zookeeper_connection.sql
@@ -10,6 +10,6 @@ ORDER BY tuple();
 
 -- keeper_api_version will by 0 for auxiliary_zookeeper2, because we fail to get /api_version due to chroot
 -- I'm not sure if it's a bug or a useful trick to fallback to basic api
-select name, host, port, index, connected_time, session_uptime_elapsed_seconds, is_expired, keeper_api_version from system.zookeeper_connection order by name;
+select name, host, port, index, (connected_time between yestedday() and today()), (abs(session_uptime_elapsed_seconds  - zookeeperSessionUptime()) < 10), is_expired, keeper_api_version from system.zookeeper_connection order by name;
 
 DROP TABLE IF EXISTS test_zk_connection_table;
