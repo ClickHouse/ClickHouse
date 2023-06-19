84f95fc28165c447e2a7202aff030de6d90ae8b4
Alexander Tokmakov <tavplubix@clickhouse.com>
2023-06-19 20:34:23 +0200

    fix test

--- tests/queries/0_stateless/02735_system_zookeeper_connection.sql
+++ tests/queries/0_stateless/02735_system_zookeeper_connection.sql
@@ -8,8 +8,14 @@ CREATE TABLE test_zk_connection_table (
 ENGINE ReplicatedMergeTree('zookeeper2:/clickhouse/{database}/02731_zk_connection/{shard}', '{replica}')
 ORDER BY tuple();
 
+select name, host, port, index, is_expired, keeper_api_version, (connected_time between yesterday() and now()),
+       (abs(session_uptime_elapsed_seconds  - zookeeperSessionUptime()) < 10)
+from system.zookeeper_connection where name='default';
+
 -- keeper_api_version will by 0 for auxiliary_zookeeper2, because we fail to get /api_version due to chroot
 -- I'm not sure if it's a bug or a useful trick to fallback to basic api
-select name, host, port, index, (connected_time between yesterday() and today()), (abs(session_uptime_elapsed_seconds  - zookeeperSessionUptime()) < 10), is_expired, keeper_api_version from system.zookeeper_connection order by name;
+-- Also, auxiliary zookeeper is created lazily
+select name, host, port, index, is_expired, keeper_api_version, (connected_time between yesterday() and now())
+from system.zookeeper_connection where name!='default';
 
 DROP TABLE IF EXISTS test_zk_connection_table;
