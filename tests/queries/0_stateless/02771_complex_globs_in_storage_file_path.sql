a26194fcef834f90bb40d2c583b3150da87d780f
zvonand <azvonov@altinity.com>
2023-06-19 23:06:08 +0200

    upd for review

--- tests/queries/0_stateless/02771_complex_globs_in_storage_file_path.sql
+++ tests/queries/0_stateless/02771_complex_globs_in_storage_file_path.sql
@@ -1,5 +1,7 @@
 -- Tags: no-replicated-database, no-parallel
 
+SELECT *, _file FROM file('02771/dir{?/subdir?1/da,2/subdir2?/da}ta/non_existing.csv', CSV); -- {serverError CANNOT_EXTRACT_TABLE_STRUCTURE}
+
 INSERT INTO TABLE FUNCTION file('02771/dir1/subdir11/data1.csv', 'CSV', 's String') SELECT 'This is file data1' SETTINGS engine_file_truncate_on_insert=1;
 INSERT INTO TABLE FUNCTION file('02771/dir2/subdir22/data2.csv', 'CSV', 's String') SELECT 'This is file data2' SETTINGS engine_file_truncate_on_insert=1;
 
