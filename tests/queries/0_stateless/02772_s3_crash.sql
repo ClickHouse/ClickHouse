75b266e179fc2994e6bbc7d15ad3e49aacffe708
Nikolay Degterinsky <evillique@gmail.com>
2023-06-21 00:04:11 +0000

    Fix crash in s3 and s3Cluster functions

---
+++ tests/queries/0_stateless/02772_s3_crash.sql
@@ -0,0 +1,2 @@
+SELECT * FROM s3(headers('random_header' = 'value')); -- { serverError NUMBER_OF_ARGUMENTS_DOESNT_MATCH }
+SELECT * FROM s3Cluster('test_cluster_two_shards_localhost', headers('random_header' = 'value')); -- { serverError NUMBER_OF_ARGUMENTS_DOESNT_MATCH }
