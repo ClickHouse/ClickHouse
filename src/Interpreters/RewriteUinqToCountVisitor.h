f9263b4fcc9f437064612ed731e829be0cd4e765
JackyWoo <wuchienchao@qq.com>
2023-06-21 10:33:11 +0800

    rewrite uniq to count

---
+++ src/Interpreters/RewriteUinqToCountVisitor.h
@@ -0,0 +1,29 @@
+#pragma once
+
+#include <Parsers/IAST.h>
+#include <Interpreters/InDepthNodeVisitor.h>
+#include "Interpreters/TreeRewriter.h"
+
+namespace DB
+{
+
+class ASTFunction;
+
+/// Simple rewrite:
+///     'SELECT uniq(x) FROM (SELECT DISTINCT x ...)' to
+///     'SELECT count() FROM (SELECT DISTINCT x ...)'
+///
+///     'SELECT uniq() FROM (SELECT x ... GROUP BY x)' to
+///     'SELECT count() FROM (SELECT x ... GROUP BY x)'
+///
+/// Note we can rewrite all uniq variants except uniqUpTo.
+class RewriteUinqToCountMatcher
+{
+public:
+    struct Data {};
+    static void visit(ASTPtr & ast, Data &);
+    static bool needChildVisit(const ASTPtr &, const ASTPtr &) { return true; }
+};
+
+using RewriteUinqToCountVisitor = InDepthNodeVisitor<RewriteUinqToCountMatcher, true>;
+}
