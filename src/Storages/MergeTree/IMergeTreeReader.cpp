48317cff5bfcfc760df302da2115b98d46be8be7
Anton Popov <anton@clickhouse.com>
2023-06-22 22:03:40 +0000

    better expception message

--- src/Storages/MergeTree/IMergeTreeReader.cpp
+++ src/Storages/MergeTree/IMergeTreeReader.cpp
@@ -280,11 +280,15 @@ void IMergeTreeReader::checkNumberOfColumns(size_t num_columns_to_read) const
 
 String IMergeTreeReader::getMessageForDiagnosticOfBrokenPart(size_t from_mark, size_t max_rows_to_read) const
 {
+    const auto & data_part_storage = data_part_info_for_read->getDataPartStorage();
     return fmt::format(
-        "(while reading from part {} in table {} from mark {} with max_rows_to_read = {})",
-        data_part_info_for_read->getDataPartStorage()->getFullPath(),
+        "(while reading from part {} in table {} located on disk {} of type {}, from mark {} with max_rows_to_read = {})",
+        data_part_storage->getFullPath(),
         data_part_info_for_read->getTableName(),
-        from_mark, max_rows_to_read);
+        data_part_storage->getDiskName(),
+        data_part_storage->getDiskType(),
+        from_mark,
+        max_rows_to_read);
 }
 
 }
