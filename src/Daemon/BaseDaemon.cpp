356eec1aef9c7872a5c2aa058072555f1e0696a5
Alexander Tokmakov <tavplubix@clickhouse.com>
2023-06-23 20:48:56 +0200

    unify merge predicate

--- src/Daemon/BaseDaemon.cpp
+++ src/Daemon/BaseDaemon.cpp
@@ -418,7 +418,7 @@ private:
             SentryWriter::onFault(sig, error_message, stack_trace);
 
             /// Advice the user to send it manually.
-            if constexpr (std::string_view(VERSION_OFFICIAL).contains("official build"))
+            if (String(VERSION_OFFICIAL).contains("official build"))
             {
                 const auto & date_lut = DateLUT::instance();
 
