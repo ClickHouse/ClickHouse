358493273dde1ed9b0395bd4c683e99f4ea01413
yariks5s <114298166+yariks5s@users.noreply.github.com>
2023-06-25 23:50:20 +0200

    Update src/Interpreters/DatabaseCatalog.cpp
    Co-authored-by: Yakov Olkhovskiy <99031427+yakov-olkhovskiy@users.noreply.github.com>

--- src/Interpreters/DatabaseCatalog.cpp
+++ src/Interpreters/DatabaseCatalog.cpp
@@ -441,8 +441,7 @@ DatabaseAndTable DatabaseCatalog::getTableImpl(
         }
         else
         {
-            std::string suggested_name = names[0];
-            exception->emplace(Exception(ErrorCodes::UNKNOWN_TABLE, "Table {} doesn't exist. Maybe you wanted to type {}?", table_id.getNameForLogs(), backQuoteIfNeed(suggested_name))
+            exception->emplace(Exception(ErrorCodes::UNKNOWN_TABLE, "Table {} doesn't exist. Maybe you wanted to type {}?", table_id.getNameForLogs(), backQuoteIfNeed(names[0]))
             );
         }
     }
