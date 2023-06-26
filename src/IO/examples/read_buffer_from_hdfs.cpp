4a8644387dbe849ca7f46e95e671cedc6078b558
taiyang-li <654010905@qq.com>
2023-06-26 16:50:18 +0800

    add example for test

--- src/IO/examples/read_buffer_from_hdfs.cpp
+++ src/IO/examples/read_buffer_from_hdfs.cpp
@@ -11,15 +11,15 @@ using namespace DB;
 
 int main()
 {
-    setenv("LIBHDFS3_CONF", "/data1/clickhouse_official/conf/hdfs-site.bigocluster.xml", true); /// NOLINT
-    String hdfs_uri = "hdfs://bigocluster";
-    String hdfs_file_path = "/data/hive/report_tb.db/bigolive_wj_pos_sdk_video_stats_event_allv1/day=2023-03-14/"
-                            "part-00014-272de29e-098c-4007-987a-f6b7ae740402-c000";
+    setenv("LIBHDFS3_CONF", "/path/to/hdfs-site.xml", true); /// NOLINT
+    String hdfs_uri = "hdfs://cluster_name";
+    String hdfs_file_path = "/path/to/hdfs/file";
     ConfigurationPtr config = Poco::AutoPtr(new Poco::Util::MapConfiguration());
     ReadSettings read_settings;
-    ReadBufferFromHDFS read_buffer(hdfs_uri, hdfs_file_path, *config, read_settings, 625150306UL, false);
+    ReadBufferFromHDFS read_buffer(hdfs_uri, hdfs_file_path, *config, read_settings, 2097152UL, false);
 
     String download_path = "./download";
     WriteBufferFromFile write_buffer(download_path);
     copyData(read_buffer, write_buffer);
+    return 0;
 }
