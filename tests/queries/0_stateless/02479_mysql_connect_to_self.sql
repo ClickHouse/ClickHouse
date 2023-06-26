49a21cd71f3d606e611549b80d4d5c5bc6d9a800
Alexey Milovidov <milovidov@clickhouse.com>
2023-06-26 02:48:30 +0200

    Fix bad test

--- tests/queries/0_stateless/02479_mysql_connect_to_self.sql
+++ tests/queries/0_stateless/02479_mysql_connect_to_self.sql
@@ -2,6 +2,8 @@
 
 SET send_logs_level = 'fatal'; -- failed connection tries are ok, if it succeeded after retry.
 
+DROP TABLE IF EXISTS foo;
+
 CREATE TABLE foo (key UInt32, a String, b Int64, c String) ENGINE = TinyLog;
 INSERT INTO foo VALUES (1, 'one', -1, 'een'), (2, 'two', -2, 'twee'), (3, 'three', -3, 'drie'), (4, 'four', -4, 'vier'), (5, 'five', -5, 'vijf');
 
@@ -40,3 +42,5 @@ SELECT * FROM mysql(
 SELECT '---';
 SELECT count() FROM mysql('127.0.0.1:9004', currentDatabase(), foo, 'default', '', SETTINGS connection_pool_size = 1, connect_timeout = 100, connection_wait_timeout = 100);
 SELECT count() FROM mysql('127.0.0.1:9004', currentDatabase(), foo, 'default', '', SETTINGS connection_pool_size = 0); -- { serverError BAD_ARGUMENTS }
+
+DROP TABLE foo;
