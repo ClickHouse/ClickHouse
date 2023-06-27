d4c8021f8078425ae3e7a74e32477f78307a0999
Igor Nikonov <igor@clickhouse.com>
2023-06-27 12:41:21 +0000

    Extend consistency check for ColumnObject
    + check that there arer subcolumns when there are rows

--- tests/queries/0_stateless/02789_object_type_invalid_num_of_rows.sql
+++ tests/queries/0_stateless/02789_object_type_invalid_num_of_rows.sql
@@ -1,2 +1,2 @@
 set allow_experimental_object_type=1;
-SELECT '0.02' GROUP BY GROUPING SETS (('6553.6'), (CAST(CAST('{"x" : 1}', 'Object(Nullable(\'json\'))'), 'Object(\'json\')'))) settings max_threads=1; -- { serverError NOT_IMPLEMENTED }
+SELECT '0.02' GROUP BY GROUPING SETS (('6553.6'), (CAST('{"x" : 1}', 'Object(\'json\')'))) settings max_threads=1; -- { serverError NOT_IMPLEMENTED }
