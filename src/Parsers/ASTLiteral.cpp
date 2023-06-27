6491a0569ff845dce385f48551d4a57dfc84d238
frinkr <frinkr@outlook.com>
2023-06-27 16:32:19 +0800

    update comments

--- src/Parsers/ASTLiteral.cpp
+++ src/Parsers/ASTLiteral.cpp
@@ -86,8 +86,8 @@ void ASTLiteral::appendColumnNameImpl(WriteBuffer & ostr) const
     }
     else
     {
-        /// Special case for huge AST. The `FieldVisitorToString` becomes expensive
-        /// for tons of literals.
+        /// Shortcut for huge AST. The `FieldVisitorToString` becomes expensive
+        /// for tons of literals as it creates temporary String.
         if (value.getType() == Field::Types::String)
         {
             writeQuoted(value.get<String>(), ostr);
