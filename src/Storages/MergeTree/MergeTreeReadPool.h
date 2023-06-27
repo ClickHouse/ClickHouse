b327f2f64b29545cac1f8e103ffe9506b9d1034f
Nikita Mikhaylov <mikhaylovnikitka@gmail.com>
2023-06-27 13:49:45 +0200

    Update src/Storages/MergeTree/MergeTreeReadPool.h

--- src/Storages/MergeTree/MergeTreeReadPool.h
+++ src/Storages/MergeTree/MergeTreeReadPool.h
@@ -193,7 +193,7 @@ public:
             predict_block_size_bytes, column_names, virtual_column_names, prewhere_info,
             actions_settings, reader_settings, per_part_params);
 
-        extension.all_callback({
+        extension.all_callback(InitialAllRangesAnnouncement{
             CoordinationMode::Default,
             parts_ranges.getDescriptions(),
             extension.number_of_current_replica
