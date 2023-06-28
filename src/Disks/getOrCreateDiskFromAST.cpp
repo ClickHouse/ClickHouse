eadc547376319e0a6a2d25569c8a224d2da2528e
kssenii <sumarokovakseniia@mail.ru>
2023-06-28 19:05:35 +0200

    Allow to add disk name for custom disk

--- src/Disks/getOrCreateDiskFromAST.cpp
+++ src/Disks/getOrCreateDiskFromAST.cpp
@@ -26,12 +26,20 @@ namespace
 {
     std::string getOrCreateDiskFromDiskAST(const ASTFunction & function, ContextPtr context)
     {
-        /// We need a unique name for a created custom disk, but it needs to be the same
-        /// after table is reattached or server is restarted, so take a hash of the disk
-        /// configuration serialized ast as a disk name suffix.
-        auto disk_setting_string = serializeAST(function, true);
-        auto disk_name = DiskSelector::TMP_INTERNAL_DISK_PREFIX
-            + toString(sipHash128(disk_setting_string.data(), disk_setting_string.size()));
+        std::string disk_name;
+        if (function.name == "disk")
+        {
+            /// We need a unique name for a created custom disk, but it needs to be the same
+            /// after table is reattached or server is restarted, so take a hash of the disk
+            /// configuration serialized ast as a disk name suffix.
+            auto disk_setting_string = serializeAST(function, true);
+            disk_name = DiskSelector::TMP_INTERNAL_DISK_PREFIX
+                + toString(sipHash128(disk_setting_string.data(), disk_setting_string.size()));
+        }
+        else
+        {
+            disk_name = function.name.substr(std::strlen("disk_"));
+        }
 
         auto result_disk = context->getOrCreateDisk(disk_name, [&](const DisksMap & disks_map) -> DiskPtr {
             const auto * function_args_expr = assert_cast<const ASTExpressionList *>(function.arguments.get());
@@ -42,6 +50,9 @@ namespace
             disk->markDiskAsCustom();
             return disk;
         });
+
+        if (!result_disk->isCustomDisk())
+            throw Exception(ErrorCodes::BAD_ARGUMENTS, "Disk with name `{}` already exist", disk_name);
 
         if (!result_disk->isRemote())
         {
---
+++ tests/queries/0_stateless/02808_custom_disk_with_user_defined_name.sh
@@ -0,0 +1,31 @@
+# Tags: no-fasttest, no-parallel
+
+# set -x
+
+CUR_DIR=$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)
+# shellcheck source=../shell_config.sh
+. "$CUR_DIR"/../shell_config.sh
+
+
+$CLICKHOUSE_CLIENT -nm --query """
+DROP TABLE IF EXISTS test;
+CREATE TABLE test (a Int32, b String)
+ENGINE = MergeTree() ORDER BY tuple()
+SETTINGS disk = disk_s3disk(type = cache, max_size = '100Ki', path = ${CLICKHOUSE_TEST_UNIQUE_NAME}, disk = s3disk);
+""" 2>&1 | grep -q "Disk with name \`s3disk\` already exist" && echo 'OK' || echo 'FAIL'
+
+$CLICKHOUSE_CLIENT -nm --query """
+SELECT count() FROM system.disks WHERE name = '$disk_name'
+"""
+
+disk_name="${CLICKHOUSE_TEST_UNIQUE_NAME}"
+$CLICKHOUSE_CLIENT -nm --query """
+DROP TABLE IF EXISTS test;
+CREATE TABLE test (a Int32, b String)
+ENGINE = MergeTree() ORDER BY tuple()
+SETTINGS disk = disk_$disk_name(type = cache, max_size = '100Ki', path = ${CLICKHOUSE_TEST_UNIQUE_NAME}, disk = s3disk);
+"""
+
+$CLICKHOUSE_CLIENT -nm --query """
+SELECT count() FROM system.disks WHERE name = '$disk_name'
+"""
