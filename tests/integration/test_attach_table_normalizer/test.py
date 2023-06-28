5df6f3d6e28483a029f3a8859c8bd09fdab008a0
robot-clickhouse <robot-clickhouse@users.noreply.github.com>
2023-06-28 03:40:09 +0000

    Automatic style fix

--- tests/integration/test_attach_table_normalizer/test.py
+++ tests/integration/test_attach_table_normalizer/test.py
@@ -3,9 +3,7 @@ import pytest
 from helpers.cluster import ClickHouseCluster
 
 cluster = ClickHouseCluster(__file__)
-node = cluster.add_instance(
-    "node", stay_alive=True
-)
+node = cluster.add_instance("node", stay_alive=True)
 
 
 @pytest.fixture(scope="module")
