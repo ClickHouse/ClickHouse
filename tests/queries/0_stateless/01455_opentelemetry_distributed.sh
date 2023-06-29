162eda333d42106a1d133e13aaf0b5666a97a545
Nikita Taranov <nikita.taranov@clickhouse.com>
2023-06-29 18:24:22 +0200

    impl

--- tests/queries/0_stateless/01455_opentelemetry_distributed.sh
+++ tests/queries/0_stateless/01455_opentelemetry_distributed.sh
@@ -3,8 +3,6 @@
 
 set -ue
 
-unset CLICKHOUSE_LOG_COMMENT
-
 CURDIR=$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)
 # shellcheck source=../shell_config.sh
 . "$CURDIR"/../shell_config.sh
--- tests/queries/0_stateless/01872_initial_query_start_time.sh
+++ tests/queries/0_stateless/01872_initial_query_start_time.sh
@@ -3,8 +3,6 @@ set -ue
 
 # this test doesn't need 'current_database = currentDatabase()',
 
-unset CLICKHOUSE_LOG_COMMENT
-
 CURDIR=$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)
 # shellcheck source=../shell_config.sh
 . "$CURDIR"/../shell_config.sh
