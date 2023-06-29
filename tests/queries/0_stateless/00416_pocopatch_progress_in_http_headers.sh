58581ce5f6bdfe0df9135a95c0df14404af91e2a
Nikolay Degterinsky <43110995+evillique@users.noreply.github.com>
2023-06-29 02:37:09 +0200

    Update 00416_pocopatch_progress_in_http_headers.sh

--- tests/queries/0_stateless/00416_pocopatch_progress_in_http_headers.sh
+++ tests/queries/0_stateless/00416_pocopatch_progress_in_http_headers.sh
@@ -12,7 +12,6 @@ counter=0
 while [ $counter -lt $RETRIES ] && [ $(echo "$result" | wc -l) != "$lines_expected" ]; do
     result=$(${CLICKHOUSE_CURL} -vsS "${CLICKHOUSE_URL}&max_block_size=5&send_progress_in_http_headers=1&http_headers_progress_interval_ms=0" -d 'SELECT max(number) FROM numbers(10)' 2>&1 | grep -E 'Content-Encoding|X-ClickHouse-Progress|^[0-9]')
     let counter=counter+1
-    # echo "$result" | wc -l
 done
 echo "$result"
 
@@ -22,7 +21,6 @@ counter=0
 while [ $counter -lt $RETRIES ] && [ $(echo "$result" | wc -l) != "$lines_expected" ]; do
     result=$(${CLICKHOUSE_CURL} -vsS "${CLICKHOUSE_URL}&max_block_size=1&send_progress_in_http_headers=1&http_headers_progress_interval_ms=0&output_format_parallel_formatting=0" -d 'SELECT number FROM numbers(10)' 2>&1 | grep -E 'Content-Encoding|X-ClickHouse-Progress|^[0-9]')
     let counter=counter+1
-    # echo "$result" | wc -l
 done
 echo "$result"
 
