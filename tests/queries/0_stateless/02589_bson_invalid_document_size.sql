8e296f6ca414a9794a59eb2dda7dda7b191e180b
Nikita Mikhaylov <nikitamikhaylov@clickhouse.com>
2023-06-29 13:14:23 +0000

    Fix test

--- tests/queries/0_stateless/02589_bson_invalid_document_size.sql
+++ tests/queries/0_stateless/02589_bson_invalid_document_size.sql
@@ -1,4 +1,3 @@
 set input_format_parallel_parsing=1;
 set max_threads=0;
-select * from format(BSONEachRow, 'x UInt32', x'00000000'); -- {serverError INCORRECT_DATA}
-
+select * from format(BSONEachRow, 'x UInt32', x'00000000'); -- {serverError CANNOT_PARSE_NUMBER}
