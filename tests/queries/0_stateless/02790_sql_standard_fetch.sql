6488c005be8a3c9cd2e0b57c69480d8c71dc4a9e
Sema Checherinda <Sema.Checherinda@clickhouse.com>
2023-06-29 16:32:52 +0200

    make test result stable

--- tests/queries/0_stateless/02790_sql_standard_fetch.sql
+++ tests/queries/0_stateless/02790_sql_standard_fetch.sql
@@ -4,28 +4,28 @@ CREATE TEMPORARY TABLE employees (id UInt64, name String, department String, sal
 INSERT INTO employees VALUES (23, 'Henry', 'it', 104), (24, 'Irene', 'it', 104), (25, 'Frank', 'it', 120), (31, 'Cindy', 'sales', 96), (33, 'Alice', 'sales', 100), (32, 'Dave', 'sales', 96), (22, 'Grace', 'it', 90), (21, 'Emma', 'it', '84');
 
 select * from employees
-order by salary desc
+order by salary desc, id asc
 limit 5
 format PrettyCompactNoEscapes;
 
 select * from employees
-order by salary desc
+order by salary desc, id asc
 fetch first 5 rows only
 format PrettyCompactNoEscapes;
 
 select * from employees
-order by salary desc
+order by salary desc, id asc
 fetch first 5 rows with ties
 format PrettyCompactNoEscapes;
 
 select * from employees
-order by salary desc
+order by salary desc, id asc
 offset 3 rows
 fetch next 5 rows only
 format PrettyCompactNoEscapes;
 
 select * from employees
-order by salary desc
+order by salary desc, id asc
 offset 3 rows
 fetch first 5 rows only
 format PrettyCompactNoEscapes;
