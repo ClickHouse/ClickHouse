4ba334c98360bb24e7d21eb12453ac5cc765d9d8
kssenii <sumarokovakseniia@mail.ru>
2023-06-29 13:01:31 +0200

    Review fix

--- tests/queries/0_stateless/02808_custom_disk_with_user_defined_name.sh
+++ tests/queries/0_stateless/02808_custom_disk_with_user_defined_name.sh
@@ -1,3 +1,4 @@
+#!/usr/bin/env bash
 # Tags: no-fasttest, no-parallel
 
 # set -x
@@ -14,11 +15,12 @@ ENGINE = MergeTree() ORDER BY tuple()
 SETTINGS disk = disk_s3disk(type = cache, max_size = '100Ki', path = ${CLICKHOUSE_TEST_UNIQUE_NAME}, disk = s3disk);
 """ 2>&1 | grep -q "Disk with name \`s3disk\` already exist" && echo 'OK' || echo 'FAIL'
 
+disk_name="${CLICKHOUSE_TEST_UNIQUE_NAME}"
+
 $CLICKHOUSE_CLIENT -nm --query """
 SELECT count() FROM system.disks WHERE name = '$disk_name'
 """
 
-disk_name="${CLICKHOUSE_TEST_UNIQUE_NAME}"
 $CLICKHOUSE_CLIENT -nm --query """
 DROP TABLE IF EXISTS test;
 CREATE TABLE test (a Int32, b String)
