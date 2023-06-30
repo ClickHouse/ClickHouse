dd1c5013e65ad4e84f842c1438cc1ac0d6213628
Amos Bird <amosbird@gmail.com>
2023-06-30 22:34:47 +0800

    Try to fix style issues

--- src/Common/ProfileEvents.cpp
+++ src/Common/ProfileEvents.cpp
@@ -537,9 +537,9 @@ The server successfully detected this situation and will download merged part fr
     M(LogFatal, "Number of log messages with level Fatal") \
 
 #ifdef APPLY_FOR_EXTERNAL_EVENTS
-  #define APPLY_FOR_EVENTS(M) APPLY_FOR_BUILTIN_EVENTS(M) APPLY_FOR_EXTERNAL_EVENTS(M)
+    #define APPLY_FOR_EVENTS(M) APPLY_FOR_BUILTIN_EVENTS(M) APPLY_FOR_EXTERNAL_EVENTS(M)
 #else
-  #define APPLY_FOR_EVENTS(M) APPLY_FOR_BUILTIN_EVENTS(M)
+    #define APPLY_FOR_EVENTS(M) APPLY_FOR_BUILTIN_EVENTS(M)
 #endif
 
 namespace ProfileEvents
