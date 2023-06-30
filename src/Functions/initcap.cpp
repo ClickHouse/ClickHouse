d6dacd3ccfe340410ead90ffcadd769716a61ec7
Dmitry Kardymon <d.kardymon@arenadata.io>
2023-06-30 13:53:20 +0000

    Fix test with num

--- src/Functions/initcap.cpp
+++ src/Functions/initcap.cpp
@@ -1,5 +1,6 @@
 #include <Functions/FunctionFactory.h>
 #include <Functions/FunctionStringToString.h>
+#include <Common/StringUtils/StringUtils.h>
 
 namespace DB
 {
@@ -27,37 +28,22 @@ struct InitcapImpl
 private:
     static void array(const UInt8 * src, const UInt8 * src_end, UInt8 * dst)
     {
-        const auto flip_case_mask = 'A' ^ 'a';
-
-        auto is_lower_alpha = [](UInt8 c) { return c >= 'a' && c <= 'z'; };
-        auto is_upper_alpha = [](UInt8 c) { return c >= 'A' && c <= 'Z'; };
-        //auto is_digit = [](UInt8 c) { return c >= '0' && c <= '9'; };
-
-        bool prev_is_alpha = false;
+        bool prev_alphanum = false;
 
         for (; src < src_end; ++src, ++dst)
         {
-            bool lower = is_lower_alpha(*src);
-            bool is_alpha = lower || is_upper_alpha(*src);
-            if (!is_alpha)
-            {
-                *dst = *src;
-            }
-            else if (!prev_is_alpha)
-            {
-                if (lower)
-                    *dst = *src ^ flip_case_mask;
+            char c = *src;
+            bool alphanum = isAlphaNumericASCII(c);
+            if (alphanum && !prev_alphanum)
+                if (isAlphaASCII(c))
+                    *dst = toUpperIfAlphaASCII(c);
                 else
-                    *dst = *src;
-            }
+                    *dst = c;
+            else if (isAlphaASCII(c))
+                *dst = toLowerIfAlphaASCII(c);
             else
-            {
-                if (!lower)
-                    *dst = *src ^ flip_case_mask;
-                else
-                    *dst = *src;
-            }
-            prev_is_alpha = is_alpha;
+                *dst = c;
+            prev_alphanum = alphanum;
         }
     }
 };
--- tests/queries/0_stateless/02810_initcap.sql
+++ tests/queries/0_stateless/02810_initcap.sql
@@ -4,3 +4,4 @@ select initcap('hello');
 select initcap('hello world');
 select initcap('yeah, well, i`m gonna go build my own theme park');
 select initcap('CRC32IEEE is best function');
+select initcap('42oK');
