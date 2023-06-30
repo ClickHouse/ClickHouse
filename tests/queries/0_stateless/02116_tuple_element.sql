65b82dea47ae98da57bbd788f8844c9d5d17c987
Robert Schulze <robert@clickhouse.com>
2023-06-30 10:34:38 +0000

    Fix 02116_tuple_element with Analyzer

--- tests/queries/0_stateless/02116_tuple_element.sql
+++ tests/queries/0_stateless/02116_tuple_element.sql
@@ -17,8 +17,10 @@ EXPLAIN SYNTAX SELECT tupleElement(t1, 'a') FROM t_tuple_element;
 SELECT tupleElement(number, 1) FROM numbers(1); -- { serverError ILLEGAL_TYPE_OF_ARGUMENT }
 SELECT tupleElement(t1) FROM t_tuple_element; -- { serverError NUMBER_OF_ARGUMENTS_DOESNT_MATCH }
 SELECT tupleElement(t1, 'b') FROM t_tuple_element; -- { serverError NOT_FOUND_COLUMN_IN_BLOCK, UNKNOWN_IDENTIFIER }
-SELECT tupleElement(t1, 0) FROM t_tuple_element; -- { serverError ILLEGAL_INDEX }
-SELECT tupleElement(t1, 3) FROM t_tuple_element; -- { serverError ILLEGAL_INDEX }
+SELECT tupleElement(t1, 0) FROM t_tuple_element SETTINGS allow_experimental_analyzer = 0; -- { serverError ILLEGAL_INDEX }
+SELECT tupleElement(t1, 0) FROM t_tuple_element SETTINGS allow_experimental_analyzer = 1; -- { serverError NOT_FOUND_COLUMN_IN_BLOCK }
+SELECT tupleElement(t1, 3) FROM t_tuple_element SETTINGS allow_experimental_analyzer = 0; -- { serverError ILLEGAL_INDEX }
+SELECT tupleElement(t1, 3) FROM t_tuple_element SETTINGS allow_experimental_analyzer = 1; -- { serverError NOT_FOUND_COLUMN_IN_BLOCK }
 SELECT tupleElement(t1, materialize('a')) FROM t_tuple_element; -- { serverError ILLEGAL_TYPE_OF_ARGUMENT }
 
 SELECT t2.1 FROM t_tuple_element;
@@ -29,8 +31,10 @@ EXPLAIN SYNTAX SELECT tupleElement(t2, 1) FROM t_tuple_element;
 
 SELECT tupleElement(t2) FROM t_tuple_element; -- { serverError NUMBER_OF_ARGUMENTS_DOESNT_MATCH }
 SELECT tupleElement(t2, 'a') FROM t_tuple_element; -- { serverError NOT_FOUND_COLUMN_IN_BLOCK, UNKNOWN_IDENTIFIER }
-SELECT tupleElement(t2, 0) FROM t_tuple_element; -- { serverError ILLEGAL_INDEX }
-SELECT tupleElement(t2, 3) FROM t_tuple_element; -- { serverError ILLEGAL_INDEX }
+SELECT tupleElement(t2, 0) FROM t_tuple_element SETTINGS allow_experimental_analyzer = 0; -- { serverError ILLEGAL_INDEX }
+SELECT tupleElement(t2, 0) FROM t_tuple_element SETTINGS allow_experimental_analyzer = 1; -- { serverError NOT_FOUND_COLUMN_IN_BLOCK }
+SELECT tupleElement(t2, 3) FROM t_tuple_element SETTINGS allow_experimental_analyzer = 0; -- { serverError ILLEGAL_INDEX }
+SELECT tupleElement(t2, 3) FROM t_tuple_element SETTINGS allow_experimental_analyzer = 1; -- { serverError NOT_FOUND_COLUMN_IN_BLOCK }
 SELECT tupleElement(t2, materialize(1)) FROM t_tuple_element; -- { serverError ILLEGAL_TYPE_OF_ARGUMENT }
 
 DROP TABLE t_tuple_element;
