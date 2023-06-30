3f29bd1bd42f7ebff737d2196c285aa7be4c6989
kssenii <sumarokovakseniia@mail.ru>
2023-06-30 18:20:59 +0200

    Try fix test

--- tests/queries/0_stateless/02789_filesystem_cache_alignment.sh
+++ tests/queries/0_stateless/02789_filesystem_cache_alignment.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
-# Tags: no-fasttest, no-parallel, no-random-settings
+# Tags: no-fasttest, no-parallel, no-random-settings, no-random-merge-tree-settings
 
 CURDIR=$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)
 # shellcheck source=../shell_config.sh
