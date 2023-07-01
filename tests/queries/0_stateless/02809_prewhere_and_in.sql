9147f7607ce99a31b1e5f47d1aea39c8941bb31f
Alexander Gololobov <440544+davenger@users.noreply.github.com>
2023-07-01 09:19:54 +0200

    Fix test with analyzer

--- tests/queries/0_stateless/02809_prewhere_and_in.sql
+++ tests/queries/0_stateless/02809_prewhere_and_in.sql
@@ -16,37 +16,37 @@ AS SELECT * FROM numbers(10);
 SET optimize_move_to_prewhere=1;
 
 -- Queries with 'IN'
-SELECT * FROM (EXPLAIN SYNTAX
+SELECT substring(explain, 1, 13) FROM (EXPLAIN SYNTAX
      SELECT * FROM t_02809 WHERE a IN (SELECT * FROM system.one)
 ) WHERE explain LIKE '%WHERE%';
 
-SELECT * FROM (EXPLAIN SYNTAX
+SELECT substring(explain, 1, 13) FROM (EXPLAIN SYNTAX
      SELECT * FROM t_02809 WHERE a IN (1,2,3)
 ) WHERE explain LIKE '%WHERE%';
 
-SELECT * FROM (EXPLAIN SYNTAX
+SELECT substring(explain, 1, 13) FROM (EXPLAIN SYNTAX
      SELECT * FROM t_02809 WHERE a IN t_02809_set
 ) WHERE explain LIKE '%WHERE%';
 
-SELECT * FROM (EXPLAIN SYNTAX
+SELECT substring(explain, 1, 13) FROM (EXPLAIN SYNTAX
      SELECT * FROM t_02809 WHERE a IN t_02809_aux
 ) WHERE explain LIKE '%WHERE%';
 
 
 -- Queries with 'NOT IN'
-SELECT * FROM (EXPLAIN SYNTAX
+SELECT substring(explain, 1, 17) FROM (EXPLAIN SYNTAX
      SELECT * FROM t_02809 WHERE b NOT IN (SELECT * FROM system.one)
 ) WHERE explain LIKE '%WHERE%';
 
-SELECT * FROM (EXPLAIN SYNTAX
+SELECT substring(explain, 1, 17) FROM (EXPLAIN SYNTAX
      SELECT * FROM t_02809 WHERE b NOT IN (1,2,3)
 ) WHERE explain LIKE '%WHERE%';
 
-SELECT * FROM (EXPLAIN SYNTAX
+SELECT substring(explain, 1, 17) FROM (EXPLAIN SYNTAX
      SELECT * FROM t_02809 WHERE b NOT IN t_02809_set
 ) WHERE explain LIKE '%WHERE%';
 
-SELECT * FROM (EXPLAIN SYNTAX
+SELECT substring(explain, 1, 17) FROM (EXPLAIN SYNTAX
      SELECT * FROM t_02809 WHERE b NOT IN t_02809_aux
 ) WHERE explain LIKE '%WHERE%';
 
