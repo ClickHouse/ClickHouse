ff9748bb2b5274d43e2fa1bc6d020685e90d0ab7
kevinyhzou <kevinyunhe8@gmail.com>
2023-07-03 12:09:34 +0800

    test

--- tests/queries/0_stateless/02810_csv_with_input_too_few_fields.sh
+++ tests/queries/0_stateless/02810_csv_with_input_too_few_fields.sh
@@ -7,7 +7,7 @@ CURDIR=$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)
 . "$CURDIR"/../shell_config.sh
 
 $CLICKHOUSE_CLIENT -q "drop table if exists test_tbl"
-$CLICKHOUSE_CLIENT -q "create table test_tbl (x UInt32, y String, z String) engine=MergeTree order by x"
-cat $CURDIR/data_csv/csv_with_diff_number_fields.csv | ${CLICKHOUSE_CLIENT} -q "INSERT INTO test_tbl SETTINGS format_csv_delimiter=' ', input_format_csv_allow_set_column_default_value_if_no_input=true FORMAT CSV"
+$CLICKHOUSE_CLIENT -q "create table test_tbl (x String, y String, z String, w String) engine=MergeTree order by x"
+cat $CURDIR/data_csv/csv_with_diff_number_fields.csv | ${CLICKHOUSE_CLIENT} -q "INSERT INTO test_tbl SETTINGS input_format_csv_allow_set_column_default_value_if_no_input=true FORMAT CSV"
 $CLICKHOUSE_CLIENT -q "select * from test_tbl"
 $CLICKHOUSE_CLIENT -q "drop table test_tbl"; 
