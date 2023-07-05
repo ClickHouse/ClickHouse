bce8650305bb79b6fa6bbe128f03b7514a820fee
Arthur Passos <arthur.ti@outlook.com>
2023-07-05 11:09:22 -0300

    add a few tests, still incomplete

---
+++ tests/integration/test_s3_table_function_with_proxy/test.py
@@ -0,0 +1,94 @@
+import logging
+import os
+import time
+
+import pytest
+from helpers.cluster import ClickHouseCluster
+
+
+# Runs simple proxy resolver in python env container.
+def run_resolver(cluster):
+    container_id = cluster.get_container_id("resolver")
+    current_dir = os.path.dirname(__file__)
+    cluster.copy_file_to_container(
+        container_id,
+        os.path.join(current_dir, "proxy-resolver", "resolver.py"),
+        "resolver.py",
+    )
+    cluster.exec_in_container(container_id, ["python", "resolver.py"], detach=True)
+
+@pytest.fixture(scope="module")
+def cluster():
+    try:
+        cluster = ClickHouseCluster(__file__)
+
+        # cluster.add_instance("minio_node", with_minio=True)
+
+        # cluster.add_instance(
+        #     "proxy_list_node", main_configs=["configs/config.d/proxy_list.xml"]
+        # )
+
+        cluster.add_instance(
+            "remote_proxy_node", main_configs=["configs/config.d/proxy_remote.xml"], with_minio=True
+        )
+
+        logging.info("Starting cluster...")
+        cluster.start()
+        logging.info("Cluster started")
+
+        run_resolver(cluster)
+        logging.info("Proxy resolver started")
+
+        yield cluster
+    finally:
+        cluster.shutdown()
+
+def check_proxy_logs(cluster, proxy_instance, http_methods={"POST", "PUT", "GET"}):
+    for i in range(10):
+        logs = cluster.get_container_logs(proxy_instance)
+        # Check with retry that all possible interactions with Minio are present
+        for http_method in http_methods:
+            if logs.find(http_method + " http://minio1") >= 0:
+                return
+            time.sleep(1)
+        else:
+            assert False, "http method not found in logs"
+
+# def test_s3_with_proxy_list(cluster):
+#     node = cluster.instances["proxy_list_node"]
+#
+#     node.query(
+#         """
+#         INSERT INTO FUNCTION
+#         s3('http://minio1:9001/root/data/ch-proxy-test/test.csv', 'minio', 'minio123', 'CSV', 'key String, value String')
+#         VALUES ('color','red'),('size','10')
+#         """
+#     )
+#
+#     assert (
+#         node.query("SELECT * FROM s3('http://minio1:9001/root/data/ch-proxy-test/test.csv', 'minio', 'minio123', 'CSV') FORMAT Values")
+#         == "('color','red'),('size','10')"
+#     )
+#
+#     for proxy in ["proxy1", "proxy2"]:
+#         check_proxy_logs(cluster, proxy, ["PUT", "GET"])
+
+
+def test_s3_with_remote_resolver(cluster):
+    node = cluster.instances["remote_proxy_node"]
+
+    node.query(
+        """
+        INSERT INTO FUNCTION
+        s3('http://minio1:9001/root/data/ch-proxy-test/test.csv', 'minio', 'minio123', 'CSV', 'key String, value String')
+        VALUES ('color','red'),('size','10')
+        """
+    )
+
+    assert (
+        node.query("SELECT * FROM s3('http://minio1:9001/root/data/ch-proxy-test/test.csv', 'minio', 'minio123', 'CSV') FORMAT Values")
+        == "('color','red'),('size','10')"
+    )
+
+    for proxy in ["proxy1", "proxy2"]:
+        check_proxy_logs(cluster, proxy, ["PUT", "GET"])
