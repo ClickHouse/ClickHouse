cccc73b8a7c1350626289d01058a9f6b469e3fc2
Duc Canh Le <duccanh.le@ahrefs.com>
2023-07-05 05:30:43 +0000

    ch: add max_execution_time_leaf

--- src/Interpreters/ClusterProxy/SelectStreamFactory.cpp
+++ src/Interpreters/ClusterProxy/SelectStreamFactory.cpp
@@ -14,6 +14,8 @@
 
 #include <Client/IConnections.h>
 #include <Common/logger_useful.h>
+#include <Parsers/ASTSelectQuery.h>
+#include <Parsers/ASTSetQuery.h>
 #include <Processors/QueryPlan/QueryPlan.h>
 #include <Processors/QueryPlan/ReadFromRemote.h>
 #include <Processors/QueryPlan/ExpressionStep.h>
@@ -113,25 +115,47 @@ void SelectStreamFactory::createForShard(
     auto it = objects_by_shard.find(shard_info.shard_num);
     if (it != objects_by_shard.end())
         replaceMissedSubcolumnsByConstants(storage_snapshot->object_columns, it->second, query_ast);
+    
+    const auto & settings = context->getSettingsRef();    
+    ASTPtr query;
+    
+    if (settings.max_execution_time_leaf.value != 0)
+    {
+        query = query_ast->clone();
+        /// Replace 'max_execution_time' of this sub-query with 'max_execution_time_leaf' and 'timeout_oferflow_mode'
+        /// with 'timeout_overflow_mode_leaf'
+        if (auto * select = query->as<ASTSelectQuery>())
+        {
+            auto query_settings = select->settings();
+            if (!query_settings)
+                query_settings = std::make_shared<ASTSetQuery>();
+            
+            query_settings->as<ASTSetQuery &>().is_standalone = false;
+            auto & changes = query_settings->as<ASTSetQuery &>().changes;
+            changes.setSetting("max_execution_time", settings.max_execution_time_leaf.value.totalSeconds());
+            changes.setSetting("timeout_overflow_mode", settings.timeout_overflow_mode_leaf.toString());
+            select->setExpression(ASTSelectQuery::Expression::SETTINGS, std::move(query_settings));
+        }
+    }
+    else
+        query = query_ast;
 
     auto emplace_local_stream = [&]()
     {
         local_plans.emplace_back(createLocalPlan(
-            query_ast, header, context, processed_stage, shard_info.shard_num, shard_count, /*replica_num=*/0, /*replica_count=*/0, /*coordinator=*/nullptr));
+            query, header, context, processed_stage, shard_info.shard_num, shard_count, /*replica_num=*/0, /*replica_count=*/0, /*coordinator=*/nullptr));
     };
 
     auto emplace_remote_stream = [&](bool lazy = false, time_t local_delay = 0)
     {
         remote_shards.emplace_back(Shard{
-            .query = query_ast,
+            .query = query,
             .header = header,
             .shard_info = shard_info,
             .lazy = lazy,
             .local_delay = local_delay,
         });
     };
-
-    const auto & settings = context->getSettingsRef();
 
     if (settings.prefer_localhost_replica && shard_info.isLocal())
     {
