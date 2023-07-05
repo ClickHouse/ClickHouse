dbd721f8784694e340b0489cd2cc708f099c586e
Nikolai Kochetov <KochetovNicolai@users.noreply.github.com>
2023-07-05 18:14:52 +0200

    Update 02811_read_in_order_and_array_join_bug.sql

--- tests/queries/0_stateless/02811_read_in_order_and_array_join_bug.sql
+++ tests/queries/0_stateless/02811_read_in_order_and_array_join_bug.sql
@@ -12,6 +12,6 @@ create table test_array_joins
 insert into test_array_joins (id,arr_1, arr_2, arr_3, arr_4)
 SELECT number,array(randomPrintableASCII(3)),array(randomPrintableASCII(3)),array(randomPrintableASCII(3)),array(randomPrintableASCII(3))
 from numbers(1000);
-create view v4test_array_joins as SELECT * from default.test_array_joins where id != 10;
+create view v4test_array_joins as SELECT * from test_array_joins where id != 10;
 select * from v4test_array_joins array join columns('^arr') where match(arr_4,'a') and id < 100 order by id format Null settings optimize_read_in_order = 1;
 
