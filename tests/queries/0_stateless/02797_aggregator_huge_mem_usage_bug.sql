137263c9de56ceaf3e6d8a3e3027f2717438d228
Nikita Taranov <nikita.taranov@clickhouse.com>
2023-07-05 15:14:32 +0200

    add test

---
+++ tests/queries/0_stateless/02797_aggregator_huge_mem_usage_bug.sql
@@ -0,0 +1,8 @@
+create view v (s LowCardinality(String), n UInt8) as select 'test' as s, toUInt8(number) as n from numbers(10000000);
+
+-- this is what allows mem usage to go really high
+set max_block_size=10000000000;
+
+set max_memory_usage_for_user = '1Gi';
+
+select s, sum(n) from v group by s format Null;
