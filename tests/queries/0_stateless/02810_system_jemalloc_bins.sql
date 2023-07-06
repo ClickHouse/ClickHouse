257f7b594c5a0b2bce09211cf6908ecf0c025143
Alexander Gololobov <440544+davenger@users.noreply.github.com>
2023-07-06 09:09:55 +0200

    Better naming

--- tests/queries/0_stateless/02810_system_jemalloc_bins.sql
+++ tests/queries/0_stateless/02810_system_jemalloc_bins.sql
@@ -3,8 +3,8 @@ WITH
     (SELECT count() FROM system.jemalloc_bins) AS total_bins,
     (SELECT count() FROM system.jemalloc_bins WHERE large) AS large_bins,
     (SELECT count() FROM system.jemalloc_bins WHERE NOT large) AS small_bins,
-    (SELECT sum(size * (nmalloc - ndalloc)) FROM system.jemalloc_bins WHERE large) AS large_allocated_bytes,
-    (SELECT sum(size * (nmalloc - ndalloc)) FROM system.jemalloc_bins WHERE NOT large) AS small_allocated_bytes
+    (SELECT sum(size * (allocations - deallocations)) FROM system.jemalloc_bins WHERE large) AS large_allocated_bytes,
+    (SELECT sum(size * (allocations - deallocations)) FROM system.jemalloc_bins WHERE NOT large) AS small_allocated_bytes
 SELECT
     (total_bins > 0) = jemalloc_enabled,
     (large_bins > 0) = jemalloc_enabled,
