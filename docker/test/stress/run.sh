63ac2eeb99f38c5217a886bd2cb709f4f1563ee0
avogar <avogar@clickhouse.com>
2023-07-06 18:05:09 +0000

    Fix

--- docker/test/stress/run.sh
+++ docker/test/stress/run.sh
@@ -14,6 +14,7 @@ ln -s /usr/share/clickhouse-test/clickhouse-test /usr/bin/clickhouse-test
 
 # Stress tests and upgrade check uses similar code that was placed
 # in a separate bash library. See tests/ci/stress_tests.lib
+source /usr/share/clickhouse-test/ci/attach_gdb.lib
 source /usr/share/clickhouse-test/ci/stress_tests.lib
 
 install_packages package_folder
