4471e1c013578b70e1995842c73cd6dba965b950
Antonio Andelic <antonio@clickhouse.com>
2023-07-07 11:41:01 +0000

    Remove maybe_unused

--- src/Common/ThreadStatus.h
+++ src/Common/ThreadStatus.h
@@ -224,7 +224,7 @@ private:
 
     Poco::Logger * log = nullptr;
 
-    [[maybe_unused]] bool check_current_thread_on_destruction;
+    bool check_current_thread_on_destruction;
 
 public:
     explicit ThreadStatus(bool check_current_thread_on_destruction_ = true);
