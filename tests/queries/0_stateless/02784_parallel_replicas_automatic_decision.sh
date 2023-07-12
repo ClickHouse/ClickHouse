985d2bb89d927b15d597613def0004b588145e78
Raúl Marín <git@rmr.ninja>
2023-07-12 17:47:31 +0200

    Reduce number of tests to avoid taking too much time

--- tests/queries/0_stateless/02784_parallel_replicas_automatic_decision.sh
+++ tests/queries/0_stateless/02784_parallel_replicas_automatic_decision.sh
@@ -68,7 +68,6 @@ query_id_base="02784_automatic_parallel_replicas-$CLICKHOUSE_DATABASE"
 
 #### Reading 10M rows without filters
 whole_table_query="SELECT sum(number) FROM test_parallel_replicas_automatic_count format Null"
-run_query_with_pure_parallel_replicas "${query_id_base}_whole_table_0" 0 "$whole_table_query"
 run_query_with_pure_parallel_replicas "${query_id_base}_whole_table_10M" 10000000 "$whole_table_query"
 run_query_with_pure_parallel_replicas "${query_id_base}_whole_table_6M" 6000000 "$whole_table_query" # 1.6 replicas -> 1 replica -> No parallel replicas
 run_query_with_pure_parallel_replicas "${query_id_base}_whole_table_5M" 5000000 "$whole_table_query"
@@ -76,31 +75,24 @@ run_query_with_pure_parallel_replicas "${query_id_base}_whole_table_1M" 1000000 
 
 ##### Reading 2M rows without filters as partition (p=3) is pruned completely
 query_with_partition_pruning="SELECT sum(number) FROM test_parallel_replicas_automatic_count WHERE p != 3 format Null"
-run_query_with_pure_parallel_replicas "${query_id_base}_pruning_0" 0 "$query_with_partition_pruning"
 run_query_with_pure_parallel_replicas "${query_id_base}_pruning_10M" 10000000 "$query_with_partition_pruning"
 run_query_with_pure_parallel_replicas "${query_id_base}_pruning_1M" 1000000 "$query_with_partition_pruning"
 run_query_with_pure_parallel_replicas "${query_id_base}_pruning_500k" 500000 "$query_with_partition_pruning"
 
 #### Reading ~500k rows as index filter should prune granules from partition=1 and partition=2, and drop p3 completely
 query_with_index="SELECT sum(number) FROM test_parallel_replicas_automatic_count WHERE number < 500_000 format Null"
-run_query_with_pure_parallel_replicas "${query_id_base}_index_0" 0 "$query_with_index"
 run_query_with_pure_parallel_replicas "${query_id_base}_index_1M" 1000000 "$query_with_index"
-run_query_with_pure_parallel_replicas "${query_id_base}_index_300k" 300000 "$query_with_index"
 run_query_with_pure_parallel_replicas "${query_id_base}_index_200k" 200000 "$query_with_index"
 run_query_with_pure_parallel_replicas "${query_id_base}_index_100k" 100000 "$query_with_index"
 
 #### Reading 1M (because of LIMIT)
 limit_table_query="SELECT sum(number) FROM (SELECT number FROM test_parallel_replicas_automatic_count LIMIT 1_000_000) format Null"
-run_query_with_pure_parallel_replicas "${query_id_base}_limit_0" 0 "$limit_table_query"
 run_query_with_pure_parallel_replicas "${query_id_base}_limit_10M" 10000000 "$limit_table_query"
 run_query_with_pure_parallel_replicas "${query_id_base}_limit_1M" 1000000 "$limit_table_query"
 run_query_with_pure_parallel_replicas "${query_id_base}_limit_500k" 500000 "$limit_table_query"
 
 #### Reading 10M (because of LIMIT is applied after aggregations)
 limit_agg_table_query="SELECT sum(number) FROM test_parallel_replicas_automatic_count LIMIT 1_000_000 format Null"
-run_query_with_pure_parallel_replicas "${query_id_base}_useless_limit_0" 0 "$limit_agg_table_query"
-run_query_with_pure_parallel_replicas "${query_id_base}_useless_limit_10M" 10000000 "$limit_agg_table_query"
-run_query_with_pure_parallel_replicas "${query_id_base}_useless_limit_1M" 1000000 "$limit_agg_table_query"
 run_query_with_pure_parallel_replicas "${query_id_base}_useless_limit_500k" 500000 "$limit_agg_table_query"
 
 #### JOIN (left side 10M, right side 1M)
@@ -117,7 +109,6 @@ run_query_with_pure_parallel_replicas "${query_id_base}_simple_join_300k" 400000
 
 #### If the filter does not help, it shouldn't disable parallel replicas. Table has 1M rows, filter removes all rows
 helpless_filter_query="SELECT sum(number) FROM test_parallel_replicas_automatic_count_right_side WHERE value = 42 format Null"
-run_query_with_pure_parallel_replicas "${query_id_base}_helpless_filter_0" 0 "$helpless_filter_query"
 run_query_with_pure_parallel_replicas "${query_id_base}_helpless_filter_2M" 2000000 "$helpless_filter_query"
 run_query_with_pure_parallel_replicas "${query_id_base}_helpless_filter_500000" 500000 "$helpless_filter_query"
 run_query_with_pure_parallel_replicas "${query_id_base}_helpless_filter_100000" 100000 "$helpless_filter_query"
