70e7d34f15aedd1d3cd8cffb6832d9dc6b8235de
Victor Krasnov <v.krasnov@arenadata.io>
2023-07-16 18:44:03 +0800

    Include email and number of commits in the  contents of system.contributors

--- src/Storages/System/StorageSystemContributors.cpp
+++ src/Storages/System/StorageSystemContributors.cpp
@@ -2,10 +2,12 @@
 
 #include <algorithm>
 #include <DataTypes/DataTypeString.h>
+#include <DataTypes/DataTypesNumber.h>
 #include <Common/thread_local_rng.h>
 
 
-extern const char * auto_contributors[];
+extern size_t auto_contributors_size;
+extern std::tuple<const char *, const char *, size_t> auto_contributors[];
 
 namespace DB
 {
@@ -13,18 +15,26 @@ NamesAndTypesList StorageSystemContributors::getNamesAndTypes()
 {
     return {
         {"name", std::make_shared<DataTypeString>()},
+        {"email", std::make_shared<DataTypeString>()},
+        {"commits", std::make_shared<DataTypeUInt32>()},
     };
 }
 
 void StorageSystemContributors::fillData(MutableColumns & res_columns, ContextPtr, const SelectQueryInfo &) const
 {
-    std::vector<const char *> contributors;
-    for (auto * it = auto_contributors; *it; ++it)
-        contributors.emplace_back(*it);
+    std::vector<std::tuple<const char *, const char *, size_t>> contributors;
+    for (size_t i = 0; i < auto_contributors_size; ++i)
+    {
+        contributors.emplace_back(auto_contributors[i]);
+    }
 
     std::shuffle(contributors.begin(), contributors.end(), thread_local_rng);
 
-    for (auto & it : contributors)
-        res_columns[0]->insert(String(it));
+    for (const auto & item : contributors)
+    {
+        res_columns[0]->insert(String{std::get<0>(item)});
+        res_columns[1]->insert(String{std::get<1>(item)});
+        res_columns[2]->insert(std::get<2>(item));
+    }
 }
 }
