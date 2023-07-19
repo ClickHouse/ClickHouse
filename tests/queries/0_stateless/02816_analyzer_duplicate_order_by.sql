4a6772869608d4e5c2a2b28bc45a6c04d202fa63
JackyWoo <wuchienchao@qq.com>
2023-07-19 10:53:35 +0800

    add tests for group by

--- tests/queries/0_stateless/02816_analyzer_duplicate_order_by.sql
+++ tests/queries/0_stateless/02816_analyzer_duplicate_order_by.sql
@@ -37,31 +37,19 @@ set optimize_duplicate_order_by_and_distinct = 1;
 EXPLAIN QUERY TREE SELECT *
 FROM
 (
-    SELECT *
-    FROM
-    (
-        SELECT number % 2
-        AS number
-        FROM numbers(3)
-        ORDER BY number
-    )
-    ORDER BY number
+   SELECT *
+   FROM numbers(3)
+   ORDER BY number
 )
-ORDER BY number;
+GROUP BY number;
 
 set optimize_duplicate_order_by_and_distinct = 0;
 
 EXPLAIN QUERY TREE SELECT *
 FROM
 (
-    SELECT *
-    FROM
-    (
-        SELECT number % 2
-        AS number
-        FROM numbers(3)
-        ORDER BY number
-    )
-    ORDER BY number
+   SELECT *
+   FROM numbers(3)
+   ORDER BY number
 )
-ORDER BY number;
+GROUP BY number;
