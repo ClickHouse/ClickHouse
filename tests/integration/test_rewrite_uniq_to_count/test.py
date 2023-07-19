6c6ef25dda1b9e3aa3dd17d948e2f678f7912487
JackyWoo <wuchienchao@qq.com>
2023-07-19 18:46:20 +0800

    support alias for new analyzer
    (cherry picked from commit 08409059cc198873ffbf11060bfdabaa0c74f07f)

--- tests/integration/test_rewrite_uniq_to_count/test.py
+++ tests/integration/test_rewrite_uniq_to_count/test.py
@@ -83,13 +83,13 @@ def test_rewrite_distinct(started_cluster):
     )
 
     # test select expression alias
-    check_by_old_analyzer(
-        "SELECT uniq(a) FROM (SELECT DISTINCT test_rewrite_uniq_to_count.a as alias_of_a FROM test_rewrite_uniq_to_count) t",
+    check(
+        "SELECT uniq(alias_of_a) FROM (SELECT DISTINCT test_rewrite_uniq_to_count.a as alias_of_a FROM test_rewrite_uniq_to_count) t",
         3,
     )
 
     # test select expression alias
-    check_by_old_analyzer(
+    check(
         "SELECT uniq(alias_of_a) FROM (SELECT DISTINCT a as alias_of_a FROM test_rewrite_uniq_to_count) t",
         3,
     )
@@ -109,19 +109,19 @@ def test_rewrite_group_by(started_cluster):
     )
 
     # test select expression alias
-    check_by_old_analyzer(
+    check(
         "SELECT uniq(t.alias_of_a) FROM (SELECT a as alias_of_a, sum(b) FROM test_rewrite_uniq_to_count GROUP BY a) t",
         3,
     )
 
     # test select expression alias
-    check_by_old_analyzer(
-        "SELECT uniq(t.a) FROM (SELECT a as alias_of_a, sum(b) FROM test_rewrite_uniq_to_count GROUP BY alias_of_a) t",
+    check(
+        "SELECT uniq(t.alias_of_a) FROM (SELECT a as alias_of_a, sum(b) FROM test_rewrite_uniq_to_count GROUP BY alias_of_a) t",
         3,
     )
 
     # test select expression alias
-    check_by_old_analyzer(
-        "SELECT uniq(t.alias_of_a) FROM (SELECT a as alias_of_a, sum(b) FROM test_rewrite_uniq_to_count GROUP BY alias_of_a) t",
+    check(
+        "SELECT uniq(t.alias_of_a) FROM (SELECT a as alias_of_a, sum(b) FROM test_rewrite_uniq_to_count GROUP BY a) t",
         3,
     )
