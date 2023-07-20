0c36e0febaa6dbf057b27d44eef9220cc30d84db
xiedeyantu <czjourney@163.com>
2023-07-20 19:58:51 +0800

    ignore test

--- tests/queries/0_stateless/02813_system_events_and_metrics_add_alias.sql
+++
@@ -1,4 +0,0 @@
-show create table system.metrics;
-select equals((select count() from system.metrics where name=metric) as r1, (select count() from system.metrics) as r2);
-show create table system.events;
-select equals((select count() from system.events where name=event) as r1, (select count() from system.events) as r2);
