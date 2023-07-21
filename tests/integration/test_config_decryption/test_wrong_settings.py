f2d57ccb8f699b85f87680bd25bb2ad8de4225c8
Roman Vasin <r.vasin@arenadata.io>
2023-07-21 13:31:42 +0000

    Fix black formatting

--- tests/integration/test_config_decryption/test_wrong_settings.py
+++ tests/integration/test_config_decryption/test_wrong_settings.py
@@ -28,7 +28,8 @@ def test_invalid_chars():
 
 def test_no_encryption_key():
     start_clickhouse(
-        "configs/config_no_encryption_key.xml", "There is no key 0 in config for AES_128_GCM_SIV encryption codec"
+        "configs/config_no_encryption_key.xml",
+        "There is no key 0 in config for AES_128_GCM_SIV encryption codec",
     )
 
 
