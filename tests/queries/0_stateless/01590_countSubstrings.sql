8d4c840e2da0401787bafa2239907ff59160a003
Alexander Gololobov <440544+davenger@users.noreply.github.com>
2023-07-21 15:15:35 +0200

    Some more cases

--- tests/queries/0_stateless/01590_countSubstrings.sql
+++ tests/queries/0_stateless/01590_countSubstrings.sql
@@ -14,6 +14,9 @@ select countSubstrings('', '');
 select countSubstrings('.', '');
 select countSubstrings(toString(number), '') from numbers(1);
 select countSubstrings('', toString(number)) from numbers(1);
+select countSubstrings('aaa', materialize(''));
+select countSubstrings(materialize('aaa'), '');
+select countSubstrings(materialize('aaa'), materialize(''));
 
 select 'char';
 select countSubstrings('foobar.com', '.');
