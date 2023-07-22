2dc526e03c1c0fbe9dfb089ca224fd1b2e39e532
robot-clickhouse <robot-clickhouse@users.noreply.github.com>
2023-07-22 17:05:39 +0000

    Backport #52449 to 23.6: Remove `hashid`

--- docker/test/fasttest/run.sh
+++ docker/test/fasttest/run.sh
@@ -141,7 +141,6 @@ function clone_submodules
             contrib/jemalloc
             contrib/replxx
             contrib/wyhash
-            contrib/hashidsxx
             contrib/c-ares
             contrib/morton-nd
             contrib/xxHash
