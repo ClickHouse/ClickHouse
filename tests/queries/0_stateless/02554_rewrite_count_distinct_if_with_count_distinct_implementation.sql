322e850ac2394b4744d2600c05ddd3344ec1c132
flynn <fenglv15@mails.ucas.ac.cn>
2023-07-22 17:47:27 +0000

    fix

--- tests/queries/0_stateless/02554_rewrite_count_distinct_if_with_count_distinct_implementation.sql
+++ tests/queries/0_stateless/02554_rewrite_count_distinct_if_with_count_distinct_implementation.sql
@@ -6,13 +6,3 @@ EXPLAIN SYNTAX SELECT countDistinctIf(number % 10, number % 5 = 2) FROM numbers(
 SET rewrite_count_distinct_if_with_count_distinct_implementation = 1;
 SELECT countDistinctIf(number % 10, number % 5 = 2) FROM numbers(1000);
 EXPLAIN SYNTAX SELECT countDistinctIf(number % 10, number % 5 = 2) FROM numbers(1000);
-
-SET allow_experimental_analyzer = 1;
-
-SET rewrite_count_distinct_if_with_count_distinct_implementation = 0;
-SELECT countDistinctIf(number % 10, number % 5 = 2) FROM numbers(1000);
-EXPLAIN SYNTAX SELECT countDistinctIf(number % 10, number % 5 = 2) FROM numbers(1000);
-
-SET rewrite_count_distinct_if_with_count_distinct_implementation = 1;
-SELECT countDistinctIf(number % 10, number % 5 = 2) FROM numbers(1000);
-EXPLAIN SYNTAX SELECT countDistinctIf(number % 10, number % 5 = 2) FROM numbers(1000);
