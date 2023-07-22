028f6fcf2d2c321b5d768c10fc921d1bcc7798f1
Igor Nikonov <igor@clickhouse.com>
2023-07-22 17:43:22 +0000

    Add test with materialize() + fix

--- tests/queries/0_stateless/02810_fix_remove_dedundant_distinct_view.sql
+++ tests/queries/0_stateless/02810_fix_remove_dedundant_distinct_view.sql
@@ -19,4 +19,12 @@ FROM
 )
 WHERE explain ILIKE '%distinct%';
 
-SELECT DISTINCT x FROM tab_v;
+SELECT DISTINCT x FROM tab_v ORDER BY x;
+
+-- explicitly checking that materialize() doesn't affect the result, - redundant DISTINCT is still removed
+SELECT count()
+FROM
+(
+    EXPLAIN SELECT DISTINCT x FROM (SELECT materialize(x) as x FROM (select DISTINCT x from tab))
+)
+WHERE explain ILIKE '%distinct%';
