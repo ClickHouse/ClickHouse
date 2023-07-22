7004a949ac80b5cca50f656261c4a8a8a29d00c1
rajat mohan <77490034+rajatmohan22@users.noreply.github.com>
2023-07-22 22:38:23 +0530

    Update 00469_array_remove.sql
    removed '->'

--- tests/queries/0_stateless/00469_array_remove.sql
+++ tests/queries/0_stateless/00469_array_remove.sql
@@ -1,2 +1,17 @@
-SELECT sum(length(arr)) FROM (SELECT arrayRemove(range(number % 10), x -> length(toString(x)) % 2 = 0) AS arr FROM (SELECT * FROM system.numbers LIMIT 1000));
-SELECT sum(length(arr)) FROM (SELECT arrayRemove(range(number % 10), x -> length(x) % 2 = 0) AS arr FROM (SELECT * FROM system.numbers LIMIT 1000));
+SELECT SUM(LENGTH(arr))
+FROM (
+  SELECT
+    arrayRemove(range(number % 10), x)
+    FROM (SELECT * FROM system.numbers LIMIT 1000)
+    CROSS JOIN lateral arrayEnumerate(range(number % 10)) AS t(idx, x)
+    WHERE LENGTH(toString(x)) % 2 = 0
+) AS tmp(arr);
+
+SELECT SUM(LENGTH(arr))
+FROM (
+  SELECT
+    arrayRemove(range(number % 10), x)
+    FROM (SELECT * FROM system.numbers LIMIT 1000)
+    CROSS JOIN lateral arrayEnumerate(range(number % 10)) AS t(idx, x)
+    WHERE LENGTH(x) % 2 = 0
+) AS tmp(arr);
