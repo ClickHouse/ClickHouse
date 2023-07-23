47679b01239940e98846206ffa34964a544401bc
Alexey Milovidov <milovidov@clickhouse.com>
2023-07-23 06:09:15 +0200

    Fix errors

--- src/Storages/System/StorageSystemTimeZones.cpp
+++ src/Storages/System/StorageSystemTimeZones.cpp
@@ -4,8 +4,7 @@
 #include <DataTypes/DataTypeString.h>
 
 
-struct TimeZone { const char * name; const unsigned char * data; size_t size; };
-extern TimeZone auto_time_zones[];
+extern const char * auto_time_zones[];
 
 namespace DB
 {
@@ -18,7 +17,7 @@ NamesAndTypesList StorageSystemTimeZones::getNamesAndTypes()
 
 void StorageSystemTimeZones::fillData(MutableColumns & res_columns, ContextPtr, const SelectQueryInfo &) const
 {
-    for (auto * it = auto_time_zones; it->name != nullptr; ++it)
-        res_columns[0]->insert(String(it->name));
+    for (auto * it = auto_time_zones; *it; ++it)
+        res_columns[0]->insert(String(*it));
 }
 }
