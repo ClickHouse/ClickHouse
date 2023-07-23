db246743efe06fd79951e7050a3c4bee40b06db3
rajatmohan22 <lkrajath22@gmail.com>
2023-07-23 12:03:42 +0530

    modify files

--- tests/queries/0_stateless/02819_array_remove.sql
+++ tests/queries/0_stateless/02819_array_remove.sql
@@ -1,17 +1,2 @@
-SELECT SUM(LENGTH(arr))
-FROM (
-  SELECT
-    arrayRemove(range(number % 10), x)
-    FROM (SELECT * FROM system.numbers LIMIT 1000)
-    CROSS JOIN lateral arrayEnumerate(range(number % 10)) AS t(idx, x)
-    WHERE LENGTH(toString(x)) % 2 = 0
-) AS tmp(arr);
-
-SELECT SUM(LENGTH(arr))
-FROM (
-  SELECT
-    arrayRemove(range(number % 10), x)
-    FROM (SELECT * FROM system.numbers LIMIT 1000)
-    CROSS JOIN lateral arrayEnumerate(range(number % 10)) AS t(idx, x)
-    WHERE LENGTH(x) % 2 = 0
-) AS tmp(arr);
+SELECT SUM(LENGTH(arr)) FROM (SELECT arrayMap(x -> toString(x), range(number % 10)) AS arr FROM (SELECT * FROM system.numbers LIMIT 1000));
+SELECT SUM(LENGTH(arr)) FROM (SELECT range(number % 10) AS arr FROM (SELECT * FROM system.numbers LIMIT 1000));
