27f3c5cfa27f9aef5df563a9fd90089963afe26c
taiyang-li <654010905@qq.com>
2023-07-26 10:53:07 +0800

    change as requested

--- src/Functions/startsWithUTF8.cpp
+++ src/Functions/startsWithUTF8.cpp
@@ -10,7 +10,12 @@ using FunctionStartsWithUTF8 = FunctionStartsEndsWith<NameStartsWithUTF8>;
 
 REGISTER_FUNCTION(StartsWithUTF8)
 {
-    factory.registerFunction<FunctionStartsWithUTF8>();
+    factory.registerFunction<FunctionStartsWithUTF8>(FunctionDocumentation{
+        .description = R"(
+Returns whether string `str` starts with `prefix`, the difference between `startsWithUTF8` and `startsWith` is that `startsWithUTF8` match `str` and `suffix` by UTF-8 characters.
+        )",
+        .examples{{"startsWithUTF8", "select startsWithUTF8('富强民主文明和谐', '富强');", ""}},
+        .categories{"String"}});
 }
 
 }
