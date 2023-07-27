8c14c4b5e8925c6793c04cced5ebe64668ba3944
Robert Schulze <robert@clickhouse.com>
2023-07-27 14:59:28 +0000

    Remove unneeded readBinary() specializations + update docs
    The default instantiation
    
        template <typename T>
        requires is_arithmetic_v<T>
        inline void readBinary(T & x, ReadBuffer & buf) { readPODBinary(x, buf); }
    
    covers Int32, Int128, Int256, UInt32, UInt128 and UInt256 cases already.

--- src/IO/ReadHelpers.h
+++ src/IO/ReadHelpers.h
@@ -1096,12 +1096,6 @@ inline void readBinary(bool & x, ReadBuffer & buf)
 }
 
 inline void readBinary(String & x, ReadBuffer & buf) { readStringBinary(x, buf); }
-inline void readBinary(Int32 & x, ReadBuffer & buf) { readPODBinary(x, buf); }
-inline void readBinary(Int128 & x, ReadBuffer & buf) { readPODBinary(x, buf); }
-inline void readBinary(Int256 & x, ReadBuffer & buf) { readPODBinary(x, buf); }
-inline void readBinary(UInt32 & x, ReadBuffer & buf) { readPODBinary(x, buf); }
-inline void readBinary(UInt128 & x, ReadBuffer & buf) { readPODBinary(x, buf); }
-inline void readBinary(UInt256 & x, ReadBuffer & buf) { readPODBinary(x, buf); }
 inline void readBinary(Decimal32 & x, ReadBuffer & buf) { readPODBinary(x, buf); }
 inline void readBinary(Decimal64 & x, ReadBuffer & buf) { readPODBinary(x, buf); }
 inline void readBinary(Decimal128 & x, ReadBuffer & buf) { readPODBinary(x, buf); }
