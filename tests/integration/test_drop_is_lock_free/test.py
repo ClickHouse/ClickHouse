c93d4373da6fc4819cdfc061af76ed1cb610a804
Yarik Briukhovetskyi <114298166+yariks5s@users.noreply.github.com>
2023-07-28 15:14:22 +0200

    Update test.py

--- tests/integration/test_drop_is_lock_free/test.py
+++ tests/integration/test_drop_is_lock_free/test.py
@@ -129,6 +129,7 @@ def test_query_is_lock_free(lock_free_query, exclusive_table):
             f"Table default.{exclusive_table} does not exist. Maybe you meant table_test_query_is_lock_free_detach_part_?"
             in result
         )
+    else:
         assert 0 == int(
             node.query(
                 f"""
