76b61605c9b0f65bbac82768384beb3eb37070e8
zzznikola <140064935+zzznikola@users.noreply.github.com>
2023-07-28 22:03:11 +0300

    add commas to sql statements in 02841_column_not_under_agg_func.sql

--- tests/queries/0_stateless/02841_column_not_under_agg_func.sql
+++ tests/queries/0_stateless/02841_column_not_under_agg_func.sql
@@ -1,3 +1,3 @@
-CREATE TABLE test_02841(dt DateTime, n Int64) ENGINE=Memory()
-SELECT timezoneOf(dt) tz, count(*) FROM test_02841 GROUP BY tz
-DROP TABLE test_02841
+CREATE TABLE test_02841(dt DateTime, n Int64) ENGINE=Memory();
+SELECT timezoneOf(dt) tz, count(*) FROM test_02841 GROUP BY tz;
+DROP TABLE test_02841;
