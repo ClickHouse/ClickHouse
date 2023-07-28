52e448d67f4cccaf2be5310cbe9b2944553cd901
Igor Nikonov <igor@clickhouse.com>
2023-07-28 23:57:56 +0000

    More test queries
    + update file with broken tests

--- tests/queries/0_stateless/00979_set_index_not.sql
+++ tests/queries/0_stateless/00979_set_index_not.sql
@@ -11,5 +11,7 @@ insert into set_index_not values ('Jon','alive'),('Ramsey','rip');
 
 select * from set_index_not where status!='rip';
 select * from set_index_not where NOT (status ='rip');
+select * from set_index_not where NOT (status!='rip');
+select * from set_index_not where NOT (NOT (status ='rip'));
 
 DROP TABLE set_index_not;
