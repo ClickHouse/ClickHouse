e83e0ec2cd78a8bc09f34a7cde849531a59eafdb
Antonio Andelic <antonio@clickhouse.com>
2023-07-28 11:55:23 +0000

    Fix build

---
+++ src/IO/Archives/ArchiveUtils.h
@@ -0,0 +1,7 @@
+#ifdef __clang__
+#pragma clang diagnostic push
+#pragma clang diagnostic ignored "-Wreserved-macro-identifier"
+
+#include <archive.h>
+#include <archive_entry.h>
+#endif
--- src/IO/tests/gtest_archive_reader_and_writer.cpp
+++ src/IO/tests/gtest_archive_reader_and_writer.cpp
@@ -1,11 +1,18 @@
 #include <gtest/gtest.h>
 #include "config.h"
 
+#ifdef __clang__
+#pragma clang diagnostic push
+#pragma clang diagnostic ignored "-Wreserved-macro-identifier"
+
+#include <archive.h>
+#include <archive_entry.h>
+#endif
+
 #include <IO/Archives/IArchiveReader.h>
 #include <IO/Archives/IArchiveWriter.h>
 #include <IO/Archives/createArchiveReader.h>
 #include <IO/Archives/createArchiveWriter.h>
-#include <IO/Archives/util/archive_writers.h>
 #include <IO/ReadBufferFromFileBase.h>
 #include <IO/ReadBufferFromString.h>
 #include <IO/ReadHelpers.h>
@@ -20,11 +27,52 @@
 namespace DB::ErrorCodes
 {
     extern const int CANNOT_UNPACK_ARCHIVE;
+    extern const int LOGICAL_ERROR;
 }
 
 namespace fs = std::filesystem;
 using namespace DB;
 
+enum class ArchiveType : uint8_t
+{
+    Tar,
+    SevenZip
+};
+
+template <ArchiveType archive_type>
+bool createArchiveWithFiles(const std::string & archivename, std::map<std::string, std::string> files)
+{
+    struct archive * a;
+    struct archive_entry * entry;
+
+    a = archive_write_new();
+
+    if constexpr (archive_type == ArchiveType::Tar)
+        archive_write_set_format_pax_restricted(a);
+    else if constexpr (archive_type == ArchiveType::SevenZip)
+        archive_write_set_format_7zip(a);
+    else
+        throw DB::Exception(ErrorCodes::LOGICAL_ERROR, "Invalid archive type requested: {}", static_cast<size_t>(archive_type));
+
+    archive_write_open_filename(a, archivename.c_str());
+
+    for (auto & [filename, content] : files) {
+        entry = archive_entry_new();
+        archive_entry_set_pathname(entry, filename.c_str());
+        archive_entry_set_size(entry, content.size());
+        archive_entry_set_mode(entry, S_IFREG | 0644); // regular file with rw-r--r-- permissions
+        archive_entry_set_mtime(entry, time(nullptr), 0);
+        archive_write_header(a, entry);
+        archive_write_data(a, content.c_str(), content.size());
+        archive_entry_free(entry);
+    }
+    
+    archive_write_close(a);
+    archive_write_free(a);
+
+    return true;
+
+}
 
 class ArchiveReaderAndWriterTest : public ::testing::TestWithParam<const char *>
 {
@@ -332,7 +380,7 @@ TEST(TarArchiveReaderTest, FileExists) {
     String archive_path = "archive.tar";
     String filename = "file.txt";
     String contents = "test";
-    bool created = create_tar_with_file(archive_path, {{filename, contents}});
+    bool created = createArchiveWithFiles<ArchiveType::Tar>(archive_path, {{filename, contents}});
     EXPECT_EQ(created, true);
     auto reader = createArchiveReader(archive_path);
     EXPECT_EQ(reader->fileExists(filename), true);
@@ -343,7 +391,7 @@ TEST(TarArchiveReaderTest, ReadFile) {
     String archive_path = "archive.tar";
     String filename = "file.txt";
     String contents = "test";
-    bool created = create_tar_with_file(archive_path, {{filename, contents}});
+    bool created = createArchiveWithFiles<ArchiveType::Tar>(archive_path, {{filename, contents}});
     EXPECT_EQ(created, true);
     auto reader = createArchiveReader(archive_path);
     auto in = reader->readFile(filename);
@@ -359,7 +407,7 @@ TEST(TarArchiveReaderTest, ReadTwoFiles) {
     String contents1 = "test1";
     String file2 = "file2.txt";
     String contents2 = "test2";
-    bool created = create_tar_with_file(archive_path, {{file1, contents1}, {file2, contents2}});
+    bool created = createArchiveWithFiles<ArchiveType::Tar>(archive_path, {{file1, contents1}, {file2, contents2}});
     EXPECT_EQ(created, true);
     auto reader = createArchiveReader(archive_path);
     EXPECT_EQ(reader->fileExists(file1), true);
@@ -380,7 +428,7 @@ TEST(TarArchiveReaderTest, CheckFileInfo) {
     String archive_path = "archive.tar";
     String filename = "file.txt";
     String contents = "test";
-    bool created = create_tar_with_file(archive_path, {{filename, contents}});
+    bool created = createArchiveWithFiles<ArchiveType::Tar>(archive_path, {{filename, contents}});
     EXPECT_EQ(created, true);
     auto reader = createArchiveReader(archive_path);
     auto info = reader->getFileInfo(filename);
@@ -393,7 +441,7 @@ TEST(SevenZipArchiveReaderTest, FileExists) {
     String archive_path = "archive.7z";
     String filename = "file.txt";
     String contents = "test";
-    bool created = create_7z_with_file(archive_path, {{filename, contents}});
+    bool created = createArchiveWithFiles<ArchiveType::SevenZip>(archive_path, {{filename, contents}});
     EXPECT_EQ(created, true);
     auto reader = createArchiveReader(archive_path);
     EXPECT_EQ(reader->fileExists(filename), true);
@@ -404,7 +452,7 @@ TEST(SevenZipArchiveReaderTest, ReadFile) {
     String archive_path = "archive.7z";
     String filename = "file.txt";
     String contents = "test";
-    bool created = create_7z_with_file(archive_path, {{filename, contents}});
+    bool created = createArchiveWithFiles<ArchiveType::SevenZip>(archive_path, {{filename, contents}});
     EXPECT_EQ(created, true);
     auto reader = createArchiveReader(archive_path);
     auto in = reader->readFile(filename);
@@ -418,7 +466,7 @@ TEST(SevenZipArchiveReaderTest, CheckFileInfo) {
     String archive_path = "archive.7z";
     String filename = "file.txt";
     String contents = "test";
-    bool created = create_7z_with_file(archive_path, {{filename, contents}});
+    bool created = createArchiveWithFiles<ArchiveType::SevenZip>(archive_path, {{filename, contents}});
     EXPECT_EQ(created, true);
     auto reader = createArchiveReader(archive_path);
     auto info = reader->getFileInfo(filename);
@@ -433,7 +481,7 @@ TEST(SevenZipArchiveReaderTest, ReadTwoFiles) {
     String contents1 = "test1";
     String file2 = "file2.txt";
     String contents2 = "test2";
-    bool created = create_7z_with_file(archive_path, {{file1, contents1}, {file2, contents2}});
+    bool created = createArchiveWithFiles<ArchiveType::SevenZip>(archive_path, {{file1, contents1}, {file2, contents2}});
     EXPECT_EQ(created, true);
     auto reader = createArchiveReader(archive_path);
     EXPECT_EQ(reader->fileExists(file1), true);
