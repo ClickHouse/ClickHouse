01a58ec0ccee10ecfce18c8e424b0cb7e07f6310
zvonand <azvonov@altinity.com>
2023-07-30 19:42:10 +0200

    diverge parsing methods

---
+++ tests/queries/0_stateless/02813_float_parsing.sql
@@ -0,0 +1,10 @@
+SELECT
+    toFloat64('1.7091'),
+    toFloat64('15008753.'),
+    toFloat64('6e-09'),
+    toFloat64('6.000000000000001e-9'),
+    toFloat32('1.7091'),
+    toFloat32('15008753.'),
+    toFloat32('6e-09'),
+    toFloat32('6.000000000000001e-9')
+SETTINGS precise_float_parsing = 1;
