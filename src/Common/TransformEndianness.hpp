5570333c7c13773342c4190a6b829d00a7a3f9a3
ltrk2 <107155950+ltrk2@users.noreply.github.com>
2023-07-31 15:00:32 -0700

    Add support for arbitrary endianness transformation

--- src/Common/TransformEndianness.hpp
+++ src/Common/TransformEndianness.hpp
@@ -9,19 +9,19 @@
 
 namespace DB
 {
-template <std::endian endian, typename T>
+template <std::endian ToEndian, std::endian FromEndian = std::endian::native, typename T>
 requires std::is_integral_v<T>
 inline void transformEndianness(T & value)
 {
-    if constexpr (endian != std::endian::native)
+    if constexpr (ToEndian != FromEndian)
         value = std::byteswap(value);
 }
 
-template <std::endian endian, typename T>
+template <std::endian ToEndian, std::endian FromEndian = std::endian::native, typename T>
 requires is_big_int_v<T>
 inline void transformEndianness(T & x)
 {
-    if constexpr (std::endian::native != endian)
+    if constexpr (ToEndian != FromEndian)
     {
         auto & items = x.items;
         std::transform(std::begin(items), std::end(items), std::begin(items), [](auto & item) { return std::byteswap(item); });
@@ -29,49 +29,49 @@ inline void transformEndianness(T & x)
     }
 }
 
-template <std::endian endian, typename T>
+template <std::endian ToEndian, std::endian FromEndian = std::endian::native, typename T>
 requires is_decimal<T>
 inline void transformEndianness(T & x)
 {
-    transformEndianness<endian>(x.value);
+    transformEndianness<ToEndian, FromEndian>(x.value);
 }
 
-template <std::endian endian, typename T>
+template <std::endian ToEndian, std::endian FromEndian = std::endian::native, typename T>
 requires std::is_floating_point_v<T>
 inline void transformEndianness(T & value)
 {
-    if constexpr (std::endian::native != endian)
+    if constexpr (ToEndian != FromEndian)
     {
         auto * start = reinterpret_cast<std::byte *>(&value);
         std::reverse(start, start + sizeof(T));
     }
 }
 
-template <std::endian endian, typename T>
+template <std::endian ToEndian, std::endian FromEndian = std::endian::native, typename T>
 requires std::is_scoped_enum_v<T>
 inline void transformEndianness(T & x)
 {
     using UnderlyingType = std::underlying_type_t<T>;
-    transformEndianness<endian>(reinterpret_cast<UnderlyingType &>(x));
+    transformEndianness<ToEndian, FromEndian>(reinterpret_cast<UnderlyingType &>(x));
 }
 
-template <std::endian endian, typename A, typename B>
+template <std::endian ToEndian, std::endian FromEndian = std::endian::native, typename A, typename B>
 inline void transformEndianness(std::pair<A, B> & pair)
 {
-    transformEndianness<endian>(pair.first);
-    transformEndianness<endian>(pair.second);
+    transformEndianness<ToEndian, FromEndian>(pair.first);
+    transformEndianness<ToEndian, FromEndian>(pair.second);
 }
 
-template <std::endian endian, typename T, typename Tag>
+template <std::endian ToEndian, std::endian FromEndian = std::endian::native, typename T, typename Tag>
 inline void transformEndianness(StrongTypedef<T, Tag> & x)
 {
-    transformEndianness<endian>(x.toUnderType());
+    transformEndianness<ToEndian, FromEndian>(x.toUnderType());
 }
 
-template <std::endian endian>
+template <std::endian ToEndian, std::endian FromEndian = std::endian::native>
 inline void transformEndianness(CityHash_v1_0_2::uint128 & x)
 {
-    transformEndianness<endian>(x.low64);
-    transformEndianness<endian>(x.high64);
+    transformEndianness<ToEndian, FromEndian>(x.low64);
+    transformEndianness<ToEndian, FromEndian>(x.high64);
 }
 }
