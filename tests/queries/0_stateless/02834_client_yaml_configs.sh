a0ff04e0e7cc7bd911d80a01aee34846ec26bb3c
Alexey Milovidov <milovidov@clickhouse.com>
2023-08-01 08:03:09 +0200

    Add a test

---
+++ tests/queries/0_stateless/02834_client_yaml_configs.sh
@@ -0,0 +1,21 @@
+#!/usr/bin/env bash
+
+CUR_DIR=$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)
+# shellcheck source=../shell_config.sh
+. "$CUR_DIR"/../shell_config.sh
+
+pushd "${CLICKHOUSE_TMP}" > /dev/null
+
+echo "max_block_size: 31337" > clickhouse-client.yaml
+${CLICKHOUSE_CLIENT} --query "SELECT getSetting('max_block_size')"
+rm clickhouse-client.yaml
+
+echo "max_block_size: 31337" > clickhouse-client.yml
+${CLICKHOUSE_CLIENT} --query "SELECT getSetting('max_block_size')"
+rm clickhouse-client.yml
+
+echo "<clickhouse><max_block_size>31337</max_block_size></clickhouse>" > clickhouse-client.xml
+${CLICKHOUSE_CLIENT} --query "SELECT getSetting('max_block_size')"
+rm clickhouse-client.xml
+
+popd > /dev/null
