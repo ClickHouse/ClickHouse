5e69836f2348db967c37345ff17d5efb75bdcbfd
Alexey Milovidov <milovidov@clickhouse.com>
2023-08-01 17:40:55 +0300

    Update tests/queries/0_stateless/02834_client_yaml_configs.sh
    Co-authored-by: János Benjamin Antal <antaljanosbenjamin@users.noreply.github.com>

--- tests/queries/0_stateless/02834_client_yaml_configs.sh
+++ tests/queries/0_stateless/02834_client_yaml_configs.sh
@@ -11,11 +11,11 @@ echo "max_block_size: 31337" > clickhouse-client.yaml
 ${CLICKHOUSE_CLIENT} --query "SELECT getSetting('max_block_size')"
 rm clickhouse-client.yaml
 
-echo "max_block_size: 31337" > clickhouse-client.yml
+echo "max_block_size: 31338" > clickhouse-client.yml
 ${CLICKHOUSE_CLIENT} --query "SELECT getSetting('max_block_size')"
 rm clickhouse-client.yml
 
-echo "<clickhouse><max_block_size>31337</max_block_size></clickhouse>" > clickhouse-client.xml
+echo "<clickhouse><max_block_size>31339</max_block_size></clickhouse>" > clickhouse-client.xml
 ${CLICKHOUSE_CLIENT} --query "SELECT getSetting('max_block_size')"
 rm clickhouse-client.xml
 
