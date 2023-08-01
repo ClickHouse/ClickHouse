8b37abfa598b80bf9a1281d519f09d953e18523e
Alexey Milovidov <milovidov@clickhouse.com>
2023-08-01 11:16:00 +0300

    Update 02834_client_yaml_configs.sh

--- tests/queries/0_stateless/02834_client_yaml_configs.sh
+++ tests/queries/0_stateless/02834_client_yaml_configs.sh
@@ -5,7 +5,7 @@ CUR_DIR=$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)
 # shellcheck source=../shell_config.sh
 . "$CUR_DIR"/../shell_config.sh
 
-pushd "${CLICKHOUSE_TMP}" > /dev/null
+pushd "${CLICKHOUSE_TMP}" || exit > /dev/null
 
 echo "max_block_size: 31337" > clickhouse-client.yaml
 ${CLICKHOUSE_CLIENT} --query "SELECT getSetting('max_block_size')"
@@ -19,4 +19,4 @@ echo "<clickhouse><max_block_size>31337</max_block_size></clickhouse>" > clickho
 ${CLICKHOUSE_CLIENT} --query "SELECT getSetting('max_block_size')"
 rm clickhouse-client.xml
 
-popd > /dev/null
+popd || exit > /dev/null
