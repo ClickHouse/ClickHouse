718c2909e650a6c7f4f6d34d5cff953474df73ba
rajatmohan22 <lkrajath22@gmail.com>
2023-08-02 10:45:12 +0530

    Add ArrayRemove Function

---
+++ src/Functions/array/arrayRemove.h
@@ -0,0 +1,30 @@
+
+#pragma once
+
+#include <Columns/ColumnsNumber.h>
+#include <DataTypes/DataTypesNumber.h>
+#include "FunctionArrayMapped.h"
+
+
+namespace DB
+{/** arrayRemove(array, x -> predicate) - Returns a pointer to the modified array after removing the elements that passed the predicate condition.
+  */
+struct ArrayRemoveImpl
+{
+    static bool needBoolean() { return true; }
+    static bool needExpression() { return true; }
+    static bool needOneArray() { return true; }
+
+    static DataTypePtr getReturnType(const DataTypePtr & /*expression_return*/, const DataTypePtr & array_element)
+    {
+        return std::make_shared<DataTypeArray>(array_element);
+    }
+
+    /// If there are several arrays, the first one is passed here.
+    static ColumnPtr execute(const ColumnArray & array, ColumnPtr mapped);
+};
+
+struct NameArrayFilter { static constexpr auto name = "arrayRemove"; };
+using FunctionArrayRemove = FunctionArrayMapped<ArrayRemoveImpl, NameArrayFilter>;
+
+}
