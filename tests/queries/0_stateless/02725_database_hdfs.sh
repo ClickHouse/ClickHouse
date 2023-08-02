4c5c3a338d9ce83b2788a24878507ce978f24e69
Alexey Milovidov <milovidov@clickhouse.com>
2023-08-02 17:48:10 +0200

    Maybe fix tests

--- tests/queries/0_stateless/02725_database_hdfs.sh
+++ tests/queries/0_stateless/02725_database_hdfs.sh
@@ -1,6 +1,8 @@
 #!/usr/bin/env bash
 # Tags: no-fasttest, use-hdfs, no-parallel
 
+CLICKHOUSE_CLIENT_SERVER_LOGS_LEVEL=none
+
 CURDIR=$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)
 # shellcheck source=../shell_config.sh
 . "$CURDIR"/../shell_config.sh
