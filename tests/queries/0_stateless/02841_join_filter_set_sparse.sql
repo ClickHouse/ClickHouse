e152b0cea7857201e631d73ab17425cac6a58b32
Alexey Milovidov <milovidov@clickhouse.com>
2023-08-05 00:49:39 +0300

    Update tests/queries/0_stateless/02841_join_filter_set_sparse.sql
    Co-authored-by: Alexander Gololobov <440544+davenger@users.noreply.github.com>

--- tests/queries/0_stateless/02841_join_filter_set_sparse.sql
+++ tests/queries/0_stateless/02841_join_filter_set_sparse.sql
@@ -17,3 +17,6 @@ SELECT countIf(ignore(*) == 0) FROM t1 JOIN t2 ON t1.s = t2.s;
 SET join_algorithm = 'full_sorting_merge', max_rows_in_set_to_optimize_join = 100_000;
 
 SELECT countIf(ignore(*) == 0) FROM t1 JOIN t2 ON t1.s = t2.s;
+
+DROP TABLE t1;
+DROP TABLE t2;
