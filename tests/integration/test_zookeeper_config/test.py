ea5a5d85e1e6ea80f7dfdde677de33ebd4b86555
Alexey Milovidov <milovidov@clickhouse.com>
2023-08-05 13:54:36 +0200

    Something with tests

--- tests/integration/test_zookeeper_config/test.py
+++ tests/integration/test_zookeeper_config/test.py
@@ -58,9 +58,7 @@ def test_chroot_with_same_root(started_cluster):
             node.query("INSERT INTO simple VALUES ({0}, {0})".format(i))
 
     # Replication might take time
-
     assert_eq_with_retry(node1, "select count() from simple", "2\n")
-
     assert_eq_with_retry(node2, "select count() from simple", "2\n")
 
 
@@ -78,5 +76,6 @@ def test_chroot_with_different_root(started_cluster):
         for j in range(2):  # Second insert to test deduplication
             node.query("INSERT INTO simple_different VALUES ({0}, {0})".format(i))
 
-    assert node1.query("select count() from simple_different").strip() == "1"
-    assert node3.query("select count() from simple_different").strip() == "1"
+    # Replication might take time
+    assert_eq_with_retry(node1, "select count() from simple_different", "1\n")
+    assert_eq_with_retry(node3, "select count() from simple_different", "1\n")
