b29d0e56f3089fc9dc3685532347d698c7ec8948
Jiyoung Yoo <jiyoung.yoo03@gmail.com>
2023-08-05 00:26:32 -0700

    Additional fix for issue#40220

--- tests/queries/0_stateless/02841_remote_parameter_parsing_error.sql
+++ tests/queries/0_stateless/02841_remote_parameter_parsing_error.sql
@@ -1,7 +1,6 @@
 -- Tags: shard
 
 
-select * from remote('127.0.0.1'); -- { serverError NUMBER_OF_ARGUMENTS_DOESNT_MATCH }
 select * from remote('127.0.0.1', sys); -- { serverError NUMBER_OF_ARGUMENTS_DOESNT_MATCH }
 select * from remote('127.0.0.1', system); -- { serverError NUMBER_OF_ARGUMENTS_DOESNT_MATCH }
 select * from remote('127.0.0.1', system.o); -- { serverError UNKNOWN_TABLE }
@@ -11,4 +10,5 @@ select * from remote('127.0.0.1', system.one, default, key1); -- { serverError B
 select * from remote('127.0.0.1', system.one, 'default', '', key1); -- { serverError UNKNOWN_IDENTIFIER }
 select * from remote('127.0.0.1', system.one, default, '', key1); -- { serverError BAD_ARGUMENTS }
 select * from remote('127.0.0.1', system.one, 'default', pwd, key1); -- { serverError BAD_ARGUMENTS }
+select * from remote('127.0.0.1', system.one, 'default', '', key1, key2); -- { serverError NUMBER_OF_ARGUMENTS_DOESNT_MATCH }
 select * from remote('127.0.0.1', system, one, 'default', '', key1, key2); -- { serverError NUMBER_OF_ARGUMENTS_DOESNT_MATCH }
