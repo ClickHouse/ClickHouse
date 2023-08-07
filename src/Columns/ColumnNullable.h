dad45ff8aa4c35542ef26b37fbf11627b3e87f71
liuneng <1398775315@qq.com>
2023-08-07 13:37:24 +0800

    add more test

--- src/Columns/ColumnNullable.h
+++ src/Columns/ColumnNullable.h
@@ -62,7 +62,7 @@ public:
     StringRef getDataAt(size_t) const override;
     /// Will insert null value if pos=nullptr
     void insertData(const char * pos, size_t length) override;
-    StringRef serializeValueIntoArena(size_t n, Arena & arena, char const *& begin, const UInt8 * null_bit = nullptr) const override;
+    StringRef serializeValueIntoArena(size_t n, Arena & arena, char const *& begin, const UInt8 * null_bit) const override;
     const char * deserializeAndInsertFromArena(const char * pos) override;
     const char * skipSerializedInArena(const char * pos) const override;
     void insertRangeFrom(const IColumn & src, size_t start, size_t length) override;
