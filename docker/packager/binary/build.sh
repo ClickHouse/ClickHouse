f795ad8143d6ea07ed10b54c0fafdb395eb9eeb3
Alexey Milovidov <milovidov@clickhouse.com>
2023-08-08 15:17:20 +0200

    Fix errors

--- docker/packager/binary/build.sh
+++ docker/packager/binary/build.sh
@@ -184,6 +184,6 @@ fi
 # Prepare profile info (time-trace)
 mkdir -p profile-tmp
 ../utils/prepare-time-trace/prepare-time-trace.sh . profile-tmp
-cat profile-tmp/* > /profile/profile.json
+ls -1 profile-tmp | xargs cat > /profile/profile.json
 
 ls -l /output
--- tests/ci/build_check.py
+++ tests/ci/build_check.py
@@ -417,14 +417,13 @@ def main():
     )
     clickhouse_ci_logs_host = os.getenv("CLICKHOUSE_CI_LOGS_HOST")
     maybe_clickhouse_ci_logs_password: str = os.getenv("CLICKHOUSE_CI_LOGS_PASSWORD") or ""
-    clickhouse_ci_logs_password = urllib.parse.quote(maybe_clickhouse_ci_logs_password)
-    url = f"https://ci:{clickhouse_ci_logs_password}@{clickhouse_ci_logs_host}/?query={query}"
+    url = f"https://{clickhouse_ci_logs_host}/?query={query}"
     file_path = os.path.join(build_profile_path, "profile.json")
 
     print(f"::notice ::Log Uploading profile data, path: {file_path}, query: {query}")
 
     with open(file_path, "rb") as file:
-        requests.post(url, data=file)
+        requests.post(url, data=file, auth=("ci", maybe_clickhouse_ci_logs_password))
 
     # Upload statistics to CI database
 
