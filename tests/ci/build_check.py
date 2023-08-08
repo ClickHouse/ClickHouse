a91edb8b554e51203675d78d3b98ce6f3d4c6fa4
Alexey Milovidov <milovidov@clickhouse.com>
2023-08-08 04:11:32 +0200

    Fix mypy

--- tests/ci/build_check.py
+++ tests/ci/build_check.py
@@ -416,7 +416,8 @@ def main():
     """
     )
     clickhouse_ci_logs_host = os.getenv("CLICKHOUSE_CI_LOGS_HOST")
-    clickhouse_ci_logs_password = urllib.parse.quote(os.getenv("CLICKHOUSE_CI_LOGS_PASSWORD"))
+    maybe_clickhouse_ci_logs_password: str = os.getenv("CLICKHOUSE_CI_LOGS_PASSWORD") or ""
+    clickhouse_ci_logs_password = urllib.parse.quote(maybe_clickhouse_ci_logs_password)
     url = f"https://ci:{clickhouse_ci_logs_password}@{clickhouse_ci_logs_host}/?query={query}"
     file_path = os.path.join(build_profile_path, "profile.json")
 
