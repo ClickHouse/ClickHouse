4b6ade18b2a7dd43e06ba838460a9b76d313af10
robot-clickhouse <robot-clickhouse@users.noreply.github.com>
2023-08-08 20:06:59 +0000

    Automatic style fix

--- tests/integration/test_system_flush_logs/test.py
+++ tests/integration/test_system_flush_logs/test.py
@@ -46,7 +46,10 @@ def test_system_logs(flush_logs, table, exists):
         node.query(q)
     else:
         response = node.query_and_get_error(q)
-        assert "Table {} doesn't exist".format(table) in response or "Unknown table expression identifier '{}'".format(table) in response
+        assert (
+            "Table {} doesn't exist".format(table) in response
+            or "Unknown table expression identifier '{}'".format(table) in response
+        )
 
 
 # Logic is tricky, let's check that there is no hang in case of message queue
