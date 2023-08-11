62a629a1e89c082a2c85e95405e22d28d8b4349e
Александр Нам <47687537+seshWCS@users.noreply.github.com>
2023-08-12 01:27:04 +0300

    Update 02842_gcd_codec.sql

--- tests/queries/0_stateless/02842_gcd_codec.sql
+++ tests/queries/0_stateless/02842_gcd_codec.sql
@@ -1,8 +1,4 @@
 DROP TABLE IF EXISTS aboba;
-CREATE TABLE aboba (
-    s String,
-    ui UInt8 CODEC(GCD, ZSTD)
-)
-ENGINE = Memory;
-INSERT INTO aboba VALUES ('Hello', 239), ('World', 0), ('Goodbye', 37);
+CREATE TABLE aboba (s String, ui UInt8 CODEC(GCD, ZSTD)) ENGINE = Memory;
+INSERT INTO aboba (*) VALUES ('Hello', 239), ('World', 0), ('Goodbye', 37);
 SELECT ui FROM aboba;
