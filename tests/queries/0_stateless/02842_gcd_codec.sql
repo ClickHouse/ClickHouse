d929def7727317f159824653490f71677a8520f4
Александр Нам <superalexman700@gmail.com>
2023-08-11 14:41:39 +0000

    Fixed tests

--- tests/queries/0_stateless/02842_gcd_codec.sql
+++ tests/queries/0_stateless/02842_gcd_codec.sql
@@ -4,8 +4,5 @@ CREATE TABLE aboba (
     ui UInt8 CODEC(GCD, ZSTD)
 )
 ENGINE = Memory;
-INSERT INTO aboba VALUES 
-    ('Hello', 1263),
-    ('World', 0),
-    ('Goodbye', 293);
+INSERT INTO aboba VALUES ('Hello', 239), ('World', 0), ('Goodbye', 37);
 SELECT ui FROM aboba;
