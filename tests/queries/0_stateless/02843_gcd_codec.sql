9ff2005b7a8fdd30223833770d0fe48ba8d8a105
Александр Нам <superalexman700@gmail.com>
2023-08-12 02:10:11 +0000

    Test-fix

--- tests/queries/0_stateless/02843_gcd_codec.sql
+++ tests/queries/0_stateless/02843_gcd_codec.sql
@@ -1,4 +1,4 @@
 DROP TABLE IF EXISTS aboba;
-CREATE TABLE aboba (s String, ui UInt8 CODEC(GCD)) ENGINE = Memory;
+CREATE TABLE aboba (s String, ui UInt8 CODEC(GCD(1), LZ4)) ENGINE = Memory;
 INSERT INTO aboba (*) VALUES ('Hello', 239), ('World', 0), ('Goodbye', 37);
 SELECT ui FROM aboba;
