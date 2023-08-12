4c3b00c8a22d7d520053cae5878c9dba1fdfb13d
Alexey Milovidov <milovidov@clickhouse.com>
2023-08-12 02:09:51 +0200

    Fix tests

--- tests/queries/0_stateless/02536_delta_gorilla_corruption.sql
+++ tests/queries/0_stateless/02536_delta_gorilla_corruption.sql
@@ -10,8 +10,8 @@ drop table if exists bug_delta_gorilla;
 
 create table bug_delta_gorilla
 (value_bug UInt64 codec (Delta, Gorilla))
-engine = MergeTree SETTINGS index_granularity = 8192, index_granularity_bytes = '10Mi'
-order by tuple()
+engine = MergeTree
+order by tuple() SETTINGS index_granularity = 8192, index_granularity_bytes = '10Mi'
 as (select 0 from numbers(30000000));
 
 select count(*)
@@ -31,8 +31,8 @@ drop table if exists bug_delta_gorilla;
 select 'The same issue in a much smaller repro happens also in Debug builds';
 
 create table bug_delta_gorilla (val UInt64 codec (Delta, Gorilla))
-engine = MergeTree SETTINGS index_granularity = 8192, index_granularity_bytes = '10Mi'
-order by val;
+engine = MergeTree
+order by val SETTINGS index_granularity = 8192, index_granularity_bytes = '10Mi';
 insert into bug_delta_gorilla values (0)(1)(3);
 select * from bug_delta_gorilla;
 
