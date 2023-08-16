7149c9d634fda8b610888934651d19d3e25969a0
taiyang-li <654010905@qq.com>
2023-08-16 18:13:02 +0800

    fix ut 01651_bugs_from_15889

--- tests/queries/0_stateless/01651_bugs_from_15889.sql
+++ tests/queries/0_stateless/01651_bugs_from_15889.sql
@@ -54,7 +54,7 @@ WHERE (query_id =
     WHERE current_database = currentDatabase() AND (query LIKE '%test cpu time query profiler%') AND (query NOT LIKE '%system%')
     ORDER BY event_time DESC
     LIMIT 1
-)) AND (symbol LIKE '%Source%'); -- { serverError 125 }
+)) AND (symbol LIKE '%Source%'); -- { serverError 386 }
 
 
 WITH addressToSymbol(arrayJoin(trace)) AS symbol
@@ -69,7 +69,7 @@ WHERE greaterOrEquals(event_date, ignore(ignore(ignore(NULL, '')), 256), yesterd
     WHERE current_database = currentDatabase() AND (event_date >= yesterday()) AND (query LIKE '%test memory profiler%')
     ORDER BY event_time DESC
     LIMIT 1
-)); -- { serverError 125 }
+)); -- { serverError 42 }
 
 DROP TABLE IF EXISTS trace_log;
 
