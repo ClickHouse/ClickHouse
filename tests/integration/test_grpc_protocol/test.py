970eb24067ac54ed709a6ca4823b37e8d891a578
Alexander Tokmakov <tavplubix@clickhouse.com>
2023-08-16 22:57:49 +0200

    fix

--- tests/integration/test_grpc_protocol/test.py
+++ tests/integration/test_grpc_protocol/test.py
@@ -352,13 +352,13 @@ def test_authentication():
 
 
 def test_logs():
-    query = "SELECT has(groupArray(number), 42) FROM numbers(1000)"
+    query = "SELECT has(groupArray(number), 42) FROM numbers(100000)"
     logs = query_and_get_logs(
         query,
         settings={"send_logs_level": "debug"},
     )
     assert query in logs
-    assert "Read 1 rows" in logs
+    assert "Read 100000 rows" in logs
     assert "Peak memory usage" in logs
 
 
