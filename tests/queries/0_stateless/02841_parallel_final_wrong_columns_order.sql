409ad66b5d5c885a1388e489b4c798c3723a195e
Alexey Milovidov <milovidov@clickhouse.com>
2023-08-17 01:16:58 +0300

    Update 02841_parallel_final_wrong_columns_order.sql

--- tests/queries/0_stateless/02841_parallel_final_wrong_columns_order.sql
+++ tests/queries/0_stateless/02841_parallel_final_wrong_columns_order.sql
@@ -1,3 +1,6 @@
+-- Tags: no-random-settings
+-- Because we insert one million rows, it shouldn't choose too low index granularity.
+
 drop table if exists tab2;
 create table tab2 (id String, version Int64, l String, accountCode String, z Int32) engine = ReplacingMergeTree(z) PRIMARY KEY (accountCode, id) ORDER BY (accountCode, id, version, l);
 insert into tab2 select toString(number), number, toString(number), toString(number), 0 from numbers(1e6);
