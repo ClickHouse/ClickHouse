6ba6d879fe4aaca62e4a1078bbfc5f5fe443619d
Nikolai Kochetov <nk@clickhouse.com>
2023-08-16 20:23:10 +0000

    Fix wrong columns order for queries with parallel FINAL.

---
+++ tests/queries/0_stateless/02841_parallel_final_wrong_columns_order.sql
@@ -0,0 +1,5 @@
+drop table if exists tab2;
+create table tab2 (id String, version Int64, l String, accountCode String, z Int32) engine = ReplacingMergeTree(z) PRIMARY KEY (accountCode, id) ORDER BY (accountCode, id, version, l);
+insert into tab2 select toString(number), number, toString(number), toString(number), 0 from numbers(1e6);
+set max_threads=2;
+select count() from tab2 final;
