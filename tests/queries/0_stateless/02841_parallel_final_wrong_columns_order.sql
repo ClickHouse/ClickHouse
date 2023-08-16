368191af2fc55e528691765a4a564fa46fc14e76
Alexey Milovidov <milovidov@clickhouse.com>
2023-08-17 01:18:00 +0300

    Update 02841_parallel_final_wrong_columns_order.sql

--- tests/queries/0_stateless/02841_parallel_final_wrong_columns_order.sql
+++ tests/queries/0_stateless/02841_parallel_final_wrong_columns_order.sql
@@ -1,4 +1,4 @@
--- Tags: no-random-settings
+-- Tags: no-random-merge-tree-settings
 -- Because we insert one million rows, it shouldn't choose too low index granularity.
 
 drop table if exists tab2;
