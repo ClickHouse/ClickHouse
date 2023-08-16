6d3a6cecb15ae5856de74ff88c8d724e1129d6ad
Alexey Milovidov <milovidov@clickhouse.com>
2023-08-17 01:15:40 +0300

    Update 02841_parallel_final_wrong_columns_order.sql

--- tests/queries/0_stateless/02841_parallel_final_wrong_columns_order.sql
+++ tests/queries/0_stateless/02841_parallel_final_wrong_columns_order.sql
@@ -3,3 +3,4 @@ create table tab2 (id String, version Int64, l String, accountCode String, z Int
 insert into tab2 select toString(number), number, toString(number), toString(number), 0 from numbers(1e6);
 set max_threads=2;
 select count() from tab2 final;
+DROP TABLE tab2;
