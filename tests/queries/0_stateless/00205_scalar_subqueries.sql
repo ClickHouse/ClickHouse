a6d0feb38f923b2d214f6bcd99285bbff3054585
taiyang-li <654010905@qq.com>
2023-08-17 17:29:42 +0800

    fxi ut 00205_scalar_subqueries

--- tests/queries/0_stateless/00205_scalar_subqueries.sql
+++ tests/queries/0_stateless/00205_scalar_subqueries.sql
@@ -11,9 +11,8 @@ SELECT (SELECT toDate('2015-01-02'), 'Hello') AS x, x, identity((SELECT 1)), ide
 
 -- Scalar subquery with 0 rows must return Null
 SELECT (SELECT 1 WHERE 0);
--- But tuple and array can't be inside nullable
-SELECT (SELECT 1, 2 WHERE 0); -- { serverError 125 }
-SELECT (SELECT [1] WHERE 0); -- { serverError 125 }
+SELECT (SELECT 1, 2 WHERE 0);
+SELECT (SELECT [1] WHERE 0);
 -- Works for not-empty casle
 SELECT (SELECT 1, 2);
 SELECT (SELECT [1]);
