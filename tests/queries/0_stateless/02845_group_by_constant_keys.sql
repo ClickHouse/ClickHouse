27fb1b5ced804a37fee6c9fd110e029bb6cc01c5
Kruglov Pavel <48961922+Avogar@users.noreply.github.com>
2023-08-23 22:18:30 +0200

    Fix test

--- tests/queries/0_stateless/02845_group_by_constant_keys.sql
+++ tests/queries/0_stateless/02845_group_by_constant_keys.sql
@@ -4,7 +4,7 @@ select count(number), 1 AS k1, 2 as k2, 3 as k3 from numbers_mt(10000000) group 
 select count(number), 1 AS k1, 2 as k2, 3 as k3 from numbers_mt(10000000) group by k1, k2, k3 settings optimize_group_by_constant_keys=1, enable_software_prefetch_in_aggregation=1, compile_aggregate_expressions = 1;
 
 drop table if exists test;
-create table test (x UInt64) engine=File(Parquet);
+create table test (x UInt64) engine=File(JSON);
 set engine_file_allow_create_multiple_files = 1;
 insert into test select * from numbers(10);
 insert into test select * from numbers(10);
