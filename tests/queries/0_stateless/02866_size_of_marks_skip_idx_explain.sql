9e9fae632fad104fd0439ce9d91a15fa66c7269d
Robert Schulze <robert@clickhouse.com>
2023-08-23 11:15:29 +0000

    Convert to sql test, fix expected results of usearch test

---
+++ tests/queries/0_stateless/02866_size_of_marks_skip_idx_explain.sql
@@ -0,0 +1,20 @@
+-- Tags: no-random-merge-tree-settings
+
+SET optimize_move_to_prewhere = 1;
+SET convert_query_to_cnf = 0;
+SET optimize_read_in_order = 1;
+
+DROP TABLE IF EXISTS test_skip_idx;
+
+CREATE TABLE test_skip_idx (
+    id UInt32,
+    INDEX name_idx_g2 id TYPE minmax GRANULARITY 2,
+    INDEX name_idx_g1 id TYPE minmax GRANULARITY 1)
+ENGINE = MergeTree
+ORDER BY tuple()
+SETTINGS index_granularity = 1, index_granularity_bytes = 0, min_bytes_for_wide_part = 0;
+
+INSERT INTO test_skip_idx SELECT number FROM system.numbers LIMIT 5 OFFSET 1;
+
+EXPLAIN indexes = 1 SELECT * FROM test_skip_idx WHERE id < 2;
+EXPLAIN indexes = 1 SELECT * FROM test_skip_idx WHERE id < 3;
