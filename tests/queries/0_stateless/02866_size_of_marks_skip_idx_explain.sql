d76206098b033b8cbb025fded368d1420a33359f
Robert Schulze <robert@clickhouse.com>
2023-08-24 10:23:17 +0000

    Force-enable analyzer

--- tests/queries/0_stateless/02866_size_of_marks_skip_idx_explain.sql
+++ tests/queries/0_stateless/02866_size_of_marks_skip_idx_explain.sql
@@ -4,6 +4,8 @@ SET optimize_move_to_prewhere = 1;
 SET convert_query_to_cnf = 0;
 SET optimize_read_in_order = 1;
 
+SET allow_experimental_analyzer = 1; -- slightly different operator names than w/o
+
 DROP TABLE IF EXISTS test_skip_idx;
 
 CREATE TABLE test_skip_idx (
