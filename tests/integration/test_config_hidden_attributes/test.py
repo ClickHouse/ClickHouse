9d3273a331263574c50df990c34fb3542900635a
Roman Vasin <r.vasin@arenadata.io>
2023-08-24 14:23:46 +0000

    Add test for nested nodes, add checks for presence of nodes in pre-processed config

--- tests/integration/test_config_hidden_attributes/test.py
+++ tests/integration/test_config_hidden_attributes/test.py
@@ -2,8 +2,11 @@ import pytest
 import os
 from helpers.cluster import ClickHouseCluster
 
+
 cluster = ClickHouseCluster(__file__)
-node = cluster.add_instance("node", main_configs=["configs/config.xml"])
+node = cluster.add_instance(
+    "node", main_configs=["configs/config.xml"], user_configs=["configs/users.xml"]
+)
 
 
 @pytest.fixture(scope="module")
@@ -23,3 +26,16 @@ def test_hidden(started_cluster):
         )
         == "60000000000\n"
     )
+    assert (
+        node.query(
+            "select value from system.server_settings where name ='max_partition_size_to_drop'"
+        )
+        == "40000000000\n"
+    )
+    assert "key_1" in node.query("select collection from system.named_collections")
+    out = node.exec_in_container(
+        ["cat", "/var/lib/clickhouse/preprocessed_configs/config.xml"]
+    )
+    assert "max_table_size_to_drop" not in out
+    assert "max_partition_size_to_drop" in out
+    assert "named_collections" not in out
