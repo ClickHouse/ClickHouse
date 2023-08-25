e62dc054219aa144e8d8682b376ef5622e259e2e
Yakov Olkhovskiy <yakov@clickhouse.com>
2023-08-25 21:06:14 +0000

    fix

--- src/Common/MemoryTracker.cpp
+++ src/Common/MemoryTracker.cpp
@@ -85,7 +85,10 @@ inline std::string_view toDescription(OvercommitResult result)
 
 bool shouldTrackAllocation(DB::Float64 probability, void * ptr)
 {
+#   pragma clang diagnostic push
+#   pragma clang diagnostic ignored "-Wimplicit-const-int-float-conversion"
     return intHash64(uintptr_t(ptr)) < std::numeric_limits<uint64_t>::max() * probability;
+#   pragma clang diagnostic pop
 }
 
 }
