3a5f2101b056ac5c2702c7fb4918ef27d973966d
xuelei <xuelei0227@foxmail.com>
2023-08-28 06:05:21 -0300

    deal with conflict

--- src/Disks/ObjectStorages/S3/registerDiskS3.cpp
+++ src/Disks/ObjectStorages/S3/registerDiskS3.cpp
@@ -104,7 +104,6 @@ void registerDiskS3(DiskFactory & factory, bool global_skip_access_check)
     {
         String endpoint = context->getMacros()->expand(config.getString(config_prefix + ".endpoint"));
         S3::URI uri(endpoint);
-
         if(!uri.key.ends_with('/'))
             uri.key.push_back('/');
 
