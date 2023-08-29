7e58894847dcd8f008a486228a20effaf885f269
irenjj <renj.jiang@gmail.com>
2023-08-30 00:44:38 +0800

    add test

--- tests/queries/0_stateless/01231_markdown_format.sql
+++ tests/queries/0_stateless/01231_markdown_format.sql
@@ -4,3 +4,16 @@ INSERT INTO markdown VALUES (1, 'name1', [1,2,3], 'Some long string', 'name1', 1
 
 SELECT * FROM markdown FORMAT Markdown;
 DROP TABLE IF EXISTS markdown;
+
+
+DROP TABLE IF EXISTS markdown;
+SET output_format_markdown_escape_special_characters = true;
+SELECT '!' as a, '#' as b, '$' as c, '%' as d, '&' as e, '(' as f, '*' as g, '+' as h, ',' as i, '-' as j, '.' as k FORMAT Markdown;
+SELECT '/' as a, ':' as b, ';' as c, '<' as d, '=' as e, '>' as f, '?' as g, '@' as h, '[' as i, '^' as j, '_' as k FORMAT Markdown;
+SELECT '`' as a, '{' as b, '|' as c, '}' as d, '~' as e FORMAT Markdown;
+
+SET output_format_markdown_escape_special_characters = false;
+SELECT '!' as a, '#' as b, '$' as c, '%' as d, '&' as e, '(' as f, '*' as g, '+' as h, ',' as i, '-' as j, '.' as k FORMAT Markdown;
+SELECT '/' as a, ':' as b, ';' as c, '<' as d, '=' as e, '>' as f, '?' as g, '@' as h, '[' as i, '^' as j, '_' as k FORMAT Markdown;
+SELECT '`' as a, '{' as b, '|' as c, '}' as d, '~' as e FORMAT Markdown;
+DROP TABLE IF EXISTS markdown;
