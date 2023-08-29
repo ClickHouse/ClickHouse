a1a89ab1505cf2ede9504b33b850a7ccf38f883b
robot-clickhouse <robot-clickhouse@users.noreply.github.com>
2023-08-29 14:18:58 +0000

    Backport #52517 to 23.6: Fix lightweight delete after drop of projection

--- src/Storages/MergeTree/MergeTreeData.cpp
+++ src/Storages/MergeTree/MergeTreeData.cpp
@@ -5683,6 +5683,10 @@ bool MergeTreeData::supportsLightweightDelete() const
     auto lock = lockParts();
     for (const auto & part : data_parts_by_info)
     {
+        if (part->getState() == MergeTreeDataPartState::Outdated
+            || part->getState() == MergeTreeDataPartState::Deleting)
+            continue;
+
         if (!part->supportLightweightDeleteMutate())
             return false;
     }
