9b7d3376faa818044f67ce776e958d0e76a1f968
seshWCS <superalexman700@gmail.com>
2023-08-29 19:01:31 +0000

    Deleted unused ErrorCode

--- src/Compression/CompressionCodecGCD.cpp
+++ src/Compression/CompressionCodecGCD.cpp
@@ -46,7 +46,6 @@ namespace ErrorCodes
     extern const int CANNOT_COMPRESS;
     extern const int CANNOT_DECOMPRESS;
     extern const int ILLEGAL_SYNTAX_FOR_CODEC_TYPE;
-    extern const int ILLEGAL_CODEC_PARAMETER;
     extern const int BAD_ARGUMENTS;
 }
 
