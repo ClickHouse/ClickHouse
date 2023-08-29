42c87615d188296d2c17e1e012f048f730ed8dc1
Roman Vasin <r.vasin@arenadata.io>
2023-08-29 11:22:23 +0000

    Fix flakiness in test_config_hide_in_preprocessed

--- tests/integration/test_config_hide_in_preprocessed/test.py
+++ tests/integration/test_config_hide_in_preprocessed/test.py
@@ -42,7 +42,16 @@ def test_hide_in_preprocessed(started_cluster):
     out = node.exec_in_container(
         ["cat", "/var/lib/clickhouse/preprocessed_configs/config.xml"]
     )
-    assert "max_thread_pool_free_size" not in out
-    assert "max_table_size_to_drop" not in out
-    assert "max_partition_size_to_drop" in out
-    assert "named_collections" not in out
+    assert (
+        '<max_thread_pool_free_size hide_in_preprocessed="1">2000</max_thread_pool_free_size>'
+        not in out
+    )
+    assert (
+        '<max_table_size_to_drop hide_in_preprocessed="true">60000000000</max_table_size_to_drop>'
+        not in out
+    )
+    assert (
+        '<max_partition_size_to_drop hide_in_preprocessed="false">40000000000</max_partition_size_to_drop>'
+        in out
+    )
+    assert '<named_collections hide_in_preprocessed="true">' not in out
