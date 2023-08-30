5d6d84b2f778648eef0ee5f73e4ae6b0bb8ddab7
irenjj <renj.jiang@gmail.com>
2023-08-30 23:51:41 +0800

    add test

--- tests/queries/0_stateless/01231_markdown_format.sql
+++ tests/queries/0_stateless/01231_markdown_format.sql
@@ -8,17 +8,11 @@ DROP TABLE IF EXISTS markdown;
 
 SET output_format_markdown_escape_special_characters = true;
 SELECT '!' AS a, '#' AS b, '$' AS c, '%' AS d, '&' AS e, '(' AS f, '*' AS g, '+' AS h, ',' AS i, '-' AS j, '.' AS k, '/' AS l, ':' AS m, ';' AS n, '<' AS o, '=' AS p, '>' AS q, '?' AS r, '@' AS s, '[' AS t, '^' AS u, '_' AS v, '`' AS w, '{' AS x, '|' AS y, '}' AS z, '~' AS A FORMAT Markdown;
-SET output_format_markdown_escape_special_characters = false;
-SELECT '!' AS a, '#' AS b, '$' AS c, '%' AS d, '&' AS e, '(' AS f, '*' AS g, '+' AS h, ',' AS i, '-' AS j, '.' AS k, '/' AS l, ':' AS m, ';' AS n, '<' AS o, '=' AS p, '>' AS q, '?' AS r, '@' AS s, '[' AS t, '^' AS u, '_' AS v, '`' AS w, '{' AS x, '|' AS y, '}' AS z, '~' AS A FORMAT Markdown;
-
-
-SET output_format_markdown_escape_special_characters = true;
+SELECT '!#$%&(*+,-./:<=>?@[^`{|}~' AS a FORMAT Markdown;
 SELECT CAST(1 AS Enum('!#$%&(*+,-./:<=>?@[^`{|}~' = 1)) AS a FORMAT Markdown;
-SET output_format_markdown_escape_special_characters = false;
-SELECT CAST(1 AS Enum('!#$%&(*+,-./:<=>?@[^`{|}~' = 1)) AS a FORMAT Markdown;
-
-
-SET output_format_markdown_escape_special_characters = true;
 SELECT toFixedString('!#$%&(*+,-./:<=>?@[^`{|}~', 25) AS a FORMAT Markdown;
+
 SET output_format_markdown_escape_special_characters = false;
+SELECT '!#$%&(*+,-./:<=>?@[^`{|}~' AS a FORMAT Markdown;
+SELECT CAST(1 AS Enum('!#$%&(*+,-./:<=>?@[^`{|}~' = 1)) AS a FORMAT Markdown;
 SELECT toFixedString('!#$%&(*+,-./:<=>?@[^`{|}~', 25) AS a FORMAT Markdown;
