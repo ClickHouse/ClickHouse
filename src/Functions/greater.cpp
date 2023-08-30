32a23244c9ca491200f35ee7a6e523f9f7f96620
Nikolai Kochetov <nk@clickhouse.com>
2023-08-30 19:53:32 +0000

    Remove context from comparison functions.
    (cherry picked from commit 57608cc6f90ae7c29d7fc82ed228c1eaf6e25c73)

--- src/Functions/greater.cpp
+++ src/Functions/greater.cpp
@@ -1,11 +1,14 @@
 #include <Functions/FunctionFactory.h>
 #include <Functions/FunctionsComparison.h>
+#include <Functions/FunctionsLogical.h>
+
 
 
 namespace DB
 {
 
 using FunctionGreater = FunctionComparison<GreaterOp, NameGreater>;
+using FunctionEquals = FunctionComparison<EqualsOp, NameEquals>;
 
 REGISTER_FUNCTION(Greater)
 {
@@ -16,14 +19,24 @@ template <>
 ColumnPtr FunctionComparison<GreaterOp, NameGreater>::executeTupleImpl(
     const ColumnsWithTypeAndName & x, const ColumnsWithTypeAndName & y, size_t tuple_size, size_t input_rows_count) const
 {
-    auto greater = FunctionFactory::instance().get("greater", context);
+    FunctionOverloadResolverPtr greater
+        = std::make_unique<FunctionToOverloadResolverAdaptor>(std::make_shared<FunctionGreater>(check_decimal_overflow));
+
+    FunctionOverloadResolverPtr func_builder_or
+        = std::make_unique<FunctionToOverloadResolverAdaptor>(std::make_shared<FunctionOr>());
+
+    FunctionOverloadResolverPtr func_builder_and
+        = std::make_unique<FunctionToOverloadResolverAdaptor>(std::make_shared<FunctionAnd>());
+
+    FunctionOverloadResolverPtr func_builder_equals
+        = std::make_unique<FunctionToOverloadResolverAdaptor>(std::make_shared<FunctionEquals>(check_decimal_overflow));
 
     return executeTupleLessGreaterImpl(
         greater,
         greater,
-        FunctionFactory::instance().get("and", context),
-        FunctionFactory::instance().get("or", context),
-        FunctionFactory::instance().get("equals", context),
+        func_builder_and,
+        func_builder_or,
+        func_builder_equals,
         x, y, tuple_size, input_rows_count);
 }
 
--- src/Functions/notEquals.cpp
+++ src/Functions/notEquals.cpp
@@ -1,5 +1,7 @@
 #include <Functions/FunctionFactory.h>
 #include <Functions/FunctionsComparison.h>
+#include <Functions/FunctionsLogical.h>
+
 
 
 namespace DB
@@ -16,9 +18,15 @@ template <>
 ColumnPtr FunctionComparison<NotEqualsOp, NameNotEquals>::executeTupleImpl(
     const ColumnsWithTypeAndName & x, const ColumnsWithTypeAndName & y, size_t tuple_size, size_t input_rows_count) const
 {
+    FunctionOverloadResolverPtr func_builder_not_equals
+        = std::make_unique<FunctionToOverloadResolverAdaptor>(std::make_shared<FunctionNotEquals>(check_decimal_overflow));
+
+    FunctionOverloadResolverPtr func_builder_or
+        = std::make_unique<FunctionToOverloadResolverAdaptor>(std::make_shared<FunctionOr>());
+
     return executeTupleEqualityImpl(
-        FunctionFactory::instance().get("notEquals", context),
-        FunctionFactory::instance().get("or", context),
+        func_builder_not_equals,
+        func_builder_or,
         x, y, tuple_size, input_rows_count);
 }
 
