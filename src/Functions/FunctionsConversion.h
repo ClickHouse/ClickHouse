278cc494a249b8979ebd70c11aebbb65138790bb
Yarik Briukhovetskyi <114298166+yariks5s@users.noreply.github.com>
2023-09-01 17:40:04 +0200

    Updated FunctionsConversion.h

--- src/Functions/FunctionsConversion.h
+++ src/Functions/FunctionsConversion.h
@@ -2840,7 +2840,7 @@ protected:
     bool useDefaultImplementationForNulls() const override { return false; }
     /// CAST(Nothing, T) -> T
     bool useDefaultImplementationForNothing() const override { return false; }
-    bool useDefaultImplementationForConstants() const override { return false; }
+    bool useDefaultImplementationForConstants() const override { return true; }
     bool useDefaultImplementationForLowCardinalityColumns() const override { return false; }
     ColumnNumbers getArgumentsThatAreAlwaysConstant() const override { return {1}; }
 
