00e29d239fc279d8a9ae6c82437993fa96bad49f
Joe Lynch <joe.lynch@aiven.io>
2023-09-01 14:04:01 +0200

    Add elapsed_ns to HTTP headers X-ClickHouse-Progress and X-ClickHouse-Summary
    The TCP interface progress has this field. This is not a super accurate
    measure of server side query time, but trying to measure from the client
    is even worse.

--- src/IO/Progress.cpp
+++ src/IO/Progress.cpp
@@ -90,7 +90,8 @@ void ProgressValues::writeJSON(WriteBuffer & out, bool add_braces) const
     writeText(result_rows, out);
     writeCString("\",\"result_bytes\":\"", out);
     writeText(result_bytes, out);
-    writeCString("\"", out);
+    writeCString("\",\"elapsed_ns\":\"", out);
+    writeText(elapsed_ns, out);
     if (add_braces)
         writeCString("}", out);
 }
@@ -237,6 +238,11 @@ void Progress::write(WriteBuffer & out, UInt64 client_revision) const
 void Progress::writeJSON(WriteBuffer & out, bool add_braces) const
 {
     getValues().writeJSON(out, add_braces);
+}
+
+void Progress::incrementElapsedNs(UInt64 elapsed_ns_)
+{
+    elapsed_ns.fetch_add(elapsed_ns_, std::memory_order_relaxed);
 }
 
 }
