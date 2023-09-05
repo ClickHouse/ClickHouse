c5b57e1e4c811cb465d916231281ed3057eacbf5
Alexander Tokmakov <tavplubix@clickhouse.com>
2023-09-05 15:35:18 +0200

    fix test

--- tests/queries/0_stateless/02413_replace_partition_zero_copy.sql
+++ tests/queries/0_stateless/02413_replace_partition_zero_copy.sql
@@ -6,6 +6,12 @@ create table src2 (n int) engine=ReplicatedMergeTree('/test/02413/{database}/src
 create table dst1 (n int) engine=ReplicatedMergeTree('/test/02413/{database}/dst', '1') order by tuple() settings storage_policy='s3_cache', allow_remote_fs_zero_copy_replication=1;
 create table dst2 (n int) engine=ReplicatedMergeTree('/test/02413/{database}/dst', '2') order by tuple() settings storage_policy='s3_cache', allow_remote_fs_zero_copy_replication=1;
 
+-- FIXME zero-copy locks may remain in ZooKeeper forever if we failed to insert a part.
+-- Probably that's why we have to replace repsistent lock with ephemeral sometimes.
+-- See also "Replacing persistent lock with ephemeral for path {}. It can happen only in case of local part loss"
+-- in StorageReplicatedMergeTree::createZeroCopyLockNode
+set insert_keeper_fault_injection_probability=0;
+
 insert into src1 values(1);
 insert into src2 values(2);
 system sync replica src1 lightweight;
