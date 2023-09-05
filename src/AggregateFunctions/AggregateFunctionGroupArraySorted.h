a4dc12c4c9f8fd65abf2811a479882e1ef656074
yariks5s <yaroslav.briukhovetskyi@clickhouse.com>
2023-09-05 18:30:27 +0000

    resolved reviews

--- src/AggregateFunctions/AggregateFunctionGroupArraySorted.h
+++ src/AggregateFunctions/AggregateFunctionGroupArraySorted.h
@@ -26,11 +26,10 @@
 
 #include <AggregateFunctions/IAggregateFunction.h>
 
+#include <Common/RadixSort.h>
 #include <algorithm>
 #include <type_traits>
 #include <utility>
-
-#define AGGREGATE_FUNCTION_GROUP_ARRAY_MAX_ARRAY_SIZE 0xFFFFFF
 
 
 namespace DB
@@ -50,15 +49,15 @@ class GroupArraySortedNumericImpl final
     using Data = GroupArrayNumericData<T, Trait::sampler != Sampler::NONE>;
     static constexpr bool limit_num_elems = Trait::has_limit;
     UInt64 max_elems;
-    UInt64 seed;
+    SerializationPtr serialization;
 
 public:
     explicit GroupArraySortedNumericImpl(
-        const DataTypePtr & data_type_, const Array & parameters_, UInt64 max_elems_ = std::numeric_limits<UInt64>::max(), UInt64 seed_ = 123456)
+        const DataTypePtr & data_type_, const Array & parameters_, UInt64 max_elems_ = std::numeric_limits<UInt64>::max())
         : IAggregateFunctionDataHelper<GroupArrayNumericData<T, false>, GroupArraySortedNumericImpl<T, Trait>>(
             {data_type_}, parameters_, std::make_shared<DataTypeArray>(data_type_))
         , max_elems(max_elems_)
-        , seed(seed_)
+        , serialization(data_type_->getDefaultSerialization())
     {
     }
 
@@ -68,6 +67,7 @@ public:
     {
         [[maybe_unused]] auto a = new (place) Data;
     }
+    /// NOTE: Later we need to remove: limit_num_elems (if possible)
 
     void add(AggregateDataPtr __restrict place, const IColumn ** columns, size_t row_num, Arena * arena) const override
     {
@@ -79,75 +79,50 @@ public:
         const auto & row_value = row_values[row_num];
         auto & cur_elems = this->data(place);
 
-        ++cur_elems.total_values;
         cur_elems.value.push_back(row_value, arena);
 
-        if (!limit_num_elems && columns[0]->size() == cur_elems.value.size())
-            std::sort(cur_elems.value.begin(), cur_elems.value.end());
-        else if (columns[0]->size() == cur_elems.value.size())
+        /// To optimize, we sort (2 * max_size) elements of input array over and over again
+        /// and after each loop we delete the last half of sorted array
+        if (cur_elems.value.size() >= max_elems * 2)
         {
-            /// To optimize, we sort (2 * max_size) elements of input array over and over again
-            /// and after each loop we delete the last half of sorted array
-            bool sorted = false, part_sorted = false;
-            while (!sorted)
-            {
-                if (cur_elems.value.size() >= max_elems * 2)
-                {
-                    std::sort(cur_elems.value.begin(), cur_elems.value.begin() + (max_elems * 2));
-                    cur_elems.value.erase(cur_elems.value.begin() + max_elems, cur_elems.value.begin() + (max_elems * 2));
-                    part_sorted = true;
-                }
-                else if (cur_elems.value.size() > max_elems)
-                {
-                    std::sort(cur_elems.value.begin(), cur_elems.value.end());
-                    cur_elems.value.resize(max_elems, arena);
-                    sorted = true;
-                }
-                else if (cur_elems.value.size() == max_elems)
-                {
-                    if (part_sorted)
-                        sorted = true;
-                    else
-                    {
-                        std::sort(cur_elems.value.begin(), cur_elems.value.end());
-                        sorted = true;
-                    }
-                }
-            }
+            RadixSort<RadixSortNumTraits<T>>::executeLSD(cur_elems.value.data(), cur_elems.value.size());
+            cur_elems.value.resize(max_elems, arena);
         }
     }
 
     void merge(AggregateDataPtr __restrict place, ConstAggregateDataPtr rhs, Arena * arena) const override
     {
         auto & cur_elems = this->data(place);
-        size_t cur_size = cur_elems.value.size();
         auto & rhs_elems = this->data(rhs);
 
         if (rhs_elems.value.empty())
             return;
 
+        std::sort(cur_elems.value.begin(), cur_elems.value.end());
         if (limit_num_elems)
-        {
-            UInt64 elems_to_insert = std::min(static_cast<size_t>(max_elems) - cur_size, rhs_elems.value.size());
-            if (elems_to_insert)
-            {
-                cur_elems.value.insertByOffsets(rhs_elems.value, 0, elems_to_insert, arena);
-            }
-        }
-        else
-        {
-            if (rhs_elems.value.size())
-                cur_elems.value.insertByOffsets(rhs_elems.value, 0, rhs_elems.value.size(), arena);
-        }
+            cur_elems.value.resize(max_elems, arena);
+
+        if (rhs_elems.value.size())
+            cur_elems.value.insertByOffsets(rhs_elems.value, 0, rhs_elems.value.size(), arena);
     }
 
     void serialize(ConstAggregateDataPtr __restrict place, WriteBuffer & buf, std::optional<size_t> /* version */) const override
     {
-        const auto & value = this->data(place).value;
-        const size_t size = value.size();
-        writeVarUInt(size, buf);
-        for (const auto & element : value)
-            writeBinaryLittleEndian(element, buf);
+        writeVarUInt(this->data(place).value.size(), buf);
+
+        auto & value = this->data(place).value;
+        for (const Field & elem : value)
+        {
+            if (elem.isNull())
+            {
+                writeBinary(UInt8(1), buf);
+            }
+            else
+            {
+                writeBinary(UInt8(0), buf);
+                serialization->serializeBinary(elem, buf, {});
+            }
+        }
     }
 
     void deserialize(AggregateDataPtr __restrict place, ReadBuffer & buf, std::optional<size_t> /* version */, Arena * arena) const override
@@ -155,10 +130,9 @@ public:
         size_t size = 0;
         readVarUInt(size, buf);
 
-        if (unlikely(size > AGGREGATE_FUNCTION_GROUP_ARRAY_MAX_ARRAY_SIZE))
+        if (size > 0xFFFFFF)
             throw Exception(ErrorCodes::TOO_LARGE_ARRAY_SIZE,
-                            "Too large array size (maximum: {})", AGGREGATE_FUNCTION_GROUP_ARRAY_MAX_ARRAY_SIZE);
-
+                            "Too large array size (maximum: {})", 0xFFFFFF);
         if (limit_num_elems && unlikely(size > max_elems))
             throw Exception(ErrorCodes::TOO_LARGE_ARRAY_SIZE, "Too large array size, it should not exceed {}", max_elems);
 
@@ -169,9 +143,13 @@ public:
             readBinaryLittleEndian(element, buf);
     }
 
-    void insertResultInto(AggregateDataPtr __restrict place, IColumn & to, Arena *) const override
+    void insertResultInto(AggregateDataPtr __restrict place, IColumn & to, Arena * a) const override
     {
-        const auto & value = this->data(place).value;
+        auto& value = this->data(place).value;
+
+        RadixSort<RadixSortNumTraits<T>>::executeLSD(value.data(), value.size());
+        if (limit_num_elems)
+            value.resize(max_elems, a);
         size_t size = value.size();
 
         ColumnArray & arr_to = assert_cast<ColumnArray &>(to);
@@ -200,8 +178,6 @@ struct GroupArraySortedGeneralData<Node, false>
     using Allocator = MixedAlignedArenaAllocator<alignof(Node *), 4096>;
     using Array = PODArray<Field, 32, Allocator>;
 
-    // For groupArrayLast()
-    size_t total_values = 0;
     Array value;
 };
 
@@ -222,15 +198,16 @@ class GroupArraySortedGeneralImpl final
 
     DataTypePtr & data_type;
     UInt64 max_elems;
-    UInt64 seed;
+    SerializationPtr serialization;
+    
 
 public:
-    GroupArraySortedGeneralImpl(const DataTypePtr & data_type_, const Array & parameters_, UInt64 max_elems_ = std::numeric_limits<UInt64>::max(), UInt64 seed_ = 123456)
+    GroupArraySortedGeneralImpl(const DataTypePtr & data_type_, const Array & parameters_, UInt64 max_elems_ = std::numeric_limits<UInt64>::max())
         : IAggregateFunctionDataHelper<GroupArraySortedGeneralData<Node, false>, GroupArraySortedGeneralImpl<Node, Trait>>(
             {data_type_}, parameters_, std::make_shared<DataTypeArray>(data_type_))
         , data_type(this->argument_types[0])
         , max_elems(max_elems_)
-        , seed(seed_)
+        , serialization(data_type->getDefaultSerialization())
     {
     }
 
@@ -248,41 +225,15 @@ public:
 
         auto & cur_elems = data(place);
 
-        ++cur_elems.total_values;
         cur_elems.value.push_back(columns[0][0][row_num], arena);
 
-        if (!limit_num_elems && columns[0]->size() == cur_elems.value.size())
-            std::sort(cur_elems.value.begin(), cur_elems.value.end());
-        else if (columns[0]->size() == cur_elems.value.size())
+        /// To optimize, we sort (2 * max_size) elements of input array over and over again and
+        /// after each loop we delete the last half of sorted array
+
+        if (cur_elems.value.size() >= max_elems * 2)
         {
-            /// To optimize, we sort (2 * max_size) elements of input array over and over again and
-            /// after each loop we delete the last half of sorted array
-            bool sorted = false, part_sorted = false;
-            while (!sorted)
-            {
-                if (cur_elems.value.size() >= max_elems * 2)
-                {
-                    std::sort(cur_elems.value.begin(), cur_elems.value.begin() + (max_elems * 2));
-                    cur_elems.value.erase(cur_elems.value.begin() + max_elems, cur_elems.value.begin() + (max_elems * 2));
-                    part_sorted = true;
-                }
-                else if (cur_elems.value.size() > max_elems)
-                {
-                    std::sort(cur_elems.value.begin(), cur_elems.value.end());
-                    cur_elems.value.resize(max_elems, arena);
-                    sorted = true;
-                }
-                else if (cur_elems.value.size() == max_elems)
-                {
-                    if (part_sorted)
-                        sorted = true;
-                    else
-                    {
-                        std::sort(cur_elems.value.begin(), cur_elems.value.end());
-                        sorted = true;
-                    }
-                }
-            }
+            std::sort(cur_elems.value.begin(), cur_elems.value.begin() + (max_elems * 2));
+            cur_elems.value.erase(cur_elems.value.begin() + max_elems, cur_elems.value.begin() + (max_elems * 2));
         }
     }
 
@@ -293,16 +244,10 @@ public:
 
         if (rhs_elems.value.empty())
             return;
-        mergeNoSampler(cur_elems, rhs_elems, arena);
-    }
 
-    void ALWAYS_INLINE mergeNoSampler(Data & cur_elems, const Data & rhs_elems, Arena * arena) const
-    {
         UInt64 new_elems;
         if (limit_num_elems)
         {
-            if (cur_elems.value.size() >= max_elems)
-                return;
             new_elems = std::min(rhs_elems.value.size(), static_cast<size_t>(max_elems) - cur_elems.value.size());
         }
         else
@@ -315,69 +260,65 @@ public:
     void serialize(ConstAggregateDataPtr __restrict place, WriteBuffer & buf, std::optional<size_t> /* version */) const override
     {
         writeVarUInt(data(place).value.size(), buf);
-        Arena * arena = new Arena;
-        const char * begin = arena->alignedAlloc(sizeof(Node), alignof(Node));
-        ColumnConst * column = nullptr;
 
         auto & value = data(place).value;
-        for (size_t row_num = 0; row_num < value.size(); ++row_num)
+        for (const Field & elem : value)
         {
-            column->insert(value[row_num]);
-            column->serializeValueIntoArena(row_num, *arena, begin, nullptr);
-            Node * node = Node::allocate(*column, row_num, arena);
-            node->write(buf);
-        }
-
-        if constexpr (Trait::last)
-            writeBinaryLittleEndian(data(place).total_values, buf);
-
-        if constexpr (Trait::sampler == Sampler::RNG)
-        {
-            writeBinaryLittleEndian(data(place).total_values, buf);
-            WriteBufferFromOwnString rng_buf;
-            rng_buf << data(place).rng;
-            writeStringBinary(rng_buf.str(), buf);
+            if (elem.isNull())
+            {
+                writeBinary(UInt8(1), buf);
+            }
+            else
+            {
+                writeBinary(UInt8(0), buf);
+                serialization->serializeBinary(elem, buf, {});
+            }
         }
     }
 
     void deserialize(AggregateDataPtr __restrict place, ReadBuffer & buf, std::optional<size_t> /* version */, Arena * arena) const override
     {
-        UInt64 elems;
-        readVarUInt(elems, buf);
+        size_t size = 0;
+        readVarUInt(size, buf);
 
-        if (unlikely(elems == 0))
-            return;
-
-        if (unlikely(elems > AGGREGATE_FUNCTION_GROUP_ARRAY_MAX_ARRAY_SIZE))
+        if (size > 0xFFFFFF)
             throw Exception(ErrorCodes::TOO_LARGE_ARRAY_SIZE,
-                            "Too large array size (maximum: {})", AGGREGATE_FUNCTION_GROUP_ARRAY_MAX_ARRAY_SIZE);
+                            "Too large array size (maximum: {})", 0xFFFFFF);
 
-        if (limit_num_elems && unlikely(elems > max_elems))
+        if (limit_num_elems && unlikely(size > max_elems))
             throw Exception(ErrorCodes::TOO_LARGE_ARRAY_SIZE, "Too large array size, it should not exceed {}", max_elems);
 
-        auto & value = data(place).value;
+        auto & arr = data(place).value;
 
-        value.resize_exact(elems, arena);
-        for (UInt64 i = 0; i < elems; ++i)
-            value[i] = Node::read(buf, arena);
+        arr.resize(size, arena);
+        for (size_t i = 0; i < size; ++i)
+        {
+            UInt8 is_null = 0;
+            readBinary(is_null, buf);
+            if (!is_null)
+                serialization->deserializeBinary(arr[i], buf, {});
+        }
     }
 
-    void insertResultInto(AggregateDataPtr __restrict place, IColumn & to, Arena *) const override
+    void insertResultInto(AggregateDataPtr __restrict place, IColumn & to, Arena * arena) const override
     {
         auto & column_array = assert_cast<ColumnArray &>(to);
+        auto & value = data(place).value;
+        std::sort(value.begin(), value.end());
+        if (limit_num_elems)
+            value.resize_exact(max_elems, arena);
 
         auto & offsets = column_array.getOffsets();
-        offsets.push_back(offsets.back() + data(place).value.size());
+        offsets.push_back(offsets.back() + value.size());
 
         auto & column_data = column_array.getData();
 
         if (std::is_same_v<Node, GroupArrayNodeString>)
         {
             auto & string_offsets = assert_cast<ColumnString &>(column_data).getOffsets();
-            string_offsets.reserve(string_offsets.size() + data(place).value.size());
+            string_offsets.reserve(string_offsets.size() + value.size());
         }
 
-        auto & value = data(place).value;
         for (const Field& field : value)
             column_data.insert(field);
     }
