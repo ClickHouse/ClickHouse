38fd13251bd3f528eb2d320f244efc09b690a07e
seshWCS <superalexman700@gmail.com>
2023-09-05 10:59:04 +0000

    Added casts

--- src/Compression/fuzzers/gcd_decompress_fuzzer.cpp
+++ src/Compression/fuzzers/gcd_decompress_fuzzer.cpp
@@ -3,6 +3,7 @@
 
 #include <Compression/ICompressionCodec.h>
 #include <IO/BufferWithOwnMemory.h>
+#include "base/types.h"
 
 namespace DB
 {
@@ -34,7 +35,7 @@ try
     DB::Memory<> memory;
     memory.resize(output_buffer_size + codec->getAdditionalSizeAtTheEndOfBuffer());
 
-    codec->doDecompressData(reinterpret_cast<const char *>(data), size, memory.data(), output_buffer_size);
+    codec->doDecompressData(reinterpret_cast<const char *>(data), static_cast<UInt32>(size), memory.data(), static_cast<UInt32>(output_buffer_size));
 
     return 0;
 }
