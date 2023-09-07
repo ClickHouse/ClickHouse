e761532d7ad94bd5ce351b696d1d82accaa2fee4
Robert Schulze <robert@clickhouse.com>
2023-09-07 15:00:06 +0200

    Fix style

--- src/Functions/FunctionBase64Conversion.h
+++ src/Functions/FunctionBase64Conversion.h
@@ -18,9 +18,7 @@ namespace DB
 {
 namespace ErrorCodes
 {
-    extern const int BAD_ARGUMENTS;
     extern const int ILLEGAL_COLUMN;
-    extern const int ILLEGAL_TYPE_OF_ARGUMENT;
     extern const int INCORRECT_DATA;
 }
 
