03a755732aed456166018e94278bc47d5b2fdf1e
Alexey Milovidov <milovidov@clickhouse.com>
2023-09-09 03:10:23 +0300

    Revert "Add settings for real-time updates during query execution"

--- src/Processors/QueryPlan/BuildQueryPipelineSettings.cpp
+++ src/Processors/QueryPlan/BuildQueryPipelineSettings.cpp
@@ -9,12 +9,7 @@ namespace DB
 BuildQueryPipelineSettings BuildQueryPipelineSettings::fromContext(ContextPtr from)
 {
     BuildQueryPipelineSettings settings;
-
-    const auto & context_settings = from->getSettingsRef();
-    settings.partial_result_limit = context_settings.max_rows_in_partial_result;
-    settings.partial_result_duration_ms = context_settings.partial_result_update_duration_ms.totalMilliseconds();
-
-    settings.actions_settings = ExpressionActionsSettings::fromSettings(context_settings, CompileExpressions::yes);
+    settings.actions_settings = ExpressionActionsSettings::fromSettings(from->getSettingsRef(), CompileExpressions::yes);
     settings.process_list_element = from->getProcessListElement();
     settings.progress_callback = from->getProgressCallback();
     return settings;
