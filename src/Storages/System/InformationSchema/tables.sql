0a1495fd0028ad9ad859af9d85f12a96b58e527c
slvrtrn <hypnoash@gmail.com>
2023-09-12 18:46:10 +0200

    Revert information_schema changes (new PR)

--- src/Storages/System/InformationSchema/tables.sql
+++ src/Storages/System/InformationSchema/tables.sql
@@ -1,35 +1,17 @@
 ATTACH VIEW tables
-    (
-     `table_catalog` String,
-     `table_schema` String,
-     `table_name` String,
-     `table_type` String,
-     `table_comment` String,
-     `table_collation` String,
-     `TABLE_CATALOG` String,
-     `TABLE_SCHEMA` String,
-     `TABLE_NAME` String,
-     `TABLE_TYPE` String,
-     `TABLE_COMMENT` String,
-     `TABLE_COLLATION` String
-        ) AS
-SELECT database             AS `table_catalog`,
-       database             AS `table_schema`,
-       name                 AS `table_name`,
-       comment              AS `table_comment`,
-       multiIf(
-               is_temporary, 'LOCAL TEMPORARY',
-               engine LIKE '%View', 'VIEW',
-               engine LIKE 'System%', 'SYSTEM VIEW',
-               has_own_data = 0, 'FOREIGN TABLE',
-               'BASE TABLE'
-           )                AS `table_type`,
-       'utf8mb4_0900_ai_ci' AS `table_collation`,
-
-       table_catalog        AS `TABLE_CATALOG`,
-       table_schema         AS `TABLE_SCHEMA`,
-       table_name           AS `TABLE_NAME`,
-       table_comment        AS `TABLE_COMMENT`,
-       table_type           AS `TABLE_TYPE`,
-       table_collation      AS `TABLE_COLLATION`
+(
+    `table_catalog` String,
+    `table_schema` String,
+    `table_name` String,
+    `table_type` Enum8('BASE TABLE' = 1, 'VIEW' = 2, 'FOREIGN TABLE' = 3, 'LOCAL TEMPORARY' = 4, 'SYSTEM VIEW' = 5),
+    `TABLE_CATALOG` String ALIAS table_catalog,
+    `TABLE_SCHEMA` String ALIAS table_schema,
+    `TABLE_NAME` String ALIAS table_name,
+    `TABLE_TYPE` Enum8('BASE TABLE' = 1, 'VIEW' = 2, 'FOREIGN TABLE' = 3, 'LOCAL TEMPORARY' = 4, 'SYSTEM VIEW' = 5) ALIAS table_type
+) AS
+SELECT
+    database AS table_catalog,
+    database AS table_schema,
+    name AS table_name,
+    multiIf(is_temporary, 4, engine like '%View', 2, engine LIKE 'System%', 5, has_own_data = 0, 3, 1) AS table_type
 FROM system.tables
