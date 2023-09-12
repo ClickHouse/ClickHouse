fb022c19ff0dc663adfc6fb4f21c1332e7fa3058
Vasily Nemkov <V.Nemkov@gmail.com>
2023-09-12 22:47:16 +0200

    PartitionActionBlocker

---
+++ src/Storages/MergeTree/PartitionActionBlocker.cpp
@@ -0,0 +1,71 @@
+#include <Storages/MergeTree/PartitionActionBlocker.h>
+
+#include <iostream>
+#include <sstream>
+
+namespace DB
+{
+
+ActionLock PartitionActionBlocker::cancelForPartition(const std::string & partition_id)
+{
+    std::unique_lock lock(mutex);
+    const size_t prev_size = partition_blockers.size();
+
+    ActionLock result = partition_blockers[partition_id].cancel();
+
+    // Cleanup stale `ActionBlocker` instances once in a while, to prevent unbound growth.
+    if (prev_size != partition_blockers.size() && ++cleanup_counter > 32) // 32 is arbitrary.
+        compactPartitionBlockersLocked();
+
+    return result;
+}
+
+bool PartitionActionBlocker::isCancelledForPartition(const std::string & partition_id) const
+{
+    if (isCancelled())
+        return true;
+
+    std::shared_lock lock(mutex);
+    auto p = partition_blockers.find(partition_id);
+    return p != partition_blockers.end() && p->second.isCancelled();
+}
+
+size_t PartitionActionBlocker::countPartitionBlockers() const
+{
+    std::shared_lock lock(mutex);
+    return partition_blockers.size();
+}
+
+void PartitionActionBlocker::compactPartitionBlockers()
+{
+    std::unique_lock lock(mutex);
+
+    compactPartitionBlockersLocked();
+}
+
+void PartitionActionBlocker::compactPartitionBlockersLocked()
+{
+    std::erase_if(partition_blockers, [](const auto & p)
+    {
+        return !p.second.isCancelled();
+    });
+    cleanup_counter = 0;
+}
+
+std::string PartitionActionBlocker::formatDebug() const
+{
+    std::shared_lock lock(mutex);
+
+    std::stringstream sstr;
+    sstr << "Global locks: " << global_blocker.getCounter().load()
+            << "\n\t and " << partition_blockers.size() << " partition locks";
+
+    for (const auto & p : partition_blockers)
+    {
+        sstr << "\n\t'" << p.first << "' == " << p.second.getCounter().load();
+    }
+
+    return sstr.str();
+}
+
+}
