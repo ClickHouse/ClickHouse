758562f11bc9cd65bc5a288c7cdec4679c2cce61
Robert Schulze <robert@clickhouse.com>
2023-09-13 12:55:17 +0000

    Fix style check

--- src/Functions/FunctionsStringArray.cpp
+++ src/Functions/FunctionsStringArray.cpp
@@ -5,8 +5,7 @@ namespace DB
 {
 namespace ErrorCodes
 {
-    extern const int ILLEGAL_TYPE_OF_ARGUMENT;
-    extern const int NUMBER_OF_ARGUMENTS_DOESNT_MATCH;
+    extern const int ILLEGAL_COLUMN;
 }
 
 template <typename DataType>
--- src/Functions/FunctionsStringArray.h
+++ src/Functions/FunctionsStringArray.h
@@ -23,10 +23,8 @@ namespace DB
 
 namespace ErrorCodes
 {
-    extern const int ILLEGAL_TYPE_OF_ARGUMENT;
     extern const int BAD_ARGUMENTS;
     extern const int ILLEGAL_COLUMN;
-    extern const int NUMBER_OF_ARGUMENTS_DOESNT_MATCH;
 }
 
 
