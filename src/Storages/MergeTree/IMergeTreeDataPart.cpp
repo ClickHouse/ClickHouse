72f2bfd166220e0cb94dd80e18c95202a4ac8cc0
Anton Popov <anton@clickhouse.com>
2023-09-19 05:35:57 +0000

    fix build

--- src/Storages/MergeTree/IMergeTreeDataPart.cpp
+++ src/Storages/MergeTree/IMergeTreeDataPart.cpp
@@ -2154,6 +2154,8 @@ std::optional<std::string> getIndexExtensionFromFilesystem(const IDataPartStorag
     {
         for (auto it = data_part_storage.iterate(); it->isValid(); it->next())
         {
+            const auto & extension = fs::path(it->name()).extension();
+            if (extension == getIndexExtension(true))
                 return extension;
         }
     }
