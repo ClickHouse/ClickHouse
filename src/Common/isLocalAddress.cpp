f81f0b6e3d96843205c1422cb0902f6e8bfb87c5
JackyWoo <wuchienchao@qq.com>
2023-09-20 17:42:25 +0800

    Use levenshteinDistance for neareast hostname load balancer

--- src/Common/isLocalAddress.cpp
+++ src/Common/isLocalAddress.cpp
@@ -5,6 +5,7 @@
 #include <optional>
 #include <base/types.h>
 #include <Common/Exception.h>
+#include <Common/levenshteinDistance.h>
 #include <Poco/Net/IPAddress.h>
 #include <Poco/Net/SocketAddress.h>
 
@@ -124,12 +125,7 @@ bool isLocalAddress(const Poco::Net::SocketAddress & address, UInt16 clickhouse_
 
 size_t getHostNameDifference(const std::string & local_hostname, const std::string & host)
 {
-    /// FIXME should we replace it with Levenstein distance? (we already have it in NamePrompter)
-    size_t hostname_difference = 0;
-    for (size_t i = 0; i < std::min(local_hostname.length(), host.length()); ++i)
-        if (local_hostname[i] != host[i])
-            ++hostname_difference;
-    return hostname_difference;
+    return levenshteinDistance(local_hostname, host);
 }
 
 }
