2b8a263355f79e10619ad209e3efcfebedae5d58
slvrtrn <hypnoash@gmail.com>
2023-09-21 16:11:08 +0200

    Change referential_constraints to an empty table

--- src/Storages/System/InformationSchema/referential_constraints.sql
+++ src/Storages/System/InformationSchema/referential_constraints.sql
@@ -1,4 +1,4 @@
-ATTACH VIEW referential_constraints
+CREATE TABLE referential_constraints
     (
      `constraint_catalog` String,
      `constraint_schema` String,
@@ -22,28 +22,4 @@ ATTACH VIEW referential_constraints
      `DELETE_RULE` String,
      `TABLE_NAME` String,
      `REFERENCED_TABLE_NAME` String
-) AS
-SELECT 
-    ''                        AS constraint_catalog,
-    NULL                      AS constraint_name,
-    ''                        AS constraint_schema,
-    ''                        AS unique_constraint_catalog,
-    NULL                      AS unique_constraint_name,
-    ''                        AS unique_constraint_schema,
-    ''                        AS match_option,
-    ''                        AS update_rule,
-    ''                        AS delete_rule,
-    ''                        AS table_name,
-    ''                        AS referenced_table_name,
-    constraint_catalog        AS CONSTRAINT_CATALOG,
-    constraint_name           AS CONSTRAINT_NAME,
-    constraint_schema         AS CONSTRAINT_SCHEMA,
-    unique_constraint_catalog AS UNIQUE_CONSTRAINT_CATALOG,
-    unique_constraint_name    AS UNIQUE_CONSTRAINT_NAME,
-    unique_constraint_schema  AS UNIQUE_CONSTRAINT_SCHEMA,
-    match_option              AS MATCH_OPTION,
-    update_rule               AS UPDATE_RULE,
-    delete_rule               AS DELETE_RULE,
-    table_name                AS TABLE_NAME,
-    referenced_table_name     AS REFERENCED_TABLE_NAME
-WHERE false; -- make sure this view is always empty
+) ENGINE Memory;
