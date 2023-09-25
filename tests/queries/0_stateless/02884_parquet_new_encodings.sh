bf9567aac4506102232f9cd1200e8ebae11d2c8a
avogar <avogar@clickhouse.com>
2023-09-25 18:26:29 +0000

    Fix typo in filename

--- tests/queries/0_stateless/02884_parquet_new_encodings.sh
+++ tests/queries/0_stateless/02884_parquet_new_encodings.sh
@@ -5,5 +5,5 @@ CURDIR=$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)
 # shellcheck source=../shell_config.sh
 . "$CURDIR"/../shell_config.sh
 
-$CLICKHOUSE_LOCAL -q "select * from file('$CURDIR/data_parquet/delta_lenght_byte_array_encoding.parquet')"
+$CLICKHOUSE_LOCAL -q "select * from file('$CURDIR/data_parquet/delta_length_byte_array_encoding.parquet')"
 
