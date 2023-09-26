1506a42e014fdd386fd3569dc6e18bcbc9b0ae24
VanDarkholme7 <980181529@qq.com>
2023-09-26 02:32:05 +0000

    fix

--- src/Analyzer/Passes/QueryAnalysisPass.cpp
+++ src/Analyzer/Passes/QueryAnalysisPass.cpp
@@ -3051,6 +3051,8 @@ QueryTreeNodePtr QueryAnalyzer::tryResolveIdentifierFromJoin(const IdentifierLoo
     JoinKind join_kind = from_join_node.getKind();
     bool join_use_nulls = scope.context->getSettingsRef().join_use_nulls;
 
+    /// If columns from left or right table were missed Object(Nullable('json')) subcolumns, they will be replaced
+    /// to ConstantNode(NULL), which can't be cast to ColumnNode, so we resolve it here.
     if (left_resolved_identifier && right_resolved_identifier && left_resolved_identifier->getNodeType() == QueryTreeNodeType::CONSTANT
         && right_resolved_identifier->getNodeType() == QueryTreeNodeType::CONSTANT)
     {
