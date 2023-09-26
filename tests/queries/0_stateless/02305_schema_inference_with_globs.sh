e67536b75347b950178818f8a2e82ca62298f78d
avogar <avogar@clickhouse.com>
2023-09-26 07:36:50 +0000

    Fix test

--- tests/queries/0_stateless/02305_schema_inference_with_globs.sh
+++ tests/queries/0_stateless/02305_schema_inference_with_globs.sh
@@ -13,5 +13,6 @@ $CLICKHOUSE_CLIENT -q "desc file('${CLICKHOUSE_TEST_UNIQUE_NAME}_data*.jsonl') s
 $CLICKHOUSE_CLIENT -q "desc file('${CLICKHOUSE_TEST_UNIQUE_NAME}_data*.jsonl') settings input_format_max_rows_to_read_for_schema_inference=24, input_format_json_infer_incomplete_types_as_strings=0" 2>&1 | grep -c 'ONLY_NULLS_WHILE_READING_SCHEMA';
 
 $CLICKHOUSE_CLIENT -q "insert into function file(${CLICKHOUSE_TEST_UNIQUE_NAME}_data4.jsonl) select number % 2 ? number::UInt32 : NULL as x from numbers(10)"
-$CLICKHOUSE_CLIENT -q "desc file('${CLICKHOUSE_TEST_UNIQUE_NAME}_data*.jsonl') settings input_format_max_rows_to_read_for_schema_inference=32"
-$CLICKHOUSE_CLIENT -q "desc file('${CLICKHOUSE_TEST_UNIQUE_NAME}_data*.jsonl') settings input_format_max_rows_to_read_for_schema_inference=100"
+$CLICKHOUSE_CLIENT -q "desc file('${CLICKHOUSE_TEST_UNIQUE_NAME}_data*.jsonl') settings input_format_max_rows_to_read_for_schema_inference=32, input_format_json_infer_incomplete_types_as_strings=0"
+$CLICKHOUSE_CLIENT -q "desc file('${CLICKHOUSE_TEST_UNIQUE_NAME}_data*.jsonl') settings input_format_max_rows_to_read_for_schema_inference=100, input_format_json_infer_incomplete_types_as_strings=0"
+
