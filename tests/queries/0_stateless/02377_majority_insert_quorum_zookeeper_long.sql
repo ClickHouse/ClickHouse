db63a1c1558ba503433aeafd556a969f023947a5
Igor Nikonov <igor@clickhouse.com>
2023-09-27 19:39:07 +0000

    Fix waiting for quorum timeout in test

--- tests/queries/0_stateless/02377_majority_insert_quorum_zookeeper_long.sql
+++ tests/queries/0_stateless/02377_majority_insert_quorum_zookeeper_long.sql
@@ -39,7 +39,7 @@ SYSTEM STOP FETCHES quorum3;
 INSERT INTO quorum1 VALUES (1, '2018-11-15');
 SELECT x FROM quorum1 ORDER BY x;
 SELECT x FROM quorum2 ORDER BY x;
-SELECT x FROM quorum3 ORDER BY x; -- {serverError 289}
+SELECT x FROM quorum3 ORDER BY x; -- {serverError REPLICA_IS_NOT_IN_QUORUM}
 
 -- Sync replica 3
 SYSTEM START FETCHES quorum3;
@@ -50,7 +50,7 @@ SELECT x FROM quorum3 ORDER BY x;
 SYSTEM STOP FETCHES quorum2;
 SYSTEM STOP FETCHES quorum3;
 SET insert_quorum_timeout = 5000;
-INSERT INTO quorum1 VALUES (2, '2018-11-15'); -- { serverError 319 }
+INSERT INTO quorum1 VALUES (2, '2018-11-15'); -- { serverError UNKNOWN_STATUS_OF_INSERT }
 SELECT x FROM quorum1 ORDER BY x;
 SELECT x FROM quorum2 ORDER BY x;
 SELECT x FROM quorum3 ORDER BY x;
@@ -61,6 +61,7 @@ SYSTEM SYNC REPLICA quorum2;
 SYSTEM START FETCHES quorum3;
 SYSTEM SYNC REPLICA quorum3;
 
+SET insert_quorum_timeout = 600000; -- default value
 INSERT INTO quorum1 VALUES (3, '2018-11-15');
 SELECT x FROM quorum1 ORDER BY x;
 SYSTEM SYNC REPLICA quorum2;
