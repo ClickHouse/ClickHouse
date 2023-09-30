a6968a9a0243251bf5e3b9e677bd90ab42706623
Gabriel Archer <118470133+garcher22@users.noreply.github.com>
2023-09-30 13:14:09 +0200

    simpler

--- tests/queries/0_stateless/02890_untuple_column_names.sql
+++ tests/queries/0_stateless/02890_untuple_column_names.sql
@@ -1,13 +1,20 @@
 -- { echoOn }
 
+-- If the untuple() function has an alias, and the tuple element has an explicit name,
+-- we want to use it to generate the resulting column name. Check all permutations of
+-- aliases. Also use different tuple types to check that we don't generate different
+-- columns with the same name (see #26179).
+
+select untuple(tuple(1)::Tuple(a Int)), untuple(tuple('s')::Tuple(a String)) format TSVWithNames;
+
+select untuple(tuple(1)::Tuple(a Int)) x, untuple(tuple('s')::Tuple(a String)) y format TSVWithNames;
+
+select untuple(tuple(1)::Tuple(Int)) x, untuple(tuple('s')::Tuple(String)) y format TSVWithNames;
+
+select untuple(tuple(1)::Tuple(Int)), untuple(tuple('s')::Tuple(String)) format TSVWithNames;
+
 -- tuple() loses the column names (would be good to fix, see #36773)
-select untuple(tuple(1 as a, 2 as b)) format TSVWithNames;
+select untuple(tuple(1 as a)) as t format TSVWithNames;
 
--- explicitly specified tuple element names are used as column names
-select untuple(tuple(1, 2)::Tuple(a Int, b Int)) format TSVWithNames;
-
--- indexes are used when untuple() has an alias (backward compatibility)
-select untuple(tuple(1, 2)::Tuple(a Int, b Int)) u format TSVWithNames;
-
--- default names are used when there are no tuple element names and no untuple alias
-select untuple(tuple(1, 2)::Tuple(Int, Int)) format TSVWithNames;
+-- thankfully JSONExtract() keeps them
+select untuple(JSONExtract('{"key": "value"}', 'Tuple(key String)')) x format TSVWithNames;
