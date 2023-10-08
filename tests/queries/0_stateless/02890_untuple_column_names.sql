1da5e7d6cee2e565b21ca703ee2b3e9b53eaff8b
Robert Schulze <robert@clickhouse.com>
2023-10-08 09:32:02 +0000

    Some fixups

--- tests/queries/0_stateless/02890_untuple_column_names.sql
+++ tests/queries/0_stateless/02890_untuple_column_names.sql
@@ -1,20 +1,28 @@
--- { echoOn }
-
 -- If the untuple() function has an alias, and the tuple element has an explicit name,
 -- we want to use it to generate the resulting column name. Check all permutations of
 -- aliases. Also use different tuple types to check that we don't generate different
 -- columns with the same name (see #26179).
 
-select untuple(tuple(1)::Tuple(a Int)), untuple(tuple('s')::Tuple(a String)) format TSVWithNames;
+SELECT '-- tuple element alias';
+SELECT untuple(tuple(1)::Tuple(a Int)), untuple(tuple('s')::Tuple(a String)) FORMAT TSVWithNames SETTINGS allow_experimental_analyzer = 0;
+SELECT untuple(tuple(1)::Tuple(a Int)), untuple(tuple('s')::Tuple(a String)) FORMAT TSVWithNames SETTINGS allow_experimental_analyzer = 1;
 
-select untuple(tuple(1)::Tuple(a Int)) x, untuple(tuple('s')::Tuple(a String)) y format TSVWithNames;
+SELECT '-- tuple element alias, untuple() alias';
+SELECT untuple(tuple(1)::Tuple(a Int)) x, untuple(tuple('s')::Tuple(a String)) y FORMAT TSVWithNames SETTINGS allow_experimental_analyzer = 0;
+SELECT untuple(tuple(1)::Tuple(a Int)) x, untuple(tuple('s')::Tuple(a String)) y FORMAT TSVWithNames SETTINGS allow_experimental_analyzer = 1;
 
-select untuple(tuple(1)::Tuple(Int)) x, untuple(tuple('s')::Tuple(String)) y format TSVWithNames;
+SELECT '-- untuple() alias';
+SELECT untuple(tuple(1)::Tuple(Int)) x, untuple(tuple('s')::Tuple(String)) y FORMAT TSVWithNames SETTINGS allow_experimental_analyzer = 0;
+SELECT untuple(tuple(1)::Tuple(Int)) x, untuple(tuple('s')::Tuple(String)) y FORMAT TSVWithNames SETTINGS allow_experimental_analyzer = 1;
 
-select untuple(tuple(1)::Tuple(Int)), untuple(tuple('s')::Tuple(String)) format TSVWithNames;
+SELECT '-- no aliases';
+SELECT untuple(tuple(1)::Tuple(Int)), untuple(tuple('s')::Tuple(String)) FORMAT TSVWithNames SETTINGS allow_experimental_analyzer = 0;
+SELECT untuple(tuple(1)::Tuple(Int)), untuple(tuple('s')::Tuple(String)) FORMAT TSVWithNames SETTINGS allow_experimental_analyzer = 1;
 
--- tuple() loses the column names (would be good to fix, see #36773)
-select untuple(tuple(1 as a)) as t format TSVWithNames;
+SELECT '-- tuple() loses the column names (would be good to fix, see #36773)';
+SELECT untuple(tuple(1 as a)) as t FORMAT TSVWithNames SETTINGS allow_experimental_analyzer = 0;
+SELECT untuple(tuple(1 as a)) as t FORMAT TSVWithNames SETTINGS allow_experimental_analyzer = 1;
 
--- thankfully JSONExtract() keeps them
-select untuple(JSONExtract('{"key": "value"}', 'Tuple(key String)')) x format TSVWithNames;
+SELECT '-- thankfully JSONExtract() keeps them';
+SELECT untuple(JSONExtract('{"key": "value"}', 'Tuple(key String)')) x FORMAT TSVWithNames SETTINGS allow_experimental_analyzer = 0;
+SELECT untuple(JSONExtract('{"key": "value"}', 'Tuple(key String)')) x FORMAT TSVWithNames SETTINGS allow_experimental_analyzer = 1;
