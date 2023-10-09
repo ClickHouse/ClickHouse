4499dd3e2f5f515343c8a5751cf6a72afd178622
凌涛 <lingtaolf@gmail.com>
2023-10-09 19:03:17 +0800

    modify style

--- tests/queries/0_stateless/02891_rename_table_without_keyword.sql
+++ tests/queries/0_stateless/02891_rename_table_without_keyword.sql
@@ -1,7 +1,7 @@
 DROP DATABASE IF EXISTS rename_db;
-CREATE DATABASE rename_db;
+CREATE DATABASE IF NOT EXISTS rename_db;
 
-CREATE TABLE rename_db.r1 (name String) Engine=Memory();
+CREATE TABLE IF NOT EXISTS rename_db.r1 (name String) Engine=Memory();
 SHOW TABLES FROM rename_db;
 
 RENAME TABLE rename_db.r1 TO rename_db.r2;
@@ -10,5 +10,29 @@ SHOW TABLES FROM rename_db;
 RENAME rename_db.r2 TO rename_db.r3;
 SHOW TABLES FROM rename_db;
 
-DROP DATABASE rename_db;
+CREATE TABLE IF NOT EXISTS rename_db.source_table (
+    id UInt64,
+    value String
+) ENGINE = Memory;
+
+CREATE DICTIONARY IF NOT EXISTS rename_db.test_dictionary
+(
+    id UInt64,
+    value String
+)
+PRIMARY KEY id
+SOURCE(CLICKHOUSE(TABLE 'rename_db.dictionary_table'))
+LAYOUT(FLAT())
+LIFETIME(MIN 0 MAX 1000);
+
+SHOW DICTIONARIES FROM rename_db;
+
+RENAME rename_db.test_dictionary TO rename_db.test_dictionary_2; -- { serverError UNKNOWN_TABLE }
+SHOW DICTIONARIES FROM rename_db;
+
+SHOW DATABASES LIKE 'rename_db';
+RENAME rename_db TO rename_db_2; -- { serverError UNKNOWN_TABLE }
+SHOW DATABASES LIKE 'rename_db';
+
+DROP DATABASE IF EXISTS rename_db;
 
