a97ce4cbfcc16b98e0179614ad536ef5f621d659
flynn <fenglv15@mails.ucas.ac.cn>
2023-10-12 09:15:54 +0000

    update test

--- tests/queries/0_stateless/02895_forbid_create_inverted_index.sql
+++ tests/queries/0_stateless/02895_forbid_create_inverted_index.sql
@@ -1,5 +1,6 @@
 SET allow_experimental_inverted_index = 0;
-CREATE OR REPLACE TABLE tab
+DROP TABLE IF EXISTS tab;
+CREATE TABLE tab
 (
     `key` UInt64,
     `str` String
@@ -8,3 +9,5 @@ ENGINE = MergeTree
 ORDER BY key;
 
 ALTER TABLE tab ADD INDEX inv_idx(str) TYPE inverted(0); -- { serverError SUPPORT_IS_DISABLED }
+
+DROP TABLE tab;
