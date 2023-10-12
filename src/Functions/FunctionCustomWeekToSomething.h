bc2f9fdf45a4ccd1461d6c6d52af5f7c71dcb052
slvrtrn <hypnoash@gmail.com>
2023-10-12 20:51:13 +0200

    Support String argument in toDayOfWeek function

--- src/Functions/FunctionCustomWeekToSomething.h
+++ src/Functions/FunctionCustomWeekToSomething.h
@@ -1,4 +1,5 @@
 #pragma once
+#include <DataTypes/DataTypeString.h>
 #include <Functions/IFunctionCustomWeek.h>
 
 namespace DB
@@ -9,7 +10,6 @@ namespace ErrorCodes
     extern const int ILLEGAL_TYPE_OF_ARGUMENT;
 }
 
-
 /// See CustomWeekTransforms.h
 template <typename ToDataType, typename Transform>
 class FunctionCustomWeekToSomething : public IFunctionCustomWeek<Transform>
@@ -19,8 +19,64 @@ public:
 
     DataTypePtr getReturnTypeImpl(const ColumnsWithTypeAndName & arguments) const override
     {
-        this->checkArguments(arguments);
-
+        // Adjusted from IFunctionCustomWeek::checkArguments to support String arguments,
+        // but only for the functions that use FunctionCustomWeekToSomething at the moment (see toDayOfWeek.cpp).
+        // If we decide to support String arguments for all the functions that use IFunctionCustomWeek,
+        // makes sense to just add !isString check to the FunctionCustomWeekToSomething::checkArguments and use it instead.
+        if (arguments.size() == 1)
+        {
+            auto t0 = arguments[0].type;
+            if (!isDate(t0) && !isDate32(t0) && !isDateTime(t0) && !isDateTime64(t0) && !isString(t0))
+                throw Exception(
+                    ErrorCodes::ILLEGAL_TYPE_OF_ARGUMENT,
+                    "Illegal type {} of argument of function {}. Must be Date, Date32, DateTime or DateTime64.",
+                    arguments[0].type->getName(),
+                    this->getName());
+        }
+        else if (arguments.size() == 2)
+        {
+            auto t0 = arguments[0].type;
+            if (!isDate(t0) && !isDate32(t0) && !isDateTime(t0) && !isDateTime64(t0) && !isString(t0))
+                throw Exception(
+                    ErrorCodes::ILLEGAL_TYPE_OF_ARGUMENT,
+                    "Illegal type {} of 1st argument of function {}. Must be Date, Date32, DateTime or DateTime64.",
+                    arguments[0].type->getName(),
+                    this->getName());
+            if (!isUInt8(arguments[1].type))
+                throw Exception(
+                    ErrorCodes::ILLEGAL_TYPE_OF_ARGUMENT,
+                    "Illegal type {} of 2nd (optional) argument of function {}. Must be constant UInt8 (week mode).",
+                    arguments[1].type->getName(),
+                    this->getName());
+        }
+        else if (arguments.size() == 3)
+        {
+            auto t0 = arguments[0].type;
+            if (!isDate(t0) && !isDate32(t0) && !isDateTime(t0) && !isDateTime64(t0) && !isString(t0))
+                throw Exception(
+                    ErrorCodes::ILLEGAL_TYPE_OF_ARGUMENT,
+                    "Illegal type {} of argument of function {}. Must be Date, Date32, DateTime or DateTime64",
+                    arguments[0].type->getName(),
+                    this->getName());
+            if (!isUInt8(arguments[1].type))
+                throw Exception(
+                    ErrorCodes::ILLEGAL_TYPE_OF_ARGUMENT,
+                    "Illegal type {} of 2nd (optional) argument of function {}. Must be constant UInt8 (week mode).",
+                    arguments[1].type->getName(),
+                    this->getName());
+            if (!isString(arguments[2].type))
+                throw Exception(
+                    ErrorCodes::ILLEGAL_TYPE_OF_ARGUMENT,
+                    "Illegal type {} of 3rd (optional) argument of function {}. Must be constant string (timezone name).",
+                    arguments[2].type->getName(),
+                    this->getName());
+        }
+        else
+            throw Exception(
+                ErrorCodes::NUMBER_OF_ARGUMENTS_DOESNT_MATCH,
+                "Number of arguments for function {} doesn't match: passed {}, expected 1, 2 or 3.",
+                this->getName(),
+                arguments.size());
         return std::make_shared<ToDataType>();
     }
 
@@ -30,19 +86,22 @@ public:
         WhichDataType which(from_type);
 
         if (which.isDate())
-            return CustomWeekTransformImpl<DataTypeDate, ToDataType>::execute(
-                arguments, result_type, input_rows_count, Transform{});
+            return CustomWeekTransformImpl<DataTypeDate, ToDataType>::execute(arguments, result_type, input_rows_count, Transform{});
         else if (which.isDate32())
-            return CustomWeekTransformImpl<DataTypeDate32, ToDataType>::execute(
-                arguments, result_type, input_rows_count, Transform{});
+            return CustomWeekTransformImpl<DataTypeDate32, ToDataType>::execute(arguments, result_type, input_rows_count, Transform{});
         else if (which.isDateTime())
-            return CustomWeekTransformImpl<DataTypeDateTime, ToDataType>::execute(
-                arguments, result_type, input_rows_count, Transform{});
+            return CustomWeekTransformImpl<DataTypeDateTime, ToDataType>::execute(arguments, result_type, input_rows_count, Transform{});
         else if (which.isDateTime64())
         {
             return CustomWeekTransformImpl<DataTypeDateTime64, ToDataType>::execute(
-                arguments, result_type, input_rows_count,
+                arguments,
+                result_type,
+                input_rows_count,
                 TransformDateTime64<Transform>{assert_cast<const DataTypeDateTime64 *>(from_type)->getScale()});
+        }
+        else if (which.isString())
+        {
+            return CustomWeekTransformImpl<DataTypeString, ToDataType>::execute(arguments, result_type, input_rows_count, Transform{});
         }
         else
             throw Exception(ErrorCodes::ILLEGAL_TYPE_OF_ARGUMENT,
--- tests/queries/0_stateless/01661_test_toDayOfWeek_mysql_compatibility.sql
+++ tests/queries/0_stateless/01661_test_toDayOfWeek_mysql_compatibility.sql
@@ -1,3 +1,27 @@
 -- Tags: no-fasttest
 
-SELECT DAYOFWEEK(toDateTime('2016-06-15 23:00:00'));
+SELECT '--- Usage with a constant argument';
+
+SELECT DAYOFWEEK(toDateTime('2016-06-15 23:00:00')), DAYOFWEEK('2016-06-15'), DAYOFWEEK('2016-06-15 23:00:00'), DAYOFWEEK('2016-06-15 23:00:00.123456');
+SELECT DAYOFWEEK('invalid'); -- { serverError CANNOT_PARSE_DATETIME }
+
+SELECT '--- Usage with a table field';
+
+CREATE OR REPLACE TABLE `01661_test_toDayOfWeek_mysql_compatibility`
+(
+    d        Date,
+    dt       DateTime('UTC'),
+    dt64     DateTime64(6, 'UTC'),
+    str_d    String,
+    str_dt   String,
+    str_dt64 String,
+    invalid  String
+) ENGINE MergeTree ORDER BY dt;
+
+INSERT INTO `01661_test_toDayOfWeek_mysql_compatibility` VALUES (toDate('2017-09-13'), toDateTime('2017-09-13 19:10:22', 'UTC'), toDateTime64('2017-09-13 19:10:22.123456', 6, 'UTC'), '2017-09-13', '2017-09-13 19:10:22', '2017-09-13 19:10:22.123456', 'foo');
+INSERT INTO `01661_test_toDayOfWeek_mysql_compatibility` VALUES (toDate('2017-09-24'), toDateTime('2017-09-24 12:05:34', 'UTC'), toDateTime64('2017-09-24 12:05:34.123456', 6, 'UTC'), '2017-09-24', '2017-09-24 12:05:34', '2017-09-24 12:05:34.123456', 'bar');
+INSERT INTO `01661_test_toDayOfWeek_mysql_compatibility` VALUES (toDate('2018-01-29'), toDateTime('2018-01-29 02:09:48', 'UTC'), toDateTime64('2018-01-29 02:09:48.123456', 6, 'UTC'), '2018-01-29', '2018-01-29 02:09:48', '2018-01-29 02:09:48.123456', 'qaz');
+INSERT INTO `01661_test_toDayOfWeek_mysql_compatibility` VALUES (toDate('2019-02-21'), toDateTime('2019-02-21 15:07:43', 'UTC'), toDateTime64('2019-02-21 15:07:43.123456', 6, 'UTC'), '2019-02-21', '2019-02-21 15:07:43', '2019-02-21 15:07:43.123456', 'qux');
+
+SELECT DAYOFWEEK(d), DAYOFWEEK(dt), DAYOFWEEK(dt64), DAYOFWEEK(str_d), DAYOFWEEK(str_dt), DAYOFWEEK(str_dt64) FROM `01661_test_toDayOfWeek_mysql_compatibility` ORDER BY d;
+SELECT DAYOFWEEK(invalid) FROM `01661_test_toDayOfWeek_mysql_compatibility`; -- { serverError CANNOT_PARSE_DATETIME }
