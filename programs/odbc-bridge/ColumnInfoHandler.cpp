aae3894c23ff8bccac282a40cdadb71808fb3127
vdimir <vdimir@clickhouse.com>
2023-10-13 10:50:36 +0000

    Throw an exception in odbc-bridge if more than one table matched a single query

--- programs/odbc-bridge/ColumnInfoHandler.cpp
+++ programs/odbc-bridge/ColumnInfoHandler.cpp
@@ -145,11 +145,11 @@ void ODBCColumnsInfoHandler::handleRequest(HTTPServerRequest & request, HTTPServ
             if (tables.next())
             {
                 catalog_name = tables.table_catalog();
+                /// `tables.next()` call is mandatory to drain the iterator before next operation and avoid "Invalid cursor state"
+                if (tables.next())
+                    throw Exception(ErrorCodes::UNKNOWN_TABLE, "Driver returned more than one table for '{}': '{}' and '{}'",
+                                    table_name, catalog_name, tables.table_schema());
                 LOG_TRACE(log, "Will fetch info for table '{}.{}'", catalog_name, table_name);
-                while (tables.next())
-                {
-                    /// drain the iterator before next operation to avoid "Invalid cursor state"
-                }
                 return catalog.find_columns(/* column = */ "", table_name, /* schema = */ "", catalog_name);
             }
 
@@ -157,11 +157,11 @@ void ODBCColumnsInfoHandler::handleRequest(HTTPServerRequest & request, HTTPServ
             if (tables.next())
             {
                 catalog_name = tables.table_catalog();
+                /// `tables.next()` call is mandatory to drain the iterator before next operation and avoid "Invalid cursor state"
+                if (tables.next())
+                    throw Exception(ErrorCodes::UNKNOWN_TABLE, "Driver returned more than one table for '{}': '{}' and '{}'",
+                                    table_name, catalog_name, tables.table_schema());
                 LOG_TRACE(log, "Will fetch info for table '{}.{}.{}'", catalog_name, schema_name, table_name);
-                while (tables.next())
-                {
-                    /// drain the iterator before next operation to avoid "Invalid cursor state"
-                }
                 return catalog.find_columns(/* column = */ "", table_name, schema_name, catalog_name);
             }
 
