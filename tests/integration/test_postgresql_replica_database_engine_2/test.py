19e7a8ec9a4cd9a3fc361c80c015978fd0e097c8
Kseniia Sumarokova <54203879+kssenii@users.noreply.github.com>
2023-10-15 14:19:44 +0200

    Update test.py

--- tests/integration/test_postgresql_replica_database_engine_2/test.py
+++ tests/integration/test_postgresql_replica_database_engine_2/test.py
@@ -719,51 +719,6 @@ def test_too_many_parts(started_cluster):
     pg_manager2.drop_materialized_db()
 
 
-def test_replica_consumer(started_cluster):
-    table = "test_replica_consumer"
-
-    pg_manager_replica = PostgresManager()
-    pg_manager_replica.init(
-        instance2,
-        cluster.postgres_ip,
-        cluster.postgres_port,
-        default_database="postgres_database",
-        postgres_db_exists=True,
-    )
-
-    for pm in [pg_manager, pg_manager_replica]:
-        pm.create_and_fill_postgres_table(table)
-        pm.create_materialized_db(
-            ip=started_cluster.postgres_ip,
-            port=started_cluster.postgres_port,
-            settings=[
-                f"materialized_postgresql_tables_list = '{table}'",
-                "materialized_postgresql_backoff_min_ms = 100",
-                "materialized_postgresql_backoff_max_ms = 100",
-                "materialized_postgresql_use_unique_replication_consumer_identifier = 1",
-            ],
-        )
-
-    assert 50 == int(instance.query(f"SELECT count() FROM test_database.{table}"))
-    assert 50 == int(instance2.query(f"SELECT count() FROM test_database.{table}"))
-
-    instance.query(
-        f"INSERT INTO postgres_database.{table} SELECT number, number from numbers(1000, 1000)"
-    )
-
-    check_tables_are_synchronized(
-        instance, table, postgres_database=pg_manager.get_default_database()
-    )
-    check_tables_are_synchronized(
-        instance2, table, postgres_database=pg_manager_replica.get_default_database()
-    )
-
-    assert 1050 == int(instance.query(f"SELECT count() FROM test_database.{table}"))
-    assert 1050 == int(instance2.query(f"SELECT count() FROM test_database.{table}"))
-
-    pg_manager_replica.clear()
-
-
 def test_toast(started_cluster):
     table = "test_toast"
     pg_manager.execute(
@@ -792,6 +747,58 @@ VALUES (1, (SELECT array_to_string(ARRAY(SELECT chr((100 + round(random() * 25))
         postgres_database=pg_manager.get_default_database(),
         order_by="id",
     )
+
+
+def test_replica_consumer(started_cluster):
+    table = "test_replica_consumer"
+
+    pg_manager_replica = PostgresManager()
+    pg_manager_replica.init(
+        instance2,
+        cluster.postgres_ip,
+        cluster.postgres_port,
+        default_database="postgres_database",
+        postgres_db_exists=True,
+    )
+
+    for pm in [pg_manager, pg_manager_replica]:
+        pm.create_and_fill_postgres_table(table)
+        pm.create_materialized_db(
+            ip=started_cluster.postgres_ip,
+            port=started_cluster.postgres_port,
+            settings=[
+                f"materialized_postgresql_tables_list = '{table}'",
+                "materialized_postgresql_backoff_min_ms = 100",
+                "materialized_postgresql_backoff_max_ms = 100",
+                "materialized_postgresql_use_unique_replication_consumer_identifier = 1",
+            ],
+        )
+
+    check_tables_are_synchronized(
+        instance, table, postgres_database=pg_manager.get_default_database()
+    )
+    check_tables_are_synchronized(
+        instance2, table, postgres_database=pg_manager_replica.get_default_database()
+    )
+
+    assert 50 == int(instance.query(f"SELECT count() FROM test_database.{table}"))
+    assert 50 == int(instance2.query(f"SELECT count() FROM test_database.{table}"))
+
+    instance.query(
+        f"INSERT INTO postgres_database.{table} SELECT number, number from numbers(1000, 1000)"
+    )
+
+    check_tables_are_synchronized(
+        instance, table, postgres_database=pg_manager.get_default_database()
+    )
+    check_tables_are_synchronized(
+        instance2, table, postgres_database=pg_manager_replica.get_default_database()
+    )
+
+    assert 1050 == int(instance.query(f"SELECT count() FROM test_database.{table}"))
+    assert 1050 == int(instance2.query(f"SELECT count() FROM test_database.{table}"))
+
+    pg_manager_replica.clear()
 
 
 if __name__ == "__main__":
