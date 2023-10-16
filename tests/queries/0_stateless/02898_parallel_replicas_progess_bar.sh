97517ff9ea29249ce8be2db57617262df4822c4d
Igor Nikonov <igor@clickhouse.com>
2023-10-16 19:42:37 +0000

    Fix test flakyness

--- tests/queries/0_stateless/02898_parallel_replicas_progess_bar.sh
+++ tests/queries/0_stateless/02898_parallel_replicas_progess_bar.sh
@@ -21,4 +21,10 @@ system sync replica t1;
 system sync replica t2;
 system sync replica t3;
 "
-${CLICKHOUSE_CURL} -sS "${CLICKHOUSE_URL}&send_progress_in_http_headers=1&http_headers_progress_interval_ms=0" -d @- <<< "SELECT count(), min(k), max(k), avg(k) FROM t1 SETTINGS allow_experimental_parallel_reading_from_replicas = 1, max_parallel_replicas = 3, prefer_localhost_replica = 0, use_hedged_requests=0, cluster_for_parallel_replicas='test_cluster_one_shard_three_replicas_localhost'" -v |& grep -e "X-ClickHouse-Progress:" | sed 's/,\"elapsed_ns[^}]*//' | grep -F -m 1 '"total_rows_to_read":"3000"'
+
+${CLICKHOUSE_CURL} -sS "${CLICKHOUSE_URL}&send_progress_in_http_headers=1&http_headers_progress_interval_ms=0" -d @- <<< "SELECT count(), min(k), max(k), avg(k) FROM t1 SETTINGS allow_experimental_parallel_reading_from_replicas = 1, max_parallel_replicas = 3, prefer_localhost_replica = 0, use_hedged_requests=0, cluster_for_parallel_replicas='test_cluster_one_shard_three_replicas_localhost'" -v |& grep -e "X-ClickHouse-Progress:" | sed 's/,\"elapsed_ns[^}]*//' | grep -F -m 1 '"total_rows_to_read":"3000"' | sed 's/\"read_rows[^,]*//' | sed 's/,\"read_bytes[^,]*//'
+
+$CLICKHOUSE_CLIENT -nm -q "
+DROP TABLE t1 SYNC;
+DROP TABLE t2 SYNC;
+DROP TABLE t3 SYNC;"
