26c0c0b86d0c570254b58d6db457ace6ecf0b749
Alexey Milovidov <milovidov@clickhouse.com>
2023-10-16 02:19:24 +0200

    More tests

--- tests/queries/0_stateless/02899_indexing_by_space_filling_curves.sql
+++ tests/queries/0_stateless/02899_indexing_by_space_filling_curves.sql
@@ -20,4 +20,16 @@ INSERT INTO test SELECT number DIV 32, number % 32 FROM numbers(1024);
 SET max_rows_to_read = 200, force_primary_key = 1, analyze_index_with_space_filling_curves = 1;
 SELECT count() FROM test WHERE x >= 10 AND x <= 20 AND y >= 20 AND y <= 30;
 
+-- Various other conditions
+
+SELECT count() FROM test WHERE x = 10 SETTINGS max_rows_to_read = 64;
+SELECT count() FROM test WHERE x = 10 AND y > 10 SETTINGS max_rows_to_read = 42;
+SELECT count() FROM test WHERE x = 10 AND y < 10 SETTINGS max_rows_to_read = 20;
+
+SELECT count() FROM test WHERE y = 10 SETTINGS max_rows_to_read = 48;
+SELECT count() FROM test WHERE x >= 10 AND y = 10 SETTINGS max_rows_to_read = 33;
+SELECT count() FROM test WHERE y = 10 AND x <= 10 SETTINGS max_rows_to_read = 17;
+
+SELECT count() FROM test PREWHERE x >= 10 WHERE x < 11 AND y = 10 SETTINGS max_rows_to_read = 3;
+
 DROP TABLE test;
