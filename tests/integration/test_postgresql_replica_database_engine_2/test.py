e7977fe41d17ed1501be01fd6066c7e25f30d03d
kssenii <sumarokovakseniia@mail.ru>
2023-10-18 13:01:35 +0200

    Fix black check

--- tests/integration/test_postgresql_replica_database_engine_2/test.py
+++ tests/integration/test_postgresql_replica_database_engine_2/test.py
@@ -759,6 +759,7 @@ VALUES (1, (SELECT array_to_string(ARRAY(SELECT chr((100 + round(random() * 25))
         order_by="id",
     )
 
+
 def test_replica_consumer(started_cluster):
     table = "test_replica_consumer"
     pg_manager_instance2.restart()
