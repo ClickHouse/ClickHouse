414b5ed8639f7830c4af03be965feaff420d36e1
flynn <fenglv15@mails.ucas.ac.cn>
2023-10-20 17:31:33 +0000

    fix test

--- tests/queries/0_stateless/02896_union_distinct_http_format.sh
+++ tests/queries/0_stateless/02896_union_distinct_http_format.sh
@@ -4,4 +4,4 @@ CURDIR=$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)
 # shellcheck source=../shell_config.sh
 . "$CURDIR"/../shell_config.sh
 
-echo 'SELECT 1 UNION DISTINCT SELECT 2 FORMAT PrettyCompactMonoBlock' | curl 'http://localhost:8123/' --data-binary @-
+curl -d@- -sS "${CLICKHOUSE_URL}" <<< 'SELECT 1 as a UNION DISTINCT SELECT 2 as a order by a FORMAT PrettyCompactMonoBlock'
