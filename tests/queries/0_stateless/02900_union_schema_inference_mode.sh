de90e5fbb66abc61349a42e854b54a0531829a3b
Kruglov Pavel <48961922+Avogar@users.noreply.github.com>
2023-10-20 23:39:09 +0200

    Try fix style

--- tests/queries/0_stateless/02900_union_schema_inference_mode.sh
+++ tests/queries/0_stateless/02900_union_schema_inference_mode.sh
@@ -6,8 +6,6 @@ CURDIR=$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)
 . "$CURDIR"/../shell_config.sh
 
 mkdir -p $CLICKHOUSE_TEST_UNIQUE_NAME
-rm -rf "${CLICKHOUSE_TEST_UNIQUE_NAME}/*"
-
 echo '{"a" : 1, "obj" : {"f1" : 1, "f2" : "2020-01-01"}}' > $CLICKHOUSE_TEST_UNIQUE_NAME/data1.jsonl
 echo '{"b" : 2, "obj" : {"f3" : 2, "f2" : "Some string"}}' > $CLICKHOUSE_TEST_UNIQUE_NAME/data2.jsonl
 echo '{"c" : "hello"}' > $CLICKHOUSE_TEST_UNIQUE_NAME/data3.jsonl
