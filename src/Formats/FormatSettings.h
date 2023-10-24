23c47cdc39f0ef6bc4fb5d991a67474bb93b7f39
李扬 <654010905@qq.com>
2023-10-25 03:08:17 +0800

    Support orc filter push down (file + stripe + rowgroup level) (#55330)
    * support orc filter push down
    
    * update orc lib version
    
    * replace setqueryinfo with setkeycondition
    
    * fix issue https://github.com/ClickHouse/ClickHouse/issues/53536
    
    * refactor source with key condition
    
    * fix building error
    
    * remove std::cout
    
    * update orc
    
    * update orc version
    
    * fix bugs
    
    * improve code
    
    * upgrade orc lib
    
    * fix code style
    
    * change as requested
    
    * add performance tests for orc filter push down
    
    * add performance tests for orc filter push down
    
    * fix all bugs
    
    * fix default as null issue
    
    * add uts for null as default issues
    
    * upgrade orc lib
    
    * fix failed orc lib uts and fix typo
    
    * fix failed uts
    
    * fix failed uts
    
    * fix ast fuzzer tests
    
    * fix bug of uint64 overflow in https://s3.amazonaws.com/clickhouse-test-reports/55330/de22fdcaea2e12c96f300e95f59beba84401712d/fuzzer_astfuzzerubsan/report.html
    
    * fix asan fatal caused by reused column vector batch in native orc input format. refer to https://s3.amazonaws.com/clickhouse-test-reports/55330/be39d23af2d7e27f5ec7f168947cf75aeaabf674/stateless_tests__asan__[4_4].htm
    
    * fix wrong performance tests
    
    * disable 02892_orc_filter_pushdown on aarch64. https://s3.amazonaws.com/clickhouse-test-reports/55330/be39d23af2d7e27f5ec7f168947cf75aeaabf674/stateless_tests__aarch64_.html
    
    * add some comments
    
    * add some comments
    
    * inline range::equals and range::less
    
    * fix data race of key condition
    
    * trigger ci

--- src/Formats/FormatSettings.h
+++ src/Formats/FormatSettings.h
@@ -364,6 +364,8 @@ struct FormatSettings
         bool output_string_as_string = false;
         ORCCompression output_compression_method = ORCCompression::NONE;
         bool use_fast_decoder = true;
+        bool filter_push_down = true;
+        UInt64 output_row_index_stride = 10'000;
     } orc;
 
     /// For capnProto format we should determine how to
