f94c33586c8fe322220365634506b1f919ba3974
Robert Schulze <robert@clickhouse.com>
2023-10-24 15:14:20 +0000

    Fix issue #42966

--- tests/queries/0_stateless/01942_dateTimeToSnowflake.sql
+++ tests/queries/0_stateless/01942_dateTimeToSnowflake.sql
@@ -1,23 +1,43 @@
 -- Error cases
-SELECT dateTimeToSnowflake();  -- {serverError 42}
-SELECT dateTime64ToSnowflake();  -- {serverError 42}
+SELECT dateTimeToSnowflake();  -- {serverError NUMBER_OF_ARGUMENTS_DOESNT_MATCH}
+SELECT dateTime64ToSnowflake();  -- {serverError NUMBER_OF_ARGUMENTS_DOESNT_MATCH}
 
-SELECT dateTimeToSnowflake('abc');  -- {serverError 43}
-SELECT dateTime64ToSnowflake('abc');  -- {serverError 43}
+SELECT dateTimeToSnowflake('abc');  -- {serverError ILLEGAL_TYPE_OF_ARGUMENT}
+SELECT dateTime64ToSnowflake('abc');  -- {serverError ILLEGAL_TYPE_OF_ARGUMENT}
 
-SELECT dateTimeToSnowflake('abc', 123);  -- {serverError 42}
-SELECT dateTime64ToSnowflake('abc', 123);  -- {serverError 42}
+SELECT dateTimeToSnowflake('abc', 123);  -- {serverError NUMBER_OF_ARGUMENTS_DOESNT_MATCH}
+SELECT dateTime64ToSnowflake('abc', 123);  -- {serverError NUMBER_OF_ARGUMENTS_DOESNT_MATCH}
 
-SELECT 'const column';
+SELECT '-- const / non-const inputs';
+
 WITH toDateTime('2021-08-15 18:57:56', 'Asia/Shanghai') AS dt
-SELECT dt, dateTimeToSnowflake(dt);
+SELECT dt, dateTimeToSnowflake(dt), materialize(dateTimeToSnowflake(dt));
 
 WITH toDateTime64('2021-08-15 18:57:56.492', 3, 'Asia/Shanghai') AS dt64
-SELECT dt64, dateTime64ToSnowflake(dt64);
+SELECT dt64, dateTime64ToSnowflake(dt64), materialize(dateTime64ToSnowflake(dt64));
 
-SELECT 'non-const column';
-WITH toDateTime('2021-08-15 18:57:56', 'Asia/Shanghai') AS x
-SELECT materialize(x) as dt, dateTimeToSnowflake(dt);;
+SELECT '-- different DateTime64 scales';
 
-WITH toDateTime64('2021-08-15 18:57:56.492', 3, 'Asia/Shanghai') AS x
-SELECT materialize(x) as dt64, dateTime64ToSnowflake(dt64);
+WITH toDateTime64('2021-08-15 18:57:56.492', 0, 'UTC') AS dt64_0,
+     toDateTime64('2021-08-15 18:57:56.492', 1, 'UTC') AS dt64_1,
+     toDateTime64('2021-08-15 18:57:56.492', 2, 'UTC') AS dt64_2,
+     toDateTime64('2021-08-15 18:57:56.492', 3, 'UTC') AS dt64_3,
+     toDateTime64('2021-08-15 18:57:56.492', 4, 'UTC') AS dt64_4
+SELECT dateTime64ToSnowflake(dt64_0),
+       dateTime64ToSnowflake(dt64_1),
+       dateTime64ToSnowflake(dt64_2),
+       dateTime64ToSnowflake(dt64_3),
+       dateTime64ToSnowflake(dt64_4);
+
+-- DateTime64-to-Snowflake-to-DateTime64 is idempotent *if* the scale is <=3 (millisecond
+-- precision)
+WITH now64(0) AS dt64_0,
+     now64(1) AS dt64_1,
+     now64(2) AS dt64_2,
+     now64(3) AS dt64_3,
+     now64(4) AS dt64_4
+SELECT snowflakeToDateTime64(dateTime64ToSnowflake(dt64_0)) == dt64_0,
+       snowflakeToDateTime64(dateTime64ToSnowflake(dt64_1)) == dt64_1,
+       snowflakeToDateTime64(dateTime64ToSnowflake(dt64_2)) == dt64_2,
+       snowflakeToDateTime64(dateTime64ToSnowflake(dt64_3)) == dt64_3,
+       snowflakeToDateTime64(dateTime64ToSnowflake(dt64_4)) == dt64_4;
