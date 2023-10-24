c521fd7c5fad7a66dff56646d6a058f1d83b0d56
Azat Khuzhin <a3at.mail@gmail.com>
2023-10-24 16:12:47 +0200

    Limit log frequence for "Skipping send data over distributed table" message
    After SYSTEM STOP DISTRIBUTED SENDS it will constantly print this
    message.
    
    Signed-off-by: Azat Khuzhin <a.khuzhin@semrush.com>

--- src/Storages/Distributed/DistributedAsyncInsertDirectoryQueue.cpp
+++ src/Storages/Distributed/DistributedAsyncInsertDirectoryQueue.cpp
@@ -234,7 +234,7 @@ void DistributedAsyncInsertDirectoryQueue::run()
             }
         }
         else
-            LOG_TEST(log, "Skipping send data over distributed table.");
+            LOG_TEST(LogFrequencyLimiter(log, 30), "Skipping send data over distributed table.");
 
         const auto now = std::chrono::system_clock::now();
         if (now - last_decrease_time > decrease_error_count_period)
