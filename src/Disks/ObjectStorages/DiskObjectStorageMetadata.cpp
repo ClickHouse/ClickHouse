6d25d6f797f08f2a150b6ded2bc89f7fd6b1902d
Sema Checherinda <Sema.Checherinda@clickhouse.com>
2023-10-25 23:01:50 +0300

    turn on storage_metadata_write_full_object_key

--- src/Disks/ObjectStorages/DiskObjectStorageMetadata.cpp
+++ src/Disks/ObjectStorages/DiskObjectStorageMetadata.cpp
@@ -206,7 +206,7 @@ bool DiskObjectStorageMetadata::getWriteFullObjectKeySetting()
 #ifndef CLICKHOUSE_KEEPER_STANDALONE_BUILD
     return Context::getGlobalContextInstance()->getSettings().storage_metadata_write_full_object_key;
 #else
-    return false;
+    return true;
 #endif
 }
 
