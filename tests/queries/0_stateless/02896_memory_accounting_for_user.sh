d05ac4567d69f33a524b71eaff20aedd55f9390e
Nikita Taranov <nikita.taranov@clickhouse.com>
2023-10-27 23:16:33 +0200

    add tests

--- tests/queries/0_stateless/02896_memory_accounting_for_user.sh
+++ tests/queries/0_stateless/02896_memory_accounting_for_user.sh
@@ -1,31 +1,46 @@
 #!/usr/bin/env bash
-# Tags: no-parallel
+# Tags: no-parallel, long
 
 CUR_DIR=$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)
 # shellcheck source=../shell_config.sh
 . "$CUR_DIR"/../shell_config.sh
 
 
-total_iterations=1000
-iterations_in_parallel=8
+total_iterations=16
+parallelism=32
 
-$CLICKHOUSE_CLIENT --query='drop table if exists test_inserts'
-$CLICKHOUSE_CLIENT --query='create table test_inserts engine=Null AS system.numbers'
+$CLICKHOUSE_CLIENT --query='DROP TABLE IF EXISTS test_inserts'
+$CLICKHOUSE_CLIENT --query='CREATE TABLE test_inserts ENGINE=Null AS system.numbers'
 
-for ((i = 1; i <= $total_iterations; i++)); do
-    while true; do
-        running_jobs=$(jobs -p | wc -w)
-        if [ $running_jobs -lt $iterations_in_parallel ]; then
-            break
-        fi
-        sleep 0.01
-    done
-    (
-        (
-            $CLICKHOUSE_LOCAL --query='SELECT * FROM numbers(1000000)' | curl 'http://localhost:8123/?query=INSERT%20INTO%20test_inserts%20FORMAT%20TSV' --data-binary @- 2>/dev/null
-            $CLICKHOUSE_CLIENT -q "insert into test_inserts select * from numbers_mt(1e6)"
-            # $CLICKHOUSE_CLIENT --query="WITH (SELECT memory_usage FROM system.user_processes WHERE user='default') as user_mem, (SELECT value FROM system.metrics WHERE metric='MemoryTracking') AS system_mem SELECT throwIf(user_mem > system_mem) FORMAT Null"
-    ) &)
+run_query() {
+  ( $CLICKHOUSE_CLIENT --query='SELECT * FROM numbers_mt(1000000) FORMAT CSV' | $CLICKHOUSE_CLIENT --max_threads 8 --max_memory_usage_for_user 1073741824 -q 'INSERT INTO test_inserts FORMAT CSV' 2>/dev/null )
+}
+
+for ((i = 1; i <= total_iterations; i++)); do
+  for ((j = 1; j <= parallelism; j++)); do
+    run_query & pids+=($!)
+  done
+
+  EXIT_CODE=0
+  new_pids=()
+  for pid in "${pids[@]:0:parallelism}"; do
+    CODE=0
+    wait "${pid}" || CODE=$?
+    run_query & new_pids+=($!)
+    if [[ "${CODE}" != "0" ]]; then
+        EXIT_CODE=1;
+    fi
+  done
+  for pid in "${pids[@]:parallelism}"; do
+    CODE=0
+    wait "${pid}" || CODE=$?
+    if [[ "${CODE}" != "0" ]]; then
+        EXIT_CODE=1;
+    fi
+  done
+  pids=("${new_pids[@]}")
+
+  if [[ $EXIT_CODE -ne 0 ]]; then
+    exit $EXIT_CODE
+  fi
 done
-
-wait
