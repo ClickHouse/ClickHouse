c544a0221010d812c68fb805aee3eed1a252b50c
Alexey Milovidov <milovidov@clickhouse.com>
2023-10-30 02:32:22 +0300

    Update 02907_query_profiler_concurrency_overruns.sh

--- tests/queries/0_stateless/02907_query_profiler_concurrency_overruns.sh
+++ tests/queries/0_stateless/02907_query_profiler_concurrency_overruns.sh
@@ -10,12 +10,12 @@ CUR_DIR=$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)
 while true
 do
     ${CLICKHOUSE_CLIENT} -n --query="
-    SELECT count() FROM numbers_mt(1000000) SETTINGS
+    SELECT count() FROM zeros_mt(1000000) SETTINGS
         query_profiler_real_time_period_ns = 1000000,
         query_profiler_cpu_time_period_ns = 1000000,
         max_threads = 1000,
         max_block_size = 100;
     SELECT anyIf(value, event = 'QueryProfilerRuns') > 0, anyIf(value, event = 'QueryProfilerConcurrencyOverruns') > 0 FROM system.events;
-    " | tr '\t\n' '  ' | grep '1000000000 1 1' && break
+    " | tr '\t\n' '  ' | grep '1000000 1 1' && break
     sleep 1
 done
