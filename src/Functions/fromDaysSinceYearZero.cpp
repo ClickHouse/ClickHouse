4d76c22a0dce9836f870cea145201af9274f29d7
Joanna Hulboj <joanna.hulboj@gmail.com>
2023-10-31 19:39:40 +0000

    Flesh out logic for fromDaysSinceYearZero and tests

--- src/Functions/fromDaysSinceYearZero.cpp
+++ src/Functions/fromDaysSinceYearZero.cpp
@@ -31,13 +31,40 @@ namespace ErrorCodes
 namespace
 {
 
-// TODO: Move to a common header (used in makeDate)
-class FunctionWithNumericParamsBase : public IFunction
+struct DateTraits
 {
+    static constexpr auto name = "fromDaysSinceYearZero";
+    using ReturnDataType = DataTypeDate;
+    static constexpr auto min_days = 719528;
+    static constexpr auto max_days = 785063;
+};
+
+struct DateTraits32
+{
+    static constexpr auto name = "fromDaysSinceYearZero32";
+    using ReturnDataType = DataTypeDate32;
+    static constexpr auto min_days = 693961;
+    static constexpr auto max_days = 840056;
+};
+
+template <typename Traits>
+class FunctionFromDaysSinceYearZero : public IFunction
+{
+
 public:
+    static constexpr auto name = Traits::name;
+    using RawReturnType = typename Traits::ReturnDataType::FieldType;
+
+    static FunctionPtr create(ContextPtr ctx) { return std::make_shared<FunctionFromDaysSinceYearZero>(ctx); }
+
+    FunctionFromDaysSinceYearZero() = default;
+    explicit FunctionFromDaysSinceYearZero(ContextPtr ctx_) : ctx(ctx_) {}
+
+    String getName() const override { return name; }
+
     bool isInjective(const ColumnsWithTypeAndName &) const override
     {
-        return false; /// invalid argument values and timestamps that are out of supported range are converted into a default value
+        return false; /// invalid argument values that are out of supported range are converted into a default value
     }
 
     bool isSuitableForShortCircuitArgumentsExecution(const DataTypesWithConstInfo & /*arguments*/) const override { return true; }
@@ -50,55 +77,13 @@ public:
 
     size_t getNumberOfArguments() const override { return 1; }
 
-protected:
-    template <class DataType = DataTypeFloat32, class ArgumentNames>
-    Columns convertMandatoryArguments(const ColumnsWithTypeAndName & arguments, const ArgumentNames & argument_names) const
+    DataTypePtr getReturnTypeImpl(const DataTypes & arguments) const override
     {
-        Columns converted_arguments;
-        const DataTypePtr converted_argument_type = std::make_shared<DataType>();
-        for (size_t i = 0; i < argument_names.size(); ++i)
-        {
-            ColumnPtr argument_column = castColumn(arguments[i], converted_argument_type);
-            argument_column = argument_column->convertToFullColumnIfConst();
-            converted_arguments.push_back(argument_column);
-        }
-        return converted_arguments;
-    }
-};
+        WhichDataType which_first(arguments[0]->getTypeId());
 
-
-struct DateTraits
-{
-    static constexpr auto name = "fromDaysSinceYearZero";
-    using ReturnDataType = DataTypeDate;
-};
-
-struct DateTraits32
-{
-    static constexpr auto name = "fromDaysSinceYearZero32";
-    using ReturnDataType = DataTypeDate32;
-};
-
-template <typename Traits>
-class FunctionFromDaysSinceYearZero : public FunctionWithNumericParamsBase
-{
-private:
-    static constexpr std::array mandatory_argument_names = {"daysSinceYearZero"};
-
-public:
-    static constexpr auto name = Traits::name;
-    using RawReturnType = typename Traits::ReturnDataType::FieldType;
-
-    static FunctionPtr create(ContextPtr) { return std::make_shared<FunctionFromDaysSinceYearZero>(); }
-
-    String getName() const override { return name; }
-
-    DataTypePtr getReturnTypeImpl(const ColumnsWithTypeAndName & arguments) const override
-    {
-        FunctionArgumentDescriptors args{
-            {mandatory_argument_names[0], &isNumber<IDataType>, nullptr, "Number"}
-        };
-        validateFunctionArgumentTypes(*this, arguments, args);
+        if (!which_first.isInt() && !which_first.isUInt())
+            throw Exception(ErrorCodes::ILLEGAL_TYPE_OF_ARGUMENT, "Illegal type {} of argument of function {}",
+                            arguments[0]->getName(), getName());
 
         return std::make_shared<typename Traits::ReturnDataType>();
     }
@@ -106,28 +91,84 @@ public:
     ColumnPtr executeImpl(const ColumnsWithTypeAndName & arguments, const DataTypePtr &, size_t input_rows_count) const override
     {
         using namespace std::chrono;
-        Columns converted_arguments;
-        converted_arguments = convertMandatoryArguments(arguments, mandatory_argument_names);
 
         auto res_column = Traits::ReturnDataType::ColumnType::create(input_rows_count);
-        auto & result_data = res_column->getData();
+        const auto & src_column = arguments[0];
 
-        const auto & days_data  = typeid_cast<const ColumnFloat32 &>(*converted_arguments[0]).getData();
+        [[maybe_unused]] FormatSettings::DateTimeOverflowBehavior date_time_overflow_behavior = default_date_time_overflow_behavior;
 
-        for (size_t i = 0; i < input_rows_count; ++i)
+        if (ctx)
+            date_time_overflow_behavior = ctx->getSettingsRef().date_time_overflow_behavior.value;
+
+        auto try_type = [&]<typename T>(T)
         {
-            auto result = static_cast<Int32>(days_data[i]) - DAYS_BETWEEN_YEARS_0_AND_1970;
-            // TODO: Complain if out of range rather than clamp
-            //       Or provide two flavours - one that clamps and one that complains
-            if (result < 0)
-                result = 0;
-            if (result > std::numeric_limits<RawReturnType>::max())
-                result = std::numeric_limits<RawReturnType>::max();
-            result_data[i] = result;
-        }
+            using ColVecType = ColumnVector<T>;
 
-        return res_column;
+            if (const ColVecType * col_vec = checkAndGetColumn<ColVecType>(src_column.column.get()))
+            {
+                switch (date_time_overflow_behavior)
+                {
+                    case FormatSettings::DateTimeOverflowBehavior::Throw:
+                        execute<FormatSettings::DateTimeOverflowBehavior::Throw, T>(*col_vec, *res_column, input_rows_count);
+                        break;
+                    case FormatSettings::DateTimeOverflowBehavior::Ignore:
+                        execute<FormatSettings::DateTimeOverflowBehavior::Ignore, T>(*col_vec, *res_column, input_rows_count);
+                        break;
+                    case FormatSettings::DateTimeOverflowBehavior::Saturate:
+                        execute<FormatSettings::DateTimeOverflowBehavior::Saturate, T>(*col_vec, *res_column, input_rows_count);
+                        break;
+                }
+                return true;
+            }
+            return false;
+        };
+
+        const auto res = false // NOLINT
+            || try_type(UInt8{})
+            || try_type(UInt16{})
+            || try_type(UInt32{})
+            || try_type(UInt64{})
+            || try_type(Int8{})
+            || try_type(Int16{})
+            || try_type(Int32{})
+            || try_type(Int64{});
+        if (res)
+            return res_column;
+
+        throw Exception(ErrorCodes::ILLEGAL_TYPE_OF_ARGUMENT, "Illegal column while execute function {}", getName());
     }
+
+    template <FormatSettings::DateTimeOverflowBehavior overflow_behaviour, typename T, typename ColVecType, typename ResCol>
+    static void execute(const ColVecType & col, ResCol & result_column, size_t rows_count)
+    {
+        const auto & src_data = col.getData();
+        auto & dst_data = result_column.getData();
+        dst_data.resize(rows_count);
+
+        for (size_t i = 0; i < rows_count; ++i)
+        {
+            auto raw_value = src_data[i];
+            auto value = static_cast<Int64>(raw_value);
+            if constexpr  (overflow_behaviour == FormatSettings::DateTimeOverflowBehavior::Saturate)
+            {
+                if (value < Traits::min_days)
+                    value = Traits::min_days;
+                else if (value > Traits::max_days)
+                    value = Traits::max_days;
+                dst_data[i] = static_cast<RawReturnType>(value - DAYS_BETWEEN_YEARS_0_AND_1970);
+            }
+            else if constexpr (overflow_behaviour == FormatSettings::DateTimeOverflowBehavior::Throw)
+            {
+                if (value < Traits::min_days || value > Traits::max_days) [[unlikely]]
+                    throw Exception(ErrorCodes::VALUE_IS_OUT_OF_RANGE_OF_DATA_TYPE, "Value {} is out of bounds", value);
+                dst_data[i] = static_cast<RawReturnType>(value - DAYS_BETWEEN_YEARS_0_AND_1970);
+            } else
+                dst_data[i] = static_cast<RawReturnType>(value - DAYS_BETWEEN_YEARS_0_AND_1970);
+        }
+    }
+
+private:
+    ContextPtr ctx;
 };
 
 
@@ -135,10 +176,21 @@ public:
 
 REGISTER_FUNCTION(FromDaysSinceYearZero)
 {
-    // TODO: Docs
-    factory.registerFunction<FunctionFromDaysSinceYearZero<DateTraits>>({}, FunctionFactory::CaseInsensitive);
-    factory.registerFunction<FunctionFromDaysSinceYearZero<DateTraits32>>({}, FunctionFactory::CaseInsensitive);
-    // Also could do to pretty string for out of range dates (as per civil_from_days)
+    factory.registerFunction<FunctionFromDaysSinceYearZero<DateTraits>>(FunctionDocumentation{
+        .description = R"(
+Given the number of days passed since 1 January 0000 in the  proleptic Gregorian calendar defined by ISO 8601 return a corresponding date.
+The calculation is the same as in MySQL's FROM_DAYS() function. If an overflow of the range supported by Date were to happen the behaviour is controlled by DateTimeOverflowBehavior setting.
+)",
+        .examples{{"typical", "SELECT fromDaysSinceYearZero(713569)", "2023-09-08"}},
+        .categories{"Dates and Times"}});
+
+    factory.registerFunction<FunctionFromDaysSinceYearZero<DateTraits32>>(FunctionDocumentation{
+        .description = R"(
+Given the number of days passed since 1 January 0000 in the  proleptic Gregorian calendar defined by ISO 8601 return a corresponding date.
+The calculation is the same as in MySQL's FROM_DAYS() function. If an overflow of the range supported by Date32 were to happen the behaviour is controlled by DateTimeOverflowBehavior setting.
+)",
+        .examples{{"typical", "SELECT fromDaysSinceYearZero32(713569)", "2023-09-08"}},
+        .categories{"Dates and Times"}});
 }
 
 }
