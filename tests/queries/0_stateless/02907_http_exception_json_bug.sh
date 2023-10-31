3cbf3c64ef5099f9a412db4f0f3f404267537209
Kruglov Pavel <48961922+Avogar@users.noreply.github.com>
2023-10-31 12:52:12 +0100

    Update tests/queries/0_stateless/02907_http_exception_json_bug.sh
    Co-authored-by: Raúl Marín <git@rmr.ninja>

--- tests/queries/0_stateless/02907_http_exception_json_bug.sh
+++ tests/queries/0_stateless/02907_http_exception_json_bug.sh
@@ -4,7 +4,7 @@ CUR_DIR=$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)
 # shellcheck source=../shell_config.sh
 . "$CUR_DIR"/../shell_config.sh
 
-CH_URL="$CLICKHOUSE_URL&http_write_exception_in_output_format=1&allow_experimental_analyzer=0"
+CH_URL="$CLICKHOUSE_URL&http_write_exception_in_output_format=1"
 
 ${CLICKHOUSE_CURL} -sS "$CH_URL" -d "SELECT repeat('1 ', 1000000) from system.one format JSONEachRow settings output_format_json_validate_utf8=1" | wc -w
 
