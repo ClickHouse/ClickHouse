71b90e610ffbd0cd35f191feab80f4214ba612a9
taiyang-li <654010905@qq.com>
2023-11-01 19:51:03 +0800

    fix failed uts

--- tests/queries/0_stateless/02797_read_subcolumns_from_files.sh
+++ tests/queries/0_stateless/02797_read_subcolumns_from_files.sh
@@ -12,12 +12,13 @@ settings="input_format_orc_allow_missing_columns = 1, input_format_parquet_allow
 for format in ${formats}; do
     echo $format
 
-    rm $DATA_FILE
     $CLICKHOUSE_LOCAL -q "select ((1, 2), 3)::Tuple(b Tuple(c UInt32, d UInt32), e UInt32) as a format $format" > $DATA_FILE
     $CLICKHOUSE_LOCAL -q "select a.b.d, a.b, a.e from file('$DATA_FILE', $format, 'a Tuple(b Tuple(c UInt32, d UInt32), e UInt32)') settings $settings"
     $CLICKHOUSE_LOCAL -q "select x.b.d, x.b, x.e from file('$DATA_FILE', $format, 'x Tuple(b Tuple(c UInt32, d UInt32), e UInt32)') settings $settings"
 
     # Currently ORC and Parquet do not support deducing default values for subcolumn from type definition.
     $CLICKHOUSE_LOCAL -q "select x.b.d, x.b, x.e from file('$DATA_FILE', $format, 'x Tuple(b Tuple(c UInt32, d UInt32), e UInt32) default ((42, 42), 42)') settings $settings"
+
+    rm $DATA_FILE
 done
 
