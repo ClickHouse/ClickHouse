42cfb8c22b1168401a8dacebcc72beaaca16065f
Robert Schulze <robert@clickhouse.com>
2023-11-02 21:40:20 +0000

    Make the tests 14.3% more aesthetic

--- tests/queries/0_stateless/02907_fromDaysSinceYearZero.sql
+++ tests/queries/0_stateless/02907_fromDaysSinceYearZero.sql
@@ -1,20 +1,38 @@
 SET session_timezone = 'Europe/Amsterdam'; -- disable time zone randomization in CI
 
-SELECT 'Const argument';
-SELECT 719528 AS x, fromDaysSinceYearZero(x);
-SELECT 739136 AS x, fromDaysSinceYearZero(x);
-SELECT 785063 AS x, fromDaysSinceYearZero(x);
-SELECT 840057 AS x, fromDaysSinceYearZero32(x);
-SELECT 693961 AS x, fromDaysSinceYearZero32(x);
-SELECT 719527 AS x, fromDaysSinceYearZero32(x);
-SELECT 719528 AS x, fromDaysSinceYearZero32(x);
-SELECT 739136 AS x, fromDaysSinceYearZero32(x);
-SELECT 693961 AS x, fromDaysSinceYearZero32(x);
-SELECT 785063 AS x, fromDaysSinceYearZero32(x);
-SELECT 785064 AS x, fromDaysSinceYearZero32(x);
-SELECT -10 AS x, fromDaysSinceYearZero(x), 'lower clip, 1970-01-01 min'; -- {serverError ILLEGAL_TYPE_OF_ARGUMENT}
-SELECT -10 AS x, fromDaysSinceYearZero32(x), 'lower clip, 1970-01-01 min'; -- {serverError ILLEGAL_TYPE_OF_ARGUMENT}
-SELECT fromDaysSinceYearZero(NULL);
-SELECT fromDaysSinceYearZero32(NULL);
+SELECT '-- negative tests';
+SELECT fromDaysSinceYearZero(); -- { serverError NUMBER_OF_ARGUMENTS_DOESNT_MATCH }
+SELECT fromDaysSinceYearZero32(); -- { serverError NUMBER_OF_ARGUMENTS_DOESNT_MATCH }
+SELECT fromDaysSinceYearZero(1, 2); -- { serverError NUMBER_OF_ARGUMENTS_DOESNT_MATCH }
+SELECT fromDaysSinceYearZero32(1, 2); -- { serverError NUMBER_OF_ARGUMENTS_DOESNT_MATCH }
+SELECT fromDaysSinceYearZero('needs a number'); -- { serverError ILLEGAL_TYPE_OF_ARGUMENT }
+SELECT fromDaysSinceYearZero32('needs a number'); -- { serverError ILLEGAL_TYPE_OF_ARGUMENT }
+SELECT fromDaysSinceYearZero(-3); -- { serverError ILLEGAL_TYPE_OF_ARGUMENT }
+SELECT fromDaysSinceYearZero32(-3); -- { serverError ILLEGAL_TYPE_OF_ARGUMENT }
 
-SELECT FROM_DAYS(1); -- test alias
+SELECT '-- const and non-const arguments';
+
+SELECT 719527 AS x, fromDaysSinceYearZero(x), fromDaysSinceYearZero(materialize(x)); -- outside Date's range
+SELECT 719528 AS x, fromDaysSinceYearZero(x), fromDaysSinceYearZero(materialize(x));
+SELECT 719529 AS x, fromDaysSinceYearZero(x), fromDaysSinceYearZero(materialize(x));
+SELECT 785062 AS x, fromDaysSinceYearZero(x), fromDaysSinceYearZero(materialize(x));
+SELECT 785063 AS x, fromDaysSinceYearZero(x), fromDaysSinceYearZero(materialize(x));
+SELECT 785064 AS x, fromDaysSinceYearZero(x), fromDaysSinceYearZero(materialize(x)); -- outside Date's range
+
+SELECT 693960 AS x, fromDaysSinceYearZero32(x), fromDaysSinceYearZero32(materialize(x)); -- outside Date32's range
+SELECT 693961 AS x, fromDaysSinceYearZero32(x), fromDaysSinceYearZero32(materialize(x));
+SELECT 693962 AS x, fromDaysSinceYearZero32(x), fromDaysSinceYearZero32(materialize(x));
+SELECT 840056 AS x, fromDaysSinceYearZero32(x), fromDaysSinceYearZero32(materialize(x));
+SELECT 840057 AS x, fromDaysSinceYearZero32(x), fromDaysSinceYearZero32(materialize(x));
+SELECT 840058 AS x, fromDaysSinceYearZero32(x), fromDaysSinceYearZero32(materialize(x)); -- outside Date32's range
+
+SELECT '-- integer types != UInt32';
+SELECT toUInt8(255) AS x, fromDaysSinceYearZero(x), fromDaysSinceYearZero32(x); -- outside Date's range for all UInt8-s
+SELECT toUInt16(65535) AS x, fromDaysSinceYearZero(x), fromDaysSinceYearZero32(x); -- outside Date's range for all UInt16-s
+SELECT toUInt64(719529) AS x, fromDaysSinceYearZero(x), fromDaysSinceYearZero32(x); -- something useful
+
+SELECT '-- NULL handling';
+SELECT fromDaysSinceYearZero(NULL), fromDaysSinceYearZero32(NULL);
+
+SELECT '-- Alias';
+SELECT FROM_DAYS(1);
