084bd4f68d51f3f7d04cc608d9f4c24bc9714cea
Nikita Taranov <nikita.taranov@clickhouse.com>
2023-11-03 16:41:24 +0100

    fix

--- src/Storages/MergeTree/MergeTreeReadTask.cpp
+++ src/Storages/MergeTree/MergeTreeReadTask.cpp
@@ -113,7 +113,11 @@ void MergeTreeReadTask::initializeRangeReaders(
 UInt64 MergeTreeReadTask::estimateNumRows(const BlockSizeParams & params) const
 {
     if (!size_predictor)
-        throw Exception(ErrorCodes::LOGICAL_ERROR, "Size predictor is not set, it might lead to a performance degradation");
+    {
+        if (params.preferred_block_size_bytes)
+            throw Exception(ErrorCodes::LOGICAL_ERROR, "Size predictor is not set, it might lead to a performance degradation");
+        return static_cast<size_t>(params.max_block_size_rows);
+    }
 
     /// Calculates number of rows will be read using preferred_block_size_bytes.
     /// Can't be less than avg_index_granularity.
