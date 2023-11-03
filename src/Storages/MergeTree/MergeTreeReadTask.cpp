b9d45cc232de00afafa27c0aea6797e5091c5a2d
Nikita Taranov <nikita.taranov@clickhouse.com>
2023-11-03 13:05:16 +0100

    Test

--- src/Storages/MergeTree/MergeTreeReadTask.cpp
+++ src/Storages/MergeTree/MergeTreeReadTask.cpp
@@ -1,5 +1,6 @@
 #include <Storages/MergeTree/MergeTreeReadTask.h>
 #include <Storages/MergeTree/MergeTreeBlockReadUtils.h>
+#include <Common/Exception.h>
 
 namespace DB
 {
@@ -112,7 +113,7 @@ void MergeTreeReadTask::initializeRangeReaders(
 UInt64 MergeTreeReadTask::estimateNumRows(const BlockSizeParams & params) const
 {
     if (!size_predictor)
-        return static_cast<size_t>(params.max_block_size_rows);
+        throw Exception(ErrorCodes::LOGICAL_ERROR, "Size predictor is not set, it might lead to a performance degradation");
 
     /// Calculates number of rows will be read using preferred_block_size_bytes.
     /// Can't be less than avg_index_granularity.
