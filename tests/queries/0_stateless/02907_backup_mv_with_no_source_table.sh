10571d905ab1dca38208bcd46c9bfda22121e3e7
kssenii <sumarokovakseniia@mail.ru>
2023-11-06 18:14:35 +0100

    Fix

---
+++ tests/queries/0_stateless/02907_backup_mv_with_no_source_table.sh
@@ -0,0 +1,26 @@
+#!/usr/bin/env bash
+
+CUR_DIR=$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)
+# shellcheck source=../shell_config.sh
+. "$CUR_DIR"/../shell_config.sh
+
+${CLICKHOUSE_CLIENT} -nm --query "
+drop table if exists src;
+create table src (a Int32) engine = MergeTree() order by tuple();
+
+drop table if exists dst;
+create table dst (a Int32) engine = MergeTree() order by tuple();
+
+drop table if exists mv;
+create materialized view mv to dst (a Int32) as select * from src;
+"
+
+${CLICKHOUSE_CLIENT} -nm --query "
+drop table src;
+backup database ${CLICKHOUSE_DATABASE} on cluster test_shard_localhost to Disk('backups', '${CLICKHOUSE_TEST_UNIQUE_NAME}');
+" | grep -o "BACKUP_CREATED"
+
+${CLICKHOUSE_CLIENT} -nm --query "
+drop database ${CLICKHOUSE_DATABASE};
+restore database ${CLICKHOUSE_DATABASE} on cluster test_shard_localhost from Disk('backups', '${CLICKHOUSE_TEST_UNIQUE_NAME}');
+" | grep -o "RESTORED"
