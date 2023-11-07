dbbf6bb2c38f29aad80e7d35be46b693a6ae3ce5
Denny Crane <denis.zhuravlov@gmail.com>
2023-11-07 02:24:53 -0400

    Update 02006_test_positional_arguments_on_cluster.sql

--- tests/queries/0_stateless/02006_test_positional_arguments_on_cluster.sql
+++ tests/queries/0_stateless/02006_test_positional_arguments_on_cluster.sql
@@ -14,3 +14,6 @@ format Null;
 ALTER TABLE t02006 on cluster test_shard_localhost ADD COLUMN IF NOT EXISTS f UInt64 format Null;
 
 DESC t02006;
+
+DROP TABLE IF EXISTS t02006 on cluster test_shard_localhost format Null;
+DROP TABLE IF EXISTS m02006 on cluster test_shard_localhost format Null;
