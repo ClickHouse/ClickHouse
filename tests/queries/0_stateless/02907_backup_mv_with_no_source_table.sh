c781501eb6d199bbbb01361469ad772057ec395e
kssenii <sumarokovakseniia@mail.ru>
2023-11-07 16:19:32 +0100

    Better test

--- tests/queries/0_stateless/02907_backup_mv_with_no_source_table.sh
+++ tests/queries/0_stateless/02907_backup_mv_with_no_source_table.sh
@@ -22,5 +22,29 @@ backup database ${CLICKHOUSE_DATABASE} on cluster test_shard_localhost to Disk('
 
 ${CLICKHOUSE_CLIENT} -nm --query "
 drop database ${CLICKHOUSE_DATABASE};
+set allow_deprecated_database_ordinary=1;
 restore database ${CLICKHOUSE_DATABASE} on cluster test_shard_localhost from Disk('backups', '${CLICKHOUSE_TEST_UNIQUE_NAME}');
+" | grep -o "RESTORED"
+
+${CLICKHOUSE_CLIENT} -nm --query "
+drop table if exists src;
+create table src (a Int32) engine = MergeTree() order by tuple();
+
+drop table if exists dst;
+create table dst (a Int32) engine = MergeTree() order by tuple();
+
+drop table if exists mv;
+create materialized view mv to dst (a Int32) as select * from src;
+"
+
+${CLICKHOUSE_CLIENT} -nm --query "
+drop table src;
+drop table dst;
+backup database ${CLICKHOUSE_DATABASE} on cluster test_shard_localhost to Disk('backups', '${CLICKHOUSE_TEST_UNIQUE_NAME}2');
+" | grep -o "BACKUP_CREATED"
+
+${CLICKHOUSE_CLIENT} -nm --query "
+drop database ${CLICKHOUSE_DATABASE};
+set allow_deprecated_database_ordinary=1;
+restore database ${CLICKHOUSE_DATABASE} on cluster test_shard_localhost from Disk('backups', '${CLICKHOUSE_TEST_UNIQUE_NAME}2');
 " | grep -o "RESTORED"
