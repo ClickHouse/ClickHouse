1a6a70f1113aa6fc7cc4175a7ba75e8f0fa8de58
yariks5s <yaroslav.briukhovetskyi@clickhouse.com>
2023-11-07 12:59:31 +0000

    fix flaky

--- tests/queries/0_stateless/02907_preferred_optimize_projection_name.sh
+++ tests/queries/0_stateless/02907_preferred_optimize_projection_name.sh
@@ -39,7 +39,7 @@ SELECT number,
     'test'
 FROM numbers(100, 500);"
 
-$CLICKHOUSE_CLIENT --query_id '02907_test' -q "
+$CLICKHOUSE_CLIENT --query_id 02907_test_$CLICKHOUSE_DATABASE -q "
 SELECT test_string
 FROM test_opt_proj
 WHERE (test_id > 50)
@@ -51,18 +51,18 @@ $CLICKHOUSE_CLIENT -q "SYSTEM FLUSH LOGS;"
 $CLICKHOUSE_CLIENT -q "
 SELECT projections
 FROM system.query_log
-WHERE query_id = '02907_test' AND current_database=currentDatabase()
+WHERE query_id = '02907_test_$CLICKHOUSE_DATABASE' AND current_database=currentDatabase()
 LIMIT 1;" | grep -o "projection_test_by_string" || true
 
 $CLICKHOUSE_CLIENT -q "
 SELECT projections
 FROM system.query_log 
-WHERE query_id = '02907_test' AND current_database=currentDatabase()
+WHERE query_id = '02907_test_$CLICKHOUSE_DATABASE' AND current_database=currentDatabase()
 LIMIT 1;" | grep -o "projection_test_by_more" || true
 
 echo "Executing query with setting"
 
-$CLICKHOUSE_CLIENT --query_id '02907_test_1' --preferred_optimize_projection_name 'projection_test_by_more' -q "
+$CLICKHOUSE_CLIENT --query_id 02907_test_1_$CLICKHOUSE_DATABASE --preferred_optimize_projection_name 'projection_test_by_more' -q "
 SELECT test_string
 FROM test_opt_proj
 WHERE (test_id > 50)
@@ -74,18 +74,18 @@ $CLICKHOUSE_CLIENT -q "SYSTEM FLUSH LOGS;"
 $CLICKHOUSE_CLIENT -q "
 SELECT projections
 FROM system.query_log
-WHERE query_id = '02907_test_1' AND current_database=currentDatabase()
+WHERE query_id = '02907_test_1_$CLICKHOUSE_DATABASE' AND current_database=currentDatabase()
 LIMIT 1;" | grep -o "projection_test_by_more" || true
 
 $CLICKHOUSE_CLIENT -q "
 SELECT projections
 FROM system.query_log
-WHERE query_id = '02907_test_1' AND current_database=currentDatabase()
+WHERE query_id = '02907_test_1_$CLICKHOUSE_DATABASE' AND current_database=currentDatabase()
 LIMIT 1" | grep -o "projection_test_by_string" || true
 
 echo "Executing query with wrong projection"
 
-$CLICKHOUSE_CLIENT --query_id '02907_test_2' --preferred_optimize_projection_name 'non_existing_projection' -q "
+$CLICKHOUSE_CLIENT --query_id 02907_test_2_$CLICKHOUSE_DATABASE --preferred_optimize_projection_name 'non_existing_projection' -q "
 SELECT test_string
 FROM test_opt_proj
 WHERE (test_id > 50)
@@ -97,11 +97,11 @@ $CLICKHOUSE_CLIENT -q "SYSTEM FLUSH LOGS;"
 $CLICKHOUSE_CLIENT -q "
 SELECT projections
 FROM system.query_log
-WHERE query_id = '02907_test_2' AND current_database=currentDatabase()
+WHERE query_id = '02907_test_2_$CLICKHOUSE_DATABASE' AND current_database=currentDatabase()
 LIMIT 1;" | grep -o "projection_test_by_string" || true
 
 $CLICKHOUSE_CLIENT -q "
 SELECT projections
 FROM system.query_log 
-WHERE query_id = '02907_test_2' AND current_database=currentDatabase()
+WHERE query_id = '02907_test_2_$CLICKHOUSE_DATABASE' AND current_database=currentDatabase()
 LIMIT 1;" | grep -o "projection_test_by_more" || true
