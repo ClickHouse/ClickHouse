8b03f8d550058fa08769a44338db6dd657be171d
Igor Nikonov <igor@clickhouse.com>
2023-11-07 19:29:41 +0000

    Simplify test

--- tests/queries/0_stateless/02904_background_part_check.sql
+++ tests/queries/0_stateless/02904_background_part_check.sql
@@ -1,22 +1,22 @@
-DROP TABLE IF EXISTS part_check_t1 SYNC;
-DROP TABLE IF EXISTS part_check_t2 SYNC;
-DROP TABLE IF EXISTS part_check_t3 SYNC;
+DROP TABLE IF EXISTS part_check_t SYNC;
 
-CREATE TABLE part_check_t1(k UInt32, v String) ENGINE ReplicatedMergeTree('/part_check/{database}/test_tbl', 'r1') ORDER BY k SETTINGS background_part_check_delay_seconds=0;
-CREATE TABLE part_check_t2(k UInt32, v String) ENGINE ReplicatedMergeTree('/part_check/{database}/test_tbl', 'r2') ORDER BY k SETTINGS background_part_check_delay_seconds=0;
-CREATE TABLE part_check_t3(k UInt32, v String) ENGINE ReplicatedMergeTree('/part_check/{database}/test_tbl', 'r3') ORDER BY k SETTINGS background_part_check_delay_seconds=0;
+CREATE TABLE part_check_t(k UInt32, v String) ENGINE ReplicatedMergeTree('/{database}/part_check', 'r1') ORDER BY k SETTINGS background_part_check_delay_seconds=0;
 
-insert into part_check_t1 select number, toString(number) from numbers(1000, 1000);
-
-SYSTEM SYNC REPLICA part_check_t1;
-SYSTEM SYNC REPLICA part_check_t2;
-SYSTEM SYNC REPLICA part_check_t3;
+insert into part_check_t select number, toString(number) from numbers(1000);
 
 SELECT count() > 0
 FROM system.text_log
-WHERE logger_name ILIKE '%' || currentDatabase() || '%part_check_t%ReplicatedMergeTreePartCheckThread%' AND message ILIKE '%Background part check succeeded%' AND event_date >= yesterday()
+WHERE logger_name ILIKE '%' || currentDatabase() || '%part_check%ReplicatedMergeTreePartCheckThread%' AND message ILIKE '%Background part check succeeded%' AND event_date >= yesterday()
 GROUP BY logger_name;
 
-DROP TABLE part_check_t1 SYNC;
-DROP TABLE part_check_t2 SYNC;
-DROP TABLE part_check_t3 SYNC;
+DROP TABLE part_check_t SYNC;
+
+CREATE TABLE no_part_check_t(k UInt32, v String) ENGINE ReplicatedMergeTree('/{database}/no_part_check', 'r1') ORDER BY k SETTINGS background_part_check_time_to_total_time_ratio=0;
+
+insert into no_part_check_t select number, toString(number) from numbers(1000);
+
+SELECT count()
+FROM system.text_log
+WHERE logger_name ILIKE '%' || currentDatabase() || '%no_part_check_t%ReplicatedMergeTreePartCheckThread%' AND message ILIKE '%Background part check%' AND event_date >= yesterday();
+
+DROP TABLE no_part_check_t SYNC;
