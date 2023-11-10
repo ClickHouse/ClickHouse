78f0a2a61d6ab089c475d3f47926353139d5f2aa
Raúl Marín <git@rmr.ninja>
2023-11-10 17:20:17 +0000

    Revert "Merge pull request #56591 from ClickHouse/revert-56500-function-sleep-exception-message-fix"
    This reverts commit e8875d881de0f0aaafaa306814ecc33b29ddff49, reversing
    changes made to a5b1474b6a8de71cdfa3b579efbb76b25c5741bd.

--- src/Functions/sleep.h
+++ src/Functions/sleep.h
@@ -112,13 +112,15 @@ public:
         if (size > 0)
         {
             /// When sleeping, the query cannot be cancelled. For ability to cancel query, we limit sleep time.
+            UInt64 microseconds = static_cast<UInt64>(seconds * 1e6);
             if (max_microseconds && seconds * 1e6 > max_microseconds)
-                throw Exception(ErrorCodes::TOO_SLOW, "The maximum sleep time is {} microseconds. Requested: {}", max_microseconds, seconds);
+                throw Exception(ErrorCodes::TOO_SLOW, "The maximum sleep time is {} microseconds. Requested: {} microseconds",
+                    max_microseconds, microseconds);
 
             if (!dry_run)
             {
                 UInt64 count = (variant == FunctionSleepVariant::PerBlock ? 1 : size);
-                UInt64 microseconds = static_cast<UInt64>(seconds * count * 1e6);
+                microseconds = static_cast<UInt64>(seconds * count * 1e6);
 
                 if (max_microseconds && microseconds > max_microseconds)
                     throw Exception(ErrorCodes::TOO_SLOW,
