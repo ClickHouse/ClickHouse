64292f36f5a3cdaa617116b470a16895b159d423
Bhavna Jindal <bhavna.jindal@ibm.com>
2023-11-10 11:44:29 -0800

    fixed getReturnTypeImpl to accept only array argument

--- src/Functions/seriesPeriodDetectFFT.cpp
+++ src/Functions/seriesPeriodDetectFFT.cpp
@@ -30,6 +30,7 @@ namespace ErrorCodes
 {
 extern const int BAD_ARGUMENTS;
 extern const int ILLEGAL_COLUMN;
+extern const int ILLEGAL_TYPE_OF_ARGUMENT;
 }
 
 /*Detect Period in time series data using FFT.
