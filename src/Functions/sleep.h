21a814c20b7dfac153c8382569b9ac8a0a0759ae
Alexey Milovidov <milovidov@clickhouse.com>
2023-11-10 18:53:56 +0300

    Revert "FunctionSleep exception message fix"

--- src/Functions/sleep.h
+++ src/Functions/sleep.h
@@ -112,15 +112,13 @@ public:
         if (size > 0)
         {
             /// When sleeping, the query cannot be cancelled. For ability to cancel query, we limit sleep time.
-            UInt64 microseconds = static_cast<UInt64>(seconds * 1e6);
             if (max_microseconds && seconds * 1e6 > max_microseconds)
-                throw Exception(ErrorCodes::TOO_SLOW, "The maximum sleep time is {} microseconds. Requested: {} microseconds",
-                    max_microseconds, microseconds);
+                throw Exception(ErrorCodes::TOO_SLOW, "The maximum sleep time is {} microseconds. Requested: {}", max_microseconds, seconds);
 
             if (!dry_run)
             {
                 UInt64 count = (variant == FunctionSleepVariant::PerBlock ? 1 : size);
-                microseconds = static_cast<UInt64>(seconds * count * 1e6);
+                UInt64 microseconds = static_cast<UInt64>(seconds * count * 1e6);
 
                 if (max_microseconds && microseconds > max_microseconds)
                     throw Exception(ErrorCodes::TOO_SLOW,
