4fdca75daba49fca36c73685347ecd7889fd3933
Nikolay Degterinsky <evillique@gmail.com>
2023-11-13 17:08:40 +0000

    Change cluster name

--- tests/queries/0_stateless/02911_row_policy_on_cluster.sql
+++ tests/queries/0_stateless/02911_row_policy_on_cluster.sql
@@ -1,10 +1,10 @@
 -- Tags: no-parallel, zookeeper
 
-DROP ROW POLICY IF EXISTS 02911_rowpolicy ON default.* ON CLUSTER default;
-DROP USER IF EXISTS 02911_user ON CLUSTER default;
+DROP ROW POLICY IF EXISTS 02911_rowpolicy ON default.* ON CLUSTER test_shard_localhost;
+DROP USER IF EXISTS 02911_user ON CLUSTER test_shard_localhost;
 
-CREATE USER 02911_user ON CLUSTER default;
-CREATE ROW POLICY 02911_rowpolicy ON CLUSTER default ON default.* USING 1 TO 02911_user;
+CREATE USER 02911_user ON CLUSTER test_shard_localhost;
+CREATE ROW POLICY 02911_rowpolicy ON CLUSTER test_shard_localhost ON default.* USING 1 TO 02911_user;
 
-DROP ROW POLICY 02911_rowpolicy ON default.* ON CLUSTER default;
-DROP USER 02911_user ON CLUSTER default;
+DROP ROW POLICY 02911_rowpolicy ON default.* ON CLUSTER test_shard_localhost;
+DROP USER 02911_user ON CLUSTER test_shard_localhost;
