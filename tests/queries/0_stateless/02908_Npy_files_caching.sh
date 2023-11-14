9ce9dc86d38662aa4163a5047047639b88c21945
yariks5s <yaroslav.briukhovetskyi@clickhouse.com>
2023-11-14 13:27:32 +0000

    fixed due to review

--- tests/queries/0_stateless/02908_Npy_files_caching.sh
+++ tests/queries/0_stateless/02908_Npy_files_caching.sh
@@ -11,3 +11,9 @@ $CLICKHOUSE_LOCAL -nm -q "
 desc file('$CURDIR/data_npy/one_dim.npy');
 select number_of_rows from system.schema_inference_cache where format='Npy';
 "
+$CLICKHOUSE_LOCAL -q "select count() from file('$CURDIR/data_npy/npy_big.npy') settings optimize_count_from_files=0"
+$CLICKHOUSE_LOCAL -q "select count() from file('$CURDIR/data_npy/npy_big.npy') settings optimize_count_from_files=1"
+$CLICKHOUSE_LOCAL -nm -q "
+desc file('$CURDIR/data_npy/npy_big.npy');
+select number_of_rows from system.schema_inference_cache where format='Npy';
+"
