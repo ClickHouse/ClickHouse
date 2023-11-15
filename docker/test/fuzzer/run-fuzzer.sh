4259924c1e67fe4fa09c0b3d001f0f4f41a8793f
avogar <avogar@clickhouse.com>
2023-11-15 15:06:39 +0000

    Try to fix fuzzer script

--- docker/test/fuzzer/run-fuzzer.sh
+++ docker/test/fuzzer/run-fuzzer.sh
@@ -20,8 +20,6 @@ repo_dir=ch
 BINARY_TO_DOWNLOAD=${BINARY_TO_DOWNLOAD:="clang-17_debug_none_unsplitted_disable_False_binary"}
 BINARY_URL_TO_DOWNLOAD=${BINARY_URL_TO_DOWNLOAD:="https://clickhouse-builds.s3.amazonaws.com/$PR_TO_TEST/$SHA_TO_TEST/clickhouse_build_check/$BINARY_TO_DOWNLOAD/clickhouse"}
 
-ln -s /usr/share/clickhouse-test/clickhouse-test /usr/bin/clickhouse-test
-
 function git_clone_with_retry
 {
     for _ in 1 2 3 4; do
