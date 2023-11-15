83acec9511e082bd872f2968ec90eb4579abaf57
Dmitry Novik <n0vik@clickhouse.com>
2023-11-15 18:58:14 +0000

    Fix max_threads = 1 case

--- tests/queries/0_stateless/01568_window_functions_distributed.sql
+++ tests/queries/0_stateless/01568_window_functions_distributed.sql
@@ -19,6 +19,8 @@ select sum(number) over w, max(number) over w from t_01568 window w as (partitio
 
 select sum(number) over w as x, max(number) over w as y from remote('127.0.0.{1,2}', '', t_01568) window w as (partition by p) order by x, y;
 
+select sum(number) over w as x, max(number) over w as y from remote('127.0.0.{1,2}', '', t_01568) window w as (partition by p) order by x, y SETTINGS max_threads = 1;
+
 select distinct sum(number) over w as x, max(number) over w as y from remote('127.0.0.{1,2}', '', t_01568) window w as (partition by p) order by x, y;
 
 -- window functions + aggregation w/shards
