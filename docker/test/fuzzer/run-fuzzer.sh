2784813760f582ca53bb89774188cc0ff8458280
Kruglov Pavel <48961922+Avogar@users.noreply.github.com>
2023-11-15 19:38:04 +0100

    Fix

--- docker/test/fuzzer/run-fuzzer.sh
+++ docker/test/fuzzer/run-fuzzer.sh
@@ -225,7 +225,7 @@ quit
 
     setup_logs_replication
 
-    /usr/share/clickhouse-test/clickhouse-test --fuzz \
+    $repo_dir/test/clickhouse-test --fuzz \
         --query-fuzzer-runs=1000 \
         --create-query-fuzzer-runs=50 \
          $NEW_TESTS_OPT \
