c4f46c7ce575fbfcb52c69343534b9539b3f719d
Denny Crane <denis.zhuravlov@gmail.com>
2023-11-17 10:27:02 +0300

    test for #56790

--- tests/queries/0_stateless/00059_shard_global_in_mergetree.sql
+++ tests/queries/0_stateless/00059_shard_global_in_mergetree.sql
@@ -7,8 +7,6 @@ ENGINE = MergeTree order by x as select * from numbers(10);
   
 select count() from remote('127.0.0.1,127.0.0.2', currentDatabase(), test_local);
 
-select count() from remote('127.0.0.1,127.0.0.2', currentDatabase(), test_local);
-
 select count() from remote('127.0.0.1,127.0.0.2', currentDatabase(), test_local) where 'XXX' global in (select 'XXX');
 
 select count() from remote('127.0.0.1,127.0.0.2', currentDatabase(), test_local) where * global in (select * from test_local);
