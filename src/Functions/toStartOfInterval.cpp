f596ae7a2c02e7915917daca82dad717a7df2640
Robert Schulze <robert@clickhouse.com>
2023-11-18 16:24:56 +0000

    Fix style

--- src/Functions/toStartOfInterval.cpp
+++ src/Functions/toStartOfInterval.cpp
@@ -20,11 +20,12 @@ namespace DB
 {
 namespace ErrorCodes
 {
-    extern const int NUMBER_OF_ARGUMENTS_DOESNT_MATCH;
-    extern const int ILLEGAL_COLUMN;
-    extern const int ILLEGAL_TYPE_OF_ARGUMENT;
     extern const int ARGUMENT_OUT_OF_BOUND;
     extern const int BAD_ARGUMENTS;
+    extern const int DECIMAL_OVERFLOW;
+    extern const int ILLEGAL_COLUMN;
+    extern const int ILLEGAL_TYPE_OF_ARGUMENT;
+    extern const int NUMBER_OF_ARGUMENTS_DOESNT_MATCH;
 }
 
 
