bbf070caa638da425f2cbb1620046d1e9858cef4
Nikita Taranov <nikita.taranov@clickhouse.com>
2023-11-18 23:59:09 +0100

    impl

--- src/IO/ReadBufferFromIStream.cpp
+++ src/IO/ReadBufferFromIStream.cpp
@@ -1,4 +1,5 @@
 #include <IO/ReadBufferFromIStream.h>
+#include <Poco/Net/HTTPBasicStreamBuf.h>
 #include <Common/Exception.h>
 
 
@@ -8,14 +9,54 @@ namespace DB
 namespace ErrorCodes
 {
     extern const int CANNOT_READ_FROM_ISTREAM;
+    extern const int LOGICAL_ERROR;
 }
 
 bool ReadBufferFromIStream::nextImpl()
 {
-    istr.read(internal_buffer.begin(), internal_buffer.size());
-    size_t gcount = istr.gcount();
+    size_t bytes_read = 0;
 
-    if (!gcount)
+    /// Special case. Here we can avoid buffering inside `BasicBufferedStreamBuf` and read straight to the `internal_buffer`.
+    if (auto * response_buf = dynamic_cast<Poco::Net::HTTPBasicStreamBuf *>(istr.rdbuf()); response_buf)
+    {
+        if (response_buf->in_avail())
+            /// It would mean that we already read some data from this `istr`, while we should have use the direct reading scheme.
+            /// TODO: replace with chassert
+            throw Exception(ErrorCodes::LOGICAL_ERROR, "Unexpected buffer state");
+
+        char * read_to = internal_buffer.begin();
+        /// It is necessary to read in a loop, since socket usually returns only data available at the moment.
+        while (bytes_read < internal_buffer.size())
+        {
+            try
+            {
+                const auto bytes_read_last_time = response_buf->readFromDevice(read_to, internal_buffer.size() - bytes_read);
+                if (bytes_read_last_time <= 0)
+                {
+                    /// We just replicate what `std::istream` does
+                    istr.setstate(istr.failbit | istr.eofbit);
+                    break;
+                }
+
+                bytes_read += bytes_read_last_time;
+                read_to += bytes_read_last_time;
+            }
+            catch (...)
+            {
+                /// We just replicate what `std::istream` does
+                istr.setstate(istr.badbit);
+                break;
+            }
+        }
+    }
+    else
+    {
+        /// General case when we will read from `istr`.
+        istr.read(internal_buffer.begin(), internal_buffer.size());
+        bytes_read = istr.gcount();
+    }
+
+    if (!bytes_read)
     {
         if (istr.eof())
             return false;
@@ -26,7 +67,7 @@ bool ReadBufferFromIStream::nextImpl()
         throw Exception(ErrorCodes::CANNOT_READ_FROM_ISTREAM, "Unexpected state of istream at offset {}", count());
     }
     else
-        working_buffer.resize(gcount);
+        working_buffer.resize(bytes_read);
 
     return true;
 }
