5bb2b1c6e4bdceceea23808032b8e01086a2f8bd
robot-clickhouse <robot-clickhouse@users.noreply.github.com>
2023-11-20 23:05:07 +0000

    Backport #56989 to 23.3: Follow up the fix from #44311

--- utils/prepare-time-trace/prepare-time-trace.sh
+++ utils/prepare-time-trace/prepare-time-trace.sh
@@ -8,7 +8,7 @@
 
 # See also https://docs.google.com/document/d/1CvAClvFfyA5R-PhYUmn5OOQtYMH4h6I0nSsKchNAySU/preview
 
-<<///
+true<<///
 CREATE TABLE build_time_trace
 (
     -- extra columns here
@@ -36,7 +36,7 @@ ENGINE = MergeTree ORDER BY (date, file, name, args_name);
 INPUT_DIR=$1
 OUTPUT_DIR=$2
 
-find "$INPUT_DIR" -name '*.json' | grep -P '\.(c|cpp|cc|cxx)\.json$' | xargs -P $(nproc) -I{} bash -c "
+find "$INPUT_DIR" -name '*.json' | grep -P '\.(c|cpp|cc|cxx)\.json$' | xargs -P "$(nproc)" -I{} bash -c "
 
     ORIGINAL_FILENAME=\$(echo '{}' | sed -r -e 's!\.json\$!!; s!/CMakeFiles/[^/]+\.dir!!')
     LIBRARY_NAME=\$(echo '{}' | sed -r -e 's!^.*/CMakeFiles/([^/]+)\.dir/.*\$!\1!')
