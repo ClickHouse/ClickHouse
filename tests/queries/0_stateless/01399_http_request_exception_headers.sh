31d00ace65710ee95f1553853682eb76b7c507d7
slvrtrn <hypnoash@gmail.com>
2023-11-21 22:08:35 +0100

    Update tests

--- tests/queries/0_stateless/01399_http_request_exception_headers.sh
+++ tests/queries/0_stateless/01399_http_request_exception_headers.sh
@@ -5,10 +5,10 @@ CURDIR=$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)
 . "$CURDIR"/../shell_config.sh
 
 echo "-- Non-retryable exceptions"
-RES=$(curl -vs http://localhost:8123 -d 'SELECT x' 2>&1)
+RES=$(${CLICKHOUSE_CURL} -vs "${CLICKHOUSE_URL}" -d 'SELECT x' 2>&1)
 echo $RES | grep -c '< X-ClickHouse-Exception-Code: 47'
 echo $RES | grep -c '< X-ClickHouse-Exception-Retryable' || true
-RES=$(curl -vs http://localhost:8123 -d 'SELECT someUnknownFunction(42)' 2>&1)
+RES=$(${CLICKHOUSE_CURL} -vs "${CLICKHOUSE_URL}" -d 'SELECT someUnknownFunction(42)' 2>&1)
 echo $RES | grep -c '< X-ClickHouse-Exception-Code: 46'
 echo $RES | grep -c '< X-ClickHouse-Exception-Retryable' || true
 
@@ -17,7 +17,7 @@ declare -a RETRYABLE_EXCEPTION_CODES=(3 159 164 202 203 209 210 242 252 285 319 
 for code in "${RETRYABLE_EXCEPTION_CODES[@]}"
 do
   echo "-- Code: $code"
-  RES=$(curl -vs http://localhost:8123 -d "select throwIf(1, 'test', toInt16($code)) settings allow_custom_error_code_in_throwif=1" 2>&1)
+  RES=$(${CLICKHOUSE_CURL} -vs "${CLICKHOUSE_URL}" -d "select throwIf(1, 'test', toInt16($code)) settings allow_custom_error_code_in_throwif=1" 2>&1)
   echo $RES | grep -c "X-ClickHouse-Exception-Code: $code"
   echo $RES | grep -c '< X-ClickHouse-Exception-Retryable: 1'
 done
