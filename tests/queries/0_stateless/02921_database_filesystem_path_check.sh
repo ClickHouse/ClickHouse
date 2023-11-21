3eef0601e3404a9b1238f24f0f2deab571e4a17c
santrancisco <san.tran@ebfe.pw>
2023-11-21 16:29:40 +1100

    Fix file path validation for DatabaseFileSystem

---
+++ tests/queries/0_stateless/02921_database_filesystem_path_check.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+
+CUR_DIR=$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)
+# shellcheck source=../shell_config.sh
+. "$CUR_DIR"/../shell_config.sh
+
+function get_exception_message()
+{
+  $CLICKHOUSE_CLIENT --query "$1" |& grep -o 'Path must.*path'
+}
+
+get_exception_message "create database db_filesystem ENGINE=Filesystem('/etc');"
+get_exception_message "create database db_filesystem ENGINE=Filesystem('../../../../../../../../etc')';"
