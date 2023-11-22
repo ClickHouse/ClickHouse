e11bb59d559dcbb05cd576f4dc6672d759ed4e13
Arthur Passos <arthur.ti@outlook.com>
2023-11-22 15:30:10 -0300

    remove duplication of applyfunction

--- src/Interpreters/MonotonicityCheckVisitor.h
+++ src/Interpreters/MonotonicityCheckVisitor.h
@@ -6,6 +6,7 @@
 #include <IO/WriteHelpers.h>
 #include <Interpreters/InDepthNodeVisitor.h>
 #include <Interpreters/IdentifierSemantic.h>
+#include <Interpreters/applyFunction.h>
 #include <Parsers/ASTFunction.h>
 #include <Parsers/ASTIdentifier.h>
 #include <Parsers/ASTOrderByElement.h>
@@ -221,46 +222,6 @@ public:
         result.column = result.type->createColumnConst(0, literal->value);
 
         return result;
-    }
-
-    static Field applyFunctionForField(
-        const FunctionBasePtr & func,
-        const DataTypePtr & arg_type,
-        const Field & arg_value)
-    {
-        ColumnsWithTypeAndName columns
-            {
-                { arg_type->createColumnConst(1, arg_value), arg_type, "x" },
-            };
-
-        auto col = func->execute(columns, func->getResultType(), 1);
-        return (*col)[0];
-    }
-
-    static FieldRef applyFunction(const FunctionBasePtr & func, const DataTypePtr & current_type, const FieldRef & field)
-    {
-        /// Fallback for fields without block reference.
-        if (field.isExplicit())
-            return applyFunctionForField(func, current_type, field);
-
-        String result_name = "_" + func->getName() + "_" + toString(field.column_idx);
-        const auto & columns = field.columns;
-        size_t result_idx = columns->size();
-
-        for (size_t i = 0; i < result_idx; ++i)
-        {
-            if ((*columns)[i].name == result_name)
-                result_idx = i;
-        }
-
-        if (result_idx == columns->size())
-        {
-            ColumnsWithTypeAndName args{(*columns)[field.column_idx]};
-            field.columns->emplace_back(ColumnWithTypeAndName {nullptr, func->getResultType(), result_name});
-            (*columns)[result_idx].column = func->execute(args, (*columns)[result_idx].type, columns->front().column->size());
-        }
-
-        return {field.columns, field.row_idx, result_idx};
     }
 };
 
--- src/Storages/MergeTree/KeyCondition.cpp
+++ src/Storages/MergeTree/KeyCondition.cpp
@@ -11,6 +11,7 @@
 #include <Interpreters/ExpressionAnalyzer.h>
 #include <Interpreters/ExpressionActions.h>
 #include <Interpreters/castColumn.h>
+#include <Interpreters/applyFunction.h>
 #include <Interpreters/misc.h>
 #include <Functions/FunctionFactory.h>
 #include <Functions/indexHint.h>
@@ -911,21 +912,6 @@ bool KeyCondition::getConstant(const ASTPtr & expr, Block & block_with_constants
     return node.tryGetConstant(out_value, out_type);
 }
 
-
-static Field applyFunctionForField(
-    const FunctionBasePtr & func,
-    const DataTypePtr & arg_type,
-    const Field & arg_value)
-{
-    ColumnsWithTypeAndName columns
-    {
-        { arg_type->createColumnConst(1, arg_value), arg_type, "x" },
-    };
-
-    auto col = func->execute(columns, func->getResultType(), 1);
-    return (*col)[0];
-}
-
 /// The case when arguments may have types different than in the primary key.
 static std::pair<Field, DataTypePtr> applyFunctionForFieldOfUnknownType(
     const FunctionBasePtr & func,
@@ -963,33 +949,6 @@ static std::pair<Field, DataTypePtr> applyBinaryFunctionForFieldOfUnknownType(
     Field result = (*col)[0];
 
     return {std::move(result), std::move(return_type)};
-}
-
-
-static FieldRef applyFunction(const FunctionBasePtr & func, const DataTypePtr & current_type, const FieldRef & field)
-{
-    /// Fallback for fields without block reference.
-    if (field.isExplicit())
-        return applyFunctionForField(func, current_type, field);
-
-    String result_name = "_" + func->getName() + "_" + toString(field.column_idx);
-    const auto & columns = field.columns;
-    size_t result_idx = columns->size();
-
-    for (size_t i = 0; i < result_idx; ++i)
-    {
-        if ((*columns)[i].name == result_name)
-            result_idx = i;
-    }
-
-    if (result_idx == columns->size())
-    {
-        ColumnsWithTypeAndName args{(*columns)[field.column_idx]};
-        field.columns->emplace_back(ColumnWithTypeAndName {nullptr, func->getResultType(), result_name});
-        (*columns)[result_idx].column = func->execute(args, (*columns)[result_idx].type, columns->front().column->size());
-    }
-
-    return {field.columns, field.row_idx, result_idx};
 }
 
 /** When table's key has expression with these functions from a column,
