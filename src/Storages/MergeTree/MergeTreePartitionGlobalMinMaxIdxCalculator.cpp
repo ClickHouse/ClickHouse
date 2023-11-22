54ae855047e3db9ffd00692c27a3aeb3e64e3f79
Arthur Passos <arthur.ti@outlook.com>
2023-11-22 15:37:27 -0300

    move functions to anonymous namespace

--- src/Storages/MergeTree/MergeTreePartitionGlobalMinMaxIdxCalculator.cpp
+++ src/Storages/MergeTree/MergeTreePartitionGlobalMinMaxIdxCalculator.cpp
@@ -3,64 +3,70 @@
 namespace DB
 {
 
-std::vector<std::pair<Field, Field>> MergeTreePartitionGlobalMinMaxIdxCalculator::extractMinMaxIndexesFromBlock(
-    const Block & block_with_min_max_idx
-)
+
+namespace
 {
-    std::vector<std::pair<Field, Field>> min_max_indexes;
 
-    for (auto i = 0u; i < block_with_min_max_idx.columns(); i++)
+    std::vector<std::pair<Field, Field>> extractMinMaxIndexesFromBlock(
+        const Block & block_with_min_max_idx
+    )
     {
-        Field min_idx;
-        Field max_idx;
+        std::vector<std::pair<Field, Field>> min_max_indexes;
 
-        block_with_min_max_idx.getByPosition(i).column->get(0, min_idx);
-        block_with_min_max_idx.getByPosition(i).column->get(1, max_idx);
+        for (auto i = 0u; i < block_with_min_max_idx.columns(); i++)
+        {
+            Field min_idx;
+            Field max_idx;
 
-        min_max_indexes.emplace_back(std::move(min_idx), std::move(max_idx));
+            block_with_min_max_idx.getByPosition(i).column->get(0, min_idx);
+            block_with_min_max_idx.getByPosition(i).column->get(1, max_idx);
+
+            min_max_indexes.emplace_back(std::move(min_idx), std::move(max_idx));
+        }
+
+        return min_max_indexes;
     }
 
-    return min_max_indexes;
-}
-
-std::vector<std::pair<Field, Field>> MergeTreePartitionGlobalMinMaxIdxCalculator::calculateMinMaxIndexesForPart(
-    const MergeTreeData & storage,
-    const DataPartPtr & part
-)
-{
-    auto metadata_manager = std::make_shared<PartMetadataManagerOrdinary>(part.get());
-
-    auto min_max_index = DataPart::MinMaxIndex();
-
-    min_max_index.load(storage, metadata_manager);
-
-    auto block_with_min_max_partition_ids = min_max_index.getBlock(storage);
-
-    return extractMinMaxIndexesFromBlock(block_with_min_max_partition_ids);
-}
-
-void MergeTreePartitionGlobalMinMaxIdxCalculator::updateGlobalMinMaxIndexes(
-    std::vector<std::pair<Field, Field>> & global_min_max_indexes,
-    const std::vector<std::pair<Field, Field>> & local_min_max_indexes
-)
-{
-    if (global_min_max_indexes.empty())
+    std::vector<std::pair<Field, Field>> calculateMinMaxIndexesForPart(
+        const MergeTreeData & storage,
+        const MergeTreeData::DataPartPtr & part
+    )
     {
-        global_min_max_indexes = local_min_max_indexes;
-        return;
+        auto metadata_manager = std::make_shared<PartMetadataManagerOrdinary>(part.get());
+
+        auto min_max_index = MergeTreeData::DataPart::MinMaxIndex();
+
+        min_max_index.load(storage, metadata_manager);
+
+        auto block_with_min_max_partition_ids = min_max_index.getBlock(storage);
+
+        return extractMinMaxIndexesFromBlock(block_with_min_max_partition_ids);
     }
 
-    assert(local_min_max_indexes.size() == global_min_max_indexes.size());
-
-    for (auto i = 0u; i < local_min_max_indexes.size(); i++)
+    void updateGlobalMinMaxIndexes(
+        std::vector<std::pair<Field, Field>> & global_min_max_indexes,
+        const std::vector<std::pair<Field, Field>> & local_min_max_indexes
+    )
     {
-        const auto & [local_min_idx, local_max_idx] = local_min_max_indexes[i];
+        if (global_min_max_indexes.empty())
+        {
+            global_min_max_indexes = local_min_max_indexes;
+            return;
+        }
 
-        global_min_max_indexes[i] = {
-            std::min(global_min_max_indexes[i].first, local_min_idx),
-            std::max(global_min_max_indexes[i].second, local_max_idx)
-        };
+        assert(local_min_max_indexes.size() == global_min_max_indexes.size());
+
+        for (auto i = 0u; i < local_min_max_indexes.size(); i++)
+        {
+            const auto & [local_min_idx, local_max_idx] = local_min_max_indexes[i];
+
+            global_min_max_indexes[i] = {
+                std::min(global_min_max_indexes[i].first, local_min_idx),
+                std::max(global_min_max_indexes[i].second, local_max_idx)
+            };
+        }
     }
+
 }
 
 std::vector<std::pair<Field, Field>> MergeTreePartitionGlobalMinMaxIdxCalculator::calculate(
--- src/Storages/MergeTree/MergeTreePartitionGlobalMinMaxIdxCalculator.h
+++ src/Storages/MergeTree/MergeTreePartitionGlobalMinMaxIdxCalculator.h
@@ -16,19 +16,12 @@ class MergeTreePartitionGlobalMinMaxIdxCalculator
     using DataPart = IMergeTreeDataPart;
     using DataPartPtr = std::shared_ptr<const DataPart>;
     using DataPartsVector = std::vector<DataPartPtr>;
+
 public:
     static std::vector<std::pair<Field, Field>> calculate(
         const MergeTreeData & storage,
         const DataPartsVector & parts
     );
-
-private:
-    static std::vector<std::pair<Field, Field>> extractMinMaxIndexesFromBlock(const Block & block_with_min_max_idx);
-    static std::vector<std::pair<Field, Field>> calculateMinMaxIndexesForPart(const MergeTreeData & storage, const DataPartPtr & part);
-    static void updateGlobalMinMaxIndexes(
-        std::vector<std::pair<Field, Field>> & global_min_max_indexes,
-        const std::vector<std::pair<Field, Field>> & local_min_max_indexes);
-
 };
 
 }
