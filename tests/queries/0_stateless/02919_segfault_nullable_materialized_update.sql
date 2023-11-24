78fe2eef2dad0160ffa6be4fe27e0badb8545eb5
Nikolay Degterinsky <evillique@gmail.com>
2023-11-24 20:51:18 +0000

    Better test

--- tests/queries/0_stateless/02919_segfault_nullable_materialized_update.sql
+++ tests/queries/0_stateless/02919_segfault_nullable_materialized_update.sql
@@ -2,14 +2,17 @@ DROP TABLE IF EXISTS crash_02919;
 
 CREATE TABLE crash_02919 (
     b Int64,
-    c Int64 MATERIALIZED b
+    c Nullable(Int64) MATERIALIZED b,
+    d Nullable(Bool) MATERIALIZED b
 )
 ENGINE = MergeTree
 ORDER BY tuple();
 
-INSERT INTO crash_02919 VALUES (1);
-SELECT b, c FROM crash_02919;
-ALTER TABLE crash_02919 UPDATE b = 0 WHERE 1=1 SETTINGS mutations_sync = 1;
-SELECT b, c FROM crash_02919;
+INSERT INTO crash_02919 VALUES (0);
+SELECT b, c, d FROM crash_02919;
+ALTER TABLE crash_02919 UPDATE b = 1 WHERE 1=1 SETTINGS mutations_sync = 1;
+SELECT b, c, d FROM crash_02919;
+ALTER TABLE crash_02919 UPDATE b = 0.1 WHERE 1=1 SETTINGS mutations_sync = 1;
+SELECT b, c, d FROM crash_02919;
 
 DROP TABLE crash_02919;
