7aca6ee5fcfc5412fce5a5daf48863a13bc5f9b6
Nikita Taranov <nikita.taranov@clickhouse.com>
2023-11-28 19:09:45 +0100

    fix test

--- tests/queries/0_stateless/02907_read_buffer_content_is_cached_multiple_blobs.sql
+++ tests/queries/0_stateless/02907_read_buffer_content_is_cached_multiple_blobs.sql
@@ -1,3 +1,5 @@
+-- Tags: no-fasttest
+
 -- We want to test `isContentCached(offset, size)` method implementation in ReadBufferFromRemoteFSGather and CachedOnDiskReadBufferFromFile
 -- Specifically, how they handle `offset` parameter when we have multiple S3 objects representing a single ClickHouse file
 -- Log englie table files will be represented by multiple objects on S3
