7528275e1337ee7ec17ba0b779d084fff1557070
Igor Nikonov <igor@clickhouse.com>
2023-11-29 00:05:11 +0000

    Update test

--- tests/queries/0_stateless/02918_parallel_replicas_custom_key_unavailable_replica.sql
+++ tests/queries/0_stateless/02918_parallel_replicas_custom_key_unavailable_replica.sql
@@ -4,20 +4,24 @@ CREATE TABLE 02918_parallel_replicas (x String, y Int32) ENGINE = MergeTree ORDE
 
 INSERT INTO 02918_parallel_replicas SELECT toString(number), number % 4 FROM numbers(1000);
 
-SET async_socket_for_remote=0;
-SET async_query_sending_for_remote=0;
+-- SET async_socket_for_remote=0;
+-- SET async_query_sending_for_remote=0;
+-- SET load_balancing='round_robin';
+-- SET use_hedged_requests=0;
 
+-- { echoOn }
 SELECT y, count()
-FROM cluster(parallel_replicas_custom_key_unavailable_replica, currentDatabase(), 02918_parallel_replicas) 
--- FROM cluster(test_cluster_one_shard_two_replicas, currentDatabase(), 02918_parallel_replicas)
+FROM cluster(parallel_replicas_custom_key_unavailable_replica, currentDatabase(), 02918_parallel_replicas)
 GROUP BY y
 ORDER BY y
-SETTINGS max_parallel_replicas=3, parallel_replicas_custom_key='cityHash64(y)', parallel_replicas_custom_key_filter_type='default';
+SETTINGS max_parallel_replicas=2, parallel_replicas_custom_key='cityHash64(y)', parallel_replicas_custom_key_filter_type='default';
 
 SELECT y, count()
 FROM cluster(parallel_replicas_custom_key_unavailable_replica, currentDatabase(), 02918_parallel_replicas)
 GROUP BY y
 ORDER BY y
 SETTINGS max_parallel_replicas=3, parallel_replicas_custom_key='cityHash64(y)', parallel_replicas_custom_key_filter_type='range';
+
+-- { echoOff }
 
 DROP TABLE 02918_parallel_replicas;
