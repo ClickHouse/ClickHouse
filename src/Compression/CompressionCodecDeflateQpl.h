a89676249b061e55b04be3c21b882995a3662162
Robert Schulze <robert@clickhouse.com>
2023-11-29 21:44:29 +0000

    Fix clang-tidy build

--- src/Compression/CompressionCodecDeflateQpl.h
+++ src/Compression/CompressionCodecDeflateQpl.h
@@ -68,7 +68,7 @@ public:
     /// Maximum times to check if hardware job complete, otherwise fallback to software codec.
     static constexpr UInt32 MAX_CHECKS = UINT16_MAX;
 
-    HardwareCodecDeflateQpl(SoftwareCodecDeflateQpl & codec);
+    HardwareCodecDeflateQpl(SoftwareCodecDeflateQpl & sw_codec_);
     ~HardwareCodecDeflateQpl();
 
     Int32 doCompressData(const char * source, UInt32 source_size, char * dest, UInt32 dest_size) const;
