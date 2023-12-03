24ac367e604ef2ed2581795e5bed9d1e67348218
wxybear <wangxingyu0927@outlook.com>
2023-12-04 01:28:16 +0800

    fix: typo, duplicate sql

--- tests/queries/0_stateless/02842_largestTriangleThreeBuckets_aggregate_function.sql
+++ tests/queries/0_stateless/02842_largestTriangleThreeBuckets_aggregate_function.sql
@@ -60,10 +60,4 @@ SELECT
   point_x - neighbor(point_x, -1) AS point_x_diff_with_previous_row 
 FROM largestTriangleTreeBucketsBucketSizeTest LIMIT 990, 10;
 
-SELECT 
-  arrayJoin(lttb(1000)(x, y)) AS point, 
-  tupleElement(point, 1) AS point_x, 
-  point_x - neighbor(point_x, -1) AS point_x_diff_with_previous_row 
-FROM largestTriangleTreeBucketsBucketSizeTest LIMIT 990, 10;
-
 DROP TABLE largestTriangleTreeBucketsBucketSizeTest;
