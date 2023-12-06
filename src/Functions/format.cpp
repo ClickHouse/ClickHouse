cfdfde68799b4b70cecdc494f0942b989177cd80
Robert Schulze <robert@clickhouse.com>
2023-12-06 08:34:32 +0000

    Fix style

--- src/Functions/format.cpp
+++ src/Functions/format.cpp
@@ -18,7 +18,6 @@ namespace DB
 namespace ErrorCodes
 {
     extern const int ILLEGAL_COLUMN;
-    extern const int ILLEGAL_TYPE_OF_ARGUMENT;
     extern const int NUMBER_OF_ARGUMENTS_DOESNT_MATCH;
 }
 
