3723573f81d16daefc75cb3c152eff9c29c60a7d
Alexey Milovidov <milovidov@clickhouse.com>
2023-12-06 19:09:07 +0100

    Revert "Merge pull request #56724 from canhld94/ch_replicated_column_mismatch"
    This reverts commit a9bb76378fbd7ec800efefbd383f84e75a7e75d2, reversing
    changes made to 588fd165185db9837761e42ed2acb07604b6e458.

--- src/Storages/ColumnsDescription.h
+++ src/Storages/ColumnsDescription.h
@@ -91,7 +91,6 @@ struct ColumnDescription
     ColumnDescription(const ColumnDescription &) = default;
     ColumnDescription(String name_, DataTypePtr type_);
 
-    bool identical(const ColumnDescription & other) const;
     bool operator==(const ColumnDescription & other) const;
     bool operator!=(const ColumnDescription & other) const { return !(*this == other); }
 
@@ -126,7 +125,6 @@ public:
     /// NOTE Must correspond with Nested::flatten function.
     void flattenNested(); /// TODO: remove, insert already flattened Nested columns.
 
-    bool identical(const ColumnsDescription & other) const;
     bool operator==(const ColumnsDescription & other) const { return columns == other.columns; }
     bool operator!=(const ColumnsDescription & other) const { return !(*this == other); }
 
