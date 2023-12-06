b0b1394ae9c33f4a16d1d4df6624cc42b49d8860
Igor Nikonov <igor@clickhouse.com>
2023-12-06 11:45:46 +0000

    Fix test

--- tests/queries/0_stateless/02935_parallel_replicas_settings.sql
+++ tests/queries/0_stateless/02935_parallel_replicas_settings.sql
@@ -16,17 +16,20 @@ SELECT count() > 0 FROM system.text_log
 WHERE yesterday() <= event_date
       AND query_id in (select query_id from system.query_log where current_database=currentDatabase() AND log_comment='0_f621c4f2-4da7-4a7c-bb6d-052c442d0f7f')
       AND level = 'Information'
-      AND message ILIKE '%Disabling ''use_hedged_requests'' in favor of ''allow_experimental_parallel_reading_from_replicas''%';
+      AND message ILIKE '%Disabling ''use_hedged_requests'' in favor of ''allow_experimental_parallel_reading_from_replicas''%'
+SETTINGS allow_experimental_parallel_reading_from_replicas=0;
 
 SET use_hedged_requests=1;
 SELECT count() FROM test_parallel_replicas_settings WHERE NOT ignore(*) settings log_comment='1_f621c4f2-4da7-4a7c-bb6d-052c442d0f7f';
 
 SYSTEM FLUSH LOGS;
 
+SET allow_experimental_parallel_reading_from_replicas=0;
 SELECT count() > 0 FROM system.text_log
 WHERE yesterday() <= event_date
       AND query_id in (select query_id from system.query_log where current_database = currentDatabase() AND log_comment = '1_f621c4f2-4da7-4a7c-bb6d-052c442d0f7f')
       AND level = 'Warning'
-      AND message ILIKE '%Setting ''use_hedged_requests'' explicitly with enabled ''allow_experimental_parallel_reading_from_replicas'' has no effect%';
+      AND message ILIKE '%Setting ''use_hedged_requests'' explicitly with enabled ''allow_experimental_parallel_reading_from_replicas'' has no effect%'
+SETTINGS allow_experimental_parallel_reading_from_replicas=0;
 
 DROP TABLE test_parallel_replicas_settings;
