da10b21616121ef32a265769e28f04e9c94d8a40
Joanna Hulboj <joanna.hulboj@gmail.com>
2023-12-08 17:42:11 +0000

    Some test clarification

--- tests/queries/0_stateless/02896_leading_zeroes_no_octal.sql
+++ tests/queries/0_stateless/02896_leading_zeroes_no_octal.sql
@@ -1,4 +1,6 @@
 DROP TABLE IF EXISTS t_leading_zeroes;
+DROP TABLE IF EXISTS t_leading_zeroes_f;
+
 CREATE TABLE t_leading_zeroes(id INTEGER, input String, val INTEGER, expected INTEGER) ENGINE=MergeTree ORDER BY id;
 
 INSERT INTO t_leading_zeroes VALUES (1, '00000', 00000, 0), (2, '0', 0, 0), (3, '00', 00, 0), (4, '01', 01, 1), (5, '+01', +01, 1);
@@ -10,10 +12,14 @@ INSERT INTO t_leading_zeroes VALUES (17, '1000.0008', 1000.0008, 1000), (18, '00
 SELECT 'Leading zeroes into INTEGER';
 SELECT t.val == t.expected AS ok, * FROM t_leading_zeroes t ORDER BY id;
 
+-- Floats don't go via the weird octal path:
 CREATE TABLE t_leading_zeroes_f(id INTEGER, input String, val Float32, expected Float32) ENGINE=MergeTree ORDER BY id;
 INSERT INTO t_leading_zeroes_f VALUES (1, '00000', 00000, 0), (2, '00009.00009', 00009.00009, 9.00009), (3, '00009e9', 00009e9, 9e9), (4, '00009e09', 00009e09, 9e9), (5, '00009e0009', 00009e0009, 9e9);
--- Turns out this is not ok in master as well - will have a look and fix
---  (6, '00009e00009', 00009e00009, 9e9);
+INSERT INTO t_leading_zeroes_f VALUES (6, '-00000', -00000.1, -0.1), (7, '-00009.00009', -00009.00009, -9.00009), (8, '-00009e9', -00009e9, -9e9), (9, '-00009e09', -00009e09, -9e9), (10, '-00009e0009', -00009e0009, -9e9);
+INSERT INTO t_leading_zeroes_f VALUES (11, '+00000', +00000., 0), (12, '+00009.00009', +00009.00009, 9.00009), (13, '+00009e9', +00009e9, 9e9), (14, '+00009e09', +00009e09, 9e9), (15, '+00009e0009', +00009e0009, 9e9);
+-- Coincidentally, the following result in 9 rather than 9e9 because of readFloatTextFastImpl
+-- using readUIntTextUpToNSignificantDigits<4>(exponent, in)
+-- INSERT INTO t_leading_zeroes_f VALUES (100, '00009e00009', 00009e00009, 9e9), (101, '-00009e00009', -00009e00009, -9e9), (102, '+00009e00009', +00009e00009, 9e9)
 
 SELECT 'Leading zeroes into Float32';
 SELECT t.val == t.expected AS ok, * FROM t_leading_zeroes_f t ORDER BY id;
