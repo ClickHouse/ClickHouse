3771acdefbb441059ff01e9e779be3e6bd31c2a9
jinjunzh <jasper.zhu@intel.com>
2023-12-12 11:15:11 -0500

    fixed reported code issues

--- src/Compression/CompressionCodecZSTD.cpp
+++ src/Compression/CompressionCodecZSTD.cpp
@@ -1,4 +1,3 @@
-#include <Compression/ICompressionCodec.h>
 #include <Compression/CompressionInfo.h>
 #include <Compression/CompressionFactory.h>
 #include <zstd.h>
@@ -11,44 +10,10 @@
 #include <IO/BufferWithOwnMemory.h>
 #include <Poco/Logger.h>
 #include <Common/logger_useful.h>
-#ifdef ENABLE_QATZSTD_COMPRESSION
-#include "qatseqprod.h"
-#endif
+#include "CompressionCodecZSTD.h"
 
 namespace DB
 {
-
-class CompressionCodecZSTD : public ICompressionCodec
-{
-public:
-    static constexpr auto ZSTD_DEFAULT_LEVEL = 1;
-    static constexpr auto ZSTD_DEFAULT_LOG_WINDOW = 24;
-
-    explicit CompressionCodecZSTD(int level_);
-    CompressionCodecZSTD(int level_, int window_log);
-
-    uint8_t getMethodByte() const override;
-
-    UInt32 getMaxCompressedDataSize(UInt32 uncompressed_size) const override;
-
-    void updateHash(SipHash & hash) const override;
-
-protected:
-
-    UInt32 doCompressData(const char * source, UInt32 source_size, char * dest) const override;
-
-    void doDecompressData(const char * source, UInt32 source_size, char * dest, UInt32 uncompressed_size) const override;
-
-    bool isCompression() const override { return true; }
-    bool isGenericCompression() const override { return true; }
-
-private:
-    const int level;
-    const bool enable_long_range;
-    const int window_log;
-};
-
-
 namespace ErrorCodes
 {
     extern const int CANNOT_COMPRESS;
@@ -159,98 +124,6 @@ void registerCodecZSTD(CompressionCodecFactory & factory)
         return std::make_shared<CompressionCodecZSTD>(level);
     });
 }
-
-#ifdef ENABLE_QATZSTD_COMPRESSION
-class CompressionCodecQATZSTD : public CompressionCodecZSTD
-{
-public:
-    static constexpr auto QATZSTD_SUPPORTED_MIN_LEVEL = 1;
-    static constexpr auto QATZSTD_SUPPORTED_MAX_LEVEL = 12;
-    explicit CompressionCodecQATZSTD(int level_);
-    ~CompressionCodecQATZSTD() override;
-
-protected:
-    UInt32 doCompressData(const char * source, UInt32 source_size, char * dest) const override;
-
-private:
-    const int level;
-    mutable bool initialized;
-    mutable ZSTD_CCtx* cctx;
-    mutable void *sequenceProducerState;
-    Poco::Logger * log;
-};
-
-UInt32 CompressionCodecQATZSTD::doCompressData(const char * source, UInt32 source_size, char * dest) const
-{
-    if (!initialized)
-    {
-        cctx = ZSTD_createCCtx();
-        /* Start QAT device, start QAT device at any time before compression job started */
-        int res = QZSTD_startQatDevice();
-        /* Create sequence producer state for QAT sequence producer */
-        sequenceProducerState = QZSTD_createSeqProdState();
-        /* register qatSequenceProducer */
-        ZSTD_registerSequenceProducer(
-            cctx,
-            sequenceProducerState,
-            qatSequenceProducer
-        );
-        /* Enable sequence producer fallback */
-        ZSTD_CCtx_setParameter(cctx, ZSTD_c_enableSeqProducerFallback, 1);
-        ZSTD_CCtx_setParameter(cctx, ZSTD_c_compressionLevel, level);
-        initialized = true;
-        LOG_WARNING(log, "Initialization of hardware-assisted(QAT) ZSTD codec result: {} ", static_cast<UInt32>(res));
-    }
-    size_t compressed_size = ZSTD_compress2(cctx, dest, ZSTD_compressBound(source_size), source, source_size);
-
-    if (ZSTD_isError(compressed_size))
-        throw Exception(ErrorCodes::CANNOT_COMPRESS, "Cannot compress with ZSTD codec: {}", std::string(ZSTD_getErrorName(compressed_size)));
-
-    return static_cast<UInt32>(compressed_size);
-}
-
-void registerCodecQATZSTD(CompressionCodecFactory & factory)
-{
-    factory.registerCompressionCodec("QATZSTD", {}, [&](const ASTPtr & arguments) -> CompressionCodecPtr
-    {
-        int level = CompressionCodecZSTD::ZSTD_DEFAULT_LEVEL;
-        if (arguments && !arguments->children.empty())
-        {
-            if (arguments->children.size() > 1)
-                throw Exception(ErrorCodes::ILLEGAL_SYNTAX_FOR_CODEC_TYPE, "QATZSTD codec must have 1 parameter, given {}", arguments->children.size());
-
-            const auto children = arguments->children;
-            const auto * literal = children[0]->as<ASTLiteral>();
-            if (!literal)
-                throw Exception(ErrorCodes::ILLEGAL_CODEC_PARAMETER, "QATZSTD codec argument must be integer");
-
-            level = static_cast<int>(literal->value.safeGet<UInt64>());
-            if (level > CompressionCodecQATZSTD::QATZSTD_SUPPORTED_MAX_LEVEL || level < CompressionCodecQATZSTD::QATZSTD_SUPPORTED_MIN_LEVEL)
-                throw Exception(ErrorCodes::ILLEGAL_CODEC_PARAMETER,
-                    "QATZSTD codec doesn't support level more than {} and lower than {} , given {}",
-                    CompressionCodecQATZSTD::QATZSTD_SUPPORTED_MAX_LEVEL, CompressionCodecQATZSTD::QATZSTD_SUPPORTED_MIN_LEVEL, level);
-        }
-
-        return std::make_shared<CompressionCodecQATZSTD>(level);
-    });
-}
-
-CompressionCodecQATZSTD::CompressionCodecQATZSTD(int level_)
-    : CompressionCodecZSTD(level_), level(level_), initialized(false), log(&Poco::Logger::get("CompressionCodecQATZSTD"))
-{
-    setCodecDescription("QATZSTD", {std::make_shared<ASTLiteral>(static_cast<UInt64>(level))});
-}
-
-CompressionCodecQATZSTD::~CompressionCodecQATZSTD()
-{
-    if (initialized)
-    {
-        /* Free sequence producer state */
-        QZSTD_freeSeqProdState(sequenceProducerState);
-        ZSTD_freeCCtx(cctx);
-    }
-}
-#endif
 
 CompressionCodecPtr getCompressionCodecZSTD(int level)
 {
