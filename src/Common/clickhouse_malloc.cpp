29057264afac6c55714e1003434bf1cb9cc229c6
Raúl Marín <git@rmr.ninja>
2023-12-12 18:29:08 +0100

    Revert changes in clickhouse_malloc as croaring is incompatible

--- src/Common/clickhouse_malloc.cpp
+++ src/Common/clickhouse_malloc.cpp
@@ -1,5 +1,4 @@
 #include <cstdlib>
-#include <Common/MemoryTrackerBlockerInThread.h>
 #include <Common/memory.h>
 
 
@@ -9,15 +8,10 @@
 extern "C" void * clickhouse_malloc(size_t size)
 {
     void * res = malloc(size);
-    if (likely(res))
+    if (res)
     {
         AllocationTrace trace;
         size_t actual_size = Memory::trackMemoryNoExcept(size, trace);
-        if (unlikely(trace.isNull()))
-        {
-            free(res);
-            return nullptr;
-        }
         trace.onAlloc(res, actual_size);
     }
     return res;
@@ -26,15 +20,10 @@ extern "C" void * clickhouse_malloc(size_t size)
 extern "C" void * clickhouse_calloc(size_t number_of_members, size_t size)
 {
     void * res = calloc(number_of_members, size);
-    if (likely(res))
+    if (res)
     {
         AllocationTrace trace;
         size_t actual_size = Memory::trackMemoryNoExcept(number_of_members * size, trace);
-        if (unlikely(trace.isNull()))
-        {
-            free(res);
-            return nullptr;
-        }
         trace.onAlloc(res, actual_size);
     }
     return res;
@@ -42,35 +31,19 @@ extern "C" void * clickhouse_calloc(size_t number_of_members, size_t size)
 
 extern "C" void * clickhouse_realloc(void * ptr, size_t size)
 {
-    AllocationTrace trace_free;
-    size_t original_size = 0;
-    if (likely(ptr))
-        original_size = Memory::untrackMemory(ptr, trace_free);
-
-    AllocationTrace trace_alloc;
-    size_t new_size = Memory::trackMemoryNoExcept(size, trace_alloc);
-    if (unlikely(trace_alloc.isNull()))
+    if (ptr)
     {
-        /// The new request failed. We need to track again the previous one (and ignore any errors)
-        MemoryTrackerBlockerInThread memory_blocker;
-        trace_alloc = CurrentMemoryTracker::allocNoThrow(original_size);
-        return nullptr;
+        AllocationTrace trace;
+        size_t actual_size = Memory::untrackMemory(ptr, trace);
+        trace.onFree(ptr, actual_size);
     }
-
     void * res = realloc(ptr, size);
-    if (unlikely(!res))
+    if (res)
     {
-        /// Both changes worked but the final allocation failed, so we need to revert them
-        MemoryTrackerBlockerInThread memory_blocker;
-        if (original_size < new_size)
-            trace_alloc = CurrentMemoryTracker::free(new_size - original_size);
-        else if (original_size > new_size)
-            trace_alloc = CurrentMemoryTracker::allocNoThrow(original_size - new_size);
-        return nullptr;
+        AllocationTrace trace;
+        size_t actual_size = Memory::trackMemoryNoExcept(size, trace);
+        trace.onAlloc(res, actual_size);
     }
-
-    trace_free.onFree(ptr, original_size);
-    trace_alloc.onAlloc(res, new_size);
     return res;
 }
 
@@ -94,15 +67,10 @@ extern "C" void clickhouse_free(void * ptr)
 extern "C" int clickhouse_posix_memalign(void ** memptr, size_t alignment, size_t size)
 {
     int res = posix_memalign(memptr, alignment, size);
-    if (likely(res == 0))
+    if (res == 0)
     {
         AllocationTrace trace;
         size_t actual_size = Memory::trackMemoryNoExcept(size, trace);
-        if (unlikely(trace.isNull()))
-        {
-            free(*memptr);
-            return ENOMEM;
-        }
         trace.onAlloc(*memptr, actual_size);
     }
     return res;
