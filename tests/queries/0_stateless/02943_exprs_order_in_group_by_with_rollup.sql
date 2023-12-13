a4581dabc6f7f42be989cd39c5c0d6e0f1089bed
chen768959 <934103231@qq.com>
2023-12-13 14:27:37 +0800

    update test case

--- tests/queries/0_stateless/02943_exprs_order_in_group_by_with_rollup.sql
+++ tests/queries/0_stateless/02943_exprs_order_in_group_by_with_rollup.sql
@@ -7,6 +7,6 @@ insert into test_group_by_with_rollup_order values(2,2,'a');
 insert into test_group_by_with_rollup_order values(3,3,'b');
 insert into test_group_by_with_rollup_order values(4,4,'b');
 
-SELECT toString(a) as r1, b, count() FROM test_group_by_with_rollup_order GROUP BY r1, b WITH ROLLUP;
+SELECT toString(a) as r1, b, count() FROM test_group_by_with_rollup_order GROUP BY r1, b WITH ROLLUP ORDER BY b,r1;
 
 DROP TABLE IF EXISTS test_group_by_with_rollup_order;
