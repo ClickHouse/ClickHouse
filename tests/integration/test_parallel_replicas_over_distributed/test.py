7762beaf6cd64f2553e81db5f7b1e5ba4ea4d8bd
Igor Nikonov <igor@clickhouse.com>
2023-12-13 11:23:53 +0000

    Fix: w/o replicas sync query result can vary

--- tests/integration/test_parallel_replicas_over_distributed/test.py
+++ tests/integration/test_parallel_replicas_over_distributed/test.py
@@ -129,6 +129,9 @@ def test_parallel_replicas_over_distributed(
     node = nodes[0]
     expected_result = f"6003\t-1999\t1999\t3\n"
 
+    # sync all replicas to get consistent result
+    node.query(f"SYSTEM SYNC REPLICA ON CLUSTER {cluster} {table_name}")
+
     # parallel replicas
     assert (
         node.query(
@@ -143,11 +146,12 @@ def test_parallel_replicas_over_distributed(
         == expected_result
     )
 
-    # sync all replicas to get consistent result by next distributed query
-    node.query(f"SYSTEM SYNC REPLICA ON CLUSTER {cluster} {table_name}")
-
     # w/o parallel replicas
     assert (
-        node.query(f"SELECT count(), min(key), max(key), sum(key) FROM {table_name}_d")
+        node.query(f"SELECT count(), min(key), max(key), sum(key) FROM {table_name}_d",
+            settings={
+                "allow_experimental_parallel_reading_from_replicas": 0,
+           }
+        )
         == expected_result
     )
