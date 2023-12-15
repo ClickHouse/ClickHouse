239c7310eb5e7a938fe47ae2771103bf814b6f19
JackyWoo <wuchienchao@qq.com>
2023-12-15 15:11:22 +0800

    Fix test window function failure

--- tests/integration/test_query_coordination_window_function/test.py
+++ tests/integration/test_query_coordination_window_function/test.py
@@ -1,5 +1,4 @@
 import pytest
-
 from helpers.cluster import ClickHouseCluster
 
 cluster = ClickHouseCluster(__file__)
@@ -14,27 +13,43 @@ def started_cluster():
     try:
         cluster.start()
 
-        node1.query(
-            """CREATE TABLE wf_partition_local ON CLUSTER test_two_shards (part_key UInt64, value UInt64, order UInt64) ENGINE = MergeTree ORDER BY value SETTINGS index_granularity=100;"""
-        )
+        node1.query("""
+            CREATE TABLE test_table_local ON CLUSTER test_two_shards
+            (part_key UInt64, value UInt64, order UInt64)
+            ENGINE = MergeTree ORDER BY value SETTINGS index_granularity=100;
+        """)
 
-        node1.query(
-            """CREATE TABLE wf_partition ON CLUSTER test_two_shards (part_key UInt64, value UInt64, order UInt64) ENGINE = Distributed(test_two_shards, default, wf_partition_local, rand());"""
-        )
+        node1.query("""
+            CREATE TABLE test_table ON CLUSTER test_two_shards
+            (part_key UInt64, value UInt64, order UInt64)
+            ENGINE = Distributed(test_two_shards, default, test_table_local, rand());
+        """)
 
         yield cluster
-
     finally:
         cluster.shutdown()
 
+
 def exec_query_compare_result(query_text):
     accurate_result = node1.query(query_text)
-    test_result = node1.query(query_text + " SETTINGS allow_experimental_query_coordination = 1")
+    test_result = node1.query(
+        query_text + " SETTINGS allow_experimental_query_coordination = 1"
+    )
     assert accurate_result == test_result
 
+
 def test_query(started_cluster):
-    node1.query("INSERT INTO wf_partition SELECT part_key, value, order FROM generateRandom('part_key UInt8, value Int8, order Int8') LIMIT 200")
+    node1.query("""
+        INSERT INTO test_table
+        SELECT part_key, value, order
+        FROM generateRandom('part_key UInt8, value Int8, order Int8') LIMIT 200
+    """)
 
-    node1.query("SYSTEM FLUSH DISTRIBUTED wf_partition")
+    node1.query("SYSTEM FLUSH DISTRIBUTED test_table")
 
-    exec_query_compare_result("SELECT part_key, value, order, groupArray(value) OVER (PARTITION BY part_key) AS frame_values FROM wf_partition ORDER BY part_key ASC, value ASC")
+    exec_query_compare_result("""
+        SELECT part_key, value, order, groupArray(value) OVER (PARTITION BY part_key ORDER BY value)
+        AS frame_values
+        FROM test_table
+        ORDER BY part_key ASC, value ASC
+    """)
