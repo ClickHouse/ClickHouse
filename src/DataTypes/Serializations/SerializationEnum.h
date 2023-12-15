6e6eb5c01efd64b7a725ec6d5dd3c6d98798f0ab
Duc Canh Le <duccanh.le@ahrefs.com>
2023-12-15 06:49:12 +0000

    fast get default enum serialization
    Signed-off-by: Duc Canh Le <duccanh.le@ahrefs.com>

--- src/DataTypes/Serializations/SerializationEnum.h
+++ src/DataTypes/Serializations/SerializationEnum.h
@@ -1,20 +1,32 @@
 #pragma once
 
+#include <memory>
 #include <DataTypes/Serializations/SerializationNumber.h>
 #include <DataTypes/EnumValues.h>
+#include <DataTypes/DataTypeEnum.h>
 
 namespace DB
 {
 
 template <typename Type>
-class SerializationEnum : public SerializationNumber<Type>, public EnumValues<Type>
+class SerializationEnum : public SerializationNumber<Type>
 {
 public:
     using typename SerializationNumber<Type>::FieldType;
     using typename SerializationNumber<Type>::ColumnType;
-    using typename EnumValues<Type>::Values;
+    using Values = EnumValues<Type>::Values;
 
-    explicit SerializationEnum(const Values & values_) : EnumValues<Type>(values_) {}
+    SerializationEnum() = delete;
+    /// To explicitly create a SerializationEnum from Values
+    explicit SerializationEnum(const Values & values_) : own_enum_values(values_), ref_enum_values(&own_enum_values.value()) { }
+    /// To create a SerializationEnum from an IDataType instance, will reuse EnumValues from the type
+    /// Motivation: some Enum type has many elements, and building EnumValues is not trivial
+    /// This constructor allow to create many SerializationEnum from same IDataType without rebuilding
+    /// EnumValues for every call, so it's useful to get default serialization.
+    explicit SerializationEnum(const std::shared_ptr<const DataTypeEnum<Type>> & enum_type)
+        : own_enum_type(enum_type), ref_enum_values(static_cast<const EnumValues<Type> *>(enum_type.get()))
+    {
+    }
 
     void serializeText(const IColumn & column, size_t row_num, WriteBuffer & ostr, const FormatSettings &) const override;
     void serializeTextEscaped(const IColumn & column, size_t row_num, WriteBuffer & ostr, const FormatSettings &) const override;
@@ -35,8 +47,12 @@ public:
     {
         FieldType x;
         readText(x, istr);
-        return this->findByValue(x)->first;
+        return ref_enum_values->findByValue(x)->first;
     }
+
+    std::optional<EnumValues<Type>> own_enum_values;
+    std::shared_ptr<const DataTypeEnum<Type>> own_enum_type;
+    const EnumValues<Type> * ref_enum_values;
 };
 
 }
