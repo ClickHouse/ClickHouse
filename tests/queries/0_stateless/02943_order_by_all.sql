b0e17f8b999068818846553f1ea68c068ebf6b63
ubuntu <872237106@qq.com>
2023-12-15 11:32:07 +0800

    fix test

--- tests/queries/0_stateless/02943_order_by_all.sql
+++ tests/queries/0_stateless/02943_order_by_all.sql
@@ -8,8 +8,9 @@ CREATE TABLE order_by_all
 )
 engine = Memory;
 
-insert into order_by_all values ('abc2', 3, 2), ('abc3', 2, 3), ('abc4', 1, 4), ('abc1', 3, 2);
+insert into order_by_all values ('abc2', 3, 2), ('abc3', 2, 3), ('abc2', 1, 1), ('abc1', 3, 2);
 
 select a, b, c from order_by_all order by all;
-select a, count(b), count(c) from order_by_all group by all order by all;
-select substring(a, 1, 3), count(b) from order_by_all group by all order by all;
+select count(b), a, count(c) from order_by_all group by all order by all;
+select substring(a, 1, 3), b, c from order_by_all order by all;
+
