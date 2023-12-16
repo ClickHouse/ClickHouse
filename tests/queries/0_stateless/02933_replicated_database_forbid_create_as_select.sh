af2a46064734de2820f8efb42c0ddec0ef7df9a0
Nikolay Degterinsky <evillique@gmail.com>
2023-12-16 14:50:56 +0000

    Fix style

--- tests/queries/0_stateless/02933_replicated_database_forbid_create_as_select.sh
+++ tests/queries/0_stateless/02933_replicated_database_forbid_create_as_select.sh
@@ -12,5 +12,5 @@ ${CLICKHOUSE_CLIENT} --allow_experimental_database_replicated=1 --query "CREATE 
 # Non-replicated engines are allowed
 ${CLICKHOUSE_CLIENT} --distributed_ddl_output_mode=none --query "CREATE TABLE ${CLICKHOUSE_DATABASE}_db.test (id UInt64) ENGINE = MergeTree() ORDER BY id AS SELECT 1"
 # Replicated storafes are forbidden
-${CLICKHOUSE_CLIENT} --query "CREATE TABLE ${CLICKHOUSE_DATABASE}_db.test (id UInt64) ENGINE = ReplicatedMergeTree() ORDER BY id AS SELECT 1" |& grep -cm1 "SUPPORT_IS_DISABLED"
+${CLICKHOUSE_CLIENT} --query "CREATE TABLE ${CLICKHOUSE_DATABASE}_db.test2 (id UInt64) ENGINE = ReplicatedMergeTree('/clickhouse/$CLICKHOUSE_TEST_ZOOKEEPER_PREFIX/test2', '1') ORDER BY id AS SELECT 1" |& grep -cm1 "SUPPORT_IS_DISABLED"
 ${CLICKHOUSE_CLIENT} --query "DROP DATABASE ${CLICKHOUSE_DATABASE}_db"
