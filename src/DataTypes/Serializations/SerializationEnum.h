b07cde5ebb1cd2f054bd06462759879a2fec4267
Duc Canh Le <duccanh.le@ahrefs.com>
2023-12-18 05:35:38 +0000

    use const reference instead for pointer to values
    Co-authored-by: Robert Schulze <robert@clickhouse.com>
    Signed-off-by: Duc Canh Le <duccanh.le@ahrefs.com>

--- src/DataTypes/Serializations/SerializationEnum.h
+++ src/DataTypes/Serializations/SerializationEnum.h
@@ -16,15 +16,18 @@ public:
     using typename SerializationNumber<Type>::ColumnType;
     using Values = EnumValues<Type>::Values;
 
-    SerializationEnum() = delete;
-    /// To explicitly create a SerializationEnum from Values
-    explicit SerializationEnum(const Values & values_) : own_enum_values(values_), ref_enum_values(&own_enum_values.value()) { }
-    /// To create a SerializationEnum from an IDataType instance, will reuse EnumValues from the type
-    /// Motivation: some Enum type has many elements, and building EnumValues is not trivial
-    /// This constructor allow to create many SerializationEnum from same IDataType without rebuilding
-    /// EnumValues for every call, so it's useful to get default serialization.
+    // SerializationEnum can be constructed in two ways:
+    /// - Make a copy of the Enum name-to-type mapping.
+    /// - Only store a reference to an existing mapping. This is faster if the Enum has a lot of different values or if SerializationEnum is
+    ///   constructed very frequently. Make sure that the pointed-to mapping has a longer lifespan than SerializationEnum!
+
+    explicit SerializationEnum(const Values & values_)
+        : own_enum_values(values_), ref_enum_values(own_enum_values.value())
+    {
+    }
+
     explicit SerializationEnum(const std::shared_ptr<const DataTypeEnum<Type>> & enum_type)
-        : own_enum_type(enum_type), ref_enum_values(static_cast<const EnumValues<Type> *>(enum_type.get()))
+        : own_enum_type(enum_type), ref_enum_values(*enum_type)
     {
     }
 
@@ -47,12 +50,12 @@ public:
     {
         FieldType x;
         readText(x, istr);
-        return ref_enum_values->findByValue(x)->first;
+        return ref_enum_values.findByValue(x)->first;
     }
 
     std::optional<EnumValues<Type>> own_enum_values;
     std::shared_ptr<const DataTypeEnum<Type>> own_enum_type;
-    const EnumValues<Type> * ref_enum_values;
+    const EnumValues<Type> & ref_enum_values;
 };
 
 }
