761554e86d04ca3d7984037cfba72a34e00b1498
凌涛 <lingtaolf@gmail.com>
2023-12-18 18:08:41 +0800

    fix test

--- tests/queries/0_stateless/02943_tokenbf_support_match.sql
+++ tests/queries/0_stateless/02943_tokenbf_support_match.sql
@@ -1,3 +1,5 @@
+-- Tags: no-parallel
+
 DROP DATABASE IF EXISTS test_tokenbf_match;
 
 CREATE DATABASE test_tokenbf_match;
@@ -14,29 +16,35 @@ SETTINGS index_granularity = 1;
  
 INSERT INTO test_tokenbf_match.test_tokenbf VALUES (1, 'Hello ClickHouse'), (2, 'Hello World'), (3, 'Hello Github'), (4, 'Hello Cloud'), (5, 'OLAP Database');
 
-SELECT '============== SKIP 3 GRANUS ============';
-SELECT '============== Required String: Hello ============';
-SELECT '============== Alternative String: Hello ClickHouse ============';
-SELECT '============== Required String: Hello World ============';
+SELECT '========================================';
+SELECT '| SKIP 3 GRANUS                        |';
+SELECT '| Required String: Hello               |';
+SELECT '| Alternative String: Hello ClickHouse |';
+SELECT '| Alternative String: Hello World      |';
+SELECT '========================================';
 
 EXPLAIN indexes=1 SELECT * FROM test_tokenbf_match.test_tokenbf WHERE match(str, 'Hello (ClickHouse|World)');
 
 SELECT '';
 SELECT '';
 
-SELECT '============== SKIP 3 GRANUS ============';
-SELECT '============== No Required String ============';
-SELECT '============== Alternative String: ClickHouse ============';
-SELECT '============== Alternative String: World ============';
+SELECT '========================================';
+SELECT '| SKIP 3 GRANUS                        |';
+SELECT '| No Required String                   |';
+SELECT '| Alternative String: ClickHouse       |';
+SELECT '| Alternative String: World            |';
+SELECT '========================================';
 
 EXPLAIN indexes = 1 SELECT * FROM test_tokenbf_match.test_tokenbf where match(str, '(.*?)* (ClickHouse|World)');
 
 SELECT '';
 SELECT '';
 
-SELECT '============== SKIP 4 GRANUS ============';
-SELECT '============== Required String: OLAP============';
-SELECT '============== No Alternative String============';
+SELECT '========================================';
+SELECT '| SKIP 4 GRANUS                        |';
+SELECT '| Required String: OLAP                |';
+SELECT '| No Alternative String                |';
+SELECT '========================================';
 
 EXPLAIN indexes = 1 SELECT * FROM test_tokenbf_match.test_tokenbf where match(str, 'OLAP (.*?)*');
 
