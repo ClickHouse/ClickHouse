975c954d1a9430079005605025fd52ee00f392ef
yariks5s <yaroslav.briukhovetskyi@clickhouse.com>
2023-12-18 15:42:50 +0000

    style check

--- src/Processors/Transforms/PasteJoinTransform.cpp
+++ src/Processors/Transforms/PasteJoinTransform.cpp
@@ -24,7 +24,6 @@ namespace ErrorCodes
 {
     extern const int LOGICAL_ERROR;
     extern const int NOT_IMPLEMENTED;
-    extern const int LOGICAL_ERROR;
 }
 
 namespace
--- src/QueryPipeline/QueryPipelineBuilder.cpp
+++ src/QueryPipeline/QueryPipelineBuilder.cpp
@@ -37,6 +37,7 @@ namespace ErrorCodes
 {
     extern const int LOGICAL_ERROR;
     extern const int NOT_IMPLEMENTED;
+    extern const int BAD_ARGUMENTS;
 }
 
 void QueryPipelineBuilder::checkInitialized()
