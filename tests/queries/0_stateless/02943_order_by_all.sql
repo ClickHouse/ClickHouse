a077ad4c15936c2adae1397b4d9b8c3fabc30277
ubuntu <872237106@qq.com>
2023-12-19 15:18:27 +0800

    support new analyzer

--- tests/queries/0_stateless/02943_order_by_all.sql
+++ tests/queries/0_stateless/02943_order_by_all.sql
@@ -43,4 +43,39 @@ SELECT format('{} {}', a, b) AS all FROM order_by_all ORDER BY all SETTINGS enab
 
 SELECT a, b, all FROM order_by_all ORDER BY all, a;
 
+SELECT '-- enable new analyzer';
+set allow_experimental_analyzer = 1;
+
+SELECT '-- no modifiers';
+SELECT a, b FROM order_by_all ORDER BY ALL;
+SELECT b, a FROM order_by_all ORDER BY ALL;
+
+SELECT '-- with ASC/DESC modifiers';
+SELECT a, b FROM order_by_all ORDER BY ALL ASC;
+SELECT a, b FROM order_by_all ORDER BY ALL DESC;
+
+SELECT '-- with NULLS FIRST/LAST modifiers';
+SELECT b, a FROM order_by_all ORDER BY ALL NULLS FIRST;
+SELECT b, a FROM order_by_all ORDER BY ALL NULLS LAST;
+
+SELECT '-- what happens if some column "all" already exists?';
+
+-- columns
+SELECT a, b, all FROM order_by_all ORDER BY all;  -- { serverError UNEXPECTED_EXPRESSION }
+SELECT a, b, all FROM order_by_all ORDER BY ALL;  -- { serverError UNEXPECTED_EXPRESSION }
+SELECT a, b, all FROM order_by_all ORDER BY all SETTINGS enable_order_by_all = false;
+
+-- column aliases
+SELECT a, b AS all FROM order_by_all ORDER BY all;  -- { serverError UNEXPECTED_EXPRESSION }
+SELECT a, b AS all FROM order_by_all ORDER BY ALL;  -- { serverError UNEXPECTED_EXPRESSION }
+SELECT a, b AS all FROM order_by_all ORDER BY all SETTINGS enable_order_by_all = false;
+
+-- expressions
+SELECT format('{} {}', a, b) AS all FROM order_by_all ORDER BY all;  -- { serverError UNEXPECTED_EXPRESSION }
+SELECT format('{} {}', a, b) AS all FROM order_by_all ORDER BY ALL;  -- { serverError UNEXPECTED_EXPRESSION }
+SELECT format('{} {}', a, b) AS all FROM order_by_all ORDER BY all SETTINGS enable_order_by_all = false;
+
+SELECT a, b, all FROM order_by_all ORDER BY all, a;
+
 DROP TABLE order_by_all;
+
