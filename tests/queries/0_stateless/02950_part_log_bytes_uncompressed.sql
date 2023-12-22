b4c3969d3a79fb931c1eff095686494b79556dcc
Jordi Villar <jrdi.villar@gmail.com>
2023-12-22 18:30:44 +0100

    Add bytes_uncompressed to system.part_log

---
+++ tests/queries/0_stateless/02950_part_log_bytes_uncompressed.sql
@@ -0,0 +1,21 @@
+CREATE TABLE part_log_bytes_uncompressed (
+    key UInt8,
+    value UInt8
+)
+Engine=MergeTree()
+ORDER BY key;
+
+INSERT INTO part_log_bytes_uncompressed VALUES (1, 1);
+INSERT INTO part_log_bytes_uncompressed VALUES (2, 1);
+
+OPTIMIZE TABLE part_log_bytes_uncompressed FINAL;
+
+ALTER TABLE part_log_bytes_uncompressed UPDATE value = 3 WHERE 1 = 1 SETTINGS mutations_sync=2;
+
+SYSTEM FLUSH LOGS;
+
+SELECT event_type, table, part_name, bytes_uncompressed FROM system.part_log
+WHERE event_date >= yesterday() AND database = currentDatabase() AND table = 'part_log_bytes_uncompressed'
+ORDER BY event_time_microseconds;
+
+DROP TABLE part_log_bytes_uncompressed;
