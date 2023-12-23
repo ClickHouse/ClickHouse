f780a2f838d2b8a83eed2ec97703571528a7c7ed
una <dengmengda@gmail.com>
2023-12-23 18:42:41 +0800

    feat:add InitialQuery event

--- src/Interpreters/DDLWorker.cpp
+++ src/Interpreters/DDLWorker.cpp
@@ -490,7 +490,7 @@ bool DDLWorker::tryExecuteQuery(DDLTaskBase & task, const ZooKeeperPtr & zookeep
 
         if (!task.is_initial_query)
             query_scope.emplace(query_context);
-        
+
         executeQuery(istr, ostr, !task.is_initial_query, query_context, {}, QueryFlags{ .internal = false, .distributed_backup_restore = task.entry.is_backup_restore });
 
         if (auto txn = query_context->getZooKeeperMetadataTransaction())
