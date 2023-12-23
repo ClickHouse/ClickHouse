56e7965d8d4ab63259a0a2f3538c3137df06275f
Jordi Villar <jrdi.villar@gmail.com>
2023-12-23 09:39:49 +0100

    Try to fix tests for the CI

--- tests/queries/0_stateless/02950_part_log_bytes_uncompressed.sql
+++ tests/queries/0_stateless/02950_part_log_bytes_uncompressed.sql
@@ -5,19 +5,19 @@ CREATE TABLE part_log_bytes_uncompressed (
 Engine=MergeTree()
 ORDER BY key;
 
-INSERT INTO part_log_bytes_uncompressed VALUES (1, 1);
-INSERT INTO part_log_bytes_uncompressed VALUES (2, 1);
+INSERT INTO part_log_bytes_uncompressed SELECT 1, 1 FROM numbers(1000);
+INSERT INTO part_log_bytes_uncompressed SELECT 2, 1 FROM numbers(1000);
 
 OPTIMIZE TABLE part_log_bytes_uncompressed FINAL;
 
 ALTER TABLE part_log_bytes_uncompressed UPDATE value = 3 WHERE 1 = 1 SETTINGS mutations_sync=2;
 
-INSERT INTO part_log_bytes_uncompressed VALUES (3, 1);
+INSERT INTO part_log_bytes_uncompressed SELECT 3, 1 FROM numbers(1000);
 ALTER TABLE part_log_bytes_uncompressed DROP PART 'all_4_4_0' SETTINGS mutations_sync=2;
 
 SYSTEM FLUSH LOGS;
 
-SELECT event_type, table, part_name, bytes_uncompressed FROM system.part_log
+SELECT event_type, table, part_name, bytes_uncompressed > 0, size_in_bytes < bytes_uncompressed FROM system.part_log
 WHERE event_date >= yesterday() AND database = currentDatabase() AND table = 'part_log_bytes_uncompressed'
 ORDER BY event_time_microseconds;
 
