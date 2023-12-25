9196c2b994475db5273f50d9695a055b82a98ae3
Alexey Milovidov <milovidov@clickhouse.com>
2023-12-25 04:54:54 +0100

    Follow-up

--- tests/queries/0_stateless/02952_binary.sh
+++ tests/queries/0_stateless/02952_binary.sh
@@ -4,4 +4,4 @@ CURDIR=$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)
 # shellcheck source=../shell_config.sh
 . "$CURDIR"/../shell_config.sh
 
-${CLICKHOUSE_CURL} -sS "${CLICKHOUSE_PORT_HTTP_PROTO}://${CLICKHOUSE_HOST}:${CLICKHOUSE_PORT_HTTP}/binary" | grep -oF --max-count 1 'addressToSymbol'
+${CLICKHOUSE_CURL} -s "${CLICKHOUSE_PORT_HTTP_PROTO}://${CLICKHOUSE_HOST}:${CLICKHOUSE_PORT_HTTP}/binary" 2>/dev/null | grep -oF --max-count 1 'addressToSymbol'
