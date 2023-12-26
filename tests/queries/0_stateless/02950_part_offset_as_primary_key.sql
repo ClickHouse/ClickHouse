4fbe41b47e21ee9e80467c39f214942d5551f730
Alexey Milovidov <milovidov@clickhouse.com>
2023-12-26 16:51:39 +0300

    Update 02950_part_offset_as_primary_key.sql

--- tests/queries/0_stateless/02950_part_offset_as_primary_key.sql
+++ tests/queries/0_stateless/02950_part_offset_as_primary_key.sql
@@ -6,7 +6,7 @@ insert into a select -number from numbers(5);
 -- nothing to read
 select i from a where _part_offset >= 5 order by i settings max_bytes_to_read = 1;
 
--- one granules
+-- one granule
 select i from a where _part_offset = 0 order by i settings max_rows_to_read = 2;
 select i from a where _part_offset = 1 order by i settings max_rows_to_read = 2;
 select i from a where _part_offset = 2 order by i settings max_rows_to_read = 2;
@@ -17,7 +17,7 @@ select i from a where _part_offset = 4 order by i settings max_rows_to_read = 1;
 select i from a where _part_offset in (1, 4) order by i settings max_rows_to_read = 3;
 select i from a where _part_offset not in (1, 4) order by i settings max_rows_to_read = 4;
 
--- force primary key check still works
+-- the force_primary_key check still works
 select i from a where _part_offset = 4 order by i settings force_primary_key = 1; -- { serverError INDEX_NOT_USED }
 
 -- combining with other primary keys doesn't work (makes no sense)
