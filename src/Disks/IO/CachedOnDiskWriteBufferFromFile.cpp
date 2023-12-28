d5b138eee4668018330d423aef33bd38a105298c
kssenii <sumarokovakseniia@mail.ru>
2023-12-28 13:41:07 +0100

    Log exception

--- src/Disks/IO/CachedOnDiskWriteBufferFromFile.cpp
+++ src/Disks/IO/CachedOnDiskWriteBufferFromFile.cpp
@@ -231,6 +231,8 @@ void CachedOnDiskWriteBufferFromFile::nextImpl()
     }
     catch (...)
     {
+        tryLogCurrentException(__PRETTY_FUNCTION__);
+
         /// If something was already written to cache, remove it.
         cache_writer.reset();
         cache->removeKeyIfExists(key);
