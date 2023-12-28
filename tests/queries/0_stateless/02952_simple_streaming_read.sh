6137505e9b78b2b19b30590d059599b45f856048
Mikhail Artemenko <michicosun@ya.ru>
2023-12-28 20:24:05 +0000

    improve test

--- tests/queries/0_stateless/02952_simple_streaming_read.sh
+++ tests/queries/0_stateless/02952_simple_streaming_read.sh
@@ -9,36 +9,50 @@ opts=(
     "--allow_experimental_streaming=1"
 )
 
-$CLICKHOUSE_CLIENT "${opts[@]}" -q "DROP TABLE IF EXISTS t_streaming_test"
-$CLICKHOUSE_CLIENT "${opts[@]}" -q "CREATE TABLE t_streaming_test (a String, b UInt64) ENGINE = MergeTree ORDER BY (a)"
+function run_test {
+    printf "=====\n"
+    printf "running test for engine: $1\n"
+    sleep 0.5
 
-$CLICKHOUSE_CLIENT "${opts[@]}" -q "SELECT count() FROM t_streaming_test"
-$CLICKHOUSE_CLIENT "${opts[@]}" -q "INSERT INTO t_streaming_test VALUES ('a', 100)"
-sleep 0.5
+    $CLICKHOUSE_CLIENT "${opts[@]}" -q "DROP TABLE IF EXISTS t_streaming_test"
+    $CLICKHOUSE_CLIENT "${opts[@]}" -q "CREATE TABLE t_streaming_test (a String, b UInt64) ENGINE = $1"
 
-# spawn stream
-$CLICKHOUSE_CLIENT "${opts[@]}" -q "SELECT a, a || '-' || a, b * b FROM t_streaming_test STREAM" &
-sleep 0.5
+    $CLICKHOUSE_CLIENT "${opts[@]}" -q "SELECT count() FROM t_streaming_test"
+    $CLICKHOUSE_CLIENT "${opts[@]}" -q "INSERT INTO t_streaming_test VALUES ('a', 100)"
+    sleep 0.5
 
-# insert some data into stream
-$CLICKHOUSE_CLIENT "${opts[@]}" -q "INSERT INTO t_streaming_test (*) select number as a, number as b from numbers(7)"
-sleep 0.5
+    # spawn stream
+    $CLICKHOUSE_CLIENT "${opts[@]}" -q "SELECT a, a || '-' || a, b * b FROM t_streaming_test STREAM" &
+    sleep 0.5
 
-# stop reading by killing client job
-pkill -P $$
-sleep 0.5
+    # insert some data into stream
+    $CLICKHOUSE_CLIENT "${opts[@]}" -q "INSERT INTO t_streaming_test (*) select number as a, number as b from numbers(7)"
+    sleep 0.5
 
-# clear table
-$CLICKHOUSE_CLIENT "${opts[@]}" -q "TRUNCATE t_streaming_test"
+    # stop reading by killing client job
+    pkill -P $$
+    sleep 0.5
 
-# spawn 2 streams
-$CLICKHOUSE_CLIENT "${opts[@]}" -q "SELECT a FROM t_streaming_test STREAM" &
-$CLICKHOUSE_CLIENT "${opts[@]}" -q "SELECT a FROM t_streaming_test STREAM" &
-sleep 0.5
+    # clear table
+    $CLICKHOUSE_CLIENT "${opts[@]}" -q "TRUNCATE t_streaming_test"
 
-# insert some data into stream
-$CLICKHOUSE_CLIENT "${opts[@]}" -q "INSERT INTO t_streaming_test (*) select 'same-x-2' as a, number as b from numbers(2)"
-sleep 0.5
+    # spawn 2 streams
+    $CLICKHOUSE_CLIENT "${opts[@]}" -q "SELECT a FROM t_streaming_test STREAM" &
+    $CLICKHOUSE_CLIENT "${opts[@]}" -q "SELECT a FROM t_streaming_test STREAM" &
+    sleep 0.5
 
-# stop reading by killing client job
-pkill -P $$
+    # insert some data into stream
+    $CLICKHOUSE_CLIENT "${opts[@]}" -q "INSERT INTO t_streaming_test (*) select 'same-x-2' as a, number as b from numbers(2)"
+    sleep 0.5
+
+    # stop reading by killing client job
+    pkill -P $$
+    sleep 0.5
+}
+
+for engine_type in "MergeTree() ORDER BY (a)" "TinyLog" "StripeLog" "Memory"
+do
+   run_test "$engine_type"
+done
+
+
