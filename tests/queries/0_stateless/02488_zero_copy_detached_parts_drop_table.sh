c4e46db3c049fd6f7cf028f1d983a1122f044a0e
Alexander Tokmakov <tavplubix@clickhouse.com>
2023-12-29 13:05:50 +0100

    Update 02488_zero_copy_detached_parts_drop_table.sh

--- tests/queries/0_stateless/02488_zero_copy_detached_parts_drop_table.sh
+++ tests/queries/0_stateless/02488_zero_copy_detached_parts_drop_table.sh
@@ -6,9 +6,9 @@ CURDIR=$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)
 . "$CURDIR"/../shell_config.sh
 
 $CLICKHOUSE_CLIENT -q "create table rmt1 (n int) engine=ReplicatedMergeTree('/test/02488/$CLICKHOUSE_TEST_ZOOKEEPER_PREFIX', '1') order by n
-  settings min_bytes_for_wide_part=0, allow_remote_fs_zero_copy_replication=1, storage_policy='s3'"
+  settings min_bytes_for_wide_part=0, allow_remote_fs_zero_copy_replication=1, storage_policy='s3_cache'"
 $CLICKHOUSE_CLIENT -q "create table rmt2 (n int) engine=ReplicatedMergeTree('/test/02488/$CLICKHOUSE_TEST_ZOOKEEPER_PREFIX', '2') order by n
-  settings min_bytes_for_wide_part=0, allow_remote_fs_zero_copy_replication=1, storage_policy='s3'"
+  settings min_bytes_for_wide_part=0, allow_remote_fs_zero_copy_replication=1, storage_policy='s3_cache'"
 
 $CLICKHOUSE_CLIENT --insert_keeper_fault_injection_probability=0 -q "insert into rmt2 values (42)"
 $CLICKHOUSE_CLIENT -q "system sync replica rmt1"
