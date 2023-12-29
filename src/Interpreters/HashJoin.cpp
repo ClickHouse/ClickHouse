5c42f2c0411ba9a3604405d41370304a26a6f542
liuneng <1398775315@qq.com>
2023-12-29 15:29:54 +0800

    optimize default value

--- src/Interpreters/HashJoin.cpp
+++ src/Interpreters/HashJoin.cpp
@@ -1102,12 +1102,19 @@ public:
         for (size_t i = 0; i < this->size(); ++i)
         {
             auto& col = columns[i];
+            size_t default_count = 0;
             for (size_t j = 0; j < lazy_output.blocks.size(); ++j)
             {
                 if (!lazy_output.blocks[j])
                 {
-                    type_name[i].type->insertDefaultInto(*col);
+//                    type_name[i].type->insertDefaultInto(*col);
+                    default_count ++;
                     continue;
+                }
+                if (default_count > 0)
+                {
+                    JoinCommon::addDefaultValues(*col, type_name[i].type, default_count);
+                    default_count = 0;
                 }
                 const auto & column_from_block = reinterpret_cast<const Block *>(lazy_output.blocks[j])->getByPosition(right_indexes[i]);
                 /// If it's joinGetOrNull, we need to wrap not-nullable columns in StorageJoin.
@@ -1121,6 +1128,11 @@ public:
                     }
                 }
                 col->insertFrom(*column_from_block.column, lazy_output.row_nums[j]);
+            }
+            if (default_count > 0)
+            {
+                JoinCommon::addDefaultValues(*col, type_name[i].type, default_count);
+                default_count = 0;
             }
         }
     }
