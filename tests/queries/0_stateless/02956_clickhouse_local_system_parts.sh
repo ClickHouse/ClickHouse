743a5dd156157370cd88768ddb365c87324aba1b
Alexey Milovidov <milovidov@clickhouse.com>
2023-12-30 14:34:15 +0300

    Update 02956_clickhouse_local_system_parts.sh

--- tests/queries/0_stateless/02956_clickhouse_local_system_parts.sh
+++ tests/queries/0_stateless/02956_clickhouse_local_system_parts.sh
@@ -5,4 +5,4 @@ CUR_DIR=$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)
 # shellcheck source=../shell_config.sh
 . "$CUR_DIR"/../shell_config.sh
 
-$CLICKHOUSE_LOCAL --multiquery "CREATE TABLE test (x UInt8) ENGINE = MergeTree ORDER BY (); INSERT INTO test SELECT 1; SELECT table, name, rows FROM system.parts;"
+$CLICKHOUSE_LOCAL --multiquery "CREATE TABLE test (x UInt8) ENGINE = MergeTree ORDER BY (); INSERT INTO test SELECT 1; SELECT table, name, rows FROM system.parts WHERE database = currentDatabase();"
