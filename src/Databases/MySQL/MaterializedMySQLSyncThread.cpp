f7ef5633bdaf8b7ac502700d920add8a9d54bb61
Alexey Milovidov <milovidov@clickhouse.com>
2023-12-30 18:03:20 +0100

    Remove "unsafe" from MaterializedMySQL

--- src/Databases/MySQL/MaterializedMySQLSyncThread.cpp
+++ src/Databases/MySQL/MaterializedMySQLSyncThread.cpp
@@ -590,7 +590,7 @@ static void writeFieldsToColumn(
         {
             for (size_t index = 0; index < rows_data.size(); ++index)
             {
-                const Tuple & row_data = rows_data[index].get<const Tuple &>();
+                const Tuple & row_data = rows_data[index].safeGet<const Tuple &>();
                 const Field & value = row_data[column_index];
 
                 if (write_data_to_null_map(value, index))
@@ -630,17 +630,17 @@ static void writeFieldsToColumn(
         {
             for (size_t index = 0; index < rows_data.size(); ++index)
             {
-                const Tuple & row_data = rows_data[index].get<const Tuple &>();
+                const Tuple & row_data = rows_data[index].safeGet<const Tuple &>();
                 const Field & value = row_data[column_index];
 
                 if (write_data_to_null_map(value, index))
                 {
                     if (value.getType() == Field::Types::UInt64)
-                        casted_int32_column->insertValue(static_cast<Int32>(value.get<Int32>()));
+                        casted_int32_column->insertValue(static_cast<Int32>(value.safeGet<Int32>()));
                     else if (value.getType() == Field::Types::Int64)
                     {
                         /// For MYSQL_TYPE_INT24
-                        const Int32 & num = static_cast<Int32>(value.get<Int32>());
+                        const Int32 & num = static_cast<Int32>(value.safeGet<Int32>());
                         casted_int32_column->insertValue(num & 0x800000 ? num | 0xFF000000 : num);
                     }
                     else
@@ -652,12 +652,12 @@ static void writeFieldsToColumn(
         {
             for (size_t index = 0; index < rows_data.size(); ++index)
             {
-                const Tuple & row_data = rows_data[index].get<const Tuple &>();
+                const Tuple & row_data = rows_data[index].safeGet<const Tuple &>();
                 const Field & value = row_data[column_index];
 
                 if (write_data_to_null_map(value, index))
                 {
-                    const String & data = value.get<const String &>();
+                    const String & data = value.safeGet<const String &>();
                     casted_string_column->insertData(data.data(), data.size());
                 }
             }
@@ -666,12 +666,12 @@ static void writeFieldsToColumn(
         {
             for (size_t index = 0; index < rows_data.size(); ++index)
             {
-                const Tuple & row_data = rows_data[index].get<const Tuple &>();
+                const Tuple & row_data = rows_data[index].safeGet<const Tuple &>();
                 const Field & value = row_data[column_index];
 
                 if (write_data_to_null_map(value, index))
                 {
-                    const String & data = value.get<const String &>();
+                    const String & data = value.safeGet<const String &>();
                     casted_fixed_string_column->insertData(data.data(), data.size());
                 }
             }
@@ -718,7 +718,7 @@ static inline size_t onUpdateData(const Row & rows_data, Block & buffer, size_t 
     {
         writeable_rows_mask[index + 1] = true;
         writeable_rows_mask[index] = differenceSortingKeys(
-            rows_data[index].get<const Tuple &>(), rows_data[index + 1].get<const Tuple &>(), sorting_columns_index);
+            rows_data[index].safeGet<const Tuple &>(), rows_data[index + 1].safeGet<const Tuple &>(), sorting_columns_index);
     }
 
     for (size_t column = 0; column < buffer.columns() - 2; ++column)
