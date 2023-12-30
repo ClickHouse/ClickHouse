3e16559b496fe322f340c07d37347064c4300d2a
Christoph Wurm <christoph@clickhouse.com>
2023-12-30 00:09:49 +0000

    Fix test.

--- tests/queries/0_stateless/02564_read_in_order_final_desc.sql
+++ tests/queries/0_stateless/02564_read_in_order_final_desc.sql
@@ -28,7 +28,7 @@ SELECT if(explain like '%ReadType: InOrder%', 'Ok', 'Error: ' || explain) FROM (
 ) WHERE explain like '%ReadType%';
 
 
-SELECT if(explain like '%ReadType: Default%', 'Ok', 'Error: ' || explain) FROM (
+SELECT if(explain like '%ReadType: InReverseOrder%', 'Ok', 'Error: ' || explain) FROM (
     EXPLAIN PLAN actions = 1
     SELECT timestamp, value
     FROM mytable FINAL
