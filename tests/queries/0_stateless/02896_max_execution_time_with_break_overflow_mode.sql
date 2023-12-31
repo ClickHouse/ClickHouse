730135f933575a1d1c56cde18b5e3f02ebd79e57
zhangyifan27 <chinazhangyifan@163.com>
2023-12-31 12:53:53 +0800

    Seperate max_execution_time and max_estimated_execution_time

--- tests/queries/0_stateless/02896_max_execution_time_with_break_overflow_mode.sql
+++ tests/queries/0_stateless/02896_max_execution_time_with_break_overflow_mode.sql
@@ -4,7 +4,10 @@
 SELECT * FROM numbers(100000000) SETTINGS max_block_size=1, max_execution_time=13, timeout_overflow_mode='break' FORMAT Null;
 
 -- Query returns an error when runtime is estimated after 10 sec of execution
-SELECT * FROM numbers(100000000) SETTINGS max_block_size=1, max_execution_time=13, timeout_overflow_mode='throw' FORMAT Null; -- { serverError TOO_SLOW }
+SELECT * FROM numbers(100000000) SETTINGS max_block_size=1, max_estimated_execution_time=13, timeout_overflow_mode='throw' FORMAT Null; -- { serverError TOO_SLOW }
+
+-- Query returns an error after timeout
+SELECT * FROM numbers(100000000) SETTINGS max_block_size=1, max_execution_time=13, timeout_overflow_mode='throw' FORMAT Null; -- { serverError TIMEOUT_EXCEEDED }
 
 -- Query returns timeout error before its full execution time is estimated
 SELECT * FROM numbers(100000000) SETTINGS max_block_size=1, max_execution_time=2, timeout_overflow_mode='throw' FORMAT Null; -- { serverError TIMEOUT_EXCEEDED }
