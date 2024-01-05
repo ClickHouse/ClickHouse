12585ded09e99d67667d95fe7818646da63182f5
yariks5s <yaroslav.briukhovetskyi@clickhouse.com>
2024-01-05 16:51:35 +0000

    init

---
+++ tests/queries/0_stateless/02956_fix_to_start_of_milli_microsecond.sql
@@ -0,0 +1,2 @@
+SELECT toStartOfInterval(toDateTime64('2023-10-09 10:11:12.000999', 6), toIntervalMillisecond(1));
+SELECT toStartOfInterval(toDateTime64('2023-10-09 10:11:12.000999', 6), toIntervalMillisecond(10));
