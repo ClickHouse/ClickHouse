b05211cc342c6ba329f1699c18fc2923848cdb3d
Alexander Tokmakov <tavplubix@clickhouse.com>
2024-01-08 20:16:21 +0100

    Update 02947_dropped_tables_parts.sql

--- tests/queries/0_stateless/02947_dropped_tables_parts.sql
+++ tests/queries/0_stateless/02947_dropped_tables_parts.sql
@@ -8,6 +8,7 @@ INSERT INTO 02947_table_1 VALUES (1),(2);
 INSERT INTO 02947_table_2 VALUES (3),(4);
 
 SELECT database, table, name FROM system.parts WHERE database = currentDatabase() AND startsWith(table, '02947_table_');
+select * from system.dropped_tables_parts format Null;
 
 DROP TABLE 02947_table_1;
 DROP TABLE 02947_table_2;
