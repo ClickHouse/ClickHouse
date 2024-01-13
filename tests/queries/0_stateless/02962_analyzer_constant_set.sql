2ecbe1f76ddc5fbd03893a2a01df3b19ff492d74
Alexey Milovidov <milovidov@clickhouse.com>
2024-01-13 06:14:57 +0300

    Update 02962_analyzer_constant_set.sql

--- tests/queries/0_stateless/02962_analyzer_constant_set.sql
+++ tests/queries/0_stateless/02962_analyzer_constant_set.sql
@@ -1,17 +1,15 @@
-create table test_parallel_index (
-  x UInt64, 
-  y UInt64, 
-  z UInt64, 
-  INDEX a (y) TYPE minmax GRANULARITY 2,
-  INDEX b (z) TYPE set(8) GRANULARITY 2
-) engine = MergeTree
-order by x 
-partition by bitAnd(x, 63 * 64)
-settings index_granularity = 4;
+DROP TABLE IF EXISTS test_parallel_index;
 
-insert into test_parallel_index
-  select number, number, number from numbers(1048576);
+CREATE TABLE test_parallel_index
+(
+    z UInt64,
+    INDEX i z TYPE set(8)
+)
+ENGINE = MergeTree
+ORDER BY ();
 
-select sum(z)
-from test_parallel_index
-where z = 2 or z = 7 or z = 13 or z = 17 or z = 19 or z = 23;
+insert into test_parallel_index select number from numbers(10);
+
+select sum(z) from test_parallel_index where z = 2 or z = 7 or z = 13 or z = 17 or z = 19 or z = 23;
+
+DROP TABLE test_parallel_index;
