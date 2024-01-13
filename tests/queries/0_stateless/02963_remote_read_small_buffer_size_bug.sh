013f2dcc4af67f051e0022a1096d4aee668d5067
Nikita Taranov <nikita.taranov@clickhouse.com>
2024-01-13 17:22:03 +0100

    fix test

--- tests/queries/0_stateless/02963_remote_read_small_buffer_size_bug.sh
+++ tests/queries/0_stateless/02963_remote_read_small_buffer_size_bug.sh
@@ -12,10 +12,12 @@ DROP TABLE IF EXISTS test;
 
 CREATE TABLE test (a Int32, s String)
 ENGINE = MergeTree()
-ORDER BY (s, a)
+ORDER BY a
 SETTINGS disk = disk(name = '$disk_name', type = cache, max_size = '10Gi', path = '$disk_name', disk = 's3_disk');
 
 INSERT INTO test SELECT number % 1000000, randomString(1) FROM numbers_mt(1e7) SETTINGS enable_filesystem_cache_on_write_operations = 0;
+
+OPTIMIZE TABLE test FINAL;
 "
 
 query_id=$(random_str 10)
@@ -24,7 +26,15 @@ $CLICKHOUSE_CLIENT -nm --query_id "$query_id" --query "
 WITH RANDOM_SET AS (
     SELECT rand32() % 10000 FROM numbers(100)
 )
-SELECT * FROM test WHERE a IN RANDOM_SET AND s = 'x' FORMAT Null SETTINGS max_threads = 5;
+SELECT *
+FROM test
+WHERE a IN RANDOM_SET AND s IN ('x', 'y', 'z')
+FORMAT Null
+SETTINGS
+    max_threads = 10,
+    allow_prefetched_read_pool_for_remote_filesystem = 1, filesystem_prefetch_min_bytes_for_single_read_task = '1Ki',
+    merge_tree_min_bytes_for_concurrent_read = 1, merge_tree_min_rows_for_concurrent_read = 1,
+    merge_tree_min_bytes_for_concurrent_read_for_remote_filesystem = 1, merge_tree_min_rows_for_concurrent_read_for_remote_filesystem = 1;
 "
 
 $CLICKHOUSE_CLIENT -nm --query "
