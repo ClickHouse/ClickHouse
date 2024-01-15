95c753a7248f0447eb376fee433290d361f0fd95
Smita Kulkarni <Smita.Kulkarni@clickhouse.com>
2024-01-15 10:05:05 +0100

    Add _block_number column as first column to avoid column number miscalculation

--- src/Storages/MergeTree/MergeTreeDataPartCompact.cpp
+++ src/Storages/MergeTree/MergeTreeDataPartCompact.cpp
@@ -62,15 +62,16 @@ IMergeTreeDataPart::MergeTreeWriterPtr MergeTreeDataPartCompact::getWriter(
     std::copy_if(columns_list.begin(), columns_list.end(), std::back_inserter(ordered_columns_list),
         [this](const auto & column) { return getColumnPosition(column.name) != std::nullopt; });
 
-    /// Order of writing is important in compact format
-    ordered_columns_list.sort([this](const auto & lhs, const auto & rhs)
-        { return *getColumnPosition(lhs.name) < *getColumnPosition(rhs.name); });
-
     /// _block_number column is not added by user, but is persisted in a part after merge
     /// If _block_number is not present in the parts to be merged, then it won't have a position
     /// So check if its not present and add it at the end
     if (columns_list.contains(BlockNumberColumn::name) && !ordered_columns_list.contains(BlockNumberColumn::name))
         ordered_columns_list.emplace_back(NameAndTypePair{BlockNumberColumn::name, BlockNumberColumn::type});
+
+    /// Order of writing is important in compact format
+    ordered_columns_list.sort([this](const auto & lhs, const auto & rhs)
+        { return *getColumnPosition(lhs.name) < *getColumnPosition(rhs.name); });
+
 
     return std::make_unique<MergeTreeDataPartWriterCompact>(
         shared_from_this(), ordered_columns_list, metadata_snapshot,
