8deeed86a78fb4de30073dec8103c9133473e4e8
Smita Kulkarni <Smita.Kulkarni@clickhouse.com>
2024-01-15 15:04:57 +0100

    Fix adding _block_number column to the beginning

--- src/Storages/MergeTree/MergeTreeDataPartCompact.cpp
+++ src/Storages/MergeTree/MergeTreeDataPartCompact.cpp
@@ -62,15 +62,15 @@ IMergeTreeDataPart::MergeTreeWriterPtr MergeTreeDataPartCompact::getWriter(
     std::copy_if(columns_list.begin(), columns_list.end(), std::back_inserter(ordered_columns_list),
         [this](const auto & column) { return getColumnPosition(column.name) != std::nullopt; });
 
-    /// _block_number column is not added by user, but is persisted in a part after merge
-    /// If _block_number is not present in the parts to be merged, then it won't have a position
-    /// So check if its not present and add it at the end
-    if (columns_list.contains(BlockNumberColumn::name) && !ordered_columns_list.contains(BlockNumberColumn::name))
-        ordered_columns_list.emplace_back(NameAndTypePair{BlockNumberColumn::name, BlockNumberColumn::type});
-
     /// Order of writing is important in compact format
     ordered_columns_list.sort([this](const auto & lhs, const auto & rhs)
         { return *getColumnPosition(lhs.name) < *getColumnPosition(rhs.name); });
+
+    /// _block_number column is not added by user, but is persisted in a part after merge
+    /// If _block_number is not present in the parts to be merged, then it won't have a position
+    /// So check if its not present and add it at the beginning
+    if (columns_list.contains(BlockNumberColumn::name) && !ordered_columns_list.contains(BlockNumberColumn::name))
+        ordered_columns_list.insert(ordered_columns_list.begin(), NameAndTypePair{BlockNumberColumn::name, BlockNumberColumn::type});
 
 
     return std::make_unique<MergeTreeDataPartWriterCompact>(
