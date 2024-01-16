a9e12d7498040eb0dde57dc312f47bf77d49b94e
Yakov Olkhovskiy <yakov@clickhouse.com>
2024-01-16 23:40:25 +0000

    fix test

--- tests/queries/0_stateless/02967_http_compressed.sh
+++ tests/queries/0_stateless/02967_http_compressed.sh
@@ -8,5 +8,4 @@ curl -sS -d 'SELECT 1,2,3,42' -H 'Accept-Encoding: gzip' -X POST "${CLICKHOUSE_U
 curl -sS -d 'SELECT 1,2,3,42' -H 'Accept-Encoding: br' -X POST "${CLICKHOUSE_URL}&compress=1&enable_http_compression=1" | brotli -d
 curl -sS -d 'SELECT 1,2,3,42' -H 'Accept-Encoding: xz' -X POST "${CLICKHOUSE_URL}&compress=1&enable_http_compression=1" | xz -d
 curl -sS -d 'SELECT 1,2,3,42' -H 'Accept-Encoding: zstd' -X POST "${CLICKHOUSE_URL}&compress=1&enable_http_compression=1" | zstd -d
-curl -sS -d 'SELECT 1,2,3,42' -H 'Accept-Encoding: lz4' -X POST "${CLICKHOUSE_URL}&compress=1&enable_http_compression=1" | lz4 -dc
 curl -sS -d 'SELECT 1,2,3,42' -H 'Accept-Encoding: bz2' -X POST "${CLICKHOUSE_URL}&compress=1&enable_http_compression=1" | bzip2 -d
