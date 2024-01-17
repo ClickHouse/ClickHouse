7706840dd96d04529833fc4290f98cb8c77e5e80
Yakov Olkhovskiy <yakov@clickhouse.com>
2024-01-17 06:33:41 +0000

    fix test

--- tests/queries/0_stateless/02967_http_compressed.sh
+++ tests/queries/0_stateless/02967_http_compressed.sh
@@ -5,6 +5,4 @@ CURDIR=$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)
 . "$CURDIR"/../shell_config.sh
 
 curl -sS -d 'SELECT 1,2,3,42' -H 'Accept-Encoding: gzip' -X POST "${CLICKHOUSE_URL}&compress=1&enable_http_compression=1" | gzip -d | hexdump
-curl -sS -d 'SELECT 1,2,3,42' -H 'Accept-Encoding: br' -X POST "${CLICKHOUSE_URL}&compress=1&enable_http_compression=1" | brotli -d | hexdump
-curl -sS -d 'SELECT 1,2,3,42' -H 'Accept-Encoding: xz' -X POST "${CLICKHOUSE_URL}&compress=1&enable_http_compression=1" | xz -d | hexdump
 curl -sS -d 'SELECT 1,2,3,42' -H 'Accept-Encoding: zstd' -X POST "${CLICKHOUSE_URL}&compress=1&enable_http_compression=1" | zstd -d | hexdump
