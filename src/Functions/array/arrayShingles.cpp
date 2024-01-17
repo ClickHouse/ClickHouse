84ab20ff7798ed4df5fb915f2b4ef730527a6873
Robert Schulze <robert@clickhouse.com>
2024-01-17 12:01:52 +0000

    Some fixups

--- src/Functions/array/arrayShingles.cpp
+++ src/Functions/array/arrayShingles.cpp
@@ -1,6 +1,4 @@
-#include <Columns/ColumnTuple.h>
 #include <Columns/ColumnArray.h>
-#include <DataTypes/DataTypeTuple.h>
 #include <DataTypes/DataTypeArray.h>
 #include <Functions/FunctionFactory.h>
 #include <Functions/FunctionHelpers.h>
@@ -12,87 +10,76 @@ namespace DB
 
 namespace ErrorCodes
 {
-extern const int ILLEGAL_TYPE_OF_ARGUMENT;
-extern const int ILLEGAL_COLUMN;
+    extern const int BAD_ARGUMENTS;
+    extern const int ILLEGAL_COLUMN;
 }
 
 class FunctionArrayShingles : public IFunction
 {
 public:
-    static constexpr auto name = "shingles";
+    static constexpr auto name = "arrayShingles";
+
     static FunctionPtr create(ContextPtr) { return std::make_shared<FunctionArrayShingles>(); }
-
-    String getName() const override
-    {
-        return name;
-    }
-
-    bool isVariadic() const override { return false; }
+    String getName() const override { return name; }
     size_t getNumberOfArguments() const override { return 2; }
     bool useDefaultImplementationForConstants() const override { return true; }
-
     bool isSuitableForShortCircuitArgumentsExecution(const DataTypesWithConstInfo & /*arguments*/) const override { return true; }
 
     DataTypePtr getReturnTypeImpl(const ColumnsWithTypeAndName & arguments) const override
     {
+            FunctionArgumentDescriptors args{
+                {"array", &isArray<IDataType>, nullptr, "Array"},
+                {"length", &isInteger<IDataType>, nullptr, "Integer"}
+            };
+            validateFunctionArgumentTypes(*this, arguments, args);
+
         const DataTypeArray * array_type = checkAndGetDataType<DataTypeArray>(arguments[0].type.get());
-        if (!array_type)
-            throw Exception(ErrorCodes::ILLEGAL_TYPE_OF_ARGUMENT, "Argument {} of function {} must be array. Found {} instead.",
-                            toString(1), getName(), arguments[0].type->getName());
-
-        if (!isInteger(arguments[1].type))
-            throw Exception(ErrorCodes::ILLEGAL_TYPE_OF_ARGUMENT, "Argument {} of function {} must be positive integer. Found {} instead.",
-                            toString(2), getName(), arguments[1].type->getName());
-
         return std::make_shared<DataTypeArray>(std::make_shared<DataTypeArray>(array_type->getNestedType()));
     }
 
     ColumnPtr executeImpl(const ColumnsWithTypeAndName & arguments, const DataTypePtr & /*result_type*/, size_t input_rows_count) const override
     {
-        const ColumnArray * column_array = checkAndGetColumn<ColumnArray>(arguments[0].column.get());
-        if (!column_array)
-            throw Exception(ErrorCodes::ILLEGAL_COLUMN, "Expected array column.");
-        const ColumnPtr & length_column = arguments[1].column;
+        const ColumnArray * col_array = checkAndGetColumn<ColumnArray>(arguments[0].column.get());
+        if (!col_array)
+            throw Exception(ErrorCodes::ILLEGAL_COLUMN, "Expected array column for function {}", getName());
 
-        const IColumn::Offsets & src_offsets = column_array->getOffsets();
-        const auto & src_values = column_array->getData();
-        auto res_values_column = src_values.cloneEmpty();
+        const ColumnPtr & col_length = arguments[1].column;
 
-        auto column_offsets_2 = ColumnArray::ColumnOffsets::create();
-        auto column_offsets_1 = ColumnArray::ColumnOffsets::create();
-        IColumn::Offsets & out_offsets_2 = column_offsets_2->getData();
-        IColumn::Offsets & out_offsets_1 = column_offsets_1->getData();
+        const auto & arr_offsets = col_array->getOffsets();
+        const auto & arr_values = col_array->getData();
+
+        auto col_res_data = arr_values.cloneEmpty();
+        auto col_res_inner_offsets = ColumnArray::ColumnOffsets::create();
+        auto col_res_outer_offsets = ColumnArray::ColumnOffsets::create();
+        IColumn::Offsets & out_offsets_2 = col_res_inner_offsets->getData();
+        IColumn::Offsets & out_offsets_1 = col_res_outer_offsets->getData();
 
         size_t pos1 = 0, pos2 = 0;
-        for (size_t i = 0; i < input_rows_count; ++i)
+        for (size_t row = 0; row < input_rows_count; ++row)
         {
-            auto subarray_length = length_column->getInt(i);
-            auto row_size = src_offsets[i] - src_offsets[i - 1];
-            if (subarray_length > 0 && static_cast<unsigned>(subarray_length) <= row_size)
-            {
-                for (size_t j = 0; j < row_size - subarray_length + 1; ++j)
-                {
-                    res_values_column->insertRangeFrom(src_values, src_offsets[i - 1] + j, subarray_length);
-                    pos1 += subarray_length;
-                    out_offsets_2.push_back(pos1);
-                }
-                pos2 += row_size - subarray_length + 1;
-            }
-            else
-            {
-                throw Exception(ErrorCodes::ILLEGAL_TYPE_OF_ARGUMENT, "Argument {} of function {} must be a positive integer which is less than or equal to array length.",
-                                toString(2), getName());
-            }
+            const Int64 shingle_length = col_length->getInt(row);
+            if (shingle_length < 1)
+                throw Exception(ErrorCodes::BAD_ARGUMENTS, "Shingle argument of function {} must be a positive integer.", getName());
 
+            const size_t array_length = arr_offsets[row] - arr_offsets[row - 1];
+            if (static_cast<size_t>(shingle_length) > array_length)
+                throw Exception(ErrorCodes::BAD_ARGUMENTS, "Shingle argument of function {} must less or equal than the array length.", getName());
+
+            for (size_t i = 0; i < array_length - shingle_length + 1; ++i)
+            {
+                col_res_data->insertRangeFrom(arr_values, arr_offsets[row - 1] + i, shingle_length);
+                pos1 += shingle_length;
+                out_offsets_2.push_back(pos1);
+            }
+            pos2 += array_length - shingle_length + 1;
             out_offsets_1.push_back(pos2);
         }
 
         return ColumnArray::create(
             ColumnArray::create(
-                std::move(res_values_column),
-                std::move(column_offsets_2)
-                    ),
-            std::move(column_offsets_1)
+                std::move(col_res_data),
+                std::move(col_res_inner_offsets)),
+            std::move(col_res_outer_offsets)
         );
     }
 };
@@ -102,14 +89,10 @@ REGISTER_FUNCTION(ArrayShingles)
     factory.registerFunction<FunctionArrayShingles>(
         FunctionDocumentation{
             .description = R"(
-Returns an array of sub-arrays from the original array.
-Each sub-array contains a sequence of elements from the original array. The length of these sequences is specified by the second argument of the function.
-[example:simple_int]
-[example:simple_string]
+Generates an array of "shingles", i.e. consecutive sub-arrays with specified length of the input array.
 )",
             .examples{
-                {"simple_int", "SELECT shingles([1, 2, 3, 4, 5], 3)", "[[1,2,3],[2,3,4],[3,4,5]]"},
-                {"simple_string", "SELECT shingles(['a', 'b', 'c', 'd', 'e'], 4)", "[['a','b','c','d'],['b','c','d','e']]"}
+                {"example 1", "SELECT arrayShingles( [1,2,3,4,5], 3)", "[[1,2,3],[2,3,4],[3,4,5]]"}
             },
             .categories = {"Array"},
         });
