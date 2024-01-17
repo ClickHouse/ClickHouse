37d059bd73db4fcf4a406f2e9a1695ac16493148
MikhailBurdukov <burdukvmikhail@gmail.com>
2024-01-17 11:36:53 +0000

    Disable test in fasttest

--- tests/queries/0_stateless/02966_s3_access_key_id_restriction.sql
+++ tests/queries/0_stateless/02966_s3_access_key_id_restriction.sql
@@ -1,3 +1,5 @@
+-- Tags: no-fasttest
+
 select * from s3('http://localhost:11111/test/a.tsv', '\ninjection\n', 'admin'); -- { serverError 36 }
 select * from deltaLake('http://localhost:11111/test/a.tsv', '\ninjection\n', 'admin'); -- { serverError 36 }
 select * from hudi('http://localhost:11111/test/a.tsv', '\ninjection\n', 'admin'); -- { serverError 36 }
