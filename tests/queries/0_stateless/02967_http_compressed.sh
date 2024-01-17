5da897ac2097ada781ced39beffa6fd72ef4dd7c
Yakov Olkhovskiy <yakov@clickhouse.com>
2024-01-17 00:19:59 +0000

    fix test

--- tests/queries/0_stateless/02967_http_compressed.sh
+++ tests/queries/0_stateless/02967_http_compressed.sh
@@ -4,8 +4,8 @@ CURDIR=$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)
 # shellcheck source=../shell_config.sh
 . "$CURDIR"/../shell_config.sh
 
-curl -sS -d 'SELECT 1,2,3,42' -H 'Accept-Encoding: gzip' -X POST "${CLICKHOUSE_URL}&compress=1&enable_http_compression=1" | gzip -d
-curl -sS -d 'SELECT 1,2,3,42' -H 'Accept-Encoding: br' -X POST "${CLICKHOUSE_URL}&compress=1&enable_http_compression=1" | brotli -d
-curl -sS -d 'SELECT 1,2,3,42' -H 'Accept-Encoding: xz' -X POST "${CLICKHOUSE_URL}&compress=1&enable_http_compression=1" | xz -d
-curl -sS -d 'SELECT 1,2,3,42' -H 'Accept-Encoding: zstd' -X POST "${CLICKHOUSE_URL}&compress=1&enable_http_compression=1" | zstd -d
-curl -sS -d 'SELECT 1,2,3,42' -H 'Accept-Encoding: bz2' -X POST "${CLICKHOUSE_URL}&compress=1&enable_http_compression=1" | bzip2 -d
+curl -sS -d 'SELECT 1,2,3,42' -H 'Accept-Encoding: gzip' -X POST "${CLICKHOUSE_URL}&compress=1&enable_http_compression=1" | gzip -d | hexdump
+curl -sS -d 'SELECT 1,2,3,42' -H 'Accept-Encoding: br' -X POST "${CLICKHOUSE_URL}&compress=1&enable_http_compression=1" | brotli -d | hexdump
+curl -sS -d 'SELECT 1,2,3,42' -H 'Accept-Encoding: xz' -X POST "${CLICKHOUSE_URL}&compress=1&enable_http_compression=1" | xz -d | hexdump
+curl -sS -d 'SELECT 1,2,3,42' -H 'Accept-Encoding: zstd' -X POST "${CLICKHOUSE_URL}&compress=1&enable_http_compression=1" | zstd -d | hexdump
+curl -sS -d 'SELECT 1,2,3,42' -H 'Accept-Encoding: bz2' -X POST "${CLICKHOUSE_URL}&compress=1&enable_http_compression=1" | bzip2 -d | hexdump
