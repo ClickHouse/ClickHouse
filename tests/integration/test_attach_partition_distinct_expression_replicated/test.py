13d3f52d05b4ea8a9c4845698077b83ae9c9ded8
Arthur Passos <arthur.ti@outlook.com>
2024-01-18 15:26:27 -0300

    add one more integ test

--- tests/integration/test_attach_partition_distinct_expression_replicated/test.py
+++ tests/integration/test_attach_partition_distinct_expression_replicated/test.py
@@ -23,6 +23,11 @@ def start_cluster():
         cluster.shutdown()
 
 
+def cleanup(nodes):
+    for node in nodes:
+        node.query("DROP TABLE IF EXISTS source SYNC")
+        node.query("DROP TABLE IF EXISTS destination SYNC")
+
 def create_table(node, table_name, replicated):
     replica = node.name
     engine = (
@@ -35,7 +40,7 @@ def create_table(node, table_name, replicated):
     )
     node.query_with_retry(
         """
-        CREATE TABLE IF NOT EXISTS {table_name}(timestamp DateTime)
+        CREATE TABLE {table_name}(timestamp DateTime)
         ENGINE = {engine}
         ORDER BY tuple() PARTITION BY {partition_expression}
         SETTINGS cleanup_delay_period=1, cleanup_delay_period_random_add=1, max_cleanup_delay_period=1;
@@ -68,6 +73,8 @@ def test_both_replicated(start_cluster):
         replica2.query(f"SELECT * FROM destination"),
     )
 
+    cleanup([replica1, replica2])
+
 
 def test_only_destination_replicated(start_cluster):
     create_table(replica1, "source", False)
@@ -88,3 +95,57 @@ def test_only_destination_replicated(start_cluster):
         f"SELECT * FROM destination",
         replica2.query(f"SELECT * FROM destination"),
     )
+
+    cleanup([replica1, replica2])
+
+def test_both_replicated_partitioned_to_unpartitioned(start_cluster):
+    def create_tables(nodes):
+        for node in nodes:
+            source_engine = f"ReplicatedMergeTree('/clickhouse/tables/1/source', '{node.name}')"
+            node.query(
+                """
+                CREATE TABLE source(timestamp DateTime)
+                ENGINE = {engine}
+                ORDER BY tuple() PARTITION BY toYYYYMMDD(timestamp)
+                SETTINGS cleanup_delay_period=1, cleanup_delay_period_random_add=1, max_cleanup_delay_period=1;
+                """.format(
+                    engine=source_engine,
+                )
+            )
+
+            destination_engine = f"ReplicatedMergeTree('/clickhouse/tables/1/destination', '{node.name}')"
+            node.query(
+                """
+                CREATE TABLE destination(timestamp DateTime)
+                ENGINE = {engine}
+                ORDER BY tuple() PARTITION BY tuple()
+                SETTINGS cleanup_delay_period=1, cleanup_delay_period_random_add=1, max_cleanup_delay_period=1;
+                """.format(
+                    engine=destination_engine,
+                )
+            )
+
+    create_tables([replica1, replica2])
+
+    replica1.query("INSERT INTO source VALUES ('2010-03-02 02:01:01')")
+    replica1.query("INSERT INTO source VALUES ('2010-03-03 02:01:01')")
+    replica1.query("SYSTEM SYNC REPLICA source")
+    replica1.query("SYSTEM SYNC REPLICA destination")
+
+    replica1.query(
+        f"ALTER TABLE destination ATTACH PARTITION ID '20100302' FROM source"
+    )
+    replica1.query(
+        f"ALTER TABLE destination ATTACH PARTITION ID '20100303' FROM source"
+    )
+
+    assert_eq_with_retry(
+        replica1, f"SELECT * FROM destination", "2010-03-02 02:01:01\n2010-03-03 02:01:01\n"
+    )
+    assert_eq_with_retry(
+        replica1,
+        f"SELECT * FROM destination",
+        replica2.query(f"SELECT * FROM destination"),
+    )
+
+    cleanup([replica1, replica2])
