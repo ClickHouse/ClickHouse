938551b4eac27a309f1a774445ae802919b6cb59
Arthur Passos <arthur.ti@outlook.com>
2024-01-19 11:44:34 -0300

    add one more integ test

--- tests/integration/test_attach_partition_distinct_expression_replicated/test.py
+++ tests/integration/test_attach_partition_distinct_expression_replicated/test.py
@@ -155,3 +155,60 @@ def test_both_replicated_partitioned_to_unpartitioned(start_cluster):
     )
 
     cleanup([replica1, replica2])
+
+
+def test_both_replicated_different_exp_same_id(start_cluster):
+    def create_tables(nodes):
+        for node in nodes:
+            source_engine = (
+                f"ReplicatedMergeTree('/clickhouse/tables/1/source', '{node.name}')"
+            )
+            node.query(
+                """
+                CREATE TABLE source(a UInt16,b UInt16,c UInt16,extra UInt64,Path String,Time DateTime,Value Float64,Timestamp Int64,sign Int8)
+                ENGINE = {engine}
+                ORDER BY tuple() PARTITION BY a % 3
+                SETTINGS cleanup_delay_period=1, cleanup_delay_period_random_add=1, max_cleanup_delay_period=1;
+                """.format(
+                    engine=source_engine,
+                )
+            )
+
+            destination_engine = f"ReplicatedMergeTree('/clickhouse/tables/1/destination', '{node.name}')"
+            node.query(
+                """
+                CREATE TABLE destination(a UInt16,b UInt16,c UInt16,extra UInt64,Path String,Time DateTime,Value Float64,Timestamp Int64,sign Int8)
+                ENGINE = {engine}
+                ORDER BY tuple() PARTITION BY a
+                SETTINGS cleanup_delay_period=1, cleanup_delay_period_random_add=1, max_cleanup_delay_period=1;
+                """.format(
+                    engine=destination_engine,
+                )
+            )
+
+    create_tables([replica1, replica2])
+
+    replica1.query("INSERT INTO source (a, b, c, extra, sign) VALUES (1, 5, 9, 1000, 1)")
+    replica1.query("INSERT INTO source (a, b, c, extra, sign) VALUES (2, 6, 10, 1000, 1)")
+    replica1.query("SYSTEM SYNC REPLICA source")
+    replica1.query("SYSTEM SYNC REPLICA destination")
+
+    replica1.query(
+        f"ALTER TABLE destination ATTACH PARTITION 1 FROM source"
+    )
+    replica1.query(
+        f"ALTER TABLE destination ATTACH PARTITION 2 FROM source"
+    )
+
+    assert_eq_with_retry(
+        replica1,
+        f"SELECT * FROM destination",
+        "1\t5\t9\t1000\t\t1970-01-01 00:00:00\t0\t0\t1\n2\t6\t10\t1000\t\t1970-01-01 00:00:00\t0\t0\t1\n",
+    )
+    assert_eq_with_retry(
+        replica1,
+        f"SELECT * FROM destination",
+        replica2.query(f"SELECT * FROM destination"),
+    )
+
+    cleanup([replica1, replica2])
