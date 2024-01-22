c0eeeb26a97543e1147b848e2534acb36d4e90d2
Amos Bird <amosbird@gmail.com>
2024-01-23 00:27:01 +0800

    Disable sparse column in test

--- tests/queries/0_stateless/03000_virtual_columns_in_prewhere.sql
+++ tests/queries/0_stateless/03000_virtual_columns_in_prewhere.sql
@@ -1,6 +1,6 @@
 drop table if exists x;
 
-create table x (i int, j int, k int) engine MergeTree order by tuple() settings index_granularity=8192, min_bytes_for_wide_part=0, min_rows_for_wide_part=0;
+create table x (i int, j int, k int) engine MergeTree order by tuple() settings index_granularity=8192, index_granularity_bytes = '10Mi',  min_bytes_for_wide_part=0, min_rows_for_wide_part=0, ratio_of_defaults_for_sparse_serialization=1;
 
 insert into x select number, number * 2, number * 3 from numbers(100000);
 
