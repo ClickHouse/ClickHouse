6f7c808c1e7b95aa7f7ce3bf975b259dc0cbe1e8
taiyang-li <654010905@qq.com>
2024-01-23 10:23:03 +0800

    fix uts

--- tests/queries/0_stateless/02967_parallel_bzip2_decompress.sql
+++ tests/queries/0_stateless/02967_parallel_bzip2_decompress.sql
@@ -1,10 +1,11 @@
 -- Tags: no-parallel, no-fasttest
 set allow_parallel_decompress = true;
 set max_download_buffer_size = 1048576;
+set engine_file_truncate_on_insert = 1;
 
 -- clickhouse-client should not use parallel decompression, because parallel decompression depends on io thread pool,
 -- which is not initialized in clickhouse-client.
-SELECT number, number::String FROM numbers(1000000) INTO OUTFILE 'data_02967_1.csv.bz2' COMPRESSION 'bz2';
+SELECT number, number::String FROM numbers(1000000) INTO OUTFILE 'data_02967_1.csv.bz2' TRUNCATE COMPRESSION 'bz2';
 CREATE TABLE test_02967 (x UInt64, y String) ENGINE = Memory;
 INSERT INTO test_02967 (x, y) FROM INFILE 'data_02967_1.csv.bz2' COMPRESSION 'bz2';
 DROP TABLE IF EXISTS test_02967;
