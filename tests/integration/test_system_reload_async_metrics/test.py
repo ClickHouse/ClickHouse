e72b44a0bfabfda81ad8a6718babc265cf3db26f
Robert Schulze <robert@clickhouse.com>
2024-01-23 05:58:24 +0000

    Fix style, pt. II

--- tests/integration/test_system_reload_async_metrics/test.py
+++ tests/integration/test_system_reload_async_metrics/test.py
@@ -36,9 +36,10 @@ def test_query_cache_size_is_runtime_configurable(start_cluster):
         "SELECT value FROM system.asynchronous_metrics WHERE metric = 'NumberOfTables'"
     )
 
+    # create table and test that the table creation is reflected in the asynchronous metrics
     node.query(
         "CREATE TABLE tab (col UInt64) ENGINE MergeTree ORDER BY tuple()"
-    ) # do anything dumb
+    )
 
     node.query("SYSTEM RELOAD ASYNCHRONOUS METRICS")
 
