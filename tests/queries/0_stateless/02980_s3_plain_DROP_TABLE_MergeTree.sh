d99fb4ce5be1086c0b90d20c85bd7f98414b34a9
Azat Khuzhin <a3at.mail@gmail.com>
2024-01-25 19:24:55 +0100

    tests: rewrite test for MergeTree engines in s3_plain using ATTACH
    Signed-off-by: Azat Khuzhin <a.khuzhin@semrush.com>

--- tests/queries/0_stateless/02980_s3_plain_DROP_TABLE_MergeTree.sh
+++ tests/queries/0_stateless/02980_s3_plain_DROP_TABLE_MergeTree.sh
@@ -6,11 +6,38 @@ CUR_DIR=$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)
 # shellcheck source=../shell_config.sh
 . "$CUR_DIR"/../shell_config.sh
 
+# config for clickhouse-disks (to check leftovers)
 config="${BASH_SOURCE[0]/.sh/.yml}"
+
+# only in Atomic ATTACH from s3_plain works
+new_database="ordinary_$CLICKHOUSE_DATABASE"
+$CLICKHOUSE_CLIENT --allow_deprecated_database_ordinary=1 -q "create database $new_database engine=Ordinary"
+CLICKHOUSE_CLIENT=${CLICKHOUSE_CLIENT/--database=$CLICKHOUSE_DATABASE/--database=$new_database}
+CLICKHOUSE_DATABASE="$new_database"
 
 $CLICKHOUSE_CLIENT -nm -q "
     drop table if exists data;
-    create table data (key Int) engine=MergeTree() order by key settings disk='s3_plain_disk';
+    create table data (key Int) engine=MergeTree() order by key settings disk='s3_plain_disk'; -- { serverError TABLE_IS_READ_ONLY }
+
+    create table data (key Int) engine=MergeTree() order by key;
+    insert into data values (1);
+    select 'data after INSERT', count() from data;
+"
+
+# suppress output
+$CLICKHOUSE_CLIENT -q "backup table data to S3('http://localhost:11111/test/backups/$CLICKHOUSE_DATABASE', 'test', 'testtest')" > /dev/null
+
+$CLICKHOUSE_CLIENT -nm -q "
+    drop table data;
+    attach table data (key Int) engine=MergeTree() order by key
+    settings
+        max_suspicious_broken_parts=0,
+        disk=disk(type=s3_plain,
+            endpoint='http://localhost:11111/test/backups/$CLICKHOUSE_DATABASE',
+            access_key_id='test',
+            secret_access_key='testtest');
+    select 'data after ATTACH', count() from data;
+
     insert into data values (1); -- { serverError TABLE_IS_READ_ONLY }
     optimize table data final; -- { serverError TABLE_IS_READ_ONLY }
 "
@@ -19,17 +46,12 @@ path=$($CLICKHOUSE_CLIENT -q "SELECT replace(data_paths[1], 's3_plain', '') FROM
 # trim / to fix "Unable to parse ExceptionName: XMinioInvalidObjectName Message: Object name contains unsupported characters."
 path=${path%/}
 
-echo "Files before DROP TABLE"
+echo "Files before DETACH TABLE"
 clickhouse-disks -C "$config" --disk s3_plain_disk list --recursive "${path:?}" | tail -n+2
 
 $CLICKHOUSE_CLIENT -q "detach table data"
 echo "Files after DETACH TABLE"
 clickhouse-disks -C "$config" --disk s3_plain_disk list --recursive "$path" | tail -n+2
 
-$CLICKHOUSE_CLIENT -nm -q "
-    attach table data;
-    drop table data;
-"
-# Check that there is no leftovers:
-echo "Files after DROP TABLE"
-clickhouse-disks -C "$config" --disk s3_plain_disk list --recursive "$path" | tail -n+2
+# metadata file is left
+$CLICKHOUSE_CLIENT --force_remove_data_recursively_on_drop=1 -q "drop database if exists $CLICKHOUSE_DATABASE"
--- tests/queries/0_stateless/02980_s3_plain_DROP_TABLE_ReplicatedMergeTree.sh
+++ tests/queries/0_stateless/02980_s3_plain_DROP_TABLE_ReplicatedMergeTree.sh
@@ -8,14 +8,41 @@ CUR_DIR=$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)
 
 config="${BASH_SOURCE[0]/.sh/.yml}"
 
+# only in Atomic ATTACH from s3_plain works
+new_database="ordinary_$CLICKHOUSE_DATABASE"
+$CLICKHOUSE_CLIENT --allow_deprecated_database_ordinary=1 -q "create database $new_database engine=Ordinary"
+CLICKHOUSE_CLIENT=${CLICKHOUSE_CLIENT/--database=$CLICKHOUSE_DATABASE/--database=$new_database}
+CLICKHOUSE_DATABASE="$new_database"
+
 $CLICKHOUSE_CLIENT -nm -q "
     drop table if exists data_read;
     drop table if exists data_write;
 
-    create table data_read (key Int) engine=ReplicatedMergeTree('/tables/{database}/data', 'read') order by key settings disk='s3_plain_disk';
     create table data_write (key Int) engine=ReplicatedMergeTree('/tables/{database}/data', 'write') order by key;
+    create table data_read (key Int) engine=ReplicatedMergeTree('/tables/{database}/data', 'read') order by key settings disk='s3_plain_disk'; -- { serverError TABLE_IS_READ_ONLY }
+    create table data_read (key Int) engine=ReplicatedMergeTree('/tables/{database}/data', 'read') order by key;
 
     insert into data_write values (1);
+    system sync replica data_read;
+    select 'data after INSERT', count() from data_read;
+"
+
+# suppress output
+$CLICKHOUSE_CLIENT -q "backup table data_read to S3('http://localhost:11111/test/backups/$CLICKHOUSE_DATABASE', 'test', 'testtest')" > /dev/null
+
+$CLICKHOUSE_CLIENT -nm -q "
+    drop table data_read;
+    attach table data_read (key Int) engine=ReplicatedMergeTree('/tables/{database}/data', 'read') order by key
+    settings
+        max_suspicious_broken_parts=0,
+        disk=disk(type=s3_plain,
+            endpoint='http://localhost:11111/test/backups/$CLICKHOUSE_DATABASE',
+            access_key_id='test',
+            secret_access_key='testtest');
+    select 'data after ATTACH', count() from data_read;
+
+    insert into data_read values (1); -- { serverError TABLE_IS_READ_ONLY }
+    optimize table data_read final; -- { serverError TABLE_IS_READ_ONLY }
     system sync replica data_read; -- { serverError TABLE_IS_READ_ONLY }
 "
 
@@ -23,7 +50,7 @@ path=$($CLICKHOUSE_CLIENT -q "SELECT replace(data_paths[1], 's3_plain', '') FROM
 # trim / to fix "Unable to parse ExceptionName: XMinioInvalidObjectName Message: Object name contains unsupported characters."
 path=${path%/}
 
-echo "Files before DROP TABLE"
+echo "Files before DETACH TABLE"
 clickhouse-disks -C "$config" --disk s3_plain_disk list --recursive "${path:?}" | tail -n+2
 
 $CLICKHOUSE_CLIENT -nm -q "
@@ -33,13 +60,5 @@ $CLICKHOUSE_CLIENT -nm -q "
 echo "Files after DETACH TABLE"
 clickhouse-disks -C "$config" --disk s3_plain_disk list --recursive "$path" | tail -n+2
 
-$CLICKHOUSE_CLIENT -nm -q "
-    attach table data_read;
-    attach table data_write;
-
-    drop table data_read;
-    drop table data_write;
-"
-# Check that there is no leftovers:
-echo "Files after DETACH TABLE"
-clickhouse-disks -C "$config" --disk s3_plain_disk list --recursive "$path" | tail -n+2
+# metadata file is left
+$CLICKHOUSE_CLIENT --force_remove_data_recursively_on_drop=1 -q "drop database if exists $CLICKHOUSE_DATABASE"
