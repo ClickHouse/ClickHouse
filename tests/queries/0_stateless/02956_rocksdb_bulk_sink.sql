ff9a9e51564b6272b78d76d2ca87e51502fa4ea1
Duc Canh Le <duccanh.le@ahrefs.com>
2024-01-26 13:25:56 +0000

    fix tests
    Signed-off-by: Duc Canh Le <duccanh.le@ahrefs.com>

--- tests/queries/0_stateless/02956_rocksdb_bulk_sink.sql
+++ tests/queries/0_stateless/02956_rocksdb_bulk_sink.sql
@@ -1,4 +1,6 @@
--- Tags: no-fasttest
+-- Tags: no-ordinary-database, no-fasttest
+-- Tag no-ordinary-database: Sometimes cannot lock file most likely due to concurrent or adjacent tests, but we don't care how it works in Ordinary database
+-- Tag no-fasttest: In fasttest, ENABLE_LIBRARIES=0, so rocksdb engine is not enabled by default
 CREATE TABLE rocksdb_worm (key UInt64, value UInt64) ENGINE = EmbeddedRocksDB() PRIMARY KEY key SETTINGS optimize_for_bulk_insert = 1;
 INSERT INTO rocksdb_worm SELECT number, number+1 FROM numbers_mt(1000) SETTINGS max_insert_threads = 2;
 SELECT count() FROM rocksdb_worm;
