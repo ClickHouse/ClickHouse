5023851b471c3fc4564db3bc8d01c351f6cb1fa2
avogar <avogar@clickhouse.com>
2024-01-27 22:09:40 +0000

    Add test

---
+++ tests/queries/0_stateless/02973_s3_compressed_file_in_error_message.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+# Tags: no-fasttest
+
+CURDIR=$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)
+# shellcheck source=../shell_config.sh
+. "$CURDIR"/../shell_config.sh
+
+$CLICKHOUSE_LOCAL -q "select * from s3('http://localhost:11111/test/a.tsv', TSV, 'x String', 'gzip')" 2>&1 | grep -c -F "a.tsv"
+
