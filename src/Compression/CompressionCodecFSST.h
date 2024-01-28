f5092fc35517dc39c60d89a6317d91db11269512
Ulad <anokhov04@gmail.com>
2024-01-28 21:58:58 +0000

    aboba

--- src/Compression/CompressionCodecFSST.h
+++ src/Compression/CompressionCodecFSST.h
@@ -1,12 +1,14 @@
 #include <Compression/ICompressionCodec.h>
 #include <Compression/CompressionInfo.h>
 #include <Compression/CompressionFactory.h>
+#include <double-conversion/utils.h>
 #include "base/defines.h"
 #include "base/types.h"
 #include "fsst12.h"
 
 #include <fsst.h>
 #include <cstring>
+#include <stdexcept>
 #include <vector>
 
 namespace DB
@@ -30,26 +32,31 @@ protected:
         FsstParams params = FsstParams(reinterpret_cast<const unsigned char*>(source), source_size);
         fsst_encoder_t *encoder = fsst_create(params.n, params.len_in.data(),
                     const_cast<unsigned char**>(params.str_in.data()), 1);
-        fsst_export(encoder, reinterpret_cast<unsigned char*>(dest));
+        auto header_size = fsst_export(encoder, reinterpret_cast<unsigned char*>(dest));
 
         lenOut.reserve(params.n);
         strOut.reserve(params.n);
-        fsst_compress(encoder, params.n, params.len_in.data(),
+        auto compressed_count = fsst_compress(encoder, params.n, params.len_in.data(),
             const_cast<unsigned char**>(params.str_in.data()),
-            source_size + FSST_MAXHEADER, reinterpret_cast<unsigned char*>(dest),
+            source_size + FSST_MAXHEADER - header_size, reinterpret_cast<unsigned char*>(dest + header_size),
             lenOut.data(), strOut.data());
+        if (compressed_count < params.n) {
+            throw std::runtime_error("FSST compression failed");
+        }
+        auto compressed_size = lenOut.back() + (strOut.back() - reinterpret_cast<const unsigned char*>(source));
         fsst_destroy(encoder);
-
-        // if (fsst_compress(encoder, 1, &srcLen[swap], &srcBuf[swap], FSST_MEMBUF*2, dstMem[swap]+FSST_MAXHEADER+3,
-        //                                &dstLen[swap], &dstBuf[swap]) < 1) return -1;
+        return static_cast<UInt32>(header_size) + static_cast<UInt32>(compressed_size);
     }
+
     void doDecompressData(const char * source, UInt32 source_size, char * dest, UInt32 uncompressed_size) const override {
         fsst_decoder_t decoder;
         unsigned char* mutable_source = const_cast<unsigned char*>(reinterpret_cast<const unsigned char*>(source));
 
         auto header_len = fsst_import(&decoder, mutable_source);
-        auto uncompressed_len = fsst_decompress(&decoder, source_size, mutable_source, uncompressed_size, reinterpret_cast<unsigned char*>(dest));
-        UNUSED(header_len, uncompressed_len);
+        auto uncompressed_len = fsst_decompress(&decoder, source_size,
+            mutable_source + header_len, uncompressed_size,
+            reinterpret_cast<unsigned char*>(dest));
+        ASSERT(uncompressed_len == uncompressed_size)
     }
 
     bool isCompression() const override { return true; }
