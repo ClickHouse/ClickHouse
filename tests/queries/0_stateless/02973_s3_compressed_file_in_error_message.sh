9d5af9d9cf922f5bf803e8b1f7d4ae520e9b3cce
Kruglov Pavel <48961922+Avogar@users.noreply.github.com>
2024-01-29 19:31:05 +0100

    Try fix

--- tests/queries/0_stateless/02973_s3_compressed_file_in_error_message.sh
+++ tests/queries/0_stateless/02973_s3_compressed_file_in_error_message.sh
@@ -5,5 +5,5 @@ CURDIR=$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)
 # shellcheck source=../shell_config.sh
 . "$CURDIR"/../shell_config.sh
 
-$CLICKHOUSE_LOCAL -q "select * from s3('http://localhost:11111/test/a.tsv', TSV, 'x String', 'gzip')" 2>&1 | grep -c -F "a.tsv"
+$CLICKHOUSE_CLIENT -q "select * from s3('http://localhost:11111/test/a.tsv', TSV, 'x String', 'gzip')" 2>&1 | grep -c -F "a.tsv"
 
