e2a66f8e6594fcb8c95f47a6f2670869c78a4a35
avogar <avogar@clickhouse.com>
2024-01-30 16:39:52 +0000

    Fix tests

--- tests/queries/0_stateless/02010_array_index_bad_cast.sql
+++ tests/queries/0_stateless/02010_array_index_bad_cast.sql
@@ -1,2 +1,3 @@
 -- This query throws exception about uncomparable data types (but at least it does not introduce bad cast in code).
+SET allow_suspicious_low_cardinality_types=1;
 SELECT has(materialize(CAST(['2021-07-14'] AS Array(LowCardinality(Nullable(DateTime))))), materialize('2021-07-14'::DateTime64(7))); -- { serverError 44 }
