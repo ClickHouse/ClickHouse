c891ed03c1ae5d85acc1b5f124433d634d23b278
Blargian <shaunstruwig.ss@gmail.com>
2024-01-30 14:19:10 +0100

    update test to use CLICKHOUSE_TEST_UNIQUE_NAME so parallel tests don't fail

--- tests/queries/0_stateless/00937_format_schema_rows_template.sh
+++ tests/queries/0_stateless/00937_format_schema_rows_template.sh
@@ -20,10 +20,11 @@ format_template_rows_between_delimiter = ';\n'";
 
 echo -e "\n"
 
-# Test that if both format_template_row_format setting and format_template_row are provided, error is thrown 
-echo -ne 'Question: ${question:Quoted}, Answer: ${answer:Quoted}, Number of Likes: ${likes:Raw}, Date: ${date:Raw}' > "$CURDIR"/00937_template_output_format_row.tmp
+# Test that if both format_template_row_format setting and format_template_row are provided, error is thrown
+row_format_file="$CURDIR"/"${CLICKHOUSE_TEST_UNIQUE_NAME}"_template_output_format_row.tmp
+echo -ne 'Question: ${question:Quoted}, Answer: ${answer:Quoted}, Number of Likes: ${likes:Raw}, Date: ${date:Raw}' > $row_format_file
 $CLICKHOUSE_CLIENT --multiline --multiquery --query "SELECT * FROM template GROUP BY question, answer, likes, date WITH TOTALS ORDER BY date LIMIT 3 FORMAT Template SETTINGS \
-format_template_row = '$CURDIR/00937_template_output_format_row.tmp', \
+format_template_row = '$row_format_file', \
 format_template_row_format = 'Question: \${question:Quoted}, Answer: \${answer:Quoted}, Number of Likes: \${likes:Raw}, Date: \${date:Raw}', \
 format_template_rows_between_delimiter = ';\n'; --{clientError 474}"
 
@@ -35,13 +36,14 @@ format_template_resultset_format = '===== Results ===== \n\${data}\n============
 format_template_rows_between_delimiter = ';\n'";
 
 # Test that if both format_template_result_format setting and format_template_resultset are provided, error is thrown
-echo -ne '===== Resultset ===== \n \${data} \n ===============' > "$CURDIR"/00937_template_output_format_resultset.tmp
+resultset_output_file="$CURDIR"/"$CLICKHOUSE_TEST_UNIQUE_NAME"_template_output_format_resultset.tmp
+echo -ne '===== Resultset ===== \n \${data} \n ===============' > $resultset_output_file
 $CLICKHOUSE_CLIENT --multiline --multiquery --query "SELECT * FROM template GROUP BY question, answer, likes, date WITH TOTALS ORDER BY date LIMIT 3 FORMAT Template SETTINGS \
-format_template_resultset = '$CURDIR/00937_template_output_format_resultset.tmp', \
+format_template_resultset = '$resultset_output_file', \
 format_template_resultset_format = '===== Resultset ===== \n \${data} \n ===============', \
 format_template_row_format = 'Question: \${question:Quoted}, Answer: \${answer:Quoted}, Number of Likes: \${likes:Raw}, Date: \${date:Raw}', \
 format_template_rows_between_delimiter = ';\n'; --{clientError 474}"
 
 $CLICKHOUSE_CLIENT --query="DROP TABLE template";
-rm "$CURDIR"/00937_template_output_format_row.tmp
-rm "$CURDIR"/00937_template_output_format_resultset.tmp
+rm $row_format_file
+rm $resultset_output_file
