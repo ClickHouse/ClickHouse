3b8b5f7403fba7b090c67838d9b34722160750ac
Duc Canh Le <duccanh.le@ahrefs.com>
2024-01-31 15:17:15 +0000

    fix typo
    Signed-off-by: Duc Canh Le <duccanh.le@ahrefs.com>

--- src/Storages/RocksDB/RocksDBSettings.h
+++ src/Storages/RocksDB/RocksDBSettings.h
@@ -24,7 +24,7 @@ struct Settings;
 
 #define ROCKSDB_SETTINGS(M, ALIAS) \
     M(Bool, optimize_for_bulk_insert, true, "Table is optimized for bulk insertions (insert pipeline will create SST files and import to rocksdb database instead of writing to memtables)", 0) \
-    M(UInt64, bulk_insert_block_size, DEFAULT_INSERT_BLOCK_SIZE, "Size of block for bulk insert, if it's smaller than query setting min_insert_block_size_rows then it will be overriden by min_insert_block_size_rows", 0) \
+    M(UInt64, bulk_insert_block_size, DEFAULT_INSERT_BLOCK_SIZE, "Size of block for bulk insert, if it's smaller than query setting min_insert_block_size_rows then it will be overridden by min_insert_block_size_rows", 0) \
 
 #define LIST_OF_ROCKSDB_SETTINGS(M, ALIAS) ROCKSDB_SETTINGS(M, ALIAS)
 
