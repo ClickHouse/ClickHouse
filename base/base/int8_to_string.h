333c45634fa8fd239d562b958146fc2178e1cd2a
Raúl Marín <git@rmr.ninja>
2024-02-01 16:53:31 +0100

    Infinite recursion

--- base/base/int8_to_string.h
+++ base/base/int8_to_string.h
@@ -6,25 +6,8 @@
 #include <fmt/format.h>
 
 template <>
-struct fmt::formatter<Int8>
+struct fmt::formatter<Int8> : fmt::formatter<int8_t>
 {
-    constexpr auto parse(format_parse_context & ctx)
-    {
-        const auto * it = ctx.begin();
-        const auto * end = ctx.end();
-
-        /// Only support {}.
-        if (it != end && *it != '}')
-            throw format_error("invalid format");
-
-        return it;
-    }
-
-    template <typename FormatContext>
-    auto format(const Int8 & value, FormatContext & ctx) -> decltype(ctx.out())
-    {
-        return format<FormatContext>(int8_t{value}, ctx);
-    }
 };
 
 
