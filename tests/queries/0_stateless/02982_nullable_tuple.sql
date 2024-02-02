a3d51cae68bbfbc9e5c75a25dfe11da6b1bc01b1
Dmitry Novik <mrnovikd@gmail.com>
2024-02-02 19:30:16 +0100

    Update test

--- tests/queries/0_stateless/02982_nullable_tuple.sql
+++ tests/queries/0_stateless/02982_nullable_tuple.sql
@@ -3,7 +3,8 @@ CREATE TABLE test
     x Nullable(Tuple(UInt64, UInt64))
 )
 ENGINE = MergeTree()
-ORDER BY x;
+ORDER BY x
+SETTINGS allow_nullable_key = 1;
 
 INSERT INTO test VALUES (NULL), (0, 0);
 
