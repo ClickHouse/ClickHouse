4ef32b52948bb96f0ef2bbdaf7a84a7c7e586e91
凌涛 <lingtaolf@gmail.com>
2024-02-04 11:44:21 +0800

    modify test

--- tests/queries/0_stateless/02982_enhance_progress_bar.sh
+++ tests/queries/0_stateless/02982_enhance_progress_bar.sh
@@ -1,14 +1,13 @@
 #!/usr/bin/env bash
-#Tags: no-parallel
 
 CUR_DIR=$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)
 # shellcheck source=../shell_config.sh
 . "$CUR_DIR"/../shell_config.sh
 
-${CLICKHOUSE_CLIENT} -mn --query="DROP DATABASE IF EXISTS 02982_enhance_progress_bar"
-${CLICKHOUSE_CLIENT} -mn --query="CREATE DATABASE IF NOT EXISTS 02982_enhance_progress_bar"
+${CLICKHOUSE_CLIENT} -mn --query="DROP TABLE IF EXISTS 02982_enhance_progress_bar"
+
 ${CLICKHOUSE_CLIENT} -mn --query="
-    CREATE TABLE 02982_enhance_progress_bar.test_bar
+    CREATE TABLE 02982_enhance_progress_bar
     (
         id UInt64
     )
@@ -16,7 +15,6 @@ ${CLICKHOUSE_CLIENT} -mn --query="
     ORDER BY id
     SETTINGS index_granularity = 8192
 "
-${CLICKHOUSE_CURL} "http://localhost:8123/?database=02982_enhance_progress_bar&wait_end_of_query=1&query=INSERT+INTO+test_bar+(id)+SELECT+*+FROM+generateRandom()+LIMIT+100000000"\
+${CLICKHOUSE_CURL} "${CLICKHOUSE_URL}&wait_end_of_query=1&query=INSERT+INTO+default.02982_enhance_progress_bar+(id)+SELECT+*+FROM+generateRandom()+LIMIT+1000"\
                       -v 2>&1 | grep 'X-ClickHouse-Summary' | sed 's/,\"elapsed_ns[^}]*//'
 
-${CLICKHOUSE_CLIENT} -mn --query="DROP DATABASE IF EXISTS 02982_enhance_progress_bar"
