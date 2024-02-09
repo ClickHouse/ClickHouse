aa004cabffdc5a7a40b0090f3540da0464c0924b
Alexey Milovidov <milovidov@clickhouse.com>
2024-02-09 02:00:05 +0100

    Fix tests

--- src/Parsers/IParser.h
+++ src/Parsers/IParser.h
@@ -82,7 +82,7 @@ public:
               * The frequency is arbitrary, but not too large, not too small,
               * and a power of two to simplify the division.
               */
-#ifdef USE_MUSL
+#if defined(USE_MUSL) || defined(SANITIZER)
             static constexpr uint32_t check_frequency = 128;
 #else
             static constexpr uint32_t check_frequency = 8192;
