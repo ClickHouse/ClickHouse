b769a881de429f3fcb03ae9ee1ed752f8968adbc
Nikita Mikhaylov <mikhaylovnikitka@gmail.com>
2024-02-10 14:50:53 +0100

    Update 02988_ordinary_database_warning.sql

--- tests/queries/0_stateless/02988_ordinary_database_warning.sql
+++ tests/queries/0_stateless/02988_ordinary_database_warning.sql
@@ -1,6 +1,7 @@
 DROP DATABASE IF EXISTS 02988_ordinary;
 
 SET allow_deprecated_database_ordinary = 1;
+SET send_logs_level='fatal';
 CREATE DATABASE 02988_ordinary ENGINE=Ordinary;
 
 SELECT 'Ok.' FROM system.warnings WHERE message ILIKE '%Ordinary%' and message ILIKE '%deprecated%';
