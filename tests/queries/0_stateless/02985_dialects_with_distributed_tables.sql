0458f470b948bded04c2bf729d923c4fae951d8e
Alexey Milovidov <milovidov@clickhouse.com>
2024-02-11 07:55:04 +0100

    Update 02985_dialects_with_distributed_tables.sql

--- tests/queries/0_stateless/02985_dialects_with_distributed_tables.sql
+++ tests/queries/0_stateless/02985_dialects_with_distributed_tables.sql
@@ -1,15 +1,13 @@
--- Tags: no-fasttest, distributed, replica, shard, zookeeper
+-- Tags: no-fasttest, distributed
 
 DROP TABLE IF EXISTS shared_test_table;
 DROP TABLE IF EXISTS distributed_test_table;
 
 CREATE TABLE shared_test_table 
-ON CLUSTER test_cluster_two_shard_three_replicas_localhost (id Int16) 
-ENGINE = ReplicatedMergeTree('/clickhouse/tables/test_02985_{database}/{shard}/shared_test_table', '{replica}') 
+ENGINE = MergeTree
 ORDER BY (id);
 
 CREATE TABLE distributed_test_table 
-ON CLUSTER test_cluster_two_shard_three_replicas_localhost (id Int16) 
 ENGINE = Distributed(test_cluster_two_shard_three_replicas_localhost, currentDatabase(), shared_test_table);
 
 INSERT INTO shared_test_table VALUES (123), (651), (446), (315), (234), (764);
@@ -25,3 +23,8 @@ SET dialect = 'prql';
 from distributed_test_table
 select {id}
 take 1..3
+
+SET dialect = 'clickhouse';
+
+DROP TABLE distributed_test_table;
+DROP TABLE shared_test_table;
