f5880d7201e0c939629d8115cf2cbfb148d0966c
robot-clickhouse <robot-clickhouse@users.noreply.github.com>
2024-02-12 20:55:20 +0000

    Automatic style fix

--- tests/integration/test_insert_exception_over_http/test.py
+++ tests/integration/test_insert_exception_over_http/test.py
@@ -16,14 +16,22 @@ def start_cluster():
 
 
 def test_insert_exception_over_http(start_cluster):
-    instance.query("DROP TABLE IF EXISTS tt SYNC");
-    instance.query("CREATE TABLE tt (KeyID UInt32) Engine = ReplicatedMergeTree('/test_insert_exception_over_http/tt', 'r1') ORDER BY (KeyID)");
-    instance.query("SYSTEM ENABLE FAILPOINT execute_query_calling_empty_set_result_func_on_exception");
+    instance.query("DROP TABLE IF EXISTS tt SYNC")
+    instance.query(
+        "CREATE TABLE tt (KeyID UInt32) Engine = ReplicatedMergeTree('/test_insert_exception_over_http/tt', 'r1') ORDER BY (KeyID)"
+    )
+    instance.query(
+        "SYSTEM ENABLE FAILPOINT execute_query_calling_empty_set_result_func_on_exception"
+    )
 
-    assert True == instance.http_query_and_get_error("insert into tt settings insert_keeper_max_retries=0, insert_keeper_fault_injection_probability=1.0, log_comment='02988_66a57d6f-d1cc-4693-8bf4-206848edab87' values (1), (2), (3), (4), (5)").startswith("500 Internal Server Error")
+    assert True == instance.http_query_and_get_error(
+        "insert into tt settings insert_keeper_max_retries=0, insert_keeper_fault_injection_probability=1.0, log_comment='02988_66a57d6f-d1cc-4693-8bf4-206848edab87' values (1), (2), (3), (4), (5)"
+    ).startswith("500 Internal Server Error")
 
     assert "0\n" == instance.query("select count() from tt")
     instance.query("SYSTEM FLUSH LOGS")
-    assert "2\n" ==  instance.query("select count() from system.query_log where log_comment ='02988_66a57d6f-d1cc-4693-8bf4-206848edab87' and current_database = currentDatabase() and event_date >= yesterday()")
+    assert "2\n" == instance.query(
+        "select count() from system.query_log where log_comment ='02988_66a57d6f-d1cc-4693-8bf4-206848edab87' and current_database = currentDatabase() and event_date >= yesterday()"
+    )
 
     instance.query("DROP TABLE tt SYNC")
