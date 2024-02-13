7f34af467e955a363debb3575b45f30cd60b64f3
Smita Kulkarni <Smita.Kulkarni@clickhouse.com>
2024-02-13 16:00:26 +0100

    Add azure_cache as storage policy to tests

--- tests/queries/0_stateless/02313_filesystem_cache_seeks.sh
+++ tests/queries/0_stateless/02313_filesystem_cache_seeks.sh
@@ -8,7 +8,7 @@ CUR_DIR=$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)
 . "$CUR_DIR"/../shell_config.sh
 
 
-for STORAGE_POLICY in 's3_cache' 'local_cache' 's3_cache_multi'; do
+for STORAGE_POLICY in 's3_cache' 'local_cache' 's3_cache_multi' 'azure_cache'; do
     echo "Using storage policy: $STORAGE_POLICY"
     $CLICKHOUSE_CLIENT --query "SYSTEM DROP FILESYSTEM CACHE"
 
