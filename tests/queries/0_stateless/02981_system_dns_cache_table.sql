e5e13f5df3565c697d77ef9cf97f34d4b9e2da8c
Kirill Nikiforov <allmazz@allmazz.me>
2024-02-15 01:04:01 +0300

    stateful test for system.dns_cache table

--- tests/queries/0_stateless/02981_system_dns_cache_table.sql
+++ tests/queries/0_stateless/02981_system_dns_cache_table.sql
@@ -1,1 +1,2 @@
-select hostname, ip_address, ip_family, (isNotNull(cached_at) and cached_at > '1970-01-01 00:00:00') from system.dns_cache where hostname = 'localhost'
+SELECT * FROM url('http://localhost:8123/ping', CSV, 'auto', headers());
+SELECT hostname, ip_address, ip_family, (isNotNull(cached_at) AND cached_at > '1970-01-01 00:00:00') FROM system.dns_cache WHERE hostname = 'localhost';
