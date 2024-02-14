c4fdb44ecc850aedb536bd6c7d3de232f48a0805
Bhavna Jindal <bhavna.jindal@ibm.com>
2024-02-14 07:04:29 -0800

    style-check fixes

--- src/Functions/seriesDecomposeSTL.cpp
+++ src/Functions/seriesDecomposeSTL.cpp
@@ -94,15 +94,15 @@ public:
                 || checkAndGetColumn<ColumnUInt32>(period_ptr.get())
                 || checkAndGetColumn<ColumnUInt64>(period_ptr.get()))
                 period = period_ptr->getUInt(i);
-            else if(checkAndGetColumn<ColumnFloat32>(period_ptr.get())
+            else if (checkAndGetColumn<ColumnFloat32>(period_ptr.get())
                 || checkAndGetColumn<ColumnFloat64>(period_ptr.get()))
-                {
-                    auto period_val =  period_ptr->getFloat64(i);
-                    if(isnan(period_val) || !std::isfinite(period_val) || period_val < 0.0)
-                        throw Exception(ErrorCodes::BAD_ARGUMENTS, "The second for function {} should be a positive number", getName());
+            {
+                auto period_val = period_ptr->getFloat64(i);
+                if (isnan(period_val) || !std::isfinite(period_val) || period_val < 0.0)
+                    throw Exception(ErrorCodes::BAD_ARGUMENTS, "The second for function {} should be a positive number", getName());
 
-                    period = static_cast<int>(std::round(period_val));
-                }
+                period = static_cast<int>(std::round(period_val));
+            }
             else
                 throw Exception(
                     ErrorCodes::ILLEGAL_COLUMN,
