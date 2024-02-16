cc134b0e5eea544772d806c72ef0fd7698057305
Yong Wang <yong.wang@ibm.com>
2024-02-16 12:16:27 -0800

    fix_kql_issue_found_by_wingfuzz: add test case for max_query_size

--- tests/queries/0_stateless/02366_kql_tabular.sql
+++ tests/queries/0_stateless/02366_kql_tabular.sql
@@ -86,3 +86,6 @@ hits | where CounterID == 62 and EventDate >= '2013-07-14' and EventDate <= '201
 
 print '-- Missing column in front of startsWith --';
 StormEvents | where startswith "W" | summarize Count=count() by State; -- { clientError SYNTAX_ERROR }
+
+SET max_query_size = 55;
+Customers | where Education contains 'degree' | order by LastName; -- { serverError 62 }
