7032e0c68f08d474c2e030edaf568cc91e87b681
Alexey Milovidov <milovidov@clickhouse.com>
2024-02-17 10:13:25 +0100

    Fix inconsistent formatting of INSERT queries

--- tests/queries/0_stateless/02994_inconsistent_formatting.sql
+++ tests/queries/0_stateless/02994_inconsistent_formatting.sql
@@ -1,6 +1,7 @@
 CREATE TEMPORARY TABLE table (x UInt8);
 INSERT INTO `table` FORMAT Values (1);
 INSERT INTO TABLE `table` FORMAT Values (2);
+INSERT INTO TABLE table FORMAT Values (3);
 SELECT * FROM table ORDER BY x;
 DROP TABLE table;
 
