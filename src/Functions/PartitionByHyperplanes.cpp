9f83f8b679a6bd81fa0c2a4e433975f6f07c2bc8
Roman Glinskikh <glinskikh.roman@yandex.ru>
2024-02-17 23:54:53 +0300

    init

--- src/Functions/PartitionByHyperplanes.cpp
+++ src/Functions/PartitionByHyperplanes.cpp
@@ -1,3 +1,4 @@
+#include <base/types.h>
 #include <Columns/ColumnNullable.h>
 #include <Columns/ColumnString.h>
 #include <Columns/ColumnVector.h>
@@ -9,6 +10,10 @@
 #include <Functions/IFunction.h>
 #include <Interpreters/Context.h>
 #include "Columns/ColumnArray.h"
+#include "Columns/ColumnFixedString.h"
+#include "Columns/ColumnsNumber.h"
+#include "Core/TypeId.h"
+#include "DataTypes/IDataType.h"
 #include "config.h"
 
 
@@ -39,13 +44,13 @@ namespace
         {
             FunctionArgumentDescriptors mandatory_args
             {
-                {"vec", &isArray<IDataType>, nullptr, "Array"},
-                {"normals", &isString<IDataType>, isColumnConst, "const Array"},
+                {"vec", &isFloat<IDataType>, nullptr, "Froat"},
+                {"normals", &isArray<IDataType>, isColumnConst, "const Array"},
             };
 
             FunctionArgumentDescriptors optional_args
             {
-                {"offsets", &isString<IDataType>, isColumnConst, "const Array"},
+                {"offsets", &isFloat<IDataType>, isColumnConst, "const Array"},
             };
 
             validateFunctionArgumentTypes(*this, arguments, mandatory_args, optional_args);
@@ -56,11 +61,30 @@ namespace
         ColumnPtr executeImpl(const ColumnsWithTypeAndName & arguments, const DataTypePtr &, size_t input_rows_count) const override
         {
             const ColumnPtr column = arguments[0].column;
-            const ColumnArray * vec = typeid_cast<const ColumnArray *>(arguments[0].column.get());
+            const ColumnFloat64 * vec = typeid_cast<const ColumnFloat64 *>(arguments[0].column.get());
             if (!vec)
-                throw Exception(ErrorCodes::ILLEGAL_COLUMN, "First argument of function {} must be array", getName());
+                throw Exception(ErrorCodes::ILLEGAL_COLUMN, "First argument of function {} must be Int64", getName());
 
-            
+            const ColumnArray * normals = typeid_cast<const ColumnArray *>(arguments[1].column.get());
+
+            const ColumnFloat64 * offsets = typeid_cast<const ColumnFloat64 *>(arguments[0].column.get());
+            if (!offsets)
+                throw Exception(ErrorCodes::ILLEGAL_COLUMN, "Third argument of function {} must be Int64", getName());
+
+            auto col_res = ColumnFixedString::create(input_rows_count);
+            auto& dst_chars = col_res->getChars();
+            dst_chars.resize(input_rows_count);
+            for (size_t i = 0; i < input_rows_count; ++i)
+            {
+                double res = 0.0;
+                auto normal = (*normals)[i].get<Array>();
+                for (size_t j = 0; j < input_rows_count; ++j)
+                {
+                    res += ((*vec)[i].get<Float64>() - (*offsets)[i].get<Float64>()) * normal[j].get<Float64>();
+                }
+                dst_chars[i] = res == 0;
+            }
+            return ColumnConst::create(std::move(col_res), input_rows_count);
         }
     };
 
