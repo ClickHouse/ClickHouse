731c484b3d03edb7e1d62f5d97913b5658cfe375
Alexey Milovidov <milovidov@clickhouse.com>
2024-02-17 01:46:41 +0100

    Revert "Implement system.dns_cache table (#59856)"
    This reverts commit b5ef034697188f7c2d61723800f718c55864193e.

--- tests/queries/0_stateless/02981_system_dns_cache_table.sql
+++
@@ -1,2 +0,0 @@
-SELECT * FROM url('http://localhost:8123/ping', CSV, 'auto', headers());
-SELECT hostname, ip_address, ip_family, (isNotNull(cached_at) AND cached_at > '1970-01-01 00:00:00') FROM system.dns_cache WHERE hostname = 'localhost' AND ip_family = 'IPv4';
