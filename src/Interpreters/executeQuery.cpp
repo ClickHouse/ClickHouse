41701ce39d677ab7af752e5442bccd9171e69fe5
Alexey Milovidov <milovidov@clickhouse.com>
2024-02-21 08:20:29 +0100

    Fix error

--- src/Interpreters/executeQuery.cpp
+++ src/Interpreters/executeQuery.cpp
@@ -729,7 +729,7 @@ static std::tuple<ASTPtr, BlockIO> executeQueryImpl(
             String formatted1 = ast->formatWithPossiblyHidingSensitiveData(0, true, true);
 
             /// The query can become more verbose after formatting, so:
-            size_t new_max_query_size = 1000 + 2 * max_query_size;
+            size_t new_max_query_size = max_query_size > 0 ? (1000 + 2 * max_query_size) : 0;
 
             ASTPtr ast2 = parseQuery(parser,
                 formatted1.data(),
