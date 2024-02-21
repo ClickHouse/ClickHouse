07c9deed41a835e37a0a3098141576a83fe935a8
Raúl Marín <git@rmr.ninja>
2024-02-21 18:40:00 +0100

    Make cloud sync required

--- tests/ci/ci_config.py
+++ tests/ci/ci_config.py
@@ -1139,6 +1139,7 @@ CI_CONFIG.validate()
 # checks required by Mergeable Check
 REQUIRED_CHECKS = [
     "PR Check",
+    "A Sync",  # Cloud sync
     JobNames.BUILD_CHECK,
     JobNames.BUILD_CHECK_SPECIAL,
     JobNames.DOCS_CHECK,
