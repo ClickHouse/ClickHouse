b71bdbae1b852ed7361ce09119f41035fc66caf1
Raúl Marín <git@rmr.ninja>
2024-02-21 15:33:37 +0100

    Treat 2+ in allow_experimental_parallel_reading_from_replicas as 2

--- tests/queries/0_stateless/02972_parallel_replicas_cte.sql
+++ tests/queries/0_stateless/02972_parallel_replicas_cte.sql
@@ -19,6 +19,11 @@ WITH filtered_groups AS (SELECT a FROM pr_1 WHERE a >= 10000)
 SELECT count() FROM pr_2 INNER JOIN filtered_groups ON pr_2.a = filtered_groups.a
 SETTINGS allow_experimental_analyzer = 0, allow_experimental_parallel_reading_from_replicas = 2, parallel_replicas_for_non_replicated_merge_tree = 1, cluster_for_parallel_replicas = 'test_cluster_one_shard_three_replicas_localhost', max_parallel_replicas = 3; -- { serverError SUPPORT_IS_DISABLED }
 
+-- Disabled for any value of allow_experimental_parallel_reading_from_replicas != 1, not just 2
+WITH filtered_groups AS (SELECT a FROM pr_1 WHERE a >= 10000)
+SELECT count() FROM pr_2 INNER JOIN filtered_groups ON pr_2.a = filtered_groups.a
+SETTINGS allow_experimental_analyzer = 0, allow_experimental_parallel_reading_from_replicas = 512, parallel_replicas_for_non_replicated_merge_tree = 1, cluster_for_parallel_replicas = 'test_cluster_one_shard_three_replicas_localhost', max_parallel_replicas = 3; -- { serverError SUPPORT_IS_DISABLED }
+
 -- Sanitizer
 SELECT count() FROM pr_2 JOIN numbers(10) as pr_1 ON pr_2.a = pr_1.number
 SETTINGS allow_experimental_parallel_reading_from_replicas = 1, parallel_replicas_for_non_replicated_merge_tree = 1, cluster_for_parallel_replicas = 'test_cluster_one_shard_three_replicas_localhost', max_parallel_replicas = 3;
