da8d8fee4714acd276641ed6a84de2536650b8ba
avogar <avogar@clickhouse.com>
2024-02-23 13:45:03 +0000

    Validate experimental and suspicious types inside nested types under a setting to reflect it in compatibility setting

--- src/Core/SettingsChangesHistory.h
+++ src/Core/SettingsChangesHistory.h
@@ -85,6 +85,7 @@ namespace SettingsChangesHistory
 static std::map<ClickHouseVersion, SettingsChangesHistory::SettingsChanges> settings_changes_history =
 {
     {"24.2", {
+              {"validate_experimental_and_suspicious_types_inside_nested_types", false, true, "Validate usage of experimental and suspicious types inside nested types"},
               {"output_format_values_escape_quote_with_quote", false, false, "If true escape ' with '', otherwise quoted with \\'"},
               {"input_format_try_infer_exponent_floats", true, false, "Don't infer floats in exponential notation by default"},
               {"async_insert_max_data_size", 1000000, 10485760, "The previous value appeared to be too small."},
