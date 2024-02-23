68239075b7074a704d9b541b18afab5d1bce6c01
zhongyuankai <54787696+zhongyuankai@users.noreply.github.com>
2024-02-23 18:33:24 +0800

    Query cache does not record the number of queries by default

--- src/Interpreters/executeQuery.cpp
+++ src/Interpreters/executeQuery.cpp
@@ -1146,8 +1146,7 @@ static std::tuple<ASTPtr, BlockIO> executeQueryImpl(
                                 std::chrono::system_clock::now() + std::chrono::seconds(settings.query_cache_ttl),
                                 settings.query_cache_compress_entries);
 
-                            const size_t num_query_runs = query_cache->recordQueryRun(key);
-                            if (num_query_runs <= settings.query_cache_min_query_runs)
+                            if (settings.query_cache_min_query_runs && query_cache->recordQueryRun(key) <= settings.query_cache_min_query_runs)
                             {
                                 LOG_TRACE(getLogger("QueryCache"),
                                         "Skipped insert because the query ran {} times but the minimum required number of query runs to cache the query result is {}",
