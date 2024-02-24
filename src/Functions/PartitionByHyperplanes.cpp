5390d570b0f2ddb95040c19b933e3ded1ba0c870
Roman Glinskikh <glinskikh.roman@yandex.ru>
2024-02-24 23:50:15 +0300

    fix input types and add description

--- src/Functions/PartitionByHyperplanes.cpp
+++ src/Functions/PartitionByHyperplanes.cpp
@@ -1,3 +1,4 @@
+#include <cstddef>
 #include <base/types.h>
 #include <Columns/ColumnNullable.h>
 #include <Columns/ColumnString.h>
@@ -9,7 +10,9 @@
 #include <Functions/FunctionHelpers.h>
 #include <Functions/IFunction.h>
 #include <Interpreters/Context.h>
+#include "Common/FunctionDocumentation.h"
 #include "Columns/ColumnArray.h"
+#include "Columns/ColumnConst.h"
 #include "Columns/ColumnFixedString.h"
 #include "Columns/ColumnsNumber.h"
 #include "Core/TypeId.h"
@@ -22,6 +25,7 @@ namespace DB
 namespace ErrorCodes
 {
     extern const int ILLEGAL_COLUMN;
+    extern const int SIZES_OF_ARRAYS_DONT_MATCH;
 }
 
 namespace
@@ -44,13 +48,13 @@ namespace
         {
             FunctionArgumentDescriptors mandatory_args
             {
-                {"vec", &isFloat<IDataType>, nullptr, "Froat"},
+                {"vectors", &isArray<IDataType>, nullptr, "Array"},
                 {"normals", &isArray<IDataType>, isColumnConst, "const Array"},
             };
 
             FunctionArgumentDescriptors optional_args
             {
-                {"offsets", &isFloat<IDataType>, isColumnConst, "const Array"},
+                {"offsets", &isArray<IDataType>, isColumnConst, "const Array"},
             };
 
             validateFunctionArgumentTypes(*this, arguments, mandatory_args, optional_args);
@@ -58,33 +62,67 @@ namespace
             return std::make_shared<DataTypeString>();
         }
 
-        ColumnPtr executeImpl(const ColumnsWithTypeAndName & arguments, const DataTypePtr &, size_t input_rows_count) const override
+        ColumnPtr executeImpl(const ColumnsWithTypeAndName & arguments, const DataTypePtr & result_type, size_t input_rows_count) const override
         {
-            const ColumnPtr column = arguments[0].column;
-            const ColumnFloat64 * vec = typeid_cast<const ColumnFloat64 *>(arguments[0].column.get());
-            if (!vec)
-                throw Exception(ErrorCodes::ILLEGAL_COLUMN, "First argument of function {} must be Int64", getName());
+            const ColumnArray * vectors = typeid_cast<const ColumnArray *>(arguments[0].column.get());
+            if (!vectors)
+                throw Exception(ErrorCodes::ILLEGAL_COLUMN, "First argument of function {} must be Array", getName());
+            const auto & nested_vectors_data = typeid_cast<const ColumnFloat64 &>(vectors->getData());
 
             const ColumnArray * normals = typeid_cast<const ColumnArray *>(arguments[1].column.get());
+            if (!normals)
+                throw Exception(ErrorCodes::ILLEGAL_COLUMN, "Second argument of function {} must be Array", getName());   
+            const auto & nested_normals_data = typeid_cast<const ColumnFloat64 &>(normals->getData()); 
 
-            const ColumnFloat64 * offsets = typeid_cast<const ColumnFloat64 *>(arguments[0].column.get());
-            if (!offsets)
-                throw Exception(ErrorCodes::ILLEGAL_COLUMN, "Third argument of function {} must be Int64", getName());
+            const size_t size = vectors->getOffsets().front();
 
-            auto col_res = ColumnFixedString::create(input_rows_count);
+            auto nested_offsets_data = [&] {
+                if (arguments.size() < 3)
+                    return ColumnConst::create(ColumnFloat64::create(1, 0.0), input_rows_count * size)->getDataColumnPtr();
+
+                const ColumnArray * offsets = typeid_cast<const ColumnArray *>(arguments[2].column.get());
+                if (!offsets)
+                    throw Exception(ErrorCodes::ILLEGAL_COLUMN, "Third argument of function {} must be Array", getName());
+                return offsets->getDataPtr();
+            }();
+
+            if (input_rows_count == 0)
+                return result_type->createColumn();
+
+            auto col_res = ColumnFixedString::create(size);
             auto& dst_chars = col_res->getChars();
-            dst_chars.resize(input_rows_count);
+            dst_chars.resize(input_rows_count * input_rows_count);
+
+            size_t current_offset = 0;
             for (size_t i = 0; i < input_rows_count; ++i)
             {
-                double res = 0.0;
-                auto normal = (*normals)[i].get<Array>();
+                if (vectors->getOffsets()[i] - current_offset != size)
+                    throw Exception(ErrorCodes::SIZES_OF_ARRAYS_DONT_MATCH, "All vectors must have equal size");
+
+                size_t current_offset_normals = 0;
                 for (size_t j = 0; j < input_rows_count; ++j)
                 {
-                    res += ((*vec)[i].get<Float64>() - (*offsets)[i].get<Float64>()) * normal[j].get<Float64>();
+                    if (normals->getOffsets()[j] - current_offset_normals != size)
+                        throw Exception(ErrorCodes::SIZES_OF_ARRAYS_DONT_MATCH, "All normals must have equal size");
+
+                    double res = 0.0;
+                    for (size_t k = 0; k < size; ++k)
+                    {
+                        const size_t vector_index = current_offset + k;
+                        const size_t normal_index = current_offset_normals + k;
+
+                        res += (nested_vectors_data.getFloat64(vector_index) - nested_offsets_data->getFloat64(vector_index))
+                                * nested_normals_data.getFloat64(normal_index);
+                    }
+                    dst_chars[current_offset + j] = (res == 0);
+
+                    current_offset_normals = normals->getOffsets()[j];
                 }
-                dst_chars[i] = res == 0;
+
+                current_offset = vectors->getOffsets()[i];
             }
-            return ColumnConst::create(std::move(col_res), input_rows_count);
+
+            return col_res;
         }
     };
 
@@ -92,7 +130,15 @@ namespace
 
 REGISTER_FUNCTION(PartitionByHyperplanes)
 {
-    factory.registerFunction<FunctionPartitionByHyperplanes>();
+    factory.registerFunction<FunctionPartitionByHyperplanes>(FunctionDocumentation{
+        .description=R"(
+Given a vector and a hyperplanes, represented as vectors of normals and optioal offsets.
+Returns a String with every bit corresponding to a subspace, relative to the corresponding hyperplane.
+All the vectors an normals should lie in space of the same dimension.
+        )",
+        .examples{{"partitionByHyperplanes", "SELECT(partitionByHyperplanes([1, 2, 3, 4, 5], [5, 4, 3, 2, -38]))", "\"1\""}},
+        .categories{"OtherFunctions"}
+    });
 }
 
 }
