e78691c07b8cec8070a7a3f350ceb7e51c22e098
Alexey Milovidov <milovidov@clickhouse.com>
2024-02-26 00:08:40 +0100

    Update tests

--- tests/queries/0_stateless/01710_projection_with_ast_rewrite_settings.sql
+++ tests/queries/0_stateless/01710_projection_with_ast_rewrite_settings.sql
@@ -22,7 +22,7 @@ DROP TABLE IF EXISTS legacy_column_name_of_tuple_literal;
 
 CREATE TABLE legacy_column_name_of_tuple_literal (`x` UInt32, `y` UInt64, PROJECTION p (SELECT sum(y in (1,2,3)))) ENGINE = MergeTree ORDER BY tuple();
 
-INSERT INTO legacy_column_name_of_tuple_literal SELECT number, number * 2 FROM numbers(8192 * 10) SETTINGS legacy_column_name_of_tuple_literal = true;
+INSERT INTO legacy_column_name_of_tuple_literal SELECT number, number * 2 FROM numbers(8192 * 10) SETTINGS legacy_column_name_of_literal = true;
 
 SELECT count() FROM legacy_column_name_of_tuple_literal;
 
