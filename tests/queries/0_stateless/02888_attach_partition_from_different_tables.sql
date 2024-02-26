f04debe6484fce2b8d8529a81d4d1bb81dc012fd
Arthur Passos <arthur.ti@outlook.com>
2024-02-26 11:10:22 -0300

    add test that validates attach partition fails if structure differs because of materialized column

--- tests/queries/0_stateless/02888_attach_partition_from_different_tables.sql
+++ tests/queries/0_stateless/02888_attach_partition_from_different_tables.sql
@@ -88,3 +88,25 @@ ALTER TABLE attach_partition_t6 ATTACH PARTITION tuple() FROM attach_partition_t
 
 SELECT * FROM attach_partition_t6 WHERE b = '1';
 SELECT b, sum(a) FROM attach_partition_t6 GROUP BY b ORDER BY b;
+
+CREATE TABLE attach_partition_t7 (
+                                     a UInt32,
+                                     b UInt32
+)
+    ENGINE = MergeTree
+PARTITION BY a ORDER BY a;
+
+ALTER TABLE attach_partition_t7
+    ADD COLUMN mat_column
+        UInt32 MATERIALIZED a+b;
+
+insert into attach_partition_t7 values (1, 2);
+
+CREATE TABLE attach_partition_t8 (
+                                     a UInt32,
+                                     b UInt32
+)
+    ENGINE = MergeTree
+PARTITION BY a ORDER BY a;
+
+ALTER TABLE attach_partition_t8 ATTACH PARTITION ID '1' FROM attach_partition_t7; -- {serverError 122};
