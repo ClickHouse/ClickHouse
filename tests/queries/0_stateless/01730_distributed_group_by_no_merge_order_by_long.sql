29e43cc3953ee1f8b464e897cc7c0a7d80913de2
Raúl Marín <git@rmr.ninja>
2024-02-26 19:46:49 +0100

    Remove wrong changes

--- tests/queries/0_stateless/01730_distributed_group_by_no_merge_order_by_long.sql
+++ tests/queries/0_stateless/01730_distributed_group_by_no_merge_order_by_long.sql
@@ -4,7 +4,7 @@ drop table if exists data_01730;
 
 -- does not use 127.1 due to prefer_localhost_replica
 
-select * from remote('127.{2..11}', view(select * from numbers(1e6))) group by number order by number limit 20 settings distributed_group_by_no_merge=0, max_memory_usage='200Mi'; -- { serverError 241 }
+select * from remote('127.{2..11}', view(select * from numbers(1e6))) group by number order by number limit 20 settings distributed_group_by_no_merge=0, max_memory_usage='100Mi'; -- { serverError 241 }
 -- no memory limit error, because with distributed_group_by_no_merge=2 remote servers will do ORDER BY and will cut to the LIMIT
 select * from remote('127.{2..11}', view(select * from numbers(1e6))) group by number order by number limit 20 settings distributed_group_by_no_merge=2, max_memory_usage='100Mi';
 
