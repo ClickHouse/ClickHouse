7723e310d791b0e2d95f53ba6a759501f169a511
robot-clickhouse <robot-clickhouse@users.noreply.github.com>
2024-02-26 14:04:19 +0000

    Backport #59626 to 23.11: Fix_kql_issue_found_by_wingfuzz

--- src/Parsers/Kusto/KustoFunctions/KQLDateTimeFunctions.cpp
+++ src/Parsers/Kusto/KustoFunctions/KQLDateTimeFunctions.cpp
@@ -15,6 +15,7 @@
 #include <Parsers/Kusto/KustoFunctions/KQLTimeSeriesFunctions.h>
 #include <Parsers/Kusto/ParserKQLQuery.h>
 #include <Parsers/Kusto/ParserKQLStatement.h>
+#include <Parsers/Kusto/Utilities.h>
 #include <Parsers/ParserSetQuery.h>
 #include "Poco/String.h"
 namespace DB::ErrorCodes
@@ -522,7 +523,7 @@ bool MakeTimeSpan::convertImpl(String & out, IParser::Pos & pos)
     String second;
     int arg_count = 0;
     std::vector<String> args;
-    while (!pos->isEnd() && pos->type != TokenType::ClosingRoundBracket)
+    while (isValidKQLPos(pos) && pos->type != TokenType::ClosingRoundBracket)
     {
         String arg = getConvertedArgument(fn_name, pos);
         args.insert(args.begin(), arg);
@@ -589,7 +590,7 @@ bool MakeDateTime::convertImpl(String & out, IParser::Pos & pos)
     String arguments;
     int arg_count = 0;
 
-    while (!pos->isEnd() && pos->type != TokenType::ClosingRoundBracket)
+    while (isValidKQLPos(pos) && pos->type != TokenType::ClosingRoundBracket)
     {
         String arg = getConvertedArgument(fn_name, pos);
         if (pos->type == TokenType::Comma)
