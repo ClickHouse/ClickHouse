7ed31df6b8b461608a6349ec7a3f03b258227a06
Yarik Briukhovetskyi <114298166+yariks5s@users.noreply.github.com>
2024-02-27 13:56:16 +0100

    reload ci

--- tests/queries/0_stateless/02983_empty_map_hasToken.sql
+++ tests/queries/0_stateless/02983_empty_map_hasToken.sql
@@ -11,7 +11,7 @@ ORDER BY (t, id) SETTINGS index_granularity = 4096 ;
 
 insert into test values ('xxx', 'x', {'content-type':'text/plain','user-agent':'bulk-tests'});
 insert into test values ('xxx', 'y', {'content-type':'application/json','user-agent':'bulk-tests'});
-insert into test select 'xxx', number, map('content-type', 'x' ) from numbers(1e2);
+insert into test select 'xxx', number, map('content-type', 'x' ) FROM numbers(1e2);
 
 optimize table test final;
 
