dd458932c94103bafedb2e6a389171a3d5e54ddd
Nikolai Kochetov <nk@clickhouse.com>
2024-02-27 17:46:13 +0000

    Try to avoid calculation of scalar subqueries for CREATE TABLE.

--- src/Functions/URL/URLPathHierarchy.cpp
+++ src/Functions/URL/URLPathHierarchy.cpp
@@ -22,6 +22,8 @@ public:
     static bool isVariadic() { return false; }
     static size_t getNumberOfArguments() { return 1; }
 
+    static ColumnNumbers getArgumentsThatAreAlwaysConstant() { return {}; }
+
     static void checkArguments(const IFunction & func, const ColumnsWithTypeAndName & arguments)
     {
         FunctionArgumentDescriptors mandatory_args{
