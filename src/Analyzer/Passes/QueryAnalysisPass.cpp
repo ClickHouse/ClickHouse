ad7715b2fa2f63183b6989bfedd2a741a4df30cd
Yakov Olkhovskiy <yakov@clickhouse.com>
2024-02-27 23:12:29 +0000

    fix style

--- src/Analyzer/Passes/QueryAnalysisPass.cpp
+++ src/Analyzer/Passes/QueryAnalysisPass.cpp
@@ -5135,7 +5135,7 @@ ProjectionNames QueryAnalyzer::resolveFunction(QueryTreeNodePtr & node, Identifi
             {
                 if (auto * constant = argument_nodes[n]->as<ConstantNode>())
                 {
-                    auto mask = scope.projection_mask_map->insert( {constant->getTreeHash(), scope.projection_mask_map->size() + 1} ).first->second;
+                    auto mask = scope.projection_mask_map->insert({constant->getTreeHash(), scope.projection_mask_map->size() + 1}).first->second;
                     constant->setMaskId(mask);
                     arguments_projection_names[n] = "[HIDDEN id: " + std::to_string(mask) + "]";
                 }
