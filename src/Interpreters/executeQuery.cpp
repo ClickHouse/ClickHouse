69676b18c001383a6d236bbc9f48b73bfc4d091b
robot-ch-test-poll4 <69306974+robot-ch-test-poll4@users.noreply.github.com>
2024-02-28 16:14:50 +0100

    Backport #59626 to 23.12: Fix_kql_issue_found_by_wingfuzz (#60417)
    * Backport #59626 to 23.12: Fix_kql_issue_found_by_wingfuzz
    
    * Fix build
    
    ---------
    
    Co-authored-by: robot-clickhouse <robot-clickhouse@users.noreply.github.com>
    Co-authored-by: János Benjamin Antal <antaljanosbenjamin@users.noreply.github.com>

--- src/Interpreters/executeQuery.cpp
+++ src/Interpreters/executeQuery.cpp
@@ -704,7 +704,6 @@ static std::tuple<ASTPtr, BlockIO> executeQueryImpl(
         if (settings.dialect == Dialect::kusto && !internal)
         {
             ParserKQLStatement parser(end, settings.allow_settings_after_format_in_insert);
-
             /// TODO: parser should fail early when max_query_size limit is reached.
             ast = parseKQLQuery(parser, begin, end, "", max_query_size, settings.max_parser_depth);
         }
