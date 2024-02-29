846a4ae99579288538e583cd9c7cd5a9392f79c3
Nikita Taranov <nikita.taranov@clickhouse.com>
2024-02-29 21:56:24 +0100

    fix test

--- tests/queries/0_stateless/02734_optimize_group_by.sql
+++ tests/queries/0_stateless/02734_optimize_group_by.sql
@@ -1,7 +1,7 @@
-SELECT 'a' AS key, 'b' as value GROUP BY key WITH CUBE SETTINGS allow_experimental_analyzer = 0;
-SELECT 'a' AS key, 'b' as value GROUP BY key WITH CUBE SETTINGS allow_experimental_analyzer = 1;
+SELECT 'a' AS key, 'b' as value GROUP BY key WITH CUBE ORDER BY ALL SETTINGS allow_experimental_analyzer = 0;
+SELECT 'a' AS key, 'b' as value GROUP BY key WITH CUBE ORDER BY ALL SETTINGS allow_experimental_analyzer = 1;
 
-SELECT 'a' AS key, 'b' as value GROUP BY ignore(1) WITH CUBE;
+SELECT 'a' AS key, 'b' as value GROUP BY ignore(1) WITH CUBE ORDER BY ALL;
 
 SELECT 'a' AS key, 'b' as value GROUP BY ignore(1);
 SELECT 'a' AS key, 'b' as value GROUP BY key;
