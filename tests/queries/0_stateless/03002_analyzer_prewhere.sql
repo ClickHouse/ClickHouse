4cf8e2cb9d4e5af1dc78abdd488e5a9f087f340b
Alexey Milovidov <milovidov@clickhouse.com>
2024-03-02 01:35:30 +0300

    Update 03002_analyzer_prewhere.sql

--- tests/queries/0_stateless/03002_analyzer_prewhere.sql
+++ tests/queries/0_stateless/03002_analyzer_prewhere.sql
@@ -9,3 +9,4 @@ INSERT INTO t__fuzz_0 SELECT * FROM generateRandom() LIMIT 3;
 INSERT INTO t__fuzz_0 SELECT * FROM generateRandom() LIMIT 3;
 
 SELECT * FROM t__fuzz_0 PREWHERE (i < 5) AND (j IN (1, 2)) WHERE i < 5;
+DROP TABLE t__fuzz_0;
