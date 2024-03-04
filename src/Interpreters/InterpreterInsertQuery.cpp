1da1bbeae2aee280fca052acd2b19672188a1ce1
Jordi Villar <jrdi.villar@gmail.com>
2024-03-04 14:35:57 +0100

    Set streams to 1 when using insert_deduplication_token

--- src/Interpreters/InterpreterInsertQuery.cpp
+++ src/Interpreters/InterpreterInsertQuery.cpp
@@ -513,6 +513,12 @@ BlockIO InterpreterInsertQuery::execute()
                 const bool resize_to_max_insert_threads = !table->isView() && views.empty();
                 pre_streams_size = resize_to_max_insert_threads ? settings.max_insert_threads
                                                                 : std::min<size_t>(settings.max_insert_threads, pipeline.getNumStreams());
+
+                /// Deduplication when passing insert_deduplication_token breaks if using more than one thread
+                const String & deduplication_token = settings.insert_deduplication_token;
+                if (!deduplication_token.empty())
+                    pre_streams_size = 1;
+
                 if (table->supportsParallelInsert())
                     sink_streams_size = pre_streams_size;
             }
