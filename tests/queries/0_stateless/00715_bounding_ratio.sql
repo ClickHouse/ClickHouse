347c24e6f3da9499fbfc82c6466696dbe3c52c4a
wangtao.2077 <wangtao.2077@bytedance.com>
2024-03-04 22:22:06 +0800

    add tests

--- tests/queries/0_stateless/00715_bounding_ratio.sql
+++ tests/queries/0_stateless/00715_bounding_ratio.sql
@@ -1,9 +1,18 @@
 drop table if exists rate_test;
+drop table if exists rate_test2;
 
 create table rate_test (timestamp UInt32, event UInt32) engine=Memory;
 insert into rate_test values (0,1000),(1,1001),(2,1002),(3,1003),(4,1004),(5,1005),(6,1006),(7,1007),(8,1008);
 
 select 1.0 = boundingRatio(timestamp, event) from rate_test;
+
+create table rate_test2 (timestamp UInt32, event UInt32) engine=Memory;
+
+SELECT boundingRatioMerge(state) FROM (
+  select boundingRatioState(timestamp, event) as state from rate_test
+  UNION ALL
+  SELECT boundingRatioState(timestamp, event) FROM rate_test2 WHERE 1=0
+);
 
 drop table if exists rate_test2;
 create table rate_test2 (uid UInt32 default 1,timestamp DateTime, event UInt32) engine=Memory;
