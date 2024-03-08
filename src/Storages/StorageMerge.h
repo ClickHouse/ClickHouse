0738ef200e24af28d526b1b5f7d0e9211f53b2dc
Amos Bird <amosbird@gmail.com>
2024-03-08 20:10:29 +0800

    Relax supportsPrewhere check for StorageMerge

--- src/Storages/StorageMerge.h
+++ src/Storages/StorageMerge.h
@@ -49,8 +49,10 @@ public:
     bool supportsSampling() const override { return true; }
     bool supportsFinal() const override { return true; }
     bool supportsSubcolumns() const override { return true; }
-    bool supportsPrewhere() const override { return tableSupportsPrewhere(); }
+    bool supportsPrewhere() const override { return true; }
     std::optional<NameSet> supportedPrewhereColumns() const override;
+
+    bool canMoveConditionsToPrewhere() const override { return tableSupportsPrewhere(); }
 
     QueryProcessingStage::Enum
     getQueryProcessingStage(ContextPtr, QueryProcessingStage::Enum, const StorageSnapshotPtr &, SelectQueryInfo &) const override;
