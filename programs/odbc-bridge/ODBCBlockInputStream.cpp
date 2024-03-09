47b308d2346ef2b87d478d8ee29ee439097df000
Alexey Milovidov <milovidov@clickhouse.com>
2024-03-09 08:42:33 +0100

    Simplify bridges

--- programs/odbc-bridge/ODBCBlockInputStream.cpp
+++ programs/odbc-bridge/ODBCBlockInputStream.cpp
@@ -3,12 +3,8 @@
 #include <IO/ReadBufferFromString.h>
 #include <DataTypes/DataTypeNullable.h>
 #include <DataTypes/DataTypeDateTime64.h>
-#include <Columns/ColumnNullable.h>
-#include <Columns/ColumnString.h>
-#include <Columns/ColumnsNumber.h>
 #include <Common/assert_cast.h>
 #include <IO/ReadHelpers.h>
-#include <IO/WriteHelpers.h>
 #include <Common/logger_useful.h>
 
 
@@ -54,21 +50,7 @@ Chunk ODBCSource::generate()
             const auto & sample = description.sample_block.getByPosition(idx);
 
             if (!result.is_null(idx))
-            {
-                bool is_nullable = description.types[idx].second;
-
-                if (is_nullable)
-                {
-                    ColumnNullable & column_nullable = assert_cast<ColumnNullable &>(*columns[idx]);
-                    const auto & data_type = assert_cast<const DataTypeNullable &>(*sample.type);
-                    insertValue(column_nullable.getNestedColumn(), data_type.getNestedType(), description.types[idx].first, result, idx);
-                    column_nullable.getNullMapData().emplace_back(0);
-                }
-                else
-                {
-                    insertValue(*columns[idx], sample.type, description.types[idx].first, result, idx);
-                }
-            }
+                insertValue(*columns[idx], sample.type, description.types[idx].first, result, idx);
             else
                 insertDefaultValue(*columns[idx], *sample.column);
         }
@@ -87,49 +69,49 @@ void ODBCSource::insertValue(
     switch (type)
     {
         case ValueType::vtUInt8:
-            assert_cast<ColumnUInt8 &>(column).insertValue(row.get<uint16_t>(idx));
+            column.insert(row.get<uint16_t>(idx));
             break;
         case ValueType::vtUInt16:
-            assert_cast<ColumnUInt16 &>(column).insertValue(row.get<uint16_t>(idx));
+            column.insert(row.get<uint16_t>(idx));
             break;
         case ValueType::vtUInt32:
-            assert_cast<ColumnUInt32 &>(column).insertValue(row.get<uint32_t>(idx));
+            column.insert(row.get<uint32_t>(idx));
             break;
         case ValueType::vtUInt64:
-            assert_cast<ColumnUInt64 &>(column).insertValue(row.get<uint64_t>(idx));
+            column.insert(row.get<uint64_t>(idx));
             break;
         case ValueType::vtInt8:
-            assert_cast<ColumnInt8 &>(column).insertValue(row.get<int16_t>(idx));
+            column.insert(row.get<int16_t>(idx));
             break;
         case ValueType::vtInt16:
-            assert_cast<ColumnInt16 &>(column).insertValue(row.get<int16_t>(idx));
+            column.insert(row.get<int16_t>(idx));
             break;
         case ValueType::vtInt32:
-            assert_cast<ColumnInt32 &>(column).insertValue(row.get<int32_t>(idx));
+            column.insert(row.get<int32_t>(idx));
             break;
         case ValueType::vtInt64:
-            assert_cast<ColumnInt64 &>(column).insertValue(row.get<int64_t>(idx));
+            column.insert(row.get<int64_t>(idx));
             break;
         case ValueType::vtFloat32:
-            assert_cast<ColumnFloat32 &>(column).insertValue(row.get<float>(idx));
+            column.insert(row.get<float>(idx));
             break;
         case ValueType::vtFloat64:
-            assert_cast<ColumnFloat64 &>(column).insertValue(row.get<double>(idx));
+            column.insert(row.get<double>(idx));
             break;
         case ValueType::vtFixedString:[[fallthrough]];
         case ValueType::vtEnum8:
         case ValueType::vtEnum16:
         case ValueType::vtString:
-            assert_cast<ColumnString &>(column).insert(row.get<std::string>(idx));
+            column.insert(row.get<std::string>(idx));
             break;
         case ValueType::vtUUID:
         {
             auto value = row.get<std::string>(idx);
-            assert_cast<ColumnUInt128 &>(column).insert(parse<UUID>(value.data(), value.size()));
+            column.insert(parse<UUID>(value.data(), value.size()));
             break;
         }
         case ValueType::vtDate:
-            assert_cast<ColumnUInt16 &>(column).insertValue(UInt16{LocalDate{row.get<std::string>(idx)}.getDayNum()});
+            column.insert(UInt16{LocalDate{row.get<std::string>(idx)}.getDayNum()});
             break;
         case ValueType::vtDateTime:
         {
@@ -139,7 +121,7 @@ void ODBCSource::insertValue(
             readDateTimeText(time, in, assert_cast<const DataTypeDateTime *>(data_type.get())->getTimeZone());
             if (time < 0)
                 time = 0;
-            assert_cast<ColumnUInt32 &>(column).insertValue(static_cast<UInt32>(time));
+            column.insert(static_cast<UInt32>(time));
             break;
         }
         case ValueType::vtDateTime64:
@@ -149,7 +131,7 @@ void ODBCSource::insertValue(
             DateTime64 time = 0;
             const auto * datetime_type = assert_cast<const DataTypeDateTime64 *>(data_type.get());
             readDateTime64Text(time, datetime_type->getScale(), in, datetime_type->getTimeZone());
-            assert_cast<DataTypeDateTime64::ColumnType &>(column).insertValue(time);
+            column.insert(time);
             break;
         }
         case ValueType::vtDecimal32: [[fallthrough]];
