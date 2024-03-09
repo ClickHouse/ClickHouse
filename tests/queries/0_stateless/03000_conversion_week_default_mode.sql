b6b7a2157f2c6938062cd22cc95bdcbbc3cca4a6
Jordi Villar <jrdi.villar@gmail.com>
2024-03-09 16:05:22 +0100

    Allow changing dateDiff and age to sunday mode

--- tests/queries/0_stateless/03000_conversion_week_default_mode.sql
+++ tests/queries/0_stateless/03000_conversion_week_default_mode.sql
@@ -1,26 +1,50 @@
 SELECT
     toDateTime('2024-01-02 00:00:00', 'UTC') dt,
-    toStartOfWeek(dt) w,
-    toStartOfInterval(dt, toIntervalWeek(1)) w_1,
-    toStartOfInterval(dt, toIntervalWeek(2)) w_2,
+    toStartOfWeek(dt) w, -- Sunday, Dec 31
+    toStartOfInterval(dt, toIntervalWeek(1)) w_1, -- Monday, Jan 01
+    toStartOfInterval(dt, toIntervalWeek(2)) w_2, -- Monday, Dec 25
     toBool(w - w_1 = 0) b_1,
-    toBool(w - w_2 = 7) b_2
+    toBool(w - w_2 = 7) b_2,
+    toDateTime('2023-01-22 00:00:00', 'UTC') sunday,
+    toDateTime('2023-01-23 00:00:00', 'UTC') monday,
+    toDateTime('2023-01-24 00:00:00', 'UTC') tuesday,
+    dateDiff('week', monday, tuesday),
+    dateDiff('week', sunday, monday),
+    age('week', monday, tuesday),
+    age('week', sunday, monday),
+    age('week', sunday, monday + toIntervalDay(10))
 ;
 
 SELECT
     toDateTime('2024-01-02 00:00:00', 'UTC') dt,
-    toStartOfWeek(dt) w,
-    toStartOfInterval(dt, toIntervalWeek(1)) w_1,
-    toStartOfInterval(dt, toIntervalWeek(2)) w_2,
+    toStartOfWeek(dt) w, -- Sunday, Dec 31
+    toStartOfInterval(dt, toIntervalWeek(1)) w_1, -- Monday, Jan 01
+    toStartOfInterval(dt, toIntervalWeek(2)) w_2, -- Monday, Dec 25
     toBool(w - w_1 = 0) b_1,
-    toBool(w - w_2 = 7) b_2
+    toBool(w - w_2 = 7) b_2,
+    toDateTime('2023-01-22 00:00:00', 'UTC') sunday,
+    toDateTime('2023-01-23 00:00:00', 'UTC') monday,
+    toDateTime('2023-01-24 00:00:00', 'UTC') tuesday,
+    dateDiff('week', monday, tuesday),
+    dateDiff('week', sunday, monday),
+    age('week', monday, tuesday),
+    age('week', sunday, monday),
+    age('week', sunday, monday + toIntervalDay(10))
 SETTINGS default_mode_week_functions = 'monday';
 
 SELECT
     toDateTime('2024-01-02 00:00:00', 'UTC') dt,
-    toStartOfWeek(dt) w,
-    toStartOfInterval(dt, toIntervalWeek(1)) w_1,
-    toStartOfInterval(dt, toIntervalWeek(2)) w_2,
+    toStartOfWeek(dt) w, -- Sunday, Dec 31
+    toStartOfInterval(dt, toIntervalWeek(1)) w_1, -- Sunday, Dec 31
+    toStartOfInterval(dt, toIntervalWeek(2)) w_2, -- Sunday, Dec 24
     toBool(w - w_1 = 0) b_1,
-    toBool(w - w_2 = 7) b_2
+    toBool(w - w_2 = 7) b_2,
+    toDateTime('2023-01-22 00:00:00', 'UTC') sunday,
+    toDateTime('2023-01-23 00:00:00', 'UTC') monday,
+    toDateTime('2023-01-24 00:00:00', 'UTC') tuesday,
+    dateDiff('week', monday, tuesday),
+    dateDiff('week', sunday, monday),
+    age('week', monday, tuesday),
+    age('week', sunday, monday),
+    age('week', sunday, monday + toIntervalDay(10))
 SETTINGS default_mode_week_functions = 'sunday';
