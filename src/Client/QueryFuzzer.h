4af16043b86a6e72b3f13579a938d271fd0b77dd
tomershafir <tomer.shafir8@gmail.com>
2024-03-09 21:27:28 +0200

    improve randomness by erase buf elem when full

--- src/Client/QueryFuzzer.h
+++ src/Client/QueryFuzzer.h
@@ -50,12 +50,12 @@ struct QueryFuzzer
     // we are currently fuzzing. We add some part from each new query we are asked
     // to fuzz, and keep this state between queries, so the fuzzing output becomes
     // more interesting over time, as the queries mix.
-    // The maps are used for collection, and the vectors are used for random access.
+    // The hash tables are used for collection, and the vectors are used for random access.
     std::unordered_map<std::string, ASTPtr> column_like_map;
-    std::vector<ASTPtr> column_like;
+    std::vector<std::pair<std::string, ASTPtr>> column_like;
 
     std::unordered_map<std::string, ASTPtr> table_like_map;
-    std::vector<ASTPtr> table_like;
+    std::vector<std::pair<std::string, ASTPtr>> table_like;
 
     // Some debug fields for detecting problematic ASTs with loops.
     // These are reset for each fuzzMain call.
