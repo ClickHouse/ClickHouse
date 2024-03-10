d2b7fb03b12c5ce3ec5a377f3872483639e8c4fe
Alexey Milovidov <milovidov@clickhouse.com>
2024-03-10 23:36:03 +0100

    Fix localization in check-large-objects

--- utils/check-style/check-large-objects.sh
+++ utils/check-style/check-large-objects.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/env bash
 
+export LC_ALL=C # The "total" should be printed without localization
+
 # Check that there are no new translation units compiled to an object file larger than a certain size.
 
 TU_EXCLUDES=(
