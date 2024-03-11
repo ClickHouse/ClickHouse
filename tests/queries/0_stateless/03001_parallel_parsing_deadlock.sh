724cc903afb9283a8369a62a836d04eceae42e57
Kruglov Pavel <48961922+Avogar@users.noreply.github.com>
2024-03-11 15:56:02 +0100

    Restart CI

--- tests/queries/0_stateless/03001_parallel_parsing_deadlock.sh
+++ tests/queries/0_stateless/03001_parallel_parsing_deadlock.sh
@@ -9,4 +9,3 @@ DATA_FILE=$CLICKHOUSE_TEST_UNIQUE_NAME.csv
 $CLICKHOUSE_LOCAL -q "select number > 1000000 ? 'error' : toString(number) from numbers(2000000) format CSV" > $DATA_FILE
 $CLICKHOUSE_LOCAL -q "select * from file($DATA_FILE, CSV, 'x UInt64') format Null settings input_format_allow_errors_ratio=1"
 rm $DATA_FILE
-
