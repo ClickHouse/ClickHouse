28256564d8bf839e712c7f2c10b0fef1c16000f9
Yakov Olkhovskiy <yakov@clickhouse.com>
2024-03-11 21:56:09 +0000

    fix

--- src/Interpreters/DatabaseCatalog.cpp
+++ src/Interpreters/DatabaseCatalog.cpp
@@ -950,8 +950,8 @@ StoragePtr DatabaseCatalog::getTable(const StorageID & table_id, ContextPtr loca
 {
     std::optional<Exception> exc;
     auto table = local_context->hasQueryContext() ?
-        local_context->getQueryContext()->getOrCacheStorage(table_id.getFullNameNotQuoted(), [&](){ return getTableImpl(table_id, local_context, nullptr).second; }) :
-        getTableImpl(table_id, local_context, nullptr).second;
+        local_context->getQueryContext()->getOrCacheStorage(table_id.getFullNameNotQuoted(), [&](){ return getTableImpl(table_id, local_context, &exc).second; }) :
+        getTableImpl(table_id, local_context, &exc).second;
     if (!table)
         throw Exception(*exc);
     return table;
