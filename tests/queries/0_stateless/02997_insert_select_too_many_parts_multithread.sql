c0bfafa203091abbe2f5af6d1253249fc432ad64
Yatsishin Ilya <2159081+qoega@users.noreply.github.com>
2024-03-13 10:48:02 +0000

    fix tags

--- tests/queries/0_stateless/02997_insert_select_too_many_parts_multithread.sql
+++ tests/queries/0_stateless/02997_insert_select_too_many_parts_multithread.sql
@@ -1,5 +1,5 @@
-# Tags: disabled
-# TODO: Fix parts_to_throw_insert logic for parallel  MergeTreeSink onStart calls
+--  Tags: disabled
+-- TODO: Fix parts_to_throw_insert logic for parallel  MergeTreeSink onStart calls
 DROP TABLE IF EXISTS too_many_parts;
 
 CREATE TABLE too_many_parts (x UInt64) ENGINE = MergeTree ORDER BY tuple() SETTINGS parts_to_delay_insert = 5, parts_to_throw_insert = 5;
