da3cb02df5985b0425facb8543f67236e5d9bf38
PapaToemmsn <160967948+PapaToemmsn@users.noreply.github.com>
2024-03-13 18:10:26 +0100

    COMMIT/ROLLBACK with no active transaction do not throw an exception for MySQL clients anymore

--- src/Interpreters/InterpreterTransactionControlQuery.cpp
+++ src/Interpreters/InterpreterTransactionControlQuery.cpp
@@ -52,7 +52,13 @@ BlockIO InterpreterTransactionControlQuery::executeCommit(ContextMutablePtr sess
 {
     auto txn = session_context->getCurrentTransaction();
     if (!txn)
-        return {};
+    {
+        if (session_context->getClientInfo().interface == ClientInfo::Interface::MYSQL)
+            return {};
+        else
+            throw Exception(ErrorCodes::INVALID_TRANSACTION, "There is no current transaction");
+    }
+    
     if (txn->getState() != MergeTreeTransaction::RUNNING)
         throw Exception(ErrorCodes::INVALID_TRANSACTION, "Transaction is not in RUNNING state");
 
@@ -111,7 +117,13 @@ BlockIO InterpreterTransactionControlQuery::executeRollback(ContextMutablePtr se
 {
     auto txn = session_context->getCurrentTransaction();
     if (!txn)
-        return {};
+    {
+        if (session_context->getClientInfo().interface == ClientInfo::Interface::MYSQL)
+            return {};
+        else
+            throw Exception(ErrorCodes::INVALID_TRANSACTION, "There is no current transaction");
+    }
+    
     if (txn->getState() == MergeTreeTransaction::COMMITTED)
         throw Exception(ErrorCodes::LOGICAL_ERROR, "Transaction is in COMMITTED state");
     if (txn->getState() == MergeTreeTransaction::COMMITTING)
