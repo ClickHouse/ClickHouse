9815cedd6334c1e10c6c09d5547dc9a43875410a
vdimir <vdimir@clickhouse.com>
2024-03-14 14:43:00 +0100

    fix style

--- src/Storages/StorageMergeTree.cpp
+++ src/Storages/StorageMergeTree.cpp
@@ -66,7 +66,6 @@ namespace ErrorCodes
     extern const int NO_SUCH_DATA_PART;
     extern const int ABORTED;
     extern const int SUPPORT_IS_DISABLED;
-    extern const int TABLE_IS_READ_ONLY;
     extern const int UNSUPPORTED_METHOD;
 }
 
