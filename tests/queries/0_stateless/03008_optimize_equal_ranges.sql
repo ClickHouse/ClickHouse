02e81329794ed326853f1e039bca49e960ed469d
Anton Popov <anton@clickhouse.com>
2024-03-14 14:05:19 +0000

    fix test

--- tests/queries/0_stateless/03008_optimize_equal_ranges.sql
+++ tests/queries/0_stateless/03008_optimize_equal_ranges.sql
@@ -10,16 +10,19 @@ INSERT INTO t_optimize_equal_ranges SELECT 0, toString(number), number FROM numb
 INSERT INTO t_optimize_equal_ranges SELECT 1, toString(number), number FROM numbers(30000);
 INSERT INTO t_optimize_equal_ranges SELECT 2, toString(number), number FROM numbers(30000);
 
-SELECT a, uniqExact(b) FROM t_optimize_equal_ranges GROUP BY a ORDER BY a;
-SELECT a, sum(c) FROM t_optimize_equal_ranges GROUP BY a ORDER BY a;
+SELECT a, uniqExact(b) FROM t_optimize_equal_ranges GROUP BY a ORDER BY a SETTINGS max_threads = 16;
+SELECT a, uniqExact(b) FROM t_optimize_equal_ranges GROUP BY a ORDER BY a SETTINGS max_threads = 1;
+SELECT a, sum(c) FROM t_optimize_equal_ranges GROUP BY a ORDER BY a SETTINGS max_threads = 16;
+SELECT a, sum(c) FROM t_optimize_equal_ranges GROUP BY a ORDER BY a SETTINGS max_threads = 1;
 
 SYSTEM FLUSH LOGS;
 
 SELECT
     used_aggregate_functions[1] AS func,
+    Settings['max_threads'] AS threads,
     ProfileEvents['AggregationOptimizedEqualRangesOfKeys'] > 0
 FROM system.query_log
 WHERE type = 'QueryFinish' AND current_database = currentDatabase() AND query LIKE '%SELECT%FROM%t_optimize_equal_ranges%'
-ORDER BY func;
+ORDER BY func, threads;
 
 DROP TABLE t_optimize_equal_ranges;
