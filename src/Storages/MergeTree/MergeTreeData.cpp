3e88747fd484033806324db2d7e694451a36c522
Arthur Passos <arthur.ti@outlook.com>
2024-03-18 14:25:29 -0300

    remove unnecessary renameTo

--- src/Storages/MergeTree/MergeTreeData.cpp
+++ src/Storages/MergeTree/MergeTreeData.cpp
@@ -6173,7 +6173,6 @@ MergeTreeData::MutableDataPartsVector MergeTreeData::tryLoadPartsToAttach(const 
         part_info.level = 0u;
         part_info.use_leagcy_max_level = false;
         part->setName(part->getNewName(part_info));
-        part->renameTo(source_dir + part->name, true);
 
         loadPartAndFixMetadataImpl(part, local_context, getInMemoryMetadataPtr()->getMetadataVersion(), getSettings()->fsync_after_insert);
         loaded_parts.push_back(part);
