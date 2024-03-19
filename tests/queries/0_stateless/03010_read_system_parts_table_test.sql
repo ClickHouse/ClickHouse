8d49ff7350304619386cb849851432a5ca434619
Dan Wu <wudanzy@google.com>
2024-03-19 03:12:32 +0000

    Add tests for system.parts_columns table.

--- tests/queries/0_stateless/03010_read_system_parts_table_test.sql
+++ tests/queries/0_stateless/03010_read_system_parts_table_test.sql
@@ -6,5 +6,6 @@ INSERT INTO users VALUES (6666, 'Ksenia', 48);
 
 SELECT uuid, name from system.parts WHERE database = currentDatabase() AND table = 'users';
 
-SELECT table from system.parts WHERE database = currentDatabase() AND uuid = '00000000-0000-0000-0000-000000000000';
+SELECT uuid, name, table from system.parts WHERE database = currentDatabase() AND table = 'users' AND uuid = '00000000-0000-0000-0000-000000000000';
+SELECT uuid, name, table, column from system.parts_columns WHERE database = currentDatabase() AND table = 'users' AND uuid = '00000000-0000-0000-0000-000000000000';
 DROP TABLE IF EXISTS users;
