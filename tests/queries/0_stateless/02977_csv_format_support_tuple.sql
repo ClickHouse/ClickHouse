4d99ffbe1c1d91d30fe390c0b7ea446b7a86676a
shuai-xu <xushuai@bigo.sg>
2024-03-22 16:10:42 +0800

    fix test failure

--- tests/queries/0_stateless/02977_csv_format_support_tuple.sql
+++ tests/queries/0_stateless/02977_csv_format_support_tuple.sql
@@ -1,4 +1,5 @@
--- Tags: no-fasttest
+-- Tags: no-parallel
+
 insert into function file('02977_1.csv') select '20240305', 1, ['s', 'd'], map('a', 2), tuple('222', 33, map('abc', 5));
 desc file('02977_1.csv');
 select * from file('02977_1.csv') settings max_threads=1;
