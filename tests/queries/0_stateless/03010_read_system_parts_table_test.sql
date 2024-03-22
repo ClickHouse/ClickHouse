184400581f62aaab56c04cfa50ae2ac574a4c716
János Benjamin Antal <benjamin.antal@clickhouse.com>
2024-03-22 11:30:06 +0000

    Add test for filtering by UUID in `system.parts` and `system.parts_columns`

--- tests/queries/0_stateless/03010_read_system_parts_table_test.sql
+++ tests/queries/0_stateless/03010_read_system_parts_table_test.sql
@@ -11,3 +11,18 @@ SELECT uuid, name from system.parts WHERE database = currentDatabase() AND table
 SELECT uuid, name, table from system.parts WHERE database = currentDatabase() AND table = 'users' AND uuid = '00000000-0000-0000-0000-000000000000';
 SELECT uuid, name, table, column from system.parts_columns WHERE database = currentDatabase() AND table = 'users' AND uuid = '00000000-0000-0000-0000-000000000000';
 DROP TABLE IF EXISTS users;
+
+CREATE TABLE mt_with_uuid (x Int64) ENGINE = MergeTree ORDER BY x SETTINGS assign_part_uuids = 1;
+
+INSERT INTO mt_with_uuid SELECT number AS x FROM numbers(5);
+
+CREATE TABLE uuid_store (uuid UUID) ENGINE = Memory;
+INSERT INTO uuid_store SELECT uuid FROM system.parts WHERE database = currentDatabase() AND table = 'mt_with_uuid';
+
+INSERT INTO mt_with_uuid SELECT number AS x FROM numbers(5);
+
+-- Avoid querying UUID as it changes from run to run
+SELECT 'without_uuid_filter', name, table FROM system.parts WHERE database = currentDatabase() AND table = 'mt_with_uuid' ORDER BY name;
+SELECT 'with_uuid_filter', name, table FROM system.parts WHERE uuid IN (SELECT uuid FROM uuid_store);
+SELECT 'without_uuid_filter', name, table, column FROM system.parts_columns WHERE database = currentDatabase() AND table = 'mt_with_uuid' ORDER BY name;
+SELECT 'with_uuid_filter', name, table, column FROM system.parts_columns WHERE uuid IN (SELECT uuid FROM uuid_store);
