8c49c9bf9e119ecbfdf3ec9147190a226424a9d2
János Benjamin Antal <antaljanosbenjamin@users.noreply.github.com>
2024-03-22 15:20:01 +0100

    Revert "Fix bug when reading system.parts using UUID (issue 61220)." (#61774)

--- src/Storages/System/StorageSystemPartsBase.cpp
+++ src/Storages/System/StorageSystemPartsBase.cpp
@@ -190,7 +190,7 @@ StoragesInfoStream::StoragesInfoStream(const ActionsDAGPtr & filter_by_database,
     block_to_filter.insert(ColumnWithTypeAndName(std::move(table_column_mut), std::make_shared<DataTypeString>(), "table"));
     block_to_filter.insert(ColumnWithTypeAndName(std::move(engine_column_mut), std::make_shared<DataTypeString>(), "engine"));
     block_to_filter.insert(ColumnWithTypeAndName(std::move(active_column_mut), std::make_shared<DataTypeUInt8>(), "active"));
-    block_to_filter.insert(ColumnWithTypeAndName(std::move(storage_uuid_column_mut), std::make_shared<DataTypeUUID>(), "storage_uuid"));
+    block_to_filter.insert(ColumnWithTypeAndName(std::move(storage_uuid_column_mut), std::make_shared<DataTypeUUID>(), "uuid"));
 
     if (rows)
     {
@@ -203,7 +203,7 @@ StoragesInfoStream::StoragesInfoStream(const ActionsDAGPtr & filter_by_database,
     database_column = block_to_filter.getByName("database").column;
     table_column = block_to_filter.getByName("table").column;
     active_column = block_to_filter.getByName("active").column;
-    storage_uuid_column = block_to_filter.getByName("storage_uuid").column;
+    storage_uuid_column = block_to_filter.getByName("uuid").column;
 }
 
 class ReadFromSystemPartsBase : public SourceStepWithFilter
