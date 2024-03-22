e1f9e496408d57acc5dcb2808f77bf38f1e8a524
Alexander Tokmakov <tavplubix@clickhouse.com>
2024-03-22 17:21:48 +0100

    fix test for smt

--- tests/queries/0_stateless/02916_move_partition_inactive_replica.sql
+++ tests/queries/0_stateless/02916_move_partition_inactive_replica.sql
@@ -12,6 +12,7 @@ create table shard_0.from_1 (x UInt32) engine = ReplicatedMergeTree('/clickhouse
 create table shard_1.from_1 (x UInt32) engine = ReplicatedMergeTree('/clickhouse/tables/from_1_' || currentDatabase(), '1') order by x settings old_parts_lifetime=1, max_cleanup_delay_period=1, cleanup_delay_period=1;
 
 system stop merges shard_0.from_1;
+system stop merges shard_1.from_1;
 insert into shard_0.from_1 select number + 20 from numbers(10);
 insert into shard_0.from_1 select number + 30 from numbers(10);
 
@@ -20,7 +21,7 @@ insert into shard_0.from_1 select number + 50 from numbers(10);
 
 system sync replica shard_1.from_1;
 
-create table shard_0.to (x UInt32) engine = ReplicatedMergeTree('/clickhouse/tables/to_' || currentDatabase(), '0') order by x settings old_parts_lifetime=1, max_cleanup_delay_period=1, cleanup_delay_period=1, max_parts_to_merge_at_once=2;
+create table shard_0.to (x UInt32) engine = ReplicatedMergeTree('/clickhouse/tables/to_' || currentDatabase(), '0') order by x settings old_parts_lifetime=1, max_cleanup_delay_period=1, cleanup_delay_period=1, max_parts_to_merge_at_once=2, shared_merge_tree_disable_merges_and_mutations_assignment=1;
 
 create table shard_1.to (x UInt32) engine = ReplicatedMergeTree('/clickhouse/tables/to_' || currentDatabase(), '1') order by x settings old_parts_lifetime=1, max_cleanup_delay_period=1, cleanup_delay_period=1, max_parts_to_merge_at_once=2;
 
