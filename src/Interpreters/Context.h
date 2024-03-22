c67a898a2f28d431f531f5bc404566dc409eca91
Yakov Olkhovskiy <yakov@clickhouse.com>
2024-03-22 21:52:14 +0000

    fix

--- src/Interpreters/Context.h
+++ src/Interpreters/Context.h
@@ -21,6 +21,7 @@
 #include <Interpreters/Context_fwd.h>
 #include <Interpreters/StorageID.h>
 #include <Interpreters/MergeTreeTransactionHolder.h>
+#include <Interpreters/DatabaseCatalog.h>
 #include <Parsers/IAST_fwd.h>
 #include <Server/HTTP/HTTPContext.h>
 #include <Storages/ColumnsDescription.h>
