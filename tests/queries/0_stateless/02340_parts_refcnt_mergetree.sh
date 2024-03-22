05f2a995b9ee12f6580a061c3213b4a91d33ac24
Maksim Kita <kitaetoya@gmail.com>
2024-03-22 14:02:08 +0300

    Test 02340_parts_refcnt_mergetree disable random settings

--- tests/queries/0_stateless/02340_parts_refcnt_mergetree.sh
+++ tests/queries/0_stateless/02340_parts_refcnt_mergetree.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
-# Tags: no-fasttest, long
+# Tags: no-fasttest, long, no-random-settings
 
 CUR_DIR=$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)
 # shellcheck source=../shell_config.sh
