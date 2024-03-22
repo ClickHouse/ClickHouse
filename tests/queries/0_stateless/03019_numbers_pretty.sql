1a7494f04f4525494eb40c0edd2669c8a515e49b
Alexey Milovidov <milovidov@clickhouse.com>
2024-03-22 20:25:58 +0100

    Add a test
    Revert changes from another branch
    
    Revert changes from another branch
    
    Revert changes from another branch

---
+++ tests/queries/0_stateless/03019_numbers_pretty.sql
@@ -0,0 +1,6 @@
+SET output_format_pretty_row_numbers = 0;
+SELECT 1.23e9 FORMAT Pretty;
+SELECT -1.23e9 FORMAT Pretty;
+SELECT inf FORMAT Pretty;
+SELECT -inf FORMAT Pretty;
+SELECT nan FORMAT Pretty;
