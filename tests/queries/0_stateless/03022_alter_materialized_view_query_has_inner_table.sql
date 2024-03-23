7bdab0544182307f9a4c9d104077b7d904499fd1
Alexey Milovidov <milovidov@clickhouse.com>
2024-03-24 00:09:29 +0300

    Update 03022_alter_materialized_view_query_has_inner_table.sql

--- tests/queries/0_stateless/03022_alter_materialized_view_query_has_inner_table.sql
+++ tests/queries/0_stateless/03022_alter_materialized_view_query_has_inner_table.sql
@@ -2,7 +2,7 @@ DROP TABLE IF EXISTS src_table;
 DROP TABLE IF EXISTS mv;
 
 CREATE TABLE src_table (`a` UInt32, `b` UInt32) ENGINE = MergeTree ORDER BY a;
-CREATE MATERIALIZED VIEW mv UUID '2bad6d75-86fe-4da0-815b-2c7410253941' (`a` UInt32) ENGINE = MergeTree ORDER BY a AS SELECT a FROM src_table;
+CREATE MATERIALIZED VIEW mv (`a` UInt32) ENGINE = MergeTree ORDER BY a AS SELECT a FROM src_table;
 
 INSERT INTO src_table (a, b) VALUES (1, 1), (2, 2);
 
