965a3ac2f32f620acdbdbcf22dc645fbc18b8c20
Alexey Milovidov <milovidov@clickhouse.com>
2024-03-23 21:19:35 +0100

    Add function getClientHTTPHeader, part 2

---
+++ src/Functions/getClientHTTPHeader.cpp
@@ -0,0 +1,94 @@
+#include <Functions/IFunction.h>
+#include <Functions/FunctionFactory.h>
+#include <Functions/FunctionHelpers.h>
+#include <DataTypes/DataTypeString.h>
+#include <Columns/ColumnString.h>
+#include <Interpreters/Context.h>
+#include <Core/Field.h>
+
+
+namespace DB
+{
+namespace ErrorCodes
+{
+    extern const int ILLEGAL_TYPE_OF_ARGUMENT;
+    extern const int FUNCTION_NOT_ALLOWED;
+}
+
+namespace
+{
+
+class FunctionGetClientHTTPHeader : public IFunction, WithContext
+{
+private:
+
+public:
+    explicit FunctionGetClientHTTPHeader(ContextPtr context_)
+        : WithContext(context_)
+    {
+        if (!getContext()->getSettingsRef().allow_get_http_client_header)
+            throw Exception(ErrorCodes::FUNCTION_NOT_ALLOWED, "The function {} requires setting `allow_get_client_http_header` to be enabled.", getName());
+    }
+
+    String getName() const override { return "getClientHTTPHeader"; }
+
+    bool useDefaultImplementationForConstants() const override { return true; }
+    bool isSuitableForShortCircuitArgumentsExecution(const DataTypesWithConstInfo &) const override { return false; }
+
+    size_t getNumberOfArguments() const override
+    {
+        return 1;
+    }
+
+    DataTypePtr getReturnTypeImpl(const DataTypes & arguments) const override
+    {
+        if (!isString(arguments[0]))
+            throw Exception(ErrorCodes::ILLEGAL_TYPE_OF_ARGUMENT, "The argument of function {} must be String", getName());
+        return std::make_shared<DataTypeString>();
+    }
+
+    ColumnPtr executeImpl(const ColumnsWithTypeAndName & arguments, const DataTypePtr & result_type, size_t input_rows_count) const override
+    {
+        const ClientInfo & client_info = getContext()->getClientInfo();
+
+        const auto & source = arguments[0].column;
+        auto result = result_type->createColumn();
+        result->reserve(input_rows_count);
+
+        for (size_t row = 0; row < input_rows_count; ++row)
+        {
+            Field header;
+            source->get(row, header);
+            if (auto it = client_info.http_headers.find(header.get<String>()); it != client_info.http_headers.end())
+                result->insert(it->second);
+            else
+                result->insertDefault();
+        }
+
+        return result;
+    }
+};
+
+}
+
+REGISTER_FUNCTION(GetClientHTTPHeader)
+{
+    factory.registerFunction("getClientHTTPHeader",
+        [](ContextPtr context) { return std::make_shared<FunctionGetClientHTTPHeader>(context); },
+        FunctionDocumentation{
+            .description = R"(
+Get the value of an HTTP header.
+
+If there no such header or the current request is not performed via the HTTP interface, the function returns an empty string.
+Certain HTTP headers (e.g., `Authentication` and `X-ClickHouse-*`) are restricted.
+
+The function requires the setting `allow_get_client_http_header` to be enabled.
+The setting is not enabled by default for security reasons, because some headers, such as `Cookie`, could contain sensitive info.
+",
+            .syntax = "getClientHTTPHeader(name)",
+            .arguments = {{"name", "The HTTP header name (String)"}},
+            .returned_value = "The value of the header (String).",
+            .categories{"Miscellaneous"}});
+}
+
+}
