35d0220e19e61332bd29c8c1bf24ca301cc2287f
Alexey Milovidov <milovidov@clickhouse.com>
2024-03-24 19:26:12 +0100

    Remove garbage from KQL, part 2

--- src/Parsers/Kusto/ParserKQLStatement.cpp
+++ src/Parsers/Kusto/ParserKQLStatement.cpp
@@ -2,11 +2,11 @@
 #include <Parsers/ASTSelectWithUnionQuery.h>
 #include <Parsers/CommonParsers.h>
 #include <Parsers/IParserBase.h>
-#include <Parsers/Kusto/KustoFunctions/KQLFunctionFactory.h>
 #include <Parsers/Kusto/ParserKQLQuery.h>
 #include <Parsers/Kusto/ParserKQLStatement.h>
-#include <Parsers/Kusto/Utilities.h>
 #include <Parsers/ParserSetQuery.h>
+#include <Parsers/ASTLiteral.h>
+
 
 namespace DB
 {
@@ -62,50 +62,25 @@ bool ParserKQLWithUnionQuery::parseImpl(Pos & pos, ASTPtr & node, Expected & exp
 
 bool ParserKQLTableFunction::parseImpl(Pos & pos, ASTPtr & node, Expected & expected)
 {
-    ParserKQLWithUnionQuery kql_p;
-    ASTPtr select;
-    ParserToken s_lparen(TokenType::OpeningRoundBracket);
+    ParserToken lparen(TokenType::OpeningRoundBracket);
+    ParserToken rparen(TokenType::OpeningRoundBracket);
 
-    auto begin = pos;
-    auto paren_count = 0;
-    String kql_statement;
+    ASTPtr string_literal;
+    ParserStringLiteral parser_string_literal;
 
-    if (s_lparen.ignore(pos, expected))
+    if (!(lparen.ignore(pos, expected)
+        && parser_string_literal.parse(pos, string_literal, expected)
+        && rparen.ignore(pos, expected)))
     {
-        if (pos->type == TokenType::HereDoc)
-        {
-            kql_statement = String(pos->begin + 2, pos->end - 2);
-        }
-        else
-        {
-            ++paren_count;
-            auto pos_start = pos;
-            while (isValidKQLPos(pos))
-            {
-                if (pos->type == TokenType::ClosingRoundBracket)
-                    --paren_count;
-                if (pos->type == TokenType::OpeningRoundBracket)
-                    ++paren_count;
-
-                if (paren_count == 0)
-                    break;
-                ++pos;
-            }
-            kql_statement = String(pos_start->begin, (--pos)->end);
-        }
-        ++pos;
-        Tokens token_kql(kql_statement.c_str(), kql_statement.c_str() + kql_statement.size());
-        IParser::Pos pos_kql(token_kql, pos.max_depth, pos.max_backtracks);
-
-        if (kql_p.parse(pos_kql, select, expected))
-        {
-            node = select;
-            ++pos;
-            return true;
-        }
+        return false;
     }
-    pos = begin;
-    return false;
+
+    String kql_statement = typeid_cast<const ASTLiteral &>(*string_literal).value.safeGet<String>();
+
+    Tokens token_kql(kql_statement.data(), kql_statement.data() + kql_statement.size());
+    IParser::Pos pos_kql(token_kql, pos.max_depth, pos.max_backtracks);
+
+    return ParserKQLWithUnionQuery().parse(pos_kql, node, expected);
 }
 
 }
