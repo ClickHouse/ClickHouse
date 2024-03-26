bbb14955a6b44fc4c4c4bcb76105c54ab70cbe67
robot-clickhouse <robot-clickhouse@users.noreply.github.com>
2024-03-26 12:48:01 +0000

    Automatic style fix

--- tests/integration/test_parallel_replicas_failover_timeout/test.py
+++ tests/integration/test_parallel_replicas_failover_timeout/test.py
@@ -4,9 +4,15 @@ from helpers.cluster import ClickHouseCluster
 
 cluster = ClickHouseCluster(__file__)
 
-node1 = cluster.add_instance("node1", main_configs=["configs/remote_servers.xml"], with_zookeeper=True)
-node2 = cluster.add_instance("node2", main_configs=["configs/remote_servers.xml"], with_zookeeper=True)
-node3 = cluster.add_instance("node3", main_configs=["configs/remote_servers.xml"], with_zookeeper=True)
+node1 = cluster.add_instance(
+    "node1", main_configs=["configs/remote_servers.xml"], with_zookeeper=True
+)
+node2 = cluster.add_instance(
+    "node2", main_configs=["configs/remote_servers.xml"], with_zookeeper=True
+)
+node3 = cluster.add_instance(
+    "node3", main_configs=["configs/remote_servers.xml"], with_zookeeper=True
+)
 
 
 @pytest.fixture(scope="module")
