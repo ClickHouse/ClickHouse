3af0f12d68fa6cbccc248bde69b1be14961ad46e
Arthur Passos <arthur.ti@outlook.com>
2024-03-26 09:47:40 -0300

    databse atomic

--- tests/queries/0_stateless/03013_forbid_attach_table_if_active_replica_already_exists.sql
+++ tests/queries/0_stateless/03013_forbid_attach_table_if_active_replica_already_exists.sql
@@ -1,6 +1,7 @@
-create table t1 (a Int)
+create database at engine = Atomic;
+create table at.t1 (a Int)
     engine=ReplicatedMergeTree('/clickhouse/tables/{database}/test', 'r1')
     order by tuple() SETTINGS index_granularity = 8192;
-attach table t2 UUID '6c32d92e-bebf-4730-ae73-c43e5748f829'
+attach table at.t2 UUID '6c32d92e-bebf-4730-ae73-c43e5748f829'
        (a Int) engine=ReplicatedMergeTree('/clickhouse/tables/{database}/test', 'r1')
        order by tuple() SETTINGS index_granularity = 8192; -- { serverError REPLICA_ALREADY_EXISTS };
