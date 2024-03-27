78088ce59a9562e3c805ab54147c68c888228615
Azat Khuzhin <a3at.mail@gmail.com>
2024-03-27 11:51:59 +0100

    Reduce lock contention for MergeTree tables (by renaming parts without holding lock)
    Under heavy load, or not so heavy but with fsync_part_directory=1,
    time that renameTo() holds DataPartsLock will be increased, and this
    will affect almost every operation with this table.
    
    On one of production clusters I saw ~60 seconds with
    fsync_part_directory=1.
    
    Move the renameTo() out from the critical section.
    
    v2: instead of using DataPartsLock.lock.lock()/unlock() move the renameTo() into MergeTreeData::Transaction::commit()
    Signed-off-by: Azat Khuzhin <a.khuzhin@semrush.com>

--- src/Storages/MergeTree/MergeTreeData.cpp
+++ src/Storages/MergeTree/MergeTreeData.cpp
@@ -3906,12 +3906,9 @@ void MergeTreeData::preparePartForCommit(MutableDataPartPtr & part, Transaction 
                return !may_be_cleaned_up || temporary_parts.contains(dir_name);
            }());
 
-    if (need_rename)
-        part->renameTo(part->name, true);
-
     LOG_TEST(log, "preparePartForCommit: inserting {} into data_parts_indexes", part->getNameWithState());
     data_parts_indexes.insert(part);
-    out_transaction.addPart(part);
+    out_transaction.addPart(part, need_rename);
 }
 
 bool MergeTreeData::addTempPart(
@@ -6617,9 +6614,11 @@ TransactionID MergeTreeData::Transaction::getTID() const
     return Tx::PrehistoricTID;
 }
 
-void MergeTreeData::Transaction::addPart(MutableDataPartPtr & part)
+void MergeTreeData::Transaction::addPart(MutableDataPartPtr & part, bool need_rename)
 {
     precommitted_parts.insert(part);
+    if (need_rename)
+        precommitted_parts_need_rename.insert(part);
 }
 
 void MergeTreeData::Transaction::rollback(DataPartsLock * lock)
@@ -6665,7 +6664,9 @@ void MergeTreeData::Transaction::rollback(DataPartsLock * lock)
 
 void MergeTreeData::Transaction::clear()
 {
+    chassert(precommitted_parts.size() >= precommitted_parts_need_rename.size());
     precommitted_parts.clear();
+    precommitted_parts_need_rename.clear();
 }
 
 MergeTreeData::DataPartsVector MergeTreeData::Transaction::commit(DataPartsLock * acquired_parts_lock)
@@ -6681,6 +6682,9 @@ MergeTreeData::DataPartsVector MergeTreeData::Transaction::commit(DataPartsLock 
         for (const auto & part : precommitted_parts)
             if (part->getDataPartStorage().hasActiveTransaction())
                 part->getDataPartStorage().commitTransaction();
+
+        for (const auto & part_need_rename : precommitted_parts_need_rename)
+            part_need_rename->renameTo(part_need_rename->name, true);
 
         if (txn)
         {
