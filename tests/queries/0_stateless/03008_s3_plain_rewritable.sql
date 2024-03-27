cb3cf73be20e55b28b81fab0260004bdc7979a27
Julia Kartseva <yulia.kartseva@gmail.com>
2024-03-27 02:12:26 +0000

    more tests

--- tests/queries/0_stateless/03008_s3_plain_rewritable.sql
+++ tests/queries/0_stateless/03008_s3_plain_rewritable.sql
@@ -1,18 +1,22 @@
 -- Tags: no-fasttest
 -- Tag: no-fasttest -- requires S3
 
-drop table if exists test_s3;
-create table test_s3 (a Int32) engine = MergeTree() order by a
-settings disk=disk(name='s3_plain_rewritable',
-                   type = s3_plain_rewritable,
-                   endpoint = 'http://localhost:11111/test/s3_plain_rewritable/',
-                   access_key_id = clickhouse,
-                   secret_access_key = clickhouse,
-                   send_metadata = false, skip_access_check=true
-            );
+drop table if exists test_mt;
+create table test_mt (a Int32, b Int64) engine = MergeTree() order by a
+settings disk = disk(
+    type = s3_plain_rewritable,
+    endpoint = 'http://localhost:11111/test/test_mt/',
+    access_key_id = clickhouse,
+    secret_access_key = clickhouse);
 
-insert into test_s3 (*) values (1), (2), (3), (4), (5), (6);
-insert into test_s3 (*) select * from numbers(10000000);
+insert into test_mt (*) values (1, 2), (2, 2), (3, 1), (4, 7), (5, 10), (6, 12);
+insert into test_mt (*) select number, number from numbers_mt(1000000);
 
-select count(*) from test_s3 LIMIT 10;
+select count(*) from test_mt;
+select (*) from test_mt order by tuple(a, b) limit 10;
 
+-- File moving is not supported.
+alter table test_mt update b = 0 where a % 2 = 1; --{ serverError NOT_IMPLEMENTED }
+
+alter table test_mt add column c Int64 after b; --{ serverError BAD_GET }
+alter table test_mt drop column b; --{ serverError BAD_GET }
