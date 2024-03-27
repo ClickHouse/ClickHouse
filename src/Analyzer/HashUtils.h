6a7d48b932f67e4191d280e17d1e18151489318b
Nikolai Kochetov <nk@clickhouse.com>
2024-03-27 17:26:12 +0000

    Fix aliases.

--- src/Analyzer/HashUtils.h
+++ src/Analyzer/HashUtils.h
@@ -11,35 +11,37 @@ namespace DB
   * Example of usage:
   * std::unordered_map<QueryTreeNodeConstRawPtrWithHash, std::string> map;
   */
-template <typename QueryTreeNodePtrType>
+template <typename QueryTreeNodePtrType, bool compare_aliases = true>
 struct QueryTreeNodeWithHash
 {
     QueryTreeNodeWithHash(QueryTreeNodePtrType node_) /// NOLINT
         : node(std::move(node_))
-        , hash(node->getTreeHash())
+        , hash(node->getTreeHash({.compare_aliases = compare_aliases}))
     {}
 
     QueryTreeNodePtrType node = nullptr;
     CityHash_v1_0_2::uint128 hash;
 };
 
-template <typename T>
-inline bool operator==(const QueryTreeNodeWithHash<T> & lhs, const QueryTreeNodeWithHash<T> & rhs)
+template <typename T, bool compare_aliases>
+inline bool operator==(const QueryTreeNodeWithHash<T, compare_aliases> & lhs, const QueryTreeNodeWithHash<T, compare_aliases> & rhs)
 {
-    return lhs.hash == rhs.hash && lhs.node->isEqual(*rhs.node);
+    return lhs.hash == rhs.hash && lhs.node->isEqual(*rhs.node, {.compare_aliases = compare_aliases});
 }
 
-template <typename T>
-inline bool operator!=(const QueryTreeNodeWithHash<T> & lhs, const QueryTreeNodeWithHash<T> & rhs)
+template <typename T, bool compare_aliases>
+inline bool operator!=(const QueryTreeNodeWithHash<T, compare_aliases> & lhs, const QueryTreeNodeWithHash<T, compare_aliases> & rhs)
 {
     return !(lhs == rhs);
 }
 
 using QueryTreeNodePtrWithHash = QueryTreeNodeWithHash<QueryTreeNodePtr>;
+using QueryTreeNodePtrWithHashWithoutAlias = QueryTreeNodeWithHash<QueryTreeNodePtr, /*compare_aliases*/ false>;
 using QueryTreeNodeRawPtrWithHash = QueryTreeNodeWithHash<IQueryTreeNode *>;
 using QueryTreeNodeConstRawPtrWithHash = QueryTreeNodeWithHash<const IQueryTreeNode *>;
 
 using QueryTreeNodePtrWithHashSet = std::unordered_set<QueryTreeNodePtrWithHash>;
+using QueryTreeNodePtrWithHashWithoutAliasSet = std::unordered_set<QueryTreeNodePtrWithHashWithoutAlias>;
 using QueryTreeNodeConstRawPtrWithHashSet = std::unordered_set<QueryTreeNodeConstRawPtrWithHash>;
 
 template <typename Value>
