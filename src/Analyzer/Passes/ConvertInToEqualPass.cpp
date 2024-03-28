54d7a6041e36d6d8eaf793f0a931e34b6bd8e103
Nikolai Kochetov <nk@clickhouse.com>
2024-03-28 17:35:38 +0000

    Fix type for ConvertInToEqualPass

--- src/Analyzer/Passes/ConvertInToEqualPass.cpp
+++ src/Analyzer/Passes/ConvertInToEqualPass.cpp
@@ -40,7 +40,10 @@ public:
             return ;
         auto result_func_name = MAPPING.at(func_node->getFunctionName());
         auto equal = std::make_shared<FunctionNode>(result_func_name);
-        QueryTreeNodes arguments{column_node->clone(), constant_node->clone()};
+        QueryTreeNodes arguments{
+            column_node->clone(),
+            std::make_shared<ConstantNode>(constant_node->getValue(), removeNullable(constant_node->getResultType()))
+        };
         equal->getArguments().getNodes() = std::move(arguments);
         FunctionOverloadResolverPtr resolver;
         bool decimal_check_overflow = getContext()->getSettingsRef().decimal_check_overflow;
