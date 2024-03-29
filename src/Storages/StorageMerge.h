02a17293ea3d888df55ba5508a48e96048c40da4
Mikhail Artemenko <michicosun@ya.ru>
2024-03-29 17:26:01 +0000

    fix getStorageSnapshot inst

--- src/Storages/StorageMerge.h
+++ src/Storages/StorageMerge.h
@@ -55,7 +55,7 @@ public:
     QueryProcessingStage::Enum
     getQueryProcessingStage(ContextPtr, QueryProcessingStage::Enum, const StorageSnapshotPtr &, SelectQueryInfo &) const override;
 
-    StorageSnapshotPtr getStorageSnapshot(const StorageMetadataPtr & metadata_snapshot, ContextPtr) const override;
+    StorageSnapshotPtr getStorageSnapshot(const StorageMetadataPtr & metadata_snapshot, ContextPtr, const StorageSnapshotSettings &) const override;
 
     void read(
         QueryPlan & query_plan,
