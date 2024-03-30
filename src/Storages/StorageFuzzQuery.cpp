77e28e29e9b2550204ea8d60647115587f36673e
pufit <pufit@clickhouse.com>
2024-03-30 14:40:18 -0400

    fix style

--- src/Storages/StorageFuzzQuery.cpp
+++ src/Storages/StorageFuzzQuery.cpp
@@ -19,8 +19,6 @@ namespace ErrorCodes
 {
     extern const int BAD_ARGUMENTS;
     extern const int NUMBER_OF_ARGUMENTS_DOESNT_MATCH;
-    extern const int LOGICAL_ERROR;
-    extern const int INCORRECT_DATA;
 }
 
 ColumnPtr FuzzQuerySource::createColumn()
