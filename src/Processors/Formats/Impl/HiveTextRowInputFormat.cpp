2b4e6439ef6c5746085e39c9224024bf2f643a8d
kevinyhzou <kevinyunhe8@gmail.com>
2024-04-01 18:29:32 +0800

    remove useless code

--- src/Processors/Formats/Impl/HiveTextRowInputFormat.cpp
+++ src/Processors/Formats/Impl/HiveTextRowInputFormat.cpp
@@ -34,7 +34,7 @@ HiveTextRowInputFormat::HiveTextRowInputFormat(
 HiveTextRowInputFormat::HiveTextRowInputFormat(
     const Block & header_, std::shared_ptr<PeekableReadBuffer> buf_, const Params & params_, const FormatSettings & format_settings_)
     : CSVRowInputFormat(
-        header_, buf_, params_, true, false, format_settings_, std::make_unique<HiveTextFormatReader>(*buf_, header_, format_settings_))
+        header_, buf_, params_, true, false, format_settings_, std::make_unique<HiveTextFormatReader>(*buf_, format_settings_))
 {
 }
 
