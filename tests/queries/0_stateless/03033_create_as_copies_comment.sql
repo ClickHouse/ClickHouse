836169af18c371104274c8108820aa592968e0f2
Pablo Marcos <pablo.marcos.oltra@gmail.com>
2024-04-02 16:50:03 +0200

    Use currentDatabase() instead of CLICKHOUSE_DATABASE

--- tests/queries/0_stateless/03033_create_as_copies_comment.sql
+++ tests/queries/0_stateless/03033_create_as_copies_comment.sql
@@ -1,10 +1,11 @@
-DROP DATABASE IF EXISTS {CLICKHOUSE_DATABASE:Identifier};
-CREATE DATABASE {CLICKHOUSE_DATABASE:Identifier};
+DROP TABLE IF EXISTS base;
+DROP TABLE IF EXISTS copy_without_comment;
+DROP TABLE IF EXISTS copy_with_comment;
 
-CREATE TABLE {CLICKHOUSE_DATABASE:Identifier}.base (a Int32) ENGINE = TinyLog COMMENT 'original comment';
-CREATE TABLE {CLICKHOUSE_DATABASE:Identifier}.copy_without_comment AS base;
-CREATE TABLE {CLICKHOUSE_DATABASE:Identifier}.copy_with_comment AS base COMMENT 'new comment';
+CREATE TABLE base (a Int32) ENGINE = TinyLog COMMENT 'original comment';
+CREATE TABLE copy_without_comment AS base;
+CREATE TABLE copy_with_comment AS base COMMENT 'new comment';
 
-SELECT comment FROM system.tables WHERE database = {CLICKHOUSE_DATABASE:String} AND name = 'base';
-SELECT comment FROM system.tables WHERE database = {CLICKHOUSE_DATABASE:String} AND name = 'copy_without_comment';
-SELECT comment FROM system.tables WHERE database = {CLICKHOUSE_DATABASE:String} AND name = 'copy_with_comment';
+SELECT comment FROM system.tables WHERE database = currentDatabase() AND name = 'base';
+SELECT comment FROM system.tables WHERE database = currentDatabase() AND name = 'copy_without_comment';
+SELECT comment FROM system.tables WHERE database = currentDatabase() AND name = 'copy_with_comment';
