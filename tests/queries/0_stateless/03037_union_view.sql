4441a1b3f3c2f6844e233e379335d4bdf4922ab5
Nikita Mikhaylov <nikitamikhaylov@clickhouse.com>
2024-04-02 15:59:17 +0000

    Close https://github.com/ClickHouse/ClickHouse/issues/55803

---
+++ tests/queries/0_stateless/03037_union_view.sql
@@ -0,0 +1,24 @@
+-- https://github.com/ClickHouse/ClickHouse/issues/55803
+CREATE TABLE broken_table
+(
+    start DateTime64(6),
+    end DateTime64(6),
+)
+ENGINE = ReplacingMergeTree(start)
+ORDER BY (start);
+
+CREATE VIEW broken_view as
+SELECT
+  t.start as start,
+  t.end as end,
+  cast(datediff('second', t.start, t.end) as float) as total_sec
+FROM broken_table t FINAL
+UNION ALL
+SELECT
+  null as start,
+  null as end,
+  null as total_sec;
+
+SELECT v.start, v.total_sec
+FROM broken_view v FINAL
+WHERE v.start IS NOT NULL;
