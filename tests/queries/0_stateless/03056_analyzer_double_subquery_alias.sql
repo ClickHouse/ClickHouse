809461ccdae2896e7b91dba36667645864a9aaae
Nikita Fomichev <nikita.fomichev@clickhouse.com>
2024-04-03 10:03:47 +0200

    Close: https://github.com/ClickHouse/ClickHouse/issues/22627

---
+++ tests/queries/0_stateless/03056_analyzer_double_subquery_alias.sql
@@ -0,0 +1,16 @@
+-- https://github.com/ClickHouse/ClickHouse/issues/22627
+WITH
+    x AS
+    (
+        SELECT 1 AS a
+    ),
+    xx AS
+    (
+        SELECT *
+        FROM x
+        , x AS x1
+        , x AS x2
+    )
+SELECT *
+FROM xx
+WHERE a = 1;
