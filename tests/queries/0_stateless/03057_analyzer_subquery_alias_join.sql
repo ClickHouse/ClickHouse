a401d328fd35a65973306571e67539fcd8ec4909
Nikita Fomichev <nikita.fomichev@clickhouse.com>
2024-04-03 10:06:11 +0200

    Close: https://github.com/ClickHouse/ClickHouse/issues/10276

---
+++ tests/queries/0_stateless/03057_analyzer_subquery_alias_join.sql
@@ -0,0 +1,12 @@
+-- https://github.com/ClickHouse/ClickHouse/issues/10276
+SELECT
+    sum(x.n) as n,
+    sum(z.n) as n2
+FROM
+(
+    SELECT 1000 AS n,1 as id
+) AS x
+join (select 10000 as n,1 as id) as y
+on x.id = y.id
+left join (select 100000 as n,1 as id) as z
+on x.id = z.id;
