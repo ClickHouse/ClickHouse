5e30984d8d7780182dda35c827b4c4626c06210d
Nikita Fomichev <nikita.fomichev@clickhouse.com>
2024-04-03 10:19:14 +0200

    Close: https://github.com/ClickHouse/ClickHouse/issues/17710

---
+++ tests/queries/0_stateless/03059_analyzer_join_engine_missing_column.sql
@@ -0,0 +1,8 @@
+-- https://github.com/ClickHouse/ClickHouse/issues/17710
+CREATE TABLE id_val(id UInt32, val UInt32) ENGINE = Memory;
+CREATE TABLE id_val_join0(id UInt32, val UInt8) ENGINE = Join(ANY, LEFT, id) SETTINGS join_use_nulls = 0;
+CREATE TABLE id_val_join1(id UInt32, val UInt8) ENGINE = Join(ANY, LEFT, id) SETTINGS join_use_nulls = 1;
+
+SELECT * FROM id_val ANY LEFT JOIN id_val_join0 USING (id) SETTINGS join_use_nulls = 0;
+
+SELECT * FROM id_val ANY LEFT JOIN id_val_join1 USING (id) SETTINGS join_use_nulls = 1;
