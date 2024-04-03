c7487647eac70765a1b893ae973860e11409c7ed
f.abapolov <f.abapolov@okko.tv>
2024-04-03 19:14:08 +0530

    tests for arraySymmetricDifference

---
+++ tests/queries/0_stateless/03033_array_symmetric_difference.sql
@@ -0,0 +1,14 @@
+select arraySymmetricDifference([], []);
+select arraySymmetricDifference([1], []);
+select arraySymmetricDifference([1], [1]);
+select arraySymmetricDifference([1, 2], [1, 3], [2, 3]);
+select arraySymmetricDifference([1, 2], [1, 3], [1, 4]);
+select arraySymmetricDifference([1, -1], [1]);
+select arraySymmetricDifference([1, -1], [Null, 1]);
+select arraySymmetricDifference([1, -1, Null], [Null, 1]);
+select arraySymmetricDifference(cast([1, 2] as Array(Nullable(Int8))), [1, 3]);
+select arraySymmetricDifference(CAST([1, -1] AS Array(Nullable(Int8))), [NULL, 1]);
+select arraySymmetricDifference([[1, 2], [1, 1]], [[2, 1], [1, 1]]);
+select arraySymmetricDifference([[1, 2, Null], [1, 1]], [[-2, 1], [1, 1]]);
+select arraySymmetricDifference([(1, ['a', 'b']), (Null, ['c'])], [(2, ['c', Null]), (1, ['a', 'b'])]);
+select toTypeName(arraySymmetricDifference([(1, ['a', 'b']), (Null, ['c'])], [(2, ['c', Null]), (1, ['a', 'b'])]));
