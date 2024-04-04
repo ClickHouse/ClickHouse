66a2962ccef3f64f3c51178955d2839739d3d882
Azat Khuzhin <a3at.mail@gmail.com>
2024-04-04 20:26:33 +0200

    Add reason into "Part {} is broken and need manual correction" message
    Signed-off-by: Azat Khuzhin <a.khuzhin@semrush.com>

--- src/Storages/MergeTree/IMergeTreeDataPart.cpp
+++ src/Storages/MergeTree/IMergeTreeDataPart.cpp
@@ -737,7 +737,11 @@ void IMergeTreeDataPart::loadColumnsChecksumsIndexes(bool require_columns_checks
     {
         /// Don't scare people with broken part error
         if (!isRetryableException(std::current_exception()))
-            LOG_ERROR(storage.log, "Part {} is broken and need manual correction", getDataPartStorage().getFullPath());
+        {
+            auto message = getCurrentExceptionMessage(true);
+            LOG_ERROR(storage.log, "Part {} is broken and need manual correction. Reason: {}",
+                getDataPartStorage().getFullPath(), message);
+        }
 
         // There could be conditions that data part to be loaded is broken, but some of meta infos are already written
         // into meta data before exception, need to clean them all.
