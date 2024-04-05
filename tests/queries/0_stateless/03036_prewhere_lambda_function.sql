0f4efdaa4788dc5fd9e4ee96ca611eb35d63a29a
vdimir <vdimir@clickhouse.com>
2024-04-05 14:48:39 +0000

    remove case from 03036_prewhere_lambda_function

--- tests/queries/0_stateless/03036_prewhere_lambda_function.sql
+++ tests/queries/0_stateless/03036_prewhere_lambda_function.sql
@@ -3,6 +3,5 @@ CREATE TABLE t (A Array(Int64)) Engine = MergeTree ORDER BY tuple();
 INSERT INTO t VALUES ([1,2,3]), ([4,5,6]), ([7,8,9]);
 
 SELECT * FROM t PREWHERE arrayExists(x -> x = 5, A);
-SELECT * FROM t PREWHERE arrayExists(lamdba(tuple(x), x = 5), A);
 
 DROP TABLE t;
