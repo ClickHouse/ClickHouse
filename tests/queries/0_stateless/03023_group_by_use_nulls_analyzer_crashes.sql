56d6ec5f302f10b02bd7fd1fe47618083626a06c
Nikolai Kochetov <nk@clickhouse.com>
2024-04-06 12:13:22 +0200

    Update 03023_group_by_use_nulls_analyzer_crashes.sql

--- tests/queries/0_stateless/03023_group_by_use_nulls_analyzer_crashes.sql
+++ tests/queries/0_stateless/03023_group_by_use_nulls_analyzer_crashes.sql
@@ -6,6 +6,6 @@ select tuple(array(number)) as x FROM numbers(10) GROUP BY number, array(number)
 
 SELECT tuple(number) AS x FROM numbers(10) GROUP BY GROUPING SETS (number) order by x;
 
-SELECT ignore(toFixedString('Lambda as function parameter', 28), toNullable(28), ignore(8)), sum(marks) FROM system.parts GROUP BY GROUPING SETS ((2)) FORMAT Null settings optimize_injective_functions_in_group_by=1, optimize_group_by_function_keys=1, group_by_use_nulls=1; -- { serverError ILLEGAL_AGGREGATION }
+SELECT ignore(toFixedString('Lambda as function parameter', 28), toNullable(28), ignore(8)), sum(marks) FROM system.parts WHERE database = currentDatabase() GROUP BY GROUPING SETS ((2)) FORMAT Null settings optimize_injective_functions_in_group_by=1, optimize_group_by_function_keys=1, group_by_use_nulls=1; -- { serverError ILLEGAL_AGGREGATION }
 
 SELECT toLowCardinality(materialize('a' AS key)),  'b' AS value GROUP BY key WITH CUBE SETTINGS group_by_use_nulls = 1;
