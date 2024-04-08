0d6426d781538b899acfcfbff736f16422097466
skyoct <skyoct@163.com>
2024-04-08 13:34:54 +0000

    batter

--- tests/queries/0_stateless/03036_clamp.sql
+++ tests/queries/0_stateless/03036_clamp.sql
@@ -6,3 +6,5 @@ SELECT clamp(today(), yesterday() - 10, yesterday() + 10) - today()
 SELECT clamp([], ['hello'], ['world']);
 SELECT clamp(-1., -1000., 18446744073709551615.);
 SELECT clamp(toNullable(123), 234, 456);
+select clamp(1, null, 5);
+select clamp(1, 6, null);
