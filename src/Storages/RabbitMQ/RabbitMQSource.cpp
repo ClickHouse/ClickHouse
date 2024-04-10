263dcee5eab06151310132ac3ee12d6ee41627aa
Kseniia Sumarokova <54203879+kssenii@users.noreply.github.com>
2024-04-10 22:41:25 +0200

    Update RabbitMQSource.cpp

--- src/Storages/RabbitMQ/RabbitMQSource.cpp
+++ src/Storages/RabbitMQ/RabbitMQSource.cpp
@@ -204,9 +204,8 @@ Chunk RabbitMQSource::generateImpl()
             const auto exchange_name = storage.getExchange();
             const auto & message = consumer->currentMessage();
 
-            LOG_TEST(log, "Pulled {} rows, message delivery tag: {} "
-                     "(previous delivery tag: {}, redelivered: {}, failed delivery tags by this moment: {}). "
-                     "Exception message: {}",
+            LOG_TEST(log, "Pulled {} rows, message delivery tag: {}, "
+                     "previous delivery tag: {}, redelivered: {}, failed delivery tags by this moment: {}, exception message: {}",
                      new_rows, message.delivery_tag, commit_info.delivery_tag, message.redelivered,
                      commit_info.failed_delivery_tags.size(),
                      exception_message.has_value() ? exception_message.value() : "None");
