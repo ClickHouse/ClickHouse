54930e6b22c26620fb8ca5298d38112ca8661bec
Nikita Fomichev <nikita.fomichev@clickhouse.com>
2024-04-10 22:44:56 +0200

    Close: https://github.com/ClickHouse/ClickHouse/issues/33000

--- tests/queries/0_stateless/03119_analyzer_window_function_in_CTE_alias.sql
+++ tests/queries/0_stateless/03119_analyzer_window_function_in_CTE_alias.sql
@@ -1,7 +1,6 @@
 -- https://github.com/ClickHouse/ClickHouse/issues/47422
 SET allow_experimental_analyzer=1;
 
-
 DROP TEMPORARY TABLE IF EXISTS test;
 CREATE TEMPORARY TABLE test (a Float32, id UInt64);
 INSERT INTO test VALUES (10,10),(20,20);
