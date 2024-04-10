c6f6745a95ebfca3eb479dd0abf5baa69f9bce5c
Nikita Mikhaylov <nikitamikhaylov@clickhouse.com>
2024-04-10 17:10:02 +0200

    Closes: https://github.com/ClickHouse/ClickHouse/issues/56466

---
+++ tests/queries/0_stateless/03103_positional_arguments.sql
@@ -0,0 +1,41 @@
+-- https://github.com/ClickHouse/ClickHouse/issues/56466
+
+DROP TABLE IF EXISTS users;
+
+CREATE TABLE users (uid Int16, name String, age Int16) ENGINE=Memory;
+
+INSERT INTO users VALUES (1231, 'John', 33);
+INSERT INTO users VALUES (6666, 'Ksenia', 48);
+INSERT INTO users VALUES (8888, 'Alice', 50);
+
+-- The query works when using a single SELECT * 
+SELECT *
+FROM
+(
+    SELECT
+        name,
+        age
+    FROM users
+)
+GROUP BY
+    1,
+    2;
+
+-- It doesn't when the GROUP BY is nested deeper
+SELECT *
+FROM
+(
+	SELECT *
+	FROM
+	(
+    	SELECT
+        	name,
+        	age
+    	FROM users
+	)
+	GROUP BY
+    	1,
+    	2
+);
+
+DROP TABLE IF EXISTS users;
