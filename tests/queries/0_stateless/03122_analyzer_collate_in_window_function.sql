62f15e6cc198e93d9dfb73e2b3d57b48c15034ae
Nikita Fomichev <nikita.fomichev@clickhouse.com>
2024-04-12 00:50:16 +0200

    add missing SET allow_experimental_analyzer=1;

--- tests/queries/0_stateless/03122_analyzer_collate_in_window_function.sql
+++ tests/queries/0_stateless/03122_analyzer_collate_in_window_function.sql
@@ -1,5 +1,7 @@
 -- Tags: no-fasttest
 -- https://github.com/ClickHouse/ClickHouse/issues/44039
+SET allow_experimental_analyzer=1;
+
 create table test_window_collate(c1 String, c2 String) engine=MergeTree order by c1;
 insert into test_window_collate values('1', '上海');
 insert into test_window_collate values('1', '北京');
