110f2f6cdcf91570743ddb8ae6b695d4212accc4
Konstantin Morozov <just.morozov.k@gmail.com>
2024-04-12 18:11:47 +0000

    add sync for drop table in tests

--- tests/queries/0_stateless/03093_drop_detached_table.sql
+++ tests/queries/0_stateless/03093_drop_detached_table.sql
@@ -5,10 +5,9 @@ CREATE DATABASE db_03093_atomic Engine=Atomic;
 CREATE TABLE db_03093_atomic.test_table  (number UInt64) ENGINE=MergeTree ORDER BY number;
 INSERT INTO db_03093_atomic.test_table SELECT number FROM system.numbers LIMIT 6;
 DETACH TABLE db_03093_atomic.test_table;
-DROP TABLE db_03093_atomic.test_table;
+DROP TABLE db_03093_atomic.test_table SYNC;
 DROP DATABASE db_03093_atomic SYNC;
 
-DROP DATABASE IF EXISTS db_03093_atomic SYNC;
 CREATE DATABASE db_03093_atomic Engine=Atomic;
 CREATE TABLE db_03093_atomic.test_table  (number UInt64) ENGINE=MergeTree ORDER BY number;
 INSERT INTO db_03093_atomic.test_table SELECT number FROM system.numbers LIMIT 6;
@@ -20,11 +19,18 @@ CREATE DATABASE db_03093_atomic Engine=Atomic;
 CREATE TABLE db_03093_atomic.test_table  (number UInt64) ENGINE=MergeTree ORDER BY number;
 INSERT INTO db_03093_atomic.test_table SELECT number FROM system.numbers LIMIT 6;
 DETACH TABLE db_03093_atomic.test_table PERMANENTLY;
-DROP TABLE db_03093_atomic.test_table;
+DROP TABLE db_03093_atomic.test_table SYNC;
 DROP DATABASE db_03093_atomic SYNC;
 
 CREATE DATABASE db_03093_atomic Engine=Atomic;
 CREATE TABLE db_03093_atomic.test_table  (number UInt64) ENGINE=MergeTree ORDER BY number;
+INSERT INTO db_03093_atomic.test_table SELECT number FROM system.numbers LIMIT 6;
+DETACH TABLE db_03093_atomic.test_table PERMANENTLY;
+DROP TABLE db_03093_atomic.test_table SYNC SYNC;
+DROP DATABASE db_03093_atomic SYNC;
+
+CREATE DATABASE db_03093_atomic Engine=Atomic;
+CREATE TABLE db_03093_atomic.test_table  (number UInt64) ENGINE=ReplacingMergeTree() ORDER BY number;
 INSERT INTO db_03093_atomic.test_table SELECT number FROM system.numbers LIMIT 6;
 DETACH TABLE db_03093_atomic.test_table PERMANENTLY;
 DROP TABLE db_03093_atomic.test_table SYNC;
@@ -33,69 +39,62 @@ DROP DATABASE db_03093_atomic SYNC;
 CREATE DATABASE db_03093_atomic Engine=Atomic;
 CREATE TABLE db_03093_atomic.test_table  (number UInt64) ENGINE=ReplacingMergeTree() ORDER BY number;
 INSERT INTO db_03093_atomic.test_table SELECT number FROM system.numbers LIMIT 6;
-DETACH TABLE db_03093_atomic.test_table PERMANENTLY;
-DROP TABLE db_03093_atomic.test_table;
-DROP DATABASE db_03093_atomic SYNC;
-
-CREATE DATABASE db_03093_atomic Engine=Atomic;
-CREATE TABLE db_03093_atomic.test_table  (number UInt64) ENGINE=ReplacingMergeTree() ORDER BY number;
-INSERT INTO db_03093_atomic.test_table SELECT number FROM system.numbers LIMIT 6;
 DETACH TABLE db_03093_atomic.test_table;
-DROP TABLE db_03093_atomic.test_table;
+DROP TABLE db_03093_atomic.test_table SYNC;
 DROP DATABASE db_03093_atomic SYNC;
 
 CREATE DATABASE db_03093_atomic Engine=Atomic;
 CREATE TABLE db_03093_atomic.test_table  (number UInt64) ENGINE=SummingMergeTree ORDER BY number;
 INSERT INTO db_03093_atomic.test_table SELECT number FROM system.numbers LIMIT 6;
 DETACH TABLE db_03093_atomic.test_table PERMANENTLY;
-DROP TABLE db_03093_atomic.test_table;
+DROP TABLE db_03093_atomic.test_table SYNC;
 DROP DATABASE db_03093_atomic SYNC;
 
 CREATE DATABASE db_03093_atomic Engine=Atomic;
 CREATE TABLE db_03093_atomic.test_table  (number UInt64) ENGINE=SummingMergeTree ORDER BY number;
 INSERT INTO db_03093_atomic.test_table SELECT number FROM system.numbers LIMIT 6;
 DETACH TABLE db_03093_atomic.test_table;
-DROP TABLE db_03093_atomic.test_table;
+DROP TABLE db_03093_atomic.test_table SYNC;
 DROP DATABASE db_03093_atomic SYNC;
 
 CREATE DATABASE db_03093_atomic Engine=Atomic;
 CREATE TABLE db_03093_atomic.test_table  (number UInt64) ENGINE=AggregatingMergeTree ORDER BY number;
 INSERT INTO db_03093_atomic.test_table SELECT number FROM system.numbers LIMIT 6;
 DETACH TABLE db_03093_atomic.test_table PERMANENTLY;
-DROP TABLE db_03093_atomic.test_table;
+DROP TABLE db_03093_atomic.test_table SYNC;
 DROP DATABASE db_03093_atomic SYNC;
 
 CREATE DATABASE db_03093_atomic Engine=Atomic;
 CREATE TABLE db_03093_atomic.test_table  (number UInt64) ENGINE=AggregatingMergeTree ORDER BY number;
 INSERT INTO db_03093_atomic.test_table SELECT number FROM system.numbers LIMIT 6;
 DETACH TABLE db_03093_atomic.test_table;
-DROP TABLE db_03093_atomic.test_table;
+DROP TABLE db_03093_atomic.test_table SYNC;
 DROP DATABASE db_03093_atomic SYNC;
 
 CREATE DATABASE db_03093_atomic Engine=Atomic;
 CREATE TABLE db_03093_atomic.test_table  (number UInt64, sign Int8) ENGINE=CollapsingMergeTree(sign) ORDER BY number;
 INSERT INTO db_03093_atomic.test_table SELECT number, 1 FROM system.numbers LIMIT 6;
 DETACH TABLE db_03093_atomic.test_table PERMANENTLY;
-DROP TABLE db_03093_atomic.test_table;
+DROP TABLE db_03093_atomic.test_table SYNC;
 DROP DATABASE db_03093_atomic SYNC;
 
 CREATE DATABASE db_03093_atomic Engine=Atomic;
 CREATE TABLE db_03093_atomic.test_table  (number UInt64, sign Int8) ENGINE=CollapsingMergeTree(sign) ORDER BY number;
 INSERT INTO db_03093_atomic.test_table SELECT number, 1 FROM system.numbers LIMIT 6;
 DETACH TABLE db_03093_atomic.test_table;
-DROP TABLE db_03093_atomic.test_table;
+DROP TABLE db_03093_atomic.test_table SYNC;
 DROP DATABASE db_03093_atomic SYNC;
 
 CREATE DATABASE db_03093_atomic Engine=Atomic;
 CREATE TABLE db_03093_atomic.test_table  (number UInt64, sign Int8, version Int32) ENGINE=VersionedCollapsingMergeTree(sign, version) ORDER BY number;
 INSERT INTO db_03093_atomic.test_table SELECT number, 1, 1 FROM system.numbers LIMIT 6;
 DETACH TABLE db_03093_atomic.test_table PERMANENTLY;
-DROP TABLE db_03093_atomic.test_table;
+DROP TABLE db_03093_atomic.test_table SYNC;
 DROP DATABASE db_03093_atomic SYNC;
 
 CREATE DATABASE db_03093_atomic Engine=Atomic;
 CREATE TABLE db_03093_atomic.test_table  (number UInt64, sign Int8, version Int32) ENGINE=VersionedCollapsingMergeTree(sign, version) ORDER BY number;
 INSERT INTO db_03093_atomic.test_table SELECT number, 1, 1 FROM system.numbers LIMIT 6;
 DETACH TABLE db_03093_atomic.test_table;
-DROP TABLE db_03093_atomic.test_table;
+DROP TABLE db_03093_atomic.test_table SYNC;
 DROP DATABASE db_03093_atomic SYNC;
