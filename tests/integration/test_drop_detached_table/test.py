8c36550775b2c80e40e71cb22a467b10406a707a
Konstantin Morozov <just.morozov.k@gmail.com>
2024-04-15 11:12:23 +0000

    add s3 test, fix unit

--- tests/integration/test_drop_detached_table/test.py
+++ tests/integration/test_drop_detached_table/test.py
@@ -1,3 +1,5 @@
+# Tag no-fasttest: requires S3
+
 import pytest
 from helpers.cluster import ClickHouseCluster
 
@@ -10,6 +12,11 @@ replica2 = cluster.add_instance(
     "replica2", with_zookeeper=True, main_configs=["configs/remote_servers.xml"]
 )
 
+node_s3 = cluster.add_instance(
+        "node_s3",
+        main_configs=["configs/disk_s3.xml"],
+        with_minio=True,
+    )
 
 @pytest.fixture(scope="module")
 def start_cluster():
@@ -22,7 +29,7 @@ def start_cluster():
         cluster.shutdown()
 
 
-def create_table(node, table_name):
+def create_replicated_table(node, table_name):
     replica = node.name
     engine = f"ReplicatedMergeTree('/clickhouse/tables/1/{table_name}', '{replica}')"
 
@@ -40,15 +47,41 @@ def create_table(node, table_name):
     )
 
 
-def test_drop_replicated(start_cluster):
-    create_table(replica1, "test_table")
-    create_table(replica2, "test_table")
+def create_s3_table(node, table_name):
+    engine = "MergeTree"
 
-    replica1.query("INSERT INTO test_table SELECT number FROM system.numbers LIMIT 6;")
-    replica1.query("SYSTEM SYNC REPLICA test_table;", timeout=20)
+    node.query_with_retry(
+        """
+        CREATE TABLE {table_name} 
+        (
+            number UInt64
+        ) 
+        ENGINE={engine}
+        ORDER BY number
+        SETTINGS storage_policy='s3'
+        """.format(
+            table_name=table_name, engine=engine
+        )
+    )
 
-    replica1.query("DETACH TABLE test_table PERMANENTLY;")
-    replica1.query("SET allow_drop_detached_table=1; DROP TABLE test_table SYNC;")
 
-    replica2.query("DETACH TABLE test_table PERMANENTLY;")
-    replica2.query("SET allow_drop_detached_table=1; DROP TABLE test_table SYNC;")
+def test_drop_replicated_table(start_cluster):
+    create_replicated_table(replica1, "test_replicated_table")
+    create_replicated_table(replica2, "test_replicated_table")
+
+    replica1.query("INSERT INTO test_replicated_table SELECT number FROM system.numbers LIMIT 6;")
+    replica1.query("SYSTEM SYNC REPLICA test_replicated_table;", timeout=20)
+
+    replica1.query("DETACH TABLE test_replicated_table PERMANENTLY;")
+    replica1.query("SET allow_drop_detached_table=1; DROP TABLE test_replicated_table SYNC;")
+
+    replica2.query("DETACH TABLE test_replicated_table PERMANENTLY;")
+    replica2.query("SET allow_drop_detached_table=1; DROP TABLE test_replicated_table SYNC;")
+
+
+def test_drop_s3_table(start_cluster):
+    create_s3_table(node_s3, "test_s3_table")
+    node_s3.query("INSERT INTO test_s3_table SELECT number FROM system.numbers LIMIT 6;")
+
+    node_s3.query("DETACH TABLE test_s3_table PERMANENTLY;")
+    node_s3.query("SET allow_drop_detached_table=1; DROP TABLE test_s3_table SYNC;")
--- tests/queries/0_stateless/03093_drop_detached_table.sql
+++ tests/queries/0_stateless/03093_drop_detached_table.sql
@@ -1,75 +1,71 @@
 SET allow_drop_detached_table=1;
 
-DROP DATABASE IF EXISTS db_03093_atomic SYNC;
-CREATE DATABASE db_03093_atomic Engine=Atomic;
+CREATE TABLE test_table_03093  (number UInt64) ENGINE=MergeTree ORDER BY number;
+INSERT INTO test_table_03093 SELECT number FROM system.numbers LIMIT 6;
+DETACH TABLE test_table_03093;
+DROP TABLE test_table_03093 SYNC;
 
-CREATE TABLE db_03093_atomic.test_table  (number UInt64) ENGINE=MergeTree ORDER BY number;
-INSERT INTO db_03093_atomic.test_table SELECT number FROM system.numbers LIMIT 6;
-DETACH TABLE db_03093_atomic.test_table;
-DROP TABLE db_03093_atomic.test_table SYNC;
+CREATE TABLE test_table_03093  (number UInt64) ENGINE=MergeTree ORDER BY number;
+INSERT INTO test_table_03093 SELECT number FROM system.numbers LIMIT 6;
+DETACH TABLE test_table_03093;
+DROP TABLE test_table_03093 SYNC;
 
-CREATE TABLE db_03093_atomic.test_table  (number UInt64) ENGINE=MergeTree ORDER BY number;
-INSERT INTO db_03093_atomic.test_table SELECT number FROM system.numbers LIMIT 6;
-DETACH TABLE db_03093_atomic.test_table;
-DROP TABLE db_03093_atomic.test_table SYNC;
+CREATE TABLE test_table_03093  (number UInt64) ENGINE=MergeTree ORDER BY number;
+INSERT INTO test_table_03093 SELECT number FROM system.numbers LIMIT 6;
+DETACH TABLE test_table_03093 PERMANENTLY;
+DROP TABLE test_table_03093 SYNC;
 
-CREATE TABLE db_03093_atomic.test_table  (number UInt64) ENGINE=MergeTree ORDER BY number;
-INSERT INTO db_03093_atomic.test_table SELECT number FROM system.numbers LIMIT 6;
-DETACH TABLE db_03093_atomic.test_table PERMANENTLY;
-DROP TABLE db_03093_atomic.test_table SYNC;
+CREATE TABLE test_table_03093  (number UInt64) ENGINE=MergeTree ORDER BY number;
+INSERT INTO test_table_03093 SELECT number FROM system.numbers LIMIT 6;
+DETACH TABLE test_table_03093 PERMANENTLY;
+DROP TABLE test_table_03093 SYNC;
 
-CREATE TABLE db_03093_atomic.test_table  (number UInt64) ENGINE=MergeTree ORDER BY number;
-INSERT INTO db_03093_atomic.test_table SELECT number FROM system.numbers LIMIT 6;
-DETACH TABLE db_03093_atomic.test_table PERMANENTLY;
-DROP TABLE db_03093_atomic.test_table SYNC;
+CREATE TABLE test_table_03093  (number UInt64) ENGINE=ReplacingMergeTree() ORDER BY number;
+INSERT INTO test_table_03093 SELECT number FROM system.numbers LIMIT 6;
+DETACH TABLE test_table_03093 PERMANENTLY;
+DROP TABLE test_table_03093 SYNC;
 
-CREATE TABLE db_03093_atomic.test_table  (number UInt64) ENGINE=ReplacingMergeTree() ORDER BY number;
-INSERT INTO db_03093_atomic.test_table SELECT number FROM system.numbers LIMIT 6;
-DETACH TABLE db_03093_atomic.test_table PERMANENTLY;
-DROP TABLE db_03093_atomic.test_table SYNC;
+CREATE TABLE test_table_03093  (number UInt64) ENGINE=ReplacingMergeTree() ORDER BY number;
+INSERT INTO test_table_03093 SELECT number FROM system.numbers LIMIT 6;
+DETACH TABLE test_table_03093;
+DROP TABLE test_table_03093 SYNC;
 
-CREATE TABLE db_03093_atomic.test_table  (number UInt64) ENGINE=ReplacingMergeTree() ORDER BY number;
-INSERT INTO db_03093_atomic.test_table SELECT number FROM system.numbers LIMIT 6;
-DETACH TABLE db_03093_atomic.test_table;
-DROP TABLE db_03093_atomic.test_table SYNC;
+CREATE TABLE test_table_03093  (number UInt64) ENGINE=SummingMergeTree ORDER BY number;
+INSERT INTO test_table_03093 SELECT number FROM system.numbers LIMIT 6;
+DETACH TABLE test_table_03093 PERMANENTLY;
+DROP TABLE test_table_03093 SYNC;
 
-CREATE TABLE db_03093_atomic.test_table  (number UInt64) ENGINE=SummingMergeTree ORDER BY number;
-INSERT INTO db_03093_atomic.test_table SELECT number FROM system.numbers LIMIT 6;
-DETACH TABLE db_03093_atomic.test_table PERMANENTLY;
-DROP TABLE db_03093_atomic.test_table SYNC;
+CREATE TABLE test_table_03093  (number UInt64) ENGINE=SummingMergeTree ORDER BY number;
+INSERT INTO test_table_03093 SELECT number FROM system.numbers LIMIT 6;
+DETACH TABLE test_table_03093;
+DROP TABLE test_table_03093 SYNC;
 
-CREATE TABLE db_03093_atomic.test_table  (number UInt64) ENGINE=SummingMergeTree ORDER BY number;
-INSERT INTO db_03093_atomic.test_table SELECT number FROM system.numbers LIMIT 6;
-DETACH TABLE db_03093_atomic.test_table;
-DROP TABLE db_03093_atomic.test_table SYNC;
+CREATE TABLE test_table_03093  (number UInt64) ENGINE=AggregatingMergeTree ORDER BY number;
+INSERT INTO test_table_03093 SELECT number FROM system.numbers LIMIT 6;
+DETACH TABLE test_table_03093 PERMANENTLY;
+DROP TABLE test_table_03093 SYNC;
 
-CREATE TABLE db_03093_atomic.test_table  (number UInt64) ENGINE=AggregatingMergeTree ORDER BY number;
-INSERT INTO db_03093_atomic.test_table SELECT number FROM system.numbers LIMIT 6;
-DETACH TABLE db_03093_atomic.test_table PERMANENTLY;
-DROP TABLE db_03093_atomic.test_table SYNC;
+CREATE TABLE test_table_03093  (number UInt64) ENGINE=AggregatingMergeTree ORDER BY number;
+INSERT INTO test_table_03093 SELECT number FROM system.numbers LIMIT 6;
+DETACH TABLE test_table_03093;
+DROP TABLE test_table_03093 SYNC;
 
-CREATE TABLE db_03093_atomic.test_table  (number UInt64) ENGINE=AggregatingMergeTree ORDER BY number;
-INSERT INTO db_03093_atomic.test_table SELECT number FROM system.numbers LIMIT 6;
-DETACH TABLE db_03093_atomic.test_table;
-DROP TABLE db_03093_atomic.test_table SYNC;
+CREATE TABLE test_table_03093  (number UInt64, sign Int8) ENGINE=CollapsingMergeTree(sign) ORDER BY number;
+INSERT INTO test_table_03093 SELECT number, 1 FROM system.numbers LIMIT 6;
+DETACH TABLE test_table_03093 PERMANENTLY;
+DROP TABLE test_table_03093 SYNC;
 
-CREATE TABLE db_03093_atomic.test_table  (number UInt64, sign Int8) ENGINE=CollapsingMergeTree(sign) ORDER BY number;
-INSERT INTO db_03093_atomic.test_table SELECT number, 1 FROM system.numbers LIMIT 6;
-DETACH TABLE db_03093_atomic.test_table PERMANENTLY;
-DROP TABLE db_03093_atomic.test_table SYNC;
+CREATE TABLE test_table_03093  (number UInt64, sign Int8) ENGINE=CollapsingMergeTree(sign) ORDER BY number;
+INSERT INTO test_table_03093 SELECT number, 1 FROM system.numbers LIMIT 6;
+DETACH TABLE test_table_03093;
+DROP TABLE test_table_03093 SYNC;
 
-CREATE TABLE db_03093_atomic.test_table  (number UInt64, sign Int8) ENGINE=CollapsingMergeTree(sign) ORDER BY number;
-INSERT INTO db_03093_atomic.test_table SELECT number, 1 FROM system.numbers LIMIT 6;
-DETACH TABLE db_03093_atomic.test_table;
-DROP TABLE db_03093_atomic.test_table SYNC;
+CREATE TABLE test_table_03093  (number UInt64, sign Int8, version Int32) ENGINE=VersionedCollapsingMergeTree(sign, version) ORDER BY number;
+INSERT INTO test_table_03093 SELECT number, 1, 1 FROM system.numbers LIMIT 6;
+DETACH TABLE test_table_03093 PERMANENTLY;
+DROP TABLE test_table_03093 SYNC;
 
-CREATE TABLE db_03093_atomic.test_table  (number UInt64, sign Int8, version Int32) ENGINE=VersionedCollapsingMergeTree(sign, version) ORDER BY number;
-INSERT INTO db_03093_atomic.test_table SELECT number, 1, 1 FROM system.numbers LIMIT 6;
-DETACH TABLE db_03093_atomic.test_table PERMANENTLY;
-DROP TABLE db_03093_atomic.test_table SYNC;
-
-CREATE TABLE db_03093_atomic.test_table  (number UInt64, sign Int8, version Int32) ENGINE=VersionedCollapsingMergeTree(sign, version) ORDER BY number;
-INSERT INTO db_03093_atomic.test_table SELECT number, 1, 1 FROM system.numbers LIMIT 6;
-DETACH TABLE db_03093_atomic.test_table;
-DROP TABLE db_03093_atomic.test_table SYNC;
-DROP DATABASE db_03093_atomic SYNC;
+CREATE TABLE test_table_03093  (number UInt64, sign Int8, version Int32) ENGINE=VersionedCollapsingMergeTree(sign, version) ORDER BY number;
+INSERT INTO test_table_03093 SELECT number, 1, 1 FROM system.numbers LIMIT 6;
+DETACH TABLE test_table_03093;
+DROP TABLE test_table_03093 SYNC;
