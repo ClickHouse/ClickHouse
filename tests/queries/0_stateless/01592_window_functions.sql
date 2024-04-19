b38476840cf44e7022c67be62c73847d578b9469
lgbo-ustc <lgbo.ustc@gmail.com>
2024-04-19 14:42:24 +0800

    update

--- tests/queries/0_stateless/01592_window_functions.sql
+++ tests/queries/0_stateless/01592_window_functions.sql
@@ -109,14 +109,14 @@ FROM
         product_name,
         group_name,
         price,
-		rank() OVER (PARTITION BY group_name ORDER BY price ASC) AS rank,
+        rank() OVER (PARTITION BY group_name ORDER BY price ASC) AS rank,
         percent_rank() OVER (PARTITION BY group_name ORDER BY price ASC) AS percent
     FROM products
     INNER JOIN product_groups USING (group_id)
 ) AS t
 ORDER BY
     group_name ASC,
-	price ASC,
+    price ASC,
     product_name ASC;
 
 drop table product_groups;
