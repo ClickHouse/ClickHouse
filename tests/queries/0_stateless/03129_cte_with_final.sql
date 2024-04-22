298206c0af5cd12c1cadfb620576a2ad71ae6857
Duc Canh Le <duccanh.le@ahrefs.com>
2024-04-22 01:27:44 +0000

    minor fix in test
    Signed-off-by: Duc Canh Le <duccanh.le@ahrefs.com>

--- tests/queries/0_stateless/03129_cte_with_final.sql
+++ tests/queries/0_stateless/03129_cte_with_final.sql
@@ -1,4 +1,6 @@
-CREATE OR REPLACE TABLE test
+DROP TABLE IF EXISTS t;
+
+CREATE TABLE t
 (
     `key` Int64,
     `someCol` String,
@@ -7,16 +9,18 @@ CREATE OR REPLACE TABLE test
 ENGINE = ReplacingMergeTree(eventTime)
 ORDER BY key;
 
-INSERT INTO test Values (1, 'first', '2024-04-19 01:01:01');
-INSERT INTO test Values (1, 'first', '2024-04-19 01:01:01');
+INSERT INTO t Values (1, 'first', '2024-04-19 01:01:01');
+INSERT INTO t Values (1, 'first', '2024-04-19 01:01:01');
 
 EXPLAIN QUERY TREE passes=1
 WITH merged_test AS(
-	SELECT * FROM  test Final
+	SELECT * FROM  t Final
 )
 SELECT * FROM  merged_test;
 
 WITH merged_test AS(
-	SELECT * FROM  test Final
+	SELECT * FROM  t Final
 )
 SELECT * FROM  merged_test;
+
+DROP TABLE t;
