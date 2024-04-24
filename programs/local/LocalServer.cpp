bf3d33b61c6a8a7991eca06f86c686434e30f49a
Nikolay Degterinsky <evillique@gmail.com>
2024-04-24 21:08:49 +0000

    Fix data race

--- programs/local/LocalServer.cpp
+++ programs/local/LocalServer.cpp
@@ -469,7 +469,6 @@ try
     registerFormats();
 
     processConfig();
-    adjustSettings();
     initTTYBuffer(toProgressOption(config().getString("progress", "default")));
     ASTAlterCommand::setFormatAlterCommandsWithParentheses(true);
 
@@ -676,6 +675,7 @@ void LocalServer::processConfig()
     /// NOTE: it is important to apply any overrides before
     /// setDefaultProfiles() calls since it will copy current context (i.e.
     /// there is separate context for Buffer tables).
+    adjustSettings();
     applySettingsOverridesForLocal(global_context);
     applyCmdOptions(global_context);
 
