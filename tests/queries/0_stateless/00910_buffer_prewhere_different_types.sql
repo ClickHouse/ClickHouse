fc40098460c333f722ed92f6831b6437f4f384e6
Nikolai Kochetov <nk@clickhouse.com>
2024-04-24 18:06:55 +0200

    Update tests/queries/0_stateless/00910_buffer_prewhere_different_types.sql
    Co-authored-by: Alexander Gololobov <davenger@clickhouse.com>

--- tests/queries/0_stateless/00910_buffer_prewhere_different_types.sql
+++ tests/queries/0_stateless/00910_buffer_prewhere_different_types.sql
@@ -9,3 +9,7 @@ SET send_logs_level='error';
 CREATE TABLE buffer_table1__fuzz_28 (`x` Nullable(UInt32)) ENGINE = Buffer(currentDatabase(), 'merge_tree_table1', 16, 10, 60, 10, 1000, 1048576, 2097152);
 
 SELECT * FROM buffer_table1__fuzz_28 PREWHERE x = toLowCardinality(1);
+
+CREATE ROW POLICY rp ON buffer_table1__fuzz_28 FOR SELECT USING x = toLowCardinality(1) TO default;
+
+SELECT * FROM buffer_table1__fuzz_28;
