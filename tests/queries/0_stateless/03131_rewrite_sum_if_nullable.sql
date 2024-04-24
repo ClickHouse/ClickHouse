bff2f2db65c3a9a7083c563a2449c5aa2dd61c01
Raúl Marín <git@rmr.ninja>
2024-04-24 16:20:03 +0200

    Add more tests

--- tests/queries/0_stateless/03131_rewrite_sum_if_nullable.sql
+++ tests/queries/0_stateless/03131_rewrite_sum_if_nullable.sql
@@ -1,5 +1,15 @@
 -- https://github.com/ClickHouse/ClickHouse/issues/62890
+-- { echoOn }
 SELECT sum(if(materialize(0), toNullable(1), 0));
 SELECT sum(if(dummy, 0, toNullable(0)));
-
 SELECT sum(if(s == '', v, 0)) b from VALUES ('v Nullable(Int64), s String',(1, 'x'));
+
+SELECT sumOrNull(if(materialize(0), toNullable(1), 0));
+SELECT sumOrNull(if(materialize(0), 1, 0));
+SELECT sum(if(materialize(0), toNullable(1), 0)) settings aggregate_functions_null_for_empty=1;
+SELECT sum(if(materialize(0), 1, 0)) settings aggregate_functions_null_for_empty=1;
+
+SELECT sumOrNull(if(materialize(1), toNullable(1), 10));
+SELECT sumOrNull(if(materialize(1), 1, 10));
+SELECT sum(if(materialize(1), toNullable(1), 10)) settings aggregate_functions_null_for_empty=1;
+SELECT sum(if(materialize( 1), 1, 10)) settings aggregate_functions_null_for_empty=1;
