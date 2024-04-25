ad78591269991aa600846bc678a0af26b18e592b
Arthur Passos <arthur.ti@outlook.com>
2024-04-25 18:08:30 -0300

    update docs

--- tests/queries/0_stateless/03036_test_parquet_bloom_filter_push_down.sh
+++ tests/queries/0_stateless/03036_test_parquet_bloom_filter_push_down.sh
@@ -10,28 +10,64 @@ CUR_DIR=$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)
 # 1. create file with pyarrow
 # 2. create bloom filters with pyspark
 #
-# import pyarrow as pa
-# from pyarrow import parquet as pq
-# import numpy as np
-# np.random.seed(0)
-# table = pa.Table.from_pydict({
-#   'f32': np.random.normal(size=100000).astype(np.float32),
-#   'f64': np.random.normal(size=100000).astype(np.float64),
-# })
+#import pyarrow as pa
+#from pyarrow import parquet as pq
+#import numpy as np
+#import pyarrow.compute as pc
+#import random
+#import string
 #
-# pq.write_table(
-#   table,
-#   'simple-fd.parquet'
-# )
-# #-
+## Set the random seed for reproducibility
+#np.random.seed(0)
 #
-# import pyspark
-# from pyspark.sql import SparkSession
-# spark=SparkSession.builder.appName("parquetFile").config("spark.driver.memory", "45g").getOrCreate()
+#size = 3000
 #
-# house_df = spark.read.parquet('simple-fd.parquet')
+## Generating float columns
+#f32_data = np.random.normal(size=size).astype(np.float32)
+#f64_data = np.random.normal(size=size).astype(np.float64)
 #
-# house_df.coalesce(1).write.mode('overwrite').option('parquet.bloom.filter.enabled#f32', "true").option('parquet.bloom.filter.enabled#f64', "true").option("parquet.block.size", 1024*1024).parquet("simple-fd-bf.parquet")
+## Generating integer columns
+#int_data = np.random.randint(low=0, high=10000, size=size, dtype='int32')
+#
+## Generating string columns
+#str_data = np.array([''.join(random.choices(string.ascii_uppercase, k=np.random.randint(0, 5))) for i in range(size)])
+#
+#fixed_str_data = np.array([''.join(random.choices(string.ascii_uppercase, k=4)) for i in range(size)])
+#
+## Generating array columns (list of integers)
+#array_data = pa.array([np.random.randint(0, 100, size=5).tolist() for _ in range(size)])
+#
+## Create a table with the generated data
+#table = pa.Table.from_arrays(
+#    [
+#        pa.array(f32_data, pa.float32()),
+#        pa.array(f64_data, pa.float64()),
+#        pa.array(int_data, pa.int32()),
+#        pa.array(str_data, pa.string()),
+#        pa.array(fixed_str_data, pa.string()),
+#        array_data
+#    ],
+#    names=[
+#        'f32',
+#        'f64',
+#        'int',
+#        'str',
+#        'fixed_str',
+#        'array'
+#    ]
+#)
+#
+## Write the table to a Parquet file
+#pq.write_table(table, 'enhanced-fd.parquet')
+# ----------------
+#
+#import pyspark
+#from pyspark.sql import SparkSession
+#spark=SparkSession.builder.appName("parquetFile").config("spark.driver.memory", "45g").getOrCreate()
+#
+#house_df = spark.read.parquet('enhanced-fd.parquet')
+#
+#house_df.coalesce(1).write.mode('overwrite').option('parquet.bloom.filter.enabled#f32', "true").option('parquet.bloom.filter.enabled#f64', "true").option('parquet.bloom.filter.enabled#str', "true").option('parquet.bloom.filter.enabled#fixed_str', "true").option('parquet.bloom.filter.enabled#item', "true").option("parquet.block.size", 1024*50).parquet("enhanced", compression='gzip')
 
 multi_column_filename="multi_column_bf.parquet"
 
