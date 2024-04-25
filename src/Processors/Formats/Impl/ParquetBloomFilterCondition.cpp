d6834bd41864d8a6cc6f83887fa35a51de2ff65c
Arthur Passos <arthur.ti@outlook.com>
2024-04-25 16:33:22 -0300

    try to fix defines

--- src/Processors/Formats/Impl/ParquetBloomFilterCondition.cpp
+++ src/Processors/Formats/Impl/ParquetBloomFilterCondition.cpp
@@ -14,9 +14,8 @@ namespace DB
 namespace ErrorCodes
 {
     extern const int BAD_ARGUMENTS;
-    extern const int ILLEGAL_COLUMN;
-    extern const int INCORRECT_QUERY;
-    extern const int NUMBER_OF_ARGUMENTS_DOESNT_MATCH;
+    extern const int LOGICAL_ERROR;
+    extern const int ILLEGAL_TYPE_OF_ARGUMENT;
 }
 
 namespace
