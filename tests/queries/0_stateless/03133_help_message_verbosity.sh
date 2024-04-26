ebad7062a8f8b6346a302f952ef0f178e9889855
Yarik Briukhovetskyi <114298166+yariks5s@users.noreply.github.com>
2024-04-26 20:31:26 +0200

    un-flaky the test

--- tests/queries/0_stateless/03133_help_message_verbosity.sh
+++ tests/queries/0_stateless/03133_help_message_verbosity.sh
@@ -5,13 +5,16 @@ CUR_DIR=$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)
 # shellcheck source=../shell_config.sh
 . "$CUR_DIR"/../shell_config.sh
 
-# Get the help message in short and vebose form and put them into txt files
-$CLICKHOUSE_CLIENT --help > help_msg.txt
-$CLICKHOUSE_CLIENT --help --verbose > verbose_help_msg.txt
+# Unique identifier for concurrent execution
+PID=$$
+
+# Get the help message in short and verbose form and put them into txt files
+$CLICKHOUSE_CLIENT --help > "help_msg_$PID.txt"
+$CLICKHOUSE_CLIENT --help --verbose > "verbose_help_msg_$PID.txt"
 
 # Sizes of files
-size_short=$(stat -c %s help_msg.txt)
-size_verbose=$(stat -c %s verbose_help_msg.txt)
+size_short=$(stat -c %s "help_msg_$PID.txt")
+size_verbose=$(stat -c %s "verbose_help_msg_$PID.txt")
 
 # If the size of the short help message is less, everything is OK
 if [ $size_short -lt $size_verbose ]; then
@@ -20,16 +23,15 @@ else
   echo "Not OK"
 fi
 
-rm help_msg.txt
-rm verbose_help_msg.txt
-
+rm "help_msg_$PID.txt"
+rm "verbose_help_msg_$PID.txt"
 
 # The same for clickhouse local
-$CLICKHOUSE_LOCAL --help > help_msg.txt
-$CLICKHOUSE_LOCAL --help --verbose > verbose_help_msg.txt
+$CLICKHOUSE_LOCAL --help > "help_msg_$PID.txt"
+$CLICKHOUSE_LOCAL --help --verbose > "verbose_help_msg_$PID.txt"
 
-size_short=$(stat -c %s help_msg.txt)
-size_verbose=$(stat -c %s verbose_help_msg.txt)
+size_short=$(stat -c %s "help_msg_$PID.txt")
+size_verbose=$(stat -c %s "verbose_help_msg_$PID.txt")
 
 if [ $size_short -lt $size_verbose ]; then
   echo "OK"
@@ -37,5 +39,5 @@ else
   echo "Not OK"
 fi
 
-rm help_msg.txt
-rm verbose_help_msg.txt
+rm "help_msg_$PID.txt"
+rm "verbose_help_msg_$PID.txt"
