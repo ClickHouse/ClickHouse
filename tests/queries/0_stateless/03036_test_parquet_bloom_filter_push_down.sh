af546f30ef3dac326409201c30bff3ce8378ee1f
Arthur Passos <arthur.ti@outlook.com>
2024-04-27 09:28:36 -0300

    make test file reproducible, add bloom filter to the array column and implement in and has conditions

--- tests/queries/0_stateless/03036_test_parquet_bloom_filter_push_down.sh
+++ tests/queries/0_stateless/03036_test_parquet_bloom_filter_push_down.sh
@@ -5,92 +5,87 @@ CUR_DIR=$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)
 # shellcheck source=../shell_config.sh
 . "$CUR_DIR"/../shell_config.sh
 
-# File generated in two steps (why two steps? I don't remember):
-#
-# 1. create file with pyarrow
-# 2. create bloom filters with pyspark
-#
-#import pyarrow as pa
-#from pyarrow import parquet as pq
-#import numpy as np
-#import pyarrow.compute as pc
-#import random
-#import string
-#
-## Set the random seed for reproducibility
-#np.random.seed(0)
-#
-#size = 3000
-#
-## Generating float columns
-#f32_data = np.random.normal(size=size).astype(np.float32)
-#f64_data = np.random.normal(size=size).astype(np.float64)
-#
-## Generating integer columns
-#int_data = np.random.randint(low=0, high=10000, size=size, dtype='int32')
-#
-## Generating string columns
-#str_data = np.array([''.join(random.choices(string.ascii_uppercase, k=np.random.randint(0, 5))) for i in range(size)])
-#
-#fixed_str_data = np.array([''.join(random.choices(string.ascii_uppercase, k=4)) for i in range(size)])
-#
-## Generating array columns (list of integers)
-#array_data = pa.array([np.random.randint(0, 100, size=5).tolist() for _ in range(size)])
-#
-## Create a table with the generated data
-#table = pa.Table.from_arrays(
-#    [
-#        pa.array(f32_data, pa.float32()),
-#        pa.array(f64_data, pa.float64()),
-#        pa.array(int_data, pa.int32()),
-#        pa.array(str_data, pa.string()),
-#        pa.array(fixed_str_data, pa.string()),
-#        array_data
-#    ],
-#    names=[
-#        'f32',
-#        'f64',
-#        'int',
-#        'str',
-#        'fixed_str',
-#        'array'
-#    ]
-#)
-#
-## Write the table to a Parquet file
-#pq.write_table(table, 'enhanced-fd.parquet')
-# ----------------
-#
-#import pyspark
+# File generated with below script
 #from pyspark.sql import SparkSession
-#spark=SparkSession.builder.appName("parquetFile").config("spark.driver.memory", "45g").getOrCreate()
+#from pyspark.sql.types import StructType, StructField, FloatType, DoubleType, StringType, IntegerType, ArrayType
+#import random
 #
-#house_df = spark.read.parquet('enhanced-fd.parquet')
+#spark = SparkSession.builder \
+#    .appName("High Cardinality Parquet File Creator with Bloom Filters") \
+#    .config("spark.sql.parquet.enable.bloom.filter", "true") \
+#    .config("spark.driver.memory", "2g") \
+#    .getOrCreate()
 #
-#house_df.coalesce(1).write.mode('overwrite').option('parquet.bloom.filter.enabled#f32', "true").option('parquet.bloom.filter.enabled#f64', "true").option('parquet.bloom.filter.enabled#str', "true").option('parquet.bloom.filter.enabled#fixed_str', "true").option('parquet.bloom.filter.enabled#item', "true").option("parquet.block.size", 1024*50).parquet("enhanced", compression='gzip')
+#schema = StructType([
+#    StructField("f32", FloatType(), True),
+#    StructField("f64", DoubleType(), True),
+#    StructField("int", IntegerType(), True),
+#    StructField("str", StringType(), True),
+#    StructField("fixed_str", StringType(), True),
+#    StructField("array", ArrayType(IntegerType()), True)
+#])
+#
+#random.seed(42)
+#
+#def generate_data(num_rows):
+#    data = []
+#    for _ in range(num_rows):
+#        f32 = random.uniform(-100, 100)
+#        f64 = random.uniform(-100, 100)
+#        int_value = random.randint(1, 100)
+#        str_value = ''.join(random.choices('ABCDEFGJKLYZ', k=5))
+#        fixed_str = ''.join(random.choices('ABCDNOUVWXYZ', k=4))
+#        array = [random.randint(1, 50) for _ in range(5)]
+#        data.append((f32, f64, int_value, str_value, fixed_str, array))
+#    return data
+#
+#data = generate_data(3000)
+#
+#df = spark.createDataFrame(data, schema)
+#
+#df.coalesce(1).write.mode('overwrite')\
+#    .option("parquet.bloom.filter.enabled", "true")\
+#    .option("parquet.block.size", 1024*70)\
+#    .option("parquet.bloom.filter.enabled#int", "false")\
+#    .parquet("output.parquet", compression="gzip")
+#
+#spark.stop()
 
-multi_column_filename="multi_column_bf.parquet"
 
-${CLICKHOUSE_CLIENT} --query="select count(*) from file('${multi_column_filename}', Parquet) SETTINGS use_cache_for_count_from_files=false;"
+USER_FILES_PATH=$($CLICKHOUSE_CLIENT_BINARY --query "select _path,_file from file('nonexist.txt', 'CSV', 'val1 char')" 2>&1 | grep Exception | awk '{gsub("/nonexist.txt","",$9); print $9}')
+
+WORKING_DIR="${USER_FILES_PATH}/${CLICKHOUSE_TEST_UNIQUE_NAME}"
+
+mkdir -p "${WORKING_DIR}"
+
+DATA_FILE="${CUR_DIR}/data_parquet/multi_column_bf.gz.parquet"
+
+DATA_FILE_USER_PATH="${WORKING_DIR}/multi_column_bf.gz.parquet"
+
+cp ${DATA_FILE} ${DATA_FILE_USER_PATH}
+
+${CLICKHOUSE_CLIENT} --query="select count(*) from file('${DATA_FILE_USER_PATH}', Parquet) SETTINGS use_cache_for_count_from_files=false;"
 
 # bloom filter is off, all row groups should be read
 # expect rows_read = select count()
-${CLICKHOUSE_CLIENT} --query="select * from file('${multi_column_filename}', Parquet) where f32=toFloat32(1.7640524) and fixed_str='GFZI' or str='OTDY' order by f32 asc FORMAT Json SETTINGS input_format_parquet_bloom_filter_push_down=false, input_format_parquet_filter_push_down=false;"  | jq 'del(.meta,.statistics.elapsed)'
+${CLICKHOUSE_CLIENT} --query="select * from file('${DATA_FILE_USER_PATH}', Parquet) where f32=toFloat32(-64.12787) and fixed_str='BYYC' or str='KCGEY' order by f32 asc FORMAT Json SETTINGS input_format_parquet_bloom_filter_push_down=false, input_format_parquet_filter_push_down=false;"  | jq 'del(.meta,.statistics.elapsed)'
 
 # bloom filter is on, some row groups should be skipped
 # expect rows_read much less than select count()
-${CLICKHOUSE_CLIENT} --query="select * from file('${multi_column_filename}', Parquet) where f32=toFloat32(1.7640524) and fixed_str='GFZI' or str='OTDY' order by f32 asc FORMAT Json SETTINGS input_format_parquet_bloom_filter_push_down=true, input_format_parquet_filter_push_down=false;"  | jq 'del(.meta,.statistics.elapsed)'
+${CLICKHOUSE_CLIENT} --query="select * from file('${DATA_FILE_USER_PATH}', Parquet) where f32=toFloat32(-64.12787) and fixed_str='BYYC' or str='KCGEY' order by f32 asc FORMAT Json SETTINGS input_format_parquet_bloom_filter_push_down=true, input_format_parquet_filter_push_down=false;"  | jq 'del(.meta,.statistics.elapsed)'
+
+# bloom filter is on, but where predicate contains data from 2 row groups out of 3.
+# Rows read should be less than select count, by greater than previous selects
+${CLICKHOUSE_CLIENT} --query="select * from file('${DATA_FILE_USER_PATH}', Parquet) where f32=toFloat32(-64.12787) or f32=toFloat32(-95.748695)  order by f32 asc Format JSON SETTINGS input_format_parquet_bloom_filter_push_down=true, input_format_parquet_filter_push_down=false;" | jq 'del(.meta,.statistics.elapsed)'
+
+# bloom filter is on, but where predicate contains data from all row groups
+# expect rows_read = select count()
+${CLICKHOUSE_CLIENT} --query="select * from file('${DATA_FILE_USER_PATH}', Parquet) where f32=toFloat32(-64.12787) or f32=toFloat32(-95.748695) or f32=toFloat32(-42.54674) order by f32 asc Format JSON SETTINGS input_format_parquet_bloom_filter_push_down=true, input_format_parquet_filter_push_down=false;" | jq 'del(.meta,.statistics.elapsed)'
 
 # IN check
-${CLICKHOUSE_CLIENT} --query="select * from file('${multi_column_filename}', Parquet) where f32=toFloat32(1.7640524) and fixed_str in ('GFZI', 'OTDY') order by f32 asc FORMAT Json SETTINGS input_format_parquet_bloom_filter_push_down=true, input_format_parquet_filter_push_down=false;"  | jq 'del(.meta,.statistics.elapsed)'
+${CLICKHOUSE_CLIENT} --query="select * from file('${DATA_FILE_USER_PATH}', Parquet) where f32=toFloat32(-15.910733) and fixed_str in ('BYYC', 'DCXV') order by f32 asc FORMAT Json SETTINGS input_format_parquet_bloom_filter_push_down=true, input_format_parquet_filter_push_down=false;"  | jq 'del(.meta,.statistics.elapsed)'
 
 # IN check for floats
-${CLICKHOUSE_CLIENT} --query="select * from file('${multi_column_filename}', Parquet) where f32 in (toFloat32(1.7640524), toFloat32(0.09082593)) or fixed_str = 'RIJV' order by f32 asc FORMAT Json SETTINGS input_format_parquet_bloom_filter_push_down=true, input_format_parquet_filter_push_down=false;"  | jq 'del(.meta,.statistics.elapsed)'
+${CLICKHOUSE_CLIENT} --query="select * from file('${DATA_FILE_USER_PATH}', Parquet) where f64 in (toFloat64(22.89182051713945), toFloat64(68.62704389505595)) order by f32 asc FORMAT Json SETTINGS input_format_parquet_bloom_filter_push_down=true, input_format_parquet_filter_push_down=false;"  | jq 'del(.meta,.statistics.elapsed)'
 
-# bloom filter is on and it is filtering by data in the other row group, which is bigger than the former. Expect most row groups to be read, but not all
-# expect rows_read less than select count(), but higher than previous query
-${CLICKHOUSE_CLIENT} --query="select * from file('multi_column_bf.parquet', Parquet) where str='PFRI' or str='ES' order by f32 asc Format JSON SETTINGS input_format_parquet_bloom_filter_push_down=true, input_format_parquet_filter_push_down=false;" | jq 'del(.meta,.statistics.elapsed)'
-
-# bloom filter is on, but includes data from both (all row groups), all row groups should be read
-# expect rows_read = select count()
-${CLICKHOUSE_CLIENT} --query="select * from file('multi_column_bf.parquet', Parquet) where str='PFRI' or str='ES' or str='OTDY' or fixed_str='GFZI' order by f32 asc Format JSON SETTINGS input_format_parquet_bloom_filter_push_down=true, input_format_parquet_filter_push_down=false;" | jq 'del(.meta,.statistics.elapsed)'
+rm -rf ${USER_FILES_PATH}/${CLICKHOUSE_TEST_UNIQUE_NAME:?}/*
