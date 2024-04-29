490a0cd6e726c8e38bae5a809f24cdafa11983ed
Andrei Ivanov <ivanov17andrey@gmail.com>
2024-04-29 19:04:11 +0300

    fix

--- src/Databases/removeWhereConditionPlaceholder.cpp
+++ src/Databases/removeWhereConditionPlaceholder.cpp
@@ -3,19 +3,23 @@
 namespace DB
 {
 
-std::string removeWhereConditionPlaceholder(const std::string &query)
+std::string removeWhereConditionPlaceholder(const std::string & query)
 {
     static constexpr auto true_condition = "(1 = 1)";
-    std::string modified_query = query;
-    size_t condition_position = modified_query.find(CONDITION_PLACEHOLDER_TO_REPLACE_VALUE);
+    auto condition_position = query.find(CONDITION_PLACEHOLDER_TO_REPLACE_VALUE);
 
+    // We use a mutable copy of the query to perform replacements
+    auto query_copy = query;
+
+    // Loop until no placeholders are found
     while (condition_position != std::string::npos)
     {
-        modified_query.replace(condition_position, CONDITION_PLACEHOLDER_TO_REPLACE_VALUE.size(), true_condition);
-        condition_position = modified_query.find(CONDITION_PLACEHOLDER_TO_REPLACE_VALUE, condition_position + true_condition.size());
+        query_copy.replace(condition_position, CONDITION_PLACEHOLDER_TO_REPLACE_VALUE.size(), true_condition);
+        // Update the position for the next occurrence
+        condition_position = query_copy.find(CONDITION_PLACEHOLDER_TO_REPLACE_VALUE, condition_position + true_condition.size());
     }
 
-    return modified_query;
+    return query_copy;
 }
 
 }
