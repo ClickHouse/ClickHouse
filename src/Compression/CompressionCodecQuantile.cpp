bbe2ca718e856a83869ce8f1d2d5db39573fb1ee
Roman Golovachev <golovachyov2003@yandex.ru>
2024-04-29 11:46:49 +0300

    mark quantile codec as experimental

--- src/Compression/CompressionCodecQuantile.cpp
+++ src/Compression/CompressionCodecQuantile.cpp
@@ -38,6 +38,7 @@ protected:
     bool isCompression() const override { return true; }
     bool isGenericCompression() const override { return false; }
     bool isFloatingPointTimeSeriesCodec() const override { return true; }
+    bool isExperimental() const override { return true; }
 
 private:
     static constexpr UInt32 HEADER_SIZE = 2;
