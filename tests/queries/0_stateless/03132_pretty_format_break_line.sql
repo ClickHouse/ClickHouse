43b9ba08995c48f36f7881b2f80ecf0900ba6b0f
Volodya <vova.giro2000@gmail.com>
2024-04-29 19:21:56 +0000

    order by

--- tests/queries/0_stateless/03132_pretty_format_break_line.sql
+++ tests/queries/0_stateless/03132_pretty_format_break_line.sql
@@ -12,7 +12,7 @@ SELECT * FROM t_break_line FORMAT PrettySpace SETTINGS output_format_pretty_row_
 SELECT * FROM t_break_line FORMAT PrettySpace;
 
 INSERT INTO t_break_line VALUES(1, 'hello world', 'hello\nworld');
-SELECT * FROM t_break_line FORMAT PrettyMonoBlock SETTINGS output_format_pretty_row_numbers = 0;
-SELECT * FROM t_break_line FORMAT PrettyMonoBlock;
+SELECT * FROM t_break_line ORDER BY id FORMAT PrettyMonoBlock SETTINGS output_format_pretty_row_numbers = 0;
+SELECT * FROM t_break_line ORDER BY id FORMAT PrettyMonoBlock;
 
 DROP TABLE t_break_line;
