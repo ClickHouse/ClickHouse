a4d811eb1b7bdf0b7eb95d4ec085b55a34b55f2f
Nikita Taranov <nikita.taranov@clickhouse.com>
2024-04-30 20:46:03 +0100

    fix tests 4

--- tests/queries/0_stateless/03131_deprecated_functions.sql
+++ tests/queries/0_stateless/03131_deprecated_functions.sql
@@ -1,8 +1,8 @@
-SELECT number, neighbor(number, 2) FROM system.numbers LIMIT 10; -- { serverError 720 }
+SELECT number, neighbor(number, 2) FROM system.numbers LIMIT 10; -- { serverError 721 }
 
-SELECT runningDifference(number) FROM system.numbers LIMIT 10; -- { serverError 720 }
+SELECT runningDifference(number) FROM system.numbers LIMIT 10; -- { serverError 721 }
 
-SELECT k, runningAccumulate(sum_k) AS res FROM (SELECT number as k, sumState(k) AS sum_k FROM numbers(10) GROUP BY k ORDER BY k); -- { serverError 720 }
+SELECT k, runningAccumulate(sum_k) AS res FROM (SELECT number as k, sumState(k) AS sum_k FROM numbers(10) GROUP BY k ORDER BY k); -- { serverError 721 }
 
 SET allow_deprecated_functions=1;
 
