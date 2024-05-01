b8e437654deff2a6932fb866bb263e63321d6c16
Mikhail Artemenko <michicosun@ya.ru>
2024-05-01 11:04:30 +0000

    improve order test

--- tests/queries/0_stateless/02952_streaming_merge_tree_order.sh
+++ tests/queries/0_stateless/02952_streaming_merge_tree_order.sh
@@ -18,19 +18,21 @@ insert_opts=(
 
 $CLICKHOUSE_CLIENT "${opts[@]}" -q "DROP TABLE IF EXISTS t_streaming_test"
 $CLICKHOUSE_CLIENT "${opts[@]}" -q "CREATE TABLE t_streaming_test (a String, b UInt64) ENGINE = MergeTree ORDER BY a SETTINGS queue_mode=1"
-$CLICKHOUSE_CLIENT "${opts[@]}" -q "INSERT INTO t_streaming_test VALUES ('started', 0)"
 
 echo "=== Test Streaming strict fifo order check ==="
-
-# start stream
-read -r fifo_1 pid_1 < <(spawn $CLICKHOUSE_CLIENT "${opts[@]}" -q "SELECT _queue_block_number FROM t_streaming_test STREAM")
-read_until "$fifo_1" "1"
 
 echo "start parallel insert"
 $CLICKHOUSE_CLIENT "${insert_opts[@]}" -q "INSERT INTO t_streaming_test (*) select number, number from numbers(100)" &
 $CLICKHOUSE_CLIENT "${insert_opts[@]}" -q "INSERT INTO t_streaming_test (*) select number, number from numbers(100)" &
 
-read_until "$fifo_1" "201"
+echo "start stream"
+read -r fifo_1 pid_1 < <(spawn $CLICKHOUSE_CLIENT "${opts[@]}" -q "SELECT _queue_block_number FROM t_streaming_test STREAM")
+
+echo "start parallel insert"
+$CLICKHOUSE_CLIENT "${insert_opts[@]}" -q "INSERT INTO t_streaming_test (*) select number, number from numbers(100)" &
+$CLICKHOUSE_CLIENT "${insert_opts[@]}" -q "INSERT INTO t_streaming_test (*) select number, number from numbers(100)" &
+
+read_until "$fifo_1" "400"
 
 # stop reading by killing client job
 cleanup "$fifo_1" "$pid_1"
