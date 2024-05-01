dcbde07b1eda291cd90890e3c0a46167a2682c12
Artem Khromov <artem.a.khromov@gmail.com>
2024-05-01 18:10:01 +0300

    MVP kinda done

--- src/Functions/ggmlEvaluate.cpp
+++ src/Functions/ggmlEvaluate.cpp
@@ -471,6 +471,324 @@ private:
 
         return tokens;
     }
+    // evaluate the transformer
+    //
+    //   - model:     the model
+    //   - n_threads: number of threads to use
+    //   - n_past:    the context size so far
+    //   - embd_inp:  the embeddings of the tokens in the context
+    //   - embd_w:    the predicted logits for the next token
+    //
+    // The GPT-J model requires about 16MB of memory per input token.
+    //
+    bool gptj_eval(
+            const gptj_model & model,
+            const int n_threads,
+            const int n_past,
+            const std::vector<gpt_vocab::id> & embd_inp,
+                std::vector<float>         & embd_w,
+                size_t                     & mem_per_token) const {
+        const int N = static_cast<int>(embd_inp.size());
+
+        const auto & hparams = model.hparams;
+
+        const int n_embd  = hparams.n_embd;
+        const int n_layer = hparams.n_layer;
+        const int n_ctx   = hparams.n_ctx;
+        const int n_head  = hparams.n_head;
+        const int n_vocab = hparams.n_vocab;
+        const int n_rot   = hparams.n_rot;
+
+        static size_t buf_size = 256u*1024*1024;
+        static void * buf = malloc(buf_size);
+
+        if (mem_per_token > 0 && mem_per_token*N > buf_size) {
+            const size_t buf_size_new = static_cast<size_t>(1.1*(mem_per_token*N)); // add 10% to account for ggml object overhead
+            //printf("\n%s: reallocating buffer from %zu to %zu bytes\n", __func__, buf_size, buf_size_new);
+
+            // reallocate
+            buf_size = buf_size_new;
+            buf = realloc(buf, buf_size);
+            if (buf == nullptr) {
+                return false;
+            }
+        }
+
+        struct ggml_init_params params = {
+            /*.mem_size   =*/ buf_size,
+            /*.mem_buffer =*/ buf,
+            /*.no_alloc   =*/ false,
+        };
+
+        struct ggml_context * ctx0 = ggml_init(params);
+        struct ggml_cgraph * gf = ggml_new_graph(ctx0);
+
+        // KQ_pos - contains the positions
+        struct ggml_tensor * KQ_pos = ggml_new_tensor_1d(ctx0, GGML_TYPE_I32, N);
+        int * data = reinterpret_cast<int*>(KQ_pos->data);
+        for (int i = 0; i < N; ++i) {
+            data[i] = n_past + i;
+        }
+
+        struct ggml_tensor * embd = ggml_new_tensor_1d(ctx0, GGML_TYPE_I32, N);
+        memcpy(embd->data, embd_inp.data(), N*ggml_element_size(embd));
+
+        // wte
+        struct ggml_tensor * inpL = ggml_get_rows(ctx0, model.wte, embd);
+
+        for (int il = 0; il < n_layer; ++il) {
+            struct ggml_tensor * cur;
+
+            // norm
+            {
+                cur = ggml_norm(ctx0, inpL, hparams.eps);
+
+                // cur = ln_1_g*cur + ln_1_b
+                cur = ggml_add(ctx0,
+                        ggml_mul(ctx0,
+                            ggml_repeat(ctx0, model.layers[il].ln_1_g, cur),
+                            cur),
+                        ggml_repeat(ctx0, model.layers[il].ln_1_b, cur));
+            }
+
+            struct ggml_tensor * inpSA = cur;
+
+            // self-attention
+            {
+                struct ggml_tensor * Qcur = ggml_rope_inplace(ctx0, ggml_reshape_3d(ctx0, ggml_mul_mat(ctx0, model.layers[il].c_attn_q_proj_w, cur), n_embd/n_head, n_head, N), KQ_pos, n_rot, 0, 0);
+                struct ggml_tensor * Kcur = ggml_rope_inplace(ctx0, ggml_reshape_3d(ctx0, ggml_mul_mat(ctx0, model.layers[il].c_attn_k_proj_w, cur), n_embd/n_head, n_head, N), KQ_pos, n_rot, 0, 0);
+
+                // store key and value to memory
+                {
+                    struct ggml_tensor * Vcur = ggml_transpose(ctx0, ggml_mul_mat(ctx0, model.layers[il].c_attn_v_proj_w, cur));
+
+                    struct ggml_tensor * k = ggml_view_1d(ctx0, model.memory_k, N*n_embd, (ggml_element_size(model.memory_k)*n_embd)*(il*n_ctx + n_past));
+                    struct ggml_tensor * v = ggml_view_2d(ctx0, model.memory_v, N, n_embd,
+                            (   n_ctx)*ggml_element_size(model.memory_v),
+                            (il*n_ctx)*ggml_element_size(model.memory_v)*n_embd + n_past*ggml_element_size(model.memory_v));
+
+                    ggml_build_forward_expand(gf, ggml_cpy(ctx0, Kcur, k));
+                    ggml_build_forward_expand(gf, ggml_cpy(ctx0, Vcur, v));
+                }
+
+                // Q = Qcur.contiguous().view(n_embd/n_head, n_head, N).permute(0, 2, 1, 3)
+                struct ggml_tensor * Q =
+                    ggml_permute(ctx0,
+                            Qcur,
+                            0, 2, 1, 3);
+
+                // K = Kmem.view(n_embd/n_head, n_head, n_past + N).permute(0, 2, 1, 3)
+                struct ggml_tensor * K =
+                    ggml_permute(ctx0,
+                            ggml_reshape_3d(ctx0,
+                                ggml_view_1d(ctx0, model.memory_k, (n_past + N)*n_embd, il*n_ctx*ggml_element_size(model.memory_k)*n_embd),
+                                n_embd/n_head, n_head, n_past + N),
+                            0, 2, 1, 3);
+
+                // K * Q
+                struct ggml_tensor * KQ = ggml_mul_mat(ctx0, K, Q);
+
+                // KQ_scaled = KQ / sqrt(n_embd/n_head)
+                struct ggml_tensor * KQ_scaled =
+                    ggml_scale_inplace(ctx0,
+                            KQ,
+                            1.0f/sqrt(float(n_embd)/n_head));
+
+                // KQ_masked = mask_past(KQ_scaled)
+                struct ggml_tensor * KQ_masked = ggml_diag_mask_inf_inplace(ctx0, KQ_scaled, n_past);
+
+                // KQ = soft_max(KQ_masked)
+                struct ggml_tensor * KQ_soft_max = ggml_soft_max_inplace(ctx0, KQ_masked);
+
+                // V_trans = Vmem.view(n_embd/n_head, n_head, n_past + N).permute(1, 2, 0, 3).contiguous()
+                struct ggml_tensor * V =
+                    ggml_view_3d(ctx0, model.memory_v,
+                            n_past + N, n_embd/n_head, n_head,
+                            n_ctx*ggml_element_size(model.memory_v),
+                            n_ctx*ggml_element_size(model.memory_v)*n_embd/n_head,
+                            il*n_ctx*ggml_element_size(model.memory_v)*n_embd);
+
+                // KQV = transpose(V) * KQ_soft_max
+                struct ggml_tensor * KQV = ggml_mul_mat(ctx0, V, KQ_soft_max);
+
+                // KQV_merged = KQV.permute(0, 2, 1, 3)
+                struct ggml_tensor * KQV_merged = ggml_permute(ctx0, KQV, 0, 2, 1, 3);
+
+                // cur = KQV_merged.contiguous().view(n_embd, N)
+                cur = ggml_cpy(ctx0,
+                        KQV_merged,
+                        ggml_new_tensor_2d(ctx0, GGML_TYPE_F32, n_embd, N));
+
+                // projection (no bias)
+                cur = ggml_mul_mat(ctx0,
+                        model.layers[il].c_attn_proj_w,
+                        cur);
+            }
+
+            struct ggml_tensor * inpFF = cur;
+
+            // feed-forward network
+            // this is independent of the self-attention result, so it could be done in parallel to the self-attention
+            {
+                // note here we pass inpSA instead of cur
+                cur = ggml_mul_mat(ctx0,
+                        model.layers[il].c_mlp_fc_w,
+                        inpSA);
+
+                cur = ggml_add(ctx0,
+                        ggml_repeat(ctx0, model.layers[il].c_mlp_fc_b, cur),
+                        cur);
+
+                // GELU activation
+                cur = ggml_gelu(ctx0, cur);
+
+                // projection
+                // cur = proj_w*cur + proj_b
+                cur = ggml_mul_mat(ctx0,
+                        model.layers[il].c_mlp_proj_w,
+                        cur);
+
+                cur = ggml_add(ctx0,
+                        ggml_repeat(ctx0, model.layers[il].c_mlp_proj_b, cur),
+                        cur);
+            }
+
+            // self-attention + FF
+            cur  = ggml_add(ctx0, cur, inpFF);
+
+            // input for next layer
+            inpL = ggml_add(ctx0, cur, inpL);
+        }
+
+        // norm
+        {
+            inpL = ggml_norm(ctx0, inpL, hparams.eps);
+
+            // inpL = ln_f_g*inpL + ln_f_b
+            inpL = ggml_add(ctx0,
+                    ggml_mul(ctx0,
+                        ggml_repeat(ctx0, model.ln_f_g, inpL),
+                        inpL),
+                    ggml_repeat(ctx0, model.ln_f_b, inpL));
+        }
+
+        // lm_head
+        {
+            inpL = ggml_mul_mat(ctx0, model.lmh_g, inpL);
+
+            inpL = ggml_add(ctx0,
+                    ggml_repeat(ctx0, model.lmh_b, inpL),
+                    inpL);
+        }
+
+        // logits -> probs
+        //inpL = ggml_soft_max_inplace(ctx0, inpL);
+
+        // run the computation
+        ggml_build_forward_expand(gf, inpL);
+        ggml_graph_compute_with_ctx(ctx0, gf, n_threads);
+
+        //if (n_past%100 == 0) {
+        //    ggml_graph_print   (&gf);
+        //    ggml_graph_dump_dot(&gf, NULL, "gpt-j.dot");
+        //}
+
+        //embd_w.resize(n_vocab*N);
+        //memcpy(embd_w.data(), ggml_get_data(inpL), sizeof(float)*n_vocab*N);
+
+        // return result for just the last token
+        embd_w.resize(n_vocab);
+        memcpy(embd_w.data(), reinterpret_cast<float*>(ggml_get_data(inpL)) + (n_vocab*(N-1)), sizeof(float)*n_vocab);
+
+        if (mem_per_token == 0) {
+            mem_per_token = ggml_used_mem(ctx0)/N;
+        }
+        //printf("used_mem = %zu\n", ggml_used_mem(ctx0));
+
+        ggml_free(ctx0);
+
+        return true;
+    }
+
+    gpt_vocab::id gpt_sample_top_k_top_p(
+            const gpt_vocab & vocab,
+            const float * logits,
+            int    top_k,
+            double top_p,
+            double temp,
+            std::mt19937 & rng) const {
+        int n_logits = static_cast<int>(vocab.id_to_token.size());
+
+        std::vector<std::pair<double, gpt_vocab::id>> logits_id;
+        logits_id.reserve(n_logits);
+
+        {
+            const double scale = 1.0/temp;
+            for (int i = 0; i < n_logits; ++i) {
+                logits_id.push_back(std::make_pair(logits[i]*scale, i));
+            }
+        }
+
+        // find the top K tokens
+        std::partial_sort(
+                logits_id.begin(),
+                logits_id.begin() + top_k, logits_id.end(),
+                [](const std::pair<double, gpt_vocab::id> & a, const std::pair<double, gpt_vocab::id> & b) {
+            return a.first > b.first;
+        });
+
+        logits_id.resize(top_k);
+
+        double maxl = -INFINITY;
+        for (const auto & kv : logits_id) {
+            maxl = std::max(maxl, kv.first);
+        }
+
+        // compute probs for the top K tokens
+        std::vector<double> probs;
+        probs.reserve(logits_id.size());
+
+        double sum = 0.0;
+        for (const auto & kv : logits_id) {
+            double p = exp(kv.first - maxl);
+            probs.push_back(p);
+            sum += p;
+        }
+
+        // normalize the probs
+        for (auto & p : probs) {
+            p /= sum;
+        }
+
+        if (top_p < 1.0f) {
+            double cumsum = 0.0f;
+            for (int i = 0; i < top_k; i++) {
+                cumsum += probs[i];
+                if (cumsum >= top_p) {
+                    top_k = i + 1;
+                    probs.resize(top_k);
+                    logits_id.resize(top_k);
+                    break;
+                }
+            }
+
+            cumsum = 1.0/cumsum;
+            for (double & prob : probs) {
+                prob *= cumsum;
+            }
+        }
+
+        //printf("\n");
+        //for (int i = 0; i < (int) probs.size(); i++) {
+        //    printf("%d: '%s' %f\n", i, vocab.id_to_token.at(logits_id[i].second).c_str(), probs[i]);
+        //}
+        //exit(0);
+
+        std::discrete_distribution<> dist(probs.begin(), probs.end());
+        int idx = dist(rng);
+
+        return logits_id[idx].second;
+    }
     #undef return
 
 
@@ -524,31 +842,91 @@ public:
         col_res->reserve(input_rows_count);
         UInt64 totalsize = 0;
         std::vector<String> result_raw(input_rows_count);
-        for (size_t i = 0; i < input_rows_count; ++i) {
+        int initial_n_predict = params.n_predict;
+        std::mt19937 rng(params.seed);
+
+        for (size_t j = 0; j < input_rows_count; ++j) {
             Field field;
-            field = vals[i]; // get(i, field);
+            field = vals[j]; // get(i, field);
             std::string val;
             if (!field.tryGet(val)) {
-                std::cout << "Ploho!" << std::endl;
                 throw Exception(ErrorCodes::SYNTAX_ERROR, "Nasrali");
             }
             else {
-                std::cout << "Ne Ploho! " << val << std::endl;
                 std::vector<gpt_vocab::id> embd_inp = gpt_tokenize(vocab, val);
-                std::cout << "Tokenized '" << val << "': ";
-                for (auto &x : embd_inp)
-                    std::cout << x << ' ';
-                std::cout << std::endl;
+                std::vector<gpt_vocab::id> embd;
+
+                // std::string result;
+                // for (auto &x : embd_inp) {
+                //     result += std::to_string(x) + " ";
+                // }
+                // if (!result.empty()) {
+                //     result.pop_back();
+                // }
+                // result = "Tokenized '" + val + "' is '" + result + "'";
+                // determine the required inference memory per token:
+
+                int n_past = 0;
+                std::vector<float> logits;
+                params.n_predict = std::min(initial_n_predict, model.hparams.n_ctx - static_cast<int>(embd_inp.size()));
+
+                size_t mem_per_token = 0;
+                gptj_eval(model, params.n_threads, 0, { 0, 1, 2, 3 }, logits, mem_per_token);
+
                 std::string result;
-                for (auto &x : embd_inp) {
-                    result += std::to_string(x) + " ";
+
+                for (size_t i = embd.size(); i < embd_inp.size() + params.n_predict; i++) {
+                    // predict
+                    if (!embd.empty()) {
+                        if (!gptj_eval(model, params.n_threads, n_past, embd, logits, mem_per_token)) {
+
+                            throw Exception(ErrorCodes::SYNTAX_ERROR, "Failed to predict");
+                        }
+                    }
+
+                    n_past += embd.size();
+                    embd.clear();
+
+                    if (i >= embd_inp.size()) {
+                        // sample next token
+                        const int   top_k = params.top_k;
+                        const float top_p = params.top_p;
+                        const float temp  = params.temp;
+
+                        const int n_vocab = model.hparams.n_vocab;
+
+                        gpt_vocab::id id = 0;
+
+                        id = gpt_sample_top_k_top_p(vocab, logits.data() + (logits.size() - n_vocab), top_k, top_p, temp, rng);
+
+                        // add it to the context
+                        embd.push_back(id);
+                    } else {
+                        // if here, it means we are still processing the input prompt
+                        for (size_t k = i; k < embd_inp.size(); k++) {
+                            embd.push_back(embd_inp[k]);
+                            if (int32_t(embd.size()) > params.n_batch) {
+                                break;
+                            }
+                        }
+                        i += embd.size() - 1;
+                    }
+
+                    // display text
+                    for (auto id : embd) {
+                        result += vocab.id_to_token[id];
+                    }
+
+                    // end of text token
+                    if (embd.back() == 50256) {
+                        break;
+                    }
                 }
-                if (!result.empty()) {
-                    result.pop_back();
-                }
-                result = "Tokenized '" + val + "' is '" + result + "'";
-                result_raw[i] = std::move(result);
-                totalsize += result_raw[i].size() + 1;
+
+                ggml_free(model.ctx);
+
+                result_raw[j] = std::move(result);
+                totalsize += result_raw[j].size() + 1;
             }
         }
         col_res->getChars().resize(totalsize);
