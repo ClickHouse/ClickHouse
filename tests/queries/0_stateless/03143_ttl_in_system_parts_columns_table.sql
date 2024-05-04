2045c7f04076ab5b48f946a9900fc8a78b8145c4
Xiaofei Hu <10776938+litlig@users.noreply.github.com>
2024-05-04 14:13:18 -0700

    rename

--- tests/queries/0_stateless/03143_ttl_in_system_parts_columns_table.sql
+++ tests/queries/0_stateless/03143_ttl_in_system_parts_columns_table.sql
@@ -16,8 +16,8 @@ SELECT
     name,
     column,
     type,
-    column_ttl_info.min,
-    column_ttl_info.max
+    column_ttl_min,
+    column_ttl_max
 FROM system.parts_columns
 WHERE table = 'test_03143' and database = currentDatabase()
 ORDER BY name, column;
