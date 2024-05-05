b5ce9b5aa54d9c1a484cb23e2d50f9723a0f62bc
Nikita Keba <nikita.keba@gmail.com>
2024-05-05 18:44:42 +0000

    add ParserPostgreSQLQuery

--- src/Parsers/PostgresQL/ParserPostgresQLQuery.cpp
+++ src/Parsers/PostgresQL/ParserPostgresQLQuery.cpp
@@ -1,1 +1,34 @@
 #include <Parsers/PostgreSQL/ParserPostgreSQLQuery.h>
+#include <string>
+
+#include "Parsers/Lexer.h"
+#include "config.h"
+
+#if USE_PostgreSQL
+#    include <PostgreSQL.h>
+#endif
+
+#include <Parsers/ParserQuery.h>
+#include <Parsers/ParserSetQuery.h>
+#include <Parsers/parseQuery.h>
+#include <base/scope_guard.h>
+
+namespace DB
+{
+
+namespace ErrorCodes
+{
+    extern const int SYNTAX_ERROR;
+    extern const int SUPPORT_IS_DISABLED;
+}
+
+bool ParserPostgreSQLQuery::parseImpl(Pos & pos, ASTPtr & node, Expected & expected)
+{
+#if !USE_PostgreSQL
+    throw Exception(
+        ErrorCodes::SUPPORT_IS_DISABLED, "PostgreSQL is not available. PostgreSQL may be disabled. Use another dialect!");
+#else
+    return true;
+#endif
+}
+}
--- src/Parsers/PostgresQL/ParserPostgresQLQuery.h
+++ src/Parsers/PostgresQL/ParserPostgresQLQuery.h
@@ -4,5 +4,25 @@
 
 namespace DB
 {
-    
+// Even when PostgreSQL is disabled, it is not possible to exclude this parser because changing the dialect via `SET dialect = '...'` queries should succeed.
+// Another solution would be disabling setting the dialect to PostgreSQL, but it requires a lot of finicky conditional compiling around the Dialect setting enum.
+// Therefore the decision, for now, is to use this parser even when PostgreSQL is disabled to enable users to switch to another dialect.
+class ParserPostgreSQLQuery final : public IParserBase
+{
+private:
+    // These fields are not used when PostgreSQL is disabled at build time.
+    [[maybe_unused]] size_t max_query_size;
+    [[maybe_unused]] size_t max_parser_depth;
+    [[maybe_unused]] size_t max_parser_backtracks;
+
+public:
+    ParserPostgreSQLQuery(size_t max_query_size_, size_t max_parser_depth_, size_t max_parser_backtracks_) : max_query_size(max_query_size_), max_parser_depth(max_parser_depth_), max_parser_backtracks(max_parser_backtracks_)
+    {
+    }
+
+    const char * getName() const override { return "PostgreSQL Statement"; }
+
+protected:
+    bool parseImpl(Pos & pos, ASTPtr & node, Expected & expected) override;
+};
 }
