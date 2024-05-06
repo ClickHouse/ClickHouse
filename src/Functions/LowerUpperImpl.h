02a964caa5bd2083ca5f9868a9a11ebcc181ab8b
Robert Schulze <robert@clickhouse.com>
2024-05-06 18:37:49 +0000

    Incorporate my feedback

--- src/Functions/LowerUpperImpl.h
+++ src/Functions/LowerUpperImpl.h
@@ -32,25 +32,27 @@ static void arrayImpl(const UInt8 * src, const UInt8 * src_end, UInt8 * dst)
     const auto src_end_avx = src_end - (src_end - src) % byte_avx512;
     if (src < src_end_avx)
     {
-        const auto v_not_case_lower_bound = _mm512_set1_epi8(not_case_lower_bound - 1);
-        const auto v_not_case_upper_bound = _mm512_set1_epi8(not_case_upper_bound + 1);
+        const __m512i v_not_case_lower_bound = _mm512_set1_epi8(not_case_lower_bound - 1);
+        const __m512i v_not_case_upper_bound = _mm512_set1_epi8(not_case_upper_bound + 1);
 
         for (; src < src_end_avx; src += byte_avx512, dst += byte_avx512)
         {
-            const auto chars = _mm512_loadu_si512(reinterpret_cast<const __m512i *>(src));
+            /// load 64 sequential 8-bit characters
+            const __m512i chars = _mm512_loadu_si512(reinterpret_cast<const __m512i *>(src));
 
-            const auto is_not_case
-                    = _mm512_mask_cmplt_epi8_mask(_mm512_cmpgt_epi8_mask(chars, v_not_case_lower_bound),
-                    chars, v_not_case_upper_bound);
+            const __mmask64 is_not_case = _mm512_mask_cmplt_epi8_mask(_mm512_cmpgt_epi8_mask(chars, v_not_case_lower_bound), chars, v_not_case_upper_bound);
 
-            const auto xor_mask = _mm512_maskz_set1_epi8(is_not_case, flip_case_mask);
+            const __m512i xor_mask = _mm512_maskz_set1_epi8(is_not_case, flip_case_mask);
 
-            const auto cased_chars = _mm512_xor_si512(chars, xor_mask);
+            /// flip case by applying calculated mask
+            const __m512i cased_chars = _mm512_xor_si512(chars, xor_mask);
 
+            /// store result back to destination
             _mm512_storeu_si512(reinterpret_cast<__m512i *>(dst), cased_chars);
         }
     }
 
+    /// Handle the tail
     for (; src < src_end; ++src, ++dst)
         if (*src >= not_case_lower_bound && *src <= not_case_upper_bound)
             *dst = *src ^ flip_case_mask;
@@ -68,30 +70,30 @@ static void arrayImpl(const UInt8 * src, const UInt8 * src_end, UInt8 * dst)
     const auto * src_end_avx = src_end - (src_end - src) % bytes_avx;
     if (src < src_end_avx)
     {
-        const auto v_not_case_lower_bound = _mm256_set1_epi8(not_case_lower_bound - 1);
-        const auto v_not_case_upper_bound = _mm256_set1_epi8(not_case_upper_bound + 1);
-        const auto v_flip_case_mask = _mm256_set1_epi8(flip_case_mask);
+        const __m256i v_not_case_lower_bound = _mm256_set1_epi8(not_case_lower_bound - 1);
+        const __m256i v_not_case_upper_bound = _mm256_set1_epi8(not_case_upper_bound + 1);
+        const __m256i v_flip_case_mask = _mm256_set1_epi8(flip_case_mask);
 
         for (; src < src_end_avx; src += bytes_avx, dst += bytes_avx)
         {
             /// load 32 sequential 8-bit characters
-            const auto chars = _mm256_loadu_si256(reinterpret_cast<const __m256i *>(src));
+            const __m256i chars = _mm256_loadu_si256(reinterpret_cast<const __m256i *>(src));
 
             /// find which 8-bit sequences belong to range [case_lower_bound, case_upper_bound]
-            const auto is_not_case
-                = _mm256_and_si256(_mm256_cmpgt_epi8(chars, v_not_case_lower_bound), _mm256_cmpgt_epi8(v_not_case_upper_bound, chars));
+            const __m256i is_not_case = _mm256_and_si256(_mm256_cmpgt_epi8(chars, v_not_case_lower_bound), _mm256_cmpgt_epi8(v_not_case_upper_bound, chars));
 
             /// keep `flip_case_mask` only where necessary, zero out elsewhere
-            const auto xor_mask = _mm256_and_si256(v_flip_case_mask, is_not_case);
+            const __m256i xor_mask = _mm256_and_si256(v_flip_case_mask, is_not_case);
 
             /// flip case by applying calculated mask
-            const auto cased_chars = _mm256_xor_si256(chars, xor_mask);
+            const __m256i cased_chars = _mm256_xor_si256(chars, xor_mask);
 
             /// store result back to destination
             _mm256_storeu_si256(reinterpret_cast<__m256i *>(dst), cased_chars);
         }
     }
 
+    /// Handle the tail
     for (; src < src_end; ++src, ++dst)
         if (*src >= not_case_lower_bound && *src <= not_case_upper_bound)
             *dst = *src ^ flip_case_mask;
@@ -109,30 +111,30 @@ static void arrayImpl(const UInt8 * src, const UInt8 * src_end, UInt8 * dst)
     const auto * src_end_sse = src_end - (src_end - src) % bytes_sse;
     if (src < src_end_sse)
     {
-        const auto v_not_case_lower_bound = _mm_set1_epi8(not_case_lower_bound - 1);
-        const auto v_not_case_upper_bound = _mm_set1_epi8(not_case_upper_bound + 1);
-        const auto v_flip_case_mask = _mm_set1_epi8(flip_case_mask);
+        const __m128i v_not_case_lower_bound = _mm_set1_epi8(not_case_lower_bound - 1);
+        const __m128i v_not_case_upper_bound = _mm_set1_epi8(not_case_upper_bound + 1);
+        const __m128i v_flip_case_mask = _mm_set1_epi8(flip_case_mask);
 
         for (; src < src_end_sse; src += bytes_sse, dst += bytes_sse)
         {
             /// load 16 sequential 8-bit characters
-            const auto chars = _mm_loadu_si128(reinterpret_cast<const __m128i *>(src));
+            const __m128i chars = _mm_loadu_si128(reinterpret_cast<const __m128i *>(src));
 
             /// find which 8-bit sequences belong to range [case_lower_bound, case_upper_bound]
-            const auto is_not_case
-                = _mm_and_si128(_mm_cmpgt_epi8(chars, v_not_case_lower_bound), _mm_cmplt_epi8(chars, v_not_case_upper_bound));
+            const __m128i is_not_case = _mm_and_si128(_mm_cmpgt_epi8(chars, v_not_case_lower_bound), _mm_cmplt_epi8(chars, v_not_case_upper_bound));
 
             /// keep `flip_case_mask` only where necessary, zero out elsewhere
-            const auto xor_mask = _mm_and_si128(v_flip_case_mask, is_not_case);
+            const __m128i xor_mask = _mm_and_si128(v_flip_case_mask, is_not_case);
 
             /// flip case by applying calculated mask
-            const auto cased_chars = _mm_xor_si128(chars, xor_mask);
+            const __m128i cased_chars = _mm_xor_si128(chars, xor_mask);
 
             /// store result back to destination
             _mm_storeu_si128(reinterpret_cast<__m128i *>(dst), cased_chars);
         }
     }
 
+    /// Handle the tail
     for (; src < src_end; ++src, ++dst)
         if (*src >= not_case_lower_bound && *src <= not_case_upper_bound)
             *dst = *src ^ flip_case_mask;
@@ -168,28 +170,11 @@ struct LowerUpperImpl
         array<not_case_lower_bound, not_case_upper_bound>(data.data(), data.data() + data.size(), res_data.data());
     }
 
-    static void vectorInPlace(ColumnString::Chars & data, ColumnString::Offsets & /*offsets*/)
-    {
-        array<not_case_lower_bound, not_case_upper_bound>(data.data(), data.data() + data.size(), data.data());
-    }
-
-
     static void vectorFixed(const ColumnString::Chars & data, size_t /*n*/, ColumnString::Chars & res_data)
     {
         res_data.resize(data.size());
         array<not_case_lower_bound, not_case_upper_bound>(data.data(), data.data() + data.size(), res_data.data());
     }
-
-    static void vectorFixedInPlace(ColumnString::Chars & data, size_t /*n*/)
-    {
-        array<not_case_lower_bound, not_case_upper_bound>(data.data(), data.data() + data.size(), data.data());
-    }
 };
-
-template <typename T>
-struct IsLowerUpper: std::false_type {};
-
-template <char not_case_lower_bound, char not_case_upper_bound>
-struct IsLowerUpper<LowerUpperImpl<not_case_lower_bound, not_case_upper_bound>> : std::true_type {};
 
 }
