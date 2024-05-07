95c0b47cbb0835a9207e3f06ef1ee88d2d4c1f82
Andrey Zvonov <azvonov@altinity.com>
2024-05-07 13:05:43 +0000

    add test for prefer_system_tzdata

---
+++ tests/integration/test_prefer_system_tzdata/test.py
@@ -0,0 +1,29 @@
+import pytest
+from helpers.cluster import ClickHouseCluster
+
+cluster = ClickHouseCluster(__file__)
+
+node = cluster.add_instance("node", main_configs=["config.d/prefer_system_tz.xml",])
+
+
+@pytest.fixture(scope="module")
+def start_cluster():
+    try:
+        cluster.start()
+
+        yield cluster
+    finally:
+        cluster.shutdown()
+
+
+def test_prefer_system_tzdata(start_cluster):
+    node.exec_in_container(
+        [
+            "bash",
+            "-c",
+            f"rm /usr/share/zoneinfo/Africa/Tunis && ln -s /usr/share/zoneinfo/America/New_York /usr/share/zoneinfo/Africa/Tunis",
+        ],
+        privileged=True,
+    )
+
+    assert node.exec_in_container([f"bash", "-c", f"echo \"SELECT toDateTime(toDateTime('2024-05-01 12:12:12', 'UTC'), 'Africa/Tunis')\" | curl -s {node.hostname}:8123/ --data-binary @-"]) == "2024-05-01 08:12:12\n"
