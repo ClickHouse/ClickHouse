49284724ef4b34bbc5112bfa63a99625e8d35fe7
Yarik Briukhovetskyi <114298166+yariks5s@users.noreply.github.com>
2024-05-07 16:10:21 +0200

    fix errorCodes in fast tests

--- tests/queries/0_stateless/01045_array_zip.sql
+++ tests/queries/0_stateless/01045_array_zip.sql
@@ -2,7 +2,7 @@ SELECT arrayZip(['a', 'b', 'c'], ['d', 'e', 'f']);
 
 SELECT arrayZip(['a', 'b', 'c'], ['d', 'e', 'f'], ['g', 'h', 'i']);
 
-SELECT arrayZip(); -- { serverError 42 }
+SELECT arrayZip(); -- { serverError TOO_FEW_ARGUMENTS_FOR_FUNCTION }
 
 SELECT arrayZip('a', 'b', 'c'); -- { serverError 43 }
 
