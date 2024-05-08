e708a54d4fc3175cde12c825bf5208322c328435
sarielwxm <1059293451@qq.com>
2024-05-08 22:24:14 +0800

    fast test

---
+++ tests/queries/0_stateless/03147_table_function_loop.sql
@@ -0,0 +1,14 @@
+SELECT * FROM loop(numbers(3)) LIMIT 10;
+SELECT * FROM loop (numbers(3)) LIMIT 10 settings max_block_size = 1;
+
+
+DROP DATABASE IF EXISTS 03147_db;
+CREATE DATABASE 03147_db;
+CREATE TABLE 03147_db.t (n Int8) ENGINE=MergeTree ORDER BY n;
+INSERT INTO 03147_db.t SELECT * FROM numbers(10);
+USE 03147_db;
+
+SELECT * FROM loop(03147_db.t) LIMIT 15;
+SELECT * FROM loop(t) LIMIT 15;
+SELECT * FROM loop(03147_db, t) LIMIT 15;
+SELECT * FROM loop(mysql('127.0.0.1:9004', currentDatabase(), t, 'default', '')) LIMIT 15;
