4ff054ad7464a2888dad1d4c0ac0266d97bffd04
Nikita Mikhaylov <nikitamikhaylov@clickhouse.com>
2024-05-08 17:18:02 +0000

    Format

--- src/Processors/Formats/Impl/SQLiteInputVFS.h
+++ src/Processors/Formats/Impl/SQLiteInputVFS.h
@@ -4,326 +4,316 @@
 
 #if USE_SQLITE
 
-#include <sqlite3.h>
+#    include <sqlite3.h>
 
-#include <Processors/Formats/Impl/ArrowBufferedStreams.h>
-#include <arrow/result.h>
+#    include <Processors/Formats/Impl/ArrowBufferedStreams.h>
+#    include <arrow/result.h>
 
-#define ORIGVFS(p) (static_cast<sqlite3_vfs*>((p)->pAppData))
+#    define ORIGVFS(p) (static_cast<sqlite3_vfs *>((p)->pAppData))
 
 class RandomAccessSQLiteFile : public sqlite3_file
 {
 public:
-	arrow::io::RandomAccessFile* ptr;
+    arrow::io::RandomAccessFile * ptr;
 };
 
-static int spmemfileClose( sqlite3_file * file );
-static int spmemfileRead( sqlite3_file * file, void * buffer, int len, sqlite3_int64 offset );
-static int spmemfileWrite( sqlite3_file * file, const void * buffer, int len, sqlite3_int64 offset );
-static int spmemfileTruncate( sqlite3_file * file, sqlite3_int64 size );
-static int spmemfileSync( sqlite3_file * file, int flags );
-static int spmemfileFileSize( sqlite3_file * file, sqlite3_int64 * size );
-static int spmemfileLock( sqlite3_file * file, int type );
-static int spmemfileUnlock( sqlite3_file * file, int type );
-static int spmemfileCheckReservedLock( sqlite3_file * file, int * result );
-static int spmemfileFileControl( sqlite3_file * file, int op, void * arg );
-static int spmemfileSectorSize( sqlite3_file * file );
-static int spmemfileDeviceCharacteristics( sqlite3_file * file );
-static int memShmMap(sqlite3_file*, int iPg, int pgsz, int, void volatile**);
-static int memShmLock(sqlite3_file*, int offset, int n, int flags);
-static void memShmBarrier(sqlite3_file*);
-static int memShmUnmap(sqlite3_file*, int deleteFlag);
-static int memFetch(sqlite3_file*, sqlite3_int64 iOfst, int iAmt, void **pp);
-static int memUnfetch(sqlite3_file*, sqlite3_int64 iOfst, void *p);
+static int spmemfileClose(sqlite3_file * file);
+static int spmemfileRead(sqlite3_file * file, void * buffer, int len, sqlite3_int64 offset);
+static int spmemfileWrite(sqlite3_file * file, const void * buffer, int len, sqlite3_int64 offset);
+static int spmemfileTruncate(sqlite3_file * file, sqlite3_int64 size);
+static int spmemfileSync(sqlite3_file * file, int flags);
+static int spmemfileFileSize(sqlite3_file * file, sqlite3_int64 * size);
+static int spmemfileLock(sqlite3_file * file, int type);
+static int spmemfileUnlock(sqlite3_file * file, int type);
+static int spmemfileCheckReservedLock(sqlite3_file * file, int * result);
+static int spmemfileFileControl(sqlite3_file * file, int op, void * arg);
+static int spmemfileSectorSize(sqlite3_file * file);
+static int spmemfileDeviceCharacteristics(sqlite3_file * file);
+static int memShmMap(sqlite3_file *, int iPg, int pgsz, int, void volatile **);
+static int memShmLock(sqlite3_file *, int offset, int n, int flags);
+static void memShmBarrier(sqlite3_file *);
+static int memShmUnmap(sqlite3_file *, int deleteFlag);
+static int memFetch(sqlite3_file *, sqlite3_int64 iOfst, int iAmt, void ** pp);
+static int memUnfetch(sqlite3_file *, sqlite3_int64 iOfst, void * p);
 
-static int memOpen(sqlite3_vfs*, const char *, sqlite3_file*, int , int *);
-static int memDelete(sqlite3_vfs*, const char *zName, int syncDir);
-static int memAccess(sqlite3_vfs*, const char *zName, int flags, int *);
-static int memFullPathname(sqlite3_vfs*, const char *zName, int, char *zOut);
-static void *memDlOpen(sqlite3_vfs*, const char *zFilename);
-static void memDlError(sqlite3_vfs*, int nByte, char *zErrMsg);
-static void (*memDlSym(sqlite3_vfs *pVfs, void *p, const char*zSym))(void);
-static void memDlClose(sqlite3_vfs*, void*);
-static int memRandomness(sqlite3_vfs*, int nByte, char *zOut);
-static int memSleep(sqlite3_vfs*, int microseconds);
-static int memCurrentTime(sqlite3_vfs*, double*);
-static int memGetLastError(sqlite3_vfs*, int, char *);
-static int memCurrentTimeInt64(sqlite3_vfs*, sqlite3_int64*);
-static int memSetSystemCall(sqlite3_vfs*, const char *zName, sqlite3_syscall_ptr);
-static sqlite3_syscall_ptr memGetSystemCall(sqlite3_vfs*, const char *zName);
-const char * memNextSystemCall(sqlite3_vfs*, const char *zName);
+static int memOpen(sqlite3_vfs *, const char *, sqlite3_file *, int, int *);
+static int memDelete(sqlite3_vfs *, const char * zName, int syncDir);
+static int memAccess(sqlite3_vfs *, const char * zName, int flags, int *);
+static int memFullPathname(sqlite3_vfs *, const char * zName, int, char * zOut);
+static void * memDlOpen(sqlite3_vfs *, const char * zFilename);
+static void memDlError(sqlite3_vfs *, int nByte, char * zErrMsg);
+static void (*memDlSym(sqlite3_vfs * pVfs, void * p, const char * zSym))(void);
+static void memDlClose(sqlite3_vfs *, void *);
+static int memRandomness(sqlite3_vfs *, int nByte, char * zOut);
+static int memSleep(sqlite3_vfs *, int microseconds);
+static int memCurrentTime(sqlite3_vfs *, double *);
+static int memGetLastError(sqlite3_vfs *, int, char *);
+static int memCurrentTimeInt64(sqlite3_vfs *, sqlite3_int64 *);
+static int memSetSystemCall(sqlite3_vfs *, const char * zName, sqlite3_syscall_ptr);
+static sqlite3_syscall_ptr memGetSystemCall(sqlite3_vfs *, const char * zName);
+const char * memNextSystemCall(sqlite3_vfs *, const char * zName);
 
 static sqlite3_io_methods mem_io_methods = {
-	1,                                  /* iVersion */
-	spmemfileClose,                     /* xClose */
-	spmemfileRead,                      /* xRead */
-	spmemfileWrite,                     /* xWrite */
-	spmemfileTruncate,                  /* xTruncate */
-	spmemfileSync,                      /* xSync */
-	spmemfileFileSize,                  /* xFileSize */
-	spmemfileLock,                      /* xLock */
-	spmemfileUnlock,                    /* xUnlock */
-	spmemfileCheckReservedLock,         /* xCheckReservedLock */
-	spmemfileFileControl,               /* xFileControl */
-	spmemfileSectorSize,                /* xSectorSize */
-	spmemfileDeviceCharacteristics,     /* xDeviceCharacteristics */
-  memShmMap,                          /* xShmMap */
-  memShmLock,                         /* xShmLock */
-  memShmBarrier,                      /* xShmBarrier */
-  memShmUnmap,                        /* xShmUnmap */
-  memFetch,                           /* xFetch */
-  memUnfetch                          /* xUnfetch */
+    1, /* iVersion */
+    spmemfileClose, /* xClose */
+    spmemfileRead, /* xRead */
+    spmemfileWrite, /* xWrite */
+    spmemfileTruncate, /* xTruncate */
+    spmemfileSync, /* xSync */
+    spmemfileFileSize, /* xFileSize */
+    spmemfileLock, /* xLock */
+    spmemfileUnlock, /* xUnlock */
+    spmemfileCheckReservedLock, /* xCheckReservedLock */
+    spmemfileFileControl, /* xFileControl */
+    spmemfileSectorSize, /* xSectorSize */
+    spmemfileDeviceCharacteristics, /* xDeviceCharacteristics */
+    memShmMap, /* xShmMap */
+    memShmLock, /* xShmLock */
+    memShmBarrier, /* xShmBarrier */
+    memShmUnmap, /* xShmUnmap */
+    memFetch, /* xFetch */
+    memUnfetch /* xUnfetch */
 };
 
 static sqlite3_vfs mem_vfs = {
-  1,                                /* iVersion */
-  sizeof(RandomAccessSQLiteFile),   /* szOsFile */
-  1024,                             /* mxPathname */
-  nullptr,                          /* pNext */
-  "ch_read_vfs",                    /* zName */
-  nullptr,                          /* pAppData (set in initMemVFS) */
-  memOpen,                          /* xOpen */
-  memDelete,                        /* xDelete */
-  memAccess,                        /* xAccess */
-  memFullPathname,                  /* xFullPathname */
-  memDlOpen,                        /* xDlOpen */
-  memDlError,                       /* xDlError */
-  memDlSym,                         /* xDlSym */
-  memDlClose,                       /* xDlClose */
-  memRandomness,                    /* xRandomness */
-  memSleep,                         /* xSleep */
-  memCurrentTime,                   /* xCurrentTime */
-  memGetLastError,                  /* xGetLastError */
-  memCurrentTimeInt64,              /* xCurrentTimeInt64 */
-  memSetSystemCall,                 /* xSetSystemCall */
-  memGetSystemCall,                 /* xGetSystemCall */
-  memNextSystemCall                 /* xNextSystemCall */
+    1, /* iVersion */
+    sizeof(RandomAccessSQLiteFile), /* szOsFile */
+    1024, /* mxPathname */
+    nullptr, /* pNext */
+    "ch_read_vfs", /* zName */
+    nullptr, /* pAppData (set in initMemVFS) */
+    memOpen, /* xOpen */
+    memDelete, /* xDelete */
+    memAccess, /* xAccess */
+    memFullPathname, /* xFullPathname */
+    memDlOpen, /* xDlOpen */
+    memDlError, /* xDlError */
+    memDlSym, /* xDlSym */
+    memDlClose, /* xDlClose */
+    memRandomness, /* xRandomness */
+    memSleep, /* xSleep */
+    memCurrentTime, /* xCurrentTime */
+    memGetLastError, /* xGetLastError */
+    memCurrentTimeInt64, /* xCurrentTimeInt64 */
+    memSetSystemCall, /* xSetSystemCall */
+    memGetSystemCall, /* xGetSystemCall */
+    memNextSystemCall /* xNextSystemCall */
 };
 
 static std::atomic<int> is_inited = 0;
 
-int spmemfileClose( sqlite3_file * )
+int spmemfileClose(sqlite3_file *)
 {
-	return SQLITE_OK;
+    return SQLITE_OK;
 }
 
-int spmemfileRead( sqlite3_file * file, void * buffer, int len, sqlite3_int64 offset )
+int spmemfileRead(sqlite3_file * file, void * buffer, int len, sqlite3_int64 offset)
 {
-	RandomAccessSQLiteFile * memfile = static_cast<RandomAccessSQLiteFile *>(file);
+    RandomAccessSQLiteFile * memfile = static_cast<RandomAccessSQLiteFile *>(file);
 
-    if (*(memfile->ptr->ReadAt(offset, len, buffer)) != len) {
+    if (*(memfile->ptr->ReadAt(offset, len, buffer)) != len)
+    {
         return SQLITE_IOERR_SHORT_READ;
     }
 
-	return SQLITE_OK;
+    return SQLITE_OK;
 }
 
-int spmemfileWrite( sqlite3_file * , const void * , int , sqlite3_int64 )
+int spmemfileWrite(sqlite3_file *, const void *, int, sqlite3_int64)
 {
-	return SQLITE_READONLY;
+    return SQLITE_READONLY;
 }
 
-int spmemfileTruncate( sqlite3_file * , sqlite3_int64 )
+int spmemfileTruncate(sqlite3_file *, sqlite3_int64)
 {
-	return SQLITE_OK;
+    return SQLITE_OK;
 }
 
-int spmemfileSync( sqlite3_file * , int )
+int spmemfileSync(sqlite3_file *, int)
 {
-	return SQLITE_OK;
+    return SQLITE_OK;
 }
 
-int spmemfileFileSize( sqlite3_file * file, sqlite3_int64 * size )
+int spmemfileFileSize(sqlite3_file * file, sqlite3_int64 * size)
 {
-	RandomAccessSQLiteFile * memfile = static_cast<RandomAccessSQLiteFile *>(file);
+    RandomAccessSQLiteFile * memfile = static_cast<RandomAccessSQLiteFile *>(file);
 
-	* size = *(memfile->ptr->GetSize());
+    *size = *(memfile->ptr->GetSize());
 
-	return SQLITE_OK;
+    return SQLITE_OK;
 }
 
-int spmemfileLock( sqlite3_file * , int  )
+int spmemfileLock(sqlite3_file *, int)
 {
-	return SQLITE_OK;
+    return SQLITE_OK;
 }
 
-int spmemfileUnlock( sqlite3_file * , int )
+int spmemfileUnlock(sqlite3_file *, int)
 {
-	return SQLITE_OK;
+    return SQLITE_OK;
 }
 
-int spmemfileCheckReservedLock( sqlite3_file *, int * result )
+int spmemfileCheckReservedLock(sqlite3_file *, int * result)
 {
-	*result = 0;
-	return SQLITE_OK;
+    *result = 0;
+    return SQLITE_OK;
 }
 
-int spmemfileFileControl( sqlite3_file * , int op, void * )
+int spmemfileFileControl(sqlite3_file *, int op, void *)
 {
-  int rc = SQLITE_NOTFOUND;
-  if( op==SQLITE_FCNTL_VFSNAME ){
-    rc = SQLITE_OK;
-  }
-	return rc;
+    int rc = SQLITE_NOTFOUND;
+    if (op == SQLITE_FCNTL_VFSNAME)
+    {
+        rc = SQLITE_OK;
+    }
+    return rc;
 }
 
-int spmemfileSectorSize( sqlite3_file * )
+int spmemfileSectorSize(sqlite3_file *)
 {
-	return 0;
+    return 0;
 }
 
-int spmemfileDeviceCharacteristics( sqlite3_file * )
+int spmemfileDeviceCharacteristics(sqlite3_file *)
 {
-	return 0;
+    return 0;
 }
 
-int memShmMap(
-  sqlite3_file *,
-  int ,
-  int ,
-  int ,
-  void volatile **
-){
-  return SQLITE_IOERR_SHMMAP;
+int memShmMap(sqlite3_file *, int, int, int, void volatile **)
+{
+    return SQLITE_IOERR_SHMMAP;
 }
 
-static int memShmLock(sqlite3_file *, int , int , int ){
-  return SQLITE_IOERR_SHMLOCK;
+static int memShmLock(sqlite3_file *, int, int, int)
+{
+    return SQLITE_IOERR_SHMLOCK;
 }
 
-static void memShmBarrier(sqlite3_file *){
-  return;
+static void memShmBarrier(sqlite3_file *)
+{
+    return;
 }
 
-static int memShmUnmap(sqlite3_file *, int ){
-  return SQLITE_OK;
+static int memShmUnmap(sqlite3_file *, int)
+{
+    return SQLITE_OK;
 }
 
-static int memFetch(
-  sqlite3_file *,
-  sqlite3_int64 ,
-  int ,
-  void **
-){
-  return SQLITE_OK;
+static int memFetch(sqlite3_file *, sqlite3_int64, int, void **)
+{
+    return SQLITE_OK;
 }
 
-static int memUnfetch(sqlite3_file *, sqlite3_int64 , void *){
-  return SQLITE_OK;
+static int memUnfetch(sqlite3_file *, sqlite3_int64, void *)
+{
+    return SQLITE_OK;
 }
 
 
 //////////////////////////////////
 
 static int memOpen(
-  sqlite3_vfs */*pVfs*/,
-  const char *zName,
-  sqlite3_file *pFile,
-  int /*flags*/,
-  int */*pOutFlags*/
-){
-  RandomAccessSQLiteFile *c = reinterpret_cast<RandomAccessSQLiteFile*>(pFile);
-  c->ptr = reinterpret_cast<arrow::io::RandomAccessFile*>(std::stoll(zName, nullptr, 16));
-  if (c->ptr == nullptr) {
-    return SQLITE_CANTOPEN;
-  }
-  pFile->pMethods = &mem_io_methods;
-  return SQLITE_OK;
-}
-
-static int memDelete(sqlite3_vfs */*pVfs*/, const char */*zPath*/, int /*dirSync*/) {
-  return SQLITE_IOERR_DELETE;
-}
-
-static int memAccess(
-  sqlite3_vfs */*pVfs*/,
-  const char */*zPath*/,
-  int /*flags*/,
-  int *pResOut
-){
-  *pResOut = 0;
-  return SQLITE_OK;
-}
-
-static int memFullPathname(
-  sqlite3_vfs */*pVfs*/,
-  const char *zPath,
-  int nOut,
-  char *zOut
-){
-  sqlite3_snprintf(nOut, zOut, "%s", zPath);
-  return SQLITE_OK;
-}
-
-static void *memDlOpen(sqlite3_vfs *pVfs, const char *zPath)
+    sqlite3_vfs * /*pVfs*/, const char * zName, sqlite3_file * pFile, int /*flags*/, int * /*pOutFlags*/
+)
 {
-  return ORIGVFS(pVfs)->xDlOpen(ORIGVFS(pVfs), zPath);
+    RandomAccessSQLiteFile * c = reinterpret_cast<RandomAccessSQLiteFile *>(pFile);
+    c->ptr = reinterpret_cast<arrow::io::RandomAccessFile *>(std::stoll(zName, nullptr, 16));
+    if (c->ptr == nullptr)
+    {
+        return SQLITE_CANTOPEN;
+    }
+    pFile->pMethods = &mem_io_methods;
+    return SQLITE_OK;
 }
 
-static void memDlError(sqlite3_vfs *pVfs, int nByte, char *zErrMsg)
+static int memDelete(sqlite3_vfs * /*pVfs*/, const char * /*zPath*/, int /*dirSync*/)
 {
-  ORIGVFS(pVfs)->xDlError(ORIGVFS(pVfs), nByte, zErrMsg);
+    return SQLITE_IOERR_DELETE;
 }
 
-static void (*memDlSym(sqlite3_vfs *pVfs, void *p, const char *zSym))(void)
+static int memAccess(sqlite3_vfs * /*pVfs*/, const char * /*zPath*/, int /*flags*/, int * pResOut)
 {
-  return ORIGVFS(pVfs)->xDlSym(ORIGVFS(pVfs), p, zSym);
+    *pResOut = 0;
+    return SQLITE_OK;
 }
 
-static void memDlClose(sqlite3_vfs *pVfs, void *pHandle)
+static int memFullPathname(sqlite3_vfs * /*pVfs*/, const char * zPath, int nOut, char * zOut)
 {
-  ORIGVFS(pVfs)->xDlClose(ORIGVFS(pVfs), pHandle);
+    sqlite3_snprintf(nOut, zOut, "%s", zPath);
+    return SQLITE_OK;
 }
 
-static int memRandomness(sqlite3_vfs *pVfs, int nByte, char *zBufOut)
+static void * memDlOpen(sqlite3_vfs * pVfs, const char * zPath)
 {
-  return ORIGVFS(pVfs)->xRandomness(ORIGVFS(pVfs), nByte, zBufOut);
+    return ORIGVFS(pVfs)->xDlOpen(ORIGVFS(pVfs), zPath);
 }
 
-static int memSleep(sqlite3_vfs *pVfs, int nMicro)
+static void memDlError(sqlite3_vfs * pVfs, int nByte, char * zErrMsg)
 {
-  return ORIGVFS(pVfs)->xSleep(ORIGVFS(pVfs), nMicro);
+    ORIGVFS(pVfs)->xDlError(ORIGVFS(pVfs), nByte, zErrMsg);
 }
 
-static int memCurrentTime(sqlite3_vfs *pVfs, double *pTimeOut)
+static void (*memDlSym(sqlite3_vfs * pVfs, void * p, const char * zSym))(void)
 {
-  return ORIGVFS(pVfs)->xCurrentTime(ORIGVFS(pVfs), pTimeOut);
+    return ORIGVFS(pVfs)->xDlSym(ORIGVFS(pVfs), p, zSym);
 }
 
-static int memGetLastError(sqlite3_vfs *pVfs, int a, char *b)
+static void memDlClose(sqlite3_vfs * pVfs, void * pHandle)
 {
-  return ORIGVFS(pVfs)->xGetLastError(ORIGVFS(pVfs), a, b);
+    ORIGVFS(pVfs)->xDlClose(ORIGVFS(pVfs), pHandle);
 }
 
-static int memCurrentTimeInt64(sqlite3_vfs *pVfs, sqlite3_int64 *p)
+static int memRandomness(sqlite3_vfs * pVfs, int nByte, char * zBufOut)
 {
-  return ORIGVFS(pVfs)->xCurrentTimeInt64(ORIGVFS(pVfs), p);
+    return ORIGVFS(pVfs)->xRandomness(ORIGVFS(pVfs), nByte, zBufOut);
 }
 
-static int memSetSystemCall(sqlite3_vfs*, const char *, sqlite3_syscall_ptr)
+static int memSleep(sqlite3_vfs * pVfs, int nMicro)
 {
-  return SQLITE_OK;
+    return ORIGVFS(pVfs)->xSleep(ORIGVFS(pVfs), nMicro);
 }
 
-static sqlite3_syscall_ptr memGetSystemCall(sqlite3_vfs*, const char *)
+static int memCurrentTime(sqlite3_vfs * pVfs, double * pTimeOut)
 {
-  return nullptr;
+    return ORIGVFS(pVfs)->xCurrentTime(ORIGVFS(pVfs), pTimeOut);
 }
 
-const char * memNextSystemCall(sqlite3_vfs*, const char *)
+static int memGetLastError(sqlite3_vfs * pVfs, int a, char * b)
 {
-  return nullptr;
+    return ORIGVFS(pVfs)->xGetLastError(ORIGVFS(pVfs), a, b);
+}
+
+static int memCurrentTimeInt64(sqlite3_vfs * pVfs, sqlite3_int64 * p)
+{
+    return ORIGVFS(pVfs)->xCurrentTimeInt64(ORIGVFS(pVfs), p);
+}
+
+static int memSetSystemCall(sqlite3_vfs *, const char *, sqlite3_syscall_ptr)
+{
+    return SQLITE_OK;
+}
+
+static sqlite3_syscall_ptr memGetSystemCall(sqlite3_vfs *, const char *)
+{
+    return nullptr;
+}
+
+const char * memNextSystemCall(sqlite3_vfs *, const char *)
+{
+    return nullptr;
 }
 
 static int initMemVFS()
 {
-  int zero = 0;
-  if (is_inited.compare_exchange_strong(zero, 1)) {
-    mem_vfs.pAppData = sqlite3_vfs_find(nullptr);
-    if( mem_vfs.pAppData==nullptr ) return SQLITE_ERROR;
-    return sqlite3_vfs_register(&mem_vfs, 0);
-  }
-  return SQLITE_OK;
+    int zero = 0;
+    if (is_inited.compare_exchange_strong(zero, 1))
+    {
+        mem_vfs.pAppData = sqlite3_vfs_find(nullptr);
+        if (mem_vfs.pAppData == nullptr)
+            return SQLITE_ERROR;
+        return sqlite3_vfs_register(&mem_vfs, 0);
+    }
+    return SQLITE_OK;
 }
 
 #endif
--- src/Processors/Formats/Impl/SQLiteOutputFormat.cpp
+++ src/Processors/Formats/Impl/SQLiteOutputFormat.cpp
@@ -2,56 +2,62 @@
 
 #if USE_SQLITE
 
-#include <Interpreters/ProcessList.h>
-#include <iostream>
-#include <sqlite3.h>
+#    include <iostream>
+#    include <Interpreters/ProcessList.h>
+#    include <sqlite3.h>
 
 namespace DB
 {
 
 namespace ErrorCodes
 {
-    extern const int SQLITE_ENGINE_ERROR;
-    extern const int UNKNOWN_TABLE;
+extern const int SQLITE_ENGINE_ERROR;
+extern const int UNKNOWN_TABLE;
 }
 
 SQLiteOutputFormat::SQLiteOutputFormat(WriteBuffer & out_, const Block & header_, const FormatSettings & settings_)
-    : IOutputFormat(header_, out_)
-    , format_settings(settings_)
+    : IOutputFormat(header_, out_), format_settings(settings_)
 {
 }
 
 void SQLiteOutputFormat::writePrefix()
 {
-    if (db) {
+    if (db)
+    {
         return;
     }
     auto * fd_in = dynamic_cast<WriteBufferFromFileDescriptor *>(&out);
     int fd = 0;
     String uri;
-    if (fd_in) {
+    if (fd_in)
+    {
         fd = fd_in->getFD();
-        if (fd <= 2) {
+        if (fd <= 2)
+        {
             throw Exception(ErrorCodes::UNKNOWN_TABLE, "Can't use non-seekable WriteBuffer");
         }
         uri = fmt::format("file:///dev/fd/{}", fd);
     }
 
-    sqlite3 *db_ptr;
+    sqlite3 * db_ptr;
 
     int status = sqlite3_open_v2(uri.c_str(), &db_ptr, SQLITE_OPEN_READWRITE | SQLITE_OPEN_URI, nullptr);
 
-    if (status != SQLITE_OK) {
-        throw Exception::createDeprecated(fmt::format("Cannot open sqlite database. Error status: {}. Message: {}",
-                                       status, sqlite3_errstr(status)), ErrorCodes::SQLITE_ENGINE_ERROR);
+    if (status != SQLITE_OK)
+    {
+        throw Exception::createDeprecated(
+            fmt::format("Cannot open sqlite database. Error status: {}. Message: {}", status, sqlite3_errstr(status)),
+            ErrorCodes::SQLITE_ENGINE_ERROR);
     }
     db.reset(db_ptr, sqlite3_close_v2);
 
 
     auto names_and_types = getPort(PortKind::Main).getHeader().getNamesAndTypes();
     String names_and_types_sub_query = "";
-    for (size_t i = 0; i < names_and_types.size(); i++) {
-        if (i != 0) {
+    for (size_t i = 0; i < names_and_types.size(); i++)
+    {
+        if (i != 0)
+        {
             names_and_types_sub_query += ", ";
         }
         names_and_types_sub_query += (names_and_types[i].name + " " + names_and_types[i].type->getName());
@@ -60,11 +66,12 @@ void SQLiteOutputFormat::writePrefix()
     String sql = fmt::format("CREATE TABLE if not exists result({})", names_and_types_sub_query);
     status = sqlite3_exec(db.get(), sql.c_str(), nullptr, nullptr, nullptr);
 
-    if (status != SQLITE_OK) {
-        throw Exception::createDeprecated(fmt::format("Cannot create SQLite table. Error status: {}. Message: {}",
-                                       status, sqlite3_errstr(status)), ErrorCodes::SQLITE_ENGINE_ERROR);
+    if (status != SQLITE_OK)
+    {
+        throw Exception::createDeprecated(
+            fmt::format("Cannot create SQLite table. Error status: {}. Message: {}", status, sqlite3_errstr(status)),
+            ErrorCodes::SQLITE_ENGINE_ERROR);
     }
-
 }
 
 void SQLiteOutputFormat::consume(Chunk chunk)
@@ -74,7 +81,8 @@ void SQLiteOutputFormat::consume(Chunk chunk)
     {
         const Columns & columns = chunk.getColumns();
 
-        if (i != 0) {
+        if (i != 0)
+        {
             values += ",";
         }
         values += "(";
@@ -84,7 +92,8 @@ void SQLiteOutputFormat::consume(Chunk chunk)
             WriteBufferFromOwnString ostr;
             serializations[j]->serializeTextQuoted(*columns[j], i, ostr, format_settings);
 
-            if (j != 0) {
+            if (j != 0)
+            {
                 values += ",";
             }
             values += ostr.str();
@@ -96,24 +105,24 @@ void SQLiteOutputFormat::consume(Chunk chunk)
     String sql = fmt::format("INSERT INTO result VALUES {}", values);
     int status = sqlite3_exec(db.get(), sql.c_str(), nullptr, nullptr, nullptr);
 
-    if (status != SQLITE_OK) {
-        throw Exception::createDeprecated(fmt::format("Cannot insert into SQLite table. Error status: {}. Message: {}",
-                                       status, sqlite3_errstr(status)), ErrorCodes::SQLITE_ENGINE_ERROR);
+    if (status != SQLITE_OK)
+    {
+        throw Exception::createDeprecated(
+            fmt::format("Cannot insert into SQLite table. Error status: {}. Message: {}", status, sqlite3_errstr(status)),
+            ErrorCodes::SQLITE_ENGINE_ERROR);
     }
 }
 
 void SQLiteOutputFormat::flush()
 {
-
 }
 
 void registerOutputFormatSQLite(FormatFactory & factory)
 {
     factory.registerOutputFormat(
         "SQLite",
-        [](WriteBuffer & buf,
-           const Block & sample,
-           const FormatSettings & settings) { return std::make_shared<SQLiteOutputFormat>(buf, sample, settings); });
+        [](WriteBuffer & buf, const Block & sample, const FormatSettings & settings)
+        { return std::make_shared<SQLiteOutputFormat>(buf, sample, settings); });
 }
 }
 
