f74403d525a5fbe63660ebf0e5fe91b1ea55c42b
shuai-xu <xushuai@bigo.sg>
2024-05-08 14:22:49 +0800

    truncate file instead of remove

--- tests/queries/0_stateless/03093_support_output_hive_text_format.sh
+++ tests/queries/0_stateless/03093_support_output_hive_text_format.sh
@@ -6,8 +6,6 @@ CURDIR=$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)
 
 user_files_path=$($CLICKHOUSE_CLIENT --query "select _path,_file from file('nonexist.txt', 'CSV', 'val1 char')" 2>&1 | grep -E '^Code: 107.*FILE_DOESNT_EXIST' | head -1 | awk '{gsub("/nonexist.txt","",$9); print $9}')
 
-${CLICKHOUSE_CLIENT} --query "insert into function file('test_support_hive_text_format.hivetext') select '20240305' as cstring, toInt32(100) as cint, toFloat32(1.22) as cfloat, tuple(123567, 'e01001', map('action1', 33333, 'act2', 5555)) as event, ['abc', 'def'] as locations, map('ddd', 2, 'eee', 6) as score, true as cbool, toInt8(1) as cbyte, toInt16(22) as cshort, toFloat64(333.333) as cdouble, toDate('2024-04-09') as d, timestamp('2022-01-02 00:00:00') as ts, X'41', 555.55, 66.66, [map('k1', 2, 'k2', 3), map('k3', 5, 'k5', 6)], [map('k1', tuple('1', map('k11', 'v22', 'k33', 'v55')), 'k2', tuple('11', map('k111', 'v222', 'k333', 'v555'))), map('k3', tuple('22', map('k22', 'v333', 'k33', 'v44')), 'k5', tuple('222', map('k222', 'v222')))];" 2>/dev/null
+${CLICKHOUSE_CLIENT} --query "insert into function file('test_support_hive_text_format.hivetext') select '20240305' as cstring, toInt32(100) as cint, toFloat32(1.22) as cfloat, tuple(123567, 'e01001', map('action1', 33333, 'act2', 5555)) as event, ['abc', 'def'] as locations, map('ddd', 2, 'eee', 6) as score, true as cbool, toInt8(1) as cbyte, toInt16(22) as cshort, toFloat64(333.333) as cdouble, toDate('2024-04-09') as d, timestamp('2022-01-02 00:00:00') as ts, X'41', 555.55, 66.66, [map('k1', 2, 'k2', 3), map('k3', 5, 'k5', 6)], [map('k1', tuple('1', map('k11', 'v22', 'k33', 'v55')), 'k2', tuple('11', map('k111', 'v222', 'k333', 'v555'))), map('k3', tuple('22', map('k22', 'v333', 'k33', 'v44')), 'k5', tuple('222', map('k222', 'v222')))] SETTINGS engine_file_truncate_on_insert=1;" 2>/dev/null
 
 cat "${user_files_path}/test_support_hive_text_format.hivetext"
-
-rm "${user_files_path}/test_support_hive_text_format.hivetext"
