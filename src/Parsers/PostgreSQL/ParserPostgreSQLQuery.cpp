ae5a2703c5773bdeae50ec92fd74e5ad4980bff2
seshWCS <superalexman700@gmail.com>
2024-05-10 19:23:13 +0000

    fix

--- src/Parsers/PostgreSQL/ParserPostgreSQLQuery.cpp
+++ src/Parsers/PostgreSQL/ParserPostgreSQLQuery.cpp
@@ -12,30 +12,30 @@
 
 #include "config.h"
 
-#include <PostgreSQL.h>
+// #include <PostgreSQL.h>
 
 #include <base/scope_guard.h>
 
 namespace DB
 {
 
-bool ParserPostgreSQLQuery::parseImpl(Pos & pos, ASTPtr & node, Expected & expected)
+bool ParserPostgreSQLQuery::parseImpl(Pos & pos, [[maybe_unused]] ASTPtr & node, [[maybe_unused]] Expected & expected)
 {
     const auto * begin = pos->begin;
     while (!pos->isEnd() && pos->type != TokenType::Semicolon)
         ++pos;
     const auto * end = pos->begin;
     
-    const auto * begin_char_ptr = reinterpret_cast<char *>(begin);
-    const auto * end_char_ptr = reinterpret_cast<char *>(end);
-    String res = "";
-    for (auto it = begin_char_ptr; it != end_char_ptr; it++) {
+    const auto * begin_char_ptr = begin;
+    const auto * end_char_ptr = end;
+    String res;
+    for (const auto *it = begin_char_ptr; it != end_char_ptr; it++) {
         res += *it;
     }
     
     Poco::Logger * log = &Poco::Logger::get("registerCompressionCodecWithType");
 
-    LOG_DEBUG(log, "POSTGRESQL RES: {}", res);
+    log->debug("POSTGRESQL RES: {}", res);
     
 
     return true;
