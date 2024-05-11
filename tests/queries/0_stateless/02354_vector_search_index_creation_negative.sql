0984f9ffc44220376ebd79c2dab295d76772c57b
Robert Schulze <robert@clickhouse.com>
2024-05-11 14:12:13 +0000

    Cosmetics in tests
    Cleaned up some tests to improve readability.

--- tests/queries/0_stateless/02354_vector_search_index_creation_negative.sql
+++ tests/queries/0_stateless/02354_vector_search_index_creation_negative.sql
@@ -1,46 +1,37 @@
 -- Tags: no-fasttest, no-ordinary-database
 
--- Tests vector search in ClickHouse, i.e. Usearch indexes. Both index types share similarities in implementation and usage,
--- therefore they are tested in a single file.
-
--- This file tests that various conditions are checked during creation of vector search indexes.
+-- Tests that various conditions are checked during creation of vector search indexes.
 
 SET allow_experimental_usearch_index = 1;
 
 DROP TABLE IF EXISTS tab;
 
 SELECT 'At most two index arguments';
-CREATE TABLE tab(id Int32, vec Array(Float32), INDEX idx vec TYPE usearch('too', 'many', 'args')) ENGINE = MergeTree ORDER BY id; -- { serverError INCORRECT_QUERY }
+CREATE TABLE tab(id Int32, vec Array(Float32), PRIMARY KEY id, INDEX vec_idx vec TYPE usearch('too', 'many', 'args')); -- { serverError INCORRECT_QUERY }
 
 SELECT '1st argument (distance function) must be String';
-CREATE TABLE tab(id Int32, vec Array(Float32), INDEX idx vec TYPE usearch(3)) ENGINE = MergeTree ORDER BY id; -- { serverError INCORRECT_QUERY }
+CREATE TABLE tab(id Int32, vec Array(Float32), PRIMARY KEY id, INDEX vec_idx vec TYPE usearch(3)); -- { serverError INCORRECT_QUERY }
 
-SELECT 'Rejects unsupported distance functions';
-CREATE TABLE tab(id Int32, vec Array(Float32), INDEX idx vec TYPE usearch('invalidDistance')) ENGINE = MergeTree ORDER BY id; -- { serverError INCORRECT_DATA }
+SELECT 'Unsupported distance functions are rejected';
+CREATE TABLE tab(id Int32, vec Array(Float32), PRIMARY KEY id, INDEX vec_idx vec TYPE usearch('invalid_distance_function')); -- { serverError INCORRECT_DATA }
 
-SELECT '2nd argument (scalar kind) must be String (Usearch)';
-CREATE TABLE tab(id Int32, vec Array(Float32), INDEX idx vec TYPE usearch(3)) ENGINE = MergeTree ORDER BY id; -- { serverError INCORRECT_QUERY }
+SELECT '2nd argument (scalar kind) must be String';
+CREATE TABLE tab(id Int32, vec Array(Float32), PRIMARY KEY id, INDEX vec_idx vec TYPE usearch(3)); -- { serverError INCORRECT_QUERY }
 
-SELECT 'Rejects unsupported scalar kinds (only Usearch)';
-CREATE TABLE tab(id Int32, vec Array(Float32), INDEX idx vec TYPE usearch('L2Distance', 'invalidKind')) ENGINE = MergeTree ORDER BY id; -- { serverError INCORRECT_DATA }
+SELECT 'Unsupported scalar kinds are rejected';
+CREATE TABLE tab(id Int32, vec Array(Float32), PRIMARY KEY id, INDEX vec_idx vec TYPE usearch('L2Distance', 'invalid_kind')); -- { serverError INCORRECT_DATA }
 
 SELECT 'Must be created on single column';
-CREATE TABLE tab(id Int32, vec Array(Float32), INDEX idx (vec, id) TYPE usearch()) ENGINE = MergeTree ORDER BY id; -- { serverError INCORRECT_NUMBER_OF_COLUMNS }
+CREATE TABLE tab(id Int32, vec Array(Float32), PRIMARY KEY id, INDEX vec_idx (vec, id) TYPE usearch()); -- { serverError INCORRECT_NUMBER_OF_COLUMNS }
 
 SELECT 'Must be created on Array(Float32) columns';
-
 SET allow_suspicious_low_cardinality_types = 1;
-
-CREATE TABLE tab(id Int32, vec Float32, INDEX idx vec TYPE usearch()) ENGINE = MergeTree ORDER BY id; -- { serverError ILLEGAL_COLUMN }
-
-CREATE TABLE tab(id Int32, vec Array(Float64), INDEX idx vec TYPE usearch()) ENGINE = MergeTree ORDER BY id; -- { serverError ILLEGAL_COLUMN }
-
-CREATE TABLE tab(id Int32, vec LowCardinality(Float32), INDEX idx vec TYPE usearch()) ENGINE = MergeTree ORDER BY id; -- { serverError ILLEGAL_COLUMN }
-
-CREATE TABLE tab(id Int32, vec Nullable(Float32), INDEX idx vec TYPE usearch()) ENGINE = MergeTree ORDER BY id; -- { serverError ILLEGAL_COLUMN }
+CREATE TABLE tab(id Int32, vec Float32, PRIMARY KEY id, INDEX vec_idx vec TYPE usearch()); -- { serverError ILLEGAL_COLUMN }
+CREATE TABLE tab(id Int32, vec Array(Float64), PRIMARY KEY id, INDEX vec_idx vec TYPE usearch()); -- { serverError ILLEGAL_COLUMN }
+CREATE TABLE tab(id Int32, vec LowCardinality(Float32), PRIMARY KEY id, INDEX vec_idx vec TYPE usearch()); -- { serverError ILLEGAL_COLUMN }
+CREATE TABLE tab(id Int32, vec Nullable(Float32), PRIMARY KEY id, INDEX vec_idx vec TYPE usearch()); -- { serverError ILLEGAL_COLUMN }
 
 SELECT 'Rejects INSERTs of Arrays with different sizes';
-
-CREATE TABLE tab(id Int32, vec Array(Float32), INDEX idx vec TYPE usearch()) ENGINE = MergeTree ORDER BY id;
+CREATE TABLE tab(id Int32, vec Array(Float32), PRIMARY KEY id, INDEX vec_idx vec TYPE usearch());
 INSERT INTO tab values (0, [2.2, 2.3]) (1, [3.1, 3.2, 3.3]); -- { serverError INCORRECT_DATA }
 DROP TABLE tab;
