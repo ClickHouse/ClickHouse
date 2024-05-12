76ec28ff02ee11f4f8e7bea7478fdbe3aa047249
Fargat <bijiare77@gmail.com>
2024-05-12 14:05:58 +0300

    fix formatimpl

--- src/Parsers/ASTDefaultedColumn.cpp
+++ src/Parsers/ASTDefaultedColumn.cpp
@@ -12,6 +12,16 @@ ASTPtr ASTDefaultedColumn::clone() const
 }
 void ASTDefaultedColumn::formatImpl(const DB::IAST::FormatSettings & settings, DB::IAST::FormatState & state, DB::IAST::FormatStateStacked frame) const
 {
+    if(settings.one_line) {
+        return;
+    }
+    if(state.printed_asts_with_alias) {
+        return;
+    }
+    if(frame.need_parens) {
+        return;
+    }
+
     return;
 }
 }
