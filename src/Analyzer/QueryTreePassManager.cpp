b33c1f132f71bfaf38bd2ff4437ffc21cf560974
VKlin357 <palysaev.vadim@yandex.ru>
2024-05-12 17:27:48 +0000

    e

--- src/Analyzer/QueryTreePassManager.cpp
+++ src/Analyzer/QueryTreePassManager.cpp
@@ -28,6 +28,7 @@
 #include <Analyzer/Passes/MultiIfToIfPass.h>
 #include <Analyzer/Passes/IfConstantConditionPass.h>
 #include <Analyzer/Passes/IfChainToMultiIfPass.h>
+#include <Analyzer/Passes/OptimizeL2DistancePass.h>
 #include <Analyzer/Passes/OrderByTupleEliminationPass.h>
 #include <Analyzer/Passes/NormalizeCountVariantsPass.h>
 #include <Analyzer/Passes/AggregateFunctionsArithmericOperationsPass.h>
