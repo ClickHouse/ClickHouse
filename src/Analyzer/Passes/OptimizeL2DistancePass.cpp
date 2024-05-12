23aa26f4d4569dd993c56842052b95e4b6c17de7
VKlin357 <palysaev.vadim@yandex.ru>
2024-05-12 17:49:20 +0000

    у

--- src/Analyzer/Passes/OptimizeL2DistancePass.cpp
+++ src/Analyzer/Passes/OptimizeL2DistancePass.cpp
@@ -20,23 +20,52 @@ public:
 
     void enterImpl(QueryTreeNodePtr & node)
     {
+            // Проверяем, является ли текущий узел функциональным узлом.
         auto * function_node = node->as<FunctionNode>();
-        if (!function_node || function_node->getFunctionName() != "L2Distance")
+        if (!function_node)
             return;
 
-        auto & arguments = function_node->getArguments().getNodes();
-        if (arguments.size() != 2)
-            return;
+        // Проверяем, является ли функция вызовом L2Distance.
+        if (function_node->getFunctionName() == "L2Distance")
+        {
+            auto & arguments = function_node->getArguments().getNodes();
+            if (arguments.size() == 2)
+            {
+                // Создаем узел для функции L2SquaredDistance.
+                auto squared_distance_function = std::make_shared<FunctionNode>("L2SquaredDistance");
+                squared_distance_function->getArguments().getNodes().insert(squared_distance_function->getArguments().getNodes().end(), arguments.begin(), arguments.end());
+                squared_distance_function->resolveAsFunction(FunctionFactory::instance().get("L2SquaredDistance", context));
 
-        auto squared_distance_function = std::make_shared<FunctionNode>("L2SquaredDistance");
-        squared_distance_function->getArguments().getNodes().insert(squared_distance_function->getArguments().getNodes().end(), arguments.begin(), arguments.end());
-        squared_distance_function->resolveAsFunction(FunctionFactory::instance().get("L2SquaredDistance", context));
+                // Создаем узел для функции sqrt.
+                auto sqrt_function = std::make_shared<FunctionNode>("sqrt");
+                sqrt_function->getArguments().getNodes().push_back(squared_distance_function);
+                sqrt_function->resolveAsFunction(FunctionFactory::instance().get("sqrt", context));
 
-        auto sqrt_function = std::make_shared<FunctionNode>("sqrt");
-        sqrt_function->getArguments().getNodes().push_back(squared_distance_function);
-        sqrt_function->resolveAsFunction(FunctionFactory::instance().get("sqrt", context));
+                // Заменяем узел функции L2Distance на узел функции sqrt(L2SquaredDistance).
+                node = sqrt_function;
+            }
+        }
+        // Обработка вызовов функции L2Distance в секции ORDER BY.
+        else if (auto * order_by_node = node->as<OrderByNode>())
+        {
+            auto * function_node_order_by = order_by_node->getChild()->as<FunctionNode>();
+            if (function_node_order_by && function_node_order_by->getFunctionName() == "L2Distance")
+            {
+                auto & arguments_order_by = function_node_order_by->getArguments().getNodes();
+                if (arguments_order_by.size() == 2)
+                {
+                    auto squared_distance_function_order_by = std::make_shared<FunctionNode>("L2SquaredDistance");
+                    squared_distance_function_order_by->getArguments().getNodes().insert(squared_distance_function_order_by->getArguments().getNodes().end(), arguments_order_by.begin(), arguments_order_by.end());
+                    squared_distance_function_order_by->resolveAsFunction(FunctionFactory::instance().get("L2SquaredDistance", context));
 
-        node = sqrt_function;
+                    auto sqrt_function_order_by = std::make_shared<FunctionNode>("sqrt");
+                    sqrt_function_order_by->getArguments().getNodes().push_back(squared_distance_function_order_by);
+                    sqrt_function_order_by->resolveAsFunction(FunctionFactory::instance().get("sqrt", context));
+
+                    order_by_node->setChild(sqrt_function_order_by);
+                }
+            }
+        }
     }
 
 private:
