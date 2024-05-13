6ec868a3df1dac8f905a9df00e609743eb0c3346
divanik <dan.ivanik@clickhouse.com>
2024-05-13 20:04:21 +0000

    Add schema inference test

---
+++ tests/queries/0_stateless/03036_schema_inference_cache_s3_archives.sql
@@ -0,0 +1,6 @@
+select * from s3(s3_conn, filename='03036_archive1.zip :: example{1,2}.csv') order by tuple(*);
+select schema_inference_mode, splitByChar('/', source)[-1] as file, schema from system.schema_inference_cache order by file;
+
+set schema_inference_mode = 'union';
+select * from s3(s3_conn, filename='03036_json_archive.zip :: example{11,12}.jsonl') order by tuple(*);
+select schema_inference_mode, splitByChar('/', source)[-1] as file, schema from system.schema_inference_cache order by file;
