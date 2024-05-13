6942ae0c1e6204d8ee91b8e69e88be85ec289620
vdimir <vdimir@clickhouse.com>
2024-05-13 12:00:52 +0000

    Fix SimpleSquashingChunksTransform (02115_rewrite_local_join_right_distribute_table)

--- src/Processors/Transforms/SquashingChunksTransform.h
+++ src/Processors/Transforms/SquashingChunksTransform.h
@@ -2,6 +2,7 @@
 
 #include <Interpreters/SquashingTransform.h>
 #include <Processors/ISimpleTransform.h>
+#include <Processors/IInflatingTransform.h>
 #include <Processors/Sinks/SinkToStorage.h>
 
 namespace DB
@@ -29,7 +30,7 @@ private:
 };
 
 /// Doesn't care about propagating exceptions and thus doesn't throw LOGICAL_ERROR if the following transform closes its input port.
-class SimpleSquashingChunksTransform : public ISimpleTransform
+class SimpleSquashingChunksTransform : public IInflatingTransform
 {
 public:
     explicit SimpleSquashingChunksTransform(const Block & header, size_t min_block_size_rows, size_t min_block_size_bytes);
@@ -37,14 +38,13 @@ public:
     String getName() const override { return "SimpleSquashingTransform"; }
 
 protected:
-    void transform(Chunk &) override;
-
-    IProcessor::Status prepare() override;
+    void consume(Chunk chunk) override;
+    bool canGenerate(bool is_read_finished) override;
+    Chunk generate() override;
 
 private:
     SquashingTransform squashing;
 
-    /// When consumption is finished we need to release the final chunk regardless of its size.
-    bool finished = false;
+    Block current_block;
 };
 }
