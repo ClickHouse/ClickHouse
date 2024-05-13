ecbffe67a43e7ebb06d3268d343c638a0997e49f
VKlin357 <palysaev.vadim@yandex.ru>
2024-05-13 16:47:09 +0000

    к

--- src/Processors/QueryPlan/Optimizations/L2DistanceOptimize.cpp
+++ src/Processors/QueryPlan/Optimizations/L2DistanceOptimize.cpp
@@ -13,16 +13,13 @@ namespace DB::QueryPlanOptimizations
 {
 size_t tryReplaceL2DistanceWithL2Squared(QueryPlan::Node * parent_node, QueryPlan::Nodes & nodes) {
     // Проверяем, является ли узел ExpressionStep, содержащий вызов L2Distance
-    if (auto *expression_node = typeid_cast<ExpressionStep *>(parent_node->get())) {
-        if (expression_node->expression->function_name == "L2Distance") {
+    if (auto *expression_node = typeid_cast<ExpressionStep *>(parent_node->node)) {
+        if (expression_node->expression->getFunctionName() == "L2Distance") {
             // Заменяем вызов L2Distance на sqrt(L2SquaredDistance)
             auto l2_distance_args = std::move(expression_node->children.front()->children);
             auto l2_squared_distance = std::make_shared<ExpressionFunction>("L2SquaredDistance", l2_distance_args);
             auto sqrt_function = std::make_shared<ExpressionFunction>("sqrt", ExpressionActions::Actions{{}, l2_squared_distance});
             expression_node->expression = sqrt_function;
-
-            // Обновляем информацию о монотонности функции sqrt
-            parent_node->setIsMonotonic(true);
 
             // Возвращаем 1, чтобы указать, что мы внесли изменения
             return 1;
