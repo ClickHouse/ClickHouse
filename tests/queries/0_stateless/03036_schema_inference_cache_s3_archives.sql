1eb72e7af3f7f4d9aed43e535712a4d41dd60b71
divanik <dan.ivanik@clickhouse.com>
2024-05-14 09:25:23 +0000

    Add no fasttest tag

--- tests/queries/0_stateless/03036_schema_inference_cache_s3_archives.sql
+++ tests/queries/0_stateless/03036_schema_inference_cache_s3_archives.sql
@@ -1,3 +1,6 @@
+-- Tags: no-fasttest
+-- Tag no-fasttest: Depends on AWS
+
 select * from s3(s3_conn, filename='03036_archive1.zip :: example{1,2}.csv') order by tuple(*);
 select schema_inference_mode, splitByChar('/', source)[-1] as file, schema from system.schema_inference_cache order by file;
 
