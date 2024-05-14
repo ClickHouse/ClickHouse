5c0a173a321d45cecd0a1d7fe645e2700a0ddc7c
VKlin357 <palysaev.vadim@yandex.ru>
2024-05-14 14:28:03 +0000

    е

--- src/Processors/QueryPlan/Optimizations/L2DistanceOptimize.cpp
+++ src/Processors/QueryPlan/Optimizations/L2DistanceOptimize.cpp
@@ -4,21 +4,25 @@
 #include <Processors/QueryPlan/Optimizations/Optimizations.h>
 #include <Processors/QueryPlan/SortingStep.h>
 #include <Common/Exception.h>
-#include <DataTypes/IDataType.h>
-#include <Processors/QueryPlan/QueryPlan.h>
 #include <Functions/FunctionFactory.h>
 #include <Functions/IFunction.h>
-#include <Functions/FunctionHelpers.h>
 
-namespace DB
+namespace
 {
 
-namespace QueryPlanOptimizations
+const DB::DataStream & getChildOutputStream(DB::QueryPlan::Node & node)
 {
+    if (node.children.size() != 1)
+        throw DB::Exception(DB::ErrorCodes::LOGICAL_ERROR, "Node \"{}\" is expected to have only one child.", node.step->getName());
+    return node.children.front()->step->getOutputStream();
+}
 
-size_t tryReplaceL2DistanceWithL2Squared(QueryPlan::Node * parent_node, QueryPlan::Nodes & nodes)
-{
-   if (parent_node->children.size() != 1)
+}
+
+namespace DB::QueryPlanOptimizations {
+
+size_t tryReplaceL2DistanceWithL2Squared(QueryPlan::Node *parent_node, QueryPlan::Nodes &nodes) {
+    if (parent_node->children.size() != 1)
         return 0;
 
     auto *child_node = parent_node->children.front();
@@ -30,23 +34,17 @@ size_t tryReplaceL2DistanceWithL2Squared(QueryPlan::Node * parent_node, QueryPla
     if (!sorting_step || !expression_step)
         return 0;
 
-    auto &actions = expression_step->getActions();
+    auto actions = expression_step->getActions();
     auto *function_expr = dynamic_cast<FunctionExpression *>(actions.back().get());
 
-    if (!function_expr || function_expr->function_name != "L2Distance")
+    if (!function_expr || function_expr->functionName() != "L2Distance")
         return 0;
 
-    auto *l2_squared_distance_function = FunctionFactory::instance().tryGet("l2SquaredDistance");
-    auto *sqrt_function = FunctionFactory::instance().tryGet("sqrt");
+    auto *l2_squared_distance_function = FunctionFactory::instance().tryGet("l2SquaredDistance", {});
+    auto *sqrt_function = FunctionFactory::instance().tryGet("sqrt", {});
 
-    if (!l2_squared_distance_function)
-        throw Exception("L2SquaredDistance function not found", ErrorCodes::LOGICAL_ERROR);
-
-    if (!sqrt_function)
-        throw Exception("sqrt function not found", ErrorCodes::LOGICAL_ERROR);
-
-    auto sqrt_l2_squared_distance = FunctionFactory::instance().build("sqrt", {function_expr->arguments->clone()});
-    auto l2_squared_distance = FunctionFactory::instance().build("l2SquaredDistance", {function_expr->arguments->clone()});
+    auto sqrt_l2_squared_distance = FunctionFactory::instance().build("sqrt", {function_expr->arguments()->clone()}, {});
+    auto l2_squared_distance = FunctionFactory::instance().build("l2SquaredDistance", {function_expr->arguments()->clone()}, {});
 
     expression_step->replaceAction(actions.size() - 1, std::move(sqrt_l2_squared_distance));
 
@@ -54,14 +52,12 @@ size_t tryReplaceL2DistanceWithL2Squared(QueryPlan::Node * parent_node, QueryPla
     std::swap(node_with_l2_squared.children, child_node->children);
     child_node->children = {&node_with_l2_squared};
 
-    node_with_l2_squared.step = std::make_unique<ExpressionStep>(getChildOutputStream(node_with_l2_squared), std::move(l2_squared_distance));
+    node_with_l2_squared.step = std::make_unique<ExpressionStep>(child_step->getOutputStream(), std::move(l2_squared_distance));
     node_with_l2_squared.step->setStepDescription(child_step->getStepDescription());
 
-    sorting_step->updateInputStream(getChildOutputStream(*child_node));
+    sorting_step->updateInputStream(node_with_l2_squared.step->getOutputStream());
 
     return 3;
 }
 
-}
-
-}
+} // namespace DB::QueryPlanOptimizations
