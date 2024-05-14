c5a84779cdba572af5d616798b521aaa2a99361f
VKlin357 <palysaev.vadim@yandex.ru>
2024-05-14 14:53:10 +0000

    к

--- src/Processors/QueryPlan/Optimizations/L2DistanceOptimize.cpp
+++ src/Processors/QueryPlan/Optimizations/L2DistanceOptimize.cpp
@@ -28,25 +28,28 @@ size_t tryReplaceL2DistanceWithL2Squared(QueryPlan::Node *parent_node, QueryPlan
     auto *child_node = parent_node->children.front();
     auto &parent_step = parent_node->step;
     auto &child_step = child_node->step;
-    auto *sorting_step = typeid_cast<SortingStep *>(parent_step.get());
-    auto *expression_step = typeid_cast<ExpressionStep *>(child_step.get());
+    auto *sorting_step = dynamic_cast<SortingStep *>(parent_step.get());
+    auto *expression_step = dynamic_cast<ExpressionStep *>(child_step.get());
 
     if (!sorting_step || !expression_step)
         return 0;
 
-    auto actions = expression_step->getActions();
-    auto *function_expr = dynamic_cast<FunctionExpression *>(actions.back().get());
-
-    if (!function_expr || function_expr->functionName() != "L2Distance")
+    auto &expressions = expression_step->getExpressions();
+    if (expressions.size() != 1)
         return 0;
 
-    auto *l2_squared_distance_function = FunctionFactory::instance().tryGet("l2SquaredDistance", {});
-    auto *sqrt_function = FunctionFactory::instance().tryGet("sqrt", {});
+    auto &function_expr = expressions.front();
+    if (function_expr->getFunctionName() != "L2Distance")
+        return 0;
 
-    auto sqrt_l2_squared_distance = FunctionFactory::instance().build("sqrt", {function_expr->arguments()->clone()}, {});
-    auto l2_squared_distance = FunctionFactory::instance().build("l2SquaredDistance", {function_expr->arguments()->clone()}, {});
+    auto l2_squared_distance_function = FunctionFactory::instance().get("l2SquaredDistance");
+    auto sqrt_function = FunctionFactory::instance().get("sqrt");
 
-    expression_step->replaceAction(actions.size() - 1, std::move(sqrt_l2_squared_distance));
+    if (!l2_squared_distance_function || !sqrt_function)
+        return 0;
+
+    auto sqrt_l2_squared_distance = std::make_shared<FunctionExpression>(sqrt_function, function_expr->getArguments()->clone());
+    auto l2_squared_distance = std::make_shared<FunctionExpression>(l2_squared_distance_function, function_expr->getArguments()->clone());
 
     auto &node_with_l2_squared = nodes.emplace_back();
     std::swap(node_with_l2_squared.children, child_node->children);
@@ -60,4 +63,4 @@ size_t tryReplaceL2DistanceWithL2Squared(QueryPlan::Node *parent_node, QueryPlan
     return 3;
 }
 
-} // namespace DB::QueryPlanOptimizations
+}
