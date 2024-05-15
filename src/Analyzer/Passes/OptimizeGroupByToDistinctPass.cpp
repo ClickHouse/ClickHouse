7da320450e45056d39672ce27c185356546e0028
VKlin357 <palysaev.vadim@yandex.ru>
2024-05-15 11:12:54 +0000

    r

--- src/Analyzer/Passes/OptimizeGroupByToDistinctPass.cpp
+++ src/Analyzer/Passes/OptimizeGroupByToDistinctPass.cpp
@@ -16,20 +16,15 @@ public:
 
     void enterImpl(QueryTreeNodePtr & node)
     {
-        
         auto * query_node = node->as<QueryNode>();
 
         // Проверяем, что запрос содержит только SELECT
-        if (!query_node || (query_node->hasWith() || query_node->hasPrewhere() || query_node->hasWhere() || query_node->hasHaving() ||
-            query_node->hasWindow() || query_node->hasOrderBy() || query_node->hasLimitByLimit() || query_node->hasLimitByOffset() ||
-            query_node->hasLimitBy() || query_node->hasLimit() || query_node->hasOffset()))
+        if (!query_node)
+        return;
+
+        if (!query_node->hasGroupBy() || query_node->hasAggregates())
             return;
 
-        // Проверяем, что в запросе есть оператор GROUP BY
-        if (!query_node->hasGroupBy())
-            return;
-
-        // Проверяем, что запрос имеет оператор LIMIT
         if (!query_node->hasLimit())
             return;
 
@@ -38,20 +33,21 @@ public:
 
         // Создаем новый узел запроса с оператором DISTINCT
         auto distinct_query_node = std::make_shared<QueryNode>(Context::createCopy(query_node->getContext()));
+        distinct_query_node->setDistinct(true);
+
         distinct_query_node->getJoinTree() = query_node->getJoinTree();
 
         // Копируем столбцы для группировки в узел DISTINCT
         for (const auto & column_node : group_by_columns)
         {
             distinct_query_node->getProjection().getNodes().push_back(column_node);
-            distinct_query_node->getGroupBy().getNodes().push_back(column_node);
         }
 
         // Копируем оператор LIMIT в узел DISTINCT
         distinct_query_node->getLimit() = query_node->getLimit();
 
         // Заменяем текущий узел запроса узлом с оператором DISTINCT
-        query_node = distinct_query_node.get();
+        query_node = distinct_query_node;
     }
 };
 
--- src/Processors/QueryPlan/Optimizations/L2DistanceOptimize.cpp
+++ src/Processors/QueryPlan/Optimizations/L2DistanceOptimize.cpp
@@ -1,54 +1,54 @@
-#include <Interpreters/ActionsDAG.h>
-#include <Processors/QueryPlan/ExpressionStep.h>
-#include <Processors/QueryPlan/FillingStep.h>
-#include <Processors/QueryPlan/Optimizations/Optimizations.h>
-#include <Processors/QueryPlan/SortingStep.h>
-#include <Common/Exception.h>
-#include <Functions/FunctionFactory.h>
-#include <Functions/IFunction.h>
+// #include <Interpreters/ActionsDAG.h>
+// #include <Processors/QueryPlan/ExpressionStep.h>
+// #include <Processors/QueryPlan/FillingStep.h>
+// #include <Processors/QueryPlan/Optimizations/Optimizations.h>
+// #include <Processors/QueryPlan/SortingStep.h>
+// #include <Common/Exception.h>
+// #include <Functions/FunctionFactory.h>
+// #include <Functions/IFunction.h>
 
-namespace
-{
+// namespace
+// {
 
-const DB::DataStream & getChildOutputStream(DB::QueryPlan::Node & node)
-{
-    if (node.children.size() != 1)
-        throw DB::Exception(DB::ErrorCodes::LOGICAL_ERROR, "Node \"{}\" is expected to have only one child.", node.step->getName());
-    return node.children.front()->step->getOutputStream();
-}
+// const DB::DataStream & getChildOutputStream(DB::QueryPlan::Node & node)
+// {
+//     if (node.children.size() != 1)
+//         throw DB::Exception(DB::ErrorCodes::LOGICAL_ERROR, "Node \"{}\" is expected to have only one child.", node.step->getName());
+//     return node.children.front()->step->getOutputStream();
+// }
 
-}
+// }
 
-namespace DB::QueryPlanOptimizations {
+// namespace DB::QueryPlanOptimizations {
 
-size_t tryReplaceL2DistanceWithL2Squared(QueryPlan::Node *parent_node, QueryPlan::Nodes &nodes) {
-    if (!parent_node || parent_node->children.empty())
-        return 0;
+// size_t tryReplaceL2DistanceWithL2Squared(QueryPlan::Node *parent_node, QueryPlan::Nodes &nodes) {
+//     if (!parent_node || parent_node->children.empty())
+//         return 0;
 
-    auto & children = parent_node->children;
+//     auto & children = parent_node->children;
 
-    for (size_t i = 0; i < children.size(); ++i)
-    {
-        auto & child = children[i];
+//     for (size_t i = 0; i < children.size(); ++i)
+//     {
+//         auto & child = children[i];
         
-        // If the child node is of type ExpressionStep and represents L2Distance function
-        if (child->type == QueryPlanNodeType::ExpressionStep &&
-            child->expression == "L2Distance")
-        {
-            // Replace L2Distance with sqrt(L2SquaredDistance)
-            child->expression = "sqrt";
-            child->arguments.clear();
-            child->arguments.emplace_back("L2SquaredDistance");
-            // No need to continue the loop as we've made the replacement
-            return 1;
-        }
+//         // If the child node is of type ExpressionStep and represents L2Distance function
+//         if (child->type == QueryPlanNodeType::ExpressionStep &&
+//             child->expression == "L2Distance")
+//         {
+//             // Replace L2Distance with sqrt(L2SquaredDistance)
+//             child->expression = "sqrt";
+//             child->arguments.clear();
+//             child->arguments.emplace_back("L2SquaredDistance");
+//             // No need to continue the loop as we've made the replacement
+//             return 1;
+//         }
         
-        // Recursively apply the optimization to the child nodes
-        if (tryReplaceL2DistanceWithL2Squared(child.get(), nodes) > 0)
-            return 1;
-    }
+//         // Recursively apply the optimization to the child nodes
+//         if (tryReplaceL2DistanceWithL2Squared(child->get(), nodes) > 0)
+//             return 1;
+//     }
 
-    return 0;
-}
+//     return 0;
+// }
 
-}
+// }
