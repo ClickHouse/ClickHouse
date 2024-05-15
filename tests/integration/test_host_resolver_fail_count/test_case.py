e9d2b54de67868bb1249511809e8b0f768547b63
Anton Ivashkin <iantonspb@yandex-team.ru>
2024-05-15 18:17:50 +0200

    Reformat test file

--- tests/integration/test_host_resolver_fail_count/test_case.py
+++ tests/integration/test_host_resolver_fail_count/test_case.py
@@ -11,6 +11,7 @@ node = cluster.add_instance(
     with_minio=True,
 )
 
+
 @pytest.fixture(scope="module")
 def start_cluster():
     try:
@@ -20,6 +21,7 @@ def start_cluster():
     finally:
         cluster.shutdown()
 
+
 # The same value as in ClickHouse, this can't be confugured via config now
 DEFAULT_RESOLVE_TIME_HISTORY_SECONDS = 2*60
 
@@ -27,31 +29,39 @@ DEFAULT_RESOLVE_TIME_HISTORY_SECONDS = 2*60
 def test_host_resolver(start_cluster):
     minio_ip = cluster.get_instance_ip("minio1")
 
-    # drop DNS cache    
-    node.set_hosts([
-        (minio_ip, "minio1"),
-        (node.ip_address, "minio1"), # no answer on 9001 port on this IP
-        ])
+    # drop DNS cache
+    node.set_hosts(
+        [
+            (minio_ip, "minio1"),
+            (node.ip_address, "minio1"),  # no answer on 9001 port on this IP
+        ]
+    )
     
     node.query("SYSTEM DROP DNS CACHE")
     node.query("SYSTEM DROP CONNECTIONS CACHE")
 
-    node.query("""
+    node.query(
+        """
                 CREATE TABLE test (key UInt32, value UInt32)
                 Engine=MergeTree()
                 ORDER BY key PARTITION BY key
                 SETTINGS storage_policy='s3'
-                """)
+                """
+    )
 
     initial_fails = "0\n"
     k = 0
     limit = 100
     while initial_fails == "0\n":
-        node.query(f"""
+        node.query(
+            f"""
                     INSERT INTO test VALUES (0,{k})
-                    """)
+                    """
+        )
         # HostResolver chooses IP randomly, so on single call can choose worked ID
-        initial_fails = node.query("SELECT value FROM system.events WHERE event LIKE 'AddressesMarkedAsFailed'")
+        initial_fails = node.query(
+            "SELECT value FROM system.events WHERE event LIKE 'AddressesMarkedAsFailed'"
+        )
         k += 1
         if k >= limit:
             # Dead IP was not choosen for 100 iteration.
@@ -63,10 +73,14 @@ def test_host_resolver(start_cluster):
 
     for j in range(10):
         for i in range(10):
-            node.query(f"""
+            node.query(
+                f"""
                         INSERT INTO test VALUES ({i+1},{j+1})
-                        """)
-            fails = node.query("SELECT value FROM system.events WHERE event LIKE 'AddressesMarkedAsFailed'")
+                        """
+            )
+            fails = node.query(
+                "SELECT value FROM system.events WHERE event LIKE 'AddressesMarkedAsFailed'"
+            )
             assert fails == initial_fails
 
     # Check that clickhouse tries to recheck IP after 2 minutes
@@ -75,9 +89,11 @@ def test_host_resolver(start_cluster):
     intermediate_fails = initial_fails
     limit = k + 100
     while intermediate_fails == initial_fails:
-        node.query(f"""
+        node.query(
+            f"""
                     INSERT INTO test VALUES (101,{k})
-                    """)
+                    """
+        )
         intermediate_fails = node.query("SELECT value FROM system.events WHERE event LIKE 'AddressesMarkedAsFailed'")
         k += 1
         if k >= limit:
@@ -92,10 +108,14 @@ def test_host_resolver(start_cluster):
     initial_fails = intermediate_fails
     limit = k + 100
     while intermediate_fails == initial_fails:
-        node.query(f"""
+        node.query(
+            f"""
                     INSERT INTO test VALUES (102,{k})
-                    """)
-        intermediate_fails = node.query("SELECT value FROM system.events WHERE event LIKE 'AddressesMarkedAsFailed'")
+                    """
+        )
+        intermediate_fails = node.query(
+            "SELECT value FROM system.events WHERE event LIKE 'AddressesMarkedAsFailed'"
+        )
         k += 1
         if k >= limit:
             break
