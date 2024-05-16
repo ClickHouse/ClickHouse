a643f2ff99e04acdaa230b0b9166d6a95e91e980
sarielwxm <1059293451@qq.com>
2024-05-16 21:22:54 +0800

    fix

--- src/Processors/QueryPlan/ReadFromLoopStep.cpp
+++ src/Processors/QueryPlan/ReadFromLoopStep.cpp
@@ -9,134 +9,134 @@
 #include <Processors/ISource.h>
 #include <Processors/Sources/NullSource.h>
 #include <Processors/Executors/PullingPipelineExecutor.h>
-#include <Common/FieldVisitors.h>
 
 namespace DB
 {
-class PullingPipelineExecutor;
+    class PullingPipelineExecutor;
 
-class LoopSource : public ISource
-{
-public:
-
-    LoopSource(
-        const Names & column_names_,
-        const SelectQueryInfo & query_info_,
-        const StorageSnapshotPtr & storage_snapshot_,
-        ContextPtr & context_,
-        QueryProcessingStage::Enum processed_stage_,
-        StoragePtr inner_storage_,
-        size_t max_block_size_,
-        size_t num_streams_)
-        : ISource(storage_snapshot_->getSampleBlockForColumns(column_names_))
-        , column_names(column_names_)
-        , query_info(query_info_)
-        , storage_snapshot(storage_snapshot_)
-        , processed_stage(processed_stage_)
-        , context(context_)
-        , inner_storage(std::move(inner_storage_))
-        , max_block_size(max_block_size_)
-        , num_streams(num_streams_)
+    class LoopSource : public ISource
     {
-    }
+    public:
 
-    String getName() const override { return "Loop"; }
-
-    Chunk generate() override
-    {
-        while (true)
+        LoopSource(
+                const Names & column_names_,
+                const SelectQueryInfo & query_info_,
+                const StorageSnapshotPtr & storage_snapshot_,
+                ContextPtr & context_,
+                QueryProcessingStage::Enum processed_stage_,
+                StoragePtr inner_storage_,
+                size_t max_block_size_,
+                size_t num_streams_)
+                : ISource(storage_snapshot_->getSampleBlockForColumns(column_names_))
+                , column_names(column_names_)
+                , query_info(query_info_)
+                , storage_snapshot(storage_snapshot_)
+                , processed_stage(processed_stage_)
+                , context(context_)
+                , inner_storage(std::move(inner_storage_))
+                , max_block_size(max_block_size_)
+                , num_streams(num_streams_)
         {
-            if (!loop)
+        }
+
+        String getName() const override { return "Loop"; }
+
+        Chunk generate() override
+        {
+            while (true)
             {
-                QueryPlan plan;
-                inner_storage->read(
-                    plan,
-                    column_names,
-                    storage_snapshot,
-                    query_info,
-                    context,
-                    processed_stage,
-                    max_block_size,
-                    num_streams);
-                auto builder = plan.buildQueryPipeline(
-                    QueryPlanOptimizationSettings::fromContext(context),
-                    BuildQueryPipelineSettings::fromContext(context));
-                QueryPlanResourceHolder resources;
-                auto pipe = QueryPipelineBuilder::getPipe(std::move(*builder), resources);
-                query_pipeline = QueryPipeline(std::move(pipe));
-                executor = std::make_unique<PullingPipelineExecutor>(query_pipeline);
-                loop = true;
-            }
-            Chunk chunk;
-            if (executor->pull(chunk))
-            {
-                if (chunk)
-                    return chunk;
-            }
-            else
-            {
-                loop = false;
-                executor.reset();
-                query_pipeline.reset();
+                if (!loop)
+                {
+                    QueryPlan plan;
+                    auto storage_snapshot_ = inner_storage->getStorageSnapshotForQuery(inner_storage->getInMemoryMetadataPtr(), nullptr, context);
+                    inner_storage->read(
+                            plan,
+                            column_names,
+                            storage_snapshot_,
+                            query_info,
+                            context,
+                            processed_stage,
+                            max_block_size,
+                            num_streams);
+                    auto builder = plan.buildQueryPipeline(
+                            QueryPlanOptimizationSettings::fromContext(context),
+                            BuildQueryPipelineSettings::fromContext(context));
+                    QueryPlanResourceHolder resources;
+                    auto pipe = QueryPipelineBuilder::getPipe(std::move(*builder), resources);
+                    query_pipeline = QueryPipeline(std::move(pipe));
+                    executor = std::make_unique<PullingPipelineExecutor>(query_pipeline);
+                    loop = true;
+                }
+                Chunk chunk;
+                if (executor->pull(chunk))
+                {
+                    if (chunk)
+                        return chunk;
+                }
+                else
+                {
+                    loop = false;
+                    executor.reset();
+                    query_pipeline.reset();
+                }
             }
         }
-    }
 
-private:
+    private:
 
-    const Names column_names;
-    SelectQueryInfo query_info;
-    const StorageSnapshotPtr storage_snapshot;
-    QueryProcessingStage::Enum processed_stage;
-    ContextPtr context;
-    StoragePtr inner_storage;
-    size_t max_block_size;
-    size_t num_streams;
-    bool loop = false;
-    QueryPipeline query_pipeline;
-    std::unique_ptr<PullingPipelineExecutor> executor;
-};
+        const Names column_names;
+        SelectQueryInfo query_info;
+        const StorageSnapshotPtr storage_snapshot;
+        QueryProcessingStage::Enum processed_stage;
+        ContextPtr context;
+        StoragePtr inner_storage;
+        size_t max_block_size;
+        size_t num_streams;
+        bool loop = false;
+        QueryPipeline query_pipeline;
+        std::unique_ptr<PullingPipelineExecutor> executor;
+    };
 
-ReadFromLoopStep::ReadFromLoopStep(
-    const Names & column_names_,
-    const SelectQueryInfo & query_info_,
-    const StorageSnapshotPtr & storage_snapshot_,
-    const ContextPtr & context_,
-    QueryProcessingStage::Enum processed_stage_,
-    StoragePtr inner_storage_,
-    size_t max_block_size_,
-    size_t num_streams_)
-    : SourceStepWithFilter(
-          DataStream{.header = storage_snapshot_->getSampleBlockForColumns(column_names_)},
-          column_names_,
-          query_info_,
-          storage_snapshot_,
-          context_)
-    , column_names(column_names_)
-    , processed_stage(processed_stage_)
-    , inner_storage(std::move(inner_storage_))
-    , max_block_size(max_block_size_)
-    , num_streams(num_streams_)
-{
-}
-
-Pipe ReadFromLoopStep::makePipe()
-{
-    return Pipe(std::make_shared<LoopSource>(
-        column_names, query_info, storage_snapshot, context, processed_stage, inner_storage, max_block_size, num_streams));
-}
-
-void ReadFromLoopStep::initializePipeline(QueryPipelineBuilder & pipeline, const BuildQueryPipelineSettings &)
-{
-    auto pipe = makePipe();
-
-    if (pipe.empty())
+    ReadFromLoopStep::ReadFromLoopStep(
+            const Names & column_names_,
+            const SelectQueryInfo & query_info_,
+            const StorageSnapshotPtr & storage_snapshot_,
+            const ContextPtr & context_,
+            QueryProcessingStage::Enum processed_stage_,
+            StoragePtr inner_storage_,
+            size_t max_block_size_,
+            size_t num_streams_)
+            : SourceStepWithFilter(
+            DataStream{.header = storage_snapshot_->getSampleBlockForColumns(column_names_)},
+            column_names_,
+            query_info_,
+            storage_snapshot_,
+            context_)
+            , column_names(column_names_)
+            , processed_stage(processed_stage_)
+            , inner_storage(std::move(inner_storage_))
+            , max_block_size(max_block_size_)
+            , num_streams(num_streams_)
     {
-        assert(output_stream != std::nullopt);
-        pipe = Pipe(std::make_shared<NullSource>(output_stream->header));
     }
 
-    pipeline.init(std::move(pipe));
-}
+    Pipe ReadFromLoopStep::makePipe()
+    {
+        return Pipe(std::make_shared<LoopSource>(
+                column_names, query_info, storage_snapshot, context, processed_stage, inner_storage, max_block_size, num_streams));
+    }
+
+    void ReadFromLoopStep::initializePipeline(QueryPipelineBuilder & pipeline, const BuildQueryPipelineSettings &)
+    {
+        auto pipe = makePipe();
+
+        if (pipe.empty())
+        {
+            assert(output_stream != std::nullopt);
+            pipe = Pipe(std::make_shared<NullSource>(output_stream->header));
+        }
+
+        pipeline.init(std::move(pipe));
+    }
 
 }
--- src/Processors/QueryPlan/ReadFromLoopStep.h
+++ src/Processors/QueryPlan/ReadFromLoopStep.h
@@ -1,40 +1,37 @@
 #pragma once
 #include <Core/QueryProcessingStage.h>
-#include <Interpreters/Context_fwd.h>
 #include <Processors/QueryPlan/SourceStepWithFilter.h>
 #include <QueryPipeline/Pipe.h>
-#include <Storages/IStorage_fwd.h>
 #include <Storages/SelectQueryInfo.h>
-#include <Storages/StorageSnapshot.h>
 
 namespace DB
 {
 
-class ReadFromLoopStep final : public SourceStepWithFilter
-{
-public:
-    ReadFromLoopStep(
-        const Names & column_names_,
-        const SelectQueryInfo & query_info_,
-        const StorageSnapshotPtr & storage_snapshot_,
-        const ContextPtr & context_,
-        QueryProcessingStage::Enum processed_stage_,
-        StoragePtr inner_storage_,
-        size_t max_block_size_,
-        size_t num_streams_);
+    class ReadFromLoopStep final : public SourceStepWithFilter
+    {
+    public:
+        ReadFromLoopStep(
+                const Names & column_names_,
+                const SelectQueryInfo & query_info_,
+                const StorageSnapshotPtr & storage_snapshot_,
+                const ContextPtr & context_,
+                QueryProcessingStage::Enum processed_stage_,
+                StoragePtr inner_storage_,
+                size_t max_block_size_,
+                size_t num_streams_);
 
-    String getName() const override { return "ReadFromLoop"; }
+        String getName() const override { return "ReadFromLoop"; }
 
-    void initializePipeline(QueryPipelineBuilder & pipeline, const BuildQueryPipelineSettings &) override;
+        void initializePipeline(QueryPipelineBuilder & pipeline, const BuildQueryPipelineSettings &) override;
 
-private:
+    private:
 
-    Pipe makePipe();
+        Pipe makePipe();
 
-    const Names column_names;
-    QueryProcessingStage::Enum processed_stage;
-    StoragePtr inner_storage;
-    size_t max_block_size;
-    size_t num_streams;
-};
+        const Names column_names;
+        QueryProcessingStage::Enum processed_stage;
+        StoragePtr inner_storage;
+        size_t max_block_size;
+        size_t num_streams;
+    };
 }
