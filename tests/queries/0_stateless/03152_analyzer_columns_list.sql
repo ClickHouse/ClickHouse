61ee5e46ad50fcedd86f6d62d4c2bda2f6fedade
Dmitry Novik <n0vik@clickhouse.com>
2024-05-17 11:43:33 +0200

    Update the test

--- tests/queries/0_stateless/03152_analyzer_columns_list.sql
+++ tests/queries/0_stateless/03152_analyzer_columns_list.sql
@@ -1,3 +1,13 @@
-SELECT COLUMNS(license_text, library_name) APPLY (length) FROM system.licenses ORDER BY library_name LIMIT 1;
+CREATE TABLE test
+(
+    foo String,
+    bar String,
+)
+ENGINE = MergeTree()
+ORDER BY (foo, bar);
 
-SELECT COLUMNS(license_text, library_name, xyz) APPLY (length) FROM system.licenses; -- { serverError UNKNOWN_IDENTIFIER }
+INSERT INTO test VALUES ('foo', 'bar1');
+
+SELECT COLUMNS(bar, foo) APPLY (length) FROM test;
+
+SELECT COLUMNS(bar, foo, xyz) APPLY (length) FROM test; -- { serverError UNKNOWN_IDENTIFIER }
