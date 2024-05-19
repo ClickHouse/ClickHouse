ca8dce13c7efc8f1da4059a010c18988105256b2
wxybear <wangxingyu0927@outlook.com>
2024-05-20 02:16:30 +0800

    feat: add a test for multiquery split(parse)

---
+++ tests/queries/0_stateless/03156_default_multiquery_split.sh
@@ -0,0 +1,40 @@
+#!/usr/bin/env bash
+# Tags: no-fasttest, no-parallel, no-ordinary-database, long
+
+CUR_DIR=$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)
+# shellcheck source=../shell_config.sh
+. "$CUR_DIR"/../shell_config.sh
+
+SQL_FILE_NAME=$"03156_default_multiquery_split_$$.sql"
+
+printf 'drop table if exists test1; drop table if exists test2;
+create table test1 (value Float64) ENGINE=MergeTree ORDER BY tuple();
+create table test2 (value String) ENGINE=MergeTree ORDER BY tuple();
+insert into test1 values
+(101),
+(102);
+insert into test2 format csv
+1
+2;
+
+insert into test2 format csv
+3
+4
+
+6;' > "$SQL_FILE_NAME"
+$CLICKHOUSE_CLIENT -m -n < "$SQL_FILE_NAME"
+
+printf 'insert into test2 values
+('7'),
+('8'),
+
+('9');
+
+select * from test1 order by value;
+select * from test2 order by value;
+drop table test1;drop table test2;
+' > "$SQL_FILE_NAME"
+
+$CLICKHOUSE_CLIENT -m -n < "$SQL_FILE_NAME"
+
+rm "$SQL_FILE_NAME"
