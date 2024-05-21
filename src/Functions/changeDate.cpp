c026a5b7e938b797d3f8cb016457dab38c002aab
Robert Schulze <robert@clickhouse.com>
2024-05-21 05:20:46 +0000

    Fix style check

--- src/Functions/changeDate.cpp
+++ src/Functions/changeDate.cpp
@@ -23,13 +23,6 @@
 namespace DB
 {
 
-namespace ErrorCodes
-{
-    extern const int ILLEGAL_TYPE_OF_ARGUMENT;
-    extern const int LOGICAL_ERROR;
-    extern const int NUMBER_OF_ARGUMENTS_DOESNT_MATCH;
-}
-
 namespace
 {
 
