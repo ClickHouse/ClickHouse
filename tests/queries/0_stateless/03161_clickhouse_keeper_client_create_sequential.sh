37f1d59d122b1b37d7be4fa2386297957255a81c
Duc Canh Le <duccanh.le@ahrefs.com>
2024-05-21 15:55:55 +0000

    fix test
    Signed-off-by: Duc Canh Le <duccanh.le@ahrefs.com>

--- tests/queries/0_stateless/03161_clickhouse_keeper_client_create_sequential.sh
+++ tests/queries/0_stateless/03161_clickhouse_keeper_client_create_sequential.sh
@@ -9,6 +9,7 @@ path="/test-keeper-client-$CLICKHOUSE_DATABASE"
 $CLICKHOUSE_KEEPER_CLIENT -q "rm $path" >& /dev/null
 
 $CLICKHOUSE_KEEPER_CLIENT -q "create $path 'foobar'"
-$CLICKHOUSE_KEEPER_CLIENT -q "create $path/tmp- 'foobar0' EPHEMERAL SEQUENTIAL; get $path/tmp-0000000000"
-$CLICKHOUSE_KEEPER_CLIENT -q "create $path/tmp- 'foobar1' PERSISTENT SEQUENTIAL; get $path/tmp-0000000001"
+$CLICKHOUSE_KEEPER_CLIENT -q "create $path/tmp- 'foobar0' EPHEMERAL SEQUENTIAL"
+$CLICKHOUSE_KEEPER_CLIENT -q "create $path/tmp- 'foobar1' PERSISTENT SEQUENTIAL"
+$CLICKHOUSE_KEEPER_CLIENT -q "get $path/tmp-0000000001"
 $CLICKHOUSE_KEEPER_CLIENT -q "rmr $path"
