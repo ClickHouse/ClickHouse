68a08e5a5e52f37ab1cdb614b7c4a00d285d88fb
Yarik Briukhovetskyi <114298166+yariks5s@users.noreply.github.com>
2024-05-22 12:50:00 +0000

    add test for #37090

---
+++ tests/queries/0_stateless/03158_unkn_col_distributed_table_with_alias.sql
@@ -0,0 +1,6 @@
+drop table IF EXISTS local;
+drop table IF EXISTS dist;
+CREATE TABLE local(`dummy` UInt8) ENGINE = MergeTree ORDER BY tuple();
+CREATE TABLE dist AS default.local ENGINE = Distributed(localhost_cluster, currentDatabase(), local);
+SET prefer_localhost_replica = 1;
+WITH 'Hello' AS `alias` SELECT `alias` FROM default.dist GROUP BY `alias`;
