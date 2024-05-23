c1950236ced0b110e679c4042d1fab2c7df26f2f
Robert Schulze <robert@clickhouse.com>
2024-05-23 15:24:18 +0000

    Cosmetics, pt. IV

--- src/Functions/generateSerialID.cpp
+++ src/Functions/generateSerialID.cpp
@@ -12,8 +12,6 @@ namespace DB
 
 namespace ErrorCodes
 {
-    extern const int NUMBER_OF_ARGUMENTS_DOESNT_MATCH;
-    extern const int ILLEGAL_TYPE_OF_ARGUMENT;
     extern const int KEEPER_EXCEPTION;
 }
 
