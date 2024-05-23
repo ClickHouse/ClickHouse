5718375131970bf8107b38d54f7747bb27d30978
Francisco Javier Jurado Moreno <9376816+Beetelbrox@users.noreply.github.com>
2024-05-23 15:15:02 +0200

    Restore newline

--- src/Functions/FunctionsComparison.h
+++ src/Functions/FunctionsComparison.h
@@ -1169,6 +1169,7 @@ public:
         const DataTypeTuple * right_tuple = checkAndGetDataType<DataTypeTuple>(arguments[1].get());
 
         bool both_represented_by_number = arguments[0]->isValueRepresentedByNumber() && arguments[1]->isValueRepresentedByNumber();
+
         bool has_date = left.isDateOrDate32() || right.isDateOrDate32();
         if (!((both_represented_by_number && !has_date)   /// Do not allow to compare date and number.
             || (left.isStringOrFixedString() || right.isStringOrFixedString())  /// Everything can be compared with string by conversion.
