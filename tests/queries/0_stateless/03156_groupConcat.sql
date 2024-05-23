8693082a631b54c1efd87fdbc678e61b0ed298f6
Maksim Galkin <msgalkin@edu.hse.ru>
2024-05-23 19:19:03 +0000

    update tests

--- tests/queries/0_stateless/03156_groupConcat.sql
+++ tests/queries/0_stateless/03156_groupConcat.sql
@@ -19,3 +19,9 @@ SELECT groupConcat(',')(p_string) FROM test_groupConcat;
 SELECT groupConcat(',', 2)(p_array) FROM test_groupConcat;
 
 SELECT groupConcat(p_int) FROM test_groupConcat WHERE id = 1;
+
+SELECT groupConcat(123)(number) FROM system.numbers(10); -- { serverError ILLEGAL_TYPE_OF_ARGUMENT }
+SELECT groupConcat(',', '3')(number) FROM system.numbers(10); -- { serverError BAD_ARGUMENTS }
+SELECT groupConcat(',', 0)(number) FROM system.numbers(10); -- { serverError BAD_ARGUMENTS }
+SELECT groupConcat(',', -1)(number) FROM system.numbers(10); -- { serverError BAD_ARGUMENTS }
+SELECT groupConcat(',', 3, 3)(number) FROM system.numbers(10); -- { serverError TOO_MANY_ARGUMENTS_FOR_FUNCTION }
