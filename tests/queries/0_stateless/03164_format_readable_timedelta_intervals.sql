e61a6fcb45f829d6d9507099746664f0e9850a07
Francisco Javier Jurado Moreno <9376816+Beetelbrox@users.noreply.github.com>
2024-05-23 23:07:46 +0200

    Improve tests

--- tests/queries/0_stateless/03164_format_readable_timedelta_intervals.sql
+++ tests/queries/0_stateless/03164_format_readable_timedelta_intervals.sql
@@ -1,8 +1,18 @@
-SELECT formatReadableTimeDelta(INTERVAL 1 SECOND);
+SELECT formatReadableTimeDelta(INTERVAL 1 NANOSECOND, 'seconds');
+SELECT formatReadableTimeDelta(INTERVAL 1 MICROSECOND, 'seconds');
+SELECT formatReadableTimeDelta(INTERVAL 1 MILLISECOND, 'seconds');
+SELECT formatReadableTimeDelta(INTERVAL 1 SECOND, 'seconds');
+SELECT formatReadableTimeDelta(INTERVAL 1 MINUTE, 'seconds');
+SELECT formatReadableTimeDelta(INTERVAL 1 HOUR, 'seconds');
+SELECT formatReadableTimeDelta(INTERVAL 1 DAY, 'seconds');
+SELECT formatReadableTimeDelta(INTERVAL 1 WEEK, 'seconds');
+
+-- MONTH, QUARTER & YEAR don't work as they don't represent constant intervals
+
 SELECT formatReadableTimeDelta(INTERVAL 60 SECOND);
 SELECT formatReadableTimeDelta(INTERVAL 3601 SECOND);
 SELECT formatReadableTimeDelta(INTERVAL 61 MINUTE);
-
 SELECT formatReadableTimeDelta(INTERVAL 3600 SECOND, 'minutes');
+
 SELECT formatReadableTimeDelta(-INTERVAL 1 SECOND);
 SELECT formatReadableTimeDelta(-INTERVAL 60 SECOND);
