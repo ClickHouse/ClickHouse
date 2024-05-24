fe8ede53a23a0fb573fe56d379cefa706d4e40ec
Dmitry Novik <n0vik@clickhouse.com>
2024-05-24 15:07:45 +0200

    Update 01372_wrong_order_by_removal

--- tests/queries/0_stateless/01372_wrong_order_by_removal.sql
+++ tests/queries/0_stateless/01372_wrong_order_by_removal.sql
@@ -6,4 +6,5 @@ CREATE TEMPORARY TABLE moving_sum_num
 );
 
 -- ORDER BY from subquery shall not be removed.
-EXPLAIN SYNTAX SELECT k, groupArrayMovingSum(v) FROM (SELECT * FROM moving_sum_num ORDER BY k, dt) GROUP BY k ORDER BY k;
+EXPLAIN SYNTAX SELECT k, groupArrayMovingSum(v) FROM (SELECT * FROM moving_sum_num ORDER BY k, dt) GROUP BY k ORDER BY k SETTINGS allow_experimental_analyzer = 0;
+EXPLAIN SYNTAX SELECT k, groupArrayMovingSum(v) FROM (SELECT * FROM moving_sum_num ORDER BY k, dt) GROUP BY k ORDER BY k SETTINGS allow_experimental_analyzer = 1;
