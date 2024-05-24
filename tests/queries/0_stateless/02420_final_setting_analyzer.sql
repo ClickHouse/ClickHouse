1837569181f2b5bf25de9c1ee7cdeb231417cb22
Dmitry Novik <n0vik@clickhouse.com>
2024-05-24 14:47:14 +0200

    Update 02420_final_setting_analyzer

--- tests/queries/0_stateless/02420_final_setting_analyzer.sql
+++ tests/queries/0_stateless/02420_final_setting_analyzer.sql
@@ -71,7 +71,13 @@ ORDER BY left_table.id, val_left, val_middle, val_right;
 explain syntax select left_table.id,val_left, val_middle, val_right from left_table
                                                                              inner join middle_table on left_table.id = middle_table.id
                                                                              inner join right_table on middle_table.id = right_table.id
-               ORDER BY left_table.id, val_left, val_middle, val_right;
+ORDER BY left_table.id, val_left, val_middle, val_right
+SETTINGS allow_experimental_analyzer = 0;
+explain syntax select left_table.id,val_left, val_middle, val_right from left_table
+                                                                             inner join middle_table on left_table.id = middle_table.id
+                                                                             inner join right_table on middle_table.id = right_table.id
+ORDER BY left_table.id, val_left, val_middle, val_right
+SETTINGS allow_experimental_analyzer = 1;
 
 -- extra: same with subquery
 select left_table.id,val_left, val_middle, val_right from left_table
