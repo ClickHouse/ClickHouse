32a974bc361ab75fa454a8d61a84b8559f2c1aed
Dmitry Novik <n0vik@clickhouse.com>
2024-05-24 14:09:00 +0200

    Update 01582_deterministic_function_with_predicate

--- tests/queries/0_stateless/01582_deterministic_function_with_predicate.sql
+++ tests/queries/0_stateless/01582_deterministic_function_with_predicate.sql
@@ -1,1 +1,2 @@
-EXPLAIN SYNTAX SELECT count(*) FROM ( SELECT number FROM ( SELECT number FROM numbers(1000000) ) WHERE rand64() < (0.01 * 18446744073709552000.));
+EXPLAIN SYNTAX SELECT count(*) FROM ( SELECT number FROM ( SELECT number FROM numbers(1000000) ) WHERE rand64() < (0.01 * 18446744073709552000.)) SETTINGS allow_experimental_analyzer = 0;
+EXPLAIN SYNTAX SELECT count(*) FROM ( SELECT number FROM ( SELECT number FROM numbers(1000000) ) WHERE rand64() < (0.01 * 18446744073709552000.)) SETTINGS allow_experimental_analyzer = 1;
