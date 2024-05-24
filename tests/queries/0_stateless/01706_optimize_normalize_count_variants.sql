6b4705bfcdbceba62f1c48a53df9e977294742d2
Dmitry Novik <n0vik@clickhouse.com>
2024-05-24 16:06:55 +0200

    Update 01706_optimize_normalize_count_variants

--- tests/queries/0_stateless/01706_optimize_normalize_count_variants.sql
+++ tests/queries/0_stateless/01706_optimize_normalize_count_variants.sql
@@ -1,8 +1,10 @@
 
 set optimize_normalize_count_variants = 1;
 
-explain syntax select count(), count(1), count(-1), sum(1), count(null);
+explain syntax select count(), count(1), count(-1), sum(1), count(null) SETTINGS allow_experimental_analyzer = 0;
+explain syntax select count(), count(1), count(-1), sum(1), count(null) SETTINGS allow_experimental_analyzer = 1;
 
 set aggregate_functions_null_for_empty = 1;
 
-explain syntax select sum(1) from numbers(10) where 0;
+explain syntax select sum(1) from numbers(10) where 0 SETTINGS allow_experimental_analyzer = 0;
+explain syntax select sum(1) from numbers(10) where 0 SETTINGS allow_experimental_analyzer = 1;
