6878506b6bbcbdd352b95bf2db1fe61572455194
Alex Koledaev <ax3l3rator@gmail.com>
2024-05-26 16:51:37 +0300

    Improved comments in IProtocolServer

--- src/Server/IProtocolServer.h
+++ src/Server/IProtocolServer.h
@@ -21,30 +21,34 @@ public:
 
     virtual ~IProtocolServer() = default;
 
-    /// Starts the server. A new thread will be created that waits for and accepts incoming connections.
+    // Starts the server. A new thread will be created that waits for and accepts incoming connections.
     virtual void start() = 0;
 
-    /// Stops the server. No new connections will be accepted.
+    // Stops the server. No new connections will be accepted.
     virtual void stop() { is_open = false; }
 
-    // Returns the state of the server
+    // Returns if the server is accepting connections.
     bool isOpen() const { return is_open; }
+
+    // Returns if the server is stopping.
     bool isStopping() const { return !is_open; }
 
-    /// Returns the port this server is listening to.
+    // Returns the port this server is listening to.
     UInt16 portNumber() const { return port_number; }
+
+    // Returns the name of the port this server is listening to.
     const std::string & getPortName() const { return port_name; }
 
-    /// Returns the number of current threads.
+    // Returns the number of current threads.
     virtual size_t currentThreads() const = 0;
 
-    /// Returns the number of currently handled connections.
+    // Returns the number of currently handled connections.
     virtual size_t currentConnections() const = 0;
 
-    // Returns server's listen host
+    // Returns server's listen host.
     const std::string & getListenHost() const { return listen_host; }
 
-    // Returns server's description
+    // Returns server's description.
     const std::string & getDescription() const { return description; }
 
 private:
