09606d376099fff81a274088224c6f36ed4a8de3
Roman Glinskikh <glinskikh.roman@yandex.ru>
2024-05-27 17:06:34 +0300

    fix test

--- tests/queries/0_stateless/03152_partition_by_hyperplanes.sql
+++ tests/queries/0_stateless/03152_partition_by_hyperplanes.sql
@@ -2,4 +2,4 @@ select hex((SELECT partitionByHyperplanes([toFloat32(2.0), toFloat32(3.0)], [toF
 select hex((SELECT partitionByHyperplanes([toFloat32(2.0), toFloat32(3.0)], [toFloat32(1.0), toFloat32(-1.0)])));
 SELECT hex((select partitionByHyperplanes([toFloat32(2.0), toFloat32(3.0)], [[toFloat32(1.0), toFloat32(1.0)], [toFloat32(1.0), toFloat32(2.0)]])));
 
-SELECT hex((select partitionByHyperplanes([toFloat32(2.0), toFloat32(3.0)], [[toFloat32(1.0), toFloat32(1.0)], [toFloat32(1.0)]])));
+SELECT hex((select partitionByHyperplanes([toFloat32(2.0), toFloat32(3.0)], [[toFloat32(1.0), toFloat32(1.0)], [toFloat32(1.0)]])));  -- { serverError 190 }
