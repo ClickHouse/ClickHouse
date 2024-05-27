b5daa653ee2c1afba391691d3d9006422bf319c9
Francisco Javier Jurado Moreno <9376816+Beetelbrox@users.noreply.github.com>
2024-05-27 15:22:44 +0200

    At even more tests

--- tests/queries/0_stateless/03166_from_readable_size.sql
+++ tests/queries/0_stateless/03166_from_readable_size.sql
@@ -24,29 +24,38 @@ SELECT formatReadableDecimalSize(fromReadableSize('1 mb'));
 SELECT fromReadableSize('1.00 KiB');    -- 1024
 SELECT fromReadableSize('3.00 KiB');    -- 3072
 
--- Resulting bytes are rounded up
-SELECT fromReadableSize('1.0001 KiB');  -- 1025
+-- Should be able to parse negative numbers
+SELECT fromReadableSize('-1.00 KiB');    -- 1024
 
 -- Infix whitespace is ignored
 SELECT fromReadableSize('1    KiB');
 SELECT fromReadableSize('1KiB');
 
+-- Can parse LowCardinality
+SELECT fromReadableSize(toLowCardinality('1 KiB'));
+
+-- Can parse nullable fields
+SELECT fromReadableSize(toNullable('1 KiB'));
+
+-- Can parse non-const columns fields
+SELECT fromReadableSize(materialize('1 KiB'));
+
+-- Output is NULL if NULL arg is passed
+SELECT fromReadableSize(NULL);
 
 -- ERRORS
 -- No arguments
-SELECT fromReadableSize() -- { serverError NUMBER_OF_ARGUMENTS_DOESNT_MATCH }
+SELECT fromReadableSize(); -- { serverError NUMBER_OF_ARGUMENTS_DOESNT_MATCH }
 -- Too many arguments
-SELECT fromReadableSize('1 B', '2 B') -- { serverError NUMBER_OF_ARGUMENTS_DOESNT_MATCH }
+SELECT fromReadableSize('1 B', '2 B'); -- { serverError NUMBER_OF_ARGUMENTS_DOESNT_MATCH }
 -- Wrong Type
-SELECT fromReadableSize(12) -- { serverError ILLEGAL_TYPE_OF_ARGUMENT }
+SELECT fromReadableSize(12); -- { serverError ILLEGAL_TYPE_OF_ARGUMENT }
 -- Invalid input - overall garbage
-SELECT fromReadableSize("oh no") -- { serverError BAD_ARGUMENTS }
+SELECT fromReadableSize('oh no'); -- { serverError BAD_ARGUMENTS }
 -- Invalid input - unknown unit
-SELECT fromReadableSize("12.3 rb") -- { serverError BAD_ARGUMENTS }
+SELECT fromReadableSize('12.3 rb'); -- { serverError BAD_ARGUMENTS }
 -- Invalid input - Leading whitespace
-SELECT fromReadableSize(' 1 B') -- { serverError BAD_ARGUMENTS }
+SELECT fromReadableSize(' 1 B'); -- { serverError BAD_ARGUMENTS }
 -- Invalid input - Trailing characters
-SELECT fromReadableSize('1 B leftovers') -- { serverError BAD_ARGUMENTS }
-SELECT fromReadableSize(' 1 B') -- { serverError BAD_ARGUMENTS }
--- Invalid input - Result too big to fit in output typez
-SELECT fromReadableSize('1000000 EiB') -- { serverError BAD_ARGUMENTS }
+SELECT fromReadableSize('1 B leftovers'); -- { serverError BAD_ARGUMENTS }
+SELECT fromReadableSize(' 1 B'); -- { serverError BAD_ARGUMENTS }
