3bd3717d3437ab00aa240e4908b9b82542c42de1
liuneng <1398775315@qq.com>
2024-05-28 17:47:39 +0800

    revert setting rename

--- tests/queries/0_stateless/03164_adapting_parquet_reader_output_size.sql
+++ tests/queries/0_stateless/03164_adapting_parquet_reader_output_size.sql
@@ -6,7 +6,7 @@ INSERT INTO test_parquet SELECT rand(),rand(),rand(),rand(),rand(),rand(),rand()
 SELECT max(blockSize()) FROM test_parquet;
 
 DROP TABLE IF EXISTS test_parquet;
-CREATE TABLE test_parquet (col1 String, col2 String, col3 String, col4 String, col5 String, col6 String, col7 String) ENGINE=File(Parquet) settings input_format_parquet_max_block_rows=16;
+CREATE TABLE test_parquet (col1 String, col2 String, col3 String, col4 String, col5 String, col6 String, col7 String) ENGINE=File(Parquet) settings input_format_parquet_max_block_size=16;
 INSERT INTO test_parquet SELECT rand(),rand(),rand(),rand(),rand(),rand(),rand() FROM numbers(100000);
 SELECT max(blockSize()) FROM test_parquet;
 
