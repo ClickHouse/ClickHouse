16fb16ae2654cd6c59b64699f3438b3a33462c4c
Francisco Javier Jurado Moreno <9376816+Beetelbrox@users.noreply.github.com>
2024-05-28 12:28:15 +0200

    Add code documentation for all flavours of fromReadable

--- src/Functions/fromReadableDecimalSize.cpp
+++ src/Functions/fromReadableDecimalSize.cpp
@@ -8,7 +8,6 @@ namespace DB
 namespace
 {
 
-// ISO/IEC 80000-13 binary units
 const std::unordered_map<std::string_view, size_t> scale_factors =
 {
     {"b", 1L},
@@ -47,29 +46,50 @@ using FunctionFromReadableDecimalSize = FunctionFromReadable<NameFromReadableDec
 using FunctionFromReadableDecimalSizeOrNull = FunctionFromReadable<NameFromReadableDecimalSizeOrNull, Impl, ErrorHandling::Null>;
 using FunctionFromReadableDecimalSizeOrZero = FunctionFromReadable<NameFromReadableDecimalSizeOrZero, Impl, ErrorHandling::Zero>;
 
+
+FunctionDocumentation fromReadableDecimalSize_documentation {
+    .description = "Given a string containing the readable representation of a byte size with decimal units this function returns the corresponding number of bytes.",
+    .syntax = "fromReadableDecimalSize(x)",
+    .arguments = {{"x", "Readable size with decimal units ([String](../../sql-reference/data-types/string.md))"}},
+    .returned_value = "Number of bytes, rounded up to the nearest integer ([UInt64](../../sql-reference/data-types/int-uint.md))",
+    .examples = {
+        {"example_integer", "SELECT fromReadableDecimalSize('1 KB')", "1024"},
+        {"example_decimal", "SELECT fromReadableDecimalSize('1.1 KB')", "1127"},
+    },
+    .categories = {"OtherFunctions"},
+};
+
+FunctionDocumentation fromReadableDecimalSizeOrNull_documentation {
+    .description = "Given a string containing the readable representation of a byte size with decimal units this function returns the corresponding number of bytes, or `NULL` if unable to parse the value.",
+    .syntax = "fromReadableDecimalSizeOrNull(x)",
+    .arguments = {{"x", "Readable size with decimal units ([String](../../sql-reference/data-types/string.md))"}},
+    .returned_value = "Number of bytes, rounded up to the nearest integer, or NULL if unable to parse the input (Nullable([UInt64](../../sql-reference/data-types/int-uint.md)))",
+    .examples = {
+        {"example_integer", "SELECT fromReadableDecimalSizeOrNull('1 KiB')", "1024"},
+        {"example_decimal", "SELECT fromReadableDecimalSizeOrNull('1.1 KiB')", "1127"},
+        {"example_null", "SELECT fromReadableDecimalSizeOrNull('invalid')", "NULL"},
+    },
+    .categories = {"OtherFunctions"},
+};
+
+FunctionDocumentation fromReadableDecimalSizeOrZero_documentation {
+    .description = "Given a string containing the readable representation of a byte size with decimal units this function returns the corresponding number of bytes, or 0 if unable to parse the value.",
+    .syntax = "formatReadableSizeOrZero(x)",
+    .arguments = {{"x", "Readable size with decimal units ([String](../../sql-reference/data-types/string.md))"}},
+    .returned_value = "Number of bytes, rounded up to the nearest integer, or 0 if unable to parse the input ([UInt64](../../sql-reference/data-types/int-uint.md))",
+    .examples = {
+        {"example_integer", "SELECT fromReadableDecimalSizeOrZero('1 KiB')", "1024"},
+        {"example_decimal", "SELECT fromReadableDecimalSizeOrZero('1.1 KiB')", "1127"},
+        {"example_null", "SELECT fromReadableDecimalSizeOrZero('invalid')", "0"},
+    },
+    .categories = {"OtherFunctions"},
+};
 }
 
 REGISTER_FUNCTION(FromReadableDecimalSize)
 {
-    factory.registerFunction<FunctionFromReadableDecimalSize>(FunctionDocumentation
-        {
-            .description=R"(
-Given a string containing the readable representation of a byte size, this function returns the corresponding number of bytes:
-[example:basic_binary]
-[example:basic_decimal]
-
-Accepts readable sizes up to the Exabyte (EB/EiB).
-
-)",
-            .examples{
-                {"basic_binary", "SELECT fromReadableSize('1 KiB')", "1024"},
-                {"basic_decimal", "SELECT fromReadableSize('1.523 KB')", "1523"},
-            },
-            .categories{"OtherFunctions"}
-        }
-    );
-    factory.registerFunction<FunctionFromReadableDecimalSizeOrNull>();
-    factory.registerFunction<FunctionFromReadableDecimalSizeOrZero>();
+    factory.registerFunction<FunctionFromReadableDecimalSize>(fromReadableDecimalSize_documentation);
+    factory.registerFunction<FunctionFromReadableDecimalSizeOrNull>(fromReadableDecimalSizeOrNull_documentation);
+    factory.registerFunction<FunctionFromReadableDecimalSizeOrZero>(fromReadableDecimalSizeOrZero_documentation);
 }
-
 }
--- src/Functions/fromReadableSize.cpp
+++ src/Functions/fromReadableSize.cpp
@@ -1,6 +1,7 @@
 #include <base/types.h>
 #include <Functions/FunctionFactory.h>
 #include <Functions/fromReadable.h>
+#include "Common/FunctionDocumentation.h"
 
 namespace DB
 {
@@ -49,28 +50,49 @@ using FunctionFromReadableSize = FunctionFromReadable<NameFromReadableSize, Impl
 using FunctionFromReadableSizeOrNull = FunctionFromReadable<NameFromReadableSizeOrNull, Impl, ErrorHandling::Null>;
 using FunctionFromReadableSizeOrZero = FunctionFromReadable<NameFromReadableSizeOrZero, Impl, ErrorHandling::Zero>;
 
+FunctionDocumentation fromReadableSize_documentation {
+    .description = "Given a string containing the readable representation of a byte size with ISO/IEC 80000-13 units this function returns the corresponding number of bytes.",
+    .syntax = "fromReadableSize(x)",
+    .arguments = {{"x", "Readable size with ISO/IEC 80000-13 units ([String](../../sql-reference/data-types/string.md))"}},
+    .returned_value = "Number of bytes, rounded up to the nearest integer ([UInt64](../../sql-reference/data-types/int-uint.md))",
+    .examples = {
+        {"example_integer", "SELECT fromReadableSize('1 KiB')", "1024"},
+        {"example_decimal", "SELECT fromReadableSize('1.1 KiB')", "1127"},
+    },
+    .categories = {"OtherFunctions"},
+};
+
+FunctionDocumentation fromReadableSizeOrNull_documentation {
+    .description = "Given a string containing the readable representation of a byte size with ISO/IEC 80000-13 units this function returns the corresponding number of bytes, or `NULL` if unable to parse the value.",
+    .syntax = "fromReadableSizeOrNull(x)",
+    .arguments = {{"x", "Readable size with ISO/IEC 80000-13 units ([String](../../sql-reference/data-types/string.md))"}},
+    .returned_value = "Number of bytes, rounded up to the nearest integer, or NULL if unable to parse the input (Nullable([UInt64](../../sql-reference/data-types/int-uint.md)))",
+    .examples = {
+        {"example_integer", "SELECT fromReadableSizeOrNull('1 KiB')", "1024"},
+        {"example_decimal", "SELECT fromReadableSizeOrNull('1.1 KiB')", "1127"},
+        {"example_null", "SELECT fromReadableSizeOrNull('invalid')", "NULL"},
+    },
+    .categories = {"OtherFunctions"},
+};
+
+FunctionDocumentation fromReadableSizeOrZero_documentation {
+    .description = "Given a string containing the readable representation of a byte size with ISO/IEC 80000-13 units this function returns the corresponding number of bytes, or 0 if unable to parse the value.",
+    .syntax = "fromReadableSizeOrZero(x)",
+    .arguments = {{"x", "Readable size with ISO/IEC 80000-13 units ([String](../../sql-reference/data-types/string.md))"}},
+    .returned_value = "Number of bytes, rounded up to the nearest integer, or 0 if unable to parse the input ([UInt64](../../sql-reference/data-types/int-uint.md))",
+    .examples = {
+        {"example_integer", "SELECT fromReadableSizeOrZero('1 KiB')", "1024"},
+        {"example_decimal", "SELECT fromReadableSizeOrZero('1.1 KiB')", "1127"},
+        {"example_null", "SELECT fromReadableSizeOrZero('invalid')", "0"},
+    },
+    .categories = {"OtherFunctions"},
+};
 }
 
 REGISTER_FUNCTION(FromReadableSize)
 {
-    factory.registerFunction<FunctionFromReadableSize>(FunctionDocumentation
-        {
-            .description=R"(
-Given a string containing the readable representation of a byte size, this function returns the corresponding number of bytes:
-[example:basic_binary]
-[example:basic_decimal]
-
-Accepts readable sizes up to the Exabyte (EB/EiB).
-
-)",
-            .examples{
-                {"basic_binary", "SELECT fromReadableSize('1 KiB')", "1024"},
-                {"basic_decimal", "SELECT fromReadableSize('1.523 KB')", "1523"},
-            },
-            .categories{"OtherFunctions"}
-        }
-    );
-    factory.registerFunction<FunctionFromReadableSizeOrNull>();
-    factory.registerFunction<FunctionFromReadableSizeOrZero>();
+    factory.registerFunction<FunctionFromReadableSize>(fromReadableSize_documentation);
+    factory.registerFunction<FunctionFromReadableSizeOrNull>(fromReadableSizeOrNull_documentation);
+    factory.registerFunction<FunctionFromReadableSizeOrZero>(fromReadableSizeOrZero_documentation);
 }
 }
