c3ac0117951577f669d3b6c1f4f40559bb7da533
Igor Markelov <ismarkelov@edu.hse.ru>
2024-05-28 11:21:59 +0000

    Update comments for test

--- tests/queries/0_stateless/03164_optimize_row_order_during_insert.sql
+++ tests/queries/0_stateless/03164_optimize_row_order_during_insert.sql
@@ -1,10 +1,10 @@
--- Checks that no bad things happen when the table optimizes the row order to improve compressability during inserts.
+-- Checks that no bad things happen when the table optimizes the row order to improve compressability during just simple insert.
 
 DROP TABLE IF EXISTS tab;
 
 CREATE TABLE tab (name String, event Int8) ENGINE = MergeTree ORDER BY name SETTINGS allow_experimental_optimized_row_order = true;
 INSERT INTO tab VALUES ('Igor', 3), ('Egor', 1), ('Egor', 2), ('Igor', 2), ('Igor', 1);
 
-SELECT * FROM tab ORDER BY name SETTINGS max_threads=1;
+SELECT * FROM tab SETTINGS max_threads=1;
 
 DROP TABLE tab;
