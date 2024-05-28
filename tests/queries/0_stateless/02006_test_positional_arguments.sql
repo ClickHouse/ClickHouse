c027cf2ebe86b8549a353150d36a6f1bdb944334
Dmitry Novik <n0vik@clickhouse.com>
2024-05-28 18:31:57 +0200

    Update 02006_test_positional_arguments

--- tests/queries/0_stateless/02006_test_positional_arguments.sql
+++ tests/queries/0_stateless/02006_test_positional_arguments.sql
@@ -33,26 +33,52 @@ select x1, x2, x3 from test order by 3 limit 1 by 3;
 select x1, x2, x3 from test order by x3 limit 1 by x1;
 select x1, x2, x3 from test order by 3 limit 1 by 1;
 
-explain syntax select x3, x2, x1 from test order by 1;
-explain syntax select x3 + 1, x2, x1 from test order by 1;
-explain syntax select x3, x2, x1 from test order by -1;
-explain syntax select x3 + 1, x2, x1 from test order by -1;
-explain syntax select x3, x3 - x2, x2, x1 from test order by 2;
-explain syntax select x3, x3 - x2, x2, x1 from test order by -2;
-explain syntax select x3, if(x3 > 10, x3, plus(x1, x2)), x1 + x2 from test order by 2;
-explain syntax select x3, if(x3 > 10, x3, plus(x1, x2)), x1 + x2 from test order by -2;
-explain syntax select max(x1), x2 from test group by 2 order by 1, 2;
-explain syntax select max(x1), x2 from test group by -1 order by -2, -1;
-explain syntax select 1 + greatest(x1, 1), x2 from test group by 1, 2;
-explain syntax select 1 + greatest(x1, 1), x2 from test group by -2, -1;
+explain syntax select x3, x2, x1 from test order by 1 SETTINGS allow_experimental_analyzer = 0;
+explain query tree select x3, x2, x1 from test order by 1 SETTINGS allow_experimental_analyzer = 1;
+
+explain syntax select x3 + 1, x2, x1 from test order by 1 SETTINGS allow_experimental_analyzer = 0;
+explain query tree select x3 + 1, x2, x1 from test order by 1 SETTINGS allow_experimental_analyzer = 1;
+
+explain syntax select x3, x2, x1 from test order by -1 SETTINGS allow_experimental_analyzer = 0;
+explain query tree select x3, x2, x1 from test order by -1 SETTINGS allow_experimental_analyzer = 1;
+
+explain syntax select x3 + 1, x2, x1 from test order by -1 SETTINGS allow_experimental_analyzer = 0;
+explain query tree select x3 + 1, x2, x1 from test order by -1 SETTINGS allow_experimental_analyzer = 1;
+
+explain syntax select x3, x3 - x2, x2, x1 from test order by 2 SETTINGS allow_experimental_analyzer = 0;
+explain query tree select x3, x3 - x2, x2, x1 from test order by 2 SETTINGS allow_experimental_analyzer = 1;
+
+explain syntax select x3, x3 - x2, x2, x1 from test order by -2 SETTINGS allow_experimental_analyzer = 0;
+explain query tree select x3, x3 - x2, x2, x1 from test order by -2 SETTINGS allow_experimental_analyzer = 1;
+
+explain syntax select x3, if(x3 > 10, x3, plus(x1, x2)), x1 + x2 from test order by 2 SETTINGS allow_experimental_analyzer = 0;
+explain query tree select x3, if(x3 > 10, x3, plus(x1, x2)), x1 + x2 from test order by 2 SETTINGS allow_experimental_analyzer = 1;
+
+explain syntax select x3, if(x3 > 10, x3, plus(x1, x2)), x1 + x2 from test order by -2 SETTINGS allow_experimental_analyzer = 0;
+explain query tree select x3, if(x3 > 10, x3, plus(x1, x2)), x1 + x2 from test order by -2 SETTINGS allow_experimental_analyzer = 1;
+
+explain syntax select max(x1), x2 from test group by 2 order by 1, 2 SETTINGS allow_experimental_analyzer = 0;
+explain query tree select max(x1), x2 from test group by 2 order by 1, 2 SETTINGS allow_experimental_analyzer = 1;
+
+explain syntax select max(x1), x2 from test group by -1 order by -2, -1 SETTINGS allow_experimental_analyzer = 0;
+explain query tree select max(x1), x2 from test group by -1 order by -2, -1 SETTINGS allow_experimental_analyzer = 1;
+
+explain syntax select 1 + greatest(x1, 1), x2 from test group by 1, 2 SETTINGS allow_experimental_analyzer = 0;
+explain query tree select 1 + greatest(x1, 1), x2 from test group by 1, 2 SETTINGS allow_experimental_analyzer = 1;
+
+explain syntax select 1 + greatest(x1, 1), x2 from test group by -2, -1 SETTINGS allow_experimental_analyzer = 0;
+explain query tree select 1 + greatest(x1, 1), x2 from test group by -2, -1 SETTINGS allow_experimental_analyzer = 1;
 
 select max(x1), x2 from test group by 1, 2; -- { serverError 43, 184 }
 select 1 + max(x1), x2 from test group by 1, 2; -- { serverError 43, 184 }
 select max(x1), x2 from test group by -2, -1; -- { serverError 43, 184 }
 select 1 + max(x1), x2 from test group by -2, -1; -- { serverError 43, 184 }
 
-explain syntax select x1 + x3, x3 from test group by 1, 2;
-explain syntax select x1 + x3, x3 from test group by -2, -1;
+explain syntax select x1 + x3, x3 from test group by 1, 2 SETTINGS allow_experimental_analyzer = 0;
+explain query tree select x1 + x3, x3 from test group by 1, 2 SETTINGS allow_experimental_analyzer = 1;
+
+explain syntax select x1 + x3, x3 from test group by -2, -1 SETTINGS allow_experimental_analyzer = 0;
+explain query tree select x1 + x3, x3 from test group by -2, -1 SETTINGS allow_experimental_analyzer = 1;
 
 create table test2(x1 Int, x2 Int, x3 Int) engine=Memory;
 insert into test2 values (1, 10, 100), (10, 1, 10), (100, 100, 1);
@@ -62,7 +88,8 @@ select x1, x1 * 2, max(x2), max(x3) from test2 group by 2, 1, x1 order by 1, 2, 
 select a, b, c, d, e, f  from (select 44 a, 88 b, 13 c, 14 d, 15 e, 16 f) t group by 1,2,3,4,5,6 order by a;
 select a, b, c, d, e, f  from (select 44 a, 88 b, 13 c, 14 d, 15 e, 16 f) t group by 1,2,3,-3,-2,-1 order by a;
 
-explain syntax select plus(1, 1) as a group by a;
+explain syntax select plus(1, 1) as a group by a SETTINGS allow_experimental_analyzer = 0;
+explain query tree select plus(1, 1) as a group by a SETTINGS allow_experimental_analyzer = 1;
 select substr('aaaaaaaaaaaaaa', 8) as a  group by a order by a;
 select substr('aaaaaaaaaaaaaa', 8) as a  group by substr('aaaaaaaaaaaaaa', 8) order by a;
 
