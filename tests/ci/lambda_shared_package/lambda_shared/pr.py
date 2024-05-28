a15db7b40099a797c69991965afff2b1ffdf41f4
robot-clickhouse <robot-clickhouse@users.noreply.github.com>
2024-05-28 11:16:44 +0000

    Automatic style fix

--- tests/ci/lambda_shared_package/lambda_shared/pr.py
+++ tests/ci/lambda_shared_package/lambda_shared/pr.py
@@ -86,9 +86,7 @@ LABEL_CATEGORIES = {
         "Bug Fix (user-visible misbehaviour in official stable or prestable release)",
         "Bug Fix (user-visible misbehavior in official stable or prestable release)",
     ],
-    "pr-critical-bugfix": [
-        "Critical Bug Fix (crash, LOGICAL_ERROR, data loss, RBAC)"
-    ],
+    "pr-critical-bugfix": ["Critical Bug Fix (crash, LOGICAL_ERROR, data loss, RBAC)"],
     "pr-build": [
         "Build/Testing/Packaging Improvement",
         "Build Improvement",
