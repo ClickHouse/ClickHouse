bfd61043cd346cc1ee8853eb611a6e815a7aafe1
liuneng <1398775315@qq.com>
2024-05-29 18:10:00 +0800

    fix ut

--- tests/queries/0_stateless/03164_adapting_parquet_reader_output_size.sql
+++ tests/queries/0_stateless/03164_adapting_parquet_reader_output_size.sql
@@ -1,5 +1,7 @@
 -- Tags: no-fasttest, no-parallel
 
+set max_insert_threads=1;
+
 DROP TABLE IF EXISTS test_parquet;
 CREATE TABLE test_parquet (col1 String, col2 String, col3 String, col4 String, col5 String, col6 String, col7 String) ENGINE=File(Parquet);
 INSERT INTO test_parquet SELECT rand(),rand(),rand(),rand(),rand(),rand(),rand() FROM numbers(100000);
@@ -16,7 +18,7 @@ INSERT INTO test_parquet SELECT rand(),rand(),rand(),rand(),rand(),rand(),rand()
 SELECT max(blockSize()) FROM test_parquet;
 
 DROP TABLE IF EXISTS test_parquet;
-CREATE TABLE test_parquet (col1 String, col2 String, col3 String, col4 String, col5 String, col6 String, col7 String) ENGINE=File(Parquet) settings input_format_parquet_prefer_block_bytes=30000;
+CREATE TABLE test_parquet (col1 String, col2 String, col3 String, col4 String, col5 String, col6 String, col7 String) ENGINE=File(Parquet) settings input_format_parquet_prefer_block_bytes=30720;
 INSERT INTO test_parquet SELECT rand(),rand(),rand(),rand(),rand(),rand(),rand() FROM numbers(100000);
 SELECT max(blockSize()) FROM test_parquet;
 
