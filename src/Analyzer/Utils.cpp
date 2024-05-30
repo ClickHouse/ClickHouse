9ad616adb53e0248acfba3151790a20d7b9d2df2
Maksim Kita <kitaetoya@gmail.com>
2024-05-30 15:23:17 +0300

    Fixed build

--- src/Analyzer/Utils.cpp
+++ src/Analyzer/Utils.cpp
@@ -370,11 +370,11 @@ void addTableExpressionModifiersToTablesInsideSubquery(const QueryTreeNodePtr & 
     for (auto & table_or_table_function_node : table_or_table_function_nodes)
     {
         if (auto * identifier_node = table_or_table_function_node->as<IdentifierNode>())
-            identifier_node->setOrMergeTableExpressionModifiers(std::move(table_expression_modifiers));
+            identifier_node->setOrMergeTableExpressionModifiers(table_expression_modifiers);
         else if (auto * table_node = table_or_table_function_node->as<TableNode>())
-            table_node->setOrMergeTableExpressionModifiers(std::move(table_expression_modifiers));
+            table_node->setOrMergeTableExpressionModifiers(table_expression_modifiers);
         else if (auto * table_function_node = table_or_table_function_node->as<TableFunctionNode>())
-            table_function_node->setOrMergeTableExpressionModifiers(std::move(table_expression_modifiers));
+            table_function_node->setOrMergeTableExpressionModifiers(table_expression_modifiers);
         else
             throw Exception(ErrorCodes::LOGICAL_ERROR, "Expected table, table function or identifier node");
     }
