6a8bd46d3688544ed05c3d98a2e0042258cab88c
Anton Popov <anton@clickhouse.com>
2024-05-31 15:51:49 +0000

    add tests

---
+++ tests/queries/0_stateless/03168_read_in_order_buffering_2.sql
@@ -0,0 +1,16 @@
+-- Tags: long, no-random-settings
+
+DROP TABLE IF EXISTS t_read_in_order_2;
+
+CREATE TABLE t_read_in_order_2 (id UInt64, v UInt64) ENGINE = MergeTree ORDER BY id;
+
+INSERT INTO t_read_in_order_2 SELECT number, number FROM numbers(100000000);
+
+SET optimize_read_in_order = 1;
+SET max_threads = 8;
+SET read_in_order_max_bytes_to_buffer = '100M';
+SET max_memory_usage = '250M';
+
+SELECT * FROM t_read_in_order_2 ORDER BY id FORMAT Null;
+
+DROP TABLE t_read_in_order_2;
