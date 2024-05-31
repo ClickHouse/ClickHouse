e3d57ab117391c3b99a8937783320a8c59e0b196
Yakov Olkhovskiy <yakov@clickhouse.com>
2024-05-31 16:05:42 +0000

    set default protocol to notchunked_optional for cluster clients

--- src/Interpreters/Cluster.h
+++ src/Interpreters/Cluster.h
@@ -114,8 +114,8 @@ public:
         UInt16 port{0};
         String user;
         String password;
-        String proto_send_chunked;
-        String proto_recv_chunked;
+        String proto_send_chunked = "notchunked_optional";
+        String proto_recv_chunked = "notchunked_optional";
         String quota_key;
 
         /// For inter-server authorization
