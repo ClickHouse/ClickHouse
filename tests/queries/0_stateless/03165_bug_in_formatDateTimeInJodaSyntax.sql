223712f48569c44b257ed99b08d5a5363d862999
Robert Schulze <robert@clickhouse.com>
2024-05-31 12:17:04 +0000

    Fixups

--- tests/queries/0_stateless/03165_bug_in_formatDateTimeInJodaSyntax.sql
+++
@@ -1,5 +0,0 @@
-select formatDateTimeInJodaSyntax(toDate32('2012-05-29'), 'D');
-select formatDateTimeInJodaSyntax(toDate32('2012-04-19'), 'D');
-select formatDateTimeInJodaSyntax(toDate32('2010-10-27 13:41:27.0'), 'D');
-
-
