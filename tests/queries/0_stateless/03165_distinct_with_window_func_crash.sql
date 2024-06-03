f8e6614b80108016e10fba835df06c0bbfa126e6
Igor Nikonov <igor@clickhouse.com>
2024-06-03 20:52:23 +0000

    Fix crash with DISTINCT and window functions

---
+++ tests/queries/0_stateless/03165_distinct_with_window_func_crash.sql
@@ -0,0 +1,25 @@
+DROP TABLE IF EXISTS atable;
+
+CREATE TABLE atable
+(
+    cdu_date Int16,
+    loanx_id String,
+    rating_sp String
+)
+ENGINE = MergeTree
+ORDER BY tuple();
+
+SELECT DISTINCT
+    loanx_id,
+    rating_sp,
+    cdu_date,
+    row_number() OVER (PARTITION BY cdu_date) AS row_number,
+    last_value(cdu_date) OVER (PARTITION BY loanx_id ORDER BY cdu_date ASC) AS last_cdu_date
+FROM atable
+GROUP BY
+    cdu_date,
+    loanx_id,
+    rating_sp
+SETTINGS query_plan_remove_redundant_distinct = 1;
+
+DROP TABLE atable;
