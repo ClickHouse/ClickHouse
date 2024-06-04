b9758944c9ac11a4fdf1be2b26fdb91cc841a523
morningchen <morningchen@tencent.com>
2024-06-04 18:07:14 +0800

    Fix token extractor for function startsWith and endsWith

--- src/Interpreters/ITokenExtractor.h
+++ src/Interpreters/ITokenExtractor.h
@@ -16,10 +16,13 @@ struct ITokenExtractor
     /// Fast inplace implementation for regular use.
     /// Gets string (data ptr and len) and start position for extracting next token (state of extractor).
     /// Returns false if parsing is finished, otherwise returns true.
-    virtual bool nextInString(const char * data, size_t length, size_t * __restrict pos, size_t * __restrict token_start, size_t * __restrict token_length) const = 0;
+    virtual bool nextInString(
+        const char * data, size_t length, size_t * __restrict pos, size_t * __restrict token_start, size_t * __restrict token_length) const
+        = 0;
 
     /// Optimized version that can assume at least 15 padding bytes after data + len (as our Columns provide).
-    virtual bool nextInStringPadded(const char * data, size_t length, size_t * __restrict pos, size_t * __restrict token_start, size_t * __restrict token_length) const
+    virtual bool nextInStringPadded(
+        const char * data, size_t length, size_t * __restrict pos, size_t * __restrict token_start, size_t * __restrict token_length) const
     {
         return nextInString(data, length, pos, token_start, token_length);
     }
@@ -37,6 +40,14 @@ struct ITokenExtractor
 
     virtual void stringLikeToBloomFilter(const char * data, size_t length, BloomFilter & bloom_filter) const = 0;
 
+    /// Special implementation for creating bloom filter for startsWith function.
+    /// It ignores the last token.
+    virtual void stringStartsWithToBloomFilter(const char * data, size_t length, BloomFilter & bloom_filter) const = 0;
+
+    /// Special implementation for creating bloom filter for startsWith function.
+    /// It ignores the first token.
+    virtual void stringEndsWithToBloomFilter(const char * data, size_t length, BloomFilter & bloom_filter) const = 0;
+
     virtual void stringToGinFilter(const char * data, size_t length, GinFilter & gin_filter) const = 0;
 
     virtual void stringPaddedToGinFilter(const char * data, size_t length, GinFilter & gin_filter) const
@@ -46,6 +57,9 @@ struct ITokenExtractor
 
     virtual void stringLikeToGinFilter(const char * data, size_t length, GinFilter & gin_filter) const = 0;
 
+    virtual void stringStartsWithToGinFilter(const char * data, size_t length, GinFilter & gin_filter) const = 0;
+
+    virtual void stringEndsWithToGinFilter(const char * data, size_t length, GinFilter & gin_filter) const = 0;
 };
 
 using TokenExtractorPtr = const ITokenExtractor *;
@@ -82,6 +96,39 @@ class ITokenExtractorHelper : public ITokenExtractor
             bloom_filter.add(token.c_str(), token.size());
     }
 
+    void stringStartsWithToBloomFilter(const char * data, size_t length, BloomFilter & bloom_filter) const override
+    {
+        size_t cur = 0;
+        size_t token_start = 0, prev_token_start = 0;
+        size_t token_len = 0, prev_token_len = 0;
+
+        while (cur < length && static_cast<const Derived *>(this)->nextInString(data, length, &cur, &token_start, &token_len))
+        {
+            if (prev_token_len > 0)
+            {
+                bloom_filter.add(data + prev_token_start, prev_token_len);
+            }
+            prev_token_start = token_start;
+            prev_token_len = token_len;
+        }
+    }
+
+    void stringEndsWithToBloomFilter(const char * data, size_t length, BloomFilter & bloom_filter) const override
+    {
+        size_t cur = 0;
+        size_t token_start = 0;
+        size_t token_len = 0;
+        bool first_token = true;
+
+        while (cur < length && static_cast<const Derived *>(this)->nextInString(data, length, &cur, &token_start, &token_len))
+        {
+            if (!first_token)
+                bloom_filter.add(data + token_start, token_len);
+            else
+                first_token = false;
+        }
+    }
+
     void stringToGinFilter(const char * data, size_t length, GinFilter & gin_filter) const override
     {
         gin_filter.setQueryString(data, length);
@@ -116,24 +163,65 @@ class ITokenExtractorHelper : public ITokenExtractor
         while (cur < length && static_cast<const Derived *>(this)->nextInStringLike(data, length, &cur, token))
             gin_filter.addTerm(token.c_str(), token.size());
     }
+
+    void stringStartsWithToGinFilter(const char * data, size_t length, GinFilter & gin_filter) const override
+    {
+        gin_filter.setQueryString(data, length);
+
+        size_t cur = 0;
+        size_t token_start = 0, prev_token_start = 0;
+        size_t token_len = 0, prev_token_len = 0;
+
+        while (cur < length && static_cast<const Derived *>(this)->nextInString(data, length, &cur, &token_start, &token_len))
+        {
+            if (prev_token_len > 0)
+            {
+                gin_filter.addTerm(data + prev_token_start, prev_token_len);
+            }
+            prev_token_start = token_start;
+            prev_token_len = token_len;
+        }
+    }
+
+    void stringEndsWithToGinFilter(const char * data, size_t length, GinFilter & gin_filter) const override
+    {
+        gin_filter.setQueryString(data, length);
+
+        size_t cur = 0;
+        size_t token_start = 0;
+        size_t token_len = 0;
+        bool first_token = true;
+
+        while (cur < length && static_cast<const Derived *>(this)->nextInString(data, length, &cur, &token_start, &token_len))
+        {
+            if (!first_token)
+                gin_filter.addTerm(data + token_start, token_len);
+            else
+                first_token = false;
+        }
+    }
 };
 
 
 /// Parser extracting all ngrams from string.
 struct NgramTokenExtractor final : public ITokenExtractorHelper<NgramTokenExtractor>
 {
-    explicit NgramTokenExtractor(size_t n_) : n(n_) {}
+    explicit NgramTokenExtractor(size_t n_) : n(n_) { }
 
     static const char * getName() { return "ngrambf_v1"; }
 
-    bool nextInString(const char * data, size_t length, size_t *  __restrict pos, size_t * __restrict token_start, size_t * __restrict token_length) const override;
+    bool nextInString(
+        const char * data,
+        size_t length,
+        size_t * __restrict pos,
+        size_t * __restrict token_start,
+        size_t * __restrict token_length) const override;
 
     bool nextInStringLike(const char * data, size_t length, size_t * pos, String & token) const override;
 
     size_t getN() const { return n; }
 
 private:
-
     size_t n;
 };
 
@@ -142,12 +230,20 @@ struct SplitTokenExtractor final : public ITokenExtractorHelper<SplitTokenExtrac
 {
     static const char * getName() { return "tokenbf_v1"; }
 
-    bool nextInString(const char * data, size_t length, size_t * __restrict pos, size_t * __restrict token_start, size_t * __restrict token_length) const override;
+    bool nextInString(
+        const char * data,
+        size_t length,
+        size_t * __restrict pos,
+        size_t * __restrict token_start,
+        size_t * __restrict token_length) const override;
 
-    bool nextInStringPadded(const char * data, size_t length, size_t * __restrict pos, size_t * __restrict token_start, size_t * __restrict token_length) const override;
+    bool nextInStringPadded(
+        const char * data,
+        size_t length,
+        size_t * __restrict pos,
+        size_t * __restrict token_start,
+        size_t * __restrict token_length) const override;
 
     bool nextInStringLike(const char * data, size_t length, size_t * __restrict pos, String & token) const override;
-
 };
-
 }
