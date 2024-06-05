877717840bd889d0952531b2159321737d1d6200
Arthur Passos <arthur.ti@outlook.com>
2024-06-05 10:18:04 -0300

    fix and simplify proxy provider ut

--- src/Common/tests/gtest_proxy_configuration_resolver_provider.cpp
+++ src/Common/tests/gtest_proxy_configuration_resolver_provider.cpp
@@ -1,6 +1,9 @@
 #include <gtest/gtest.h>
 
 #include <Common/ProxyConfigurationResolverProvider.h>
+#include <Common/RemoteProxyConfigurationResolver.h>
+#include <Common/ProxyListConfigurationResolver.h>
+#include <Common/EnvironmentProxyConfigurationResolver.h>
 #include <Common/tests/gtest_global_context.h>
 #include <Common/tests/gtest_helper_functions.h>
 
@@ -38,18 +41,11 @@ TEST_F(ProxyConfigurationResolverProviderTests, EnvironmentResolverShouldBeUsedI
     EnvironmentProxySetter setter(http_env_proxy_server, https_env_proxy_server, no_proxy_hosts);
     const auto & config = getContext().context->getConfigRef();
 
-    auto http_configuration = DB::ProxyConfigurationResolverProvider::get(DB::ProxyConfiguration::Protocol::HTTP, config)->resolve();
-    auto https_configuration = DB::ProxyConfigurationResolverProvider::get(DB::ProxyConfiguration::Protocol::HTTPS, config)->resolve();
+    auto http_resolver = DB::ProxyConfigurationResolverProvider::get(DB::ProxyConfiguration::Protocol::HTTP, config);
+    auto https_resolver = DB::ProxyConfigurationResolverProvider::get(DB::ProxyConfiguration::Protocol::HTTPS, config);
 
-    ASSERT_EQ(http_configuration.host, http_env_proxy_server.getHost());
-    ASSERT_EQ(http_configuration.port, http_env_proxy_server.getPort());
-    ASSERT_EQ(http_configuration.protocol, DB::ProxyConfiguration::protocolFromString(http_env_proxy_server.getScheme()));
-    ASSERT_EQ(http_configuration.no_proxy_hosts, no_proxy_hosts);
-
-    ASSERT_EQ(https_configuration.host, https_env_proxy_server.getHost());
-    ASSERT_EQ(https_configuration.port, https_env_proxy_server.getPort());
-    ASSERT_EQ(https_configuration.protocol, DB::ProxyConfiguration::protocolFromString(https_env_proxy_server.getScheme()));
-    ASSERT_EQ(https_configuration.no_proxy_hosts, no_proxy_hosts);
+    ASSERT_TRUE(std::dynamic_pointer_cast<DB::EnvironmentProxyConfigurationResolver>(http_resolver));
+    ASSERT_TRUE(std::dynamic_pointer_cast<DB::EnvironmentProxyConfigurationResolver>(https_resolver));
 }
 
 TEST_F(ProxyConfigurationResolverProviderTests, ListHTTPOnly)
@@ -62,19 +58,11 @@ TEST_F(ProxyConfigurationResolverProviderTests, ListHTTPOnly)
     config->setString("proxy.http.uri", http_list_proxy_server.toString());
     context->setConfig(config);
 
-    auto http_proxy_configuration = DB::ProxyConfigurationResolverProvider::get(DB::ProxyConfiguration::Protocol::HTTP, *config)->resolve();
+    auto http_resolver = DB::ProxyConfigurationResolverProvider::get(DB::ProxyConfiguration::Protocol::HTTP, *config);
+    auto https_resolver = DB::ProxyConfigurationResolverProvider::get(DB::ProxyConfiguration::Protocol::HTTPS, *config);
 
-    ASSERT_EQ(http_proxy_configuration.host, http_list_proxy_server.getHost());
-    ASSERT_EQ(http_proxy_configuration.port, http_list_proxy_server.getPort());
-    ASSERT_EQ(http_proxy_configuration.protocol, DB::ProxyConfiguration::protocolFromString(http_list_proxy_server.getScheme()));
-    ASSERT_EQ(http_proxy_configuration.no_proxy_hosts, no_proxy_hosts);
-
-    auto https_proxy_configuration = DB::ProxyConfigurationResolverProvider::get(DB::ProxyConfiguration::Protocol::HTTPS, *config)->resolve();
-
-    // No https configuration since it's not set
-    ASSERT_EQ(https_proxy_configuration.host, "");
-    ASSERT_EQ(https_proxy_configuration.port, 0);
-    ASSERT_EQ(https_proxy_configuration.no_proxy_hosts, "");
+    ASSERT_TRUE(std::dynamic_pointer_cast<DB::ProxyListConfigurationResolver>(http_resolver));
+    ASSERT_TRUE(std::dynamic_pointer_cast<DB::EnvironmentProxyConfigurationResolver>(https_resolver));
 }
 
 TEST_F(ProxyConfigurationResolverProviderTests, ListHTTPSOnly)
@@ -87,19 +75,11 @@ TEST_F(ProxyConfigurationResolverProviderTests, ListHTTPSOnly)
     config->setString("proxy.https.uri", https_list_proxy_server.toString());
     context->setConfig(config);
 
-    auto http_proxy_configuration = DB::ProxyConfigurationResolverProvider::get(DB::ProxyConfiguration::Protocol::HTTP, *config)->resolve();
+    auto http_resolver = DB::ProxyConfigurationResolverProvider::get(DB::ProxyConfiguration::Protocol::HTTP, *config);
+    auto https_resolver = DB::ProxyConfigurationResolverProvider::get(DB::ProxyConfiguration::Protocol::HTTPS, *config);
 
-    ASSERT_EQ(http_proxy_configuration.host, "");
-    ASSERT_EQ(http_proxy_configuration.port, 0);
-
-    auto https_proxy_configuration = DB::ProxyConfigurationResolverProvider::get(DB::ProxyConfiguration::Protocol::HTTPS, *config)->resolve();
-
-    ASSERT_EQ(https_proxy_configuration.host, https_list_proxy_server.getHost());
-
-    // still HTTP because the proxy host is not HTTPS
-    ASSERT_EQ(https_proxy_configuration.protocol, DB::ProxyConfiguration::protocolFromString(https_list_proxy_server.getScheme()));
-    ASSERT_EQ(https_proxy_configuration.port, https_list_proxy_server.getPort());
-    ASSERT_EQ(https_proxy_configuration.no_proxy_hosts, no_proxy_hosts);
+    ASSERT_TRUE(std::dynamic_pointer_cast<DB::EnvironmentProxyConfigurationResolver>(http_resolver));
+    ASSERT_TRUE(std::dynamic_pointer_cast<DB::ProxyListConfigurationResolver>(https_resolver));
 }
 
 TEST_F(ProxyConfigurationResolverProviderTests, ListBoth)
@@ -117,35 +97,38 @@ TEST_F(ProxyConfigurationResolverProviderTests, ListBoth)
 
     context->setConfig(config);
 
-    auto http_proxy_configuration = DB::ProxyConfigurationResolverProvider::get(DB::ProxyConfiguration::Protocol::HTTP, *config)->resolve();
+    auto http_resolver = DB::ProxyConfigurationResolverProvider::get(DB::ProxyConfiguration::Protocol::HTTP, *config);
+    auto https_resolver = DB::ProxyConfigurationResolverProvider::get(DB::ProxyConfiguration::Protocol::HTTPS, *config);
 
-    ASSERT_EQ(http_proxy_configuration.host, http_list_proxy_server.getHost());
-    ASSERT_EQ(http_proxy_configuration.protocol, DB::ProxyConfiguration::protocolFromString(http_list_proxy_server.getScheme()));
-    ASSERT_EQ(http_proxy_configuration.port, http_list_proxy_server.getPort());
-    ASSERT_EQ(http_proxy_configuration.no_proxy_hosts, no_proxy_hosts);
-
-    auto https_proxy_configuration = DB::ProxyConfigurationResolverProvider::get(DB::ProxyConfiguration::Protocol::HTTPS, *config)->resolve();
-
-    ASSERT_EQ(https_proxy_configuration.host, https_list_proxy_server.getHost());
-
-    // still HTTP because the proxy host is not HTTPS
-    ASSERT_EQ(https_proxy_configuration.protocol, DB::ProxyConfiguration::protocolFromString(https_list_proxy_server.getScheme()));
-    ASSERT_EQ(https_proxy_configuration.port, https_list_proxy_server.getPort());
-    ASSERT_EQ(https_proxy_configuration.no_proxy_hosts, no_proxy_hosts);
+    ASSERT_TRUE(std::dynamic_pointer_cast<DB::ProxyListConfigurationResolver>(http_resolver));
+    ASSERT_TRUE(std::dynamic_pointer_cast<DB::ProxyListConfigurationResolver>(https_resolver));
 }
 
-TEST_F(ProxyConfigurationResolverProviderTests, RemoteResolverIsBasedOnProtocolConfigurationHTTP)
+TEST_F(ProxyConfigurationResolverProviderTests, RemoteResolverIsBasedOnProtocolConfigurationHTTPS)
 {
-    /*
-     * Since there is no way to call `ProxyConfigurationResolver::resolve` on remote resolver,
-     * it is hard to verify the remote resolver was actually picked. One hackish way to assert
-     * the remote resolver was OR was not picked based on the configuration, is to use the
-     * environment resolver. Since the environment resolver is always returned as a fallback,
-     * we can assert the remote resolver was not picked if `ProxyConfigurationResolver::resolve`
-     * succeeds and returns an environment proxy configuration.
-     * */
-    EnvironmentProxySetter setter(http_env_proxy_server, https_env_proxy_server);
+    ConfigurationPtr config = Poco::AutoPtr(new Poco::Util::MapConfiguration());
 
+    config->setString("proxy", "");
+    config->setString("proxy.http", "");
+    config->setString("proxy.http.resolver", "");
+    config->setString("proxy.http.resolver.endpoint", "http://resolver:8080/hostname");
+
+    // even tho proxy protocol / scheme is https, it should not be picked (prior to this PR, it would be picked)
+    config->setString("proxy.http.resolver.proxy_scheme", "https");
+    config->setString("proxy.http.resolver.proxy_port", "80");
+    config->setString("proxy.http.resolver.proxy_cache_time", "10");
+
+    context->setConfig(config);
+
+    auto http_resolver = DB::ProxyConfigurationResolverProvider::get(DB::ProxyConfiguration::Protocol::HTTP, *config);
+    auto https_resolver = DB::ProxyConfigurationResolverProvider::get(DB::ProxyConfiguration::Protocol::HTTPS, *config);
+
+    ASSERT_TRUE(std::dynamic_pointer_cast<DB::RemoteProxyConfigurationResolver>(http_resolver));
+    ASSERT_TRUE(std::dynamic_pointer_cast<DB::EnvironmentProxyConfigurationResolver>(https_resolver));
+}
+
+TEST_F(ProxyConfigurationResolverProviderTests, RemoteResolverHTTPSOnly)
+{
     ConfigurationPtr config = Poco::AutoPtr(new Poco::Util::MapConfiguration());
 
     config->setString("proxy", "");
@@ -161,66 +144,21 @@ TEST_F(ProxyConfigurationResolverProviderTests, RemoteResolverIsBasedOnProtocolC
 
     context->setConfig(config);
 
-    auto http_proxy_configuration = DB::ProxyConfigurationResolverProvider::get(DB::ProxyConfiguration::Protocol::HTTP, *config)->resolve();
+    auto http_resolver = DB::ProxyConfigurationResolverProvider::get(DB::ProxyConfiguration::Protocol::HTTP, *config);
+    auto https_resolver = DB::ProxyConfigurationResolverProvider::get(DB::ProxyConfiguration::Protocol::HTTPS, *config);
 
-    /*
-     * Asserts env proxy is used and not the remote resolver. If the remote resolver is picked, it is an error because
-     * there is no `http` specification for remote resolver
-     * */
-    ASSERT_EQ(http_proxy_configuration.host, http_env_proxy_server.getHost());
-    ASSERT_EQ(http_proxy_configuration.port, http_env_proxy_server.getPort());
-    ASSERT_EQ(http_proxy_configuration.protocol, DB::ProxyConfiguration::protocolFromString(http_env_proxy_server.getScheme()));
-    ASSERT_EQ(http_proxy_configuration.no_proxy_hosts, "");
+    ASSERT_TRUE(std::dynamic_pointer_cast<DB::EnvironmentProxyConfigurationResolver>(http_resolver));
+    ASSERT_TRUE(std::dynamic_pointer_cast<DB::RemoteProxyConfigurationResolver>(https_resolver));
 }
-
-TEST_F(ProxyConfigurationResolverProviderTests, RemoteResolverIsBasedOnProtocolConfigurationHTTPS)
-{
-    /*
-     * Since there is no way to call `ProxyConfigurationResolver::resolve` on remote resolver,
-     * it is hard to verify the remote resolver was actually picked. One hackish way to assert
-     * the remote resolver was OR was not picked based on the configuration, is to use the
-     * environment resolver. Since the environment resolver is always returned as a fallback,
-     * we can assert the remote resolver was not picked if `ProxyConfigurationResolver::resolve`
-     * succeeds and returns an environment proxy configuration.
-     * */
-    EnvironmentProxySetter setter(http_env_proxy_server, https_env_proxy_server);
-
-    ConfigurationPtr config = Poco::AutoPtr(new Poco::Util::MapConfiguration());
-
-    config->setString("proxy", "");
-    config->setString("proxy.http", "");
-    config->setString("proxy.http.resolver", "");
-    config->setString("proxy.http.resolver.endpoint", "http://resolver:8080/hostname");
-
-    // even tho proxy protocol / scheme is https, it should not be picked (prior to this PR, it would be picked)
-    config->setString("proxy.http.resolver.proxy_scheme", "https");
-    config->setString("proxy.http.resolver.proxy_port", "80");
-    config->setString("proxy.http.resolver.proxy_cache_time", "10");
-
-    context->setConfig(config);
-
-    auto http_proxy_configuration = DB::ProxyConfigurationResolverProvider::get(DB::ProxyConfiguration::Protocol::HTTPS, *config)->resolve();
-
-    /*
-     * Asserts env proxy is used and not the remote resolver. If the remote resolver is picked, it is an error because
-     * there is no `http` specification for remote resolver
-     * */
-    ASSERT_EQ(http_proxy_configuration.host, https_env_proxy_server.getHost());
-    ASSERT_EQ(http_proxy_configuration.port, https_env_proxy_server.getPort());
-    ASSERT_EQ(http_proxy_configuration.protocol, DB::ProxyConfiguration::protocolFromString(https_env_proxy_server.getScheme()));
-    ASSERT_EQ(http_proxy_configuration.no_proxy_hosts, "");
-}
-
-// remote resolver is tricky to be tested in unit tests
 
 template <bool DISABLE_TUNNELING_FOR_HTTPS_REQUESTS_OVER_HTTP_PROXY, bool STRING>
 void test_tunneling(DB::ContextMutablePtr context)
 {
-    EnvironmentProxySetter setter(http_env_proxy_server, https_env_proxy_server);
-
     ConfigurationPtr config = Poco::AutoPtr(new Poco::Util::MapConfiguration());
 
     config->setString("proxy", "");
+    config->setString("proxy.https", "");
+    config->setString("proxy.https.uri", http_list_proxy_server.toString());
 
     if constexpr (STRING)
     {
