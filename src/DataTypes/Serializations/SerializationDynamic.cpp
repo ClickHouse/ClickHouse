1c806e6fdd902c675cbe60785df37679d4d8eacc
shuai-xu <xushuai@bigo.sg>
2024-06-07 18:15:37 +0800

    fix code style

--- src/DataTypes/Serializations/SerializationDynamic.cpp
+++ src/DataTypes/Serializations/SerializationDynamic.cpp
@@ -20,6 +20,7 @@ namespace ErrorCodes
 {
     extern const int INCORRECT_DATA;
     extern const int LOGICAL_ERROR;
+    extern const int NOT_IMPLEMENTED;
 }
 
 struct SerializeBinaryBulkStateDynamic : public ISerialization::SerializeBinaryBulkState
