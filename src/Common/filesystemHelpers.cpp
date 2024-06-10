e6f88126964f3018f07d4137e89541f8b45806a3
Duc Canh Le <duccanh.le@ahrefs.com>
2024-06-10 06:53:25 +0000

    fix mistake
    Signed-off-by: Duc Canh Le <duccanh.le@ahrefs.com>

--- src/Common/filesystemHelpers.cpp
+++ src/Common/filesystemHelpers.cpp
@@ -1,5 +1,5 @@
 #include "filesystemHelpers.h"
-#include <bits/types/struct_timeval.h>
+#include <utime.h>
 #include <sys/select.h>
 
 #if defined(OS_LINUX)
@@ -371,11 +371,10 @@ Poco::Timestamp getModificationTimestamp(const std::string & path)
 
 void setModificationTime(const std::string & path, time_t time)
 {
-    struct timeval times[2];
-    times[0].tv_usec = times[1].tv_usec = 0;
-    times[0].tv_sec = ::time(nullptr);
-    times[1].tv_sec = time;
-    if (utimes(path.c_str(), times) != 0)
+    struct utimbuf tb;
+    tb.actime  = time;
+    tb.modtime = time;
+    if (utime(path.c_str(), &tb) != 0)
         DB::ErrnoException::throwFromPath(DB::ErrorCodes::PATH_ACCESS_DENIED, path, "Cannot set modification time to file: {}", path);
 }
 
