fb49cf503e4159549348c76ebf9c3ca686b9f02f
Yakov Olkhovskiy <yakov@clickhouse.com>
2024-06-11 16:47:05 +0000

    some fixes

--- src/IO/WriteBufferFromPocoSocketChunked.h
+++ src/IO/WriteBufferFromPocoSocketChunked.h
@@ -35,7 +35,7 @@ public:
 
             /// If current chunk is empty it means we are finishing a chunk previously sent by next(),
             /// we want to convert current chunk header into end-of-chunk marker and initialize next chunk.
-            /// We don't need to wary about if it's the end of the buffer because next() always sends the whole buffer
+            /// We don't need to worry about if it's the end of the buffer because next() always sends the whole buffer
             /// so it should be a beginning of the buffer.
 
             chassert(reinterpret_cast<Position>(chunk_size_ptr) == working_buffer.begin());
@@ -50,6 +50,13 @@ public:
             return;
         }
 
+        /// Previously finished chunk wasn't sent yet
+        if (last_finish_chunk == chunk_size_ptr)
+        {
+            chunk_started = false;
+            LOG_TEST(log, "{} -> {} Chunk send ended.", ourAddress().toString(), peerAddress().toString());
+        }
+
         /// Fill up current chunk size
         *chunk_size_ptr = toLittleEndian(static_cast<UInt32>(pos - reinterpret_cast<Position>(chunk_size_ptr) - sizeof(*chunk_size_ptr)));
 
@@ -59,7 +66,10 @@ public:
                     static_cast<unsigned int>(*(reinterpret_cast<char *>(chunk_size_ptr) + sizeof(*chunk_size_ptr))),
                     *chunk_size_ptr);
         else
+        {
             chunk_started = false;
+            LOG_TEST(log, "{} -> {} Chunk send continued. Size {}", ourAddress().toString(), peerAddress().toString(), *chunk_size_ptr);
+        }
 
         LOG_TEST(log, "{} -> {} Chunk send ended.", ourAddress().toString(), peerAddress().toString());
 
@@ -79,6 +89,18 @@ public:
         pos += std::min(available(), sizeof(*chunk_size_ptr));
 
         last_finish_chunk = chunk_size_ptr;
+    }
+
+    ~WriteBufferFromPocoSocketChunked() override
+    {
+        try
+        {
+            finalize();
+        }
+        catch (...)
+        {
+            tryLogCurrentException(__PRETTY_FUNCTION__);
+        }
     }
 
 protected:
@@ -171,6 +193,13 @@ protected:
         nextimpl_working_buffer_offset = sizeof(*chunk_size_ptr);
 
         last_finish_chunk = nullptr;
+    }
+
+    void finalizeImpl() override
+    {
+        if (offset() == sizeof(*chunk_size_ptr))
+            pos -= sizeof(*chunk_size_ptr);
+        WriteBufferFromPocoSocket::finalizeImpl();
     }
 
     Poco::Net::SocketAddress peerAddress()
