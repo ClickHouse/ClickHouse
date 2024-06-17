f00750a152babd8bab7141abfac47b626e8a12e2
Max K <maxkaynov@gmail.com>
2024-06-17 08:33:31 +0200

    do not overwrite Sync status if set

--- tests/ci/ci.py
+++ tests/ci/ci.py
@@ -31,6 +31,7 @@ from commit_status_helper import (
     get_commit,
     post_commit_status,
     set_status_comment,
+    get_commit_filtered_statuses,
 )
 from digest_helper import DockerDigester
 from env_helper import (
@@ -909,13 +910,23 @@ def _cancel_pr_wf(s3: S3Helper, pr_number: int, cancel_sync: bool = False) -> No
 def _set_pending_statuses(pr_info: PRInfo) -> None:
     commit = get_commit(GitHub(get_best_robot_token(), per_page=100), pr_info.sha)
     try:
-        print("Set SYNC status to pending")
-        commit.create_status(
-            state=PENDING,
-            target_url="",
-            description=CI.SyncState.PENDING,
-            context=CI.StatusNames.SYNC,
-        )
+        found = False
+        statuses = get_commit_filtered_statuses(commit)
+        for commit_status in statuses:
+            if commit_status.context == CI.StatusNames.SYNC:
+                print(
+                    f"Sync status found [{commit_status.state}], [{commit_status.description}] - won't be overwritten"
+                )
+                found = True
+                break
+        if not found:
+            print("Set Sync status to pending")
+            commit.create_status(
+                state=PENDING,
+                target_url="",
+                description=CI.SyncState.PENDING,
+                context=CI.StatusNames.SYNC,
+            )
     except Exception as ex:
         print(f"ERROR: failed to set GH commit status, ex: {ex}")
 
