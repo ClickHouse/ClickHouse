ed44e4cf6b9f66fed04a4267b72cb375c9eae295
Nikita Taranov <nikita.taranov@clickhouse.com>
2024-06-18 23:17:50 +0100

    fix test

--- tests/queries/0_stateless/03173_parallel_replicas_join_bug.sh
+++ tests/queries/0_stateless/03173_parallel_replicas_join_bug.sh
@@ -1,12 +1,22 @@
-CREATE TABLE ids (id UUID, whatever String) Engine=MergeTree ORDER BY tuple();
-INSERT INTO ids VALUES ('a1451105-722e-4fe7-bfaa-65ad2ae249c2', 'whatever');
+#!/usr/bin/env bash
 
-CREATE TABLE data (id UUID, event_time DateTime, status String) Engine=MergeTree ORDER BY tuple();
-INSERT INTO data VALUES ('a1451105-722e-4fe7-bfaa-65ad2ae249c2', '2000-01-01', 'CREATED');
+CURDIR=$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)
+# shellcheck source=../shell_config.sh
+. "$CURDIR"/../shell_config.sh
 
-CREATE TABLE data2 (id UUID, event_time DateTime, status String) Engine=MergeTree ORDER BY tuple();
-INSERT INTO data2 VALUES ('a1451105-722e-4fe7-bfaa-65ad2ae249c2', '2000-01-02', 'CREATED');
 
+$CLICKHOUSE_CLIENT -nq "
+  CREATE TABLE ids (id UUID, whatever String) Engine=MergeTree ORDER BY tuple();
+  INSERT INTO ids VALUES ('a1451105-722e-4fe7-bfaa-65ad2ae249c2', 'whatever');
+
+  CREATE TABLE data (id UUID, event_time DateTime, status String) Engine=MergeTree ORDER BY tuple();
+  INSERT INTO data VALUES ('a1451105-722e-4fe7-bfaa-65ad2ae249c2', '2000-01-01', 'CREATED');
+
+  CREATE TABLE data2 (id UUID, event_time DateTime, status String) Engine=MergeTree ORDER BY tuple();
+  INSERT INTO data2 VALUES ('a1451105-722e-4fe7-bfaa-65ad2ae249c2', '2000-01-02', 'CREATED');
+"
+
+$CLICKHOUSE_CLIENT -nq "
 SET allow_experimental_analyzer = 1, cluster_for_parallel_replicas = 'parallel_replicas', max_parallel_replicas = 10, allow_experimental_parallel_reading_from_replicas = 2, parallel_replicas_for_non_replicated_merge_tree = 1, max_threads = 1;
 
 SELECT
@@ -15,7 +25,7 @@ SELECT
 FROM ids AS l
 INNER JOIN view(
     SELECT *
-    FROM merge(currentDatabase(), 'data*')
+    FROM merge($CLICKHOUSE_DATABASE, 'data.*')
 ) AS s ON l.id = s.id
 WHERE status IN ['CREATED', 'CREATING']
 ORDER BY event_time DESC;
@@ -64,4 +74,4 @@ results2 as (
     on t1.id = t2.id
 )
 select * from results1 union all select * from results2;
-
+"
