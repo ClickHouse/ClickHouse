df32e75fbec9e5b0c6ec34590a5b5f33a45692c3
Larry Luo <larry.luo@ca.ibm.com>
2024-06-18 14:21:14 -0400

    update test

--- tests/queries/0_stateless/02935_http_content_type_setter_for_json.sh
+++ tests/queries/0_stateless/02935_http_content_type_setter_for_json.sh
@@ -8,6 +8,6 @@ CURDIR=$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)
 for frmt in JSONStrings JSON JSONEachRow JSONColumns JSONCompact JSONObjectEachRow
 do
   echo $frmt
-  url="${CLICKHOUSE_URL}/&output_format_json_content_type_header=custom-type&query=select+sleepEachRow(0.01)from+numbers(10)+FORMAT+${frmt}"
+  url="${CLICKHOUSE_URL}/&output_format_json_content_type_header=custom-type&query=select+1+FORMAT+${frmt}"
   (seq 1 200| xargs -n1 -P0 -Ixxx curl -Ss -v -o /dev/null ${url} 2>&1|grep -Eo " Content-Type:.*$")|strings|sort -u
 done
