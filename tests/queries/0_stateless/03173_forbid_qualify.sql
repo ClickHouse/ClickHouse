72e3fdc8cae727e925d0628c5eb5e1f25f9bf578
Dmitry Novik <mrnovikd@gmail.com>
2024-06-18 09:53:37 +0200

    Use test database

--- tests/queries/0_stateless/03173_forbid_qualify.sql
+++ tests/queries/0_stateless/03173_forbid_qualify.sql
@@ -1,11 +1,11 @@
-drop table if exists default.test_qualify;
-create table default.test_qualify (number Int64) ENGINE = MergeTree ORDER BY (number);
+drop table if exists test_qualify;
+create table test_qualify (number Int64) ENGINE = MergeTree ORDER BY (number);
 
-insert into default.test_qualify SELECT * FROM numbers(100);
+insert into test_qualify SELECT * FROM numbers(100);
 
-select count() from default.test_qualify; -- 100
-select * from default.test_qualify qualify row_number() over (order by number) = 50 SETTINGS allow_experimental_analyzer = 1; -- 49
-select * from default.test_qualify qualify row_number() over (order by number) = 50 SETTINGS allow_experimental_analyzer = 0; -- { serverError NOT_IMPLEMENTED }
+select count() from test_qualify; -- 100
+select * from test_qualify qualify row_number() over (order by number) = 50 SETTINGS allow_experimental_analyzer = 1; -- 49
+select * from test_qualify qualify row_number() over (order by number) = 50 SETTINGS allow_experimental_analyzer = 0; -- { serverError NOT_IMPLEMENTED }
 
-delete from default.test_qualify where number in (select number from default.test_qualify qualify row_number() over (order by number) = 50); -- { serverError UNFINISHED }
-select count() from default.test_qualify; -- 100
+delete from test_qualify where number in (select number from test_qualify qualify row_number() over (order by number) = 50); -- { serverError UNFINISHED }
+select count() from test_qualify; -- 100
