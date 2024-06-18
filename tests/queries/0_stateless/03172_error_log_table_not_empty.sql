4c38f9fb342aa014b7cce3a3d2f1704bbc59f497
Pablo Marcos <pablo.marcos.oltra@clickhouse.com>
2024-06-18 15:29:27 +0000

    Improve error_log test to ensure errors are propagated

--- tests/queries/0_stateless/03172_error_log_table_not_empty.sql
+++ tests/queries/0_stateless/03172_error_log_table_not_empty.sql
@@ -1,5 +1,25 @@
-select sleep(2) format Null; --More than collect_interval_milliseconds
+-- Throw three random errors: 111, 222 and 333
+SELECT throwIf(true, 'test', toInt16(111)) SETTINGS allow_custom_error_code_in_throwif=1; -- { serverError 111 }
+SELECT throwIf(true, 'test', toInt16(222)) SETTINGS allow_custom_error_code_in_throwif=1; -- { serverError 222 }
+SELECT throwIf(true, 'test', toInt16(333)) SETTINGS allow_custom_error_code_in_throwif=1; -- { serverError 333 }
 
-system flush logs;
+-- Wait for more than collect_interval_milliseconds to ensure system.error_log is flushed
+SELECT sleep(2) FORMAT NULL;
+SYSTEM FLUSH LOGS;
 
-select count()>0 from system.error_log
+-- Check that the three random errors are propagated
+SELECT sum(value) > 0 FROM system.error_log WHERE code = 111 AND event_time < now() - INTERVAL 1 MINUTE;
+SELECT sum(value) > 0 FROM system.error_log WHERE code = 222 AND event_time < now() - INTERVAL 1 MINUTE;
+SELECT sum(value) > 0 FROM system.error_log WHERE code = 333 AND event_time < now() - INTERVAL 1 MINUTE;
+
+-- Ensure that if we throw them again, they're still propagated
+SELECT throwIf(true, 'test', toInt16(111)) SETTINGS allow_custom_error_code_in_throwif=1; -- { serverError 111 }
+SELECT throwIf(true, 'test', toInt16(222)) SETTINGS allow_custom_error_code_in_throwif=1; -- { serverError 222 }
+SELECT throwIf(true, 'test', toInt16(333)) SETTINGS allow_custom_error_code_in_throwif=1; -- { serverError 333 }
+
+SELECT sleep(2) FORMAT NULL;
+SYSTEM FLUSH LOGS;
+
+SELECT sum(value) > 1 FROM system.error_log WHERE code = 111 AND event_time < now() - INTERVAL 1 MINUTE;
+SELECT sum(value) > 1 FROM system.error_log WHERE code = 222 AND event_time < now() - INTERVAL 1 MINUTE;
+SELECT sum(value) > 1 FROM system.error_log WHERE code = 333 AND event_time < now() - INTERVAL 1 MINUTE;
