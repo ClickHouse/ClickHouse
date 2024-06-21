ea403eec0a8251c92f35f12a704108f3138dca1c
Michael Kolupaev <michael.kolupaev@clickhouse.com>
2024-06-21 22:45:00 +0000

    Fix

--- src/Interpreters/InterpreterCreateQuery.cpp
+++ src/Interpreters/InterpreterCreateQuery.cpp
@@ -898,7 +898,7 @@ InterpreterCreateQuery::TableProperties InterpreterCreateQuery::getTableProperti
     assert(as_database_saved.empty() && as_table_saved.empty());
     std::swap(create.as_database, as_database_saved);
     std::swap(create.as_table, as_table_saved);
-    if (!as_database_saved.empty())
+    if (!as_table_saved.empty())
         create.is_create_empty = false;
 
     return properties;
