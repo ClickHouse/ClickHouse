c1b6e091d0b9414936e6ba280c2124b47067a5b8
Yarik Briukhovetskyi <114298166+yariks5s@users.noreply.github.com>
2024-06-21 17:09:49 +0200

    make code prettier

--- src/Processors/Transforms/PlanSquashingTransform.cpp
+++ src/Processors/Transforms/PlanSquashingTransform.cpp
@@ -1,7 +1,5 @@
 #include <Processors/Transforms/PlanSquashingTransform.h>
 #include <Processors/IProcessor.h>
-#include "Common/Logger.h"
-#include "Common/logger_useful.h"
 #include <Common/Exception.h>
 
 namespace DB
@@ -20,14 +18,12 @@ PlanSquashingTransform::PlanSquashingTransform(
 
 void PlanSquashingTransform::consume(Chunk chunk)
 {
-    LOG_TRACE(getLogger("consume"), "1");
     if (Chunk current_chunk = squashing.add(std::move(chunk)))
         squashed_chunk.swap(current_chunk);
 }
 
 Chunk PlanSquashingTransform::generate()
 {
-    LOG_TRACE(getLogger("generate"), "1");
     if (!squashed_chunk.hasChunkInfo())
         throw Exception(ErrorCodes::LOGICAL_ERROR, "Can't generate chunk in SimpleSquashingChunksTransform");
 
@@ -38,18 +34,12 @@ Chunk PlanSquashingTransform::generate()
 
 bool PlanSquashingTransform::canGenerate()
 {
-    LOG_TRACE(getLogger("canGenerate"), "1");
     return squashed_chunk.hasChunkInfo();
 }
 
 Chunk PlanSquashingTransform::getRemaining()
 {
-    LOG_TRACE(getLogger("getRemaining"), "1");
     Chunk current_chunk = squashing.flush();
-    // squashed_chunk.swap(current_chunk);
-
-    // Chunk result_chunk;
-    // result_chunk.swap(squashed_chunk);
     return current_chunk;
 }
 }
