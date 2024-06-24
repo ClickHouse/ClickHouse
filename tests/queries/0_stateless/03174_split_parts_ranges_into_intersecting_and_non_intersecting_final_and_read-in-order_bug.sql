59a1e6fed12f2cebb15febce76322b8f9405d56a
Nikolai Kochetov <nk@clickhouse.com>
2024-06-24 13:13:57 +0200

    Update 03174_split_parts_ranges_into_intersecting_and_non_intersecting_final_and_read-in-order_bug.sql

--- tests/queries/0_stateless/03174_split_parts_ranges_into_intersecting_and_non_intersecting_final_and_read-in-order_bug.sql
+++ tests/queries/0_stateless/03174_split_parts_ranges_into_intersecting_and_non_intersecting_final_and_read-in-order_bug.sql
@@ -1,3 +1,5 @@
+-- Tags: no-tsan, no-asan, no-msan, no-fasttest
+-- Test is slow
 create table tab (x DateTime('UTC'), y UInt32, v Int32) engine = ReplacingMergeTree(v) order by x;
 insert into tab select toDateTime('2000-01-01', 'UTC') + number, number, 1 from numbers(1e7);
 optimize table tab final;
