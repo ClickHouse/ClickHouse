556c7deeff11e404630948adf3bbd171170dd3eb
jsc0218 <jsc0218@gmail.com>
2024-06-24 01:19:50 +0000

    add drop option in lightweight delete on table with projections

--- tests/queries/0_stateless/03161_lightweight_delete_projection.sql
+++ tests/queries/0_stateless/03161_lightweight_delete_projection.sql
@@ -5,7 +5,8 @@ CREATE TABLE users (
     uid Int16,
     name String,
     age Int16,
-    projection p1 (select count(), age group by age)
+    projection p1 (select count(), age group by age),
+    projection p2 (select age, name group by age, name)
 ) ENGINE = MergeTree order by uid;
 
 INSERT INTO users VALUES (1231, 'John', 33);
@@ -13,3 +14,11 @@ INSERT INTO users VALUES (6666, 'Ksenia', 48);
 INSERT INTO users VALUES (8888, 'Alice', 50);
 
 DELETE FROM users WHERE 1; -- { serverError NOT_IMPLEMENTED }
+
+DELETE FROM users WHERE uid = 8888 SETTINGS lightweight_mutation_projection_mode = 'throw';  -- { serverError NOT_IMPLEMENTED }
+
+DELETE FROM users WHERE uid = 6666 SETTINGS lightweight_mutation_projection_mode = 'drop';
+
+SELECT * FROM users;
+
+DROP TABLE users;
