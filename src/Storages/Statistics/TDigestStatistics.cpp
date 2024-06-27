9a1df094e91690bd852bc07d8d23e42874c4fda7
Robert Schulze <robert@clickhouse.com>
2024-06-27 16:36:43 +0000

    Cosmetics
    No code was harmed in the process (really just cosmetics).

--- src/Storages/Statistics/TDigestStatistics.cpp
+++ src/Storages/Statistics/TDigestStatistics.cpp
@@ -8,53 +8,53 @@ namespace ErrorCodes
     extern const int ILLEGAL_STATISTICS;
 }
 
-TDigestStatistics::TDigestStatistics(const SingleStatisticsDescription & stat_):
-    IStatistics(stat_)
+TDigestStatistics::TDigestStatistics(const SingleStatisticsDescription & stat_)
+    : IStatistics(stat_)
 {
 }
 
 Float64 TDigestStatistics::estimateLess(Float64 val) const
 {
-    return data.getCountLessThan(val);
+    return t_digest.getCountLessThan(val);
 }
 
 Float64 TDigestStatistics::estimateEqual(Float64 val) const
 {
-    return data.getCountEqual(val);
+    return t_digest.getCountEqual(val);
 }
 
 void TDigestStatistics::serialize(WriteBuffer & buf)
 {
-    data.serialize(buf);
+    t_digest.serialize(buf);
 }
 
 void TDigestStatistics::deserialize(ReadBuffer & buf)
 {
-    data.deserialize(buf);
+    t_digest.deserialize(buf);
 }
 
 void TDigestStatistics::update(const ColumnPtr & column)
 {
-    size_t size = column->size();
+    size_t rows = column->size();
 
-    for (size_t i = 0; i < size; ++i)
+    for (size_t row = 0; row < rows; ++row)
     {
         /// TODO: support more types.
-        Float64 value = column->getFloat64(i);
-        data.add(value, 1);
+        Float64 value = column->getFloat64(row);
+        t_digest.add(value, 1);
     }
-}
-
-StatisticsPtr TDigestCreator(const SingleStatisticsDescription & stat, DataTypePtr)
-{
-    return std::make_shared<TDigestStatistics>(stat);
 }
 
 void TDigestValidator(const SingleStatisticsDescription &, DataTypePtr data_type)
 {
     data_type = removeNullable(data_type);
     if (!data_type->isValueRepresentedByNumber())
-        throw Exception(ErrorCodes::ILLEGAL_STATISTICS, "Statistics of type 'tdigest' does not support type {}", data_type->getName());
+        throw Exception(ErrorCodes::ILLEGAL_STATISTICS, "Statistics of type 'tdigest' do not support type {}", data_type->getName());
+}
+
+StatisticsPtr TDigestCreator(const SingleStatisticsDescription & stat, DataTypePtr)
+{
+    return std::make_shared<TDigestStatistics>(stat);
 }
 
 }
--- src/Storages/Statistics/UniqStatistics.h
+++ src/Storages/Statistics/UniqStatistics.h
@@ -17,7 +17,6 @@ public:
     UInt64 getCardinality();
 
     void serialize(WriteBuffer & buf) override;
-
     void deserialize(ReadBuffer & buf) override;
 
     void update(const ColumnPtr & column) override;
@@ -30,7 +29,7 @@ private:
 
 };
 
-StatisticsPtr UniqCreator(const SingleStatisticsDescription & stat, DataTypePtr data_type);
 void UniqValidator(const SingleStatisticsDescription &, DataTypePtr data_type);
+StatisticsPtr UniqCreator(const SingleStatisticsDescription & stat, DataTypePtr data_type);
 
 }
