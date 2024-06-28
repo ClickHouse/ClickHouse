c45d5f138a3ce56cfeeb4c264b73b709b7f461e5
liuneng <1398775315@qq.com>
2024-06-28 18:32:04 +0800

    draft

--- src/Processors/Formats/Impl/Parquet/SelectiveColumnReader.cpp
+++ src/Processors/Formats/Impl/Parquet/SelectiveColumnReader.cpp
@@ -1,7 +1,17 @@
 #include "SelectiveColumnReader.h"
 
+#include <Columns/ColumnsNumber.h>
+#include <bits/stat.h>
+#include <Common/assert_cast.h>
+
 namespace DB
 {
+
+namespace ErrorCodes
+{
+extern const int UNSUPPORTED_METHOD;
+}
+
 Chunk RowGroupChunkReader::readChunk(int rows)
 {
     // compute row_set;
@@ -10,23 +20,94 @@ Chunk RowGroupChunkReader::readChunk(int rows)
 
     // read column
     Columns columns;
-    std::ranges::for_each(column_readers, [&](auto & reader) {columns.push_back(reader->read(row_set, rows));});
+    std::ranges::for_each(column_readers, [&](auto & reader) { columns.push_back(reader->read(row_set, rows)); });
 
     return Chunk(columns, columns[0]->size());
 }
-
-void Int64ColumnDirectReader::computeRowSet(RowSet row_set, size_t& rows_to_read)
+void OptionalColumnReader::computeRowSet(RowSet row_set, size_t & rows_to_read)
 {
-    if (!state.page || state.offset >= state.page->size())
-        state.page = page_reader->NextPage();
-    size_t remain_rows = (state.page->size() - state.offset) / sizeof(Int64);
-    rows_to_read = std::min(remain_rows, rows_to_read);
+}
+
+void SelectiveColumnReader::readPageIfNeeded()
+{
+    if (!state.page || !state.remain_rows)
+        readPage();
+    // may read dict page first;
+    if (!state.remain_rows)
+        readPage();
+}
+
+void SelectiveColumnReader::readPage()
+{
+    auto page = page_reader->NextPage();
+    state.page = page;
+    if (page->type() == parquet::PageType::DATA_PAGE)
+    {
+        readDataPageV1(assert_cast<const parquet::DataPageV1 &>(*page));
+    }
+    else if (page->type() == parquet::PageType::DICTIONARY_PAGE)
+    {
+        readDictPage(assert_cast<const parquet::DictionaryPage &>(*page));
+    }
+    else
+    {
+        throw Exception(ErrorCodes::UNSUPPORTED_METHOD, "Unsupported page type {}", magic_enum::enum_name(page->type()));
+    }
+}
+void SelectiveColumnReader::readDataPageV1(const parquet::DataPageV1 & page)
+{
+    parquet::LevelDecoder decoder;
+    state.buffer = page.data();
+    auto max_size = page.size();
+    state.remain_rows = page.num_values();
+    auto max_rep_level = scan_spec.column_desc->max_repetition_level();
+    auto max_def_level = scan_spec.column_desc->max_definition_level();
+    if (scan_spec.column_desc->max_repetition_level() > 0)
+    {
+        auto rep_bytes = decoder.SetData(page.repetition_level_encoding(), max_rep_level, state.remain_rows, state.buffer, max_size);
+        max_size -= rep_bytes;
+        state.buffer += rep_bytes;
+        state.rep_levels.resize_fill(state.remain_rows);
+        decoder.Decode(state.remain_rows, state.rep_levels.data());
+    }
+    if (scan_spec.column_desc->max_definition_level() > 0)
+    {
+        auto def_bytes = decoder.SetData(page.definition_level_encoding(), max_def_level, state.remain_rows, state.buffer, max_size);
+        max_size -= def_bytes;
+        state.buffer += def_bytes;
+        state.def_levels.resize_fill(state.remain_rows);
+        decoder.Decode(state.remain_rows, state.def_levels.data());
+    }
+}
+
+void Int64ColumnDirectReader::computeRowSet(RowSet row_set, size_t & rows_to_read)
+{
+    readPageIfNeeded();
+    rows_to_read = std::min(state.remain_rows, rows_to_read);
     size_t row_set_offset = row_set.offset();
-    const Int64 * start = reinterpret_cast<const Int64 *>(state.page->data() + state.offset);
+    const Int64 * start = reinterpret_cast<const Int64 *>(state.buffer);
     for (size_t i = 0; i < rows_to_read; i++)
     {
         row_set.set(i, scan_spec.filter->testInt64(start[i]));
     }
-
 }
+
+void Int64ColumnDirectReader::read(MutableColumnPtr & column, RowSet row_set, size_t & rows_to_read)
+{
+    ColumnInt64 * data = assert_cast<ColumnInt64 *>(column.get());
+    rows_to_read = std::min(state.remain_rows, rows_to_read);
+    size_t rows_readed = 0;
+    const Int64 * start = reinterpret_cast<const Int64 *>(state.buffer);
+    while (rows_readed < rows_to_read)
+    {
+        if (row_set.get(row_set.offset() + rows_readed))
+        {
+            data->getData().push_back(start[rows_readed]);
+        }
+        rows_readed ++;
+    }
+    state.buffer += rows_to_read * sizeof(Int64);
+    state.remain_rows -= rows_to_read;
+}
+
 }
--- src/Processors/Formats/Impl/Parquet/SelectiveColumnReader.h
+++ src/Processors/Formats/Impl/Parquet/SelectiveColumnReader.h
@@ -6,6 +6,7 @@
 #include <Processors/Chunk.h>
 #include <parquet/column_page.h>
 #include <parquet/column_reader.h>
+#include <Common/PODArray.h>
 
 namespace parquet
 {
@@ -17,8 +18,10 @@ namespace DB
 class RowSet
 {
 public:
-    inline void set(size_t i, bool value) { }
+    void set(size_t i, bool value) { }
+    bool get(size_t i) { return true; }
     size_t offset() { return 0; }
+    size_t addOffset(size_t i);
     size_t totalRows() { return 0; }
 };
 
@@ -30,7 +33,7 @@ struct ScanSpec
 {
     String column_name;
     DataTypePtr expected_type;
-    const parquet::ColumnDescriptor * column_desc;
+    const parquet::ColumnDescriptor * column_desc = nullptr;
     ColumnFilterPtr filter;
 };
 
@@ -38,8 +41,11 @@ class ScanState
 {
 public:
     std::shared_ptr<parquet::Page> page;
-    Int32 offset = 0;
+    PaddedPODArray<Int16> def_levels;
+    PaddedPODArray<Int16> rep_levels;
+    const uint8_t * buffer = nullptr;
     size_t rows_read = 0;
+    size_t remain_rows = 0;
 };
 
 class SelectiveColumnReader
@@ -47,19 +53,24 @@ class SelectiveColumnReader
 public:
     virtual ~SelectiveColumnReader();
     virtual void computeRowSet(RowSet row_set, size_t & rows_to_read) = 0;
-    virtual ColumnPtr read(RowSet row_set, size_t & rows_to_read) = 0;
+    virtual void read(MutableColumnPtr & column, RowSet row_set, size_t & rows_to_read) = 0;
     virtual void getValues() { }
+    const PaddedPODArray<Int16> & getDefenitionLevels()
+    {
+        readPageIfNeeded();
+        return state.def_levels;
+    }
 
 protected:
+    void readPageIfNeeded();
     void readPage();
-    void readPageV1(const parquet::DataPageV1 & page);
+    void readDataPageV1(const parquet::DataPageV1 & page);
+    void readDictPage(const parquet::DictionaryPage & page);
 
     std::unique_ptr<parquet::PageReader> page_reader;
     ScanState state;
     ScanSpec scan_spec;
-
 };
-
 
 
 class Int64ColumnDirectReader : public SelectiveColumnReader
@@ -67,10 +78,7 @@ class Int64ColumnDirectReader : public SelectiveColumnReader
 public:
     ~Int64ColumnDirectReader() override = default;
     void computeRowSet(RowSet row_set, size_t & rows_to_read) override;
-    ColumnPtr read(RowSet row_set, size_t & rows_to_read) override;
-
-private:
-
+    void read(MutableColumnPtr & column, RowSet row_set, size_t & rows_to_read) override;
 };
 
 class RowGroupChunkReader
@@ -82,5 +90,15 @@ private:
     std::vector<String> filter_columns;
     std::unordered_map<String, SelectiveColumnReaderPtr> reader_columns_mapping;
     std::vector<SelectiveColumnReaderPtr> column_readers;
+};
+
+class OptionalColumnReader : public SelectiveColumnReader
+{
+public:
+    void computeRowSet(RowSet row_set, size_t & rows_to_read) override;
+    void read(MutableColumnPtr & column, RowSet row_set, size_t & rows_to_read) override;
+
+private:
+    SelectiveColumnReaderPtr child;
 };
 }
