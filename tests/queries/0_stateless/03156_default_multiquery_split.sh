830ffd962f0cc4778cf4d0e41a9ee083a5336d9c
wxybear <wangxingyu0927@outlook.com>
2024-06-30 20:07:28 +0800

    feat: modify the tests to fit the new multiquery implements

--- tests/queries/0_stateless/03156_default_multiquery_split.sh
+++ tests/queries/0_stateless/03156_default_multiquery_split.sh
@@ -18,6 +18,7 @@ SQL_FILE_NAME=$"03156_default_multiquery_split_$$.sql"
 # 1. insert 101, 102 into test1
 # 2. insert 1, 2; into test2, ';' will be treated as a part of a value
 # 3. insert 3, 4; '6' will be treated as the next query because of the empty line, we use empty line to determine the end of insert query(format IS NOT VALUES)
+#     '6' will cause Syntax error
 cat << EOF > "$SQL_FILE_NAME"
 DROP TABLE IF EXISTS TEST1; DROP TABLE IF EXISTS TEST2;
 CREATE TABLE TEST1 (value Float64) ENGINE=MergeTree ORDER BY tuple();
@@ -36,7 +37,7 @@ INSERT INTO TEST2 FORMAT CSV
 6
 EOF
 
-$CLICKHOUSE_CLIENT -m < "$SQL_FILE_NAME"
+$CLICKHOUSE_CLIENT -m < "$SQL_FILE_NAME" 2>&1 | grep -o 'Syntax error'
 
 # insert 7, 8, 9 into test2, because we use semicolon to determine the end of insert query(format is VALUES)
 # then select all data from test1 and test2
