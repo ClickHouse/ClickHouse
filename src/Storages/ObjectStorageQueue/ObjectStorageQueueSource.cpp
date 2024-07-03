2371077ba6fba67428272afefda0e5b367e79c6f
robot-clickhouse <robot-clickhouse@users.noreply.github.com>
2024-07-03 10:06:13 +0000

    Backport #65928 to 24.6: Follow up to #65046

--- src/Storages/ObjectStorageQueue/ObjectStorageQueueSource.cpp
+++ src/Storages/ObjectStorageQueue/ObjectStorageQueueSource.cpp
@@ -471,8 +471,6 @@ Chunk ObjectStorageQueueSource::generateImpl()
                     LOG_ERROR(log, "Failed to set file {} as failed: {}",
                              object_info->relative_path, getCurrentExceptionMessage(true));
                 }
-
-                appendLogElement(reader.getObjectInfo()->getPath(), *file_status, processed_rows_from_file, false);
             }
 
             LOG_TEST(log, "Query is cancelled");
@@ -503,8 +501,6 @@ Chunk ObjectStorageQueueSource::generateImpl()
                     LOG_ERROR(log, "Failed to set file {} as failed: {}",
                               object_info->relative_path, getCurrentExceptionMessage(true));
                 }
-
-                appendLogElement(path, *file_status, processed_rows_from_file, false);
 
                 /// Leave the file half processed. Table is being dropped, so we do not care.
                 break;
@@ -550,7 +546,6 @@ Chunk ObjectStorageQueueSource::generateImpl()
 
             failed_during_read_files.push_back(file_metadata);
             file_status->onFailed(getCurrentExceptionMessage(true));
-            appendLogElement(path, *file_status, processed_rows_from_file, false);
 
             if (processed_rows_from_file == 0)
             {
@@ -568,8 +563,6 @@ Chunk ObjectStorageQueueSource::generateImpl()
 
             throw;
         }
-
-        appendLogElement(path, *file_status, processed_rows_from_file, true);
 
         file_status->setProcessingEndTime();
         file_status.reset();
@@ -665,10 +658,14 @@ void ObjectStorageQueueSource::commit(bool success, const std::string & exceptio
             applyActionAfterProcessing(file_metadata->getPath());
         }
         else
+        {
             file_metadata->setFailed(
                 exception_message,
                 /* reduce_retry_count */false,
                 /* overwrite_status */true);
+
+        }
+        appendLogElement(file_metadata->getPath(), *file_metadata->getFileStatus(), processed_rows_from_file, /* processed */success);
     }
 
     for (const auto & file_metadata : failed_during_read_files)
@@ -679,6 +676,8 @@ void ObjectStorageQueueSource::commit(bool success, const std::string & exceptio
             file_metadata->getFileStatus()->getException(),
             /* reduce_retry_count */true,
             /* overwrite_status */false);
+
+        appendLogElement(file_metadata->getPath(), *file_metadata->getFileStatus(), processed_rows_from_file, /* processed */false);
     }
 }
 
