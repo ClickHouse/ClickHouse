be9a17f2cca06814c68b712a6df2c18a44362220
gun9nir <gun9nir.guo@gmail.com>
2024-07-02 21:42:59 -0700

    feat: implicitly append wildcard if querying directory in file engine

---
+++ tests/queries/0_stateless/03198_table_function_directory_path.sql
@@ -0,0 +1,11 @@
+INSERT INTO FUNCTION file('data_03198_table_function_directory_path/1.csv', 'csv') SELECT '1.csv';
+INSERT INTO FUNCTION file('data_03198_table_function_directory_path/2.csv', 'csv') SELECT '2.csv';
+INSERT INTO FUNCTION file('data_03198_table_function_directory_path/dir/3.csv', 'csv') SELECT '3.csv';
+INSERT INTO FUNCTION file('data_03198_table_function_directory_path/dir1/dir/4.csv', 'csv') SELECT '4.csv';
+INSERT INTO FUNCTION file('data_03198_table_function_directory_path/dir2/dir/5.csv', 'csv') SELECT '5.csv';
+
+SELECT COUNT(*) FROM file('data_03198_table_function_directory_path');
+SELECT COUNT(*) FROM file('data_03198_table_function_directory_path/');
+SELECT COUNT(*) FROM file('data_03198_table_function_directory_path/dir');
+SELECT COUNT(*) FROM file('data_03198_table_function_directory_path/*/dir', 'csv'); -- { serverError 74, 636 }
+SELECT COUNT(*) FROM file('data_03198_table_function_directory_pat'); -- { serverError 400 }
