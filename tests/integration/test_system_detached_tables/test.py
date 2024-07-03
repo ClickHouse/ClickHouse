1bb1e0189207c458550ab677983e3efeebad23b1
Konstantin Morozov <just.morozov.k@gmail.com>
2024-07-03 12:12:09 +0000

    fix style

--- tests/integration/test_system_detached_tables/test.py
+++ tests/integration/test_system_detached_tables/test.py
@@ -2,7 +2,8 @@ import pytest
 from helpers.cluster import ClickHouseCluster
 
 cluster = ClickHouseCluster(__file__)
-node = cluster.add_instance("replica1",
+node = cluster.add_instance(
+    "replica1",
     with_zookeeper=True,
     main_configs=["configs/remote_servers.xml"],
     macros={"replica": "replica1"},
@@ -17,25 +18,42 @@ def start_cluster():
     finally:
         cluster.shutdown()
 
+
 @pytest.mark.parametrize(
     "db_name,db_engine,table_engine,table_engine_params",
     [
         pytest.param(
-            "test_db_atomic", "Atomic", "MergeTree", "ORDER BY n", id="Atomic db with MergeTree table"
+            "test_db_atomic",
+            "Atomic",
+            "MergeTree",
+            "ORDER BY n",
+            id="Atomic db with MergeTree table",
         ),
         pytest.param(
-            "test_db_lazy", "Lazy(60)", "Log", "", id="Lazy db with Log table"
+            "test_db_lazy",
+            "Lazy(60)",
+            "Log",
+            "",
+            id="Lazy db with Log table",
         ),
         pytest.param(
-            "test_db_repl", "Replicated('/clickhouse/tables/test_table', 'shard1', 'replica1')", "ReplicatedMergeTree", "ORDER BY n", id="Replicated db with ReplicatedMergeTree table"
+            "test_db_repl",
+            "Replicated('/clickhouse/tables/test_table','shard1', 'replica1')", 
+            "ReplicatedMergeTree", 
+            "ORDER BY n",
+            id="Replicated db with ReplicatedMergeTree table",
         ),
     ],
 )
 def test_system_detached_tables(start_cluster, db_name, db_engine, table_engine, table_engine_params):
     node.query(f"CREATE DATABASE IF NOT EXISTS {db_name} ENGINE={db_engine};")
 
-    node.query(f"CREATE TABLE {db_name}.test_table (n Int64) ENGINE={table_engine} {table_engine_params};")
-    node.query(f"CREATE TABLE {db_name}.test_table_perm (n Int64) ENGINE={table_engine} {table_engine_params};")
+    node.query(
+        f"CREATE TABLE {db_name}.test_table (n Int64) ENGINE={table_engine} {table_engine_params};"
+    )
+    node.query(
+        f"CREATE TABLE {db_name}.test_table_perm (n Int64) ENGINE={table_engine} {table_engine_params};"
+    )
 
     test_table_uuid = node.query(
         "SELECT uuid FROM system.tables WHERE table='test_table'"
@@ -51,14 +69,18 @@ def test_system_detached_tables(start_cluster, db_name, db_engine, table_engine,
         "SELECT metadata_path FROM system.tables WHERE table='test_table_perm'"
     ).rstrip("\n")
 
-    assert "" == node.query(f"SELECT * FROM system.detached_tables WHERE database='{db_name}'")
+    assert "" == node.query(
+        f"SELECT * FROM system.detached_tables WHERE database='{db_name}'"
+    )
 
-    node.query(f"SET database_replicated_always_detach_permanently=1; DETACH TABLE {db_name}.test_table")
+    node.query(
+        f"SET database_replicated_always_detach_permanently=1; DETACH TABLE {db_name}.test_table"
+    )
     node.query(f"DETACH TABLE {db_name}.test_table_perm PERMANENTLY")
 
     querry = f"SELECT database, table, is_permanently, uuid, metadata_path FROM system.detached_tables WHERE database='{db_name}' FORMAT Values"
     result = node.query(querry)
-    
+
     if db_engine.startswith("Repl"):
         expected_before_restart = f"('{db_name}','test_table',1,'{test_table_uuid}','{test_table_metadata_path}'),('{db_name}','test_table_perm',1,'{test_table_perm_uuid}','{test_table_perm_metadata_path}')"
     else:
@@ -68,7 +90,7 @@ def test_system_detached_tables(start_cluster, db_name, db_engine, table_engine,
 
     if db_engine.startswith("Lazy"):
         return
-    
+
     node.restart_clickhouse()
 
     if db_engine.startswith("Repl"):
