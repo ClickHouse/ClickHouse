036d0c3fad88e7092af2c2e749ce7132db811d26
alesapin <alesapin@gmail.com>
2024-07-04 14:08:08 +0200

    Revert "Try to disable sccache"
    This reverts commit 75828c6e817e0c2a2c68040a63a46a083fc56e7a.

--- tests/ci/build_check.py
+++ tests/ci/build_check.py
@@ -62,7 +62,7 @@ def get_packager_cmd(
     if build_config.tidy:
         cmd += " --clang-tidy"
 
-    cmd += " --cache=ccache"
+    cmd += " --cache=sccache"
     cmd += " --s3-rw-access"
     cmd += f" --s3-bucket={S3_BUILDS_BUCKET}"
 
