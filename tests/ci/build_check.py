75828c6e817e0c2a2c68040a63a46a083fc56e7a
alesapin <alesapin@gmail.com>
2024-07-04 12:16:00 +0200

    Try to disable sccache

--- tests/ci/build_check.py
+++ tests/ci/build_check.py
@@ -62,7 +62,7 @@ def get_packager_cmd(
     if build_config.tidy:
         cmd += " --clang-tidy"
 
-    cmd += " --cache=sccache"
+    cmd += " --cache=ccache"
     cmd += " --s3-rw-access"
     cmd += f" --s3-bucket={S3_BUILDS_BUCKET}"
 
