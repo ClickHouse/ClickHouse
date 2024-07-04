5c0d88221fb6794c94ed586ef781d3c26842276f
kevinyhzou <kevinyunhe8@gmail.com>
2024-07-04 09:43:22 +0800

    check style

--- tests/queries/0_stateless/03198_orc_read_time_zone.sh
+++ tests/queries/0_stateless/03198_orc_read_time_zone.sh
@@ -6,6 +6,6 @@ CURDIR=$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)
 
 $CLICKHOUSE_CLIENT -q "drop table if exists test"
 $CLICKHOUSE_CLIENT -q "create table test(id UInt64, t DateTime64) Engine=MergeTree order by id"
-$CLICKHOUSE_CLIENT -q "insert into test from infile '"$CURDIR"/data_orc/read_time_zone.snappy.orc' SETTINGS input_format_orc_read_use_writer_time_zone=true FORMAT ORC"
+$CLICKHOUSE_CLIENT -q "insert into test from infile '$CURDIR/data_orc/read_time_zone.snappy.orc' SETTINGS input_format_orc_read_use_writer_time_zone=true FORMAT ORC"
 $CLICKHOUSE_CLIENT -q "select * from test"
 $CLICKHOUSE_CLIENT -q "drop table test"
