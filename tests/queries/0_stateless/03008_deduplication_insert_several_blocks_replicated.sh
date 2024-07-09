5daa28d5c5726478cbd635ce0ca6b8ce77f5fdce
Sema Checherinda <Sema.Checherinda@clickhouse.com>
2024-07-09 13:27:58 +0200

    mark heavy tests with no-asan tag, they are slow with asan and asuze

--- tests/queries/0_stateless/03008_deduplication_insert_several_blocks_replicated.sh
+++ tests/queries/0_stateless/03008_deduplication_insert_several_blocks_replicated.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
-# Tags: long, no-fasttest, no-parallel
+# Tags: long, no-fasttest, no-parallel, no-asan
 
 CURDIR=$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)
 # shellcheck source=../shell_config.sh
