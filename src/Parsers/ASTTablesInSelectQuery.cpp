6e762d404456debca4ee2d5ccce94deb32c3fbad
vdimir <vdimir@clickhouse.com>
2024-07-10 08:57:33 +0000

    Fix aliased JOIN ON expression formatting

--- src/Parsers/ASTTablesInSelectQuery.cpp
+++ src/Parsers/ASTTablesInSelectQuery.cpp
@@ -235,7 +235,12 @@ void ASTTableJoin::formatImplAfterTable(const FormatSettings & settings, FormatS
     else if (on_expression)
     {
         settings.ostr << (settings.hilite ? hilite_keyword : "") << " ON " << (settings.hilite ? hilite_none : "");
+        bool on_has_alias = !on_expression->tryGetAlias().empty();
+        if (on_has_alias)
+            settings.ostr << "(";
         on_expression->formatImpl(settings, state, frame);
+        if (on_has_alias)
+            settings.ostr << ")";
     }
 }
 
