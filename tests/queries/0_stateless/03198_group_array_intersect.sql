67b7cd40d42f306fe3b8ed997bded7678c9c4e78
Yarik Briukhovetskyi <114298166+yariks5s@users.noreply.github.com>
2024-07-11 21:28:56 +0200

    Update 03198_group_array_intersect.sql

--- tests/queries/0_stateless/03198_group_array_intersect.sql
+++ tests/queries/0_stateless/03198_group_array_intersect.sql
@@ -1,4 +1,5 @@
 DROP TABLE IF EXISTS test_numbers__fuzz_29;
+SET max_threads=1, max_insert_threads=1;
 CREATE TABLE test_numbers__fuzz_29 (`a` Array(Nullable(FixedString(19)))) ENGINE = MergeTree ORDER BY a SETTINGS allow_nullable_key=1;
 
 INSERT INTO test_numbers__fuzz_29 VALUES ([1,2,3,4,5,6]);
