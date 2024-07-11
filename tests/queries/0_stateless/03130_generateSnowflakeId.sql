404840bf2c797d8a500d50ad1f2411b330622273
Zawa-II <zawalee33@gmail.com>
2024-07-10 21:29:09 -0700

    [ClickHouse-66010] Add-machine_id-to-generateSnowflakeID
    Extend existing tests and retain expr argument in generateSnowflakeID
    
    Commit expected output file
    
    Add argument validation in generateSnowflakeID function
    
    Style Check
    
    Style Check by changing error type
    
    Style Check by removing error check
    
    Add new test cases for  function including  parameter
    
    Add new test cases for generateSnowflakeID function with machine_id parameter and update reference file
    
    Add new test cases for generateSnowflakeID function with machine_id parameter and update reference file to match expected output
    
    Fix generateSnowflakeID function to correctly handle optional machine_id parameter, update documentation and examples, and clean up tests.
    
    Fix typo in generateSnowflakeID function and correctly handle optional machine_id parameter.

--- tests/queries/0_stateless/03130_generateSnowflakeId.sql
+++ tests/queries/0_stateless/03130_generateSnowflakeId.sql
@@ -1,12 +1,15 @@
 -- Test SQL function 'generateSnowflakeID'
 
 SELECT bitAnd(bitShiftRight(toUInt64(generateSnowflakeID()), 63), 1) = 0; -- check first bit is zero
-
-SELECT generateSnowflakeID(1) = generateSnowflakeID(2); -- disabled common subexpression elimination --> lhs != rhs
 SELECT generateSnowflakeID() = generateSnowflakeID(1); -- same as ^^
 SELECT generateSnowflakeID(1) = generateSnowflakeID(1); -- enabled common subexpression elimination
 
-SELECT generateSnowflakeID(1, 2); -- { serverError NUMBER_OF_ARGUMENTS_DOESNT_MATCH }
+SELECT generateSnowflakeID(1) = generateSnowflakeID(2); -- disabled common subexpression elimination --> lhs != rhs
+SELECT generateSnowflakeID(1) != generateSnowflakeID(); -- Check different invocations yield different results
+
+SELECT generateSnowflakeID('expr', 1) = generateSnowflakeID('expr', 1); -- enabled common subexpression elimination
+SELECT generateSnowflakeID('expr', 1) != generateSnowflakeID('expr', 2); -- different machine IDs should produce different results
+SELECT generateSnowflakeID('expr', 1) != generateSnowflakeID('different_expr', 1); -- different expressions should bypass common subexpression elimination
 
 SELECT count(*)
 FROM
