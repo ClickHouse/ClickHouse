35eabcc9d1cf3a5339c20ea34d006edfc858ae68
János Benjamin Antal <benjamin.antal@clickhouse.com>
2024-07-13 16:01:15 +0000

    Split test cases

--- tests/queries/0_stateless/03203_system_numbers_limit_and_offset_complex.sql
+++ tests/queries/0_stateless/03203_system_numbers_limit_and_offset_complex.sql
@@ -1,15 +1,3 @@
-SELECT 'case 1';
-SELECT number FROM numbers_mt(10000)
-WHERE (number % 10) = 0
-ORDER BY number ASC
-LIMIT 990, 3;
-
-SELECT 'case 2';
-SELECT number FROM numbers_mt(10000)
-WHERE (number % 10) = 0
-ORDER BY number ASC
-LIMIT 999, 20 SETTINGS max_block_size = 31;
-
 --- The following query was buggy before, so let's use it as a test case
 WITH
     toUInt64(-1) AS umax,
