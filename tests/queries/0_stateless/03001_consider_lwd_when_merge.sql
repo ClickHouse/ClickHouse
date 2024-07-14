b98bca8ded67d0fa0e42a5edab97e1a2cb95f3d5
Alexey Milovidov <milovidov@clickhouse.com>
2024-07-14 21:34:32 +0200

    Something in the test `03001_consider_lwd_when_merge`

--- tests/queries/0_stateless/03001_consider_lwd_when_merge.sql
+++ tests/queries/0_stateless/03001_consider_lwd_when_merge.sql
@@ -7,12 +7,12 @@ SETTINGS max_bytes_to_merge_at_max_space_in_pool = 80000, exclude_deleted_rows_f
 INSERT INTO lwd_merge SELECT number FROM numbers(10000);
 INSERT INTO lwd_merge SELECT number FROM numbers(10000, 10000);
 
-OPTIMIZE TABLE lwd_merge;
+OPTIMIZE TABLE lwd_merge FINAL;
 SELECT count() FROM system.parts WHERE database = currentDatabase() AND table = 'lwd_merge' AND active = 1;
 
 DELETE FROM lwd_merge WHERE id % 10 > 0;
 
-OPTIMIZE TABLE lwd_merge;
+OPTIMIZE TABLE lwd_merge FINAL;
 SELECT count() FROM system.parts WHERE database = currentDatabase() AND table = 'lwd_merge' AND active = 1;
 
 ALTER TABLE lwd_merge MODIFY SETTING exclude_deleted_rows_for_part_size_in_merge = 1;
@@ -20,7 +20,7 @@ ALTER TABLE lwd_merge MODIFY SETTING exclude_deleted_rows_for_part_size_in_merge
 -- delete again because deleted rows will be counted in mutation
 DELETE FROM lwd_merge WHERE id % 100 == 0;
 
-OPTIMIZE TABLE lwd_merge;
+OPTIMIZE TABLE lwd_merge FINAL;
 SELECT count() FROM system.parts WHERE database = currentDatabase() AND table = 'lwd_merge' AND active = 1;
 
 DROP TABLE IF EXISTS lwd_merge;
