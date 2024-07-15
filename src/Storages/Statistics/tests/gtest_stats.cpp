76e1dea7ccdabc27129fdf911845f2622bbbc371
JackyWoo <wuchienchao@qq.com>
2024-07-15 18:17:09 +0800

    Fix unit test

--- src/Storages/Statistics/tests/gtest_stats.cpp
+++ src/Storages/Statistics/tests/gtest_stats.cpp
@@ -59,7 +59,7 @@ void testConvertFieldToDataType(const DataTypePtr & data_type, const Fields & fi
         }
         catch(...)
         {
-            ASSERT_FALSE(convert_failed);
+            ASSERT_TRUE(convert_failed);
         }
         if (convert_failed)
             ASSERT_TRUE(converted_value.isNull());
