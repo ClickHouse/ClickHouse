9c1532e02f5f1dfeb2f3833afe585e26d08ccbf0
lgbo-ustc <lgbo.ustc@gmail.com>
2024-07-15 09:46:51 +0800

    add tests

--- tests/queries/0_stateless/00202_cross_join.sql
+++ tests/queries/0_stateless/00202_cross_join.sql
@@ -5,5 +5,3 @@ SELECT x, y FROM (SELECT number AS x FROM system.numbers LIMIT 3) js1 CROSS JOIN
 
 SET allow_experimental_analyzer = 1;
 SELECT x, y FROM (SELECT number AS x FROM system.numbers LIMIT 3) js1 CROSS JOIN (SELECT number AS y FROM system.numbers LIMIT 5) js2;
-
-SELECT * FROM ( SELECT 1 AS a, toLowCardinality(1), 1) AS t1 CROSS  JOIN (SELECT toLowCardinality(1 AS a), 1 AS b) AS t2;
