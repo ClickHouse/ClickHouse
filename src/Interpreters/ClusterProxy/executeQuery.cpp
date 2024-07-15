02a2f509163e5df370440b4eed792dcbd995942c
Igor Nikonov <igor@clickhouse.com>
2024-07-15 18:37:19 +0000

    Fix clang warning

--- src/Interpreters/ClusterProxy/executeQuery.cpp
+++ src/Interpreters/ClusterProxy/executeQuery.cpp
@@ -519,7 +519,7 @@ void executeQueryWithParallelReplicas(
                 "`cluster_for_parallel_replicas` setting refers to cluster with several shards. Expected a cluster with one shard");
     }
 
-    const auto replica_count = std::min(settings.max_parallel_replicas.value, new_cluster->getShardsInfo().begin()->getAllNodeCount());
+    const auto replica_count = std::min<size_t>(settings.max_parallel_replicas.value, new_cluster->getShardsInfo().begin()->getAllNodeCount());
 
     auto coordinator = std::make_shared<ParallelReplicasReadingCoordinator>(replica_count, settings.parallel_replicas_mark_segment_size);
 
