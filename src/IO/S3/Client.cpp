0de54b9d296927b7a8bc84bf1f8bfe5a5a01f080
Konstantin Morozov <just.morozov.k@gmail.com>
2024-07-17 13:01:04 +0000

    fix comma

--- src/IO/S3/Client.cpp
+++ src/IO/S3/Client.cpp
@@ -828,11 +828,11 @@ void Client::updateURIForBucket(const std::string & bucket, S3::URI new_uri) con
 ClientCache::ClientCache(const ClientCache & other)
 {
     {
-        std::lock_guard lock(region_cache_mutex);
+        std::lock_guard lock(other.region_cache_mutex);
         region_for_bucket_cache = other.region_for_bucket_cache;
     }
     {
-        std::lock_guard lock(uri_cache_mutex);
+        std::lock_guard lock(other.uri_cache_mutex);
         uri_for_bucket_cache = other.uri_for_bucket_cache;
     }
 }
