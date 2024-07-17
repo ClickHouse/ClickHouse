80e1377e5d2223176274c319938187f0da799280
Nikolai Kochetov <nk@clickhouse.com>
2024-07-17 17:30:28 +0000

    Fixing build.

--- src/Storages/System/StorageSystemTables.cpp
+++ src/Storages/System/StorageSystemTables.cpp
@@ -125,7 +125,7 @@ ColumnPtr getFilteredTables(
         block.insert(ColumnWithTypeAndName(std::move(engine_column), std::make_shared<DataTypeString>(), "engine"));
 
     if (dag)
-        VirtualColumnUtils::filterBlockWithDAG(dag, block, context);
+        VirtualColumnUtils::filterBlockWithDAG(std::move(*dag), block, context);
 
     return block.getByPosition(0).column;
 }
