478a19cde4ef6ecf2455f312cd1beb87098ede9e
Arthur Passos <arthur.ti@outlook.com>
2024-07-19 12:18:07 -0300

    extern bad arguments

--- src/Storages/MergeTree/KeyCondition.cpp
+++ src/Storages/MergeTree/KeyCondition.cpp
@@ -49,6 +49,7 @@ namespace ErrorCodes
 {
     extern const int LOGICAL_ERROR;
     extern const int BAD_TYPE_OF_FIELD;
+    extern const int BAD_ARGUMENTS;
 }
 
 /// Returns the prefix of like_pattern before the first wildcard, e.g. 'Hello\_World% ...' --> 'Hello\_World'
