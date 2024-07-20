79f41c23450e7b67d99ff84a875369a4cc557243
Panagiotis Liakos <liakospanagiotis@gmail.com>
2024-07-20 19:46:13 +0300

    renamed input file variable in test

--- tests/queries/0_stateless/02584_compressor_codecs.sh
+++ tests/queries/0_stateless/02584_compressor_codecs.sh
@@ -4,7 +4,7 @@ CURDIR=$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)
 # shellcheck source=../shell_config.sh
 . "$CURDIR"/../shell_config.sh
 
-DATA_FILE=$(mktemp -q 02584_test_out_XXXXXX)
+DATA_FILE=$(mktemp -q 02584_test_data_XXXXXX)
 OUT_FILE=$(mktemp -q 02584_test_out_XXXXXX)
 
 echo "Hello, World!" > $DATA_FILE;
