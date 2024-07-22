133e734d73b87707ccba17bd488cf60e1895abe5
Antonio Andelic <antonio@clickhouse.com>
2024-07-22 21:53:22 +0200

    Move to integration test

--- tests/queries/0_stateless/03208_keepermap_incomplete_data_drop.sql
+++
@@ -1,8 +0,0 @@
-DROP TABLE IF EXISTS 03208_keepermap_test SYNC;
-
-CREATE TABLE 03208_keepermap_test (key UInt64, value UInt64) Engine=KeeperMap('/' || currentDatabase() || '/test03208') PRIMARY KEY(key);
-INSERT INTO 03208_keepermap_test VALUES (1, 11);
-SYSTEM ENABLE FAILPOINT keepermap_fail_drop_data;
-DROP TABLE 03208_keepermap_test SYNC; -- { KEEPER_EXCEPTION }
-SYSTEM DISABLE FAILPOINT keepermap_fail_drop_data;
-CREATE TABLE 03208_keepermap_test_another (key UInt64, value UInt64) Engine=KeeperMap('/' || currentDatabase() || '/test03208') PRIMARY KEY(key);
