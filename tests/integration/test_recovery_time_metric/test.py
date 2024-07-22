c2ac13291f3bf201f7189bd36f2c9be7c06aa886
Michael Stetsyuk <mike.stetsyuk@gmail.com>
2024-07-22 09:06:13 +0100

    fix tests

--- tests/integration/test_recovery_time_metric/test.py
+++ tests/integration/test_recovery_time_metric/test.py
@@ -3,7 +3,7 @@ from helpers.cluster import ClickHouseCluster
 
 cluster = ClickHouseCluster(__file__)
 node = cluster.add_instance(
-    "node", main_configs=["configs/config.xml"], with_zookeeper=True
+    "node", main_configs=["configs/config.xml"], with_zookeeper=True, stay_alive=True,
 )
 
 
