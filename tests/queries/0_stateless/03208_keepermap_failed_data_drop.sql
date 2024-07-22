c36dde51031e817b20db6773df47a42092f6616b
Antonio Andelic <antonio@clickhouse.com>
2024-07-22 12:17:29 +0200

    Fix KeeperMap create after incomplete drop

---
+++ tests/queries/0_stateless/03208_keepermap_failed_data_drop.sql
@@ -0,0 +1,7 @@
+DROP TABLE IF EXISTS 03208_keepermap_test SYNC;
+
+CREATE TABLE 03208_keepermap_test (key UInt64, value UInt64) Engine=KeeperMap('/' || currentDatabase() || '/test03208') PRIMARY KEY(key);
+INSERT INTO 03208_keepermap_test VALUES (1, 11);
+SYSTEM ENABLE FAILPOINT keepermap_fail_drop_data;
+DROP TABLE 03208_keepermap_test;
+CREATE TABLE 03208_keepermap_test_another (key UInt64, value UInt64) Engine=KeeperMap('/' || currentDatabase() || '/test03208') PRIMARY KEY(key);
