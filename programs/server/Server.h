9af6803da93ab305bfc1074fd2623bbf8a210975
TTPO100AJIEX <ttpo100ajiex@mail.ru>
2024-07-22 20:17:25 +0300

    Return some code to avoid conflicts

--- programs/server/Server.h
+++ programs/server/Server.h
@@ -1,10 +1,15 @@
 #pragma once
 
 #include <Server/IServer.h>
+
 #include <Daemon/BaseDaemon.h>
+#include <Server/HTTP/HTTPContext.h>
+#include <Server/TCPProtocolStackFactory.h>
+#include <Server/ServerType.h>
+#include <Poco/Net/HTTPServerParams.h>
 
 /** Server provides three interfaces:
-  * 1. HTTP, GRPC - simple interfaces for any applications.
+  * 1. HTTP - simple interface for any applications.
   * 2. TCP - interface for native clickhouse-client and for server to server internal communications.
   *    More rich and efficient, but less compatible
   *     - data is transferred by columns;
@@ -13,8 +18,18 @@
   * 3. Interserver HTTP - for replication.
   */
 
+namespace Poco
+{
+    namespace Net
+    {
+        class ServerSocket;
+    }
+}
+
 namespace DB
 {
+class AsynchronousMetrics;
+class ProtocolServerAdapter;
 
 class Server : public BaseDaemon, public IServer
 {
@@ -33,19 +48,73 @@ public:
 
 protected:
     int run() override;
-
     void initialize(Application & self) override;
-
     void uninitialize() override;
-
     int main(const std::vector<std::string> & args) override;
-
     std::string getDefaultCorePath() const override;
-
 private:
     ContextMutablePtr global_context;
     /// Updated/recent config, to compare http_handlers
     ConfigurationPtr latest_config;
+
+    // TODO: remove everything below after 64599
+
+    HTTPContextPtr httpContext() const;
+
+    Poco::Net::SocketAddress socketBindListen(
+        const Poco::Util::AbstractConfiguration & config,
+        Poco::Net::ServerSocket & socket,
+        const std::string & host,
+        UInt16 port,
+        [[maybe_unused]] bool secure = false) const;
+
+    std::unique_ptr<TCPProtocolStackFactory> buildProtocolStackFromConfig(
+        const Poco::Util::AbstractConfiguration & config,
+        const std::string & protocol,
+        Poco::Net::HTTPServerParams::Ptr http_params,
+        AsynchronousMetrics & async_metrics,
+        bool & is_secure);
+
+    using CreateServerFunc = std::function<ProtocolServerAdapter(UInt16)>;
+    void createServer(
+        Poco::Util::AbstractConfiguration & config,
+        const std::string & listen_host,
+        const char * port_name,
+        bool listen_try,
+        bool start_server,
+        std::vector<ProtocolServerAdapter> & servers,
+        CreateServerFunc && func) const;
+
+    void createServers(
+        Poco::Util::AbstractConfiguration & config,
+        const Strings & listen_hosts,
+        bool listen_try,
+        Poco::ThreadPool & server_pool,
+        AsynchronousMetrics & async_metrics,
+        std::vector<ProtocolServerAdapter> & servers,
+        bool start_servers = false,
+        const ServerType & server_type = ServerType(ServerType::Type::QUERIES_ALL));
+
+    void createInterserverServers(
+        Poco::Util::AbstractConfiguration & config,
+        const Strings & interserver_listen_hosts,
+        bool listen_try,
+        Poco::ThreadPool & server_pool,
+        AsynchronousMetrics & async_metrics,
+        std::vector<ProtocolServerAdapter> & servers,
+        bool start_servers = false,
+        const ServerType & server_type = ServerType(ServerType::Type::QUERIES_ALL));
+
+    void updateServers(
+        Poco::Util::AbstractConfiguration & config,
+        Poco::ThreadPool & server_pool,
+        AsynchronousMetrics & async_metrics,
+        std::vector<ProtocolServerAdapter> & servers,
+        std::vector<ProtocolServerAdapter> & servers_to_start_before_tables);
+
+    void stopServers(
+        std::vector<ProtocolServerAdapter> & servers,
+        const ServerType & server_type) const;
 };
 
 }
