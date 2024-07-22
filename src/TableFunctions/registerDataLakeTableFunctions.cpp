c59949d057bba311f040f7b6386b1f6b481a23dd
divanik <dan.ivanik@clickhouse.com>
2024-07-22 09:50:47 +0000

    Add different iceberg tables

--- src/TableFunctions/registerDataLakeTableFunctions.cpp
+++ src/TableFunctions/registerDataLakeTableFunctions.cpp
@@ -4,24 +4,31 @@
 namespace DB
 {
 
-#if USE_AWS_S3
 #if USE_AVRO
 void registerTableFunctionIceberg(TableFunctionFactory & factory)
 {
+#    if USE_AWS_S3
     factory.registerFunction<TableFunctionIceberg>(
-    {
-        .documentation =
-        {
-            .description=R"(The table function can be used to read the Iceberg table stored on object store.)",
+        {.documentation
+         = {.description = R"(The table function can be used to read the Iceberg table stored on S3 object store. Alias to icebergS3)",
             .examples{{"iceberg", "SELECT * FROM iceberg(url, access_key_id, secret_access_key)", ""}},
-            .categories{"DataLake"}
-        },
-        .allow_readonly = false
-    });
+            .categories{"DataLake"}},
+         .allow_readonly = false});
+    factory.registerFunction<TableFunctionIcebergS3>(
+        {.documentation
+         = {.description = R"(The table function can be used to read the Iceberg table stored on S3 object store.)",
+            .examples{{"iceberg", "SELECT * FROM iceberg(url, access_key_id, secret_access_key)", ""}},
+            .categories{"DataLake"}},
+         .allow_readonly = false});
+
+#    endif
+#    if USE_AZURE_BLOB_STORAGE
+#    endif
 }
 #endif
 
-#if USE_PARQUET
+#if USE_AWS_S3
+#    if USE_PARQUET
 void registerTableFunctionDeltaLake(TableFunctionFactory & factory)
 {
     factory.registerFunction<TableFunctionDeltaLake>(
