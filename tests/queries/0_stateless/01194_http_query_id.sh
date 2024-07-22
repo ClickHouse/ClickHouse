0f327869132940b3bae932730cb2ce2f9c394163
Raúl Marín <git@rmr.ninja>
2024-07-22 10:46:42 +0200

    Better random and queries

--- tests/queries/0_stateless/01194_http_query_id.sh
+++ tests/queries/0_stateless/01194_http_query_id.sh
@@ -4,14 +4,22 @@ CURDIR=$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)
 # shellcheck source=../shell_config.sh
 . "$CURDIR"/../shell_config.sh
 
-rnd=$RANDOM
-url="${CLICKHOUSE_URL}&session_id=test_01194_$RANDOM"
+rnd="$CLICKHOUSE_DATABASE"
+url="${CLICKHOUSE_URL}&session_id=test_01194_${CLICKHOUSE_DATABASE}"
 
 ${CLICKHOUSE_CURL} -sS "$url&query=SELECT+'test_01194',$rnd,1" > /dev/null
 ${CLICKHOUSE_CURL} -sS "$url&query=SELECT+'test_01194',$rnd,2" > /dev/null
 ${CLICKHOUSE_CURL} -sS "$url" --data "SELECT 'test_01194',$rnd,3" > /dev/null
 ${CLICKHOUSE_CURL} -sS "$url" --data "SELECT 'test_01194',$rnd,4" > /dev/null
 
-${CLICKHOUSE_CURL} -sS "${CLICKHOUSE_URL}" --data "SYSTEM FLUSH LOGS"
+$CLICKHOUSE_CLIENT -q "SYSTEM FLUSH LOGS"
 
-${CLICKHOUSE_CURL} -sS "${CLICKHOUSE_URL}" --data "SELECT count(DISTINCT query_id) FROM system.query_log WHERE current_database = currentDatabase() AND event_date >= yesterday() AND query LIKE 'SELECT ''test_01194'',$rnd%'"
+$CLICKHOUSE_CLIENT -q "
+  SELECT
+    count(DISTINCT query_id)
+  FROM system.query_log
+  WHERE
+        current_database = currentDatabase()
+    AND event_date >= yesterday()
+    AND query LIKE 'SELECT ''test_01194'',$rnd%'
+    AND query_id != queryID()"
