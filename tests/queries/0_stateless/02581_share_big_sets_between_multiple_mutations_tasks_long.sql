54a503910299c13dac305049a98e8136ab989fbb
Alexander Gololobov <davenger@clickhouse.com>
2024-07-23 19:25:38 +0200

    Count log messages for building set and reusing set from cache

--- tests/queries/0_stateless/02581_share_big_sets_between_multiple_mutations_tasks_long.sql
+++ tests/queries/0_stateless/02581_share_big_sets_between_multiple_mutations_tasks_long.sql
@@ -31,22 +31,22 @@ DELETE FROM 02581_trips                        WHERE id IN (SELECT (number*10 + 
 SELECT count(), _part from 02581_trips WHERE description = '' GROUP BY _part ORDER BY _part SETTINGS select_sequential_consistency=1;
 
 SYSTEM FLUSH LOGS;
-
--- Check that in every mutation there were parts where selected rows count then the size of big sets which will mean that sets were shared
--- Also check that there was at least one part that read more rows then the size of set which will mean that the 
-WITH 10000000 AS rows_in_set
+-- Check that in every mutation there were parts that built sets (log messages like 'Created Set with 10000000 entries from 10000000 rows in 0.388989187 sec.' )
+-- and parts that shared sets (log messages like 'Got set from cache in 0.388930505 sec.' )
+WITH (
+        SELECT uuid
+        FROM system.tables
+        WHERE (database = currentDatabase()) AND (name = '02581_trips')
+    ) AS table_uuid
 SELECT
-    mutation_version,
-    countIf(read_rows >= rows_in_set) >= 1 as has_parts_for_which_set_was_built,
-    countIf(read_rows <= rows_in_set) >= 1 as has_parts_that_shared_set
-FROM
-(
-    SELECT
-        CAST(splitByChar('_', part_name)[5], 'UInt64') AS mutation_version,
-        read_rows
-    FROM system.part_log
-    WHERE database = currentDatabase() and (event_date >= yesterday()) AND (`table` = '02581_trips') AND (event_type = 'MutatePart')
-)
+    CAST(splitByChar('_', query_id)[5], 'UInt64') AS mutation_version, -- '5521485f-8a40-4aba-87a2-00342c369563::all_3_3_0_6'
+    sum(message LIKE 'Created Set with % entries%') >= 1  AS has_parts_for_which_set_was_built,
+    sum(message LIKE 'Got set from cache%') >= 1 AS has_parts_that_shared_set
+FROM system.text_log
+WHERE
+    query_id LIKE concat(CAST(table_uuid, 'String'), '::all\\_%')
+    AND (event_date >= yesterday())
+    AND (message LIKE 'Created Set with % entries%' OR message LIKE 'Got set from cache%')
 GROUP BY mutation_version ORDER BY mutation_version FORMAT TSVWithNames;
 
 DROP TABLE 02581_trips;
