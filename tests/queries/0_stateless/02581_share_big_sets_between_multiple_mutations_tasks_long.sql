07f642f1cac5253fda7eaa928ca5e659159b6b16
Alexey Milovidov <milovidov@clickhouse.com>
2024-07-23 03:13:52 +0200

    Fix bad tests `share_big_sets`, CC @davenger

--- tests/queries/0_stateless/02581_share_big_sets_between_multiple_mutations_tasks_long.sql
+++ tests/queries/0_stateless/02581_share_big_sets_between_multiple_mutations_tasks_long.sql
@@ -18,12 +18,15 @@ SELECT name FROM system.parts WHERE database=currentDatabase() AND table = '0258
 
 -- Start multiple mutations simultaneously
 SYSTEM STOP MERGES 02581_trips;
-ALTER TABLE 02581_trips UPDATE description='5' WHERE id IN (SELECT (number*10 + 5)::UInt32 FROM numbers(200000000)) SETTINGS mutations_sync=0;
-ALTER TABLE 02581_trips UPDATE description='6' WHERE id IN (SELECT (number*10 + 6)::UInt32 FROM numbers(200000000)) SETTINGS mutations_sync=0;
-ALTER TABLE 02581_trips DELETE WHERE id IN (SELECT (number*10 + 7)::UInt32 FROM numbers(200000000)) SETTINGS mutations_sync=0;
-ALTER TABLE 02581_trips UPDATE description='8' WHERE id IN (SELECT (number*10 + 8)::UInt32 FROM numbers(200000000)) SETTINGS mutations_sync=0;
+ALTER TABLE 02581_trips UPDATE description='5' WHERE id IN (SELECT (number*10 + 5)::UInt32 FROM numbers(10000000)) SETTINGS mutations_sync=0;
+ALTER TABLE 02581_trips UPDATE description='6' WHERE id IN (SELECT (number*10 + 6)::UInt32 FROM numbers(10000000)) SETTINGS mutations_sync=0;
+ALTER TABLE 02581_trips DELETE                 WHERE id IN (SELECT (number*10 + 7)::UInt32 FROM numbers(10000000)) SETTINGS mutations_sync=0;
+ALTER TABLE 02581_trips UPDATE description='8' WHERE id IN (SELECT (number*10 + 8)::UInt32 FROM numbers(10000000)) SETTINGS mutations_sync=0;
 SYSTEM START MERGES 02581_trips;
-DELETE FROM 02581_trips WHERE id IN (SELECT (number*10 + 9)::UInt32 FROM numbers(200000000));
+DELETE FROM 02581_trips                        WHERE id IN (SELECT (number*10 + 9)::UInt32 FROM numbers(10000000));
 SELECT count(), _part from 02581_trips WHERE description = '' GROUP BY _part ORDER BY _part;
+
+SYSTEM FLUSH LOGS;
+SELECT DISTINCT peak_memory_usage < 2000000000 ? 'Ok' : toString(tuple(*)) FROM system.part_log WHERE database = currentDatabase() AND event_date >= yesterday() AND table = '02581_trips' AND event_type = 'MutatePart';
 
 DROP TABLE 02581_trips;
--- tests/queries/0_stateless/02581_share_big_sets_between_mutation_tasks_long.sql
+++ tests/queries/0_stateless/02581_share_big_sets_between_mutation_tasks_long.sql
@@ -18,42 +18,45 @@ SELECT count() from 02581_trips WHERE description = '';
 SELECT name FROM system.parts WHERE database=currentDatabase() AND table = '02581_trips' AND active ORDER BY name;
 
 -- Run mutation with `id` a 'IN big subquery'
-ALTER TABLE 02581_trips UPDATE description='a' WHERE id IN (SELECT (number*10)::UInt32 FROM numbers(200000000)) SETTINGS mutations_sync=2;
+ALTER TABLE 02581_trips UPDATE description='a' WHERE id IN (SELECT (number*10)::UInt32 FROM numbers(10000000)) SETTINGS mutations_sync=2;
 SELECT count() from 02581_trips WHERE description = '';
 
-ALTER TABLE 02581_trips UPDATE description='a' WHERE id IN (SELECT (number*10 + 1)::UInt32 FROM numbers(200000000)) SETTINGS mutations_sync=2, max_rows_in_set=1000;
+ALTER TABLE 02581_trips UPDATE description='a' WHERE id IN (SELECT (number*10 + 1)::UInt32 FROM numbers(10000000)) SETTINGS mutations_sync=2, max_rows_in_set=1000;
 SELECT count() from 02581_trips WHERE description = '';
 
 -- Run mutation with func(`id`) IN big subquery
-ALTER TABLE 02581_trips UPDATE description='b' WHERE id::UInt64 IN (SELECT (number*10 + 2)::UInt32 FROM numbers(200000000)) SETTINGS mutations_sync=2;
+ALTER TABLE 02581_trips UPDATE description='b' WHERE id::UInt64 IN (SELECT (number*10 + 2)::UInt32 FROM numbers(10000000)) SETTINGS mutations_sync=2;
 SELECT count() from 02581_trips WHERE description = '';
 
 -- Run mutation with non-PK `id2` IN big subquery
-ALTER TABLE 02581_trips UPDATE description='c' WHERE id2 IN (SELECT (number*10 + 3)::UInt32 FROM numbers(200000000)) SETTINGS mutations_sync=2;
+ALTER TABLE 02581_trips UPDATE description='c' WHERE id2 IN (SELECT (number*10 + 3)::UInt32 FROM numbers(10000000)) SETTINGS mutations_sync=2;
 SELECT count() from 02581_trips WHERE description = '';
 
 -- Run mutation with PK and non-PK IN big subquery
 ALTER TABLE 02581_trips UPDATE description='c'
 WHERE
-    (id IN (SELECT (number*10 + 4)::UInt32 FROM numbers(200000000))) OR
-    (id2 IN (SELECT (number*10 + 4)::UInt32 FROM numbers(200000000)))
+    (id IN (SELECT (number*10 + 4)::UInt32 FROM numbers(10000000))) OR
+    (id2 IN (SELECT (number*10 + 4)::UInt32 FROM numbers(10000000)))
 SETTINGS mutations_sync=2;
 SELECT count() from 02581_trips WHERE description = '';
 
 -- Run mutation with PK and non-PK IN big subquery
 ALTER TABLE 02581_trips UPDATE description='c'
 WHERE
-    (id::UInt64 IN (SELECT (number*10 + 5)::UInt32 FROM numbers(200000000))) OR
-    (id2::UInt64 IN (SELECT (number*10 + 5)::UInt32 FROM numbers(200000000)))
+    (id::UInt64 IN (SELECT (number*10 + 5)::UInt32 FROM numbers(10000000))) OR
+    (id2::UInt64 IN (SELECT (number*10 + 5)::UInt32 FROM numbers(10000000)))
 SETTINGS mutations_sync=2;
 SELECT count() from 02581_trips WHERE description = '';
 
 -- Run mutation with PK and non-PK IN big subquery
 ALTER TABLE 02581_trips UPDATE description='c'
 WHERE
-    (id::UInt32 IN (SELECT (number*10 + 6)::UInt32 FROM numbers(200000000))) OR
-    ((id2+1)::String IN (SELECT (number*10 + 6)::UInt32 FROM numbers(200000000)))
+    (id::UInt32 IN (SELECT (number*10 + 6)::UInt32 FROM numbers(10000000))) OR
+    ((id2+1)::String IN (SELECT (number*10 + 6)::UInt32 FROM numbers(10000000)))
 SETTINGS mutations_sync=2;
 SELECT count() from 02581_trips WHERE description = '';
+
+SYSTEM FLUSH LOGS;
+SELECT DISTINCT peak_memory_usage < 2000000000 ? 'Ok' : toString(tuple(*)) FROM system.part_log WHERE database = currentDatabase() AND event_date >= yesterday() AND table = '02581_trips' AND event_type = 'MutatePart';
 
 DROP TABLE 02581_trips;
