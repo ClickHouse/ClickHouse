caaf14a0a91bd4779cc292dd674761a47148d24d
Alexey Korepanov <alexey.korepanov@clickhouse.com>
2024-07-24 17:56:14 +0100

    Fix flaky test by ignoring progress lines when comparing output

--- tests/queries/0_stateless/03174_json_compact_with_progress.sh
+++ tests/queries/0_stateless/03174_json_compact_with_progress.sh
@@ -11,10 +11,10 @@ $CLICKHOUSE_CLIENT -q "SELECT 1;"
 # Check JSONCompactWithProgress Output 
 $CLICKHOUSE_CLIENT -q "CREATE TABLE test_table (value UInt8, name String) ENGINE = MergeTree() ORDER BY value;"
 $CLICKHOUSE_CLIENT -q "INSERT INTO test_table VALUES (1, 'a'), (2, 'b'), (3, 'c');"
-$CLICKHOUSE_CLIENT -q "SELECT *, sleep(0.1) FROM test_table FORMAT JSONCompactWithProgress settings max_block_size=2;" | sed -E 's/"elapsed_ns":"[0-9]+"/"elapsed_ns":"ELAPSED_NS"/g; s/"elapsed":[0-9]+\.[0-9]+/"elapsed":ELAPSED/g'
+$CLICKHOUSE_CLIENT -q "SELECT * FROM test_table FORMAT JSONCompactWithProgress settings max_block_size=2;" |  grep -v --text "progress" | sed -E 's/"elapsed":[0-9]+\.[0-9]+/"elapsed":ELAPSED/g'
 
 $CLICKHOUSE_CLIENT -q "SELECT 2;"
 # Check Totals 
-$CLICKHOUSE_CLIENT -q "SELECT name, count(), sleep(0.1) AS c FROM test_table GROUP BY name WITH TOTALS ORDER BY name FORMAT JSONCompactWithProgress settings max_block_size=2;" | sed -E 's/"elapsed_ns":"[0-9]+"/"elapsed_ns":"ELAPSED_NS"/g; s/"elapsed":[0-9]+\.[0-9]+/"elapsed":ELAPSED/g'
+$CLICKHOUSE_CLIENT -q "SELECT name, count() AS c FROM test_table GROUP BY name WITH TOTALS ORDER BY name FORMAT JSONCompactWithProgress settings max_block_size=2;" | grep -v --text "progress" | sed -E 's/"elapsed":[0-9]+\.[0-9]+/"elapsed":ELAPSED/g'
 
 $CLICKHOUSE_CLIENT -q "DROP TABLE IF EXISTS test_table;"
