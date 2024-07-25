3f70977cd660e4617d9bbd68cc229020adc57f98
jsc0218 <jsc0218@gmail.com>
2024-07-25 21:02:30 +0000

    try to fix

--- tests/queries/0_stateless/02572_query_views_log_background_thread.sql
+++ tests/queries/0_stateless/02572_query_views_log_background_thread.sql
@@ -19,13 +19,13 @@ create materialized view mv_02572 to copy_02572 as select * from data_02572;
 -- { echoOn }
 insert into buffer_02572 values (1);
 -- ensure that the flush was not direct
+select * from buffer_02572;
 select * from data_02572;
 select * from copy_02572;
 -- we cannot use OPTIMIZE, this will attach query context, so let's wait
 SET function_sleep_max_microseconds_per_block = 6000000;
 select sleepEachRow(1) from numbers(3*2) format Null;
-select * from data_02572;
-select * from copy_02572;
+select sleepEachRow(1) from numbers(3*2) format Null;
 
 system flush logs;
 select count() > 0, lower(status::String), errorCodeToName(exception_code)
@@ -34,3 +34,7 @@ select count() > 0, lower(status::String), errorCodeToName(exception_code)
     view_target = concatWithSeparator('.', currentDatabase(), 'copy_02572')
     group by 2, 3
 ;
+
+select * from buffer_02572;
+select * from data_02572;
+select * from copy_02572;
