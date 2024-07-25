c6a643f981505d0293358c912723f1aece480c7c
Nikita Fomichev <nikita.fomichev@clickhouse.com>
2024-07-25 10:31:38 +0200

    Update tests/queries/0_stateless/02992_all_columns_should_have_comment.sql

--- tests/queries/0_stateless/02992_all_columns_should_have_comment.sql
+++ tests/queries/0_stateless/02992_all_columns_should_have_comment.sql
@@ -3,6 +3,6 @@ SELECT 'Column ' || name || ' from table ' || concat(database, '.', table) || ' 
 FROM system.columns
 WHERE (database = 'system') AND
       (comment = '') AND
-      (table NOT ILIKE '%\_log\_%') AND
+      (table NOT ILIKE '%log%') AND
       (table NOT IN ('numbers', 'numbers_mt', 'one', 'generate_series', 'generateSeries', 'coverage_log', 'filesystem_read_prefetches_log')) AND
       (default_kind != 'ALIAS');
