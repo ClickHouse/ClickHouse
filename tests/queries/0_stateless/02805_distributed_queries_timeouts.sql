1a4730f1f390e468dab2849bd1b2770e0fb2cbe6
Azat Khuzhin <a3at.mail@gmail.com>
2024-07-25 12:03:50 +0200

    Use Distributed table to avoid extra DESC queries
    Signed-off-by: Azat Khuzhin <a.khuzhin@semrush.com>

--- tests/queries/0_stateless/02805_distributed_queries_timeouts.sql
+++ tests/queries/0_stateless/02805_distributed_queries_timeouts.sql
@@ -1,3 +1,4 @@
-select * from remote('127.2', view(select sleep(3) from system.one)) settings receive_timeout=1, async_socket_for_remote=0, use_hedged_requests=1 format Null;
-select * from remote('127.2', view(select sleep(3) from system.one)) settings receive_timeout=1, async_socket_for_remote=1, use_hedged_requests=0 format Null;
-select * from remote('127.2', view(select sleep(3) from system.one)) settings receive_timeout=1, async_socket_for_remote=0, use_hedged_requests=0 format Null;
+create table dist as system.one engine=Distributed(test_shard_localhost, system, one);
+select sleep(3) from dist settings prefer_localhost_replica=0, receive_timeout=1, async_socket_for_remote=0, use_hedged_requests=1 format Null;
+select sleep(3) from dist settings prefer_localhost_replica=0, receive_timeout=1, async_socket_for_remote=1, use_hedged_requests=0 format Null;
+select sleep(3) from dist settings prefer_localhost_replica=0, receive_timeout=1, async_socket_for_remote=0, use_hedged_requests=0 format Null;
