109bd52296550f506b14134b934c6eb8fd3ec094
Nikita Taranov <nikita.taranov@clickhouse.com>
2024-07-26 23:05:56 +0100

    impl

--- src/AggregateFunctions/fuzzers/aggregate_function_state_deserialization_fuzzer.cpp
+++ src/AggregateFunctions/fuzzers/aggregate_function_state_deserialization_fuzzer.cpp
@@ -12,6 +12,7 @@
 
 #include <Interpreters/Context.h>
 
+#include <AggregateFunctions/IAggregateFunction.h>
 #include <AggregateFunctions/registerAggregateFunctions.h>
 
 #include <base/scope_guard.h>
