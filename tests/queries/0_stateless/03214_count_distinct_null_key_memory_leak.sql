aec431f68bf16b45a6b36deb61c146e08cb4f644
heguangnan <heguangnan@bytedance.com>
2024-07-30 22:54:05 +0800

    fix test

--- tests/queries/0_stateless/03214_count_distinct_null_key_memory_leak.sql
+++ tests/queries/0_stateless/03214_count_distinct_null_key_memory_leak.sql
@@ -12,7 +12,7 @@ PARTITION BY tuple()
 ORDER BY c
 SETTINGS index_granularity = 8192, allow_nullable_key=1;
 
-INSERT INTO testnull(b,c) SELECT toString(rand64()) AS b, toString(rand64()) AS c FROM numbers(1000000)
+INSERT INTO testnull(b,c) SELECT toString(rand64()) AS b, toString(rand64()) AS c FROM numbers(1000000);
 SELECT count(distinct b) FROM testnull GROUP BY a  SETTINGS max_memory_usage = 54748364; -- {serverError MEMORY_LIMIT_EXCEEDED}
 
 DROP TABLE testnull;
