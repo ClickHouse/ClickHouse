c81d3322b18b0eb4b45b91ac019a8c4f42d7518d
Kseniia Sumarokova <54203879+kssenii@users.noreply.github.com>
2024-07-31 13:39:30 +0200

    Update 02150_index_hypothesis_race_long.sh

--- tests/queries/0_stateless/02150_index_hypothesis_race_long.sh
+++ tests/queries/0_stateless/02150_index_hypothesis_race_long.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
-# Tags: long, no-distributed-cache
+# Tags: long, no-random-settings, no-distributed-cache
 
 CURDIR=$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)
 # shellcheck source=../shell_config.sh
