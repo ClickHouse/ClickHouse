472cba0dd002ed967eace577a7d775e19021c726
Haydn <zawalee33@gmail.com>
2024-07-30 21:16:54 -0700

    Tests without simulating network error

--- tests/queries/0_stateless/03204_replica_outage_management.sql
+++ tests/queries/0_stateless/03204_replica_outage_management.sql
@@ -1,5 +1,5 @@
--- Step 1: Create necessary tables
-CREATE TABLE local_table ON CLUSTER '{cluster}'
+-- Step 1: Create necessary tables on the cluster
+CREATE TABLE local_table ON CLUSTER 'test_shard_localhost'
 (
     id UInt32,
     value String
@@ -7,19 +7,17 @@ CREATE TABLE local_table ON CLUSTER '{cluster}'
 ENGINE = MergeTree()
 ORDER BY id;
 
-CREATE TABLE distributed_table ON CLUSTER '{cluster}'
+CREATE TABLE distributed_table ON CLUSTER 'test_shard_localhost'
 (
     id UInt32,
     value String
 )
-ENGINE = Distributed('{cluster}', 'default', 'local_table', rand());
+ENGINE = Distributed('test_shard_localhost', 'default', 'local_table', rand());
 
 -- Step 2: Insert data into the distributed table
 INSERT INTO distributed_table VALUES (1, 'test1'), (2, 'test2'), (3, 'test3');
 
 -- Step 3: Simulate network error
--- This would typically be done by changing the network conditions or the configuration of the cluster.
--- For the sake of this test, we will assume the implementation of such a simulation is handled by the testing framework.
 
 -- Step 4: Query the distributed table to verify the results
 SELECT id, value FROM distributed_table ORDER BY id;
