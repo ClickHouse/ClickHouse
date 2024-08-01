51b39a6c745d61cb2e6feb39659ddb3cac57ad03
Nikita Taranov <nikita.taranov@clickhouse.com>
2024-08-01 18:24:45 +0100

    some more

--- tests/integration/test_executable_dictionary/test.py
+++ tests/integration/test_executable_dictionary/test.py
@@ -235,6 +235,7 @@ def test_executable_implicit_input_signalled_python(started_cluster):
     )
 
 
+@pytest.mark.repeat(50)
 def test_executable_input_slow_python(started_cluster):
     skip_test_msan(node)
     assert node.query_and_get_error(
