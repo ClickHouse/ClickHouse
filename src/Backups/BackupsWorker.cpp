08f8d94856841254957e1e746685d32de2074dc6
Sema Checherinda <Sema.Checherinda@clickhouse.com>
2024-08-02 12:16:44 +0200

    no flush backup logs at shutdown, flush all logs

--- src/Backups/BackupsWorker.cpp
+++ src/Backups/BackupsWorker.cpp
@@ -1171,8 +1171,6 @@ void BackupsWorker::waitAll()
     for (const auto & id : current_operations)
         wait(id, /* rethrow_exception= */ false);
 
-    backup_log->flush(backup_log->getLastLogIndex(), /* should_prepare_tables_anyway */ false);
-
     LOG_INFO(log, "Backups and restores finished");
 }
 
@@ -1222,8 +1220,6 @@ void BackupsWorker::cancelAll(bool wait_)
     if (wait_)
         for (const auto & id : current_operations)
             wait(id, /* rethrow_exception= */ false);
-
-    backup_log->flush(backup_log->getLastLogIndex(), /* should_prepare_tables_anyway */ false);
 
     LOG_INFO(log, "Backups and restores finished or stopped");
 }
