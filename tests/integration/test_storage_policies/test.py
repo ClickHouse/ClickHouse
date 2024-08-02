462fe8b7ffa98743ecc01c98eb0bfae840b05dca
Sema Checherinda <Sema.Checherinda@clickhouse.com>
2024-08-02 18:23:56 +0200

    repro for #67478

--- tests/integration/test_storage_policies/test.py
+++ tests/integration/test_storage_policies/test.py
@@ -38,3 +38,82 @@ def test_storage_policy_configuration_change(started_cluster):
         "/etc/clickhouse-server/config.d/disks.xml",
     )
     node.start_clickhouse()
+
+
+def test_disk_is_immutable(started_cluster):
+    node.query("DROP TABLE IF EXISTS test_1")
+
+    node.query(
+        """
+        create table test_1 (a Int32)
+        engine = MergeTree()
+        order by tuple()
+        settings
+            disk=disk(
+                name='not_uniq_disk_name',
+                type = object_storage,
+                object_storage_type = local_blob_storage,
+                path='./03215_data_test_1/')
+        """
+    )
+
+    node.query("INSERT INTO test_1 VALUES (1)")
+    node.query("SYSTEM FLUSH LOGS;")
+
+    print(
+        node.query(
+            "SELECT 'test_1', * FROM system.blob_storage_log"
+        )
+    )
+
+    print(
+        node.query(
+            "SELECT 'test_1', * FROM test_1"
+        )
+    )
+
+    node.query("DROP TABLE test_1 SYNC")
+    node.query("DROP TABLE IF EXISTS test_2")
+
+    node.query(
+        """
+        create table test_2 (a Int32)
+        engine = MergeTree()
+        order by tuple()
+        settings
+            disk=disk(
+                name='not_uniq_disk_name',
+                type = object_storage,
+                object_storage_type = local_blob_storage,
+                path='./03215_data_test_2/')
+        """
+    )
+
+    node.query("INSERT INTO test_2 VALUES (1)")
+    node.query("SYSTEM FLUSH LOGS;")
+
+    print(
+        node.query(
+            "SELECT 'test_2', * FROM system.blob_storage_log"
+        )
+    )
+
+    print(
+        node.query(
+            "SELECT 'test_2', * FROM test_2"
+        )
+    )
+
+    node.restart_clickhouse()
+
+    print(
+        node.query(
+            "SELECT 'test_2', * FROM system.blob_storage_log"
+        )
+    )
+
+    print(
+        node.query(
+            "SELECT 'test_2', * FROM test_2"
+        )
+    )
