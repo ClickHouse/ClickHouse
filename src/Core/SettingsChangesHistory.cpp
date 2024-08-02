62d94e70170446e6f2417ba2a94962813f9d6691
Konstantin Morozov <just.morozov.k@gmail.com>
2024-08-02 17:26:10 +0000

    set setting

--- src/Core/SettingsChangesHistory.cpp
+++ src/Core/SettingsChangesHistory.cpp
@@ -75,6 +75,7 @@ static std::initializer_list<std::pair<ClickHouseVersion, SettingsChangesHistory
     },
     {"24.8",
         {
+            {"allow_experimental_drop_detached_table", true, false, "Enable drop detach table."},
             {"merge_tree_min_bytes_per_task_for_remote_reading", 4194304, 2097152, "Value is unified with `filesystem_prefetch_min_bytes_for_single_read_task`"},
         }
     },
