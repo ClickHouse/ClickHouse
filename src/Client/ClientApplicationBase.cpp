3fd6de51ba218ff4c09e54a523dabec23419a52e
Robert Schulze <robert@clickhouse.com>
2024-08-04 11:20:47 +0000

    (ignore)

--- src/Client/ClientApplicationBase.cpp
+++ src/Client/ClientApplicationBase.cpp
@@ -162,7 +162,7 @@ void ClientApplicationBase::init(int argc, char ** argv)
 
         ("query,q", po::value<std::vector<std::string>>()->multitoken(), R"(Query. Can be specified multiple times (--query "SELECT 1" --query "SELECT 2") or once with multiple comma-separated queries (--query "SELECT 1; SELECT 2;"). In the latter case, INSERT queries with non-VALUE format must be separated by empty lines.)")
         ("queries-file", po::value<std::vector<std::string>>()->multitoken(), "file path with queries to execute; multiple files can be specified (--queries-file file1 file2...)")
-        ("multiquery,n", "Obsolete, does nothing")
+        /// ("multiquery,n", "Obsolete, does nothing")
         ("multiline,m", "If specified, allow multiline queries (do not send the query on Enter)")
         ("database,d", po::value<std::string>(), "database")
         ("query_kind", po::value<std::string>()->default_value("initial_query"), "One of initial_query/secondary_query/no_query")
