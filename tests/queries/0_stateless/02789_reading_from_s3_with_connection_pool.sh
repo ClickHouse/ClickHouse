ff0b8889ab8bf347343e18b4e9a407b3cab8264b
Alexey Milovidov <milovidov@clickhouse.com>
2024-08-05 14:36:24 +0200

    Update 02789_reading_from_s3_with_connection_pool.sh

--- tests/queries/0_stateless/02789_reading_from_s3_with_connection_pool.sh
+++ tests/queries/0_stateless/02789_reading_from_s3_with_connection_pool.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
-# Tags: no-fasttest, no-random-settings, no-replicated-database
+# Tags: no-fasttest, no-random-settings, no-replicated-database, no-distributed-cache
 
 CUR_DIR=$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)
 # shellcheck source=../shell_config.sh
