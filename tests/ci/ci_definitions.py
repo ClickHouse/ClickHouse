086b3d240dd696c483f136d79db4587a83bb0a14
Nikita Fomichev <nikita.fomichev@clickhouse.com>
2024-08-07 23:34:36 +0200

    CI: push CI

--- tests/ci/ci_definitions.py
+++ tests/ci/ci_definitions.py
@@ -420,7 +420,7 @@ class CommonJobConfigs:
         ),
         run_command='functional_test_check.py "$CHECK_NAME"',
         runner_type=Runners.FUNC_TESTER,
-        timeout=1000,  # test
+        timeout=1001,  # test
     )
     STATEFUL_TEST = JobConfig(
         job_name_keyword="stateful",
