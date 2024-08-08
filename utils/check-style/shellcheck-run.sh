a11163c0535577994924c0bb09e8eeba3fc9c31b
Pablo Marcos <pablo.marcos.oltra@clickhouse.com>
2024-08-08 13:55:42 +0000

    Moar traces

--- utils/check-style/shellcheck-run.sh
+++ utils/check-style/shellcheck-run.sh
@@ -4,6 +4,9 @@ NPROC=$(($(nproc) + 3))
 
 start_total=$(date +%s)
 
+echo "Finding test shell files..."
+time find "$ROOT_PATH/tests/queries/"{0_stateless,1_stateful} -name '*.sh' -print0 >& /dev/null
+
 # Check sh tests with Shellcheck
 find "$ROOT_PATH/tests/queries/"{0_stateless,1_stateful} -name '*.sh' -print0 | \
   xargs -0 -P "$NPROC" -n 20 time shellcheck --check-sourced --external-sources --source-path=SCRIPTDIR \
@@ -11,6 +14,12 @@ find "$ROOT_PATH/tests/queries/"{0_stateless,1_stateful} -name '*.sh' -print0 | 
 
 after_test_sh=$(date +%s)
 echo "Running schellcheck on test shell files took $((after_test_sh - start_total)) seconds"
+
+
+echo "Finding docker shell files..."
+time find "$ROOT_PATH/docker" -type f -exec file -F' ' --mime-type {} + | \
+  awk '$2=="text/x-shellscript" {print $1}' | \
+  grep -v "compare.sh" >& /dev/null
 
 # Check docker scripts with shellcheck
 # Do not check sourced files, since it causes broken --source-path=SCRIPTDIR
