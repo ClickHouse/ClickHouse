dc67b51db6d9388afbc482eb7cf35ed1dbc494ce
Pablo Marcos <pablo.marcos.oltra@clickhouse.com>
2024-08-08 15:25:54 +0000

    Fix time command

--- utils/check-style/shellcheck-run.sh
+++ utils/check-style/shellcheck-run.sh
@@ -6,18 +6,18 @@ NPROC=$(($(nproc) + 3))
 start_total=$(date +%s)
 
 echo "Finding test shell files..."
-time find "$ROOT_PATH/tests/queries/"{0_stateless,1_stateful} -name '*.sh' -print0 >& /dev/null
+/usr/bin/time find "$ROOT_PATH/tests/queries/"{0_stateless,1_stateful} -name '*.sh' -print0 >& /dev/null
 
 # Check sh tests with Shellcheck
 find "$ROOT_PATH/tests/queries/"{0_stateless,1_stateful} -name '*.sh' -print0 | \
-  xargs -0 -P "$NPROC" -n 20 time shellcheck --check-sourced --external-sources --source-path=SCRIPTDIR \
+  xargs -0 -P "$NPROC" -n 20 /usr/bin/time shellcheck --check-sourced --external-sources --source-path=SCRIPTDIR \
   --severity info --exclude SC1071,SC2086,SC2016
 
 after_test_sh=$(date +%s)
 echo "Running schellcheck on test shell files took $((after_test_sh - start_total)) seconds"
 
 echo "Finding docker shell files..."
-time find "$ROOT_PATH/docker" -type f -exec file -F' ' --mime-type {} + | \
+/usr/bin/time find "$ROOT_PATH/docker" -type f -exec file -F' ' --mime-type {} + | \
   awk '$2=="text/x-shellscript" {print $1}' | \
   grep -v "compare.sh" >& /dev/null
 
@@ -26,6 +26,6 @@ time find "$ROOT_PATH/docker" -type f -exec file -F' ' --mime-type {} + | \
 find "$ROOT_PATH/docker" -type f -exec file -F' ' --mime-type {} + | \
   awk '$2=="text/x-shellscript" {print $1}' | \
   grep -v "compare.sh" | \
-  xargs -P "$NPROC" -n 20 time shellcheck --external-sources --source-path=SCRIPTDIR
+  xargs -P "$NPROC" -n 20 /usr/bin/time shellcheck --external-sources --source-path=SCRIPTDIR
 
 echo "Running schellcheck on docker shell files took $(($(date +%s) - after_test_sh)) seconds"
