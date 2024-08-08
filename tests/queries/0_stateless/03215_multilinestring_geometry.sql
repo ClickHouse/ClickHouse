2bd7606280b0714febc1d034233ee0fb9a3532a6
Jacob Reckhard <jacobreckhard@gmail.com>
2024-08-08 11:39:06 -0600

    added additional tests

--- tests/queries/0_stateless/03215_multilinestring_geometry.sql
+++ tests/queries/0_stateless/03215_multilinestring_geometry.sql
@@ -10,3 +10,15 @@ SELECT wkt(readWKTMultiLineString('MULTILINESTRING ((1 1, 2 2, 3 3, 1 1), (1 0, 
 -- Native Array(Array(Tuple(Float64, Float64))) is treated as Polygon, not as MultiLineString.
 WITH wkt(CAST([[(1, 1), (2, 2), (3, 3), (1, 1)]], 'Array(Array(Tuple(Float64, Float64)))')) as x
 SELECT x, toTypeName(x), readWKTPolygon(x) as y, toTypeName(y);
+
+-- Non constant tests
+
+CREATE TABLE IF NOT EXISTS t (shape Array(Array(Tuple(Float64, Float64))), wkt_string String) Engine = Memory;
+INSERT INTO t (shape, wkt_string) VALUES ([[(1, 1), (2, 2), (3, 3), (1, 1)]], 'MULTILINESTRING ((1 1, 2 2, 3 3, 1 1))');
+INSERT INTO t (shape, wkt_string) VALUES ([[(1, 1), (2, 2), (3, 3), (1, 1)], [(1, 0), (2, 0), (3, 0)]], 'MULTILINESTRING ((1 1, 2 2, 3 3, 1 1), (1 0, 2 0, 3 0))');
+
+-- Native Array(Array(Tuple(Float64, Float64))) is treated as Polygon, not as MultiLineString.
+-- but reading MultiLineString should still return an Array(Array(Tuple(Float64, Float64)))
+select wkt(shape), readWKTMultiLineString(wkt_string), readWKTMultiLineString(wkt_string) = shape from t;
+
+
