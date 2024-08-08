6e7587e9b3dbc791f6970fe23703c1d65aa4b7f0
JackyWoo <wuchienchao@qq.com>
2024-08-08 14:44:08 +0800

    Fix test for old analyzer

--- tests/queries/0_stateless/02864_statistics_estimation.sql
+++ tests/queries/0_stateless/02864_statistics_estimation.sql
@@ -6,6 +6,7 @@ DROP TABLE IF EXISTS tab2 SYNC;
 SET allow_experimental_statistics = 1;
 SET allow_statistics_optimize = 1;
 SET allow_suspicious_low_cardinality_types=1;
+SET allow_experimental_analyzer=1;
 SET mutations_sync = 2;
 
 CREATE TABLE tab
@@ -27,9 +28,9 @@ SELECT 'Test statistics count_min:';
 
 ALTER TABLE tab ADD STATISTICS a, b, c TYPE count_min;
 ALTER TABLE tab MATERIALIZE STATISTICS a, b, c;
-SELECT replaceRegexpAll(explain, '__table1.|_UInt8|_Int8|_UInt16|_String|_DateTime', '')
+SELECT replaceRegexpAll(explain,  '_CAST', 'CAST') FROM (SELECT replaceRegexpAll(explain, '__table1.|_UInt8|_Int8|_UInt16|_String|_DateTime', '') as explain
 FROM (EXPLAIN actions=1 SELECT count(*) FROM tab WHERE c = 0/*100*/ and b = 0/*10*/ and a = '0'/*1*/)
-WHERE explain LIKE '%Prewhere%' OR explain LIKE '%Filter column%';
+WHERE explain LIKE '%Prewhere%' OR explain LIKE '%Filter column%');
 ALTER TABLE tab DROP STATISTICS a, b, c;
 
 
@@ -38,9 +39,9 @@ SELECT 'Test statistics minmax:';
 ALTER TABLE tab ADD STATISTICS b, c, d TYPE minmax;
 ALTER TABLE tab MATERIALIZE STATISTICS b, c, d;
 
-SELECT replaceRegexpAll(explain, '__table1.|_UInt8|_Int8|_UInt16|_String|_DateTime', '')
+SELECT replaceRegexpAll(explain,  '_CAST', 'CAST') FROM (SELECT replaceRegexpAll(explain, '__table1.|_UInt8|_Int8|_UInt16|_String|_DateTime', '') as explain
 FROM (EXPLAIN actions=1 SELECT count(*) FROM tab WHERE b > 0/*10000*/ and c < -1/*9990*/ and d > cast(9998, 'DateTime')/*1*/)
-WHERE explain LIKE '%Prewhere%' OR explain LIKE '%Filter column%';
+WHERE explain LIKE '%Prewhere%' OR explain LIKE '%Filter column%');
 
 ALTER TABLE tab DROP STATISTICS b, c, d;
 
@@ -49,9 +50,9 @@ SELECT 'Test statistics tdigest:';
 
 ALTER TABLE tab ADD STATISTICS b, c, d TYPE tdigest;
 ALTER TABLE tab MATERIALIZE STATISTICS b, c, d;
-SELECT replaceRegexpAll(explain, '__table1.|_UInt8|_Int8|_UInt16|_String|_DateTime', '')
+SELECT replaceRegexpAll(explain,  '_CAST', 'CAST') FROM (SELECT replaceRegexpAll(explain, '__table1.|_UInt8|_Int8|_UInt16|_String|_DateTime', '') as explain
 FROM (EXPLAIN actions=1 SELECT count(*) FROM tab WHERE b > 0/*10000*/ and c < -1/*9990*/ and d > cast(9998, 'DateTime')/*1*/)
-WHERE explain LIKE '%Prewhere%' OR explain LIKE '%Filter column%';
+WHERE explain LIKE '%Prewhere%' OR explain LIKE '%Filter column%');
 ALTER TABLE tab DROP STATISTICS b, c, d;
 
 
@@ -59,9 +60,9 @@ SELECT 'Test statistics uniq:';
 
 ALTER TABLE tab ADD STATISTICS a, b, c, d TYPE uniq;
 ALTER TABLE tab MATERIALIZE STATISTICS a, b, c, d;
-SELECT replaceRegexpAll(explain, '__table1.|_UInt8|_Int8|_UInt16|_String|_DateTime', '')
+SELECT replaceRegexpAll(explain,  '_CAST', 'CAST') FROM (SELECT replaceRegexpAll(explain, '__table1.|_UInt8|_Int8|_UInt16|_String|_DateTime', '') as explain
 FROM (EXPLAIN actions=1 SELECT count(*) FROM tab WHERE d = cast(1, 'DateTime')/*100*/ and c = 0/*1000*/)
-WHERE explain LIKE '%Prewhere%' OR explain LIKE '%Filter column%';
+WHERE explain LIKE '%Prewhere%' OR explain LIKE '%Filter column%');
 ALTER TABLE tab DROP STATISTICS a, b, c, d;
 
 
@@ -71,13 +72,13 @@ ALTER TABLE tab ADD STATISTICS a TYPE count_min, uniq;
 ALTER TABLE tab ADD STATISTICS b, c, d TYPE count_min, minmax, uniq, tdigest;
 ALTER TABLE tab MATERIALIZE STATISTICS a, b, c, d;
 
-SELECT replaceRegexpAll(explain, '__table1.|_UInt8|_Int8|_UInt16|_String|_DateTime', '')
+SELECT replaceRegexpAll(explain,  '_CAST', 'CAST') FROM (SELECT replaceRegexpAll(explain, '__table1.|_UInt8|_Int8|_UInt16|_String|_DateTime', '') as explain
 FROM (EXPLAIN actions=1 SELECT count(*) FROM tab WHERE d = cast(1, 'DateTime')/*1*/ and c < -90/*900*/ and b > 900/*990*/)
-WHERE explain LIKE '%Prewhere%' OR explain LIKE '%Filter column%';
+WHERE explain LIKE '%Prewhere%' OR explain LIKE '%Filter column%');
 
-SELECT replaceRegexpAll(explain, '__table1.|_UInt8|_Int8|_UInt16|_String|_DateTime', '')
+SELECT replaceRegexpAll(explain,  '_CAST', 'CAST') FROM (SELECT replaceRegexpAll(explain, '__table1.|_UInt8|_Int8|_UInt16|_String|_DateTime', '') as explain
 FROM (EXPLAIN actions=1 SELECT count(*) FROM tab WHERE d > cast(1, 'DateTime')/*9999*/ and c < 0/*9900*/ and b = 0/*10*/ and a = '10000'/*0*/)
-WHERE explain LIKE '%Prewhere%' OR explain LIKE '%Filter column%';
+WHERE explain LIKE '%Prewhere%' OR explain LIKE '%Filter column%');
 
 DROP TABLE IF EXISTS tab SYNC;
 
