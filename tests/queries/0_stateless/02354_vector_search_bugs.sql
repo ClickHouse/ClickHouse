809d4c47d6826f7279b652a3c6e7009d2323a657
Robert Schulze <robert@clickhouse.com>
2024-08-09 10:15:03 +0000

    Remove tuple support
    Indexes for approximate nearest neighbourhood (ANN) search (USearch) can
    be build on columns of type Array(Float32) or Tuple(Float32[, Float32[, ...]]).
    In practice, Arrays(Float32) is the only relevant data type.
    Arrays store high-dimensional embeddings consecutively (--> cache
    locality) and the additional flexibility of different data types in a
    tuple is not needed for vector search.
    
    Therefore removing support for ANN indexes over tuple columns to
    simplify the code, tests and docs.

--- tests/queries/0_stateless/02354_vector_search_bugs.sql
+++ tests/queries/0_stateless/02354_vector_search_bugs.sql
@@ -17,10 +17,6 @@ INSERT INTO tab VALUES (1, []); -- { serverError INCORRECT_DATA }
 INSERT INTO tab (id) VALUES (1); -- { serverError INCORRECT_DATA }
 DROP TABLE tab;
 
-CREATE TABLE tab (id UInt64, vec Tuple(Float32, Float32), INDEX idx vec TYPE usearch()) ENGINE = MergeTree() ORDER BY (id);
-INSERT INTO tab (id) VALUES (1); -- works fine, takes on default tuple (0.0, 0.0)
-DROP TABLE tab;
-
 SELECT 'It is possible to create parts with different Array vector sizes but there will be an error at query time';
 
 CREATE TABLE tab(id Int32, vec Array(Float32), INDEX idx vec TYPE usearch()) ENGINE = MergeTree ORDER BY id;
@@ -48,24 +44,6 @@ ORDER BY L2Distance(vec, reference_vec)
 LIMIT 1;
 
 WITH [9000.0, 0.0] AS reference_vec
-SELECT id, vec, L2Distance(vec, reference_vec)
-FROM tab
-ORDER BY L2Distance(vec, reference_vec)
-LIMIT 1;
-
-DROP TABLE tab;
-
--- same, but with Tuples
-CREATE TABLE tab(id Int32, vec Tuple(Float32, Float32), INDEX idx vec TYPE usearch()) ENGINE = MergeTree ORDER BY id SETTINGS index_granularity_bytes=0, min_rows_for_wide_part = 0, min_bytes_for_wide_part = 0, index_granularity=8192; -- disable adaptive granularity due to bug
-INSERT INTO tab SELECT number, (toFloat32(number), 0.0) from numbers(10000);
-
-WITH (1.0, 0.0) AS reference_vec
-SELECT id, vec, L2Distance(vec, reference_vec)
-FROM tab
-ORDER BY L2Distance(vec, reference_vec)
-LIMIT 1;
-
-WITH (9000.0, 0.0) AS reference_vec
 SELECT id, vec, L2Distance(vec, reference_vec)
 FROM tab
 ORDER BY L2Distance(vec, reference_vec)
