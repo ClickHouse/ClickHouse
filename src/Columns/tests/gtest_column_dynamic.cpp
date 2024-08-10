805a2e33bfedbdfc2393217e7b485761b1943e1d
avogar <avogar@clickhouse.com>
2024-08-10 00:47:43 +0000

    Fix unit tests

--- src/Columns/tests/gtest_column_dynamic.cpp
+++ src/Columns/tests/gtest_column_dynamic.cpp
@@ -7,7 +7,7 @@ using namespace DB;
 
 TEST(ColumnDynamic, CreateEmpty)
 {
-    auto column = ColumnDynamic::create(255);
+    auto column = ColumnDynamic::create(254);
     ASSERT_TRUE(column->empty());
     ASSERT_EQ(column->getVariantInfo().variant_type->getName(), "Variant(SharedVariant)");
     ASSERT_EQ(column->getVariantInfo().variant_names.size(), 1);
@@ -19,7 +19,7 @@ TEST(ColumnDynamic, CreateEmpty)
 
 TEST(ColumnDynamic, InsertDefault)
 {
-    auto column = ColumnDynamic::create(255);
+    auto column = ColumnDynamic::create(254);
     column->insertDefault();
     ASSERT_TRUE(column->size() == 1);
     ASSERT_EQ(column->getVariantInfo().variant_type->getName(), "Variant(SharedVariant)");
@@ -34,7 +34,7 @@ TEST(ColumnDynamic, InsertDefault)
 
 TEST(ColumnDynamic, InsertFields)
 {
-    auto column = ColumnDynamic::create(255);
+    auto column = ColumnDynamic::create(254);
     column->insert(Field(42));
     column->insert(Field(-42));
     column->insert(Field("str1"));
@@ -56,7 +56,7 @@ TEST(ColumnDynamic, InsertFields)
 
 ColumnDynamic::MutablePtr getDynamicWithManyVariants(size_t num_variants, Field tuple_element = Field(42))
 {
-    auto column = ColumnDynamic::create(255);
+    auto column = ColumnDynamic::create(254);
     for (size_t i = 0; i != num_variants; ++i)
     {
         Tuple tuple;
@@ -136,7 +136,7 @@ TEST(ColumnDynamic, InsertFieldsOverflow2)
 
 ColumnDynamic::MutablePtr getInsertFromColumn(size_t num = 1)
 {
-    auto column_from = ColumnDynamic::create(255);
+    auto column_from = ColumnDynamic::create(254);
     for (size_t i = 0; i != num; ++i)
     {
         column_from->insert(Field(42));
@@ -170,13 +170,13 @@ void checkInsertFrom(const ColumnDynamic::MutablePtr & column_from, ColumnDynami
 
 TEST(ColumnDynamic, InsertFrom1)
 {
-    auto column_to = ColumnDynamic::create(255);
+    auto column_to = ColumnDynamic::create(254);
     checkInsertFrom(getInsertFromColumn(), column_to, "Variant(Float64, Int8, SharedVariant, String)", {"Float64", "Int8", "SharedVariant", "String"}, {{"Float64", 0}, {"Int8", 1}, {"SharedVariant", 2}, {"String", 3}});
 }
 
 TEST(ColumnDynamic, InsertFrom2)
 {
-    auto column_to = ColumnDynamic::create(255);
+    auto column_to = ColumnDynamic::create(254);
     column_to->insert(Field(42));
     column_to->insert(Field(42.42));
     column_to->insert(Field("str"));
@@ -186,7 +186,7 @@ TEST(ColumnDynamic, InsertFrom2)
 
 TEST(ColumnDynamic, InsertFrom3)
 {
-    auto column_to = ColumnDynamic::create(255);
+    auto column_to = ColumnDynamic::create(254);
     column_to->insert(Field(42));
     column_to->insert(Field(42.42));
     column_to->insert(Field("str"));
@@ -197,7 +197,7 @@ TEST(ColumnDynamic, InsertFrom3)
 
 TEST(ColumnDynamic, InsertFromOverflow1)
 {
-    auto column_from = ColumnDynamic::create(255);
+    auto column_from = ColumnDynamic::create(254);
     column_from->insert(Field(42));
     column_from->insert(Field(42.42));
     column_from->insert(Field("str"));
@@ -227,7 +227,7 @@ TEST(ColumnDynamic, InsertFromOverflow1)
 
 TEST(ColumnDynamic, InsertFromOverflow2)
 {
-    auto column_from = ColumnDynamic::create(255);
+    auto column_from = ColumnDynamic::create(254);
     column_from->insert(Field(42));
     column_from->insert(Field(42.42));
 
@@ -251,7 +251,7 @@ TEST(ColumnDynamic, InsertFromOverflow3)
     column_from->insert(Field(42));
     column_from->insert(Field(42.42));
 
-    auto column_to = ColumnDynamic::create(255);
+    auto column_to = ColumnDynamic::create(254);
     column_to->insert(Field(41));
 
     column_to->insertFrom(*column_from, 0);
@@ -297,13 +297,13 @@ void checkInsertManyFrom(const ColumnDynamic::MutablePtr & column_from, ColumnDy
 
 TEST(ColumnDynamic, InsertManyFrom1)
 {
-    auto column_to = ColumnDynamic::create(255);
+    auto column_to = ColumnDynamic::create(254);
     checkInsertManyFrom(getInsertFromColumn(), column_to, "Variant(Float64, Int8, SharedVariant, String)", {"Float64", "Int8", "SharedVariant", "String"}, {{"Float64", 0}, {"Int8", 1}, {"SharedVariant", 2}, {"String", 3}});
 }
 
 TEST(ColumnDynamic, InsertManyFrom2)
 {
-    auto column_to = ColumnDynamic::create(255);
+    auto column_to = ColumnDynamic::create(254);
     column_to->insert(Field(42));
     column_to->insert(Field(42.42));
     column_to->insert(Field("str"));
@@ -313,7 +313,7 @@ TEST(ColumnDynamic, InsertManyFrom2)
 
 TEST(ColumnDynamic, InsertManyFrom3)
 {
-    auto column_to = ColumnDynamic::create(255);
+    auto column_to = ColumnDynamic::create(254);
     column_to->insert(Field(42));
     column_to->insert(Field(42.42));
     column_to->insert(Field("str"));
@@ -324,7 +324,7 @@ TEST(ColumnDynamic, InsertManyFrom3)
 
 TEST(ColumnDynamic, InsertManyFromOverflow1)
 {
-    auto column_from = ColumnDynamic::create(255);
+    auto column_from = ColumnDynamic::create(254);
     column_from->insert(Field(42));
     column_from->insert(Field(42.42));
     column_from->insert(Field("str"));
@@ -361,7 +361,7 @@ TEST(ColumnDynamic, InsertManyFromOverflow1)
 
 TEST(ColumnDynamic, InsertManyFromOverflow2)
 {
-    auto column_from = ColumnDynamic::create(255);
+    auto column_from = ColumnDynamic::create(254);
     column_from->insert(Field(42));
     column_from->insert(Field(42.42));
 
@@ -393,7 +393,7 @@ TEST(ColumnDynamic, InsertManyFromOverflow3)
     column_from->insert(Field(42));
     column_from->insert(Field(42.42));
 
-    auto column_to = ColumnDynamic::create(255);
+    auto column_to = ColumnDynamic::create(254);
     column_to->insert(Field(41));
 
     column_to->insertManyFrom(*column_from, 0, 2);
@@ -441,13 +441,13 @@ void checkInsertRangeFrom(const ColumnDynamic::MutablePtr & column_from, ColumnD
 
 TEST(ColumnDynamic, InsertRangeFrom1)
 {
-    auto column_to = ColumnDynamic::create(255);
+    auto column_to = ColumnDynamic::create(254);
     checkInsertRangeFrom(getInsertFromColumn(2), column_to, "Variant(Float64, Int8, SharedVariant, String)", {"Float64", "Int8", "SharedVariant", "String"}, {{"Float64", 0}, {"Int8", 1}, {"SharedVariant", 2}, {"String", 3}});
 }
 
 TEST(ColumnDynamic, InsertRangeFrom2)
 {
-    auto column_to = ColumnDynamic::create(255);
+    auto column_to = ColumnDynamic::create(254);
     column_to->insert(Field(42));
     column_to->insert(Field(42.42));
     column_to->insert(Field("str1"));
@@ -457,7 +457,7 @@ TEST(ColumnDynamic, InsertRangeFrom2)
 
 TEST(ColumnDynamic, InsertRangeFrom3)
 {
-    auto column_to = ColumnDynamic::create(255);
+    auto column_to = ColumnDynamic::create(254);
     column_to->insert(Field(42));
     column_to->insert(Field(42.42));
     column_to->insert(Field("str1"));
@@ -468,7 +468,7 @@ TEST(ColumnDynamic, InsertRangeFrom3)
 
 TEST(ColumnDynamic, InsertRangeFromOverflow1)
 {
-    auto column_from = ColumnDynamic::create(255);
+    auto column_from = ColumnDynamic::create(254);
     column_from->insert(Field(42));
     column_from->insert(Field(43));
     column_from->insert(Field(42.42));
@@ -494,7 +494,7 @@ TEST(ColumnDynamic, InsertRangeFromOverflow1)
 
 TEST(ColumnDynamic, InsertRangeFromOverflow2)
 {
-    auto column_from = ColumnDynamic::create(255);
+    auto column_from = ColumnDynamic::create(254);
     column_from->insert(Field(42));
     column_from->insert(Field(43));
     column_from->insert(Field(42.42));
@@ -516,7 +516,7 @@ TEST(ColumnDynamic, InsertRangeFromOverflow2)
 
 TEST(ColumnDynamic, InsertRangeFromOverflow3)
 {
-    auto column_from = ColumnDynamic::create(255);
+    auto column_from = ColumnDynamic::create(254);
     column_from->insert(Field(42));
     column_from->insert(Field(43));
     column_from->insert(Field(42.42));
@@ -539,7 +539,7 @@ TEST(ColumnDynamic, InsertRangeFromOverflow3)
 
 TEST(ColumnDynamic, InsertRangeFromOverflow4)
 {
-    auto column_from = ColumnDynamic::create(255);
+    auto column_from = ColumnDynamic::create(254);
     column_from->insert(Field(42));
     column_from->insert(Field(42.42));
     column_from->insert(Field("str"));
@@ -561,7 +561,7 @@ TEST(ColumnDynamic, InsertRangeFromOverflow4)
 
 TEST(ColumnDynamic, InsertRangeFromOverflow5)
 {
-    auto column_from = ColumnDynamic::create(255);
+    auto column_from = ColumnDynamic::create(254);
     column_from->insert(Field(42));
     column_from->insert(Field(43));
     column_from->insert(Field(42.42));
@@ -587,7 +587,7 @@ TEST(ColumnDynamic, InsertRangeFromOverflow5)
 
 TEST(ColumnDynamic, InsertRangeFromOverflow6)
 {
-    auto column_from = ColumnDynamic::create(255);
+    auto column_from = ColumnDynamic::create(254);
     column_from->insert(Field(42));
     column_from->insert(Field(43));
     column_from->insert(Field(44));
@@ -619,7 +619,7 @@ TEST(ColumnDynamic, InsertRangeFromOverflow6)
 
 TEST(ColumnDynamic, InsertRangeFromOverflow7)
 {
-    auto column_from = ColumnDynamic::create(3);
+    auto column_from = ColumnDynamic::create(2);
     column_from->insert(Field(42.42));
     column_from->insert(Field("str1"));
     column_from->insert(Field(42));
@@ -629,7 +629,7 @@ TEST(ColumnDynamic, InsertRangeFromOverflow7)
     column_from->insert(Field("str2"));
     column_from->insert(Field(Array({Field(42)})));
 
-    auto column_to = ColumnDynamic::create(255);
+    auto column_to = ColumnDynamic::create(254);
     column_to->insert(Field(42));
 
     column_to->insertRangeFrom(*column_from, 0, 8);
@@ -659,7 +659,7 @@ TEST(ColumnDynamic, InsertRangeFromOverflow7)
 
 TEST(ColumnDynamic, InsertRangeFromOverflow8)
 {
-    auto column_from = ColumnDynamic::create(3);
+    auto column_from = ColumnDynamic::create(2);
     column_from->insert(Field(42.42));
     column_from->insert(Field("str1"));
     column_from->insert(Field(42));
@@ -669,7 +669,7 @@ TEST(ColumnDynamic, InsertRangeFromOverflow8)
     column_from->insert(Field("str2"));
     column_from->insert(Field(Array({Field(42)})));
 
-    auto column_to = ColumnDynamic::create(3);
+    auto column_to = ColumnDynamic::create(2);
     column_to->insert(Field(42));
     column_from->insert(Field("str1"));
 
@@ -711,7 +711,7 @@ TEST(ColumnDynamic, InsertRangeFromOverflow9)
     column_from->insert(Field("str2"));
     column_from->insert(Field(Array({Field(42)})));
 
-    auto column_to = ColumnDynamic::create(3);
+    auto column_to = ColumnDynamic::create(2);
     column_to->insert(Field(42));
 
     column_to->insertRangeFrom(*column_from, 0, 9);
@@ -743,7 +743,7 @@ TEST(ColumnDynamic, InsertRangeFromOverflow9)
 
 TEST(ColumnDynamic, SerializeDeserializeFromArena1)
 {
-    auto column = ColumnDynamic::create(255);
+    auto column = ColumnDynamic::create(254);
     column->insert(Field(42));
     column->insert(Field(42.42));
     column->insert(Field("str"));
@@ -768,7 +768,7 @@ TEST(ColumnDynamic, SerializeDeserializeFromArena1)
 
 TEST(ColumnDynamic, SerializeDeserializeFromArena2)
 {
-    auto column_from = ColumnDynamic::create(255);
+    auto column_from = ColumnDynamic::create(254);
     column_from->insert(Field(42));
     column_from->insert(Field(42.42));
     column_from->insert(Field("str"));
@@ -781,7 +781,7 @@ TEST(ColumnDynamic, SerializeDeserializeFromArena2)
     column_from->serializeValueIntoArena(2, arena, pos);
     column_from->serializeValueIntoArena(3, arena, pos);
 
-    auto column_to = ColumnDynamic::create(255);
+    auto column_to = ColumnDynamic::create(254);
     pos = column_to->deserializeAndInsertFromArena(ref1.data);
     pos = column_to->deserializeAndInsertFromArena(pos);
     pos = column_to->deserializeAndInsertFromArena(pos);
@@ -800,7 +800,7 @@ TEST(ColumnDynamic, SerializeDeserializeFromArena2)
 
 TEST(ColumnDynamic, SerializeDeserializeFromArenaOverflow1)
 {
-    auto column_from = ColumnDynamic::create(255);
+    auto column_from = ColumnDynamic::create(254);
     column_from->insert(Field(42));
     column_from->insert(Field(42.42));
     column_from->insert(Field("str"));
@@ -846,7 +846,7 @@ TEST(ColumnDynamic, SerializeDeserializeFromArenaOverflow2)
     column_from->serializeValueIntoArena(3, arena, pos);
     column_from->serializeValueIntoArena(4, arena, pos);
 
-    auto column_to = ColumnDynamic::create(3);
+    auto column_to = ColumnDynamic::create(2);
     column_to->insert(Field(42.42));
     pos = column_to->deserializeAndInsertFromArena(ref1.data);
     pos = column_to->deserializeAndInsertFromArena(pos);
@@ -882,7 +882,7 @@ TEST(ColumnDynamic, skipSerializedInArena)
     auto ref4 = column_from->serializeValueIntoArena(3, arena, pos);
 
     const char * end = ref4.data + ref4.size;
-    auto column_to = ColumnDynamic::create(255);
+    auto column_to = ColumnDynamic::create(254);
     pos = column_to->skipSerializedInArena(ref1.data);
     pos = column_to->skipSerializedInArena(pos);
     pos = column_to->skipSerializedInArena(pos);
