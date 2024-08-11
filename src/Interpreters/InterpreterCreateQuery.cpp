868a1e78f2d919ed7c581c9a74102887dbd204bf
Peter Nguyen <petern0408@gmail.com>
2024-08-11 11:47:30 -0600

    Add support for new create_if_not_exists setting in InterpreterCreateQuery.cpp

--- src/Interpreters/InterpreterCreateQuery.cpp
+++ src/Interpreters/InterpreterCreateQuery.cpp
@@ -1946,6 +1946,8 @@ BlockIO InterpreterCreateQuery::execute()
     FunctionNameNormalizer::visit(query_ptr.get());
     auto & create = query_ptr->as<ASTCreateQuery &>();
 
+    create.if_not_exists |= getContext()->getSettingsRef().create_if_not_exists;
+
     bool is_create_database = create.database && !create.table;
     if (!create.cluster.empty() && !maybeRemoveOnCluster(query_ptr, getContext()))
     {
