dcf96fa9f4363f7607e9b5ed82056d94c49a6ee3
Nikolai Kochetov <nk@clickhouse.com>
2024-08-13 17:57:06 +0200

    Update 03221_insert_timeout_overflow_mode.sh

--- tests/queries/0_stateless/03221_insert_timeout_overflow_mode.sh
+++ tests/queries/0_stateless/03221_insert_timeout_overflow_mode.sh
@@ -5,4 +5,4 @@ CUR_DIR=$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)
 . "$CUR_DIR"/../shell_config.sh
 
 ${CLICKHOUSE_CLIENT} --query "create table null_t (number UInt64) engine = Null;"
-${CLICKHOUSE_CLIENT} --query "select sleep(0.1) from system.numbers settings max_block_size = 1 format Native" 2>/dev/null | ${CLICKHOUSE_CLIENT} --max_execution_time = 0.3 --timeout_overflow_mode = 'break' --query "insert into null_t format Native" 2>&1 | grep -o "QUERY_WAS_CANCELLED"
+${CLICKHOUSE_CLIENT} --query "select sleep(0.1) from system.numbers settings max_block_size = 1 format Native" 2>/dev/null | ${CLICKHOUSE_CLIENT} --max_execution_time 0.3 --timeout_overflow_mode break --query "insert into null_t format Native" 2>&1 | grep -o "QUERY_WAS_CANCELLED"
