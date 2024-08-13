3ac15bf696ec401b5fbba65820c0b72c21e3eda0
Ilya Ageev <maniak89s@gmail.com>
2024-08-13 22:47:00 +0400

    Enable jwt-cpp in fasttest

--- tests/docker_scripts/fasttest_runner.sh
+++ tests/docker_scripts/fasttest_runner.sh
@@ -158,6 +158,7 @@ function clone_submodules
             contrib/libfiu
             contrib/incbin
             contrib/yaml-cpp
+            contrib/jwt-cpp
         )
 
         git submodule sync
