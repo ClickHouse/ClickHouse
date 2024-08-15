ca7b07c48eeb08c7e2b5465b2ccf4cfb61f2d312
Kruglov Pavel <48961922+Avogar@users.noreply.github.com>
2024-08-15 13:49:48 +0200

    Fix build

--- src/DataTypes/Serializations/SerializationVariantElement.cpp
+++ src/DataTypes/Serializations/SerializationVariantElement.cpp
@@ -153,21 +153,11 @@ void SerializationVariantElement::deserializeBinaryBulkWithMultipleStreams(
     nested_serialization->deserializeBinaryBulkWithMultipleStreams(variant_element_state->variant, variant_limit, settings, variant_element_state->variant_element_state, cache);
     removeVariantFromPath(settings.path);
 
-    /// If nothing was deserialized when variant_limit > 0
-    /// it means that we don't have a stream for such sub-column.
-    /// It may happen during ALTER MODIFY column with Variant extension.
-    /// In this case we should just insert default values.
-    if (variant_element_state->variant->empty())
-    {
-        mutable_column->insertManyDefaults(limit);
-        return;
-    }
-
     /// If there was nothing to deserialize or nothing was actually deserialized when variant_limit > 0, just insert defaults.
     /// The second case means that we don't have a stream for such sub-column. It may happen during ALTER MODIFY column with Variant extension.
     if (variant_limit == 0 || variant_element_state->variant->empty())
     {
-        mutable_column->insertManyDefaults(num_new_discriminators);
+        mutable_column->insertManyDefaults(limit);
         return;
     }
 
