5ed3c29d4a1e1992671af7168c0a1b01757d97bb
Alexey Milovidov <milovidov@clickhouse.com>
2024-08-19 01:18:12 +0200

    Update tests

--- tests/queries/0_stateless/01825_type_json_ghdata_insert_select.sh
+++ tests/queries/0_stateless/01825_type_json_ghdata_insert_select.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
-# Tags: no-fasttest, no-s3-storage
+# Tags: no-fasttest, no-s3-storage, long
 # ^ no-s3-storage: too memory hungry
 
 CUR_DIR=$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)
