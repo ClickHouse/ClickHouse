d5db40406b5015e238a728251138959b52c33e5b
Alexey Milovidov <milovidov@clickhouse.com>
2024-08-19 03:58:06 +0200

    Update SettingsChangesHistory.cpp

--- src/Core/SettingsChangesHistory.cpp
+++ src/Core/SettingsChangesHistory.cpp
@@ -71,6 +71,7 @@ static std::initializer_list<std::pair<ClickHouseVersion, SettingsChangesHistory
     },
     {"24.9",
         {
+            {"cast_keys_to_string_from_json", false, true, "Either to cast string to int while enumerating json or not."},
         }
     },
     {"24.8",
@@ -95,7 +96,6 @@ static std::initializer_list<std::pair<ClickHouseVersion, SettingsChangesHistory
             {"join_output_by_rowlist_perkey_rows_threshold", 0, 5, "The lower limit of per-key average rows in the right table to determine whether to output by row list in hash join."},
             {"allow_experimental_vector_similarity_index", false, false, "Added new setting to allow experimental vector similarity indexes"},
             {"input_format_try_infer_datetimes_only_datetime64", true, false, "Allow to infer DateTime instead of DateTime64 in data formats"},
-            {"cast_keys_to_string_from_json", false, true, "Either to cast string to int while enumerating json or not."}
         }
     },
     {"24.7",
