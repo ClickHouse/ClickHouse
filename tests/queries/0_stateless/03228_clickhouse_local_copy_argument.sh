065022fe60b92abe5689a920c4b79afc7d177a87
Denis Hananein <denis.hananein@pm.me>
2024-08-20 12:03:35 +0200

    Fix CURDIR env var

--- tests/queries/0_stateless/03228_clickhouse_local_copy_argument.sh
+++ tests/queries/0_stateless/03228_clickhouse_local_copy_argument.sh
@@ -4,4 +4,4 @@ CURDIR=$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)
 # shellcheck source=../shell_config.sh
 . "$CURDIR"/../shell_config.sh
 
-${CLICKHOUSE_LOCAL} --copy < $CUR_DIR/data_json/twitter.jsonl
+${CLICKHOUSE_LOCAL} --copy < $CURDIR/data_json/twitter.jsonl
