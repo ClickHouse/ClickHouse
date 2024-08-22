06c46ee75bcb94fe02ac68df6a4a044145804d76
Yarik Briukhovetskyi <114298166+yariks5s@users.noreply.github.com>
2024-08-22 18:56:50 +0200

    add one more test

--- tests/queries/0_stateless/03203_hive_style_partitioning.sh
+++ tests/queries/0_stateless/03203_hive_style_partitioning.sh
@@ -30,6 +30,12 @@ SELECT a FROM file('$CURDIR/data_hive/partitioning/a=b/a=b/sample.parquet') LIMI
 """
 
 $CLICKHOUSE_LOCAL -n -q """
+set use_hive_partitioning = 1;
+
+SELECT *, _column0 FROM file('$CURDIR/data_hive/partitioning/column0=Elizabeth/column0=Elizabeth1/sample.parquet') LIMIT 10;
+""" 2>&1 | grep -c "INCORRECT_DATA"
+
+$CLICKHOUSE_LOCAL -n -q """
 set use_hive_partitioning = 0;
 
 SELECT *, non_existing_column FROM file('$CURDIR/data_hive/partitioning/non_existing_column=Elizabeth/sample.parquet') LIMIT 10;
