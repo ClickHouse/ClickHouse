b16bc70e951ef6d09f6a987aa39710680d5c9fb4
Sema Checherinda <Sema.Checherinda@clickhouse.com>
2024-08-23 13:07:02 +0200

    cancel or finalize write buffer

--- src/IO/WriteBuffer.cpp
+++ src/IO/WriteBuffer.cpp
@@ -11,22 +11,17 @@ namespace DB
 WriteBuffer::~WriteBuffer()
 {
     // That destructor could be call with finalized=false in case of exceptions
-    if (count() > 0 && !finalized && !canceled)
+    if (!finalized && !canceled)
     {
-        /// It is totally OK to destroy instance without finalization when an exception occurs
-        /// However it is suspicious to destroy instance without finalization at the green path
-        if (!std::uncaught_exceptions() && std::current_exception() == nullptr)
-        {
-            LoggerPtr log = getLogger("WriteBuffer");
-            LOG_ERROR(
-                log,
-                "WriteBuffer is neither finalized nor canceled when destructor is called. "
-                "No exceptions in flight are detected. "
-                "The file might not be written at all or might be truncated. "
-                "Stack trace: {}",
-                StackTrace().toString());
-            chassert(false && "WriteBuffer is not finalized in destructor.");
-        }
+        LoggerPtr log = getLogger("WriteBuffer");
+        LOG_ERROR(
+            log,
+            "WriteBuffer is neither finalized nor canceled when destructor is called. "
+            "No exceptions in flight are detected. "
+            "The file might not be written at all or might be truncated. "
+            "Stack trace: {}",
+            StackTrace().toString());
+        chassert(false && "WriteBuffer is not finalized in destructor.");
     }
 }
 
