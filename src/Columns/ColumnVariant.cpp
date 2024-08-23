5d6b861ff055de0d04e0c574bf2ebb1e51215ace
avogar <avogar@clickhouse.com>
2024-08-23 13:49:36 +0000

    Fix index with limit=0

--- src/Columns/ColumnVariant.cpp
+++ src/Columns/ColumnVariant.cpp
@@ -953,7 +953,7 @@ ColumnPtr ColumnVariant::index(const IColumn & indexes, size_t limit) const
 {
     /// If we have only NULLs, index will take no effect, just return resized column.
     if (hasOnlyNulls())
-        return cloneResized(limit);
+        return cloneResized(limit == 0 ? indexes.size(): limit);
 
     /// Optimization when we have only one non empty variant and no NULLs.
     /// In this case local_discriminators column is filled with identical values and offsets column
