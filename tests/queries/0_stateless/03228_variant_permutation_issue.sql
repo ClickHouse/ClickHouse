6d69cd1f84d91767ead43e521cb2322bb78e8372
Kruglov Pavel <48961922+Avogar@users.noreply.github.com>
2024-08-27 14:53:42 +0200

    Update 03228_variant_permutation_issue.sql

--- tests/queries/0_stateless/03228_variant_permutation_issue.sql
+++ tests/queries/0_stateless/03228_variant_permutation_issue.sql
@@ -1,1 +1,9 @@
+SET allow_experimental_variant_type = 1;
 
+select 'test';
+drop table if exists test;
+create table test (id UInt32, data Variant(String), version UInt64) engine=ReplacingMergeTree(version) order by id;
+insert into test values (1, NULL, 1), (2, 'bar', 1), (3, 'bar', 1);
+insert into test select id, 'baz' as _data, version+1 as _version from test where id=2;
+select * from test final WHERE data is not null format Null;
+drop table test;
