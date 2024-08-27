14775bb6e6ba499c207aec8a5fcd8a793a753fce
Alexander Gololobov <davenger@clickhouse.com>
2024-08-27 15:20:58 +0200

    fix UT

--- src/Coordination/tests/gtest_coordination.cpp
+++ src/Coordination/tests/gtest_coordination.cpp
@@ -3112,7 +3112,8 @@ TYPED_TEST(CoordinationTest, TestFeatureFlags)
     feature_flags.setFeatureFlags(get_response.data);
     ASSERT_TRUE(feature_flags.isEnabled(KeeperFeatureFlag::FILTERED_LIST));
     ASSERT_TRUE(feature_flags.isEnabled(KeeperFeatureFlag::MULTI_READ));
-    ASSERT_FALSE(feature_flags.isEnabled(KeeperFeatureFlag::CHECK_NOT_EXISTS));
+    ASSERT_TRUE(feature_flags.isEnabled(KeeperFeatureFlag::CHECK_NOT_EXISTS));
+    ASSERT_TRUE(feature_flags.isEnabled(KeeperFeatureFlag::CREATE_IF_NOT_EXISTS));
 }
 
 TYPED_TEST(CoordinationTest, TestSystemNodeModify)
