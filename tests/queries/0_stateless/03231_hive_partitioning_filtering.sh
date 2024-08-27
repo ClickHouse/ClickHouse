60c6eb26100d61b0a1f642a0bf00293725d91408
Yarik Briukhovetskyi <114298166+yariks5s@users.noreply.github.com>
2024-08-27 19:42:47 +0200

    trying to fix the test

--- tests/queries/0_stateless/03231_hive_partitioning_filtering.sh
+++ tests/queries/0_stateless/03231_hive_partitioning_filtering.sh
@@ -5,29 +5,25 @@ CURDIR=$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)
 . "$CURDIR"/../shell_config.sh
 
 $CLICKHOUSE_LOCAL --query_id="test_03231_1" --query "
-    SET use_hive_partitioning = 1;
-"
-
-$CLICKHOUSE_LOCAL --query_id="test_03231_1" --query "
-    SELECT countDistinct(_path) FROM file('$CURDIR/data_hive/partitioning/column0=*/sample.parquet') WHERE column0 = 'Elizabeth';
+    SELECT countDistinct(_path) FROM file('$CURDIR/data_hive/partitioning/column0=*/sample.parquet') WHERE column0 = 'Elizabeth' SETTINGS use_hive_partitioning=1;
 "
 
 $CLICKHOUSE_LOCAL --query "
-    SELECT ProfileEvents['EngineFileLikeReadFiles'] FROM system.query_log WHERE query_id='test_03231_1' WHERE current_database = currentDatabase();
+    SELECT ProfileEvents['EngineFileLikeReadFiles'] FROM system.query_log WHERE query_id='test_03231_1' AND current_database = currentDatabase();
 "
 
 $CLICKHOUSE_LOCAL --query_id="test_03231_2" --query "
-    SELECT countDistinct(_path) FROM file('$CURDIR/data_hive/partitioning/identifier=*/email.csv') WHERE identifier = 2070;
+    SELECT countDistinct(_path) FROM file('$CURDIR/data_hive/partitioning/identifier=*/email.csv') WHERE identifier = 2070 SETTINGS use_hive_partitioning=1;
 "
 
 $CLICKHOUSE_LOCAL --query "
-    SELECT ProfileEvents['EngineFileLikeReadFiles'] FROM system.query_log WHERE query_id='test_03231_2' WHERE current_database = currentDatabase();
+    SELECT ProfileEvents['EngineFileLikeReadFiles'] FROM system.query_log WHERE query_id='test_03231_2' AND current_database = currentDatabase();
 "
 
 $CLICKHOUSE_LOCAL --query_id="test_03231_3" --query "
-    SELECT countDistinct(_path) FROM file('$CURDIR/data_hive/partitioning/array=*/sample.parquet') WHERE array = [1,2,3];
+    SELECT countDistinct(_path) FROM file('$CURDIR/data_hive/partitioning/array=*/sample.parquet') WHERE array = [1,2,3] SETTINGS use_hive_partitioning=1;
 "
 
 $CLICKHOUSE_LOCAL --query "
-    SELECT ProfileEvents['EngineFileLikeReadFiles'] FROM system.query_log WHERE query_id='test_03231_3' WHERE current_database = currentDatabase();
+    SELECT ProfileEvents['EngineFileLikeReadFiles'] FROM system.query_log WHERE query_id='test_03231_3' AND current_database = currentDatabase();
 "
