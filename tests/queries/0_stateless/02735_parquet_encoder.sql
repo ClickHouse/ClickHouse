8a923d2b21e9becf39656da161e8778411d4dccd
Nikita Mikhaylov <mikhaylovnikitka@gmail.com>
2024-08-28 09:58:50 +0200

    Update 02735_parquet_encoder.sql
    Co-authored-by: János Benjamin Antal <antaljanosbenjamin@users.noreply.github.com>

--- tests/queries/0_stateless/02735_parquet_encoder.sql
+++ tests/queries/0_stateless/02735_parquet_encoder.sql
@@ -1,4 +1,4 @@
--- Tags: no-fasttest, no-parallel, no-tsan
+-- Tags: no-fasttest, no-parallel, no-tsan, no-msan
 
 set output_format_parquet_use_custom_encoder = 1;
 set output_format_parquet_row_group_size = 1000;
