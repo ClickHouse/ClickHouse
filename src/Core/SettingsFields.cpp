0815a85e2bece8e9b5d22e4217a9a505ff9a231a
Konstantin Bogdanov <thevar1able@users.noreply.github.com>
2024-08-28 16:15:10 +0200

    Lint

--- src/Core/SettingsFields.cpp
+++ src/Core/SettingsFields.cpp
@@ -237,7 +237,7 @@ SettingFieldMaxThreads & SettingFieldMaxThreads::operator=(const Field & f)
 String SettingFieldMaxThreads::toString() const
 {
     if (is_auto)
-        /// Removing quotes this will introduce incompatibility between replicas with different version.
+        /// Removing these quotes this will introduce incompatibility between replicas with different versions.
         return "'auto(" + ::DB::toString(value) + ")'";
     else
         return ::DB::toString(value);
