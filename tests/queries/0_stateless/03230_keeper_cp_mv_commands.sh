1aae191b810d30519cef1fd9784567ee03fd34e5
Mikhail Artemenko <mikhail.artemenko@clickhouse.com>
2024-08-29 15:26:42 +0000

    use db name in keeper path

--- tests/queries/0_stateless/03230_keeper_cp_mv_commands.sh
+++ tests/queries/0_stateless/03230_keeper_cp_mv_commands.sh
@@ -4,8 +4,7 @@ CUR_DIR=$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)
 # shellcheck source=../shell_config.sh
 . "$CUR_DIR"/../shell_config.sh
 
-path="/test-keeper-client-cp-mv-commands"
-
+path="/test-keeper-client-$CLICKHOUSE_DATABASE"
 $CLICKHOUSE_KEEPER_CLIENT -q "rm '$path'" >& /dev/null
 
 $CLICKHOUSE_KEEPER_CLIENT -q "create '$path' 'root'"
