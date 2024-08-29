7a879980d8e2deace80b084e8281488995ca23c4
Yarik Briukhovetskyi <114298166+yariks5s@users.noreply.github.com>
2024-08-29 18:25:11 +0200

    try to fix tests

--- tests/queries/0_stateless/03231_hive_partitioning_filtering.sh
+++ tests/queries/0_stateless/03231_hive_partitioning_filtering.sh
@@ -9,7 +9,7 @@ DATA_DIR=$USER_FILES_PATH/$CLICKHOUSE_TEST_UNIQUE_NAME
 mkdir -p $DATA_DIR
 cp -r $CURDIR/data_hive/ $DATA_DIR
 
-$CLICKHOUSE_CLIENT --query_id="test_03231_1" --query -nm "
+$CLICKHOUSE_CLIENT --query_id="test_03231_1" --query "
     SELECT countDistinct(_path) FROM file('$DATA_DIR/data_hive/partitioning/column0=*/sample.parquet') WHERE column0 = 'Elizabeth' SETTINGS use_hive_partitioning=1;
     SYSTEM FLUSH LOGS;
 "
@@ -18,7 +18,7 @@ $CLICKHOUSE_CLIENT --query "
     SELECT ProfileEvents['EngineFileLikeReadFiles'] FROM system.query_log WHERE query_id='test_03231_1' AND current_database = currentDatabase();
 "
 
-$CLICKHOUSE_CLIENT --query_id="test_03231_2" --query -nm "
+$CLICKHOUSE_CLIENT --query_id="test_03231_2" --query "
     SELECT countDistinct(_path) FROM file('$DATA_DIR/data_hive/partitioning/identifier=*/email.csv') WHERE identifier = 2070 SETTINGS use_hive_partitioning=1;
     SYSTEM FLUSH LOGS;
 "
@@ -27,7 +27,7 @@ $CLICKHOUSE_CLIENT --query "
     SELECT ProfileEvents['EngineFileLikeReadFiles'] FROM system.query_log WHERE query_id='test_03231_2' AND current_database = currentDatabase();
 "
 
-$CLICKHOUSE_CLIENT --query_id="test_03231_3" --query -nm "
+$CLICKHOUSE_CLIENT --query_id="test_03231_3" --query "
     SELECT countDistinct(_path) FROM file('$DATA_DIR/data_hive/partitioning/array=*/sample.parquet') WHERE array = [1,2,3] SETTINGS use_hive_partitioning=1;
     SYSTEM FLUSH LOGS;
 "
