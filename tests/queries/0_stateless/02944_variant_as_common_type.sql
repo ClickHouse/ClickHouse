91b345714dac713f115525df48d3872c26868002
Alejandro <alejandro@tinybird.co>
2024-08-30 13:16:31 +0100

    Revert enabling the legacy analyzer

--- tests/queries/0_stateless/02944_variant_as_common_type.sql
+++ tests/queries/0_stateless/02944_variant_as_common_type.sql
@@ -1,6 +1,5 @@
 set allow_experimental_variant_type=1;
 set use_variant_as_common_type=1;
-set allow_experimental_analyzer=0;
 
 select toTypeName(res), if(1, [1,2,3], 'str_1') as res;
 select toTypeName(res), if(1, [1,2,3], 'str_1'::Nullable(String)) as res;
