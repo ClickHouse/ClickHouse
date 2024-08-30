8ed128792d623d137efe5708908d5eead776a34f
Alejandro <alejandro@tinybird.co>
2024-08-30 08:59:40 +0100

    remove comment

--- tests/queries/0_stateless/02944_variant_as_common_type.sql
+++ tests/queries/0_stateless/02944_variant_as_common_type.sql
@@ -1,7 +1,5 @@
 set allow_experimental_variant_type=1;
 set use_variant_as_common_type=1;
-
--- This test should be run with allow_experimental_analyzer=0, because it checks the behavior of the old analyzer.
 set allow_experimental_analyzer=0;
 
 select toTypeName(res), if(1, [1,2,3], 'str_1') as res;
