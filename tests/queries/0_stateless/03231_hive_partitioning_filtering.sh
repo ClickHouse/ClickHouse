8896d1b78b9a96cc6f5b8349eb869d8e1c8807f0
Yarik Briukhovetskyi <114298166+yariks5s@users.noreply.github.com>
2024-09-04 14:46:29 +0200

    try to fix tests

--- tests/queries/0_stateless/03231_hive_partitioning_filtering.sh
+++ tests/queries/0_stateless/03231_hive_partitioning_filtering.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
-# Tags: no-fasttest, no-parallel
+# Tags: no-fasttest
 
 CURDIR=$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)
 # shellcheck source=../shell_config.sh
@@ -9,30 +9,30 @@ DATA_DIR=$USER_FILES_PATH/$CLICKHOUSE_TEST_UNIQUE_NAME
 mkdir -p $DATA_DIR
 cp -r $CURDIR/data_hive/ $DATA_DIR
 
-$CLICKHOUSE_CLIENT --query_id="test_03231_1" --query "
+$CLICKHOUSE_CLIENT --query_id="test_03231_1_$CLICKHOUSE_TEST_UNIQUE_NAME" --query "
     SELECT countDistinct(_path) FROM file('$DATA_DIR/data_hive/partitioning/column0=*/sample.parquet') WHERE column0 = 'Elizabeth' SETTINGS use_hive_partitioning=1;
     SYSTEM FLUSH LOGS;
 "
 
 $CLICKHOUSE_CLIENT --query "
-    SELECT ProfileEvents['EngineFileLikeReadFiles'] FROM system.query_log WHERE query_id='test_03231_1' AND current_database = currentDatabase();
+    SELECT ProfileEvents['EngineFileLikeReadFiles'] FROM system.query_log WHERE query_id='test_03231_1' AND current_database = currentDatabase() and type='QueryFinish';
 "
 
-$CLICKHOUSE_CLIENT --query_id="test_03231_2" --query "
+$CLICKHOUSE_CLIENT --query_id="test_03231_2_$CLICKHOUSE_TEST_UNIQUE_NAME" --query "
     SELECT countDistinct(_path) FROM file('$DATA_DIR/data_hive/partitioning/identifier=*/email.csv') WHERE identifier = 2070 SETTINGS use_hive_partitioning=1;
     SYSTEM FLUSH LOGS;
 "
 
 $CLICKHOUSE_CLIENT --query "
-    SELECT ProfileEvents['EngineFileLikeReadFiles'] FROM system.query_log WHERE query_id='test_03231_2' AND current_database = currentDatabase();
+    SELECT ProfileEvents['EngineFileLikeReadFiles'] FROM system.query_log WHERE query_id='test_03231_2' AND current_database = currentDatabase() and type='QueryFinish';
 "
 
-$CLICKHOUSE_CLIENT --query_id="test_03231_3" --query "
+$CLICKHOUSE_CLIENT --query_id="test_03231_3_$CLICKHOUSE_TEST_UNIQUE_NAME" --query "
     SELECT countDistinct(_path) FROM file('$DATA_DIR/data_hive/partitioning/array=*/sample.parquet') WHERE array = [1,2,3] SETTINGS use_hive_partitioning=1;
     SYSTEM FLUSH LOGS;
 "
 
 $CLICKHOUSE_CLIENT --query "
-    SELECT ProfileEvents['EngineFileLikeReadFiles'] FROM system.query_log WHERE query_id='test_03231_3' AND current_database = currentDatabase();
+    SELECT ProfileEvents['EngineFileLikeReadFiles'] FROM system.query_log WHERE query_id='test_03231_3' AND current_database = currentDatabase() and type='QueryFinish';
 "
 rm -rf $DATA_DIR
