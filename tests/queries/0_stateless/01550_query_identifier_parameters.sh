7ee7388bec92cbafa93a5266b36430204803af69
Alexander Tokmakov <tavplubix@gmail.com>
2024-09-10 15:17:45 +0200

    restart the CI

--- tests/queries/0_stateless/01550_query_identifier_parameters.sh
+++ tests/queries/0_stateless/01550_query_identifier_parameters.sh
@@ -11,5 +11,6 @@ $CLICKHOUSE_CLIENT --param_col 'number' --query 'select a.{col:Identifier} from 
 $CLICKHOUSE_CLIENT --param_tbl 'numbers' --param_col 'number' --query 'select sum({tbl:Identifier}.{col:Identifier}) FROM (select * from system.{tbl:Identifier} limit 10) numbers'
 $CLICKHOUSE_CLIENT --param_alias 'numnum' --query 'select number as {alias:Identifier} from system.numbers limit 1 format CSVWithNames'
 $CLICKHOUSE_CLIENT --param_alias 'numnum' --param_tbl 'numbers' --query 'select number as {alias:Identifier} from system.{tbl:Identifier} limit 1 format CSVWithNames'
+
 # functions and grouping / ordering support
 $CLICKHOUSE_CLIENT --param_alias 'numnum' --query 'select number % 2 as {alias:Identifier}, count() from (select number from system.numbers limit 10) group by {alias:Identifier} order by {alias:Identifier} desc format CSVWithNames'
