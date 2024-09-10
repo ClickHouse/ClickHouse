9b517a939fce931da57a7d0604dc366ef35bf3fd
Yarik Briukhovetskyi <114298166+yariks5s@users.noreply.github.com>
2024-09-10 19:53:16 +0200

    Update 03236_squashing_high_memory.sql

--- tests/queries/0_stateless/03236_squashing_high_memory.sql
+++ tests/queries/0_stateless/03236_squashing_high_memory.sql
@@ -5,7 +5,7 @@ DROP TABLE IF EXISTS test_table;
 CREATE TABLE id_values ENGINE MergeTree ORDER BY id1 AS
     SELECT arrayJoin(range(500000)) AS id1, arrayJoin(range(1000)) AS id2;
 
-SET max_memory_usage = 1G;
+SET max_memory_usage = '1G';
 
 CREATE TABLE test_table ENGINE MergeTree ORDER BY id AS
 SELECT id_values.id1             AS id,
