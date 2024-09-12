0d6f40a875b53b21f66e9cedacf65a3443f935e6
Julia Kartseva <yulia.kartseva@gmail.com>
2024-09-12 01:23:14 +0000

    adjust 03036_udf_user_defined_directory_in_client test

--- tests/queries/0_stateless/03036_udf_user_defined_directory_in_client.sh
+++ tests/queries/0_stateless/03036_udf_user_defined_directory_in_client.sh
@@ -8,7 +8,7 @@ ${CLICKHOUSE_CLIENT} --query "DROP TABLE IF EXISTS test"
 ${CLICKHOUSE_CLIENT} --query "CREATE TABLE test (s String) ENGINE = Memory"
 
 # Calling an unknown function should not lead to creation of a 'user_defined' directory in the current directory
-${CLICKHOUSE_CLIENT} --query "INSERT INTO test VALUES (xyz('abc'))" 2>&1 | grep -o -F 'Unknown function'
+(( $(${CLICKHOUSE_CLIENT} --query "INSERT INTO test VALUES (xyz('abc'))" 2>&1 | grep -o -F -c 'Unknown function') >= 1 )) && echo "OK" || echo "FAIL"
 
 ls -ld user_defined 2> /dev/null
 
