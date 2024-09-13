59d1f9a6b1dacc0c2001d36dd4a9191a028a58aa
Michael Kolupaev <michael.kolupaev@clickhouse.com>
2024-09-13 20:04:28 +0000

    Fix test

--- tests/queries/0_stateless/03234_replicated_table_parent_znode_cleanup.sh
+++ tests/queries/0_stateless/03234_replicated_table_parent_znode_cleanup.sh
@@ -5,11 +5,11 @@ CURDIR=$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)
 
 db="rdb_$CLICKHOUSE_DATABASE"
 
-$CLICKHOUSE_CLIENT -nq "
+$CLICKHOUSE_CLIENT --distributed_ddl_output_mode=none -nq "
     create database $db engine=Replicated('/test/$CLICKHOUSE_DATABASE/rdb', 's1', 'r1');
     create table $db.a (x Int8) engine ReplicatedMergeTree order by x;"
 uuid=`$CLICKHOUSE_CLIENT -q "select uuid from system.tables where database = '$db' and name = 'a'"`
-$CLICKHOUSE_CLIENT -nq "
+$CLICKHOUSE_CLIENT --distributed_ddl_output_mode=none -nq "
     select count() from system.zookeeper where path = '/clickhouse/tables' and name = '$uuid';
     drop table $db.a sync;
     select count() from system.zookeeper where path = '/clickhouse/tables' and name = '$uuid';"
