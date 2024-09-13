991279e5c626067f9f371da5115e6993ed665ee6
Yarik Briukhovetskyi <114298166+yariks5s@users.noreply.github.com>
2024-09-13 19:23:00 +0200

    revert

--- src/Storages/VirtualColumnUtils.cpp
+++ src/Storages/VirtualColumnUtils.cpp
@@ -233,6 +233,8 @@ static void addFilterDataToVirtualColumns(Block & block, const String & path, si
         auto it = keys.find(virt_column.name);
         if (it != keys.end())
         {
+            if (!block.has(virt_column.name))
+                block.insert({virt_column.type->createColumn(), virt_column.type, virt_column.name});
             auto & column = block.getByName(virt_column.name).column;
             ReadBufferFromString buf(it->second);
             virt_column.type->getDefaultSerialization()->deserializeWholeText(column->assumeMutableRef(), buf, getFormatSettings(context));
