b0e4891b991b4ed008c1e6ca44b17758f0669a33
Nikolai Kochetov <nk@clickhouse.com>
2024-09-17 12:50:26 +0000

    Fixing tests.

--- tests/queries/0_stateless/02377_optimize_sorting_by_input_stream_properties_2.sql
+++ tests/queries/0_stateless/02377_optimize_sorting_by_input_stream_properties_2.sql
@@ -9,14 +9,13 @@ set optimize_aggregation_in_order=1;
 set enable_memory_bound_merging_of_aggregation_results=1;
 set prefer_localhost_replica=1;
 
+-- Nothing is working here :(
+
 select sum(y) as s from remote('127.0.0.{1,2}', currentDatabase(), tab) group by x order by x;
-select 'sort properties are reused after MergingAggregated';
 select replaceAll(trimLeft(explain), '__table1.', '') from (explain actions = 1, sorting=1, description=0 select sum(y) as s from remote('127.0.0.{1,2}', currentDatabase(), tab) group by x order by x) where explain ilike '%sort%' or explain like '%ReadFromMergeTree%' or explain like '%Aggregat%';
 
 select sum(y) as s from remote('127.0.0.{1,2}', currentDatabase(), tab) group by x order by x desc;
-select 'choosing of sort order is not working';
 select replaceAll(trimLeft(explain), '__table1.', '') from (explain actions = 1, sorting=1, description=0 select sum(y) as s from remote('127.0.0.{1,2}', currentDatabase(), tab) group by x order by x desc ) where explain ilike '%sort%' or explain like '%ReadFromMergeTree%' or explain like '%Aggregat%';
 
 select sum(y) as s from remote('127.0.0.{1,2}', currentDatabase(), tab) group by x order by x, s;
-select 'finish sorting is used';
 select replaceAll(trimLeft(explain), '__table1.', '') from (explain actions = 1, sorting=1, description=0 select sum(y) as s from remote('127.0.0.{1,2}', currentDatabase(), tab) group by x order by x, s) where explain ilike '%sort%' or explain like '%ReadFromMergeTree%' or explain like '%Aggregat%';
