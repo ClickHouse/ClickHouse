f768717be86d0198847783f10b4c9a8f662d78d5
Vitaly Baranov <vitlibar@clickhouse.com>
2024-09-17 13:05:02 +0200

    Fix test.

--- tests/queries/0_stateless/03231_restore_user_with_existing_role.sh
+++ tests/queries/0_stateless/03231_restore_user_with_existing_role.sh
@@ -34,7 +34,7 @@ do_check()
         SHOW GRANTS FOR ${role_b};
     " | sed "${replacements}")
     local expected
-    expected=$'CREATE USER user_a DEFAULT ROLE role_b SETTINGS custom_x = 2\nGRANT role_b TO user_a\nCREATE ROLE role_b SETTINGS custom_x = 1'
+    expected=$'CREATE USER user_a IDENTIFIED WITH no_password DEFAULT ROLE role_b SETTINGS custom_x = 2\nGRANT role_b TO user_a\nCREATE ROLE role_b SETTINGS custom_x = 1'
     if [[ "${check_info}" != "${expected}" ]]; then
        echo "Assertion failed:"
        echo "\"${check_info}\""
