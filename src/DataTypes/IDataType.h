6d545acf303345e976eed4e8d2ca2edbeac6c871
zhanglistar <zhanglinuxstar@gmail.com>
2024-09-18 17:35:22 +0800

    revert useless code.

--- src/DataTypes/IDataType.h
+++ src/DataTypes/IDataType.h
@@ -540,7 +540,6 @@ inline bool isNullableOrLowCardinalityNullable(const DataTypePtr & data_type)
     return data_type->isNullable() || data_type->isLowCardinalityNullable();
 }
 
-class DataTypeNullable;
 
 template <typename DataType> constexpr bool IsDataTypeDecimal = false;
 template <typename DataType> constexpr bool IsDataTypeNumber = false;
@@ -548,11 +547,9 @@ template <typename DataType> constexpr bool IsDataTypeDateOrDateTime = false;
 template <typename DataType> constexpr bool IsDataTypeDate = false;
 template <typename DataType> constexpr bool IsDataTypeEnum = false;
 template <typename DataType> constexpr bool IsDataTypeStringOrFixedString = false;
-template <typename DataType> constexpr bool IsDataTypeNullable = false;
 
 template <typename DataType> constexpr bool IsDataTypeDecimalOrNumber = IsDataTypeDecimal<DataType> || IsDataTypeNumber<DataType>;
 
-template <> inline constexpr bool IsDataTypeNullable<DataTypeNullable> = true;
 
 template <is_decimal T>
 class DataTypeDecimal;
