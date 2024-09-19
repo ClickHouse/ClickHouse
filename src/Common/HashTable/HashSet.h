7521fd2dcb2050d9837e19b6374dfa0a11f5106e
Raúl Marín <git@rmr.ninja>
2024-09-19 18:17:04 +0200

    Remove unused buggy code

--- src/Common/HashTable/HashSet.h
+++ src/Common/HashTable/HashSet.h
@@ -55,26 +55,6 @@ public:
             if (!rhs.buf[i].isZero(*this))
                 this->insert(rhs.buf[i].getValue());
     }
-
-
-    void readAndMerge(DB::ReadBuffer & rb)
-    {
-        Cell::State::read(rb);
-
-        size_t new_size = 0;
-        DB::readVarUInt(new_size, rb);
-        if (new_size > 100'000'000'000)
-            throw DB::Exception(DB::ErrorCodes::TOO_LARGE_ARRAY_SIZE, "The size of serialized hash table is suspiciously large: {}", new_size);
-
-        this->resize(new_size);
-
-        for (size_t i = 0; i < new_size; ++i)
-        {
-            Cell x;
-            x.read(rb);
-            this->insert(x.getValue());
-        }
-    }
 };
 
 
