8a86cbf4fe6a76a0c9c67b9468ab2ce6f42e40fd
Mikhail f. Shiryaev <felixoid@clickhouse.com>
2024-09-23 14:35:05 +0200

    Multiple improvements of docs_check.py
    - Run the script as a current user, not root
    - Add `--no-pull-image` to use a local one
    - Simplify logs analyze

--- tests/ci/docs_check.py
+++ tests/ci/docs_check.py
@@ -1,14 +1,14 @@
 #!/usr/bin/env python3
 import argparse
 import logging
-import subprocess
+import os
 import sys
 from pathlib import Path
 
 from docker_images_helper import get_docker_image, pull_image
-from env_helper import REPO_COPY, TEMP_PATH, ROOT_DIR
+from env_helper import REPO_COPY, TEMP_PATH
 from pr_info import PRInfo
-from report import FAILURE, SUCCESS, JobReport, TestResult, TestResults
+from report import FAIL, FAILURE, OK, SUCCESS, JobReport, TestResult, TestResults
 from stopwatch import Stopwatch
 from tee_popen import TeePopen
 
@@ -27,6 +27,14 @@ def parse_args() -> argparse.Namespace:
         "--force",
         action="store_true",
         help="check the docs even if there no changes",
+    )
+    parser.add_argument("--pull-image", default=True, help=argparse.SUPPRESS)
+    parser.add_argument(
+        "--no-pull-image",
+        dest="pull_image",
+        action="store_false",
+        default=argparse.SUPPRESS,
+        help="do not pull docker image, use existing",
     )
     return parser.parse_args()
 
@@ -61,105 +69,84 @@ def main():
     elif args.force:
         logging.info("Check the docs because of force flag")
 
-    docker_image = pull_image(get_docker_image("clickhouse/docs-builder"))
+    docker_image = get_docker_image("clickhouse/docs-builder")
+    if args.pull_image:
+        docker_image = pull_image(docker_image)
 
-    test_output = temp_path / "docs_check_log"
+    test_output = temp_path / "docs_check"
     test_output.mkdir(parents=True, exist_ok=True)
 
-    docs_output = temp_path / "docs_output"
-    docs_output.mkdir(parents=True, exist_ok=True)
-
     cmd = (
-        f"docker run --cap-add=SYS_PTRACE -e GIT_DOCS_BRANCH={args.docs_branch} "
+        f"docker run --cap-add=SYS_PTRACE --user={os.geteuid()}:{os.getegid()} "
+        f"-e GIT_DOCS_BRANCH={args.docs_branch} "
         f"--volume={repo_path}:/ClickHouse --volume={test_output}:/output_path "
-        f"--volume={docs_output}:/opt/clickhouse-docs/build "
         f"{docker_image}"
     )
 
     run_log_path = test_output / "run.log"
     logging.info("Running command: '%s'", cmd)
 
-    with TeePopen(cmd, run_log_path) as process:
+    test_results = []  # type: TestResults
+
+    test_sw = Stopwatch()
+    with TeePopen(f"{cmd} --out-dir /output_path/build", run_log_path) as process:
         retcode = process.wait()
         if retcode == 0:
             logging.info("Run successfully")
-            status = SUCCESS
+            job_status = SUCCESS
+            build_status = OK
             description = "Docs check passed"
         else:
             description = "Docs check failed (non zero exit code)"
-            status = FAILURE
+            job_status = FAILURE
+            build_status = FAIL
             logging.info("Run failed")
 
-    html_test_cmd = (
-        f"docker run --rm --cap-add=SYS_PTRACE "
-        f"--volume={docs_output}:/test "
-        f"wjdp/htmltest:v0.17.0 -c /test/.htmltest.yml"
+    if build_status == OK:
+        with open(run_log_path, "r", encoding="utf-8") as lfd:
+            for line in lfd:
+                if "ERROR" in line:
+                    build_status = FAIL
+                    job_status = FAIL
+                    break
+
+    test_results.append(
+        TestResult("Docs build", build_status, test_sw.duration_seconds, [run_log_path])
     )
 
-    subprocess.check_call(f"sudo chown -R ubuntu:ubuntu {temp_path}", shell=True)
+    htmltest_log = test_output / "htmltest.log"
 
-    docs_path = Path(ROOT_DIR) / "docs"
-    html_test_path = docs_path / ".htmltest.yml"
-    html_test_logs_path = docs_output / "tmp" / ".htmltest" / "htmltest.log"
-    subprocess.check_call(f"cp {html_test_path} {docs_output}/", shell=True)
-
-    html_test_return_code = subprocess.call(html_test_cmd, shell=True)
-
-    test_results = []  # type: TestResults
-    if html_test_return_code != 0:
-        dirs_to_check = []
-        langs = ["en", "ru", "zh"]
-
-        # check only directories from this repository
-        for lang in langs:
-            lang_path = docs_path / lang
-            for dir in lang_path.iterdir():
-                if dir.is_dir():
-                    dirs_to_check.append(f"{lang_path.name}/{dir.name}")
-
-        with open(html_test_logs_path, "r", encoding="utf-8") as html_test_logs:
-            if not any(html_test_logs):
-                html_test_error = f"Empty htmltest logs, but return code is {html_test_return_code}"
-                logging.error(html_test_error)
-                description = html_test_error
-                status = FAILURE
-
-            for line in html_test_logs:
-                for dir in dirs_to_check:
-                    if dir in line:
-                        test_results.append(TestResult(line, "FAIL"))
-                        break
-
-    additional_files = []
-    if not any(test_output.iterdir()):
-        logging.error("No output files after docs check")
-        description = "No output files after docs check"
-        status = FAILURE
-    else:
-        for p in test_output.iterdir():
-            additional_files.append(p)
-            with open(p, "r", encoding="utf-8") as check_file:
-                for line in check_file:
-                    if "ERROR" in line:
-                        test_results.append(TestResult(line.split(":")[-1], "FAIL"))
-        if test_results:
-            status = FAILURE
-            description = "Found errors in docs"
-        elif status != FAILURE:
-            test_results.append(TestResult("No errors found", "OK"))
+    # FIXME: after all issues in htmltest will be fixed, consider the failure as a
+    # failed job
+    test_sw.reset()
+    with TeePopen(
+        f"{cmd} htmltest -c /ClickHouse/docs/.htmltest.yml /output_path/build",
+        htmltest_log,
+    ) as process:
+        retcode = process.wait()
+        if retcode == 0:
+            logging.info("Run successfully")
+            test_results.append(
+                TestResult("htmltest", OK, test_sw.duration_seconds, [htmltest_log])
+            )
         else:
-            test_results.append(TestResult("Non zero exit code", "FAIL"))
+            logging.info("Run failed")
+            test_results.append(
+                TestResult(
+                    "htmltest", "FLAKY", test_sw.duration_seconds, [htmltest_log]
+                )
+            )
 
     JobReport(
         description=description,
         test_results=test_results,
-        status=status,
+        status=job_status,
         start_time=stopwatch.start_time_str,
         duration=stopwatch.duration_seconds,
-        additional_files=additional_files,
+        additional_files=[],
     ).dump()
 
-    if status == FAILURE:
+    if job_status == FAILURE:
         sys.exit(1)
 
 
