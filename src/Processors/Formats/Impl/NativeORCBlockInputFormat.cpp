e100db3fd440413b26ec31fdeb4966d3a404cb79
Nikita Taranov <nikita.taranov@clickhouse.com>
2024-09-24 19:22:22 +0100

    avoid using uninitialized value with nullable

--- src/Processors/Formats/Impl/NativeORCBlockInputFormat.cpp
+++ src/Processors/Formats/Impl/NativeORCBlockInputFormat.cpp
@@ -16,13 +16,13 @@
 #    include <DataTypes/DataTypeIPv4andIPv6.h>
 #    include <DataTypes/DataTypeLowCardinality.h>
 #    include <DataTypes/DataTypeMap.h>
+#    include <DataTypes/DataTypeNested.h>
 #    include <DataTypes/DataTypeNullable.h>
 #    include <DataTypes/DataTypeString.h>
 #    include <DataTypes/DataTypeTuple.h>
 #    include <DataTypes/DataTypesDecimal.h>
 #    include <DataTypes/DataTypesNumber.h>
 #    include <DataTypes/NestedUtils.h>
-#    include <DataTypes/DataTypeNested.h>
 #    include <Formats/FormatFactory.h>
 #    include <Formats/SchemaInferenceUtils.h>
 #    include <Formats/insertNullAsDefaultIfNeeded.h>
@@ -31,10 +31,33 @@
 #    include <IO/copyData.h>
 #    include <Interpreters/castColumn.h>
 #    include <Storages/MergeTree/KeyCondition.h>
-#    include <boost/algorithm/string/case_conv.hpp>
+#    include <orc/MemoryPool.hh>
+#    include <Common/Allocator.h>
 #    include <Common/FieldVisitorsAccurateComparison.h>
+#    include <Common/MemorySanitizer.h>
+
 #    include "ArrowBufferedStreams.h"
 
+#    include <boost/algorithm/string/case_conv.hpp>
+
+namespace
+{
+
+class MemoryPool : public orc::MemoryPool
+{
+public:
+    char * malloc(uint64_t size) override
+    {
+        auto * ptr = ::malloc(size);
+        /// For nullable columns some of the values will not be initialized.
+        __msan_unpoison(ptr, size);
+        return static_cast<char *>(ptr);
+    }
+
+    void free(char * p) override { ::free(p); }
+};
+
+}
 
 namespace DB
 {
@@ -694,12 +717,17 @@ buildORCSearchArgument(const KeyCondition & key_condition, const Block & header,
 }
 
 static void getFileReader(
-    ReadBuffer & in, std::unique_ptr<orc::Reader> & file_reader, const FormatSettings & format_settings, std::atomic<int> & is_stopped)
+    ReadBuffer & in,
+    std::unique_ptr<orc::Reader> & file_reader,
+    orc::MemoryPool & pool,
+    const FormatSettings & format_settings,
+    std::atomic<int> & is_stopped)
 {
     if (is_stopped)
         return;
 
     orc::ReaderOptions options;
+    options.setMemoryPool(pool);
     auto input_stream = asORCInputStream(in, format_settings, is_stopped);
     file_reader = orc::createReader(std::move(input_stream), options);
 }
@@ -844,13 +872,16 @@ static void updateIncludeTypeIds(
 }
 
 NativeORCBlockInputFormat::NativeORCBlockInputFormat(ReadBuffer & in_, Block header_, const FormatSettings & format_settings_)
-    : IInputFormat(std::move(header_), &in_), format_settings(format_settings_), skip_stripes(format_settings.orc.skip_stripes)
+    : IInputFormat(std::move(header_), &in_)
+    , memory_pool(std::make_unique<MemoryPool>())
+    , format_settings(format_settings_)
+    , skip_stripes(format_settings.orc.skip_stripes)
 {
 }
 
 void NativeORCBlockInputFormat::prepareFileReader()
 {
-    getFileReader(*in, file_reader, format_settings, is_stopped);
+    getFileReader(*in, file_reader, *memory_pool, format_settings, is_stopped);
     if (is_stopped)
         return;
 
@@ -987,7 +1018,8 @@ NamesAndTypesList NativeORCSchemaReader::readSchema()
 {
     std::unique_ptr<orc::Reader> file_reader;
     std::atomic<int> is_stopped = 0;
-    getFileReader(in, file_reader, format_settings, is_stopped);
+    MemoryPool memory_pool;
+    getFileReader(in, file_reader, memory_pool, format_settings, is_stopped);
 
     const auto & schema = file_reader->getType();
     Block header;
