f8056f3a3b021e933cc0c66fea31d20068a2ca5b
Han Fei <hanfei19910905@gmail.com>
2024-09-24 15:19:06 +0000

    update tests

--- tests/queries/0_stateless/03237_create_table_select_as_with_recursive.sql
+++ tests/queries/0_stateless/03237_create_table_select_as_with_recursive.sql
@@ -1,3 +1,9 @@
 drop table if exists t;
-create view t AS (WITH RECURSIVE 42 as ttt SELECT ttt);
+create table t1 (a Int64, s DateTime('Asia/Istanbul')) Engine = MergeTree() ORDER BY a;
+create view t AS (
+    WITH RECURSIVE 42 as ttt,
+    toDate(s) as start_date,
+    _table as (select 1 as number union all select number + 1 from _table where number < 10)
+    SELECT a, ttt from t1 join _table on t1.a = _table.number and start_date = '2024-09-23'
+);
 drop table t;
