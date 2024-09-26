21a12b1f0933f629256877091e4dab43ca696dbe
Alexey Milovidov <milovidov@clickhouse.com>
2024-09-26 07:25:24 +0200

    More tests

--- tests/queries/0_stateless/03244_format_drop_function_on_cluster.sh
+++ tests/queries/0_stateless/03244_format_drop_function_on_cluster.sh
@@ -9,3 +9,8 @@ CREATE FUNCTION linear_equation AS (x, k, b) -> ((k * x) + b);
 
 DROP FUNCTION linear_equation ON CLUSTER test;
 " | ${CLICKHOUSE_FORMAT} --multiquery
+
+echo "CREATE FUNCTION linear_equation AS (x, k, b) -> ((k * x) + b); DROP FUNCTION linear_equation;" | $CLICKHOUSE_FORMAT -n
+echo "CREATE FUNCTION linear_equation AS (x, k, b) -> ((k * x) + b); DROP FUNCTION linear_equation ON CLUSTER test" | $CLICKHOUSE_FORMAT -n
+echo "CREATE FUNCTION linear_equation AS (x, k, b) -> ((k * x) + b); DROP FUNCTION linear_equation ON CLUSTER test;" | $CLICKHOUSE_FORMAT -n
+echo "CREATE FUNCTION linear_equation AS (x, k, b) -> ((k * x) + b); DROP FUNCTION linear_equation ON CLUSTER test; CREATE FUNCTION linear_equation AS (x, k, b) -> ((k * x) + b);" | $CLICKHOUSE_FORMAT -n
