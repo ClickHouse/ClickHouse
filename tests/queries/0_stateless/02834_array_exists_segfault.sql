6434da72499f612a48aa2b7c80dea1d5fafa3ca2
vdimir <vdimir@clickhouse.com>
2024-09-27 16:49:53 +0000

    up tests

--- tests/queries/0_stateless/02834_array_exists_segfault.sql
+++ tests/queries/0_stateless/02834_array_exists_segfault.sql
@@ -1,4 +1,6 @@
 DROP TABLE IF EXISTS 02834_t;
 CREATE TABLE 02834_t (id UInt64, arr Array(UInt64)) ENGINE = MergeTree ORDER BY id;
-WITH subquery AS (SELECT []) SELECT t.* FROM 02834_t AS t JOIN subquery ON arrayExists(x -> x = 1, t.arr); -- { serverError INVALID_JOIN_ON_EXPRESSION }
+WITH subquery AS (SELECT []) SELECT t.* FROM 02834_t AS t JOIN subquery ON arrayExists(x -> x = 1, t.arr);
+INSERT INTO 02834_t VALUES (1, [1]), (2, [2]), (3, [1, 3]);
+WITH subquery AS (SELECT []) SELECT t.* FROM 02834_t AS t JOIN subquery ON arrayExists(x -> x = 1, t.arr) ORDER BY t.id;
 DROP TABLE 02834_t;
