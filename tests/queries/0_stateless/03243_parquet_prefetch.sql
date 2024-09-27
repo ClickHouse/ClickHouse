b09d06e879c16752d1aa45cc32de8f2159876ae0
liuneng <1398775315@qq.com>
2024-09-27 11:42:48 +0800

    fix ut

--- tests/queries/0_stateless/03243_parquet_prefetch.sql
+++
@@ -1,5 +0,0 @@
--- Tags: no-fasttest, no-parallel
-
-insert into table function hdfs('hdfs://localhost:12222/test_03243.parquet', 'Parquet') select number as i from numbers(100000) settings output_format_parquet_row_group_size=10000,hdfs_truncate_on_insert=1;
-select max(i) from table function hdfs('hdfs://localhost:12222/test_03243.parquet', 'Parquet');
-
