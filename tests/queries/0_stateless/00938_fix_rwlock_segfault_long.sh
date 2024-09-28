e3599ce3319364d24d877514257cad70047f37bd
Alexey Milovidov <milovidov@clickhouse.com>
2024-09-28 19:18:32 +0200

    Update 00938_fix_rwlock_segfault_long.sh

--- tests/queries/0_stateless/00938_fix_rwlock_segfault_long.sh
+++ tests/queries/0_stateless/00938_fix_rwlock_segfault_long.sh
@@ -1,5 +1,8 @@
 #!/usr/bin/env bash
 # Tags: long, no-debug, no-parallel
+# No parallel: querying `system.tables` with a `Merge` table can throw an exception 
+# if a database under the `Merge` table has been dropped at the same time
+# if you query columns related to total table size in rows/bytes.
 
 # Test fix for issue #5066
 
