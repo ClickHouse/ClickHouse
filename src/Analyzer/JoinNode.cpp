03937ad3b9dc4fccf2c22cb7f49723a09697d015
vdimir <vdimir@clickhouse.com>
2024-09-30 13:00:16 +0000

    fix JoinNode::{updateTreeHashImpl,isEqualImpl,toASTImpl}

--- src/Analyzer/JoinNode.cpp
+++ src/Analyzer/JoinNode.cpp
@@ -47,7 +47,7 @@ ASTPtr JoinNode::toASTTableJoin() const
     {
         auto join_expression_ast = children[join_expression_child_index]->toAST();
 
-        if (children[join_expression_child_index]->getNodeType() == QueryTreeNodeType::LIST)
+        if (is_using_join_expression)
             join_ast->using_expression_list = std::move(join_expression_ast);
         else
             join_ast->on_expression = std::move(join_expression_ast);
@@ -84,7 +84,8 @@ void JoinNode::dumpTreeImpl(WriteBuffer & buffer, FormatState & format_state, si
 bool JoinNode::isEqualImpl(const IQueryTreeNode & rhs, CompareOptions) const
 {
     const auto & rhs_typed = assert_cast<const JoinNode &>(rhs);
-    return locality == rhs_typed.locality && strictness == rhs_typed.strictness && kind == rhs_typed.kind;
+    return locality == rhs_typed.locality && strictness == rhs_typed.strictness && kind == rhs_typed.kind &&
+        is_using_join_expression == rhs_typed.is_using_join_expression;
 }
 
 void JoinNode::updateTreeHashImpl(HashState & state, CompareOptions) const
@@ -92,6 +93,7 @@ void JoinNode::updateTreeHashImpl(HashState & state, CompareOptions) const
     state.update(locality);
     state.update(strictness);
     state.update(kind);
+    state.update(is_using_join_expression);
 }
 
 QueryTreeNodePtr JoinNode::cloneImpl() const
