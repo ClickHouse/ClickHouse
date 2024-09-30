0666792cbe50ba05e56bae080de2a65ff887c721
avogar <avogar@clickhouse.com>
2024-09-30 12:23:25 +0000

    Update test

--- tests/queries/0_stateless/03243_check_for_nullable_nothing_in_alter.sql
+++ tests/queries/0_stateless/03243_check_for_nullable_nothing_in_alter.sql
@@ -1,3 +1,6 @@
+drop table if exists src;
+drop table if exists dst;
+drop view if exists v;
 create table src (x Nullable(Int32)) engine=Memory;
 alter table src modify column x Nullable(Nothing); -- {serverError DATA_TYPE_CANNOT_BE_USED_IN_TABLES}
 create table dst (x Nullable(Int32)) engine=Memory;
