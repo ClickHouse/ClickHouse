e077e0c7fef593d933c8b0e5eb9a46d07056564e
Arthur Passos <arthur.ti@outlook.com>
2024-09-30 15:28:21 -0300

    tests for expiration time for each auth metho

--- tests/integration/test_user_valid_until/test.py
+++ tests/integration/test_user_valid_until/test.py
@@ -124,3 +124,41 @@ def test_restart(started_cluster):
     assert error in node.query_and_get_error("SELECT 1", user="user_restart")
 
     node.query("DROP USER IF EXISTS user_restart")
+
+
+def test_multiple_authentication_methods(started_cluster):
+    node.query("DROP USER IF EXISTS user_basic")
+
+    node.query(
+        "CREATE USER user_basic IDENTIFIED WITH plaintext_password BY 'no_expiration',"
+        "plaintext_password by 'not_expired' VALID UNTIL '06/11/2040', plaintext_password by 'expired' VALID UNTIL '06/11/2010',"
+        "plaintext_password by 'infinity' VALID UNTIL 'infinity'"
+    )
+
+    assert (
+            node.query("SHOW CREATE USER user_basic")
+            == "CREATE USER user_basic IDENTIFIED WITH plaintext_password, plaintext_password VALID UNTIL \\'2040-11-06 00:00:00\\', "
+               "plaintext_password VALID UNTIL \\'2010-11-06 00:00:00\\', plaintext_password\n"
+    )
+    assert node.query("SELECT 1", user="user_basic", password="no_expiration") == "1\n"
+    assert node.query("SELECT 1", user="user_basic", password="not_expired") == "1\n"
+    assert node.query("SELECT 1", user="user_basic", password="infinity") == "1\n"
+
+    error = "Authentication failed"
+    assert error in node.query_and_get_error("SELECT 1", user="user_basic", password="expired")
+
+    # Expire them all
+    node.query("ALTER USER user_basic VALID UNTIL '06/11/2010 08:03:20'")
+
+    assert (
+            node.query("SHOW CREATE USER user_basic")
+            == "CREATE USER user_basic IDENTIFIED WITH plaintext_password VALID UNTIL \\'2010-11-06 08:03:20\\',"
+               " plaintext_password VALID UNTIL \\'2010-11-06 08:03:20\\',"
+               " plaintext_password VALID UNTIL \\'2010-11-06 08:03:20\\',"
+               " plaintext_password VALID UNTIL \\'2010-11-06 08:03:20\\'\n"
+    )
+
+    assert error in node.query_and_get_error("SELECT 1", user="user_basic", password="no_expiration")
+    assert error in node.query_and_get_error("SELECT 1", user="user_basic", password="not_expired")
+    assert error in node.query_and_get_error("SELECT 1", user="user_basic", password="infinity")
+    assert error in node.query_and_get_error("SELECT 1", user="user_basic", password="expired")
