df18735416c53c3b42b2ec85e2dd13c7fa859bfc
Hayk Manukyan <pmi.manukyan@gmail.com>
2024-09-30 17:25:00 +0300

    fix

--- src/Processors/QueryPlan/LimitInRangeStep.cpp
+++ src/Processors/QueryPlan/LimitInRangeStep.cpp
@@ -16,6 +16,7 @@ static ITransformingStep::Traits getTraits()
         {
             .returns_single_stream = false,
             .preserves_number_of_streams = true,
+            .preserves_sorting = true,
         },
         {
             .preserves_number_of_rows = false,
