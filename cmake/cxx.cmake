b9b7a1091bc13dfc3c5fe7528432dbc5367958b0
Robert Schulze <robert@clickhouse.com>
2024-09-30 16:58:33 +0000

    (Re)-enable libcxx debug mode
    - We previously compiled libcxx with _LIBCPP_DEBUG=0. In old libcxx
      versions, this (surprisingly) enabled the basic debug mode [1].
    
    - In libcxx 15 (the version we are currently using), _LIBCPP_DEBUG=0
      does nothing [2], the replacement is _LIBCPP_ENABLE_DEBUG_MODE=1.
    
    - The debug mode is only enabled in Debug builds. Their docs say the
      extra check change complexity guarantees + the asserts crash which we
      don't want in Release builds. The debug mode detects issues like in
      [3]. Crashes look like this (for example)
    
      ```
      /data/ch/contrib/llvm-project/libcxx/include/__iterator/wrap_iter.h:99: assertion ::std::__libcpp_is_constant_evaluated() || (__get_const_db()->__dereferenceable(this)) failed: Attempted to increment a non-incrementable iteratorAborted (core dumped)
      ```
    
    - I had to mute some new clang-tidy warnings in places that deal with
      container iterators. They got heavier and copy-by-value now yields a
      warning, e.g.
    
         for (auto it : iterators) /// <-- warning
             [...]
    
    [1] https://releases.llvm.org/12.0.0/projects/libcxx/docs/DesignDocs/DebugMode.html
    [2] https://releases.llvm.org/15.0.0/projects/libcxx/docs/DesignDocs/DebugMode.html
    [3] https://github.com/llvm/llvm-project/blob/main/libcxx/test/support/container_debug_tests.h

--- cmake/cxx.cmake
+++ cmake/cxx.cmake
@@ -1,4 +1,21 @@
-set (CMAKE_CXX_FLAGS_DEBUG "${CMAKE_CXX_FLAGS_DEBUG} -D_LIBCPP_DEBUG=0") # More checks in debug build.
+if (CMAKE_BUILD_TYPE_UC STREQUAL "DEBUG")
+    # Enable libcxx debug mode:
+    # --> https://releases.llvm.org/15.0.0/projects/libcxx/docs/DesignDocs/DebugMode.html
+    # The docs say the debug mode violates complexity guarantees, so do this only for Debug builds.
+    set (CMAKE_CXX_FLAGS_DEBUG "${CMAKE_CXX_FLAGS_DEBUG} -D_LIBCPP_ENABLE_DEBUG_MODE=1")
+
+    # Also, enable randomization of unspecified behavior, e.g. the order of equal elements in std::sort.
+    # "Randomization" means that Debug and Release builds use different seeds.
+    set (CMAKE_CXX_FLAGS_DEBUG "${CMAKE_CXX_FLAGS_DEBUG} -D_LIBCPP_DEBUG_RANDOMIZE_UNSPECIFIED_STABILITY_SEED=12345678")
+
+    # Note: Libcxx also has provides extra assertions:
+    # --> https://releases.llvm.org/15.0.0/projects/libcxx/docs/UsingLibcxx.html#assertions-mode
+    # These look orthogonal to the debug mode but the debug mode imlicitly enables them:
+    # --> https://github.com/llvm/llvm-project/blob/release/15.x/libcxx/include/__assert#L29
+
+    # TODO Once we upgrade to LLVM 18+, reconsider all of the above as they introduced "hardening modes":
+    # https://libcxx.llvm.org/Hardening.html
+endif ()
 
 add_subdirectory(contrib/libcxxabi-cmake)
 add_subdirectory(contrib/libcxx-cmake)
