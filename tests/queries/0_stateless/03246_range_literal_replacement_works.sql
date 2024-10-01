0498cd30a761bc025f20d5ed4a78dcd9953b2e25
Yarik Briukhovetskyi <114298166+yariks5s@users.noreply.github.com>
2024-10-01 16:54:04 +0200

    Cleanup the table and add NOT EXISTS

--- tests/queries/0_stateless/03246_range_literal_replacement_works.sql
+++ tests/queries/0_stateless/03246_range_literal_replacement_works.sql
@@ -1,5 +1,7 @@
-CREATE TABLE 03246_range_literal_replacement_works (id UInt8) Engine=Memory;
+CREATE TABLE IF NOT EXISTS 03246_range_literal_replacement_works (id UInt8) Engine=Memory;
 
 INSERT INTO 03246_range_literal_replacement_works VALUES (1 BETWEEN 0 AND 2);
 
 SELECT * FROM 03246_range_literal_replacement_works;
+
+DROP TABLE IF EXISTS 03246_range_literal_replacement_works;
