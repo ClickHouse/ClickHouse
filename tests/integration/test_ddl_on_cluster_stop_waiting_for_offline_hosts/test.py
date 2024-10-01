860587c6c03624c35a5893e5d1be28349433ab23
Tuan Pham Anh <tuan.pham.anh@clickhouse.com>
2024-10-01 08:34:01 +0000

    Tidy the test file

--- tests/integration/test_ddl_on_cluster_stop_waiting_for_offline_hosts/test.py
+++ tests/integration/test_ddl_on_cluster_stop_waiting_for_offline_hosts/test.py
@@ -43,33 +43,29 @@ def test_stop_waiting_for_offline_hosts(started_cluster):
     )
     assert time.time() - start < timeout
 
-    start = time.time()
     node1.query(
         "CREATE TABLE test_table ON CLUSTER test_cluster (x Int) Engine=Memory",
         settings=settings,
     )
-    assert time.time() - start < timeout
 
     node4.stop()
 
     start = time.time()
-    with pytest.raises(Exception) as err:
-        node1.query(
-            "DROP TABLE IF EXISTS test_table ON CLUSTER test_cluster SYNC",
-            settings=settings,
-        )
-    assert "Return code: 159" in str(err.value)
+    assert "Code: 159. DB::Exception" in node1.query_and_get_error(
+        "DROP TABLE IF EXISTS test_table ON CLUSTER test_cluster SYNC",
+        settings=settings,
+    )
+
     assert time.time() - start >= timeout
 
     start = time.time()
-    with pytest.raises(Exception) as err:
-        node1.query(
-            "CREATE TABLE test_table ON CLUSTER test_cluster (x Int) Engine=Memory",
-            settings=settings,
-        )
-    assert "Return code: 159" in str(err.value)
+    assert "Code: 159. DB::Exception" in node1.query_and_get_error(
+        "CREATE TABLE test_table ON CLUSTER test_cluster (x Int) Engine=Memory",
+        settings=settings,
+    )
     assert time.time() - start >= timeout
 
+    # set `distributed_ddl_output_mode` = `throw_only_active``
     settings = {
         "distributed_ddl_task_timeout": timeout,
         "distributed_ddl_output_mode": "throw_only_active",
