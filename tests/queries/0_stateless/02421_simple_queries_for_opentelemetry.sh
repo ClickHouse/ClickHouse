e52446948a200aed74a8b2c5c1a38ff72029ea8a
Sharath K S <Sharath.S2@ibm.com>
2024-10-01 03:27:00 -0700

    Fixed review comments
    Removed config files changes

--- tests/queries/0_stateless/02421_simple_queries_for_opentelemetry.sh
+++ tests/queries/0_stateless/02421_simple_queries_for_opentelemetry.sh
@@ -44,17 +44,35 @@ ${CLICKHOUSE_CLIENT} -q "
     ;"
 }
 
-function check_query_settings()
-{
-    ${CLICKHOUSE_CLIENT} -q "
-        SYSTEM FLUSH LOGS;
-        SELECT attribute
-        FROM system.opentelemetry_span_log
-        WHERE finish_date >= yesterday()
-        AND operation_name = 'query'
-        AND attribute['clickhouse.query_id'] = '${1}'
-        FORMAT JSONEachRow;
-    "
+function check_query_settings() {
+  result=$(${CLICKHOUSE_CLIENT} -q "
+      SYSTEM FLUSH LOGS;
+      SELECT
+          attribute['min_compress_block_size'],
+          attribute['max_block_size'],
+          attribute['max_execution_time']
+      FROM system.opentelemetry_span_log
+      WHERE finish_date >= yesterday()
+      AND operation_name = 'query'
+      AND attribute['clickhouse.query_id'] = '${1}'
+      FORMAT JSONEachRow;
+  ")
+
+  local min_present="not found"
+  local max_present="not found"
+  local execution_time_present="not found"
+
+  if [[ $result == *"min_compress_block_size"* ]]; then
+      min_present="present"
+  fi
+  if [[ $result == *"max_block_size"* ]]; then
+      max_present="present"
+  fi
+  if [[ $result == *"max_execution_time"* ]]; then
+      execution_time_present="present"
+  fi
+
+  echo "{\"min_compress_block_size\":\"$min_present\",\"max_block_size\":\"$max_present\",\"max_execution_time\":\"$execution_time_present\"}"
 }
 
 #
@@ -85,8 +103,12 @@ check_query_span "$query_id"
 query_id=$(${CLICKHOUSE_CLIENT} -q "select generateUUIDv4()");
 execute_query $query_id 'select * from opentelemetry_test format Null'
 check_query_span $query_id
-check_query_settings
 
+# Test 5: A normal select query with a setting
+query_id=$(${CLICKHOUSE_CLIENT} -q "SELECT generateUUIDv4() SETTINGS max_execution_time=3600")
+execute_query "$query_id" 'SELECT * FROM opentelemetry_test FORMAT Null'
+check_query_span "$query_id"
+check_query_settings "$query_id" "max_execution_time"
 
 #
 # Tear down
