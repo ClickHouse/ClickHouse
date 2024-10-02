96d4fbf1b2f275f7f9f8a576b442a39b0c7746ed
kssenii <sumarokovakseniia@mail.ru>
2024-10-02 10:45:17 +0200

    Avoid `Cannot schedule a task` error when loading parts

--- src/Storages/MergeTree/PartMetadataManagerOrdinary.cpp
+++ src/Storages/MergeTree/PartMetadataManagerOrdinary.cpp
@@ -11,7 +11,11 @@ namespace DB
 std::unique_ptr<ReadBuffer> PartMetadataManagerOrdinary::read(const String & file_name) const
 {
     size_t file_size = part->getDataPartStorage().getFileSize(file_name);
-    auto res = part->getDataPartStorage().readFile(file_name, getReadSettings().adjustBufferSize(file_size), file_size, std::nullopt);
+    auto read_settings = getReadSettings().adjustBufferSize(file_size);
+    /// Default read method is pread_threadpool, but there is not much point in it here.
+    read_settings.local_fs_method = LocalFSReadMethod::pread;
+
+    auto res = part->getDataPartStorage().readFile(file_name, read_settings, file_size, std::nullopt);
 
     if (isCompressedFromFileName(file_name))
         return std::make_unique<CompressedReadBufferFromFile>(std::move(res));
