5b82ef90e51d728306d3c1aedbf1f7aed6dc8f88
Hayk Manukyan <pmi.manukyan@gmail.com>
2024-10-02 13:55:04 +0300

    Revert "fix"
    This reverts commit 0d9c78bbe126e77914156c75299337b326de3877.

--- tests/queries/0_stateless/03228_limit_in_range_base.sql
+++ tests/queries/0_stateless/03228_limit_in_range_base.sql
@@ -1,14 +1,14 @@
 -- set max_block_size = 30000;
-DROP TABLE IF EXISTS test_table_h;
+-- DROP TABLE test_table;
 -- DROP TABLE test_table_100k;
 
-CREATE TABLE test_table_h (
+CREATE TABLE test_table (
     id UInt64,
     data String
 ) ENGINE = MergeTree()
 ORDER BY id;
 
-INSERT INTO test_table_h
+INSERT INTO test_table
 SELECT number AS id, concat('data_', toString(number)) AS data
 FROM numbers(4);
 
@@ -25,22 +25,22 @@ FROM numbers(4);
 -- Only FROM expression is provided:
 SELECT '[FROM] Query 1 result:';
 
-SELECT * FROM test_table_h
+SELECT * FROM test_table
 LIMIT INRANGE FROM id = 0;
 
 SELECT '[FROM] Query 2 result:';
 
-SELECT * FROM test_table_h
+SELECT * FROM test_table
 LIMIT INRANGE FROM id = 3;
 
 SELECT '[FROM] Query 3 result:';
 
-SELECT * FROM test_table_h
+SELECT * FROM test_table
 LIMIT INRANGE FROM data LIKE 'data_2';
 
 SELECT '[FROM] Query 4 result:';
 
-SELECT * FROM test_table_h
+SELECT * FROM test_table
 LIMIT INRANGE FROM id = -1; 
 
 
@@ -50,22 +50,22 @@ SELECT '--------------------';
 -- Only TO expression is provided:
 SELECT '[TO] Query 1 result:';
 
-SELECT * FROM test_table_h
+SELECT * FROM test_table
 LIMIT INRANGE TO id = 0;
 
 SELECT '[TO] Query 2 result:';
 
-SELECT * FROM test_table_h
+SELECT * FROM test_table
 LIMIT INRANGE TO id = 3;
 
 SELECT '[TO] Query 3 result:';
 
-SELECT * FROM test_table_h
+SELECT * FROM test_table
 LIMIT INRANGE TO data LIKE 'data_2';
 
 -- SELECT '[TO] Query 4 result:';
 
--- SELECT * FROM test_table_h
+-- SELECT * FROM test_table
 -- LIMIT INRANGE TO id = -1; -- all data returned, exception thrown.
 
 
