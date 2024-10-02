d81115b3b2dbc3816965d7554cec0221dd1c4940
Mikhail f. Shiryaev <felixoid@clickhouse.com>
2024-10-02 17:53:16 +0200

    Use common functions, fix flaky tests

--- tests/integration/test_replicated_users/test.py
+++ tests/integration/test_replicated_users/test.py
@@ -1,13 +1,19 @@
 import inspect
 import time
 from dataclasses import dataclass
+from os import path as p
 
 import pytest
 
 from helpers.cluster import ClickHouseCluster
-from helpers.keeper_utils import get_active_zk_connections
+from helpers.keeper_utils import (
+    get_active_zk_connections,
+    replace_zookeeper_config,
+    reset_zookeeper_config,
+)
 from helpers.test_tools import TSV, assert_eq_with_retry
 
+default_zk_config = p.join(p.dirname(p.realpath(__file__)), "configs/zookeeper.xml")
 cluster = ClickHouseCluster(__file__, zookeeper_config_path="configs/zookeeper.xml")
 
 node1 = cluster.add_instance(
@@ -171,12 +177,6 @@ def test_rename_replicated(started_cluster, entity):
 
 # ReplicatedAccessStorage must be able to continue working after reloading ZooKeeper.
 def test_reload_zookeeper(started_cluster):
-    def replace_zookeeper_config(new_config):
-        node1.replace_config("/etc/clickhouse-server/conf.d/zookeeper.xml", new_config)
-        node2.replace_config("/etc/clickhouse-server/conf.d/zookeeper.xml", new_config)
-        node1.query("SYSTEM RELOAD CONFIG")
-        node2.query("SYSTEM RELOAD CONFIG")
-
     node1.query("CREATE USER u1")
     assert_eq_with_retry(
         node2, "SELECT name FROM system.users WHERE name ='u1'", "u1\n"
@@ -184,6 +184,7 @@ def test_reload_zookeeper(started_cluster):
 
     ## remove zoo2, zoo3 from configs
     replace_zookeeper_config(
+        (node1, node2),
         """
 <clickhouse>
     <zookeeper>
@@ -194,7 +195,7 @@ def test_reload_zookeeper(started_cluster):
         <session_timeout_ms>2000</session_timeout_ms>
     </zookeeper>
 </clickhouse>
-"""
+""",
     )
 
     ## config reloads, but can still work
@@ -222,6 +223,7 @@ def test_reload_zookeeper(started_cluster):
 
     ## set config to zoo2, server will be normal
     replace_zookeeper_config(
+        (node1, node2),
         """
 <clickhouse>
     <zookeeper>
@@ -232,7 +234,7 @@ def test_reload_zookeeper(started_cluster):
         <session_timeout_ms>2000</session_timeout_ms>
     </zookeeper>
 </clickhouse>
-"""
+""",
     )
 
     active_zk_connections = get_active_zk_connections(node1)
@@ -251,3 +253,9 @@ def test_reload_zookeeper(started_cluster):
     assert (
         len(active_zk_connections) == 1
     ), "Total connections to ZooKeeper not equal to 1, {}".format(active_zk_connections)
+
+    # Restore the test state
+    node1.query("DROP USER u1, u2, u3")
+    cluster.start_zookeeper_nodes(["zoo1", "zoo2", "zoo3"])
+    cluster.wait_zookeeper_nodes_to_start(["zoo1", "zoo2", "zoo3"])
+    reset_zookeeper_config((node1, node2), default_zk_config)
