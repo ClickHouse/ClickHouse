6f3cf49229f9b871000c51a6a40b96831b01e053
jsc0218 <jsc0218@gmail.com>
2024-10-03 17:44:35 +0000

    add test and change doc

--- tests/queries/0_stateless/03206_projection_merge_special_mergetree.sql
+++ tests/queries/0_stateless/03206_projection_merge_special_mergetree.sql
@@ -104,3 +104,36 @@ FROM system.projection_parts
 WHERE (database = currentDatabase()) AND (`table` = 'tp') AND (active = 1);
 
 DROP TABLE tp;
+
+
+-- test ignore option
+
+CREATE TABLE tp (
+    type Int32,
+    eventcnt UInt64,
+    PROJECTION p (select sum(eventcnt), type group by type)
+) engine = ReplacingMergeTree order by type
+SETTINGS deduplicate_merge_projection_mode = 'ignore';
+
+INSERT INTO tp SELECT number%3, 1 FROM numbers(3);
+INSERT INTO tp SELECT number%3, 2 FROM numbers(3);
+
+OPTIMIZE TABLE tp DEDUPLICATE;
+
+OPTIMIZE TABLE tp FINAL;
+
+SET optimize_use_projections = false, force_optimize_projection = false;
+
+SELECT sum(eventcnt) eventcnt, type
+FROM tp
+GROUP BY type
+ORDER BY eventcnt, type;
+
+SET optimize_use_projections = true, force_optimize_projection = true;
+
+SELECT sum(eventcnt) eventcnt, type
+FROM tp
+GROUP BY type
+ORDER By eventcnt, type;
+
+DROP TABLE tp;
