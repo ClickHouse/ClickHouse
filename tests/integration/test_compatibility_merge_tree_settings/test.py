1d4e70c9b331f8a28702da8e77dff5fabe67a375
Nikolai Kochetov <nk@clickhouse.com>
2024-10-03 13:32:20 +0000

    Add anotehr test.

--- tests/integration/test_compatibility_merge_tree_settings/test.py
+++ tests/integration/test_compatibility_merge_tree_settings/test.py
@@ -8,6 +8,7 @@ node = cluster.add_instance("node1", with_zookeeper=True)
 node_with_compatibility = cluster.add_instance(
     "node2", with_zookeeper=True, user_configs=["configs/compatibility.xml"]
 )
+node_with_compatibility_and_mt_setings = cluster.add_instance("node3", with_zookeeper=True, main_configs=["configs/mt_settings.xml"], user_configs=["configs/compatibility.xml"])
 
 
 @pytest.fixture(scope="module")
@@ -87,3 +88,13 @@ def test_check_projections_compatibility(started_cluster):
     )
     node_with_compatibility.query(alter_add_projection)
     node_with_compatibility.query("drop table tp;")
+
+
+def test_config_overrides_compatibility(started_cluster):
+    create_with_invalid_projection = """
+        CREATE TABLE tp (type Int32, eventcnt UInt64, PROJECTION p (select sum(eventcnt), type group by type))
+        engine = {} order by type;
+    """
+
+    assert "Projection is fully supported" in node_with_compatibility_and_mt_setings.query_and_get_error(create_with_invalid_projection.format("ReplacingMergeTree"))
+    assert "Projection is fully supported" in node_with_compatibility_and_mt_setings.query_and_get_error(create_with_invalid_projection.format("ReplicatedReplacingMergeTree('/tables/tp', '0')"))
