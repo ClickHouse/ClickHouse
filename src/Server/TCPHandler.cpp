7dab7ae9a40b81486178d842dcf9c91edaf4abaf
Sema Checherinda <Sema.Checherinda@clickhouse.com>
2024-10-03 16:23:57 +0200

    fix tests

--- src/Server/TCPHandler.cpp
+++ src/Server/TCPHandler.cpp
@@ -35,6 +35,7 @@
 #include <Poco/Net/NetException.h>
 #include <Poco/Net/SocketAddress.h>
 #include <Poco/Util/LayeredConfiguration.h>
+#include "Common/StackTrace.h"
 #include <Common/Exception.h>
 #include <Common/CurrentMetrics.h>
 #include <Common/CurrentThread.h>
@@ -598,11 +599,7 @@ void TCPHandler::runImpl()
             auto finish_or_cancel = [this]()
             {
                 if (state.cancellation_status == CancellationStatus::FULLY_CANCELLED)
-                {
                     state.io.onCancelOrConnectionLoss();
-                    state.cancelOut();
-                    state.reset();
-                }
                 else
                     state.io.onFinish();
             };
@@ -674,13 +671,23 @@ void TCPHandler::runImpl()
             log_query_duration();
 
             if (state.is_connection_closed)
+            {
+                state.cancelOut();
+                state.reset();
                 break;
+            }
 
-            if (!state.empty() && state.cancellation_status != CancellationStatus::FULLY_CANCELLED)
             {
                 std::lock_guard lock(out_mutex);
                 sendLogs();
                 sendEndOfStream();
+            }
+
+            if (state.empty() || state.cancellation_status == CancellationStatus::FULLY_CANCELLED)
+            {
+                state.cancelOut();
+                state.reset();
+                break;
             }
 
             /// QueryState should be cleared before QueryScope, since otherwise
@@ -1035,7 +1042,6 @@ void TCPHandler::processInsertQuery()
 
         if (state.cancellation_status == CancellationStatus::FULLY_CANCELLED)
         {
-            state.cancelOut();
             executor.cancel();
         }
         else
@@ -2494,6 +2500,9 @@ void TCPHandler::sendEndOfStream()
 {
     state.sent_all_data = true;
     state.io.setAllDataSent();
+
+    if (out->isCanceled())
+        return;
 
     writeVarUInt(Protocol::Server::EndOfStream, *out);
 
