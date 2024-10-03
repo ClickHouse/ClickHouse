86045e0f0913f4d746a721bc365d52b0efddebee
Tuan Pham Anh <tuan.pham.anh@clickhouse.com>
2024-10-03 07:09:41 +0000

    Tidy test_ddl_on_cluster_stop_waiting_for_offline_hosts

--- tests/integration/test_ddl_on_cluster_stop_waiting_for_offline_hosts/test.py
+++ tests/integration/test_ddl_on_cluster_stop_waiting_for_offline_hosts/test.py
@@ -1,5 +1,6 @@
-import pytest
 import time
+
+import pytest
 
 from helpers.cluster import ClickHouseCluster
 
@@ -18,7 +19,10 @@ node3 = cluster.add_instance(
     "node3", main_configs=["configs/remote_servers.xml"], with_zookeeper=True
 )
 node4 = cluster.add_instance(
-    "node4", main_configs=["configs/remote_servers.xml"], with_zookeeper=True
+    "node4",
+    main_configs=["configs/remote_servers.xml"],
+    with_zookeeper=True,
+    stay_alive=True,
 )
 
 
@@ -36,51 +40,49 @@ def test_stop_waiting_for_offline_hosts(started_cluster):
     timeout = 10
     settings = {"distributed_ddl_task_timeout": timeout}
 
-    start = time.time()
     node1.query(
         "DROP TABLE IF EXISTS test_table ON CLUSTER test_cluster SYNC",
         settings=settings,
     )
-    assert time.time() - start < timeout
 
     node1.query(
         "CREATE TABLE test_table ON CLUSTER test_cluster (x Int) Engine=Memory",
         settings=settings,
     )
 
-    node4.stop()
+    try:
+        node4.stop_clickhouse()
 
-    start = time.time()
-    assert "Code: 159. DB::Exception" in node1.query_and_get_error(
-        "DROP TABLE IF EXISTS test_table ON CLUSTER test_cluster SYNC",
-        settings=settings,
-    )
+        start = time.time()
+        assert "Code: 159. DB::Exception" in node1.query_and_get_error(
+            "DROP TABLE IF EXISTS test_table ON CLUSTER test_cluster SYNC",
+            settings=settings,
+        )
+        assert time.time() - start >= timeout
 
-    assert time.time() - start >= timeout
+        start = time.time()
+        assert "Code: 159. DB::Exception" in node1.query_and_get_error(
+            "CREATE TABLE test_table ON CLUSTER test_cluster (x Int) Engine=Memory",
+            settings=settings,
+        )
+        assert time.time() - start >= timeout
 
-    start = time.time()
-    assert "Code: 159. DB::Exception" in node1.query_and_get_error(
-        "CREATE TABLE test_table ON CLUSTER test_cluster (x Int) Engine=Memory",
-        settings=settings,
-    )
-    assert time.time() - start >= timeout
+        # set `distributed_ddl_output_mode` = `throw_only_active``
+        settings = {
+            "distributed_ddl_task_timeout": timeout,
+            "distributed_ddl_output_mode": "throw_only_active",
+        }
 
-    # set `distributed_ddl_output_mode` = `throw_only_active``
-    settings = {
-        "distributed_ddl_task_timeout": timeout,
-        "distributed_ddl_output_mode": "throw_only_active",
-    }
+        start = time.time()
+        node1.query(
+            "DROP TABLE IF EXISTS test_table ON CLUSTER test_cluster SYNC",
+            settings=settings,
+        )
 
-    start = time.time()
-    node1.query(
-        "DROP TABLE IF EXISTS test_table ON CLUSTER test_cluster SYNC",
-        settings=settings,
-    )
-    assert time.time() - start < timeout
-
-    start = time.time()
-    node1.query(
-        "CREATE TABLE test_table ON CLUSTER test_cluster (x Int) Engine=Memory",
-        settings=settings,
-    )
-    assert time.time() - start < timeout
+        start = time.time()
+        node1.query(
+            "CREATE TABLE test_table ON CLUSTER test_cluster (x Int) Engine=Memory",
+            settings=settings,
+        )
+    finally:
+        node4.start_clickhouse()
