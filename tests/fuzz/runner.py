08d098a2f486fab845fa46459b5e842132028ea4
Yakov Olkhovskiy <yakov@clickhouse.com>
2024-10-04 00:15:36 +0000

    fix

--- tests/fuzz/runner.py
+++ tests/fuzz/runner.py
@@ -58,12 +58,12 @@ def process_error(error: str):
 
 
 def kill_fuzzer(fuzzer: str):
-    p = subprocess.Popen(["ps", "-A"], stdout=subprocess.PIPE)
-    out, err = p.communicate()
-    for line in out.splitlines():
-        if fuzzer in line:
-            pid = int(line.split(None, 1)[0])
-            os.kill(pid, signal.SIGKILL)
+    with subprocess.Popen(["ps", "-A"], stdout=subprocess.PIPE) as p
+        out, _ = p.communicate()
+        for line in out.splitlines():
+            if fuzzer in line:
+                pid = int(line.split(None, 1)[0])
+                os.kill(pid, signal.SIGKILL)
 
 
 def run_fuzzer(fuzzer: str, timeout: int):
