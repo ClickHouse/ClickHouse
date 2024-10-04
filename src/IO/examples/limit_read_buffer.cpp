1e49313ed6e63c48ad1683807227eb8f208c9d0a
Sema Checherinda <Sema.Checherinda@clickhouse.com>
2024-10-04 10:46:56 +0200

    fix build

--- src/IO/examples/limit_read_buffer.cpp
+++ src/IO/examples/limit_read_buffer.cpp
@@ -24,13 +24,13 @@ int main(int argc, char ** argv)
 
     writeCString("--- first ---\n", out);
     {
-        LimitReadBuffer limit_in(in, limit, /* trow_exception */ false, /* exact_limit */ {});
+        LimitReadBuffer limit_in(in, {.read_no_more=limit});
         copyData(limit_in, out);
     }
 
     writeCString("\n--- second ---\n", out);
     {
-        LimitReadBuffer limit_in(in, limit, /* trow_exception */ false, /* exact_limit */ {});
+        LimitReadBuffer limit_in( in,{.read_no_more=limit});
         copyData(limit_in, out);
     }
 
