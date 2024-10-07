a87f09f118e8deadc13bdb4a41daab5404581df2
Amos Bird <amosbird@gmail.com>
2024-10-07 19:31:31 +0800

    Address review

--- tests/queries/0_stateless/03240_insert_select_named_tuple.sql
+++ tests/queries/0_stateless/03240_insert_select_named_tuple.sql
@@ -1,3 +1,5 @@
+SET enable_named_columns_in_function_tuple = 1;
+
 DROP TABLE IF EXISTS src;
 DROP TABLE IF EXISTS dst;
 
@@ -10,3 +12,10 @@ SELECT * FROM dst;
 
 DROP TABLE src;
 DROP TABLE dst;
+
+DROP TABLE IF EXISTS src;
+CREATE TABLE src (id UInt32, type String, data String) ENGINE=MergeTree ORDER BY tuple();
+INSERT INTO src VALUES (1, 'ok', 'data');
+SELECT id, tuple(replaceAll(data, 'a', 'e') AS col_a, type) AS a, tuple(replaceAll(data, 'a', 'e') AS col_b, type) AS b FROM cluster(test_cluster_two_shards, currentDatabase(), src) SETTINGS prefer_localhost_replica=0 FORMAT JSONEachRow;
+
+DROP TABLE src;
