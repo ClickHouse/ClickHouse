5ecd654ec0466946cec40bf53c2735e92d33ebe3
David Tsukernik <david.tsukernik@sentry.io>
2024-10-08 04:12:14 +0000

    Fix tests

--- tests/queries/0_stateless/03250_client_default_bool_cli_settings_with_no_value_to_true.sh
+++ tests/queries/0_stateless/03250_client_default_bool_cli_settings_with_no_value_to_true.sh
@@ -5,4 +5,5 @@ CUR_DIR=$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)
 # shellcheck source=../shell_config.sh
 . "$CUR_DIR"/../shell_config.sh
 
-$CLICKHOUSE_CLIENT --optimize_aggregation_in_order --query "SELECT 1" 2>&1 | grep -q "1" && echo "OK" || echo "FAIL"
+$CLICKHOUSE_CLIENT --optimize_aggregation_in_order --query "SELECT value FROM system.settings WHERE name = 'optimize_aggregation_in_order'" 2>&1 | grep -q "1" && echo "OK" || echo "FAIL"
+$CLICKHOUSE_CLIENT --optimize_aggregation_in_order false --query "SELECT value FROM system.settings WHERE name = 'optimize_aggregation_in_order'" 2>&1 | grep -q "0" && echo "OK" || echo "FAIL"
