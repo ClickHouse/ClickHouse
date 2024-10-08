07da0c99b8318cd368c52a0d573e598599207196
avogar <avogar@clickhouse.com>
2024-10-08 05:52:25 +0000

    Fix tests

--- tests/queries/0_stateless/03248_string_to_json_alter_fuzz.sql
+++ tests/queries/0_stateless/03248_string_to_json_alter_fuzz.sql
@@ -7,8 +7,8 @@ drop named collection if exists json_alter_fuzzer;
 
 create table test (json String) engine=MergeTree order by tuple();
 create named collection json_alter_fuzzer AS json_str='{}';
-insert into test select * from fuzzJSON(json_alter_fuzzer, reuse_output=true, max_output_length=128) limit 200000;
-alter table test modify column json JSON settings mutations_sync=1;
+insert into test select * from fuzzJSON(json_alter_fuzzer, reuse_output=true, max_output_length=64) limit 200000;
+alter table test modify column json JSON(max_dynamic_paths=100) settings mutations_sync=1;
 select json from test format Null;
 optimize table test final;
 select json from test format Null;
