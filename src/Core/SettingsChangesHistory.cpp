35e6a6081f255317ee1cbf79547e4a34371b2e1a
vdimir <vdimir@clickhouse.com>
2024-10-09 13:42:33 +0000

    Add setting max_parts_to_move
    Signed-off-by: vdimir <vdimir@clickhouse.com>

--- src/Core/SettingsChangesHistory.cpp
+++ src/Core/SettingsChangesHistory.cpp
@@ -96,6 +96,7 @@ static std::initializer_list<std::pair<ClickHouseVersion, SettingsChangesHistory
             {"distributed_cache_read_alignment", 0, 0, "A setting for ClickHouse Cloud"},
             {"distributed_cache_max_unacked_inflight_packets", 10, 10, "A setting for ClickHouse Cloud"},
             {"distributed_cache_data_packet_ack_window", 5, 5, "A setting for ClickHouse Cloud"},
+            {"max_parts_to_move", 0, 1000, "New setting"},
         }
     },
     {"24.9",
