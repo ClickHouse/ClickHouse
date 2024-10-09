86909a84bb3b7fd858d43f5a54016351db66192d
János Benjamin Antal <benjamin.antal@clickhouse.com>
2024-10-09 03:23:15 +0000

    Use logging instead of print

--- tests/integration/test_disk_configuration/test.py
+++ tests/integration/test_disk_configuration/test.py
@@ -2,6 +2,7 @@ import pytest
 
 from helpers.client import QueryRuntimeException
 from helpers.cluster import ClickHouseCluster
+import logging
 
 cluster = ClickHouseCluster(__file__)
 
@@ -220,8 +221,8 @@ def test_merge_tree_custom_disk_setting(start_cluster):
     count2 = len(list2)
 
     if count1 != count2:
-        print("list1: ", list1)
-        print("list2: ", list2)
+        logging.info(f"list1: {list1}")
+        logging.info(f"list2: {list2}")
 
     assert count1 == count2
     assert (
