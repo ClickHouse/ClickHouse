8264417d1109dbfdb2d45871097aaf58ffe815f2
Azakarka <azakarka2016@gmail.com>
2024-10-09 18:01:44 +0000

    fix compilation errors

--- programs/server/Server.cpp
+++ programs/server/Server.cpp
@@ -2681,7 +2681,7 @@ void Server::createServers(
                 Poco::Net::ServerSocket socket;
                 auto address = socketBindListen(config, socket, listen_host, port, /* secure = */ true);
                 socket.setReceiveTimeout(Poco::Timespan());
-                socket.setSendTimeout(settings.send_timeout);
+                socket.setSendTimeout(settings[Setting::send_timeout]);
                 return ProtocolServerAdapter(
                     listen_host,
                     port_name,
