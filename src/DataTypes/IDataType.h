0410f38dece178fe90885f50a969e0cb4172e624
zhanglistar <zhanglinuxstar@gmail.com>
2024-10-10 17:09:16 +0800

    clean code

--- src/DataTypes/IDataType.h
+++ src/DataTypes/IDataType.h
@@ -583,11 +583,6 @@ class DataTypeEnum;
 
 template <typename T> inline constexpr bool IsDataTypeEnum<DataTypeEnum<T>> = true;
 
-//class DataTypeNullable;
-//template <typename T> inline constexpr bool IsDataTypeNullable = false;
-
-//template <> inline constexpr bool IsDataTypeNullable<DataTypeNullable> = true;
-
 #define FOR_BASIC_NUMERIC_TYPES(M) \
     M(UInt8) \
     M(UInt16) \
