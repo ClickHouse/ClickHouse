fcb1b37ec203b495988b0073d99f3859bda65edf
Konstantin Bogdanov <konstantin@clickhouse.com>
2024-10-10 02:39:49 +0200

    Fix

--- src/Interpreters/executeQuery.cpp
+++ src/Interpreters/executeQuery.cpp
@@ -1167,7 +1167,6 @@ static std::tuple<ASTPtr, BlockIO> executeQueryImpl(
                     auto timeout = settings[Setting::wait_for_async_insert_timeout].totalMilliseconds();
                     auto source = std::make_shared<WaitForAsyncInsertSource>(std::move(result.future), timeout);
                     res.pipeline = QueryPipeline(Pipe(std::move(source)));
-                    res.pipeline.complete(std::make_shared<NullSink>(Block()));
                 }
 
                 const auto & table_id = insert_query->table_id;
