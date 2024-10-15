6d9bbb2d66ce0783e6315e1a6431f0ba1c3f4c46
Peter Nguyen <petern0408@gmail.com>
2024-10-15 08:43:51 -0700

    Switch configs and update name of test function in test_regexp_logger

--- tests/integration/test_regexp_logger/test.py
+++ tests/integration/test_regexp_logger/test.py
@@ -9,12 +9,26 @@ node = cluster.add_instance(
     "node", with_zookeeper=False, main_configs=["configs/log.xml"]
 )
 
-config = """<clickhouse>
+updated_config = """
+<clickhouse>
     <logger>
         <level>trace</level>
         <log>/var/log/clickhouse-server/clickhouse-server.log</log>
+
+        <!-- Global: Don't log and Trace messages -->
+        <message_regexp_negative>.*Trace.*</message_regexp_negative>
+
+        <message_regexps>
+            <logger>
+                <!-- For the executeQuery logger, only log if message has "Read", but not "from" -->
+                <name>executeQuery</name>
+                <message_regexp>.*Read.*</message_regexp>
+                <message_regexp_negative>.*from.*</message_regexp_negative>
+            </logger>
+        </message_regexps>
     </logger>
-</clickhouse>"""
+</clickhouse>
+"""
 
 
 @pytest.fixture(scope="module")
@@ -33,17 +47,17 @@ def get_log(node):
     )
 
 
-def test_log_levels_update(start_cluster):
+def test_regexp_pattern_update(start_cluster):
     # Make sure that there are enough log messages for the test
     for _ in range(5):
         node.query("SELECT 1")
 
     log = get_log(node)
-    assert re.search("<Trace>", log)
-    assert re.search(".*executeQuery.*Read.*", log)
-    assert re.search(".*executeQuery.*from.*", log)
+    assert re.search(r"<Trace>", log)
+    assert re.search(r".*executeQuery.*Read.*", log)
+    assert re.search(r".*executeQuery.*from.*", log)
 
-    node.replace_config("/etc/clickhouse-server/config.d/log.xml", config)
+    node.replace_config("/etc/clickhouse-server/config.d/log.xml", updated_config)
     node.query("SYSTEM RELOAD CONFIG;")
     node.exec_in_container(
         ["bash", "-c", "> /var/log/clickhouse-server/clickhouse-server.log"]
@@ -55,6 +69,6 @@ def test_log_levels_update(start_cluster):
     log = get_log(node)
     assert len(log) > 0
 
-    assert not re.search("<Trace>", log)
-    assert re.search(".*executeQuery.*Read.*", log)
-    assert not re.search(".*executeQuery.*from.*", log)
+    assert not re.search(r"<Trace>", log)
+    assert re.search(r".*executeQuery.*Read.*", log)
+    assert not re.search(r".*executeQuery.*from.*", log)
