642eb7d140d3b8c969d9d8bf2bf1cfab729b3c4a
Yakov Olkhovskiy <yakov@clickhouse.com>
2024-10-17 13:23:23 +0000

    fix style

--- tests/ci/libfuzzer_test_check.py
+++ tests/ci/libfuzzer_test_check.py
@@ -28,7 +28,7 @@ def zipdir(path, ziph):
     for root, dirs, files in os.walk(path):
         for file in files:
             ziph.write(
-                os.path.join(root, file), 
+                os.path.join(root, file),
                 os.path.relpath(os.path.join(root, file), os.path.join(path, "..")),
             )
 
