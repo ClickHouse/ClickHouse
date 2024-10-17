50d14076d19e3007adb5a60e694122a39a29fb19
Vitaly Baranov <vitlibar@clickhouse.com>
2024-10-17 13:12:24 +0200

    Improve checking capability for batch delete from S3.

--- src/Disks/ObjectStorages/ObjectStorageFactory.cpp
+++ src/Disks/ObjectStorages/ObjectStorageFactory.cpp
@@ -158,21 +158,6 @@ S3::URI getS3URI(const Poco::Util::AbstractConfiguration & config, const std::st
     return uri;
 }
 
-void checkS3Capabilities(
-    S3ObjectStorage & storage, const S3Capabilities s3_capabilities, const String & name)
-{
-    /// If `support_batch_delete` is turned on (default), check and possibly switch it off.
-    if (s3_capabilities.support_batch_delete && !checkBatchRemove(storage))
-    {
-        LOG_WARNING(
-            getLogger("S3ObjectStorage"),
-            "Storage for disk {} does not support batch delete operations, "
-            "so `s3_capabilities.support_batch_delete` was automatically turned off during the access check. "
-            "To remove this message set `s3_capabilities.support_batch_delete` for the disk to `false`.",
-            name);
-        storage.setCapabilitiesSupportBatchDelete(false);
-    }
-}
 }
 
 static std::string getEndpoint(
@@ -192,7 +177,7 @@ void registerS3ObjectStorage(ObjectStorageFactory & factory)
         const Poco::Util::AbstractConfiguration & config,
         const std::string & config_prefix,
         const ContextPtr & context,
-        bool skip_access_check) -> ObjectStoragePtr
+        bool /* skip_access_check */) -> ObjectStoragePtr
     {
         auto uri = getS3URI(config, config_prefix, context);
         auto s3_capabilities = getCapabilitiesFromConfig(config, config_prefix);
@@ -203,10 +188,6 @@ void registerS3ObjectStorage(ObjectStorageFactory & factory)
 
         auto object_storage = createObjectStorage<S3ObjectStorage>(
             ObjectStorageType::S3, config, config_prefix, std::move(client), std::move(settings), uri, s3_capabilities, key_generator, name);
-
-        /// NOTE: should we still perform this check for clickhouse-disks?
-        if (!skip_access_check)
-            checkS3Capabilities(*dynamic_cast<S3ObjectStorage *>(object_storage.get()), s3_capabilities, name);
 
         return object_storage;
     });
@@ -221,7 +202,7 @@ void registerS3PlainObjectStorage(ObjectStorageFactory & factory)
         const Poco::Util::AbstractConfiguration & config,
         const std::string & config_prefix,
         const ContextPtr & context,
-        bool skip_access_check) -> ObjectStoragePtr
+        bool /* skip_access_check */) -> ObjectStoragePtr
     {
         /// send_metadata changes the filenames (includes revision), while
         /// s3_plain do not care about this, and expect that the file name
@@ -241,10 +222,6 @@ void registerS3PlainObjectStorage(ObjectStorageFactory & factory)
         auto object_storage = std::make_shared<PlainObjectStorage<S3ObjectStorage>>(
             std::move(client), std::move(settings), uri, s3_capabilities, key_generator, name);
 
-        /// NOTE: should we still perform this check for clickhouse-disks?
-        if (!skip_access_check)
-            checkS3Capabilities(*dynamic_cast<S3ObjectStorage *>(object_storage.get()), s3_capabilities, name);
-
         return object_storage;
     });
 }
@@ -259,7 +236,7 @@ void registerS3PlainRewritableObjectStorage(ObjectStorageFactory & factory)
            const Poco::Util::AbstractConfiguration & config,
            const std::string & config_prefix,
            const ContextPtr & context,
-           bool skip_access_check) -> ObjectStoragePtr
+           bool /* skip_access_check */) -> ObjectStoragePtr
         {
             /// send_metadata changes the filenames (includes revision), while
             /// s3_plain_rewritable does not support file renaming.
@@ -276,10 +253,6 @@ void registerS3PlainRewritableObjectStorage(ObjectStorageFactory & factory)
             auto metadata_storage_metrics = DB::MetadataStorageMetrics::create<S3ObjectStorage, MetadataStorageType::PlainRewritable>();
             auto object_storage = std::make_shared<PlainRewritableObjectStorage<S3ObjectStorage>>(
                 std::move(metadata_storage_metrics), std::move(client), std::move(settings), uri, s3_capabilities, key_generator, name);
-
-            /// NOTE: should we still perform this check for clickhouse-disks?
-            if (!skip_access_check)
-                checkS3Capabilities(*dynamic_cast<S3ObjectStorage *>(object_storage.get()), s3_capabilities, name);
 
             return object_storage;
         });
