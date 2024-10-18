d828adf2c53b5b055966b53ed603150f1173c398
Yarik Briukhovetskyi <114298166+yariks5s@users.noreply.github.com>
2024-10-18 16:20:20 +0000

    fix tests (stderr -> stdout)

--- tests/queries/0_stateless/03167_improvement_table_name_too_long.sh
+++ tests/queries/0_stateless/03167_improvement_table_name_too_long.sh
@@ -10,6 +10,6 @@ let excess_length=allowed_name_length+1
 long_table_name=$(openssl rand -base64 $excess_length | tr -dc A-Za-z | head -c $excess_length)
 allowed_table_name=$(openssl rand -base64 $allowed_name_length | tr -dc A-Za-z | head -c $allowed_name_length)
 
-$CLICKHOUSE_CLIENT -mn --query="CREATE TABLE $long_table_name (id UInt32, long_table_name String) Engine=MergeTree() order by id;" 2>&1 | grep -o -m "ARGUMENT_OUT_OF_BOUND"
+$CLICKHOUSE_CLIENT -mn --query="CREATE TABLE $long_table_name (id UInt32, long_table_name String) Engine=MergeTree() order by id;" 2>&1 | grep -o -m 1 "ARGUMENT_OUT_OF_BOUND"
 $CLICKHOUSE_CLIENT -mn --query="CREATE TABLE $allowed_table_name (id UInt32, allowed_table_name String) Engine=MergeTree() order by id;"
 $CLICKHOUSE_CLIENT -mn --query="DROP TABLE $allowed_table_name;"
