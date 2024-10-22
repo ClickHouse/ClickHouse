08a4310822e43efd6601760fbc00558c68a59c0a
Smita Kulkarni <Smita.Kulkarni@clickhouse.com>
2024-10-22 15:24:56 +0200

    Added back COMMON_SETTINGS_SUPPORTED_TYPES

--- src/Core/Settings.h
+++ src/Core/Settings.h
@@ -42,6 +42,65 @@ class ReadBuffer;
 struct SettingsImpl;
 class WriteBuffer;
 
+/// List of available types supported in Settings object (!= MergeTreeSettings, MySQLSettings, etc)
+#define COMMON_SETTINGS_SUPPORTED_TYPES(CLASS_NAME, M) \
+    M(CLASS_NAME, ArrowCompression) \
+    M(CLASS_NAME, Bool) \
+    M(CLASS_NAME, CapnProtoEnumComparingMode) \
+    M(CLASS_NAME, Char) \
+    M(CLASS_NAME, DateTimeInputFormat) \
+    M(CLASS_NAME, DateTimeOutputFormat) \
+    M(CLASS_NAME, DateTimeOverflowBehavior) \
+    M(CLASS_NAME, DefaultDatabaseEngine) \
+    M(CLASS_NAME, DefaultTableEngine) \
+    M(CLASS_NAME, Dialect) \
+    M(CLASS_NAME, DistributedCacheLogMode) /* Cloud only */ \
+    M(CLASS_NAME, DistributedCachePoolBehaviourOnLimit) /* Cloud only */ \
+    M(CLASS_NAME, DistributedDDLOutputMode) \
+    M(CLASS_NAME, DistributedProductMode) \
+    M(CLASS_NAME, Double) \
+    M(CLASS_NAME, EscapingRule) \
+    M(CLASS_NAME, Float) \
+    M(CLASS_NAME, IdentifierQuotingRule) \
+    M(CLASS_NAME, IdentifierQuotingStyle) \
+    M(CLASS_NAME, Int32) \
+    M(CLASS_NAME, Int64) \
+    M(CLASS_NAME, IntervalOutputFormat) \
+    M(CLASS_NAME, JoinAlgorithm) \
+    M(CLASS_NAME, JoinStrictness) \
+    M(CLASS_NAME, LightweightMutationProjectionMode) \
+    M(CLASS_NAME, LoadBalancing) \
+    M(CLASS_NAME, LocalFSReadMethod) \
+    M(CLASS_NAME, LogQueriesType) \
+    M(CLASS_NAME, LogsLevel) \
+    M(CLASS_NAME, Map) \
+    M(CLASS_NAME, MaxThreads) \
+    M(CLASS_NAME, Milliseconds) \
+    M(CLASS_NAME, MsgPackUUIDRepresentation) \
+    M(CLASS_NAME, MySQLDataTypesSupport) \
+    M(CLASS_NAME, NonZeroUInt64) \
+    M(CLASS_NAME, ORCCompression) \
+    M(CLASS_NAME, OverflowMode) \
+    M(CLASS_NAME, OverflowModeGroupBy) \
+    M(CLASS_NAME, ParallelReplicasMode) \
+    M(CLASS_NAME, ParallelReplicasCustomKeyFilterType) \
+    M(CLASS_NAME, ParquetCompression) \
+    M(CLASS_NAME, ParquetVersion) \
+    M(CLASS_NAME, QueryCacheNondeterministicFunctionHandling) \
+    M(CLASS_NAME, QueryCacheSystemTableHandling) \
+    M(CLASS_NAME, SchemaInferenceMode) \
+    M(CLASS_NAME, Seconds) \
+    M(CLASS_NAME, SetOperationMode) \
+    M(CLASS_NAME, ShortCircuitFunctionEvaluation) \
+    M(CLASS_NAME, SQLSecurityType) \
+    M(CLASS_NAME, StreamingHandleErrorMode) \
+    M(CLASS_NAME, String) \
+    M(CLASS_NAME, Timezone) \
+    M(CLASS_NAME, TotalsMode) \
+    M(CLASS_NAME, TransactionsWaitCSNMode) \
+    M(CLASS_NAME, UInt64) \
+    M(CLASS_NAME, UInt64Auto) \
+    M(CLASS_NAME, URI)
 
 COMMON_SETTINGS_SUPPORTED_TYPES(Settings, DECLARE_SETTING_TRAIT)
 struct Settings
