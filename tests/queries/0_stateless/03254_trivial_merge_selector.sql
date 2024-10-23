f6d2c24373be7f8a5241ee6f674f18250c1d23eb
Alexey Milovidov <milovidov@clickhouse.com>
2024-10-23 18:48:55 +0200

    Add a test

---
+++ tests/queries/0_stateless/03254_trivial_merge_selector.sql
@@ -0,0 +1,35 @@
+# This is a smoke test, it proves that the Trivial merge selector exists and does something.
+
+DROP TABLE IF EXISTS test;
+CREATE TABLE test (x UInt64) ENGINE = MergeTree ORDER BY x SETTINGS merge_selector_algorithm = 'Trivial';
+INSERT INTO test VALUES (1);
+SELECT x FROM test ORDER BY x;
+SELECT name FROM system.parts WHERE active AND table = 'test' AND database = currentDatabase();
+INSERT INTO test VALUES (2);
+SELECT x FROM test ORDER BY x;
+SELECT name FROM system.parts WHERE active AND table = 'test' AND database = currentDatabase();
+INSERT INTO test VALUES (3);
+SELECT x FROM test ORDER BY x;
+SELECT name FROM system.parts WHERE active AND table = 'test' AND database = currentDatabase();
+INSERT INTO test VALUES (4);
+SELECT x FROM test ORDER BY x;
+SELECT name FROM system.parts WHERE active AND table = 'test' AND database = currentDatabase();
+INSERT INTO test VALUES (5);
+SELECT x FROM test ORDER BY x;
+SELECT name FROM system.parts WHERE active AND table = 'test' AND database = currentDatabase();
+INSERT INTO test VALUES (6);
+SELECT x FROM test ORDER BY x;
+SELECT name FROM system.parts WHERE active AND table = 'test' AND database = currentDatabase();
+INSERT INTO test VALUES (7);
+SELECT x FROM test ORDER BY x;
+SELECT name FROM system.parts WHERE active AND table = 'test' AND database = currentDatabase();
+INSERT INTO test VALUES (8);
+SELECT x FROM test ORDER BY x;
+SELECT name FROM system.parts WHERE active AND table = 'test' AND database = currentDatabase();
+INSERT INTO test VALUES (9);
+SELECT x FROM test ORDER BY x;
+SELECT name FROM system.parts WHERE active AND table = 'test' AND database = currentDatabase();
+INSERT INTO test VALUES (10);
+SELECT x FROM test ORDER BY x;
+OPTIMIZE TABLE test;
+SELECT name FROM system.parts WHERE active AND table = 'test' AND database = currentDatabase();
