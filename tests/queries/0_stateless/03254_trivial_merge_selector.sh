22fd5ef6ba5f25371e9a1ff65bc107a668552a39
Alexey Milovidov <milovidov@clickhouse.com>
2024-10-24 03:52:36 +0200

    Update test

--- tests/queries/0_stateless/03254_trivial_merge_selector.sh
+++ tests/queries/0_stateless/03254_trivial_merge_selector.sh
@@ -1,5 +1,12 @@
+#!/usr/bin/env bash
+
+CUR_DIR=$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)
+# shellcheck source=../shell_config.sh
+. "$CUR_DIR"/../shell_config.sh
+
 # This is a smoke test, it proves that the Trivial merge selector exists and does something.
 
+${CLICKHOUSE_CLIENT} --query "
 DROP TABLE IF EXISTS test;
 CREATE TABLE test (x UInt64) ENGINE = MergeTree ORDER BY x SETTINGS merge_selector_algorithm = 'Trivial';
 INSERT INTO test VALUES (1);
@@ -32,5 +39,14 @@ SELECT name FROM system.parts WHERE active AND table = 'test' AND database = cur
 INSERT INTO test VALUES (10);
 SELECT x FROM test ORDER BY x;
 OPTIMIZE TABLE test;
-SELECT name FROM system.parts WHERE active AND table = 'test' AND database = currentDatabase();
+"
+
+while true
+do
+    ${CLICKHOUSE_CLIENT} --query "SELECT name FROM system.parts WHERE active AND table = 'test' AND database = currentDatabase() AND name = 'all_1_10_1'" | grep . && break
+    sleep 0.1
+done
+
+${CLICKHOUSE_CLIENT} --query "
 DROP TABLE test;
+"
