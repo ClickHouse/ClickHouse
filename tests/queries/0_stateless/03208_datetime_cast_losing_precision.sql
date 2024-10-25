e32a8a286657b38678bb8d96345d2385f470a715
Yarik Briukhovetskyi <114298166+yariks5s@users.noreply.github.com>
2024-10-25 13:19:49 +0200

    enhance tests

--- tests/queries/0_stateless/03208_datetime_cast_losing_precision.sql
+++ tests/queries/0_stateless/03208_datetime_cast_losing_precision.sql
@@ -1,2 +1,19 @@
 with toDateTime('2024-10-16 18:00:30') as t
-SELECT toDateTime64(t, 3) + interval 100 milliseconds IN (SELECT t);
+SELECT toDateTime64(t, 3) + interval 100 milliseconds IN (SELECT t) settings transform_null_in=0;
+
+with toDateTime('2024-10-16 18:00:30') as t
+SELECT toDateTime64(t, 3) + interval 100 milliseconds IN (SELECT t) settings transform_null_in=1;
+
+with toDateTime('1970-01-01 00:00:01') as t
+SELECT toDateTime64(t, 3) + interval 100 milliseconds IN (now(), Null) settings transform_null_in=1;
+
+with toDateTime('1970-01-01 00:00:01') as t
+SELECT toDateTime64(t, 3) + interval 100 milliseconds IN (now(), Null) settings transform_null_in=0;
+
+with toDateTime('1970-01-01 00:00:01') as t,
+    arrayJoin([Null, toDateTime64(t, 3) + interval 100 milliseconds]) as x
+SELECT x IN (now(), Null) settings transform_null_in=0;
+
+with toDateTime('1970-01-01 00:00:01') as t,
+    arrayJoin([Null, toDateTime64(t, 3) + interval 100 milliseconds]) as x
+SELECT x IN (now(), Null) settings transform_null_in=1;
