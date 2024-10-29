c801184c4fa72cc0d9c83b07d8d442de8ae92778
Amos Bird <amosbird@gmail.com>
2024-10-29 10:40:43 +0800

    Stabilize test

--- tests/queries/0_stateless/03257_reverse_sorting_key.sql
+++ tests/queries/0_stateless/03257_reverse_sorting_key.sql
@@ -8,6 +8,8 @@ create table x1 (i Nullable(int)) engine MergeTree order by i desc settings allo
 
 insert into x1 select * from numbers(100);
 
+optimize table x1 final;
+
 select * from x1 where i = 3;
 
 select count() from x1 where i between 3 and 10;
@@ -23,6 +25,8 @@ select * from x1 order by i limit 5;
 create table x2 (i Nullable(int), j Nullable(int)) engine MergeTree order by (i, j desc) settings allow_nullable_key = 1, index_granularity = 2, allow_experimental_reverse_key = 1;
 
 insert into x2 select number % 10, number + 1000 from numbers(100);
+
+optimize table x2 final;
 
 select * from x2 where j = 1003;
 
