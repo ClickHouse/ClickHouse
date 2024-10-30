bd9cfaecea93dc3b6d469f3898fcde9506ae5f9b
Amos Bird <amosbird@gmail.com>
2024-10-30 14:35:06 +0800

    No need to create tmp files

--- tests/queries/0_stateless/03260_compressor_stat.sh
+++ tests/queries/0_stateless/03260_compressor_stat.sh
@@ -4,10 +4,4 @@ CURDIR=$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)
 # shellcheck source=../shell_config.sh
 . "$CURDIR"/../shell_config.sh
 
-echo "Hello, World!" > 03260_test_data
-
-$CLICKHOUSE_COMPRESSOR --codec 'Delta' --codec 'LZ4' --input '03260_test_data' --output '03260_test_out'
-
-$CLICKHOUSE_COMPRESSOR --stat '03260_test_out'
-
-rm -f 03260_test_data 03260_test_out
+echo "Hello, World!" | $CLICKHOUSE_COMPRESSOR --codec 'Delta' --codec 'LZ4' | $CLICKHOUSE_COMPRESSOR --stat
