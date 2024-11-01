7691b7dd4435d1df5cd43cdf9169277aa9e81996
Christoph Wurm <christoph@clickhouse.com>
2024-11-01 11:06:49 +0000

    Fix test

--- tests/queries/0_stateless/03256_invalid_mutation_query.sql
+++ tests/queries/0_stateless/03256_invalid_mutation_query.sql
@@ -9,6 +9,8 @@ DELETE FROM t WHERE x IN (SELECT * FROM t2); -- { serverError 60 }
 ALTER TABLE t DELETE WHERE x in (SELECT y FROM t); -- { serverError 47 }
 ALTER TABLE t UPDATE x = 1 WHERE x IN (SELECT y FROM t); -- { serverError 47 }
 
+DELETE FROM t WHERE x IN (SELECT foo FROM bar) SETTINGS validate_mutation_query = 0;
+
 ALTER TABLE t ADD COLUMN y int;
 DELETE FROM t WHERE y in (SELECT y FROM t);
 
