321cf6ad7a320f5e8c5365d7f8cc0804c1ff90e3
Michael Kolupaev <michael.kolupaev@clickhouse.com>
2024-11-05 00:34:48 +0000

    Fix negate function monotonicity

--- src/Functions/negate.cpp
+++ src/Functions/negate.cpp
@@ -32,9 +32,30 @@ using FunctionNegate = FunctionUnaryArithmetic<NegateImpl, NameNegate, true>;
 template <> struct FunctionUnaryArithmeticMonotonicity<NameNegate>
 {
     static bool has() { return true; }
-    static IFunction::Monotonicity get(const Field &, const Field &)
+    static IFunction::Monotonicity get(const Field & left, const Field & right)
     {
-        return { .is_monotonic = true, .is_positive = false, .is_strict = true };
+        /// negate(UInt64) -> Int64:
+        ///  * monotonically decreases on [0, 2^63] (no overflow),
+        ///  * then jumps up from -2^63 to 2^63-1, then
+        ///  * monotonically decreases on [2^63+1, 2^64-1] (with overflow).
+        /// Similarly for UInt128 and UInt256.
+        bool is_monotonic = true;
+        switch (left.getType())
+        {
+            case Field::Types::UInt64:
+                is_monotonic = (left.safeGet<UInt64>() > 1ul << 63) == (right.safeGet<UInt64>() > 1ul << 63);
+                break;
+            case Field::Types::UInt128:
+                is_monotonic = (left.safeGet<UInt128>() > UInt128(1) << 127) == (right.safeGet<UInt128>() > UInt128(1) << 127);
+                break;
+            case Field::Types::UInt256:
+                is_monotonic = (left.safeGet<UInt256>() > UInt256(1) << 255) == (right.safeGet<UInt256>() > UInt256(1) << 255);
+                break;
+            default:
+                break;
+        }
+
+        return { .is_monotonic = is_monotonic, .is_positive = false, .is_strict = true };
     }
 };
 
