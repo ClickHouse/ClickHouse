bbe28d45bff0bd721685c812706f113e1412ed6b
vdimir <vdimir@clickhouse.com>
2024-11-05 12:33:25 +0000

    fix

--- src/TableFunctions/TableFunctionMongoDBPocoLegacy.cpp
+++ src/TableFunctions/TableFunctionMongoDBPocoLegacy.cpp
@@ -15,6 +15,7 @@
 #include <TableFunctions/registerTableFunctions.h>
 #include <Storages/checkAndGetLiteralArgument.h>
 #include <Storages/ColumnsDescription.h>
+#include <TableFunctions/TableFunctionMongoDB.h>
 
 
 namespace DB
@@ -97,19 +98,11 @@ void TableFunctionMongoDBPocoLegacy::parseArguments(const ASTPtr & ast_function,
     {
         if (const auto * ast_func = typeid_cast<const ASTFunction *>(args[i].get()))
         {
-            const auto * args_expr = assert_cast<const ASTExpressionList *>(ast_func->arguments.get());
-            const auto & function_args = args_expr->children;
-            if (function_args.size() != 2 || ast_func->name != "equals" || function_args[0]->as<ASTIdentifier>())
-                throw Exception(ErrorCodes::BAD_ARGUMENTS, "Expected key-value defined argument, got {}", ast_func->formatForErrorMessage());
-
-            auto arg_name = function_args[0]->as<ASTIdentifier>()->name();
-
+            const auto & [arg_name, arg_value] = getKeyValueMongoDBArgument(ast_func);
             if (arg_name == "structure")
-                structure = checkAndGetLiteralArgument<String>(function_args[1], "structure");
+                structure = checkAndGetLiteralArgument<String>(arg_value, "structure");
             else if (arg_name == "options")
-                main_arguments.push_back(function_args[1]);
-            else
-                throw Exception(ErrorCodes::BAD_ARGUMENTS, "Expected key-value defined argument, got {}", ast_func->formatForErrorMessage());
+                main_arguments.push_back(arg_value);
         }
         else if (i == 5)
         {
