996773b205121f55d6f066826dab95b38b49dbbf
Raúl Marín <git@rmr.ninja>
2024-11-05 12:39:36 +0100

    Test with both analyzers

--- tests/queries/0_stateless/03261_optimize_rewrite_array_exists_to_has_crash.sql
+++ tests/queries/0_stateless/03261_optimize_rewrite_array_exists_to_has_crash.sql
@@ -7,4 +7,4 @@ INNER JOIN rewrite AS y ON (
     SELECT 1
 )
 INNER JOIN rewrite AS z ON 1
-SETTINGS allow_experimental_analyzer=0, optimize_rewrite_array_exists_to_has=1;
+SETTINGS optimize_rewrite_array_exists_to_has=1;
