b77501713b88452c3461d9a1e77b1c923c315644
János Benjamin Antal <benjamin.antal@clickhouse.com>
2024-11-06 16:49:14 +0000

    Add tests to `WHERE`

---
+++ tests/queries/0_stateless/03262_common_expression_optimization.sql
@@ -0,0 +1,58 @@
+DROP TABLE IF EXISTS x;
+CREATE TABLE x (x Int64, A UInt8, B UInt8, C UInt8, D UInt8, E UInt8, F UInt8) ENGINE = MergeTree ORDER BY x;
+INSERT INTO x SELECT * FROM generateRandom('x Int64, A UInt8, B UInt8, C UInt8, D UInt8, E UInt8, F UInt8') LIMIT 10000;
+
+EXPLAIN QUERY TREE SELECT count()
+FROM x
+WHERE A AND ((B AND C) OR (B AND C AND F));
+
+EXPLAIN QUERY TREE SELECT count()
+FROM x
+WHERE A AND ((B AND C AND E) OR (B AND C AND F));
+
+EXPLAIN QUERY TREE SELECT count()
+FROM x
+WHERE A AND ((B AND (C AND E)) OR (B AND C AND F));
+
+EXPLAIN QUERY TREE SELECT count()
+FROM x
+WHERE A AND ((B AND C) OR (B AND D) OR (B AND E));
+
+EXPLAIN QUERY TREE SELECT count()
+FROM x
+WHERE A AND ((B AND C) OR ((B AND D) OR (B AND E)));
+
+-- Without AND as a root
+EXPLAIN QUERY TREE SELECT count()
+FROM x
+WHERE ((B AND C) OR (B AND C AND F));
+
+EXPLAIN QUERY TREE SELECT count()
+FROM x
+WHERE ((B AND C AND E) OR (B AND C AND F));
+
+EXPLAIN QUERY TREE SELECT count()
+FROM x
+WHERE ((B AND (C AND E)) OR (B AND C AND F));
+
+EXPLAIN QUERY TREE SELECT count()
+FROM x
+WHERE ((B AND C) OR (B AND D) OR (B AND E));
+
+EXPLAIN QUERY TREE SELECT count()
+FROM x
+WHERE ((B AND C) OR ((B AND D) OR (B AND E)));
+
+-- Complex expression
+EXPLAIN QUERY TREE SELECT count()
+FROM x
+WHERE (A AND (sipHash64(C) = sipHash64(D))) OR (B AND (sipHash64(C) = sipHash64(D)));
+
+-- Flattening is only happening if something can be extracted
+EXPLAIN QUERY TREE SELECT count()
+FROM x
+WHERE ((A AND B) OR ((C AND D) OR (E AND F)));
+
+EXPLAIN QUERY TREE SELECT count()
+FROM x
+WHERE ((A AND B) OR ((B AND D) OR (E AND F)));
