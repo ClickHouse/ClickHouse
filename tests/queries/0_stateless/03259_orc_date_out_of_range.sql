590029a33bfd844eede8b4ad570464d0cf86c938
taiyang-li <654010905@qq.com>
2024-11-06 16:38:09 +0800

    fix orc date32 overflow

--- tests/queries/0_stateless/03259_orc_date_out_of_range.sql
+++ tests/queries/0_stateless/03259_orc_date_out_of_range.sql
@@ -3,12 +3,12 @@
 SET session_timezone = 'UTC';
 SET engine_file_truncate_on_insert = 1;
 
-insert into function file('03259.orc')
+insert into function file('03259.orc', 'ORC')
 select
     number,
     if (number % 2 = 0, null, toDate32(number)) as date_field
-    from numbers(10);
+from numbers(10);
 
-desc file('03259.orc');
+desc file('03259.orc', 'ORC');
 
-select date_field from file('03259.orc') order by number;
+select date_field from file('03259.orc', 'ORC') order by number;
