b08fd779c3aa100d5c361268688d8c23aae3d41c
Alexander Gololobov <davenger@clickhouse.com>
2024-11-12 16:24:01 +0100

    Test to check that max_threads_for_indexes>1 uses thread pool

---
+++ tests/queries/0_stateless/03263_max_threads_for_idx.sql
@@ -0,0 +1,27 @@
+DROP TABLE IF EXISTS t_03263;
+
+CREATE TABLE t_03263(k UInt64) ENGINE = MergeTree() ORDER BY k;
+
+-- Create table with 6 parts
+SYSTEM STOP MERGES t_03263;
+
+INSERT INTO t_03263 VALUES(1);
+INSERT INTO t_03263 VALUES(2);
+INSERT INTO t_03263 VALUES(3);
+INSERT INTO t_03263 VALUES(4);
+INSERT INTO t_03263 VALUES(5);
+INSERT INTO t_03263 VALUES(6);
+
+SELECT count() FROM system.parts WHERE database = currentDatabase() AND table = 't_03263';
+
+SELECT * FROM t_03263 LIMIT 1 SETTINGS max_threads=1, log_comment='t_03263--1' FORMAT Null;
+SELECT * FROM t_03263 LIMIT 1 SETTINGS max_threads=1, max_threads_for_indexes=4, log_comment='t_03263--2' FORMAT Null;
+SELECT * FROM t_03263 LIMIT 1 SETTINGS max_threads=4, max_threads_for_indexes=1, log_comment='t_03263--3' FORMAT Null;
+SELECT * FROM t_03263 LIMIT 1 SETTINGS max_threads=4, max_threads_for_indexes=4, log_comment='t_03263--4' FORMAT Null;
+
+SYSTEM FLUSH LOGS;
+
+-- Check if queries where using local thread pool (with max_threads_for_indexes > 1 they should)
+SELECT log_comment, ProfileEvents['SelectedPartsTotal'],  ProfileEvents['LocalThreadPoolJobs'] >= 6 FROM system.query_log
+WHERE current_database = currentDatabase() AND type = 'QueryFinish' AND log_comment LIKE 't_03263--%'
+ORDER BY log_comment;
