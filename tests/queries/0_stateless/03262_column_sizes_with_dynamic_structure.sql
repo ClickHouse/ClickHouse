43456d74476c99ebdb25ae74a9e6f471421f3191
Pavel Kruglov <48961922+Avogar@users.noreply.github.com>
2024-11-12 16:30:57 +0100

    Restart CI

--- tests/queries/0_stateless/03262_column_sizes_with_dynamic_structure.sql
+++ tests/queries/0_stateless/03262_column_sizes_with_dynamic_structure.sql
@@ -3,6 +3,7 @@
 set allow_experimental_dynamic_type = 1;
 set allow_experimental_json_type = 1;
 
+
 drop table if exists test;
 create table test (d Dynamic, json JSON) engine=MergeTree order by tuple() settings min_rows_for_wide_part=0, min_bytes_for_wide_part=1;
 insert into test select number, '{"a" : 42, "b" : "Hello, World"}' from numbers(10000000);
