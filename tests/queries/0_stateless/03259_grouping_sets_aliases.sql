26767b7955c06b57e8e966d54f3f81ed628657c2
vdimir <vdimir@clickhouse.com>
2024-11-13 12:46:36 +0000

    proper fix

--- tests/queries/0_stateless/03259_grouping_sets_aliases.sql
+++ tests/queries/0_stateless/03259_grouping_sets_aliases.sql
@@ -4,6 +4,6 @@ CREATE TABLE users (name String, score UInt8, user_level String ALIAS multiIf(sc
 INSERT INTO users VALUES ('a',1),('b',2),('c', 50);
 
 SELECT user_level as level_alias, uniq(name) as name_alias, grouping(level_alias) as _totals
-FROM remote('127.0.0.{1,2}', default, users)
+FROM remote('127.0.0.{1,2}', currentDatabase(), users)
 GROUP BY GROUPING SETS ((level_alias))
 ORDER BY name_alias DESC;
