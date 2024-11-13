43f3c886a23de600b50813fbfc4fdebb58a7325f
udiz <udi@groundcover.com>
2024-11-13 22:46:36 +0000

    add test

--- tests/queries/0_stateless/03216_arrayWithConstant_limits.sql
+++ tests/queries/0_stateless/03216_arrayWithConstant_limits.sql
@@ -1,3 +1,6 @@
 SELECT arrayWithConstant(96142475, ['qMUF']); -- { serverError TOO_LARGE_ARRAY_SIZE }
 SELECT arrayWithConstant(100000000, materialize([[[[[[[[[['Hello, world!']]]]]]]]]])); -- { serverError TOO_LARGE_ARRAY_SIZE }
 SELECT length(arrayWithConstant(10000000, materialize([[[[[[[[[['Hello world']]]]]]]]]])));
+
+CREATE TEMPORARY TABLE args (value Array(Int)) ENGINE=Memory AS SELECT [1, 1, 1, 1] as value FROM numbers(1, 10000);
+CREATE TEMPORARY TABLE results (value Array(Array(Int))) ENGINE=Memory AS SELECT arrayWithConstant(10000, value) FROM args;
