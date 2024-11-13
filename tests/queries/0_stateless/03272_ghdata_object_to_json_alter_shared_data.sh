e7f708696f2b5501a90b67f95f72adeda362fa57
avogar <avogar@clickhouse.com>
2024-11-13 13:26:16 +0000

    Fix tests

--- tests/queries/0_stateless/03272_ghdata_object_to_json_alter_shared_data.sh
+++ tests/queries/0_stateless/03272_ghdata_object_to_json_alter_shared_data.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
-# Tags: no-fasttest, long
+# Tags: no-fasttest, no-msan, no-tsan, no-asan, long
 
 CUR_DIR=$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)
 # shellcheck source=../shell_config.sh
