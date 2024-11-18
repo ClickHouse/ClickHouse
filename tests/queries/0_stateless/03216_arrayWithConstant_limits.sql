ed95e0781f7e9b2374ed2403d43b3ee6b368c9c1
udiz <udi@groundcover.com>
2024-11-18 22:48:38 +0000

    test uses less memory

--- tests/queries/0_stateless/03216_arrayWithConstant_limits.sql
+++ tests/queries/0_stateless/03216_arrayWithConstant_limits.sql
@@ -2,5 +2,5 @@ SELECT arrayWithConstant(96142475, ['qMUF']); -- { serverError TOO_LARGE_ARRAY_S
 SELECT arrayWithConstant(100000000, materialize([[[[[[[[[['Hello, world!']]]]]]]]]])); -- { serverError TOO_LARGE_ARRAY_SIZE }
 SELECT length(arrayWithConstant(10000000, materialize([[[[[[[[[['Hello world']]]]]]]]]])));
 
-CREATE TEMPORARY TABLE args (value Array(Int)) ENGINE=Memory AS SELECT [1, 1, 1, 1] as value FROM numbers(1, 10000);
-CREATE TEMPORARY TABLE results (value Array(Array(Int))) ENGINE=Memory AS SELECT arrayWithConstant(10000, value) FROM args;
+CREATE TEMPORARY TABLE args (value Array(Int)) ENGINE=Memory AS SELECT [1, 1, 1, 1] as value FROM numbers(1, 100);
+SELECT arrayWithConstant(1000000, value) FROM args;
