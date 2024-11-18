62c5c15ab6bde76cb79d9a5f228b8f99337b7b9d
János Benjamin Antal <benjamin.antal@clickhouse.com>
2024-11-18 14:56:34 +0000

    Update tests

--- tests/queries/0_stateless/01429_join_on_error_messages.sql
+++ tests/queries/0_stateless/01429_join_on_error_messages.sql
@@ -20,4 +20,4 @@ SELECT 1 FROM (select 1 a) A JOIN (select 1 b, 1 c) B ON a = b OR a = c; -- { se
 SET join_algorithm = 'hash';
 
 -- conditions for different table joined via OR
-SELECT * FROM (SELECT 1 AS a, 1 AS b, 1 AS c) AS t1 INNER JOIN (SELECT 1 AS a, 1 AS b, 1 AS c) AS t2 ON t1.a = t2.a AND (t1.b > 0 OR t2.b > 0); -- { serverError INVALID_JOIN_ON_EXPRESSION }
+SELECT * FROM (SELECT 1 AS a, 1 AS b, 1 AS c) AS t1 INNER JOIN (SELECT 1 AS a, 1 AS b, 1 AS c) AS t2 ON t1.a = t2.a AND (t1.b > 0 OR t2.b > 0);
