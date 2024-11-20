e81c5171b78a659726ac6380a6c5f5e70acfbe1d
Yarik Briukhovetskyi <114298166+yariks5s@users.noreply.github.com>
2024-11-20 20:10:04 +0100

    Small fixup.

--- src/Interpreters/QueryAliasesVisitor.cpp
+++ src/Interpreters/QueryAliasesVisitor.cpp
@@ -115,30 +115,14 @@ void QueryAliasesMatcher<T>::visit(const ASTSubquery & const_subquery, const AST
 }
 
 template <typename T>
-bool QueryAliasesMatcher<T>::checkIfNamesAreSame(Data & data, const ASTPtr & ast)
-{
-    String name = ast->getColumnName();
-    for (const auto & obj : data)
-    {
-        if (obj.second->getColumnName() == name)
-            return true;
-    }
-    return false;
-}
-
-template <typename T>
 void QueryAliasesMatcher<T>::visitOther(const ASTPtr & ast, Data & data)
 {
     auto & aliases = data;
     String alias = ast->tryGetAlias();
-
     if (!alias.empty())
     {
         if (aliases.contains(alias) && ast->getTreeHash(/*ignore_aliases=*/ true) != aliases[alias]->getTreeHash(/*ignore_aliases=*/ true))
-        {
-            if (checkIfNamesAreSame(aliases, ast))
                 throw Exception(wrongAliasMessage(ast, aliases[alias], alias), ErrorCodes::MULTIPLE_EXPRESSIONS_FOR_ALIAS);
-        }
 
         aliases[alias] = ast;
     }
