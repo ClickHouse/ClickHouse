ed285f47dc2b47fada0343ed69b7c3d91f7a2915
Yarik Briukhovetskyi <114298166+yariks5s@users.noreply.github.com>
2024-11-20 23:14:19 +0100

    Update 03274_aliases_in_udf.sql

--- tests/queries/0_stateless/03274_aliases_in_udf.sql
+++ tests/queries/0_stateless/03274_aliases_in_udf.sql
@@ -1,4 +1,4 @@
-SELECT "FIX ISSUE #69143";
+SELECT 'FIX ISSUE #69143';
 
 CREATE OR REPLACE FUNCTION test_function AS ( input_column_name ) -> ((
         '1' AS a,
@@ -25,7 +25,7 @@ INSERT INTO test_table SELECT 'a', 'b';
 SELECT mat_a FROM test_table;
 SELECT mat_b FROM test_table;
 
-SELECT "EXPAIN SYNTAX OF UDF";
+SELECT 'EXPAIN SYNTAX OF UDF';
 
 CREATE OR REPLACE FUNCTION test_03274 AS ( x ) -> ((x + 1 as y, y + 2));
 
