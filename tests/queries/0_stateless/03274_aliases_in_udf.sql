7dcaacab9daa7dd491dde5fcaf698d2d85a21d85
Yarik Briukhovetskyi <114298166+yariks5s@users.noreply.github.com>
2024-11-21 01:00:37 +0100

    Update 03274_aliases_in_udf.sql

--- tests/queries/0_stateless/03274_aliases_in_udf.sql
+++ tests/queries/0_stateless/03274_aliases_in_udf.sql
@@ -1,11 +1,15 @@
 SELECT 'FIX ISSUE #69143';
 
+DROP FUNCTION IF EXISTS test_function;
+DROP FUNCTION IF EXISTS test_03274;
+DROP TABLE IF EXISTS test_table;
+
 CREATE OR REPLACE FUNCTION test_function AS ( input_column_name ) -> ((
         '1' AS a,
         input_column_name AS input_column_name
     ).2);
 
-CREATE TABLE test_table
+CREATE TABLE IF NOT EXISTS test_table
 (
     `metadata_a` String,
     `metadata_b` String
@@ -30,3 +34,7 @@ SELECT 'EXPAIN SYNTAX OF UDF';
 CREATE OR REPLACE FUNCTION test_03274 AS ( x ) -> ((x + 1 as y, y + 2));
 
 EXPAIN SYNTAX SELECT test_03274(4 + 2);
+
+DROP FUNCTION IF EXISTS test_function;
+DROP FUNCTION IF EXISTS test_03274;
+DROP TABLE IF EXISTS test_table;
