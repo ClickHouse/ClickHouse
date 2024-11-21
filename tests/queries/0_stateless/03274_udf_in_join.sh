7baaf24fc32264de2a845a17306cece48a618e61
Raúl Marín <git@rmr.ninja>
2024-11-21 12:35:14 +0100

    Enable setting

--- tests/queries/0_stateless/03274_udf_in_join.sh
+++ tests/queries/0_stateless/03274_udf_in_join.sh
@@ -8,7 +8,7 @@ $CLICKHOUSE_CLIENT -q "
   CREATE VIEW v0 AS SELECT 1 AS c0;
   CREATE FUNCTION ${CLICKHOUSE_DATABASE}_second AS (x, y) -> y;
   CREATE FUNCTION ${CLICKHOUSE_DATABASE}_equals AS (x, y) -> x = y;
-  -- SET optimize_rewrite_array_exists_to_has = 1;
+  SET optimize_rewrite_array_exists_to_has = 1;
 
   EXPLAIN SYNTAX SELECT 1 FROM v0 JOIN v0 vx ON ${CLICKHOUSE_DATABASE}_second(v0.c0, vx.c0); -- { serverError INVALID_JOIN_ON_EXPRESSION }
   EXPLAIN SYNTAX SELECT 1 FROM v0 JOIN v0 vx ON ${CLICKHOUSE_DATABASE}_equals(v0.c0, vx.c0);
