163f2b11215f0e57fecb802415300485dd56b512
Yakov Olkhovskiy <yakov@clickhouse.com>
2024-11-21 05:57:12 +0000

    run time to 5 min per test

--- tests/ci/libfuzzer_test_check.py
+++ tests/ci/libfuzzer_test_check.py
@@ -21,7 +21,7 @@ from s3_helper import S3Helper
 from stopwatch import Stopwatch
 from tee_popen import TeePopen
 
-TIMEOUT = 60
+TIMEOUT = 60 * 5
 NO_CHANGES_MSG = "Nothing to run"
 s3 = S3Helper()
 
