97aefe668ef0d1dab08231bd1ad183ea9fdbc613
taiyang-li <654010905@qq.com>
2024-11-21 09:38:52 +0800

    fix style

--- tests/queries/0_stateless/03150_dynamic_type_mv_insert.sql
+++ tests/queries/0_stateless/03150_dynamic_type_mv_insert.sql
@@ -20,11 +20,11 @@ CREATE TABLE to_table
 )
 ENGINE = MergeTree ORDER BY n1;
 
-INSERT INTO null_table ( n1, n2 ) VALUES (1, '2024-01-01'), (2, toDateTime64('2024-01-01', 3, 'Asia/Istanbul')), (3, toFloat32(1)), (4, toFloat64(2));
+INSERT INTO null_table ( n1, n2 ) VALUES (1, '2024-01-01'), (2, toDateTime64('2024-01-01', 3)), (3, toFloat32(1)), (4, toFloat64(2));
 SELECT *, dynamicType(n2) FROM to_table ORDER BY ALL;
 
 select '';
-INSERT INTO null_table ( n1, n2 ) VALUES (1, '2024-01-01'), (2, toDateTime64('2024-01-01', 3, 'Asia/Istanbul')), (3, toFloat32(1)), (4, toFloat64(2));
+INSERT INTO null_table ( n1, n2 ) VALUES (1, '2024-01-01'), (2, toDateTime64('2024-01-01', 3)), (3, toFloat32(1)), (4, toFloat64(2));
 SELECT *, dynamicType(n2) FROM to_table ORDER BY ALL;
 
 select '';
@@ -33,7 +33,7 @@ SELECT *, dynamicType(n2) FROM to_table ORDER BY ALL;
 
 select '';
 ALTER TABLE to_table MODIFY COLUMN n2 Dynamic(max_types=10);
-INSERT INTO null_table ( n1, n2 ) VALUES (1, '2024-01-01'), (2, toDateTime64('2024-01-01', 3, 'Asia/Istanbul')), (3, toFloat32(1)), (4, toFloat64(2));
+INSERT INTO null_table ( n1, n2 ) VALUES (1, '2024-01-01'), (2, toDateTime64('2024-01-01', 3)), (3, toFloat32(1)), (4, toFloat64(2));
 SELECT *, dynamicType(n2) FROM to_table ORDER BY ALL;
 
 DROP TABLE null_table;
