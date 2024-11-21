8d380470e098f4efc6fef1a59218077e3e872419
Pablo Marcos <pablo.marcos.oltra@clickhouse.com>
2024-11-21 12:37:23 +0000

    Restrict the files to sparse checkout

--- contrib/sparse-checkout/update-google-cloud-cpp.sh
+++ contrib/sparse-checkout/update-google-cloud-cpp.sh
@@ -4,7 +4,11 @@ echo "Using sparse checkout for google-cloud-cpp"
 
 FILES_TO_CHECKOUT=$(git rev-parse --git-dir)/info/sparse-checkout
 echo '!/*' > $FILES_TO_CHECKOUT
-echo '/google/*' >> $FILES_TO_CHECKOUT
+echo '/google/cloud/*.cc' >> $FILES_TO_CHECKOUT
+echo '/google/cloud/*.h' >> $FILES_TO_CHECKOUT
+echo '/google/cloud/internal/*' >> $FILES_TO_CHECKOUT
+echo '/google/cloud/grpc_utils/*' >> $FILES_TO_CHECKOUT
+echo '/google/cloud/kms/*' >> $FILES_TO_CHECKOUT
 echo '/cmake/*' >> $FILES_TO_CHECKOUT
 echo '/protos/*' >> $FILES_TO_CHECKOUT
 echo '/external/googleapis' >> $FILES_TO_CHECKOUT
