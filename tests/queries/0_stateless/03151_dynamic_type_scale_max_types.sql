3f945619b46ce35d80656bce45aedd907e23bc88
taiyang-li <654010905@qq.com>
2024-11-21 09:40:38 +0800

    fix case

--- tests/queries/0_stateless/03151_dynamic_type_scale_max_types.sql
+++ tests/queries/0_stateless/03151_dynamic_type_scale_max_types.sql
@@ -1,6 +1,7 @@
 SET allow_experimental_dynamic_type = 1;
 SET allow_suspicious_types_in_order_by = 1;
 SET optimize_read_in_order = 1;
+SET session_timezone = 'UTC';
 
 drop table if exists to_table;
 
