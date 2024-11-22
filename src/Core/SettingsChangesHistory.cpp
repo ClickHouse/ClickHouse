c10d68c7a0ab4413e96ddb8aafd38849d7927835
Vladimir Cherkasov <vdimir@clickhouse.com>
2024-11-22 12:51:01 +0100

    Update SettingsChangesHistory.cpp

--- src/Core/SettingsChangesHistory.cpp
+++ src/Core/SettingsChangesHistory.cpp
@@ -60,6 +60,7 @@ static std::initializer_list<std::pair<ClickHouseVersion, SettingsChangesHistory
 {
     {"24.12",
         {
+            {"query_plan_join_swap_table", "false", "auto", "New setting. Right table was always chosen before."},
         }
     },
     {"24.11",
@@ -80,7 +81,6 @@ static std::initializer_list<std::pair<ClickHouseVersion, SettingsChangesHistory
             {"backup_restore_keeper_max_retries_while_initializing", 0, 20, "New setting."},
             {"backup_restore_keeper_max_retries_while_handling_error", 0, 20, "New setting."},
             {"backup_restore_finish_timeout_after_error_sec", 0, 180, "New setting."},
-            {"query_plan_join_swap_table", "false", "auto", "New setting. Right table was always chosen before."},
             {"query_plan_merge_filters", false, true, "Allow to merge filters in the query plan. This is required to properly support filter-push-down with a new analyzer."},
             {"parallel_replicas_local_plan", false, true, "Use local plan for local replica in a query with parallel replicas"},
             {"min_joined_block_size_bytes", 524288, 524288, "New setting."},
