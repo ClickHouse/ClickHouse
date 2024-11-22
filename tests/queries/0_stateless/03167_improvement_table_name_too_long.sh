f0b706ffdef2e2596108e19d0459f31a1528f504
Yarik Briukhovetskyi <114298166+yariks5s@users.noreply.github.com>
2024-11-22 14:10:40 +0100

    Fix the test (take the test_ prefix into account).

--- tests/queries/0_stateless/03167_improvement_table_name_too_long.sh
+++ tests/queries/0_stateless/03167_improvement_table_name_too_long.sh
@@ -4,14 +4,13 @@ CUR_DIR=$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)
 # shellcheck source=../shell_config.sh
 . "$CUR_DIR"/../shell_config.sh
 
-# allowed_name_length=$($CLICKHOUSE_CLIENT -mn --query="SELECT getMaxTableNameLengthForDatabase('$CLICKHOUSE_DATABASE')")
-# let excess_length=allowed_name_length+1
+max_name_length=$($CLICKHOUSE_CLIENT -mn --query="SELECT getMaxTableNameLengthForDatabase('$CLICKHOUSE_DATABASE')")
+let allowed_name_length=max_name_length-5 # We substract 5 from the name length because when we create files in the test environment, they have the 'test_' prefix
+let excess_length=allowed_name_length+1
 
-getconf NAME_MAX /fasttest-workspace/db-fasttest/store
-# long_table_name=$(openssl rand -base64 $excess_length | tr -dc A-Za-z | head -c $excess_length)
-# allowed_table_name=$(openssl rand -base64 $allowed_name_length | tr -dc A-Za-z | head -c $allowed_name_length)
+long_table_name=$(openssl rand -base64 $excess_length | tr -dc A-Za-z | head -c $excess_length)
+allowed_table_name=$(openssl rand -base64 $allowed_name_length | tr -dc A-Za-z | head -c $allowed_name_length)
 
-# $CLICKHOUSE_CLIENT -mn --query="CREATE TABLE $long_table_name (id UInt32, long_table_name String) Engine=MergeTree() order by id;" 2>&1 | grep -o -m 1 "ARGUMENT_OUT_OF_BOUND"
-# $CLICKHOUSE_CLIENT -mn --query="CREATE TABLE $allowed_table_name (id UInt32, allowed_table_name String) Engine=MergeTree() order by id;"
-# $CLICKHOUSE_CLIENT -mn --query="DROP TABLE $allowed_table_name;"
-# ToHqNPfhZpLevFeVCquBvDPDdVSFxSTRaEkCIPQzhXCVAwbmHXNRJiEFqQGgNzLULAufPDpRIjXoTxYWcdnNStNLdWDIjNiHoKltMpdGbDzeVsXFPIynefYqJPLqCuuuckRpBjOFPGoAKndFzAOGwCcZaIsGFFkOOpPwTesCZfbjvtZWrGYeYqAWJsLPQPIRzFHfebEYLOguMMNjQ
+$CLICKHOUSE_CLIENT -mn --query="CREATE TABLE $long_table_name (id UInt32, long_table_name String) Engine=MergeTree() order by id;" 2>&1 | grep -o -m 1 "ARGUMENT_OUT_OF_BOUND"
+$CLICKHOUSE_CLIENT -mn --query="CREATE TABLE $allowed_table_name (id UInt32, allowed_table_name String) Engine=MergeTree() order by id;"
+$CLICKHOUSE_CLIENT -mn --query="DROP TABLE $allowed_table_name;"
