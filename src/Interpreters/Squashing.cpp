2bea36c19f3f7e8bf515495bcccea343e41dad59
vdimir <vdimir@clickhouse.com>
2024-11-22 15:35:45 +0000

    fix

--- src/Interpreters/Squashing.cpp
+++ src/Interpreters/Squashing.cpp
@@ -146,13 +146,14 @@ Chunk Squashing::squash(std::vector<Chunk> && input_chunks, Chunk::ChunkInfoColl
         for (size_t j = 0; j != num_columns; ++j)
         {
             /// IColumn::structureEquals is not implemented for deprecated object type, ignore it and always convert to non-sparse.
-            bool has_object_deprecated = false;
+            bool has_object_deprecated = columns[j]->getDataType() == TypeIndex::ObjectDeprecated ||
+                mutable_columns[j]->getDataType() == TypeIndex::ObjectDeprecated;
             auto has_object_deprecated_lambda = [&has_object_deprecated](const auto & subcolumn)
             {
                 has_object_deprecated = has_object_deprecated || subcolumn.getDataType() == TypeIndex::ObjectDeprecated;
             };
-            columns[j]->forEachSubcolumnRecursively(has_object_deprecated);
-            mutable_columns[j]->forEachSubcolumnRecursively(has_object_deprecated);
+            columns[j]->forEachSubcolumnRecursively(has_object_deprecated_lambda);
+            mutable_columns[j]->forEachSubcolumnRecursively(has_object_deprecated_lambda);
 
             /// Need to check if there are any sparse columns in subcolumns,
             /// since `IColumn::isSparse` is not recursive but sparse column can be inside a tuple, for example.
