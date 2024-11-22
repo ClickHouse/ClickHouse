6c7cde57028ca11e49b6f695e37734ba50b98531
Raúl Marín <git@rmr.ninja>
2024-11-22 19:56:07 +0100

    Better

--- src/IO/S3Common.h
+++ src/IO/S3Common.h
@@ -32,8 +32,8 @@ public:
 
     // Format message with fmt::format, like the logging functions.
     template <typename... Args>
-    S3Exception(Aws::S3::S3Errors code_, fmt::format_string<Args...> fmt, Args &&... args)
-        : Exception(ErrorCodes::S3_ERROR, PreformattedMessage::create(fmt, std::forward<Args>(args)...)), code(code_)
+    S3Exception(Aws::S3::S3Errors code_, FormatStringHelper<Args...> fmt, Args &&... args)
+        : Exception(PreformattedMessage{fmt.format(std::forward<Args>(args)...)}, ErrorCodes::S3_ERROR), code(code_)
     {
     }
 
