a35971f7498e15000d17ff8754a7840b81a43ed6
Alexander Gololobov <davenger@clickhouse.com>
2024-11-25 20:19:46 +0100

    style

--- src/Columns/ColumnLowCardinality.cpp
+++ src/Columns/ColumnLowCardinality.cpp
@@ -489,7 +489,6 @@ void ColumnLowCardinality::updatePermutation(IColumn::PermutationSortDirection d
         throw Exception(ErrorCodes::LOGICAL_ERROR,
             "Dictionary permutation size {} is equal to dictionary size {}. It is a bug.",
             dict_perm.size(), getDictionary().size());
-    
     PaddedPODArray<UInt64> position_by_index(dict_perm.size());
     for (size_t i = 0; i < dict_perm.size(); ++i)
         position_by_index[dict_perm[i]] = i;
