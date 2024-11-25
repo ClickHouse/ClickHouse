207cc837f5103e27a45296db89aee9eddb69175a
Pavel Kruglov <48961922+Avogar@users.noreply.github.com>
2024-11-25 15:29:03 +0100

    Update SettingsChangesHistory.cpp

--- src/Core/SettingsChangesHistory.cpp
+++ src/Core/SettingsChangesHistory.cpp
@@ -91,6 +91,9 @@ static std::initializer_list<std::pair<ClickHouseVersion, SettingsChangesHistory
             {"s3_skip_empty_files", false, true, "We hope it will provide better UX"},
             {"filesystem_cache_boundary_alignment", 0, 0, "New setting"},
             {"push_external_roles_in_interserver_queries", false, false, "New setting."},
+            {"enable_variant_type", false, false, "Add alias to allow_experimental_variant_type"},
+            {"enable_dynamic_type", false, false, "Add alias to allow_experimental_dynamic_type"},
+            {"enable_json_type", false, false, "Add alias to allow_experimental_json_type"},
         }
     },
     {"24.10",
