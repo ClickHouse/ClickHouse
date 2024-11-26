46cd1fed513de9a61fa88a5089f07baedddb0eba
“JiaQi <2963103258@qq.com>
2024-11-27 07:50:05 +0800

    fix tests

--- tests/queries/0_stateless/03272_parallel_replicas_read_in_order.sql
+++ tests/queries/0_stateless/03272_parallel_replicas_read_in_order.sql
@@ -12,6 +12,8 @@ enable_parallel_replicas = 1,
 max_threads=1;
 
 -- Check we don't read more mark in parallel replicas
-select count(1) from system.query_log where current_database = currentDatabase() AND query LIKE '%read_in_order_with_parallel_replicas%' AND query NOT LIKE '%system%' and read_rows>2;
+select sleep(3);
+select count(1) from system.query_log where current_database = currentDatabase() AND query LIKE '%SELECT * from read_in_order_with_parallel_replicas%' AND query NOT LIKE '%system%' and read_rows>2;
+select count(1) from system.query_log where current_database = currentDatabase() AND query LIKE '%SELECT * from read_in_order_with_parallel_replicas%' AND query NOT LIKE '%system%' and is_initial_query and type=2;
 
 DROP TABLE IF EXISTS read_in_order_with_parallel_replicas;
