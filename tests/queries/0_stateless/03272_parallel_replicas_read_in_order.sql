657ff3e74c223dac7e0a442947f2048794c740bd
“JiaQi <2963103258@qq.com>
2024-11-26 15:44:03 +0800

    add tests

---
+++ tests/queries/0_stateless/03272_parallel_replicas_read_in_order.sql
@@ -0,0 +1,16 @@
+DROP TABLE IF EXISTS read_in_order_with_parallel_replicas;
+CREATE TABLE read_in_order_with_parallel_replicas(id UInt64) ENGINE=MergeTree ORDER BY id SETTINGS index_granularity=1;
+
+
+INSERT INTO read_in_order_with_parallel_replicas SELECT number from system.numbers limit 100000;
+
+SELECT * from read_in_order_with_parallel_replicas ORDER BY id desc limit 1;
+
+SELECT * from read_in_order_with_parallel_replicas ORDER BY id desc limit 1
+SETTINGS max_parallel_replicas = 2,
+enable_parallel_replicas = 1;
+
+-- Check we don't read more mark in parallel replicas
+select count(1) from system.query_log where current_database = currentDatabase() AND query LIKE '%read_in_order_with_parallel_replicas%' AND query NOT LIKE '%system%' and read_rows>2;
+
+DROP TABLE IF EXISTS read_in_order_with_parallel_replicas;
