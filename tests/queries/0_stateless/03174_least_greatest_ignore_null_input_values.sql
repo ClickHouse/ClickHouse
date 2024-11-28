4c3d4043fa4cf8e1152b3240ddd7895a738ab389
Robert Schulze <robert@clickhouse.com>
2024-11-28 11:47:55 +0100

    Update 03174_least_greatest_ignore_null_input_values.sql

--- tests/queries/0_stateless/03174_least_greatest_ignore_null_input_values.sql
+++ tests/queries/0_stateless/03174_least_greatest_ignore_null_input_values.sql
@@ -10,7 +10,7 @@ SELECT greatest(NULL, 'a'), least(NULL, 'a');
 SELECT greatest('a', NULL), least('a', NULL);
 
 SELECT 'Test with one non-const argument';
-SELECT greatest(materialize(NULL)), least(materialize(NULL));                           -- see (*) below
+SELECT greatest(materialize(NULL)), least(materialize(NULL));
 
 SELECT 'Test with two non-const arguments';
 SELECT greatest(materialize(1), NULL), least(materialize(1), NULL);
@@ -20,6 +20,6 @@ SELECT greatest(materialize(1.0), NULL), least(materialize(1.0), NULL);
 SELECT greatest(materialize(NULL), 'a'), least(materialize(NULL), 'a');
 SELECT greatest(materialize('a'), NULL), least(materialize('a'), NULL);
 
-SELECT 'Special cases';                                                  -- see (**) below
+SELECT 'Special cases';
 SELECT greatest(toNullable(1), 2), least(toNullable(1), 2);
 SELECT greatest(toLowCardinality(1), NULL), least(toLowCardinality(1), NULL);
