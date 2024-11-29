98a358db10bc1401d8c4199138c42959061a0618
Yarik Briukhovetskyi <114298166+yariks5s@users.noreply.github.com>
2024-11-29 18:49:09 +0100

    Fix tests

--- tests/queries/0_stateless/03278_dateTime64_in_dateTime64_bug.sql
+++ tests/queries/0_stateless/03278_dateTime64_in_dateTime64_bug.sql
@@ -5,3 +5,5 @@ INSERT INTO datetime64_issue(id, dt, dtn) VALUES (1, toDateTime64('2001-01-11 01
 SELECT * FROM datetime64_issue WHERE dt in (toDateTime64('2001-01-11 01:11:21.100', 3));
 
 SELECT * FROM datetime64_issue WHERE dtn in (toDateTime64('2001-01-11 01:11:21.100', 3));
+
+DROP TABLE datetime64_issue;
