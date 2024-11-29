1e587f5100b4dfb7f94658031a86da97c2bae0a3
avogar <avogar@clickhouse.com>
2024-11-29 20:52:34 +0000

    Fix style

--- src/Processors/QueryPlan/ExtractColumnsStep.cpp
+++ src/Processors/QueryPlan/ExtractColumnsStep.cpp
@@ -9,16 +9,16 @@ namespace DB
 static ITransformingStep::Traits getTraits()
 {
     return ITransformingStep::Traits
+    {
         {
-            {
-                .returns_single_stream = false,
-                .preserves_number_of_streams = true,
-                .preserves_sorting = true,
-            },
-            {
-                .preserves_number_of_rows = true,
-            }
-        };
+            .returns_single_stream = false,
+            .preserves_number_of_streams = true,
+            .preserves_sorting = true,
+        },
+        {
+            .preserves_number_of_rows = true,
+        }
+    };
 }
 
 ExtractColumnsStep::ExtractColumnsStep(const Header & input_header_, const NamesAndTypesList & requested_columns_)
