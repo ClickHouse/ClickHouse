2372e9fece9ebbfc9398a72bd070470cd6f8222e
Alexey Milovidov <milovidov@clickhouse.com>
2024-11-30 22:53:13 +0100

    Add `http_response_headers` setting

--- src/Interpreters/executeQuery.cpp
+++ src/Interpreters/executeQuery.cpp
@@ -180,6 +180,7 @@ namespace ErrorCodes
     extern const int SYNTAX_ERROR;
     extern const int SUPPORT_IS_DISABLED;
     extern const int INCORRECT_QUERY;
+    extern const int BAD_ARGUMENTS;
 }
 
 namespace FailPoints
