e8d6eed6a2319d7886ff2601626a3b6d0d112d10
Arthur Passos <arthur.ti@outlook.com>
2024-12-04 10:57:25 -0300

    define err code

--- src/Processors/Formats/Impl/ParquetBlockInputFormat.cpp
+++ src/Processors/Formats/Impl/ParquetBlockInputFormat.cpp
@@ -55,6 +55,7 @@ namespace ErrorCodes
     extern const int INCORRECT_DATA;
     extern const int CANNOT_READ_ALL_DATA;
     extern const int CANNOT_PARSE_NUMBER;
+    extern const int LOGICAL_ERROR;
 }
 
 #define THROW_ARROW_NOT_OK(status)                                     \
