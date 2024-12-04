6a4da34a134ef93091abc894184576b0011de865
Azat Khuzhin <a3at.mail@gmail.com>
2024-12-04 11:40:24 +0100

    tests: fix 00110_external_sort and make it better
    Signed-off-by: Azat Khuzhin <a.khuzhin@semrush.com>

--- tests/queries/0_stateless/00110_external_sort.sql
+++ tests/queries/0_stateless/00110_external_sort.sql
@@ -1,11 +1,10 @@
 -- Tags: no-parallel, no-fasttest
 
-SET max_memory_usage = 300000000;
-SET max_bytes_before_external_sort = 20000000;
-SELECT number FROM (SELECT number FROM system.numbers LIMIT 10000000) ORDER BY number * 1234567890123456789 LIMIT 9999990, 10;
+-- { echoOn }
+SELECT number FROM (SELECT number FROM system.numbers LIMIT 10000000) ORDER BY number * 1234567890123456789 LIMIT 9999990, 10 SETTINGS max_memory_usage=300000000, max_bytes_before_external_sort=70000000;
+SYSTEM FLUSH LOGS;
+SELECT ProfileEvents['ExternalSortWritePart'] FROM system.query_log WHERE type != 'QueryStart' AND current_database = currentDatabase() AND Settings['max_bytes_before_external_sort']='70000000';
 
-SELECT '-';
-
-SET max_bytes_before_external_sort = 33554432;
-set max_block_size = 1048576;
-SELECT number FROM (SELECT number FROM numbers(2097152)) ORDER BY number * 1234567890123456789 LIMIT 2097142, 10;
+SELECT number FROM (SELECT number FROM numbers(2097152)) ORDER BY number * 1234567890123456789 LIMIT 2097142, 10 SETTINGS max_memory_usage=300000000, max_bytes_before_external_sort=33554432, max_block_size=1048576;
+SYSTEM FLUSH LOGS;
+SELECT ProfileEvents['ExternalSortWritePart'] FROM system.query_log WHERE type != 'QueryStart' AND current_database = currentDatabase() AND Settings['max_bytes_before_external_sort']='33554432';
