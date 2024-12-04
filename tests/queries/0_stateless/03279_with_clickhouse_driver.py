516fd98327270ed928a0c85c36718c394b8b686c
robot-clickhouse <robot-clickhouse@users.noreply.github.com>
2024-12-04 22:15:35 +0000

    Automatic style fix

--- tests/queries/0_stateless/03279_with_clickhouse_driver.py
+++ tests/queries/0_stateless/03279_with_clickhouse_driver.py
@@ -2,17 +2,20 @@
 # Tags: no-fasttest
 
 import os
+
 from clickhouse_driver import Client
 
 
 def run(database):
-    client = Client("localhost",user="default",password="")
-    client.execute(f"CREATE TABLE IF NOT EXISTS {database}.test (x Int32) ENGINE = Memory")
+    client = Client("localhost", user="default", password="")
+    client.execute(
+        f"CREATE TABLE IF NOT EXISTS {database}.test (x Int32) ENGINE = Memory"
+    )
     client.execute(f"INSERT INTO {database}.test (x) VALUES", [{"x": 100}])
     result = client.execute(f"SELECT * FROM {database}.test")
     print(result)
 
 
 if __name__ == "__main__":
-    database =  os.environ["CLICKHOUSE_DATABASE"]
+    database = os.environ["CLICKHOUSE_DATABASE"]
     run(database)
