f3342776988a7ecb320ec19993cbd9701d7c2044
Sema Checherinda <Sema.Checherinda@clickhouse.com>
2024-12-09 12:01:59 +0100

    fix race on getDataPartStorage().getFullPath()

--- src/Storages/System/StorageSystemParts.cpp
+++ src/Storages/System/StorageSystemParts.cpp
@@ -244,7 +244,7 @@ void StorageSystemParts::processNextStorage(
         {
             /// The full path changes at clean up thread, so do not read it if parts can be deleted, avoid the race.
             if (part->isStoredOnDisk()
-                && part_state != State::Deleting && part_state != State::DeleteOnDestroy && part_state != State::Temporary)
+                && part_state != State::Deleting && part_state != State::DeleteOnDestroy && part_state != State::Temporary && part_state != State::PreActive)
             {
                 columns[res_index++]->insert(part->getDataPartStorage().getFullPath());
             }
