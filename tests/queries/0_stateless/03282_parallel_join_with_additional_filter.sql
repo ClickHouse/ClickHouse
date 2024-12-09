201f021db1e35d29ad3eed10c067e645deced66b
Nikita Taranov <nikita.taranov@clickhouse.com>
2024-12-09 23:02:26 +0100

    allow with parallel_hash

--- tests/queries/0_stateless/03282_parallel_join_with_additional_filter.sql
+++ tests/queries/0_stateless/03282_parallel_join_with_additional_filter.sql
@@ -21,6 +21,4 @@ SELECT '---- HASH';
 SELECT t1.*, t2.* FROM t1 LEFT JOIN t2 ON t1.key = t2.key AND (t1.key < t2.a OR t1.a % 2 = 0) ORDER BY ALL SETTINGS join_algorithm = 'hash';
 
 SELECT '---- PARALLEL HASH';
-SELECT t1.*, t2.* FROM t1 LEFT JOIN t2 ON t1.key = t2.key AND (t1.key < t2.a OR t1.a % 2 = 0) ORDER BY ALL SETTINGS join_algorithm = 'parallel_hash'; -- { serverError NOT_IMPLEMENTED}
-
-SELECT t1.*, t2.* FROM t1 LEFT JOIN t2 ON t1.key = t2.key AND (t1.key < t2.a OR t1.a % 2 = 0) ORDER BY ALL;
+SELECT t1.*, t2.* FROM t1 LEFT JOIN t2 ON t1.key = t2.key AND (t1.key < t2.a OR t1.a % 2 = 0) ORDER BY ALL SETTINGS join_algorithm = 'parallel_hash';
