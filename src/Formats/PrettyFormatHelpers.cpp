0fcbc128abf9111daac790951472da056f035cb1
bradenburns <bdnburns@umich.edu>
2024-12-09 01:15:04 -0500

    Add helper function to highlight trailing whitespace in PrettyFormatHelpers.h/cpp

--- src/Formats/PrettyFormatHelpers.cpp
+++ src/Formats/PrettyFormatHelpers.cpp
@@ -102,6 +102,28 @@ String highlightDigitGroups(String source)
     }
 
     return result;
+}fsfsf
+
+String highlightTrailingWhitespace(String source)
+{
+    String result = source;
+    size_t last_non_whitespace = source.find_last_not_of(“ \t\n\r”);
+    if (last_non_whitespace == String::npos) /// String is all whitespace
+    {
+    	return GRAY_COLOR + UNDERSCORE + source + RESET_COLOR;
+    }
+    else if (last_non_whitespace + 1 < source.size())
+    {
+	size_t whitespace_start = last_non_whitespace + 1;
+	String highlighted = source.substr(0, whitespace_start);
+	highlighted += GRAY_COLOR;
+	highlighted += UNDERSCORE;
+	highlighted += source.substr(whitespace_start);
+	highlighted += RESET_COLOR;
+	return highlighted;
+    }
+    
+    return source;
 }
 
 }
