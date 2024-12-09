29d9e869e986fdae8096ea56f33966dd10ca8873
MikhailBurdukov <burdukvmikhail@gmail.com>
2024-12-09 14:40:23 +0000

    Add new column(shard_name) in the shard_info

--- tests/queries/0_stateless/03273_shard_names.sh
+++ tests/queries/0_stateless/03273_shard_names.sh
@@ -5,68 +5,95 @@ CUR_DIR=$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)
 # shellcheck source=../shell_config.sh
 . "$CUR_DIR"/../shell_config.sh
 
-config_path=${CLICKHOUSE_CONFIG_DIR}/config.d/static_shards_cluster.xml
 
-## Test static shard numbers
+config_path=${CLICKHOUSE_CONFIG_DIR}/config.d/03273_shard_names_cluster.xml
+
+## Test shard name literal
 cat > "$config_path" <<EOL
 <clickhouse>
     <remote_servers>
-        <test_static_shard_nums>
+        <test_shard_names>
             <shard>
-                <shard_number>3</shard_number>
+                <shard_name>first</shard_name>
                 <replica>
                     <host>localhost</host>
                     <port>9000</port>
                 </replica>
             </shard>
             <shard>
-                <shard_number>10</shard_number>
+                <shard_name>second</shard_name>
                 <replica>
                     <host>localhost</host>
                     <port>1</port>
                 </replica>
             </shard>
-        </test_static_shard_nums>
+        </test_shard_names>
     </remote_servers>
 </clickhouse>
 EOL
 $CLICKHOUSE_CLIENT --query="SYSTEM RELOAD CONFIG"
-$CLICKHOUSE_CLIENT --query="SELECT shard_num FROM system.clusters WHERE cluster='test_static_shard_nums'"
+$CLICKHOUSE_CLIENT --query="SELECT shard_name FROM system.clusters WHERE cluster='test_shard_names'"
 
-## Test static nodes numbers
+## Test shard name
+cat > "$config_path" <<EOL
+<clickhouse>
+    <remote_servers>
+        <test_shard_names>
+            <shard>
+                <shard_name>3</shard_name>
+                <replica>
+                    <host>localhost</host>
+                    <port>9000</port>
+                </replica>
+            </shard>
+            <shard>
+                <shard_name>10</shard_name>
+                <replica>
+                    <host>localhost</host>
+                    <port>1</port>
+                </replica>
+            </shard>
+        </test_shard_names>
+    </remote_servers>
+</clickhouse>
+EOL
+$CLICKHOUSE_CLIENT --query="SYSTEM RELOAD CONFIG"
+$CLICKHOUSE_CLIENT --query="SELECT shard_name FROM system.clusters WHERE cluster='test_shard_names'"
+
+## Test shard name for nodes
 cat > "$config_path" <<EOL
 <clickhouse>
      <remote_servers>
-         <test_static_nodes_nums>
+         <test_shard_names>
              <node>
-                 <shard_number>3</shard_number>
+                 <shard_name>3</shard_name>
                  <host>localhost</host>
                  <port>9000</port>
              </node>
              <node>
-                 <shard_number>6</shard_number>
+                 <shard_name>6</shard_name>
                  <host>localhost</host>
                  <port>9000</port>
              </node>
-         </test_static_nodes_nums>
+         </test_shard_names>
      </remote_servers>
  </clickhouse>
 EOL
 $CLICKHOUSE_CLIENT --query="SYSTEM RELOAD CONFIG"
-$CLICKHOUSE_CLIENT --query="SELECT shard_num FROM system.clusters WHERE cluster='test_static_nodes_nums'"
+$CLICKHOUSE_CLIENT --query="SELECT shard_name FROM system.clusters WHERE cluster='test_shard_names'"
 
 
-## Test incorrect shard number, 0-based.
+# Test incorrect shard name, empty.
 cat > "$config_path" <<EOL
 <clickhouse>
      <remote_servers>
-         <test_static_nodes_nums>
+         <test_shard_names>
              <node>
-                 <shard_number>0</shard_number>
+                 <shard_name></shard_name>
                  <host>localhost</host>
                  <port>9000</port>
              </node>
-         </test_static_nodes_nums>
+         </test_shard_names>
      </remote_servers>
  </clickhouse>
 EOL
@@ -76,18 +103,18 @@ $CLICKHOUSE_CLIENT --query="SYSTEM RELOAD CONFIG; -- { serverError INVALID_SHARD
 cat > "$config_path" <<EOL
 <clickhouse>
      <remote_servers>
-         <test_static_nodes_nums>
+         <test_shard_names>
              <node>
-                 <shard_number>3</shard_number>
+                 <shard_name>3</shard_name>
                  <host>localhost</host>
                  <port>9000</port>
              </node>
              <node>
-                 <shard_number>3</shard_number>
+                 <shard_name>3</shard_name>
                  <host>localhost</host>
                  <port>9000</port>
              </node>
-         </test_static_nodes_nums>
+         </test_shard_names>
      </remote_servers>
  </clickhouse>
 EOL
