9072d1bd425cc322deaa3cc33b4b7582c387ca1a
Azat Khuzhin <a3at.mail@gmail.com>
2024-12-10 17:37:37 +0100

    Fix possible overestimate memory tracking
    Before this patch the untracked_memory was dumped before reseting the
    current_thread which could lead to leaking some of untracked_memory in
    `ThreadStatus::detachFromGroup` [1].
    
    In particular I found that the local_data (which is 136 bytes) can be
    "leaked" that way.
    
      [1]: https://github.com/ClickHouse/ClickHouse/blob/8999fdd95ea98fa083133d5cdf64ee149cd7f864/src/Interpreters/ThreadStatusExt.cpp#L292-L306
    
    This pops up on one of clusters where I noticed that the difference
    between MemoryTracking and MemoryResident keeps growing over the time.
    the difference grows ~200KiB per second - 16.5GiB per day.
    
    That cluster has tons MVs triggered from Buffer/Distributed (i.e. now
    query context), and this is indeed is the reason for leaking local_data,
    since in this case there is no thread group in the query, and it is
    created explicitly in `InterpreterInsertQuery::buildChain` [1].
    
      [1]: https://github.com/ClickHouse/ClickHouse/blob/20ccb638ba00103f116855afbc473950ef2b867b/src/Interpreters/InterpreterInsertQuery.cpp#L321
    
    Signed-off-by: Azat Khuzhin <a.khuzhin@semrush.com>

--- src/Common/ThreadStatus.cpp
+++ src/Common/ThreadStatus.cpp
@@ -216,8 +216,6 @@ size_t ThreadStatus::getNextPipelineProcessorIndex() const
 
 ThreadStatus::~ThreadStatus()
 {
-    flushUntrackedMemory();
-
     /// It may cause segfault if query_context was destroyed, but was not detached
     auto query_context_ptr = query_context.lock();
     assert((!query_context_ptr && getQueryId().empty()) || (query_context_ptr && getQueryId() == query_context_ptr->getCurrentQueryId()));
@@ -227,6 +225,9 @@ ThreadStatus::~ThreadStatus()
         deleter();
 
     chassert(!check_current_thread_on_destruction || current_thread == this);
+
+    /// Flush untracked_memory **before** switching the current_thread (to avoid losing untracked_memory)
+    flushUntrackedMemory();
 
     /// Only change current_thread if it's currently being used by this ThreadStatus
     /// For example, PushingToViews chain creates and deletes ThreadStatus instances while running in the main query thread
