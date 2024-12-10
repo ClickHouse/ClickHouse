bdcfc60d7e167eae3b872b37445cc1a7af99e270
Pablo Marcos <pablo.marcos.oltra@clickhouse.com>
2024-12-10 15:30:34 +0000

    Fix for real the arg issue
    Turns out that timeout takes a while to spawn the process
    when the number of arguments is huge. I wonder how this
    ever worked before. Maybe it was flaky?

--- docker/test/fuzzer/run-fuzzer.sh
+++ docker/test/fuzzer/run-fuzzer.sh
@@ -283,21 +283,30 @@ EOF
         exit 1
     fi
 
-    # Workaround to pass that many arguments through the command line. While it works without this
-    # when running the process in the foreground, it fails when we run it in the background, for
-    # some reason.
-    echo $FUZZER_ARGS > fuzzer_args.txt
-
     timeout -s TERM --preserve-status 30m clickhouse-client \
         --max_memory_usage_in_client=1000000000 \
         --receive_timeout=10 \
         --receive_data_timeout_ms=10000 \
         --stacktrace \
-        $(< fuzzer_args.txt) \
+        $FUZZER_ARGS \
         > fuzzer.log \
         2>&1 &
     fuzzer_pid=$!
     echo "Fuzzer pid is $fuzzer_pid"
+
+    # We need to give timeout some time to execute the underlying command with that many arguments
+    elapsed=0
+    maximum=50
+    while [[ $elapsed -lt $maximum ]]; do
+        if ps -o pid= --pid "$fuzzer_pid"; then
+            echo "Found underlying PID!"
+            break;
+        else
+            echo "Not found. Trying again..."
+        fi
+        sleep 0.1
+        elapsed=$((elapsed+1))
+    done
 
     # The fuzzer_pid belongs to the timeout process.
     actual_fuzzer_pid=$(ps -o pid= --ppid "$fuzzer_pid")
