026235aad4502497ed5a806c286c0a953d041818
Anton Ivashkin <ianton@live.com>
2024-12-11 00:16:42 +0100

    Keep initial_query_id for remote with INITIAL_QUERY

--- src/Interpreters/Context.cpp
+++ src/Interpreters/Context.cpp
@@ -2695,8 +2695,11 @@ void Context::setCurrentQueryId(const String & query_id)
 
     client_info.current_query_id = query_id_to_set;
 
-    if (client_info.query_kind == ClientInfo::QueryKind::INITIAL_QUERY)
+    if (client_info.query_kind == ClientInfo::QueryKind::INITIAL_QUERY
+        && client_info.initial_query_id.empty())
+    {
         client_info.initial_query_id = client_info.current_query_id;
+    }
 }
 
 void Context::setBackgroundOperationTypeForContext(ClientInfo::BackgroundOperationType background_operation)
