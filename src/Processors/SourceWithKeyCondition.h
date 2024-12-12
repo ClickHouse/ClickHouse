3d1b1ba2f6bc148470b40866db91989b35b62c9c
Arthur Passos <arthur.ti@outlook.com>
2024-12-12 13:55:35 -0300

    updt

--- src/Processors/SourceWithKeyCondition.h
+++ src/Processors/SourceWithKeyCondition.h
@@ -16,12 +16,10 @@ protected:
     /// Represents pushed down filters in source
     std::shared_ptr<const KeyCondition> key_condition;
 
-    void setKeyConditionImpl(const ActionsDAGPtr & filter_actions_dag, ContextPtr context, const Block & keys)
+    void setKeyConditionImpl(const std::optional<ActionsDAG> & filter_actions_dag, ContextPtr context, const Block & keys)
     {
         key_condition = std::make_shared<const KeyCondition>(
             filter_actions_dag ? &*filter_actions_dag : nullptr,
-            context,
-            filter_actions_dag,
             context,
             keys.getNames(),
             std::make_shared<ExpressionActions>(ActionsDAG(keys.getColumnsWithTypeAndName())));
