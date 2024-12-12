8448a691388abe7d3e16ac0ee3cd6b0f82e4d992
robot-clickhouse <robot-clickhouse@users.noreply.github.com>
2024-12-12 17:11:46 +0000

    Backport #72545 to 24.9: fix async insert in TCP

--- src/Server/TCPHandler.cpp
+++ src/Server/TCPHandler.cpp
@@ -474,6 +474,7 @@ void TCPHandler::runImpl()
                 state.logs_queue->setSourceRegexp(query_context->getSettingsRef()[Setting::send_logs_source_regexp]);
                 CurrentThread::attachInternalTextLogsQueue(state.logs_queue, client_logs_level);
             }
+
             if (client_tcp_protocol_version >= DBMS_MIN_PROTOCOL_VERSION_WITH_INCREMENTAL_PROFILE_EVENTS)
             {
                 state.profile_queue = std::make_shared<InternalProfileEventsQueue>(std::numeric_limits<int>::max());
@@ -1044,6 +1045,7 @@ void TCPHandler::processInsertQuery()
         if (result.status == AsynchronousInsertQueue::PushResult::OK)
         {
             /// Reset pipeline because it may hold write lock for some storages.
+            state.io.pipeline.cancel();
             state.io.pipeline.reset();
             if (settings[Setting::wait_for_async_insert])
             {
