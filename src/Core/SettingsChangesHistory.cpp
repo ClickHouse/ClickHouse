5c62f77a86ea72dd1d12c80c68b41bebb2211107
lgbo-ustc <lgbo.ustc@gmail.com>
2024-12-13 08:59:07 +0800

    update setting history

--- src/Core/SettingsChangesHistory.cpp
+++ src/Core/SettingsChangesHistory.cpp
@@ -100,6 +100,11 @@ const VersionToSettingsChangesMap & getSettingsChangesHistory()
             {"allow_experimental_prql_dialect", true, false, "A new setting"},
         });
         addSettingsChanges(settings_changes_history, "24.12",
+
+            {"enable_adaptive_memory_spill_scheduler", false, false, "New setting. Enable spill memory data into external storage adaptively."},
+        }
+    },
+    {"24.12",
         {
             /// Release closed. Please use 25.1
             {"allow_experimental_database_iceberg", false, false, "New setting."},
