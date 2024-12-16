18ba61c00b5478394b1c86f8f1004d5bb2a1c5e6
Raúl Marín <git@rmr.ninja>
2024-12-16 19:33:30 +0100

    Fix build

--- src/Core/Settings.cpp
+++ src/Core/Settings.cpp
@@ -25,7 +25,7 @@
 constexpr UInt64 default_max_size_to_drop = 50000000000lu;
 constexpr UInt64 default_distributed_cache_connect_max_tries = 100lu;
 #else
-constexpr UInt64 max_size_to_drop_default_value = 0lu;
+constexpr UInt64 default_max_size_to_drop = 0lu;
 constexpr UInt64 default_distributed_cache_connect_max_tries = DistributedCache::DEFAULT_CONNECT_MAX_TRIES;
 #endif
 
