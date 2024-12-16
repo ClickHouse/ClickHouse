ee913fbc66b49bba4492d01b0ebfb6c1a72eacf9
Robert Schulze <robert@clickhouse.com>
2024-12-16 19:01:24 +0000

    Add setting 'query_plan_try_use_vector_search' (forgot this in an earlier PR)

--- src/Processors/QueryPlan/Optimizations/QueryPlanOptimizationSettings.cpp
+++ src/Processors/QueryPlan/Optimizations/QueryPlanOptimizationSettings.cpp
@@ -30,6 +30,7 @@ namespace Setting
     extern const SettingsBool query_plan_remove_redundant_sorting;
     extern const SettingsBool query_plan_reuse_storage_ordering_for_window_functions;
     extern const SettingsBool query_plan_split_filter;
+    extern const SettingsBool query_plan_try_use_vector_search;
     extern const SettingsString force_optimize_projection_name;
     extern const SettingsUInt64 query_plan_max_optimizations_to_apply;
 }
@@ -55,6 +56,7 @@ QueryPlanOptimizationSettings QueryPlanOptimizationSettings::fromSettings(const 
     settings.lift_up_union = from[Setting::query_plan_enable_optimizations] && from[Setting::query_plan_lift_up_union];
     settings.aggregate_partitions_independently = from[Setting::query_plan_enable_optimizations] && from[Setting::allow_aggregate_partitions_independently];
     settings.remove_redundant_distinct = from[Setting::query_plan_enable_optimizations] && from[Setting::query_plan_remove_redundant_distinct];
+    settings.try_use_vector_search = from[Setting::query_plan_enable_optimizations] && from[Setting::query_plan_try_use_vector_search];
 
     settings.optimize_prewhere = from[Setting::query_plan_enable_optimizations] && from[Setting::query_plan_optimize_prewhere];
     settings.read_in_order = from[Setting::query_plan_enable_optimizations] && from[Setting::optimize_read_in_order] && from[Setting::query_plan_read_in_order];
