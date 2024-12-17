8dd87512dbd8a5399b55cd4fdc8ff6f981bae5db
Nikolai Kochetov <nk@clickhouse.com>
2024-12-17 16:31:54 +0100

    Update tests/queries/0_stateless/03286_parallel_replicas_cross_join_bug.sql
    Co-authored-by: Igor Nikonov <954088+devcrafter@users.noreply.github.com>

--- tests/queries/0_stateless/03286_parallel_replicas_cross_join_bug.sql
+++ tests/queries/0_stateless/03286_parallel_replicas_cross_join_bug.sql
@@ -2,7 +2,7 @@ drop table if exists tab;
 create table tab (x UInt64) engine = MergeTree order by tuple();
 insert into tab select number from numbers(1e7);
 
-set allow_experimental_parallel_reading_from_replicas = 1, max_parallel_replicas = 3, cluster_for_parallel_replicas = 'parallel_replicas', parallel_replicas_for_non_replicated_merge_tree = true, parallel_replicas_local_plan = false;
+set enable_parallel_replicas = 1, max_parallel_replicas = 3, cluster_for_parallel_replicas = 'parallel_replicas', parallel_replicas_for_non_replicated_merge_tree = true, parallel_replicas_local_plan = false;
 
 select * from tab l, tab r where l.x < r.x and r.x < 2;
 
