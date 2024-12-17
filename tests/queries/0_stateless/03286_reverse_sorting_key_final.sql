43eabb2b80702bdbcb2ef914cec2a9fa1115323e
Amos Bird <amosbird@gmail.com>
2024-12-17 15:55:30 +0800

    Make reverse key work with PartsSplitter.

---
+++ tests/queries/0_stateless/03286_reverse_sorting_key_final.sql
@@ -0,0 +1,23 @@
+DROP TABLE IF EXISTS t0;
+CREATE TABLE t0 (c0 Nested(c1 Int)) ENGINE = SummingMergeTree ORDER BY (c0.c1 DESC) SETTINGS allow_experimental_reverse_key = 1;
+INSERT INTO t0 (c0.c1) VALUES ([1]), ([2]);
+SELECT 1 FROM t0 FINAL;
+DROP TABLE t0;
+
+-- PartsSplitter is disabled when some keys are in ascending order while others are in descending order.
+CREATE TABLE t0(a Int, b Int) Engine=ReplacingMergeTree order by (a desc, b) SETTINGS allow_experimental_reverse_key = 1, index_granularity = 8192, index_granularity_bytes = '10Mi';
+INSERT INTO t0 select number, number from numbers(5);
+INSERT INTO t0 select number, number from numbers(5,2);
+set max_threads = 2;
+explain pipeline select * from t0 final SETTINGS enable_vertical_final = 0;
+select * from t0 final SETTINGS enable_vertical_final = 0;
+DROP TABLE t0;
+
+-- PartsSplitter should also work for reverse nullable keys.
+CREATE TABLE t0(a Nullable(Int), b Nullable(Int)) Engine=ReplacingMergeTree order by (a desc, b desc) SETTINGS allow_experimental_reverse_key = 1, allow_nullable_key = 1, index_granularity = 8192, index_granularity_bytes = '10Mi';
+INSERT INTO t0 select number, number from numbers(5);
+INSERT INTO t0 select null, null union all select number, number from numbers(5, 2);
+set max_threads = 2;
+explain pipeline select * from t0 final SETTINGS enable_vertical_final = 0;
+select * from t0 final SETTINGS enable_vertical_final = 0;
+DROP TABLE t0;
