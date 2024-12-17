8be824839d7d3f07f166f4ae124d3002d27eb06d
Antonio Andelic <antonio@clickhouse.com>
2024-12-17 10:14:39 +0100

    Fix ObjectStorageQueue with ZK and older Keeper

--- tests/integration/test_storage_s3_queue/test.py
+++ tests/integration/test_storage_s3_queue/test.py
@@ -100,7 +100,6 @@ def started_cluster():
             with_minio=True,
             with_azurite=True,
             with_zookeeper=True,
-            keeper_required_feature_flags=["create_if_not_exists"],
             main_configs=[
                 "configs/zookeeper.xml",
                 "configs/s3queue_log.xml",
@@ -112,7 +111,6 @@ def started_cluster():
             user_configs=["configs/users.xml"],
             with_minio=True,
             with_zookeeper=True,
-            keeper_required_feature_flags=["create_if_not_exists"],
             main_configs=[
                 "configs/s3queue_log.xml",
             ],
@@ -121,7 +119,6 @@ def started_cluster():
         cluster.add_instance(
             "old_instance",
             with_zookeeper=True,
-            keeper_required_feature_flags=["create_if_not_exists"],
             image="clickhouse/clickhouse-server",
             tag="23.12",
             stay_alive=True,
@@ -131,7 +128,6 @@ def started_cluster():
         cluster.add_instance(
             "node1",
             with_zookeeper=True,
-            keeper_required_feature_flags=["create_if_not_exists"],
             stay_alive=True,
             main_configs=[
                 "configs/zookeeper.xml",
@@ -142,7 +138,6 @@ def started_cluster():
         cluster.add_instance(
             "node2",
             with_zookeeper=True,
-            keeper_required_feature_flags=["create_if_not_exists"],
             stay_alive=True,
             main_configs=[
                 "configs/zookeeper.xml",
@@ -155,7 +150,6 @@ def started_cluster():
             user_configs=["configs/users.xml"],
             with_minio=True,
             with_zookeeper=True,
-            keeper_required_feature_flags=["create_if_not_exists"],
             main_configs=[
                 "configs/s3queue_log.xml",
                 "configs/merge_tree.xml",
@@ -165,7 +159,6 @@ def started_cluster():
         cluster.add_instance(
             "instance_24.5",
             with_zookeeper=True,
-            keeper_required_feature_flags=["create_if_not_exists"],
             image="clickhouse/clickhouse-server",
             tag="24.5",
             stay_alive=True,
@@ -178,7 +171,6 @@ def started_cluster():
         cluster.add_instance(
             "node_cloud_mode",
             with_zookeeper=True,
-            keeper_required_feature_flags=["create_if_not_exists"],
             stay_alive=True,
             main_configs=[
                 "configs/zookeeper.xml",
