acbafdbe28f9949ecf0a6f1c59ca63416bd6a271
Kseniia Sumarokova <54203879+kssenii@users.noreply.github.com>
2024-12-17 10:59:45 +0100

    Update DistributedCacheDefines.h

--- src/Core/DistributedCacheDefines.h
+++ src/Core/DistributedCacheDefines.h
@@ -17,13 +17,4 @@ static constexpr auto ACK_DATA_PACKET_WINDOW = 5;
 static constexpr auto DEFAULT_CONNECTION_POOL_SIZE = 15000;
 static constexpr auto DEFAULT_CONNECTION_TTL_SEC = 200;
 
-static constexpr auto INITIAL_PROTOCOL_VERSION = 0;
-static constexpr auto PROTOCOL_VERSION_WITH_QUERY_ID = 1;
-static constexpr auto PROTOCOL_VERSION_WITH_MAX_INFLIGHT_PACKETS = 2;
-static constexpr auto PROTOCOL_VERSION_WITH_GCS_TOKEN = 3;
-static constexpr UInt32 PROTOCOL_VERSION_WITH_AZURE_AUTH = 4;
-static constexpr UInt32 PROTOCOL_VERSION_WITH_TEMPORATY_DATA = 5;
-
-static constexpr UInt32 CURRENT_PROTOCOL_VERSION = PROTOCOL_VERSION_WITH_TEMPORATY_DATA;
-
 }
