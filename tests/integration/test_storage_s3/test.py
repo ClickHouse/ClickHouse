bafd2ad83e06e2797eee41b764cea1bf420add8c
Konstantin Bogdanov <konstantin@clickhouse.com>
2024-12-18 16:38:31 +0100

    Do not use list for single range glob

--- tests/integration/test_storage_s3/test.py
+++ tests/integration/test_storage_s3/test.py
@@ -647,7 +647,6 @@ def test_s3_glob_scheherazade(started_cluster):
     bucket = started_cluster.minio_bucket
     instance = started_cluster.instances["dummy"]  # type: ClickHouseInstance
     table_format = "column1 UInt32, column2 UInt32, column3 UInt32"
-    max_path = ""
     values = "(1, 1, 1)"
     nights_per_job = 1001 // 30
     jobs = []
@@ -683,6 +682,52 @@ def test_s3_glob_scheherazade(started_cluster):
         table_format,
     )
     assert run_query(instance, query).splitlines() == ["1001\t1001\t1001\t1001"]
+
+
+def test_s3_trivial_glob_should_not_list(started_cluster):
+    bucket = started_cluster.minio_bucket
+    instance = started_cluster.instances["dummy"]  # type: ClickHouseInstance
+    table_format = "column1 UInt32, column2 UInt32, column3 UInt32"
+    values = "(1, 1, 1)"
+    nights_per_job = 1001 // 30
+    jobs = []
+    for night in range(0, 1001, nights_per_job):
+
+        def add_tales(start, end):
+            for i in range(start, end):
+                path = "night_{}/tale.csv".format(i)
+                query = "insert into table function s3('http://{}:{}/{}/{}', 'CSV', '{}') values {}".format(
+                    started_cluster.minio_ip,
+                    MINIO_INTERNAL_PORT,
+                    bucket,
+                    path,
+                    table_format,
+                    values,
+                )
+                run_query(instance, query)
+
+        jobs.append(
+            threading.Thread(
+                target=add_tales, args=(night, min(night + nights_per_job, 1001))
+            )
+        )
+        jobs[-1].start()
+
+    for job in jobs:
+        job.join()
+
+    query = "select count(), sum(column1), sum(column2), sum(column3) from s3('http://{}:{}/{}/night_{{0,1,2,3,4,5,6,7,8,9,10}}/tale.csv', 'CSV', '{}')".format(
+        started_cluster.minio_redirect_host,
+        started_cluster.minio_redirect_port,
+        bucket,
+        table_format,
+    )
+    query_id = "validate_no_s3_list_requests"
+    assert run_query(instance, query, query_id=query_id).splitlines() == ["11\t11\t11\t11"]
+
+    instance.query("SYSTEM FLUSH LOGS")
+    list_request_count = instance.query(f"select ProfileEvents['S3ListObjects'] from system.query_log where query_id = '{query_id}' and type = 'QueryFinish'")
+    assert list_request_count == "0\n"
 
 
 # a bit simplified version of scheherazade test
