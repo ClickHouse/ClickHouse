eb840523b2b66fd9987316753d069fa3c16b9402
Robert Schulze <robert@clickhouse.com>
2024-12-18 14:01:22 +0000

    Support collations with modifiers

---
+++ tests/queries/0_stateless/28323_collation_locale_with_modifier.sql
@@ -0,0 +1,7 @@
+SELECT 'Sort by standard Turkish locale';
+SELECT arrayJoin(['kk 50', 'KK 01', '    KK 2', '  KK    3', 'kk 1', 'x9y99', 'x9y100']) item
+ORDER BY item ASC COLLATE 'tr';
+
+SELECT 'Sort by Turkish locale with modifiers';
+SELECT arrayJoin(['kk 50', 'KK 01', '    KK 2', '  KK    3', 'kk 1', 'x9y99', 'x9y100']) item
+ORDER BY item ASC COLLATE 'tr-u-kn-true-ka-shifted';
