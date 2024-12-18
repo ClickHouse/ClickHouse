1a036ad615abe420fe32caa45ba985011af5dd9b
taiyang-li <654010905@qq.com>
2024-12-18 17:05:25 +0800

    fix building

--- src/DataTypes/Native.h
+++ src/DataTypes/Native.h
@@ -31,7 +31,7 @@ bool canBeNativeType(const IDataType & type);
 bool canBeNativeType(const DataTypePtr & type);
 
 template <typename Type>
-static inline bool canBeNativeType()
+static constexpr bool canBeNativeType()
 {
     if constexpr (std::is_same_v<Type, Float32> || std::is_same_v<Type, Float64>)
         return true;
