862e924de6b35469f325672253168bc6134028d6
robot-clickhouse <robot-clickhouse@users.noreply.github.com>
2024-12-18 16:23:32 +0000

    Automatic style fix

--- ci/praktika/mangle.py
+++ ci/praktika/mangle.py
@@ -68,7 +68,9 @@ def _update_workflow_artifacts(workflow):
         if artifact.name in artifact_job:
             artifact._provided_by = artifact_job[artifact.name]
         else:
-            print(f"WARNING: Artifact [{artifact.name}] in workflow [{workflow.name}] has no job that provides it")
+            print(
+                f"WARNING: Artifact [{artifact.name}] in workflow [{workflow.name}] has no job that provides it"
+            )
 
 
 def _update_workflow_with_native_jobs(workflow):
