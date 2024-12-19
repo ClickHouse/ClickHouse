0e8ccfb82e4a0040ae811a009863961ba2884023
Arthur Passos <arthur.ti@outlook.com>
2024-12-19 19:44:20 -0300

    parallel encoding does not affect

--- tests/queries/0_stateless/03276_parquet_output_compression_level.sql
+++ tests/queries/0_stateless/03276_parquet_output_compression_level.sql
@@ -1,7 +1,7 @@
 -- Tags: no-parallel
 
-insert into function file(03276_parquet_compression_level_1.parquet) SETTINGS output_format_parquet_compression_method = 'zstd', output_format_parquet_compression_level=1,output_format_parquet_parallel_encoding=0, output_format_parquet_use_custom_encoder=0 SELECT number AS id, toString(number) AS name, now() + number AS timestamp FROM numbers(100000);
-insert into function file(03276_parquet_compression_level_22.parquet) SETTINGS output_format_parquet_compression_method = 'zstd', output_format_parquet_compression_level=22,output_format_parquet_parallel_encoding=0, output_format_parquet_use_custom_encoder=0 SELECT number AS id, toString(number) AS name, now() + number AS timestamp FROM numbers(100000);
+insert into function file(03276_parquet_compression_level_1.parquet) SETTINGS output_format_parquet_compression_method = 'zstd', output_format_parquet_compression_level=1, output_format_parquet_use_custom_encoder=0 SELECT number AS id, toString(number) AS name, now() + number AS timestamp FROM numbers(100000);
+insert into function file(03276_parquet_compression_level_22.parquet) SETTINGS output_format_parquet_compression_method = 'zstd', output_format_parquet_compression_level=22, output_format_parquet_use_custom_encoder=0 SELECT number AS id, toString(number) AS name, now() + number AS timestamp FROM numbers(100000);
 
 WITH
     (SELECT total_compressed_size FROM file(03276_parquet_compression_level_1.parquet, ParquetMetadata)) AS size_level_1,
