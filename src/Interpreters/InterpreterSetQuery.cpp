c53f1ac16bb0bfbad71b55cb1ef3ffe4f4c4f3fd
Raúl Marín <git@rmr.ninja>
2024-12-20 19:00:05 +0100

    WIP

--- src/Interpreters/InterpreterSetQuery.cpp
+++ src/Interpreters/InterpreterSetQuery.cpp
@@ -1,17 +1,25 @@
+#include <Core/Settings.h>
 #include <Interpreters/Context.h>
 #include <Interpreters/InterpreterFactory.h>
 #include <Interpreters/InterpreterSetQuery.h>
-#include <Parsers/ASTSelectQuery.h>
-#include <Parsers/ASTSetQuery.h>
 #include <Parsers/ASTCreateQuery.h>
 #include <Parsers/ASTExplainQuery.h>
+#include <Parsers/ASTFunction.h>
 #include <Parsers/ASTInsertQuery.h>
 #include <Parsers/ASTQueryWithOutput.h>
+#include <Parsers/ASTSelectQuery.h>
 #include <Parsers/ASTSelectWithUnionQuery.h>
-
+#include <Parsers/ASTSetQuery.h>
+#include <Storages/MemorySettings.h>
+#include <Storages/MergeTree/MergeTreeSettings.h>
 
 namespace DB
 {
+
+namespace Setting
+{
+    extern const SettingsDefaultTableEngine default_table_engine;
+}
 
 namespace ErrorCodes
 {
@@ -53,6 +61,35 @@ static void applySettingsFromSelectWithUnion(const ASTSelectWithUnionQuery & sel
         InterpreterSetQuery(last_select->settings(), context).executeForCurrentContext(/* ignore_setting_constraints= */ false);
 }
 
+namespace
+{
+std::optional<String> getTableStorageName(const ASTCreateQuery & create, ContextMutablePtr context)
+{
+    if ((create.database && !create.table) || create.isView())
+        return {};
+    if (create.storage->engine)
+        return create.storage->engine->name;
+    switch (context->getSettingsRef()[Setting::default_table_engine])
+    {
+        case DefaultTableEngine::None:
+            return {};
+        case DefaultTableEngine::Log:
+        case DefaultTableEngine::StripeLog:
+            return "Log";
+        case DefaultTableEngine::MergeTree:
+        case DefaultTableEngine::ReplacingMergeTree:
+        case DefaultTableEngine::ReplicatedMergeTree:
+        case DefaultTableEngine::ReplicatedReplacingMergeTree:
+        case DefaultTableEngine::SharedMergeTree:
+        case DefaultTableEngine::SharedReplacingMergeTree:
+            return "MergeTree";
+        case DefaultTableEngine::Memory:
+            return "Memory";
+    }
+}
+}
+
+
 void InterpreterSetQuery::applySettingsFromQuery(const ASTPtr & ast, ContextMutablePtr context_)
 {
     if (!ast)
@@ -69,27 +106,54 @@ void InterpreterSetQuery::applySettingsFromQuery(const ASTPtr & ast, ContextMuta
             if (create_query->select)
                 applySettingsFromSelectWithUnion(create_query->select->as<ASTSelectWithUnionQuery &>(), context_);
             else if (
-                !create_query->settings_ast && create_query->storage && !create_query->storage->engine && create_query->storage->settings
+                !create_query->settings_ast && create_query->storage && create_query->storage->settings
                 && context_->getApplicationType() != Context::ApplicationType::CLIENT)
             {
-                /// If we parsed one set of settings and no engine we don't know if they belong to the query
-                /// or to the engine. To disambiguate, we try to apply them in the context, if it works then we move
-                /// them there; if it doesn't we assume they are engine settings.
-                /// Note that this only works in the server, since the client ignores unknown settings (for compatibility reasons)
-                try
-                {
-                    ASTPtr set = create_query->storage->settings->shared_from_this();
-                    InterpreterSetQuery(set, context_).executeForCurrentContext(/* ignore_setting_constraints= */ false);
+                /// If we parsed one set of settings we don't know if it was the engine settings or the query settings
+                /// We also want to allow users to mix them (so they don't need to declare SETTINGS engine_setting=0 SETTINGS query_setting=0
+                /// So what we are going to do now is to check if each setting belongs to the engine or not, and if it doesn't
+                /// then move it to the context
 
-                    auto * create_query_mutable = const_cast<ASTCreateQuery *>(create_query);
-                    auto * storage_mutable = const_cast<ASTStorage *>(create_query->storage);
-                    create_query_mutable->settings_ast = set;
-                    storage_mutable->settings = nullptr;
-                }
-                catch (const Exception & e)
+                ASTSetQuery * engine_settings = create_query->storage->settings;
+                auto storage_name = getTableStorageName(*create_query, context_);
+                if (storage_name->contains("Memory"))
                 {
-                    if (e.code() != ErrorCodes::UNKNOWN_SETTING)
-                        throw;
+                    /// TODO: Support MemorySettings.has()
+                    // MemorySettings memory_settings;
+                    // for (auto it = engine_settings->changes.begin(); it != engine_settings->changes.end();)
+                    // {
+                    //     String & name = it->name;
+                    //     if (!memory_settings.has(name) && context_->getSettingsRef().has(name))
+                    //     {
+                    //         context_->setSetting(name, it->value);
+                    //         it = engine_settings->changes.erase(it);
+                    //     }
+                    //     else
+                    //     {
+                    //         it++;
+                    //     }
+                    // }
+                }
+                else if (storage_name->contains(("MergeTree")))
+                {
+                    MergeTreeSettings mergetree_settings;
+                    for (auto it = engine_settings->changes.begin(); it != engine_settings->changes.end();)
+                    {
+                        String & name = it->name;
+                        if (!mergetree_settings.has(name) && context_->getSettingsRef().has(name))
+                        {
+                            context_->setSetting(name, it->value);
+                            it = engine_settings->changes.erase(it);
+                        }
+                        else
+                        {
+                            it++;
+                        }
+                    }
+                }
+                else if (storage_name->contains("..."))
+                {
+                    /// TODO: Pending for other storages. Maybe uniformize it somehow with a template or similar
                 }
             }
         }
