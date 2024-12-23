4161f630656a4952c6553c8da7828f2132ef8430
Mikhail Koviazin <mikhail.koviazin@aiven.io>
2024-12-23 17:45:42 +0100

    Set Kafka comment
    This is a subset of changes that were upstreamed with https://github.com/ClickHouse/ClickHouse/pull/66832
    
    This commit was applied from the patch file 0030-Kafka_Table_Comment.patch
    
    Co-authored-by: Kevin Michel <kevin.michel@aiven.io>
    Co-authored-by: Joe Lynch <joe.lynch@aiven.io>
    Signed-off-by: Mikhail Koviazin <mikhail.koviazin@aiven.io>

--- src/Storages/Kafka/StorageKafka.cpp
+++ src/Storages/Kafka/StorageKafka.cpp
@@ -330,7 +330,7 @@ namespace
 
 StorageKafka::StorageKafka(
     const StorageID & table_id_, ContextPtr context_,
-    const ColumnsDescription & columns_, std::unique_ptr<KafkaSettings> kafka_settings_,
+    const ColumnsDescription & columns_, const String & comment, std::unique_ptr<KafkaSettings> kafka_settings_,
     const String & collection_name_)
     : IStorage(table_id_)
     , WithContext(context_->getGlobalContext())
@@ -362,6 +362,7 @@ StorageKafka::StorageKafka(
 
     StorageInMemoryMetadata storage_metadata;
     storage_metadata.setColumns(columns_);
+    storage_metadata.setComment(comment);
     setInMemoryMetadata(storage_metadata);
     setVirtuals(createVirtuals(kafka_settings->kafka_handle_error_mode));
 
@@ -1214,7 +1215,7 @@ void registerStorageKafka(StorageFactory & factory)
                                                        "See https://clickhouse.com/docs/en/engines/table-engines/integrations/kafka/#configuration");
         }
 
-        return std::make_shared<StorageKafka>(args.table_id, args.getContext(), args.columns, std::move(kafka_settings), collection_name);
+        return std::make_shared<StorageKafka>(args.table_id, args.getContext(), args.columns, args.comment, std::move(kafka_settings), collection_name);
     };
 
     factory.registerStorage("Kafka", creator_fn, StorageFactory::StorageFeatures{ .supports_settings = true, });
