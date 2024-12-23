42716b1579f74133c838c4e2f9846aa6894476fa
Igor Nikonov <igor@clickhouse.com>
2024-12-23 12:22:01 +0000

    Clean up

--- tests/queries/0_stateless/03290_pr_non_replicated_in_subquery.sql
+++ tests/queries/0_stateless/03290_pr_non_replicated_in_subquery.sql
@@ -1,6 +1,5 @@
 DROP TABLE IF EXISTS table1;
 
 SYSTEM FLUSH LOGS;
--- select query_kind, tables, query from system.query_log where has(databases, currentDatabase());
 select count() from (select * from system.query_log where has(databases, currentDatabase()) and tables[1] ilike '%table1' and query_kind = 'Drop' and current_database = currentDatabase());
 select count() from (select * from system.query_log where has(databases, currentDatabase()) and tables[1] ilike '%table1' and query_kind = 'Drop' and current_database = currentDatabase()) settings enable_parallel_replicas=1, max_parallel_replicas=2, cluster_for_parallel_replicas='parallel_replicas';
