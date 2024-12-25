c9bd22316bde820fe5564d4aa24e87f4271bb69b
zhanglistar <zhanglinuxstar@gmail.com>
2024-12-25 09:14:28 +0800

    fix style failure

--- src/Functions/FunctionsStringSearch.h
+++ src/Functions/FunctionsStringSearch.h
@@ -68,6 +68,7 @@ namespace ErrorCodes
     extern const int ILLEGAL_COLUMN;
     extern const int ILLEGAL_TYPE_OF_ARGUMENT;
     extern const int NUMBER_OF_ARGUMENTS_DOESNT_MATCH;
+    extern const int LOGICAL_ERROR;
 }
 
 enum class ExecutionErrorPolicy : uint8_t
