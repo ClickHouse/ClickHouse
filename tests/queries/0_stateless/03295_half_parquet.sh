9fda6d91335917f26a67acdf96dc2d3d6b87efd3
Alexey Milovidov <milovidov@clickhouse.com>
2024-12-25 16:50:25 +0100

    Add a test

---
+++ tests/queries/0_stateless/03295_half_parquet.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/env bash
+
+CUR_DIR=$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)
+# shellcheck source=../shell_config.sh
+. "$CUR_DIR"/../shell_config.sh
+
+$CLICKHOUSE_LOCAL "SELECT * FROM 'data_parquet/example_half_float.parquet'"
