9fd150a71984456b248fe7512adf67485a9a98c6
Alexey Milovidov <milovidov@clickhouse.com>
2024-12-26 14:29:16 +0100

    Autodetect input compression in CLI

---
+++ tests/queries/0_stateless/03297_input_compression_autodetect.sh
@@ -0,0 +1,21 @@
+#!/usr/bin/env bash
+
+CUR_DIR=$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)
+# shellcheck source=../shell_config.sh
+. "$CUR_DIR"/../shell_config.sh
+
+$CLICKHOUSE_LOCAL --query "SELECT number FROM numbers(10)" > numbers.jsonl.gz
+$CLICKHOUSE_LOCAL --query "SELECT * FROM table" < numbers.jsonl.gz
+$CLICKHOUSE_LOCAL --query "SELECT * FROM table" < numbers.jsonl.gz > numbers.csv.bz2
+$CLICKHOUSE_LOCAL --copy < numbers.csv.bz2 > numbers.parquet
+$CLICKHOUSE_LOCAL --copy < numbers.parquet
+
+echo '---'
+
+$CLICKHOUSE_CLIENT --query "DROP TABLE IF EXISTS test"
+$CLICKHOUSE_CLIENT --query "CREATE TABLE test (number UInt64) ENGINE = Memory"
+$CLICKHOUSE_CLIENT --query "INSERT INTO test FORMAT JSONLines" < numbers.jsonl.gz
+$CLICKHOUSE_CLIENT --query "INSERT INTO test FORMAT CSV" < numbers.csv.bz2
+$CLICKHOUSE_CLIENT --query "INSERT INTO test SELECT c1 FROM input() FORMAT Parquet" < numbers.parquet
+$CLICKHOUSE_CLIENT --query "SELECT * FROM test"
+$CLICKHOUSE_CLIENT --query "DROP TABLE test"
