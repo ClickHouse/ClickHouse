2e0411df208342b811e6ceb19e1f23242b480a8a
Alexey Milovidov <milovidov@clickhouse.com>
2024-12-26 23:14:54 +0100

    Add a metric on the number of currently merging source parts

--- tests/queries/0_stateless/03300_merge_parts_metric.sh
+++ tests/queries/0_stateless/03300_merge_parts_metric.sh
@@ -5,7 +5,7 @@ CUR_DIR=$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)
 . "$CUR_DIR"/../shell_config.sh
 
 # Among 1000 invocations, the values are equal most of the time
-# Note: the could be not equal due to timing differences.
+# Note: they could be non-equal due to timing differences.
 
 $CLICKHOUSE_CLIENT --query "DROP TABLE IF EXISTS test; CREATE TABLE test (x Bool) ENGINE = Memory;"
 yes "INSERT INTO test SELECT sum(length(source_part_names)) = (SELECT value FROM system.metrics WHERE name = 'MergeParts') FROM system.merges;" | head -n1000 | $CLICKHOUSE_CLIENT
