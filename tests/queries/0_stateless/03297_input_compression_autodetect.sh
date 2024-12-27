53643635687bb5bce04b9d847293e475096f711e
Alexey Milovidov <milovidov@clickhouse.com>
2024-12-27 01:16:48 +0100

    Fix test

--- tests/queries/0_stateless/03297_input_compression_autodetect.sh
+++ tests/queries/0_stateless/03297_input_compression_autodetect.sh
@@ -5,18 +5,18 @@ CUR_DIR=$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)
 # shellcheck source=../shell_config.sh
 . "$CUR_DIR"/../shell_config.sh
 
-$CLICKHOUSE_LOCAL --query "SELECT number FROM numbers(10)" > numbers.jsonl.gz
-$CLICKHOUSE_LOCAL --query "SELECT * FROM table" < numbers.jsonl.gz
-$CLICKHOUSE_LOCAL --query "SELECT * FROM table" < numbers.jsonl.gz > numbers.csv.bz2
-$CLICKHOUSE_LOCAL --copy < numbers.csv.bz2 > numbers.parquet
-$CLICKHOUSE_LOCAL --copy < numbers.parquet
+$CLICKHOUSE_LOCAL --query "SELECT number FROM numbers(10)" > "${CLICKHOUSE_TMP}/numbers.jsonl.gz"
+$CLICKHOUSE_LOCAL --query "SELECT * FROM table" < "${CLICKHOUSE_TMP}/numbers.jsonl.gz"
+$CLICKHOUSE_LOCAL --query "SELECT * FROM table" < "${CLICKHOUSE_TMP}/numbers.jsonl.gz" > "${CLICKHOUSE_TMP}/numbers.csv.bz2"
+$CLICKHOUSE_LOCAL --copy < "${CLICKHOUSE_TMP}/numbers.csv.bz2" > "${CLICKHOUSE_TMP}/numbers.parquet"
+$CLICKHOUSE_LOCAL --copy < "${CLICKHOUSE_TMP}/numbers.parquet"
 
 echo '---'
 
 $CLICKHOUSE_CLIENT --query "DROP TABLE IF EXISTS test"
 $CLICKHOUSE_CLIENT --query "CREATE TABLE test (number UInt64) ENGINE = Memory"
-$CLICKHOUSE_CLIENT --query "INSERT INTO test FORMAT JSONLines" < numbers.jsonl.gz
-$CLICKHOUSE_CLIENT --query "INSERT INTO test FORMAT CSV" < numbers.csv.bz2
-$CLICKHOUSE_CLIENT --query "INSERT INTO test SELECT c1 FROM input() FORMAT Parquet" < numbers.parquet
+$CLICKHOUSE_CLIENT --query "INSERT INTO test FORMAT JSONLines" < "${CLICKHOUSE_TMP}/numbers.jsonl.gz"
+$CLICKHOUSE_CLIENT --query "INSERT INTO test FORMAT CSV" < "${CLICKHOUSE_TMP}/numbers.csv.bz2"
+$CLICKHOUSE_CLIENT --query "INSERT INTO test SELECT c1 FROM input() FORMAT Parquet" < "${CLICKHOUSE_TMP}/numbers.parquet"
 $CLICKHOUSE_CLIENT --query "SELECT * FROM test"
 $CLICKHOUSE_CLIENT --query "DROP TABLE test"
