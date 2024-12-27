b5df1b50a6b5570441875e4ef87ae80baa861311
robot-clickhouse <robot-clickhouse@users.noreply.github.com>
2024-12-27 16:00:46 +0000

    Automatic style fix

--- tests/queries/0_stateless/03279_with_clickhouse_driver.py
+++ tests/queries/0_stateless/03279_with_clickhouse_driver.py
@@ -13,10 +13,14 @@ def run(database):
     )
 
     client.execute(f"INSERT INTO {database}.test (x) VALUES", [{"x": 100}])
-    client.execute(f"INSERT INTO {database}.test (x) SETTINGS async_insert=1, wait_for_async_insert=1 VALUES", [{"x": 101}])
+    client.execute(
+        f"INSERT INTO {database}.test (x) SETTINGS async_insert=1, wait_for_async_insert=1 VALUES",
+        [{"x": 101}],
+    )
 
     result = client.execute(f"SELECT * FROM {database}.test ORDER BY ALL")
     print(result)
+
 
 if __name__ == "__main__":
     database = os.environ["CLICKHOUSE_DATABASE"]
