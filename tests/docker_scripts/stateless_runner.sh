68d03ade4b66b01651631265a0feeb256b285dea
Nikita Fomichev <nikita.fomichev@clickhouse.com>
2024-12-27 18:29:43 +0100

    parallel replicas tests: fixes

--- tests/docker_scripts/stateless_runner.sh
+++ tests/docker_scripts/stateless_runner.sh
@@ -304,7 +304,7 @@ function run_tests()
 
     if [[ "$USE_PARALLEL_REPLICAS" -eq 1 ]]; then
         ADDITIONAL_OPTIONS+=('--parallel-replicas')
-        ADDITIONAL_OPTIONS+=('--replace-log-with-mt')
+#        ADDITIONAL_OPTIONS+=('--replace-log-with-mt')
         ADDITIONAL_OPTIONS+=('--no-zookeeper')
     else
         ADDITIONAL_OPTIONS+=('--zookeeper')
