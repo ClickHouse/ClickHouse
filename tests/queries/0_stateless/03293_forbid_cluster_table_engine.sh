48dc4f0cb956274680d737e256c892efdb850f8d
Yarik Briukhovetskyi <114298166+yariks5s@users.noreply.github.com>
2024-12-27 18:26:02 +0000

    Fix test

--- tests/queries/0_stateless/03293_forbid_cluster_table_engine.sh
+++ tests/queries/0_stateless/03293_forbid_cluster_table_engine.sh
@@ -5,7 +5,7 @@ CURDIR=$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)
 # shellcheck source=../shell_config.sh
 . "$CURDIR"/../shell_config.sh
 
-$CLICKHOUSE_CLIENT -q "CREATE TABLE test AS s3Cluster('test_shard_localhost', 'http://localhost:11111/test/a.tsv', 'TSV', 'a Int32, b Int32, c Int32');" 2>&1 | grep -m 1 -cF 'Cannot create a table with *Cluster engine'
+$CLICKHOUSE_CLIENT -q "CREATE TABLE test AS s3Cluster('test_shard_localhost', 'http://localhost:11111/test/a.tsv', 'TSV', 'a Int32, b Int32, c Int32');" 2>&1 | grep -m 1 -cF 'cannot be used to create a table'
 
 # Verify the table was not created
 table_exists=$($CLICKHOUSE_CLIENT -q "EXISTS TABLE test" | tr -d '\n')
