66cacdbcfed08e1e857834681a7fb6b8972e5add
Sema Checherinda <checherinda@gmail.com>
2024-12-27 16:54:04 +0100

    send logs and InsertProfileEvents for each block even for async insert

--- tests/queries/0_stateless/03279_with_clickhouse_driver.py
+++ tests/queries/0_stateless/03279_with_clickhouse_driver.py
@@ -11,10 +11,12 @@ def run(database):
     client.execute(
         f"CREATE TABLE IF NOT EXISTS {database}.test (x Int32) ENGINE = Memory"
     )
-    client.execute(f"INSERT INTO {database}.test (x) VALUES", [{"x": 100}])
-    result = client.execute(f"SELECT * FROM {database}.test")
-    print(result)
 
+    client.execute(f"INSERT INTO {database}.test (x) VALUES", [{"x": 100}])
+    client.execute(f"INSERT INTO {database}.test (x) SETTINGS async_insert=1, wait_for_async_insert=1 VALUES", [{"x": 101}])
+
+    result = client.execute(f"SELECT * FROM {database}.test ORDER BY ALL")
+    print(result)
 
 if __name__ == "__main__":
     database = os.environ["CLICKHOUSE_DATABASE"]
