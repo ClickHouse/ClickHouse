637dac07c22abf46bc289fedf9917cb1ddac3056
Alexey Milovidov <milovidov@clickhouse.com>
2024-12-28 22:25:17 +0100

    Update 03300_merge_parts_metric.sh

--- tests/queries/0_stateless/03300_merge_parts_metric.sh
+++ tests/queries/0_stateless/03300_merge_parts_metric.sh
@@ -10,4 +10,4 @@ CUR_DIR=$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)
 
 $CLICKHOUSE_CLIENT --query "DROP TABLE IF EXISTS test; CREATE TABLE test (x Bool) ENGINE = Memory;"
 yes "INSERT INTO test SELECT sum(length(source_part_names)) = (SELECT value FROM system.metrics WHERE name = 'MergeParts') FROM system.merges;" | head -n1000 | $CLICKHOUSE_CLIENT
-$CLICKHOUSE_CLIENT --query "SELECT sum(x) >= 900 FROM test; DROP TABLE test;"
+$CLICKHOUSE_CLIENT --query "SELECT sum(x) > 500 FROM test; DROP TABLE test;"
