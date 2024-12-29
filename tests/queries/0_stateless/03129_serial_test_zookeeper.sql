35d0d5ae913f321170efffcfb79bc1b63842ab9a
Alexey Milovidov <milovidov@clickhouse.com>
2024-12-29 21:20:03 +0100

    Update tests

--- tests/queries/0_stateless/03129_serial_test_zookeeper.sql
+++ tests/queries/0_stateless/03129_serial_test_zookeeper.sql
@@ -1,12 +1,13 @@
 -- Tags: zookeeper
 
-SELECT generateSerialID('x');
-SELECT generateSerialID('x');
-SELECT generateSerialID('y');
-SELECT generateSerialID('x') FROM numbers(5);
+SELECT generateSerialID(currentDatabase() || 'x');
+SELECT generateSerialID(currentDatabase() || 'x');
+SELECT generateSerialID(currentDatabase() || 'y');
+SELECT generateSerialID(currentDatabase() || 'x') FROM numbers(5);
 
 SELECT generateSerialID(); -- { serverError NUMBER_OF_ARGUMENTS_DOESNT_MATCH }
 SELECT generateSerialID('x', 'y'); -- { serverError NUMBER_OF_ARGUMENTS_DOESNT_MATCH }
 SELECT generateSerialID(1); -- { serverError ILLEGAL_TYPE_OF_ARGUMENT }
 
-SELECT generateSerialID('z'), generateSerialID('z') FROM numbers(5);
+-- Here the functions are identical and fall into common-subexpression-elimination:
+SELECT generateSerialID(currentDatabase() || 'z'), generateSerialID(currentDatabase() || 'z') FROM numbers(5);
