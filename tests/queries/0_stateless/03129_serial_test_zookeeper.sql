c105d7dbd6948f42bb1f3b0c3f5f7ba144108917
Alexey Milovidov <milovidov@clickhouse.com>
2024-12-29 21:53:18 +0100

    Better

--- tests/queries/0_stateless/03129_serial_test_zookeeper.sql
+++ tests/queries/0_stateless/03129_serial_test_zookeeper.sql
@@ -11,3 +11,6 @@ SELECT generateSerialID(1); -- { serverError ILLEGAL_TYPE_OF_ARGUMENT }
 
 -- Here the functions are identical and fall into common-subexpression-elimination:
 SELECT generateSerialID(currentDatabase() || 'z'), generateSerialID(currentDatabase() || 'z') FROM numbers(5);
+
+SET max_autoincrement_series = 3;
+SELECT generateSerialID('a'); -- { serverError LIMIT_EXCEEDED }
