ee9542db8d5e19e9500f98e23ca8144676ba5604
robot-clickhouse <robot-clickhouse@users.noreply.github.com>
2024-12-30 22:09:36 +0000

    Backport #73933 to 24.12: Use minimum of memory limits for external aggregation/sort

--- src/Processors/QueryPlan/SortingStep.cpp
+++ src/Processors/QueryPlan/SortingStep.cpp
@@ -19,6 +19,7 @@
 #include <Processors/Transforms/ScatterByPartitionTransform.h>
 
 #include <memory>
+#include <optional>
 
 namespace CurrentMetrics
 {
@@ -50,34 +51,29 @@ namespace ErrorCodes
     extern const int INCORRECT_DATA;
 }
 
-SortingStep::Settings::Settings(const Context & context)
+size_t getMaxBytesBeforeExternalSort(size_t max_bytes_before_external_sort, double max_bytes_ratio_before_external_sort)
 {
-    const auto & settings = context.getSettingsRef();
-    max_block_size = settings[Setting::max_block_size];
-    size_limits = SizeLimits(settings[Setting::max_rows_to_sort], settings[Setting::max_bytes_to_sort], settings[Setting::sort_overflow_mode]);
-    max_bytes_before_remerge = settings[Setting::max_bytes_before_remerge_sort];
-    remerge_lowered_memory_bytes_ratio = settings[Setting::remerge_sort_lowered_memory_bytes_ratio];
-    max_bytes_before_external_sort = settings[Setting::max_bytes_before_external_sort];
-    tmp_data = context.getTempDataOnDisk();
-    min_free_disk_space = settings[Setting::min_free_disk_space_for_temporary_data];
-    max_block_bytes = settings[Setting::prefer_external_sort_block_bytes];
-    read_in_order_use_buffering = settings[Setting::read_in_order_use_buffering];
+    std::optional<size_t> threshold;
+    if (max_bytes_before_external_sort != 0)
+        threshold = max_bytes_before_external_sort;
 
-    if (settings[Setting::max_bytes_ratio_before_external_sort] != 0.)
+    if (max_bytes_ratio_before_external_sort != 0.)
     {
-        if (settings[Setting::max_bytes_before_external_sort] > 0)
-            throw Exception(ErrorCodes::BAD_ARGUMENTS, "Settings max_bytes_ratio_before_external_sort and max_bytes_before_external_sort cannot be set simultaneously");
-
-        double ratio = settings[Setting::max_bytes_ratio_before_external_sort];
+        double ratio = max_bytes_ratio_before_external_sort;
         if (ratio < 0 || ratio >= 1.)
             throw Exception(ErrorCodes::BAD_ARGUMENTS, "Setting max_bytes_ratio_before_external_sort should be >= 0 and < 1 ({})", ratio);
 
         auto available_system_memory = getMostStrictAvailableSystemMemory();
         if (available_system_memory.has_value())
         {
-            max_bytes_before_external_sort = static_cast<size_t>(*available_system_memory * ratio);
-            LOG_TEST(getLogger("SortingStep"), "Set max_bytes_before_external_sort={} (ratio: {}, available system memory: {})",
-                formatReadableSizeWithBinarySuffix(max_bytes_before_external_sort),
+            size_t ratio_in_bytes = static_cast<size_t>(*available_system_memory * ratio);
+            if (threshold)
+                threshold = std::min(threshold.value(), ratio_in_bytes);
+            else
+                threshold = ratio_in_bytes;
+
+            LOG_TRACE(getLogger("SortingStep"), "Adjusting memory limit before external sort with {} (ratio: {}, available system memory: {})",
+                formatReadableSizeWithBinarySuffix(ratio_in_bytes),
                 ratio,
                 formatReadableSizeWithBinarySuffix(*available_system_memory));
         }
@@ -86,6 +82,22 @@ SortingStep::Settings::Settings(const Context & context)
             LOG_WARNING(getLogger("SortingStep"), "No system memory limits configured. Ignoring max_bytes_ratio_before_external_sort");
         }
     }
+
+    return threshold.value_or(0);
+}
+
+SortingStep::Settings::Settings(const Context & context)
+{
+    const auto & settings = context.getSettingsRef();
+    max_block_size = settings[Setting::max_block_size];
+    size_limits = SizeLimits(settings[Setting::max_rows_to_sort], settings[Setting::max_bytes_to_sort], settings[Setting::sort_overflow_mode]);
+    max_bytes_before_remerge = settings[Setting::max_bytes_before_remerge_sort];
+    remerge_lowered_memory_bytes_ratio = settings[Setting::remerge_sort_lowered_memory_bytes_ratio];
+    max_bytes_before_external_sort = getMaxBytesBeforeExternalSort(settings[Setting::max_bytes_before_external_sort], settings[Setting::max_bytes_ratio_before_external_sort]);
+    tmp_data = context.getTempDataOnDisk();
+    min_free_disk_space = settings[Setting::min_free_disk_space_for_temporary_data];
+    max_block_bytes = settings[Setting::prefer_external_sort_block_bytes];
+    read_in_order_use_buffering = settings[Setting::read_in_order_use_buffering];
 }
 
 SortingStep::Settings::Settings(size_t max_block_size_)
