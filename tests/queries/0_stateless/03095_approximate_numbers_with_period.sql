081594c4dfad9375c13a1c486c94e10fa7866056
Alexey Milovidov <milovidov@clickhouse.com>
2025-01-01 20:54:31 +0100

    Rename

--- tests/queries/0_stateless/03095_approximate_numbers_with_period.sql
+++ tests/queries/0_stateless/03095_approximate_numbers_with_period.sql
@@ -1,8 +1,8 @@
-SELECT 1.0, 1., 1 SETTINGS output_format_approximate_numbers_with_period=0;
-SELECT 1.0, 1., 1 SETTINGS output_format_approximate_numbers_with_period=1;
-SELECT number::Float32 FROM numbers(2) SETTINGS output_format_approximate_numbers_with_period=0;
-SELECT number::Float32 FROM numbers(2) SETTINGS output_format_approximate_numbers_with_period=1;
-SELECT number::Float64 FROM numbers(2) SETTINGS output_format_approximate_numbers_with_period=0;
-SELECT number::Float64 FROM numbers(2) SETTINGS output_format_approximate_numbers_with_period=1;
-SELECT number::Decimal(2) FROM numbers(2) SETTINGS output_format_approximate_numbers_with_period=0;
-SELECT number::Decimal(2) FROM numbers(2) SETTINGS output_format_approximate_numbers_with_period=1;
+SELECT 1.0, 1., 1 SETTINGS output_format_approximate_numbers_with_decimal_point=0;
+SELECT 1.0, 1., 1 SETTINGS output_format_approximate_numbers_with_decimal_point=1;
+SELECT number::Float32 FROM numbers(2) SETTINGS output_format_approximate_numbers_with_decimal_point=0;
+SELECT number::Float32 FROM numbers(2) SETTINGS output_format_approximate_numbers_with_decimal_point=1;
+SELECT number::Float64 FROM numbers(2) SETTINGS output_format_approximate_numbers_with_decimal_point=0;
+SELECT number::Float64 FROM numbers(2) SETTINGS output_format_approximate_numbers_with_decimal_point=1;
+SELECT number::Decimal(2) FROM numbers(2) SETTINGS output_format_approximate_numbers_with_decimal_point=0;
+SELECT number::Decimal(2) FROM numbers(2) SETTINGS output_format_approximate_numbers_with_decimal_point=1;
