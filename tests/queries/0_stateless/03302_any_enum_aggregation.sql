cb3025e544480fa087c633569af50477760fa439
Joanna Hulboj <joanna.hulboj@gmail.com>
2025-01-01 17:12:33 +0000

    Improve any enum tests

--- tests/queries/0_stateless/03302_any_enum_aggregation.sql
+++ tests/queries/0_stateless/03302_any_enum_aggregation.sql
@@ -4,8 +4,10 @@ CREATE TABLE test_33602 (name String, score UInt8, user_level  Enum8('LOW' = 1, 
 SELECT any(user_level) FROM test_33602;
 SELECT any(user_level), any(name), any(score) FROM test_33602;
 SELECT anyLast(user_level), anyLast(name), anyLast(score) FROM test_33602;
+DROP TABLE test_33602;
 
 SELECT 'Empty Enum8 table:';
+DROP TABLE IF EXISTS test_33602_t0a;
 CREATE TABLE test_33602_t0a (e Enum8('LOW' = 123, 'MEDIUM' = 12, 'HIGH' = 33)) ENGINE=Memory;
 SELECT any(e) FROM test_33602_t0a;
 SELECT anyLast(e) FROM test_33602_t0a;
@@ -14,14 +16,17 @@ SELECT 'Enum8 table with HIGH value:';
 INSERT INTO test_33602_t0a VALUES('HIGH');
 SELECT any(e) FROM test_33602_t0a;
 SELECT anyLast(e) FROM test_33602_t0a;
+DROP TABLE test_33602_t0a;
 
 
 SELECT 'Empty Enum16 table:';
+DROP TABLE IF EXISTS test_33602_t0b;
 CREATE TABLE test_33602_t0b (e Enum16('LOW' = 123, 'MEDIUM' = 12, 'HIGH' = 33)) ENGINE=Memory;
 SELECT any(e) FROM test_33602_t0b;
 SELECT anyLast(e) FROM test_33602_t0b;
 
 SELECT 'Enum16 table with HIGH value:';
 INSERT INTO test_33602_t0b VALUES('HIGH');
-SELECT any(e) FROM test_33602_t0a;
-SELECT anyLast(e) FROM test_33602_t0a;
+SELECT any(e) FROM test_33602_t0b;
+SELECT anyLast(e) FROM test_33602_t0b;
+DROP TABLE test_33602_t0b;
