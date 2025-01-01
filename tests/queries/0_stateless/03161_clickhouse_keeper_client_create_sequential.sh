15dd3ba8fa2e8cafea293e27bd5444a5861283b4
Alexey Milovidov <milovidov@clickhouse.com>
2025-01-01 02:17:41 +0100

    Update 03161_clickhouse_keeper_client_create_sequential.sh

--- tests/queries/0_stateless/03161_clickhouse_keeper_client_create_sequential.sh
+++ tests/queries/0_stateless/03161_clickhouse_keeper_client_create_sequential.sh
@@ -6,10 +6,10 @@ CUR_DIR=$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)
 
 path="/test-keeper-client-$CLICKHOUSE_DATABASE"
 
-$CLICKHOUSE_KEEPER_CLIENT -q "rm $path" >& /dev/null
+$CLICKHOUSE_KEEPER_CLIENT -q "rm '$path'" >& /dev/null
 
-$CLICKHOUSE_KEEPER_CLIENT -q "create $path 'foobar'"
-$CLICKHOUSE_KEEPER_CLIENT -q "create $path/tmp- 'foobar0' EPHEMERAL SEQUENTIAL"
-$CLICKHOUSE_KEEPER_CLIENT -q "create $path/tmp- 'foobar1' PERSISTENT SEQUENTIAL"
-$CLICKHOUSE_KEEPER_CLIENT -q "get $path/tmp-0000000001"
-$CLICKHOUSE_KEEPER_CLIENT -q "rmr $path"
+$CLICKHOUSE_KEEPER_CLIENT -q "create '$path' 'foobar'"
+$CLICKHOUSE_KEEPER_CLIENT -q "create '$path/tmp-' 'foobar0' EPHEMERAL SEQUENTIAL"
+$CLICKHOUSE_KEEPER_CLIENT -q "create '$path/tmp-' 'foobar1' PERSISTENT SEQUENTIAL"
+$CLICKHOUSE_KEEPER_CLIENT -q "get '$path/tmp-0000000001'"
+$CLICKHOUSE_KEEPER_CLIENT -q "rmr '$path'"
