46fa2d141df6aa7d23388ccc341cff4b1c21c4cc
Anton Ivashkin <ianton@live.com>
2025-01-03 18:38:12 +0100

    Left QueryID for server only

--- src/Interpreters/Context.cpp
+++ src/Interpreters/Context.cpp
@@ -2703,7 +2703,7 @@ void Context::setCurrentQueryId(const String & query_id)
     client_info.current_query_id = query_id_to_set;
 
     if (client_info.query_kind == ClientInfo::QueryKind::INITIAL_QUERY
-        && client_info.initial_query_id.empty())
+        && (getApplicationType() != ApplicationType::SERVER || client_info.initial_query_id.empty()))
     {
         client_info.initial_query_id = client_info.current_query_id;
     }
