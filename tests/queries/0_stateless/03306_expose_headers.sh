5140346614f14a3387b06cce06b1e6ec563b7655
Alexey Milovidov <milovidov@clickhouse.com>
2025-01-04 02:41:11 +0100

    Add a test

---
+++ tests/queries/0_stateless/03306_expose_headers.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+
+set -e
+
+CUR_DIR=$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)
+# shellcheck source=../shell_config.sh
+. "$CUR_DIR"/../shell_config.sh
+
+SCRIPTS_DIR=$CUR_DIR/scripts_udf
+
+${CLICKHOUSE_CURL} -v "${CLICKHOUSE_URL}&default_format=JSONEachRowWithProgress" -d 'SELECT number FROM numbers(10)' 2>&1 | grep -P 'X-ClickHouse-Format'
