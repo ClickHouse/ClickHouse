f19ca7e4ad479a758f4b479b8c0ec285e4eba22b
Alexey Milovidov <milovidov@clickhouse.com>
2025-01-04 04:13:13 +0100

    JSONEachRowWithProgress will output more events

--- src/Processors/Formats/Impl/JSONEachRowWithProgressRowOutputFormat.cpp
+++ src/Processors/Formats/Impl/JSONEachRowWithProgressRowOutputFormat.cpp
@@ -7,6 +7,24 @@
 namespace DB
 {
 
+void JSONEachRowWithProgressRowOutputFormat::writePrefix()
+{
+    writeCString("{\"meta\":[", *ostr);
+    bool first = true;
+    for (const auto & elem : getInputs().front().getHeader())
+    {
+        if (!first)
+            writeChar(',', *ostr);
+        first = false;
+        writeCString("{\"name\":", *ostr);
+        writeJSONString(elem.name, *ostr, settings);
+        writeCString(",\"type\":", *ostr);
+        writeJSONString(elem.type->getName(), *ostr, settings);
+        writeChar('}', *ostr);
+    }
+    writeCString("]}\n", *ostr);
+}
+
 void JSONEachRowWithProgressRowOutputFormat::writeRowStartDelimiter()
 {
     writeCString("{\"row\":{", *ostr);
@@ -16,6 +34,39 @@ void JSONEachRowWithProgressRowOutputFormat::writeRowEndDelimiter()
 {
     writeCString("}}\n", *ostr);
     field_number = 0;
+}
+
+void JSONEachRowWithProgressRowOutputFormat::writeSpecialRow(const char * kind, const Columns & columns, size_t row_num)
+{
+    writeCString("{\"", *ostr);
+    writeCString(kind, *ostr);
+    writeCString("\":{", *ostr);
+
+    for (size_t i = 0; i < num_columns; ++i)
+    {
+        if (i != 0)
+            writeFieldDelimiter();
+
+        writeField(*columns[i], *serializations[i], row_num);
+    }
+
+    writeCString("}}\n", *ostr);
+    field_number = 0;
+}
+
+void JSONEachRowWithProgressRowOutputFormat::writeTotals(const Columns & columns, size_t row_num)
+{
+    writeSpecialRow("totals", columns, row_num);
+}
+
+void JSONEachRowWithProgressRowOutputFormat::writeMinExtreme(const Columns & columns, size_t row_num)
+{
+    writeSpecialRow("min", columns, row_num);
+}
+
+void JSONEachRowWithProgressRowOutputFormat::writeMaxExtreme(const Columns & columns, size_t row_num)
+{
+    writeSpecialRow("max", columns, row_num);
 }
 
 void JSONEachRowWithProgressRowOutputFormat::writeProgress(const Progress & value)
