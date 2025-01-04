658f34128192adffe944d29626276c199675f681
Alexey Milovidov <milovidov@clickhouse.com>
2025-01-04 03:05:32 +0100

    Fix test

--- tests/queries/0_stateless/03306_expose_headers.sh
+++ tests/queries/0_stateless/03306_expose_headers.sh
@@ -6,6 +6,4 @@ CUR_DIR=$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)
 # shellcheck source=../shell_config.sh
 . "$CUR_DIR"/../shell_config.sh
 
-SCRIPTS_DIR=$CUR_DIR/scripts_udf
-
 ${CLICKHOUSE_CURL} -v "${CLICKHOUSE_URL}&default_format=JSONEachRowWithProgress" -d 'SELECT number FROM numbers(10)' 2>&1 | grep -P 'X-ClickHouse-Format'
