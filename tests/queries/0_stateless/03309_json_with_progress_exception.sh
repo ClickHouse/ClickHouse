c5c1500820401ec8af67f51863afbe1054725f3b
Alexey Milovidov <milovidov@clickhouse.com>
2025-01-05 23:46:40 +0100

    Update test

--- tests/queries/0_stateless/03309_json_with_progress_exception.sh
+++ tests/queries/0_stateless/03309_json_with_progress_exception.sh
@@ -5,5 +5,12 @@ CUR_DIR=$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)
 # shellcheck source=../shell_config.sh
 . "$CUR_DIR"/../shell_config.sh
 
-${CLICKHOUSE_CURL} -sS "${CLICKHOUSE_URL}&default_format=JSONEachRowWithProgress&max_execution_time=1" -d "SELECT count() FROM system.numbers" |
-    grep -F '"exception"' | grep -o -F '{"exception":"Code: 159. DB::Exception: Timeout exceeded: elapsed' | sed -r -e 's/xception/xpected/g'
+OUT=$(${CLICKHOUSE_CURL} -sS "${CLICKHOUSE_URL}&default_format=JSONEachRowWithProgress&max_execution_time=1" -d "SELECT count() FROM system.numbers")
+RES=$(echo "$OUT" | grep -F '"exception"' | grep -o -F '{"exception":"Code: 159. DB::Exception: Timeout exceeded: elapsed' | sed -r -e 's/xception/xpected/g')
+
+echo "$RES"
+
+if [ -z "$RES" ]
+then
+    echo "$OUT"
+fi
