1710fa0921726f21f4584c558a2d3d6edccdc8ae
Alexey Milovidov <milovidov@clickhouse.com>
2025-01-05 01:57:47 +0100

    A test for exception event

---
+++ tests/queries/0_stateless/03309_json_with_progress_exception.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+# Tags: long
+
+CUR_DIR=$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)
+# shellcheck source=../shell_config.sh
+. "$CUR_DIR"/../shell_config.sh
+
+${CLICKHOUSE_CURL} -sS "${CLICKHOUSE_URL}&default_format=JSONEachRowWithProgress&max_execution_time=1" -d "SELECT count() FROM system.numbers" |
+    grep -F '"exception"' | grep -o -F '{"exception":"Code: 159. DB::Exception: Timeout exceeded: elapsed'
