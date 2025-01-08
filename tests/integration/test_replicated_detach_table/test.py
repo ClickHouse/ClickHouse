6bb511562becd52a6a61d33e47234faadd544989
Konstantin Morozov <just.morozov.k@gmail.com>
2025-01-08 15:06:26 +0000

    sync tests

--- tests/integration/test_replicated_detach_table/test.py
+++ tests/integration/test_replicated_detach_table/test.py
@@ -1,7 +1,10 @@
 # Tag no-fasttest: requires S3
 
-import logging
+import random
+import string
+
 import pytest
+
 from helpers.cluster import ClickHouseCluster
 
 cluster = ClickHouseCluster(__file__)
@@ -31,14 +34,23 @@ def start_cluster():
         cluster.shutdown()
 
 
-def test_replicated_detach_table(start_cluster):
+def generate_random_string(length=8):
+    return "".join(random.choice(string.ascii_lowercase) for i in range(length))
+
+
+@pytest.fixture(scope="function")
+def exclusive_database_name():
+    return "repl_db_" + generate_random_string()
+
+
+def test_replicated_detach_table(start_cluster, exclusive_database_name):
     replica1.query(
-        f"CREATE DATABASE IF NOT EXISTS repl_db ON CLUSTER test_cluster ENGINE=Replicated('/clickhouse/tables/test_replicated_table', shard1, '{{replica}}');"
+        f"CREATE DATABASE IF NOT EXISTS {exclusive_database_name} ON CLUSTER test_cluster ENGINE=Replicated('/clickhouse/tables/{exclusive_database_name}/test_replicated_table', shard1, '{{replica}}');"
     )
 
     replica1.query_with_retry(
         f"""
-        CREATE TABLE repl_db.test_replicated_table
+        CREATE TABLE {exclusive_database_name}.test_replicated_table
         (
             number UInt64
         ) 
@@ -47,7 +59,16 @@ def test_replicated_detach_table(start_cluster):
         """
     )
     replica1.query(
-        "INSERT INTO repl_db.test_replicated_table SELECT number FROM system.numbers LIMIT 6;"
+        f"INSERT INTO {exclusive_database_name}.test_replicated_table SELECT number FROM system.numbers LIMIT 6;"
     )
-    replica1.query("SYSTEM SYNC REPLICA repl_db.test_replicated_table;", timeout=20)
-    replica1.query("DETACH TABLE repl_db.test_replicated_table PERMANENTLY;")
+    replica1.query(
+        f"SYSTEM SYNC REPLICA {exclusive_database_name}.test_replicated_table;",
+        timeout=20,
+    )
+    replica1.query(
+        f"DETACH TABLE {exclusive_database_name}.test_replicated_table PERMANENTLY;"
+    )
+
+    replica1.query(
+        f"DROP DATABASE {exclusive_database_name} ON CLUSTER test_cluster SYNC"
+    )
