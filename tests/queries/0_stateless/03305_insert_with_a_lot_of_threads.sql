26f6666ba0243d7299d36e7d10f7e4f082d98732
yariks5s <yaroslav.briukhovetskyi@clickhouse.com>
2025-01-08 14:24:37 +0000

    Fix test

--- tests/queries/0_stateless/03305_insert_with_a_lot_of_threads.sql
+++ tests/queries/0_stateless/03305_insert_with_a_lot_of_threads.sql
@@ -1,3 +1,4 @@
+SET max_rows_to_read=0;
 CREATE TABLE testing_a_lot_of_threads (a UInt64, b String) engine=MergeTree ORDER BY ();
 INSERT INTO testing_a_lot_of_threads SELECT number, toString(number) FROM system.numbers LIMIT 1000000000 SETTINGS max_insert_threads=10000;
 SELECT count(*) FROM testing_a_lot_of_threads;
