f857171a3542a0c940130189ee93c0803bc01e4e
Michael Stetsyuk <michael.stetsyuk@clickhouse.com>
2025-01-08 19:00:08 +0000

    fxs

--- src/Interpreters/TemporaryReplaceTableName.h
+++ src/Interpreters/TemporaryReplaceTableName.h
@@ -3,11 +3,6 @@
 #include <optional>
 #include <base/types.h>
 
-#include <IO/ReadBufferFromString.h>
-#include <IO/WriteBufferFromString.h>
-#include <Common/SipHash.h>
-#include <IO/Operators.h>
-
 namespace DB
 {
     struct TemporaryReplaceTableName
@@ -15,37 +10,8 @@ namespace DB
         String name_hash;
         String random_suffix;
 
-        String toString() const
-        {
-            WriteBufferFromOwnString out;
-            writeText(out);
-            return out.str();
-        }
+        String toString() const;
 
-        static std::optional<TemporaryReplaceTableName> fromString(const String & str)
-        {
-            TemporaryReplaceTableName result;
-            ReadBufferFromString in(str);
-            try
-            {
-                result.readText(in);
-            }
-            catch (...)
-            {
-                return std::nullopt;
-            }
-            return result;
-        }
-
-    private:
-        void writeText(WriteBuffer & out) const
-        {
-            out << "_tmp_replace_" << name_hash << "_" << random_suffix;
-        }
-
-        void readText(ReadBuffer & in)
-        {
-            in >> "_tmp_replace_" >> name_hash >> "_" >> random_suffix;
-        }
+        static std::optional<TemporaryReplaceTableName> fromString(const String & str);
     };
 }
