e30a78e474476a91b17092fa1465f6dae31f5130
yariks5s <yaroslav.briukhovetskyi@clickhouse.com>
2025-01-09 14:41:06 +0000

    Add max_rows_to_read in the test

--- tests/queries/0_stateless/03305_insert_with_a_lot_of_threads.sql
+++ tests/queries/0_stateless/03305_insert_with_a_lot_of_threads.sql
@@ -1,4 +1,5 @@
-SET max_memory_usage = 2e9;
+-- will be a long test later, now I don't mark it as long because I want to test it in fast tests
+SET max_memory_usage = 2e9, max_rows_to_read = 0;
 CREATE TABLE testing_memory (a UInt64, b String) engine=MergeTree ORDER BY ();
 INSERT INTO testing_memory SELECT number, toString(number) FROM system.numbers LIMIT 1000000000 SETTINGS max_insert_threads=500;
 SELECT count(*) FROM testing_memory;
