71141db6f56b1022f0bc2a217793676c220e32b0
wxybear <wangxingyu0927@outlook.com>
2025-01-10 01:54:43 +0800

    fix: support negative const offset for right/left func

--- src/Functions/LeftRight.h
+++ src/Functions/LeftRight.h
@@ -111,6 +111,15 @@ public:
         if (column_length_const)
             length_value = column_length_const->getInt(0);
 
+        
+        // If length_value < 0, we need to convert column_length to full column
+        // to handle right/left Func offset < 0 cases
+        if (length_value < 0)
+        {
+            column_length = column_length_const->convertToFullColumn();
+            column_length_const = nullptr;
+        }
+    
         if constexpr (is_utf8)
         {
             if (const ColumnString * col = checkAndGetColumn<ColumnString>(column_string.get()))
