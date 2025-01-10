42ee1f610f73790e0470dce8bf6a9866a30cec95
Nikita Fomichev <nikita.fomichev@clickhouse.com>
2025-01-10 14:40:40 +0100

    parallel replicas tests: update skip list

--- tests/queries/0_stateless/02124_json_each_row_with_progress.sh
+++ tests/queries/0_stateless/02124_json_each_row_with_progress.sh
@@ -4,8 +4,11 @@ CURDIR=$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)
 # shellcheck source=../shell_config.sh
 . "$CURDIR"/../shell_config.sh
 
+# This filters additional returned row with enabled parallel replicas
+# grep -v --text "rows_before_limit_at_least"
+
 ${CLICKHOUSE_CURL} -sS ${CLICKHOUSE_URL} -d "drop table if exists test_progress"
 ${CLICKHOUSE_CURL} -sS ${CLICKHOUSE_URL} -d "create table test_progress (x UInt64, y UInt64, d Date, a Array(UInt64), s String) engine=MergeTree() order by x"
 ${CLICKHOUSE_CURL} -sS ${CLICKHOUSE_URL} -d "insert into test_progress select number as x, number + 1 as y, toDate(number) as d, range(number % 10) as a, repeat(toString(number), 10) as s from numbers(200000)"
-${CLICKHOUSE_CURL} -sS ${CLICKHOUSE_URL} -d "SELECT * from test_progress FORMAT JSONEachRowWithProgress" | grep -v --text "progress" | wc -l
+${CLICKHOUSE_CURL} -sS ${CLICKHOUSE_URL} -d "SELECT * from test_progress FORMAT JSONEachRowWithProgress" | grep -v --text "progress" | grep -v --text "rows_before_limit_at_least" | wc -l
 ${CLICKHOUSE_CURL} -sS ${CLICKHOUSE_URL} -d "drop table test_progress";
