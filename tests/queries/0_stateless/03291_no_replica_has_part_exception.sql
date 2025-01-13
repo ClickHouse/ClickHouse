7b1f3f49e01c2739ca4ef759eaa22d56549b318d
MikhailBurdukov <burdukvmikhail@gmail.com>
2025-01-13 07:41:03 +0000

    Fix replicated database

--- tests/queries/0_stateless/03291_no_replica_has_part_exception.sql
+++ tests/queries/0_stateless/03291_no_replica_has_part_exception.sql
@@ -1,3 +1,4 @@
+-- Tags: no-replicated-database
 -- Tag no-replicated-database: Fails due to additional replicas or shards
 
 CREATE TABLE table1 (a Int32) ENGINE=ReplicatedMergeTree('/clickhouse/tables/{database}/table', 'r1') ORDER BY a;
