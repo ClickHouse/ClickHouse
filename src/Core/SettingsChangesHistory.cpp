a7c7a20e527249403edcdb3f943a0b3ed40d1024
Konstantin Bogdanov <konstantin@clickhouse.com>
2025-01-13 11:16:14 +0100

    Set `max_parallel_replicas` default to `1000`

--- src/Core/SettingsChangesHistory.cpp
+++ src/Core/SettingsChangesHistory.cpp
@@ -98,6 +98,7 @@ const VersionToSettingsChangesMap & getSettingsChangesHistory()
             {"validate_enum_literals_in_operators", false, false, "A new setting"},
             {"allow_experimental_kusto_dialect", true, false, "A new setting"},
             {"allow_experimental_prql_dialect", true, false, "A new setting"},
+            {"max_parallel_replicas", 1, 1000, "Use up to 1000 parallel replicas by default."},
         });
         addSettingsChanges(settings_changes_history, "24.12",
         {
