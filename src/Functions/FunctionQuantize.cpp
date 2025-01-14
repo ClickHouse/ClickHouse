c122cad4fedabca2331b5b48ad464e1f0c64f0b2
nikita4109 <nikita4109@gmail.com>
2025-01-14 16:38:23 +0100

    fix function & tests

--- src/Functions/FunctionQuantize.cpp
+++ src/Functions/FunctionQuantize.cpp
@@ -1,34 +1,17 @@
 #include "FunctionQuantize.h"
-#include <cmath>
-#include <Columns/ColumnArray.h>
-#include <Columns/ColumnFixedString.h>
-#include <Columns/ColumnVector.h>
-#include <DataTypes/DataTypeArray.h>
-#include <Functions/FunctionFactory.h>
-#include <Common/Arena.h>
-#include <Common/Exception.h>
-#include <Common/assert_cast.h>
-#include "Columns/ColumnsNumber.h"
-#include "Functions/FunctionApproximateL2Distance.h"
-#include "Functions/FunctionDequantize.h"
 
 namespace DB
 {
 
-REGISTER_FUNCTION(Quantize)
-{
-    factory.registerFunction<FunctionQuantize>({}, FunctionFactory::Case::Insensitive);
-    factory.registerFunction<FunctionDequantize>({}, FunctionFactory::Case::Sensitive);
-    factory.registerFunction<FunctionApproximateL2Distance>({}, FunctionFactory::Case::Insensitive);
-}
-
 namespace ErrorCodes
 {
 extern const int ILLEGAL_TYPE_OF_ARGUMENT;
+extern const int ILLEGAL_COLUMN;
 extern const int BAD_ARGUMENTS;
+extern const int NUMBER_OF_ARGUMENTS_DOESNT_MATCH;
 }
 
-UInt32 quantizeToBits(Float64 value, UInt8 bit_width)
+UInt32 FunctionQuantize::quantizeToBits(Float64 value, UInt8 bit_width)
 {
     constexpr Float64 min_value = -1.0;
     constexpr Float64 max_value = 1.0;
@@ -37,14 +20,11 @@ UInt32 quantizeToBits(Float64 value, UInt8 bit_width)
     value = std::min(value, max_value);
 
     Float64 normalized = (value - min_value) / (max_value - min_value);
-
     UInt32 max_int = (1U << bit_width) - 1;
-    UInt32 quantized = static_cast<UInt32>(std::round(normalized * max_int));
-
-    return quantized;
+    return static_cast<UInt32>(std::round(normalized * max_int));
 }
 
-UInt16 float32ToUInt16(Float32 value)
+UInt16 FunctionQuantize::float32ToUInt16(Float32 value)
 {
     union
     {
@@ -79,53 +59,82 @@ UInt16 float32ToUInt16(Float32 value)
     return h;
 }
 
+template <typename T>
+void FunctionQuantize::processArray(
+    const PaddedPODArray<T> & src_data,
+    size_t array_size,
+    UInt8 bit_width,
+    PaddedPODArray<UInt8> & result_chars,
+    size_t row,
+    size_t fixed_string_length)
+{
+    size_t offset_in_result = row * fixed_string_length;
+    size_t start_idx = row * array_size;
+
+    if (offset_in_result + fixed_string_length > result_chars.size())
+        throw Exception(ErrorCodes::LOGICAL_ERROR, "Buffer overflow in quantize function");
+
+    size_t bits_written = 0;
+    UInt8 current_byte = 0;
+
+    for (size_t i = 0; i < array_size; ++i)
+    {
+        UInt32 quantized_value;
+        if (bit_width == 16)
+            quantized_value = float32ToUInt16(static_cast<Float32>(src_data[start_idx + i]));
+        else
+            quantized_value = quantizeToBits(static_cast<Float64>(src_data[start_idx + i]), bit_width);
+
+        for (size_t bit = 0; bit < bit_width; ++bit)
+        {
+            if (quantized_value & (1U << (bit_width - 1 - bit)))
+                current_byte |= (1U << (7 - (bits_written % 8)));
+
+            bits_written++;
+            if (bits_written % 8 == 0)
+            {
+                result_chars[offset_in_result++] = current_byte;
+                current_byte = 0;
+            }
+        }
+    }
+
+    if (bits_written % 8 != 0)
+        result_chars[offset_in_result] = current_byte;
+}
+
 DataTypePtr FunctionQuantize::getReturnTypeImpl(const DataTypes & arguments) const
 {
+    if (arguments.size() != 2)
+        throw Exception(
+            ErrorCodes::NUMBER_OF_ARGUMENTS_DOESNT_MATCH,
+            "Number of arguments for function {} doesn't match: passed {}, should be 2.",
+            getName(),
+            arguments.size());
+
     const DataTypeArray * array_type = checkAndGetDataType<DataTypeArray>(arguments[0].get());
     if (!array_type)
-        throw Exception(ErrorCodes::ILLEGAL_TYPE_OF_ARGUMENT, "First argument must be an array in function {}", getName());
+        throw Exception(ErrorCodes::ILLEGAL_TYPE_OF_ARGUMENT, "First argument of function {} must be an array", getName());
 
-    const IDataType * nested_type = array_type->getNestedType().get();
-    if (!WhichDataType(nested_type).isFloat())
-        throw Exception(ErrorCodes::ILLEGAL_TYPE_OF_ARGUMENT, "Array elements must be Float32 or Float64 in function {}", getName());
-
+    const auto * nested_type = array_type->getNestedType().get();
+    if (!isFloat(nested_type->getTypeId()))
+        throw Exception(ErrorCodes::ILLEGAL_TYPE_OF_ARGUMENT, "Array elements must be Float32 or Float64, got {}", nested_type->getName());
     if (!WhichDataType(arguments[1]).isUInt8())
         throw Exception(ErrorCodes::ILLEGAL_TYPE_OF_ARGUMENT, "Second argument of function {} must be UInt8", getName());
 
-    return std::make_shared<DataTypeFixedString>(0);
+    return std::make_shared<DataTypeFixedString>(1);
 }
-
 
 ColumnPtr FunctionQuantize::executeImpl(const ColumnsWithTypeAndName & arguments, const DataTypePtr &, size_t input_rows_count) const
 {
-    const auto & array_col_with_type = arguments[0];
-    const auto & array_col = array_col_with_type.column;
-
-    const auto * col_array = checkAndGetColumn<ColumnArray>(array_col.get());
-    if (!col_array)
-        throw Exception(ErrorCodes::ILLEGAL_TYPE_OF_ARGUMENT, "First argument of function {} must be an array", getName());
-
-    const auto & data_col = col_array->getData();
-    const auto & offsets = col_array->getOffsets();
-
-    const PaddedPODArray<Float32> * float32_data = nullptr;
-    const PaddedPODArray<Float64> * float64_data = nullptr;
-    bool is_float32 = false;
-
-    if (const auto * col_float32 = checkAndGetColumn<ColumnFloat32>(&data_col))
-    {
-        float32_data = &col_float32->getData();
-        is_float32 = true;
-    }
-    else if (const auto * col_float64 = checkAndGetColumn<ColumnFloat64>(&data_col))
-    {
-        float64_data = &col_float64->getData();
-        is_float32 = false;
-    }
+    const ColumnArray * col_array = nullptr;
+    if (const auto * col_const = checkAndGetColumnConst<ColumnArray>(arguments[0].column.get()))
+        col_array = checkAndGetColumn<ColumnArray>(col_const->getDataColumnPtr().get());
     else
-    {
-        throw Exception(ErrorCodes::ILLEGAL_TYPE_OF_ARGUMENT, "Array elements in function {} must be Float32 or Float64", getName());
-    }
+        col_array = checkAndGetColumn<ColumnArray>(arguments[0].column.get());
+
+    if (!col_array)
+        throw Exception(ErrorCodes::ILLEGAL_COLUMN, "First argument of function {} must be an array", getName());
 
     UInt8 bit_width = 0;
     if (const auto * const_col = checkAndGetColumnConst<ColumnUInt8>(arguments[1].column.get()))
@@ -136,92 +145,53 @@ ColumnPtr FunctionQuantize::executeImpl(const ColumnsWithTypeAndName & arguments
     if (!(bit_width == 16 || bit_width == 8 || bit_width == 4 || bit_width == 2 || bit_width == 1))
         throw Exception(ErrorCodes::BAD_ARGUMENTS, "Bit width must be one of 16, 8, 4, 2, or 1 in function {}", getName());
 
-    size_t num_elements_per_row = 0;
-    if (input_rows_count > 0)
+    const auto & offsets = col_array->getOffsets();
+    if (offsets.empty())
+        return ColumnFixedString::create(0);
+
+    size_t array_size = offsets[0];
+    for (size_t i = 1; i < offsets.size(); ++i)
     {
-        num_elements_per_row = offsets[0];
+        if (offsets[i] - offsets[i - 1] != array_size)
+            throw Exception(ErrorCodes::ILLEGAL_COLUMN, "All arrays must have the same size in function {}", getName());
     }
 
-    for (size_t row = 1; row < input_rows_count; ++row)
-    {
-        size_t num_elements = offsets[row] - offsets[row - 1];
-        if (num_elements != num_elements_per_row)
-        {
-            throw Exception(ErrorCodes::BAD_ARGUMENTS, "All input arrays must have the same number of elements in function {}", getName());
-        }
-    }
-
-    size_t total_bits = num_elements_per_row * bit_width;
+    size_t total_bits = array_size * bit_width;
     size_t fixed_string_length = (total_bits + 7) / 8;
+    if (fixed_string_length == 0)
+        fixed_string_length = 1;
 
     auto result_column = ColumnFixedString::create(fixed_string_length);
     auto & result_chars = result_column->getChars();
-    result_chars.resize(input_rows_count * fixed_string_length);
+    result_chars.resize_fill(input_rows_count * fixed_string_length);
 
-    size_t prev_offset = 0;
-
-    for (size_t row = 0; row < input_rows_count; ++row)
+    const auto & array_data = col_array->getData();
+    if (const auto * col_float32 = checkAndGetColumn<ColumnFloat32>(&array_data))
     {
-        size_t current_offset = offsets[row];
-        size_t offset_in_result = row * fixed_string_length;
-
-        size_t bits_written = 0;
-        UInt8 current_byte = 0;
-
-        for (size_t idx = prev_offset; idx < current_offset; ++idx)
-        {
-            UInt32 quantized_value = 0;
-            if (bit_width == 16)
-            {
-                Float32 value;
-                if (is_float32)
-                {
-                    value = (*float32_data)[idx];
-                }
-                else
-                {
-                    value = static_cast<Float32>((*float64_data)[idx]);
-                }
-                quantized_value = float32ToUInt16(value);
-            }
-            else
-            {
-                Float64 value;
-                if (is_float32)
-                {
-                    value = static_cast<Float64>((*float32_data)[idx]);
-                }
-                else
-                {
-                    value = (*float64_data)[idx];
-                }
-                quantized_value = quantizeToBits(value, bit_width);
-            }
-
-            for (size_t bit = 0; bit < bit_width; ++bit)
-            {
-                if (quantized_value & (1U << (bit_width - 1 - bit)))
-                    current_byte |= (1U << (7 - (bits_written % 8)));
-
-                bits_written++;
-                if (bits_written % 8 == 0)
-                {
-                    result_chars[offset_in_result++] = current_byte;
-                    current_byte = 0;
-                }
-            }
-        }
-
-        if (bits_written % 8 != 0)
-        {
-            result_chars[offset_in_result++] = current_byte;
-        }
-
-        prev_offset = current_offset;
+        for (size_t i = 0; i < input_rows_count; ++i)
+            processArray(col_float32->getData(), array_size, bit_width, result_chars, i, fixed_string_length);
     }
+    else if (const auto * col_float64 = checkAndGetColumn<ColumnFloat64>(&array_data))
+    {
+        for (size_t i = 0; i < input_rows_count; ++i)
+            processArray(col_float64->getData(), array_size, bit_width, result_chars, i, fixed_string_length);
+    }
+    else
+        throw Exception(ErrorCodes::ILLEGAL_COLUMN, "Unexpected array element type for function {}", getName());
 
     return result_column;
 }
 
+template void
+FunctionQuantize::processArray<Float32>(const PaddedPODArray<Float32> &, size_t, UInt8, PaddedPODArray<UInt8> &, size_t, size_t);
+template void
+FunctionQuantize::processArray<Float64>(const PaddedPODArray<Float64> &, size_t, UInt8, PaddedPODArray<UInt8> &, size_t, size_t);
+}
 
+namespace DB
+{
+REGISTER_FUNCTION(Quantize)
+{
+    factory.registerFunction<FunctionQuantize>();
+}
 }
--- src/Functions/FunctionQuantize.h
+++ src/Functions/FunctionQuantize.h
@@ -1,16 +1,14 @@
-// FunctionQuantize.h
-
 #pragma once
 
-#include <memory>
 #include <Columns/ColumnArray.h>
 #include <Columns/ColumnFixedString.h>
-#include <Columns/ColumnVector.h>
+#include <Columns/ColumnsNumber.h>
+#include <Core/ColumnNumbers.h>
+#include <DataTypes/DataTypeArray.h>
 #include <DataTypes/DataTypeFixedString.h>
 #include <DataTypes/DataTypesNumber.h>
+#include <Functions/FunctionFactory.h>
 #include <Functions/IFunction.h>
-#include <Common/Logger.h>
-#include "Interpreters/Context_fwd.h"
 
 namespace DB
 {
@@ -22,15 +20,28 @@ public:
     static FunctionPtr create(ContextPtr) { return std::make_shared<FunctionQuantize>(); }
 
     String getName() const override { return name; }
-
     size_t getNumberOfArguments() const override { return 2; }
-
-    bool isSuitableForShortCircuitArgumentsExecution(const DataTypesWithConstInfo &) const override { return true; }
+    bool isInjective(const ColumnsWithTypeAndName &) const override { return false; }
 
     DataTypePtr getReturnTypeImpl(const DataTypes & arguments) const override;
-
     ColumnPtr
     executeImpl(const ColumnsWithTypeAndName & arguments, const DataTypePtr & result_type, size_t input_rows_count) const override;
+
+
+    bool isSuitableForShortCircuitArgumentsExecution(const DataTypesWithConstInfo & /*arguments*/) const override { return false; }
+
+private:
+    static UInt32 quantizeToBits(Float64 value, UInt8 bit_width);
+    static UInt16 float32ToUInt16(Float32 value);
+
+    template <typename T>
+    static void processArray(
+        const PaddedPODArray<T> & src_data,
+        size_t array_size,
+        UInt8 bit_width,
+        PaddedPODArray<UInt8> & result_chars,
+        size_t row,
+        size_t fixed_string_length);
 };
 
 }
