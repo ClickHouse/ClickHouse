941a1b87fcc546f189bd8e6d61d6b4e4a76ac14f
Pavel Kruglov <48961922+Avogar@users.noreply.github.com>
2025-01-14 13:20:51 +0100

    Update 03305_log_unsupported_types.sql

--- tests/queries/0_stateless/03305_log_unsupported_types.sql
+++ tests/queries/0_stateless/03305_log_unsupported_types.sql
@@ -1,6 +1,7 @@
 set enable_json_type=1;
 set enable_dynamic_type=1;
 
+drop table if exists test;
 create table test (d Dynamic) engine=Log(); -- {serverError ILLEGAL_COLUMN}
 create table test (d Dynamic) engine=TinyLog(); -- {serverError ILLEGAL_COLUMN}
 create table test (d Array(Map(String, Dynamic))) engine=Log(); -- {serverError ILLEGAL_COLUMN}
