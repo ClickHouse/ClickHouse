8fbf91e193c4100b1fe64190a272ffc9954da4a2
taiyang-li <654010905@qq.com>
2025-01-15 14:59:37 +0800

    wip

--- src/AggregateFunctions/AggregateFunctionCount.h
+++ src/AggregateFunctions/AggregateFunctionCount.h
@@ -3,7 +3,6 @@
 #include <IO/VarInt.h>
 #include <IO/WriteHelpers.h>
 
-#include <array>
 #include <DataTypes/DataTypesNumber.h>
 #include <DataTypes/DataTypeAggregateFunction.h>
 #include <Columns/ColumnNullable.h>
---
+++ src/AggregateFunctions/AggregateFunctionUniqHyperLogLogPlusPlus.cpp
@@ -0,0 +1,45 @@
+#include <AggregateFunctions/AggregateFunctionFactory.h>
+#include <AggregateFunctions/AggregateFunctionUniqHyperLogLogPlusPlus.h>
+#include <AggregateFunctions/Helpers.h>
+#include <DataTypes/DataTypeDate.h>
+#include <DataTypes/DataTypeDate32.h>
+#include <DataTypes/DataTypeDateTime.h>
+#include <DataTypes/DataTypeUUID.h>
+#include <DataTypes/DataTypeIPv4andIPv6.h>
+
+namespace DB
+{
+
+namespace
+{
+
+AggregateFunctionPtr createAggregateFunctionUniqHyperLogLogPlusPlus(
+    const std::string & name, const DataTypes & argument_types, const Array & , const Settings *)
+{
+    if (argument_types.size() != 1)
+        throw Exception(
+            ErrorCodes::NUMBER_OF_ARGUMENTS_DOESNT_MATCH,
+            "Aggregate function '{}' requires exactly one argument: got {}",
+            name,
+            argument_types.size());
+
+    WhichDataType which(argument_types[0]);
+    if (!which.isUInt64())
+        throw Exception(
+            ErrorCodes::ILLEGAL_TYPE_OF_ARGUMENT,
+            "Aggregate function '{}' requires an argument of type UInt64, got {}",
+            name,
+            argument_types[0]->getName());
+
+    return std::make_shared<AggregateFunctionUniqHyperLogLogPlusPlus>(argument_types);
+}
+
+}
+
+void registerAggregateFunctionUniqHyperLogLogPlusPlus(AggregateFunctionFactory & factory)
+{
+    AggregateFunctionProperties properties = {.returns_default_when_only_null = true, .is_order_dependent = false};
+    factory.registerFunction("uniqHLLPP", {createAggregateFunctionUniqHyperLogLogPlusPlus, properties});
+}
+
+}
