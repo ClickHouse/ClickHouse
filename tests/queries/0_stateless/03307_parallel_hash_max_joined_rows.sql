8df1f8e93a22285ab82ea32f8e7dc522dace450e
Nikita Taranov <nikita.taranov@clickhouse.com>
2025-01-15 22:25:46 +0100

    better

--- tests/queries/0_stateless/03307_parallel_hash_max_joined_rows.sql
+++ tests/queries/0_stateless/03307_parallel_hash_max_joined_rows.sql
@@ -8,7 +8,7 @@ insert into t select repeat('x', number%100) as s, s as s2 from numbers_mt(3e5);
 
 set max_result_rows = 0, max_result_bytes = 0;
 set max_block_size = 65409; -- randomly chosen `max_block_size` could cause a big slow down
-set filesystem_prefetch_step_bytes = 0; -- randomly chosen `max_block_size` could cause a big slow down
+set filesystem_prefetch_step_bytes = 0; -- randomly chosen `filesystem_prefetch_step_bytes` could cause a big slow down
 set max_threads = 32, max_memory_usage = '2Gi', join_algorithm = 'parallel_hash';
 
 select * from t t1 join t t2 on t1.s = t2.s where length(t1.s) % 2 = 0 format Null;
