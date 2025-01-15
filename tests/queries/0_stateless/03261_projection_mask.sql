8cf3fbea3848167dcd427084f655396bc68de76a
jsc0218 <jsc0218@gmail.com>
2025-01-15 15:53:35 +0000

    better name in test to differentiate wide and compact format

--- tests/queries/0_stateless/03261_projection_mask.sql
+++ tests/queries/0_stateless/03261_projection_mask.sql
@@ -1,7 +1,7 @@
 -- compact
-DROP TABLE IF EXISTS users;
+DROP TABLE IF EXISTS users_compact;
 
-CREATE TABLE users (
+CREATE TABLE users_compact (
     uid Int16,
     name String,
     age Int16,
@@ -12,34 +12,36 @@ CREATE TABLE users (
 ) ENGINE = MergeTree order by uid
 SETTINGS lightweight_mutation_projection_mode = 'rebuild', min_bytes_for_wide_part = 10485760;
 
-INSERT INTO users VALUES (1231, 'John', 33), (1232, 'Mary', 34);
+INSERT INTO users_compact VALUES (1231, 'John', 33), (1232, 'Mary', 34);
 
-DELETE FROM users WHERE age = 34;
+DELETE FROM users_compact WHERE age = 34;
 
 SELECT
     age,
     count()
-FROM users
+FROM users_compact
 GROUP BY age
 SETTINGS optimize_use_projections = 1, force_optimize_projection = 1;
 
 SELECT
     age,
     avg(age)
-FROM users
+FROM users_compact
 GROUP BY age
 SETTINGS optimize_use_projections = 1, force_optimize_projection = 1;
 
 SELECT
     count()
-FROM users
+FROM users_compact
 GROUP BY name
 SETTINGS optimize_use_projections = 1, force_optimize_projection = 1;
 
--- wide
-DROP TABLE IF EXISTS users;
+DROP TABLE users_compact;
 
-CREATE TABLE users (
+-- wide
+DROP TABLE IF EXISTS users_wide;
+
+CREATE TABLE users_wide (
     uid Int16,
     name String,
     age Int16,
@@ -50,32 +52,36 @@ CREATE TABLE users (
 ) ENGINE = MergeTree order by uid
 SETTINGS lightweight_mutation_projection_mode = 'rebuild', min_bytes_for_wide_part = 0;
 
-INSERT INTO users VALUES (1231, 'John', 33), (1232, 'Mary', 34);
+INSERT INTO users_wide VALUES (1231, 'John', 33), (1232, 'Mary', 34);
 
-DELETE FROM users WHERE age = 34;
+DELETE FROM users_wide WHERE age = 34;
 
 SELECT
     age,
     count()
-FROM users
+FROM users_wide
 GROUP BY age
 SETTINGS optimize_use_projections = 1, force_optimize_projection = 1;
 
 SELECT
     age,
     avg(age)
-FROM users
+FROM users_wide
 GROUP BY age
 SETTINGS optimize_use_projections = 1, force_optimize_projection = 1;
 
 SELECT
     count()
-FROM users
+FROM users_wide
 GROUP BY name
 SETTINGS optimize_use_projections = 1, force_optimize_projection = 1;
 
+DROP TABLE users_wide;
+
 -- test lightweight delete check condition (probably better put into lwd related)
 -- as with ctx->updated_header.has(RowExistsColumn::name) is not enough
+DROP TABLE IF EXISTS test;
+
 CREATE TABLE test
 (
     `a` int,
@@ -94,3 +100,5 @@ DELETE FROM test WHERE a = 5;
 
 ALTER TABLE test
     (MATERIALIZE INDEX idx);
+
+DROP TABLE test;
