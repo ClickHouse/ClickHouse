ee0f6c944cf819c9f036881b361921373492f38f
Max Kainov <max.kainov@clickhouse.com>
2025-01-16 17:26:51 +0100

    CI: Move Tidy build into stage Builds_1

--- tests/ci/test_ci_config.py
+++ tests/ci/test_ci_config.py
@@ -169,10 +169,19 @@ class TestCIConfig(unittest.TestCase):
         # check stages
         for job in CI.JobNames:
             if job in CI.BuildNames:
-                self.assertTrue(
-                    CI.get_job_ci_stage(job)
-                    in (CI.WorkflowStages.BUILDS_1, CI.WorkflowStages.BUILDS_2)
-                )
+                if job in (
+                    CI.BuildNames.BINARY_TIDY,
+                    CI.BuildNames.PACKAGE_RELEASE,
+                    CI.BuildNames.PACKAGE_AARCH64,
+                ):
+                    self.assertTrue(
+                        CI.get_job_ci_stage(job) in (CI.WorkflowStages.BUILDS_0,)
+                    )
+                else:
+                    self.assertTrue(
+                        CI.get_job_ci_stage(job)
+                        in (CI.WorkflowStages.BUILDS_1, CI.WorkflowStages.BUILDS_2)
+                    )
             else:
                 if job in (
                     CI.JobNames.STYLE_CHECK,
@@ -189,7 +198,11 @@ class TestCIConfig(unittest.TestCase):
                 else:
                     self.assertTrue(
                         CI.get_job_ci_stage(job)
-                        in (CI.WorkflowStages.TESTS_1, CI.WorkflowStages.TESTS_2),
+                        in (
+                            CI.WorkflowStages.TESTS_1,
+                            CI.WorkflowStages.TESTS_0,
+                            CI.WorkflowStages.TESTS_2,
+                        ),
                         msg=f"Stage for [{job}] is not correct",
                     )
 
@@ -200,10 +213,20 @@ class TestCIConfig(unittest.TestCase):
         # check stages
         for job in CI.JobNames:
             if job in CI.BuildNames:
-                self.assertTrue(
-                    CI.get_job_ci_stage(job)
-                    in (CI.WorkflowStages.BUILDS_1, CI.WorkflowStages.BUILDS_2)
-                )
+                if job in (
+                    CI.BuildNames.BINARY_TIDY,
+                    CI.BuildNames.PACKAGE_RELEASE,
+                    CI.BuildNames.PACKAGE_AARCH64,
+                ):
+                    self.assertTrue(
+                        CI.get_job_ci_stage(job) in (CI.WorkflowStages.BUILDS_0,),
+                        f"Invalid stage [{CI.get_job_ci_stage(job)}] for [{job}]",
+                    )
+                else:
+                    self.assertTrue(
+                        CI.get_job_ci_stage(job)
+                        in (CI.WorkflowStages.BUILDS_1, CI.WorkflowStages.BUILDS_2)
+                    )
             else:
                 if job in (
                     CI.JobNames.STYLE_CHECK,
@@ -220,7 +243,11 @@ class TestCIConfig(unittest.TestCase):
                 else:
                     self.assertTrue(
                         CI.get_job_ci_stage(job, non_blocking_ci=True)
-                        in (CI.WorkflowStages.TESTS_1, CI.WorkflowStages.TESTS_2_WW),
+                        in (
+                            CI.WorkflowStages.TESTS_1,
+                            CI.WorkflowStages.TESTS_0,
+                            CI.WorkflowStages.TESTS_2_WW,
+                        ),
                         msg=f"Stage for [{job}] is not correct",
                     )
 
