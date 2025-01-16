2b1e4f42ac7827be33a60b841c94d3a0d855b04c
Yakov Olkhovskiy <yakov@clickhouse.com>
2025-01-16 16:37:19 +0000

    better fix for #74215

--- src/AggregateFunctions/Combinators/AggregateFunctionNull.cpp
+++ src/AggregateFunctions/Combinators/AggregateFunctionNull.cpp
@@ -111,8 +111,8 @@ public:
               * To address this, we handle `nothing` in a special way in `FunctionNode::toASTImpl`.
               */
             if (properties.returns_default_when_only_null)
-                return std::make_shared<AggregateFunctionNothingUInt64>(arguments, Array());
-            return std::make_shared<AggregateFunctionNothingNull>(arguments, Array());
+                return std::make_shared<AggregateFunctionNothingUInt64>(arguments, params);
+            return std::make_shared<AggregateFunctionNothingNull>(arguments, params);
         }
 
         assert(nested_function);
