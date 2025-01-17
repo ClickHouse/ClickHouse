cb13f17b249c7b359b4990388b0985473d1d5c20
Nikita Taranov <nikita.taranov@clickhouse.com>
2025-01-17 14:23:39 +0100

    upd test

--- tests/queries/0_stateless/03312_squashing_with_low_card_mem_usage.sql
+++ tests/queries/0_stateless/03312_squashing_with_low_card_mem_usage.sql
@@ -21,5 +21,5 @@ SELECT t1.s
 FROM t AS t1
 INNER JOIN t2 ON substr(t1.s, 1, 1) = t2.s
 LIMIT 1e5
-SETTINGS max_threads = 32, max_memory_usage = '1Gi', join_algorithm = 'parallel_hash', min_joined_block_size_bytes = '1Mi'
+SETTINGS max_threads = 32, max_memory_usage = '2Gi', join_algorithm = 'parallel_hash', min_joined_block_size_bytes = '1Mi'
 FORMAT Null;
