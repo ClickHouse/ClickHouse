5d6aa41d854ef8ca6b1f8dd5f8755f6a025caf87
Yakov Olkhovskiy <yakov@clickhouse.com>
2025-01-17 13:34:07 +0000

    fix issue #74217

--- tests/queries/0_stateless/03311_constantnode.sql
+++ tests/queries/0_stateless/03311_constantnode.sql
@@ -1,1 +1,2 @@
 SELECT uniqUpTo(5)(CAST(unhex('00'), 'AggregateFunction(uniqUpTo(5), Nullable(Nothing))'));
+SELECT largestTriangleThreeBucketsMerge(4)(CAST(unhex('0101000000000000F03F000000000000F03F'), 'AggregateFunction(largestTriangleThreeBuckets(4), UInt8 ,UInt8)'));
