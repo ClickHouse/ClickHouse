a380807df8c456c0a977a20cde4fc5f5cb1e5212
Yakov Olkhovskiy <yakov@clickhouse.com>
2025-01-18 05:11:47 +0000

    fix issue #74293

--- tests/queries/0_stateless/03311_constantnode.sql
+++ tests/queries/0_stateless/03311_constantnode.sql
@@ -1,3 +1,4 @@
 SELECT uniqUpTo(5)(CAST(unhex('00'), 'AggregateFunction(uniqUpTo(5), Nullable(Nothing))'));
 SELECT largestTriangleThreeBucketsMerge(4)(CAST(unhex('0101000000000000F03F000000000000F03F'), 'AggregateFunction(largestTriangleThreeBuckets(4), UInt8 ,UInt8)'));
 SELECT map(('{"a":1,"b":1,"b":1}',),1, ('{"a":1}',),2, ('{"a":1,"c":1}',),2666514966)::Map(Tuple(JSON(max_dynamic_paths=2)),Variant(UInt32)) SETTINGS enable_variant_type = 1, allow_experimental_json_type = 1, type_json_skip_duplicated_paths = 1;
+SELECT materialize(100000000) AND 0, arrayFold((acc, x) -> x, [0, 1], toUInt8(0));
