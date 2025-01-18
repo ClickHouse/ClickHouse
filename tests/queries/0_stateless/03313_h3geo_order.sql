860e28ddd9e9faee2ff71b826fab34c64bc7b71a
Robert Schulze <robert@clickhouse.com>
2025-01-18 11:23:25 +0000

    Minor fixups

--- tests/queries/0_stateless/03313_h3geo_order.sql
+++
@@ -1,4 +0,0 @@
--- Tags: no-fasttest
-
-SELECT h3ToGeo(644325524701193974) SETTINGS h3togeo_lon_lat_result_order=true;
-SELECT h3ToGeo(644325524701193974) SETTINGS h3togeo_lon_lat_result_order=false;
