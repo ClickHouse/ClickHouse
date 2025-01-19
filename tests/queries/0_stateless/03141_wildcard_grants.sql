c86810e331347dca1f9de69fe7fee6c34dd5125a
pufit <pufit@clickhouse.com>
2025-01-18 20:56:39 -0500

    Additional test

--- tests/queries/0_stateless/03141_wildcard_grants.sql
+++ tests/queries/0_stateless/03141_wildcard_grants.sql
@@ -21,5 +21,6 @@ SELECT '---';
 
 GRANT SELECT(bar) ON foo.test* TO user_03141; -- { clientError SYNTAX_ERROR }
 GRANT SELECT(bar) ON foo.* TO user_03141; -- { clientError SYNTAX_ERROR }
+GRANT SELECT(bar) ON *.* TO user_03141; -- { clientError SYNTAX_ERROR }
 
 DROP USER user_03141;
