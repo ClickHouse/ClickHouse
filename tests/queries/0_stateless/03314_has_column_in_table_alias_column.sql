6a2003b4caf1bfe31ef23b91f2be25b8e2a208f4
Nikita Mikhaylov <mikhaylovnikitka@gmail.com>
2025-01-20 23:28:25 +0100

    default -> currentDatabase()

--- tests/queries/0_stateless/03314_has_column_in_table_alias_column.sql
+++ tests/queries/0_stateless/03314_has_column_in_table_alias_column.sql
@@ -1,4 +1,4 @@
 DROP TABLE IF EXISTS test;
 CREATE TABLE test(`a` String, `alias_col_a` String ALIAS a) ENGINE = MergeTree ORDER BY a;
-SELECT hasColumnInTable('default', 'test', 'alias_col_a');
+SELECT hasColumnInTable(currentDatabase(), 'test', 'alias_col_a');
 DROP TABLE test;
