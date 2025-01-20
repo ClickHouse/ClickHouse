d3b691c832e1e60d7e2188d3ae2fe46f3e157c2e
yariks5s <yaroslav.briukhovetskyi@clickhouse.com>
2025-01-20 17:21:19 +0000

    Give more memory usage for query.

--- tests/queries/0_stateless/03305_insert_with_a_lot_of_threads.sql
+++ tests/queries/0_stateless/03305_insert_with_a_lot_of_threads.sql
@@ -1,5 +1,5 @@
 -- Tags: long, no-parallel
-SET max_memory_usage = 4e8, max_rows_to_read = 300000000;
+SET max_memory_usage = 6e8, max_rows_to_read = 300000000;
 CREATE TABLE testing_memory (a UInt64, b String) engine=MergeTree ORDER BY ();
 SET enable_memory_based_pipeline_throttling = 1;
 INSERT INTO testing_memory SELECT number, toString(number) FROM system.numbers LIMIT 200000000 SETTINGS max_insert_threads=500;
