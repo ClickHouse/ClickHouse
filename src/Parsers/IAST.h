42956d1e57f2d127b34ed1f38ee1f2b44d999b17
Konstantin Vedernikov <vedernikovkostya@mail.ru>
2025-01-20 06:47:13 +0000

    fix

--- src/Parsers/IAST.h
+++ src/Parsers/IAST.h
@@ -365,6 +365,7 @@ public:
         SetTransactionSnapshot,
         AsyncInsertFlush,
         ParallelWithQuery,
+        Copy,
     };
     /// Return QueryKind of this AST query.
     virtual QueryKind getQueryKind() const { return QueryKind::None; }
