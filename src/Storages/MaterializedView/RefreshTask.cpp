0385be12e62c7e99cfdb9bf46c13f57deaa67180
robot-clickhouse-ci-1 <118761991+robot-clickhouse-ci-1@users.noreply.github.com>
2025-01-21 19:43:12 +0100

    Backport #73323 to 24.12: Fix refreshable MV crash on shutdown (#74823)
    Co-authored-by: robot-clickhouse <robot-clickhouse@users.noreply.github.com>

--- src/Storages/MaterializedView/RefreshTask.cpp
+++ src/Storages/MaterializedView/RefreshTask.cpp
@@ -307,6 +307,22 @@ void RefreshTask::wait()
     }
 }
 
+bool RefreshTask::tryJoinBackgroundTask(std::chrono::steady_clock::time_point deadline)
+{
+    std::unique_lock lock(mutex);
+
+    execution.cancel_ddl_queries.request_stop();
+
+    auto duration = deadline - std::chrono::steady_clock::now();
+    /// (Manually clamping to 0 because the standard library used to have (and possibly still has?)
+    ///  a bug that wait_until would wait forever if the timestamp is in the past.)
+    duration = std::max(duration, std::chrono::steady_clock::duration(0));
+    return refresh_cv.wait_for(lock, duration, [&]
+        {
+            return state != RefreshState::Running && state != RefreshState::Scheduling;
+        });
+}
+
 std::chrono::sys_seconds RefreshTask::getNextRefreshTimeslot() const
 {
     std::lock_guard guard(mutex);
