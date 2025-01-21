5d2c580b4730ce84ea985c88cfc3c295beddf4b5
Yarik Briukhovetskyi <114298166+yariks5s@users.noreply.github.com>
2025-01-21 18:48:28 +0100

    Update 03316_fixup_used_columns_exception.sh

--- tests/queries/0_stateless/03316_fixup_used_columns_exception.sh
+++ tests/queries/0_stateless/03316_fixup_used_columns_exception.sh
@@ -9,13 +9,12 @@ $CLICKHOUSE_CLIENT --query="DROP TABLE IF EXISTS test_exception;"
 
 create_status=$($CLICKHOUSE_CLIENT --query="
 CREATE TABLE test_exception(
-    `id` UInt64
+    id UInt64
 )
 ENGINE=MergeTree
-ORDER BY `All`;
-" 2>&1) || true
+ORDER BY All" 2>&1) || true
 
-if [[ $create_status == *"Missing columns: 'All' while processing: '`All`', required columns: 'All', available columns: 'id'"* ]]; then
+if [[ $create_status == *"required columns: 'All', available columns: 'id'"* ]]; then
     echo "Query got the right exception message."
 else
     echo "Unexpected behavior: $create_status"
