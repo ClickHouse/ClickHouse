14dfeec951598d4131577e1bef83b8fb264ec5fd
robot-clickhouse <robot-clickhouse@users.noreply.github.com>
2025-01-22 09:09:32 +0000

    Backport #74785 to 24.11: Fix reading Keeper logs from disk

--- src/Coordination/Changelog.h
+++ src/Coordination/Changelog.h
@@ -116,7 +116,8 @@ struct LogLocation
 {
     ChangelogFileDescriptionPtr file_description;
     size_t position;
-    size_t size;
+    size_t entry_size;
+    size_t size_in_file;
 };
 
 struct PrefetchedCacheEntry
