321cd4d4696b808230aa413aa01bdb73985de902
Amos Bird <amosbird@gmail.com>
2025-01-22 15:48:11 +0800

    Fix assertion when serialize empty tuple protobuf

---
+++ tests/queries/0_stateless/03314_empty_tuple_in_protobuf_format.sql
@@ -0,0 +1,8 @@
+DROP TABLE IF EXISTS t0;
+
+SET engine_file_truncate_on_insert = 1;
+CREATE TABLE t0 (c0 Tuple()) ENGINE = Memory();
+INSERT INTO TABLE t0 (c0) VALUES (()), (());
+INSERT INTO TABLE FUNCTION file('/tmp/test_03314.proto', 'Protobuf', 'c0 Tuple()') SELECT c0 FROM t0; -- serverError { NO_COLUMNS_SERIALIZED_TO_PROTOBUF_FIELDS }
+
+DROP TABLE t0;
