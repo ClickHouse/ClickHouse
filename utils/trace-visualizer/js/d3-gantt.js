0a0e2fcbe8cd1a2d678cd7cda58e1baa65579f86
serxa <sergei@clickhouse.com>
2025-01-23 18:27:06 +0000

    make filter case-insensitive

--- utils/trace-visualizer/js/d3-gantt.js
+++ utils/trace-visualizer/js/d3-gantt.js
@@ -288,7 +288,7 @@
                 }
             })
           .merge(bands)
-            .attr("class", d => "bar " + (filter != '' && !d.text.includes(filter) ? " bar-u" : " bar-f"))
+            .attr("class", d => "bar " + (filter != '' && !d.text.toLowerCase().includes(filter) ? " bar-u" : " bar-f"))
           .transition().duration(smooth? 250: 0)
             .attr("y", 0)
             .attr("transform", bandTransform)
@@ -446,7 +446,7 @@
     gantt.filter = function(pattern) {
         if (!arguments.length)
             return filter;
-        filter = pattern;
+        filter = pattern.toLowerCase();
         render();
         return gantt;
     }
