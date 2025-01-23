bd1ef84bf348d8e09514841a8cc28bdfc63f07a6
Alexey Milovidov <milovidov@clickhouse.com>
2025-01-23 23:42:10 +0100

    Update tests/queries/0_stateless/03317_json_with_progress_too_early.sh
    Co-authored-by: Konstantin Bogdanov <thevar1able@users.noreply.github.com>

--- tests/queries/0_stateless/03317_json_with_progress_too_early.sh
+++ tests/queries/0_stateless/03317_json_with_progress_too_early.sh
@@ -5,7 +5,7 @@ CUR_DIR=$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)
 # shellcheck source=../shell_config.sh
 . "$CUR_DIR"/../shell_config.sh
 
-# In previous versions, when there is a subquery, we start writing the progress (accumulated from processing the subquery)
+# In previous versions in presence of a subquery, we started writing the progress data (accumulated from processing the subquery)
 # during initialization of data format, which was before HTTP headers were written. 
 # This could led to the loss of HTTP headers, such as X-ClickHouse-QueryId and X-ClickHouse-Format, as well as Content-Type.
 ${CLICKHOUSE_CURL} -sS -v "${CLICKHOUSE_URL}&default_format=JSONEachRowWithProgress" -d "SELECT (SELECT 1)" 2>&1 | grep -F 'X-ClickHouse-Format: '
