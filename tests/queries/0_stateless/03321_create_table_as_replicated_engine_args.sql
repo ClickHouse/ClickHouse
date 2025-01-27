f3255e27d9984fb126efd1a72a18815fa1ddf5aa
Bharat Nallan Chakravarthy <bharat@clickhouse.com>
2025-01-26 19:20:54 -0800

    fix test

--- tests/queries/0_stateless/03321_create_table_as_replicated_engine_args.sql
+++ tests/queries/0_stateless/03321_create_table_as_replicated_engine_args.sql
@@ -10,8 +10,8 @@ CREATE TABLE test_03321.t1 (x UInt8, y String) ENGINE=ReplicatedMergeTree ORDER 
 -- While creating t2 with the table structure of t1, the AST for the create query would contain the engine args from t1. For this kind of queries, skip this validation if
 -- the value of the arguments match the default values and also skip throwing an exception.
 SET database_replicated_allow_replicated_engine_arguments=0;
-CREATE TABLE test_03321.t2 AS test_03321.t1
+CREATE TABLE test_03321.t2 AS test_03321.t1;
 
-SHOW CREATE TABLE test_03321.t2
+SHOW CREATE TABLE test_03321.t2;
 
 DROP DATABASE test_03321;
