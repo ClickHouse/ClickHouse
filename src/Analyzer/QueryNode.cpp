d45c023476a8bbb7f3300e61589d11a92d7546c1
János Benjamin Antal <benjamin.antal@clickhouse.com>
2025-01-28 17:14:05 +0000

    Remove interpolation together with projection

--- src/Analyzer/QueryNode.cpp
+++ src/Analyzer/QueryNode.cpp
@@ -20,8 +20,9 @@
 #include <Parsers/ASTSelectWithUnionQuery.h>
 #include <Parsers/ASTSetQuery.h>
 
-#include <Analyzer/Utils.h>
+#include <Analyzer/InterpolateNode.h>
 #include <Analyzer/UnionNode.h>
+#include <Analyzer/Utils.h>
 
 namespace DB
 {
@@ -86,6 +87,23 @@ void QueryNode::removeUnusedProjectionColumns(const std::unordered_set<size_t> &
 
     projection_nodes.erase(projection_nodes.begin() + write_index, projection_nodes.end());
     projection_columns.erase(projection_columns.begin() + write_index, projection_columns.end());
+
+    if (hasInterpolate())
+    {
+        std::unordered_set<String> used_projection_columns;
+        for (const auto & projection : projection_columns)
+            used_projection_columns.insert(projection.name);
+
+        auto & interpolate_node = getInterpolate();
+        auto & interpolate_list_nodes = interpolate_node->as<ListNode &>().getNodes();
+        std::erase_if(
+            interpolate_list_nodes,
+            [&used_projection_columns](const QueryTreeNodePtr & interpolate)
+            { return !used_projection_columns.contains(interpolate->as<InterpolateNode &>().getExpressionName()); });
+
+        if (interpolate_list_nodes.empty())
+            interpolate_node = nullptr;
+    }
 }
 
 void QueryNode::dumpTreeImpl(WriteBuffer & buffer, FormatState & format_state, size_t indent) const
