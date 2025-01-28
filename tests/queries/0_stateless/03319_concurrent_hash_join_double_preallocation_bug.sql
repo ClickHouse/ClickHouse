ceacf7a8c0e800ab4c6b7ebe92eaaef5aec873d9
Nikita Taranov <nikita.taranov@clickhouse.com>
2025-01-28 20:56:49 +0100

    fix test

--- tests/queries/0_stateless/03319_concurrent_hash_join_double_preallocation_bug.sql
+++ tests/queries/0_stateless/03319_concurrent_hash_join_double_preallocation_bug.sql
@@ -1,6 +1,7 @@
 -- Tags: no-tsan, no-asan, no-msan, no-ubsan, no-parallel-replicas
 
-drop table if exists t;
+drop table if exists lhs;
+drop table if exists rhs;
 
 create table lhs(a UInt64, b UInt64) Engine = MergeTree order by tuple();
 create table rhs(a UInt64, b UInt64) Engine = MergeTree order by tuple();
@@ -9,7 +10,7 @@ insert into lhs select number, number from numbers_mt(1e5);
 -- rhs should be bigger to trigger tables swap (see `query_plan_join_swap_table`)
 insert into rhs select number, number from numbers_mt(1e6);
 
-set max_threads = 8, query_plan_join_swap_table = 1, join_algorithm = 'parallel_hash';
+set max_threads = 8, query_plan_join_swap_table = 1, join_algorithm = 'parallel_hash', enable_analyzer = 1;
 
 -- First populate the cache of hash table sizes
 select * from lhs as t1 join rhs as t2 on t1.a = t2.a format Null;
