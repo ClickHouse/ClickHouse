39577887e71b558df8c03cb9e0a7cd1fb03a010f
avogar <avogar@clickhouse.com>
2025-01-31 15:10:13 +0000

    Fix build

--- src/Storages/MergeTree/MergeTask.cpp
+++ src/Storages/MergeTree/MergeTask.cpp
@@ -43,6 +43,7 @@
 #include <Processors/QueryPlan/ExtractColumnsStep.h>
 #include <Interpreters/PreparedSets.h>
 #include <Interpreters/MergeTreeTransaction.h>
+#include <Interpreters/createSubcolumnsExtractionActions.h>
 #include <QueryPipeline/QueryPipelineBuilder.h>
 
 #include "config.h"
