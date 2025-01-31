f6d20c3df974c2081c293c39e92fd96b6ebf8069
Pablo Marcos <pablo.marcos.oltra@clickhouse.com>
2025-01-31 10:06:31 +0000

    Fix build of gtest_coordination
    Also move as much implementation to .cpp as possible

--- src/Core/BackgroundSchedulePoolTaskHolder.h
+++ src/Core/BackgroundSchedulePoolTaskHolder.h
@@ -13,19 +13,19 @@ using BackgroundSchedulePoolTaskInfoPtr = std::shared_ptr<BackgroundSchedulePool
 class BackgroundSchedulePoolTaskHolder
 {
 public:
-    BackgroundSchedulePoolTaskHolder() = default;
-    explicit BackgroundSchedulePoolTaskHolder(const BackgroundSchedulePoolTaskInfoPtr & task_info_) : task_info(task_info_) {}
+    BackgroundSchedulePoolTaskHolder();
+    explicit BackgroundSchedulePoolTaskHolder(const BackgroundSchedulePoolTaskInfoPtr & task_info_);
     BackgroundSchedulePoolTaskHolder(const BackgroundSchedulePoolTaskHolder & other) = delete;
-    BackgroundSchedulePoolTaskHolder(BackgroundSchedulePoolTaskHolder && other) noexcept = default;
+    BackgroundSchedulePoolTaskHolder(BackgroundSchedulePoolTaskHolder && other) noexcept;
     BackgroundSchedulePoolTaskHolder & operator=(const BackgroundSchedulePoolTaskHolder & other) noexcept = delete;
-    BackgroundSchedulePoolTaskHolder & operator=(BackgroundSchedulePoolTaskHolder && other) noexcept = default;
+    BackgroundSchedulePoolTaskHolder & operator=(BackgroundSchedulePoolTaskHolder && other) noexcept;
 
     ~BackgroundSchedulePoolTaskHolder();
 
-    explicit operator bool() const { return task_info != nullptr; }
+    explicit operator bool() const;
 
-    BackgroundSchedulePoolTaskInfo * operator->() { return task_info.get(); }
-    const BackgroundSchedulePoolTaskInfo * operator->() const { return task_info.get(); }
+    BackgroundSchedulePoolTaskInfo * operator->();
+    const BackgroundSchedulePoolTaskInfo * operator->() const;
 
 private:
     BackgroundSchedulePoolTaskInfoPtr task_info;
