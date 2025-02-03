9718d54b6f4f6aaa7ae750cfd47cc8102f9877ab
Nikita Mikhailov <nikitamikhaylov@clickhouse.com>
2025-02-03 18:06:03 +0100

    Fix test

--- tests/queries/0_stateless/03326_client_ignore_error_multiquery.sh
+++ tests/queries/0_stateless/03326_client_ignore_error_multiquery.sh
@@ -6,6 +6,9 @@ CUR_DIR=$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)
 
 # We need to see exceptions from both queries
 echo "SELECT 1 LIMIT 1 WITH TIES BY 1; SELECT 1 FROM idontexist;" |
-    $CLICKHOUSE_CLIENT --send-logs-level error --ignore-error 2>&1 |
+    $CLICKHOUSE_CLIENT --ignore-error 2>&1 |
     grep -Fao -e 'Can not use WITH TIES alongside LIMIT BY' -e 'Unknown table expression identifier' |
-    wc -l
+# We will see 'Can not use WITH TIES alongside LIMIT BY' only once, because this is a client error.
+# and 'Unknown table expression identifier' one or two times, depends whether the log line from server
+# gets propagated back to the client. So 2 or 3 in total.
+    wc -l | grep -q -e "2" -e "3" && echo "Match"
