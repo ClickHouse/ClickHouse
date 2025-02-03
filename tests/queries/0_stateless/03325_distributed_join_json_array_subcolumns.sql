3863429c64c2ee5e320dda72fb1cdb532cabb9b4
Pavel Kruglov <48961922+Avogar@users.noreply.github.com>
2025-02-03 12:21:33 +0100

    Fix test

--- tests/queries/0_stateless/03325_distributed_join_json_array_subcolumns.sql
+++ tests/queries/0_stateless/03325_distributed_join_json_array_subcolumns.sql
@@ -18,7 +18,7 @@ CREATE TABLE test_distr
     id Int64,
     data JSON(arr1 Array(String), arr2 Array(Int32))
 )
-ENGINE = Distributed(test_cluster_one_shard_three_replicas_localhost, 'default', 'test', murmurHash2_32(id));
+ENGINE = Distributed(test_cluster_one_shard_three_replicas_localhost, currentDatabase(), 'test', murmurHash2_32(id));
 
 INSERT INTO test FORMAT Values (1, '{"arr1" : ["s1", "s2", "s3"], "arr2" : []}'), (2, '{"arr1" : ["s4", "s5"], "arr2" : [42]}');
 
