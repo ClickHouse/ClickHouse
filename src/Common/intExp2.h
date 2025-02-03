63d0ae8ea8d27487e6827cd8faa7c1a890018496
Raúl Marín <git@rmr.ninja>
2025-02-03 11:50:44 +0100

    Fix after rebase with master

--- src/Common/intExp2.h
+++ src/Common/intExp2.h
@@ -7,5 +7,9 @@
 /// On overflow, the function returns unspecified value.
 constexpr NO_SANITIZE_UNDEFINED uint64_t intExp2(int x)
 {
+    if (x < 0)
+        return 0;
+    if (x > 63)
+        return std::numeric_limits<uint64_t>::max();
     return 1ULL << x;
 }
