e2e67aed892feef8cd7158ad10cd21dc65ae1e02
Michael Kolupaev <michael.kolupaev@clickhouse.com>
2025-02-04 21:06:20 +0000

    Fix

--- src/Client/ClientBase.h
+++ src/Client/ClientBase.h
@@ -327,7 +327,7 @@ protected:
     bool select_into_file = false; /// If writing result INTO OUTFILE. It affects progress rendering.
     bool select_into_file_and_stdout = false; /// If writing result INTO OUTFILE AND STDOUT. It affects progress rendering.
     bool is_default_format = true; /// false, if format is set in the config or command line.
-    std::optional<size_t> insert_format_max_block_size_from_config = 0; /// Max block size when reading INSERT data.
+    std::optional<size_t> insert_format_max_block_size_from_config; /// Max block size when reading INSERT data.
     size_t max_client_network_bandwidth = 0; /// The maximum speed of data exchange over the network for the client in bytes per second.
 
     bool has_vertical_output_suffix = false; /// Is \G present at the end of the query string?
