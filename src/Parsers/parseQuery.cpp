ddc3fc2d6ea729c9ff1f5d257c4de208137a8abc
robot-clickhouse <robot-clickhouse@users.noreply.github.com>
2025-02-05 01:34:21 +0000

    Backport #74231 to 24.8: Update parseQuery.cpp (fix problem with "insert into select" with PostgreSQL interface)

--- src/Parsers/parseQuery.cpp
+++ src/Parsers/parseQuery.cpp
@@ -468,7 +468,7 @@ std::pair<const char *, bool> splitMultipartQuery(
 
         ast = parseQueryAndMovePosition(parser, pos, end, "", true, max_query_size, max_parser_depth, max_parser_backtracks);
 
-        if (ASTInsertQuery * insert = getInsertAST(ast))
+        if (ASTInsertQuery * insert = getInsertAST(ast); insert && insert->data)
         {
             /// Data for INSERT is broken on the new line
             pos = insert->data;
