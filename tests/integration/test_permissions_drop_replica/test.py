d928aa16382cf33ddf96394af003ad98ac7d6860
Bharat Nallan Chakravarthy <bharat@clickhouse.com>
2025-02-05 14:50:33 -0800

    better test for system drop replica

--- tests/integration/test_permissions_drop_replica/test.py
+++ tests/integration/test_permissions_drop_replica/test.py
@@ -20,46 +20,85 @@ node2 = cluster.add_instance(
     main_configs=main_configs,
     macros={"replica": "node2", "shard": "shard2"},
     with_zookeeper=True,
+    stay_alive=True,
 )
+
+
+def setup_nodes(nodes, shard):
+    for node in nodes:
+        node.query(
+            """
+                DROP TABLE IF EXISTS test.test_table;
+                DROP DATABASE IF EXISTS test;
+                DROP USER IF EXISTS test_user_xnhds;
+                CREATE DATABASE test;
+                SET database_replicated_allow_replicated_engine_arguments=2;  
+                CREATE TABLE test.test_table(date Date, id UInt32)
+                ENGINE = ReplicatedMergeTree('/clickhouse/tables/test/{shard}/replicated/test_table', '{replica}') ORDER BY id PARTITION BY toYYYYMM(date);
+            """.format(
+                shard=shard, replica=node.name
+            )
+        )
 
 
 @pytest.fixture(scope="module")
 def start_cluster():
     try:
         cluster.start()
+        setup_nodes([node1, node2], 1)
         yield cluster
     finally:
         cluster.shutdown()
 
 
 def test_drop_permissions(start_cluster):
-    node1.query("DROP DATABASE IF EXISTS r")
     node1.query(
-        f"CREATE DATABASE r ENGINE=Replicated('/clickhouse/databases/r', '{{shard}}', '{{replica}}')"
+        "INSERT INTO test.test_table SELECT number, toString(number) FROM numbers(100)"
     )
-    node1.query(
-        "CREATE TABLE r.t1 (x UInt8, y String) ENGINE=ReplicatedMergeTree ORDER BY x"
+
+    # check metadata for replica node2 in keeper
+    # this the node replica that we are going to drop later on
+    zk = cluster.get_kazoo_client("zoo1")
+    # check that the path from zk exists for the replica node2
+    assert (
+        zk.exists("/clickhouse/tables/test/1/replicated/test_table/replicas/node2")
+        is not None
     )
+
     # create user without any permissions
-    node1.query("CREATE USER foo;")
-    node1.query("REVOKE ALL ON *.* FROM foo;")
-    # try dropping replica using this user without any permissions
-    got_error = node1.query_and_get_error("SYSTEM DROP REPLICA 'node2'", user="foo")
+    node1.query("CREATE USER test_user_xnhds;")
+    node1.query("REVOKE ALL ON *.* FROM test_user_xnhds;")
+
+    # we won't be able to drop an active replica
+    assert (
+        "DB::Exception: Can't drop replica: node2, because it's active."
+        in node1.query_and_get_error("SYSTEM DROP REPLICA 'node2'")
+    )
+
+    # stop the server on replica node2 and assume it's gone forever
+    # this will allow us to drop this replica
+    node2.stop_clickhouse()
+
+    # drop replica node2 from replica node1 using user without privileges
+    got_error = node1.query_and_get_error(
+        "SYSTEM DROP REPLICA 'node2'", user="test_user_xnhds"
+    )
     # this operation should not fail silently
     assert (
         "DB::Exception: Access denied for SYSTEM DROP REPLICA. Not enough permissions to drop these databases:"
         in got_error
     )
-    # assert that the replica still exists
+
+    # drop replica node2 from replica node1 but using user with privileges
+    # this will remove all replicated table metadata belonging to node2 from keeper
+    node1.query("SYSTEM DROP REPLICA 'node2'")
+
+    # check that the metadata for replica node2 was removed from keeper
     assert (
-        node1.query("SELECT host_name FROM system.clusters WHERE replica_num=1") != ""
+        zk.exists(" /clickhouse/tables/test/1/replicated/test_table/replicas/node2")
+        is None
     )
-    # now query using default user (should have necessary permissions)
-    node1.query("SYSTEM DROP REPLICA 'node2'", user="default")
-    # assert that the replica was dropped successfully
-    assert (
-        node1.query("SELECT host_name FROM system.clusters WHERE replica_num=3").strip()
-        == ""
-    )
-    node1.query("DROP USER foo;")
-    node1.query("DROP DATABASE r")
+
+    node1.query("DROP USER test_user_xnhds;")
+    node1.query("DROP TABLE test.test_table;")
+    node1.query("DROP DATABASE test;")
