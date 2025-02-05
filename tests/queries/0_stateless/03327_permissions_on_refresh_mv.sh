bc2a31aa69dadc56d9e704b08e5c3ccee0f727bf
Bharat Nallan Chakravarthy <bharat@clickhouse.com>
2025-02-04 16:43:45 -0800

    better test

--- tests/queries/0_stateless/03327_permissions_on_refresh_mv.sh
+++ tests/queries/0_stateless/03327_permissions_on_refresh_mv.sh
@@ -10,12 +10,14 @@ CUR_DIR=$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)
 CLICKHOUSE_CLIENT="`echo "$CLICKHOUSE_CLIENT" | sed 's/--session_timezone[= ][^ ]*//g'`"
 CLICKHOUSE_CLIENT="`echo "$CLICKHOUSE_CLIENT --allow_materialized_view_with_bad_select=0 --session_timezone Etc/UTC"`"
 db=$CLICKHOUSE_DATABASE
+user_1="user_${CLICKHOUSE_TEST_UNIQUE_NAME}_1"
+user_2="user_${CLICKHOUSE_TEST_UNIQUE_NAME}_2"
 
 $CLICKHOUSE_CLIENT -q "
 drop view if exists mv_test_03327;
 drop table if exists test_03327;
-drop user if exists test_03327_user_1;
-drop user if exists test_03327_user_2;
+drop user if exists $user_1;
+drop user if exists $user_2;
 "
 
 $CLICKHOUSE_CLIENT -q "
@@ -28,29 +30,34 @@ create materialized view mv_test_03327 refresh every 10 minute append (x Int64) 
 
 # user with missing system views grant
 $CLICKHOUSE_CLIENT -q "
-create user test_03327_user_1;
-revoke all on *.* from test_03327_user_1;
-GRANT SHOW ON $db to test_03327_user_1;
+create user if not exists $user_1;
+revoke all on *.* from $user_1;
+grant select on $db.mv_test_03327 to $user_1;
+GRANT SHOW ON $db to $user_1;
 "
 
 # this should not work
-$CLICKHOUSE_CLIENT -u 'test_03327_user_1' -q "
+$CLICKHOUSE_CLIENT -u $user_1 -q "
 use $db;
 system refresh view mv_test_03327; -- { serverError ACCESS_DENIED }
+system start view mv_test_03327; -- { serverError ACCESS_DENIED }
+select * from mv_test_03327 format null;
+system wait view mv_test_03327; -- { serverError ACCESS_DENIED }
+system stop view mv_test_03327; -- { serverError ACCESS_DENIED }
+system cancel view mv_test_03327; -- { serverError ACCESS_DENIED }
 "
 
 # user with system views grant
 $CLICKHOUSE_CLIENT -q "
-create user test_03327_user_2;
-revoke all on *.* from test_03327_user_2;
-grant select on $db.test_03327 to test_03327_user_2;
-grant select on $db.mv_test_03327 to test_03327_user_2;
-grant show on $db to test_03327_user_2;
-grant system views on $db.* to test_03327_user_2;
+create user if not exists $user_2;
+revoke all on *.* from $user_2;
+grant select on $db.mv_test_03327 to $user_2;
+grant show on $db to $user_2;
+grant system views on $db.* to $user_2;
 "
 
 # all these operations should work
-$CLICKHOUSE_CLIENT -u 'test_03327_user_2' -q "
+$CLICKHOUSE_CLIENT -u $user_2 -q "
 use $db;
 system refresh view mv_test_03327;
 system start view mv_test_03327;
@@ -63,6 +70,6 @@ system cancel view mv_test_03327;
 $CLICKHOUSE_CLIENT -q "
 drop view mv_test_03327;
 drop table test_03327;
-drop user test_03327_user_1;
-drop user test_03327_user_2;
+drop user $user_1;
+drop user $user_2;
 "
