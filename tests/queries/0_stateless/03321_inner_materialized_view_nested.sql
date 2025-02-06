45c999e88eac549d63933b4fdbc39e65326a4644
Christoph Wurm <christoph@clickhouse.com>
2025-02-06 18:52:50 +0000

    Expand test

--- tests/queries/0_stateless/03321_inner_materialized_view_nested.sql
+++ tests/queries/0_stateless/03321_inner_materialized_view_nested.sql
@@ -13,3 +13,14 @@ SELECT * FROM mv;
 
 DROP TABLE t;
 DROP TABLE mv;
+
+CREATE TABLE t (x int, y Array(int), z Array(int)) ORDER BY x;
+
+CREATE MATERIALIZED VIEW mv ORDER BY () AS SELECT x, arrayZip(y, z)::Nested(a int, b int) n FROM t;
+
+INSERT INTO t VALUES (1, [1, 2], [3, 4]);
+
+SELECT n.a[1], n.a[2], n.b[1], n.b[2] FROM mv;
+
+DROP TABLE t;
+DROP TABLE mv;
