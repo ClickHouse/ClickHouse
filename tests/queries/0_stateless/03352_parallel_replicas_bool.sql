0ab8070bb132e93b59b9e5e20d003980637655b1
Michael Kolupaev <michael.kolupaev@clickhouse.com>
2025-02-07 09:29:42 +0000

    Fix test

--- tests/queries/0_stateless/03352_parallel_replicas_bool.sql
+++ tests/queries/0_stateless/03352_parallel_replicas_bool.sql
@@ -3,4 +3,4 @@ ENGINE = ReplicatedMergeTree('/clickhouse/{database}/test_03352', 'r1') ORDER BY
 
 INSERT INTO parallel_replicas_bool VALUES ('meow');
 
-SELECT materialize(true) FROM parallel_replicas_bool SETTINGS max_parallel_replicas = 2, allow_experimental_parallel_reading_from_replicas = 1, cluster_for_parallel_replicas='parallel_replicas';
+SELECT materialize(true) FROM parallel_replicas_bool SETTINGS max_parallel_replicas=2, allow_experimental_parallel_reading_from_replicas=1, cluster_for_parallel_replicas='parallel_replicas', enable_analyzer=1;
