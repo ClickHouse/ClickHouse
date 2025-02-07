388143dbc5af838ca4c1d4de46d356c341b0feab
Konstantin Morozov <just.morozov.k@gmail.com>
2025-02-07 14:39:19 +0000

    use hardlinks

--- src/Storages/MergeTree/IMergeTreeDataPart.h
+++ src/Storages/MergeTree/IMergeTreeDataPart.h
@@ -422,14 +422,6 @@ public:
     DataPartStoragePtr makeCloneInDetached(
         const String & prefix, const StorageMetadataPtr & metadata_snapshot, const DiskTransactionPtr & disk_transaction) const;
 
-    DataPartStoragePtr makeCloneInDetached(
-        const String & prefix,
-        const StorageMetadataPtr & metadata_snapshot,
-        const DiskTransactionPtr & disk_transaction,
-        bool copy_instead_of_hardlink) const;
-
-    bool isReplicatedZeroCopy() const;
-
     /// Makes full clone of part in specified subdirectory (relative to storage data directory, e.g. "detached") on another disk
     MutableDataPartStoragePtr makeCloneOnDisk(
         const DiskPtr & disk,
