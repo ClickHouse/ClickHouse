d9e9f9fbc1dab5297ff24413c3a1f6873b6dbb6b
Konstantin Vedernikov <vedernikovkostya@mail.ru>
2025-02-08 20:44:18 +0000

    update config

--- tests/integration/test_storage_iceberg/test.py
+++ tests/integration/test_storage_iceberg/test.py
@@ -1552,7 +1552,6 @@ def test_tuple_evolved_nested(
             ['1', "(1.23,(4.559999942779541,NULL))", '(1,2)']
         ],
     )
-
     execute_spark_query(
         f"""
             ALTER TABLE {TABLE_NAME} ALTER COLUMN b.b.nc FIRST;
