702a9257b79e664aefefae0a6382fe28884551ad
Alexey Milovidov <milovidov@clickhouse.com>
2025-02-08 03:47:22 +0100

    Update tests

--- tests/queries/0_stateless/00989_parallel_parts_loading.sql
+++ tests/queries/0_stateless/00989_parallel_parts_loading.sql
@@ -6,7 +6,7 @@ CREATE TABLE mt (x UInt64) ENGINE = MergeTree ORDER BY x SETTINGS parts_to_delay
 
 SYSTEM STOP MERGES mt;
 
-SET max_block_size = 1, min_insert_block_size_rows = 0, min_insert_block_size_bytes = 0;
+SET max_block_size = 1, min_insert_block_size_rows = 0, min_insert_block_size_bytes = 0, max_execution_time = 600;
 INSERT INTO mt SELECT * FROM numbers(1000);
 SET max_block_size = 65536;
 
