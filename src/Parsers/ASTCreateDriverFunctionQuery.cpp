136b7d32c9b378173c28406e40497a12944e7ee2
Daniil Timizhev <ditimizhev@ya.ru>
2025-02-08 20:21:34 +0300

    create new statement for udf with engine

---
+++ src/Parsers/ASTCreateDriverFunctionQuery.cpp
@@ -0,0 +1,69 @@
+#include <IO/Operators.h>
+#include <Parsers/ASTCreateDriverFunctionQuery.h>
+#include <Parsers/ASTExpressionList.h>
+#include <Parsers/ASTFunction.h>
+#include <Common/quoteString.h>
+
+
+namespace DB
+{
+
+ASTPtr ASTCreateDriverFunctionQuery::clone() const
+{
+    auto res = std::make_shared<ASTCreateDriverFunctionQuery>(*this);
+    res->children.clear();
+
+    res->function_name = function_name->clone();
+    res->children.push_back(res->function_name);
+
+    res->function_params = function_params->clone();
+    res->children.push_back(res->function_params);
+
+    res->function_return_type = function_return_type->clone();
+    res->children.push_back(res->function_return_type);
+
+    res->engine_name = engine_name->clone();
+    res->children.push_back(res->engine_name);
+
+    res->function_body = function_body->clone();
+    res->children.push_back(res->function_body);
+
+    return res;
+}
+
+void ASTCreateDriverFunctionQuery::formatImpl(
+    WriteBuffer & ostr, const IAST::FormatSettings & settings, IAST::FormatState & state, IAST::FormatStateStacked frame) const
+{
+    ostr << (settings.hilite ? hilite_keyword : "") << "CREATE FUNCTION ";
+    ostr << (settings.hilite ? hilite_none : "");
+    ostr << (settings.hilite ? hilite_identifier : "") << backQuoteIfNeed(getFunctionName()) << (settings.hilite ? hilite_none : "");
+
+    ostr << "(";
+    function_params->format(ostr, settings, state, frame);
+    ostr << ")";
+
+    ostr << " RETURNS ";
+    function_return_type->format(ostr, settings, state, frame);
+
+    ostr << " ENGINE = ";
+    ostr << (settings.hilite ? hilite_none : "");
+    ostr << (settings.hilite ? hilite_identifier : "") << backQuoteIfNeed(getEngineName()) << (settings.hilite ? hilite_none : "");
+
+    ostr << " AS ";
+    function_body->format(ostr, settings, state, frame);
+}
+
+String ASTCreateDriverFunctionQuery::getFunctionName() const
+{
+    String name;
+    tryGetIdentifierNameInto(function_name, name);
+    return name;
+}
+
+String ASTCreateDriverFunctionQuery::getEngineName() const
+{
+    String name;
+    tryGetIdentifierNameInto(engine_name, name);
+    return name;
+}
+}
---
+++ src/Parsers/ParserCreateDriverFunctionQuery.cpp
@@ -0,0 +1,97 @@
+#include <Parsers/ParserCreateDriverFunctionQuery.h>
+
+#include <Parsers/ASTCreateDriverFunctionQuery.h>
+#include <Parsers/CommonParsers.h>
+#include <Parsers/ExpressionElementParsers.h>
+#include <Parsers/Lexer.h>
+#include <Parsers/ParserCreateQuery.h>
+#include <Parsers/ParserDataType.h>
+
+
+namespace DB
+{
+
+bool ParserCreateDriverFunctionQuery::parseImpl(IParser::Pos & pos, ASTPtr & node, Expected & expected)
+{
+    ParserKeyword s_create(Keyword::CREATE);
+    ParserKeyword s_function(Keyword::FUNCTION);
+    ParserIdentifier function_name_p;
+    ParserToken s_lparen(TokenType::OpeningRoundBracket);
+    ParserToken s_rparen(TokenType::ClosingRoundBracket);
+    ParserNameTypePairList function_params_p;
+    ParserKeyword s_returns(Keyword::RETURNS);
+    ParserDataType function_return_type_p;
+    ParserKeyword s_engine(Keyword::ENGINE);
+    ParserToken s_equals(TokenType::Equals);
+    ParserIdentifier engine_name_p;
+    ParserKeyword s_as(Keyword::AS);
+    ParserToken s_dollar(TokenType::DollarSign);
+    ParserStringLiteral function_body_p;
+
+    ASTPtr function_name;
+    ASTPtr function_params;
+    ASTPtr function_return_type;
+    ASTPtr engine_name;
+    ASTPtr function_body;
+
+    if (!s_create.ignore(pos, expected))
+        return false;
+
+    if (!s_function.ignore(pos, expected))
+        return false;
+
+    if (!function_name_p.parse(pos, function_name, expected))
+        return false;
+
+    if (!s_lparen.ignore(pos, expected))
+        return false;
+
+    if (!function_params_p.parse(pos, function_params, expected))
+        return false;
+
+    if (!s_rparen.ignore(pos, expected))
+        return false;
+
+    if (!s_returns.ignore(pos, expected))
+        return false;
+
+    if (!function_return_type_p.parse(pos, function_return_type, expected))
+        return false;
+
+    if (!s_engine.ignore(pos, expected))
+        return false;
+
+    if (!s_equals.ignore(pos, expected))
+        return false;
+
+    if (!engine_name_p.parse(pos, engine_name, expected))
+        return false;
+
+    if (!s_as.ignore(pos, expected))
+        return false;
+
+    if (!function_body_p.parse(pos, function_body, expected))
+        return false;
+
+    auto create_function_query = std::make_shared<ASTCreateDriverFunctionQuery>();
+    node = create_function_query;
+
+    create_function_query->function_name = function_name;
+    create_function_query->children.push_back(function_name);
+
+    create_function_query->function_params = function_params;
+    create_function_query->children.push_back(function_params);
+
+    create_function_query->function_return_type = function_return_type;
+    create_function_query->children.push_back(function_return_type);
+
+    create_function_query->engine_name = engine_name;
+    create_function_query->children.push_back(engine_name);
+
+    create_function_query->function_body = function_body;
+    create_function_query->children.push_back(function_body);
+
+    return true;
+}
+
+}
