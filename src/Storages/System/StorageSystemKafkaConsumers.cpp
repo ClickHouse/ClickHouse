153af2db94634b1a6e929c514fa811357be4ac2c
Azat Khuzhin <a3at.mail@gmail.com>
2025-02-10 12:37:33 +0100

    Update src/Storages/System/StorageSystemKafkaConsumers.cpp

--- src/Storages/System/StorageSystemKafkaConsumers.cpp
+++ src/Storages/System/StorageSystemKafkaConsumers.cpp
@@ -43,7 +43,7 @@ ColumnsDescription StorageSystemKafkaConsumers::getColumnsDescription()
         {"num_rebalance_revocations", std::make_shared<DataTypeUInt64>(), "Number of times the consumer was revoked its partitions."},
         {"num_rebalance_assignments", std::make_shared<DataTypeUInt64>(), "Number of times the consumer was assigned to Kafka cluster."},
         {"is_currently_used", std::make_shared<DataTypeUInt8>(), "The flag which shows whether the consumer is in use."},
-        {"last_used", std::make_shared<DataTypeDateTime64>(6), "The last time this consumer was in use, unix time in microseconds."},
+        {"last_used", std::make_shared<DataTypeDateTime64>(6), "The last time this consumer was in use."},
         {"rdkafka_stat", std::make_shared<DataTypeString>(), "Library internal statistic. Set statistics_interval_ms to 0 disable, default is 3000 (once in three seconds)."},
     };
 }
