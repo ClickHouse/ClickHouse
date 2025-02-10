d94edca37e10c355869e868a3a8c73405f059db4
Miсhael Stetsyuk <59827607+mstetsyuk@users.noreply.github.com>
2025-02-10 18:33:22 +0000

    Update 03328_formatting_assignment_expression.sql

--- tests/queries/0_stateless/03328_formatting_assignment_expression.sql
+++ tests/queries/0_stateless/03328_formatting_assignment_expression.sql
@@ -1,3 +1,2 @@
-SELECT formatQuery('ALTER TABLE t (UPDATE c = (1 AS a) WHERE true)');
-
-SELECT formatQuery('ALTER TABLE t (UPDATE c = a > 1 WHERE true)');
+SELECT formatQuerySingleLine('ALTER TABLE t (UPDATE c = (1 AS a) WHERE true)');
+SELECT formatQuerySingleLine('ALTER TABLE t (UPDATE c = a > 1 WHERE true)');
