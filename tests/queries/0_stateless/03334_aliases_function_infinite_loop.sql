b36b991fafe6292504e8ed5916062675a064a20b
Alexey Milovidov <milovidov@clickhouse.com>
2025-02-10 09:01:02 +0100

    Add a test for #74976

---
+++ tests/queries/0_stateless/03334_aliases_function_infinite_loop.sql
@@ -0,0 +1,7 @@
+-- Tags: no-parallel
+-- ^ creates a function
+DROP FUNCTION IF EXISTS f0;
+DROP VIEW IF EXISTS v0;
+CREATE FUNCTION f0 AS (x) -> toInt32((x AS c0) % 2 AS c1);
+CREATE VIEW v0 AS (SELECT 0 AS c0, c0 AS c1, f0(c1) AS c2); -- { serverError UNKNOWN_IDENTIFIER }
+DROP FUNCTION f0;
