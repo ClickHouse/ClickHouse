bda225243002832779c0afe10f259658bf1caa03
Nikita Mikhaylov <mikhaylovnikitka@gmail.com>
2025-02-11 23:38:48 +0100

    No fasttest

--- tests/queries/0_stateless/03356_postgresql_mysql_endpoint_parsing.sql
+++ tests/queries/0_stateless/03356_postgresql_mysql_endpoint_parsing.sql
@@ -1,3 +1,4 @@
+-- Tags: no-fasttest
 -- We test only that parsing of the endpoint works in this test.
 CREATE OR REPLACE TABLE tablefunc01 (x int) AS postgresql('localhost:9005/postgresql', 'postgres_db', 'postgres_table', 'postgres_user', '124444');
 CREATE OR REPLACE TABLE tablefunc02 (x int) AS mysql('127.0.0.1:9004/mysql', 'mysql_db', 'mysql_table', 'mysql_user','123123');
