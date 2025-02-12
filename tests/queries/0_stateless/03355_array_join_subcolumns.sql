f6bd6aafb121fc4d113975acb2e0408d5f3102f3
Pavel Kruglov <48961922+Avogar@users.noreply.github.com>
2025-02-13 00:04:38 +0100

    Update 03355_array_join_subcolumns.sql

--- tests/queries/0_stateless/03355_array_join_subcolumns.sql
+++ tests/queries/0_stateless/03355_array_join_subcolumns.sql
@@ -1,3 +1,7 @@
+-- Tags: no-parallel-replicas
+
+set enable_analyzer=1;
+
 drop table if exists test;
 create table test (arr Array(UInt64), t Tuple(a Array(UInt32), b Array(UInt32))) engine=Memory;
 insert into test select [1, 2, 3], tuple([1, 2], [1, 2, 3, 4]);
