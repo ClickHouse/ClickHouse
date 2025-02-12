7a2f228d6d50bd7050fd051af6991a9aeda72a64
Nikita Mikhailov <nikitamikhaylov@clickhouse.com>
2025-02-12 12:44:44 +0100

    Updated a test

--- tests/queries/0_stateless/03356_postgresql_mysql_endpoint_parsing.sql
+++ tests/queries/0_stateless/03356_postgresql_mysql_endpoint_parsing.sql
@@ -1,4 +1,6 @@
 -- Tags: no-fasttest
 -- We test only that parsing of the endpoint works in this test.
-CREATE OR REPLACE TABLE tablefunc01 (x int) AS postgresql('localhost:9005/postgresql', 'postgres_db', 'postgres_table', 'postgres_user', '124444');
-CREATE OR REPLACE TABLE tablefunc02 (x int) AS mysql('127.0.0.1:9004/mysql', 'mysql_db', 'mysql_table', 'mysql_user','123123');
+DROP TABLE IF EXISTS tablefunc01;
+DROP TABLE IF EXISTS tablefunc02;
+CREATE TABLE tablefunc01 (x int) AS postgresql('localhost:9005/postgresql', 'postgres_db', 'postgres_table', 'postgres_user', '124444');
+CREATE TABLE tablefunc02 (x int) AS mysql('127.0.0.1:9004/mysql', 'mysql_db', 'mysql_table', 'mysql_user','123123');
