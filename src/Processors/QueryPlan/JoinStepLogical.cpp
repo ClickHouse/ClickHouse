2ed3dd5a4d16681be9397f674ab28266d7cdd397
Vladimir Cherkasov <vdimir@clickhouse.com>
2025-02-13 18:17:32 +0100

    Update src/Processors/QueryPlan/JoinStepLogical.cpp

--- src/Processors/QueryPlan/JoinStepLogical.cpp
+++ src/Processors/QueryPlan/JoinStepLogical.cpp
@@ -388,7 +388,7 @@ bool addJoinConditionToTableJoin(JoinCondition & join_condition, TableJoin::Join
         {
             auto [left_key_node, right_key_node] = leftAndRightNodes(predicate);
             bool null_safe_comparison = PredicateOperator::NullSafeEquals == predicate.op;
-            if (null_safe_comparison && isNullableOrLowCardinalityNullable(left_key_node->result_type) && isNullableOrLowCardinalityNullable(right_key_node->result_type))
+            if (null_safe_comparison && left_key_node->result_type->isNullable() && right_key_node->result_type->isNullable())
             {
                 /**
                   * In case of null-safe comparison (a IS NOT DISTINCT FROM b),
