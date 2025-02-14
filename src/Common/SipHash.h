510570386f9d0d3082b9a83689b529b36de0255d
Raúl Marín <git@rmr.ninja>
2025-02-14 16:40:44 +0100

    Continue hiding Exception.h

--- src/Common/SipHash.h
+++ src/Common/SipHash.h
@@ -21,16 +21,10 @@
 #include <base/types.h>
 #include <base/unaligned.h>
 #include <base/hex.h>
-#include <Common/Exception.h>
 #include <Common/transformEndianness.h>
 
 #include <city.h>
 
-
-namespace DB::ErrorCodes
-{
-    extern const int LOGICAL_ERROR;
-}
 
 #define SIPROUND                                                  \
     do                                                            \
@@ -205,30 +199,11 @@ public:
         return res;
     }
 
-    UInt128 get128Reference()
-    {
-        if (!is_reference_128)
-            throw DB::Exception(
-                DB::ErrorCodes::LOGICAL_ERROR, "Can't call get128Reference when is_reference_128 is not set");
-        finalize();
-        const auto lo = v0 ^ v1 ^ v2 ^ v3;
-        v1 ^= 0xdd;
-        SIPROUND;
-        SIPROUND;
-        SIPROUND;
-        SIPROUND;
-        const auto hi = v0 ^ v1 ^ v2 ^ v3;
-
-        UInt128 res = hi;
-        res <<= 64;
-        res |= lo;
-        return res;
-    }
+    UInt128 get128Reference();
 };
 
 
 #undef ROTL
-#undef SIPROUND
 
 #include <cstddef>
 
--- src/IO/ConcatSeekableReadBuffer.cpp
+++ src/IO/ConcatSeekableReadBuffer.cpp
@@ -1,5 +1,6 @@
 #include <IO/ConcatSeekableReadBuffer.h>
 
+#include <Common/Exception.h>
 
 namespace DB
 {
