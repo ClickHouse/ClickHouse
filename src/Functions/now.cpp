9cb4ed58c344614810d85a2abba6764c746ac5d5
Michael Kolupaev <michael.kolupaev@clickhouse.com>
2025-02-15 03:42:45 +0000

    ErrorCodes

--- src/Functions/now.cpp
+++ src/Functions/now.cpp
@@ -19,6 +19,7 @@ namespace ErrorCodes
 {
     extern const int ILLEGAL_TYPE_OF_ARGUMENT;
     extern const int NUMBER_OF_ARGUMENTS_DOESNT_MATCH;
+    extern const int CANNOT_CLOCK_GETTIME;
 }
 
 namespace
