0c2bce53cb18606c6f051d2f0c4c1239131c97a3
Mikhail f. Shiryaev <felixoid@clickhouse.com>
2025-02-16 13:18:58 +0100

    Adjust parallel_skip.sh to manually edited parallel_skip.json

--- tests/integration/parallel_skip.sh
+++ tests/integration/parallel_skip.sh
@@ -5,37 +5,50 @@
 NON_PARALLEL_TESTS=(
   test_atomic_drop_table/
   test_attach_without_fetching/
-  test_broken_part_during_merge/
   test_cleanup_dir_after_bad_zk_conn/
   test_consistent_parts_after_clone_replica/
+  test_crash_log/
   test_cross_replication/
   test_ddl_worker_non_leader/
   test_delayed_replica_failover/
   test_dictionary_allow_read_expired_keys/
   test_disabled_mysql_server/
-  test_distributed_ddl  # all test_distributed_ddl* tests
   test_distributed_respect_user_timeouts/
-  test_drop_replica  # all test_drop_replica* tests
-  test_hedged_requests/
-  test_hedged_requests_parallel/
+  test_dns_cache/
+  test_global_overcommit_tracker/
+  test_grpc_protocol/
+  test_host_regexp_multiple_ptr_records/
+  test_http_failover/
   test_https_replication/
-  test_insert_into_distributed  # all test_insert_into_distributed*
+  test_insert_into_distributed/
+  test_insert_into_distributed_through_materialized_view/
+  test_keeper_map/
   test_keeper_multinode_simple/
+  test_keeper_two_nodes_cluster/
   test_limited_replicated_fetches/
-  test_materialized_mysql_database/
+  test_merge_tree_s3/
+  test_mysql_database_engine/
   test_parts_delete_zookeeper/
-  test_polymorphic_parts/
+  test_profile_max_sessions_for_user/
   test_quorum_inserts_parallel/
   test_random_inserts/
-  test_reload_clusters_config/
+  test_redirect_url_storage/
   test_replace_partition/
-  test_replicated_database  # all test_replicated_database*
+  test_replicated_database/
   test_replicated_fetches_timeouts/
+  test_replicated_merge_tree_wait_on_shutdown/
+  test_server_reload/
   test_storage_kafka/
   test_storage_kerberized_kafka/
-  test_system_clusters_actual_information/
+  test_storage_s3/
+  test_system_flush_logs/
+  test_system_logs/
   test_system_metrics/
   test_system_replicated_fetches/
+  test_ttl_move/
+  test_user_ip_restrictions/
+  test_zookeeper_config_load_balancing/
+  test_zookeeper_fallback_session/
 )
 # 2. Filter known tests that are currently not run in parallel
 printf "%s\n" "${NON_PARALLEL_TESTS[@]}" | jq -R -n '[inputs] | .' > parallel_skip.json
