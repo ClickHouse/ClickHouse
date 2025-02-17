46ab10756a2fc16c8f27cd1fb17104297805d94a
Bhavneek Singh <bhavneek2002@gmail.com>
2025-02-17 10:46:36 +0900

    execute generic working now for nested but not yet impleented

--- src/Functions/array/transposeBits.cpp
+++ src/Functions/array/transposeBits.cpp
@@ -46,9 +46,6 @@ public:
 private:
     template <typename T>
     static bool executeNumber(const IColumn & src_data, const ColumnArray::Offsets & src_offsets, IColumn & res_data);
-
-    static bool executeFixedString(const IColumn & src_data, const ColumnArray::Offsets & src_offsets, IColumn & res_data);
-    static bool executeString(const IColumn & src_data, const ColumnArray::Offsets & src_array_offsets, IColumn & res_data);
     static bool executeGeneric(const IColumn & src_data, const ColumnArray::Offsets & src_array_offsets, IColumn & res_data);
 };
 
