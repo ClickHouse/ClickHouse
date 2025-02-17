6d75517e66232df2baeafb88c27ed7a7c1fe5c9f
Dergousov Maxim <dergousovmaxim99@gmail.com>
2025-02-17 15:35:02 +0000

    fix: style

--- src/Formats/PngWriter.h
+++ src/Formats/PngWriter.h
@@ -10,18 +10,55 @@ namespace DB
 namespace ErrorCodes
 {
 extern const int LOGICAL_ERROR;
+extern const int CANNOT_CONVERT_TYPE;
 }
 
 class WriteBuffer;
 
-/// Utility class for writing in the png format.
-/// Just provides useful functions to serialize data
-class PngWriter : boost::noncopyable
+/**
+ * RAII-wrapper for managing libpng resources
+ */
+class PngStructWrapper
+{
+public:
+    PngStructWrapper(png_structp png_ptr_, png_infop info_ptr_);
+
+    ~PngStructWrapper();
+
+    PngStructWrapper(const PngStructWrapper &) = delete;
+
+    PngStructWrapper & operator=(const PngStructWrapper &) = delete;
+
+    PngStructWrapper(PngStructWrapper && other) noexcept;
+
+    PngStructWrapper & operator=(PngStructWrapper && other) noexcept;
+
+    png_structp getPngPtr() const { return png_ptr; }
+
+    png_infop getInfoPtr() const { return info_ptr; }
+
+private:
+    void cleanup();
+
+    png_structp png_ptr = nullptr;
+    png_infop   info_ptr = nullptr;
+};
+
+
+/** 
+ * Utility class for writing in the png format.
+ * Just provides useful functions to serialize data 
+ */
+class PngWriter final : boost::noncopyable
 {
 public:
     explicit PngWriter(WriteBuffer &, Int32);
 
     ~PngWriter();
+
+    PngWriter(PngWriter && other) noexcept;
+    
+    PngWriter & operator=(PngWriter && other) noexcept;
 
     void startImage(size_t width_, size_t height_);
 
@@ -42,8 +79,7 @@ private:
     Int32 bit_depth;
     bool started = false;
 
-    png_struct_def * png_ptr = nullptr;
-    png_info_def * info_ptr = nullptr;
+    std::unique_ptr<PngStructWrapper> png_resource;
 };
 
 }
