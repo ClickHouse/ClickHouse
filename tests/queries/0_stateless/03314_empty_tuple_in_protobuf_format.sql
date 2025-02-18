9a7cfd996a053c025edb416180863c93c28bf9ff
Nikita Mikhaylov <mikhaylovnikitka@gmail.com>
2025-02-18 12:30:52 +0100

    /dev/null

--- tests/queries/0_stateless/03314_empty_tuple_in_protobuf_format.sql
+++ tests/queries/0_stateless/03314_empty_tuple_in_protobuf_format.sql
@@ -5,6 +5,6 @@ DROP TABLE IF EXISTS t0;
 SET engine_file_truncate_on_insert = 1;
 CREATE TABLE t0 (c0 Tuple()) ENGINE = Memory();
 INSERT INTO TABLE t0 (c0) VALUES (()), (());
-INSERT INTO TABLE FUNCTION file('/tmp/test_03314.proto', 'Protobuf', 'c0 Tuple()') SELECT c0 FROM t0; -- serverError { NO_COLUMNS_SERIALIZED_TO_PROTOBUF_FIELDS }
+INSERT INTO TABLE FUNCTION file('/dev/null', 'Protobuf', 'c0 Tuple()') SELECT c0 FROM t0; -- serverError { NO_COLUMNS_SERIALIZED_TO_PROTOBUF_FIELDS }
 
 DROP TABLE t0;
