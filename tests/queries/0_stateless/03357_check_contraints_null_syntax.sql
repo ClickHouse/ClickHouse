733d223e1233fed28841153d2f752ca3d119cf86
Nikita Mikhailov <nikitamikhaylov@clickhouse.com>
2025-02-18 15:05:05 +0100

    Close: https://github.com/ClickHouse/ClickHouse/issues/73689

---
+++ tests/queries/0_stateless/03357_check_contraints_null_syntax.sql
@@ -0,0 +1,10 @@
+CREATE TABLE mister_table
+(
+    c0 Nullable(Int),
+    CONSTRAINT c1 check c0.null
+)
+ENGINE = Memory();
+
+CREATE TABLE mister_table_2 (c0 Nullable(Int)) ENGINE = MergeTree() ORDER BY tuple();
+CREATE MATERIALIZED VIEW mister_view ENGINE = MergeTree() ORDER BY tuple() AS (SELECT mister_table_2.c0.null AS c0 FROM mister_table_2);
+INSERT INTO mister_table_2 VALUES (1);
