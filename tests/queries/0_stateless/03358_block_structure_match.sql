dd631bf757bad6a61a60187a6a6483e5a41409dc
Nikita Mikhaylov <mikhaylovnikitka@gmail.com>
2025-02-18 01:00:53 +0100

    Analyzer

--- tests/queries/0_stateless/03358_block_structure_match.sql
+++ tests/queries/0_stateless/03358_block_structure_match.sql
@@ -1,3 +1,5 @@
+SET enable_analyzer=1;
+
 SELECT * FROM (SELECT 1 AS a, 2 AS b FROM system.one INNER JOIN system.one USING (dummy) UNION ALL SELECT 3 AS a, 4 AS b FROM system.one) WHERE a != 10 ORDER BY a ASC, a != 10 ASC, b ASC;
 
 SELECT 4
