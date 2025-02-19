88596620757c1b04ea844038ed869fe190b9b270
Alexey Milovidov <milovidov@clickhouse.com>
2025-02-19 04:39:46 +0100

    Update 03360_cpu_overload_metric.sql

--- tests/queries/0_stateless/03360_cpu_overload_metric.sql
+++ tests/queries/0_stateless/03360_cpu_overload_metric.sql
@@ -1,4 +1,5 @@
 -- A crude test. Overloads the server for two seconds and checks that the overload was recorded.
-SELECT count() FROM system.numbers_mt SETTINGS max_threads = 256, use_concurrency_control = 0, max_execution_time = 2, timeout_overflow_mode = 'break', max_rows_to_read = 0, max_bytes_to_read = 0;
+SELECT count() FROM system.numbers_mt SETTINGS max_threads = 256, use_concurrency_control = 0, max_execution_time = 2, timeout_overflow_mode = 'break',
+    max_rows_to_read = 0, max_bytes_to_read = 0, max_rows_to_read_leaf = 0, max_bytes_to_read_leaf = 0;
 SYSTEM FLUSH LOGS;
 SELECT 1 FROM system.asynchronous_metric_log WHERE event_date >= yesterday() AND metric = 'OSCPUOverload' AND value >= 1 ORDER BY event_time DESC LIMIT 1;
