423cf55aca45100167c061b82923708cde0ad2f3
Vladimir Cherkasov <vdimir@clickhouse.com>
2025-02-19 14:49:47 +0100

    Another build fix

--- programs/library-bridge/LibraryBridgeHandlerFactory.cpp
+++ programs/library-bridge/LibraryBridgeHandlerFactory.cpp
@@ -28,9 +28,9 @@ std::unique_ptr<HTTPRequestHandler> LibraryBridgeHandlerFactory::createRequestHa
     if (request.getMethod() == Poco::Net::HTTPRequest::HTTP_GET)
     {
         if (uri.getPath() == "/extdict_ping")
-            return std::make_unique<ExternalDictionaryLibraryBridgeExistsHandler>(keep_alive_timeout, getContext(), libraries_paths);
+            return std::make_unique<ExternalDictionaryLibraryBridgeExistsHandler>(keep_alive_timeout, getContext());
         else if (uri.getPath() == "/catboost_ping")
-            return std::make_unique<CatBoostLibraryBridgeExistsHandler>(keep_alive_timeout, getContext(), libraries_paths);
+            return std::make_unique<CatBoostLibraryBridgeExistsHandler>(keep_alive_timeout, getContext());
     }
 
     if (request.getMethod() == Poco::Net::HTTPRequest::HTTP_POST)
