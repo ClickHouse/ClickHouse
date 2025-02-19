0eb298bac245b5c4d7ae247eaa75a4a170e81217
Maksim Kita <kitaetoya@gmail.com>
2025-02-19 18:05:22 +0300

    Fix tests

--- tests/queries/0_stateless/03276_database_backup_merge_tree_table_file_engine.sql
+++ tests/queries/0_stateless/03276_database_backup_merge_tree_table_file_engine.sql
@@ -9,12 +9,12 @@ INSERT INTO 03276_test_database.test_table SELECT number, number FROM numbers(15
 
 SELECT (id % 10) AS key, count() FROM 03276_test_database.test_table GROUP BY key ORDER BY key;
 
-BACKUP TABLE 03276_test_database.test_table TO File('03276_test_database.test_table.zip') FORMAT Null;
+BACKUP TABLE 03276_test_database.test_table TO File('03276_test_database.test_table') FORMAT Null;
 
 SELECT '--';
 
 DROP DATABASE IF EXISTS 03276_test_table_backup_database;
-CREATE DATABASE 03276_test_table_backup_database ENGINE = Backup('03276_test_database', File('03276_test_database.test_table.zip'));
+CREATE DATABASE 03276_test_table_backup_database ENGINE = Backup('03276_test_database', File('03276_test_database.test_table'));
 
 SELECT (id % 10) AS key, count() FROM 03276_test_table_backup_database.test_table GROUP BY key ORDER BY key;
 
