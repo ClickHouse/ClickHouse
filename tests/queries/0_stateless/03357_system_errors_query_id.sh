eb0bbe70e60f73bcd7254807f82c79fd77d79f8b
Vladimir Baikov <bkvvldmr@gmail.com>
2025-02-21 11:05:05 +0000

    Removed unused variable

--- tests/queries/0_stateless/03357_system_errors_query_id.sh
+++ tests/queries/0_stateless/03357_system_errors_query_id.sh
@@ -5,7 +5,6 @@ CUR_DIR=$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)
 . "$CUR_DIR"/../shell_config.sh
 
 query_id="01545_system_errors-$CLICKHOUSE_DATABASE"
-query_id1="01546_system_errors-$CLICKHOUSE_DATABASE"
 $CLICKHOUSE_CLIENT --query_id="$query_id" -q "select query_id;" &>/dev/null
 
 $CLICKHOUSE_CLIENT -q "
