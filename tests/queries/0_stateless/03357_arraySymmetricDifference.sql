cfa2793cb39cbf7f8193837d993fbf2caba65716
f.abapolov <faabapolov@edu.hse.ru>
2025-02-21 12:51:01 +0700

    nonconst tests based on materialized()

--- tests/queries/0_stateless/03357_arraySymmetricDifference.sql
+++ tests/queries/0_stateless/03357_arraySymmetricDifference.sql
@@ -20,24 +20,11 @@ SELECT arraySort(arraySymmetricDifference([1, 2], [1, 2], [1, 3]));
 SELECT toTypeName(arraySymmetricDifference([(1, ['a', 'b']), (Null, ['c'])], [(2, ['c', Null]), (1, ['a', 'b'])]));
 
 SELECT 'Non-const arguments';
-
-DROP TABLE IF EXISTS tab;
-
-CREATE TABLE tab (
-    id                              UInt8,
-    array_tuple1                    Array( Tuple(Nullable(Int32),Array(String)) ),
-    array_tuple2                    Array( Tuple(Nullable(Int32),Array(String)) ),
-    array_lowcardinality_string1    Array( LowCardinality(String) ),
-    array_lowcardinality_string2    Array( LowCardinality(String) )
-) ENGINE = MergeTree ORDER BY id;
-
-INSERT INTO tab VALUES (1, [(1, ['a', 'b']), (Null, ['c'])], [(2, ['c', Null]), (1, ['a', 'b'])], ['a', 'b', 'c'], ['c', 'a', 'b']);
-INSERT INTO tab VALUES (2, [(2, ['d', 'e']), (Null, ['f'])], [(3, ['g', Null]), (2, ['d', 'e'])], ['d', 'e', 'f'], ['f', 'd', 'e']);
-INSERT INTO tab VALUES (3, [(3, ['g', 'h']), (Null, ['i'])], [(4, ['j', Null]), (3, ['g', 'h'])], ['g', 'h', 'i'], ['i', 'g', 'h']);
-INSERT INTO tab VALUES (4, [(4, ['j', 'k']), (Null, ['l'])], [(5, ['m', Null]), (4, ['j', 'k'])], ['j', 'k', 'l'], ['l', 'j', 'k']);
-
---
-SELECT arraySort(arraySymmetricDifference(array_tuple1, array_tuple2)) FROM tab ORDER BY id;
-SELECT arraySort(arraySymmetricDifference(array_lowcardinality_string1, array_lowcardinality_string2)) FROM tab ORDER BY id;
-
-DROP TABLE tab;
+WITH
+    materialize([(1, ['a', 'b']), (NULL, ['c'])]) AS f,
+    materialize([(2, ['c', NULL]), (1, ['a', 'b'])]) AS s
+SELECT arraySymmetricDifference(f, s);
+WITH
+    materialize([(1, ['a', 'b']::Array(LowCardinality(String))), (NULL, ['c']::Array(LowCardinality(String)))]) AS f,
+    materialize([(2, ['c', NULL]::Array(LowCardinality(Nullable(String)))), (1, ['a', 'b']::Array(LowCardinality(String)))]) AS s
+SELECT arraySymmetricDifference(f, s)
