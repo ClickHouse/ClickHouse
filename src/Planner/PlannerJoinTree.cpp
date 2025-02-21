1401c818e92f6d5a70fcd023c0fa0d09bcea5530
Igor Nikonov <igor@clickhouse.com>
2025-02-21 22:44:54 +0100

    Propagate planner query context to subqueries

--- src/Planner/PlannerJoinTree.cpp
+++ src/Planner/PlannerJoinTree.cpp
@@ -681,6 +681,12 @@ JoinTreeQueryPlan buildQueryPlanForTableExpression(QueryTreeNodePtr table_expres
     const auto & query_context = planner_context->getQueryContext();
     const auto & settings = query_context->getSettingsRef();
 
+    LOG_DEBUG(
+        getLogger(__PRETTY_FUNCTION__),
+        "enable_parallel_replicas={}\n{}",
+        settings[Setting::allow_experimental_parallel_reading_from_replicas],
+        StackTrace().toString());
+
     auto & table_expression_data = planner_context->getTableExpressionDataOrThrow(table_expression);
 
     QueryProcessingStage::Enum from_stage = QueryProcessingStage::Enum::FetchColumns;
@@ -1187,6 +1193,13 @@ JoinTreeQueryPlan buildQueryPlanForTableExpression(QueryTreeNodePtr table_expres
                 subquery_planner_context = planner_context->getGlobalPlannerContext();
 
             auto subquery_options = select_query_options.subquery();
+
+            query_node->getMutableContext() = planner_context->getMutableQueryContext();
+            LOG_DEBUG(
+                getLogger(__PRETTY_FUNCTION__),
+                "table_expression/enable_parallel_replicas={}",
+                query_node->getContext()->getSettingsRef()[Setting::allow_experimental_parallel_reading_from_replicas]);
+
             Planner subquery_planner(table_expression, subquery_options, subquery_planner_context);
             /// Propagate storage limits to subquery
             subquery_planner.addStorageLimits(*select_query_info.storage_limits);
@@ -2216,7 +2229,14 @@ JoinTreeQueryPlan buildJoinTreeQueryPlan(const QueryTreeNodePtr & query_node,
 
     const auto & settings = planner_context->getQueryContext()->getSettingsRef();
     if (!settings[Setting::parallel_replicas_for_queries_with_multiple_tables] && table_expressions_stack_size > 1)
+    {
+        LOG_DEBUG(getLogger(__PRETTY_FUNCTION__), "Disable parallel replicas due to parallel_replicas_for_queries_with_multiple_tables setting");
         planner_context->getMutableQueryContext()->setSetting("enable_parallel_replicas", Field{0});
+    }
+    LOG_DEBUG(
+        getLogger(__PRETTY_FUNCTION__),
+        "enable_parallel_replicas={}",
+        settings[Setting::allow_experimental_parallel_reading_from_replicas]);
 
     /// disable parallel replicas for n-way join with FULL JOIN involved
     if (joins_count > 1 && is_full_join)
