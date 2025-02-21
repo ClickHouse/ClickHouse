0c511b4d94036034a155aea409222da61d5f30be
taiyang-li <654010905@qq.com>
2025-02-21 16:25:56 +0800

    commit again

--- src/AggregateFunctions/SingleValueData.cpp
+++ src/AggregateFunctions/SingleValueData.cpp
@@ -300,6 +300,12 @@ void SingleValueDataFixed<T>::setSmallest(const IColumn & column, size_t row_beg
         if (opt.has_value())
             setIfSmaller(T(*opt));
     }
+    else if constexpr (std::is_same_v<T, UInt256>)
+    {
+        std::optional<T> opt = findExtremeMinBigInt(vec.getData().data(), row_begin, row_end);
+        if (opt.has_value())
+            setIfSmaller(*opt);
+    }
     else
     {
         for (size_t i = row_begin; i < row_end; i++)
@@ -326,6 +332,12 @@ void SingleValueDataFixed<T>::setGreatest(const IColumn & column, size_t row_beg
         std::optional<U> opt = findExtremeMax(reinterpret_cast<const U *>(vec.getData().data()), row_begin, row_end);
         if (opt.has_value())
             setIfGreater(T(*opt));
+    }
+    else if constexpr (std::is_same_v<T, UInt256>)
+    {
+        std::optional<T> opt = findExtremeMaxBigInt(vec.getData().data(), row_begin, row_end);
+        if (opt.has_value())
+            setIfGreater(*opt);
     }
     else
     {
