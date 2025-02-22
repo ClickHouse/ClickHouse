39a2eb2276f316df2a3d94ac44e6dfc9e6c4f294
Nikolay Degterinsky <evillique@gmail.com>
2025-02-22 19:18:11 +0000

    Fix premature server_uuid macro substitution

--- tests/queries/0_stateless/02711_server_uuid_macro.sql
+++ tests/queries/0_stateless/02711_server_uuid_macro.sql
@@ -1,12 +1,11 @@
--- Tags: no-shared-catalog
-
 DROP TABLE IF EXISTS test;
 
 -- You can create a table with the {server_uuid} substituted.
 CREATE TABLE test (x UInt8) ENGINE = ReplicatedMergeTree('/clickhouse/tables/{database}/test', 'replica-{server_uuid}') ORDER BY x;
 
 -- The server UUID is correctly substituted.
-SELECT engine_full LIKE ('%replica-' || serverUUID()::String || '%') FROM system.tables WHERE database = currentDatabase() AND name = 'test';
+SELECT engine_full LIKE ('%replica-{server_uuid}%') FROM system.tables WHERE database = currentDatabase() AND name = 'test';
+SELECT count() > 0 FROM system.zookeeper WHERE path = '/clickhouse/tables/' || currentDatabase() || '/test/replicas/' AND name LIKE 'replica-' || serverUUID()::String || '%';
 
 -- An attempt to create a second table with the same UUID results in error.
 CREATE TABLE test2 (x UInt8) ENGINE = ReplicatedMergeTree('/clickhouse/tables/{database}/test', 'replica-{server_uuid}') ORDER BY x; -- { serverError REPLICA_ALREADY_EXISTS }
