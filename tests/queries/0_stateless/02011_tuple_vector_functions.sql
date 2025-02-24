81cfe0379f9b7f1a8fd45d42aa5742a0b12ea252
rienath <raufs.dunamalijevs@clickhouse.com>
2025-02-24 15:48:47 +0000

    transposeBits functional tests

--- tests/queries/0_stateless/02011_tuple_vector_functions.sql
+++ tests/queries/0_stateless/02011_tuple_vector_functions.sql
@@ -110,3 +110,15 @@ SELECT cosineDistance(materialize((NULL, -2147483648)), (1048577, 1048575));
 
 -- not extra parentheses
 EXPLAIN SYNTAX SELECT -((3, 7, 3), 100);
+
+SELECT transposeBits(emptyArrayFloat32());
+SELECT transposeBits([toFloat32(0), toFloat32(0), toFloat32(0)]);
+SELECT transposeBits(transposeBits([toFloat32(-16), toFloat32(-15), toFloat32(-14), toFloat32(-13), toFloat32(-12),
+                                    toFloat32(-11), toFloat32(-10), toFloat32(-9), toFloat32(-8), toFloat32(-7),
+                                    toFloat32(-6), toFloat32(-5), toFloat32(-4), toFloat32(-3), toFloat32(-2),
+                                    toFloat32(-1), toFloat32(0), toFloat32(1), toFloat32(2), toFloat32(3), toFloat32(4),
+                                    toFloat32(5), toFloat32(6), toFloat32(7), toFloat32(8), toFloat32(9), toFloat32(10),
+                                    toFloat32(11), toFloat32(12), toFloat32(13), toFloat32(14), toFloat32(15)]));
+SELECT transposeBits([toFloat32(-3.0316488e-13), toFloat32(14660155000000.), toFloat32(-3.0316488e-13)]);
+SELECT transposeBits([toFloat32(-3.0316488e-13), NULL]); -- { serverError ILLEGAL_TYPE_OF_ARGUMENT }
+SELECT transposeBits([1, 2, 3]); -- { serverError ILLEGAL_TYPE_OF_ARGUMENT }
