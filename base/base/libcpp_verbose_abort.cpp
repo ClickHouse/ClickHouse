bf36bb5cb638bf80e21c4497f0229f3c7f76b3cd
Robert Schulze <robert@clickhouse.com>
2025-02-25 12:45:21 +0000

    Fix style

--- base/base/libcpp_verbose_abort.cpp
+++ base/base/libcpp_verbose_abort.cpp
@@ -1,6 +1,9 @@
 #include <version>
 
-void std::__libcpp_verbose_abort(char const* format, ...) {
+/// Custom assertion handler for hardened libcxx
+/// See https://prereleases.llvm.org/16.0.0/rc3/projects/libcxx/docs/UsingLibcxx.html#enabling-the-safe-libc-mode
+void std::__libcpp_verbose_abort(char const * format, ...)
+{
     va_list list;
     va_start(list, format);
     std::vfprintf(stderr, format, list);
