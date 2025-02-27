0634dda8b1af3cd8857af3f5062956228ab88254
NamHoaiNguyen <nguyen.hoai.nam.3912@gmail.com>
2025-02-27 19:35:31 +0700

    Fix style check

--- src/Databases/PostgreSQL/DatabasePostgreSQL.cpp
+++ src/Databases/PostgreSQL/DatabasePostgreSQL.cpp
@@ -46,7 +46,6 @@ namespace Setting
 namespace ErrorCodes
 {
     extern const int BAD_ARGUMENTS;
-    extern const int LOGICAL_ERROR;
     extern const int NOT_IMPLEMENTED;
     extern const int UNKNOWN_TABLE;
     extern const int TABLE_IS_DROPPED;
