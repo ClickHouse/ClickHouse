d80b1c1229c8e339c076e52fac2027f4aac9e717
Onkar Deshpande <onkar.deshpande@sentry.io>
2025-02-27 05:29:23 +0000

    Stabilize stateless test

--- tests/queries/0_stateless/03362_filter_transform_profile_events.sql
+++ tests/queries/0_stateless/03362_filter_transform_profile_events.sql
@@ -1,6 +1,2 @@
 SELECT * FROM system.numbers WHERE number % 2 = 0 LIMIT 100;
 SELECT count() = 2 AS assert_exists FROM system.events WHERE name IN ('FilterTransformPassedRows', 'FilterTransformPassedBytes') HAVING assert_exists;
-
-SYSTEM FLUSH LOGS;
-SET log_queries = 1;
-SELECT ProfileEvents['FilterTransformPassedRows'] AS passed_rows, ProfileEvents['FilterTransformPassedBytes'] AS passed_bytes FROM system.query_log WHERE type = 'QueryFinish' AND current_database = currentDatabase() ORDER BY event_time DESC LIMIT 1;
