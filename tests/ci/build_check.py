ce9323d6fc0da9d7205b34100ba6c0d741c6cd8f
Raúl Marín <raul.marin@clickhouse.com>
2025-02-27 20:40:39 +0100

    Remove a CI debug file. Add other one

--- tests/ci/build_check.py
+++ tests/ci/build_check.py
@@ -267,7 +267,7 @@ def main():
         duration=elapsed,
         additional_files=[
             str(log_path),
-            "/home/ubuntu/actions-runner/_work/ClickHouse/ClickHouse/build_docker/contrib/delta-kernel-rs-cmake/_delta_kernel_rs_target-prefix/src/_delta_kernel_rs_target-stamp/_delta_kernel_rs_target-build-err.log"
+            "/home/ubuntu/actions-runner/_work/ClickHouse/ClickHouse/build_docker/contrib/corrosion-cmake/config.toml"
         ],
         build_dir_for_upload=build_output_path,
         version=version.describe,
