2d2bfe7ec9b5b57b677049e43c1829729ee046e1
NamHoaiNguyen <nguyen.hoai.nam.3912@gmail.com>
2025-02-27 19:58:52 +0700

    Fix style check again

--- src/Databases/DatabaseOnDisk.cpp
+++ src/Databases/DatabaseOnDisk.cpp
@@ -72,7 +72,6 @@ namespace ErrorCodes
     extern const int DATABASE_NOT_EMPTY;
     extern const int INCORRECT_QUERY;
     extern const int ARGUMENT_OUT_OF_BOUND;
-    extern const int BAD_ARGUMENTS;
 }
 
 
