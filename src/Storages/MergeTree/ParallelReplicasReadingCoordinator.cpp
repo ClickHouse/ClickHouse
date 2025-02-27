258488610cdbd2cb1fdb0896027b7c6def0586b0
Igor Nikonov <igor@clickhouse.com>
2025-02-27 16:37:41 +0100

    PR: check reading mode in all coordinators

--- src/Storages/MergeTree/ParallelReplicasReadingCoordinator.cpp
+++ src/Storages/MergeTree/ParallelReplicasReadingCoordinator.cpp
@@ -441,6 +441,14 @@ void DefaultCoordinator::doHandleInitialAllRangesAnnouncement(InitialAllRangesAn
 {
     LOG_DEBUG(log, "Initial request: {}", announcement.describe());
 
+    if (announcement.mode != CoordinationMode::Default)
+        throw Exception(
+            ErrorCodes::LOGICAL_ERROR,
+            "Replica {} decided to read in {} mode, not in {}. This is a bug",
+            announcement.replica_num,
+            magic_enum::enum_name(announcement.mode),
+            magic_enum::enum_name(CoordinationMode::Default));
+
     const auto replica_num = announcement.replica_num;
 
     initializeReadingState(std::move(announcement));
@@ -770,6 +778,14 @@ ParallelReadResponse DefaultCoordinator::handleRequest(ParallelReadRequest reque
         request.min_number_of_marks,
         stats[request.replica_num].number_of_requests);
 
+    if (request.mode != CoordinationMode::Default)
+        throw Exception(
+            ErrorCodes::LOGICAL_ERROR,
+            "Replica {} decided to read in {} mode, not in {}. This is a bug",
+            request.replica_num,
+            magic_enum::enum_name(request.mode),
+            magic_enum::enum_name(CoordinationMode::Default));
+
     ParallelReadResponse response;
 
     size_t current_mark_size = 0;
@@ -880,6 +896,14 @@ void InOrderCoordinator<mode>::doHandleInitialAllRangesAnnouncement(InitialAllRa
 {
     LOG_TRACE(log, "Received an announcement : {}", announcement.describe());
 
+    if (announcement.mode != mode)
+        throw Exception(
+            ErrorCodes::LOGICAL_ERROR,
+            "Replica {} decided to read in {} mode, not in {}. This is a bug",
+            announcement.replica_num,
+            magic_enum::enum_name(announcement.mode),
+            magic_enum::enum_name(mode));
+
     ++stats[announcement.replica_num].number_of_requests;
 
     size_t new_rows_to_read = 0;
@@ -963,9 +987,12 @@ template <CoordinationMode mode>
 ParallelReadResponse InOrderCoordinator<mode>::handleRequest(ParallelReadRequest request)
 {
     if (request.mode != mode)
-        throw Exception(ErrorCodes::LOGICAL_ERROR,
+        throw Exception(
+            ErrorCodes::LOGICAL_ERROR,
             "Replica {} decided to read in {} mode, not in {}. This is a bug",
-            request.replica_num, magic_enum::enum_name(request.mode), magic_enum::enum_name(mode));
+            request.replica_num,
+            magic_enum::enum_name(request.mode),
+            magic_enum::enum_name(mode));
 
     LOG_TRACE(log, "Got read request: {}", request.describe());
 
