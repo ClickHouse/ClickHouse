156ff5c7d0eee7f028b06ad9068f47ca331c7c4e
Tuan Pham Anh <tuan.pham.anh@clickhouse.com>
2025-02-28 09:24:58 +0000

    Fix flaky test_keeper_three_nodes_start

--- tests/integration/test_keeper_three_nodes_start/test.py
+++ tests/integration/test_keeper_three_nodes_start/test.py
@@ -3,26 +3,28 @@
 import helpers.keeper_utils as keeper_utils
 from helpers.cluster import ClickHouseCluster
 
-cluster = ClickHouseCluster(__file__)
-node1 = cluster.add_instance(
-    "node1", main_configs=["configs/enable_keeper1.xml"], stay_alive=True
-)
-node2 = cluster.add_instance(
-    "node2", main_configs=["configs/enable_keeper2.xml"], stay_alive=True
-)
 
 
-def get_fake_zk(nodename, timeout=30.0):
+
+def get_fake_zk(cluster, nodename, timeout=30.0):
     return keeper_utils.get_fake_zk(cluster, nodename, timeout=timeout)
 
 
 def test_smoke():
+    cluster = ClickHouseCluster(__file__)
+    cluster.add_instance(
+        "node1", main_configs=["configs/enable_keeper1.xml"], stay_alive=True
+    )
+    cluster.add_instance(
+        "node2", main_configs=["configs/enable_keeper2.xml"], stay_alive=True
+    )
+
     node1_zk = None
 
     try:
         cluster.start()
 
-        node1_zk = get_fake_zk("node1")
+        node1_zk = get_fake_zk(cluster, "node1")
         node1_zk.create("/test_alive", b"aaaa")
 
     finally:
