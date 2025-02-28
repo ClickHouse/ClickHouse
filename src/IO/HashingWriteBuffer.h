068c5de0f9150896ce9d458506b227bd4acdc029
Sema Checherinda <checherinda@gmail.com>
2025-02-28 15:25:28 +0100

    reduce hashing buffer

--- src/IO/HashingWriteBuffer.h
+++ src/IO/HashingWriteBuffer.h
@@ -5,7 +5,7 @@
 #include <IO/ReadHelpers.h>
 #include <city.h>
 
-constexpr size_t DBMS_DEFAULT_HASHING_BLOCK_SIZE = 2048ULL;
+constexpr size_t DBMS_DEFAULT_HASHING_BLOCK_SIZE = 128;
 
 
 namespace DB
