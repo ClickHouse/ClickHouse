6805e224714e5ee1aced209a69bbdeff5d29b59a
Pavel Kruglov <48961922+Avogar@users.noreply.github.com>
2025-03-04 13:29:35 +0100

    Fix new version in settings changes history

--- src/Core/SettingsChangesHistory.cpp
+++ src/Core/SettingsChangesHistory.cpp
@@ -64,7 +64,7 @@ const VersionToSettingsChangesMap & getSettingsChangesHistory()
         /// controls new feature and it's 'true' by default, use 'false' as previous_value).
         /// It's used to implement `compatibility` setting (see https://github.com/ClickHouse/ClickHouse/issues/35972)
         /// Note: please check if the key already exists to prevent duplicate entries.
-        addSettingsChanges(settings_changes_history, "25.2",
+        addSettingsChanges(settings_changes_history, "25.3",
         {
             {"output_format_json_quote_64bit_integers", true, false, "Disable quoting of the 64 bit integers in JSON by default"},
         });
