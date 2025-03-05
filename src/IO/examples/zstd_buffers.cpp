3db8579705acd419a9273500ab85bfaada79b1c4
Raúl Marín <raul.marin@clickhouse.com>
2025-03-05 17:05:31 +0100

    Tidy

--- src/IO/examples/zstd_buffers.cpp
+++ src/IO/examples/zstd_buffers.cpp
@@ -1,6 +1,3 @@
-#include <iomanip>
-#include <iostream>
-
 #include <IO/ReadBufferFromFile.h>
 #include <IO/ReadHelpers.h>
 #include <IO/WriteBufferFromFile.h>
@@ -8,6 +5,18 @@
 #include <IO/ZstdDeflatingWriteBuffer.h>
 #include <IO/ZstdInflatingReadBuffer.h>
 #include <Common/Stopwatch.h>
+
+#include <cerrno>
+#include <cstdlib>
+#include <iomanip>
+#include <iostream>
+
+#include <zstd.h>
+
+namespace DB::ErrorCodes
+{
+extern const int BAD_ARGUMENTS;
+}
 
 
 int main(int argc, char ** argv)
@@ -18,9 +27,23 @@ try
     size_t n = 10000000;
     Stopwatch stopwatch;
 
-    int compression_level = 3;
+    int compression_level = ZSTD_defaultCLevel();
     if (argc > 1)
-        compression_level = atoi(argv[1]);
+    {
+        errno = 0;
+        char * pos_integer = argv[1];
+        int uint_value = static_cast<int>(std::strtoul(argv[1], &pos_integer, 10));
+
+        if (pos_integer == argv[1] + strlen(argv[1]) && errno != ERANGE && uint_value <= ZSTD_maxCLevel() && uint_value >= ZSTD_minCLevel())
+            compression_level = uint_value;
+        else
+            throw DB::Exception(
+                DB::ErrorCodes::BAD_ARGUMENTS,
+                "Wrong compression level: '{}'. Expected an integer ({} <= N <= {})",
+                argv[1],
+                ZSTD_minCLevel(),
+                ZSTD_maxCLevel());
+    }
 
     {
         auto buf
