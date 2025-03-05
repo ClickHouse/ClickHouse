f1406673b29a31dfa5c33113af60dc5c7eeb5d12
Azat Khuzhin <a3at.mail@gmail.com>
2025-03-05 12:46:22 +0100

    tests: make test_parts_delete_zookeeper idempotent

--- tests/integration/test_parts_delete_zookeeper/test.py
+++ tests/integration/test_parts_delete_zookeeper/test.py
@@ -16,71 +16,75 @@ node1 = cluster.add_instance(
 def start_cluster():
     try:
         cluster.start()
-
-        node1.query(
-            """
-            CREATE DATABASE test;
-            CREATE TABLE test_table(date Date, id UInt32)
-            ENGINE = ReplicatedMergeTree('/clickhouse/tables/test/replicated', 'node1')
-            ORDER BY id PARTITION BY toYYYYMM(date) SETTINGS old_parts_lifetime=4, cleanup_delay_period=1, cleanup_thread_preferred_points_per_iteration=0;
-            """
-        )
-
         yield cluster
-
     except Exception as ex:
         print(ex)
-
     finally:
         cluster.shutdown()
 
 
+id_counter = 0
+
+
 # Test that outdated parts are not removed when they cannot be removed from zookeeper
 def test_merge_doesnt_work_without_zookeeper(start_cluster):
+    global id_counter
+    id_counter += 1
+
+    table = f"test_table_{id_counter}"
+
     node1.query(
-        "INSERT INTO test_table VALUES ('2018-10-01', 1), ('2018-10-02', 2), ('2018-10-03', 3)"
+        f"""
+        CREATE TABLE {table}(date Date, id UInt32)
+        ENGINE = ReplicatedMergeTree('/clickhouse/tables/test/replicated', 'node1')
+        ORDER BY id PARTITION BY toYYYYMM(date) SETTINGS old_parts_lifetime=4, cleanup_delay_period=1, cleanup_thread_preferred_points_per_iteration=0;
+        """
+    )
+
+    node1.query(
+        f"INSERT INTO {table} VALUES ('2018-10-01', 1), ('2018-10-02', 2), ('2018-10-03', 3)"
     )
     node1.query(
-        "INSERT INTO test_table VALUES ('2018-10-01', 4), ('2018-10-02', 5), ('2018-10-03', 6)"
+        f"INSERT INTO {table} VALUES ('2018-10-01', 4), ('2018-10-02', 5), ('2018-10-03', 6)"
     )
     assert (
-        node1.query("SELECT count(*) from system.parts where table = 'test_table'")
+        node1.query(f"SELECT count(*) from system.parts where table = '{table}'")
         == "2\n"
     )
 
-    node1.query("OPTIMIZE TABLE test_table FINAL")
+    node1.query(f"OPTIMIZE TABLE {table} FINAL")
     assert_eq_with_retry(
         node1,
-        "SELECT count(*) from system.parts where table = 'test_table' and active = 1",
+        f"SELECT count(*) from system.parts where table = '{table}' and active = 1",
         "1",
     )
 
-    node1.query("TRUNCATE TABLE test_table")
+    node1.query(f"TRUNCATE TABLE {table}")
 
     total_parts = node1.query(
-        "SELECT count(*) from system.parts where table = 'test_table'"
+        f"SELECT count(*) from system.parts where table = '{table}'"
     )
     assert total_parts == "0\n" or total_parts == "1\n"
 
     assert (
         node1.query(
-            "SELECT count(*) from system.parts where table = 'test_table' and active = 1"
+            f"SELECT count(*) from system.parts where table = '{table}' and active = 1"
         )
         == "0\n"
     )
 
-    node1.query("DETACH TABLE test_table SYNC")
-    node1.query("ATTACH TABLE test_table")
+    node1.query(f"DETACH TABLE {table} SYNC")
+    node1.query(f"ATTACH TABLE {table}")
 
     node1.query(
-        "INSERT INTO test_table VALUES ('2018-10-01', 1), ('2018-10-02', 2), ('2018-10-03', 3)"
+        f"INSERT INTO {table} VALUES ('2018-10-01', 1), ('2018-10-02', 2), ('2018-10-03', 3)"
     )
     node1.query(
-        "INSERT INTO test_table VALUES ('2018-10-01', 4), ('2018-10-02', 5), ('2018-10-03', 6)"
+        f"INSERT INTO {table} VALUES ('2018-10-01', 4), ('2018-10-02', 5), ('2018-10-03', 6)"
     )
     assert (
         node1.query(
-            "SELECT count(*) from system.parts where table = 'test_table' and active"
+            f"SELECT count(*) from system.parts where table = '{table}' and active"
         )
         == "2\n"
     )
@@ -88,12 +92,12 @@ def test_merge_doesnt_work_without_zookeeper(start_cluster):
     # Parts may be moved to Deleting state and then back in Outdated state.
     # But system.parts returns only Active and Outdated parts if _state column is not queried.
     with PartitionManager() as pm:
-        node1.query("OPTIMIZE TABLE test_table FINAL")
+        node1.query(f"OPTIMIZE TABLE {table} FINAL")
         pm.drop_instance_zk_connections(node1)
         # unfortunately we can be too fast and delete node before partition with ZK
         if (
             node1.query(
-                "SELECT count(*) from system.parts where table = 'test_table' and _state!='dummy'"
+                f"SELECT count(*) from system.parts where table = '{table}' and _state!='dummy'"
             )
             == "1\n"
         ):
@@ -102,13 +106,15 @@ def test_merge_doesnt_work_without_zookeeper(start_cluster):
             time.sleep(10)  # > old_parts_lifetime
             assert (
                 node1.query(
-                    "SELECT count(*) from system.parts where table = 'test_table' and _state!='dummy'"
+                    f"SELECT count(*) from system.parts where table = '{table}' and _state!='dummy'"
                 )
                 == "3\n"
             )
 
     assert_eq_with_retry(
         node1,
-        "SELECT count(*) from system.parts where table = 'test_table' and active = 1",
+        f"SELECT count(*) from system.parts where table = '{table}' and active = 1",
         "1",
     )
+
+    node1.query(f"DROP TABLE {table} SYNC")
