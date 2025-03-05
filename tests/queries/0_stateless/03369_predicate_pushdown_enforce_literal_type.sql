736632cdcd9c580dd7d9750e34f53c5ca098f115
Duc Canh Le <duccanh.le@ahrefs.com>
2025-03-05 15:59:27 +0800

    Update 03369_predicate_pushdown_enforce_literal_type.sql

--- tests/queries/0_stateless/03369_predicate_pushdown_enforce_literal_type.sql
+++ tests/queries/0_stateless/03369_predicate_pushdown_enforce_literal_type.sql
@@ -1,7 +1,9 @@
-CREATE OR REPLACE TABLE t (d Date, event String, c UInt32) ENGINE = Memory;
+DROP TABLE IF EXISTS t_03369;
 
-INSERT INTO t VALUES (toDate('2025-03-03'), 'foo', 1);
+CREATE TABLE t_03369 (d Date, event String, c UInt32) ENGINE = Memory;
+
+INSERT INTO t_03369 VALUES (toDate('2025-03-03'), 'foo', 1);
 
 SET prefer_localhost_replica = 0, allow_push_predicate_ast_for_distributed_subqueries = 1;
 
-SELECT d, count() FROM remote('127.0.0.{1..10}', currentDatabase(), t) WHERE event != '' GROUP BY d HAVING d >= toDate(1738281600) AND count() >= 1;
+SELECT d, count() FROM remote('127.0.0.{1..10}', currentDatabase(), t_03369) WHERE event != '' GROUP BY d HAVING d >= toDate(1738281600) AND count() >= 1;
