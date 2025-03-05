ffbde6bc6fe17569c69e9327f62a8c03eca0eed0
Max Kainov <max.kainov@clickhouse.com>
2025-03-05 19:58:04 +0100

    return binary build

--- ci/workflows/merge_queue.py
+++ ci/workflows/merge_queue.py
@@ -11,8 +11,24 @@ workflow = Workflow.Config(
         JobConfigs.docker_build_amd,
         JobConfigs.style_check,
         JobConfigs.fast_test,
+        *[job for job in JobConfigs.build_jobs if job.name == "Build (amd_binary)"],
+        # *[
+        #     job
+        #     for job in JobConfigs.unittest_jobs
+        #     if job.name == JobNames.UNITTEST + " (binary)"
+        # ],
     ],
     artifacts=[
+        *[
+            a
+            for a in ArtifactConfigs.unittests_binaries
+            if a.name == ArtifactNames.UNITTEST_AMD_BINARY
+        ],
+        *[
+            a
+            for a in ArtifactConfigs.clickhouse_binaries
+            if a.name == ArtifactNames.CH_AMD_BINARY
+        ],
         ArtifactConfigs.fast_test,
     ],
     # dockers=DOCKERS,
