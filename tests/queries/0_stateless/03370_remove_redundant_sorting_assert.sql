1047212438f53fa8eb838bdddde7d7a922c9006d
Igor Nikonov <igor@clickhouse.com>
2025-03-06 16:00:03 +0100

    Fix test

--- tests/queries/0_stateless/03370_remove_redundant_sorting_assert.sql
+++ tests/queries/0_stateless/03370_remove_redundant_sorting_assert.sql
@@ -1,6 +1,6 @@
 DROP TABLE IF EXISTS t0;
 CREATE TABLE t0 (c0 Int) ENGINE = Memory;
 INSERT INTO TABLE t0 (c0) VALUES (1);
-SELECT 1 FROM (SELECT materialize(1) FROM remote('localhost:9000', 'default', 't0') ORDER BY 1) ORDER BY 1;
+SELECT 1 FROM (SELECT materialize(1) FROM remote('localhost:9000', currentDatabase(), t0) ORDER BY 1) ORDER BY 1;
 
 DROP TABLE t0;
