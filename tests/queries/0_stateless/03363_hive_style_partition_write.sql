795506f4ab13f538f32ccf6836dc0717d03c21f6
Arthur Passos <arthur.ti@outlook.com>
2025-03-06 13:56:14 -0300

    tests

--- tests/queries/0_stateless/03363_hive_style_partition_write.sql
+++ tests/queries/0_stateless/03363_hive_style_partition_write.sql
@@ -23,6 +23,10 @@ INSERT INTO t_03363_s3_sink SETTINGS s3_truncate_on_insert=1 VALUES
 CREATE TABLE t_03363_s3_sink_read (year UInt16, country String, random UInt8)
 ENGINE = S3(s3_conn, filename = 't_03363_s3_sink_root/**.parquet', format = Parquet);
 
-select replaceRegexpAll(_path, '/[0-9]+\\.parquet', '/<snowflakeid>.parquet') AS _path, * from t_03363_s3_sink_read order by random SETTINGS use_hive_partitioning=1;
+select replaceRegexpAll(_path, '/[0-9]+\\.parquet', '/<snowflakeid>.parquet') AS _path, * from t_03363_s3_sink_read order by random SETTINGS use_hive_partitioning=1, input_format_parquet_use_native_reader=0;
 
 DROP TABLE t_03363_s3_sink, t_03363_s3_sink_read;
+
+CREATE TABLE t_03363_s3_sink (year UInt16, country String, random UInt8)
+ENGINE = S3(s3_conn, filename = 't_03363_s3_sink_root/{_partition_id}', format = Parquet, partitioning_style='hive')
+PARTITION BY (year, country) -- {serverError BAD_ARGUMENTS};
