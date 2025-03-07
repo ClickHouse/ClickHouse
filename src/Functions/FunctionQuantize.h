f2cd12f6a34bc39773a93f75ec454af685d533c7
nikita4109 <nikita4109@gmail.com>
2025-03-07 17:44:14 +0100

    fixed

--- src/Functions/FunctionQuantize.h
+++ src/Functions/FunctionQuantize.h
@@ -22,7 +22,6 @@ namespace ErrorCodes
 {
 extern const int NUMBER_OF_ARGUMENTS_DOESNT_MATCH;
 extern const int ILLEGAL_COLUMN;
-extern const int ILLEGAL_TYPE_OF_ARGUMENT;
 }
 
 template <typename Traits, typename QuantizeImpl>
