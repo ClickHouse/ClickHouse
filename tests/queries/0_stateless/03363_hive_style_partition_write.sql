b9cae0a1a3c36b20c99363509e1f2241a1b19163
Arthur Passos <arthur.ti@outlook.com>
2025-03-09 16:44:08 -0300

    fix tests

--- tests/queries/0_stateless/03363_hive_style_partition_write.sql
+++ tests/queries/0_stateless/03363_hive_style_partition_write.sql
@@ -2,7 +2,7 @@
 
 DROP TABLE IF EXISTS t_03363_s3_sink, t_03363_s3_sink_read;
 
-CREATE TABLE t_03363_s3_sink (year UInt16, country String, random UInt8)
+CREATE TABLE t_03363_s3_sink (year UInt16, country String, counter UInt8)
 ENGINE = S3(s3_conn, filename = 't_03363_s3_sink_root', format = Parquet, partitioning_style='hive')
 PARTITION BY (year, country);
 
@@ -20,13 +20,14 @@ INSERT INTO t_03363_s3_sink SETTINGS s3_truncate_on_insert=1 VALUES
     (2024, 'CN', 11);
 
 -- while reading from object storage partitioned table is not supported, an auxiliary table must be created
-CREATE TABLE t_03363_s3_sink_read (year UInt16, country String, random UInt8)
+CREATE TABLE t_03363_s3_sink_read (year UInt16, country String, counter UInt8)
 ENGINE = S3(s3_conn, filename = 't_03363_s3_sink_root/**.parquet', format = Parquet);
 
-select replaceRegexpAll(_path, '/[0-9]+\\.parquet', '/<snowflakeid>.parquet') AS _path, * from t_03363_s3_sink_read order by random SETTINGS use_hive_partitioning=1, input_format_parquet_use_native_reader=0;
+-- distinct because minio isn't cleaned up
+select distinct on (counter) replaceRegexpAll(_path, '/[0-9]+\\.parquet', '/<snowflakeid>.parquet') AS _path, counter from t_03363_s3_sink_read order by counter SETTINGS use_hive_partitioning=1, input_format_parquet_use_native_reader=0;
 
 DROP TABLE t_03363_s3_sink, t_03363_s3_sink_read;
 
-CREATE TABLE t_03363_s3_sink (year UInt16, country String, random UInt8)
+CREATE TABLE t_03363_s3_sink (year UInt16, country String, counter UInt8)
 ENGINE = S3(s3_conn, filename = 't_03363_s3_sink_root/{_partition_id}', format = Parquet, partitioning_style='hive')
 PARTITION BY (year, country) -- {serverError BAD_ARGUMENTS};
