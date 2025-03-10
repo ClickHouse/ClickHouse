934fd13ebab5775cb1ec67fc9c406ee1c8bbf6e7
Sergei Trifonov <svtrifonov@gmail.com>
2025-03-10 09:25:46 +0100

    Update src/Storages/StorageMemory.h

--- src/Storages/StorageMemory.h
+++ src/Storages/StorageMemory.h
@@ -6,7 +6,6 @@
 
 #include <Core/Block_fwd.h>
 #include <Interpreters/DatabaseCatalog.h>
-// #include <Interpreters/DatabaseCatalog.h>
 #include <Core/NamesAndTypes.h>
 #include <Storages/IStorage.h>
 
