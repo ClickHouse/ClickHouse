b69611651483e73515a1f2205572f809adb5346a
Julia Kartseva <yulia.kartseva@gmail.com>
2025-03-11 03:49:47 +0000

    tags

--- tests/queries/0_stateless/03008_azure_plain_rewritable_alter_partition.sh
+++ tests/queries/0_stateless/03008_azure_plain_rewritable_alter_partition.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
-
+# Tags: no-fasttest, no-shared-merge-tree, no-distributed-cache
 # Tag no-fasttest: requires Azure
 # Tag no-shared-merge-tree: does not support replication
 # Tag no-distributed-cache: Not supported auth type
