c2a1093a1c10daee98f1b2a97db93a51eaa94ecb
Yarik Briukhovetskyi <114298166+yariks5s@users.noreply.github.com>
2025-03-11 12:29:00 +0100

    Fix flaky test

--- tests/queries/0_stateless/03390_non_constant_case.sql
+++ tests/queries/0_stateless/03390_non_constant_case.sql
@@ -20,10 +20,10 @@ SELECT 'Date CASE';
 SELECT event_date,
     CASE event_date
         WHEN toDate('2024-03-10') THEN 'Special day'
-        WHEN today() THEN 'Today'
+        WHEN toDate('2024-03-12') THEN 'Today'
         ELSE 'Normal Day'
     END AS event_type
-FROM (SELECT arrayJoin([toDate('2024-03-10'), toDate('2024-03-11'), today()]) AS event_date);
+FROM (SELECT arrayJoin([toDate('2024-03-10'), toDate('2024-03-11'), toDate('2024-03-12')]) AS event_date);
 
 SELECT '1M Rows';
 SELECT count() FROM numbers(1000000) WHERE CASE number % 2
