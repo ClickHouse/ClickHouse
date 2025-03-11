7f2e4040a206a66d7a5aa5bd61760b8be29ffc71
Dmitry Novik <n0vik@clickhouse.com>
2025-03-11 12:21:55 +0100

    Fix test

--- tests/queries/0_stateless/03381_analyzer_distributed_predicate_pushdown.sql
+++ tests/queries/0_stateless/03381_analyzer_distributed_predicate_pushdown.sql
@@ -1,10 +1,14 @@
 -- https://github.com/ClickHouse/ClickHouse/issues/76182
 
+CREATE TABLE users (uid Int16, name String, age Int16) ENGINE=Memory;
+
+INSERT INTO users VALUES (1231, 'John', 33);
+
 SELECT toDayOfWeek(toDateTime(0) + uid)
-FROM cluster(test_cluster_two_shards, default.users)
+FROM cluster(test_cluster_two_shards, currentDatabase(), users)
 FORMAT Null;
 
 SELECT toDayOfWeek(toDateTime(0) + uid) in (7)
-FROM cluster(test_cluster_two_shards, default.users)
+FROM cluster(test_cluster_two_shards, currentDatabase(), users)
 FORMAT Null;
 
