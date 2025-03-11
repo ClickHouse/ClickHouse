a1df305525b4d923f124308c168a23296f2aff9c
Igor Nikonov <igor@clickhouse.com>
2025-03-11 16:47:16 +0100

    Revert changes in 01290_max_execution_speed_distributed

--- tests/queries/0_stateless/01290_max_execution_speed_distributed.sql
+++ tests/queries/0_stateless/01290_max_execution_speed_distributed.sql
@@ -18,7 +18,7 @@ FROM
         number
     FROM remote('127.0.0.{2,3}', numbers(100000))
 )
-SETTINGS max_execution_speed = 100000, timeout_before_checking_execution_speed = 0, max_block_size = 1000, log_comment='01290_8ca5d52f-8582-4ee3-8674-351c76d67b8c';
+SETTINGS max_execution_speed = 100000, timeout_before_checking_execution_speed = 0, max_block_size = 1000;
 
 INSERT INTO times SELECT now();
 
@@ -26,15 +26,12 @@ SELECT max(t) - min(t) >= 1 FROM times;
 
 -- Check that the query was also throttled on "remote" servers.
 SYSTEM FLUSH LOGS query_log;
-SELECT COUNT()
+SELECT DISTINCT query_duration_ms >= 500
 FROM system.query_log
 WHERE
     current_database = currentDatabase() AND
     event_date >= yesterday() AND
     event_time >= now() - INTERVAL '5 MINUTES' AND -- time limit for tests not marked `long` is 3 minutes, 5 should be more than enough
-    log_comment = '01290_8ca5d52f-8582-4ee3-8674-351c76d67b8c' AND
     query LIKE '%special query for 01290_max_execution_speed_distributed%' AND
     query NOT LIKE '%system.query_log%' AND
-    type = 'QueryFinish' AND
-    query_duration_ms >= 500
-SETTINGS max_threads = 0;
+    type = 2;
