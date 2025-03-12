cf920fe902febaeea17e386fc2dbe2689d1df274
vdimir <vdimir@clickhouse.com>
2025-03-12 11:25:28 +0000

    Use CLICKHOUSE_USER_FILES env in sql test

--- tests/queries/0_stateless/03381_file_log_merge_empty.sql
+++ tests/queries/0_stateless/03381_file_log_merge_empty.sql
@@ -1,6 +1,7 @@
 
 DROP TABLE IF EXISTS file_log;
-CREATE TABLE file_log  (`key` UInt8, `value` UInt8) ENGINE = FileLog('./user_files/02889_file_log_save_errors_test_11/', 'JSONEachRow') SETTINGS handle_error_mode = 'stream'
+
+CREATE TABLE file_log  (`key` UInt8, `value` UInt8) ENGINE = FileLog({CLICKHOUSE_USER_FILES_UNIQUE:String}, 'JSONEachRow') SETTINGS handle_error_mode = 'stream'
 ;
 
 SET stream_like_engine_allow_direct_select = 1;
