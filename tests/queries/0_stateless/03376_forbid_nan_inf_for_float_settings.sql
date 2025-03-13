d7b3a8dd27cab4a8609506b6eb7a1caa35616b03
Yarik Briukhovetskyi <114298166+yariks5s@users.noreply.github.com>
2025-03-13 16:45:15 +0100

    Remove obsolete tests

--- tests/queries/0_stateless/03376_forbid_nan_inf_for_float_settings.sql
+++ tests/queries/0_stateless/03376_forbid_nan_inf_for_float_settings.sql
@@ -2,4 +2,4 @@ SET log_queries_probability = inf; --  { serverError CANNOT_PARSE_NUMBER }
 SET log_queries_probability = nan; --  { serverError CANNOT_PARSE_NUMBER }
 SET log_queries_probability = -inf; --  { serverError CANNOT_PARSE_NUMBER }
 
-SELECT 1 SETTINGS log_queries_probability = -inf; --  { serverError CANNOT_PARSE_NUMBER }
+SELECT 1 SETTINGS log_queries_probability = -inf; --  { clientError CANNOT_PARSE_NUMBER }
