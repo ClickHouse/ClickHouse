f7a2bf6ac55dd37cb4f78fa2cde4ef9a76bcc1c5
Max Kainov <max.kainov@clickhouse.com>
2025-03-14 17:45:07 +0100

    enable for hardcoded users

--- ci/jobs/scripts/workflow_hooks/quick_sync.py
+++ ci/jobs/scripts/workflow_hooks/quick_sync.py
@@ -1,17 +1,29 @@
 import sys
 import traceback
 
-from praktika.info import Info
-from praktika.utils import Shell
+from ci.defs.defs import SYNC
+from ci.praktika.gh import GH
+from ci.praktika.info import Info
+from ci.praktika.utils import Shell
 
 
 def check():
     info = Info()
-    Shell.check(
-        f"gh workflow run private_quick_sync.yml --repo ClickHouse/clickhouse-private --ref ci_quick_sync --field pr_number={info.pr_number} --field branch_name={info.git_branch} --field title='{info.pr_title}' --field sha={info.sha}",
+    if info.user_name not in ("maxknv", "yariks5s", "kssenii"):
+        print(f"Not enabled for [{info.user_name}]")
+        return
+    if not Shell.check(
+        f"gh workflow run private_quick_sync.yml --repo ClickHouse/clickhouse-private --ref master --field pr_number={info.pr_number} --field branch_name={info.git_branch} --field title='{info.pr_title}' --field sha={info.sha}",
         verbose=True,
         strict=True,
-    )
+    ):
+        GH.post_commit_status(
+            name=SYNC, status="error", description="failed to start the sync", url=""
+        )
+    else:
+        GH.post_commit_status(
+            name=SYNC, status="pending", description="sync started", url=""
+        )
 
 
 if __name__ == "__main__":
