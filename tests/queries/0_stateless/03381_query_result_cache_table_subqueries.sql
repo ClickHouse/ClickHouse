edb2b700bdafd3fe4e26df0f631bdec853298f8e
Баранник Никита Павлович <npbarannik@edu.hse.ru>
2025-03-14 14:14:31 +0300

    Change query_result_cache_table_subqueries test

--- tests/queries/0_stateless/03381_query_result_cache_table_subqueries.sql
+++ tests/queries/0_stateless/03381_query_result_cache_table_subqueries.sql
@@ -7,10 +7,13 @@ SYSTEM DROP QUERY CACHE;
 SELECT ceil(avg(number)) FROM numbers(1, 100) SETTINGS use_query_cache = true, query_cache_for_subqueries = true;
 
 -- Should be one record in system.query_cache with is_subquery = 1 
-SELECT count(*) FROM system.query_cache WHERE is_subquery = 1;
+SELECT * FROM system.query_cache WHERE is_subquery = 1 ORDER BY expires_at;
 
 -- SELECT with sub-query result in cache
 SELECT * FROM (SELECT ceil(avg(number)) FROM numbers(1, 100)) SETTINGS use_query_cache = true, query_cache_for_subqueries = true;
+
+-- Should be one record in system.query_cache with is_subquery = 1 
+SELECT * FROM system.query_cache WHERE is_subquery = 1 ORDER BY expires_at;
 
 -- Check CacheHit
 SYSTEM FLUSH LOGS query_log;
