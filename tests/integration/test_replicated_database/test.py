a9ab3950ca06971e82d20048651914aa244845a2
Christoph Wurm <christoph@clickhouse.com>
2025-03-15 09:41:40 +0000

    Fix tests

--- tests/integration/test_replicated_database/test.py
+++ tests/integration/test_replicated_database/test.py
@@ -947,12 +947,7 @@ def test_recover_staled_replica(started_cluster):
         )
         == f"{test_recover_staled_replica_run}\n"
     )
-    assert (
-        dummy_node.query(
-            "SELECT count() FROM system.tables WHERE database='recover_broken_replicated_tables'"
-        )
-        == f"{test_recover_staled_replica_run}\n"
-    )
+    assert_eq_with_retry(dummy_node, "SELECT count() FROM system.tables WHERE database='recover_broken_replicated_tables'", test_recover_staled_replica_run)
     test_recover_staled_replica_run += 1
 
     table = dummy_node.query(
