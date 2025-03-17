2a4653d9868241de53c2820424b2a2cfa904e93b
Manish Gill <gill.manish90@gmail.com>
2025-03-17 15:26:17 +0000

    Linter fix

--- src/Functions/array/arrayResize.cpp
+++ src/Functions/array/arrayResize.cpp
@@ -18,7 +18,6 @@ namespace DB
 namespace ErrorCodes
 {
     extern const int LOGICAL_ERROR;
-    extern const int NUMBER_OF_ARGUMENTS_DOESNT_MATCH;
     extern const int ILLEGAL_TYPE_OF_ARGUMENT;
 }
 
