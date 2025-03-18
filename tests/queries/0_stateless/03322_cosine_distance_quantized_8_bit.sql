e21dc9201c2e1bce1899a96321aafc078ef1f9dd
nikita4109 <nikita4109@gmail.com>
2025-03-18 22:57:38 +0100

    fixed

--- tests/queries/0_stateless/03322_cosine_distance_quantized_8_bit.sql
+++ tests/queries/0_stateless/03322_cosine_distance_quantized_8_bit.sql
@@ -1,2 +1,2 @@
-SELECT quantized8BitCosineDistance(toFixedString(unhex('70F068E8'), 4), toFixedString(unhex('70F068E8'), 4));
-SELECT quantized8BitCosineDistance(toFixedString(unhex('78F8'), 4), toFixedString(unhex('F878'), 4));
+SELECT quantizedSFP8BitCosineDistance(toFixedString(unhex('70F068E8'), 4), toFixedString(unhex('70F068E8'), 4));
+SELECT quantizedSFP8BitCosineDistance(toFixedString(unhex('78F8'), 4), toFixedString(unhex('F878'), 4));
