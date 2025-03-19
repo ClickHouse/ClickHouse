9e867c9ebb3c86189a9103b4a9fa9b47b250bb1f
cjw <cjwcommuny@outlook.com>
2025-03-19 14:46:23 +0800

    Update MergeTreeIndexFullText.cpp

--- src/Storages/MergeTree/MergeTreeIndexFullText.cpp
+++ src/Storages/MergeTree/MergeTreeIndexFullText.cpp
@@ -289,7 +289,7 @@ bool MergeTreeConditionFullText::mayBeTrueOnGranuleInPart(MergeTreeIndexGranuleP
 
                 const auto & gin_filters = element.set_gin_filters[column];
                 for (size_t row = 0; row < gin_filters.size(); ++row)
-                    result[row] = granule->gin_filters[key_idx].contains(gin_filters[row], cache_store);
+                    result[row] = result[row] && granule->gin_filters[key_idx].contains(gin_filters[row], cache_store);
             }
 
             rpn_stack.emplace_back(std::find(std::cbegin(result), std::cend(result), true) != std::end(result), true);
