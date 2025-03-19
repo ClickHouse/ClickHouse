f06677f960ef2b600045d1e0347b90ae0c5f98c0
Azat Khuzhin <a3at.mail@gmail.com>
2025-03-19 17:12:01 +0100

    Add ability to configure number of columns that merges can flush in parallel
    Introduce new setting max_merge_delayed_streams_for_parallel_write, that
    is analog of max_insert_delayed_streams_for_parallel_write, but for
    merges.
    
    Since merges does not have query context and so the only way that you
    can change it without this patch is to set
    max_insert_delayed_streams_for_parallel_write for system profile (i.e.
    default user), plus, this can be useful to configure per-table limits.
    
    For instance system.metric_log (which has 1200+ columns) takes >1GiB of
    RAM with default 1000 columns limit when dumped to S3.

--- src/Storages/MergeTree/MergeTask.cpp
+++ src/Storages/MergeTree/MergeTask.cpp
@@ -111,6 +111,7 @@ namespace MergeTreeSetting
     extern const MergeTreeSettingsBool materialize_skip_indexes_on_merge;
     extern const MergeTreeSettingsBool prewarm_mark_cache;
     extern const MergeTreeSettingsBool use_const_adaptive_granularity;
+    extern const MergeTreeSettingsUInt64 max_merge_delayed_streams_for_parallel_write;
 }
 
 namespace ErrorCodes
@@ -954,21 +955,13 @@ bool MergeTask::VerticalMergeStage::prepareVerticalMergeForAllColumns() const
 
     ctx->it_name_and_type = global_ctx->gathering_columns.cbegin();
 
-    const auto & settings = global_ctx->context->getSettingsRef();
+    const auto & storage_settings = *global_ctx->data->getSettings();
 
-    size_t max_delayed_streams = 0;
     if (global_ctx->new_data_part->getDataPartStorage().supportParallelWrite())
-    {
-        if (settings[Setting::max_insert_delayed_streams_for_parallel_write].changed)
-            max_delayed_streams = settings[Setting::max_insert_delayed_streams_for_parallel_write];
-        else
-            max_delayed_streams = DEFAULT_DELAYED_STREAMS_FOR_PARALLEL_WRITE;
-    }
-
-    ctx->max_delayed_streams = max_delayed_streams;
+        ctx->max_delayed_streams = storage_settings[MergeTreeSetting::max_merge_delayed_streams_for_parallel_write];
 
     bool all_parts_on_remote_disks = std::ranges::all_of(global_ctx->future_part->parts, [](const auto & part) { return part->isStoredOnRemoteDisk(); });
-    ctx->use_prefetch = all_parts_on_remote_disks && (*global_ctx->data->getSettings())[MergeTreeSetting::vertical_merge_remote_filesystem_prefetch];
+    ctx->use_prefetch = all_parts_on_remote_disks && storage_settings[MergeTreeSetting::vertical_merge_remote_filesystem_prefetch];
 
     if (ctx->use_prefetch && ctx->it_name_and_type != global_ctx->gathering_columns.end())
         ctx->prepared_pipeline = createPipelineForReadingOneColumn(ctx->it_name_and_type->name);
