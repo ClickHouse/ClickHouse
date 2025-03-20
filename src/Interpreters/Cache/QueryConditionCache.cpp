bbd1211e8ac88f5c38f8b270e7ae0d829a09e041
Alexey Milovidov <milovidov@clickhouse.com>
2025-03-20 07:56:43 +0100

    Remove garbage from the query condition cache and enable it by default

--- src/Interpreters/Cache/QueryConditionCache.cpp
+++ src/Interpreters/Cache/QueryConditionCache.cpp
@@ -39,16 +39,15 @@ void QueryConditionCache::write(
         if (has_final_mark)
             entry->matching_marks[marks_count - 1] = false;
 
-        LOG_DEBUG(
+        LOG_TRACE(
             logger,
-            "{} entry for table_id: {}, part_name: {}, condition_hash: {}, marks_count: {}, has_final_mark: {}, ranges: {}",
+            "{} entry for table_id: {}, part_name: {}, condition_hash: {}, marks_count: {}, has_final_mark: {}",
             inserted ? "Inserted" : "Updated",
             table_id,
             part_name,
             condition_hash,
             marks_count,
-            has_final_mark,
-            toString(mark_ranges));
+            has_final_mark);
     }
 }
 
@@ -62,13 +61,12 @@ std::optional<QueryConditionCache::MatchingMarks> QueryConditionCache::read(cons
 
         std::shared_lock lock(entry->mutex);
 
-        LOG_DEBUG(
+        LOG_TRACE(
             logger,
-            "Read entry for table_uuid: {}, part: {}, condition_hash: {}, ranges: {}",
+            "Read entry for table_uuid: {}, part: {}, condition_hash: {}",
             table_id,
             part_name,
-            condition_hash,
-            toString(entry->matching_marks));
+            condition_hash);
 
         return {entry->matching_marks};
     }
