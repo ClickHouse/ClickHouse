ed23b26ea4bbf303b61feaeeeb8d461085881520
Alexander Gololobov <davenger@clickhouse.com>
2025-03-21 16:05:42 +0100

    Simple test for distributed plan with shuffle hash joins

---
+++ tests/queries/0_stateless/03394_distributed_shuffle_join.sql
@@ -0,0 +1,16 @@
+# Tags: no-fasttest
+# no-fasttest: requires object storage
+
+CREATE TABLE test_3(id UInt64, a Array(Int64)) ENGINE = MergeTree ORDER BY id;
+
+insert into test_3 select number, [number] from numbers(0, 1000000);
+
+
+SELECT count()
+FROM test_3 AS a, test_3 AS b, test_3 AS c, test_3 AS d
+WHERE (a.id = (b.id + 1)) AND (b.id = (c.id + 100)) AND ((c.id % 11111) = ((d.id % 12345) + 17));
+
+SELECT count()
+FROM test_3 AS a, test_3 AS b, test_3 AS c, test_3 AS d
+WHERE (a.id = (b.id + 1)) AND (b.id = (c.id + 100)) AND ((c.id % 11111) = ((d.id % 12345) + 17))
+SETTINGS make_distributed_plan = 1, enable_parallel_replicas = 0, default_shuffle_join_bucket_count = 20;
