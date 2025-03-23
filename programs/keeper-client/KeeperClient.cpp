0efc0cb071c75daa65ec14f0ca087db81f554ea3
Azat Khuzhin <a3at.mail@gmail.com>
2025-03-23 23:51:39 +0100

    keeper-client: do not append trailing slash in completion
    Everything works without it, and it breaks completion after it appends
    leading slash.

--- programs/keeper-client/KeeperClient.cpp
+++ programs/keeper-client/KeeperClient.cpp
@@ -139,7 +139,7 @@ std::vector<String> KeeperClient::getCompletions(String prefix) const
     try
     {
         for (const auto & child : zookeeper->getChildren(parent_path))
-            result.push_back(fmt::format("\"{}{}\"", parent_prefix, child));
+            result.push_back("\"" + parent_prefix + child);
     }
     catch (Coordination::Exception &) {} // NOLINT(bugprone-empty-catch)
 
