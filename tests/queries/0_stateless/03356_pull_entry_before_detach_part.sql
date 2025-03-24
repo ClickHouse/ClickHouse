f7b43a8c2acb24731f1b428cdd8a6691ff379e96
Aleksei Filatov <alexfvk@yandex-team.ru>
2025-03-24 20:46:42 +0000

    Add query test

---
+++ tests/queries/0_stateless/03356_pull_entry_before_detach_part.sql
@@ -0,0 +1,13 @@
+-- Tags: no-fasttest
+
+DROP TABLE IF EXISTS t1 SYNC;
+
+CREATE TABLE t1 (x UInt32) ENGINE = ReplicatedMergeTree('/clickhouse/tables/{database}/test_03356/t1', '1') ORDER BY tuple();
+
+SYSTEM STOP PULLING REPLICATION LOG t1;
+
+INSERT INTO t1 VALUES (1);
+
+SYSTEM START PULLING REPLICATION LOG t1;
+
+ALTER TABLE t1 DETACH PART 'all_0_0_0';
