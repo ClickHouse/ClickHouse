eff12471b07b1f0fbb20b4587f15e0ce9c7cf67c
Vico.Wu <583424568@qq.com>
2025-03-24 22:37:14 +0800

    upper case for desc

--- src/Core/Settings.cpp
+++ src/Core/Settings.cpp
@@ -5974,7 +5974,7 @@ Normally this setting should be set in user profile (users.xml or queries like `
 Note that initially (24.12) there was a server setting (`send_settings_to_client`), but latter it got replaced with this client setting, for better usability.
 )", 0)                                  \
     DECLARE(Milliseconds, low_priority_query_wait_time_ms, 1000, R"(
-wait time in milliseconds when lower priority query meets higher priority query.
+Wait time in milliseconds when lower priority query meets higher priority query.
 )", BETA) \
     \
     /* ####################################################### */ \
