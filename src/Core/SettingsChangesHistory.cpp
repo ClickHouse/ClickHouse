b897def48648ce119baa5265284fe48f54bfe9ec
Vico.Wu <583424568@qq.com>
2025-03-24 23:33:54 +0800

    add to change history

--- src/Core/SettingsChangesHistory.cpp
+++ src/Core/SettingsChangesHistory.cpp
@@ -69,7 +69,7 @@ const VersionToSettingsChangesMap & getSettingsChangesHistory()
         addSettingsChanges(settings_changes_history, "25.4",
         {
             {"enable_hdfs_pread", true, true, "New setting."},
-            {"low_priority_query_wait_time_ms", 1000, 1000, "New setting.."},
+            {"low_priority_query_wait_time_ms", 1000, 1000, "New setting."},
         });
         addSettingsChanges(settings_changes_history, "25.3",
         {
