5d3366e1f22bde72700e8a901bd5a46b19b8d6fe
Alexander Gololobov <davenger@clickhouse.com>
2025-03-24 12:46:46 +0100

    Force logical join step

--- tests/queries/0_stateless/03394_distributed_shuffle_join.sql
+++ tests/queries/0_stateless/03394_distributed_shuffle_join.sql
@@ -5,7 +5,6 @@ CREATE TABLE test_3(id UInt64, a Array(Int64)) ENGINE = MergeTree ORDER BY id;
 
 insert into test_3 select number, [number] from numbers(0, 1000000);
 
-
 SELECT count()
 FROM test_3 AS a, test_3 AS b, test_3 AS c, test_3 AS d
 WHERE (a.id = (b.id + 1)) AND (b.id = (c.id + 100)) AND ((c.id % 11111) = ((d.id % 12345) + 17));
@@ -13,4 +12,4 @@ WHERE (a.id = (b.id + 1)) AND (b.id = (c.id + 100)) AND ((c.id % 11111) = ((d.id
 SELECT count()
 FROM test_3 AS a, test_3 AS b, test_3 AS c, test_3 AS d
 WHERE (a.id = (b.id + 1)) AND (b.id = (c.id + 100)) AND ((c.id % 11111) = ((d.id % 12345) + 17))
-SETTINGS make_distributed_plan = 1, enable_parallel_replicas = 0, default_shuffle_join_bucket_count = 20;
+SETTINGS make_distributed_plan = 1, enable_parallel_replicas = 0, default_shuffle_join_bucket_count = 20, query_plan_use_new_logical_join_step=1;
