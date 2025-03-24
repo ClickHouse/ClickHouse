a21d9db985c21314eec9c4e8ed57d492322fe7f9
Igor Nikonov <igor@clickhouse.com>
2025-03-24 11:50:02 +0100

    Update 02354_vector_search_choose_correct_index

--- tests/queries/0_stateless/02354_vector_search_choose_correct_index.sql
+++ tests/queries/0_stateless/02354_vector_search_choose_correct_index.sql
@@ -1,8 +1,8 @@
--- Tags: no-fasttest, no-ordinary-database, no-parallel-replicas
--- no-parallel-replicas: EXPLAIN returns a different plan, this is expected behavior
+-- Tags: no-fasttest, no-ordinary-database
 
 SET allow_experimental_vector_similarity_index=1;
-SET enable_analyzer = 1; -- analyzer vs. non-analyzer produce slightly different EXPLAIN
+
+SET parallel_replicas_local_plan=1; -- this setting is randomized, set it explicitly to have local plan for parallel replicas
 
 -- Test for issue #77978
 
@@ -13,7 +13,7 @@ CREATE TABLE tab(id Int32, vec1 Array(Float32), vec2 Array(Float32), INDEX idx v
 INSERT INTO tab VALUES (0, [1.0, 0.0], [1.0, 0.0]), (1, [1.1, 0.0], [1.1, 0.0]), (2, [1.2, 0.0], [1.2, 0.0]), (3, [1.3, 0.0], [1.3, 0.0]), (4, [1.4, 0.0], [1.4, 0,0]), (5, [1.5, 0.0], [1.5, 0.0]), (6, [0.0, 2.0], [0.0, 2.0]), (7, [0.0, 2.1], [0.0, 2.1]), (8, [0.0, 2.2], [0.0, 2.2]), (9, [0.0, 2.3], [0.0, 2.3]), (10, [0.0, 2.4], [0.0, 2.4]), (11, [0.0, 2.5], [0.0, 2.5]);
 
 SELECT 'Searches on vec1 should use the vector index';
-EXPLAIN indexes=1 WITH [0.0, 2.0] AS reference_vec SELECT id FROM tab ORDER BY L2Distance(vec1, reference_vec) LIMIT 3;
+select trimLeft(explain) as e from (EXPLAIN indexes=1 WITH [0.0, 2.0] AS reference_vec SELECT id FROM tab ORDER BY L2Distance(vec1, reference_vec) LIMIT 3) where e ilike '%Skip%' or e ilike '%Name: idx%' or e ilike '%vector_similarity%';
 
 SELECT 'Searches on vec2 should be brute force';
-EXPLAIN indexes=1 WITH [0.0, 2.0] AS reference_vec SELECT id FROM tab ORDER BY L2Distance(vec2, reference_vec) LIMIT 3;
+select trimLeft(explain) as e from (EXPLAIN indexes=1 WITH [0.0, 2.0] AS reference_vec SELECT id FROM tab ORDER BY L2Distance(vec2, reference_vec) LIMIT 3) where e ilike '%Skip%' or e ilike '%Name: idx%' or e ilike '%vector_similarity%';
