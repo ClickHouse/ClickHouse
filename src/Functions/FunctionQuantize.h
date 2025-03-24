22e2b96a037d01f70d3078642d0388d855715b3e
nikita4109 <nikita4109@gmail.com>
2025-03-24 18:36:05 +0100

    fixed

--- src/Functions/FunctionQuantize.h
+++ src/Functions/FunctionQuantize.h
@@ -43,58 +43,34 @@ public:
 
     static FunctionPtr create(ContextPtr) { return std::make_shared<FunctionQuantizeBase>(); }
 
-    DataTypePtr getReturnTypeImpl(const DataTypes & arguments) const override
+    DataTypePtr getReturnTypeImpl(const ColumnsWithTypeAndName & arguments) const override
     {
-        if (arguments.size() != 1)
+        if (arguments.size() != 2)
             throw Exception(
                 ErrorCodes::NUMBER_OF_ARGUMENTS_DOESNT_MATCH,
-                "Number of arguments for function {} doesn't match: passed {}, expected 1",
+                "Number of arguments for function {} doesn't match: passed {}, expected 2",
                 getName(),
                 arguments.size());
-        const DataTypeArray * array_type = typeid_cast<const DataTypeArray *>(arguments[0].get());
+
+        const auto * col_size = checkAndGetColumn<ColumnConst>(arguments[1].column.get());
+        if (!col_size)
+            throw Exception(ErrorCodes::ILLEGAL_COLUMN, "Second argument of function {} must be an uint32", getName());
+
+        size_t array_size = col_size->getValue<UInt32>();
+        if (array_size == 0)
+            throw Exception(ErrorCodes::ILLEGAL_COLUMN, "Second argument of function {} is invalid ", getName());
+        size_t fixed_string_length = array_size * Traits::multiplier / Traits::divider;
+
+        DataTypes data_types(arguments.size());
+        for (size_t i = 0; i < arguments.size(); ++i)
+            data_types[i] = arguments[i].type;
+
+        const DataTypeArray * array_type = typeid_cast<const DataTypeArray *>(data_types[0].get());
         if (!array_type)
             throw Exception(ErrorCodes::ILLEGAL_TYPE_OF_ARGUMENT, "First argument of function {} must be an array", getName());
         const auto * nested_type = array_type->getNestedType().get();
         if (!isFloat(nested_type->getTypeId()))
             throw Exception(ErrorCodes::ILLEGAL_TYPE_OF_ARGUMENT, "Array elements must be Float32 or Float64");
-        return std::make_shared<DataTypeFixedString>(1);
-    }
-
-    DataTypePtr getReturnTypeImpl(const ColumnsWithTypeAndName & arguments) const override
-    {
-        if (arguments.size() != 1)
-            throw Exception(
-                ErrorCodes::NUMBER_OF_ARGUMENTS_DOESNT_MATCH,
-                "Number of arguments for function {} doesn't match: passed {}, expected 1",
-                getName(),
-                arguments.size());
-
-        if (arguments[0].column == nullptr)
-        {
-            DataTypes data_types(arguments.size());
-            for (size_t i = 0; i < arguments.size(); ++i)
-                data_types[i] = arguments[i].type;
-
-            return getReturnTypeImpl(data_types);
-        }
-
-        const ColumnArray * col_array = nullptr;
-        if (const auto * col_const = checkAndGetColumnConst<ColumnArray>(arguments[0].column.get()))
-            col_array = checkAndGetColumn<ColumnArray>(col_const->getDataColumnPtr().get());
-        else
-            col_array = checkAndGetColumn<ColumnArray>(arguments[0].column.get());
-
-        if (!col_array)
-            throw Exception(ErrorCodes::ILLEGAL_COLUMN, "First argument of function {} must be an array", getName());
-
-        const auto & offsets = col_array->getOffsets();
-        if (offsets.empty())
-            return std::make_shared<DataTypeFixedString>(1);
-
-        size_t array_size = offsets[0];
-        size_t fixed_string_length = array_size * Traits::multiplier / Traits::divider;
-        fixed_string_length = std::max(1ul, fixed_string_length);
-
         return std::make_shared<DataTypeFixedString>(fixed_string_length);
     }
 
--- tests/queries/0_stateless/03312_quantize_16_bit.sql
+++ tests/queries/0_stateless/03312_quantize_16_bit.sql
@@ -1,38 +1,38 @@
-SELECT hex(quantize16Bit(cast([1.0, -1.0] as Array(Float32))));
-SELECT hex(quantize16Bit(cast([1.0, -1.0] as Array(Float64))));
-SELECT hex(quantize16Bit(cast([0.5, -0.5, 0.25, -0.25] as Array(Float32))));
-SELECT hex(quantize16Bit(cast([1.0, -1.0] as Array(Float32))));
-SELECT hex(quantize16Bit(cast([1.0, 0.0, -1.0] as Array(Float32))));
-SELECT hex(quantize16Bit(cast([0.0, 0.0] as Array(Float32))));
-SELECT hex(quantize16Bit(cast([0.0, 0.0, 0.0, 0.0] as Array(Float32))));
-SELECT hex(quantize16Bit(cast([0.000001, -0.000001] as Array(Float32))));
-SELECT hex(quantize16Bit(cast([1e-10, -1e-10, 1e-20, -1e-20] as Array(Float32))));
+SELECT hex(quantize16Bit(cast([1.0, -1.0] as Array(Float32)), 2));
+SELECT hex(quantize16Bit(cast([1.0, -1.0] as Array(Float64)), 2));
+SELECT hex(quantize16Bit(cast([0.5, -0.5, 0.25, -0.25] as Array(Float32)), 4));
+SELECT hex(quantize16Bit(cast([1.0, -1.0] as Array(Float32)), 2));
+SELECT hex(quantize16Bit(cast([1.0, 0.0, -1.0] as Array(Float32)), 3));
+SELECT hex(quantize16Bit(cast([0.0, 0.0] as Array(Float32)), 2));
+SELECT hex(quantize16Bit(cast([0.0, 0.0, 0.0, 0.0] as Array(Float32)), 4));
+SELECT hex(quantize16Bit(cast([0.000001, -0.000001] as Array(Float32)), 2));
+SELECT hex(quantize16Bit(cast([1e-10, -1e-10, 1e-20, -1e-20] as Array(Float32)), 4));
 
-SELECT hex(quantize16Bit(cast([1e10, -1e10] as Array(Float32))));
-SELECT hex(quantize16Bit(cast([1e20, -1e20, 1e30, -1e30] as Array(Float32))));
+SELECT hex(quantize16Bit(cast([1e10, -1e10] as Array(Float32)), 2));
+SELECT hex(quantize16Bit(cast([1e20, -1e20, 1e30, -1e30] as Array(Float32)), 4));
 
-SELECT hex(quantize16Bit(cast([nan, inf, -inf] as Array(Float32))));
-SELECT hex(quantize16Bit(cast([nan, inf, -inf, 1.0] as Array(Float32))));
+SELECT hex(quantize16Bit(cast([nan, inf, -inf] as Array(Float32)), 3));
+SELECT hex(quantize16Bit(cast([nan, inf, -inf, 1.0] as Array(Float32)), 4));
 
-SELECT hex(quantize16Bit(cast([1.0, 0.1, 0.01, 0.001] as Array(Float32))));
-SELECT hex(quantize16Bit(cast([-1.0, -0.1, -0.01, -0.001] as Array(Float32))));
+SELECT hex(quantize16Bit(cast([1.0, 0.1, 0.01, 0.001] as Array(Float32)), 4));
+SELECT hex(quantize16Bit(cast([-1.0, -0.1, -0.01, -0.001] as Array(Float32)), 4));
 
-SELECT hex(quantize16Bit(cast([1.0, -1.0, 0.5, -0.5] as Array(Float32))));
-SELECT hex(quantize16Bit(cast([0.1, -0.1, 0.01, -0.01] as Array(Float32))));
+SELECT hex(quantize16Bit(cast([1.0, -1.0, 0.5, -0.5] as Array(Float32)), 4));
+SELECT hex(quantize16Bit(cast([0.1, -0.1, 0.01, -0.01] as Array(Float32)), 4));
 
-SELECT hex(quantize16Bit(cast([2.0, 4.0, 8.0, 16.0] as Array(Float32))));
-SELECT hex(quantize16Bit(cast([1.0, 0.5, 0.25, 0.125] as Array(Float32))));
+SELECT hex(quantize16Bit(cast([2.0, 4.0, 8.0, 16.0] as Array(Float32)), 4));
+SELECT hex(quantize16Bit(cast([1.0, 0.5, 0.25, 0.125] as Array(Float32)), 4));
 
-SELECT hex(quantize16Bit(cast([1/3, -1/3, 2/3, -2/3] as Array(Float32))));
-SELECT hex(quantize16Bit(cast([1/7, -1/7, 1/11, -1/11] as Array(Float32))));
+SELECT hex(quantize16Bit(cast([1/3, -1/3, 2/3, -2/3] as Array(Float32)), 4));
+SELECT hex(quantize16Bit(cast([1/7, -1/7, 1/11, -1/11] as Array(Float32)), 4));
 
-SELECT hex(quantize16Bit(cast([toFloat32(1.0), toFloat32(-1.0)] as Array(Float32))));
+SELECT hex(quantize16Bit(cast([toFloat32(1.0), toFloat32(-1.0)] as Array(Float32)), 2));
 
-SELECT hex(quantize16Bit(cast([1.0] as Array(Float32))));
-SELECT hex(quantize16Bit(cast([1.0, -1.0, 0.5, -0.5, 0.25, -0.25, 0.125, -0.125] as Array(Float32))));
+SELECT hex(quantize16Bit(cast([1.0] as Array(Float32)), 1));
+SELECT hex(quantize16Bit(cast([1.0, -1.0, 0.5, -0.5, 0.25, -0.25, 0.125, -0.125] as Array(Float32)), 8));
 
-SELECT hex(quantize16Bit(cast([1.0, 1.0, 1.0, 1.0] as Array(Float32))));
-SELECT hex(quantize16Bit(cast([-1.0, -1.0, -1.0, -1.0] as Array(Float32))));
+SELECT hex(quantize16Bit(cast([1.0, 1.0, 1.0, 1.0] as Array(Float32)), 4));
+SELECT hex(quantize16Bit(cast([-1.0, -1.0, -1.0, -1.0] as Array(Float32)), 4));
 
-SELECT hex(quantize16Bit(cast([0.5, 0.25, 0.125, 0.0625] as Array(Float32))));
-SELECT hex(quantize16Bit(cast([1.0, 0.999999, 0.500001, 0.499999] as Array(Float32))));
+SELECT hex(quantize16Bit(cast([0.5, 0.25, 0.125, 0.0625] as Array(Float32)), 4));
+SELECT hex(quantize16Bit(cast([1.0, 0.999999, 0.500001, 0.499999] as Array(Float32)), 4));
