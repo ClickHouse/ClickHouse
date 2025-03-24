8699134741d12da89a52fa514c1ff3bc23d30459
Robert Schulze <robert@clickhouse.com>
2025-03-24 11:03:37 +0000

    Add DROP TABLE

--- tests/queries/0_stateless/02354_vector_search_choose_correct_index.sql
+++ tests/queries/0_stateless/02354_vector_search_choose_correct_index.sql
@@ -13,7 +13,15 @@ CREATE TABLE tab(id Int32, vec1 Array(Float32), vec2 Array(Float32), INDEX idx v
 INSERT INTO tab VALUES (0, [1.0, 0.0], [1.0, 0.0]), (1, [1.1, 0.0], [1.1, 0.0]), (2, [1.2, 0.0], [1.2, 0.0]), (3, [1.3, 0.0], [1.3, 0.0]), (4, [1.4, 0.0], [1.4, 0,0]), (5, [1.5, 0.0], [1.5, 0.0]), (6, [0.0, 2.0], [0.0, 2.0]), (7, [0.0, 2.1], [0.0, 2.1]), (8, [0.0, 2.2], [0.0, 2.2]), (9, [0.0, 2.3], [0.0, 2.3]), (10, [0.0, 2.4], [0.0, 2.4]), (11, [0.0, 2.5], [0.0, 2.5]);
 
 SELECT 'Searches on vec1 should use the vector index';
-select trimLeft(explain) as e from (EXPLAIN indexes=1 WITH [0.0, 2.0] AS reference_vec SELECT id FROM tab ORDER BY L2Distance(vec1, reference_vec) LIMIT 3) where e ilike '%Skip%' or e ilike '%Name: idx%' or e ilike '%vector_similarity%';
+SELECT trimLeft(explain) AS explain FROM (
+    EXPLAIN indexes=1 WITH [0.0, 2.0] AS reference_vec SELECT id FROM tab ORDER BY L2Distance(vec1, reference_vec) LIMIT 3
+)
+WHERE explain ILIKE '%Skip%' OR explain ILIKE '%Name: idx%' OR explain ILIKE '%vector_similarity%';
 
 SELECT 'Searches on vec2 should be brute force';
-select trimLeft(explain) as e from (EXPLAIN indexes=1 WITH [0.0, 2.0] AS reference_vec SELECT id FROM tab ORDER BY L2Distance(vec2, reference_vec) LIMIT 3) where e ilike '%Skip%' or e ilike '%Name: idx%' or e ilike '%vector_similarity%';
+SELECT trimLeft(explain) AS explain FROM (
+    EXPLAIN indexes=1 WITH [0.0, 2.0] AS reference_vec SELECT id FROM tab ORDER BY L2Distance(vec2, reference_vec) LIMIT 3
+)
+WHERE explain ILIKE '%Skip%' OR explain ILIKE '%Name: idx%' OR explain ILIKE '%vector_similarity%';
+
+DROP TABLE tab;
