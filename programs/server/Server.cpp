82bc32fc6760839e9b37df18578ea8f73ea19d84
Vico.Wu <583424568@qq.com>
2025-03-24 16:45:32 +0800

    resolve conflict

--- programs/server/Server.cpp
+++ programs/server/Server.cpp
@@ -319,6 +319,7 @@ namespace ServerSetting
     extern const ServerSettingsDouble page_cache_free_memory_ratio;
     extern const ServerSettingsUInt64 page_cache_lookahead_blocks;
     extern const ServerSettingsUInt64 page_cache_shards;
+    extern const ServerSettingsUInt64 low_priority_wait_timeout_millisec;
 }
 
 }
@@ -1960,6 +1961,7 @@ try
             global_context->getProcessList().setMaxInsertQueriesAmount(new_server_settings[ServerSetting::max_concurrent_insert_queries]);
             global_context->getProcessList().setMaxSelectQueriesAmount(new_server_settings[ServerSetting::max_concurrent_select_queries]);
             global_context->getProcessList().setMaxWaitingQueriesAmount(new_server_settings[ServerSetting::max_waiting_queries]);
+            global_context->getProcessList().setLowPriorityWaitTimeoutMillisec(new_server_settings[ServerSetting::low_priority_wait_timeout_millisec]);
 
             if (config->has("keeper_server"))
                 global_context->updateKeeperConfiguration(*config);
