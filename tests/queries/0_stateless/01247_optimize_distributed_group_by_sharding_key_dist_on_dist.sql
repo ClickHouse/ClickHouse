54f7d98e55f27e297ef4b6e18ad0acf27294c820
Nikolai Kochetov <nk@clickhouse.com>
2025-03-25 17:01:24 +0000

    Update tests.

--- tests/queries/0_stateless/01247_optimize_distributed_group_by_sharding_key_dist_on_dist.sql
+++ tests/queries/0_stateless/01247_optimize_distributed_group_by_sharding_key_dist_on_dist.sql
@@ -22,7 +22,7 @@ create table dist_01247 as data_01247 engine=Distributed(test_cluster_two_shards
 select count(), * from dist_01247 group by number order by number limit 1 settings prefer_localhost_replica=1;
 select '-';
 -- Now, sharding key optimization is not supported for distributed over distributed with serialized plan.
-select count(), * from dist_01247 group by number order by number limit 1 settings prefer_localhost_replica=0, serialize_query_plan=1;
+select count(), * from dist_01247 group by number order by number limit 1 settings prefer_localhost_replica=0, serialize_query_plan=1, enable_analyzer=1;
 drop table if exists dist_01247;
 drop table if exists dist_layer_01247;
 
