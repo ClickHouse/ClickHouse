bb93ae9b10aecb6650aab4cf0a46562a7ed27067
Vladimir Cherkasov <vdimir@clickhouse.com>
2025-03-25 12:19:06 +0100

    fx

--- tests/queries/0_stateless/03381_file_log_merge_empty.sh
+++ tests/queries/0_stateless/03381_file_log_merge_empty.sh
@@ -13,4 +13,4 @@ ${CLICKHOUSE_CLIENT} --query "DROP TABLE IF EXISTS file_log;"
 ${CLICKHOUSE_CLIENT} --query "CREATE TABLE file_log  (key UInt8, value UInt8) ENGINE = FileLog('${USER_FILES_PATH}/${CLICKHOUSE_TEST_UNIQUE_NAME}', 'JSONEachRow') SETTINGS handle_error_mode = 'stream'"
 
 # send_logs_level: reading from empty file log produces warning
-${CLICKHOUSE_CLIENT} --stream_like_engine_allow_direct_select=1 --send_logs_level=error --query "select * from merge('', 'file_log');"
+${CLICKHOUSE_CLIENT} --stream_like_engine_allow_direct_select=1 --query "select * from merge('', 'file_log') SETTINGS send_logs_level='error';"
