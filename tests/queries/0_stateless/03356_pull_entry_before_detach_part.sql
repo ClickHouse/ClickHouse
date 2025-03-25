f364900f29dda34fcb06e9cc2f5164ef9dc5388a
Azat Khuzhin <a3at.mail@gmail.com>
2025-03-25 21:57:14 +0100

    tests: disable ZK fault injection in 03356_pull_entry_before_detach_part

--- tests/queries/0_stateless/03356_pull_entry_before_detach_part.sql
+++ tests/queries/0_stateless/03356_pull_entry_before_detach_part.sql
@@ -1,5 +1,8 @@
 -- Tags: no-fasttest, no-parallel
 
+-- Forbid fault injection to avoid part name randomization, since we rely on it
+SET insert_keeper_fault_injection_probability=0;
+
 DROP TABLE IF EXISTS t1 SYNC;
 
 CREATE TABLE t1 (x UInt32) ENGINE = ReplicatedMergeTree('/clickhouse/tables/{database}/test_03356/t1', '1') ORDER BY tuple();
