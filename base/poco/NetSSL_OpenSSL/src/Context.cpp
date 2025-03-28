81d026238ff6f3606c399357a33ca69a85bd4a58
Konstantin Bogdanov <konstantin@clickhouse.com>
2025-03-28 19:59:08 +0100

    Clean up legacy OpenSSL usages

--- base/poco/NetSSL_OpenSSL/src/Context.cpp
+++ base/poco/NetSSL_OpenSSL/src/Context.cpp
@@ -536,7 +536,7 @@ void Context::createSSLContext()
 {
 	if (SSLManager::isFIPSEnabled())
 	{
-		_pSSLContext = SSL_CTX_new(TLSv1_method());
+		_pSSLContext = SSL_CTX_new(TLS_method());
 	}
 	else
 	{
@@ -556,42 +556,14 @@ void Context::createSSLContext()
 			_pSSLContext = SSL_CTX_new(SSLv23_server_method());
 #endif
 			break;
-#if defined(SSL_OP_NO_TLSv1) && !defined(OPENSSL_NO_TLS1)
-		case TLSV1_CLIENT_USE:
-			_pSSLContext = SSL_CTX_new(TLSv1_client_method());
-			break;
-		case TLSV1_SERVER_USE:
-			_pSSLContext = SSL_CTX_new(TLSv1_server_method());
-			break;
-#endif
-#if defined(SSL_OP_NO_TLSv1_1) && !defined(OPENSSL_NO_TLS1)
-/* SSL_OP_NO_TLSv1_1 is defined in ssl.h if the library version supports TLSv1.1.
- * OPENSSL_NO_TLS1 is defined in opensslconf.h or on the compiler command line
- * if TLS1.x was removed at OpenSSL library build time via Configure options.
- */
-        case TLSV1_1_CLIENT_USE:
-            _pSSLContext = SSL_CTX_new(TLSv1_1_client_method());
-            break;
-        case TLSV1_1_SERVER_USE:
-            _pSSLContext = SSL_CTX_new(TLSv1_1_server_method());
-            break;
-#endif
-#if defined(SSL_OP_NO_TLSv1_2) && !defined(OPENSSL_NO_TLS1)
-        case TLSV1_2_CLIENT_USE:
-            _pSSLContext = SSL_CTX_new(TLSv1_2_client_method());
-            break;
-        case TLSV1_2_SERVER_USE:
-            _pSSLContext = SSL_CTX_new(TLSv1_2_server_method());
-            break;
-#endif
 		default:
 			throw Poco::InvalidArgumentException("Invalid or unsupported usage");
 		}
 	}
 	if (!_pSSLContext)
 	{
-		unsigned long err = ERR_get_error();
-		throw SSLException("Cannot create SSL_CTX object", ERR_error_string(err, 0));
+		uint64_t err = ERR_get_error();
+		throw SSLException("Cannot create SSL_CTX object", ERR_error_string(err, nullptr));
 	}
 
 	SSL_CTX_set_default_passwd_cb(_pSSLContext, &SSLManager::privateKeyPassphraseCallback);
@@ -603,136 +575,44 @@ void Context::createSSLContext()
 
 void Context::initDH(const std::string& dhParamsFile)
 {
-#ifndef OPENSSL_NO_DH
-    // 1024-bit MODP Group with 160-bit prime order subgroup (RFC5114)
-	// -----BEGIN DH PARAMETERS-----
-	// MIIBDAKBgQCxC4+WoIDgHd6S3l6uXVTsUsmfvPsGo8aaap3KUtI7YWBz4oZ1oj0Y
-	// mDjvHi7mUsAT7LSuqQYRIySXXDzUm4O/rMvdfZDEvXCYSI6cIZpzck7/1vrlZEc4
-	// +qMaT/VbzMChUa9fDci0vUW/N982XBpl5oz9p21NpwjfH7K8LkpDcQKBgQCk0cvV
-	// w/00EmdlpELvuZkF+BBN0lisUH/WQGz/FCZtMSZv6h5cQVZLd35pD1UE8hMWAhe0
-	// sBuIal6RVH+eJ0n01/vX07mpLuGQnQ0iY/gKdqaiTAh6CR9THb8KAWm2oorWYqTR
-	// jnOvoy13nVkY0IvIhY9Nzvl8KiSFXm7rIrOy5QICAKA=
-	// -----END DH PARAMETERS-----
-	//
-
-	static const unsigned char dh1024_p[] =
-	{
-		0xB1,0x0B,0x8F,0x96,0xA0,0x80,0xE0,0x1D,0xDE,0x92,0xDE,0x5E,
-		0xAE,0x5D,0x54,0xEC,0x52,0xC9,0x9F,0xBC,0xFB,0x06,0xA3,0xC6,
-		0x9A,0x6A,0x9D,0xCA,0x52,0xD2,0x3B,0x61,0x60,0x73,0xE2,0x86,
-		0x75,0xA2,0x3D,0x18,0x98,0x38,0xEF,0x1E,0x2E,0xE6,0x52,0xC0,
-		0x13,0xEC,0xB4,0xAE,0xA9,0x06,0x11,0x23,0x24,0x97,0x5C,0x3C,
-		0xD4,0x9B,0x83,0xBF,0xAC,0xCB,0xDD,0x7D,0x90,0xC4,0xBD,0x70,
-		0x98,0x48,0x8E,0x9C,0x21,0x9A,0x73,0x72,0x4E,0xFF,0xD6,0xFA,
-		0xE5,0x64,0x47,0x38,0xFA,0xA3,0x1A,0x4F,0xF5,0x5B,0xCC,0xC0,
-		0xA1,0x51,0xAF,0x5F,0x0D,0xC8,0xB4,0xBD,0x45,0xBF,0x37,0xDF,
-		0x36,0x5C,0x1A,0x65,0xE6,0x8C,0xFD,0xA7,0x6D,0x4D,0xA7,0x08,
-		0xDF,0x1F,0xB2,0xBC,0x2E,0x4A,0x43,0x71,
-	};
-
-	static const unsigned char dh1024_g[] =
-	{
-		0xA4,0xD1,0xCB,0xD5,0xC3,0xFD,0x34,0x12,0x67,0x65,0xA4,0x42,
-		0xEF,0xB9,0x99,0x05,0xF8,0x10,0x4D,0xD2,0x58,0xAC,0x50,0x7F,
-		0xD6,0x40,0x6C,0xFF,0x14,0x26,0x6D,0x31,0x26,0x6F,0xEA,0x1E,
-		0x5C,0x41,0x56,0x4B,0x77,0x7E,0x69,0x0F,0x55,0x04,0xF2,0x13,
-		0x16,0x02,0x17,0xB4,0xB0,0x1B,0x88,0x6A,0x5E,0x91,0x54,0x7F,
-		0x9E,0x27,0x49,0xF4,0xD7,0xFB,0xD7,0xD3,0xB9,0xA9,0x2E,0xE1,
-		0x90,0x9D,0x0D,0x22,0x63,0xF8,0x0A,0x76,0xA6,0xA2,0x4C,0x08,
-		0x7A,0x09,0x1F,0x53,0x1D,0xBF,0x0A,0x01,0x69,0xB6,0xA2,0x8A,
-		0xD6,0x62,0xA4,0xD1,0x8E,0x73,0xAF,0xA3,0x2D,0x77,0x9D,0x59,
-		0x18,0xD0,0x8B,0xC8,0x85,0x8F,0x4D,0xCE,0xF9,0x7C,0x2A,0x24,
-		0x85,0x5E,0x6E,0xEB,0x22,0xB3,0xB2,0xE5,
-	};
-
-	DH* dh = 0;
 	if (!dhParamsFile.empty())
 	{
-		BIO* bio = BIO_new_file(dhParamsFile.c_str(), "r");
-		if (!bio)
-		{
-			std::string msg = Utility::getLastError();
-			throw SSLContextException(std::string("Error opening Diffie-Hellman parameters file ") + dhParamsFile, msg);
-		}
-		dh = PEM_read_bio_DHparams(bio, 0, 0, 0);
-		BIO_free(bio);
-		if (!dh)
-		{
-			std::string msg = Utility::getLastError();
-			throw SSLContextException(std::string("Error reading Diffie-Hellman parameters from file ") + dhParamsFile, msg);
-		}
+        BIO* bio = BIO_new_file(dhParamsFile.c_str(), "r");
+        if (!bio)
+        {
+            std::string msg = Utility::getLastError();
+            throw SSLContextException("Error opening Diffie-Hellman parameters file " + dhParamsFile, msg);
+        }
+
+        EVP_PKEY* dh_params = PEM_read_bio_Parameters(bio, nullptr);
+        BIO_free(bio);
+
+        if (!dh_params)
+        {
+            std::string msg = Utility::getLastError();
+            throw SSLContextException("Error reading Diffie-Hellman parameters from file " + dhParamsFile, msg);
+        }
+
+        if (SSL_CTX_set0_tmp_dh_pkey(_pSSLContext, dh_params) != 1)
+        {
+            EVP_PKEY_free(dh_params);
+            throw SSLContextException("Failed to set DH parameters in SSL context", Utility::getLastError());
+        }
+
+        SSL_CTX_set_options(_pSSLContext, SSL_OP_SINGLE_DH_USE);
 	}
 	else
-	{
-		dh = DH_new();
-		if (!dh)
-		{
-			std::string msg = Utility::getLastError();
-			throw SSLContextException("Error creating Diffie-Hellman parameters", msg);
-		}
-#if OPENSSL_VERSION_NUMBER >= 0x10100000L && !defined(LIBRESSL_VERSION_NUMBER) && !defined(BORINGSSL_DEPRECATED)
-		BIGNUM* p = BN_bin2bn(dh1024_p, sizeof(dh1024_p), 0);
-		BIGNUM* g = BN_bin2bn(dh1024_g, sizeof(dh1024_g), 0);
-		DH_set0_pqg(dh, p, 0, g);
-		DH_set_length(dh, 160);
-		if (!p || !g)
-		{
-			DH_free(dh);
-			throw SSLContextException("Error creating Diffie-Hellman parameters");
-		}
-#else
-		dh->p = BN_bin2bn(dh1024_p, sizeof(dh1024_p), 0);
-		dh->g = BN_bin2bn(dh1024_g, sizeof(dh1024_g), 0);
-#ifdef BORINGSSL_DEPRECATED
-		dh->priv_length = 160;
-#else
-		dh->length = 160;
-#endif
-		if ((!dh->p) || (!dh->g))
-		{
-			DH_free(dh);
-			throw SSLContextException("Error creating Diffie-Hellman parameters");
-		}
-#endif
-	}
-	SSL_CTX_set_tmp_dh(_pSSLContext, dh);
-	SSL_CTX_set_options(_pSSLContext, SSL_OP_SINGLE_DH_USE);
-	DH_free(dh);
-#else
-	if (!dhParamsFile.empty())
-		throw SSLContextException("OpenSSL does not support DH");
-#endif
+        SSL_CTX_set_dh_auto(_pSSLContext, 1);
 }
 
 
 void Context::initECDH(const std::string& curve)
 {
-#if OPENSSL_VERSION_NUMBER >= 0x0090800fL
-#ifndef OPENSSL_NO_ECDH
-	int nid = 0;
-	if (!curve.empty())
-	{
-		nid = OBJ_sn2nid(curve.c_str());
-	}
-	else
-	{
-		nid = OBJ_sn2nid("prime256v1");
-	}
-	if (nid == 0)
-	{
-		throw SSLContextException("Unknown ECDH curve name", curve);
-	}
-
-	EC_KEY* ecdh = EC_KEY_new_by_curve_name(nid);
-	if (!ecdh)
-	{
-		throw SSLContextException("Cannot create ECDH curve");
-	}
-	SSL_CTX_set_tmp_ecdh(_pSSLContext, ecdh);
-	SSL_CTX_set_options(_pSSLContext, SSL_OP_SINGLE_ECDH_USE);
-	EC_KEY_free(ecdh);
-#endif
-#endif
+    std::string use_curve = curve.empty() ? "P-256" : curve;
+    if (SSL_CTX_set1_groups_list(_pSSLContext, use_curve.c_str()) != 1)
+    {
+        throw SSLContextException("Unsupported ECDH curve or group: " + use_curve);
+    }
 }
 
 
