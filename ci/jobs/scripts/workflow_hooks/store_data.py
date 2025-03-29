a8eb5dd1f70ff593796bf040601f06b425ccb004
Max Kainov <max.kainov@clickhouse.com>
2025-03-30 00:05:41 +0100

    CI: Fix perf tests reference clickhouse lookup on master

--- ci/jobs/scripts/workflow_hooks/store_data.py
+++ ci/jobs/scripts/workflow_hooks/store_data.py
@@ -18,3 +18,18 @@ if __name__ == "__main__":
     if changed_files_str:
         changed_files = changed_files_str.split("\n")
         info.store_custom_data("changed_files", changed_files)
+
+    if info.git_branch == "master" and info.repo_name == "ClickHouse/ClickHouse":
+        # store previous commits for perf tests
+        raw = Shell.get_output(
+            f"gh api 'repos/ClickHouse/ClickHouse/commits?sha={info.git_branch}&per_page=30' -q '.[].sha' | head -n30",
+            verbose=True,
+        )
+        commits = raw.splitlines()
+
+        for sha in commits:
+            if sha == info.sha:
+                break
+            commits.pop(0)
+
+        info.store_custom_data("previous_commits_sha", commits)
