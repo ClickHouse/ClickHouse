6820193aacaf522453e6cb1d1625c9f7881dd6bc
Max Kainov <max.kainov@clickhouse.com>
2025-03-30 12:30:59 +0200

    ut in fasttest image

--- ci/defs/job_configs.py
+++ ci/defs/job_configs.py
@@ -465,7 +465,7 @@ class JobConfigs:
         name=JobNames.UNITTEST,
         runs_on=["..params.."],
         command=f"python3 ./ci/jobs/unit_tests_job.py",
-        run_in_docker="clickhouse/binary-builder",
+        run_in_docker="clickhouse/fasttest",
         digest_config=Job.CacheDigestConfig(
             include_paths=["./ci/jobs/unit_tests_job.py"],
         ),
