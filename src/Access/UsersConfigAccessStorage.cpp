dfecf18772c868af5d4d2151e05fea7e7ee2431d
Bharat Nallan Chakravarthy <bharat@clickhouse.com>
2025-03-31 19:49:07 -0700

    simpler check

--- src/Access/UsersConfigAccessStorage.cpp
+++ src/Access/UsersConfigAccessStorage.cpp
@@ -696,8 +696,8 @@ namespace
                     profile_element.min_value = settingStringToValueUtil(setting_name, config.getString(path_to_name + "." + constraint_type));
                 else if (constraint_type == "max")
                     profile_element.max_value = settingStringToValueUtil(setting_name, config.getString(path_to_name + "." + constraint_type));
-                /// When the xml config is parsed, the first constraint_type is `disallow` and when there's more than one disallowed value, subsequent ones are parsed as disallow[1], disallow[2] and so on.
-                /// To ensure that only the following kind of pattern is allowed, validate the constraint_type that's parsed.
+                /// When the xml config is parsed, the first constraint_type is parsed as `disallow` and the subsequent ones are parsed as
+                /// disallow[1], disallow[2] and so on. So, both `disallow` and `disallow[` should be considered as valid constraint types.
                 /// Example:
                 ///   <constraints>
                 ///     <max_execution_time>
@@ -707,8 +707,7 @@ namespace
                 ///         <disallow>5</disallow>
                 ///     </max_execution_time>
                 /// </constraints>
-                else if (constraint_type == "disallow" || (constraint_type.starts_with("disallow[") && constraint_type.ends_with("]")
-                    && std::all_of(constraint_type.begin() + 9, constraint_type.end() - 1, ::isdigit)))
+                else if (constraint_type == "disallow" || constraint_type.starts_with("disallow["))
                     profile_element.disallowed_values.push_back(settingStringToValueUtil(setting_name, config.getString(path_to_name + "." + constraint_type)));
                 else if (constraint_type == "readonly" || constraint_type == "const")
                 {
