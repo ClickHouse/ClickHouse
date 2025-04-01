9b55dd35211ebc6701faa1d77d20e0353f0b3d90
zouyunhe <kevinyunhe8@gmail.com>
2025-04-01 11:06:16 +0000

    fix build error

--- src/Functions/FunctionBinaryArithmetic.h
+++ src/Functions/FunctionBinaryArithmetic.h
@@ -2446,7 +2446,7 @@ ColumnPtr executeStringInteger(const ColumnsWithTypeAndName & arguments, const A
                 auto res = removeNullable(result_type)->createColumn();
                 res->insertManyDefaults(input_rows_count);
                 auto null_map_col = ColumnUInt8::create(input_rows_count, 1);
-                return null_map_col->size() > 0 ? wrapInNullable(std::move(res), std::move(null_map_col)) : makeNullable(std::move(res));
+                return !null_map_col->empty() ? wrapInNullable(std::move(res), std::move(null_map_col)) : makeNullable(std::move(res));
             }
             else if (result_type->isNullable())
             {
@@ -2455,7 +2455,7 @@ ColumnPtr executeStringInteger(const ColumnsWithTypeAndName & arguments, const A
                 for (size_t i = 0; i < input_rows_count; ++i)
                     null_map_data[i] = left_argument.column->isNullAt(i) || !right_argument.column->getBool(i);
                 auto res = executeImpl2(createBlockWithNestedColumns(arguments), removeNullable(result_type), input_rows_count, right_nullmap);
-                return null_map_col->size() > 0 ? wrapInNullable(res, std::move(null_map_col)) : makeNullable(res);
+                return !null_map_col->empty() ? wrapInNullable(res, std::move(null_map_col)) : makeNullable(res);
             }
         }
 
