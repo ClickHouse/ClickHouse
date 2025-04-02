60cf48e12703a45177093b57f73b93c478e1d016
Julia Kartseva <yulia.kartseva@gmail.com>
2025-04-02 23:00:59 +0000

    add test

---
+++ tests/integration/test_plain_rewr_legacy_layout/test.py
@@ -0,0 +1,109 @@
+import os
+import random
+import string
+
+import pytest
+
+from minio.commonconfig import CopySource
+
+from helpers.cluster import ClickHouseCluster
+from helpers.s3_tools import get_file_contents
+
+cluster = ClickHouseCluster(__file__)
+
+NUM_WORKERS=2
+MAX_ROWS = 100
+
+key_prefix="data/"
+
+def gen_insert_values(size):
+    return ",".join(
+        f"({i},'{''.join(random.choices(string.ascii_lowercase, k=5))}')"
+        for i in range(size)
+    )
+
+@pytest.fixture(scope="module", autouse=True)
+def start_cluster():
+    node_names=["source", "dest"]
+    for i in range(NUM_WORKERS):
+        cluster.add_instance(
+            node_names[i],
+            main_configs=["configs/storage_conf.xml"],
+            with_minio=True,
+            env_variables={"ENDPOINT_SUBPATH": node_names[i]},
+            stay_alive=True,
+            # Override ENDPOINT_SUBPATH.
+            instance_env_variables=i > 0,
+        )
+
+    try:
+        cluster.start()
+        yield cluster
+    finally:
+        cluster.shutdown()
+
+
+@pytest.mark.parametrize(
+    "storage_policy",
+    [
+        pytest.param("s3_plain_rewritable"),
+    ],
+)
+def test(storage_policy):
+    def create_insert(node, insert_values):
+        node.query(
+            """
+            CREATE TABLE test (
+                id Int64,
+                data String
+            ) ENGINE=MergeTree()
+            ORDER BY id
+            SETTINGS storage_policy='{}'
+            """.format(
+                storage_policy
+            )
+        )
+        if insert_values:
+            node.query("INSERT INTO test VALUES {}".format(insert_values))
+
+    insert_values = gen_insert_values(random.randint(1, MAX_ROWS))
+
+    create_insert(cluster.instances["source"], insert_values)
+
+    def copy_to_legacy_layout(node_from, node_to):
+        metadata_it = cluster.minio_client.list_objects(
+            cluster.minio_bucket, f"{key_prefix}{node_from}/__meta/", recursive=True)
+        remote_to_local = {}
+        for obj in metadata_it:
+            remote_path = obj.object_name
+            assert remote_path.endswith("prefix.path")
+
+            local_path = get_file_contents(cluster.minio_client, cluster.minio_bucket, remote_path).strip()
+            remote_dir = os.path.dirname(remote_path).replace("__meta/", "")
+            remote_to_local[remote_dir] = local_path
+
+        data_it = cluster.minio_client.list_objects(
+            cluster.minio_bucket, f"{key_prefix}{node_from}/", recursive=True)
+
+        for obj in data_it:
+            remote_path = obj.object_name
+            remote_dir = os.path.dirname(remote_path).replace("__meta/", "")
+            assert remote_dir in remote_to_local
+            filename = os.path.basename(remote_path)
+
+            destination_key = f"{key_prefix}{node_to}/{remote_to_local[remote_dir]}{filename}"
+
+            cluster.minio_client.copy_object(
+                    cluster.minio_bucket,
+                    destination_key,
+                    CopySource(cluster.minio_bucket, obj.object_name))
+
+
+    copy_to_legacy_layout("source", "dest")
+
+    dest = cluster.instances["dest"]
+    dest.restart_clickhouse()
+
+    create_insert(dest, [])
+    assert int(dest.query("SELECT value FROM system.events WHERE name = 'DiskPlainRewritableLegacyLayoutDiskCount'").strip()) > 0
+
