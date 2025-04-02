3ea45f8f9d455b8ad3ebd6f7c496fd2cab15ab86
flynn <fenglv15@mails.ucas.ac.cn>
2025-04-02 12:05:47 +0000

    update test

--- tests/queries/0_stateless/03402_fix_pipe_resize_with_two_level_hash.sh
+++ tests/queries/0_stateless/03402_fix_pipe_resize_with_two_level_hash.sh
@@ -1,4 +1,5 @@
 #!/usr/bin/env bash
+# Tags: no-fasttest
 
 CURDIR=$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)
 # shellcheck source=../shell_config.sh
@@ -15,8 +16,8 @@ for i in {1..10}; do
     done
 done
 
-$CLICKHOUSE_CLIENT --query "EXPLAIN PIPELINE SELECT cityHash64(n) % 65556 AS n, sum(1) FROM fileCluster('test_cluster_two_shards_localhost', '${CLICKHOUSE_TEST_UNIQUE_NAME}/file{1..10}.csv', 'CSV', 'n UInt32') GROUP BY n SETTINGS distributed_aggregation_memory_efficient=1, max_threads = 8"
+$CLICKHOUSE_CLIENT --query "EXPLAIN PIPELINE SELECT cityHash64(n) % 65536 AS n, sum(1) FROM fileCluster('test_cluster_two_shards_localhost', '${CLICKHOUSE_TEST_UNIQUE_NAME}/file{1..10}.csv', 'CSV', 'n UInt32') GROUP BY n SETTINGS distributed_aggregation_memory_efficient=1, max_threads = 8"
 
-$CLICKHOUSE_CLIENT --query "SELECT cityHash64(n) % 65556 AS n, sum(1) FROM fileCluster('test_cluster_two_shards_localhost', '${CLICKHOUSE_TEST_UNIQUE_NAME}/file{1..10}.csv', 'CSV', 'n UInt32') GROUP BY n FORMAT NULL SETTINGS distributed_aggregation_memory_efficient=1, max_threads = 8"
+$CLICKHOUSE_CLIENT --query "SELECT cityHash64(n) % 65536 AS n, sum(1) FROM fileCluster('test_cluster_two_shards_localhost', '${CLICKHOUSE_TEST_UNIQUE_NAME}/file{1..10}.csv', 'CSV', 'n UInt32') GROUP BY n FORMAT NULL SETTINGS distributed_aggregation_memory_efficient=1, max_threads = 8"
 
 rm "${USER_FILES_PATH}"/"${CLICKHOUSE_TEST_UNIQUE_NAME}"/file*.csv
