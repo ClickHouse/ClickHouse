d00822b7a0bd5d59e003bbc0738430170fa7b6a9
Max Kainov <max.kainov@clickhouse.com>
2025-04-03 17:14:53 +0200

    another example

--- ci/jobs/docs_job.py
+++ ci/jobs/docs_job.py
@@ -3,8 +3,26 @@ from ci.praktika.utils import Shell
 
 if __name__ == "__main__":
 
-    res = Shell.check("echo Hello Shaun && exit 1", verbose=True)
+    results = []
 
-    Result.create_from(status=res, info="Shaun, please fix me").add_job_summary_to_info(
+    results.append(
+        Result.from_commands_run(
+            name="task 1", command="echo Hello Shaun", with_info=True
+        )
+    )
+
+    results.append(
+        Result.from_commands_run(
+            name="task 2", command="echo How are you?", with_info=True
+        )
+    )
+
+    results.append(
+        Result.from_commands_run(
+            name="task 3", command="echo Did you fix the docs job? && exit 1", with_info=True
+        )
+    )
+
+    Result.create_from(results=results).add_job_summary_to_info(
         with_local_run_command=True
     ).complete_job()
