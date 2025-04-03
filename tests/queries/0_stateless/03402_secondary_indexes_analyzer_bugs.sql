bfb24f46f3a131a05037f8c35dff39dad116ab8d
Nikolai Kochetov <nk@clickhouse.com>
2025-04-03 10:38:17 +0000

    Unflaky the new test.

--- tests/queries/0_stateless/03402_secondary_indexes_analyzer_bugs.sql
+++ tests/queries/0_stateless/03402_secondary_indexes_analyzer_bugs.sql
@@ -1,4 +1,4 @@
--- Tags: no-random-merge-tree-settings, no-random-settings
+-- Tags: no-random-merge-tree-settings, no-random-settings, no-parallel-replicas
 
 --- #65607
 
@@ -16,7 +16,7 @@ SETTINGS index_granularity = 8192, index_granularity_bytes = '10Mi';
 
 INSERT INTO t SELECT * FROM generateRandom('tenant String, recordTimestamp Int64, responseBody String') LIMIT 65536;
 
-explain indexes=1 select tenant,recordTimestamp from t where colAlias like '%abcd%';
+explain indexes=1 select tenant,recordTimestamp from t where colAlias like '%abcd%' settings enable_analyzer=1;
 
 
 --- #69373
