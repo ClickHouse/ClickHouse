3db4d34f7c122280aec299172f93a21362784ac3
Shaun Struwig <41984034+Blargian@users.noreply.github.com>
2025-04-03 18:30:32 +0200

    update docs_job to build docusaurus

--- ci/jobs/docs_job.py
+++ ci/jobs/docs_job.py
@@ -1,25 +1,40 @@
 from ci.praktika.result import Result
 from ci.praktika.utils import Shell
 
+# Get the latest changes from the ClickHouse/clickhouse-docs repo
+def fetch_latest_docs_repo_changes():
+    git_branch = Shell.check("git branch --show-current", verbose=True, strict=True)
+
+    if  git_branch != "main":
+        Shell.run(f"git fetch origin main")
+        Shell.run(f"git checkout -f main")
+    else:
+        # Update docs repo
+        Shell.run("git pull")
+
+# Install packages and copy the ClickHouse/ClickHouse docs
+def run_docs_repo_setup():
+    Shell.run("yarn install")
+    Shell.run("copy-clickhouse-repo-docs -l docs")
+
+def build_docusaurus():
+    res, out, err = Shell.get_res_stdout_stderr(
+        "yarn build --locale en"
+    )
+    if err:
+        out += err
+    return out
+
 if __name__ == "__main__":
 
     results = []
 
-    results.append(
-        Result.from_commands_run(
-            name="task 1", command="echo Hello Shaun", with_info=True
-        )
-    )
+    fetch_latest_docs_repo_changes()
+    run_docs_repo_setup()
 
     results.append(
         Result.from_commands_run(
-            name="task 2", command="echo How are you?", with_info=True
-        )
-    )
-
-    results.append(
-        Result.from_commands_run(
-            name="task 3", command="echo Did you fix the docs job? && exit 1", with_info=True
+            name="Build Docusaurus", command=build_docusaurus, with_info=True
         )
     )
 
