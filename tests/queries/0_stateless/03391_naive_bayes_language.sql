5a72677966b6cbfec8d65dc07f7cf23f44a0535a
nihalzp <81457724+nihalzp@users.noreply.github.com>
2025-04-05 02:11:54 +0800

    Add stateless tests for sample sentiment and language models

---
+++ tests/queries/0_stateless/03391_naive_bayes_language.sql
@@ -0,0 +1,4 @@
+SELECT naiveBayesClassifier('language', 'hello world');
+SELECT naiveBayesClassifier('language', 'hola amigo');
+SELECT naiveBayesClassifier('language', 'bonjour tout le monde');
+SELECT naiveBayesClassifier('language', 'привет мир');
