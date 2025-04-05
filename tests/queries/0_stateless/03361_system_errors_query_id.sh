e29657986f1ad0b520a7eaddb17baaec22a0dbe3
Vladimir Baikov <bkvvldmr@gmail.com>
2025-04-05 21:34:25 +0100

    fixed racy test

--- tests/queries/0_stateless/03361_system_errors_query_id.sh
+++ tests/queries/0_stateless/03361_system_errors_query_id.sh
@@ -6,11 +6,10 @@ CUR_DIR=$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)
 . "$CUR_DIR"/../shell_config.sh
 
 query_id="01545_system_errors-$CLICKHOUSE_DATABASE"
-$CLICKHOUSE_CLIENT --query_id="$query_id" -q "select query_id" &>/dev/null
 
 retry=0
 max_retries=30
-while [[ $($CLICKHOUSE_CLIENT -q "SELECT count(*) FROM system.errors WHERE query_id = '$query_id' and code = 47") -ne 1 ]]; do
+while [[ $($CLICKHOUSE_CLIENT -q "SELECT count(*) FROM system.errors WHERE query_id = '$query_id' and code = 47") -lt 1 ]]; do
   retry=$((retry+1))
 
   if [ $retry -ge $max_retries ]; then
@@ -19,6 +18,8 @@ while [[ $($CLICKHOUSE_CLIENT -q "SELECT count(*) FROM system.errors WHERE query
   fi
 
   sleep 0.5
+
+  $CLICKHOUSE_CLIENT --query_id="$query_id" -q "select query_id" &>/dev/null
 done
 
 echo "OK"
