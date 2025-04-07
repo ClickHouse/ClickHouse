f0ac79be948ef92a6f11ecef29263e3bf18860a1
Yakov Olkhovskiy <99031427+yakov-olkhovskiy@users.noreply.github.com>
2025-04-07 10:40:43 -0400

    fix test

--- tests/queries/0_stateless/03402_join_using_alias.sql
+++ tests/queries/0_stateless/03402_join_using_alias.sql
@@ -1,4 +1,4 @@
 set enable_analyzer=1;
 
 CREATE OR REPLACE TABLE t0 (c0 Int, c1 Int ALIAS 1) ENGINE = Memory;
-SELECT c0 FROM remote('localhost:9000', 'default', 't0') tx JOIN t0 USING (c1); -- { serverError BAD_ARGUMENTS }
+SELECT c0 FROM remote('localhost', currentDatabase(), 't0') tx JOIN t0 USING (c1); -- { serverError BAD_ARGUMENTS }
