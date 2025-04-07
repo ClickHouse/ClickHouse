2ca80089b8efc9151394da182f87be799b4c9159
Leo Qu <leoqu9577@gmail.com>
2025-04-07 17:27:13 -0400

    fixed style check

--- src/Disks/ObjectStorages/S3/S3ObjectStorage.cpp
+++ src/Disks/ObjectStorages/S3/S3ObjectStorage.cpp
@@ -138,7 +138,7 @@ private:
             auto objects = outcome.GetResult().GetContents();
             for (const auto & object : objects)
             {
-                ObjectMetadata metadata{static_cast<uint64_t>(object.GetSize()), Poco::Timestamp::fromEpochTime(object.GetLastModified().Seconds()), object.GetETag(),  {}, {}};
+                ObjectMetadata metadata{static_cast<uint64_t>(object.GetSize()), Poco::Timestamp::fromEpochTime(object.GetLastModified().Seconds()), object.GetETag(), {}, {}};
                 batch.emplace_back(std::make_shared<RelativePathWithMetadata>(object.GetKey(), std::move(metadata)));
             }
 
