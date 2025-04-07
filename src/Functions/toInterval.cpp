db1e95005dbb7339eeae563455ebeaae5dd185a0
Drew Davis <pulpdrew@gmail.com>
2025-04-06 21:00:31 -0400

    rename makeInterval to toInterval

--- src/Functions/toInterval.cpp
+++ src/Functions/toInterval.cpp
@@ -16,20 +16,20 @@ namespace ErrorCodes
     extern const int NUMBER_OF_ARGUMENTS_DOESNT_MATCH;
 }
 
-class FunctionMakeInterval : public IFunction
+class FunctionToInterval : public IFunction
 {
 public:
-    static constexpr auto name = "makeInterval";
+    static constexpr auto name = "toInterval";
 
-    explicit FunctionMakeInterval(ContextPtr context_) : context(context_) {}
+    explicit FunctionToInterval(ContextPtr context_) : context(context_) {}
 
-    static FunctionPtr create(ContextPtr context) { return std::make_shared<FunctionMakeInterval>(context); }
+    static FunctionPtr create(ContextPtr context) { return std::make_shared<FunctionToInterval>(context); }
 
     String getName() const override { return name; }
     bool isSuitableForShortCircuitArgumentsExecution(const DataTypesWithConstInfo & /*arguments*/) const override { return true; }
     size_t getNumberOfArguments() const override { return 2; }
     bool useDefaultImplementationForConstants() const override { return true; }
-    ColumnNumbers getArgumentsThatAreAlwaysConstant() const override { return {0}; }
+    ColumnNumbers getArgumentsThatAreAlwaysConstant() const override { return {1}; }
 
     bool hasInformationAboutMonotonicity() const override { return true; }
 
@@ -43,24 +43,19 @@ public:
         if (arguments.size() != 2)
             throw Exception(ErrorCodes::NUMBER_OF_ARGUMENTS_DOESNT_MATCH, "Function {} must be 2 arguments", getName());
 
-        /// The first argument is a constant string with the name of interval kind.
+        /// The second argument is a constant string with the name of interval kind.
         String interval_kind;
-        const ColumnConst * datepart_column = checkAndGetColumnConst<ColumnString>(arguments[0].column.get());
-        if (!datepart_column)
-            throw Exception(ErrorCodes::ILLEGAL_TYPE_OF_ARGUMENT, "First argument for function {} must be constant string: "
-                "name of datepart", getName());
+        const ColumnConst * kind_column = checkAndGetColumnConst<ColumnString>(arguments[1].column.get());
+        if (!kind_column)
+            throw Exception(ErrorCodes::ILLEGAL_TYPE_OF_ARGUMENT, "Second argument for function {} must be constant string: "
+                "unit of interval", getName());
 
-        interval_kind = Poco::toLower(datepart_column->getValue<String>());
+        interval_kind = Poco::toLower(kind_column->getValue<String>());
         if (interval_kind.empty())
-            throw Exception(ErrorCodes::BAD_ARGUMENTS, "First argument (name of datepart) for function {} cannot be empty", getName());
+            throw Exception(ErrorCodes::BAD_ARGUMENTS, "Second argument (unit) for function {} cannot be empty", getName());
         
         if (!IntervalKind::tryParseString(interval_kind, kind.kind))
-            throw Exception(ErrorCodes::BAD_ARGUMENTS, "{} doesn't look like datepart name in {}", interval_kind, getName());
-
-        /// The second argument is an integer representing the length of the interval
-        if (!isInteger(arguments[1].type))
-            throw Exception(ErrorCodes::ILLEGAL_TYPE_OF_ARGUMENT, "Illegal type {} of 2nd argument of function {}. "
-                "Should be an integer", arguments[1].type->getName(), getName());
+            throw Exception(ErrorCodes::BAD_ARGUMENTS, "{} doesn't look like an interval unit in {}", interval_kind, getName());
 
         return std::make_shared<DataTypeInterval>(kind);
     }
@@ -68,7 +63,7 @@ public:
     ColumnPtr executeImpl(const ColumnsWithTypeAndName & arguments, const DataTypePtr & result_type, size_t input_rows_count) const override
     {
         ColumnsWithTypeAndName temp_columns(1);
-        temp_columns[0] = arguments[1];
+        temp_columns[0] = arguments[0];
 
         const char * to_interval_function_name = kind.toNameOfFunctionToIntervalDataType();
         auto to_interval_function = FunctionFactory::instance().get(to_interval_function_name, context);
@@ -81,10 +76,10 @@ private:
     mutable IntervalKind kind = IntervalKind::Kind::Second;
 };
 
-REGISTER_FUNCTION(MakeInterval)
+REGISTER_FUNCTION(ToInterval)
 {
-    factory.registerFunction<FunctionMakeInterval>(
-        FunctionDocumentation{.description = R"(Creates an interval value from an interval type and length.)"});
+    factory.registerFunction<FunctionToInterval>(
+        FunctionDocumentation{.description = R"(Creates an interval from a value and a unit.)"});
 }
 
 }
