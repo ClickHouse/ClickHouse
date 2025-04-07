42f9e488dab43c91963a7911001d27785a896008
Alex Katsman <alex.katsman@clickhouse.com>
2025-04-07 19:24:31 +0000

    Improve test

--- tests/integration/test_server_overload/test.py
+++ tests/integration/test_server_overload/test.py
@@ -31,23 +31,35 @@ def started_cluster():
 
 
 def test_overload(started_cluster):
+    queries = []
     for i in range(4):
-        node1.get_query_request("select * from numbers(1e18) format null", ignore_error=True, timeout=30)
+        queries.append(node1.get_query_request("select * from numbers(1e18) format null", ignore_error=True, timeout=15))
+
+    def wait_for_queries():
+        for query in queries:
+            query.get_answer()
 
     for i in range(30):
         try:
             node1.query("select 1 settings min_os_cpu_wait_time_ratio_to_throw=2, max_os_cpu_wait_time_ratio_to_throw=6")
         except QueryRuntimeException as ex:
             assert "(SERVER_OVERLOADED)" in str(ex), "Only server overloaded error is expected"
+            wait_for_queries() # Needed for flaky check to make sure CPU is not loaded with queries from previous runs
             return
-        time.sleep(0.5)
+        time.sleep(0.3)
 
     assert False, "Expected to get the server overloaded error at least once"
+    wait_for_queries() # Needed for flaky check to make sure CPU is not loaded with queries from previous runs
 
 
 def test_drop_connections(started_cluster):
+    queries = []
     for i in range(4):
-        node2.get_query_request("select * from numbers(1e18) format null", ignore_error=True, timeout=30)
+        queries.append(node2.get_query_request("select * from numbers(1e18) format null", ignore_error=True, timeout=15))
+
+    def wait_for_queries():
+        for query in queries:
+            query.get_answer()
 
     for i in range(30):
         try:
@@ -56,7 +68,9 @@ def test_drop_connections(started_cluster):
             assert "Connection reset by peer" in str(ex), "Only connection drop is expected"
             assert node2.contains_in_log("CPU is overloaded, CPU is waiting for execution way more than executing"), "Expected server overloaded error in the log"
             assert node2.contains_in_log("probability used to decide whether to drop the connection"), "Expected message that the connection was dropped due to server overload in the log"
+            wait_for_queries() # Needed for flaky check to make sure CPU is not loaded with queries from previous runs
             return
-        time.sleep(0.5)
+        time.sleep(0.3)
 
     assert False, "Expected to drop the connection due to the server being overloaded at least once"
+    wait_for_queries() # Needed for flaky check to make sure CPU is not loaded with queries from previous runs
