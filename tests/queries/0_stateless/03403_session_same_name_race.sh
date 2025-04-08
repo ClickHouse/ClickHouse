ec25ef1a092a6c3daa897d4bae15746194cea38e
Alex Katsman <alex.katsman@clickhouse.com>
2025-04-08 11:50:16 +0000

    Fix test

--- tests/queries/0_stateless/03403_session_same_name_race.sh
+++ tests/queries/0_stateless/03403_session_same_name_race.sh
@@ -6,8 +6,10 @@ CURDIR=$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)
 
 export SESSION="03403_session_${CLICKHOUSE_DATABASE}"
 export SESSION_ID="${SESSION}_$RANDOM.$RANDOM"
-export SETTINGS="session_id=$SESSION_ID&session_timeout=2&throw_on_unsupported_query_inside_transaction=0"
+export SETTINGS="session_id=$SESSION_ID&session_timeout=2"
 
+# The general idea behind the test is to check that if the session was closed,
+# we won't try to close a newly created session with the same name (and cause some inconsistent state by it)
 $CLICKHOUSE_CURL -sS -d 'select 1' "$CLICKHOUSE_URL&$SETTINGS&close_session=1"
 # Sleep some more time to give more chances for a cleanThread in Session.cpp to schedule a close for this session
 $CLICKHOUSE_CURL -sS -d 'select sleep(5) settings function_sleep_max_microseconds_per_block = 5000000' "$CLICKHOUSE_URL&$SETTINGS"
