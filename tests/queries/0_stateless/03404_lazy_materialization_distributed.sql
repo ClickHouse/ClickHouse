8c54eb728d4ff5ee0f8b21ac709b76663ba4d6bb
Igor Nikonov <igor@clickhouse.com>
2025-04-08 10:33:22 +0200

    Test

---
+++ tests/queries/0_stateless/03404_lazy_materialization_distributed.sql
@@ -0,0 +1,16 @@
+SELECT * FROM numbers(10)
+SETTINGS log_comment = '42809e74-efe7-412f-8326-7569039feecb' FORMAT Null;
+
+-- make sure the optimization is enabled
+set query_plan_optimize_lazy_materialization=true, query_plan_max_limit_for_lazy_materialization=10;
+
+SYSTEM FLUSH LOGS;
+SELECT
+    log_comment,
+    query,
+    query_kind,
+    type
+FROM clusterAllReplicas(test_cluster_one_shard_three_replicas_localhost, system.query_log)
+WHERE log_comment = '42809e74-efe7-412f-8326-7569039feecb' AND type != 'QueryStart' and currentDatabase() = current_database
+ORDER BY event_time ASC
+LIMIT 1;
