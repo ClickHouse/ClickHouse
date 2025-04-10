0012d4887195108463bfcd385ce5320e09e62af6
Konstantin Bogdanov <konstantin@clickhouse.com>
2025-04-10 22:36:41 +0200

    Lint

--- programs/main.cpp
+++ programs/main.cpp
@@ -227,8 +227,6 @@ __attribute__((constructor(0))) void init_je_malloc_message()
         write(STDERR_FILENO, message_view.data(), message_view.size());
 #    endif
     };
-
-    DB::UseSSL();
 }
 #endif
 
