0cb1653483445a9a04d0563c5ff3521d0a1afc30
Yakov Olkhovskiy <yakov@clickhouse.com>
2025-04-10 01:26:12 +0000

    fix test

--- tests/integration/test_prefer_global_in_and_join/test.py
+++ tests/integration/test_prefer_global_in_and_join/test.py
@@ -25,21 +25,16 @@ ch2 = cluster.add_instance(
 def started_cluster():
     try:
         cluster.start()
-        ch1.query("CREATE DATABASE test_default_database ON CLUSTER 'cluster';")
         yield cluster
 
     finally:
         cluster.shutdown()
 
-@pytest.fixture(autouse=True)
-def cleanup_after_test():
-    try:
-        yield
-    finally:
-        ch1.query("DROP DATABASE IF EXISTS test_default_database ON CLUSTER 'cluster' SYNC")
 
 
 def test_in_and_join(started_cluster):
+    ch1.query("CREATE DATABASE test_default_database ON CLUSTER 'cluster';")
+
     ch1.query(
         database="test_default_database",
         sql="CREATE TABLE t1 (A Int64) ENGINE = MergeTree ORDER BY A AS SELECT * FROM values((1),(2),(3));",
@@ -74,3 +69,5 @@ CREATE TABLE t2 (A Int64) ENGINE = MergeTree ORDER BY A AS SELECT * FROM values(
             sql="SELECT * FROM t1_d JOIN t2 USING (A) SETTINGS prefer_global_in_and_join = 1, enable_analyzer = 1"
         ) == "1\n2\n"
     )
+
+    ch1.query("DROP DATABASE IF EXISTS test_default_database ON CLUSTER 'cluster' SYNC")
