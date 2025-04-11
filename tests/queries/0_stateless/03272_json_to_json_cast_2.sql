918f7b019a6f6509af0ad45ea43ccfc5a400e0ad
avogar <avogar@clickhouse.com>
2025-04-11 12:00:12 +0000

    Update test

--- tests/queries/0_stateless/03272_json_to_json_cast_2.sql
+++ tests/queries/0_stateless/03272_json_to_json_cast_2.sql
@@ -13,13 +13,13 @@ insert into test format JSONAsObject
 {"k1" : 4, "k4" : 24, "k3" : 34, "k2" : 44};
 
 select 'max_dynamic_paths=3';
-select json::JSON(max_dynamic_paths=3) as json2, JSONDynamicPaths(json2), JSONSharedDataPaths(json2), json2.k1, json2.k2, json2.k3, json2.k4 from test;
+select json::JSON(max_dynamic_paths=3) as json2, JSONAllPaths(json2), JSONSharedDataPaths(json2), json2.k1, json2.k2, json2.k3, json2.k4 from test;
 select 'max_dynamic_paths=2';
-select json::JSON(max_dynamic_paths=2) as json2, JSONDynamicPaths(json2), JSONSharedDataPaths(json2), json2.k1, json2.k2, json2.k3, json2.k4 from test;
+select json::JSON(max_dynamic_paths=2) as json2, JSONAllPaths(json2), JSONSharedDataPaths(json2), json2.k1, json2.k2, json2.k3, json2.k4 from test;
 select 'max_dynamic_paths=1';
-select json::JSON(max_dynamic_paths=1) as json2, JSONDynamicPaths(json2), JSONSharedDataPaths(json2), json2.k1, json2.k2, json2.k3, json2.k4 from test;
+select json::JSON(max_dynamic_paths=1) as json2, JSONAllPaths(json2), JSONSharedDataPaths(json2), json2.k1, json2.k2, json2.k3, json2.k4 from test;
 select 'max_dynamic_paths=0';
-select json::JSON(max_dynamic_paths=0) as json2, JSONDynamicPaths(json2), JSONSharedDataPaths(json2), json2.k1, json2.k2, json2.k3, json2.k4 from test;
+select json::JSON(max_dynamic_paths=0) as json2, JSONAllPaths(json2), JSONSharedDataPaths(json2), json2.k1, json2.k2, json2.k3, json2.k4 from test;
 
 drop table test;
 
@@ -29,24 +29,16 @@ create table test (id UInt64, json JSON(max_dynamic_paths=4)) engine=MergeTree o
 insert into test select number, multiIf(number < 10000, '{"k2" : 42}', number < 30000, '{"k3" : 42}', number < 60000, '{"k4" : 42}', number < 100000, '{"k1" : 42}', '{"k1" : 42, "k2" : 42, "k3" : 42, "k4" : 42}') from numbers(150000);
 
 select 'max_dynamic_paths=3';
-select 'Dynamic paths';
-select distinct arrayJoin(JSONDynamicPaths(json2)) from (select json::JSON(max_dynamic_paths=3) as json2 from test) order by all;
-select 'Shared data paths';
-select distinct arrayJoin(JSONSharedDataPaths(json2)) from (select json::JSON(max_dynamic_paths=3) as json2 from test) order by all;
+select 'All paths';
+select distinct arrayJoin(JSONAllPaths(json2)) from (select json::JSON(max_dynamic_paths=3) as json2 from test) order by all;
 select 'max_dynamic_paths=2';
-select 'Dynamic paths';
-select distinct arrayJoin(JSONDynamicPaths(json2)) from (select json::JSON(max_dynamic_paths=2) as json2 from test) order by all;
-select 'Shared data paths';
-select distinct arrayJoin(JSONSharedDataPaths(json2)) from (select json::JSON(max_dynamic_paths=2) as json2 from test) order by all;
+select 'All paths';
+select distinct arrayJoin(JSONAllPaths(json2)) from (select json::JSON(max_dynamic_paths=2) as json2 from test) order by all;
 select 'max_dynamic_paths=1';
-select 'Dynamic paths';
-select distinct arrayJoin(JSONDynamicPaths(json2)) from (select json::JSON(max_dynamic_paths=1) as json2 from test) order by all;
-select 'Shared data paths';
-select distinct arrayJoin(JSONSharedDataPaths(json2)) from (select json::JSON(max_dynamic_paths=1) as json2 from test) order by all;
+select 'All paths';
+select distinct arrayJoin(JSONAllPaths(json2)) from (select json::JSON(max_dynamic_paths=1) as json2 from test) order by all;
 select 'max_dynamic_paths=0';
-select 'Dynamic paths';
-select distinct arrayJoin(JSONDynamicPaths(json2)) from (select json::JSON(max_dynamic_paths=0) as json2 from test) order by all;
-select 'Shared data paths';
-select distinct arrayJoin(JSONSharedDataPaths(json2)) from (select json::JSON(max_dynamic_paths=0) as json2 from test) order by all;
+select 'All paths';
+select distinct arrayJoin(JSONAllPaths(json2)) from (select json::JSON(max_dynamic_paths=0) as json2 from test) order by all;
 
 drop table test;
