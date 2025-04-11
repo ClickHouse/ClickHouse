b64861ffafb9e86c97e60b94779c19b726de0996
Igor Nikonov <igor@clickhouse.com>
2025-04-11 18:42:24 +0200

    Rewrite test

--- tests/queries/0_stateless/03404_lazy_materialization_distributed.sql
+++ tests/queries/0_stateless/03404_lazy_materialization_distributed.sql
@@ -1,16 +1,15 @@
-SELECT * FROM numbers(10)
-SETTINGS log_comment = '42809e74-efe7-412f-8326-7569039feecb' FORMAT Null;
+DROP TABLE IF EXISTS tt;
+CREATE TABLE tt (k UInt64, v String, blob String) ENGINE=MergeTree() ORDER BY tuple();
+INSERT INTO tt SELECT number, toString(number), repeat('blob_bob', number) FROM numbers(1, 10);
 
 -- make sure the optimization is enabled
 set query_plan_optimize_lazy_materialization=true, query_plan_max_limit_for_lazy_materialization=10;
 
-SYSTEM FLUSH LOGS;
 SELECT
-    log_comment,
-    query,
-    query_kind,
-    type
-FROM clusterAllReplicas(test_cluster_one_shard_three_replicas_localhost, system.query_log)
-WHERE log_comment = '42809e74-efe7-412f-8326-7569039feecb' AND type != 'QueryStart' and current_database = currentDatabase() and event_date >= yesterday()
-ORDER BY event_time ASC
-LIMIT 1;
+    v,
+    blob
+FROM clusterAllReplicas(test_cluster_one_shard_three_replicas_localhost, currentDatabase(), tt)
+ORDER BY k
+LIMIT 3;
+
+DROP TABLE tt;
