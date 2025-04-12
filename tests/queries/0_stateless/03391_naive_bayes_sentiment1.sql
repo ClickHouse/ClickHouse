ada282bb42e28ddf86a9b3ba02601ffddd71d122
nihalzp <81457724+nihalzp@users.noreply.github.com>
2025-04-12 21:26:21 +0800

    Update and add tests for naiveBayesClassifier

--- tests/queries/0_stateless/03391_naive_bayes_sentiment1.sql
+++
@@ -1,9 +0,0 @@
--- Tags: no-fasttest
--- no-fasttest: depends on model binary and model details via config files
-
-SELECT naiveBayesClassifier('sentiment1', 'The interface is beautiful and intuitive');
-SELECT naiveBayesClassifier('sentiment1', 'This product is amazing in every way');
-SELECT naiveBayesClassifier('sentiment1', 'I am impressed by the excellent quality');
-SELECT naiveBayesClassifier('sentiment1', 'The app is awful and barely works');
-SELECT naiveBayesClassifier('sentiment1', 'Customer support was horrible today');
-SELECT naiveBayesClassifier('sentiment1', 'This experience was a total disaster');
--- tests/queries/0_stateless/03392_naive_bayes_sentiment2.sql
+++
@@ -1,9 +0,0 @@
--- Tags: no-fasttest
--- no-fasttest: depends on model binary and model details via config files
-
-SELECT naiveBayesClassifier('sentiment2', 'The interface is beautiful and intuitive');
-SELECT naiveBayesClassifier('sentiment2', 'This product is amazing in every way');
-SELECT naiveBayesClassifier('sentiment2', 'I am impressed by the excellent quality');
-SELECT naiveBayesClassifier('sentiment2', 'The app is awful and barely works');
-SELECT naiveBayesClassifier('sentiment2', 'Customer support was horrible today');
-SELECT naiveBayesClassifier('sentiment2', 'This experience was a total disaster');
