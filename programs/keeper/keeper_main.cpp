25af85cd7d6c191ba7d8e5ce35f53829acab366a
Konstantin Bogdanov <konstantin@clickhouse.com>
2025-04-13 23:35:26 +0200

    Fix

--- programs/keeper/keeper_main.cpp
+++ programs/keeper/keeper_main.cpp
@@ -154,7 +154,7 @@ __attribute__((constructor(0))) void init_je_malloc_message()
 #endif
 
 /// OpenSSL early initialization.
-__attribute__((constructor(202))) void init_ssl()
+__attribute__((constructor(0))) void init_ssl()
 {
     DB::OpenSSLInitializer::initialize();
 }
--- src/Common/Crypto/OpenSSLInitializer.cpp
+++ src/Common/Crypto/OpenSSLInitializer.cpp
@@ -10,13 +10,11 @@
 #    include <openssl/ssl.h>
 #endif
 
+#include <iostream>
+
+
 namespace DB
 {
-
-namespace ErrorCodes
-{
-    extern const int OPENSSL_ERROR;
-}
 
 #if USE_SSL
 std::atomic<uint8_t> DB::OpenSSLInitializer::ref_count{0};
@@ -36,15 +34,15 @@ void OpenSSLInitializer::initialize()
     {
         int basic_init = OPENSSL_init_ssl(OPENSSL_INIT_LOAD_CONFIG, nullptr);
         if (!basic_init)
-            throw Exception(ErrorCodes::OPENSSL_ERROR, "Failed to initialize OpenSSL.");
+            throw std::runtime_error("Failed to initialize OpenSSL.");
 
         default_provider = OSSL_PROVIDER_load(nullptr, "default");
         if (!default_provider)
-            throw Exception(ErrorCodes::OPENSSL_ERROR, "Failed to load 'default' provider.");
+            throw std::runtime_error("Failed to load 'default' provider.");
 
         legacy_provider = OSSL_PROVIDER_load(nullptr, "legacy");
         if (!legacy_provider)
-            throw Exception(ErrorCodes::OPENSSL_ERROR, "Failed to load 'legacy' provider.");
+            std::cerr << "Failed to load 'legacy' OpenSSL provider, legacy ciphers will not be supported." << std::endl;
     }
 #endif
 }
