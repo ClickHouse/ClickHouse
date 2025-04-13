eb6f7e664c4221601458e1d9f2408536a3b99121
Robert Schulze <robert@clickhouse.com>
2025-04-13 10:27:26 +0000

    Less noisy logging

--- src/Interpreters/Cache/QueryConditionCache.cpp
+++ src/Interpreters/Cache/QueryConditionCache.cpp
@@ -61,17 +61,16 @@ void QueryConditionCache::write(
     if (has_final_mark)
         entry->matching_marks[marks_count - 1] = false;
 
-    LOG_DEBUG(
+    LOG_TRACE(
         logger,
-        "{} entry for table_id: {}, part_name: {}, condition_hash: {}, condition: {}, marks_count: {}, has_final_mark: {}, ranges: {}",
+        "{} entry for table_id: {}, part_name: {}, condition_hash: {}, condition: {}, marks_count: {}, has_final_mark: {}",
         inserted ? "Inserted" : "Updated",
         table_id,
         part_name,
         condition_hash,
         condition,
         marks_count,
-        has_final_mark,
-        toString(mark_ranges));
+        has_final_mark);
 }
 
 std::optional<QueryConditionCache::MatchingMarks> QueryConditionCache::read(const UUID & table_id, const String & part_name, UInt64 condition_hash)
@@ -84,13 +83,12 @@ std::optional<QueryConditionCache::MatchingMarks> QueryConditionCache::read(cons
 
         std::shared_lock lock(entry->mutex);
 
-        LOG_DEBUG(
+        LOG_TRACE(
             logger,
-            "Read entry for table_uuid: {}, part: {}, condition_hash: {}, ranges: {}",
+            "Read entry for table_uuid: {}, part: {}, condition_hash: {}",
             table_id,
             part_name,
-            condition_hash,
-            toString(entry->matching_marks));
+            condition_hash);
 
         return {entry->matching_marks};
     }
@@ -98,7 +96,7 @@ std::optional<QueryConditionCache::MatchingMarks> QueryConditionCache::read(cons
     {
         ProfileEvents::increment(ProfileEvents::QueryConditionCacheMisses);
 
-        LOG_DEBUG(
+        LOG_TRACE(
             logger,
             "Could not find entry for table_uuid: {}, part: {}, condition_hash: {}",
             table_id,
