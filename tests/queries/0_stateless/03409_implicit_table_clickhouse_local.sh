bb831503a9d7b5fc60706b1d6b713d40f5a9c423
Alexey Milovidov <milovidov@clickhouse.com>
2025-04-14 05:34:58 +0200

    Fix test

--- tests/queries/0_stateless/03409_implicit_table_clickhouse_local.sh
+++ tests/queries/0_stateless/03409_implicit_table_clickhouse_local.sh
@@ -4,4 +4,4 @@ CURDIR=$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)
 # shellcheck source=../shell_config.sh
 . "$CURDIR"/../shell_config.sh
 
-echo '{"Hello": {"world": 123}, "goodbye": "test"}' | ${CLICKHOUSE_LOCAL} -q "Hello.world, goodbye"
+echo '{"Hello": {"world": 123}, "goodbye": "test"}' | ${CLICKHOUSE_LOCAL} -q "Hello.world, goodbye" --input-format JSONEachRow
