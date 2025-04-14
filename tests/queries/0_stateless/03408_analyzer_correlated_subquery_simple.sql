171da05b8a42a141dc14bb69ad423c72d2483610
Dmitry Novik <n0vik@clickhouse.com>
2025-04-14 16:42:01 +0200

    Add a test for #72459

---
+++ tests/queries/0_stateless/03408_analyzer_correlated_subquery_simple.sql
@@ -0,0 +1,17 @@
+CREATE TABLE t1 (c1 UInt64, c2 UInt64) ORDER BY c1;
+CREATE TABLE t2 (c1 UInt64) ORDER BY c1;
+
+INSERT INTO t1 SELECT number, number % 100 FROM numbers(100);
+INSERT INTO t2 SELECT number*number FROM numbers(100);
+
+set allow_experimental_correlated_subqueries = 1;
+
+SELECT count(t1.c1) FROM t1
+WHERE 
+t1.c2 = 10
+AND EXISTS (SELECT * FROM t2 WHERE t1.c1 = t2.c1);
+
+SELECT count(t1.c1) FROM t1
+WHERE 
+t1.c2 = 10
+AND NOT EXISTS (SELECT * FROM t2 WHERE t1.c1 = t2.c1);
