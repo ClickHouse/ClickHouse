fb01d5d3bdc6cd1625472f34ee86d85648742c77
Alexey Milovidov <milovidov@clickhouse.com>
2025-04-14 16:52:16 +0200

    Fix test

--- tests/queries/0_stateless/03408_implicit_table.sql
+++ tests/queries/0_stateless/03408_implicit_table.sql
@@ -1,5 +1,5 @@
 -- { echo }
-SET implicit_select = 1, implicit_table_at_top_level = 'test';
+SET implicit_select = 1, implicit_table_at_top_level = 'test', enable_analyzer = 1;
 DROP TABLE IF EXISTS test;
 CREATE TABLE test (s String) ENGINE = Memory;
 INSERT INTO test VALUES ('Hello'), ('World');
