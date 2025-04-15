ed4603e39643be5d740d34e3a0b9a84053f743af
flynn <fenglv15@mails.ucas.ac.cn>
2025-04-15 05:09:51 +0000

    Add ProfileEvents ParquetReadRowGroups and ParquetPrunedRowGroups to profile parquet index prune

---
+++ tests/queries/0_stateless/03408_parquet_row_group_profile_events.sh
@@ -0,0 +1,20 @@
+#!/usr/bin/env bash
+
+CUR_DIR=$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)
+# shellcheck source=../shell_config.sh
+. "$CUR_DIR"/../shell_config.sh
+
+
+query_id="${CLICKHOUSE_DATABASE}_parquet_group_group_prune_profile_$RANDOM"
+
+# A parquet file with 10 row groups from 1 to 100, 10 rows per row group.
+${CLICKHOUSE_CLIENT} "${opts[@]}" --query_id="${query_id}" --query="SELECT * FROM file('$CUR_DIR/data_parquet/row_group.parquet.') WHERE col_int > 70 format Null" --format Null
+
+${CLICKHOUSE_CLIENT} -nq "
+  SYSTEM FLUSH LOGS query_log;
+
+  SELECT ProfileEvents['ParquetReadRowGroups'], ProfileEvents['ParquetPrunedRowGroups']
+    FROM system.query_log
+   WHERE event_date >= yesterday() AND query_id = '$query_id' AND type = 'QueryFinish'
+"
+
