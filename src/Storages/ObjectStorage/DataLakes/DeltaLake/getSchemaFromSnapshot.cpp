9fe9542552bd5067968ba9ab3533cafe7908bb86
robot-clickhouse <robot-clickhouse@users.noreply.github.com>
2025-04-15 14:11:41 +0000

    Backport #78255 to 25.2: delta-kernel: fix builder auth options

--- src/Storages/ObjectStorage/DataLakes/DeltaLake/getSchemaFromSnapshot.cpp
+++ src/Storages/ObjectStorage/DataLakes/DeltaLake/getSchemaFromSnapshot.cpp
@@ -56,6 +56,7 @@ namespace
             case TypeIndex::Date: [[fallthrough]];
             case TypeIndex::Date32: [[fallthrough]];
             case TypeIndex::DateTime: [[fallthrough]];
+            case TypeIndex::DateTime64: [[fallthrough]];
             case TypeIndex::UUID: [[fallthrough]];
             case TypeIndex::String:
                 return true;
@@ -440,7 +441,7 @@ DB::DataTypes SchemaVisitorData::getDataTypesFromTypeList(size_t list_idx)
             {
                 throw DB::Exception(
                     DB::ErrorCodes::NOT_IMPLEMENTED,
-                    "Unsupported complex data type: {}", field.child_list_id);
+                    "Column {} has unsupported complex data type: {}", field.name, field.type);
             }
         }
     }
