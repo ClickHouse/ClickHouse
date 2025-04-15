64cb9cae9e7154cef312549bc05a10bb91f80315
Andrey Zvonov <azvonov@altinity.com>
2025-04-15 21:51:01 +0000

    stabilize test (2)

--- tests/queries/0_stateless/03274_handle_is_deleted_with_prewhere_opt.sql
+++ tests/queries/0_stateless/03274_handle_is_deleted_with_prewhere_opt.sql
@@ -1,4 +1,9 @@
+-- Tags: long, no-parallel
+-- no-parallel: working with system.query_log
+
 drop table if exists 03274_prewhere_is_deleted;
+
+system stop merges;
 
 create table 03274_prewhere_is_deleted (number UInt64, value String, version UInt64, is_deleted UInt8) engine=ReplacingMergeTree(version,is_deleted) ORDER BY number AS SELECT number, toString(number), 1, 1 FROM numbers(10000000);
 
@@ -19,7 +24,7 @@ select count() from 03274_prewhere_is_deleted final prewhere number > 100 SETTIN
 system flush logs;
 
 -- should be zero in case the optimization worked
-SELECT sum(ProfileEvents['ReplacingSortedMilliseconds']) FROM system.query_log WHERE current_database = currentDatabase() AND log_comment = '3274_final_query';
+SELECT sum(ProfileEvents['ReplacingSortedMilliseconds']) FROM system.query_log WHERE current_database = currentDatabase() AND type = 2 AND log_comment = '3274_final_query';
 
 truncate table system.query_log;
 
