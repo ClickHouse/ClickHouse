a4ae926efc319fa8da1fa93784cd1e633a434f2f
flynn <fenglv15@mails.ucas.ac.cn>
2025-04-15 06:25:21 +0000

    Fix test

--- tests/queries/0_stateless/03408_parquet_row_group_profile_events.sh
+++ tests/queries/0_stateless/03408_parquet_row_group_profile_events.sh
@@ -8,13 +8,13 @@ CUR_DIR=$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)
 query_id="${CLICKHOUSE_DATABASE}_parquet_group_group_prune_profile_$RANDOM"
 
 # A parquet file with 10 row groups from 1 to 100, 10 rows per row group.
-${CLICKHOUSE_CLIENT} "${opts[@]}" --query_id="${query_id}" --query="SELECT * FROM file('$CUR_DIR/data_parquet/row_group.parquet.') WHERE col_int > 70 format Null" --format Null
+${CLICKHOUSE_CLIENT} "${opts[@]}" --query_id="${query_id}" --query="SELECT * FROM file('$CUR_DIR/data_parquet/row_group.parquet') WHERE col_int > 70 format Null" --format Null
 
 ${CLICKHOUSE_CLIENT} -nq "
   SYSTEM FLUSH LOGS query_log;
 
   SELECT ProfileEvents['ParquetReadRowGroups'], ProfileEvents['ParquetPrunedRowGroups']
     FROM system.query_log
-   WHERE event_date >= yesterday() AND query_id = '$query_id' AND type = 'QueryFinish'
+	WHERE event_date >= yesterday() AND query_id = '$query_id' AND type = 'QueryFinish' and current_database = currentDatabase();
 "
 
