c54a0432ce8c4d1424d99b3170155b421b172991
Michael Kolupaev <michael.kolupaev@clickhouse.com>
2025-04-17 21:32:40 +0000

    Don't re-check existence of default database

--- src/Storages/MaterializedView/RefreshTask.cpp
+++ src/Storages/MaterializedView/RefreshTask.cpp
@@ -568,6 +568,12 @@ std::optional<UUID> RefreshTask::executeRefreshUnlocked(bool append, int32_t roo
     std::optional<StorageID> table_to_drop;
     auto new_table_id = StorageID::createEmpty();
 
+    std::optional<QueryLogElement> query_log_elem;
+    std::shared_ptr<ASTInsertQuery> refresh_query;
+    String query_for_logging;
+    UInt64 normalized_query_hash = 0;
+    std::shared_ptr<OpenTelemetry::SpanHolder> query_span = std::make_shared<OpenTelemetry::SpanHolder>("query");
+
     try
     {
         refresh_context = view->createRefreshContext(log_comment);
@@ -580,11 +586,6 @@ std::optional<UUID> RefreshTask::executeRefreshUnlocked(bool append, int32_t roo
                 refresh_context->setDDLAdditionalChecksOnEnqueue({zkutil::makeCheckRequest(coordination.path, root_znode_version)});
         }
 
-        std::optional<QueryLogElement> query_log_elem;
-        std::shared_ptr<ASTInsertQuery> refresh_query;
-        String query_for_logging;
-        UInt64 normalized_query_hash = 0;
-        std::shared_ptr<OpenTelemetry::SpanHolder> query_span = std::make_shared<OpenTelemetry::SpanHolder>("query");
         ProcessList::EntryPtr process_list_entry;
 
         {
