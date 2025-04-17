63acd54acdef9d66c4e8fa9e7b29d1481f3fb4b6
Alexey Milovidov <milovidov@clickhouse.com>
2025-04-17 20:37:43 +0200

    Update 03412_merge_final_prewhere.sql

--- tests/queries/0_stateless/03412_merge_final_prewhere.sql
+++ tests/queries/0_stateless/03412_merge_final_prewhere.sql
@@ -1,10 +1,21 @@
-CREATE TABLE tdata0 (k Int64, dt DateTime, s String)
-ENGINE=ReplacingMergeTree
-order by (k,dt);
+DROP TABLE IF EXISTS t;
+DROP TABLE IF EXISTS tmerge;
 
-create table tmerge as tdata0 Engine=Merge('default', 'tdata');
+CREATE TABLE t
+(
+    k Int64,
+    dt DateTime,
+    s String
+)
+ENGINE = ReplacingMergeTree
+ORDER BY (k, dt);
 
-INSERT INTO tdata0
-select number, '2020-01-01 00:00:00', '' from numbers(10);
+CREATE TABLE tmerge AS t ENGINE = Merge(currentDatabase(), '^t$');
 
-select count() from tmerge final prewhere dt >= '2020-01-01 00:00:00';
+INSERT INTO t
+SELECT number, '2020-01-01 00:00:00', '' FROM numbers(10);
+
+SELECT count() FROM tmerge FINAL PREWHERE dt >= '2020-01-01 00:00:00';
+
+DROP TABLE tmerge;
+DROP TABLE t;
