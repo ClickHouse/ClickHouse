54d4b9b115ef65a2b431ee9b6b83d828deb522ff
Nikita Taranov <nikita.taranov@clickhouse.com>
2025-04-17 12:22:00 +0100

    fix test

--- tests/queries/0_stateless/03403_parallel_blocks_marshalling_for_distributed.sql
+++ tests/queries/0_stateless/03403_parallel_blocks_marshalling_for_distributed.sql
@@ -6,7 +6,7 @@ INSERT INTO t SELECT
 FROM numbers_mt(1000000);
 
 SET enable_parallel_replicas = 1, parallel_replicas_local_plan = 1, max_parallel_replicas = 3, cluster_for_parallel_replicas = 'test_cluster_one_shard_three_replicas_localhost', parallel_replicas_for_non_replicated_merge_tree = 1;
-SET prefer_localhost_replica = 1;
+SET prefer_localhost_replica = 1, enable_analyzer = 1;
 
 SELECT replaceRegexpAll(explain, 'ReadFromRemoteParallelReplicas.*', 'ReadFromRemoteParallelReplicas')
 FROM (
