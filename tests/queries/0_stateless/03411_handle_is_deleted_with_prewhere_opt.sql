8d52dcd52e22a059af007e6c89d17e73fac077a0
Andrey Zvonov <azvonov@altinity.com>
2025-04-17 12:14:29 +0000

    make test dumber

--- tests/queries/0_stateless/03411_handle_is_deleted_with_prewhere_opt.sql
+++ tests/queries/0_stateless/03411_handle_is_deleted_with_prewhere_opt.sql
@@ -1,6 +1,8 @@
 -- Tags: long, no-parallel
 -- no-parallel: working with system.query_log
 
+-- Test for https://github.com/ClickHouse/ClickHouse/pull/76978
+
 drop table if exists 03274_prewhere_is_deleted;
 
 set optimize_on_insert=0;
@@ -16,8 +18,6 @@ optimize table 03274_prewhere_is_deleted final;
 
 set do_not_merge_across_partitions_select_final=1;
 
-truncate table system.query_log;
-
 select count() from 03274_prewhere_is_deleted final SETTINGS log_comment='3274_final_query';
 
 -- check that it works for queries that already have an explicit WHERE
@@ -25,12 +25,5 @@ select count() from 03274_prewhere_is_deleted final where number > 100 SETTINGS 
 
 -- check that it works for queries that already have an explicit PREWHERE
 select count() from 03274_prewhere_is_deleted final prewhere number > 100 SETTINGS log_comment='3274_final_query';
-
-system flush logs;
-
--- should be zero in case the optimization worked
-SELECT sum(ProfileEvents['ReplacingSortedMilliseconds']) FROM system.query_log WHERE current_database = currentDatabase() AND type = 2 AND log_comment = '3274_final_query';
-
-truncate table system.query_log;
 
 drop table if exists 03274_prewhere_is_deleted;
