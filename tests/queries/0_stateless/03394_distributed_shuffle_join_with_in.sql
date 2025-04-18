681384c8cecf6c1b1fac03a573d2d1f873ce2f9e
Alexander Gololobov <davenger@clickhouse.com>
2025-04-18 10:46:39 +0200

    Test for IN with tuple

---
+++ tests/queries/0_stateless/03394_distributed_shuffle_join_with_in.sql
@@ -0,0 +1,35 @@
+CREATE TABLE test(path String, lang String, hits UInt64) ENGINE MergeTree() ORDER BY tuple();
+
+INSERT INTO test SELECT 'path_' || number::String, 'en', number FROM numbers(5);
+INSERT INTO test SELECT 'path_' || (number%3)::String, 'de', number%4 FROM numbers(10);
+
+INSERT INTO test SELECT 'path_' || number::String, 'en', number FROM numbers(5);
+INSERT INTO test SELECT 'path_' || (number%3)::String, 'de', number%4 FROM numbers(10);
+
+
+SET
+    make_distributed_plan = 1,
+    enable_parallel_replicas = 0,
+    default_shuffle_join_bucket_count=3,
+    default_reader_bucket_count=3,
+    force_exchange_kind='Streaming';
+
+SELECT '----------';
+
+-- Query with col IN (val1, val2, ...)
+-- It passes the set corresponding to IN conditions as ColumnSet
+EXPLAIN SELECT *
+FROM
+   (SELECT path, sum(hits) as hits FROM test WHERE lang IN ('en', 'de') GROUP BY path) AS en,
+   (SELECT path, sum(hits) as hits FROM test WHERE lang = 'de' GROUP BY path) AS de
+WHERE (en.path = de.path)
+ORDER BY ALL;
+
+SELECT '----------';
+
+SELECT *
+FROM
+   (SELECT path, sum(hits) as hits FROM test WHERE lang IN ('en', 'de') GROUP BY path) AS en,
+   (SELECT path, sum(hits) as hits FROM test WHERE lang = 'de' GROUP BY path) AS de
+WHERE (en.path = de.path)
+ORDER BY ALL;
