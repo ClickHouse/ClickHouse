91004d2811ef2101323d2ea88dea329528e44819
Igor Nikonov <igor@clickhouse.com>
2025-04-18 19:45:31 +0200

    Split 03394_pr_insert_select once more (select ... except can be long)

--- tests/queries/0_stateless/03394_pr_insert_select_local_pipeline.sql
+++ tests/queries/0_stateless/03394_pr_insert_select_local_pipeline.sql
@@ -12,11 +12,12 @@ select 'mt source table count()', count() from t_mt_source;
 
 SET enable_parallel_replicas = 1, cluster_for_parallel_replicas = 'test_cluster_one_shard_three_replicas_localhost', max_parallel_replicas = 3;
 
-select '-- check result without local pipeline';
-INSERT INTO t_mt_target SELECT * FROM t_mt_source SETTINGS log_comment='cb01f13a-410c-4985-b233-35289776b58f', parallel_replicas_for_non_replicated_merge_tree = 1, parallel_replicas_local_plan=0;
+select '-- check result with local pipeline';
+TRUNCATE TABLE t_mt_target;
+INSERT INTO t_mt_target SELECT * FROM t_mt_source SETTINGS log_comment='c1fcb43d-1703-4ddb-b353-c8079b405c16', parallel_replicas_for_non_replicated_merge_tree = 1, parallel_replicas_local_plan=1, parallel_replicas_insert_select_local_pipeline=1;
 
 SYSTEM FLUSH LOGS query_log;
-select count() from system.query_log where (current_database = currentDatabase() or has(databases, currentDatabase())) and type = 'QueryFinish' and query_kind = 'Insert' and log_comment='cb01f13a-410c-4985-b233-35289776b58f' and event_date >= yesterday();
+select count() from system.query_log where (current_database = currentDatabase() or has(databases, currentDatabase())) and type = 'QueryFinish' and query_kind = 'Insert' and log_comment='c1fcb43d-1703-4ddb-b353-c8079b405c16' and event_date >= yesterday();
 
 select count() from t_mt_target;
 select * from t_mt_target order by k
