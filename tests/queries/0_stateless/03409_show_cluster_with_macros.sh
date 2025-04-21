ee9842a23e7f31e5841971979fcbc7df36f20578
Tuan Pham Anh <tuan.pham.anh@clickhouse.com>
2025-04-21 23:53:04 +0000

    Make test 03409_show_cluster_with_macros predictable

--- tests/queries/0_stateless/03409_show_cluster_with_macros.sh
+++ tests/queries/0_stateless/03409_show_cluster_with_macros.sh
@@ -6,8 +6,8 @@ CUR_DIR=$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)
 
 echo "Output of: SHOW CLUSTER '{default_cluster_macro}'"
 
-$CLICKHOUSE_CLIENT -q "show cluster '{default_cluster_macro}'"
+$CLICKHOUSE_CLIENT -q "show cluster '{default_cluster_macro}'" | cut -f1-4,6
 
 echo "to match Output of: SHOW CLUSTER 'test_shard_localhost'"
 
-$CLICKHOUSE_CLIENT -q "show cluster 'test_shard_localhost'"
+$CLICKHOUSE_CLIENT -q "show cluster 'test_shard_localhost'" | cut -f1-4,6
