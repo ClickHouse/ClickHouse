22d815a3a09c276428c068f63bb9cfe2de99bae4
nihalzp <81457724+nihalzp@users.noreply.github.com>
2025-04-23 01:54:05 +0800

    Skip fasttest for naive bayes negative test

--- tests/queries/0_stateless/03394_naive_bayes_negative.sql
+++ tests/queries/0_stateless/03394_naive_bayes_negative.sql
@@ -1,3 +1,6 @@
+-- Tags: no-fasttest
+-- no-fasttest: depends on model binary and model details via config files
+
 SELECT naiveBayesClassifier('sentiment', 3); -- { serverError BAD_ARGUMENTS }
 
 SELECT naiveBayesClassifier(0, 'hello'); -- { serverError BAD_ARGUMENTS }
