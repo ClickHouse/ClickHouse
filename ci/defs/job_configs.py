21d7a26b837a17ac2ecd74dc8b955acebcf11ce3
Shaun Struwig <41984034+Blargian@users.noreply.github.com>
2025-04-22 19:59:08 +0200

    Update docs check to build sourcecode documentation from branch

--- ci/defs/job_configs.py
+++ ci/defs/job_configs.py
@@ -896,7 +896,7 @@ class JobConfigs:
             ],
         ),
         run_in_docker="clickhouse/docs-builder",
-        requires=[JobNames.STYLE_CHECK],
+        requires=[JobNames.STYLE_CHECK, ArtifactNames.CH_ARM_BIN],
     )
     docker_sever = Job.Config(
         name=JobNames.DOCKER_SERVER,
