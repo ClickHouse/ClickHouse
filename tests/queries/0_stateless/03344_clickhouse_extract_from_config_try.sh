8c579da8e0a0bb8a3faef02d68ad0d0b2d1addd4
samay-sharma <samay.sharma@clickhouse.com>
2025-04-23 13:54:25 -0700

    Make test 03344_clickhouse_extract_from_config_try platform independent
    Running this test on my machine failed due to usage of |& syntax.
    The error was line 12: syntax error near unexpected token `&'. Switched
    it to 2>&1 | as that seems to be more widely supported. Changing
    this made the test succeed for me locally.

--- tests/queries/0_stateless/03344_clickhouse_extract_from_config_try.sh
+++ tests/queries/0_stateless/03344_clickhouse_extract_from_config_try.sh
@@ -9,12 +9,12 @@ CUR_DIR=$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)
 prefix=$CUR_DIR/"$(basename "${BASH_SOURCE[0]}" .sh)"
 
 $CLICKHOUSE_BINARY extract-from-config --key merge_tree.comment --config $prefix.xml
-$CLICKHOUSE_BINARY extract-from-config --key merge_tree.anything_xml --config $prefix.xml |& grep -o 'Not found: merge_tree.anything_xml'
+$CLICKHOUSE_BINARY extract-from-config --key merge_tree.anything_xml --config $prefix.xml 2>&1 | grep -o 'Not found: merge_tree.anything_xml'
 $CLICKHOUSE_BINARY extract-from-config --key merge_tree.anything_xml --config $prefix.xml --try
 
 $CLICKHOUSE_BINARY extract-from-config --key merge_tree.comment --config $prefix.yaml
-$CLICKHOUSE_BINARY extract-from-config --key merge_tree.anything_yaml --config $prefix.yaml |& grep -o 'Not found: merge_tree.anything_yaml'
+$CLICKHOUSE_BINARY extract-from-config --key merge_tree.anything_yaml --config $prefix.yaml 2>&1 | grep -o 'Not found: merge_tree.anything_yaml'
 $CLICKHOUSE_BINARY extract-from-config --key merge_tree.anything_yaml --config $prefix.yaml --try
 
-$CLICKHOUSE_BINARY extract-from-config --key include_from --config ${prefix}_bad_include_from.xml |& grep -o 'File not found: I hope such path will never exists, since it does not even contain a single slash'
+$CLICKHOUSE_BINARY extract-from-config --key include_from --config ${prefix}_bad_include_from.xml 2>&1 | grep -o 'File not found: I hope such path will never exists, since it does not even contain a single slash'
 $CLICKHOUSE_BINARY extract-from-config --key include_from --config ${prefix}_bad_include_from.xml --try
