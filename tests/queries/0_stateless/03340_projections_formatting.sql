a280fe3ff76c36960206de91a4561a19d31f9adc
Azat Khuzhin <a3at.mail@gmail.com>
2025-04-23 15:02:01 +0200

    Remove trailing whitespaces during formating SELECT query in PROJECTION
    Note, that should not affect the metadata in ReplicatedMergeTree, since
    it uses oneline formatting (which is not affected by this patch):
    
        (CONNECTED [127.1:9181]) /> get /tables/test/replicas/r1/metadata
        metadata format version: 1
        date column:
        sampling expression:
        index granularity: 8192
        mode: 0
        sign column:
        primary key: user_id
        data format version: 1
        partition key:
        projections: order_by_item_id (SELECT _part_offset, user_id ORDER BY item_id)
        granularity bytes: 10485760
        merge parameters format version: 2

---
+++ tests/queries/0_stateless/03340_projections_formatting.sql
@@ -0,0 +1,86 @@
+CREATE OR REPLACE TABLE test
+(
+    `user_id` UInt64,
+    `item_id` UInt64,
+    PROJECTION order_by_item_id
+    (
+        WITH toString(user_id) as user
+        SELECT user
+        ORDER BY user_id
+    )
+)
+ENGINE = MergeTree ORDER BY ();
+SHOW CREATE test FORMAT LineAsString;
+
+CREATE OR REPLACE TABLE test
+(
+    `user_id` UInt64,
+    `item_id` UInt64,
+    PROJECTION order_by_item_id
+    (
+        WITH toString(user_id) as user, toString(item_id) as item
+        SELECT user, item
+        ORDER BY user_id, item_id
+    )
+)
+ENGINE = MergeTree ORDER BY ();
+SHOW CREATE test FORMAT LineAsString;
+
+CREATE OR REPLACE TABLE test
+(
+    `user_id` UInt64,
+    `item_id` UInt64,
+    PROJECTION order_by_item_id
+    (
+        SELECT _part_offset ORDER BY item_id
+    )
+)
+ENGINE = MergeTree ORDER BY ();
+SHOW CREATE test FORMAT LineAsString;
+
+CREATE OR REPLACE TABLE test
+(
+    `user_id` UInt64,
+    `item_id` UInt64,
+    PROJECTION order_by_item_id
+    (
+        SELECT _part_offset, user_id ORDER BY item_id
+    )
+)
+ENGINE = MergeTree ORDER BY ();
+SHOW CREATE test FORMAT LineAsString;
+
+CREATE OR REPLACE TABLE test
+(
+    `user_id` UInt64,
+    `item_id` UInt64,
+    PROJECTION order_by_item_id
+    (
+        SELECT _part_offset ORDER BY user_id, item_id
+    )
+)
+ENGINE = MergeTree ORDER BY ();
+SHOW CREATE test FORMAT LineAsString;
+
+CREATE OR REPLACE TABLE test
+(
+    `user_id` UInt64,
+    `item_id` UInt64,
+    PROJECTION order_by_item_id
+    (
+        SELECT user_id GROUP BY user_id
+    )
+)
+ENGINE = MergeTree ORDER BY ();
+SHOW CREATE test FORMAT LineAsString;
+CREATE OR REPLACE TABLE test
+(
+    `user_id` UInt64,
+    `item_id` UInt64,
+    PROJECTION order_by_item_id
+    (
+        SELECT user_id, item_id GROUP BY user_id, item_id
+    )
+)
+ENGINE = MergeTree ORDER BY ();
+SHOW CREATE test FORMAT LineAsString;
