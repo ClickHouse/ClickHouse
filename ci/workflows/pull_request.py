3c9608e2927caa93f0c59ecd8f0cc956257d1a9c
Max Kainov <max.kainov@clickhouse.com>
2025-04-23 16:56:10 +0000

    restore default workflows

--- ci/workflows/pull_request.py
+++ ci/workflows/pull_request.py
@@ -55,7 +55,6 @@ workflow = Workflow.Config(
         *JobConfigs.ast_fuzzer_jobs,
         *JobConfigs.buzz_fuzzer_jobs,
         *JobConfigs.performance_comparison_with_master_head_jobs,
-        *JobConfigs.clickbench_master_jobs,
     ],
     artifacts=[
         *ArtifactConfigs.unittests_binaries,
