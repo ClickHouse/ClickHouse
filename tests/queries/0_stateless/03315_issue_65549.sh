3148c7a7645a82dc323fbdee148ce3ebc553add8
Engel Danila <qwrnsqlx@yandex.ru>
2025-04-24 08:41:23 +0300

    Changed test to initial in issue

--- tests/queries/0_stateless/03315_issue_65549.sh
+++ tests/queries/0_stateless/03315_issue_65549.sh
@@ -4,7 +4,7 @@ CUR_DIR=$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)
 # shellcheck source=../shell_config.sh
 . "$CUR_DIR"/../shell_config.sh
 
-QUERY="SELECT * FROM s3('http://localhost:9000/test/hello.csv', 'secret', 'secret', 'CSV', 'value String');"
+QUERY="SELECT * FROM s3('s3://test/hello.csv', 'ASIAIOSFODNN7EXAMPLE', 'wJalrXUtnFEMI/K7MDENG/bPxRfiCYEXAMPLEKEY');"
 
 ${CLICKHOUSE_CLIENT} --query "${QUERY}" &
 pid=$!
@@ -14,7 +14,7 @@ sleep 1
 ps -p "$pid" -o args=
 
 
-QUERY="SELECT * FROM s3('http://localhost:9000/test/hello.csv', 'secret', 'secret', 'CSV', 'value String');"
+QUERY="SELECT * FROM s3('s3://test/hello.csv', 'ASIAIOSFODNN7EXAMPLE', 'wJalrXUtnFEMI/K7MDENG/bPxRfiCYEXAMPLEKEY');"
 
 ${CLICKHOUSE_CLIENT} --query="${QUERY}" &
 pid=$!
