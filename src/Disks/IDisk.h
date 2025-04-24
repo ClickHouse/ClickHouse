c9e46d5f50a4247f6bb6e3338bb281614f74e4e4
Julia Kartseva <yulia.kartseva@gmail.com>
2025-04-24 06:24:05 +0000

    add method

--- src/Disks/IDisk.h
+++ src/Disks/IDisk.h
@@ -442,6 +442,8 @@ public:
 
     virtual bool isReadOnly() const { return false; }
 
+    virtual bool isPlain() const { return false; }
+
     virtual bool isWriteOnce() const { return false; }
 
     virtual bool supportsHardLinks() const { return true; }
