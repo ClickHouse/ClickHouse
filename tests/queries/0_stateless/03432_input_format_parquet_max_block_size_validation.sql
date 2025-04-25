3c55294de4b32c7d1770bf9dc13275c7535f99e6
Aleksei Bashkeev <abash@clickhouse.com>
2025-04-25 19:15:54 +0100

    Fix #79394: non-zero parameter fix, tests added

---
+++ tests/queries/0_stateless/03432_input_format_parquet_max_block_size_validation.sql
@@ -0,0 +1,12 @@
+-- Test that setting input_format_parquet_max_block_size to 0 is not allowed
+SET input_format_parquet_max_block_size = 0; -- { serverError CANNOT_SET_NON_ZERO_VALUE_TO_ZERO }
+
+-- Test that negative values are not allowed
+SET input_format_parquet_max_block_size = -1; -- { serverError CANNOT_CONVERT_TYPE }
+
+-- Test that valid positive values are allowed
+SET input_format_parquet_max_block_size = 8192;
+SELECT name, value FROM system.settings WHERE name = 'input_format_parquet_max_block_size' FORMAT TSV;
+
+-- Test that the setting works with queries
+SELECT 1 AS x FORMAT Parquet SETTINGS input_format_parquet_max_block_size = 16384;
