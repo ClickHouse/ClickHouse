649d8e89529f117b06832bd82c94008625c05846
Amos Bird <amosbird@gmail.com>
2025-04-25 20:43:56 +0800

    Fix test

--- tests/queries/0_stateless/03443_part_starting_offset.sql
+++ tests/queries/0_stateless/03443_part_starting_offset.sql
@@ -17,11 +17,11 @@ select _part, _part_starting_offset, _part_offset from test order by _part;
 select _part, _part_starting_offset, _part_offset from test where j = 8 order by _part;
 
 -- make sure key analysis works correctly
-select *, _part_starting_offset + _part_offset from test where _part_starting_offset + _part_offset = 8 settings max_rows_to_read = 1;
-select *, _part_offset + _part_starting_offset from test where _part_offset + _part_starting_offset = 8 settings max_rows_to_read = 1;
+select *, _part_starting_offset + _part_offset from test where _part_starting_offset + _part_offset = 8 settings parallel_replicas_local_plan = 0, max_rows_to_read = 1;
+select *, _part_offset + _part_starting_offset from test where _part_offset + _part_starting_offset = 8 settings parallel_replicas_local_plan = 0, max_rows_to_read = 1;
 
 -- from fuzzer
-select * from test prewhere 8 = (_part_offset + _part_starting_offset) where 8 = (_part_offset + _part_starting_offset) settings max_rows_to_read = 1;
+select * from test prewhere 8 = (_part_offset + _part_starting_offset) where 8 = (_part_offset + _part_starting_offset) settings parallel_replicas_local_plan = 0, max_rows_to_read = 1;
 select * from test prewhere (8 = (_part_starting_offset * _part_offset)) AND 3 WHERE 8 = (_part_starting_offset + _part_offset);
 
 drop table test;
