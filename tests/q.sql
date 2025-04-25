31c02764ec3bbb9d0a6cf97e835e770b6e48cbd6
Pavel Kruglov <48961922+Avogar@users.noreply.github.com>
2025-04-25 14:25:32 +0200

    Delete tests/q.sql

--- tests/q.sql
+++
@@ -1,5 +0,0 @@
-set max_threads=1;
-CREATE OR REPLACE TABLE t0 (c0 Variant(String, Int)) ENGINE = MergeTree() PRIMARY KEY tuple() SETTINGS use_compact_variant_discriminators_serialization = 0, index_granularity=1;
-INSERT INTO TABLE t0 (c0) VALUES (42), ('a');
-optimize table t0 final;
-SELECT c0 FROM t0;
