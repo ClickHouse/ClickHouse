57c4ff442f51ec169d2d17c7cfbf737843b65205
Aleksei Bashkeev <abash@clickhouse.com>
2025-04-25 20:02:59 +0100

    Fix #79394: non-zero parameter fix, error code fixed

--- tests/queries/0_stateless/03432_input_format_parquet_max_block_size_validation.sql
+++ tests/queries/0_stateless/03432_input_format_parquet_max_block_size_validation.sql
@@ -1,5 +1,5 @@
 -- Test that setting input_format_parquet_max_block_size to 0 is not allowed
-SET input_format_parquet_max_block_size = 0; -- { serverError CANNOT_SET_NON_ZERO_VALUE_TO_ZERO }
+SET input_format_parquet_max_block_size = 0; -- { serverError BAD_ARGUMENTS }
 
 -- Test that negative values are not allowed
 SET input_format_parquet_max_block_size = -1; -- { serverError CANNOT_CONVERT_TYPE }
