c459128b31b9546121de78fa64ebd2a24862733a
Onkar Deshpande <onkar.deshpande@sentry.io>
2025-04-27 22:57:50 +0000

    Support DESCRIBE SELECT in addition to DESCRIBE (SELECT ...)

--- src/Parsers/ParserDescribeTableQuery.cpp
+++ src/Parsers/ParserDescribeTableQuery.cpp
@@ -1,5 +1,6 @@
 #include <Parsers/ParserSelectWithUnionQuery.h>
 #include <Parsers/TablePropertiesQueriesASTs.h>
+#include <Parsers/ASTTablesInSelectQuery.h>
 
 #include <Parsers/CommonParsers.h>
 #include <Parsers/ParserDescribeTableQuery.h>
@@ -20,23 +21,23 @@ bool ParserDescribeTableQuery::parseImpl(Pos & pos, ASTPtr & node, Expected & ex
     ParserKeyword s_settings(Keyword::SETTINGS);
     ParserSetQuery parser_settings(true);
 
-    ASTPtr database;
-    ASTPtr table;
+    ASTPtr select;
 
     if (!s_describe.ignore(pos, expected) && !s_desc.ignore(pos, expected))
         return false;
 
     auto query = std::make_shared<ASTDescribeQuery>();
-
     s_table.ignore(pos, expected);
 
-    if (ParserSelectWithUnionQuery().parse(pos, query->table_expression, expected))
+    if (ParserSelectWithUnionQuery().parse(pos, select, expected))
     {
-        node = query;
-        return true;
+        auto table_expr = std::make_shared<ASTTableExpression>();
+        table_expr->subquery = select;
+        table_expr->children.push_back(select);
+        query->table_expression = table_expr;
+        query->children.push_back(query->table_expression);
     }
-
-    if (!ParserTableExpression().parse(pos, query->table_expression, expected))
+    else if (!ParserTableExpression().parse(pos, query->table_expression, expected))
         return false;
 
     /// For compatibility with SELECTs, where SETTINGS can be in front of FORMAT
