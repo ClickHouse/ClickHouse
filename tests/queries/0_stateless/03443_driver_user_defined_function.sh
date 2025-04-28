d1213c92abf5080f7b927de156933a93aa80e40b
Daniil Timizhev <ditimizhev@edu.hse.ru>
2025-04-28 22:21:36 +0000

    Fixes + check unsupported driver

--- tests/queries/0_stateless/03443_driver_user_defined_function.sh
+++ tests/queries/0_stateless/03443_driver_user_defined_function.sh
@@ -37,9 +37,11 @@ $CLICKHOUSE_LOCAL -q 'CREATE OR REPLACE FUNCTION test_arg_driver(Col1 Int32, Col
 $$'
 $CLICKHOUSE_LOCAL -q 'SELECT test_arg_driver(4, 5)'
 
-$CLICKHOUSE_LOCAL -q 'CREATE TABLE test_driver_table(Col1 Int32 PRIMARY KEY, Col2 Int32)'
+$CLICKHOUSE_LOCAL -q 'CREATE TABLE IF NOT EXISTS test_driver_table(Col1 Int32 PRIMARY KEY, Col2 Int32)'
 $CLICKHOUSE_LOCAL -q 'INSERT INTO test_driver_table (Col1, Col2) VALUES (1, 2), (3, 5), (-4, 0), (5, -8)'
 $CLICKHOUSE_LOCAL -q 'SELECT test_arg_driver(Col1, Col2) FROM test_driver_table'
 
-$CLICKHOUSE_LOCAL -q 'DROP FUNCTION test_arg_driver'
+$CLICKHOUSE_LOCAL -q 'DROP FUNCTION IF EXISTS test_arg_driver'
 $CLICKHOUSE_LOCAL -q 'DROP FUNCTION test_arg_driver -- { serverError UNKNOWN_FUNCTION }'
+
+$CLICKHOUSE_LOCAL -q 'CREATE FUNCTION IF NOT EXISTS test_driver RETURNS Int32 ENGINE = Nothing AS $$echo 1$$ -- { serverError UNSUPPORTED_DRIVER }'
