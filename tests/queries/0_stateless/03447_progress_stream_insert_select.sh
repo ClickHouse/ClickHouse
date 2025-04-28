deae3dbdb3664a09a72b086f5cc915e81b6bed00
ilyaleshchik <ilyuleshchik@gmail.com>
2025-04-28 20:55:00 +0000

    fix teset query

--- tests/queries/0_stateless/03447_progress_stream_insert_select.sh
+++ tests/queries/0_stateless/03447_progress_stream_insert_select.sh
@@ -4,4 +4,4 @@ CUR_DIR=$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)
 # shellcheck source=../shell_config.sh
 . "$CUR_DIR"/../shell_config.sh
 
-$CLICKHOUSE_CURL -sS -v -H "Accept: text/event-stream" "${CLICKHOUSE_URL}&send_progress_in_http_headers=1&http_headers_progress_interval_ms=0" -d "insert into function null('_ Int') select * from numbers(5) settings max_block_size=1, max_insert_threads=1" 2>/dev/null | head -n100 | sed -r -e 's/,?"elapsed_ns":"[0-9]+"//'
+$CLICKHOUSE_CURL -sS -v -H "Accept: text/event-stream" "${CLICKHOUSE_URL}&send_progress_in_http_headers=1&http_headers_progress_interval_ms=0" -d "insert into function null('_ Int') select * from numbers(5) settings max_block_size=1, max_insert_threads=1" 2>/dev/null | head -n29 | sed -r -e 's/,?"elapsed_ns":"[0-9]+"//'
