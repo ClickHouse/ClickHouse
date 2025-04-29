476326a6452e149089e4630c344d61a87d32cd36
cjw <cjwcommuny@outlook.com>
2025-04-29 15:53:58 +0800

    test: remove database

--- tests/queries/0_stateless/03441_max_estimated_rows_to_read.sql
+++ tests/queries/0_stateless/03441_max_estimated_rows_to_read.sql
@@ -1,8 +1,3 @@
--- Database
-DROP DATABASE IF EXISTS max_estimated_rows_to_read_example;
-CREATE DATABASE max_estimated_rows_to_read_example;
-USE max_estimated_rows_to_read_example;
-
 -- Tables
 CREATE TABLE company(
     id UInt64 NOT NULL PRIMARY KEY
