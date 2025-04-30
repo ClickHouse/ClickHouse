f7e374014532b9df41071777a84257eb9d195114
Igor Nikonov <954088+devcrafter@users.noreply.github.com>
2025-04-30 16:10:05 +0200

    Update SettingsChangesHistory.cpp
    Remove outdated comment

--- src/Core/SettingsChangesHistory.cpp
+++ src/Core/SettingsChangesHistory.cpp
@@ -91,7 +91,7 @@ const VersionToSettingsChangesMap & getSettingsChangesHistory()
             {"iceberg_snapshot_id", 0, 0, "New setting."},
             {"use_iceberg_metadata_files_cache", true, true, "New setting"},
             {"query_plan_join_shard_by_pk_ranges", false, false, "New setting"},
-            {"parallel_replicas_insert_select_local_pipeline", false, true, "Use local pipeline during distributed INSERT SELECT with parallel replicas. Currently disabled due to performance issues"},
+            {"parallel_replicas_insert_select_local_pipeline", false, true, "Use local pipeline during distributed INSERT SELECT with parallel replicas"},
             {"parallel_hash_join_threshold", 0, 0, "New setting"},
             /// Release closed. Please use 25.5
         });
