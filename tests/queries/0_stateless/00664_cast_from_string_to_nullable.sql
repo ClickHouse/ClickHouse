ea9cc83dfc5be9f84cb3582c1067a58d89b7107b
jitendra <jackrh2017@gmail.com>
2025-04-30 20:51:01 +0530

    added test cases

--- tests/queries/0_stateless/00664_cast_from_string_to_nullable.sql
+++ tests/queries/0_stateless/00664_cast_from_string_to_nullable.sql
@@ -8,4 +8,6 @@ SELECT toDateOrNull(arrayJoin(['', '2018', '2018-01-02', '2018-1-2', '2018-01-2'
 
 SELECT toDateTimeOrZero(arrayJoin(['', '2018', '2018-01-02 01:02:03', '2018-01-02T01:02:03', '2018-01-02 01:02:03 abc']), 'UTC');
 SELECT toDateTimeOrNull(arrayJoin(['', '2018', '2018-01-02 01:02:03', '2018-01-02T01:02:03', '2018-01-02 01:02:03 abc']));
-
+SELECT toDateTimeOrNull(arrayJoin([0, 1, 2018, 1583851242, 4294967295]));
+SELECT toDateTimeOrNull(arrayJoin([4294967296]));
+SELECT toDateTimeOrNull(arrayJoin([5294967295]));
