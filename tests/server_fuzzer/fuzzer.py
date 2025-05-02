9234d2cdfe0da6a478d35cc725f32c2352228876
Pedro Ferreira <pedro.silva.ferreira@clickhouse.com>
2025-05-02 10:21:39 +0000

    New settings

--- tests/server_fuzzer/fuzzer.py
+++ tests/server_fuzzer/fuzzer.py
@@ -12,23 +12,32 @@ from integration.helpers.cluster import ClickHouseCluster
 from generators import BuzzHouseGenerator
 from properties import modify_server_settings_with_random_properties
 
+def ordered_pair(value):
+    try:
+        x, y = map(int, value.split(','))
+        if x >= y:
+            raise argparse.ArgumentTypeError(f"First value must be less than second (got {x} >= {y})")
+        return (x, y)
+    except ValueError:
+        raise argparse.ArgumentTypeError("Must be two comma-separated integers (e.g., '1,10')")
+
+
 parser = argparse.ArgumentParser()
-parser.add_argument("--server-settings-prob", type = int, default = 80, help = 'Probability to set server properties')
+parser.add_argument("--server-settings-prob", type = int, default = 80, choices=range(0, 100), help = 'Probability to set server properties')
 parser.add_argument("--client-binary", type = pathlib.Path, help = 'Path to client binary')
 parser.add_argument("-c", "--client-config", type = pathlib.Path, help = 'Path to client configuration file')
 parser.add_argument("-g", "--generator", choices =['buzzhouse'], type = str.lower, required = True, help = 'What generator to use')
 #parser.add_argument("--number-instances", type = int, default = 2, help = 'Number of default instances')
-parser.add_argument("-l", "--log-path", type = pathlib.Path, help = 'Log path')
+parser.add_argument("-l", "--log-path", type = pathlib.Path, default=tempfile.NamedTemporaryFile(), help = 'Log path')
 parser.add_argument("--server-config", type = pathlib.Path, help = 'Path to config.xml file')
 parser.add_argument("-s", "--seed", type = int, default = 0, help = 'Server fuzzer seed')
 parser.add_argument("-u", "--user-config", type = pathlib.Path, help = 'Path to users.xml file')
-parser.add_argument("--kill-server-prob", type = int, default = 50, help = 'Probability to kill the server instead of shutting it down')
+parser.add_argument("--kill-server-prob", type = int, default = 50, choices=range(0, 100), help = 'Probability to kill the server instead of shutting it down')
+parser.add_argument('--time-between-shutdowns', type=ordered_pair, default=(20, 30), help="Two ordered integers separated by comma (e.g., 30,60)")
 args = parser.parse_args()
 
-log_path = tempfile.NamedTemporaryFile()
-if args.log is not None:
-    log_path = args.log_path
-logging.basicConfig(filename=args.log_path, level=logging.INFO)
+logging.basicConfig(filename=args.log_path, filemode='w',
+                    level=logging.INFO, format='%(asctime)s - %(name)s - %(levelname)s - %(message)s')
 logger = logging.getLogger(__name__)
 
 # Set seed first
@@ -82,7 +91,8 @@ while True:
         logger.info("The server is not running")
         break
 
-    time.sleep(int(random.uniform(30, 60)))
+    lower_bound, upper_bound = args.time_between_shutdowns
+    time.sleep(int(random.uniform(lower_bound, upper_bound)))
     kill_server = random.randint(1, 100) <= args.kill_server_prob
     logger.info("Restart the server with", "kill" if kill_server else "manual shutdown")
     server.restart_clickhouse(stop_start_wait_sec = 10, kill = kill_server)
