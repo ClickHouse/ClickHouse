155e3524b1118e135c6585a0e5102652bfec0803
Andrey Zvonov <azvonov@altinity.com>
2025-05-02 13:08:10 +0000

    update test

--- tests/queries/0_stateless/03447_grouping_sets_analyzer_const_columns.sql
+++ tests/queries/0_stateless/03447_grouping_sets_analyzer_const_columns.sql
@@ -1,10 +1,12 @@
+SELECT 'Const column in grouping set, analyzer on:';
+
 SELECT grouping(key_a), grouping(key_b), key_a, key_b, count()  FROM (
     SELECT 'value' as key_a, number as key_b FROM numbers(4)
 )
 GROUP BY GROUPING SETS((key_b), (key_a, key_b))
 ORDER BY (grouping(key_a), grouping(key_b), key_a, key_b);
 
-SELECT '=================================================';
+SELECT 'Non-const column in grouping set, analyzer on:';
 
 SELECT grouping(key_a), grouping(key_b), key_a, key_b, count() FROM (
     SELECT materialize('value') as key_a, number as key_b FROM numbers(4)
@@ -12,7 +14,7 @@ SELECT grouping(key_a), grouping(key_b), key_a, key_b, count() FROM (
 GROUP BY GROUPING SETS((key_b), (key_a, key_b))
 ORDER BY (grouping(key_a), grouping(key_b), key_a, key_b);
 
-SELECT '=================================================';
+SELECT 'Const column in grouping set, analyzer off:';
 
 SELECT grouping(key_a), grouping(key_b), key_a, key_b, count() FROM (
     SELECT 'value' as key_a, number as key_b FROM numbers(4)
