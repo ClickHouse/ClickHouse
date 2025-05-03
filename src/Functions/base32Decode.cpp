a15389523cb28d3dd3afc94c737513ef1398ae9e
Joanna Hulboj <joanna.hulboj@gmail.com>
2025-05-03 20:03:41 +0100

    Base32 - add documentation

--- src/Functions/base32Decode.cpp
+++ src/Functions/base32Decode.cpp
@@ -19,6 +19,18 @@ using FunctionBase32Decode = FunctionBaseXXConversion<Base32DecodeImpl>;
 
 REGISTER_FUNCTION(Base32Decode)
 {
-    factory.registerFunction<FunctionBase32Decode>();
+    factory.registerFunction<FunctionBase32Decode>(FunctionDocumentation{
+        .description = R"(
+Decode a [Base32](https://datatracker.ietf.org/doc/html/rfc4648) encoded string. The input string must be a valid Base32 encoded string, otherwise an exception will be thrown.)",
+        .arguments = {
+            {"arg", "A Base32 (rfc4648) encoded string"},
+        },
+        .examples = {
+            {"simple_decoding1", "SELECT base32Decode('ME======')", "a"},
+            {"simple_decoding2", "SELECT base32Decode('JBSWY3DP')", "Hello"},
+            {"empty_string", "SELECT base32Decode('')", ""},
+            {"non_ascii", "SELECT hex(base32Decode('4W2HIXV4'))", "E5B4745EBC"},
+        },
+        .category = FunctionDocumentation::Category::String});
 }
 }
--- src/Functions/base32Encode.cpp
+++ src/Functions/base32Encode.cpp
@@ -6,7 +6,16 @@ namespace DB
 
 REGISTER_FUNCTION(Base32Encode)
 {
-    factory.registerFunction<FunctionBaseXXConversion<BaseXXEncode<Base32Traits>>>();
+    factory.registerFunction<FunctionBaseXXConversion<BaseXXEncode<Base32Traits>>>(FunctionDocumentation{
+        .description = R"(
+Encode a string with [Base32](https://datatracker.ietf.org/doc/html/rfc4648) encoding.)",
+        .arguments = {
+            {"arg", "A string to be encoded"},
+        },
+        .examples = {
+            {"simple_encoding1", "SELECT base32Encode('a')", "ME======"},
+            {"simple_encoding2", "SELECT base32Encode('Hello')", "JBSWY3DP"}
+        },
+        .category = FunctionDocumentation::Category::String});
 }
-
 }
