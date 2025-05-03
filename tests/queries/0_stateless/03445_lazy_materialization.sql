40aa217571c98de33698f2fbb38b322555a4fa7c
Bharat Nallan Chakravarthy <bharat@clickhouse.com>
2025-05-03 10:13:10 -0700

    add a test case with projection

--- tests/queries/0_stateless/03445_lazy_materialization.sql
+++ tests/queries/0_stateless/03445_lazy_materialization.sql
@@ -9,4 +9,11 @@ SELECT count() FROM (SELECT * FROM test_03445_lazy ORDER BY rand() LIMIT 11);
 SELECT count() FROM (SELECT * FROM test_03445_lazy ORDER BY rand() LIMIT 50);
 SELECT count() FROM (SELECT * FROM test_03445_lazy ORDER BY rand());
 
+
+CREATE TABLE test_03445_lazy_projection (x Int32, y Int32, projection p (select x, y order by x)) engine = MergeTree ORDER BY y partition BY intDiv(y, 100);
+
+INSERT INTO test_03445_lazy_projection SELECT number, number FROM numbers(100);
+SELECT count() FROM (SELECT * FROM test_03445_lazy_projection ORDER BY x LIMIT 5);
+
 DROP TABLE test_03445_lazy;
+DROP TABLE test_03445_lazy_projection;
