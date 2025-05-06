f4e65898005218980a2143aa1f77e9cdd6599350
nihalzp <81457724+nihalzp@users.noreply.github.com>
2025-05-06 23:25:38 +0800

    Increase coverage of the stateless test

--- tests/queries/0_stateless/03447_window_functions_distinct.sql
+++ tests/queries/0_stateless/03447_window_functions_distinct.sql
@@ -1,4 +1,27 @@
+SELECT 'Single Numeric Data:';
 SELECT number, avg(DISTINCT number) OVER () FROM numbers(0, 5) ORDER BY number;
-SELECT number, count(DISTINCT number) OVER () FROM numbers(0, 5) ORDER BY number;
-SELECT number, max(DISTINCT number) OVER () FROM numbers(0, 5) ORDER BY number;
-SELECT number, sum(DISTINCT number) OVER () FROM numbers(0, 5) ORDER BY number;
+SELECT number, avg(DISTINCT number) OVER (ORDER BY number) FROM numbers(0, 5) ORDER BY number;
+
+SELECT 'Single Generic Plain Column:';
+SELECT number, s, min(DISTINCT s) OVER(PARTITION BY number % 2) AS min_varlen_string
+FROM
+(
+    SELECT number, toString(number % 5) AS s
+    FROM numbers(11)
+) ORDER BY number;
+
+SELECT 'Single Generic Non-Plain Column:';
+SELECT arr, min(DISTINCT arr) OVER (ORDER BY arr)
+FROM
+(
+    SELECT [toString(number), toString(number+1)] AS arr
+    FROM numbers(10)
+) ORDER BY arr;
+
+SELECT 'Multiple Generic Columns:';
+SELECT s, ts, argMax(DISTINCT ts, s) OVER(PARTITION BY ts % 2) AS value_with_max_ts
+FROM
+(
+    SELECT number AS ts, toString(number % 5) AS s
+    FROM numbers(11)
+) ORDER BY ts;
