2d19fc774f4b0d72b5cb5f73fb5d456eeacfec73
Nikita Mikhailov <nikitamikhaylov@clickhouse.com>
2025-05-08 00:24:07 +0200

    Improve the test

--- tests/queries/0_stateless/03312_issue_63093.sh
+++ tests/queries/0_stateless/03312_issue_63093.sh
@@ -1,4 +1,8 @@
 #!/usr/bin/env bash
 
-../../../build/programs/clickhouse client --param-test "param-test OK" -q "SELECT {test:String}"
-../../../build/programs/clickhouse client --param_test "param_test OK" -q "SELECT {test:String}"
+CUR_DIR=$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)
+# shellcheck source=../shell_config.sh
+. "$CUR_DIR"/../shell_config.sh
+
+${CLICKHOUSE_CLIENT} --param-test "param-test OK" -q "SELECT {test:String}"
+${CLICKHOUSE_CLIENT} --param_test "param_test OK" -q "SELECT {test:String}"
