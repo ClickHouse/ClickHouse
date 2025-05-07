79f448abff10aee6bf934fc30c638aa06ece8a4a
Unamed <dtitmoav@gmail.com>
2025-05-07 19:13:48 +0300

    fix BFloat16 hash

--- base/base/BFloat16.h
+++ base/base/BFloat16.h
@@ -327,13 +327,22 @@ constexpr inline auto operator/(BFloat16 a, T b)
 
 namespace std
 {
-template <>
-class numeric_limits<BFloat16>
-{
-public:
-    static constexpr BFloat16 lowest() noexcept { return BFloat16::fromBits(0b1111111101111111); }
-    static constexpr BFloat16 min() noexcept { return BFloat16::fromBits(0b0000000100000000); }
-    static constexpr BFloat16 max() noexcept { return BFloat16::fromBits(0b0111111101111111); }
-    static constexpr BFloat16 infinity() noexcept { return BFloat16::fromBits(0b0111111110000000); }
-};
+    template <>
+    class numeric_limits<BFloat16>
+    {
+    public:
+        static constexpr BFloat16 lowest() noexcept { return BFloat16::fromBits(0b1111111101111111); }
+        static constexpr BFloat16 min() noexcept { return BFloat16::fromBits(0b0000000100000000); }
+        static constexpr BFloat16 max() noexcept { return BFloat16::fromBits(0b0111111101111111); }
+        static constexpr BFloat16 infinity() noexcept { return BFloat16::fromBits(0b0111111110000000); }
+    };
+    
+    template <>
+    struct hash<BFloat16>
+    {
+        std::size_t operator()(const BFloat16 & x) const noexcept
+        {
+            return std::hash<UInt16>()(x.raw());
+        }
+    };
 }
