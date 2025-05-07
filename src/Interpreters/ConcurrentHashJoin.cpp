11482a35e5d2a9456c082e60978aee8f6128d629
Yarik Briukhovetskyi <114298166+yariks5s@users.noreply.github.com>
2025-05-07 11:59:13 +0000

    minor

--- src/Interpreters/ConcurrentHashJoin.cpp
+++ src/Interpreters/ConcurrentHashJoin.cpp
@@ -82,7 +82,7 @@ void updateStatistics(const auto & hash_joins, const DB::StatsCollectingParams &
     const auto ht_size = hash_joins.at(0)->data->getTotalRowCount();
     if (!std::ranges::all_of(hash_joins, [&](const auto & hash_join) { return hash_join->data->getTotalRowCount() == ht_size; }))
         throw DB::Exception(DB::ErrorCodes::LOGICAL_ERROR, "HashJoin instances have different sizes");
-   
+
     const auto source_rows = std::accumulate(
         hash_joins.begin(),
         hash_joins.end(),
@@ -157,6 +157,7 @@ ConcurrentHashJoin::ConcurrentHashJoin(
     bool any_take_last_row_)
     : table_join(table_join_)
     , slots(toPowerOfTwo(std::min<UInt32>(static_cast<UInt32>(slots_), 256)))
+    , any_take_last_row(any_take_last_row_)
     , pool(std::make_unique<ThreadPool>(
           CurrentMetrics::ConcurrentHashJoinPoolThreads,
           CurrentMetrics::ConcurrentHashJoinPoolThreadsActive,
