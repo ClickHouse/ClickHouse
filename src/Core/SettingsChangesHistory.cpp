af67018664452ed986ae11f6c25582a1be4c19fc
Shankar Iyer <shankar.iyer@clickhouse.com>
2025-05-08 11:51:21 +0000

    Correct the old value

--- src/Core/SettingsChangesHistory.cpp
+++ src/Core/SettingsChangesHistory.cpp
@@ -86,7 +86,7 @@ const VersionToSettingsChangesMap & getSettingsChangesHistory()
             {"force_exchange_kind", "", "", "New experimental setting."},
             {"allow_experimental_delta_kernel_rs", true, true, "New setting"},
             {"allow_experimental_database_hms_catalog", false, false, "Allow experimental database engine DataLakeCatalog with catalog_type = 'hive'"},
-            {"vector_search_filtering", 0, 0, "Vector search related "},
+            {"vector_search_filtering", "auto", "auto", "Vector search related "},
             {"vector_search_postfilter_multiplier", 1, 1, "Vector search related "},
         });
         addSettingsChanges(settings_changes_history, "25.4",
