2a6c80c4353adf963bc0b17d23f0159bfa690186
Andrey Zvonov <azvonov@altinity.com>
2025-05-09 10:35:35 +0000

    add default common headers section

--- src/Server/StaticRequestHandler.cpp
+++ src/Server/StaticRequestHandler.cpp
@@ -155,13 +155,16 @@ StaticRequestHandler::StaticRequestHandler(
 
 HTTPRequestHandlerFactoryPtr createStaticHandlerFactory(IServer & server,
     const Poco::Util::AbstractConfiguration & config,
-    const std::string & config_prefix)
+    const std::string & config_prefix,
+    std::unordered_map<String, String> & common_headers)
 {
     int status = config.getInt(config_prefix + ".handler.status", 200);
     std::string response_content = config.getRawString(config_prefix + ".handler.response_content", "Ok.\n");
 
     std::unordered_map<String, String> http_response_headers_override
         = parseHTTPResponseHeaders(config, config_prefix, "text/plain; charset=UTF-8");
+
+    http_response_headers_override.insert(common_headers.begin(), common_headers.end());
 
     auto creator = [&server, http_response_headers_override, response_content, status]() -> std::unique_ptr<StaticRequestHandler>
     { return std::make_unique<StaticRequestHandler>(server, response_content, http_response_headers_override, status); };
