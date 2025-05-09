401c1d0c78ab3c0e3b4f9b84b43a5576c6145c51
Andrey Zvonov <azvonov@altinity.com>
2025-05-09 11:26:21 +0000

    add test

--- tests/queries/0_stateless/03448_http_response_headers_in_config.sh
+++ tests/queries/0_stateless/03448_http_response_headers_in_config.sh
@@ -13,81 +13,134 @@ cat > "$config_path" <<EOF
 <?xml version="1.0"?>
 <clickhouse>
     <http_handlers>
+        <common_http_response_headers>
+            <X-My-Common-Header>Common header present</X-My-Common-Header>
+        </common_http_response_headers>
+
         <rule>
-            <url>/static</url>
             <methods>GET,HEAD</methods>
+            <url>/static</url>
             <handler>
                 <type>static</type>
                 <response_expression>config://http_server_default_response</response_expression>
                 <content_type>text/html; charset=UTF-8</content_type>
                 <http_response_headers>
-                    <X-My-Answer>42</X-My-Answer>
+                    <X-My-Answer>Iam static</X-My-Answer>
                 </http_response_headers>
             </handler>
         </rule>
 
         <rule>
+            <methods>GET,HEAD</methods>
             <url>/ping</url>
-            <methods>GET</methods>
             <handler>
                 <type>ping</type>
                 <http_response_headers>
-                    <X-My-Answer>42</X-My-Answer>
+                    <X-My-Answer>Iam ping</X-My-Answer>
                 </http_response_headers>
             </handler>
         </rule>
 
         <rule>
-            <url>/replicas_status</url>
             <methods>GET,HEAD</methods>
+            <url>/replicas_status</url>
             <handler>
                 <type>replicas_status</type>
                 <http_response_headers>
-                    <X-My-Answer>42</X-My-Answer>
+                    <X-My-Answer>Iam replicas_status</X-My-Answer>
                 </http_response_headers>
             </handler>
         </rule>
 
         <rule>
-            <url>/play</url>
             <methods>GET,HEAD</methods>
+            <url>/play</url>
             <handler>
                 <type>play</type>
                 <http_response_headers>
-                    <X-My-Answer>42</X-My-Answer>
+                    <X-My-Answer>Iam play</X-My-Answer>
                 </http_response_headers>
             </handler>
         </rule>
 
         <rule>
-            <url>/dashboard</url>
             <methods>GET,HEAD</methods>
+            <url>/dashboard</url>
             <handler>
                 <type>dashboard</type>
                 <http_response_headers>
-                    <X-My-Answer>42</X-My-Answer>
+                    <X-My-Answer>Iam dashboard</X-My-Answer>
                 </http_response_headers>
             </handler>
         </rule>
 
         <rule>
-            <url>/binary</url>
             <methods>GET,HEAD</methods>
+            <url>/binary</url>
             <handler>
                 <type>binary</type>
                 <http_response_headers>
-                    <X-My-Answer>42</X-My-Answer>
+                    <X-My-Answer>Iam binary</X-My-Answer>
                 </http_response_headers>
             </handler>
         </rule>
 
         <rule>
-            <url>/merges</url>
             <methods>GET,HEAD</methods>
+            <url>/merges</url>
             <handler>
                 <type>merges</type>
                 <http_response_headers>
-                    <X-My-Answer>42</X-My-Answer>
+                    <X-My-Answer>Iam merges</X-My-Answer>
+                </http_response_headers>
+            </handler>
+        </rule>
+
+        <rule>
+            <methods>GET,HEAD</methods>
+            <url>/metrics</url>
+            <handler>
+                <type>prometheus</type>
+                <http_response_headers>
+                    <X-My-Answer>Iam metrics</X-My-Answer>
+                </http_response_headers>
+            </handler>
+        </rule>
+
+        <rule>
+            <url>/js/lz-string.js</url>
+            <handler>
+                <type>js</type>
+                <http_response_headers>
+                    <X-My-Answer>Iam js/lz-string</X-My-Answer>
+                </http_response_headers>
+            </handler>
+        </rule>
+
+        <rule>
+            <methods>GET,HEAD</methods>
+            <url>/js/uplot.js</url>
+            <handler>
+                <type>js</type>
+                <http_response_headers>
+                    <X-My-Answer>Iam js/uplot.js</X-My-Answer>
+                </http_response_headers>
+            </handler>
+        </rule>
+
+        <rule>
+            <url>/query_param_with_url</url>
+            <methods>GET,HEAD</methods>
+            <headers>
+                <PARAMS_XXX><![CDATA[regex:(?P<name_1>[^/]+)]]></PARAMS_XXX>
+            </headers>
+            <handler>
+                <type>predefined_query_handler</type>
+                <query>
+                    SELECT {name_1:String}
+                </query>
+                <http_response_headers>
+                    <X-My-Answer>Iam predefined</X-My-Answer>
                 </http_response_headers>
             </handler>
         </rule>
@@ -98,7 +151,7 @@ cat > "$config_path" <<EOF
                 <type>dynamic_query_handler</type>
                 <query_param_name>query</query_param_name>
                 <http_response_headers>
-                    <X-My-Answer>42</X-My-Answer>
+                    <X-My-Answer>Iam dynamic</X-My-Answer>
                 </http_response_headers>
             </handler>
         </rule>
@@ -108,11 +161,11 @@ EOF
 
 $CLICKHOUSE_CLIENT --query "SYSTEM RELOAD CONFIG" 2>&1
 
-${CLICKHOUSE_CURL} -I "http://localhost:8123/static" 2>&1 | grep -i 'X-My-Answer'
-${CLICKHOUSE_CURL} -I "http://localhost:8123/ping" 2>&1 | grep -i 'X-My-Answer'
-${CLICKHOUSE_CURL} -I "http://localhost:8123/replicas_status" 2>&1 | grep -i 'X-My-Answer'
-${CLICKHOUSE_CURL} -I "http://localhost:8123/play" 2>&1 | grep -i 'X-My-Answer'
-${CLICKHOUSE_CURL} -I "http://localhost:8123/dashboard" 2>&1 | grep -i 'X-My-Answer'
-${CLICKHOUSE_CURL} -I "http://localhost:8123/binary" 2>&1 | grep -i 'X-My-Answer'
-${CLICKHOUSE_CURL} -I "http://localhost:8123/merges" 2>&1 | grep -i 'X-My-Answer'
-${CLICKHOUSE_CURL} -I "http://localhost:8123/?query=SELECT%201" 2>&1 | grep -i 'X-My-Answer'
+for endpoint in static ping replicas_status play dashboard binary merges metrics "js/lz-string.js" "js/uplot.js" "?query=SELECT%201"; do
+  ${CLICKHOUSE_CURL} -I "http://localhost:8123/$endpoint" 2>&1 | grep -i 'X-My-Answer';
+  ${CLICKHOUSE_CURL} -I "http://localhost:8123/$endpoint" 2>&1 | grep -i 'X-My-Common-Header';
+done
+
+${CLICKHOUSE_CURL} -I -H 'PARAMS_XXX:test_param' "http://localhost:8123/query_param_with_url" 2>&1 | grep -i 'X-My-Answer'
+${CLICKHOUSE_CURL} -I -H 'PARAMS_XXX:test_param' "http://localhost:8123/query_param_with_url" 2>&1 | grep -i 'X-My-Common-Header'
+
