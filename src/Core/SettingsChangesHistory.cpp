ef138426fa290bb33f97c2627323a8f0f00bed09
Konstantin Vedernikov <vedernikovkostya@mail.ru>
2025-05-10 18:25:37 +0000

    start

--- src/Core/SettingsChangesHistory.cpp
+++ src/Core/SettingsChangesHistory.cpp
@@ -86,6 +86,7 @@ const VersionToSettingsChangesMap & getSettingsChangesHistory()
             {"force_exchange_kind", "", "", "New experimental setting."},
             {"allow_experimental_delta_kernel_rs", true, true, "New setting"},
             {"allow_experimental_database_hms_catalog", false, false, "Allow experimental database engine DataLakeCatalog with catalog_type = 'hive'"},
+            {"output_format_parquet_allow_geoparquet_parser", false, true, "A new setting to use geo columns in parquet file"},
         });
         addSettingsChanges(settings_changes_history, "25.4",
         {
