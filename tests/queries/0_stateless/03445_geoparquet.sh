91c71cafb45f996f7f50bb377e53935d06a14baf
Konstantin Vedernikov <vedernikovkostya@mail.ru>
2025-05-10 19:50:09 +0000

    rename setting from output to input

--- tests/queries/0_stateless/03445_geoparquet.sh
+++ tests/queries/0_stateless/03445_geoparquet.sh
@@ -10,4 +10,4 @@ $CLICKHOUSE_LOCAL -q "select * from file('$CURDIR/data_parquet/03445_geoparquet_
 $CLICKHOUSE_LOCAL -q "select * from file('$CURDIR/data_parquet/03445_geoparquet_null_point.parquet', Parquet)"
 $CLICKHOUSE_LOCAL -q "select * from file('$CURDIR/data_parquet/03445_geoparquet_null_linestring.parquet', Parquet)"
 $CLICKHOUSE_LOCAL -q "select * from file('$CURDIR/data_parquet/03445_geoparquet_null_polygon.parquet', Parquet)"
-$CLICKHOUSE_LOCAL -q "select * from file('$CURDIR/data_parquet/03445_geoparquet_wkb.parquet', Parquet) SETTINGS output_format_parquet_allow_geoparquet_parser=false;"
+$CLICKHOUSE_LOCAL -q "select * from file('$CURDIR/data_parquet/03445_geoparquet_wkb.parquet', Parquet) SETTINGS input_format_parquet_allow_geoparquet_parser=false;"
