023f6da3daa65d54a88455af87fc64aa07ebbc2d
Joanna Hulboj <joanna.hulboj@gmail.com>
2025-05-11 18:30:18 +0100

    Improve sizing for Base58 decode buffer

--- src/Functions/FunctionBase58Conversion.h
+++ src/Functions/FunctionBase58Conversion.h
@@ -29,9 +29,11 @@ struct Base58DecodeTraits
     template <typename Col>
     static size_t getBufferSize(Col const & src_column)
     {
-        /// For Base58, each character represents ~5.9 bits
-        /// We just need the input size as the upper bound
-        return src_column.byteSize();
+        auto const string_length = src_column.byteSize();
+        auto const string_count = src_column.size();
+        /// decoded size is at most length of encoded (every 8 bytes becomes at most 6 bytes)
+        /// plus one byte for null terminator for each string
+        return (string_length * 6 / 8 + 1) * string_count;
     }
 
     static std::optional<size_t> perform(std::string_view src, UInt8 * dst)
