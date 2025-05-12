a493436ab0437cde68152b6e4e97fb2ab0aff5fd
Shankar Iyer <shankar.iyer@clickhouse.com>
2025-05-12 05:58:19 +0000

    Default of enable_url_encoding set to 0

--- src/Core/SettingsChangesHistory.cpp
+++ src/Core/SettingsChangesHistory.cpp
@@ -101,6 +101,7 @@ const VersionToSettingsChangesMap & getSettingsChangesHistory()
             {"compile_expressions", false, true, "We believe that the LLVM infrastructure behind the JIT compiler is stable enough to enable this setting by default."},
             {"use_legacy_to_time", false, false, "New setting. Allows for user to use the old function logic for toTime, which works as toTimeWithFixedDate."},
             {"input_format_parquet_allow_geoparquet_parser", false, true, "A new setting to use geo columns in parquet file"},
+            {"enable_url_encoding", true, false, "Changed existing setting's default value"},
         });
         addSettingsChanges(settings_changes_history, "25.4",
         {
