4118b6e0bd2f2df21ce8897a81af7d83ca5b5551
nihalzp <81457724+nihalzp@users.noreply.github.com>
2025-05-13 23:25:14 +0800

    Add language code mappings as comment to tests

--- tests/queries/0_stateless/03406_naive_bayes_codepoint.sql
+++ tests/queries/0_stateless/03406_naive_bayes_codepoint.sql
@@ -1,6 +1,18 @@
 -- Tags: no-fasttest
 -- no-fasttest: depends on model binary and model details via config files
 
+/*
+Output language code mapping:
+  Bengali           0
+  Mandarin Chinese  1
+  German            2
+  Greek             3
+  English           4
+  French            5
+  Russian           6
+  Spanish           7
+*/
+
 SELECT naiveBayesClassifier('lang_codepoint_1', 'আজ আকাশটা খুব পরিষ্কার।');
 SELECT naiveBayesClassifier('lang_codepoint_1', '她每天早上喝一杯绿茶');
 SELECT naiveBayesClassifier('lang_codepoint_1', 'Der Hund schläft unter dem Tisch.');
--- tests/queries/0_stateless/03407_naive_bayes_byte.sql
+++ tests/queries/0_stateless/03407_naive_bayes_byte.sql
@@ -1,6 +1,18 @@
 -- Tags: no-fasttest
 -- no-fasttest: depends on model binary and model details via config files
 
+/*
+Output language code mapping:
+  Bengali           0
+  Mandarin Chinese  1
+  German            2
+  Greek             3
+  English           4
+  French            5
+  Russian           6
+  Spanish           7
+*/
+
 SELECT naiveBayesClassifier('lang_byte_2', 'বইটি টেবিলের উপর রাখা আছে।');
 SELECT naiveBayesClassifier('lang_byte_2', '他们正在公园里散步');
 SELECT naiveBayesClassifier('lang_byte_2', 'Er kocht Suppe für seine Familie');
