962feedd305c3be9a03a2aeafb50be166c14f75e
vdimir <vdimir@clickhouse.com>
2025-05-14 12:21:25 +0000

    f

--- tests/queries/0_stateless/03512_join_using_parent_scope_matcher.sql
+++ tests/queries/0_stateless/03512_join_using_parent_scope_matcher.sql
@@ -10,6 +10,7 @@ INSERT INTO t1 VALUES (1.0), (2.0), (3.0), (4.0), (5.0);
 CREATE TABLE t2 (`a` UInt32) ENGINE = MergeTree ORDER BY a;
 INSERT INTO t2 VALUES (1), (2), (3), (4), (5);
 
+SET enable_analyzer = 1;
 
 SET analyzer_compatibility_join_using_top_level_identifier = 1;
 
