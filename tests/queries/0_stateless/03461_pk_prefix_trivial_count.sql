bb6b9715f38d1d59e49fa76cb7d827d846c8578e
Amos Bird <amosbird@gmail.com>
2025-05-16 03:13:22 +0800

    Better tests

--- tests/queries/0_stateless/03461_pk_prefix_trivial_count.sql
+++ tests/queries/0_stateless/03461_pk_prefix_trivial_count.sql
@@ -2,8 +2,10 @@
 
 DROP TABLE IF EXISTS t;
 
-CREATE TABLE t(k String) ORDER BY k as select 'dst_'||number from numbers(1e6);
+CREATE TABLE t(k String) ORDER BY k as select 'dst_'||number from numbers(10);
 
 SELECT count(*) FROM t WHERE k LIKE 'dst_kkkk_1111%';
+
+SELECT count(*) FROM t WHERE k LIKE 'dst%kkkk';
 
 DROP TABLE t;
