6cd42173cfccf244f535a1cb3b483a04225245e6
Pedro Ferreira <pedro.silva.ferreira@clickhouse.com>
2025-05-16 10:14:34 +0000

    Another fix

--- tests/casa_del_dolor/dolor.py
+++ tests/casa_del_dolor/dolor.py
@@ -127,7 +127,7 @@ def dolor_cleanup():
     except FileNotFoundError:
         pass
     try:
-        os.unlink(generator.temp)
+        os.unlink(generator.temp.name)
     except FileNotFoundError:
         pass
 atexit.register(dolor_cleanup)
