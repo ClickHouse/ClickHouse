542f5876592727b19effe7b02578f7f902ef2ea4
Amos Bird <amosbird@gmail.com>
2025-05-17 00:02:05 +0800

    Fix test

--- tests/queries/0_stateless/01710_minmax_count_projection.sql
+++ tests/queries/0_stateless/01710_minmax_count_projection.sql
@@ -71,7 +71,7 @@ drop table d;
 -- count variant optimization
 
 drop table if exists test;
-create table test (id Int64, d Int64, projection dummy(select * order by id)) engine MergeTree order by id;
+create table test (id Int64, d Int64) engine MergeTree order by id;
 insert into test select number, number from numbers(1e3);
 
 select count(if(d=4, d, 1)) from test settings force_optimize_projection = 1;
