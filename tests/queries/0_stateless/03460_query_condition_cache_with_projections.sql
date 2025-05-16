524e8185a73f798436afc20a8780b1ec8e84e12b
Amos Bird <amosbird@gmail.com>
2025-05-16 23:35:04 +0800

    Fix tests

--- tests/queries/0_stateless/03460_query_condition_cache_with_projections.sql
+++ tests/queries/0_stateless/03460_query_condition_cache_with_projections.sql
@@ -20,7 +20,7 @@ system drop query condition cache;
 
 select j from t where j > 3 and i = 20 order by j settings max_threads = 1, use_query_condition_cache = 1, query_condition_cache_store_conditions_as_plaintext = 1;
 
-select part_name, condition from system.query_condition_cache order by condition;
+select part_name, condition from system.query_condition_cache order by part_name;
 
 select j from t where j > 3 and i = 20 order by j settings max_threads = 1, use_query_condition_cache = 1, query_condition_cache_store_conditions_as_plaintext = 1;
 
