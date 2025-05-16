f75d558713499cbdbf8284519250e2cccaf47aa2
nihalzp <81457724+nihalzp@users.noreply.github.com>
2025-05-16 23:45:23 +0800

    Move parquet json parsing setting to 25.6

--- src/Core/SettingsChangesHistory.cpp
+++ src/Core/SettingsChangesHistory.cpp
@@ -69,7 +69,7 @@ const VersionToSettingsChangesMap & getSettingsChangesHistory()
         /// Note: please check if the key already exists to prevent duplicate entries.
         addSettingsChanges(settings_changes_history, "25.6",
         {
-
+            {"input_format_parquet_enable_json_parsing", false, false, "When reading Parquet files, parse JSON columns as ClickHouse JSON Column."},
         });
         addSettingsChanges(settings_changes_history, "25.5",
         {
@@ -105,7 +105,6 @@ const VersionToSettingsChangesMap & getSettingsChangesHistory()
             {"allow_experimental_lightweight_update", false, false, "A new setting"},
             {"allow_experimental_delta_kernel_rs", true, true, "New setting"},
             {"allow_experimental_database_hms_catalog", false, false, "Allow experimental database engine DataLakeCatalog with catalog_type = 'hive'"},
-            {"input_format_parquet_enable_json_parsing", false, false, "When reading Parquet files, parse JSON columns as ClickHouse JSON Column."},
             {"vector_search_filter_strategy", "auto", "auto", "New setting"},
             {"vector_search_postfilter_multiplier", 1, 1, "New setting"},
             {"compile_expressions", false, true, "We believe that the LLVM infrastructure behind the JIT compiler is stable enough to enable this setting by default."},
