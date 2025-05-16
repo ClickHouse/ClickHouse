b6e072c3845361857699e519d67cbaf777b2c3c4
Kirill Nikiforov <me@allmazz.me>
2025-05-17 03:44:05 +0400

    fix style

--- src/Processors/Sources/MongoDBSource.h
+++ src/Processors/Sources/MongoDBSource.h
@@ -48,14 +48,15 @@ private:
     Block sample_block;
     std::unordered_map<size_t, std::pair<size_t, std::pair<DataTypePtr, Field>>> arrays_info;
     const UInt64 max_block_size;
+    bool all_read = false;
 
-    JSONBuilder::FormatSettings json_format_settings{
+    const JSONBuilder::FormatSettings json_format_settings
+    {
             {.json = {.quote_64bit_integers = false, .max_depth = 0}},
             0,
             true,
             true,
     };
-    bool all_read = false;
 };
 
 }
