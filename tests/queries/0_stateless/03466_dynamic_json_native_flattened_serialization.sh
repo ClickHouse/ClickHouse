e59b0e161e552807eebd3c940f331934a1c34ab9
avogar <avogar@clickhouse.com>
2025-05-19 17:54:34 +0000

    Fix tests

--- tests/queries/0_stateless/03466_dynamic_json_native_flattened_serialization.sh
+++ tests/queries/0_stateless/03466_dynamic_json_native_flattened_serialization.sh
@@ -27,7 +27,3 @@ create table test (json JSON(max_dynamic_types=0, max_dynamic_paths=0)) engine=M
 insert into test values ('{\"a\" : [{\"b\" : 42, \"c\" : \"2020-01-01\"}]}'), ('{\"a\" : [{\"b\" : \"2020-01-01\", \"c\" : 42, \"d\" : 42}]}'), ('{\"a\" : [{\"b\" : \"str1\", \"c\" : [1, 2, 3], \"d\" : [1, 2, 3]}]}'), ('{\"a\" : [{\"b\" : 43, \"c\" : 43}]}');
 select * from test format Native settings output_format_native_use_flattened_dynamic_and_json_serialization=1;
 " | $CLICKHOUSE_LOCAL --table test --input-format Native -q "select json, JSONDynamicPathsWithTypes(json.a[][1]), JSONSharedDataPathsWithTypes(json.a[][1]) from test settings output_format_json_quote_64bit_integers=0"
-
-
-
-:) insert into test values ('{"a" : 1, "b" : 42, "c" : "2020-01-01"}'), ('{"a" : 2, "b" : "2020-01-01", "c" : 42, "d" : 42}'), ('{"a" : 3, "b" : "str1", "c" : [1, 2, 3], "d" : "str1", "e" : 42}'), ('{"a" : 4, "b" : [1, 2, 3], "c" : "str1", "d" : [1, 2, 3], "e" : "str1"}');
