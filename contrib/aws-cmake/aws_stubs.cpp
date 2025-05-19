b456446083938fb14c1efdfbe2f99cd8d84ffe4d
Konstantin Bogdanov <konstantin@clickhouse.com>
2025-05-19 18:59:03 +0200

    Add noop

--- contrib/aws-cmake/aws_stubs.cpp
+++ contrib/aws-cmake/aws_stubs.cpp
@@ -4,21 +4,15 @@
 
 extern "C" {
 
-#define AWS_TRAP(sym) \
-    __attribute__((weak)) void sym() { \
-        std::fprintf(stderr, "TRAPPED: " #sym " called\n"); \
-        std::abort(); \
-    }
-
 #define AWS_NOOP(sym) \
     __attribute__((weak)) void sym() { \
         /* No operation */ \
     }
 
-AWS_TRAP(aws_tls_client_ctx_new)
-AWS_TRAP(aws_tls_server_ctx_new)
-AWS_TRAP(aws_tls_init_static_state)
-AWS_TRAP(aws_tls_clean_up_static_state)
+AWS_NOOP(aws_tls_client_ctx_new)
+AWS_NOOP(aws_tls_server_ctx_new)
+AWS_NOOP(aws_tls_init_static_state)
+AWS_NOOP(aws_tls_clean_up_static_state)
 AWS_NOOP(aws_mqtt_library_init)
 AWS_NOOP(aws_mqtt_library_clean_up)
 
