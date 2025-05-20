cad2ea19d0d311e3bda336b17dedeb6271260f1f
nihalzp <81457724+nihalzp@users.noreply.github.com>
2025-05-21 01:32:13 +0800

    Update introduced in version number

--- src/Functions/FunctionNaiveBayesClassifier.cpp
+++ src/Functions/FunctionNaiveBayesClassifier.cpp
@@ -350,7 +350,7 @@ REGISTER_FUNCTION(NaiveBayesClassifier)
 
           Result 0 might represent English, while 1 could indicate French - class meanings depend on your training data.
         )"}};
-    FunctionDocumentation::IntroducedIn introduced_in = {25, 5};
+    FunctionDocumentation::IntroducedIn introduced_in = {25, 6};
     FunctionDocumentation::Category category = FunctionDocumentation::Category::MachineLearning;
 
     FunctionDocumentation function_documentation
