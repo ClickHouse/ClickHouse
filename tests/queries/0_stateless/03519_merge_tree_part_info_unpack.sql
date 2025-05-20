47eda8366314ec7f09f9c99e68aa26d58be90d8d
Mikhail Artemenko <mikhail.artemenko@clickhouse.com>
2025-05-20 13:07:53 +0000

    adapt test

--- tests/queries/0_stateless/03519_merge_tree_part_info_unpack.sql
+++ tests/queries/0_stateless/03519_merge_tree_part_info_unpack.sql
@@ -1,5 +1,5 @@
-WITH 'all_12_25_7_4' AS part_name
-SELECT partPartitionId(part_name), partMinBlock(part_name), partMaxBlock(part_name), partMergeLevel(part_name), partMutationLevel(part_name);
+WITH mergeTreePartInfo('all_12_25_7_4') AS info
+SELECT info.partition_id, info.min_block, info.max_block, info.level, info.mutation;
 
 CREATE TABLE mt(key UInt64, value String)
 ENGINE = MergeTree
@@ -7,6 +7,6 @@ ORDER BY key;
 
 SYSTEM STOP MERGES mt;
 INSERT INTO mt SELECT rand(), rand() FROM numbers(4) SETTINGS min_insert_block_size_rows=1, max_block_size=1;
-SELECT _part FROM mt ORDER BY partMaxBlock(_part) DESC;
+SELECT _part FROM mt ORDER BY mergeTreePartInfo(_part).max_block DESC;
 
-SELECT _part, isPartCoveredBy(_part, 'all_1_2_10') FROM mt ORDER BY partMaxBlock(_part) DESC;
+SELECT _part, isMergeTreePartCoveredBy(_part, 'all_1_2_10') FROM mt ORDER BY mergeTreePartInfo(_part).max_block DESC;
