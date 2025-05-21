a7bb282af71f6854f6e176b58604d38d6287e0cf
yariks5s <yaroslav.briukhovetskyi@clickhouse.com>
2025-05-21 18:49:11 +0000

    Fix test (not for the old analyzer)

--- tests/queries/0_stateless/03393_non_constant_second_argument_for_in.sql
+++ tests/queries/0_stateless/03393_non_constant_second_argument_for_in.sql
@@ -1,3 +1,5 @@
+SET allow_experimental_analyzer = 1;
+
 SELECT number FROM numbers(10) WHERE has([number % 3, number % 5], number % 2) ORDER BY number;
 SELECT '-- IN --';
 SELECT number FROM numbers(10) WHERE number % 2 IN [number % 3, number % 5] ORDER BY number SETTINGS allow_experimental_analyzer = 1;
