ddb22a85796c0a6fe13fca1da1cfc1feb10d4d61
Nikita Taranov <nikita.taranov@clickhouse.com>
2025-05-21 18:14:01 +0100

    better

--- src/Common/examples/test_xray_tracing.sh
+++ src/Common/examples/test_xray_tracing.sh
@@ -5,12 +5,10 @@ export XRAY_OPTIONS="xray_mode=xray-basic verbosity=1"
 res=$(
 rm xray-log.* ; \
   ninja xray_tracing -C build > /dev/null 2>&1 && \
-  build/src/Common/examples/xray_tracing 2 > /dev/null && \
+  build/src/Common/examples/xray_tracing > /dev/null 2>&1 && \
   llvm-xray convert --symbolize --instr_map=build/src/Common/examples/xray_tracing --output-format=trace_event "$(ls -lt xray-log.* | awk '{ print $NF }' | tail -n1)" \
   | jq '.traceEvents | length'
 )
-
-echo "res: $res"
 
 if [ "$res" -eq "12" ]; then
   echo -e "Test \033[32mpassed\033[0m"
