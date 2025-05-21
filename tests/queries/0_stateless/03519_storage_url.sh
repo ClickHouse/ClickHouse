a2b533b9fde1be26d8915234b49ec44896e5e248
Sema Checherinda <checherinda@gmail.com>
2025-05-21 16:01:34 +0200

    turn off async_inserts

--- tests/queries/0_stateless/03519_storage_url.sh
+++ tests/queries/0_stateless/03519_storage_url.sh
@@ -23,13 +23,13 @@ engine=MergeTree()
 ORDER BY key;
 
 create table dst (key Int, value Int)
-engine=URL('${CLICKHOUSE_URL}&query=insert+into+dst_remote+format+TSV', TSV);
+engine=URL('${CLICKHOUSE_URL}&async_insert=0&query=insert+into+dst_remote+format+TSV', TSV);
 
 create materialized view mv to dst
 as select * from src where value > 0;
 "
 
-$CLICKHOUSE_CLIENT -q "
+$CLICKHOUSE_CLIENT --async_insert=0 -q "
 insert into src values (1, 0), (2, 1), (3, 0), (4, 1);
 
 insert into src values (10, 0), (20, 0), (30, 0), (40, 0);
