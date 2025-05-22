afbe7defa201eff68bf562771c02cf513befb373
Igor Nikonov <igor@clickhouse.com>
2025-05-22 16:19:18 +0200

    Revert incorrect test change

--- tests/queries/0_stateless/02317_distinct_in_order_optimization.sql
+++ tests/queries/0_stateless/02317_distinct_in_order_optimization.sql
@@ -1,6 +1,3 @@
-set optimize_read_in_order=0;
-set enable_parallel_replicas=1, max_parallel_replicas=3, cluster_for_parallel_replicas='test_cluster_one_shard_three_replicas_localhost', parallel_replicas_for_non_replicated_merge_tree=1, parallel_replicas_local_plan=1;
-
 select '-- enable distinct in order optimization';
 set optimize_distinct_in_order=1;
 select '-- create table with only primary key columns';
