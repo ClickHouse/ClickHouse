d29b05faf81e15b1df9d273776781948fa491ebb
Pablo Marcos <pablo.marcos.oltra@clickhouse.com>
2025-05-22 08:01:06 +0000

    Add ProfileEvents for index search algorithm
    Update 03518_key_condition_binary_search stateless test to use
    the new ProfileEvents instead of relying on log traces.

--- tests/queries/0_stateless/03518_key_condition_binary_search.sh
+++ tests/queries/0_stateless/03518_key_condition_binary_search.sh
@@ -4,6 +4,8 @@ CUR_DIR=$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)
 # shellcheck source=../shell_config.sh
 . "$CUR_DIR"/../shell_config.sh
 
+readonly query_prefix=$CLICKHOUSE_DATABASE
+
 $CLICKHOUSE_CLIENT -n -q "
 DROP TABLE IF EXISTS t;
 CREATE TABLE t
@@ -12,11 +14,11 @@ CREATE TABLE t
 )
 ENGINE = MergeTree
 ORDER BY c;
-INSERT INTO t values('One');
-SELECT * FROM t WHERE c = 1 settings send_logs_level='trace';
-SELECT * FROM t WHERE c = 'One' settings send_logs_level='trace';
-SELECT * FROM t WHERE c = 1 and 1 = 1 settings send_logs_level='trace';
-" 2>&1 | grep -c "binary search"
+INSERT INTO t values('One');"
+
+$CLICKHOUSE_CLIENT -n -q "SELECT * FROM t WHERE c = 1 FORMAT Null;" --query_id="${query_prefix}_binary1"
+$CLICKHOUSE_CLIENT -n -q "SELECT * FROM t WHERE c = 'One' FORMAT Null;" --query_id="${query_prefix}_binary2"
+$CLICKHOUSE_CLIENT -n -q "SELECT * FROM t WHERE c = 1 and 1 = 1  FORMAT Null;" --query_id="${query_prefix}_binary3"
 
 $CLICKHOUSE_CLIENT -n -q "
 DROP TABLE IF EXISTS t1;
@@ -26,6 +28,12 @@ CREATE TABLE t1
 )
 ENGINE = MergeTree
 ORDER BY timestamp;
-INSERT INTO t1 VALUES ('2025-05-21 00:00:00');
-SELECT * FROM t1 WHERE toDayOfMonth(timestamp) = 1 settings send_logs_level='trace';
-" 2>&1 | grep -c "generic exclusion search"
+INSERT INTO t1 VALUES ('2025-05-21 00:00:00');"
+
+$CLICKHOUSE_CLIENT -n -q "SELECT * FROM t1 WHERE toDayOfMonth(timestamp) = 1 FORMAT Null;" --query-id="${query_prefix}_generic"
+$CLICKHOUSE_CLIENT -n -q "SYSTEM FLUSH LOGS;"
+
+$CLICKHOUSE_CLIENT -n -q "SELECT sum(ProfileEvents['IndexBinarySearchAlgorithm']), sum(ProfileEvents['IndexGenericExclusionSearchAlgorithm']) FROM system.query_log
+    WHERE type > 1 AND event_date >= yesterday() AND query_id ILIKE '${query_prefix}_binary%' AND current_database = currentDatabase()"
+$CLICKHOUSE_CLIENT -n -q "SELECT sum(ProfileEvents['IndexBinarySearchAlgorithm']), sum(ProfileEvents['IndexGenericExclusionSearchAlgorithm']) FROM system.query_log
+    WHERE type > 1 AND event_date >= yesterday() AND query_id ILIKE '${query_prefix}_generic%' AND current_database = currentDatabase()"
