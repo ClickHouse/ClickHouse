ff8e0db56d79ca7ac9e2e649e66a39ec6025e406
Azat Khuzhin <a3at.mail@gmail.com>
2025-05-24 22:01:17 +0200

    Fix excessive number of files (leads to excessive memory usage) for external ORDER BY
    In #72598 the behavior of `max_bytes_before_external_sort` has been
    changed, it has been switched to rely on the total memory usage of the
    query instead of the size of a sorting data.
    
    But, the problem with this approach is that you can have a lot of memory
    used outside of sorting (FINAL, aggregation), and spilling sorting
    blocks to disk will not help reduce memory usage obviously, but it will
    make things even worse, because it will create lots of small files, that
    will require a lot of memory to merge them.
    
    So this patch reverts the behavior of `max_bytes_before_external_sort`
    back, now it will rely on the sorting block size.

--- tests/queries/0_stateless/03339_native_reader_exact_rows.sql
+++ tests/queries/0_stateless/03339_native_reader_exact_rows.sql
@@ -11,6 +11,5 @@ SETTINGS
     /* 65536 rows takes buffer of 512KB, so use slightly bigger value to increase overhead */
     max_block_size=65540,
     max_bytes_before_external_sort='10Mi',
-    max_bytes_ratio_before_external_sort=0,
-    min_external_sort_block_bytes='1Mi'
+    max_bytes_ratio_before_external_sort=0
 FORMAT Null
