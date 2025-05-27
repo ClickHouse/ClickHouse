5da129d0350c6b321e416e8f28e21f078f4e81ae
pufit <pufit@clickhouse.com>
2025-05-27 19:00:24 -0400

    Try to fix test

--- tests/queries/0_stateless/03408_current_database_on_cluster_constraint.sql
+++ tests/queries/0_stateless/03408_current_database_on_cluster_constraint.sql
@@ -1,3 +1,3 @@
 -- Tags: no-replicated-database
 
-CREATE TABLE t0 ON CLUSTER 'test_shard_localhost' (c0 Int, CONSTRAINT cc CHECK currentDatabase()) ENGINE = MergeTree() ORDER BY tuple();
+CREATE TABLE t0 ON CLUSTER 'test_cluster_two_shards_different_databases' (c0 Int, CONSTRAINT cc CHECK currentDatabase()) ENGINE = MergeTree() ORDER BY tuple();
