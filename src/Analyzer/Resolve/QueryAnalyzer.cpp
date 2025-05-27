67d71dc0fe249b1120baf33dc125b8230f28c677
robot-clickhouse <robot-clickhouse@users.noreply.github.com>
2025-05-27 14:10:19 +0000

    Backport #80757 to 25.3: Ignore cte name when calculate hash for In SET name.

--- src/Analyzer/Resolve/QueryAnalyzer.cpp
+++ src/Analyzer/Resolve/QueryAnalyzer.cpp
@@ -3518,7 +3518,7 @@ ProjectionNames QueryAnalyzer::resolveFunction(QueryTreeNodePtr & node, Identifi
 
             SizeLimits size_limits_for_set = {settings[Setting::max_rows_in_set], settings[Setting::max_bytes_in_set], settings[Setting::set_overflow_mode]};
 
-            auto hash = function_arguments[1]->getTreeHash();
+            auto hash = function_arguments[1]->getTreeHash({ .ignore_cte = true });
             auto ast = function_arguments[1]->toAST();
             auto future_set = std::make_shared<FutureSetFromTuple>(hash, std::move(ast), std::move(result_block), settings[Setting::transform_null_in], size_limits_for_set);
 
