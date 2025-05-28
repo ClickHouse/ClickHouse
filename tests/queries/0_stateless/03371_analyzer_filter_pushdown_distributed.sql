c522c34d1526ef31f1e1b10802f011ccb30733c4
Nikolai Kochetov <nk@clickhouse.com>
2025-05-28 15:58:24 +0000

    Add tests from #80903

--- tests/queries/0_stateless/03371_analyzer_filter_pushdown_distributed.sql
+++ tests/queries/0_stateless/03371_analyzer_filter_pushdown_distributed.sql
@@ -1,4 +1,6 @@
-CREATE TABLE bug_table  
+set enable_analyzer=1;
+
+CREATE TABLE bug_table
 (
     `date_column` Date,
     `c1` String,
@@ -15,7 +17,7 @@ INSERT INTO bug_table values
     (toDate(now()),hex(rand()),hex(now())),
     (toDate(now()),hex(rand()),hex(now()));
 
-CREATE TABLE distributed_bug_table 
+CREATE TABLE distributed_bug_table
 (
 date_column Date,
 c1 String,
@@ -27,14 +29,14 @@ set distributed_product_mode = 'allow';
 
 set prefer_localhost_replica=1;
 
-WITH alias_1 AS 
-   (SELECT c1,c2 FROM distributed_bug_table) 
+WITH alias_1 AS
+   (SELECT c1,c2 FROM distributed_bug_table)
 SELECT c1 from alias_1 where c2 IN (SELECT DISTINCT c2 from alias_1)
 FORMAT Null;
 
 set prefer_localhost_replica=0;
 
-WITH alias_1 AS 
-   (SELECT c1,c2 FROM distributed_bug_table) 
+WITH alias_1 AS
+   (SELECT c1,c2 FROM distributed_bug_table)
 SELECT c1 from alias_1 where c2 IN (SELECT DISTINCT c2 from alias_1)
 FORMAT Null;
