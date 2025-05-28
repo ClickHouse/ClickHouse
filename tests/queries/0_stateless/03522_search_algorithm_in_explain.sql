6aba5d88eee5197150600da0bde59f52cb04ccfa
Pablo Marcos <pablo.marcos.oltra@clickhouse.com>
2025-05-28 17:02:31 +0000

    Tune test to check only what we're interested in

--- tests/queries/0_stateless/03522_search_algorithm_in_explain.sql
+++
@@ -1,9 +0,0 @@
-CREATE TABLE t (a UInt8, b UInt8) ORDER BY (a, b);
-INSERT INTO t VALUES (0,0);
-
-SELECT '-- Output when the key condition is the first of the compound key';
-EXPLAIN indexes = 1 SELECT * FROM t WHERE a = 0;
-
-SELECT '';
-SELECT '-- Output when the key condition is the second of the compound key';
-EXPLAIN indexes = 1 SELECT * FROM t WHERE b = 0;
