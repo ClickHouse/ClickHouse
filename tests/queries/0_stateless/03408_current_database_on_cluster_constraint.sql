14bf1ddcc9a3ad80d69cd1b9ea1170ca1f6d585c
pufit <pufit@clickhouse.com>
2025-05-29 17:22:54 -0400

    Update 03408_current_database_on_cluster_constraint.sql

--- tests/queries/0_stateless/03408_current_database_on_cluster_constraint.sql
+++ tests/queries/0_stateless/03408_current_database_on_cluster_constraint.sql
@@ -1,2 +1,2 @@
 
-CREATE TABLE t0 ON CLUSTER 'test_cluster_two_shards_different_databases' (c0 Int, CONSTRAINT cc CHECK currentDatabase()) ENGINE = MergeTree() ORDER BY tuple();
+CREATE TABLE t0 ON CLUSTER 'test_cluster_two_shards' (c0 Int, CONSTRAINT cc CHECK currentDatabase()) ENGINE = MergeTree() ORDER BY tuple();
