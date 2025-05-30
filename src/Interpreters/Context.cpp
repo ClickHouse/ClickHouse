61fb53887dfe2909a6e2e026b6806dc67a8277c5
Azat Khuzhin <a3at.mail@gmail.com>
2025-05-30 23:46:53 +0200

    Clean index mark cache on SYSTEM DROP MARK CACHE
    Likely this is the reason of CI failures for 03277_prewarm_cache_2,
    where we have 2 more in MarkCacheFiles, since previously it was not
    accounted at all in asynchornouse_metrics, while now, since it shares
    metric they does
    
    CI: https://s3.amazonaws.com/clickhouse-test-reports/json.html?PR=81023&sha=b140973235744f0fea34daa6272cfa4602d72a5b&name_0=PR&name_1=Fast+test&name_2=Tests

--- src/Interpreters/Context.cpp
+++ src/Interpreters/Context.cpp
@@ -3421,11 +3421,20 @@ MarkCachePtr Context::getMarkCache() const
 void Context::clearMarkCache() const
 {
     /// Get local shared pointer to the cache
-    MarkCachePtr cache = getMarkCache();
+    if (MarkCachePtr cache = getMarkCache())
+    {
+        /// Clear the cache without holding context mutex to avoid blocking context for a long time
+        if (cache)
+            cache->clear();
+    }
 
-    /// Clear the cache without holding context mutex to avoid blocking context for a long time
-    if (cache)
-        cache->clear();
+    /// Get local shared pointer to the cache
+    if (MarkCachePtr cache = getIndexMarkCache())
+    {
+        /// Clear the cache without holding context mutex to avoid blocking context for a long time
+        if (cache)
+            cache->clear();
+    }
 }
 
 ThreadPool & Context::getLoadMarksThreadpool() const
