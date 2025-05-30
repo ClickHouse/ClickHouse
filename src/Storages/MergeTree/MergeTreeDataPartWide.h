42b0712c27b51e50c41393404681f70c6a7e658f
avogar <avogar@clickhouse.com>
2025-05-30 19:25:08 +0000

    Add columns_substreams.txt file to Wide part to track all substreams stored in the part

--- src/Storages/MergeTree/MergeTreeDataPartWide.h
+++ src/Storages/MergeTree/MergeTreeDataPartWide.h
@@ -54,9 +54,9 @@ private:
     /// Loads marks index granularity into memory
     void loadIndexGranularity() override;
 
-    ColumnSize getColumnSizeImpl(const NameAndTypePair & column, std::unordered_set<String> * processed_substreams, std::optional<Block> columns_sample) const;
+    ColumnSize getColumnSizeImpl(const NameAndTypePair & column, std::unordered_set<String> * processed_substreams) const;
 
-    void calculateEachColumnSizes(ColumnSizeByName & each_columns_size, ColumnSize & total_size, std::optional<Block> columns_sample) const override;
+    void calculateEachColumnSizes(ColumnSizeByName & each_columns_size, ColumnSize & total_size) const override;
 
 };
 
