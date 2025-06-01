60468b2f1f3391b12cadea4caca857871a999efc
Alexey Milovidov <milovidov@clickhouse.com>
2025-06-01 03:44:34 +0200

    Update 03528_s3_insert_partition_by_whitespaces.sql

--- tests/queries/0_stateless/03528_s3_insert_partition_by_whitespaces.sql
+++ tests/queries/0_stateless/03528_s3_insert_partition_by_whitespaces.sql
@@ -3,7 +3,7 @@
 
 INSERT INTO FUNCTION
    s3(
-       s3_conn
+       s3_conn,
        filename = currentDatabase() || '/test1.parquet',
        format = Parquet
     )
@@ -18,7 +18,7 @@ SELECT * FROM s3(s3_conn, filename = currentDatabase() || '/test1.parquet');
 
 INSERT INTO FUNCTION
    s3(
-       s3_conn
+       s3_conn,
        filename = currentDatabase() || '/test2.parquet',
        format = Parquet
     ) PARTITION BY rand() % 10 SELECT
