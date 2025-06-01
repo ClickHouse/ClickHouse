cdda9e9e4e8e1b269ad7d4626d2fe351cb0167e6
Alexey Milovidov <milovidov@clickhouse.com>
2025-06-01 02:53:51 +0200

    Add a test for #68279

---
+++ tests/queries/0_stateless/03528_s3_insert_partition_by_whitespaces.sql
@@ -0,0 +1,26 @@
+INSERT INTO FUNCTION
+   s3(
+       s3_conn
+       filename = currentDatabase() || '/test1.parquet',
+       format = Parquet
+    )
+    PARTITION BY rand() % 10
+SELECT
+    *
+FROM system.numbers
+LIMIT 10;
+
+SELECT * FROM s3(s3_conn, filename = currentDatabase() || '/test1.parquet');
+
+
+INSERT INTO FUNCTION
+   s3(
+       s3_conn
+       filename = currentDatabase() || '/test2.parquet',
+       format = Parquet
+    ) PARTITION BY rand() % 10 SELECT
+    *
+FROM system.numbers
+LIMIT 10;
+
+SELECT * FROM s3(s3_conn, filename = currentDatabase() || '/test2.parquet');
