195587333bfa71c14b8a8aed45f2a57909901df6
Shakhaev Kyamran <shkyga@yandex.ru>
2025-06-01 08:16:32 +0300

    Add no-parallel tag

--- tests/queries/0_stateless/03525_sql_udf_names_in_system_query_log.sh
+++ tests/queries/0_stateless/03525_sql_udf_names_in_system_query_log.sh
@@ -1,8 +1,9 @@
 #!/usr/bin/env bash
+# Tags: no-parallel
 
-CUR_DIR=$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)
+CURDIR=$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)
 # shellcheck source=../shell_config.sh
-. "$CUR_DIR"/../shell_config.sh
+. "$CURDIR"/../shell_config.sh
 
 check_sql_udf_functions() {
     if [ "${#}" == "0" ]; then
