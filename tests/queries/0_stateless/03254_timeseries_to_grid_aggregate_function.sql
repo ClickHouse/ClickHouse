f52b23cb0154f657c06d0ffb485cff40656d8367
Alexander Gololobov <davenger@clickhouse.com>
2025-06-03 13:09:00 +0200

    Test with subsecond step and window params

--- tests/queries/0_stateless/03254_timeseries_to_grid_aggregate_function.sql
+++ tests/queries/0_stateless/03254_timeseries_to_grid_aggregate_function.sql
@@ -107,6 +107,15 @@ SELECT timeSeriesResampleToGridWithStaleness(100::Int32, 150::UInt16, 15::Decima
 SELECT timeSeriesResampleToGridWithStaleness(100, 100, 15, 50)(timestamp::DateTime64(3, 'UTC'), value::Float32) AS res FROM ts_data;
 SELECT timeSeriesResampleToGridWithStalenessIf(100, 150, 15, 50)(timestamp, value, value%2==0) AS res FROM ts_data;
 
+-- Subsecond step and window parameters
+select timeSeriesResampleToGridWithStaleness(
+    '2025-06-01 12:00:00.300'::DateTime64(3, 'UTC'),
+    '2025-06-01 12:00:00.900'::DateTime64(3, 'UTC'),
+    '0.300'::Decimal64(3),
+    '0.500'::Decimal64(3))
+  (['2025-06-01 12:00:00.011', '2025-06-01 12:00:00.768']::Array(DateTime64(3, 'UTC')),
+   [10, 20]::Array(Float64));
+
 SELECT timeSeriesResampleToGridWithStaleness(100, 150, 15, 50)(timestamp, value::Decimal(10,3)) AS res FROM ts_data; -- { serverError ILLEGAL_TYPE_OF_ARGUMENT }
 SELECT timeSeriesResampleToGridWithStaleness(100, 150, 15, 50)(timestamp, value::Int64) AS res FROM ts_data; -- { serverError ILLEGAL_TYPE_OF_ARGUMENT }
 SELECT timeSeriesResampleToGridWithStaleness(100, 150, 15, 50)(timestamp, value::String) AS res FROM ts_data; -- { serverError ILLEGAL_TYPE_OF_ARGUMENT }
