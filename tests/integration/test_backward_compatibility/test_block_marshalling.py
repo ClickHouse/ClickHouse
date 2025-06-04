23110f6ebfc68bb993d84f2612d947fb88720d7c
Nikita Taranov <nikita.taranov@clickhouse.com>
2025-06-04 20:18:03 +0100

    rework test

--- tests/integration/test_backward_compatibility/test_block_marshalling.py
+++ tests/integration/test_backward_compatibility/test_block_marshalling.py
@@ -3,26 +3,23 @@ import pytest
 from helpers.cluster import ClickHouseCluster
 
 cluster = ClickHouseCluster(__file__)
-cluster_name = "parallel_replicas"
 nodes = [
     cluster.add_instance(
-        f"node{num}",
+        "node0",
         main_configs=["configs/clusters.xml"],
         with_zookeeper=False,
         image="library/clickhouse",
         tag="25.1",
         stay_alive=True,
-        use_old_analyzer=True,
         with_installed_binary=True,
     )
-    for num in range(2)
 ] + [
     cluster.add_instance(
-        "node2",
+        f"node{num + 1}",
         main_configs=["configs/clusters.xml"],
         with_zookeeper=False,
-        use_old_analyzer=True,
     )
+    for num in range(2)
 ]
 
 
@@ -36,76 +33,49 @@ def start_cluster():
         cluster.shutdown()
 
 
-def test_query_plan_for_replicas(start_cluster):
+def test_simple_distributed_aggregation_with_parallel_replicas(start_cluster):
     for node in nodes:
-        node.query("create table t (a UInt64) engine = MergeTree order by tuple()")
-        node.query("insert into t select number % 100000 from numbers_mt(1000000)")
+        node.query("drop table if exists t")
+        node.query("create table t (a UInt64) engine = MergeTree() order by tuple()")
+        node.query("insert into t select number % 100000 from numbers_mt(1e7)")
+        node.query("optimize table t final")
 
     # when initiator has an old version
-    assert (
-        nodes[0].query(
-            """
-            explain distributed=1
-            select sum(a)
-            from t
-            """,
-            settings={
-                "cluster_for_parallel_replicas": "parallel_replicas",
-                "max_parallel_replicas": 3,
-                "allow_experimental_parallel_reading_from_replicas": 1,
-                "parallel_replicas_for_non_replicated_merge_tree": 1,
-            },
-        )
-        == """Expression ((Projection + Before ORDER BY))
-  MergingAggregated
-    ReadFromRemoteParallelReplicas (Query: SELECT sum(`a`) FROM `default`.`t` Replicas: node0:9000, node1:9000, node2:9000)
-      Expression ((Projection + Before ORDER BY))
-        Aggregating
-          Expression (Before GROUP BY)
-            ReadFromMergeTree (default.t)
-      Expression ((Projection + Before ORDER BY))
-        Aggregating
-          Expression (Before GROUP BY)
-            ReadFromMergeTree (default.t)
-      Aggregating
-        Expression (Before GROUP BY)
-          ReadFromMergeTree (default.t)
-"""
+    nodes[0].query(
+        """
+        select sum(a)
+        from t
+        group by a
+        format Null
+        """,
+        settings={
+            "cluster_for_parallel_replicas": "parallel_replicas",
+            "enable_analyzer": 1,
+            "allow_experimental_parallel_reading_from_replicas": 1,
+            "max_parallel_replicas": 3,
+            "parallel_replicas_for_non_replicated_merge_tree": 1,
+            "merge_tree_min_bytes_per_task_for_remote_reading": 1,
+            "max_threads": 2,
+        },
     )
 
     # when initiator has a newer version
-    assert (
-        nodes[2].query(
-            """
-            explain distributed=1
-            select sum(a)
-            from t
-            group by a
-            """,
-            settings={
-                "cluster_for_parallel_replicas": "parallel_replicas",
-                "enable_analyzer": 1,
-                "enable_parallel_replicas": 1,
-                "max_parallel_replicas": 3,
-                "parallel_replicas_for_non_replicated_merge_tree": 1,
-            },
-        )
-        == """Expression ((Project names + Projection))
-  MergingAggregated
-    Union
-      Aggregating
-        Expression ((Before GROUP BY + Change column names to column identifiers))
-          ReadFromMergeTree (default.t)
-      ReadFromRemoteParallelReplicas (Query: SELECT sum(`__table1`.`a`) AS `sum(a)` FROM `default`.`t` AS `__table1` GROUP BY `__table1`.`a` Replicas: node0:9000, node1:9000)
-        Expression ((Project names + Projection))
-          Aggregating
-            Expression ((Before GROUP BY + Change column names to column identifiers))
-              ReadFromMergeTree (default.t)
-        Expression ((Project names + Projection))
-          Aggregating
-            Expression ((Before GROUP BY + Change column names to column identifiers))
-              ReadFromMergeTree (default.t)
-"""
+    nodes[2].query(
+        """
+        select sum(a)
+        from t
+        group by a
+        format Null
+        """,
+        settings={
+            "cluster_for_parallel_replicas": "parallel_replicas",
+            "enable_analyzer": 1,
+            "allow_experimental_parallel_reading_from_replicas": 1,
+            "max_parallel_replicas": 3,
+            "parallel_replicas_for_non_replicated_merge_tree": 1,
+            "merge_tree_min_bytes_per_task_for_remote_reading": 1,
+            "max_threads": 2,
+        },
     )
 
     for node in nodes:
