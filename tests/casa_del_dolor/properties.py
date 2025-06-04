f1f9cf8eb2227e031555ed2f5f3c1571242b8c91
Pedro Ferreira <pedro.silva.ferreira@clickhouse.com>
2025-06-04 10:03:26 +0000

    Fixes

--- tests/casa_del_dolor/properties.py
+++ tests/casa_del_dolor/properties.py
@@ -228,10 +228,9 @@ cache_storage_properties = {
     "keep_free_space_size_ratio": threshold_generator(0.2, 0.2, 0.0, 1.0),
     "load_metadata_asynchronously": lambda: random.randint(0, 1),
     "load_metadata_threads": lambda: random.randint(0, multiprocessing.cpu_count()),
-    "max_elements": threshold_generator(0.2, 0.2, 0, 10000000),
+    "max_elements": threshold_generator(0.2, 0.2, 1, 10000000),
     "max_file_segment_size": file_size_value(),
     # "max_size_ratio_to_total_space": threshold_generator(0.2, 0.2, 0.0, 1.0), cannot be specified with `max_size` at the same time
-    "min_bytes_for_seek": threshold_generator(0.2, 0.2, 0, 10 * 1024 * 1024),
     "slru_size_ratio": threshold_generator(0.2, 0.2, 0.0, 1.0),
     "write_cache_per_user_id_directory": lambda: random.randint(0, 1),
 }
