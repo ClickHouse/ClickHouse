939d6d271c544410920c0ed68d6128a5a962e0ef
Igor Nikonov <igor@clickhouse.com>
2025-06-05 11:13:41 +0200

    Fix 02898_parallel_replicas_progress_bar.sql

--- tests/queries/0_stateless/02898_parallel_replicas_progress_bar.sql
+++ tests/queries/0_stateless/02898_parallel_replicas_progress_bar.sql
@@ -26,8 +26,8 @@ SELECT count(), min(k), max(k), avg(k) FROM t1 SETTINGS log_comment='02898_defau
 SYSTEM FLUSH LOGS text_log, query_log;
 SET max_rows_to_read = 0; -- system.text_log can be really big
 SELECT count() > 0 FROM system.text_log
-WHERE query_id in (select query_id from system.query_log where current_database = currentDatabase() AND log_comment='02898_default_190aed82-2423-413b-ad4c-24dcca50f65b')
-    AND message LIKE '%Total rows to read: 3000%' AND event_date > yesterday() SETTINGS enable_parallel_replicas=0;
+WHERE query_id in (select query_id from system.query_log where current_database = currentDatabase() AND log_comment='02898_default_190aed82-2423-413b-ad4c-24dcca50f65b' and event_date >= yesterday())
+    AND message LIKE '%Total rows to read: 3000%' AND event_date >= yesterday();
 
 -- reading in order coordinator
 -- disable parallel_replicas_local_plan since the test relay on traces which only present in case of no local plan
@@ -35,8 +35,8 @@ SELECT k, sipHash64(v) FROM t1 order by k limit 5 offset 998 SETTINGS optimize_r
 
 SYSTEM FLUSH LOGS text_log, query_log;
 SELECT count() > 0 FROM system.text_log
-WHERE query_id in (select query_id from system.query_log where current_database = currentDatabase() AND log_comment='02898_inorder_190aed82-2423-413b-ad4c-24dcca50f65b')
-    AND message LIKE '%Updated total rows to read: added % rows, total 3000 rows%' AND event_date > yesterday() SETTINGS enable_parallel_replicas=0;
+WHERE query_id in (select query_id from system.query_log where current_database = currentDatabase() AND log_comment='02898_inorder_190aed82-2423-413b-ad4c-24dcca50f65b' and event_date >= yesterday())
+    AND message LIKE '%Updated total rows to read: added % rows, total 3000 rows%' AND event_date >= yesterday();
 
 DROP TABLE t1 SYNC;
 DROP TABLE t2 SYNC;
