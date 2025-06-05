6749f22fa395ef6e2c92b5a5bfcf1b8251aaec4e
kssenii <kssenii@clickhouse.com>
2025-06-05 19:03:46 +0200

    Fix test

--- tests/queries/0_stateless/03411_dynamic_resize_filesystem_cache_3.sh
+++ tests/queries/0_stateless/03411_dynamic_resize_filesystem_cache_3.sh
@@ -5,7 +5,7 @@ CUR_DIR=$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)
 # shellcheck source=../shell_config.sh
 . "$CUR_DIR"/../shell_config.sh
 
-# In this test we only increase cache size two times and then decrease it back.
+# In this test we only increase cache size and then decrease it back while making concurrent selects.
 # There is no .reference file, because the purpose of this test
 # is to be run in parallel to other tests, not to produce .reference.
 # `s3_cache` is used for many tests and for all tests in case of s3-storage run.
@@ -24,7 +24,7 @@ $CLICKHOUSE_CLIENT --query "SELECT current_size > 0 FROM system.filesystem_cache
 
 config_path=${CLICKHOUSE_CONFIG_DIR}/config.d/storage_conf.xml
 
-new_max_size=$($CLICKHOUSE_CLIENT --query "SELECT multiply(max_size, 5) FROM system.filesystem_cache_settings WHERE cache_name = '$disk_name'")
+new_max_size=$($CLICKHOUSE_CLIENT --query "SELECT multiply(max_size, 3) FROM system.filesystem_cache_settings WHERE cache_name = '$disk_name'")
 sed -i "s|<max_size>$prev_max_size<\/max_size>|<max_size>$new_max_size<\/max_size>|"  $config_path
 
 function select {
@@ -41,12 +41,22 @@ timeout 5 bash -c select 2>/dev/null &
 timeout 5 bash -c select 2>/dev/null &
 
 $CLICKHOUSE_CLIENT -m --query "
-set send_logs_level='fatal';
 SYSTEM RELOAD CONFIG"
 
 $CLICKHOUSE_CLIENT --query "SELECT sleep(3) FORMAT Null"
 wait
 
-new_max_size=$($CLICKHOUSE_CLIENT --query "SELECT divide(max_size, 2) FROM system.filesystem_cache_settings WHERE cache_name = '$disk_name'")
-sed -i "s|<max_size>$prev_max_size<\/max_size>|<max_size>$new_max_size<\/max_size>|"  $config_path
+sed -i "s|<max_size>$new_max_size<\/max_size>|<max_size>$prev_max_size<\/max_size>|"  $config_path
 
+export -f select
+timeout 5 bash -c select 2>/dev/null &
+timeout 5 bash -c select 2>/dev/null &
+timeout 5 bash -c select 2>/dev/null &
+timeout 5 bash -c select 2>/dev/null &
+timeout 5 bash -c select 2>/dev/null &
+
+$CLICKHOUSE_CLIENT -m --query "
+SYSTEM RELOAD CONFIG"
+
+$CLICKHOUSE_CLIENT --query "SELECT sleep(3) FORMAT Null"
+wait
