2e9d1320f9df4f0c11ab3977d2b86df7f5a7a2ea
Alexander Gololobov <davenger@clickhouse.com>
2025-06-05 09:55:43 +0200

    Add fuzzed test cases

--- tests/queries/0_stateless/03532_window_function_and_null_source_max_threads.sql
+++ tests/queries/0_stateless/03532_window_function_and_null_source_max_threads.sql
@@ -74,6 +74,70 @@ SELECT trimLeft(explain) FROM (
         SETTINGS max_threads = 20, enable_parallel_replicas=0
 ) WHERE explain LIKE '%Resize%' LIMIT 3;
 
+
+-- Same query by with crazy max_threads
+SELECT trimLeft(explain) FROM (
+    EXPLAIN PIPELINE
+        WITH
+            nums AS
+            (
+                SELECT
+                    n1.n AS a,
+                    n2.n AS b
+                FROM empty AS n1, empty AS n2
+                WHERE (n1.n % 7) = (n2.n % 5)
+            ),
+            window1 AS
+            (
+                SELECT
+                    a,
+                    lagInFrame(a, 1, a) OVER (PARTITION BY a ORDER BY a ASC ROWS BETWEEN UNBOUNDED PRECEDING AND CURRENT ROW) AS lag
+                FROM nums
+            ),
+            window2 AS
+            (
+                SELECT
+                    lag,
+                    leadInFrame(lag, 1, a) OVER (PARTITION BY a ORDER BY a ASC ROWS BETWEEN UNBOUNDED PRECEDING AND CURRENT ROW) AS lead
+                FROM window1
+            )
+        SELECT lead
+        FROM window2
+        SETTINGS max_threads = 2000, enable_parallel_replicas=0
+) WHERE explain LIKE '%Resize%' LIMIT 3; -- {serverError LIMIT_EXCEEDED}
+
+
+SELECT 'Same query with max_threads = 300';
+SELECT trimLeft(explain) FROM (
+    EXPLAIN PIPELINE
+        WITH
+            nums AS
+            (
+                SELECT
+                    n1.n AS a,
+                    n2.n AS b
+                FROM empty AS n1, empty AS n2
+                WHERE (n1.n % 7) = (n2.n % 5)
+            ),
+            window1 AS
+            (
+                SELECT
+                    a,
+                    lagInFrame(a, 1, a) OVER (PARTITION BY a ORDER BY a ASC ROWS BETWEEN UNBOUNDED PRECEDING AND CURRENT ROW) AS lag
+                FROM nums
+            ),
+            window2 AS
+            (
+                SELECT
+                    lag,
+                    leadInFrame(lag, 1, a) OVER (PARTITION BY a ORDER BY a ASC ROWS BETWEEN UNBOUNDED PRECEDING AND CURRENT ROW) AS lead
+                FROM window1
+            )
+        SELECT lead
+        FROM window2
+        SETTINGS max_threads = 300, enable_parallel_replicas=0
+) WHERE explain LIKE '%Resize%' LIMIT 1;
+
 DROP TABLE empty;
 
 
