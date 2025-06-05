753347c74343bc7dd3b798e3ce219d30a578f3ab
Azat Khuzhin <a3at.mail@gmail.com>
2025-06-05 11:59:47 +0200

    tests: replace update_lag with now()+X in 02530_dictionaries_update_field

--- tests/queries/0_stateless/02530_dictionaries_update_field_complex_key_hashed.sh
+++ tests/queries/0_stateless/02530_dictionaries_update_field_complex_key_hashed.sh
@@ -8,16 +8,4 @@ CUR_DIR=$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)
 # shellcheck source=./02530_dictionaries_update_field.lib
 . "$CUR_DIR"/02530_dictionaries_update_field.lib
 
-# NOTE: dictionaries will be updated according to server TZ, not session, so prohibit it's randomization
-$CLICKHOUSE_CLIENT --session_timezone '' -q "
-    CREATE TABLE table_for_update_field_dictionary
-    (
-        key UInt64,
-        value String,
-        last_insert_time DateTime
-    )
-    ENGINE = MergeTree()
-    ORDER BY tuple()
-"
-
 run_test_with_layout "complex_key_hashed"
