a8809c41f6a39d022e8c1eeba982993e3829e006
Vico.Wu <583424568@qq.com>
2025-06-06 19:00:26 +0800

    refine logs

--- src/Disks/DiskLocal.cpp
+++ src/Disks/DiskLocal.cpp
@@ -27,6 +27,9 @@
 #include <pcg_random.hpp>
 #include <Common/logger_useful.h>
 
+#include <Poco/Process.h>
+#include <Poco/Pipe.h>
+
 
 namespace CurrentMetrics
 {
@@ -618,7 +621,30 @@ try
 }
 catch (...)
 {
-    LOG_WARNING(logger, "Cannot achieve read over the disk directory: {}", disk_path);
+    try
+    {
+        Poco::Pipe out_pipe;
+        Poco::ProcessHandle ph = Poco::Process::launch("ls", {"-l", disk_path}, nullptr, &out_pipe, nullptr);
+
+        constexpr size_t BUF_SIZE = 4096;
+        char buffer[BUF_SIZE];
+        std::ostringstream ss;
+        int n = 0;
+
+        while ((n = out_pipe.readBytes(buffer, BUF_SIZE)) > 0)
+        {
+            ss.write(buffer, n);
+        }
+
+        ph.wait(); // Ensure child process exits
+
+        LOG_WARNING(logger, "Cannot achieve read over the disk directory: {}. Directory contents:\n{}", disk_path, ss.str());
+    }
+    catch (...)
+    {
+        LOG_WARNING(logger, "Cannot achieve read over the disk directory: {}, and failed to list contents due to internal error.", disk_path);
+    }
+    //LOG_WARNING(logger, "Cannot achieve read over the disk directory: {}", disk_path);
     return false;
 }
 
