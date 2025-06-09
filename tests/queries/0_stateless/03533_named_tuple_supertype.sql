0cd00b63fbd31f5141ec792a601f85a5546b8979
lgbo-ustc <lgbo.ustc@gmail.com>
2025-06-09 09:15:08 +0800

    fix: test failures

--- tests/queries/0_stateless/03533_named_tuple_supertype.sql
+++ tests/queries/0_stateless/03533_named_tuple_supertype.sql
@@ -1,13 +1,13 @@
-create table named_tuples_03533_1 (`a` Tuple(s String, i Int64), `b` Tuple(s String, i Int32)) ENGINE=Memory;
+SET enable_analyzer=1;
+
+CREATE TABLE named_tuples_03533_1 (`a` Tuple(s String, i Int64), `b` Tuple(s String, i Int32)) ENGINE=Memory;
 
 INSERT INTO named_tuples_03533_1 VALUES (('y', 20),('a', 100)), (('x',-10),('b', 10));
 
-select arrayFilter(x -> ((x.i) > 10), [if(a.i > 0, a, b)]) from named_tuples_03533_1;
+SELECT x, toTypeName(x) FROM ( SELECT arrayFilter(x -> ((x.i) > 10), [if(a.i > 0, a, b)]) AS x FROM named_tuples_03533_1 );
 
-
-create table named_tuples_03533_2 (`a` Tuple(s String, i Int64), `b` Tuple(x String, y Int32)) ENGINE=Memory;
+CREATE TABLE named_tuples_03533_2 (`a` Tuple(s String, i Int64), `b` Tuple(x String, y Int32)) ENGINE=Memory;
 
 INSERT INTO named_tuples_03533_2 VALUES (('y', 10),('a', 100)), (('x',-10),('b', 10));
 
-select if(a.i > 0, a, b) from named_tuples_03533_2;
-
+SELECT x, toTypeName(x) FROM ( SELECT if(a.i > 0, a, b) AS x FROM named_tuples_03533_2 );
