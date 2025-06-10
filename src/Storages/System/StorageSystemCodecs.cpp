59ff3067069190ea419c5a7d2e887c464781e11a
Jimmy Aguilar Mena <jimmy.aguilar.mena@clickhouse.com>
2025-06-10 03:58:34 +0200

    Introspect the list of codecs and their properties.
    * src/Storages/System/StorageSystemCodecs.{h,cpp} : New files with the new table class.
    * src/Storages/System/StorageSystemCodecs.{h,cpp} (getMutableColumns) : New function to get columns information accessing the private members in CompressionCodecFactory
    * src/Storages/System/attachSystemTables.cpp : Attach the new table to system.

---
+++ src/Storages/System/StorageSystemCodecs.cpp
@@ -0,0 +1,35 @@
+
+#include <Storages/System/StorageSystemCodecs.h>
+#include <Storages/MergeTree/MergeTreeData.h>
+#include <Columns/ColumnsNumber.h>
+#include <Columns/ColumnString.h>
+#include <Columns/ColumnNullable.h>
+#include <Compression/CompressionFactory.h>
+
+
+namespace DB
+{
+
+ColumnsDescription StorageSystemCodecs::getColumnsDescription()
+{
+    return ColumnsDescription
+    {
+        { "codec",                     std::make_shared<DataTypeString>(), "Codec name."},
+        { "method_byte",               std::make_shared<DataTypeUInt8>(), "Byte which indicates codec in compressed file."},
+        { "compression",               std::make_shared<DataTypeUInt8>(), "The codec is a compression."},
+        { "generic_compression",       std::make_shared<DataTypeUInt8>(), "The codec is a generic compression."},
+        { "encription",                std::make_shared<DataTypeUInt8>(), "The codec encrypts."},
+        { "floating_point_timeseries", std::make_shared<DataTypeUInt8>(), "The codec is for floating point timeseries codec."},
+        { "delta_compression",         std::make_shared<DataTypeUInt8>(), "The codec is for delta compression."},
+        { "experimental",              std::make_shared<DataTypeUInt8>(), "The codec is experimental."},
+    };
+}
+
+void StorageSystemCodecs::fillData(MutableColumns & res_columns, ContextPtr, const ActionsDAG::Node *, std::vector<UInt8>) const
+{
+    CompressionCodecFactory::instance().getMutableColumns(res_columns);
+}
+
+
+
+}
