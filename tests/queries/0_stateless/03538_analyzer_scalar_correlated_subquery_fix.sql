b150985c4f5ad109a7d8bd7d7ebd07e09c670a19
Dmitry Novik <n0vik@clickhouse.com>
2025-06-11 01:54:27 +0200

    Update test to reproduce the issue

--- tests/queries/0_stateless/03538_analyzer_scalar_correlated_subquery_fix.sql
+++ tests/queries/0_stateless/03538_analyzer_scalar_correlated_subquery_fix.sql
@@ -3,11 +3,12 @@ set allow_experimental_correlated_subqueries = 1;
 
 CREATE TABLE partsupp (
     ps_partkey     Int32,
-    ps_suppkey     Int32
+    ps_suppkey     Int32,
+    ps_availqty    Int32
 )
 ORDER BY (ps_partkey, ps_suppkey);
 
-INSERT INTO partsupp (ps_partkey, ps_suppkey) VALUES (114, 115), (369, 7870);
+INSERT INTO partsupp (ps_partkey, ps_suppkey, ps_availqty) VALUES (114, 115, 1), (369, 7870, 1);
 
 CREATE TABLE lineitem (
     l_partkey        Int32,
@@ -29,3 +30,13 @@ SELECT
 FROM partsupp
 WHERE (ps_partkey, ps_suppkey) IN ((114, 115), (369, 7870))
 ORDER BY ps_partkey, ps_suppkey;
+
+SELECT
+    ps_partkey,
+    ps_suppkey
+FROM partsupp
+WHERE ps_availqty > (
+        SELECT 0.5 * sum(l_quantity)
+        FROM lineitem
+        WHERE (l_partkey = ps_partkey) AND (l_suppkey = ps_suppkey)
+    );
