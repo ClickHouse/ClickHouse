a53758d8f816df46f7effd69ff7ecc266ae742c5
Azat Khuzhin <a3at.mail@gmail.com>
2025-06-11 18:52:54 +0200

    tests: fix 03540_system_dashboards under ASan (use curl over client binary)

--- tests/queries/0_stateless/03540_system_dashboards.sh
+++ tests/queries/0_stateless/03540_system_dashboards.sh
@@ -7,9 +7,9 @@ CUR_DIR=$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)
 
 # - replace default with test_shard_localhost (CI does not have default cluster)
 # - replace log_ with log$ to avoid reading all tables matching $log_ pattern, since this also includes distributed tables from ci-logs
-mapfile queries <<<"$($CLICKHOUSE_CLIENT --format LineAsString "SELECT formatQuerySingleLine(replace(replace(query, '(default', '(test_shard_localhost'), '_log', '_log$')) FROM system.dashboards")"
-$CLICKHOUSE_CLIENT "SYSTEM FLUSH LOGS"
+mapfile queries <<<"$($CLICKHOUSE_CURL -sSk "${CLICKHOUSE_URL}&default_format=LineAsString" -d "SELECT formatQuerySingleLine(replace(replace(query, '(default', '(test_shard_localhost'), '_log', '_log$')) FROM system.dashboards")"
+$CLICKHOUSE_CURL -sSk "${CLICKHOUSE_URL}" -d "SYSTEM FLUSH LOGS"
 for q in "${queries[@]}"; do
-  $CLICKHOUSE_CLIENT --param_rounding 60 --param_seconds 600 --format Null "$q" || echo "$q" &
+  $CLICKHOUSE_CURL -sSk "${CLICKHOUSE_URL}&param_rounding=60&param_seconds=600&default_format=Null" -d "$q" || echo "$q" &
 done
 wait
