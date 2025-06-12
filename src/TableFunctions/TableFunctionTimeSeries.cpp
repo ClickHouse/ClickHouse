30a610c76666de31c38c7bdb0621f9c4c876b586
Vitaly Baranov <vitlibar@clickhouse.com>
2025-06-12 15:04:29 +0200

    Add table function timeSeriesSelector().

--- src/TableFunctions/TableFunctionTimeSeries.cpp
+++ src/TableFunctions/TableFunctionTimeSeries.cpp
@@ -8,6 +8,7 @@
 #include <Storages/StorageTimeSeries.h>
 #include <Storages/checkAndGetLiteralArgument.h>
 #include <TableFunctions/TableFunctionFactory.h>
+#include <TableFunctions/TableFunctionTimeSeriesSelector.h>
 
 
 namespace DB
@@ -121,6 +122,12 @@ void registerTableFunctionTimeSeries(TableFunctionFactory & factory)
         {.documentation = {
             .description=R"(Provides direct access to the 'metrics' target table for a specified TimeSeries table.)",
             .examples{{"timeSeriesMetrics", "SELECT * from timeSeriesMetrics('mydb', 'time_series_table');", ""}},
+            .category = FunctionDocumentation::Category::TableFunction}
+        });
+    factory.registerFunction<TableFunctionTimeSeriesSelector>(
+        {.documentation = {
+            .description=R"(Reads time series from a specified TimeSeries table.)",
+            .examples{{"timeSeriesSelector", "SELECT * from timeSeriesSelector('mydb', 'time_series_table', 'http_requests{job=\"prometheus\"}', now() - INTERVAL 10 MINUTES, now());", ""}},
             .category = FunctionDocumentation::Category::TableFunction}
         });
 }
