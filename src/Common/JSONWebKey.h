3af8a9bf58ce52640affd19eee2985b4438a3967
Konstantin Bogdanov <konstantin@clickhouse.com>
2025-06-13 04:34:19 +0200

    More comments

--- src/Common/JSONWebKey.h
+++ src/Common/JSONWebKey.h
@@ -10,10 +10,15 @@ namespace DB
 
 class KeyPair;
 
+/// Represents a JSON Web Key (JWK) for RSA keys.
+/// See RFC 7517.
 struct JSONWebKey
 {
+    /// The public exponent of the RSA key.
     std::string e;
+    /// The modulus of the RSA key.
     std::string n;
+    /// Key type, e.g., "RSA".
     std::string kty;
 
     std::string toString() const;
