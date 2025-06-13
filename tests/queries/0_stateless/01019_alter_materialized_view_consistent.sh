93e66dd57f268f2ec615522ce0f6a74405a95f2a
Vico.Wu <583424568@qq.com>
2025-06-13 16:53:27 +0800

    revert unused change

--- tests/queries/0_stateless/01019_alter_materialized_view_consistent.sh
+++ tests/queries/0_stateless/01019_alter_materialized_view_consistent.sh
@@ -1,5 +1,4 @@
 #!/usr/bin/env bash
-# Tags: no-fasttest, no-debug
 
 set -e
 
@@ -8,95 +7,85 @@ CURDIR=$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)
 . "$CURDIR"/../shell_config.sh
 
 $CLICKHOUSE_CLIENT <<EOF
-DROP TABLE IF EXISTS src_123;
-DROP TABLE IF EXISTS mv_123;
+DROP TABLE IF EXISTS src_a;
+DROP TABLE IF EXISTS src_b;
 
-CREATE TABLE src_123 (v UInt64) ENGINE = Null;
-CREATE MATERIALIZED VIEW mv_123 (v UInt8) Engine = MergeTree() ORDER BY v AS SELECT v FROM src_123;
+DROP TABLE IF EXISTS mv;
+
+CREATE TABLE src_a (v UInt64) ENGINE = Null;
+CREATE TABLE src_b (v UInt64) ENGINE = Null;
+
+CREATE MATERIALIZED VIEW mv (test UInt8, case UInt8)
+Engine = MergeTree()
+ORDER BY test AS
+SELECT v == 1 as test, v as case FROM src_a;
 EOF
 
-STOP_ALTER=0
-total_alter_time=0
-alter_count=0
-total_sleep_time=0
-sleep_count=0
+# The purpose of this test is to ensure that MV query A is always used against source table A. Same for query/table B.
+# Also, helps detect data races.
 
-function alter_thread()
-{
-    trap 'STOP_ALTER=1' SIGINT SIGTERM
+function insert_thread() {
+    # Always wait for all background INSERTs to finish to catch stuck queries.
+    trap 'wait; exit;' INT
 
-    ALTERS[0]="ALTER TABLE mv_123 MODIFY QUERY SELECT v FROM src_123;"
-    ALTERS[1]="ALTER TABLE mv_123 MODIFY QUERY SELECT v * 2 as v FROM src_123;"
+    INSERT[0]="INSERT INTO TABLE src_a VALUES (1);"
+    INSERT[1]="INSERT INTO TABLE src_b VALUES (2);"
 
-    while [[ $STOP_ALTER -eq 0 ]]; do
-        start_alter=$(date +%s%3N)
-        $CLICKHOUSE_CLIENT --allow_experimental_alter_materialized_view_structure=1 -q "${ALTERS[$RANDOM % 2]}"
-        end_alter=$(date +%s%3N)
-
-        duration_alter=$((end_alter - start_alter))
-        echo "[$(date "+%Y-%m-%d %H:%M:%S.%3N")] [ALTER] took ${duration_alter}ms"
-
-        total_alter_time=$((total_alter_time + duration_alter))
-        alter_count=$((alter_count + 1))
-
-        start_sleep=$(date +%s%3N)
-        sleep "$(echo 0.$RANDOM)"
-        end_sleep=$(date +%s%3N)
-
-        duration_sleep=$((end_sleep - start_sleep))
-        echo "[$(date "+%Y-%m-%d %H:%M:%S.%3N")] [SLEEP] took ${duration_sleep}ms"
-
-        total_sleep_time=$((total_sleep_time + duration_sleep))
-        sleep_count=$((sleep_count + 1))
-    done
-
-    echo
-    echo "[$(date "+%Y-%m-%d %H:%M:%S.%3N")] ==================== ALTER Thread Stats ===================="
-    if [[ $alter_count -gt 0 ]]; then
-        echo "[$(date "+%Y-%m-%d %H:%M:%S.%3N")] ALTER Count: $alter_count, Avg Duration: $((total_alter_time / alter_count)) ms"
-    else
-        echo "[$(date "+%Y-%m-%d %H:%M:%S.%3N")] No ALTER executed."
-    fi
-    if [[ $sleep_count -gt 0 ]]; then
-        echo "[$(date "+%Y-%m-%d %H:%M:%S.%3N")] SLEEP Count: $sleep_count, Avg Duration: $((total_sleep_time / sleep_count)) ms"
-    else
-        echo "[$(date "+%Y-%m-%d %H:%M:%S.%3N")] No sleep executed."
-    fi
-    echo "[$(date "+%Y-%m-%d %H:%M:%S.%3N")] ============================================================"
-}
-
-export -f alter_thread;
-
-timeout 10 bash -c alter_thread &
-ALTER_PID=$!
-
-total_insert_time=0
-insert_count=0
-
-for i in {1..100}; do
     while true; do
-        start_insert=$(date +%s%3N)
-        if $CLICKHOUSE_CLIENT -q "INSERT INTO src_123 VALUES (1);" 2>/dev/null; then
-            end_insert=$(date +%s%3N)
-            duration_insert=$((end_insert - start_insert))
-            echo "[$(date "+%Y-%m-%d %H:%M:%S.%3N")] [INSERT #$i] took ${duration_insert}ms"
+        # trigger 50 concurrent inserts at a time
+        for _ in {0..50}; do
+            # ignore `Possible deadlock avoided. Client should retry`
+            $CLICKHOUSE_CLIENT -q "${INSERT[$RANDOM % 2]}" 2>/dev/null &
+        done
+        wait
 
-            total_insert_time=$((total_insert_time + duration_insert))
-            insert_count=$((insert_count + 1))
+        is_done=$($CLICKHOUSE_CLIENT -q "SELECT countIf(case = 1) > 0 AND countIf(case = 2) > 0 FROM mv;")
+
+        if [ "$is_done" -eq "1" ]; then
             break
         fi
     done
-done
+}
 
-if [[ $insert_count -gt 0 ]]; then
-    echo "[$(date "+%Y-%m-%d %H:%M:%S.%3N")] ==================== INSERT Stats ===================="
-    echo "[$(date "+%Y-%m-%d %H:%M:%S.%3N")] INSERT Count: $insert_count, Avg Duration: $((total_insert_time / insert_count)) ms"
-    echo "[$(date "+%Y-%m-%d %H:%M:%S.%3N")] ======================================================"
-fi
+function alter_thread() {
+    trap 'exit' INT
 
-$CLICKHOUSE_CLIENT -q "SELECT count() FROM mv_123;"
+    # Generate random ALTERs, but make sure that at least one of them is for each source table.
+    for i in {0..5}; do
+        ALTER[$i]="ALTER TABLE mv MODIFY QUERY SELECT v == 1 as test, v as case FROM src_a;"
+    done
+    # Insert 3 ALTERs to src_b randomly in each third of array.
+    ALTER[$RANDOM % 2]="ALTER TABLE mv MODIFY QUERY SELECT v == 2 as test, v as case FROM src_b;"
+    ALTER[$RANDOM % 2 + 2]="ALTER TABLE mv MODIFY QUERY SELECT v == 2 as test, v as case FROM src_b;"
+    ALTER[$RANDOM % 2 + 4]="ALTER TABLE mv MODIFY QUERY SELECT v == 2 as test, v as case FROM src_b;"
 
-wait $ALTER_PID || true
+    i=0
+    while true; do
+        $CLICKHOUSE_CLIENT --allow_experimental_alter_materialized_view_structure=1 -q "${ALTER[$i % 6]}"
+        ((i=i+1))
 
-$CLICKHOUSE_CLIENT -q "DROP VIEW mv_123"
-$CLICKHOUSE_CLIENT -q "DROP TABLE src_123"
+        sleep "0.0$RANDOM"
+
+        is_done=$($CLICKHOUSE_CLIENT -q "SELECT countIf(case = 1) > 0 AND countIf(case = 2) > 0 FROM mv;")
+
+        if [ "$is_done" -eq "1" ]; then
+            break
+        fi
+    done
+}
+
+export -f insert_thread;
+export -f alter_thread;
+
+# finishes much faster with all builds, except debug with coverage
+timeout 120 bash -c insert_thread &
+timeout 120 bash -c alter_thread &
+
+wait
+
+$CLICKHOUSE_CLIENT -q "SELECT countIf(case = 1) > 0 AND countIf(case = 2) > 0 FROM mv LIMIT 1;"
+$CLICKHOUSE_CLIENT -q "SELECT 'inconsistencies', count() FROM mv WHERE test == 0;"
+
+$CLICKHOUSE_CLIENT -q "DROP VIEW mv"
+$CLICKHOUSE_CLIENT -q "DROP TABLE src_a"
+$CLICKHOUSE_CLIENT -q "DROP TABLE src_b"
