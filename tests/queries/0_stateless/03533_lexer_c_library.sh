2cda578b93c31b96d1de4befcadccb812ba43ad4
Yakov Olkhovskiy <yakov@clickhouse.com>
2025-06-14 07:35:15 +0000

    fix test

--- tests/queries/0_stateless/03533_lexer_c_library.sh
+++ tests/queries/0_stateless/03533_lexer_c_library.sh
@@ -4,4 +4,4 @@ CUR_DIR=$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)
 # shellcheck source=../shell_config.sh
 . "$CUR_DIR"/../shell_config.sh
 
-lexer_test 'SELECT 1, 2, /* Hello */ "test" AS x'
+"$CUR_DIR"/../../lexer/lexer_test 'SELECT 1, 2, /* Hello */ "test" AS x'
