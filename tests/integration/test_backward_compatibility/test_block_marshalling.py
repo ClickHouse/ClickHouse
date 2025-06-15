b667e9e4fef3a45212d5ef3aaaed9016f593e1ee
Nikita Taranov <nikita.taranov@clickhouse.com>
2025-06-15 21:11:40 +0200

    fix test for private

--- tests/integration/test_backward_compatibility/test_block_marshalling.py
+++ tests/integration/test_backward_compatibility/test_block_marshalling.py
@@ -7,7 +7,7 @@ nodes = [
     cluster.add_instance(
         "node0",
         main_configs=["configs/clusters.xml"],
-        with_zookeeper=False,
+        with_zookeeper=True,
         image="library/clickhouse",
         tag="25.1",
         stay_alive=True,
@@ -17,7 +17,7 @@ nodes = [
     cluster.add_instance(
         f"node{num + 1}",
         main_configs=["configs/clusters.xml"],
-        with_zookeeper=False,
+        with_zookeeper=True,
     )
     for num in range(2)
 ]
@@ -34,9 +34,16 @@ def start_cluster():
 
 
 def test_simple_distributed_aggregation_with_parallel_replicas(start_cluster):
-    for node in nodes:
-        node.query("drop table if exists t")
-        node.query("create table t (a UInt64) engine = MergeTree() order by tuple()")
+    for num in range(len(nodes)):
+        node = nodes[num]
+        node.query("drop table if exists t sync")
+        node.query(
+            f"""
+            create table if not exists t (a UInt64)
+            engine = ReplicatedMergeTree('/test_simple_distributed_aggregation_with_parallel_replicas/shard0/t', '{num}')
+            order by ()
+        """
+        )
         node.query("insert into t select number % 100000 from numbers_mt(1e7)")
         node.query("optimize table t final")
 
@@ -54,7 +61,6 @@ def test_simple_distributed_aggregation_with_parallel_replicas(start_cluster):
             "enable_analyzer": 1,
             "allow_experimental_parallel_reading_from_replicas": 1,
             "max_parallel_replicas": 3,
-            "parallel_replicas_for_non_replicated_merge_tree": 1,
             "merge_tree_min_bytes_per_task_for_remote_reading": 1,
             "max_threads": 2,
         },
@@ -74,11 +80,7 @@ def test_simple_distributed_aggregation_with_parallel_replicas(start_cluster):
             "enable_analyzer": 1,
             "allow_experimental_parallel_reading_from_replicas": 1,
             "max_parallel_replicas": 3,
-            "parallel_replicas_for_non_replicated_merge_tree": 1,
             "merge_tree_min_bytes_per_task_for_remote_reading": 1,
             "max_threads": 2,
         },
     )
-
-    for node in nodes:
-        node.query("drop table t")
