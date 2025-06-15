4a719c2eb460377466d6bb6e75f349974ed77dec
Nikita Taranov <nikita.taranov@clickhouse.com>
2025-06-15 18:42:51 +0200

    impl

--- src/Core/SettingsChangesHistory.cpp
+++ src/Core/SettingsChangesHistory.cpp
@@ -81,6 +81,7 @@ const VersionToSettingsChangesMap & getSettingsChangesHistory()
             {"use_skip_indexes_if_final", 0, 1, "Change in default value of setting"},
             {"use_skip_indexes_if_final_exact_mode", 0, 1, "Change in default value of setting"},
             {"min_outstreams_per_resize_after_split", 0, 24, "New setting."},
+            {"min_joined_block_size_rows", 0, DEFAULT_BLOCK_SIZE, "New setting."},
         });
         addSettingsChanges(settings_changes_history, "25.5",
         {
