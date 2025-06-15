f088b881a4f6ccdd8056131033468e21dff1e925
Alexey Milovidov <milovidov@clickhouse.com>
2025-06-16 00:36:16 +0200

    Add a test

---
+++ tests/queries/0_stateless/03545_http_content_leftovers.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+# Tags: no-parallel
+# - no-parallel - spawning bunch of processes with sanitizers can use significant amount of memory
+
+CUR_DIR=$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)
+# shellcheck source=../shell_config.sh
+. "$CUR_DIR"/../shell_config.sh
+
+${CLICKHOUSE_CURL} -v -sS "${CLICKHOUSE_PORT_HTTP_PROTO}://${CLICKHOUSE_HOST}:${CLICKHOUSE_PORT_HTTP}/js/lz-string.js" 2>&1 | grep -P 'Leftovers after chunking|var LZString'
