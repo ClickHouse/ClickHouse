641600e0c016acee469093e5fe1101507d119a55
Azat Khuzhin <a3at.mail@gmail.com>
2025-06-16 19:41:17 +0200

    tests: use separate server logs in 01507_clickhouse_server_start_with_embedded_config

--- tests/queries/0_stateless/01507_clickhouse_server_start_with_embedded_config.sh
+++ tests/queries/0_stateless/01507_clickhouse_server_start_with_embedded_config.sh
@@ -10,7 +10,7 @@ CURDIR=$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)
 
 echo "Starting clickhouse-server"
 
-$CLICKHOUSE_BINARY server -- --tcp_port "$CLICKHOUSE_PORT_TCP" --path "${CLICKHOUSE_TMP}/" > server.log 2>&1 &
+$CLICKHOUSE_BINARY server -- --tcp_port "$CLICKHOUSE_PORT_TCP" --path "${CLICKHOUSE_TMP}/" > "${CLICKHOUSE_TMP}/server.log" 2>&1 &
 PID=$!
 
 function finish()
@@ -26,7 +26,7 @@ for i in {1..30}; do
     sleep 1
     $CLICKHOUSE_CLIENT --query "SELECT 1" 2>/dev/null && break
     if [[ $i == 30 ]]; then
-        cat server.log
+        cat "${CLICKHOUSE_TMP}/server.log"
         exit 1
     fi
 done
@@ -52,7 +52,7 @@ kill $PID
 # Dump server.log in case wait hangs
 function trace()
 {
-    cat server.log
+    cat "${CLICKHOUSE_TMP}/server.log"
 }
 trap trace EXIT
 wait
