1ea640d082edd49a9647f74336ee4aff2e8570d4
Somrat Dutta <38795369+somratdutta@users.noreply.github.com>
2025-06-17 14:48:29 +0530

    modify cluster.py to support additional_parameters

--- tests/integration/helpers/cluster.py
+++ tests/integration/helpers/cluster.py
@@ -1466,14 +1466,17 @@ class ClickHouseCluster:
         return self.base_iceberg_hms_cmd
 
     def setup_iceberg_catalog_cmd(
-        self, instance, env_variables, docker_compose_yml_dir
+        self, instance, env_variables, docker_compose_yml_dir,extra_parameters=None
     ):
         self.with_iceberg_catalog = True
+        file_name = "docker_compose_iceberg_rest_catalog.yml"
+        if extra_parameters is not None and extra_parameters["docker_compose_file_name"] != "":
+            file_name = extra_parameters["docker_compose_file_name"]
         self.base_cmd.extend(
             [
                 "--file",
                 p.join(
-                    docker_compose_yml_dir, "docker_compose_iceberg_rest_catalog.yml"
+                    docker_compose_yml_dir, file_name
                 ),
             ]
         )
@@ -1481,7 +1484,7 @@ class ClickHouseCluster:
             "--env-file",
             instance.env_file,
             "--file",
-            p.join(docker_compose_yml_dir, "docker_compose_iceberg_rest_catalog.yml"),
+            p.join(docker_compose_yml_dir, file_name),
         )
         return self.base_iceberg_catalog_cmd
 
@@ -1691,7 +1694,8 @@ class ClickHouseCluster:
         use_docker_init_flag=False,
         clickhouse_start_cmd=CLICKHOUSE_START_COMMAND,
         with_dolor=False,
-        storage_opt=None
+        storage_opt=None,
+        extra_parameters=None,
     ) -> "ClickHouseInstance":
         """Add an instance to the cluster.
 
@@ -1826,6 +1830,7 @@ class ClickHouseCluster:
             use_docker_init_flag=use_docker_init_flag,
             with_dolor=with_dolor,
             storage_opt=storage_opt,
+            extra_parameters=extra_parameters,
         )
 
         docker_compose_yml_dir = get_docker_compose_path()
@@ -1984,7 +1989,7 @@ class ClickHouseCluster:
         if with_iceberg_catalog and not self.with_iceberg_catalog:
             cmds.append(
                 self.setup_iceberg_catalog_cmd(
-                    instance, env_variables, docker_compose_yml_dir
+                    instance, env_variables, docker_compose_yml_dir, extra_parameters
                 )
             )
 
@@ -3679,6 +3684,7 @@ class ClickHouseInstance:
         use_docker_init_flag=False,
         with_dolor=False,
         storage_opt=None,
+        extra_parameters=None,
     ):
         self.name = name
         self.base_cmd = cluster.base_cmd
