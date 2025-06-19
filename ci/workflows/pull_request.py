0e5282845487e044b16cf7a705ebe04ec34b0173
Max Kainov <max.kainov@clickhouse.com>
2025-06-19 09:58:21 +0200

    CI: Move test check from pre to post hook

--- ci/workflows/pull_request.py
+++ ci/workflows/pull_request.py
@@ -88,12 +88,12 @@ workflow = Workflow.Config(
         "python3 ./ci/jobs/scripts/workflow_hooks/pr_description.py",
         "python3 ./ci/jobs/scripts/workflow_hooks/version_log.py",
         "python3 ./ci/jobs/scripts/workflow_hooks/quick_sync.py",
-        "python3 ./ci/jobs/scripts/workflow_hooks/new_tests_check.py",
         "python3 ./ci/jobs/scripts/workflow_hooks/team_notifications.py",
     ],
     workflow_filter_hooks=[should_skip_job],
     post_hooks=[
         "python3 ./ci/jobs/scripts/workflow_hooks/feature_docs.py",
+        "python3 ./ci/jobs/scripts/workflow_hooks/new_tests_check.py",
         "python3 ./ci/jobs/scripts/workflow_hooks/can_be_merged.py",
     ],
 )
