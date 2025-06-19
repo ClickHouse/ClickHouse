98a65b01b35e2c1a48ad9f6fc011419eb148ac25
Pavel Kruglov <48961922+Avogar@users.noreply.github.com>
2025-06-19 14:54:01 +0200

    Update tests/queries/0_stateless/03525_transform_null_in_subqeury_with_not_nullable_type.sql
    Co-authored-by: Yarik Briukhovetskyi <114298166+yariks5s@users.noreply.github.com>

--- tests/queries/0_stateless/03525_transform_null_in_subqeury_with_not_nullable_type.sql
+++ tests/queries/0_stateless/03525_transform_null_in_subqeury_with_not_nullable_type.sql
@@ -1,9 +1,10 @@
-select null::Nullable(String) in (select 'abc') settings transform_null_in=1;
-select (null::Nullable(String), 42) in (select 'abc', 42) settings transform_null_in=1;
-select (null::Nullable(String), null::Nullable(UInt32)) in (select 'abc', 42) settings transform_null_in=1;
+set transform_null_in=1;
 
-select (number % 2 ? null : 'abc') in (select 'abc') from numbers(2) settings transform_null_in=1;
-select (number % 2 ? null : 'abc', materialize(42)) in (select 'abc', 42) from numbers(2) settings transform_null_in=1;
-select (number % 2 == 0 ? null : 'abc', number < 2 ? null : 42) in (select 'abc', 42) from numbers(4) settings transform_null_in=1;
+select null::Nullable(String) in (select 'abc');
+select (null::Nullable(String), 42) in (select 'abc', 42);
+select (null::Nullable(String), null::Nullable(UInt32)) in (select 'abc', 42);
 
+select (number % 2 ? null : 'abc') in (select 'abc') from numbers(2);
+select (number % 2 ? null : 'abc', materialize(42)) in (select 'abc', 42) from numbers(2);
+select (number % 2 == 0 ? null : 'abc', number < 2 ? null : 42) in (select 'abc', 42) from numbers(4);
 
