8caf8f9236622f7e9e27ac1e3e59f2d53f997f76
Amos Bird <amosbird@gmail.com>
2025-06-23 03:39:13 +0800

    Better query condition cache

--- tests/queries/0_stateless/02346_text_index_function_search.sql
+++ tests/queries/0_stateless/02346_text_index_function_search.sql
@@ -4,6 +4,7 @@ SET enable_analyzer = 1;
 SET allow_experimental_full_text_index = 1;
 -- Force using skip indexes in planning to proper test with EXPLAIN indexes = 1.
 SET use_skip_indexes_on_data_read = 0;
+SET use_query_condition_cache = 0;
 
 DROP TABLE IF EXISTS tab;
 
