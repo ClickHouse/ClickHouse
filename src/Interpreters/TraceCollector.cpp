e763185a650b926c620cc2e48d2139bb45be71f0
Alexey Milovidov <milovidov@clickhouse.com>
2025-06-22 02:01:39 +0200

    Fix build

--- src/Interpreters/TraceCollector.cpp
+++ src/Interpreters/TraceCollector.cpp
@@ -104,7 +104,9 @@ void TraceCollector::run()
     MemoryTrackerBlockerInThread untrack_lock(VariableContext::Global);
     ReadBufferFromFileDescriptor in(TraceSender::pipe.fds_rw[0]);
 
+#if defined(__ELF__) && !defined(OS_FREEBSD)
     const auto * object = SymbolIndex::instance().thisObject();
+#endif
 
     try
     {
@@ -134,8 +136,10 @@ void TraceCollector::run()
 
                 /// Addresses in the main object will be normalized to the physical file offsets for convenience and security.
                 uintptr_t offset = 0;
+#if defined(__ELF__) && !defined(OS_FREEBSD)
                 if (object && uintptr_t(object->address_begin) <= addr && addr < uintptr_t(object->address_end))
                     offset = uintptr_t(object->address_begin);
+#endif
 
                 trace.emplace_back(static_cast<UInt64>(addr) - offset);
             }
