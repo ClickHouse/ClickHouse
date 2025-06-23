9157734faa71fe145580e462e7f57dd31359e939
Konstantin Bogdanov <konstantin@clickhouse.com>
2025-06-23 21:09:03 +0200

    Fix fasttest
    Signed-off-by: Konstantin Bogdanov <konstantin@clickhouse.com>

--- cmake/libllvmlibc.cmake
+++ cmake/libllvmlibc.cmake
@@ -4,3 +4,5 @@ enable_dummy_launchers_if_needed()
 
 set(LLVM_LIBC_DIR "${CMAKE_BINARY_DIR}/contrib/libllvmlibc-cmake")
 link_directories("${LLVM_LIBC_DIR}")
+
+target_link_libraries(global-libs INTERFACE libllvmlibc)
