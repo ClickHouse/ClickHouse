9e493d2911301a50c98ee797cc82957517742b49
Konstantin Bogdanov <konstantin@clickhouse.com>
2025-06-23 20:41:52 +0200

    Add cmake file
    Signed-off-by: Konstantin Bogdanov <konstantin@clickhouse.com>

---
+++ cmake/libllvmlibc.cmake
@@ -0,0 +1,6 @@
+disable_dummy_launchers_if_needed()
+# add_subdirectory(contrib/libllvmlibc-cmake)
+enable_dummy_launchers_if_needed()
+
+set(LLVM_LIBC_DIR "${CMAKE_BINARY_DIR}/contrib/libllvmlibc-cmake")
+link_directories("${LLVM_LIBC_DIR}")
