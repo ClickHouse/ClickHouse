84edadb5142fb8dbfcd2dd85949d035434da3ae0
Yakov Olkhovskiy <yakov@clickhouse.com>
2025-06-24 12:26:21 +0000

    test

--- tests/queries/0_stateless/03533_lexer_c_library.sh
+++ tests/queries/0_stateless/03533_lexer_c_library.sh
@@ -9,4 +9,6 @@ CUR_DIR=$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)
 # shellcheck source=../shell_config.sh
 . "$CUR_DIR"/../shell_config.sh
 
-$CLICKHOUSE_PROGRAMS_DIR/lexer_test 'SELECT 1, 2, /* Hello */ "test" AS x'
+ls -al $CLICKHOUSE_PROGRAMS_DIR
+
+#$CLICKHOUSE_PROGRAMS_DIR/lexer_test 'SELECT 1, 2, /* Hello */ "test" AS x'
