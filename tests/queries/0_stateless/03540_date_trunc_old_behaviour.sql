26b2e11487baf1d8012e68bf908a4b845172a122
Pavel Kruglov <48961922+Avogar@users.noreply.github.com>
2025-06-25 14:22:28 +0200

    Rename setting in the test

--- tests/queries/0_stateless/03540_date_trunc_old_behaviour.sql
+++ tests/queries/0_stateless/03540_date_trunc_old_behaviour.sql
@@ -1,4 +1,4 @@
-set function_date_trunc_use_datetime64_and_date32_return_type_on_datetime64_and_date32_arguments=0;
+set function_date_trunc_return_type_behavior=1;
 set session_timezone='UTC';
 select dateTrunc('second', '2020-10-10 10:10:10.10'::DateTime64(2)) as result, toTypeName(result);
 select dateTrunc('month', '2020-10-10'::Date32) as result, toTypeName(result);
