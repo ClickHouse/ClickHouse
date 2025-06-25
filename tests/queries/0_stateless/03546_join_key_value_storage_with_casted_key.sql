d5c81c6c16238b4fbddbadc78624081225767890
Pervakov Grigorii <pervakov.grigory@gmail.com>
2025-06-25 15:21:23 +0200

    force new analyzer

--- tests/queries/0_stateless/03546_join_key_value_storage_with_casted_key.sql
+++ tests/queries/0_stateless/03546_join_key_value_storage_with_casted_key.sql
@@ -1,4 +1,6 @@
 -- Tags: use-rocksdb
+-- Old analyzer doesn't support this case
+SET enable_analyzer=1;
 
 CREATE TABLE t0 (c0 Bool) ENGINE = EmbeddedRocksDB PRIMARY KEY (c0);
 INSERT INTO TABLE t0 (c0) VALUES (TRUE);
