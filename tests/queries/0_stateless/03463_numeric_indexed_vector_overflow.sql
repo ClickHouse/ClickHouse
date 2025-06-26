8369e4f8be80e2a646c0c8be77525576c80b6216
rienath <raufs.dunamalijevs@clickhouse.com>
2025-06-26 17:54:12 +0000

    More overflow tests for NumericIndexedVector

--- tests/queries/0_stateless/03463_numeric_indexed_vector_overflow.sql
+++ tests/queries/0_stateless/03463_numeric_indexed_vector_overflow.sql
@@ -43,6 +43,9 @@ SELECT numericIndexedVectorToMap(numericIndexedVectorPointwiseGreater(vec_1, inf
 WITH (SELECT groupNumericIndexedVectorState(uin, value) FROM uin_value_details) AS vec_1
 SELECT numericIndexedVectorToMap(numericIndexedVectorPointwiseGreaterEqual(vec_1, inf)); -- { serverError INCORRECT_DATA }
 
+INSERT INTO uin_value_details (uin, value) values (1, 7.3), (2, 8.3), (3, 0), (4, 0), (5, 0), (6, 100.6543782), (7, inf);
+WITH (SELECT groupNumericIndexedVectorState(uin, value) FROM uin_value_details) AS vec_1
+SELECT numericIndexedVectorToMap(numericIndexedVectorPointwiseGreaterEqual(vec_1, 0)); -- { serverError INCORRECT_DATA }
 
 DROP TABLE uin_value_details;
 
