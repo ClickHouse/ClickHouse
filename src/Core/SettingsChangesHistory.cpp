e7f34dc061b80795d4606c6636906c1f55251bca
haowenfeng <haowenfeng@microsoft.com>
2025-06-26 12:46:43 +0800

    fix comment

--- src/Core/SettingsChangesHistory.cpp
+++ src/Core/SettingsChangesHistory.cpp
@@ -70,8 +70,6 @@ const VersionToSettingsChangesMap & getSettingsChangesHistory()
         addSettingsChanges(settings_changes_history, "25.7",
         {
             {"enable_add_distinct_to_in_subqueries", false, true, "New setting to reduce the size of temporary tables transferred for distributed IN subqueries."},
-            {"format_schema_source", "file", "file", "New setting"},
-            {"format_schema_message_name", "", "", "New setting"},
             {"output_format_parquet_geometadata", false, true, "A new setting to allow to write information about geo columns in parquet metadata and encode columns in WKB format."},
             {"distributed_plan_max_rows_to_broadcast", 20000, 20000, "New experimental setting."},
         });
