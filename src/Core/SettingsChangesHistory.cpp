77a06473769bb38be74fdb7f9da2da281921be5e
Igor Nikonov <igor@clickhouse.com>
2025-06-27 11:04:54 +0200

    Update setting history

--- src/Core/SettingsChangesHistory.cpp
+++ src/Core/SettingsChangesHistory.cpp
@@ -71,6 +71,7 @@ const VersionToSettingsChangesMap & getSettingsChangesHistory()
         {
             {"output_format_parquet_geometadata", false, true, "A new setting to allow to write information about geo columns in parquet metadata and encode columns in WKB format."},
             {"distributed_plan_max_rows_to_broadcast", 20000, 20000, "New experimental setting."},
+            {"parallel_distributed_insert_select", 0, 2, "Enable parallel distributed insert select by default"},
         });
         addSettingsChanges(settings_changes_history, "25.6",
         {
