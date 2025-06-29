ab7d9323f5cbaaf376acfaba74fdfb03ac19a92a
Alexey Milovidov <milovidov@clickhouse.com>
2025-06-29 08:28:37 +0200

    More tests

--- tests/queries/0_stateless/03552_inconsistent_formatting_operator_as_table_function.sql
+++ tests/queries/0_stateless/03552_inconsistent_formatting_operator_as_table_function.sql
@@ -1,3 +1,5 @@
 SELECT * FROM globalIn('a', 1); -- { serverError UNKNOWN_FUNCTION }
 SELECT * FROM plus(1, 2); -- { serverError UNKNOWN_FUNCTION }
 SELECT * FROM negate(x); -- { serverError UNKNOWN_FUNCTION }
+
+SELECT not((SELECT * AND(16)) AND 1);
