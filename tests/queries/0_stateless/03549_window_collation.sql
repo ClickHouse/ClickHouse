578d6525701b1fb950cbbb75be720b7e5fc44bf4
Yakov Olkhovskiy <yakov@clickhouse.com>
2025-06-30 12:22:43 +0000

    fix test

--- tests/queries/0_stateless/03549_window_collation.sql
+++ tests/queries/0_stateless/03549_window_collation.sql
@@ -1,6 +1,8 @@
 -- Tags: no-fasttest
 -- no-fasttest - collations support is disabled for fasttest build
 
+set enable_analyzer = 1;
+
 SELECT rank() OVER (ORDER BY c0) FROM (SELECT '1' c0) v0 QUALIFY rank() OVER (ORDER BY c0 COLLATE 'vi') > 0; -- { serverError BAD_COLLATION }
 SELECT rank() OVER (ORDER BY c0 COLLATE 'vi') FROM (SELECT '1' c0) v0 QUALIFY rank() OVER (ORDER BY c0 COLLATE 'vi') > 0;
 SELECT rank() OVER (ORDER BY c0) FROM (SELECT '1' c0) v0 QUALIFY rank() OVER (ORDER BY c0) > 0;
