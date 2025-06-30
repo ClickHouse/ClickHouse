badfaaa8707e429cf0c632a07149e8f240153424
Igor Nikonov <igor@clickhouse.com>
2025-07-01 00:05:07 +0200

    Fix 02875_parallel_replicas_cluster_all_replicas

--- tests/queries/0_stateless/02875_parallel_replicas_cluster_all_replicas.sql
+++ tests/queries/0_stateless/02875_parallel_replicas_cluster_all_replicas.sql
@@ -7,12 +7,9 @@ SET parallel_replicas_only_with_analyzer = 0;  -- necessary for CI run with disa
 SET enable_parallel_replicas=1, max_parallel_replicas=3, parallel_replicas_for_non_replicated_merge_tree=1;
 SELECT count() FROM clusterAllReplicas('test_cluster_two_shard_three_replicas_localhost', currentDatabase(), tt) settings log_comment='02875_190aed82-2423-413b-ad4c-24dcca50f65b';
 
-SYSTEM FLUSH LOGS text_log, query_log;
+SYSTEM FLUSH LOGS query_log;
 
-SET enable_parallel_replicas=0;
-SET max_rows_to_read = 0; -- system.text_log can be really big
-SELECT count() > 0 FROM system.text_log
-WHERE query_id in (select query_id from system.query_log where current_database = currentDatabase() AND log_comment = '02875_190aed82-2423-413b-ad4c-24dcca50f65b')
-    AND message LIKE '%Parallel reading from replicas is disabled for cluster%';
+SELECT ProfileEvents['ParallelReplicasUsedCount'] FROM system.query_log
+WHERE current_database = currentDatabase() AND type = 'QueryFinish' AND log_comment = '02875_190aed82-2423-413b-ad4c-24dcca50f65b' AND event_date >= yesterday();
 
 DROP TABLE tt;
