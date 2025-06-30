b58444f79ef9723c0d1786860a23d0788b5a7986
Dmitry Novik <n0vik@clickhouse.com>
2025-06-30 22:33:25 +0200

    Fix filter conjuncts removal in StorageMerge

---
+++ tests/queries/0_stateless/03549_analyzer_fix_filter_removal.sql
@@ -0,0 +1,6 @@
+CREATE TABLE m (`key` UInt32) ENGINE = Merge(currentDatabase(), 'a');
+CREATE TABLE b (`key` UInt32, `ID` UInt32) ENGINE = MergeTree ORDER BY key;
+
+CREATE TABLE a1 (`day` Date, `id` UInt32) ENGINE = Distributed('test_cluster_two_shards', currentDatabase(), a1_replicated, id);
+
+SELECT * FROM m INNER JOIN b USING (key) WHERE ID = 1; -- { serverError UNKNOWN_TABLE }
