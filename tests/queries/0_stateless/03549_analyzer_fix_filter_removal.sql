6f57bfbabafabb5496fc89795747385b28711b48
Dmitry Novik <n0vik@clickhouse.com>
2025-07-01 11:02:42 +0200

    Add another error code to the test

--- tests/queries/0_stateless/03549_analyzer_fix_filter_removal.sql
+++ tests/queries/0_stateless/03549_analyzer_fix_filter_removal.sql
@@ -3,4 +3,4 @@ CREATE TABLE b (`key` UInt32, `ID` UInt32) ENGINE = MergeTree ORDER BY key;
 
 CREATE TABLE a1 (`day` Date, `id` UInt32) ENGINE = Distributed('test_cluster_two_shards', currentDatabase(), a1_replicated, id);
 
-SELECT * FROM m INNER JOIN b USING (key) WHERE ID = 1; -- { serverError UNKNOWN_TABLE }
+SELECT * FROM m INNER JOIN b USING (key) WHERE ID = 1; -- { serverError UNKNOWN_TABLE, ALL_CONNECTION_TRIES_FAILED }
