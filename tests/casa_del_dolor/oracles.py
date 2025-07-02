5c27e6df22748149d1ef5fed5adaeee2d2d25f85
Pedro Ferreira <pedro.silva.ferreira@clickhouse.com>
2025-07-02 14:33:28 +0000

    Make sure table is renamed back

--- tests/casa_del_dolor/oracles.py
+++ tests/casa_del_dolor/oracles.py
@@ -24,9 +24,9 @@ class ClickHouseTable:
         self.schema_name = _schema_name
         self.table_name = _table_name
         self.table_engine = _table_engine
-        self.first_hash = ""
+        self.first_hash = None
 
-    def set_first_hash(self, _first_hash: str):
+    def set_first_hash(self, _first_hash: Optional[str]):
         self.first_hash = _first_hash
 
     def get_sql_escaped_full_name(self, rename=None) -> str:
@@ -66,6 +66,7 @@ class ElOraculoDeTablas:
     ) -> Optional[ClickHouseTable]:
         next_node_name: str = random.choice(list(cluster.instances.keys()))
         next_node: ClickHouseInstance = cluster.instances[next_node_name]
+        next_tbl: Optional[ClickHouseTable] = None
         client = Client(
             host=next_node.ip_address, port=9000, command=cluster.client_bin_path
         )
@@ -89,11 +90,8 @@ class ElOraculoDeTablas:
                 tuple(line.split("\t")) for line in tables_str.split("\n") if line
             ]
             random_table: tuple[str, ...] = random.choice(fetched_tables)
-            next_tbl: ClickHouseTable = ClickHouseTable(
+            next_tbl = ClickHouseTable(
                 next_node_name, random_table[0], random_table[1], random_table[2]
-            )
-            logger.info(
-                f"Collecting table {next_tbl.get_sql_escaped_full_name()} hash from node {next_node.name} before shutdown"
             )
 
             # Rename table, so the client generator, won't update it
@@ -101,25 +99,32 @@ class ElOraculoDeTablas:
             client.query(
                 f"RENAME TABLE {next_tbl.get_sql_escaped_full_name()} TO {next_tbl.get_sql_escaped_full_name(self.get_current_table_name())};"
             )
+        except Exception as ex:
+            logger.warn(
+                f"Error occurred while picking a table for hashing before restarting server: {ex}"
+            )
+            return None
+        # Table was renamed, so it has to be renamed back later
+        try:
             # Make sure all replicas are in sync
             if next_tbl.is_shared_or_replicated_merge_tree():
                 client.query(
                     f"SYSTEM SYNC REPLICA {next_tbl.get_sql_escaped_full_name(self.get_current_table_name())};"
                 )
             # Fetch table data and hash it
+            logger.info(
+                f"Collecting table {next_tbl.get_sql_escaped_full_name()} hash from node {next_node.name} before shutdown"
+            )
             next_hash = client.query(
                 next_tbl.get_hash_query(self.get_current_table_name())
             )
-            if not isinstance(next_hash, str) or next_hash == "":
-                return None
-            next_tbl.set_first_hash(next_hash)
-
-            return next_tbl
+            if isinstance(next_hash, str) and next_hash != "":
+                next_tbl.set_first_hash(next_hash)
         except Exception as ex:
             logger.warn(
-                f"Error occurred while picking a table for hashing before restarting server: {ex}"
+                f"Error occurred while processing hash for a table before restarting server: {ex}"
             )
-        return None
+        return next_tbl
 
     def collect_table_hash_after_shutdown(
         self, cluster: ClickHouseCluster, logger, next_tbl: Optional[ClickHouseTable]
@@ -131,29 +136,30 @@ class ElOraculoDeTablas:
                 host=next_node.ip_address, port=9000, command=cluster.client_bin_path
             )
 
-            logger.info(
-                f"Collecting table {next_tbl.get_sql_escaped_full_name()} hash from node {next_node.name} after shutdown"
-            )
-            try:
-                # Fetch table data and hash it
-                next_hash = client.query(
-                    next_tbl.get_hash_query(self.get_current_table_name())
+            if next_tbl.first_hash is not None:
+                logger.info(
+                    f"Collecting table {next_tbl.get_sql_escaped_full_name()} hash from node {next_node.name} after shutdown"
                 )
-            except Exception as ex:
-                logger.warn(
-                    f"Error occurred while picking a table for hashing after restarting server: {ex}"
-                )
-            if isinstance(next_hash, str) and next_hash != next_tbl.first_hash:
-                message: str = (
-                    f"Hash mismatch for table {next_tbl.get_sql_escaped_full_name()}"
-                )
-                logger.warn(message)
-                raise Exception(message)
+                try:
+                    # Fetch table data and hash it
+                    next_hash = client.query(
+                        next_tbl.get_hash_query(self.get_current_table_name())
+                    )
+                except Exception as ex:
+                    logger.warn(
+                        f"Error occurred while hashing a table after restarting server: {ex}"
+                    )
+                if isinstance(next_hash, str) and next_hash != next_tbl.first_hash:
+                    message: str = (
+                        f"Hash mismatch for table {next_tbl.get_sql_escaped_full_name()}"
+                    )
+                    logger.warn(message)
+                    raise Exception(message)
 
             try:
-                # Remove readonly status
+                # Rename back
                 client.query(
                     f"RENAME TABLE {next_tbl.get_sql_escaped_full_name(self.get_current_table_name())} TO {next_tbl.get_sql_escaped_full_name()};"
                 )
             except Exception as ex:
-                logger.warn(f"Error while removing readonly property from table: {ex}")
+                logger.warn(f"Error while renaming table after restarting server: {ex}")
