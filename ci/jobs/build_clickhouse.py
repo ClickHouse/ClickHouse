fc950e1c041157c1f28c465a9b2b008fb068ab26
Pablo Marcos <pablo.marcos.oltra@clickhouse.com>
2025-07-03 16:25:55 +0000

    Clean up leftovers when debugging

--- ci/jobs/build_clickhouse.py
+++ ci/jobs/build_clickhouse.py
@@ -185,12 +185,10 @@ def main():
         if build_type in (BuildTypes.AMD_TIDY, BuildTypes.ARM_TIDY):
             run_shell("clang-tidy-cache stats", "clang-tidy-cache.py --show-stats")
             clang_tidy_cache_log = "./ci/tmp/clang-tidy-cache.log"
-            run_shell("clang-tidy-cache logs file", "ls -lh /tmp/clang-tidy-cache.log")
-            Shell.check(f"cp /tmp/clang-tidy-cache.log {clang_tidy_cache_log}", verbose=True)
-            run_shell("clang-tidy-cache logs file", f"ls -lh {clang_tidy_cache_log}")
+            Shell.check(f"cp /tmp/clang-tidy-cache.log {clang_tidy_cache_log}")
             files.append(clang_tidy_cache_log)
             run_shell("clang-tidy-cache log stats", f'echo "$(grep "exists in cache" {clang_tidy_cache_log} | wc -l) in cache\n'
-                                                           '$(grep "does not exist in cache" {clang_tidy_cache_log} | wc -l) not in cache"')
+                                                    f'$(grep "does not exist in cache" {clang_tidy_cache_log} | wc -l) not in cache"')
         run_shell("Output programs", f"ls -l {build_dir}/programs/", verbose=True)
         Shell.check("pwd")
         res = results[-1].is_ok()
