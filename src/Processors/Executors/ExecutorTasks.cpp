95ad21bed78f874a09246cb0a00e9c9848920f4f
serxa <sergei@clickhouse.com>
2025-07-03 18:48:07 +0000

    fix: pop context local task during downscaling

--- src/Processors/Executors/ExecutorTasks.cpp
+++ src/Processors/Executors/ExecutorTasks.cpp
@@ -248,6 +248,11 @@ void ExecutorTasks::downscale(size_t slot_id)
 {
     std::lock_guard lock(mutex);
 
+    // We should make sure that downscaled thread has no local task inside context.
+    // It is allowed to have tasks in `task_queue` or `fast_task_queue` because they can be stealed by other threads.
+    if (auto * task = executor_contexts[slot_id]->popTask())
+        task_queue.push(task, slot_id);
+
     if (slot_id >= slot_count.size() || slot_count[slot_id] == 0)
         return;
     --slot_count[slot_id];
