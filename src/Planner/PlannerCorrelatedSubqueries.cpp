be7fa324590fc0c91b1ba9132cb76f2106c1c050
Alexander Gololobov <davenger@clickhouse.com>
2025-07-03 13:14:51 +0200

    Fix for merging existing equivalence classes into one

--- src/Planner/PlannerCorrelatedSubqueries.cpp
+++ src/Planner/PlannerCorrelatedSubqueries.cpp
@@ -37,6 +37,7 @@
 #include <string_view>
 #include <unordered_map>
 #include <unordered_set>
+#include <utility>
 
 #include <fmt/format.h>
 
@@ -109,25 +110,69 @@ CorrelatedPlanStepMap buildCorrelatedPlanStepMap(QueryPlan & correlated_query_pl
 
 struct EquivalenceClasses
 {
-    void add(const String & lhs, const String & rhs)
+    void add(const String & a, const String & b)
     {
-        for (auto & equivalence_class : classes)
+        auto & class_a = member_to_class[a];
+        auto & class_b = member_to_class[b];
+
+        if (!class_a && class_b)
         {
-            if (equivalence_class.contains(lhs))
+            /// Add A to existing class B
+            class_a = class_b;
+            class_b->push_back(a);
+        }
+        else if (class_a && !class_b)
+        {
+            /// Add B to exeisting class A
+            class_b = class_a;
+            class_a->push_back(b);
+        }
+        else if (!class_a && !class_b)
+        {
+            /// Both A and B are new, create a class for them
+            auto new_class = std::make_shared<std::list<String>>();
+            new_class->push_back(a);
+            class_a = new_class;
+            if (a != b)
             {
-                equivalence_class.insert(rhs);
-                return;
-            }
-            if (equivalence_class.contains(rhs))
-            {
-                equivalence_class.insert(lhs);
-                return;
+                new_class->push_back(b);
+                class_b = new_class;
             }
         }
-        classes.emplace_back(std::unordered_set<String>{ lhs, rhs });
+        else
+        {
+            /// A and B already belong to the same class?
+            if (class_a == class_b)
+                return;
+
+            /// Merge class of smaller size into bigger one
+            if (class_a->size() < class_b->size())
+                mergeFromTo(class_a, class_b);
+            else
+                mergeFromTo(class_b, class_a);
+        }
     }
 
-    std::vector<std::unordered_set<String>> classes;
+    std::shared_ptr<const std::list<String>> getClass(const String & name) const
+    {
+        auto it = member_to_class.find(name);
+        if (it == member_to_class.end())
+            return {};
+        return it->second;
+    }
+
+private:
+    void mergeFromTo(std::shared_ptr<std::list<String>> class_from, std::shared_ptr<std::list<String>> class_to)
+    {
+        /// For all existing members of class From set their class to To
+        for (const auto & member_from : *class_from)
+            member_to_class[member_from] = class_to;
+        /// Add all elements from class From to class To
+        class_to->splice(class_to->end(), *class_from);
+    }
+
+    /// Elements that belong to the same class will point to the same list of all elements of this class
+    std::unordered_map<String, std::shared_ptr<std::list<String>>> member_to_class;
 };
 
 struct DecorrelationContext
@@ -198,20 +243,17 @@ QueryPlan decorrelateQueryPlan(
             std::vector<std::pair<const ActionsDAG::Node *, const String &>> expression_renamings;
             for (const auto & correlated_column_identifier : context.correlated_subquery.correlated_column_identifiers)
             {
-                for (const auto & equivalence_class : context.equivalence_class_stack.back().classes)
+                auto equivalence_class = context.equivalence_class_stack.back().getClass(correlated_column_identifier);
+                if (equivalence_class)
                 {
-                    if (equivalence_class.contains(correlated_column_identifier))
+                    for (const auto & column_name : *equivalence_class)
                     {
-                        for (const auto & column_name : equivalence_class)
+                        auto it = decorrelated_nodes_names.find(column_name);
+                        if (it != decorrelated_nodes_names.end())
                         {
-                            auto it = decorrelated_nodes_names.find(column_name);
-                            if (it != decorrelated_nodes_names.end())
-                            {
-                                expression_renamings.emplace_back(it->second, correlated_column_identifier);
-                                break;
-                            }
+                            expression_renamings.emplace_back(it->second, correlated_column_identifier);
+                            break;
                         }
-                        break;
                     }
                 }
             }
