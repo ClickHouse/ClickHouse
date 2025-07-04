74c72ffbcc54ba107aee0f22297ed9620f1d08b1
Michael Stetsyuk <michael.stetsyuk@clickhouse.com>
2025-07-04 14:17:55 +0000

    minor fxs

--- tests/queries/0_stateless/03561_logging_internal_queries.sql
+++ tests/queries/0_stateless/03561_logging_internal_queries.sql
@@ -7,16 +7,18 @@ CREATE DICTIONARY test_logging_internal_queries_dict
 PRIMARY KEY name
 SOURCE(
     CLICKHOUSE(
-        QUERY 'SELECT toString(number) AS name, toFloat64(number) AS value FROM system.functions LIMIT 10'
+        QUERY 'SELECT toString(number) AS name, toFloat64(number) AS value FROM system.numbers LIMIT 10'
     )
 )
 LIFETIME(30)
 LAYOUT(HASHED());
 
+SYSTEM RELOAD DICTIONARY test_logging_internal_queries_dict;
+
 SYSTEM FLUSH LOGS;
 
 SELECT countIf(type = 'QueryStart') > 0, countIf(type = 'QueryFinish') > 0
 FROM system.query_log
-WHERE is_internal = 1
+WHERE is_internal = 1;
 
 DROP DICTIONARY test_logging_internal_queries_dict;
