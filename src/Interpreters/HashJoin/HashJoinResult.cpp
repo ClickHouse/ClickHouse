fd39504dda616145cd1d2c1e3839d69bb966f690
Nikolai Kochetov <nk@clickhouse.com>
2025-07-05 19:15:58 +0000

    Fixing style.

--- src/Interpreters/HashJoin/HashJoinResult.cpp
+++ src/Interpreters/HashJoin/HashJoinResult.cpp
@@ -4,6 +4,11 @@
 namespace DB
 {
 
+namespace ErrorCodes
+{
+    extern const int LOGICAL_ERROR;
+}
+
 static void correctNullabilityInplace(ColumnWithTypeAndName & column, bool nullable)
 {
     if (nullable)
