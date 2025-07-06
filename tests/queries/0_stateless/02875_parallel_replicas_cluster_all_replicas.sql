47185a89583d632f53cf51bf2c63e08a6e7bc576
Igor Nikonov <igor@clickhouse.com>
2025-07-06 21:32:25 +0200

    Update 02875_parallel_replicas_cluster_all_replicas

--- tests/queries/0_stateless/02875_parallel_replicas_cluster_all_replicas.sql
+++ tests/queries/0_stateless/02875_parallel_replicas_cluster_all_replicas.sql
@@ -9,8 +9,9 @@ SELECT count() FROM clusterAllReplicas('test_cluster_two_shard_three_replicas_lo
 
 SYSTEM FLUSH LOGS query_log;
 
-SET parallel_replicas_for_non_replicated_merge_tree=0;
-SELECT ProfileEvents['ParallelReplicasUsedCount'] FROM system.query_log
-WHERE current_database = currentDatabase() AND type = 'QueryFinish' AND log_comment = '02875_190aed82-2423-413b-ad4c-24dcca50f65b' AND event_date >= yesterday();
+SELECT countIf(ProfileEvents['ParallelReplicasQueryCount']>0) FROM system.query_log
+WHERE type = 'QueryFinish' AND event_date >= yesterday()
+AND initial_query_id IN (select query_id from system.query_log where current_database = currentDatabase() AND type = 'QueryFinish' AND event_date >= yesterday() AND log_comment = '02875_190aed82-2423-413b-ad4c-24dcca50f65b')
+SETTINGS parallel_replicas_for_non_replicated_merge_tree=0;
 
 DROP TABLE tt;
