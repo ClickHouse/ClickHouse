83b490f459c24272d6470f012486fb679942b92c
f2quantum <fanziqi.local@bytedance.com>
2025-07-08 01:44:11 +0800

    fix logic && modify test-case

--- tests/queries/0_stateless/03562_workload_crash_with_max_threads.sh
+++ tests/queries/0_stateless/03562_workload_crash_with_max_threads.sh
@@ -1,4 +1,5 @@
 #!/usr/bin/env bash
+# Tags: no-parallel
 
 CUR_DIR=$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)
 # shellcheck source=../shell_config.sh
@@ -6,18 +7,18 @@ CUR_DIR=$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)
 
 # create or replace resource and workload
 $CLICKHOUSE_CLIENT -nm -q "
-    CREATE OR REPLACE RESOURCE worker_cpu (WORKER THREAD);
+    CREATE OR REPLACE RESOURCE 03562_worker_cpu (WORKER THREAD);
 
-    CREATE OR REPLACE RESOURCE master_cpu (MASTER THREAD);
+    CREATE OR REPLACE RESOURCE 03562_master_cpu (MASTER THREAD);
 
-    CREATE OR REPLACE WORKLOAD all
-    SETTINGS max_concurrent_threads = 16 FOR worker_cpu,
-            max_concurrent_threads = 32 FOR master_cpu;
+    CREATE OR REPLACE WORKLOAD 03562_wl
+    SETTINGS max_concurrent_threads = 16 FOR 03562_worker_cpu,
+            max_concurrent_threads = 32 FOR 03562_master_cpu;
 "
 
 # run benchmark with max_threads and workload
 $CLICKHOUSE_BENCHMARK --randomize --timelimit 10 --continue_on_errors --concurrency 10 >& /dev/null <<EOL
-SELECT sum(number) FROM numbers(100000000) settings max_threads=1, workload = 'all';
+SELECT sum(number) FROM numbers(100000000) settings max_threads=1, workload = '03562_wl';
 EOL
 
 # server is alive
@@ -25,7 +26,7 @@ $CLICKHOUSE_CLIENT -q "SELECT 1 FORMAT Null"
 
 # clean resource and workload
 $CLICKHOUSE_CLIENT -nm -q "
-    DROP WORKLOAD all;
-    DROP RESOURCE worker_cpu;
-    DROP RESOURCE master_cpu;
+    DROP WORKLOAD 03562_wl;
+    DROP RESOURCE 03562_worker_cpu;
+    DROP RESOURCE 03562_master_cpu;
 "
