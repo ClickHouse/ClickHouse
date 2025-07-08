4ee9d91d73e7461f0fc773cf1b0c468d57bd74a0
Vasily Nemkov <V.Nemkov@gmail.com>
2025-07-08 15:22:04 +0200

    Merge pull request #886 from Altinity/25.3_fix_stateless_test_play
    Antalya 25.3: Attempt to fix tests/queries/0_stateless/01528_play.sh

--- tests/queries/0_stateless/01528_play.sh
+++ tests/queries/0_stateless/01528_play.sh
@@ -4,4 +4,4 @@ CURDIR=$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)
 # shellcheck source=../shell_config.sh
 . "$CURDIR"/../shell_config.sh
 
-${CLICKHOUSE_CURL} -sS "${CLICKHOUSE_PORT_HTTP_PROTO}://${CLICKHOUSE_HOST}:${CLICKHOUSE_PORT_HTTP}/play" | grep -o -F 'clickhouse.com'
+${CLICKHOUSE_CURL} -sS "${CLICKHOUSE_PORT_HTTP_PROTO}://${CLICKHOUSE_HOST}:${CLICKHOUSE_PORT_HTTP}/play" | grep -o -F '<link rel="icon" href="data:image/svg+xml;base64,PHN2ZyB'
