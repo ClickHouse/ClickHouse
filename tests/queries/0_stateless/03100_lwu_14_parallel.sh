d8ce35711f6171f97e119047dd74b82aec111e16
Anton Popov <anton@clickhouse.com>
2025-07-08 16:00:25 +0000

    fix tests

--- tests/queries/0_stateless/03100_lwu_14_parallel.sh
+++ tests/queries/0_stateless/03100_lwu_14_parallel.sh
@@ -1,4 +1,6 @@
 #!/usr/bin/env bash
+# Tags: no-replicated-database
+# no-replicated-database - path in zookeeper differs with replicated database
 
 CURDIR=$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)
 # shellcheck source=../shell_config.sh
