2858f9ca745468646856758f22bafbe591f30b98
MikhailBurdukov <burdukvmikhail@gmail.com>
2025-07-08 14:30:26 +0000

    style

--- src/Core/Settings.cpp
+++ src/Core/Settings.cpp
@@ -6655,7 +6655,7 @@ Note that initially (24.12) there was a server setting (`send_settings_to_client
 File/S3 engines/table function will parse paths with '::' as `<archive> :: <file>` if the archive has correct extension.
 )", 0) \
     DECLARE(S3UriStyle, s3_uri_style, S3UriStyle::AUTO, R"(
-Force the s3 endpoint style. Possible valuse: auto, virtual_hosted, path.
+Force the s3 endpoint style. Possible values: auto, virtual_hosted, path.
 )", 0) \
     DECLARE(Milliseconds, low_priority_query_wait_time_ms, 1000, R"(
 When the query prioritization mechanism is employed (see setting `priority`), low-priority queries wait for higher-priority queries to finish. This setting specifies the duration of waiting.
