a4fb3c09110b7fb9c7bf22aa4cbe48cb09259c72
Mithun P <mithunputhusseri@gmail.com>
2025-07-08 20:13:13 +0530

    Test fix

--- tests/queries/0_stateless/03545_union_allow_column_with_no_common_type.sql
+++ tests/queries/0_stateless/03545_union_allow_column_with_no_common_type.sql
@@ -1,3 +1,4 @@
+set use_variant_as_common_type=1;
 SELECT 1 AS x UNION ALL SELECT 'Hello' ORDER BY 'x';
 SELECT x FROM (SELECT 1 AS x, 1 AS ord UNION ALL SELECT 'Hello', 2 AS ord) ORDER BY ord;
 SELECT x FROM (SELECT 1 AS x, 1 AS ord UNION ALL SELECT 'Hello', 2 AS ord UNION ALL SELECT 3.14 AS x, 3 AS ord) ORDER BY ord;
