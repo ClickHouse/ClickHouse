a086668a36627ae840ae7cc0b6088e35d0c1060a
f2quantum <fanziqi.local@bytedance.com>
2025-07-08 17:31:44 +0800

    try fix ci

--- tests/queries/0_stateless/03562_workload_crash_with_max_threads.sh
+++ tests/queries/0_stateless/03562_workload_crash_with_max_threads.sh
@@ -7,13 +7,8 @@ CUR_DIR=$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)
 
 # create or replace resource and workload
 $CLICKHOUSE_CLIENT -nm -q "
-    CREATE OR REPLACE RESOURCE 03562_worker_cpu (WORKER THREAD);
-
-    CREATE OR REPLACE RESOURCE 03562_master_cpu (MASTER THREAD);
-
-    CREATE OR REPLACE WORKLOAD 03562_wl
-    SETTINGS max_concurrent_threads = 16 FOR 03562_worker_cpu,
-            max_concurrent_threads = 32 FOR 03562_master_cpu;
+    CREATE OR REPLACE RESOURCE 03562_worker_cpu (WORKER THREAD, MASTER THREAD);
+    CREATE OR REPLACE WORKLOAD 03562_wl SETTINGS max_concurrent_threads = 1;
 "
 
 # run benchmark with max_threads and workload
@@ -24,9 +19,8 @@ EOL
 # server is alive
 $CLICKHOUSE_CLIENT -q "SELECT 1"
 
-# clean resource and workload
+# clean up resource and workload
 $CLICKHOUSE_CLIENT -nm -q "
     DROP WORKLOAD 03562_wl;
     DROP RESOURCE 03562_worker_cpu;
-    DROP RESOURCE 03562_master_cpu;
 "
