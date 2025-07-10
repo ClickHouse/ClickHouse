a5b486fc93c3415c8d094cfd4bd7ec25f0e462da
MikhailBurdukov <burdukvmikhail@gmail.com>
2025-07-10 14:27:27 +0000

    Enable Date/Date32 as integers in JSON input format

--- src/DataTypes/Serializations/SerializationDate32.cpp
+++ src/DataTypes/Serializations/SerializationDate32.cpp
@@ -83,8 +83,13 @@ void SerializationDate32::serializeTextJSON(const IColumn & column, size_t row_n
     writeChar('"', ostr);
 }
 
-void SerializationDate32::deserializeTextJSON(IColumn & column, ReadBuffer & istr, const FormatSettings &) const
+void SerializationDate32::deserializeTextJSON(IColumn & column, ReadBuffer & istr, const FormatSettings & format_settings) const
 {
+    if (!checkChar('"', istr))
+    {
+        SerializationNumber<Int32>::deserializeTextJSON(column, istr, format_settings);
+        return;
+    }
     ExtendedDayNum x;
     assertChar('"', istr);
     readDateText(x, istr, time_zone);
@@ -92,8 +97,11 @@ void SerializationDate32::deserializeTextJSON(IColumn & column, ReadBuffer & ist
     assert_cast<ColumnInt32 &>(column).getData().push_back(x);
 }
 
-bool SerializationDate32::tryDeserializeTextJSON(IColumn & column, ReadBuffer & istr, const FormatSettings &) const
+bool SerializationDate32::tryDeserializeTextJSON(IColumn & column, ReadBuffer & istr, const FormatSettings & format_settings) const
 {
+    if (!checkChar('"', istr))
+        return SerializationNumber<Int32>::tryDeserializeTextJSON(column, istr, format_settings);
+
     ExtendedDayNum x;
     if (!checkChar('"', istr) || !tryReadDateText(x, istr, time_zone) || !checkChar('"', istr))
         return false;
