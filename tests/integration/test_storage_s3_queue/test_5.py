5e5d17ad4ece05e4364ba1c5be778a5e6167a15a
Kseniia Sumarokova <54203879+kssenii@users.noreply.github.com>
2025-07-11 10:38:58 +0200

    Fix test

--- tests/integration/test_storage_s3_queue/test_5.py
+++ tests/integration/test_storage_s3_queue/test_5.py
@@ -877,9 +877,9 @@ def test_shutdown_logs(started_cluster):
 
 def test_shutdown_order(started_cluster):
     node = started_cluster.instances["instance"]
-    table_name = f"test_shutdown_order_{uuid.uuid4()}"
+    table_name = f"test_shutdown_order_{generate_random_string()}"
     dst_table_name = f"a_{table_name}_dst"
-    keeper_path = f"/clickhouse/test_{table_name}_{generate_random_string()}"
+    keeper_path = f"/clickhouse/test_{table_name}}"
     files_path = f"{table_name}_data"
 
     format = "column1 Int32, column2 String"
