a9c1a192d4fb7b49642334a3b993ae339c6c3152
Konstantin Vedernikov <konstantinvedernikov@Konstantins-MacBook-Pro.local>
2025-07-12 19:47:32 +0200

    start

--- src/Storages/ObjectStorage/StorageObjectStorage.h
+++ src/Storages/ObjectStorage/StorageObjectStorage.h
@@ -285,6 +285,8 @@ public:
         throw Exception(ErrorCodes::NOT_IMPLEMENTED, "Method getDataLakeSettings() is not implemented for configuration type {}", getTypeName());
     }
 
+    virtual std::shared_ptr<void> getOpaque() const { return nullptr; }
+
     String format = "auto";
     String compression_method = "auto";
     String structure = "auto";
