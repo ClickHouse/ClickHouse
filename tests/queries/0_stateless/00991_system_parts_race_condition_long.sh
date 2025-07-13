388afbdf0412e9ff3d61641446942db788e69d44
Alexey Milovidov <milovidov@clickhouse.com>
2025-07-13 22:55:57 +0200

    One step back

--- tests/queries/0_stateless/00991_system_parts_race_condition_long.sh
+++ tests/queries/0_stateless/00991_system_parts_race_condition_long.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
-# Tags: race, no-parallel, no-flaky-check
-# no-parallel because we run many concurrent mutations, which may break other tests by delaying their mutations for a long time.
-# no-flaky-check: some queries are too long with Thread Fuzzer
+# Tags: race, no-parallel
+# no-parallel because we run many concurrent mutations, which may break other tests by delaying their
+# mutations for a long time.
 
 CURDIR=$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)
 # shellcheck source=../shell_config.sh
@@ -15,77 +15,61 @@ $CLICKHOUSE_CLIENT -q "CREATE TABLE alter_table (a UInt8, b Int16, c Float32, d 
 function thread1()
 {
     # NOTE: database = $CLICKHOUSE_DATABASE is unwanted
-    local TIMELIMIT=$((SECONDS+TIMEOUT))
-    while [ $SECONDS -lt "$TIMELIMIT" ]
-    do
-        $CLICKHOUSE_CLIENT --query "SELECT * FROM system.parts FORMAT Null"
-    done
+    while true; do $CLICKHOUSE_CLIENT --query "SELECT * FROM system.parts FORMAT Null"; done
 }
 
 function thread2()
 {
-    local TIMELIMIT=$((SECONDS+TIMEOUT))
-    while [ $SECONDS -lt "$TIMELIMIT" ]
-    do
-        $CLICKHOUSE_CLIENT --send-logs-level fatal --query "
-            ALTER TABLE alter_table ADD COLUMN $1 String DEFAULT '0';
-            ALTER TABLE alter_table MODIFY COLUMN $1 UInt64;
-            ALTER TABLE alter_table DROP COLUMN $1;" 2>/dev/null
-    done
+    while true; do $CLICKHOUSE_CLIENT --query "ALTER TABLE alter_table ADD COLUMN h String '0'; ALTER TABLE alter_table MODIFY COLUMN h UInt64; ALTER TABLE alter_table DROP COLUMN h;"; done
 }
 
 function thread3()
 {
-    local TIMELIMIT=$((SECONDS+TIMEOUT))
-    while [ $SECONDS -lt "$TIMELIMIT" ]
-    do
-        $CLICKHOUSE_CLIENT -q "INSERT INTO alter_table (a, b, c, d, e, f, g) SELECT rand(1), rand(2), 1 / rand(3), toString(rand(4)), [rand(5), rand(6)], rand(7) % 2 ? NULL : generateUUIDv4(), (rand(8), rand(9)) FROM numbers(100000)"
-    done
+    while true; do $CLICKHOUSE_CLIENT -q "INSERT INTO alter_table SELECT rand(1), rand(2), 1 / rand(3), toString(rand(4)), [rand(5), rand(6)], rand(7) % 2 ? NULL : generateUUIDv4(), (rand(8), rand(9)) FROM numbers(100000)"; done
 }
 
 function thread4()
 {
-    local TIMELIMIT=$((SECONDS+TIMEOUT))
-    while [ $SECONDS -lt "$TIMELIMIT" ]
-    do
-        $CLICKHOUSE_CLIENT -q "OPTIMIZE TABLE alter_table FINAL"
-    done
+    while true; do $CLICKHOUSE_CLIENT -q "OPTIMIZE TABLE alter_table FINAL"; done
 }
 
 function thread5()
 {
-    local TIMELIMIT=$((SECONDS+TIMEOUT))
-    while [ $SECONDS -lt "$TIMELIMIT" ]
-    do
-        $CLICKHOUSE_CLIENT -q "ALTER TABLE alter_table DELETE WHERE rand() % 2 = 1"
-    done
+    while true; do $CLICKHOUSE_CLIENT -q "ALTER TABLE alter_table DELETE WHERE rand() % 2 = 1"; done
 }
+
+# https://stackoverflow.com/questions/9954794/execute-a-shell-function-with-timeout
+export -f thread1;
+export -f thread2;
+export -f thread3;
+export -f thread4;
+export -f thread5;
 
 TIMEOUT=30
 
-thread1 &
-thread2 h &
-thread3 &
-thread4 &
-thread5 &
+timeout $TIMEOUT bash -c thread1 2> /dev/null &
+timeout $TIMEOUT bash -c thread2 2> /dev/null &
+timeout $TIMEOUT bash -c thread3 2> /dev/null &
+timeout $TIMEOUT bash -c thread4 2> /dev/null &
+timeout $TIMEOUT bash -c thread5 2> /dev/null &
 
-thread1 &
-thread2 h &
-thread3 &
-thread4 &
-thread5 &
+timeout $TIMEOUT bash -c thread1 2> /dev/null &
+timeout $TIMEOUT bash -c thread2 2> /dev/null &
+timeout $TIMEOUT bash -c thread3 2> /dev/null &
+timeout $TIMEOUT bash -c thread4 2> /dev/null &
+timeout $TIMEOUT bash -c thread5 2> /dev/null &
 
-thread1 &
-thread2 h &
-thread3 &
-thread4 &
-thread5 &
+timeout $TIMEOUT bash -c thread1 2> /dev/null &
+timeout $TIMEOUT bash -c thread2 2> /dev/null &
+timeout $TIMEOUT bash -c thread3 2> /dev/null &
+timeout $TIMEOUT bash -c thread4 2> /dev/null &
+timeout $TIMEOUT bash -c thread5 2> /dev/null &
 
-thread1 &
-thread2 h &
-thread3 &
-thread4 &
-thread5 &
+timeout $TIMEOUT bash -c thread1 2> /dev/null &
+timeout $TIMEOUT bash -c thread2 2> /dev/null &
+timeout $TIMEOUT bash -c thread3 2> /dev/null &
+timeout $TIMEOUT bash -c thread4 2> /dev/null &
+timeout $TIMEOUT bash -c thread5 2> /dev/null &
 
 
 wait
