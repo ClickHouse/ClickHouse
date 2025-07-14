adee6ef109f39b48e0c74239db007bab950e97af
Fgrtue <fgrtue@gmail.com>
2025-07-14 12:45:53 +0200

    Changed the tests for colorSRGBToOkLCH, now rounding numbers up to 6 digit

--- tests/queries/0_stateless/03561_colorSRGBToOkLCH.sql
+++ tests/queries/0_stateless/03561_colorSRGBToOkLCH.sql
@@ -5,27 +5,31 @@ SELECT colorSRGBToOkLCH((1, 2)); -- { serverError ILLEGAL_TYPE_OF_ARGUMENT }
 SELECT colorSRGBToOkLCH((1, 'a', 3)); -- { serverError ILLEGAL_TYPE_OF_ARGUMENT }
 SELECT colorSRGBToOkLCH((1, 2, 3), 'a'); -- { serverError ILLEGAL_TYPE_OF_ARGUMENT }
 
+SET allow_experimental_object_type = 1;
+DROP FUNCTION IF EXISTS tup_round6;
+CREATE FUNCTION tup_round6 AS (t) -> tuple(round(t.1, 6), round(t.2, 6), round(t.3, 6));
+
 SELECT '--- Regular calls';
-SELECT colorSRGBToOkLCH((0, 0, 0));
-SELECT colorSRGBToOkLCH((255, 0, 0));
-SELECT colorSRGBToOkLCH((0, 255, 0));
-SELECT colorSRGBToOkLCH((0, 0, 255));
-SELECT colorSRGBToOkLCH((1, 2, 3)); 
-SELECT colorSRGBToOkLCH((15, 241, 63)); 
-SELECT colorSRGBToOkLCH((129, 87, 220)); 
+SELECT tup_round6(colorSRGBToOkLCH((0, 0, 0)));
+SELECT tup_round6(colorSRGBToOkLCH((255, 0, 0)));
+SELECT tup_round6(colorSRGBToOkLCH((0, 255, 0)));
+SELECT tup_round6(colorSRGBToOkLCH((0, 0, 255)));
+SELECT tup_round6(colorSRGBToOkLCH((1, 2, 3))); 
+SELECT tup_round6(colorSRGBToOkLCH((15, 241, 63)));
+SELECT tup_round6(colorSRGBToOkLCH((129, 87, 220)));
 
 SELECT '--- Varying gamma';
-SELECT colorSRGBToOkLCH((128, 64, 32), 1.0);
-SELECT colorSRGBToOkLCH((128, 64, 32), 1.8);
-SELECT colorSRGBToOkLCH((128, 64, 32), 2.2);
-SELECT colorSRGBToOkLCH((128, 64, 32), 2.4);
-SELECT colorSRGBToOkLCH((128, 64, 32), 3.0);
+SELECT tup_round6(colorSRGBToOkLCH((128, 64, 32), 1.0));
+SELECT tup_round6(colorSRGBToOkLCH((128, 64, 32), 1.8));
+SELECT tup_round6(colorSRGBToOkLCH((128, 64, 32), 2.2));
+SELECT tup_round6(colorSRGBToOkLCH((128, 64, 32), 2.4));
+SELECT tup_round6(colorSRGBToOkLCH((128, 64, 32), 3.0));
 
 SELECT '--- Edge case colors';
-SELECT colorSRGBToOkLCH((128, 128, 128));
-SELECT colorSRGBToOkLCH((0.628, 0.2577, 29.2));
-SELECT colorSRGBToOkLCH((-53, -134, -180));
-SELECT colorSRGBToOkLCH((53, 134, 180), 0);
-SELECT colorSRGBToOkLCH((53, 134, 180), -1000);
-SELECT colorSRGBToOkLCH((53, 134, 180), 1000);
-SELECT colorSRGBToOkLCH((1e-3, 1e-6, 180));
+SELECT tup_round6(colorSRGBToOkLCH((128, 128, 128)));
+SELECT tup_round6(colorSRGBToOkLCH((0.628, 0.2577, 29.2)));
+SELECT tup_round6(colorSRGBToOkLCH((-53, -134, -180)));
+SELECT tup_round6(colorSRGBToOkLCH((53, 134, 180), 0));
+SELECT tup_round6(colorSRGBToOkLCH((53, 134, 180), -1000));
+SELECT tup_round6(colorSRGBToOkLCH((53, 134, 180), 1000));
+SELECT tup_round6( colorSRGBToOkLCH((1e-3, 1e-6, 180)));
