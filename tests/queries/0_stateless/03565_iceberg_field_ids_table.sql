779c62345e857bd342f4351a58fb8664da9e5936
Konstantin Vedernikov <vedernikovkostya@mail.ru>
2025-07-14 08:47:58 +0000

    update test

--- tests/queries/0_stateless/03565_iceberg_field_ids_table.sql
+++ tests/queries/0_stateless/03565_iceberg_field_ids_table.sql
@@ -1,4 +1,4 @@
 -- Tags: no-fasttest
 -- Tag no-fasttest: Depends on AWS
 
-SELECT * FROM icebergS3(s3_conn, filename='field_ids_table_test', SETTINGS iceberg_metadata_table_uuid = 'd1effb24-3d84-4d18-ae2c-b6d3cacc574a') ORDER BY ALL;
+SELECT * FROM icebergS3(s3_conn, filename='field_ids_table_test', SETTINGS iceberg_metadata_table_uuid = '8f1f9ae2-18bb-421e-b640-ec2f85e67bce') ORDER BY ALL;
