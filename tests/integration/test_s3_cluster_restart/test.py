595525314947fff112904a6e9dc6c87226c2d463
Igor Nikonov <igor@clickhouse.com>
2025-07-14 23:14:28 +0200

    Check DistributedConnectionFailTry profile event

--- tests/integration/test_s3_cluster_restart/test.py
+++ tests/integration/test_s3_cluster_restart/test.py
@@ -270,9 +270,14 @@ def test_insert_select(started_cluster, wait_restart, missing_table):
         != 0
     )
 
+    if (not wait_restart):
+        node1.query("SYSTEM FLUSH LOGS query_log");
+        assert ( node1.query(f"select ProfileEvents['DistributedConnectionFailTry'] from system.query_log where query_id = '{uuid}' and type = 'QueryFinish'") == "1\n")
+
+
     if (missing_table):
         node1.query("SYSTEM FLUSH LOGS query_log");
-        assert ( node1.query(f"select ProfileEvents['DistributedConnectionMissingTable'] as missing_table from system.query_log where query_id = '{uuid}' and type = 'QueryFinish'") == "1\n")
+        assert ( node1.query(f"select ProfileEvents['DistributedConnectionMissingTable'] from system.query_log where query_id = '{uuid}' and type = 'QueryFinish'") == "1\n")
 
 
     if (wait_restart):
