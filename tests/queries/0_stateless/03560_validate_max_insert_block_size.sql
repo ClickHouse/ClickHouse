e7f4b898fcde460667197522f761aa791cab3c47
Bharat Nallan Chakravarthy <bharat@clickhouse.com>
2025-07-14 23:59:51 -0700

    add test comment

--- tests/queries/0_stateless/03560_validate_max_insert_block_size.sql
+++ tests/queries/0_stateless/03560_validate_max_insert_block_size.sql
@@ -1,3 +1,5 @@
+-- Test for https://github.com/ClickHouse/ClickHouse/issues/83620
+
 DROP TABLE IF EXISTS t;
 
 CREATE TABLE t (n Int) ENGINE = MergeTree ORDER BY n SETTINGS merge_max_block_size = 0; -- { serverError BAD_ARGUMENTS }
