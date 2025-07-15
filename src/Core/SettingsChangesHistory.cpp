bbdaece28077fd7d55fffa094441079d0c2ee356
Alexander Gololobov <davenger@clickhouse.com>
2025-07-15 15:18:21 +0200

    Sync distributed_plan_force_shuffle_aggregation setting from cloud

--- src/Core/SettingsChangesHistory.cpp
+++ src/Core/SettingsChangesHistory.cpp
@@ -52,6 +52,7 @@ const VersionToSettingsChangesMap & getSettingsChangesHistory()
             {"min_joined_block_size_rows", 0, DEFAULT_BLOCK_SIZE, "New setting."},
             {"table_engine_read_through_distributed_cache", false, false, "New setting"},
             {"distributed_cache_alignment", 0, 0, "Rename of distributed_cache_read_alignment"},
+            {"distributed_plan_force_shuffle_aggregation", 0, 0, "New experimental setting"},
         });
         addSettingsChanges(settings_changes_history, "25.6",
         {
