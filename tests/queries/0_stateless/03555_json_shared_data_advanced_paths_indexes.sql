448b7a752a668b8535e138cf426a27aadecd38ce
avogar <avogar@clickhouse.com>
2025-07-15 14:51:46 +0000

    Fix failing test

--- tests/queries/0_stateless/03555_json_shared_data_advanced_paths_indexes.sql
+++ tests/queries/0_stateless/03555_json_shared_data_advanced_paths_indexes.sql
@@ -1,3 +1,5 @@
+set output_format_json_quote_64bit_integers=0;
+
 drop table if exists test;
 
 create table test (json JSON(max_dynamic_paths=0)) engine=MergeTree order by tuple() settings index_granularity=1, min_bytes_for_wide_part='200G', min_rows_for_wide_part=1, write_marks_for_substreams_in_compact_parts=0, object_serialization_version='v3', object_shared_data_serialization_version='advanced', object_shared_data_serialization_version_for_zero_level_parts='advanced', object_shared_data_buckets_for_compact_part=1;
