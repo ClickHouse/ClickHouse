f6def860ed4526cb78feea9fc2045ce1c6df5faf
Alexander Gololobov <davenger@clickhouse.com>
2025-07-16 09:28:15 +0200

    fix order

--- src/Core/SettingsChangesHistory.cpp
+++ src/Core/SettingsChangesHistory.cpp
@@ -52,8 +52,8 @@ const VersionToSettingsChangesMap & getSettingsChangesHistory()
             {"min_joined_block_size_rows", 0, DEFAULT_BLOCK_SIZE, "New setting."},
             {"table_engine_read_through_distributed_cache", false, false, "New setting"},
             {"distributed_cache_alignment", 0, 0, "Rename of distributed_cache_read_alignment"},
-            {"distributed_plan_force_shuffle_aggregation", 0, 0, "New experimental setting"},
             {"output_format_parquet_enum_as_byte_array", false, false, "Write enum using parquet physical type: BYTE_ARRAY and logical type: ENUM"},
+            {"distributed_plan_force_shuffle_aggregation", 0, 0, "New experimental setting"},
         });
         addSettingsChanges(settings_changes_history, "25.6",
         {
