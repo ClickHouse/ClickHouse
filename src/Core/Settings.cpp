d33848f2b7badfca67271c9ef4cbba86f4df69c2
Yakov Olkhovskiy <yakov@clickhouse.com>
2025-07-16 04:25:57 +0000

    fix style

--- src/Core/Settings.cpp
+++ src/Core/Settings.cpp
@@ -6890,11 +6890,11 @@ Maximum rows to use broadcast join instead of shuffle join in distributed query 
 Experimental timeSeries* aggregate functions for Prometheus-like timeseries resampling, rate, delta calculation.
 )", EXPERIMENTAL, allow_experimental_ts_to_grid_aggregate_function) \
     DECLARE(Int64, optimize_const_array_to_scalar_size, -1, R"(
-This is an experimental setting. Enables conversion of a constant array to scalar. 
+This is an experimental setting. Enables conversion of a constant array to scalar.
 
 Possible values:
 
-- positive integer - number of elements in an array
+- positive integer - number of elements in an array,
 - 0 — always,
 - negative integer - never.
 )", EXPERIMENTAL) \
