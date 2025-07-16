67420bf4c565b38f7ab1d4501c3d170a2b92a124
RuS2m <rustam.guseyzade@gmail.com>
2025-07-16 14:26:18 +0000

    make tests more deterministic

--- tests/queries/0_stateless/03566_system_completions_table.sql
+++ tests/queries/0_stateless/03566_system_completions_table.sql
@@ -1,14 +1,15 @@
-CREATE DATABASE IF NOT EXISTS `aaadatabase`;
-USE `aaadatabase`;
+CREATE DATABASE IF NOT EXISTS `0003566aaadatabase`;
+USE `0003566aaadatabase`;
 
-CREATE TABLE IF NOT EXISTS `aaatable` (
-    `aaafoo`    String,
-    `aaabar`    UInt16,
-    `aaabaz`    UInt128
+CREATE TABLE IF NOT EXISTS `0003566aaatable` (
+    `0003566aaafoo`     String,
+    `0003566aaabar`     UInt16,
+    `0003566aaabaz`     UInt128
 ) ENGINE = Memory;
 
 SELECT DISTINCT lower(word) AS token
 FROM system.completions
+WHERE startsWith(token, '0003566')
 ORDER BY token
-LIMIT 20
+LIMIT 5
 FORMAT PrettyCompact;
