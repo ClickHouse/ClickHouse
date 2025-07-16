ca2f50b56a3dd0b6217c20bbfa880bc2ae057c62
Pervakov Grigorii <pervakov.grigory@gmail.com>
2025-07-16 15:49:33 +0200

    fix tests and issues

--- tests/queries/0_stateless/03560_new_analyzer_default_expression.sql
+++ tests/queries/0_stateless/03560_new_analyzer_default_expression.sql
@@ -1,20 +1,26 @@
 SET enable_analyzer=1;
 
-CREATE TABLE with_default (a Int64 DEFAULT 1) Engine=Memory();
+CREATE TABLE default (a Int64 DEFAULT 1) Engine=Memory();
 -- This case is failing with old analyzer, because "_subquery" name prefix has a special treatment.
-CREATE TABLE with_clashing_name (_subquery_test Int64 DEFAULT 7) Engine=Memory();
-CREATE TABLE with_dependent_defaults (a Int64 DEFAULT 7, b Int64 DEFAULT a + 7, c Int64 DEFAULT a * b) Engine=Memory();
-CREATE TABLE with_regular_column (a Int64, b Int64 DEFAULT 7, c Int64 DEFAULT a * b) Engine=Memory();
+CREATE TABLE clashing_name (_subquery_test Int64 DEFAULT 7) Engine=Memory();
+CREATE TABLE dependent_defaults (a Int64 DEFAULT 7, b Int64 DEFAULT a + 7, c Int64 DEFAULT a * b) Engine=Memory();
+CREATE TABLE regular_column (a Int64, b Int64 DEFAULT 7, c Int64 DEFAULT a * b) Engine=Memory();
+CREATE TABLE complex_name_backward_compat (a Int64, `b.b` Int64 DEFAULT 7, c Int64 DEFAULT a * `b.b`) Engine=Memory();
+CREATE TABLE compound_id (a Tuple(x Int64, y Int64), b Int64 DEFAULT a.x + 7) Engine=Memory();
 
 SET insert_null_as_default=1;
 
-INSERT INTO with_default SELECT NULL FROM system.one;
-INSERT INTO with_clashing_name SELECT NULL FROM system.one;
-INSERT INTO with_dependent_defaults SELECT NULL, NULL, NULL FROM system.one;
-INSERT INTO with_regular_column SELECT 2, NULL, NULL FROM system.one;
+INSERT INTO default SELECT NULL FROM system.one;
+INSERT INTO clashing_name SELECT NULL FROM system.one;
+INSERT INTO dependent_defaults SELECT NULL, NULL, NULL FROM system.one;
+INSERT INTO regular_column SELECT 2, NULL, NULL FROM system.one;
+INSERT INTO complex_name_backward_compat SELECT 3, NULL, NULL FROM system.one;
+INSERT INTO compound_id (a) VALUES ((1, 2));
 
 -- { echoOn }
-SELECT * FROM with_default ORDER BY ALL;
-SELECT * FROM with_clashing_name ORDER BY ALL;
-SELECT * FROM with_dependent_defaults ORDER BY ALL;
-SELECT * FROM with_regular_column ORDER BY ALL;
+SELECT * FROM default ORDER BY ALL;
+SELECT * FROM clashing_name ORDER BY ALL;
+SELECT * FROM dependent_defaults ORDER BY ALL;
+SELECT * FROM regular_column ORDER BY ALL;
+SELECT * FROM complex_name_backward_compat ORDER BY ALL;
+SELECT * FROM compound_id ORDER BY ALL;
