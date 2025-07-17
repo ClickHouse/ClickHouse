8632122ba7503157b4bbdd89ec60a635c94c6c61
Blargian <shaun.struwig@clickhouse.com>
2025-07-17 13:34:27 +0000

    fix style

--- src/Functions/h3GetResolution.cpp
+++ src/Functions/h3GetResolution.cpp
@@ -116,7 +116,6 @@ Returns the resolution of the [H3](#h3-index) index.
     FunctionDocumentation::IntroducedIn introduced_in = {20, 1};
     FunctionDocumentation::Category category = FunctionDocumentation::Category::Geo;
     FunctionDocumentation documentation = {description, syntax, arguments, returned_value, examples, introduced_in, category};
- 
     factory.registerFunction<FunctionH3GetResolution>(documentation);
 }
 
