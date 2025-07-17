09e384315525f4c40cc2ab47754adc2c2bc1da04
avogar <avogar@clickhouse.com>
2025-07-17 17:06:03 +0000

    Reduce tests sizes

--- tests/queries/0_stateless/03554_json_shared_data_tuple_advanced_serialization_wide_part_big.sql
+++ tests/queries/0_stateless/03554_json_shared_data_tuple_advanced_serialization_wide_part_big.sql
@@ -4,20 +4,20 @@
 drop table if exists test_wide_advanced_tuple;
 create table test_wide_advanced_tuple (json Tuple(data JSON(max_dynamic_paths=8))) engine=MergeTree order by tuple() settings min_bytes_for_wide_part=1, min_rows_for_wide_part=1, write_marks_for_substreams_in_compact_parts=1, object_serialization_version='v3', object_shared_data_serialization_version='advanced', object_shared_data_serialization_version_for_zero_level_parts='advanced', object_shared_data_buckets_for_wide_part=2;
 insert into test_wide_advanced_tuple select tuple(multiIf(
-number < 150000,
+number < 15000,
 '{"?1" : 1, "?2" : 1, "?3" : 1, "?4" : 1, "?5" : 1, "?6" : 1, "?7" : 1, "?8" : 1}',
-number < 200000,
+number < 20000,
 '{"a" : {"a1" : 1, "a2" : 2, "arr" : [{"arr1" : 3, "arr2" : 4, "arr3" : 5, "arr4" : 6}]}, "b" : 7, "c" : 8, "arr" : [{"arr1" : 9, "arr2" : 10, "arr3" : 11, "arr4" : 12}]}',
-number < 250000,
+number < 25000,
 '{}',
-number < 300000,
+number < 30000,
 '{"a" : {"a1" : 3, "a2" : 4, "arr" : [{"arr1" : 5, "arr2" : 6, "arr3" : 7, "arr4" : 8}]}}',
-number < 350000,
+number < 35000,
 '{"b" : 9, "c" : 10}',
-number < 400000,
+number < 40000,
 '{"arr" : [{"arr1" : 11, "arr2" : 12, "arr3" : 13, "arr4" : 14}]}',
 '{"a" : {"a1" : 5, "a2" : 6}}'
-)) from numbers(450000);
+)) from numbers(45000);
 
 select 'select json.data';
 select json.data from test_wide_advanced_tuple format Null;
