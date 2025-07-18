de43c77116dc6c93b4e2f02efd47f91c41d773c0
Alexander Tokmakov <tavplubix@clickhouse.com>
2025-07-18 16:57:34 +0200

    cleanup temp part without global lock

--- src/Common/FailPoint.cpp
+++ src/Common/FailPoint.cpp
@@ -97,6 +97,7 @@ static struct InitFiu
     REGULAR(plain_object_storage_copy_fail_on_file_move) \
     REGULAR(database_replicated_delay_recovery) \
     REGULAR(database_replicated_delay_entry_execution) \
+    REGULAR(remove_merge_tree_part_delay) \
     REGULAR(plain_object_storage_copy_temp_source_file_fail_on_file_move) \
     REGULAR(plain_object_storage_copy_temp_target_file_fail_on_file_move) \
     REGULAR(output_format_sleep_on_progress) \
