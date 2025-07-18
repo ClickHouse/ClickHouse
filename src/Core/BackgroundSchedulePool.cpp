5ed6b1a17b58e3098a609fbb7556e314d64d2f9c
Alexander Tokmakov <tavplubix@clickhouse.com>
2025-07-18 20:46:24 +0200

    try to make BackgroundSchedulePool less unfair

--- src/Core/BackgroundSchedulePool.cpp
+++ src/Core/BackgroundSchedulePool.cpp
@@ -15,6 +15,9 @@
 
 #include <chrono>
 
+#include <Common/SipHash.h>
+#include <Common/thread_local_rng.h>
+
 
 namespace DB
 {
@@ -201,15 +204,16 @@ Coordination::WatchCallback BackgroundSchedulePoolTaskInfo::getWatchCallback()
 /// BackgroundSchedulePool
 ///
 
-BackgroundSchedulePoolPtr BackgroundSchedulePool::create(size_t size, CurrentMetrics::Metric tasks_metric, CurrentMetrics::Metric size_metric, const char * thread_name)
+BackgroundSchedulePoolPtr BackgroundSchedulePool::create(size_t size, size_t max_parallel_tasks_per_type, CurrentMetrics::Metric tasks_metric, CurrentMetrics::Metric size_metric, const char * thread_name)
 {
-    return std::shared_ptr<BackgroundSchedulePool>(new BackgroundSchedulePool(size, tasks_metric, size_metric, thread_name));
+    return std::shared_ptr<BackgroundSchedulePool>(new BackgroundSchedulePool(size, max_parallel_tasks_per_type, tasks_metric, size_metric, thread_name));
 }
 
-BackgroundSchedulePool::BackgroundSchedulePool(size_t size_, CurrentMetrics::Metric tasks_metric_, CurrentMetrics::Metric size_metric_, const char * thread_name_)
+BackgroundSchedulePool::BackgroundSchedulePool(size_t size_, size_t max_parallel_tasks_per_type_, CurrentMetrics::Metric tasks_metric_, CurrentMetrics::Metric size_metric_, const char * thread_name_)
     : tasks_metric(tasks_metric_)
     , size_metric(size_metric_, size_)
     , thread_name(thread_name_)
+    , max_parallel_tasks_per_type(max_parallel_tasks_per_type_ ? max_parallel_tasks_per_type_ : size_)
 {
     LOG_INFO(getLogger("BackgroundSchedulePool/" + thread_name), "Create BackgroundSchedulePool with {} threads", size_);
 
@@ -300,11 +304,35 @@ BackgroundSchedulePool::TaskHolder BackgroundSchedulePool::createTask(const std:
     return TaskHolder(std::shared_ptr<TaskInfo>(new TaskInfo(weak_from_this(), name, function)));
 }
 
+template<typename T>
+UInt64 getFunctionID(const std::function<T> & func)
+{
+    /// Get a pointer to the task function and use it as an identifier of the task type
+    auto * func_ptr = func.template target<T *>();
+    if (func_ptr)
+        return reinterpret_cast<UInt64>(func_ptr);
+
+    /// Lambdas have weird types, and we cannot get a pointer. Let's use has of the lambda type name,
+    /// which is usually smth like "ZN2DB22BackgroundJobsAssignee5startEvE3" or "DB::BackgroundJobsAssignee::start()::$_0"
+    /// And it's a good identifier
+    SipHash hash;
+    hash.update(func.target_type().name());
+    return hash.get64();
+}
+
 void BackgroundSchedulePool::scheduleTask(TaskInfo & task_info)
 {
     {
         std::lock_guard tasks_lock(tasks_mutex);
-        tasks.emplace_back(task_info.shared_from_this());
+        /// Get a pointer to the task function and use it as an identifier of the task type
+        UInt64 task_type = getFunctionID(task_info.function);
+        auto & group = task_groups[task_type];
+        group.tasks.emplace_back(task_info.shared_from_this());
+        if (!group.runnable_list_pos && group.num_running < max_parallel_tasks_per_type)
+        {
+            group.runnable_list_pos = runnable_task_types.size();
+            runnable_task_types.push_back(task_type);
+        }
     }
 
     tasks_cond_var.notify_one();
@@ -347,6 +375,7 @@ void BackgroundSchedulePool::threadFunction()
 
     while (!shutdown)
     {
+        UInt64 task_type_to_run;
         TaskInfoPtr task;
 
         {
@@ -356,20 +385,52 @@ void BackgroundSchedulePool::threadFunction()
             /// tasks_lock has already locked tasks_mutex.
             tasks_cond_var.wait(tasks_lock.getUnderlyingLock(), [&]() TSA_NO_THREAD_SAFETY_ANALYSIS
             {
-                return shutdown || !tasks.empty();
+                return shutdown || !runnable_task_types.empty();
             });
             if (shutdown)
                 break;
 
-            if (!tasks.empty())
+            if (runnable_task_types.empty())
+                continue;
+
+            task_type_to_run = runnable_task_types[thread_local_rng() % runnable_task_types.size()];
+            auto & group = task_groups[task_type_to_run];
+            chassert(!group.tasks.empty());
+            chassert(group.num_running < max_parallel_tasks_per_type);
+
+            task = group.tasks.front();
+            group.tasks.pop_front();
+            ++group.num_running;
+
+            if (group.num_running == max_parallel_tasks_per_type || group.tasks.empty())
             {
-                task = tasks.front();
-                tasks.pop_front();
+                /// Tasks from this group are not runnable anymore
+                auto & other_group = task_groups[runnable_task_types.back()];
+                std::swap(runnable_task_types[*group.runnable_list_pos], runnable_task_types.back());
+                runnable_task_types.pop_back();
+                other_group.runnable_list_pos = group.runnable_list_pos;
+                group.runnable_list_pos.reset();
+                if (group.num_running == max_parallel_tasks_per_type)
+                    LOG_WARNING(getLogger("BackgroundSchedulePool/" + thread_name), "Temporarily pause scheduling of tasks with id {}, example log_name={}", task_type_to_run, task->log_name);
             }
         }
 
         if (task)
+        {
             task->execute(*this);
+
+            UniqueLock tasks_lock(tasks_mutex);
+            auto & group = task_groups[task_type_to_run];
+            chassert(group.num_running);
+            --group.num_running;
+            if (!group.tasks.empty() && !group.runnable_list_pos)
+            {
+                chassert(group.num_running < max_parallel_tasks_per_type);
+                group.runnable_list_pos = runnable_task_types.size();
+                runnable_task_types.push_back(task_type_to_run);
+                tasks_cond_var.notify_one();
+            }
+        }
     }
 }
 
