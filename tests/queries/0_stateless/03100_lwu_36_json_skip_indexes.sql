3e7a56a2786247213a727d9c849eeacd8eec9b11
Anton Popov <anton@clickhouse.com>
2025-07-18 13:26:07 +0000

    fix disabling skip indexes with dynamic subcolumns

--- tests/queries/0_stateless/03100_lwu_36_json_skip_indexes.sql
+++ tests/queries/0_stateless/03100_lwu_36_json_skip_indexes.sql
@@ -3,12 +3,12 @@ DROP TABLE IF EXISTS test;
 CREATE TABLE test (
     id UInt64,
     document JSON(name String, age UInt16),
-    INDEX ix_name document.name TYPE bloom_filter(0.01)
+    INDEX ix_name document.name TYPE bloom_filter(0.01) GRANULARITY 1,
+    INDEX ix_country document.country::String TYPE bloom_filter(0.01) GRANULARITY 1
 )
 ENGINE = MergeTree()
 ORDER BY (id)
-SETTINGS enable_block_number_column = 1, enable_block_offset_column = 1;
-
+SETTINGS enable_block_number_column = 1, enable_block_offset_column = 1, index_granularity = 1;
 
 INSERT INTO test VALUES (1, '{"name":"foo", "age":15}');
 INSERT INTO test VALUES (2, '{"name":"boo", "age":15}');
@@ -16,7 +16,7 @@ INSERT INTO test VALUES (3, '{"name":"bar", "age":15}');
 
 SET allow_experimental_lightweight_update = 1;
 
-UPDATE test SET document = '{"name":"aaa", "age":15}' WHERE id = 1;
+UPDATE test SET document = '{"name":"aaa", "age":15, "country": "USA"}' WHERE id = 1;
 
 SELECT * FROM test
 WHERE document.name = 'aaa' OR document.name = 'boo'
@@ -32,5 +32,17 @@ SELECT * FROM test
 WHERE document.name = 'aaa' OR document.name = 'boo'
 ORDER BY id
 SETTINGS apply_patch_parts = 0, force_data_skipping_indices = 'ix_name';
+
+SELECT count()FROM test
+WHERE document.country::String = 'USA'
+SETTINGS apply_patch_parts = 1;
+
+SELECT count() FROM test
+WHERE document.country::String = 'USA'
+SETTINGS apply_patch_parts = 1, force_data_skipping_indices = 'ix_country'; -- { serverError INDEX_NOT_USED }
+
+SELECT count() FROM test
+WHERE document.country::String = 'USA'
+SETTINGS apply_patch_parts = 0, force_data_skipping_indices = 'ix_country';
 
 DROP TABLE IF EXISTS test;
