c7d69a60d390dd45e12d5b958bb2dfb94cce5645
Mikhail f. Shiryaev <felixoid@clickhouse.com>
2025-07-21 20:41:01 +0200

    Reduce insert memory usage

--- tests/queries/0_stateless/03563_lock_merge_tree_shutdown.sh
+++ tests/queries/0_stateless/03563_lock_merge_tree_shutdown.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
-# Tags: no-ordinary-database, long, no-flaky-check
+# Tags: no-ordinary-database, long, no-flaky-check, no-parallel
 #
 # - no-flaky-check -- not compatible with ThreadFuzzer
 
@@ -19,6 +19,8 @@ export -f reattach
 TABLES=( test_shutdown_lock_{01..20} )
 
 for table in "${TABLES[@]}"; do
+  # Create tables with as system.metric_log without wide parts,
+  # the settings are applied after inserts to avoid excessive memory usage
   $CLICKHOUSE_CLIENT -m << SQL
     SYSTEM FLUSH LOGS system.metric_log;
 
@@ -27,15 +29,8 @@ for table in "${TABLES[@]}"; do
     PARTITION BY ()
     ORDER BY ()
     SETTINGS
-        -- horizontal merges does opens all stream at once, so will still use huge amount of memory
-        min_rows_for_wide_part = 0,
-        min_bytes_for_wide_part = 0,
-        vertical_merge_algorithm_min_rows_to_activate = 0,
-        vertical_merge_algorithm_min_columns_to_activate = 1,
-        min_bytes_for_full_part_storage = 0,
-        min_parts_to_merge_at_once = 6,
-        --- avoid excessive memory usage (due to default buffer size of 1MiB that is created for each column)
-        max_merge_delayed_streams_for_parallel_write = 100
+        -- prevent merges from being executed automatically
+        min_parts_to_merge_at_once = 10
     ;
 SQL
 done
@@ -54,6 +49,23 @@ done
 wait
 
 for table in "${TABLES[@]}"; do
+  $CLICKHOUSE_CLIENT -m << SQL
+    ALTER TABLE ${CLICKHOUSE_DATABASE}.${table} MODIFY SETTING
+        -- horizontal merges does opens all stream at once, so will still use huge amount of memory
+        min_rows_for_wide_part = 0,
+        min_bytes_for_wide_part = 0,
+        vertical_merge_algorithm_min_rows_to_activate = 0,
+        vertical_merge_algorithm_min_columns_to_activate = 1,
+        min_bytes_for_full_part_storage = 0,
+        --- avoid excessive memory usage (due to default buffer size of 1MiB that is created for each column)
+        max_merge_delayed_streams_for_parallel_write = 1
+    ;
+SQL
+done
+
+# $CLICKHOUSE_CLIENT -q "SYSTEM ENABLE FAILPOINT remove_merge_tree_part_delay"
+
+for table in "${TABLES[@]}"; do
   # Should raise either ABORTED or UNKNOWN_TABLE exception
   { $CLICKHOUSE_CLIENT -m << SQL 2>&1
 OPTIMIZE TABLE ${table} FINAL;
@@ -65,6 +77,7 @@ done | sort -n &
 
 reattach
 
+$CLICKHOUSE_CLIENT -q "SYSTEM DISABLE FAILPOINT remove_merge_tree_part_delay"
 
 $CLICKHOUSE_CLIENT -m << SQL 2>&1
     SYSTEM FLUSH LOGS system.part_log;
