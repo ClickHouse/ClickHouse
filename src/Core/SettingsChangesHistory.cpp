e51322fca49850d9f7ec43b75e4e9cf4b75e60d7
avogar <avogar@clickhouse.com>
2025-07-21 18:19:13 +0000

    Enable output_format_parquet_enum_as_byte_array by default

--- src/Core/SettingsChangesHistory.cpp
+++ src/Core/SettingsChangesHistory.cpp
@@ -52,6 +52,7 @@ const VersionToSettingsChangesMap & getSettingsChangesHistory()
             {"azure_sdk_use_native_client", false, true, "New setting"},
             {"opentelemetry_trace_cpu_scheduling", false, false, "New setting to trace `cpu_slot_preemption` feature."},
             {"vector_search_with_rescoring", true, true, "New setting."},
+            {"output_format_parquet_enum_as_byte_array", false, true, "Enable writing Enum as byte array in Parquet by default"},
         });
         addSettingsChanges(settings_changes_history, "25.7",
         {
