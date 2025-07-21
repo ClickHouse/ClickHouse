47b279ac0172d87225c90d91be666152bce7fdc1
Igor Nikonov <igor@clickhouse.com>
2025-07-21 14:15:58 +0200

    Update test

--- tests/queries/0_stateless/03285_pr_join_with_group_by.sql
+++ tests/queries/0_stateless/03285_pr_join_with_group_by.sql
@@ -7,7 +7,7 @@ create table messages (id Int64, user_id Int64, text String) engine=ReplicatedMe
 insert into users select number, concat('user_', toString(number)) from numbers(10);
 insert into messages select 100+number, number, concat('message_', toString(number)) from numbers(10);
 
-SET enable_parallel_replicas = 1, max_parallel_replicas = 3, cluster_for_parallel_replicas = 'test_cluster_1_shard_3_replicas_1_unavailable';
+SET enable_parallel_replicas = 1, max_parallel_replicas = 3, cluster_for_parallel_replicas = 'test_cluster_one_shard_three_replicas_localhost';
 
 SELECT
     name,
