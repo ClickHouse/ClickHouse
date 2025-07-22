55900b0d17c54e7255bf251f62f42b5f2f46ce22
Denis K <kalantaevskii@gmail.com>
2025-07-22 10:48:03 +0200

    Reapply "feat: additional_table_filter support query parameters"
    This reverts commit b29c9493d5215d08159c3bf290cd46e22dc6deec.

--- src/Interpreters/InterpreterSelectQuery.cpp
+++ src/Interpreters/InterpreterSelectQuery.cpp
@@ -46,6 +46,7 @@
 #include <Interpreters/RewriteUniqToCountVisitor.h>
 #include <Interpreters/getCustomKeyFilterForParallelReplicas.h>
 #include <Interpreters/Context.h>
+#include <Interpreters/ReplaceQueryParameterVisitor.h>
 
 #include <QueryPipeline/Pipe.h>
 #include <Processors/QueryPlan/AggregatingStep.h>
@@ -97,6 +98,8 @@
 #include <Interpreters/HashTablesStatistics.h>
 #include <Interpreters/IJoin.h>
 #include <QueryPipeline/SizeLimits.h>
+#include <base/map.h>
+#include <base/find_symbols.h>
 #include <Common/FieldVisitorToString.h>
 #include <Common/FieldAccurateComparison.h>
 #include <Common/NaNUtils.h>
@@ -434,7 +437,7 @@ ASTPtr parseAdditionalFilterConditionForTable(
             /// Try to parse expression
             ParserExpression parser;
             const auto & settings = context.getSettingsRef();
-            return parseQuery(
+            auto query_ast = parseQuery(
                 parser,
                 filter.data(),
                 filter.data() + filter.size(),
@@ -442,6 +445,13 @@ ASTPtr parseAdditionalFilterConditionForTable(
                 settings[Setting::max_query_size],
                 settings[Setting::max_parser_depth],
                 settings[Setting::max_parser_backtracks]);
+
+            if (find_first_symbols<'{'>(filter.data(), filter.data() + filter.size()) && !context.getQueryParameters().empty())
+            {
+                ReplaceQueryParameterVisitor visitor(context.getQueryParameters());
+                visitor.visit(query_ast);
+            }
+            return query_ast;
         }
     }
 
