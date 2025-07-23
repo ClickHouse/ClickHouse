44066b2c07142e6f1cef48bd30100f045374b29b
Max Kainov <max.kainov@clickhouse.com>
2025-07-23 10:49:48 +0200

    00097_long_storage_buffer_race_condition_mt.sh try no-parallel

--- tests/queries/0_stateless/00097_long_storage_buffer_race_condition_mt.sh
+++ tests/queries/0_stateless/00097_long_storage_buffer_race_condition_mt.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/env bash
-# Tags: race
+# Tags: race, no-parallel
+#  - no-parallel: FIXME start to timeout on bigger machines with more runner thread
 
 CURDIR=$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)
 # shellcheck source=../shell_config.sh
