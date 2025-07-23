19e4b8f8b2cd45d3f84abce6bdb7ef6eabd03e46
Elmi Ahmadov <elmi.ahmadov@clickhouse.com>
2025-07-23 14:48:18 +0000

    add unit tests

--- tests/queries/0_stateless/02789_set_index_nullable_condition_bug.sql
+++ tests/queries/0_stateless/02789_set_index_nullable_condition_bug.sql
@@ -12,4 +12,46 @@ SELECT * FROM test_table
 WHERE 1 == 1 AND col1 == col1 OR
        0 AND col2 == NULL;
 
-drop table if exists test_table;
+DROP TABLE test_table;
+
+CREATE TABLE test_table
+(
+    col Nullable(Boolean),
+    INDEX col_idx col TYPE set(0)
+)
+ENGINE = MergeTree()
+ORDER BY tuple()
+SETTINGS allow_nullable_key = 1;
+
+INSERT INTO test_table VALUES(DEFAULT), (DEFAULT);
+
+SELECT count() FROM test_table where col OR col IS NULL;
+DROP TABLE test_table;
+
+CREATE TABLE test_table
+(
+    col Nullable(Boolean),
+    INDEX col_idx col TYPE set(0)
+)
+ENGINE = MergeTree()
+ORDER BY tuple()
+SETTINGS allow_nullable_key = 1;
+
+INSERT INTO test_table VALUES(DEFAULT), (DEFAULT), (TRUE);
+
+SELECT count() FROM test_table where col OR col IS NULL;
+DROP TABLE test_table;
+
+CREATE TABLE test_table
+(
+    col Nullable(Boolean),
+    INDEX col_idx col TYPE set(0)
+)
+ENGINE = MergeTree()
+ORDER BY tuple()
+SETTINGS allow_nullable_key = 1;
+
+INSERT INTO test_table VALUES(DEFAULT), (DEFAULT), (FALSE);
+
+SELECT count() FROM test_table where col OR col IS NULL;
+DROP TABLE test_table;
