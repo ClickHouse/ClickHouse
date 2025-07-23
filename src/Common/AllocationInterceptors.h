9f3b5f6f19f30f6dde58a49608914c41afb3ad7f
Nikita Mikhaylov <nikitamikhaylov@clickhouse.com>
2025-07-24 01:16:38 +0200

    Fixing FreeBSD again

--- src/Common/AllocationInterceptors.h
+++ src/Common/AllocationInterceptors.h
@@ -8,7 +8,7 @@
 
 // NOLINTBEGIN
 
-#if defined(SANITIZER) || defined(SANITIZE_COVERAGE) || defined(OS_DARWIN)
+#if defined(SANITIZER) || defined(SANITIZE_COVERAGE) || defined(OS_DARWIN) || defined(OS_FREEBSD)
 
 #define __real_malloc(size) ::malloc(size)
 #define __real_calloc(nmemb, size) ::calloc(nmemb, size)
