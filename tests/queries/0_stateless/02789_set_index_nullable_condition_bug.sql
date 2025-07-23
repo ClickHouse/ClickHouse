271a33925849205c2c256075092a93c52d7ba5a9
Robert Schulze <robert@clickhouse.com>
2025-07-23 15:34:51 +0000

    Cosmetics

--- tests/queries/0_stateless/02789_set_index_nullable_condition_bug.sql
+++ tests/queries/0_stateless/02789_set_index_nullable_condition_bug.sql
@@ -1,57 +1,65 @@
-drop table if exists test_table;
-CREATE TABLE test_table
+DROP TABLE IF EXISTS tab;
+
+CREATE TABLE tab
 (
     col1 String,
     col2 String,
     INDEX test_table_col2_idx col2 TYPE set(0) GRANULARITY 1
 ) ENGINE = MergeTree()
-      ORDER BY col1
+ORDER BY col1
 AS SELECT 'v1', 'v2';
 
-SELECT * FROM test_table
+SELECT * FROM tab
 WHERE 1 == 1 AND col1 == col1 OR
        0 AND col2 == NULL;
 
-DROP TABLE test_table;
+DROP TABLE tab;
 
-CREATE TABLE test_table
+-- Test for issue #75485
+
+CREATE TABLE tab
 (
     col Nullable(Boolean),
     INDEX col_idx col TYPE set(0)
 )
 ENGINE = MergeTree()
-ORDER BY tuple()
-SETTINGS allow_nullable_key = 1;
+ORDER BY tuple();
 
-INSERT INTO test_table VALUES(DEFAULT), (DEFAULT);
+INSERT INTO tab VALUES
+    (DEFAULT),
+    (DEFAULT);
 
-SELECT count() FROM test_table where col OR col IS NULL;
-DROP TABLE test_table;
+SELECT count() FROM tab WHERE col OR col IS NULL;
+DROP TABLE tab;
 
-CREATE TABLE test_table
+CREATE TABLE tab
 (
     col Nullable(Boolean),
     INDEX col_idx col TYPE set(0)
 )
 ENGINE = MergeTree()
-ORDER BY tuple()
-SETTINGS allow_nullable_key = 1;
+ORDER BY tuple();
 
-INSERT INTO test_table VALUES(DEFAULT), (DEFAULT), (TRUE);
+INSERT INTO tab VALUES
+    (DEFAULT),
+    (DEFAULT),
+    (TRUE);
 
-SELECT count() FROM test_table where col OR col IS NULL;
-DROP TABLE test_table;
+SELECT count() FROM tab WHERE col OR col IS NULL;
+DROP TABLE tab;
 
-CREATE TABLE test_table
+CREATE TABLE tab
 (
     col Nullable(Boolean),
     INDEX col_idx col TYPE set(0)
 )
 ENGINE = MergeTree()
-ORDER BY tuple()
-SETTINGS allow_nullable_key = 1;
+ORDER BY tuple();
 
-INSERT INTO test_table VALUES(DEFAULT), (DEFAULT), (FALSE);
+INSERT INTO tab VALUES
+    (DEFAULT),
+    (DEFAULT),
+    (FALSE);
 
-SELECT count() FROM test_table where col OR col IS NULL;
-DROP TABLE test_table;
+SELECT count() FROM tab WHERE col OR col IS NULL;
+DROP TABLE tab;
