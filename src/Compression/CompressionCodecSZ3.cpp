95923e21cb401fe11ce2d60a106815803e2c7b50
Konstantin Vedernikov <75157521+scanhex12@users.noreply.github.com>
2025-07-23 23:44:35 +0200

    Update src/Compression/CompressionCodecSZ3.cpp
    Co-authored-by: Alexey Milovidov <milovidov@clickhouse.com>

--- src/Compression/CompressionCodecSZ3.cpp
+++ src/Compression/CompressionCodecSZ3.cpp
@@ -198,6 +198,7 @@ void CompressionCodecSZ3::setAndCheckVectorDimension(size_t dimension_)
 
 void CompressionCodecSZ3::doDecompressData(const char * source, UInt32 source_size, char * dest, UInt32 /*uncompressed_size*/) const
 {
+    /// Hardcoded, because it is not declared (we just calculated the minimal size of decompressed config) and it is less than sizeof(SZ3::Config).
     static constexpr const size_t config_size = 39;
     if (source_size < 1 + config_size)
         throw Exception(ErrorCodes::CORRUPTED_DATA, "Can not decompress data {} that is less than minimal size of compressed", source_size);
