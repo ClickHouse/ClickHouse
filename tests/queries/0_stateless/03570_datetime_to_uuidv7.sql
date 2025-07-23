65140d6e27bcb9018e24a7bb028c780583a6128c
Sam Radovich <samuel.radovich@target.com>
2025-07-23 12:37:46 -0500

    add unit tests

--- tests/queries/0_stateless/03570_datetime_to_uuidv7.sql
+++ tests/queries/0_stateless/03570_datetime_to_uuidv7.sql
@@ -5,9 +5,12 @@ SELECT toTypeName(dateTimeToUUIDv7(toDateTime('2021-08-15 18:57:56')));
 SELECT substring(hex(dateTimeToUUIDv7(toDateTime('2021-08-15 18:57:56'))), 13, 1); -- check version bits (should be '7')
 SELECT bitAnd(bitShiftRight(toUInt128(dateTimeToUUIDv7(toDateTime('2021-08-15 18:57:56'))), 62), 3); -- check variant bits (should be '2')
 
--- Counter functionality tests (multiple calls with same timestamp)
-SELECT dateTimeToUUIDv7(toDateTime('2021-08-15 18:57:56')) = dateTimeToUUIDv7(toDateTime('2021-08-15 18:57:56'));
-SELECT dateTimeToUUIDv7(toDateTime('2021-08-15 18:57:56')) != dateTimeToUUIDv7(toDateTime('2021-08-15 18:57:56'));
+-- Counter functionality tests (multiple calls with same timestamp), should increment
+SELECT
+    max(uuid) > min(uuid)
+FROM (
+    SELECT dateTimeToUUIDv7(toDateTime('2021-08-15 18:57:56')) AS uuid FROM numbers(2)
+);
 
 -- Different timestamps should produce different UUIDs
 SELECT dateTimeToUUIDv7(toDateTime('2021-08-15 18:57:56')) != dateTimeToUUIDv7(toDateTime('2021-08-15 18:57:57'));
@@ -19,10 +22,7 @@ SELECT dateTimeToUUIDv7(toDateTime('2021-08-15 18:57:56', 'UTC')) != dateTimeToU
 SELECT dateTimeToUUIDv7(); -- { serverError NUMBER_OF_ARGUMENTS_DOESNT_MATCH }
 SELECT dateTimeToUUIDv7('2021-08-15 18:57:56'); -- { serverError ILLEGAL_TYPE_OF_ARGUMENT }
 SELECT dateTimeToUUIDv7(toDateTime('2021-08-15 18:57:56'), 'extra'); -- { serverError NUMBER_OF_ARGUMENTS_DOESNT_MATCH }
+SELECT dateTimeToUUIDv7(123.123456); -- { serverError ILLEGAL_TYPE_OF_ARGUMENT }
 
 -- Verify timestamp extraction works correctly
-SELECT UUIDv7ToDateTime(dateTimeToUUIDv7(toDateTime('2021-08-15 18:57:56'))) = toDateTime('2021-08-15 18:57:56');
-
--- Test with constant and non-constant arguments
-SELECT dateTimeToUUIDv7(toDateTime('2021-08-15 18:57:56'));
-SELECT dateTimeToUUIDv7(materialize(toDateTime('2021-08-15 18:57:56')));
+SELECT UUIDv7ToDateTime(dateTimeToUUIDv7(toDateTime('2021-08-15 18:57:56'))) == toDateTime('2021-08-15 18:57:56');
