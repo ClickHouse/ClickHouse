a9ae8db93715468d58b307999f42d3f2d98aa3ac
Ilya Golshtein <igolshtein@altinity.com>
2025-07-24 15:14:47 +0300

    protobuf_oneof_present: make test output more stable

--- tests/queries/0_stateless/03447_protobuf_format_oneof.sh
+++ tests/queries/0_stateless/03447_protobuf_format_oneof.sh
@@ -17,7 +17,7 @@ SELECT '>> string_or_string';
 CREATE TABLE string_or_string_3447 ( string1 String, string2 String, string_oneof Enum('no'=0, 'hello' = 1, 'world' = 2) )  Engine=MergeTree ORDER BY tuple();
 INSERT INTO string_or_string_3447 from INFILE '$CURDIR/data_protobuf/String1' SETTINGS format_schema='$SCHEMADIR/03447_string_or_string.proto:StringOrString' FORMAT ProtobufSingle;
 INSERT INTO string_or_string_3447 from INFILE '$CURDIR/data_protobuf/String2' SETTINGS format_schema='$SCHEMADIR/03447_string_or_string.proto:StringOrString' FORMAT ProtobufSingle;
-SELECT * FROM string_or_string_3447 ORDER BY string1 Format pretty;
+SELECT * FROM string_or_string_3447 ORDER BY string1 Format PrettyMonoBlock;
 EOF
 
 
@@ -38,7 +38,7 @@ CREATE TABLE inner_string_or_string_3447 ( \`outer.string\` String, \`inner.stri
 INSERT INTO inner_string_or_string_3447 from INFILE '$CURDIR/data_protobuf/InnerString1' SETTINGS format_schema='$SCHEMADIR/03447_inner_string_or_string.proto:InnerStringOrString' FORMAT ProtobufSingle;
 INSERT INTO inner_string_or_string_3447 from INFILE '$CURDIR/data_protobuf/InnerString2' SETTINGS format_schema='$SCHEMADIR/03447_inner_string_or_string.proto:InnerStringOrString' FORMAT ProtobufSingle;
 INSERT INTO inner_string_or_string_3447 from INFILE '$CURDIR/data_protobuf/String1' SETTINGS format_schema='$SCHEMADIR/03447_inner_string_or_string.proto:InnerStringOrString' FORMAT ProtobufSingle;
-SELECT * FROM inner_string_or_string_3447 ORDER BY \`outer.string\` Format pretty;
+SELECT * FROM inner_string_or_string_3447 ORDER BY \`outer.string\` Format PrettyMonoBlock;
 EOF
 
 # date: "10.10.2025"
@@ -103,7 +103,7 @@ CREATE TABLE oneof_repeated_3447
 )
 ENGINE = MergeTree ORDER BY tuple();
 INSERT INTO oneof_repeated_3447 from INFILE '$CURDIR/data_protobuf/OneofRepeated' SETTINGS format_schema='$SCHEMADIR/03447_oneof_repeated.proto:TestOneOfRepeated' FORMAT ProtobufSingle;
-SELECT * FROM oneof_repeated_3447 FORMAT Vertical;
+SELECT * FROM oneof_repeated_3447 ORDER BY \`items.name\` FORMAT Vertical;
 EOF
 
 # string1: "string1"
@@ -137,5 +137,5 @@ DROP TABLE IF EXISTS string_or_string_int32_3447;
 SELECT '>> string_or_string_int32';
 CREATE TABLE string_or_string_int32_3447 ( string1 String, string2 String, string_oneof Int32 )  Engine=MergeTree ORDER BY tuple();
 INSERT INTO string_or_string_int32_3447 from INFILE '$CURDIR/data_protobuf/String1' SETTINGS format_schema='$SCHEMADIR/03447_string_or_string.proto:StringOrString' FORMAT ProtobufSingle;
-SELECT * FROM string_or_string_int32_3447 FORMAT Vertical;
+SELECT * FROM string_or_string_int32_3447 ORDER BY string1 FORMAT Vertical;
 EOF
