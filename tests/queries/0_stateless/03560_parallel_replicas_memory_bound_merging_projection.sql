f534f36c883bd09a26bf2f25a72042807af1b87f
zoomxi <419486879@qq.com>
2025-07-24 22:47:59 +0800

    better test

--- tests/queries/0_stateless/03560_parallel_replicas_memory_bound_merging_projection.sql
+++ tests/queries/0_stateless/03560_parallel_replicas_memory_bound_merging_projection.sql
@@ -7,24 +7,20 @@ insert into pr_t select number % 1000, number % 1000 from numbers_mt(1e6);
 set parallel_replicas_only_with_analyzer = 0;  -- necessary for CI run with disabled analyzer
 
 set max_threads = 4;
-set enable_parallel_replicas = 1;
-set parallel_replicas_for_non_replicated_merge_tree = 1;
-set max_parallel_replicas = 3;
-set cluster_for_parallel_replicas = 'test_cluster_one_shard_three_replicas_localhost';
-set distributed_aggregation_memory_efficient = 1;
-set enable_memory_bound_merging_of_aggregation_results = 1;
-set optimize_aggregation_in_order = 1;
-set distributed_aggregation_memory_efficient = 1;
+set enable_parallel_replicas = 1, parallel_replicas_for_non_replicated_merge_tree = 1, max_parallel_replicas = 3, cluster_for_parallel_replicas = 'test_cluster_one_shard_three_replicas_localhost';
+set distributed_aggregation_memory_efficient = 1, enable_memory_bound_merging_of_aggregation_results = 1, optimize_aggregation_in_order = 1, distributed_aggregation_memory_efficient = 1;
+set enable_analyzer = 1, parallel_replicas_local_plan = 1, optimize_use_projections = 1;
 
 -- { echoOn } --
-explain pipeline select sum(a) from pr_t group by b order by b limit 5 offset 500 SETTINGS enable_analyzer=1, parallel_replicas_local_plan=1, optimize_use_projections=1, parallel_replicas_support_projection=0;
-select sum(a) from pr_t group by b order by b limit 5 offset 500 SETTINGS enable_analyzer=1, parallel_replicas_local_plan=1, optimize_use_projections=1, parallel_replicas_support_projection=0;
+explain pipeline select sum(a) from pr_t group by b order by b limit 5 offset 500 SETTINGS parallel_replicas_support_projection=0;
+select sum(a) from pr_t group by b order by b limit 5 offset 500 SETTINGS parallel_replicas_support_projection=0;
 
-explain pipeline select sum(a) from pr_t group by b order by b limit 5 offset 500 SETTINGS enable_analyzer=1, parallel_replicas_local_plan=1, optimize_use_projections=1, parallel_replicas_support_projection=0, force_aggregation_in_order=1;
-select sum(a) from pr_t group by b order by b limit 5 offset 500 SETTINGS enable_analyzer=1, parallel_replicas_local_plan=1, optimize_use_projections=1, parallel_replicas_support_projection=0, force_aggregation_in_order=1;
+explain pipeline select sum(a) from pr_t group by b order by b limit 5 offset 500 SETTINGS parallel_replicas_support_projection=0, force_aggregation_in_order=1;
+select sum(a) from pr_t group by b order by b limit 5 offset 500 SETTINGS parallel_replicas_support_projection=0, force_aggregation_in_order=1;
 
-explain pipeline select sum(a) from pr_t group by b order by b limit 5 offset 500 SETTINGS enable_analyzer=1, parallel_replicas_local_plan=1, optimize_use_projections=1, parallel_replicas_support_projection=1;
-select sum(a) from pr_t group by b order by b limit 5 offset 500 SETTINGS enable_analyzer=1, parallel_replicas_local_plan=1, optimize_use_projections=1, parallel_replicas_support_projection=1;
+SELECT trimLeft(*) FROM (explain select sum(a) from pr_t group by b order by b limit 5 offset 500 SETTINGS parallel_replicas_support_projection=1) WHERE explain LIKE '%ReadFromMergeTree%';
+explain pipeline select sum(a) from pr_t group by b order by b limit 5 offset 500 SETTINGS parallel_replicas_support_projection=1;
+select sum(a) from pr_t group by b order by b limit 5 offset 500 SETTINGS parallel_replicas_support_projection=1;
 -- { echoOff } --
 
 drop table if exists pr_t;
