ca2a491b5f3fc845cce07415c969cc01e812ad2b
Max Kainov <max.kainov@clickhouse.com>
2025-07-24 22:16:30 +0200

    update praktika config and workflows

--- ci/defs/job_configs.py
+++ ci/defs/job_configs.py
@@ -648,19 +648,21 @@ class JobConfigs:
             "asan, old analyzer, 4/6",
             "asan, old analyzer, 5/6",
             "asan, old analyzer, 6/6",
-            "release, 1/4",
-            "release, 2/4",
-            "release, 3/4",
-            "release, 4/4",
+            "release, 1/6",
+            "release, 2/6",
+            "release, 3/6",
+            "release, 4/6",
+            "release, 5/6",
+            "release, 6/6",
             "aarch64, distributed plan, 1/4",
             "aarch64, distributed plan, 2/4",
             "aarch64, distributed plan, 3/4",
             "aarch64, distributed plan, 4/4",
         ],
-        runs_on=[RunnerLabels.FUNC_TESTER_AMD for _ in range(10)]
+        runs_on=[RunnerLabels.FUNC_TESTER_AMD for _ in range(12)]
         + [RunnerLabels.FUNC_TESTER_ARM for _ in range(4)],
         requires=[["Build (amd_asan)"] for _ in range(6)]
-        + [["Build (amd_release)"] for _ in range(4)]
+        + [["Build (amd_release)"] for _ in range(6)]
         + [["Build (arm_release)"] for _ in range(4)],
     )
     integration_test_jobs_non_required = Job.Config(
