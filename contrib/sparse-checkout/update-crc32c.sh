adae68d8e449d14e074d3939af6409d27bdd62ba
János Benjamin Antal <antaljanosbenjamin@users.noreply.github.com>
2025-07-24 13:06:00 +0200

    Fix sparse checkout for crc32c

--- contrib/sparse-checkout/update-crc32c.sh
+++ contrib/sparse-checkout/update-crc32c.sh
@@ -3,6 +3,7 @@
 echo "Using sparse checkout for crc32c"
 
 FILES_TO_CHECKOUT=$(git rev-parse --git-dir)/info/sparse-checkout
+echo '/*' > $FILES_TO_CHECKOUT
 echo '!/third_party' > $FILES_TO_CHECKOUT
 
 git config core.sparsecheckout true
