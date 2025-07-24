969431381b9233eafb83c244617eb47cd8b32e27
Igor Nikonov <igor@clickhouse.com>
2025-07-24 23:27:27 +0200

    Fix 03572_pr_remote_in_subquery

--- tests/queries/0_stateless/03572_pr_remote_in_subquery.sql
+++ tests/queries/0_stateless/03572_pr_remote_in_subquery.sql
@@ -1,5 +1,5 @@
 set enable_analyzer = 1;
-set enable_parallel_replicas=1, max_parallel_replicas=2, parallel_replicas_local_plan=1, parallel_replicas_for_non_replicated_merge_tree = 1;
+set enable_parallel_replicas=1, max_parallel_replicas=2, parallel_replicas_local_plan=1;
 
 select '-- SELECT';
 SELECT * FROM (SELECT dummy AS k FROM remote('127.0.0.{1,2}', system.one));
@@ -14,7 +14,7 @@ select '-- UNION';
 )
 UNION ALL
 (
-    SELECT 1, c1 FROM t0
+    SELECT 1, c1 FROM t0 SETTINGS cluster_for_parallel_replicas='test_cluster_one_shard_three_replicas_localhost', parallel_replicas_for_non_replicated_merge_tree = 1
 );
 
 DROP TABLE t0;
