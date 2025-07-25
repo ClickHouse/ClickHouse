133545906524696270c2a977082b0735ccc6b269
zoomxi <419486879@qq.com>
2025-07-25 16:39:36 +0800

    fix test

--- tests/queries/0_stateless/03560_parallel_replicas_memory_bound_merging_projection.sql
+++ tests/queries/0_stateless/03560_parallel_replicas_memory_bound_merging_projection.sql
@@ -9,7 +9,7 @@ set parallel_replicas_only_with_analyzer = 0;  -- necessary for CI run with disa
 set max_threads = 4;
 set enable_parallel_replicas = 1, parallel_replicas_for_non_replicated_merge_tree = 1, max_parallel_replicas = 3, cluster_for_parallel_replicas = 'test_cluster_one_shard_three_replicas_localhost';
 set distributed_aggregation_memory_efficient = 1, enable_memory_bound_merging_of_aggregation_results = 1, optimize_aggregation_in_order = 1;
-set enable_analyzer = 1, parallel_replicas_local_plan = 1, optimize_use_projections = 1， parallel_replicas_support_projection = 1;
+set enable_analyzer = 1, parallel_replicas_local_plan = 1, optimize_use_projections = 1, parallel_replicas_support_projection = 1;
 
 -- { echoOn } --
 explain pipeline select a from pr_t group by a order by a limit 5 offset 500;
