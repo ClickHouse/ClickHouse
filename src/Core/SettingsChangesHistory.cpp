66a5890ad4c292d285a24d38bd6d7e64b4556014
vdimir <vdimir@clickhouse.com>
2025-07-25 11:20:54 +0000

    fixing tests

--- src/Core/SettingsChangesHistory.cpp
+++ src/Core/SettingsChangesHistory.cpp
@@ -54,7 +54,8 @@ const VersionToSettingsChangesMap & getSettingsChangesHistory()
             {"azure_sdk_use_native_client", false, true, "New setting"},
             {"opentelemetry_trace_cpu_scheduling", false, false, "New setting to trace `cpu_slot_preemption` feature."},
             {"vector_search_with_rescoring", true, true, "New setting."},
-            {"delta_lake_enable_expression_visitor_logging", false, false, "New setting"},
+            {"query_plan_optimize_join_order", false, false, "New setting"},
+            {"query_plan_display_internal_aliases", false, false, "New setting"},
         });
         addSettingsChanges(settings_changes_history, "25.7",
         {
--- tests/queries/0_stateless/01062_pm_all_join_with_block_continuation.sql
+++ tests/queries/0_stateless/01062_pm_all_join_with_block_continuation.sql
@@ -21,6 +21,8 @@ SELECT count(1), uniqExact(n) FROM (
 -- errors
 SET max_joined_block_size_rows = 0;
 
+SET query_plan_join_swap_table = 'false';
+
 SELECT count(1) FROM (
     SELECT materialize(1) as k, n FROM numbers(10) nums
     JOIN (SELECT materialize(1) AS k, number n FROM numbers(1000000)) j
