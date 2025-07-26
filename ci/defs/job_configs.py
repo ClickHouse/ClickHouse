051eb5812e3fef42a265fa1ada13fa17d68c0153
Max Kainov <max.kainov@clickhouse.com>
2025-07-26 17:21:37 +0000

    2 way of install and start clickhouse for complex and light jobs

--- ci/defs/job_configs.py
+++ ci/defs/job_configs.py
@@ -903,7 +903,7 @@ class JobConfigs:
                 "./ci/jobs/scripts/functional_tests/setup_log_cluster.sh",
             ],
         ),
-        run_in_docker="clickhouse/stateless-test+--shm-size=16g+--network=host+--volume=./ci/tmp/var/lib/clickhouse:/var/lib/clickhouse+--volume=./ci/tmp/etc/clickhouse-client:/etc/clickhouse-client+--volume=./ci/tmp/etc/clickhouse-server:/etc/clickhouse-server+--volume=./ci/tmp/var/log:/var/log",
+        run_in_docker="clickhouse/stateless-test+--shm-size=16g+--network=host",
     ).parametrize(
         Job.ParamSet(
             parameter=BuildTypes.AMD_RELEASE,
