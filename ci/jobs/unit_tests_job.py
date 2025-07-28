26a13ff6c79bbe3b23d1102191d2e450ee030973
Azat Khuzhin <a3at.mail@gmail.com>
2025-07-28 22:17:45 +0200

    ci: enable stacktrace on timeout with gdb for non-asan

--- ci/jobs/unit_tests_job.py
+++ ci/jobs/unit_tests_job.py
@@ -5,8 +5,8 @@ if __name__ == "__main__":
     # With gdb we will capture stacktrace in case of abnormal termination and timeout (30 mins)
     # 'catch syscall exit_group' is a hack to avoid exiting with non-zero exit code when process terminated successfully
     #
-    # Note, LSan does not compatible with debugger, so let's not run binary under gdb for sanitizers
-    if "san" not in Info().job_name:
+    # Note, LSan does not compatible with debugger
+    if "asan" not in Info().job_name:
         command_launcher = f"timeout 30m gdb -batch -ex 'handle all nostop' -ex 'set print thread-events off' -ex 'set pagination off' -ex 'catch syscall exit_group' -ex run -ex bt -arg"
     else:
         command_launcher = ""
