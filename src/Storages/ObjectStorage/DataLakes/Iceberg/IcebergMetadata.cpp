ff2674b68542ea0e4ce0dd4d3e278a7c68224387
Konstantin Vedernikov <75157521+scanhex12@users.noreply.github.com>
2025-07-28 20:35:06 +0200

    Update IcebergMetadata.cpp

--- src/Storages/ObjectStorage/DataLakes/Iceberg/IcebergMetadata.cpp
+++ src/Storages/ObjectStorage/DataLakes/Iceberg/IcebergMetadata.cpp
@@ -484,9 +484,9 @@ void IcebergMetadata::createInitial(
             throw Exception(ErrorCodes::TABLE_ALREADY_EXISTS, "Iceberg table with path {} already exists", configuration_ptr->getPathForRead().path);
     }
 
-    auto metadata_content = createEmptyMetadataFile(configuration_ptr->getRawPath(), *columns, partition_by, configuration_ptr->getDataLakeSettings()[DataLakeStorageSetting::iceberg_format_version]);
+    auto metadata_content = createEmptyMetadataFile(configuration_ptr->getRawPath().path, *columns, partition_by, configuration_ptr->getDataLakeSettings()[DataLakeStorageSetting::iceberg_format_version]);
     {
-        auto filename = configuration_ptr->getRawPath() + "metadata/v1.metadata.json";
+        auto filename = configuration_ptr->getRawPath().path + "metadata/v1.metadata.json";
         auto buffer_metadata = object_storage->writeObject(
             StoredObject(filename), WriteMode::Rewrite, std::nullopt, DBMS_DEFAULT_BUFFER_SIZE, local_context->getWriteSettings());
         buffer_metadata->write(metadata_content.data(), metadata_content.size());
@@ -494,7 +494,7 @@ void IcebergMetadata::createInitial(
     }
     if (catalog)
     {
-        auto catalog_filename = configuration_ptr->getTypeName() + "://" + configuration_ptr->getNamespace() + "/" + configuration_ptr->getRawPath() + "metadata/v1.metadata.json";
+        auto catalog_filename = configuration_ptr->getTypeName() + "://" + configuration_ptr->getNamespace() + "/" + configuration_ptr->getRawPath().path + "metadata/v1.metadata.json";
         const auto & [namespace_name, table_name] = DataLake::parseTableName(table_id_.getTableName());
         catalog->createTable(namespace_name, table_name, catalog_filename);
     }
