4a9f52ab722c2fd3ebb5ae170779b4ab669c996e
Azat Khuzhin <a3at.mail@gmail.com>
2025-07-29 00:35:45 +0200

    ci: try to catch exit_group by syscall number
    Since now CI reports [1]:
    
        Unknown syscall name 'exit_group'.
    
      [1]: https://s3.amazonaws.com/clickhouse-test-reports/PRs/84527/389b200810a6876a5e388344bb09c663e99864de//unit_tests_tsan/job.log

--- ci/jobs/unit_tests_job.py
+++ ci/jobs/unit_tests_job.py
@@ -3,11 +3,11 @@ from ci.praktika.result import Result
 
 if __name__ == "__main__":
     # With gdb we will capture stacktrace in case of abnormal termination and timeout (30 mins)
-    # 'catch syscall exit_group' is a hack to avoid exiting with non-zero exit code when process terminated successfully
+    # 'catch syscall 231' (exit_group) is a hack to avoid exiting with non-zero exit code when process terminated successfully
     #
     # Note, LSan does not compatible with debugger
     if "asan" not in Info().job_name:
-        command_launcher = f"timeout 30m gdb -batch -ex 'handle all nostop' -ex 'set print thread-events off' -ex 'set pagination off' -ex 'catch syscall exit_group' -ex run -ex bt -arg"
+        command_launcher = f"timeout 30m gdb -batch -ex 'handle all nostop' -ex 'set print thread-events off' -ex 'set pagination off' -ex 'catch syscall 231' -ex run -ex bt -arg"
     else:
         command_launcher = ""
 
