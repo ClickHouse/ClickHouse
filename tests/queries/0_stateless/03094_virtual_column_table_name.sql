f223564fc1eaac79e4578d2a1e38ea7f18d12733
wudidapaopao <xiaozhe.yu@clickhouse.com>
2025-07-29 04:42:13 +0800

    Fix tests

--- tests/queries/0_stateless/03094_virtual_column_table_name.sql
+++ tests/queries/0_stateless/03094_virtual_column_table_name.sql
@@ -13,6 +13,7 @@ DROP TABLE IF EXISTS d4;
 DROP TABLE IF EXISTS d5;
 DROP TABLE IF EXISTS d6;
 DROP TABLE IF EXISTS d7;
+DROP TABLE IF EXISTS d8;
 DROP TABLE IF EXISTS buffer1;
 DROP VIEW IF EXISTS view1;
 DROP VIEW IF EXISTS view2;
@@ -24,9 +25,10 @@ DROP TABLE IF EXISTS dist6;
 CREATE TABLE d1 (key Int, value Int) ENGINE=Memory();
 CREATE TABLE d2 (key Int, value Int) ENGINE=MergeTree() ORDER BY key;
 CREATE TABLE d3 (_table Int, value Int) ENGINE=Memory();
+CREATE TABLE d8 (key Int, value Int) ENGINE=Memory();
 
 CREATE TABLE m0 ENGINE=Merge(currentDatabase(), '^(d1|d2)$');
-CREATE TABLE d4 ENGINE=Distributed('test_shard_localhost', currentDatabase(), d2, rand());
+CREATE TABLE d4 ENGINE=Distributed('test_shard_localhost', currentDatabase(), d8, rand());
 CREATE TABLE dist5 ENGINE=Distributed('test_shard_localhost', currentDatabase(), d4, rand());
 CREATE TABLE dist6 ENGINE=Distributed('test_shard_localhost', currentDatabase(), m0, rand());
 
@@ -38,6 +40,9 @@ INSERT INTO d2 VALUES (4, 40);
 
 INSERT INTO d3 VALUES (5, 50);
 INSERT INTO d3 VALUES (6, 60);
+
+INSERT INTO d8 VALUES (5, 50);
+INSERT INTO d8 VALUES (6, 60);
 
 CREATE TABLE m1 ENGINE=Merge(currentDatabase(), '^(d1|d2)$');
 CREATE TABLE m2 ENGINE=Merge(currentDatabase(), '^(d1|d4)$');
@@ -72,8 +77,8 @@ SELECT _table, key, value FROM d2 WHERE value = 40;
 SELECT _table, value FROM d3 WHERE _table = 6;
 
 SELECT _table FROM d4;
-SELECT count(_table) FROM d4 WHERE _table = 'd4' GROUP BY _table;
-SELECT _table, key, value FROM d4 WHERE value = 40;
+SELECT count(_table) FROM d4 WHERE _table = 'd8' GROUP BY _table;
+SELECT _table, key, value FROM d4 WHERE value = 60;
 
 SELECT _table FROM m1 ORDER BY _table ASC;
 SELECT count(_table) FROM m1 WHERE _table = 'd1' GROUP BY _table;
@@ -81,7 +86,7 @@ SELECT _table, key, value FROM m1 WHERE _table = 'd2' and value <= 30;
 
 SELECT _table FROM m2 ORDER BY _table ASC;
 SELECT count(_table) FROM m2 WHERE _table = 'd1' GROUP BY _table;
-SELECT _table, key, value FROM m2 WHERE _table = 'd4' and value <= 30;
+SELECT _table, key, value FROM m2 WHERE _table = 'd8' and value >= 60;
 
 SELECT _table, key, value FROM (SELECT _table, key, value FROM d1 UNION ALL SELECT _table, key, value FROM d2) ORDER BY key ASC;
 
@@ -96,7 +101,7 @@ SELECT _table, key, value FROM mv2 ORDER BY key ASC;
 SELECT _table, * FROM dist5 ORDER BY key ASC;
 SELECT _table, * FROM dist6 ORDER BY key ASC;
 SELECT _table, * FROM m3 ORDER BY key ASC;
-SELECT _table, * FROM m4 ORDER BY key ASC;
-SELECT _table, * FROM m5 ORDER BY key ASC;
+SELECT _table, * FROM m4 WHERE _table = 'd8' ORDER BY key ASC;
+SELECT _table, * FROM m5 WHERE _table = 'd8' ORDER BY key ASC;
 
 SELECT * FROM d1 PREWHERE _table = 'd1'; -- { serverError ILLEGAL_PREWHERE }
