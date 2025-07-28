096326799043361afc249d324b0b312dd2376b02
Mikhail f. Shiryaev <felixoid@clickhouse.com>
2025-07-28 15:22:45 +0200

    Make shellcheck a bit more happy about the script

--- tests/queries/0_stateless/02443_detach_attach_partition.sh
+++ tests/queries/0_stateless/02443_detach_attach_partition.sh
@@ -19,21 +19,21 @@ $CLICKHOUSE_CLIENT -q "
 function thread_detach()
 {
     while true; do
-        $CLICKHOUSE_CLIENT -mn -q "ALTER TABLE alter_table$(($RANDOM % 2)) DETACH PARTITION ID 'all'; SELECT sleep($RANDOM / 32000) format Null;" 2>/dev/null ||:
+        $CLICKHOUSE_CLIENT -mn -q "ALTER TABLE alter_table$table_id DETACH PARTITION ID 'all'; SELECT sleep($RANDOM / 32000) format Null;" 2>/dev/null ||:
     done
 }
 function thread_attach()
 {
     while true; do
-        $CLICKHOUSE_CLIENT -mn -q "ALTER TABLE alter_table$(($RANDOM % 2)) ATTACH PARTITION ID 'all'; SELECT sleep($RANDOM / 32000) format Null;" 2>/dev/null ||:
+        $CLICKHOUSE_CLIENT -mn -q "ALTER TABLE alter_table$table_id ATTACH PARTITION ID 'all'; SELECT sleep($RANDOM / 32000) format Null;" 2>/dev/null ||:
     done
 }
 
-insert_type=$(($RANDOM % 3))
+insert_type=$((RANDOM % 3))
 
 engine=$($CLICKHOUSE_CLIENT -q "SELECT engine FROM system.tables WHERE database=currentDatabase() AND table='alter_table0'")
 if [[ "$engine" == "ReplicatedMergeTree" || "$engine" == "SharedMergeTree" ]]; then
-    insert_type=$(($RANDOM % 2))
+    insert_type=$((RANDOM % 2))
 fi
 $CLICKHOUSE_CLIENT -q "SELECT '$CLICKHOUSE_DATABASE', 'insert_type $insert_type' FORMAT Null"
 
@@ -41,15 +41,15 @@ function insert()
 {
     # Fault injection may lead to duplicates
     if [[ "$insert_type" -eq 0 ]]; then
-        $CLICKHOUSE_CLIENT --insert_keeper_fault_injection_probability=0 -q "INSERT INTO alter_table$(($RANDOM % 2)) SELECT $RANDOM, $1" 2>/dev/null
+        $CLICKHOUSE_CLIENT --insert_keeper_fault_injection_probability=0 -q "INSERT INTO alter_table$((RANDOM % 2)) SELECT $RANDOM, $1" 2>/dev/null
     elif [[ "$insert_type" -eq 1 ]]; then
-        $CLICKHOUSE_CLIENT -q "INSERT INTO alter_table$(($RANDOM % 2)) SELECT $1, $1" 2>/dev/null
+        $CLICKHOUSE_CLIENT -q "INSERT INTO alter_table$((RANDOM % 2)) SELECT $1, $1" 2>/dev/null
     else
         # It may reproduce something interesting: if the insert status is unknown (due to fault injection in retries)
         # and the part was committed locally but not in zk, then it will be active and DETACH may detach it.
         # And we will ATTACH it later. But the next INSERT attempt will not be deduplicated because the first one failed.
         # So we will get duplicates.
-        $CLICKHOUSE_CLIENT --insert_deduplication_token=$1 -q "INSERT INTO alter_table$(($RANDOM % 2)) SELECT $RANDOM, $1" 2>/dev/null
+        $CLICKHOUSE_CLIENT --insert_deduplication_token="$1" -q "INSERT INTO alter_table$((RANDOM % 2)) SELECT $RANDOM, $1" 2>/dev/null
     fi
 }
 
@@ -61,7 +61,7 @@ thread_attach & PID_4=$!
 function do_inserts()
 {
     for i in {1..30}; do
-        while ! insert $i; do $CLICKHOUSE_CLIENT -q "SELECT '$CLICKHOUSE_DATABASE', 'retrying insert $i' FORMAT Null"; done
+        while ! insert "$i"; do $CLICKHOUSE_CLIENT -q "SELECT '$CLICKHOUSE_DATABASE', 'retrying insert $i' FORMAT Null"; done
     done
 }
 
