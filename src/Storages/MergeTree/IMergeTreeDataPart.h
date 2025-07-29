0959ffe9c1240606876b593781485381959bf519
robot-clickhouse <robot-clickhouse@users.noreply.github.com>
2025-07-29 15:14:13 +0000

    Backport #84311 to 25.3: Cleanup temp part without global lock when canceling merges

--- src/Storages/MergeTree/IMergeTreeDataPart.h
+++ src/Storages/MergeTree/IMergeTreeDataPart.h
@@ -254,6 +254,7 @@ public:
     /// If true, the destructor will delete the directory with the part.
     /// FIXME Why do we need this flag? What's difference from Temporary and DeleteOnDestroy state? Can we get rid of this?
     bool is_temp = false;
+    std::atomic<bool> is_removed = false;
 
     /// This type and the field remove_tmp_policy is used as a hint
     /// to help avoid communication with keeper when temporary part is deleting.
@@ -621,6 +622,8 @@ public:
     mutable std::atomic<DataPartRemovalState> removal_state = DataPartRemovalState::NOT_ATTEMPTED;
 
     mutable std::atomic<time_t> last_removal_attempt_time = 0;
+
+    void removeIfNeeded();
 
 protected:
     /// Primary key (correspond to primary.idx file).
