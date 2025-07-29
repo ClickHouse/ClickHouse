69ae60e97b75564b0ead393fd291058944e3de1b
Azat Khuzhin <a3at.mail@gmail.com>
2025-07-29 18:49:19 +0200

    ci: replace catch syscall with break _Exit

--- ci/jobs/unit_tests_job.py
+++ ci/jobs/unit_tests_job.py
@@ -2,12 +2,10 @@ from ci.praktika.info import Info
 from ci.praktika.result import Result
 
 if __name__ == "__main__":
-    # With gdb we will capture stacktrace in case of abnormal termination and timeout (45 mins)
-    # 'catch syscall 231' (exit_group) is a hack to avoid exiting with non-zero exit code when process terminated successfully
-    #
     # Note, LSan does not compatible with debugger
     if "asan" not in Info().job_name:
-        command_launcher = f"timeout -v 45m gdb -batch -ex 'handle all nostop' -ex 'set print thread-events off' -ex 'catch syscall 231' -ex run -ex bt -ex 'thread apply all bt' -arg"
+        # With gdb we will capture stacktrace in case of abnormal termination and timeout (45 mins)
+        command_launcher = f"timeout -v 45m gdb -batch -ex 'handle all nostop' -ex 'set print thread-events off' -ex 'b _Exit' -ex run -ex bt -ex 'thread apply all bt' -arg"
     else:
         command_launcher = ""
 
