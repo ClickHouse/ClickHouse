407e14d405a7df36b302dc9fa6f36a3086acf5c9
Nikita Taranov <nikita.taranov@clickhouse.com>
2025-07-30 20:57:22 +0200

    rm redundant setting

--- tests/queries/0_stateless/03277_join_adaptive_spill_oom.sql
+++ tests/queries/0_stateless/03277_join_adaptive_spill_oom.sql
@@ -13,7 +13,6 @@ set max_memory_usage=314572800;
 set collect_hash_table_stats_during_joins=0;
 -- don't limit the memory usage for join
 set max_bytes_in_join=0;
-set enable_producing_buckets_out_of_order_in_aggregation=false;
 
 set enable_adaptive_memory_spill_scheduler=false;
 select t1.k, t2.x, t3.x from adaptive_spill_03277_1 as t1 left join adaptive_spill_03277_2 as t2 on t1.k = t2.k left join adaptive_spill_03277_3 as t3 on t1.k = t3.k Format Null; --{serverError MEMORY_LIMIT_EXCEEDED}
