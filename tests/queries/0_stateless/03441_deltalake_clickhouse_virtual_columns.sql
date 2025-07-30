50e451b25b5cca49a6479237d79765174a197c6a
Pete Hampton <pete@clickhouse.com>
2025-07-30 13:56:06 +0100

    Fix setting

--- tests/queries/0_stateless/03441_deltalake_clickhouse_virtual_columns.sql
+++ tests/queries/0_stateless/03441_deltalake_clickhouse_virtual_columns.sql
@@ -2,6 +2,8 @@
 -- Tag no-fasttest: Depends on AWS
 -- Tag no-msan: delta-kernel is not built with msan
 
+SET parallel_replicas_for_cluster_engines = 0;
+
 SELECT _data_lake_snapshot_version
-FROM deltaLake('https://clickhouse-public-datasets.s3.amazonaws.com/delta_lake/hits/', NOSIGN, SETTINGS parallel_replicas_for_cluster_engines = 0)
+FROM deltaLake('https://clickhouse-public-datasets.s3.amazonaws.com/delta_lake/hits/', NOSIGN)
 LIMIT 1;
