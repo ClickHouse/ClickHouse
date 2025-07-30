89414153609167628dbe845306061fad8a97b321
fastio <pengjian.uestc@tencent.com>
2025-07-30 23:26:21 +0800

    fix integration tests

--- tests/integration/test_external_query_result_cache/test.py
+++ tests/integration/test_external_query_result_cache/test.py
@@ -5,8 +5,8 @@ import sys
 
 import pytest
 import redis
-import uuid 
-import time 
+import uuid
+import time
 
 from helpers.client import QueryRuntimeException
 from helpers.cluster import ClickHouseCluster
@@ -26,35 +26,27 @@ def started_cluster():
     finally:
         cluster.shutdown()
 
-
-def get_redis_connection(db_id=0):
-    client = redis.Redis(
-        host="172.16.1.2", port=6379, password="clickhouse", db=db_id
-    )
-    return client
-
 def check_cache_hits(node, query_id):
     query = "select ProfileEvents['QueryCacheHits'] from system.query_log where query_id='{}' and type='QueryFinish'".format(query_id)
-    result = node.query(query)  
+    result = node.query(query)
     return result.strip() == '1'
 
 def check_cache_misses(node, query_id):
     query = "select ProfileEvents['QueryCacheMisses'] from system.query_log where query_id='{}' and type='QueryFinish'".format(query_id)
-    result = node.query(query)  
+    result = node.query(query)
     return result.strip() == '1'
 
+def drop_query_cache():
+    node1.query(f"system drop query cache")
+
 def test_simple_select(started_cluster):
-    client = get_redis_connection()
-
-    # clean all
-    client.flushall()
-
+    drop_query_cache()
 
     settings = {"use_query_cache":1, "query_cache_ttl":10}
     query_id_node1_1 = str(uuid.uuid4())
     query_id_node1_2 = str(uuid.uuid4())
     query_id_node2_1 = str(uuid.uuid4())
-    
+
     #The first time the query is executed, the cache does not hit.
     node1.query(f"select 1", settings = settings, query_id=query_id_node1_1)
     node1.query(f"select 1", settings = settings, query_id=query_id_node1_2)
@@ -66,56 +58,53 @@ def test_simple_select(started_cluster):
     assert (check_cache_hits(node1, query_id_node1_2) == True)
     assert (check_cache_hits(node2, query_id_node2_1) == True)
 
+    drop_query_cache()
+
 def test_clear_external_cache(started_cluster):
-    client = get_redis_connection()
 
-    # clean all
-    client.flushall()
-
-
+    drop_query_cache()
     settings = {"use_query_cache":1, "query_cache_ttl":10}
     query_id_node1_1 = str(uuid.uuid4())
     query_id_node1_2 = str(uuid.uuid4())
     query_id_node2_1 = str(uuid.uuid4())
-    
+
     #The first time the query is executed, the cache does not hit.
     node1.query(f"select 1", settings = settings, query_id=query_id_node1_1)
-   
+
     # clean all
-    client.flushall()
+    drop_query_cache()
+
     node1.query(f"select 1", settings = settings, query_id=query_id_node1_2)
     node2.query(f"select 1", settings = settings, query_id=query_id_node2_1)
-    
+
     node1.query(f"system flush logs")
     node2.query(f"system flush logs")
-    
+
     assert (check_cache_misses(node1, query_id_node1_1) == True)
     assert (check_cache_misses(node1, query_id_node1_2) == True)
     assert (check_cache_hits(node2, query_id_node2_1) == True)
+    drop_query_cache()
 
 def test_external_cache_ttl(started_cluster):
-    client = get_redis_connection()
-
-    # clean all
-    client.flushall()
-
+    drop_query_cache()
 
     settings = {"use_query_cache":1, "query_cache_ttl":2}
     query_id_node1_1 = str(uuid.uuid4())
     query_id_node1_2 = str(uuid.uuid4())
     query_id_node2_1 = str(uuid.uuid4())
-    
+
     #The first time the query is executed, the cache does not hit.
     node1.query(f"select 1", settings = settings, query_id=query_id_node1_1)
     node1.query(f"select 1", settings = settings, query_id=query_id_node1_2)
-    
+
     time.sleep(2)
     node2.query(f"select 1", settings = settings, query_id=query_id_node2_1)
-    
+
     node1.query(f"system flush logs")
     node2.query(f"system flush logs")
-    
+
 
     assert (check_cache_misses(node1, query_id_node1_1) == True)
     assert (check_cache_hits(node1, query_id_node1_2) == True)
     assert (check_cache_misses(node2, query_id_node2_1) == True)
+    drop_query_cache()
