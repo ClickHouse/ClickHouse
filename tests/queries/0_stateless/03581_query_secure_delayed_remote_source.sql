9a4c9e8b06f677be836ef1f1f05d3efd7531381d
George Larionov <george.larionov@clickhouse.com>
2025-07-31 20:47:11 +0000

    reverting changes to 02863 test and moving to new test

---
+++ tests/queries/0_stateless/03581_query_secure_delayed_remote_source.sql
@@ -0,0 +1,14 @@
+-- Tags: no-parallel, no-fasttest
+-- Tag no-parallel: failpoint is used which can force DelayedSource on other tests
+-- Tag no-fasttest: fasttest is built without SSL so remoteSecure() fails
+
+DROP TABLE IF EXISTS t0;
+CREATE TABLE t0 (c0 Int) ENGINE = Memory;
+
+SYSTEM ENABLE FAILPOINT use_delayed_remote_source;
+
+SELECT count() FROM remoteSecure('localhost:9440', 'default', 't0') AS tx; -- { serverError 265 }
+
+SYSTEM DISABLE FAILPOINT use_delayed_remote_source;
+
+DROP TABLE t0;
