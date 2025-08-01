6744289a6b2aa9fea1c03e9d7617293337c201c8
Antonio Andelic <antonio@clickhouse.com>
2025-08-01 14:19:22 +0200

    Fix test again

--- tests/integration/test_keeper_four_word_command/test.py
+++ tests/integration/test_keeper_four_word_command/test.py
@@ -602,12 +602,12 @@ def test_cmd_csnp(started_cluster):
     try:
         wait_nodes()
         zk = get_fake_zk(node1.name, timeout=30.0)
-        for i in range(10):
+        for i in range(20):
             data = keeper_utils.send_4lw_cmd(cluster, node1, cmd="csnp")
 
             if "Failed to schedule snapshot creation task" not in data:
                 break
-            time.sleep(0.5)
+            time.sleep(1)
         else:
             assert False, "Failed to schedule snapshot after multiple retries"
 
