bf282b24be38830bd1cb3354cc27f257040c6114
kssenii <sumarokovakseniia@mail.ru>
2025-08-01 20:24:47 +0200

    Fix clang tidy

--- src/Storages/ObjectStorage/DataLakes/DeltaLake/EnginePredicate.cpp
+++ src/Storages/ObjectStorage/DataLakes/DeltaLake/EnginePredicate.cpp
@@ -194,7 +194,7 @@ static uintptr_t visitLiteralValue(
         case DB::TypeIndex::String:
         case DB::TypeIndex::FixedString:
         {
-            auto & value_str = value.safeGet<String>();
+            const auto & value_str = value.safeGet<String>();
             auto value_delta_str = KernelUtils::toDeltaString(value_str);
             return KernelUtils::unwrapResult(
                 ffi::visit_expression_literal_string(
@@ -211,7 +211,7 @@ static uintptr_t visitLiteralValue(
         {
             if (isBool(data_type))
             {
-                bool result = value.safeGet<Int16>();
+                bool result = value.safeGet<UInt8>();
                 return ffi::visit_expression_literal_bool(state, result); /// Accepts bool
             }
             else
