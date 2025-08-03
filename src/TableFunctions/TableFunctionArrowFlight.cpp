b8542c152d15592ec9965d6eec135def7c1c1100
Vitaly Baranov <vitlibar@clickhouse.com>
2025-08-03 15:49:06 +0200

    Add documentation for table function arrowFlight().

--- src/TableFunctions/TableFunctionArrowFlight.cpp
+++ src/TableFunctions/TableFunctionArrowFlight.cpp
@@ -108,7 +108,11 @@ void TableFunctionArrowFlight::parseArguments(const ASTPtr & ast_function, Conte
 
 void registerTableFunctionArrowFlight(TableFunctionFactory & factory)
 {
-    factory.registerFunction<TableFunctionArrowFlight>();
+    factory.registerFunction<TableFunctionArrowFlight>(
+        {.documentation
+         = {.description = R"(Allows to perform queries on data exposed via an Apache Arrow Flight server.)",
+            .examples{{"arrowFlight", "SELECT * FROM arrowFlight('127.0.0.1:9005', 'sample_dataset') ORDER BY id;", ""}},
+            .category = FunctionDocumentation::Category::TableFunction}});
 }
 
 }
