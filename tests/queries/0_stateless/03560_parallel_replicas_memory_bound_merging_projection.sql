6d8c7dcdee8c4e2b560b925f8d2e6ef0362f1483
zoomxi <419486879@qq.com>
2025-08-04 12:10:00 +0800

    add a read_in_order_two_level_merge_threshold setting to avoid flaky test

--- tests/queries/0_stateless/03560_parallel_replicas_memory_bound_merging_projection.sql
+++ tests/queries/0_stateless/03560_parallel_replicas_memory_bound_merging_projection.sql
@@ -10,6 +10,7 @@ set max_threads = 4;
 set enable_parallel_replicas = 1, parallel_replicas_for_non_replicated_merge_tree = 1, max_parallel_replicas = 3, cluster_for_parallel_replicas = 'test_cluster_one_shard_three_replicas_localhost';
 set distributed_aggregation_memory_efficient = 1, enable_memory_bound_merging_of_aggregation_results = 1;
 set enable_analyzer = 1, parallel_replicas_local_plan = 1, optimize_use_projections = 1, parallel_replicas_support_projection = 1;
+set read_in_order_two_level_merge_threshold = 1000;
 
 -- { echoOn } --
 set optimize_aggregation_in_order = 0;
