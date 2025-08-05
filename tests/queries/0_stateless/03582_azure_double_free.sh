79afe385648ea398d8695e5c4a8c86723ef0d968
Nikita Taranov <nikita.taranov@clickhouse.com>
2025-08-05 20:44:33 +0200

    upd test

--- tests/queries/0_stateless/03582_azure_double_free.sh
+++ tests/queries/0_stateless/03582_azure_double_free.sh
@@ -5,20 +5,22 @@ CUR_DIR=$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)
 . "$CUR_DIR"/../shell_config.sh
 
 
-$CLICKHOUSE_CLIENT -q "
-INSERT INTO FUNCTION
-azureBlobStorage(
-  'DefaultEndpointsProtocol=http;AccountName=devstoreaccount1;AccountKey=Eby8vdM02xNOcqFlqUwJPLlmEtlCDXJ1OUzFT50uSRZ6IFsuFq2UVErCz4I6tq/K1SZFPTOtr/KBHBeksoGMGw==;BlobEndpoint=http://localhost:10000/devstoreaccount1;',
-  '03582azuredoublefreecont',
-  'big_blob',
-  'Native',
-  'auto',
-  'a UInt64'
-)
-SELECT *
-FROM numbers_mt(1e3)
-SETTINGS azure_truncate_on_insert = 1
-"
+for id in {1..6}; do
+  $CLICKHOUSE_CLIENT -q "
+  INSERT INTO FUNCTION
+  azureBlobStorage(
+    'DefaultEndpointsProtocol=http;AccountName=devstoreaccount1;AccountKey=Eby8vdM02xNOcqFlqUwJPLlmEtlCDXJ1OUzFT50uSRZ6IFsuFq2UVErCz4I6tq/K1SZFPTOtr/KBHBeksoGMGw==;BlobEndpoint=http://localhost:10000/devstoreaccount1;',
+    '03582azuredoublefreecont3',
+    'blob$id.native',
+    'Native',
+    'auto',
+    'a UInt64'
+  )
+  SELECT *
+  FROM numbers_mt(1e3)
+  SETTINGS azure_truncate_on_insert = 1
+  "
+done
 
 # Previously, because we can call `AzureIteratorAsync::getBatchAndCheckNext()` from a thread pool after destruction of a `AzureIteratorAsync` object,
 # the 'libc++abi: Pure virtual function called!' error was happening under the ASan.
@@ -27,12 +29,12 @@ for _ in {1..15}; do
     SELECT *
     FROM azureBlobStorage(
       'DefaultEndpointsProtocol=http;AccountName=devstoreaccount1;AccountKey=Eby8vdM02xNOcqFlqUwJPLlmEtlCDXJ1OUzFT50uSRZ6IFsuFq2UVErCz4I6tq/K1SZFPTOtr/KBHBeksoGMGw==;BlobEndpoint=http://localhost:10000/devstoreaccount1;',
-      'testcont2',
+      '03582azuredoublefreecont3',
       'blob*'
     )
     LIMIT 1
-    SETTINGS azure_list_object_keys_size = 1, max_block_size = 1, max_threads = 1
     FORMAT Null
+    SETTINGS azure_list_object_keys_size = 1, max_block_size = 1, max_threads = 1
   "
 done
 
