77eedd06cccf69c69c088e66de1ad3a8250f9a5b
George Larionov <george.larionov@clickhouse.com>
2025-08-05 17:30:20 +0000

    updating test

--- tests/queries/0_stateless/03581_query_secure_delayed_remote_source.sql
+++ tests/queries/0_stateless/03581_query_secure_delayed_remote_source.sql
@@ -1,15 +1,5 @@
--- Tags: no-parallel, no-fasttest, no-parallel-replicas, no-replicated-database
+-- Tags: no-parallel
 -- Tag no-parallel: failpoint is used which can force DelayedSource on other tests
--- Tag no-fasttest: fasttest is built without SSL so remoteSecure() fails
--- Tag no-parallel-replicas: there is a different code path for parallel replicas which avoids the issue
-
-DROP TABLE IF EXISTS t0;
-CREATE TABLE t0 (c0 Int) ENGINE = Memory;
 
 SYSTEM ENABLE FAILPOINT use_delayed_remote_source;
-
-SELECT count() FROM remoteSecure('localhost:9440', currentDatabase(), 't0') AS tx; -- { serverError 265 }
-
-SYSTEM DISABLE FAILPOINT use_delayed_remote_source;
-
-DROP TABLE t0;
+SELECT count() FROM remoteSecure('localhost');
