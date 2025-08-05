ca30e1dd1d8004a6c3ce107361d05ce4de355cea
Nikita Taranov <nikita.taranov@clickhouse.com>
2025-08-05 20:44:22 +0200

    add test

---
+++ tests/queries/0_stateless/03582_azure_double_free.sh
@@ -0,0 +1,39 @@
+#!/usr/bin/env bash
+
+CUR_DIR=$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)
+# shellcheck source=../shell_config.sh
+. "$CUR_DIR"/../shell_config.sh
+
+
+$CLICKHOUSE_CLIENT -q "
+INSERT INTO FUNCTION
+azureBlobStorage(
+  'DefaultEndpointsProtocol=http;AccountName=devstoreaccount1;AccountKey=Eby8vdM02xNOcqFlqUwJPLlmEtlCDXJ1OUzFT50uSRZ6IFsuFq2UVErCz4I6tq/K1SZFPTOtr/KBHBeksoGMGw==;BlobEndpoint=http://localhost:10000/devstoreaccount1;',
+  '03582azuredoublefreecont',
+  'big_blob',
+  'Native',
+  'auto',
+  'a UInt64'
+)
+SELECT *
+FROM numbers_mt(1e3)
+SETTINGS azure_truncate_on_insert = 1
+"
+
+# Previously, because we can call `AzureIteratorAsync::getBatchAndCheckNext()` from a thread pool after destruction of a `AzureIteratorAsync` object,
+# the 'libc++abi: Pure virtual function called!' error was happening under the ASan.
+for _ in {1..15}; do
+  $CLICKHOUSE_CLIENT -q "
+    SELECT *
+    FROM azureBlobStorage(
+      'DefaultEndpointsProtocol=http;AccountName=devstoreaccount1;AccountKey=Eby8vdM02xNOcqFlqUwJPLlmEtlCDXJ1OUzFT50uSRZ6IFsuFq2UVErCz4I6tq/K1SZFPTOtr/KBHBeksoGMGw==;BlobEndpoint=http://localhost:10000/devstoreaccount1;',
+      'testcont2',
+      'blob*'
+    )
+    LIMIT 1
+    SETTINGS azure_list_object_keys_size = 1, max_block_size = 1, max_threads = 1
+    FORMAT Null
+  "
+done
+
+exit 0
