83df7a24ec80309580621aae7a820d2341c693fd
George Larionov <george.larionov@clickhouse.com>
2025-08-05 20:44:15 +0000

    removing stateless test and adding integration test

--- tests/queries/0_stateless/03581_query_secure_delayed_remote_source.sql
+++
@@ -1,5 +0,0 @@
--- Tags: no-parallel
--- Tag no-parallel: failpoint is used which can force DelayedSource on other tests
-
-SYSTEM ENABLE FAILPOINT use_delayed_remote_source;
-SELECT count() FROM remoteSecure('localhost');
