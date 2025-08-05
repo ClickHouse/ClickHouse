4ccbd572815c697e60939b3563e197e4d26ff242
Konstantin Bogdanov <konstantin@clickhouse.com>
2025-08-05 16:14:25 +0200

    Fix test harder

--- tests/queries/0_stateless/03275_auto_cluster_functions_with_parallel_replicas.sql
+++ tests/queries/0_stateless/03275_auto_cluster_functions_with_parallel_replicas.sql
@@ -6,11 +6,12 @@ SET enable_parallel_replicas=1;
 SET max_parallel_replicas=4;
 SET cluster_for_parallel_replicas='test_cluster_two_shards';
 SET parallel_replicas_for_cluster_engines=true;
+SET query_plan_join_swap_table=0;
 
 EXPLAIN SELECT * FROM url('http://localhost:8123');
 EXPLAIN SELECT * FROM s3('http://localhost:11111/test/a.tsv', 'TSV');
 EXPLAIN SELECT * FROM s3('http://localhost:11111/test/a.tsv', 'TSV') where c1 in (SELECT c1 FROM s3('http://localhost:11111/test/a.tsv', 'TSV'));
-SELECT explain FROM (EXPLAIN SELECT number FROM system.numbers n JOIN (SELECT * FROM s3('http://localhost:11111/test/a.tsv', 'TSV')) s ON (toInt64(n.number) = toInt64(s.c1))) ORDER BY explain DESC;
+EXPLAIN SELECT number FROM system.numbers n JOIN (SELECT * FROM s3('http://localhost:11111/test/a.tsv', 'TSV')) s ON (toInt64(n.number) = toInt64(s.c1));
 
 SET parallel_replicas_for_cluster_engines=false;
 
