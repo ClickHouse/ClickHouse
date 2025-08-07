e4040b19fc82b8edce9554bf6bdf9cb607f6760e
Kseniia Sumarokova <54203879+kssenii@users.noreply.github.com>
2025-08-07 21:18:13 +0200

    Update DeltaLakeMetadataDeltaKernel.cpp

--- src/Storages/ObjectStorage/DataLakes/DeltaLakeMetadataDeltaKernel.cpp
+++ src/Storages/ObjectStorage/DataLakes/DeltaLakeMetadataDeltaKernel.cpp
@@ -3,6 +3,7 @@
 #if USE_PARQUET && USE_DELTA_KERNEL_RS
 #include <Storages/ObjectStorage/DataLakes/DeltaLakeMetadataDeltaKernel.h>
 #include <Storages/ObjectStorage/DataLakes/DeltaLake/TableSnapshot.h>
+#include <Common/logger_useful.h>
 #include <fmt/ranges.h>
 
 namespace DB
