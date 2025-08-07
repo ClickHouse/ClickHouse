ffd0c1c7f7ad9e5b5aabcb004dcde400f87a8132
Arthur Passos <arthur.ti@outlook.com>
2025-08-06 21:34:19 -0300

    add security tests

--- tests/queries/0_stateless/03591_optimize_prewhere_row_policy.sql
+++ tests/queries/0_stateless/03591_optimize_prewhere_row_policy.sql
@@ -1,12 +1,26 @@
+SET use_query_condition_cache=0;
+
 DROP TABLE IF EXISTS 03591_test;
 
 DROP ROW POLICY IF EXISTS 03591_rp ON 03591_test;
 
 CREATE TABLE 03591_test (a Int32, b Int32) ENGINE=MergeTree ORDER BY tuple();
 
-INSERT INTO 03591_test VALUES (1, 2);
+INSERT INTO 03591_test VALUES (1, 1), (2, 2);
 
-CREATE ROW POLICY 03591_rp ON 03591_test USING a=1 TO CURRENT_USER;
+SELECT * FROM 03591_test;
 
--- Print plan with actions to make sure both a=1 and b=2 are present in the prewhere section
+SELECT * FROM 03591_test WHERE throwIf(b=2, 'Should throw') SETTINGS optimize_move_to_prewhere = 1; -- {serverError FUNCTION_THROW_IF_VALUE_IS_NON_ZERO}
+
+CREATE ROW POLICY 03591_rp ON 03591_test USING b=1 TO CURRENT_USER;
+
+SELECT * FROM 03591_test;
+
+-- Print plan with actions to make sure both b=1 and b=2 are present in the prewhere section
 EXPLAIN PLAN actions=1 SELECT * FROM 03591_test WHERE b=2 SETTINGS optimize_move_to_prewhere = 1;
+
+SELECT * FROM 03591_test WHERE throwIf(b=2, 'Should not throw because b=2 is not visible to this user due to the b=1 row policy') SETTINGS optimize_move_to_prewhere = 1;
+
+DROP ROW POLICY 03591_rp ON 03591_test;
+
+SELECT * FROM 03591_test WHERE throwIf(b=2, 'Should throw') SETTINGS optimize_move_to_prewhere = 1; -- {serverError FUNCTION_THROW_IF_VALUE_IS_NON_ZERO}
