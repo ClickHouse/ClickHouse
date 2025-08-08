8558e96eed17f25e3b577f6921dc67e2095caf32
talmawash <tariqalmawash@gmail.com>
2025-08-08 22:28:05 +0300

    add func pushdown optimization

--- src/Processors/QueryPlan/Optimizations/optimizeTree.cpp
+++ src/Processors/QueryPlan/Optimizations/optimizeTree.cpp
@@ -192,6 +192,8 @@ void optimizeTreeSecondPass(
 
         auto & frame = stack.back();
 
+        pushDownVolumeReducingFunctions(frame.node, &root, optimization_settings);
+
         /// Traverse all children first.
         if (frame.next_child < frame.node->children.size())
         {
