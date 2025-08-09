4c329815df274f94c2cb4de91b02a52e859647aa
Konstantin Vedernikov <vedernikovkostya@mail.ru>
2025-08-09 08:39:10 +0000

    add settings to history

--- src/Core/SettingsChangesHistory.cpp
+++ src/Core/SettingsChangesHistory.cpp
@@ -64,7 +64,10 @@ const VersionToSettingsChangesMap & getSettingsChangesHistory()
             {"output_format_orc_compression_block_size", 65536, 262144, "New setting"},
             {"vector_search_index_fetch_multiplier", 1.0, 1.0, "New setting, replaces the obsolete 'vector_search_postfilter_multiplier' setting"},
             {"backup_slow_all_threads_after_retryable_s3_error", true, true, "New setting"},
-
+            {"iceberg_period_compaction", 0, 0, "New setting "},
+            {"iceberg_compaction_backoff_time", 0, 0, "New setting "},
+            {"allow_experimental_iceberg_compaction", 0, 0, "New setting "},
+            {"allow_experimental_iceberg_background_compaction", 0, 0, "New setting "},
         });
         addSettingsChanges(settings_changes_history, "25.7",
         {
