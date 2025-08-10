4d9428fcb832d8e07a6188a237f06d2ee5af4e40
Alexey Milovidov <milovidov@clickhouse.com>
2025-08-11 00:10:35 +0200

    Fix `bech32`

--- src/Functions/bech32.cpp
+++ src/Functions/bech32.cpp
@@ -12,6 +12,7 @@
 
 #include <bech32.h>
 
+
 namespace
 {
 /** Max length of Bech32 or Bech32m encoding is 90 chars, this includes:
@@ -237,17 +238,13 @@ private:
         ColumnString::Offsets & out_offsets = out_col->getOffsets();
 
         out_offsets.resize(input_rows_count);
-        out_vec.resize((max_address_len + 1 /* trailing 0 */) * input_rows_count);
+        out_vec.resize(max_address_len * input_rows_count);
 
         char * out_begin = reinterpret_cast<char *>(out_vec.data());
         char * out_pos = out_begin;
 
         size_t human_readable_part_prev_offset = 0;
         size_t data_prev_offset = 0;
-
-        /// In ColumnString each value ends with a trailing 0, in ColumnFixedString there is no trailing 0
-        size_t human_readable_part_zero_offset = human_readable_part_width == 0 ? 1 : 0;
-        size_t data_zero_offset = data_width == 0 ? 1 : 0;
 
         for (size_t i = 0; i < input_rows_count; ++i)
         {
@@ -260,8 +257,8 @@ private:
 
             /// max encodable data to stay within 90-char limit on Bech32 output
             /// human_readable_part must be at least 1 character and no more than 83
-            auto data_len = data_new_offset - data_prev_offset - data_zero_offset;
-            auto human_readable_part_len = human_readable_part_new_offset - human_readable_part_prev_offset - human_readable_part_zero_offset;
+            auto data_len = data_new_offset - data_prev_offset;
+            auto human_readable_part_len = human_readable_part_new_offset - human_readable_part_prev_offset;
             if (data_len > max_data_len || human_readable_part_len > max_human_readable_part_len || human_readable_part_len < 1)
             {
                 finalizeRow(out_offsets, out_pos, out_begin, i);
@@ -273,11 +270,11 @@ private:
 
             std::string human_readable_part(
                 reinterpret_cast<const char *>(&human_readable_part_vec[human_readable_part_prev_offset]),
-                reinterpret_cast<const char *>(&human_readable_part_vec[human_readable_part_new_offset - human_readable_part_zero_offset]));
+                reinterpret_cast<const char *>(&human_readable_part_vec[human_readable_part_new_offset]));
 
             bech32_data input(
                 reinterpret_cast<const uint8_t *>(&data_vec[data_prev_offset]),
-                reinterpret_cast<const uint8_t *>(&data_vec[data_new_offset - data_zero_offset]));
+                reinterpret_cast<const uint8_t *>(&data_vec[data_new_offset]));
 
             uint8_t witness_version = have_witness_version ? witness_version_col->getUInt(i) : default_witness_version;
 
@@ -390,8 +387,8 @@ private:
         human_readable_part_offsets.resize(input_rows_count);
         data_offsets.resize(input_rows_count);
 
-        human_readable_part_vec.resize((max_human_readable_part_len + 1 /* trailing 0 */) * input_rows_count);
-        data_vec.resize((max_data_len + 1 /* trailing 0 */) * input_rows_count);
+        human_readable_part_vec.resize(max_human_readable_part_len * input_rows_count);
+        data_vec.resize(max_data_len * input_rows_count);
 
         char * human_readable_part_begin = reinterpret_cast<char *>(human_readable_part_vec.data());
         char * human_readable_part_pos = human_readable_part_begin;
@@ -400,9 +397,6 @@ private:
         char * data_pos = data_begin;
 
         size_t prev_offset = 0;
-
-        /// In ColumnString each value ends with a trailing 0, in ColumnFixedString there is no trailing 0
-        size_t trailing_zero_offset = col_width == 0 ? 1 : 0;
 
         for (size_t i = 0; i < input_rows_count; ++i)
         {
@@ -413,7 +407,7 @@ private:
                 --new_offset;
 
             /// enforce char limit
-            if ((new_offset - prev_offset - trailing_zero_offset) > max_address_len)
+            if (new_offset - prev_offset > max_address_len)
             {
                 finalizeRow(human_readable_part_offsets, human_readable_part_pos, human_readable_part_begin, i);
                 finalizeRow(data_offsets, data_pos, data_begin, i);
@@ -424,7 +418,7 @@ private:
 
             std::string input(
                 reinterpret_cast<const char *>(&in_vec[prev_offset]),
-                reinterpret_cast<const char *>(&in_vec[new_offset - trailing_zero_offset]));
+                reinterpret_cast<const char *>(&in_vec[new_offset]));
 
             const auto dec = bech32::decode(input);
 
