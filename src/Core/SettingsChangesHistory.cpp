eebf12adc0d1e72b54847a1cad07102ad6e3332d
Konstantin Vedernikov <vedernikovkostya@mail.ru>
2025-08-11 09:51:25 +0000

    fix

--- src/Core/SettingsChangesHistory.cpp
+++ src/Core/SettingsChangesHistory.cpp
@@ -68,7 +68,7 @@ const VersionToSettingsChangesMap & getSettingsChangesHistory()
             {"backup_restore_s3_retry_jitter_factor", 0.0, 0.1, "New setting"},
             {"vector_search_index_fetch_multiplier", 1.0, 1.0, "New setting, replaces the obsolete 'vector_search_postfilter_multiplier' setting"},
             {"backup_slow_all_threads_after_retryable_s3_error", true, true, "New setting"},
-            {"output_format_compression_level", "", "", "New setting"},
+            {"iceberg_metadata_compression_method", "", "", "New setting"},
         });
         addSettingsChanges(settings_changes_history, "25.7",
         {
