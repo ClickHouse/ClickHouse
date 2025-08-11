016e4d9f3fa15b9d5689316f0d072d5ad4ac1aaa
Alexander Gololobov <davenger@clickhouse.com>
2025-08-11 15:40:11 +0200

    More test cases

--- tests/queries/0_stateless/03583_rewrite_in_to_join.sql
+++ tests/queries/0_stateless/03583_rewrite_in_to_join.sql
@@ -1,3 +1,4 @@
+SET enable_analyzer=1;
 SET allow_experimental_correlated_subqueries=1;
 
 -- {echoOn}
@@ -5,5 +6,12 @@ SELECT number IN (SELECT * FROM numbers(2)) FROM numbers(3);
 
 SELECT number IN (SELECT number FROM numbers(2)) FROM numbers(3);
 
+SELECT * FROM numbers(3) WHERE number IN (SELECT number FROM numbers(2));
+
 SELECT number IN (SELECT number, number FROM numbers(2)) FROM numbers(3); -- {serverError NUMBER_OF_COLUMNS_DOESNT_MATCH,BAD_ARGUMENTS}
+
+SELECT number IN (SELECT number IN (SELECT * FROM numbers(1)) FROM numbers(2)) FROM numbers(3);
+
+SELECT number IN (SELECT number FROM numbers(2) WHERE number IN (SELECT * FROM numbers(1))) FROM numbers(3);
+
 --{echoOff}
