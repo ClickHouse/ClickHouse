f292fbb98ddead1acbe172c5a05c2aaf493fa788
Igor Nikonov <igor@clickhouse.com>
2025-08-12 16:34:35 +0200

    Fix test

--- tests/queries/0_stateless/03582_pr_read_in_order_hits.sql
+++ tests/queries/0_stateless/03582_pr_read_in_order_hits.sql
@@ -3,7 +3,7 @@ SET max_threads = 0; -- let's reset to automatic detection of the number of thre
 
 SELECT DISTINCT
     CounterID,
-FROM hits
+FROM test.hits
 WHERE (URL ILIKE '%rambler%') AND (EventDate = '2013-07-15')
 ORDER BY CounterID DESC
 SETTINGS enable_parallel_replicas = 0;
@@ -12,7 +12,7 @@ SELECT '-----------';
 
 SELECT DISTINCT
     CounterID,
-FROM hits
+FROM test.hits
 WHERE (URL ILIKE '%rambler%') AND (EventDate = '2013-07-15')
 ORDER BY CounterID DESC
 SETTINGS enable_parallel_replicas = 1, max_parallel_replicas=3, parallel_replicas_for_non_replicated_merge_tree = 1, cluster_for_parallel_replicas = 'test_cluster_one_shard_three_replicas_localhost';
