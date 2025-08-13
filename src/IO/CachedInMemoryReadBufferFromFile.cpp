956b3191f389a82bcf5c785eaea8f5bcbfc37e9a
Antonio Andelic <antonio@clickhouse.com>
2025-08-13 14:24:33 +0200

    Fix test

--- src/IO/CachedInMemoryReadBufferFromFile.cpp
+++ src/IO/CachedInMemoryReadBufferFromFile.cpp
@@ -224,9 +224,9 @@ bool CachedInMemoryReadBufferFromFile::isContentCached(size_t offset, size_t /*s
     }
 
     size_t block_size = settings.page_cache_block_size;
-    cache_key.offset = offset / block_size * block_size;
-    cache_key.size = std::min(block_size, file_size.value() - cache_key.offset);
-
+    auto old_offset = std::exchange(cache_key.offset, offset / block_size * block_size);
+    auto old_size = std::exchange(cache_key.size, std::min(block_size, file_size.value() - cache_key.offset));
+    SCOPE_EXIT(cache_key.offset = old_offset; cache_key.size = old_size;);
     return cache->contains(cache_key, settings.page_cache_inject_eviction);
 }
 
