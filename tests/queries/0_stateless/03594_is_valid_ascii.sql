865959e346aa6ff715032d05746016dd5311c045
Luke Pullicino <59690689+LPullicino@users.noreply.github.com>
2025-08-13 22:36:50 +0200

    ♻️Changed test files

--- tests/queries/0_stateless/03594_is_valid_ascii.sql
+++ tests/queries/0_stateless/03594_is_valid_ascii.sql
@@ -8,3 +8,17 @@ SELECT 1 = isValidASCII('\x7F\x00') FROM system.numbers LIMIT 1;
 SELECT 0 = isValidASCII('какой-то текст') FROM system.numbers LIMIT 1;
 SELECT 0 = isValidASCII('\xC2\x80') FROM system.numbers LIMIT 1;
 SELECT 0 = isValidASCII('hello world!') FROM system.numbers LIMIT 1;
+
+-- Testing Alias
+
+SELECT 1 = isASCII('') FROM system.numbers LIMIT 1;
+SELECT 1 = isASCII('some text') FROM system.numbers LIMIT 1;
+SELECT 1 = isASCII('\x00') FROM system.numbers LIMIT 1;
+SELECT 1 = isASCII('\x66') FROM system.numbers LIMIT 1;
+SELECT 1 = isASCII('\x7F') FROM system.numbers LIMIT 1;
+SELECT 1 = isASCII('\x00\x7F') FROM system.numbers LIMIT 1;
+SELECT 1 = isASCII('\x7F\x00') FROM system.numbers LIMIT 1;
+SELECT 0 = isASCII('какой-то текст') FROM system.numbers LIMIT 1;
+SELECT 0 = isASCII('\xC2\x80') FROM system.numbers LIMIT 1;
+SELECT 0 = isASCII('hello world!') FROM system.numbers LIMIT 1;
+
