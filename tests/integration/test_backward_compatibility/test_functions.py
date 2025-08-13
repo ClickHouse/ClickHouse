dbdbe4f9522f003725e1a233b81c2e97e9b7314e
Alexey Milovidov <milovidov@clickhouse.com>
2025-08-13 22:35:11 +0200

    Fix integration test

--- tests/integration/test_backward_compatibility/test_functions.py
+++ tests/integration/test_backward_compatibility/test_functions.py
@@ -212,6 +212,8 @@ def test_string_functions(start_cluster):
         "filesystemAvailable",
         # Exclude it for now. Looks like the result depends on the build type.
         "farmHash64",
+        # Depends on the in-memory format of String columns, that we legitimately changed.
+        "byteSize",
     ]
     functions = filter(lambda x: x not in excludes, functions)
 
