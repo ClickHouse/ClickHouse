9c5728b5246530a2fad4e6bd026dbc8459bd4ab8
“JiaQi <2963103258@qq.com>
2025-08-13 11:20:11 +0800

    fix style

--- src/Parsers/FunctionSecretArgumentsFinder.h
+++ src/Parsers/FunctionSecretArgumentsFinder.h
@@ -102,7 +102,7 @@ protected:
         }
         else if ((function->name() == "s3") || (function->name() == "cosn") || (function->name() == "oss") ||
                  (function->name() == "deltaLake") || (function->name() == "hudi") || (function->name() == "iceberg") ||
-                 (function->name() == "gcs") || (function->name() == "icebergS3") || (function->name() == "paimon") || 
+                 (function->name() == "gcs") || (function->name() == "icebergS3") || (function->name() == "paimon") ||
                  (function->name() == "paimonS3"))
         {
             /// s3('url', 'aws_access_key_id', 'aws_secret_access_key', ...)
