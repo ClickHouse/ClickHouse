f0fa8b73616fb66aec036e84cfcae4c9b67ee6de
Azat Khuzhin <a3at.mail@gmail.com>
2025-08-14 14:42:47 +0200

    tests: simplify 00076_system_columns_bytes

--- tests/queries/0_stateless/00076_system_columns_bytes.sql
+++ tests/queries/0_stateless/00076_system_columns_bytes.sql
@@ -1,3 +1,4 @@
--- Tags: stateful
+-- Ensure that there will be at least one non-empty MergeTree table
+SYSTEM FLUSH LOGS text_log;
 -- NOTE: database = currentDatabase() is not mandatory
 SELECT sum(data_compressed_bytes) > 0, sum(data_uncompressed_bytes) > 0, sum(marks_bytes) > 0 FROM system.columns;
