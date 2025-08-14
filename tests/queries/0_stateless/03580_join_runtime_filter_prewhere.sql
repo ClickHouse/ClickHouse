e824ee60a568dd3e42dacdaea0d61f80e0d87674
Alexander Gololobov <davenger@clickhouse.com>
2025-08-14 22:05:37 +0200

    Test with swapped tables

--- tests/queries/0_stateless/03580_join_runtime_filter_prewhere.sql
+++ tests/queries/0_stateless/03580_join_runtime_filter_prewhere.sql
@@ -7,7 +7,7 @@ INSERT INTO customer SELECT number, 5 FROM numbers(500);
 
 
 SELECT '-- Check that filter on c_nationkey is moved to PREWHERE';
-SELECT REGEXP_REPLACE(explain, '_runtime_filter_\\d+', '_runtime_filter_UNIQ_ID')
+SELECT REGEXP_REPLACE(trimLeft(explain), '_runtime_filter_\\d+', '_runtime_filter_UNIQ_ID')
 FROM
 (
     SELECT *
@@ -20,4 +20,21 @@ FROM
         SETTINGS enable_join_runtime_filters = 1, optimize_move_to_prewhere = 1, query_plan_join_swap_table = 0
     ))
 )
-WHERE (explain LIKE '%ReadFromMergeTree%') OR (explain LIKE '%Prewhere filter column:%');
+WHERE (explain LIKE '%ReadFromMergeTree%') OR (explain LIKE '%Prewhere filter column:%') OR (explain LIKE '%Build%');
+
+
+SELECT '-- Check the same query but with swapped tables';
+SELECT REGEXP_REPLACE(trimLeft(explain), '_runtime_filter_\\d+', '_runtime_filter_UNIQ_ID')
+FROM
+(
+    SELECT *
+    FROM viewExplain('EXPLAIN', 'actions = 1', (
+        SELECT
+            count(),
+            max(c_custkey)
+        FROM nation, customer
+        WHERE (c_nationkey = n_nationkey) AND (n_name = 'FRANCE')
+        SETTINGS enable_join_runtime_filters = 1, optimize_move_to_prewhere = 1, query_plan_join_swap_table = 1
+    ))
+)
+WHERE (explain LIKE '%ReadFromMergeTree%') OR (explain LIKE '%Prewhere filter column:%') OR (explain LIKE '%Build%');
