6aebc36a332ca1de3cbf3146756b3c90ea9f74be
Shaun Struwig <41984034+Blargian@users.noreply.github.com>
2025-08-14 10:53:41 +0200

    Update src/Core/Settings.cpp

--- src/Core/Settings.cpp
+++ src/Core/Settings.cpp
@@ -2629,7 +2629,7 @@ Possible values:
 Prefer maximum block bytes for external sort, reduce the memory usage during merging.
 )", 0) \
     DECLARE(UInt64, max_bytes_before_external_sort, 0, R"(
-Cloud default value: `0`
+Cloud default value: half the memory amount per replica.
 
 Enables or disables execution of `ORDER BY` clauses in external memory. See [ORDER BY Implementation Details](../../sql-reference/statements/select/order-by.md#implementation-details)
 If memory usage during ORDER BY operation exceeds this threshold in bytes, the 'external sorting' mode (spill data to disk) is activated.
