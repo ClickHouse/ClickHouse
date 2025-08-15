04de59a740d3fb7ab2d6757a467e986777f532dc
Joel Walker <joelwalker1995@gmail.com>
2025-08-15 01:44:13 -0700

    Added an integration test for outgoing postgres metrics and events

--- tests/integration/test_storage_postgresql/test.py
+++ tests/integration/test_storage_postgresql/test.py
@@ -892,6 +892,43 @@ def test_postgres_datetime(started_cluster):
     assert result == "2025-01-02 03:04:05.678900\n"
 
 
+def test_postgres_outgoing_metrics(started_cluster):
+    cursor = started_cluster.postgres_conn.cursor()
+
+    # Run a query to make sure at least one postgres connection is created
+    check = "SELECT 123 FROM postgresql(`postgres5`)"
+    assert (node1.query(check)).rstrip() == "123"
+
+    postgres_connection_events = int(
+        node1.query(
+            "SELECT value FROM system.events WHERE event='PostgreSQLClientConnectionsCreated'"
+        )
+    )
+
+    postgres_current_connections = int(
+        node1.query(
+            "SELECT value FROM system.metrics WHERE metric='PostgreSQLClientConnections'"
+        )
+    )
+
+    postgres_idle_connections = int(
+        node1.query(
+            "SELECT value FROM system.metrics WHERE metric='PostgreSQLClientConnectionsIdle'"
+        )
+    )
+
+    postgres_active_connections = int(
+        node1.query(
+            "SELECT value FROM system.metrics WHERE metric='PostgreSQLClientConnectionsInUse'"
+        )
+    )
+
+    assert postgres_connection_events > 0
+    assert postgres_current_connections > 0
+    assert postgres_idle_connections > 0
+    assert postgres_active_connections == 0
+
+
 if __name__ == "__main__":
     cluster.start()
     input("Cluster created, press any key to destroy...")
