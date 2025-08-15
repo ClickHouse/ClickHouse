88fc5d24925077ff46a83e7705f3750a19cc5795
Azat Khuzhin <a3at.mail@gmail.com>
2025-08-15 14:29:27 +0200

    Revert "tests: simplify 00076_system_columns_bytes"
    We cannot do this since text_log may have only Compact parts and for
    then, we do not calculate values properly
    
    This reverts commit f0fa8b73616fb66aec036e84cfcae4c9b67ee6de.

--- tests/queries/0_stateless/00076_system_columns_bytes.sql
+++ tests/queries/0_stateless/00076_system_columns_bytes.sql
@@ -1,4 +1,3 @@
--- Ensure that there will be at least one non-empty MergeTree table
-SYSTEM FLUSH LOGS text_log;
+-- Tags: stateful
 -- NOTE: database = currentDatabase() is not mandatory
 SELECT sum(data_compressed_bytes) > 0, sum(data_uncompressed_bytes) > 0, sum(marks_bytes) > 0 FROM system.columns;
