21c5fe171b455680e5467a8f7e904ced9d995613
Konstantin Bogdanov <konstantin@clickhouse.com>
2025-08-15 02:57:40 +0200

    Figuring out how to use passwords in CI by random walk

--- ci/defs/defs.py
+++ ci/defs/defs.py
@@ -64,6 +64,10 @@ SECRETS = [
         name="clickhouse-test-stat-password",
         type=Secret.Type.AWS_SSM_VAR,
     ),
+    Secret.Config(
+        name="chcache_password",
+        type=Secret.Type.AWS_SSM_VAR,
+    ),
     azure_secret,
     Secret.Config(
         name="woolenwolf_gh_app.clickhouse-app-id",
