8cc8acb1e080ff935496f681ac210328f657dd67
Alexander Gololobov <davenger@clickhouse.com>
2025-08-18 16:28:21 +0200

    Test case for ANY join

--- tests/queries/0_stateless/03580_join_runtime_filter.sql
+++ tests/queries/0_stateless/03580_join_runtime_filter.sql
@@ -62,3 +62,19 @@ WHERE (explain ILIKE '%Filter column%') OR (explain LIKE '%BuildRuntimeFilter%')
 SELECT count()
 FROM customer, nation
 WHERE c_nationkey = n_nationkey AND n_nationkey%10 = c_custkey%100;
+
+
+-- ANY JOIN
+SELECT REGEXP_REPLACE(trimLeft(explain), '_runtime_filter_\\d+', '_runtime_filter_UNIQ_ID')
+FROM (
+    EXPLAIN actions=1
+    SELECT count()
+    FROM customer ANY JOIN nation
+    ON c_nationkey = n_nationkey
+    SETTINGS query_plan_join_swap_table=0
+)
+WHERE (explain ILIKE '%Filter column%') OR (explain LIKE '%BuildRuntimeFilter%') OR (explain LIKE '% Type:%') OR (explain LIKE '% Strictness:%');
+
+SELECT count()
+FROM customer ANY JOIN nation
+ON c_nationkey = n_nationkey;
