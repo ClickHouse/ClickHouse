3a751617371dcc3b4083ff8445eff1aa09c283b8
travis <travisdang@outlook.com>
2025-08-18 10:42:56 -0700

    Update query-level tests to return setting instead of 1

--- tests/queries/0_stateless/03595_set_query_no_eq_set_to_one.sql
+++ tests/queries/0_stateless/03595_set_query_no_eq_set_to_one.sql
@@ -1,7 +1,8 @@
-SELECT 1 SETTINGS force_index_by_date;
-SELECT 1 SETTINGS force_index_by_date = 1;
-SELECT 1 SETTINGS force_index_by_date = 0;
-SELECT name, value, changed, default FROM system.settings WHERE name = 'force_index_by_date';
+SELECT name, value, changed, default FROM system.settings WHERE name = 'force_index_by_date' SETTINGS force_index_by_date;
+SELECT name, value, changed, default FROM system.settings WHERE name = 'force_index_by_date' SETTINGS force_index_by_date = 0;
+SELECT name, value, changed, default FROM system.settings WHERE name = 'force_index_by_date' SETTINGS force_index_by_date = 1;
+SELECT name, value, changed, default FROM system.settings WHERE name = 'force_index_by_date' SETTINGS force_index_by_date = DEFAULT;
+
 SET force_index_by_date;
 SELECT name, value, changed, default FROM system.settings WHERE name = 'force_index_by_date';
 SET force_index_by_date = DEFAULT;
@@ -11,8 +12,7 @@ SELECT name, value, changed, default FROM system.settings WHERE name = 'force_in
 SET force_index_by_date = 0;
 SELECT name, value, changed, default FROM system.settings WHERE name = 'force_index_by_date';
 
-SELECT 1 SETTINGS force_index_by_date, log_queries;
-SELECT name, value, changed, default FROM system.settings WHERE name IN ('force_index_by_date', 'log_queries');
+SELECT name, value, changed, default FROM system.settings WHERE name IN ('force_index_by_date', 'log_queries') SETTINGS force_index_by_date, log_queries;
 SET force_index_by_date, log_queries = 0;
 SELECT name, value, changed, default FROM system.settings WHERE name IN ('force_index_by_date', 'log_queries');
 SET force_index_by_date = 0, log_queries;
