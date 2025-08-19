223311628f83bce574cd2d5ec6e6f0f720e241ee
Dmitry Novik <n0vik@clickhouse.com>
2025-08-19 17:41:18 +0200

    Update tests

--- tests/queries/0_stateless/03545_analyzer_correlated_subquery_exists_asterisk.sql
+++ tests/queries/0_stateless/03545_analyzer_correlated_subquery_exists_asterisk.sql
@@ -1,6 +1,7 @@
 SET enable_analyzer = 1;
 SET allow_experimental_correlated_subqueries = 1;
 SET enable_parallel_replicas = 0;
+SET correlated_subqueries_default_join_kind = 'left';
 
 -- Disable table swaps during query planning
 SET query_plan_join_swap_table = false;
