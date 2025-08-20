4fa0665a1bda0bec90a48ca96efb374d0383eaaa
avogar <avogar@clickhouse.com>
2025-08-20 11:11:56 +0000

    Add stateful flag to the new test

--- tests/queries/0_stateless/03595_extract_url_parameters.sql
+++ tests/queries/0_stateless/03595_extract_url_parameters.sql
@@ -1,3 +1,3 @@
--- Tags: no-fasttest
+-- Tags: no-fasttest, stateful
 SELECT sum(cityHash64(extractURLParameters(URL))) FROM test.hits;
 SELECT sum(cityHash64(extractURLParameterNames(URL))) FROM test.hits;
