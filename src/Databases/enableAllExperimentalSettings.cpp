d5af3cb60395aee329bb83673a7fe547ff22d4eb
melvynator <melvyn.peignon@gmail.com>
2025-08-20 21:20:37 +0200

    Back to trying to fix test

--- src/Databases/enableAllExperimentalSettings.cpp
+++ src/Databases/enableAllExperimentalSettings.cpp
@@ -57,9 +57,6 @@ void enableAllExperimentalSettings(ContextMutablePtr context)
     context->setSetting("allow_experimental_database_unity_catalog", 1);
     context->setSetting("allow_experimental_database_glue_catalog", 1);
     context->setSetting("allow_experimental_delta_kernel_rs", 1);
-    context->setSetting("allow_experimental_ytsaurus_table_function", 1);
-    context->setSetting("allow_experimental_ytsaurus_table_engine", 1);
-    context->setSetting("allow_experimental_ytsaurus_dictionary_source", 1);
     context->setSetting("allow_experimental_time_series_aggregate_functions", 1);
     context->setSetting("allow_experimental_lightweight_update", 1);
     context->setSetting("allow_experimental_insert_into_iceberg", 1);
