602ea4e192dca0ece27a37071267c17373434b68
Bharat Nallan Chakravarthy <bharat@clickhouse.com>
2025-08-19 18:26:29 -0700

    fix setting

--- tests/queries/0_stateless/00365_statistics_in_formats.sh
+++ tests/queries/0_stateless/00365_statistics_in_formats.sh
@@ -13,9 +13,9 @@ $CLICKHOUSE_CLIENT --query="SELECT number FROM numbers LIMIT 10 FORMAT JSONCompa
 $CLICKHOUSE_CLIENT --query="SELECT number FROM numbers LIMIT 10 FORMAT XML" | grep 'rows_read';
 
 # use_query_cache=0 - to ensure that results are not cached and we always get the proper statistics for number of rows read
-# wait_end_of_query=1 - to ensure that the query statistics are sent after the query is fully executed
-${CLICKHOUSE_CURL} -sS "${CLICKHOUSE_URL}?use_query_cache=0&wait_end_of_query=1" -d "SELECT number FROM numbers LIMIT 10 FORMAT JSON" | grep 'rows_read';
-${CLICKHOUSE_CURL} -sS "${CLICKHOUSE_URL}?use_query_cache=0&wait_end_of_query=1" -d "SELECT number FROM numbers LIMIT 10 FORMAT JSONCompact" | grep 'rows_read';
-${CLICKHOUSE_CURL} -sS "${CLICKHOUSE_URL}?use_query_cache=0&wait_end_of_query=1" -d "SELECT number FROM numbers LIMIT 10 FORMAT XML" | grep 'rows_read';
+# http_wait_end_of_query=1 - to ensure that the query statistics are sent after the query is fully executed
+${CLICKHOUSE_CURL} -sS "${CLICKHOUSE_URL}?use_query_cache=0&http_wait_end_of_query=1" -d "SELECT number FROM numbers LIMIT 10 FORMAT JSON" | grep 'rows_read';
+${CLICKHOUSE_CURL} -sS "${CLICKHOUSE_URL}?use_query_cache=0&http_wait_end_of_query=1" -d "SELECT number FROM numbers LIMIT 10 FORMAT JSONCompact" | grep 'rows_read';
+${CLICKHOUSE_CURL} -sS "${CLICKHOUSE_URL}?use_query_cache=0&http_wait_end_of_query=1" -d "SELECT number FROM numbers LIMIT 10 FORMAT XML" | grep 'rows_read';
 
 $CLICKHOUSE_CLIENT --query="DROP TABLE IF EXISTS numbers";
