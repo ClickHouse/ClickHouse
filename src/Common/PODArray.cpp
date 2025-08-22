159cad48975a131b396973c44d3557c94dbc7815
Michael Kolupaev <michael.kolupaev@clickhouse.com>
2025-08-22 04:35:31 +0000

    More-careful inlining in PODArray reserve/resize/insert

--- src/Common/PODArray.cpp
+++ src/Common/PODArray.cpp
@@ -22,20 +22,9 @@ void protectMemoryRegion(void * addr, size_t len, int prot)
 }
 #endif
 
-ALWAYS_INLINE size_t byte_size(size_t num_elements, size_t element_size)
+void throw_alloc_error()
 {
-    size_t amount;
-    if (__builtin_mul_overflow(num_elements, element_size, &amount))
-        throw Exception(ErrorCodes::CANNOT_ALLOCATE_MEMORY, "Amount of memory requested to allocate is more than allowed");
-    return amount;
-}
-
-ALWAYS_INLINE size_t minimum_memory_for_elements(size_t num_elements, size_t element_size, size_t pad_left, size_t pad_right)
-{
-    size_t amount;
-    if (__builtin_add_overflow(byte_size(num_elements, element_size), pad_left + pad_right, &amount))
-        throw Exception(ErrorCodes::CANNOT_ALLOCATE_MEMORY, "Amount of memory requested to allocate is more than allowed");
-    return amount;
+    throw Exception(ErrorCodes::CANNOT_ALLOCATE_MEMORY, "Amount of memory requested to allocate is more than allowed");
 }
 
 }
