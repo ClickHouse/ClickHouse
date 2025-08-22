7953bc522c0ebb859434c174cf6f1c475a6be611
Julia Kartseva <yulia.kartseva@gmail.com>
2025-08-22 00:24:00 +0000

    allow restores of empty memory table

--- src/Common/FailPoint.cpp
+++ src/Common/FailPoint.cpp
@@ -113,7 +113,8 @@ static struct InitFiu
     REGULAR(output_format_sleep_on_progress) \
     REGULAR(slowdown_parallel_replicas_local_plan_read) \
     ONCE(iceberg_writes_cleanup) \
-    ONCE(smt_commit_exception_before_op)
+    ONCE(smt_commit_exception_before_op) \
+    ONCE(backup_add_empty_memory_table)
 
 namespace FailPoints
 {
