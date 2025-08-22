a8402389099614bc814dcda6acd27d8c1b9626d4
Robert Schulze <robert@clickhouse.com>
2025-08-22 10:46:47 +0000

    Cleanup hasTokenIndex docs

--- src/Functions/hasTokenIndex.cpp
+++ src/Functions/hasTokenIndex.cpp
@@ -8,18 +8,17 @@ REGISTER_FUNCTION(HasTokenIndex)
 {
     factory.registerFunction<FunctionHasTokenIndex>(
         FunctionDocumentation{
-            .description="Performs token search using only index. Optimized equivalent of hasToken. This function is intended to be used mainly internally.",
+            .description="Counterpart of hasToken but performs a token search in the text index. This is an internal function, do not use it from SQL.",
             .arguments = {
-                {"index_name", "The name of the text index to use. This should be the name of a text index associated with the column argument of hasToken.", {"String"}},
-                {"token", "Token to search for in the column.", {"String"}},
+                {"index_name", "The name of the text index.", {"String"}},
+                {"token", "Token to search for.", {"String"}},
                 {"_part_index", "The internal virtual column with this name"},
                 {"_part_offset", "The internal virtual column with this name"},
             },
-            .returned_value = {"Returns 0 or 1 depending for filtering purposes is the row has 'token'."},
+            .returned_value = {"Returns 0 (no match) or 1 (match)."},
             .introduced_in = {25, 8},
             .category = FunctionDocumentation::Category::StringSearch,
-        },
-        DB::FunctionFactory::Case::Insensitive);
+        });
 }
 
 }
