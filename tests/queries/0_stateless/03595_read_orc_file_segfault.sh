2b4b5022687e072e3d76f6f3f2c7ce40d6b69e59
yanglongwei <ylong2483@gmail.com>
2025-08-25 21:38:06 +0800

    use random name in 03595_read_orc_file_segfault.sh

--- tests/queries/0_stateless/03595_read_orc_file_segfault.sh
+++ tests/queries/0_stateless/03595_read_orc_file_segfault.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
-# Tags: no-fasttest, no-parallel
+# Tags: no-fasttest
 
 # Test the segment fault issue when reading ORC files
 # Fixes issue #85292
@@ -8,14 +8,15 @@ CUR_DIR=$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)
 # shellcheck source=../shell_config.sh
 . "$CUR_DIR"/../shell_config.sh
 
-[ -e "${USER_FILES_PATH}"/test.data ] && rm "${USER_FILES_PATH}"/test.data
+TEST_FILE="orc_file_${RANDOM}${RANDOM}.data"
+TABLE_NAME="orc_table_${RANDOM}${RANDOM}"
 
-${CLICKHOUSE_CLIENT} --query "DROP TABLE IF EXISTS t0"
-${CLICKHOUSE_CLIENT} --query "CREATE TABLE t0 (c0 Int) ENGINE = File(ORC, '${USER_FILES_PATH}/test.data')"
+${CLICKHOUSE_CLIENT} --query "DROP TABLE IF EXISTS ${TABLE_NAME} SYNC"
+${CLICKHOUSE_CLIENT} --query "CREATE TABLE ${TABLE_NAME} (c0 Int) ENGINE = File(ORC, '${USER_FILES_PATH}/${TEST_FILE}')"
 
-${CLICKHOUSE_CLIENT} --query "INSERT INTO TABLE t0 (c0) VALUES (1),(2),(3),(5.4)"
-${CLICKHOUSE_CLIENT} --query "SELECT * FROM file('${USER_FILES_PATH}/test.data', 'ORC') AS tx SETTINGS input_format_orc_use_fast_decoder = 0"
+${CLICKHOUSE_CLIENT} --query "INSERT INTO TABLE ${TABLE_NAME} (c0) VALUES (1),(2),(3),(5.4)"
+${CLICKHOUSE_CLIENT} --query "SELECT * FROM file('${USER_FILES_PATH}/${TEST_FILE}', 'ORC') AS tx SETTINGS input_format_orc_use_fast_decoder = 0"
 
-${CLICKHOUSE_CLIENT} --query "DROP TABLE IF EXISTS t0"
+${CLICKHOUSE_CLIENT} --query "DROP TABLE IF EXISTS ${TABLE_NAME} sync"
 
-rm -f "${USER_FILES_PATH}"/test.data
+rm -f "${USER_FILES_PATH}/${TEST_FILE}"
