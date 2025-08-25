0e4fefc822e6ecf7acabc9f06dabf10aba3f3403
Lennard Eijsackers <lennardeijsackers92@gmail.com>
2025-08-25 15:47:09 +0200

    change to contains_in_log & readd shutdown check

--- tests/integration/test_server_startup_and_shutdown_logs/test.py
+++ tests/integration/test_server_startup_and_shutdown_logs/test.py
@@ -18,10 +18,9 @@ def started_cluster():
     try:
         cluster.start()
         # Validate on startup we increase the log level.
-        instance.wait_for_log_line("Starting root logger in level trace")
+        assert instance.contains_in_log("Starting root logger in level trace")
         yield cluster
     finally:
-        # Validate on shutdown we set the log level to trace.
         cluster.shutdown()
 
 def test_server_no_logging(started_cluster):
@@ -29,6 +28,14 @@ def test_server_no_logging(started_cluster):
     # The log level is set to 'none' in the logger.xml configuration.
     assert instance.http_query("SELECT 1") == "1\n"
 
-    with pytest.raises(Exception):
-        # Attempting to wait for HTTP logs should raise an exception since logging is disabled.
-        instance.wait_for_log_line("HTTP Request for HTTPHandler-factory", timeout=5)
+    assert not instance.contains_in_log("HTTP Request for HTTPHandler-factory", timeout=5)
+
+    # shutdown Clickhouse
+    signal = 15 # SIGTERM
+    instance.exec_in_container(
+        ["bash", "-c", "pkill -{} clickhouse".format(signal)], user="root"
+    )
+
+    assert instance.contains_in_log("Set root logger in level trace before shutdown")
+
+    instance.start_clickhouse()
