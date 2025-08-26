68bd23c9f99c1214cbb735e2d8de5612075d7fcf
“JiaQi <2963103258@qq.com>
2025-08-26 10:30:19 +0800

    remove unused code

--- src/Storages/ObjectStorage/DataLakes/Paimon/PaimonClient.h
+++ src/Storages/ObjectStorage/DataLakes/Paimon/PaimonClient.h
@@ -203,7 +203,6 @@ struct PaimonManifestEntry
                       .getValueFromRowByName(row_num, concatPath({root_path, COLUMN_PAIMON_MANIFEST_FILE_FILE_NAME}), TypeIndex::String)
                       .safeGet<std::string>();
             bucket_path = Paimon::getBucketPath(partition_, bucket_, table_schema, partition_default_name);
-            // full_path = (std::filesystem::path(Paimon::getBucketPath(partition_, bucket_, table_schema, partition_default_name)) / file_name).string();
             LOG_TEST(&Poco::Logger::get("DataFileMeta"), "bucket_path: {}", bucket_path);
             file_size
                 = avro_deserializer
