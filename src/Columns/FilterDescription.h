dff0038647ccb40f3938fcd1b5de46ee2ffbb092
Nikolai Kochetov <nk@clickhouse.com>
2025-08-26 10:09:01 +0000

    Allow FilterDescription by numeric column.

--- src/Columns/FilterDescription.h
+++ src/Columns/FilterDescription.h
@@ -10,6 +10,7 @@ template <class> class ColumnVector;
 using ColumnUInt64 = ColumnVector<UInt64>;
 
 using IColumnFilter = PaddedPODArray<UInt8>;
+void convertAnyColumnToBool(const IColumn * column, IColumnFilter & res, int error_code);
 
 /// Support methods for implementation of WHERE, PREWHERE and HAVING.
 
