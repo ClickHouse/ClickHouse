96fc362a1fb9f4e717edf900a85644d24cdc1722
taiyang-li <654010905@qq.com>
2025-08-27 18:23:52 +0800

    change as requested

--- tests/queries/0_stateless/03580_improve_prewhere.sql
+++ tests/queries/0_stateless/03580_improve_prewhere.sql
@@ -43,10 +43,10 @@ WHERE date = '2025-08-05' and primary_key = '00' and normal_column != 'hello' an
 
 -- Condition: lower(primary_key) IN ('00', '01') should be placed before Condition: normal_column != 'hello' and value < 100
 -- because it has a lower estimated selectivity.
-select trimLeft(explain) from (
+select trimLeft(replaceRegexpAll(explain, '__set_String_\\d+_\\d+', '__set_String')) from (
 EXPLAIN actions=1
 SELECT * FROM test_improve_prewhere
-WHERE date = '2025-08-05' and lower(primary_key) in '00' and normal_column != 'hello' and value < 100
+WHERE date = '2025-08-05' and lower(primary_key) IN ('00', '01') and normal_column != 'hello' and value < 100
 ) where explain ilike '%Prewhere filter column%';
 -- { echoOff }
 
