15b7d501152e4922d005311b8555bba7f0df3846
robot-clickhouse <robot-clickhouse@users.noreply.github.com>
2025-08-27 02:38:20 +0000

    Backport #86012 to 25.6: Fix restoring empty `Memory` table from backup

--- src/Common/FailPoint.cpp
+++ src/Common/FailPoint.cpp
@@ -92,7 +92,8 @@ static struct InitFiu
     ONCE(parallel_replicas_wait_for_unused_replicas) \
     REGULAR(database_replicated_delay_recovery) \
     REGULAR(database_replicated_delay_entry_execution) \
-    REGULAR(remove_merge_tree_part_delay)
+    REGULAR(remove_merge_tree_part_delay) \
+    ONCE(backup_add_empty_memory_table)
 
 
 namespace FailPoints
