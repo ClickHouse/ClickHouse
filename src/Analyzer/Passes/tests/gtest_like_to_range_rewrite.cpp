60f03a4858b07f01dd0810ca1d4b13ddd3a6d00d
Guang Zhao <zheguang.zhao@gmail.com>
2025-08-27 23:46:41 +1000

    Refactor analyzer utils

--- src/Analyzer/Passes/tests/gtest_like_to_range_rewrite.cpp
+++ src/Analyzer/Passes/tests/gtest_like_to_range_rewrite.cpp
@@ -1,68 +1,29 @@
-#include <DataTypes/DataTypeString.h>
 #include <gtest/gtest.h>
-
-#include <Analyzer/Passes/LikeToRangeRewritePass.h>
 
 #include <Analyzer/ColumnNode.h>
 #include <Analyzer/FunctionNode.h>
 #include <Analyzer/IdentifierNode.h>
 #include <Analyzer/QueryTreeBuilder.h>
+#include <Analyzer/Passes/LikeToRangeRewritePass.h>
+#include <Analyzer/Passes/tests/gtest_analyzer_utils.h>
 #include <Analyzer/Utils.h>
 #include <Common/tests/gtest_global_context.h>
 #include <Common/tests/gtest_global_register.h>
+#include <DataTypes/DataTypeString.h>
 #include <DataTypes/DataTypesNumber.h>
 #include <Parsers/parseQuery.h>
 #include <Parsers/ExpressionListParsers.h>
 
 using namespace DB;
 
-static QueryTreeNodePtr resolveStringColumn(QueryTreeNodePtr node, std::map<String, QueryTreeNodePtr> & resolved_map, ContextPtr context)
-{
-    auto * function_node = node->as<FunctionNode>();
-    if (!function_node)
-    {
-        auto * identifier_node = node->as<IdentifierNode>();
-        /// it is a column
-        if (identifier_node)
-        {
-            String col_name = identifier_node->getIdentifier().getFullName();
-            auto it = resolved_map.find(col_name);
-            if (it != resolved_map.end())
-                return it->second;
-            DataTypePtr type = DataTypePtr(new DataTypeString());
-            auto column = std::make_shared<ColumnNode>(NameAndTypePair(col_name, type), node);
-            resolved_map[col_name] = column;
-            return column;
-        }
-        /// it is constant
-        return node;
-    }
-    QueryTreeNodes new_args;
-    for (const auto & argument : function_node->getArguments())
-    {
-        auto arg = resolveStringColumn(argument, resolved_map, context);
-        new_args.push_back(arg);
-    }
-    function_node->getArguments().getNodes() = std::move(new_args);
-    resolveOrdinaryFunctionNodeByName(*function_node, function_node->getFunctionName(), context);
-    return node;
-};
-
 TEST(LikeToRangeRewrite, rewrite)
 {
     tryRegisterFunctions();
-    std::map<String, QueryTreeNodePtr> resolved_map;
     auto test_f = [&](const String & cond, const String & expected)
     {
-        ContextPtr context = getContext().context;
-        ParserExpressionWithOptionalAlias exp_elem(false);
-        ASTPtr query = parseQuery(exp_elem, cond, 10000, 10000, 10000);
-        QueryTreeNodePtr node = buildQueryTree(query, context);
-
-        node = resolveStringColumn(node, resolved_map, context);
-        LikeToRangeRewritePass pass;
-        pass.run(node, context);
-        EXPECT_EQ(node->formatConvertedASTForErrorMessage(), expected);
+        testPassOnCondition(
+            QueryTreePassPtr(new LikeToRangeRewritePass()), DataTypePtr(new DataTypeString()), 
+            cond, expected);
     };
 
     /// Perfect prefix LIKE
