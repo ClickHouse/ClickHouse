8cc7f76e30ea0543f89724a51aa58a2064845b9f
Michael Stetsyuk <michael.stetsyuk@clickhouse.com>
2025-08-27 15:49:01 +0000

    Revert "experimental handling of context in StorageDictionary::read"
    This reverts commit 197e2805840444b9ff810bbfa6830ace5d5cf702.

--- src/Storages/StorageDictionary.h
+++ src/Storages/StorageDictionary.h
@@ -5,7 +5,6 @@
 #include <Storages/IStorage.h>
 #include <Interpreters/IExternalLoaderConfigRepository.h>
 #include <base/scope_guard.h>
-#include "Interpreters/Context_fwd.h"
 
 
 namespace DB
@@ -66,24 +65,14 @@ public:
     void checkTableCanBeDropped([[ maybe_unused ]] ContextPtr query_context) const override;
     void checkTableCanBeDetached() const override;
 
-    void read(
-        QueryPlan & query_plan,
-        const Names & /*column_names*/,
-        const StorageSnapshotPtr & /*storage_snapshot*/,
-        SelectQueryInfo & /*query_info*/,
-        ContextPtr /*context*/,
-        QueryProcessingStage::Enum /*processed_stage*/,
-        size_t /*max_block_size*/,
-        size_t /*num_streams*/) final;
-
     Pipe read(
         const Names & column_names,
         const StorageSnapshotPtr & storage_snapshot,
         SelectQueryInfo & query_info,
-        ContextMutablePtr context,
+        ContextPtr context,
         QueryProcessingStage::Enum processed_stage,
         size_t max_block_size,
-        size_t threads);
+        size_t threads) override;
 
     /// FIXME: processing after reading from dictionaries are not parallelized due to some bug:
     /// count() can return wrong result, see test_dictionaries_redis/test_long.py::test_redis_dict_long
