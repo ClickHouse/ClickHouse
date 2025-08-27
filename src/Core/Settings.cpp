9ba250ed68d90e5b74dbf1cae493710c93789943
Konstantin Vedernikov <vedernikovkostya@mail.ru>
2025-08-27 15:23:26 +0000

    refactoring

--- src/Core/Settings.cpp
+++ src/Core/Settings.cpp
@@ -6791,7 +6791,7 @@ When the query prioritization mechanism is employed (see setting `priority`), lo
     DECLARE(UInt64, max_iceberg_data_file_rows, 100000, R"(
 Max rows of iceberg parquet data file on insert operation.
 )", 0) \
-    DECLARE(UInt64, max_iceberg_data_file_bytes, 100000000, R"(
+    DECLARE(UInt64, max_iceberg_data_file_bytes, 1_GiB, R"(
 Max rows of iceberg parquet data file on insert operation.
 )", 0) \
     DECLARE(Float, min_os_cpu_wait_time_ratio_to_throw, 0.0, "Min ratio between OS CPU wait (OSCPUWaitMicroseconds metric) and busy (OSCPUVirtualTimeMicroseconds metric) times to consider rejecting queries. Linear interpolation between min and max ratio is used to calculate the probability, the probability is 0 at this point.", 0) \
