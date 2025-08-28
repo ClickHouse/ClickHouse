c874878e06c5ae28064ebb787ca1fe7f62b0bd3e
robot-clickhouse <robot-clickhouse@users.noreply.github.com>
2025-08-28 11:10:38 +0000

    Backport #86281 to 24.8: Fix crash during ALTER UPDATE Nullable(JSON)

--- src/Columns/ColumnNullable.h
+++ src/Columns/ColumnNullable.h
@@ -209,6 +209,7 @@ public:
 
     bool hasDynamicStructure() const override { return nested_column->hasDynamicStructure(); }
     void takeDynamicStructureFromSourceColumns(const Columns & source_columns) override;
+    bool dynamicStructureEquals(const IColumn & rhs) const override;
 
 private:
     WrappedPtr nested_column;
