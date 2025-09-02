904391c99b09b7005d18dfb8096b91e31db8555d
Konstantin Vedernikov <vedernikovkostya@mail.ru>
2025-09-02 08:32:21 +0000

    better

--- tests/integration/test_storage_iceberg/test.py
+++ tests/integration/test_storage_iceberg/test.py
@@ -2914,36 +2914,6 @@ def test_writes_schema_evolution(started_cluster, format_version, storage_type):
     df = spark.read.format("iceberg").load(f"/iceberg_data/default/{TABLE_NAME}").collect()
     assert len(df) == 2
 
-@pytest.mark.parametrize("format_version", [1, 2])
-@pytest.mark.parametrize("storage_type", ["local"])
-def test_full_drop(started_cluster, format_version, storage_type):
-    instance = started_cluster.instances["node1"]
-    spark = started_cluster.spark_session
-    TABLE_NAME = "test_full_drop_" + storage_type + "_" + get_uuid_str()
-    TABLE_NAME_2 = "test_full_drop_" + storage_type + "_" + get_uuid_str()
-
-    create_iceberg_table(storage_type, instance, TABLE_NAME, started_cluster, "(x Nullable(Int32))", format_version)
-    assert instance.query(f"SELECT * FROM {TABLE_NAME} ORDER BY ALL") == ''
-    instance.query(f"INSERT INTO {TABLE_NAME} VALUES (123);", settings={"allow_experimental_insert_into_iceberg": 1})
-    assert instance.query(f"SELECT * FROM {TABLE_NAME} ORDER BY ALL") == '123\n'
-
-    create_iceberg_table(storage_type, instance, TABLE_NAME_2, started_cluster, "(x Nullable(Int32))", format_version)
-    assert instance.query(f"SELECT * FROM {TABLE_NAME_2} ORDER BY ALL") == ''
-    instance.query(f"INSERT INTO {TABLE_NAME_2} VALUES (777);", settings={"allow_experimental_insert_into_iceberg": 1})
-    assert instance.query(f"SELECT * FROM {TABLE_NAME_2} ORDER BY ALL") == '777\n'
-
-    instance.query(f"DROP TABLE {TABLE_NAME}")
-
-    #exception means that there are no files.
-    with pytest.raises(Exception):
-        default_download_directory(
-            started_cluster,
-            storage_type,
-            f"/iceberg_data/default/{TABLE_NAME}/",
-            f"/iceberg_data/default/{TABLE_NAME}/",
-        )
-
-    assert instance.query(f"SELECT * FROM {TABLE_NAME_2} ORDER BY ALL") == '777\n'
 
 @pytest.mark.parametrize("storage_type", ["s3", "local", "azure"])
 @pytest.mark.parametrize("partition_type", ["", "identity(x)", "icebergBucket(3, x)"])
