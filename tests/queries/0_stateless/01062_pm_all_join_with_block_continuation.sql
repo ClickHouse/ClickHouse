3051a5d4b71b284d4eda27ca032811c6210b9ca4
vdimir <vdimir@clickhouse.com>
2025-09-02 07:27:03 +0000

    no-asan 01062_pm_all_join_with_block_continuation

--- tests/queries/0_stateless/01062_pm_all_join_with_block_continuation.sql
+++ tests/queries/0_stateless/01062_pm_all_join_with_block_continuation.sql
@@ -1,3 +1,6 @@
+-- Tags: no-asan
+-- test is slow to pass flaky check when changed
+
 SET max_memory_usage = 50000000;
 SET join_algorithm = 'partial_merge';
 SET analyzer_compatibility_join_using_top_level_identifier = 1;
