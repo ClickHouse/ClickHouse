3fdd6a22bc672014f86eff071a533f521b153dc8
Vasily Nemkov <vnemkov@altinity.com>
2025-09-03 20:34:36 +0200

    Added Antalya exceptions

--- tests/queries/0_stateless/03413_experimental_settings_cannot_be_enabled_by_default.sql
+++ tests/queries/0_stateless/03413_experimental_settings_cannot_be_enabled_by_default.sql
@@ -4,5 +4,11 @@
 
 -- However, some settings in the experimental tier are meant to control another experimental feature, and then they can be enabled as long as the feature itself is disabled.
 -- These are in the exceptions list inside NOT IN.
-SELECT name, value FROM system.settings WHERE tier = 'Experimental' AND type = 'Bool' AND value != '0' AND name NOT IN ('throw_on_unsupported_query_inside_transaction');
+SELECT name, value FROM system.settings WHERE tier = 'Experimental' AND type = 'Bool' AND value != '0' AND name NOT IN (
+    'throw_on_unsupported_query_inside_transaction',
+-- turned ON for Altinity Antalya builds specifically
+    'allow_experimental_database_iceberg',
+    'allow_experimental_database_unity_catalog',
+    'allow_experimental_database_glue_catalog'
+);
 SELECT name, value FROM system.merge_tree_settings WHERE tier = 'Experimental' AND type = 'Bool' AND value != '0' AND name NOT IN ('remove_rolled_back_parts_immediately');
