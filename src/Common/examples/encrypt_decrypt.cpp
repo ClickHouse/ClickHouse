4637f991e72d857acd1175f392a7accd1e501366
Raúl Marín <raul.marin@clickhouse.com>
2025-09-04 10:41:32 +0200

    Fix example build

--- src/Common/examples/encrypt_decrypt.cpp
+++ src/Common/examples/encrypt_decrypt.cpp
@@ -86,7 +86,7 @@ int main(int argc, char ** argv)
             auto zookeeper = zkutil::ZooKeeper::createWithoutKillingPreviousSessions(
                 *bootstrap_configuration, bootstrap_configuration->has("zookeeper") ? "zookeeper" : "keeper");
 
-            zkutil::ZooKeeperNodeCache zk_node_cache([&] { return zookeeper; });
+            zkutil::ZooKeeperNodeCache zk_node_cache([&](UInt64) { return zookeeper; });
             config_xml = processor.processConfig(&has_zk_includes, &zk_node_cache);
         }
         DB::ConfigurationPtr configuration(new Poco::Util::XMLConfiguration(config_xml));
