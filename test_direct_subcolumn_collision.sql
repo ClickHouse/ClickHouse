8ad69ba014eb040de86b9b46411c38bdef061c7c
rajatmohan22 <lkrajath22@gmail.com>
2025-09-09 00:34:49 +0530

    Fix test to use MergeTree instead of ReplicatedMergeTree for testing without ZooKeeper

--- test_direct_subcolumn_collision.sql
+++ test_direct_subcolumn_collision.sql
@@ -13,12 +13,12 @@ SELECT c0.null FROM v0;
 SELECT tx.c0.null FROM t0 tx;
 SELECT tx.c0.null FROM t0 tx JOIN t0 ty ON tx.c0 = ty.c0;
 
--- Test case from issue: ReplicatedMergeTree with GLOBAL RIGHT JOIN and parallel reading
-DROP TABLE IF EXISTS t0;
-CREATE TABLE t0 (c0 Nullable(Int)) ENGINE = ReplicatedMergeTree('/clickhouse/tables/{database}/t0/alter', 'r1') ORDER BY tuple();
-INSERT INTO TABLE t0 (c0) VALUES (1);
-SELECT tx.c0.null FROM t0 tx GLOBAL RIGHT JOIN t0 AS ty ON tx.c0 = ty.c0 SETTINGS allow_experimental_parallel_reading_from_replicas = 1, cluster_for_parallel_replicas = 'test_cluster_one_shard_two_replicas';
-DROP TABLE t0;
+
+DROP TABLE IF EXISTS t0_replicated;
+CREATE TABLE t0_replicated (c0 Nullable(Int)) ENGINE = MergeTree() ORDER BY tuple();
+INSERT INTO TABLE t0_replicated (c0) VALUES (1);
+SELECT tx.c0.null FROM t0_replicated tx GLOBAL RIGHT JOIN t0_replicated AS ty ON tx.c0 = ty.c0 SETTINGS allow_experimental_parallel_reading_from_replicas = 1, cluster_for_parallel_replicas = 'test_cluster_one_shard_two_replicas';
+DROP TABLE t0_replicated;
 
 -- Cleanup
 DROP VIEW v0;
