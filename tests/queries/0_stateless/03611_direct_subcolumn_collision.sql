f25fc391f0f1d586b07398f542bc1351ae5fd872
rajatmohan22 <lkrajath22@gmail.com>
2025-09-09 01:22:17 +0530

    Move subcolumn collision test to proper test directory

---
+++ tests/queries/0_stateless/03611_direct_subcolumn_collision.sql
@@ -0,0 +1,26 @@
+-- Test case for direct subcolumn collision
+-- This tries to trigger the addSubcolumns function multiple times
+
+CREATE TABLE t0 (c0 Nullable(Int)) ENGINE = MergeTree() ORDER BY tuple();
+INSERT INTO TABLE t0 (c0) VALUES (1);
+
+-- Create a view that uses subcolumns
+CREATE VIEW v0 AS SELECT c0.null FROM t0;
+
+-- Try to access the subcolumn multiple times in different ways
+SELECT c0.null FROM t0;
+SELECT c0.null FROM v0;
+SELECT tx.c0.null FROM t0 tx;
+SELECT tx.c0.null FROM t0 tx JOIN t0 ty ON tx.c0 = ty.c0;
+
+
+DROP TABLE IF EXISTS t0_replicated;
+CREATE TABLE t0_replicated (c0 Nullable(Int)) ENGINE = MergeTree() ORDER BY tuple();
+INSERT INTO TABLE t0_replicated (c0) VALUES (1);
+SELECT tx.c0.null FROM t0_replicated tx GLOBAL RIGHT JOIN t0_replicated AS ty ON tx.c0 = ty.c0 SETTINGS allow_experimental_parallel_reading_from_replicas = 1, cluster_for_parallel_replicas = 'test_cluster_one_shard_two_replicas';
+DROP TABLE t0_replicated;
+
+-- Cleanup
+DROP VIEW v0;
+DROP TABLE t0;
+
