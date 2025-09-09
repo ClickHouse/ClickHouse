c13464677e5de715eede2ae403c6e321c0d820ee
travis <travisdang@outlook.com>
2025-09-08 19:25:11 -0700

    Added stateless tests for clickhouse-disks ls cmd

---
+++ tests/queries/0_stateless/03600_disk_app_plain_rewritable_disks_list_cmd.sh
@@ -0,0 +1,26 @@
+#!/usr/bin/env bash
+# Tags: no-fasttest
+
+CUR_DIR=$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)
+# shellcheck source=../shell_config.sh
+. "$CUR_DIR"/../shell_config.sh
+
+config="$CUR_DIR/03600_disk_app_plain_rewritable_disks_list_cmd.xml"
+
+function test_copy()
+{
+    disk_name="$1"
+
+    clickhouse-disks -C "$config" --disk "$disk_name" --query "current_disk_with_path"
+    # Disk before adding directories
+    clickhouse-disks -C "$config" --disk "$disk_name" --query "ls --recursive"
+    # Disk after adding directories
+    clickhouse-disks -C "$config" --disk "$disk_name" --query "mkdir hello; cd hello; mkdir world; cd world; mkdir Clickhouse"
+    clickhouse-disks -C "$config" --disk "$disk_name" --query "ls --recursive"
+    clickhouse-disks -C "$config" --disk "$disk_name" --query "cd hello; current_disk_with_path; cd world; current_disk_with_path; ls; ls --recursive"
+
+    clickhouse-disks -C "$config" --disk "$disk_name" --query "rm -r /hello/"
+}
+
+test_copy "test_plain_rewritable_metadata_object_storage"
+
