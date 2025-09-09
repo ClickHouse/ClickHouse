224a35e6d2a50d42be810ad6c01350129933cae4
Michael Stetsyuk <michael.stetsyuk@clickhouse.com>
2025-09-09 10:16:27 +0000

    remove server settings from settings changes history

--- src/Core/SettingsChangesHistory.cpp
+++ src/Core/SettingsChangesHistory.cpp
@@ -56,8 +56,6 @@ const VersionToSettingsChangesMap & getSettingsChangesHistory()
             {"delta_lake_insert_max_rows_in_data_file", 100000, 100000, "New setting."},
             {"promql_evaluation_time", Field{"auto"}, Field{"auto"}, "The setting was renamed. The previous name is `evaluation_time`."},
             {"evaluation_time", 0, 0, "Old setting which popped up here being renamed."},
-            {"os_threads_nice_value_merge_mutate", 0, 0, "New setting."},
-            {"os_threads_nice_value_zookeeper_client_send_receive", 0, 0, "New setting."},
             {"os_threads_nice_value_query", 0, 0, "New setting."},
             {"os_threads_nice_value_materialized_view", 0, 0, "New setting."},
             {"os_thread_priority", 0, 0, "Obsolete setting."},
