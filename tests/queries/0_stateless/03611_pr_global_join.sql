d837a9bf1869b1b007db8e542758003da829e191
Igor Nikonov <igor@clickhouse.com>
2025-09-10 14:48:14 +0200

    Update the test

--- tests/queries/0_stateless/03611_pr_global_join.sql
+++ tests/queries/0_stateless/03611_pr_global_join.sql
@@ -15,10 +15,16 @@ RIGHT JOIN t1 ON true;
 
 SELECT '---';
 
+-- this query was problematic, now GLOBAL JOINs has been disabled in n-way JOINs
 SELECT *
 FROM t2
 INNER JOIN t2 AS tx ON true
 GLOBAL RIGHT JOIN t1 ON true;
+
+SELECT '---';
+-- just check that simple GLOBAL JOIN works with parallel replicas
+SELECT *
+FROM t2 GLOBAL RIGHT JOIN t1 ON true;
 
 DROP TABLE t1;
 DROP TABLE t2;
