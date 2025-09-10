afa2179bfc90b37ef739dbbfccba7b88cad2b745
divanik <dan.ivanik@clickhouse.com>
2025-09-10 16:19:24 +0200

    Remove unrelated test diff

--- tests/integration/test_storage_iceberg_no_spark/test_writes_create_table_bugs.py
+++ tests/integration/test_storage_iceberg_no_spark/test_writes_create_table_bugs.py
@@ -6,10 +6,10 @@ from helpers.iceberg_utils import (
 )
 
 
-def test_writes_create_table_bugs(started_cluster):
-    instance = started_cluster.instances["node1"]
-    TABLE_NAME = "test_relevant_iceberg_schema_chosen_" + get_uuid_str()
-    TABLE_NAME_1 = "test_relevant_iceberg_schema_chosen_" + get_uuid_str()
+def test_writes_create_table_bugs(started_cluster_iceberg_no_spark):
+    instance = started_cluster_iceberg_no_spark.instances["node1"]
+    TABLE_NAME = "test_writes_create_table_bugs_" + get_uuid_str()
+    TABLE_NAME_1 = "test_writes_create_table_bugs_" + get_uuid_str()
     instance.query(
         f"CREATE TABLE {TABLE_NAME} (c0 Int) ENGINE = IcebergLocal('/iceberg_data/default/{TABLE_NAME}/', 'CSV') AS (SELECT 1 OFFSET 1 ROW);",
         settings={"allow_experimental_insert_into_iceberg": 1}
