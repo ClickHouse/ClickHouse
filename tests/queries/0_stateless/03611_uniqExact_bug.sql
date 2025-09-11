a4b23b95ffbace4c75759d144cf1b2be62b3892d
Nikita Taranov <nikita.taranov@clickhouse.com>
2025-09-11 20:09:57 +0200

    impl

--- tests/queries/0_stateless/03611_uniqExact_bug.sql
+++ tests/queries/0_stateless/03611_uniqExact_bug.sql
@@ -1,29 +1,20 @@
 set max_threads=4;
 
 create table test(c1 Int64, c2 Int64)
-  Engine=MergeTree order by c2
-as  
+Engine=MergeTree ORDER BY c2 AS  
+WITH gen as 
+  (SELECT xxHash32(number)   % 1000000 / 1000000 AS u1,
+          xxHash32(number+12345) % 1000000 / 1000000 AS u2
+   FROM numbers(1e6))
 SELECT
     1071106,
     5+abs(1 + 151703 * (sqrt(-2 * log(u1)) * cos(2 * pi() * u2)))
-FROM
-(
-    SELECT
-        (cityHash64(number) % 1000000) / 1000000.0 AS u1,
-        (cityHash64(number+12345) % 1000000) / 1000000.0 AS u2
-    FROM numbers(1e6)
-) 
+FROM gen
 union all
 SELECT
     1071102,
     8+abs(1 + 151693 * (sqrt(-2 * log(u1)) * cos(2 * pi() * u2)))
-FROM
-(
-    SELECT
-        (cityHash64(number) % 1000000) / 1000000.0 AS u1,
-        (cityHash64(number+12345) % 1000000) / 1000000.0 AS u2
-    FROM numbers(1e6)
-);
+FROM gen;
 
 select '--- ROLLUP ---' format TSVRaw;
 SELECT
