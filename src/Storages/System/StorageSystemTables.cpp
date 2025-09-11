a83e775190c93f750e38b565bf98b8b94c3d7f4d
Roman Vasin <r.vasin@arenadata.io>
2025-09-11 11:39:51 +0000

    Fix UUIDs in StorageSystemTables

--- src/Storages/System/StorageSystemTables.cpp
+++ src/Storages/System/StorageSystemTables.cpp
@@ -3,7 +3,6 @@
 #include <Access/ContextAccess.h>
 #include <Columns/ColumnString.h>
 #include <Core/Settings.h>
-#include <Core/ServerSettings.h>
 #include <DataTypes/DataTypeArray.h>
 #include <DataTypes/DataTypeDateTime.h>
 #include <DataTypes/DataTypeNullable.h>
@@ -41,11 +40,6 @@ namespace Setting
     extern const SettingsUInt64 select_sequential_consistency;
     extern const SettingsBool show_table_uuid_in_table_create_query_if_not_nil;
     extern const SettingsBool show_data_lake_catalogs_in_system_tables;
-}
-
-namespace ServerSetting
-{
-    extern const ServerSettingsBool enable_uuids_for_columns;
 }
 
 namespace
@@ -573,8 +567,8 @@ protected:
                             ast_create->targets->resetInnerUUIDs();
                     }
 
-                    if (ast_create && context->getGlobalContext()->getServerSettings()[ServerSetting::enable_uuids_for_columns])
-                        ast_create->resetColumnUUIDs();
+                    // Always reset column UUIDs because they are not displayed in create_table_query field
+                    ast_create->resetColumnUUIDs();
 
                     if (columns_mask[src_index++])
                         res_columns[res_index++]->insert(ast ? format({context, *ast}) : "");
