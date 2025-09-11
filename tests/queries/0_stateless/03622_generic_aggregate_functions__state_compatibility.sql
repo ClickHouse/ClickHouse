be32c26ff2d9c50eaefbf801b34d4babe828ad30
avogar <avogar@clickhouse.com>
2025-09-11 12:06:02 +0000

    Add tests for LowCardinality(String)

--- tests/queries/0_stateless/03622_generic_aggregate_functions__state_compatibility.sql
+++ tests/queries/0_stateless/03622_generic_aggregate_functions__state_compatibility.sql
@@ -6,3 +6,12 @@ select hex(maxDistinctState(['str']));
 select hex(argMaxDistinctState(['str'], ['str']));
 select hex(groupUniqArrayState(['str']));
 select hex(groupArrayIntersectState(['str']));
+
+select hex(approx_top_sumState(3)([toLowCardinality('str')], 1));
+select hex(approx_top_kState(3)([toLowCardinality('str')]));
+select hex(topKState(3)([toLowCardinality('str')]));
+select hex(topKWeightedState(3)([toLowCardinality('str')], 1));
+select hex(maxDistinctState([toLowCardinality('str')]));
+select hex(argMaxDistinctState([toLowCardinality('str')], [toLowCardinality('str')]));
+select hex(groupUniqArrayState([toLowCardinality('str')]));
+select hex(groupArrayIntersectState([toLowCardinality('str')]))
