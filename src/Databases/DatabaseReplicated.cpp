a289fedbb78f5110485b441dc40ced76eebb0cc7
Dmitry Novik <n0vik@clickhouse.com>
2025-09-11 19:13:28 +0200

    Fix build

--- src/Databases/DatabaseReplicated.cpp
+++ src/Databases/DatabaseReplicated.cpp
@@ -1446,7 +1446,7 @@ void DatabaseReplicated::recoverLostReplica(const ZooKeeperPtr & current_zookeep
             LOG_DEBUG(log, "Will RENAME TABLE {} TO {}.{}", backQuoteIfNeed(broken_table_name), backQuoteIfNeed(to_database_name), backQuoteIfNeed(to_name));
             assert(db_name < to_database_name);
             DDLGuardPtr to_table_guard = DatabaseCatalog::instance().getDDLGuard(to_database_name, to_name);
-            auto to_db_ptr = DatabaseCatalog::instance().getDatabaseOrThrow(to_database_name, getContext());
+            auto to_db_ptr = DatabaseCatalog::instance().getDatabase(to_database_name);
 
             std::lock_guard lock{metadata_mutex};
             UInt64 new_digest = tables_metadata_digest;
