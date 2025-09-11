7ab8de5de87eb0128983f6d4e2ae00e041cdaffd
Yakov Olkhovskiy <yakov@clickhouse.com>
2025-09-11 16:54:52 +0000

    add test

--- tests/queries/0_stateless/03611_cte_deterministic.sql
+++ tests/queries/0_stateless/03611_cte_deterministic.sql
@@ -1,2 +1,3 @@
 with (select randConstant()) as a select a = a;
 with (select now() + sleep(1)) as a select a = a;
+with (select randConstant()) as b select b = b, a = b, `a=a` from (with (select randConstant()) as a select a, a = a as `a=a`);
