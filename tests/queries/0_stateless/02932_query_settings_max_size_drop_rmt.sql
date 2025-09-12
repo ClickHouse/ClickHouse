0bb7700abf1afdf5f3e0abddc725f4238e1cfccb
Anton Popov <anton@clickhouse.com>
2025-09-12 12:58:48 +0000

    move common code to base class

---
+++ tests/queries/0_stateless/02932_query_settings_max_size_drop_rmt.sql
@@ -0,0 +1,31 @@
+DROP TABLE IF EXISTS test_max_size_drop SYNC;
+SET insert_keeper_fault_injection_probability = 0.0;
+
+CREATE TABLE test_max_size_drop
+ENGINE = ReplicatedMergeTree('/clickhouse/tables/{database}/test_max_size_drop', '1')
+ORDER BY number
+AS SELECT number
+FROM numbers(1000);
+
+DROP TABLE test_max_size_drop SETTINGS max_table_size_to_drop = 1; -- { serverError TABLE_SIZE_EXCEEDS_MAX_DROP_SIZE_LIMIT }
+DROP TABLE test_max_size_drop SYNC;
+
+CREATE TABLE test_max_size_drop
+Engine = ReplicatedMergeTree('/clickhouse/tables/{database}/test_max_size_drop', '1')
+ORDER BY number
+AS SELECT number
+FROM numbers(1000);
+
+ALTER TABLE test_max_size_drop DROP PARTITION tuple() SETTINGS max_partition_size_to_drop = 1; -- { serverError TABLE_SIZE_EXCEEDS_MAX_DROP_SIZE_LIMIT }
+ALTER TABLE test_max_size_drop DROP PARTITION tuple();
+DROP TABLE test_max_size_drop SYNC;
+
+CREATE TABLE test_max_size_drop
+Engine = ReplicatedMergeTree('/clickhouse/tables/{database}/test_max_size_drop', '1')
+ORDER BY number
+AS SELECT number
+FROM numbers(1000);
+
+ALTER TABLE test_max_size_drop DROP PART 'all_0_0_0' SETTINGS max_partition_size_to_drop = 1; -- { serverError TABLE_SIZE_EXCEEDS_MAX_DROP_SIZE_LIMIT }
+ALTER TABLE test_max_size_drop DROP PART 'all_0_0_0';
+DROP TABLE test_max_size_drop SYNC;
