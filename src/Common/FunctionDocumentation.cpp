c031db9fb4e859297d71fde3e051670939ed5f80
Blargian <shaun.struwig@clickhouse.com>
2025-09-12 14:17:46 +0000

    Add link to Time data types

--- src/Common/FunctionDocumentation.cpp
+++ src/Common/FunctionDocumentation.cpp
@@ -1,6 +1,7 @@
 #include <Common/FunctionDocumentation.h>
 
 #include <Common/Exception.h>
+#include <boost/algorithm/string/predicate.hpp>
 #include <boost/algorithm/string/trim.hpp>
 #include <unordered_map>
 
@@ -56,6 +57,20 @@ String mapTypesToTypesWithLinks(const std::vector<std::string> & types, const Fu
             result += "`](/sql-reference/data-types/datetime)";
         else if (type.starts_with("DateTime64")) /// "DateTime64(P)", "DateTime64(3)", "DateTime64(6)", ...
             result += "`](/sql-reference/data-types/datetime64)";
+        else if (type == "Time")
+            result += "`](/sql-reference/data-types/time)";
+        else if (type.starts_with("Time64")) //// "Time64(P)", "Time64(3)", ...
+            result += "`](/sql-reference/data-types/time64)";
+        else if (type == "Time64")
+            result += "`](/sql-reference/data-types/time64)";
+        else if (type == "Time64")
+            result += "`](/sql-reference/data-types/time64)";
+        else if (type == "Time64")
+            result += "`](/sql-reference/data-types/time64)";
+        else if (type == "Time64")
+            result += "`](/sql-reference/data-types/time64)";
+        else if (type == "Time64")
+            result += "`](/sql-reference/data-types/time64)";
         else if (type == "Enum")
             result += "`](/sql-reference/data-types/enum)";
         else if (type == "UUID")
@@ -147,7 +162,17 @@ String FunctionDocumentation::argumentsAsString() const
 
 String FunctionDocumentation::syntaxAsString() const
 {
-    return boost::algorithm::trim_copy(syntax);
+    String trimmed_syntax = boost::algorithm::trim_copy(syntax);
+
+    /// It is tempting to write 'SELECT someFunction(arg1, arg2)' in the syntax field but we
+    /// really want 'someFunction(arg1, arg2)'.
+    if (boost::algorithm::istarts_with(trimmed_syntax, "SELECT "))
+        throw Exception(ErrorCodes::LOGICAL_ERROR, "Syntax field must not start with 'SELECT': {}", syntax);
+
+    if (syntax.ends_with(";"))
+        throw Exception(ErrorCodes::LOGICAL_ERROR, "Syntax field must not end with ';': {}", syntax);
+
+    return trimmed_syntax;
 }
 
 String FunctionDocumentation::returnedValueAsString() const
@@ -198,6 +223,7 @@ String FunctionDocumentation::categoryAsString() const
         {Category::Comparison, "Comparison"},
         {Category::Conditional, "Conditional"},
         {Category::DateAndTime, "Dates and Times"},
+        {Category::Decimal, "Decimal"},
         {Category::Dictionary, "Dictionary"},
         {Category::Distance, "Distance"},
         {Category::EmbeddedDictionary, "Embedded Dictionary"},
