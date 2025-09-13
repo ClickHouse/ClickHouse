0b936b2dcef22fd2bd2815da8d396f5f17808f20
Amos Bird <amosbird@gmail.com>
2025-09-13 18:51:58 +0800

    Fix some tests

--- src/Columns/ColumnObjectDeprecated.cpp
+++ src/Columns/ColumnObjectDeprecated.cpp
@@ -291,7 +291,7 @@ void ColumnObjectDeprecated::Subcolumn::insert(Field field)
 
 void ColumnObjectDeprecated::Subcolumn::addNewColumnPart(DataTypePtr type)
 {
-    auto serialization = type->getSparseSerialization();
+    auto serialization = type->getSerialization(ISerialization::Kind::SPARSE);
     data.push_back(type->createColumn(*serialization));
     least_common_type = LeastCommonType{std::move(type)};
 }
