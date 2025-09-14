a9ce6d48785d3130904fe62e30aa839e2b52ad6c
Raúl Marín <raul.marin@clickhouse.com>
2025-09-14 23:05:46 +0200

    EmbeddedRocksDB: Path must be inside user_files

--- src/Storages/RocksDB/StorageEmbeddedRocksDB.cpp
+++ src/Storages/RocksDB/StorageEmbeddedRocksDB.cpp
@@ -31,6 +31,7 @@
 #include <Common/Logger.h>
 #include <Common/logger_useful.h>
 #include <Common/Exception.h>
+#include <Common/filesystemHelpers.h>
 #include <Common/JSONBuilder.h>
 #include <Core/Settings.h>
 
@@ -210,6 +211,17 @@ StorageEmbeddedRocksDB::StorageEmbeddedRocksDB(const StorageID & table_id_,
 {
     setInMemoryMetadata(metadata_);
     setSettings(std::move(settings_));
+
+    bool is_local = context_->getApplicationType() == Context::ApplicationType::LOCAL;
+    fs::path user_files_path = is_local ? "" : fs::canonical(getContext()->getUserFilesPath());
+    if (fs::path(rocksdb_dir).is_relative())
+        rocksdb_dir = user_files_path / rocksdb_dir;
+    rocksdb_dir = fs::absolute(rocksdb_dir).lexically_normal();
+
+    if (!is_local && !pathStartsWith(fs::path(rocksdb_dir), user_files_path))
+        throw Exception(ErrorCodes::BAD_ARGUMENTS,
+                        "Path must be inside user-files path: {}", user_files_path.string());
+
     if (rocksdb_dir.empty())
     {
         rocksdb_dir = context_->getPath() + relative_data_path_;
