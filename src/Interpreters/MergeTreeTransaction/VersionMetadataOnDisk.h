7ccf361a572e170607947a8d8d535c204225da52
Tuan Pham Anh <tuan.pham.anh@clickhouse.com>
2025-09-15 11:29:31 +0000

    - Make `VersionMetadata` a base class. - Create `VersionMetadataOnDisk` subclass inheriting from `VersionMetadata` and persisting metadata on a disk along with the data part. - Move the logic of the old `VersionMetadata` to `VersionMetadataOnDisk` - Move version logic from `IMergeTreeDataPart` to `VersionMetadata` and `VersionMetadataOnDisk`

---
+++ src/Interpreters/MergeTreeTransaction/VersionMetadataOnDisk.h
@@ -0,0 +1,39 @@
+#pragma once
+#include <Interpreters/MergeTreeTransaction/VersionMetadata.h>
+#include <Storages/MergeTree/IMergeTreeDataPart.h>
+
+namespace DB
+{
+/**
+* @brief VersionMetadataOnDisk is a subclass of VersionMetadata which persiss data on a disk along with the data part.
+*/
+class VersionMetadataOnDisk : public VersionMetadata
+{
+public:
+    explicit VersionMetadataOnDisk(IMergeTreeDataPart * merge_tree_data_part_);
+
+    void loadMetadata() override;
+    void storeMetadata(bool force) override;
+    bool assertHasValidMetadata() const override;
+
+    bool tryLockRemovalTID(const TransactionID & tid, const TransactionInfoContext & context, TIDHash * locked_by_id) override;
+    void unlockRemovalTID(const TransactionID & tid, const TransactionInfoContext & context) override;
+    bool isRemovalTIDLocked() const override;
+    TIDHash getRemovalTIDLock() const override { return removal_tid_lock; }
+
+    bool hasStoredMetadata() const override;
+
+    inline static constexpr auto TXN_VERSION_METADATA_FILE_NAME = "txn_version.txt";
+
+protected:
+    void setRemovalTIDLock(TIDHash removal_tid_hash) override;
+    void appendCreationCSNToStoredMetadataImpl() override;
+    void appendRemovalCSNToStoredMetadataImpl() override;
+    void appendRemovalTIDToStoredMetadataImpl(bool clear) override;
+
+private:
+    /// Hash of removal_tid, used to lock the object for removal
+    std::atomic<TIDHash> removal_tid_lock = 0;
+};
+
+}
