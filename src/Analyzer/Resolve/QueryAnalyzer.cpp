69e8a3850d463e42e9db8bd550f9ad8a06de781f
robot-clickhouse <robot-clickhouse@users.noreply.github.com>
2025-09-16 10:14:23 +0000

    Backport #85966 to 25.7: Analyzer: check for valid correlated subqueries in the FROM clause

--- src/Analyzer/Resolve/QueryAnalyzer.cpp
+++ src/Analyzer/Resolve/QueryAnalyzer.cpp
@@ -6012,6 +6012,7 @@ void QueryAnalyzer::resolveQuery(const QueryTreeNodePtr & query_node, Identifier
 
     expandGroupByAll(query_node_typed);
 
+    validateFromClause(query_node);
     validateFilters(query_node);
     validateAggregates(query_node, {.group_by_use_nulls = scope.group_by_use_nulls});
 
