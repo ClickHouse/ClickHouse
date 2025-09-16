0df038a0868fd40dd80bb661aa6c90b6f7fa0ae2
Igor Nikonov <igor@clickhouse.com>
2025-09-16 12:36:35 +0200

    Simplify test

--- tests/queries/0_stateless/03623_pr_tpch.sql
+++ tests/queries/0_stateless/03623_pr_tpch.sql
@@ -98,14 +98,14 @@ ORDER BY (l_orderkey, l_linenumber);
 -- "Quantifying TPC-H Choke Points and Their Optimizations":
 -- ORDER BY (l_shipdate, l_orderkey, l_linenumber);
 
-INSERT INTO nation SELECT * FROM generateRandom() LIMIT 10;
-INSERT INTO region SELECT * FROM generateRandom() LIMIT 10;
-INSERT INTO part SELECT * FROM generateRandom() LIMIT 10;
-INSERT INTO supplier SELECT * FROM generateRandom() LIMIT 10;
-INSERT INTO partsupp SELECT * FROM generateRandom() LIMIT 10;
-INSERT INTO customer SELECT * FROM generateRandom() LIMIT 10;
-INSERT INTO orders SELECT * FROM generateRandom() LIMIT 100;
-INSERT INTO lineitem SELECT * FROM generateRandom() LIMIT 100;
+INSERT INTO nation SELECT * FROM generateRandom() LIMIT 1;
+INSERT INTO region SELECT * FROM generateRandom() LIMIT 1;
+INSERT INTO part SELECT * FROM generateRandom() LIMIT 1;
+INSERT INTO supplier SELECT * FROM generateRandom() LIMIT 1;
+INSERT INTO partsupp SELECT * FROM generateRandom() LIMIT 1;
+INSERT INTO customer SELECT * FROM generateRandom() LIMIT 1;
+INSERT INTO orders SELECT * FROM generateRandom() LIMIT 1;
+INSERT INTO lineitem SELECT * FROM generateRandom() LIMIT 1;
 
 SET enable_parallel_replicas = 1, max_parallel_replicas = 3, cluster_for_parallel_replicas = 'test_cluster_one_shard_three_replicas_localhost', parallel_replicas_for_non_replicated_merge_tree=1;
 
