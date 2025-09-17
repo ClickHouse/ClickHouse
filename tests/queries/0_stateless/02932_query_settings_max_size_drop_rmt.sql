c6175f6e46d0742b9f0b4f42b336964a61a6f27d
Anton Popov <anton@clickhouse.com>
2025-09-17 17:25:53 +0200

    fix test

--- tests/queries/0_stateless/02932_query_settings_max_size_drop_rmt.sql
+++ tests/queries/0_stateless/02932_query_settings_max_size_drop_rmt.sql
@@ -19,13 +19,3 @@ INSERT INTO test_max_size_drop SELECT number FROM numbers(1000);
 ALTER TABLE test_max_size_drop DROP PARTITION tuple() SETTINGS max_partition_size_to_drop = 1; -- { serverError TABLE_SIZE_EXCEEDS_MAX_DROP_SIZE_LIMIT }
 ALTER TABLE test_max_size_drop DROP PARTITION tuple();
 DROP TABLE test_max_size_drop SYNC;
-
-CREATE TABLE test_max_size_drop (number UInt64)
-Engine = ReplicatedMergeTree('/clickhouse/tables/{database}/test_max_size_drop', '1')
-ORDER BY number;
-
-INSERT INTO test_max_size_drop SELECT number FROM numbers(1000);
-
-ALTER TABLE test_max_size_drop DROP PART 'all_0_0_0' SETTINGS max_partition_size_to_drop = 1; -- { serverError TABLE_SIZE_EXCEEDS_MAX_DROP_SIZE_LIMIT }
-ALTER TABLE test_max_size_drop DROP PART 'all_0_0_0';
-DROP TABLE test_max_size_drop SYNC;
