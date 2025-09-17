f329f51fb92504de998cc75fce0c88d9b15725f3
Alexander Gololobov <davenger@clickhouse.com>
2025-09-17 17:11:52 +0200

    Update version
    Co-authored-by: Dmitry Novik <n0vik@clickhouse.com>

--- src/Functions/FunctionFilterContains.cpp
+++ src/Functions/FunctionFilterContains.cpp
@@ -107,7 +107,7 @@ REGISTER_FUNCTION(FilterContains)
     };
     FunctionDocumentation::ReturnedValue returned_value = {"True if the key was found in the filter", {"Bool"}};
     FunctionDocumentation::Examples examples = {{"Example", "This function is not supposed to be used in user queries. It might be added to query plan during optimization. ", ""}};
-    FunctionDocumentation::IntroducedIn introduced_in = {25, 8};
+    FunctionDocumentation::IntroducedIn introduced_in = {25, 9};
     FunctionDocumentation::Category category = FunctionDocumentation::Category::Other;
 
     factory.registerFunction<FunctionFilterContains>({description, syntax, arguments, returned_value, examples, introduced_in, category}, FunctionFactory::Case::Sensitive);
