62921d2d17483bad5ea8a3a308632cc92aa91c72
Igor Nikonov <igor@clickhouse.com>
2025-09-19 13:33:16 +0200

    Update test

--- tests/queries/0_stateless/03624_pr_lefl_right_joins_chain.sql
+++ tests/queries/0_stateless/03624_pr_lefl_right_joins_chain.sql
@@ -28,6 +28,21 @@ ANY RIGHT JOIN mem2 ON k2 = mem2.k
 ORDER BY tab.v
 SETTINGS enable_parallel_replicas=1, max_parallel_replicas=3, cluster_for_parallel_replicas='test_cluster_one_shard_three_replicas_localhost', parallel_replicas_for_non_replicated_merge_tree=1;
 
+
+SELECT '-- explain: check parallel replicas is disabled, looking at ReadFromRemoteParallelReplicas steps count --';
+SELECT count()
+FROM
+(
+    EXPLAIN
+    SELECT *
+    FROM tab
+    ANY LEFT JOIN mem ON k1 = mem.k
+    ANY RIGHT JOIN mem2 ON k2 = mem2.k
+    ORDER BY tab.v ASC
+    SETTINGS enable_parallel_replicas = 1, max_parallel_replicas = 3, cluster_for_parallel_replicas = 'test_cluster_one_shard_three_replicas_localhost', parallel_replicas_for_non_replicated_merge_tree = 1
+)
+WHERE explain ILIKE '%ReadFromRemoteParallelReplicas%';
+
 DROP TABLE mem2;
 DROP TABLE mem;
 DROP TABLE tab;
