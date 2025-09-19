47691940499dae3d97e7f42c4fb4e6894ad35523
Michael Stetsyuk <michael.stetsyuk@clickhouse.com>
2025-09-19 12:57:38 +0000

    create thread groups for startup script queries

--- programs/server/Server.cpp
+++ programs/server/Server.cpp
@@ -892,6 +892,9 @@ void loadStartupScripts(const Poco::Util::AbstractConfiguration & config, Contex
                 LOG_DEBUG(log, "Checking startup query condition `{}`", condition);
                 startup_context->setQueryKind(ClientInfo::QueryKind::INITIAL_QUERY);
                 startup_context->setCurrentQueryId("");
+
+                CurrentThread::QueryScope query_scope(startup_context);
+
                 executeQuery(condition_read_buffer, condition_write_buffer, true, startup_context, callback, QueryFlags{ .internal = true }, std::nullopt, {});
 
                 auto result = condition_write_buffer.str();
@@ -922,6 +925,9 @@ void loadStartupScripts(const Poco::Util::AbstractConfiguration & config, Contex
             LOG_DEBUG(log, "Executing query `{}`", query);
             startup_context->setQueryKind(ClientInfo::QueryKind::INITIAL_QUERY);
             startup_context->setCurrentQueryId("");
+
+            CurrentThread::QueryScope query_scope(startup_context);
+
             executeQuery(read_buffer, write_buffer, true, startup_context, callback, QueryFlags{ .internal = true }, std::nullopt, {});
         }
 
