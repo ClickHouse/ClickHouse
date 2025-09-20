23d1bf1f960ffb687f5560e4cea2de54d119e112
chhetripradeep <pradeepchhetri4444@gmail.com>
2025-09-20 14:08:34 +0800

    add integration test

---
+++ tests/integration/test_system_reconnect_zookeeper/test.py
@@ -0,0 +1,27 @@
+import os
+import time
+
+import pytest
+
+from helpers.cluster import ClickHouseCluster
+
+cluster = ClickHouseCluster(__file__)
+instance = cluster.add_instance("instance", with_zookeeper=True)
+
+
+@pytest.fixture(scope="module")
+def start_cluster():
+    try:
+        cluster.start()
+        yield cluster
+    finally:
+        cluster.shutdown()
+
+
+def test_system_reconnect_zookeeper(start_cluster):
+    instance.query(
+        "CREATE TABLE simple (date Date, id UInt32) ENGINE = ReplicatedMergeTree('/clickhouse/tables/0/simple', 'instance') ORDER BY tuple() PARTITION BY date;"
+    )
+
+    instance.query("SYSTEM RECONNECT ZOOKEEPER")
+    assert instance.contains_in_log("Trying to establish a new connection with ZooKeeper")
