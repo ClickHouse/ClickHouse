f13e4a5d97caeacdf15b92aa7a5804c0cb95d60f
jitendra1411 <jackrh2017@gmail.com>
2025-09-22 19:54:16 +0530

    added happy test case

--- tests/queries/0_stateless/03629_grant_unknown_table_engine.sh
+++ tests/queries/0_stateless/03629_grant_unknown_table_engine.sh
@@ -5,4 +5,5 @@ CURDIR=$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)
 # shellcheck source=../shell_config.sh
 . "$CURDIR"/../shell_config.sh
 
-${CLICKHOUSE_CLIENT} -q "GRANT TABLE ENGINE ON RandomName34343 TO user_${CLICKHOUSE_DATABASE}" 2>&1 | grep -om1 "Unknown table engine"
+${CLICKHOUSE_CLIENT} -q "GRANT TABLE ENGINE ON MergeTree TO user_${CLICKHOUSE_DATABASE}" 2>&1 | grep -om1 "Unknown table engine"
+{ ${CLICKHOUSE_CLIENT} -q "GRANT TABLE ENGINE ON RandomName34343 TO user_${CLICKHOUSE_DATABASE}" 2>&1 || true; } | grep -om1 "Unknown table engine"
