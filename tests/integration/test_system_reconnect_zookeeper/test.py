75974626cb2905157ff79238d271438530a4f87e
chhetripradeep <pradeepchhetri4444@gmail.com>
2025-09-22 23:19:23 +0800

    add jianfei's suggestion

--- tests/integration/test_system_reconnect_zookeeper/test.py
+++ tests/integration/test_system_reconnect_zookeeper/test.py
@@ -18,10 +18,21 @@ def start_cluster():
         cluster.shutdown()
 
 
+def get_zk_connected_time(instance):
+    query_zk_connected_time = (
+        "SELECT connected_time FROM system.zookeeper_connection;"
+    )
+    return instance.query(query_zk_connected_time).strip()
+
+
 def test_system_reconnect_zookeeper(start_cluster):
-    instance.query("DROP TABLE IF EXISTS simple SYNC")
+    instance.query("DROP TABLE IF EXISTS simple SYNC;")
     instance.query(
         "CREATE TABLE simple (date Date, id UInt32) ENGINE = ReplicatedMergeTree('/clickhouse/tables/0/simple', 'instance') ORDER BY tuple() PARTITION BY date;"
     )
-    instance.query("SYSTEM RECONNECT ZOOKEEPER")
-    assert instance.contains_in_log("ZooKeeper connection closed")
+
+    old_zk_connected_time = get_zk_connected_time(instance)
+    instance.query("SYSTEM RECONNECT ZOOKEEPER;")
+    new_zk_connected_time = get_zk_connected_time(instance)
+
+    assert old_zk_connected_time != new_zk_connected_time
