f58164c0f0b950e8bb0d3b4fea8d3dbb44fe3e0c
avogar <avogar@clickhouse.com>
2025-09-23 11:31:58 +0000

    Fix build

--- src/Storages/StorageSnapshot.h
+++ src/Storages/StorageSnapshot.h
@@ -48,7 +48,6 @@ struct StorageSnapshot
         const IStorage & storage_,
         StorageMetadataPtr metadata_,
         VirtualsDescriptionPtr virtual_columns_,
-        ColumnsDescription object_columns_,
         DataPtr data_);
 
     std::shared_ptr<StorageSnapshot> clone(DataPtr data_) const;
