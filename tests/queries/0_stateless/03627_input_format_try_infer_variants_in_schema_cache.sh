d6c5c73d7e3b7dea649f871a92c5fdbaa5f260fa
Pavel Kruglov <48961922+Avogar@users.noreply.github.com>
2025-09-23 17:11:11 +0200

    Make new test stable

--- tests/queries/0_stateless/03627_input_format_try_infer_variants_in_schema_cache.sh
+++ tests/queries/0_stateless/03627_input_format_try_infer_variants_in_schema_cache.sh
@@ -8,7 +8,7 @@ echo '{"x" : [1, "Hello"]}' > $CLICKHOUSE_TEST_UNIQUE_NAME.json
 $CLICKHOUSE_LOCAL -m -q "
 DESC file('$CLICKHOUSE_TEST_UNIQUE_NAME.json') SETTINGS input_format_try_infer_variants=0;
 DESC file('$CLICKHOUSE_TEST_UNIQUE_NAME.json') SETTINGS input_format_try_infer_variants=1;
-SELECT additional_format_info from system.schema_inference_cache"
+SELECT additional_format_info from system.schema_inference_cache ORDER BY ALL"
 
 rm $CLICKHOUSE_TEST_UNIQUE_NAME.json
 
