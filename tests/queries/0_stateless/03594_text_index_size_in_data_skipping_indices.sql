16055401a26887755646540b80366c057afc0eaa
George Larionov <george.larionov@clickhouse.com>
2025-09-23 17:30:58 +0000

    narrowing output of test query

--- tests/queries/0_stateless/03594_text_index_size_in_data_skipping_indices.sql
+++ tests/queries/0_stateless/03594_text_index_size_in_data_skipping_indices.sql
@@ -32,7 +32,7 @@ SELECT
     secondary_indices_compressed_bytes,
     secondary_indices_uncompressed_bytes,
     secondary_indices_marks_bytes
-FROM system.parts WHERE active = 1
+FROM system.parts WHERE active = 1 AND partition = 'tuple()'
 FORMAT Vertical;
 
 DROP TABLE tab;
