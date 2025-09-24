2faa6f76687eeaada52ebd084d563ff18da5f75b
Amos Bird <amosbird@gmail.com>
2025-09-25 04:31:27 +0800

    CI is green. Set back to default and add randomization

--- src/Storages/MergeTree/MergeTreeSettings.cpp
+++ src/Storages/MergeTree/MergeTreeSettings.cpp
@@ -247,7 +247,7 @@ namespace ErrorCodes
     This mode allows to use significantly less memory for storing discriminators
     in parts when there is mostly one variant or a lot of NULL values.
     )", 0) \
-    DECLARE(MergeTreeSerializationInfoVersion, serialization_info_version, "with_types", R"(
+    DECLARE(MergeTreeSerializationInfoVersion, serialization_info_version, "default", R"(
     Serialization info version used when writing `serialization.json`.
     This setting is required for compatibility during cluster upgrades.
 
@@ -262,7 +262,7 @@ namespace ErrorCodes
     data parts compatible with old servers. After the upgrade completes,
     switch to `WITH_TYPES` to enable per-type serialization versions.
     )", 0) \
-    DECLARE(MergeTreeStringSerializationVersion, string_serialization_version, "with_size_stream", R"(
+    DECLARE(MergeTreeStringSerializationVersion, string_serialization_version, "default", R"(
     Controls the serialization format for top-level `String` columns.
 
     This setting is only effective when `serialization_info_version` is set to "with_types".
