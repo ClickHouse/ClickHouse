7a6bc689bc24fa19e71ec4594f102d9eacc6f7ad
Bharat Nallan Chakravarthy <bharat@clickhouse.com>
2025-09-24 07:45:23 -0700

    fix test

--- tests/queries/0_stateless/03629_storage_s3_disallow_index_alter.sql
+++ tests/queries/0_stateless/03629_storage_s3_disallow_index_alter.sql
@@ -3,7 +3,7 @@
 -- Issue: https://github.com/ClickHouse/ClickHouse/issues/87059
     
 DROP TABLE IF EXISTS test_03629;
-CREATE TABLE test_03629 (a UInt64) ENGINE = S3('http://localhost:9101/root', filename='test_03629_{_partition_id}', format='Native') PARTITION BY a;
+CREATE TABLE test_03629 (a UInt64) ENGINE = S3(s3_conn, filename='test_03629_{_partition_id}', format='Native') PARTITION BY a;
 ALTER TABLE test_03629 ADD INDEX a_idx a TYPE set(0); -- { serverError NOT_IMPLEMENTED }
 
 DROP TABLE test_03629;
