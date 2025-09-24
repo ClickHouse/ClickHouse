9ee42ab6822b51cdbcdbd46ae79afed81ea4cb83
Roman Vasin <r.vasin@arenadata.io>
2025-09-24 10:48:03 +0000

    Add API call in readSecret()

--- src/Common/Vault.cpp
+++ src/Common/Vault.cpp
@@ -45,9 +45,34 @@ String Vault::readSecret(const String & secret, const String & key)
 {
     LOG_DEBUG(log, "readSecret {} {}", secret, key);
 
-    // TODO
+    DB::HTTPHeaderEntries headers;
+    headers.emplace_back("X-Vault-Token", token);
+    Poco::URI uri = Poco::URI(fmt::format("{}/v1/secret/data/{}", url, secret));
+    Poco::Net::HTTPBasicCredentials credentials{};
 
-    return std::to_string(0);
+    // TODO exception catching for 404
+    auto wb = DB::BuilderRWBufferFromHTTP(uri)
+                  .withConnectionGroup(DB::HTTPConnectionGroupType::HTTP)
+                  .withMethod(Poco::Net::HTTPRequest::HTTP_GET)
+                  // TODO add context as parameter
+                  // .withTimeouts(DB::ConnectionTimeouts::getHTTPTimeouts(context->getSettingsRef(), context->getServerSettings()))
+                  .withSkipNotFound(false)
+                  .withHeaders(headers)
+                  .create(credentials);
+
+    std::string json_str;
+    readJSONObjectPossiblyInvalid(json_str, *wb);
+    LOG_DEBUG(log, "Vault response '{}'.", json_str);
+
+    Poco::JSON::Parser parser;
+    Poco::Dynamic::Var res_json = parser.parse(json_str);
+    const Poco::JSON::Object::Ptr & root = res_json.extract<Poco::JSON::Object::Ptr>();
+    const Poco::JSON::Object::Ptr & data = root->getObject("data");
+    const Poco::JSON::Object::Ptr & kv = data->getObject("data");
+    const auto value = kv->get(key).extract<String>();
+    LOG_DEBUG(log, "Vault value '{}'.", value);
+
+    return value;
 }
 
 }
