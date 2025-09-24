2e026105db7790af733db66eb2adda967867c262
0xgouda <gouda0x@gmail.com>
2025-09-24 19:09:05 +0300

    test using both `-` and `_` (e.g `--max-threads` and `--max_threads`)

--- tests/queries/0_stateless/03623_benchmark_use_dashes_for_underscored_flags.sh
+++ tests/queries/0_stateless/03623_benchmark_use_dashes_for_underscored_flags.sh
@@ -6,6 +6,7 @@ CURDIR=$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)
 
 QUERY_ID=$RANDOM
 $CLICKHOUSE_BENCHMARK --query-id $QUERY_ID --max-concurrency 3 --max-threads 2  -i 1 --query "SELECT 1" 2>&1 | grep -F 'Queries executed' 
+$CLICKHOUSE_BENCHMARK --query_id $QUERY_ID --max_concurrency 3 --max_threads 2  -i 1 --query "SELECT 1" 2>&1 | grep -F 'Queries executed' 
 
 $CLICKHOUSE_CLIENT -q "SYSTEM FLUSH LOGS query_log"
 $CLICKHOUSE_CLIENT -q "SELECT count() FROM system.query_log WHERE current_database = currentDatabase() AND query_id='$QUERY_ID'"
