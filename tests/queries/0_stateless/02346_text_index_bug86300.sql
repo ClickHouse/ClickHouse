a5ee52f406a12dec3cce3707fefb528cae0ad4c3
Jimmy Aguilar Mena <jimmy.aguilar.mena@clickhouse.com>
2025-09-25 12:39:53 +0000

    Make hasToken behave exactly equal with and without index.

--- tests/queries/0_stateless/02346_text_index_bug86300.sql
+++ tests/queries/0_stateless/02346_text_index_bug86300.sql
@@ -3,7 +3,7 @@
 SET allow_experimental_full_text_index = 1;
 SET use_skip_indexes_on_data_read = 1;
 
-SELECT 'Match every row for empty needles';
+SELECT 'Match every row for empty needles with text Index';
 
 DROP TABLE IF EXISTS tab;
 CREATE TABLE tab (
@@ -15,6 +15,24 @@ ENGINE=MergeTree()
 ORDER BY (id);
 
 INSERT INTO tab VALUES(1, 'bar'), (2, 'foo');
+
+SELECT count() FROM tab WHERE searchAny(text, []);
+SELECT count() FROM tab WHERE searchAll(text, []);
+
+SELECT count() FROM tab WHERE searchAny(text, ['']);
+SELECT count() FROM tab WHERE searchAll(text, ['']);
+SELECT count() FROM tab WHERE hasToken(text, '');
+
+select '---';
+
+SELECT count() FROM tab WHERE NOT searchAny(text, []);
+SELECT count() FROM tab WHERE NOT searchAll(text, []);
+SELECT count() FROM tab WHERE NOT searchAny(text, ['']);
+SELECT count() FROM tab WHERE NOT searchAll(text, ['']);
+SELECT count() FROM tab WHERE NOT hasToken(text, '');
+
+SELECT 'Match every row for empty needles without text Index';
+SET use_skip_indexes_on_data_read = 0;
 
 SELECT count() FROM tab WHERE searchAny(text, []);
 SELECT count() FROM tab WHERE searchAll(text, []);
