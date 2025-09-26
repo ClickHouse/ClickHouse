e0fd63ff3f6f25d56a9610b8dd2a4d14724361ef
Smita Kulkarni <Smita.Kulkarni@clickhouse.com>
2025-09-26 21:49:41 +0200

    Fix create statement when using default minmax indicies

---
+++ tests/queries/0_stateless/03632_default_minmax_indices_alter.sql
@@ -0,0 +1,11 @@
+drop table if exists t;
+
+create table t (a UInt64, s String) engine = MergeTree order by tuple() settings add_minmax_index_for_numeric_columns = 1;
+
+show create table t;
+
+alter table t drop column s;
+
+show create table t;
+
+drop table t;
