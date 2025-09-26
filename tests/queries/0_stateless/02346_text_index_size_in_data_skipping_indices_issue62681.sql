db2fed6d059df0bf6b87884ec3ebc3a3d1a893ca
George Larionov <george.larionov@clickhouse.com>
2025-09-26 21:33:14 +0000

    updating tests to be more robust

--- tests/queries/0_stateless/02346_text_index_size_in_data_skipping_indices_issue62681.sql
+++ tests/queries/0_stateless/02346_text_index_size_in_data_skipping_indices_issue62681.sql
@@ -20,15 +20,25 @@ OPTIMIZE TABLE tab FINAL;
 -- sum up .idx files for data_compressed_bytes
 -- note that `du` rounds to nearest 4KB so it is not accurate here
 -- also note that different runs of db might all show up, only sum up one set
-SELECT * FROM system.data_skipping_indices WHERE database = currentDatabase() AND type = 'text' FORMAT Vertical;
+SELECT 
+    database,
+    table,
+    name,
+    type,
+    type_full,
+    granularity,
+    data_compressed_bytes > 100,
+    data_uncompressed_bytes > 75,
+    marks_bytes
+FROM system.data_skipping_indices WHERE database = currentDatabase() AND type = 'text' FORMAT Vertical;
 
 -- to double check: `ll -h $(find . -name "*skp_idx*")` from build dir
 -- see above notes
 SELECT
     partition,
     name,
-    secondary_indices_compressed_bytes,
-    secondary_indices_uncompressed_bytes,
+    secondary_indices_compressed_bytes > 150,
+    secondary_indices_uncompressed_bytes > 100,
     secondary_indices_marks_bytes
 FROM system.parts 
 WHERE database = currentDatabase() AND table = 'tab' AND active = 1 AND partition = 'tuple()'
