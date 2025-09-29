deebb2aa379b04fabe4cf662a496a1f8c32843c6
Raúl Marín <raul.marin@clickhouse.com>
2025-09-29 13:12:57 +0200

    Fix secondary index bulk reader

--- tests/queries/0_stateless/03633_set_index_bulk_filtering.sql
+++ tests/queries/0_stateless/03633_set_index_bulk_filtering.sql
@@ -7,5 +7,5 @@ engine=MergeTree
 order by (x, k, s)
 as select number%3, 1, 4, number%10 from numbers(1e6);
 
-select count(x) from tbulk where g = 1 and k = 1
-settings secondary_indices_enable_bulk_filtering=1;
+select count(x) from tbulk where g = 1 and k = 1 settings secondary_indices_enable_bulk_filtering=0;
+select count(x) from tbulk where g = 1 and k = 1 settings secondary_indices_enable_bulk_filtering=1;
