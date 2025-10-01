0ef5d082d23ec3a45297fe771b0efd5418feb82e
0xgouda <gouda0x@gmail.com>
2025-10-02 01:47:08 +0300

    Update test to invertly match expected errors and have empty `.reference`

--- tests/queries/0_stateless/03636_benchmark_error_messages.sh
+++ tests/queries/0_stateless/03636_benchmark_error_messages.sh
@@ -4,6 +4,7 @@ CURDIR=$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)
 # shellcheck source=../shell_config.sh
 . "$CURDIR"/../shell_config.sh
 
-$CLICKHOUSE_BENCHMARK --bad-option 2>&1
-$CLICKHOUSE_BENCHMARK --timelimit "bad value" 2>&1
-$CLICKHOUSE_BENCHMARK --user "invalid user" --password "invalid password" --query "SELECT 1" 2>&1
+# No additional stack traces or errors other than invertly matched ones should be found.
+$CLICKHOUSE_BENCHMARK --bad-option |& grep -v "DB::Exception: Unrecognized option '--bad-option'"
+$CLICKHOUSE_BENCHMARK --timelimit "bad value" |& grep -v "Bad arguments: the argument ('bad value') for option '--timelimit' is invalid"
+$CLICKHOUSE_BENCHMARK --user "invalid user" --query "SELECT 1" |& grep -v -e "DB::Exception: invalid user: Authentication failed" -e "Loaded 1 queries"
