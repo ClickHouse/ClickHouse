b29f200d558ffa3f7a224e228cf2515b5b98ff8f
Dmitry Novik <n0vik@clickhouse.com>
2025-10-01 14:33:50 +0200

    Update test

--- tests/queries/0_stateless/03591_optimize_prewhere_row_policy.sql
+++ tests/queries/0_stateless/03591_optimize_prewhere_row_policy.sql
@@ -1,6 +1,7 @@
 -- {echoOn}
 
-SET use_query_condition_cache=0;
+SET use_query_condition_cache = 0;
+SET enable_parallel_replicas = 0;
 
 DROP TABLE IF EXISTS 03591_test;
 
@@ -19,12 +20,14 @@ CREATE ROW POLICY 03591_rp ON 03591_test USING b=2 TO CURRENT_USER;
 SELECT * FROM 03591_test;
 
 -- Print plan with actions to make sure both a > 0 and b=2 are present in the prewhere section
-EXPLAIN PLAN actions=1 SELECT * FROM 03591_test WHERE a > 0 SETTINGS optimize_move_to_prewhere = 1;
+EXPLAIN PLAN actions=1 SELECT * FROM 03591_test WHERE a > 0 SETTINGS optimize_move_to_prewhere = 1, allow_experimental_analyzer = 1;
+EXPLAIN PLAN actions=1 SELECT * FROM 03591_test WHERE a > 0 SETTINGS optimize_move_to_prewhere = 1, allow_experimental_analyzer = 0;
 
 SELECT * FROM 03591_test WHERE throwIf(b=1, 'Should not throw because b=1 is not visible to this user due to the b=2 row policy') SETTINGS optimize_move_to_prewhere = 1;
 
 -- Print plan with actions to make sure a > 0, b = 2 and a = 3 are present in the prewhere section
-EXPLAIN PLAN actions=1 SELECT * FROM 03591_test WHERE a > 0 SETTINGS optimize_move_to_prewhere = 1, additional_table_filters={'03591_test': 'a=3'};
+EXPLAIN PLAN actions=1 SELECT * FROM 03591_test WHERE a > 0 SETTINGS optimize_move_to_prewhere = 1, additional_table_filters={'03591_test': 'a=3'}, allow_experimental_analyzer = 1;
+EXPLAIN PLAN actions=1 SELECT * FROM 03591_test WHERE a > 0 SETTINGS optimize_move_to_prewhere = 1, additional_table_filters={'03591_test': 'a=3'}, allow_experimental_analyzer = 0;
 
 DROP ROW POLICY 03591_rp ON 03591_test;
 
