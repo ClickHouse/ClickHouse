a9e70dabe2428e94ee2e18ef3f3b7901e754145b
Raphaël Thériault <raphael.theriault@solarwinds.com>
2025-10-02 15:13:33 -0400

    fix quantildd merge test

--- tests/queries/0_stateless/03568_ddsketch_merge.sql
+++ tests/queries/0_stateless/03568_ddsketch_merge.sql
@@ -1,5 +1,8 @@
-SELECT quantileDDMerge(0.01)(state) != 0 FROM (
-  SELECT * FROM format(RowBinary, 'state AggregateFunction(quantileDD(0.01), Float64)', base64Decode('AgAAAAAAAABAAAAAAAAAAAABDAEPAgAAAAAAAPA/AwwA/v///w8CBAAAAAAAAAAA'))
-  UNION ALL
-  SELECT * FROM format(RowBinary, 'state AggregateFunction(quantileDD(0.01), Float64)', base64Decode('As07f2aeoPY/AAAAAAAAAAABDAEjAgAAAAAAAPA/AwwA/v///w8CBAAAAAAAAAAA'))
+-- Tags: no-fasttest
+-- Tag no-fasttest: Depends on base64Decode
+
+SELECT quantileDDMerge(0.01)(state) != 0 FROM format(
+    RowBinary,
+    'state AggregateFunction(quantileDD(0.01), Float64)',
+    base64Decode('AgAAAAAAAABAAAAAAAAAAAABDAEPAgAAAAAAAPA/AwwA/v///w8CBAAAAAAAAAAAAs07f2aeoPY/AAAAAAAAAAABDAEjAgAAAAAAAPA/AwwA/v///w8CBAAAAAAAAAAA')
 )
