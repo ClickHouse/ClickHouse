c7ac6bf97cd1e5ce4d7aa2ea5e7eec0930403940
Konstantin Bogdanov <konstantin@clickhouse.com>
2025-10-02 23:16:35 +0200

    Undo vectorizing modulo, makes performance worse on aarch64

--- src/Functions/modulo.cpp
+++ src/Functions/modulo.cpp
@@ -58,7 +58,6 @@ struct ModuloByConstantImpl
         /// Modulo with too small divisor.
         if (unlikely((std::is_signed_v<B> && b == -1) || b == 1))
         {
-            _Pragma("clang loop vectorize(enable)")
             for (size_t i = 0; i < size; ++i)
                 dst[i] = 0;
             return;
@@ -68,7 +67,6 @@ struct ModuloByConstantImpl
         if (unlikely(b > std::numeric_limits<A>::max()
             || (std::is_signed_v<A> && std::is_signed_v<B> && b < std::numeric_limits<A>::lowest())))
         {
-            _Pragma("clang loop vectorize(enable)")
             for (size_t i = 0; i < size; ++i)
                 dst[i] = static_cast<ResultType>(src[i]);
             return;
@@ -91,7 +89,6 @@ struct ModuloByConstantImpl
         {
             libdivide::divider<A> divider(static_cast<A>(b));
 
-            _Pragma("clang loop vectorize(enable)")
             for (size_t i = 0; i < size; ++i)
             {
                 /// NOTE: perhaps, the division semantics with the remainder of negative numbers is not preserved.
@@ -103,7 +100,6 @@ struct ModuloByConstantImpl
             // gcc libdivide doesn't work well for pow2 division
             auto mask = b - 1;
 
-            _Pragma("clang loop vectorize(enable)")
             for (size_t i = 0; i < size; ++i)
                 dst[i] = static_cast<ResultType>(src[i] & mask);
         }
