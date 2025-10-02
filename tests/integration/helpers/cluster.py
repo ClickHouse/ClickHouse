c2f5e4a918501378f0b084f0b0f732f0ac9536b2
Tuan Pham Anh <tuan.pham.anh@clickhouse.com>
2025-10-02 07:15:25 +0000

    Clean up temp files generated in MetadataStorageFromPlainObjectStorageMoveFileOperation when loading table metadata in DatabaseOnDisk

--- tests/integration/helpers/cluster.py
+++ tests/integration/helpers/cluster.py
@@ -1913,19 +1913,16 @@ class ClickHouseCluster:
             with_remote_database_disk = False
 
         if with_remote_database_disk is None:
-            # FIXME: https://github.com/ClickHouse/ClickHouse/issues/87656
-            #
-            # if ClickHouseInstance.is_local_server_asan_build == None:
-            #     build_opts = subprocess.check_output(
-            #         f"""{self.server_bin_path} local -q "SELECT value FROM system.build_options WHERE name = 'CXX_FLAGS'" """,
-            #         stderr=subprocess.STDOUT,
-            #         shell=True,
-            #     ).decode()
-            #     ClickHouseInstance.is_local_server_asan_build = (
-            #         "-fsanitize=address" in build_opts
-            #     )
-            # with_remote_database_disk = ClickHouseInstance.is_local_server_asan_build
-            with_remote_database_disk = False
+            if ClickHouseInstance.is_local_server_asan_build == None:
+                build_opts = subprocess.check_output(
+                    f"""{self.server_bin_path} local -q "SELECT value FROM system.build_options WHERE name = 'CXX_FLAGS'" """,
+                    stderr=subprocess.STDOUT,
+                    shell=True,
+                ).decode()
+                ClickHouseInstance.is_local_server_asan_build = (
+                    "-fsanitize=address" in build_opts
+                )
+            with_remote_database_disk = ClickHouseInstance.is_local_server_asan_build
 
         if with_remote_database_disk:
             logging.debug(f"Instance {name}, with_remote_database_disk enabled")
@@ -3152,7 +3149,6 @@ class ClickHouseCluster:
 
     def wait_arrowflight_to_start(self):
         time.sleep(5) # TODO
-
 
     def start(self):
         pytest_xdist_logging_to_separate_files.setup()
