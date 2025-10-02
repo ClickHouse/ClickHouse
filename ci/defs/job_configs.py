19521ebd3017040b437cb8e1cb20865def7e885f
Pedro Ferreira <pedro.silva.ferreira@clickhouse.com>
2025-10-02 14:07:43 +0000

    Fix

--- ci/defs/job_configs.py
+++ ci/defs/job_configs.py
@@ -823,11 +823,10 @@ class JobConfigs:
         name=JobNames.BUZZHOUSE,
         runs_on=["#from param"],
         command="python3 ./ci/jobs/buzzhouse_job.py",
+        run_in_docker="clickhouse/stateless-test",
         digest_config=Job.CacheDigestConfig(
             include_paths=[
-                "./ci/docker/fuzzer",
-                "./ci/jobs/scripts/fuzzer/",
-                "./ci/jobs/scripts/functional_tests/setup_log_cluster.sh",
+                "./ci/docker/stateless-test",
                 "./ci/jobs/buzzhouse_job.py",
             ],
         ),
