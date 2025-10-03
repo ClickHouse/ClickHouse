aa5639fbc856ecd81f3e0a4265d6a56665c4f474
punithns97 <54839414+punithns97@users.noreply.github.com>
2025-10-03 23:26:01 +0000

    Added Setting to Setting History

--- src/Core/SettingsChangesHistory.cpp
+++ src/Core/SettingsChangesHistory.cpp
@@ -61,6 +61,7 @@ const VersionToSettingsChangesMap & getSettingsChangesHistory()
             {"query_plan_use_logical_join_step", true, true, "Added alias"},
             {"schema_inference_make_columns_nullable", 1, 3, "Take nullability information from Parquet/ORC/Arrow metadata by default, instead of making everything nullable."},
             {"materialized_views_squash_parallel_inserts", false, true, "Added setting to preserve old behavior if needed."},
+            {"aggregate_function_input_format", "state", "state", "New setting to control AggregateFunction input format during INSERT operations"},
         });
         addSettingsChanges(settings_changes_history, "25.9",
         {
