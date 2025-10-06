6d6b1d96f2070494ab9a26f0647953a2d33f3a7a
Mikhail Artemenko <mikhail.artemenko@clickhouse.com>
2025-10-06 11:55:34 +0000

    fix style

--- src/Disks/ObjectStorages/InMemoryDirectoryPathMap.h
+++ src/Disks/ObjectStorages/InMemoryDirectoryPathMap.h
@@ -20,7 +20,7 @@ public:
     InMemoryDirectoryPathMap(CurrentMetrics::Metric metric_directories_name, CurrentMetrics::Metric metric_files_name)
         : metric_directories(metric_directories_name, 0), metric_files(metric_files_name, 0)
     {
-        addOrReplacePath("",  RemotePathInfo{});
+        addOrReplacePath("", RemotePathInfo{});
     }
 
     /// Breadth-first order.
