00ad42265594d3f1183cbcc2f0d6d3d48f546244
Robert Schulze <robert@clickhouse.com>
2025-10-06 09:11:05 +0000

    Revert "Update tests"
    This reverts commit 40c15b91f10ea12d35a72802e661dd09af486e6a.

--- tests/queries/0_stateless/inject_random_order_for_select_without_order_by.sql
+++ tests/queries/0_stateless/inject_random_order_for_select_without_order_by.sql
@@ -3,9 +3,6 @@
 -- The setting is disabled by default, enable it for the test.
 SET inject_random_order_for_select_without_order_by = 1;
 
--- Works only with enabled analyzer
-SET enable_analyzer = 1;
-
 -- If enabled, `ORDER BY rand()` is injected into the query plan.
 -- We can not test the query result directly (because of randomization), we test the presence
 -- or absence of sorting operators in the query plan.
