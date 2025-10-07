39085eae382d9cab8f5e4dbdd39b8c6ef5fd536e
George Larionov <george.larionov@clickhouse.com>
2025-10-07 20:49:24 +0000

    pr comments

--- tests/queries/0_stateless/02346_search_on_partially_indexed_col.sql
+++ tests/queries/0_stateless/02346_search_on_partially_indexed_col.sql
@@ -3,12 +3,18 @@ SET parallel_replicas_local_plan = 1; -- this setting may skip index analysis wh
 SET use_skip_indexes_on_data_read = 0;
 SET mutations_sync = 2; -- want synchronous materialize
 
+-- In this test we make sure that text search functions hasAny/AllTokens work correctly for
+-- tables in which only some parts have a materialized index. The expected behavior is that 
+-- the search acts the same as if the whole column was indexed, but of course an inefficient
+-- brute force search is used for the un-indexed rows. Furthermore we test that the same
+-- tokenizer used to create the index is used for the un-indexed parts as is specified in
+-- the index.
+
 DROP TABLE IF EXISTS tab;
 CREATE TABLE tab
 (
     id UInt32,
     message String,
-    INDEX idx(`message`) TYPE text(tokenizer = 'splitByString') GRANULARITY 1,
 )
 ENGINE = MergeTree
 ORDER BY (id) SETTINGS index_granularity = 2;
@@ -20,55 +26,10 @@ CREATE VIEW explain_indexes
         SELECT groupArray(id) FROM tab WHERE hasAnyTokens(message, ['def'])
     ));
 
--- bar# tests that same tokenizer is used for brute force search
-INSERT INTO tab(id, message)
-VALUES
-    (1, 'abc def foo'),
-    (2, 'abc def bar#'),
-    (3, 'abc baz foo'),
-    (4, 'abc baz bar'),
-    (5, 'abc zzz foo'),
-    (6, 'abc zzz bar');
-
--- { echoOn }
-SELECT * FROM explain_indexes;
---
---Establishing baseline, compare future tests to these:
---hasAnyTokens:
-SELECT arraySort(groupArray(id)) FROM tab WHERE hasAnyTokens(message, ['abc']);
-SELECT arraySort(groupArray(id)) FROM tab WHERE hasAnyTokens(message, ['ab']);
-SELECT arraySort(groupArray(id)) FROM tab WHERE hasAnyTokens(message, ['foo']);
-SELECT arraySort(groupArray(id)) FROM tab WHERE hasAnyTokens(message, ['bar']);
-SELECT arraySort(groupArray(id)) FROM tab WHERE hasAnyTokens(message, ['abc', 'foo']);
-SELECT arraySort(groupArray(id)) FROM tab WHERE hasAnyTokens(message, ['abc', 'bar']);
-SELECT arraySort(groupArray(id)) FROM tab WHERE hasAnyTokens(message, ['foo', 'bar']);
-SELECT arraySort(groupArray(id)) FROM tab WHERE hasAnyTokens(message, ['foo', 'ba']);
-SELECT arraySort(groupArray(id)) FROM tab WHERE hasAnyTokens(message, ['fo', 'ba']);
---
---hasAllTokens:
-SELECT arraySort(groupArray(id)) FROM tab WHERE hasAllTokens(message, ['abc']);
-SELECT arraySort(groupArray(id)) FROM tab WHERE hasAllTokens(message, ['ab']);
-SELECT arraySort(groupArray(id)) FROM tab WHERE hasAllTokens(message, ['foo']);
-SELECT arraySort(groupArray(id)) FROM tab WHERE hasAllTokens(message, ['bar']);
-SELECT arraySort(groupArray(id)) FROM tab WHERE hasAllTokens(message, ['abc', 'foo']);
-SELECT arraySort(groupArray(id)) FROM tab WHERE hasAllTokens(message, ['abc', 'bar']);
-SELECT arraySort(groupArray(id)) FROM tab WHERE hasAllTokens(message, ['foo', 'bar']);
-SELECT arraySort(groupArray(id)) FROM tab WHERE hasAllTokens(message, ['abc', 'fo']);
--- { echoOff }
-
-DROP TABLE tab;
-
-CREATE TABLE tab
-(
-    id UInt32,
-    message String,
-)
-ENGINE = MergeTree
-ORDER BY (id) SETTINGS index_granularity = 2;
-
 SYSTEM STOP MERGES tab;
 
--- bar# tests that same tokenizer is used for brute force search
+-- bar# will be parsed differently by different tokenizers, e.g. splitbyString -> 'bar#'; splitByNonAlpha -> 'bar'
+-- we want to test that the search functions will use the same tokenizer on un-materialized column parts
 INSERT INTO tab(id, message)
 VALUES
     (1, 'abc def foo'),
@@ -87,26 +48,17 @@ VALUES
 SELECT * FROM explain_indexes;
 
 --
---Should match baseline:
 --hasAnyTokens:
-SELECT arraySort(groupArray(id)) FROM tab WHERE hasAnyTokens(message, ['abc']);
-SELECT arraySort(groupArray(id)) FROM tab WHERE hasAnyTokens(message, ['ab']);
 SELECT arraySort(groupArray(id)) FROM tab WHERE hasAnyTokens(message, ['foo']);
 SELECT arraySort(groupArray(id)) FROM tab WHERE hasAnyTokens(message, ['bar']);
-SELECT arraySort(groupArray(id)) FROM tab WHERE hasAnyTokens(message, ['abc', 'foo']);
-SELECT arraySort(groupArray(id)) FROM tab WHERE hasAnyTokens(message, ['abc', 'bar']);
 SELECT arraySort(groupArray(id)) FROM tab WHERE hasAnyTokens(message, ['foo', 'bar']);
 SELECT arraySort(groupArray(id)) FROM tab WHERE hasAnyTokens(message, ['foo', 'ba']);
 SELECT arraySort(groupArray(id)) FROM tab WHERE hasAnyTokens(message, ['fo', 'ba']);
 
 --
 --hasAllTokens:
-SELECT arraySort(groupArray(id)) FROM tab WHERE hasAllTokens(message, ['abc']);
-SELECT arraySort(groupArray(id)) FROM tab WHERE hasAllTokens(message, ['ab']);
 SELECT arraySort(groupArray(id)) FROM tab WHERE hasAllTokens(message, ['foo']);
 SELECT arraySort(groupArray(id)) FROM tab WHERE hasAllTokens(message, ['bar']);
-SELECT arraySort(groupArray(id)) FROM tab WHERE hasAllTokens(message, ['abc', 'foo']);
-SELECT arraySort(groupArray(id)) FROM tab WHERE hasAllTokens(message, ['abc', 'bar']);
 SELECT arraySort(groupArray(id)) FROM tab WHERE hasAllTokens(message, ['foo', 'bar']);
 SELECT arraySort(groupArray(id)) FROM tab WHERE hasAllTokens(message, ['abc', 'fo']);
 -- { echoOff }
@@ -141,10 +93,13 @@ SELECT * FROM explain_indexes;
 --
 -- Test that splitByNonAlpha tokenizer is applied even to column part which is not materialized
 SELECT arraySort(groupArray(id)) FROM tab WHERE hasAnyTokens(message, ['bar']);
+SELECT arraySort(groupArray(id)) FROM tab WHERE hasAnyTokens(message, ['bar$']); -- test default tokenizer
+SELECT arraySort(groupArray(id)) FROM tab WHERE hasAnyTokens(message, tokens('bar$', 'splitByNonAlpha'));
 SELECT arraySort(groupArray(id)) FROM tab WHERE hasAllTokens(message, ['bar']);
+SELECT arraySort(groupArray(id)) FROM tab WHERE hasAllTokens(message, ['bar$']); -- test default tokenizer
+SELECT arraySort(groupArray(id)) FROM tab WHERE hasAllTokens(message, tokens('bar$', 'splitByNonAlpha'));
 -- { echoOff }
 
 DROP TABLE tab;
 DROP VIEW explain_indexes;
-SYSTEM START MERGES tab;
 
