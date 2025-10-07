5b65c7ab866c71a738ec2846493b6dfe9c92224a
George Larionov <george.larionov@clickhouse.com>
2025-10-07 17:57:43 +0000

    updating test to be sensitive to different tokenizer being used for brute force search

--- tests/queries/0_stateless/02346_search_on_partially_indexed_col.sql
+++ tests/queries/0_stateless/02346_search_on_partially_indexed_col.sql
@@ -1,28 +1,40 @@
 SET allow_experimental_full_text_index = 1;
 SET parallel_replicas_local_plan = 1; -- this setting may skip index analysis when false
 SET use_skip_indexes_on_data_read = 0;
+SET mutations_sync = 2; -- want synchronous materialize
 
 DROP TABLE IF EXISTS tab;
 CREATE TABLE tab
 (
     id UInt32,
     message String,
-    INDEX idx(`message`) TYPE text(tokenizer = 'splitByString'),
+    INDEX idx(`message`) TYPE text(tokenizer = 'splitByString') GRANULARITY 1,
 )
 ENGINE = MergeTree
-ORDER BY (id);
+ORDER BY (id) SETTINGS index_granularity = 2;
 
+DROP VIEW IF EXISTS explain_indexes;
+CREATE VIEW explain_indexes
+  AS  SELECT *
+    FROM viewExplain('EXPLAIN', 'indexes = 1', (
+        SELECT groupArray(id) FROM tab WHERE hasAnyTokens(message, ['def'])
+    ));
+
+-- bar# tests that same tokenizer is used for brute force search
 INSERT INTO tab(id, message)
 VALUES
     (1, 'abc def foo'),
-    (2, 'abc def bar'),
+    (2, 'abc def bar#'),
     (3, 'abc baz foo'),
     (4, 'abc baz bar'),
     (5, 'abc zzz foo'),
     (6, 'abc zzz bar');
 
-SELECT 'Establishing baseline, compare future tests to these:';
-SELECT 'hasAny:';
+-- { echoOn }
+SELECT * FROM explain_indexes;
+--
+--Establishing baseline, compare future tests to these:
+--hasAnyTokens:
 SELECT arraySort(groupArray(id)) FROM tab WHERE hasAnyTokens(message, ['abc']);
 SELECT arraySort(groupArray(id)) FROM tab WHERE hasAnyTokens(message, ['ab']);
 SELECT arraySort(groupArray(id)) FROM tab WHERE hasAnyTokens(message, ['foo']);
@@ -31,10 +43,9 @@ SELECT arraySort(groupArray(id)) FROM tab WHERE hasAnyTokens(message, ['abc', 'f
 SELECT arraySort(groupArray(id)) FROM tab WHERE hasAnyTokens(message, ['abc', 'bar']);
 SELECT arraySort(groupArray(id)) FROM tab WHERE hasAnyTokens(message, ['foo', 'bar']);
 SELECT arraySort(groupArray(id)) FROM tab WHERE hasAnyTokens(message, ['foo', 'ba']);
-SELECT arraySort(groupArray(id)) FROM tab WHERE hasAnyToken(message, ['fo', 'ba']); -- test alias
-
-SELECT '';
-SELECT 'hasAny:';
+SELECT arraySort(groupArray(id)) FROM tab WHERE hasAnyTokens(message, ['fo', 'ba']);
+--
+--hasAllTokens:
 SELECT arraySort(groupArray(id)) FROM tab WHERE hasAllTokens(message, ['abc']);
 SELECT arraySort(groupArray(id)) FROM tab WHERE hasAllTokens(message, ['ab']);
 SELECT arraySort(groupArray(id)) FROM tab WHERE hasAllTokens(message, ['foo']);
@@ -42,7 +53,8 @@ SELECT arraySort(groupArray(id)) FROM tab WHERE hasAllTokens(message, ['bar']);
 SELECT arraySort(groupArray(id)) FROM tab WHERE hasAllTokens(message, ['abc', 'foo']);
 SELECT arraySort(groupArray(id)) FROM tab WHERE hasAllTokens(message, ['abc', 'bar']);
 SELECT arraySort(groupArray(id)) FROM tab WHERE hasAllTokens(message, ['foo', 'bar']);
-SELECT arraySort(groupArray(id)) FROM tab WHERE hasAllToken(message, ['abc', 'fo']); -- test alias
+SELECT arraySort(groupArray(id)) FROM tab WHERE hasAllTokens(message, ['abc', 'fo']);
+-- { echoOff }
 
 DROP TABLE tab;
 
@@ -52,15 +64,18 @@ CREATE TABLE tab
     message String,
 )
 ENGINE = MergeTree
-ORDER BY (id);
+ORDER BY (id) SETTINGS index_granularity = 2;
 
+SYSTEM STOP MERGES;
+
+-- bar# tests that same tokenizer is used for brute force search
 INSERT INTO tab(id, message)
 VALUES
     (1, 'abc def foo'),
-    (2, 'abc def bar'),
+    (2, 'abc def bar#'),
     (3, 'abc baz foo');
 
-ALTER TABLE tab ADD INDEX  idx(`message`) TYPE text(tokenizer = 'splitByString');
+ALTER TABLE tab ADD INDEX  idx(`message`) TYPE text(tokenizer = 'splitByString') GRANULARITY 1;
 
 INSERT INTO tab(id, message)
 VALUES
@@ -68,9 +83,12 @@ VALUES
     (5, 'abc zzz foo'),
     (6, 'abc zzz bar');
 
-SELECT '';
-SELECT 'Should match baseline:';
-SELECT 'hasAny:';
+-- { echoOn }
+SELECT * FROM explain_indexes;
+
+--
+--Should match baseline:
+--hasAnyTokens:
 SELECT arraySort(groupArray(id)) FROM tab WHERE hasAnyTokens(message, ['abc']);
 SELECT arraySort(groupArray(id)) FROM tab WHERE hasAnyTokens(message, ['ab']);
 SELECT arraySort(groupArray(id)) FROM tab WHERE hasAnyTokens(message, ['foo']);
@@ -79,10 +97,10 @@ SELECT arraySort(groupArray(id)) FROM tab WHERE hasAnyTokens(message, ['abc', 'f
 SELECT arraySort(groupArray(id)) FROM tab WHERE hasAnyTokens(message, ['abc', 'bar']);
 SELECT arraySort(groupArray(id)) FROM tab WHERE hasAnyTokens(message, ['foo', 'bar']);
 SELECT arraySort(groupArray(id)) FROM tab WHERE hasAnyTokens(message, ['foo', 'ba']);
-SELECT arraySort(groupArray(id)) FROM tab WHERE hasAnyToken(message, ['fo', 'ba']); -- test alias
+SELECT arraySort(groupArray(id)) FROM tab WHERE hasAnyTokens(message, ['fo', 'ba']);
 
-SELECT '';
-SELECT 'hasAny:';
+--
+--hasAllTokens:
 SELECT arraySort(groupArray(id)) FROM tab WHERE hasAllTokens(message, ['abc']);
 SELECT arraySort(groupArray(id)) FROM tab WHERE hasAllTokens(message, ['ab']);
 SELECT arraySort(groupArray(id)) FROM tab WHERE hasAllTokens(message, ['foo']);
@@ -90,6 +108,43 @@ SELECT arraySort(groupArray(id)) FROM tab WHERE hasAllTokens(message, ['bar']);
 SELECT arraySort(groupArray(id)) FROM tab WHERE hasAllTokens(message, ['abc', 'foo']);
 SELECT arraySort(groupArray(id)) FROM tab WHERE hasAllTokens(message, ['abc', 'bar']);
 SELECT arraySort(groupArray(id)) FROM tab WHERE hasAllTokens(message, ['foo', 'bar']);
-SELECT arraySort(groupArray(id)) FROM tab WHERE hasAllToken(message, ['abc', 'fo']); -- test alias
+SELECT arraySort(groupArray(id)) FROM tab WHERE hasAllTokens(message, ['abc', 'fo']);
+-- { echoOff }
 
 DROP TABLE tab;
+
+CREATE TABLE tab
+(
+    id UInt32,
+    message String,
+)
+ENGINE = MergeTree
+ORDER BY (id) SETTINGS index_granularity = 2;
+
+INSERT INTO tab(id, message)
+VALUES
+    (1, 'abc def foo'),
+    (2, 'abc def bar#'),
+    (3, 'abc baz foo');
+
+ALTER TABLE tab ADD INDEX  idx(`message`) TYPE text(tokenizer = 'splitByNonAlpha') GRANULARITY 1;
+
+INSERT INTO tab(id, message)
+VALUES
+    (4, 'abc baz bar'),
+    (5, 'abc zzz foo'),
+    (6, 'abc zzz bar');
+
+-- { echoOn }
+SELECT * FROM explain_indexes;
+
+--
+-- Test that splitByNonAlpha tokenizer is applied even to column part which is not materialized
+SELECT arraySort(groupArray(id)) FROM tab WHERE hasAnyTokens(message, ['bar']);
+SELECT arraySort(groupArray(id)) FROM tab WHERE hasAllTokens(message, ['bar']);
+-- { echoOff }
+
+DROP TABLE tab;
+DROP VIEW explain_indexes;
+SYSTEM START MERGES;
+
