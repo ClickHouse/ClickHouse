560f14e5152f004b854129d78af4398a70fb98d5
Yarik Briukhovetskyi <114298166+yariks5s@users.noreply.github.com>
2025-10-08 17:48:00 +0000

    Fix order in 20731 + style

--- src/Interpreters/ConcurrentHashJoin.cpp
+++ src/Interpreters/ConcurrentHashJoin.cpp
@@ -515,7 +515,7 @@ IBlocksStreamPtr ConcurrentHashJoin::getNonJoinedBlocks(
         {
             std::lock_guard lock(hash_join->mutex);
 
-            if (hash_join->data->hasNonJoinedRows() || 
+            if (hash_join->data->hasNonJoinedRows() ||
                 hash_join->has_non_joined_rows.load(std::memory_order_relaxed))
             {
                 if (auto s = hash_join->data->getNonJoinedBlocks(
