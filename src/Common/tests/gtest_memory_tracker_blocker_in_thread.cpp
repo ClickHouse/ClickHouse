23f36dcfe08dde0e52e90aaf7c25096c391e8c72
Azat Khuzhin <a3at.mail@gmail.com>
2025-10-08 19:08:17 +0200

    Add unit tests for MemoryTrackerBlockerInThread

---
+++ src/Common/tests/gtest_memory_tracker_blocker_in_thread.cpp
@@ -0,0 +1,56 @@
+#include <gtest/gtest.h>
+
+#include <Common/MemoryTracker.h>
+#include <Common/CurrentThread.h>
+#include <Common/MemoryTrackerBlockerInThread.h>
+#include <Common/VariableContext.h>
+
+using namespace DB;
+
+TEST(MemoryTrackerBlockerInThread, Default)
+{
+    ASSERT_FALSE(MemoryTrackerBlockerInThread::isBlocked(VariableContext::Process));
+    MemoryTrackerBlockerInThread blocker;
+    ASSERT_TRUE(MemoryTrackerBlockerInThread::isBlocked(VariableContext::User));
+    ASSERT_FALSE(MemoryTrackerBlockerInThread::isBlocked(VariableContext::Global));
+    ASSERT_EQ(MemoryTrackerBlockerInThread::getLevel(), VariableContext::User);
+}
+
+TEST(MemoryTrackerBlockerInThread, Global)
+{
+    ASSERT_FALSE(MemoryTrackerBlockerInThread::isBlocked(VariableContext::Process));
+    MemoryTrackerBlockerInThread blocker(VariableContext::Global);
+    ASSERT_TRUE(MemoryTrackerBlockerInThread::isBlocked(VariableContext::Global));
+    ASSERT_EQ(MemoryTrackerBlockerInThread::getLevel(), VariableContext::Global);
+}
+
+TEST(MemoryTrackerBlockerInThread, Nested)
+{
+    {
+        MemoryTrackerBlockerInThread blocker_global(VariableContext::Global);
+        {
+            MemoryTrackerBlockerInThread blocker_user(VariableContext::User);
+            ASSERT_TRUE(MemoryTrackerBlockerInThread::isBlocked(VariableContext::User));
+            ASSERT_FALSE(MemoryTrackerBlockerInThread::isBlocked(VariableContext::Global));
+            ASSERT_EQ(MemoryTrackerBlockerInThread::getLevel(), VariableContext::User);
+        }
+        ASSERT_TRUE(MemoryTrackerBlockerInThread::isBlocked(VariableContext::Global));
+        ASSERT_EQ(MemoryTrackerBlockerInThread::getLevel(), VariableContext::Global);
+    }
+    ASSERT_FALSE(MemoryTrackerBlockerInThread::isBlocked(VariableContext::Process));
+    ASSERT_EQ(MemoryTrackerBlockerInThread::getLevel(), VariableContext::Max);
+}
+
+TEST(MemoryTrackerBlockerInThread, Move)
+{
+    {
+        MemoryTrackerBlockerInThread blocker_global(VariableContext::Global);
+        MemoryTrackerBlockerInThread blocker_user(VariableContext::User);
+
+        blocker_user = std::move(blocker_global);
+        ASSERT_TRUE(MemoryTrackerBlockerInThread::isBlocked(VariableContext::Global));
+        ASSERT_EQ(MemoryTrackerBlockerInThread::getLevel(), VariableContext::Global);
+    }
+    ASSERT_FALSE(MemoryTrackerBlockerInThread::isBlocked(VariableContext::Process));
+    ASSERT_EQ(MemoryTrackerBlockerInThread::getLevel(), VariableContext::Max);
+}
