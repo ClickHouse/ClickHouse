28361d7a8d2f22145633e238496e9db3d3111762
robot-clickhouse <robot-clickhouse@users.noreply.github.com>
2025-10-09 11:10:58 +0000

    Backport #88213 to 25.7: Fix propagation of `is_shared` flag in `ColumnLowCardinality`

--- src/Functions/IFunction.cpp
+++ src/Functions/IFunction.cpp
@@ -407,9 +407,9 @@ ColumnPtr IExecutableFunction::executeWithoutSparseColumns(
             ColumnUniquePtr res_dictionary = std::move(res_mut_dictionary);
 
             if (indexes && !res_is_constant)
-                result = ColumnLowCardinality::create(res_dictionary, res_indexes->index(*indexes, 0));
+                result = ColumnLowCardinality::create(res_dictionary, res_indexes->index(*indexes, 0), /*is_shared=*/false);
             else
-                result = ColumnLowCardinality::create(res_dictionary, res_indexes);
+                result = ColumnLowCardinality::create(res_dictionary, res_indexes, /*is_shared=*/false);
 
             if (res_is_constant)
                 result = ColumnConst::create(std::move(result), input_rows_count);
