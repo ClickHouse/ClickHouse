a423e8e3e3fa1419f20982ed701759680d74c864
Christian Endres <cendres@cloudflare.com>
2025-10-10 08:21:44 +0000

    fix test flakiness

--- tests/queries/0_stateless/03403_distributed_merge_two_level_aggregation.sql
+++ tests/queries/0_stateless/03403_distributed_merge_two_level_aggregation.sql
@@ -6,7 +6,7 @@ SYSTEM STOP MERGES test_table_1;
 
 DROP TABLE IF EXISTS dist_test_table_1;
 CREATE TABLE dist_test_table_1(number UInt64) ENGINE = Distributed('test_cluster_thirty_shards_localhost', currentDatabase(), test_table_1, rand());
-INSERT INTO dist_test_table_1 SELECT number from numbers_mt(100000);
+INSERT INTO dist_test_table_1 SELECT number from numbers_mt(10000) SETTINGS distributed_foreground_insert = 1;
 
 DROP TABLE IF EXISTS test_table_2;
 CREATE TABLE test_table_2(number UInt64) ENGINE = MergeTree ORDER BY number;
@@ -14,7 +14,7 @@ SYSTEM STOP MERGES test_table_2;
 
 DROP TABLE IF EXISTS dist_test_table_2;
 CREATE TABLE dist_test_table_2(number UInt64) ENGINE = Distributed('test_cluster_thirty_shards_localhost', currentDatabase(), test_table_2, rand());
-INSERT INTO dist_test_table_2 SELECT number from numbers_mt(100000);
+INSERT INTO dist_test_table_2 SELECT number from numbers_mt(10000) SETTINGS distributed_foreground_insert = 1;
 
 DROP TABLE IF EXISTS merge_test_table;
 CREATE TABLE merge_test_table ENGINE = Merge(currentDatabase(), '^dist_test_table_(1|2)$');
