1c6d2d7fe19c48575981c22e84dda072fdce8e2e
robot-clickhouse <robot-clickhouse@users.noreply.github.com>
2025-10-10 16:13:57 +0000

    Backport #88330 to 25.9: Add missing checks for canContainMergeTreeTables() into system tables

--- src/Databases/DataLake/DatabaseDataLake.cpp
+++ src/Databases/DataLake/DatabaseDataLake.cpp
@@ -508,7 +508,6 @@ DatabaseTablesIteratorPtr DatabaseDataLake::getTablesIterator(
                     }
                     catch (...)
                     {
-                        tryLogCurrentException(log, fmt::format("Ignoring table {}", table_name));
                         promise->set_exception(std::current_exception());
                     }
                 });
@@ -595,7 +594,7 @@ DatabaseTablesIteratorPtr DatabaseDataLake::getLightweightTablesIterator(
                     }
                     catch (...)
                     {
-                        tryLogCurrentException(log, fmt::format("ignoring table {}", table_name));
+                        tryLogCurrentException(log, fmt::format("Ignoring table {}", table_name));
                     }
                     promise->set_value(storage);
                 });
