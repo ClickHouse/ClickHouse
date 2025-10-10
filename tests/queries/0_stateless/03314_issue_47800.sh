8b062e8230e2fa9a955b7149dd5792bd67242fce
rienath <raufs.dunamalijevs@clickhouse.com>
2025-10-10 15:51:26 +0000

    Fix tests

--- tests/queries/0_stateless/03314_issue_47800.sh
+++ tests/queries/0_stateless/03314_issue_47800.sh
@@ -1,41 +1,16 @@
-#!/usr/bin/expect -f
-set basedir [file dirname $argv0]
-set basename [file tail $argv0]
-if {[info exists env(CLICKHOUSE_TMP)]} {
-    set CLICKHOUSE_TMP $env(CLICKHOUSE_TMP)
-} else {
-    set CLICKHOUSE_TMP "."
-}
-exp_internal -f $CLICKHOUSE_TMP/$basename.debuglog 0
+#!/usr/bin/env bash
 
-set timeout 30
-log_user 0
-match_max 100000
-set stty_init "rows 25 cols 120"
+CUR_DIR=$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)
+# shellcheck source=../shell_config.sh
+. "$CUR_DIR"/../shell_config.sh
 
-expect_after {
-    -i $any_spawn_id eof { exp_continue }
-    -i $any_spawn_id timeout { exit 1 }
-}
+# Track degradations for issue https://github.com/ClickHouse/ClickHouse/issues/47800
 
+$CLICKHOUSE_CLIENT --query "CREATE DATABASE IF NOT EXISTS test_03314"
+$CLICKHOUSE_CLIENT --query "CREATE TABLE test_03314.simple_table (id UInt64, name String) ENGINE = Memory"
 
-spawn bash
-send "source $basedir/../shell_config.sh\r"
+$CLICKHOUSE_CLIENT --processed-rows --query "INSERT INTO test_03314.simple_table SELECT 1 AS id, 'Alice' AS name"
+$CLICKHOUSE_CLIENT --processed-rows --query "INSERT INTO test_03314.simple_table SELECT number + 1 AS id, ['Alice','Bob','Charlie'][number + 1] AS name FROM numbers(3)"
 
-send "\$CLICKHOUSE_CLIENT --query 'CREATE DATABASE IF NOT EXISTS test;' >/dev/null 2>&1\r"
-expect "\r"
-
-send "\$CLICKHOUSE_CLIENT --query 'CREATE TABLE IF NOT EXISTS test.simple_table (id UInt64, name String) ENGINE = Memory;' >/dev/null 2>&1\r"
-expect "\r"
-
-send "\$CLICKHOUSE_CLIENT --processed-rows --query \"INSERT INTO test.simple_table SELECT 1 AS id, 'Alice' AS name;\" \r"
-expect "Processed rows: 1"
-
-send "\$CLICKHOUSE_CLIENT --processed-rows --query \"INSERT INTO test.simple_table SELECT number + 1 AS id, \['Alice','Bob','Charlie'\]\[number + 1\] AS name FROM numbers(3);\" \r"
-expect "Processed rows: 3"
-
-send "echo OK\r"
-expect "OK"
-
-send "exit\r"
-expect eof
+$CLICKHOUSE_CLIENT --query "DROP TABLE test_03314.simple_table"
+$CLICKHOUSE_CLIENT --query "DROP DATABASE test_03314"
