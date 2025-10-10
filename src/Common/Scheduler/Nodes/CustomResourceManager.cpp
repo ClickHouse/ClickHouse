1b4360215ac2404a48636b9fc8d6b8323f604c65
Neerav <neeravsalaria@gmail.com>
2025-10-10 10:23:26 +0000

    Using UNUSED macro instead of void for resource_name

--- src/Common/Scheduler/Nodes/CustomResourceManager.cpp
+++ src/Common/Scheduler/Nodes/CustomResourceManager.cpp
@@ -179,7 +179,7 @@ ResourceLink CustomResourceManager::Classifier::get(const String & resource_name
 
 WorkloadSettings CustomResourceManager::Classifier::getWorkloadSettings(const String & resource_name) const
 {
-    (void)resource_name;
+    UNUSED(resource_name);
     return {};
 }
 
