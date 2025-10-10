9b307871d4a8d497c8a90a296860977d6839d629
János Benjamin Antal <benjamin.antal@clickhouse.com>
2025-10-10 08:21:03 +0000

    Add input headers to JSON format of `EXPLAIN` result

--- tests/queries/0_stateless/03652_explain_input_header.sql
+++ tests/queries/0_stateless/03652_explain_input_header.sql
@@ -19,4 +19,19 @@ EXPLAIN PLAN header = 1, input_headers = 1
     SELECT * FROM t1 INNER JOIN t2 USING x WHERE t1.x > 2 OR t2.y = 'a' LIMIT 10
 ) UNION ALL (
     SELECT * FROM t2 LEFT JOIN t1 USING x WHERE t2.x < 2 OR t1.y = 'c' LIMIT 10
-)
+);
+
+-- Same as above, but in JSON format
+-- All possible combinations of header and input_header options
+EXPLAIN PLAN json = 1, header = 0, input_headers = 0 SELECT * FROM t1 WHERE x > 2 OR y = 'a' LIMIT 10;
+EXPLAIN PLAN json = 1, header = 0, input_headers = 1 SELECT * FROM t1 WHERE x > 2 OR y = 'a' LIMIT 10;
+EXPLAIN PLAN json = 1, header = 1, input_headers = 0 SELECT * FROM t1 WHERE x > 2 OR y = 'a' LIMIT 10;
+EXPLAIN PLAN json = 1, header = 1, input_headers = 1 SELECT * FROM t1 WHERE x > 2 OR y = 'a' LIMIT 10;
+
+-- One complex query with multiple input headers
+EXPLAIN PLAN json = 1, header = 1, input_headers = 1
+(
+    SELECT * FROM t1 INNER JOIN t2 USING x WHERE t1.x > 2 OR t2.y = 'a' LIMIT 10
+) UNION ALL (
+    SELECT * FROM t2 LEFT JOIN t1 USING x WHERE t2.x < 2 OR t1.y = 'c' LIMIT 10
+);
