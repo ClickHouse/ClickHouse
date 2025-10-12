8145f0b1a23da6185f7cef0e040b2e7f0410b65c
Alexey Milovidov <milovidov@clickhouse.com>
2025-10-12 13:32:44 +0200

    Update 03657_hash_vs_full_sorting_merge_join.sql

--- tests/queries/0_stateless/03657_hash_vs_full_sorting_merge_join.sql
+++ tests/queries/0_stateless/03657_hash_vs_full_sorting_merge_join.sql
@@ -13,7 +13,7 @@ INSERT INTO tn1 VALUES (1, 'val1'), (NULL, 'val21'), (NULL, 'val22'), (NULL, 'va
 INSERT INTO t2 VALUES (1, 'val11'), (1, 'val12'), (2, 'val22'), (2, 'val23'), (2, 'val24'), (2, 'val25'), (2, 'val26'), (2, 'val27'), (2, 'val28'), (3, 'val3');
 INSERT INTO tn2 VALUES (1, 'val11'), (1, 'val12'), (NULL, 'val22'), (NULL, 'val23'), (NULL, 'val24'), (NULL, 'val25'), (NULL, 'val26'), (NULL, 'val27'), (NULL, 'val28'), (3, 'val3');
 
-
+SET enable_analyzer = 1;
 SET join_algorithm = 'hash';
 
 SELECT '---';
