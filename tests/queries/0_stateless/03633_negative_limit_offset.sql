574a2e35050dd236d5f14dd1319db2a29b041305
nihalzp <81457724+nihalzp@users.noreply.github.com>
2025-10-13 03:09:50 +0800

    Improve negative limit offset test

--- tests/queries/0_stateless/03633_negative_limit_offset.sql
+++ tests/queries/0_stateless/03633_negative_limit_offset.sql
@@ -23,7 +23,7 @@ SELECT number FROM numbers(10) ORDER BY number LIMIT -1 OFFSET -5;
 SELECT number FROM numbers(10) ORDER BY number LIMIT -3 OFFSET -9;
 SELECT number FROM numbers(10) ORDER BY number LIMIT -2 OFFSET -15;
 SELECT number FROM numbers(1000) ORDER BY number LIMIT -5 OFFSET -4;
-SELECT number FROM numbers(100000) LIMIT -5 OFFSET -1000;
+SELECT number FROM numbers(100000) ORDER BY number LIMIT -5 OFFSET -1000;
 
 SELECT 'Negative Limit and Positive Offset';
 SELECT number FROM numbers(10) ORDER BY number LIMIT -1 OFFSET 5;
@@ -37,15 +37,38 @@ SELECT number FROM numbers(10) ORDER BY number LIMIT 3 OFFSET -8;
 SELECT number FROM numbers(10) ORDER BY number LIMIT 8 OFFSET -3;
 
 SELECT 'Misc';
-SELECT DISTINCT number % 8 FROM numbers(120) ORDER BY number LIMIT -3 OFFSET -2;
-SELECT DISTINCT number % 80 FROM numbers(120) ORDER BY number LIMIT -3 OFFSET 50;
+SELECT DISTINCT number % 8 AS x FROM numbers(120) ORDER BY x LIMIT -3 OFFSET -2;
+SELECT DISTINCT number % 80 AS x FROM numbers(120) ORDER BY x LIMIT -3 OFFSET 50;
 SELECT * FROM system.numbers_mt WHERE number = 1000000 OFFSET -1;
 SELECT * FROM system.numbers_mt WHERE number = 1000000 LIMIT -1;
 SELECT * FROM system.numbers_mt WHERE number = 1000000 LIMIT -1 OFFSET -1;
 SELECT number FROM numbers(1000000) ORDER BY number LIMIT -1;
-SELECT DISTINCT number FROM numbers(1000000) LIMIT -1 OFFSET -999999;
+SELECT DISTINCT number FROM numbers(1000000) ORDER BY number LIMIT -1 OFFSET -999999;
 SELECT number FROM numbers(1000000) ORDER BY number LIMIT -1 OFFSET -999999;
-SELECT DISTINCT number FROM numbers(20) LIMIT 18446744073709551615 OFFSET 18446744073709551615;
+SELECT DISTINCT number FROM numbers(20) LIMIT 18446744073709551615 OFFSET -446744073709551615;
+
+SELECT 'Double Column';
+DROP TABLE IF EXISTS num_tab;
+CREATE TABLE num_tab
+(
+    `id` UInt8,
+    `val` UInt32
+)
+ENGINE = MergeTree
+ORDER BY (id, val)
+AS SELECT
+    number % 2 AS id,
+    number AS val
+FROM numbers(20);
+
+SELECT if((count() = 5) AND (min(val) = 15) AND (max(val) = 19) AND (sum(val) = 85) AND (uniqExact(id) = 2), 'OK', 'FAIL')
+FROM
+(
+    SELECT
+        id,
+        val
+    FROM num_tab ORDER BY val ASC LIMIT -5
+);
 
 SELECT 'Big Tables';
 DROP TABLE IF EXISTS num_tab;
@@ -101,7 +124,7 @@ SELECT number FROM numbers(10) ORDER BY number LIMIT -1 OFFSET -5;
 SELECT number FROM numbers(10) ORDER BY number LIMIT -3 OFFSET -9;
 SELECT number FROM numbers(10) ORDER BY number LIMIT -2 OFFSET -15;
 SELECT number FROM numbers(1000) ORDER BY number LIMIT -5 OFFSET -4;
-SELECT number FROM numbers(100000) LIMIT -5 OFFSET -1000;
+SELECT number FROM numbers(100000) ORDER BY number LIMIT -5 OFFSET -1000;
 
 SELECT 'Negative Limit and Positive Offset';
 SELECT number FROM numbers(10) ORDER BY number LIMIT -1 OFFSET 5;
@@ -115,15 +138,38 @@ SELECT number FROM numbers(10) ORDER BY number LIMIT 3 OFFSET -8;
 SELECT number FROM numbers(10) ORDER BY number LIMIT 8 OFFSET -3;
 
 SELECT 'Misc';
-SELECT DISTINCT number % 8 FROM numbers(120) ORDER BY number LIMIT -3 OFFSET -2;
-SELECT DISTINCT number % 80 FROM numbers(120) ORDER BY number LIMIT -3 OFFSET 50;
+SELECT DISTINCT number % 8 AS x FROM numbers(120) ORDER BY x LIMIT -3 OFFSET -2;
+SELECT DISTINCT number % 80 AS x FROM numbers(120) ORDER BY x LIMIT -3 OFFSET 50;
 SELECT * FROM system.numbers_mt WHERE number = 1000000 OFFSET -1;
 SELECT * FROM system.numbers_mt WHERE number = 1000000 LIMIT -1;
 SELECT * FROM system.numbers_mt WHERE number = 1000000 LIMIT -1 OFFSET -1;
 SELECT number FROM numbers(1000000) ORDER BY number LIMIT -1;
-SELECT DISTINCT number FROM numbers(1000000) LIMIT -1 OFFSET -999999;
+SELECT DISTINCT number FROM numbers(1000000) ORDER BY number LIMIT -1 OFFSET -999999;
 SELECT number FROM numbers(1000000) ORDER BY number LIMIT -1 OFFSET -999999;
-SELECT DISTINCT number FROM numbers(20) LIMIT 18446744073709551615 OFFSET 18446744073709551615;
+SELECT DISTINCT number FROM numbers(20) LIMIT 18446744073709551615 OFFSET -446744073709551615;
+
+SELECT 'Double Column';
+DROP TABLE IF EXISTS num_tab;
+CREATE TABLE num_tab
+(
+    `id` UInt8,
+    `val` UInt32
+)
+ENGINE = MergeTree
+ORDER BY (id, val)
+AS SELECT
+    number % 2 AS id,
+    number AS val
+FROM numbers(20);
+
+SELECT if((count() = 5) AND (min(val) = 15) AND (max(val) = 19) AND (sum(val) = 85) AND (uniqExact(id) = 2), 'OK', 'FAIL')
+FROM
+(
+    SELECT
+        id,
+        val
+    FROM num_tab ORDER BY val ASC LIMIT -5
+);
 
 SELECT 'Big Tables';
 DROP TABLE IF EXISTS num_tab;
