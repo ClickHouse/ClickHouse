b3a832efdc288da2ab032159aaa128978f1263c3
robot-clickhouse <robot-clickhouse@users.noreply.github.com>
2025-10-15 09:14:33 +0000

    Backport #87903 to 25.9: Add more information for Iceberg SELECT queries profiling

--- src/Storages/ObjectStorage/DataLakes/Iceberg/IcebergMetadata.cpp
+++ src/Storages/ObjectStorage/DataLakes/Iceberg/IcebergMetadata.cpp
@@ -69,7 +69,9 @@
 
 namespace ProfileEvents
 {
-    extern const Event IcebergTrivialCountOptimizationApplied;
+extern const Event IcebergIteratorInitializationMicroseconds;
+extern const Event IcebergMetadataUpdateMicroseconds;
+extern const Event IcebergTrivialCountOptimizationApplied;
 }
 
 namespace DB
@@ -251,6 +253,7 @@ bool IcebergMetadata::update(const ContextPtr & local_context)
         DB::IcebergMetadataLogLevel::Metadata,
         configuration_ptr->getRawPath().path,
         metadata_file_path,
+        std::nullopt,
         std::nullopt);
 
     if (previous_snapshot_id != relevant_snapshot_id)
@@ -588,7 +591,9 @@ DataLakeMetadataPtr IcebergMetadata::create(
         DB::IcebergMetadataLogLevel::Metadata,
         configuration_ptr->getRawPath().path,
         metadata_file_path,
-        std::nullopt);
+        std::nullopt,
+        std::nullopt
+    );
     return std::make_unique<IcebergMetadata>(object_storage, configuration_ptr, local_context, metadata_version, format_version, object, cache_ptr, compression_method);
 }
 
@@ -782,6 +787,8 @@ ObjectIterator IcebergMetadata::iterate(
      ContextPtr local_context) const
 {
     SharedLockGuard lock(mutex);
+
+    ProfileEventTimeIncrement<Microseconds> watch(ProfileEvents::IcebergIteratorInitializationMicroseconds);
 
     auto table_snapshot
         = std::make_shared<IcebergTableStateSnapshot>(last_metadata_version, relevant_snapshot_schema_id, relevant_snapshot_id);
