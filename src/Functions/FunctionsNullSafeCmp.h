c3ae0db31128c2d547deec00a6a87b7f380848b4
yanglongwei <ylong2483@gmail.com>
2025-10-16 15:46:52 +0000

    fix: Style check

--- src/Functions/FunctionsNullSafeCmp.h
+++ src/Functions/FunctionsNullSafeCmp.h
@@ -13,6 +13,7 @@ namespace ErrorCodes
 {
     extern const int NUMBER_OF_ARGUMENTS_DOESNT_MATCH;
     extern const int BAD_ARGUMENTS;
+    extern const int ILLEGAL_TYPE_OF_ARGUMENT;
 }
 
 enum class NullSafeCmpMode : uint8_t
