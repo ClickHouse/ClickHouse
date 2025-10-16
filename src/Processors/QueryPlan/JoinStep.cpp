dcdbb4bad2147095b40945574b9c685c42dfd856
vdimir <vdimir@clickhouse.com>
2025-10-16 12:24:50 +0000

    revert remove sqashing in min_joined_block_size_rows=0

--- src/Processors/QueryPlan/JoinStep.cpp
+++ src/Processors/QueryPlan/JoinStep.cpp
@@ -168,7 +168,7 @@ QueryPipelineBuilderPtr JoinStep::updatePipeline(QueryPipelineBuilders pipelines
         });
     }
 
-    if (join->supportParallelJoin() && (min_block_size_rows > 1 || min_block_size_bytes > 1))
+    if (join->supportParallelJoin())
     {
         joined_pipeline->addSimpleTransform(
             [&](const SharedHeader & header)
