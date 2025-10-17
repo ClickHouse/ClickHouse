15fa2a69abf7188508a1987c9768c0af9bb8634a
Christoph Wurm <christoph@clickhouse.com>
2025-10-17 20:18:01 +0100

    Try fix CI

--- tests/queries/0_stateless/03652_memory_usage_headers.sh
+++ tests/queries/0_stateless/03652_memory_usage_headers.sh
@@ -27,10 +27,10 @@ grep -q '"memory_usage":"[1-9][0-9]*"' && echo "Ok"
 # Check that we have memory_usage in summary for 241 OOM errors
 CURL_OUTPUT="$(
     ${CLICKHOUSE_CURL} -s -S -v -N -G "${CLICKHOUSE_URL}" \
-    --data-urlencode "query=SELECT number, avg(number) FROM numbers(1e5) GROUP BY number FORMAT Null SETTINGS max_threads = 1" \
+    --data-urlencode "query=SELECT number, avg(number) FROM numbers(1e12) GROUP BY number FORMAT Null SETTINGS max_threads = 1" \
     --data-urlencode "cancel_http_readonly_queries_on_client_close=1" \
     --data-urlencode "send_progress_in_http_headers=1" \
-    --data-urlencode "max_memory_usage=100" 2>&1
+    --data-urlencode "max_memory_usage=100000000" 2>&1
 )"
 
 echo "$CURL_OUTPUT" | grep 'X-ClickHouse-Summary:' | grep -q '"memory_usage":"[1-9][0-9]*"' && echo "Ok"
