00a121d55310c74c795bfa51da597674183f3a3e
0xgouda <gouda0x@gmail.com>
2025-10-18 14:49:41 +0300

    Add test for distributed fractional limit and offset queries

---
+++ tests/queries/0_stateless/03681_distributed_fractional_limit_offset.sql
@@ -0,0 +1,27 @@
+SET enable_analyzer=0;
+
+SELECT 'Old Analyzer:';
+
+SELECT number from remote('127.0.0.1', numbers_mt(100)) LIMIT 0.01;
+
+SELECT number from remote('127.0.0.1', numbers_mt(100)) OFFSET 0.99;
+
+SELECT number from remote('127.0.0.1', numbers_mt(100)) LIMIT 0.01 OFFSET 0.9;
+
+SELECT number from remote('127.0.0.1', numbers_mt(100)) LIMIT 1 OFFSET 0.9;
+
+SELECT number from remote('127.0.0.1', numbers_mt(100)) LIMIT 0.01 OFFSET 90;
+
+SET enable_analyzer=1;
+
+SELECT 'New Analyzer:';
+
+SELECT number from remote('127.0.0.1', numbers_mt(100)) ORDER BY number LIMIT 0.01;
+
+SELECT number from remote('127.0.0.1', numbers_mt(100)) OFFSET 0.99;
+
+SELECT number from remote('127.0.0.1', numbers_mt(100)) LIMIT 0.01 OFFSET 0.9;
+
+SELECT number from remote('127.0.0.1', numbers_mt(100)) LIMIT 1 OFFSET 0.9;
+
+SELECT number from remote('127.0.0.1', numbers_mt(100)) LIMIT 0.01 OFFSET 90;
