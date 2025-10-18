9d148c8795cf1169224cd1ae2d55e65a4a5cd9fd
Alexey Milovidov <milovidov@clickhouse.com>
2025-10-19 00:45:23 +0200

    Update 3457_query_cache_log_comment.sql

--- tests/queries/0_stateless/3457_query_cache_log_comment.sql
+++ tests/queries/0_stateless/3457_query_cache_log_comment.sql
@@ -10,6 +10,6 @@ SELECT c FROM tab SETTINGS use_query_cache = 1, log_comment='';
 SELECT c FROM tab SETTINGS use_query_cache = 1, log_comment='aaa';
 SELECT c FROM tab SETTINGS use_query_cache = 1, log_comment='bbb';
 
-system flush logs;
+system flush logs query_log;
 
 select log_comment, ProfileEvents['QueryCacheHits'], ProfileEvents['QueryCacheMisses'] from system.query_log where type = 'QueryFinish' and event_time > now() - 600 and current_database = currentDatabase() AND query like 'SELECT c FROM tab SETTINGS use_query_cache%' order by event_time_microseconds;
