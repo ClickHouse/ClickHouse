225f2e448b7467c42dfedc841d154eb17f1d8a11
Alexey Milovidov <milovidov@clickhouse.com>
2025-10-18 22:43:32 +0200

    Update 03314_empty_tuple_in_protobuf_format.sql

--- tests/queries/0_stateless/03314_empty_tuple_in_protobuf_format.sql
+++ tests/queries/0_stateless/03314_empty_tuple_in_protobuf_format.sql
@@ -3,7 +3,6 @@
 
 DROP TABLE IF EXISTS t0;
 
-SET engine_file_truncate_on_insert = 1;
 CREATE TABLE t0 (c0 Tuple()) ENGINE = Memory();
 INSERT INTO TABLE t0 (c0) VALUES (()), (());
 INSERT INTO TABLE FUNCTION file(currentDatabase() || '.protobuf', 'Protobuf', 'c0 Tuple()') SELECT c0 FROM t0; -- { serverError NO_COLUMNS_SERIALIZED_TO_PROTOBUF_FIELDS }
