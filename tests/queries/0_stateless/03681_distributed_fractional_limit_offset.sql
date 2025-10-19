10a5268472e46789e5a24a6202b51e63f3c8ccd3
0xgouda <gouda0x@gmail.com>
2025-10-19 19:33:16 +0300

    a small update, to get tests re-runned

--- tests/queries/0_stateless/03681_distributed_fractional_limit_offset.sql
+++ tests/queries/0_stateless/03681_distributed_fractional_limit_offset.sql
@@ -1,6 +1,6 @@
 SET enable_analyzer=0;
 
-SELECT 'Old Analyzer:';
+SELECT 'Old Analyzer';
 
 SELECT number from remote('127.0.0.1', numbers_mt(100)) LIMIT 0.01;
 
@@ -14,7 +14,7 @@ SELECT number from remote('127.0.0.1', numbers_mt(100)) LIMIT 0.01 OFFSET 90;
 
 SET enable_analyzer=1;
 
-SELECT 'New Analyzer:';
+SELECT 'New Analyzer';
 
 SELECT number from remote('127.0.0.1', numbers_mt(100)) ORDER BY number LIMIT 0.01;
 
