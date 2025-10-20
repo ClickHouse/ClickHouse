9b790f3e98cb874edb61ab361fed27409c527bb2
zwy991114 <643044969@qq.com>
2025-10-20 20:28:48 +0800

    add test_kill_not_throw_if_query_exist

--- tests/integration/test_kill_throw_if_noop/test.py
+++ tests/integration/test_kill_throw_if_noop/test.py
@@ -43,3 +43,18 @@ def test_kill_not_throw_if_noop(start_cluster):
     node.query(
         "KILL mutation where is_done = 0 SETTINGS kill_throw_if_noop = 0;"
     )
+
+def test_kill_not_throw_if_query_exist(start_cluster):
+    node.exec_in_container(
+        ["bash", "-c", 'clickhouse client --query_id "test_kill_not_throw_if_query_exist" -q "SELECT number, sleepEachRow(1) FROM system.numbers LIMIT 100 settings function_sleep_max_microseconds_per_block=300000000" &'],
+        privileged=True,
+        user="root",
+    )
+
+    assert node.query("SELECT count(*) FROM system.processes where query_id='test_kill_not_throw_if_query_exist'") == "1\n"
+
+    assert "" in node.query(
+        "KILL query where query_id = 'test_kill_not_throw_if_query_exist' SYNC"
+    )
+
+    assert node.query("SELECT count(*) FROM system.processes where query_id='test_kill_not_throw_if_query_exist'") == "0\n"
