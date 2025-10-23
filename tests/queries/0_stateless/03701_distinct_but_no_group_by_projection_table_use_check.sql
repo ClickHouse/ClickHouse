d8ddcae1b77bb237588f9d9064ec07d1e1b64cdb
nihalzp <81457724+nihalzp@users.noreply.github.com>
2025-10-23 09:18:23 +0800

    Skip parallel replicas for test

--- tests/queries/0_stateless/03701_distinct_but_no_group_by_projection_table_use_check.sql
+++ tests/queries/0_stateless/03701_distinct_but_no_group_by_projection_table_use_check.sql
@@ -1,5 +1,6 @@
--- Tags: no-replicated-database
--- no-replicated-database: explain output differs for replicated database.
+-- Tags: no-replicated-database, no-parallel-replicas
+-- no-replicated-database: EXPLAIN output differs for replicated database.
+-- no-parallel-replicas: EXPLAIN output differs for parallel replicas.
 
 SELECT 'Simple:';
 
