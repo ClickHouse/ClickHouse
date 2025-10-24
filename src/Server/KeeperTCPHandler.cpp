29f69fb0cf54f217e6f9a0cad185546e8d105277
Michael Stetsyuk <michael.stetsyuk@clickhouse.com>
2025-10-24 11:03:01 +0000

    better enqueue_ts

--- src/Server/KeeperTCPHandler.cpp
+++ src/Server/KeeperTCPHandler.cpp
@@ -452,7 +452,6 @@ void KeeperTCPHandler::runImpl()
     auto response_callback = [my_responses = this->responses, my_poll_wrapper = this->poll_wrapper](
                                  const Coordination::ZooKeeperResponsePtr & response, Coordination::ZooKeeperRequestPtr request)
     {
-        response->enqueue_ts = std::chrono::steady_clock::now();
         if (!my_responses->push(RequestWithResponse{response, std::move(request)}))
             throw Exception(ErrorCodes::SYSTEM_ERROR, "Could not push response with xid {} and zxid {}", response->xid, response->zxid);
 
