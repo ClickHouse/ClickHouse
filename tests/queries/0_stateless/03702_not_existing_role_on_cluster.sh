c987f56c7ad304815767655792f7692c50e2832c
pufit <pufit@clickhouse.com>
2025-10-27 16:14:01 +0100

    Fix queries with external role on cluster when it was dropped

---
+++ tests/queries/0_stateless/03702_not_existing_role_on_cluster.sh
@@ -0,0 +1,37 @@
+#!/usr/bin/env bash
+# Tags: no-replicated-database
+
+CUR_DIR=$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)
+# shellcheck source=../shell_config.sh
+. "$CUR_DIR"/../shell_config.sh
+
+user="user03702_${CLICKHOUSE_DATABASE}_$RANDOM"
+role="role03702_${CLICKHOUSE_DATABASE}_$RANDOM"
+db=${CLICKHOUSE_DATABASE}
+
+${CLICKHOUSE_CLIENT} <<EOF
+DROP USER IF EXISTS $user ON CLUSTER test_cluster_two_shards;
+CREATE USER $user ON CLUSTER test_cluster_two_shards;
+
+DROP ROLE IF EXISTS $role ON CLUSTER test_cluster_two_shards;
+CREATE ROLE $role ON CLUSTER test_cluster_two_shards;
+
+GRANT REMOTE ON *.* TO $user ON CLUSTER test_cluster_two_shards;
+GRANT SELECT ON *.* TO $role ON CLUSTER test_cluster_two_shards;
+
+GRANT $role TO $user ON CLUSTER test_cluster_two_shards;
+DROP ROLE $role ON CLUSTER test_cluster_two_shards;
+EOF
+
+${CLICKHOUSE_CLIENT} --user $user <<EOF
+SELECT
+    hostName() AS h,
+    count()
+FROM clusterAllReplicas('test_cluster_two_shards', system.one)
+GROUP BY h
+FORMAT Null
+EOF;
+
+${CLICKHOUSE_CLIENT} <<EOF
+DROP USER IF EXISTS $user ON CLUSTER test_cluster_two_shards;
+EOF
