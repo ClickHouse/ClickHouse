a625f6fbdd80efece93c5344d094346c4ecf9221
fastio <pengjian.uestc@gmail.com>
2025-10-28 22:00:48 +0800

    fix test case

--- tests/queries/0_stateless/02346_text_index_direct_read_crash.sql
+++ tests/queries/0_stateless/02346_text_index_direct_read_crash.sql
@@ -1,6 +1,9 @@
-set allow_experimental_full_text_index = 1;
+-- Test that the text index works correctly when the number of rows in a part is smaller than the index_granularity.
+
+SET allow_experimental_full_text_index = 1;
 
 DROP TABLE IF EXISTS tab;
+
 CREATE TABLE tab
 (
     `id`   UInt64,
@@ -10,8 +13,13 @@ CREATE TABLE tab
 ENGINE = MergeTree
 ORDER BY id
 SETTINGS index_granularity = 32;
+
 INSERT INTO tab VALUES (0,'a'),(1,'b'),(2,'c'),(3,'d');
+
 SELECT id FROM tab WHERE hasToken(text, 'b');
+
 SELECT id FROM tab WHERE hasToken(text, 'c');
+
 INSERT INTO tab SELECT number , 'aaabbbccc' FROM numbers(128);
+
 SELECT id FROM tab WHERE hasToken(text, 'aaabbbccc');
