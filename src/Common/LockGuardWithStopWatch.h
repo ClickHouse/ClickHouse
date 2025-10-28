8e22bedad3de0922159889fe75187fa47386cf07
Nikita Mikhaylov <nikitamikhaylov@clickhouse.com>
2025-10-28 11:51:30 +0100

    Added finalize to cancel method + avoid overflow in stopwatch

--- src/Common/LockGuardWithStopWatch.h
+++ src/Common/LockGuardWithStopWatch.h
@@ -32,6 +32,7 @@ struct TSA_SCOPED_LOCKABLE LockGuardWithStopWatch final
 
         wait_watch.emplace(CLOCK_MONOTONIC);
         lock.emplace(mutex_);
+        wait_watch->stop();
         lock_watch.emplace(CLOCK_MONOTONIC);
     }
 
@@ -46,13 +47,12 @@ struct TSA_SCOPED_LOCKABLE LockGuardWithStopWatch final
 
         /// Must be destroyed first.
         lock.reset();
-        wait_watch->stop();
         lock_watch->stop();
 
-        if (wait_watch->elapsedMilliseconds() - lock_watch->elapsedMilliseconds() > THRESHOLD_MILLISECONDS)
+        if (wait_watch->elapsedMilliseconds() > THRESHOLD_MILLISECONDS)
         {
             LOG_WARNING(log, "Lock acquisition took {} ms for thread {} in [{}], Stack trace (when copying this message, always include the lines below): \n {}",
-                wait_watch->elapsedMilliseconds() - lock_watch->elapsedMilliseconds(), thread_id, caller, StackTrace().toString());
+                wait_watch->elapsedMilliseconds(), thread_id, caller, StackTrace().toString());
         }
 
         if (lock_watch->elapsedMilliseconds() > THRESHOLD_MILLISECONDS)
