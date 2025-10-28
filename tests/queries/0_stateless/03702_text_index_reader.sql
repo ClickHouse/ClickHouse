f5f993faf0a35f9ebc03195279a46c01685135b5
fastio <pengjian.uestc@gmail.com>
2025-10-28 15:34:32 +0800

    test cases

---
+++ tests/queries/0_stateless/03702_text_index_reader.sql
@@ -0,0 +1,15 @@
+DROP TABLE IF EXISTS default.t;
+CREATE TABLE default.t
+(
+    `id`   UInt64,
+    `text` String,
+    INDEX inv_idx text TYPE text(tokenizer = 'default') GRANULARITY 4
+)
+ENGINE = MergeTree
+ORDER BY id
+SETTINGS index_granularity = 32;
+INSERT INTO t VALUES (0,'a'),(1,'b'),(2,'c'),(3,'d');
+SELECT id FROM t WHERE hasToken(text, 'b');
+SELECT id FROM t WHERE hasToken(text, 'c');
+INSERT INTO t SELECT number , 'aaabbbccc' FROM numbers(128);
+SELECT id FROM t WHERE hasToken(text, 'aaabbbccc');
