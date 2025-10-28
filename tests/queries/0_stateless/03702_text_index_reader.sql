19eb1381209a9dfc6fc1684fec8a337fcce0dc95
fastio <pengjian.uestc@gmail.com>
2025-10-28 20:18:46 +0800

    fix test case

--- tests/queries/0_stateless/03702_text_index_reader.sql
+++ tests/queries/0_stateless/03702_text_index_reader.sql
@@ -1,17 +1,17 @@
 set allow_experimental_full_text_index = 1;
 
-DROP TABLE IF EXISTS default.t;
-CREATE TABLE default.t
+DROP TABLE IF EXISTS tab;
+CREATE TABLE tab
 (
     `id`   UInt64,
     `text` String,
-    INDEX inv_idx text TYPE text(tokenizer = 'default') GRANULARITY 4
+    INDEX inv_idx text TYPE text(tokenizer = 'splitByNonAlpha') GRANULARITY 4
 )
 ENGINE = MergeTree
 ORDER BY id
 SETTINGS index_granularity = 32;
-INSERT INTO t VALUES (0,'a'),(1,'b'),(2,'c'),(3,'d');
-SELECT id FROM t WHERE hasToken(text, 'b');
-SELECT id FROM t WHERE hasToken(text, 'c');
-INSERT INTO t SELECT number , 'aaabbbccc' FROM numbers(128);
-SELECT id FROM t WHERE hasToken(text, 'aaabbbccc');
+INSERT INTO tab VALUES (0,'a'),(1,'b'),(2,'c'),(3,'d');
+SELECT id FROM tab WHERE hasToken(text, 'b');
+SELECT id FROM tab WHERE hasToken(text, 'c');
+INSERT INTO tab SELECT number , 'aaabbbccc' FROM numbers(128);
+SELECT id FROM tab WHERE hasToken(text, 'aaabbbccc');
