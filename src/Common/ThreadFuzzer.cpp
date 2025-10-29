c8ea4497ccc292bf4362b1318d48be526fed5ad7
Raúl Marín <raul.marin@clickhouse.com>
2025-10-29 10:09:51 +0100

    Avoid recursion when initializing ThreadFuzzer::instance()

--- src/Common/ThreadFuzzer.cpp
+++ src/Common/ThreadFuzzer.cpp
@@ -52,7 +52,8 @@ namespace ErrorCodes
 ThreadFuzzer::ThreadFuzzer()
 {
     initConfiguration();
-    start();
+    if (isEffective())
+        started.store(true, std::memory_order_relaxed);
 }
 
 template <typename T>
