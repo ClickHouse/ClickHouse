17b880e2ad4b3ee14767254d3025596f02420e36
Robert Schulze <robert@clickhouse.com>
2025-10-29 11:55:48 +0000

    Check flakyness

--- tests/queries/0_stateless/03457_query_cache_log_comment.sql
+++ tests/queries/0_stateless/03457_query_cache_log_comment.sql
@@ -12,4 +12,7 @@ SELECT c FROM tab SETTINGS use_query_cache = 1, log_comment='bbb';
 
 system flush logs query_log;
 
-select log_comment, ProfileEvents['QueryCacheHits'], ProfileEvents['QueryCacheMisses'] from system.query_log where type = 'QueryFinish' and event_time > now() - 600 and current_database = currentDatabase() AND query like 'SELECT c FROM tab SETTINGS use_query_cache%' order by event_time_microseconds;
+select log_comment, ProfileEvents['QueryCacheHits'], ProfileEvents['QueryCacheMisses']
+from system.query_log
+where type = 'QueryFinish' and event_time > now() - 600 and current_database = currentDatabase() AND query like 'SELECT c FROM tab SETTINGS use_query_cache%'
+order by event_time_microseconds;
