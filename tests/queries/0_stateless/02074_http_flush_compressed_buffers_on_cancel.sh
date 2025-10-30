5f00eb3e46ccff01f117aa3f0aeb3fa8a9d0cca8
Alexander Tokmakov <tavplubix@clickhouse.com>
2025-10-30 20:01:20 +0100

    flush gzip buffer when cancelling http response buffer

---
+++ tests/queries/0_stateless/02074_http_flush_compressed_buffers_on_cancel.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env bash
+
+CURDIR=$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)
+# shellcheck source=../shell_config.sh
+. "$CURDIR"/../shell_config.sh
+
+echo "select throwIf(number > 10000000) + number check from numbers(10000005) format CSV" | ${CLICKHOUSE_CURL} "${CLICKHOUSE_URL}&query=&enable_http_compression=1" -H "Accept-Encoding: zstd" --compressed --data-binary @- | grep -q "__exception__" && echo 'zstd OK'
+echo "select throwIf(number > 10000000) + number check from numbers(10000005) format CSV" | ${CLICKHOUSE_CURL} "${CLICKHOUSE_URL}&query=&enable_http_compression=1" -H "Accept-Encoding: gzip" --compressed --data-binary @- | grep -q "__exception__" && echo 'gzip OK'
