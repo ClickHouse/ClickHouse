d985643f0907f0a55491770ea278ada5b7867122
Pavel Kruglov <48961922+Avogar@users.noreply.github.com>
2025-10-31 13:53:27 +0100

    Fix test 03701_json_duplicate_path_insert_select.sql

--- tests/queries/0_stateless/03701_json_duplicate_path_insert_select.sql
+++ tests/queries/0_stateless/03701_json_duplicate_path_insert_select.sql
@@ -3,10 +3,10 @@ DROP TABLE IF EXISTS test_json_duplicates;
 CREATE TABLE test_json_duplicates (json JSON) ENGINE = Memory;
 
 -- Should fail without setting
-INSERT INTO test_json_duplicates SELECT '{"a": 1, "a": 2}'::JSON; -- {serverError INCORRECT_DATA}
+INSERT INTO test_json_duplicates SELECT '{"a": 1, "a": 2}'; -- {serverError INCORRECT_DATA}
 
 -- Should succeed with setting
-INSERT INTO test_json_duplicates SELECT '{"a": 1, "a": 2}'::JSON SETTINGS type_json_skip_duplicated_paths=1;
+INSERT INTO test_json_duplicates SELECT '{"a": 1, "a": 2}' SETTINGS type_json_skip_duplicated_paths=1;
 SELECT * FROM test_json_duplicates;
 
 DROP TABLE test_json_duplicates;
