07c1c3fe6d5a64100401296c571b50f0cb470e4e
Alexey Milovidov <milovidov@clickhouse.com>
2025-10-31 22:26:16 +0100

    `create_table_empty_primary_key_by_default` did not work for the default table engine

--- src/Storages/MergeTree/registerStorageMergeTree.cpp
+++ src/Storages/MergeTree/registerStorageMergeTree.cpp
@@ -220,7 +220,7 @@ static TableZnodeInfo extractZooKeeperPathAndReplicaNameFromEngineArgs(
 {
     chassert(isReplicated(engine_name));
 
-    bool is_extended_storage_def = isExtendedStorageDef(query);
+    bool is_extended_storage_def = engine_args.empty() || isExtendedStorageDef(query);
 
     if (is_extended_storage_def)
     {
@@ -248,7 +248,6 @@ static TableZnodeInfo extractZooKeeperPathAndReplicaNameFromEngineArgs(
     {
         bool is_replicated_database = local_context->getClientInfo().query_kind == ClientInfo::QueryKind::SECONDARY_QUERY &&
             DatabaseCatalog::instance().getDatabase(table_id.database_name)->getEngineName() == "Replicated";
-
 
         /// Get path and name from engine arguments
         auto * ast_zk_path = engine_args[arg_num]->as<ASTLiteral>();
@@ -392,7 +391,7 @@ static StoragePtr create(const StorageFactory::Arguments & args)
         *  - Additional MergeTreeSettings in the SETTINGS clause;
         */
 
-    bool is_extended_storage_def = isExtendedStorageDef(args.query);
+    bool is_extended_storage_def = args.engine_args.empty() || isExtendedStorageDef(args.query);
 
     const Settings & local_settings = args.getLocalContext()->getSettingsRef();
 
