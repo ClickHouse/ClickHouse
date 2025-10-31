d3a0edef0f6488802627795d8a3fac3892149183
Shankar Iyer <shankar.iyer@clickhouse.com>
2025-10-31 04:02:01 +0000

    fix a test

--- tests/queries/0_stateless/02346_text_index_dictionary_cache.sql
+++ tests/queries/0_stateless/02346_text_index_dictionary_cache.sql
@@ -9,6 +9,7 @@ SET use_skip_indexes_on_data_read = 1;
 SET query_plan_direct_read_from_text_index = 1;
 SET use_text_index_dictionary_cache = 1;
 SET log_queries = 1;
+SET max_rows_to_read = 0;
 
 DROP TABLE IF EXISTS tab;
 CREATE TABLE tab
