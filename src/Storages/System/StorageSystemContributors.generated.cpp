7f89863de0d5614c57bcec3c8ea8c6f0db69daab
robot-clickhouse <robot-clickhouse@users.noreply.github.com>
2025-11-01 12:47:02 +0000

    Update autogenerated version to 25.10.1.3832 and contributors

--- src/Storages/System/StorageSystemContributors.generated.cpp
+++ src/Storages/System/StorageSystemContributors.generated.cpp
@@ -1653,6 +1653,7 @@ const char * auto_contributors[] {
     "cjw",
     "clarkcaoliu",
     "clickhouse-adrianfraguela",
+    "clickhouse-gh[bot]",
     "clickhouse-robot-curie",
     "clundro",
     "cluster",
