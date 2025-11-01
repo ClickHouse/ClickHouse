3914ad8f8821716a5d08838c477f9d3e65032ea2
0xgouda <gouda0x@gmail.com>
2025-11-01 09:15:17 +0200

    Throw exception on using fractional/negative limit/offset in limit by expression

--- src/Planner/Planner.cpp
+++ src/Planner/Planner.cpp
@@ -1017,14 +1017,27 @@ void addWithFillStepIfNeeded(QueryPlan & query_plan,
 void addLimitByStep(
     QueryPlan & query_plan, const LimitByAnalysisResult & limit_by_analysis_result, const QueryNode & query_node, bool do_not_skip_offset)
 {
-    /// Constness of LIMIT BY limit is validated during query analysis stage
-    UInt64 limit_by_limit = query_node.getLimitByLimit()->as<ConstantNode &>().getValue().safeGet<UInt64>();
+    Field limit_by_limit_field = query_node.getLimitByLimit()->as<ConstantNode &>().getValue();
+    const Field converted_limit_value_uint = convertFieldToType(limit_by_limit_field, DataTypeUInt64());
+    if (converted_limit_value_uint.isNull())
+        throw Exception(
+            ErrorCodes::INVALID_LIMIT_EXPRESSION,
+            "The value {} of LIMIT in LIMIT BY expression is not representable as UInt64",
+            applyVisitor(FieldVisitorToString(), limit_by_limit_field));
+
+    UInt64 limit_by_limit = limit_by_limit_field.safeGet<UInt64>();
     UInt64 limit_by_offset = 0;
 
     if (query_node.hasLimitByOffset())
     {
-        /// Constness of LIMIT BY offset is validated during query analysis stage
-        limit_by_offset = query_node.getLimitByOffset()->as<ConstantNode &>().getValue().safeGet<UInt64>();
+        Field limit_by_offset_field = query_node.getLimitByOffset()->as<ConstantNode &>().getValue();
+        const Field converted_offset_value_uint = convertFieldToType(limit_by_offset_field, DataTypeUInt64());
+        if (converted_offset_value_uint.isNull())
+            throw Exception(
+                ErrorCodes::INVALID_LIMIT_EXPRESSION,
+                "The value {} of OFFSET in LIMIT BY expression is not representable as UInt64",
+                applyVisitor(FieldVisitorToString(), limit_by_offset_field));
+        limit_by_offset = limit_by_offset_field.safeGet<UInt64>();
     }
 
     if (do_not_skip_offset)
