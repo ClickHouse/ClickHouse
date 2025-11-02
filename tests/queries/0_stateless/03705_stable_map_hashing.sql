7a1522d7717a9b38f03ee748af3f54bfbecadd6b
Manuel Raimann <raimannma@outlook.de>
2025-11-02 22:18:25 +0100

    Add test for maps with duplicate keys

--- tests/queries/0_stateless/03705_stable_map_hashing.sql
+++ tests/queries/0_stateless/03705_stable_map_hashing.sql
@@ -21,3 +21,23 @@ SELECT
     sipHash64(map_a) = sipHash64(map_b) AS hashes_equal_sip,
     xxHash64(map_a) = xxHash64(map_b) AS hashes_equal_xx,
     murmurHash3_64(map_a) = murmurHash3_64(map_b) AS hashes_equal_murmur;
+
+SELECT '-- Test 3: Maps with duplicate keys (different value order)';
+WITH
+    materialize(map('aaa', 1, 'aaa', 2)) AS map_a,
+    materialize(map('aaa', 2, 'aaa', 1)) AS map_b
+SELECT
+    cityHash64(map_a) = cityHash64(map_b) AS hashes_equal,
+    sipHash64(map_a) = sipHash64(map_b) AS hashes_equal_sip,
+    xxHash64(map_a) = xxHash64(map_b) AS hashes_equal_xx,
+    murmurHash3_64(map_a) = murmurHash3_64(map_b) AS hashes_equal_murmur;
+
+SELECT '-- Test 4: Maps with duplicate keys and multiple keys';
+WITH
+    materialize(map('x', 1, 'x', 2, 'y', 3)) AS map_a,
+    materialize(map('y', 3, 'x', 2, 'x', 1)) AS map_b
+SELECT
+    cityHash64(map_a) = cityHash64(map_b) AS hashes_equal,
+    sipHash64(map_a) = sipHash64(map_b) AS hashes_equal_sip,
+    xxHash64(map_a) = xxHash64(map_b) AS hashes_equal_xx,
+    murmurHash3_64(map_a) = murmurHash3_64(map_b) AS hashes_equal_murmur;
