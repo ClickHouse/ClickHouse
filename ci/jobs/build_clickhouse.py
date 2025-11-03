108b26c9a3317d1bd3eeaaa8826cc57f7658e22b
Max Kainov <max.kainov@clickhouse.com>
2025-11-03 10:39:49 +0100

    CI: Build script should not overwrite timers set by praktika

--- ci/jobs/build_clickhouse.py
+++ ci/jobs/build_clickhouse.py
@@ -86,8 +86,6 @@ def run_shell(name, command, **kwargs):
 def main():
     args = parse_args()
 
-    stop_watch = Utils.Stopwatch()
-
     stages = list(JobStages)
     stage = args.param or JobStages.CHECKOUT_SUBMODULES
     if stage:
@@ -269,9 +267,7 @@ def main():
         )
         res = results[-1].is_ok()
 
-    Result.create_from(
-        results=results, stopwatch=stop_watch, files=files
-    ).complete_job()
+    Result.create_from(results=results, files=files).complete_job()
 
 
 if __name__ == "__main__":
