15af39ef22763631275738a61835377aa02b3504
Alexander Tokmakov <tavplubix@clickhouse.com>
2025-11-05 11:56:32 +0100

    update test

--- tests/queries/0_stateless/02074_http_flush_compressed_buffers_on_cancel.sh
+++ tests/queries/0_stateless/02074_http_flush_compressed_buffers_on_cancel.sh
@@ -7,3 +7,5 @@ CURDIR=$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)
 
 echo "select throwIf(number > 10000000) + number check from numbers(10000005) format CSV" | ${CLICKHOUSE_CURL} "${CLICKHOUSE_URL}&query=&enable_http_compression=1" -H "Accept-Encoding: zstd" --compressed --data-binary @- | grep -q "__exception__" && echo 'zstd OK'
 echo "select throwIf(number > 10000000) + number check from numbers(10000005) format CSV" | ${CLICKHOUSE_CURL} "${CLICKHOUSE_URL}&query=&enable_http_compression=1" -H "Accept-Encoding: gzip" --compressed --data-binary @- | grep -q "__exception__" && echo 'gzip OK'
+echo "select throwIf(number > 10000000) + number check from numbers(10000005) format CSV" | ${CLICKHOUSE_CURL} "${CLICKHOUSE_URL}&query=&enable_http_compression=1" -H "Accept-Encoding: br" --compressed --data-binary @- | grep -q "__exception__" && echo 'br OK'
+echo "select throwIf(number > 10000000) + number check from numbers(10000005) format CSV" | ${CLICKHOUSE_CURL} "${CLICKHOUSE_URL}&query=&enable_http_compression=1" -H "Accept-Encoding: deflate" --compressed --data-binary @- | grep -q "__exception__" && echo 'deflate OK'
