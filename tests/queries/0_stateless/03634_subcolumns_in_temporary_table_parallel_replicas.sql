e06b52f227958ead03eedb4676660f0ad4f34720
Pavel Kruglov <48961922+Avogar@users.noreply.github.com>
2025-11-05 14:46:27 +0100

    Enable analyzer explicitly in new test

--- tests/queries/0_stateless/03634_subcolumns_in_temporary_table_parallel_replicas.sql
+++ tests/queries/0_stateless/03634_subcolumns_in_temporary_table_parallel_replicas.sql
@@ -1,3 +1,4 @@
+SET enable_analyzer=1;
 DROP TABLE IF EXISTS t0;
 CREATE TABLE t0 (c0 Nullable(Int)) ENGINE = ReplicatedMergeTree('/clickhouse/tables/test_03634_{database}/t0', 'r1') ORDER BY tuple();
 INSERT INTO TABLE t0 (c0) VALUES (1);
