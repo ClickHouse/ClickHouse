6cfeb43285854a7618892eef403b41774c401de8
Kaviraj <kavirajkanagaraj@gmail.com>
2025-11-05 21:40:44 +0100

    add new setting `send_profile_events` to SettingsChangeHistory
    Signed-off-by: Kaviraj <kavirajkanagaraj@gmail.com>

--- src/Core/SettingsChangesHistory.cpp
+++ src/Core/SettingsChangesHistory.cpp
@@ -41,6 +41,7 @@ const VersionToSettingsChangesMap & getSettingsChangesHistory()
         /// Note: please check if the key already exists to prevent duplicate entries.
         addSettingsChanges(settings_changes_history, "25.11",
         {
+            {"send_profile_events", false, true, "New setting. Whether to send profile events to the clients."},
             {"into_outfile_create_parent_directories", false, false, "New setting"},
             {"correlated_subqueries_default_join_kind", "left", "right", "New setting. Default join kind for decorrelated query plan."},
             {"use_statistics_cache", 0, 0, "New setting"},
