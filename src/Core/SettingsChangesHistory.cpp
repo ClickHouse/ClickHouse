f4baac6d4bae95bff19484a6d3c57f8dab1063fd
János Benjamin Antal <benjamin.antal@clickhouse.com>
2025-11-05 13:17:52 +0000

    Fix merge tree setting changes history

--- src/Core/SettingsChangesHistory.cpp
+++ src/Core/SettingsChangesHistory.cpp
@@ -932,6 +932,8 @@ const VersionToSettingsChangesMap & getMergeTreeSettingsChangesHistory()
         {
             {"refresh_statistics_interval", 0, 0, "New setting"},
             {"shared_merge_tree_create_per_replica_metadata_nodes", true, false, "Reduce the amount of metadata in Keeper."},
+            {"serialization_info_version", "basic", "with_types", "Change to the newer format allowing custom string serialization"},
+            {"string_serialization_version", "single_stream", "with_size_stream", "Change to the newer format with separate sizes"},
         });
         addSettingsChanges(merge_tree_settings_changes_history, "25.10",
         {
