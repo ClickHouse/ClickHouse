f449e78f159fe58b5640213745f6da3c3f193b62
robot-clickhouse <robot-clickhouse@users.noreply.github.com>
2025-11-05 17:12:25 +0000

    Backport #89527 to 25.3: Fix possible "Context has expired" with analyzer and PK IN (subquery) (v2)

--- src/Processors/QueryPlan/QueryPlan.h
+++ src/Processors/QueryPlan/QueryPlan.h
@@ -107,6 +107,7 @@ public:
     /// Do not allow to change the table while the pipeline alive.
     void addTableLock(TableLockHolder lock) { resources.table_locks.emplace_back(std::move(lock)); }
     void addInterpreterContext(std::shared_ptr<const Context> context) { resources.interpreter_context.emplace_back(std::move(context)); }
+    auto getInterpretersContexts() const { return resources.interpreter_context; }
     void addStorageHolder(StoragePtr storage) { resources.storage_holders.emplace_back(std::move(storage)); }
 
     void addResources(QueryPlanResourceHolder resources_) { resources = std::move(resources_); }
