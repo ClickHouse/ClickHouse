cc03bacfa1cc5c6abce7fee6d1259b479a275eaa
Raúl Marín <raul.marin@clickhouse.com>
2025-11-05 19:36:11 +0100

    Typo

--- src/Loggers/OwnSplitChannel.cpp
+++ src/Loggers/OwnSplitChannel.cpp
@@ -344,7 +344,7 @@ void AsyncLogMessageQueue::enqueueMessage(AsyncLogMessagePtr message)
     }
 
     message_queue.push_back(std::move(message));
-    /// Request the thread to flush as fast as possible (without acquiring the mutex everytime)
+    /// Request the thread to flush as fast as possible (without acquiring the mutex every time)
     if (current_size > max_size / 2)
         request_flush = true;
     condition.notify_one();
