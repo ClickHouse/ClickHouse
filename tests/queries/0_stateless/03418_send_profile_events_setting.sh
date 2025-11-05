6b683a22e16b15a635b832ebeb976fa9a3a2ec1a
Kaviraj <kavirajkanagaraj@gmail.com>
2025-11-05 18:16:27 +0100

    fix some comments on tests script
    Signed-off-by: Kaviraj <kavirajkanagaraj@gmail.com>

--- tests/queries/0_stateless/03418_send_profile_events_setting.sh
+++ tests/queries/0_stateless/03418_send_profile_events_setting.sh
@@ -6,11 +6,11 @@ CURDIR=$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)
 # shellcheck source=../shell_config.sh
 . "$CURDIR"/../shell_config.sh
 
-# default should send profile events without any explicit setting
+# default should send profile events without any explicit setting.
 ${CLICKHOUSE_CLIENT} -q 'SELECT 1' --print-profile-events 2>&1 | grep -q "MemoryTrackerUsage:" && echo "default OK" 
-# enabled with explicit setting
+# enabled with explicit setting.
 ${CLICKHOUSE_CLIENT} -q 'SELECT 1 SETTINGS send_profile_events=1' --print-profile-events 2>&1 | grep -q "MemoryTrackerUsage:" && echo "enabled OK" 
 
-# disabled with explicit setting
+# disabled with explicit setting.
 ${CLICKHOUSE_CLIENT} -q 'SELECT 1 SETTINGS send_profile_events=0' --print-profile-events 2>&1 | grep -v "MemoryTrackerUsage:" | grep -q "1" && echo "disabled OK" 
 
