7091da0eddf23ebc4ee1736bbd7e21078767fcd0
garrettthomas <garrett.thomas@clickhouse.com>
2025-11-05 08:28:33 -0800

    add semi colons

--- tests/queries/0_stateless/03641_max_subcolumns.sql
+++ tests/queries/0_stateless/03641_max_subcolumns.sql
@@ -4,7 +4,7 @@ CREATE TABLE WorksBeforeSubcolumnLimit
     `Subcolumns` Nested(subcolumn1 UInt32, subcolumn2 UInt32, subcolumn3 DateTime, subcolumn3 Int64, subcolumn4 String, subcolumn5 UInt32)
 )
 ENGINE = MergeTree
-ORDER BY CounterID
+ORDER BY CounterID;
 
 SET max_subcolumns = 2;
 
@@ -14,4 +14,4 @@ CREATE TABLE FailsAfterSubcolumnLimit
     `Subcolumns` Nested(subcolumn1 UInt32, subcolumn2 UInt32, subcolumn3 DateTime, subcolumn4 Int64, subcolumn5 String, subcolumn6 UInt32)
 )
 ENGINE = MergeTree
-ORDER BY CounterID
+ORDER BY CounterID;
