0fe7801b646cc7f697bc7087d35abfed8de6292d
Alexander Gololobov <davenger@clickhouse.com>
2025-11-06 21:40:29 +0100

    Only with analyzer enabled

--- tests/queries/0_stateless/03580_join_runtime_filter_column_type.sql
+++ tests/queries/0_stateless/03580_join_runtime_filter_column_type.sql
@@ -2,4 +2,4 @@
 SELECT 1
 FROM numbers(1) AS t0
 WHERE EXISTS (SELECT t0._table)
-SETTINGS enable_join_runtime_filters = 1, allow_experimental_correlated_subqueries = 1;
+SETTINGS enable_join_runtime_filters = 1, allow_experimental_correlated_subqueries = 1, enable_analyzer = 1;
