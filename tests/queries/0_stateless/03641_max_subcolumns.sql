739a64f3138c613ece7052b6dcf5609cdb8e78b9
garrettthomas <garrett.thomas@clickhouse.com>
2025-11-05 16:37:23 -0800

    fix duplicate subcolumn

--- tests/queries/0_stateless/03641_max_subcolumns.sql
+++ tests/queries/0_stateless/03641_max_subcolumns.sql
@@ -1,7 +1,7 @@
 CREATE TABLE WorksBeforeSubcolumnLimit
 (
     `ID` UInt32,
-    `Subcolumns` Nested(subcolumn1 UInt32, subcolumn2 UInt32, subcolumn3 DateTime, subcolumn3 Int64, subcolumn4 String, subcolumn5 UInt32)
+    `Subcolumns` Nested(subcolumn1 UInt32, subcolumn2 UInt32, subcolumn3 DateTime, subcolumn4 Int64, subcolumn5 String, subcolumn6 UInt32)
 )
 ENGINE = MergeTree
 ORDER BY CounterID;
