32fe88c6253887521d868401b86dd2756c9bd502
Kaviraj <kavirajkanagaraj@gmail.com>
2025-11-06 10:35:12 +0100

    change profile event to simple `Query` to make less flaky
    Signed-off-by: Kaviraj <kavirajkanagaraj@gmail.com>

--- tests/queries/0_stateless/03418_send_profile_events_setting.sh
+++ tests/queries/0_stateless/03418_send_profile_events_setting.sh
@@ -9,10 +9,10 @@ CURDIR=$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)
 . "$CURDIR"/../shell_config.sh
 
 # default should send profile events without any explicit setting.
-${CLICKHOUSE_CLIENT} -q 'SELECT 1' --print-profile-events 2>&1 | grep -q "MemoryTrackerUsage:" && echo "default OK" 
+${CLICKHOUSE_CLIENT} -q 'SELECT 1' --print-profile-events 2>&1 | grep -q "Query" && echo "default OK" 
 # enabled with explicit setting.
-${CLICKHOUSE_CLIENT} -q 'SELECT 1 SETTINGS send_profile_events=1' --print-profile-events 2>&1 | grep -q "MemoryTrackerUsage:" && echo "enabled OK" 
+${CLICKHOUSE_CLIENT} -q 'SELECT 1 SETTINGS send_profile_events=1' --print-profile-events 2>&1 | grep -q "Query" && echo "enabled OK" 
 
 # disabled with explicit setting.
-${CLICKHOUSE_CLIENT} -q 'SELECT 1 SETTINGS send_profile_events=0' --print-profile-events 2>&1 | grep -v "MemoryTrackerUsage:" | grep -q "1" && echo "disabled OK" 
+${CLICKHOUSE_CLIENT} -q 'SELECT 1 SETTINGS send_profile_events=0' --print-profile-events 2>&1 | grep -v "Query" | grep -q "1" && echo "disabled OK" 
 
