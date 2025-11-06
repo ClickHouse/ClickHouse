dd3f595b04b31116fdd5de56fbcd0dc9650cd662
rienath <raufs.dunamalijevs@clickhouse.com>
2025-11-06 16:58:56 +0000

    Fix to pass flaky check

--- tests/queries/0_stateless/03314_insert_select_message.sh
+++ tests/queries/0_stateless/03314_insert_select_message.sh
@@ -7,22 +7,22 @@ CUR_DIR=$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)
 # Track degradations for issue https://github.com/ClickHouse/ClickHouse/issues/47800
 
 $CLICKHOUSE_CLIENT --query "CREATE DATABASE IF NOT EXISTS test_03314"
-$CLICKHOUSE_CLIENT --query "CREATE TABLE IF NOT EXISTS test_03314.simple_table (id UInt64, name String) ENGINE = Memory"
+$CLICKHOUSE_CLIENT --query "CREATE TABLE IF NOT EXISTS test_03314.table (id UInt64, name String) ENGINE = Memory"
 
 # Test 1: INSERT-SELECT (processed_rows_from_progress)
-$CLICKHOUSE_CLIENT --processed-rows --query "INSERT INTO test_03314.simple_table SELECT number AS id, toString(number) AS name FROM numbers(100)"
+$CLICKHOUSE_CLIENT --processed-rows --query "INSERT INTO test_03314.table SELECT number AS id, toString(number) AS name FROM numbers(100)"
 
 # Test 2: Large INSERT-SELECT (processed_rows_from_progress)
-$CLICKHOUSE_CLIENT --processed-rows --query "INSERT INTO test_03314.simple_table SELECT number AS id, toString(number) AS name FROM numbers(1000000)"
+$CLICKHOUSE_CLIENT --processed-rows --query "INSERT INTO test_03314.table SELECT number AS id, toString(number) AS name FROM numbers(1000000)"
 
 # Test 3: Regular SELECT (processed_rows_from_blocks)
-$CLICKHOUSE_CLIENT --processed-rows --query "SELECT * FROM test_03314.simple_table ORDER BY id LIMIT 30"
+$CLICKHOUSE_CLIENT --processed-rows --query "SELECT * FROM test_03314.table ORDER BY id LIMIT 30"
 
 # Test 4: INSERT with Materialized View (to verify we correctly show that data was inserted twice)
-$CLICKHOUSE_CLIENT --query "CREATE MATERIALIZED VIEW IF NOT EXISTS test_03314.mv_simple ENGINE = Memory AS SELECT id FROM test_03314.simple_table"
-$CLICKHOUSE_CLIENT --processed-rows --query "INSERT INTO test_03314.simple_table SELECT number AS id, toString(number) AS name FROM numbers(50)"
+$CLICKHOUSE_CLIENT --query "CREATE MATERIALIZED VIEW IF NOT EXISTS test_03314.mv ENGINE = Memory AS SELECT id FROM test_03314.table"
+$CLICKHOUSE_CLIENT --processed-rows --query "INSERT INTO test_03314.table SELECT number AS id, toString(number) AS name FROM numbers(50)"
 
 # Cleanup
-$CLICKHOUSE_CLIENT --query "DROP VIEW test_03314.mv_simple"
-$CLICKHOUSE_CLIENT --query "DROP TABLE test_03314.simple_table"
-$CLICKHOUSE_CLIENT --query "DROP DATABASE test_03314"
+$CLICKHOUSE_CLIENT --query "DROP VIEW IF EXISTS test_03314.mv"
+$CLICKHOUSE_CLIENT --query "DROP TABLE IF EXISTS test_03314.table"
+$CLICKHOUSE_CLIENT --query "DROP DATABASE IF EXISTS test_03314"
