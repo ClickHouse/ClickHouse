aac7ace1d87a4f7b846f2ab24e212b2e813dd87b
Alexander Tokmakov <tavplubix@gmail.com>
2025-11-07 17:07:47 +0100

    Revert "Move partition commands check out of the disks"

--- src/Disks/ObjectStorages/IMetadataStorage.h
+++ src/Disks/ObjectStorages/IMetadataStorage.h
@@ -26,6 +26,7 @@ namespace ErrorCodes
 }
 
 class IMetadataStorage;
+struct PartitionCommand;
 
 /// Return the result of operation to the caller.
 /// It is used in `IDiskObjectStorageOperation::finalize` after metadata transaction executed to make decision on blob removal.
@@ -239,6 +240,8 @@ public:
     {
         throwNotImplemented();
     }
+
+    virtual bool supportsPartitionCommand(const PartitionCommand & /* command */) const = 0;
 
     virtual std::vector<std::string> listDirectory(const std::string & path) const = 0;
 
