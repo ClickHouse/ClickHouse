0008af0a2cdf3aca6dfdb3b03e2794d9ef04933b
Mikhail Artemenko <mikhail.artemenko@clickhouse.com>
2025-11-08 23:48:45 +0000

    do not run 03001_matview_columns_after_modify_query under remote db disk

--- tests/queries/0_stateless/03001_matview_columns_after_modify_query.sh
+++ tests/queries/0_stateless/03001_matview_columns_after_modify_query.sh
@@ -1,7 +1,8 @@
 #!/usr/bin/env bash
-# Tags: no-replicated-database, no-shared-catalog
+# Tags: no-replicated-database, no-shared-catalog, no-remote-database-disk
 # Tag no-replicated-database -- modify on-disk metadata that may lead to "Digest does not match" in case Replicated database
 # Tag no-shared-catalog -- same
+# Tag no-remote-database-disk -- plain rewritable disk can not handle out or server disk change
 
 CURDIR=$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)
 # shellcheck source=../shell_config.sh
@@ -66,17 +67,7 @@ data_path=$(${CLICKHOUSE_CLIENT} -q "SELECT path FROM system.disks WHERE name = 
 
 data_path=$(${CLICKHOUSE_CLIENT} -q "SELECT path FROM system.disks WHERE name = 'default'")
 
-if [ -e "$data_path$mv_metadata_path" ]; then
-    #cat $mv_metadata_path
-    sed -i -e 's/`timestamp` DateTime,/`timestamp` DateTime64(9),/g' -e 's/`c12` Nullable(String)/`c12` String/g' "$data_path$mv_metadata_path"
-    #cat $mv_metadata_path
-else
-    # Using a remote DB disk
-    config="${CURDIR}/03001_matview_columns_after_modify_query.xml"
-    mv_metadata=$(clickhouse-disks -C "$config" --disk "disk_db_remote" --save-logs --query "read $mv_metadata_path") 
-    mv_metadata_updated=$(echo $mv_metadata | sed -e 's/`timestamp` DateTime,/`timestamp` DateTime64(9),/g' -e 's/`c12` Nullable(String)/`c12` String/g')
-    echo $mv_metadata_updated | clickhouse-disks -C "$config" --disk "disk_db_remote" --save-logs --query "write --path-to $mv_metadata_path"
-fi
+sed -i -e 's/`timestamp` DateTime,/`timestamp` DateTime64(9),/g' -e 's/`c12` Nullable(String)/`c12` String/g' "$data_path$mv_metadata_path"
 
 ${CLICKHOUSE_CLIENT} -q "ATTACH TABLE mv"
 
