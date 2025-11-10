0c45edadf5967ba76444c84a35a92eba0168f5d0
yanglongwei <ylong2483@gmail.com>
2025-11-10 16:56:55 +0000

    Add unit test for compressing a zero-size buffer in T64 compression

--- src/Compression/tests/gtest_compressionCodec.cpp
+++ src/Compression/tests/gtest_compressionCodec.cpp
@@ -1382,7 +1382,7 @@ TEST(T64Test, TranscodeRawInput)
 
     for (const auto & type : types)
     {
-        for (size_t buffer_size = 1; buffer_size < 2000; buffer_size++)
+        for (size_t buffer_size = 8; buffer_size < 2000; buffer_size++)
         {
             DB::Memory<> source_memory;
             source_memory.resize(buffer_size);
@@ -1404,6 +1404,39 @@ TEST(T64Test, TranscodeRawInput)
             ASSERT_EQ(decompressed, source_memory.size());
             for (size_t i = 0; i < decompressed; ++i)
                 ASSERT_EQ(memory_for_decompression.data()[i], source_memory.data()[i]) << "with data type " << type->getName() << " with buffer size " << buffer_size << " at position " << i;
+        }
+    }
+}
+
+TEST(T64Test, CompressZeroBufferThrows)
+{
+    std::vector<DataTypePtr> types = {
+        std::make_shared<DataTypeInt8>(),
+        std::make_shared<DataTypeInt16>(),
+        std::make_shared<DataTypeInt32>(),
+        std::make_shared<DataTypeInt64>(),
+        std::make_shared<DataTypeUInt8>(),
+        std::make_shared<DataTypeUInt16>(),
+        std::make_shared<DataTypeUInt32>(),
+        std::make_shared<DataTypeUInt64>(),
+    };
+
+    for (const auto & type : types)
+    {
+        for (size_t buffer_size = 0; buffer_size < type->getSizeOfValueInMemory(); buffer_size++)
+        {
+            DB::Memory<> source_memory;
+            source_memory.resize(buffer_size);
+
+            for (size_t i = 0; i < buffer_size; ++i)
+                source_memory.data()[i] = i;
+
+            DB::Memory<> memory_for_compression;
+            auto codec = makeCodec("T64", type);
+
+            memory_for_compression.resize(codec->getCompressedReserveSize(buffer_size));
+
+            ASSERT_THROW(codec->compress(source_memory.data(), UInt32(source_memory.size()), memory_for_compression.data()), Exception);
         }
     }
 }
