06a964d43365375f11e4cbc4d830f0e4655553ff
Mikhail Artemenko <mikhail.artemenko@clickhouse.com>
2025-11-10 13:44:16 +0000

    do not perform access check for read only disks

--- src/Disks/ObjectStorages/RegisterDiskObjectStorage.cpp
+++ src/Disks/ObjectStorages/RegisterDiskObjectStorage.cpp
@@ -63,6 +63,9 @@ void registerDiskObjectStorage(DiskFactory & factory, bool global_skip_access_ch
             disk = std::make_shared<ReadOnlyDiskWrapper>(disk);
         }
 
+        if (disk->isReadOnly())
+            skip_access_check = true;
+
         disk->startup(skip_access_check);
         return disk;
     };
