e44d8b2b576d9726b4fb9976b60f93222bc9a71d
Grant Holly <grant.holly@clickhouse.com>
2025-11-10 09:34:27 -0800

    removing profile events and metrics from metadata cache cpp file

--- src/Processors/Formats/Impl/ParquetMetadataCache.cpp
+++ src/Processors/Formats/Impl/ParquetMetadataCache.cpp
@@ -1,22 +1,9 @@
 #include <Processors/Formats/Impl/ParquetMetadataCache.h>
 
 #if USE_PARQUET
-
-namespace ProfileEvents
-{
-    extern const Event ParquetMetadataCacheMisses;
-    extern const Event ParquetMetadataCacheHits;
-    extern const Event ParquetMetadataCacheWeightLost;
-}
-
-namespace CurrentMetrics
-{
-    extern const Metric ParquetMetadataCacheBytes;
-    extern const Metric ParquetMetadataCacheFiles;
-}
-
 namespace DB
 {
+    
 std::pair<String, String> extractFilePathAndETagFromReadBuffer(ReadBuffer & in)
 {
     String full_path = getFileNameFromReadBuffer(in);
@@ -39,5 +26,4 @@ std::pair<String, String> extractFilePathAndETagFromReadBuffer(ReadBuffer & in)
 }
 
 }
-
 #endif
