4347f1a9013bfae0787e5e1259be061f05c3690a
Michael Stetsyuk <michael.stetsyuk@clickhouse.com>
2025-11-10 13:56:56 +0000

    fix stateless worker

--- src/Server/StatelessWorker/StatelessTaskExecutor.cpp
+++ src/Server/StatelessWorker/StatelessTaskExecutor.cpp
@@ -76,7 +76,7 @@ StatelessTaskExecutor::Result StatelessTaskExecutor::startTask(const String & un
         Stopwatch start_watch(CLOCK_MONOTONIC);
         ASTSelectQuery ast_stub; /// FIXME: this is only used to populate query_kind
         auto query_plan_hash = sipHash64(task_description.serialized_query_plan);
-        auto process_list_entry = query_context->getProcessList().insert(task_description.task.task_id, query_plan_hash, &ast_stub, query_context, start_watch.getStart());
+        auto process_list_entry = query_context->getProcessList().insert(task_description.task.task_id, query_plan_hash, &ast_stub, query_context, start_watch.getStart(), false);
         query_context->setProcessListElement(process_list_entry->getQueryStatus());
 
         try
