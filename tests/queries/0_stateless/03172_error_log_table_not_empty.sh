d104d5320c0289748ecb24c73b8675e150cadd97
Narasimha Pakeer <narasimha.pakeer@clickhouse.com>
2025-11-11 13:31:36 -0600

    Adding tests to check new fields in Error Log table

--- tests/queries/0_stateless/03172_error_log_table_not_empty.sh
+++ tests/queries/0_stateless/03172_error_log_table_not_empty.sh
@@ -44,3 +44,10 @@ SELECT sum(value) > $(($errors_111+1)) FROM system.error_log WHERE code = 111;
 SELECT sum(value) > $(($errors_222+1)) FROM system.error_log WHERE code = 222;
 SELECT sum(value) > $(($errors_333+1)) FROM system.error_log WHERE code = 333;
 "
+#Test to check if new columns are populated
+$CLICKHOUSE_CLIENT -m -q "
+SELECT not isNull(last_error_time) FROM system.error_log WHERE code = 333 LIMIT 1;
+SELECT not empty(last_error_trace) FROM system.error_log WHERE code = 333 LIMIT 1;
+SELECT query_id != '' FROM system.error_log WHERE code = 333 LIMIT 1;
+SELECT last_error_message != '' FROM system.error_log WHERE code = 333 LIMIT 1;
+"
