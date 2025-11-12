ed8a4f39c18750f5447bc900ba7807eb4680721d
Mikhail Artemenko <mikhail.artemenko@clickhouse.com>
2025-11-12 16:06:49 +0000

    use drop metadata cache query in 03001_matview_columns_after_modify_query

--- tests/queries/0_stateless/03001_matview_columns_after_modify_query.sh
+++ tests/queries/0_stateless/03001_matview_columns_after_modify_query.sh
@@ -1,9 +1,7 @@
 #!/usr/bin/env bash
-# Tags: no-replicated-database, no-shared-catalog, no-asan, no-flaky-check
+# Tags: no-replicated-database, no-shared-catalog
 # Tag no-replicated-database -- modify on-disk metadata that may lead to "Digest does not match" in case Replicated database
 # Tag no-shared-catalog -- same
-# Tag no-asan -- plain rewritable disk can not handle out or server disk change. remote db disk will be enabled only for asan builds.
-# Tag no-flaky-check -- flaky check always runs under asan
 
 CURDIR=$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)
 # shellcheck source=../shell_config.sh
@@ -68,7 +66,20 @@ data_path=$(${CLICKHOUSE_CLIENT} -q "SELECT path FROM system.disks WHERE name = 
 
 data_path=$(${CLICKHOUSE_CLIENT} -q "SELECT path FROM system.disks WHERE name = 'default'")
 
-sed -i -e 's/`timestamp` DateTime,/`timestamp` DateTime64(9),/g' -e 's/`c12` Nullable(String)/`c12` String/g' "$data_path$mv_metadata_path"
+if [ -e "$data_path$mv_metadata_path" ]; then
+    #cat $mv_metadata_path
+    sed -i -e 's/`timestamp` DateTime,/`timestamp` DateTime64(9),/g' -e 's/`c12` Nullable(String)/`c12` String/g' "$data_path$mv_metadata_path"
+    #cat $mv_metadata_path
+else
+    # Using a remote DB disk
+    config="${CURDIR}/03001_matview_columns_after_modify_query.xml"
+    mv_metadata=$(clickhouse-disks -C "$config" --disk "disk_db_remote" --save-logs --query "read $mv_metadata_path")
+    mv_metadata_updated=$(echo $mv_metadata | sed -e 's/`timestamp` DateTime,/`timestamp` DateTime64(9),/g' -e 's/`c12` Nullable(String)/`c12` String/g')
+    echo $mv_metadata_updated | clickhouse-disks -C "$config" --disk "disk_db_remote" --save-logs --query "write --path-to $mv_metadata_path"
+
+    # We need to reload disk_db_remote if it is a plain-rewritable disk to be able to see the changes
+    ${CLICKHOUSE_CLIENT} -q "SYSTEM DROP DISK METADATA CACHE 'disk_db_remote'"
+fi
 
 ${CLICKHOUSE_CLIENT} -q "ATTACH TABLE mv"
 
