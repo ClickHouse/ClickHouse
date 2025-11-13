d417d20a8443774c43e2cb336e7d12e02c08bd52
Pablo Marcos <pablo.marcos.oltra@clickhouse.com>
2025-11-13 11:13:16 +0000

    Use hashed index for function_id

--- src/Interpreters/InstrumentationManager.h
+++ src/Interpreters/InstrumentationManager.h
@@ -119,7 +119,7 @@ private:
         InstrumentedPointInfo,
         boost::multi_index::indexed_by<
             boost::multi_index::ordered_unique<boost::multi_index::tag<Id>, boost::multi_index::member<InstrumentedPointInfo, UInt64, &InstrumentedPointInfo::id>>,
-            boost::multi_index::ordered_non_unique<boost::multi_index::tag<FunctionId>, boost::multi_index::member<InstrumentedPointInfo, Int32, &InstrumentedPointInfo::function_id>>
+            boost::multi_index::hashed_non_unique<boost::multi_index::tag<FunctionId>, boost::multi_index::member<InstrumentedPointInfo, Int32, &InstrumentedPointInfo::function_id>>
         >>;
 
     InstrumentationManager();
