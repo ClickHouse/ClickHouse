10c5c97a4b0ec0665e1631fab94197cbef4ab263
Pablo Marcos <pablo.marcos.oltra@clickhouse.com>
2025-11-13 11:13:47 +0000

    Make the sleep test faster

--- tests/queries/0_stateless/03642_system_instrument_sleep.sh
+++ tests/queries/0_stateless/03642_system_instrument_sleep.sh
@@ -33,12 +33,14 @@ $CLICKHOUSE_CLIENT -q """
 
 query_id="${CLICKHOUSE_DATABASE}_sleep_random"
 
-for _ in $(seq 1 10); do
-    $CLICKHOUSE_CLIENT --query-id="$query_id" -q "SELECT 1 FORMAT Null;"
+for i in $(seq 1 10); do
+    $CLICKHOUSE_CLIENT --query-id="${query_id}_{$i}" -q "SELECT 1 FORMAT Null;" &
 done
+
+wait
 
 $CLICKHOUSE_CLIENT -q """
     SYSTEM INSTRUMENT REMOVE ALL;
     SYSTEM FLUSH LOGS system.query_log;
-    SELECT uniq(query_duration_ms/10) > 5 FROM system.query_log WHERE current_database = currentDatabase() AND event_date >= yesterday() AND type > 1 AND query_id = '$query_id';
+    SELECT uniq(query_duration_ms/10) > 5 FROM system.query_log WHERE current_database = currentDatabase() AND event_date >= yesterday() AND type > 1 AND query_id ILIKE '${query_id}_%';
 """
