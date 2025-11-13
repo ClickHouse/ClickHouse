05e0afc278bf8f6973b753472fcb7d7d5c542cca
Nihal Z. Miaji <81457724+nihalzp@users.noreply.github.com>
2025-11-13 11:14:43 +0800

    Remove incompatible test
    Removed unused table creation and updates for t1.

--- tests/queries/0_stateless/03715_empty_tuple_functions_conversion.sql
+++ tests/queries/0_stateless/03715_empty_tuple_functions_conversion.sql
@@ -10,10 +10,3 @@ DROP TABLE IF EXISTS t0;
 CREATE TABLE t0 (c0 SimpleAggregateFunction(min, Tuple())) ENGINE = MergeTree() ORDER BY tuple();
 INSERT INTO t0 (c0) VALUES (tuple());
 SELECT * FROM t0;
-
-DROP TABLE IF EXISTS t1;
-CREATE TABLE t1 (c0 SimpleAggregateFunction(min, Tuple())) ENGINE = MergeTree() ORDER BY tuple() SETTINGS enable_block_number_column = 1, enable_block_offset_column = 1;
-UPDATE t1 SET c0 = () WHERE TRUE;
-INSERT INTO t1 (c0) VALUES (tuple()), (tuple()), (tuple());
-UPDATE t1 SET c0 = () WHERE TRUE;
-SELECT * FROM t1;
