e54baa858cc36d1a406b3eada7d27cb116d4563e
Nihal Z. Miaji <81457724+nihalzp@users.noreply.github.com>
2025-11-14 16:45:41 +0000

    Make test result hardware independent

--- tests/queries/0_stateless/03719_ntile_no_partition_by_check.sql
+++ tests/queries/0_stateless/03719_ntile_no_partition_by_check.sql
@@ -1,7 +1,7 @@
 SELECT 'With Partition By';
 
 SELECT
-    exp(number) AS x,
+    round(exp(number), 3) AS x,
     percent_rank(x) OVER (ORDER BY number ASC) AS rank,
     ntile(10) OVER (PARTITION BY 1 ORDER BY number ASC) AS bucket
 FROM numbers(11);
@@ -9,7 +9,7 @@ FROM numbers(11);
 SELECT 'No Partition By';
 
 SELECT
-    exp(number) AS x,
+    round(exp(number), 3) AS x,
     percent_rank(x) OVER (ORDER BY number ASC) AS rank,
     ntile(10) OVER (ORDER BY number ASC) AS bucket
 FROM numbers(11);
