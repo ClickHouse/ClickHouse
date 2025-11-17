070553173308cdc90607df7ebc5fbf2783837741
William Schoeffel <william.schoeffel@filterbuy.com>
2025-11-16 22:57:10 -0300

    add test for non hive partition

--- tests/queries/0_stateless/03629_truncate_hive_partitioning.sql
+++ tests/queries/0_stateless/03629_truncate_hive_partitioning.sql
@@ -1,5 +1,6 @@
--- Tests truncate hive partitioning
+-- Tests truncate for both hive partitioned and non-partitioned tables
 
+-- Test 1: Hive partitioned table
 DROP TABLE IF EXISTS test_truncate_hive;
 
 CREATE TABLE test_truncate_hive (year UInt16, country String, counter UInt8)
@@ -15,3 +16,19 @@ TRUNCATE TABLE test_truncate_hive;
 SELECT count() FROM test_truncate_hive;
 
 DROP TABLE test_truncate_hive;
+
+-- Test 2: Non-partitioned table
+DROP TABLE IF EXISTS test_truncate_regular;
+
+CREATE TABLE test_truncate_regular (year UInt16, country String, counter UInt8)
+ENGINE = S3(s3_conn, filename = 'test_truncate_regular', format = 'Parquet');
+
+INSERT INTO test_truncate_regular VALUES (2022, 'USA', 1), (2023, 'Canada', 2), (2024, 'Mexico', 3);
+
+SELECT count() FROM test_truncate_regular;
+
+TRUNCATE TABLE test_truncate_regular;
+
+SELECT count() FROM test_truncate_regular;
+
+DROP TABLE test_truncate_regular;
