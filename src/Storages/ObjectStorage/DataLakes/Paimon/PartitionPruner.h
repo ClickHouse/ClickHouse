c0f546b80fd178e47b6d9438e1bea5e4a7bed76a
“JiaQi <2963103258@qq.com>
2025-11-18 10:21:07 +0800

    add partition prun for paimon table function

---
+++ src/Storages/ObjectStorage/DataLakes/Paimon/PartitionPruner.h
@@ -0,0 +1,24 @@
+#pragma once
+#include <Storages/ObjectStorage/DataLakes/Paimon/PaimonTableSchema.h>
+#include <Interpreters/Context_fwd.h>
+#include <Storages/ObjectStorage/IObjectIterator.h>
+#include <Storages/MergeTree/KeyCondition.h>
+#include <Storages/KeyDescription.h>
+#include "Storages/ObjectStorage/DataLakes/Paimon/PaimonClient.h"
+
+
+namespace Paimon 
+{
+    class PartitionPruner
+    {
+    public:
+        PartitionPruner(const PaimonTableSchema & table_schema,
+                        const DB::ActionsDAG & filter_dag,
+                        DB::ContextPtr context);
+        bool canBePruned(const PaimonManifestEntry & manifest_entry);
+    private:
+        const PaimonTableSchema & table_schema;
+        std::optional<DB::KeyCondition> key_condition;
+        DB::KeyDescription partition_key;
+    };
+}
