129d091538888ea1e314a25265acababc67f659c
Grant Holly <grant.holly@clickhouse.com>
2025-11-17 21:08:43 -0800

    fixing unreachable code issue

--- src/Interpreters/InterpreterSystemQuery.cpp
+++ src/Interpreters/InterpreterSystemQuery.cpp
@@ -413,8 +413,8 @@ BlockIO InterpreterSystemQuery::execute()
 #else
             throw Exception(ErrorCodes::SUPPORT_IS_DISABLED, "The server was compiled without the support for AVRO");
 #endif
-# if USE_PARQUET
         case Type::DROP_PARQUET_METADATA_CACHE:
+# if USE_PARQUET
             getContext()->checkAccess(AccessType::SYSTEM_DROP_PARQUET_METADATA_CACHE);
             system_context->clearParquetMetadataCache();
             system_context->clearParquetV3MetadataCache();
