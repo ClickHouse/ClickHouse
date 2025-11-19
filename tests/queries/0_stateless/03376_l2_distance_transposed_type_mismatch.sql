74477cf6069a1ba77421ec3b1a078f7d657fbf31
rienath <raufs.dunamalijevs@clickhouse.com>
2025-11-19 18:25:12 +0000

    Cast reference vector to QBit element type s.t. we later know what type QBit had

---
+++ tests/queries/0_stateless/03376_l2_distance_transposed_type_mismatch.sql
@@ -0,0 +1,22 @@
+-- https://github.com/ClickHouse/ClickHouse/issues/89976
+-- Check whether the distance is calculated correctly when query_vec has a different type than qbit
+
+SET allow_experimental_qbit_type = 1;
+
+DROP TABLE IF EXISTS test;
+
+CREATE TABLE test 
+(
+    id UInt32, 
+    qbit QBit(Float32, 16)
+)
+ENGINE = MergeTree
+ORDER BY id;
+
+INSERT INTO test VALUES (1, [-0.042587746,0.029204812,-0.018542241,-0.0006326993,-0.046840265,0.017869968,-0.036177695,0.008778641,-0.0062302556,0.030549359,-0.009787052,-0.01996496,-0.0034493103,-0.01415683,-0.04583967,-0.047684517]);
+
+WITH [-0.042587746, 0.029204812, -0.018542241, -0.0006326993, -0.046840265, 0.017869968, -0.036177695, 0.008778641, -0.0062302556, 0.030549359, -0.009787052, -0.01996496, -0.0034493103, -0.01415683, -0.04583967, -0.047684517] AS query_vec
+SELECT id, L2DistanceTransposed(qbit, query_vec, 32) AS distance
+FROM test SETTINGS enable_analyzer=1;
+
+DROP TABLE test;
