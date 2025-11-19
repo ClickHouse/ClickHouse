6b601d0e07eae4ac3b35f06db4c3affa6dc6c3ed
Pablo Marcos <pablo.marcos.oltra@clickhouse.com>
2025-11-19 11:56:30 +0000

    Enable XRay by default except for sanitized builds

--- cmake/xray_instrumentation.cmake
+++ cmake/xray_instrumentation.cmake
@@ -1,14 +1,14 @@
 # https://llvm.org/docs/XRay.html
 
-option (ENABLE_XRAY "Enable LLVM XRay" OFF)
+option (ENABLE_XRAY "Enable LLVM XRay" ON)
 
 if (NOT ENABLE_XRAY)
     message (STATUS "Not using LLVM XRay")
     return()
 endif()
 
-if (NOT ((ARCH_AMD64 OR ARCH_AARCH64) AND OS_LINUX))
-    message (STATUS "Not using LLVM XRay, only Linux on amd64 and aarch64 is supported")
+if (NOT ((ARCH_AMD64 OR ARCH_AARCH64) AND OS_LINUX) OR SANITIZE)
+    message (STATUS "Not using LLVM XRay, only Linux on amd64 and aarch64 is supported without sanitizers")
     return()
 endif()
 
