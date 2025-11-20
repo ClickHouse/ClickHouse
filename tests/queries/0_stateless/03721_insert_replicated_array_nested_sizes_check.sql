69c118cb7dd9eb4997a5c058613cdc13270861e4
Pavel Kruglov <48961922+Avogar@users.noreply.github.com>
2025-11-20 19:28:34 +0100

    Enable analyzer explicitly in new test

--- tests/queries/0_stateless/03721_insert_replicated_array_nested_sizes_check.sql
+++ tests/queries/0_stateless/03721_insert_replicated_array_nested_sizes_check.sql
@@ -1,3 +1,5 @@
+set enable_analyzer=1;
+
 drop table if exists test;
 create table test (a Array(UInt64)) engine=MergeTree order by tuple();
 insert into test select range(number) from numbers(3) array join range(number + 1);
