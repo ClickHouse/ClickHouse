90a6e4b4429b6e3d12820268e0d7bb4b7a931f47
avogar <avogar@clickhouse.com>
2025-11-20 18:12:06 +0000

    Fix the same bug in variant element null map

--- tests/queries/0_stateless/03725_json_dynamic_subcolumn_prewhere_zero_index_granularity_bytes.sql
+++ tests/queries/0_stateless/03725_json_dynamic_subcolumn_prewhere_zero_index_granularity_bytes.sql
@@ -6,7 +6,7 @@ CREATE TABLE test (
 )
 ENGINE = MergeTree
 ORDER BY (s)
-SETTINGS index_granularity = 2, index_granularity_bytes = 0;
+SETTINGS index_granularity = 2, index_granularity_bytes = 0, min_rows_for_wide_part=0, min_bytes_for_wide_part=0;
 
 INSERT INTO test SELECT 'a', '{}' FROM numbers(1);
 
