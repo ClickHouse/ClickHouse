88bb5a536b6cd7e62b2fc0e7db0e74cbd67d213b
Pablo Marcos <pablo.marcos.oltra@clickhouse.com>
2025-11-20 10:59:02 +0000

    Fix XRay cmake config after enabling it by default

--- cmake/xray_instrumentation.cmake
+++ cmake/xray_instrumentation.cmake
@@ -1,21 +1,13 @@
 # https://llvm.org/docs/XRay.html
 
-option (ENABLE_XRAY "Enable LLVM XRay" ON)
+if (TARGET ch_contrib::llvm)
+    # The target clang must support xray, otherwise it should error on invalid option
+    set (XRAY_FLAGS "-fxray-instrument")
+    set (USE_XRAY 1)
+    set (CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} ${XRAY_FLAGS}")
+    set (CMAKE_C_FLAGS "${CMAKE_C_FLAGS} ${XRAY_FLAGS}")
 
-if (NOT ENABLE_XRAY)
-    message (STATUS "Not using LLVM XRay")
-    return()
+    message (STATUS "Using LLVM XRay")
+else()
+    message (STATUS "Not using LLVM XRay because LLVM is not built along")
 endif()
-
-if (NOT ((ARCH_AMD64 OR ARCH_AARCH64) AND OS_LINUX) OR SANITIZE)
-    message (STATUS "Not using LLVM XRay, only Linux on amd64 and aarch64 is supported without sanitizers")
-    return()
-endif()
-
-# The target clang must support xray, otherwise it should error on invalid option
-set (XRAY_FLAGS "-fxray-instrument")
-set (USE_XRAY 1)
-set (CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} ${XRAY_FLAGS}")
-set (CMAKE_C_FLAGS "${CMAKE_C_FLAGS} ${XRAY_FLAGS}")
-
-message (STATUS "Using LLVM XRay")
