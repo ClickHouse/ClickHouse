d51b883a591dff5771ebb9d1d3708b43e55a5ba8
nauu <zhukai@apache.org>
2025-11-21 20:17:45 +0800

    add comments

--- src/Storages/IStorage.h
+++ src/Storages/IStorage.h
@@ -185,6 +185,7 @@ public:
     using ColumnSizeByName = std::unordered_map<std::string, ColumnSize>;
     virtual ColumnSizeByName getColumnSizes() const { return {}; }
 
+    /// Same as getColumnSizes() but may return nullopt in some specific engines like Merge/Alias
     virtual std::optional<ColumnSizeByName> tryGetColumnSizes() const { return getColumnSizes(); }
 
     /// Optional size information of each secondary index.
@@ -205,6 +206,7 @@ public:
         return metadata.get();
     }
 
+    /// Same as getInMemoryMetadataPtr() but may return nullopt in some specific engines like Alias
     virtual std::optional<StorageMetadataPtr> tryGetInMemoryMetadataPtr() const { return getInMemoryMetadataPtr(); }
 
     /// Update storage metadata. Used in ALTER or initialization of Storage.
@@ -292,6 +294,7 @@ public:
     /// Returns hints for serialization of columns accorsing to statistics accumulated by storage.
     virtual SerializationInfoByName getSerializationHints() const { return SerializationInfoByName{{}}; }
 
+    /// Same as getSerializationHints() but may return nullopt in some specific engines like Alias
     virtual std::optional<SerializationInfoByName> tryGetSerializationHints() const { return getSerializationHints(); }
 
     /// Add engine args that were inferred during storage creation to create query to avoid the same
@@ -679,11 +682,13 @@ public:
     /// Returns data paths if storage supports it, empty vector otherwise.
     virtual Strings getDataPaths() const { return {}; }
 
+    /// Same as getDataPaths() but may return nullopt in some specific engines like Alias
     virtual std::optional<Strings> tryGetDataPaths() const { return getDataPaths(); }
 
     /// Returns storage policy if storage supports it.
     virtual StoragePolicyPtr getStoragePolicy() const { return {}; }
 
+    /// Same as getStoragePolicy() but may return nullopt in some specific engines like Alias
     virtual std::optional<StoragePolicyPtr> tryGetStoragePolicy() const { return getStoragePolicy(); }
 
     /// Returns true if all disks of storage are read-only or write-once.
@@ -730,6 +735,7 @@ public:
     /// Does not take the underlying Storage (if any) into account.
     virtual std::optional<UInt64> lifetimeRows() const { return {}; }
 
+    /// Same as lifetimeRows() but may return nullopt in some specific engines like Alias
     virtual std::optional<std::optional<UInt64>> tryLifetimeRows() const { return lifetimeRows(); }
 
     /// Number of bytes INSERTed since server start.
@@ -737,6 +743,7 @@ public:
     /// Does not take the underlying Storage (if any) into account.
     virtual std::optional<UInt64> lifetimeBytes() const { return {}; }
 
+    /// Same as lifetimeBytes() but may return nullopt in some specific engines like Alias
     virtual std::optional<std::optional<UInt64>> tryLifetimeBytes() const { return lifetimeBytes(); }
 
     /// Creates a storage snapshot from given metadata.
