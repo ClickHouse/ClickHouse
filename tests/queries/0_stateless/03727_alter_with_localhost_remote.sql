3b3443ada111ad9fe99ee3a7941a355ad9796068
pufit <pufit@clickhouse.com>
2025-11-24 13:59:49 -0500

    Fix access validation for ALTER UPDATE with `remote` table function

---
+++ tests/queries/0_stateless/03727_alter_with_localhost_remote.sql
@@ -0,0 +1,27 @@
+-- Tags: no-replicated-database, no-parallel
+
+CREATE USER test_03727;
+
+CREATE TABLE normal
+(
+    n Int32,
+    s String
+)
+ENGINE = MergeTree()
+ORDER BY n;
+
+CREATE TABLE secret
+(
+    s String
+)
+ENGINE = MergeTree()
+ORDER BY s;
+
+INSERT INTO normal VALUES (1, '');
+INSERT INTO secret VALUES ('secret');
+
+GRANT ALTER UPDATE ON normal TO test_03727;
+GRANT READ ON REMOTE to test_03727;
+GRANT CREATE TEMPORARY TABLE ON *.* TO test_03727;
+
+EXECUTE AS test_03727 ALTER TABLE normal UPDATE s = (SELECT * FROM remote('localhost', 'default', 'secret') LIMIT 1) WHERE n=1; -- { serverError ACCESS_ENTITY_ALREADY_EXISTS }
