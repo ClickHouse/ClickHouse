2b33a2995d0d9b8e765d63e771528c7e592c9c8f
Bharat Nallan Chakravarthy <bharat@clickhouse.com>
2025-11-26 19:40:22 +0530

    support geometry for flipCoordinates func

--- src/Functions/flipCoordinates.cpp
+++ src/Functions/flipCoordinates.cpp
@@ -1,13 +1,20 @@
 #include <Columns/ColumnArray.h>
 #include <Columns/ColumnConst.h>
 #include <Columns/ColumnTuple.h>
+#include <Columns/ColumnVariant.h>
 #include <Columns/IColumn.h>
 #include <Columns/ColumnsNumber.h>
 #include <DataTypes/DataTypeArray.h>
 #include <DataTypes/DataTypeTuple.h>
+#include <DataTypes/DataTypeVariant.h>
+#include <DataTypes/DataTypeFactory.h>
 #include <Functions/FunctionFactory.h>
 #include <Functions/FunctionHelpers.h>
 #include <Functions/IFunction.h>
+#include <Functions/geometry.h>
+#include <Functions/geometryConverters.h>
+
+#include <magic_enum.hpp>
 
 namespace DB
 {
@@ -43,7 +50,7 @@ public:
         return arguments[0];
     }
 
-    ColumnPtr executeImpl(const ColumnsWithTypeAndName & arguments, const DataTypePtr &, size_t) const override
+    ColumnPtr executeImpl(const ColumnsWithTypeAndName & arguments, const DataTypePtr &, size_t /*input_rows_count*/) const override
     {
         const ColumnWithTypeAndName & arg = arguments[0];
 
@@ -60,7 +67,12 @@ public:
 
         ColumnPtr result;
 
-        if (checkAndGetDataType<DataTypeTuple>(arg.type.get()))
+        // Check if input is Geometry variant type
+        if (const auto * variant_type = checkAndGetDataType<DataTypeVariant>(arg.type.get()))
+        {
+            result = executeForGeometry(column, variant_type);
+        }
+        else if (checkAndGetDataType<DataTypeTuple>(arg.type.get()))
         {
             result = executeForPoint(column);
         }
@@ -72,7 +84,7 @@ public:
         {
             throw Exception(
                 ErrorCodes::ILLEGAL_TYPE_OF_ARGUMENT,
-                "Illegal type {} of argument of function {}. Expected Point (Tuple), Ring (Array(Point)), Polygon (Array(Ring)), or MultiPolygon (Array(Polygon))",
+                "Illegal type {} of argument of function {}. Expected Point (Tuple), Ring (Array(Point)), Polygon (Array(Ring)), MultiPolygon (Array(Polygon)), or Geometry (Variant)",
                 arg.type->getName(),
                 getName());
         }
@@ -84,6 +96,146 @@ public:
     }
 
 private:
+    static CartesianPoint flipPoint(const CartesianPoint & point)
+    {
+        return CartesianPoint(point.get<1>(), point.get<0>());
+    }
+
+    static CartesianLineString flipLineString(const CartesianLineString & linestring)
+    {
+        CartesianLineString result;
+        result.reserve(linestring.size());
+        for (const auto & point : linestring)
+            result.push_back(flipPoint(point));
+        return result;
+    }
+
+    static CartesianRing flipRing(const CartesianRing & ring)
+    {
+        CartesianRing result;
+        result.reserve(ring.size());
+        for (const auto & point : ring)
+            result.push_back(flipPoint(point));
+        return result;
+    }
+
+    static CartesianPolygon flipPolygon(const CartesianPolygon & polygon)
+    {
+        CartesianPolygon result;
+        result.outer() = flipRing(polygon.outer());
+        result.inners().reserve(polygon.inners().size());
+        for (const auto & inner : polygon.inners())
+            result.inners().push_back(flipRing(inner));
+        return result;
+    }
+
+    static CartesianMultiLineString flipMultiLineString(const CartesianMultiLineString & multilinestring)
+    {
+        CartesianMultiLineString result;
+        result.reserve(multilinestring.size());
+        for (const auto & linestring : multilinestring)
+            result.push_back(flipLineString(linestring));
+        return result;
+    }
+
+    static CartesianMultiPolygon flipMultiPolygon(const CartesianMultiPolygon & multipolygon)
+    {
+        CartesianMultiPolygon result;
+        result.reserve(multipolygon.size());
+        for (const auto & polygon : multipolygon)
+            result.push_back(flipPolygon(polygon));
+        return result;
+    }
+
+    ColumnPtr executeForGeometry(const ColumnPtr & column, const DataTypeVariant * /*variant_type*/) const
+    {
+        const auto & column_variant = assert_cast<const ColumnVariant &>(*column);
+        const size_t rows = column_variant.size();
+
+        // Create serializers matching GeometryColumnType enum:
+        // Linestring=0, MultiLinestring=1, MultiPolygon=2, Point=3, Polygon=4, Ring=5
+        PointSerializer<CartesianPoint> point_serializer;
+        LineStringSerializer<CartesianPoint> linestring_serializer;
+        PolygonSerializer<CartesianPoint> polygon_serializer;
+        MultiLineStringSerializer<CartesianPoint> multilinestring_serializer;
+        MultiPolygonSerializer<CartesianPoint> multipolygon_serializer;
+        RingSerializer<CartesianPoint> ring_serializer;
+
+        auto discriminators_column = ColumnVariant::ColumnDiscriminators::create();
+        discriminators_column->reserve(rows);
+
+        Field field;
+        const auto & descriptors = column_variant.getLocalDiscriminators();
+
+        // Process each row using the discriminator to determine geometry type
+        for (size_t i = 0; i < rows; ++i)
+        {
+            const auto discr = descriptors[i];
+            discriminators_column->insertValue(discr);
+
+            column_variant.get(i, field);
+            auto type = magic_enum::enum_cast<GeometryColumnType>(discr);
+            if (!type)
+                throw Exception(
+                    ErrorCodes::ILLEGAL_TYPE_OF_ARGUMENT, "Unknown geometry type with discriminator {}", static_cast<Int32>(discr));
+
+            // Process based on GeometryColumnType enum
+            switch (*type)
+            {
+                case GeometryColumnType::Point: {
+                    auto point = getPointFromField<CartesianPoint>(field);
+                    auto flipped = flipPoint(point);
+                    point_serializer.add(flipped);
+                    break;
+                }
+                case GeometryColumnType::Linestring: {
+                    auto linestring = getLineStringFromField<CartesianPoint>(field);
+                    auto flipped = flipLineString(linestring);
+                    linestring_serializer.add(flipped);
+                    break;
+                }
+                case GeometryColumnType::Ring: {
+                    auto ring = getRingFromField<CartesianPoint>(field);
+                    auto flipped = flipRing(ring);
+                    ring_serializer.add(flipped);
+                    break;
+                }
+                case GeometryColumnType::Polygon: {
+                    auto polygon = getPolygonFromField<CartesianPoint>(field);
+                    auto flipped = flipPolygon(polygon);
+                    polygon_serializer.add(flipped);
+                    break;
+                }
+                case GeometryColumnType::MultiLinestring: {
+                    auto multilinestring = getMultiLineStringFromField<CartesianPoint>(field);
+                    auto flipped = flipMultiLineString(multilinestring);
+                    multilinestring_serializer.add(flipped);
+                    break;
+                }
+                case GeometryColumnType::MultiPolygon: {
+                    auto multipolygon = getMultiPolygonFromField<CartesianPoint>(field);
+                    auto flipped = flipMultiPolygon(multipolygon);
+                    multipolygon_serializer.add(flipped);
+                    break;
+                }
+                case GeometryColumnType::Null: {
+                    throw Exception(ErrorCodes::ILLEGAL_TYPE_OF_ARGUMENT, "Cannot flip coordinates of NULL geometry");
+                }
+            }
+        }
+
+        // Build result columns in GeometryColumnType enum order
+        Columns result_columns;
+        result_columns.push_back(linestring_serializer.finalize()); // 0 = Linestring
+        result_columns.push_back(multilinestring_serializer.finalize()); // 1 = MultiLinestring
+        result_columns.push_back(multipolygon_serializer.finalize()); // 2 = MultiPolygon
+        result_columns.push_back(point_serializer.finalize()); // 3 = Point
+        result_columns.push_back(polygon_serializer.finalize()); // 4 = Polygon
+        result_columns.push_back(ring_serializer.finalize()); // 5 = Ring
+
+        return ColumnVariant::create(std::move(discriminators_column), result_columns);
+    }
+
     ColumnPtr executeForPoint(const ColumnPtr & column) const
     {
         const auto * column_tuple = checkAndGetColumn<ColumnTuple>(column.get());
