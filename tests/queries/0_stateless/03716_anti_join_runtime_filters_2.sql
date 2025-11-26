75c1be70ee0aafeb0c0c2b0d0ffb6f20505e3b9b
Dmitry Novik <n0vik@clickhouse.com>
2025-11-26 14:35:53 +0100

    Make test stable

--- tests/queries/0_stateless/03716_anti_join_runtime_filters_2.sql
+++ tests/queries/0_stateless/03716_anti_join_runtime_filters_2.sql
@@ -9,6 +9,7 @@ INSERT INTO customer SELECT number, 5 - (number % 2) FROM numbers(500);
 
 SET enable_parallel_replicas=0;
 SET query_plan_join_swap_table=0;
+SET join_algorithm='hash'; -- to make plan stable
 
 -- RIGHT ANTI JOIN
 SELECT REGEXP_REPLACE(explain, '_runtime_filter_\\d+', '_runtime_filter_UNIQ_ID')
