6d6d641344cce80841d52d8f71c6f9e1d47cdf0b
Pablo Marcos <pablo.marcos.oltra@clickhouse.com>
2025-11-27 15:52:24 +0000

    Disable XRay modes except the core library for function entry and exit
    This way we make sure that by default only function entry and exit
    slopes are emitted. Also, we avoid linking unnecessarily xray-basic
    and xray-fdr.

--- cmake/xray_instrumentation.cmake
+++ cmake/xray_instrumentation.cmake
@@ -1,8 +1,10 @@
 # https://llvm.org/docs/XRay.html
 
 if (TARGET ch_contrib::llvm)
-    # The target clang must support xray, otherwise it should error on invalid option
-    set (XRAY_FLAGS "-fxray-instrument")
+    # The target clang must support XRay, otherwise it should error on invalid option
+    # Explicitly disable all XRay modes to avoid linking against xray-basic and xray-fdr.
+    # Emit instrumentation only for function-entry and function-exit.
+    set (XRAY_FLAGS "-fxray-instrument -fxray-modes=none -fxray-instrumentation-bundle=function")
     set (USE_XRAY 1)
     set (CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} ${XRAY_FLAGS}")
     set (CMAKE_C_FLAGS "${CMAKE_C_FLAGS} ${XRAY_FLAGS}")
