9c159492719ecc75042dff50460377466964f01c
Robert Schulze <robert@clickhouse.com>
2025-11-27 18:36:59 +0000

    Minor test cleanups

--- tests/queries/0_stateless/03640_skip_indexes_with_or.sql
+++ tests/queries/0_stateless/03640_skip_indexes_with_or.sql
@@ -1,10 +1,17 @@
--- Tests for skip index with predicate conditions containing AND and OR.
+-- Tags: no-parallel-replicas
+-- no-parallel-replicas: funny EXPLAIN PLAN output
 
-SET parallel_replicas_local_plan = 1; -- test verifies EXPLAIN output
+-- Test that the skip indexes are utilized for AND and OR connected filter conditions
 
-DROP TABLE IF EXISTS tab1;
+-- Settings needed to achieve stable EXPLAIN PLAN output
+SET parallel_replicas_local_plan = 1;
+SET use_query_condition_cache = 0;
+SET use_skip_indexes_on_data_read = 0;
+SET use_skip_indexes = 1;
 
-CREATE TABLE tab1
+DROP TABLE IF EXISTS tab;
+
+CREATE TABLE tab
 (
     id UInt32,
     v1 UInt32,
@@ -15,73 +22,74 @@ CREATE TABLE tab1
 )
 ENGINE = MergeTree
 ORDER BY id
-SETTINGS index_granularity = 64,min_bytes_for_wide_part = 0, min_bytes_for_full_part_storage = 0, max_bytes_to_merge_at_max_space_in_pool = 1, use_const_adaptive_granularity = 1, index_granularity_bytes = 0;
+SETTINGS
+    index_granularity = 64, index_granularity_bytes = 0,
+    min_bytes_for_wide_part = 0,
+    min_bytes_for_full_part_storage = 0,
+    max_bytes_to_merge_at_max_space_in_pool = 1,
+    use_const_adaptive_granularity = 1;
 
--- Total 157 ranges
-INSERT INTO tab1 SELECT number+1, number+1, (10000 - number), (number * 5) FROM numbers(10000);
+-- 157 ranges in total
+INSERT INTO tab SELECT number + 1, number + 1, (10000 - number), (number * 5) FROM numbers(10000);
 
-SET use_skip_indexes = 1;
-SET use_skip_indexes_on_data_read = 0;
+SELECT 'Test without utilizing skip indexes for disjunctions';
 SET use_skip_indexes_on_disjuncts = 0;
-SET use_query_condition_cache = 0; -- explain plan stability
 
--- 157
-SELECT trimLeft(explain) AS explain FROM (
-    EXPLAIN indexes = 1 SELECT id FROM tab1 WHERE (v1 = 111 OR v2 = 111)
-) WHERE explain LIKE '%Granules%';
+SELECT '-- Simple OR condition'; -- surviving granules: 159
+SELECT explain AS explain FROM (
+    EXPLAIN indexes = 1 SELECT id FROM tab WHERE (v1 = 111 OR v2 = 111)
+) WHERE explain LIKE '%Granules%' OR explain LIKE '%PrimaryKey%' OR explain LIKE '%Name%';
 
--- 79
-SELECT trimLeft(explain) AS explain FROM (
-    EXPLAIN indexes = 1 SELECT id FROM tab1 WHERE (id >= 5000 AND (v1 = 111 OR v2 = 111))
-) WHERE explain LIKE '%Granules%';
+SELECT '-- Mixed AND/OR condition'; -- 79
+SELECT explain AS explain FROM (
+    EXPLAIN indexes = 1 SELECT id FROM tab WHERE (id >= 5000 AND (v1 = 111 OR v2 = 111))
+) WHERE explain LIKE '%Granules%' OR explain LIKE '%PrimaryKey%' OR explain LIKE '%Name%';
 
--- 79
-SELECT trimLeft(explain) AS explain FROM (
-    EXPLAIN indexes = 1 SELECT id FROM tab1 WHERE (_part_offset >= 5000 AND (v1 = 111 OR v2 = 111))
-) WHERE explain LIKE '%Granules%';
+SELECT '-- Mixed _part_offset / OR condition'; -- 79
+SELECT explain AS explain FROM (
+    EXPLAIN indexes = 1 SELECT id FROM tab WHERE (_part_offset >= 5000 AND (v1 = 111 OR v2 = 111))
+) WHERE explain LIKE '%Granules%' OR explain LIKE '%PrimaryKey%' OR explain LIKE '%Name%';
 
--- 157 (No skip index on v3)
-SELECT trimLeft(explain) AS explain FROM (
-    EXPLAIN indexes = 1 SELECT id FROM tab1 WHERE v1 = 111 OR v2 = 111 OR v3 = 90000
-) WHERE explain LIKE '%Granules%';
+SELECT '-- No skip index on v3'; -- 159
+SELECT explain AS explain FROM (
+    EXPLAIN indexes = 1 SELECT id FROM tab WHERE v1 = 111 OR v2 = 111 OR v3 = 90000
+) WHERE explain LIKE '%Granules%' OR explain LIKE '%PrimaryKey%' OR explain LIKE '%Name%';
 
-SELECT 'Now using the disjunct processing';
+SELECT 'Test with utilizing skip indexes for disjunctions';
 SET use_skip_indexes_on_disjuncts = 1;
 
--- Final 2
-SELECT trimLeft(explain) AS explain FROM (
-    EXPLAIN indexes = 1 SELECT id FROM tab1 WHERE (v1 = 111 OR v2 = 111)
-) WHERE explain LIKE '%Granules%';
+SELECT '-- Simple OR condition'; -- 2
+SELECT explain AS explain FROM (
+    EXPLAIN indexes = 1 SELECT id FROM tab WHERE (v1 = 111 OR v2 = 111)
+) WHERE explain LIKE '%Granules%' OR explain LIKE '%PrimaryKey%' OR explain LIKE '%Name%';
 
--- Final 1
-SELECT trimLeft(explain) AS explain FROM (
-    EXPLAIN indexes = 1 SELECT id FROM tab1 WHERE (id >= 5000 AND (v1 = 111 OR v2 = 111))
-) WHERE explain LIKE '%Granules%';
+SELECT '-- Mixed AND/OR condition'; -- 1
+SELECT explain AS explain FROM (
+    EXPLAIN indexes = 1 SELECT id FROM tab WHERE (id >= 5000 AND (v1 = 111 OR v2 = 111))
+) WHERE explain LIKE '%Granules%' OR explain LIKE '%PrimaryKey%' OR explain LIKE '%Name%';
 
--- Final 1
-SELECT trimLeft(explain) AS explain FROM (
-    EXPLAIN indexes = 1 SELECT id FROM tab1 WHERE (_part_offset >= 5000 AND (v1 = 111 OR v2 = 111))
-) WHERE explain LIKE '%Granules%';
+SELECT '-- Mixed _part_offset / OR condition'; -- 1
+SELECT explain AS explain FROM (
+    EXPLAIN indexes = 1 SELECT id FROM tab WHERE (_part_offset >= 5000 AND (v1 = 111 OR v2 = 111))
+) WHERE explain LIKE '%Granules%' OR explain LIKE '%PrimaryKey%' OR explain LIKE '%Name%';
 
--- Final 157 (because no skip index on v3)
-SELECT trimLeft(explain) AS explain FROM (
-    EXPLAIN indexes = 1 SELECT id FROM tab1 WHERE v1 = 111 OR v2 = 111 OR v3 = 90000
-) WHERE explain LIKE '%Granules%';
+SELECT '-- No skip index on v3'; -- 157
+SELECT explain AS explain FROM (
+    EXPLAIN indexes = 1 SELECT id FROM tab WHERE v1 = 111 OR v2 = 111 OR v3 = 90000
+) WHERE explain LIKE '%Granules%' OR explain LIKE '%PrimaryKey%' OR explain LIKE '%Name%';
 
--- Complex condition, 0 granules selected
-SELECT trimLeft(explain) AS explain FROM (
-    EXPLAIN indexes = 1 SELECT id FROM tab1 WHERE (v1 BETWEEN 10 AND 20 AND v2 BETWEEN 10 AND 20) OR (v1 BETWEEN 100 AND 2000 AND v2 BETWEEN 100 AND 2000) OR (v1 > 9000 AND v2 > 9000)
-) WHERE explain LIKE '%Granules%';
+SELECT '-- Complex condition'; -- 0
+SELECT explain AS explain FROM (
+    EXPLAIN indexes = 1 SELECT id FROM tab WHERE (v1 BETWEEN 10 AND 20 AND v2 BETWEEN 10 AND 20) OR (v1 BETWEEN 100 AND 2000 AND v2 BETWEEN 100 AND 2000) OR (v1 > 9000 AND v2 > 9000)
+) WHERE explain LIKE '%Granules%' OR explain LIKE '%PrimaryKey%' OR explain LIKE '%Name%';
 
 -- Test with RPN size of 23 - only 6 granules and 6x64=384 rows should be read
-SELECT count(*) FROM tab1 WHERE  (v1 = 1 AND v2 = 10000) OR (v1 = 129 AND v2 = 9872) OR (v1 = 999 OR v2 = 9002) OR (v1 = 1300 AND v2 = 8701) OR (v1 = 5000 AND v2 = 5001) OR (v1 = 9000 AND v2 = 1001) SETTINGS max_rows_to_read=384;
+SELECT count(*) FROM tab WHERE  (v1 = 1 AND v2 = 10000) OR (v1 = 129 AND v2 = 9872) OR (v1 = 999 OR v2 = 9002) OR (v1 = 1300 AND v2 = 8701) OR (v1 = 5000 AND v2 = 5001) OR (v1 = 9000 AND v2 = 1001) SETTINGS max_rows_to_read=384;
 
-DROP TABLE tab1;
+DROP TABLE tab;
 
-DROP TABLE IF EXISTS tab2;
-
--- Test with composite primary key condition
-CREATE TABLE tab2
+SELECT 'Test with composite primary key condition';
+CREATE TABLE tab
 (
     x UInt32,
     y UInt32,
@@ -92,14 +100,18 @@ CREATE TABLE tab2
     INDEX v2_index v2 TYPE minmax,
 )
 ENGINE = MergeTree
-ORDER BY (x,y)
-SETTINGS index_granularity = 100, min_bytes_for_wide_part = 0, min_bytes_for_full_part_storage = 0, max_bytes_to_merge_at_max_space_in_pool = 1, use_const_adaptive_granularity = 1, index_granularity_bytes = 0;
+ORDER BY (x, y)
+SETTINGS
+    index_granularity = 100, index_granularity_bytes = 0,
+    min_bytes_for_wide_part = 0, min_bytes_for_full_part_storage = 0,
+    max_bytes_to_merge_at_max_space_in_pool = 1,
+    use_const_adaptive_granularity = 1;
 
-INSERT INTO tab2 SELECT (number+1)/10, (number+1)%100, number+1, (10000 - number), (number * 5) FROM numbers(1000);
+INSERT INTO tab SELECT (number + 1) / 10, (number + 1) % 100, number + 1, (10000 - number), (number * 5) FROM numbers(1000);
 
--- Final 1
-SELECT trimLeft(explain) AS explain FROM (
-    EXPLAIN indexes = 1 SELECT x,y,v1,v2 FROM tab2 WHERE (x < 100 AND y < 20) AND (v1 = 111 OR v2 = 111)
-) WHERE explain LIKE '%Granules%';
+-- 1
+SELECT explain AS explain FROM (
+    EXPLAIN indexes = 1 SELECT x, y, v1, v2 FROM tab WHERE (x < 100 AND y < 20) AND (v1 = 111 OR v2 = 111)
+) WHERE explain LIKE '%Granules%' OR explain LIKE '%PrimaryKey%' OR explain LIKE '%Name%';
 
-DROP TABLE tab2;
+DROP TABLE tab;
