ad8ac11bbe18ccd07979c7b5556d1dfb129fa721
Nikita Taranov <nikita.taranov@clickhouse.com>
2025-11-27 17:37:50 +0100

    Revert "Merge pull request #85924 from bigo-sg/opt_serialized"
    This reverts commit 42be83a1b6391373463667c1cd986361eaf0ffe0, reversing
    changes made to 7787aa9c97e79026e53a372a869e701a5309e4b8.

--- src/Common/HashTable/HashTableKeyHolder.h
+++ src/Common/HashTable/HashTableKeyHolder.h
@@ -74,13 +74,10 @@ struct ArenaKeyHolder
 {
     std::string_view key;
     Arena & pool;
-    /// When key is not held by any external instance, then it is held by this unique_ptr.
-    std::unique_ptr<char[]> holder;
 
-    ArenaKeyHolder(const std::string_view key_, Arena & pool_, std::unique_ptr<char[]> holder_ = {})
+    ArenaKeyHolder(const std::string_view key_, Arena & pool_)
         : key(key_)
         , pool(pool_)
-        , holder(std::move(holder_))
     {
     }
 };
