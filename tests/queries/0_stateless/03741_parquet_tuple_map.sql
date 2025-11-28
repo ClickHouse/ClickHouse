3ddb5d712afd323632e8aa4082ca420399d9ffd7
Pedro Ferreira <pedro.silva.ferreira@clickhouse.com>
2025-11-28 12:49:25 +0000

    The issue is not yet fixed

--- tests/queries/0_stateless/03741_parquet_tuple_map.sql
+++
@@ -1,4 +0,0 @@
--- Tags: no-fasttest, no-parallel
-
-INSERT INTO TABLE FUNCTION file('f', 'Parquet', 'c0 Tuple(c1 Map(Int,Int))') SELECT c0 FROM generateRandom('c0 Tuple(c1 Map(Int,Int))', 5638103928316992985, 1, 1) LIMIT 1;
-SELECT 1 FROM file('f', 'Parquet', 'c0 Tuple(c1 Map(Int,Int))') tx GROUP BY c0 SETTINGS input_format_parquet_use_native_reader = 1;
