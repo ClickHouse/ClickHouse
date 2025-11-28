f50989924d353d9487e5e349fd9594a06f28f58d
zoomxi <419486879@qq.com>
2025-11-28 10:34:01 +0800

    update test case

--- tests/queries/0_stateless/03709_parallel_replicas_right_join_with_distributed.sql
+++ tests/queries/0_stateless/03709_parallel_replicas_right_join_with_distributed.sql
@@ -24,11 +24,13 @@ CREATE TABLE dis_table_r
 )
 ENGINE = Distributed(test_cluster_one_shard_three_replicas_localhost, currentDatabase(), 'local_table_r');
 
-SET parallel_replicas_only_with_analyzer = 0;  -- necessary for CI run with disabled analyzer
-SET serialize_query_plan = 0;
+SET parallel_replicas_only_with_analyzer=0;  -- necessary for CI run with disabled analyzer
+SET serialize_query_plan=0;
 SET enable_parallel_replicas=1, parallel_replicas_local_plan=1, max_parallel_replicas=3, parallel_replicas_for_non_replicated_merge_tree=1, cluster_for_parallel_replicas='test_cluster_one_shard_three_replicas_localhost';
+SET parallel_replicas_mark_segment_size=1, max_threads=1;
 
-INSERT INTO local_table_r SELECT number AS c FROM numbers(100);
+INSERT INTO local_table_l SELECT number AS c FROM numbers(10000);
+INSERT INTO local_table_r SELECT number AS c FROM numbers(10000);
 
 SELECT count() FROM local_table_l AS l RIGHT JOIN dis_table_r AS r ON l.c = r.c;
 
