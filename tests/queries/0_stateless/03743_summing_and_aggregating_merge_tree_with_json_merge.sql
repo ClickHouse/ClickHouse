3b7a7a58371f3c8f6fc7f8a73e84ff28af0491b4
Pavel Kruglov <48961922+Avogar@users.noreply.github.com>
2025-12-01 11:56:30 +0100

    Make test faster

--- tests/queries/0_stateless/03743_summing_and_aggregating_merge_tree_with_json_merge.sql
+++ tests/queries/0_stateless/03743_summing_and_aggregating_merge_tree_with_json_merge.sql
@@ -1,9 +1,9 @@
 set mutations_sync=1;
 
 drop table if exists test;
-create table test (id UInt64, json JSON) engine=SummingMergeTree order by id settings min_bytes_for_wide_part=1, min_rows_for_wide_part=1, index_granularity_bytes=8192, merge_max_block_size=8192;
-insert into test select number, '{}' from numbers(100000);
-alter table test update json = '{"a" : 42}' where id > 90000;
+create table test (id UInt64, json JSON) engine=SummingMergeTree order by id settings min_bytes_for_wide_part=1, min_rows_for_wide_part=1, index_granularity=32, merge_max_block_size=32;
+insert into test select number, '{}' from numbers(100);
+alter table test update json = '{"a" : 42}' where id > 90;
 optimize table test final;
 select 'Dynamic paths';
 select distinct arrayJoin(JSONDynamicPaths(json)) from test;
@@ -11,9 +11,9 @@ select 'Shared data parhs';
 select distinct arrayJoin(JSONSharedDataPaths(json)) from test;
 drop table test;
 
-create table test (id UInt64, json JSON) engine=AggregatingMergeTree order by id settings min_bytes_for_wide_part=1, min_rows_for_wide_part=1, index_granularity_bytes=8192, merge_max_block_size=8192;
-insert into test select number, '{}' from numbers(100000);
-alter table test update json = '{"a" : 42}' where id > 90000;
+create table test (id UInt64, json JSON) engine=AggregatingMergeTree order by id settings min_bytes_for_wide_part=1, min_rows_for_wide_part=1, index_granularity=32, merge_max_block_size=32;
+insert into test select number, '{}' from numbers(100);
+alter table test update json = '{"a" : 42}' where id > 90;
 optimize table test final;
 select 'Dynamic paths';
 select distinct arrayJoin(JSONDynamicPaths(json)) from test;
@@ -21,9 +21,9 @@ select 'Shared data parhs';
 select distinct arrayJoin(JSONSharedDataPaths(json)) from test;
 drop table test;
 
-create table test (id UInt64, json JSON) engine=CoalescingMergeTree order by id settings min_bytes_for_wide_part=1, min_rows_for_wide_part=1, index_granularity_bytes=8192, merge_max_block_size=8192;
-insert into test select number, '{}' from numbers(100000);
-alter table test update json = '{"a" : 42}' where id > 90000;
+create table test (id UInt64, json JSON) engine=CoalescingMergeTree order by id settings min_bytes_for_wide_part=1, min_rows_for_wide_part=1, index_granularity=32, merge_max_block_size=32;
+insert into test select number, '{}' from numbers(100);
+alter table test update json = '{"a" : 42}' where id > 90;
 optimize table test final;
 select 'Dynamic paths';
 select distinct arrayJoin(JSONDynamicPaths(json)) from test;
