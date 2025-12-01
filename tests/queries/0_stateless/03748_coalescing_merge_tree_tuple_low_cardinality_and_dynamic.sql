4733add6dceed64b800e2fc5878210ea90b0cdd3
Pavel Kruglov <48961922+Avogar@users.noreply.github.com>
2025-12-01 23:53:14 +0100

    Fix test

--- tests/queries/0_stateless/03748_coalescing_merge_tree_tuple_low_cardinality_and_dynamic.sql
+++ tests/queries/0_stateless/03748_coalescing_merge_tree_tuple_low_cardinality_and_dynamic.sql
@@ -1,3 +1,4 @@
+set mutations_sync=1;
 drop table if exists test;
 create table test (id UInt64, t Tuple(a LowCardinality(String), json JSON)) engine=CoalescingMergeTree order by id settings min_bytes_for_wide_part=1, min_rows_for_wide_part=1, index_granularity=32, merge_max_block_size=32;
 insert into test select number, tuple('str', '{}') from numbers(100);
