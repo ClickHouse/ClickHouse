009402e5d326bfce1b849853b30416e09e32045e
Grant Holly <grant.holly@clickhouse.com>
2025-12-01 13:58:21 -0800

    adding parquet test files and adding a new test

--- tests/queries/0_stateless/03707_parquet_metadata_cache.sql
+++ tests/queries/0_stateless/03707_parquet_metadata_cache.sql
@@ -1,31 +1,19 @@
 -- Test Parquet metadata cache functionality
 -- Tag: no-fasttest: Depends on AWS
 
-DROP TABLE IF EXISTS test_03707_parquet_metadata_cache;
-
--- create a table with two parquet files using partitions
--- test_03707_parquet_metadata_cache_0 and test_03707_parquet_metadata_cache_1
-CREATE TABLE test_03707_parquet_metadata_cache (
-    id UInt32, 
-    name String, 
-    value Float64
-) 
-ENGINE = S3(s3_conn, filename='test_03707_parquet_metadata_cache_{_partition_id}', format='Parquet')
-PARTITION BY id % 2;
-
-INSERT INTO test_03707_parquet_metadata_cache VALUES (1, 'test1', 1.23), (2, 'test2', 4.56);
+SYSTEM DROP PARQUET METADATA CACHE;
 
 -- Test cache with S3 table function
 -- Triggers caching of the file
-SELECT 
-    count() 
-FROM s3(s3_conn, filename='test_03707_parquet_metadata_cache_*.parquet', format='Parquet');
+SELECT
+    count()
+FROM s3(s3_conn, filename='03707_cache_test.parquet', format='Parquet');
 
 -- Test cache with wildcards
 -- Should be a cache hit
-SELECT 
+SELECT
     count()
-FROM s3(s3_conn, filename='test_03707_parquet_metadata_cache_*.parquet', format='Parquet');
+FROM s3(s3_conn, filename='03707_cache_test.parquet', format='Parquet');
 
 SELECT
     sum(ProfileEvent_ParquetMetadataCacheMisses),
@@ -36,7 +24,7 @@ FROM system.metric_log;
 -- Another cache hit
 SELECT
     count()
-FROM s3(s3_conn, filename='test_03707_parquet_metadata_cache_*.parquet', format='Parquet');
+FROM s3(s3_conn, filename='03707_cache_test.parquet', format='Parquet');
 
 SELECT
     sum(ProfileEvent_ParquetMetadataCacheMisses),
@@ -60,6 +48,6 @@ SELECT
     sum(ProfileEvent_ParquetMetadataCacheWeightLost)
 FROM system.metric_log;
 
-DROP TABLE test_03707_parquet_metadata_cache;
 -- TODO: Add tests for ParquetMetadataCacheBytes and ParquetMetadataCacheFiles
 -- TODO: Add a test the triggers cache eviction
+
---
+++ tests/queries/0_stateless/03707_parquet_metadata_cache_from_file.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env bash
+# Tags: no-fasttest
+
+CURDIR=$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)
+# shellcheck source=../shell_config.sh
+. "$CURDIR"/../shell_config.sh
+
+$CLICKHOUSE_LOCAL -q "select count() from file('$CURDIR/data_parquet/03707_cache_test.parquet', Parquet)"
