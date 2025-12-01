48096eb51f5c7220b9692d14874dc7e51f2027c9
Pavel Kruglov <48961922+Avogar@users.noreply.github.com>
2025-12-01 11:24:10 +0100

    Make test faster

--- tests/queries/0_stateless/03745_fix_dynamic_structure_in_compact_part.sql
+++ tests/queries/0_stateless/03745_fix_dynamic_structure_in_compact_part.sql
@@ -1,15 +1,6 @@
 drop table if exists test;
-create table test (id UInt64, json JSON) engine=CoalescingMergeTree order by id settings min_bytes_for_wide_part='100G', merge_max_block_size=333, index_granularity=8128;
-insert into test select number, '{}' from numbers(100000);
-alter table test update json = '{"a" : 42}' where id > 90000 settings mutations_sync=1;
+create table test (id UInt64, json JSON) engine=CoalescingMergeTree order by id settings min_bytes_for_wide_part='100G', merge_max_block_size=33, index_granularity=800;
+insert into test select number, '{}' from numbers(10000);
+alter table test update json = '{"a" : 42}' where id > 9000 settings mutations_sync=1;
 optimize table test final;
 drop table test;
-
--- Second table with no fixed merge_max_block_size and index_granularity for randomization.
-drop table if exists test;
-create table test (id UInt64, json JSON) engine=CoalescingMergeTree order by id settings min_bytes_for_wide_part='100G';
-insert into test select number, '{}' from numbers(100000);
-alter table test update json = '{"a" : 42}' where id > 90000 settings mutations_sync=1;
-optimize table test final;
-drop table test;
-
