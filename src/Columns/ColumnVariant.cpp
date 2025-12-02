655622d00254bb202f69b351e4619ad664e10735
robot-clickhouse <robot-clickhouse@users.noreply.github.com>
2025-12-02 15:14:16 +0000

    Backport #91152 to 25.9: Fix possible inconsistent dynamic structure during writing in compact parts

--- src/Columns/ColumnVariant.cpp
+++ src/Columns/ColumnVariant.cpp
@@ -1789,5 +1789,11 @@ void ColumnVariant::takeDynamicStructureFromColumn(const ColumnPtr & source_colu
         getVariantByGlobalDiscriminator(i).takeDynamicStructureFromColumn(source_variant.getVariantPtrByGlobalDiscriminator(i));
 }
 
+void ColumnVariant::fixDynamicStructure()
+{
+    for (auto & variant : variants)
+        variant->fixDynamicStructure();
+}
+
 
 }
