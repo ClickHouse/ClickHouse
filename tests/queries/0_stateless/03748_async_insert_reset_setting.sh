df7ab8fb136f2369790f573f2d2631392e858c27
Mikhail f. Shiryaev <felixoid@clickhouse.com>
2025-12-04 20:07:15 +0100

    Reimplement tests in bash to address race by uniq users

--- tests/queries/0_stateless/03748_async_insert_reset_setting.sh
+++ tests/queries/0_stateless/03748_async_insert_reset_setting.sh
@@ -1,10 +1,20 @@
-DROP TABLE IF EXISTS source_table, target_table, target_table_remote_sync, target_table_remote_async, async_insert_mv, sync_insert_mv;
-DROP USER IF EXISTS 03748_test_user_sync, 03748_test_user_async;
+#!/usr/bin/env bash
 
-CREATE USER 03748_test_user_sync HOST ANY IDENTIFIED WITH NO_PASSWORD SETTINGS async_insert = 0;
-CREATE USER 03748_test_user_async HOST ANY IDENTIFIED WITH NO_PASSWORD SETTINGS async_insert = 1;
-GRANT ALL ON target_table TO 03748_test_user_sync;
-GRANT ALL ON target_table TO 03748_test_user_async;
+CURDIR=$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)
+# shellcheck source=../shell_config.sh
+. "$CURDIR"/../shell_config.sh
+
+SYNC_USER="${CLICKHOUSE_DATABASE}_sync_user"
+ASYNC_USER="${CLICKHOUSE_DATABASE}_async_user"
+
+${CLICKHOUSE_CLIENT} --multiquery <<EOF
+DROP TABLE IF EXISTS source_table, target_table, target_table_remote_sync, target_table_remote_async, async_insert_mv, sync_insert_mv;
+DROP USER IF EXISTS ${SYNC_USER}, ${ASYNC_USER};
+
+CREATE USER ${SYNC_USER} HOST ANY IDENTIFIED WITH NO_PASSWORD SETTINGS async_insert = 0;
+CREATE USER ${ASYNC_USER} HOST ANY IDENTIFIED WITH NO_PASSWORD SETTINGS async_insert = 1;
+GRANT ALL ON target_table TO ${SYNC_USER};
+GRANT ALL ON target_table TO ${ASYNC_USER};
 
 CREATE TABLE source_table (
     id UInt64,
@@ -24,13 +34,13 @@ CREATE TABLE target_table_remote_sync (
     id UInt64,
     data String
 )
-AS remote('127.0.0.2', currentDatabase(), 'target_table', '03748_test_user_sync');
+AS remote('127.0.0.2', currentDatabase(), 'target_table', '${SYNC_USER}');
 
 CREATE TABLE target_table_remote_async (
     id UInt64,
     data String
 )
-AS remote('127.0.0.2', currentDatabase(), 'target_table', '03748_test_user_async');
+AS remote('127.0.0.2', currentDatabase(), 'target_table', '${ASYNC_USER}');
 
 CREATE MATERIALIZED VIEW async_insert_mv TO target_table_remote_async AS
 SELECT * FROM source_table;
@@ -52,10 +62,11 @@ INSERT INTO source_table (id, data) VALUES (7, 'test7'), (8, 'test8'), (9, 'test
 SYSTEM FLUSH LOGS query_log;
 SELECT count() FROM system.query_log
 WHERE query_kind = 'Insert' AND type = 'QueryFinish'
-  AND user IN ('03748_test_user_sync', '03748_test_user_async')
+  AND user IN ('${SYNC_USER}', '${ASYNC_USER}')
 --  AND current_database = currentDatabase() -- to silent strange style check warning
   AND tables = [currentDatabase() || '.target_table']
 GROUP BY Settings['async_insert']
 ORDER BY count() ASC;
 
 SELECT * FROM target_table ORDER BY id;
+EOF
