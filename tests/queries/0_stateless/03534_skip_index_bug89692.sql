2dbe5397ebf9b079beb3392917a25816aa59e1ee
fastio <pengjian.uestc@gmail.com>
2025-12-04 12:23:34 +0800

    fix tests

--- tests/queries/0_stateless/03534_skip_index_bug89692.sql
+++ tests/queries/0_stateless/03534_skip_index_bug89692.sql
@@ -1,9 +1,6 @@
 -- { echo ON }
 
 SET use_skip_indexes_on_data_read = 1;
-SET use_skip_indexes = 1;
-SET use_query_condition_cache = 0;
-SET max_rows_to_read = 0;
 
 DROP TABLE IF EXISTS tab;
 
@@ -15,7 +12,7 @@ CREATE TABLE tab
 )
 ENGINE = MergeTree
 ORDER BY i
-SETTINGS index_granularity = 4,index_granularity_bytes = 0, min_bytes_for_wide_part = 0;
+SETTINGS index_granularity = 4,index_granularity_bytes = 0;
 
 INSERT INTO tab SELECT number, toString(number) FROM numbers(6); -- The Last granule contained rows smaller than index_granularity.
 
