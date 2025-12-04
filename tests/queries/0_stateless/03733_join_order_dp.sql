c5c6217d28e7d15a94aab1b2cfe0ed14c64c566c
Alexander Gololobov <davenger@clickhouse.com>
2025-12-04 19:06:56 +0100

    Only use uniq statistics

--- tests/queries/0_stateless/03733_join_order_dp.sql
+++ tests/queries/0_stateless/03733_join_order_dp.sql
@@ -9,7 +9,7 @@ CREATE TABLE R1 (
     A_Description String
 ) ENGINE = MergeTree()
 PRIMARY KEY (A_ID)
-SETTINGS auto_statistics_types = 'tdigest,countmin,uniq,minmax';
+SETTINGS auto_statistics_types = 'uniq';
 
 -- R2: Large fact table (Demo size: 1,000)
 -- Joins only with R1.
@@ -19,7 +19,7 @@ CREATE TABLE R2 (
     B_Data Float64
 ) ENGINE = MergeTree()
 PRIMARY KEY (B_ID)
-SETTINGS auto_statistics_types = 'tdigest,countmin,uniq,minmax';
+SETTINGS auto_statistics_types = 'uniq';
 
 -- R3: Another large fact table (Demo size: 1,000)
 -- Joins with R1 and R4.
@@ -30,7 +30,7 @@ CREATE TABLE R3 (
     C_Value Int32
 ) ENGINE = MergeTree()
 PRIMARY KEY (C_ID)
-SETTINGS auto_statistics_types = 'tdigest,countmin,uniq,minmax';
+SETTINGS auto_statistics_types = 'uniq';
 
 -- R4: Small lookup table (Demo size: 10)
 -- Joins only with R3.
@@ -39,7 +39,7 @@ CREATE TABLE R4 (
     D_LookupCode String
 ) ENGINE = MergeTree()
 PRIMARY KEY (D_ID)
-SETTINGS auto_statistics_types = 'tdigest,countmin,uniq,minmax';
+SETTINGS auto_statistics_types = 'uniq';
 
 
 -- Populate R1 (Small: 10 rows)
