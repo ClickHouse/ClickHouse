6f2d6060acc20223902475646729a56c61c3d7ad
FriendLey <friendley@qq.com>
2025-12-04 19:08:02 +0800

    Update src/Functions/FunctionsNumericIndexedVector.cpp
    Co-authored-by: Raufs Dunamalijevs <raufs.dunamalijevs@clickhouse.com>

--- src/Functions/FunctionsNumericIndexedVector.cpp
+++ src/Functions/FunctionsNumericIndexedVector.cpp
@@ -346,7 +346,7 @@ Performs a pointwise maximum between two numericIndexedVector objects. For each 
         FunctionDocumentation::Syntax syntax = "numericIndexedVectorPointwiseMax(v1, v2)";
 
         FunctionDocumentation::Arguments arguments
-            = {{"v1", "", {"numericIndexedVector"}}, {"v2", "Another numericIndexedVector object.", {"numericIndexedVector"}}};
+            = {{"v1", "", {"numericIndexedVector"}}, {"v2", "", {"numericIndexedVector"}}};
 
         FunctionDocumentation::ReturnedValue returned_value = {"Returns a new numericIndexedVector object.", {"numericIndexedVector"}};
 
