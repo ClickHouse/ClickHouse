1ea4fbbcb1956e03f528660b62d5c1315b2a4271
Yarik Briukhovetskyi <114298166+yariks5s@users.noreply.github.com>
2025-12-05 22:54:14 +0000

    fix test

--- tests/queries/0_stateless/03755_concurrent_hash_join_dispatch_bug.sql
+++ tests/queries/0_stateless/03755_concurrent_hash_join_dispatch_bug.sql
@@ -1,13 +1,12 @@
 -- Test for concurrent hash join with ON FALSE or missing key columns see https://github.com/ClickHouse/ClickHouse/issues/91173
 
-DROP DATABASE IF EXISTS 03755_test;
 DROP TABLE IF EXISTS 03755_test.t0;
+DROP DATABASE IF EXISTS 03755_test;
 
 CREATE DATABASE 03755_test;
 CREATE TABLE 03755_test.t0 (c0 Int) ENGINE = Memory;
-SELECT 1 FROM remote('localhost', 03755_test.t0) AS t0
-JOIN 03755_test.t0 t1 ON FALSE
-RIGHT JOIN 03755_test.t0 t2 ON FALSE;
+SELECT 1 FROM remote('localhost', 03755_test.t0) AS t0 JOIN 03755_test.t0 t1 ON FALSE RIGHT JOIN t0 t2 ON FALSE;
 
 DROP TABLE 03755_test.t0;
 DROP DATABASE 03755_test;
+
