ba96ecde61e5946e601784ba781016e5b75f45ca
Grant Holly <grant.holly@clickhouse.com>
2025-12-04 17:08:30 -0800

    updating tests to use query_log table instead of metric_log

--- tests/queries/0_stateless/03707_parquet_metadata_cache.sql
+++ tests/queries/0_stateless/03707_parquet_metadata_cache.sql
@@ -1,53 +1,34 @@
 -- Test Parquet metadata cache functionality
 -- Tag: no-fasttest: Depends on AWS
-
+SET log_queries = 1;
 SYSTEM DROP PARQUET METADATA CACHE;
 
 -- Test cache with S3 table function
 -- Triggers caching of the file
 SELECT
-    count()
-FROM s3(s3_conn, filename='03707_cache_test.parquet', format='Parquet');
+    *
+FROM s3(s3_conn, filename='03707_cache_test.parquet', format='Parquet') 
+settings log_comment = '03707-first-test-query';
 
--- Test cache with wildcards
--- Should be a cache hit
+-- should be a cache miss as we load the cache the first time
 SELECT
-    count()
-FROM s3(s3_conn, filename='03707_cache_test.parquet', format='Parquet');
+    ProfileEvents['ParquetMetadataCacheHits'],
+    ProfileEvents['ParquetMetadataCacheMisses'],
+FROM system.query_log
+WHERE log_comment = '03707-first-test-query'
+AND type = 'QueryFinish';
 
+-- Should be a cache hit as we use the same file
 SELECT
-    sum(ProfileEvent_ParquetMetadataCacheMisses),
-    sum(ProfileEvent_ParquetMetadataCacheHits),
-    sum(ProfileEvent_ParquetMetadataCacheWeightLost)
-FROM system.metric_log;
-
--- Another cache hit
-SELECT
-    count()
-FROM s3(s3_conn, filename='03707_cache_test.parquet', format='Parquet');
+    id, upper(name), value + 1
+FROM s3(s3_conn, filename='03707_cache_test.parquet', format='Parquet')
+settings log_comment = '03707-second-test-query';
 
 SELECT
-    sum(ProfileEvent_ParquetMetadataCacheMisses),
-    sum(ProfileEvent_ParquetMetadataCacheHits),
-    sum(ProfileEvent_ParquetMetadataCacheWeightLost)
-FROM system.metric_log;
-
-/*
-SELECT
-    sum(CurrentMetric_ParquetMetadataCacheBytes),
-    sum(CurrentMetric_ParquetMetadataCacheFiles)
-FROM system.metric_log;
-*/
+    ProfileEvents['ParquetMetadataCacheHits'],
+    ProfileEvents['ParquetMetadataCacheMisses'],
+FROM system.query_log
+WHERE log_comment = '03707-second-test-query'
+AND type = 'QueryFinish';
 
 SYSTEM DROP PARQUET METADATA CACHE;
-
--- Should be all zero
-SELECT
-    sum(ProfileEvent_ParquetMetadataCacheMisses),
-    sum(ProfileEvent_ParquetMetadataCacheHits),
-    sum(ProfileEvent_ParquetMetadataCacheWeightLost)
-FROM system.metric_log;
-
--- TODO: Add tests for ParquetMetadataCacheBytes and ParquetMetadataCacheFiles
--- TODO: Add a test the triggers cache eviction
-
