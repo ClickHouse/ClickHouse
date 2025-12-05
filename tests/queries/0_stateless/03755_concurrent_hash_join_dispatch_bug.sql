b03bb62a528bc50b61dbcdc8220e925a374c5f7f
Yarik Briukhovetskyi <114298166+yariks5s@users.noreply.github.com>
2025-12-05 22:25:22 +0000

    Add original db name

--- tests/queries/0_stateless/03755_concurrent_hash_join_dispatch_bug.sql
+++ tests/queries/0_stateless/03755_concurrent_hash_join_dispatch_bug.sql
@@ -1,10 +1,13 @@
 -- Test for concurrent hash join with ON FALSE or missing key columns see https://github.com/ClickHouse/ClickHouse/issues/91173
 
-DROP TABLE IF EXISTS t0;
+DROP DATABASE IF EXISTS 03755_test;
+DROP TABLE IF EXISTS 03755_test.t0;
 
-CREATE TABLE t0 (c0 Int) ENGINE = Memory;
-SELECT 1 FROM remote('localhost', default.t0) AS t0
-JOIN t0 t1 ON FALSE
-RIGHT JOIN t0 t2 ON FALSE;
+CREATE DATABASE 03755_test;
+CREATE TABLE 03755_test.t0 (c0 Int) ENGINE = Memory;
+SELECT 1 FROM remote('localhost', 03755_test.t0) AS t0
+JOIN 03755_test.t0 t1 ON FALSE
+RIGHT JOIN 03755_test.t0 t2 ON FALSE;
 
-DROP TABLE t0;
+DROP TABLE 03755_test.t0;
+DROP DATABASE 03755_test;
