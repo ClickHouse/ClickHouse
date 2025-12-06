6357ffa4c14e93658ef7755b595a5e09da86bfbc
Grant Holly <grant.holly@clickhouse.com>
2025-12-05 16:46:22 -0800

    updating tests with flush logs

--- tests/queries/0_stateless/03707_parquet_metadata_cache.sql
+++ tests/queries/0_stateless/03707_parquet_metadata_cache.sql
@@ -2,33 +2,52 @@
 -- Tag: no-fasttest: Depends on AWS
 SET log_queries = 1;
 SYSTEM DROP PARQUET METADATA CACHE;
+DELETE FROM system.query_log WHERE log_comment like '03707%';
 
--- Test cache with S3 table function
 -- Triggers caching of the file
-SELECT
-    *
-FROM s3(s3_conn, filename='03707_cache_test.parquet', format='Parquet') 
-settings log_comment = '03707-first-test-query';
-
 -- should be a cache miss as we load the cache the first time
+SELECT *
+FROM s3(s3_conn, filename = '03707_cache_test.parquet', format = 'Parquet')
+SETTINGS log_comment = '03707-first-test-query';
+
+SYSTEM FLUSH LOGS query_log;
+
 SELECT
-    ProfileEvents['ParquetMetadataCacheHits'],
-    ProfileEvents['ParquetMetadataCacheMisses'],
+    ProfileEvents['ParquetMetadataCacheHits'] AS hits,
+    ProfileEvents['ParquetMetadataCacheMisses'] AS misses
 FROM system.query_log
-WHERE log_comment = '03707-first-test-query'
-AND type = 'QueryFinish';
+WHERE (log_comment = '03707-first-test-query') AND (type = 'QueryFinish');
 
 -- Should be a cache hit as we use the same file
 SELECT
-    id, upper(name), value + 1
-FROM s3(s3_conn, filename='03707_cache_test.parquet', format='Parquet')
-settings log_comment = '03707-second-test-query';
+    id,
+    upper(name),
+    value + 1
+FROM s3(s3_conn, filename = '03707_cache_test.parquet', format = 'Parquet')
+SETTINGS log_comment = '03707-second-test-query';
+
+SYSTEM FLUSH LOGS query_log;
 
 SELECT
-    ProfileEvents['ParquetMetadataCacheHits'],
-    ProfileEvents['ParquetMetadataCacheMisses'],
+    ProfileEvents['ParquetMetadataCacheHits'] AS hits,
+    ProfileEvents['ParquetMetadataCacheMisses'] AS misses
 FROM system.query_log
-WHERE log_comment = '03707-second-test-query'
-AND type = 'QueryFinish';
+WHERE (log_comment = '03707-second-test-query') AND (type = 'QueryFinish');
 
 SYSTEM DROP PARQUET METADATA CACHE;
+
+-- Should be back to a cache miss since we dropped the cache
+SELECT
+    name,
+    value + value
+FROM s3(s3_conn, filename = '03707_cache_test.parquet', format = 'Parquet')
+SETTINGS log_comment = '03707-third-test-query';
+
+SYSTEM FLUSH LOGS query_log;
+
+SELECT
+    ProfileEvents['ParquetMetadataCacheHits'] AS hits,
+    ProfileEvents['ParquetMetadataCacheMisses'] AS misses
+FROM system.query_log
+WHERE (log_comment = '03707-third-test-query') AND (type = 'QueryFinish');
+
