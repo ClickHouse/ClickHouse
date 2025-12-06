036193c2e979f87477f879d7cc5b8a3deaa1656a
Ilya Golshtein <igolshtein@altinity.com>
2025-12-06 19:57:48 +0300

    protobuf_oneof_several_values: test switched to clickhouse-local

--- tests/queries/0_stateless/03710_protobuf_oneof_several_values.sh
+++ tests/queries/0_stateless/03710_protobuf_oneof_several_values.sh
@@ -8,15 +8,12 @@ SCHEMADIR=$CURDIR/format_schemas
 . "$CURDIR"/../shell_config.sh
 
 
-# USE WORKING_FOLDER_03710 = ${CLICKHOUSE_TMP} to run locally
-WORKING_FOLDER_03710="${USER_FILES_PATH}/${CLICKHOUSE_DATABASE}"
+set -e
+
+[ -e "${CLICKHOUSE_TMP}"/oneof_several_values.bin ] && rm "${CLICKHOUSE_TMP}"/oneof_several_values.bin
 
 
-rm -rf "${WORKING_FOLDER_03710}"
-mkdir "${WORKING_FOLDER_03710}"
-
-
-${CLICKHOUSE_CLIENT} <<EOF
+${CLICKHOUSE_LOCAL} --logger.console=0 --query "
 CREATE TABLE string_or_string
 (
 string1 String,
@@ -24,28 +21,28 @@ string2 String
 )
 ENGINE = MergeTree
 ORDER BY tuple();
-
 insert into string_or_string values ('str1',''), ('','str2');
-
 SELECT * FROM string_or_string
-INTO OUTFILE '${WORKING_FOLDER_03710}/oneof_several_values.bin'
 SETTINGS format_schema_source='string',
-format_schema = 'syntax = "proto3";message StringOrString {oneof string_oneof {string string1 = 1;string string2 = 42;}}',
+format_schema = 'syntax = \"proto3\";message StringOrString {oneof string_oneof {string string1 = 1;string string2 = 42;}}',
 format_schema_message_name='StringOrString'
 FORMAT Protobuf;
+" > "${CLICKHOUSE_TMP}/oneof_several_values.bin"
 
-SELECT * FROM file('${WORKING_FOLDER_03710}/oneof_several_values.bin', Protobuf)
-SETTINGS format_schema_source='string',
-format_schema = 'syntax = "proto3";message StringOrString {oneof string_oneof {string string1 = 1;string string2 = 42;}}',
-format_schema_message_name='StringOrString';
-EOF
-
-${CLICKHOUSE_CLIENT} <<EOF
+${CLICKHOUSE_LOCAL} --logger.console=0 --input-format=Protobuf --structure="string1 String, string2 String" --query "
 SELECT *
-FROM file('${WORKING_FOLDER_03710}/oneof_several_values.bin', Protobuf)
+FROM file('stdin', Protobuf)
 SETTINGS format_schema_source='string',
-format_schema = 'syntax = "proto3";message StringOrString {oneof string_oneof {string string1 = 1;string string2 = 42;}}',
-format_schema_message_name='StringOrString', input_format_protobuf_oneof_presence = true; -- { serverError PROTOBUF_ONEOF_HAS_SEVERAL_VALUES }
-EOF
+format_schema = 'syntax = \"proto3\";message StringOrString {oneof string_oneof {string string1 = 1;string string2 = 42;}}',
+format_schema_message_name='StringOrString';
+" < "${CLICKHOUSE_TMP}/oneof_several_values.bin"
 
-rm -rf "${WORKING_FOLDER_03710}"
+${CLICKHOUSE_LOCAL} --logger.console=0 --input-format=Protobuf --structure="string1 String, string2 String" --query "
+SELECT *
+FROM file('stdin', Protobuf)
+SETTINGS format_schema_source='string',
+format_schema = 'syntax = \"proto3\";message StringOrString {oneof string_oneof {string string1 = 1;string string2 = 42;}}',
+format_schema_message_name='StringOrString', input_format_protobuf_oneof_presence = true; -- { serverError PROTOBUF_ONEOF_HAS_SEVERAL_VALUES }
+" < "${CLICKHOUSE_TMP}/oneof_several_values.bin"
+
+rm "${CLICKHOUSE_TMP}"/oneof_several_values.bin
