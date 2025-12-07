08f63a1b52b4f9d6f0ed57dc425d5890c6b8e6a7
Vitaly Baranov <vitlibar@clickhouse.com>
2025-12-07 21:39:02 +0100

    Support binary operators.

--- src/Storages/TimeSeries/PrometheusQueryToSQL/Converter.cpp
+++ src/Storages/TimeSeries/PrometheusQueryToSQL/Converter.cpp
@@ -2,6 +2,7 @@
 
 #include <Storages/TimeSeries/PrometheusQueryToSQL/ConverterContext.h>
 #include <Storages/TimeSeries/PrometheusQueryToSQL/SQLQueryPiece.h>
+#include <Storages/TimeSeries/PrometheusQueryToSQL/applyBinaryOperator.h>
 #include <Storages/TimeSeries/PrometheusQueryToSQL/applyFunctionOverRange.h>
 #include <Storages/TimeSeries/PrometheusQueryToSQL/applyUnaryOperator.h>
 #include <Storages/TimeSeries/PrometheusQueryToSQL/finalizeSQL.h>
@@ -92,6 +93,14 @@ namespace
                 const auto * unary_operator = static_cast<const PQT::UnaryOperator *>(node);
                 SQLQueryPiece argument = visitNode(unary_operator->getArgument(), context);
                 return applyUnaryOperator(unary_operator, std::move(argument), context);
+            }
+
+            case NodeType::BinaryOperator:
+            {
+                const auto * binary_operator = static_cast<const PQT::BinaryOperator *>(node);
+                SQLQueryPiece left_argument = visitNode(binary_operator->getLeftArgument(), context);
+                SQLQueryPiece right_argument = visitNode(binary_operator->getRightArgument(), context);
+                return applyBinaryOperator(binary_operator, std::move(left_argument), std::move(right_argument), context);
             }
 
             default:
