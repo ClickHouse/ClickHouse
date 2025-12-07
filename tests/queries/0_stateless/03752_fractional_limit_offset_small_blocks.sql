a71341361122cb77e5728eb6b483debb2db15a86
0xgouda <gouda0x@gmail.com>
2025-12-07 14:59:08 +0200

    Make `03752_fractional_limit_offset_small_blocks` test faster.

--- tests/queries/0_stateless/03752_fractional_limit_offset_small_blocks.sql
+++ tests/queries/0_stateless/03752_fractional_limit_offset_small_blocks.sql
@@ -10,6 +10,8 @@ SELECT 'Fractional Limit, Normal Offset - Early Pushing/Evicting';
 
 SELECT number FROM numbers(20) ORDER BY number LIMIT 0.5 OFFSET 5;
 SELECT '*';
+SELECT number FROM numbers(20) ORDER BY number LIMIT 0.25 OFFSET 12;
+SELECT '*';
 SELECT number FROM numbers(20) ORDER BY number LIMIT 0.5 OFFSET 10;
 SELECT '*';
 SELECT number FROM numbers(20) ORDER BY number LIMIT 0.9 OFFSET 5;
@@ -17,6 +19,8 @@ SELECT number FROM numbers(20) ORDER BY number LIMIT 0.9 OFFSET 5;
 SELECT 'Fractional Limit, Fractional Offset - Early Pushing/Evicting';
 
 SELECT number FROM numbers(20) ORDER BY number LIMIT 0.5 OFFSET 0.25;
+SELECT '*';
+SELECT number FROM numbers(20) ORDER BY number LIMIT 0.25 OFFSET 0.6;
 SELECT '*';
 SELECT number FROM numbers(20) ORDER BY number LIMIT 0.5 OFFSET 0.5;
 SELECT '*';
@@ -27,30 +31,3 @@ SELECT 'Fractional Offset Only - Early Evicting';
 SELECT number FROM numbers(20) ORDER BY number OFFSET 0.5;
 SELECT '*';
 SELECT number FROM numbers(20) ORDER BY number OFFSET 0.9;
-
-SELECT 'Big Tables';
-
-DROP TABLE IF EXISTS num_tab;
-CREATE TABLE num_tab
-ENGINE = MergeTree
-ORDER BY number
-AS SELECT number FROM numbers(1000000);
-
-SELECT 
-    number 
-FROM 
-    num_tab 
-ORDER BY number
-LIMIT 10 
-OFFSET 0.99999;
-
-SET max_block_size = 7;
-SELECT '*';
-
-SELECT
-    number
-FROM 
-    num_tab
-ORDER BY number
-LIMIT 10 
-OFFSET 0.99999;
