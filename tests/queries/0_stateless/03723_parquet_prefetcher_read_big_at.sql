0e67babb1bfe6d38311983174e0d0d00abfce34d
alesapin <alesapin@gmail.com>
2025-12-07 19:12:42 +0100

    Update tests/queries/0_stateless/03723_parquet_prefetcher_read_big_at.sql

--- tests/queries/0_stateless/03723_parquet_prefetcher_read_big_at.sql
+++ tests/queries/0_stateless/03723_parquet_prefetcher_read_big_at.sql
@@ -1,4 +1,4 @@
--- Tags: no-fasttest
+-- Tags: no-fasttest, no-random-settings
 -- Closes #90890
 -- This tests verifies that while reading a parquet file, the prefetcher leverages more efficient readBigAt logic (ReadMode::RandomRead)
 
