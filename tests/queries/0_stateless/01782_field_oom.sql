8c121db5ac0456a09762d9075124e73c5af3da6a
Fgrtue <kirill.kopnev@clickhouse.com>
2025-12-09 19:57:14 +0000

    Trying to see if the test 01782_field_oom fails after the fix

--- tests/queries/0_stateless/01782_field_oom.sql
+++ tests/queries/0_stateless/01782_field_oom.sql
@@ -1,2 +1,2 @@
-SET max_memory_usage = '500M';
+SET max_memory_usage = '500M', max_execution_time = 0;
 SELECT sumMap([number], [number]) FROM system.numbers_mt; -- { serverError MEMORY_LIMIT_EXCEEDED }
