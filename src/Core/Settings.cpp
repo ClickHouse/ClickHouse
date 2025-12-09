93cb314e885130c871ee35d78216e3047c28c579
Dmitry Novik <n0vik@clickhouse.com>
2025-12-09 20:26:01 +0100

    Fix some tests

--- src/Core/Settings.cpp
+++ src/Core/Settings.cpp
@@ -7117,8 +7117,6 @@ Possible values:
 - 0 - Disable
 - 1 - Enable
 )", 0) \
-    DECLARE(UInt64, jemalloc_profiler_sample_rate, 0, R"(
-)", 0) \
     DECLARE(Bool, jemalloc_enable_profiler, false, R"(
 Enable jemalloc profiler for the query. Jemalloc will sample allocations and all deallocations for sampled allocations.
 Profiles can be flushed using SYSTEM JEMALLOC FLUSH PROFILE which can be used for allocation analysis.
