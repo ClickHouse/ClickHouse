d4459e3e08d742314b9c83f9be79e552f4a7e31c
Antonio Andelic <antonio@clickhouse.com>
2025-12-10 14:17:16 +0100

    Better test

--- tests/queries/0_stateless/03755_final_prewhere_duplicate_columns.sql
+++ tests/queries/0_stateless/03755_final_prewhere_duplicate_columns.sql
@@ -1,4 +1,4 @@
-CREATE TABLE 03755_final_prewhere_duplicate_columns (c0 UInt8) ENGINE = AggregatingMergeTree() ORDER BY (farmHash64(c0));
-INSERT INTO TABLE 03755_final_prewhere_duplicate_columns (c0) SELECT 244 FROM numbers(10);
-INSERT INTO TABLE 03755_final_prewhere_duplicate_columns (c0) SELECT c0 FROM generateRandom('c0 UInt8', 8343867420892784557, 1, 1) LIMIT 100;
-SELECT 03755_final_prewhere_duplicate_columns.c0 FROM 03755_final_prewhere_duplicate_columns FINAL PREWHERE 03755_final_prewhere_duplicate_columns.c0 ORDER BY 03755_final_prewhere_duplicate_columns.c0;
+CREATE TABLE 03755_final_prewhere_duplicate_columns (c0 UInt8) ENGINE = AggregatingMergeTree() ORDER BY (c0);
+INSERT INTO TABLE 03755_final_prewhere_duplicate_columns (c0) SELECT 2 FROM numbers(3);
+INSERT INTO TABLE 03755_final_prewhere_duplicate_columns (c0) SELECT number FROM numbers(10);
+SELECT 03755_final_prewhere_duplicate_columns.c0 FROM 03755_final_prewhere_duplicate_columns FINAL PREWHERE 03755_final_prewhere_duplicate_columns.c0 ORDER BY c0;
