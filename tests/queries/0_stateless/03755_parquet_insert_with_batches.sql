c020e7796c93f4e20b74fda36e7d20aefc3ea7a7
Alex Bakharew <alex.bakharew@gmail.com>
2025-12-10 15:28:09 +0100

    add test

---
+++ tests/queries/0_stateless/03755_parquet_insert_with_batches.sql
@@ -0,0 +1,4 @@
+DROP TABLE IF EXISTS t0;
+CREATE TABLE t0 (c0 JSON) ENGINE = MergeTree() ORDER BY tuple();
+INSERT INTO TABLE FUNCTION url('http://localhost:8123/?query=INSERT+INTO+t0+(c0)+FORMAT+Parquet', 'Parquet', 'c0 JSON') SELECT '{"c0":1}' FROM numbers(10) SETTINGS output_format_parquet_batch_size = 4;
+SELECT count(*) FROM t0;
