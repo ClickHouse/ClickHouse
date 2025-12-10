5ba0c90ac3ac3f4a6c7be11bccae9646a2045929
Alexander Gololobov <davenger@clickhouse.com>
2025-12-10 11:14:02 +0100

    Disable DPsize by default for now

--- src/Core/Settings.cpp
+++ src/Core/Settings.cpp
@@ -7395,7 +7395,7 @@ Allow to create table with the Alias engine.
     DECLARE(Bool, use_paimon_partition_pruning, false, R"(
 Use Paimon partition pruning for Paimon table functions
 )", EXPERIMENTAL) \
-DECLARE(JoinOrderAlgorithm, query_plan_optimize_join_order_algorithm, "dpsize,greedy", R"(
+DECLARE(JoinOrderAlgorithm, query_plan_optimize_join_order_algorithm, "greedy", R"(
 Specifies which JOIN order algorithms to attempt during query plan optimization. The following algorithms are available:
  - 'greedy' - basic greedy algorithm - works fast but might not produce the best join order
  - 'dpsize' - implements DPsize algorithm currently only for Inner joins - considers all possible join orders and finds the most optimal one but might be slow for queries with many tables and join predicates.
