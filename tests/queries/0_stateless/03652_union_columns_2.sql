1e4b5e424c948cbef1975d9261652a5490f6fdb1
Dmitry Novik <mrnovikd@gmail.com>
2025-12-10 10:09:31 +0100

    Update tests

--- tests/queries/0_stateless/03652_union_columns_2.sql
+++ tests/queries/0_stateless/03652_union_columns_2.sql
@@ -12,7 +12,7 @@ INSERT INTO right VALUES
 
 SET enable_analyzer = 1;
 
-select '::run';
+SELECT 'run enable-analyzer=1';
 with differences as
     (
         (
@@ -35,4 +35,57 @@ diff_counts as
     (
         select g, count(*) from differences group by g
     )
-select * from diff_counts;
+select * from diff_counts
+ORDER BY g;
+
+SELECT 'run enable-analyzer=1 ignore';
+with differences as
+    (
+        (
+            select g, i from left
+            where g BETWEEN 0 and 10
+            EXCEPT ALL
+            select g, i from right
+            where g BETWEEN 0 and 10
+        )
+        UNION ALL
+        (
+            select g, i from right
+            where g BETWEEN 0 and 10
+            EXCEPT ALL
+            select g, i from left
+            where g BETWEEN 0 and 10
+        )
+    ),
+diff_counts as
+    (
+        select g, count(ignore(*)) from differences group by g
+    )
+select * from diff_counts
+ORDER BY g;
+
+SET enable_analyzer = 0;
+SELECT 'run enable-analyzer=0';
+with differences as
+    (
+        (
+            select g, i from left
+            where g BETWEEN 0 and 10
+            EXCEPT ALL
+            select g, i from right
+            where g BETWEEN 0 and 10
+        )
+        UNION ALL
+        (
+            select g, i from right
+            where g BETWEEN 0 and 10
+            EXCEPT ALL
+            select g, i from left
+            where g BETWEEN 0 and 10
+        )
+    ),
+diff_counts as
+    (
+        select g, count(*) from differences group by g
+    )
+select * from diff_counts; -- { serverError LOGICAL_ERROR }
