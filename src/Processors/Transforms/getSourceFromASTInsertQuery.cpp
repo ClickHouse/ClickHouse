396ee5ced96bde4ea7b9b240e13703176a08121d
Pervakov Grigorii <pervakov.grigory@gmail.com>
2025-12-10 19:03:49 +0000

    reimplement with aggregating processor

--- src/Processors/Transforms/getSourceFromASTInsertQuery.cpp
+++ src/Processors/Transforms/getSourceFromASTInsertQuery.cpp
@@ -1,20 +1,31 @@
 #include <cstddef>
-#include <Parsers/ASTInsertQuery.h>
+
+#include <AggregateFunctions/IAggregateFunction.h>
+#include <Columns/ColumnString.h>
+#include <Core/Settings.h>
+#include <DataTypes/DataTypeAggregateFunction.h>
+#include <DataTypes/DataTypeArray.h>
+#include <DataTypes/DataTypeTuple.h>
+#include <Functions/FunctionFactory.h>
+#include <IO/CompressionMethod.h>
+#include <IO/ConcatReadBuffer.h>
+#include <IO/EmptyReadBuffer.h>
+#include <IO/ReadBuffer.h>
+#include <IO/ReadBufferFromFile.h>
+#include <IO/ReadBufferFromMemory.h>
+#include <Interpreters/ActionsDAG.h>
 #include <Interpreters/Context.h>
 #include <Interpreters/DatabaseCatalog.h>
-#include <IO/ConcatReadBuffer.h>
-#include <IO/ReadBuffer.h>
-#include <IO/ReadBufferFromMemory.h>
-#include <IO/ReadBufferFromFile.h>
-#include <IO/EmptyReadBuffer.h>
-#include <QueryPipeline/BlockIO.h>
-#include <Processors/Transforms/getSourceFromASTInsertQuery.h>
-#include <Processors/Transforms/AddingDefaultsTransform.h>
-#include <Storages/IStorage.h>
-#include <QueryPipeline/Pipe.h>
-#include <IO/CompressionMethod.h>
-#include <Core/Settings.h>
+#include <Interpreters/ExpressionActions.h>
+#include <Parsers/ASTInsertQuery.h>
 #include <Parsers/ASTLiteral.h>
+#include <Planner/PlannerActionsVisitor.h>
+#include <Processors/Transforms/AddingDefaultsTransform.h>
+#include <Processors/Transforms/ExpressionTransform.h>
+#include <Processors/Transforms/getSourceFromASTInsertQuery.h>
+#include <QueryPipeline/Pipe.h>
+#include <Storages/IStorage.h>
+#include "DataTypes/DataTypesNumber.h"
 
 
 namespace DB
@@ -23,6 +34,7 @@ namespace Setting
 {
     extern const SettingsBool input_format_defaults_for_omitted_fields;
     extern const SettingsNonZeroUInt64 max_insert_block_size;
+    extern const SettingsAggregateFunctionInputFormat aggregate_function_input_format;
 }
 
 namespace ErrorCodes
@@ -147,5 +159,84 @@ std::unique_ptr<ReadBuffer> getReadBufferFromASTInsertQuery(const ASTPtr & ast)
     chassert(!buffers.empty());
     return std::make_unique<ConcatReadBuffer>(std::move(buffers));
 }
+
+std::pair<Block, bool> transformAggregateColumnsToValues(ContextPtr context, const Block & header)
+{
+    auto format = context->getSettingsRef()[Setting::aggregate_function_input_format];
+    if (format == FormatSettings::AggregateFunctionInputFormat::State)
+        return {header, false};
+
+    Block new_header;
+    bool changed = false;
+    // TODO subcolumns
+    for (const auto & col : header)
+    {
+        if (const auto * func = dynamic_cast<const DataTypeAggregateFunction*>(col.type.get()))
+        {
+            changed = true;
+            const auto & args = func->getFunction()->getArgumentTypes();
+            auto value_type = args.size() > 1 ? std::make_shared<DataTypeTuple>(args) : args.at(0);
+            if (format == FormatSettings::AggregateFunctionInputFormat::Value)
+                new_header.insert(ColumnWithTypeAndName(value_type, col.name));
+            else
+                new_header.insert(ColumnWithTypeAndName(std::make_shared<const DataTypeArray>(value_type), col.name));
+        }
+        else
+            new_header.insert(col);
+    }
+
+    return {new_header, changed};
+}
+
+ProcessorPtr createAggregationFromValuesProcessor(ContextPtr context, const SharedHeader & format_header, const Block & header)
+{
+    chassert(format_header->columns() == header.columns());
+    chassert(context->getSettingsRef()[Setting::aggregate_function_input_format] != FormatSettings::AggregateFunctionInputFormat::State);
+    bool is_array = context->getSettingsRef()[Setting::aggregate_function_input_format] == FormatSettings::AggregateFunctionInputFormat::Array;
+
+    ActionsDAG dag(format_header->getNamesAndTypesList());
+    for (size_t pos = 0; pos < header.columns(); ++pos)
+    {
+        const auto & agg_column = header.getByPosition(pos);
+        const auto & value_column = format_header->getByPosition(pos);
+        const auto * func = dynamic_cast<const DataTypeAggregateFunction*>(agg_column.type.get());
+        if (func && agg_column.type != value_column.type)
+        {
+            auto init_aggr = FunctionFactory::instance().get("initializeAggregation", context);
+
+            //calculateConstantActionNodeName
+            auto agg_func_name = std::format("{}State{}", func->getFunctionName(), is_array ? "Array" : "");
+            ColumnWithTypeAndName agg_func_name_column;
+            agg_func_name_column.name = calculateConstantActionNodeName(agg_func_name);
+            agg_func_name_column.column = DataTypeString().createColumnConst(0, agg_func_name);
+            agg_func_name_column.type = std::make_shared<DataTypeString>();
+
+            ActionsDAG::NodeRawConstPtrs args = {&dag.addColumn(std::move(agg_func_name_column))};
+            auto args_size = func->getArgumentsDataTypes().size();
+            args.reserve(1 + args_size);
+            if (args_size == 1)
+                args.push_back(dag.getOutputs()[pos]);
+            else
+            {
+                auto args_tuple = dag.getOutputs()[pos];
+                auto tuple_element_func = FunctionFactory::instance().get("tupleElement", context);
+                for (size_t arg_num = 0; arg_num < args_size; ++arg_num)
+                {
+                    ColumnWithTypeAndName tuple_index_column;
+                    tuple_index_column.name = calculateConstantActionNodeName(arg_num);
+                    tuple_index_column.column = DataTypeUInt64().createColumnConst(0, arg_num + 1);
+                    tuple_index_column.type = std::make_shared<DataTypeUInt64>();
+
+                    args.push_back(&dag.addFunction(tuple_element_func, {args_tuple, &dag.addColumn(std::move(tuple_index_column))}, {}));
+                }
+            }
+
+            dag.getOutputs()[pos] = &dag.addFunction(init_aggr, std::move(args), agg_column.name);
+        }
+    }
+
+   return std::make_shared<ExpressionTransform>(format_header, std::make_shared<ExpressionActions>(std::move(dag)));
+}
+
 
 }
