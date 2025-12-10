cecdf9f24c28199fa37d15e9ba23a4297c5af4c2
rienath <raufs.dunamalijevs@clickhouse.com>
2025-12-10 13:00:16 +0000

    Ignore clang-tidy

--- src/Functions/FunctionsConversion.h
+++ src/Functions/FunctionsConversion.h
@@ -4893,8 +4893,8 @@ private:
     WrapperType createArrayToQBitWrapper(const DataTypeArray & from_array_type, const DataTypeQBit & to_qbit_type) const
     {
         /// Extract values from reference parameters to avoid dangling references in the lambda
-        const DataTypePtr from_nested_type = from_array_type.getNestedType();
-        const DataTypePtr to_nested_type = to_qbit_type.getElementType();
+        const DataTypePtr from_nested_type = from_array_type.getNestedType(); /// NOLINT
+        const DataTypePtr to_nested_type = to_qbit_type.getElementType(); /// NOLINT
         const size_t dimension = to_qbit_type.getDimension();
         const size_t element_size = to_qbit_type.getElementSize();
 
