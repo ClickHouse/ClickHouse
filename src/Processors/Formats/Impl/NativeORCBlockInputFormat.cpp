e98f33fb221a1b717eb5a4ad7c392a685c1a9535
fastio <pengjian.uestc@gmail.com>
2025-12-10 22:41:36 +0800

    Fix ORC reader for all-NULL DICTIONARY_V2 columns

--- src/Processors/Formats/Impl/NativeORCBlockInputFormat.cpp
+++ src/Processors/Formats/Impl/NativeORCBlockInputFormat.cpp
@@ -1312,7 +1312,7 @@ static ColumnWithTypeAndName readColumnWithEncodedStringOrFixedStringData(
     size_t rows = orc_str_column.numElements;
     const auto & orc_dict = *orc_str_column.dictionary;
     if (orc_dict.dictionaryOffset.size() <= 1)
-        return {internal_type->createColumn(), internal_type, column_name};
+        return {internal_type->createColumnConstWithDefaultValue(rows), internal_type, column_name};
 
     size_t dict_size = orc_dict.dictionaryOffset.size() - 1;
     auto holder_column = holder_type->createColumn();
