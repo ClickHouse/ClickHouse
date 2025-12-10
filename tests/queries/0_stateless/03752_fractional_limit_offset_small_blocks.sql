01b353253f6b6c6903a401cc930aebd3899192b9
0xgouda <gouda0x@gmail.com>
2025-12-10 04:37:44 +0200

    duplicate tests but using block size 3.

--- tests/queries/0_stateless/03752_fractional_limit_offset_small_blocks.sql
+++ tests/queries/0_stateless/03752_fractional_limit_offset_small_blocks.sql
@@ -1,12 +1,35 @@
-SET max_block_size = 10;
 
 SELECT 'Fractional Limit Only - Early Pushing';
+
+SET max_block_size = 10;
+
+SELECT number FROM numbers(20) ORDER BY number LIMIT 0.5;
+SELECT '*';
+SELECT number FROM numbers(20) ORDER BY number LIMIT 0.9;
+SELECT '*';
+
+SET max_block_size = 3;
 
 SELECT number FROM numbers(20) ORDER BY number LIMIT 0.5;
 SELECT '*';
 SELECT number FROM numbers(20) ORDER BY number LIMIT 0.9;
 
 SELECT 'Fractional Limit, Normal Offset - Early Pushing/Evicting';
+
+SET max_block_size = 10;
+
+SELECT number FROM numbers(20) ORDER BY number LIMIT 0.5 OFFSET 5;
+SELECT '*';
+SELECT number FROM numbers(20) ORDER BY number LIMIT 0.25 OFFSET 12;
+SELECT '*';
+SELECT number FROM numbers(20) ORDER BY number LIMIT 0.5 OFFSET 10;
+SELECT '*';
+SELECT number FROM numbers(20) ORDER BY number LIMIT 0.9 OFFSET 5;
+SELECT '*';
+SELECT number FROM numbers(15) ORDER BY number LIMIT 0.5 OFFSET 5;
+SELECT '*';
+
+SET max_block_size = 3;
 
 SELECT number FROM numbers(20) ORDER BY number LIMIT 0.5 OFFSET 5;
 SELECT '*';
@@ -20,6 +43,21 @@ SELECT number FROM numbers(15) ORDER BY number LIMIT 0.5 OFFSET 5;
 
 SELECT 'Fractional Limit, Fractional Offset - Early Pushing/Evicting';
 
+SET max_block_size = 10;
+
+SELECT number FROM numbers(20) ORDER BY number LIMIT 0.5 OFFSET 0.25;
+SELECT '*';
+SELECT number FROM numbers(20) ORDER BY number LIMIT 0.25 OFFSET 0.6;
+SELECT '*';
+SELECT number FROM numbers(20) ORDER BY number LIMIT 0.5 OFFSET 0.5;
+SELECT '*';
+SELECT number FROM numbers(20) ORDER BY number LIMIT 0.9 OFFSET 0.25;
+SELECT '*';
+SELECT number FROM numbers(15) ORDER BY number LIMIT 0.5 OFFSET 0.33;
+SELECT '*';
+
+SET max_block_size = 3;
+
 SELECT number FROM numbers(20) ORDER BY number LIMIT 0.5 OFFSET 0.25;
 SELECT '*';
 SELECT number FROM numbers(20) ORDER BY number LIMIT 0.25 OFFSET 0.6;
@@ -31,6 +69,15 @@ SELECT '*';
 SELECT number FROM numbers(15) ORDER BY number LIMIT 0.5 OFFSET 0.33;
 
 SELECT 'Fractional Offset Only - Early Evicting';
+
+SET max_block_size = 10;
+
+SELECT number FROM numbers(20) ORDER BY number OFFSET 0.5;
+SELECT '*';
+SELECT number FROM numbers(20) ORDER BY number OFFSET 0.9;
+SELECT '*';
+
+SET max_block_size = 3;
 
 SELECT number FROM numbers(20) ORDER BY number OFFSET 0.5;
 SELECT '*';
