91db92f6e8c2fd662bd483a5ea19d1b3f900ac89
Alex Bakharew <alex.bakharew@gmail.com>
2025-12-11 14:46:05 +0100

    try to fix test

--- tests/queries/0_stateless/03755_parquet_insert_with_batches.sql
+++ tests/queries/0_stateless/03755_parquet_insert_with_batches.sql
@@ -1,5 +1,5 @@
 -- Tags: no-fasttest
 DROP TABLE IF EXISTS t0;
 CREATE TABLE t0 (c0 JSON) ENGINE = MergeTree() ORDER BY tuple();
-INSERT INTO TABLE FUNCTION url('http://localhost:8123/?query=INSERT+INTO+t0+(c0)+FORMAT+Parquet', 'Parquet', 'c0 JSON') SELECT '{"c0":1}' FROM numbers(10) SETTINGS output_format_parquet_batch_size = 4;
+INSERT INTO TABLE FUNCTION url('http://127.0.0.1:8123/?query=INSERT+INTO+t0+(c0)+FORMAT+Parquet', 'Parquet', 'c0 JSON') SELECT '{"c0":1}' FROM numbers(10) SETTINGS output_format_parquet_batch_size = 4;
 SELECT count(*) FROM t0;
