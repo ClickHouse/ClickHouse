5aa489035c5502e068fa441cd3183094a341572d
Alex Bakharew <alex.bakharew@gmail.com>
2025-12-11 15:48:25 +0100

    fix test

--- tests/queries/0_stateless/03755_parquet_insert_with_batches.sql
+++ tests/queries/0_stateless/03755_parquet_insert_with_batches.sql
@@ -1,5 +1,7 @@
 -- Tags: no-fasttest
-DROP TABLE IF EXISTS t0;
-CREATE TABLE t0 (c0 JSON) ENGINE = MergeTree() ORDER BY tuple();
-INSERT INTO TABLE FUNCTION url('http://127.0.0.1:8123/?query=INSERT+INTO+t0+(c0)+FORMAT+Parquet', 'Parquet', 'c0 JSON') SELECT '{"c0":1}' FROM numbers(10) SETTINGS output_format_parquet_batch_size = 4;
-SELECT count(*) FROM t0;
+DROP DATABASE IF EXISTS test_parquet_insert_with_batches;
+CREATE DATABASE test_parquet_insert_with_batches;
+CREATE TABLE test_parquet_insert_with_batches.t0 (c0 JSON) ENGINE = MergeTree() ORDER BY tuple();
+INSERT INTO TABLE FUNCTION url('http://127.0.0.1:8123/?query=INSERT+INTO+test_parquet_insert_with_batches.t0+(c0)+FORMAT+Parquet', 'Parquet', 'c0 JSON') SELECT '{"c0":1}' FROM numbers(10) SETTINGS output_format_parquet_batch_size = 4;
+SELECT count(*) FROM test_parquet_insert_with_batches.t0;
+DROP DATABASE test_parquet_insert_with_batches;
