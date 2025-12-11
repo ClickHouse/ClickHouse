0a0faf044e7a29feafbaa8b8563e3581269a930f
Smita Kulkarni <Smita.Kulkarni@clickhouse.com>
2025-12-11 22:49:28 +0100

    Try working test

--- tests/queries/0_stateless/03753_s3_table_alter_modify_order_by.sql
+++ tests/queries/0_stateless/03753_s3_table_alter_modify_order_by.sql
@@ -1,5 +1,7 @@
 DROP TABLE IF EXISTS test_s3;
+set s3_truncate_on_insert = 1;
 CREATE TABLE test_s3 (c0 Int) ENGINE = S3(s3_conn, filename = 'file.csv');
+INSERT INTO test_s3 VALUES(1);
 ALTER TABLE test_s3 MODIFY ORDER BY (c0);
-SELECT DISTINCT 1 FROM test_s3;
+SELECT * FROM test_s3;
 DROP TABLE test_s3;
