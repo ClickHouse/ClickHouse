c19347fd272e3a1a449176201d1b889f7baf8025
Alex Bakharew <alex.bakharew@gmail.com>
2025-12-11 13:59:54 +0100

    Disable fast tests

--- tests/queries/0_stateless/03755_parquet_insert_with_batches.sql
+++ tests/queries/0_stateless/03755_parquet_insert_with_batches.sql
@@ -1,4 +1,4 @@
-# Tags: no-fasttest
+-- Tags: no-fasttest
 DROP TABLE IF EXISTS t0;
 CREATE TABLE t0 (c0 JSON) ENGINE = MergeTree() ORDER BY tuple();
 INSERT INTO TABLE FUNCTION url('http://localhost:8123/?query=INSERT+INTO+t0+(c0)+FORMAT+Parquet', 'Parquet', 'c0 JSON') SELECT '{"c0":1}' FROM numbers(10) SETTINGS output_format_parquet_batch_size = 4;
