bb88641e768e4ef34c363aa03cc76c04f3b2d468
Sema Checherinda <checherinda@gmail.com>
2025-12-12 17:35:19 +0100

    fix tests

--- tests/queries/0_stateless/03755_insert_select_duduplication.sh
+++ tests/queries/0_stateless/03755_insert_select_duduplication.sh
@@ -13,6 +13,10 @@ ORDER BY id
 SETTINGS non_replicated_deduplication_window = 1000;
 EOF
 
+# async_insert=0 -- it is insert select queries no need to async inserts
+# insert_deduplicate=1 -- to enable deduplication feature
+CLICKHOUSE_CLIENT="${CLICKHOUSE_CLIENT} --async_insert=0 --insert_deduplicate=1"
+
 echo "insert_select_deduplicate=1 with non-stable select, exception expected"
 ${CLICKHOUSE_CLIENT} --insert_select_deduplicate=1 -q "
     INSERT INTO dst SELECT number AS id, toString(number) AS data FROM numbers(10);
@@ -29,7 +33,7 @@ ${CLICKHOUSE_CLIENT} --insert_select_deduplicate=1 -q "
 CH_CLIENT=$(echo ${CLICKHOUSE_CLIENT} | sed 's/--send_logs_level=[^ ]*//')
 
 echo "insert_select_deduplicate=auto and insert_deduplicate=1 with non-stable select, warning expected, deduplication disabled"
-${CH_CLIENT} --insert_select_deduplicate='auto' --insert_deduplicate=1 --send_logs_level='information' -q "
+${CH_CLIENT} --insert_select_deduplicate='auto' --send_logs_level='information' -q "
     INSERT INTO dst SELECT number AS id, toString(number) AS data FROM numbers(10);
     INSERT INTO dst SELECT number AS id, toString(number) AS data FROM numbers(10);
 " 2>&1 | grep -o "INSERT SELECT deduplication is disabled because SELECT is not stable" | uniq
@@ -40,7 +44,7 @@ ${CLICKHOUSE_CLIENT} -q "
 "
 
 echo "insert_select_deduplicate=auto and insert_deduplicate=1 with stable select, deduplication happens"
-${CLICKHOUSE_CLIENT} --insert_select_deduplicate='auto' --insert_deduplicate=1 -q "
+${CLICKHOUSE_CLIENT} --insert_select_deduplicate='auto' -q "
     INSERT INTO dst SELECT number AS id, toString(number) AS data FROM numbers(10) ORDER BY all;
     INSERT INTO dst SELECT number AS id, toString(number) AS data FROM numbers(10) ORDER BY all;
     SELECT count() FROM dst;
@@ -48,7 +52,7 @@ ${CLICKHOUSE_CLIENT} --insert_select_deduplicate='auto' --insert_deduplicate=1 -
 "
 
 echo "insert_select_deduplicate=0 and insert_deduplicate=1 with stable select, deduplication disabled"
-${CLICKHOUSE_CLIENT} --insert_select_deduplicate=0 --insert_deduplicate=1 -q "
+${CLICKHOUSE_CLIENT} --insert_select_deduplicate=0 -q "
     INSERT INTO dst SELECT number AS id, toString(number) AS data FROM numbers(10) ORDER BY all;
     INSERT INTO dst SELECT number AS id, toString(number) AS data FROM numbers(10) ORDER BY all;
     SELECT count() FROM dst;
@@ -56,7 +60,7 @@ ${CLICKHOUSE_CLIENT} --insert_select_deduplicate=0 --insert_deduplicate=1 -q "
 "
 
 echo "insert_select_deduplicate=1 and insert_deduplicate=1 with stable select, deduplication happens"
-${CLICKHOUSE_CLIENT} --insert_select_deduplicate=1 --insert_deduplicate=1 -q "
+${CLICKHOUSE_CLIENT} --insert_select_deduplicate=1 -q "
     INSERT INTO dst SELECT 1, 'one' ORDER BY all;
     INSERT INTO dst SELECT 1, 'one' ORDER BY all;
     SELECT count() FROM dst;
