69b9d751ad0ab611953f49f82804018d37a851b6
mostafa <sasa1x2001@gmail.com>
2025-12-13 22:30:48 +0200

    Add OrderBy To Ensure Determnistic test output

--- tests/queries/0_stateless/03727_named_tuples_pretty_format.sql
+++ tests/queries/0_stateless/03727_named_tuples_pretty_format.sql
@@ -1,40 +1,47 @@
-SELECT
-    1 AS top_int,
-    NULL AS top_null,
-    'Row1' AS top_str,
-    CAST((
-        42,                                     
-        'ClickHouse',                             
-        CAST((1, 'nested', NULL) AS Tuple(n_id UInt8, n_name String, n_null Nullable(Int32))),
-        CAST([(100, 'foo'), (200, 'bar')] AS Array(Tuple(a UInt16, b String))),
-        NULL,                                     
-        999                                       
-    ) AS Tuple(
-        id UInt64,
-        name String,
-        nested Tuple(n_id UInt8, n_name String, n_null Nullable(Int32)),
-        arr Array(Tuple(a UInt16, b String)),
-        optional_field Nullable(String),
-        extra_id UInt32
-    )) AS my_named_tuple
-UNION ALL
-SELECT
-    2 AS top_int,
-    'Hello' AS top_null,
-    NULL AS top_str,
-    CAST((
-        7, 
-        'Database',
-        CAST((2, 'inner', 123) AS Tuple(n_id UInt8, n_name String, n_null Nullable(Int32))), 
-        CAST([(300, 'baz'), (400, 'qux')] AS Array(Tuple(a UInt16, b String))),
-        'Some text',                              
-        555                                       
-    ) AS Tuple(
-        id UInt64,
-        name String,
-        nested Tuple(n_id UInt8, n_name String, n_null Nullable(Int32)),
-        arr Array(Tuple(a UInt16, b String)),
-        optional_field Nullable(String),
-        extra_id UInt32
-    )) AS my_named_tuple 
-FORMAT PRETTY
+SELECT *
+FROM
+(
+    SELECT
+        1 AS top_int,
+        NULL AS top_null,
+        'Row1' AS top_str,
+        CAST((
+            42,
+            'ClickHouse',
+            CAST((1, 'nested', NULL) AS Tuple(n_id UInt8, n_name String, n_null Nullable(Int32))),
+            CAST([(100, 'foo'), (200, 'bar')] AS Array(Tuple(a UInt16, b String))),
+            NULL,
+            999
+        ) AS Tuple(
+            id UInt64,
+            name String,
+            nested Tuple(n_id UInt8, n_name String, n_null Nullable(Int32)),
+            arr Array(Tuple(a UInt16, b String)),
+            optional_field Nullable(String),
+            extra_id UInt32
+        )) AS my_named_tuple
+
+    UNION ALL
+
+    SELECT
+        2 AS top_int,
+        'Hello' AS top_null,
+        NULL AS top_str,
+        CAST((
+            7,
+            'Database',
+            CAST((2, 'inner', 123) AS Tuple(n_id UInt8, n_name String, n_null Nullable(Int32))),
+            CAST([(300, 'baz'), (400, 'qux')] AS Array(Tuple(a UInt16, b String))),
+            'Some text',
+            555
+        ) AS Tuple(
+            id UInt64,
+            name String,
+            nested Tuple(n_id UInt8, n_name String, n_null Nullable(Int32)),
+            arr Array(Tuple(a UInt16, b String)),
+            optional_field Nullable(String),
+            extra_id UInt32
+        )) AS my_named_tuple
+)
+ORDER BY top_int
+FORMAT PRETTY;
