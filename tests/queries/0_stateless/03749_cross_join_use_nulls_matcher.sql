1590d944db9a04eceb7a2edc2e5c6330faaa4efe
Pedro Ferreira <pedro.silva.ferreira@clickhouse.com>
2025-12-16 14:42:16 +0000

    Add ordering to test

--- tests/queries/0_stateless/03749_cross_join_use_nulls_matcher.sql
+++ tests/queries/0_stateless/03749_cross_join_use_nulls_matcher.sql
@@ -1,7 +1,8 @@
 SET enable_analyzer = 1;
 
-SELECT concat(*)
+SELECT concat(*) x
 FROM numbers(2) AS n1, numbers(3) AS n2
 RIGHT JOIN numbers(4) AS n3
     ON n2.number = n3.number
+ORDER BY x
 SETTINGS join_use_nulls = true;
