5cd11381c1b3191844f97b1e95c305d8f6b5545a
robot-clickhouse <robot-clickhouse@users.noreply.github.com>
2025-12-16 16:17:47 +0000

    Backport #91309 to 25.3: Fix logical error Bad cast from type DB::ColumnSparse to DB::ColumnNullable

--- src/Interpreters/inplaceBlockConversions.cpp
+++ src/Interpreters/inplaceBlockConversions.cpp
@@ -224,7 +224,7 @@ static std::unordered_map<String, ColumnPtr> collectOffsetsColumns(
         if (res_columns[i] == nullptr || isColumnConst(*res_columns[i]))
             continue;
 
-        auto serialization = IDataType::getSerialization(*available_column);
+        auto serialization = available_column->type->getSerialization(*available_column->type->getSerializationInfo(*res_columns[i]));
         serialization->enumerateStreams([&](const auto & subpath)
         {
             if (subpath.empty() || subpath.back().type != ISerialization::Substream::ArraySizes)
