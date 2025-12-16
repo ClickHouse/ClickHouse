2644afdec8e2a0b320d23cffcbbd50b395292363
Elmi Ahmadov <elmi.ahmadov@clickhouse.com>
2025-12-16 18:33:38 +0100

    add new test

--- tests/queries/0_stateless/02346_text_index_bug90609.sql
+++ tests/queries/0_stateless/02346_text_index_bug90609.sql
@@ -25,4 +25,21 @@ FROM
 PREWHERE
   (EXPLAIN actions = 1 SELECT 1 FROM tab PREWHERE hasAllTokens(text, ['Alick'])) = '1'; -- { serverError INCORRECT_RESULT_OF_SCALAR_SUBQUERY }
 
+-- Another test caused the same bug.
+WITH sub AS
+(
+  SELECT text
+  FROM tab
+  WHERE hasAnyTokens(text, ['Alick'])
+)
+SELECT *
+FROM
+(
+  SELECT text
+  FROM tab
+  WHERE hasAnyTokens(text, ['Alick'])
+)
+WHERE (SELECT * FROM sub) != ''
+SETTINGS use_skip_indexes_on_data_read = 1;
+
 DROP TABLE tab;
