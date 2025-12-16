7fe0ece535b684515b362af061f20e111eaf9130
Nikolai Kochetov <nk@clickhouse.com>
2025-12-16 18:46:36 +0000

    Add another test.

---
+++ tests/queries/0_stateless/03761_lazy_materialization_split_expr.sql
@@ -0,0 +1,8 @@
+SET query_plan_optimize_lazy_materialization = 1;
+SET query_plan_max_limit_for_lazy_materialization = 10;
+
+create table tab (x UInt32, y UInt32, z UInt32) engine = MergeTree order by tuple();
+insert into tab select number, number, number from numbers(1e6);
+
+select trimLeft(explain) from (explain actions=1 select * from (select sin(x) + y as a, sin(x) - z as b from tab) order by b limit 10
+settings query_plan_max_limit_for_lazy_materialization=0, query_plan_execute_functions_after_sorting=0) where explain like '%Lazily read columns%';
