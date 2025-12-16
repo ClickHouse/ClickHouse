c6bdeed1ccc9544b9dc7b2f33b09d6fc868dfefd
fastio <pengjian.uestc@gmail.com>
2025-12-16 10:02:46 +0800

    fix tests

--- tests/queries/0_stateless/03534_skip_index_bug90826.sql
+++ tests/queries/0_stateless/03534_skip_index_bug90826.sql
@@ -1,7 +1,6 @@
 -- { echo ON }
 
 SET use_skip_indexes_on_data_read = 1;
-SET max_rows_to_read = 0;
 SET allow_experimental_full_text_index = 1;
 
 DROP TABLE IF EXISTS tab;
@@ -14,11 +13,11 @@ CREATE TABLE tab
 )
 ENGINE = MergeTree
 ORDER BY i
-SETTINGS index_granularity = 4,index_granularity_bytes = 0;
+SETTINGS index_granularity_bytes = 0;
 
-INSERT INTO tab SELECT number, toString(number) FROM numbers(6); -- The Last granule contained rows smaller than index_granularity.
+INSERT INTO tab SELECT number, toString(number) FROM numbers(8194); -- The Last granule contained rows smaller than index_granularity.
 
-SELECT i, text FROM tab WHERE text = '5';
+SELECT i, text FROM tab WHERE text = '8192';
 
 DROP TABLE tab;
 
@@ -30,10 +29,10 @@ CREATE TABLE tab
 )
 ENGINE = MergeTree
 ORDER BY i
-SETTINGS index_granularity = 4, index_granularity_bytes = 0;
+SETTINGS index_granularity_bytes = 0;
 
-INSERT INTO tab SELECT number, toString(number) FROM numbers(6); -- The Last granule contained rows smaller than index_granularity.
+INSERT INTO tab SELECT number, toString(number) FROM numbers(8193); -- The Last granule contained rows smaller than index_granularity.
 
-SELECT i, text FROM tab WHERE hasToken(text, '5');
+SELECT i, text FROM tab WHERE hasToken(text, '8192');
 
 DROP TABLE tab;
