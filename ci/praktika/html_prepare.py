1306ba01aa76242731efea9b3e8279b9a9115e56
Max Kainov <max.kainov@clickhouse.com>
2025-12-17 01:57:12 +0100

    style

--- ci/praktika/html_prepare.py
+++ ci/praktika/html_prepare.py
@@ -24,7 +24,12 @@ class Html:
 
         compressed_file = Utils.compress_gz(page_file)
 
-        S3.copy_file_to_s3(s3_path=str(Path(Settings.HTML_S3_PATH) / Path(page_file).name), local_path=compressed_file, content_encoding="gzip", with_rename=True)
+        S3.copy_file_to_s3(
+            s3_path=str(Path(Settings.HTML_S3_PATH) / Path(page_file).name),
+            local_path=compressed_file,
+            content_encoding="gzip",
+            with_rename=True,
+        )
 
         with open(page_file, "w", encoding="utf-8") as f:
             f.write(html)
