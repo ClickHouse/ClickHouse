a3ccdeb4dfc8240bcba0e2f5675e3ad334aec6f6
Nikolai Kochetov <nk@clickhouse.com>
2025-12-17 17:26:27 +0000

    Better

--- tests/integration/test_backward_compatibility/test_string_serialization_version_alter.py
+++ tests/integration/test_backward_compatibility/test_string_serialization_version_alter.py
@@ -7,7 +7,7 @@ node1 = cluster.add_instance(
     "node1",
     with_zookeeper=False,
     image="clickhouse/clickhouse-server",
-    tag=CLICKHOUSE_CI_MIN_TESTED_VERSION,
+    tag="25.10",
     with_installed_binary=True,
     stay_alive=True,
 )
@@ -25,14 +25,17 @@ def start_cluster():
 
 def test_strings_update_add_column(start_cluster):
     node1.query("create table tab (s String) engine = MergeTree order by tuple() settings min_bytes_for_wide_part=1")
-    node1.query("insert into tab select 'abc'")
-    assert('abc\n' == node1.query("select s from tab"))
+    node1.query("insert into tab select 'abc' || toString(number) from numbers(100)")
+    assert('100\n' == node1.query("select count() from tab where not ignore(*)"))
 
-    node1.restart_with_latest_version();
+    node1.restart_with_latest_version()
 
     node1.query("alter table tab add column s3 String default 'def' settings alter_sync=2")
-    node1.query("alter table tab MATERIALIZE column s3 settings alter_sync=2, allow_experimental_statistics=1")
-    assert('abc\tdef\n' == node1.query("select s, s3 from tab"))
+    node1.query("alter table tab MATERIALIZE column s3 settings alter_sync=2, mutations_sync=2, allow_experimental_statistics=1")
+    node1.query("detach table tab")
+    node1.query("attach table tab")
+    assert('abc99\tdef\n' == node1.query("select max(s), max(s3) from tab"))
+    # assert('1' == node1.query('check table tab'))
 
 
 def test_strings_and_update(start_cluster):
@@ -40,8 +43,10 @@ def test_strings_and_update(start_cluster):
     node1.query("insert into tab select 'abc', 1")
     assert('abc\n' == node1.query("select s from tab"))
 
-    node1.restart_with_latest_version();
+    node1.restart_with_latest_version()
 
     node1.query("alter table tab add statistics x type Uniq settings alter_sync=2, allow_experimental_statistics=1")
     node1.query("alter table tab MATERIALIZE STATISTICS ALL settings alter_sync=2, allow_experimental_statistics=1")
+    node1.query("detach table tab")
+    node1.query("attach table tab")
     assert('abc\n' == node1.query("select s from tab"))
