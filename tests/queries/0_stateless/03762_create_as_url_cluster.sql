f5a3e5674b869f1232e57ed2b9973cbb55363cc2
kssenii <kssenii@clickhouse.com>
2025-12-17 18:50:47 +0100

    Fix logical error with create as url cluster

---
+++ tests/queries/0_stateless/03762_create_as_url_cluster.sql
@@ -0,0 +1,3 @@
+DROP DATABASE {CLICKHOUSE_DATABASE:Identifier};
+CREATE DATABASE {CLICKHOUSE_DATABASE:Identifier} ENGINE = Replicated('/clickhouse/03762_create_as_url_cluster/{database}_replicated', 'shard1', 'replica1') FORMAT Null;
+CREATE TABLE {CLICKHOUSE_DATABASE:Identifier}.test (c0 Int) ENGINE = Memory AS (SELECT 1 FROM url('http://localhost:8123/?query=SELECT+1+FORMAT+Values', 'Values', 'c0 Int') tx)
