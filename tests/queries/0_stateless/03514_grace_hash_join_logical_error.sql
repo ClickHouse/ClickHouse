7c4846e129a8cae42dfc4fc8b5422a5df55a23a5
vdimir <vdimir@clickhouse.com>
2025-12-17 14:10:25 +0000

    investigating 03514_grace_hash_join_logical_error with temporary_files_buffer_size

--- tests/queries/0_stateless/03514_grace_hash_join_logical_error.sql
+++ tests/queries/0_stateless/03514_grace_hash_join_logical_error.sql
@@ -1,7 +1,9 @@
 DROP TABLE IF EXISTS A;
 
-create table A (A Int64, B Int64, S String) Engine=MergeTree order by A 
+create table A (A Int64, B Int64, S String) Engine=MergeTree order by A
 as select number,number, toString(arrayMap(i->cityHash64(i*number), range(10))) from numbers(1e6);
+
+SET temporary_files_buffer_size=725;
 
 SET join_algorithm = 'grace_hash', grace_hash_join_initial_buckets=128, grace_hash_join_max_buckets=256;
 
