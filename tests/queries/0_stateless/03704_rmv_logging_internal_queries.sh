cd7a9b8ad1254752a02ac55e91bec5343b59ee3e
Pedro Ferreira <pedro.silva.ferreira@clickhouse.com>
2025-12-18 13:33:11 +0000

    Revert database rename

--- tests/queries/0_stateless/03704_rmv_logging_internal_queries.sh
+++ tests/queries/0_stateless/03704_rmv_logging_internal_queries.sh
@@ -7,11 +7,10 @@ CURDIR=$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)
 
 # Test that some internal queries from refreshable materialized views are logged correctly
 
-VIEW_DATABASE_NAME=${CLICKHOUSE_TEST_UNIQUE_NAME}testdb
-$CLICKHOUSE_CLIENT --query "CREATE DATABASE $VIEW_DATABASE_NAME"
-$CLICKHOUSE_CLIENT --query "CREATE VIEW $VIEW_DATABASE_NAME.one_proxy AS SELECT * FROM system.one"
+$CLICKHOUSE_CLIENT --query "CREATE DATABASE testdb"
+$CLICKHOUSE_CLIENT --query "CREATE VIEW testdb.one_proxy AS SELECT * FROM system.one"
 $CLICKHOUSE_CLIENT --query "
-CREATE MATERIALIZED VIEW $VIEW_DATABASE_NAME.rmv_test
+CREATE MATERIALIZED VIEW testdb.rmv_test
 REFRESH AFTER 1 HOUR
 (
     dummy UInt8
@@ -21,24 +20,24 @@ ORDER BY dummy
 EMPTY
 AS SELECT
     dummy
-FROM $VIEW_DATABASE_NAME.one_proxy"
-$CLICKHOUSE_CLIENT --query "SYSTEM REFRESH VIEW $VIEW_DATABASE_NAME.rmv_test"
-$CLICKHOUSE_CLIENT --query "SYSTEM WAIT VIEW $VIEW_DATABASE_NAME.rmv_test"
+FROM testdb.one_proxy"
+$CLICKHOUSE_CLIENT --query "SYSTEM REFRESH VIEW testdb.rmv_test"
+$CLICKHOUSE_CLIENT --query "SYSTEM WAIT VIEW testdb.rmv_test"
 
 # refresh with the source table absent to verify that exceptions are logged, too
-$CLICKHOUSE_CLIENT --query "DROP VIEW $VIEW_DATABASE_NAME.one_proxy"
-$CLICKHOUSE_CLIENT --query "SYSTEM REFRESH VIEW $VIEW_DATABASE_NAME.rmv_test"
-$CLICKHOUSE_CLIENT --query "SYSTEM WAIT VIEW $VIEW_DATABASE_NAME.rmv_test" 2> /dev/null
+$CLICKHOUSE_CLIENT --query "DROP VIEW testdb.one_proxy"
+$CLICKHOUSE_CLIENT --query "SYSTEM REFRESH VIEW testdb.rmv_test"
+$CLICKHOUSE_CLIENT --query "SYSTEM WAIT VIEW testdb.rmv_test" 2> /dev/null
 
 $CLICKHOUSE_CLIENT --query "SYSTEM FLUSH LOGS query_log"
 $CLICKHOUSE_CLIENT --query "
 SELECT
-    countIf(query LIKE '%INSERT INTO $VIEW_DATABASE_NAME.\`.tmp.inner_id.%' AND type = 'QueryStart'),
-    countIf(query LIKE '%INSERT INTO $VIEW_DATABASE_NAME.\`.tmp.inner_id.%' AND type = 'QueryFinish'),
+    countIf(query LIKE '%INSERT INTO testdb.\`.tmp.inner_id.%' AND type = 'QueryStart'),
+    countIf(query LIKE '%INSERT INTO testdb.\`.tmp.inner_id.%' AND type = 'QueryFinish'),
     countIf(query = '(create target table)' AND type = 'ExceptionBeforeStart') > 0
 FROM system.query_log
 WHERE is_internal = 1 AND current_database IN [currentDatabase(), 'default']
 "
 
-$CLICKHOUSE_CLIENT --query "DROP VIEW $VIEW_DATABASE_NAME.rmv_test"
-$CLICKHOUSE_CLIENT --query "DROP DATABASE $VIEW_DATABASE_NAME"
+$CLICKHOUSE_CLIENT --query "DROP VIEW testdb.rmv_test"
+$CLICKHOUSE_CLIENT --query "DROP DATABASE testdb.rmv_test"
