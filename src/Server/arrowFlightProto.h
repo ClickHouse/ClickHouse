e7dc5429bdcddc97763a6165da5db63758b99e5f
Yakov Olkhovskiy <yakov@clickhouse.com>
2025-12-18 08:15:35 +0000

    fix tidy

--- src/Server/arrowFlightProto.h
+++ src/Server/arrowFlightProto.h
@@ -1,8 +1,9 @@
 #pragma once
-
+// NOLINTBEGIN
 #pragma GCC diagnostic push
 #pragma GCC diagnostic ignored "-Weverything"
 
 #include <Server/grpc_protos/CommandStatementIngest.pb.h>
 
 #pragma GCC diagnostic pop
+// NOLINTEND
