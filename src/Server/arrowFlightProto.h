e9a0c51af1ccd55d23314e2183903cc0ed616c45
Yakov Olkhovskiy <yakov@clickhouse.com>
2025-12-18 07:30:40 +0000

    fix tidy

--- src/Server/arrowFlightProto.h
+++ src/Server/arrowFlightProto.h
@@ -1,7 +1,7 @@
 #pragma once
 
 #pragma GCC diagnostic push
-#pragma GCC diagnostic ignored "-Wreserved-identifier"
+#pragma GCC diagnostic ignored "-Weverything"
 
 #include <Server/grpc_protos/CommandStatementIngest.pb.h>
 
