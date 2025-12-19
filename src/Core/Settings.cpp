01f358aeda5437a79b129f99f6492781d3ad8801
Fgrtue <kirill.kopnev@clickhouse.com>
2025-12-19 15:30:56 +0000

    Changed default of max_insert_block_size_bytes to disabled

--- src/Core/Settings.cpp
+++ src/Core/Settings.cpp
@@ -148,7 +148,7 @@ The default is slightly more than max_block_size. The reason for this is that ce
 Possible values:
 - Positive integer.
 )", 0, max_insert_block_size_rows) \
-DECLARE(UInt64, max_insert_block_size_bytes, DEFAULT_INSERT_BLOCK_SIZE, R"(
+DECLARE(UInt64, max_insert_block_size_bytes, 0, R"(
 The maximum size of blocks (in bytes) to form for insertion into a table.
 
 This setting works together with max_insert_block_size_rows and controls block formation in the same contexts (format parsing and INSERT operations). See max_insert_block_size_rows for detailed information about when and how these settings are applied.
