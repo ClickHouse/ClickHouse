d62c349be6b276af9191cfce8440cefa7cff4196
robot-clickhouse <robot-clickhouse@users.noreply.github.com>
2025-12-21 17:13:35 +0000

    Backport #92748 to 25.8: Do not attempt to delete temporary directories at startup if a MergeTree table is created over a readonly disk

--- src/Storages/StorageMergeTree.cpp
+++ src/Storages/StorageMergeTree.cpp
@@ -192,6 +192,10 @@ StorageMergeTree::StorageMergeTree(
 
 void StorageMergeTree::startup()
 {
+    /// Do not schedule any background jobs if current storage has static data files.
+    if (isStaticStorage())
+        return;
+
     clearEmptyParts();
 
     /// Temporary directories contain incomplete results of merges (after forced restart)
@@ -201,10 +205,6 @@ void StorageMergeTree::startup()
     /// NOTE background task will also do the above cleanups periodically.
     time_after_previous_cleanup_parts.restart();
     time_after_previous_cleanup_temporary_directories.restart();
-
-    /// Do not schedule any background jobs if current storage has static data files.
-    if (isStaticStorage())
-        return;
 
     try
     {
