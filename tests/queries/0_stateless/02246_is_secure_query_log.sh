208e13d8b70e9c4332dfd269bc9422c8bbe6926c
Konstantin Bogdanov <konstantin@clickhouse.com>
2025-12-23 17:18:40 +0100

    Set `http_wait_end_of_query=1` explicitly

--- tests/queries/0_stateless/02246_is_secure_query_log.sh
+++ tests/queries/0_stateless/02246_is_secure_query_log.sh
@@ -7,8 +7,8 @@ CURDIR=$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)
 
 ${CLICKHOUSE_CLIENT} --log_queries=1 --query_id "2246_${CLICKHOUSE_DATABASE}_client_nonsecure" -q "select 'native plain'"
 ${CLICKHOUSE_CLIENT_SECURE} --log_queries=1 --query_id "2246_${CLICKHOUSE_DATABASE}_client_secure" -q "select 'native secure'"
-${CLICKHOUSE_CURL} -sS "${CLICKHOUSE_URL}&log_queries=1&query_id=2246_${CLICKHOUSE_DATABASE}_http_nonsecure" -d "select 'http plain'"
-${CLICKHOUSE_CURL} -sSk "${CLICKHOUSE_URL_HTTPS}&log_queries=1&query_id=2246_${CLICKHOUSE_DATABASE}_http_secure" -d "select 'http secure'"
+${CLICKHOUSE_CURL} -sS "${CLICKHOUSE_URL}&log_queries=1&http_wait_end_of_query=1&query_id=2246_${CLICKHOUSE_DATABASE}_http_nonsecure" -d "select 'http plain'"
+${CLICKHOUSE_CURL} -sSk "${CLICKHOUSE_URL_HTTPS}&log_queries=1&http_wait_end_of_query=1&query_id=2246_${CLICKHOUSE_DATABASE}_http_secure" -d "select 'http secure'"
 
 ${CLICKHOUSE_CLIENT} -q "system flush logs query_log"
 
