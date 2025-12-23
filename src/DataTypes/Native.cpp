540691840452deee3d6871a9b267ca7eed5fb664
Alexey Milovidov <milovidov@clickhouse.com>
2025-12-23 17:22:30 +0100

    Fix error

--- src/DataTypes/Native.cpp
+++ src/DataTypes/Native.cpp
@@ -39,8 +39,12 @@ bool canBeNativeType(const IDataType & type)
         return canBeNativeType(*data_type_nullable.getNestedType());
     }
 
-    return data_type.isInt() || data_type.isUInt() || data_type.isNativeFloat() || data_type.isDate() || data_type.isDate32()
-        || data_type.isDateTime() || data_type.isEnum() || data_type.isDecimal() || data_type.isDateTime64() || data_type.isTime();
+    return ((data_type.isInt() || data_type.isUInt()) && type.getSizeOfValueInMemory() <= MAX_NATIVE_INT_SIZE)
+        || data_type.isNativeFloat()
+        || data_type.isDate() || data_type.isDate32()
+        || data_type.isDateTime() || data_type.isDateTime64()
+        || data_type.isTime() || data_type.isTime64()
+        || data_type.isEnum() || data_type.isDecimal();
 }
 
 bool canBeNativeType(const DataTypePtr & type)
