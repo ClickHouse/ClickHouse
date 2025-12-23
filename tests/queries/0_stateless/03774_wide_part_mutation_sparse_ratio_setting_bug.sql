a663c069db5259c5cf492ad369558b23eb14aa80
Pavel Kruglov <48961922+Avogar@users.noreply.github.com>
2025-12-23 13:40:21 +0100

    Update tests/queries/0_stateless/03774_wide_part_mutation_sparse_ratio_setting_bug.sql
    Co-authored-by: Azat Khuzhin <a3at.mail@gmail.com>

--- tests/queries/0_stateless/03774_wide_part_mutation_sparse_ratio_setting_bug.sql
+++ tests/queries/0_stateless/03774_wide_part_mutation_sparse_ratio_setting_bug.sql
@@ -1,5 +1,5 @@
 drop table if exists test;
-create table test (a UInt32, b UInt32) engine=MergeTree order by tuple() settings min_bytes_for_wide_part=0, ratio_of_defaults_for_sparse_serialization=0.0;
+create table test (a UInt32, b UInt32) engine=MergeTree order by tuple() settings min_bytes_for_wide_part=0, ratio_of_defaults_for_sparse_serialization=0.0, max_suspicious_broken_parts=0, max_suspicious_broken_parts_bytes=0;
 insert into test select number, number from numbers(10);
 
 detach table test;
