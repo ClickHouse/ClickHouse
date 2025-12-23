4e34125a03f54f1d5a1ab1d32e35550035145fd6
Ilya Golshtein <igolshtein@altinity.com>
2025-12-23 23:13:17 +0000

    kafka_produce_time: forgotten file

---
+++ src/Storages/Kafka/SimpleTopicPartition.cpp
@@ -0,0 +1,20 @@
+#include "SimpleTopicPartition.h"
+#include <Common/SipHash.h>
+
+namespace DB
+{
+std::size_t SimpleTopicPartitionHash::operator()(const SimpleTopicPartition & tp) const
+{
+    SipHash s;
+    s.update(tp.topic);
+    s.update(tp.partition_id);
+    return s.get64();
+}
+
+bool SimpleTopicPartition::operator<(const SimpleTopicPartition & other) const
+{
+    return std::tie(topic, partition_id) < std::tie(other.topic, other.partition_id);
+}
+
+
+}
