fb0d59ad3942e2b71528113f7e77fafec8fb7eea
Dmitry Novik <n0vik@clickhouse.com>
2025-12-24 14:34:23 +0100

    Update tests

--- tests/queries/0_stateless/03773_semi_join_equivalent_sets.sql
+++ tests/queries/0_stateless/03773_semi_join_equivalent_sets.sql
@@ -4,8 +4,32 @@ INSERT INTO users VALUES (1231, 'John', 33);
 INSERT INTO users VALUES (6666, 'Ksenia', 48);
 INSERT INTO users VALUES (8888, 'Alice', 50);
 
-EXPLAIN
-SELECT * FROM users LEFT ANY JOIN (SELECT number FROM numbers(10)) as t2 ON users.uid = t2.number WHERE t2.number = 0;
+SELECT explain FROM (
+    EXPLAIN actions = 1
+    SELECT * FROM users LEFT ANY JOIN (SELECT number FROM numbers(10)) as t2 ON users.uid = t2.number WHERE t2.number = 0
+) WHERE explain ilike '%Type:%' OR explain ilike '%Strictness%' OR explain ilike '%filter column%';
 
 EXPLAIN actions = 1
 SELECT * FROM users LEFT SEMI JOIN (SELECT number FROM numbers(10)) as t2 ON users.uid = t2.number WHERE t2.number = 1;
+
+SELECT explain FROM (
+  EXPLAIN actions = 1 SELECT * FROM users LEFT ANY JOIN (SELECT number FROM numbers(10)) as t2 ON users.uid = t2.number WHERE t2.number = 1
+) WHERE explain ilike '%Type:%' OR explain ilike '%Strictness%' OR explain ilike '%filter column%';
+
+SELECT '--';
+
+SELECT explain FROM (
+  EXPLAIN actions = 1 SELECT * FROM users LEFT SEMI JOIN (SELECT number FROM numbers(10)) as t2 ON users.uid = t2.number WHERE users.uid = 1
+) WHERE explain ilike '%Type:%' OR explain ilike '%Strictness%' OR explain ilike '%filter column%';
+
+SELECT '--';
+
+SELECT explain FROM (
+  EXPLAIN actions = 1 SELECT * FROM users RIGHT ANY JOIN (SELECT number FROM numbers(10)) as t2 ON users.uid = t2.number WHERE users.uid = 1
+) WHERE explain ilike '%Type:%' OR explain ilike '%Strictness%' OR explain ilike '%filter column%';
+
+SELECT '--';
+
+SELECT explain FROM (
+  EXPLAIN actions = 1 SELECT * FROM users RIGHT SEMI JOIN (SELECT number FROM numbers(10)) as t2 ON users.uid = t2.number WHERE t2.number = 1
+) WHERE explain ilike '%Type:%' OR explain ilike '%Strictness%' OR explain ilike '%filter column%';
