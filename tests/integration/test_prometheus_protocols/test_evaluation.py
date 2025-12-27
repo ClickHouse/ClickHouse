a4fca8383fead8ba411bff07ad2d171dc745c2ec
Vitaly Baranov <vitlibar@clickhouse.com>
2025-12-27 18:03:37 +0100

    Add test for unary operators.

--- tests/integration/test_prometheus_protocols/test_evaluation.py
+++ tests/integration/test_prometheus_protocols/test_evaluation.py
@@ -500,6 +500,41 @@ def test_literals():
     )
 
 
+def test_unary_operators():
+    do_query_test(
+        "+test",
+        180,
+        '{"resultType": "vector", "result": [{"metric": {"__name__": "test"}, "value": [180, "4"]}]}',
+        [["[('__name__','test')]", "1970-01-01 00:03:00.000", 4]],
+    )
+
+    do_query_test(
+        "+test",
+        180,
+        '{"resultType": "vector", "result": [{"metric": {"__name__": "test"}, "value": [180, "4"]}]}',
+        [["[('__name__','test')]", "1970-01-01 00:03:00.000", 4]],
+    )
+
+    do_query_test(
+        "-test",
+        180,
+        '{"resultType": "vector", "result": [{"metric": {}, "value": [180, "-4"]}]}',
+        [["[]", "1970-01-01 00:03:00.000", -4]],
+    )
+
+    do_query_test(
+        "(-test)[120s:15s]",
+        180,
+        '{"resultType": "matrix", "result": [{"metric": {}, "values": [[120, "-1"], [135, "-3"], [150, "-4"], [165, "-4"], [180, "-4"]]}]}',
+        [
+            [
+                "[]",
+                "[('1970-01-01 00:02:00.000',-1),('1970-01-01 00:02:15.000',-3),('1970-01-01 00:02:30.000',-4),('1970-01-01 00:02:45.000',-4),('1970-01-01 00:03:00.000',-4)]",
+            ]
+        ],
+    )
+
+
 def test_range_query():
     do_range_query_test(
         "test",
