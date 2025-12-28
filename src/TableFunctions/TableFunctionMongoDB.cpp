ec37bc4faa9a3c1f9d624129c4b5eaac4528df12
vanchaklar <38177830+vanchaklar@users.noreply.github.com>
2025-12-28 16:50:05 +0330

    Update TableFunctionMongoDB.cpp
    Enhanced error messages.

--- src/TableFunctions/TableFunctionMongoDB.cpp
+++ src/TableFunctions/TableFunctionMongoDB.cpp
@@ -113,8 +113,12 @@ std::pair<String, ASTPtr> getKeyValueMongoDBArgument(const ASTFunction * ast_fun
 {
     const auto * args_expr = assert_cast<const ASTExpressionList *>(ast_func->arguments.get());
     const auto & function_args = args_expr->children;
-    if (function_args.size() != 2 || ast_func->name != "equals" || !function_args[0]->as<ASTIdentifier>())
-        throw Exception(ErrorCodes::BAD_ARGUMENTS, "Expected key-value defined argument, got {}", ast_func->formatForErrorMessage());
+    if (function_args.size() != 2)
+        throw Exception(ErrorCodes::BAD_ARGUMENTS, "Expected key-value defined argument, got {}. Expected size == 2, got {}", ast_func->formatForErrorMessage(), function_args.size());
+    if (ast_func->name != "equals")
+        throw Exception(ErrorCodes::BAD_ARGUMENTS, "Expected key-value defined argument, got {}. '{}' is not allowed.", ast_func->formatForErrorMessage(), ast_func->name);
+    if (!function_args[0]->as<ASTIdentifier>())
+        throw Exception(ErrorCodes::BAD_ARGUMENTS, "Expected key-value defined argument, got {}. Key is not an identifier.", ast_func->formatForErrorMessage());
 
     const auto & arg_name = function_args[0]->as<ASTIdentifier>()->name();
     static const std::unordered_set<std::string> allowed_keys = {
