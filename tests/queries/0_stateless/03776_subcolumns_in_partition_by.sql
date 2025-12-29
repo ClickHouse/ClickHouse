ccaae1ffc89290229eefe1d8305646bab4d10522
avogar <avogar@clickhouse.com>
2025-12-29 18:00:08 +0000

    Fix test

--- tests/queries/0_stateless/03776_subcolumns_in_partition_by.sql
+++ tests/queries/0_stateless/03776_subcolumns_in_partition_by.sql
@@ -1,3 +1,7 @@
+-- Tags: no-parallel-replicas
+-- Tag no-parallel-replicas: result of explain is different
+
+set enable_analyzer=1;
 drop table if exists test;
 create table test (json JSON(d Date)) engine=MergeTree order by tuple() partition by json.d;
 insert into test select '{"d" : "2020-01-01"}';
