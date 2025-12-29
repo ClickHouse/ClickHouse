bdef2247e782522a0219d0761707a49ad33a5f06
kssenii <kssenii@clickhouse.com>
2025-12-29 17:52:11 +0100

    Fix flaky dynamically_resize_filesystem_cache

--- tests/queries/0_stateless/03032_dynamically_resize_filesystem_cache_2.sh
+++ tests/queries/0_stateless/03032_dynamically_resize_filesystem_cache_2.sh
@@ -16,11 +16,11 @@ INSERT INTO test SELECT randomString(1000);
 $CLICKHOUSE_CLIENT --query "SELECT * FROM test FORMAT Null"
 
 prev_max_size=$($CLICKHOUSE_CLIENT --query "SELECT max_size FROM system.filesystem_cache_settings WHERE cache_name = '$disk_name'")
-$CLICKHOUSE_CLIENT --query "SELECT current_size > 0 FROM system.filesystem_cache_settings WHERE cache_name = '$disk_name' FORMAT TabSeparated"
+$CLICKHOUSE_CLIENT --query "SELECT current_size > 0, 'current size is non-zero' FROM system.filesystem_cache_settings WHERE cache_name = '$disk_name' FORMAT TabSeparated"
 
 config_path=${CLICKHOUSE_CONFIG_DIR}/config.d/storage_conf.xml
 
-new_max_size=$($CLICKHOUSE_CLIENT --query "SELECT divide(max_size, 2) FROM system.filesystem_cache_settings WHERE cache_name = '$disk_name'")
+new_max_size=$($CLICKHOUSE_CLIENT --query "SELECT max_size * 0.8 FROM system.filesystem_cache_settings WHERE cache_name = '$disk_name'")
 sed -i "s|<max_size>$prev_max_size<\/max_size>|<max_size>$new_max_size<\/max_size>|"  $config_path
 
 # echo $prev_max_size
@@ -30,9 +30,9 @@ $CLICKHOUSE_CLIENT -m --query "
 set send_logs_level='fatal';
 SYSTEM RELOAD CONFIG"
 
-$CLICKHOUSE_CLIENT --query "SELECT max_size == $new_max_size FROM system.filesystem_cache_settings WHERE cache_name = '$disk_name' FORMAT TabSeparated"
-$CLICKHOUSE_CLIENT --query "SELECT current_size > 0 FROM system.filesystem_cache_settings WHERE cache_name = '$disk_name' FORMAT TabSeparated"
-$CLICKHOUSE_CLIENT --query "SELECT current_size <= max_size FROM system.filesystem_cache_settings WHERE cache_name = '$disk_name' FORMAT TabSeparated"
+$CLICKHOUSE_CLIENT --query "SELECT max_size == $new_max_size, 'max size is changed' FROM system.filesystem_cache_settings WHERE cache_name = '$disk_name' FORMAT TabSeparated"
+$CLICKHOUSE_CLIENT --query "SELECT current_size > 0, 'current size is still non-zero' FROM system.filesystem_cache_settings WHERE cache_name = '$disk_name' FORMAT TabSeparated"
+$CLICKHOUSE_CLIENT --query "SELECT current_size <= max_size, 'current size is less than max size' FROM system.filesystem_cache_settings WHERE cache_name = '$disk_name' FORMAT TabSeparated"
 
 sed -i "s|<max_size>$new_max_size<\/max_size>|<max_size>$prev_max_size<\/max_size>|"  $config_path
 
@@ -40,6 +40,6 @@ $CLICKHOUSE_CLIENT -m --query "
 set send_logs_level='fatal';
 SYSTEM RELOAD CONFIG"
 
-$CLICKHOUSE_CLIENT --query "SELECT max_size == $prev_max_size FROM system.filesystem_cache_settings WHERE cache_name = '$disk_name' FORMAT TabSeparated"
-$CLICKHOUSE_CLIENT --query "SELECT current_size > 0 FROM system.filesystem_cache_settings WHERE cache_name = '$disk_name' FORMAT TabSeparated"
-$CLICKHOUSE_CLIENT --query "SELECT current_size <= max_size FROM system.filesystem_cache_settings WHERE cache_name = '$disk_name' FORMAT TabSeparated"
+$CLICKHOUSE_CLIENT --query "SELECT max_size == $prev_max_size, 'max size is reloaded' FROM system.filesystem_cache_settings WHERE cache_name = '$disk_name' FORMAT TabSeparated"
+$CLICKHOUSE_CLIENT --query "SELECT current_size > 0, 'current size is still non-zero' FROM system.filesystem_cache_settings WHERE cache_name = '$disk_name' FORMAT TabSeparated"
+$CLICKHOUSE_CLIENT --query "SELECT current_size <= max_size, 'current size is less than max size' FROM system.filesystem_cache_settings WHERE cache_name = '$disk_name' FORMAT TabSeparated"
