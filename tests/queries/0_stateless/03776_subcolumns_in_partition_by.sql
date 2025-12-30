02e263e223d2bbedf81a94d1b5596ca57a6e3814
avogar <avogar@clickhouse.com>
2025-12-30 13:37:51 +0000

    Fix possible crash

--- tests/queries/0_stateless/03776_subcolumns_in_partition_by.sql
+++ tests/queries/0_stateless/03776_subcolumns_in_partition_by.sql
@@ -12,3 +12,8 @@ select * from test order by json.d;
 explain indexes=1 select * from test where json.d = '2020-01-01';
 drop table test;
 
+create table test (c0 Array(Nullable(Int)), c1 Int, c2 Int) ENGINE = MergeTree() partition by (c0.null) order by (c1);
+insert into test  VALUES ([], 1, 2), ([1], 3, 4);
+select * from test order by all;
+drop table test;
+
