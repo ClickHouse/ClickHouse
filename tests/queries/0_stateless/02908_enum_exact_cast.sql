91b07b56c9af3f73e6a32aca91cc363e372126f5
Elmi Ahmadov <elmi.ahmadov@clickhouse.com>
2025-12-31 00:59:42 +0100

    handle null value for enum

--- tests/queries/0_stateless/02908_enum_exact_cast.sql
+++ tests/queries/0_stateless/02908_enum_exact_cast.sql
@@ -1,13 +1,28 @@
 --- GitHub issue 56144 
 --- Tests number values of enum are validated during insertion.
 
-SET input_format_numbers_enum_on_conversion_error = 0; -- default value
+SELECT 'Table insertion';
+
+SELECT 'Non-nullable Enum';
 
 DROP TABLE IF EXISTS enum_table;
 CREATE TABLE enum_table (
   id UInt64,
-  e Enum('first' = 1, 'second' = 2, 'third' = 3)
+  val Enum('first' = 1, 'second' = 2, 'third' = 3)
 ) ENGINE = Memory;
+
+SELECT 'Null value';
+
+SELECT '-- treat NULL as default value';
+INSERT INTO enum_table VALUES (0, NULL); -- input_format_null_as_default is enabled by default
+SELECT val FROM enum_table;
+
+SELECT '-- treat NULL as non-default value';
+INSERT INTO enum_table SETTINGS input_format_null_as_default = 0 VALUES (0, NULL); -- { clientError TYPE_MISMATCH }
+
+SELECT 'Non-null values';
+
+SET input_format_numbers_enum_on_conversion_error = 0; -- current behavior
 
 INSERT INTO enum_table VALUES (0, 'first');
 INSERT INTO enum_table VALUES (0, 'fifth'); -- { clientError UNKNOWN_ELEMENT_OF_ENUM }
@@ -21,6 +36,61 @@ INSERT INTO enum_table VALUES (0, 0); -- { clientError UNKNOWN_ELEMENT_OF_ENUM }
 
 DROP TABLE enum_table;
 
+SELECT 'Nullable Enum';
+
+DROP TABLE IF EXISTS nullable_enum_table;
+CREATE TABLE nullable_enum_table (
+  id UInt64,
+  val Nullable(Enum('first' = 1, 'second' = 2, 'third' = 3))
+) ENGINE = Memory;
+
+SELECT 'Null value';
+
+SELECT '-- store NULL instead of default value for Nullable column';
+INSERT INTO nullable_enum_table SETTINGS input_format_null_as_default = 1 VALUES (0, NULL);
+INSERT INTO nullable_enum_table SETTINGS input_format_null_as_default = 0 VALUES (0, NULL);
+SELECT val FROM nullable_enum_table;
+
+SELECT 'Non-null values';
+
+SET input_format_numbers_enum_on_conversion_error = 0; -- current behavior
+
+INSERT INTO nullable_enum_table VALUES (0, 'first');
+INSERT INTO nullable_enum_table VALUES (0, 'fifth'); -- { clientError UNKNOWN_ELEMENT_OF_ENUM }
+INSERT INTO nullable_enum_table VALUES (0, NULL);
+INSERT INTO nullable_enum_table VALUES (0, 0);
+
+SET input_format_numbers_enum_on_conversion_error = 1;
+
+INSERT INTO nullable_enum_table VALUES (0, 'first');
+INSERT INTO nullable_enum_table VALUES (0, 'fifth'); -- { clientError UNKNOWN_ELEMENT_OF_ENUM }
+INSERT INTO nullable_enum_table VALUES (0, NULL);
+INSERT INTO nullable_enum_table VALUES (0, 0); -- { clientError UNKNOWN_ELEMENT_OF_ENUM }
+
+DROP TABLE nullable_enum_table;
+
+SELECT 'CAST';
+
+SELECT '-- current behaviour';
+SET input_format_numbers_enum_on_conversion_error = 0;
+
+SELECT (('first'::String)::Enum('first' = 1, 'second' = 2, 'third' = 3))::UInt64;
+SELECT (('second'::String)::Enum('first' = 1, 'second' = 2, 'third' = 3))::UInt64;
+SELECT (('third'::String)::Enum('first' = 1, 'second' = 2, 'third' = 3))::UInt64;
+SELECT (('fifth'::String)::Enum('first' = 1, 'second' = 2, 'third' = 3))::UInt64; -- { serverError UNKNOWN_ELEMENT_OF_ENUM }
+
+SELECT ((4::Int8)::Enum('first' = 1, 'second' = 2, 'third' = 3))::UInt64;
+SELECT ((4::Int16)::Enum('first' = 1, 'second' = 2, 'third' = 3))::UInt64;
+SELECT ((4::Int32)::Enum('first' = 1, 'second' = 2, 'third' = 3))::UInt64;
+SELECT ((4::Int64)::Enum('first' = 1, 'second' = 2, 'third' = 3))::UInt64;
+SELECT ((4::UInt8)::Enum('first' = 1, 'second' = 2, 'third' = 3))::UInt64;
+SELECT ((4::UInt16)::Enum('first' = 1, 'second' = 2, 'third' = 3))::UInt64;
+SELECT ((4::UInt32)::Enum('first' = 1, 'second' = 2, 'third' = 3))::UInt64;
+SELECT ((4::UInt64)::Enum('first' = 1, 'second' = 2, 'third' = 3))::UInt64;
+SELECT ((4::Float32)::Enum('first' = 1, 'second' = 2, 'third' = 3))::UInt64;
+SELECT ((4::Float64)::Enum('first' = 1, 'second' = 2, 'third' = 3))::UInt64;
+
+SELECT '-- new behaviour';
 SET input_format_numbers_enum_on_conversion_error = 1;
 
 SELECT (('first'::String)::Enum('first' = 1, 'second' = 2, 'third' = 3))::UInt64;
@@ -49,21 +119,3 @@ SELECT ((4::UInt32)::Enum('first' = 1, 'second' = 2, 'third' = 3))::UInt64; -- {
 SELECT ((4::UInt64)::Enum('first' = 1, 'second' = 2, 'third' = 3))::UInt64; -- { serverError UNKNOWN_ELEMENT_OF_ENUM }
 SELECT ((4::Float32)::Enum('first' = 1, 'second' = 2, 'third' = 3))::UInt64; -- { serverError UNKNOWN_ELEMENT_OF_ENUM }
 SELECT ((4::Float64)::Enum('first' = 1, 'second' = 2, 'third' = 3))::UInt64; -- { serverError UNKNOWN_ELEMENT_OF_ENUM }
-
-SET input_format_numbers_enum_on_conversion_error = 0;
-
-SELECT (('first'::String)::Enum('first' = 1, 'second' = 2, 'third' = 3))::UInt64;
-SELECT (('second'::String)::Enum('first' = 1, 'second' = 2, 'third' = 3))::UInt64;
-SELECT (('third'::String)::Enum('first' = 1, 'second' = 2, 'third' = 3))::UInt64;
-SELECT (('fifth'::String)::Enum('first' = 1, 'second' = 2, 'third' = 3))::UInt64; -- { serverError UNKNOWN_ELEMENT_OF_ENUM }
-
-SELECT ((4::Int8)::Enum('first' = 1, 'second' = 2, 'third' = 3))::UInt64;
-SELECT ((4::Int16)::Enum('first' = 1, 'second' = 2, 'third' = 3))::UInt64;
-SELECT ((4::Int32)::Enum('first' = 1, 'second' = 2, 'third' = 3))::UInt64;
-SELECT ((4::Int64)::Enum('first' = 1, 'second' = 2, 'third' = 3))::UInt64;
-SELECT ((4::UInt8)::Enum('first' = 1, 'second' = 2, 'third' = 3))::UInt64;
-SELECT ((4::UInt16)::Enum('first' = 1, 'second' = 2, 'third' = 3))::UInt64;
-SELECT ((4::UInt32)::Enum('first' = 1, 'second' = 2, 'third' = 3))::UInt64;
-SELECT ((4::UInt64)::Enum('first' = 1, 'second' = 2, 'third' = 3))::UInt64;
-SELECT ((4::Float32)::Enum('first' = 1, 'second' = 2, 'third' = 3))::UInt64;
-SELECT ((4::Float64)::Enum('first' = 1, 'second' = 2, 'third' = 3))::UInt64;
