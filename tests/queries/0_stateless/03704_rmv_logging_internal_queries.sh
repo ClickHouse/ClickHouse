879a953c1e095823ea7ee8b1a498a2417161e983
Pedro Ferreira <pedro.silva.ferreira@clickhouse.com>
2025-12-30 16:27:10 +0000

    Add comment with reason

--- tests/queries/0_stateless/03704_rmv_logging_internal_queries.sh
+++ tests/queries/0_stateless/03704_rmv_logging_internal_queries.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/env bash
 # Tags: no-parallel, atomic-database
+# If other tests are using materialized views, the `countIf` query below may be affected.
 
 CURDIR=$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)
 # shellcheck source=../shell_config.sh
