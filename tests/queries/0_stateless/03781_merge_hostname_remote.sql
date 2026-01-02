7322996397ea40973d52e78bdb36737743c96cc8
Alexey Milovidov <milovidov@clickhouse.com>
2026-01-02 01:05:55 +0100

    Update 03781_merge_hostname_remote.sql

--- tests/queries/0_stateless/03781_merge_hostname_remote.sql
+++ tests/queries/0_stateless/03781_merge_hostname_remote.sql
@@ -6,7 +6,7 @@ INSERT INTO merge_host_remote_tab_a VALUES (1);
 
 CREATE TABLE merge_host_remote_tab_b AS remote('127.0.0.1', numbers(2));
 
-SET allow_experimental_analyzer = 1;
+SET enable_analyzer = 1;
 
 SELECT 'before';
 SELECT hostName(), * FROM merge(currentDatabase(), '^merge_host_remote_tab_') ORDER BY number FORMAT Null;
