a4a7f2320db2686d7d8d63c43a541a7c39238d3b
Bharat Nallan Chakravarthy <bharat@clickhouse.com>
2026-01-02 12:37:09 +0530

    fix

--- tests/queries/0_stateless/03733_validate_join_on_disk_max_files_to_merge.sql
+++
@@ -1,3 +0,0 @@
--- Fixes issue: https://github.com/ClickHouse/ClickHouse/issues/84668
-
-SELECT 1 FROM (SELECT 1 c0) tx JOIN (SELECT 1 c0) ty ON tx.c0 = ty.c0 SETTINGS default_max_bytes_in_join = 1, join_on_disk_max_files_to_merge = 18446744073709551615, join_algorithm = 'prefer_partial_merge';
