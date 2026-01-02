bb480a421b08a5ca881c463f666630d2cefd2fdf
pranavt84 <pranvat84@gmail.com>
2026-01-02 13:21:32 +0530

    Disabled this test in certain configurations.

--- tests/queries/0_stateless/03644_explain_indices.sql
+++ tests/queries/0_stateless/03644_explain_indices.sql
@@ -1,11 +1,14 @@
--- Tags: no-fasttest
+-- Tags: no-fasttest skip_msan skip_s3 skip_parallel skip_tsan no-parallel-replicas
 
-CREATE TABLE IF NOT EXISTS test_indexed
+
+DROP TABLE IF EXISTS test_indexed;
+
+CREATE TABLE test_indexed
 (
     id UInt32,
     value String
 ) ENGINE = MergeTree()
-    ORDER BY id;
+ORDER BY id;
 
 INSERT INTO test_indexed
 VALUES (1,'a'),(5,'b'),(10,'c'),(15,'d'),(20,'e');
@@ -14,3 +17,7 @@ EXPLAIN indices = 1
 SELECT *
 FROM test_indexed
 WHERE id = 5
+FORMAT TSVRaw;
+
+DROP TABLE test_indexed;
+
