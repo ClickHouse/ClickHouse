be3d131f4d8c5c20b26a99753f41b88a149f9d7f
Nihal Z. Miaji <81457724+nihalzp@users.noreply.github.com>
2026-01-02 19:10:30 +0800

    Update 03644_explain_indices.sql

--- tests/queries/0_stateless/03644_explain_indices.sql
+++ tests/queries/0_stateless/03644_explain_indices.sql
@@ -1,5 +1,6 @@
--- Tags: no-fasttest skip_msan skip_s3 skip_parallel skip_tsan no-parallel-replicas
-
+-- Tags: no-replicated-database, no-parallel-replicas
+-- no-replicated-database: EXPLAIN output differs for replicated database.
+-- no-parallel-replicas: EXPLAIN output differs for parallel replicas.
 
 DROP TABLE IF EXISTS test_indexed;
 
@@ -10,14 +11,11 @@ CREATE TABLE test_indexed
 ) ENGINE = MergeTree()
 ORDER BY id;
 
-INSERT INTO test_indexed
-VALUES (1,'a'),(5,'b'),(10,'c'),(15,'d'),(20,'e');
+INSERT INTO test_indexed VALUES (1,'a'),(5,'b'),(10,'c'),(15,'d'),(20,'e');
 
 EXPLAIN indices = 1
 SELECT *
 FROM test_indexed
-WHERE id = 5
-FORMAT TSVRaw;
+WHERE id = 5;
 
 DROP TABLE test_indexed;
-
