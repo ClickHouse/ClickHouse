4c4c5f29802e4376b42873c25f5a29514618b1e3
Azat Khuzhin <a3at.mail@gmail.com>
2026-01-03 18:11:32 +0100

    tests: optimize 01154_move_partition_long

--- tests/queries/0_stateless/01154_move_partition_long.sh
+++ tests/queries/0_stateless/01154_move_partition_long.sh
@@ -14,11 +14,11 @@ engines[1]="ReplicatedMergeTree('/test/$CLICKHOUSE_TEST_ZOOKEEPER_PREFIX/{shard}
 engines[2]="ReplicatedMergeTree('/test/$CLICKHOUSE_TEST_ZOOKEEPER_PREFIX/{shard}/src_' || toString(randConstant()), '{replica}')"
 
 for ((i=0; i<16; i++)) do
-    $CLICKHOUSE_CLIENT -q "CREATE TABLE dst_$i (p UInt64, k UInt64, v UInt64)
+    ${CLICKHOUSE_CURL} -sS "${CLICKHOUSE_URL}" -d "CREATE TABLE dst_$i (p UInt64, k UInt64, v UInt64)
           ENGINE=ReplicatedMergeTree('/test/$CLICKHOUSE_TEST_ZOOKEEPER_PREFIX/dst', '$i')
           PARTITION BY p % 10 ORDER BY k" 2>&1| grep -Pv "Retrying createReplica|created by another server at the same moment, will retry|is already started to be removing" 2>&1 &
     engine=${engines[$((i % ${#engines[@]}))]}
-    $CLICKHOUSE_CLIENT -q "CREATE TABLE src_$i (p UInt64, k UInt64, v UInt64) ENGINE=$engine
+    ${CLICKHOUSE_CURL} -sS "${CLICKHOUSE_URL}" -d "CREATE TABLE src_$i (p UInt64, k UInt64, v UInt64) ENGINE=$engine
           PARTITION BY p % 10 ORDER BY k" 2>&1| grep -Pv "Retrying createReplica|created by another server at the same moment, will retry|is already started to be removing" 2>&1 &
 done
 wait
@@ -42,7 +42,7 @@ function insert_thread()
     do
         REPLICA=$(($RANDOM % 16))
         LIMIT=$(($RANDOM % 100))
-        $CLICKHOUSE_CLIENT -q "INSERT INTO $1_$REPLICA SELECT * FROM generateRandom('p UInt64, k UInt64, v UInt64') LIMIT $LIMIT" 2>/dev/null
+        ${CLICKHOUSE_CURL} -sS "${CLICKHOUSE_URL}" -d "INSERT INTO $1_$REPLICA SELECT * FROM generateRandom('p UInt64, k UInt64, v UInt64') LIMIT $LIMIT" >&/dev/null
     done
 }
 
@@ -54,7 +54,7 @@ function move_partition_src_dst_thread()
         FROM_REPLICA=$(($RANDOM % 16))
         TO_REPLICA=$(($RANDOM % 16))
         PARTITION=$(($RANDOM % 10))
-        $CLICKHOUSE_CLIENT -q "ALTER TABLE src_$FROM_REPLICA MOVE PARTITION $PARTITION TO TABLE dst_$TO_REPLICA" 2>/dev/null
+        ${CLICKHOUSE_CURL} -sS "${CLICKHOUSE_URL}" -d "ALTER TABLE src_$FROM_REPLICA MOVE PARTITION $PARTITION TO TABLE dst_$TO_REPLICA" >&/dev/null
         sleep 0.$RANDOM;
     done
 }
@@ -67,7 +67,7 @@ function replace_partition_src_src_thread()
         FROM_REPLICA=$(($RANDOM % 16))
         TO_REPLICA=$(($RANDOM % 16))
         PARTITION=$(($RANDOM % 10))
-        $CLICKHOUSE_CLIENT -q "ALTER TABLE src_$TO_REPLICA REPLACE PARTITION $PARTITION FROM src_$FROM_REPLICA" 2>/dev/null
+        ${CLICKHOUSE_CURL} -sS "${CLICKHOUSE_URL}" -d "ALTER TABLE src_$TO_REPLICA REPLACE PARTITION $PARTITION FROM src_$FROM_REPLICA" >&/dev/null
         sleep 0.$RANDOM;
     done
 }
@@ -79,7 +79,7 @@ function drop_partition_thread()
     do
         REPLICA=$(($RANDOM % 16))
         PARTITION=$(($RANDOM % 10))
-        $CLICKHOUSE_CLIENT -q "ALTER TABLE dst_$REPLICA DROP PARTITION $PARTITION" 2>/dev/null
+        ${CLICKHOUSE_CURL} -sS "${CLICKHOUSE_URL}" -d "ALTER TABLE dst_$REPLICA DROP PARTITION $PARTITION" >&/dev/null
         sleep 0.$RANDOM;
     done
 }
@@ -94,7 +94,7 @@ function optimize_thread()
         if (( RANDOM % 2 )); then
             TABLE="dst"
         fi
-        $CLICKHOUSE_CLIENT -q "OPTIMIZE TABLE ${TABLE}_$REPLICA" 2>/dev/null
+        ${CLICKHOUSE_CURL} -sS "${CLICKHOUSE_URL}" -d "OPTIMIZE TABLE ${TABLE}_$REPLICA" >&/dev/null
         sleep 0.$RANDOM;
     done
 }
@@ -105,8 +105,8 @@ function drop_part_thread()
     while [ $SECONDS -lt "$TIMELIMIT" ]
     do
         REPLICA=$(($RANDOM % 16))
-        part=$($CLICKHOUSE_CLIENT -q "SELECT name FROM system.parts WHERE active AND database='$CLICKHOUSE_DATABASE' and table='dst_$REPLICA' ORDER BY rand() LIMIT 1")
-        $CLICKHOUSE_CLIENT -q "ALTER TABLE dst_$REPLICA DROP PART '$part'" 2>/dev/null
+        part=$(${CLICKHOUSE_CURL} -sS "${CLICKHOUSE_URL}" -d "SELECT name FROM system.parts WHERE active AND database='$CLICKHOUSE_DATABASE' and table='dst_$REPLICA' ORDER BY rand() LIMIT 1")
+        ${CLICKHOUSE_CURL} -sS "${CLICKHOUSE_URL}" -d "ALTER TABLE dst_$REPLICA DROP PART '$part'" >&/dev/null
         sleep 0.$RANDOM;
     done
 }
@@ -126,11 +126,11 @@ wait
 check_replication_consistency "dst_" "count(), sum(p), sum(k), sum(v)"
 try_sync_replicas "src_" 300
 
-$CLICKHOUSE_CLIENT -q "SELECT table, lost_part_count FROM system.replicas WHERE database=currentDatabase() AND lost_part_count!=0";
+${CLICKHOUSE_CURL} -sS "${CLICKHOUSE_URL}" -d "SELECT table, lost_part_count FROM system.replicas WHERE database=currentDatabase() AND lost_part_count!=0"
 
 for ((i=0; i<16; i++)) do
-    $CLICKHOUSE_CLIENT -q "DROP TABLE dst_$i" 2>&1| grep -Fv "is already started to be removing" &
-    $CLICKHOUSE_CLIENT -q "DROP TABLE IF EXISTS src_$i" 2>&1| grep -Fv "is already started to be removing" &
+    ${CLICKHOUSE_CURL} -sS "${CLICKHOUSE_URL}" -d "DROP TABLE dst_$i" 2>&1| grep -Fv "is already started to be removing" &
+    ${CLICKHOUSE_CURL} -sS "${CLICKHOUSE_URL}" -d "DROP TABLE IF EXISTS src_$i" 2>&1| grep -Fv "is already started to be removing" &
 done
 
 wait
