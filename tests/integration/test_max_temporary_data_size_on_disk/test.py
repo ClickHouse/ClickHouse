50f46c34cd2dd366d8b7d3e7078a3eb66ae985b5
Azat Khuzhin <a3at.mail@gmail.com>
2026-01-03 10:02:19 +0100

    tests: convert max_temporary_data_size_on_disk into integration test

---
+++ tests/integration/test_max_temporary_data_size_on_disk/test.py
@@ -0,0 +1,33 @@
+import pytest
+
+from helpers.cluster import ClickHouseCluster
+
+cluster = ClickHouseCluster(__file__)
+
+node = cluster.add_instance("node", main_configs=["configs/overrides.yaml"])
+
+
+@pytest.fixture(scope="module")
+def start_cluster():
+    try:
+        cluster.start()
+        yield cluster
+    except Exception as ex:
+        print(ex)
+    finally:
+        cluster.shutdown()
+
+
+def test(start_cluster):
+    try:
+        node.query(
+            f"""
+            DROP TABLE IF EXISTS t_proj_external;
+            CREATE TABLE t_proj_external (k1 UInt32, k2 UInt32, k3 UInt32, value UInt32) ENGINE = MergeTree ORDER BY tuple() SETTINGS index_granularity = 8192;
+            INSERT INTO t_proj_external SELECT 1, number%2, number%4, number FROM numbers(50000);
+            """
+        )
+        for i in range(10):
+            node.query("SELECT k1, k2, k3, sum(value) v FROM t_proj_external GROUP BY k1, k2, k3 ORDER BY k1, k2, k3 SETTINGS optimize_aggregation_in_order = 0, max_bytes_before_external_group_by = 1, max_bytes_ratio_before_external_group_by = 0, group_by_two_level_threshold = 1")
+    finally:
+        node.query("DROP TABLE IF EXISTS t_proj_external SYNC")
