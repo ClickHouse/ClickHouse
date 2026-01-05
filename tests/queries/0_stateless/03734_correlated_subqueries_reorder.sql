67755aa69e3992658f05e9cf3b8c9038fdd2d566
Dmitry Novik <n0vik@clickhouse.com>
2026-01-05 17:05:20 +0100

    Show JOIN order without optimizations

--- tests/queries/0_stateless/03734_correlated_subqueries_reorder.sql
+++ tests/queries/0_stateless/03734_correlated_subqueries_reorder.sql
@@ -58,3 +58,23 @@ WHERE
 )
 WHERE explain ilike '%ReadFrom%' or explain ilike '%JoinLogical%' or explain ilike '% Type: %' or explain ilike '%Save%';
 
+SET query_plan_optimize_join_order_limit = 0;
+SELECT '-- No join order optimization --';
+
+SELECT explain FROM (
+EXPLAIN actions = 1, keep_logical_steps = 1
+SELECT
+    sum(l_extendedprice) / 7.0 AS avg_yearly
+FROM
+    (SELECT l_quantity, p_partkey, l_extendedprice FROM lineitem, part WHERE p_partkey = l_partkey) AS lp
+WHERE
+    l_quantity < (
+        SELECT
+            0.2 * avg(l_quantity)
+        FROM
+            lineitem
+        WHERE
+            l_partkey = p_partkey
+    )
+)
+WHERE explain ilike '%ReadFrom%' or explain ilike '%JoinLogical%' or explain ilike '% Type: %' or explain ilike '%Save%';
