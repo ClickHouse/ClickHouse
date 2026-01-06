562db2b4477a46677bc324d2110cb0272534d325
Nikolai Kochetov <nk@clickhouse.com>
2026-01-06 18:16:59 +0000

    Better test.

--- tests/queries/0_stateless/03783_nested_sparse_serialization_bug.sh
+++ tests/queries/0_stateless/03783_nested_sparse_serialization_bug.sh
@@ -6,12 +6,11 @@ CUR_DIR=$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)
 
 cd "$CLICKHOUSE_TMP"
 
-$CLICKHOUSE_LOCAL -n <<'SQL'
-select (tuple(tuple(''), ''), tuple(tuple('a'), 'a')) union all select (tuple(tuple('a'), 'a'), tuple(tuple(''), '')) INTO OUTFILE '03783_nested_sparse_serialization_bug.jer'
-SQL
+
+$CLICKHOUSE_CLIENT --query="insert into function file('${CLICKHOUSE_TEST_UNIQUE_NAME}.jer', 'CSV') select (tuple(tuple(''), ''), tuple(tuple('a'), 'a')) union all select (tuple(tuple('a'), 'a'), tuple(tuple(''), ''))"
 
 $CLICKHOUSE_CLIENT --query="create table hmmm (a Tuple(b Tuple(c String), d String), e Tuple(f Tuple(g String), h String), t  DateTime DEFAULT now()) engine = MergeTree order by (e.h, a.d) ttl t + toIntervalDay(2) settings min_bytes_for_wide_part=1, ratio_of_defaults_for_sparse_serialization=0.01;"
 $CLICKHOUSE_CLIENT --query="insert into hmmm (a, e) values (tuple(tuple(''), ''), tuple(tuple('a'), 'a')), (tuple(tuple('a'), 'a'), tuple(tuple(''), ''));"
-$CLICKHOUSE_CLIENT --query="insert into hmmm (a, e) select * FROM file('t.jer', 'CSV')"
+$CLICKHOUSE_CLIENT --query="insert into hmmm (a, e) select * FROM file('${CLICKHOUSE_TEST_UNIQUE_NAME}.jer', 'CSV')"
 
 rm -f 03783_nested_sparse_serialization_bug.jer
