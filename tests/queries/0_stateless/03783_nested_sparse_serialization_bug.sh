4e05565bb47509e8ec02ca36421ab60ec5c798d5
Nikolai Kochetov <nk@clickhouse.com>
2026-01-06 17:51:16 +0000

    Add a test with nested sparse serialization bug.

---
+++ tests/queries/0_stateless/03783_nested_sparse_serialization_bug.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env bash
+
+CUR_DIR=$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)
+# shellcheck source=../shell_config.sh
+. "$CUR_DIR"/../shell_config.sh
+
+cd "$CLICKHOUSE_TMP"
+
+$CLICKHOUSE_LOCAL -n <<'SQL'
+select (tuple(tuple(''), ''), tuple(tuple('a'), 'a')) union all select (tuple(tuple('a'), 'a'), tuple(tuple(''), '')) INTO OUTFILE '03783_nested_sparse_serialization_bug.jer'
+SQL
+
+$CLICKHOUSE_CLIENT --query="create table hmmm (a Tuple(b Tuple(c String), d String), e Tuple(f Tuple(g String), h String), t  DateTime DEFAULT now()) engine = MergeTree order by (e.h, a.d) ttl t + toIntervalDay(2) settings min_bytes_for_wide_part=1, ratio_of_defaults_for_sparse_serialization=0.01;"
+$CLICKHOUSE_CLIENT --query="insert into hmmm (a, e) values (tuple(tuple(''), ''), tuple(tuple('a'), 'a')), (tuple(tuple('a'), 'a'), tuple(tuple(''), ''));"
+$CLICKHOUSE_CLIENT --query="insert into hmmm (a, e) select * FROM file('t.jer', 'CSV')"
+
+rm -f 03783_nested_sparse_serialization_bug.jer
