a30c8cb87e6132975f1930e4a3233c02254e34cf
pranavt84 <pranvat84@gmail.com>
2026-01-06 21:15:35 +0530

    Fixed failing test case.

--- tests/queries/0_stateless/03780_srgbToOklab_and_oklabToSrgb.sql
+++ tests/queries/0_stateless/03780_srgbToOklab_and_oklabToSrgb.sql
@@ -1,6 +1,14 @@
 
-WITH (127,66,37) AS rgb
+WITH (127, 66, 37) AS rgb
 SELECT
-    rgb AS original,
-    colorSRGBToOKLAB(rgb, 2.2) AS oklab,
-    colorOKLABToSRGB(colorSRGBToOKLAB(rgb, 2.2), 2.2) AS roundtrip;
+rgb AS original,
+(
+ round(colorSRGBToOKLAB(rgb, 2.2).1, 6),
+ round(colorSRGBToOKLAB(rgb, 2.2).2, 6),
+ round(colorSRGBToOKLAB(rgb, 2.2).3, 6)
+) AS oklab,
+(
+ round(colorOKLABToSRGB(colorSRGBToOKLAB(rgb, 2.2), 2.2).1, 3),
+ round(colorOKLABToSRGB(colorSRGBToOKLAB(rgb, 2.2), 2.2).2, 3),
+ round(colorOKLABToSRGB(colorSRGBToOKLAB(rgb, 2.2), 2.2).3, 3)
+) AS roundtrip;
