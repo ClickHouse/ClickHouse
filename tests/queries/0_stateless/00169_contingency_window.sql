22c75e8a577692817c6c39dc9d48331e9211c287
Nihal Z. Miaji <81457724+nihalzp@users.noreply.github.com>
2026-01-06 18:11:07 +0000

    Make test run faster

--- tests/queries/0_stateless/00169_contingency_window.sql
+++ tests/queries/0_stateless/00169_contingency_window.sql
@@ -1,17 +1,19 @@
--- Tags: stateful, long, no-asan, no-tsan, no-ubsan, no-msan
--- no-asan, no-tsan, no-ubsan, no-msan: Too slow
+-- Tags: stateful
 
 WITH URLDomain AS a, URLDomain AS b
 SELECT round(cramersVWindow(a, b), 2), round(cramersVBiasCorrectedWindow(a, b), 2), round(theilsUWindow(a, b), 2), round(theilsUWindow(b, a), 2), round(contingencyWindow(a, b), 2) FROM test.hits;
 
-WITH URLDomain AS a, RefererDomain AS b
-SELECT round(cramersVWindow(a, b), 2), round(cramersVBiasCorrectedWindow(a, b), 2), round(theilsUWindow(a, b), 2), round(theilsUWindow(b, a), 2), round(contingencyWindow(a, b), 2) FROM test.hits;
+WITH URLDomain AS a, RefererDomain AS b, cityHash64(WatchID) AS sample_hash
+SELECT round(cramersVWindow(a, b), 2), round(cramersVBiasCorrectedWindow(a, b), 2), round(theilsUWindow(a, b), 2), round(theilsUWindow(b, a), 2), round(contingencyWindow(a, b), 2)
+FROM test.hits WHERE sample_hash % 10 = 0;
 
-WITH URLDomain AS a, CounterID AS b
-SELECT round(cramersVWindow(a, b), 2), round(cramersVBiasCorrectedWindow(a, b), 2), round(theilsUWindow(a, b), 2), round(theilsUWindow(b, a), 2), round(contingencyWindow(a, b), 2) FROM test.hits;
+WITH URLDomain AS a, CounterID AS b, cityHash64(WatchID) AS sample_hash
+SELECT round(cramersVWindow(a, b), 2), round(cramersVBiasCorrectedWindow(a, b), 2), round(theilsUWindow(a, b), 2), round(theilsUWindow(b, a), 2), round(contingencyWindow(a, b), 2)
+FROM test.hits WHERE sample_hash % 10 = 0;
 
-WITH ClientIP AS a, RemoteIP AS b
-SELECT round(cramersVWindow(a, b), 2), round(cramersVBiasCorrectedWindow(a, b), 2), round(theilsUWindow(a, b), 2), round(theilsUWindow(b, a), 2), round(contingencyWindow(a, b), 2) FROM test.hits;
+WITH ClientIP AS a, RemoteIP AS b, cityHash64(WatchID) AS sample_hash
+SELECT round(cramersVWindow(a, b), 2), round(cramersVBiasCorrectedWindow(a, b), 2), round(theilsUWindow(a, b), 2), round(theilsUWindow(b, a), 2), round(contingencyWindow(a, b), 2)
+FROM test.hits WHERE sample_hash % 10 = 0;
 
 WITH ResolutionWidth AS a, ResolutionHeight AS b
 SELECT round(cramersVWindow(a, b), 2), round(cramersVBiasCorrectedWindow(a, b), 2), round(theilsUWindow(a, b), 2), round(theilsUWindow(b, a), 2), round(contingencyWindow(a, b), 2) FROM test.hits;
