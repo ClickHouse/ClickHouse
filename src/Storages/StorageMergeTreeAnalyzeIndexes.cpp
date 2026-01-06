c7907187896c23f0dc84dec3e30ddf4d529315da
Azat Khuzhin <a3at.mail@gmail.com>
2026-01-06 12:38:43 +0100

    mergeTreeAnalyzeIndexes: sets with subqueries support (key IN (SELECT))

--- src/Storages/StorageMergeTreeAnalyzeIndexes.cpp
+++ src/Storages/StorageMergeTreeAnalyzeIndexes.cpp
@@ -5,6 +5,7 @@
 #include <Core/Field.h>
 #include <Planner/CollectSets.h>
 #include <Planner/CollectTableExpressionData.h>
+#include <Planner/Planner.h>
 #include <Planner/PlannerContext.h>
 #include <Planner/Utils.h>
 #include <Processors/QueryPlan/ReadFromMergeTree.h>
@@ -145,6 +146,21 @@ protected:
                 planner_context,
                 empty_correlated_columns_set);
             correlated_subtrees.assertEmpty("in constant expression without query context");
+
+            auto subquery_options = SelectQueryOptions{}.subquery();
+            subquery_options.ignore_limits = false;
+            for (auto & subquery : planner_context->getPreparedSets().getSubqueries())
+            {
+                auto query_tree = subquery->detachQueryTree();
+                Planner subquery_planner(
+                    query_tree,
+                    subquery_options,
+                    std::make_shared<GlobalPlannerContext>(nullptr, nullptr, FiltersForTableExpressionMap{}));
+                subquery_planner.buildQueryPlanIfNeeded();
+
+                auto subquery_plan = std::move(subquery_planner).extractQueryPlan();
+                subquery->setQueryPlan(std::make_unique<QueryPlan>(std::move(subquery_plan)));
+            }
 
             filter_dag.emplace(std::move(actions));
         }
