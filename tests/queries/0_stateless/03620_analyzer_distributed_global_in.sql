31818cbce4aae0e0d3556452dcaddf46408e963e
Azat Khuzhin <a3at.mail@gmail.com>
2026-01-07 08:56:29 +0100

    Improve 03620_analyzer_distributed_global_in test

--- tests/queries/0_stateless/03620_analyzer_distributed_global_in.sql
+++ tests/queries/0_stateless/03620_analyzer_distributed_global_in.sql
@@ -10,6 +10,10 @@ select sum(y) from (select * from remote('127.0.0.1', currentDatabase(), tab0)) 
 select sum(y) from (select * from remote('127.0.0.1', currentDatabase(), tab0)) where x global in (select number + 42 from numbers(1));
 select sum(y) from (select * from remote('127.0.0.2', currentDatabase(), tab0)) where x in (select number + 42 from numbers(1));
 select sum(y) from (select * from remote('127.0.0.2', currentDatabase(), tab0)) where x global in (select number + 42 from numbers(1));
+select sum(y) from (select * from remote('127.0.0.{1,2}', currentDatabase(), tab0)) where x in (select number + 42 from numbers(1));
+select sum(y) from (select * from remote('127.0.0.{1,2}', currentDatabase(), tab0)) where x global in (select number + 42 from numbers(1));
+select sum(y) from (select * from remote('127.0.0.{2,3}', currentDatabase(), tab0)) where x in (select number + 42 from numbers(1));
+select sum(y) from (select * from remote('127.0.0.{2,3}', currentDatabase(), tab0)) where x global in (select number + 42 from numbers(1));
 select * from (explain indexes=1
     select sum(y) from (select * from remote('127.0.0.1', currentDatabase(), tab0)) where x global in (select number + 42 from numbers(1))
 );
