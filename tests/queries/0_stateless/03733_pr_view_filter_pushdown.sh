213edb4c4bb080ecd2e3f41cd056e1c468bd99df
Igor Nikonov <igor@clickhouse.com>
2026-01-08 16:56:07 +0100

    Update test

--- tests/queries/0_stateless/03733_pr_view_filter_pushdown.sh
+++ tests/queries/0_stateless/03733_pr_view_filter_pushdown.sh
@@ -6,6 +6,8 @@ CUR_DIR=$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)
 
 CLICKHOUSE_CLIENT_TRACE=${CLICKHOUSE_CLIENT/"--send_logs_level=${CLICKHOUSE_CLIENT_SERVER_LOGS_LEVEL}"/"--send_logs_level=trace"}
 
+PARALLEL_REPLICAS_SETTINGS="enable_parallel_replicas=1, max_parallel_replicas=2, cluster_for_parallel_replicas='test_cluster_one_shard_three_replicas_localhost', parallel_replicas_for_non_replicated_merge_tree=1"
+
 $CLICKHOUSE_CLIENT --query "
 drop table if exists t_03733;
 drop table if exists v_03733;
@@ -13,14 +15,12 @@ create table t_03733(a UInt32, b String) engine=MergeTree order by ();
 create view v_03733 as select * from t_03733;
 insert into t_03733 select number, toString(number) from numbers(10);
 "
-PARALLEL_REPLICAS_SETTINGS="enable_parallel_replicas=1, max_parallel_replicas=2, cluster_for_parallel_replicas='test_cluster_one_shard_three_replicas_localhost', parallel_replicas_for_non_replicated_merge_tree=1"
 
 # check filter push down for remote
 $CLICKHOUSE_CLIENT_TRACE --query "
 SET ${PARALLEL_REPLICAS_SETTINGS};
 SELECT * FROM v_03733 WHERE a = 0 SETTINGS parallel_replicas_local_plan=0;
 " |& grep 'executeQuery' | grep 'HAVING' | wc -l;
-
 # check filter pushdown for local replica
 $CLICKHOUSE_CLIENT --query "
 SET ${PARALLEL_REPLICAS_SETTINGS};
@@ -38,7 +38,6 @@ $CLICKHOUSE_CLIENT_TRACE --query "
 SET ${PARALLEL_REPLICAS_SETTINGS};
 SELECT * FROM vv_03733 WHERE a = 0 SETTINGS parallel_replicas_local_plan=0;
 " |& grep 'executeQuery' | grep 'HAVING' | wc -l;
-
 # check filter pushdown for local replica
 $CLICKHOUSE_CLIENT --query "
 SET ${PARALLEL_REPLICAS_SETTINGS};
@@ -56,3 +55,9 @@ $CLICKHOUSE_CLIENT_TRACE --query "
 SET ${PARALLEL_REPLICAS_SETTINGS};
 SELECT * FROM v1_03733 WHERE c = 0 SETTINGS parallel_replicas_local_plan=0;
 " |& grep 'executeQuery' | grep 'HAVING' | wc -l;
+# check filter pushdown for local replica
+$CLICKHOUSE_CLIENT --query "
+SET ${PARALLEL_REPLICAS_SETTINGS};
+select trimLeft(explain) from
+  (explain description=0, actions=1 select * from vv_03733 where a = 0 settings parallel_replicas_local_plan=1)
+where explain ilike '%Prewhere%' limit 1;"
