6a0971b33c4a8786b34e099bfbfaaa5795265170
Azat Khuzhin <a3at.mail@gmail.com>
2026-01-08 22:04:17 +0100

    tests: more stable 03620_mergeTreeAnalyzeIndexes_sets
    CI: https://s3.amazonaws.com/clickhouse-test-reports/json.html?PR=86786&sha=27233b7e7855bbe02f02b51320ff1217584cd459&name_0=PR&name_1=Stateless%20tests%20%28amd_asan%2C%20flaky%20check%29

--- tests/queries/0_stateless/03620_mergeTreeAnalyzeIndexes_sets.sql
+++ tests/queries/0_stateless/03620_mergeTreeAnalyzeIndexes_sets.sql
@@ -1,5 +1,5 @@
 drop table if exists data;
-create table data (key Int, value Int) engine=MergeTree() order by key settings merge_selector_base = 1000, index_granularity=8192, min_bytes_for_wide_part=1e9 as select *, *+1000000 from numbers(100000);
+create table data (key Int, value Int) engine=MergeTree() order by key settings merge_selector_base = 1000, index_granularity=8192, index_granularity_bytes=10e9, min_bytes_for_wide_part=1e9 as select *, *+1000000 from numbers(100000) settings max_insert_threads=1;
 
 drop table if exists keys_1;
 create table keys_1 (key Int) engine=Log() as select * from numbers(10);
