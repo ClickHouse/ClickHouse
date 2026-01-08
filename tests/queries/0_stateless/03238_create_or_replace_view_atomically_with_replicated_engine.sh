f67e1ea6d79fba8efa80243d7afac130b674180b
Alexey Milovidov <milovidov@clickhouse.com>
2026-01-08 08:14:29 +0100

    Fix bad test

--- tests/queries/0_stateless/03238_create_or_replace_view_atomically_with_replicated_engine.sh
+++ tests/queries/0_stateless/03238_create_or_replace_view_atomically_with_replicated_engine.sh
@@ -13,7 +13,6 @@ function create_or_replace_view_thread
         $CLICKHOUSE_CLIENT --query "CREATE OR REPLACE VIEW ${CLICKHOUSE_DATABASE}_db.test_view AS SELECT 'abcdef'" > /dev/null
     done
 }
-export -f create_or_replace_view_thread;
 
 function select_view_thread
 {
@@ -21,32 +20,31 @@ function select_view_thread
         $CLICKHOUSE_CLIENT --query "SELECT * FROM ${CLICKHOUSE_DATABASE}_db.test_view" > /dev/null
     done
 }
-export -f select_view_thread;
 
 $CLICKHOUSE_CLIENT --query "CREATE OR REPLACE VIEW ${CLICKHOUSE_DATABASE}_db.test_view AS SELECT 'abcdef'" > /dev/null
 
-bash -c select_view_thread &
-bash -c select_view_thread &
-bash -c select_view_thread &
-bash -c select_view_thread &
-bash -c select_view_thread &
-bash -c select_view_thread &
-bash -c select_view_thread &
-bash -c select_view_thread &
-bash -c select_view_thread &
-bash -c select_view_thread &
-bash -c select_view_thread &
-bash -c select_view_thread &
+select_view_thread &
+select_view_thread &
+select_view_thread &
+select_view_thread &
+select_view_thread &
+select_view_thread &
+select_view_thread &
+select_view_thread &
+select_view_thread &
+select_view_thread &
+select_view_thread &
+select_view_thread &
 
-bash -c create_or_replace_view_thread &
-bash -c create_or_replace_view_thread &
-bash -c create_or_replace_view_thread &
-bash -c create_or_replace_view_thread &
-bash -c create_or_replace_view_thread &
-bash -c create_or_replace_view_thread &
-bash -c create_or_replace_view_thread &
-bash -c create_or_replace_view_thread &
-bash -c create_or_replace_view_thread &
-bash -c create_or_replace_view_thread &
+create_or_replace_view_thread &
+create_or_replace_view_thread &
+create_or_replace_view_thread &
+create_or_replace_view_thread &
+create_or_replace_view_thread &
+create_or_replace_view_thread &
+create_or_replace_view_thread &
+create_or_replace_view_thread &
+create_or_replace_view_thread &
+create_or_replace_view_thread &
 
 wait
