fb3135f3137054324a3334579fad5a05fd797409
Alexey Milovidov <milovidov@clickhouse.com>
2026-01-08 03:39:10 +0100

    Maybe this will be ok

--- ci/defs/defs.py
+++ ci/defs/defs.py
@@ -133,12 +133,6 @@ DOCKERS = [
         depends_on=["clickhouse/test-base"],
     ),
     Docker.Config(
-        name="clickhouse/performance-comparison",
-        path="./ci/docker/performance-comparison",
-        platforms=Docker.Platforms.arm_amd,
-        depends_on=["clickhouse/test-base"],
-    ),
-    Docker.Config(
         name="clickhouse/keeper-jepsen-test",
         path="./ci/docker/keeper-jepsen-test",
         platforms=Docker.Platforms.arm_amd,
