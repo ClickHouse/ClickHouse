48df365371082b12d651b090bde4a4f2b485debe
George Larionov <george.larionov@clickhouse.com>
2026-01-08 00:23:16 +0000

    fixing AST inconsistency with SELECT and EXCEPT

--- tests/queries/0_stateless/03780_AST_formatting_inconsistencies_in_debug_check.sql
+++ tests/queries/0_stateless/03780_AST_formatting_inconsistencies_in_debug_check.sql
@@ -1,6 +1,6 @@
 SET enable_analyzer = 1;
 
-SELECT 'These tests just need to run, dont care about output';
+--These tests just need to run, dont care about output
 SELECT 'Test repeated alias for literal as 2nd arg to IN operator:';
 SELECT ([1] AS foo), [1] IN ([1] AS foo);
 
@@ -22,7 +22,7 @@ SELECT 'Test repeated alias in subquery after NOT:';
 SELECT ((SELECT 1) AS a1), NOT ((SELECT 1) AS a1);
 
 SELECT 'Test repeated alias for tuple after IN:';
-select tuple(1, 'a') as a1, tuple(1, 'a') IN (tuple(1, 'a') as a1);
+SELECT tuple(1, 'a') AS a1, tuple(1, 'a') IN (tuple(1, 'a') AS a1);
 
 SELECT 'Alias in ON clause of JOIN:';
 CREATE TABLE t1 (x int);
@@ -31,4 +31,14 @@ SELECT * FROM t1 JOIN t2 ON ((t1.x = t2.x) AND (t1.x IS NULL) AS e2);
 DROP TABLE t1, t2;
 
 SELECT 'Test repeated alias for tuple after NOT fails cleanly:';
-select tuple(1, 'a') as a1, not (tuple(1, 'a') as a1); -- { serverError ILLEGAL_TYPE_OF_ARGUMENT }
+SELECT tuple(1, 'a') AS a1, NOT (tuple(1, 'a') AS a1); -- { serverError ILLEGAL_TYPE_OF_ARGUMENT }
+
+SELECT 'Test that INSERT INTO with EXCEPT does not crash debug build:';
+CREATE TABLE tab (c1 int, c2 int, c3 int);
+CREATE TABLE tab2 (c1 int, c2 int, c3 int);
+INSERT INTO tab2 SELECT * FROM tab EXCEPT SELECT * FROM tab;
+DROP TABLE tab;
+DROP TABLE tab2;
+
+SELECT 'Test weird SELECT/EXCEPT/SELECT statement:';
+SELECT 1,2,3 EXCEPT SELECT 1,2,3;
