1cdca20f5fb86f70db79c32867d5f6661601ecc1
Igor Nikonov <igor@clickhouse.com>
2026-01-09 22:26:07 +0100

    Fix

--- src/Interpreters/ClusterProxy/executeQuery.cpp
+++ src/Interpreters/ClusterProxy/executeQuery.cpp
@@ -704,7 +704,7 @@ void executeQueryWithParallelReplicas(
             return;
         }
 
-        auto remote_query_plan = std::make_shared<const QueryPlan>();
+        std::shared_ptr<const QueryPlan> remote_query_plan;
         if (new_context->getSettingsRef()[Setting::serialize_query_plan])
             remote_query_plan = createRemotePlanForParallelReplicas(query_ast, * header, new_context, processed_stage);
 
