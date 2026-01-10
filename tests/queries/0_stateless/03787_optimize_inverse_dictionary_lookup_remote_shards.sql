8d6cd5dbd87249a1ad8b8ad9b3d6a040859f448b
Nihal Z. Miaji <81457724+nihalzp@users.noreply.github.com>
2026-01-10 09:45:22 +0000

    Add similar test for composite keys

--- tests/queries/0_stateless/03787_optimize_inverse_dictionary_lookup_remote_shards.sql
+++ tests/queries/0_stateless/03787_optimize_inverse_dictionary_lookup_remote_shards.sql
@@ -27,3 +27,27 @@ FROM remote('localhost,localhost', system.one)
 LIMIT 1;
 
 DROP DICTIONARY IF EXISTS inverse_dict_lookup_remote_shards;
+
+DROP DICTIONARY IF EXISTS inverse_dict_lookup_remote_shards_composite_key;
+CREATE DICTIONARY inverse_dict_lookup_remote_shards_composite_key
+(
+  k1 Int64,
+  k2 UInt32,
+  f  Int64
+)
+PRIMARY KEY k1, k2
+SOURCE(CLICKHOUSE(QUERY 'SELECT toInt64(1) k1, toUInt32(1) k2, toInt64(2) f'))
+LAYOUT(hashed())
+LIFETIME(0);
+
+SET optimize_inverse_dictionary_lookup = 0;
+SELECT dictGet('inverse_dict_lookup_remote_shards_composite_key', 'f', tuple(toInt64(dummy), toUInt32(dummy))) = 12 AS limit_and_equals
+FROM remote('localhost,localhost', system.one)
+LIMIT 1;
+
+SET optimize_inverse_dictionary_lookup = 1;
+SELECT dictGet('inverse_dict_lookup_remote_shards_composite_key', 'f', tuple(toInt64(dummy), toUInt32(dummy))) = 12 AS limit_and_equals
+FROM remote('localhost,localhost', system.one)
+LIMIT 1;
+
+DROP DICTIONARY IF EXISTS inverse_dict_lookup_remote_shards_composite_key;
