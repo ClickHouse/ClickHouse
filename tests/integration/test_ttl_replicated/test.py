bea826a507cfd9062933a8dbc2c76eb9eff986f8
Alexey Milovidov <milovidov@clickhouse.com>
2026-01-11 18:02:25 +0100

    Something

--- tests/integration/test_ttl_replicated/test.py
+++ tests/integration/test_ttl_replicated/test.py
@@ -546,10 +546,11 @@ def test_ttl_compatibility(started_cluster, node_left, node_right, num_run):
     for node in [node_left, node_right]:
         node.query(
             """
+                DROP TABLE IF EXISTS {table}_delete SYNC;
                 CREATE TABLE {table}_delete(date DateTime, id UInt32)
                 ENGINE = ReplicatedMergeTree('/clickhouse/tables/test/{table}_delete', '{replica}')
                 ORDER BY id PARTITION BY toDayOfMonth(date)
-                TTL date + INTERVAL 3 SECOND
+                TTL date + INTERVAL 3 SECOND;
             """.format(
                 table=table, replica=node.name
             )
@@ -557,10 +558,11 @@ def test_ttl_compatibility(started_cluster, node_left, node_right, num_run):
 
         node.query(
             """
+                DROP TABLE IF EXISTS {table}_group_by SYNC;
                 CREATE TABLE {table}_group_by(date DateTime, id UInt32, val UInt64)
                 ENGINE = ReplicatedMergeTree('/clickhouse/tables/test/{table}_group_by', '{replica}')
                 ORDER BY id PARTITION BY toDayOfMonth(date)
-                TTL date + INTERVAL 3 SECOND GROUP BY id SET val = sum(val)
+                TTL date + INTERVAL 3 SECOND GROUP BY id SET val = sum(val);
             """.format(
                 table=table, replica=node.name
             )
@@ -568,10 +570,11 @@ def test_ttl_compatibility(started_cluster, node_left, node_right, num_run):
 
         node.query(
             """
+                DROP TABLE IF EXISTS {table}_where SYNC;
                 CREATE TABLE {table}_where(date DateTime, id UInt32)
                 ENGINE = ReplicatedMergeTree('/clickhouse/tables/test/{table}_where', '{replica}')
                 ORDER BY id PARTITION BY toDayOfMonth(date)
-                TTL date + INTERVAL 3 SECOND DELETE WHERE id % 2 = 1
+                TTL date + INTERVAL 3 SECOND DELETE WHERE id % 2 = 1;
             """.format(
                 table=table, replica=node.name
             )
