863549b0898b30087341b75526546d5910fb1325
Antonio Andelic <antonio@clickhouse.com>
2026-01-12 12:26:18 +0100

    Fix system.zookeeper_info

--- src/Storages/ObjectStorageQueue/ObjectStorageQueueMetadata.cpp
+++ src/Storages/ObjectStorageQueue/ObjectStorageQueueMetadata.cpp
@@ -623,7 +623,7 @@ namespace
             return buf.str();
         }
 
-        static Info deserialize(const std::string & str)
+        static Info deserialize(std::string_view str)
         {
             ReadBufferFromString buf(str);
             Info info;
@@ -684,7 +684,7 @@ void ObjectStorageQueueMetadata::registerNonActive(const StorageID & storage_id,
             bool registry_exists = zk_client->tryGet(registry_path, registry_str, &stat);
             if (registry_exists)
             {
-                Strings registered;
+                std::vector<std::string_view> registered;
                 splitInto<','>(registered, registry_str);
 
                 if (zk_retries.isRetry() && registered.size() == 1 && (Info::deserialize(registered[0]) == self))
@@ -697,7 +697,7 @@ void ObjectStorageQueueMetadata::registerNonActive(const StorageID & storage_id,
 
                 created_new_metadata = false;
 
-                for (const auto & elem : registered)
+                for (auto elem : registered)
                 {
                     if (elem.empty())
                         continue;
@@ -840,7 +840,7 @@ void ObjectStorageQueueMetadata::unregisterNonActive(const StorageID & storage_i
                 return;
             }
 
-            Strings registered;
+            std::vector<std::string_view> registered;
             splitInto<','>(registered, registry_str);
 
             bool found = false;
