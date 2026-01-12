2d4fcff34c75cc807a5975a3ac73b171c65debcf
Mikhail Artemenko <mikhail.artemenko@clickhouse.com>
2026-01-12 12:09:10 +0000

    better fix

--- src/Common/ZooKeeper/IKeeper.h
+++ src/Common/ZooKeeper/IKeeper.h
@@ -552,7 +552,7 @@ struct CheckRequest : virtual Request
     void addRootPath(const String & root_path) override;
     String getPath() const override { return path; }
 
-    size_t bytesSize() const override { return path.size() + sizeof(version) + (stat_to_check.has_value() ? sizeof(Stat) : 0); }
+    size_t bytesSize() const override { return path.size() + sizeof(version) + sizeof(stat_to_check); }
 };
 
 struct CheckResponse : virtual Response
