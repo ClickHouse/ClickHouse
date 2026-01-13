e8e442b148331d37adcced6216138d7a47704e53
Alexey Milovidov <milovidov@clickhouse.com>
2026-01-13 15:30:15 +0800

    Add strong negative test for GROUPING SETS optimization

--- tests/queries/0_stateless/02815_analyzer_aggregate_functions_of_group_by_keys.sql
+++ tests/queries/0_stateless/02815_analyzer_aggregate_functions_of_group_by_keys.sql
@@ -32,6 +32,17 @@ EXPLAIN QUERY TREE SELECT min(number % 2) AS a FROM numbers(100) GROUP BY GROUPI
 EXPLAIN QUERY TREE SELECT max(number % 2) AS a, min(number % 3) AS b FROM numbers(100) GROUP BY GROUPING SETS ((number % 2, number % 3), (number % 2, number % 3, number % 5)) ORDER BY a, b;
 EXPLAIN QUERY TREE SELECT max(number % 2) AS a FROM numbers(100) GROUP BY GROUPING SETS ((number % 2), (number % 3)) ORDER BY a;
 
+SELECT 'GROUPING SETS negative test: b is not in all sets, so max(b) should NOT be eliminated';
+DROP TABLE IF EXISTS test_grouping_sets;
+CREATE TABLE test_grouping_sets (a UInt8, b UInt8) ENGINE = Memory;
+INSERT INTO test_grouping_sets SELECT
+    number % 2 + 1 AS a,
+    (number % 5 + 1) * 10 + (number % 2) * 50 AS b
+FROM numbers(1000);
+SELECT a, max(b) AS mb FROM test_grouping_sets GROUP BY GROUPING SETS ((a, b), (a)) ORDER BY a, mb;
+EXPLAIN QUERY TREE SELECT a, max(b) AS mb FROM test_grouping_sets GROUP BY GROUPING SETS ((a, b), (a)) ORDER BY a, mb;
+DROP TABLE test_grouping_sets;
+
 SELECT 'set optimize_aggregators_of_group_by_keys = 0';
 set optimize_aggregators_of_group_by_keys = 0;
 
