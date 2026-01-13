749a49a6a4e13484c5fd82a7b88a145b57da97a8
robot-clickhouse <robot-clickhouse@users.noreply.github.com>
2026-01-13 13:27:53 +0000

    Backport #93035 to 25.10: More respect to memory limit from `AggregateFunctionWindowFunnel`

--- src/Storages/FileLog/FileLogConsumer.h
+++ src/Storages/FileLog/FileLogConsumer.h
@@ -4,7 +4,7 @@
 #include <IO/ReadBuffer.h>
 #include <Storages/FileLog/StorageFileLog.h>
 #include <Common/AllocatorWithMemoryTracking.h>
-#include <Common/TrackedString.h>
+#include <Common/StrictString.h>
 
 namespace DB
 {
@@ -57,7 +57,7 @@ private:
 
     struct Record
     {
-        TrackedString data;
+        StrictString data;
         std::string file_name;
         /// Offset is the start of a row, which is needed for virtual columns.
         UInt64 offset;
