70c6970a2a0a5d36226e7c7657f044f2016ad36d
Fgrtue <kirill.kopnev@clickhouse.com>
2026-01-14 22:12:21 +0000

    Removed Parquet test

--- tests/queries/0_stateless/03781_insert_block_size_settings_squashing_on_insert.sh
+++ tests/queries/0_stateless/03781_insert_block_size_settings_squashing_on_insert.sh
@@ -15,12 +15,10 @@ CUR_DIR=$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)
 $CLICKHOUSE_CLIENT -q "DROP TABLE IF EXISTS test_native_max_rows"
 $CLICKHOUSE_CLIENT -q "DROP TABLE IF EXISTS test_native_max_bytes"
 $CLICKHOUSE_CLIENT -q "DROP TABLE IF EXISTS test_native_min_squash"
-$CLICKHOUSE_CLIENT -q "DROP TABLE IF EXISTS test_parquet_max_rows"
 
 $CLICKHOUSE_CLIENT -q "CREATE TABLE test_native_max_rows (id UInt64) ENGINE = MergeTree() ORDER BY id"
 $CLICKHOUSE_CLIENT -q "CREATE TABLE test_native_max_bytes (id UInt64) ENGINE = MergeTree() ORDER BY id"
 $CLICKHOUSE_CLIENT -q "CREATE TABLE test_native_min_squash (id UInt64) ENGINE = MergeTree() ORDER BY id"
-$CLICKHOUSE_CLIENT -q "CREATE TABLE test_parquet_max_rows (id UInt64) ENGINE = MergeTree() ORDER BY id"
 
 $CLICKHOUSE_CLIENT -q "SELECT number FROM numbers(100) FORMAT Native" | \
 $CLICKHOUSE_CLIENT \
@@ -48,16 +46,6 @@ $CLICKHOUSE_CLIENT \
     --min_insert_block_size_bytes=8 \
     --use_strict_insert_block_limits=1 \
     -q "INSERT INTO test_native_min_squash FORMAT Native"
-
-
-$CLICKHOUSE_CLIENT -q "SELECT number FROM numbers(100) FORMAT Parquet" | \
-$CLICKHOUSE_CLIENT \
-    --max_insert_block_size_rows=32 \
-    --max_insert_block_size_bytes=0 \
-    --min_insert_block_size_rows=0 \
-    --min_insert_block_size_bytes=0 \
-    --use_strict_insert_block_limits=1 \
-    -q "INSERT INTO test_parquet_max_rows FORMAT Parquet"
 
 $CLICKHOUSE_CLIENT -q "SYSTEM FLUSH LOGS query_log, part_log;"
 
@@ -106,27 +94,10 @@ AND (query_id = (
 ));
 "
 
-# Test 4: Expect 4 parts (ceil(100 / 32) = 4)
-$CLICKHOUSE_CLIENT -q "
-SELECT count()
-FROM system.part_log
-WHERE table = 'test_parquet_max_rows'
-AND event_type = 'NewPart' 
-AND database = currentDatabase()
-AND (query_id = (
-    SELECT argMax(query_id, event_time) 
-    FROM system.query_log 
-    WHERE query LIKE '%INSERT INTO test_parquet_max_rows FORMAT Parquet%' 
-    AND current_database = currentDatabase() 
-));
-"
-
 $CLICKHOUSE_CLIENT -q "SELECT count() FROM test_native_max_rows"
 $CLICKHOUSE_CLIENT -q "SELECT count() FROM test_native_max_bytes"
 $CLICKHOUSE_CLIENT -q "SELECT count() FROM test_native_min_squash"
-$CLICKHOUSE_CLIENT -q "SELECT count() FROM test_parquet_max_rows"
 
 $CLICKHOUSE_CLIENT -q "DROP TABLE test_native_max_rows"
 $CLICKHOUSE_CLIENT -q "DROP TABLE test_native_max_bytes"
 $CLICKHOUSE_CLIENT -q "DROP TABLE test_native_min_squash"
-$CLICKHOUSE_CLIENT -q "DROP TABLE test_parquet_max_rows"
