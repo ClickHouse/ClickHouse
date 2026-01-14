824a4b96a6a21ef017f31b6057f746109394e9f7
Azat Khuzhin <a3at.mail@gmail.com>
2026-01-14 18:34:03 +0100

    tests: fix test_max_temporary_data_size_on_disk flakiness
    Integration tests randomizes max_block_size, from 8k to 100k

--- tests/integration/test_max_temporary_data_size_on_disk/test.py
+++ tests/integration/test_max_temporary_data_size_on_disk/test.py
@@ -28,6 +28,6 @@ def test(start_cluster):
             """
         )
         for i in range(10):
-            node.query("SELECT k1, k2, k3, sum(value) v FROM t_proj_external GROUP BY k1, k2, k3 ORDER BY k1, k2, k3 SETTINGS optimize_aggregation_in_order = 0, max_bytes_before_external_group_by = 1, max_bytes_ratio_before_external_group_by = 0, group_by_two_level_threshold = 1")
+            node.query("SELECT k1, k2, k3, sum(value) v FROM t_proj_external GROUP BY k1, k2, k3 ORDER BY k1, k2, k3 SETTINGS optimize_aggregation_in_order = 0, max_bytes_before_external_group_by = 1, max_bytes_ratio_before_external_group_by = 0, group_by_two_level_threshold = 1, max_block_size=65000")
     finally:
         node.query("DROP TABLE IF EXISTS t_proj_external SYNC")
