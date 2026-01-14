56bb673efa0df112178302442adba3e551c27af2
Alexander Gololobov <davenger@clickhouse.com>
2026-01-14 15:54:32 +0100

    Update test

--- tests/queries/0_stateless/03777_runtime_filter_and_merge_table.sql
+++ tests/queries/0_stateless/03777_runtime_filter_and_merge_table.sql
@@ -15,7 +15,7 @@ CREATE TABLE foo_merge as foo ENGINE=Merge(currentDatabase(), '^foo');
 CREATE TABLE t2 (Id Int32, Val Int64, X UInt256) Engine=Memory;
 INSERT INTO t2 values (4, 3, 4);
 
-SELECT REGEXP_REPLACE(explain, '_runtime_filter_\\d+', '_runtime_filter_UNIQ_ID') FROM (
+SELECT explain FROM (
 EXPLAIN
 SELECT count()
 FROM foo_merge JOIN t2 USING Val
