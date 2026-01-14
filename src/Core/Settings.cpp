e8063d2f60cb67602b587de74d5ec6cf3f6826f3
Alexander Gololobov <davenger@clickhouse.com>
2026-01-14 13:14:28 +0100

    Mark `enable_join_runtime_filters` as beta

--- src/Core/Settings.cpp
+++ src/Core/Settings.cpp
@@ -7484,7 +7484,7 @@ Use Shuffle aggregation strategy instead of PartialAggregation + Merge in distri
 )", EXPERIMENTAL) \
     DECLARE(Bool, enable_join_runtime_filters, true, R"(
 Filter left side by set of JOIN keys collected from the right side at runtime.
-)", EXPERIMENTAL) \
+)", BETA) \
     DECLARE(UInt64, join_runtime_filter_exact_values_limit, 10000, R"(
 Maximum number of elements in runtime filter that are stored as is in a set, when this threshold is exceeded if switches to bloom filter.
 )", EXPERIMENTAL) \
