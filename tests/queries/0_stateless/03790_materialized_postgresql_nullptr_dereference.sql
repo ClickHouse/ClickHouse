08ffba3efa91db7783f1b9c575f8bb7fbcac1507
Alexey Milovidov <milovidov@clickhouse.com>
2026-01-14 17:35:25 +0100

    Update 03790_materialized_postgresql_nullptr_dereference.sql

--- tests/queries/0_stateless/03790_materialized_postgresql_nullptr_dereference.sql
+++ tests/queries/0_stateless/03790_materialized_postgresql_nullptr_dereference.sql
@@ -2,5 +2,5 @@
 -- depends on libpq
 
 SET allow_experimental_database_materialized_postgresql = 1;
-CREATE DATABASE test ENGINE = MaterializedPostgreSQL; -- { serverError BAD_ARGUMENTS }
-CREATE DATABASE test ENGINE = PostgreSQL; -- { serverError BAD_ARGUMENTS }
+CREATE DATABASE d03790_materialized_postgresql_nullptr_dereference ENGINE = MaterializedPostgreSQL; -- { serverError BAD_ARGUMENTS }
+CREATE DATABASE d03790_materialized_postgresql_nullptr_dereference ENGINE = PostgreSQL; -- { serverError BAD_ARGUMENTS }
