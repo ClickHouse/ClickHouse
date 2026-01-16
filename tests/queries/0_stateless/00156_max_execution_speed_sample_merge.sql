297275978a5556e1d011fe7d18caabe207f95098
Arsen Muk <arsenmuk@clickhouse.com>
2026-01-16 17:54:23 +0000

    Fix flaky 00156_max_execution_speed_sample_merge by using small data and more predictable timings

--- tests/queries/0_stateless/00156_max_execution_speed_sample_merge.sql
+++ tests/queries/0_stateless/00156_max_execution_speed_sample_merge.sql
@@ -1,26 +1,29 @@
--- Tags: no-tsan, no-msan, no-asan, stateful
--- This test validates that max_execution_speed & timeout_before_checking_execution_speed works as expected with:
+-- Tags: stateful
+-- This test validates that max_execution_speed & timeout_before_checking_execution_speed works with:
 --      a) regular query
 --      b) temp merge query
--- Our goal is to read enough data to trigger execution throttling (it won't work on small ans simple datasets), but not too much
--- to avoid unnecessarily long runtime.  Using of a local table, like test.hits or test.visits, is still too fast to trigger throttling.
--- This is why we use 'test.hist_s3' and sampling it heavily aiming for the shortest runtime.
--- NOTE: Normally test finishes in seconds, but accessing s3 still might hang. If becomes flaky again due to execution timeouts, try tag 'long'
+-- NOTE: This test uses simple synthetic data to validate the fact throttling was applied.
+-- If throttling works as expected - each execution will take >= 1 second, as we allow not more than {max_execution_speed} records/seconds
+-- If it doesn't - each select will finish immediately, and the test will fail
 
-SET max_execution_speed = 1000000;
+SET max_execution_speed = 5;
 SET timeout_before_checking_execution_speed = 0;
 
 CREATE TEMPORARY TABLE times (t DateTime);
 
+DROP TABLE IF EXISTS t00156_max_execution_speed_sample_merge;
+CREATE TABLE t00156_max_execution_speed_sample_merge (v UInt64);
+INSERT INTO t00156_max_execution_speed_sample_merge SELECT number FROM numbers(8);
+
 INSERT INTO times SELECT now();
-SELECT count() FROM test.hits_s3 SAMPLE 1500000;
+SELECT * FROM t00156_max_execution_speed_sample_merge WHERE sleepEachRow(0.1) == 0 FORMAT Null;
 INSERT INTO times SELECT now();
 
 SELECT max(t) - min(t) >= 1 FROM times;
 TRUNCATE TABLE times;
 
 INSERT INTO times SELECT now();
-SELECT count() FROM merge(test, '^hits_s3$') SAMPLE 1500000;
+SELECT * FROM t00156_max_execution_speed_sample_merge WHERE sleepEachRow(0.1) == 0 FORMAT Null;
 INSERT INTO times SELECT now();
 
 SELECT max(t) - min(t) >= 1 FROM times;
