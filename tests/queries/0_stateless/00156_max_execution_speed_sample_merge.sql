a1d6708bd141e11284c29a5e5470dfb5bb50a3f5
Arsen Muk <arsenmuk@clickhouse.com>
2026-01-16 16:08:51 +0000

    Fix flaky 00156_max_execution_speed_sample_merge

--- tests/queries/0_stateless/00156_max_execution_speed_sample_merge.sql
+++ tests/queries/0_stateless/00156_max_execution_speed_sample_merge.sql
@@ -1,20 +1,26 @@
--- Tags: no-tsan, no-msan, no-asan, no-parallel, stateful
--- Tag no san & no-parallel: plumbing of a large dataset within the strict time expectation might get unpredictalbe with sanitizers and under heavy concurrency
+-- Tags: no-tsan, no-msan, no-asan, stateful
+-- This test validates that max_execution_speed & timeout_before_checking_execution_speed works as expected with:
+--      a) regular query
+--      b) temp merge query
+-- Our goal is to read enough data to trigger execution throttling (it won't work on small ans simple datasets), but not too much
+-- to avoid unnecessarily long runtime.  Using of a local table, like test.hits or test.visits, is still too fast to trigger throttling.
+-- This is why we use 'test.hist_s3' and sampling it heavily aiming for the shortest runtime.
+-- NOTE: Normally test finishes in seconds, but accessing s3 still might hang. If becomes flaky again due to execution timeouts, try tag 'long'
 
-SET max_execution_speed = 8000000;
+SET max_execution_speed = 1000000;
 SET timeout_before_checking_execution_speed = 0;
 
 CREATE TEMPORARY TABLE times (t DateTime);
 
 INSERT INTO times SELECT now();
-SELECT count() FROM test.hits_s3 SAMPLE 1 / 2;
+SELECT count() FROM test.hits_s3 SAMPLE 1500000;
 INSERT INTO times SELECT now();
 
 SELECT max(t) - min(t) >= 1 FROM times;
 TRUNCATE TABLE times;
 
 INSERT INTO times SELECT now();
-SELECT count() FROM merge(test, '^hits_s3$') SAMPLE 1 / 2;
+SELECT count() FROM merge(test, '^hits_s3$') SAMPLE 1500000;
 INSERT INTO times SELECT now();
 
 SELECT max(t) - min(t) >= 1 FROM times;
