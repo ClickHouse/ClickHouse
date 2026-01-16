72ddea428721019860ad8758dc5cfa8dfd028212
Igor Nikonov <igor@clickhouse.com>
2026-01-16 12:39:29 +0100

    Update test

--- tests/queries/0_stateless/03787_pr_group_by_read_mode_error.sql
+++ tests/queries/0_stateless/03787_pr_group_by_read_mode_error.sql
@@ -9,8 +9,7 @@ INSERT INTO t2 SELECT number % 1000 from numbers(100000);
 
 SET enable_parallel_replicas = 1, max_parallel_replicas = 2, cluster_for_parallel_replicas = 'test_cluster_one_shard_three_replicas_localhost', parallel_replicas_for_non_replicated_merge_tree=1;
 SYSTEM ENABLE FAILPOINT parallel_replicas_wait_for_unused_replicas;
-SELECT a FROM (SELECT key + 1 as a, key FROM t1 GROUP BY key) settings parallel_replicas_local_plan=1, optimize_aggregation_in_order=1 FORMAT Null;
--- SELECT key + 1 as a, key FROM t1 GROUP BY key HAVING key FORMAT Null;
+SELECT a FROM (SELECT key + 1 as a, key FROM t1 GROUP BY key HAVING key) settings parallel_replicas_local_plan=1, optimize_aggregation_in_order=1 FORMAT Null;
 
--- DROP TABLE IF EXISTS t1;
--- DROP TABLE IF EXISTS t2;
+DROP TABLE IF EXISTS t1;
+DROP TABLE IF EXISTS t2;
