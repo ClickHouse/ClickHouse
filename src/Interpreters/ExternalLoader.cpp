430fe15d6bb5ecb0a3f68c0aad0f165f7daa0236
Alexey Milovidov <milovidov@clickhouse.com>
2026-01-18 00:30:45 +0100

    Miscellaneous

--- src/Interpreters/ExternalLoader.cpp
+++ src/Interpreters/ExternalLoader.cpp
@@ -912,7 +912,8 @@ private:
         putBackFinishedThreadsToPool();
 
         /// All loadings have unique loading IDs.
-        size_t loading_id = next_id_counter++;
+        size_t loading_id = next_id_counter;
+        ++next_id_counter;
         info.loading_id = loading_id;
         info.loading_start_time = std::chrono::system_clock::now();
         info.loading_end_time = TimePoint{};
