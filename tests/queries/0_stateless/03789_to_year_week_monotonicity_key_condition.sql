110ce649e7f4484a24d0107fe4ac76f7c1b3093d
Nihal Z. Miaji <81457724+nihalzp@users.noreply.github.com>
2026-01-18 23:12:37 +0000

    Add one more bug to test

--- tests/queries/0_stateless/03789_to_year_week_monotonicity_key_condition.sql
+++ tests/queries/0_stateless/03789_to_year_week_monotonicity_key_condition.sql
@@ -31,3 +31,22 @@ WHERE toWeek(d) = toWeek(toDate32('2020-12-31'));
 SET use_primary_key = 0;
 SELECT count() FROM t
 WHERE toWeek(d) = toWeek(toDate32('2020-12-31'));
+
+
+DROP TABLE IF EXISTS t;
+CREATE TABLE t (s String)
+ENGINE = MergeTree
+ORDER BY s;
+
+INSERT INTO t VALUES
+    ('2020-02-11 00:00:00'),
+    ('2020-02-3 00:00:00');
+
+-- This used to throw an error but should return 1
+SET use_primary_key = 1;
+SELECT count() FROM t
+WHERE toWeek(s) = toWeek('2020-02-3 00:00:00');
+
+SET use_primary_key = 0;
+SELECT count() FROM t
+WHERE toWeek(s) = toWeek('2020-02-3 00:00:00');
