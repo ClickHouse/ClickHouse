acb43c29144b604efff8343b442f6f9e5d20f288
Robert Schulze <robert@clickhouse.com>
2026-01-19 12:55:00 +0100

    Update 02346_text_index_mark_file_compatibility.sql

--- tests/queries/0_stateless/02346_text_index_mark_file_compatibility.sql
+++ tests/queries/0_stateless/02346_text_index_mark_file_compatibility.sql
@@ -1,12 +1,11 @@
--- For tables with a TextIndex, the index .mrk file format
--- generated during data merges has a compatibility issue.
+-- For tables with a text index, the merge may produce a corrupt .mrk file format
 
 SET allow_experimental_full_text_index = 1;
 SET use_query_condition_cache = 0;
 
 DROP TABLE IF EXISTS tab;
 
--- Make min_bytes_for_wide_part = 0 to avoid warning about non-adaptive granularity
+-- 
 -- (index_granularity_bytes=0) being incompatible with Compact part format.
 CREATE TABLE tab
 (
@@ -18,8 +17,9 @@ CREATE TABLE tab
 )
 ENGINE = MergeTree
 ORDER BY i
-SETTINGS index_granularity_bytes = 0, min_bytes_for_wide_part = 0;
-   
+SETTINGS index_granularity_bytes = 0, -- non-adaptive granularity
+         min_bytes_for_wide_part = 0; -- avoid warning about non-adaptive granularity being incompatible with compact part format
+
 INSERT INTO tab
 SELECT
     2,
@@ -30,6 +30,6 @@ FROM numbers(1024000);
 
 OPTIMIZE TABLE tab FINAL;
 
-SELECT count() FROM tab WHERE hasToken(str, 'aa');
+SELECT count() FROM tab WHERE hasToken(str, 'aa'); -- this must not return an error
 
 DROP TABLE tab;
