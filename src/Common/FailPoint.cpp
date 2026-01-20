bd1a5e322d454ce1f3aa1b0d3633706569342307
Tuan Pham Anh <tuan.pham.anh@clickhouse.com>
2026-01-20 15:01:33 +0800

    Update FailPoint.cpp

--- src/Common/FailPoint.cpp
+++ src/Common/FailPoint.cpp
@@ -120,7 +120,6 @@ static struct InitFiu
     ONCE(smt_commit_exception_before_op) \
     ONCE(disk_object_storage_fail_commit_metadata_transaction) \
     ONCE(database_replicated_drop_before_removing_keeper_failed) \
-    ONCE(database_replicated_drop_after_removing_keeper_failed) \
     PAUSEABLE_ONCE(mt_mutate_task_pause_in_prepare) \
     PAUSEABLE_ONCE(smt_mutate_task_pause_in_prepare) \
     PAUSEABLE_ONCE(smt_merge_selecting_task_pause_when_scheduled) \
