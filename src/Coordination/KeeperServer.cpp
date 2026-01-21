cdaf1ee1fd99cb3f84a085d50726610676044955
Konstantin Bogdanov <konstantin@clickhouse.com>
2026-01-21 18:30:33 +0100

    Use `LOG_FATAL`

--- src/Coordination/KeeperServer.cpp
+++ src/Coordination/KeeperServer.cpp
@@ -880,7 +880,17 @@ nuraft::cb_func::ReturnCode KeeperServer::callbackFunc(nuraft::cb_func::Type typ
                 auto & req = *static_cast<nuraft::req_msg *>(param->ctx);
 
                 if (req.get_src() == server_id)
-                    throw Exception(ErrorCodes::LOGICAL_ERROR, "Raft received its own message intended for another peer, this indicates misconfiguration; server_id: {}, src: {}, dst: {}", server_id, req.get_src(), req.get_dst());
+                {
+                    LOG_FATAL
+                    (
+                        log,
+                        "Raft received its own message intended for another peer, this indicates misconfiguration; server_id: {}, src: {}, dst: {}",
+                        server_id,
+                        req.get_src(),
+                        req.get_dst()
+                    );
+                    std::terminate();
+                }
 
                 break;
             }
