afcedc57f3a6228aa51c2b0d466e5327a3a7d770
Nihal Z. Miaji <81457724+nihalzp@users.noreply.github.com>
2026-01-21 21:01:23 +0000

    Add monotonicity for `ifNull`

--- src/Functions/ifNull.cpp
+++ src/Functions/ifNull.cpp
@@ -39,6 +39,22 @@ public:
     bool isSuitableForShortCircuitArgumentsExecution(const DataTypesWithConstInfo & /*arguments*/) const override { return false; }
     ColumnNumbers getArgumentsThatDontImplyNullableReturnType(size_t /*number_of_arguments*/) const override { return {0}; }
 
+    bool hasInformationAboutMonotonicity() const override { return true; }
+
+    Monotonicity getMonotonicityForRange(const IDataType & type, const Field & /*left*/, const Field & right) const override
+    {
+        /// ifNull() is identity when its first argument cannot be NULL, so it preserves ordering and thus monotonic.
+        /// For Nullable types, ifNull() substitutes NULLs with the second argument and is not
+        /// monotonic in general. We treat it as monotonic only when the analyzed range is guaranteed to not contain
+        /// NULLs. NULLs always represented as POSITIVE_INFINITY and they will always be at the end of ordering.
+        /// So, we do not need to check left.isNull().
+        bool is_nullable_or_lc_nullable = type.isNullable() || type.isLowCardinalityNullable();
+        if (is_nullable_or_lc_nullable && right.isNull())
+            return {};
+
+        return { .is_monotonic = true, .is_positive = true, .is_always_monotonic = !is_nullable_or_lc_nullable };
+    }
+
     DataTypePtr getReturnTypeImpl(const DataTypes & arguments) const override
     {
         if (arguments[0]->onlyNull())
