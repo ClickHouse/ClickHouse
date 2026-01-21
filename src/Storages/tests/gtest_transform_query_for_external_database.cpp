c5ff900b910b37f472d4c8428b32f5cb97bd431c
George Larionov <george.larionov@clickhouse.com>
2026-01-21 21:52:48 +0000

    updating more failing tests

--- src/Storages/tests/gtest_transform_query_for_external_database.cpp
+++ src/Storages/tests/gtest_transform_query_for_external_database.cpp
@@ -311,7 +311,7 @@ TEST(TransformQueryForExternalDatabase, Aliases)
 
     check(state, 1, {"field"},
           "SELECT field AS value, field AS display FROM table WHERE field NOT IN ('') AND display LIKE '%test%'",
-          R"(SELECT "field" FROM "test"."table" WHERE ("field" NOT IN ('')) AND ("field" LIKE '%test%'))");
+          R"(SELECT "field" FROM "test"."table" WHERE ("field" NOT IN '') AND ("field" LIKE '%test%'))");
 }
 
 TEST(TransformQueryForExternalDatabase, ForeignColumnInWhere)
@@ -417,7 +417,7 @@ TEST(TransformQueryForExternalDatabase, Analyzer)
 
     check(state, 1, {"column", "apply_id", "apply_type", "apply_status", "create_time", "field", "value", "a", "b", "foo"},
         "SELECT * EXCEPT (is_value) FROM table WHERE (column) IN (1)",
-        R"(SELECT "column", "apply_id", "apply_type", "apply_status", "create_time", "field", "value", "a", "b", "foo" FROM "test"."table" WHERE "column" IN (1))");
+        R"(SELECT "column", "apply_id", "apply_type", "apply_status", "create_time", "field", "value", "a", "b", "foo" FROM "test"."table" WHERE "column" IN 1)");
 
     check(state, 1, {"is_value"},
         "SELECT is_value FROM table WHERE is_value = true",
