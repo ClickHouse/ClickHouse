c940ac71e335981978f6f39ec8468865aae43799
zoomxi <419486879@qq.com>
2026-01-22 21:27:25 +0800

    add test cases

--- src/Common/tests/gtest_field_accurate_comparison.cpp
+++ src/Common/tests/gtest_field_accurate_comparison.cpp
@@ -29,4 +29,12 @@ TEST(FieldAccurateComparisonTest, DecimalVsFloat64)
     Field float_neg = Float64(-44.40);
     EXPECT_TRUE(accurateEquals(decimal_neg, float_neg));
     EXPECT_TRUE(accurateLess(decimal_neg, decimal_44_40));
+
+    /// Comparison of 2 non-equal negative values
+    Field decimal_neg_33_30 = DecimalField<Decimal64>(-3330, 2);
+    Field float_neg_44_40 = Float64(-44.40);
+    EXPECT_FALSE(accurateLess(decimal_neg_33_30, float_neg_44_40));
+    EXPECT_TRUE(accurateLess(float_neg_44_40, decimal_neg_33_30));
+    EXPECT_FALSE(accurateEquals(decimal_neg_33_30, float_neg_44_40));
+    EXPECT_FALSE(accurateLessOrEqual(decimal_neg_33_30, float_neg_44_40));
 }
