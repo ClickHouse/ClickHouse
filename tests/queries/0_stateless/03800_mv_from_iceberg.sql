4b46364581f6ffac53f30075a0693a73cc8f4e21
divanik <dan.ivanik@clickhouse.com>
2026-01-23 16:25:25 +0100

    Add randomization to test

--- tests/queries/0_stateless/03800_mv_from_iceberg.sql
+++
@@ -1,7 +0,0 @@
--- Tags: no-fasttest, no-parallel-replicas
-
-DROP TABLE IF EXISTS t0, t1, v0;
-CREATE TABLE t0 (c0 Int) ENGINE = MergeTree() ORDER BY tuple();
-CREATE TABLE IF NOT EXISTS t1 (c0 Int) ENGINE = IcebergS3(s3_conn, filename = '03800_mv_from_iceberg/t1');
-CREATE MATERIALIZED VIEW v0 TO t0 AS (SELECT c0 FROM t1);
-INSERT INTO TABLE t1 (c0) SETTINGS allow_experimental_insert_into_iceberg = 1 VALUES (1);
