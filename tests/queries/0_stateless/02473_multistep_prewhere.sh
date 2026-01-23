fcab1de454206f12fa49e116cdb36196ff6cba34
Alexander Gololobov <davenger@clickhouse.com>
2026-01-23 14:45:47 +0100

    Update tests/queries/0_stateless/02473_multistep_prewhere.sh
    Co-authored-by: Antonio Andelic <antonio2368@users.noreply.github.com>

--- tests/queries/0_stateless/02473_multistep_prewhere.sh
+++ tests/queries/0_stateless/02473_multistep_prewhere.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
-# Tags: long, no-parallel, no-msan, no-asan, no-flaky-check, no-tsan
+# Tags: long, no-parallel, no-sanitizers, no-flaky-check
 # ^ The test is heavy
 
 CURDIR=$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)
