11047059a1488b0fa85645a3fab883eed7b76cc3
divanik <dan.ivanik@clickhouse.com>
2026-01-23 14:19:53 +0100

    Replace the test

--- tests/queries/0_stateless/03800_mv_from_iceberg.sql
+++ tests/queries/0_stateless/03800_mv_from_iceberg.sql
@@ -1,5 +1,7 @@
+-- Tags: no-fasttest, no-parallel-replicas
+
 DROP TABLE IF EXISTS t0, t1, v0;
 CREATE TABLE t0 (c0 Int) ENGINE = MergeTree() ORDER BY tuple();
-CREATE TABLE t1 (c0 Int) ENGINE = IcebergLocal('./user_files/lakehouse/t1/');
+CREATE TABLE t1 (c0 Int) ENGINE = IcebergS3(s3_conn, filename = '03800_mv_from_iceberg/t1');
 CREATE MATERIALIZED VIEW v0 TO t0 AS (SELECT c0 FROM t1);
 INSERT INTO TABLE t1 (c0) SETTINGS allow_experimental_insert_into_iceberg = 1 VALUES (1);
