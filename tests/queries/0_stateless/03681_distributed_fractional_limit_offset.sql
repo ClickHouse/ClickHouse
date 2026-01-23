31a94c17c986a24620a1d8bb9e38fafb7c6435d4
Nihal Z. Miaji <81457724+nihalzp@users.noreply.github.com>
2026-01-24 02:54:04 +0800

    Update 03681_distributed_fractional_limit_offset.sql

--- tests/queries/0_stateless/03681_distributed_fractional_limit_offset.sql
+++ tests/queries/0_stateless/03681_distributed_fractional_limit_offset.sql
@@ -22,9 +22,10 @@ SELECT number from remote('127.0.0.{1,2,3}', numbers_mt(100)) ORDER BY number LI
 
 SELECT number from remote('127.0.0.{1,2,3}', numbers_mt(100)) ORDER BY number LIMIT 0.01 OFFSET 297;
 
-SELECT 'Old Analyzer - Distributed Table';
+-- Distributed Table
 
-SET enable_analyzer=0;
+SET extremes = 1;
+SET prefer_localhost_replica = 0;
 
 DROP TABLE IF EXISTS test__fuzz_2_local;
 DROP TABLE IF EXISTS test__fuzz_2_dist;
@@ -42,9 +43,9 @@ INSERT INTO test__fuzz_2_local VALUES (1),(2),(3);
 CREATE TABLE test__fuzz_2_dist AS test__fuzz_2_local
 ENGINE = Distributed(test_cluster_two_shards, currentDatabase(), test__fuzz_2_local, rand());
 
+SELECT 'Old Analyzer - Distributed Table';
+
 SET enable_analyzer = 0;
-SET extremes = 1;
-SET prefer_localhost_replica = 0;
 
 SELECT 0 AS `0`, 18 AS `18`, __table1.k AS k
 FROM test__fuzz_2_dist AS __table1
@@ -52,11 +53,9 @@ PREWHERE _CAST(11, 'Nullable(UInt8)')
 ORDER BY __table1.k DESC NULLS LAST
 LIMIT 0.9999, _CAST(100, 'UInt64');
 
-SET extremes = 0;
 SELECT 'New Analyzer - Distributed Table';
 
 SET enable_analyzer = 1;
-SET extremes = 1;
 
 SELECT 0 AS `0`, 18 AS `18`, __table1.k AS k
 FROM test__fuzz_2_dist AS __table1
