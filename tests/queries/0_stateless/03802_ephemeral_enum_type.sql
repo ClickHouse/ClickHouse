f9fa718c752bbb34746d71c1ffc4fc27580eb930
Alexey Milovidov <milovidov@clickhouse.com>
2026-01-23 22:28:33 +0100

    Update 03802_ephemeral_enum_type.sql

--- tests/queries/0_stateless/03802_ephemeral_enum_type.sql
+++ tests/queries/0_stateless/03802_ephemeral_enum_type.sql
@@ -4,9 +4,9 @@
 DROP TABLE IF EXISTS t_ephemeral_enum;
 
 CREATE TABLE t_ephemeral_enum (x UInt32, y Enum8('a' = 1, 'b' = 2) EPHEMERAL) ENGINE = Memory;
-DESCRIBE TABLE t_ephemeral_enum;
+DESCRIBE TABLE t_ephemeral_enum FORMAT TSVRaw;
 DROP TABLE t_ephemeral_enum;
 
 CREATE TABLE t_ephemeral_enum (x UInt32, y Enum16('foo' = 100, 'bar' = 200) EPHEMERAL) ENGINE = Memory;
-DESCRIBE TABLE t_ephemeral_enum;
+DESCRIBE TABLE t_ephemeral_enum FORMAT TSVRaw;
 DROP TABLE t_ephemeral_enum;
