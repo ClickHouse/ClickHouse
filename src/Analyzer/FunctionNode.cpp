c4e5ebce974ef088c7bb89632817d3c31ee6d959
Mohamed Abdelhalim <1abdelhalim@users.noreply.github.com>
2026-01-24 23:38:11 +0200

    Format operators as functions in EXPLAIN SYNTAX and toAST
    This implements the core fix for issue #94603 by modifying
    FunctionNode::toASTImpl() to always format operators as function calls,
    and adds comprehensive tests.

--- src/Analyzer/FunctionNode.cpp
+++ src/Analyzer/FunctionNode.cpp
@@ -240,7 +240,7 @@ ASTPtr FunctionNode::toASTImpl(const ConvertToASTOptions & options) const
 
     function_ast->name = function_name;
     function_ast->nulls_action = nulls_action;
-    function_ast->is_operator = is_operator;
+    function_ast->is_operator = false;
 
     if (isWindowFunction())
     {
