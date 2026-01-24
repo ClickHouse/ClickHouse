78e2ead06431d26153680edfd2e1fbecf4fa620c
Alexey Milovidov <milovidov@clickhouse.com>
2026-01-24 20:07:58 +0100

    Revert "Add unit test for PeekableReadBuffer canceled flag reset"
    This reverts commit ef546f70240186c3e02ec6fc4a7af67ae2ea5df7.

--- src/IO/tests/gtest_peekable_read_buffer.cpp
+++ src/IO/tests/gtest_peekable_read_buffer.cpp
@@ -5,15 +5,6 @@
 #include <IO/ReadBufferFromString.h>
 #include <IO/ConcatReadBuffer.h>
 #include <IO/PeekableReadBuffer.h>
-#include <Common/Exception.h>
-
-namespace DB
-{
-    namespace ErrorCodes
-    {
-        extern const int CANNOT_READ_ALL_DATA;
-    }
-}
 
 static void readAndAssert(DB::ReadBuffer & buf, const char * str)
 {
@@ -152,82 +143,6 @@ try
     ASSERT_TRUE(!peekable.hasUnreadData());
     readAndAssert(peekable, "wertyuiop");
     ASSERT_TRUE(peekable.eof());
-}
-catch (const DB::Exception & e)
-{
-    std::cerr << e.what() << ", " << e.displayText() << std::endl;
-    throw;
-}
-
-/// A ReadBuffer that throws an exception after reading some data.
-/// Used to test that PeekableReadBuffer::rollbackToCheckpoint() resets the canceled flag.
-class ThrowingReadBuffer : public DB::ReadBuffer
-{
-public:
-    ThrowingReadBuffer(const std::string & data_, size_t throw_after_bytes_)
-        : ReadBuffer(nullptr, 0), data(data_), throw_after_bytes(throw_after_bytes_), current_pos(0)
-    {
-    }
-
-private:
-    bool nextImpl() override
-    {
-        if (current_pos >= throw_after_bytes)
-            throw DB::Exception(DB::ErrorCodes::CANNOT_READ_ALL_DATA, "Test exception from ThrowingReadBuffer");
-
-        if (current_pos >= data.size())
-            return false;
-
-        size_t to_read = std::min(chunk_size, data.size() - current_pos);
-        working_buffer = DB::BufferBase::Buffer(const_cast<char *>(data.data()) + current_pos, const_cast<char *>(data.data()) + current_pos + to_read);
-        current_pos += to_read;
-        return true;
-    }
-
-    std::string data;
-    size_t throw_after_bytes;
-    size_t current_pos;
-    static constexpr size_t chunk_size = 5;
-};
-
-TEST(PeekableReadBuffer, RollbackResetsTheCanceledFlag)
-try
-{
-    /// Test that rollbackToCheckpoint() resets the canceled flag.
-    /// This is important for format auto-detection where we try multiple formats
-    /// and some may fail with exceptions.
-
-    std::string data = "0123456789abcdefghij";
-
-    /// Create a buffer that will throw after reading 10 bytes
-    ThrowingReadBuffer throwing_buf(data, 10);
-    DB::PeekableReadBuffer peekable(throwing_buf, 0);
-
-    peekable.setCheckpoint();
-
-    /// Read some data (less than throw_after_bytes)
-    readAndAssert(peekable, "01234");
-
-    /// This should throw because we're past the threshold
-    ASSERT_THROW(
-    {
-        while (!peekable.eof())
-            peekable.ignore();
-    }, DB::Exception);
-
-    /// After the exception, the buffer should be canceled
-    ASSERT_TRUE(peekable.isCanceled());
-
-    /// Rollback should reset the canceled flag
-    peekable.rollbackToCheckpoint();
-    ASSERT_FALSE(peekable.isCanceled());
-
-    /// Now we should be able to read again from the checkpoint
-    /// (though the underlying buffer might still throw if we read too far)
-    assertAvailable(peekable, "01234");
-    readAndAssert(peekable, "01234");
-
-    peekable.dropCheckpoint();
 }
 catch (const DB::Exception & e)
 {
