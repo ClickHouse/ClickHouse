62f7afab5510c9dbdccd37ab95dec927a5142612
Alex Bakharew <alex.bakharew@gmail.com>
2026-01-26 11:16:58 +0100

    Increase sleep duration

--- ci/jobs/integration_test_job.py
+++ ci/jobs/integration_test_job.py
@@ -109,7 +109,7 @@ def merge_profraw_files(llvm_profdata: str, batch_num: int, loop: bool = False):
     
     while True:
         if loop:
-            time.sleep(180)
+            time.sleep(600) # Sleep for 10 minutes between merges
         
         # Find all profraw files (already-merged ones were deleted)
         profraw_files = [str(p) for p in Path(".").rglob("*.profraw")]
