64c728b5a37db38d7ea067066ed1b55233fee75f
Michael Stetsyuk <michael.stetsyuk@clickhouse.com>
2026-01-27 16:25:52 +0000

    move opentelemetry_keeper_spans_probability to 26.2

--- src/Core/SettingsChangesHistory.cpp
+++ src/Core/SettingsChangesHistory.cpp
@@ -41,7 +41,7 @@ const VersionToSettingsChangesMap & getSettingsChangesHistory()
         /// Note: please check if the key already exists to prevent duplicate entries.
         addSettingsChanges(settings_changes_history, "26.2",
         {
-
+            {"opentelemetry_keeper_spans_probability", 0.0, 0.0, "New setting"},
         });
         addSettingsChanges(settings_changes_history, "26.1",
         {
@@ -81,7 +81,6 @@ const VersionToSettingsChangesMap & getSettingsChangesHistory()
             {"throw_if_deduplication_in_dependent_materialized_views_enabled_with_async_insert", true, false, "It becomes obsolete."},
             {"database_datalake_require_metadata_access", true, true, "New setting."},
             {"automatic_parallel_replicas_min_bytes_per_replica", 0, 1_MiB, "Better default value derived from testing results"},
-            {"opentelemetry_keeper_spans_probability", 0.0, 0.0, "New setting"},
         });
         addSettingsChanges(settings_changes_history, "25.12",
         {
