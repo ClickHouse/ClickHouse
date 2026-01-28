97d607bc167a3ccb793e4654abe1e82844f8ccf0
Nihal Z. Miaji <81457724+nihalzp@users.noreply.github.com>
2026-01-28 09:52:28 +0000

    Fix pipeline stuck for `NegativeOffsetTransform`

--- src/Processors/NegativeOffsetTransform.h
+++ src/Processors/NegativeOffsetTransform.h
@@ -11,7 +11,7 @@ namespace DB
 
 /// Implementation for OFFSET -N (without limit) (drop last N)
 /// This processor support multiple inputs and outputs (the same number).
-/// Each pair of input and output port works independently.
+/// The output ports are interchangeable, chunks can be pushed to any available output.
 
 /// Streams by keeping a tail buffer of size N (in rows) across all input ports.
 class NegativeOffsetTransform final : public IProcessor
@@ -49,9 +49,10 @@ private:
 
     struct ChunkWithPort
     {
-        OutputPort * output_port = nullptr;
         Chunk chunk;
     };
+
+    size_t next_output_port = 0;
 
     /// Stores the pending chunks which are not yet confirmed whether they are
     /// full outside the offset or not. Once we can be sure that a chunk is fully
@@ -77,6 +78,11 @@ private:
 
     /// Does not do anything if the front chunk is not fully outside the offset.
     Status tryPushWholeFrontChunk();
+
+    /// Find an output port that can accept data at the moment.
+    OutputPort * getAvailableOutputPort();
+
+    bool allOutputsFinished() const;
 
     /// This should only be called when we know that the front chunk is NOT
     /// fully outside the offset.
