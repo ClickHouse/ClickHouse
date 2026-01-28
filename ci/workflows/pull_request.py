15ca91a7f01e4a5189d07d3e82c66db25d8d3596
Alex Bakharew <alex.bakharew@gmail.com>
2026-01-28 11:15:24 +0100

    Revert changes for ci/workflows/pull_request.py

--- ci/workflows/pull_request.py
+++ ci/workflows/pull_request.py
@@ -71,7 +71,6 @@ workflow = Workflow.Config(
             )
             for j in JobConfigs.functional_tests_jobs
         ],
-        
         *[
             job.set_dependency(FUNCTIONAL_TESTS_PARALLEL_BLOCKING_JOB_NAMES)
             for job in JobConfigs.integration_test_jobs_required[:]
@@ -80,7 +79,6 @@ workflow = Workflow.Config(
             job.set_dependency(FUNCTIONAL_TESTS_PARALLEL_BLOCKING_JOB_NAMES)
             for job in JobConfigs.integration_test_jobs_non_required
         ],
-        
         *JobConfigs.unittest_jobs,
         JobConfigs.docker_server.set_dependency(
             FUNCTIONAL_TESTS_PARALLEL_BLOCKING_JOB_NAMES
