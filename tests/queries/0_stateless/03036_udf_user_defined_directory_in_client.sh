7fa8ddbb2c49178618fc3f766f9f3c13375e4630
Alexei Fedotov <alexei.fedotov@clickhouse.com>
2026-01-29 09:19:38 +0100

    Fix a review comment

--- tests/queries/0_stateless/03036_udf_user_defined_directory_in_client.sh
+++ tests/queries/0_stateless/03036_udf_user_defined_directory_in_client.sh
@@ -8,11 +8,8 @@ ${CLICKHOUSE_CLIENT} --query "DROP TABLE IF EXISTS test"
 ${CLICKHOUSE_CLIENT} --query "CREATE TABLE test (s String) ENGINE = Memory"
 
 # Calling an unknown function should not lead to creation of a 'user_defined' directory in the current directory
-mkdir -p tmp$PPID
-cd tmp$PPID
+rm -rf user_defined
 ${CLICKHOUSE_CLIENT} --query "INSERT INTO test VALUES (xyz('abc'))" 2>&1 | grep -q -F 'UNKNOWN_FUNCTION' && echo 1 || echo 0
 ls -ld user_defined 2> /dev/null
-cd ..
-rm -rf tmp$PPID
 
 ${CLICKHOUSE_CLIENT} --query "DROP TABLE test"
