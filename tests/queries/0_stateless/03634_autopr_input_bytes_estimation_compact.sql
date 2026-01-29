73a1a19a8082ca6777157f6b2d80152aaa66e3e4
Nikita Taranov <nikita.taranov@clickhouse.com>
2026-01-29 19:07:59 +0100

    stash

--- tests/queries/0_stateless/03634_autopr_input_bytes_estimation_compact.sql
+++ tests/queries/0_stateless/03634_autopr_input_bytes_estimation_compact.sql
@@ -1,7 +1,4 @@
--- Tags: stateful, no-random-settings, no-random-merge-tree-settings
-
--- remove me
-set max_memory_usage=10000000000;
+-- Tags: stateful
 
 SET local_filesystem_read_prefetch=0, merge_tree_read_split_ranges_into_intersecting_and_non_intersecting_injection_probability=0, local_filesystem_read_method='pread_threadpool', use_uncompressed_cache=0;
 
@@ -13,11 +10,10 @@ set max_threads=4, max_block_size=8192;
 -- Reading of aggregation states from disk will affect `ReadCompressedBytes`
 SET max_bytes_before_external_group_by=0, max_bytes_ratio_before_external_group_by=0;
 
-CREATE TABLE t(a UInt64, s String, d Date) ENGINE=MergeTree PARTITION BY toYYYYMM(d) ORDER BY a SETTINGS index_granularity=8192, min_bytes_for_wide_part = 1e18;
+-- Statistics are disabled to avoid accounting for them in `ReadCompressedBytes`
+CREATE TABLE t(a UInt64, s String, d Date) ENGINE=MergeTree PARTITION BY toYYYYMM(d) ORDER BY a SETTINGS auto_statistics_types='', index_granularity=8192, min_bytes_for_wide_part = 1e18;
 
 INSERT INTO t SELECT number, toString(number), today() - INTERVAL (number % 30) DAY FROM numbers(1e6);
-
--- select name, part_type, rows from system.parts where table='t' and database=currentDatabase() and active;
 
 SELECT a FROM t FORMAT Null SETTINGS log_comment='query_1';
 
