04a0fcdd513849146d08e2706274bee552e41172
Konstantin Vedernikov <vedernikovkostya@mail.ru>
2026-01-29 22:20:51 +0000

    better

--- tests/queries/0_stateless/03821_parquet_multilple_where_column_usage.sql
+++ tests/queries/0_stateless/03821_parquet_multilple_where_column_usage.sql
@@ -1,2 +1,4 @@
-insert into function file('t.parquet') select number as x, number as y, number as z from numbers(10) settings engine_file_truncate_on_insert=1;
-select * from file('t.parquet') prewhere x > 5 and (x > 6 or y > 3);
+-- Tags: no-fasttest, no-parallel
+
+insert into function file('03821_file.parquet') select number as x, number as y, number as z from numbers(10) settings engine_file_truncate_on_insert=1;
+select * from file('03821_file.parquet') prewhere x > 5 and (x > 6 or y > 3);
