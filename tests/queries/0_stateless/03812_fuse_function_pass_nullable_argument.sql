438042a7be86fde5a3fc17933e7f2c4b7649c729
Nihal Z. Miaji <81457724+nihalzp@users.noreply.github.com>
2026-01-29 13:00:05 +0000

    Add more tests

--- tests/queries/0_stateless/03812_fuse_function_pass_nullable_argument.sql
+++ tests/queries/0_stateless/03812_fuse_function_pass_nullable_argument.sql
@@ -1,7 +1,11 @@
+-- { echo }
+
 SET optimize_syntax_fuse_functions = 1;
 
 DROP TABLE IF EXISTS test;
 CREATE TABLE test (`a` Float64, `b` Nullable(Int8)) ENGINE = Log;
+
+SELECT count(b) * count(b) IGNORE NULLS FROM (SELECT b FROM test);
 
 SELECT avg(b) * 3, (sum(b) + 1) + count(b), count(b) * count(b), count() IGNORE NULLS FROM (SELECT b FROM test);
 
@@ -18,3 +22,21 @@ INSERT INTO test (a, b) VALUES
     (-Inf, -7);
 
 SELECT avg(b) * 3, (sum(b) + 1) + count(b), count(b) * count(b), count() IGNORE NULLS FROM (SELECT b FROM test);
+
+SELECT sum(b), count(b), sum(a), count(a) FROM (SELECT a, b FROM test);
+
+SELECT sum(a), count(a) FROM ( SELECT CAST(materialize(1), 'Nullable(Int32)') AS a) t;
+
+SET enable_analyzer = 1;
+
+EXPLAIN SYNTAX run_query_tree_passes=1
+SELECT count(b) * count(b) IGNORE NULLS FROM (SELECT b FROM test);
+
+EXPLAIN SYNTAX run_query_tree_passes=1
+SELECT avg(b) * 3, (sum(b) + 1) + count(b), count(b) * count(b), count() IGNORE NULLS FROM (SELECT b FROM test);
+
+EXPLAIN SYNTAX run_query_tree_passes=1
+SELECT sum(b), count(b), sum(a), count(a) FROM (SELECT a, b FROM test);
+
+EXPLAIN SYNTAX run_query_tree_passes=1
+SELECT sum(a), count(a) FROM ( SELECT CAST(materialize(1), 'Nullable(Int32)') AS a) t;
