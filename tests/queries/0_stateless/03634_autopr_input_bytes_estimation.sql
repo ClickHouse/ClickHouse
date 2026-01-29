4695962e8bf615260101d15ff0e97932a8972ce0
Nikita Taranov <nikita.taranov@clickhouse.com>
2026-01-29 20:29:07 +0100

    better

--- tests/queries/0_stateless/03634_autopr_input_bytes_estimation.sql
+++ tests/queries/0_stateless/03634_autopr_input_bytes_estimation.sql
@@ -1,6 +1,6 @@
 -- Tags: stateful, long
 
-SET local_filesystem_read_prefetch=0, merge_tree_read_split_ranges_into_intersecting_and_non_intersecting_injection_probability=0, local_filesystem_read_method='pread_threadpool', use_uncompressed_cache=0;
+SET use_uncompressed_cache=0;
 
 SET enable_parallel_replicas=0, automatic_parallel_replicas_mode=2, parallel_replicas_local_plan=1, parallel_replicas_index_analysis_only_on_coordinator=1,
     parallel_replicas_for_non_replicated_merge_tree=1, max_parallel_replicas=3, cluster_for_parallel_replicas='parallel_replicas';
