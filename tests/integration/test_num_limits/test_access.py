44efe5278df48ba2f73b71661fc2c80a0be21c08
kirillgarbar <sexyboar322@gmail.com>
2026-01-29 13:08:32 +0000

    Refactored tests

--- tests/integration/test_num_limits/test_access.py
+++ tests/integration/test_num_limits/test_access.py
@@ -1,13 +1,15 @@
 import pytest
 
 from helpers.cluster import ClickHouseCluster
+from helpers.test_tools import assert_eq_with_retry
+from .common import verify_no_warning, verify_warning_with_values
 
 cluster = ClickHouseCluster(__file__)
 
 node = cluster.add_instance(
     "node3",
     with_zookeeper=False,
-    main_configs=["configs/config.xml"],
+    main_configs=["config/access/config.xml"],
     stay_alive=True,
 )
 
@@ -15,7 +17,7 @@ node1 = cluster.add_instance(
     "node1",
     with_zookeeper=True,
     macros={"replica": "r1"},
-    main_configs=["configs/config.xml", "configs/config_replicated.xml"],
+    main_configs=["config/access/config.xml", "config/access/replicated.xml"],
     stay_alive=True,
 )
 
@@ -23,9 +25,12 @@ node2 = cluster.add_instance(
     "node2",
     with_zookeeper=True,
     macros={"replica": "r2"},
-    main_configs=["configs/config.xml", "configs/config_replicated.xml"],
+    main_configs=["config/access/config.xml", "config/access/replicated.xml"],
     stay_alive=True,
 )
+
+limit = 10
+get_metric_query = "select value from system.metrics where metric = 'AccessEntities'"
 
 
 @pytest.fixture(scope="module")
@@ -46,16 +51,30 @@ def clear(node, users, roles):
         node.query("drop role if exists r{}".format(i))
 
 
+def get_metric_value(node):
+    return int(node.query(get_metric_query))
+
+
+def assert_correct_metric(node):
+    assert "Too many access entities" in node.query_and_get_error("create user ux")
+    assert limit == get_metric_value(node)
+
+
 def test_access_limit_default(started_cluster):
-    roles = 5
-    users = 5
+    # Some users and profiles from config are already counted
+    default_count = get_metric_value(node)
+    roles = 3
+    users = limit - roles - default_count
+
+    assert users > 0
+
     for i in range(roles):
         node.query("create role r{}".format(i))
 
     for i in range(users):
         node.query("create user u{}".format(i))
 
-    assert "Too many access entities" in node.query_and_get_error("create user ux")
+    assert_correct_metric(node)
 
     # Updating existing users not changing user count
     for i in range(users):
@@ -71,27 +90,23 @@ def test_access_limit_default(started_cluster):
     for i in range(users):
         node.query("create user u{}".format(i))
 
-    assert "Too many access entities" in node.query_and_get_error("create user ux")
+    assert_correct_metric(node)
 
     # Loaded users are counted correctly
     node.restart_clickhouse()
 
-    assert "Too many access entities" in node.query_and_get_error("create user ux")
+    assert_correct_metric(node)
 
     clear(node, users, roles)
 
 
 def test_access_limit_replicated(started_cluster):
-    roles = 5
-    users = 5
-
-    for i in range(roles):
-        node1.query("create role r{}".format(i))
+    users = limit - get_metric_value(node1)
 
     for i in range(users):
         node1.query("create user u{}".format(i))
 
-    assert "Too many access entities" in node1.query_and_get_error("create user ux")
+    assert_correct_metric(node1)
 
     users -= 1
     node1.query("drop user u{}".format(users))
@@ -111,17 +126,16 @@ def test_access_limit_replicated(started_cluster):
     for i in range(users):
         node1.query("create user u{}".format(i))
 
-    assert "Too many access entities" in node1.query_and_get_error("create user ux")
+    assert_correct_metric(node1)
 
     # Loaded users are counted correctly
     node1.restart_clickhouse()
 
-    assert "Too many access entities" in node1.query_and_get_error("create user ux")
+    assert_correct_metric(node1)
 
     # Replica counts entities too
-    assert "Too many access entities" in node2.query_and_get_error_with_retry(
-        "create user ux"
-    )
+    assert_eq_with_retry(node2, get_metric_query, f"{limit}\n")
+    assert_correct_metric(node2)
 
     for i in range(users):
         node1.query("drop user u{}".format(i))
@@ -129,6 +143,30 @@ def test_access_limit_replicated(started_cluster):
     for i in range(users):
         node2.query("create user u{}".format(i))
 
-    assert "Too many access entities" in node2.query_and_get_error("create user ux")
+    assert_correct_metric(node2)
 
-    clear(node1, users, roles)
+    clear(node1, users, 0)
+    clear(node2, users, 0)
+
+
+def test_warnings(started_cluster):
+    users = limit - get_metric_value(node)
+    warn_limit = limit // 2
+
+    for i in range(users):
+        node.query("create user u{}".format(i))
+
+    verify_warning_with_values(
+        node,
+        current_val=users,
+        warn_val=warn_limit,
+        throw_val=limit
+    )
+
+    # Drop decreases the metric
+    for i in range(users):
+        node.query("drop user u{}".format(i))
+
+    verify_no_warning(node, "The number of access entities")
+
+    clear(node, users, 0)
