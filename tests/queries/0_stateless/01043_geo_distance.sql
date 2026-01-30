faedad5ada0617aaf0cc116e9a0e664b7c07c89c
Konstantin Bogdanov <konstantin@clickhouse.com>
2026-01-30 22:15:28 +0100

    Add rounding to tests

--- tests/queries/0_stateless/01043_geo_distance.sql
+++ tests/queries/0_stateless/01043_geo_distance.sql
@@ -20,8 +20,8 @@ SELECT greatCircleDistance(0., 89., 0, 90.);
 SELECT geoDistance(0., 0., 0., 1.);
 SELECT geoDistance(0., 89., 0., 90.);
 
-SELECT greatCircleDistance(0., 0., 90., 0.);
-SELECT greatCircleDistance(0., 0., 0., 90.);
+SELECT round(greatCircleDistance(0., 0., 90., 0.), 8);
+SELECT round(greatCircleDistance(0., 0., 0., 90.), 8);
 
 SELECT geoDistance(0., 0., 90., 0.);
 SELECT geoDistance(0., 0., 0., 90.);
