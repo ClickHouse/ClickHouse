8d47cb5e9991710d83666f256647cd8715d2d0a0
Alexander Gololobov <davenger@clickhouse.com>
2026-01-30 15:20:30 +0100

    Simplify test query, remove repeated comment

--- tests/queries/0_stateless/03812_join_order_ubsan_overflow.sql
+++ tests/queries/0_stateless/03812_join_order_ubsan_overflow.sql
@@ -7,17 +7,13 @@ DROP TABLE IF EXISTS data_03812;
 CREATE TABLE data_03812 (key UInt64, value UInt64) ENGINE = MergeTree ORDER BY key;
 INSERT INTO data_03812 VALUES (1, 1), (2, 2);
 
--- This query triggers the join order optimizer with extremely large estimated row counts
--- which can cause overflow when converting double to UInt64 in estimateJoinCardinality
--- Using EXPLAIN to trigger planning without actual execution
--- The GROUPING SETS clause helps trigger the code path that exercises the join optimizer
+
 EXPLAIN PLAN
-SELECT DISTINCT 1
+SELECT 1
 FROM data_03812 AS t1
 ALL INNER JOIN (
     SELECT number FROM system.numbers LIMIT 9223372036854775806
-) AS t2 ON t2.number = t2.number
-GROUP BY GROUPING SETS ((t1.value), (t1.key))
+) AS t2 ON 1
 FORMAT Null;
 
 DROP TABLE data_03812;
