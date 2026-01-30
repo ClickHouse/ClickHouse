b7b2df4b1a282c4a31d70a341d08176dd28d6dc5
Konstantin Bogdanov <konstantin@clickhouse.com>
2026-01-30 22:43:11 +0100

    Round references too

--- tests/queries/0_stateless/01043_geo_distance.sql
+++ tests/queries/0_stateless/01043_geo_distance.sql
@@ -23,5 +23,5 @@ SELECT geoDistance(0., 89., 0., 90.);
 SELECT round(greatCircleDistance(0., 0., 90., 0.), 8);
 SELECT round(greatCircleDistance(0., 0., 0., 90.), 8);
 
-SELECT geoDistance(0., 0., 90., 0.);
-SELECT geoDistance(0., 0., 0., 90.);
+SELECT round(geoDistance(0., 0., 90., 0.), 8);
+SELECT round(geoDistance(0., 0., 0., 90.), 8);
