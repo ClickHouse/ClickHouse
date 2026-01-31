f70f2f4f05f4d0b9a0b3a6709aa4ae77ed2eeda0
robot-clickhouse <robot-clickhouse@users.noreply.github.com>
2026-01-31 21:13:56 +0000

    Backport #95664 to 25.10: Fix race between distributed DDL and dropping Replicated database

--- src/Interpreters/ReplicatedDatabaseQueryStatusSource.h
+++ src/Interpreters/ReplicatedDatabaseQueryStatusSource.h
@@ -11,7 +11,7 @@ class ReplicatedDatabaseQueryStatusSource final : public DistributedQueryStatusS
 {
 public:
     ReplicatedDatabaseQueryStatusSource(
-        const String & zk_node_path, const String & zk_replicas_path, ContextPtr context_, const Strings & hosts_to_wait);
+        const String & zk_node_path, const String & zk_replicas_path, ContextPtr context_, const Strings & hosts_to_wait, DDLGuardPtr && database_guard_);
 
     String getName() const override { return "ReplicatedDatabaseQueryStatus"; }
 
@@ -26,6 +26,8 @@ protected:
 
 private:
     static Block getSampleBlock();
+    /// A kind of read lock for the database which prevents dropping the database (and its metadata from zk that we use for getting the query status)
+    DDLGuardPtr database_guard;
 
 
 };
