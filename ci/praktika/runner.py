73eb7d6fdadff986eaa4f13f08034044f40eb862
Azat Khuzhin <a3at.mail@gmail.com>
2026-02-02 21:16:08 +0100

    ci: remove adjusting kernel.core_pattern
    It is done in the image and I've checked it on a random CI worker

--- ci/praktika/runner.py
+++ ci/praktika/runner.py
@@ -283,9 +283,6 @@ class Runner:
         if job.enable_gh_auth:
             _GH_Auth(workflow=workflow)
 
-        # core.COMM.PID-TID
-        Shell.check("sysctl kernel.core_pattern='core.%e.%p-%P'", verbose=True)
-
         if job.run_in_docker and not no_docker:
             job.run_in_docker, docker_settings = (
                 job.run_in_docker.split("+")[0],
