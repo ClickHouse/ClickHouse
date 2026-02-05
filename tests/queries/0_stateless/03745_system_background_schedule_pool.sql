b337fa5deec66e5281b7d1ccf842014b272c1374
Amos Bird <amosbird@gmail.com>
2026-02-05 11:15:33 +0800

    Fix test

--- tests/queries/0_stateless/03745_system_background_schedule_pool.sql
+++ tests/queries/0_stateless/03745_system_background_schedule_pool.sql
@@ -7,7 +7,7 @@ DROP TABLE IF EXISTS test_buffer_03745;
 CREATE TABLE test_table_03745 (x UInt64) ENGINE = Memory;
 CREATE TABLE test_buffer_03745 (x UInt64) ENGINE = Buffer(currentDatabase(), test_table_03745, 1, 10, 100, 10000, 1000000, 10000000, 100000000);
 INSERT INTO test_buffer_03745 VALUES (1), (2), (3);
-SELECT pool, database, table, table_uuid != toUUIDOrDefault(0) AS has_uuid, log_name FROM system.background_schedule_pool WHERE database = currentDatabase();
+SELECT pool, database, table, table_uuid != toUUIDOrDefault(0) AS has_uuid, log_name FROM system.background_schedule_pool WHERE database = currentDatabase() ORDER BY ALL;
 DROP TABLE test_buffer_03745;
 DROP TABLE test_table_03745;
 
@@ -15,7 +15,7 @@ DROP TABLE test_table_03745;
 DROP TABLE IF EXISTS test_merge_tree_03745;
 CREATE TABLE test_merge_tree_03745 (x UInt64, y String) ENGINE = MergeTree() ORDER BY x SETTINGS refresh_statistics_interval = '0';
 INSERT INTO test_merge_tree_03745 VALUES (1, 'a'), (2, 'b');
-SELECT pool, database, table, table_uuid != toUUIDOrDefault(0) AS has_uuid, log_name FROM system.background_schedule_pool WHERE database = currentDatabase();
+SELECT pool, database, table, table_uuid != toUUIDOrDefault(0) AS has_uuid, log_name FROM system.background_schedule_pool WHERE database = currentDatabase() ORDER BY ALL;
 DROP TABLE test_merge_tree_03745;
 
 -- Test 3: Distributed table (distributed pool)
@@ -26,6 +26,6 @@ CREATE TABLE test_distributed_03745 (x UInt64) ENGINE = Distributed(test_shard_l
 SYSTEM STOP DISTRIBUTED SENDS test_distributed_03745;
 -- Pool is created only for async INSERTs
 INSERT INTO test_distributed_03745 SETTINGS prefer_localhost_replica=0, distributed_foreground_insert=0 VALUES (1), (2), (3);
-SELECT pool, database, table, table_uuid != toUUIDOrDefault(0) AS has_uuid, log_name FROM system.background_schedule_pool WHERE database = currentDatabase();
+SELECT pool, database, table, table_uuid != toUUIDOrDefault(0) AS has_uuid, log_name FROM system.background_schedule_pool WHERE database = currentDatabase() ORDER BY ALL;
 DROP TABLE test_distributed_03745;
 DROP TABLE test_local_03745;
