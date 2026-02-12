844b8b206d770487e019f2e40a17c40da3400c40
David Vogelbacher <david@nominal.io>
2026-02-11 22:39:44 -0800

    use log comment

--- tests/queries/0_stateless/03916_rows_after_prewhere_and_where_filter.sql
+++ tests/queries/0_stateless/03916_rows_after_prewhere_and_where_filter.sql
@@ -1,36 +1,28 @@
 -- Tags: no-parallel
--- Test RowsAfterPrewhereAndWhereFilter profile event
 
 DROP TABLE IF EXISTS test_output_rows;
 CREATE TABLE test_output_rows (k UInt64, v String) ENGINE = MergeTree ORDER BY k;
 INSERT INTO test_output_rows SELECT number, toString(number) FROM numbers(1000);
 
 -- Case 1: No WHERE -- counts all storage output rows
-SELECT * FROM test_output_rows FORMAT Null;
-SYSTEM FLUSH LOGS query_log;
-SELECT ProfileEvents['RowsAfterPrewhereAndWhereFilter']
-FROM system.query_log
-WHERE (type = 'QueryFinish') AND (current_database = currentDatabase())
-    AND (query LIKE '%Case 1%FROM test_output_rows FORMAT Null%')
-    AND (query NOT LIKE '%system.query_log%');
+SELECT * FROM test_output_rows FORMAT Null SETTINGS log_comment = '03916_case1';
+-- Case 2: WHERE fully pushed to PREWHERE (simple column filter)
+SELECT * FROM test_output_rows WHERE k < 100 FORMAT Null SETTINGS log_comment = '03916_case2';
+-- Case 3: WHERE NOT pushed to PREWHERE
+SELECT * FROM test_output_rows WHERE k < 100 FORMAT Null SETTINGS optimize_move_to_prewhere = 0, log_comment = '03916_case3';
 
--- Case 2: WHERE fully pushed to PREWHERE (simple column filter) -- counts post-all-filtering rows
-SELECT * FROM test_output_rows WHERE k < 100 FORMAT Null; -- Case 2
 SYSTEM FLUSH LOGS query_log;
-SELECT ProfileEvents['RowsAfterPrewhereAndWhereFilter']
-FROM system.query_log
-WHERE (type = 'QueryFinish') AND (current_database = currentDatabase())
-    AND (query LIKE '%Case 2%FROM test_output_rows WHERE%FORMAT Null%')
-    AND (query NOT LIKE '%system.query_log%');
 
--- Case 3: WHERE NOT pushed to PREWHERE (non-deterministic function prevents push)
--- optimize_move_to_prewhere=0 forces WHERE to stay as FilterTransform
-SELECT * FROM test_output_rows WHERE k < 100 FORMAT Null SETTINGS optimize_move_to_prewhere=0; -- Case 3
-SYSTEM FLUSH LOGS query_log;
 SELECT ProfileEvents['RowsAfterPrewhereAndWhereFilter']
 FROM system.query_log
-WHERE (type = 'QueryFinish') AND (current_database = currentDatabase())
-    AND (query LIKE '%Case 3%FROM test_output_rows WHERE%FORMAT Null%')
-    AND (query NOT LIKE '%system.query_log%');
+WHERE current_database = currentDatabase() AND type = 'QueryFinish' AND log_comment = '03916_case1';
+
+SELECT ProfileEvents['RowsAfterPrewhereAndWhereFilter']
+FROM system.query_log
+WHERE current_database = currentDatabase() AND type = 'QueryFinish' AND log_comment = '03916_case2';
+
+SELECT ProfileEvents['RowsAfterPrewhereAndWhereFilter']
+FROM system.query_log
+WHERE current_database = currentDatabase() AND type = 'QueryFinish' AND log_comment = '03916_case3';
 
 DROP TABLE test_output_rows;
