2b3e5e6d7b5c4df3a0e97cff8dbd5895d081c3ae
mostafa <sasa1x2001@gmail.com>
2026-02-15 06:05:21 +0200

    Add currentDatabase() to test

--- tests/queries/0_stateless/03916_ctas_atomic.sql
+++ tests/queries/0_stateless/03916_ctas_atomic.sql
@@ -1,4 +1,4 @@
 DROP TABLE IF EXISTS test_ctas_failed;
 CREATE TABLE test_ctas_failed ENGINE = MergeTree() ORDER BY n AS
 SELECT number as n FROM numbers(10) WHERE throwIf(n = 4); -- { serverError FUNCTION_THROW_IF_VALUE_IS_NON_ZERO }
-SELECT name FROM system.tables WHERE name = 'test_ctas_failed';
+SELECT name FROM system.tables WHERE name = 'test_ctas_failed AND database = currentDatabase()';
