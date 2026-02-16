1525d736a8e8ff05b894a497868f9ad3f399991a
Alexei Fedotov <alexei.fedotov@clickhouse.com>
2026-02-16 19:52:27 +0100

    Add a praktika build workflow, r

--- ci/workflows/custom_build_praktika.py
+++ ci/workflows/custom_build_praktika.py
@@ -1,9 +1,10 @@
-from ci.defs.defs import RunnerLabels
+from ci.defs.defs import RunnerLabels, BASE_BRANCH
 from praktika import Job, Workflow
 
 workflow = Workflow.Config(
     name="Build Praktika for PyPy",
-    event=Workflow.Event.DISPATCH,
+    event=Workflow.Event.PULL_REQUEST, # for debug Workflow.Event.DISPATCH,
+    base_branches=[BASE_BRANCH], # REMOVEME
     jobs=[
         Job.Config(
             name="Build Praktika",
