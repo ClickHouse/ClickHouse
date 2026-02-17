02a46ab1b1536859d58916131f8cd462ea67d234
Konstantin Vedernikov <vedernikovkostya@mail.ru>
2026-02-17 13:33:25 +0000

    init

--- src/Processors/Formats/Impl/Parquet/Reader.cpp
+++ src/Processors/Formats/Impl/Parquet/Reader.cpp
@@ -5,6 +5,7 @@
 #include <Columns/ColumnString.h>
 #include <Columns/ColumnTuple.h>
 #include <Columns/FilterDescription.h>
+#include <Common/Exception.h>
 #include <Common/FieldAccurateComparison.h>
 #include <Formats/FormatFilterInfo.h>
 #include <Interpreters/castColumn.h>
@@ -27,6 +28,7 @@
 
 namespace DB::ErrorCodes
 {
+    extern const int BAD_ARGUMENTS;
     extern const int CANNOT_DECOMPRESS;
     extern const int CANNOT_INSERT_NULL_IN_ORDINARY_COLUMN;
     extern const int FEATURE_IS_NOT_ENABLED_AT_BUILD_TIME;
@@ -308,7 +310,7 @@ void Reader::prefilterAndInitRowGroups(const std::optional<std::unordered_set<UI
         extended_sample_block.insert(col);
     extended_sample_block_data_types = extended_sample_block.getDataTypes();
     const auto & row_level_filter = format_filter_info->row_level_filter;
-    const auto & prewhere_info = format_filter_info->prewhere_info;
+    const auto & prewhere_info = format_filter_info->prewhere_info;    
 
     /// Process schema.
     SchemaConverter schemer(file_metadata, options, &extended_sample_block);
@@ -343,6 +345,15 @@ void Reader::prefilterAndInitRowGroups(const std::optional<std::unordered_set<UI
 
     if (format_filter_info->key_condition)
     {
+        std::unordered_set<String> columns_parquet;
+        for (const auto & column : file_metadata.schema)
+            columns_parquet.insert(column.name);
+
+        for (const auto & column : format_filter_info->key_condition->getKeyColumns())
+        {
+            if (!columns_parquet.contains(column.first))
+                throw Exception(ErrorCodes::BAD_ARGUMENTS, "Key condition uses column {} which is not specified in parquet schema", column.first);
+        }
         for (size_t idx_in_output_block : format_filter_info->key_condition->getUsedColumns())
         {
             const auto & output_idx = sample_block_to_output_columns_idx.at(idx_in_output_block);
