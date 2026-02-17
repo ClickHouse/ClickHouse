128d60b2ab1d5c71317a36582812a848b9f07258
Nikita Taranov <nikita.taranov@clickhouse.com>
2026-02-17 20:18:37 +0000

    better

--- tests/queries/0_stateless/03801_autopr_input_bytes_estimation_query_with_subqueries.sql
+++ tests/queries/0_stateless/03801_autopr_input_bytes_estimation_query_with_subqueries.sql
@@ -1,4 +1,7 @@
--- Tags: stateful, no-random-settings
+-- Tags: stateful
+
+-- To avoid too slow test execution
+set remote_filesystem_read_method='threadpool', allow_prefetched_read_pool_for_remote_filesystem=1, filesystem_prefetch_step_marks=0, filesystem_prefetch_step_bytes='100Mi';
 
 SET enable_parallel_replicas=0, automatic_parallel_replicas_mode=2, parallel_replicas_local_plan=1, parallel_replicas_index_analysis_only_on_coordinator=1,
     parallel_replicas_for_non_replicated_merge_tree=1, max_parallel_replicas=3, cluster_for_parallel_replicas='parallel_replicas';
