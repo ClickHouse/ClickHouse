6a3be9cf86593925319adaa51acebf9b5c9b5daa
Nikita Mikhaylov <nikitamikhaylov@clickhouse.com>
2026-02-18 14:52:38 +0100

    Improve the test

--- tests/queries/0_stateless/03821_test_snappy_basic_decompression.sh
+++ tests/queries/0_stateless/03821_test_snappy_basic_decompression.sh
@@ -4,10 +4,12 @@ CURDIR=$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)
 # shellcheck source=../shell_config.sh
 . "$CURDIR"/../shell_config.sh
 
-${CLICKHOUSE_CURL} 'Accept-Encoding: snappy'              "${CLICKHOUSE_URL}&enable_http_compression=1" -d "SELECT 1"
+${CLICKHOUSE_CURL} -H 'Accept-Encoding: snappy'              "${CLICKHOUSE_URL}&enable_http_compression=1" -d "SELECT 1" --output - | ${CLICKHOUSE_SIMPLE_SNAPPY}
 
-${CLICKHOUSE_CURL} 'Accept-Encoding: snappy'              "${CLICKHOUSE_URL}&enable_http_compression=1" -d "SELECT 123987"
+${CLICKHOUSE_CURL} -H 'Accept-Encoding: snappy'              "${CLICKHOUSE_URL}&enable_http_compression=1" -d "SELECT 123987" --output -  | ${CLICKHOUSE_SIMPLE_SNAPPY}
 
-${CLICKHOUSE_CURL} 'Accept-Encoding: snappy'              "${CLICKHOUSE_URL}&enable_http_compression=1" -d "MAL_FORMED" 2>&1 | grep -o -q 'Code: 62. DB::Exception: Syntax error:' && echo "OK" || echo "FAIL"
+${CLICKHOUSE_CURL} -H 'Accept-Encoding: snappy'              "${CLICKHOUSE_URL}&enable_http_compression=1" -d "select 'Hello world Hello world Hello world'" --output -  | ${CLICKHOUSE_SIMPLE_SNAPPY}
 
-${CLICKHOUSE_CURL} 'Accept-Encoding: snappy'              "${CLICKHOUSE_URL}&enable_http_compression=1" -d "SELECT MAL_FORMED" 2>&1 | grep -o -q 'Code: 47. DB::Exception: Unknown expression identifier' && echo "OK" || echo "FAIL"
+${CLICKHOUSE_CURL} -H 'Accept-Encoding: snappy'              "${CLICKHOUSE_URL}&enable_http_compression=1" -d "MAL_FORMED" --output -  2>&1 | grep -o -q 'Code: 62. DB::Exception: Syntax error:' && echo "OK" || echo "FAIL"
+
+${CLICKHOUSE_CURL} -H 'Accept-Encoding: snappy'              "${CLICKHOUSE_URL}&enable_http_compression=1" -d "SELECT MAL_FORMED" --output - 2>&1 | ${CLICKHOUSE_SIMPLE_SNAPPY}  | grep -o -q 'Code: 47. DB::Exception: Unknown expression identifier' && echo "OK" || echo "FAIL"
