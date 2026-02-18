8fea2070943a1aac0c86ab3c34c14cdd16020360
Nihal Z. Miaji <81457724+nihalzp@users.noreply.github.com>
2026-02-18 13:46:50 +0000

    Add the `dynamicElement` related tests to integration test

--- tests/integration/test_nullable_tuple_subcolumns/test.py
+++ tests/integration/test_nullable_tuple_subcolumns/test.py
@@ -27,6 +27,7 @@ BASE_CASES = [
     "03162_dynamic_type_nested",
     "03369_variant_escape_filename_merge_tree",
     "03913_tuple_inside_nullable_subcolumns",
+    "03915_tuple_inside_nullable_variant_dynamic_element",
 ]
 
 # When the setting is disabled, some queries throw errors; when it is enabled, they do not.
