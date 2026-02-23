aedca04e28bcfa372a105d7f0617d4f30082d6d0
Alexander Gololobov <davenger@clickhouse.com>
2026-02-23 11:08:24 +0100

    Fix missing fields in clone()

--- src/Processors/QueryPlan/ReadFromMergeTree.cpp
+++ src/Processors/QueryPlan/ReadFromMergeTree.cpp
@@ -3287,6 +3287,8 @@ QueryPlanStepPtr ReadFromMergeTree::clone() const
         number_of_current_replica);
     cloned_step->allow_query_condition_cache = allow_query_condition_cache;
     cloned_step->enable_remove_parts_from_snapshot_optimization = enable_remove_parts_from_snapshot_optimization;
+    cloned_step->distributed_read_bucket_count = distributed_read_bucket_count;
+    cloned_step->setStepDescription(*this);
     return cloned_step;
 }
 
