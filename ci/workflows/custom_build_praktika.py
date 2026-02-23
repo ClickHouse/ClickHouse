62b447bb98b5d364f70d2ab21ce7c06e864bc53b
Alexei Fedotov <alexei.fedotov@clickhouse.com>
2026-02-23 13:04:27 +0100

    Add a new branch, r

---
+++ ci/workflows/custom_build_praktika.py
@@ -0,0 +1,26 @@
+from ci.defs.defs import RunnerLabels, BASE_BRANCH
+from ci.praktika.secret import Secret
+from praktika import Job, Workflow
+
+workflow = Workflow.Config(
+    name="Build Praktika for PyPI",
+    event=Workflow.Event.PULL_REQUEST,  # for debug Workflow.Event.DISPATCH,
+    base_branches=[BASE_BRANCH],  # REMOVEME
+    jobs=[
+        Job.Config(
+            name="Build Praktika",
+            runs_on=RunnerLabels.ARM_LARGE,
+            command="python3 ./ci/jobs/build_praktika.py",
+        ),
+    ],
+    secrets=[
+        Secret.Config(
+            name="TWINE_PASSWORD",
+            type=Secret.Type.GH_SECRET,
+        ),
+    ],
+)
+
+WORKFLOWS = [
+    workflow,
+]
