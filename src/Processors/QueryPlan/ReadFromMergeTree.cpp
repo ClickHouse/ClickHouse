668d9883c9202c037353e3508a0deb07a59ee8d4
robot-clickhouse <robot-clickhouse@users.noreply.github.com>
2026-02-23 15:31:49 +0000

    Backport #97698 to 25.12: Correct the check for RPN length in skip index processing with ORs

--- src/Processors/QueryPlan/ReadFromMergeTree.cpp
+++ src/Processors/QueryPlan/ReadFromMergeTree.cpp
@@ -1950,7 +1950,8 @@ static void buildIndexes(
 
     indexes->use_skip_indexes_for_disjunctions = settings[Setting::use_skip_indexes_for_disjunctions]
                                                     && skip_indexes.useful_indices.size() > 1
-                                                    && !indexes->key_condition_rpn_template->hasOnlyConjunctions();
+                                                    && !indexes->key_condition_rpn_template->hasOnlyConjunctions()
+                                                    && indexes->key_condition_rpn_template->getRPN().size() <= MergeTreeDataSelectExecutor::MAX_BITS_FOR_PARTIAL_DISJUNCTION_RESULT;
 
     {
         std::vector<size_t> index_sizes;
