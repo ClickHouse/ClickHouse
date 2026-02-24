7fb6e511e5839af7b6c60e8d490590050f09526a
Michael Kolupaev <michael.kolupaev@clickhouse.com>
2026-02-23 21:17:51 -0800

    Add setting functions_h3_default_if_invalid

--- src/Functions/h3GetFaces.cpp
+++ src/Functions/h3GetFaces.cpp
@@ -30,7 +30,11 @@ class FunctionH3GetFaces : public IFunction
 public:
     static constexpr auto name = "h3GetFaces";
 
-    static FunctionPtr create(ContextPtr) { return std::make_shared<FunctionH3GetFaces>(); }
+    H3Validator validator;
+
+    FunctionH3GetFaces(const ContextPtr & context) : validator(context) {}
+
+    static FunctionPtr create(ContextPtr context) { return std::make_shared<FunctionH3GetFaces>(context); }
 
     std::string getName() const override { return name; }
 
@@ -79,6 +83,12 @@ public:
 
         for (size_t row = 0; row < input_rows_count; ++row)
         {
+            if (!validator.validateCell(data[row]))
+            {
+                result_offsets[row] = current_offset;
+                continue;
+            }
+
             int max_faces = maxFaceCount(data[row]);
 
             faces.resize(max_faces);
