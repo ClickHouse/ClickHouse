c5548656ac4217915e1dbd996c71e7e8a21b0c74
Nikita Fomichev <nikita.fomichev@clickhouse.com>
2026-02-24 17:14:31 +0100

    ast fuzzer: add job with old_compatibility

--- ci/workflows/pull_request.py
+++ ci/workflows/pull_request.py
@@ -68,6 +68,7 @@ workflow = Workflow.Config(
         JobConfigs.stateless_tests_targeted_pr_jobs[0].set_allow_merge_on_failure(),
         JobConfigs.integration_test_targeted_pr_jobs[0].set_allow_merge_on_failure(),
         JobConfigs.ast_fuzzer_targeted_pr_jobs[0].set_allow_merge_on_failure(),
+        JobConfigs.ast_fuzzer_targeted_pr_jobs[1].set_allow_merge_on_failure(),
         *JobConfigs.stateless_tests_flaky_pr_jobs,
         *JobConfigs.integration_test_asan_flaky_pr_jobs,
         JobConfigs.bugfix_validation_ft_pr_job,
