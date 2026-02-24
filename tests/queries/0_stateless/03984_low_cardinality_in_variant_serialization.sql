67d2370669493de207a6d727bd3c9ec49f6bcd4d
murphy-4o <murphy.wang@clickhouse.com>
2026-02-24 08:32:06 +0000

    add test case
    Signed-off-by: murphy-4o <murphy.wang@clickhouse.com>

--- tests/queries/0_stateless/03984_low_cardinality_in_variant_serialization.sql
+++ tests/queries/0_stateless/03984_low_cardinality_in_variant_serialization.sql
@@ -3,5 +3,16 @@
 -- when the column was unwrapped but the data type still expected ColumnLowCardinality.
 SET use_variant_as_common_type = 1;
 
+-- Text serialization path (serializeImpl)
 SELECT concat([toLowCardinality(17), 42, 'x'], 'a');
 SELECT concat([[toLowCardinality(17), 42]], 'b');
+
+-- Binary serialization path (serializeBinaryBulkWithMultipleStreams):
+-- writing a Variant column with a LowCardinality element to a MergeTree table.
+SET allow_suspicious_variant_types = 1;
+SET allow_suspicious_low_cardinality_types = 1;
+DROP TABLE IF EXISTS t_lc_variant_binary;
+CREATE TABLE t_lc_variant_binary (v Array(Variant(LowCardinality(UInt8), String, UInt8))) ENGINE = MergeTree ORDER BY tuple();
+INSERT INTO t_lc_variant_binary SELECT [toLowCardinality(17), 42, 'x'];
+SELECT * FROM t_lc_variant_binary;
+DROP TABLE t_lc_variant_binary;
