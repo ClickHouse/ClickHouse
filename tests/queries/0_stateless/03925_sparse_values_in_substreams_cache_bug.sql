5cf34e4f8283df12474a584befc8bf750a4caee2
Pavel Kruglov <48961922+Avogar@users.noreply.github.com>
2026-02-25 13:37:16 +0100

    Update 03925_sparse_values_in_substreams_cache_bug.sql

--- tests/queries/0_stateless/03925_sparse_values_in_substreams_cache_bug.sql
+++ tests/queries/0_stateless/03925_sparse_values_in_substreams_cache_bug.sql
@@ -2,21 +2,20 @@ DROP TABLE IF EXISTS t_sparse_string_size;
 CREATE TABLE t_sparse_string_size
 (
     id UInt64,
-    t Tuple(a Nullable(String), b Nullable(UInt64))
+    t Tuple(a String, b UInt64)
 )
 ENGINE = MergeTree ORDER BY ()
 SETTINGS
     index_granularity = 10,
     min_bytes_for_wide_part = 0,
     ratio_of_defaults_for_sparse_serialization = 0.9,
-    serialization_info_version = 'with_types',
-    nullable_serialization_version = 'allow_sparse';
+    serialization_info_version = 'with_types';
 
 INSERT INTO t_sparse_string_size
 SELECT
     number,
-    (if(number % 13 = 0, toString(number), NULL),
-     if(number % 11 = 0, number, NULL))
+    (if(number % 13 = 0, toString(number), ''),
+     if(number % 11 = 0, number, 0))
 FROM numbers(200, 30);
 
 
