98a26303e76a136ac40295da5e77fb136938b0ac
Mark Andreev <mark.andreev@gmail.com>
2026-02-26 21:37:57 +0000

    Handle dangling symlinks gracefully in listFilesWithRegexpMatchingImpl

---
+++ tests/queries/0_stateless/04002_dangling_symlinks.sh
@@ -0,0 +1,20 @@
+#!/usr/bin/env bash
+# Tags: no-parallel, no-fasttest
+
+CURDIR=$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)
+# shellcheck source=../shell_config.sh
+. "$CURDIR"/../shell_config.sh
+
+TEST_DIR_NAME=test_04002
+DATA_DIR=${USER_FILES_PATH:?}/$TEST_DIR_NAME
+
+mkdir -p $DATA_DIR
+
+echo "data" >> ${DATA_DIR}/good.txt
+ln -s /tmp/$(uuidgen) ${DATA_DIR}/broken_link
+
+echo $DATA_DIR
+
+$CLICKHOUSE_CLIENT -q "SELECT * FROM file('$DATA_DIR/**', 'LineAsString')"
+
+rm -rf $DATA_DIR
