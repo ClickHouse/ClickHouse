16e0c10cb1aacaad8b53aa478a6d6cb2d87bf25d
robot-clickhouse <robot-clickhouse@users.noreply.github.com>
2026-02-26 14:31:46 +0000

    Backport #97832 to 25.11: Attribute the thread of Iceberg Iterator to query thread group

--- src/Storages/ObjectStorage/DataLakes/Iceberg/IcebergIterator.cpp
+++ src/Storages/ObjectStorage/DataLakes/Iceberg/IcebergIterator.cpp
@@ -178,11 +178,11 @@ std::optional<ManifestFileEntry> SingleThreadIcebergKeysIterator::next()
                 case PruningReturnStatus::NOT_PRUNED:
                     return manifest_file_entry;
                 case PruningReturnStatus::MIN_MAX_INDEX_PRUNED: {
-                    ++min_max_index_pruned_files;
+                    ProfileEvents::increment(ProfileEvents::IcebergMinMaxIndexPrunedFiles, 1);
                     break;
                 }
                 case PruningReturnStatus::PARTITION_PRUNED: {
-                    ++partition_pruned_files;
+                    ProfileEvents::increment(ProfileEvents::IcebergPartitionPrunedFiles, 1);
                     break;
                 }
             }
@@ -195,14 +195,6 @@ std::optional<ManifestFileEntry> SingleThreadIcebergKeysIterator::next()
     }
 
     return std::nullopt;
-}
-
-SingleThreadIcebergKeysIterator::~SingleThreadIcebergKeysIterator()
-{
-    if (partition_pruned_files > 0)
-        ProfileEvents::increment(ProfileEvents::IcebergPartitionPrunedFiles, partition_pruned_files);
-    if (min_max_index_pruned_files > 0)
-        ProfileEvents::increment(ProfileEvents::IcebergMinMaxIndexPrunedFiles, min_max_index_pruned_files);
 }
 
 SingleThreadIcebergKeysIterator::SingleThreadIcebergKeysIterator(
@@ -300,8 +292,9 @@ IcebergIterator::IcebergIterator(
     std::sort(equality_deletes_files.begin(), equality_deletes_files.end());
     std::sort(position_deletes_files.begin(), position_deletes_files.end());
     producer_task.emplace(
-        [this]()
+        [this, thread_group = CurrentThread::getGroup()]()
         {
+            DB::ThreadGroupSwitcher switcher(thread_group, DB::ThreadName::ICEBERG_ITERATOR);
             while (!blocking_queue.isFinished())
             {
                 std::optional<ManifestFileEntry> entry;
--- src/Storages/ObjectStorage/DataLakes/Iceberg/IcebergIterator.h
+++ src/Storages/ObjectStorage/DataLakes/Iceberg/IcebergIterator.h
@@ -50,8 +50,6 @@ public:
 
     std::optional<DB::Iceberg::ManifestFileEntry> next();
 
-    ~SingleThreadIcebergKeysIterator();
-
 private:
     ObjectStoragePtr object_storage;
     std::shared_ptr<const ActionsDAG> filter_dag;
@@ -73,9 +71,6 @@ private:
     std::optional<Iceberg::ManifestFilesPruner> current_pruner;
 
     const Iceberg::ManifestFileContentType manifest_file_content_type;
-
-    size_t min_max_index_pruned_files = 0;
-    size_t partition_pruned_files = 0;
 };
 
 }
