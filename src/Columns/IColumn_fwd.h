32065a05afa06a3499e9740a26124d8eca87e590
Nikita Taranov <nikita.taranov@clickhouse.com>
2026-02-27 00:12:53 +0100

    fix

--- src/Columns/IColumn_fwd.h
+++ src/Columns/IColumn_fwd.h
@@ -23,7 +23,7 @@ void intrusive_ptr_release(const IColumn * c);
 
 using ColumnPtr = COW<IColumn>::Ptr;
 using MutableColumnPtr = COW<IColumn>::MutablePtr;
-using Columns = std::vector<ColumnPtr>; // STYLE_CHECK_ALLOW_STD_STRING_STREAM
-using MutableColumns = std::vector<MutableColumnPtr>; // STYLE_CHECK_ALLOW_STD_STRING_STREAM
-using ColumnRawPtrs = std::vector<const IColumn *>; // STYLE_CHECK_ALLOW_STD_STRING_STREAM
+using Columns = std::vector<ColumnPtr>; // STYLE_CHECK_ALLOW_STD_CONTAINERS
+using MutableColumns = std::vector<MutableColumnPtr>; // STYLE_CHECK_ALLOW_STD_CONTAINERS
+using ColumnRawPtrs = std::vector<const IColumn *>; // STYLE_CHECK_ALLOW_STD_CONTAINERS
 }
