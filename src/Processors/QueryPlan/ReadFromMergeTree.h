8f7ffeef990d1848dfd7f0e98ad60e389fe9b91c
robot-clickhouse <robot-clickhouse@users.noreply.github.com>
2026-02-27 11:24:09 +0000

    Backport #98097 to 25.12: Fix incorrect result of FINAL queries when mixing primary key and non primary key skip indexes

--- src/Processors/QueryPlan/ReadFromMergeTree.h
+++ src/Processors/QueryPlan/ReadFromMergeTree.h
@@ -303,7 +303,6 @@ public:
         bool is_parallel_reading_from_replicas_,
         bool allow_query_condition_cache_);
 
-    static bool areSkipIndexColumnsInPrimaryKey(const Names & primary_key_columns, const UsefulSkipIndexes & skip_indexes, bool any_one);
 
     AnalysisResultPtr selectRangesToRead(bool find_exact_ranges = false) const;
 
