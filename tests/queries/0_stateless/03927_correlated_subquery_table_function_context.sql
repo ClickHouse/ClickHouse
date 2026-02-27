f8938e951aff3e83635fdf8661abdeafad6087f9
Alexey Milovidov <milovidov@clickhouse.com>
2026-02-27 23:03:47 +0100

    Update tests/queries/0_stateless/03927_correlated_subquery_table_function_context.sql
    Co-authored-by: Kirill Kopnev <113183103+Fgrtue@users.noreply.github.com>

--- tests/queries/0_stateless/03927_correlated_subquery_table_function_context.sql
+++ tests/queries/0_stateless/03927_correlated_subquery_table_function_context.sql
@@ -8,5 +8,8 @@ CREATE TABLE t0_03927 (c0 Int) ENGINE = Memory();
 INSERT INTO t0_03927 VALUES (1);
 
 SELECT (SELECT t0_03927.c0 FROM url('http://localhost:8123/?query=SELECT+1', 'CSV') tx) FROM t0_03927;
-
+SELECT * FROM t0_03927 WHERE EXISTS (
+    SELECT 1 FROM url('http://localhost:8123/?query=SELECT+1', 'CSV') tx 
+    WHERE tx.c1 = t0_03927.c0
+);
 DROP TABLE t0_03927;
