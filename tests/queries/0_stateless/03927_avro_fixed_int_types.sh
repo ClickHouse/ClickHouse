d2226752cf87cc5876f433a49b37e4bcafcfe3db
Patrick Pichler <git@patrickpichler.dev>
2026-02-27 18:25:02 +0100

    Add support for inserting Avro Fixed for more Int types
    This commit add support for inserting Avro Fixed for the following
    types:
    * Int8/UInt8
    * Int16/UInt16
    * Int32/UInt32
    * Int64/UInt64
    
    Fixes #98136

---
+++ tests/queries/0_stateless/03927_avro_fixed_int_types.sh
@@ -0,0 +1,27 @@
+#!/usr/bin/env bash
+# Tags: no-fasttest
+
+set -e
+
+CUR_DIR=$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)
+# shellcheck source=../shell_config.sh
+. "$CUR_DIR"/../shell_config.sh
+
+DATA_DIR=$CUR_DIR/data_avro
+
+${CLICKHOUSE_LOCAL} <<EOF
+  CREATE TABLE test (
+    uint8 UInt8,
+    int8 Int8,
+    uint16 UInt16,
+    int16 Int16,
+    uint32 UInt32,
+    int32 Int32,
+    uint64 UInt64,
+    int64 Int64,
+  );
+
+  INSERT INTO test FROM INFILE '$DATA_DIR/fixed.avro' FORMAT avro;
+
+  SELECT * FROM test FORMAT CSV;
+EOF
