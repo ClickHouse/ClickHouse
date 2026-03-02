bedabe0b042494100eaaa64596062a6d31591267
robot-clickhouse <robot-clickhouse@users.noreply.github.com>
2026-03-02 12:24:37 +0000

    Backport #98232 to 26.2: Make jemalloc profiling cover all threads when enabled

--- programs/server/Server.cpp
+++ programs/server/Server.cpp
@@ -1229,7 +1229,7 @@ try
     MainThreadStatus::getInstance();
 
 #if USE_JEMALLOC
-    Jemalloc::setup(
+    Jemalloc::verifySetup(
         server_settings[ServerSetting::jemalloc_enable_global_profiler],
         server_settings[ServerSetting::jemalloc_enable_background_threads],
         server_settings[ServerSetting::jemalloc_max_background_threads_num],
