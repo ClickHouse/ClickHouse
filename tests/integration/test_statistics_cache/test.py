af54ded4835c521d4dffe9a408722c44f2758b29
Alexey Milovidov <milovidov@clickhouse.com>
2026-03-03 00:12:12 +0100

    Revert "Make statistics GA & automatically create `minmax` + `uniq` statistics for new columns"

--- tests/integration/test_statistics_cache/test.py
+++ tests/integration/test_statistics_cache/test.py
@@ -11,7 +11,7 @@ r2  = cluster.add_instance("r2", with_zookeeper=True, stay_alive=True)
 TEST_DB = f"db_sc_{uuid.uuid4().hex[:6]}"
 SET_PREFIX = f"""\
 USE {TEST_DB};
-SET allow_statistics = 1;
+SET allow_experimental_statistics = 1;
 SET use_statistics = 1;
 SET mutations_sync = 2;
 SET log_queries = 1;
@@ -67,7 +67,7 @@ def _create_tbl(node, name, interval):
         CREATE TABLE {name} (k UInt32, v Float64)
         ENGINE=MergeTree
         ORDER BY k
-        SETTINGS refresh_statistics_interval = {interval}, auto_statistics_types = ''
+        SETTINGS refresh_statistics_interval = {interval}
     """)
     _query(node, f"INSERT INTO {name} SELECT number, toFloat64(rand())/4294967296.0 FROM numbers({ROWS_SMALL})")
     _query_retry(node, f"ALTER TABLE {name} ADD STATISTICS v TYPE TDigest")
