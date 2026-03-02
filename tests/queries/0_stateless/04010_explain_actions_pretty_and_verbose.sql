e9917942baa3a15601043eb813534bd364bfab0f
Fgrtue <kirill.kopnev@clickhouse.com>
2026-03-02 16:47:30 +0000

    Simplified tests

--- tests/queries/0_stateless/04010_explain_actions_pretty_and_verbose.sql
+++ tests/queries/0_stateless/04010_explain_actions_pretty_and_verbose.sql
@@ -1,79 +1,25 @@
-CREATE TABLE customer
-(
-    c_custkey     Int32,
-    c_mktsegment  String
-)
-ENGINE = MergeTree
-ORDER BY (c_custkey);
 
-CREATE TABLE orders
-(
-    o_orderkey      Int32,
-    o_custkey       Int32,
-    o_orderdate     Date,
-    o_shippriority  Int32
-)
-ENGINE = MergeTree
-ORDER BY (o_orderkey);
 
-CREATE TABLE lineitem
-(
-    l_orderkey       Int32,
-    l_extendedprice  Decimal(15,2),
-    l_discount       Decimal(15,2),
-    l_shipdate       Date
-)
-ENGINE = MergeTree
-ORDER BY (l_orderkey);
+EXPLAIN actions = 1, verbose = 0
+SELECT number % 10 AS key, sum(number * 2) AS total
+FROM numbers(1000)
+WHERE number > 100
+GROUP BY key
+ORDER BY total DESC
+LIMIT 5;
 
-EXPLAIN actions=1, verbose=0 SELECT
-    l_orderkey,
-    sum(l_extendedprice * (1 - l_discount)) AS revenue,
-    o_orderdate,
-    o_shippriority
-FROM
-    customer,
-    orders,
-    lineitem
-WHERE
-    c_mktsegment = 'BUILDING'
-    AND c_custkey = o_custkey
-    AND l_orderkey = o_orderkey
-    AND o_orderdate < DATE '1995-03-15'
-    AND l_shipdate > DATE '1995-03-15'
-GROUP BY
-    l_orderkey,
-    o_orderdate,
-    o_shippriority
-ORDER BY
-    revenue DESC,
-    o_orderdate
-LIMIT 10;
+EXPLAIN actions = 1, pretty = 1
+SELECT number % 10 AS key, sum(number * 2) AS total
+FROM numbers(1000)
+WHERE number > 100
+GROUP BY key
+ORDER BY total DESC
+LIMIT 5;
 
-EXPLAIN actions=1, pretty=1 SELECT
-    l_orderkey,
-    sum(l_extendedprice * (1 - l_discount)) AS revenue,
-    o_orderdate,
-    o_shippriority
-FROM
-    customer,
-    orders,
-    lineitem
-WHERE
-    c_mktsegment = 'BUILDING'
-    AND c_custkey = o_custkey
-    AND l_orderkey = o_orderkey
-    AND o_orderdate < DATE '1995-03-15'
-    AND l_shipdate > DATE '1995-03-15'
-GROUP BY
-    l_orderkey,
-    o_orderdate,
-    o_shippriority
-ORDER BY
-    revenue DESC,
-    o_orderdate
-LIMIT 10;
-
-DROP TABLE lineitem;
-DROP TABLE customer;
-DROP TABLE orders;
+EXPLAIN actions = 1, pretty = 1, verbose = 0
+SELECT number % 10 AS key, sum(number * 2) AS total
+FROM numbers(1000)
+WHERE number > 100
+GROUP BY key
+ORDER BY total DESC
+LIMIT 5;
