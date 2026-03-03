fe5767cd7addc535133ec116d74a512b05ba981b
Narasimha Pakeer <narasimha.pakeer@clickhouse.com>
2026-03-03 15:36:52 -0600

    Add a test for the fix

---
+++ tests/integration/test_insert_profile_events/test.py
@@ -0,0 +1,63 @@
+import logging
+from time import sleep
+
+import pytest
+
+from helpers.cluster import ClickHouseCluster
+
+cluster = ClickHouseCluster(__file__)
+node = cluster.add_instance(
+    "node", main_configs=["configs/config.xml"],
+    with_zookeeper=True,
+    # Server is very short on memory and may fail while initializing remote database disk
+    with_remote_database_disk=False,
+)
+
+
+@pytest.fixture(scope="module")
+def started_cluster():
+    try:
+        cluster.start()
+        yield cluster
+    finally:
+        cluster.shutdown()
+
+
+def test_insert_profile_events(started_cluster):
+    node = started_cluster.instances["node"]
+    select_insert_query = (
+        "SELECT value FROM system.events WHERE event == 'InsertQuery'"
+    )
+    select_async_insert_query = (
+        "SELECT value FROM system.events WHERE event == 'AsyncInsertQuery'"
+    )
+   
+    non_async_inserts = 1000   
+    async_inserts = 1000    
+    inserts = non_async_inserts  + async_inserts 
+    
+    node.query(
+        "CREATE TABLE test_insert_profile_events  (id UInt32, s String) ENGINE = Memory"
+    )
+
+    for i in range(non_async_inserts):
+       node.query(
+        f"INSERT INTO test_insert_profile_events VALUES ({i},'Non Async Insert')"
+       )
+
+    for i in range(async_inserts):
+       node.query(
+        f"INSERT INTO test_insert_profile_events SETTINGS async_insert = 1 VALUES ({i},'Async Insert')"
+       )
+
+    insert_count = node.query(select_insert_query).strip()
+    async_insert_count = node.query(select_async_insert_query).strip()
+    
+    current_insert_count = int(insert_count) if insert_count else 0
+    current_async_insert_count = int(async_insert_count) if async_insert_count else 0
+ 
+    assert current_insert_count == inserts
+    assert current_async_insert_count == async_inserts
+
+    node.query("DROP TABLE IF EXISTS test_insert_profile_events SYNC")
+
