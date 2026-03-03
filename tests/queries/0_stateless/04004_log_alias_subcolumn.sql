59241b3ca6c320f6495a95754fc78852781a8435
Alexey Milovidov <milovidov@clickhouse.com>
2026-03-03 18:49:26 +0100

    Update 04004_log_alias_subcolumn.sql

--- tests/queries/0_stateless/04004_log_alias_subcolumn.sql
+++ tests/queries/0_stateless/04004_log_alias_subcolumn.sql
@@ -2,6 +2,8 @@
 -- should not cause a LOGICAL_ERROR exception.
 -- https://s3.amazonaws.com/clickhouse-test-reports/json.html?PR=98367&sha=9acc57e1ae51dba197e0d9b12743fb3804683bc4&name_0=PR&name_1=BuzzHouse%20%28arm_asan%29
 
+SET enable_analyzer = 1;
+
 DROP TABLE IF EXISTS t_log_alias;
 CREATE TABLE t_log_alias (c0 Array(String) ALIAS [toString(_table)], c1 Int64) ENGINE = Log;
 INSERT INTO t_log_alias (c1) VALUES (1), (2), (3);
