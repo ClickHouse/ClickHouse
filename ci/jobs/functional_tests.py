1c3fc5bcb1ac52f24cefaa61c593ef681795b40d
Alexei Fedotov <alexei.fedotov@clickhouse.com>
2026-03-06 11:07:48 +0100

    Remove debug, r

--- ci/jobs/functional_tests.py
+++ ci/jobs/functional_tests.py
@@ -747,8 +747,7 @@ def main():
         print("Collect logs")
 
         def collect_logs():
-            #CH.prepare_logs(all=test_result and not test_result.is_ok(), info=info)
-            CH.prepare_logs(all=True, info=info) #REMOVEME
+            CH.prepare_logs(all=test_result and not test_result.is_ok(), info=info)
 
         results.append(
             Result.from_commands_run(
