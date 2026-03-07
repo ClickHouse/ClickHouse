ce6e1d53249d51758f5e944c0314607f9976faa0
Nikita Fomichev <nikita.fomichev@clickhouse.com>
2026-03-07 16:11:34 +0100

    upd

--- ci/jobs/scripts/find_tests.py
+++ ci/jobs/scripts/find_tests.py
@@ -253,13 +253,13 @@ class Targeting:
 
         info = "Tests not found for lines:\n"
         for (file_, line), symbol in not_resolved_file_lines.items():
-            info += f"  {file_}:{line} -> symbol: {symbol[:70] + '...' if symbol else 'NOT FOUND'}\n"
+            info += f"  {file_}:{line} -> symbol: {symbol[:90] + '...' if symbol else 'NOT FOUND'}\n"
         info += "Tests found for lines:\n"
         if not resolved_file_lines:
             info += "  No updates in source code\n"
         else:
             for (file_, line), (symbol, tests) in resolved_file_lines.items():
-                info += f"  {file_}:{line} -> symbol: {symbol[:70]}...\n"
+                info += f"  {file_}:{line} -> symbol: {symbol[:90]}...\n"
                 if len(tests) > max_tests_per_symbol:
                     info += f"    skipping {len(tests)} tests (too common code)\n"
                 else:
@@ -271,6 +271,7 @@ class Targeting:
                 info += f"    ... and {len(selected_tests_list) - 10} more tests\n"
         info += f"Total unique tests: {len(selected_tests)}\n"
         selected_tests = list(selected_tests)
+        print(selected_tests)
         return selected_tests, Result(
             name="tests found by coverage", status=Result.StatusExtended.OK, info=info
         )
