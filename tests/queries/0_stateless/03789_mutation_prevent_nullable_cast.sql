0a7c6da16c88fd3ca3f8d056fe21ac48eeec96b5
Artem Zuikov <chertus@gmail.com>
2026-03-16 15:18:46 +0300

    CI related changes (better test)

--- tests/queries/0_stateless/03789_mutation_prevent_nullable_cast.sql
+++ tests/queries/0_stateless/03789_mutation_prevent_nullable_cast.sql
@@ -35,3 +35,29 @@ UPDATE
 WHERE
     1
 SETTINGS validate_mutation_query = 1; -- { serverError 349 }
+
+TRUNCATE TABLE warden_a;
+
+ALTER TABLE warden_a
+ADD COLUMN IF NOT EXISTS `parsed_salary` Array(Int64);
+ALTER TABLE
+    warden_a
+UPDATE
+    `parsed_salary` = multiIf(
+        salary IS NULL, CAST('[]', 'Array(Int64)'),
+        CAST(
+            coalesce(
+                arrayCompact(
+                    arrayMap(
+                        x -> toInt64OrNull(trimBoth(x)),
+                        splitByString(',', ifNull(salary, ''))
+                    )
+                ),
+                []
+            ),
+            'Array(Int64)'
+        )
+    )
+WHERE
+    1
+SETTINGS validate_mutation_query = 0;
