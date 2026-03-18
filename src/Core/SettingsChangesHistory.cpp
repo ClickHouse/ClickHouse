14e169dbac9d263ebd6a2954f1e88acd60fa20c3
Kaviraj <kavirajkanagaraj@gmail.com>
2026-03-18 22:03:40 +0100

    chore: fix StyleCheck and settings changes duplicates
    Signed-off-by: Kaviraj <kavirajkanagaraj@gmail.com>

--- src/Core/SettingsChangesHistory.cpp
+++ src/Core/SettingsChangesHistory.cpp
@@ -66,7 +66,6 @@ const VersionToSettingsChangesMap & getSettingsChangesHistory()
             {"max_skip_unavailable_shards_num", 0, 0, "New setting to limit the number of shards that can be silently skipped when skip_unavailable_shards is enabled."},
             {"max_skip_unavailable_shards_ratio", 0, 0, "New setting to limit the ratio of shards that can be silently skipped when skip_unavailable_shards is enabled."},
             {"allow_experimental_detach_non_readonly_queries", false, false, "New setting to detach non-readonly queries (run in background thread) and return query_id immediately."},
-            {"allow_experimental_detach_non_readonly_queries", false, false, "New setting to detach non-readonly queries (run in background thread) and return query_id immediately."},
         });
         addSettingsChanges(settings_changes_history, "26.2",
         {
