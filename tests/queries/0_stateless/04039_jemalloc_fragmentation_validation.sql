45b6a0480d0870dc5bf73dfdec5e766bccc61ed3
Narasimha Pakeer <narasimha.pakeer@clickhouse.com>
2026-03-18 18:49:50 -0500

    Handle for negative fragmentation values

--- tests/queries/0_stateless/04039_jemalloc_fragmentation_validation.sql
+++ tests/queries/0_stateless/04039_jemalloc_fragmentation_validation.sql
@@ -1,5 +1,7 @@
 --Test whether jemalloc.fragmentation metric is the difference of active and allocated
 
-SELECT (maxIf(value, name = 'jemalloc.active') - maxIf(value, name = 'jemalloc.allocated')) = maxIf(value, name = 'jemalloc.fragmentation')
+SELECT if(maxIf(value, name = 'jemalloc.active') >= maxIf(value, name = 'jemalloc.allocated'),
+          maxIf(value, name = 'jemalloc.active') - maxIf(value, name = 'jemalloc.allocated'),
+          0) = maxIf(value, name = 'jemalloc.fragmentation')
 FROM system.asynchronous_metrics
 WHERE name IN ('jemalloc.active', 'jemalloc.allocated', 'jemalloc.fragmentation')
