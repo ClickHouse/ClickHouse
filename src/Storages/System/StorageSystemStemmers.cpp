021f18b5a9aa21c936a58d60d43652b8d23ff4c4
Jimmy Aguilar Mena <jimmy.aguilar.mena@clickhouse.com>
2026-03-24 19:08:24 +0000

    Fix issue when NLP setting is disabled

--- src/Storages/System/StorageSystemStemmers.cpp
+++ src/Storages/System/StorageSystemStemmers.cpp
@@ -3,14 +3,25 @@
 #if USE_NLP
 
 #include <Storages/System/StorageSystemStemmers.h>
+#include <Core/Settings.h>
 #include <DataTypes/DataTypeString.h>
 #include <Interpreters/Context.h>
+#include <Common/Exception.h>
 
 #include <libstemmer.h>
 
 
 namespace DB
 {
+namespace ErrorCodes
+{
+    extern const int SUPPORT_IS_DISABLED;
+}
+
+namespace Setting
+{
+    extern const SettingsBool allow_experimental_nlp_functions;
+}
 
 ColumnsDescription StorageSystemStemmers::getColumnsDescription()
 {
@@ -25,8 +36,13 @@ StorageSystemStemmers::StorageSystemStemmers(const StorageID & table_id)
 {
 }
 
-void StorageSystemStemmers::fillData(MutableColumns & res_columns, ContextPtr, const ActionsDAG::Node *, std::vector<UInt8>) const
+void StorageSystemStemmers::fillData(MutableColumns & res_columns, ContextPtr context, const ActionsDAG::Node *, std::vector<UInt8>) const
 {
+    if (!context->getSettingsRef()[Setting::allow_experimental_nlp_functions])
+        throw Exception(ErrorCodes::SUPPORT_IS_DISABLED,
+            "system.stemmers is an experimental table. "
+            "Set `allow_experimental_nlp_functions` setting to enable it");
+
     for (const char ** language = sb_stemmer_list(); *language != nullptr; ++language)
         res_columns[0]->insert(String(*language));
 }
--- tests/queries/0_stateless/03926_system_stemmers.sql
+++ tests/queries/0_stateless/03926_system_stemmers.sql
@@ -1,4 +1,8 @@
 -- Tags: no-fasttest
 -- Tag no-fasttest: depends on libstemmer_c
 
+SET allow_experimental_nlp_functions = 0;
+SELECT * FROM system.stemmers; -- { serverError SUPPORT_IS_DISABLED }
+
+SET allow_experimental_nlp_functions = 1;
 SELECT * FROM system.stemmers ORDER BY ALL;
