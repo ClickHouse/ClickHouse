068508623a8070d3ee1f14c8d5209388c1c3011c
Mikhail Artemenko <mikhail.artemenko@clickhouse.com>
2026-03-27 11:37:05 +0000

    fix mv

--- src/Storages/StorageMaterializedView.cpp
+++ src/Storages/StorageMaterializedView.cpp
@@ -29,6 +29,7 @@
 #include <Storages/StorageFactory.h>
 #include <Storages/ReadInOrderOptimizer.h>
 #include <Storages/SelectQueryDescription.h>
+#include <Storages/VirtualColumnUtils.h>
 
 #include <Common/typeid_cast.h>
 #include <Common/checkStackSize.h>
@@ -379,9 +380,13 @@ void StorageMaterializedView::read(
     if (!has_inner_table && !storage_id.empty() && getInMemoryMetadataPtr()->sql_security_type)
         context->checkAccess(AccessType::SELECT, storage_id, column_names);
 
+    auto physical_column_names = VirtualColumnUtils::filterCommonVirtualColumns(column_names, shared_from_this());
+
     auto src_table_query_info = query_info;
     src_table_query_info.initial_storage_snapshot = storage_snapshot;
-    storage->read(query_plan, column_names, target_storage_snapshot, src_table_query_info, context, processed_stage, max_block_size, num_streams);
+    storage->read(query_plan, physical_column_names, target_storage_snapshot, src_table_query_info, context, processed_stage, max_block_size, num_streams);
+
+    query_plan = VirtualColumnUtils::extendWithCommonVirtualColumns(std::move(query_plan), column_names, shared_from_this());
 
     if (query_plan.isInitialized())
     {
