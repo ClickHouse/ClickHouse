71f72bc3dce19f981058c8a2d5a2ef4ebbb7108d
Matvei Zamiatin <matvey.zamyatin13@gmail.com>
2026-03-28 22:02:26 +0300

    add kinesis to config

--- src/Access/Common/AccessType.h
+++ src/Access/Common/AccessType.h
@@ -44,6 +44,7 @@ enum class Source : uint8_t
     M(RABBITMQ, "RabbitMQ") \
     M(YTSAURUS, "YTsaurus") \
     M(ARROW_FLIGHT, "ArrowFlight") \
+    M(KINESIS, "") \
 
 #define DECLARE_ACCESS_TYPE_OBJECTS_ENUM_CONST(name, aliases) name,
 
@@ -422,6 +423,7 @@ enum class AccessType : uint8_t
     M(RABBITMQ, "", GLOBAL, ALL) \
     M(YTSAURUS, "", GLOBAL, ALL) \
     M(ARROW_FLIGHT, "", GLOBAL, ALL) \
+    M(KINESIS, "", GLOBAL, ALL) \
     M(SOURCES, "", GLOBAL, ALL) \
     \
     /* Consts */ \
