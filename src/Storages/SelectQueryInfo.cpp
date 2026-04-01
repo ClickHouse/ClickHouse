b9bf09cf2eafd049745bee1e6e7ff074333ec376
János Benjamin Antal <benjamin.antal@clickhouse.com>
2026-04-01 13:13:46 +0000

    Revert "Do not change query plan serialization"
    This reverts commit ee271d74fec0b551250f4f61348b168f53901b83.

--- src/Storages/SelectQueryInfo.cpp
+++ src/Storages/SelectQueryInfo.cpp
@@ -54,7 +54,7 @@ PrewhereInfo PrewhereInfo::clone() const
 void PrewhereInfo::serialize(IQueryPlanStep::Serialization & ctx) const
 {
     prewhere_actions.serialize(ctx.out, ctx.registry);
-    writeStringBinary(prewhere_actions.getOutputs()[prewhere_column_position]->result_name, ctx.out);
+    writeVarUInt(prewhere_column_position, ctx.out);
     writeBinary(remove_prewhere_column, ctx.out);
 }
 
@@ -63,9 +63,7 @@ PrewhereInfo PrewhereInfo::deserialize(IQueryPlanStep::Deserialization & ctx)
     PrewhereInfo prewhere_info;
 
     prewhere_info.prewhere_actions = ActionsDAG::deserialize(ctx.in, ctx.registry, ctx.context);
-    String prewhere_column_name;
-    readStringBinary(prewhere_column_name, ctx.in);
-    prewhere_info.prewhere_column_position = prewhere_info.prewhere_actions.findPositionInOutputs(prewhere_column_name);
+    readVarUInt(prewhere_info.prewhere_column_position, ctx.in);
     readBinary(prewhere_info.remove_prewhere_column, ctx.in);
     prewhere_info.need_filter = true;
 
