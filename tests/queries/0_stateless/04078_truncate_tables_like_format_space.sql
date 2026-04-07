f191e7537eb0a0e5877c3a2792d66f9222ae1871
Yarik Briukhovetskyi <114298166+yariks5s@users.noreply.github.com>
2026-04-07 11:39:49 +0000

    Fix for whitespace bug in LIKE parsing

---
+++ tests/queries/0_stateless/04078_truncate_tables_like_format_space.sql
@@ -0,0 +1,4 @@
+SELECT formatQuery('TRUNCATE TABLES FROM default LIKE \'%test%\'');
+SELECT formatQuery('TRUNCATE TABLES FROM default ILIKE \'%test%\'');
+SELECT formatQuery('TRUNCATE TABLES FROM default NOT LIKE \'%test%\'');
+SELECT formatQuery('TRUNCATE TABLES FROM default NOT ILIKE \'%test%\'');
