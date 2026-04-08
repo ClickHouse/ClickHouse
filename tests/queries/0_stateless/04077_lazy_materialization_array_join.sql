bcda4f22a8afe9da6da883ee184ebdbd1f4552d1
vdimir <vdimir@clickhouse.com>
2026-04-08 08:45:57 +0000

    fix 04077_lazy_materialization_array_join

--- tests/queries/0_stateless/04077_lazy_materialization_array_join.sql
+++ tests/queries/0_stateless/04077_lazy_materialization_array_join.sql
@@ -1,5 +1,3 @@
-#!/usr/bin/env -S ${HOME}/clickhouse client --queries-file
-
 SET query_plan_optimize_lazy_materialization = 1;
 SET flatten_nested = 0;
 
@@ -12,10 +10,10 @@ CREATE TABLE t0
 ENGINE = MergeTree
 ORDER BY id;
 
-INSERT INTO t0 SELECT number, arrayMap(x -> (x, arrayMap(y -> (toString(y * x), y + x), range(number % 17))), range(number % 19)) FROM numbers(10);
+INSERT INTO t0 SELECT number, arrayMap(x -> (x, arrayMap(y -> (toString(number), number), range(number))), range(number)) FROM numbers(10);
 
-SELECT arrayJoin(col1.n) FROM t0 ORDER BY id LIMIT 2 OFFSET 2;
+SELECT arrayJoin(col1.n) as e FROM t0 ORDER BY id LIMIT 2 OFFSET 2;
 
 SELECT '----------------------';
 
-SELECT arrayJoin(col1.n) FROM t0 ORDER BY id LIMIT 2 OFFSET 2 SETTINGS optimize_read_in_order=0, query_plan_execute_functions_after_sorting=0;
+SELECT arrayJoin(col1.n) as e FROM t0 ORDER BY id LIMIT 2 OFFSET 2 SETTINGS optimize_read_in_order=0, query_plan_execute_functions_after_sorting=0;
