8b3dac8eb57c1257ec939db7e7916a2bd084dc2b
liyang <sunning@tencent.com>
2026-04-09 19:24:54 +0800

    add default nonexistent_policy tests

--- tests/queries/0_stateless/04075_check_multi_storage_policy.sql
+++ tests/queries/0_stateless/04075_check_multi_storage_policy.sql
@@ -7,6 +7,11 @@ SETTINGS storage_policy = 'nonexistent_policy', storage_policy = 'default';-- { 
 DROP TABLE IF EXISTS test_policy;
 CREATE TABLE test_policy (id UInt64)
     ENGINE = MergeTree() ORDER BY id
+SETTINGS storage_policy = 'default', storage_policy = 'nonexistent_policy';-- { serverError UNKNOWN_POLICY }
+
+DROP TABLE IF EXISTS test_policy;
+CREATE TABLE test_policy (id UInt64)
+    ENGINE = MergeTree() ORDER BY id
 SETTINGS storage_policy = 'nonexistent_policy';-- { serverError UNKNOWN_POLICY }
 
 DROP TABLE IF EXISTS test_policy;
