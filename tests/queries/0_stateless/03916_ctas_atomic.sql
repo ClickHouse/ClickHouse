651eb6652d57645518186f2150c9123ea5363d72
mostafa <sasa1x2001@gmail.com>
2026-04-09 13:26:04 +0200

    Add IFNE to test

--- tests/queries/0_stateless/03916_ctas_atomic.sql
+++ tests/queries/0_stateless/03916_ctas_atomic.sql
@@ -2,3 +2,8 @@ DROP TABLE IF EXISTS test_ctas_failed;
 CREATE TABLE test_ctas_failed ENGINE = MergeTree() ORDER BY n AS
 SELECT number as n FROM numbers(10) WHERE throwIf(n = 4); -- { serverError FUNCTION_THROW_IF_VALUE_IS_NON_ZERO }
 SELECT name FROM system.tables WHERE name = 'test_ctas_failed' AND database = currentDatabase();
+
+DROP TABLE IF EXISTS test_ctas_ifne;
+CREATE TABLE test_ctas_ifne ENGINE = MergeTree() ORDER BY n AS SELECT number as n FROM numbers(5);
+CREATE TABLE IF NOT EXISTS test_ctas_ifne ENGINE = MergeTree() ORDER BY n AS SELECT number as n FROM numbers(10);
+SELECT count() FROM test_ctas_ifne; 
