8ddb4e0d10b5999506a6b8584bffe0bfad9d4af1
Jimmy Aguilar Mena <jimmy.aguilar.mena@clickhouse.com>
2026-04-10 10:38:03 +0000

    Add tests with strings

--- tests/queries/0_stateless/02346_text_index_function_hasAnyAllTokens_bug101913.sql
+++ tests/queries/0_stateless/02346_text_index_function_hasAnyAllTokens_bug101913.sql
@@ -24,5 +24,14 @@ SETTINGS serialize_query_plan = 1, prefer_localhost_replica = 0;
 SELECT count() FROM tab_dist WHERE hasAnyTokens(str, ['xyz'])
 SETTINGS serialize_query_plan = 1, prefer_localhost_replica = 0;
 
+-- Same failure mode with a const String needle (goes through Field::Types::String branch).
+-- 'foo' tokenizes to ['foo']; only row 1 matches.
+SELECT count() FROM tab_dist WHERE hasAllTokens(str, 'foo')
+SETTINGS serialize_query_plan = 1, prefer_localhost_replica = 0;
+
+-- 'foo BAR' tokenizes to ['foo', 'BAR']; rows 1 and 2 match.
+SELECT count() FROM tab_dist WHERE hasAnyTokens(str, 'foo BAR')
+SETTINGS serialize_query_plan = 1, prefer_localhost_replica = 0;
+
 DROP TABLE tab_dist;
 DROP TABLE tab;
