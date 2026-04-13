9d59855e46c66298c7ecf0ae705a4e90872f04cf
vdimir <vdimir@clickhouse.com>
2026-04-13 09:46:22 +0000

    add whitespace to 03037_dynamic_merges

--- tests/queries/0_stateless/03037_dynamic_merges_1_horizontal_compact_wide_tree.sql
+++ tests/queries/0_stateless/03037_dynamic_merges_1_horizontal_compact_wide_tree.sql
@@ -1,7 +1,7 @@
 -- Tags: long, no-tsan, no-msan, no-ubsan, no-asan
 -- Random settings limits: index_granularity=(100, None); merge_max_block_size=(100, None)
 
-set allow_experimental_dynamic_type=1;
+set allow_experimental_dynamic_type = 1;
 
 drop table if exists test;
 create table test (id UInt64, d Dynamic(max_types=2)) engine=MergeTree order by id settings min_rows_for_wide_part=1, min_bytes_for_wide_part=1, vertical_merge_algorithm_min_columns_to_activate=10, index_granularity_bytes=10485760, index_granularity=8192, merge_max_block_size=8192, merge_max_block_size_bytes=10485760, lock_acquire_timeout_for_background_operations=600;
