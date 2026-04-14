88be9f9f6997193eeebacf63522512b76f901000
avogar <avogar@clickhouse.com>
2026-04-14 13:02:50 +0000

    Modify test

--- tests/queries/0_stateless/04093_avro_deeply_nested_schema_stack_overflow.sh
+++ tests/queries/0_stateless/04093_avro_deeply_nested_schema_stack_overflow.sh
@@ -1,36 +1,13 @@
 #!/usr/bin/env bash
-# Tags: no-fasttest
+# Tags: no-fasttest, long
 
 CURDIR=$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)
 # shellcheck source=../shell_config.sh
 . "$CURDIR"/../shell_config.sh
 
-# Generate a deeply nested Avro file that would cause stack overflow
-# without proper recursion depth checks.
-AVRO_FILE="${CLICKHOUSE_TMP}/deeply_nested_${CLICKHOUSE_DATABASE}.avro"
-
-python3 -c "
-import sys, avro.schema, avro.datafile, avro.io
-sys.setrecursionlimit(20000)
-
-depth = 2000
-
-# Build the JSON schema string iteratively to avoid json.dumps recursion.
-schema_str = '{\"type\": \"int\"}'
-for i in range(depth):
-    schema_str = '{\"type\": \"array\", \"items\": ' + schema_str + '}'
-schema_str = '{\"type\": \"record\", \"name\": \"test\", \"fields\": [{\"name\": \"nested\", \"type\": ' + schema_str + '}]}'
-schema = avro.schema.parse(schema_str)
-
-data = 42
-for i in range(depth):
-    data = [data]
-
-with open('${AVRO_FILE}', 'wb') as f:
-    writer = avro.datafile.DataFileWriter(f, avro.io.DatumWriter(), schema)
-    writer.append({'nested': data})
-    writer.close()
-"
+# Use a pre-generated deeply nested Avro file (2000 levels of nested arrays)
+# that would cause stack overflow without proper recursion depth checks.
+AVRO_FILE="${CURDIR}/data_avro/deeply_nested_schema.avro"
 
 # Should not crash (SIGSEGV). May succeed or fail with TOO_DEEP_RECURSION depending on build type.
 ${CLICKHOUSE_LOCAL} -q "SELECT * FROM file('${AVRO_FILE}', 'Avro') FORMAT Null" > /dev/null 2>&1
@@ -38,5 +15,3 @@ ${CLICKHOUSE_LOCAL} -q "SELECT * FROM file('${AVRO_FILE}', 'Avro') FORMAT Null" 
 
 ${CLICKHOUSE_LOCAL} -q "DESC file('${AVRO_FILE}', 'Avro')" > /dev/null 2>&1
 [ $? -lt 128 ] || echo "CRASHED"
-
-rm -f "${AVRO_FILE}"
