3356374b37f617b86413af0c7cf0fe56039a745a
UnamedRus <UnamedRus@users.noreply.github.com>
2026-04-17 16:52:05 +0000

    fix state

--- src/AggregateFunctions/Combinators/AggregateFunctionDistinctBy.cpp
+++ src/AggregateFunctions/Combinators/AggregateFunctionDistinctBy.cpp
@@ -4,9 +4,11 @@
 #include <AggregateFunctions/IAggregateFunction.h>
 #include <AggregateFunctions/KeyHolderHelpers.h>
 #include <Common/HashTable/HashMap.h>
+#include <Common/UnorderedMapWithMemoryTracking.h>
 #include <base/types.h>
 #include <IO/ReadHelpers.h>
 #include <IO/ReadHelpersArena.h>
+#include <IO/ReadBufferFromMemory.h>
 
 #include <DataTypes/Serializations/SerializationWrapper.h>
 #include <IO/WriteHelpers.h>
@@ -30,21 +32,21 @@ namespace
         using Map = HashMapWithStackMemory<Key, UInt64, DefaultHash<Key>, 4>;
         using Self = AggregateFunctionDistinctByNumericKeyData;
         Map map;
+        size_t value_size = 0;
 
         void add(const IColumn ** columns, size_t arg_num, size_t row_num, Arena *)
         {
             const auto & key_vec = assert_cast<const ColumnVector<Key> &>(*columns[arg_num]).getData();
             auto ref = columns[arg_num - 1]->getDataAt(row_num);
-
             UInt64 raw = 0;
-            std::memcpy(&raw, ref.data, ref.size);
+            std::memcpy(&raw, ref.data(), ref.size());
             map[key_vec[row_num]] = raw;
         }
 
         void merge(const Self & rhs, Arena *)
         {
             auto & mutable_rhs_map = const_cast<Map &>(rhs.map);
-            mutable_rhs_map.mergeToViaEmplace(map,[&](UInt64 & dst, UInt64 & src, bool found)
+            mutable_rhs_map.mergeToViaEmplace(map, [&](UInt64 & dst, UInt64 & src, bool found)
             {
                 if (!found)
                     dst = src;
@@ -53,27 +55,38 @@ namespace
 
         void serialize(WriteBuffer & buf) const
         {
-            map.write(buf);
+            writeVarUInt(map.size(), buf);
+            for (const auto & [key, raw_value] : map)
+            {
+                writeBinaryLittleEndian(key, buf);
+                buf.write(reinterpret_cast<const char *>(&raw_value), value_size);
+            }
         }
 
         void deserialize(ReadBuffer & buf, Arena *)
         {
-            map.read(buf);
+            size_t size;
+            readVarUInt(size, buf);
+            for (size_t i = 0; i < size; ++i)
+            {
+                Key key;
+                readBinaryLittleEndian(key, buf);
+                UInt64 raw = 0;
+                buf.readStrict(reinterpret_cast<char *>(&raw), value_size);
+                map[key] = raw;
+            }
         }
 
         MutableColumns getArguments(const DataTypes & argument_types, size_t /* arg_num */) const
         {
             MutableColumns argument_columns;
             argument_columns.emplace_back(argument_types[0]->createColumn());
-            const auto & type = *argument_types[0];
-            const size_t value_size = type.getSizeOfValueInMemory();
-
+            const size_t actual_value_size = argument_types[0]->getSizeOfValueInMemory();
             for (const auto & [_, raw_value] : map)
             {
-                char buffer[sizeof(UInt64)];
-                std::memcpy(buffer, &raw_value, sizeof(UInt64));
-
-                argument_columns[0]->insertData(buffer, value_size);
+                char buffer[sizeof(UInt64)] = {};
+                std::memcpy(buffer, &raw_value, actual_value_size);
+                argument_columns[0]->insertData(buffer, actual_value_size);
             }
             return argument_columns;
         }
@@ -90,7 +103,7 @@ namespace
         void add(const IColumn ** columns, size_t arg_num, size_t row_num, Arena *)
         {
             const auto & key_vec = assert_cast<const ColumnVector<T> &>(*columns[arg_num]).getData();
-            const auto & val_vec = assert_cast<const ColumnVector<T> &>(*columns[arg_num]).getData();
+            const auto & val_vec = assert_cast<const ColumnVector<T> &>(*columns[0]).getData();
             map[key_vec[row_num]] = val_vec[row_num];
         }
 
@@ -106,12 +119,26 @@ namespace
 
         void serialize(WriteBuffer & buf) const
         {
-            map.write(buf);
+            writeVarUInt(map.size(), buf);
+            for (const auto & [key, value] : map)
+            {
+                writeBinaryLittleEndian(key, buf);
+                writeBinaryLittleEndian(value, buf);
+            }
         }
 
         void deserialize(ReadBuffer & buf, Arena *)
         {
-            map.read(buf);
+            size_t size;
+            readVarUInt(size, buf);
+            for (size_t i = 0; i < size; ++i)
+            {
+                T key;
+                readBinaryLittleEndian(key, buf);
+                T value;
+                readBinaryLittleEndian(value, buf);
+                map[key] = value;
+            }
         }
 
         MutableColumns getArguments(const DataTypes & argument_types, size_t /* arg_num */) const
@@ -127,7 +154,7 @@ namespace
 
 struct AggregateFunctionDistinctByGenericData
 {
-    using Map = HashMapWithSavedHash<StringRef, StringRef, StringRefHash>;
+    using Map = HashMapWithSavedHash<std::string_view, std::string_view, StringViewHash>;
     using Self = AggregateFunctionDistinctByGenericData;
     Map map;
 
@@ -135,7 +162,7 @@ struct AggregateFunctionDistinctByGenericData
     {
         const IColumn * key_column = columns[arg_num];
         const char * begin = nullptr;
-        StringRef key = key_column->serializeValueIntoArena(row_num, *arena, begin);
+        std::string_view key = key_column->serializeValueIntoArena(row_num, *arena, begin, nullptr);
 
         typename Map::LookupResult it;
         bool inserted;
@@ -143,14 +170,13 @@ struct AggregateFunctionDistinctByGenericData
         if (inserted)
         {
             const char * val_begin = nullptr;
-            StringRef value(val_begin, 0);
+            size_t total_size = 0;
             for (size_t i = 0; i < arg_num; ++i)
             {
-                auto cur = columns[i]->serializeValueIntoArena(row_num, *arena, val_begin);
-                value.data = cur.data - value.size;
-                value.size += cur.size;
+                auto cur = columns[i]->serializeValueIntoArena(row_num, *arena, val_begin, nullptr);
+                total_size += cur.size();
             }
-            it->getMapped() = value;
+            it->getMapped() = std::string_view(val_begin, total_size);
             //nested_func->add(nested_place, columns, row_num, arena);
         }
     }
@@ -158,10 +184,10 @@ struct AggregateFunctionDistinctByGenericData
     void merge(const Self & rhs, Arena * arena)
     {
         auto & mutable_rhs_map = const_cast<Map &>(rhs.map);
-        mutable_rhs_map.mergeToViaEmplace(map,[&](StringRef dst, StringRef src, bool found)
+        mutable_rhs_map.mergeToViaEmplace(map, [&](std::string_view & dst, std::string_view & src, bool found)
         {
             if (!found)
-                dst = StringRef(arena->insert(src.data, src.size));
+                dst = std::string_view(arena->insert(src.data(), src.size()), src.size());
         });
     }
 
@@ -181,8 +207,8 @@ struct AggregateFunctionDistinctByGenericData
         readVarUInt(size, buf);
         for (size_t i = 0; i < size; ++i)
         {
-            StringRef key = readStringBinaryInto(*arena, buf);
-            StringRef value = readStringBinaryInto(*arena, buf);
+            std::string_view key = readStringBinaryInto(*arena, buf);
+            std::string_view value = readStringBinaryInto(*arena, buf);
             typename Map::LookupResult it;
             bool inserted;
             map.emplace(ArenaKeyHolder{key, *arena}, it, inserted);
@@ -198,9 +224,10 @@ struct AggregateFunctionDistinctByGenericData
 
         for (const auto & elem : map)
         {
-            const char * begin = elem.getMapped().data;
+            auto mapped = elem.getMapped();
+            ReadBufferFromMemory buf(mapped.data(), mapped.size());
             for (size_t i = 0; i < arg_num; ++i)
-                begin = argument_columns[i]->deserializeAndInsertFromArena(begin);
+                argument_columns[i]->deserializeAndInsertFromArena(buf, nullptr);
         }
         return argument_columns;
     }
@@ -264,7 +291,8 @@ public:
     void create(AggregateDataPtr __restrict place) const override
     {
         new (place) Data;
-        nested_func->create(place);
+        if constexpr (requires (Data & d) { d.value_size; })
+            this->data(place).value_size = this->argument_types[0]->getSizeOfValueInMemory();
     }
 
     void destroy(AggregateDataPtr place) const noexcept override
@@ -361,8 +389,7 @@ public:
                 res.reset(createWithNumericType<
                     AggregateFunctionDistinctBy,
                     AggregateFunctionDistinctByNumericData>(*arguments[1], nested_function, arguments, params));
-
-            if (arguments[0]->getSizeOfValueInMemory() <= sizeof(UInt64))
+            else if (arguments[0]->getSizeOfValueInMemory() <= sizeof(UInt64))
                 res.reset(createWithNumericType<
                     AggregateFunctionDistinctBy,
                     AggregateFunctionDistinctByNumericKeyData>(*arguments[1], nested_function, arguments, params));
---
+++ tests/queries/0_stateless/04093_distinct_by_combinator.sql
@@ -0,0 +1,123 @@
+SET max_block_size = 100;
+
+-- Basic correctness: same numeric type (NumericData path: value type == key type)
+SELECT sumDistinctBy(k, k) FROM (SELECT number % 5 AS k FROM numbers_mt(10000));
+
+-- Basic correctness: different numeric sizes, 1-byte value (NumericKeyData path, value_size=1)
+SELECT sumDistinctBy(toUInt8(number % 5), toUInt32(number % 5)) FROM numbers_mt(10000);
+
+-- Basic correctness: different numeric sizes, 2-byte value (NumericKeyData path, value_size=2)
+SELECT sumDistinctBy(toUInt16(number % 5), toUInt32(number % 5)) FROM numbers_mt(10000);
+
+-- Basic correctness: different numeric sizes, 8-byte value (NumericKeyData path, value_size=8)
+SELECT sumDistinctBy(toUInt64(number % 5), toUInt32(number % 5)) FROM numbers_mt(10000);
+
+-- Basic correctness: String key (GenericData path)
+SELECT sumDistinctBy(number % 5, toString(number % 5)) FROM numbers_mt(10000);
+
+-- GenericData path: Tuple key (composite), LCM(3,7)=21 distinct pairs
+SELECT countDistinctBy(1, (number % 3, number % 7)) FROM numbers_mt(21000);
+
+-- GenericData path: String value and String key
+SELECT arraySort(groupArrayDistinctBy(toString(number % 5), toString(number % 5))) FROM numbers_mt(10000);
+
+-- Multiple groups: confirm per-group deduplication
+SELECT grp, sumDistinctBy(k, k)
+FROM (SELECT number % 4 AS grp, number % 5 AS k FROM numbers_mt(10000))
+GROUP BY grp
+ORDER BY grp;
+
+-- Multi-threading: NumericData path
+SELECT sumDistinctBy(k, k)
+FROM (SELECT number % 100 AS k FROM numbers_mt(1000000));
+
+-- Multi-threading: NumericKeyData path
+SELECT sumDistinctBy(toUInt16(number % 100), toUInt64(number % 100))
+FROM numbers_mt(1000000);
+
+-- Multi-threading: GenericData path (String key)
+SELECT countDistinctBy(1, toString(number % 50)) FROM numbers_mt(1000000);
+
+-- Multi-threading: GenericData path (Tuple key)
+SELECT countDistinctBy(1, (number % 7, number % 11)) FROM numbers_mt(77000);
+
+-- Error: too few arguments
+SELECT sumDistinctBy(number) FROM numbers_mt(10000); -- { serverError NUMBER_OF_ARGUMENTS_DOESNT_MATCH }
+
+-- AggregatingMergeTree: NumericData path (same types, UInt64 value + UInt64 key)
+DROP TABLE IF EXISTS test_distinct_by_num;
+CREATE TABLE test_distinct_by_num
+(
+    grp UInt32,
+    s   AggregateFunction(sumDistinctBy, UInt64, UInt64)
+) ENGINE = AggregatingMergeTree() ORDER BY grp;
+
+INSERT INTO test_distinct_by_num
+SELECT grp, sumDistinctByState(k, k)
+FROM (SELECT number % 3 AS grp, number % 5 AS k FROM numbers_mt(15000))
+GROUP BY grp;
+
+SELECT grp, sumDistinctByMerge(s) FROM test_distinct_by_num GROUP BY grp ORDER BY grp;
+
+INSERT INTO test_distinct_by_num
+SELECT grp, sumDistinctByState(k, k)
+FROM (SELECT number % 3 AS grp, (number % 5) + 5 AS k FROM numbers_mt(15000))
+GROUP BY grp;
+
+OPTIMIZE TABLE test_distinct_by_num FINAL;
+
+SELECT grp, sumDistinctByMerge(s) FROM test_distinct_by_num GROUP BY grp ORDER BY grp;
+
+DROP TABLE test_distinct_by_num;
+
+-- AggregatingMergeTree: NumericKeyData path (UInt8 value + UInt32 key, value_size=1)
+DROP TABLE IF EXISTS test_distinct_by_keydata;
+CREATE TABLE test_distinct_by_keydata
+(
+    grp UInt32,
+    s   AggregateFunction(sumDistinctBy, UInt8, UInt32)
+) ENGINE = AggregatingMergeTree() ORDER BY grp;
+
+INSERT INTO test_distinct_by_keydata
+SELECT grp, sumDistinctByState(toUInt8(k), toUInt32(k))
+FROM (SELECT number % 3 AS grp, number % 5 AS k FROM numbers_mt(15000))
+GROUP BY grp;
+
+SELECT grp, sumDistinctByMerge(s) FROM test_distinct_by_keydata GROUP BY grp ORDER BY grp;
+
+INSERT INTO test_distinct_by_keydata
+SELECT grp, sumDistinctByState(toUInt8(k), toUInt32(k))
+FROM (SELECT number % 3 AS grp, (number % 5) + 5 AS k FROM numbers_mt(15000))
+GROUP BY grp;
+
+OPTIMIZE TABLE test_distinct_by_keydata FINAL;
+
+SELECT grp, sumDistinctByMerge(s) FROM test_distinct_by_keydata GROUP BY grp ORDER BY grp;
+
+DROP TABLE test_distinct_by_keydata;
+
+-- AggregatingMergeTree: GenericData path (UInt64 value + String key)
+DROP TABLE IF EXISTS test_distinct_by_str;
+CREATE TABLE test_distinct_by_str
+(
+    grp UInt32,
+    s   AggregateFunction(sumDistinctBy, UInt64, String)
+) ENGINE = AggregatingMergeTree() ORDER BY grp;
+
+INSERT INTO test_distinct_by_str
+SELECT grp, sumDistinctByState(k, toString(k))
+FROM (SELECT number % 3 AS grp, number % 5 AS k FROM numbers_mt(15000))
+GROUP BY grp;
+
+SELECT grp, sumDistinctByMerge(s) FROM test_distinct_by_str GROUP BY grp ORDER BY grp;
+
+INSERT INTO test_distinct_by_str
+SELECT grp, sumDistinctByState(k, toString(k))
+FROM (SELECT number % 3 AS grp, (number % 5) + 5 AS k FROM numbers_mt(15000))
+GROUP BY grp;
+
+OPTIMIZE TABLE test_distinct_by_str FINAL;
+
+SELECT grp, sumDistinctByMerge(s) FROM test_distinct_by_str GROUP BY grp ORDER BY grp;
+
+DROP TABLE test_distinct_by_str;
