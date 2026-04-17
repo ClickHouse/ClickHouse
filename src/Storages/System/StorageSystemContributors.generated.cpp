3f6c029ea990d9a368094e971a131a949776a0d0
robot-clickhouse <robot-clickhouse@users.noreply.github.com>
2026-04-17 18:01:21 +0000

    Update autogenerated version to 25.8.22.28 and contributors

--- src/Storages/System/StorageSystemContributors.generated.cpp
+++ src/Storages/System/StorageSystemContributors.generated.cpp
@@ -1089,6 +1089,7 @@ const char * auto_contributors[] {
     "Rafael Acevedo",
     "Rafael David Tinoco",
     "Rafael Roquetto",
+    "Rahul",
     "Rajakavitha Kodhandapani",
     "Rajkumar",
     "Rajkumar Varada",
