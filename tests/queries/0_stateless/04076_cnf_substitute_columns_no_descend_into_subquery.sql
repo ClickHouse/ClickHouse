ed39539e15f4a3c8783b66f1a0475c8bd46ab765
Pavel Kruglov <48961922+Avogar@users.noreply.github.com>
2026-04-17 15:07:20 +0200

    Update test

--- tests/queries/0_stateless/04076_cnf_substitute_columns_no_descend_into_subquery.sql
+++ tests/queries/0_stateless/04076_cnf_substitute_columns_no_descend_into_subquery.sql
@@ -2,8 +2,6 @@ SET optimize_substitute_columns = 1, convert_query_to_cnf = 1, enable_analyzer =
 
 CREATE TABLE t_cnf_subquery_subst (a Int64, d Int32, CONSTRAINT c1 ASSUME a = d) ENGINE = TinyLog;
 
--- Used to produce: Logical error: 'Bad cast from type DB::FunctionNode to DB::ColumnNode'
--- Now consistently raises NOT_IMPLEMENTED (same as Memory/MergeTree without the bug).
 SELECT anyLastDistinct((SELECT 1 WHERE a = 1)) FROM t_cnf_subquery_subst WHERE isNotNull(a); -- { serverError NOT_IMPLEMENTED }
 
 DROP TABLE t_cnf_subquery_subst;
