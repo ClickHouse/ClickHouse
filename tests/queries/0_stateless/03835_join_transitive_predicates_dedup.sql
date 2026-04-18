a3e14f6c18168fe87747121cac59e499f5136a45
Alexander Gololobov <davenger@clickhouse.com>
2026-04-18 19:58:14 +0200

    Stabilize test

--- tests/queries/0_stateless/03835_join_transitive_predicates_dedup.sql
+++ tests/queries/0_stateless/03835_join_transitive_predicates_dedup.sql
@@ -3,6 +3,7 @@
 SET allow_experimental_analyzer = 1;
 SET query_plan_optimize_join_order_limit = 10;
 SET use_statistics = 1;
+SET materialize_statistics_on_insert = 1;
 SET query_plan_join_swap_table = 'auto';
 SET enable_join_runtime_filters = 0;
 SET enable_parallel_replicas = 0;
