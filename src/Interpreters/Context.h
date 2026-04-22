250f9bc89d9927fc621d4f54172ada316a130dc2
robot-clickhouse <robot-clickhouse@users.noreply.github.com>
2026-04-22 11:37:31 +0000

    Backport #103285 to 26.1: Reduce default HTTP header limits to prevent pre-auth memory exhaustion

--- src/Interpreters/Context.h
+++ src/Interpreters/Context.h
@@ -1809,6 +1809,10 @@ struct HTTPContext : public IHTTPContext
 
     uint64_t getMaxFieldValueSize() const override;
 
+    uint64_t getMaxRequestHeaderSize() const override;
+
+    Poco::Timespan getHeadersReadTimeout() const override;
+
     Poco::Timespan getReceiveTimeout() const override;
 
     Poco::Timespan getSendTimeout() const override;
