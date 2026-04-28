84c5324bc83638fc286e46030defb75a708f314b
Hechem Selmi <hechem.selmi@clickhouse.com>
2026-04-28 10:07:35 +0900

    update tests.

--- tests/queries/0_stateless/04003_join_lazy_indexing.sql
+++ tests/queries/0_stateless/04003_join_lazy_indexing.sql
@@ -1,63 +1,60 @@
 -- Test lazy columns indexing in joins with small limit
 
-SET enable_analyzer = 1;
+SET query_plan_min_columns_for_join_lazy_indexing = 1;
+SET query_plan_optimize_join_order_limit = 1;
+SET query_plan_join_swap_table = false;
 SET join_algorithm = 'hash';
 
 DROP TABLE IF EXISTS t1;
 DROP TABLE IF EXISTS t2;
 DROP TABLE IF EXISTS t3;
 
-CREATE TABLE t1 (a UInt64, b String) ENGINE = MergeTree ORDER BY a;
-CREATE TABLE t2 (a UInt64, b String) ENGINE = MergeTree ORDER BY a;
-CREATE TABLE t3 (a UInt64, b String) ENGINE = MergeTree ORDER BY a;
+CREATE TABLE t1 (a UInt64, b UInt64, c String) ENGINE = MergeTree ORDER BY a;
+CREATE TABLE t2 (a UInt64, b UInt64, c String) ENGINE = MergeTree ORDER BY a;
+CREATE TABLE t3 (a UInt64, c String) ENGINE = MergeTree ORDER BY a;
 
-INSERT INTO t1 SELECT number, toString(number) FROM numbers(10000);
-INSERT INTO t2 SELECT number, toString(number) FROM numbers(100);
+INSERT INTO t1 SELECT number, number, toString(number) FROM numbers(10000);
+INSERT INTO t2 SELECT number, 50, toString(number) FROM numbers(100);
 INSERT INTO t3 SELECT number, toString(number) FROM numbers(100);
 
 -- Join followed by LIMIT
 SELECT count(*) FROM (
     SELECT * FROM t1 JOIN t2 ON t1.a = t2.a LIMIT 5
-) SETTINGS query_plan_min_columns_for_join_lazy_indexing = 0;
+);
 
+-- Join followed by filter and LIMIT
 SELECT count(*) FROM (
-    SELECT * FROM t1 JOIN t2 ON t1.a = t2.a LIMIT 5
-) SETTINGS query_plan_min_columns_for_join_lazy_indexing = 1;
+    SELECT * FROM t1 JOIN t2 ON t1.a = t2.a WHERE t1.b < t2.b LIMIT 5
+);
 
 -- Join followed by ORDER BY + LIMIT
 SELECT count(*) FROM (
     SELECT * FROM t1 JOIN t2 ON t1.a = t2.a ORDER BY t1.a LIMIT 5
-) SETTINGS query_plan_min_columns_for_join_lazy_indexing = 0;
+);
 
+-- Two joins
 SELECT count(*) FROM (
-    SELECT * FROM t1 JOIN t2 ON t1.a = t2.a ORDER BY t1.a LIMIT 5
-) SETTINGS query_plan_min_columns_for_join_lazy_indexing = 1;
+    SELECT * FROM t1 JOIN t2 ON t1.a = t2.a JOIN t3 ON t1.a == t3.a
+);
+
+-- Filter between joins
+SELECT count(*) FROM (
+    SELECT * FROM t1 JOIN t2 ON t1.a = t2.a JOIN t3 ON t1.a == t3.a WHERE t1.b < t2.b
+);
 
 -- Two joins followed by LIMIT
 SELECT count(*) FROM (
     SELECT * FROM t1 JOIN t2 ON t1.a = t2.a JOIN t3 ON t1.a == t3.a LIMIT 5
-) SETTINGS query_plan_min_columns_for_join_lazy_indexing = 0;
+);
 
-SELECT count(*) FROM (
-    SELECT * FROM t1 JOIN t2 ON t1.a = t2.a JOIN t3 ON t1.a == t3.a LIMIT 5
-) SETTINGS query_plan_min_columns_for_join_lazy_indexing = 1;
-
--- Limit between joins
+-- LIMIT between joins
 SELECT count() FROM (
     SELECT *
     FROM (
         SELECT * FROM t1 JOIN t2 ON t1.a = t2.a LIMIT 5
     ) sub
     JOIN t3 ON sub.a = t3.a
-) SETTINGS query_plan_min_columns_for_join_lazy_indexing = 0;
-
-SELECT count() FROM (
-    SELECT *
-    FROM (
-        SELECT * FROM t1 JOIN t2 ON t1.a = t2.a LIMIT 5
-    ) sub
-    JOIN t3 ON sub.a = t3.a
-) SETTINGS query_plan_min_columns_for_join_lazy_indexing = 1;
+);
 
 DROP TABLE t1;
 DROP TABLE t2;
