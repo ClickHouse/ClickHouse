7150465ab57b335f888a453eb3b4ef108d24dc31
Alexander Gololobov <davenger@clickhouse.com>
2026-04-30 16:42:57 +0900

    Stabilize test

--- tests/queries/0_stateless/02498_storage_join_key_positions.sql
+++ tests/queries/0_stateless/02498_storage_join_key_positions.sql
@@ -60,8 +60,8 @@ SELECT * FROM t1 ALL INNER JOIN tj ON t1.key1 = tj.key1; -- { serverError INCOMP
 SELECT * FROM t1 ALL INNER JOIN tj ON t1.key1 = tj.key1 AND t1.key2 = tj.key2 AND t1.key3 = tj.attr; -- { serverError INCOMPATIBLE_TYPE_OF_JOIN }
 SELECT * FROM t1 ALL INNER JOIN tj ON t1.key1 = tj.key1 AND t1.key2 = tj.key2 AND t1.key3 = tj.key3 AND t1.key1 = tj.key1 SETTINGS enable_join_transitive_predicates = 0; -- { serverError INCOMPATIBLE_TYPE_OF_JOIN }
 
-SELECT '--- duplicated predicated removed ---';
-SELECT * FROM t1 ALL INNER JOIN tj ON t1.key1 = tj.key1 AND t1.key2 = tj.key2 AND t1.key3 = tj.key3 AND t1.key1 = tj.key1 SETTINGS enable_join_transitive_predicates = 1;
+SELECT '--- duplicated predicate removed ---';
+SELECT * FROM t1 ALL INNER JOIN tj ON t1.key1 = tj.key1 AND t1.key2 = tj.key2 AND t1.key3 = tj.key3 AND t1.key1 = tj.key1 SETTINGS enable_join_transitive_predicates = 1, query_plan_optimize_join_order_limit = 10;
 
 SELECT '--- reuse column from left ---';
 SELECT * FROM t1 ALL INNER JOIN tjj ON t1.key1 = tjj.key1 AND t1.key1 = tjj.key2 AND t1.key1 = tjj.key3 ORDER BY t1.key1;
