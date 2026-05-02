d88d4d1f7673f64d07bbc168ad4b46ba60353e3e
mostafa <sasa1x2001@gmail.com>
2026-05-02 21:30:33 +0300

    Add optimize_row_order_if_no_order_by

---
+++ tests/queries/0_stateless/04146_merge_tree_optimize_row_order_if_no_order_by.sql
@@ -0,0 +1,18 @@
+SET max_insert_threads = 1;
+
+DROP TABLE IF EXISTS tab;
+
+CREATE TABLE tab (
+    name String,
+    timestamp Int64,
+    money UInt8,
+    flag String
+) ENGINE = MergeTree
+ORDER BY ()
+    -- Disable add_minmax_index_for_numeric_columns since it affects the order
+SETTINGS optimize_row_order_if_no_order_by = True, add_minmax_index_for_numeric_columns=0;
+INSERT INTO tab VALUES ('Bob', 4, 100, '1'), ('Nikita', 2, 54, '1'), ('Nikita', 1, 228, '1'), ('Alex', 4, 83, '1'), ('Alex', 4, 134, '1'), ('Alex', 1, 65, '0'), ('Alex', 4, 134, '1'), ('Bob', 2, 53, '0'), ('Alex', 4, 83, '0'), ('Alex', 1, 63, '1'), ('Bob', 2, 53, '1'), ('Alex', 4, 192, '1'), ('Alex', 2, 128, '1'), ('Nikita', 2, 148, '0'), ('Bob', 4, 177, '0'), ('Nikita', 1, 173, '0'), ('Alex', 1, 239, '0'), ('Alex', 1, 63, '0'), ('Alex', 2, 224, '1'), ('Bob', 4, 177, '0'), ('Alex', 2, 128, '1'), ('Alex', 4, 134, '0'), ('Alex', 4, 83, '1'), ('Bob', 4, 100, '0'), ('Nikita', 2, 54, '1'), ('Alex', 1, 239, '1'), ('Bob', 2, 187, '1'), ('Alex', 1, 65, '1'), ('Bob', 2, 53, '1'), ('Alex', 2, 224, '0'), ('Alex', 4, 192, '0'), ('Nikita', 1, 173, '1'), ('Nikita', 2, 148, '1'), ('Bob', 2, 187, '1'), ('Nikita', 2, 208, '1'), ('Nikita', 2, 208, '0'), ('Nikita', 1, 228, '0'), ('Nikita', 2, 148, '0');
+
+SELECT * FROM tab SETTINGS max_threads=1;
+
+DROP TABLE tab;
