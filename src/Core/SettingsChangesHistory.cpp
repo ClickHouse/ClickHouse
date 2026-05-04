d4b469e03a92a07d878dd6fc66ce9b402a4d8142
vdimir <vdimir@clickhouse.com>
2026-05-04 15:13:35 +0000

    Make query_plan_use_logical_join_step setting obsolete

--- src/Core/SettingsChangesHistory.cpp
+++ src/Core/SettingsChangesHistory.cpp
@@ -57,6 +57,8 @@ const VersionToSettingsChangesMap & getSettingsChangesHistory()
             {"max_streams_for_union_step", 0, 0, "New setting to limit the number of simultaneously active data streams in a UNION step to reduce peak memory usage."},
             {"max_streams_for_union_step_to_max_threads_ratio", 0, 8, "New setting: the limit on simultaneously active streams in a UNION step is computed as min(max_streams_for_union_step, max_threads * max_streams_for_union_step_to_max_threads_ratio), either being 0 disables that input."},
             {"send_table_structure_on_insert_with_inline_data", true, true, "New setting to control whether server sends table structure for INSERT queries with inline data."},
+            {"query_plan_use_logical_join_step", true, true, "Obsolete setting, the logical join step is now always used."},
+            {"query_plan_use_new_logical_join_step", true, true, "Obsolete setting, the logical join step is now always used."},
         });
         addSettingsChanges(settings_changes_history, "26.4",
         {
