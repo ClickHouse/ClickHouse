869ff25c0eb1d18ee24ef135ce3b3e02c49fa520
Pedro Ferreira <pedro.silva.ferreira@clickhouse.com>
2026-05-05 11:49:38 +0000

    Add comments back

--- tests/queries/0_stateless/03916_system_table.sql
+++ tests/queries/0_stateless/03916_system_table.sql
@@ -1,4 +1,5 @@
 -- Tags: no-parallel
+-- Other tests can enable failpoints and interfere with this test
 
 -- Basic: table exists and returns rows
 SELECT count() > 0 FROM system.fail_points;
@@ -8,6 +9,11 @@ SELECT name, type FROM system.columns WHERE database = 'system' AND table = 'fai
 
 -- All four types are present
 SELECT type, count() > 0 FROM system.fail_points GROUP BY type ORDER BY type;
+-- Expected:
+-- once            1
+-- regular         1
+-- pauseable_once  1
+-- pauseable       1
 
 -- Filtering by type works
 SELECT count() > 0 FROM system.fail_points WHERE type = 'pauseable';
