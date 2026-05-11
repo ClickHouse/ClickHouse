2e9111f9e587f0e307f87556a37646f22d81ba0a
Pedro Ferreira <pedro.silva.ferreira@clickhouse.com>
2026-05-11 14:00:27 +0000

    Update job config

--- ci/defs/job_configs.py
+++ ci/defs/job_configs.py
@@ -1240,43 +1240,6 @@ class JobConfigs:
             requires=[ArtifactNames.DEB_AMD_TSAN],
         ),
     )
-    buzz_fuzzer_serverfuzz_jobs = Job.Config(
-        name=JobNames.BUZZHOUSE,
-        runs_on=[],  # from parametrize()
-        command="python3 ./ci/jobs/buzzhouse_job.py",
-        digest_config=Job.CacheDigestConfig(
-            include_paths=[
-                "./ci/docker/fuzzer",
-                "./ci/jobs/buzzhouse_job.py",
-                "./ci/jobs/ast_fuzzer_job.py",
-                "./ci/jobs/scripts/log_parser.py",
-                "./ci/jobs/scripts/functional_tests/setup_log_cluster.sh",
-                "./ci/jobs/scripts/fuzzer/",
-                "./ci/docker/fuzzer",
-            ],
-        ),
-    ).parametrize(
-        Job.ParamSet(
-            parameter="experimental, serverfuzz, amd_debug",
-            runs_on=RunnerLabels.AMD_MEDIUM,
-            requires=[ArtifactNames.CH_AMD_DEBUG],
-        ),
-        Job.ParamSet(
-            parameter="experimental, serverfuzz, arm_asan_ubsan",
-            runs_on=RunnerLabels.ARM_MEDIUM,
-            requires=[ArtifactNames.CH_ARM_ASAN_UBSAN],
-        ),
-        Job.ParamSet(
-            parameter="experimental, serverfuzz, amd_tsan",
-            runs_on=RunnerLabels.AMD_MEDIUM,
-            requires=[ArtifactNames.CH_AMD_TSAN],
-        ),
-        Job.ParamSet(
-            parameter="experimental, serverfuzz, amd_msan",
-            runs_on=RunnerLabels.AMD_MEDIUM,
-            requires=[ArtifactNames.CH_AMD_MSAN],
-        ),
-    )
     performance_comparison_with_master_head_jobs = Job.Config(
         name=JobNames.PERFORMANCE,
         runs_on=["#from param"],
