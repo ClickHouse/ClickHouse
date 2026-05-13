ec73c289a9310e84de0f6fc53e906bb9b1b5f40a
robot-clickhouse <robot-clickhouse@users.noreply.github.com>
2026-05-13 07:22:56 +0000

    Backport #98460 to 26.2: Fix null pointer dereference in dictGetOrDefault with nullable key

--- src/Functions/FunctionsExternalDictionaries.h
+++ src/Functions/FunctionsExternalDictionaries.h
@@ -721,9 +721,9 @@ private:
                 result = dictionary->getColumn(attribute_names[0], attribute_type, key_columns, key_types, default_mask);
 
                 auto [defaults_column, mask_column] =
-                    getDefaultsShortCircuit(std::move(default_mask), result_type, last_argument);
+                    getDefaultsShortCircuit(std::move(default_mask), attribute_type, last_argument);
 
-                restoreShortCircuitColumn(result, defaults_column, mask_column, result_type);
+                restoreShortCircuitColumn(result, defaults_column, mask_column, attribute_type);
             }
             else
             {
