f306138cb92ad211b94d55e21f51b5e7abc9b083
Mikhail Artemenko <mikhail.artemenko@clickhouse.com>
2026-05-15 09:39:23 +0000

    allow to start streaming query on empty table

--- src/Storages/MergeTree/MergeTreeDataSelectExecutor.cpp
+++ src/Storages/MergeTree/MergeTreeDataSelectExecutor.cpp
@@ -1516,13 +1516,18 @@ QueryPlanStepPtr MergeTreeDataSelectExecutor::readFromParts(
     /// If merge_tree_select_result_ptr != nullptr, we use analyzed result so parts will always be empty.
     if (merge_tree_select_result_ptr)
     {
-        if (merge_tree_select_result_ptr->parts_with_ranges.empty())
+        if (!query_info.isStream() && merge_tree_select_result_ptr->parts_with_ranges.empty())
             return {};
     }
     /// If merge_tree_enable_remove_parts_from_snapshot_optimization is true it nukes our list of parts
     else if (!parts)
-        return {};
-    else if (parts->empty())
+    {
+        if (!query_info.isStream())
+            return {};
+
+        parts = std::make_shared<const RangesInDataParts>();
+    }
+    else if (parts->empty() && !query_info.isStream())
         return {};
 
     return std::make_unique<ReadFromMergeTree>(
