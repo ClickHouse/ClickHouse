01892f617390fa6fa3ebbcd542aa9cb8aa82196c
Mikhail Artemenko <mikhail.artemenko@clickhouse.com>
2026-05-16 16:06:40 +0000

    update test tags

--- tests/queries/0_stateless/04241_streaming_replicated_merge_tree.sh
+++ tests/queries/0_stateless/04241_streaming_replicated_merge_tree.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
-# Tags: zookeeper, no-shared-merge-tree
+# Tags: long, zookeeper, no-shared-merge-tree
 
 CURDIR=$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)
 # shellcheck source=../shell_config.sh
