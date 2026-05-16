5899817ce2178af4e02b77082241128647ea002c
Alexey Milovidov <milovidov@clickhouse.com>
2026-05-16 09:33:14 +0000

    Fix whitespace bug in SHOW COLUMNS LIKE formatting
    Address review: apply the same fix symmetrically to
    `ASTShowColumnsQuery::formatQueryImpl`, which used `" LIKE"` (no trailing
    space) before `quoteString(like)` and produced malformed output like
    `LIKE'%x%'`. Extend the stateless `formatQuery` test to cover this path.

--- tests/queries/0_stateless/04078_truncate_tables_like_format_space.sql
+++ tests/queries/0_stateless/04078_truncate_tables_like_format_space.sql
@@ -2,3 +2,7 @@ SELECT formatQuery('TRUNCATE TABLES FROM default LIKE \'%test%\'');
 SELECT formatQuery('TRUNCATE TABLES FROM default ILIKE \'%test%\'');
 SELECT formatQuery('TRUNCATE TABLES FROM default NOT LIKE \'%test%\'');
 SELECT formatQuery('TRUNCATE TABLES FROM default NOT ILIKE \'%test%\'');
+SELECT formatQuery('SHOW COLUMNS FROM tbl LIKE \'%a%\'');
+SELECT formatQuery('SHOW COLUMNS FROM tbl ILIKE \'%a%\'');
+SELECT formatQuery('SHOW COLUMNS FROM tbl NOT LIKE \'%a%\'');
+SELECT formatQuery('SHOW COLUMNS FROM tbl NOT ILIKE \'%a%\'');
