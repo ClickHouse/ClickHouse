08c00b8f3556df8b22a591666fc28ebeaa0b3e71
Raúl Marín <raul.marin@clickhouse.com>
2026-05-18 09:56:23 +0200

    Restore clickbench
    Removed by mistake

--- ci/workflows/master.py
+++ ci/workflows/master.py
@@ -56,6 +56,7 @@ workflow = Workflow.Config(
         *JobConfigs.buzz_fuzzer_serverfuzz_jobs,
         *JobConfigs.performance_comparison_with_master_head_jobs,
         *JobConfigs.performance_comparison_with_release_base_jobs,
+        *JobConfigs.clickbench_master_jobs,
         JobConfigs.sqltest_master_job,
         JobConfigs.sqllogic_test_master_job,
         JobConfigs.llvm_coverage_job,
