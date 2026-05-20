b651f92a0f9cace40cd16038326eae59f6dcded2
Azat Khuzhin <a3at.mail@gmail.com>
2026-05-20 10:35:17 +0200

    Fix 04207_pr_additional_filters flakiness

--- tests/queries/0_stateless/04207_pr_additional_filters.sql
+++ tests/queries/0_stateless/04207_pr_additional_filters.sql
@@ -72,6 +72,7 @@ SELECT count() FROM atf_p SETTINGS additional_table_filters = {'atf_p': 'x <= 2'
     max_parallel_replicas = 3,
     cluster_for_parallel_replicas = 'test_cluster_one_shard_three_replicas_localhost',
     parallel_replicas_for_non_replicated_merge_tree = 1,
+    query_plan_remove_unused_columns = 1,
     parallel_replicas_local_plan = 1,
     serialize_query_plan = 1;
 
