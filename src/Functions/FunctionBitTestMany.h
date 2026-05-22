64b9c6adf729ec26d1fcb0ad23fd2b45d3061497
Hechem Selmi <hechem.selmi@clickhouse.com>
2026-05-22 07:36:26 +0000

    fix BitTestMany canThrow.

--- src/Functions/FunctionBitTestMany.h
+++ src/Functions/FunctionBitTestMany.h
@@ -31,7 +31,7 @@ public:
 
     bool isVariadic() const override { return true; }
     bool isSuitableForShortCircuitArgumentsExecution(const DataTypesWithConstInfo & /*arguments*/) const override { return false; }
-    bool canThrow(const DataTypesWithConstInfo & /*arguments*/) const override { return false; }
+    bool canThrow(const DataTypesWithConstInfo & /*arguments*/) const override { return true; }
     size_t getNumberOfArguments() const override { return 0; }
 
     DataTypePtr getReturnTypeImpl(const DataTypes & arguments) const override
