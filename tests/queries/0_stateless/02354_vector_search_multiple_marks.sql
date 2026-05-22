30655b8e058e43d56aa48b51d74b93b10c99e7c8
Shankar Iyer <shankar.iyer@clickhouse.com>
2026-05-22 16:34:38 +0000

    quick hacks

--- tests/queries/0_stateless/02354_vector_search_multiple_marks.sql
+++ tests/queries/0_stateless/02354_vector_search_multiple_marks.sql
@@ -5,6 +5,8 @@
 
 DROP TABLE IF EXISTS tab;
 
+SET use_skip_indexes = 0;
+
 CREATE TABLE tab(id Int32, vec Array(Float32), INDEX idx vec TYPE vector_similarity('hnsw', 'L2Distance', 2)) ENGINE = MergeTree ORDER BY id SETTINGS index_granularity = 8192, index_granularity_bytes = 10485760;
 INSERT INTO tab SELECT number, [toFloat32(number), 0.0] from numbers(10000);
 
