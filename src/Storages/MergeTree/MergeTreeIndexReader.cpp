4afefe7e0f90542b253c095227862f572f8f06af
zex-hyd <zli112@student.ubc.ca>
2026-05-23 16:21:35 -0400

    Use default read buffer for vector_spann posting substream

--- src/Storages/MergeTree/MergeTreeIndexReader.cpp
+++ src/Storages/MergeTree/MergeTreeIndexReader.cpp
@@ -195,7 +195,7 @@ MergeTreeReaderSettings MergeTreeIndexReader::patchSettings(MergeTreeReaderSetti
     /// Adjust read buffer sizes for text index dictionaries and postings
     /// because usually we read relatively small amounts of data from random places of
     /// these substreams. So, it doesn't make sense to read more data in the buffer.
-    if (substream == TextIndexDictionary || substream == TextIndexPostings || substream == SpannPostingLists)
+    if (substream == TextIndexDictionary || substream == TextIndexPostings)
     {
         settings.read_settings.local_fs_buffer_size = 16 * 1024;
         settings.read_settings.remote_fs_buffer_size = 16 * 1024;
