b77fdc1e495f560890ae026453af831327d85be1
Anton Popov <anton@clickhouse.com>
2026-05-23 23:44:39 +0000

    MergeTreeReaderTextIndex::setPrecomputedGranule

--- src/Storages/MergeTree/MergeTreeReaderTextIndex.cpp
+++ src/Storages/MergeTree/MergeTreeReaderTextIndex.cpp
@@ -891,13 +891,14 @@ void MergeTreeReaderTextIndex::fillColumnFallback(
 
 void MergeTreeReaderTextIndex::setPrecomputedGranule(const IndexGranulesMap & granules)
 {
-    if (is_initialized)
-        throw Exception(ErrorCodes::LOGICAL_ERROR, "Precomputed text index granule must be set before the reader is initialized");
-
     auto it = granules.find(index.index->index.name);
 
     if (it != granules.end() && it->second)
+    {
+        lazy_cursors.clear();
+        postings_blocks.clear();
         setIndexGranule(it->second);
+    }
 }
 
 MergeTreeReaderPtr createMergeTreeReaderTextIndex(
