df9f30cc88816d30b7a70619c17d1626599d11cb
Pablo Marcos <pablo.marcos.oltra@clickhouse.com>
2026-05-27 16:25:05 +0000

    Improve `SYSTEM INSTRUMENT` regression tests
    Use `formatQuerySingleLine` instead of `EXPLAIN SYNTAX` to test formatter output for `SYSTEM INSTRUMENT ADD` arguments.
    
    Make the reversed `SLEEP` range case explicit and verify failed validation leaves no matching instrumentation point behind.
    
    https://github.com/ClickHouse/ClickHouse/issues/105939
    
    https://github.com/ClickHouse/ClickHouse/pull/103854#discussion_r3235932168

--- tests/queries/0_stateless/04296_system_instrument_sleep_invalid_arguments.sql
+++ tests/queries/0_stateless/04296_system_instrument_sleep_invalid_arguments.sql
@@ -3,4 +3,8 @@
 -- https://github.com/ClickHouse/ClickHouse/pull/103854#discussion_r3235932168
 
 SYSTEM INSTRUMENT ADD 'QueryMetricLog::startQuery' SLEEP ENTRY 0 1 2; -- { serverError BAD_ARGUMENTS }
+SELECT count() FROM system.instrumentation WHERE handler = 'sleep' AND entry_type = 'Entry' AND toString(arguments) = '[0, 1, 2]';
+
+SELECT 'min_greater_than_max';
 SYSTEM INSTRUMENT ADD 'QueryMetricLog::startQuery' SLEEP ENTRY 2 1; -- { serverError BAD_ARGUMENTS }
+SELECT count() FROM system.instrumentation WHERE handler = 'sleep' AND entry_type = 'Entry' AND toString(arguments) = '[2, 1]';
