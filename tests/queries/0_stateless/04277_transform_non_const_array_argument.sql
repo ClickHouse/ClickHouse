3f32dd02a8941534165af728d930328bec47fb30
zhanglistar <zhanglinuxstar@gmail.com>
2026-05-27 12:13:31 +0800

    fix

--- tests/queries/0_stateless/04277_transform_non_const_array_argument.sql
+++ tests/queries/0_stateless/04277_transform_non_const_array_argument.sql
@@ -1,8 +1,8 @@
 -- Regression: transform's from/to arrays must be constant; non-constant arrays must not be cached.
 
-SELECT transform(number % 2, [0, 1], [number, number + 10]) FROM numbers(3); -- { clientError ILLEGAL_COLUMN }
+SELECT transform(number % 2, [0, 1], [number, number + 10]) FROM numbers(3); -- { serverError ILLEGAL_COLUMN }
 
-SELECT transform(number % 2, [0, 1], materialize([0, 10])) FROM numbers(3); -- { clientError ILLEGAL_COLUMN }
+SELECT transform(number % 2, [0, 1], materialize([0, 10])) FROM numbers(3); -- { serverError ILLEGAL_COLUMN }
 
 SET allow_experimental_nullable_array_type = 1;
 
