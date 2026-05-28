bb0b177b3876a4e94f889a7a6185e4420a3e3f91
Pablo Marcos <pablo.marcos.oltra@clickhouse.com>
2026-05-28 15:55:40 +0000

    Fix `SYSTEM INSTRUMENT` invalid argument checks
    Compare `toString(arguments)` with the actual compact array serialization so the test verifies that invalid `SLEEP` instrumentations were not registered.
    
    Addresses feedback in https://github.com/ClickHouse/ClickHouse/pull/105984.

--- tests/queries/0_stateless/04296_system_instrument_sleep_invalid_arguments.sql
+++ tests/queries/0_stateless/04296_system_instrument_sleep_invalid_arguments.sql
@@ -3,11 +3,11 @@
 -- https://github.com/ClickHouse/ClickHouse/pull/103854#discussion_r3235932168
 
 SYSTEM INSTRUMENT ADD 'QueryMetricLog::startQuery' SLEEP ENTRY 0 1 2; -- { serverError BAD_ARGUMENTS }
-SELECT count() FROM system.instrumentation WHERE handler = 'sleep' AND entry_type = 'Entry' AND toString(arguments) = '[0, 1, 2]';
+SELECT count() FROM system.instrumentation WHERE handler = 'sleep' AND entry_type = 'Entry' AND toString(arguments) = '[0,1,2]';
 
 SELECT 'min_greater_than_max';
 SYSTEM INSTRUMENT ADD 'QueryMetricLog::startQuery' SLEEP ENTRY 2 1; -- { serverError BAD_ARGUMENTS }
-SELECT count() FROM system.instrumentation WHERE handler = 'sleep' AND entry_type = 'Entry' AND toString(arguments) = '[2, 1]';
+SELECT count() FROM system.instrumentation WHERE handler = 'sleep' AND entry_type = 'Entry' AND toString(arguments) = '[2,1]';
 
 SELECT 'negative_duration';
 SYSTEM INSTRUMENT ADD 'QueryMetricLog::startQuery' SLEEP ENTRY -1; -- { serverError BAD_ARGUMENTS }
@@ -15,8 +15,8 @@ SELECT count() FROM system.instrumentation WHERE handler = 'sleep' AND entry_typ
 
 SELECT 'negative_min';
 SYSTEM INSTRUMENT ADD 'QueryMetricLog::startQuery' SLEEP ENTRY -1 1; -- { serverError BAD_ARGUMENTS }
-SELECT count() FROM system.instrumentation WHERE handler = 'sleep' AND entry_type = 'Entry' AND toString(arguments) = '[-1, 1]';
+SELECT count() FROM system.instrumentation WHERE handler = 'sleep' AND entry_type = 'Entry' AND toString(arguments) = '[-1,1]';
 
 SELECT 'negative_max';
 SYSTEM INSTRUMENT ADD 'QueryMetricLog::startQuery' SLEEP ENTRY 0 -1; -- { serverError BAD_ARGUMENTS }
-SELECT count() FROM system.instrumentation WHERE handler = 'sleep' AND entry_type = 'Entry' AND toString(arguments) = '[0, -1]';
+SELECT count() FROM system.instrumentation WHERE handler = 'sleep' AND entry_type = 'Entry' AND toString(arguments) = '[0,-1]';
