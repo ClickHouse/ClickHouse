da125c4e42708c2f9c7fee99891fa0ec6ba0471d
yousefQadry <yousefqadryy@gmail.com>
2026-05-29 09:01:03 +0200

    Update test cases and documentation for h3PolygonToCellsWithContainment

--- tests/queries/0_stateless/04141_h3_polygon_to_cells_with_containment.sql
+++ tests/queries/0_stateless/04141_h3_polygon_to_cells_with_containment.sql
@@ -15,17 +15,31 @@ FROM
 WITH
     [(-122.4089866999972145, 37.813318999983238), (-122.3544736999993603, 37.7198061999978478), (-122.4798767000009008, 37.8151571999998453)] AS ring,
     7 AS resolution
-SELECT length(h3PolygonToCellsWithContainment(ring, resolution, 1)) <= length(h3PolygonToCellsWithContainment(ring, resolution, 0));
+SELECT arraySort(arrayMap(x -> h3ToString(x), h3PolygonToCellsWithContainment(ring, resolution, 1)))
+    = ['87283082bffffff'];
 
 WITH
     [(-122.4089866999972145, 37.813318999983238), (-122.3544736999993603, 37.7198061999978478), (-122.4798767000009008, 37.8151571999998453)] AS ring,
     7 AS resolution
-SELECT length(h3PolygonToCellsWithContainment(ring, resolution, 2)) >= length(h3PolygonToCellsWithContainment(ring, resolution, 0));
+SELECT arraySort(arrayMap(x -> h3ToString(x), h3PolygonToCellsWithContainment(ring, resolution, 2)))
+    = ['87283080cffffff','87283080dffffff','872830820ffffff','872830821ffffff','872830823ffffff','872830826ffffff','872830828ffffff','872830829ffffff','87283082affffff','87283082bffffff','87283082cffffff','87283082effffff','872830870ffffff','872830874ffffff','872830876ffffff'];
 
 WITH
     [(-122.4089866999972145, 37.813318999983238), (-122.3544736999993603, 37.7198061999978478), (-122.4798767000009008, 37.8151571999998453)] AS ring,
     7 AS resolution
-SELECT length(h3PolygonToCellsWithContainment(ring, resolution, 3)) >= length(h3PolygonToCellsWithContainment(ring, resolution, 2));
+SELECT arraySort(arrayMap(x -> h3ToString(x), h3PolygonToCellsWithContainment(ring, resolution, 3)))
+    = ['872830801ffffff','872830805ffffff','872830808ffffff','87283080cffffff','87283080dffffff','872830820ffffff','872830821ffffff','872830822ffffff','872830823ffffff','872830824ffffff','872830825ffffff','872830826ffffff','872830828ffffff','872830829ffffff','87283082affffff','87283082bffffff','87283082cffffff','87283082dffffff','87283082effffff','872830870ffffff','872830871ffffff','872830872ffffff','872830874ffffff','872830875ffffff','872830876ffffff','872830919ffffff','872830952ffffff','87283095affffff'];
+
+WITH
+    [(-122.4089866999972145, 37.813318999983238), (-122.3544736999993603, 37.7198061999978478), (-122.4798767000009008, 37.8151571999998453)] AS ring,
+    7 AS resolution
+SELECT
+    arraySort(arrayMap(x -> h3ToString(x), h3PolygonToCellsWithContainment(ring, resolution, 0)))
+        != arraySort(arrayMap(x -> h3ToString(x), h3PolygonToCellsWithContainment(ring, resolution, 1)))
+    AND arraySort(arrayMap(x -> h3ToString(x), h3PolygonToCellsWithContainment(ring, resolution, 1)))
+        != arraySort(arrayMap(x -> h3ToString(x), h3PolygonToCellsWithContainment(ring, resolution, 2)))
+    AND arraySort(arrayMap(x -> h3ToString(x), h3PolygonToCellsWithContainment(ring, resolution, 2)))
+        != arraySort(arrayMap(x -> h3ToString(x), h3PolygonToCellsWithContainment(ring, resolution, 3));
 
 WITH
     [(-122.4089866999972145, 37.813318999983238), (-122.3544736999993603, 37.7198061999978478), (-122.4798767000009008, 37.8151571999998453)] AS ring,
