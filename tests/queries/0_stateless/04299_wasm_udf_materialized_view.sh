98e516c063373b773f397564064910bcf5781014
Yue Ni <niyue.com@gmail.com>
2026-05-31 18:04:23 +0800

    Use unique `WASM` names in materialized view test
    Derive the `WASM` module and `CREATE FUNCTION` names from `CLICKHOUSE_TEST_UNIQUE_NAME` so parallel `clickhouse-test` runs do not share global `system.webassembly_modules` state.

--- tests/queries/0_stateless/04299_wasm_udf_materialized_view.sh
+++ tests/queries/0_stateless/04299_wasm_udf_materialized_view.sh
@@ -5,20 +5,23 @@ CUR_DIR=$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)
 # shellcheck source=../shell_config.sh
 . "$CUR_DIR"/../shell_config.sh
 
+module_name="test_04299_module_${CLICKHOUSE_TEST_UNIQUE_NAME}"
+function_name="test_04299_wasm_identity_${CLICKHOUSE_TEST_UNIQUE_NAME}"
+
 ${CLICKHOUSE_CLIENT} --enable_analyzer=1 << EOF
 DROP VIEW IF EXISTS test_04299_wasm_mv;
 DROP TABLE IF EXISTS test_04299_wasm_mv_dst;
 DROP TABLE IF EXISTS test_04299_wasm_mv_src;
-DROP FUNCTION IF EXISTS test_04299_wasm_identity;
-DELETE FROM system.webassembly_modules WHERE name = 'test_04299_module';
+DROP FUNCTION IF EXISTS ${function_name};
+DELETE FROM system.webassembly_modules WHERE name = '${module_name}';
 EOF
 
-cat ${CUR_DIR}/wasm/identity_int.wasm | ${CLICKHOUSE_CLIENT} --query "INSERT INTO system.webassembly_modules (name, code) SELECT 'test_04299_module', code FROM input('code String') FORMAT RawBlob"
+cat ${CUR_DIR}/wasm/identity_int.wasm | ${CLICKHOUSE_CLIENT} --query "INSERT INTO system.webassembly_modules (name, code) SELECT '${module_name}', code FROM input('code String') FORMAT RawBlob"
 
 ${CLICKHOUSE_CLIENT} --enable_analyzer=1 << EOF
-CREATE FUNCTION test_04299_wasm_identity
+CREATE FUNCTION ${function_name}
     LANGUAGE WASM ABI ROW_DIRECT
-    FROM 'test_04299_module' :: 'identity_raw'
+    FROM '${module_name}' :: 'identity_raw'
     ARGUMENTS (x Int32) RETURNS Int32;
 EOF
 
@@ -27,7 +30,7 @@ CREATE TABLE test_04299_wasm_mv_src (value Int32) ENGINE = Memory;
 CREATE TABLE test_04299_wasm_mv_dst (value Int32) ENGINE = Memory;
 
 CREATE MATERIALIZED VIEW test_04299_wasm_mv TO test_04299_wasm_mv_dst
-AS SELECT test_04299_wasm_identity(value) AS value
+AS SELECT ${function_name}(value) AS value
 FROM test_04299_wasm_mv_src;
 
 INSERT INTO test_04299_wasm_mv_src VALUES (1), (2), (3);
@@ -38,6 +41,6 @@ ${CLICKHOUSE_CLIENT} --enable_analyzer=1 << EOF
 DROP VIEW test_04299_wasm_mv;
 DROP TABLE test_04299_wasm_mv_dst;
 DROP TABLE test_04299_wasm_mv_src;
-DROP FUNCTION test_04299_wasm_identity;
-DELETE FROM system.webassembly_modules WHERE name = 'test_04299_module';
+DROP FUNCTION ${function_name};
+DELETE FROM system.webassembly_modules WHERE name = '${module_name}';
 EOF
