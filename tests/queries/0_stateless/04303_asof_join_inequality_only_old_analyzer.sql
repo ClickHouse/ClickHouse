938df5275f2809040225a9182997560a98f36b26
Fgrtue <kirill.kopnev@clickhouse.com>
2026-06-02 13:42:45 +0000

    Adjusted the test to compare with new analyzer

--- tests/queries/0_stateless/04303_asof_join_inequality_only_old_analyzer.sql
+++ tests/queries/0_stateless/04303_asof_join_inequality_only_old_analyzer.sql
@@ -15,16 +15,19 @@ DROP TABLE IF EXISTS t1_04303;
 CREATE TABLE t0_04303 (x Int, y Int) ENGINE = Memory;
 CREATE TABLE t1_04303 (x Int, y Int) ENGINE = Memory;
 
+INSERT INTO t0_04303 VALUES (1, 10);
+INSERT INTO t1_04303 VALUES (1, 5);
+
 -- Pure inequality: must throw INVALID_JOIN_ON_EXPRESSION, not abort.
 SELECT * FROM t0_04303 ASOF LEFT JOIN t1_04303 ON t0_04303.y > t1_04303.y SETTINGS enable_analyzer = 0; -- { serverError INVALID_JOIN_ON_EXPRESSION }
 
 -- The shape the AST fuzzer found: single inequality wrapped in `and(...)`.
 SELECT * FROM t0_04303 ASOF LEFT JOIN t1_04303 ON and((t0_04303.y > t1_04303.y)) SETTINGS enable_analyzer = 0; -- { serverError INVALID_JOIN_ON_EXPRESSION }
 
--- Sanity: a proper `ASOF` JOIN with an equality and an inequality still works.
-INSERT INTO t0_04303 VALUES (1, 10);
-INSERT INTO t1_04303 VALUES (1, 5);
-SELECT * FROM t0_04303 ASOF LEFT JOIN t1_04303 ON t0_04303.x = t1_04303.x AND t0_04303.y > t1_04303.y SETTINGS enable_analyzer = 0;
+-- Comparison of queries with enabled analyzer.
+
+SELECT * FROM t0_04303 ASOF LEFT JOIN t1_04303 ON t0_04303.y > t1_04303.y SETTINGS enable_analyzer = 1; -- { serverError NOT_IMPLEMENTED }
+SELECT * FROM t0_04303 ASOF LEFT JOIN t1_04303 ON and((t0_04303.y > t1_04303.y)) SETTINGS enable_analyzer = 1; -- { serverError TOO_FEW_ARGUMENTS_FOR_FUNCTION }
 
 DROP TABLE t0_04303;
 DROP TABLE t1_04303;
