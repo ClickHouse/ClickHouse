f0ac9a7bc4f0d7349ad4a12c40476aca0728ada8
zouyunhe <kevinyunhe8@gmail.com>
2026-06-02 13:01:57 +0000

    fix ci

--- tests/queries/0_stateless/02668_fix_parse_datetime_in_joda_syntax.sh
+++ tests/queries/0_stateless/02668_fix_parse_datetime_in_joda_syntax.sh
@@ -8,5 +8,6 @@ CURDIR=$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)
 
 echo "Query: select parseDateTime64InJodaSyntax('2024-10-09 10:30:10.1234567', 'yyyy-MM-dd HH:mm:ss.SSSSSSS', 'UTC')"
 echo "Expected error: Precision 7 is invalid (must be \[0, 6\])"
-echo "Output:"
-${CLICKHOUSE_CLIENT} -q "select parseDateTime64InJodaSyntax('2024-10-09 10:30:10.1234567', 'yyyy-MM-dd HH:mm:ss.SSSSSSS', 'UTC')" 2>&1 | grep "Precision 7 is invalid (must be \[0, 6\])" && echo "OK" || echo "FAIL"
+echo "Result:"
+output=$(${CLICKHOUSE_CLIENT} -q "select parseDateTime64InJodaSyntax('2024-10-09 10:30:10.1234567', 'yyyy-MM-dd HH:mm:ss.SSSSSSS', 'UTC')" 2>&1)
+echo "$output" | grep -Fq 'Precision 7 is invalid (must be [0, 6])' && echo "OK" || echo "FAIL"
