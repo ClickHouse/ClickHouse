87931eeb0b256b99454475cfe5d49b1c3d3550de
Michael Stetsyuk <michael.stetsyuk@clickhouse.com>
2026-06-02 17:50:54 +0000

    better

--- src/Common/CurrentThread.cpp
+++ src/Common/CurrentThread.cpp
@@ -15,7 +15,7 @@ namespace DB
 #if defined(OS_LINUX)
 /// Silk::FiberLocal implements the thread-local storage abstraction
 /// for both plain threads and silk fibers.
-constinit Silk::FiberLocal<ThreadStatus *, Silk::CurrentThreadSlot> current_thread;
+Silk::FiberLocal<ThreadStatus *, Silk::CurrentThreadSlot> current_thread;
 #else
 thread_local ThreadStatus constinit * current_thread = nullptr;
 #endif
