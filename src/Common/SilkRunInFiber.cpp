075c7b912dbdc7efd1e944612a75d8960c1f4444
Michael Stetsyuk <michael.stetsyuk@clickhouse.com>
2026-06-03 17:55:17 +0000

    better

--- src/Common/SilkRunInFiber.cpp
+++ src/Common/SilkRunInFiber.cpp
@@ -5,7 +5,9 @@
 #include <Common/CurrentThread.h>
 
 #include <silk/fibers/fiber.h>
+#include <silk/fibers/future.h>
 
+#include <functional>
 #include <utility>
 
 namespace Silk
@@ -14,25 +16,53 @@ namespace Silk
 namespace
 {
 
+struct FiberContext
+{
+    DB::ThreadStatus * saved_current_thread;
+    std::function<int()> func;
+
+    static int main(FiberContext * self) noexcept
+    {
+        return self->func();
+    }
+};
+
 void onFiberResumeSuspend(silk::Fiber * fiber) noexcept
 {
-    auto * storage = static_cast<detail::FiberLocalStorage *>(silk::FiberScheduler::getFiberParameters(fiber));
-    std::swap(storage->saved_current_thread, DB::current_thread);
+    auto * context = static_cast<FiberContext *>(silk::FiberScheduler::getFiberParameters(fiber));
+    std::swap(context->saved_current_thread, DB::current_thread);
 }
 
 }
 
 void initializeFiberScheduler()
 {
-    silk::FiberScheduler::Options options;
-    options.fiberResume = &onFiberResumeSuspend;
-    options.fiberSuspend = &onFiberResumeSuspend;
+    const silk::FiberScheduler::Options options =
+    {
+        .fiberSuspend = &onFiberResumeSuspend,
+        .fiberResume = &onFiberResumeSuspend,
+    };
     silk::FiberScheduler::initialize(&options);
 }
 
 void destroyFiberScheduler()
 {
     silk::FiberScheduler::destroy();
+}
+
+int RunInFiber(std::function<int()> func, silk::FiberFuture & future)
+{
+    return silk::FiberScheduler::run(
+        &FiberContext::main,
+        FiberContext{ .saved_current_thread = nullptr, .func = std::move(func) },
+        &future);
+}
+
+int RunInFiber(std::function<int()> func)
+{
+    silk::FiberFuture future;
+    int r = RunInFiber(std::move(func), future);
+    return r ? r : future.wait();
 }
 
 }
--- src/Common/SilkRunInFiber.h
+++ src/Common/SilkRunInFiber.h
@@ -2,64 +2,21 @@
 
 #if defined(OS_LINUX)
 
-#include <Common/ThreadGroupSwitcher.h>
-#include <Common/ThreadStatus.h>
+#include <functional>
 
-#include <silk/fibers/fiber.h>
-#include <silk/fibers/future.h>
-
-#include <type_traits>
-#include <utility>
+namespace silk
+{
+class FiberFuture;
+}
 
 namespace Silk
 {
 
-namespace detail
-{
-
-struct FiberLocalStorage
-{
-    DB::ThreadStatus * saved_current_thread = nullptr;
-};
-
-/// Inheritance is required,
-/// so we can cast void * received from silk::FiberScheduler::getFiberParameters to FiberLocalStorage *.
-template <typename F>
-struct FiberContext : FiberLocalStorage
-{
-    F func;
-
-    static int main(FiberContext * self) noexcept
-    {
-        return self->func();
-    }
-};
-
-}
-
 void initializeFiberScheduler();
 void destroyFiberScheduler();
 
-template <typename F>
-[[nodiscard]] int RunInFiber(F && func, silk::FiberFuture & future)
-{
-    using Func = std::decay_t<F>;
-    return silk::FiberScheduler::run(
-        &detail::FiberContext<Func>::main,
-        detail::FiberContext<Func>
-        {
-            .func = std::forward<F>(func),
-        },
-        &future);
-}
-
-template <typename F>
-[[nodiscard]] int RunInFiber(F && func)
-{
-    silk::FiberFuture future;
-    int r = RunInFiber(std::forward<F>(func), future);
-    return r ? r : future.wait();
-}
+[[nodiscard]] int RunInFiber(std::function<int()> func, silk::FiberFuture & future);
+[[nodiscard]] int RunInFiber(std::function<int()> func);
 
 }
 
