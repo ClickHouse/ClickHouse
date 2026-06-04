d017cdc63d22bd8ed9dd0b21f70ed88fbf8a175b
Julia Kartseva <yulia.kartseva@gmail.com>
2026-06-04 00:05:44 +0000

    apply black to tests/integration/test_refreshable_mv/test.py

--- tests/integration/test_refreshable_mv/test.py
+++ tests/integration/test_refreshable_mv/test.py
@@ -108,11 +108,9 @@ def test_refreshable_mv_in_replicated_db(started_cluster, cleanup):
         for node in nodes:
             # Wait twice to make sure we wait for a refresh that started after we adjusted the clock.
             # Otherwise another refresh may start right after (because clock moved far forward).
-            node.query(
-                f"system wait view re.{name};\
+            node.query(f"system wait view re.{name};\
                 system refresh view re.{name};\
-                system wait view re.{name};"
-            )
+                system wait view re.{name};")
         for node in nodes:
             node.query(f"system sync replica re.{name};")
         rows_before = int(nodes[randint(0, 1)].query(f"select count() from re.{name}"))
