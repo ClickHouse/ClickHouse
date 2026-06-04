5a35c56ad005f500f8944abbc27b7f703e53816f
Nihal Z. Miaji <81457724+nihalzp@users.noreply.github.com>
2026-06-04 19:05:59 +0000

    Skip fast test for collation test

--- tests/queries/0_stateless/04299_limit_by_collation.sql
+++ tests/queries/0_stateless/04299_limit_by_collation.sql
@@ -1,3 +1,6 @@
+-- Tags: no-fasttest
+-- no-fasttest: `COLLATE` needs ICU, which is not available in fast test
+
 -- { echo }
 SELECT count() FROM (SELECT s FROM (SELECT arrayJoin(['a', 'A', 'b']) AS s) ORDER BY s COLLATE 'en-u-ks-level2' LIMIT 1 BY s);
 SELECT count() FROM (SELECT s FROM (SELECT arrayJoin(['a', 'a', 'A', 'A', 'b', 'b']) AS s) ORDER BY s COLLATE 'en-u-ks-level2' LIMIT -1 BY s);
