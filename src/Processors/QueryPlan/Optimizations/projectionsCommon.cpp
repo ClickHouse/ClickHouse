bd967e8bd8e09d9d7cb0091bfcc456d048790ed4
János Benjamin Antal <benjamin.antal@clickhouse.com>
2026-06-04 10:10:45 +0000

    Use boolean-context type check for projection filters
    The same as we use for filters

--- src/Processors/QueryPlan/Optimizations/projectionsCommon.cpp
+++ src/Processors/QueryPlan/Optimizations/projectionsCommon.cpp
@@ -126,9 +126,9 @@ static const ActionsDAG::Node * findInOutputs(ActionsDAG & dag, const std::strin
             if (node->result_type->onlyNull())
                 return nullptr;
 
-            if (!isUInt8(removeNullable(removeLowCardinality(node->result_type))))
+            if (!node->result_type->canBeUsedInBooleanContext())
                 throw Exception(ErrorCodes::ILLEGAL_TYPE_OF_COLUMN_FOR_FILTER,
-                    "Illegal type {} of column {} for filter. Must be UInt8 or Nullable(UInt8).",
+                    "Illegal type {} of column {} for filter. Must be native integer or float type",
                     node->result_type->getName(), name);
 
             if (remove)
