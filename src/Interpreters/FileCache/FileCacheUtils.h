c9b42278b0dcd09128c23418f492555c3059e6a5
Alexey Milovidov <milovidov@clickhouse.com>
2026-06-03 20:53:42 -0700

    Update FileCacheUtils.h

--- src/Interpreters/FileCache/FileCacheUtils.h
+++ src/Interpreters/FileCache/FileCacheUtils.h
@@ -21,11 +21,10 @@ inline size_t roundUpToMultiple(size_t num, size_t multiple)
     if (!multiple)
         return num;
 
-    size_t upper_bound = num;
-    if (common::addOverflow(upper_bound, multiple - 1))
+    if (common::addOverflow(num, multiple - 1))
         throw std::overflow_error("FileCacheUtils::roundUpToMultiple: rounded-up value does not fit in size_t");
 
-    return roundDownToMultiple(upper_bound, multiple);
+    return roundDownToMultiple(num, multiple);
 }
 
 }
