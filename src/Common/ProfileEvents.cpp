5223a2f36b21a04af6835647173cc8d9b6b12989
Dmitry Novik <n0vik@clickhouse.com>
2026-06-05 19:16:25 +0200

    Add QueryAnalysis/QueryTreeOptimize/QueryPlanning profile events
    Define three new microsecond ProfileEvents for query processing phases.
    QueryPlanOptimizeMicroseconds already exists and is reused as-is.

--- src/Common/ProfileEvents.cpp
+++ src/Common/ProfileEvents.cpp
@@ -403,6 +403,9 @@
     M(JoinReorderMicroseconds, "Total time spent executing JOIN reordering algorithm.", ValueType::Microseconds) \
     M(JoinOptimizeMicroseconds, "Total time spent executing JOIN plan optimizations.", ValueType::Microseconds) \
     M(QueryPlanOptimizeMicroseconds, "Total time spent executing query plan optimizations.", ValueType::Microseconds) \
+    M(QueryAnalysisMicroseconds, "Total time spent in query analysis: name and type resolution of the query tree (the QueryAnalysisPass). This is a part of QueryTreeOptimizeMicroseconds.", ValueType::Microseconds) \
+    M(QueryTreeOptimizeMicroseconds, "Total time spent running query tree passes: query analysis and all query-tree-level optimizations. Includes QueryAnalysisMicroseconds.", ValueType::Microseconds) \
+    M(QueryPlanningMicroseconds, "Total time spent building the query plan from the query tree.", ValueType::Microseconds) \
     \
     M(DeltaLakePartitionPrunedFiles, "Number of skipped files during DeltaLake partition pruning", ValueType::Number) \
     M(DeltaLakeSnapshotInitializations, "Number of times a DeltaLake table snapshot was initialized (loaded from object storage)", ValueType::Number) \
