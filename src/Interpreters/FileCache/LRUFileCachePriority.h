085dc16c14125703d3cfc581b1276e978e774725
robot-clickhouse <robot-clickhouse@users.noreply.github.com>
2026-06-08 11:53:22 +0000

    Backport #106286 to 26.5: fs cache fix for 26.1+ slru downgrade failure rollback; fix for system/data split priority total space resize

--- src/Interpreters/FileCache/LRUFileCachePriority.h
+++ src/Interpreters/FileCache/LRUFileCachePriority.h
@@ -6,6 +6,7 @@
 #include <Common/logger_useful.h>
 #include <Interpreters/FileCache/Guards.h>
 
+class FileCacheTest_MoveEvictionPos_Test;
 
 namespace DB
 {
@@ -162,6 +163,7 @@ private:
     class LRUIterator;
     using LRUQueue = std::list<EntryPtr>;
     friend class SLRUFileCachePriority;
+    friend class ::FileCacheTest_MoveEvictionPos_Test;
 
     LRUQueue queue;
     const std::string description;
@@ -246,7 +248,7 @@ public:
 
     void remove(const CachePriorityGuard::WriteLock &) override;
 
-    void invalidate() override;
+    void invalidate() noexcept override;
 
     void incrementSize(size_t size, const CacheStateGuard::Lock &) override;
 
