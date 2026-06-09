b8521fb74e0f67fdf57d8384c8b8b7fe6ce129e3
Yue Ni <niyue.com@gmail.com>
2026-06-09 18:05:31 +0800

    Reuse `DEFAULT` cycle detection for late matcher validation
    Run the expanded default expression through `detectRecursiveDefaultCycles` so late matcher validation covers `MATERIALIZED` and other default-bearing columns, not only `ALIAS` columns.
    
    Related: https://github.com/ClickHouse/ClickHouse/pull/105045#discussion_r3379617063

--- tests/queries/0_stateless/04329_default_expression_matcher_alias_late_cycle_missing_defaults.sql
+++ tests/queries/0_stateless/04329_default_expression_matcher_alias_late_cycle_missing_defaults.sql
@@ -1,7 +1,25 @@
 DROP TABLE IF EXISTS default_expression_matcher_alias_late_cycle_missing_direct;
 DROP TABLE IF EXISTS default_expression_matcher_alias_late_cycle_missing_indirect;
+DROP TABLE IF EXISTS default_expression_matcher_materialized_late_cycle_missing;
 
 SET asterisk_include_alias_columns = 0;
+
+SET asterisk_include_materialized_columns = 0;
+
+CREATE TABLE default_expression_matcher_materialized_late_cycle_missing
+(
+    a UInt8,
+    b String DEFAULT toJSONString(tuple(* EXCEPT b))
+)
+ENGINE = Memory;
+
+ALTER TABLE default_expression_matcher_materialized_late_cycle_missing ADD COLUMN m String MATERIALIZED b;
+
+SET asterisk_include_materialized_columns = 1;
+
+INSERT INTO default_expression_matcher_materialized_late_cycle_missing (a) VALUES (1); -- { serverError CYCLIC_ALIASES }
+
+SET asterisk_include_materialized_columns = 0;
 
 CREATE TABLE default_expression_matcher_alias_late_cycle_missing_direct
 (
@@ -34,3 +52,4 @@ INSERT INTO default_expression_matcher_alias_late_cycle_missing_indirect (a) VAL
 
 DROP TABLE default_expression_matcher_alias_late_cycle_missing_indirect;
 DROP TABLE default_expression_matcher_alias_late_cycle_missing_direct;
+DROP TABLE default_expression_matcher_materialized_late_cycle_missing;
