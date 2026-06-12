311d7d49b9ea761f4e3adce911a6a342984e0638
kssenii <kssenii@clickhouse.com>
2026-06-12 19:44:56 +0200

    Sync dist cache metrics

--- src/Common/ErrorCodes.cpp
+++ src/Common/ErrorCodes.cpp
@@ -669,6 +669,7 @@
     M(1005, STALE_VERSION) \
     M(1006, INVALID_CURSOR_LOOKUP) \
     M(1007, ILLEGAL_STREAM) \
+    M(1008, TEMPORARY_DATA_NOT_IN_CACHE) \
     /* See END */
 
 #ifdef APPLY_FOR_EXTERNAL_ERROR_CODES
@@ -685,7 +686,7 @@ namespace ErrorCodes
     APPLY_FOR_ERROR_CODES(M)
 #undef M
 
-    constexpr ErrorCode END = 1007;
+    constexpr ErrorCode END = 1008;
     ErrorPairHolder values[END + 1]{};
 
     struct ErrorCodesNames
