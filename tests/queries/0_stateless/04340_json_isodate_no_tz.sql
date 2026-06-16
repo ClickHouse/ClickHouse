68bf9aeb574e294b50dcb036b1d5bbe80effbbc6
Eduardo Gomez Saldias <50159560+edugomez102@users.noreply.github.com>
2026-06-16 19:23:42 +0200

    update tests

--- tests/queries/0_stateless/04340_json_isodate_no_tz.sql
+++ tests/queries/0_stateless/04340_json_isodate_no_tz.sql
@@ -2,6 +2,6 @@ DROP TABLE IF EXISTS test_iso;
 
 CREATE TABLE test_iso ( ts DateTime64(3)) ENGINE = Memory;
 
-INSERT INTO test_iso FORMAT JSONEachRow {"ts": new ISODate("2024-05-29T23:16:12.256")};
+INSERT INTO test_iso FORMAT JSONEachRow {"ts": ISODate("2024-05-29T23:16:12.256")};
 
 SELECT toString(ts) FROM test_iso;
