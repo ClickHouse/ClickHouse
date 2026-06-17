81a945aa9864ed2f9acb80d6714b05ee458353eb
Azat Khuzhin <a3at.mail@gmail.com>
2026-06-17 13:10:14 +0200

    tests: fix flakiness of 03602_alter_update_nullable_json
    CI: https://s3.amazonaws.com/clickhouse-test-reports/json.html?PR=107678&sha=554f564face5193e65890ffc3fc9bb496a7e0370&name_0=PR&name_1=Stateless%20tests%20%28amd_msan%2C%20WasmEdge%2C%20parallel%2C%202%2F2%29

--- tests/queries/0_stateless/03602_alter_update_nullable_json.sql
+++ tests/queries/0_stateless/03602_alter_update_nullable_json.sql
@@ -2,7 +2,7 @@ set mutations_sync=1;
 set max_block_size=1000;
 
 drop table if exists test;
-create table test (id UInt32, json Nullable(JSON(max_dynamic_paths=1))) engine=MergeTree order by tuple() settings min_bytes_for_wide_part='200G';
+create table test (id UInt32, json Nullable(JSON(max_dynamic_paths=1))) engine=MergeTree order by tuple() settings min_bytes_for_wide_part='200G', index_granularity=8192, index_granularity_bytes='10M';
 insert into test select number, '{"a" : 1}' from numbers(100000);
 alter table test update json='{"b" : 1}' where id > 90000;
 select 'Dynamic paths';
@@ -12,7 +12,7 @@ select distinct arrayJoin(JSONSharedDataPaths(assumeNotNull(json))) from test;
 
 drop table test;
 
-create table test (id UInt32, json Nullable(JSON(max_dynamic_paths=1))) engine=MergeTree order by tuple() settings min_bytes_for_wide_part=1, min_rows_for_wide_part=1;
+create table test (id UInt32, json Nullable(JSON(max_dynamic_paths=1))) engine=MergeTree order by tuple() settings min_bytes_for_wide_part=1, min_rows_for_wide_part=1, index_granularity=8192, index_granularity_bytes='10M';
 insert into test select number, '{"a" : 1}' from numbers(100000);
 alter table test update json='{"b" : 1}' where id > 90000;
 select 'Dynamic paths';
