79132f9f3933307c5ca5b7ac0cd8c75c70210026
Nikita Taranov <nikita.taranov@clickhouse.com>
2026-06-17 13:01:06 +0000

    fix test

--- tests/queries/0_stateless/04305_autopr_output_bytes_estimation_join.sql
+++ tests/queries/0_stateless/04305_autopr_output_bytes_estimation_join.sql
@@ -1,3 +1,6 @@
+-- For runs with the old analyzer
+SET enable_analyzer=1;
+
 SET enable_parallel_replicas=1, automatic_parallel_replicas_mode=2, parallel_replicas_local_plan=1, parallel_replicas_index_analysis_only_on_coordinator=1,
     parallel_replicas_for_non_replicated_merge_tree=1, max_parallel_replicas=3, cluster_for_parallel_replicas='parallel_replicas';
 SET parallel_replicas_prefer_local_join=1;
