9f1f8af9e017157a36418441869a58c744719e1c
Pablo Marcos <pablo.marcos.oltra@clickhouse.com>
2026-06-17 16:39:51 +0000

    Avoid part pointer copies in restored attach loop

--- src/Storages/StorageReplicatedMergeTree.cpp
+++ src/Storages/StorageReplicatedMergeTree.cpp
@@ -11782,7 +11782,7 @@ void StorageReplicatedMergeTree::attachRestoredParts(
         /* async_insert */ false, *this, metadata_snapshot, /* quorum */ 0, /* quorum_timeout_ms */ 0, /* max_parts_per_block */ 0, /* quorum_parallel */ false,
         /* majority_quorum */ false, getContext(), /* is_attach */ true, /* allow_attach_while_readonly */ false, zookeeper_retries_info);
 
-    for (auto part : parts)
+    for (auto & part : parts)
         sink->writeExistingPart(part, /* deduplicate_part */ false);
 }
 
