be930770b0c328c94deebe354041d110264d8838
Roman Vasin <r.vasin@arenadata.io>
2026-06-19 13:45:01 +0000

    Refactor test_sorted_kill_query test

--- tests/integration/test_limit_by_transform_kill_query/test.py
+++ tests/integration/test_limit_by_transform_kill_query/test.py
@@ -1,8 +1,6 @@
 import pytest
 import uuid
 import threading
-import time
-
 from helpers.cluster import ClickHouseCluster
 
 cluster = ClickHouseCluster(__file__)
@@ -22,11 +20,11 @@ SORTED_QUERY = """SELECT
     number AS key1,
     number + 1 AS key2,
     number AS value
-FROM numbers(100000000)
+FROM numbers(30000000)
 ORDER BY key1 ASC, key2 ASC
 LIMIT 1 BY key1, key2
 FORMAT Null
-SETTINGS max_block_size=100000000, max_threads=1, max_rows_to_read=0"""
+SETTINGS max_block_size=30000000, max_threads=1, max_rows_to_read=0"""
 
 
 @pytest.fixture(scope="module")
@@ -38,7 +36,7 @@ def started_cluster():
         cluster.shutdown()
 
 
-def run_kill_query_test(query, log_line_pattern):
+def run_kill_query_test(query, log_line_pattern, log_timeout=30):
     query_id = str(uuid.uuid4())
     thread_error = [None]
 
@@ -55,8 +53,7 @@ def run_kill_query_test(query, log_line_pattern):
     query_thread = threading.Thread(target=execute_query)
     query_thread.start()
 
-    node1.wait_for_log_line(log_line_pattern)
-    time.sleep(1)
+    node1.wait_for_log_line(log_line_pattern, timeout=log_timeout)
 
     node1.query(f"KILL QUERY WHERE query_id='{query_id}' SYNC")
 
@@ -85,4 +82,5 @@ def test_sorted_kill_query(started_cluster):
     run_kill_query_test(
         SORTED_QUERY,
         "Transform a chunk in LimitBySortedStreamTransform",
+        log_timeout=60,
     )
