26edfaf7d1a6d4f1b3f2924b7e248753244ff006
Sreyas <srajasekharu@umass.edu>
2026-06-20 14:07:04 -0400

    fix settings and add submodule

--- src/Core/Settings.cpp
+++ src/Core/Settings.cpp
@@ -7461,6 +7461,9 @@ Enable Kusto Query Language (KQL) - an alternative to SQL.
     DECLARE(Bool, allow_experimental_prql_dialect, false, R"(
 Enable PRQL - an alternative to SQL.
 )", EXPERIMENTAL) \
+    DECLARE(Bool, allow_experimental_substrait, false, R"(
+Allow EXPLAIN SUBSTRAIT queries which serialize query plans to the Substrait format.
+)", EXPERIMENTAL) \
     DECLARE(Bool, enable_adaptive_memory_spill_scheduler, false, R"(
 Trigger processor to spill data into external storage adpatively. grace join is supported at present.
 )", EXPERIMENTAL) \
