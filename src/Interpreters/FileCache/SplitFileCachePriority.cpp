8cd464e7ab8e059edda705a3872e4a756d36b366
robot-clickhouse <robot-clickhouse@users.noreply.github.com>
2026-06-22 15:59:26 +0000

    Backport #107903 to 26.5: FileCache: stop invalidated priority entries from pinning KeyMetadata

--- src/Interpreters/FileCache/SplitFileCachePriority.cpp
+++ src/Interpreters/FileCache/SplitFileCachePriority.cpp
@@ -312,7 +312,7 @@ bool SplitFileCachePriority::tryIncreasePriority(
     CachePriorityGuard & queue_guard,
     CacheStateGuard & state_guard)
 {
-    const auto type = getPriorityType(iterator.getEntry()->key_metadata->origin.segment_type);
+    const auto type = getPriorityType(iterator.getEntry()->getKeyMetadata()->origin.segment_type);
     return getPriority(type).tryIncreasePriority(iterator, is_space_reservation_complete, queue_guard, state_guard);
 }
 
