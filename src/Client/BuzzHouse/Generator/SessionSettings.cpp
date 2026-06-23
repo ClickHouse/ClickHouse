9afc6efcd64a7ef3b40804284e7945edfe5f0b00
Fisnik Kastrati <fisnik.kastrati@clickhouse.com>
2026-06-23 23:06:32 +0200

    changes few more references and SessionSettings

--- src/Client/BuzzHouse/Generator/SessionSettings.cpp
+++ src/Client/BuzzHouse/Generator/SessionSettings.cpp
@@ -435,8 +435,8 @@ std::unordered_map<String, CHSetting> performanceSettings
             false)},
        {"query_plan_optimize_join_order_algorithm",
         CHSetting(
-            [](RandomGenerator & rg, FuzzConfig &) { return settingCombinations(rg, {"greedy", "dpsize", "dpsub"}); },
-            {"'greedy'", "'dpsize'", "'dpsub'"},
+            [](RandomGenerator & rg, FuzzConfig &) { return settingCombinations(rg, {"greedy", "dpsize"}); },
+            {"'greedy'", "'dpsize'"},
             false)},
        {"query_plan_optimize_join_order_randomize",
         CHSetting(
