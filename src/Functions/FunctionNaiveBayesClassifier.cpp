e1e4b216cec43626867a0e21846207d1adfae637
Nihal Z. Miaji <81457724+nihalzp@users.noreply.github.com>
2026-06-24 23:06:20 +0000

    Enable short-circuit lazy execution for the NaiveBayes classifier functions

--- src/Functions/FunctionNaiveBayesClassifier.cpp
+++ src/Functions/FunctionNaiveBayesClassifier.cpp
@@ -115,7 +115,7 @@ public:
     bool isVariadic() const override { return false; }
     bool useDefaultImplementationForConstants() const override { return true; }
     ColumnNumbers getArgumentsThatAreAlwaysConstant() const override { return {0}; }
-    bool isSuitableForShortCircuitArgumentsExecution(const DataTypesWithConstInfo &) const override { return false; }
+    bool isSuitableForShortCircuitArgumentsExecution(const DataTypesWithConstInfo &) const override { return true; }
     size_t getNumberOfArguments() const override { return 2; }
 };
 
