da3a0090db8933b1a2b023d03ea4df45e9459ca1
Raúl Marín <raul.marin@clickhouse.com>
2026-06-24 14:57:14 +0200

    Fix more missing std::atomic notify on wait sites (blob copier, changelog)

--- src/Coordination/Changelog.cpp
+++ src/Coordination/Changelog.cpp
@@ -2745,6 +2745,8 @@ void Changelog::backgroundChangelogOperationsThread()
             chassert(false);
         }
         changelog_operation->done = true;
+        /// Wake up `waitAllAsyncOperations`; a bare store does not wake an `std::atomic::wait`.
+        changelog_operation->done.notify_all();
     }
 }
 
