15bbe747af0b167b14d36240a1145f69e003ebfd
Dmitriy Borisenko <otselnik@yandex.ru>
2026-06-27 15:41:47 +0000

    style check fix

--- src/AggregateFunctions/AggregateFunctionGroupBloomFilter.cpp
+++ src/AggregateFunctions/AggregateFunctionGroupBloomFilter.cpp
@@ -31,7 +31,6 @@ namespace ErrorCodes
     extern const int ILLEGAL_TYPE_OF_ARGUMENT;
     extern const int NUMBER_OF_ARGUMENTS_DOESNT_MATCH;
     extern const int BAD_ARGUMENTS;
-    extern const int INCORRECT_DATA;
 }
 
 namespace
