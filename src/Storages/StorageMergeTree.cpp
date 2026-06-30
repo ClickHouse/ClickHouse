a1cd5f966644938b024c4d005a71554ae43de52b
Mikhail Artemenko <mikhail.artemenko@clickhouse.com>
2026-06-30 11:30:05 +0000

    make it exception safe

--- src/Storages/StorageMergeTree.cpp
+++ src/Storages/StorageMergeTree.cpp
@@ -2991,37 +2991,39 @@ void StorageMergeTree::movePartitionToTable(const StoragePtr & dest_table, const
         auto future_parts = initCoverageWithNewEmptyParts(src_parts);
         auto [new_empty_covering_src_parts, _] = createEmptyDataParts(*this, future_parts, txn);
 
-        std::optional<DataPartsLock> dest_data_parts_lock = dest_table_storage->lockParts();
-        std::optional<DataPartsLock> src_data_parts_lock = lockParts();
+        auto dest_data_parts_lock = dest_table_storage->lockParts();
+        auto src_data_parts_lock = lockParts();
 
         Transaction dest_transaction(*dest_table_storage, txn.get());
-        std::vector<std::unique_ptr<PlainCommittingBlockHolder>> block_holders;
+        Transaction src_transaction(*this, txn.get());
+
+        try
         {
+            std::vector<std::unique_ptr<PlainCommittingBlockHolder>> block_holders;
+
             for (auto & part : dst_parts)
             {
-                block_holders.push_back(dest_table_storage->fillNewPartName(part, dest_data_parts_lock.value()));
-                dest_table_storage->renameTempPartAndReplaceUnlocked(part, dest_transaction, dest_data_parts_lock.value(), /*rename_in_transaction=*/true);
+                block_holders.push_back(dest_table_storage->fillNewPartName(part, dest_data_parts_lock));
+                dest_table_storage->renameTempPartAndReplaceUnlocked(part, dest_transaction, dest_data_parts_lock, /*rename_in_transaction=*/true);
             }
-        }
 
-        Transaction src_transaction(*this, txn.get());
-        {
             for (auto & part : new_empty_covering_src_parts)
             {
-                renameTempPartAndReplaceUnlocked(part, src_data_parts_lock.value(), src_transaction, /*rename_in_transaction=*/true);
+                renameTempPartAndReplaceUnlocked(part, src_data_parts_lock, src_transaction, /*rename_in_transaction=*/true);
             }
+
+            dest_transaction.renameParts();
+            dest_transaction.commit(dest_data_parts_lock);
+
+            src_transaction.renameParts();
+            src_transaction.commit(src_data_parts_lock);
         }
-
-        dest_transaction.renameParts();
-        dest_transaction.commit(dest_data_parts_lock.value());
-
-        src_transaction.renameParts();
-        src_transaction.commit(src_data_parts_lock.value());
-
-        dest_data_parts_lock.reset();
-        src_data_parts_lock.reset();
-
-        clearOldPartsFromFilesystem();
+        catch (...)
+        {
+            tryLogCurrentException(log.load());
+            dest_transaction.rollback(&dest_data_parts_lock);
+            src_transaction.rollback(&src_data_parts_lock);
+        }
 
         /// Note: same elapsed time and profile events for all parts is used
         PartLog::addNewParts(getContext(), PartLog::createPartLogEntries(dst_parts, watch.elapsed(), profile_events_scope.getSnapshot()));
@@ -3031,6 +3033,8 @@ void StorageMergeTree::movePartitionToTable(const StoragePtr & dest_table, const
         PartLog::addNewParts(getContext(), PartLog::createPartLogEntries(dst_parts, watch.elapsed()), ExecutionStatus::fromCurrentException("", true));
         throw;
     }
+
+    clearOldPartsFromFilesystem();
 }
 
 ActionLock StorageMergeTree::getActionLock(StorageActionBlockType action_type)
