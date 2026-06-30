38baec8273076040ee310791260824d800cff94a
Raúl Marín <git@rmr.ninja>
2026-06-30 16:47:40 +0200

    Update 04489_uniq_exact_grouping_sets_concurrent_merge.sql

--- tests/queries/0_stateless/04489_uniq_exact_grouping_sets_concurrent_merge.sql
+++ tests/queries/0_stateless/04489_uniq_exact_grouping_sets_concurrent_merge.sql
@@ -11,9 +11,7 @@ CREATE TABLE st_04489 (a UInt32, b UInt32, c UInt32, s AggregateFunction(uniqExa
 -- Each (a, b, c) group accumulates > 100000 distinct values, so the uniqExact sets become two-level.
 INSERT INTO st_04489 SELECT number % 4, number % 3, number % 2, uniqExactState(number) FROM numbers_mt(4000000) GROUP BY 1, 2, 3;
 
--- Repeat a few times: the race is timing dependent but used to crash almost immediately.
-SELECT a, b, c, uniqExactMerge(s) FROM st_04489 GROUP BY GROUPING SETS ((a), (b), (c), (a, b), (b, c)) SETTINGS max_threads = 16, group_by_two_level_threshold = 1, group_by_use_nulls = 1 FORMAT Null;
-SELECT a, b, c, uniqExactMerge(s) FROM st_04489 GROUP BY GROUPING SETS ((a), (b), (c), (a, b), (b, c)) SETTINGS max_threads = 16, group_by_two_level_threshold = 1, group_by_use_nulls = 1 FORMAT Null;
+-- The race is timing dependent but TSAN detected the problem
 SELECT a, b, c, uniqExactMerge(s) FROM st_04489 GROUP BY GROUPING SETS ((a), (b), (c), (a, b), (b, c)) SETTINGS max_threads = 16, group_by_two_level_threshold = 1, group_by_use_nulls = 1 FORMAT Null;
 SELECT a, b, c, uniqExactMerge(s) FROM st_04489 GROUP BY CUBE (a, b, c) SETTINGS max_threads = 16, group_by_two_level_threshold = 1, group_by_use_nulls = 1 FORMAT Null;
 SELECT a, b, c, uniqExactMerge(s) FROM st_04489 GROUP BY ROLLUP (a, b, c) SETTINGS max_threads = 16, group_by_two_level_threshold = 1, group_by_use_nulls = 1 FORMAT Null;
