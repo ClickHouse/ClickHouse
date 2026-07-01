ef93d88aa0ad5aa1a9e14b906ce9689f9008f4af
Pedro Ferreira <pedro.silva.ferreira@clickhouse.com>
2026-07-01 08:07:48 +0000

    Address review comments

--- tests/queries/0_stateless/04061_text_index_json_all_values.sh
+++ tests/queries/0_stateless/04061_text_index_json_all_values.sh
@@ -7,8 +7,8 @@ CUR_DIR=$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)
 # shellcheck source=../shell_config.sh
 . "$CUR_DIR"/../shell_config.sh
 
-MY_CLICKHOUSE_CLIENT="${CLICKHOUSE_CLIENT} --enable_analyzer 1 --use_text_index_like_evaluation_by_dictionary_scan 1"
 CLICKHOUSE_CLIENT="$CLICKHOUSE_CLIENT --explain_query_plan_default=legacy"
+MY_CLICKHOUSE_CLIENT="${CLICKHOUSE_CLIENT} --enable_analyzer 1 --use_text_index_like_evaluation_by_dictionary_scan 1"
 
 function run_query()
 {
