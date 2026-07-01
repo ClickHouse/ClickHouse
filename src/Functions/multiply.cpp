be2047ecaf486c574c0eebaf841056cf8d2edbd6
Lalit Yadav <lalityadav.x17@gmail.com>
2026-07-01 08:15:06 -0500

    Add sqr arithmetic function

--- src/Functions/multiply.cpp
+++ src/Functions/multiply.cpp
@@ -53,6 +53,49 @@ struct MultiplyImpl
 struct NameMultiply { static constexpr auto name = "multiply"; };
 using FunctionMultiply = BinaryArithmeticOverloadResolver<MultiplyImpl, NameMultiply>;
 
+class FunctionSqr final : public IFunction
+{
+public:
+    static constexpr auto name = "sqr";
+
+    static FunctionPtr create(ContextPtr context) { return std::make_shared<FunctionSqr>(context); }
+
+    explicit FunctionSqr(ContextPtr context)
+        : multiply(FunctionFactory::instance().get("multiply", context))
+    {
+    }
+
+    String getName() const override { return name; }
+    size_t getNumberOfArguments() const override { return 1; }
+
+    bool useDefaultImplementationForConstants() const override { return true; }
+
+    bool isSuitableForShortCircuitArgumentsExecution(const DataTypesWithConstInfo & /*arguments*/) const override { return false; }
+
+    DataTypePtr getReturnTypeImpl(const DataTypes & arguments) const override
+    {
+        ColumnWithTypeAndName argument;
+        argument.type = arguments.front();
+        return buildMultiply(argument)->getResultType();
+    }
+
+    DataTypePtr getReturnTypeImpl(const ColumnsWithTypeAndName & arguments) const override
+    {
+        return buildMultiply(arguments.front())->getResultType();
+    }
+
+    ColumnPtr executeImpl(const ColumnsWithTypeAndName & arguments, const DataTypePtr & result_type, size_t input_rows_count) const override
+    {
+        const auto & argument = arguments.front();
+        return buildMultiply(argument)->execute({argument, argument}, result_type, input_rows_count, /* dry_run = */ false);
+    }
+
+private:
+    FunctionBasePtr buildMultiply(const ColumnWithTypeAndName & argument) const { return multiply->build({argument, argument}); }
+
+    FunctionOverloadResolverPtr multiply;
+};
+
 REGISTER_FUNCTION(Multiply)
 {
     FunctionDocumentation::Description description = "Calculates the product of two values `x` and `y`.";
@@ -71,4 +114,17 @@ REGISTER_FUNCTION(Multiply)
     factory.registerFunction<FunctionMultiply>(documentation);
 }
 
+REGISTER_FUNCTION(Sqr)
+{
+    FunctionDocumentation::Description description = "Calculates the square of a value `x`.";
+    FunctionDocumentation::Syntax syntax = "sqr(x)";
+    FunctionDocumentation::Arguments arguments = {{"x", "Value to square.", {"(U)Int*", "Float*", "Decimal"}}};
+    FunctionDocumentation::ReturnedValue returned_value = {"Returns the product of `x` multiplied by itself."};
+    FunctionDocumentation::Examples examples = {{"Squaring a number", "SELECT sqr(5)", "25"}};
+    FunctionDocumentation::IntroducedIn introduced_in = {26, 7};
+    FunctionDocumentation::Category category = FunctionDocumentation::Category::Arithmetic;
+    FunctionDocumentation documentation = {description, syntax, arguments, {}, returned_value, examples, introduced_in, category};
+
+    factory.registerFunction<FunctionSqr>(documentation, FunctionFactory::Case::Insensitive);
+}
 }
