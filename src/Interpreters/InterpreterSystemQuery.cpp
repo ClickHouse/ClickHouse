40f3ba7a789a7e038e93cd105a84e757698c366e
robot-clickhouse <robot-clickhouse@users.noreply.github.com>
2026-07-02 10:40:24 +0000

    Backport #108460 to 26.5: Require SYSTEM RESET DDL WORKER privilege for SYSTEM RESET DDL WORKER

--- src/Interpreters/InterpreterSystemQuery.cpp
+++ src/Interpreters/InterpreterSystemQuery.cpp
@@ -1224,6 +1224,7 @@ BlockIO InterpreterSystemQuery::execute()
 #endif
 
         case Type::RESET_DDL_WORKER:
+            getContext()->checkAccess(AccessType::SYSTEM_RESET_DDL_WORKER);
             getContext()->getDDLWorker().requestToResetState();
             break;
         default:
@@ -2739,6 +2740,11 @@ AccessRightsElements InterpreterSystemQuery::getRequiredAccessForDDLOnCluster() 
             required_access.emplace_back(AccessType::SYSTEM_INSTRUMENT_REMOVE);
             break;
         }
+        case Type::RESET_DDL_WORKER:
+        {
+            required_access.emplace_back(AccessType::SYSTEM_RESET_DDL_WORKER);
+            break;
+        }
         case Type::ALLOCATE_MEMORY:
         case Type::FREE_MEMORY:
         {
@@ -2754,7 +2760,6 @@ AccessRightsElements InterpreterSystemQuery::getRequiredAccessForDDLOnCluster() 
         case Type::RESET_COVERAGE:
         case Type::SET_COVERAGE_TEST:
         case Type::UNKNOWN:
-        case Type::RESET_DDL_WORKER:
         case Type::END: break;
     }
     return required_access;
