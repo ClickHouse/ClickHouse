d78287aa0ed71c77b5a59c359cb4b5f8cb15de07
uber <uberdever@mail.ru>
2026-07-03 13:40:45 +0300

    compatibility settings

--- tests/queries/0_stateless/04357_compatibility_drop_detached_setting.sql
+++ tests/queries/0_stateless/04357_compatibility_drop_detached_setting.sql
@@ -1,2 +1,2 @@
 SET compatibility = '26.5';
-SELECT name, value, changed FROM system.settings WHERE name = 'allow_experimental_drop_detached_table';
+SELECT name, value FROM system.settings WHERE name = 'allow_experimental_drop_detached_table';
