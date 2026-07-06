e2b558774ff0c07a104046993c8540a4d8f15c70
Eduardo Gomez Saldias <50159560+edugomez102@users.noreply.github.com>
2026-07-06 15:08:24 +0200

    update UTC tests

--- tests/queries/0_stateless/04341_json_isodate_utc_z.sql
+++ tests/queries/0_stateless/04341_json_isodate_utc_z.sql
@@ -2,6 +2,6 @@ DROP TABLE IF EXISTS test_iso;
 
 CREATE TABLE test_iso (ts DateTime64(3, 'UTC')) ENGINE = Memory;
 
-INSERT INTO test_iso FORMAT JSONEachRow {"ts": ISODate("2024-05-29T23:16:12.256")};
+INSERT INTO test_iso FORMAT JSONEachRow {"ts": ISODate("2024-05-29T23:16:12.256Z")};
 
 SELECT ts FROM test_iso;
