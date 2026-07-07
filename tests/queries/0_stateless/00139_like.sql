d476853a62025a8d2faecc8658a45435b1d48e8d
strtgbb <146047128+strtgbb@users.noreply.github.com>
2026-07-07 14:21:35 -0400

    no-random-settings for 00139_like

--- tests/queries/0_stateless/00139_like.sql
+++ tests/queries/0_stateless/00139_like.sql
@@ -1,4 +1,4 @@
--- Tags: stateful
+-- Tags: stateful, no-random-settings
 /* Note that queries are written as the user doesn't really understand that the symbol _ has special meaning in LIKE pattern. */
 SELECT count() FROM test.hits WHERE URL LIKE '%/avtomobili_s_probegom/_%__%__%__%';
 SELECT count() FROM test.hits WHERE URL LIKE '/avtomobili_s_probegom/_%__%__%__%';
