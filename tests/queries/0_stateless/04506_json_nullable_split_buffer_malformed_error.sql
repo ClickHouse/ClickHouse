e1ed590732475e4dfb6a7a198a39db0d4743458c
Eduardo Gomez Saldias <50159560+edugomez102@users.noreply.github.com>
2026-07-08 21:32:52 +0200

    update test 04506 json isodate malformed

--- tests/queries/0_stateless/04506_json_nullable_split_buffer_malformed_error.sql
+++ tests/queries/0_stateless/04506_json_nullable_split_buffer_malformed_error.sql
@@ -1,10 +1,5 @@
 SET session_timezone = 'Etc/UTC';
 
-DROP TABLE IF EXISTS test_nullable_isodate_split;
-
-CREATE TABLE test_nullable_isodate_split (id UInt8, ts Nullable(DateTime64(3))) ENGINE = Memory;
-
 -- A malformed "new ISODate(" near-miss with a tiny read buffer should throw a clean parse error, not crash.
-INSERT INTO test_nullable_isodate_split SETTINGS max_read_buffer_size = 1, input_format_parallel_parsing = 0 FORMAT JSONEachRow {"id": 1, "ts": new Foo(1)}; -- { serverError CANNOT_PARSE_INPUT_ASSERTION_FAILED }
-
-DROP TABLE test_nullable_isodate_split;
+SELECT * FROM format(JSONEachRow, 'id UInt8, ts Nullable(DateTime64(3))', '{"id": 1, "ts": new Foo(1)}')
+    SETTINGS max_read_buffer_size = 1, input_format_parallel_parsing = 0; -- { serverError CANNOT_PARSE_INPUT_ASSERTION_FAILED }
