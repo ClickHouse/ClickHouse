7d9ac08f0f0c4bf54cc9de83ceb2d569a47348f5
Fgrtue <kirill.kopnev@clickhouse.com>
2026-07-11 19:57:43 +0000

    Removed unuser error entry

--- src/Interpreters/InterpreterExplainQuery.cpp
+++ src/Interpreters/InterpreterExplainQuery.cpp
@@ -92,7 +92,6 @@ namespace ErrorCodes
     extern const int LOGICAL_ERROR;
     extern const int NOT_IMPLEMENTED;
     extern const int BAD_ARGUMENTS;
-    extern const int ILLEGAL_STREAM;
 }
 
 namespace
