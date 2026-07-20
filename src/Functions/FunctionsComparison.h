31510141f74a1b5e8e0ad157708a554f335b4184
Diego Gomes Tome <diego.gomestome@clickhouse.com>
2026-07-20 10:55:53 +0000

    missing include

--- src/Functions/FunctionsComparison.h
+++ src/Functions/FunctionsComparison.h
@@ -24,6 +24,7 @@
 #include <DataTypes/DataTypeTuple.h>
 #include <DataTypes/DataTypeUUID.h>
 #include <DataTypes/DataTypesNumber.h>
+#include <DataTypes/DataTypeLowCardinality.h>
 #include <DataTypes/NumberTraits.h>
 #include <DataTypes/getLeastSupertype.h>
 #include <Functions/FunctionHelpers.h>
