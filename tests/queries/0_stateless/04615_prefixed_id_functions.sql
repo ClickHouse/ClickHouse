794ba57ec93fdaa4880caa71becd98c01fe1848f
Kseniia Lukina <ksluki98@yandex.ru>
2026-07-21 05:18:53 +0400

    Use neutral prefixes in examples.

--- tests/queries/0_stateless/04615_prefixed_id_functions.sql
+++ tests/queries/0_stateless/04615_prefixed_id_functions.sql
@@ -1,63 +1,63 @@
 -- The reference table of split semantics: split at the last underscore, never fails.
 SELECT id, prefixedIDPrefix(id), prefixedIDBody(id), splitPrefixedID(id) FROM (SELECT arrayJoin([
-    'cus_NffrFeUfNV2Hib',
-    'pk_test_51TpZvW',
+    'user_NffrFeUfNV2Hib',
+    'ch_test_51TpZvW',
     'abc123',
     '',
-    'cus_',
+    'user_',
     '_abc']) id);
 
 -- The scalar functions must agree with the tuple function.
 SELECT sum((prefixedIDPrefix(id), prefixedIDBody(id)) != splitPrefixedID(id)) FROM (SELECT arrayJoin([
-    'cus_NffrFeUfNV2Hib', 'pk_test_51TpZvW', 'abc123', '', 'cus_', '_abc', 'a_b_c_d', '___', '_']) id);
+    'user_NffrFeUfNV2Hib', 'ch_test_51TpZvW', 'abc123', '', 'user_', '_abc', 'a_b_c_d', '___', '_']) id);
 
 -- Validity: optional multi-segment prefix, then a non-empty base62 body.
 SELECT id, isValidPrefixedID(id) FROM (SELECT arrayJoin([
-    'cus_NffrFeUfNV2Hib',
-    'pk_test_51TpZvW',
+    'user_NffrFeUfNV2Hib',
+    'ch_test_51TpZvW',
     'abc123',
     '123abc',
-    'cus_9x',
+    'user_9x',
     'a_1_b',
     '',
-    'cus_',
+    'user_',
     '_abc',
-    'cus__x',
-    '1cus_x',
-    'cus-x',
-    'cus x',
-    'cus_x!']) id);
+    'user__x',
+    '1user_x',
+    'user-x',
+    'user x',
+    'user_x!']) id);
 
 -- Validity with an expected prefix.
-SELECT isValidPrefixedID('cus_NffrFeUfNV2Hib', 'cus');
-SELECT isValidPrefixedID('cus_NffrFeUfNV2Hib', 'pk');
-SELECT isValidPrefixedID('pk_test_51TpZvW', 'pk_test');
-SELECT isValidPrefixedID('pk_test_51TpZvW', 'pk');
+SELECT isValidPrefixedID('user_NffrFeUfNV2Hib', 'user');
+SELECT isValidPrefixedID('user_NffrFeUfNV2Hib', 'ch');
+SELECT isValidPrefixedID('ch_test_51TpZvW', 'ch_test');
+SELECT isValidPrefixedID('ch_test_51TpZvW', 'ch');
 SELECT isValidPrefixedID('abc123', 'abc');
-SELECT isValidPrefixedID('cus__x', 'cus_');
-SELECT isValidPrefixedID('cus_NffrFeUfNV2Hib', ''); -- { serverError BAD_ARGUMENTS }
+SELECT isValidPrefixedID('user__x', 'user_');
+SELECT isValidPrefixedID('user_NffrFeUfNV2Hib', ''); -- { serverError BAD_ARGUMENTS }
 
--- Generator: format, default and custom body length, multi-segment prefix, non-constant arguments.
-SELECT match(generatePrefixedID('cus'), '^cus_[0-9A-Za-z]{22}$');
-SELECT match(generatePrefixedID('pk_test', 10), '^pk_test_[0-9A-Za-z]{10}$');
+-- Generator: format, default and usertom body length, multi-segment prefix, non-constant arguments.
+SELECT match(generatePrefixedID('user'), '^user_[0-9A-Za-z]{22}$');
+SELECT match(generatePrefixedID('ch_test', 10), '^ch_test_[0-9A-Za-z]{10}$');
 SELECT match(generatePrefixedID('a', 1), '^a_[0-9A-Za-z]$');
 SELECT match(generatePrefixedID('a', 255), '^a_[0-9A-Za-z]{255}$');
-SELECT isValidPrefixedID(generatePrefixedID('cus'), 'cus');
-SELECT prefixedIDPrefix(generatePrefixedID(prefix)) = prefix FROM (SELECT arrayJoin(['cus', 'pk_test', 'acct']) prefix);
+SELECT isValidPrefixedID(generatePrefixedID('user'), 'user');
+SELECT prefixedIDPrefix(generatePrefixedID(prefix)) = prefix FROM (SELECT arrayJoin(['user', 'ch_test', 'ord']) prefix);
 SELECT length(prefixedIDBody(generatePrefixedID('x', number + 1))) = number + 1 FROM numbers(3);
 SELECT prefixedIDBody(generatePrefixedID('x')) != prefixedIDBody(generatePrefixedID('y'));
 
 -- Generator argument validation.
 SELECT generatePrefixedID(''); -- { serverError BAD_ARGUMENTS }
-SELECT generatePrefixedID('_cus'); -- { serverError BAD_ARGUMENTS }
-SELECT generatePrefixedID('cus_'); -- { serverError BAD_ARGUMENTS }
-SELECT generatePrefixedID('cus__test'); -- { serverError BAD_ARGUMENTS }
-SELECT generatePrefixedID('1cus'); -- { serverError BAD_ARGUMENTS }
-SELECT generatePrefixedID('pk_1test'); -- { serverError BAD_ARGUMENTS }
-SELECT generatePrefixedID('pk-test'); -- { serverError BAD_ARGUMENTS }
-SELECT generatePrefixedID('cus', 0); -- { serverError BAD_ARGUMENTS }
-SELECT generatePrefixedID('cus', 256); -- { serverError BAD_ARGUMENTS }
-SELECT generatePrefixedID('cus', -1); -- { serverError ILLEGAL_TYPE_OF_ARGUMENT }
+SELECT generatePrefixedID('_user'); -- { serverError BAD_ARGUMENTS }
+SELECT generatePrefixedID('user_'); -- { serverError BAD_ARGUMENTS }
+SELECT generatePrefixedID('user__test'); -- { serverError BAD_ARGUMENTS }
+SELECT generatePrefixedID('1user'); -- { serverError BAD_ARGUMENTS }
+SELECT generatePrefixedID('ch_1test'); -- { serverError BAD_ARGUMENTS }
+SELECT generatePrefixedID('ch-test'); -- { serverError BAD_ARGUMENTS }
+SELECT generatePrefixedID('user', 0); -- { serverError BAD_ARGUMENTS }
+SELECT generatePrefixedID('user', 256); -- { serverError BAD_ARGUMENTS }
+SELECT generatePrefixedID('user', -1); -- { serverError ILLEGAL_TYPE_OF_ARGUMENT }
 SELECT generatePrefixedID(42); -- { serverError ILLEGAL_TYPE_OF_ARGUMENT }
 SELECT prefixedIDPrefix(42); -- { serverError ILLEGAL_TYPE_OF_ARGUMENT }
-SELECT isValidPrefixedID('cus_x', 'cus', 'extra'); -- { serverError NUMBER_OF_ARGUMENTS_DOESNT_MATCH }
+SELECT isValidPrefixedID('user_x', 'user', 'extra'); -- { serverError NUMBER_OF_ARGUMENTS_DOESNT_MATCH }
