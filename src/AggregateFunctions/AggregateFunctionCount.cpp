6f400332619bde446aeb48411a087c1c0dfbd513
Elmi Ahmadov <elmi.ahmadov@clickhouse.com>
2026-07-23 00:43:04 +0200

    use memory tracked vector

--- src/AggregateFunctions/AggregateFunctionCount.cpp
+++ src/AggregateFunctions/AggregateFunctionCount.cpp
@@ -3,6 +3,7 @@
 #include <Columns/ColumnAggregateFunction.h>
 #include <AggregateFunctions/AggregateFunctionCount.h>
 #include <AggregateFunctions/FactoryHelpers.h>
+#include <Common/VectorWithMemoryTracking.h>
 #include <Common/scope_guard_safe.h>
 
 #if USE_EMBEDDED_COMPILER
@@ -23,7 +24,7 @@ struct Settings;
 
 ColumnPtr createSingleCountStateColumn(const AggregateFunctionPtr & count_function, UInt64 num_rows)
 {
-    std::vector<char> state(count_function->sizeOfData());
+    VectorWithMemoryTracking<char> state(count_function->sizeOfData());
     AggregateDataPtr place = state.data();
     count_function->create(place);
     SCOPE_EXIT_MEMORY_SAFE(count_function->destroy(place));
