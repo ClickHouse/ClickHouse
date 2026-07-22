d4d67a3a9081817daf9dc8b44c7181419a696859
Hechem Selmi <hechem.selmi@clickhouse.com>
2026-07-22 07:07:37 +0000

    Pin new analyzer.

--- tests/queries/0_stateless/04614_join_runtime_filter_shared_fixed_hash_table_key_cast.sql
+++ tests/queries/0_stateless/04614_join_runtime_filter_shared_fixed_hash_table_key_cast.sql
@@ -17,9 +17,12 @@ INSERT INTO t_build_u8 SELECT toUInt8(number) FROM numbers(50);
 INSERT INTO t_probe_nullable SELECT if(number % 50 = 0, NULL, toInt32(number % 250)) FROM numbers(20000);
 INSERT INTO t_probe_i32 SELECT toInt32(number % 250 - 100) FROM numbers(20000);
 
+SET enable_analyzer = 1;
+SET enable_parallel_replicas = 0; -- The descriptors are not serialized with the query plan.
+
 SET join_algorithm = 'hash', max_bytes_before_external_join = 0, max_bytes_ratio_before_external_join = 0;
 SET enable_join_runtime_filters = 1, enable_join_fixed_hash_table_conversion = 1, join_runtime_filter_from_fixed_hash_table = 1;
-SET enable_parallel_replicas = 0; -- The descriptors are not serialized with the query plan.
+
 
 -- Nullable probe + non-Nullable build: the common type Nullable(Int32) forces a cast on the build key.
 SELECT '-- nullable probe, plain build';
