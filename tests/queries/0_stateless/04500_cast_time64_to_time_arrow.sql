5b26b643b827fee6c12aa91740a4fff0e537a958
yariks5s <yaroslav.briukhovetskyi@clickhouse.com>
2026-07-24 15:01:08 +0000

    Move SETTINGS before VALUES in 04500 Arrow test too

--- tests/queries/0_stateless/04500_cast_time64_to_time_arrow.sql
+++ tests/queries/0_stateless/04500_cast_time64_to_time_arrow.sql
@@ -3,5 +3,5 @@
 
 -- Arrow stores Time as Time64(0), so reading a Time column back needs the Time64 -> Time cast.
 SET enable_time_time64_type = 1;
-INSERT INTO FUNCTION file('04500_time64_to_time.arrow', Arrow, 'c1 Time') VALUES ('10:00:00') SETTINGS engine_file_truncate_on_insert = 1;
+INSERT INTO FUNCTION file('04500_time64_to_time.arrow', Arrow, 'c1 Time') SETTINGS engine_file_truncate_on_insert = 1 VALUES ('10:00:00');
 SELECT c1 = CAST('10:00:00' AS Time) FROM file('04500_time64_to_time.arrow', Arrow, 'c1 Time');
