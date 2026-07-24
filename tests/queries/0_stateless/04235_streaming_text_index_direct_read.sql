3268c88403948a4216d2815e124fb46ba6dd3558
Mikhail Artemenko <mikhail.artemenko@clickhouse.com>
2026-07-24 16:01:36 +0000

    add test

---
+++ tests/queries/0_stateless/04235_streaming_text_index_direct_read.sql
@@ -0,0 +1,25 @@
+-- Tags: no-parallel-replicas, no-old-analyzer
+
+SET enable_streaming_queries = 1;
+SET allow_experimental_full_text_index = 1;
+SET query_plan_direct_read_from_text_index = 1;
+SET query_plan_text_index_add_hint = 1;
+SET query_plan_merge_filters = 1;
+
+DROP TABLE IF EXISTS t_stream_text_index;
+
+CREATE TABLE t_stream_text_index
+(
+    id UInt64,
+    map Map(String, String),
+    INDEX idx_map_keys mapKeys(map) TYPE text(tokenizer = 'array') GRANULARITY 1,
+    INDEX idx_map_values mapValues(map) TYPE text(tokenizer = 'array') GRANULARITY 1
+)
+ENGINE = MergeTree ORDER BY id
+SETTINGS min_bytes_for_wide_part = 0, enable_block_number_column = 1, enable_block_offset_column = 1;
+
+INSERT INTO t_stream_text_index SELECT number, map('env', if(number < 3, 'prod', 'staging')) FROM numbers(20);
+
+SELECT id FROM (SELECT id FROM t_stream_text_index STREAM PREWHERE ('prod') IN (map[materialize('env')]) LIMIT 3) ORDER BY id;
+
+DROP TABLE t_stream_text_index;
