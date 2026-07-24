43507715e1e823b23702350632c5e522fcc1ef6f
Albert Zhemukov <101018980+zhemalb@users.noreply.github.com>
2026-07-24 17:58:01 +0300

    Update geo aggregate introduction version to 26.8

--- src/AggregateFunctions/AggregateFunctionGroupPolygonUnion.cpp
+++ src/AggregateFunctions/AggregateFunctionGroupPolygonUnion.cpp
@@ -323,7 +323,7 @@ SELECT wkt(groupPolygonUnion(p)) FROM (
             R"(
 MULTIPOLYGON(((1 2,1 3,3 3,3 1,2 1,2 0,0 0,0 2,1 2)))
         )"}};
-    FunctionDocumentation::IntroducedIn introduced_in = {26, 7};
+    FunctionDocumentation::IntroducedIn introduced_in = {26, 8};
     FunctionDocumentation::Category category = FunctionDocumentation::Category::GeoPolygon;
     FunctionDocumentation documentation = {description, syntax, arguments, {}, returned_value, examples, introduced_in, category};
 
