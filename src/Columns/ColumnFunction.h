f19712dc3fd35d9f671ef065898614571d8f1589
Diego Gomes Tomé <diegogomest@gmail.com>
2026-07-27 14:10:18 +0200

    Update src/Columns/ColumnFunction.h
    Co-authored-by: Raufs Dunamalijevs <raufs.dunamalijevs@clickhouse.com>

--- src/Columns/ColumnFunction.h
+++ src/Columns/ColumnFunction.h
@@ -218,8 +218,7 @@ private:
     /// Determine if passed function is compiled. Used for profiling.
     bool is_function_compiled;
 
-    /// If true, replicate function wraps captured columns into ColumnReplicated instead of
-    /// physically copying them
+    /// If true, replicate function wraps captured columns into ColumnReplicated instead of physically copying them.
     /// Controlled by the setting enable_lazy_columns_replication.
     bool allow_lazy_replicated_captures = false;
 
