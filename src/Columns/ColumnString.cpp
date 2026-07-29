2d0952f4b9e2a07bc1b03fec4ef9c6497675873d
Miсhael Stetsyuk <59827607+mstetsyuk@users.noreply.github.com>
2026-07-29 11:47:17 +0100

    fix build

--- src/Columns/ColumnString.cpp
+++ src/Columns/ColumnString.cpp
@@ -25,6 +25,7 @@ namespace ErrorCodes
     extern const int PARAMETER_OUT_OF_BOUND;
     extern const int SIZES_OF_COLUMNS_DOESNT_MATCH;
     extern const int LOGICAL_ERROR;
+    extern const int INCORRECT_DATA;
 }
 
 
