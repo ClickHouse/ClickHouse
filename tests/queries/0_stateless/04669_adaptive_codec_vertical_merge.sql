cf3fc5dfd9461b044d53bcf192b57cd67c3a92f1
Raufs Dunamalijevs <raufs.dunamalijevs@clickhouse.com>
2026-08-02 22:36:59 +0200

    Apply suggestion from @rienath

--- tests/queries/0_stateless/04669_adaptive_codec_vertical_merge.sql
+++ tests/queries/0_stateless/04669_adaptive_codec_vertical_merge.sql
@@ -17,7 +17,7 @@ OPTIMIZE TABLE t_vert_adaptive FINAL;
 SYSTEM FLUSH LOGS part_log;
 
 -- The merge actually used the vertical algorithm; otherwise `n` is written by the horizontal
--- call site (MergeTask.cpp:1053) and the vertical writer at MergeTask.cpp:1985 stays untested.
+-- call site and the vertical writer stays untested.
 SELECT 'merge_algorithm', merge_algorithm FROM system.part_log
 WHERE database = currentDatabase() AND table = 't_vert_adaptive' AND event_type = 'MergeParts'
 ORDER BY event_time_microseconds DESC LIMIT 1;
