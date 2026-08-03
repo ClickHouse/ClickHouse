e8e5f9d50f64d9b684dfe6605c0521e79fc2b2a1
robot-clickhouse <robot-clickhouse@users.noreply.github.com>
2026-08-03 23:31:01 +0000

    Backport #111379 to 26.5: Fix misreporting of datalake catalog errors as missing tables

--- src/Common/FailPoint.cpp
+++ src/Common/FailPoint.cpp
@@ -190,6 +190,7 @@ static struct InitFiu
     REGULAR(database_replicated_force_metadata_digest_check) \
     PAUSEABLE(truncate_database_tables_pause) \
     REGULAR(datalake_try_get_table_return_nullptr) \
+    REGULAR(datalake_get_tables_throw) \
     PAUSEABLE_ONCE(drop_database_before_exclusive_ddl_lock) \
     REGULAR(storage_merge_tree_background_schedule_merge_fail) \
     REGULAR(patch_parts_reverse_column_order) \
