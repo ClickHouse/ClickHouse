3cbf22a84614941563f50a06cc07fd35a2bd2114
Valerii Petrov <valerii.petrov@ringcentral.com>
2026-08-04 16:36:07 -0700

    Rename PQT:: to PrometheusQueryTree:: for compatibility with master's namespace unwind

--- src/Storages/TimeSeries/PrometheusQueryToSQL/applySortFunction.h
+++ src/Storages/TimeSeries/PrometheusQueryToSQL/applySortFunction.h
@@ -13,5 +13,5 @@ bool isSortFunction(std::string_view function_name);
 
 /// Applies `sort` or `sort_desc` to an instant vector and captures the order at this point.
 SQLQueryPiece applySortFunction(
-    const PQT::Function * function_node, std::vector<SQLQueryPiece> && arguments, ConverterContext & context);
+    const PrometheusQueryTree::Function * function_node, std::vector<SQLQueryPiece> && arguments, ConverterContext & context);
 }
