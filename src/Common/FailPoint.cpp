f06246f29bc7f60a179d2c9baea4db111bc82e50
robot-clickhouse <robot-clickhouse@users.noreply.github.com>
2026-08-07 13:10:50 +0000

    Backport #113322 to 26.7: fs cache download wait timeout

--- src/Common/FailPoint.cpp
+++ src/Common/FailPoint.cpp
@@ -89,6 +89,7 @@ static struct InitFiu
     REGULAR(distributed_cache_cancel_query_in_response_wait) \
     REGULAR(file_cache_stall_free_space_ratio_keeping_thread) \
     PAUSEABLE(file_cache_pause_before_do_eviction) \
+    PAUSEABLE(file_segment_pause_before_write) \
     REGULAR(file_cache_simulate_evicting_segment) \
     REGULAR(cache_filesystem_failure) \
     REGULAR(cache_filesystem_failure_non_errno) \
