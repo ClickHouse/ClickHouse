fa9fc3303a7c5d1e05cc50414f4d38a74465789d
Robert Schulze <robert@clickhouse.com>
2026-08-07 08:37:09 +0000

    Docs: Make it more clear which NLP functions are experimental or non-experimental

--- src/Functions/FunctionsCharsetClassification.cpp
+++ src/Functions/FunctionsCharsetClassification.cpp
@@ -149,12 +149,15 @@ using FunctionDetectLanguageUnknown = FunctionTextClassificationString<CharsetCl
 REGISTER_FUNCTION(DetectCharset)
 {
     FunctionDocumentation::Description description_charset = R"(
-Detects the character set of a non-UTF8-encoded input string.
+<ExperimentalBadge/>
+<CloudNotSupportedBadge/>
 
 :::warning
 This function is experimental and may change in unpredictable backwards-incompatible ways in future releases.
 Set `allow_experimental_nlp_functions = 1` to enable it.
 :::
+
+Detects the character set of a non-UTF8-encoded input string.
 )";
     FunctionDocumentation::Syntax syntax_charset = "detectCharset(s)";
     FunctionDocumentation::Arguments arguments_charset = {
@@ -171,13 +174,16 @@ Set `allow_experimental_nlp_functions = 1` to enable it.
     factory.registerFunction<FunctionDetectCharset>(documentation_charset);
 
     FunctionDocumentation::Description description_unknown = R"(
-Similar to the [`detectLanguage`](#detectLanguage) function, except the detectLanguageUnknown function works with non-UTF8-encoded strings.
-Prefer this version when your character set is UTF-16 or UTF-32.
+<ExperimentalBadge/>
+<CloudNotSupportedBadge/>
 
 :::warning
 This function is experimental and may change in unpredictable backwards-incompatible ways in future releases.
 Set `allow_experimental_nlp_functions = 1` to enable it.
 :::
+
+Similar to the [`detectLanguage`](#detectLanguage) function, except the detectLanguageUnknown function works with non-UTF8-encoded strings.
+Prefer this version when your character set is UTF-16 or UTF-32.
 )";
     FunctionDocumentation::Syntax syntax_unknown = "detectLanguageUnknown('s')";
     FunctionDocumentation::Arguments arguments_unknown = {
