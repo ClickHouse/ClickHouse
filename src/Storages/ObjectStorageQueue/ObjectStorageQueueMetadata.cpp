0cd5a4e8e521193434877e0be6f7f634d6dce332
Aditya Kumar <aditya.kumar@ksolves.com>
2026-08-07 20:17:49 +0530

    Fix style check: add missing NOT_IMPLEMENTED error code declaration
    The ErrorCodes::NOT_IMPLEMENTED was used at line 1477 but not declared
    in the namespace ErrorCodes block. This caused a style-check failure in CI.
    
    Added the missing extern declaration to fix the build.

--- src/Storages/ObjectStorageQueue/ObjectStorageQueueMetadata.cpp
+++ src/Storages/ObjectStorageQueue/ObjectStorageQueueMetadata.cpp
@@ -48,6 +48,7 @@ namespace ErrorCodes
     extern const int REPLICA_ALREADY_EXISTS;
     extern const int SUPPORT_IS_DISABLED;
     extern const int TIMEOUT_EXCEEDED;
+    extern const int NOT_IMPLEMENTED;
 }
 
 namespace Setting
