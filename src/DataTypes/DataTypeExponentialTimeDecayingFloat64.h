a1e28f1e9fc92e19622fbdd1dfe78be57f9c3002
vanchaklar <38177830+vanchaklar@users.noreply.github.com>
2026-08-08 12:44:12 +0330

    Parameterize exponential time-decaying values

--- src/DataTypes/DataTypeExponentialTimeDecayingFloat64.h
+++ src/DataTypes/DataTypeExponentialTimeDecayingFloat64.h
@@ -1,13 +1,32 @@
 #pragma once
 
+#include <DataTypes/DataTypeCustom.h>
 #include <DataTypes/IDataType.h>
+
+#include <optional>
 
 namespace DB
 {
 
 class DataTypeFactory;
 
-DataTypePtr createDataTypeExponentialTimeDecayingFloat64();
+class DataTypeCustomExponentialTimeDecayingFloat64 final : public IDataTypeCustomName
+{
+public:
+    explicit DataTypeCustomExponentialTimeDecayingFloat64(Float64 decay_length_)
+        : decay_length(decay_length_)
+    {
+    }
+
+    String getName() const override;
+    Float64 getDecayLength() const { return decay_length; }
+
+private:
+    const Float64 decay_length;
+};
+
+DataTypePtr createDataTypeExponentialTimeDecayingFloat64(Float64 decay_length);
+std::optional<Float64> tryGetExponentialTimeDecayingFloat64DecayLength(const DataTypePtr & type);
 bool isExponentialTimeDecayingFloat64(const DataTypePtr & type);
 
 void registerDataTypeExponentialTimeDecayingFloat64(DataTypeFactory & factory);
