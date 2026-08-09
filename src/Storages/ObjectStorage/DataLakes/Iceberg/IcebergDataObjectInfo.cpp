e0fa8f5a2adcec466bdd64231a5bf0ee6333adfb
linjiayu <linjiayu1025@gmail.com>
2026-08-09 23:34:41 +0800

    iceberg deletion vectors delete useless config

--- src/Storages/ObjectStorage/DataLakes/Iceberg/IcebergDataObjectInfo.cpp
+++ src/Storages/ObjectStorage/DataLakes/Iceberg/IcebergDataObjectInfo.cpp
@@ -34,7 +34,6 @@ namespace DB
 
 namespace Setting
 {
-extern const SettingsBool allow_experimental_iceberg_deletion_vectors;
 extern const SettingsBool use_roaring_bitmap_iceberg_positional_deletes;
 };
 
@@ -117,14 +116,14 @@ void IcebergDataObjectInfo::addPositionDeleteObject(Iceberg::ProcessedManifestFi
         if (!position_delete_object->parsed_entry->content_offset.has_value()
             || !position_delete_object->parsed_entry->content_size_in_bytes.has_value())
             throw Exception(ErrorCodes::ICEBERG_SPECIFICATION_VIOLATION, "Iceberg deletion vector does not have content offset or size");
-       
+
         /// There can be at most one deletion vector for a data file in a snapshot.
         if (std::any_of(
                 info.position_deletes_objects.begin(),
                 info.position_deletes_objects.end(),
                 [](const auto & object) { return object.isDeletionVector(); }))
             throw Exception(ErrorCodes::ICEBERG_SPECIFICATION_VIOLATION, "Multiple deletion vectors apply to the same Iceberg data file");
-        
+
         /// Iceberg guarantees that a deletion vector for this data file contains all position deletes
         /// from the files it replaces, so remove the replaced position delete files.
         info.position_deletes_objects.erase(
