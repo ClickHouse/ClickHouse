c8133483244c238c3f325bc649a49184c70f51b5
Minh Vu <vuhoangminh97@gmail.com>
2026-08-11 00:48:19 +0200

    Use dense lookup for TimeSeries tag transforms

--- tests/queries/0_stateless/03779_time_series_tags_functions.sql
+++ tests/queries/0_stateless/03779_time_series_tags_functions.sql
@@ -273,3 +273,80 @@ FROM
 (
     SELECT timeSeriesTagsToGroup([('__name__', 'up')]) AS group
 );  -- { serverError CANNOT_EXECUTE_PROMQL_QUERY }
+
+SELECT '';
+SELECT 'timeSeriesRemoveTag vectorized dense groups:';
+
+SELECT timeSeriesGroupToTags(new_group)
+FROM
+(
+    SELECT number,
+           timeSeriesTagsToGroup([('__name__', 'metric'), ('instance', toString(number))]) AS old_group,
+           timeSeriesRemoveTag(old_group, '__name__') AS new_group
+    FROM numbers(6)
+)
+ORDER BY number;
+
+SELECT '';
+SELECT 'timeSeriesRemoveTag vectorized sparse groups:';
+
+WITH
+    (
+        SELECT groupArray(group)
+        FROM
+        (
+            SELECT number,
+                   timeSeriesTagsToGroup([('__name__', 'metric'), ('instance', toString(number))]) AS group
+            FROM numbers(32)
+            ORDER BY number
+        )
+    ) AS groups
+SELECT timeSeriesGroupToTags(timeSeriesRemoveTag(groups[number * 8 + 1], '__name__'))
+FROM numbers(4)
+ORDER BY number;
+
+SELECT '';
+SELECT 'timeSeriesRemoveAllTagsExcept vectorized repeated dense groups:';
+
+WITH
+    (
+        SELECT groupArray(group)
+        FROM
+        (
+            SELECT number,
+                   timeSeriesTagsToGroup([('shared', 'value'), ('unique', toString(number))]) AS group
+            FROM numbers(3)
+            ORDER BY number
+        )
+    ) AS groups
+SELECT count(),
+       uniqExact(new_group),
+       groupUniqArray(timeSeriesGroupToTags(new_group))
+FROM
+(
+    SELECT timeSeriesRemoveAllTagsExcept(groups[[1, 2, 1, 3, 2, 1][number + 1]], ['shared']) AS new_group
+    FROM numbers(6)
+);
+
+SELECT '';
+SELECT 'timeSeriesRemoveAllTagsExcept vectorized repeated sparse groups:';
+
+WITH
+    (
+        SELECT groupArray(group)
+        FROM
+        (
+            SELECT number,
+                   timeSeriesTagsToGroup([('shared', 'value'), ('unique', toString(number))]) AS group
+            FROM numbers(32)
+            ORDER BY number
+        )
+    ) AS groups
+SELECT count(),
+       uniqExact(new_group),
+       groupUniqArray(timeSeriesGroupToTags(new_group))
+FROM
+(
+    SELECT timeSeriesRemoveAllTagsExcept(groups[[1, 25, 1, 25, 1][number + 1]], ['shared']) AS new_group
+    FROM numbers(5)
+);
