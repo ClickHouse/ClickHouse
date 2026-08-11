638d9047b9bd040a938d986839e46416fb33ea17
uber <uberdever@mail.ru>
2026-08-11 17:06:26 +0300

    updated docs to highlight that observations are local

--- src/Coordination/KeeperAsynchronousMetrics.cpp
+++ src/Coordination/KeeperAsynchronousMetrics.cpp
@@ -94,8 +94,8 @@ void updateKeeperInformation(KeeperDispatcher & keeper_dispatcher, AsynchronousM
     new_values["KeeperIsObserver"] = { is_observer, "1 if ClickHouse Keeper is an observer, 0 otherwise." };
     new_values["KeeperIsStandalone"] = { is_standalone, "1 if ClickHouse Keeper is in a standalone mode, 0 otherwise." };
     new_values["KeeperIsExceedingMemorySoftLimitHit"] = { is_exceeding_mem_soft_limit, "1 if ClickHouse Keeper is exceeding the memory soft limit, 0 otherwise." };
-    new_values["KeeperLastLeaderElectionTime"] = { last_leader_election_time_ms, "Duration in milliseconds of the most recent election won by this ClickHouse Keeper leader. `0` if this instance is not the active leader or has no recorded completed election." };
-    new_values["KeeperLastLeaderUnavailableTime"] = { last_leader_unavailable_time_ms, "Duration in milliseconds of the most recent no-leader window completed by this ClickHouse Keeper leader. `0` if this instance is not the active leader or has no recorded completed no-leader window." };
+    new_values["KeeperLastLeaderElectionTime"] = { last_leader_election_time_ms, "Duration in milliseconds of the most recent locally observed leader election won by this ClickHouse Keeper leader. `0` if this instance is not the active leader or has no recorded completed election." };
+    new_values["KeeperLastLeaderUnavailableTime"] = { last_leader_unavailable_time_ms, "Duration in milliseconds of the most recent locally observed no-leader window completed by this ClickHouse Keeper leader. `0` if this instance is not the active leader or has no recorded completed no-leader window." };
 
     new_values["KeeperZnodeCount"] = { znode_count, "The number of nodes (data entries) in ClickHouse Keeper." };
     new_values["KeeperWatchCount"] = { watch_count, "The number of watches in ClickHouse Keeper." };
