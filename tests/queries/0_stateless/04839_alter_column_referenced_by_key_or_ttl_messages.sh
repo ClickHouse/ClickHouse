c995a2b17082bc4c314efac0e7a134582902825e
Dergousov Maksim <dergousovmaxim99@gmail.com>
2026-08-11 08:00:00 +0000

    fix: limit grep to first match in ALTER test script
    Signed-off-by: Dergousov Maksim <dergousovmaxim99@gmail.com>

--- tests/queries/0_stateless/04839_alter_column_referenced_by_key_or_ttl_messages.sh
+++ tests/queries/0_stateless/04839_alter_column_referenced_by_key_or_ttl_messages.sh
@@ -5,26 +5,31 @@ CUR_DIR=$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)
 . "$CUR_DIR"/../shell_config.sh
 
 $CLICKHOUSE_CLIENT -q "
+    DROP TABLE IF EXISTS key_column;
     CREATE TABLE key_column (a UInt64, b UInt64) ENGINE = MergeTree ORDER BY a;
     ALTER TABLE key_column DROP COLUMN a;
-" 2>&1 | grep -oF 'Trying to ALTER DROP key a column which is a part of key expression'
+" 2>&1 | grep -m1 -oF 'Trying to ALTER DROP key a column which is a part of key expression'
 
 $CLICKHOUSE_CLIENT -q "
+    DROP TABLE IF EXISTS subcolumn;
     CREATE TABLE subcolumn (a Tuple(x UInt64, y UInt64), b UInt64) ENGINE = MergeTree ORDER BY a.x;
     ALTER TABLE subcolumn DROP COLUMN a;
-" 2>&1 | grep -oF 'Trying to ALTER DROP column a whose subcolumns (`a.x`) are part of key expression'
+" 2>&1 | grep -m1 -oF 'Trying to ALTER DROP column a whose subcolumns (`a.x`) are part of key expression'
 
 $CLICKHOUSE_CLIENT -q "
+    DROP TABLE IF EXISTS sign_column;
     CREATE TABLE sign_column (a UInt64, s Int8) ENGINE = CollapsingMergeTree(s) ORDER BY a;
     ALTER TABLE sign_column DROP COLUMN s;
-" 2>&1 | grep -oF 'Trying to ALTER DROP sign (s) column'
+" 2>&1 | grep -m1 -oF 'Trying to ALTER DROP sign (s) column'
 
 $CLICKHOUSE_CLIENT -q "
+    DROP TABLE IF EXISTS table_ttl;
     CREATE TABLE table_ttl (d Date, a UInt64) ENGINE = MergeTree ORDER BY a TTL d + INTERVAL 1 DAY;
     ALTER TABLE table_ttl DROP COLUMN d;
-" 2>&1 | grep -oF 'Cannot apply ALTER because it breaks the TTL of the table'
+" 2>&1 | grep -m1 -oF 'Cannot apply ALTER because it breaks the TTL of the table'
 
 $CLICKHOUSE_CLIENT -q "
+    DROP TABLE IF EXISTS column_ttl;
     CREATE TABLE column_ttl (d Date, a UInt64, v UInt64 TTL d + INTERVAL 1 DAY) ENGINE = MergeTree ORDER BY a;
     ALTER TABLE column_ttl DROP COLUMN d;
-" 2>&1 | grep -oF 'Cannot apply ALTER because it breaks the TTL of column `v`'
+" 2>&1 | grep -m1 -oF 'Cannot apply ALTER because it breaks the TTL of column `v`'
