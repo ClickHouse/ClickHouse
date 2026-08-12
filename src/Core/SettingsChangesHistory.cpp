5ff3ce714d59cdbc612649fd50f5a0a48550943e
Alexey Milovidov <milovidov@clickhouse.com>
2026-08-12 22:22:45 +0200

    Update SettingsChangesHistory.cpp

--- src/Core/SettingsChangesHistory.cpp
+++ src/Core/SettingsChangesHistory.cpp
@@ -43,7 +43,7 @@ const VersionToSettingsChangesMap & getSettingsChangesHistory()
         /// Note: please check if the key already exists to prevent duplicate entries.
         addSettingsChanges(settings_changes_history, "26.8",
         {
-            {"s3_list_object_parallelism", 1, 1, "New setting to list globbed `s3()` paths with several concurrent `ListObjects` requests by walking the common-prefix tree in parallel."},
+            {"s3_list_object_parallelism", 1, 1, "New setting to list globbed `s3` paths with several concurrent `ListObjects` requests by walking the common-prefix tree in parallel."},
             {"output_format_image_time_multiplier_seconds", 1, 1, "New setting controlling the numerator of the time unit of the `t` column, which makes image output formats such as `PNG` produce an animation."},
             {"output_format_image_time_divisor_seconds", 60, 60, "New setting controlling the denominator of the time unit of the `t` column, which makes image output formats such as `PNG` produce an animation."},
             {"output_format_image_streaming_animation", false, false, "New setting controlling whether image output formats such as `PNG` write each frame of an animation as soon as the next value of `t` is seen, instead of buffering all the frames in memory."},
