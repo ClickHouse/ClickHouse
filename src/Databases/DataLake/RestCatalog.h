f2d85ffc6f1560a420595a5abc91d7cc35806cd6
Konstantin Vedernikov <vedernikovkostya@mail.ru>
2026-08-12 12:07:32 +0000

    start

--- src/Databases/DataLake/RestCatalog.h
+++ src/Databases/DataLake/RestCatalog.h
@@ -77,6 +77,10 @@ public:
     /// Inherited by every catalog based on the Iceberg REST protocol.
     DataLakeTableFormat getTableFormat(const TableMetadata &) const override { return DataLakeTableFormat::ICEBERG; }
 
+    std::optional<std::string> getDefaultTableLocation(
+        const std::string & namespace_name,
+        const std::string & table_name) const override;
+
     void createTable(const String & namespace_name, const String & table_name, const String & new_metadata_path, Poco::JSON::Object::Ptr metadata_content) const override;
 
     bool updateMetadata(const String & namespace_name, const String & table_name, const String & new_metadata_path, Poco::JSON::Object::Ptr new_snapshot) const override;
@@ -149,6 +153,8 @@ protected:
         DB::ContextPtr context_);
 
     void createNamespaceIfNotExists(const String & namespace_name, const String & location) const override;
+
+    std::optional<std::string> getNamespaceLocation(const std::string & namespace_name) const;
 
     const std::filesystem::path base_url;
     const LoggerPtr log;
--- src/Storages/ObjectStorage/StorageObjectStorageConfiguration.h
+++ src/Storages/ObjectStorage/StorageObjectStorageConfiguration.h
@@ -287,6 +287,11 @@ public:
         return nullptr;
     }
 
+    virtual ASTs completeEngineArgsFromCatalog(const StorageID & /*table_id*/, ContextPtr /*context*/) const
+    {
+        return {};
+    }
+
     virtual bool optimize(ObjectStoragePtr /*object_storage*/, const StorageMetadataPtr & /*metadata_snapshot*/, ContextPtr /*context*/, const std::optional<FormatSettings> & /*format_settings*/)
     {
         throw Exception(ErrorCodes::NOT_IMPLEMENTED, "Table engine {} doesn't support optimize", getTypeName());
