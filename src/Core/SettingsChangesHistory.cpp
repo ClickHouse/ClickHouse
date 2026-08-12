ee53ef0617586ee3e18a3cc432009ed22a52d249
Anton Ivashkin <ianton@live.com>
2026-08-12 12:08:18 +0200

    Fix version in settings history

--- src/Core/SettingsChangesHistory.cpp
+++ src/Core/SettingsChangesHistory.cpp
@@ -39,7 +39,7 @@ const VersionToSettingsChangesMap & getSettingsChangesHistory()
         /// controls new feature and it's 'true' by default, use 'false' as previous_value).
         /// It's used to implement `compatibility` setting (see https://github.com/ClickHouse/ClickHouse/issues/35972)
         /// Note: please check if the key already exists to prevent duplicate entries.
-        addSettingsChanges(settings_changes_history, "26.6.1.20001.altinityantalya",
+        addSettingsChanges(settings_changes_history, "26.6.2.20001.altinityantalya",
         {
             {"use_puffin_files_cache", true, true, "Enables cache of parsed Puffin file content such as deletion vectors."},
         });
