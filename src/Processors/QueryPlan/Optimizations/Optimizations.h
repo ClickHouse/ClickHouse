c431959951b58b10678b7675fa7f8b625b0b9c4e
Nikolai Kochetov <nk@clickhouse.com>
2026-08-13 19:14:01 +0200

    Update Optimizations.h

--- src/Processors/QueryPlan/Optimizations/Optimizations.h
+++ src/Processors/QueryPlan/Optimizations/Optimizations.h
@@ -160,8 +160,6 @@ size_t tryRemoveUnusedColumns(QueryPlan::Node * node, QueryPlan::Nodes &, const 
 /// This condition can potentially be pushed down all the way to the storage and filter unmatched rows very early.
 bool tryAddJoinRuntimeFilter(QueryPlan::Node & node, QueryPlan::Nodes & nodes, const QueryPlanOptimizationSettings & optimization_settings);
 
-
-
 /// Optimize ORDER BY ... LIMIT n query by using skip index or Prewhere threshold filtering
 size_t tryOptimizeTopK(QueryPlan::Node * parent_node, QueryPlan::Nodes & nodes, const Optimization::ExtraSettings & settings);
 
