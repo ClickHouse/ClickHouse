6ddad89db50279a4397d58ee0e4ab5342ec37970
Alexey Milovidov <milovidov@clickhouse.com>
2026-08-15 19:55:30 +0000

    Fix `FunctionMathUnary` Float32 signature return type

--- src/Functions/FunctionMathUnary.h
+++ src/Functions/FunctionMathUnary.h
@@ -46,7 +46,7 @@ private:
         if constexpr (Impl::always_returns_float64)
             return "(Number) -> Float64";
         else
-            return "(T : Float32 | Float64) -> T OR (Number) -> Float64";
+            return "(Float32) -> Float32 OR (Float64) -> Float64 OR (Number) -> Float64";
     }
 
     DataTypePtr getReturnTypeForDefaultImplementationForDynamic() const override
