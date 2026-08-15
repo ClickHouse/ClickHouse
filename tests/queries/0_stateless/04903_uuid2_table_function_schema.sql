20d38d666ba3e643c8e5ba28e5ada735228eb914
Alexey Milovidov <milovidov@clickhouse.com>
2026-08-15 21:54:49 +0000

    Fix UUID2 table-function schema test portability

--- tests/queries/0_stateless/04903_uuid2_table_function_schema.sql
+++ tests/queries/0_stateless/04903_uuid2_table_function_schema.sql
@@ -1,6 +1,6 @@
 DROP VIEW IF EXISTS uuid2_table_function_schema_file;
 DROP VIEW IF EXISTS uuid2_table_function_schema_file_cluster;
-DROP VIEW IF EXISTS uuid2_table_function_schema_named;
+DROP VIEW IF EXISTS uuid2_table_function_schema_format;
 
 SET uuid_type_version = 2;
 
@@ -8,8 +8,8 @@ CREATE VIEW uuid2_table_function_schema_file AS
     SELECT * FROM file('uuid2_table_function_schema.csv', 'CSV', 'id UUID');
 CREATE VIEW uuid2_table_function_schema_file_cluster AS
     SELECT * FROM fileCluster('test_shard_localhost', 'uuid2_table_function_schema.csv', 'CSV', 'id UUID');
-CREATE VIEW uuid2_table_function_schema_named AS
-    SELECT * FROM s3('https://example.com/uuid2_table_function_schema.csv', format = 'CSV', structure = 'id UUID');
+CREATE VIEW uuid2_table_function_schema_format AS
+    SELECT * FROM format('CSV', 'id UUID', '00000000-0000-0000-0000-000000000000');
 
 SELECT name, position(as_select, 'UUID2') > 0
 FROM system.tables
@@ -18,4 +18,4 @@ ORDER BY name;
 
 DROP VIEW uuid2_table_function_schema_file;
 DROP VIEW uuid2_table_function_schema_file_cluster;
-DROP VIEW uuid2_table_function_schema_named;
+DROP VIEW uuid2_table_function_schema_format;
