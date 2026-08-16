3e48cb8d69a7a6818400e733bf362f70db2f78d0
Alexey Milovidov <milovidov@clickhouse.com>
2026-08-16 12:32:08 +0000

    Fix vector search row policy regression test

--- tests/queries/0_stateless/04907_vector_search_no_rescoring_row_policy.sql
+++ tests/queries/0_stateless/04907_vector_search_no_rescoring_row_policy.sql
@@ -14,7 +14,7 @@ CREATE TABLE tab_vec_row_policy
     vec Array(Float32),
     INDEX idx vec TYPE vector_similarity('hnsw', 'L2Distance', 2) GRANULARITY 100000000
 )
-ENGINE = MergeTree ORDER BY id SETTINGS index_granularity = 2;
+ENGINE = ReplacingMergeTree ORDER BY id SETTINGS index_granularity = 2;
 
 INSERT INTO tab_vec_row_policy VALUES (0, 1, [1.0, 0.0]), (1, 1, [1.1, 0.0]), (2, 1, [1.2, 0.0]), (3, 1, [1.3, 0.0]), (4, 1, [1.4, 0.0]), (5, 1, [0.0, 2.0]), (6, 1, [0.0, 2.1]), (7, 1, [0.0, 2.2]), (8, 1, [0.0, 2.3]), (9, 1, [0.0, 2.4]);
 
