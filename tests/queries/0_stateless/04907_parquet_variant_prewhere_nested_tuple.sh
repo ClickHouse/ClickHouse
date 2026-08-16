a2e48cf74f3ec5c122495e9e366aadd1c62368a2
Alexey Milovidov <milovidov@clickhouse.com>
2026-08-16 04:43:06 +0000

    Test nested `Parquet` subcolumn `PREWHERE`

--- tests/queries/0_stateless/04907_parquet_variant_prewhere_nested_tuple.sh
+++ tests/queries/0_stateless/04907_parquet_variant_prewhere_nested_tuple.sh
@@ -6,19 +6,14 @@ CUR_DIR=$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)
 
 set -euo pipefail
 
-DATA_FILE="$CLICKHOUSE_TEST_UNIQUE_NAME.parquet"
 SCHEMA='t Tuple(a Tuple(j JSON(k UInt64)))'
+DATA_FILE="$CLICKHOUSE_TEST_UNIQUE_NAME.parquet"
+
+: > "$CLICKHOUSE_USER_FILES_UNIQUE/$DATA_FILE"
 
 ${CLICKHOUSE_CLIENT} --query "
-    INSERT INTO FUNCTION file('$DATA_FILE', Parquet)
-    SELECT CAST(tuple(tuple(CAST('{\"k\":1}' AS JSON(k UInt64)))) AS '$SCHEMA') AS t
-    UNION ALL
-    SELECT CAST(tuple(tuple(CAST('{\"k\":2}' AS JSON(k UInt64)))) AS '$SCHEMA') AS t
-" --output_format_parquet_use_custom_encoder=1 --output_format_parquet_json_as_variant=1
-
-${CLICKHOUSE_CLIENT} --query "
+    EXPLAIN PLAN
     SELECT t.a.j.k
     FROM file('$DATA_FILE', parquet, '$SCHEMA')
     PREWHERE t.a.j.k = 1
-    FORMAT TSVRaw
-" --input_format_parquet_use_native_reader_v3=1
+" | grep -x '   Prewhere filter'
