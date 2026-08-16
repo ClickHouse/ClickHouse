a09efb7487ff09eb9afbd9bcce59dca3d3668fb5
Alexey Milovidov <milovidov@clickhouse.com>
2026-08-16 13:15:10 +0000

    Fix accurate casts to scale-zero `Time64` types
    CI: https://github.com/ClickHouse/ClickHouse/pull/106019

--- src/Functions/FunctionsConversion.h
+++ src/Functions/FunctionsConversion.h
@@ -4175,15 +4175,8 @@ public:
                 if (arguments.size() > 1)
                     scale = extractToDecimalScale(arguments[1]);
 
-                if (scale == 0)
-                {
-                    result_column = executeInternal<DataTypeDateTime>(arguments, result_type, input_rows_count, 0);
-                }
-                else
-                {
-                    result_column
-                        = executeInternal<DataTypeDateTime64>(arguments, result_type, input_rows_count, static_cast<UInt32>(scale));
-                }
+                result_column
+                    = executeInternal<DataTypeDateTime64>(arguments, result_type, input_rows_count, static_cast<UInt32>(scale));
             }
             else
             {
@@ -4198,15 +4191,8 @@ public:
                 if (arguments.size() > 1)
                     scale = extractToDecimalScale(arguments[1]);
 
-                if (scale == 0)
-                {
-                    result_column = executeInternal<DataTypeTime>(arguments, result_type, input_rows_count, 0);
-                }
-                else
-                {
-                    result_column
-                        = executeInternal<DataTypeTime64>(arguments, result_type, input_rows_count, static_cast<UInt32>(scale));
-                }
+                result_column
+                    = executeInternal<DataTypeTime64>(arguments, result_type, input_rows_count, static_cast<UInt32>(scale));
             }
             else
             {
