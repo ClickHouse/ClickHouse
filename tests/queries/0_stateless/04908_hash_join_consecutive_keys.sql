7854e1817959c0dca188e17889fbb72b025c962c
Minh Vu <vuhoangminh97@gmail.com>
2026-08-16 23:35:17 +0200

    Expand HashJoin consecutive-key coverage

--- tests/queries/0_stateless/04908_hash_join_consecutive_keys.sql
+++ tests/queries/0_stateless/04908_hash_join_consecutive_keys.sql
@@ -48,3 +48,32 @@ ALL FULL JOIN
     FROM numbers(8)
 ) AS r ON l.k = r.k
 SETTINGS join_algorithm = 'hash', query_plan_join_swap_table = 0, max_threads = 1;
+
+SELECT
+    'packed-full',
+    count(),
+    sum(ifNull(l.v, 0)),
+    sum(ifNull(r.v, 0))
+FROM
+(
+    WITH toUInt64(intDiv(number, 2)) AS g
+    SELECT
+        g AS k1,
+        g * 3 + 1 AS k2,
+        g * 5 + 2 AS k3,
+        g * 7 + 3 AS k4,
+        number AS v
+    FROM numbers(10)
+) AS l
+ALL FULL JOIN
+(
+    WITH toUInt64(if(number < 6, intDiv(number, 2), 5)) AS g
+    SELECT
+        g AS k1,
+        g * 3 + 1 AS k2,
+        g * 5 + 2 AS k3,
+        g * 7 + 3 AS k4,
+        number + 10 AS v
+    FROM numbers(8)
+) AS r ON l.k1 = r.k1 AND l.k2 = r.k2 AND l.k3 = r.k3 AND l.k4 = r.k4
+SETTINGS join_algorithm = 'hash', query_plan_join_swap_table = 0, max_threads = 1;
