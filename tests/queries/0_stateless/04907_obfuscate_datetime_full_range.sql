055ddb9361d753d9673c055123783bd8a146d718
Alexey Milovidov <milovidov@clickhouse.com>
2026-08-16 09:48:19 +0000

    Stabilize `obfuscate` DateTime range test

--- tests/queries/0_stateless/04907_obfuscate_datetime_full_range.sql
+++ tests/queries/0_stateless/04907_obfuscate_datetime_full_range.sql
@@ -1,6 +1,6 @@
 -- `DateTime` uses the full `UInt32` timestamp range. Its continuity deltas therefore need
 -- `Int64`: timestamps after the 2038 boundary must not turn into negative `Int32` values.
-SELECT toString(x)
+SELECT toDate(x)
 FROM obfuscate(
     SELECT toDateTime('2038-01-19 03:14:08', 'UTC') + toIntervalSecond(number * 20650752) AS x
     FROM numbers(1))
