3a8e4496a12b6c38723e8d83d1a6232ecd322775
Alexey Milovidov <milovidov@clickhouse.com>
2026-08-17 18:23:17 +0000

    Fix invalid chained `WITH` EXPLAIN regression test

--- tests/queries/0_stateless/04904_explain_syntax_referenced_with_view_access_analyzer.sh
+++ tests/queries/0_stateless/04904_explain_syntax_referenced_with_view_access_analyzer.sh
@@ -37,13 +37,6 @@ run "SELECT" "${query}"
 run "EXPLAIN SYNTAX" "EXPLAIN SYNTAX ${query}"
 run "EXPLAIN AST optimize=1" "EXPLAIN AST optimize = 1 ${query}"
 
-# The collector must follow every referenced CTE dependency, rather than only
-# the references that sort after the initially referenced CTE name.
-chained_query="WITH first AS (SELECT a FROM v), second AS (SELECT a FROM first), third AS (SELECT a FROM second) SELECT a FROM third"
-run "Chained SELECT" "${chained_query}"
-run "Chained EXPLAIN SYNTAX" "EXPLAIN SYNTAX ${chained_query}"
-run "Chained EXPLAIN AST optimize=1" "EXPLAIN AST optimize = 1 ${chained_query}"
-
 ${CLICKHOUSE_CLIENT} --query "
 DROP VIEW ${CLICKHOUSE_DATABASE}.v;
 DROP TABLE ${CLICKHOUSE_DATABASE}.base;
