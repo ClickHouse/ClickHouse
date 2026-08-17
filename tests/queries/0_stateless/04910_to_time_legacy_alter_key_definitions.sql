b1800f4b6b5baf94daefea2732313c06e6d6f29d
Alexey Milovidov <milovidov@clickhouse.com>
2026-08-17 05:47:54 +0000

    Fix `toTime` ALTER test sampling key

--- tests/queries/0_stateless/04910_to_time_legacy_alter_key_definitions.sql
+++ tests/queries/0_stateless/04910_to_time_legacy_alter_key_definitions.sql
@@ -6,13 +6,13 @@ SET use_legacy_to_time = 1;
 DROP TABLE IF EXISTS t_totime_alter_key;
 
 CREATE TABLE t_totime_alter_key (c0 DateTime, c1 UInt32, v UInt32)
-ENGINE = MergeTree() ORDER BY (toTime(c0), c1);
+ENGINE = MergeTree() ORDER BY (toUInt32(toTime(c0)), c1) SAMPLE BY toUInt32(toTime(c0));
 
-ALTER TABLE t_totime_alter_key MODIFY ORDER BY (toTime(c0), c1);
+ALTER TABLE t_totime_alter_key MODIFY ORDER BY (toUInt32(toTime(c0)), c1);
 SELECT sorting_key FROM system.tables WHERE database = currentDatabase() AND name = 't_totime_alter_key';
 
-ALTER TABLE t_totime_alter_key MODIFY TTL c0 + INTERVAL 1 DAY GROUP BY toTime(c0), c1 SET v = max(v);
-SELECT extract(create_table_query, 'GROUP BY toTime[A-Za-z]*')
+ALTER TABLE t_totime_alter_key MODIFY TTL c0 + INTERVAL 1 DAY GROUP BY toUInt32(toTime(c0)), c1 SET v = max(v);
+SELECT extract(create_table_query, 'GROUP BY toUInt32\\(toTime[A-Za-z]*')
 FROM system.tables WHERE database = currentDatabase() AND name = 't_totime_alter_key';
 
 ALTER TABLE t_totime_alter_key MODIFY SAMPLE BY toUInt32(toTime(c0));
@@ -23,7 +23,7 @@ DROP TABLE t_totime_alter_key;
 CREATE FUNCTION f_totime_alter_key AS (x) -> toTime(x);
 
 CREATE TABLE t_totime_alter_key_udf (c0 DateTime, c1 UInt32)
-ENGINE = MergeTree() ORDER BY (toUInt32(f_totime_alter_key(c0)), c1);
+ENGINE = MergeTree() ORDER BY (toUInt32(f_totime_alter_key(c0)), c1) SAMPLE BY toUInt32(f_totime_alter_key(c0));
 
 ALTER TABLE t_totime_alter_key_udf MODIFY ORDER BY (toUInt32(f_totime_alter_key(c0)), c1);
 SELECT sorting_key FROM system.tables WHERE database = currentDatabase() AND name = 't_totime_alter_key_udf';
