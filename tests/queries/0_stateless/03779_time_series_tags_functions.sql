b87d67ec9900f87f5f2084d0a3a13443cb5c88c8
Minh Vu <vuhoangminh97@gmail.com>
2026-08-18 00:48:21 +0200

    Strengthen vectorized tag transform coverage

--- tests/queries/0_stateless/03779_time_series_tags_functions.sql
+++ tests/queries/0_stateless/03779_time_series_tags_functions.sql
@@ -461,7 +461,8 @@ WITH
     ) AS src_groups
 SELECT count(),
        uniqExact(new_group),
-       arraySort(groupUniqArray(timeSeriesGroupToTags(new_group)))
+       arraySort(groupUniqArray(timeSeriesGroupToTags(new_group))),
+       groupArray(timeSeriesGroupToTags(new_group))
 FROM
 (
     SELECT timeSeriesCopyTag(dest_group, src_group, 'src') AS new_group
@@ -499,7 +500,8 @@ WITH
     ) AS src_groups
 SELECT count(),
        uniqExact(new_group),
-       arraySort(groupUniqArray(timeSeriesGroupToTags(new_group)))
+       arraySort(groupUniqArray(timeSeriesGroupToTags(new_group))),
+       groupArray(timeSeriesGroupToTags(new_group))
 FROM
 (
     SELECT timeSeriesCopyTags(dest_group, src_group, ['src']) AS new_group
