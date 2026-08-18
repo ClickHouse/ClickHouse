ClickHouse/ClickHouse

commit 6146766465486724c1baace82c1bdf786d7ffbd6

Use function ParserList::parseUtil() to parse lists more accurately.

src/Parsers/ParserCreateQuotaQuery.cpp
@@ -8,6 +8,7 @@
 #include <Parsers/ASTLiteral.h>
 #include <Parsers/ASTIdentifier.h>
 #include <Parsers/ASTRolesOrUsersSet.h>
+#include <Parsers/ExpressionListParsers.h>
 #include <ext/range.h>
 #include <boost/algorithm/string/predicate.hpp>
 
@@ -126,17 +127,17 @@ namespace
     }
 
 
-    bool parseLimit(IParserBase::Pos & pos, Expected & expected, bool first, bool & max_prefix_encountered, ResourceType & resource_type, ResourceAmount & max)
+    bool parseLimits(IParserBase::Pos & pos, Expected & expected, std::vector<std::pair<ResourceType, ResourceAmount>> & limits)
     {
-        return IParserBase::wrapParseImpl(pos, [&]
-        {
-            if (!first && !ParserToken{TokenType::Comma}.ignore(pos, expected))
-                return false;
+        std::vector<std::pair<ResourceType, ResourceAmount>> res_limits;
+        bool max_prefix_encountered = false;
 
+        auto parse_limit = [&]
+        {
             max_prefix_encountered |= ParserKeyword{"MAX"}.ignore(pos, expected);
 
-            ResourceType res_resource_type;
-            if (!parseResourceType(pos, expected, res_resource_type))
+            ResourceType resource_type;
+            if (!parseResourceType(pos, expected, resource_type))
                 return false;
 
             if (max_prefix_encountered)
@@ -149,25 +150,32 @@ namespace
                     return false;
             }
 
-            ResourceAmount res_max;
-            if (!parseMaxAmount(pos, expected, res_resource_type, res_max))
+            ResourceAmount max;
+            if (!parseMaxAmount(pos, expected, resource_type, max))
                 return false;
 
-            resource_type = res_resource_type;
-            max = res_max;
+            res_limits.emplace_back(resource_type, max);
             return true;
-        });
+        };
+
+        if (!ParserList::parseUtil(pos, expected, parse_limit, false))
+            return false;
+
+        limits = std::move(res_limits);
+        return true;
     }
 
-    bool parseIntervalWithLimits(IParserBase::Pos & pos, Expected & expected, ASTCreateQuotaQuery::Limits & limits)
+    bool parseIntervalsWithLimits(IParserBase::Pos & pos, Expected & expected, std::vector<ASTCreateQuotaQuery::Limits> & all_limits)
     {
-        return IParserBase::wrapParseImpl(pos, [&]
+        std::vector<ASTCreateQuotaQuery::Limits> res_all_limits;
+
+        auto parse_interval_with_limits = [&]
         {
-            ASTCreateQuotaQuery::Limits new_limits;
             if (!ParserKeyword{"FOR"}.ignore(pos, expected))
                 return false;
 
-            new_limits.randomize_interval = ParserKeyword{"RANDOMIZED"}.ignore(pos, expected);
+            ASTCreateQuotaQuery::Limits limits;
+            limits.randomize_interval = ParserKeyword{"RANDOMIZED"}.ignore(pos, expected);
 
             ParserKeyword{"INTERVAL"}.ignore(pos, expected);
 
@@ -181,53 +189,34 @@ namespace
             if (!parseIntervalKind(pos, expected, interval_kind))
                 return false;
 
-            new_limits.duration = std::chrono::seconds(static_cast<UInt64>(num_intervals * interval_kind.toAvgSeconds()));
+            limits.duration = std::chrono::seconds(static_cast<UInt64>(num_intervals * interval_kind.toAvgSeconds()));
+            std::vector<std::pair<ResourceType, ResourceAmount>> maxs;
 
             if (ParserKeyword{"NO LIMITS"}.ignore(pos, expected))
             {
-                new_limits.drop = true;
+                limits.drop = true;
             }
             else if (ParserKeyword{"TRACKING ONLY"}.ignore(pos, expected))
             {
             }
+            else if (parseLimits(pos, expected, maxs))
+            {
+                for (const auto & [resource_type, max] : maxs)
+                    limits.max[resource_type] = max;
+            }
             else
-            {
-                ResourceType resource_type;
-                ResourceAmount max;
-                bool max_prefix_encountered = false;
-                if (!parseLimit(pos, expected, true, max_prefix_encountered, resource_type, max))
-                    return false;
+                return false;
 
-                new_limits.max[resource_type] = max;
-                while (parseLimit(pos, expected, false, max_prefix_encountered, resource_type, max))
-                    new_limits.max[resource_type] = max;
-            }
-
-            limits = new_limits;
+            res_all_limits.emplace_back(std::move(limits));
             return true;
-        });
-    }
+        };
 
-    bool parseIntervalsWithLimits(IParserBase::Pos & pos, Expected & expected, std::vector<ASTCreateQuotaQuery::Limits> & all_limits)
-    {
-        return IParserBase::wrapParseImpl(pos, [&]
-        {
-            size_t old_size = all_limits.size();
-            do
-            {
-                ASTCreateQuotaQuery::Limits limits;
-                if (!parseIntervalWithLimits(pos, expected, limits))
-                {
-                    all_limits.resize(old_size);
-                    return false;
-                }
-                all_limits.push_back(limits);
-            }
-            while (ParserToken{TokenType::Comma}.ignore(pos, expected));
-            return true;
-        });
-    }
+        if (!ParserList::parseUtil(pos, expected, parse_interval_with_limits, false))
+            return false;
 
+        all_limits = std::move(res_all_limits);
+        return true;
+    }
 
     bool parseToRoles(IParserBase::Pos & pos, Expected & expected, bool id_mode, std::shared_ptr<ASTRolesOrUsersSet> & roles)
     {

src/Parsers/ExpressionListParsers.cpp
@@ -76,41 +76,25 @@ const char * ParserTupleElementExpression::operators[] =
 
 bool ParserList::parseImpl(Pos & pos, ASTPtr & node, Expected & expected)
 {
-    bool first = true;
+    ASTs elements;
+
+    auto parse_element = [&]
+    {
+        ASTPtr element;
+        if (!elem_parser->parse(pos, element, expected))
+            return false;
+
+        elements.push_back(element);
+        return true;
+    };
+
+    if (!parseUtil(pos, expected, parse_element, *separator_parser, allow_empty))
+        return false;
 
     auto list = std::make_shared<ASTExpressionList>(result_separator);
+    list->children = std::move(elements);
     node = list;
-
-    while (true)
-    {
-        if (first)
-        {
-            ASTPtr elem;
-            if (!elem_parser->parse(pos, elem, expected))
-                break;
-
-            list->children.push_back(elem);
-            first = false;
-        }
-        else
-        {
-            auto prev_pos = pos;
-
-            if (!separator_parser->ignore(pos, expected))
-                break;
-
-            ASTPtr elem;
-            if (!elem_parser->parse(pos, elem, expected))
-            {
-                pos = prev_pos;
-                break;
-            }
-
-            list->children.push_back(elem);
-        }
-    }
-
-    return allow_empty || !first;
+    return true;
 }
 
 

src/Parsers/ExpressionListParsers.h
@@ -26,6 +26,43 @@ public:
         , result_separator(result_separator_)
     {
     }
+
+    template <typename F>
+    static bool parseUtil(Pos & pos, Expected & expected, const F & parse_element, IParser & separator_parser_, bool allow_empty_ = true)
+    {
+        Pos begin = pos;
+        if (!parse_element())
+        {
+            pos = begin;
+            return allow_empty_;
+        }
+
+        while (true)
+        {
+            begin = pos;
+            if (!separator_parser_.ignore(pos, expected) || !parse_element())
+            {
+                pos = begin;
+                return true;
+            }
+        }
+
+        return false;
+    }
+
+    template <typename F>
+    static bool parseUtil(Pos & pos, Expected & expected, const F & parse_element, TokenType separator, bool allow_empty_ = true)
+    {
+        ParserToken sep_parser{separator};
+        return parseUtil(pos, expected, parse_element, sep_parser, allow_empty_);
+    }
+
+    template <typename F>
+    static bool parseUtil(Pos & pos, Expected & expected, const F & parse_element, bool allow_empty_ = true)
+    {
+        return parseUtil(pos, expected, parse_element, TokenType::Comma, allow_empty_);
+    }
+
 protected:
     const char * getName() const override { return "list of elements"; }
     bool parseImpl(Pos & pos, ASTPtr & node, Expected & expected) override;

src/Parsers/ParserCreateRoleQuery.cpp
@@ -6,6 +6,7 @@
 #include <Parsers/ASTSettingsProfileElement.h>
 #include <Parsers/ParserSettingsProfileElement.h>
 #include <Parsers/parseUserName.h>
+#include <boost/range/algorithm_ext/push_back.hpp>
 
 
 namespace DB
@@ -23,7 +24,7 @@ namespace
         });
     }
 
-    bool parseSettings(IParserBase::Pos & pos, Expected & expected, bool id_mode, std::shared_ptr<ASTSettingsProfileElements> & settings)
+    bool parseSettings(IParserBase::Pos & pos, Expected & expected, bool id_mode, std::vector<std::shared_ptr<ASTSettingsProfileElement>> & settings)
     {
         return IParserBase::wrapParseImpl(pos, [&]
         {
@@ -36,10 +37,7 @@ namespace
             if (!elements_p.parse(pos, new_settings_ast, expected))
                 return false;
 
-            if (!settings)
-                settings = std::make_shared<ASTSettingsProfileElements>();
-            const auto & new_settings = new_settings_ast->as<const ASTSettingsProfileElements &>();
-            settings->elements.insert(settings->elements.end(), new_settings.elements.begin(), new_settings.elements.end());
+            settings = std::move(new_settings_ast->as<const ASTSettingsProfileElements &>().elements);
             return true;
         });
     }
@@ -99,8 +97,14 @@ bool ParserCreateRoleQuery::parseImpl(Pos & pos, ASTPtr & node, Expected & expec
         if (alter && new_name.empty() && (names.size() == 1) && parseRenameTo(pos, expected, new_name))
             continue;
 
-        if (parseSettings(pos, expected, attach_mode, settings))
+        std::vector<std::shared_ptr<ASTSettingsProfileElement>> new_settings;
+        if (parseSettings(pos, expected, attach_mode, new_settings))
+        {
+            if (!settings)
+                settings = std::make_shared<ASTSettingsProfileElements>();
+            boost::range::push_back(settings->elements, std::move(new_settings));
             continue;
+        }
 
         if (cluster.empty() && parseOnCluster(pos, expected, cluster))
             continue;

src/Parsers/ParserCreateRowPolicyQuery.cpp
@@ -75,6 +75,7 @@ namespace
         });
     }
 
+
     void addAllCommands(boost::container::flat_set<std::string_view> & commands)
     {
         for (auto condition_type : ext::range(MAX_CONDITION_TYPE))
@@ -84,44 +85,47 @@ namespace
         }
     }
 
-    bool parseCommands(IParserBase::Pos & pos, Expected & expected, boost::container::flat_set<std::string_view> & commands)
+
+    bool parseCommands(IParserBase::Pos & pos, Expected & expected,
+                       boost::container::flat_set<std::string_view> & commands)
     {
-        return IParserBase::wrapParseImpl(pos, [&]
+        boost::container::flat_set<std::string_view> res_commands;
+
+        auto parse_command = [&]
         {
             if (ParserKeyword{"ALL"}.ignore(pos, expected))
             {
-                addAllCommands(commands);
+                addAllCommands(res_commands);
                 return true;
             }
 
-            boost::container::flat_set<std::string_view> res_commands;
-            do
+            for (auto condition_type : ext::range(MAX_CONDITION_TYPE))
             {
-                bool found_keyword = false;
-                for (auto condition_type : ext::range(MAX_CONDITION_TYPE))
+                const std::string_view & command = ConditionTypeInfo::get(condition_type).command;
+                if (ParserKeyword{command.data()}.ignore(pos, expected))
                 {
-                    const std::string_view & command = ConditionTypeInfo::get(condition_type).command;
-                    if (ParserKeyword{command.data()}.ignore(pos, expected))
-                    {
-                        res_commands.emplace(command);
-                        found_keyword = true;
-                        break;
-                    }
+                    res_commands.emplace(command);
+                    return true;
                 }
-
-                if (!found_keyword)
-                    return false;
             }
-            while (ParserToken{TokenType::Comma}.ignore(pos, expected));
 
-            commands = std::move(res_commands);
-            return true;
-        });
+            return false;
+        };
+
+        if (!ParserList::parseUtil(pos, expected, parse_command, false))
+            return false;
+
+        commands = std::move(res_commands);
+        return true;
     }
 
-    bool parseForClause(IParserBase::Pos & pos, Expected & expected, bool alter, std::vector<std::pair<ConditionType, ASTPtr>> & conditions)
+
+    bool
+    parseForClauses(IParserBase::Pos & pos, Expected & expected, bool alter, std::vector<std::pair<ConditionType, ASTPtr>> & conditions)
     {
-        return IParserBase::wrapParseImpl(pos, [&]
+        std::vector<std::pair<ConditionType, ASTPtr>> res_conditions;
+
+        auto parse_for_clause = [&]
         {
             boost::container::flat_set<std::string_view> commands;
 
@@ -158,32 +162,20 @@ namespace
                 if (commands.count(type_info.command))
                 {
                     if (type_info.is_check && check)
-                        conditions.emplace_back(condition_type, *check);
+                        res_conditions.emplace_back(condition_type, *check);
                     else if (filter)
-                        conditions.emplace_back(condition_type, *filter);
+                        res_conditions.emplace_back(condition_type, *filter);
                 }
             }
 
             return true;
-        });
-    }
+        };
 
-    bool parseForClauses(
-        IParserBase::Pos & pos, Expected & expected, bool alter, std::vector<std::pair<ConditionType, ASTPtr>> & conditions)
-    {
-        return IParserBase::wrapParseImpl(pos, [&]
-        {
-            std::vector<std::pair<ConditionType, ASTPtr>> res_conditions;
-            do
-            {
-                if (!parseForClause(pos, expected, alter, res_conditions))
-                    return false;
-            }
-            while (ParserToken{TokenType::Comma}.ignore(pos, expected));
+        if (!ParserList::parseUtil(pos, expected, parse_for_clause, false))
+            return false;
 
-            conditions = std::move(res_conditions);
-            return true;
-        });
+        conditions = std::move(res_conditions);
+        return true;
     }
 
     bool parseToRoles(IParserBase::Pos & pos, Expected & expected, bool id_mode, std::shared_ptr<ASTRolesOrUsersSet> & roles)

src/Parsers/ParserCreateSettingsProfileQuery.cpp
@@ -8,6 +8,7 @@
 #include <Parsers/ParserRolesOrUsersSet.h>
 #include <Parsers/ASTRolesOrUsersSet.h>
 #include <Parsers/parseIdentifierOrStringLiteral.h>
+#include <boost/range/algorithm_ext/push_back.hpp>
 
 
 namespace DB
@@ -25,7 +26,7 @@ namespace
         });
     }
 
-    bool parseSettings(IParserBase::Pos & pos, Expected & expected, bool id_mode, std::shared_ptr<ASTSettingsProfileElements> & settings)
+    bool parseSettings(IParserBase::Pos & pos, Expected & expected, bool id_mode, std::vector<std::shared_ptr<ASTSettingsProfileElement>> & settings)
     {
         return IParserBase::wrapParseImpl(pos, [&]
         {
@@ -38,10 +39,7 @@ namespace
             if (!elements_p.parse(pos, new_settings_ast, expected))
                 return false;
 
-            if (!settings)
-                settings = std::make_shared<ASTSettingsProfileElements>();
-            const auto & new_settings = new_settings_ast->as<const ASTSettingsProfileElements &>();
-            settings->elements.insert(settings->elements.end(), new_settings.elements.begin(), new_settings.elements.end());
+            settings = std::move(new_settings_ast->as<const ASTSettingsProfileElements &>().elements);
             return true;
         });
     }
@@ -51,7 +49,7 @@ namespace
         return IParserBase::wrapParseImpl(pos, [&]
         {
             ASTPtr ast;
-            if (roles || !ParserKeyword{"TO"}.ignore(pos, expected))
+            if (!ParserKeyword{"TO"}.ignore(pos, expected))
                 return false;
 
             ParserRolesOrUsersSet roles_p;
@@ -119,8 +117,14 @@ bool ParserCreateSettingsProfileQuery::parseImpl(Pos & pos, ASTPtr & node, Expec
         if (alter && new_name.empty() && (names.size() == 1) && parseRenameTo(pos, expected, new_name))
             continue;
 
-        if (parseSettings(pos, expected, attach_mode, settings))
+        std::vector<std::shared_ptr<ASTSettingsProfileElement>> new_settings;
+        if (parseSettings(pos, expected, attach_mode, new_settings))
+        {
+            if (!settings)
+                settings = std::make_shared<ASTSettingsProfileElements>();
+            boost::range::push_back(settings->elements, std::move(new_settings));
             continue;
+        }
 
         if (cluster.empty() && parseOnCluster(pos, expected, cluster))
             continue;

commit eab4d203fa6efce9e573d996102e98da76ba320c

Fix silently dropped quota intervals when the separating comma is missing
CREATE QUOTA q FOR INTERVAL 1 MINUTE MAX queries = 1 FOR INTERVAL 1 HOUR
MAX queries = 100 TO u succeeded and created a quota holding only the
1 hour limit. The 1 minute rate limit the operator wrote, and got no error
for, was never installed and never enforced; only SHOW CREATE QUOTA
revealed the loss.

parseIntervalsWithLimits parses a comma-separated list of FOR INTERVAL
clauses into a local vector and then assigned it over the caller's vector.
Its call site in parseImpl's option loop is deliberately unguarded, so a
comma-less continuation is not consumed by the inner ParserList, the option
loop matches a second time, and the assignment discards everything parsed
earlier. No layer raises an error.

That assignment is a regression from 6146766465486724c1baace82c1bdf786d7ffbd6
(2020-06-06), which replaced an append with an assign while leaving the loop
unguarded. Before it the helper appended onto the caller's vector and rolled
back only its own additions on failure, so accumulation across option-loop
iterations was deliberate and the comma-less spelling parsed correctly.

Restore the append with insertAtEnd, which is what the sibling
ParserCreateRowPolicyQuery already does for the same situation. This covers
CREATE, ALTER and ATTACH QUOTA, which share parseImpl.

insertAtEnd onto an empty vector swaps, so the single-block and
comma-separated forms parse identically to before. Only input that silently
lost data now keeps it, so this is not a backward incompatible change.
Persisted, replicated and backed-up definitions are unaffected because they
are formatter-produced and the formatter always emits the separating comma.

Closes #115282.

src/Parsers/Access/ParserCreateQuotaQuery.cpp
@@ -12,6 +12,7 @@
 #include <Parsers/ExpressionListParsers.h>
 #include <Parsers/parseIdentifierOrStringLiteral.h>
 #include <Parsers/parseIntervalKind.h>
+#include <base/insertAtEnd.h>
 #include <base/range.h>
 #include <Common/FieldVisitorConvertToNumber.h>
 
@@ -263,7 +264,7 @@ namespace
         if (!ParserList::parseUtil(pos, expected, parse_interval_with_limits, false))
             return false;
 
-        all_limits = std::move(res_all_limits);
+        insertAtEnd(all_limits, std::move(res_all_limits));
         return true;
     }
 

tests/queries/0_stateless/04926_quota_interval_missing_comma.reference
@@ -1,0 +1,13 @@
+CREATE QUOTA q FOR INTERVAL 1 minute MAX queries = 1, FOR INTERVAL 1 hour MAX queries = 100 TO u
+1
+CREATE QUOTA q KEYED BY user_name FOR INTERVAL 1 minute MAX queries = 1, FOR INTERVAL 1 hour MAX queries = 100
+CREATE QUOTA q FOR INTERVAL 1 minute MAX queries = 1, FOR INTERVAL 1 hour MAX queries = 100, FOR INTERVAL 1 day MAX queries = 1000
+CREATE QUOTA q FOR INTERVAL 1 minute TRACKING ONLY, FOR INTERVAL 1 hour MAX queries = 5
+CREATE QUOTA q FOR INTERVAL 1 minute NO LIMITS, FOR INTERVAL 1 hour MAX queries = 5
+CREATE QUOTA q FOR RANDOMIZED INTERVAL 1 minute MAX queries = 1, FOR INTERVAL 1 hour MAX queries = 5
+ALTER QUOTA q FOR INTERVAL 1 minute MAX queries = 7, FOR INTERVAL 1 hour MAX queries = 8
+CREATE QUOTA q FOR INTERVAL 1 minute MAX queries = 1, FOR INTERVAL 1 minute MAX errors = 5
+CREATE QUOTA q FOR INTERVAL 1 minute MAX queries = 1
+CREATE QUOTA q KEYED BY user_name FOR INTERVAL 1 minute MAX queries = 1, FOR INTERVAL 1 hour MAX queries = 2, FOR INTERVAL 1 day MAX queries = 3
+CREATE QUOTA q FOR INTERVAL 1 minute NO LIMITS, FOR INTERVAL 1 hour TRACKING ONLY, FOR RANDOMIZED INTERVAL 1 day MAX queries = 3
+ALTER QUOTA q FOR INTERVAL 1 minute MAX queries = 7, FOR INTERVAL 1 hour MAX queries = 8

tests/queries/0_stateless/04926_quota_interval_missing_comma.sql
@@ -1,0 +1,18 @@
+SELECT formatQuery('CREATE QUOTA q FOR INTERVAL 1 MINUTE MAX queries = 1 FOR INTERVAL 1 HOUR MAX queries = 100 TO u');
+SELECT formatQuery('CREATE QUOTA q FOR INTERVAL 1 MINUTE MAX queries = 1 FOR INTERVAL 1 HOUR MAX queries = 100 TO u') = formatQuery('CREATE QUOTA q FOR INTERVAL 1 MINUTE MAX queries = 1, FOR INTERVAL 1 HOUR MAX queries = 100 TO u');
+
+SELECT formatQuery('CREATE QUOTA q FOR INTERVAL 1 MINUTE MAX queries = 1 KEYED BY user_name FOR INTERVAL 1 HOUR MAX queries = 100');
+SELECT formatQuery('CREATE QUOTA q FOR INTERVAL 1 MINUTE MAX queries = 1 FOR INTERVAL 1 HOUR MAX queries = 100 FOR INTERVAL 1 DAY MAX queries = 1000');
+SELECT formatQuery('CREATE QUOTA q FOR INTERVAL 1 MINUTE TRACKING ONLY FOR INTERVAL 1 HOUR MAX queries = 5');
+SELECT formatQuery('CREATE QUOTA q FOR INTERVAL 1 MINUTE NO LIMITS FOR INTERVAL 1 HOUR MAX queries = 5');
+SELECT formatQuery('CREATE QUOTA q FOR RANDOMIZED INTERVAL 1 MINUTE MAX queries = 1 FOR INTERVAL 1 HOUR MAX queries = 5');
+SELECT formatQuery('ALTER QUOTA q FOR INTERVAL 1 MINUTE MAX queries = 7 FOR INTERVAL 1 HOUR MAX queries = 8');
+SELECT formatQuery('CREATE QUOTA q FOR INTERVAL 1 MINUTE MAX queries = 1 FOR INTERVAL 1 MINUTE MAX errors = 5');
+
+SELECT formatQuery('CREATE QUOTA q FOR INTERVAL 1 MINUTE MAX queries = 1');
+SELECT formatQuery('CREATE QUOTA q KEYED BY user_name FOR INTERVAL 1 MINUTE MAX queries = 1, FOR INTERVAL 1 HOUR MAX queries = 2, FOR INTERVAL 1 DAY MAX queries = 3');
+SELECT formatQuery('CREATE QUOTA q FOR INTERVAL 1 MINUTE NO LIMITS, FOR INTERVAL 1 HOUR TRACKING ONLY, FOR RANDOMIZED INTERVAL 1 DAY MAX queries = 3');
+SELECT formatQuery('ALTER QUOTA q FOR INTERVAL 1 MINUTE MAX queries = 7, FOR INTERVAL 1 HOUR MAX queries = 8');
+
+SELECT formatQuery('CREATE QUOTA q FOR INTERVAL 1 MINUTE MAX queries = 1, TO u'); -- { serverError SYNTAX_ERROR }
+SELECT formatQuery('CREATE QUOTA q FOR INTERVAL 1 MINUTE MAX queries = 1 errors = 2'); -- { serverError SYNTAX_ERROR }