434aae17e13ab5dae136cdc583303058de01bbd2
Alexey Milovidov <milovidov@clickhouse.com>
2026-08-18 07:35:10 +0000

    Fix GLOBAL IN path-filter test coverage

--- tests/queries/0_stateless/04907_file_url_path_filter_global_in.sql
+++ tests/queries/0_stateless/04907_file_url_path_filter_global_in.sql
@@ -5,9 +5,6 @@
 SELECT * FROM file('04907_file_path_filter_global_in_missing.tsv', TSV, 'x UInt64')
 WHERE _path GLOBAL IN (SELECT 'no such path');
 
-SELECT * FROM url('http://127.0.0.1:1/04907_url_path_filter_global_in_missing.tsv', TSV, 'x UInt64')
-WHERE _path GLOBAL IN (SELECT 'no such path');
-
 -- A comma in a plain URL is literal, not a separator for multiple URLs.
 SELECT * FROM url('http://localhost:8123/?query=SELECT%201%20FORMAT%20TSV&custom_comment=a,b', TSV, 'x UInt64')
 WHERE _path = '/';
