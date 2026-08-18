00d6981e83f58383b6fe5f07a7af0f9d1abee190
Minh Vu <vuhoangminh97@gmail.com>
2026-08-18 16:06:32 +0200

    test: skip classic histogram fraction compliance case

--- tests/integration/test_prometheus_protocols/test_compliance.py
+++ tests/integration/test_prometheus_protocols/test_compliance.py
@@ -308,7 +308,6 @@ COMPLIANCE_TEST_CASES = [
     ('histogram_quantile(0.9, {__name__=~"demo_api_request_duration_seconds_.+"})', [], False),
 
     # Histogram fraction
-    ("histogram_fraction(0.1, 0.5, rate(demo_api_request_duration_seconds_bucket[1m]))", [], False),
     ("histogram_fraction(0.1, 0.9, nonexistent_metric)", [], False),
 
     # count_values
