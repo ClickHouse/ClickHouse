5d767cd691032fb59ccf918422d5926cb6ac5060
Kirill Shokhin <kirill.shokhin@clickhouse.com>
2026-08-18 16:07:01 +0000

    Do not allow pushing filter to the left side for `ANY JOIN` queries

--- tests/queries/0_stateless/04812_any_inner_join_filter_push_down.sql
+++ tests/queries/0_stateless/04812_any_inner_join_filter_push_down.sql
@@ -10,7 +10,7 @@ SET optimize_move_to_prewhere = 1, query_plan_optimize_prewhere = 1;
 
 CREATE TABLE t1 (k UInt64, a UInt64) ENGINE = MergeTree ORDER BY k;
 CREATE TABLE t2 (k UInt64, v UInt64) ENGINE = MergeTree ORDER BY k;
-INSERT INTO t1 VALUES (1, 100), (2, 200);
+INSERT INTO t1 VALUES (1, 100), (2, 200), (1, 200);
 INSERT INTO t2 VALUES (1, 10), (1, 20), (2, 30);
 
 SELECT '-- non-key filter must not be pushed to the right side';
@@ -19,10 +19,21 @@ SELECT explain FROM (
     SELECT * FROM t1 ANY INNER JOIN t2 ON t1.k = t2.k WHERE t2.v = 1
 ) WHERE explain ilike '%Filter column%' OR explain ilike '%Strictness%' OR explain ilike '%Type:%';
 
-SELECT '-- non-key filter may change the output set if pushed';
+SELECT '-- non-key filter must not be pushed to the left side';
+SELECT explain FROM (
+    EXPLAIN actions = 1
+    SELECT * FROM t1 ANY INNER JOIN t2 ON t1.k = t2.k WHERE t1.a = 1
+) WHERE explain ilike '%Filter column%' OR explain ilike '%Strictness%' OR explain ilike '%Type:%';
+
+SELECT '-- non-key filter may change the output set if pushed right';
 SELECT
     (SELECT count() FROM t1 ANY JOIN t2 USING (k) WHERE t2.v > 10)
     = (SELECT sum(t2.v > 10) FROM t1 ANY JOIN t2 USING (k));
+
+SELECT '-- non-key filter may change the output set if pushed left';
+SELECT
+    (SELECT count() FROM t1 ANY JOIN t2 USING (k) WHERE t1.a = 200)
+    = (SELECT sum(t1.a = 200) FROM t1 ANY JOIN t2 USING (k));
 
 SELECT '-- equi-key filter on the right column is still pushed to both sides';
 SELECT explain FROM (
@@ -36,8 +47,3 @@ SELECT explain FROM (
     SELECT * FROM t1 ANY INNER JOIN t2 ON t1.k = t2.k WHERE t1.k = 1
 ) WHERE explain ilike '%Filter column%' OR explain ilike '%Strictness%' OR explain ilike '%Type:%';
 
-SELECT '-- left-side filter is still pushed';
-SELECT explain FROM (
-    EXPLAIN actions = 1
-    SELECT * FROM t1 ANY INNER JOIN t2 ON t1.k = t2.k WHERE t1.a > 1
-) WHERE explain ilike '%Filter column%' OR explain ilike '%Strictness%' OR explain ilike '%Type:%';
