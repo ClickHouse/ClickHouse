428635392c1a671880eb342017e1fffacf2bd09c
Valery Petrov <valerypetrov@icloud.com>
2026-08-18 22:11:39 -0700

    Tag 04873_system_mutations_progress_recipe as no-replicated-database

--- tests/queries/0_stateless/04873_system_mutations_progress_recipe.sh
+++ tests/queries/0_stateless/04873_system_mutations_progress_recipe.sh
@@ -1,4 +1,6 @@
 #!/usr/bin/env bash
+# Tags: no-replicated-database
+# no-replicated-database: test uses Memory-engine mock tables and a local parameterized view
 
 CUR_DIR=$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)
 # shellcheck source=../shell_config.sh
