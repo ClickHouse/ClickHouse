4287bcdd25abf1ef43b1396a77eff66280d0c23c
Alexey Milovidov <milovidov@clickhouse.com>
2026-08-19 14:57:42 +0000

    Fix `max_table_size` patch-parts test rows

--- tests/queries/0_stateless/04911_max_table_size_patch_parts.sql
+++ tests/queries/0_stateless/04911_max_table_size_patch_parts.sql
@@ -6,9 +6,9 @@ CREATE TABLE t_max_size_patch_parts (x UInt64, y UInt64) ENGINE = MergeTree ORDE
 INSERT INTO t_max_size_patch_parts SELECT number, number FROM numbers(10);
 
 DELETE FROM t_max_size_patch_parts WHERE x = 0 SETTINGS lightweight_delete_mode = 'lightweight_update_force', lightweight_deletes_sync = 2;
-INSERT INTO t_max_size_patch_parts VALUES (10);
+INSERT INTO t_max_size_patch_parts VALUES (10, 10);
 SELECT count() FROM t_max_size_patch_parts;
-INSERT INTO t_max_size_patch_parts VALUES (11); -- { serverError TABLE_SIZE_LIMIT_EXCEEDED }
+INSERT INTO t_max_size_patch_parts VALUES (11, 11); -- { serverError TABLE_SIZE_LIMIT_EXCEEDED }
 
 DROP TABLE t_max_size_patch_parts;
 
@@ -16,8 +16,8 @@ CREATE TABLE t_max_size_patch_parts (x UInt64, y UInt64) ENGINE = MergeTree ORDE
 INSERT INTO t_max_size_patch_parts SELECT number, number FROM numbers(10);
 
 UPDATE t_max_size_patch_parts SET y = y + 100 WHERE x = 0 SETTINGS mutations_sync = 2;
-INSERT INTO t_max_size_patch_parts VALUES (10);
+INSERT INTO t_max_size_patch_parts VALUES (10, 10);
 SELECT count() FROM t_max_size_patch_parts;
-INSERT INTO t_max_size_patch_parts VALUES (11); -- { serverError TABLE_SIZE_LIMIT_EXCEEDED }
+INSERT INTO t_max_size_patch_parts VALUES (11, 11); -- { serverError TABLE_SIZE_LIMIT_EXCEEDED }
 
 DROP TABLE t_max_size_patch_parts;
