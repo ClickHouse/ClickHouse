1d962699d7345a3c660dae623f21a12afecfe29b
Alexey Milovidov <milovidov@clickhouse.com>
2026-08-19 02:32:00 +0000

    Fix `UNIQUE` scalar analysis contexts

--- tests/queries/0_stateless/04926_unique_predicate_parametric_function.sh
+++ tests/queries/0_stateless/04926_unique_predicate_parametric_function.sh
@@ -6,5 +6,5 @@ CUR_DIR=$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)
 
 ${CLICKHOUSE_CLIENT} --query "DROP VIEW IF EXISTS unique_predicate_parametric_function"
 ${CLICKHOUSE_CLIENT} --query "CREATE VIEW unique_predicate_parametric_function AS SELECT topK(UNIQUE((SELECT number FROM numbers(3))))(number) FROM numbers(10)"
-${CLICKHOUSE_CLIENT} --query "SELECT * FROM unique_predicate_parametric_function"
+${CLICKHOUSE_CLIENT} --query "SELECT * FROM unique_predicate_parametric_function FORMAT Null"
 ${CLICKHOUSE_CLIENT} --query "DROP VIEW unique_predicate_parametric_function"
