7203aa5844590a334162a865511ec59f2e402ae9
Mikhail f. Shiryaev <felixoid@clickhouse.com>
2026-08-19 11:15:43 +0200

    Preserve the real exception message when an async insert flush fails

--- src/Interpreters/AsynchronousInsertQueue.cpp
+++ src/Interpreters/AsynchronousInsertQueue.cpp
@@ -1349,7 +1349,7 @@ catch (const Poco::Exception & e)
 }
 catch (const std::exception & e)
 {
-    finishWithException(key.query, data->entries, e);
+    finishWithException(key.query, data->entries, Exception(Exception::CreateFromSTDTag{}, e));
 }
 catch (...)
 {
