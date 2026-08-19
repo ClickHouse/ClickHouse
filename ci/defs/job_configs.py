19f8d040e29d96c291b0698cc6120400fa4cb142
Rahul Nair <254529899+motsc@users.noreply.github.com>
2026-08-19 11:47:40 -0700

    ci: revert runner-label change (using OIDC instead)

--- ci/defs/job_configs.py
+++ ci/defs/job_configs.py
@@ -1834,7 +1834,7 @@ class JobConfigs:
 
     sign_macos_binary_jobs = Job.Config(
         name=JobNames.SIGN_MACOS,
-        runs_on=RunnerLabels.RELEASE_SIGNER,
+        runs_on=RunnerLabels.STYLE_CHECK_AMD,
         command="python3 ./ci/jobs/sign_macos_binary.py --build-type {PARAMETER}",
         run_in_docker="clickhouse/utils+--network=host+root",
         timeout=3600,
