585431a4f8a3e6d75822ca92facf1caba04f7b9a
Minh Vu <vuhoangminh97@gmail.com>
2026-08-20 13:22:11 +0200

    Fix aggregate state version propagation

---
+++ tests/queries/0_stateless/05029_single_value_or_null_native_protocol_round_trip.sql
@@ -0,0 +1,8 @@
+SELECT id, singleValueOrNullMerge(state)
+FROM remote('127.0.0.2', view(
+    SELECT 1 AS id, singleValueOrNullState(toUInt64(42)) AS state
+    UNION ALL
+    SELECT 2 AS id, singleValueOrNullState(number) AS state
+    FROM numbers(2)
+))
+ORDER BY id;
