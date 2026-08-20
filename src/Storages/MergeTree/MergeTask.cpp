043b3b78bbb608b60be6ad99a1eec60d70b90600
Valery Petrov <valerypetrov@icloud.com>
2026-08-19 18:44:11 -0700

    Fix designated initializer for PatchPartInfoForReader

--- src/Storages/MergeTree/MergeTask.cpp
+++ src/Storages/MergeTree/MergeTask.cpp
@@ -726,7 +726,9 @@ bool MergeTask::ExecuteAndFinalizeHorizontalPart::prepare() const
                 .part = std::move(patch_for_reader),
                 .source_parts = {},
                 .source_data_version = 0,
-                .perform_alter_conversions = true});
+                .perform_alter_conversions = true,
+                .sorting_key = nullptr,
+                .stored_sorting_key_columns = {}});
         }
 
         for (auto & storage_column : global_ctx->storage_columns)
