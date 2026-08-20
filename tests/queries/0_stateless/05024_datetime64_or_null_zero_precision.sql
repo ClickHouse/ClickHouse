8615f0571c3ab1ab15678aa6587324dcdda8bda3
Mark Needham <m.h.needham@gmail.com>
2026-08-20 14:16:02 +0100

    Keep `DateTime64` for zero-precision tolerant conversions

---
+++ tests/queries/0_stateless/05024_datetime64_or_null_zero_precision.sql
@@ -0,0 +1,10 @@
+SET session_timezone = 'UTC';
+
+SELECT toTypeName(toDateTime64OrNull('1789-07-14', 0, 'UTC'));
+SELECT toDateTime64OrNull('1789-07-14', 0, 'UTC');
+SELECT toDateTime64OrNull('1969-12-31 23:59:59', 0, 'UTC');
+SELECT toDateTime64OrNull(toInt64(-1), 0, 'UTC');
+SELECT toDateTime64OrNull('invalid', 0, 'UTC');
+
+SELECT toTypeName(toDateTime64OrZero('1789-07-14', 0, 'UTC'));
+SELECT toDateTime64OrZero('1789-07-14', 0, 'UTC');
