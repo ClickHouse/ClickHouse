52805b64574fb8240c70080d8999903c478734bf
Dominic Tran <dominic.tran@clickhouse.com>
2026-08-21 15:39:22 -0500

    Docs: preserve nested component indentation

--- src/Storages/MergeTree/registerStorageMergeTree.cpp
+++ src/Storages/MergeTree/registerStorageMergeTree.cpp
@@ -2504,15 +2504,15 @@ EXPLAIN indexes = 1 SELECT count() FROM test_stats WHERE value > 5000;
 
 - `minmax` (deprecated)
 
-<Note>
+    <Note>
     `minmax` statistics are deprecated. Use `basic` statistics instead, which is a superset of `minmax`.
-</Note>
+    </Note>
 
 - `tdigest`
 
-<Warning>
+    <Warning>
     Statistics of type `tdigest` have high creation costs and potentially slow down data ingest.
-</Warning>
+    </Warning>
 
     [TDigest](https://github.com/tdunning/t-digest) sketches which allow to compute approximate percentiles (e.g. the 90th percentile) for numeric columns.
 
@@ -2526,9 +2526,9 @@ EXPLAIN indexes = 1 SELECT count() FROM test_stats WHERE value > 5000;
 
 - `countmin`
 
-<Warning>
+    <Warning>
     Statistics of type `countmin` have high creation costs and potentially slow down data ingest.
-</Warning>
+    </Warning>
 
     [CountMin](https://en.wikipedia.org/wiki/Count%E2%80%93min_sketch) sketches which provide an approximate count of the frequency of each value in a column.
 
