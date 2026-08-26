3c54e495a2260fec448f69b9996c7f07885d88e6
Christoph Viebig <christoph.viebig@clickhouse.com>
2026-08-26 13:37:48 +0000

    Extract the `DPsize` join-order optimizer

--- src/Processors/QueryPlan/Optimizations/joinOrderAlgorithms.h
+++ src/Processors/QueryPlan/Optimizations/joinOrderAlgorithms.h
@@ -2,9 +2,21 @@
 
 #include <Processors/QueryPlan/Optimizations/joinOrder.h>
 
+#include <functional>
+#include <memory>
+
 namespace DB
 {
 
+class QueryStatus;
+using QueryStatusPtr = std::shared_ptr<QueryStatus>;
+
 DPJoinEntryPtr solveGreedyJoinOrder(QueryGraph & query_graph);
+
+DPJoinEntryPtr solveDPSizeJoinOrder(
+    QueryGraph & query_graph,
+    UInt64 max_searched_plans,
+    QueryStatusPtr query_status,
+    std::function<bool()> interactive_cancel_callback);
 
 }
