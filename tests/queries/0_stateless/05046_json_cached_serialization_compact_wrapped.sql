b11f7e2845c4f07d695068ecd0ca45d317ac1d2d
Rory Shanks <rorylshanks@gmail.com>
2026-08-28 15:14:07 +0200

    Test `Variant`-wrapped `JSON` cached serialization

--- tests/queries/0_stateless/05046_json_cached_serialization_compact_wrapped.sql
+++ tests/queries/0_stateless/05046_json_cached_serialization_compact_wrapped.sql
@@ -2,7 +2,8 @@ DROP TABLE IF EXISTS test_json_cached_serialization_compact_wrapped;
 
 CREATE TABLE test_json_cached_serialization_compact_wrapped
 (
-    json Tuple(data JSON(max_dynamic_paths = 0))
+    json Tuple(data JSON(max_dynamic_paths = 0)),
+    variant_json Variant(UInt64, JSON(stored UInt64))
 )
 ENGINE = MergeTree
 ORDER BY tuple()
@@ -11,7 +12,7 @@ SETTINGS
     min_rows_for_wide_part = 1,
     write_marks_for_substreams_in_compact_parts = 0;
 
-INSERT INTO test_json_cached_serialization_compact_wrapped VALUES (tuple('{"unstored":42}'));
+INSERT INTO test_json_cached_serialization_compact_wrapped VALUES (tuple('{"unstored":42}'), '{"stored":43}');
 
 SELECT part_type
 FROM system.parts
@@ -20,6 +21,9 @@ WHERE database = currentDatabase()
     AND active;
 
 SELECT json.data.unstored.:Int64
+FROM test_json_cached_serialization_compact_wrapped;
+
+SELECT variant_json.`JSON(stored UInt64)`.stored
 FROM test_json_cached_serialization_compact_wrapped;
 
 DROP TABLE test_json_cached_serialization_compact_wrapped;
