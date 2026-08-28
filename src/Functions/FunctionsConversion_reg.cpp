2f5dc47efecbeebba7ee56e44c0970a361fb15f3
yariks5s <Yarik.Briukhovetskyi@clickhouse.com>
2026-08-28 12:47:20 +0000

    Cover the shared parseDateTime64BestEffort* zero-precision type and fix toDateTime64OrZero docs

--- src/Functions/FunctionsConversion_reg.cpp
+++ src/Functions/FunctionsConversion_reg.cpp
@@ -2467,8 +2467,8 @@ SELECT toDateTimeOrZero('2025-12-30 13:44:17'), toDateTimeOrZero('invalid')
 
     /// toDateTime64OrZero documentation
     FunctionDocumentation::Description description_toDateTime64OrZero = R"(
-Converts an input value to a value of type [DateTime64](/reference/data-types/datetime64) but returns the lower boundary of [DateTime64](/reference/data-types/datetime64) if an invalid argument is received.
-The same as [toDateTime64](#toDateTime64) but returns lower boundary of [DateTime64](/reference/data-types/datetime64) if an invalid argument is received.
+Converts an input value to a value of type [DateTime64](/reference/data-types/datetime64) but returns the zero [DateTime64](/reference/data-types/datetime64) value (the Unix epoch, `1970-01-01 00:00:00`) if an invalid argument is received.
+The same as [toDateTime64](#toDateTime64) but returns the zero [DateTime64](/reference/data-types/datetime64) value if an invalid argument is received.
 
 See also:
 - [toDateTime64](#toDateTime64).
@@ -2482,7 +2482,7 @@ See also:
         {"precision", "Optional. The subsecond precision of the returned value.", {"UInt8"}},
         {"timezone", "Optional. Time zone of the returned value.", {"String"}},
     };
-    FunctionDocumentation::ReturnedValue returned_value_toDateTime64OrZero = {"Returns a DateTime64 value if successful, otherwise the lower boundary of DateTime64 (`1970-01-01 00:00:00.000`).", {"DateTime64"}};
+    FunctionDocumentation::ReturnedValue returned_value_toDateTime64OrZero = {"Returns a DateTime64 value if successful, otherwise the zero DateTime64 value (`1970-01-01 00:00:00`) at the requested precision.", {"DateTime64"}};
     FunctionDocumentation::Examples examples_toDateTime64OrZero = {
     {
         "Usage example",
--- tests/queries/0_stateless/05024_datetime64_or_null_zero_precision.sql
+++ tests/queries/0_stateless/05024_datetime64_or_null_zero_precision.sql
@@ -14,3 +14,11 @@ SELECT toDateTime64OrNull('invalid', 0, 'UTC');
 SELECT toTypeName(toDateTime64OrZero('1789-07-14', 0, 'UTC'));
 SELECT toDateTime64OrZero('1789-07-14', 0, 'UTC');
 SELECT toDateTime64OrZero('0001-01-01', 0, 'UTC');
+
+-- The zero-precision result type is shared with the parseDateTime64BestEffort* family.
+SELECT toTypeName(parseDateTime64BestEffort('1789-07-14', 0, 'UTC'));
+SELECT parseDateTime64BestEffort('1789-07-14', 0, 'UTC');
+SELECT toTypeName(parseDateTime64BestEffortOrNull('1789-07-14', 0, 'UTC'));
+SELECT parseDateTime64BestEffortOrNull('1789-07-14', 0, 'UTC');
+SELECT toTypeName(parseDateTime64BestEffortUSOrZero('07/14/1789', 0, 'UTC'));
+SELECT parseDateTime64BestEffortUSOrZero('07/14/1789', 0, 'UTC');
