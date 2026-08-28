360e9dddb74289ac2dfcd88e5e3babae6fac05a7
Mark Needham <m.h.needham@gmail.com>
2026-08-28 09:36:47 +0100

    Test the `DateTime64` lower boundary

--- tests/queries/0_stateless/05024_datetime64_or_null_zero_precision.sql
+++ tests/queries/0_stateless/05024_datetime64_or_null_zero_precision.sql
@@ -1,13 +1,13 @@
-SET session_timezone = 'Europe/Berlin';
+SET session_timezone = 'America/New_York';
 
-SELECT toTypeName(toDateTime64OrNull('1789-07-14', 0));
-SELECT toDateTime64OrNull('1789-07-14', 0);
+SELECT toTypeName(toDateTime64OrNull('0001-01-01', 0));
+SELECT toDateTime64OrNull('0001-01-01', 0);
 
-SELECT toTypeName(toDateTime64OrNull('1789-07-14', 0, 'UTC'));
-SELECT toDateTime64OrNull('1789-07-14', 0, 'UTC');
+SELECT toTypeName(toDateTime64OrNull('0001-01-01', 0, 'UTC'));
+SELECT toDateTime64OrNull('0001-01-01', 0, 'UTC');
 SELECT toDateTime64OrNull('1969-12-31 23:59:59', 0, 'UTC');
 SELECT toDateTime64OrNull(toInt64(-1), 0, 'UTC');
 SELECT toDateTime64OrNull('invalid', 0, 'UTC');
 
-SELECT toTypeName(toDateTime64OrZero('1789-07-14', 0, 'UTC'));
-SELECT toDateTime64OrZero('1789-07-14', 0, 'UTC');
+SELECT toTypeName(toDateTime64OrZero('0001-01-01', 0, 'UTC'));
+SELECT toDateTime64OrZero('0001-01-01', 0, 'UTC');
