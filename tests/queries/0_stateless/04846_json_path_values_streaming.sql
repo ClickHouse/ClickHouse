56e3e4c3e9b37d1c0c6737fa17210fd2aa9b9132
Rory Shanks <rorylshanks@gmail.com>
2026-08-29 19:15:28 +0200

    Remove redundant `query_log` dependency from streaming test

--- tests/queries/0_stateless/04846_json_path_values_streaming.sql
+++ tests/queries/0_stateless/04846_json_path_values_streaming.sql
@@ -1,5 +1,4 @@
 SET enable_json_type = 1;
-SET log_queries = 1;
 SET query_plan_direct_read_from_text_index = 1;
 SET use_skip_indexes_on_data_read = 1;
 
@@ -17,21 +16,10 @@ SETTINGS index_granularity = 1;
 SYSTEM STOP MERGES json_stream_direct;
 INSERT INTO json_stream_direct
 SELECT number, concat('{"s":"value-', toString(number), '","tags":["common",null],"dynamic":', toString(number), '}')
-FROM numbers(4)
-SETTINGS log_comment = '04846_stream_direct';
+FROM numbers(4);
 INSERT INTO json_stream_direct
 SELECT number, concat('{"s":"', repeat('x', 100), '-', toString(number), '","tags":["other",""]}')
-FROM numbers(4, 4)
-SETTINGS log_comment = '04846_stream_direct';
-
-SYSTEM FLUSH LOGS query_log;
-SELECT sum(ProfileEvents['JSONPathValuesTextIndexInputRows'])
-FROM system.query_log
-WHERE event_date >= yesterday()
-    AND event_time >= now() - 600
-    AND current_database = currentDatabase()
-    AND type = 'QueryFinish'
-    AND log_comment = '04846_stream_direct';
+FROM numbers(4, 4);
 
 SELECT arraySort(groupArray(id)) FROM json_stream_direct WHERE data.s = 'value-2'
 SETTINGS force_data_skipping_indices = 'tokens';
