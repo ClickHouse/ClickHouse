4577925d7a8b0d0c8eac1fa33f249f4d5144d650
Alexey Milovidov <milovidov@clickhouse.com>
2026-08-31 10:37:56 +0000

    Address review: cover `fromUnixTimestampInJodaSyntax`, renumber the test
    The fix lives in the shared `FunctionFormatDateTimeImpl<support_integer, format_syntax>`,
    so add a regression query for the remaining `support_integer` + Joda instantiation -
    `fromUnixTimestampInJodaSyntax` - which was the only one of the four entrypoints named in
    the description without direct coverage.
    
    Renumber the test from `05048` to `05054`: `05048` was taken on master in the meantime.

--- tests/queries/0_stateless/05054_formatdatetime_lowcardinality_timezone.sql
+++ tests/queries/0_stateless/05054_formatdatetime_lowcardinality_timezone.sql
@@ -5,6 +5,7 @@
 SELECT DISTINCT formatDateTime(toDateTime('2026-08-29 00:00:00', 'UTC'), '%F %T', toLowCardinality(if(number % 2, 'UTC', 'Europe/Amsterdam'))) AS x FROM numbers(4) ORDER BY x;
 SELECT DISTINCT formatDateTimeInJodaSyntax(toDateTime('2026-08-29 00:00:00', 'UTC'), 'yyyy-MM-dd HH:mm:ss', toLowCardinality(if(number % 2, 'UTC', 'Europe/Amsterdam'))) AS x FROM numbers(4) ORDER BY x;
 SELECT DISTINCT fromUnixTimestamp(toUInt32(1787961600), '%F %T', toLowCardinality(if(number % 2, 'UTC', 'Europe/Amsterdam'))) AS x FROM numbers(4) ORDER BY x;
+SELECT DISTINCT fromUnixTimestampInJodaSyntax(toUInt32(1787961600), 'yyyy-MM-dd HH:mm:ss', toLowCardinality(if(number % 2, 'UTC', 'Europe/Amsterdam'))) AS x FROM numbers(4) ORDER BY x;
 
 SELECT formatDateTime(toDateTime64('2026-08-29 00:00:00.123', 3, 'UTC'), '%F %T.%f', materialize(toLowCardinality('Asia/Istanbul')));
 SELECT toTypeName(formatDateTime(toDateTime('2026-08-29 00:00:00', 'UTC'), '%F %T', materialize(toLowCardinality('UTC'))));
