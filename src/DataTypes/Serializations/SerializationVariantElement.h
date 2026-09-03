0168831aeb5986548bf82a33bbac17c87e728119
robot-clickhouse <robot-clickhouse@users.noreply.github.com>
2026-09-03 16:18:41 +0000

    Backport #107991 to 26.3: validate discriminators in Variant binary bulk deserialization

--- src/DataTypes/Serializations/SerializationVariantElement.h
+++ src/DataTypes/Serializations/SerializationVariantElement.h
@@ -19,12 +19,20 @@ private:
     /// we need its type name and global discriminator.
     String variant_element_name;
     ColumnVariant::Discriminator variant_discriminator;
+    /// Total number of variants in the Variant type; used for bounds-checking
+    /// compact discriminators read from the wire.
+    size_t num_variants;
 
 public:
-    SerializationVariantElement(const SerializationPtr & nested_, const String & variant_element_name_, ColumnVariant::Discriminator variant_discriminator_)
+    SerializationVariantElement(
+        const SerializationPtr & nested_,
+        const String & variant_element_name_,
+        ColumnVariant::Discriminator variant_discriminator_,
+        size_t num_variants_)
         : SerializationWrapper(nested_)
         , variant_element_name(variant_element_name_)
         , variant_discriminator(variant_discriminator_)
+        , num_variants(num_variants_)
     {
     }
 
@@ -71,6 +79,7 @@ public:
         const ColumnVariant::Discriminator global_variant_discriminator;
         const ColumnVariant::Discriminator local_variant_discriminator;
         bool make_nullable;
+        size_t num_variants;
 
     public:
         VariantSubcolumnCreator(
@@ -79,7 +88,8 @@ public:
             ColumnVariant::Discriminator global_variant_discriminator_,
             ColumnVariant::Discriminator local_variant_discriminator_,
             bool make_nullable_,
-            const ColumnPtr & null_map_ = nullptr);
+            const ColumnPtr & null_map_ = nullptr,
+            size_t num_variants_ = 0);
 
         DataTypePtr create(const DataTypePtr & prev) const override;
         ColumnPtr create(const ColumnPtr & prev) const override;
@@ -99,6 +109,8 @@ private:
         ReadBuffer * stream,
         bool continuous_reading,
         DeserializeBinaryBulkStatePtr & discriminators_state_,
+        const DeserializeBinaryBulkSettings & settings,
+        size_t num_variants,
         const ISerialization * serialization);
 
     void addVariantToPath(SubstreamPath & path) const;
