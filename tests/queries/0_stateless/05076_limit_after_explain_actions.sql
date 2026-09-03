9df3983063eebeed7bbfc3b77a0ad38812dbf6fe
Nihal Z. Miaji <81457724+nihalzp@users.noreply.github.com>
2026-09-03 17:16:16 +0000

    Update the `EXPLAIN` test for the single conditions expression of `LimitRangeStep`

--- tests/queries/0_stateless/05076_limit_after_explain_actions.sql
+++ tests/queries/0_stateless/05076_limit_after_explain_actions.sql
@@ -8,5 +8,5 @@ WHERE explain LIKE '%LimitRange%' OR explain LIKE '%Limit 2%' OR explain LIKE '%
 SELECT trimLeft(explain) FROM (EXPLAIN PLAN actions = 1 SELECT number FROM numbers(5) ORDER BY number LIMIT 2 AFTER number = 3 ALL SETTINGS exact_rows_before_limit = 1)
 WHERE explain LIKE '%LimitRange%' OR explain LIKE '%Limit 2%' OR explain LIKE '%column:%' OR explain LIKE '%FUNCTION%' OR explain LIKE '%Reads all data%';
 
-SELECT explain LIKE '%"After Column"%' AND explain LIKE '%"Until Expression"%' AND explain LIKE '%"After All": false%' AND explain LIKE '%"Reads All Data": false%'
+SELECT explain LIKE '%"After Column"%' AND explain LIKE '%"Until Column"%' AND explain LIKE '%"Expression"%' AND explain LIKE '%"After All": false%' AND explain LIKE '%"Reads All Data": false%'
 FROM (EXPLAIN PLAN json = 1, actions = 1 SELECT number FROM numbers(5) ORDER BY number LIMIT 2 AFTER number >= 1 UNTIL number >= 4 SETTINGS exact_rows_before_limit = 0);
