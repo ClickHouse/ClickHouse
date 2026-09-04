7e85caef87be33d1a1a600aa50a1282677e01c83
rienath <raufs.dunamalijevs@clickhouse.com>
2026-09-04 09:55:17 +0000

    Pin serialization_info_version

--- tests/queries/0_stateless/05055_adaptive_codec_untyped_substreams.sql
+++ tests/queries/0_stateless/05055_adaptive_codec_untyped_substreams.sql
@@ -2,7 +2,7 @@ DROP TABLE IF EXISTS t_untyped_substreams;
 
 CREATE TABLE t_untyped_substreams (m Map(UInt64, UInt8))
 ENGINE = MergeTree ORDER BY tuple()
-SETTINGS min_bytes_for_wide_part = 0, enable_adaptive_codec_selection = 1,
+SETTINGS min_bytes_for_wide_part = 0, enable_adaptive_codec_selection = 1, serialization_info_version = 'with_types',
          map_serialization_version = 'with_buckets', map_buckets_strategy = 'constant', max_buckets_in_map = 256,
          map_buckets_min_avg_size = 0;
 
