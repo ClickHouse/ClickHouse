ac03a4cd20ab90dd3a9befe93ec29e09d6fdd10f
alexprabhat99 <alexpbara@gmail.com>
2026-09-04 11:58:46 +0530

    added LIKE suffix escape regression coverage

--- tests/queries/0_stateless/05055_like_perfect_suffix_escape.sql
+++ tests/queries/0_stateless/05055_like_perfect_suffix_escape.sql
@@ -1,5 +1,6 @@
 SET enable_analyzer = 1;
 SET optimize_rewrite_like_perfect_affix = 1;
+SET enable_parallel_replicas = 0;
 
 DROP TABLE IF EXISTS t_like_suffix_escape;
 
@@ -48,5 +49,9 @@ SELECT s LIKE '%a\\%' FROM t_like_suffix_escape;
 SELECT s, s LIKE '%a\\%' AS matched
 FROM t_like_suffix_escape
 ORDER BY s;
+
+-- A trailing '\' is an invalid LIKE escape sequence.
+-- The suffix optimization must not rewrite this and hide the error.
+SELECT 'x' LIKE '%abc\\'; -- { serverError CANNOT_PARSE_ESCAPE_SEQUENCE }
 
 DROP TABLE t_like_suffix_escape;
