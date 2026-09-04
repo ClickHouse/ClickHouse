3cbd827b17411a67dc956a9e45b4f839f3c71f01
rienath <raufs.dunamalijevs@clickhouse.com>
2026-09-04 15:12:16 +0000

    Reduce test size

--- tests/queries/0_stateless/05055_adaptive_codec_untyped_substreams.sql
+++ tests/queries/0_stateless/05055_adaptive_codec_untyped_substreams.sql
@@ -3,10 +3,10 @@ DROP TABLE IF EXISTS t_untyped_substreams;
 CREATE TABLE t_untyped_substreams (m Map(UInt64, UInt8))
 ENGINE = MergeTree ORDER BY tuple()
 SETTINGS min_bytes_for_wide_part = 0, enable_adaptive_codec_selection = 1, serialization_info_version = 'with_types',
-         map_serialization_version = 'with_buckets', map_buckets_strategy = 'constant', max_buckets_in_map = 256,
+         map_serialization_version = 'with_buckets', map_buckets_strategy = 'constant', max_buckets_in_map = 32,
          map_buckets_min_avg_size = 0;
 
-INSERT INTO t_untyped_substreams SELECT map(number * 2, 1, number * 2 + 1, 2) FROM numbers(100000);
+INSERT INTO t_untyped_substreams SELECT map(number * 2, 1, number * 2 + 1, 2) FROM numbers(30);
 
 OPTIMIZE TABLE t_untyped_substreams FINAL; -- inserts aren't adaptive, merges are
 
