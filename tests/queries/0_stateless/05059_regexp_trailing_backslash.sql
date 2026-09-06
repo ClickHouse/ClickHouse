294c3e7c4ca69cf2663c550bc2d372ee7fa78603
alexprabhat99 <alexpbara@gmail.com>
2026-09-06 08:55:16 +0530

    update regexp tests for trailing backslash errors

--- tests/queries/0_stateless/05059_regexp_trailing_backslash.sql
+++ tests/queries/0_stateless/05059_regexp_trailing_backslash.sql
@@ -2,12 +2,12 @@
 -- A trailing unescaped backslash is an invalid RE2 regexp and must not
 -- be treated as a trivial substring.
 
-SELECT match('abcd', 'abc\\'); -- { serverError 427 }
-SELECT match('abcd', '\\'); -- { serverError 427 }
+SELECT match('abcd', 'abc\\'); -- { serverError CANNOT_COMPILE_REGEXP }
+SELECT match('abcd', '\\'); -- { serverError CANNOT_COMPILE_REGEXP }
 
-SELECT extractAll('abcd', 'abc\\'); -- { serverError 427 }
-SELECT countMatches('abcabc', 'abc\\'); -- { serverError 427 }
-SELECT splitByRegexp('abc\\', 'xabcy'); -- { serverError 427 }
+SELECT extractAll('abcd', 'abc\\'); -- { serverError CANNOT_COMPILE_REGEXP }
+SELECT countMatches('abcabc', 'abc\\'); -- { serverError CANNOT_COMPILE_REGEXP }
+SELECT splitByRegexp('abc\\', 'xabcy'); -- { serverError CANNOT_COMPILE_REGEXP }
 
 -- Valid escaped backslashes must continue working.
 SELECT match('abc\\', 'abc\\\\');
