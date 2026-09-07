600cb267fafdc32769be73807fe10e41244bc52c
Minh Vu <vuhoangminh97@gmail.com>
2026-09-07 21:08:19 +0200

    Expand empty array aggregation coverage

--- tests/queries/0_stateless/05136_array_aggregation_constant_lambda_empty_arrays.sql
+++ tests/queries/0_stateless/05136_array_aggregation_constant_lambda_empty_arrays.sql
@@ -6,7 +6,7 @@ SELECT
     arraySum(x -> 7, arr),
     arrayAvg(x -> 7, arr),
     arrayProduct(x -> 7, arr)
-FROM values('id UInt8, arr Array(Int32)', (1, []), (2, [1, 2]), (3, []), (4, [3]), (5, []))
+FROM values('id UInt8, arr Array(Int32)', (1, []), (2, []), (3, [1, 2]), (4, [3]), (5, []), (6, []))
 ORDER BY id;
 
 SELECT
@@ -17,7 +17,7 @@ SELECT
     arraySum(x -> toFloat64('nan'), arr),
     arrayAvg(x -> toFloat64('nan'), arr),
     arrayProduct(x -> toFloat64('nan'), arr)
-FROM values('id UInt8, arr Array(Float64)', (1, []), (2, [1, 2]), (3, []), (4, [3]), (5, []))
+FROM values('id UInt8, arr Array(Float64)', (1, []), (2, []), (3, [1, 2]), (4, [3]), (5, []), (6, []))
 ORDER BY id;
 
 SELECT
@@ -28,5 +28,13 @@ SELECT
     arraySum(x -> toDecimal32(7, 2), arr),
     arrayAvg(x -> toDecimal32(7, 2), arr),
     arrayProduct(x -> toDecimal32(7, 2), arr)
-FROM values('id UInt8, arr Array(Decimal32(2))', (1, []), (2, [1, 2]), (3, []), (4, [3]), (5, []))
+FROM values('id UInt8, arr Array(Decimal32(2))', (1, []), (2, []), (3, [1, 2]), (4, [3]), (5, []), (6, []))
+ORDER BY id;
+
+SELECT
+    id,
+    arr,
+    length(arrayMin(x -> 'foo', arr)),
+    length(arrayMax(x -> 'foo', arr))
+FROM values('id UInt8, arr Array(Int32)', (1, []), (2, []), (3, [1, 2]), (4, [3]), (5, []), (6, []))
 ORDER BY id;
