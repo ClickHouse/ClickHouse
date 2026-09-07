d75feb214c7e6caec37e74cb9213094e629c33dd
Vitaly Baranov <vitlibar@clickhouse.com>
2026-09-07 23:30:09 +0200

    Pin enable_analyzer in the TimeSeries parallel replicas test

--- tests/queries/0_stateless/05104_time_series_table_functions_parallel_replicas.sql
+++ tests/queries/0_stateless/05104_time_series_table_functions_parallel_replicas.sql
@@ -7,6 +7,7 @@
 -- See https://github.com/ClickHouse/ClickHouse/issues/118130
 
 SET allow_experimental_time_series_table = 1;
+SET enable_analyzer = 1;
 SET enable_parallel_replicas = 2, max_parallel_replicas = 3, cluster_for_parallel_replicas = 'parallel_replicas', parallel_replicas_for_non_replicated_merge_tree = 1;
 SET parallel_replicas_local_plan = 0;
 SET automatic_parallel_replicas_mode = 0;
