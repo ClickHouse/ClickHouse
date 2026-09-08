28a0b483770747a0dbf8715d1b8b268d02477587
JasonLi-cn <lijingxuan92@126.com>
2026-09-08 23:10:49 +1200

    Use UUID in `Replicated` database ATTACH test

--- tests/queries/0_stateless/05137_text_index_replicated_database_alter.sql
+++ tests/queries/0_stateless/05137_text_index_replicated_database_alter.sql
@@ -21,7 +21,7 @@ ENGINE = MergeTree ORDER BY tuple();
 -- Initial `ALTER` DDL also remains strict; a replica must not introduce invalid metadata.
 ALTER TABLE {CLICKHOUSE_DATABASE_1:Identifier}.tab ADD INDEX idx t TYPE text(tokenizer = 'splitByNonAlpha'); -- { serverError BAD_ARGUMENTS }
 
-CREATE TABLE {CLICKHOUSE_DATABASE_1:Identifier}.attached
+CREATE TABLE {CLICKHOUSE_DATABASE_1:Identifier}.attached UUID 'f1b33c88-07ad-4eea-b293-4c6e1fd367b2'
 (
     t Array(Array(String))
 )
@@ -29,7 +29,7 @@ ENGINE = MergeTree ORDER BY tuple();
 
 -- A full-definition `ATTACH` is fresh DDL on the initiating replica, even in a `Replicated` database.
 DETACH TABLE {CLICKHOUSE_DATABASE_1:Identifier}.attached PERMANENTLY FORMAT Null;
-ATTACH TABLE {CLICKHOUSE_DATABASE_1:Identifier}.attached
+ATTACH TABLE {CLICKHOUSE_DATABASE_1:Identifier}.attached UUID 'f1b33c88-07ad-4eea-b293-4c6e1fd367b2'
 (
     t Array(Array(String)),
     INDEX idx t TYPE text(tokenizer = 'splitByNonAlpha')
