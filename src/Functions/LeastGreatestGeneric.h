62822cb884fd907ea70578dc1373d43a233c9d1e
Minh Vu <vuhoangminh97@gmail.com>
2026-09-09 23:54:48 +0200

    Perf: avoid materializing constants in least and greatest

--- src/Functions/LeastGreatestGeneric.h
+++ src/Functions/LeastGreatestGeneric.h
@@ -1,5 +1,6 @@
 #pragma once
 
+#include <Columns/ColumnConst.h>
 #include <DataTypes/getLeastSupertype.h>
 #include <DataTypes/NumberTraits.h>
 #include <Interpreters/Context.h>
@@ -7,6 +8,8 @@
 #include <Core/Settings.h>
 #include <Functions/IFunction.h>
 #include <Functions/FunctionFactory.h>
+
+#include <vector>
 
 namespace DB
 {
@@ -64,43 +67,98 @@ private:
         if (arguments.size() == 1)
             return arguments[0].column;
 
-        Columns converted_columns;
+        struct ConvertedColumn
+        {
+            ColumnPtr column;
+            bool is_const;
+        };
+
+        std::vector<ConvertedColumn> converted_columns;
+        bool has_const_column = false;
         for (const auto & argument : arguments)
         {
             if (!legacy_null_behavior && argument.type->onlyNull())
                 continue; /// ignore NULL arguments
-            auto converted_col = castColumn(argument, result_type)->convertToFullColumnIfConst();
-            converted_columns.push_back(converted_col);
+
+            auto converted_col = castColumn(argument, result_type);
+            if (const auto * const_column = checkAndGetColumn<ColumnConst>(converted_col.get()))
+            {
+                converted_columns.push_back({const_column->getDataColumnPtr(), true});
+                has_const_column = true;
+            }
+            else
+            {
+                converted_columns.push_back({std::move(converted_col), false});
+            }
         }
 
         if (!legacy_null_behavior && converted_columns.empty())
             return arguments[0].column;
         else if (!legacy_null_behavior && converted_columns.size() == 1)
-            return converted_columns[0];
+        {
+            if (converted_columns[0].is_const)
+                return ColumnConst::create(converted_columns[0].column, input_rows_count);
+            return converted_columns[0].column;
+        }
 
         auto result_column = result_type->createColumn();
         result_column->reserve(input_rows_count);
+
+        if (!has_const_column)
+        {
+            for (size_t row_num = 0; row_num < input_rows_count; ++row_num)
+            {
+                size_t best_arg = 0;
+                for (size_t arg = 1; arg < converted_columns.size(); ++arg)
+                {
+                    if constexpr (kind == LeastGreatest::Least)
+                    {
+                        auto cmp_result = converted_columns[arg].column->compareAt(
+                            row_num, row_num, *converted_columns[best_arg].column, 1);
+                        if (cmp_result < 0)
+                            best_arg = arg;
+                    }
+                    else
+                    {
+                        auto cmp_result = converted_columns[arg].column->compareAt(
+                            row_num, row_num, *converted_columns[best_arg].column, -1);
+                        if (cmp_result > 0)
+                            best_arg = arg;
+                    }
+                }
+
+                result_column->insertFrom(*converted_columns[best_arg].column, row_num);
+            }
+
+            return result_column;
+        }
 
         for (size_t row_num = 0; row_num < input_rows_count; ++row_num)
         {
             size_t best_arg = 0;
             for (size_t arg = 1; arg < converted_columns.size(); ++arg)
             {
+                const auto & current = converted_columns[arg];
+                const auto & best = converted_columns[best_arg];
+                const size_t current_row = current.is_const ? 0 : row_num;
+                const size_t best_row = best.is_const ? 0 : row_num;
+
                 if constexpr (kind == LeastGreatest::Least)
                 {
-                    auto cmp_result = converted_columns[arg]->compareAt(row_num, row_num, *converted_columns[best_arg], 1);
+                    auto cmp_result = current.column->compareAt(current_row, best_row, *best.column, 1);
                     if (cmp_result < 0)
                         best_arg = arg;
                 }
                 else
                 {
-                    auto cmp_result = converted_columns[arg]->compareAt(row_num, row_num, *converted_columns[best_arg], -1);
+                    auto cmp_result = current.column->compareAt(current_row, best_row, *best.column, -1);
                     if (cmp_result > 0)
                         best_arg = arg;
                 }
             }
 
-            result_column->insertFrom(*converted_columns[best_arg], row_num);
+            const auto & best = converted_columns[best_arg];
+            result_column->insertFrom(*best.column, best.is_const ? 0 : row_num);
         }
 
         return result_column;
