679c49f6dc8f2dd91f4657a26a7c3cdcc34442f0
rienath <raufs.dunamalijevs@clickhouse.com>
2026-09-10 17:25:54 +0000

    Match Native encoding and decoding in the interval regression test

--- tests/queries/0_stateless/05054_interval_binary_decoding_invalid_kind.sql
+++ tests/queries/0_stateless/05054_interval_binary_decoding_invalid_kind.sql
@@ -1,5 +1,5 @@
 SET input_format_binary_decode_types_in_binary_format = 0;
-SET input_format_native_decode_types_in_binary_format = 1;
+SET input_format_native_decode_types_in_binary_format = 1, output_format_native_encode_types_in_binary_format = 1;
 
 SELECT * FROM format(Native, 'x IntervalSecond', x'01010178220b2a00000000000000'); -- { serverError INCORRECT_DATA }
 SELECT * FROM format(Native, 'x IntervalSecond', x'01010178227f2a00000000000000'); -- { serverError INCORRECT_DATA }
@@ -9,7 +9,7 @@ SELECT * FROM format(Native, 'x IntervalSecond', x'0101017822ff2a00000000000000'
 SELECT toTypeName(x), x FROM format(Native, 'x IntervalNanosecond', x'0101017822002a00000000000000');
 SELECT toTypeName(x), x FROM format(Native, 'x IntervalYear', x'01010178220a2a00000000000000');
 
-SET input_format_native_decode_types_in_binary_format = 0;
+SET input_format_native_decode_types_in_binary_format = 0, output_format_native_encode_types_in_binary_format = 0;
 SET input_format_binary_decode_types_in_binary_format = 1;
 
 SELECT * FROM format(RowBinaryWithNamesAndTypes, 'x IntervalSecond', x'010178220b2a00000000000000'); -- { serverError INCORRECT_DATA }
