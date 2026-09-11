d092dab7d4b5ab776892cc22244dafc586d6500e
Smita Kulkarni <Smita.Kulkarni@clickhouse.com>
2026-09-11 20:51:58 +0200

    Addressed review comments

--- src/Common/QueryFuzzer.cpp
+++ src/Common/QueryFuzzer.cpp
@@ -1631,9 +1631,9 @@ void QueryFuzzer::fuzzRefreshStrategy(ASTRefreshStrategy & strategy)
         strategy.set(strategy.spread, std::move(spread));
     }
 
-    /// Toggle APPEND
+    /// Fuzz the refresh mode
     if (fuzz_rand() % 10 == 0)
-        strategy.mode = strategy.isAppend() ? RefreshMode::Replace : RefreshMode::AppendFull;
+        strategy.mode = static_cast<RefreshMode>(fuzz_rand() % 3);
 
     /// Toggle schedule kind between EVERY and AFTER
     if (strategy.schedule_kind != RefreshScheduleKind::UNKNOWN && fuzz_rand() % 10 == 0)
