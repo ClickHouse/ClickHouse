936dd4b1bdab50abdcf84040d0c425cf553df58e
yariks5s <yaroslav.briukhovetskyi@clickhouse.com>
2026-09-11 15:20:09 +0000

    Shorten the test to one table and one parameterized view

--- tests/queries/0_stateless/05182_parameterized_view_insert_select_parallel_distributed.sql
+++ tests/queries/0_stateless/05182_parameterized_view_insert_select_parallel_distributed.sql
@@ -3,17 +3,12 @@
 SET enable_analyzer = 1;
 SET parallel_distributed_insert_select = 2;
 
-CREATE TABLE t1 (id UInt8, path String) ENGINE = MergeTree ORDER BY id;
-CREATE TABLE t2 (path String) ENGINE = MergeTree ORDER BY path;
-CREATE TABLE t3 (path String) ENGINE = MergeTree ORDER BY path;
-INSERT INTO t1 VALUES (1, 'a'), (1, 'b'), (2, 'c');
+CREATE TABLE t (id UInt8, p String) ENGINE = MergeTree ORDER BY id;
+-- `SELECT i.*` over two joins yields columns named `i.p` in the legacy interpreter.
+CREATE VIEW v AS SELECT count() AS c FROM (SELECT i.* FROM t AS i LEFT JOIN t AS a ON i.p = a.p LEFT JOIN t AS b ON i.p = b.p WHERE i.id = {id:UInt8}) AS i WHERE i.p = 'a';
+CREATE TABLE d (c UInt64) ENGINE = ReplicatedMergeTree('/clickhouse/tables/{database}/d', '1') ORDER BY c;
 
--- `SELECT i.*` over two joins yields columns named `i.path` in the legacy interpreter.
-CREATE VIEW v_inner AS SELECT i.* FROM t1 AS i LEFT JOIN t2 ON i.path = t2.path LEFT JOIN t3 ON i.path = t3.path WHERE i.id = {id:UInt8};
-CREATE VIEW v_outer AS WITH issues AS (SELECT * FROM v_inner(id = {id:UInt8})) SELECT count() AS c, countIf(i.path = 'a') AS a FROM issues AS i;
-CREATE TABLE t_dst (c UInt64, a UInt64) ENGINE = ReplicatedMergeTree('/clickhouse/tables/{database}/t_dst', '1') ORDER BY c;
+INSERT INTO d SELECT * FROM v(id = 1);
+SELECT * FROM d;
 
-INSERT INTO t_dst SELECT * FROM v_outer(id = 1);
-SELECT * FROM t_dst;
-
-DROP TABLE t_dst SYNC;
+DROP TABLE d SYNC;
