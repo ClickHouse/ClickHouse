9f651e6b5ef8a77228528830cf17ffe1abb0b7e0
Ilya Yatsishin <qoega@clickhouse.com>
2026-09-12 17:34:35 +0200

    Revert "Keep multi-table storage scenario compatible with master"
    This reverts commit 32cbaeea3353b431e3710e99cb4ed4bb2d9d1592.

--- utils/storage-memory-profiler/scenarios/05_multiple_tables.sql
+++ utils/storage-memory-profiler/scenarios/05_multiple_tables.sql
@@ -1,5 +1,5 @@
 -- Test memory scaling with multiple tables
 CREATE TABLE test_mt_2 AS test_mt;
 CREATE TABLE test_mt_3 AS test_mt;
-INSERT INTO test_mt_2 SELECT * FROM test_mt SETTINGS load_marks_asynchronously = 0;
-INSERT INTO test_mt_3 SELECT * FROM test_mt SETTINGS load_marks_asynchronously = 0;
+INSERT INTO test_mt_2 SELECT * FROM test_mt;
+INSERT INTO test_mt_3 SELECT * FROM test_mt;
