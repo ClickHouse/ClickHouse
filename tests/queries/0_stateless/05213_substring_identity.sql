b15a35c334c1056484b9b2a80b18ced620920402
Minh Vu <vuhoangminh97@gmail.com>
2026-09-13 20:18:08 +0200

    Expand substring identity type coverage

--- tests/queries/0_stateless/05213_substring_identity.sql
+++ tests/queries/0_stateless/05213_substring_identity.sql
@@ -13,3 +13,10 @@ SELECT substring('abcdef', 2);
 SELECT substringUTF8('héllo', 2);
 SELECT substring('abcdef', 1, 3);
 SELECT toTypeName(substring(toFixedString('abc', 3), 1));
+SELECT toTypeName(substring(CAST('foo', 'Enum8(\'foo\' = 1)'), 1));
+
+SELECT number, substring(
+    if(number = 1, CAST(NULL, 'Nullable(String)'), CAST('hello', 'Nullable(String)')),
+    1)
+FROM numbers(3)
+ORDER BY number;
