faa2eba65e08d1827043cd06200046b54313b825
Minh Vu <vuhoangminh97@gmail.com>
2026-09-14 16:10:41 +0200

    Fix bitTest documentation types

--- src/Functions/bitTest.cpp
+++ src/Functions/bitTest.cpp
@@ -72,8 +72,8 @@ REGISTER_FUNCTION(BitTest)
     FunctionDocumentation::Description description = "Takes any number and converts it into [binary form](https://en.wikipedia.org/wiki/Binary_number), then returns the value of the bit at a specified position. Counting is done right-to-left, starting at 0.";
     FunctionDocumentation::Syntax syntax = "bitTest(a, i)";
     FunctionDocumentation::Arguments arguments = {
-        {"a", "Number to convert.", {"(U)Int8/16/32/64/128/256", "Float*"}},
-        {"i", "Position of the bit to return.", {"(U)Int8/16/32/64", "Float*"}},
+        {"a", "Number to convert.", {"(U)Int8/16/32/64/128/256"}},
+        {"i", "Position of the bit to return.", {"(U)Int8/16/32/64"}},
     };
     FunctionDocumentation::ReturnedValue returned_value = {"Returns the value of the bit at position `i` in the binary representation of `a`", {"UInt8"}};
     FunctionDocumentation::Examples examples = {{"Usage example", "SELECT bin(2), bitTest(2, 1);",
