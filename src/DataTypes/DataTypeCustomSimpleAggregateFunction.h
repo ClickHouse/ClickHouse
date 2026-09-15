1d25fbae92fede988bbee4dbde1770b1d50cad73
vanchaklar <38177830+vanchaklar@users.noreply.github.com>
2026-09-15 14:51:23 +0330

    Keep SimpleAggregateFunction custom hooks lightweight

--- src/DataTypes/DataTypeCustomSimpleAggregateFunction.h
+++ src/DataTypes/DataTypeCustomSimpleAggregateFunction.h
@@ -3,12 +3,15 @@
 #include <AggregateFunctions/IAggregateFunction_fwd.h>
 #include <Core/Field.h>
 #include <DataTypes/DataTypeCustom.h>
-#include <DataTypes/IDataType.h>
 #include <Common/VectorWithMemoryTracking.h>
 
 
 namespace DB
 {
+
+class IDataType;
+using DataTypePtr = std::shared_ptr<const IDataType>;
+using DataTypes = VectorWithMemoryTracking<DataTypePtr>;
 
 /** The type SimpleAggregateFunction(fct, type) is meant to be used in an AggregatingMergeTree. It behaves like a standard
  * data type but when rows are merged, an aggregation function is applied.
