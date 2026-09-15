1e78d93c907693e921112a3b91c27ed06b287b46
yariks5s <yaroslav.briukhovetskyi@clickhouse.com>
2026-09-15 15:29:00 +0000

    Old analyzer can no longer be disabled, drop that line from the test

--- tests/queries/0_stateless/05182_limit_shrinks_block_size_with_array_join.sql
+++ tests/queries/0_stateless/05182_limit_shrinks_block_size_with_array_join.sql
@@ -3,12 +3,10 @@ DROP TABLE IF EXISTS t_aj_limit;
 CREATE TABLE t_aj_limit (k UInt64, a Array(UInt64)) ENGINE = MergeTree ORDER BY k SETTINGS index_granularity = 8;
 INSERT INTO t_aj_limit SELECT number, [number] FROM numbers(20000);
 
-SELECT arrayJoin(a) FROM t_aj_limit LIMIT 1 FORMAT Null SETTINGS enable_analyzer = 1, log_comment = '05182_analyzer';
-SELECT arrayJoin(a) FROM t_aj_limit LIMIT 1 FORMAT Null SETTINGS enable_analyzer = 0, log_comment = '05182_old';
+SELECT arrayJoin(a) FROM t_aj_limit LIMIT 1 FORMAT Null SETTINGS log_comment = '05182_limit';
 
 SYSTEM FLUSH LOGS query_log;
-SELECT log_comment, argMax(read_rows, event_time_microseconds) < 100 FROM system.query_log
-WHERE current_database = currentDatabase() AND type = 'QueryFinish' AND log_comment LIKE '05182_%'
-GROUP BY log_comment ORDER BY log_comment;
+SELECT argMax(read_rows, event_time_microseconds) < 100 FROM system.query_log
+WHERE current_database = currentDatabase() AND type = 'QueryFinish' AND log_comment = '05182_limit';
 
 DROP TABLE t_aj_limit;
