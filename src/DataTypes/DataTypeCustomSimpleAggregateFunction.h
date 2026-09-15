084dc561a5ff0f6f6d4c1c2c752c1356c436d824
vanchaklar <38177830+vanchaklar@users.noreply.github.com>
2026-09-15 14:42:38 +0330

    Propagate custom semantics through SimpleAggregateFunction

--- src/DataTypes/DataTypeCustomSimpleAggregateFunction.h
+++ src/DataTypes/DataTypeCustomSimpleAggregateFunction.h
@@ -3,15 +3,12 @@
 #include <AggregateFunctions/IAggregateFunction_fwd.h>
 #include <Core/Field.h>
 #include <DataTypes/DataTypeCustom.h>
+#include <DataTypes/IDataType.h>
 #include <Common/VectorWithMemoryTracking.h>
 
 
 namespace DB
 {
-
-class IDataType;
-using DataTypePtr = std::shared_ptr<const IDataType>;
-using DataTypes = VectorWithMemoryTracking<DataTypePtr>;
 
 /** The type SimpleAggregateFunction(fct, type) is meant to be used in an AggregatingMergeTree. It behaves like a standard
  * data type but when rows are merged, an aggregation function is applied.
@@ -45,6 +42,28 @@ public:
     const Array & getParameters() const { return parameters; }
     String getName() const override;
     std::optional<Field> getDefault() const override;
+
+    /// SimpleAggregateFunction values intentionally behave like their stored
+    /// argument type. Propagate strict custom semantics from that argument instead
+    /// of inventing a second semantic identity for the wrapper.
+    std::optional<String> getSemanticIdentity() const override
+    {
+        if (argument_types.size() == 1)
+            return getCustomTypeSemanticIdentity(argument_types[0]);
+        return std::nullopt;
+    }
+
+    bool requiresValueValidation() const override
+    {
+        return argument_types.size() == 1 && containsCustomTypeValueValidation(argument_types[0]);
+    }
+
+    void validateColumn(const IColumn & column, const String & operation) const override
+    {
+        if (argument_types.size() == 1)
+            validateCustomDataTypeColumn(column, argument_types[0], operation);
+    }
+
     static void checkSupportedFunctions(const AggregateFunctionPtr & function);
 };
 
