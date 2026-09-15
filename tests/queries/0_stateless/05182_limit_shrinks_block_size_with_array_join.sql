2e594a756e8a4c55f95697493a00739d6d2ac8e2
yariks5s <yaroslav.briukhovetskyi@clickhouse.com>
2026-09-15 17:50:34 +0000

    Pin max_threads in the test, the rows read scale with the streams

--- tests/queries/0_stateless/05182_limit_shrinks_block_size_with_array_join.sql
+++ tests/queries/0_stateless/05182_limit_shrinks_block_size_with_array_join.sql
@@ -3,7 +3,7 @@ DROP TABLE IF EXISTS t_aj_limit;
 CREATE TABLE t_aj_limit (k UInt64, a Array(UInt64)) ENGINE = MergeTree ORDER BY k SETTINGS index_granularity = 8;
 INSERT INTO t_aj_limit SELECT number, [number] FROM numbers(20000);
 
-SELECT arrayJoin(a) FROM t_aj_limit LIMIT 1 FORMAT Null SETTINGS log_comment = '05182_limit';
+SELECT arrayJoin(a) FROM t_aj_limit LIMIT 1 FORMAT Null SETTINGS max_threads = 8, log_comment = '05182_limit';
 
 SYSTEM FLUSH LOGS query_log;
 SELECT argMax(read_rows, event_time_microseconds) < 100 FROM system.query_log
