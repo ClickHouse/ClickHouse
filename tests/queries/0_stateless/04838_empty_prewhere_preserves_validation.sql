85442cdbf3a66274f26a06b7e4df9fed2c406819
yariks5s <yaroslav.briukhovetskyi@clickhouse.com>
2026-09-16 15:01:13 +0000

    Drop the old-analyzer arm: enable_analyzer = 0 is rejected since v26.9

--- tests/queries/0_stateless/04838_empty_prewhere_preserves_validation.sql
+++ tests/queries/0_stateless/04838_empty_prewhere_preserves_validation.sql
@@ -19,12 +19,10 @@ SELECT count()
 FROM empty_prewhere_preserves_validation
 PREWHERE (_partition_id = 'all') AND value IN (SELECT toUInt64(1) WHERE false)
 SETTINGS
-    enable_analyzer = 0,
     enable_parallel_replicas = 1,
     max_parallel_replicas = 3,
     cluster_for_parallel_replicas = 'test_cluster_one_shard_three_replicas_localhost',
     parallel_replicas_for_non_replicated_merge_tree = 1,
-    parallel_replicas_min_number_of_rows_per_replica = 1,
-    parallel_replicas_only_with_analyzer = 0;
+    parallel_replicas_min_number_of_rows_per_replica = 1;
 
 DROP TABLE empty_prewhere_preserves_validation;
