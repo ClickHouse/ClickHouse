43e139f01bf4a3814e82162bbacd4e2ca4fe5bde
Minh Vu <vuhoangminh97@gmail.com>
2026-09-18 11:11:41 +0200

    Fix UNION ALL BY NAME AST JSON test

--- tests/queries/0_stateless/05221_union_all_by_name.sql
+++ tests/queries/0_stateless/05221_union_all_by_name.sql
@@ -154,12 +154,9 @@ SELECT 1 AS y; -- { serverError TYPE_MISMATCH }
 
 SELECT formatQueryFromJSON(
     replace(
-        replace(
-            parseQueryToJSON('SELECT 1 AS a UNION ALL BY NAME SELECT 2 AS a'),
-            '"union_mode":"UNION_ALL"',
-            '"union_mode":"UNION_DISTINCT"'),
-        '"is_normalized":false',
-        '"is_normalized":true')); -- { serverError BAD_ARGUMENTS }
+        parseQueryToJSON('SELECT 1 AS a UNION ALL BY NAME SELECT 2 AS a'),
+        '"list_of_modes":["UNION_ALL"]',
+        '"list_of_modes":["UNION_DISTINCT"]')); -- { serverError BAD_ARGUMENTS }
 
 SELECT 'recursive CTE';
 WITH RECURSIVE r AS
