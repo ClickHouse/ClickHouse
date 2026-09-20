6f900c3c2573fed2647c78e2f76c3369f9d4a509
robot-clickhouse <robot-clickhouse@users.noreply.github.com>
2026-09-20 07:12:06 +0000

    Revert "Merge pull request #118423 from ClickHouse/inline-disk-for-database"
    This reverts commit 0cc1c6303cca67187040180a168b9937e3897487, reversing
    changes made to 1ed111c3867ce8cb7ae561eaa9d12944b60ee0d3.

--- src/Databases/DataLake/DatabaseDataLakeSettings.h
+++ src/Databases/DataLake/DatabaseDataLakeSettings.h
@@ -77,8 +77,6 @@ struct DatabaseDataLakeSettings
     /// so catalog code can match `SettingsChanges` entries without magic strings.
     static const String & getSettingName(DatabaseDataLakeSettingsString setting);
 
-    static bool hasBuiltin(std::string_view name);
-
 private:
     std::unique_ptr<DatabaseDataLakeSettingsImpl> impl;
 };
--- src/Databases/DatabaseMetadataDiskSettings.h
+++ src/Databases/DatabaseMetadataDiskSettings.h
@@ -30,8 +30,6 @@ struct DatabaseMetadataDiskSettings
 
     void loadFromQuery(ASTStorage & storage_def, ContextPtr context, bool is_loading_from_existing_metadata);
 
-    static bool hasBuiltin(std::string_view name);
-
 private:
     std::unique_ptr<DatabaseMetadataDiskSettingsImpl> impl;
 };
