7bda77be5a4b73fdbe0f241aaa9f92c1975567ea
robot-clickhouse <robot-clickhouse@users.noreply.github.com>
2026-09-20 08:19:13 +0000

    Backport #117573 to 26.9: Fix wrong aggregates from limit pushdown when ORDER BY is a strict prefix of GROUP BY

--- src/Processors/QueryPlan/AggregatingStep.cpp
+++ src/Processors/QueryPlan/AggregatingStep.cpp
@@ -601,6 +601,7 @@ void AggregatingStep::transformPipeline(QueryPipelineBuilder & pipeline, const B
                     many_data,
                     counter++,
                     limit_hint,
+                    limit_hint_prefix_columns,
                     nullptr // `dataflow_cache_updater` will be passed to `MergingAggregatedBucketTransform` below
                 );
             });
@@ -652,6 +653,7 @@ void AggregatingStep::transformPipeline(QueryPipelineBuilder & pipeline, const B
                     sort_description_for_merging, group_by_sort_description,
                     max_block_size, aggregation_in_order_max_block_bytes,
                     limit_hint,
+                    limit_hint_prefix_columns,
                     dataflow_cache_updater);
             });
 
