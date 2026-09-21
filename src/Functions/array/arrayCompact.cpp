98346c932f93fb310dcb2fc3d5b5920d69f464da
Minh Vu <vuhoangminh97@gmail.com>
2026-09-21 07:37:59 +0200

    Fix closing parenthesis in arrayCompact Decimal dispatch

--- src/Functions/array/arrayCompact.cpp
+++ src/Functions/array/arrayCompact.cpp
@@ -148,7 +148,7 @@ struct ArrayCompactImpl
             executeType<Decimal32>(mapped, array, res) ||
             executeType<Decimal64>(mapped, array, res) ||
             executeType<Decimal128>(mapped, array, res) ||
-            executeType<Decimal256>(mapped, array, res))
+            executeType<Decimal256>(mapped, array, res)))
         {
             executeGeneric(mapped, array, res);
         }
