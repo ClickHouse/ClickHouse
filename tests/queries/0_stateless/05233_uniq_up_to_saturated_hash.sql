4d2d967277d2d1ec97a81d161944f937ab79a5d8
Minh Vu <vuhoangminh97@gmail.com>
2026-09-21 11:35:57 +0200

    Fix uniqUpTo saturation documentation and cover remaining hash paths

--- tests/queries/0_stateless/05233_uniq_up_to_saturated_hash.sql
+++ tests/queries/0_stateless/05233_uniq_up_to_saturated_hash.sql
@@ -5,8 +5,16 @@ SELECT
     uniqUpTo(10)(toString(number % 10)) AS threshold_10,
     uniqUpTo(10)(toString(number % 11)) AS threshold_10_plus_one,
     uniqUpTo(100)(toString(number % 100)) AS threshold_100,
+    uniqUpTo(100)(toString(number % 101)) AS threshold_100_plus_one,
+    uniqUpTo(1)(toFixedString(toString(number % 2), 8)) AS fixed_string_value,
     uniqUpTo(1)(toUInt128(number % 2)) AS uint128_value,
+    uniqUpTo(1)(toUInt256(number % 2)) AS uint256_value,
+    uniqUpTo(1)(toInt256(number % 2) - 1) AS int256_value,
+    uniqUpTo(1)(toUUID(concat('00000000-0000-0000-0000-00000000000', toString(number % 2)))) AS uuid_value,
     uniqUpTo(10)((toString(number % 10), number % 2)) AS tuple_value,
+    uniqUpTo(1)((toString(number % 2), number % 2)) AS saturated_tuple_value,
+    uniqUpTo(1)([number % 2]) AS array_value,
+    uniqUpTo(1)(([number % 2], toString(number % 2))) AS tuple_array_value,
     uniqUpTo(1)(toString(number % 2), number % 2) AS variadic_value
 FROM numbers(1000);
 
