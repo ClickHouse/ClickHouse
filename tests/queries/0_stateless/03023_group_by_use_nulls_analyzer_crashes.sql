8e940d000bf9cb4340289d4103bf15470203e99d
Minh Vu <vuhoangminh97@gmail.com>
2026-09-21 18:51:30 +0200

    Fix CUBE/ROLLUP regression test after tuple-key change

--- tests/queries/0_stateless/03023_group_by_use_nulls_analyzer_crashes.sql
+++ tests/queries/0_stateless/03023_group_by_use_nulls_analyzer_crashes.sql
@@ -1,6 +1,6 @@
 set enable_analyzer = 1, group_by_use_nulls = 1;
 
-SELECT tuple(tuple(number)) as x FROM numbers(10) GROUP BY (number, tuple(number)) with cube order by x;
+SELECT tuple(tuple(number)) as x FROM numbers(10) GROUP BY number, tuple(number) with cube order by x;
 
 select tuple(array(number)) as x FROM numbers(10) GROUP BY number, array(number) WITH ROLLUP order by x;
 
@@ -12,7 +12,7 @@ SELECT toLowCardinality(materialize('a' AS key)),  'b' AS value GROUP BY key WIT
 
 SELECT tuple(tuple(number)) AS x
 FROM numbers(10)
-GROUP BY (number, (toString(x), number))
+GROUP BY number, (toString(x), number)
     WITH CUBE
 SETTINGS group_by_use_nulls = 1 FORMAT Null;
 
@@ -50,7 +50,7 @@ SELECT arrayMap(x -> '.', range(number % 10)) AS k FROM remote('127.0.0.{2,3}', 
 
 SELECT count('Lambda as function parameter') AS c FROM (SELECT ignore(ignore('Lambda as function parameter', 28, 28, 28, 28, 28, 28), 28), materialize('Lambda as function parameter'), 28, 28, 'world', 5 FROM system.numbers WHERE ignore(materialize('Lambda as function parameter'), materialize(toLowCardinality(28)), 28, 28, 28, 28, toUInt128(28)) LIMIT 2) GROUP BY GROUPING SETS ((toLowCardinality(0)), (toLowCardinality(toNullable(28))), (1)) HAVING nullIf(c, 10) < 50 ORDER BY c ASC NULLS FIRST settings group_by_use_nulls=1; -- { serverError ILLEGAL_AGGREGATION }
 
-SELECT arraySplit(x -> 0, []) WHERE materialize(1) GROUP BY (0, ignore('a')) WITH ROLLUP SETTINGS group_by_use_nulls = 1;
+SELECT arraySplit(x -> 0, []) WHERE materialize(1) GROUP BY 0, ignore('a') WITH ROLLUP SETTINGS group_by_use_nulls = 1;
 
 SELECT arraySplit(x -> toUInt8(number), []) from numbers(1) GROUP BY toUInt8(number) WITH ROLLUP SETTINGS group_by_use_nulls = 1;
 
