4a3581275c6576c7d500a6eb76229a98add48891
Ben <38177830+vanchaklar@users.noreply.github.com>
2026-09-21 16:16:50 +0330

    Narrow decay ordering-key integration

--- src/Storages/MergeTree/MergeTreeDataPartWriterOnDisk.cpp
+++ src/Storages/MergeTree/MergeTreeDataPartWriterOnDisk.cpp
@@ -261,9 +261,9 @@ void MergeTreeDataPartWriterOnDisk::calculateAndSerializePrimaryIndexRow(const B
             const auto full_column = column->convertToFullColumnIfConst();
             const auto & decaying
                 = assert_cast<const ColumnExponentialTimeDecaying &>(*full_column);
-            const auto & ordering_key
+            const auto & prefix
                 = assert_cast<const ColumnUInt64 &>(decaying.getOrderingKeyColumn());
-            const UInt64 key = ordering_key.getData()[row];
+            const UInt64 key = prefix.getData()[row];
             writeBinaryLittleEndian(key, index_stream);
 
             if (settings.save_primary_index_in_memory)
