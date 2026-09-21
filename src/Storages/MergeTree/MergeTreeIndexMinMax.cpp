590a81fad371e7a9cd2f57a561bfb6cd13fb4b5c
Ben <38177830+vanchaklar@users.noreply.github.com>
2026-09-21 15:38:35 +0330

    Enable decay minmax index validation

--- src/Storages/MergeTree/MergeTreeIndexMinMax.cpp
+++ src/Storages/MergeTree/MergeTreeIndexMinMax.cpp
@@ -631,13 +631,6 @@ void minmaxIndexValidator(const IndexDescription & index, bool attach, const Mer
 
     for (const auto & column : index.sample_block)
     {
-        if (containsExponentialTimeDecayingFloat64(column.type))
-            throw Exception(
-                ErrorCodes::BAD_ARGUMENTS,
-                "Data type {} of column {} is not allowed in a minmax index because its exact ordering requires the column comparator",
-                column.type->getName(),
-                column.name);
-
         if (!column.type->isComparable())
         {
             throw Exception(ErrorCodes::BAD_ARGUMENTS,
