871888d7d4e7cac82d00a4b8a42ef41db082f378
Ben <38177830+vanchaklar@users.noreply.github.com>
2026-09-21 15:07:09 +0330

    Use compact decay keys in part splitting

--- src/Processors/QueryPlan/PartsSplitter.cpp
+++ src/Processors/QueryPlan/PartsSplitter.cpp
@@ -62,7 +62,6 @@ bool isSafePrimaryDataKeyType(const IDataType & data_type)
         case TypeIndex::Object:
         case TypeIndex::Variant:
         case TypeIndex::Dynamic:
-        case TypeIndex::ExponentialTimeDecayingFloat64:
             return false;
         case TypeIndex::Array:
         {
