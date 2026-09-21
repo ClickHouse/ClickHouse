1b91ca451a0780be365da8e54efc6d5fab773f58
Minh Vu <vuhoangminh97@gmail.com>
2026-09-21 20:00:07 +0200

    Fix recursive UNION BY NAME member alignment

--- src/Processors/Sources/RecursiveCTESource.cpp
+++ src/Processors/Sources/RecursiveCTESource.cpp
@@ -203,7 +203,10 @@ private:
         auto convert_to_temporary_tables_header_actions_dag = ActionsDAG::makeConvertingActions(
             pipeline_builder.getHeader().getColumnsWithTypeAndName(),
             header->getColumnsWithTypeAndName(),
-            ActionsDAG::MatchColumnsMode::Position,
+            recursive_query->as<UnionNode>()
+                && recursive_query->as<UnionNode &>().getColumnMatchMode() == SetOperationColumnMatchMode::Name
+                ? ActionsDAG::MatchColumnsMode::Name
+                : ActionsDAG::MatchColumnsMode::Position,
             interpreter->getContext());
         auto convert_to_temporary_tables_header_actions = std::make_shared<ExpressionActions>(std::move(convert_to_temporary_tables_header_actions_dag));
         pipeline_builder.addSimpleTransform([&](const SharedHeader & input_header)
