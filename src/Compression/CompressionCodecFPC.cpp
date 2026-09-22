aa929f372ca8a72fe95ab935e84b49c7ec5e59fa
Robert Schulze <robert@clickhouse.com>
2026-09-22 10:44:13 +0000

    Fix build

--- src/Compression/CompressionCodecFPC.cpp
+++ src/Compression/CompressionCodecFPC.cpp
@@ -15,6 +15,9 @@
 namespace DB
 {
 
+template <typename T>
+using VectorWithMemoryTracking = std::vector<T, AllocatorWithMemoryTracking<T>>;
+
 /// An implementation of the FPC codec for floating-point values described in the paper
 ///   M. Burtscher, P. Ratanaworabhan: "FPC: A high-speed compressor for double-precision floating-point data" (2008).
 /// Note: The paper only describes compression of 64-bit doubles and leaves 32-bit floats to future work. The code
