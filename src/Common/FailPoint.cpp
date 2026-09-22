2c89004b4d3d76173f07e5612f8f7abad86c9b27
robot-clickhouse <robot-clickhouse@users.noreply.github.com>
2026-09-22 16:37:26 +0000

    Backport #118457 to 26.8: Fix Paimon incremental read failing with REPLICA_IS_ALREADY_ACTIVE after a Keeper session loss

--- src/Common/FailPoint.cpp
+++ src/Common/FailPoint.cpp
@@ -161,6 +161,7 @@ static struct InitFiu
     PAUSEABLE_ONCE(kafka2_remove_zk_before_get_children) \
     PAUSEABLE_ONCE(kafka2_remove_zk_before_final_multi) \
     PAUSEABLE_ONCE(keeper_map_delete_pause_before_multi) \
+    PAUSEABLE_ONCE(paimon_incremental_read_pause_before_is_active_remove) \
     PAUSEABLE(dummy_pausable_failpoint) \
     PAUSEABLE(paimon_incremental_read_pause_after_watermark_commit) \
     ONCE(execute_query_calling_empty_set_result_func_on_exception) \
