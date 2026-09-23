4a03bb318cf3c84c914a7df4a48f33117ee99e9f
robot-clickhouse <robot-clickhouse@users.noreply.github.com>
2026-09-23 17:28:23 +0000

    Backport #119585 to 26.8: Detach a patch part whose index lost its content to a zeroed block

--- src/Common/FailPoint.cpp
+++ src/Common/FailPoint.cpp
@@ -75,6 +75,7 @@ static struct InitFiu
     ONCE(rmt_lightweight_update_sleep_after_block_allocation) \
     ONCE(rmt_merge_task_sleep_in_prepare) \
     ONCE(merge_tree_refresh_parts_throw_once) \
+    REGULAR(patch_part_index_write_empty) \
     ONCE(s3_read_buffer_throw_expired_token) \
     ONCE(s3_send_request_throw_expired_token) \
     REGULAR(s3_read_inject_etag_mismatch) \
