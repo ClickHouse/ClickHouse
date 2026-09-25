a2c9ce1940751c1be749d8af3a4cbc8a9b341065
Jaysurya Ray <jaysuryaray1998@gmail.com>
2026-09-25 13:10:28 -0400

    Add view attach regression coverage

--- tests/queries/0_stateless/05238_identifier_quotes.sh
+++ tests/queries/0_stateless/05238_identifier_quotes.sh
@@ -18,4 +18,44 @@ check_roundtrip 'SELECT `cube`, count() FROM t GROUP BY `cube`'
 check_roundtrip 'SELECT `rollup`, count() FROM t GROUP BY `rollup`'
 check_roundtrip 'WITH `recursive` AS (SELECT 1 AS x) SELECT * FROM `recursive`'
 
-echo "OK"
+$CLICKHOUSE_CLIENT --multiquery <<'SQL'
+CREATE TABLE identifier_quotes_t (`cube` UInt8, `rollup` UInt8)
+ENGINE = MergeTree
+ORDER BY tuple();
+
+INSERT INTO identifier_quotes_t VALUES (1, 2);
+
+CREATE VIEW identifier_quotes_v_cube AS
+SELECT `cube`, count() AS c
+FROM identifier_quotes_t
+GROUP BY `cube`;
+
+DETACH TABLE identifier_quotes_v_cube;
+ATTACH TABLE identifier_quotes_v_cube;
+
+SELECT * FROM identifier_quotes_v_cube;
+
+CREATE VIEW identifier_quotes_v_rollup AS
+SELECT `rollup`, count() AS c
+FROM identifier_quotes_t
+GROUP BY `rollup`;
+
+DETACH TABLE identifier_quotes_v_rollup;
+ATTACH TABLE identifier_quotes_v_rollup;
+
+SELECT * FROM identifier_quotes_v_rollup;
+
+CREATE VIEW identifier_quotes_v_recursive AS
+WITH `recursive` AS (SELECT 1 AS x)
+SELECT * FROM `recursive`;
+
+DETACH TABLE identifier_quotes_v_recursive;
+ATTACH TABLE identifier_quotes_v_recursive;
+
+SELECT * FROM identifier_quotes_v_recursive;
+
+DROP VIEW identifier_quotes_v_cube;
+DROP VIEW identifier_quotes_v_rollup;
+DROP VIEW identifier_quotes_v_recursive;
+DROP TABLE identifier_quotes_t;
+SQL
